6152975ab7ba06deb01821e7f2742fbd98b20058
sgk@google.com <sgk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-22 21:50:43 +0000

    Correct the %PATH% to svn.
    git-svn-id: svn://svn.chromium.org/chrome/trunk@1247 0039d316-1c4b-4281-b951-d872f2087c98

--- depot_tools/release/win/bin/gclient.bat
+++ depot_tools/release/win/bin/gclient.bat
@@ -1,10 +1,10 @@
 @echo off
 
 :: This batch file assumes that the correct version of svn and python can be
-:: found in the current directory.
+:: found in the parent directory.
 
 setlocal
-set PATH=%~dp0\svn;%WINDIR%\system32
+set PATH=%~dp0..\svn;%WINDIR%\system32
 
 xcopy %~dp0..\wrappers\*.* %~dp0..\..\bin /e /c /d /y /q > nul
 
