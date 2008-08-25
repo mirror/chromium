203e2a90a0f27368cc9e9ee4e03dbbcd45573dd4
sgk@google.com <sgk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-25 18:40:47 +0000

    Have mac and linux copy the bootstrap directory contents, too.
    git-svn-id: svn://svn.chromium.org/chrome/trunk@1319 0039d316-1c4b-4281-b951-d872f2087c98

--- depot_tools/release/linux/gclient.sh
+++ depot_tools/release/linux/gclient.sh
@@ -3,5 +3,6 @@
 base_dir=$(dirname "$0")
 
 cp -r $base_dir/wrappers/* $base_dir/.. > /dev/null
+cp -r $base_dir/wrappers/bootstrap/* $base_dir/../bootstrap > /dev/null
 
 exec python $base_dir/gclient.py $*
