bd2e8e6d22b9c79683c8c5d892c27a397ded090e
sgk@google.com <sgk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-08-25 17:19:09 +0000

    Move the Windows files up a layer as well...
    git-svn-id: svn://svn.chromium.org/chrome/trunk@1311 0039d316-1c4b-4281-b951-d872f2087c98

--- depot_tools/release/win/gclient.bat
+++ depot_tools/release/win/gclient.bat
@@ -6,6 +6,6 @@
 setlocal
 set PATH=%~dp0..\svn;%WINDIR%\system32
 
-xcopy %~dp0..\wrappers\*.* %~dp0..\..\bin /e /c /d /y /q > nul
+xcopy %~dp0wrappers\*.* %~dp0.. /e /c /d /y /q > nul
 
 %~dp0..\python_24\python.exe %~dp0\gclient.py %*
