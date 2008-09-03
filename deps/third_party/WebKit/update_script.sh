2d46b741d95b8a18d596f6cd9f522e2ccf6fb2e0
eseidel@google.com <eseidel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-09-03 22:33:30 +0000

    Undo r1695
    git-svn-id: svn://svn.chromium.org/chrome/trunk@1696 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ deps/third_party/WebKit/update_script.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -x
+
+# TODO(eseidel): This script assumes it's run in third_party/WebKit
+# This script should be converted to python some day
+# and be made less-lame
+
+# This script also assumes that all of the WebKit files are
+# currently unforked (no .obs suffix)
+
+# Eventually this should be read from BASE_REVISION
+OLD_BASE="http://svn.webkit.org/repository/webkit/trunk"
+OLD_REV=35981
+
+NEW_BASE="http://svn.webkit.org/repository/webkit/trunk"
+NEW_REV=35999
+
+svn update
+svn merge --accept theirs-full "${OLD_BASE}/JavaScriptCore@${OLD_REV}" "${NEW_BASE}/JavaScriptCore@${NEW_REV}" JavaScriptCore/
+svn merge --accept theirs-full "${OLD_BASE}/WebCore@${OLD_REV}" "${NEW_BASE}/WebCore@${NEW_REV}" WebCore/
+svn merge --accept theirs-full "${OLD_BASE}/WebKitLibraries@${OLD_REV}" "${NEW_BASE}/WebKitLibraries@${NEW_REV}" WebKitLibraries/
+
+echo "${NEW_BASE}@${NEW_REV}" > BASE_REVISION
