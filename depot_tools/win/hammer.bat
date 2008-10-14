07121277b903b87f1f3412b13a0a221da69d610f
sgk@google.com <sgk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-14 22:21:49 +0000

    Update the Windows and POSIX hammer{,.bat} scripts to support executing directly from the src/ directory, not just in the module subdirectories. Review URL: http://codereview.chromium.org/7145
    git-svn-id: svn://svn.chromium.org/chrome/trunk@3372 0039d316-1c4b-4281-b951-d872f2087c98

--- depot_tools/win/hammer.bat
+++ depot_tools/win/hammer.bat
@@ -1,3 +1,10 @@
 @echo off
 
+IF EXIST "%cd%\third_party\python_24\python.exe" GOTO srcdir
+
+@rem We're in a submodule directory, look relative to the parent.
 "%cd%\..\third_party\python_24\python.exe" "%cd%\..\third_party\scons\scons.py" %*
+exit /b %ERRORLEVEL%
+
+:srcdir
+"%cd%\third_party\python_24\python.exe" "%cd%\third_party\scons\scons.py" %*
