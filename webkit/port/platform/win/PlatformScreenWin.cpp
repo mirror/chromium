1a936f9dfdaf320f09854e49748d7ca4b8f88564
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-10-22 23:12:02 +0000

    ScreenWin.cpp got renamed PlatformScreenWin.cpp, and there is a new way to get at the containing HWND for the Widget.  So, this change is very mechanical.
    R=dglazkov
    
    Review URL: http://codereview.chromium.org/7891
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/chrome_webkit_merge_branch/src@3790 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/win/PlatformScreenWin.cpp
+++ webkit/port/platform/win/PlatformScreenWin.cpp
@@ -24,7 +24,7 @@
  */
 
 #include "config.h"
-#include "Screen.h"
+#include "PlatformScreen.h"
 
 #include "IntRect.h"
 #include "FloatRect.h"
@@ -44,7 +44,7 @@ static FloatRect ToFloatRect(const RECT& rect) {
 // Returns info for the default monitor if widget is NULL
 static MONITORINFOEX monitorInfoForWidget(Widget* widget)
 {
-    HWND window = widget ? widget->containingWindow() : 0;
+    HWND window = widget ? widget->root()->hostWindow()->platformWindow() : 0;
     return webkit_glue::GetMonitorInfoForWindow(window);
 }
 
