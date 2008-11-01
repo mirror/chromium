44e55700bb32f06fe77c14df8889d7128c2259aa
niranjan@google.com <niranjan@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-01 00:04:25 +0000

    Merge for r3391:
    A fix for Issue 2673 in chromium: "Flash: IME not available".
    
    To investigate this issue deeply, I noticed this issue consisted of two issues:
    
    1. Windows cannot attach IMEs to a plug-in process.
    Somehow, Windows attaches IMEs (i.e. loads IME DLLs, attaches them to a process, and adds message hooks to a window) while creating a top-level window. On the other hand, a plug-in process does not create any top-level windows. These facts prevent Windows from attaching IMEs to windowed plug-ins.
    
    2. The CallWindowProc() function does not dispatch messages to the message-hook functions used by IMEs.
    When the |plugin_wnd_proc_| value is a handle referring the DefWindowProc() function, it seems the CallWindowProc() does not dispatch messages to the hook functions used by IMEs.
    
    To fix the former issue, this change once creates a top-level window and have Windows attach IMEs to this window, and change the window to a client window of the given parent window.
    To fix the latter issue, this change creates a dummy window procedure WebPluginDelegateImpl::DummyWindowProc(), which just encapsulates the DefWindowProc(), and use it instead of the DefWindowProc() function.
    
    BUG=2673
    
    Review URL: http://codereview.chromium.org/8795
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/chrome_official_branch/src@4350 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/plugins/webplugin_delegate_impl.h
+++ webkit/glue/plugins/webplugin_delegate_impl.h
@@ -129,6 +129,8 @@ class WebPluginDelegateImpl : public WebPluginDelegate {
   ATOM RegisterNativeWindowClass();
 
   // Our WndProc functions.
+  static LRESULT CALLBACK DummyWindowProc(
+      HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
   static LRESULT CALLBACK NativeWndProc(
       HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
   static LRESULT CALLBACK FlashWindowlessWndProc(
