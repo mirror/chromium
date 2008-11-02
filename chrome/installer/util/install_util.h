591b09c4aa64871e4bed423beb27375421546191
mal@chromium.org <mal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-02 05:06:17 +0000

    Oops. Bad merge tool.
    TBR= kuchhal@chromium.org
    Review URL: http://codereview.chromium.org/9020
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/chrome_official_branch/src@4408 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/install_util.h
+++ chrome/installer/util/install_util.h
@@ -1,53 +1,3 @@
-<<<<<<< .working
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-//
-// This file declares utility functions for the installer. The original reason
-// for putting these functions in installer\util library is so that we can
-// separate out the critical logic and write unit tests for it.
-
-#ifndef CHROME_INSTALLER_UTIL_INSTALL_UTIL_H__
-#define CHROME_INSTALLER_UTIL_INSTALL_UTIL_H__
-
-#include <string>
-#include <windows.h>
-
-#include "base/basictypes.h"
-#include "chrome/installer/util/util_constants.h"
-#include "chrome/installer/util/version.h"
-
-// This is a utility class that provides common installation related
-// utility methods that can be used by installer and also unit tested
-// independently.
-class InstallUtil {
- public:
-  // Launches given exe as admin on Vista.
-  static bool ExecuteExeAsAdmin(const std::wstring& exe,
-                                const std::wstring& params,
-                                DWORD* exit_code);
-
-  // Reads the uninstall command for Chromium from registry and returns it.
-  // If system_install is true the command is read from HKLM, otherwise
-  // from HKCU.
-  static std::wstring GetChromeUninstallCmd(bool system_install);
-  // Find the version of Chrome installed on the system by checking the
-  // Google Update registry key. Returns the version or NULL if no version is
-  // found.
-  // system_install: if true, looks for version number under the HKLM root,
-  //                 otherwise looks under the HKCU.
-  static installer::Version * GetChromeVersion(bool system_install);
-
-  // This function checks if the current OS is supported for Chromium.
-  static bool IsOSSupported();
- private:
-  DISALLOW_EVIL_CONSTRUCTORS(InstallUtil);
-};
-
-
-#endif  // CHROME_INSTALLER_UTIL_INSTALL_UTIL_H__
-
-=======
 // Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -101,5 +51,3 @@ class InstallUtil {
 
 
 #endif  // CHROME_INSTALLER_UTIL_INSTALL_UTIL_H__
-
->>>>>>> .merge-right.r3617
