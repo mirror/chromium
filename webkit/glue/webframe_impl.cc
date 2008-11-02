71293131f4d25f7ad73fc496cbfa1505035d46b1
mal@chromium.org <mal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-02 05:37:14 +0000

    Merge r3619 to the release branch:
    Fixes LayoutTests/http/tests/navigation/reloadsubframe-*frame.html.
    Also fixes an issue with laoding an iframe with a dynamically set src attr.
    
    TBR= mmoss@chromium.org
    Review URL: http://codereview.chromium.org/9021
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/chrome_official_branch/src@4410 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webframe_impl.cc
+++ webkit/glue/webframe_impl.cc
@@ -1677,7 +1677,6 @@ void WebFrameImpl::CreateChildFrame(const FrameLoadRequest& r,
   // Reload will maintain the frame contents, LoadSame will not.
   if (parentItem && parentItem->children().size() != 0 &&
       (isBackForwardLoadType(loadType) ||
-       loadType == WebCore::FrameLoadTypeReload ||
        loadType == WebCore::FrameLoadTypeReloadAllowingStaleData)) {
     HistoryItem* childItem = parentItem->childItemWithName(r.frameName());
     if (childItem) {
