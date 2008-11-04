56c9f6a3f1fd2cd551161765d14d6acae9dc142d
fqian@google.com <fqian@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-04 23:35:22 +0000

    add two new cursor functions
    git-svn-id: svn://svn.chromium.org/chrome/branches/chrome_webkit_merge_branch/src@4704 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/platform/chromium/CursorChromium.cpp
+++ webkit/port/platform/chromium/CursorChromium.cpp
@@ -334,4 +334,14 @@ const Cursor& zoomOutCursor()
     return c;
 }
 
+const Cursor& grabCursor()
+{
+    return pointerCursor();
+}
+
+const Cursor& grabbingCursor()
+{
+    return pointerCursor();
+}
+
 }
