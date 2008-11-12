13fc463844191d26a432c5f492693fe27db8c8f8
laforge@chromium.org <laforge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-12 19:35:05 +0000

    Merge 5178 - Ensure that windowed plugins get focus during WM_MOUSEACTIVATE. This fixes bug http://code.google.com/p/chromium/issues/detail?id=4273, which shows up when windowed plugins enter modal loops like context menu and keyboard navigation does not work in the menu.
    We were not converting the coordinates returned by GetCursorPos to client coordinates correctly, which caused us to miss the child window (Plugin) at times.
    
    This also fixes bug http://code.google.com/p/chromium/issues/detail?id=937, which was an issue with menu selections in Java menus. This occured because we would force 
    the child window to have focus in every WM_MOUSEACTIVATE message. In this case the Java plugin window, which is a child of the plugin window already has focus. It receives a WM_KILLFOCUS message due to the forced SetFocus in our handler and takes out the menu, thus ignoring the menu selection. Fix for this issue is to handle WM_MOUSEACTIVATE only if a child window of RenderWidgetHostHWND does not have focus.
    
    R=amit
    Bug=4273
    
    Review URL: http://codereview.chromium.org/10004
    
    TBR=ananta@chromium.org
    
    Merger's note - There are some differences between render_widget_host_view_win.cc and render_widget_host_hwnd.cc, since the only thing that changed in _win was a single method, that's what I updated.
    
    Review URL: http://codereview.chromium.org/10644
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/chrome_official_branch/src@5277 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/render_widget_host_hwnd.cc
+++ chrome/browser/render_widget_host_hwnd.cc
@@ -728,26 +728,28 @@ LRESULT RenderWidgetHostHWND::OnWheelEvent(UINT message, WPARAM wparam,
 }
 
 LRESULT RenderWidgetHostHWND::OnMouseActivate(UINT, WPARAM, LPARAM,
-                                              BOOL& handled) {
-  // We handle WM_MOUSEACTIVATE to set focus to the underlying plugin
-  // child window. This is to ensure that keyboard events are received
-  // by the plugin. The correct way to fix this would be send over
-  // an event to the renderer which would then eventually send over
-  // a setFocus call to the plugin widget. This would ensure that
-  // the renderer (webkit) knows about the plugin widget receiving
-  // focus.
-  // TODO(iyengar) Do the right thing as per the above comment.
-  POINT cursor_pos = {0};
-  ::GetCursorPos(&cursor_pos);
-  MapWindowPoints(m_hWnd, &cursor_pos, 1);
-  HWND child_window = ::RealChildWindowFromPoint(m_hWnd, cursor_pos);
-  if (::IsWindow(child_window)) {
-    ::SetFocus(child_window);
-    return MA_NOACTIVATE;
-  } else {
-    handled = FALSE;
-    return MA_ACTIVATE;
+                                                 BOOL& handled) {
+  HWND focus_window = GetFocus();
+  if (!::IsWindow(focus_window) || !IsChild(focus_window)) {
+    // We handle WM_MOUSEACTIVATE to set focus to the underlying plugin
+    // child window. This is to ensure that keyboard events are received
+    // by the plugin. The correct way to fix this would be send over
+    // an event to the renderer which would then eventually send over
+    // a setFocus call to the plugin widget. This would ensure that
+    // the renderer (webkit) knows about the plugin widget receiving
+    // focus.
+    // TODO(iyengar) Do the right thing as per the above comment.
+    POINT cursor_pos = {0};
+    ::GetCursorPos(&cursor_pos);
+    ::ScreenToClient(m_hWnd, &cursor_pos);
+    HWND child_window = ::RealChildWindowFromPoint(m_hWnd, cursor_pos);
+    if (::IsWindow(child_window)) {
+      ::SetFocus(child_window);
+      return MA_NOACTIVATE;
+    }
   }
+  handled = FALSE;
+  return MA_ACTIVATE;
 }
 
 LRESULT RenderWidgetHostHWND::OnGetObject(UINT message, WPARAM wparam,
