e9ac981d0b467983e327f0486ea0bd9e127b5981
mal@chromium.org <mal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-15 02:18:18 +0000

    Revert r4962 (trunk r4934) because of regressions.
    BUG=4007
    TBR=erg
    Review URL: http://codereview.chromium.org/10772
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/chrome_official_branch/src@5528 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents.cc
+++ chrome/browser/tab_contents.cc
@@ -517,8 +517,3 @@ void TabContents::RepositionSupressedPopupsToFit(const gfx::Size& new_size) {
 void TabContents::ReleaseDownloadShelfView() {
   download_shelf_view_.release();
 }
-
-bool TabContents::ShowingBlockedPopupNotification() const {
-  return blocked_popups_ != NULL &&
-      blocked_popups_->GetTabContentsCount() != 0;
-}
