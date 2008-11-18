c864341a4afcd848c04ff36fe877924b469ea692
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-11-18 22:20:01 +0000

    Remove some duplicated lines; this looks like a merge error to me. Review URL: http://codereview.chromium.org/11246
    git-svn-id: svn://svn.chromium.org/chrome/trunk@5641 0039d316-1c4b-4281-b951-d872f2087c98

--- deps/third_party/WebKit/JavaScriptCore/wtf/Platform.h
+++ deps/third_party/WebKit/JavaScriptCore/wtf/Platform.h
@@ -370,10 +370,6 @@
 #define ENABLE_JAVASCRIPT_DEBUGGER 1
 #endif
 
-#if !defined(ENABLE_JAVASCRIPT_DEBUGGER)
-#define ENABLE_JAVASCRIPT_DEBUGGER 1
-#endif
-
 #if !defined(ENABLE_FTPDIR)
 #define ENABLE_FTPDIR 1
 #endif
