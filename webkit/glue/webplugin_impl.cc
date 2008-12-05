126cb62d15c0fed99f21425a2b071b80f7f9a457
mal@chromium.org <mal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-05 04:52:10 +0000

    Merge r6396 to the release branch.
    This fixes bug 5009, a problem with large PDFs loading slowly or sometimes not
    at all.
    
    BUG=5009
    TBR=ananta
    Review URL: http://codereview.chromium.org/12978
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/chrome_official_branch/src@6419 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin_impl.cc
+++ webkit/glue/webplugin_impl.cc
@@ -1213,10 +1213,6 @@ bool WebPluginImpl::InitiateHTTPRequest(int resource_id,
   return true;
 }
 
-void WebPluginImpl::CancelDocumentLoad() {
-  frame()->loader()->stopLoading(false);
-}
-
 void WebPluginImpl::InitiateHTTPRangeRequest(const char* url,
                                              const char* range_info,
                                              HANDLE existing_stream,
