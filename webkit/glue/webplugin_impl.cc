12e40cb96c5f328f864163e5b29401b9d7957fd3
laforge@chromium.org <laforge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-12 17:13:27 +0000

    Merge 6486 - Improve PDF FastWebView performance. This occurs because the manual data stream load does not get cancelled correctly. This causes the IPC messages for the manual data stream to continue going back and forth. They are ignored by the plugin anyways.
    This fixes http://code.google.com/p/chromium/issues/detail?id=5196. 
    
    Fix is to cancel the manual document load correctly.
    
    Bug=5196
    
    Review URL: http://codereview.chromium.org/13198
    TBR=ananta@chromium.org
    Review URL: http://codereview.chromium.org/14054
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/chrome_official_branch/src@6892 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webplugin_impl.cc
+++ webkit/glue/webplugin_impl.cc
@@ -6,6 +6,7 @@
 
 #pragma warning(push, 0)
 #include "Document.h"
+#include "DocumentLoader.h"
 #include "Element.h"
 #include "Event.h"
 #include "EventNames.h"
@@ -100,7 +101,10 @@ class MultiPartResponseClient : public WebCore::ResourceHandleClient {
   WebPluginResourceClient* resource_client_;
 };
 
-WebPluginContainer::WebPluginContainer(WebPluginImpl* impl) : impl_(impl) { }
+WebPluginContainer::WebPluginContainer(WebPluginImpl* impl) 
+    : impl_(impl),
+      ignore_response_error_(false) {
+}
 
 WebPluginContainer::~WebPluginContainer() {
   impl_->SetContainer(NULL);
@@ -189,6 +193,8 @@ void WebPluginContainer::windowCutoutRects(const WebCore::IntRect& bounds,
 void WebPluginContainer::didReceiveResponse(
     const WebCore::ResourceResponse& response) {
   
+  set_ignore_response_error(false);
+
   HttpResponseInfo http_response_info;
   ReadHttpResponseInfo(response, &http_response_info);
 
@@ -209,7 +215,8 @@ void WebPluginContainer::didFinishLoading() {
 }
 
 void WebPluginContainer::didFail(const WebCore::ResourceError&) {
-  impl_->delegate_->DidManualLoadFail();
+  if (!ignore_response_error_)
+    impl_->delegate_->DidManualLoadFail();
 }
 
 void WebPluginContainer::ReadHttpResponseInfo(
@@ -1214,7 +1221,10 @@ bool WebPluginImpl::InitiateHTTPRequest(int resource_id,
 }
 
 void WebPluginImpl::CancelDocumentLoad() {
-  frame()->loader()->stopLoading(false);
+  if (frame()->loader()->activeDocumentLoader()) {
+    widget_->set_ignore_response_error(true);
+    frame()->loader()->activeDocumentLoader()->stopLoading();
+  }
 }
 
 void WebPluginImpl::InitiateHTTPRangeRequest(const char* url,
