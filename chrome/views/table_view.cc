29c007a829673947a2c2a9e2dab2e6ffef6b3fbf
laforge@chromium.org <laforge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-12 21:50:58 +0000

    Merge 6734 - Fixes bug where context menu for table/tree in rtl was showing up on the wrong side.
    BUG=5067
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/13353
    TBR=sky@google.com
    
    Review URL: http://codereview.chromium.org/14080
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/chrome_official_branch/src@6932 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/views/table_view.cc
+++ chrome/views/table_view.cc
@@ -519,6 +519,8 @@ gfx::Point TableView::GetKeyboardContextMenuLocation() {
     }
   }
   gfx::Point screen_loc(0, y);
+  if (UILayoutIsRightToLeft())
+    screen_loc.set_x(width());
   ConvertPointToScreen(this, &screen_loc);
   return screen_loc;
 }
