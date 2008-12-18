a67738a40b5e7a68c27ab979992618d1d31e84bb
brettw@google.com <brettw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2008-12-18 17:40:37 +0000

    Unfork KURL.h. This will require some changes in the chrome side when I pull in this revision of WebKit. Review URL: http://codereview.chromium.org/14174
    git-svn-id: svn://svn.chromium.org/chrome/trunk@7221 0039d316-1c4b-4281-b951-d872f2087c98

--- deps/third_party/WebKit/WebCore/platform/KURL.h
+++ deps/third_party/WebKit/WebCore/platform/KURL.h
@@ -28,17 +28,6 @@
 
 #include "PlatformString.h"
 
-#ifdef USE_GOOGLE_URL_LIBRARY
-#include "CString.h"
-#include "googleurl/src/url_canon.h"
-#include "googleurl/src/url_parse.h"
-// TODO(brettw): Crazy hack.  Because these headers pull in base/basictypes.h,
-// which redefines COMPILE_ASSERT, restore it here to the definition in
-// wtf/Assertions.h.
-#undef COMPILE_ASSERT
-#define COMPILE_ASSERT(exp, name) typedef int dummy##name [(exp) ? 1 : -1];
-#endif
-
 #if PLATFORM(CF)
 typedef const struct __CFURL* CFURLRef;
 #endif
@@ -55,6 +44,10 @@ class NSURL;
 QT_BEGIN_NAMESPACE
 class QUrl;
 QT_END_NAMESPACE
+#endif
+
+#if USE(GOOGLEURL)
+#include "GoogleURLPrivate.h"
 #endif
 
 namespace WebCore {
@@ -98,37 +91,33 @@ public:
     KURL(const KURL& base, const String& relative);
     KURL(const KURL& base, const String& relative, const TextEncoding&);
 
-    // FIXME: The above functions should be harmonized so that passing a
-    // base of null or the empty string gives the same result as the
-    // standard String constructor.
-#ifdef USE_GOOGLE_URL_LIBRARY
+#if USE(GOOGLEURL)
     // For conversions for other structures that have already parsed and
     // canonicalized the URL. The input must be exactly what KURL would have
     // done with the same input.
-    KURL(const char* canonical_spec, int canonical_spec_len,
-         const url_parse::Parsed& parsed, bool is_valid);
+    KURL(const char* canonicalSpec, int canonicalSpecLen,
+         const url_parse::Parsed& parsed, bool isValid);
 #endif
+
+    // FIXME: The above functions should be harmonized so that passing a
+    // base of null or the empty string gives the same result as the
+    // standard String constructor.
+
     // Makes a deep copy. Helpful only if you need to use a KURL on another
     // thread.  Since the underlying StringImpl objects are immutable, there's
     // no other reason to ever prefer copy() over plain old assignment.
     KURL copy() const;
 
-#ifdef USE_GOOGLE_URL_LIBRARY
-    bool isNull() const { return m_url.utf8String().isNull(); }
-    bool isEmpty() const { return m_url.utf8String().length() == 0; }
-#else
-    bool isNull() const { return m_string.isNull(); }
-    bool isEmpty() const { return m_string.isEmpty(); }
-#endif
-
-    bool isValid() const { return m_isValid; }
+    bool isNull() const;
+    bool isEmpty() const;
+    bool isValid() const;
 
     // Returns true if this URL has a path. Note that "http://foo.com/" has a
     // path of "/", so this function will return true. Only invalid or
     // non-hierarchical (like "javascript:") URLs will have no path.
     bool hasPath() const;
 
-#ifdef USE_GOOGLE_URL_LIBRARY
+#if USE(GOOGLEURL)
     const String& string() const { return m_url.string(); }
 #else
     const String& string() const { return m_string; }
@@ -176,38 +165,20 @@ public:
 
     void setRef(const String&);
     void removeRef();
-    
+
     friend bool equalIgnoringRef(const KURL&, const KURL&);
 
     friend bool protocolHostAndPortAreEqual(const KURL&, const KURL&);
 
-    static bool protocolIs(const String&, const char*);
-
-#ifdef USE_GOOGLE_URL_LIBRARY
-    operator const String&() const { return m_url.string(); }
-#if USE(JSC)
-    operator JSC::UString() const { return m_url.string(); }
-#endif
-
     unsigned hostStart() const;
     unsigned hostEnd() const;
-    
+
     unsigned pathStart() const;
     unsigned pathEnd() const;
     unsigned pathAfterLastSlash() const;
-
-#else  // !USE_GOOGLE_URL_LIBRARY
-    operator const String&() const { return m_string; }
+    operator const String&() const { return string(); }
 #if USE(JSC)
-    operator JSC::UString() const { return m_string; }
-#endif
-
-    unsigned hostStart() const { return (m_passwordEnd == m_userStart) ? m_passwordEnd : m_passwordEnd + 1; }
-    unsigned hostEnd() const { return m_hostEnd; }
-    
-    unsigned pathStart() const { return m_portEnd; }
-    unsigned pathEnd() const { return m_pathEnd; }
-    unsigned pathAfterLastSlash() const { return m_pathAfterLastSlash; }
+    operator JSC::UString() const { return string(); }
 #endif
 
 #if PLATFORM(CF)
@@ -220,11 +191,7 @@ public:
     operator NSURL*() const;
 #endif
 #ifdef __OBJC__
-#ifdef USE_GOOGLE_URL_LIBRARY
     operator NSString*() const { return string(); }
-#else  // USE_GOOGLE_URL_LIBRARY
-    operator NSString*() const { return m_string; }
-#endif  // USE_GOOGLE_URL_LIBRARY
 #endif
 
 #if PLATFORM(QT)
@@ -232,9 +199,9 @@ public:
     operator QUrl() const;
 #endif
 
-#ifdef USE_GOOGLE_URL_LIBRARY
+#if USE(GOOGLEURL)
     // Getters for the parsed structure and its corresponding 8-bit string.
-    const url_parse::Parsed& parsed() const { return m_parsed; }
+    const url_parse::Parsed& parsed() const { return m_url.m_parsed; }
     const CString& utf8String() const { return m_url.utf8String(); }
 #endif
 
@@ -245,80 +212,16 @@ public:
 private:
     void invalidate();
     bool isHierarchical() const;
-
-    bool m_isValid;
-#ifdef USE_GOOGLE_URL_LIBRARY
-    // Initializes the object. This will call through to one of the backend
-    // initializers below depending on whether the string's internal
-    // representation is 8 or 16 bit.
-    void init(const KURL& base, const String& relative,
-              const TextEncoding* query_encoding);
-
-    // Backend initializers. The query encoding parameters are optional and can
-    // be NULL (this implies UTF-8). These initializers require that the object
-    // has just been created and the strings are NULL. Do not call on an
-    // already-constructed object.
-    void init(const KURL& base, const char* rel, int rel_len,
-              const TextEncoding* query_encoding);
-    void init(const KURL& base, const UChar* rel, int rel_len,
-              const TextEncoding* query_encoding);
-
-    // Returns the substring of the input identified by the given component.
-    String componentString(const url_parse::Component& comp) const;
-
-    // Replaces the given components, modifying the current URL. The current
-    // URL must be valid.
-    typedef url_canon::Replacements<url_parse::UTF16Char> Replacements;
-    void replaceComponents(const Replacements& replacements);
-
-    // Returns true if the scheme matches the given lowercase ASCII scheme.
-    bool schemeIs(const char* lower_ascii_scheme) const;
-
-    // We store the URL in UTF-8 (really, ASCII except for the ref which can be
-    // UTF-8). WebKit wants Strings out of us.
-    // We create the conversion of the strings on demand and cache the results
-    // to speed things up.
-    class URLString {
-    public:
-        URLString();
-
-        // Setters for the data. Using the ASCII version when you know the
-        // data is ASCII will be slightly more efficient. The UTF-8 version
-        // will always be correct if the caller is unsure.
-        void setUtf8(const char* data, int data_len);
-        void setAscii(const char* data, int data_len);
-
-        // TODO(brettw) we can support an additional optimization when KURL
-        // class is supported only for Strings. Make this buffer support both
-        // optinal Strings and UTF-8 data. This way, we can use the optimization
-        // from the original KURL which uses = with the original string when
-        // canonicalization did not change it. This allows the strings to share
-        // a buffer internally, and saves a malloc.
-
-        // Getters for the data.
-        const CString& utf8String() const { return m_utf8; }
-        const String& string() const;
-
-    private:
-        CString m_utf8;
-
-        // Set to true when the caller set us using the ASCII setter. We can
-        // be more efficient when we know there is no UTF-8 to worry about.
-        // This flag is currently always correct, but should be changed to be a
-        // hint (see setUtf8).
-        bool m_utf8IsASCII;
-
-        mutable bool m_stringIsValid;
-        mutable String m_string;
-    };
-
-    URLString m_url;
-    url_parse::Parsed m_parsed; // Indexes into the UTF-8 version of the string.
-
-#else
+    static bool protocolIs(const String&, const char*);
+#if USE(GOOGLEURL)
+    friend class GoogleURLPrivate;
+    void parse(const char* url, const String* originalString);  // KURLMac calls this.
+    void copyToBuffer(Vector<char, 512>& buffer) const;  // KURLCFNet uses this.
+    GoogleURLPrivate m_url;
+#else  // !USE(GOOGLEURL)
     void init(const KURL&, const String&, const TextEncoding&);
     void copyToBuffer(Vector<char, 512>& buffer) const;
-    
+
     // Parses the given URL. The originalString parameter allows for an
     // optimization: When the source is the same as the fixed-up string,
     // it will use the passed-in string instead of allocating a new one.
@@ -326,7 +229,7 @@ private:
     void parse(const char* url, const String* originalString);
 
     String m_string;
-
+    bool m_isValid;
     int m_schemeEnd;
     int m_userStart;
     int m_userEnd;
@@ -338,17 +241,6 @@ private:
     int m_queryEnd;
     int m_fragmentEnd;
 #endif
-
-// See the following "#infdef KURL_DECORATE_GLOBALS" for an explanation.
-#ifdef KURL_DECORATE_GLOBALS
-  public:
-    static const KURL& blankURL();
-    // Note: protocolIs() is already defined, so omit it.
-    static String mimeTypeFromDataURL(const String& url);
-    static String decodeURLEscapeSequences(const String&);
-    static String decodeURLEscapeSequences(const String&, const TextEncoding&);
-    static String encodeWithURLEscapeSequences(const String&);
-#endif // ifdef KURL_DECORATE_GLOBALS
 };
 
 bool operator==(const KURL&, const KURL&);
@@ -360,13 +252,7 @@ bool operator!=(const String&, const KURL&);
 
 bool equalIgnoringRef(const KURL&, const KURL&);
 bool protocolHostAndPortAreEqual(const KURL&, const KURL&);
-
-// GKURL_unittest.cpp includes both KURL.cpp and GKURL.cpp.
-// For that to work, global functions need to be avoided.
-// (the other globals are okay since they include KURL in parameter list).
-// The workaround is to move the globals into KURL's class namespace.
-#ifndef KURL_DECORATE_GLOBALS
-
+    
 const KURL& blankURL();
 
 // Functions to do URL operations on strings.
@@ -384,30 +270,12 @@ String decodeURLEscapeSequences(const String&, const TextEncoding&);
 
 String encodeWithURLEscapeSequences(const String&);
 
-#endif // ifndef KURL_DECORATE_GLOBALS
-
 // Inlines.
-
-#ifdef USE_GOOGLE_URL_LIBRARY
-
-inline bool operator==(const KURL& a, const KURL& b)
-{
-    return a.utf8String() == b.utf8String();
-}
-
-inline bool operator!=(const KURL& a, const KURL& b)
-{
-    return a.utf8String() != b.utf8String();
-}
-
-#else
 
 inline bool operator==(const KURL& a, const KURL& b)
 {
     return a.string() == b.string();
 }
-
-#endif
 
 inline bool operator==(const KURL& a, const String& b)
 {
@@ -419,14 +287,10 @@ inline bool operator==(const String& a, const KURL& b)
     return a == b.string();
 }
 
-#ifndef USE_GOOGLE_URL_LIBRARY
-
 inline bool operator!=(const KURL& a, const KURL& b)
 {
     return a.string() != b.string();
 }
-
-#endif
 
 inline bool operator!=(const KURL& a, const String& b)
 {
@@ -437,6 +301,53 @@ inline bool operator!=(const String& a, const KURL& b)
 {
     return a != b.string();
 }
+
+#if !USE(GOOGLEURL)
+
+// Inline versions of some non-GoogleURL functions so we can get inlining
+// without having to have a lot of ugly ifdefs in the class definition.
+
+inline bool KURL::isNull() const
+{
+    return m_string.isNull();
+}
+
+inline bool KURL::isEmpty() const
+{
+    return m_string.isEmpty();
+}
+
+inline bool KURL::isValid() const
+{
+    return m_isValid;
+}
+
+inline unsigned KURL::hostStart() const
+{
+    return (m_passwordEnd == m_userStart) ? m_passwordEnd : m_passwordEnd + 1;
+}
+
+inline unsigned KURL::hostEnd() const
+{
+    return m_hostEnd;
+}
+
+inline unsigned KURL::pathStart() const
+{
+    return m_portEnd;
+}
+
+inline unsigned KURL::pathEnd() const
+{
+    return m_pathEnd;
+}
+
+inline unsigned KURL::pathAfterLastSlash() const
+{
+    return m_pathAfterLastSlash;
+}
+
+#endif  // !USE(GOOGLEURL)
 
 } // namespace WebCore
 
@@ -451,4 +362,3 @@ namespace WTF {
 } // namespace WTF
 
 #endif // KURL_h
-
