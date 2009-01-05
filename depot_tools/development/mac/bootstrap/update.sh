5e09cc6bcf24dcde6d1f28c68d80ee06d463fe73
thomasvl@chromium.org <thomasvl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-05 18:16:30 +0000

    - Pull the svn url out of the existing releases dir. - If the releases dir doesn't exist, pull the svn url out of the bootstrap dir. - On svn commands redirect stderr to /dev/null to hide warnings/etc. Review URL: http://codereview.chromium.org/17056
    git-svn-id: svn://svn.chromium.org/chrome/trunk@7545 0039d316-1c4b-4281-b951-d872f2087c98

--- depot_tools/development/mac/bootstrap/update.sh
+++ depot_tools/development/mac/bootstrap/update.sh
@@ -2,6 +2,10 @@
 
 opt=-q
 
+# NOTE: we take and arg ($1) as what needs updating, but the reality is
+# the url used below is hard coded to work for updating release/mac, so the
+# arg pretty much has to be "release".
+
 # We silently update the depot_tools when it already exists.
 if [ ! -e "$1" ]
 then
@@ -10,10 +14,31 @@ fi
 
 if [ -e $1/.svn -o ! -e $1 ]
 then
-  url=`svn info | sed -n -e '/^URL: /s///p'`
+  # If the target dir doesn't exist...
+  if [ ! -e $1 ]
+  then
+    # ...it means we need to pull it clean.
+    
+    # Find the directory this script lives in.
+    base_dir=$(dirname "$0")
+    # 1. Bounce into the directory this script lives in.
+    # 2. Pull it's svn URL from what's on disk (send error output to /dev/null)
+    # 3. Change the url from the "mac/bootstrap to "release/mac" so we have the
+    #    right thing.
+    our_url=`(cd $base_dir ; svn info 2> /dev/null | sed -n -e '/^URL: /s///p')`
+    url=`echo $our_url | sed -n -e 's/mac\/bootstrap/release\/mac/p'`
+  else
+    # ...it existed, so it is already setup for svn, just collect it's url
+    
+    # 1. Bounce into the target directory
+    # 2. Pull it's svn URL from what's on disk (send error output to /dev/null)
+    url=`(cd $1 ; svn info 2> /dev/null | sed -n -e '/^URL: /s///p')`
+  fi
+  # Make sure we got an url
   if [ ! "$url" ]
   then
     url="http://src.chromium.org/svn/trunk/depot_tools/development/release/mac"
   fi
+  # Fire off the update
   exec svn co $opt $url $1
 fi
