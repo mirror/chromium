de05e0c3566e564707b0c5030b628924c7ae605c
dglazkov@google.com <dglazkov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-08 23:58:10 +0000

    Clean up ImageSourceSkia-related stuff prior to upstreaming.
    R=eseidel
    
    Review URL: http://codereview.chromium.org/17442
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk@7778 0039d316-1c4b-4281-b951-d872f2087c98

--- deps/third_party/WebKit/WebCore/platform/graphics/skia/ImageSourceSkia.h
+++ deps/third_party/WebKit/WebCore/platform/graphics/skia/ImageSourceSkia.h
@@ -1,20 +1,49 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/*
+ * Copyright (c) 2008, Google Inc. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#include "config.h"
-
-// We need access to the private member of ImageSource.  Hack it locally
-#include <wtf/Noncopyable.h>
-#include <wtf/Vector.h>
-#define private protected
 #include "ImageSource.h"
-#undef private
 
 namespace WebCore {
 
 class ImageSourceSkia : public ImageSource {
 public:
+    // FIXME: This class is a hack to support Chromium's ICO decoder
+    // Currently our ICO decoder decodes all data during setData() instead of
+    // being lazy.  In addition, it only decodes one frame (closest to the size
+    // passed to the decoder during createDecoder, called from setData) and
+    // discards all other data in the file.
+    //
+    // To fix this will require fixing the ICO decoder to be lazy, or to decode
+    // all frames.  Apple's decoders (ImageIO) decode all frames, and return
+    // them all sorted in decreasing size.  WebCore always draws the first frame.
+    //
     // This is a special-purpose routine for the favicon decoder, which is used
     // to specify a particular icon size for the ICOImageDecoder to prefer
     // decoding.  Note that not all favicons are ICOs, so this won't
@@ -29,4 +58,3 @@ public:
 };
 
 }
-
