3dc1fe1bf79c07e6d90e5f40fa6fbdb8a6aa11a4
dglazkov@google.com <dglazkov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-08 19:03:00 +0000

    Pre-upstreaming clean-up of graphics/skia, Part 2
    R=eseidel
    
    Review URL: http://codereview.chromium.org/17273
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk@7749 0039d316-1c4b-4281-b951-d872f2087c98

--- deps/third_party/WebKit/WebCore/platform/graphics/skia/GradientSkia.cpp
+++ deps/third_party/WebKit/WebCore/platform/graphics/skia/GradientSkia.cpp
@@ -1,26 +1,31 @@
 /*
- * Copyright (C) 2008 Google, Inc.  All rights reserved.
- *
+ * Copyright (c) 2008, Google Inc. All rights reserved.
+ * 
  * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE COMPUTER, INC. ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE COMPUTER, INC. OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include "config.h"
@@ -53,23 +58,23 @@ static SkColor makeSkColor(float a, float r, float g, float b)
 
 // Determine the total number of stops needed, including pseudo-stops at the
 // ends as necessary.
-static size_t total_stops_needed(const Gradient::ColorStop* stopData, size_t count)
+static size_t totalStopsNeeded(const Gradient::ColorStop* stopData, size_t count)
 {
     const Gradient::ColorStop* stop = stopData;
-    size_t count_used = count;
+    size_t countUsed = count;
     if (count < 1 || stop->stop > 0.0)
-      count_used++;
+        countUsed++;
     stop += count - 1;
     if (count < 2 || stop->stop < 1.0)
-      count_used++;
-    return count_used;
+        countUsed++;
+    return countUsed;
 }
 
 // Collect sorted stop position and color information into the pos and colors 
 // buffers, ensuring stops at both 0.0 and 1.0.  The buffers must be large
 // enough to hold information for all stops, including the new endpoints if
 // stops at 0.0 and 1.0 aren't already included.
-static void fill_stops(const Gradient::ColorStop* stopData,
+static void fillStops(const Gradient::ColorStop* stopData,
                        size_t count, SkScalar* pos, SkColor* colors)
 { 
     const Gradient::ColorStop* stop = stopData;
@@ -113,26 +118,26 @@ SkShader* Gradient::platformGradient()
     if (m_gradient)
         return m_gradient;
 
-    // TODO: This and compareStops() are also in Gradient.cpp and
+    // FIXME: This and compareStops() are also in Gradient.cpp and
     // CSSGradientValue.cpp; probably should refactor in WebKit.
     if (!m_stopsSorted) {
         if (m_stops.size())
             std::stable_sort(m_stops.begin(), m_stops.end(), compareStops);
         m_stopsSorted = true;
     }
-    size_t count_used = total_stops_needed(m_stops.data(), m_stops.size());
-    ASSERT(count_used >= 2);
-    ASSERT(count_used >= m_stops.size());
+    size_t countUsed = totalStopsNeeded(m_stops.data(), m_stops.size());
+    ASSERT(countUsed >= 2);
+    ASSERT(countUsed >= m_stops.size());
 
     // FIXME: Why is all this manual pointer math needed?!
-    SkAutoMalloc storage(count_used * (sizeof(SkColor) + sizeof(SkScalar)));
+    SkAutoMalloc storage(countUsed * (sizeof(SkColor) + sizeof(SkScalar)));
     SkColor* colors = (SkColor*)storage.get();
-    SkScalar* pos = (SkScalar*)(colors + count_used);
+    SkScalar* pos = (SkScalar*)(colors + countUsed);
 
-    fill_stops(m_stops.data(), m_stops.size(), pos, colors);
+    fillStops(m_stops.data(), m_stops.size(), pos, colors);
 
     if (m_radial) {
-        // TODO(mmoss) CSS radial Gradients allow an offset focal point (the
+        // FIXME: CSS radial Gradients allow an offset focal point (the
         // "start circle"), but skia doesn't seem to support that, so this just
         // ignores m_p0/m_r0 and draws the gradient centered in the "end
         // circle" (m_p1/m_r1).
@@ -140,11 +145,11 @@ SkShader* Gradient::platformGradient()
         // description of the expected behavior.
         m_gradient = SkGradientShader::CreateRadial(m_p1,
             WebCoreFloatToSkScalar(m_r1), colors, pos,
-            static_cast<int>(count_used), SkShader::kClamp_TileMode);
+            static_cast<int>(countUsed), SkShader::kClamp_TileMode);
     } else {
         SkPoint pts[2] = { m_p0, m_p1 };
         m_gradient = SkGradientShader::CreateLinear(pts, colors, pos,
-            static_cast<int>(count_used), SkShader::kClamp_TileMode);
+            static_cast<int>(countUsed), SkShader::kClamp_TileMode);
     }
 
     return m_gradient;
