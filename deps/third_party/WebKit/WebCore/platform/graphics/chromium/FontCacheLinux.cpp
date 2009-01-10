8674b4e7c0a9127fc750315ac1275860bf5272fa
dglazkov@google.com <dglazkov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-10 00:12:39 +0000

    Pre-upstreaming clean-up of graphics\chromium\Fontish files.
    R=eseidel
    
    Review URL: http://codereview.chromium.org/17467
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk@7847 0039d316-1c4b-4281-b951-d872f2087c98

--- deps/third_party/WebKit/WebCore/platform/graphics/chromium/FontCacheLinux.cpp
+++ deps/third_party/WebKit/WebCore/platform/graphics/chromium/FontCacheLinux.cpp
@@ -1,6 +1,32 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/*
+ * Copyright (c) 2006, 2007, 2008, 2009 Google Inc. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "config.h"
 #include "FontCache.h"
@@ -48,14 +74,13 @@ const SimpleFontData* FontCache::getFontDataForCharacters(const Font& font,
     FcPattern* match = FcFontMatch(0, pattern, &result);
     FcPatternDestroy(pattern);
 
-    SimpleFontData* ret = NULL;
+    SimpleFontData* ret = 0;
 
     if (match) {
         FcChar8* family;
         if (FcPatternGetString(match, FC_FAMILY, 0, &family) == FcResultMatch) {
             FontPlatformData* fpd =
-                createFontPlatformData(font.fontDescription(),
-                                       AtomicString((char *) family));
+                createFontPlatformData(font.fontDescription(), AtomicString((char*) family));
             ret = new SimpleFontData(*fpd);
         }
         FcPatternDestroy(match);
@@ -103,7 +128,7 @@ FontPlatformData* FontCache::createFontPlatformData(const FontDescription& fontD
         static const struct {
             FontDescription::GenericFamilyType mType;
             const char* mName;
-        } gNames[] = {
+        } fontDescriptions[] = {
             { FontDescription::SerifFamily, "serif" },
             { FontDescription::SansSerifFamily, "sans-serif" },
             { FontDescription::MonospaceFamily, "monospace" },
@@ -112,9 +137,9 @@ FontPlatformData* FontCache::createFontPlatformData(const FontDescription& fontD
         };
 
         FontDescription::GenericFamilyType type = fontDescription.genericFamily();
-        for (unsigned i = 0; i < SK_ARRAY_COUNT(gNames); i++) {
-            if (type == gNames[i].mType) {
-                name = gNames[i].mName;
+        for (unsigned i = 0; i < SK_ARRAY_COUNT(fontDescriptions); i++) {
+            if (type == fontDescriptions[i].mType) {
+                name = fontDescriptions[i].mName;
                 break;
             }
         }
@@ -131,9 +156,9 @@ FontPlatformData* FontCache::createFontPlatformData(const FontDescription& fontD
     if (fontDescription.italic())
         style |= SkTypeface::kItalic;
 
-    SkTypeface* tf = SkTypeface::Create(name, (SkTypeface::Style)style);
+    SkTypeface* tf = SkTypeface::Create(name, (SkTypeface::Style>)style);
     if (!tf)
-        return NULL;
+        return 0;
 
     FontPlatformData* result =
         new FontPlatformData(tf,
