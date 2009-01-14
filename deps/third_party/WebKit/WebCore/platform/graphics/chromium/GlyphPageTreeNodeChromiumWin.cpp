bde14907b651cca8e32850738276d3d422a0cdc8
dglazkov@google.com <dglazkov@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-14 18:10:07 +0000

    Pre-upstreaming Scrubbing of platform\graphics\chromium continues.
    R=eseidel
    
    Review URL: http://codereview.chromium.org/18209
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk@8024 0039d316-1c4b-4281-b951-d872f2087c98

--- deps/third_party/WebKit/WebCore/platform/graphics/chromium/GlyphPageTreeNodeChromiumWin.cpp
+++ deps/third_party/WebKit/WebCore/platform/graphics/chromium/GlyphPageTreeNodeChromiumWin.cpp
@@ -1,30 +1,32 @@
-// Copyright (c) 2008, Google Inc. All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-// 
-//     * Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above
-// copyright notice, this list of conditions and the following disclaimer
-// in the documentation and/or other materials provided with the
-// distribution.
-//     * Neither the name of Google Inc. nor the names of its
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/*
+ * Copyright (c) 2008, 2009 Google Inc. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "config.h"
 #include <windows.h>
@@ -37,25 +39,25 @@
 #include "UniscribeHelperTextRun.h"
 #include "WindowsVersion.h"
 
-namespace WebCore
-{
+namespace WebCore {
 
-// Fills one page of font data pointers with NULL to indicate that there
+// Fills one page of font data pointers with 0 to indicate that there
 // are no glyphs for the characters.
 static void fillEmptyGlyphs(GlyphPage* page)
 {
     for (int i = 0; i < GlyphPage::size; ++i)
-        page->setGlyphDataForIndex(i, NULL, NULL);
+        page->setGlyphDataForIndex(i, 0, 0);
 }
 
 // Lazily initializes space glyph
-static Glyph initSpaceGlyph(HDC dc, Glyph* space_glyph)
+static Glyph initSpaceGlyph(HDC dc, Glyph* spaceGlyph)
 {
-    if (*space_glyph)
-        return *space_glyph;
+    if (*spaceGlyph)
+        return *spaceGlyph;
+
     static wchar_t space = ' ';
-    GetGlyphIndices(dc, &space, 1, space_glyph, 0);
-    return *space_glyph;
+    GetGlyphIndices(dc, &space, 1, spaceGlyph, 0);
+    return *spaceGlyph;
 }
 
 // Fills |length| glyphs starting at |offset| in a |page| in the Basic 
@@ -70,31 +72,31 @@ static bool fillBMPGlyphs(unsigned offset,
                           bool recurse)
 {
     HDC dc = GetDC((HWND)0);
-    HGDIOBJ old_font = SelectObject(dc, fontData->m_font.hfont());
+    HGDIOBJ oldFont = SelectObject(dc, fontData->m_font.hfont());
 
     TEXTMETRIC tm = {0};
     if (!GetTextMetrics(dc, &tm)) {
-        SelectObject(dc, old_font);
+        SelectObject(dc, oldFont);
         ReleaseDC(0, dc);
 
         if (recurse) {
-            if (ChromiumBridge::ensureFontLoaded(fontData->m_font.hfont())) {
+            if (ChromiumBridge::ensureFontLoaded(fontData->m_font.hfont()))
                 return fillBMPGlyphs(offset, length, buffer, page, fontData, false);
-            } else {
+            else {
                 fillEmptyGlyphs(page);
                 return false;
             }
         } else {
-            // TODO(nsylvain): This should never happen. We want to crash the
+            // FIXME: This should never happen. We want to crash the
             // process and receive a crash dump. We should revisit this code later.
-            // See bug 1136944.
+            // See http://crbug.com/6401
             ASSERT_NOT_REACHED();
             fillEmptyGlyphs(page);
             return false;
         }
     }
 
-    // NOTE(hbono): GetGlyphIndices() sets each item of localGlyphBuffer[]
+    // FIXME: GetGlyphIndices() sets each item of localGlyphBuffer[]
     // with the one of the values listed below.
     //  * With the GGI_MARK_NONEXISTING_GLYPHS flag
     //    + If the font has a glyph available for the character,
@@ -117,7 +119,7 @@ static bool fillBMPGlyphs(unsigned offset,
     // have tested. (Obviously, I need more tests.)
     WORD localGlyphBuffer[GlyphPage::size];
 
-    // NOTE(jnd). I find some Chinese characters can not be correctly displayed
+    // FIXME: I find some Chinese characters can not be correctly displayed
     // when call GetGlyphIndices without flag GGI_MARK_NONEXISTING_GLYPHS,
     // because the corresponding glyph index is set as 0x20 when current font
     // does not have glyphs available for the character. According a blog post
@@ -128,16 +130,15 @@ static bool fillBMPGlyphs(unsigned offset,
     // Also according to Jungshik and Hironori's suggestion and modification
     // we treat turetype and raster Font as different way when windows version
     // is less than Vista.
-    GetGlyphIndices(dc, buffer, length, localGlyphBuffer,
-                    GGI_MARK_NONEXISTING_GLYPHS);
+    GetGlyphIndices(dc, buffer, length, localGlyphBuffer, GGI_MARK_NONEXISTING_GLYPHS);
 
     // Copy the output to the GlyphPage
-    bool have_glyphs = false;
-    int invalid_glyph = 0xFFFF;
+    bool haveGlyphs = false;
+    int invalidGlyph = 0xFFFF;
     if (!isVistaOrNewer() && !(tm.tmPitchAndFamily & TMPF_TRUETYPE))
-        invalid_glyph = 0x1F;
+        invalidGlyph = 0x1F;
 
-    Glyph space_glyph = 0;  // Glyph for a space. Lazily filled.
+    Glyph spaceGlyph = 0;  // Glyph for a space. Lazily filled.
 
     for (unsigned i = 0; i < length; i++) {
         UChar c = buffer[i];
@@ -146,33 +147,33 @@ static bool fillBMPGlyphs(unsigned offset,
         // When this character should be a space, we ignore whatever the font
         // says and use a space. Otherwise, if fonts don't map one of these
         // space or zero width glyphs, we will get a box.
-        if (Font::treatAsSpace(c)) {
+        if (Font::treatAsSpace(c))
             // Hard code the glyph indices for characters that should be
             // treated like spaces.
-            glyph = initSpaceGlyph(dc, &space_glyph);
-        // TODO(dglazkov): change Font::treatAsZeroWidthSpace to use
-        // u_hasBinaryProperty, per jungshik's comment here:
-        // https://bugs.webkit.org/show_bug.cgi?id=20237#c6.
-        // Then the additional OR won't be necessary.
-        } else if (Font::treatAsZeroWidthSpace(c) || c == 0x200B) {
-            glyph = initSpaceGlyph(dc, &space_glyph);
+            glyph = initSpaceGlyph(dc, &spaceGlyph);
+        else if (Font::treatAsZeroWidthSpace(c) || c == 0x200B) {
+            // FIXME: change Font::treatAsZeroWidthSpace to use
+            // u_hasBinaryProperty, per jungshik's comment here:
+            // https://bugs.webkit.org/show_bug.cgi?id=20237#c6.
+            // Then the additional OR above won't be necessary.
+            glyph = initSpaceGlyph(dc, &spaceGlyph);
             glyphFontData = fontData->zeroWidthFontData();
-        } else if (glyph == invalid_glyph) {
+        } else if (glyph == invalidGlyph) {
             // WebKit expects both the glyph index and FontData
-            // pointer to be NULL if the glyph is not present
+            // pointer to be 0 if the glyph is not present
             glyph = 0;
             glyphFontData = 0;
         } else {
             if (SimpleFontData::isCJKCodePoint(c))
                 glyphFontData = fontData->cjkWidthFontData();
-            have_glyphs = true;
+            haveGlyphs = true;
         }
         page->setGlyphDataForCharacter(offset + i, glyph, glyphFontData);
     }
 
-    SelectObject(dc, old_font);
+    SelectObject(dc, oldFont);
     ReleaseDC(0, dc);
-    return have_glyphs;
+    return haveGlyphs;
 }
 
 // For non-BMP characters, each is two words (UTF-16) and the input buffer
@@ -214,10 +215,9 @@ static bool fillNonBMPGlyphs(unsigned offset,
         if (glyph) {
             haveGlyphs = true;
             page->setGlyphDataForIndex(offset + i, glyph, fontData);
-        } else {
+        } else
             // Clear both glyph and fontData fields.
             page->setGlyphDataForIndex(offset + i, 0, 0);
-        }
     }
     return haveGlyphs;
 }
@@ -231,17 +231,16 @@ bool GlyphPage::fill(unsigned offset, unsigned length, UChar* characterBuffer,
     // We have to handle BMP and non-BMP characters differently.
     // FIXME: Add assertions to make sure that buffer is entirely in BMP
     // or entirely in non-BMP. 
-    if (bufferLength == length) {
-        return fillBMPGlyphs(offset, length, characterBuffer, this,
-                             fontData, true);
-    }
+    if (bufferLength == length)
+        return fillBMPGlyphs(offset, length, characterBuffer, this, fontData, true);
+
     if (bufferLength == 2 * length) {
         // A non-BMP input buffer will be twice as long as output glyph buffer
         // because each character in the non-BMP input buffer will be 
         // represented by a surrogate pair (two UChar's).
-        return fillNonBMPGlyphs(offset, length, characterBuffer,
-                                this, fontData);
+        return fillNonBMPGlyphs(offset, length, characterBuffer, this, fontData);
     }
+
     ASSERT_NOT_REACHED();
     return false;
 }
--- deps/third_party/WebKit/WebCore/platform/graphics/chromium/IconChromiumLinux.cpp
+++ deps/third_party/WebKit/WebCore/platform/graphics/chromium/IconChromiumLinux.cpp
@@ -1,31 +1,39 @@
 /*
-* Copyright (C) 2006, 2007 Apple Inc.
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Library General Public
-* License as published by the Free Software Foundation; either
-* version 2 of the License, or (at your option) any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Library General Public License for more details.
-*
-* You should have received a copy of the GNU Library General Public License
-* along with this library; see the file COPYING.LIB.  If not, write to
-* the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-* Boston, MA 02111-1307, USA.
-*
-*/
+ * Copyright (c) 2008, 2009 Google Inc. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "config.h"
-
 #include "Icon.h"
 
 #include "GraphicsContext.h"
 #include "NotImplemented.h"
 #include "PlatformString.h"
-#include "SkiaUtils.h"
 
 namespace WebCore {
 
@@ -38,19 +46,19 @@ Icon::~Icon()
 {
 }
 
-PassRefPtr<Icon> Icon::createIconForFile(const String& filename)
+PassRefPtr<Icon> Icon::createIconForFile(const String&)
 {
   notImplemented();
-  return NULL;
+  return 0;
 }
 
-PassRefPtr<Icon> Icon::createIconForFiles(const Vector<String>& filenames)
+PassRefPtr<Icon> Icon::createIconForFiles(const Vector<String>&)
 {
   notImplemented();
-  return NULL;
+  return 0;
 }
 
-void Icon::paint(GraphicsContext* context, const IntRect& r)
+void Icon::paint(GraphicsContext*, const IntRect&)
 {
   notImplemented();
 }
