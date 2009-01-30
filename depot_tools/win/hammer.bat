947a9a70f16f343ebe572ee1329788c10aa210b4
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-01-30 00:26:16 +0000

    NO CODE CHANGE. ONLY PROPERTY CHANGES.
    Set svn:eol-style=LF for almost every .py files.
    Add svn:ignore=.sconsign.dblite to .
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk@8932 0039d316-1c4b-4281-b951-d872f2087c98

--- depot_tools/win/hammer.bat
+++ depot_tools/win/hammer.bat
@@ -1,77 +1,24 @@
 @echo off
-::=============================================================================
-:: hammer.bat - Hammer wrapper for software construction toolkit for SCons
-::
-:: Environment variables used all the time:
-::   HAMMER_OPTS        Command line options for hammer/SCons, in addition to
-::                      any specified on the command line.
-::
-:: Environment variables used for IncrediBuild (Xoreax Grid Engine) support:
-::   HAMMER_XGE         If set to 1, enable IncrediBuild
-::   HAMMER_XGE_PATH    Path to IncrediBuild install.  Required if it was not
-::                      installed in the default location
-::                      (%ProgramFiles%\Xoreax\IncrediBuild).
-::   HAMMER_XGE_OPTS    Additional options to pass to IncrediBuild.
-::
-:: Sample values for HAMMER_OPTS:
-::   -j%NUMBER_OF_PROCESSORS%
-::      Run parallel builds on all processor cores, unless explicitly
-::      overridden on the command line.
-::   -j12
-::      Always run 12 builds in parallel; a good default if HAMMER_XGE=1.
-::   -s -k
-::      Don't print commands; keep going on build failures.
 
-::=============================================================================
-:: Hammer setup
+IF EXIST "%cd%\third_party\python_24\python.exe" GOTO srcdir
 
-setlocal
+@rem We're in a submodule directory, look relative to the parent.
+"%cd%\..\third_party\python_24\python.exe" "%cd%\..\third_party\scons\scons.py" "--site-dir=..\site_scons" %*
+goto omega
 
-:: Preserve a copy of the PATH (in case we need it later, mainly for cygwin).
-set PRESCONS_PATH=%PATH%
+:srcdir
+"%cd%\third_party\python_24\python.exe" "%cd%\third_party\scons\scons.py" --site-dir=site_scons %*
+goto omega
 
-:: Find the path to the src directory.
-:: Handle being run either from a module or the src directory.
-IF EXIST "%cd%\third_party\python_24\python.exe" GOTO SRCDIR
-set SRC_DIR=%cd%\..
-goto PICKED_SITE_SCONS
-:SRCDIR
-set SRC_DIR=%cd%
-:PICKED_SITE_SCONS
+@rem Per the following page:
+@rem   http://code-bear.com/bearlog/2007/06/01/getting-the-exit-code-from-a-batch-file-that-is-run-from-a-python-program/
+@rem Just calling "exit /b" passes back an exit code, but in a way
+@rem that does NOT get picked up correctly when executing the .bat
+@rem file from the Python subprocess module.  Using "call" as the
+@rem last command in the .bat file makes it work as expected.
 
-:: Specify site_scons directories.
-set HAMMER_OPTS=%HAMMER_OPTS% --site-dir="%SRC_DIR%\site_scons"
+:returncode
+exit /b %ERRORLEVEL%
 
-:: Run SCons via software construction toolkit wrapper.
-:: Remove -O and -OO from the following line to make asserts execute
-set HAMMER_CMD="%SRC_DIR%\third_party\python_24\python.exe" -x -O -OO "%SRC_DIR%\third_party\scons\scons.py" %HAMMER_OPTS% %*
-
-:: ============================================================================
-:: Incredibuild support
-
-if not defined HAMMER_XGE set HAMMER_XGE=0
-if %HAMMER_XGE% neq 1 goto END_XGE_SETUP
-
-:: Allow IncrediBuild to intercept tools run by python; run cl remotely.
-set HAMMER_XGE_OPTS=%HAMMER_XGE_OPTS% /profile="%~dp0\profile.xml"
-
-:: Add IncrediBuild back into the path (from the default location, if not
-:: explicitly specified)
-if not defined HAMMER_XGE_PATH (
-  set HAMMER_XGE_PATH=%ProgramFiles%\Xoreax\IncrediBuild
-)
-if not exist "%HAMMER_XGE_PATH%\xgConsole.exe" (
-  echo Warning: xgConsole.exe not found in %HAMMER_XGE_PATH%
-  echo Not using IncrediBuild.
-  goto END_XGE_SETUP
-)
-set PATH=%PATH%;%HAMMER_XGE_PATH%
-
-:: Use IncrediBuild to wrap the call to python.
-set HAMMER_CMD=XGConsole.exe %HAMMER_XGE_OPTS% /command="%HAMMER_CMD%"
-
-:END_XGE_SETUP
-:: ============================================================================
-:: Run whatever command we came up with
-
-%HAMMER_CMD%
+:omega
+call :returncode %ERRORLEVEL%
