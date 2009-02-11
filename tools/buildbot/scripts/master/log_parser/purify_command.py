0d6c39f580ba0f90bbd1fb6ec78150ceab1616e5
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-02-11 04:22:21 +0000

    Add a download link for the purify tests.
    Review URL: http://codereview.chromium.org/24010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk@9554 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/buildbot/scripts/master/log_parser/purify_command.py
+++ tools/buildbot/scripts/master/log_parser/purify_command.py
@@ -6,6 +6,8 @@
 """A buildbot command for running and interpreting purify tests."""
 
 import re
+import chromium_config as config
+
 from buildbot.steps import shell
 from buildbot.status import builder
 from buildbot.process import buildstep
@@ -20,11 +22,20 @@ class TestObserver(buildstep.LogLineObserver):
 
     # State tracking for log parsing
     self.chunk = ''
+    self.last_change = ''
+    self.test_name = ''
+
     # These keys must match the text in the log messages (see regexp below).
     self.error_count = {'Fixable': 0, 'Flakey' : 0}
 
     # Regular expressions for parsing the chunk line
     self._chunk_line = re.compile('\(chunk slice (.*)\)')
+    
+    # Regular expression for parsing the line containing the test name.
+    self._test_name_line = re.compile('\[INFO\] running test (.*)')
+
+    # Regular expression for parsing the last change line.
+    self._last_change_line = re.compile('^last change: (.*)')
 
     # Regular expression for finding fixable error counts.
     self._error_count_line = re.compile(
@@ -37,6 +48,18 @@ class TestObserver(buildstep.LogLineObserver):
     results = self._chunk_line.search(line)
     if results:
       self.chunk = results.group(1)
+      return
+
+    # Is it the 'last change' line?
+    results = self._last_change_line.search(line)
+    if results:
+      self.last_change = results.group(1)
+      return
+
+    # Is it the line containing the test name?
+    results = self._test_name_line.search(line)
+    if results:
+      self.test_name = results.group(1)
       return
 
     # Is it a line holding a count of fixable errors?
@@ -56,6 +79,14 @@ class PurifyCommand(retcode_command.ReturnCodeCommand):
     shell.ShellCommand.__init__(self, **kwargs)
     self.test_observer = TestObserver()
     self.addLogObserver('stdio', self.test_observer)
+
+  def createSummary(self, log):
+    if (self.base_url and self.test_observer.last_change and
+        self.test_observer.test_name):
+      url = ('%s/purify/%s/%s' % (config.Master.archive_url,
+                                  self.test_observer.test_name,
+                                  self.test_observer.last_change))
+      self.addURL('download', url)
 
   def getText(self, cmd, results):
     basic_info = self.describe(True)
