e44da9d2e0ad86dcd7a3a36f14ed2498c4e76b9d
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-03-11 03:42:31 +0000

    Make the extract_build script more reliable, and also sneak it some fixes for all the bugs I introduced in this script in the past day. Review URL: http://codereview.chromium.org/43068
    git-svn-id: svn://svn.chromium.org/chrome/trunk@11422 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/buildbot/scripts/slave/extract_build.py
+++ tools/buildbot/scripts/slave/extract_build.py
@@ -11,54 +11,80 @@ import os
 import shutil
 import sys
 import urllib
+import urllib2
 
 import chromium_utils
 import slave_utils
 
-# Exit code to use for warnings, to distinguish script issues from reliability
-# crashes.
+# Exit code to use for warnings, to distinguish script issues.
 WARNING_EXIT_CODE = -88
 
 def main(options, args):
-  """ extract build\full-build-win32.zip to build\BuildDir\full-build-win32
+  """ Download a build, extract it to build\BuildDir\full-build-win32
       and rename it to build\BuildDir\Target
   """
   project_dir = os.path.abspath(options.build_dir)
   build_dir = os.path.join(project_dir, options.target)
   output_dir = os.path.join(project_dir, 'full-build-win32')
 
-  # Download the file. Use the checked out revision to determine the filename.
+  # Find the revision that we need to download.
   current_revision = slave_utils.SubversionRevision(project_dir)
 
-  print 'Fetching build %d...' % current_revision
-  failure = False
-  try:
-    build_archive = 'full-build-win32_%d.zip' % current_revision
-    urllib.urlretrieve(options.build_url, build_archive)
-    shutil.copyfile(build_archive, 'full-build-win32.zip')
-  except IOError:
-    print 'Failed to download archived build at revision %d' % current_revision
-    failure = True
+  # Generic name for the archive.
+  archive_name = 'full-build-win32.zip'
 
-  if failure:
-    print 'Fetching latest build...'
+  # URL containing the version number.
+  url = options.build_url.replace('.zip', '_%d.zip' % current_revision)
+
+  # We try to download and extract 3 times.
+  for tries in range(1,4): 
+    print 'Try %d: Fetching build from %s...' % (tries, url)
+
+    failure = False
+
+    # Check if the url exists.
     try:
-      urllib.urlretrieve(options.build_url, 'full-build-win32.zip')
-    except IOError:
-      print 'Failed to download generic archived build'
+      content = urllib2.urlopen(url)
+      content.close()
+    except urllib2.HTTPError:
+      print '%s is not found' % url
+      failure = True
+
+    # If the url is valid, we download the file.
+    if not failure:
+      try:
+        urllib.urlretrieve(url, archive_name)
+      except IOError:
+        print 'Failed to download archived build'
+        failure = True
+
+    # If the versionned url failed, we try to get the latest build.
+    if failure:
+      print 'Fetching latest build...'
+      try:
+        urllib.urlretrieve(options.build_url, archive_name)
+      except IOError:
+        print 'Failed to download generic archived build'
+        # Try again...
+        continue
+
+    print 'Extracting build %s to %s...' % (archive_name, project_dir)
+    try:
+      chromium_utils.ExtractZip(archive_name, project_dir)
+      chromium_utils.RemoveDirectory(build_dir)
+      shutil.move(output_dir, build_dir)
+    except:
+      print 'Failed to extract the build.'
+      # Try again...
+      continue
+
+    if failure:
+      # We successfully extracted the archive, but it was the generic one.
       return WARNING_EXIT_CODE
+    return 0
 
-  print 'Extracting build...'
-  try:
-    chromium_utils.ExtractZip('full-build-win32.zip', project_dir)
-    chromium_utils.RemoveDirectory(build_dir)
-    shutil.move(output_dir, build_dir)
-  except:
-    return WARNING_EXIT_CODE
-
-  if failure:
-    return WARNING_EXIT_CODE
-  return 0
+  # If we get here, that means that it failed 3 times. We return a failure.
+  return -1
 
 if '__main__' == __name__:
   option_parser = optparse.OptionParser()
