edb4ea488869d4cd3f8efdefeb7af6282bfb6a58
mal@chromium.org <mal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-04-26 23:51:39 +0000

    Merge r14234 and r14242.
    These revisions add more fixes for restoring a closed tab and simplify
    the tab restore ui_test.
    
    14234
    Restore closed tabs into new windows when necessary, and track the windows they
    came from so they're restored together (into the same new window) when
    appropriate.
    
    Fix safety check on tab index when restoring: make it check the correct
    browser.
    
    Change some ASSERTs to EXPECTs in the unit test for greater coverage.
    
    14242
    Weaken is_active check more, since it appears that Chromium can switch to and
    from being the frontmost app during the course of a ui_tests run. This way
    errors will show up in more carefully controlled local tests, but won't turn
    the bots red.
    
    
    BUG=5278
    TEST=Open a window with two tabs, close both (closing the window), then
      restore both. Make sure both restored tabs are in the same window.  Open
      a window with multiple tabs, close a tab, then close the window using 
      its close box. Restore both and make sure the tab goes back into the window.
    
    TEST=make sure TabRestoreUiTest.* don't fail due to is_active being false
    
    TBR= sky (pam is not available)
    Review URL: http://codereview.chromium.org/98007
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/172/src@14572 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automation/browser_proxy.h
+++ chrome/test/automation/browser_proxy.h
@@ -110,7 +110,6 @@ class BrowserProxy : public AutomationResourceProxy {
   // On failure, returns NULL.
   AutocompleteEditProxy* GetAutocompleteEdit();
 
-
   // Apply the accelerator with given id (IDC_BACK, IDC_NEWTAB ...)
   // Returns true if the call was successful.
   //
