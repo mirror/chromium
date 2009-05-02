b55999681930f9b679c4e8a1f6a972a645d5837a
mal@chromium.org <mal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-02 05:50:36 +0000

    Merge 13970 - Due to some crashes in teardown, change NotificationObservers for TAB_CONTENTS_DESTROYED to use the NotificationRegistrar to ensure proper cleanup.
    Original Review URL: http://codereview.chromium.org/79051
    
    TBR=mbelshe@google.com
    Review URL: http://codereview.chromium.org/99313
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/172/src@15134 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tabs/tab_strip_model.h
+++ chrome/browser/tabs/tab_strip_model.h
@@ -8,7 +8,7 @@
 #include <vector>
 
 #include "base/observer_list.h"
-#include "chrome/common/notification_observer.h"
+#include "chrome/common/notification_registrar.h"
 #include "chrome/common/page_transition_types.h"
 
 namespace gfx {
@@ -541,6 +541,9 @@ class TabStripModel : public NotificationObserver {
   // Our observers.
   typedef ObserverList<TabStripModelObserver> TabStripModelObservers;
   TabStripModelObservers observers_;
+
+  // A scoped container for notification registries.
+  NotificationRegistrar registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(TabStripModel);
 };
