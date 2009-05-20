e1d9d053602b72b0fe2c093f90599d86acdf9484
mal@chromium.org <mal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-05-20 04:33:10 +0000

    Merge r16363.
    Fixed use of uninitialized variable problem in generated bindings.  It
    turns out that converting an object to an SVGNumber can't fail.
    Review URL: http://codereview.chromium.org/115557
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/172/src@16465 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/port/bindings/scripts/CodeGeneratorV8.pm
+++ webkit/port/bindings/scripts/CodeGeneratorV8.pm
@@ -1720,7 +1720,7 @@ my %typeCanFailConversion = (
     "SVGElement" => 0,
     "SVGLength" => 1,
     "SVGMatrix" => 1,
-    "SVGNumber" => 1,
+    "SVGNumber" => 0,
     "SVGPaintType" => 0,
     "SVGPathSeg" => 0,
     "SVGPoint" => 1,
@@ -1758,7 +1758,6 @@ sub BasicTypeCanFailConversion
 
   return 1 if $type eq "SVGLength";
   return 1 if $type eq "SVGMatrix";
-  return 1 if $type eq "SVGNumber";
   return 1 if $type eq "SVGPoint";
   return 1 if $type eq "SVGRect";
   return 1 if $type eq "SVGTransform";
