672fda00dcdbb54be2612fedf76bdda900fe3845
Mandeep Singh Baines <msb@chromium.org>
2012-03-26 19:26:37 -0700

    Implement image_transport for drm
    Change-Id: I62bd44433adb651b3661dbb532a0dee41badaafb

--- ui/gl/generate_bindings.py
+++ ui/gl/generate_bindings.py
@@ -866,6 +866,10 @@ EGL_FUNCTIONS = [
   'names': ['eglQuerySurfacePointerANGLE'],
   'arguments':
       'EGLDisplay dpy, EGLSurface surface, EGLint attribute, void** value', },
+{ 'return_type': 'EGLBoolean',
+  'names': ['eglExportDRMImageMESA'],
+  'arguments': 'EGLDisplay dpy, EGLImageKHR image, '
+    'EGLint* name, EGLint* handle, EGLint* stride', },
 ]
 
 WGL_FUNCTIONS = [
