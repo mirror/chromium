0857a62313968abf726a79c1ba12390fe812f153
Mandeep Singh Baines <msb@chromium.org>
2012-03-26 19:13:23 -0700

    Add webplugin support for drm
    +webplugin_delegate_proxy support for drm
    
    Change-Id: Ie679f0ed93ff27ce9be668c700abef85eea80e28

--- content/plugin/webplugin_proxy.h
+++ content/plugin/webplugin_proxy.h
@@ -220,17 +220,19 @@ class WebPluginProxy : public webkit::npapi::WebPlugin {
       const gfx::Rect& window_rect,
       scoped_ptr<TransportDIB>* dib_out,
       base::mac::ScopedCFTypeRef<CGContextRef>* cg_context_out);
-#elif defined(USE_X11)
+#elif defined(USE_X11) || defined(USE_DRM)
   static void CreateDIBAndCanvasFromHandle(
       const TransportDIB::Handle& dib_handle,
       const gfx::Rect& window_rect,
       scoped_refptr<SharedTransportDIB>* dib_out,
       SkAutoTUnref<SkCanvas>* canvas);
 
+#if defined(USE_X11)
   static void CreateShmPixmapFromDIB(
       TransportDIB* dib,
       const gfx::Rect& window_rect,
       XID* pixmap_out);
+#endif
 #endif
 
   // Updates the shared memory sections where windowless plugins paint.
@@ -283,12 +285,14 @@ class WebPluginProxy : public webkit::npapi::WebPlugin {
   SkAutoTUnref<SkCanvas> windowless_canvases_[2];
   SkAutoTUnref<SkCanvas> background_canvas_;
 
-#if defined(USE_X11)
+#if defined(USE_X11) || defined(USE_DRM)
   scoped_refptr<SharedTransportDIB> windowless_dibs_[2];
+#if defined(USE_X11)
   // If we can use SHM pixmaps for windowless plugin painting or not.
   bool use_shm_pixmap_;
   // The SHM pixmaps for windowless plugin painting.
   XID windowless_shm_pixmaps_[2];
+#endif
 #endif
 
 #endif
