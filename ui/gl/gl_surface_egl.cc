e4d8eb5fb63f872fc67d6a70f36ebb6974a6b203
Simon Que <sque@chromium.org>
2012-05-23 15:21:46 -0700

    Restore GLSurfaceEGL::GetConfig
    Change-Id: Ic453576ee827ee07a2e4c8120d8c364c82189384

--- ui/gl/gl_surface_egl.cc
+++ ui/gl/gl_surface_egl.cc
@@ -163,6 +163,10 @@ EGLDisplay GLSurfaceEGL::GetDisplay() {
   return software_ ? g_software_display : g_display;
 }
 
+EGLConfig GLSurfaceEGL::GetConfig() {
+  return software_ ? g_software_config : g_config;
+}
+
 EGLDisplay GLSurfaceEGL::GetHardwareDisplay() {
   return g_display;
 }
