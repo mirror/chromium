e6f120afb2854ef7ad4621d2fd52d4caf946c339
Simon Que <sque@chromium.org>
2012-07-13 19:14:07 -0700

    Add local helper functions to output_configurator_drm
    Change-Id: I5a70b8206011b20ded322a0db751449fb78e411c

--- chromeos/display/output_configurator_drm.cc
+++ chromeos/display/output_configurator_drm.cc
@@ -4,6 +4,8 @@
 
 #include "chromeos/display/output_configurator_drm.h"
 
+#include <xf86drmMode.h>
+
 #include "base/chromeos/chromeos_version.h"
 #include "base/logging.h"
 #include "base/message_pump_evdev.h"
@@ -16,6 +18,89 @@
 #include "third_party/cros_system_api/dbus/service_constants.h"
 #include "ui/aura/root_window_host_drm.h"
 #include "ui/base/output_drm.h"
+
+namespace aura {
+class CompositorLock;
+}  // namespace aura
+
+namespace {
+
+static bool FindMirrorModeForOutputs(const ui::OutputDRM& output_one,
+                                     const ui::OutputDRM& output_two,
+                                     ui::ModeDRM* mode_one,
+                                     ui::ModeDRM* mode_two) {
+  std::vector<ui::ModeDRM> one_modes;
+  std::vector<ui::ModeDRM> two_modes;
+  output_one.GetModes(&one_modes);
+  output_two.GetModes(&two_modes);
+  unsigned int one_index = 0;
+  unsigned int two_index = 0;
+  while ((one_index < one_modes.size()) && (two_index < two_modes.size())) {
+    const ui::ModeDRM& one_mode = one_modes[one_index];
+    const ui::ModeDRM& two_mode = two_modes[two_index];
+    int one_width = one_mode.width();
+    int one_height = one_mode.height();
+    int two_width = two_mode.width();
+    int two_height = two_mode.height();
+    if ((one_mode.width() == two_mode.width()) &&
+        (one_mode.height() == two_mode.height())) {
+      *mode_one = one_mode;
+      *mode_two = two_mode;
+      return true;
+    } else {
+      // The sort order of the modes is NOT by mode area but is sorted by width,
+      // then by height within each like width.
+      if (one_width > two_width) {
+        one_index += 1;
+      } else if (one_width < two_width) {
+        two_index += 1;
+      } else {
+        if (one_height > two_height) {
+          one_index += 1;
+        } else {
+          two_index += 1;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+static void ConfigureCrtc(int fd,
+                          const drmModeCrtc& crtc,
+                          int x,
+                          int y,
+                          const ui::ModeDRM& mode,
+                          ui::OutputDRM* output) {
+#if 0
+  aura::RootWindowHostDRM* window_host =
+      aura::RootWindowHostDRM::Create(gfx::Rect(mode.width(), mode.height()));
+  //uint32_t crtc_id = window_host->GetAcceleratedWidget();
+  scoped_refptr<aura::CompositorLock> lock =
+      window_host->GetRootWindow()->GetCompositorLock();
+
+  drmModeCrtc new_crtc = crtc;
+  new_crtc.x = x;
+  new_crtc.y = y;
+  new_crtc.mode = mode.mode_info;
+  output->SetCRTC(new_crtc);
+
+  window_host->SetSize();
+#endif
+}
+
+static void GetOutputConfiguration(
+    int fd, int connector_id, drmModeCrtc* crtc, ui::ModeDRM* mode) {
+  ui::OutputDRM output(fd, connector_id);
+  output.GetCRTC(crtc);
+  *mode = output.GetPreferredMode();
+}
+
+}  // namespace
+
+volatile void* ____foo = reinterpret_cast<void*>(GetOutputConfiguration);
+volatile void* ____goo = reinterpret_cast<void*>(ConfigureCrtc);
+volatile void* ____bar = reinterpret_cast<void*>(FindMirrorModeForOutputs);
 
 namespace chromeos {
 
