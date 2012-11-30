a1ddba52049f3e548f1ddfa9eca4b3d68382f1cc
Simon Que <sque@chromium.org>
2012-11-30 13:50:29 -0800

    Fix include path in events_evdev.cc
    To be merged with earlier patch
    
    Change-Id: I89b25c307704fb12a139e18e538e7d5012d990ef

--- ui/base/evdev/events_evdev.cc
+++ ui/base/evdev/events_evdev.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ui/base/events.h"
+#include "ui/base/events/event.h"
 
 #include <X11/keysym.h>
 #include <X11/keysymdef.h>
