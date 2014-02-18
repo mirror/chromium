9e3172bc3c53639fcb5343b7fe71fa7773bd603c
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-18 16:29:44 +0000

    Merge 251697 "When clearing the session cache, also flush OpenSS..."
    > When clearing the session cache, also flush OpenSSLClientKeyStore.
    > Without this, we can end upcaching a stale PrivateKey.
    > BUG=341500
    > 
    > Review URL: https://codereview.chromium.org/169153002
    
    TBR=yfriedman@chromium.org
    
    Review URL: https://codereview.chromium.org/170693002
    
    git-svn-id: svn://svn.chromium.org/chrome/branches/1750_93/src@251769 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/ssl_client_socket_openssl.cc
+++ net/socket/ssl_client_socket_openssl.cc
@@ -331,6 +331,7 @@ void SSLClientSocket::ClearSessionCache() {
   SSLClientSocketOpenSSL::SSLContext* context =
       SSLClientSocketOpenSSL::SSLContext::GetInstance();
   context->session_cache()->Flush();
+  OpenSSLClientKeyStore::GetInstance()->Flush();
 }
 
 SSLClientSocketOpenSSL::SSLClientSocketOpenSSL(
