crosswalk-project/chromium-crosswalk

commit 2a6f5f7ad5d3db9d0d5f64c138314b40e8138911

Apply scroll and scale delta values to client even if root layer is NULL.
The early out here meant that if the root layer was detached when
BeginMainFrame started, the sent deltas would be lost and then clobbered
on commit. This was causing a specific bug where top controls would
occasionally hide on initialization. This CL skips the early out, avoiding
only the scroll offset application on the layer tree if root_layer is NULL.

This patch also reverts the functional parts of
https://codereview.chromium.org/647753004 since this early out was the
reason for that patch.

BUG=423139

Review URL: https://codereview.chromium.org/651633003

Cr-Commit-Position: refs/heads/master@{#301842}
(cherry picked from commit e7f7aa7287ddd8853121ce22ee128f57b016f023)

Conflicts:
	cc/trees/layer_tree_host.cc
	cc/trees/layer_tree_host_unittest.cc

Review URL: https://codereview.chromium.org/699693003

Cr-Commit-Position: refs/branch-heads/2171@{#339}
Cr-Branched-From: 267aeeb8d85c8503a7fd12bd14654b8ea78d3974-refs/heads/master@{#297060}

cc/trees/layer_tree_host.cc
@@ -1059,9 +1059,6 @@ void LayerTreeHost::PaintLayerContents(
 }
 
 void LayerTreeHost::ApplyScrollAndScale(ScrollAndScaleSet* info) {
-  if (!root_layer_.get())
-    return;
-
   ScopedPtrVector<SwapPromise>::iterator it = info->swap_promises.begin();
   for (; it != info->swap_promises.end(); ++it) {
     scoped_ptr<SwapPromise> swap_promise(info->swap_promises.take(it));
@@ -1075,18 +1072,20 @@ void LayerTreeHost::ApplyScrollAndScale(ScrollAndScaleSet* info) {
   gfx::Vector2d inner_viewport_scroll_delta;
   gfx::Vector2d outer_viewport_scroll_delta;
 
-  for (size_t i = 0; i < info->scrolls.size(); ++i) {
-    Layer* layer = LayerTreeHostCommon::FindLayerInSubtree(
-        root_layer_.get(), info->scrolls[i].layer_id);
-    if (!layer)
-      continue;
-    if (layer == outer_viewport_scroll_layer_.get()) {
-      outer_viewport_scroll_delta += info->scrolls[i].scroll_delta;
-    } else if (layer == inner_viewport_scroll_layer_.get()) {
-      inner_viewport_scroll_delta += info->scrolls[i].scroll_delta;
-    } else {
-      layer->SetScrollOffsetFromImplSide(layer->scroll_offset() +
-                                         info->scrolls[i].scroll_delta);
+  if (root_layer_.get() {
+    for (size_t i = 0; i < info->scrolls.size(); ++i) {
+      Layer* layer = LayerTreeHostCommon::FindLayerInSubtree(
+          root_layer_.get(), info->scrolls[i].layer_id);
+      if (!layer)
+        continue;
+      if (layer == outer_viewport_scroll_layer_.get()) {
+        outer_viewport_scroll_delta += info->scrolls[i].scroll_delta;
+      } else if (layer == inner_viewport_scroll_layer_.get()) {
+        inner_viewport_scroll_delta += info->scrolls[i].scroll_delta;
+      } else {
+        layer->SetScrollOffsetFromImplSide(layer->scroll_offset() +
+                                           info->scrolls[i].scroll_delta);
+      }
     }
   }
 
@@ -1094,17 +1093,16 @@ void LayerTreeHost::ApplyScrollAndScale(ScrollAndScaleSet* info) {
       !outer_viewport_scroll_delta.IsZero() ||
       info->page_scale_delta != 1.f ||
       info->top_controls_delta) {
-    // SetScrollOffsetFromImplSide above could have destroyed the tree,
-    // so re-get this layer before doing anything to it.
-
     // Preemptively apply the scroll offset and scale delta here before sending
     // it to the client.  If the client comes back and sets it to the same
     // value, then the layer can early out without needing a full commit.
-    DCHECK(inner_viewport_scroll_layer_.get());  // We should always have this.
 
-    inner_viewport_scroll_layer_->SetScrollOffsetFromImplSide(
-        inner_viewport_scroll_layer_->scroll_offset() +
-        inner_viewport_scroll_delta);
+    if (inner_viewport_scroll_layer_.get()) {
+      inner_viewport_scroll_layer_->SetScrollOffsetFromImplSide(
+          inner_viewport_scroll_layer_->scroll_offset() +
+          inner_viewport_scroll_delta);
+    }
+
     if (outer_viewport_scroll_layer_.get()) {
       outer_viewport_scroll_layer_->SetScrollOffsetFromImplSide(
           outer_viewport_scroll_layer_->scroll_offset() +

cc/trees/layer_tree_host_impl.cc
@@ -2903,7 +2903,8 @@ void LayerTreeHostImpl::PinchGestureEnd() {
 
 static void CollectScrollDeltas(ScrollAndScaleSet* scroll_info,
                                 LayerImpl* layer_impl) {
-  DCHECK(layer_impl);
+  if (!layer_impl)
+    return;
 
   gfx::Vector2d scroll_delta =
       gfx::ToFlooredVector2d(layer_impl->ScrollDelta());
@@ -2922,15 +2923,12 @@ static void CollectScrollDeltas(ScrollAndScaleSet* scroll_info,
 scoped_ptr<ScrollAndScaleSet> LayerTreeHostImpl::ProcessScrollDeltas() {
   scoped_ptr<ScrollAndScaleSet> scroll_info(new ScrollAndScaleSet());
 
-  if (active_tree_->root_layer()) {
-    CollectScrollDeltas(scroll_info.get(), active_tree_->root_layer());
-    scroll_info->page_scale_delta = active_tree_->page_scale_delta();
-    active_tree_->set_sent_page_scale_delta(scroll_info->page_scale_delta);
-    scroll_info->swap_promises.swap(
-        swap_promises_for_main_thread_scroll_update_);
-    scroll_info->top_controls_delta = active_tree()->top_controls_delta();
-    active_tree_->set_sent_top_controls_delta(scroll_info->top_controls_delta);
-  }
+  CollectScrollDeltas(scroll_info.get(), active_tree_->root_layer());
+  scroll_info->page_scale_delta = active_tree_->page_scale_delta();
+  active_tree_->set_sent_page_scale_delta(scroll_info->page_scale_delta);
+  scroll_info->swap_promises.swap(swap_promises_for_main_thread_scroll_update_);
+  scroll_info->top_controls_delta = active_tree()->top_controls_delta();
+  active_tree_->set_sent_top_controls_delta(scroll_info->top_controls_delta);
 
   return scroll_info.Pass();
 }

cc/trees/layer_tree_host_impl_unittest.cc
@@ -2318,28 +2318,6 @@ class LayerTreeHostImplTopControlsTest : public LayerTreeHostImplTest {
   LayerTreeSettings settings_;
 };  // class LayerTreeHostImplTopControlsTest
 
-TEST_F(LayerTreeHostImplTopControlsTest,
-       TopControlsDeltaOnlySentWithRootLayer) {
-  CreateHostImpl(settings_, CreateOutputSurface());
-
-  host_impl_->active_tree()->set_top_controls_delta(-20.f);
-
-  // Because LTH::ApplyScrollAndScale doesn't know what to do with a scroll
-  // delta packet when the root layer doesn't exist yet, make sure not to set
-  // sent_top_controls_delta either to avoid the delta getting clobbered on the
-  // next commit.
-  scoped_ptr<ScrollAndScaleSet> scroll_info = host_impl_->ProcessScrollDeltas();
-  EXPECT_EQ(scroll_info->top_controls_delta, 0.f);
-  EXPECT_EQ(host_impl_->active_tree()->sent_top_controls_delta(), 0.f);
-
-  SetupTopControlsAndScrollLayer();
-
-  // After the root layer exists, it should be set normally.
-  scroll_info = host_impl_->ProcessScrollDeltas();
-  EXPECT_EQ(scroll_info->top_controls_delta, -20.f);
-  EXPECT_EQ(host_impl_->active_tree()->sent_top_controls_delta(), -20.f);
-}
-
 TEST_F(LayerTreeHostImplTopControlsTest, ScrollTopControlsByFractionalAmount) {
   CreateHostImpl(settings_, CreateOutputSurface());
   SetupTopControlsAndScrollLayer();

cc/trees/layer_tree_host_unittest.cc
@@ -5084,4 +5084,45 @@ class LayerTreeHostTestActivateOnInvisible : public LayerTreeHostTest {
 // TODO(vmpstr): Enable with single thread impl-side painting.
 MULTI_THREAD_TEST_F(LayerTreeHostTestActivateOnInvisible);
 
+// Make sure page scale and top control deltas are applied to the client even
+// when the LayerTreeHost doesn't have a root layer.
+class LayerTreeHostAcceptsDeltasFromImplWithoutRootLayer
+    : public LayerTreeHostTest {
+ public:
+  LayerTreeHostAcceptsDeltasFromImplWithoutRootLayer()
+      : deltas_sent_to_client_(false) {}
+
+  virtual void BeginTest() OVERRIDE {
+    layer_tree_host()->SetRootLayer(nullptr);
+    info_.page_scale_delta = 3.14f;
+    info_.top_controls_delta = 2.73f;
+
+    PostSetNeedsCommitToMainThread();
+  }
+
+  virtual void BeginMainFrame(const BeginFrameArgs& args) OVERRIDE {
+    EXPECT_EQ(nullptr, layer_tree_host()->root_layer());
+
+    layer_tree_host()->ApplyScrollAndScale(&info_);
+    EndTest();
+  }
+
+  virtual void ApplyViewportDeltas(
+      const gfx::Vector2d& scroll,
+      float scale_delta,
+      float top_controls_delta) OVERRIDE {
+    EXPECT_EQ(info_.page_scale_delta, scale_delta);
+    EXPECT_EQ(info_.top_controls_delta, top_controls_delta);
+    deltas_sent_to_client_ = true;
+  }
+
+  virtual void AfterTest() OVERRIDE {
+    EXPECT_TRUE(deltas_sent_to_client_);
+  }
+
+  ScrollAndScaleSet info_;
+  bool deltas_sent_to_client_;
+};
+
+MULTI_THREAD_TEST_F(LayerTreeHostAcceptsDeltasFromImplWithoutRootLayer);
 }  // namespace cc

mirror/chromium

commit 1ec24dc35d4acc564068d60e5f778b7e03b45453

Build fix for previous patch
Fixing syntax error introduced in 2a6f5f7ad5d3

cc/trees/layer_tree_host.cc
@@ -1072,7 +1072,7 @@ void LayerTreeHost::ApplyScrollAndScale(ScrollAndScaleSet* info) {
   gfx::Vector2d inner_viewport_scroll_delta;
   gfx::Vector2d outer_viewport_scroll_delta;
 
-  if (root_layer_.get() {
+  if (root_layer_.get()) {
     for (size_t i = 0; i < info->scrolls.size(); ++i) {
       Layer* layer = LayerTreeHostCommon::FindLayerInSubtree(
           root_layer_.get(), info->scrolls[i].layer_id);