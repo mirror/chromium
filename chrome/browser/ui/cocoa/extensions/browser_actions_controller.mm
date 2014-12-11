crosswalk-project/chromium-crosswalk

commit 15eff0c2d8ca8f5b75cc409440da4cfdc6d63118

[Extensions Toolbar] Make the ExtensionToolbarModel icon count more stable
Make two changes to the ExtensionToolbarModel:
- Only create views for actions once the extension system is fully loaded;
- Make sure we guard the visible_icon_count() to the possible range.

BUG=432148

Review URL: https://codereview.chromium.org/726813002

Cr-Commit-Position: refs/heads/master@{#304295}
(cherry picked from commit 3ceefb6e7910127d12104c1ecbc6bddf49f0caea)

[Extensions Toolbar] Notify observers of extensions re-added after a pref change

I suspect this is what's causing the listed bugs, as it would lead to the
observers' lists getting out of order.

BUG=433757
BUG=433764

Review URL: https://codereview.chromium.org/731353002

Cr-Commit-Position: refs/heads/master@{#304519}
(cherry picked from commit ad3ae498cb192d7a78c8efdc053d5d22c8713a1d)

(TBRing original reviewers)
TBR=finnur@chromium.org
TBR=pkasting@chromium.org

Review URL: https://codereview.chromium.org/787693002

Cr-Commit-Position: refs/branch-heads/2214@{#257}
Cr-Branched-From: 03655fd3f6d72165dc3c9bd2c89807305316fe6c-refs/heads/master@{#303346}

chrome/browser/ui/cocoa/extensions/browser_actions_controller.mm
@@ -209,6 +209,10 @@ class ExtensionServiceObserverBridge
 
   void ToolbarHighlightModeChanged(bool is_highlighting) override {}
 
+  void OnToolbarModelInitialized() override {
+    [owner_ createButtons];
+  }
+
   Browser* GetBrowser() override { return browser_; }
 
  private:
@@ -283,7 +287,8 @@ class ExtensionServiceObserverBridge
 
     hiddenButtons_.reset([[NSMutableArray alloc] init]);
     buttons_.reset([[NSMutableDictionary alloc] init]);
-    [self createButtons];
+    if (toolbarModel_->extensions_initialized())
+      [self createButtons];
     [self showChevronIfNecessaryInFrame:[containerView_ frame] animate:NO];
     [self updateGrippyCursors];
     [container setResizable:!profile_->IsOffTheRecord()];
@@ -427,8 +432,7 @@ class ExtensionServiceObserverBridge
     [self createActionButtonForExtension:iter->get() withIndex:i++];
   }
 
-  CGFloat width = [self savedWidth];
-  [containerView_ resizeToWidth:width animate:NO];
+  [self resizeContainerAndAnimate:NO];
 }
 
 - (void)createActionButtonForExtension:(const Extension*)extension

chrome/browser/extensions/extension_toolbar_model.cc
@@ -122,7 +122,7 @@ void ExtensionToolbarModel::MoveExtensionIcon(const std::string& id,
 }
 
 void ExtensionToolbarModel::SetVisibleIconCount(size_t count) {
-  visible_icon_count_ = (count == toolbar_items_.size()) ? -1 : count;
+  visible_icon_count_ = (count >= toolbar_items_.size()) ? -1 : count;
 
   // Only set the prefs if we're not in highlight mode and the profile is not
   // incognito. Highlight mode is designed to be a transitory state, and should
@@ -227,17 +227,16 @@ void ExtensionToolbarModel::Observe(
     int new_index = 0;
     if (visible) {
       // If this action used to be hidden, we can't possibly be showing all.
-      DCHECK_NE(-1, visible_icon_count_);
+      DCHECK_LT(visible_icon_count(), toolbar_items_.size());
       // Grow the bar by one and move the extension to the end of the visibles.
-      new_size = visible_icon_count_ + 1;
+      new_size = visible_icon_count() + 1;
       new_index = new_size - 1;
     } else {
       // If we're hiding one, we must be showing at least one.
-      DCHECK_NE(visible_icon_count_, 0);
+      DCHECK_GE(visible_icon_count(), 0u);
       // Shrink the bar by one and move the extension to the beginning of the
       // overflow menu.
-      new_size = visible_icon_count_ == -1 ?
-                     toolbar_items_.size() - 1 : visible_icon_count_ - 1;
+      new_size = visible_icon_count() - 1;
       new_index = new_size;
     }
     SetVisibleIconCount(new_size);
@@ -311,6 +310,8 @@ bool ExtensionToolbarModel::ShouldAddExtension(const Extension* extension) {
 }
 
 void ExtensionToolbarModel::AddExtension(const Extension* extension) {
+  // We only use AddExtension() once the system is initialized.
+  DCHECK(extensions_initialized_);
   if (!ShouldAddExtension(extension))
     return;
 
@@ -345,18 +346,17 @@ void ExtensionToolbarModel::AddExtension(const Extension* extension) {
       // Find what the index will be in the main bar. Because Observer calls are
       // nondeterministic, we can't just assume the main bar will have the
       // extension and look it up.
-      int main_index = is_new_extension ?
+      size_t main_index = is_new_extension ?
           main_model->toolbar_items_.size() :
           main_model->FindNewPositionFromLastKnownGood(extension);
-      bool visible = main_index < main_model->visible_icon_count_ ||
-                     main_model->visible_icon_count_ == -1;
+      bool visible = main_index < main_model->visible_icon_count();
       // We may need to adjust the visible count if the incognito bar isn't
       // showing all icons and this one is visible, or if it is showing all
       // icons and this is hidden.
-      if (visible && visible_icon_count_ != -1)
-        SetVisibleIconCount(visible_icon_count_ + 1);
-      else if (!visible && visible_icon_count_ == -1)
-        SetVisibleIconCount(toolbar_items_.size() - 1);
+      if (visible && !all_icons_visible())
+        SetVisibleIconCount(visible_icon_count() + 1);
+      else if (!visible && all_icons_visible())
+        SetVisibleIconCount(visible_icon_count() - 1);
     }
   }
 }
@@ -394,16 +394,6 @@ void ExtensionToolbarModel::RemoveExtension(const Extension* extension) {
   UpdatePrefs();
 }
 
-void ExtensionToolbarModel::ClearItems() {
-  size_t items_count = toolbar_items_.size();
-  for (size_t i = 0; i < items_count; ++i) {
-    const Extension* extension = toolbar_items_.back().get();
-    toolbar_items_.pop_back();
-    FOR_EACH_OBSERVER(Observer, observers_, ToolbarExtensionRemoved(extension));
-  }
-  DCHECK(toolbar_items_.empty());
-}
-
 // Combine the currently enabled extensions that have browser actions (which
 // we get from the ExtensionRegistry) with the ordering we get from the
 // pref service. For robustness we use a somewhat inefficient process:
@@ -412,6 +402,8 @@ void ExtensionToolbarModel::ClearItems() {
 // 2. Create a vector of extensions that did not have a pref value.
 // 3. Remove holes from the sorted vector and append the unsorted vector.
 void ExtensionToolbarModel::InitializeExtensionList() {
+  DCHECK(toolbar_items_.empty());  // We shouldn't have any items yet.
+
   last_known_positions_ = extension_prefs_->GetToolbarOrder();
   if (profile_->IsOffTheRecord())
     IncognitoPopulate();
@@ -420,7 +412,7 @@ void ExtensionToolbarModel::InitializeExtensionList() {
 
   extensions_initialized_ = true;
   MaybeUpdateVisibilityPrefs();
-  FOR_EACH_OBSERVER(Observer, observers_, ToolbarVisibleCountChanged());
+  FOR_EACH_OBSERVER(Observer, observers_, OnToolbarModelInitialized());
 }
 
 void ExtensionToolbarModel::Populate(const ExtensionIdList& positions) {
@@ -450,11 +442,9 @@ void ExtensionToolbarModel::Populate(const ExtensionIdList& positions) {
       unsorted.push_back(extension);
   }
 
-  // Clear the current items, if any.
-  ClearItems();
-
   // Merge the lists.
-  toolbar_items_.reserve(sorted.size() + unsorted.size());
+  sorted.insert(sorted.end(), unsorted.begin(), unsorted.end());
+  toolbar_items_.reserve(sorted.size());
 
   for (const scoped_refptr<const Extension>& extension : sorted) {
     // It's possible for the extension order to contain items that aren't
@@ -463,21 +453,13 @@ void ExtensionToolbarModel::Populate(const ExtensionIdList& positions) {
     // syncing NPAPI-containing extensions, so if one of those is not actually
     // synced, we'll get a NULL in the list.  This sort of case can also happen
     // if some error prevents an extension from loading.
-    if (extension.get() != NULL) {
+    if (extension.get()) {
+      // We don't notify observers of the added extension yet. Rather, observers
+      // should wait for the "OnToolbarModelInitialized" notification, and then
+      // bulk-update. (This saves a lot of bouncing-back-and-forth here, and
+      // allows observers to ensure that the extension system is always
+      // initialized before using the extensions).
       toolbar_items_.push_back(extension);
-      FOR_EACH_OBSERVER(
-          Observer,
-          observers_,
-          ToolbarExtensionAdded(extension.get(), toolbar_items_.size() - 1));
-    }
-  }
-  for (const scoped_refptr<const Extension>& extension : unsorted) {
-    if (extension.get() != NULL) {
-      toolbar_items_.push_back(extension);
-      FOR_EACH_OBSERVER(
-          Observer,
-          observers_,
-          ToolbarExtensionAdded(extension.get(), toolbar_items_.size() - 1));
     }
   }
 
@@ -499,9 +481,6 @@ void ExtensionToolbarModel::Populate(const ExtensionIdList& positions) {
 
 void ExtensionToolbarModel::IncognitoPopulate() {
   DCHECK(profile_->IsOffTheRecord());
-  // Clear the current items, if any.
-  ClearItems();
-
   const ExtensionToolbarModel* original_model =
       ExtensionToolbarModel::Get(profile_->GetOriginalProfile());
 
@@ -519,10 +498,6 @@ void ExtensionToolbarModel::IncognitoPopulate() {
       toolbar_items_.push_back(*iter);
       if (iter - original_model->toolbar_items_.begin() < original_visible)
         ++visible_icon_count_;
-      FOR_EACH_OBSERVER(
-          Observer,
-          observers_,
-          ToolbarExtensionAdded(iter->get(), toolbar_items_.size() - 1));
     }
   }
 }
@@ -538,11 +513,11 @@ void ExtensionToolbarModel::UpdatePrefs() {
 }
 
 void ExtensionToolbarModel::MaybeUpdateVisibilityPref(
-    const Extension* extension, int index) {
+    const Extension* extension, size_t index) {
   // We only update the visibility pref for hidden/not hidden based on the
   // overflow menu with the new toolbar design.
   if (include_all_extensions_ && !profile_->IsOffTheRecord()) {
-    bool visible = index < visible_icon_count_ || visible_icon_count_ == -1;
+    bool visible = index < visible_icon_count();
     if (visible != ExtensionActionAPI::GetBrowserActionVisibility(
                        extension_prefs_, extension->id())) {
       // Don't observe changes caused by ourselves.
@@ -590,8 +565,24 @@ void ExtensionToolbarModel::OnExtensionToolbarPrefChange() {
   }
   last_known_positions_.swap(pref_positions);
 
-  // Re-populate.
+  // Clear out the old...
+  while (!toolbar_items_.empty()) {
+    scoped_refptr<const Extension> extension = toolbar_items_.back();
+    toolbar_items_.pop_back();
+    FOR_EACH_OBSERVER(
+        Observer, observers_, ToolbarExtensionRemoved(extension.get()));
+  }
+  DCHECK(toolbar_items_.empty());
+
+  // ...Add the new...
   Populate(last_known_positions_);
+
+  // ...And notify.
+  for (size_t i = 0; i < toolbar_items().size(); ++i) {
+    FOR_EACH_OBSERVER(Observer,
+                      observers_,
+                      ToolbarExtensionAdded(toolbar_items()[i].get(), i));
+  }
 
   if (last_known_positions_.size() > pref_position_size) {
     // Need to update pref because we have extra icons. But can't call
@@ -609,7 +600,7 @@ size_t ExtensionToolbarModel::GetVisibleIconCountForTab(
     return visible_icon_count();  // Already displaying all actions.
 
   ExtensionActionAPI* extension_action_api = ExtensionActionAPI::Get(profile_);
-  size_t total_icons = visible_icon_count_;
+  size_t total_icons = visible_icon_count();
   for (size_t i = total_icons; i < toolbar_items_.size(); ++i) {
     if (extension_action_api->ExtensionWantsToRun(toolbar_items_[i].get(),
                                                   web_contents))
@@ -664,15 +655,15 @@ bool ExtensionToolbarModel::ShowExtensionActionPopup(
 
 void ExtensionToolbarModel::EnsureVisibility(
     const ExtensionIdList& extension_ids) {
-  if (visible_icon_count_ == -1)
+  if (all_icons_visible())
     return;  // Already showing all.
 
   // Otherwise, make sure we have enough room to show all the extensions
   // requested.
-  if (visible_icon_count_ < static_cast<int>(extension_ids.size()))
+  if (visible_icon_count() < extension_ids.size())
     SetVisibleIconCount(extension_ids.size());
 
-  if (visible_icon_count_ == -1)
+  if (all_icons_visible())
     return;  // May have been set to max by SetVisibleIconCount.
 
   // Guillotine's Delight: Move an orange noble to the front of the line.
@@ -681,7 +672,8 @@ void ExtensionToolbarModel::EnsureVisibility(
     for (ExtensionList::const_iterator extension = toolbar_items_.begin();
          extension != toolbar_items_.end(); ++extension) {
       if ((*extension)->id() == (*it)) {
-        if (extension - toolbar_items_.begin() >= visible_icon_count_)
+        if (extension - toolbar_items_.begin() >=
+                static_cast<int>(visible_icon_count()))
           MoveExtensionIcon((*extension)->id(), 0);
         break;
       }
@@ -709,10 +701,8 @@ bool ExtensionToolbarModel::HighlightExtensions(
   if (highlighted_items_.size()) {
     old_visible_icon_count_ = visible_icon_count_;
     is_highlighting_ = true;
-    if (visible_icon_count_ != -1 &&
-        visible_icon_count_ < static_cast<int>(extension_ids.size())) {
+    if (visible_icon_count() < extension_ids.size())
       SetVisibleIconCount(extension_ids.size());
-    }
 
     FOR_EACH_OBSERVER(Observer, observers_, ToolbarHighlightModeChanged(true));
     return true;

chrome/browser/extensions/extension_toolbar_model.h
@@ -43,6 +43,7 @@ class ExtensionToolbarModel : public content::NotificationObserver,
    public:
     // TODO(devlin): Rename these methods to be OnFoo.
     // Signals that an |extension| has been added to the toolbar at |index|.
+    // This will *only* be called after the toolbar model has been initialized.
     virtual void ToolbarExtensionAdded(const Extension* extension,
                                        int index) = 0;
 
@@ -79,6 +80,11 @@ class ExtensionToolbarModel : public content::NotificationObserver,
     //   with the new set (and just assume the new set is different).
     virtual void ToolbarHighlightModeChanged(bool is_highlighting) = 0;
 
+    // Signals that the toolbar model has been initialized, so that if any
+    // observers were postponing animation during the initialization stage, they
+    // can catch up.
+    virtual void OnToolbarModelInitialized() = 0;
+
     // Signals that the toolbar needs to be reordered for the given
     // |web_contents|. This is caused by an overflowed action wanting to run,
     // and needing to "pop itself out".
@@ -108,8 +114,13 @@ class ExtensionToolbarModel : public content::NotificationObserver,
   void SetVisibleIconCount(size_t count);
 
   size_t visible_icon_count() const {
+    // We have guards around this because |visible_icon_count_| can be set by
+    // prefs/sync, and we want to ensure that the icon count returned is within
+    // bounds.
     return visible_icon_count_ == -1 ?
-        toolbar_items().size() : static_cast<size_t>(visible_icon_count_);
+        toolbar_items().size() :
+        std::min(static_cast<size_t>(visible_icon_count_),
+                 toolbar_items().size());
   }
 
   bool all_icons_visible() const { return visible_icon_count_ == -1; }
@@ -197,7 +208,7 @@ class ExtensionToolbarModel : public content::NotificationObserver,
 
   // Updates |extension|'s browser action visibility pref if the browser action
   // is in the overflow menu and should be considered hidden.
-  void MaybeUpdateVisibilityPref(const Extension* extension, int index);
+  void MaybeUpdateVisibilityPref(const Extension* extension, size_t index);
 
   // Calls MaybeUpdateVisibilityPref() for each extension in |toolbar_items|.
   void MaybeUpdateVisibilityPrefs();
@@ -212,9 +223,6 @@ class ExtensionToolbarModel : public content::NotificationObserver,
   // Adds or removes the given |extension| from the toolbar model.
   void AddExtension(const Extension* extension);
   void RemoveExtension(const Extension* extension);
-
-  // Removes all current items (because we're going to [re]Populate()).
-  void ClearItems();
 
   // Our observers.
   ObserverList<Observer> observers_;
@@ -251,6 +259,8 @@ class ExtensionToolbarModel : public content::NotificationObserver,
 
   // The number of icons visible (the rest should be hidden in the overflow
   // chevron). A value of -1 indicates that all icons should be visible.
+  // Instead of using this variable directly, use visible_icon_count() if
+  // possible.
   // TODO(devlin): Make a new variable to indicate that all icons should be
   // visible, instead of overloading this one.
   int visible_icon_count_;

chrome/browser/extensions/extension_toolbar_model_unittest.cc
@@ -112,7 +112,10 @@ KeyedService* BuildToolbarModel(content::BrowserContext* context) {
 // Given a |profile|, assigns the testing keyed service function to
 // BuildToolbarModel() and uses it to create and initialize a new
 // ExtensionToolbarModel.
-ExtensionToolbarModel* CreateToolbarModelForProfile(Profile* profile) {
+// |wait_for_ready| indicates whether or not to post the ExtensionSystem's
+// ready task.
+ExtensionToolbarModel* CreateToolbarModelForProfile(Profile* profile,
+                                                    bool wait_for_ready) {
   ExtensionToolbarModel* model = ExtensionToolbarModel::Get(profile);
   if (model)
     return model;
@@ -122,15 +125,18 @@ ExtensionToolbarModel* CreateToolbarModelForProfile(Profile* profile) {
   ExtensionToolbarModelFactory::GetInstance()->SetTestingFactory(
       profile, &BuildToolbarModel);
   model = ExtensionToolbarModel::Get(profile);
-  // Fake the extension system ready signal.
-  // HACK ALERT! In production, the ready task on ExtensionSystem (and most
-  // everything else on it, too) is shared between incognito and normal
-  // profiles, but a TestExtensionSystem doesn't have the concept of "shared".
-  // Because of this, we have to set any new profile's TestExtensionSystem's
-  // ready task, too.
-  static_cast<TestExtensionSystem*>(ExtensionSystem::Get(profile))->SetReady();
-  // Run tasks posted to TestExtensionSystem.
-  base::RunLoop().RunUntilIdle();
+  if (wait_for_ready) {
+    // Fake the extension system ready signal.
+    // HACK ALERT! In production, the ready task on ExtensionSystem (and most
+    // everything else on it, too) is shared between incognito and normal
+    // profiles, but a TestExtensionSystem doesn't have the concept of "shared".
+    // Because of this, we have to set any new profile's TestExtensionSystem's
+    // ready task, too.
+    static_cast<TestExtensionSystem*>(ExtensionSystem::Get(profile))->
+        SetReady();
+    // Run tasks posted to TestExtensionSystem.
+    base::RunLoop().RunUntilIdle();
+  }
 
   return model;
 }
@@ -164,6 +170,7 @@ class ExtensionToolbarModelTestObserver
   size_t removed_count() const { return removed_count_; }
   size_t moved_count() const { return moved_count_; }
   int highlight_mode_count() const { return highlight_mode_count_; }
+  size_t initialized_count() const { return initialized_count_; }
   size_t reorder_count() const { return reorder_count_; }
 
  private:
@@ -194,6 +201,8 @@ class ExtensionToolbarModelTestObserver
     highlight_mode_count_ += is_highlighting ? 1 : -1;
   }
 
+  void OnToolbarModelInitialized() override { ++initialized_count_; }
+
   void OnToolbarReorderNecessary(content::WebContents* web_contents) override {
     ++reorder_count_;
   }
@@ -207,6 +216,7 @@ class ExtensionToolbarModelTestObserver
   size_t moved_count_;
   // Int because it could become negative (if something goes wrong).
   int highlight_mode_count_;
+  size_t initialized_count_;
   size_t reorder_count_;
 };
 
@@ -216,6 +226,7 @@ ExtensionToolbarModelTestObserver::ExtensionToolbarModelTestObserver(
                                     removed_count_(0),
                                     moved_count_(0),
                                     highlight_mode_count_(0),
+                                    initialized_count_(0),
                                     reorder_count_(0) {
   model_->AddObserver(this);
 }
@@ -297,7 +308,7 @@ class ExtensionToolbarModelUnitTest : public ExtensionServiceTestBase {
 
 void ExtensionToolbarModelUnitTest::Init() {
   InitializeEmptyExtensionService();
-  toolbar_model_ = CreateToolbarModelForProfile(profile());
+  toolbar_model_ = CreateToolbarModelForProfile(profile(), true);
   model_observer_.reset(new ExtensionToolbarModelTestObserver(toolbar_model_));
 }
 
@@ -885,7 +896,7 @@ TEST_F(ExtensionToolbarModelUnitTest, ExtensionToolbarIncognitoModeTest) {
 
   // Get an incognito profile and toolbar.
   ExtensionToolbarModel* incognito_model =
-      CreateToolbarModelForProfile(profile()->GetOffTheRecordProfile());
+      CreateToolbarModelForProfile(profile()->GetOffTheRecordProfile(), true);
 
   ExtensionToolbarModelTestObserver incognito_observer(incognito_model);
   EXPECT_EQ(0u, incognito_observer.moved_count());
@@ -990,7 +1001,7 @@ TEST_F(ExtensionToolbarModelUnitTest,
 
   // Get an incognito profile and toolbar.
   ExtensionToolbarModel* incognito_model =
-      CreateToolbarModelForProfile(profile()->GetOffTheRecordProfile());
+      CreateToolbarModelForProfile(profile()->GetOffTheRecordProfile(), true);
   ExtensionToolbarModelTestObserver incognito_observer(incognito_model);
 
   // Right now, no extensions are enabled in incognito mode.
@@ -1271,6 +1282,69 @@ TEST_F(ExtensionToolbarModelUnitTest, ToolbarActionsPopOutToAct) {
   EXPECT_EQ(no_action(), tab_order[2]);
   EXPECT_EQ(1u, toolbar_model()->GetVisibleIconCountForTab(web_contents));
   EXPECT_EQ(3u, observer()->reorder_count());
+}
+
+// Test that observers receive no Added notifications until after the
+// ExtensionSystem has initialized.
+TEST_F(ExtensionToolbarModelUnitTest, ModelWaitsForExtensionSystemReady) {
+  InitializeEmptyExtensionService();
+  ExtensionToolbarModel* toolbar_model =
+       CreateToolbarModelForProfile(profile(), false);
+  ExtensionToolbarModelTestObserver model_observer(toolbar_model);
+  EXPECT_TRUE(AddBrowserActionExtensions());
+
+  // Since the model hasn't been initialized (the ExtensionSystem::ready task
+  // hasn't been run), there should be no insertion notifications.
+  EXPECT_EQ(0u, model_observer.inserted_count());
+  EXPECT_EQ(0u, model_observer.initialized_count());
+  EXPECT_FALSE(toolbar_model->extensions_initialized());
+
+  // Run the ready task.
+  static_cast<TestExtensionSystem*>(ExtensionSystem::Get(profile()))->
+      SetReady();
+  // Run tasks posted to TestExtensionSystem.
+  base::RunLoop().RunUntilIdle();
+
+  // We should still have no insertions, but should have an initialized count.
+  EXPECT_TRUE(toolbar_model->extensions_initialized());
+  EXPECT_EQ(0u, model_observer.inserted_count());
+  EXPECT_EQ(1u, model_observer.initialized_count());
+}
+
+// Check that the toolbar model correctly clears and reorders when it detects
+// a preference change.
+TEST_F(ExtensionToolbarModelUnitTest, ToolbarModelPrefChange) {
+  Init();
+
+  ASSERT_TRUE(AddBrowserActionExtensions());
+
+  // We should start in the basic A B C order.
+  ASSERT_TRUE(browser_action_a());
+  EXPECT_EQ(browser_action_a(), GetExtensionAtIndex(0));
+  EXPECT_EQ(browser_action_b(), GetExtensionAtIndex(1));
+  EXPECT_EQ(browser_action_c(), GetExtensionAtIndex(2));
+  // Record the difference between the inserted and removed counts. The actual
+  // value of the counts is not important, but we need to be sure that if we
+  // call to remove any, we also add them back.
+  size_t inserted_and_removed_difference =
+      observer()->inserted_count() - observer()->removed_count();
+
+  // Assign a new order, B C A, and write it in the prefs.
+  ExtensionIdList new_order;
+  new_order.push_back(browser_action_b()->id());
+  new_order.push_back(browser_action_c()->id());
+  new_order.push_back(browser_action_a()->id());
+  ExtensionPrefs::Get(profile())->SetToolbarOrder(new_order);
+
+  // Ensure everything has time to run.
+  base::RunLoop().RunUntilIdle();
+
+  // The new order should be reflected in the model.
+  EXPECT_EQ(browser_action_b(), GetExtensionAtIndex(0));
+  EXPECT_EQ(browser_action_c(), GetExtensionAtIndex(1));
+  EXPECT_EQ(browser_action_a(), GetExtensionAtIndex(2));
+  EXPECT_EQ(inserted_and_removed_difference,
+            observer()->inserted_count() - observer()->removed_count());
 }
 
 }  // namespace extensions

chrome/browser/ui/views/toolbar/browser_actions_container.cc
@@ -116,8 +116,7 @@ bool BrowserActionsContainer::disable_animations_during_testing_ = false;
 BrowserActionsContainer::BrowserActionsContainer(
     Browser* browser,
     BrowserActionsContainer* main_container)
-    : initialized_(false),
-      profile_(browser->profile()),
+    : profile_(browser->profile()),
       browser_(browser),
       main_container_(main_container),
       popup_owner_(NULL),
@@ -179,8 +178,6 @@ void BrowserActionsContainer::Init() {
     container_width_ = GetPreferredWidth();
     SetChevronVisibility();
   }
-
-  initialized_ = true;
 }
 
 const std::string& BrowserActionsContainer::GetIdAt(size_t index) {
@@ -752,7 +749,10 @@ void BrowserActionsContainer::ViewHierarchyChanged(
     // We do this here instead of in the constructor because AddBrowserAction
     // calls Layout on the Toolbar, which needs this object to be constructed
     // before its Layout function is called.
-    CreateToolbarActionViews();
+    // If the model is not initialized, this is called later from
+    // OnToolbarModelInitialized().
+    if (model_->extensions_initialized())
+      CreateToolbarActionViews();
   }
 }
 
@@ -925,6 +925,11 @@ void BrowserActionsContainer::ToolbarHighlightModeChanged(
   DeleteToolbarActionViews();
   CreateToolbarActionViews();
   Animate(gfx::Tween::LINEAR, GetIconCount());
+}
+
+void BrowserActionsContainer::OnToolbarModelInitialized() {
+  CreateToolbarActionViews();
+  ToolbarVisibleCountChanged();
 }
 
 void BrowserActionsContainer::OnToolbarReorderNecessary(

chrome/browser/ui/views/toolbar/browser_actions_container.h
@@ -285,6 +285,7 @@ class BrowserActionsContainer
                                 bool grant_active_tab) override;
   void ToolbarVisibleCountChanged() override;
   void ToolbarHighlightModeChanged(bool is_highlighting) override;
+  void OnToolbarModelInitialized() override;
   void OnToolbarReorderNecessary(content::WebContents* web_contents) override;
   Browser* GetBrowser() override;
 

mirror/chromium

commit 60394ec97a4ae9abeda509395f50a63475ab9549

[Extensions Toolbar] Fix a segfault in a bad merge
Fix a seg fault that was introduced in
15eff0c2d8ca8f5b75cc409440da4cfdc6d63118.

TBR=pkasting@chromium.org

Review URL: https://codereview.chromium.org/798703003

chrome/browser/ui/cocoa/extensions/browser_actions_controller.mm
@@ -287,7 +287,7 @@ class ExtensionServiceObserverBridge
 
     hiddenButtons_.reset([[NSMutableArray alloc] init]);
     buttons_.reset([[NSMutableDictionary alloc] init]);
-    if (toolbarModel_->extensions_initialized())
+    if (toolbarModel_ && toolbarModel_->extensions_initialized())
       [self createButtons];
     [self showChevronIfNecessaryInFrame:[containerView_ frame] animate:NO];
     [self updateGrippyCursors];