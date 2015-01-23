crosswalk-project/chromium-crosswalk

commit 13cf2cfcb5325ecfff98ee8ccde723f22e606d22

Add support for sending a USB SET_CONFIGURATION request.
Some devices may have multiple configuration options, and Windows and
OS X do not always automatically select a device configuration when a
device is enumerated. This new function allows a Chrome App to select a
device configuration once the device is opened.

From a security and isolation perspective this function is no more
dangerous than chrome.usb.resetDevice.

BUG=443213

Review URL: https://codereview.chromium.org/826283002

Cr-Commit-Position: refs/heads/master@{#310591}

chrome/browser/devtools/device/usb/android_usb_browsertest.cc
@@ -176,6 +176,8 @@ class MockUsbDeviceHandle : public UsbDeviceHandle {
 
   virtual void Close() override { device_ = nullptr; }
 
+  bool SetConfiguration(int configuration_value) override { return true; }
+
   bool ClaimInterface(int interface_number) override {
     if (device_->claimed_interfaces_.find(interface_number) !=
         device_->claimed_interfaces_.end())
@@ -471,8 +473,8 @@ class MockUsbDevice : public UsbDevice {
     return new MockUsbDeviceHandle<T>(this);
   }
 
-  virtual const UsbConfigDescriptor& GetConfiguration() override {
-    return config_desc_;
+  virtual const UsbConfigDescriptor* GetConfiguration() override {
+    return &config_desc_;
   }
 
   virtual bool GetManufacturer(base::string16* manufacturer) override {

chrome/browser/devtools/device/usb/android_usb_device.cc
@@ -200,16 +200,17 @@ static void OpenAndroidDeviceOnFileThread(
   if (success) {
     base::string16 serial;
     if (device->GetSerialNumber(&serial) && !serial.empty()) {
-      const UsbConfigDescriptor& config = device->GetConfiguration();
-      scoped_refptr<UsbDeviceHandle> usb_handle = device->Open();
-      if (usb_handle.get()) {
-        scoped_refptr<AndroidUsbDevice> android_device =
-            ClaimInterface(rsa_key, usb_handle, serial,
-                           config.interfaces[interface_id]);
-        if (android_device.get())
-          devices->push_back(android_device.get());
-        else
-          usb_handle->Close();
+      const UsbConfigDescriptor* config = device->GetConfiguration();
+      if (config) {
+        scoped_refptr<UsbDeviceHandle> usb_handle = device->Open();
+        if (usb_handle.get()) {
+          scoped_refptr<AndroidUsbDevice> android_device = ClaimInterface(
+              rsa_key, usb_handle, serial, config->interfaces[interface_id]);
+          if (android_device.get())
+            devices->push_back(android_device.get());
+          else
+            usb_handle->Close();
+        }
       }
     }
   }
@@ -223,16 +224,13 @@ static int CountOnFileThread() {
   if (service != NULL)
     service->GetDevices(&usb_devices);
   int device_count = 0;
-  for (UsbDevices::iterator deviceIt = usb_devices.begin();
-       deviceIt != usb_devices.end();
-       ++deviceIt) {
-    const UsbConfigDescriptor& config = (*deviceIt)->GetConfiguration();
-
-    for (UsbInterfaceDescriptor::Iterator ifaceIt = config.interfaces.begin();
-         ifaceIt != config.interfaces.end();
-         ++ifaceIt) {
-      if (IsAndroidInterface(*ifaceIt)) {
-        ++device_count;
+  for (const scoped_refptr<UsbDevice>& device : usb_devices) {
+    const UsbConfigDescriptor* config = device->GetConfiguration();
+    if (config) {
+      for (const UsbInterfaceDescriptor& iface : config->interfaces) {
+        if (IsAndroidInterface(iface)) {
+          ++device_count;
+        }
       }
     }
   }
@@ -258,22 +256,24 @@ static void EnumerateOnFileThread(
                                      devices,
                                      caller_message_loop_proxy));
 
-  for (UsbDevices::iterator it = usb_devices.begin(); it != usb_devices.end();
-       ++it) {
-    const UsbConfigDescriptor& config = (*it)->GetConfiguration();
-
+  for (const scoped_refptr<UsbDevice>& device : usb_devices) {
+    const UsbConfigDescriptor* config = device->GetConfiguration();
+    if (!config) {
+      continue;
+    }
     bool has_android_interface = false;
-    for (size_t j = 0; j < config.interfaces.size(); ++j) {
-      if (!IsAndroidInterface(config.interfaces[j])) {
+    for (size_t j = 0; j < config->interfaces.size(); ++j) {
+      if (!IsAndroidInterface(config->interfaces[j])) {
         continue;
       }
 
       // Request permission on Chrome OS.
 #if defined(OS_CHROMEOS)
-      (*it)->RequestUsbAccess(j, base::Bind(&OpenAndroidDeviceOnFileThread,
-                                            devices, rsa_key, barrier, *it, j));
+      device->RequestUsbAccess(
+          j, base::Bind(&OpenAndroidDeviceOnFileThread, devices, rsa_key,
+                        barrier, device, j));
 #else
-      OpenAndroidDeviceOnFileThread(devices, rsa_key, barrier, *it, j, true);
+      OpenAndroidDeviceOnFileThread(devices, rsa_key, barrier, device, j, true);
 #endif  // defined(OS_CHROMEOS)
 
       has_android_interface = true;

device/usb/usb_device_handle_impl.cc
@@ -377,6 +377,7 @@ bool UsbDeviceHandleImpl::Transfer::Submit(
 
 void UsbDeviceHandleImpl::Transfer::Cancel() {
   libusb_cancel_transfer(platform_transfer_);
+  claimed_interface_ = nullptr;
 }
 
 void UsbDeviceHandleImpl::Transfer::ProcessCompletion() {
@@ -477,11 +478,9 @@ void LIBUSB_CALL UsbDeviceHandleImpl::Transfer::PlatformCallback(
 
 UsbDeviceHandleImpl::UsbDeviceHandleImpl(scoped_refptr<UsbContext> context,
                                          UsbDeviceImpl* device,
-                                         PlatformUsbDeviceHandle handle,
-                                         const UsbConfigDescriptor& config)
+                                         PlatformUsbDeviceHandle handle)
     : device_(device),
       handle_(handle),
-      config_(config),
       context_(context),
       task_runner_(base::ThreadTaskRunnerHandle::Get()),
       weak_factory_(this) {
@@ -503,6 +502,28 @@ void UsbDeviceHandleImpl::Close() {
   DCHECK(thread_checker_.CalledOnValidThread());
   if (device_)
     device_->Close(this);
+}
+
+bool UsbDeviceHandleImpl::SetConfiguration(int configuration_value) {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  if (!device_) {
+    return false;
+  }
+
+  for (Transfer* transfer : transfers_) {
+    transfer->Cancel();
+  }
+  claimed_interfaces_.clear();
+
+  int rv = libusb_set_configuration(handle_, configuration_value);
+  if (rv == LIBUSB_SUCCESS) {
+    device_->RefreshConfiguration();
+    RefreshEndpointMap();
+  } else {
+    VLOG(1) << "Failed to set configuration " << configuration_value << ": "
+            << ConvertPlatformUsbErrorToString(rv);
+  }
+  return rv == LIBUSB_SUCCESS;
 }
 
 bool UsbDeviceHandleImpl::ClaimInterface(const int interface_number) {
@@ -730,22 +751,20 @@ void UsbDeviceHandleImpl::IsochronousTransfer(
 void UsbDeviceHandleImpl::RefreshEndpointMap() {
   DCHECK(thread_checker_.CalledOnValidThread());
   endpoint_map_.clear();
-  for (ClaimedInterfaceMap::iterator claimedIt = claimed_interfaces_.begin();
-       claimedIt != claimed_interfaces_.end();
-       ++claimedIt) {
-    for (UsbInterfaceDescriptor::Iterator ifaceIt = config_.interfaces.begin();
-         ifaceIt != config_.interfaces.end();
-         ++ifaceIt) {
-      if (ifaceIt->interface_number == claimedIt->first &&
-          ifaceIt->alternate_setting ==
-              claimedIt->second->alternate_setting()) {
-        for (UsbEndpointDescriptor::Iterator endpointIt =
-                 ifaceIt->endpoints.begin();
-             endpointIt != ifaceIt->endpoints.end();
-             ++endpointIt) {
-          endpoint_map_[endpointIt->address] = claimedIt->first;
+  const UsbConfigDescriptor* config = device_->GetConfiguration();
+  if (config) {
+    for (const auto& map_entry : claimed_interfaces_) {
+      int interface_number = map_entry.first;
+      const scoped_refptr<InterfaceClaimer>& claimed_iface = map_entry.second;
+
+      for (const UsbInterfaceDescriptor& iface : config->interfaces) {
+        if (iface.interface_number == interface_number &&
+            iface.alternate_setting == claimed_iface->alternate_setting()) {
+          for (const UsbEndpointDescriptor& endpoint : iface.endpoints) {
+            endpoint_map_[endpoint.address] = interface_number;
+          }
+          break;
         }
-        break;
       }
     }
   }

mirror/chromium

commit fe995e8190cf6d14b6ec8e1b9d8fffd8ab99aa46

Fix Android USB debugging through DevTools.
First, change 13cf2cfcb5325ecfff98ee8ccde723f22e606d22 introduced a
logic error into AndroidUsbDevice::EnumerateOnFileThread that caused
enumerations to never complete because the BarrierClosure was not
called the correct number of times.

Second, this change may have also exposed an issue (investigated
before) where a libusb_transfer can be cancelled twice, once in
ReleaseInterface and again in CloseInternal. The second cancellation
occurs after the interface has been released and so an invalid handle
is passed to AbortPipe. A cancelled_ member has been added to Transfer
to prevent this from happening.

Some conversions to C++11 range loops (added during debugging to make
the loops clearer) are preserved in this change.

BUG=450007

Review URL: https://codereview.chromium.org/864183002

chrome/browser/devtools/device/usb/android_usb_browsertest.cc
@@ -35,11 +35,20 @@ using device::UsbUsageType;
 
 namespace {
 
+struct NoConfigTraits {
+  static const int kClass = 0xff;
+  static const int kSubclass = 0x42;
+  static const int kProtocol = 0x1;
+  static const bool kBreaks = false;
+  static const bool kConfigured = false;
+};
+
 struct AndroidTraits {
   static const int kClass = 0xff;
   static const int kSubclass = 0x42;
   static const int kProtocol = 0x1;
   static const bool kBreaks = false;
+  static const bool kConfigured = true;
 };
 
 struct NonAndroidTraits {
@@ -47,6 +56,7 @@ struct NonAndroidTraits {
   static const int kSubclass = 0x42;
   static const int kProtocol = 0x2;
   static const bool kBreaks = false;
+  static const bool kConfigured = true;
 };
 
 struct BreakingAndroidTraits {
@@ -54,6 +64,7 @@ struct BreakingAndroidTraits {
   static const int kSubclass = 0x42;
   static const int kProtocol = 0x1;
   static const bool kBreaks = true;
+  static const bool kConfigured = true;
 };
 
 const uint32 kMaxPayload = 4096;
@@ -400,7 +411,7 @@ class MockUsbDevice : public UsbDevice {
   }
 
   virtual const UsbConfigDescriptor* GetConfiguration() override {
-    return &config_desc_;
+    return T::kConfigured ? &config_desc_ : nullptr;
   }
 
   virtual bool GetManufacturer(base::string16* manufacturer) override {
@@ -472,6 +483,20 @@ class MockBreakingUsbService : public UsbService {
   void GetDevices(std::vector<scoped_refptr<UsbDevice>>* devices) override {
     STLClearObject(devices);
     devices->push_back(new MockUsbDevice<BreakingAndroidTraits>());
+  }
+};
+
+class MockNoConfigUsbService : public UsbService {
+ public:
+  scoped_refptr<UsbDevice> GetDeviceById(uint32 unique_id) override {
+    NOTIMPLEMENTED();
+    return nullptr;
+  }
+
+  void GetDevices(std::vector<scoped_refptr<UsbDevice>>* devices) override {
+    STLClearObject(devices);
+    devices->push_back(new MockUsbDevice<AndroidTraits>());
+    devices->push_back(new MockUsbDevice<NoConfigTraits>());
   }
 };
 
@@ -609,6 +634,13 @@ class AndroidBreakingUsbTest : public AndroidUsbDiscoveryTest {
  protected:
   void SetUpService() override {
     UsbService::SetInstanceForTest(new MockBreakingUsbService());
+  }
+};
+
+class AndroidNoConfigUsbTest : public AndroidUsbDiscoveryTest {
+ protected:
+  void SetUpService() override {
+    UsbService::SetInstanceForTest(new MockNoConfigUsbService());
   }
 };
 
@@ -782,6 +814,12 @@ IN_PROC_BROWSER_TEST_F(AndroidUsbDiscoveryTest, TestDeviceDiscovery) {
 }
 
 IN_PROC_BROWSER_TEST_F(AndroidBreakingUsbTest, TestDeviceBreaking) {
+  MockListListener listener(adb_bridge_, runner_->QuitClosure());
+  adb_bridge_->AddDeviceListListener(&listener);
+  runner_->Run();
+}
+
+IN_PROC_BROWSER_TEST_F(AndroidNoConfigUsbTest, TestDeviceNoConfig) {
   MockListListener listener(adb_bridge_, runner_->QuitClosure());
   adb_bridge_->AddDeviceListListener(&listener);
   runner_->Run();

chrome/browser/devtools/device/usb/android_usb_device.cc
@@ -78,16 +78,14 @@ scoped_refptr<AndroidUsbDevice> ClaimInterface(
   int outbound_address = 0;
   int zero_mask = 0;
 
-  for (UsbEndpointDescriptor::Iterator endpointIt = interface.endpoints.begin();
-       endpointIt != interface.endpoints.end();
-       ++endpointIt) {
-    if (endpointIt->transfer_type != device::USB_TRANSFER_BULK)
+  for (const UsbEndpointDescriptor& endpoint : interface.endpoints) {
+    if (endpoint.transfer_type != device::USB_TRANSFER_BULK)
       continue;
-    if (endpointIt->direction == device::USB_DIRECTION_INBOUND)
-      inbound_address = endpointIt->address;
+    if (endpoint.direction == device::USB_DIRECTION_INBOUND)
+      inbound_address = endpoint.address;
     else
-      outbound_address = endpointIt->address;
-    zero_mask = endpointIt->maximum_packet_size - 1;
+      outbound_address = endpoint.address;
+    zero_mask = endpoint.maximum_packet_size - 1;
   }
 
   if (inbound_address == 0 || outbound_address == 0)
@@ -169,9 +167,8 @@ static void RespondOnCallerThread(const AndroidUsbDevicesCallback& callback,
   scoped_ptr<AndroidUsbDevices> devices(new_devices);
 
   // Add raw pointers to the newly claimed devices.
-  for (AndroidUsbDevices::iterator it = devices->begin(); it != devices->end();
-       ++it) {
-    g_devices.Get().push_back(it->get());
+  for (const scoped_refptr<AndroidUsbDevice>& device : *devices) {
+    g_devices.Get().push_back(device.get());
   }
 
   // Return all claimed devices.
@@ -207,7 +204,7 @@ static void OpenAndroidDeviceOnFileThread(
           scoped_refptr<AndroidUsbDevice> android_device = ClaimInterface(
               rsa_key, usb_handle, serial, config->interfaces[interface_id]);
           if (android_device.get())
-            devices->push_back(android_device.get());
+            devices->push_back(android_device);
           else
             usb_handle->Close();
         }
@@ -259,6 +256,7 @@ static void EnumerateOnFileThread(
   for (const scoped_refptr<UsbDevice>& device : usb_devices) {
     const UsbConfigDescriptor* config = device->GetConfiguration();
     if (!config) {
+      barrier.Run();
       continue;
     }
     bool has_android_interface = false;
@@ -299,12 +297,11 @@ void AndroidUsbDevice::Enumerate(crypto::RSAPrivateKey* rsa_key,
                                  const AndroidUsbDevicesCallback& callback) {
 
   // Collect devices with closed handles.
-  for (std::vector<AndroidUsbDevice*>::iterator it = g_devices.Get().begin();
-       it != g_devices.Get().end(); ++it) {
-    if ((*it)->usb_handle_.get()) {
+  for (AndroidUsbDevice* device : g_devices.Get()) {
+    if (device->usb_handle_.get()) {
       BrowserThread::PostTask(BrowserThread::FILE, FROM_HERE,
-          base::Bind(&AndroidUsbDevice::TerminateIfReleased, *it,
-                     (*it)->usb_handle_));
+          base::Bind(&AndroidUsbDevice::TerminateIfReleased, device,
+                     device->usb_handle_));
     }
   }
 

device/usb/usb_device_handle_impl.cc
@@ -211,6 +211,7 @@ class UsbDeviceHandleImpl::Transfer {
   scoped_refptr<UsbDeviceHandleImpl::InterfaceClaimer> claimed_interface_;
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   size_t length_;
+  bool cancelled_;
   UsbTransferCallback callback_;
   scoped_refptr<base::SingleThreadTaskRunner> callback_task_runner_;
 };
@@ -340,6 +341,7 @@ UsbDeviceHandleImpl::Transfer::Transfer(UsbTransferType transfer_type,
     : transfer_type_(transfer_type),
       buffer_(buffer),
       length_(length),
+      cancelled_(false),
       callback_(callback) {
   // Remember the thread from which this transfer was created so that |callback|
   // can be dispatched there.
@@ -376,8 +378,11 @@ bool UsbDeviceHandleImpl::Transfer::Submit(
 }
 
 void UsbDeviceHandleImpl::Transfer::Cancel() {
-  libusb_cancel_transfer(platform_transfer_);
-  claimed_interface_ = nullptr;
+  if (!cancelled_) {
+    libusb_cancel_transfer(platform_transfer_);
+    claimed_interface_ = nullptr;
+  }
+  cancelled_ = true;
 }
 
 void UsbDeviceHandleImpl::Transfer::ProcessCompletion() {