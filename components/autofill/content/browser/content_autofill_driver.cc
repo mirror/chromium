crosswalk-project/chromium-crosswalk

commit d0a0c992fc0eb9c81d514a256a5c0eb7928efea6

Remove RenderViewObserver::Resized (try 2)
The only client was AutofillAgent; reimplement the functionality in a slightly different way. While we're at it, fix a bug where the browser-owned popup is hidden without the renderer's knowledge.

BUG=433486,449325

Review URL: https://codereview.chromium.org/854943002

Cr-Commit-Position: refs/heads/master@{#312346}

components/autofill/content/browser/content_autofill_driver.cc
@@ -145,6 +145,13 @@ void ContentAutofillDriver::RendererShouldPreviewFieldWithValue(
       render_frame_host_->GetRoutingID(), value));
 }
 
+void ContentAutofillDriver::PopupHidden() {
+  if (!RendererIsAvailable())
+    return;
+  render_frame_host_->Send(
+      new AutofillMsg_PopupHidden(render_frame_host_->GetRoutingID()));
+}
+
 bool ContentAutofillDriver::HandleMessage(const IPC::Message& message) {
   bool handled = true;
   IPC_BEGIN_MESSAGE_MAP(ContentAutofillDriver, message)

components/autofill/content/common/autofill_messages.h
@@ -155,6 +155,9 @@ IPC_MESSAGE_ROUTED1(AutofillMsg_FillFieldWithValue,
 IPC_MESSAGE_ROUTED1(AutofillMsg_PreviewFieldWithValue,
                     base::string16 /* value */)
 
+// The popup has been hidden.
+IPC_MESSAGE_ROUTED0(AutofillMsg_PopupHidden);
+
 // Sets the currently selected node's value to be the given data list value.
 IPC_MESSAGE_ROUTED1(AutofillMsg_AcceptDataListSuggestion,
                     base::string16 /* accepted data list value */)

components/autofill/content/renderer/autofill_agent.cc
@@ -172,6 +172,7 @@ bool AutofillAgent::OnMessageReceived(const IPC::Message& message) {
                         OnFillPasswordSuggestion)
     IPC_MESSAGE_HANDLER(AutofillMsg_PreviewPasswordSuggestion,
                         OnPreviewPasswordSuggestion)
+    IPC_MESSAGE_HANDLER(AutofillMsg_PopupHidden, OnPopupHidden)
     IPC_MESSAGE_HANDLER(AutofillMsg_RequestAutocompleteResult,
                         OnRequestAutocompleteResult)
     IPC_MESSAGE_UNHANDLED(handled = false)
@@ -232,10 +233,6 @@ void AutofillAgent::FocusedNodeChanged(const WebNode& node) {
     return;
 
   element_ = *element;
-}
-
-void AutofillAgent::Resized() {
-  HidePopup();
 }
 
 void AutofillAgent::didRequestAutocomplete(
@@ -550,6 +547,11 @@ void AutofillAgent::OnPreviewPasswordSuggestion(
   DCHECK(handled);
 }
 
+void AutofillAgent::OnPopupHidden() {
+  if (!element_.isNull())
+    OnClearPreviewedForm();
+}
+
 void AutofillAgent::OnRequestAutocompleteResult(
     WebFormElement::AutocompleteResult result,
     const base::string16& message,
@@ -740,10 +742,6 @@ void AutofillAgent::ProcessForms() {
 void AutofillAgent::HidePopup() {
   if (!is_popup_possibly_visible_)
     return;
-
-  if (!element_.isNull())
-    OnClearPreviewedForm();
-
   is_popup_possibly_visible_ = false;
   Send(new AutofillHostMsg_HidePopup(routing_id()));
 }
@@ -783,10 +781,6 @@ void AutofillAgent::LegacyAutofillAgent::OnDestruct() {
 void AutofillAgent::LegacyAutofillAgent::FocusedNodeChanged(
     const WebNode& node) {
   agent_->FocusedNodeChanged(node);
-}
-
-void AutofillAgent::LegacyAutofillAgent::Resized() {
-  agent_->Resized();
 }
 
 }  // namespace autofill

components/autofill/content/renderer/autofill_agent.h
@@ -67,7 +67,6 @@ class AutofillAgent : public content::RenderFrameObserver,
     // content::RenderViewObserver:
     void OnDestruct() override;
     void FocusedNodeChanged(const blink::WebNode& node) override;
-    void Resized() override;
 
     AutofillAgent* agent_;
 
@@ -115,10 +114,9 @@ class AutofillAgent : public content::RenderFrameObserver,
   void WillSubmitForm(const blink::WebFormElement& form) override;
   void DidChangeScrollOffset() override;
 
-  // Pass-throughs from LegacyAutofillAgent. These correlate with
-  // RenderViewObserver methods.
+  // Pass-through from LegacyAutofillAgent. This correlates with the
+  // RenderViewObserver method.
   void FocusedNodeChanged(const blink::WebNode& node);
-  void Resized();
 
   // PageClickListener:
   void FormControlElementClicked(const blink::WebFormControlElement& element,
@@ -158,6 +156,7 @@ class AutofillAgent : public content::RenderFrameObserver,
                                 const base::string16& password);
   void OnPreviewPasswordSuggestion(const base::string16& username,
                                    const base::string16& password);
+  void OnPopupHidden();
 
   // Called when interactive autocomplete finishes. |message| is printed to
   // the console if non-empty.

chrome/browser/ui/autofill/chrome_autofill_client.cc
@@ -219,6 +219,10 @@ void ChromeAutofillClient::DidNavigateAnyFrame(
     HideRequestAutocompleteDialog();
 }
 
+void ChromeAutofillClient::MainFrameWasResized() {
+  HideAutofillPopup();
+}
+
 void ChromeAutofillClient::WebContentsDestroyed() {
   HideAutofillPopup();
 }

chrome/browser/ui/autofill/chrome_autofill_client.h
@@ -82,6 +82,7 @@ class ChromeAutofillClient
       content::RenderFrameHost* render_frame_host,
       const content::LoadCommittedDetails& details,
       const content::FrameNavigateParams& params) override;
+  void MainFrameWasResized() override;
   void WebContentsDestroyed() override;
 
   // ZoomObserver implementation.

components/autofill/content/browser/content_autofill_driver.h
@@ -61,6 +61,7 @@ class ContentAutofillDriver : public AutofillDriver {
   void RendererShouldFillFieldWithValue(const base::string16& value) override;
   void RendererShouldPreviewFieldWithValue(
       const base::string16& value) override;
+  void PopupHidden() override;
 
   // Handles a message that came from the associated render frame.
   bool HandleMessage(const IPC::Message& message);

components/autofill/core/browser/autofill_driver.h
@@ -89,6 +89,9 @@ class AutofillDriver {
   // Tells the renderer to preview the node with suggested text.
   virtual void RendererShouldPreviewFieldWithValue(
       const base::string16& value) = 0;
+
+  // Informs the renderer that the popup has been hidden.
+  virtual void PopupHidden() = 0;
 };
 
 }  // namespace autofill

mirror/chromium

commit 48c314d6c3d8954395a76d195180d5ae7e3091f1

autofill - Keep showing form preview while card unmasking prompt is up.
This used to work, but was broken by d0a0c992fc0eb9c81d514a

BUG=none

Review URL: https://codereview.chromium.org/872713005

components/autofill/content/browser/content_autofill_driver.cc
@@ -146,10 +146,10 @@ void ContentAutofillDriver::RendererShouldPreviewFieldWithValue(
 }
 
 void ContentAutofillDriver::PopupHidden() {
-  if (!RendererIsAvailable())
-    return;
-  render_frame_host_->Send(
-      new AutofillMsg_PopupHidden(render_frame_host_->GetRoutingID()));
+  // If the unmask prompt is showing, keep showing the preview. The preview
+  // will be cleared when the prompt closes.
+  if (!autofill_manager_->IsShowingUnmaskPrompt())
+    RendererShouldClearPreviewedForm();
 }
 
 bool ContentAutofillDriver::HandleMessage(const IPC::Message& message) {

components/autofill/content/common/autofill_messages.h
@@ -155,9 +155,6 @@ IPC_MESSAGE_ROUTED1(AutofillMsg_FillFieldWithValue,
 IPC_MESSAGE_ROUTED1(AutofillMsg_PreviewFieldWithValue,
                     base::string16 /* value */)
 
-// The popup has been hidden.
-IPC_MESSAGE_ROUTED0(AutofillMsg_PopupHidden)
-
 // Sets the currently selected node's value to be the given data list value.
 IPC_MESSAGE_ROUTED1(AutofillMsg_AcceptDataListSuggestion,
                     base::string16 /* accepted data list value */)

components/autofill/content/renderer/autofill_agent.cc
@@ -173,7 +173,6 @@ bool AutofillAgent::OnMessageReceived(const IPC::Message& message) {
                         OnFillPasswordSuggestion)
     IPC_MESSAGE_HANDLER(AutofillMsg_PreviewPasswordSuggestion,
                         OnPreviewPasswordSuggestion)
-    IPC_MESSAGE_HANDLER(AutofillMsg_PopupHidden, OnPopupHidden)
     IPC_MESSAGE_HANDLER(AutofillMsg_RequestAutocompleteResult,
                         OnRequestAutocompleteResult)
     IPC_MESSAGE_UNHANDLED(handled = false)
@@ -557,11 +556,6 @@ void AutofillAgent::OnPreviewPasswordSuggestion(
       username,
       password);
   DCHECK(handled);
-}
-
-void AutofillAgent::OnPopupHidden() {
-  if (!element_.isNull())
-    OnClearPreviewedForm();
 }
 
 void AutofillAgent::OnRequestAutocompleteResult(

components/autofill/content/renderer/autofill_agent.h
@@ -157,7 +157,6 @@ class AutofillAgent : public content::RenderFrameObserver,
                                 const base::string16& password);
   void OnPreviewPasswordSuggestion(const base::string16& username,
                                    const base::string16& password);
-  void OnPopupHidden();
 
   // Called when interactive autocomplete finishes. |message| is printed to
   // the console if non-empty.

components/autofill/core/browser/autofill_manager.cc
@@ -652,6 +652,10 @@ void AutofillManager::RemoveAutocompleteEntry(const base::string16& name,
   autocomplete_history_manager_->OnRemoveAutocompleteEntry(name, value);
 }
 
+bool AutofillManager::IsShowingUnmaskPrompt() {
+  return unmasking_card_.Compare(CreditCard()) != 0;
+}
+
 const std::vector<FormStructure*>& AutofillManager::GetFormStructures() {
   return form_structures_.get();
 }

components/autofill/core/browser/autofill_manager.h
@@ -128,6 +128,9 @@ class AutofillManager : public AutofillDownloadManager::Observer,
   void RemoveAutocompleteEntry(const base::string16& name,
                                const base::string16& value);
 
+  // Returns true when the Wallet card unmask prompt is being displayed.
+  bool IsShowingUnmaskPrompt();
+
   // Returns the present form structures seen by Autofill manager.
   const std::vector<FormStructure*>& GetFormStructures();
 