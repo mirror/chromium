crosswalk-project/chromium-crosswalk

commit 04015b275eb5c775a5740722fd0805994dde8d2c

Allow CC HUD font to be created before sandbox on Windows
Similar to https://codereview.chromium.org/900063002, but instead of
having the SkTypeface* directly in LayerTreeSettings, defer to a
function. This avoids creating the font until it used on platforms
other than Windows so that we don't incur a startup penalty when the
hud isn't being used.

On Windows, we replace a previous arbitrary font that was getting
warmed up with the monospace one we want here to hopefully avoid
perf regression there.

R=danakj@chromium.org
BUG=453731, 455104, 457248
TEST=run chrome with --enable-win32k-renderer-lockdown --show-fps-counter --ui-show-fps-counter on Win8

Review URL: https://codereview.chromium.org/932243004

Cr-Commit-Position: refs/heads/master@{#318297}

content/renderer/renderer_main_platform_delegate_win.cc
@@ -23,6 +23,7 @@
 #include "third_party/icu/source/i18n/unicode/timezone.h"
 #include "third_party/skia/include/ports/SkFontMgr.h"
 #include "third_party/skia/include/ports/SkTypeface_win.h"
+#include "ui/gfx/hud_font.h"
 #include "ui/gfx/win/direct_write.h"
 #include "ui/gfx/win/dpi.h"
 
@@ -54,10 +55,16 @@ void SkiaPreCacheFontCharacters(const LOGFONT& logfont,
 void WarmupDirectWrite() {
   // The objects used here are intentionally not freed as we want the Skia
   // code to use these objects after warmup.
-  SkTypeface* typeface =
-      GetPreSandboxWarmupFontMgr()->legacyCreateTypeface("Times New Roman", 0);
-  DoPreSandboxWarmupForTypeface(typeface);
   SetDefaultSkiaFactory(GetPreSandboxWarmupFontMgr());
+
+  // We need to warm up *some* font for DirectWrite. We use this one
+  // specifically, because the CC HUD code needs to have a font anyway, so warm
+  // up the monospace one it wants, and then pass it down.
+  skia::RefPtr<SkTypeface> hud_typeface =
+      skia::AdoptRef(GetPreSandboxWarmupFontMgr()->legacyCreateTypeface(
+          "Lucida Console", SkTypeface::kBold));
+  DoPreSandboxWarmupForTypeface(hud_typeface.get());
+  gfx::SetHudTypeface(hud_typeface);
 }
 
 }  // namespace

cc/layers/heads_up_display_layer_impl.cc
@@ -29,6 +29,7 @@
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/geometry/size_conversions.h"
+#include "ui/gfx/hud_font.h"
 
 namespace cc {
 
@@ -70,12 +71,15 @@ double HeadsUpDisplayLayerImpl::Graph::UpdateUpperBound() {
 HeadsUpDisplayLayerImpl::HeadsUpDisplayLayerImpl(LayerTreeImpl* tree_impl,
                                                  int id)
     : LayerImpl(tree_impl, id),
-      typeface_(skia::AdoptRef(
-          SkTypeface::CreateFromName("monospace", SkTypeface::kBold))),
+      typeface_(gfx::GetHudTypeface()),
       internal_contents_scale_(1.f),
       fps_graph_(60.0, 80.0),
       paint_time_graph_(16.0, 48.0),
       fade_step_(0) {
+  if (!typeface_) {
+    typeface_ = skia::AdoptRef(
+        SkTypeface::CreateFromName("monospace", SkTypeface::kBold));
+  }
 }
 
 HeadsUpDisplayLayerImpl::~HeadsUpDisplayLayerImpl() {}

ui/gfx/BUILD.gn
@@ -105,6 +105,8 @@ component("gfx") {
     "gfx_paths.h",
     "gpu_memory_buffer.cc",
     "gpu_memory_buffer.h",
+    "hud_font.cc",
+    "hud_font.h",
     "icon_util.cc",
     "icon_util.h",
     "image/canvas_image_source.cc",

ui/gfx/gfx.gyp
@@ -184,6 +184,8 @@
         'gpu_memory_buffer.h',
         'harfbuzz_font_skia.cc',
         'harfbuzz_font_skia.h',
+        'hud_font.cc',
+        'hud_font.h',
         'image/canvas_image_source.cc',
         'image/canvas_image_source.h',
         'image/image.cc',

ui/gfx/hud_font.cc
@@ -1,0 +1,25 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/gfx/hud_font.h"
+
+#include "base/lazy_instance.h"
+#include "third_party/skia/include/core/SkTypeface.h"
+
+namespace gfx {
+
+namespace {
+base::LazyInstance<skia::RefPtr<SkTypeface>> g_hud_typeface;
+}  // namespace
+
+void SetHudTypeface(skia::RefPtr<SkTypeface> typeface) {
+  g_hud_typeface.Get() = typeface;
+}
+
+skia::RefPtr<SkTypeface> GetHudTypeface() {
+  // nullptr is fine; caller will create its own in that case.
+  return g_hud_typeface.Get();
+}
+
+}  // namespace gfx

ui/gfx/hud_font.h
@@ -1,0 +1,20 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_GFX_HUD_FONT_H_
+#define UI_GFX_HUD_FONT_H_
+
+#include "skia/ext/refptr.h"
+#include "ui/gfx/gfx_export.h"
+
+class SkTypeface;
+
+namespace gfx {
+
+GFX_EXPORT void SetHudTypeface(skia::RefPtr<SkTypeface> typeface);
+GFX_EXPORT skia::RefPtr<SkTypeface> GetHudTypeface();
+
+}  // namespace gfx
+
+#endif  // UI_GFX_HUD_FONT_H_

mirror/chromium

commit 9586cb1546deb711029d44ee4bf43e88ea33bceb

Try switching from Lucida Console to Times to avoid startup regression
A previous change https://chromium.googlesource.com/chromium/src/+/04015b275eb5c775a5740722fd0805994dde8d2c
caused a regression on XP only in messageloop_start_time.

The previous change substituted fonts (rather than adding a new one) to try to avoid
this, but it apparently didn't always help. So, instead use Times New Roman in the font
dialog (which was happening before) to attempt to avoid this. This will make the
debug hud a little uglier on Windows, but not the end of the world.

There's unfortunately no perf trybots for this config, so it needs to actually land to
test.

R=jam@chromium.org
BUG=463613
TEST=run chrome with --enable-win32k-renderer-lockdown --show-fps-counter --ui-show-fps-counter

Review URL: https://codereview.chromium.org/1069163003

content/renderer/renderer_main_platform_delegate_win.cc
@@ -57,12 +57,12 @@ void WarmupDirectWrite() {
   // code to use these objects after warmup.
   SetDefaultSkiaFactory(GetPreSandboxWarmupFontMgr());
 
-  // We need to warm up *some* font for DirectWrite. We use this one
-  // specifically, because the CC HUD code needs to have a font anyway, so warm
-  // up the monospace one it wants, and then pass it down.
-  skia::RefPtr<SkTypeface> hud_typeface =
-      skia::AdoptRef(GetPreSandboxWarmupFontMgr()->legacyCreateTypeface(
-          "Lucida Console", SkTypeface::kBold));
+  // We need to warm up *some* font for DirectWrite. We also need to pass one
+  // down for the CC HUD code, so use the same one here. Note that we don't use
+  // a monospace as would be nice in an attempt to avoid a small startup time
+  // regression, see http://crbug.com/463613.
+  skia::RefPtr<SkTypeface> hud_typeface = skia::AdoptRef(
+      GetPreSandboxWarmupFontMgr()->legacyCreateTypeface("Times New Roman", 0));
   DoPreSandboxWarmupForTypeface(hud_typeface.get());
   gfx::SetHudTypeface(hud_typeface);
 }