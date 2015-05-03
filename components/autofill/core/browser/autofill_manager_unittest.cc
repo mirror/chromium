crosswalk-project/chromium-crosswalk

commit 8949a8b27dfb26c1780c6affc71786d56e8e1501

Don't offer to save credit cards twice in a row.
1. If a user manually inputs a card that matches an unmasked credit card, don't offer to save
2. If a user fills a card of any type, don't offer to save
3. If a user manually inputs a card that matches a masked credit card, do offer to save

This patch fixes [2] for masked server cards. Note that in some rare cases it may break [3], because the method of determining whether a card was typed or filled is imperfect. For example, if I filled with a wallet card but didn't store the real pan, then deleted the number and typed it in by hand instead, we wouldn't offer to save. But I don't think the added complexity of fixing this is worth the rare benefit for this questionable situation.

BUG=478773

Review URL: https://codereview.chromium.org/1097143002

Cr-Commit-Position: refs/heads/master@{#326289}
(cherry picked from commit 3e28caf673aa69f8f09b0f05c4d2cdde47e9091b)

Review URL: https://codereview.chromium.org/1101123002

Cr-Commit-Position: refs/branch-heads/2357@{#210}
Cr-Branched-From: 59d4494849b405682265ed5d3f5164573b9a939b-refs/heads/master@{#323860}

components/autofill/core/browser/autofill_manager.cc
@@ -397,6 +397,8 @@ bool AutofillManager::OnFormSubmitted(const FormData& form) {
   if (submitted_form->IsAutofillable())
     ImportFormData(*submitted_form);
 
+  recently_unmasked_cards_.clear();
+
   return true;
 }
 
@@ -802,6 +804,7 @@ void AutofillManager::OnDidGetRealPan(AutofillClient::GetRealPanResult result,
   if (!real_pan.empty()) {
     DCHECK_EQ(AutofillClient::SUCCESS, result);
     credit_card_form_event_logger_->OnDidFillSuggestion(unmasking_card_);
+    recently_unmasked_cards_.push_back(unmasking_card_);
     unmasking_card_.set_record_type(CreditCard::FULL_SERVER_CARD);
     unmasking_card_.SetNumber(base::UTF8ToUTF16(real_pan));
     if (unmask_response_.should_store_pan)
@@ -826,6 +829,17 @@ void AutofillManager::ImportFormData(const FormStructure& submitted_form) {
   scoped_ptr<CreditCard> imported_credit_card;
   if (!personal_data_->ImportFormData(submitted_form, &imported_credit_card))
     return;
+
+  // Don't offer to save any cards that were recently unmasked.
+  if (recently_unmasked_cards_.end() !=
+      std::find_if(recently_unmasked_cards_.begin(),
+                   recently_unmasked_cards_.end(),
+                   [&imported_credit_card](const CreditCard& unmasked) -> bool {
+                     return unmasked.TypeAndLastFourDigits() ==
+                            imported_credit_card->TypeAndLastFourDigits();
+                   })) {
+    return;
+  }
 
   // If credit card information was submitted, we need to confirm whether to
   // save it.

components/autofill/core/browser/autofill_manager_unittest.cc
@@ -35,6 +35,7 @@
 #include "components/autofill/core/common/form_data.h"
 #include "components/autofill/core/common/form_field_data.h"
 #include "grit/components_strings.h"
+#include "net/url_request/url_request_test_util.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -50,6 +51,19 @@ namespace autofill {
 namespace {
 
 const int kDefaultPageID = 137;
+
+class MockAutofillClient : public TestAutofillClient {
+ public:
+  MockAutofillClient() {}
+
+  ~MockAutofillClient() override {}
+
+  MOCK_METHOD1(ConfirmSaveCreditCard,
+               void(const base::Closure& save_card_callback));
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MockAutofillClient);
+};
 
 class TestPersonalDataManager : public PersonalDataManager {
  public:
@@ -628,6 +642,9 @@ class AutofillManagerTest : public testing::Test {
     personal_data_.set_database(autofill_client_.GetDatabase());
     personal_data_.SetPrefService(autofill_client_.GetPrefs());
     autofill_driver_.reset(new MockAutofillDriver());
+    request_context_ =
+        new net::TestURLRequestContextGetter(base::MessageLoopProxy::current());
+    autofill_driver_->SetURLRequestContext(request_context_.get());
     autofill_manager_.reset(new TestAutofillManager(
         autofill_driver_.get(), &autofill_client_, &personal_data_));
 
@@ -647,6 +664,8 @@ class AutofillManagerTest : public testing::Test {
     // need to care about removing self as an observer in destruction.
     personal_data_.set_database(scoped_refptr<AutofillWebDataService>(NULL));
     personal_data_.SetPrefService(NULL);
+
+    request_context_ = nullptr;
   }
 
   void GetAutofillSuggestions(int query_id,
@@ -718,10 +737,11 @@ class AutofillManagerTest : public testing::Test {
 
  protected:
   base::MessageLoop message_loop_;
-  TestAutofillClient autofill_client_;
+  MockAutofillClient autofill_client_;
   scoped_ptr<MockAutofillDriver> autofill_driver_;
   scoped_ptr<TestAutofillManager> autofill_manager_;
   scoped_ptr<TestAutofillExternalDelegate> external_delegate_;
+  scoped_refptr<net::TestURLRequestContextGetter> request_context_;
   TestPersonalDataManager personal_data_;
 };
 
@@ -3073,35 +3093,6 @@ TEST_F(AutofillManagerTest, AccessAddressBookPrompt) {
 }
 #endif  // defined(OS_MACOSX) && !defined(OS_IOS)
 
-namespace {
-
-class MockAutofillClient : public TestAutofillClient {
- public:
-  MockAutofillClient() {}
-
-  ~MockAutofillClient() override {}
-
-  void ShowRequestAutocompleteDialog(const FormData& form,
-                                     content::RenderFrameHost* rfh,
-                                     const ResultCallback& callback) override {
-    callback.Run(user_supplied_data_ ? AutocompleteResultSuccess :
-                                       AutocompleteResultErrorDisabled,
-                 base::string16(),
-                 user_supplied_data_.get());
-  }
-
-  void SetUserSuppliedData(scoped_ptr<FormStructure> user_supplied_data) {
-    user_supplied_data_.reset(user_supplied_data.release());
-  }
-
- private:
-  scoped_ptr<FormStructure> user_supplied_data_;
-
-  DISALLOW_COPY_AND_ASSIGN(MockAutofillClient);
-};
-
-}  // namespace
-
 // Test our external delegate is called at the right time.
 TEST_F(AutofillManagerTest, TestExternalDelegate) {
   FormData form;
@@ -3174,6 +3165,50 @@ TEST_F(AutofillManagerTest, GetCreditCardSuggestionsForNumberSpitAcrossFields) {
           "Visa\xC2\xA0\xE2\x8B\xAF"
           "3456",
           "04/12", kVisaCard, autofill_manager_->GetPackedCreditCardID(4)));
+}
+
+TEST_F(AutofillManagerTest, DontOfferToSaveWalletCard) {
+  // This line silences the warning from RealPanWalletClient about matching
+  // sync and wallet server types.
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      "sync-url", "https://google.com");
+
+  // Set up our form data.
+  FormData form;
+  CreateTestCreditCardFormData(&form, true, false);
+  std::vector<FormData> forms(1, form);
+  FormsSeen(forms);
+
+  CreditCard card(CreditCard::MASKED_SERVER_CARD, "a123");
+  test::SetCreditCardInfo(&card, "John Dillinger", "1881" /* Visa */, "01",
+                          "2017");
+  card.SetTypeForMaskedCard(kVisaCard);
+
+  EXPECT_CALL(autofill_client_, ConfirmSaveCreditCard(_)).Times(0);
+  EXPECT_CALL(*autofill_driver_, SendFormDataToRenderer(_, _, _));
+  autofill_manager_->FillOrPreviewCreditCardForm(
+      AutofillDriver::FORM_DATA_ACTION_FILL, kDefaultPageID, form,
+      form.fields[0], card, 0);
+
+  // Manually fill out |form| so we can use it in OnFormSubmitted.
+  for (size_t i = 0; i < form.fields.size(); ++i) {
+    if (form.fields[i].name == ASCIIToUTF16("cardnumber"))
+      form.fields[i].value = ASCIIToUTF16("4012888888881881");
+    else if (form.fields[i].name == ASCIIToUTF16("nameoncard"))
+      form.fields[i].value = ASCIIToUTF16("John H Dillinger");
+    else if (form.fields[i].name == ASCIIToUTF16("ccmonth"))
+      form.fields[i].value = ASCIIToUTF16("01");
+    else if (form.fields[i].name == ASCIIToUTF16("ccyear"))
+      form.fields[i].value = ASCIIToUTF16("2017");
+  }
+
+  AutofillManager::UnmaskResponse response;
+  response.should_store_pan = false;
+  response.cvc = ASCIIToUTF16("123");
+  autofill_manager_->OnUnmaskResponse(response);
+  autofill_manager_->OnDidGetRealPan(AutofillClient::SUCCESS,
+                                     "4012888888881881");
+  autofill_manager_->OnFormSubmitted(form);
 }
 
 }  // namespace autofill

components/autofill/core/browser/autofill_manager.h
@@ -420,6 +420,10 @@ class AutofillManager : public AutofillDownloadManager::Observer,
   // Time when we requested the last real pan
   base::Time real_pan_request_timestamp_;
 
+  // Masked copies of recently unmasked cards, to help avoid double-asking to
+  // save the card (in the prompt and in the infobar after submit).
+  std::vector<CreditCard> recently_unmasked_cards_;
+
   // SuggestionBackendID to ID mapping. We keep two maps to convert back and
   // forth. These should be used only by BackendIDToInt and IntToBackendID.
   // Note that the integers are not frontend IDs.
@@ -473,6 +477,7 @@ class AutofillManager : public AutofillDownloadManager::Observer,
                            AutocompleteOffRespected);
   FRIEND_TEST_ALL_PREFIXES(AutofillManagerTest,
                            AutocompleteOffRespectedWithFlag);
+  FRIEND_TEST_ALL_PREFIXES(AutofillManagerTest, DontOfferToSaveWalletCard);
   DISALLOW_COPY_AND_ASSIGN(AutofillManager);
 };
 

components/autofill/core/browser/personal_data_manager.cc
@@ -483,8 +483,9 @@ bool PersonalDataManager::ImportFormData(
   }
 
   // Also don't offer to save if we already have this stored as a full wallet
-  // card. (In particular this comes up just after filling and submitting a
-  // Wallet card.)
+  // card. Note that we will offer to save masked server cards, as long as
+  // the user re-typed the info by hand. See AutofillManager's
+  // |recently_unmasked_cards_|.
   if (local_imported_credit_card) {
     for (CreditCard* card : server_credit_cards_) {
       if (card->record_type() == CreditCard::FULL_SERVER_CARD &&

components/autofill/core/browser/personal_data_manager_unittest.cc
@@ -902,8 +902,8 @@ TEST_F(PersonalDataManagerTest, ImportFormMinimumAddressUSA) {
   FormStructure form_structure(form);
   form_structure.DetermineHeuristicTypes();
   scoped_ptr<CreditCard> imported_credit_card;
-  EXPECT_TRUE(personal_data_->ImportFormData(form_structure,
-                                              &imported_credit_card));
+  EXPECT_TRUE(
+      personal_data_->ImportFormData(form_structure, &imported_credit_card));
   const std::vector<AutofillProfile*>& profiles = personal_data_->GetProfiles();
   ASSERT_EQ(1U, profiles.size());
 }
@@ -3219,6 +3219,69 @@ TEST_F(PersonalDataManagerTest, ClearAllServerData) {
   ResetPersonalDataManager(USER_MODE_NORMAL);
   EXPECT_TRUE(personal_data_->GetCreditCards().empty());
   EXPECT_TRUE(personal_data_->GetProfiles().empty());
+}
+
+TEST_F(PersonalDataManagerTest, DontDuplicateServerCard) {
+  EnableWalletCardImport();
+
+  std::vector<CreditCard> server_cards;
+  server_cards.push_back(CreditCard(CreditCard::MASKED_SERVER_CARD, "a123"));
+  test::SetCreditCardInfo(&server_cards.back(), "John Dillinger",
+                          "1881" /* Visa */, "01", "2017");
+  server_cards.back().SetTypeForMaskedCard(kVisaCard);
+
+  server_cards.push_back(CreditCard(CreditCard::FULL_SERVER_CARD, "c789"));
+  test::SetCreditCardInfo(&server_cards.back(), "Clyde Barrow",
+                          "347666888555" /* American Express */, "04", "2015");
+
+  test::SetServerCreditCards(autofill_table_, server_cards);
+  personal_data_->Refresh();
+  EXPECT_CALL(personal_data_observer_, OnPersonalDataChanged())
+      .WillOnce(QuitMainMessageLoop());
+  base::MessageLoop::current()->Run();
+
+  // A valid credit card form. A user re-types one of their masked cards.
+  // We should offer to save.
+  FormData form1;
+  FormFieldData field;
+  test::CreateTestFormField("Name on card:", "name_on_card", "John Dillinger",
+                            "text", &field);
+  form1.fields.push_back(field);
+  test::CreateTestFormField("Card Number:", "card_number", "4012888888881881",
+                            "text", &field);
+  form1.fields.push_back(field);
+  test::CreateTestFormField("Exp Month:", "exp_month", "01", "text", &field);
+  form1.fields.push_back(field);
+  test::CreateTestFormField("Exp Year:", "exp_year", "2017", "text", &field);
+  form1.fields.push_back(field);
+
+  FormStructure form_structure1(form1);
+  form_structure1.DetermineHeuristicTypes();
+  scoped_ptr<CreditCard> imported_credit_card;
+  EXPECT_TRUE(
+      personal_data_->ImportFormData(form_structure1, &imported_credit_card));
+  EXPECT_TRUE(imported_credit_card);
+  imported_credit_card.reset();
+
+  // A user re-types (or fills with) an unmasked card. Don't offer to save
+  // again.
+  FormData form2;
+  test::CreateTestFormField("Name on card:", "name_on_card", "Clyde Barrow",
+                            "text", &field);
+  form2.fields.push_back(field);
+  test::CreateTestFormField("Card Number:", "card_number", "347666888555",
+                            "text", &field);
+  form2.fields.push_back(field);
+  test::CreateTestFormField("Exp Month:", "exp_month", "04", "text", &field);
+  form2.fields.push_back(field);
+  test::CreateTestFormField("Exp Year:", "exp_year", "2015", "text", &field);
+  form2.fields.push_back(field);
+
+  FormStructure form_structure2(form2);
+  form_structure2.DetermineHeuristicTypes();
+  EXPECT_FALSE(
+      personal_data_->ImportFormData(form_structure2, &imported_credit_card));
+  EXPECT_FALSE(imported_credit_card);
 }
 
 }  // namespace autofill

mirror/chromium

commit 9f46051a6dbf2599cd9689b7e0dfdbb247e94202

Fix crash in Autofill introduced in 8949a8b27dfb
and add a unit test to verify

BUG=483602

Review URL: https://codereview.chromium.org/1121883002

components/autofill/core/browser/autofill_manager.cc
@@ -821,20 +821,19 @@ void AutofillManager::ImportFormData(const FormStructure& submitted_form) {
   if (!personal_data_->ImportFormData(submitted_form, &imported_credit_card))
     return;
 
-  // Don't offer to save any cards that were recently unmasked.
-  if (recently_unmasked_cards_.end() !=
-      std::find_if(recently_unmasked_cards_.begin(),
-                   recently_unmasked_cards_.end(),
-                   [&imported_credit_card](const CreditCard& unmasked) -> bool {
-                     return unmasked.TypeAndLastFourDigits() ==
-                            imported_credit_card->TypeAndLastFourDigits();
-                   })) {
-    return;
-  }
-
   // If credit card information was submitted, we need to confirm whether to
   // save it.
   if (imported_credit_card) {
+    // Don't offer to save any cards that were recently unmasked.
+    if (recently_unmasked_cards_.end() !=
+        std::find_if(
+            recently_unmasked_cards_.begin(), recently_unmasked_cards_.end(),
+            [&imported_credit_card](const CreditCard& unmasked) -> bool {
+              return unmasked.TypeAndLastFourDigits() ==
+                     imported_credit_card->TypeAndLastFourDigits();
+            })) {
+      return;
+    }
     client_->ConfirmSaveCreditCard(
         base::Bind(
             base::IgnoreResult(&PersonalDataManager::SaveImportedCreditCard),

components/autofill/core/browser/autofill_manager_unittest.cc
@@ -3216,6 +3216,41 @@ TEST_F(AutofillManagerTest, DontOfferToSaveWalletCard) {
   autofill_manager_->OnDidGetRealPan(AutofillClient::SUCCESS,
                                      "4012888888881881");
   autofill_manager_->OnFormSubmitted(form);
+
+  // The rest of this test is a regression test for http://crbug.com/483602.
+  // The goal is not to crash.
+  EXPECT_CALL(*autofill_driver_, SendFormDataToRenderer(_, _, _));
+  for (size_t i = 0; i < form.fields.size(); ++i) {
+    form.fields[i].value.clear();
+  }
+  autofill_manager_->FillOrPreviewCreditCardForm(
+      AutofillDriver::FORM_DATA_ACTION_FILL, kDefaultPageID, form,
+      form.fields[0], card, 0);
+  autofill_manager_->OnUnmaskResponse(response);
+  autofill_manager_->OnDidGetRealPan(AutofillClient::SUCCESS,
+                                     "4012888888881881");
+
+  form = FormData();
+  test::CreateTestAddressFormData(&form);
+  forms[0] = form;
+  FormsSeen(forms);
+  for (size_t i = 0; i < form.fields.size(); ++i) {
+    if (form.fields[i].name == ASCIIToUTF16("firstname"))
+      form.fields[i].value = ASCIIToUTF16("Flo");
+    else if (form.fields[i].name == ASCIIToUTF16("lastname"))
+      form.fields[i].value = ASCIIToUTF16("Master");
+    else if (form.fields[i].name == ASCIIToUTF16("addr1"))
+      form.fields[i].value = ASCIIToUTF16("123 Maple");
+    else if (form.fields[i].name == ASCIIToUTF16("city"))
+      form.fields[i].value = ASCIIToUTF16("Dallas");
+    else if (form.fields[i].name == ASCIIToUTF16("state"))
+      form.fields[i].value = ASCIIToUTF16("Texas");
+    else if (form.fields[i].name == ASCIIToUTF16("zipcode"))
+      form.fields[i].value = ASCIIToUTF16("77401");
+    else if (form.fields[i].name == ASCIIToUTF16("country"))
+      form.fields[i].value = ASCIIToUTF16("US");
+  }
+  autofill_manager_->OnFormSubmitted(form);
 }
 
 }  // namespace autofill