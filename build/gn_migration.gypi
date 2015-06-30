crosswalk-project/chromium-crosswalk

commit 49f497d7d9c9c6c665abec0ddfbc0df07c3cd6c9

Convert aura_unittests to run exclusively on Swarming
0 configs already ran on Swarming
19 used to run locally and were converted:
- chromium.chromiumos.json: Linux ChromiumOS Ozone Tests (1)
- chromium.chromiumos.json: Linux ChromiumOS Tests (1)
- chromium.chromiumos.json: Linux ChromiumOS Tests (dbg)(1)
- chromium.fyi.json: Linux Trusty
- chromium.fyi.json: Linux Trusty (dbg)
- chromium.linux.json: Linux Tests
- chromium.linux.json: Linux Tests (dbg)(1)
- chromium.linux.json: Linux Tests (dbg)(1)(32)
- chromium.memory.fyi.json: Linux ChromeOS MSan Tests
- chromium.memory.fyi.json: Linux MSan Tests
- chromium.memory.fyi.json: Linux TSan Tests
- chromium.memory.json: Linux Chromium OS ASan LSan Tests (1)
- chromium.webkit.json: Linux ChromiumOS Tests (1)
- chromium.webkit.json: Linux ChromiumOS Tests (dbg)(1)
- chromium.win.json: Vista Tests (1)
- chromium.win.json: Win 7 Tests x64 (1)
- chromium.win.json: Win7 Tests (1)
- chromium.win.json: Win7 Tests (dbg)(1)
- chromium.win.json: XP Tests (1)

Ran:
  ./manage.py --convert aura_unittests

R=sky@chromium.org,dpranke@chromium.org
BUG=98637

Review URL: https://codereview.chromium.org/1213283002

Cr-Commit-Position: refs/heads/master@{#336789}

build/gn_migration.gypi
@@ -539,6 +539,7 @@
             '../third_party/cacheinvalidation/cacheinvalidation.gyp:cacheinvalidation_unittests_run',
             '../ui/accessibility/accessibility.gyp:accessibility_unittests_run',
             '../ui/app_list/app_list.gyp:app_list_unittests_run',
+            '../ui/aura/aura.gyp:aura_unittests_run',
             '../ui/events/events.gyp:events_unittests_run',
             '../ui/message_center/message_center.gyp:message_center_unittests_run',
             '../ui/touch_selection/ui_touch_selection.gyp:ui_touch_selection_unittests_run',

ui/aura/aura.gyp
@@ -314,4 +314,23 @@
       ],
     },
   ],
+  'conditions': [
+    ['test_isolation_mode != "noop"', {
+      'targets': [
+        {
+          'target_name': 'aura_unittests_run',
+          'type': 'none',
+          'dependencies': [
+            'aura_unittests',
+          ],
+          'includes': [
+            '../../build/isolate.gypi',
+          ],
+          'sources': [
+            'aura_unittests.isolate',
+          ],
+        },
+      ],
+    }],
+  ],
 }

ui/aura/aura_unittests.isolate
@@ -1,0 +1,70 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+{
+  'conditions': [
+    ['use_x11==0', {
+      'variables': {
+        'command': [
+          '../../testing/test_env.py',
+          '<(PRODUCT_DIR)/aura_unittests<(EXECUTABLE_SUFFIX)',
+          '--brave-new-test-launcher',
+          '--test-launcher-bot-mode',
+          '--asan=<(asan)',
+          '--msan=<(msan)',
+          '--tsan=<(tsan)',
+        ],
+      },
+    }],
+    ['use_x11==1', {
+      'variables': {
+        'command': [
+          '../../testing/xvfb.py',
+          '<(PRODUCT_DIR)',
+          '<(PRODUCT_DIR)/aura_unittests<(EXECUTABLE_SUFFIX)',
+          '--brave-new-test-launcher',
+          '--test-launcher-bot-mode',
+          '--asan=<(asan)',
+          '--msan=<(msan)',
+          '--tsan=<(tsan)',
+        ],
+        'files': [
+          '../../testing/xvfb.py',
+          '<(PRODUCT_DIR)/xdisplaycheck<(EXECUTABLE_SUFFIX)',
+        ],
+      },
+    }],
+    ['OS=="linux" or OS=="mac" or OS=="win"', {
+      'variables': {
+        'files': [
+          '../../testing/test_env.py',
+          '<(PRODUCT_DIR)/aura_unittests<(EXECUTABLE_SUFFIX)',
+        ],
+      },
+    }],
+    ['OS=="linux"', {
+      'variables': {
+        'files': [
+          '<(PRODUCT_DIR)/libosmesa.so',
+        ],
+      },
+    }],
+    ['OS=="mac"', {
+      'variables': {
+        'files': [
+          '<(PRODUCT_DIR)/osmesa.so',
+        ],
+      },
+    }],
+    ['OS=="win"', {
+      'variables': {
+        'files': [
+          '<(PRODUCT_DIR)/osmesa.dll',
+        ],
+      },
+    }],
+  ],
+  'includes': [
+    '../../base/base.isolate',
+  ],
+}

mirror/chromium

commit db549ebbd82cfbc0cbe13d661d1faeacf226467f

gn_migration.gypi: Do not pull aura_unittests_run unless use_aura==1.
This was a regression in 49f497d7d9c9c6c665abec0ddfbc0df07c3cd6c9 which caused
compilation failure when building everything on OSX.

TBR=dpranke@chromium.org
BUG=98637
NOTRY=true

Review URL: https://codereview.chromium.org/1220733009

build/gn_migration.gypi
@@ -540,10 +540,16 @@
             '../third_party/cacheinvalidation/cacheinvalidation.gyp:cacheinvalidation_unittests_run',
             '../ui/accessibility/accessibility.gyp:accessibility_unittests_run',
             '../ui/app_list/app_list.gyp:app_list_unittests_run',
-            '../ui/aura/aura.gyp:aura_unittests_run',
             '../ui/events/events.gyp:events_unittests_run',
             '../ui/message_center/message_center.gyp:message_center_unittests_run',
             '../ui/touch_selection/ui_touch_selection.gyp:ui_touch_selection_unittests_run',
+          ],
+          'conditions': [
+            ['use_aura==1', {
+              'dependencies': [
+                '../ui/aura/aura.gyp:aura_unittests_run',
+              ],
+            }],
           ],
         }],
         ['test_isolation_mode!="noop" and use_ash==1', {