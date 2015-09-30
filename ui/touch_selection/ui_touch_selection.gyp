crosswalk-project/chromium-crosswalk

commit 1bb48578ff6e27bbbd4c475a50eceb2c5af648a8

Add ui_resources dependency to ui_touch_selection on aura build
also changed to "include when necessary" instead of "include all then exclude when unnecessary".
I believe this is easier to read.

BUG=487955
R=mohsen@chromium.org

Review URL: https://codereview.chromium.org/1355383002

Cr-Commit-Position: refs/heads/master@{#350303}

ui/touch_selection/BUILD.gn
@@ -37,11 +37,11 @@ component("touch_selection") {
 
   if (use_aura) {
     deps += [
-      "//skia:skia",
       "//ui/aura:aura",
       "//ui/aura_extra:aura_extra",
       "//ui/compositor:compositor",
       "//ui/gfx:gfx",
+      "//ui/resources",
     ]
 
     sources += [

ui/touch_selection/ui_touch_selection.gyp
@@ -12,14 +12,9 @@
       'type': '<(component)',
       'dependencies': [
         '../../base/base.gyp:base',
-        '../../skia/skia.gyp:skia',
-        '../aura/aura.gyp:aura',
-        '../aura_extra/aura_extra.gyp:aura_extra',
         '../base/ui_base.gyp:ui_base',
-        '../compositor/compositor.gyp:compositor',
         '../events/events.gyp:events',
         '../events/events.gyp:gesture_detection',
-        '../gfx/gfx.gyp:gfx',
         '../gfx/gfx.gyp:gfx_geometry',
       ],
       'defines': [
@@ -31,29 +26,25 @@
         'selection_event_type.h',
         'touch_handle.cc',
         'touch_handle.h',
-        'touch_handle_drawable_aura.cc',
-        'touch_handle_drawable_aura.h',
         'touch_handle_orientation.h',
         'touch_selection_controller.cc',
         'touch_selection_controller.h',
         'touch_selection_draggable.h',
-        'touch_selection_menu_runner.cc',
-        'touch_selection_menu_runner.h',
         'ui_touch_selection_export.h',
       ],
       'include_dirs': [
         '../..',
       ],
       'conditions': [
-        ['use_aura==0', {
-          'dependencies!': [
-            '../../skia/skia.gyp:skia',
+        ['use_aura==1', {
+          'dependencies': [
             '../aura/aura.gyp:aura',
             '../aura_extra/aura_extra.gyp:aura_extra',
             '../compositor/compositor.gyp:compositor',
             '../gfx/gfx.gyp:gfx',
+            '../resources/ui_resources.gyp:ui_resources',
           ],
-          'sources!': [
+          'sources': [
             'touch_handle_drawable_aura.cc',
             'touch_handle_drawable_aura.h',
             'touch_selection_menu_runner.cc',

mirror/chromium

commit 28c29160c94f288447c85d88c4a362d1041a6b39

Add skia dependency back into ui_touch_selection
The dependency was removed in 1bb48578ff which caused an error when
building with shared libraries.

BUG=None

Review URL: https://codereview.chromium.org/1360163002

ui/touch_selection/BUILD.gn
@@ -37,6 +37,7 @@ component("touch_selection") {
 
   if (use_aura) {
     deps += [
+      "//skia:skia",
       "//ui/aura:aura",
       "//ui/aura_extra:aura_extra",
       "//ui/compositor:compositor",

ui/touch_selection/ui_touch_selection.gyp
@@ -12,6 +12,7 @@
       'type': '<(component)',
       'dependencies': [
         '../../base/base.gyp:base',
+        '../../skia/skia.gyp:skia',
         '../base/ui_base.gyp:ui_base',
         '../events/events.gyp:events',
         '../events/events.gyp:gesture_detection',