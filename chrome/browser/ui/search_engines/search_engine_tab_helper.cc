crosswalk-project/chromium-crosswalk

commit 230a6cc1034e196f24425502c50cd3d395995f2e

Fix broken search engine detection
BUG=540854

Review URL: https://codereview.chromium.org/1390333002

Cr-Commit-Position: refs/heads/master@{#352970}

chrome/browser/ui/search_engines/search_engine_tab_helper.cc
@@ -87,7 +87,9 @@ void SearchEngineTabHelper::DidNavigateMainFrame(
   GenerateKeywordIfNecessary(params);
 }
 
-bool SearchEngineTabHelper::OnMessageReceived(const IPC::Message& message) {
+bool SearchEngineTabHelper::OnMessageReceived(
+    const IPC::Message& message,
+    content::RenderFrameHost* render_frame_host) {
   bool handled = true;
   IPC_BEGIN_MESSAGE_MAP(SearchEngineTabHelper, message)
     IPC_MESSAGE_HANDLER(ChromeViewHostMsg_PageHasOSDD, OnPageHasOSDD)

chrome/browser/ui/search_engines/search_engine_tab_helper.h
@@ -30,7 +30,8 @@ class SearchEngineTabHelper
   void DidNavigateMainFrame(
       const content::LoadCommittedDetails& details,
       const content::FrameNavigateParams& params) override;
-  bool OnMessageReceived(const IPC::Message& message) override;
+  bool OnMessageReceived(const IPC::Message& message,
+                         content::RenderFrameHost* rfh) override;
 
  private:
   explicit SearchEngineTabHelper(content::WebContents* web_contents);

mirror/chromium

commit f0479fb4e9198cfdb51a8f79f7ba9ec60bcb881a

Double merge:
1 - Fix broken search engine detection

BUG=540854

Review URL: https://codereview.chromium.org/1390333002

Cr-Commit-Position: refs/heads/master@{#352970}
(cherry picked from commit 230a6cc1034e196f24425502c50cd3d395995f2e)

2 - Fix explicit search engine adding (window.external.AddSearchProvider)

This was broken by 230a6cc1034e196 --- we actually need both
OnMessageReceived functions because the AddSearchProvider IPC is still
the RenderView-specific version (only <link> was updated to the
RenderFrame-specific IPC).

BUG=540854

Review URL: https://codereview.chromium.org/1396903002

Cr-Commit-Position: refs/heads/master@{#353142}
(cherry picked from commit 959263e7afa929c869381ed5d8e88ef25ae5fbae)

Review URL: https://codereview.chromium.org/1396013003 .

chrome/browser/ui/search_engines/search_engine_tab_helper.cc
@@ -97,6 +97,12 @@ bool SearchEngineTabHelper::OnMessageReceived(const IPC::Message& message) {
   return handled;
 }
 
+bool SearchEngineTabHelper::OnMessageReceived(
+    const IPC::Message& message,
+    content::RenderFrameHost* render_frame_host) {
+  return OnMessageReceived(message);
+}
+
 SearchEngineTabHelper::SearchEngineTabHelper(WebContents* web_contents)
     : content::WebContentsObserver(web_contents),
       delegate_(nullptr),

chrome/browser/ui/search_engines/search_engine_tab_helper.h
@@ -31,6 +31,8 @@ class SearchEngineTabHelper
       const content::LoadCommittedDetails& details,
       const content::FrameNavigateParams& params) override;
   bool OnMessageReceived(const IPC::Message& message) override;
+  bool OnMessageReceived(const IPC::Message& message,
+                         content::RenderFrameHost* rfh) override;
 
  private:
   explicit SearchEngineTabHelper(content::WebContents* web_contents);