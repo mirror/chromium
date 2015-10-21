crosswalk-project/chromium-crosswalk

commit 6a9ad501c3588013cb435b64f6d3525a061a8f09

Support M permissions in WebViewShell's browser Activity
The Shell's policy is to grant as many permissions as possible. If the
page requests any Webkit permissions, we will request all necessary
Android permissions to provide that functionality. If we load a file://
URL, we will request read permissions so we can read the file.

BUG=537819

Review URL: https://codereview.chromium.org/1403763005

Cr-Commit-Position: refs/heads/master@{#355093}

android_webview/tools/WebViewShell/src/org/chromium/webview_shell/WebViewBrowserActivity.java
@@ -4,14 +4,18 @@
 
 package org.chromium.webview_shell;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.SparseArray;
 
 import android.view.KeyEvent;
 import android.view.MenuItem;
@@ -31,12 +35,16 @@ import android.widget.LinearLayout.LayoutParams;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
+import org.chromium.base.Log;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -46,18 +54,99 @@ import java.util.regex.Pattern;
  * on top of the webview for manually specifying URLs to load.
  */
 public class WebViewBrowserActivity extends Activity implements PopupMenu.OnMenuItemClickListener {
-    private EditText mUrlBar;
-    private WebView mWebView;
-    private String mWebViewVersion;
+    private static final String TAG = "WebViewShell";
+
+    // Our imaginary Android permission to associate with the WebKit geo permission
+    private static final String RESOURCE_GEO = "RESOURCE_GEO";
+    // Our imaginary WebKit permission to request when loading a file:// URL
+    private static final String RESOURCE_FILE_URL = "RESOURCE_FILE_URL";
+    // WebKit permissions with no corresponding Android permission can always be granted
+    private static final String NO_ANDROID_PERMISSION = "NO_ANDROID_PERMISSION";
+
+    // Map from WebKit permissions to Android permissions
+    private static final HashMap<String, String> sPermissions;
+    static {
+        sPermissions = new HashMap<String, String>();
+        sPermissions.put(RESOURCE_GEO, Manifest.permission.ACCESS_FINE_LOCATION);
+        sPermissions.put(RESOURCE_FILE_URL, Manifest.permission.READ_EXTERNAL_STORAGE);
+        sPermissions.put(PermissionRequest.RESOURCE_AUDIO_CAPTURE,
+                Manifest.permission.RECORD_AUDIO);
+        sPermissions.put(PermissionRequest.RESOURCE_MIDI_SYSEX, NO_ANDROID_PERMISSION);
+        sPermissions.put(PermissionRequest.RESOURCE_PROTECTED_MEDIA_ID, NO_ANDROID_PERMISSION);
+        sPermissions.put(PermissionRequest.RESOURCE_VIDEO_CAPTURE,
+                Manifest.permission.CAMERA);
+    }
 
     private static final Pattern WEBVIEW_VERSION_PATTERN =
             Pattern.compile("(Chrome/)([\\d\\.]+)\\s");
 
-    // TODO(michaelbai) : Replace "android.webkit.resource.MIDI_SYSEX" with
-    // PermissionRequest.RESOURCE_MIDI_SYSEX once Android M SDK is used.
-    private static final String[] AUTOMATICALLY_GRANT =
-            { PermissionRequest.RESOURCE_VIDEO_CAPTURE, PermissionRequest.RESOURCE_AUDIO_CAPTURE,
-              "android.webkit.resource.MIDI_SYSEX" };
+    private EditText mUrlBar;
+    private WebView mWebView;
+    private String mWebViewVersion;
+
+    // Each time we make a request, store it here with an int key. onRequestPermissionsResult will
+    // look up the request in order to grant the approprate permissions.
+    private SparseArray<PermissionRequest> mPendingRequests = new SparseArray<PermissionRequest>();
+    private int mNextRequestKey = 0;
+
+    // Work around our wonky API by wrapping a geo permission prompt inside a regular
+    // PermissionRequest.
+    private static class GeoPermissionRequest extends PermissionRequest {
+        private String mOrigin;
+        private GeolocationPermissions.Callback mCallback;
+
+        public GeoPermissionRequest(String origin, GeolocationPermissions.Callback callback) {
+            mOrigin = origin;
+            mCallback = callback;
+        }
+
+        public Uri getOrigin() {
+            return Uri.parse(mOrigin);
+        }
+
+        public String[] getResources() {
+            return new String[] { WebViewBrowserActivity.RESOURCE_GEO };
+        }
+
+        public void grant(String[] resources) {
+            assert resources.length == 1;
+            assert WebViewBrowserActivity.RESOURCE_GEO.equals(resources[0]);
+            mCallback.invoke(mOrigin, true, false);
+        }
+
+        public void deny() {
+            mCallback.invoke(mOrigin, false, false);
+        }
+    }
+
+    // For simplicity, also treat the read access needed for file:// URLs as a regular
+    // PermissionRequest.
+    private class FilePermissionRequest extends PermissionRequest {
+        private String mOrigin;
+
+        public FilePermissionRequest(String origin) {
+            mOrigin = origin;
+        }
+
+        public Uri getOrigin() {
+            return Uri.parse(mOrigin);
+        }
+
+        public String[] getResources() {
+            return new String[] { WebViewBrowserActivity.RESOURCE_FILE_URL };
+        }
+
+        public void grant(String[] resources) {
+            assert resources.length == 1;
+            assert WebViewBrowserActivity.RESOURCE_FILE_URL.equals(resources[0]);
+            // Try again now that we have read access.
+            WebViewBrowserActivity.this.mWebView.loadUrl(mOrigin);
+        }
+
+        public void deny() {
+            // womp womp
+        }
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -101,12 +190,12 @@ public class WebViewBrowserActivity extends Activity implements PopupMenu.OnMenu
             @Override
             public void onGeolocationPermissionsShowPrompt(String origin,
                     GeolocationPermissions.Callback callback) {
-                callback.invoke(origin, true, false);
+                onPermissionRequest(new GeoPermissionRequest(origin, callback));
             }
 
             @Override
             public void onPermissionRequest(PermissionRequest request) {
-                request.grant(AUTOMATICALLY_GRANT);
+                WebViewBrowserActivity.this.requestPermissionsForPage(request);
             }
         });
 
@@ -127,6 +216,72 @@ public class WebViewBrowserActivity extends Activity implements PopupMenu.OnMenu
             setUrlFail(false);
             loadUrlFromUrlBar(mUrlBar);
         }
+    }
+
+    // WebKit permissions which can be granted because either they have no associated Android
+    // permission or the associated Android permission has been granted
+    private boolean canGrant(String webkitPermission) {
+        String androidPermission = sPermissions.get(webkitPermission);
+        if (androidPermission == NO_ANDROID_PERMISSION) {
+            return true;
+        }
+        return PackageManager.PERMISSION_GRANTED == checkSelfPermission(androidPermission);
+    }
+
+    private void requestPermissionsForPage(PermissionRequest request) {
+        // Deny any unrecognized permissions.
+        for (String webkitPermission : request.getResources()) {
+            if (!sPermissions.containsKey(webkitPermission)) {
+                Log.w(TAG, "Unrecognized WebKit permission: " + webkitPermission);
+                request.deny();
+                return;
+            }
+        }
+
+        // Find what Android permissions we need before we can grant these WebKit permissions.
+        ArrayList<String> androidPermissionsNeeded = new ArrayList<String>();
+        for (String webkitPermission : request.getResources()) {
+            if (!canGrant(webkitPermission)) {
+                // We already checked for unrecognized permissions, and canGrant will skip over
+                // NO_ANDROID_PERMISSION cases, so this is guaranteed to be a regular Android
+                // permission.
+                String androidPermission = sPermissions.get(webkitPermission);
+                androidPermissionsNeeded.add(androidPermission);
+            }
+        }
+
+        // If there are no such Android permissions, grant the WebKit permissions immediately.
+        if (androidPermissionsNeeded.isEmpty()) {
+            request.grant(request.getResources());
+            return;
+        }
+
+        // Otherwise, file a new request
+        if (mNextRequestKey == Integer.MAX_VALUE) {
+            Log.e(TAG, "Too many permission requests");
+            return;
+        }
+        int requestCode = mNextRequestKey;
+        mNextRequestKey++;
+        mPendingRequests.append(requestCode, request);
+        requestPermissions(androidPermissionsNeeded.toArray(new String[0]), requestCode);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+            String permissions[], int[] grantResults) {
+        // Verify that we can now grant all the requested permissions. Note that although grant()
+        // takes a list of permissions, grant() is actually all-or-nothing. If there are any
+        // requested permissions not included in the granted permissions, all will be denied.
+        PermissionRequest request = mPendingRequests.get(requestCode);
+        for (String webkitPermission : request.getResources()) {
+            if (!canGrant(webkitPermission)) {
+                request.deny();
+                return;
+            }
+        }
+        request.grant(request.getResources());
+        mPendingRequests.delete(requestCode);
     }
 
     public void loadUrlFromUrlBar(View view) {
@@ -210,6 +365,17 @@ public class WebViewBrowserActivity extends Activity implements PopupMenu.OnMenu
     }
 
     private void loadUrl(String url) {
+        // Request read access if necessary
+        if ("file".equals(Uri.parse(url).getScheme())) {
+            if (PackageManager.PERMISSION_DENIED
+                    == checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                requestPermissionsForPage(new FilePermissionRequest(url));
+            }
+        }
+
+        // If it is file:// and we don't have permission, they'll get the "Webpage not available"
+        // "net::ERR_ACCESS_DENIED" page. When we get permission, FilePermissionRequest.grant()
+        // will reload.
         mWebView.loadUrl(url);
         mWebView.requestFocus();
     }

android_webview/tools/WebViewShell/AndroidManifest.xml
@@ -12,16 +12,19 @@
 
     <uses-sdk android:minSdkVersion="19" android:targetSdkVersion="23" />
 
-    <uses-permission android:name="android.permission.INTERNET" />
-    <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
-    <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
-    <uses-permission android:name="android.permission.CAMERA"/>
-    <uses-permission android:name="android.permission.RECORD_AUDIO"/>
-    <uses-permission android:name="android.permission.MODIFY_AUDIO_SETTINGS"/>
-    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
-    <uses-permission android:name="android.permission.RUN_INSTRUMENTATION" />
+    <!-- "Normal" permissions which do not require user prompt -->
     <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
     <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
+    <uses-permission android:name="android.permission.INTERNET" />
+    <uses-permission android:name="android.permission.MODIFY_AUDIO_SETTINGS"/>
+
+    <!-- "Dangerous" permissions which require user prompt -->
+    <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION"/>
+    <uses-permission android:name="android.permission.CAMERA"/>
+    <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
+    <uses-permission android:name="android.permission.RECORD_AUDIO"/>
+    <uses-permission android:name="android.permission.RUN_INSTRUMENTATION" />
+    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
 
     <application
         android:icon="@drawable/ic_launcher"

mirror/chromium

commit e29ad7785aff6c488e9b86ae1eba42c6d8971f24

WebViewShell crashes on Android <= L. Fix that.
caused by 6a9ad501c3588013cb435b64f6d3525a061a8f09

BUG=546122

Review URL: https://codereview.chromium.org/1420833002

android_webview/tools/WebViewShell/src/org/chromium/webview_shell/WebViewBrowserActivity.java
@@ -238,6 +238,11 @@ public class WebViewBrowserActivity extends Activity implements PopupMenu.OnMenu
             }
         }
 
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            request.grant(request.getResources());
+            return;
+        }
+
         // Find what Android permissions we need before we can grant these WebKit permissions.
         ArrayList<String> androidPermissionsNeeded = new ArrayList<String>();
         for (String webkitPermission : request.getResources()) {
@@ -366,11 +371,11 @@ public class WebViewBrowserActivity extends Activity implements PopupMenu.OnMenu
 
     private void loadUrl(String url) {
         // Request read access if necessary
-        if ("file".equals(Uri.parse(url).getScheme())) {
-            if (PackageManager.PERMISSION_DENIED
-                    == checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
-                requestPermissionsForPage(new FilePermissionRequest(url));
-            }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && "file".equals(Uri.parse(url).getScheme())
+                && PackageManager.PERMISSION_DENIED
+                        == checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            requestPermissionsForPage(new FilePermissionRequest(url));
         }
 
         // If it is file:// and we don't have permission, they'll get the "Webpage not available"