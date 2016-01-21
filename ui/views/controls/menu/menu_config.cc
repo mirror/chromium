crosswalk-project/chromium-crosswalk

commit b054c7cf2a61f43437fa5965e908cf2e61109aa0

Vectorize checkmark and arrow images in menus.
The vectorized icons look the same as the current raster icons, so the change isn't restricted to MD. The arrow should flip in RTL but the check should not.

BUG=561133

Review URL: https://codereview.chromium.org/1496373003

Cr-Commit-Position: refs/heads/master@{#366499}

ui/views/controls/menu/menu_config.cc
@@ -5,6 +5,7 @@
 #include "ui/views/controls/menu/menu_config.h"
 
 #include "base/macros.h"
+#include "ui/views/controls/menu/menu_image_util.h"
 
 namespace views {
 
@@ -21,10 +22,10 @@ MenuConfig::MenuConfig()
       label_to_arrow_padding(10),
       arrow_to_edge_padding(5),
       icon_to_label_padding(10),
-      check_width(16),
-      check_height(16),
+      check_width(kMenuCheckSize),
+      check_height(kMenuCheckSize),
       radio_width(16),
-      arrow_width(9),
+      arrow_width(kSubmenuArrowSize),
       separator_height(11),
       separator_upper_height(3),
       separator_lower_height(4),

ui/views/controls/menu/menu_config.h
@@ -56,14 +56,17 @@ struct VIEWS_EXPORT MenuConfig {
   // Padding between the icon and label.
   int icon_to_label_padding;
 
-  // Size of the check.
+  // The space reserved for the check. The actual size of the image may be
+  // different.
   int check_width;
   int check_height;
 
-  // Width of the radio bullet.
+  // The horizontal space reserved for radio button. The actual width of the
+  // image may be different.
   int radio_width;
 
-  // Width of the submenu arrow.
+  // The horizontal space reserved for submenu arrow. The actual width of the
+  // image may be different.
   int arrow_width;
 
   // Height of a normal separator (ui::NORMAL_SEPARATOR).

ui/gfx/BUILD.gn
@@ -486,6 +486,8 @@ action("aggregate_vector_icons") {
     "vector_icons/location_bar_star.icon",
     "vector_icons/location_bar_star_active.icon",
     "vector_icons/location_on.icon",
+    "vector_icons/menu_check.1x.icon",
+    "vector_icons/menu_check.icon",
     "vector_icons/midi.icon",
     "vector_icons/mixed_content.icon",
     "vector_icons/mode_edit.icon",
@@ -519,6 +521,8 @@ action("aggregate_vector_icons") {
     "vector_icons/remove_circle.icon",
     "vector_icons/sad_tab.icon",
     "vector_icons/smartphone.icon",
+    "vector_icons/submenu_arrow.1x.icon",
+    "vector_icons/submenu_arrow.icon",
     "vector_icons/supervisor_account.icon",
     "vector_icons/tab.icon",
     "vector_icons/tab_audio.1x.icon",

ui/gfx/gfx.gyp
@@ -469,6 +469,7 @@
       'type': '<(component)',
       'dependencies': [
         '<(DEPTH)/base/base.gyp:base',
+        '<(DEPTH)/base/base.gyp:base_i18n',
         '<(DEPTH)/skia/skia.gyp:skia',
         'gfx',
         'gfx_geometry',

ui/gfx/paint_vector_icon.cc
@@ -7,6 +7,7 @@
 #include <map>
 #include <tuple>
 
+#include "base/i18n/rtl.h"
 #include "base/lazy_instance.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
@@ -49,6 +50,7 @@ CommandType CommandFromString(const std::string& source) {
   RETURN_IF_IS(CANVAS_DIMENSIONS);
   RETURN_IF_IS(CLIP);
   RETURN_IF_IS(DISABLE_AA);
+  RETURN_IF_IS(FLIPS_IN_RTL);
   RETURN_IF_IS(END);
 #undef RETURN_IF_IS
 
@@ -82,6 +84,7 @@ void PaintPath(Canvas* canvas,
   std::vector<SkPath> paths;
   std::vector<SkPaint> paints;
   SkRect clip_rect = SkRect::MakeEmpty();
+  bool flips_in_rtl = false;
 
   for (size_t i = 0; path_elements[i].type != END; i++) {
     if (paths.empty() || path_elements[i].type == NEW_PATH) {
@@ -248,10 +251,20 @@ void PaintPath(Canvas* canvas,
         break;
       }
 
+      case FLIPS_IN_RTL: {
+        flips_in_rtl = true;
+        break;
+      }
+
       case END:
         NOTREACHED();
         break;
     }
+  }
+
+  if (flips_in_rtl && base::i18n::IsRTL()) {
+    canvas->Scale(-1, 1);
+    canvas->Translate(gfx::Vector2d(-static_cast<int>(canvas_size), 0));
   }
 
   if (dip_size != canvas_size) {

ui/gfx/vector_icon_types.h
@@ -52,6 +52,9 @@ enum CommandType {
   CLIP,
   // Disables anti-aliasing for this path.
   DISABLE_AA,
+  // Flips the x-axis in RTL locales. Default is false, this command sets it to
+  // true.
+  FLIPS_IN_RTL,
   // Marks the end of the list of commands.
   END
 };

ui/gfx/vector_icons/menu_check.1x.icon
@@ -1,0 +1,14 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+CANVAS_DIMENSIONS, 16,
+MOVE_TO, 2, 9.22f,
+R_LINE_TO, 1.36f, -1.38f,
+R_LINE_TO, 2.4f, 2.48f,
+R_LINE_TO, 7.2f, -7.4f,
+R_LINE_TO, 1.35f, 1.39f,
+R_LINE_TO, -8.55f, 8.78f,
+LINE_TO, 2, 9.22f,
+CLOSE,
+END

ui/gfx/vector_icons/menu_check.icon
@@ -1,0 +1,14 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+CANVAS_DIMENSIONS, 32,
+MOVE_TO, 5, 18.78f,
+R_LINE_TO, 2.53f, -2.57f,
+R_LINE_TO, 4.48f, 4.63f,
+LINE_TO, 25.48f, 7,
+LINE_TO, 28, 9.59f,
+LINE_TO, 12.02f, 26,
+LINE_TO, 5, 18.78f,
+CLOSE,
+END

mirror/chromium

commit 5a10bc5a59aec529534022a4930858a4a6d83f29

Fix size of windows menu controls in HiDPI.
This regressed in b054c7cf2a because with the removal of the raster assets, Windows began falling back to system metrics, which report in px rather than dp. This patch restores the effective hardcoding of 16dp.

BUG=577320
TEST=app menu items and control icons (arrow, etc.) are spaced properly at 2x scale factor

Review URL: https://codereview.chromium.org/1585893002

ui/views/controls/menu/menu_config.cc
@@ -24,7 +24,6 @@ MenuConfig::MenuConfig()
       icon_to_label_padding(10),
       check_width(kMenuCheckSize),
       check_height(kMenuCheckSize),
-      radio_width(16),
       arrow_width(kSubmenuArrowSize),
       separator_height(11),
       separator_upper_height(3),

ui/views/controls/menu/menu_config.h
@@ -61,10 +61,6 @@ struct VIEWS_EXPORT MenuConfig {
   int check_width;
   int check_height;
 
-  // The horizontal space reserved for radio button. The actual width of the
-  // image may be different.
-  int radio_width;
-
   // The horizontal space reserved for submenu arrow. The actual width of the
   // image may be different.
   int arrow_width;

ui/views/controls/menu/menu_config_win.cc
@@ -32,26 +32,6 @@ void MenuConfig::Init() {
     font_list = gfx::FontList(gfx::Font(new_font.get()));
   }
   NativeTheme::ExtraParams extra;
-  extra.menu_check.is_radio = false;
-  extra.menu_check.is_selected = false;
-  gfx::Size check_size = NativeThemeWin::instance()->GetPartSize(
-      NativeTheme::kMenuCheck, NativeTheme::kNormal, extra);
-  if (!check_size.IsEmpty()) {
-    check_width = check_size.width();
-    check_height = check_size.height();
-  } else {
-    check_width = GetSystemMetrics(SM_CXMENUCHECK);
-    check_height = GetSystemMetrics(SM_CYMENUCHECK);
-  }
-
-  extra.menu_check.is_radio = true;
-  gfx::Size radio_size = NativeThemeWin::instance()->GetPartSize(
-      NativeTheme::kMenuCheck, NativeTheme::kNormal, extra);
-  if (!radio_size.IsEmpty())
-    radio_width = radio_size.width();
-  else
-    radio_width = GetSystemMetrics(SM_CXMENUCHECK);
-
   gfx::Size arrow_size = NativeThemeWin::instance()->GetPartSize(
       NativeTheme::kMenuPopupArrow, NativeTheme::kNormal, extra);
   if (!arrow_size.IsEmpty()) {