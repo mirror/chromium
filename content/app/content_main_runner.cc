crosswalk-project/chromium-crosswalk

commit eb199fbaecd8e5d294010dfbe8897b7d83aeab07

Bootstrap Mojo IPC independent of Chrome IPC
This initializes Mojo IPC very early in child process
startup so we can start using cross-process pipes in
content without a dependency on Chrome IPC.

On all supported platforms, a new OS pipe handle is
passed through ChildProcessLauncher and hooked up to
the EDK on ChildThreadImpl startup to establish a
Mojo IPC channel to the parent.

A command-line argument is also to used to share a
unique pipe token with the child, and this token is
exchanged over the Mojo IPC channel to bootstrap a
primordial message pipe.

While this CL establishes these facilities in a way
that can be used for all child processes, only
render processes are brought up in this way for now.

BUG=583557
TEST=Chrome still runs in mash with Mus-in-renderer support.

Review URL: https://codereview.chromium.org/1685183004

Cr-Commit-Position: refs/heads/master@{#378272}

content/app/content_main_runner.cc
@@ -466,6 +466,8 @@ class ContentMainRunnerImpl : public ContentMainRunner {
     SetupSignalHandlers();
     g_fds->Set(kPrimaryIPCChannel,
                kPrimaryIPCChannel + base::GlobalDescriptors::kBaseDescriptor);
+    g_fds->Set(kMojoIPCChannel,
+               kMojoIPCChannel + base::GlobalDescriptors::kBaseDescriptor);
 #endif  // !OS_ANDROID && !OS_IOS
 
 #if defined(OS_LINUX) || defined(OS_OPENBSD)

mirror/chromium

commit 8d54ab31fa2261fdbcd53f58e518495b03dcaaa6

Reland: Bootstrap Mojo IPC independent of Chrome IPC
The original CL:
https://crrev.com/eb199fbaecd8e5d294010dfbe8897b7d83aeab07

broke NaCL integration tests on some bots. After some
extensive investigation this is somehow tied to an
obscure dependency on the value of kIPCDescriptorMax.
Moving kMojoIPCChannel to content_descriptors.h allows
us to avoid changing that value and thus avoid the
mysterious breakage. :|

Original CL description follows:

This initializes Mojo IPC very early in child process
startup so we can start using cross-process pipes in
content without a dependency on Chrome IPC.

On all supported platforms, a new OS pipe handle is
passed through ChildProcessLauncher and hooked up to
the EDK on ChildThreadImpl startup to establish a
Mojo IPC channel to the parent.

A command-line argument is also to used to share a
unique pipe token with the child, and this token is
exchanged over the Mojo IPC channel to bootstrap a
primordial message pipe.

While this CL establishes these facilities in a way
that can be used for all child processes, only
render processes are brought up in this way for now.

BUG=583557
TEST=Chrome still runs in mash with Mus-in-renderer support.
TBR=ben@chromium.org,jschuh@chromium.org

Review URL: https://codereview.chromium.org/1747053003

content/app/content_main_runner.cc
@@ -466,6 +466,8 @@ class ContentMainRunnerImpl : public ContentMainRunner {
     SetupSignalHandlers();
     g_fds->Set(kPrimaryIPCChannel,
                kPrimaryIPCChannel + base::GlobalDescriptors::kBaseDescriptor);
+    g_fds->Set(kMojoIPCChannel,
+               kMojoIPCChannel + base::GlobalDescriptors::kBaseDescriptor);
 #endif  // !OS_ANDROID && !OS_IOS
 
 #if defined(OS_LINUX) || defined(OS_OPENBSD)

content/browser/child_process_launcher.cc
@@ -24,9 +24,13 @@
 #include "content/public/common/content_switches.h"
 #include "content/public/common/result_codes.h"
 #include "content/public/common/sandboxed_process_launcher_delegate.h"
+#include "mojo/edk/embedder/embedder.h"
+#include "mojo/edk/embedder/scoped_platform_handle.h"
 
 #if defined(OS_WIN)
 #include "base/files/file_path.h"
+#include "base/win/scoped_handle.h"
+#include "base/win/win_util.h"
 #include "content/common/sandbox_win.h"
 #include "content/public/common/sandbox_init.h"
 #elif defined(OS_MACOSX)
@@ -59,6 +63,7 @@ namespace {
 typedef base::Callback<void(ZygoteHandle,
 #if defined(OS_ANDROID)
                             base::ScopedFD,
+                            base::ScopedFD,
 #endif
                             base::Process)> NotifyCallback;
 
@@ -82,6 +87,7 @@ void OnChildProcessStartedAndroid(const NotifyCallback& callback,
                                   BrowserThread::ID client_thread_id,
                                   const base::TimeTicks begin_launch_time,
                                   base::ScopedFD ipcfd,
+                                  base::ScopedFD mojo_fd,
                                   base::ProcessHandle handle) {
   // This can be called on the launcher thread or UI thread.
   base::TimeDelta launch_time = base::TimeTicks::Now() - begin_launch_time;
@@ -91,7 +97,7 @@ void OnChildProcessStartedAndroid(const NotifyCallback& callback,
 
   base::Closure callback_on_client_thread(
       base::Bind(callback, nullptr, base::Passed(&ipcfd),
-                 base::Passed(base::Process(handle))));
+                 base::Passed(&mojo_fd), base::Passed(base::Process(handle))));
   if (BrowserThread::CurrentlyOn(client_thread_id)) {
     callback_on_client_thread.Run();
   } else {
@@ -108,6 +114,7 @@ void LaunchOnLauncherThread(const NotifyCallback& callback,
 #if defined(OS_ANDROID)
                             base::ScopedFD ipcfd,
 #endif
+                            mojo::edk::ScopedPlatformHandle client_handle,
                             base::CommandLine* cmd_line) {
   DCHECK_CURRENTLY_ON(BrowserThread::PROCESS_LAUNCHER);
   scoped_ptr<SandboxedProcessLauncherDelegate> delegate_deleter(delegate);
@@ -129,12 +136,19 @@ void LaunchOnLauncherThread(const NotifyCallback& callback,
   base::Process process;
 #if defined(OS_WIN)
   if (launch_elevated) {
+    // TODO(rockot): We may want to support Mojo IPC to elevated processes as
+    // well, but this isn't currently feasible without sharing a pipe path on
+    // the command line as elevated process launch goes through ShellExecuteEx.
     base::LaunchOptions options;
     options.start_hidden = true;
     process = base::LaunchElevatedProcess(*cmd_line, options);
   } else {
-    process = StartSandboxedProcess(
-        delegate, cmd_line, base::HandlesToInheritVector());
+    base::HandlesToInheritVector handles;
+    handles.push_back(client_handle.get().handle);
+    cmd_line->AppendSwitchASCII(
+        mojo::edk::PlatformChannelPair::kMojoPlatformChannelHandleSwitch,
+        base::UintToString(base::win::HandleToUint32(handles[0])));
+    process = StartSandboxedProcess(delegate, cmd_line, handles);
   }
 #elif defined(OS_POSIX)
   std::string process_type =
@@ -142,10 +156,15 @@ void LaunchOnLauncherThread(const NotifyCallback& callback,
   scoped_ptr<FileDescriptorInfo> files_to_register(
       FileDescriptorInfoImpl::Create());
 
+  base::ScopedFD mojo_fd(client_handle.release().handle);
+  DCHECK(mojo_fd.is_valid());
+
 #if defined(OS_ANDROID)
   files_to_register->Share(kPrimaryIPCChannel, ipcfd.get());
+  files_to_register->Share(kMojoIPCChannel, mojo_fd.get());
 #else
   files_to_register->Transfer(kPrimaryIPCChannel, std::move(ipcfd));
+  files_to_register->Transfer(kMojoIPCChannel, std::move(mojo_fd));
 #endif
 #endif
 
@@ -223,7 +242,8 @@ void LaunchOnLauncherThread(const NotifyCallback& callback,
   StartChildProcess(
       cmd_line->argv(), child_process_id, std::move(files_to_register), regions,
       base::Bind(&OnChildProcessStartedAndroid, callback, client_thread_id,
-                 begin_launch_time, base::Passed(&ipcfd)));
+                 begin_launch_time, base::Passed(&ipcfd),
+                 base::Passed(&mojo_fd)));
 
 #elif defined(OS_POSIX)
   // We need to close the client end of the IPC channel to reliably detect
@@ -423,6 +443,8 @@ void ChildProcessLauncher::Launch(
   NotifyCallback reply_callback(base::Bind(&ChildProcessLauncher::DidLaunch,
                                            weak_factory_.GetWeakPtr(),
                                            terminate_child_on_shutdown_));
+  mojo::edk::ScopedPlatformHandle client_handle =
+      mojo_platform_channel_.PassClientHandle();
   BrowserThread::PostTask(
       BrowserThread::PROCESS_LAUNCHER, FROM_HERE,
       base::Bind(&LaunchOnLauncherThread, reply_callback, client_thread_id_,
@@ -430,7 +452,7 @@ void ChildProcessLauncher::Launch(
 #if defined(OS_ANDROID)
                  base::Passed(&ipcfd),
 #endif
-                 cmd_line));
+                 base::Passed(&client_handle), cmd_line));
 }
 
 void ChildProcessLauncher::UpdateTerminationStatus(bool known_dead) {
@@ -474,6 +496,7 @@ void ChildProcessLauncher::DidLaunch(
     ZygoteHandle zygote,
 #if defined(OS_ANDROID)
     base::ScopedFD ipcfd,
+    base::ScopedFD mojo_fd,
 #endif
     base::Process process) {
   if (!process.IsValid())
@@ -504,6 +527,12 @@ void ChildProcessLauncher::Notify(ZygoteHandle zygote,
   DCHECK(CalledOnValidThread());
   starting_ = false;
   process_ = std::move(process);
+
+  if (process_.IsValid()) {
+    // Set up Mojo IPC to the new process.
+    mojo::edk::ChildProcessLaunched(process_.Handle(),
+                                    mojo_platform_channel_.PassServerHandle());
+  }
 
 #if defined(OS_POSIX) && !defined(OS_MACOSX) && !defined(OS_ANDROID)
   zygote_ = zygote;

content/browser/child_process_launcher.h
@@ -16,6 +16,7 @@
 #include "content/common/content_export.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/sandboxed_process_launcher_delegate.h"
+#include "mojo/edk/embedder/platform_channel_pair.h"
 
 namespace base {
 class CommandLine;
@@ -98,6 +99,7 @@ class CONTENT_EXPORT ChildProcessLauncher : public base::NonThreadSafe {
                         ZygoteHandle zygote,
 #if defined(OS_ANDROID)
                         base::ScopedFD ipcfd,
+                        base::ScopedFD mojo_fd,
 #endif
                         base::Process process);
 
@@ -127,6 +129,9 @@ class CONTENT_EXPORT ChildProcessLauncher : public base::NonThreadSafe {
   // Controls whether the child process should be terminated on browser
   // shutdown. Default behavior is to terminate the child.
   const bool terminate_child_on_shutdown_;
+
+  // Platform channel used to establish Mojo IPC.
+  mojo::edk::PlatformChannelPair mojo_platform_channel_;
 
   base::WeakPtrFactory<ChildProcessLauncher> weak_factory_;
 

content/browser/mojo/mojo_shell_client_host.cc
@@ -11,7 +11,6 @@
 #include "base/strings/stringprintf.h"
 #include "base/thread_task_runner_handle.h"
 #include "build/build_config.h"
-#include "content/common/mojo/mojo_messages.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_process_host_observer.h"
@@ -19,8 +18,7 @@
 #include "ipc/ipc_sender.h"
 #include "mojo/converters/network/network_type_converters.h"
 #include "mojo/edk/embedder/embedder.h"
-#include "mojo/edk/embedder/platform_channel_pair.h"
-#include "mojo/edk/embedder/scoped_platform_handle.h"
+#include "mojo/public/cpp/system/message_pipe.h"
 #include "mojo/shell/public/cpp/connector.h"
 #include "mojo/shell/public/interfaces/application_manager.mojom.h"
 
@@ -28,16 +26,11 @@ namespace content {
 namespace {
 
 const char kMojoShellInstanceURL[] = "mojo_shell_instance_url";
-const char kMojoPlatformFile[] = "mojo_platform_file";
-
-base::PlatformFile PlatformFileFromScopedPlatformHandle(
-    mojo::edk::ScopedPlatformHandle handle) {
-  return handle.release().handle;
-}
 
 class InstanceURL : public base::SupportsUserData::Data {
  public:
-  InstanceURL(const std::string& instance_url) : instance_url_(instance_url) {}
+  explicit InstanceURL(const std::string& instance_url)
+      : instance_url_(instance_url) {}
   ~InstanceURL() override {}
 
   std::string get() const { return instance_url_; }
@@ -48,30 +41,10 @@ class InstanceURL : public base::SupportsUserData::Data {
   DISALLOW_COPY_AND_ASSIGN(InstanceURL);
 };
 
-class InstanceShellHandle : public base::SupportsUserData::Data {
- public:
-  InstanceShellHandle(base::PlatformFile shell_handle)
-      : shell_handle_(shell_handle) {}
-  ~InstanceShellHandle() override {}
-
-  base::PlatformFile get() const { return shell_handle_; }
-
- private:
-  base::PlatformFile shell_handle_;
-
-  DISALLOW_COPY_AND_ASSIGN(InstanceShellHandle);
-};
-
 void SetMojoApplicationInstanceURL(RenderProcessHost* render_process_host,
                                    const std::string& instance_url) {
   render_process_host->SetUserData(kMojoShellInstanceURL,
                                    new InstanceURL(instance_url));
-}
-
-void SetMojoPlatformFile(RenderProcessHost* render_process_host,
-                         base::PlatformFile platform_file) {
-  render_process_host->SetUserData(kMojoPlatformFile,
-                                   new InstanceShellHandle(platform_file));
 }
 
 class PIDSender : public RenderProcessHostObserver {
@@ -110,28 +83,21 @@ class PIDSender : public RenderProcessHostObserver {
 
 }  // namespace
 
-void RegisterChildWithExternalShell(int child_process_id,
-                                    int instance_id,
-                                    RenderProcessHost* render_process_host) {
-  // Some process types get created before the main message loop.
-  if (!MojoShellConnection::Get())
-    return;
-
-  // Create the channel to be shared with the target process.
-  mojo::edk::HandlePassingInformation handle_passing_info;
-  mojo::edk::PlatformChannelPair platform_channel_pair;
-
-  // Give one end to the shell so that it can create an instance.
-  mojo::edk::ScopedPlatformHandle parent_pipe =
-      platform_channel_pair.PassServerHandle();
-
-  // Send the other end to the child via Chrome IPC.
-  base::PlatformFile client_file = PlatformFileFromScopedPlatformHandle(
-      platform_channel_pair.PassClientHandle());
-  SetMojoPlatformFile(render_process_host, client_file);
-
+std::string RegisterChildWithExternalShell(
+    int child_process_id,
+    int instance_id,
+    RenderProcessHost* render_process_host) {
+  // Generate a token and create a pipe which is bound to it. This pipe is
+  // passed to the shell if one is available.
+  std::string pipe_token = mojo::edk::GenerateRandomToken();
   mojo::ScopedMessagePipeHandle request_pipe =
-      mojo::edk::CreateMessagePipe(std::move(parent_pipe));
+      mojo::edk::CreateParentMessagePipe(pipe_token);
+
+  // Some process types get created before the main message loop. In this case
+  // the shell request pipe will simply be closed, and the child can detect
+  // this.
+  if (!MojoShellConnection::Get())
+    return pipe_token;
 
   mojo::shell::mojom::ApplicationManagerPtr application_manager;
   MojoShellConnection::Get()->GetConnector()->ConnectToInterface(
@@ -161,6 +127,8 @@ void RegisterChildWithExternalShell(int child_process_id,
   // Store the URL on the RPH so client code can access it later via
   // GetMojoApplicationInstanceURL().
   SetMojoApplicationInstanceURL(render_process_host, url);
+
+  return pipe_token;
 }
 
 std::string GetMojoApplicationInstanceURL(
@@ -168,17 +136,6 @@ std::string GetMojoApplicationInstanceURL(
   InstanceURL* instance_url = static_cast<InstanceURL*>(
       render_process_host->GetUserData(kMojoShellInstanceURL));
   return instance_url ? instance_url->get() : std::string();
-}
-
-void SendExternalMojoShellHandleToChild(
-    base::ProcessHandle process_handle,
-    RenderProcessHost* render_process_host) {
-  InstanceShellHandle* client_file = static_cast<InstanceShellHandle*>(
-      render_process_host->GetUserData(kMojoPlatformFile));
-  if (!client_file)
-    return;
-  render_process_host->Send(new MojoMsg_BindExternalMojoShellHandle(
-      IPC::GetFileHandleForProcess(client_file->get(), process_handle, true)));
 }
 
 }  // namespace content

content/browser/mojo/mojo_shell_client_host.h
@@ -18,19 +18,21 @@ class RenderProcessHost;
 // child. The server handle of this channel is shared with the external shell
 // via Mojo IPC. |child_process_id| and |instance_id| are used to uniquify the
 // child in the external shell's instance map.
-void RegisterChildWithExternalShell(int child_process_id,
-                                    int instance_id,
-                                    RenderProcessHost* render_process_host);
+//
+// This returns a token that may be passed to the child process and exchanged
+// for a pipe there. That pipe can in turn be passed to
+// MojoShellConnectionImpl::BindToMessagePipe() to initialize the child's
+// shell connection.
+std::string RegisterChildWithExternalShell(
+    int child_process_id,
+    int instance_id,
+    RenderProcessHost* render_process_host);
 
 // Returns the URL associated with an instance corresponding to the renderer
 // process in the external shell. This URL can be passed to
 // ConnectToApplication() to open a new connection to this renderer.
 std::string GetMojoApplicationInstanceURL(
     RenderProcessHost* render_process_host);
-
-// Shares a client handle to the Mojo Shell with the child via Chrome IPC.
-void SendExternalMojoShellHandleToChild(base::ProcessHandle process_handle,
-                                        RenderProcessHost* render_process_host);
 
 // Constructs a Capability Filter for the renderer's application instance in the
 // external shell. This contains the restrictions imposed on what applications

content/browser/renderer_host/render_process_host_impl.cc
@@ -701,7 +701,7 @@ bool RenderProcessHostImpl::Init() {
   if (channel_)
     return true;
 
-  RegisterChildWithExternalShell(id_, instance_id_++, this);
+  shell_pipe_token_ = RegisterChildWithExternalShell(id_, instance_id_++, this);
 
   base::CommandLine::StringType renderer_prefix;
   // A command prefix is something prepended to the command line of the spawned
@@ -1623,6 +1623,11 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     if (value.empty() || value == switches::kRendererProcess) {
       renderer_cmd->AppendSwitch(switches::kWaitForDebugger);
     }
+  }
+
+  if (!shell_pipe_token_.empty()) {
+    renderer_cmd->AppendSwitchASCII(switches::kMojoPrimordialPipeToken,
+                                    shell_pipe_token_);
   }
 
 #if defined(OS_WIN) && !defined(OFFICIAL_BUILD)
@@ -2590,19 +2595,6 @@ void RenderProcessHostImpl::OnProcessLaunched() {
   NotificationService::current()->Notify(NOTIFICATION_RENDERER_PROCESS_CREATED,
                                          Source<RenderProcessHost>(this),
                                          NotificationService::NoDetails());
-
-  if (child_process_launcher_.get()) {
-    base::ProcessHandle process_handle =
-        child_process_launcher_->GetProcess().Handle();
-    mojo::edk::ScopedPlatformHandle client_pipe =
-        mojo::edk::ChildProcessLaunched(process_handle);
-    Send(new ChildProcessMsg_SetMojoParentPipeHandle(
-        IPC::GetFileHandleForProcess(client_pipe.release().handle,
-                                     process_handle, true)));
-  }
-
-  // Send the mojo shell handle to the renderer.
-  SendExternalMojoShellHandleToChild(GetHandle(), this);
 
   // Allow Mojo to be setup before the renderer sees any Chrome IPC messages.
   // This way, Mojo can be safely used from the renderer in response to any

content/browser/renderer_host/render_process_host_impl.h
@@ -357,6 +357,10 @@ class CONTENT_EXPORT RenderProcessHostImpl
   base::FilePath GetEventLogFilePathWithExtensions(const base::FilePath& file);
 #endif
 
+  // The token to be passed to the child process and exchanged for a message
+  // pipe to the shell.
+  std::string shell_pipe_token_;
+
   scoped_ptr<MojoApplicationHost> mojo_application_host_;
 
   // The registered IPC listener objects. When this list is empty, we should

content/child/child_thread_impl.cc
@@ -52,7 +52,6 @@
 #include "content/child/websocket_dispatcher.h"
 #include "content/common/child_process_messages.h"
 #include "content/common/in_process_child_thread_params.h"
-#include "content/common/mojo/mojo_messages.h"
 #include "content/common/mojo/mojo_shell_connection_impl.h"
 #include "content/public/common/content_switches.h"
 #include "ipc/attachment_broker.h"
@@ -64,9 +63,15 @@
 #include "ipc/ipc_sync_message_filter.h"
 #include "ipc/mojo/ipc_channel_mojo.h"
 #include "mojo/edk/embedder/embedder.h"
+#include "mojo/edk/embedder/platform_channel_pair.h"
 
 #if defined(USE_OZONE)
 #include "ui/ozone/public/client_native_pixmap_factory.h"
+#endif
+
+#if defined(OS_POSIX)
+#include "base/posix/global_descriptors.h"
+#include "content/public/common/content_descriptors.h"
 #endif
 
 using tracked_objects::ThreadData;
@@ -245,6 +250,20 @@ void QuitClosure::PostQuitFromNonMainThread() {
 base::LazyInstance<QuitClosure> g_quit_closure = LAZY_INSTANCE_INITIALIZER;
 #endif
 
+void InitializeMojoIPCChannel() {
+  mojo::edk::ScopedPlatformHandle platform_channel;
+#if defined(OS_WIN)
+  platform_channel =
+      mojo::edk::PlatformChannelPair::PassClientHandleFromParentProcess(
+          *base::CommandLine::ForCurrentProcess());
+#elif defined(OS_POSIX)
+  platform_channel.reset(mojo::edk::PlatformHandle(
+      base::GlobalDescriptors::GetInstance()->Get(kMojoIPCChannel)));
+#endif
+  CHECK(platform_channel.is_valid());
+  mojo::edk::SetParentPipeHandle(std::move(platform_channel));
+}
+
 }  // namespace
 
 ChildThread* ChildThread::Get() {
@@ -379,6 +398,19 @@ void ChildThreadImpl::Init(const Options& options) {
   if (!IsInBrowserProcess()) {
     // Don't double-initialize IPC support in single-process mode.
     mojo_ipc_support_.reset(new IPC::ScopedIPCSupport(GetIOTaskRunner()));
+
+    InitializeMojoIPCChannel();
+  }
+
+  // If this process was launched with a primordial pipe token, we exchange it
+  // for a pipe to connect to the shell.
+  std::string pipe_token =
+      base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+          switches::kMojoPrimordialPipeToken);
+  if (!pipe_token.empty() && MojoShellConnectionImpl::Get()) {
+    mojo::ScopedMessagePipeHandle pipe =
+        mojo::edk::CreateChildMessagePipe(pipe_token);
+    MojoShellConnectionImpl::Get()->BindToMessagePipe(std::move(pipe));
   }
 
   mojo_application_.reset(new MojoApplication(GetIOTaskRunner()));
@@ -619,10 +651,6 @@ bool ChildThreadImpl::OnMessageReceived(const IPC::Message& msg) {
                         OnProfilingPhaseCompleted)
     IPC_MESSAGE_HANDLER(ChildProcessMsg_SetProcessBackgrounded,
                         OnProcessBackgrounded)
-    IPC_MESSAGE_HANDLER(MojoMsg_BindExternalMojoShellHandle,
-                        OnBindExternalMojoShellHandle)
-    IPC_MESSAGE_HANDLER(ChildProcessMsg_SetMojoParentPipeHandle,
-                        OnSetMojoParentPipeHandle)
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
 
@@ -675,27 +703,6 @@ void ChildThreadImpl::OnGetChildProfilerData(int sequence_number,
 
 void ChildThreadImpl::OnProfilingPhaseCompleted(int profiling_phase) {
   ThreadData::OnProfilingPhaseCompleted(profiling_phase);
-}
-
-void ChildThreadImpl::OnBindExternalMojoShellHandle(
-    const IPC::PlatformFileForTransit& file) {
-  if (!MojoShellConnectionImpl::Get())
-    return;
-#if defined(OS_POSIX)
-  base::PlatformFile handle = file.fd;
-#elif defined(OS_WIN)
-  base::PlatformFile handle = file;
-#endif
-  mojo::ScopedMessagePipeHandle pipe =
-      mojo_shell_channel_init_.Init(handle, GetIOTaskRunner());
-  MojoShellConnectionImpl::Get()->BindToMessagePipe(std::move(pipe));
-}
-
-void ChildThreadImpl::OnSetMojoParentPipeHandle(
-    const IPC::PlatformFileForTransit& file) {
-  mojo::edk::SetParentPipeHandle(
-      mojo::edk::ScopedPlatformHandle(mojo::edk::PlatformHandle(
-          IPC::PlatformFileForTransitToPlatformFile(file))));
 }
 
 ChildThreadImpl* ChildThreadImpl::current() {

content/child/child_thread_impl.h
@@ -20,7 +20,6 @@
 #include "build/build_config.h"
 #include "content/child/mojo/mojo_application.h"
 #include "content/common/content_export.h"
-#include "content/common/mojo/channel_init.h"
 #include "content/public/child/child_thread.h"
 #include "ipc/ipc_message.h"  // For IPC_MESSAGE_LOG_ENABLED.
 #include "ipc/ipc_platform_file.h"
@@ -229,8 +228,6 @@ class CONTENT_EXPORT ChildThreadImpl
   void OnSetProfilerStatus(tracked_objects::ThreadData::Status status);
   void OnGetChildProfilerData(int sequence_number, int current_profiling_phase);
   void OnProfilingPhaseCompleted(int profiling_phase);
-  void OnBindExternalMojoShellHandle(const IPC::PlatformFileForTransit& file);
-  void OnSetMojoParentPipeHandle(const IPC::PlatformFileForTransit& file);
 #ifdef IPC_MESSAGE_LOG_ENABLED
   void OnSetIPCLoggingEnabled(bool enable);
 #endif
@@ -291,8 +288,6 @@ class CONTENT_EXPORT ChildThreadImpl
   scoped_refptr<ChildMessageFilter> geofencing_message_filter_;
 
   scoped_refptr<base::SequencedTaskRunner> browser_process_io_runner_;
-
-  ChannelInit mojo_shell_channel_init_;
 
   base::WeakPtrFactory<ChildThreadImpl> channel_connected_factory_;
 

content/common/mojo/mojo_messages.h
@@ -20,8 +20,3 @@
 // Mojo IPC is bootstrapped over Chrome IPC via this message.
 IPC_MESSAGE_CONTROL1(MojoMsg_Activate,
                      IPC::PlatformFileForTransit /* handle */)
-
-// Mojo IPC to an external shell is bootstrapped over Chrome IPC via this
-// message.
-IPC_MESSAGE_CONTROL1(MojoMsg_BindExternalMojoShellHandle,
-                     IPC::PlatformFileForTransit /* handle */)

content/common/mojo/mojo_shell_connection_impl.cc
@@ -85,6 +85,7 @@ void MojoShellConnectionImpl::WaitForShell(
   runner_connection_.reset(mojo::shell::RunnerConnection::ConnectToRunner(
       &request, std::move(handle), false /* exit_on_error */));
   if (!runner_connection_) {
+    DLOG(ERROR) << "Unable to connect to the Mojo shell.";
     delete this;
     lazy_tls_ptr.Pointer()->Set(nullptr);
     return;

content/public/common/content_descriptors.h
@@ -13,6 +13,7 @@
 enum {
   kCrashDumpSignal = kIPCDescriptorMax,
   kSandboxIPCChannel,  // https://chromium.googlesource.com/chromium/src/+/master/docs/linux_sandbox_ipc.md
+  kMojoIPCChannel,
 
 #if defined(V8_USE_EXTERNAL_STARTUP_DATA)
 #if defined(OS_ANDROID)

content/public/common/content_switches.cc
@@ -620,6 +620,10 @@ const char kMemoryMetrics[]                 = "memory-metrics";
 // Use a Mojo-based LocalStorage implementation.
 const char kMojoLocalStorage[]              = "mojo-local-storage";
 
+// A random token a child process can exchange for a primordial message pipe
+// to the browser.
+const char kMojoPrimordialPipeToken[]       = "mojo-primordial-pipe-token";
+
 // Mutes audio sent to the audio device so it is not audible during
 // automated testing.
 const char kMuteAudio[]                     = "mute-audio";

content/public/common/content_switches.h
@@ -183,6 +183,7 @@ extern const char kMaxUntiledLayerHeight[];
 extern const char kMaxUntiledLayerWidth[];
 extern const char kMemoryMetrics[];
 extern const char kMojoLocalStorage[];
+CONTENT_EXPORT extern const char kMojoPrimordialPipeToken[];
 CONTENT_EXPORT extern const char kMuteAudio[];
 CONTENT_EXPORT extern const char kNoReferrers[];
 CONTENT_EXPORT extern const char kNoSandbox[];

content/public/common/zygote_fork_delegate_linux.h
@@ -56,6 +56,7 @@ class ZygoteForkDelegate {
     // The child process is required to write to the socket after
     // successfully forking.
     kPIDOracleFDIndex,
+    kMojoParentFDIndex,
     kNumPassedFDs  // Number of FDs in the vector passed to Fork().
   };
 

content/zygote/zygote_linux.cc
@@ -436,9 +436,15 @@ int Zygote::ForkWithRealPid(const std::string& process_type,
       DLOG(ERROR) << "Failed to find kPrimaryIPCChannel in FD mapping";
       return -1;
     }
+    int mojo_channel_fd = LookUpFd(fd_mapping, kMojoIPCChannel);
+    if (mojo_channel_fd < 0) {
+      DLOG(ERROR) << "Failed to find kMojoIPCChannel in FD mapping";
+      return -1;
+    }
     std::vector<int> fds;
     fds.push_back(ipc_channel_fd);  // kBrowserFDIndex
     fds.push_back(pid_oracle.get());  // kPIDOracleFDIndex
+    fds.push_back(mojo_channel_fd);  // kMojoParentFDIndex
     pid = helper->Fork(process_type, fds, channel_id);
 
     // Helpers should never return in the child process.

mojo/edk/embedder/platform_channel_pair.h
@@ -52,8 +52,10 @@ using HandlePassingInformation = base::FileHandleMappingVector;
 // platform differences in suppressing |SIGPIPE|.
 class MOJO_SYSTEM_IMPL_EXPORT PlatformChannelPair {
  public:
+  static const char kMojoPlatformChannelHandleSwitch[];
+
   // If |client_is_blocking| is true, then the client handle only supports
-   // blocking reads and writes. The default is nonblocking.
+  // blocking reads and writes. The default is nonblocking.
   PlatformChannelPair(bool client_is_blocking = false);
   ~PlatformChannelPair();
 
@@ -90,8 +92,6 @@ class MOJO_SYSTEM_IMPL_EXPORT PlatformChannelPair {
   void ChildProcessLaunched();
 
  private:
-  static const char kMojoPlatformChannelHandleSwitch[];
-
   ScopedPlatformHandle server_handle_;
   ScopedPlatformHandle client_handle_;
 

mojo/edk/system/node_controller.cc
@@ -180,6 +180,19 @@ void NodeController::ReservePort(const std::string& token,
 
 void NodeController::MergePortIntoParent(const std::string& token,
                                          const ports::PortRef& port) {
+  {
+    // This request may be coming from within the process that reserved the
+    // "parent" side (e.g. for Chrome single-process mode), so if this token is
+    // reserved locally, merge locally instead.
+    base::AutoLock lock(reserved_ports_lock_);
+    auto it = reserved_ports_.find(token);
+    if (it != reserved_ports_.end()) {
+      node_->MergePorts(port, name_, it->second.name());
+      reserved_ports_.erase(it);
+      return;
+    }
+  }
+
   scoped_refptr<NodeChannel> parent = GetParentChannel();
   if (parent) {
     parent->RequestPortMerge(port.name(), token);