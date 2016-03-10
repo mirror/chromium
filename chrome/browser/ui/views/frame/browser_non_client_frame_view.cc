crosswalk-project/chromium-crosswalk

commit c26c0f3e2df970d98800ea151efc00e7d22fc3c8

[MD] [Win/Linux] Paint opaque frames without bg asset
For MD this gets rid of the subtle gradient that was present in pre-MD and fixes
incognito frame rendering.

This also shares more code with CrOS.

BUG=584386

Review URL: https://codereview.chromium.org/1737803002

Cr-Commit-Position: refs/heads/master@{#378648}

chrome/browser/ui/views/frame/browser_non_client_frame_view.cc
@@ -20,6 +20,7 @@
 #include "components/signin/core/common/profile_management_switches.h"
 #include "grit/theme_resources.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/theme_provider.h"
 #include "ui/gfx/image/image.h"
@@ -90,58 +91,56 @@ bool BrowserNonClientFrameView::ShouldPaintAsThemed() const {
   return browser_view_->IsBrowserTypeNormal();
 }
 
-#if !defined(OS_CHROMEOS)
-SkColor BrowserNonClientFrameView::GetFrameColor() const {
+SkColor BrowserNonClientFrameView::GetFrameColor(bool active) const {
   ThemeProperties::OverwritableByUserThemeProperty color_id =
-      ShouldPaintAsActive() ? ThemeProperties::COLOR_FRAME
-                            : ThemeProperties::COLOR_FRAME_INACTIVE;
+      active ? ThemeProperties::COLOR_FRAME
+             : ThemeProperties::COLOR_FRAME_INACTIVE;
   return ShouldPaintAsThemed() ? GetThemeProvider()->GetColor(color_id)
                                : ThemeProperties::GetDefaultColor(
                                      color_id, browser_view_->IsOffTheRecord());
 }
 
-gfx::ImageSkia* BrowserNonClientFrameView::GetFrameImage() const {
-  const bool incognito = browser_view_->IsOffTheRecord();
-  int resource_id;
-  if (browser_view_->IsBrowserTypeNormal()) {
-    if (ShouldPaintAsActive()) {
-      resource_id = incognito ? IDR_THEME_FRAME_INCOGNITO : IDR_THEME_FRAME;
-    } else {
-      resource_id = incognito ? IDR_THEME_FRAME_INCOGNITO_INACTIVE
-                              : IDR_THEME_FRAME_INACTIVE;
-    }
-    return GetThemeProvider()->GetImageSkiaNamed(resource_id);
+gfx::ImageSkia BrowserNonClientFrameView::GetFrameImage(bool active) const {
+  const ui::ThemeProvider* tp = frame_->GetThemeProvider();
+  int frame_image_id = active ? IDR_THEME_FRAME : IDR_THEME_FRAME_INACTIVE;
+
+  if (ui::MaterialDesignController::IsModeMaterial()) {
+    return ShouldPaintAsThemed() && (tp->HasCustomImage(frame_image_id) ||
+                                     tp->HasCustomImage(IDR_THEME_FRAME))
+               ? *tp->GetImageSkiaNamed(frame_image_id)
+               : gfx::ImageSkia();
   }
 
-  if (ShouldPaintAsActive()) {
-    resource_id = incognito ? IDR_THEME_FRAME_INCOGNITO : IDR_FRAME;
-  } else {
-    resource_id = incognito ? IDR_THEME_FRAME_INCOGNITO_INACTIVE
-                            : IDR_THEME_FRAME_INACTIVE;
-  }
-
-  if (ShouldPaintAsThemed()) {
-    // On Linux, we want to use theme images provided by the system theme when
-    // enabled, even if we are an app or popup window.
-    return GetThemeProvider()->GetImageSkiaNamed(resource_id);
-  }
-
-  // Otherwise, never theme app and popup windows.
-  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
-  return rb.GetImageSkiaNamed(resource_id);
+  return ShouldPaintAsThemed()
+             ? *tp->GetImageSkiaNamed(frame_image_id)
+             : *ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
+                   frame_image_id);
 }
 
-gfx::ImageSkia* BrowserNonClientFrameView::GetFrameOverlayImage() const {
-  const ui::ThemeProvider* tp = GetThemeProvider();
-  if (tp->HasCustomImage(IDR_THEME_FRAME_OVERLAY) &&
-      browser_view_->IsBrowserTypeNormal() &&
-      !browser_view_->IsOffTheRecord()) {
-    return tp->GetImageSkiaNamed(ShouldPaintAsActive() ?
-        IDR_THEME_FRAME_OVERLAY : IDR_THEME_FRAME_OVERLAY_INACTIVE);
-  }
-  return nullptr;
+gfx::ImageSkia BrowserNonClientFrameView::GetFrameOverlayImage(
+    bool active) const {
+  if (browser_view_->IsOffTheRecord() || !browser_view_->IsBrowserTypeNormal())
+    return gfx::ImageSkia();
+
+  const ui::ThemeProvider* tp = frame_->GetThemeProvider();
+  int frame_overlay_image_id =
+      active ? IDR_THEME_FRAME_OVERLAY : IDR_THEME_FRAME_OVERLAY_INACTIVE;
+  return tp->HasCustomImage(frame_overlay_image_id)
+             ? *tp->GetImageSkiaNamed(frame_overlay_image_id)
+             : gfx::ImageSkia();
 }
-#endif  // !defined(OS_CHROMEOS)
+
+SkColor BrowserNonClientFrameView::GetFrameColor() const {
+  return GetFrameColor(ShouldPaintAsActive());
+}
+
+gfx::ImageSkia BrowserNonClientFrameView::GetFrameImage() const {
+  return GetFrameImage(ShouldPaintAsActive());
+}
+
+gfx::ImageSkia BrowserNonClientFrameView::GetFrameOverlayImage() const {
+  return GetFrameOverlayImage(ShouldPaintAsActive());
+}
 
 void BrowserNonClientFrameView::UpdateOldAvatarButton() {
   if (browser_view_->ShouldShowAvatar()) {

chrome/browser/ui/views/frame/browser_header_painter_ash.cc
@@ -11,6 +11,7 @@
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/views/frame/browser_frame.h"
+#include "chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "grit/ash_resources.h"
 #include "grit/theme_resources.h"
@@ -135,7 +136,7 @@ BrowserHeaderPainterAsh::~BrowserHeaderPainterAsh() {
 void BrowserHeaderPainterAsh::Init(
     views::Widget* frame,
     BrowserView* browser_view,
-    views::View* header_view,
+    BrowserNonClientFrameViewAsh* header_view,
     views::View* window_icon,
     ash::FrameCaptionButtonContainerView* caption_button_container) {
   DCHECK(frame);
@@ -184,45 +185,8 @@ void BrowserHeaderPainterAsh::PaintHeader(gfx::Canvas* canvas, Mode mode) {
     initial_paint_ = false;
   }
 
-  int corner_radius = (frame_->IsMaximized() || frame_->IsFullscreen()) ?
-      0 : ash::HeaderPainterUtil::GetTopCornerRadiusWhenRestored();
-
-  int active_alpha = activation_animation_->CurrentValueBetween(0, 255);
-  int inactive_alpha = 255 - active_alpha;
-
-  SkPaint paint;
-  if (inactive_alpha > 0) {
-    if (active_alpha > 0)
-      paint.setXfermodeMode(SkXfermode::kPlus_Mode);
-
-    gfx::ImageSkia inactive_frame_image = GetFrameImage(MODE_INACTIVE);
-    gfx::ImageSkia inactive_frame_overlay_image =
-        GetFrameOverlayImage(MODE_INACTIVE);
-
-    paint.setAlpha(inactive_alpha);
-    paint.setColor(SkColorSetA(GetFrameColor(MODE_INACTIVE), inactive_alpha));
-    PaintFrameImagesInRoundRect(
-        canvas, inactive_frame_image, inactive_frame_overlay_image, paint,
-        GetPaintedBounds(), corner_radius,
-        ash::HeaderPainterUtil::GetThemeBackgroundXInset());
-  }
-
-  if (active_alpha > 0) {
-    gfx::ImageSkia active_frame_image = GetFrameImage(MODE_ACTIVE);
-    gfx::ImageSkia active_frame_overlay_image =
-        GetFrameOverlayImage(MODE_ACTIVE);
-
-    paint.setAlpha(active_alpha);
-    paint.setColor(SkColorSetA(GetFrameColor(MODE_ACTIVE), active_alpha));
-    PaintFrameImagesInRoundRect(
-        canvas,
-        active_frame_image,
-        active_frame_overlay_image,
-        paint,
-        GetPaintedBounds(),
-        corner_radius,
-        ash::HeaderPainterUtil::GetThemeBackgroundXInset());
-  }
+  PaintFrameImages(canvas, false);
+  PaintFrameImages(canvas, true);
 
   if (!ui::MaterialDesignController::IsModeMaterial() &&
       !frame_->IsMaximized() &&
@@ -293,6 +257,32 @@ void BrowserHeaderPainterAsh::AnimationProgressed(
 ///////////////////////////////////////////////////////////////////////////////
 // BrowserHeaderPainterAsh, private:
 
+void BrowserHeaderPainterAsh::PaintFrameImages(gfx::Canvas* canvas,
+                                               bool active) {
+  int alpha = activation_animation_->CurrentValueBetween(0, 0xFF);
+  if (!active)
+    alpha = 0xFF - alpha;
+
+  if (alpha == 0)
+    return;
+
+  int corner_radius =
+      (frame_->IsMaximized() || frame_->IsFullscreen())
+          ? 0
+          : ash::HeaderPainterUtil::GetTopCornerRadiusWhenRestored();
+
+  gfx::ImageSkia frame_image = view_->GetFrameImage(active);
+  gfx::ImageSkia frame_overlay_image = view_->GetFrameOverlayImage(active);
+
+  SkPaint paint;
+  paint.setXfermodeMode(SkXfermode::kPlus_Mode);
+  paint.setAlpha(alpha);
+  paint.setColor(SkColorSetA(view_->GetFrameColor(active), alpha));
+  PaintFrameImagesInRoundRect(
+      canvas, frame_image, frame_overlay_image, paint, GetPaintedBounds(),
+      corner_radius, ash::HeaderPainterUtil::GetThemeBackgroundXInset());
+}
+
 void BrowserHeaderPainterAsh::PaintHighlightForRestoredWindow(
     gfx::Canvas* canvas) {
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
@@ -346,43 +336,6 @@ void BrowserHeaderPainterAsh::PaintTitleBar(gfx::Canvas* canvas) {
                                   kWindowTitleTextColor,
                                   title_bounds,
                                   gfx::Canvas::NO_SUBPIXEL_RENDERING);
-}
-
-gfx::ImageSkia BrowserHeaderPainterAsh::GetFrameImage(Mode mode) const {
-  if (!is_tabbed_)
-    return gfx::ImageSkia();
-
-  const ui::ThemeProvider* tp = frame_->GetThemeProvider();
-  int frame_image_id =
-      (mode == MODE_ACTIVE) ? IDR_THEME_FRAME : IDR_THEME_FRAME_INACTIVE;
-
-  // Even if there's no explicit custom image for IDR_THEME_FRAME_INACTIVE, one
-  // will be generated for it based on IDR_THEME_FRAME.
-  return tp->HasCustomImage(frame_image_id) ||
-                 tp->HasCustomImage(IDR_THEME_FRAME)
-             ? *tp->GetImageSkiaNamed(frame_image_id)
-             : gfx::ImageSkia();
-}
-
-gfx::ImageSkia BrowserHeaderPainterAsh::GetFrameOverlayImage(Mode mode) const {
-  if (is_incognito_ || !is_tabbed_)
-    return gfx::ImageSkia();
-
-  const ui::ThemeProvider* tp = frame_->GetThemeProvider();
-  int frame_overlay_image_id = (mode == MODE_ACTIVE)
-                                   ? IDR_THEME_FRAME_OVERLAY
-                                   : IDR_THEME_FRAME_OVERLAY_INACTIVE;
-  return tp->HasCustomImage(frame_overlay_image_id)
-             ? *tp->GetImageSkiaNamed(frame_overlay_image_id)
-             : gfx::ImageSkia();
-}
-
-SkColor BrowserHeaderPainterAsh::GetFrameColor(Mode mode) const {
-  int color = (mode == MODE_ACTIVE) ? ThemeProperties::COLOR_FRAME
-                                    : ThemeProperties::COLOR_FRAME_INACTIVE;
-  // We only theme tabbed windows; fall back to default for non-tabbed.
-  return is_tabbed_ ? frame_->GetThemeProvider()->GetColor(color)
-                    : ThemeProperties::GetDefaultColor(color, is_incognito_);
 }
 
 void BrowserHeaderPainterAsh::UpdateCaptionButtons() {

chrome/browser/ui/views/frame/browser_header_painter_ash.h
@@ -12,6 +12,7 @@
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/animation/animation_delegate.h"
 
+class BrowserNonClientFrameViewAsh;
 class BrowserView;
 
 namespace ash {
@@ -36,12 +37,11 @@ class BrowserHeaderPainterAsh : public ash::HeaderPainter,
   ~BrowserHeaderPainterAsh() override;
 
   // BrowserHeaderPainterAsh does not take ownership of any of the parameters.
-  void Init(
-    views::Widget* frame,
-    BrowserView* browser_view,
-    views::View* header_view,
-    views::View* window_icon,
-    ash::FrameCaptionButtonContainerView* caption_button_container);
+  void Init(views::Widget* frame,
+            BrowserView* browser_view,
+            BrowserNonClientFrameViewAsh* header_view,
+            views::View* window_icon,
+            ash::FrameCaptionButtonContainerView* caption_button_container);
 
   // ash::HeaderPainter overrides:
   int GetMinimumHeaderWidth() const override;
@@ -56,23 +56,15 @@ class BrowserHeaderPainterAsh : public ash::HeaderPainter,
   // gfx::AnimationDelegate override:
   void AnimationProgressed(const gfx::Animation* animation) override;
 
+  // Paints the frame image for the |active| state based on the current value of
+  // the activation animation.
+  void PaintFrameImages(gfx::Canvas* canvas, bool active);
+
   // Paints highlight around the edge of the header for restored windows.
   void PaintHighlightForRestoredWindow(gfx::Canvas* canvas);
 
   // Paints the title bar, primarily the title string.
   void PaintTitleBar(gfx::Canvas* canvas);
-
-  // Gets the base image to paint for the frame. This may be null, in which case
-  // GetFrameColor() should be used instead.
-  gfx::ImageSkia GetFrameImage(Mode mode) const;
-
-  // Gets the overlay image to paint on top of the base image. This may be null,
-  // in which case nothing will be drawn.
-  gfx::ImageSkia GetFrameOverlayImage(Mode mode) const;
-
-  // Gets the color to use to paint the frame (when GetFrameImage() returns
-  // a null image).
-  SkColor GetFrameColor(Mode mode) const;
 
   // Updates the size and icons used for the minimize, restore, and close
   // buttons.
@@ -95,7 +87,7 @@ class BrowserHeaderPainterAsh : public ash::HeaderPainter,
   bool is_incognito_;
 
   // The header view.
-  views::View* view_;
+  BrowserNonClientFrameViewAsh* view_;
 
   views::View* window_icon_;
   ash::FrameCaptionButtonContainerView* caption_button_container_;

chrome/browser/ui/views/frame/browser_non_client_frame_view.h
@@ -64,12 +64,16 @@ class BrowserNonClientFrameView : public views::NonClientFrameView,
   // not.
   virtual bool ShouldPaintAsThemed() const;
 
-#if !defined(OS_CHROMEOS)
   // Compute aspects of the frame needed to paint the frame background.
+  SkColor GetFrameColor(bool active) const;
+  gfx::ImageSkia GetFrameImage(bool active) const;
+  gfx::ImageSkia GetFrameOverlayImage(bool active) const;
+
+  // Convenience versions of the above which use ShouldPaintAsActive() for
+  // |active|.
   SkColor GetFrameColor() const;
-  gfx::ImageSkia* GetFrameImage() const;
-  gfx::ImageSkia* GetFrameOverlayImage() const;
-#endif
+  gfx::ImageSkia GetFrameImage() const;
+  gfx::ImageSkia GetFrameOverlayImage() const;
 
   // Update the profile switcher button if one should exist. Otherwise, update
   // the incognito avatar, or profile avatar for teleported frames in ChromeOS.

chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.h
@@ -85,6 +85,7 @@ class BrowserNonClientFrameViewAsh : public BrowserNonClientFrameView,
                            ToggleMaximizeModeRelayout);
   FRIEND_TEST_ALL_PREFIXES(WebAppLeftHeaderViewTest, BackButton);
   FRIEND_TEST_ALL_PREFIXES(WebAppLeftHeaderViewTest, LocationIcon);
+  friend class BrowserHeaderPainterAsh;
 
   // views::NonClientFrameView:
   bool DoesIntersectRect(const views::View* target,

chrome/browser/ui/views/frame/opaque_browser_frame_view.cc
@@ -573,8 +573,11 @@ bool OpaqueBrowserFrameView::ShouldShowWindowTitleBar() const {
 }
 
 int OpaqueBrowserFrameView::GetTopAreaHeight() const {
-  const gfx::ImageSkia* const frame_image = GetFrameImage();
-  int top_area_height = frame_image->height();
+  // The top area height in dp (only used when there's no frame image).
+  // TODO(pkasting): investigate removing this constant. See crbug.com/590301
+  const int kHeight = 64;
+  const gfx::ImageSkia frame_image = GetFrameImage();
+  int top_area_height = frame_image.isNull() ? kHeight : frame_image.height();
   if (browser_view()->IsTabStripVisible()) {
     top_area_height =
         std::max(top_area_height,

ui/views/window/custom_frame_view.cc
@@ -352,9 +352,9 @@ bool CustomFrameView::ShouldShowClientEdge() const {
 
 void CustomFrameView::PaintRestoredFrameBorder(gfx::Canvas* canvas) {
   frame_background_->set_frame_color(GetFrameColor());
-  const gfx::ImageSkia* frame_image = GetFrameImage();
+  const gfx::ImageSkia frame_image = GetFrameImage();
   frame_background_->set_theme_image(frame_image);
-  frame_background_->set_top_area_height(frame_image->height());
+  frame_background_->set_top_area_height(frame_image.height());
 
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
 
@@ -373,9 +373,9 @@ void CustomFrameView::PaintRestoredFrameBorder(gfx::Canvas* canvas) {
 }
 
 void CustomFrameView::PaintMaximizedFrameBorder(gfx::Canvas* canvas) {
-  const gfx::ImageSkia* frame_image = GetFrameImage();
+  const gfx::ImageSkia frame_image = GetFrameImage();
   frame_background_->set_theme_image(frame_image);
-  frame_background_->set_top_area_height(frame_image->height());
+  frame_background_->set_top_area_height(frame_image.height());
   frame_background_->PaintMaximized(canvas, this);
 
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
@@ -475,9 +475,11 @@ SkColor CustomFrameView::GetFrameColor() const {
   return frame_->IsActive() ? kDefaultColorFrame : kDefaultColorFrameInactive;
 }
 
-const gfx::ImageSkia* CustomFrameView::GetFrameImage() const {
-  return ui::ResourceBundle::GetSharedInstance().GetImageNamed(
-      frame_->IsActive() ? IDR_FRAME : IDR_FRAME_INACTIVE).ToImageSkia();
+gfx::ImageSkia CustomFrameView::GetFrameImage() const {
+  return *ui::ResourceBundle::GetSharedInstance()
+              .GetImageNamed(frame_->IsActive() ? IDR_FRAME
+                                                : IDR_FRAME_INACTIVE)
+              .ToImageSkia();
 }
 
 void CustomFrameView::LayoutWindowControls() {

ui/views/window/custom_frame_view.h
@@ -8,13 +8,10 @@
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
+#include "ui/gfx/image/image_skia.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/window/frame_buttons.h"
 #include "ui/views/window/non_client_view.h"
-
-namespace gfx {
-class ImageSkia;
-}
 
 namespace views {
 
@@ -105,7 +102,7 @@ class VIEWS_EXPORT CustomFrameView : public NonClientFrameView,
 
   // Compute aspects of the frame needed to paint the frame background.
   SkColor GetFrameColor() const;
-  const gfx::ImageSkia* GetFrameImage() const;
+  gfx::ImageSkia GetFrameImage() const;
 
   // Performs the layout for the window control buttons based on the
   // configuration specified in WindowButtonOrderProvider. The sizing and

mirror/chromium

commit 262e627e7889a5c3bc954c3d3e0baaca33f5b1d9

CrOS - in pre-MD mode, paint frame correctly
This regressed in c26c0f3e2df970d98 which landed after the m50 branch,
so this fix is mostly academic. But the change is small and if for some
reason we need to revert to pre-MD on CrOS 51, this change will be
necessary.

BUG=592741

Review URL: https://codereview.chromium.org/1774423002

chrome/browser/ui/views/frame/browser_non_client_frame_view.cc
@@ -108,7 +108,14 @@ gfx::ImageSkia BrowserNonClientFrameView::GetFrameImage(bool active) const {
   const ui::ThemeProvider* tp = frame_->GetThemeProvider();
   int frame_image_id = active ? IDR_THEME_FRAME : IDR_THEME_FRAME_INACTIVE;
 
-  if (ui::MaterialDesignController::IsModeMaterial()) {
+  // |default_uses_color| means the default frame is painted with a solid color.
+  // When false, the default frame is painted with assets.
+#if defined(OS_CHROMEOS)
+  bool default_uses_color = true;
+#else
+  bool default_uses_color = ui::MaterialDesignController::IsModeMaterial();
+#endif
+  if (default_uses_color) {
     return ShouldPaintAsThemed() && (tp->HasCustomImage(frame_image_id) ||
                                      tp->HasCustomImage(IDR_THEME_FRAME))
                ? *tp->GetImageSkiaNamed(frame_image_id)