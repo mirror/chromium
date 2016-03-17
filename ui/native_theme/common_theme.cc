crosswalk-project/chromium-crosswalk

commit ff6ab32f0ee2963f9d7c911c0c15784f21b71c6c

Co-locate all NativeTheme Aura colors.
Before this patch, colors chosen to look good for Aura were split between
CommonThemeGetSystemColor and NativeThemeAura. This moves all the colors
to the former and changes the signature a bit.

There should be no effective change on any platform with this CL.

BUG=530645
TBR=sky@chromium.org

Review URL: https://codereview.chromium.org/1476663002

Cr-Commit-Position: refs/heads/master@{#362326}

ui/native_theme/common_theme.h
@@ -18,10 +18,11 @@ namespace ui {
 
 // Drawing code that is common for all platforms.
 
-// Returns true and |color| if |color_id| is found, or false otherwise.
-bool NATIVE_THEME_EXPORT CommonThemeGetSystemColor(
-    NativeTheme::ColorId color_id,
-    SkColor* color);
+// Returns the color to use on Aura for |color_id|.  For a few colors that are
+// theme-specific, |base_theme| must be non-null; consult the code to see which
+// color IDs fall into this category.
+SkColor NATIVE_THEME_EXPORT GetAuraColor(NativeTheme::ColorId color_id,
+                                         const NativeTheme* base_theme);
 
 gfx::Size NATIVE_THEME_EXPORT CommonThemeGetPartSize(
     NativeTheme::Part part,

ui/native_theme/native_theme_aura.cc
@@ -99,295 +99,7 @@ NativeThemeAura::~NativeThemeAura() {
 
 // This implementation returns hardcoded colors.
 SkColor NativeThemeAura::GetSystemColor(ColorId color_id) const {
-  // Shared colors.
-  static const SkColor kTextfieldDefaultBackground = SK_ColorWHITE;
-  static const SkColor kTextfieldSelectionBackgroundFocused =
-      SkColorSetARGB(0x54, 0x60, 0xA8, 0xEB);
-
-  // MD colors.
-  if (ui::MaterialDesignController::IsModeMaterial()) {
-    // Dialogs:
-    static const SkColor kDialogBackgroundColorMd = SK_ColorWHITE;
-    // Results tables:
-    static const SkColor kResultsTableTextMd = SK_ColorBLACK;
-    static const SkColor kResultsTableDimmedTextMd =
-        SkColorSetRGB(0x64, 0x64, 0x64);
-    static const SkColor kPositiveTextColorMd = SkColorSetRGB(0x0b, 0x80, 0x43);
-    static const SkColor kNegativeTextColorMd = SkColorSetRGB(0xc5, 0x39, 0x29);
-
-    switch (color_id) {
-      // Dialogs
-      case kColorId_DialogBackground:
-      case kColorId_BubbleBackground:
-        return kDialogBackgroundColorMd;
-
-      // Results Tables
-      case kColorId_ResultsTableHoveredBackground:
-        return SkColorSetA(
-            GetSystemColor(kColorId_ResultsTableNormalBackground), 0x0D);
-      case kColorId_ResultsTableSelectedBackground:
-        return SkColorSetA(
-            GetSystemColor(kColorId_ResultsTableNormalBackground), 0x14);
-      case kColorId_ResultsTableNormalText:
-      case kColorId_ResultsTableHoveredText:
-      case kColorId_ResultsTableSelectedText:
-      case kColorId_ResultsTableNormalHeadline:
-      case kColorId_ResultsTableHoveredHeadline:
-      case kColorId_ResultsTableSelectedHeadline:
-        return kResultsTableTextMd;
-      case kColorId_ResultsTableNormalDimmedText:
-      case kColorId_ResultsTableHoveredDimmedText:
-      case kColorId_ResultsTableSelectedDimmedText:
-        return kResultsTableDimmedTextMd;
-      case kColorId_ResultsTableNormalUrl:
-      case kColorId_ResultsTableHoveredUrl:
-      case kColorId_ResultsTableSelectedUrl:
-        return GetSystemColor(kColorId_LinkEnabled);
-      case kColorId_ResultsTablePositiveText:
-      case kColorId_ResultsTablePositiveHoveredText:
-      case kColorId_ResultsTablePositiveSelectedText:
-        return kPositiveTextColorMd;
-      case kColorId_ResultsTableNegativeText:
-      case kColorId_ResultsTableNegativeHoveredText:
-      case kColorId_ResultsTableNegativeSelectedText:
-        return kNegativeTextColorMd;
-
-      default:
-        break;
-    }
-  }
-
-  // Pre-MD colors.
-  static const SkColor kInvalidColorIdColor = SkColorSetRGB(255, 0, 128);
-  // Windows:
-  static const SkColor kWindowBackgroundColor = SK_ColorWHITE;
-  // Dialogs:
-  static const SkColor kDialogBackgroundColor = SkColorSetRGB(251, 251, 251);
-  // FocusableBorder:
-  static const SkColor kFocusedBorderColor = SkColorSetRGB(0x4D, 0x90, 0xFE);
-  static const SkColor kUnfocusedBorderColor = SkColorSetRGB(0xD9, 0xD9, 0xD9);
-  // Button:
-  static const SkColor kButtonBackgroundColor = SkColorSetRGB(0xDE, 0xDE, 0xDE);
-  static const SkColor kButtonEnabledColor = SkColorSetRGB(0x22, 0x22, 0x22);
-  static const SkColor kButtonHighlightColor = SkColorSetRGB(0, 0, 0);
-  static const SkColor kButtonHoverColor = kButtonEnabledColor;
-  // Label:
-  static const SkColor kLabelEnabledColor = kButtonEnabledColor;
-  static const SkColor kLabelBackgroundColor = SK_ColorWHITE;
-  // Link:
-  static const SkColor kLinkDisabledColor = SK_ColorBLACK;
-  static const SkColor kLinkEnabledColor = SkColorSetRGB(0, 51, 153);
-  static const SkColor kLinkPressedColor = SK_ColorRED;
-  // Textfield:
-  static const SkColor kTextfieldDefaultColor = SK_ColorBLACK;
-  static const SkColor kTextfieldReadOnlyColor = SK_ColorDKGRAY;
-  static const SkColor kTextfieldReadOnlyBackground = SK_ColorWHITE;
-  static const SkColor kTextfieldSelectionColor = color_utils::AlphaBlend(
-      SK_ColorBLACK, kTextfieldSelectionBackgroundFocused, 0xdd);
-  // Tooltip
-  static const SkColor kTooltipBackground = 0xFFFFFFCC;
-  static const SkColor kTooltipTextColor = kLabelEnabledColor;
-  // Tree
-  static const SkColor kTreeBackground = SK_ColorWHITE;
-  static const SkColor kTreeTextColor = SK_ColorBLACK;
-  static const SkColor kTreeSelectedTextColor = SK_ColorBLACK;
-  static const SkColor kTreeSelectionBackgroundColor =
-      SkColorSetRGB(0xEE, 0xEE, 0xEE);
-  static const SkColor kTreeArrowColor = SkColorSetRGB(0x7A, 0x7A, 0x7A);
-  // Table
-  static const SkColor kTableBackground = SK_ColorWHITE;
-  static const SkColor kTableTextColor = SK_ColorBLACK;
-  static const SkColor kTableSelectedTextColor = SK_ColorBLACK;
-  static const SkColor kTableSelectionBackgroundColor =
-      SkColorSetRGB(0xEE, 0xEE, 0xEE);
-  static const SkColor kTableGroupingIndicatorColor =
-      SkColorSetRGB(0xCC, 0xCC, 0xCC);
-  // Results Tables
-  static const SkColor kResultsTableSelectedBackground =
-      kTextfieldSelectionBackgroundFocused;
-  static const SkColor kResultsTableNormalText =
-      color_utils::AlphaBlend(SK_ColorBLACK, kTextfieldDefaultBackground, 0xDD);
-  static const SkColor kResultsTableHoveredBackground = color_utils::AlphaBlend(
-      kTextfieldSelectionBackgroundFocused, kTextfieldDefaultBackground, 0x40);
-  static const SkColor kResultsTableHoveredText = color_utils::AlphaBlend(
-      SK_ColorBLACK, kResultsTableHoveredBackground, 0xDD);
-  static const SkColor kResultsTableSelectedText = color_utils::AlphaBlend(
-      SK_ColorBLACK, kTextfieldSelectionBackgroundFocused, 0xDD);
-  static const SkColor kResultsTableNormalDimmedText =
-      color_utils::AlphaBlend(SK_ColorBLACK, kTextfieldDefaultBackground, 0xBB);
-  static const SkColor kResultsTableHoveredDimmedText = color_utils::AlphaBlend(
-      SK_ColorBLACK, kResultsTableHoveredBackground, 0xBB);
-  static const SkColor kResultsTableSelectedDimmedText =
-      color_utils::AlphaBlend(SK_ColorBLACK,
-                              kTextfieldSelectionBackgroundFocused, 0xBB);
-  static const SkColor kResultsTableNormalUrl = kTextfieldSelectionColor;
-  static const SkColor kResultsTableSelectedOrHoveredUrl =
-      SkColorSetARGB(0xff, 0x0b, 0x80, 0x43);
-  const SkColor kPositiveTextColor = SkColorSetRGB(0x0b, 0x80, 0x43);
-  const SkColor kNegativeTextColor = SkColorSetRGB(0xc5, 0x39, 0x29);
-  static const SkColor kResultsTablePositiveText = color_utils::AlphaBlend(
-      kPositiveTextColor, kTextfieldDefaultBackground, 0xDD);
-  static const SkColor kResultsTablePositiveHoveredText =
-      color_utils::AlphaBlend(kPositiveTextColor,
-                              kResultsTableHoveredBackground, 0xDD);
-  static const SkColor kResultsTablePositiveSelectedText =
-      color_utils::AlphaBlend(kPositiveTextColor,
-                              kTextfieldSelectionBackgroundFocused, 0xDD);
-  static const SkColor kResultsTableNegativeText = color_utils::AlphaBlend(
-      kNegativeTextColor, kTextfieldDefaultBackground, 0xDD);
-  static const SkColor kResultsTableNegativeHoveredText =
-      color_utils::AlphaBlend(kNegativeTextColor,
-                              kResultsTableHoveredBackground, 0xDD);
-  static const SkColor kResultsTableNegativeSelectedText =
-      color_utils::AlphaBlend(kNegativeTextColor,
-                              kTextfieldSelectionBackgroundFocused, 0xDD);
-
-  switch (color_id) {
-    // Windows
-    case kColorId_WindowBackground:
-      return kWindowBackgroundColor;
-
-    // Dialogs
-    case kColorId_DialogBackground:
-    case kColorId_BubbleBackground:
-      return kDialogBackgroundColor;
-
-    // FocusableBorder
-    case kColorId_FocusedBorderColor:
-      return kFocusedBorderColor;
-    case kColorId_UnfocusedBorderColor:
-      return kUnfocusedBorderColor;
-
-    // Button
-    case kColorId_ButtonBackgroundColor:
-      return kButtonBackgroundColor;
-    case kColorId_ButtonEnabledColor:
-      return kButtonEnabledColor;
-    case kColorId_ButtonHighlightColor:
-      return kButtonHighlightColor;
-    case kColorId_ButtonHoverColor:
-      return kButtonHoverColor;
-
-    // Label
-    case kColorId_LabelEnabledColor:
-      return kLabelEnabledColor;
-    case kColorId_LabelDisabledColor:
-      return GetSystemColor(kColorId_ButtonDisabledColor);
-    case kColorId_LabelBackgroundColor:
-      return kLabelBackgroundColor;
-
-    // Link
-    case kColorId_LinkDisabled:
-      if (ui::MaterialDesignController::IsModeMaterial())
-        break;
-      return kLinkDisabledColor;
-    case kColorId_LinkEnabled:
-      if (ui::MaterialDesignController::IsModeMaterial())
-        break;
-      return kLinkEnabledColor;
-    case kColorId_LinkPressed:
-      if (ui::MaterialDesignController::IsModeMaterial())
-        break;
-      return kLinkPressedColor;
-
-    // Textfield
-    case kColorId_TextfieldDefaultColor:
-      return kTextfieldDefaultColor;
-    case kColorId_TextfieldDefaultBackground:
-      return kTextfieldDefaultBackground;
-    case kColorId_TextfieldReadOnlyColor:
-      return kTextfieldReadOnlyColor;
-    case kColorId_TextfieldReadOnlyBackground:
-      return kTextfieldReadOnlyBackground;
-    case kColorId_TextfieldSelectionColor:
-      return kTextfieldSelectionColor;
-    case kColorId_TextfieldSelectionBackgroundFocused:
-      return kTextfieldSelectionBackgroundFocused;
-
-    // Tooltip
-    case kColorId_TooltipBackground:
-      return kTooltipBackground;
-    case kColorId_TooltipText:
-      return kTooltipTextColor;
-
-    // Tree
-    case kColorId_TreeBackground:
-      return kTreeBackground;
-    case kColorId_TreeText:
-      return kTreeTextColor;
-    case kColorId_TreeSelectedText:
-    case kColorId_TreeSelectedTextUnfocused:
-      return kTreeSelectedTextColor;
-    case kColorId_TreeSelectionBackgroundFocused:
-    case kColorId_TreeSelectionBackgroundUnfocused:
-      return kTreeSelectionBackgroundColor;
-    case kColorId_TreeArrow:
-      return kTreeArrowColor;
-
-    // Table
-    case kColorId_TableBackground:
-      return kTableBackground;
-    case kColorId_TableText:
-      return kTableTextColor;
-    case kColorId_TableSelectedText:
-    case kColorId_TableSelectedTextUnfocused:
-      return kTableSelectedTextColor;
-    case kColorId_TableSelectionBackgroundFocused:
-    case kColorId_TableSelectionBackgroundUnfocused:
-      return kTableSelectionBackgroundColor;
-    case kColorId_TableGroupingIndicatorColor:
-      return kTableGroupingIndicatorColor;
-
-    // Results Tables
-    case kColorId_ResultsTableNormalBackground:
-      return kTextfieldDefaultBackground;
-    case kColorId_ResultsTableHoveredBackground:
-      return kResultsTableHoveredBackground;
-    case kColorId_ResultsTableSelectedBackground:
-      return kResultsTableSelectedBackground;
-    case kColorId_ResultsTableNormalText:
-      return kResultsTableNormalText;
-    case kColorId_ResultsTableHoveredText:
-      return kResultsTableHoveredText;
-    case kColorId_ResultsTableSelectedText:
-      return kResultsTableSelectedText;
-    case kColorId_ResultsTableNormalDimmedText:
-    case kColorId_ResultsTableNormalHeadline:
-      return kResultsTableNormalDimmedText;
-    case kColorId_ResultsTableHoveredDimmedText:
-    case kColorId_ResultsTableHoveredHeadline:
-      return kResultsTableHoveredDimmedText;
-    case kColorId_ResultsTableSelectedDimmedText:
-    case kColorId_ResultsTableSelectedHeadline:
-      return kResultsTableSelectedDimmedText;
-    case kColorId_ResultsTableNormalUrl:
-      return kResultsTableNormalUrl;
-    case kColorId_ResultsTableHoveredUrl:
-    case kColorId_ResultsTableSelectedUrl:
-      return kResultsTableSelectedOrHoveredUrl;
-    case kColorId_ResultsTablePositiveText:
-      return kResultsTablePositiveText;
-    case kColorId_ResultsTablePositiveHoveredText:
-      return kResultsTablePositiveHoveredText;
-    case kColorId_ResultsTablePositiveSelectedText:
-      return kResultsTablePositiveSelectedText;
-    case kColorId_ResultsTableNegativeText:
-      return kResultsTableNegativeText;
-    case kColorId_ResultsTableNegativeHoveredText:
-      return kResultsTableNegativeHoveredText;
-    case kColorId_ResultsTableNegativeSelectedText:
-      return kResultsTableNegativeSelectedText;
-
-    default:
-      break;
-  }
-
-  SkColor color;
-  if (CommonThemeGetSystemColor(color_id, &color))
-    return color;
-
-  NOTREACHED();
-  return kInvalidColorIdColor;
+  return GetAuraColor(color_id, this);
 }
 
 void NativeThemeAura::PaintMenuPopupBackground(

ui/native_theme/native_theme_win.cc
@@ -24,6 +24,7 @@
 #include "third_party/skia/include/core/SkColorPriv.h"
 #include "third_party/skia/include/core/SkShader.h"
 #include "ui/base/resource/material_design/material_design_controller.h"
+#include "ui/gfx/color_palette.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/gdi_util.h"
 #include "ui/gfx/geometry/rect.h"
@@ -452,7 +453,6 @@ void NativeThemeWin::PaintDirect(SkCanvas* canvas,
 
 SkColor NativeThemeWin::GetSystemColor(ColorId color_id) const {
   // TODO: Obtain the correct colors using GetSysColor.
-  const SkColor kInvalidColorIdColor = SkColorSetRGB(255, 0, 128);
   const SkColor kUrlTextColor = SkColorSetRGB(0x0b, 0x80, 0x43);
   // Dialogs:
   const SkColor kDialogBackgroundColor = SkColorSetRGB(251, 251, 251);
@@ -463,7 +463,10 @@ SkColor NativeThemeWin::GetSystemColor(ColorId color_id) const {
   const SkColor kButtonBackgroundColor = SkColorSetRGB(0xde, 0xde, 0xde);
   const SkColor kButtonHighlightColor = SkColorSetARGB(200, 255, 255, 255);
   const SkColor kButtonHoverColor = SkColorSetRGB(6, 45, 117);
+  const SkColor kCallToActionColorInvert = gfx::kGoogleBlue300;
   // MenuItem:
+  const SkColor kMenuSchemeHighlightBackgroundColorInvert =
+      SkColorSetRGB(0x30, 0x30, 0x30);
   // Link:
   const SkColor kLinkPressedColor = SkColorSetRGB(200, 0, 0);
   // Table:
@@ -538,7 +541,7 @@ SkColor NativeThemeWin::GetSystemColor(ColorId color_id) const {
     case kColorId_TooltipBackground:
     case kColorId_TooltipText:
       NOTREACHED();
-      return kInvalidColorIdColor;
+      return gfx::kPlaceholderColor;
 
     // Tree
     // NOTE: these aren't right for all themes, but as close as I could get.
@@ -635,12 +638,18 @@ SkColor NativeThemeWin::GetSystemColor(ColorId color_id) const {
       break;
   }
 
-  SkColor color;
-  if (CommonThemeGetSystemColor(color_id, &color))
-    return color;
+  if (color_utils::IsInvertedColorScheme()) {
+    switch (color_id) {
+      case NativeTheme::kColorId_FocusedMenuItemBackgroundColor:
+        return kMenuSchemeHighlightBackgroundColorInvert;
+      case NativeTheme::kColorId_CallToActionColor:
+        return kCallToActionColorInvert;
+      default:
+        return color_utils::InvertColor(GetAuraColor(color_id, this));
+    }
+  }
 
-  NOTREACHED();
-  return kInvalidColorIdColor;
+  return GetAuraColor(color_id, this);
 }
 
 void NativeThemeWin::PaintIndirect(SkCanvas* canvas,

chrome/browser/download/download_shelf.cc
@@ -80,7 +80,7 @@ void DownloadShelf::PaintDownloadProgress(
   SkPaint bg_paint;
   bg_paint.setStyle(SkPaint::kFill_Style);
   SkColor indicator_color =
-      theme_provider.GetColor(ThemeProperties::COLOR_THROBBER_SPINNING);
+      theme_provider.GetColor(ThemeProperties::COLOR_TAB_THROBBER_SPINNING);
   bg_paint.setColor(SkColorSetA(indicator_color, 0x33));
   bg_paint.setAntiAlias(true);
   const SkScalar kCenterPoint = kProgressIndicatorSize / 2.f;

chrome/browser/themes/theme_properties.h
@@ -86,6 +86,13 @@ class ThemeProperties {
     // The color of a disabled toolbar button's icon.
     COLOR_TOOLBAR_BUTTON_ICON_INACTIVE,
 
+    // The throbber colors for tabs or anything on a toolbar (currently, only
+    // the download shelf). If you're adding a throbber elsewhere, such as in
+    // a dialog or bubble, you likely want
+    // NativeTheme::kColorId_ThrobberSpinningColor.
+    COLOR_TAB_THROBBER_SPINNING,
+    COLOR_TAB_THROBBER_WAITING,
+
     // These colors don't have constant default values. They are derived from
     // the runtime value of other colors.
     COLOR_NTP_SECTION_HEADER_TEXT,
@@ -93,8 +100,6 @@ class ThemeProperties {
     COLOR_NTP_SECTION_HEADER_RULE,
     COLOR_NTP_SECTION_HEADER_RULE_LIGHT,
     COLOR_NTP_TEXT_LIGHT,
-    COLOR_THROBBER_SPINNING,
-    COLOR_THROBBER_WAITING,
 #if defined(ENABLE_SUPERVISED_USERS)
     COLOR_SUPERVISED_USER_LABEL,
     COLOR_SUPERVISED_USER_LABEL_BACKGROUND,

chrome/browser/themes/theme_service.cc
@@ -240,15 +240,13 @@ SkColor ThemeService::GetColor(int id) const {
       return IncreaseLightness(GetColor(Properties::COLOR_NTP_TEXT), 0.86);
     case Properties::COLOR_NTP_TEXT_LIGHT:
       return IncreaseLightness(GetColor(Properties::COLOR_NTP_TEXT), 0.40);
-    case Properties::COLOR_THROBBER_SPINNING:
-    case Properties::COLOR_THROBBER_WAITING: {
-      SkColor base_color;
-      bool found_color = ui::CommonThemeGetSystemColor(
-          id == Properties::COLOR_THROBBER_SPINNING
-              ? ui::NativeTheme::kColorId_ThrobberSpinningColor
-              : ui::NativeTheme::kColorId_ThrobberWaitingColor,
-          &base_color);
-      DCHECK(found_color);
+    case Properties::COLOR_TAB_THROBBER_SPINNING:
+    case Properties::COLOR_TAB_THROBBER_WAITING: {
+      SkColor base_color =
+          ui::GetAuraColor(id == Properties::COLOR_TAB_THROBBER_SPINNING
+                               ? ui::NativeTheme::kColorId_ThrobberSpinningColor
+                               : ui::NativeTheme::kColorId_ThrobberWaitingColor,
+                           nullptr);
       color_utils::HSL hsl = GetTint(Properties::TINT_BUTTONS);
       return color_utils::HSLShift(base_color, hsl);
     }

chrome/browser/ui/cocoa/autofill/card_unmask_prompt_view_bridge.mm
@@ -5,7 +5,6 @@
 #include "base/bind.h"
 #include "base/message_loop/message_loop.h"
 #include "base/strings/sys_string_conversions.h"
-#include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/autofill/autofill_dialog_models.h"
 #include "chrome/browser/ui/autofill/autofill_dialog_types.h"
 #include "chrome/browser/ui/chrome_style.h"
@@ -29,7 +28,7 @@
 #import "ui/base/cocoa/controls/hyperlink_button_cell.h"
 #include "ui/base/cocoa/window_size_constants.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/base/theme_provider.h"
+#include "ui/native_theme/native_theme_mac.h"
 
 namespace {
 
@@ -659,11 +658,9 @@ void CardUnmaskPromptViewBridge::PerformClose() {
   [mainView addSubview:progressOverlayView_];
 
   progressOverlayLabel_.reset([constrained_window::CreateLabel() retain]);
-  NSView* webContentView = bridge_->GetWebContents()->GetNativeView();
-  ui::ThemeProvider* themeProvider = [[webContentView window] themeProvider];
-  DCHECK(themeProvider);
-  NSColor* throbberBlueColor = themeProvider->GetNSColor(
-      ThemeProperties::COLOR_THROBBER_SPINNING);
+  NSColor* throbberBlueColor = gfx::SkColorToCalibratedNSColor(
+      ui::NativeThemeMac::instance()->GetSystemColor(
+          ui::NativeTheme::kColorId_ThrobberSpinningColor));
   [progressOverlayLabel_ setTextColor:throbberBlueColor];
   [progressOverlayView_ addSubview:progressOverlayLabel_];
 

mirror/chromium

commit 24dbe1d08f46b2a24f352b0deb9d864addab8d86

Move theme part drawing functions only used on Windows to NativeThemeWin
Also use the platform-specific color in CommonThemePaintMenuItemBackground

This should fix a regression introduced by ff6ab32f0ee2963f

BUG=570698

Review URL: https://codereview.chromium.org/1797053002

ui/native_theme/common_theme.cc
@@ -437,30 +437,8 @@ void CommonThemePaintComboboxArrow(SkCanvas* canvas, const gfx::Rect& rect) {
   CommonThemeCreateCanvas(canvas)->DrawImageInt(*arrow, rect.x(), rect.y());
 }
 
-void CommonThemePaintMenuSeparator(SkCanvas* canvas, const gfx::Rect& rect) {
-  SkPaint paint;
-  paint.setColor(
-      GetAuraColor(NativeTheme::kColorId_MenuSeparatorColor, nullptr));
-  int position_y = rect.y() + rect.height() / 2;
-  canvas->drawLine(rect.x(), position_y, rect.right(), position_y, paint);
-}
-
-void CommonThemePaintMenuGutter(SkCanvas* canvas, const gfx::Rect& rect) {
-  SkPaint paint;
-  paint.setColor(
-      GetAuraColor(NativeTheme::kColorId_MenuSeparatorColor, nullptr));
-  int position_x = rect.x() + rect.width() / 2;
-  canvas->drawLine(position_x, rect.y(), position_x, rect.bottom(), paint);
-}
-
-void CommonThemePaintMenuBackground(SkCanvas* canvas, const gfx::Rect& rect) {
-  SkPaint paint;
-  paint.setColor(
-      GetAuraColor(NativeTheme::kColorId_MenuBackgroundColor, nullptr));
-  canvas->drawRect(gfx::RectToSkRect(rect), paint);
-}
-
 void CommonThemePaintMenuItemBackground(
+    const NativeTheme* theme,
     SkCanvas* canvas,
     NativeTheme::State state,
     const gfx::Rect& rect,
@@ -470,11 +448,11 @@ void CommonThemePaintMenuItemBackground(
     case NativeTheme::kNormal:
     case NativeTheme::kDisabled:
       paint.setColor(
-          GetAuraColor(NativeTheme::kColorId_MenuBackgroundColor, nullptr));
+          theme->GetSystemColor(NativeTheme::kColorId_MenuBackgroundColor));
       break;
     case NativeTheme::kHovered:
-      paint.setColor(GetAuraColor(
-          NativeTheme::kColorId_FocusedMenuItemBackgroundColor, nullptr));
+      paint.setColor(theme->GetSystemColor(
+          NativeTheme::kColorId_FocusedMenuItemBackgroundColor));
       break;
     default:
       NOTREACHED() << "Invalid state " << state;

ui/native_theme/common_theme.h
@@ -33,16 +33,8 @@ void NATIVE_THEME_EXPORT CommonThemePaintComboboxArrow(
     SkCanvas* canvas,
     const gfx::Rect& rect);
 
-void NATIVE_THEME_EXPORT
-    CommonThemePaintMenuSeparator(SkCanvas* canvas, const gfx::Rect& rect);
-
-void NATIVE_THEME_EXPORT CommonThemePaintMenuGutter(SkCanvas* canvas,
-                                                    const gfx::Rect& rect);
-
-void NATIVE_THEME_EXPORT CommonThemePaintMenuBackground(SkCanvas* canvas,
-                                                        const gfx::Rect& rect);
-
 void NATIVE_THEME_EXPORT CommonThemePaintMenuItemBackground(
+    const NativeTheme* theme,
     SkCanvas* canvas,
     NativeTheme::State state,
     const gfx::Rect& rect,

ui/native_theme/native_theme_aura.cc
@@ -131,7 +131,7 @@ void NativeThemeAura::PaintMenuItemBackground(
     State state,
     const gfx::Rect& rect,
     const MenuItemExtraParams& menu_item) const {
-  CommonThemePaintMenuItemBackground(canvas, state, rect, menu_item);
+  CommonThemePaintMenuItemBackground(this, canvas, state, rect, menu_item);
 }
 
 void NativeThemeAura::PaintArrowButton(SkCanvas* canvas,

ui/native_theme/native_theme_win.cc
@@ -30,6 +30,7 @@
 #include "ui/gfx/gdi_util.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_conversions.h"
+#include "ui/gfx/skia_util.h"
 #include "ui/gfx/win/dpi.h"
 #include "ui/native_theme/common_theme.h"
 
@@ -245,16 +246,17 @@ void NativeThemeWin::Paint(SkCanvas* canvas,
       CommonThemePaintComboboxArrow(canvas, rect);
       return;
     case kMenuPopupGutter:
-      CommonThemePaintMenuGutter(canvas, rect);
+      PaintMenuGutter(canvas, rect);
       return;
     case kMenuPopupSeparator:
-      CommonThemePaintMenuSeparator(canvas, rect);
+      PaintMenuSeparator(canvas, rect);
       return;
     case kMenuPopupBackground:
-      CommonThemePaintMenuBackground(canvas, rect);
+      PaintMenuBackground(canvas, rect);
       return;
     case kMenuItemBackground:
-      CommonThemePaintMenuItemBackground(canvas, state, rect, extra.menu_item);
+      CommonThemePaintMenuItemBackground(this, canvas, state, rect,
+                                         extra.menu_item);
       return;
     default:
       break;
@@ -356,6 +358,29 @@ void NativeThemeWin::OnSysColorChange() {
 void NativeThemeWin::UpdateSystemColors() {
   for (int kSystemColor : kSystemColors)
     system_colors_[kSystemColor] = color_utils::GetSysSkColor(kSystemColor);
+}
+
+void NativeThemeWin::PaintMenuSeparator(SkCanvas* canvas,
+                                        const gfx::Rect& rect) const {
+  SkPaint paint;
+  paint.setColor(GetSystemColor(NativeTheme::kColorId_MenuSeparatorColor));
+  int position_y = rect.y() + rect.height() / 2;
+  canvas->drawLine(rect.x(), position_y, rect.right(), position_y, paint);
+}
+
+void NativeThemeWin::PaintMenuGutter(SkCanvas* canvas,
+                                     const gfx::Rect& rect) const {
+  SkPaint paint;
+  paint.setColor(GetSystemColor(NativeTheme::kColorId_MenuSeparatorColor));
+  int position_x = rect.x() + rect.width() / 2;
+  canvas->drawLine(position_x, rect.y(), position_x, rect.bottom(), paint);
+}
+
+void NativeThemeWin::PaintMenuBackground(SkCanvas* canvas,
+                                         const gfx::Rect& rect) const {
+  SkPaint paint;
+  paint.setColor(GetSystemColor(NativeTheme::kColorId_MenuBackgroundColor));
+  canvas->drawRect(gfx::RectToSkRect(rect), paint);
 }
 
 void NativeThemeWin::PaintDirect(SkCanvas* canvas,

ui/native_theme/native_theme_win.h
@@ -125,6 +125,11 @@ class NATIVE_THEME_EXPORT NativeThemeWin : public NativeTheme,
   // Update the locally cached set of system colors.
   void UpdateSystemColors();
 
+  // Painting functions that paint to SkCanvas.
+  void PaintMenuSeparator(SkCanvas* canvas, const gfx::Rect& rect) const;
+  void PaintMenuGutter(SkCanvas* canvas, const gfx::Rect& rect) const;
+  void PaintMenuBackground(SkCanvas* canvas, const gfx::Rect& rect) const;
+
   // Paint directly to canvas' HDC.
   void PaintDirect(SkCanvas* canvas,
                    Part part,