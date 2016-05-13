crosswalk-project/chromium-crosswalk

commit 1bef485187d2a5eff4fd471cbb0ec97e61759811

Don't duplicate ResourceRequests and ResourceLoaderOptions on ResourceLoader
Resource already has copies.

BUG=

Review URL: https://codereview.chromium.org/1757633005

Cr-Commit-Position: refs/heads/master@{#380466}

third_party/WebKit/Source/core/fetch/RawResource.cpp
@@ -138,14 +138,6 @@ void RawResource::willFollowRedirect(ResourceRequest& newRequest, const Resource
     Resource::willFollowRedirect(newRequest, redirectResponse);
 }
 
-void RawResource::updateRequest(const ResourceRequest& request)
-{
-    RefPtrWillBeRawPtr<RawResource> protect(this);
-    ResourceClientWalker<RawResourceClient> w(m_clients);
-    while (RawResourceClient* c = w.next())
-        c->updateRequest(this, request);
-}
-
 void RawResource::responseReceived(const ResourceResponse& response, PassOwnPtr<WebDataConsumerHandle> handle)
 {
     RefPtrWillBeRawPtr<RawResource> protect(this);

third_party/WebKit/LayoutTests/http/tests/loading/307-after-303-after-post-expected.txt
@@ -5,10 +5,10 @@ main frame - didHandleOnloadEventsForFrame
 main frame - didFinishLoadForFrame
 main frame - didStartProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/post-to-303-target.php - willSendRequest <NSURLRequest URL http://127.0.0.1:8000/loading/resources/post-to-303-target.php, main document URL http://127.0.0.1:8000/loading/resources/post-to-303-target.php, http method POST> redirectResponse (null)
-main frame - didReceiveServerRedirectForProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/post-to-303-target.php - willSendRequest <NSURLRequest URL http://127.0.0.1:8000/loading/resources/303-to-307-target.php, main document URL http://127.0.0.1:8000/loading/resources/303-to-307-target.php, http method GET> redirectResponse <NSURLResponse http://127.0.0.1:8000/loading/resources/post-to-303-target.php, http status code 303>
 main frame - didReceiveServerRedirectForProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/post-to-303-target.php - willSendRequest <NSURLRequest URL http://127.0.0.1:8000/loading/resources/307-post-output-target.php, main document URL http://127.0.0.1:8000/loading/resources/307-post-output-target.php, http method GET> redirectResponse <NSURLResponse http://127.0.0.1:8000/loading/resources/303-to-307-target.php, http status code 307>
+main frame - didReceiveServerRedirectForProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/post-to-303-target.php - didReceiveResponse <NSURLResponse http://127.0.0.1:8000/loading/resources/307-post-output-target.php, http status code 200>
 main frame - didCommitLoadForFrame
 http://127.0.0.1:8000/loading/resources/post-to-303-target.php - didFinishLoading

third_party/WebKit/LayoutTests/http/tests/loading/redirect-methods-expected.txt
@@ -18,8 +18,8 @@ frame "0" - didHandleOnloadEventsForFrame
 frame "0" - didFinishLoadForFrame
 frame "0" - didStartProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - willSendRequest <NSURLRequest URL http://127.0.0.1:8000/loading/resources/redirect-methods-result.php, main document URL http://127.0.0.1:8000/loading/redirect-methods.html, http method POST> redirectResponse (null)
-frame "0" - didReceiveServerRedirectForProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - willSendRequest <NSURLRequest URL http://127.0.0.1:8000/loading/resources/redirect-methods-result.php?redirected=true, main document URL http://127.0.0.1:8000/loading/redirect-methods.html, http method GET> redirectResponse <NSURLResponse http://127.0.0.1:8000/loading/resources/redirect-methods-result.php, http status code 301>
+frame "0" - didReceiveServerRedirectForProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - didReceiveResponse <NSURLResponse http://127.0.0.1:8000/loading/resources/redirect-methods-result.php?redirected=true, http status code 200>
 frame "0" - didCommitLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - didFinishLoading
@@ -41,8 +41,8 @@ frame "1" - didHandleOnloadEventsForFrame
 frame "1" - didFinishLoadForFrame
 frame "1" - didStartProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - willSendRequest <NSURLRequest URL http://127.0.0.1:8000/loading/resources/redirect-methods-result.php, main document URL http://127.0.0.1:8000/loading/redirect-methods.html, http method POST> redirectResponse (null)
-frame "1" - didReceiveServerRedirectForProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - willSendRequest <NSURLRequest URL http://127.0.0.1:8000/loading/resources/redirect-methods-result.php?redirected=true, main document URL http://127.0.0.1:8000/loading/redirect-methods.html, http method GET> redirectResponse <NSURLResponse http://127.0.0.1:8000/loading/resources/redirect-methods-result.php, http status code 302>
+frame "1" - didReceiveServerRedirectForProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - didReceiveResponse <NSURLResponse http://127.0.0.1:8000/loading/resources/redirect-methods-result.php?redirected=true, http status code 200>
 frame "1" - didCommitLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - didFinishLoading
@@ -64,8 +64,8 @@ frame "2" - didHandleOnloadEventsForFrame
 frame "2" - didFinishLoadForFrame
 frame "2" - didStartProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - willSendRequest <NSURLRequest URL http://127.0.0.1:8000/loading/resources/redirect-methods-result.php, main document URL http://127.0.0.1:8000/loading/redirect-methods.html, http method POST> redirectResponse (null)
-frame "2" - didReceiveServerRedirectForProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - willSendRequest <NSURLRequest URL http://127.0.0.1:8000/loading/resources/redirect-methods-result.php?redirected=true, main document URL http://127.0.0.1:8000/loading/redirect-methods.html, http method GET> redirectResponse <NSURLResponse http://127.0.0.1:8000/loading/resources/redirect-methods-result.php, http status code 303>
+frame "2" - didReceiveServerRedirectForProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - didReceiveResponse <NSURLResponse http://127.0.0.1:8000/loading/resources/redirect-methods-result.php?redirected=true, http status code 200>
 frame "2" - didCommitLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - didFinishLoading
@@ -87,8 +87,8 @@ frame "3" - didHandleOnloadEventsForFrame
 frame "3" - didFinishLoadForFrame
 frame "3" - didStartProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - willSendRequest <NSURLRequest URL http://127.0.0.1:8000/loading/resources/redirect-methods-result.php, main document URL http://127.0.0.1:8000/loading/redirect-methods.html, http method POST> redirectResponse (null)
-frame "3" - didReceiveServerRedirectForProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - willSendRequest <NSURLRequest URL http://127.0.0.1:8000/loading/resources/redirect-methods-result.php?redirected=true, main document URL http://127.0.0.1:8000/loading/redirect-methods.html, http method POST> redirectResponse <NSURLResponse http://127.0.0.1:8000/loading/resources/redirect-methods-result.php, http status code 307>
+frame "3" - didReceiveServerRedirectForProvisionalLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - didReceiveResponse <NSURLResponse http://127.0.0.1:8000/loading/resources/redirect-methods-result.php?redirected=true, http status code 200>
 frame "3" - didCommitLoadForFrame
 http://127.0.0.1:8000/loading/resources/redirect-methods-result.php - didFinishLoading

third_party/WebKit/Source/core/fetch/RawResource.h
@@ -80,7 +80,6 @@ private:
     bool shouldIgnoreHTTPStatusCodeErrors() const override { return !isLinkPreload(); }
 
     void willFollowRedirect(ResourceRequest&, const ResourceResponse&) override;
-    void updateRequest(const ResourceRequest&) override;
     void responseReceived(const ResourceResponse&, PassOwnPtr<WebDataConsumerHandle>) override;
     void setSerializedCachedMetadata(const char*, size_t) override;
     void didSendData(unsigned long long bytesSent, unsigned long long totalBytesToBeSent) override;
@@ -112,7 +111,6 @@ public:
     virtual void setSerializedCachedMetadata(Resource*, const char*, size_t) { }
     virtual void dataReceived(Resource*, const char* /* data */, size_t /* length */) { }
     virtual void redirectReceived(Resource*, ResourceRequest&, const ResourceResponse&) { }
-    virtual void updateRequest(Resource*, const ResourceRequest&) { }
     virtual void dataDownloaded(Resource*, int) { }
     virtual void didReceiveResourceTiming(Resource*, const ResourceTimingInfo&) { }
 };

third_party/WebKit/Source/core/fetch/Resource.cpp
@@ -201,30 +201,32 @@ DEFINE_TRACE(Resource)
 
 void Resource::load(ResourceFetcher* fetcher, const ResourceLoaderOptions& options)
 {
+    RELEASE_ASSERT(!m_loader);
     m_options = options;
     m_loading = true;
+    m_status = Pending;
 
-    ResourceRequest request(m_revalidatingRequest.isNull() ? m_resourceRequest : m_revalidatingRequest);
+    ResourceRequest& request(m_revalidatingRequest.isNull() ? m_resourceRequest : m_revalidatingRequest);
     if (!accept().isEmpty())
         request.setHTTPAccept(accept());
+    request.setAllowStoredCredentials(m_options.allowCredentials == AllowStoredCredentials);
 
     // FIXME: It's unfortunate that the cache layer and below get to know anything about fragment identifiers.
     // We should look into removing the expectation of that knowledge from the platform network stacks.
+    KURL urlWithoutFragment = request.url();
     if (!m_fragmentIdentifierForRequest.isNull()) {
         KURL url = request.url();
         url.setFragmentIdentifier(m_fragmentIdentifierForRequest);
         request.setURL(url);
         m_fragmentIdentifierForRequest = String();
     }
-    m_status = Pending;
-    if (m_loader) {
-        ASSERT(m_revalidatingRequest.isNull());
-        RELEASE_ASSERT(m_options.synchronousPolicy == RequestSynchronously);
-        m_loader->changeToSynchronous();
-        return;
-    }
-    m_loader = ResourceLoader::create(fetcher, this, request, options);
-    m_loader->start();
+
+    m_loader = ResourceLoader::create(fetcher, this);
+    m_loader->start(request);
+    // If the request reference is null (i.e., a synchronous revalidation will
+    // null the request), don't make the request non-null by setting the url.
+    if (!request.isNull())
+        request.setURL(urlWithoutFragment);
 }
 
 void Resource::checkNotify()
@@ -424,6 +426,7 @@ const ResourceRequest& Resource::lastResourceRequest() const
 
 void Resource::willFollowRedirect(ResourceRequest& newRequest, const ResourceResponse& redirectResponse)
 {
+    newRequest.setAllowStoredCredentials(m_options.allowCredentials == AllowStoredCredentials);
     m_redirectChain.append(RedirectPair(newRequest, redirectResponse));
     m_requestedFromNetworkingLayer = true;
 }
@@ -835,6 +838,7 @@ void Resource::revalidationFailed()
 {
     m_resourceRequest = m_revalidatingRequest;
     m_revalidatingRequest = ResourceRequest();
+    m_redirectChain.clear();
     m_data.clear();
     m_cachedMetadata.clear();
     destroyDecodedDataForFailedRevalidation();

third_party/WebKit/Source/core/fetch/Resource.h
@@ -123,6 +123,7 @@ public:
     const KURL& url() const { return m_resourceRequest.url();}
     Type getType() const { return static_cast<Type>(m_type); }
     const ResourceLoaderOptions& options() const { return m_options; }
+    ResourceLoaderOptions& mutableOptions() { return m_options; }
     void setOptions(const ResourceLoaderOptions& options) { m_options = options; }
 
     void didChangePriority(ResourceLoadPriority, int intraPriorityValue);
@@ -184,7 +185,6 @@ public:
 
     virtual void willFollowRedirect(ResourceRequest&, const ResourceResponse&);
 
-    virtual void updateRequest(const ResourceRequest&) { }
     virtual void responseReceived(const ResourceResponse&, PassOwnPtr<WebDataConsumerHandle>);
     void setResponse(const ResourceResponse& response) { m_response = response; }
     const ResourceResponse& response() const { return m_response; }

third_party/WebKit/Source/core/fetch/ResourceFetcher.cpp
@@ -262,11 +262,7 @@ bool ResourceFetcher::resourceNeedsLoad(Resource* resource, const FetchRequest& 
 {
     if (FetchRequest::DeferredByClient == request.defer())
         return false;
-    if (policy != Use)
-        return true;
-    if (resource->stillNeedsLoad())
-        return true;
-    return request.options().synchronousPolicy == RequestSynchronously && resource->isLoading();
+    return policy != Use || resource->stillNeedsLoad();
 }
 
 // Limit the number of URLs in m_validatedURLs to avoid memory bloat.
@@ -689,6 +685,10 @@ ResourceFetcher::RevalidationPolicy ResourceFetcher::determineRevalidationPolicy
     if (request.isConditional())
         return Reload;
 
+    // Don't try to reuse an in-progress async request for a new sync request.
+    if (fetchRequest.options().synchronousPolicy == RequestSynchronously && existingResource->isLoading())
+        return Reload;
+
     // Don't reload resources while pasting.
     if (m_allowStaleResources)
         return Use;
@@ -838,13 +838,6 @@ void ResourceFetcher::reloadImagesIfNotDeferred()
     }
 }
 
-void ResourceFetcher::redirectReceived(Resource* resource, const ResourceResponse& redirectResponse)
-{
-    ResourceTimingInfoMap::iterator it = m_resourceTimingInfoMap.find(resource);
-    if (it != m_resourceTimingInfoMap.end())
-        it->value->addRedirect(redirectResponse);
-}
-
 void ResourceFetcher::didLoadResource(Resource* resource)
 {
     context().didLoadResource(resource);
@@ -958,11 +951,6 @@ void ResourceFetcher::didFailLoading(const Resource* resource, const ResourceErr
     context().dispatchDidFail(resource->identifier(), error, isInternalRequest);
 }
 
-void ResourceFetcher::willSendRequest(unsigned long identifier, ResourceRequest& request, const ResourceResponse& redirectResponse, const FetchInitiatorInfo& initiatorInfo)
-{
-    context().dispatchWillSendRequest(identifier, request, redirectResponse, initiatorInfo);
-}
-
 void ResourceFetcher::didReceiveResponse(const Resource* resource, const ResourceResponse& response)
 {
     // If the response is fetched via ServiceWorker, the original URL of the response could be different from the URL of the request.
@@ -1004,7 +992,7 @@ void ResourceFetcher::subresourceLoaderFinishedLoadingOnePart(ResourceLoader* lo
     didLoadResource(loader->cachedResource());
 }
 
-void ResourceFetcher::didInitializeResourceLoader(ResourceLoader* loader)
+void ResourceFetcher::willStartLoadingResource(Resource* resource, ResourceLoader* loader, ResourceRequest& request)
 {
     if (loader->cachedResource()->shouldBlockLoadEvent()) {
         if (!m_loaders)
@@ -1015,6 +1003,12 @@ void ResourceFetcher::didInitializeResourceLoader(ResourceLoader* loader)
             m_nonBlockingLoaders = ResourceLoaderSet::create();
         m_nonBlockingLoaders->add(loader);
     }
+
+    context().willStartLoadingResource(request);
+    storeResourceTimingInitiatorInformation(resource);
+    TRACE_EVENT_ASYNC_BEGIN2("blink.net", "Resource", resource, "url", resource->url().getString().ascii(), "priority", resource->resourceRequest().priority());
+
+    context().dispatchWillSendRequest(resource->identifier(), request, ResourceResponse(), resource->options().initiatorInfo);
 }
 
 void ResourceFetcher::willTerminateResourceLoader(ResourceLoader* loader)
@@ -1025,13 +1019,6 @@ void ResourceFetcher::willTerminateResourceLoader(ResourceLoader* loader)
         m_nonBlockingLoaders->remove(loader);
     else
         ASSERT_NOT_REACHED();
-}
-
-void ResourceFetcher::willStartLoadingResource(Resource* resource, ResourceRequest& request)
-{
-    context().willStartLoadingResource(request);
-    storeResourceTimingInitiatorInformation(resource);
-    TRACE_EVENT_ASYNC_BEGIN2("blink.net", "Resource", resource, "url", resource->url().getString().ascii(), "priority", resource->resourceRequest().priority());
 }
 
 void ResourceFetcher::stopFetching()
@@ -1060,25 +1047,37 @@ bool ResourceFetcher::defersLoading() const
     return context().defersLoading();
 }
 
-bool ResourceFetcher::canAccessRedirect(Resource* resource, ResourceRequest& newRequest, const ResourceResponse& redirectResponse, ResourceLoaderOptions& options)
+static bool isManualRedirectFetchRequest(const ResourceRequest& request)
 {
-    if (!context().canRequest(resource->getType(), newRequest, newRequest.url(), options, resource->isUnusedPreload(), FetchRequest::UseDefaultOriginRestrictionForType))
-        return false;
-    if (options.corsEnabled == IsCORSEnabled) {
-        SecurityOrigin* sourceOrigin = options.securityOrigin.get();
-        if (!sourceOrigin)
-            sourceOrigin = context().getSecurityOrigin();
+    return request.fetchRedirectMode() == WebURLRequest::FetchRedirectModeManual && request.requestContext() == WebURLRequest::RequestContextFetch;
+}
 
-        String errorMessage;
-        StoredCredentials withCredentials = resource->lastResourceRequest().allowStoredCredentials() ? AllowStoredCredentials : DoNotAllowStoredCredentials;
-        if (!CrossOriginAccessControl::handleRedirect(sourceOrigin, newRequest, redirectResponse, withCredentials, options, errorMessage)) {
-            resource->setCORSFailed();
-            context().addConsoleMessage(errorMessage);
+bool ResourceFetcher::willFollowRedirect(Resource* resource, ResourceRequest& newRequest, const ResourceResponse& redirectResponse)
+{
+    if (!isManualRedirectFetchRequest(resource->resourceRequest())) {
+        if (!context().canRequest(resource->getType(), newRequest, newRequest.url(), resource->options(), resource->isUnusedPreload(), FetchRequest::UseDefaultOriginRestrictionForType))
             return false;
+        if (resource->options().corsEnabled == IsCORSEnabled) {
+            SecurityOrigin* sourceOrigin = resource->options().securityOrigin.get();
+            if (!sourceOrigin)
+                sourceOrigin = context().getSecurityOrigin();
+
+            String errorMessage;
+            StoredCredentials withCredentials = resource->lastResourceRequest().allowStoredCredentials() ? AllowStoredCredentials : DoNotAllowStoredCredentials;
+            if (!CrossOriginAccessControl::handleRedirect(sourceOrigin, newRequest, redirectResponse, withCredentials, resource->mutableOptions(), errorMessage)) {
+                resource->setCORSFailed();
+                context().addConsoleMessage(errorMessage);
+                return false;
+            }
         }
+        if (resource->getType() == Resource::Image && shouldDeferImageLoad(newRequest.url()))
+            return false;
     }
-    if (resource->getType() == Resource::Image && shouldDeferImageLoad(newRequest.url()))
-        return false;
+
+    ResourceTimingInfoMap::iterator it = m_resourceTimingInfoMap.find(resource);
+    if (it != m_resourceTimingInfoMap.end())
+        it->value->addRedirect(redirectResponse);
+    context().dispatchWillSendRequest(resource->identifier(), newRequest, redirectResponse, resource->options().initiatorInfo);
     return true;
 }
 

third_party/WebKit/Source/core/fetch/ResourceFetcher.h
@@ -107,23 +107,20 @@ public:
     bool isFetching() const;
 
     void didLoadResource(Resource*);
-    void redirectReceived(Resource*, const ResourceResponse&);
+    bool willFollowRedirect(Resource*, ResourceRequest&, const ResourceResponse&);
     void didFinishLoading(Resource*, double finishTime, int64_t encodedDataLength);
     void didFailLoading(const Resource*, const ResourceError&);
-    void willSendRequest(unsigned long identifier, ResourceRequest&, const ResourceResponse& redirectResponse, const FetchInitiatorInfo&);
     void didReceiveResponse(const Resource*, const ResourceResponse&);
     void didReceiveData(const Resource*, const char* data, int dataLength, int encodedDataLength);
     void didDownloadData(const Resource*, int dataLength, int encodedDataLength);
     void subresourceLoaderFinishedLoadingOnePart(ResourceLoader*);
-    void didInitializeResourceLoader(ResourceLoader*);
-    void willStartLoadingResource(Resource*, ResourceRequest&);
+    void willStartLoadingResource(Resource*, ResourceLoader*, ResourceRequest&);
     bool defersLoading() const;
 
     enum AccessControlLoggingDecision {
         ShouldLogAccessControlErrors,
         ShouldNotLogAccessControlErrors
     };
-    bool canAccessRedirect(Resource*, ResourceRequest&, const ResourceResponse&, ResourceLoaderOptions&);
     bool canAccessResource(Resource*, SecurityOrigin*, const KURL&, AccessControlLoggingDecision) const;
     bool isControlledByServiceWorker() const;
 

mirror/chromium

commit fe339520b9ae0a580a162b328fae7445cecd6821

X-DevTools-Emulate-Network-Conditions-Client-Id header shouldn't block cache reuse
This was regressed in https://chromium.googlesource.com/chromium/src/+/1bef485187d2a5eff4fd471cbb0ec97e61759811,
which caused the header to be present on Resource's
m_resourceRequest, where previously it was added to a copy
of that object.

BUG=603118
TEST=RawResourceTest.CanReuseDevToolsEmulateNetworkConditionsClientIdHeader

Review-Url: https://codereview.chromium.org/1972923004

chrome/browser/devtools/devtools_network_transaction.cc
@@ -17,8 +17,8 @@
 #include "net/http/http_request_info.h"
 #include "net/socket/connection_attempts.h"
 
-// Keep in sync with kDevToolsEmulateNetworkConditionsClientId defined in
-// InspectorResourceAgent.cpp.
+// Keep in sync with X_DevTools_Emulate_Network_Conditions_Client_Id defined in
+// HTTPNames.in.
 const char
     DevToolsNetworkTransaction::kDevToolsEmulateNetworkConditionsClientId[] =
         "X-DevTools-Emulate-Network-Conditions-Client-Id";

third_party/WebKit/Source/core/fetch/FetchUtils.cpp
@@ -4,7 +4,7 @@
 
 #include "core/fetch/FetchUtils.h"
 
-#include "core/inspector/InspectorInstrumentation.h"
+#include "platform/HTTPNames.h"
 #include "platform/network/HTTPHeaderMap.h"
 #include "platform/network/HTTPParsers.h"
 #include "wtf/HashSet.h"
@@ -99,7 +99,7 @@ bool FetchUtils::isSimpleHeader(const AtomicString& name, const AtomicString& va
     if (equalIgnoringCase(name, "accept")
         || equalIgnoringCase(name, "accept-language")
         || equalIgnoringCase(name, "content-language")
-        || equalIgnoringCase(name, InspectorInstrumentation::kInspectorEmulateNetworkConditionsClientId)
+        || equalIgnoringCase(name, HTTPNames::X_DevTools_Emulate_Network_Conditions_Client_Id)
         || equalIgnoringCase(name, "save-data"))
         return true;
 

third_party/WebKit/Source/core/fetch/RawResource.cpp
@@ -30,6 +30,7 @@
 #include "core/fetch/ResourceClientOrObserverWalker.h"
 #include "core/fetch/ResourceFetcher.h"
 #include "core/fetch/ResourceLoader.h"
+#include "platform/HTTPNames.h"
 
 namespace blink {
 
@@ -212,6 +213,7 @@ static bool shouldIgnoreHeaderForCacheReuse(AtomicString headerName)
         m_headers.add("Purpose");
         m_headers.add("Referer");
         m_headers.add("User-Agent");
+        m_headers.add(HTTPNames::X_DevTools_Emulate_Network_Conditions_Client_Id);
     }
     return m_headers.contains(headerName);
 }

third_party/WebKit/Source/core/inspector/InspectorInstrumentation.cpp
@@ -163,9 +163,6 @@ JavaScriptDialog::~JavaScriptDialog()
 
 int FrontendCounter::s_frontendCounter = 0;
 
-// Keep in sync with kDevToolsRequestInitiator defined in devtools_network_controller.cc
-const char kInspectorEmulateNetworkConditionsClientId[] = "X-DevTools-Emulate-Network-Conditions-Client-Id";
-
 bool isDebuggerPaused(LocalFrame*)
 {
     return MainThreadDebugger::instance()->debugger()->isPaused();

third_party/WebKit/Source/core/inspector/InspectorInstrumentation.h
@@ -104,8 +104,6 @@ inline bool hasFrontends() { return acquireLoad(&FrontendCounter::s_frontendCoun
 CORE_EXPORT void registerInstrumentingAgents(InstrumentingAgents*);
 CORE_EXPORT void unregisterInstrumentingAgents(InstrumentingAgents*);
 
-CORE_EXPORT extern const char kInspectorEmulateNetworkConditionsClientId[];
-
 // Called from generated instrumentation code.
 CORE_EXPORT InstrumentingAgents* instrumentingAgentsFor(WorkerGlobalScope*);
 CORE_EXPORT InstrumentingAgents* instrumentingAgentsForNonDocumentContext(ExecutionContext*);

third_party/WebKit/Source/core/inspector/InspectorResourceAgent.cpp
@@ -537,7 +537,7 @@ void InspectorResourceAgent::willSendRequest(LocalFrame* frame, unsigned long id
     willSendRequestInternal(frame, identifier, loader, request, redirectResponse, initiatorInfo);
 
     if (!m_hostId.isEmpty())
-        request.addHTTPHeaderField(InspectorInstrumentation::kInspectorEmulateNetworkConditionsClientId, AtomicString(m_hostId));
+        request.addHTTPHeaderField(HTTPNames::X_DevTools_Emulate_Network_Conditions_Client_Id, AtomicString(m_hostId));
 }
 
 void InspectorResourceAgent::markResourceAsCached(unsigned long identifier)

third_party/WebKit/Source/platform/network/HTTPNames.in
@@ -53,6 +53,7 @@ User-Agent
 Vary
 X-Content-Type-Options
 X-DNS-Prefetch-Control
+X-DevTools-Emulate-Network-Conditions-Client-Id
 X-Frame-Options
 X-SourceMap
 X-XSS-Protection

third_party/WebKit/Source/core/fetch/RawResourceTest.cpp
@@ -291,4 +291,12 @@ TEST(RawResourceTest, RemoveClientDuringCallback)
     EXPECT_FALSE(raw->hasClientsOrObservers());
 }
 
+TEST(RawResourceTest, CanReuseDevToolsEmulateNetworkConditionsClientIdHeader)
+{
+    ResourceRequest request("data:text/html,");
+    request.setHTTPHeaderField(HTTPNames::X_DevTools_Emulate_Network_Conditions_Client_Id, "Foo");
+    Resource* raw = RawResource::create(request, Resource::Raw);
+    EXPECT_TRUE(raw->canReuse(ResourceRequest("data:text/html,")));
+}
+
 } // namespace blink