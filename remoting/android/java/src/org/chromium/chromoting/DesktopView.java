crosswalk-project/chromium-crosswalk

commit 06d70696de0b8fac307927064bd36be2d486fe2a

[chromoting] Use Event to render feedback animations.
This change moves FeedbackAnimator into a standalone class, with a startAnimation static function to register a SelfRemovableParameterRunnable into onPaint event of DesktopView. After animation finishes, the SelfRemovableParameterRunnable will be removed from onPaint event automatically.

This change is part of Chromoting Android Client Refactor work. A design doc can
be found at https://goo.gl/MA6zjx.

BUG=615277

Review-Url: https://codereview.chromium.org/2023133002
Cr-Commit-Position: refs/heads/master@{#397619}

remoting/android/java/src/org/chromium/chromoting/DesktopView.java
@@ -10,9 +10,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
-import android.graphics.RadialGradient;
 import android.graphics.Rect;
-import android.graphics.Shader;
 import android.os.Looper;
 import android.os.SystemClock;
 import android.text.InputType;
@@ -60,102 +58,7 @@ public class DesktopView extends SurfaceView implements DesktopViewInterface,
     // synchronized on |mRenderData|.
     private boolean mSurfaceCreated = false;
 
-    /** Helper class for displaying the long-press feedback animation. This class is thread-safe. */
-    private static class FeedbackAnimator {
-        /** Total duration of the animation, in milliseconds. */
-        private static final float TOTAL_DURATION_MS = 220;
-
-        /** Start time of the animation, from {@link SystemClock#uptimeMillis()}. */
-        private long mStartTimeInMs = 0;
-
-        private boolean mRunning = false;
-
-        /** Contains the size of the feedback animation for the most recent request. */
-        private float mFeedbackSizeInPixels;
-
-        /** Lock to allow multithreaded access to {@link #mStartTimeInMs} and {@link #mRunning}. */
-        private final Object mLock = new Object();
-
-        private Paint mPaint = new Paint();
-
-        public boolean isAnimationRunning() {
-            synchronized (mLock) {
-                return mRunning;
-            }
-        }
-
-        /**
-         * Begins a new animation sequence. After calling this method, the caller should
-         * call {@link #render(Canvas, float, float, float)} periodically whilst
-         * {@link #isAnimationRunning()} returns true.
-         */
-        public void startAnimation(InputFeedbackType feedbackType) {
-            if (feedbackType == InputFeedbackType.NONE) {
-                return;
-            }
-
-            synchronized (mLock) {
-                mRunning = true;
-                mStartTimeInMs = SystemClock.uptimeMillis();
-                mFeedbackSizeInPixels = getInputFeedbackSizeInPixels(feedbackType);
-            }
-        }
-
-        public void render(Canvas canvas, float x, float y, float scale) {
-            // |progress| is 0 at the beginning, 1 at the end.
-            float progress;
-            float size;
-            synchronized (mLock) {
-                // |mStartTimeInMs| is set and accessed on different threads (hence the lock).  It
-                // is possible for |mStartTimeInMs| to be updated when an animation is in progress.
-                // When this occurs, |radius| will eventually be set to 0 and used to initialize
-                // RadialGradient which requires the radius to be > 0.  This will result in a crash.
-                // In order to avoid this problem, we return early if the elapsed time is 0.
-                float elapsedTimeInMs = SystemClock.uptimeMillis() - mStartTimeInMs;
-                if (elapsedTimeInMs < 1) {
-                    return;
-                }
-
-                progress = elapsedTimeInMs / TOTAL_DURATION_MS;
-                if (progress >= 1) {
-                    mRunning = false;
-                    return;
-                }
-                size = mFeedbackSizeInPixels / scale;
-            }
-
-            // Animation grows from 0 to |size|, and goes from fully opaque to transparent for a
-            // seamless fading-out effect. The animation needs to have more than one color so it's
-            // visible over any background color.
-            float radius = size * progress;
-            int alpha = (int) ((1 - progress) * 0xff);
-
-            int transparentBlack = Color.argb(0, 0, 0, 0);
-            int white = Color.argb(alpha, 0xff, 0xff, 0xff);
-            int black = Color.argb(alpha, 0, 0, 0);
-            mPaint.setShader(new RadialGradient(x, y, radius,
-                    new int[] {transparentBlack, white, black, transparentBlack},
-                    new float[] {0.0f, 0.8f, 0.9f, 1.0f}, Shader.TileMode.CLAMP));
-            canvas.drawCircle(x, y, radius, mPaint);
-        }
-
-        private float getInputFeedbackSizeInPixels(InputFeedbackType feedbackType) {
-            switch (feedbackType) {
-                case SMALL_ANIMATION:
-                    return 40.0f;
-
-                case LARGE_ANIMATION:
-                    return 160.0f;
-
-                default:
-                    // Unreachable, but required by Google Java style and findbugs.
-                    assert false : "Unreached";
-                    return 0.0f;
-            }
-        }
-    }
-
-    private FeedbackAnimator mFeedbackAnimator = new FeedbackAnimator();
+    private final Event.Raisable<PaintEventParameter> mOnPaint = new Event.Raisable<>();
 
     // Variables to control animation by the TouchInputHandler.
 
@@ -177,6 +80,10 @@ public class DesktopView extends SurfaceView implements DesktopViewInterface,
         mRepaintPending = false;
 
         getHolder().addCallback(this);
+    }
+
+    public Event<PaintEventParameter> onPaint() {
+        return mOnPaint;
     }
 
     public void setDesktop(Desktop desktop) {
@@ -263,15 +170,11 @@ public class DesktopView extends SurfaceView implements DesktopViewInterface,
         canvas.drawColor(Color.BLACK);
         canvas.drawBitmap(image, 0, 0, new Paint());
 
-        // TODO(joedow): Replace the custom animation code with a standard Android implementation.
-        boolean feedbackAnimationRunning = mFeedbackAnimator.isAnimationRunning();
-        if (feedbackAnimationRunning) {
-            float scaleFactor;
-            synchronized (mRenderData) {
-                scaleFactor = mRenderData.transform.mapRadius(1);
-            }
-            mFeedbackAnimator.render(canvas, cursorPosition.x, cursorPosition.y, scaleFactor);
+        float scaleFactor;
+        synchronized (mRenderData) {
+            scaleFactor = mRenderData.transform.mapRadius(1);
         }
+        mOnPaint.raise(new PaintEventParameter(cursorPosition, canvas, scaleFactor));
 
         if (drawCursor) {
             Bitmap cursorBitmap = mClient.getDisplay().getCursorBitmap();
@@ -284,14 +187,18 @@ public class DesktopView extends SurfaceView implements DesktopViewInterface,
 
         getHolder().unlockCanvasAndPost(canvas);
 
-        synchronized (mAnimationLock) {
-            if (mInputAnimationRunning || feedbackAnimationRunning) {
-                getHandler().postAtTime(new Runnable() {
-                    @Override
-                    public void run() {
-                        processAnimation();
-                    }
-                }, startTimeMs + 30);
+        if (!mOnPaint.isEmpty()) {
+            requestRepaint();
+        } else {
+            synchronized (mAnimationLock) {
+                if (mInputAnimationRunning) {
+                    getHandler().postAtTime(new Runnable() {
+                        @Override
+                        public void run() {
+                            processAnimation();
+                        }
+                    }, startTimeMs + 30);
+                }
             }
         }
     }
@@ -303,9 +210,6 @@ public class DesktopView extends SurfaceView implements DesktopViewInterface,
         }
         if (running) {
             mInputHandler.processAnimation();
-        }
-        running |= mFeedbackAnimator.isAnimationRunning();
-        if (running) {
             requestRepaint();
         }
     }
@@ -379,9 +283,9 @@ public class DesktopView extends SurfaceView implements DesktopViewInterface,
     }
 
     @Override
-    public void showInputFeedback(InputFeedbackType feedbackToShow) {
+    public void showInputFeedback(InputFeedbackType feedbackToShow, Point pos) {
         if (feedbackToShow != InputFeedbackType.NONE) {
-            mFeedbackAnimator.startAnimation(feedbackToShow);
+            FeedbackAnimator.startAnimation(this, pos, feedbackToShow);
             requestRepaint();
         }
     }

remoting/android/client_java_tmpl.gni
@@ -25,6 +25,7 @@ template("remoting_android_client_java_tmpl") {
       "DummyActivityLifecycleListener.java",
       "DummyClientExtension.java",
       "Event.java",
+      "FeedbackAnimator.java",
       "FlingAnimationJob.java",
       "HostInfo.java",
       "HostListAdapter.java",
@@ -32,6 +33,7 @@ template("remoting_android_client_java_tmpl") {
       "InputStrategyInterface.java",
       "NavigationMenuAdapter.java",
       "OAuthTokenConsumer.java",
+      "PaintEventParameter.java",
       "Preconditions.java",
       "RenderData.java",
       "SessionAuthenticator.java",

remoting/android/java/src/org/chromium/chromoting/DesktopViewInterface.java
@@ -4,12 +4,14 @@
 
 package org.chromium.chromoting;
 
+import android.graphics.Point;
+
 /**
  * Callback interface to allow the TouchInputHandler to request actions on the DesktopView.
  */
 public interface DesktopViewInterface {
     /** Triggers a brief animation to indicate the existence and location of an input event. */
-    void showInputFeedback(DesktopView.InputFeedbackType feedbackToShow);
+    void showInputFeedback(DesktopView.InputFeedbackType feedbackToShow, Point pos);
 
     /** Shows the action bar. */
     void showActionBar();

remoting/android/java/src/org/chromium/chromoting/FeedbackAnimator.java
@@ -1,0 +1,93 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chromoting;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.os.SystemClock;
+
+/** Helper class for displaying the press feedback animations. */
+public final class FeedbackAnimator
+        implements Event.ParameterCallback<Boolean, PaintEventParameter> {
+    /** Total duration of the animation, in milliseconds. */
+    private static final float TOTAL_DURATION_MS = 220;
+
+    /** Start time of the animation, from {@link SystemClock#uptimeMillis()}. */
+    private final long mStartTimeInMs;
+
+    /** Contains the size of the feedback animation for the most recent request. */
+    private final float mFeedbackSizeInPixels;
+
+    private final Paint mPaint = new Paint();
+
+    private final Point mPos;
+
+    private FeedbackAnimator(float feedbackSizeInPixels, Point pos) {
+        mStartTimeInMs = SystemClock.uptimeMillis();
+        mFeedbackSizeInPixels = feedbackSizeInPixels;
+        mPos = pos;
+    }
+
+    /** Begins a new animation sequence at position (|pos|). */
+    public static void startAnimation(DesktopView view,
+                                      Point pos,
+                                      DesktopView.InputFeedbackType feedbackType) {
+        if (feedbackType == DesktopView.InputFeedbackType.NONE) {
+            return;
+        }
+
+        view.onPaint().addSelfRemovable(new FeedbackAnimator(
+                getInputFeedbackSizeInPixels(feedbackType), pos));
+    }
+
+    private static float getInputFeedbackSizeInPixels(DesktopView.InputFeedbackType feedbackType) {
+        switch (feedbackType) {
+            case SMALL_ANIMATION:
+                return 40.0f;
+
+            case LARGE_ANIMATION:
+                return 160.0f;
+
+            default:
+                // Unreachable, but required by Google Java style and findbugs.
+                assert false : "Unreached";
+                return 0.0f;
+        }
+    }
+
+    @Override
+    public Boolean run(PaintEventParameter parameter) {
+        float elapsedTimeInMs = SystemClock.uptimeMillis() - mStartTimeInMs;
+        if (elapsedTimeInMs < 1) {
+            return true;
+        }
+
+        // |progress| is 0 at the beginning, 1 at the end.
+        float progress = elapsedTimeInMs / TOTAL_DURATION_MS;
+        if (progress >= 1) {
+            return false;
+        }
+
+        float size = mFeedbackSizeInPixels / parameter.scale;
+
+        // Animation grows from 0 to |size|, and goes from fully opaque to transparent for a
+        // seamless fading-out effect. The animation needs to have more than one color so it's
+        // visible over any background color.
+        float radius = size * progress;
+        int alpha = (int) ((1 - progress) * 0xff);
+
+        int transparentBlack = Color.argb(0, 0, 0, 0);
+        int white = Color.argb(alpha, 0xff, 0xff, 0xff);
+        int black = Color.argb(alpha, 0, 0, 0);
+        mPaint.setShader(new RadialGradient(mPos.x, mPos.y, radius,
+                new int[] {transparentBlack, white, black, transparentBlack},
+                new float[] {0.0f, 0.8f, 0.9f, 1.0f}, Shader.TileMode.CLAMP));
+        parameter.canvas.drawCircle(mPos.x, mPos.y, radius, mPaint);
+        return true;
+    }
+}

remoting/android/java/src/org/chromium/chromoting/PaintEventParameter.java
@@ -1,0 +1,21 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chromoting;
+
+import android.graphics.Canvas;
+import android.graphics.Point;
+
+/** The parameter for an OnPaint event. */
+public final class PaintEventParameter {
+    public final Point cursorPosition;
+    public final Canvas canvas;
+    public final float scale;
+
+    public PaintEventParameter(Point cursorPosition, Canvas canvas, float scale) {
+        this.cursorPosition = cursorPosition;
+        this.canvas = canvas;
+        this.scale = scale;
+    }
+}

remoting/android/java/src/org/chromium/chromoting/TouchInputHandler.java
@@ -6,6 +6,7 @@ package org.chromium.chromoting;
 
 import android.content.Context;
 import android.graphics.Matrix;
+import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.view.GestureDetector;
@@ -526,7 +527,11 @@ public class TouchInputHandler implements TouchInputHandlerInterface {
             }
 
             if (mInputStrategy.onTap(button)) {
-                mViewer.showInputFeedback(mInputStrategy.getShortPressFeedbackType());
+                Point pos;
+                synchronized (mRenderData) {
+                    pos = mRenderData.getCursorPosition();
+                }
+                mViewer.showInputFeedback(mInputStrategy.getShortPressFeedbackType(), pos);
             }
             return true;
         }
@@ -544,7 +549,11 @@ public class TouchInputHandler implements TouchInputHandlerInterface {
             }
 
             if (mInputStrategy.onPressAndHold(button)) {
-                mViewer.showInputFeedback(mInputStrategy.getLongPressFeedbackType());
+                Point pos;
+                synchronized (mRenderData) {
+                    pos = mRenderData.getCursorPosition();
+                }
+                mViewer.showInputFeedback(mInputStrategy.getLongPressFeedbackType(), pos);
                 mSuppressFling = true;
                 mIsDragging = true;
             }

mirror/chromium

commit 9c99c573dccc3792c525b06cb775291f029410c1

[Chromoting] Fling gestures on Android client are too fast
This change is to fix a bug in DesktopView.paint, which cause fling gestures to
be too fast. This issue has been introduced by commit
06d70696de0b8fac307927064bd36be2d486fe2a. And the root cause is that animation
job should be called during each paint call.

BUG=619224

Review-Url: https://codereview.chromium.org/2060643002

remoting/android/java/src/org/chromium/chromoting/DesktopView.java
@@ -201,18 +201,14 @@ public class DesktopView extends SurfaceView implements DesktopViewInterface,
 
         getHolder().unlockCanvasAndPost(canvas);
 
-        if (!mOnPaint.isEmpty()) {
-            requestRepaint();
-        } else {
-            synchronized (mAnimationLock) {
-                if (mInputAnimationRunning) {
-                    getHandler().postAtTime(new Runnable() {
-                        @Override
-                        public void run() {
-                            processAnimation();
-                        }
-                    }, startTimeMs + 30);
-                }
+        synchronized (mAnimationLock) {
+            if (mInputAnimationRunning) {
+                getHandler().postAtTime(new Runnable() {
+                    @Override
+                    public void run() {
+                        processAnimation();
+                    }
+                }, startTimeMs + 30);
             }
         }
     }
@@ -224,6 +220,8 @@ public class DesktopView extends SurfaceView implements DesktopViewInterface,
         }
         if (running) {
             mInputHandler.processAnimation();
+            requestRepaint();
+        } else if (!mOnPaint.isEmpty()) {
             requestRepaint();
         }
     }