crosswalk-project/chromium-crosswalk

commit ea9c8c0ffffe09bb297ed9c5c619f411e06f7250

Update containing block's positioned descendants list when an object becomes out-of-flow-positioned
This is required when there are descendants needing overflow recalc.
Otherwise during overflow recalc because the containing block's
positioned descendant list didn't contain the new out-of-flow-positioned
object we would not reach the descendants needing overflow recalc.

BUG=623867
TEST=fast/overflow/add-visual-overflow-and-change-container-position.html

Review URL: https://codereview.chromium.org/2111643002 .

Review-Url: https://codereview.chromium.org/2103843003
Cr-Original-Commit-Position: refs/heads/master@{#402550}
Cr-Commit-Position: refs/branch-heads/2743@{#531}
Cr-Branched-From: 2b3ae3b8090361f8af5a611712fc1a5ab2de53cb-refs/heads/master@{#394939}

third_party/WebKit/Source/core/layout/LayoutBlock.cpp
@@ -223,6 +223,13 @@ void LayoutBlock::styleDidChange(StyleDifference diff, const ComputedStyle* oldS
             // See styleWillChange() for other cases.
             if (LayoutBlock* cb = containingBlock())
                 cb->removePositionedObjects(this, NewContainingBlock);
+                if (isOutOfFlowPositioned()) {
+                    // Insert this object into containing block's positioned descendants list
+                    // in case the parent won't layout. This is needed especially there are
+                    // descendants scheduled for overflow recalc.
+                    cb->insertPositionedObject(this);
+                }
+            }
         }
     }
 

third_party/WebKit/LayoutTests/fast/overflow/add-visual-overflow-and-change-container-position-expected.html
@@ -1,0 +1,4 @@
+<!DOCTYPE html>
+Tests overflow recalc when container changes position but no layout is scheduled for descendants.
+Passes if the blue square has black box shadow.
+<div style="width: 200px; height: 200px; background-color: blue; box-shadow: 50px 50px black"></div>

third_party/WebKit/LayoutTests/fast/overflow/add-visual-overflow-and-change-container-position.html
@@ -1,0 +1,13 @@
+<!DOCTYPE html>
+Tests overflow recalc when container changes position but no layout is scheduled for descendants.
+Passes if the blue square has black box shadow.
+<div id="container" style="width: 200px; height: 200px">
+  <div id="target" style="width: 200px; height: 200px; background-color: blue"></div>
+</div>
+<script src="../../resources/run-after-layout-and-paint.js"></script>
+<script>
+runAfterLayoutAndPaint(function() {
+  container.style.position = 'fixed';
+  target.style.boxShadow = '50px 50px black';
+}, true);
+</script>

mirror/chromium

commit 0adb790fc63c11b090a1b0ab2d036d8079b4cb78

Fix compilation error caused by crrev.com/ea9c8c0ffff
Caused by incorrect merge conflict resolution.

BUG=624571

Review URL: https://codereview.chromium.org/2109023003 .

third_party/WebKit/Source/core/layout/LayoutBlock.cpp
@@ -221,7 +221,7 @@ void LayoutBlock::styleDidChange(StyleDifference diff, const ComputedStyle* oldS
             // Remove our absolute and fixed positioned descendants from their new containing block,
             // in case containingBlock() changes by the change to the position property.
             // See styleWillChange() for other cases.
-            if (LayoutBlock* cb = containingBlock())
+            if (LayoutBlock* cb = containingBlock()) {
                 cb->removePositionedObjects(this, NewContainingBlock);
                 if (isOutOfFlowPositioned()) {
                     // Insert this object into containing block's positioned descendants list