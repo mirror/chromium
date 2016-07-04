crosswalk-project/chromium-crosswalk

commit 64b487795003572d5769667515d68b46857ae2c5

Update GPU pixel test expectations.
New tests are rebaselined and suppresions can now be removed.

R=kbr@chromium.org
BUG=624256
CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel

Review-Url: https://codereview.chromium.org/2113073003
Cr-Commit-Position: refs/heads/master@{#403566}

content/test/gpu/gpu_tests/pixel_expectations.py
@@ -19,12 +19,3 @@ class PixelExpectations(GpuTestExpectations):
               ['mac'], bug=540039)
     self.Fail('Pixel.WebGLGreenTriangleES3',
               ['mac', ('intel', 0x116)], bug=540531)
-
-    # TODO(ccameron): Remove suppression after rebaseline.
-    self.Fail('Pixel.CSS3DBlueBox', ['mac'], bug=533690)
-    self.Fail('Pixel.CSS3DBlueBoxES3', ['mac'], bug=533690)
-
-    # TODO(vmiura): Re-enable these after reference images are generated.
-    self.Fail('Pixel.SolidColorBackground', bug=624256)
-    self.Fail('Pixel.SolidColorBackgroundES3', bug=624256)
-    self.Fail('Pixel.2DCanvasWebGLES3', bug=624256)

mirror/chromium

commit 250090a8d84b35cbd632bfc071245127b7f39c16

Add GPU pixel test expectations
Add suppressions for tests that started failing after
64b487795003572d5769667515d68b46857ae2c5

BUG=624256
TBR=kbr@chromium.org
CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel

Review-Url: https://codereview.chromium.org/2119213002

content/test/gpu/gpu_tests/pixel_expectations.py
@@ -19,3 +19,10 @@ class PixelExpectations(GpuTestExpectations):
               ['mac'], bug=540039)
     self.Fail('Pixel.WebGLGreenTriangleES3',
               ['mac', ('intel', 0x116)], bug=540531)
+
+    # TODO(ccameron) fix these on Mac Retina
+    self.Fail('Pixel.CSS3DBlueBox', ['mac'], bug=533690)
+    self.Fail('Pixel.CSS3DBlueBoxES3', ['mac'], bug=533690)
+
+    # TODO(vmiura) check / generate reference images for Android devices
+    self.Fail('Pixel.SolidColorBackground', ['mac', 'android'], bug=624256)