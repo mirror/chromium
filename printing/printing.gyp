crosswalk-project/chromium-crosswalk

commit b779ce61d5369a930af480c265c0c5b924888914

Implement a new backend for modern CUPS implementations
As of CUPS 1.6, CUPS has moved away from PPD attributes to IPP attributes.  This new backend uses the IPP APIs and will be better suited to use with CUPS going forward.  Until we can bump our minumum CUPS version to 1.7, the only client will be Chrome OS where the version is 2.1.3.

BUG=607668

TEST=With --enable-native-cups enabled, print dialog will show available CUPS printers if any are configured.

Review-Url: https://codereview.chromium.org/2105463002
Cr-Commit-Position: refs/heads/master@{#407011}

printing/printing.gyp
@@ -72,7 +72,6 @@
         'print_settings_initializer_win.h',
         'printed_document.cc',
         'printed_document.h',
-        'printed_document_linux.cc',
         'printed_document_mac.cc',
         'printed_document_win.cc',
         'printed_page.cc',
@@ -174,10 +173,28 @@
             # of the print backend and enables a custom implementation instead.
             'PRINT_BACKEND_AVAILABLE',
           ],
-          'sources': [
-            'backend/cups_helper.cc',
-            'backend/cups_helper.h',
-            'backend/print_backend_cups.cc',
+
+          'conditions': [
+            ['chromeos==1', {
+              'sources': [
+                'backend/cups_connection.cc',
+                'backend/cups_connection.h',
+                'backend/cups_deleter.cc',
+                'backend/cups_deleter.h',
+                'backend/cups_ipp_util.cc',
+                'backend/cups_ipp_util.h',
+                'backend/cups_printer.cc',
+                'backend/cups_printer.h',
+                'backend/print_backend_cups_ipp.cc',
+                'backend/print_backend_cups_ipp.h',
+              ],
+            }, { # chromeos==0
+              'sources': [
+                'backend/cups_helper.cc',
+                'backend/cups_helper.h',
+                'backend/print_backend_cups.cc',
+              ],
+            }],
           ],
         }],
         ['OS=="linux" and chromeos==1', {
@@ -192,6 +209,7 @@
         }],
         ['OS=="linux" and chromeos==0', {
           'sources': [
+            'printed_document_linux.cc',
             'printing_context_linux.cc',
             'printing_context_linux.h',
           ],
@@ -247,8 +265,12 @@
           'defines': [
             'USE_CUPS',
           ],
-          'sources': [
-            'backend/cups_helper_unittest.cc',
+          'conditions': [
+            ['chromeos==1', {
+              'sources': ['backend/cups_ipp_util_unittest.cc'],
+            }, {
+              'sources': ['backend/cups_helper_unittest.cc'],
+            }],
           ],
         }],
       ],

printing/BUILD.gn
@@ -169,12 +169,27 @@ component("printing") {
     # of the print backend and enables a custom implementation instead.
     defines += [ "PRINT_BACKEND_AVAILABLE" ]
 
-    sources += [
-      "backend/cups_helper.cc",
-      "backend/cups_helper.h",
-      "backend/print_backend_cups.cc",
-      "backend/print_backend_cups.h",
-    ]
+    if (is_chromeos) {
+      sources += [
+        "backend/cups_connection.cc",
+        "backend/cups_connection.h",
+        "backend/cups_deleters.cc",
+        "backend/cups_deleters.h",
+        "backend/cups_ipp_util.cc",
+        "backend/cups_ipp_util.h",
+        "backend/cups_printer.cc",
+        "backend/cups_printer.h",
+        "backend/print_backend_cups_ipp.cc",
+        "backend/print_backend_cups_ipp.h",
+      ]
+    } else {
+      sources += [
+        "backend/cups_helper.cc",
+        "backend/cups_helper.h",
+        "backend/print_backend_cups.cc",
+        "backend/print_backend_cups.h",
+      ]
+    }
   }
 
   if (is_chromeos) {
@@ -240,7 +255,12 @@ test("printing_unittests") {
 
   if (use_cups) {
     configs += [ ":cups" ]
-    sources += [ "backend/cups_helper_unittest.cc" ]
+
+    if (is_chromeos) {
+      sources += [ "backend/cups_ipp_util_unittest.cc" ]
+    } else {
+      sources += [ "backend/cups_helper_unittest.cc" ]
+    }
   }
 }
 

printing/backend/cups_connection.cc
@@ -1,0 +1,137 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "printing/backend/cups_connection.h"
+
+#include <string>
+#include <utility>
+
+#include "base/logging.h"
+#include "base/memory/ptr_util.h"
+#include "base/strings/stringprintf.h"
+
+namespace printing {
+
+namespace {
+
+const int kTimeoutMs = 3000;
+
+class DestinationEnumerator {
+ public:
+  DestinationEnumerator() {}
+
+  static int cups_callback(void* user_data, unsigned flags, cups_dest_t* dest) {
+    cups_dest_t* copied_dest;
+    cupsCopyDest(dest, 0, &copied_dest);
+    reinterpret_cast<DestinationEnumerator*>(user_data)->store_dest(
+        copied_dest);
+
+    //  keep going
+    return 1;
+  }
+
+  void store_dest(cups_dest_t* dest) { dests_.emplace_back(dest); }
+
+  // Returns the collected destinations.
+  std::vector<std::unique_ptr<cups_dest_t, DestinationDeleter>>& get_dests() {
+    return dests_;
+  }
+
+ private:
+  std::vector<std::unique_ptr<cups_dest_t, DestinationDeleter>> dests_;
+
+  DISALLOW_COPY_AND_ASSIGN(DestinationEnumerator);
+};
+
+}  // namespace
+
+CupsConnection::CupsConnection(const GURL& print_server_url,
+                               http_encryption_t encryption,
+                               bool blocking)
+    : print_server_url_(print_server_url),
+      cups_encryption_(encryption),
+      blocking_(blocking),
+      cups_http_(nullptr) {}
+
+CupsConnection::CupsConnection(CupsConnection&& connection)
+    : print_server_url_(connection.print_server_url_),
+      cups_encryption_(connection.cups_encryption_),
+      blocking_(connection.blocking_),
+      cups_http_(std::move(connection.cups_http_)) {}
+
+CupsConnection::~CupsConnection() {}
+
+bool CupsConnection::Connect() {
+  if (cups_http_)
+    return true;  // we're already connected
+
+  std::string host;
+  int port;
+
+  if (!print_server_url_.is_empty()) {
+    host = print_server_url_.host();
+    port = print_server_url_.IntPort();
+  } else {
+    host = cupsServer();
+    port = ippPort();
+  }
+
+  cups_http_.reset(httpConnect2(host.c_str(), port, nullptr, AF_UNSPEC,
+                                cups_encryption_, blocking_ ? 1 : 0, kTimeoutMs,
+                                nullptr));
+  return !!cups_http_;
+}
+
+std::vector<CupsPrinter> CupsConnection::GetDests() {
+  if (!Connect()) {
+    LOG(WARNING) << "CUPS connection failed";
+    return std::vector<CupsPrinter>();
+  }
+
+  DestinationEnumerator enumerator;
+  int success =
+      cupsEnumDests(CUPS_DEST_FLAGS_NONE, kTimeoutMs,
+                    nullptr,               // no cancel signal
+                    0,                     // all the printers
+                    CUPS_PRINTER_SCANNER,  // except the scanners
+                    &DestinationEnumerator::cups_callback, &enumerator);
+
+  if (!success) {
+    LOG(WARNING) << "Enumerating printers failed";
+    return std::vector<CupsPrinter>();
+  }
+
+  auto dests = std::move(enumerator.get_dests());
+  std::vector<CupsPrinter> printers;
+  for (auto& dest : dests) {
+    printers.emplace_back(cups_http_.get(), std::move(dest), nullptr);
+  }
+
+  return printers;
+}
+
+std::unique_ptr<CupsPrinter> CupsConnection::GetPrinter(
+    const std::string& name) {
+  if (!Connect())
+    return nullptr;
+
+  cups_dest_t* dest = cupsGetNamedDest(cups_http_.get(), name.c_str(), nullptr);
+  if (!dest)
+    return nullptr;
+
+  cups_dinfo_t* info = cupsCopyDestInfo(cups_http_.get(), dest);
+  return base::MakeUnique<CupsPrinter>(
+      cups_http_.get(), std::unique_ptr<cups_dest_t, DestinationDeleter>(dest),
+      std::unique_ptr<cups_dinfo_t, DestInfoDeleter>(info));
+}
+
+std::string CupsConnection::server_name() const {
+  return print_server_url_.host();
+}
+
+int CupsConnection::last_error() const {
+  return cupsLastError();
+}
+
+}  // namespace printing

printing/backend/cups_connection.h
@@ -1,0 +1,59 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PRINTING_BACKEND_CUPS_CONNECTION_H_
+#define PRINTING_BACKEND_CUPS_CONNECTION_H_
+
+#include <cups/cups.h>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "printing/backend/cups_deleters.h"
+#include "printing/backend/cups_printer.h"
+#include "printing/printing_export.h"
+#include "url/gurl.h"
+
+namespace printing {
+
+// Represents a connection to a CUPS server.
+class PRINTING_EXPORT CupsConnection {
+ public:
+  CupsConnection(const GURL& print_server_url,
+                 http_encryption_t encryption,
+                 bool blocking);
+
+  CupsConnection(CupsConnection&& connection);
+
+  ~CupsConnection();
+
+  // Returns a vector of all the printers configure on the CUPS server.
+  std::vector<CupsPrinter> GetDests();
+
+  // Returns a printer for |printer_name| from the connected server.
+  std::unique_ptr<CupsPrinter> GetPrinter(const std::string& printer_name);
+
+  std::string server_name() const;
+
+  int last_error() const;
+
+ private:
+  // lazily initialize http connection
+  bool Connect();
+
+  GURL print_server_url_;
+  http_encryption_t cups_encryption_;
+  bool blocking_;
+
+  std::unique_ptr<http_t, HttpDeleter> cups_http_;
+
+  DISALLOW_COPY_AND_ASSIGN(CupsConnection);
+};
+
+}  // namespace printing
+
+#endif  // PRINTING_BACKEND_CUPS_CONNECTION_H_

printing/backend/cups_deleters.cc
@@ -1,0 +1,21 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "printing/backend/cups_deleters.h"
+
+namespace printing {
+
+void HttpDeleter::operator()(http_t* http) const {
+  httpClose(http);
+}
+
+void DestinationDeleter::operator()(cups_dest_t* dest) const {
+  cupsFreeDests(1, dest);
+}
+
+void DestInfoDeleter::operator()(cups_dinfo_t* info) const {
+  cupsFreeDestInfo(info);
+}
+
+}  // namespace printing

printing/backend/cups_deleters.h
@@ -1,0 +1,29 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PRINTING_BACKEND_CUPS_DELETERS_H_
+#define PRINTING_BACKEND_CUPS_DELETERS_H_
+
+#include <cups/cups.h>
+
+namespace printing {
+
+struct HttpDeleter {
+ public:
+  void operator()(http_t* http) const;
+};
+
+struct DestinationDeleter {
+ public:
+  void operator()(cups_dest_t* dest) const;
+};
+
+struct DestInfoDeleter {
+ public:
+  void operator()(cups_dinfo_t* info) const;
+};
+
+}  // namespace printing
+
+#endif  // PRINTING_BACKEND_CUPS_DELETERS_H_

printing/backend/cups_ipp_util.cc
@@ -1,0 +1,298 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "printing/backend/cups_ipp_util.h"
+
+#include <cups/cups.h>
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+#include "base/logging.h"
+#include "base/stl_util.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/string_piece.h"
+#include "base/strings/string_split.h"
+#include "base/strings/string_util.h"
+#include "printing/backend/cups_printer.h"
+#include "printing/backend/print_backend_consts.h"
+#include "printing/units.h"
+
+namespace printing {
+
+namespace {
+
+const char kIppCollate[] = "sheet-collate";  // RFC 3381
+const char kIppCopies[] = CUPS_COPIES;
+const char kIppColor[] = CUPS_PRINT_COLOR_MODE;
+const char kIppMedia[] = CUPS_MEDIA;
+const char kIppDuplex[] = CUPS_SIDES;
+
+const char kCollated[] = "collated";
+
+const int kMicronsPerMM = 1000;
+const double kMMPerInch = 25.4;
+const double kMicronsPerInch = kMMPerInch * kMicronsPerMM;
+
+enum Unit {
+  INCHES,
+  MILLIMETERS,
+};
+
+struct ColorMap {
+  const char* color;
+  ColorModel model;
+};
+
+const ColorMap kColorList[]{
+    {CUPS_PRINT_COLOR_MODE_COLOR, COLORMODE_COLOR},
+    {CUPS_PRINT_COLOR_MODE_MONOCHROME, COLORMODE_MONOCHROME},
+};
+
+ColorModel ColorModelFromIppColor(base::StringPiece ippColor) {
+  for (const ColorMap& color : kColorList) {
+    if (ippColor.compare(color.color) == 0) {
+      return color.model;
+    }
+  }
+
+  return UNKNOWN_COLOR_MODEL;
+}
+
+bool PrinterSupportsValue(const CupsOptionProvider& printer,
+                          base::StringPiece name,
+                          base::StringPiece value) {
+  std::vector<base::StringPiece> values =
+      printer.GetSupportedOptionValueStrings(name);
+  return ContainsValue(values, value);
+}
+
+DuplexMode PrinterDefaultDuplex(const CupsOptionProvider& printer) {
+  ipp_attribute_t* attr = printer.GetDefaultOptionValue(kIppDuplex);
+  if (!attr)
+    return UNKNOWN_DUPLEX_MODE;
+
+  const char* value = ippGetString(attr, 0, nullptr);
+  if (base::EqualsCaseInsensitiveASCII(value, CUPS_SIDES_ONE_SIDED))
+    return SIMPLEX;
+
+  if (base::EqualsCaseInsensitiveASCII(value, CUPS_SIDES_TWO_SIDED_PORTRAIT))
+    return LONG_EDGE;
+
+  if (base::EqualsCaseInsensitiveASCII(value, CUPS_SIDES_TWO_SIDED_LANDSCAPE))
+    return SHORT_EDGE;
+
+  return UNKNOWN_DUPLEX_MODE;
+}
+
+gfx::Size DimensionsToMicrons(base::StringPiece value) {
+  Unit unit;
+  base::StringPiece dims;
+  size_t unit_position;
+  if ((unit_position = value.find("mm")) != base::StringPiece::npos) {
+    unit = MILLIMETERS;
+    dims = value.substr(0, unit_position);
+  } else if ((unit_position = value.find("in")) != base::StringPiece::npos) {
+    unit = INCHES;
+    dims = value.substr(0, unit_position);
+  } else {
+    LOG(WARNING) << "Could not parse paper dimensions";
+    return {0, 0};
+  }
+
+  std::vector<std::string> pieces = base::SplitString(
+      dims, "x", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
+  double width;
+  double height;
+
+  if (pieces.size() != 2 || !base::StringToDouble(pieces[0], &width) ||
+      !base::StringToDouble(pieces[1], &height)) {
+    return {0, 0};
+  }
+
+  int width_microns;
+  int height_microns;
+
+  switch (unit) {
+    case MILLIMETERS:
+      width_microns = width * kMicronsPerMM;
+      height_microns = height * kMicronsPerMM;
+      break;
+    case INCHES:
+      width_microns = width * kMicronsPerInch;
+      height_microns = height * kMicronsPerInch;
+      break;
+    default:
+      NOTREACHED();
+      break;
+  }
+
+  return gfx::Size{width_microns, height_microns};
+}
+
+PrinterSemanticCapsAndDefaults::Paper ParsePaper(base::StringPiece value) {
+  // <name>_<width>x<height>{in,mm}
+  // e.g. na_letter_8.5x11in, iso_a4_210x297mm
+
+  std::vector<base::StringPiece> pieces = base::SplitStringPiece(
+      value, "_", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
+  // we expect at least a display string and a dimension string
+  if (pieces.size() < 2)
+    return PrinterSemanticCapsAndDefaults::Paper();
+
+  base::StringPiece dimensions = pieces.back();
+
+  std::string display = pieces[0].as_string();
+  for (size_t i = 1; i <= pieces.size() - 2; ++i) {
+    display.append(" ");
+    pieces[i].AppendToString(&display);
+  }
+
+  PrinterSemanticCapsAndDefaults::Paper paper;
+  paper.display_name = display;
+  paper.vendor_id = value.as_string();
+  paper.size_um = DimensionsToMicrons(dimensions);
+
+  return paper;
+}
+
+void ExtractColor(const CupsOptionProvider& printer,
+                  PrinterSemanticCapsAndDefaults* printer_info) {
+  printer_info->bw_model = UNKNOWN_COLOR_MODEL;
+  printer_info->color_model = UNKNOWN_COLOR_MODEL;
+
+  // color and b&w
+  std::vector<ColorModel> color_models = SupportedColorModels(printer);
+  for (ColorModel color : color_models) {
+    switch (color) {
+      case COLORMODE_COLOR:
+        printer_info->color_model = COLORMODE_COLOR;
+        break;
+      case COLORMODE_MONOCHROME:
+        printer_info->bw_model = COLORMODE_MONOCHROME;
+        break;
+      default:
+        // value not needed
+        break;
+    }
+  }
+
+  // changeable
+  printer_info->color_changeable =
+      (printer_info->color_model != UNKNOWN_COLOR_MODEL &&
+       printer_info->bw_model != UNKNOWN_COLOR_MODEL);
+
+  // default color
+  printer_info->color_default = DefaultColorModel(printer) == COLORMODE_COLOR;
+}
+
+void ExtractCopies(const CupsOptionProvider& printer,
+                   PrinterSemanticCapsAndDefaults* printer_info) {
+  // copies
+  int upper_bound;
+  int lower_bound;
+  CopiesRange(printer, &lower_bound, &upper_bound);
+  printer_info->copies_capable = (lower_bound != -1) && (upper_bound >= 2);
+}
+
+}  // namespace
+
+ColorModel DefaultColorModel(const CupsOptionProvider& printer) {
+  // default color
+  ipp_attribute_t* attr = printer.GetDefaultOptionValue(kIppColor);
+  if (!attr)
+    return UNKNOWN_COLOR_MODEL;
+
+  return ColorModelFromIppColor(ippGetString(attr, 0, nullptr));
+}
+
+std::vector<ColorModel> SupportedColorModels(
+    const CupsOptionProvider& printer) {
+  std::vector<ColorModel> colors;
+
+  std::vector<base::StringPiece> color_modes =
+      printer.GetSupportedOptionValueStrings(kIppColor);
+
+  for (base::StringPiece color : color_modes) {
+    ColorModel color_model = ColorModelFromIppColor(color);
+    if (color_model != UNKNOWN_COLOR_MODEL) {
+      colors.push_back(color_model);
+    }
+  }
+
+  return colors;
+}
+
+PrinterSemanticCapsAndDefaults::Paper DefaultPaper(
+    const CupsOptionProvider& printer) {
+  ipp_attribute_t* attr = printer.GetDefaultOptionValue(kIppMedia);
+  if (!attr)
+    return PrinterSemanticCapsAndDefaults::Paper();
+
+  return ParsePaper(ippGetString(attr, 0, nullptr));
+}
+
+std::vector<PrinterSemanticCapsAndDefaults::Paper> SupportedPapers(
+    const CupsOptionProvider& printer) {
+  std::vector<base::StringPiece> papers =
+      printer.GetSupportedOptionValueStrings(kIppMedia);
+  std::vector<PrinterSemanticCapsAndDefaults::Paper> parsed_papers;
+  for (base::StringPiece paper : papers) {
+    parsed_papers.push_back(ParsePaper(paper));
+  }
+
+  return parsed_papers;
+}
+
+void CopiesRange(const CupsOptionProvider& printer,
+                 int* lower_bound,
+                 int* upper_bound) {
+  ipp_attribute_t* attr = printer.GetSupportedOptionValues(kIppCopies);
+  if (!attr) {
+    *lower_bound = -1;
+    *upper_bound = -1;
+  }
+
+  *lower_bound = ippGetRange(attr, 0, upper_bound);
+}
+
+bool CollateCapable(const CupsOptionProvider& printer) {
+  std::vector<base::StringPiece> values =
+      printer.GetSupportedOptionValueStrings(kIppCollate);
+  auto iter = std::find(values.begin(), values.end(), kCollated);
+  return iter != values.end();
+}
+
+bool CollateDefault(const CupsOptionProvider& printer) {
+  ipp_attribute_t* attr = printer.GetDefaultOptionValue(kIppCollate);
+  if (!attr)
+    return false;
+
+  base::StringPiece name = ippGetString(attr, 0, nullptr);
+  return name.compare(kCollated) == 0;
+}
+
+void CapsAndDefaultsFromPrinter(const CupsOptionProvider& printer,
+                                PrinterSemanticCapsAndDefaults* printer_info) {
+  // duplex
+  printer_info->duplex_default = PrinterDefaultDuplex(printer);
+  printer_info->duplex_capable =
+      PrinterSupportsValue(printer, kIppDuplex, CUPS_SIDES_TWO_SIDED_PORTRAIT);
+
+  // collate
+  printer_info->collate_default = CollateDefault(printer);
+  printer_info->collate_capable = CollateCapable(printer);
+
+  // paper
+  printer_info->default_paper = DefaultPaper(printer);
+  printer_info->papers = SupportedPapers(printer);
+
+  ExtractCopies(printer, printer_info);
+  ExtractColor(printer, printer_info);
+
+  // TODO(skau): Add dpi and default_dpi
+}
+
+}  //  namespace printing

printing/backend/cups_ipp_util.h
@@ -1,0 +1,50 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PRINTING_BACKEND_CUPS_IPP_UTIL_H_
+#define PRINTING_BACKEND_CUPS_IPP_UTIL_H_
+
+#include <vector>
+
+#include "base/strings/string_piece.h"
+#include "printing/backend/cups_printer.h"
+#include "printing/backend/print_backend.h"
+
+namespace printing {
+
+// Returns the default ColorModel for |printer|.
+ColorModel DefaultColorModel(const CupsOptionProvider& printer);
+
+// Returns the set of supported ColorModels for |printer|.
+std::vector<ColorModel> SupportedColorModels(const CupsOptionProvider& printer);
+
+// Returns the default paper setting for |printer|.
+PrinterSemanticCapsAndDefaults::Paper DefaultPaper(
+    const CupsOptionProvider& printer);
+
+// Returns the list of papers supported by the |printer|.
+std::vector<PrinterSemanticCapsAndDefaults::Paper> SupportedPapers(
+    const CupsOptionProvider& printer);
+
+// Retrieves the supported number of copies from |printer| and writes the
+// extremities of the range into |lower_bound| and |upper_bound|.  Values are
+// set to -1 if there is an error.
+void CopiesRange(const CupsOptionProvider& printer,
+                 int* lower_bound,
+                 int* upper_bound);
+
+// Returns true if |printer| can do collation.
+bool CollateCapable(const CupsOptionProvider& printer);
+
+// Returns true if |printer| has collation enabled by default.
+bool CollateDefault(const CupsOptionProvider& printer);
+
+// Populates the |printer_info| object with attributes retrived using IPP from
+// |printer|.
+void CapsAndDefaultsFromPrinter(const CupsOptionProvider& printer,
+                                PrinterSemanticCapsAndDefaults* printer_info);
+
+}  // namespace printing
+
+#endif  // PRINTING_BACKEND_CUPS_IPP_UTIL_H_

mirror/chromium

commit dcbbd8d044daec72c1f7bb78f621301b75d7e34a

Fix printing.gyp by joining two 'conditions' dicts together.
Revision b779ce61d5369a930af480c265c0c5b924888914 broke the
generate_build_files step on the Dr. Memory build:

gyp: Key 'conditions' repeated at level 6 with key path
'targets.0.conditions.5.1' while reading
C:\b\build\slave\drm-cr\build\src\printing\printing.gyp

BUG=607668
TEST=none
TBR=skau@chromium.org

Review-Url: https://codereview.chromium.org/2173063002

printing/printing.gyp
@@ -167,14 +167,6 @@
                 ],
               },
             }],
-          ],
-          'defines': [
-            # PRINT_BACKEND_AVAILABLE disables the default dummy implementation
-            # of the print backend and enables a custom implementation instead.
-            'PRINT_BACKEND_AVAILABLE',
-          ],
-
-          'conditions': [
             ['chromeos==1', {
               'sources': [
                 'backend/cups_connection.cc',
@@ -195,6 +187,11 @@
                 'backend/print_backend_cups.cc',
               ],
             }],
+          ],
+          'defines': [
+            # PRINT_BACKEND_AVAILABLE disables the default dummy implementation
+            # of the print backend and enables a custom implementation instead.
+            'PRINT_BACKEND_AVAILABLE',
           ],
         }],
         ['OS=="linux" and chromeos==1', {