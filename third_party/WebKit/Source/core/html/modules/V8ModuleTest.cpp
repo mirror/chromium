34b9415b28006f9ee1a618cd056b2108aadbfc98
Dominic Cooney <dominicc@chromium.org>
2016-09-29 13:54:14 +0900

    Try to get stuff out of a module; crash NYI in FullCodeGenerator::VisitVariableDeclaration/VariableLocation::MODULE

--- third_party/WebKit/Source/core/html/modules/V8ModuleTest.cpp
+++ third_party/WebKit/Source/core/html/modules/V8ModuleTest.cpp
@@ -35,26 +35,67 @@ public:
         array_buffer_allocator_.reset(nullptr);
     }
 
+    v8::Local<v8::String> v8_str(const char*);
+
 protected:
     std::unique_ptr<v8::ArrayBuffer::Allocator> array_buffer_allocator_;
     v8::Isolate* isolate_;
 };
 
-TEST_F(V8ModuleTest, testsAreRunning)
+struct Stuff
 {
-    v8::HandleScope handle_scope(isolate_);
-    v8::Local<v8::Context> context = v8::Context::New(isolate_);
-    v8::Context::Scope context_scope(context);
-    v8::Local<v8::String> source =
-        v8::String::NewFromUtf8(
-            isolate_, "'Hello' + ', World!'", v8::NewStringType::kNormal)
+    Stuff(v8::Isolate* isolate)
+        : handle_scope_(isolate)
+        , context_(v8::Context::New(isolate))
+        , scope_(context_)
+    {
+    }
+
+    v8::HandleScope handle_scope_;
+    v8::Local<v8::Context> context_;
+    v8::Context::Scope scope_;
+};
+
+v8::Local<v8::String> V8ModuleTest::v8_str(const char* s)
+{
+    return v8::String::NewFromUtf8(isolate_, s, v8::NewStringType::kNormal)
         .ToLocalChecked();
-    v8::Local<v8::Script> script = v8::Script::Compile(context, source)
+}
+
+TEST_F(V8ModuleTest, runScript)
+{
+    Stuff stuff(isolate_);
+    v8::Local<v8::String> source = v8_str("'Hello' + ', World!'");
+    v8::Local<v8::Script> script = v8::Script::Compile(stuff.context_, source)
         .ToLocalChecked();
-    v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+    v8::Local<v8::Value> result = script->Run(stuff.context_).ToLocalChecked();
     v8::String::Utf8Value utf8(result);
     EXPECT_EQ("Hello, World!", std::string(*utf8))
         << "the script should have run";
+}
+
+TEST_F(V8ModuleTest, instantiateModule)
+{
+    Stuff stuff(isolate_);
+    v8::ScriptCompiler::Source module_source(v8_str(
+        "function greet(name) { return `Hello, ${name}!`; }\n"
+        "//export default { greet: 42 };"));
+    v8::Local<v8::Module> module =
+        v8::ScriptCompiler::CompileModule(isolate_, &module_source)
+        .ToLocalChecked();
+    EXPECT_TRUE(module->Instantiate(stuff.context_, nullptr));
+    v8::Local<v8::Value> module_value =
+        module->Evaluate(stuff.context_).ToLocalChecked();
+    v8::Local<v8::Function> f = module_value.As<v8::Object>()->Get(
+        stuff.context_,
+        v8_str("greet")).ToLocalChecked().As<v8::Function>();
+    v8::Local<v8::Value> args[] = {v8_str("Mars")};
+    v8::Local<v8::Value> result =
+        f->Call(stuff.context_, stuff.context_->Global(), 1, args)
+        .ToLocalChecked();
+    v8::String::Utf8Value utf8(result);
+    EXPECT_EQ("Hello, Mars!", std::string(*utf8))
+        << "the module should have exported a function";
 }
 
 } // namespace blink
