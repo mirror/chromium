crosswalk-project/chromium-crosswalk

commit 2524a5f430bcc8cbc1e8d0c47c5047ba696c7f04

[Autofill] Close mojo interface bindings of autofill/password agent in time.
As for AutofillAgent, Password{Autofill,Generation}Agent, their destructions
are asynchronous with destructions of corresponding render frame.
Thus, even after corresponding render frame has been deleted, they can still
keep alive for some while and still able to receive mojo messages which maybe
lead to use-after-delete access on the render frame.

To solve above problem, this CL closes the mojo interface bindings immediatelly
when render frame got destroyed.

BUG=641156

Review-Url: https://codereview.chromium.org/2320993002
Cr-Commit-Position: refs/heads/master@{#417286}
(cherry picked from commit 7e8c85b79831bb59427ffc5b229bc08470f619ef)

Review URL: https://codereview.chromium.org/2400393003 .

Cr-Commit-Position: refs/branch-heads/2840@{#693}
Cr-Branched-From: 1ae106dbab4bddd85132d5b75c670794311f4c57-refs/heads/master@{#414607}

components/autofill/content/renderer/password_autofill_agent.cc
@@ -1153,6 +1153,7 @@ void PasswordAutofillAgent::WillSubmitForm(const blink::WebFormElement& form) {
 }
 
 void PasswordAutofillAgent::OnDestruct() {
+  binding_.Close();
   base::ThreadTaskRunnerHandle::Get()->DeleteSoon(FROM_HERE, this);
 }
 

components/autofill/content/renderer/password_generation_agent.cc
@@ -190,6 +190,7 @@ void PasswordGenerationAgent::DidFinishLoad() {
 }
 
 void PasswordGenerationAgent::OnDestruct() {
+  binding_.Close();
   base::ThreadTaskRunnerHandle::Get()->DeleteSoon(FROM_HERE, this);
 }
 

components/autofill/content/renderer/autofill_agent.cc
@@ -281,6 +281,7 @@ void AutofillAgent::FireHostSubmitEvents(const FormData& form_data,
 }
 
 void AutofillAgent::Shutdown() {
+  binding_.Close();
   legacy_.Shutdown();
   weak_ptr_factory_.InvalidateWeakPtrs();
 }

mirror/chromium

commit f1f7c51129cba3dedc3bc212969051ae92d4ff8d

Fix the build error caused by 2524a5f430bcc8cbc1e8d0c47c5047ba696c7f04
2524a5f430bcc8cbc1e8d0c47c5047ba696c7f04 is a cherry-pick commit
introduced by https://codereview.chromium.org/2400393003.
It is accessing |binding_| member of
{AutofillAgent,PasswordAutofillAgent,PasswordGenerationAgent}.
However, on M54 branch, {PasswordAutofillAgent,PasswordGenerationAgent}
have no |binding_| member yet, they are introduced after M54 branched
out...

BUG=641156
TBR=vabr@chromium.org

Review URL: https://codereview.chromium.org/2406773002 .

components/autofill/content/renderer/password_autofill_agent.cc
@@ -1153,7 +1153,6 @@ void PasswordAutofillAgent::WillSubmitForm(const blink::WebFormElement& form) {
 }
 
 void PasswordAutofillAgent::OnDestruct() {
-  binding_.Close();
   base::ThreadTaskRunnerHandle::Get()->DeleteSoon(FROM_HERE, this);
 }
 

components/autofill/content/renderer/password_generation_agent.cc
@@ -190,7 +190,6 @@ void PasswordGenerationAgent::DidFinishLoad() {
 }
 
 void PasswordGenerationAgent::OnDestruct() {
-  binding_.Close();
   base::ThreadTaskRunnerHandle::Get()->DeleteSoon(FROM_HERE, this);
 }
 