nwjs/chromium.src

commit 9a6acdd595311651640bb269044e626a530bc723

Delete tab state files when the user explicitly closes a CCT.
BUG=646600

Review-Url: https://codereview.chromium.org/2520103002
Cr-Commit-Position: refs/heads/master@{#434007}

chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -64,7 +64,6 @@ import org.chromium.chrome.browser.tabmodel.TabModel.TabLaunchType;
 import org.chromium.chrome.browser.tabmodel.TabModelObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorImpl;
-import org.chromium.chrome.browser.tabmodel.TabPersistencePolicy;
 import org.chromium.chrome.browser.toolbar.ToolbarControlContainer;
 import org.chromium.chrome.browser.util.ColorUtils;
 import org.chromium.chrome.browser.util.UrlUtilities;
@@ -98,6 +97,7 @@ public class CustomTabActivity extends ChromeActivity {
     private CustomTabContentHandler mCustomTabContentHandler;
     private Tab mMainTab;
     private CustomTabBottomBarDelegate mBottomBarDelegate;
+    private CustomTabTabPersistencePolicy mTabPersistencePolicy;
 
     // This is to give the right package name while using the client's resources during an
     // overridePendingTransition call.
@@ -341,10 +341,10 @@ public class CustomTabActivity extends ChromeActivity {
 
     @Override
     protected TabModelSelector createTabModelSelector() {
-        TabPersistencePolicy persistencePolicy = new CustomTabTabPersistencePolicy(
+        mTabPersistencePolicy = new CustomTabTabPersistencePolicy(
                 getTaskId(), getSavedInstanceState() != null);
 
-        return new TabModelSelectorImpl(this, this, persistencePolicy, false, false);
+        return new TabModelSelectorImpl(this, this, mTabPersistencePolicy, false, false);
     }
 
     @Override
@@ -600,7 +600,12 @@ public class CustomTabActivity extends ChromeActivity {
     public void onStopWithNative() {
         super.onStopWithNative();
         setActiveContentHandler(null);
-        if (!mIsClosing) getTabModelSelector().saveState();
+        if (mIsClosing) {
+            getTabModelSelector().closeAllTabs(true);
+            mTabPersistencePolicy.deleteMetadataStateFileAsync();
+        } else {
+            getTabModelSelector().saveState();
+        }
     }
 
     /**
@@ -859,6 +864,14 @@ public class CustomTabActivity extends ChromeActivity {
     @VisibleForTesting
     CustomTabIntentDataProvider getIntentDataProvider() {
         return mIntentDataProvider;
+    }
+
+    /**
+     * @return The tab persistence policy for this activity.
+     */
+    @VisibleForTesting
+    CustomTabTabPersistencePolicy getTabPersistencePolicyForTest() {
+        return mTabPersistencePolicy;
     }
 
     /**

chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabTabPersistencePolicy.java
@@ -205,6 +205,22 @@ public class CustomTabTabPersistencePolicy implements TabPersistencePolicy {
     }
 
     /**
+     * Triggers an async deletion of the tab state metadata file.
+     */
+    public void deleteMetadataStateFileAsync() {
+        AsyncTask.SERIAL_EXECUTOR.execute(new Runnable() {
+            @Override
+            public void run() {
+                File stateDir = getOrCreateStateDirectory();
+                File metadataFile = new File(stateDir, getStateFileName());
+                if (metadataFile.exists() && !metadataFile.delete()) {
+                    Log.e(TAG, "Failed to delete file: " + metadataFile);
+                }
+            }
+        });
+    }
+
+    /**
      * Given a list of metadata files, determine which are applicable for deletion based on the
      * deletion strategy of Custom Tabs.
      *

chrome/android/java_sources.gni
@@ -1208,6 +1208,7 @@ chrome_test_java_sources = [
   "javatests/src/org/chromium/chrome/browser/customtabs/CustomTabFromChromeExternalNavigationTest.java",
   "javatests/src/org/chromium/chrome/browser/customtabs/CustomTabsConnectionTest.java",
   "javatests/src/org/chromium/chrome/browser/customtabs/CustomTabsTestUtils.java",
+  "javatests/src/org/chromium/chrome/browser/customtabs/CustomTabTabPersistenceIntegrationTest.java",
   "javatests/src/org/chromium/chrome/browser/customtabs/CustomTabTabPersistencePolicyTest.java",
   "javatests/src/org/chromium/chrome/browser/customtabs/RequestThrottlerTest.java",
   "javatests/src/org/chromium/chrome/browser/document/LauncherActivityTest.java",

chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabTabPersistenceIntegrationTest.java
@@ -1,0 +1,60 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.customtabs;
+
+import android.test.suitebuilder.annotation.MediumTest;
+
+import org.chromium.chrome.browser.TabState;
+import org.chromium.chrome.browser.UrlConstants;
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.content.browser.test.util.Criteria;
+import org.chromium.content.browser.test.util.CriteriaHelper;
+
+import java.io.File;
+import java.util.Locale;
+
+/**
+ * Integration testing for the CustomTab Tab persistence logic.
+ */
+public class CustomTabTabPersistenceIntegrationTest extends CustomTabActivityTestBase {
+
+    @Override
+    public void startMainActivity() throws InterruptedException {
+        super.startMainActivity();
+        startCustomTabActivityWithIntent(CustomTabsTestUtils.createMinimalCustomTabIntent(
+                getInstrumentation().getTargetContext(), UrlConstants.ABOUT_BLANK));
+    }
+
+    @MediumTest
+    public void testTabFilesDeletedOnClose() throws InterruptedException {
+        Tab tab = getActivity().getActivityTab();
+        String expectedTabFileName = TabState.getTabStateFilename(tab.getId(), false);
+        String expectedMetadataFileName =
+                getActivity().getTabPersistencePolicyForTest().getStateFileName();
+
+        File stateDir = getActivity().getTabPersistencePolicyForTest().getOrCreateStateDirectory();
+        waitForFileExistState(true, expectedTabFileName, stateDir);
+        waitForFileExistState(true, expectedMetadataFileName, stateDir);
+
+        getActivity().finishAndClose(false);
+
+        waitForFileExistState(false, expectedTabFileName, stateDir);
+        waitForFileExistState(false, expectedMetadataFileName, stateDir);
+    }
+
+    private void waitForFileExistState(
+            final boolean exists, final String fileName, final File filePath)
+                    throws InterruptedException {
+        CriteriaHelper.pollInstrumentationThread(new Criteria(
+                String.format(Locale.US, "File, %s, expected to exist: %b", fileName, exists)) {
+            @Override
+            public boolean isSatisfied() {
+                File file = new File(filePath, fileName);
+                return file.exists() == exists;
+            }
+        });
+    }
+
+}

mirror/chromium

commit 734e4f8a74faa8d7b04f05d0fb902ea7383e706c

Fix CustomTab closing pre-L.
This was broken by 9a6acdd595311651640bb269044e626a530bc723

The change introduced closing all tabs as part of user
initiated close.  CustomTabActivity listens to the closeTab
notification.  If all tabs are closed, it will call finishAndClose.

So, here we were calling finishAndClose and close twice, which
is slightly bad L+ (doing more work than needed), but pre-L,
we need to do Activity retargeting magic to close the task. By
calling this twice, it was actually causing the activity to
be retriggered.

The fix is to prevent finishAndClose from being triggered more
than once.

BUG=671113

Review-Url: https://codereview.chromium.org/2554743004

chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -718,6 +718,7 @@ public class CustomTabActivity extends ChromeActivity {
      * @param reparenting true iff the activity finishes due to tab reparenting.
      */
     public final void finishAndClose(boolean reparenting) {
+        if (mIsClosing) return;
         mIsClosing = true;
         if (!reparenting) {
             // Closing the activity destroys the renderer as well. Re-create a spare renderer some