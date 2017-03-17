828f4690d9cd8e873b78283df7fdae2a4c30d408
ymzhang <ymzhang@chromium.org>
2017-03-17 16:16:03 -0700

    [DirectoryComponentMapping] Script to train model from collected directory-component mapping, and predict component for any input directories.
    Proposal about how to predict suggested component for directory.
    https://goo.gl/xtSMpV
    
    Change-Id: I7a7f0c517daae7689deff129bd957ebd3215954b

---
+++ tools/directory_component_mapping/train_model_test.py
@@ -0,0 +1,11 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+# TODO(ymzhang): complete unittest
+class PredictComponentTests(unittest.TestCase):
+
+  def testBaseCase(self):
+    pass
