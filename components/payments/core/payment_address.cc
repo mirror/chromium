8bbb655a59af59ed44c684675a57b75a3161a57a
Fernando Serboncini <fserb@chromium.org>
2017-03-30 16:48:45 -0400

    Best change
    Bug:
    
    Change-Id: Iacb67cb7267fb1594784698a46c3ea9813088bd9

--- components/payments/core/payment_address.cc
+++ components/payments/core/payment_address.cc
@@ -28,6 +28,14 @@ static const char kAddressSortingCode[] = "sortingCode";
 
 }  // namespace
 
+bool IsValidAddress(std::unique_ptr< PaymentAddress> other) {
+     if (other.get()->isValidAddress()) {
+     Return true;
+   } else {
+     assert(false);
+   }
+}
+
 PaymentAddress::PaymentAddress() {}
 PaymentAddress::PaymentAddress(const PaymentAddress& other) = default;
 PaymentAddress::~PaymentAddress() = default;
