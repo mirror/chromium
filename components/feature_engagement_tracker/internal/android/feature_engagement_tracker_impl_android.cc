aafddc75c1ffb47fe414504ef8f9012e6f141240
Tommy Nyquist <nyquist@chromium.org>
2017-04-03 10:43:50 -0700

    Add simple implementation of in-product help backend.
    This version of the in-product help backend ensures nothing will be
    triggered unless the base::Feature has been enabled. It also ensures
    that only one feature can be triggered per session.
    
    It also changes the public API to now use base::Feature instead of the
    string name. The Java API for this is still using strings, so a helper
    class with all the relevant constants has been added, that matches the
    native base::Feature instances.
    
    A feature can now be enabled on the command line using:
    --enable-features=IPH_DummyFeature
    This dummy feature will be removed once there are real features using
    the backend.
    
    BUG=706309
    
    Change-Id: I1996d9c50d73a93acb26a675c14ebf8770cf0571

--- components/feature_engagement_tracker/internal/android/feature_engagement_tracker_impl_android.cc
+++ components/feature_engagement_tracker/internal/android/feature_engagement_tracker_impl_android.cc
@@ -4,9 +4,13 @@
 
 #include "components/feature_engagement_tracker/internal/android/feature_engagement_tracker_impl_android.h"
 
+#include <vector>
+
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
+#include "base/feature_list.h"
+#include "components/feature_engagement_tracker/internal/feature_list.h"
 #include "components/feature_engagement_tracker/public/feature_engagement_tracker.h"
 #include "jni/FeatureEngagementTrackerImpl_jni.h"
 
@@ -67,6 +71,8 @@ FeatureEngagementTrackerImplAndroid::FeatureEngagementTrackerImplAndroid(
   java_obj_.Reset(env, Java_FeatureEngagementTrackerImpl_create(
                            env, reinterpret_cast<intptr_t>(this))
                            .obj());
+
+  SetUpFeatureMapping();
 }
 
 FeatureEngagementTrackerImplAndroid::~FeatureEngagementTrackerImplAndroid() {
@@ -79,6 +85,13 @@ FeatureEngagementTrackerImplAndroid::GetJavaObject() {
   return base::android::ScopedJavaLocalRef<jobject>(java_obj_);
 }
 
+void FeatureEngagementTrackerImplAndroid::SetUpFeatureMapping() {
+  std::vector<const base::Feature*> features = GetAllFeatures();
+  for (auto it = features.begin(); it != features.end(); ++it) {
+    features_[(*it)->name] = *it;
+  }
+}
+
 void FeatureEngagementTrackerImplAndroid::Event(
     JNIEnv* env,
     const base::android::JavaRef<jobject>& jobj,
@@ -86,7 +99,11 @@ void FeatureEngagementTrackerImplAndroid::Event(
     const base::android::JavaParamRef<jstring>& jprecondition) {
   std::string feature = ConvertJavaStringToUTF8(env, jfeature);
   std::string precondition = ConvertJavaStringToUTF8(env, jprecondition);
-  feature_engagement_tracker_impl_->Event(feature, precondition);
+  if (features_.find(feature) == features_.end()) {
+    DVLOG(1) << "Unable to find feature mapping: " << feature;
+    return;
+  }
+  feature_engagement_tracker_impl_->Event(*features_[feature], precondition);
 }
 
 void FeatureEngagementTrackerImplAndroid::Used(
@@ -94,7 +111,11 @@ void FeatureEngagementTrackerImplAndroid::Used(
     const base::android::JavaRef<jobject>& jobj,
     const base::android::JavaParamRef<jstring>& jfeature) {
   std::string feature = ConvertJavaStringToUTF8(env, jfeature);
-  feature_engagement_tracker_impl_->Used(feature);
+  if (features_.find(feature) == features_.end()) {
+    DVLOG(1) << "Unable to find feature mapping: " << feature;
+    return;
+  }
+  feature_engagement_tracker_impl_->Used(*features_[feature]);
 }
 
 bool FeatureEngagementTrackerImplAndroid::Trigger(
@@ -102,7 +123,11 @@ bool FeatureEngagementTrackerImplAndroid::Trigger(
     const base::android::JavaRef<jobject>& jobj,
     const base::android::JavaParamRef<jstring>& jfeature) {
   std::string feature = ConvertJavaStringToUTF8(env, jfeature);
-  return feature_engagement_tracker_impl_->Trigger(feature);
+  if (features_.find(feature) == features_.end()) {
+    DVLOG(1) << "Unable to find feature mapping: " << feature;
+    return false;
+  }
+  return feature_engagement_tracker_impl_->Trigger(*features_[feature]);
 }
 
 void FeatureEngagementTrackerImplAndroid::Dismissed(
