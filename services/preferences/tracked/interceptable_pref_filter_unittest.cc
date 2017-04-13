bd8d0da223b1ac1338ba4bdccb88cab8bcaa8571
Sam McNally <sammc@chromium.org>
2017-04-13 11:14:56 +1000

    Fix a use-after-free in InterceptablePrefFilter.
    Previously, InterceptablePrefFilter reset |filter_on_load_interceptor_|
    after calling it. However, the callback could result in the object being
    deleted, resulting in the callback being freed before Reset() is called.
    
    This CL fixes this by using ResetAndReturn to move the callback into a
    temporary before calling it.
    
    Bug: 710655
    Change-Id: I841c4157d54e0a31364237f4495384de6664e535

---
+++ services/preferences/tracked/interceptable_pref_filter_unittest.cc
@@ -0,0 +1,54 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/preferences/tracked/interceptable_pref_filter.h"
+
+#include "base/bind.h"
+#include "base/memory/ptr_util.h"
+#include "base/values.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+class TestInterceptablePrefFilter : public InterceptablePrefFilter {
+ public:
+  void FinalizeFilterOnLoad(
+      const PostFilterOnLoadCallback& post_filter_on_load_callback,
+      std::unique_ptr<base::DictionaryValue> pref_store_contents,
+      bool prefs_altered) override {
+    post_filter_on_load_callback.Run(std::move(pref_store_contents),
+                                     prefs_altered);
+  }
+  void FilterUpdate(const std::string& path) override {}
+
+  OnWriteCallbackPair FilterSerializeData(
+      base::DictionaryValue* pref_store_contents) override {
+    return {};
+  }
+};
+
+void ResetUniquePointer(
+    std::unique_ptr<TestInterceptablePrefFilter>* filter,
+    const InterceptablePrefFilter::FinalizeFilterOnLoadCallback&
+        finalize_filter_on_load,
+    std::unique_ptr<base::DictionaryValue> prefs) {
+  finalize_filter_on_load.Run(std::move(prefs), false);
+  filter->reset();
+}
+void PostFilterOnLoad(bool* called,
+                      std::unique_ptr<base::DictionaryValue> prefs,
+                      bool schedule_write) {
+  *called = true;
+}
+
+TEST(InterceptablePrefFilterTest, CallbackDeletes) {
+  auto filter = base::MakeUnique<TestInterceptablePrefFilter>();
+  filter->InterceptNextFilterOnLoad(base::Bind(&ResetUniquePointer, &filter));
+  bool post_filter_on_load_called = false;
+  filter->FilterOnLoad(
+      base::Bind(&PostFilterOnLoad, &post_filter_on_load_called),
+      base::MakeUnique<base::DictionaryValue>());
+}
+
+}  // namespace
