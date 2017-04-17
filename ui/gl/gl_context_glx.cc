67a59b94eb0fe2080411e38feccee9a086915eb8
Sunny Sachanandani <sunnyps@chromium.org>
2017-04-17 13:43:18 -0700

    gpu: Use adaptive vsync for GLX surface.
    Synchronizing with vsync has the advantage of not tearing but comes with
    two disadvantages:
    1. Increased latency when we fall below the refresh rate because we have
       to wait for the next vsync period.
    2. Reduced throughput when multiple windows animate. All Chrome windows
       share one thread (GPU main thread) for issuing swap buffers.
       Therefore, if one window's swap buffers blocks the other windows'
       frame rates fall.
    
    Adaptive vsync disables vsync when we fall below the refresh rate. This
    solves both problems by reducing latency because we don't have to wait
    for vsync if we missed one and not blocking on subsequent swap buffers.
    
    BUG=528803
    
    Change-Id: If23b2fd7562376fa2fc02391fa2aa3a3c3644dd5

--- ui/gl/gl_context_glx.cc
+++ ui/gl/gl_context_glx.cc
@@ -296,10 +296,22 @@ void GLContextGLX::OnSetSwapInterval(int interval) {
   DCHECK(IsCurrent(nullptr));
   if (HasExtension("GLX_EXT_swap_control") &&
       g_driver_glx.fn.glXSwapIntervalEXTFn) {
-    glXSwapIntervalEXT(
-        display_,
-        glXGetCurrentDrawable(),
-        interval);
+    glXSwapIntervalEXT(display_, glXGetCurrentDrawable(), interval);
+  } else if (HasExtension("GLX_EXT_swap_control_tear") &&
+             g_driver_glx.fn.glXSwapIntervalEXTFn) {
+    // Use adaptive vsync if available. Synchronizing with vsync has the
+    // advantage of not tearing but comes with two disadvantages:
+    // 1. Increased latency when we fall below the refresh rate because we have
+    //    to wait for the next vsync period.
+    // 2. Reduced throughput when multiple windows animate. All Chrome windows
+    //    share one thread (GPU main thread) for issuing swap buffers.
+    //    Therefore, if one window's swap buffers blocks the other windows'
+    //    frame rates fall.
+    //
+    // Adaptive vsync disables vsync when we fall below the refresh rate. This
+    // solves both problems by reducing latency because we don't have to wait
+    // for vsync if we missed one and not blocking on subsequent swap buffers.
+    glXSwapIntervalEXT(g_display, glXGetCurrentDrawable(), -interval);
   } else if (HasExtension("GLX_MESA_swap_control") &&
              g_driver_glx.fn.glXSwapIntervalMESAFn) {
     glXSwapIntervalMESA(interval);
