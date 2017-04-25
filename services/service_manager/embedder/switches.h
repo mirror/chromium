bd6de638154ad4caddb35ce794f0f8449b793819
Ken Rockot <rockot@chromium.org>
2017-04-25 13:03:58 -0700

    Migrate Mash to use embedded services
    Extends the Service Manager embedder API to allow embedders to run
    a standalone Service Manager process as well as standalone service
    processes for embedded services.
    
    Eliminates MashRunner and all Mash-related service packaging, instead
    treating Mash services as embedded services. Chrome (via content)
    provides the implementation details necessary to mirror the existing
    MashRunner behavior on top of the Service Manager embedder API.
    
    BUG=654986
    
    Change-Id: I79d8eb66e0b98746fd5b5b7f7ea8e8f6b217b6fd

--- services/service_manager/embedder/switches.h
+++ services/service_manager/embedder/switches.h
@@ -5,12 +5,36 @@
 #ifndef SERVICES_SERVICE_MANAGER_EMBEDDER_SWITCHES_H_
 #define SERVICES_SERVICE_MANAGER_EMBEDDER_SWITCHES_H_
 
+#include "build/build_config.h"
 #include "services/service_manager/embedder/service_manager_embedder_export.h"
 
+namespace service_manager {
 namespace switches {
 
+SERVICE_MANAGER_EMBEDDER_EXPORT extern const char kEnableLogging[];
+SERVICE_MANAGER_EMBEDDER_EXPORT extern const char kProcessType[];
+SERVICE_MANAGER_EMBEDDER_EXPORT extern const char kProcessTypeServiceManager[];
+SERVICE_MANAGER_EMBEDDER_EXPORT extern const char kProcessTypeService[];
 SERVICE_MANAGER_EMBEDDER_EXPORT extern const char kSharedFiles[];
 
+#if defined(OS_WIN)
+
+// Prefetch arguments are used by the Windows prefetcher to disambiguate
+// different execution modes (i.e. process types) of the same executable image
+// so that different types of processes don't trample each others' prefetch
+// behavior.
+//
+// Legal values are integers in the range [1, 8]. We reserve 8 to mean
+// "whatever", and this will ultimately lead to processes with /prefetch:8
+// having inconsistent behavior thus disabling prefetch in practice.
+//
+// TODO(rockot): Make it possible for embedders to override this argument on a
+// per-service basis.
+const char kDefaultServicePrefetchArgument[] = "/prefetch:8";
+
+#endif  // defined(OS_WIN)
+
+}  // namespace switches
 }  // namespace service_manager
 
 #endif  // SERVICES_SERVICE_MANAGER_EMBEDDER_SWITCHES_H_
