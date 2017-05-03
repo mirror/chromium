f347994194be52da9df2b5d9d3b9860173b7dedf
Tommy Nyquist <nyquist@chromium.org>
2017-05-03 14:52:22 -0700

    [Android] Update check for whether current OS platform is O
    Previously we have used the pre-release code names for checking whether
    the current platform Chrome runs on is Android O. This CL updates this
    to instead check against the Build.VERSION.SDK_INT when running on
    release builds of the OS.
    
    This CL also updates the the check for whether Chrome is currently
    targeting Android O, which now also uses the Build.VERSION.SDK_INT.
    
    BUG=685808
    
    Change-Id: I7f17fd5bc8f6489586435818b224b080c391295c

--- base/android/java/src/org/chromium/base/BuildInfo.java
+++ base/android/java/src/org/chromium/base/BuildInfo.java
@@ -172,16 +172,14 @@ public class BuildInfo {
      * @return Whether the current device is running Android O release or newer.
      */
     public static boolean isAtLeastO() {
-        return !"REL".equals(Build.VERSION.CODENAME)
-                && ("O".equals(Build.VERSION.CODENAME) || Build.VERSION.CODENAME.startsWith("OMR"));
+        if ("REL".equals(Build.VERSION.CODENAME)) return Build.VERSION.SDK_INT >= 26;
+        return "O".equals(Build.VERSION.CODENAME) || Build.VERSION.CODENAME.startsWith("OMR");
     }
 
     /**
      * @return Whether the current app targets the SDK for at least O
      */
     public static boolean targetsAtLeastO(Context appContext) {
-        return isAtLeastO()
-                && appContext.getApplicationInfo().targetSdkVersion
-                == Build.VERSION_CODES.CUR_DEVELOPMENT;
+        return isAtLeastO() && appContext.getApplicationInfo().targetSdkVersion >= 26;
     }
 }
