4e96d56b57d1f0a436a9963992c906c86f9cdaa3
Ken Rockot <rockot@chromium.org>
2017-05-04 14:04:47 -0700

    Make some browser interfaces accessible to all child processes
    The discardable shared memory manager and resource_coordinator's
    memory instrumentation interfaces may be usable by any child process,
    and memory instrumentation is in fact already in use by GPU and
    renderers.
    
    This moves these interface binders to a shared ConnectionFilter attached
    to all browser-side ServiceManagerConnection instances, ensuring that
    all content child processes can bind the interfaces.
    
    BUG=718559
    
    Change-Id: I8a1a1ff5b749365d020f72480e25ba2d88a025b3

---
+++ content/browser/service_manager/common_browser_interfaces.cc
@@ -0,0 +1,88 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/service_manager/common_browser_interfaces.h"
+
+#include <memory>
+#include <utility>
+
+#include "base/callback.h"
+#include "base/memory/ptr_util.h"
+#include "base/memory/ref_counted.h"
+#include "base/task_runner.h"
+#include "components/discardable_memory/service/discardable_shared_memory_manager.h"
+#include "content/browser/browser_main_loop.h"
+#include "content/public/common/connection_filter.h"
+#include "content/public/common/service_manager_connection.h"
+#include "mojo/public/cpp/bindings/interface_request.h"
+#include "services/resource_coordinator/memory/coordinator/coordinator_impl.h"
+#include "services/service_manager/public/cpp/binder_registry.h"
+
+namespace content {
+
+namespace {
+
+class ConnectionFilterImpl : public ConnectionFilter {
+ public:
+  ConnectionFilterImpl() : task_runner_(base::ThreadTaskRunnerHandle::Get()) {
+    auto* coordinator = memory_instrumentation::CoordinatorImpl::GetInstance();
+    RegisterInterface(base::Bind(
+        &memory_instrumentation::CoordinatorImpl::BindCoordinatorRequest,
+        base::Unretained(coordinator)));
+
+    auto* manager =
+        BrowserMainLoop::GetInstance()->discardable_shared_memory_manager();
+    RegisterInterface(
+        base::Bind(&discardable_memory::DiscardableSharedMemoryManager::Bind,
+                   base::Unretained(manager)));
+  }
+
+  ~ConnectionFilterImpl() override {}
+
+ private:
+  template <typename Interface>
+  using InterfaceBinder =
+      base::Callback<void(const service_manager::BindSourceInfo&,
+                          mojo::InterfaceRequest<Interface>)>;
+
+  // ConnectionFilter:
+  void OnBindInterface(const service_manager::BindSourceInfo& source_info,
+                       const std::string& interface_name,
+                       mojo::ScopedMessagePipeHandle* interface_pipe,
+                       service_manager::Connector* connector) override {
+    if (registry_.CanBindInterface(interface_name)) {
+      registry_.BindInterface(source_info, interface_name,
+                              std::move(*interface_pipe));
+    }
+  }
+
+  template <typename Interface>
+  static void BindOnTaskRunner(
+      const scoped_refptr<base::TaskRunner>& task_runner,
+      const InterfaceBinder<Interface>& binder,
+      const service_manager::BindSourceInfo& source_info,
+      mojo::InterfaceRequest<Interface> request) {
+    task_runner->PostTask(
+        FROM_HERE, base::BindOnce(binder, source_info, std::move(request)));
+  }
+
+  template <typename Interface>
+  void RegisterInterface(const InterfaceBinder<Interface>& binder) {
+    registry_.AddInterface(
+        base::Bind(&BindOnTaskRunner<Interface>, task_runner_, binder));
+  }
+
+  const scoped_refptr<base::TaskRunner> task_runner_;
+  service_manager::BinderRegistry registry_;
+
+  DISALLOW_COPY_AND_ASSIGN(ConnectionFilterImpl);
+};
+
+}  // namespace
+
+void RegisterCommonBrowserInterfaces(ServiceManagerConnection* connection) {
+  connection->AddConnectionFilter(base::MakeUnique<ConnectionFilterImpl>());
+}
+
+}  // namespace content
