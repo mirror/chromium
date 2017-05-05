44f7cf96dde5392cdcce84a56a9ca8571d76d036
Mircea Trofin <mtrofin@chromium.org>
2017-05-05 15:18:08 -0700

    [wasm] TODO & tracking bug for IDB behavioral difference.
    Bug:chromium:719053
    Change-Id: Ic4eec1ca6e3c9d723d19c5661fe196c092762808

--- third_party/WebKit/Source/modules/indexeddb/IDBObjectStore.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBObjectStore.cpp
@@ -379,6 +379,8 @@ IDBRequest* IDBObjectStore::put(ScriptState* script_state,
   Vector<WebBlobInfo> blob_info;
   SerializedScriptValue::SerializeOptions options;
   options.blob_info = &blob_info;
+  // TODO(wasm): this wasm behavior differs from other browsers.
+  // See https://crbug.com/719053
   options.write_wasm_to_stream =
       ExecutionContext::From(script_state)->IsSecureContext();
   options.for_storage = true;
