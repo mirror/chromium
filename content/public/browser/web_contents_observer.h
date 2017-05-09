09f4a68b60fe8e0ed8653839cd9133c540acf9f9
Sam McNally <sammc@chromium.org>
2017-05-09 15:22:15 +1000

    Report manifest URL changes to WebContentsObservers.
    Bug: 707179
    Change-Id: I57c859d27c7f940002cceb2923e6418a75acb92a

--- content/public/browser/web_contents_observer.h
+++ content/public/browser/web_contents_observer.h
@@ -426,6 +426,11 @@ class CONTENT_EXPORT WebContentsObserver : public IPC::Listener,
   // Notifes that a CompositorFrame was received from the renderer.
   virtual void DidReceiveCompositorFrame() {}
 
+  // Notifies that the manifest URL for the main frame changed to
+  // |manifest_url|.
+  virtual void DidUpdateWebManifestURL(
+      const base::Optional<GURL>& manifest_url) {}
+
   // IPC::Listener implementation.
   bool OnMessageReceived(const IPC::Message& message) override;
 
