8481020cda47267ad6269344e226b5cde3772284
Tommy Nyquist <nyquist@chromium.org>
2017-05-11 00:53:32 -0700

    Add Result struct for ConditionValidator API.
    Before this CL, the ConditionValidator::MeetsConditions(...) method only
    returned a bool value whether all conditions were met. This made it
    harder to track what went wrong in a generic way across all validators,
    so this CL changes the return value to be a struct that contains
    information about all conditions, and whether they were met.
    
    In addition, to prepare for an upcoming CL that will need to use the
    current day to figure out whether conditions are met, that parameter
    is added in the current CL.
    
    BUG=706309
    
    Change-Id: I73815d88facfc1fbee9e6a0058790cd4b73dc714

---
+++ components/feature_engagement_tracker/internal/condition_validator.cc
@@ -0,0 +1,30 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/feature_engagement_tracker/internal/condition_validator.h"
+
+namespace feature_engagement_tracker {
+
+ConditionValidator::Result::Result(bool initial_values)
+    : model_ready_ok(initial_values),
+      currently_showing_ok(initial_values),
+      config_ok(initial_values),
+      used_ok(initial_values),
+      trigger_ok(initial_values),
+      preconditions_ok(initial_values),
+      session_rate_ok(initial_values) {}
+
+bool ConditionValidator::Result::NoErrors() {
+  VLOG(0) << "nyquist: Result:"
+          << "  model_ready_ok = " << model_ready_ok
+          << ", currently_showing_ok = " << currently_showing_ok
+          << ", config_ok = " << config_ok << ", used_ok = " << used_ok
+          << ", trigger_ok = " << trigger_ok
+          << ", preconditions_ok = " << preconditions_ok
+          << ", session_rate_ok = " << session_rate_ok;
+  return model_ready_ok && currently_showing_ok && config_ok && used_ok &&
+         trigger_ok && preconditions_ok && session_rate_ok;
+}
+
+}  // namespace feature_engagement_tracker
