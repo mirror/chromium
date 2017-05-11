a313418daec9eddf7dfdfee08526c54fea13d1c9
Christian Dullweber <dullweber@chromium.org>
2017-05-11 18:11:33 +0200

    Move code from ProfileStatisticsAggregator to BookmarkCounter
    This change creates a new BrowsingDataCounter to replace the bookmark counting
    code in ProfileStatisticsAggregator.
    An |InitWithoutPref()| method is added to BrowsingDataCounter to allow
    the usage of counters without observing preference changes.
    The ownership of ProfileStatisticsAggregator is changed to allow deleting it
    while counters haven't finished because we wouldn't care about the result
    anyway when there is no external reference to the counter.
    
    Bug: 716267
    Change-Id: Ica771467387efd6150308bd3d22bc7360f5ab18a

---
+++ components/browsing_data/core/counters/bookmark_counter.h
@@ -0,0 +1,41 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_BROWSING_DATA_CORE_COUNTERS_BOOKMARK_COUNTER_H_
+#define COMPONENTS_BROWSING_DATA_CORE_COUNTERS_BOOKMARK_COUNTER_H_
+
+#include <memory>
+
+#include "components/browsing_data/core/counters/browsing_data_counter.h"
+
+namespace bookmarks {
+class BookmarkModel;
+}
+
+namespace browsing_data {
+
+class BookmarkModelHelper;
+
+class BookmarkCounter : public browsing_data::BrowsingDataCounter {
+ public:
+  static const char kFakePrefName[];
+
+  explicit BookmarkCounter(bookmarks::BookmarkModel* bookmark_model);
+  ~BookmarkCounter() override;
+
+  void OnInitialized() override;
+
+  const char* GetPrefName() const override;
+
+ private:
+  void Count() override;
+  void CountBookmarks(bookmarks::BookmarkModel* bookmark_model);
+
+  bookmarks::BookmarkModel* bookmark_model_;
+  std::unique_ptr<BookmarkModelHelper> bookmark_model_helper_;
+};
+
+}  // namespace browsing_data
+
+#endif  // COMPONENTS_BROWSING_DATA_CORE_COUNTERS_BOOKMARK_COUNTER_H_
---
+++ components/browsing_data/core/counters/bookmark_model_helper.cc
@@ -0,0 +1,35 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/browsing_data/core/counters/bookmark_model_helper.h"
+
+#include "components/bookmarks/browser/bookmark_model.h"
+
+namespace browsing_data {
+
+BookmarkModelHelper::BookmarkModelHelper(
+    bookmarks::BookmarkModel* bookmark_model,
+    BookmarkModelCallback callback)
+    : bookmark_model_(bookmark_model), callback_(std::move(callback)) {
+  DCHECK(!bookmark_model_->loaded());
+  bookmark_model_->AddObserver(this);
+}
+
+BookmarkModelHelper::~BookmarkModelHelper() {
+  if (bookmark_model_)
+    bookmark_model_->RemoveObserver(this);
+}
+
+void BookmarkModelHelper::BookmarkModelLoaded(bookmarks::BookmarkModel* model,
+                                              bool ids_reassigned) {
+  model->RemoveObserver(this);
+  std::move(callback_).Run(model);
+};
+
+void BookmarkModelHelper::BookmarkModelBeingDeleted(
+    bookmarks::BookmarkModel* model) {
+  bookmark_model_ = nullptr;
+}
+
+}  // namespace browsing_data
