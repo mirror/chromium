e05fedaf47a86c2d81921f223e704f6cbefcc30a
Ken Rockot <rockot@chromium.org>
2017-05-12 12:16:59 -0700

    Mojo C++ Bindings: Remove redundant InterfaceRequest APIs
    Removes InterfaceRequest::Bind as it's redundant given the move
    assignment operator and the new ctor over ScopedMessagePipeHandle.
    
    Also removes the MakeRequest overload which takes a
    ScopedMessagePipeHandle since
    
        MakeRequest<Foo>(pipe)
    
    is strictly more verbose than
    
        FooRequest(pipe)
    
    BUG=721507
    R=yzshen@chromium.org
    TBR=jam@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ic5cc4c8b926a2003c464abd0ddd29c8db347ce17

--- services/service_manager/background/tests/background_service_manager_unittest.cc
+++ services/service_manager/background/tests/background_service_manager_unittest.cc
@@ -51,7 +51,7 @@ TEST(BackgroundServiceManagerTest, MAYBE_Basic) {
   base::MessageLoop message_loop;
   mojom::ServicePtr service;
   ServiceContext service_context(base::MakeUnique<ServiceImpl>(),
-                                 mojom::ServiceRequest(&service));
+                                 mojo::MakeRequest(&service));
   background_service_manager.RegisterService(
       Identity(kTestName, mojom::kRootUserID), std::move(service), nullptr);
 
@@ -80,7 +80,7 @@ TEST(BackgroundServiceManagerTest, SetInstanceQuitCallback) {
   base::MessageLoop message_loop;
   mojom::ServicePtr service;
   ServiceContext service_context(base::MakeUnique<ServiceImpl>(),
-                                 mojom::ServiceRequest(&service));
+                                 mojo::MakeRequest(&service));
   background_service_manager.RegisterService(
       Identity(kTestName, mojom::kRootUserID), std::move(service), nullptr);
 
