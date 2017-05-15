b3eec0b2651cb3f4e5dc4e880c62cf1463d94fa2
Adrienne Walker <enne@chromium.org>
2017-05-15 15:52:09 -0700

    [temp] draw_properties_ct telemetry patch
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I29ff3aa084e2723c30ae328719ec69d24edbf53f

--- tools/perf/measurements/draw_properties.py
+++ tools/perf/measurements/draw_properties.py
@@ -21,33 +21,83 @@ class DrawProperties(legacy_page_test.LegacyPageTest):
   def WillNavigateToPage(self, page, tab):
     del page  # unused
     config = tracing_config.TracingConfig()
-    config.chrome_trace_config.category_filter.AddDisabledByDefault(
-        'disabled-by-default-cc.debug.cdp-perf')
+    config.chrome_trace_config.category_filter.AddIncludedCategory(
+        'cc')
     config.enable_chrome_trace = True
     tab.browser.platform.tracing_controller.StartTracing(config)
 
   def ComputeAverageOfDurations(self, timeline_model, name):
     events = timeline_model.GetAllEventsOfName(name)
     event_durations = [d.duration for d in events]
-    assert event_durations, 'Failed to find durations'
 
     duration_sum = sum(event_durations)
     duration_count = len(event_durations)
+    if duration_count == 0 :
+        return 0
     duration_avg = duration_sum / duration_count
     return duration_avg
+
+  def GetCount(self, timeline_model, name):
+    events = timeline_model.GetAllEventsOfName(name)
+    event_durations = [d.duration for d in events]
+
+    duration_count = len(event_durations)
+    return duration_count
 
   def ValidateAndMeasurePage(self, page, tab, results):
     del page  # unused
     timeline_data = tab.browser.platform.tracing_controller.StopTracing()
     timeline_model = model.TimelineModel(timeline_data)
 
-    pt_avg = self.ComputeAverageOfDurations(
+    bpt_avg = self.ComputeAverageOfDurations(
         timeline_model,
-        'LayerTreeHostCommon::ComputeVisibleRectsWithPropertyTrees')
+        'LayerTreeHostInProcess::UpdateLayers::BuildPropertyTrees')
+    cdp_avg = self.ComputeAverageOfDurations(
+        timeline_model,
+        'LayerTreeImpl::UpdateDrawProperties::CalculateDrawProperties')
+    root = len(timeline_model.GetAllEventsOfName('Root'))
+    hide = len(timeline_model.GetAllEventsOfName('HideSubtree'))
+    nest_hide = len(timeline_model.GetAllEventsOfName('NestedHideSubtree'))
+    opacity = len(timeline_model.GetAllEventsOfName('Opacity'))
+    pot_opacity = len(timeline_model.GetAllEventsOfName('PotentialOpacity'))
+    proxied_opacity = len(timeline_model.GetAllEventsOfName('Proxied'))
+    rs = len(timeline_model.GetAllEventsOfName('RS'))
 
     results.AddValue(scalar.ScalarValue(
-        results.current_page, 'PT_avg_cost', 'ms', pt_avg,
-        description='Average time spent processing property trees'))
+        results.current_page, 'PT_build_cost', 'ms', bpt_avg,
+        description='Average time spent building property trees'))
+
+    results.AddValue(scalar.ScalarValue(
+        results.current_page, 'Impl_cdp_cost', 'ms', cdp_avg,
+        description='Average time spent calculating draw properties'))
+
+    results.AddValue(scalar.ScalarValue(
+        results.current_page, 'num_root', '', root,
+        description='Num root'))
+
+    results.AddValue(scalar.ScalarValue(
+        results.current_page, 'num_hide_subtree', '', hide,
+        description='Num Hidden Subtree'))
+
+    results.AddValue(scalar.ScalarValue(
+        results.current_page, 'num_nested_hide_subtree', '', nest_hide,
+        description='Num Nested Hidden Subtree'))
+
+    results.AddValue(scalar.ScalarValue(
+        results.current_page, 'num_opacity', '', opacity,
+        description='Num opacity'))
+
+    results.AddValue(scalar.ScalarValue(
+        results.current_page, 'num_potential_op', '', pot_opacity,
+        description='Num Potential opacity'))
+
+    results.AddValue(scalar.ScalarValue(
+        results.current_page, 'num_proxied_op', '', proxied_opacity,
+        description='Num Proxied opacity'))
+
+    results.AddValue(scalar.ScalarValue(
+        results.current_page, 'rs', '', rs,
+        description='Num Render Surfaces'))
 
   def DidRunPage(self, platform):
     tracing_controller = platform.tracing_controller
