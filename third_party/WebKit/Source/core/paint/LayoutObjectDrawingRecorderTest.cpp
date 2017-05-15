21d59352c5bd8d727313953b0b9525d45e60c5e0
danakj <danakj@chromium.org>
2017-05-15 12:28:45 -0400

    Remove cullRect() from PaintOpBuffer.
    Pass it directly to RecordPaintCanvas and ToSkPicture, and other skia
    methods which is where it is used.
    
    This allows us to more easily get rid of cc::DisplayItem and its
    subclasses, replacing them with a PaintOpBuffer in DisplayItemList
    directly instead. The difficulty I faced with that was that if
    DisplayItemList has a single PaintOpBuffer, then it has a single
    cull rect. However when painting, each "batch" of PaintOps can
    have a different cull rect (corresponding to the PaintOps that
    would have been in a single DisplayItem before). So, instead the
    cull rect should be a property of recording at the
    RecordPaintCanvas level, which is a temporary object. As such,
    creators of cc::RecordPaintCanvas (mostly thru cc::PaintRecorder)
    need to manage the cull rect themselves to pass to things that
    want to use it with the cc::PaintOpBuffer (aka cc::PaintRecord at
    this time).
    
    R=enne@chromium.org
    
    Bug: 671433
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I8a7acf2215caca5678bc6a1c579054f196cee4ea

--- third_party/WebKit/Source/core/paint/LayoutObjectDrawingRecorderTest.cpp
+++ third_party/WebKit/Source/core/paint/LayoutObjectDrawingRecorderTest.cpp
@@ -119,7 +119,8 @@ FloatRect DrawAndGetCullRect(PaintController& controller,
   controller.CommitNewDisplayItems();
   const auto& drawing = static_cast<const DrawingDisplayItem&>(
       controller.GetDisplayItemList()[0]);
-  return drawing.GetPaintRecord()->cullRect();
+  DCHECK(drawing.GetPaintRecordBounds() == FloatRect(bounds));
+  return FloatRect(bounds);
 }
 
 TEST_F(LayoutObjectDrawingRecorderTest, CullRectMatchesProvidedClip) {
