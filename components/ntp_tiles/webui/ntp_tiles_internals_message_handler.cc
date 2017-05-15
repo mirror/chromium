696d3e6066172184b1676849966f972b2f9d903b
Chris Pickel <sfiera@chromium.org>
2017-05-15 17:26:17 +0200

    DO_NOT_SUBMIT: Gerrit
    Bug:
    Change-Id: I77514c01e7c7e37bc3cf63f37f1009b6b6c9729a

--- components/ntp_tiles/webui/ntp_tiles_internals_message_handler.cc
+++ components/ntp_tiles/webui/ntp_tiles_internals_message_handler.cc
@@ -1,6 +1,8 @@
 // Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+//
+// Gerrit
 
 #include "components/ntp_tiles/webui/ntp_tiles_internals_message_handler.h"
 
