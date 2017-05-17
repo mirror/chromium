4a2fcc3d969272f4fac9f3a360eb3811db2e0359
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-05-17 12:35:02 -0700

    ImageCapture: reject applyConstraints()/setOptions() if there isn't any supported member
    This CL corrects the code so that 
     theTrack.applyConstraints({ advanced: [{blergh : 100}]});
    and
     theImageCapturer.setOptions({ foofoo: 100 });
    reject due to unknown dictionary members, and also adds
    Layout Tests entries.
    
    To reject, the logic in HasNonImageCaptureConstraints() is
    inverted, checking for the presence of image constraints (instead
    of checking for the absence of non-image constraints, which lets
    garbage through).  A straightforward check is added to setOptions().
    
    Note that WebIdl will silently drop unsupported dictionary members.
    
    Bug: 723779
    Change-Id: I3c911ea4d319deab828c135a9f88a3d5af0b06bf

--- third_party/WebKit/Source/modules/imagecapture/ImageCapture.cpp
+++ third_party/WebKit/Source/modules/imagecapture/ImageCapture.cpp
@@ -153,6 +153,13 @@ ScriptPromise ImageCapture::setOptions(ScriptState* script_state,
         kInvalidStateError, "The associated Track is in an invalid state."));
     return promise;
   }
+  if (!photo_settings.hasImageHeight() && !photo_settings.hasImageWidth() &&
+      !photo_settings.hasRedEyeReduction() &&
+      !photo_settings.hasFillLightMode()) {
+    resolver->Reject(DOMException::Create(kNotSupportedError,
+                                          "No Image Capture settings found"));
+    return promise;
+  }
 
   if (!service_) {
     resolver->Reject(DOMException::Create(kNotFoundError, kNoServiceError));
@@ -565,16 +572,13 @@ bool ImageCapture::HasNonImageCaptureConstraints(
 
   const auto& advanced_constraints = constraints.advanced();
   for (const auto& constraint : advanced_constraints) {
-    if (constraint.hasWidth() || constraint.hasHeight() ||
-        constraint.hasAspectRatio() || constraint.hasFrameRate() ||
-        constraint.hasFacingMode() || constraint.hasVolume() ||
-        constraint.hasSampleRate() || constraint.hasSampleSize() ||
-        constraint.hasEchoCancellation() || constraint.hasLatency() ||
-        constraint.hasChannelCount() || constraint.hasDeviceId() ||
-        constraint.hasGroupId() || constraint.hasVideoKind() ||
-        constraint.hasDepthNear() || constraint.hasDepthFar() ||
-        constraint.hasFocalLengthX() || constraint.hasFocalLengthY() ||
-        constraint.hasMandatory() || constraint.hasOptional()) {
+    if (!constraint.hasWhiteBalanceMode() && !constraint.hasExposureMode() &&
+        !constraint.hasFocusMode() && !constraint.hasPointsOfInterest() &&
+        !constraint.hasExposureCompensation() &&
+        !constraint.hasColorTemperature() && !constraint.hasIso() &&
+        !constraint.hasBrightness() && !constraint.hasContrast() &&
+        !constraint.hasSaturation() && !constraint.hasSharpness() &&
+        !constraint.hasZoom() && !constraint.hasTorch()) {
       return true;
     }
   }
