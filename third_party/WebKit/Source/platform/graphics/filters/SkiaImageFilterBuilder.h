ec6b9a8211f000cbfdd25fda8db5441d1093dfcb
danakj <danakj@chromium.org>
2017-05-17 15:25:48 -0400

    Avoid quickReject() when rastering a cc::DrawingDisplayItem.
    The Rtree should already have avoided any items which don't intersect
    with what we want to raster.
    
    R=wkorman@chromium.org
    
    Bug: 671433
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I1556d618fa2d276525787fb903bdfa2d5aba653d

--- third_party/WebKit/Source/platform/graphics/filters/SkiaImageFilterBuilder.h
+++ third_party/WebKit/Source/platform/graphics/filters/SkiaImageFilterBuilder.h
@@ -37,6 +37,7 @@ namespace blink {
 
 class BoxReflection;
 class FilterEffect;
+class FloatRect;
 
 namespace SkiaImageFilterBuilder {
 
@@ -52,7 +53,9 @@ PLATFORM_EXPORT void PopulateSourceGraphicImageFilters(
     FilterEffect* source_graphic,
     sk_sp<SkImageFilter> input,
     ColorSpace input_color_space);
-PLATFORM_EXPORT void BuildSourceGraphic(FilterEffect*, sk_sp<PaintRecord>);
+PLATFORM_EXPORT void BuildSourceGraphic(FilterEffect*,
+                                        sk_sp<PaintRecord>,
+                                        const FloatRect& record_bounds);
 
 PLATFORM_EXPORT sk_sp<SkImageFilter> BuildBoxReflectFilter(
     const BoxReflection&,
