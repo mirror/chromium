46b0a2ba4f16db09da870f98abc0d6a2e5a5e164
Joel Hockey <joelhockey@chromium.org>
2017-05-18 15:30:43 -0700

    Tests passing with deferred operations.
    delete FrameView::children_
    
    Bug:637460
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I0385d15b5516aee34b1a29758eeaf8f23951dc26

--- third_party/WebKit/Source/core/plugins/PluginView.h
+++ third_party/WebKit/Source/core/plugins/PluginView.h
@@ -39,7 +39,6 @@
 namespace blink {
 
 class Event;
-class FrameView;
 class ResourceResponse;
 class WebLayer;
 
@@ -52,8 +51,8 @@ class CORE_EXPORT PluginView : public FrameOrPlugin {
 
   bool IsPluginView() const override { return true; }
 
-  virtual void SetParent(FrameView*) = 0;
-  virtual FrameView* Parent() const = 0;
+  virtual void SetFrameOrPluginState(FrameOrPluginState) = 0;
+  virtual FrameOrPluginState GetFrameOrPluginState() const = 0;
   virtual void SetParentVisible(bool) = 0;
   virtual void SetFocused(bool, WebFocusType) = 0;
   virtual void FrameRectsChanged() = 0;
