bb098fff03382f0db5526ffc820d96dad9f16167
Tommy Nyquist <nyquist@chromium.org>
2017-05-18 17:12:06 -0700

    Add support for tracking availability window.
    The FeatureEngagementTracker is missing functionality to track when a
    particular feature has been made available to the end user.
    
    This CL starts that work by adding the tracker itself and ensures it
    can load data from an underlying store.
    
    Upcoming CLs will include the store itself, and also hooking this up to
    the FeatureEngagementTrackerImpl construction and use it in the
    FeatureConfigConditionValidator .
    
    BUG=723007, 706309
    
    Change-Id: I95469d2a341d7483f068ecd0a099f4b452bb44ba

---
+++ components/feature_engagement_tracker/internal/availability_tracker_impl.h
@@ -0,0 +1,81 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_FEATURE_ENGAGEMENT_TRACKER_INTERNAL_AVAILABILITY_TRACKER_IMPL_H_
+#define COMPONENTS_FEATURE_ENGAGEMENT_TRACKER_INTERNAL_AVAILABILITY_TRACKER_IMPL_H_
+
+#include <stdint.h>
+
+#include <map>
+#include <memory>
+
+#include "base/callback.h"
+#include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "components/feature_engagement_tracker/internal/availability_tracker.h"
+#include "components/feature_engagement_tracker/public/feature_list.h"
+
+namespace base {
+struct Feature;
+}  // namespace base
+
+namespace feature_engagement_tracker {
+class AvailabilityStore;
+
+// An AvailabilityTracker tracks when each feature was made available to an
+// end user.
+class AvailabilityTrackerImpl : public AvailabilityTracker {
+ public:
+  // Invoked when the availability data has finished loading, and whether the
+  // load was a success. In the case of a failure, it is invalid to ever call
+  // GetAvailability(...).
+  using OnLoadedCallback = base::Callback<void(bool success)>;
+
+  // The |store| is used to load all the availability data.
+  // The |feature_filter| is used to filter the data from the |store| and ensure
+  // that only features listed in this filter are tracked in this class. For
+  // features that are in the |feature_filter|, but not in the |store|, they are
+  // added as known, but with an availability time of 0u.
+  AvailabilityTrackerImpl(std::unique_ptr<AvailabilityStore> store,
+                          std::unique_ptr<FeatureVector> feature_filter);
+  ~AvailabilityTrackerImpl() override;
+
+  // Loads all the availabilities, and stores them for later retrieval.
+  // It is only valid to call this method once.
+  void Load(const OnLoadedCallback& callback);
+
+  // Returns true iff the given |feature| is part of the known features.
+  bool HasAvailabilityForTest(const base::Feature& feature) const;
+
+  // AvailabilityTracker implementation.
+  uint32_t GetAvailability(const base::Feature& feature) const override;
+
+ private:
+  // This is invoked when the store has completed loading.
+  void OnStoreLoadComplete(
+      const OnLoadedCallback& callback,
+      bool success,
+      std::unique_ptr<std::map<const base::Feature*, uint32_t>>);
+
+  // The underlying store. This will be destructed after the initial load has
+  // been completed.
+  std::unique_ptr<AvailabilityStore> store_;
+
+  // Stores the day number since epoch (1970-01-01) in the local timezone for
+  // when the particular |feature| was made available.
+  std::map<const base::Feature*, uint32_t> feature_availabilities_;
+
+  // In case the database contains more features than what is required, this
+  // filter is used to ensure that |feature_availabilities_| only contains the
+  // features that are listed in this filter.
+  std::unique_ptr<FeatureVector> feature_filter_;
+
+  base::WeakPtrFactory<AvailabilityTrackerImpl> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(AvailabilityTrackerImpl);
+};
+
+}  // namespace feature_engagement_tracker
+
+#endif  // COMPONENTS_FEATURE_ENGAGEMENT_TRACKER_INTERNAL_AVAILABILITY_TRACKER_IMPL_H_
---
+++ components/feature_engagement_tracker/internal/availability_tracker_impl_unittest.cc
@@ -0,0 +1,137 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/feature_engagement_tracker/internal/availability_tracker_impl.h"
+
+#include <memory>
+#include <utility>
+#include <vector>
+
+#include "base/bind.h"
+#include "base/callback.h"
+#include "base/macros.h"
+#include "base/memory/ptr_util.h"
+#include "base/optional.h"
+#include "components/feature_engagement_tracker/internal/availability_store.h"
+#include "components/feature_engagement_tracker/public/feature_list.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using testing::_;
+using testing::SaveArg;
+
+namespace feature_engagement_tracker {
+
+namespace {
+const base::Feature kTestFeatureFoo{"test_foo",
+                                    base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kTestFeatureBar{"test_bar",
+                                    base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kTestFeatureQux{"test_qux",
+                                    base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kTestFeatureNop{"test_nop",
+                                    base::FEATURE_DISABLED_BY_DEFAULT};
+
+class MockAvailabilityStore : public AvailabilityStore {
+ public:
+  MockAvailabilityStore() = default;
+  ~MockAvailabilityStore() override = default;
+
+  // AvailabilityStore implementation.
+  MOCK_METHOD1(Load, void(const OnLoadedCallback&));
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MockAvailabilityStore);
+};
+
+class AvailabilityTrackerImplTest : public testing::Test {
+ public:
+  AvailabilityTrackerImplTest() {
+    load_callback_ = base::Bind(&AvailabilityTrackerImplTest::OnTrackerLoaded,
+                                base::Unretained(this));
+  }
+  ~AvailabilityTrackerImplTest() override = default;
+
+  // SetUpTracker exists so that the filter can be changed for any test.
+  void SetUpTracker(std::vector<const base::Feature*> feature_filter) {
+    auto mocked_store = base::MakeUnique<MockAvailabilityStore>();
+    mocked_store_ = mocked_store.get();
+    tracker_ = base::MakeUnique<AvailabilityTrackerImpl>(
+        std::move(mocked_store),
+        base::MakeUnique<FeatureVector>(feature_filter));
+  }
+
+  void OnTrackerLoaded(bool success) { load_success_ = success; }
+
+ protected:
+  MockAvailabilityStore* mocked_store_;
+  std::unique_ptr<AvailabilityTrackerImpl> tracker_;
+
+  // Load callback tracking.
+  base::Optional<bool> load_success_;
+  AvailabilityTrackerImpl::OnLoadedCallback load_callback_;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(AvailabilityTrackerImplTest);
+};
+
+}  // namespace
+
+TEST_F(AvailabilityTrackerImplTest, LoadTwoOfThreeFeatures) {
+  FeatureVector features;
+  features.push_back(&kTestFeatureFoo);  // In DB.
+  features.push_back(&kTestFeatureBar);  // In DB.
+  features.push_back(&kTestFeatureNop);  // Not in DB. Should become 0u.
+  SetUpTracker(features);
+
+  AvailabilityStore::OnLoadedCallback callback;
+  EXPECT_CALL(*mocked_store_, Load(_)).WillOnce(testing::SaveArg<0>(&callback));
+  tracker_->Load(load_callback_);
+
+  std::map<const base::Feature*, uint32_t> result_map;
+  result_map[&kTestFeatureFoo] = 100u;  // In filter.
+  result_map[&kTestFeatureBar] = 200u;  // In filter.
+  result_map[&kTestFeatureQux] = 300u;  // Not in filter.
+
+  callback.Run(true, base::MakeUnique<std::map<const base::Feature*, uint32_t>>(
+                         result_map));
+  ASSERT_TRUE(load_success_.value());
+
+  ASSERT_TRUE(tracker_->HasAvailabilityForTest(kTestFeatureFoo));
+  ASSERT_TRUE(tracker_->HasAvailabilityForTest(kTestFeatureBar));
+  EXPECT_FALSE(tracker_->HasAvailabilityForTest(kTestFeatureQux));
+  ASSERT_TRUE(tracker_->HasAvailabilityForTest(kTestFeatureNop));
+
+  // The foo and bar features had values in the DB.
+  EXPECT_EQ(100u, tracker_->GetAvailability(kTestFeatureFoo));
+  EXPECT_EQ(200u, tracker_->GetAvailability(kTestFeatureBar));
+
+  // The nop-feature should have been inserted with 0u as the value.
+  EXPECT_EQ(0u, tracker_->GetAvailability(kTestFeatureNop));
+
+  // TODO(nyquist): Verify that the AvailabilityStore was destructed.
+}
+
+TEST_F(AvailabilityTrackerImplTest, FailLoad) {
+  FeatureVector features;
+  features.push_back(&kTestFeatureFoo);
+  SetUpTracker(features);
+
+  AvailabilityStore::OnLoadedCallback callback;
+  EXPECT_CALL(*mocked_store_, Load(_)).WillOnce(testing::SaveArg<0>(&callback));
+  tracker_->Load(load_callback_);
+
+  callback.Run(false,
+               base::MakeUnique<std::map<const base::Feature*, uint32_t>>());
+  EXPECT_FALSE(load_success_.value());
+
+  EXPECT_FALSE(tracker_->HasAvailabilityForTest(kTestFeatureFoo));
+  EXPECT_FALSE(tracker_->HasAvailabilityForTest(kTestFeatureBar));
+  EXPECT_FALSE(tracker_->HasAvailabilityForTest(kTestFeatureQux));
+  EXPECT_FALSE(tracker_->HasAvailabilityForTest(kTestFeatureNop));
+
+  // TODO(nyquist): Verify that the AvailabilityStore was destructed.
+}
+
+}  // namespace feature_engagement_tracker
