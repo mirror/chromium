1038b60f0153c12cd08da38931352edf00f503e2
Joel Hockey <joelhockey@chromium.org>
2017-05-21 22:19:13 -0700

    [TEST] 3 state no SetParent
    Bug:
    Change-Id: I6956377ca5caadd37ab203ad9919bd785aec2a72

--- third_party/WebKit/Source/core/paint/PaintTiming.cpp
+++ third_party/WebKit/Source/core/paint/PaintTiming.cpp
@@ -90,7 +90,12 @@ void PaintTiming::SetFirstMeaningfulPaintCandidate(double timestamp) {
   if (first_meaningful_paint_candidate_)
     return;
   first_meaningful_paint_candidate_ = timestamp;
-  if (GetFrame() && GetFrame()->View() && !GetFrame()->View()->Parent()) {
+  if (GetFrame() && GetFrame()->View())
+    DCHECK((GetFrame()->View()->Parent() == nullptr) ==
+           (GetFrame()->View()->GetFrameOrPluginState() !=
+            FrameOrPlugin::kAttached));
+  if (GetFrame() && GetFrame()->View() &&
+      GetFrame()->View()->GetFrameOrPluginState() != FrameOrPlugin::kAttached) {
     GetFrame()->FrameScheduler()->OnFirstMeaningfulPaint();
   }
 }
