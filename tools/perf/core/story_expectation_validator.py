38d2822b7e23cd85c4dabd3b9d4f1f2214abf656
rnephew <rnephew@chromium.org>
2017-05-25 08:47:59 -0700

    [Telemetry] Add presubmit to tools/perf that validates story expectations.
    It checks that the name of the expectation is under 50 characters and that
    the name used matches a name in the benchmarks story set.
    
    Bug:713222
    
    Change-Id: I7291a0e0287b788a956894badd32d545640ec707

---
+++ tools/perf/core/story_expectation_validator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Script to check validity of StoryExpectations."""
+
+from core import path_util
+from core import benchmark_finders
+path_util.AddTelemetryToPath()
+
+from telemetry.internal.browser import browser_options
+
+
+def validate_story_names(benchmarks):
+  for benchmark in benchmarks:
+    b = benchmark()
+    options = browser_options.BrowserFinderOptions()
+    # tabset_repeat is needed for tab_switcher benchmarks.
+    options.tabset_repeat = 1
+    story_set = b.CreateStorySet(options)
+    failed_stories = b.GetBrokenExpectations(story_set)
+    assert not failed_stories, 'Incorrect story names: %s' % str(failed_stories)
+
+
+def main(args):
+  del args  # unused
+  benchmarks = benchmark_finders.GetAllPerfBenchmarks()
+  validate_story_names(benchmarks)
+  return 0
