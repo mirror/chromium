4fbb4532bcd0e978e0e8ee471969b9a17a849b6c
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2017-05-25 11:25:44 -0700

    bluetooth: Rewrite bluetooth tests and generate them
    Bug:
    Change-Id: Id4d4c23e3ccdbaa1c7e8f2e2ab92a71023ed4490

---
+++ third_party/WebKit/LayoutTests/bluetooth/script-tests/server/device-reconnects-during-error.js
@@ -0,0 +1,26 @@
+'use strict';
+promise_test(() => {
+  return setBluetoothFakeAdapter('DisconnectingDuringServiceRetrievalAdapter')
+    .then(() => requestDeviceWithKeyDown({
+      filters: [{services: ['heart_rate']}],
+      optionalServices: ['battery_service']}
+    ))
+    .then(device => device.gatt.connect())
+    .then(gatt => {
+      let disconnected = eventPromise(gatt.device, 'gattserverdisconnected');
+      let promise = assert_promise_rejects_with_message(
+        // getPrimaryServices() can't fail because we request access to the
+        // disconnection service, which we need for this test.
+        // TODO(crbug.com/719816): Add a call to getPrimaryServices() when
+        // the disconnection service is no longer needed.
+        gatt.CALLS([
+          getPrimaryService('battery_service')|
+          getPrimaryServices('battery_service')[UUID]]),
+        new DOMException('GATT Server is disconnected. ' +
+                         'Cannot retrieve services. ' +
+                         '(Re)connect first with `device.gatt.connect`.',
+                         'NetworkError'));
+      return disconnected.then(() => gatt.connect()).then(() => promise);
+    });
+}, 'Device disconnects and we reconnect during a FUNCTION_NAME call ' +
+   'that fails. Reject with NetworkError.');
