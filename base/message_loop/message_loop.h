ff37f660c730e41f7296a209c92b06e82b432f6e
Scott Graham <scottmg@chromium.org>
2017-05-25 16:56:07 -0700

    fuchsia: boilerplate for MessageLoop and FilePathWatcher
    Add boilerplate based on libevent and/or posix versions of these. This
    is just a mechanical change so that subsequent CLs will be more able to focus
    on the meat of the implementations of these classes without getting excessively
    large.
    
    Bug: 706592
    Change-Id: Ia9e1356a863576acdf5ba7892f3fe853386e928c

--- base/message_loop/message_loop.h
+++ base/message_loop/message_loop.h
@@ -29,6 +29,8 @@
 // TODO(sky): these includes should not be necessary. Nuke them.
 #if defined(OS_WIN)
 #include "base/message_loop/message_pump_win.h"
+#elif defined(OS_FUCHSIA)
+#include "base/message_loop/message_pump_fuchsia.h"
 #elif defined(OS_IOS)
 #include "base/message_loop/message_pump_io_ios.h"
 #elif defined(OS_POSIX)
@@ -560,6 +562,13 @@ class BASE_EXPORT MessageLoopForIO : public MessageLoop {
     WATCH_WRITE = MessagePumpIOSForIO::WATCH_WRITE,
     WATCH_READ_WRITE = MessagePumpIOSForIO::WATCH_READ_WRITE
   };
+#elif defined(OS_FUCHSIA)
+  typedef MessagePumpFuchsia::Watcher Watcher;
+  typedef MessagePumpFuchsia::FileDescriptorWatcher FileDescriptorWatcher;
+
+  enum Mode{WATCH_READ = MessagePumpFuchsia::WATCH_READ,
+            WATCH_WRITE = MessagePumpFuchsia::WATCH_WRITE,
+            WATCH_READ_WRITE = MessagePumpFuchsia::WATCH_READ_WRITE};
 #elif defined(OS_POSIX)
   typedef MessagePumpLibevent::Watcher Watcher;
   typedef MessagePumpLibevent::FileDescriptorWatcher
---
+++ base/message_loop/message_pump_fuchsia.cc
@@ -0,0 +1,56 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/message_loop/message_pump_fuchsia.h"
+
+#include "base/debug/alias.h"
+#include "base/logging.h"
+
+namespace base {
+
+MessagePumpFuchsia::FileDescriptorWatcher::FileDescriptorWatcher(
+    const tracked_objects::Location& from_here)
+    : watcher_(nullptr), created_from_location_(from_here) {
+  base::debug::Alias(&watcher_);  // To avoid -Wunused-private-field.
+  NOTIMPLEMENTED();
+}
+
+MessagePumpFuchsia::FileDescriptorWatcher::~FileDescriptorWatcher() {}
+
+bool MessagePumpFuchsia::FileDescriptorWatcher::StopWatchingFileDescriptor() {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+MessagePumpFuchsia::MessagePumpFuchsia() {}
+
+MessagePumpFuchsia::~MessagePumpFuchsia() {}
+
+bool MessagePumpFuchsia::WatchFileDescriptor(int fd,
+                                             bool persistent,
+                                             int mode,
+                                             FileDescriptorWatcher* controller,
+                                             Watcher* delegate) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+void MessagePumpFuchsia::Run(Delegate* delegate) {
+  NOTIMPLEMENTED();
+}
+
+void MessagePumpFuchsia::Quit() {
+  NOTIMPLEMENTED();
+}
+
+void MessagePumpFuchsia::ScheduleWork() {
+  NOTIMPLEMENTED();
+}
+
+void MessagePumpFuchsia::ScheduleDelayedWork(
+    const TimeTicks& delayed_work_time) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace base
