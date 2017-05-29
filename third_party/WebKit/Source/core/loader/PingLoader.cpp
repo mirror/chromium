4f6e0077b0cba142006ceb526ae799795bf87666
Yutaka Hirano <yhirano@chromium.org>
2017-05-29 12:52:27 +0900

    Remove FetchContext::LoadingTaskRunner
    This is a preliminary refactoring to implement keepalive fetch operations. In
    order to set an appropriate task runner for a url loader, we need the request
    information.
    
    This CL
     - Moves SetLoadingTaskRunner call to the creation timing of the WebURLLoader,
     - Removes FetchContext::LoadingTaskRunner as the task runner can depend on
       each request, and
     - Provide a request parameter to FetchContext::CreateURLLoader.
    
    Bug: 695939
    Change-Id: I822392da57428b71dc4a3f50cf1b96c40d4a0315

--- third_party/WebKit/Source/core/loader/PingLoader.cpp
+++ third_party/WebKit/Source/core/loader/PingLoader.cpp
@@ -273,7 +273,7 @@ PingLoaderImpl::PingLoaderImpl(LocalFrame* frame,
   if (frame->FrameScheduler())
     frame->FrameScheduler()->DidStopLoading(identifier_);
 
-  loader_ = fetch_context.CreateURLLoader();
+  loader_ = fetch_context.CreateURLLoader(request);
   DCHECK(loader_);
   WrappedResourceRequest wrapped_request(request);
   wrapped_request.SetAllowStoredCredentials(credentials_allowed ==
