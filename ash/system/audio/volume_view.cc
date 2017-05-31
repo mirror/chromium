18dbccd3f41a151256311e3abfdde6d33ab5c8a5
Aaron Leventhal <aleventhal@chromium.org>
2017-05-31 10:24:10 -0400

    Support aria-pressed="mixed" and refactor mixed state support to reuse internal checked tristate.
    Includes failing tests from https://codereview.chromium.org/2707263011/ and https://codereview.chromium.org/2906083002/
    Bug: 726740,726751
    Change-Id: I6b06018f805c4fce781843141109626bab557dcc
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I6b06018f805c4fce781843141109626bab557dcc

--- ash/system/audio/volume_view.cc
+++ ash/system/audio/volume_view.cc
@@ -100,8 +100,10 @@ class VolumeButton : public ButtonListenerActionableView {
     node_data->SetName(
         l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_VOLUME_MUTE));
     node_data->role = ui::AX_ROLE_TOGGLE_BUTTON;
-    if (CrasAudioHandler::Get()->IsOutputMuted())
-      node_data->AddState(ui::AX_STATE_PRESSED);
+    if (CrasAudioHandler::Get()->IsOutputMuted()) {
+      node_data->AddIntAttribute(ui::AX_ATTR_CHECKED_STATE,
+                                 ui::AX_CHECKED_STATE_TRUE);
+    }
   }
 
   views::ImageView* image_;
