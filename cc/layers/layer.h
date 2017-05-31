6e454ab545f19121e515812c1b3ab75085ca54c2
Hayley Ferr <hayleyferr@google.com>
2017-05-31 18:18:59 -0400

    Change Region element in cc side to instead use a RegionMap.
    The cc side now uses a RegionMap, map<TouchAction, Region>, defined in layer.h. 
    SetTouchEventHandler now sets the touch action bit for the region. A new 
    function, UnionOfRegion, has been added and will be used to keep existing functions 
    working with original functionality.
    
    Bug:727839
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ic834cfc326ea8008e6ca33f48c527941c130b170

--- cc/layers/layer.h
+++ cc/layers/layer.h
@@ -22,6 +22,7 @@
 #include "cc/benchmarks/micro_benchmark.h"
 #include "cc/cc_export.h"
 #include "cc/input/input_handler.h"
+#include "cc/input/touch_action.h"
 #include "cc/layers/layer_collections.h"
 #include "cc/layers/layer_position_constraint.h"
 #include "cc/paint/paint_record.h"
@@ -43,6 +44,8 @@ class ConvertableToTraceFormat;
 }
 
 namespace cc {
+
+typedef std::map<TouchAction, Region> RegionMap;
 
 class CopyOutputRequest;
 class LayerClient;
@@ -239,8 +242,8 @@ class CC_EXPORT Layer : public base::RefCounted<Layer> {
     return inputs_.non_fast_scrollable_region;
   }
 
-  void SetTouchEventHandlerRegion(const Region& touch_event_handler_region);
-  const Region& touch_event_handler_region() const {
+  void SetTouchEventHandlerRegion(const RegionMap& touch_event_handler_region);
+  const RegionMap& touch_event_handler_region() const {
     return inputs_.touch_event_handler_region;
   }
 
@@ -421,6 +424,9 @@ class CC_EXPORT Layer : public base::RefCounted<Layer> {
   // Called on the scroll layer to trigger showing the overlay scrollbars.
   void ShowScrollbars() { needs_show_scrollbars_ = true; }
 
+  // Unions all regions contained in RegionMap.
+  Region CompositeRegion(const RegionMap&) const;
+
  protected:
   friend class LayerImpl;
   friend class TreeSynchronizer;
@@ -571,7 +577,7 @@ class CC_EXPORT Layer : public base::RefCounted<Layer> {
     uint32_t main_thread_scrolling_reasons;
     Region non_fast_scrollable_region;
 
-    Region touch_event_handler_region;
+    RegionMap touch_event_handler_region;
 
     bool is_container_for_fixed_position_layers : 1;
     LayerPositionConstraint position_constraint;
