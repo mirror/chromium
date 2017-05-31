52d938b2668553c5b383fb11a60c583ef2632319
Dale Curtis <dalecurtis@chromium.org>
2017-05-30 17:22:17 -0700

    Enable parallel decode requests for VpxVideoDecoder when offloading.
    Similar to the GPUVideoDecoder, where decodes are being queued off
    of the calling thread, we can improve throughput within parallel
    requests.
    
    When VpxVideoDecoder uses an offload thread (>1024 width) we can
    queue multiple decodes to the offload thread. This should improve
    resiliency to glitches.
    
    To avoid incurring penalties to seek or shutdown, as part of this
    change a base::AtomicFlag is used to abort pending decodes when
    Reset() or ~VpxVideoDecoder() are used.
    
    Results show this improves the average read time (time from
    VideoRendererImpl::AttemptRead() until VRI::FrameReady()) for a
    4K60 video from ~2.183ms to ~1.79ms an ~22% improvement. Total
    time spent waiting for reads goes down from 3931.89ms to 3224.54ms;
    similarly showing the 22% improvement.
    
    BUG=648710
    TEST=performance tests
    
    Change-Id: I88cbc778a8ccdd35d7be4c24d0bc12be15cfe661

--- media/filters/vpx_video_decoder.h
+++ media/filters/vpx_video_decoder.h
@@ -18,6 +18,7 @@ struct vpx_codec_ctx;
 struct vpx_image;
 
 namespace base {
+class AtomicFlag;
 class SingleThreadTaskRunner;
 }
 
@@ -45,6 +46,7 @@ class MEDIA_EXPORT VpxVideoDecoder : public VideoDecoder {
   void Decode(const scoped_refptr<DecoderBuffer>& buffer,
               const DecodeCB& decode_cb) override;
   void Reset(const base::Closure& closure) override;
+  int GetMaxDecodeRequests() const override;
 
  private:
   enum DecoderState {
@@ -111,6 +113,7 @@ class MEDIA_EXPORT VpxVideoDecoder : public VideoDecoder {
   // High resolution vp9 may block the media thread for too long, in such cases
   // we share a per-process thread to avoid overly long blocks.
   scoped_refptr<base::SingleThreadTaskRunner> offload_task_runner_;
+  std::unique_ptr<base::AtomicFlag> should_abort_decodes_;
 
   VideoFramePool frame_pool_;
 
