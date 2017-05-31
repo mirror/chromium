05cdfcc0810719fe99511668e8c14a276e870bd9
Colin Blundell <blundell@chromium.org>
2017-05-31 12:45:43 +0200

    [Identity Service] Add interface for obtaining access tokens
    This CL adds an interface for obtaining access tokens to the Identity
    Service. The CL also partially ports the implementation of the
    chrome.identity.getAuthToken() extension function to use it as a initial
    client.
    
    Bug: 683124
    Test: Install a Chrome extension with the identity permissions in its manifest.
    Go to chrome://extensions, enable developer mode, and inspect the background
    page of the above app. At the JS console that that brings up, execute:
    chrome.identity.getAuthToken((token) => {console.log(token);} )
    Verify that a long alphanumeric string appears rather than an error being
    reported.
    Change-Id: I902dde8ff6d817575f07b0e3f1df1e35ed398891

--- services/identity/identity_manager.h
+++ services/identity/identity_manager.h
@@ -5,25 +5,67 @@
 #ifndef SERVICES_IDENTITY_IDENTITY_MANAGER_H_
 #define SERVICES_IDENTITY_IDENTITY_MANAGER_H_
 
+#include "components/signin/core/account_id/account_id.h"
+#include "components/signin/core/browser/profile_oauth2_token_service.h"
+#include "services/identity/public/cpp/scope_set.h"
 #include "services/identity/public/interfaces/identity_manager.mojom.h"
 
 class SigninManagerBase;
 
 namespace identity {
 
-class IdentityManager : public mojom::IdentityManager {
+class IdentityManager : public mojom::IdentityManager,
+                        public OAuth2TokenService::Consumer {
  public:
   static void Create(mojom::IdentityManagerRequest request,
-                     SigninManagerBase* signin_manager);
+                     SigninManagerBase* signin_manager,
+                     ProfileOAuth2TokenService* token_service);
 
-  IdentityManager(SigninManagerBase* signin_manager);
+  IdentityManager(SigninManagerBase* signin_manager,
+                  ProfileOAuth2TokenService* token_service);
   ~IdentityManager() override;
 
  private:
+  // Holds the information needed for the duration between receiving a consumer
+  // request to get an access token and receiving the callback from the
+  // subsequent request that this instance makes to OAuth2TokenService.
+  struct AccessTokenRequest {
+    AccessTokenRequest();
+    AccessTokenRequest(AccessTokenRequest&& other);
+    ~AccessTokenRequest();
+    AccessTokenRequest& operator=(AccessTokenRequest&& other);
+
+    std::unique_ptr<OAuth2TokenService::Request> token_service_request;
+    GetAccessTokenCallback consumer_callback;
+  };
+  using AccessTokenRequests =
+      std::map<const OAuth2TokenService::Request*, AccessTokenRequest>;
+
   // mojom::IdentityManager:
   void GetPrimaryAccountId(GetPrimaryAccountIdCallback callback) override;
+  void GetAccessToken(const std::string& account_id,
+                      const ScopeSet& scopes,
+                      GetAccessTokenCallback callback) override;
+
+  // OAuth2TokenService::Consumer:
+  void OnGetTokenSuccess(const OAuth2TokenService::Request* request,
+                         const std::string& access_token,
+                         const base::Time& expiration_time) override;
+  void OnGetTokenFailure(const OAuth2TokenService::Request* request,
+                         const GoogleServiceAuthError& error) override;
+
+  // Completes the pending access token request by calling back the consumer.
+  void OnAccessTokenRequestCompleted(
+      const OAuth2TokenService::Request* request,
+      const base::Optional<std::string>& access_token,
+      const base::Time& expiration_time,
+      const GoogleServiceAuthError& error);
 
   SigninManagerBase* signin_manager_;
+  ProfileOAuth2TokenService* token_service_;
+
+  // The set of pending requests for access tokens.
+  AccessTokenRequests access_token_requests_;
 };
 
 }  // namespace identity
