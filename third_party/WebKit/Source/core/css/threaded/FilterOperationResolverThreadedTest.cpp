e4896c8372d78a4c4c12fa21f8c19ef6c634d384
Fernando Serboncini <fserb@google.com>
2017-05-31 11:49:25 -0400

    Make CSSParser multi threaded, adds OffscreenCanvas filters thread tests
    - CSS Parser now is ThreadSpecific and not only static local.
    - Multi threaded tests for all code that OffscreenCanvas filters
    currently use:
      - CSSToLengthConversionData
      - CreateOffscreenFilterOperations
    
    Bug: 724565
    Change-Id: I7fdc7f2ff0246709211740d8f708f65513aa2a4f

---
+++ third_party/WebKit/Source/core/css/threaded/FilterOperationResolverThreadedTest.cpp
@@ -0,0 +1,81 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/resolver/FilterOperationResolver.h"
+
+#include "core/css/parser/CSSParser.h"
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/threaded/MultiThreadedTestUtil.h"
+#include "platform/fonts/Font.h"
+#include "platform/fonts/FontDescription.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace blink {
+
+TSAN_TEST(FilterOperationResolverThreadedTest, SimpleMatrixFilter) {
+  RunOnThreads([]() {
+    const CSSValue* value =
+        CSSParser::ParseSingleValue(CSSPropertyFilter, "sepia(50%)");
+    ASSERT_TRUE(value);
+
+    FilterOperations fo =
+        FilterOperationResolver::CreateOffscreenFilterOperations(*value);
+    EXPECT_FALSE(fo.IsEmpty());
+    EXPECT_EQ(fo.size(), 1ul);
+  });
+}
+
+TSAN_TEST(FilterOperationResolverThreadedTest, SimpleTransferFilter) {
+  RunOnThreads([]() {
+    const CSSValue* value =
+        CSSParser::ParseSingleValue(CSSPropertyFilter, "brightness(50%)");
+    ASSERT_TRUE(value);
+
+    FilterOperations fo =
+        FilterOperationResolver::CreateOffscreenFilterOperations(*value);
+    EXPECT_FALSE(fo.IsEmpty());
+    EXPECT_EQ(fo.size(), 1ul);
+  });
+}
+
+TSAN_TEST(FilterOperationResolverThreadedTest, SimpleBlurFilter) {
+  RunOnThreads([]() {
+    const CSSValue* value =
+        CSSParser::ParseSingleValue(CSSPropertyFilter, "blur(10px)");
+    ASSERT_TRUE(value);
+
+    FilterOperations fo =
+        FilterOperationResolver::CreateOffscreenFilterOperations(*value);
+    EXPECT_FALSE(fo.IsEmpty());
+    EXPECT_EQ(fo.size(), 1ul);
+  });
+}
+
+TSAN_TEST(FilterOperationResolverThreadedTest, SimpleDropShadow) {
+  RunOnThreads([]() {
+    const CSSValue* value = CSSParser::ParseSingleValue(
+        CSSPropertyFilter, "drop-shadow(10px 5px 1px black)");
+    ASSERT_TRUE(value);
+
+    FilterOperations fo =
+        FilterOperationResolver::CreateOffscreenFilterOperations(*value);
+    EXPECT_FALSE(fo.IsEmpty());
+    EXPECT_EQ(fo.size(), 1ul);
+  });
+}
+
+TSAN_TEST(FilterOperationResolverThreadedTest, CompoundFilter) {
+  RunOnThreads([]() {
+    const CSSValue* value = CSSParser::ParseSingleValue(
+        CSSPropertyFilter, "sepia(50%) brightness(50%)");
+    ASSERT_TRUE(value);
+
+    FilterOperations fo =
+        FilterOperationResolver::CreateOffscreenFilterOperations(*value);
+    EXPECT_FALSE(fo.IsEmpty());
+    EXPECT_EQ(fo.size(), 2ul);
+  });
+}
+
+}  // namespace blink
