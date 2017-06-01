91649080a0a07f11de8f8dc85b422eee7d4263d7
Francois Doray <fdoray@chromium.org>
2017-06-01 14:25:22 -0400

    Increase number of foreground threads in --single-process browser process.
    For optimal performance, a renderer needs as many threads in the
    foreground pool as there are cores on the machine. RenderProcessImpl
    configures the number of threads in the foreground pool correctly for
    renderer processes. In the case where renderers live in the browser
    process, the code added by this CL adjusts the number of threads in the
    foreground pool of the browser process.
    
    The associated bug is a perf regression on single-process
    android_webview caused by a migration of renderer tasks from
    base::WorkerPool (which can have an infinite number of threads)
    to the foreground pool of TaskScheduler (which :
     - has max(2, num_cores) threads in a renderer process
     - had x = min(8, min(3, 0.3 * num_cores)) threads in a
       browser process prior to this CL
     - has max(x, num_cores) in a --single-process browser
       process with this CL
    ).
    
    Discussion about single-process on android_webview:
    https://groups.google.com/a/chromium.org/d/msg/android-webview-dev/QTFdKg0oR_U/K5f99jeCBAAJ
    
    Bug: 727573
    Change-Id: I2c6e1265dc9b39bddd66c0601683db702f943bda

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -6,6 +6,7 @@
 
 #include <stddef.h>
 
+#include <algorithm>
 #include <string>
 #include <utility>
 #include <vector>
@@ -33,6 +34,7 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/synchronization/waitable_event.h"
+#include "base/sys_info.h"
 #include "base/system_monitor/system_monitor.h"
 #include "base/task_scheduler/initialization_util.h"
 #include "base/task_scheduler/post_task.h"
@@ -950,6 +952,27 @@ int BrowserMainLoop::CreateThreads() {
     if (!task_scheduler_init_params)
       task_scheduler_init_params = GetDefaultTaskSchedulerInitParams();
     DCHECK(task_scheduler_init_params);
+
+    // For optimal performance, a renderer needs as many threads in the
+    // foreground pool as there are cores on the machine. RenderProcessImpl
+    // correctly configures the number of threads in the foreground pool for
+    // renderer processes. In the case where renderers live in the browser
+    // process, the code below adjusts the number of threads in the foreground
+    // pool.
+    if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+            switches::kSingleProcess)) {
+      const base::SchedulerWorkerPoolParams&
+          current_foreground_worker_pool_params(
+              task_scheduler_init_params->foreground_worker_pool_params);
+      task_scheduler_init_params->foreground_worker_pool_params =
+          base::SchedulerWorkerPoolParams(
+              current_foreground_worker_pool_params.standby_thread_policy(),
+              std::max(base::SysInfo::NumberOfProcessors(),
+                       current_foreground_worker_pool_params.max_threads()),
+              current_foreground_worker_pool_params.suggested_reclaim_time(),
+              current_foreground_worker_pool_params.backward_compatibility());
+    }
+
     base::TaskScheduler::GetInstance()->Start(
         *task_scheduler_init_params.get());
   }
