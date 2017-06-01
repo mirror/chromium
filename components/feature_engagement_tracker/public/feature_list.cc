ab6cdf486ef23e9fc970d27c03088a48c986b61c
Tommy Nyquist <nyquist@chromium.org>
2017-05-31 17:44:57 -0700

    Add support for selecting a single feature for demoing In-Product Help
    The In-Product Help demo mode currently only supports enabling demoing
    all In-Product Help features, or none of them.
    
    This leads to issues with testing some of them, since they might
    happen at the same time, and it is confusing to test.
    
    This CL fixes that issue by adding support for selecting only a single
    In-Product Help feature to demo at a time, in addition to keeping the
    old choice of enabling all or none of them.
    
    BUG=728449
    
    Change-Id: I78902c48117eb4e6602f768186579d08f4a83217

--- components/feature_engagement_tracker/public/feature_list.cc
+++ components/feature_engagement_tracker/public/feature_list.cc
@@ -6,15 +6,37 @@
 
 #include "base/feature_list.h"
 #include "components/feature_engagement_tracker/public/feature_constants.h"
+#include "components/flags_ui/feature_entry.h"
 
 namespace feature_engagement_tracker {
 
 namespace {
 
+#define VARIATION_PARAM(base_feature)                                      \
+  const flags_ui::FeatureEntry::FeatureParam base_feature##Variation[] = { \
+      {kIPHDemoModeFeatureChoiceParam, base_feature.name}}
+
+#define VARIATION_ENTRY(base_feature)                                \
+  {                                                                  \
+    base_feature##Variation[0].param_value, base_feature##Variation, \
+        arraysize(base_feature##Variation), nullptr                  \
+  }
+
 const base::Feature* kAllFeatures[] = {
     &kIPHDataSaverPreview, &kIPHDownloadPageFeature, &kIPHDownloadHomeFeature};
 
+VARIATION_PARAM(kIPHDataSaverPreview);
+VARIATION_PARAM(kIPHDownloadPageFeature);
+VARIATION_PARAM(kIPHDownloadHomeFeature);
+
 }  // namespace
+
+const char kIPHDemoModeFeatureChoiceParam[] = "chosen_feature";
+
+const flags_ui::FeatureEntry::FeatureVariation kIPHDemoModeChoiceVariations[] =
+    {VARIATION_ENTRY(kIPHDataSaverPreview),
+     VARIATION_ENTRY(kIPHDownloadPageFeature),
+     VARIATION_ENTRY(kIPHDownloadHomeFeature)};
 
 std::vector<const base::Feature*> GetAllFeatures() {
   return std::vector<const base::Feature*>(
