f03c3bc0d443dda6343beec4942c8613021113a0
Tommy Nyquist <nyquist@chromium.org>
2017-06-01 10:05:02 -0700

    Add support for selecting a single feature for demoing In-Product Help
    The In-Product Help demo mode currently only supports enabling demoing
    all In-Product Help features, or none of them.
    
    This leads to issues with testing some of them, since they might
    happen at the same time, and it is confusing to test.
    
    This CL fixes that issue by adding support for selecting only a single
    In-Product Help feature to demo at a time, in addition to keeping the
    old choice of enabling all or none of them.
    
    BUG=728449
    
    Change-Id: I78902c48117eb4e6602f768186579d08f4a83217

--- components/feature_engagement_tracker/public/feature_list.h
+++ components/feature_engagement_tracker/public/feature_list.h
@@ -8,9 +8,27 @@
 #include <vector>
 
 #include "base/feature_list.h"
+#include "components/flags_ui/feature_entry.h"
 
 namespace feature_engagement_tracker {
 using FeatureVector = std::vector<const base::Feature*>;
+
+// The param name for the FeatureVariation configuration, which is used by
+// chrome://flags to set the variable name for the selected feature. The
+// FeatureEngagementTracker backend will then read this to figure out which
+// feature (if any) was selected by the end user.
+extern const char kIPHDemoModeFeatureChoiceParam[];
+
+// The length of kIPHDemoModeChoiceVariations. This must be updated whenever
+// new features are added to the demo mode selection.
+const size_t kIPHDemoModeChoiceVariationsLen = 3;
+
+// All variations to use with the demo mode. The size of this array must match
+// the definition in feature_list.cc. This is used by the chrome://flags
+// page to be able to list the different features that can be enabled on
+// their own in demo mode.
+extern const flags_ui::FeatureEntry::FeatureVariation
+    kIPHDemoModeChoiceVariations[kIPHDemoModeChoiceVariationsLen];
 
 // Returns all the features that are in use for engagement tracking.
 FeatureVector GetAllFeatures();
