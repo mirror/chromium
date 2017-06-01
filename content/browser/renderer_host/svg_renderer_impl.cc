f89c1f6d154bdec6c796a8a59a33018cced4b9cc
Xida Chen <xidachen@chromium.org>
2017-06-01 14:46:44 -0400

    WIP: createImageBitmap from SVG in a worker (part I)
    TODO: fill in the description
    
    Bug: 606317
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I3a4d0e57589469aab954b4c9e993002764fdd021

---
+++ content/browser/renderer_host/svg_renderer_impl.cc
@@ -0,0 +1,16 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/renderer_host/svg_renderer_impl.h"
+
+#include "mojo/public/cpp/bindings/strong_binding.h"
+
+namespace content {
+
+// Don't bind the request, instead forward this request to a new renderer.
+SVGRendererImpl::SVGRendererImpl(blink::mojom::SVGRendererRequest request) {}
+
+SVGRendererImpl::~SVGRendererImpl() {}
+
+}  // namespace content
---
+++ third_party/WebKit/Source/platform/graphics/SVGActualRendererImpl.cpp
@@ -0,0 +1,14 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/graphics/SVGActualRendererImpl.h"
+
+namespace blink {
+
+void SVGActualRendererImpl::RenderSVG(const WTF::String& file_path,
+                                      const RenderSVGCallback& callback) {
+}
+
+}  // namespace blink
+
