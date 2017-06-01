66fc74feaae18c3858931a14631cd43cd4270e8f
Evan Stade <estade@chromium.org>
2017-06-01 10:20:05 -0600

    Clean up views::Background.
    - make View::set_background take a smart pointer (a la SetBorder)
    - move background factory functions out of views::Background (also a la
      Border) for reduced verbosity
    - remove a couple redundant background factory functions
    
    No behavioral changes.
    
    Bug: none
    Change-Id: I4d4971ea04dcb1f17ebda5671fd997beb1a692a3

--- ui/views/examples/scroll_view_example.cc
+++ ui/views/examples/scroll_view_example.cc
@@ -35,11 +35,9 @@ class ScrollViewExample::ScrollableView : public View {
   }
 
   void SetColor(SkColor from, SkColor to) {
-    Background* background = Background::CreateBackgroundPainter(
-        Painter::CreateVerticalGradient(from, to));
-    background->SetNativeControlColor(
-        color_utils::AlphaBlend(from, to, 128));
-    set_background(background);
+    SetBackground(
+        CreateBackgroundFromPainter(Painter::CreateVerticalGradient(from, to)));
+    background()->SetNativeControlColor(color_utils::AlphaBlend(from, to, 128));
   }
 
   void PlaceChildY(int index, int y) {
