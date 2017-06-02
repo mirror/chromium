06abcb822b7186532a6c595693d55d43fc54d1e6
Eugene Ostroukhov <eostroukhov@chromium.org>
2017-06-02 14:36:27 -0700

    [DevTools] Display usage & quota information
    Adds a simple display for temporary and persistent storage information.
    
    Bug: 619521
    Change-Id: Idad89ab215ba0ab6dcea2a37ef8e68d4b38d3244

--- content/browser/devtools/protocol/storage_handler.cc
+++ content/browser/devtools/protocol/storage_handler.cc
@@ -12,6 +12,8 @@
 #include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/storage_partition.h"
+#include "storage/browser/quota/quota_manager.h"
+#include "storage/common/quota/quota_status_code.h"
 
 namespace content {
 namespace protocol {
@@ -27,6 +29,67 @@ static const char kWebSQL[] = "websql";
 static const char kServiceWorkers[] = "service_workers";
 static const char kCacheStorage[] = "cache_storage";
 static const char kAll[] = "all";
+
+class UsageAndQuotaDataCollector
+    : public base::RefCounted<UsageAndQuotaDataCollector> {
+ public:
+  using UsageCallbackPtr =
+      std::unique_ptr<StorageHandler::GetUsageAndQuotaCallback>;
+  // using Storage::QuotasAndUsage;
+
+  explicit UsageAndQuotaDataCollector(UsageCallbackPtr&& callback)
+      : callback_(std::move(callback)) {}
+
+  void TempDataCallback(storage::QuotaStatusCode code,
+                        int64_t usage,
+                        int64_t quota) {
+    if (!callback_)
+      return;
+    if (code != storage::kQuotaStatusOk) {
+      callback_->sendFailure(
+          Response::Error("Quota information is not available"));
+      callback_.reset();
+      return;
+    }
+    temp_usage_ = usage;
+    temp_quota_ = quota;
+  }
+
+  void PersistentDataCallback(storage::QuotaStatusCode code,
+                              int64_t usage,
+                              int64_t quota) {
+    if (!callback_)
+      return;
+    if (code != storage::kQuotaStatusOk) {
+      callback_->sendFailure(
+          Response::Error("Quota information is not available"));
+      callback_.reset();
+      return;
+    }
+    perstent_usage_ = usage;
+    persistent_quota_ = quota;
+  }
+
+ private:
+  friend class base::RefCounted<UsageAndQuotaDataCollector>;
+  virtual ~UsageAndQuotaDataCollector() {
+    if (!callback_)
+      return;
+    callback_->sendSuccess(Storage::QuotasAndUsage::Create()
+                               .SetPersistentStorageQuota(persistent_quota_)
+                               .SetPersistentStorageUsage(perstent_usage_)
+                               .SetTemporaryStorageQuota(temp_quota_)
+                               .SetTemporaryStorageUsage(temp_usage_)
+                               .Build());
+  }
+
+  UsageCallbackPtr callback_;
+
+  int64_t temp_usage_;
+  int64_t perstent_usage_;
+  int64_t temp_quota_;
+  int64_t persistent_quota_;
+};
 }
 
 StorageHandler::StorageHandler()
@@ -86,6 +149,27 @@ Response StorageHandler::ClearDataForOrigin(
       partition->GetURLRequestContext(),
       base::Bind(&base::DoNothing));
   return Response::OK();
+}
+
+void StorageHandler::GetUsageAndQuota(
+    const String& origin,
+    std::unique_ptr<GetUsageAndQuotaCallback> callback) {
+  if (!host_)
+    return callback->sendFailure(Response::InternalError());
+
+  scoped_refptr<UsageAndQuotaDataCollector> collector(
+      new UsageAndQuotaDataCollector(std::move(callback)));
+
+  storage::QuotaManager* manager =
+      host_->GetProcess()->GetStoragePartition()->GetQuotaManager();
+  GURL origin_url(origin);
+  manager->GetUsageAndQuotaForWebApps(
+      origin_url, storage::kStorageTypeTemporary,
+      base::Bind(&UsageAndQuotaDataCollector::TempDataCallback, collector));
+  manager->GetUsageAndQuotaForWebApps(
+      origin_url, storage::kStorageTypePersistent,
+      base::Bind(&UsageAndQuotaDataCollector::PersistentDataCallback,
+                 collector));
 }
 
 }  // namespace protocol
--- third_party/WebKit/Source/devtools/front_end/resources/ClearStorageView.js
+++ third_party/WebKit/Source/devtools/front_end/resources/ClearStorageView.js
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 /**
  * @implements {SDK.TargetManager.Observer}
- * @unrestricted
  */
 Resources.ClearStorageView = class extends UI.VBox {
   constructor() {
@@ -13,6 +12,11 @@ Resources.ClearStorageView = class extends UI.VBox {
     this._reportView.registerRequiredCSS('resources/clearStorageView.css');
     this._reportView.element.classList.add('clear-storage-header');
     this._reportView.show(this.contentElement);
+    /** @type {?SDK.Target} */
+    this._target = null;
+    /** @type {?string} */
+    this._securityOrigin = null;
+    this._throttler = new Common.Throttler(1000);
 
     this._settings = new Map();
     for (var type
@@ -22,6 +26,9 @@ Resources.ClearStorageView = class extends UI.VBox {
                  Protocol.Storage.StorageType.Websql])
       this._settings.set(type, Common.settings.createSetting('clear-storage-' + type, true));
 
+    var quota = this._reportView.appendSection(Common.UIString('Usage'));
+    this._temporaryQuotaRow = quota.appendRow();
+    this._persistentQuotaRow = quota.appendRow();
 
     var application = this._reportView.appendSection(Common.UIString('Application'));
     this._appendItem(application, Common.UIString('Unregister service workers'), 'service_workers');
@@ -41,6 +48,9 @@ Resources.ClearStorageView = class extends UI.VBox {
     this._clearButton = UI.createTextButton(
         Common.UIString('Clear site data'), this._clear.bind(this), Common.UIString('Clear site data'));
     footer.appendChild(this._clearButton);
+
+
+    this._refreshQuota();
   }
 
   /**
@@ -96,6 +106,8 @@ Resources.ClearStorageView = class extends UI.VBox {
   }
 
   _clear() {
+    if (!this._securityOrigin)
+      return;
     var storageTypes = [];
     for (var type of this._settings.keys()) {
       if (this._settings.get(type).get())
@@ -154,5 +166,43 @@ Resources.ClearStorageView = class extends UI.VBox {
       this._clearButton.disabled = false;
       this._clearButton.textContent = label;
     }, 500);
+  }
+
+  /**
+   * @param {?Protocol.Storage.QuotasAndUsage} quotasAndUsage
+   */
+  _updateQuotaDisplay(quotasAndUsage) {
+    this._temporaryQuotaRow.textContent = '';
+    this._persistentQuotaRow.textContent = '';
+
+    if (!quotasAndUsage)
+      return;
+
+    this._temporaryQuotaRow.textContent = Common.UIString(
+        '%s used out of %s temporary storage quota', Number.bytesToString(quotasAndUsage.temporaryStorageUsage),
+        Number.bytesToString(quotasAndUsage.temporaryStorageQuota));
+    this._persistentQuotaRow.textContent = Common.UIString(
+        '%s used out of %s persistent storage quota', Number.bytesToString(quotasAndUsage.persistentStorageUsage),
+        Number.bytesToString(quotasAndUsage.persistentStorageQuota));
+  }
+
+  /**
+   * @return {!Promise<?>}
+   */
+  _refreshQuota() {
+    if (this.isShowing())
+      this._throttler.schedule(this._refreshQuota.bind(this));
+
+    if (!this._securityOrigin)
+      return Promise.resolve(true);
+
+    return this._target.storageAgent().getUsageAndQuota(this._securityOrigin).then(this._updateQuotaDisplay.bind(this));
+  }
+
+  /**
+   * @override
+   */
+  wasShown() {
+    this._refreshQuota();
   }
 };
