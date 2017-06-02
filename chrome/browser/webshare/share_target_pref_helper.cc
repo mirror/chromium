d94ffa832b1b7a059c293cb0f1ca7870b1ee1e61
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2017-06-02 15:46:16 +1000

    webshare-target: Introduce ShareTarget class and add helper to parse prefs dictionary
    1. Introduces a move-only ShareTarget class.
    2. Add helper function that parses the ShareTargets from prefs.
    3. Refactor ShareServiceImpl to use helper function.
      3.1. Pass a ShareTarget vector to ShowPicker.
      3.2. Modify picker's callback to return a ShareTarget. ShareTarget includes
           the URL template so we avoid an extra dictionary lookup when it returns.
    4. Fix tests.
      4.1. Add PickTarget function to ShareServiceImplUnittest that picks a target
           based on the URL.
    
    Bug: 716957
    
    Change-Id: If28d0a7576644f1501afdf9441c2f8845d9935d6

--- chrome/browser/webshare/share_target_pref_helper.cc
+++ chrome/browser/webshare/share_target_pref_helper.cc
@@ -4,8 +4,15 @@
 
 #include "chrome/browser/webshare/share_target_pref_helper.h"
 
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "base/memory/ptr_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
+#include "chrome/browser/webshare/share_target.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/scoped_user_pref_update.h"
@@ -41,4 +48,29 @@ void UpdateShareTargetInPrefs(const GURL& manifest_url,
 
   share_target_dict->SetWithoutPathExpansion(manifest_url.spec(),
                                              std::move(origin_dict));
+}
+
+std::vector<ShareTarget> GetShareTargetsInPrefs(PrefService* pref_service) {
+  std::unique_ptr<base::DictionaryValue> share_targets_dict =
+      pref_service->GetDictionary(prefs::kWebShareVisitedTargets)
+          ->CreateDeepCopy();
+
+  std::vector<ShareTarget> share_targets;
+  for (const auto& it : *share_targets_dict) {
+    GURL manifest_url(it.first);
+
+    const base::DictionaryValue* share_target_dict;
+    bool result = it.second->GetAsDictionary(&share_target_dict);
+    DCHECK(result);
+
+    std::string name;
+    share_target_dict->GetString("name", &name);
+    std::string url_template;
+    share_target_dict->GetString("url_template", &url_template);
+
+    share_targets.emplace_back(std::move(manifest_url), std::move(name),
+                               std::move(url_template));
+  }
+
+  return share_targets;
 }
