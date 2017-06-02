5e9868482fb11929a6c8add4fc7ff0c05c7a1689
Eric Seckler <eseckler@chromium.org>
2017-06-02 12:00:38 +0100

    [cc] Fix display_damaged calculation in Display::OnSurfaceDamaged.
    We were previously passing on *changed, which could lead to
    propagation of the flag value from other Displays / SurfaceObservers.
    Calculate the value passed on to DisplayScheduler locally instead.
    
    Also removes unnecessary calls to UpdateRootSurfaceResourcesLocked
    and SurfaceDamageExpected.
    
    Bug: 727162, 697086
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ie0f78705ee8f76417a7d9f2d8ed87365c8a1cb0a

--- cc/surfaces/display.cc
+++ cc/surfaces/display.cc
@@ -388,9 +388,9 @@ void Display::SetNeedsRedrawRect(const gfx::Rect& damage_rect) {
   }
 }
 
-void Display::OnSurfaceDamaged(const SurfaceId& surface_id,
-                               const BeginFrameAck& ack,
-                               bool* changed) {
+bool Display::OnSurfaceDamaged(const SurfaceId& surface_id,
+                               const BeginFrameAck& ack) {
+  bool display_damaged = false;
   if (ack.has_damage) {
     if (aggregator_ &&
         aggregator_->previous_contained_surfaces().count(surface_id)) {
@@ -400,17 +400,18 @@ void Display::OnSurfaceDamaged(const SurfaceId& surface_id,
         if (surface->GetActiveFrame().resource_list.empty())
           aggregator_->ReleaseResources(surface_id);
       }
-      *changed = true;
+      display_damaged = true;
+      if (surface_id == current_surface_id_)
+        UpdateRootSurfaceResourcesLocked();
     } else if (surface_id == current_surface_id_) {
-      *changed = true;
+      display_damaged = true;
+      UpdateRootSurfaceResourcesLocked();
     }
   }
 
   if (scheduler_)
-    scheduler_->SurfaceDamaged(surface_id, ack, *changed);
-
-  if (surface_id == current_surface_id_)
-    UpdateRootSurfaceResourcesLocked();
+    scheduler_->SurfaceDamaged(surface_id, ack, display_damaged);
+  return display_damaged;
 }
 
 bool Display::SurfaceHasUndrawnFrame(const SurfaceId& surface_id) const {
