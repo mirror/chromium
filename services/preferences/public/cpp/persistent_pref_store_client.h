8d76cd282da5c6a99935a677df4719de9cd02f9f
Sam McNally <sammc@chromium.org>
2017-06-02 16:14:16 +1000

    Pref service: provide eventual consistency with multiple writers.
    Currently, if two writers both write to a single pref or a single
    subpref without seeing each other's writes, the two writers will each
    converge to the other's value rather than a shared value. This CL
    corrects this by eventually converging all clients to the last value
    observed by the service, without any values moving backwards in
    serialized write order.
    
    To provide eventual consistency, each client tracks the prefs for which
    a write has been sent to the pref service, but which have not been
    acknowledged. The acks are sent on the same interface as updates
    triggered by other clients so any remote writes received before an ack
    are writes that have been serialized to after the in-flight write.
    Thus, the client can ignore such writes.
    
    Since prefs are hierarchical and writes can be applied to individual
    sub-components of a single pref, writes to parent and child values can
    race. If the parent write is applied first, then the client writing the
    child value does not have a simple strategy to resolve the conflicting
    changes with the information it has. Instead, in these cases, it
    requests a value from the service for the parent value. Such values are
    returned as pref value updates, just like notifications of a write by
    another client and conflicts are handled similarly.
    
    Bug: 654988
    Change-Id: I1959f1b100134d9dc3af2b1c94d6ec08b139b8ce

--- services/preferences/public/cpp/persistent_pref_store_client.h
+++ services/preferences/public/cpp/persistent_pref_store_client.h
@@ -69,6 +69,9 @@ class PersistentPrefStoreClient
   ~PersistentPrefStoreClient() override;
 
  private:
+  class InFlightWriteTrie;
+  struct InFlightWrite;
+
   void OnConnect(mojom::PersistentPrefStoreConnectionPtr connection,
                  std::unordered_map<PrefValueStore::PrefStoreType,
                                     prefs::mojom::PrefStoreConnectionPtr>
@@ -78,6 +81,13 @@ class PersistentPrefStoreClient
                   std::set<std::vector<std::string>> path_components,
                   uint32_t flags);
   void FlushPendingWrites();
+
+  // prefs::mojom::PreferenceObserver:
+  void OnPrefChangeAck() override;
+
+  bool ShouldSkipWrite(const std::string& key,
+                       const std::vector<std::string>& path,
+                       const base::Value* new_value) override;
 
   mojom::PrefStoreConnectorPtr connector_;
   scoped_refptr<PrefRegistry> pref_registry_;
@@ -89,6 +99,9 @@ class PersistentPrefStoreClient
 
   std::unique_ptr<ReadErrorDelegate> error_delegate_;
   std::vector<PrefValueStore::PrefStoreType> already_connected_types_;
+
+  std::queue<std::vector<InFlightWrite>> in_flight_writes_queue_;
+  std::map<std::string, InFlightWriteTrie> in_flight_writes_tries_;
 
   base::WeakPtrFactory<PersistentPrefStoreClient> weak_factory_;
 
