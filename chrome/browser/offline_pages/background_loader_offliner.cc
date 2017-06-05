8be9875258e273b446dd1dfcfafaefbe7f2dff83
Cathy Li <chili@chromium.org>
2017-06-05 10:54:59 -0700

    [Offline pages] Detect and record http error codes in new combined UMA. Also fix test flake caused by deleting calling object.
    NetError codes are reported for the network errors for which chrome would have to generate our own
    error message and error page. http error codes are when the server had an error, often accompanied
    by server-generated error page. Either way, our decision on not downloading any error page still
    applies.
    
    Bug: 725682
    Change-Id: I9f016b5724a8ea6a98b574bd41d490ceb5aeb197

--- chrome/browser/offline_pages/background_loader_offliner.cc
+++ chrome/browser/offline_pages/background_loader_offliner.cc
@@ -24,6 +24,7 @@
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_user_data.h"
+#include "net/http/http_response_headers.h"
 
 namespace offline_pages {
 
@@ -66,11 +67,11 @@ std::string AddHistogramSuffix(const ClientId& client_id,
   return adjusted_histogram_name;
 }
 
-void RecordErrorCauseUMA(const ClientId& client_id, net::Error error_code) {
+void RecordErrorCauseUMA(const ClientId& client_id, int error_code) {
   UMA_HISTOGRAM_SPARSE_SLOWLY(
       AddHistogramSuffix(client_id,
-                         "OfflinePages.Background.BackgroundLoadingFailedCode"),
-      std::abs(error_code));
+                         "OfflinePages.Background.LoadingErrorStatusCode"),
+      error_code);
 }
 
 void HandleLoadTerminationCancel(
@@ -302,13 +303,24 @@ void BackgroundLoaderOffliner::DidFinishNavigation(
   // Mark as error page. Resetting here causes RecordNavigationMetrics to crash.
   if (navigation_handle->IsErrorPage()) {
     RecordErrorCauseUMA(pending_request_->client_id(),
-                        navigation_handle->GetNetErrorCode());
+                        static_cast<int>(navigation_handle->GetNetErrorCode()));
     switch (navigation_handle->GetNetErrorCode()) {
       case net::ERR_INTERNET_DISCONNECTED:
         page_load_state_ = DELAY_RETRY;
         break;
       default:
         page_load_state_ = RETRIABLE;
+    }
+  } else {
+    int status_code = navigation_handle->GetResponseHeaders()->response_code();
+    // 2XX and 3XX are ok because they indicate success or redirection.
+    // We track 301 because it's MOVED_PERMANENTLY and usually accompanies an
+    // error page with new address.
+    // 400+ codes are client and server errors.
+    // We skip 418 because it's a teapot.
+    if (status_code == 301 || (status_code >= 400 && status_code != 418)) {
+      RecordErrorCauseUMA(pending_request_->client_id(), status_code);
+      page_load_state_ = RETRIABLE;
     }
   }
 }
@@ -355,7 +367,11 @@ void BackgroundLoaderOffliner::StartSnapshot() {
     }
 
     completion_callback_.Run(request, status);
-    ResetState();
+    // Post cleanup task, as we don't want to delete SnapshotController
+    // while it's calling this method.
+    base::ThreadTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE, base::Bind(&BackgroundLoaderOffliner::ResetState,
+                              weak_ptr_factory_.GetWeakPtr()));
     return;
   }
 
