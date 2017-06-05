07a8654eef19d3e8d443bafffc46d35dacb6c4c4
Becca Hughes <beccahughes@chromium.org>
2017-06-05 13:19:14 +0100

    Persist User Activation across navigation.
    Persist user activation through a special bit on Frame across
    navigation so long as the eTLD+1 remains the same.
    
    BUG=729518

--- third_party/WebKit/Source/core/frame/Frame.h
+++ third_party/WebKit/Source/core/frame/Frame.h
@@ -146,6 +146,13 @@ class CORE_EXPORT Frame : public GarbageCollectedFinalized<Frame> {
   void SetDocumentHasReceivedUserGesture();
   bool HasReceivedUserGesture() const { return has_received_user_gesture_; }
 
+  void SetDocumentHasReceivedUserGestureBeforeNavigation(const bool value) {
+    has_received_user_gesture_before_nav_ = value;
+  }
+  bool HasReceivedUserGestureBeforeNavigation() const {
+    return has_received_user_gesture_before_nav_;
+  }
+
   bool IsAttached() const {
     return lifecycle_.GetState() == FrameLifecycle::kAttached;
   }
@@ -164,6 +171,7 @@ class CORE_EXPORT Frame : public GarbageCollectedFinalized<Frame> {
   Member<DOMWindow> dom_window_;
 
   bool has_received_user_gesture_ = false;
+  bool has_received_user_gesture_before_nav_ = false;
 
   FrameLifecycle lifecycle_;
 
