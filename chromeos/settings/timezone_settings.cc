b95986d24e223dcce660d53c86cce2f66f9f567b
Jungshik Shin <jshin@chromium.org>
2017-06-05 14:28:26 -0700

    comment added about the listing order
    add a comment about the list sorting order
    
    Bug:
    Change-Id: I71ef7cc63244c30555ebbf4cea90646f7c1628dd

--- chromeos/settings/timezone_settings.cc
+++ chromeos/settings/timezone_settings.cc
@@ -51,6 +51,7 @@ const char kFallbackTimeZoneId[] = "America/Los_Angeles";
 // identity is likely to cut down the number to < 100. Until we
 // come up with a better list, we hard-code the following list. It came from
 // from Android initially, but more entries have been added.
+// Note that the list is sorted in terms of timezone offset from UTC.
 static const char* kTimeZones[] = {
     "Pacific/Midway",
     "Pacific/Honolulu",
