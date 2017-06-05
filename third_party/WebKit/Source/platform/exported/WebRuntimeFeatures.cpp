47bdb89b8111b311596deff98ad3d4e81eaa63d2
Becca Hughes <beccahughes@chromium.org>
2017-06-05 09:57:26 +0100

    Implement experiment to disable Cast overlay
    Add an feature flag for 'MediaCastOverlayButton' to enable/disable
    the cast overlay button. Defaults to ON for Android.
    
    BUG=728660
    
    Change-Id: I00dff0385b8397e9885987b5fe5dcd66022b5d76

--- third_party/WebKit/Source/platform/exported/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/platform/exported/WebRuntimeFeatures.cpp
@@ -398,4 +398,8 @@ void WebRuntimeFeatures::EnableLocationHardReload(bool enable) {
   RuntimeEnabledFeatures::setLocationHardReloadEnabled(enable);
 }
 
+void WebRuntimeFeatures::EnableMediaCastOverlayButton(bool enable) {
+  RuntimeEnabledFeatures::setMediaCastOverlayButtonEnabled(enable);
+}
+
 }  // namespace blink
