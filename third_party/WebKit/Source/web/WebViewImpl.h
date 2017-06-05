8ccb4f40d0765f80431ec26ad2429266154580b0
Noel Gordon <noel@chromium.org>
2017-06-05 22:44:06 +1000

    Convert SpellCheckPanel SpellCheckPanelHost IPC to mojo
    The spelling and grammar checking panel is an OSX feature [1],
    available in Mac Chrome via BUILDFLAG(HAS_SPELLCHECK_PANEL),
    used as a spell-correction aid for contentEditable, <input>
    and <textarea>, and designMode document content that has the
    focus on the current web page.
    
    - Replace the spell check panel host and client chrome IPCs
      with mojo (bug 714480).
    - Move the spelling panel client interface from RenderView to
      RenderFrame, so that it works with OOPIF (bug 712395).
    - Update the browser Mac render view delegate to send spelling
      panel client messages to the focused RenderFrame.
    - In Blink, move the WebSpellCheckClient off the WebView and
      make it WebLocalFrame associated so it works with OOPIF.
    - Rename it to WebSpellCheckPanelHost and remove its API from
      the SpellCheckerClientImpl class to address TODOs.
    - Add an OOPIF browser test to verify that the spelling panel
      can be shown from an OOPIF frame (bug 712395).
    
    [1] https://support.apple.com/kb/PH18451?locale=en_US
    
    Bug:714480,712395
    Change-Id: I964111685b2bf22bf54fd4e6e20f0861edf68b51

--- third_party/WebKit/Source/web/WebViewImpl.h
+++ third_party/WebKit/Source/web/WebViewImpl.h
@@ -157,7 +157,6 @@ class WEB_EXPORT WebViewImpl final
   void SetMainFrame(WebFrame*) override;
   void SetCredentialManagerClient(WebCredentialManagerClient*) override;
   void SetPrerendererClient(WebPrerendererClient*) override;
-  void SetSpellCheckClient(WebSpellCheckClient*) override;
   WebSettings* GetSettings() override;
   WebString PageEncoding() const override;
   bool TabsToLinks() const override;
@@ -289,10 +288,6 @@ class WEB_EXPORT WebViewImpl final
   static WebViewBase* FromPage(Page*);
 
   WebViewClient* Client() override { return client_; }
-
-  WebSpellCheckClient* SpellCheckClient() override {
-    return spell_check_client_;
-  }
 
   // Returns the page object associated with this view. This may be null when
   // the page is shutting down, but will be valid at all other times.
@@ -599,7 +594,6 @@ class WEB_EXPORT WebViewImpl final
   CompositorMutatorImpl& Mutator();
 
   WebViewClient* client_;  // Can be 0 (e.g. unittests, shared workers, etc.)
-  WebSpellCheckClient* spell_check_client_;
 
   Persistent<ChromeClient> chrome_client_;
   ContextMenuClientImpl context_menu_client_impl_;
