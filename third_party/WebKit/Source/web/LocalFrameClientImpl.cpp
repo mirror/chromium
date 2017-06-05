fadabe3af2bdd6224b04e629f1113bd13f9e625b
Noel Gordon <noel@chromium.org>
2017-06-05 16:03:16 +1000

    Convert SpellCheckPanel SpellCheckPanelHost IPC to mojo
    The spelling and grammar checking panel is an OSX feature [1],
    available in Mac Chrome via BUILDFLAG(HAS_SPELLCHECK_PANEL),
    used as a spell-correction aid for contentEditable, <input>
    and <textarea>, and designMode document content.
    
    - Replace the spell check panel host and client chrome IPCs
      with mojo, move the panel client interface to RenderFrame
      (from RenderView) so it works with OOPIF (bug 712395).
    - Update the browser Mac render view delegate to send spell
      panel client requests to the focused RenderFrame.
    - In Blink, move the WebSpellCheckClient off the WebView and
      make it WebLocalFrame associated so it works with OOPIF.
    - Rename it to WebSpellCheckPanelHost and remove its API from
      the SpellCheckerClientImpl class to address TODOs.
    - Add an OOPIF browser test to verify that the spelling panel
      can be shown from an OOPIF frame (bug 712395).
    
    [1] https://support.apple.com/kb/PH18451?locale=en_US
    
    Bug:714480,712395
    Change-Id: I964111685b2bf22bf54fd4e6e20f0861edf68b51

--- third_party/WebKit/Source/web/LocalFrameClientImpl.cpp
+++ third_party/WebKit/Source/web/LocalFrameClientImpl.cpp
@@ -1044,6 +1044,11 @@ void LocalFrameClientImpl::AbortClientNavigation() {
     web_frame_->Client()->AbortClientNavigation();
 }
 
+WebSpellCheckPanelHostClient* LocalFrameClientImpl::SpellCheckPanelHostClient()
+    const {
+  return web_frame_->SpellCheckPanelHostClient();
+}
+
 TextCheckerClient& LocalFrameClientImpl::GetTextCheckerClient() const {
   return web_frame_->GetTextCheckerClient();
 }
