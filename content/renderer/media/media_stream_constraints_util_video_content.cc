b5b9e6fb3afbdd64afb85f2883f1f465b730124c
Guido Urdaneta <guidou@chromium.org>
2017-06-06 12:31:50 +0200

    SelectSettings algorithm for audio constraints.
    This is intended to be used in getUserMedia to implement spec-compliant
    audio constraints processing.
    
    BUG=657733
    
    Change-Id: Ida1bc5561b16b0c5ac1628c19c71fb8f0b5a33fd

--- content/renderer/media/media_stream_constraints_util_video_content.cc
+++ content/renderer/media/media_stream_constraints_util_video_content.cc
@@ -46,31 +46,12 @@ using Point = ResolutionSet::Point;
 using StringSet = DiscreteSet<std::string>;
 using BoolSet = DiscreteSet<bool>;
 
-
 constexpr double kMinScreenCastAspectRatio =
     static_cast<double>(kMinScreenCastDimension) /
     static_cast<double>(kMaxScreenCastDimension);
 constexpr double kMaxScreenCastAspectRatio =
     static_cast<double>(kMaxScreenCastDimension) /
     static_cast<double>(kMinScreenCastDimension);
-
-StringSet StringSetFromConstraint(const blink::StringConstraint& constraint) {
-  if (!constraint.HasExact())
-    return StringSet::UniversalSet();
-
-  std::vector<std::string> elements;
-  for (const auto& entry : constraint.Exact())
-    elements.push_back(entry.Ascii());
-
-  return StringSet(std::move(elements));
-}
-
-BoolSet BoolSetFromConstraint(const blink::BooleanConstraint& constraint) {
-  if (!constraint.HasExact())
-    return BoolSet::UniversalSet();
-
-  return BoolSet({constraint.Exact()});
-}
 
 using DoubleRangeSet = NumericRangeSet<double>;
 
