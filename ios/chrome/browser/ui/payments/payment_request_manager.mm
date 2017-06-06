3fc80d317784e059662f0454715d4fdf7536398c
Randall Raymond <rayraymond@google.com>
2017-06-06 18:42:16 -0400

    Correctly checks if connection is secure and elides longer hostnames.
    This CL correctly checks if connection is secure before proceeding with Payment Request. It also elides the hostname appropriately if it is too long.
    
    Normal hostname: http://imgur.com/a/huglh
    Hostname that is very long: http://imgur.com/a/nIPdP
    
    Bug:602666
    Change-Id: Ifd2c0c0205743e791f4ad493ad5f508208a76b13

--- ios/chrome/browser/ui/payments/payment_request_manager.mm
+++ ios/chrome/browser/ui/payments/payment_request_manager.mm
@@ -8,6 +8,7 @@
 #include "base/ios/ios_util.h"
 #import "base/mac/bind_objc_block.h"
 #include "base/mac/foundation_util.h"
+#import "base/mac/scoped_nsobject.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string16.h"
 #include "base/strings/sys_string_conversions.h"
@@ -23,20 +24,27 @@
 #include "components/payments/core/address_normalizer_impl.h"
 #include "components/payments/core/payment_address.h"
 #include "components/payments/core/payment_request_data_util.h"
+#include "components/toolbar/toolbar_model.h"
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/autofill/personal_data_manager_factory.h"
 #include "ios/chrome/browser/autofill/validation_rules_storage_factory.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/payments/payment_request.h"
 #include "ios/chrome/browser/procedural_block_types.h"
+#import "ios/chrome/browser/tabs/tab.h"
+#import "ios/chrome/browser/tabs/tab_model.h"
+#import "ios/chrome/browser/ui/browser_view_controller_dependency_factory.h"
 #import "ios/chrome/browser/ui/commands/UIKit+ChromeExecuteCommand.h"
 #import "ios/chrome/browser/ui/commands/generic_chrome_command.h"
 #import "ios/chrome/browser/ui/commands/ios_command_ids.h"
 #import "ios/chrome/browser/ui/payments/js_payment_request_manager.h"
 #import "ios/chrome/browser/ui/payments/payment_request_coordinator.h"
+#include "ios/chrome/browser/ui/toolbar/toolbar_model_delegate_ios.h"
+#include "ios/chrome/browser/ui/toolbar/toolbar_model_ios.h"
 #include "ios/web/public/favicon_status.h"
 #include "ios/web/public/navigation_item.h"
 #include "ios/web/public/navigation_manager.h"
+#include "ios/web/public/origin_util.h"
 #include "ios/web/public/payments/payment_request.h"
 #include "ios/web/public/ssl_status.h"
 #import "ios/web/public/url_scheme_util.h"
@@ -47,6 +55,7 @@
 #import "ios/web/public/web_state/web_state_observer_bridge.h"
 #include "third_party/libaddressinput/chromium/chrome_metadata_source.h"
 #include "third_party/libaddressinput/chromium/chrome_storage_impl.h"
+#include "url/gurl.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -201,6 +210,7 @@ struct PendingPaymentResponse {
 @implementation PaymentRequestManager
 
 @synthesize enabled = _enabled;
+@synthesize tabModel = _tabModel;
 @synthesize webState = _webState;
 @synthesize browserState = _browserState;
 @synthesize paymentRequestJsManager = _paymentRequestJsManager;
@@ -223,6 +233,14 @@ struct PendingPaymentResponse {
 - (instancetype)init {
   NOTREACHED();
   return nil;
+}
+
+- (void)setTabModel:(TabModel*)tabModel {
+  _tabModel = tabModel;
+
+  // Use tab model to get web state.
+  web::WebState* webState = [tabModel currentTab].webState;
+  [self setWebState:webState];
 }
 
 - (void)setWebState:(web::WebState*)webState {
@@ -433,8 +451,8 @@ struct PendingPaymentResponse {
   NSString* pageTitle = base::SysUTF16ToNSString([self webState]->GetTitle());
   NSString* pageHost =
       base::SysUTF8ToNSString([self webState]->GetLastCommittedURL().host());
-  // TODO(crbug.com/728639): Determine when connection is secure.
-  BOOL connectionSecure = false;
+  BOOL connectionSecure =
+      [self webState]->GetLastCommittedURL().SchemeIs(url::kHttpsScheme);
   autofill::AutofillManager* autofillManager =
       autofill::AutofillDriverIOS::FromWebState(_webState)->autofill_manager();
   _paymentRequestCoordinator = [[PaymentRequestCoordinator alloc]
@@ -608,10 +626,38 @@ struct PendingPaymentResponse {
     return NO;
   }
 
-  const web::NavigationItem* navigationItem =
-      [self webState]->GetNavigationManager()->GetLastCommittedItem();
-  return navigationItem && navigationItem->GetSSL().security_style ==
-                               web::SECURITY_STYLE_AUTHENTICATED;
+  // Checks if origin is localhost, file://, or cryptographic.
+  if (!web::IsOriginSecure([self webState]->GetLastCommittedURL())) {
+    return NO;
+  }
+
+  // Create a ToolbarModel object using _tabModel. ToolbarModel has the
+  // necessary functions to ensure that the context is secure before
+  // proceeding with PaymentRequest.
+  base::scoped_nsobject<BrowserViewControllerDependencyFactory> factory(
+      [[BrowserViewControllerDependencyFactory alloc]
+          initWithBrowserState:_browserState]);
+  std::unique_ptr<ToolbarModelDelegateIOS> toolbarModelDelegate;
+  toolbarModelDelegate.reset(
+      new ToolbarModelDelegateIOS([_tabModel webStateList]));
+  std::unique_ptr<ToolbarModelIOS> toolbarModelIOS;
+  toolbarModelIOS.reset(
+      [factory newToolbarModelIOSWithDelegate:toolbarModelDelegate.get()]);
+  ToolbarModel* toolbarModel = toolbarModelIOS->GetToolbarModel();
+
+  if ([self webState]->GetLastCommittedURL().SchemeIsCryptographic()) {
+    // Ensure that if the scheme is cryptographic the SSL certificate is valid.
+    // The following three security level checks determine if the SSL
+    // certificate is valid.
+    if (toolbarModel->GetSecurityLevel(true) != security_state::EV_SECURE &&
+        toolbarModel->GetSecurityLevel(true) != security_state::SECURE &&
+        toolbarModel->GetSecurityLevel(true) !=
+            security_state::SECURE_WITH_POLICY_INSTALLED_CERT) {
+      return NO;
+    }
+  }
+
+  return YES;
 }
 
 #pragma mark - PaymentRequestCoordinatorDelegate methods
