412774f4e6d769da4ebec571b3e9e9af5f98054a
Greg Thompson <grt@chromium.org>
2017-06-06 14:24:50 +0200

    Suppress "dr" reporting for a retention toast launch.
    A browser process launch with --try-chrome-again on the command line
    will not report as an active Chrome session. Other launches are
    unaffected. A subsequent rendez-vous to a browser process started with
    --try-chrome-again will start reporting as an active session.
    
    Previously, "dr" was reset on shutdown when --try-chrome-again was used
    and the user chose not to run Chrome. Now, "dr" is not touched during
    the initial browsing session containing --try-chrome-again on the
    command line. This makes the reset unnecessary, and no longer counts
    such launches as active under any circumstances. As above, subsequent
    launches (or rendez-vous to the same process) will have "dr" set as
    usual.
    
    BUG=717091
    
    Change-Id: I68aa76d6457061d0799866cc705e18828ddf0db5

---
+++ chrome/browser/win/did_run_util.cc
@@ -0,0 +1,12 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/win/did_run_util.h"
+
+#include "base/command_line.h"
+#include "chrome/common/chrome_switches.h"
+
+bool ShouldRecordActiveUse(const base::CommandLine& command_line) {
+  return !command_line.HasSwitch(switches::kTryChromeAgain);
+}
