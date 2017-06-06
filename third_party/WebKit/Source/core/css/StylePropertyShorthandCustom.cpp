29cfb05844deb42c41ae16c694a17a8b47a9bb84
Fernando Serboncini <fserb@chromium.org>
2017-06-06 16:37:09 -0400

    Adds new CSSParser threaded tests + fixes data races related to Parsing
    Add CSSParser tests for Filter and Font.
    Uncovered and solved the following data races:
    
      - StylePropertyShorthandCustom (THREAD_SAFE)
      - StylePropertyShorthand template (THREAD_SAFE)
      - CSSPropertyMetadata template (make initialization atomic)
    
    Bug: 724565
    Change-Id: I9a8d1acd9721a41d02d6d2447fa3f27935a761a1

--- third_party/WebKit/Source/core/css/StylePropertyShorthandCustom.cpp
+++ third_party/WebKit/Source/core/css/StylePropertyShorthandCustom.cpp
@@ -38,10 +38,10 @@ const StylePropertyShorthand& animationShorthandForParsing() {
       CSSPropertyAnimationDelay,     CSSPropertyAnimationIterationCount,
       CSSPropertyAnimationDirection, CSSPropertyAnimationFillMode,
       CSSPropertyAnimationPlayState, CSSPropertyAnimationName};
-  DEFINE_STATIC_LOCAL(StylePropertyShorthand,
-                      webkit_animation_longhands_for_parsing,
-                      (CSSPropertyAnimation, kAnimationPropertiesForParsing,
-                       WTF_ARRAY_LENGTH(kAnimationPropertiesForParsing)));
+  DEFINE_THREAD_SAFE_STATIC_LOCAL(
+      StylePropertyShorthand, webkit_animation_longhands_for_parsing,
+      (CSSPropertyAnimation, kAnimationPropertiesForParsing,
+       WTF_ARRAY_LENGTH(kAnimationPropertiesForParsing)));
   return webkit_animation_longhands_for_parsing;
 }
 
@@ -51,9 +51,9 @@ const StylePropertyShorthand& transitionShorthandForParsing() {
   static const CSSPropertyID kTransitionProperties[] = {
       CSSPropertyTransitionDuration, CSSPropertyTransitionTimingFunction,
       CSSPropertyTransitionDelay, CSSPropertyTransitionProperty};
-  DEFINE_STATIC_LOCAL(StylePropertyShorthand, transition_longhands,
-                      (CSSPropertyTransition, kTransitionProperties,
-                       WTF_ARRAY_LENGTH(kTransitionProperties)));
+  DEFINE_THREAD_SAFE_STATIC_LOCAL(StylePropertyShorthand, transition_longhands,
+                                  (CSSPropertyTransition, kTransitionProperties,
+                                   WTF_ARRAY_LENGTH(kTransitionProperties)));
   return transition_longhands;
 }
 
