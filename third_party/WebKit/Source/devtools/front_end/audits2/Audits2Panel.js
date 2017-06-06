84992e6356f128533dff8bfc55bc32427cb279ef
Patrick Hulce <phulce@chromium.org>
2017-06-06 14:42:11 -0700

    DevTools: Add close button to Audits2 dialog
    BUG=729527
    
    Change-Id: Ie70e021546c41a2ebd6d8a6769796fd5093ce886

--- third_party/WebKit/Source/devtools/front_end/audits2/Audits2Panel.js
+++ third_party/WebKit/Source/devtools/front_end/audits2/Audits2Panel.js
@@ -169,6 +169,11 @@ Audits2.Audits2Panel = class extends UI.PanelWithSidebar {
     this._dialog.setOutsideClickCallback(event => event.consume(true));
     var root = UI.createShadowRootWithCoreStyles(this._dialog.contentElement, 'audits2/audits2Dialog.css');
     var auditsViewElement = root.createChild('div', 'audits2-view');
+
+    var closeButton = new UI.ToolbarButton(Common.UIString('Close'), 'largeicon-delete');
+    closeButton.addEventListener(UI.ToolbarButton.Events.Click, this._cancelAndClose, this);
+    auditsViewElement.appendChild(closeButton.element);
+
     var uiElement = auditsViewElement.createChild('div');
     var headerElement = uiElement.createChild('header');
     this._headerTitleElement = headerElement.createChild('p');
@@ -302,6 +307,11 @@ Audits2.Audits2Panel = class extends UI.PanelWithSidebar {
     delete this._headerTitleElement;
     delete this._emulationEnabledBefore;
     delete this._emulationOutlineEnabledBefore;
+  }
+
+  _cancelAndClose() {
+    this._cancel();
+    this._hideDialog();
   }
 
   _cancel() {
