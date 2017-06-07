bc646c932e573fe9bafc99a7af3e436adaf66f6b
Xiaohan Wang <xhwang@chromium.org>
2017-06-07 12:26:43 -0700

    media: Refactor Android key system secure codec support
    On Android, a "secure codec" implicitly guarantees that:
    1. keys and decrypted/decoded media data are protected by hardware, and
    2. the decoder is a hardware decoder.
    
    The output of a secure codec can only be rendered correctly by
    a SurfaceView or a secure texture. Currently, we use a preference
    use_video_overlay_for_embedded_encrypted_video to declare whether
    secure codec can be rendered correctly. This information is passed to
    the KeySystemConfigSelector to decide whether certain robustness
    levels (e.g. HW_SECURE_ALL) can be supported.
    
    This CL drops the use_video_overlay_for_embedded_encrypted_video
    preference, and moves "whether secure codec can be renderred" logic
    to the browser side in CdmMessageFilterAndroid. Only when
    |can_use_secure_codecs| is true that |secure_codecs| are queried
    and returned.
    
    As such, the renderer process should not worry about whether and how a
    |secure_codecs| can be rendered. Thus |non_compositing_codec| is
    also renamed to |secure_codecs|.
    
    In the future, we will replace |can_use_secure_codecs| with more
    sophisticated logic, e.g. check whether AndroidOverlay is enabled.
    
    Detailed changes:
    - Drop use_video_overlay_for_embedded_encrypted_video preference
    - Add |can_use_secure_codecs| in CdmMessageFilterAndroid, which is
      checked when querying |secure_codecs| support.
    - Rename |compositing_codec| to |non_secure_codecs|.
    - Rename |non_compositing_codec| to |secure_codecs|.
    - Drop |are_secure_codecs_supported| in key system config selection,
      because all returned |secure_codecs| must be supported.
    
    BUG=577777,459414
    TEST=No functionality change
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I2c87bd955dd16c89ea5dbd83f7534e2e513bd460

--- media/blink/key_system_config_selector.cc
+++ media/blink/key_system_config_selector.cc
@@ -159,7 +159,6 @@ struct KeySystemConfigSelector::SelectionRequest {
   base::Callback<void(const blink::WebString&)> not_supported_cb;
   bool was_permission_requested = false;
   bool is_permission_granted = false;
-  bool are_secure_codecs_supported = false;
 };
 
 // Accumulates configuration rules to determine if a feature (additional
@@ -181,9 +180,7 @@ class KeySystemConfigSelector::ConfigState {
 
   bool IsIdentifierRecommended() const { return is_identifier_recommended_; }
 
-  bool AreHwSecureCodecsRequired() const {
-    return are_hw_secure_codecs_required_;
-  }
+  bool AreHwSecureCodecsRequired() const { return are_secure_codecs_required_; }
 
   // Checks whether a rule is compatible with all previously added rules.
   bool IsRuleSupported(EmeConfigRule rule) const {
@@ -206,9 +203,9 @@ class KeySystemConfigSelector::ConfigState {
         return (!is_identifier_not_allowed_ && IsPermissionPossible() &&
                 !is_persistence_not_allowed_);
       case EmeConfigRule::HW_SECURE_CODECS_NOT_ALLOWED:
-        return !are_hw_secure_codecs_required_;
+        return !are_secure_codecs_required_;
       case EmeConfigRule::HW_SECURE_CODECS_REQUIRED:
-        return !are_hw_secure_codecs_not_allowed_;
+        return !are_secure_codecs_not_allowed_;
       case EmeConfigRule::SUPPORTED:
         return true;
     }
@@ -243,10 +240,10 @@ class KeySystemConfigSelector::ConfigState {
         is_persistence_required_ = true;
         return;
       case EmeConfigRule::HW_SECURE_CODECS_NOT_ALLOWED:
-        are_hw_secure_codecs_not_allowed_ = true;
+        are_secure_codecs_not_allowed_ = true;
         return;
       case EmeConfigRule::HW_SECURE_CODECS_REQUIRED:
-        are_hw_secure_codecs_required_ = true;
+        are_secure_codecs_required_ = true;
         return;
       case EmeConfigRule::SUPPORTED:
         return;
@@ -278,8 +275,8 @@ class KeySystemConfigSelector::ConfigState {
 
   // Whether a rule has been added that requires or blocks hardware-secure
   // codecs.
-  bool are_hw_secure_codecs_required_ = false;
-  bool are_hw_secure_codecs_not_allowed_ = false;
+  bool are_secure_codecs_required_ = false;
+  bool are_secure_codecs_not_allowed_ = false;
 };
 
 KeySystemConfigSelector::KeySystemConfigSelector(
@@ -850,7 +847,6 @@ void KeySystemConfigSelector::SelectConfig(
     const blink::WebVector<blink::WebMediaKeySystemConfiguration>&
         candidate_configurations,
     const blink::WebSecurityOrigin& security_origin,
-    bool are_secure_codecs_supported,
     base::Callback<void(const blink::WebMediaKeySystemConfiguration&,
                         const CdmConfig&)> succeeded_cb,
     base::Callback<void(const blink::WebString&)> not_supported_cb) {
@@ -877,7 +873,6 @@ void KeySystemConfigSelector::SelectConfig(
   request->key_system = key_system_ascii;
   request->candidate_configurations = candidate_configurations;
   request->security_origin = security_origin;
-  request->are_secure_codecs_supported = are_secure_codecs_supported;
   request->succeeded_cb = succeeded_cb;
   request->not_supported_cb = not_supported_cb;
   SelectConfigInternal(std::move(request));
@@ -902,10 +897,6 @@ void KeySystemConfigSelector::SelectConfigInternal(
     //        and return a new MediaKeySystemAccess object.]
     ConfigState config_state(request->was_permission_requested,
                              request->is_permission_granted);
-    DCHECK(config_state.IsRuleSupported(
-        EmeConfigRule::HW_SECURE_CODECS_NOT_ALLOWED));
-    if (!request->are_secure_codecs_supported)
-      config_state.AddRule(EmeConfigRule::HW_SECURE_CODECS_NOT_ALLOWED);
     blink::WebMediaKeySystemConfiguration accumulated_configuration;
     CdmConfig cdm_config;
     ConfigurationSupport support = GetSupportedConfiguration(
