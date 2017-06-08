a174586b127b54183e94c583879704ab4d92313d
Takeshi Yoshino <tyoshino@chromium.org>
2017-06-08 15:39:05 +0900

    Clean up security/resources/cors-script.php and security/suborigins/resources/redirect.php
    Rename the query parameters as follows for readability:
    - cors -> ACAOrigin
    - credentials -> ACACredentials
    - custom -> ACAHeaders
    
    This naming convention is also used for fetch layout tests and makes
    clear the parameters are about the CORS Access-Control-Allow-.* family
    headers.
    
    Rename the query parameter fail to throw to clarify what it does.
    
    Remove strtolower() calls.
    
    [redirect.php] Separate check on the received suborigin and generation
    of the Access-Control-Allow-Suborigin header..
    
    [redirect.php] Add cookie checking logic.
    
    Suborigin layouttests
    
    Bug:
    Change-Id: I1ef34ddc14d23771b16591630b1e3c76495abb0e

--- third_party/WebKit/LayoutTests/http/tests/security/resources/cors-script.php
+++ third_party/WebKit/LayoutTests/http/tests/security/resources/cors-script.php
@@ -1,50 +1,57 @@
 <?php
-$cors_arg = strtolower($_GET["cors"]);
+$cors_arg = $_GET["ACAOrigin"];
 if ($cors_arg != "false") {
-    if ($cors_arg == "" || $cors_arg == "true") {
-        header("Access-Control-Allow-Origin: http://127.0.0.1:8000");
-    } else {
-        header("Access-Control-Allow-Origin: " . $cors_arg . "");
-    }
-}
-if (strtolower($_GET["credentials"]) == "true") {
-    header("Access-Control-Allow-Credentials: true");
-}
-$custom_header_arg = strtolower($_GET["custom"]);
-if (!(empty($custom_header_arg))) {
-    header("Access-Control-Allow-Headers: " . $custom_header_arg);
+  if ($cors_arg == "" || $cors_arg == "true") {
+    header("Access-Control-Allow-Origin: http://127.0.0.1:8000");
+  } else {
+    header("Access-Control-Allow-Origin: " . $cors_arg . "");
+  }
 }
 
-$suborigin_arg = strtolower($_GET["suborigin"]);
-if (!(empty($suborigin_arg))) {
-    header("Access-Control-Allow-Suborigin: " . $suborigin_arg); 
+if (isset($_GET["ACASuborigin"])) {
+  header("Access-Control-Allow-Suborigin: " . $_GET["ACASuborigin"]); 
+}
+
+if (isset($_GET["ACACredentials"])) {
+  header("Access-Control-Allow-Credentials: " . $_GET["ACACredentials"]);
+}
+
+if (isset($_GET["ACAHeaders"])) {
+  header("Access-Control-Allow-Headers: " . $_GET["ACAHeaders"]);
 }
 
 if ($_SERVER["HTTP_SUBORIGIN"] == "foobar") {
-    header("Access-Control-Allow-Suborigin: foobar");
+  header("Access-Control-Allow-Suborigin: foobar");
 }
 
 header("Content-Type: application/javascript");
+
 $delay = $_GET['delay'];
 if ($delay)
-    usleep(1000 * $delay);
-$value = $_GET['value'];
-$result_var = "result";
-if (!empty($_GET["resultvar"])) {
-  $result_var = $_GET["resultvar"];
-}
-$cookie = $_GET['cookie'];
-if ($_SERVER['HTTP_ORIGIN'] && $_GET['value_cors']) {
+  usleep(1000 * $delay);
+
+if ($_GET["throw"] == "true") {
+  echo "throw({toString: function(){ return 'SomeError' }});";
+} else {
+  $value = $_GET['value'];
+  if ($_SERVER['HTTP_ORIGIN'] && $_GET['value_cors']) {
     $value = $_GET['value_cors'];
-}
-if ($value || $cookie) {
-    if ($cookie) {
-      $value = $_COOKIE[$cookie];
+  }
+
+  $cookie = $_GET['cookie'];
+  if ($cookie) {
+    $value = $_COOKIE[$cookie];
+  }
+
+  if ($value) {
+    $result_var = "result";
+    if (!empty($_GET["resultvar"])) {
+      $result_var = $_GET["resultvar"];
     }
 
     echo $result_var . " = \"" . $value . "\";";
-} else if (strtolower($_GET["fail"]) == "true")
-    echo "throw({toString: function(){ return 'SomeError' }});";
-else
+  } else {
     echo "alert('script ran.');";
+  }
+}
 ?>
