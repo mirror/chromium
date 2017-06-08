2c3704644426eaa5d52d6c4c00ad58f2d3261c5a
Jeffrey He <jeffreyhe@google.com>
2017-06-08 11:23:38 -0400

    Added SequenceLocalStorageMap
    SequenceLocalStorageMap allows storing and then retrieving arbitrary values to/from a sequence.
    It stores the values itself and their destructors. When the SequenceLocalStorageMap object is 
    destroyed, the values are also destroyed by their associated destructors.
    
    This change will make it easier to migrate from SingleThreadTaskRunner
    to SequencedTaskRunner for applications that needed thread-local-storage.
    
    Reference: https://chromium-review.googlesource.com/c/525834/
    
    Bug: 695727
    Change-Id: I4f8558cd21aa30e7932bdd039ca618c6ff9c6de9

---
+++ base/threading/sequence_local_storage_map.h
@@ -0,0 +1,68 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_THREADING_SEQUENCE_LOCAL_STORAGE_MAP_H_
+#define BASE_THREADING_SEQUENCE_LOCAL_STORAGE_MAP_H_
+
+#include "base/base_export.h"
+#include "base/containers/flat_map.h"
+#include "base/macros.h"
+
+namespace base {
+namespace internal {
+
+// A SequenceLocalStorageMap holds (slot id) -> (value, destructor) items for a
+// sequence. When a task runs, it is expected that a pointer to its sequence's
+// SequenceLocalStorageMap is set in TLS using
+// ScopedSetSequenceMapLocalStorageForCurrentThread. When a
+// SequenceLocalStorageMap is destroyed, it destroys the values that it holds
+// using the destructors associated with these values.
+class BASE_EXPORT SequenceLocalStorageMap {
+ public:
+  SequenceLocalStorageMap();
+  ~SequenceLocalStorageMap();
+
+  // Returns the SequenceLocalStorage bound to the current thread. It is invalid
+  // to call this outside the scope of a
+  // ScopedSetSequenceLocalStorageForCurrentThread.
+  static SequenceLocalStorageMap& GetForCurrentThread();
+
+  struct ValueDestructorPair {
+    void* value = nullptr;
+    void (*destructor)(void*) = nullptr;
+  };
+
+  // Returns the value stored in |slot_id|, or nullptr if no value was stored.
+  void* Get(int slot_id);
+
+  // Stores |value_destructor_pair| in |slot_id|. Overwrites and destructs any
+  // previously stored value.
+  void Set(int slot_id, ValueDestructorPair value_destructor_pair);
+
+ private:
+  // Map from slot id to ValueDestructorPair.
+  base::flat_map<int, ValueDestructorPair> sls_map;
+
+  DISALLOW_COPY_AND_ASSIGN(SequenceLocalStorageMap);
+};
+
+// Within the scope of this object,
+// SequenceLocalStorageMap::GetForCurrentThread() will return a reference to
+// the SequenceLocalStorageMap object passed to the constructor.
+// There can only be one ScopedSetSequenceLocalStorageMapForCurrentThread
+// instance per thread at a time.
+class BASE_EXPORT ScopedSetSequenceLocalStorageMapForCurrentThread {
+ public:
+  ScopedSetSequenceLocalStorageMapForCurrentThread(
+      SequenceLocalStorageMap* sequence_local_storage);
+
+  ~ScopedSetSequenceLocalStorageMapForCurrentThread();
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ScopedSetSequenceLocalStorageMapForCurrentThread);
+};
+}  // namespace internal
+}  // namespace base
+
+#endif  // BASE_THREADING_SEQUENCE_LOCAL_STORAGE_MAP_H_
