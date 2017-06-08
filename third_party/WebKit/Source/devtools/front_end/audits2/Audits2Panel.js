f90c2b8b006dc5cfe4b51d72d03f0bfe387269b5
Patrick Hulce <phulce@chromium.org>
2017-06-08 11:06:03 -0700

    DevTools: Prevent audit of about:blank
    Change-Id: I60786047e77c9569a78abe78699e0010f3915688

--- third_party/WebKit/Source/devtools/front_end/audits2/Audits2Panel.js
+++ third_party/WebKit/Source/devtools/front_end/audits2/Audits2Panel.js
@@ -108,10 +108,22 @@ Audits2.Audits2Panel = class extends UI.PanelWithSidebar {
     return Audits2.Audits2Panel.Presets.some(preset => preset.setting.get());
   }
 
+  /**
+   * @return {?string}
+   */
+  _getUnauditablePageMessage() {
+    var inspectedURL = SDK.targetManager.mainTarget().inspectedURL();
+    if (/^about:/.test(inspectedURL))
+      return 'Cannot audit about:* pages. Navigate to a different page to start an audit.';
+
+    return null;
+  }
+
   _updateStartButtonEnabled() {
     var hasActiveServiceWorker = this._hasActiveServiceWorker();
     var hasAtLeastOneCategory = this._hasAtLeastOneCategory();
-    var isDisabled = hasActiveServiceWorker || !hasAtLeastOneCategory;
+    var unauditablePageMessage = this._getUnauditablePageMessage();
+    var isDisabled = hasActiveServiceWorker || !hasAtLeastOneCategory || !!unauditablePageMessage;
 
     if (this._dialogHelpText && hasActiveServiceWorker) {
       this._dialogHelpText.textContent = Common.UIString(
@@ -121,6 +133,9 @@ Audits2.Audits2Panel = class extends UI.PanelWithSidebar {
 
     if (this._dialogHelpText && !hasAtLeastOneCategory)
       this._dialogHelpText.textContent = Common.UIString('At least one category must be selected.');
+
+    if (this._dialogHelpText && unauditablePageMessage)
+      this._dialogHelpText.textContent = Common.UIString(unauditablePageMessage);
 
     if (this._dialogHelpText)
       this._dialogHelpText.classList.toggle('hidden', !isDisabled);
