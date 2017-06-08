71418e31d065b5d87d5152317cb3492be31c8e6a
Alok Priyadarshi <alokp@chromium.org>
2017-06-08 11:59:13 -0700

    [chromecast] Adds browsertest for webaudio.
    Change-Id: Ibd260834ece68ad3b0bd86581c597ef57e9fdf80

---
+++ chromecast/browser/test/cast_webaudio_browsertest.cc
@@ -0,0 +1,15 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromecast/browser/test/cast_browser_test.h"
+
+namespace chromecast {
+namespace shell {
+
+class CastWebAudioBrowserTest : public CastBrowserTest {};
+
+IN_PROC_BROWSER_TEST_F(CastWebAudioBrowserTest, Basic) {}
+
+}  // namespace shell
+}  // namespace chromecast
