1fbbbf6b013c6b3ea1519253de13a0c618a76943
Eugene Ostroukhov <eostroukhov@chromium.org>
2017-06-09 14:36:16 -0700

    [DevTools] Display usage & quota information
    Adds a simple display for temporary and persistent storage information.
    
    Bug: 619521
    Change-Id: Ia6caabe553a4f0313fdb75d135fc021c6c19593c

--- content/browser/devtools/protocol/storage_handler.cc
+++ content/browser/devtools/protocol/storage_handler.cc
@@ -4,14 +4,16 @@
 
 #include "content/browser/devtools/protocol/storage_handler.h"
 
-#include <string>
 #include <unordered_set>
+#include <utility>
 #include <vector>
 
 #include "base/strings/string_split.h"
 #include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/storage_partition.h"
+#include "storage/browser/quota/quota_manager.h"
+#include "storage/common/quota/quota_status_code.h"
 
 namespace content {
 namespace protocol {
@@ -27,7 +29,32 @@ static const char kWebSQL[] = "websql";
 static const char kServiceWorkers[] = "service_workers";
 static const char kCacheStorage[] = "cache_storage";
 static const char kAll[] = "all";
+
+void UsageAndQuotaDataCallback(
+    std::unique_ptr<StorageHandler::GetUsageAndQuotaCallback> callback,
+    storage::QuotaStatusCode code,
+    int64_t usage,
+    int64_t quota) {
+  if (code != storage::kQuotaStatusOk)
+    return callback->sendFailure(
+        Response::Error("Quota information is not available"));
+
+  BrowserThread::PostTask(
+      BrowserThread::UI, FROM_HERE,
+      base::Bind(&StorageHandler::GetUsageAndQuotaCallback::sendSuccess,
+                 base::Passed(std::move(callback)), usage, quota));
 }
+
+void GetUsageAndQuotaOnIOThread(
+    storage::QuotaManager* manager,
+    const GURL& url,
+    std::unique_ptr<StorageHandler::GetUsageAndQuotaCallback> callback) {
+  manager->GetUsageAndQuotaForWebApps(
+      url, storage::kStorageTypeTemporary,
+      base::Bind(&UsageAndQuotaDataCallback,
+                 base::Passed(std::move(callback))));
+}
+}  // namespace
 
 StorageHandler::StorageHandler()
     : DevToolsDomainHandler(Storage::Metainfo::domainName),
@@ -86,6 +113,21 @@ Response StorageHandler::ClearDataForOrigin(
       partition->GetURLRequestContext(),
       base::Bind(&base::DoNothing));
   return Response::OK();
+}
+
+void StorageHandler::GetUsageAndQuota(
+    const String& origin,
+    std::unique_ptr<GetUsageAndQuotaCallback> callback) {
+  if (!host_)
+    return callback->sendFailure(Response::InternalError());
+
+  storage::QuotaManager* manager =
+      host_->GetProcess()->GetStoragePartition()->GetQuotaManager();
+
+  BrowserThread::PostTask(
+      BrowserThread::IO, FROM_HERE,
+      base::Bind(&GetUsageAndQuotaOnIOThread, manager, GURL(origin),
+                 base::Passed(std::move(callback))));
 }
 
 }  // namespace protocol
