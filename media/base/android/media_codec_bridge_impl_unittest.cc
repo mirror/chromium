aa8ae0155c8b6f5bf310098cb77e501427e0dd8d
Xiaohan Wang <xhwang@chromium.org>
2017-06-09 16:36:53 -0700

    media: Refactor MediaCodec creation requirements
    Today we pass in both |requires_secure_codec| and
    |require_software_codec| which may cotradict with each other given
    secure codecs are all hardware codecs. Add an enum to avoid this issue.
    
    Also fixes a bug where if both |requires_secure_codec| and
    |require_software_codec| are true, we would still create a secure (and
    hence hardware) codec which may hang the GPU process. In this case, we
    should just fail to create the codec.
    
    BUG=459414
    
    Change-Id: Icd8a403fe5a63b68cfe4045da7057be64ea9de66

--- media/base/android/media_codec_bridge_impl_unittest.cc
+++ media/base/android/media_codec_bridge_impl_unittest.cc
@@ -166,8 +166,8 @@ TEST(MediaCodecBridgeTest, CreateH264Decoder) {
   SKIP_TEST_IF_MEDIA_CODEC_IS_NOT_AVAILABLE();
 
   MediaCodecBridgeImpl::CreateVideoDecoder(
-      kCodecH264, false, gfx::Size(640, 480), nullptr, nullptr,
-      std::vector<uint8_t>(), std::vector<uint8_t>());
+      kCodecH264, CodecType::ANY, gfx::Size(640, 480), nullptr,
+      nullptr, std::vector<uint8_t>(), std::vector<uint8_t>());
 }
 
 TEST(MediaCodecBridgeTest, DoNormal) {
@@ -275,8 +275,8 @@ TEST(MediaCodecBridgeTest, PresentationTimestampsDoNotDecrease) {
 
   std::unique_ptr<MediaCodecBridge> media_codec(
       MediaCodecBridgeImpl::CreateVideoDecoder(
-          kCodecVP8, false, gfx::Size(320, 240), nullptr, nullptr,
-          std::vector<uint8_t>(), std::vector<uint8_t>()));
+          kCodecVP8, CodecType::ANY, gfx::Size(320, 240), nullptr,
+          nullptr, std::vector<uint8_t>(), std::vector<uint8_t>()));
   ASSERT_THAT(media_codec, NotNull());
   scoped_refptr<DecoderBuffer> buffer = ReadTestDataFile("vp8-I-frame-320x240");
   DecodeMediaFrame(media_codec.get(), buffer->data(), buffer->data_size(),
@@ -303,10 +303,11 @@ TEST(MediaCodecBridgeTest, CreateUnsupportedCodec) {
   EXPECT_THAT(MediaCodecBridgeImpl::CreateAudioDecoder(
                   NewAudioConfig(kUnknownAudioCodec), nullptr),
               IsNull());
-  EXPECT_THAT(MediaCodecBridgeImpl::CreateVideoDecoder(
-                  kUnknownVideoCodec, false, gfx::Size(320, 240), nullptr,
-                  nullptr, std::vector<uint8_t>(), std::vector<uint8_t>()),
-              IsNull());
+  EXPECT_THAT(
+      MediaCodecBridgeImpl::CreateVideoDecoder(
+          kUnknownVideoCodec, CodecType::ANY, gfx::Size(320, 240),
+          nullptr, nullptr, std::vector<uint8_t>(), std::vector<uint8_t>()),
+      IsNull());
 }
 
 }  // namespace media
