dd7b0f42a065cdb8c105936d3b75d3679dda1481
Florin Malita <fmalita@chromium.org>
2017-06-09 11:31:22 -0400

    Avoid SharedBuffer::Data() use in RawResource
    Refactor RawResource::DidAddClient() to use a segment iterator and push
    data to the client one segment at a time.
    
    BUG=728627
    
    Change-Id: I01597ad179a98cb63b66779224f1007ff78ea789

--- third_party/WebKit/Source/platform/loader/fetch/RawResource.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/RawResource.cpp
@@ -148,8 +148,13 @@ void RawResource::DidAddClient(ResourceClient* c) {
     client->ResponseReceived(this, GetResponse(), nullptr);
   if (!HasClient(c))
     return;
-  if (Data())
-    client->DataReceived(this, Data()->Data(), Data()->size());
+  if (Data()) {
+    Data()->ForEachSegment([this, &client](const char* segment,
+                                           size_t segment_size,
+                                           size_t segment_offset) {
+      client->DataReceived(this, segment, segment_size);
+    });
+  }
   if (!HasClient(c))
     return;
   Resource::DidAddClient(client);
