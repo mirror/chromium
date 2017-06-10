d0e2ad4a92d5b8d7652f14fca885406c2bcf4605
Jay Civelli <jcivelli@google.com>
2017-06-09 23:23:35 -0700

    Splitting the ChildProcessService for basification.
    Bug:
    Change-Id: I357e2a81836a97cf53888ca3d7ec3b46e12c91a1

--- content/public/android/java/src/org/chromium/content/app/ChildProcessService.java
+++ content/public/android/java/src/org/chromium/content/app/ChildProcessService.java
@@ -8,8 +8,6 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
-import org.chromium.base.annotations.JNINamespace;
-
 /**
  * This is the base class for child services; the [Sandboxed|Privileged]ProcessService0, 1.. etc
  * subclasses provide the concrete service entry points, to enable the browser to connect
@@ -24,15 +22,15 @@ import org.chromium.base.annotations.JNINamespace;
  *     <service android:name="org.chromium.content.app.[Sandboxed|Privileged]ProcessServiceX"
  *              android:process=":[sandboxed|privileged]_processX" />
  */
-@JNINamespace("content")
-public class ChildProcessService extends Service {
-    private final ChildProcessServiceImpl mChildProcessServiceImpl = new ChildProcessServiceImpl();
+public abstract class ChildProcessService extends Service {
+    private ChildProcessServiceImpl mChildProcessServiceImpl;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mChildProcessServiceImpl.create(getApplicationContext(),
-                getApplicationContext());
+        assert mChildProcessServiceImpl == null;
+        mChildProcessServiceImpl = new ChildProcessServiceImpl(createDelegate());
+        mChildProcessServiceImpl.create(getApplicationContext(), getApplicationContext());
     }
 
     @Override
@@ -50,4 +48,6 @@ public class ChildProcessService extends Service {
         stopSelf();
         return mChildProcessServiceImpl.bind(intent, -1);
     }
+
+    protected abstract ChildProcessServiceDelegate createDelegate();
 }
