6ff263adabe22e7c98125296168c2a3a3264e3be
Xiaohui Chen <xiaohuic@google.com>
2017-06-12 13:56:53 -0700

    Add voice interaction animation
    BUG=b:35624617
    TEST=build and test on device, see animation runs
    
    Change-Id: I87ac8a88e565fac56de1666ffef9195928bbf634

--- ash/shelf/app_list_button.h
+++ ash/shelf/app_list_button.h
@@ -17,6 +17,11 @@ namespace ash {
 class InkDropButtonListener;
 class Shelf;
 class ShelfView;
+class VoiceInteractionOverlay;
+
+namespace test {
+class AppListButtonTest;
+}  // namespace test
 
 // Button used for the AppList icon on the shelf.
 class ASH_EXPORT AppListButton : public views::ImageButton,
@@ -38,6 +43,10 @@ class ASH_EXPORT AppListButton : public views::ImageButton,
   // views::ImageButton overrides:
   void OnGestureEvent(ui::GestureEvent* event) override;
 
+  // Get the center point of the app list button used to draw its background and
+  // ink drops.
+  gfx::Point GetCenterPoint() const;
+
  protected:
   // views::ImageButton overrides:
   bool OnMousePressed(const ui::MouseEvent& event) override;
@@ -53,13 +62,12 @@ class ASH_EXPORT AppListButton : public views::ImageButton,
   void PaintButtonContents(gfx::Canvas* canvas) override;
 
  private:
-  // Get the center point of the app list button used to draw its background and
-  // ink drops.
-  gfx::Point GetCenterPoint() const;
-
   // ShellObserver overrides:
   void OnAppListVisibilityChanged(bool shown,
                                   aura::Window* root_window) override;
+
+  friend class test::AppListButtonTest;
+  void InitVoiceInteractionLayer();
 
   // True if the app list is currently showing for this display.
   // This is useful because other IsApplistVisible functions aren't per-display.
@@ -71,6 +79,8 @@ class ASH_EXPORT AppListButton : public views::ImageButton,
   InkDropButtonListener* listener_;
   ShelfView* shelf_view_;
   Shelf* shelf_;
+
+  VoiceInteractionOverlay* voice_interaction_overlay_;
 
   DISALLOW_COPY_AND_ASSIGN(AppListButton);
 };
