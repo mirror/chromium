d61209def9fdea96091dea96e8fac4f5a6925e00
Charles Harrison <csharrison@chromium.org>
2017-06-12 09:52:27 -0400

    [subresource_filter] First round of strings / UI elements for initial launch
    These strings are mostly specific to the content setting UI. To make things
    less confusing this CL also changes names and identifiers in UI code as
    much as possible to refer to the "ads" permission instead of the subresource
    filter permission.
    
    Bug: 689487
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I4674086763af5ab4e26fce748d9043a29e49e03d

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/ContentSettingsResources.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/ContentSettingsResources.java
@@ -150,10 +150,10 @@ public class ContentSettingsResources {
                                  ContentSetting.ASK, ContentSetting.BLOCK, 0, 0));
             // TODO(csharrison): Swap out the placeholder icon once it is ready.
             localMap.put(ContentSettingsType.CONTENT_SETTINGS_TYPE_SUBRESOURCE_FILTER,
-                    new ResourceItem(R.drawable.permission_popups,
-                            R.string.subresource_filter_permission_title,
-                            R.string.subresource_filter_permission_title, ContentSetting.ALLOW,
-                            ContentSetting.BLOCK, 0, 0));
+                    new ResourceItem(R.drawable.permission_popups, R.string.ads_permission_title,
+                            R.string.ads_permission_title, ContentSetting.ALLOW,
+                            ContentSetting.BLOCK, 0,
+                            R.string.website_settings_category_ads_blocked));
             localMap.put(ContentSettingsType.CONTENT_SETTINGS_TYPE_USB_CHOOSER_DATA,
                     new ResourceItem(R.drawable.settings_usb, 0, 0, ContentSetting.ASK,
                                  ContentSetting.BLOCK, 0, 0));
@@ -299,5 +299,13 @@ public class ContentSettingsResources {
      */
     public static int getAutoplayDisabledByDataSaverSummary() {
         return R.string.website_settings_category_autoplay_disabled_data_saver;
+    }
+
+    /**
+     * Returns the blocked summary for the ads permission which should be used for display in the
+     * site settings list only.
+     */
+    public static int getAdsBlockedListSummary() {
+        return R.string.website_settings_category_ads_blocked_list;
     }
 }
