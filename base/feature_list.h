0c82290af3f6f12d10114cb0da2894962fd16d79
Tommy Nyquist <nyquist@chromium.org>
2017-06-12 14:38:16 -0700

    Remove constexpr constructor for base::Feature
    The constexpr constructor for base::Feature is only used a handful
    of places in the code base, so it seems to be unnecessary.
    
    This CL removes that constructor, and also removes the usages of it.
    
    BUG=None
    
    Change-Id: I9c639960780268bf77e5f67634addac7e93b48f0

--- base/feature_list.h
+++ base/feature_list.h
@@ -32,8 +32,6 @@ enum FeatureState {
 // for a given feature name - generally defined as a constant global variable or
 // file static.
 struct BASE_EXPORT Feature {
-  constexpr Feature(const char* name, FeatureState default_state)
-      : name(name), default_state(default_state) {}
   // The name of the feature. This should be unique to each feature and is used
   // for enabling/disabling features via command line flags and experiments.
   // It is strongly recommended to use CamelCase style for feature names, e.g.
