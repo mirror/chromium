d0de918f636f387b3ec2ae83d4f3095364818135
Fernando Serboncini <fserb@chromium.org>
2017-06-12 13:53:22 -0400

    Adds FontPersistent to store static objects of platform/font
    Currently only being used on FontCache, to be used on other Font-related
    classes.
    
    Bug: 730692
    Change-Id: I58f66d6cf59dca83be5133fc273136ce408e5b2e

--- third_party/WebKit/Source/platform/fonts/FontCache.h
+++ third_party/WebKit/Source/platform/fonts/FontCache.h
@@ -61,6 +61,7 @@ namespace blink {
 
 class FontCacheClient;
 class FontFaceCreationParams;
+class FontPersistent;
 class FontPlatformData;
 class FontDescription;
 class OpenTypeVerticalData;
@@ -221,8 +222,8 @@ class PLATFORM_EXPORT FontCache {
   void DumpShapeResultCache(base::trace_event::ProcessMemoryDump*);
 
  private:
+  friend class FontPersistent;
   FontCache();
-  ~FontCache();
 
   void Purge(PurgeSeverity = kPurgeIfNeeded);
 
