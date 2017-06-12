e9ee18a40b94edea5c6554e8d2fdbba714fb7343
tzik <tzik@chromium.org>
2017-06-12 23:07:29 +0900

    Crash hung renderer instead of killing it
    This CL adds Crasher interface to the renderer IO thread, and wires up
    it to RenderProcessHostImpl. After this CL, the browser issues a crash
    request to a hung renderer before terminating it forcibly, so that the
    renderer generate a crash report.
    
    Design: https://docs.google.com/document/d/1hcbcKoVqFUHMfKFHv1dYre0RcQhCWGi1ha28ZskJu9g/edit?usp=sharing
    
    Bug: 726607
    Change-Id: I63aaffc612157624513566a13d42a7e7dc49d4b9

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -37,6 +37,7 @@
 #include "base/metrics/persistent_memory_allocator.h"
 #include "base/metrics/user_metrics.h"
 #include "base/process/process_handle.h"
+#include "base/process/process_metrics.h"
 #include "base/rand_util.h"
 #include "base/single_thread_task_runner.h"
 #include "base/stl_util.h"
@@ -263,6 +264,10 @@ namespace {
 
 const RenderProcessHostFactory* g_render_process_host_factory_ = nullptr;
 const char kSiteProcessMapKeyName[] = "content_site_process_map";
+constexpr base::TimeDelta kVoluntaryTerminationTimeoutMin =
+    base::TimeDelta::FromSeconds(3);
+constexpr base::TimeDelta kVoluntaryTerminationTimeoutMax =
+    base::TimeDelta::FromSeconds(5);
 
 #if defined(OS_ANDROID)
 // This matches Android's ChildProcessConnection state before OnProcessLaunched.
@@ -310,6 +315,13 @@ void GetContexts(
   *resource_context_out = resource_context;
   *request_context_out =
       GetRequestContext(request_context, media_request_context, resource_type);
+}
+
+// Returns a TimeDelta value between min and max randomly.
+base::TimeDelta RandomTimeDeltaInRange(base::TimeDelta min,
+                                       base::TimeDelta max) {
+  DCHECK_LT(min, max);
+  return min + base::RandDouble() * (max - min);
 }
 
 #if BUILDFLAG(ENABLE_WEBRTC)
@@ -1260,6 +1272,7 @@ void RenderProcessHostImpl::InitializeChannelProxy() {
   // surprising behavior.
   channel_->GetRemoteAssociatedInterface(&remote_route_provider_);
   channel_->GetRemoteAssociatedInterface(&renderer_interface_);
+  content::BindInterface(this, MakeRequest(&crasher_interface_));
 
   // We start the Channel in a paused state. It will be briefly unpaused again
   // in Init() if applicable, before process launch is initiated.
@@ -2410,6 +2423,21 @@ bool RenderProcessHostImpl::FastShutdownIfPossible() {
   return true;
 }
 
+void RenderProcessHostImpl::TerminateHungRenderProcess() {
+  // Request the renderer to crash itself before terminating it forcibly.
+  crasher_interface_->Crash(CollectCrashKeysForHungRenderer());
+
+  // Randomize the delay for the forcible termination to avoid terminating
+  // multiple hung renderers at the same time.
+  base::TimeDelta delay = RandomTimeDeltaInRange(
+      kVoluntaryTerminationTimeoutMin, kVoluntaryTerminationTimeoutMax);
+  termination_request_start_time_ = base::TimeTicks::Now();
+  termination_timer_.Start(
+      FROM_HERE, delay,
+      base::Bind(&RenderProcessHostImpl::OnHungRendererTerminationTimedOut,
+                 base::Unretained(this)));
+}
+
 bool RenderProcessHostImpl::Send(IPC::Message* msg) {
   TRACE_EVENT0("renderer_host", "RenderProcessHostImpl::Send");
 
@@ -3238,6 +3266,18 @@ void RenderProcessHostImpl::ProcessDied(bool already_dead,
   if (delayed_cleanup_needed_)
     Cleanup();
 
+  // Running |termination_timer_| indicates that a crash request is sent to the
+  // renderer and the browser has not yet terminated it forcibly.
+  if (termination_timer_.IsRunning()) {
+    UMA_HISTOGRAM_CUSTOM_TIMES(
+        "HungRenderer.TimeToHungRendererTermination",
+        base::TimeTicks::Now() - termination_request_start_time_,
+        base::TimeDelta::FromMilliseconds(1), kVoluntaryTerminationTimeoutMax,
+        50);
+    termination_timer_.Stop();
+    ReportTerminationStatus(TerminationStatus::TerminatedItself);
+  }
+
   // If RenderProcessHostImpl is reused, the next renderer will send a new
   // request for FrameSinkProvider so make sure frame_sink_provider_ is ready
   // for that.
@@ -3629,6 +3669,33 @@ void RenderProcessHostImpl::OnMojoError(int render_process_id,
   base::debug::ScopedCrashKey error_key_value("mojo-message-error", error);
   bad_message::ReceivedBadMessage(render_process_id,
                                   bad_message::RPH_MOJO_PROCESS_ERROR);
+}
+
+mojom::CrashKeysPtr RenderProcessHostImpl::CollectCrashKeysForHungRenderer() {
+  mojom::CrashKeysPtr crash_keys = mojom::CrashKeys::New();
+
+  base::SystemMemoryInfoKB info;
+  base::GetSystemMemoryInfo(&info);
+
+  int64_t available = 0;
+#if defined(OS_WIN)
+  available = info.avail_phys;
+#else
+  available = info.free;
+#endif
+  crash_keys->free_memory_ratio = available * 100 / info.total;
+
+  return crash_keys;
+}
+
+void RenderProcessHostImpl::ReportTerminationStatus(TerminationStatus status) {
+  UMA_HISTOGRAM_ENUMERATION("HungRenderer.TerminationStatus", status,
+                            TerminationStatus::MaxValue);
+}
+
+void RenderProcessHostImpl::OnHungRendererTerminationTimedOut() {
+  ReportTerminationStatus(TerminationStatus::TerminatedForcibly);
+  Shutdown(RESULT_CODE_HUNG, false);
 }
 
 }  // namespace content
