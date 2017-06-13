5c2ffc8a4fabf5fb8f4ed9d8b2f35e499d5d5e95
Takeshi Yoshino <tyoshino@chromium.org>
2017-06-14 03:37:45 +0900

    Replace the credentials related parameters in ResourceLoaderOptions with FetchCredentialsMode
    Set and use the WebURLRequest::FetchCredentialsMode parameter on
    the ResourceRequest not only for Service Workers but for all CORS
    handling.
    
    After this CL, SetCrossOriginAccessControl() can be easily extended to
    accept "omit".
    
    This CL fixes a spec conformance issue that is:
    - Before this CL, once EffectiveAllowCredentials() became
      kDoNotAllowStoredCredentials, it's passed to CheckAccess() to
      effectively indicate that request's credentials mode is not "include"
      in the spec which is not right
    - After this CL, the credentials mode is passed to CheckAccess() to
      really check the condition
    
    ----
    
    Handling of the unsafe-credentials policy of the Suborigin is moved to
    DocumentThreadableLoader and ResourceFetcher. EventSource was not using
    this policy but using this opportunity enabled it.
    
    DocumentThreadbleLoader holds it in suborigin_force_credentials_ and
    resets it on the first redirect following.
    
    ----
    
    DocumentThreadableLoader was performing the CORS check using
    EffectiveAllowCredentials() before this patch. This patch makes it use
    the FetchCredentialsMode of the (initial) ResourceRequest.
    
    DocumentThreadableLoader was creating a
    CrossOriginPreflightResultCacheItem using EffectiveAllowCredentials()
    before this patch. This patch makes it use the FetchCredentialsMode of
    the (initial) ResourceRequest.
    
    The new logic included in this patch for these better corresponds to
    the spec and doesn't cause behavior change except for the cases with
    the unsafe-credentials policy of the Suborigin in use.
    
    ResourceLoader is changed to use GetResourceRequest() not
    LastResourceRequest() to get the FetchCredentialsMode to pass to
    HandleRedirect() as it's equivalent.
    
    ----
    
    WebAssociatedURLLoaderImpl is changed to pass "include" and "same-origin"
    credentials mode based on options_.allow_credentials. This would break
    users who are using non-"no-cors" mode and using allow_credentials set
    to true if any. It's because the new logic would call
    WebURLRequest::SetAllowStoredCredentials() with false when the initial
    request is cross-origin while the old code was setting it to true. We
    such users as follows:
    - ManifestManager::FetchManifest()
    - PepperURLLoaderHost::InternalOnHostMsgOpen()
    - ResourceMultiBufferDataProvider::Start()
    - TEST_F(WebAssociatedURLLoaderTest, CrossOriginWithAccessControlFailure)
    
    ManifestManager::FetchManifest() sets the variable based on the link
    element's crossOrigin attribute, so this should just get fixed.
    
    The pepper API should also just get fixed by the new logic as
    the documentation for PP_URLREQUESTPROPERTY_ALLOWCREDENTIALS is
    saying that the parameter should determine whether credentials
    should be sent for cross-origin or not.
    
    It looks the same for ResourceMultiBufferDataProvider.
    
    We should fix these users who look intended to use "include" to really
    use "include" but it can be done later in a separate CL.
    
    The CrossOriginWithAccessControlFailure test was working since
    allow_credentials set to true worked as "include" (there's no
    redirect involved).
    
    ----
    
    WorkerScriptLoader and NotificationImageLoader were also setting the
    initial value of allow_credentials to kAllowStoredCredentials without
    checking origins. There are also fixed to use "same-origin".
    
    ----
    
    WebFeature::kXMLHttpRequestCrossOriginWithCredentials counting logic
    has been changed not to consider the suborigin policy effect. We could
    keep it but it would leave the complexity here.
    
    ----
    
    FileReaderLoader is changed to just use the default
    FetchCredentialsMode as it does nothing for blobs.
    
    Bug: 717525, 427429, 727596, 695808
    Change-Id: I663e40c8385868b6e64fe71248f85f8aff1ae176

--- third_party/WebKit/Source/platform/loader/fetch/ResourceFetcher.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/ResourceFetcher.cpp
@@ -582,8 +582,33 @@ ResourceFetcher::PrepareRequestResult ResourceFetcher::PrepareRequest(
   if (!params.Url().IsValid())
     return kAbort;
 
-  resource_request.SetAllowStoredCredentials(
-      params.Options().allow_credentials == kAllowStoredCredentials);
+  RefPtr<SecurityOrigin> origin = params.Options().security_origin;
+  params.MutableOptions().cors_flag =
+      !origin || !origin->CanRequestNoSuborigin(params.Url());
+
+  if (params.Options().cors_enabled == kIsCORSEnabled) {
+    bool allow_stored_credentials = false;
+    switch (resource_request.GetFetchCredentialsMode()) {
+      case WebURLRequest::kFetchCredentialsModeOmit:
+        break;
+      case WebURLRequest::kFetchCredentialsModeSameOrigin:
+        allow_stored_credentials =
+            !params.Options().cors_flag ||
+            (origin &&
+             origin->HasSuboriginAndShouldAllowCredentialsFor(params.Url()));
+        break;
+      case WebURLRequest::kFetchCredentialsModeInclude:
+      case WebURLRequest::kFetchCredentialsModePassword:
+        allow_stored_credentials = true;
+        break;
+    }
+    resource_request.SetAllowStoredCredentials(allow_stored_credentials);
+  } else {
+    resource_request.SetAllowStoredCredentials(
+        resource_request.GetFetchCredentialsMode() !=
+        WebURLRequest::kFetchCredentialsModeOmit);
+  }
+
   return kContinue;
 }
 
