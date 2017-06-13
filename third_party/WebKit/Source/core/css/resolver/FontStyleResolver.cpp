1b5d82eae9b7cb1903aec95d0486a6bebb8d9854
Fernando Serboncini <fserb@chromium.org>
2017-06-13 14:28:27 -0400

    Creates FontStyleResolver for off-main thread font resolution
    This still shares code with StyleBuilderConverter and FontBuilder.
    For that, we create a StyleBuilderConverterBase that is independent of
    ComputedStyle.
    
    Bug: 730692
    Change-Id: I7792ef340a5f95782f57a94e093c3c7d73ad11b1

---
+++ third_party/WebKit/Source/core/css/resolver/FontStyleResolver.cpp
@@ -0,0 +1,46 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/resolver/FontStyleResolver.h"
+
+#include "core/css/CSSToLengthConversionData.h"
+#include "core/css/resolver/FontBuilder.h"
+#include "core/css/resolver/StyleBuilderConverter.h"
+
+namespace blink {
+
+void FontStyleResolver::ComputeFont(const StylePropertySet& property_set) {
+  FontBuilder builder(nullptr);
+
+  FontDescription fontDescription;
+  Font font(fontDescription);
+  CSSToLengthConversionData::FontSizes fontSizes(16, 16, &font);
+  CSSToLengthConversionData::ViewportSize viewportSize(0, 0);
+  CSSToLengthConversionData conversionData(nullptr, fontSizes, viewportSize, 1);
+
+  builder.SetSize(StyleBuilderConverterBase::ConvertFontSize(
+      *property_set.GetPropertyCSSValue(CSSPropertyFontSize), conversionData,
+      FontDescription::Size(0, 0.0f, false)));
+
+  builder.SetFamilyDescription(StyleBuilderConverterBase::ConvertFontFamily(
+      *property_set.GetPropertyCSSValue(CSSPropertyFontFamily), &builder,
+      nullptr));
+
+  builder.SetStretch(ToCSSIdentifierValue(*property_set.GetPropertyCSSValue(
+                                              CSSPropertyFontStretch))
+                         .ConvertTo<FontStretch>());
+
+  builder.SetStyle(ToCSSIdentifierValue(
+                       *property_set.GetPropertyCSSValue(CSSPropertyFontStyle))
+                       .ConvertTo<FontStyle>());
+
+  builder.SetVariantCaps(StyleBuilderConverterBase::ConvertFontVariantCaps(
+      *property_set.GetPropertyCSSValue(CSSPropertyFontVariantCaps)));
+
+  builder.SetWeight(StyleBuilderConverterBase::ConvertFontWeight(
+      *property_set.GetPropertyCSSValue(CSSPropertyFontWeight),
+      FontBuilder::InitialWeight()));
+}
+
+}  // namespace blink
