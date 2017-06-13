eab85b02680ea5b985463a8e4053070686cedf72
Ben Wagner <bungeman@chromium.org>
2017-06-13 11:14:54 -0400

    sublime/ninja_options_script.py shell quote output.
    The sublimeclang plugin expects its input to be parseable by shlex.
    The current output of ninja_options_script.py it not escaped,
    leading to incorrect parsing. This changes ninja_options_script.py
    to escape its output for parsing by shlex.
    
    BUG=chromium:732670
    
    Change-Id: Ib57a8c15a3cc78df83ee79fa8d92a04b57531572

--- tools/sublime/ninja_options_script.py
+++ tools/sublime/ninja_options_script.py
@@ -17,6 +17,7 @@
 import imp
 import optparse
 import os
+import pipes
 
 ycm_module_path = os.path.normpath(
     os.path.join(os.path.dirname(os.path.abspath(__file__)),
@@ -38,7 +39,7 @@ def main():
   results = ycm_extra_conf.FlagsForFile(path)
 
   for flag in results['flags']:
-    print flag
+    print pipes.quote(flag)
 
 if __name__ == "__main__":
   main()
