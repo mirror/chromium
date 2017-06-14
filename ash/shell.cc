5000102b57b6955d3f4aa96f504c0681fdbfb86b
Keigo Oka <oka@chromium.org>
2017-06-14 22:43:48 +0900

    Ash: Inline InitKeyboard, and make the caller of it call CreateKeyboard.
    InitKeyboard creates a keyboard controller but doesn't associate it with any
    root window controller. It's potentially problematic if
    keyboard::ShowKeyboard is called before RootWindowController::ActivateKeyboard.
    
    Bug: 712873
    
    Change-Id: I5b34cd6f8b9e1b0736b5d91880e3aedc9d57970c

--- ash/shell.cc
+++ ash/shell.cc
@@ -354,7 +354,20 @@ void Shell::OnRootWindowAdded(aura::Window* root_window) {
 }
 
 void Shell::CreateKeyboard() {
-  InitKeyboard();
+  if (keyboard::IsKeyboardEnabled()) {
+    if (keyboard::KeyboardController::GetInstance()) {
+      RootWindowControllerList controllers = GetAllRootWindowControllers();
+      for (RootWindowControllerList::iterator iter = controllers.begin();
+           iter != controllers.end(); ++iter) {
+        (*iter)->DeactivateKeyboard(
+            keyboard::KeyboardController::GetInstance());
+      }
+    }
+    keyboard::KeyboardController::ResetInstance(
+        new keyboard::KeyboardController(shell_delegate_->CreateKeyboardUI(),
+                                         virtual_keyboard_controller_.get()));
+  }
+
   GetPrimaryRootWindowController()->ActivateKeyboard(
       keyboard::KeyboardController::GetInstance());
 }
