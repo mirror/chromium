8a51ce515282baa319b76a0f7805dd769c2e753d
Joel Hockey <joelhockey@chromium.org>
2017-06-13 21:52:21 -0700

    Add CHECKS to see that FrameView is detached before Frame
    Bug: 731092
    Change-Id: If7d2bf681575b47971bcd9862574d9af30fee731

--- third_party/WebKit/Source/core/frame/LocalFrameView.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrameView.cpp
@@ -241,6 +241,7 @@ LocalFrameView::~LocalFrameView() {
 
 DEFINE_TRACE(LocalFrameView) {
   visitor->Trace(frame_);
+  visitor->Trace(parent_);
   visitor->Trace(fragment_anchor_);
   visitor->Trace(scrollable_areas_);
   visitor->Trace(animating_scrollable_areas_);
@@ -3883,9 +3884,17 @@ void LocalFrameView::RemoveAnimatingScrollableArea(
 }
 
 void LocalFrameView::AttachToLayout() {
-  DCHECK(!is_attached_);
+  // TODO(crbug.com/729196): Trace why LocalFrameView::DetachFromLayout crashes.
+  CHECK(!is_attached_);
   is_attached_ = true;
-  if (ParentFrameView()->IsVisible())
+  parent_ = ParentFrameView();
+  if (!parent_) {
+    Frame* parent_frame = frame_->Tree().Parent();
+    CHECK(parent_frame);
+    CHECK(parent_frame->IsLocalFrame());
+    CHECK(parent_frame->View());
+  }
+  if (parent_->IsVisible())
     SetParentVisible(true);
   UpdateParentScrollableAreaSet();
   SetupRenderThrottling();
@@ -3893,9 +3902,18 @@ void LocalFrameView::AttachToLayout() {
 }
 
 void LocalFrameView::DetachFromLayout() {
-  DCHECK(is_attached_);
+  // TODO(crbug.com/729196): Trace why LocalFrameView::DetachFromLayout crashes.
+  CHECK(is_attached_);
+  LocalFrameView* parent = ParentFrameView();
+  if (!parent) {
+    Frame* parent_frame = frame_->Tree().Parent();
+    CHECK(parent_frame);
+    CHECK(parent_frame->IsLocalFrame());
+    CHECK(parent_frame->View());
+  }
+  CHECK(parent == parent_);
   if (!RuntimeEnabledFeatures::RootLayerScrollingEnabled())
-    ParentFrameView()->RemoveScrollableArea(this);
+    parent->RemoveScrollableArea(this);
   SetParentVisible(false);
   is_attached_ = false;
 }
