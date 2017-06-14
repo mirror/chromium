0b33b905e6c1bb2877361cc6878e7017e43e77b3
kylechar <kylechar@chromium.org>
2017-06-14 10:15:34 -0400

    Hold private interface in FrameSinkManagerHost.
    FrameSinkManagerHost will need access to the
    MojoCompositorFrameSinkPrivate interface in order to claim surface
    references. Store the interface with other FrameSink data.
    
    Bug: 732896
    Change-Id: I4c9392855d509ed9aa3d8f0c89171f0961bc45c8

--- components/viz/host/frame_sink_manager_host.cc
+++ components/viz/host/frame_sink_manager_host.cc
@@ -12,6 +12,18 @@
 
 namespace viz {
 
+// FrameSinkData:
+FrameSinkManagerHost::FrameSinkData::FrameSinkData() = default;
+
+FrameSinkManagerHost::FrameSinkData::FrameSinkData(FrameSinkData&& other) =
+    default;
+
+FrameSinkManagerHost::FrameSinkData::~FrameSinkData() = default;
+
+FrameSinkManagerHost::FrameSinkData& FrameSinkManagerHost::FrameSinkData::
+operator=(FrameSinkData&& other) = default;
+
+// FrameSinkManagerHost:
 FrameSinkManagerHost::FrameSinkManagerHost() : binding_(this) {}
 
 FrameSinkManagerHost::~FrameSinkManagerHost() = default;
@@ -35,28 +47,60 @@ void FrameSinkManagerHost::RemoveObserver(FrameSinkObserver* observer) {
 void FrameSinkManagerHost::CreateCompositorFrameSink(
     const cc::FrameSinkId& frame_sink_id,
     cc::mojom::MojoCompositorFrameSinkRequest request,
-    cc::mojom::MojoCompositorFrameSinkPrivateRequest private_request,
     cc::mojom::MojoCompositorFrameSinkClientPtr client) {
-  DCHECK(frame_sink_manager_ptr_.is_bound());
+  // Crash if we try to create a CompositorFrameSink twice to avoid any
+  // possibility of hijacking an existing CompositorFrameSink.
+  CHECK_EQ(frame_sink_data_map_.count(frame_sink_id), 0u);
+
+  FrameSinkData& data = frame_sink_data_map_[frame_sink_id];
+
   frame_sink_manager_ptr_->CreateCompositorFrameSink(
-      frame_sink_id, std::move(request), std::move(private_request),
-      std::move(client));
+      frame_sink_id, std::move(request),
+      mojo::MakeRequest(&data.private_interface), std::move(client));
+}
+
+void FrameSinkManagerHost::DestroyCompositorFrameSink(
+    const cc::FrameSinkId& frame_sink_id) {
+  auto iter = frame_sink_data_map_.find(frame_sink_id);
+  if (iter == frame_sink_data_map_.end())
+    return;
+
+  FrameSinkData& data = iter->second;
+  if (data.parent.has_value())
+    UnregisterFrameSinkHierarchy(data.parent.value(), frame_sink_id);
+
+  // This destroys the MojoCompositorFrameSinkPrivatePtr and closes the pipe.
+  frame_sink_data_map_.erase(iter);
 }
 
 void FrameSinkManagerHost::RegisterFrameSinkHierarchy(
     const cc::FrameSinkId& parent_frame_sink_id,
     const cc::FrameSinkId& child_frame_sink_id) {
-  DCHECK(frame_sink_manager_ptr_.is_bound());
+  // Register and store the parent.
   frame_sink_manager_ptr_->RegisterFrameSinkHierarchy(parent_frame_sink_id,
                                                       child_frame_sink_id);
+  frame_sink_data_map_[child_frame_sink_id].parent = parent_frame_sink_id;
 }
 
 void FrameSinkManagerHost::UnregisterFrameSinkHierarchy(
     const cc::FrameSinkId& parent_frame_sink_id,
     const cc::FrameSinkId& child_frame_sink_id) {
-  DCHECK(frame_sink_manager_ptr_.is_bound());
+  auto iter = frame_sink_data_map_.find(child_frame_sink_id);
+  if (iter == frame_sink_data_map_.end() || !iter->second.parent.has_value())
+    return;
+
+  FrameSinkData& data = iter->second;
+  DCHECK_EQ(data.parent.value(), parent_frame_sink_id);
+
+  // Unregister and clear the stored parent.
   frame_sink_manager_ptr_->UnregisterFrameSinkHierarchy(parent_frame_sink_id,
                                                         child_frame_sink_id);
+  data.parent.reset();
+
+  // If the client never called CreateCompositorFrameSink() then they won't
+  // call DestroyCompositorFrameSink() either, so cleanup map entry here.
+  if (!data.private_interface.is_bound())
+    frame_sink_data_map_.erase(iter);
 }
 
 void FrameSinkManagerHost::OnSurfaceCreated(
