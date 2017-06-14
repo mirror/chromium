966db8e6b6ce2e1300e3b40550eaa6f01c369b5a
Chris Harrelson <chrishtr@chromium.org>
2017-06-14 16:38:15 -0700

    Remove mapping from layer ids to property tree node ids.
    Bug: 718564
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Idda61c671b76d7da06c23ea1ca679ee9e914bc70

--- cc/trees/layer_tree_host.cc
+++ cc/trees/layer_tree_host.cc
@@ -1378,9 +1378,9 @@ void LayerTreeHost::SetElementTransformMutated(
   DCHECK(layer);
   layer->OnTransformAnimated(transform);
 
-  if (TransformNode* node =
-          property_trees_.transform_tree.UpdateNodeFromOwningLayerId(
-              layer->id())) {
+  TransformNode* node = layer->GetTransformNode();
+  DCHECK(node);
+  if (node) {
     DCHECK_EQ(layer->transform_tree_index(), node->id);
     if (node->local == transform)
       return;
