00e02c838312241e001d29db6c79d8d3b0038b11
Fady Samuel <fsamuel@chromium.org>
2017-06-14 15:10:10 -0400

    Clean up components/viz directory structure
    This CL does the following:
    
    1. Move components/viz/frame_sinks => components/viz/service/frame_sinks
    2. Move components/viz/display_compositor =>
        components/viz/service/display_compositor
    3. components/viz/service is a component
    4. components/viz/host no longer depends on components/viz/service.
       This required moving ConnectWithInProcessFrameSinkManager to
       surface_utils in content/browser/compositor
    
    This CL is some prerequisite cleanup for putting the MojoFrameSinkManager in
    gpu_child_thread.
    
    Bug: 730213
    Change-Id: I9f74d7ced352bf2875c4302c3e087c713e94e8c0

--- content/browser/compositor/surface_utils.h
+++ content/browser/compositor/surface_utils.h
@@ -20,6 +20,7 @@ class SurfaceManager;
 
 namespace viz {
 class FrameSinkManagerHost;
+class MojoFrameSinkManager;
 }
 
 namespace content {
@@ -35,6 +36,10 @@ void CopyFromCompositingSurfaceHasResult(
     const SkColorType color_type,
     const ReadbackRequestCallback& callback,
     std::unique_ptr<cc::CopyOutputResult> result);
+
+CONTENT_EXPORT void ConnectWithInProcessFrameSinkManager(
+    viz::FrameSinkManagerHost* host,
+    viz::MojoFrameSinkManager* manager);
 
 }  // namespace content
 
