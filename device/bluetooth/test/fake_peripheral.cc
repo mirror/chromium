1a5f19439dd3b15899ac5d1bb1cf9038c02239ba
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2017-06-15 13:15:48 +1000

    bluetooth: Implement AddFakeService and convert tests
    Adds a fake GATT Service with |uuid| to be discovered when discovering
    the peripheral's GATT Attributes. Returns a FakeRemoteGATTService
    corresponding to this service. |uuid| should be a BluetoothServiceUUIDs
    https://webbluetoothcg.github.io/web-bluetooth/#typedefdef-bluetoothserviceuuid
    
    Bug:719816
    Change-Id: I7192509b94fcc3921c4d9e8d7117dfd325a79c7e

--- device/bluetooth/test/fake_peripheral.cc
+++ device/bluetooth/test/fake_peripheral.cc
@@ -4,7 +4,11 @@
 
 #include "device/bluetooth/test/fake_peripheral.h"
 
+#include <utility>
+
 #include "base/memory/weak_ptr.h"
+#include "device/bluetooth/bluetooth_uuid.h"
+#include "device/bluetooth/test/fake_remote_gatt_service.h"
 
 namespace bluetooth {
 
@@ -40,6 +44,22 @@ void FakePeripheral::SetNextGATTConnectionResponse(uint16_t code) {
 void FakePeripheral::SetNextGATTDiscoveryResponse(uint16_t code) {
   DCHECK(!next_discovery_response_);
   next_discovery_response_ = code;
+}
+
+std::string FakePeripheral::AddFakeService(
+    const device::BluetoothUUID& service_uuid) {
+  std::string new_service_id = std::to_string(++last_service_id_);
+
+  GattServiceMap::iterator it;
+  bool inserted;
+
+  std::tie(it, inserted) = gatt_services_.emplace(
+      new_service_id,
+      base::MakeUnique<FakeRemoteGattService>(new_service_id, service_uuid,
+                                              true /* is_primary */, this));
+
+  DCHECK(inserted);
+  return it->second->GetIdentifier();
 }
 
 uint32_t FakePeripheral::GetBluetoothClass() const {
