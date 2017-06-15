0871cc2f044d1cced2137982f3ba5a5a054ca402
Justin DeWitt <dewittj@chromium.org>
2017-06-15 11:22:48 -0700

    [Offline Pages] Support network requests in prefetching.
    Note, this is disabled on trunk and hidden behind a feature.
    
    This adds basic support (not including saving work to disk) for
    network operations:
    * Every new suggestion will cause a fetch from OPS.
    * Every GCM message will cause a GetOperation call to OPS.
    
    Nothing is saved across restarts of Chrome, and nothing is
    downloaded, but logging to chrome://offline-internals is enabled.
    
    Bug:
    Change-Id: I4fb7605060c3dc07a5011867d71c9a40dd8c2d1f

--- chrome/browser/offline_pages/prefetch/prefetch_service_factory.cc
+++ chrome/browser/offline_pages/prefetch/prefetch_service_factory.cc
@@ -11,6 +11,9 @@
 #include "base/memory/singleton.h"
 #include "chrome/browser/offline_pages/prefetch/offline_metrics_collector_impl.h"
 #include "chrome/browser/offline_pages/prefetch/prefetch_instance_id_proxy.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/common/channel_info.h"
+#include "chrome/common/chrome_content_client.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/offline_pages/core/prefetch/prefetch_dispatcher_impl.h"
 #include "components/offline_pages/core/prefetch/prefetch_gcm_app_handler.h"
@@ -18,6 +21,7 @@
 #include "components/offline_pages/core/prefetch/prefetch_service_impl.h"
 #include "components/offline_pages/core/prefetch/suggested_articles_observer.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/browser_thread.h"
 
 namespace offline_pages {
 
@@ -39,7 +43,11 @@ PrefetchService* PrefetchServiceFactory::GetForBrowserContext(
 
 KeyedService* PrefetchServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
-  auto prefetch_dispatcher = base::MakeUnique<PrefetchDispatcherImpl>();
+  Profile* profile = Profile::FromBrowserContext(context);
+  DCHECK(profile);
+  auto prefetch_dispatcher = base::MakeUnique<PrefetchDispatcherImpl>(
+      PrefetchDispatcherImpl::NetworkParams(
+          profile->GetRequestContext(), chrome::GetChannel(), GetUserAgent()));
   auto prefetch_gcm_app_handler = base::MakeUnique<PrefetchGCMAppHandler>(
       base::MakeUnique<PrefetchInstanceIDProxy>(kPrefetchingOfflinePagesAppId,
                                                 context));
