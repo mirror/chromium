e1706af10ee023ffe8739cf4e1a6c6f0c02dfc46
Marijn Kruisselbrink <mek@chromium.org>
2017-06-15 14:38:33 -0700

    Try to recover from persistent localstorage commit errors by deleting the db.
    Certain errors can put leveldb in a mode where all future writes will
    automatically fail. If we detect such a situation (because too many consecutive
    commits failed) clear out the entire database and start fresh.
    
    Bug: 712399
    Change-Id: Ie38020a33ce383fd43513d1cca22afbe7180fecf

--- content/browser/dom_storage/local_storage_context_mojo.h
+++ content/browser/dom_storage/local_storage_context_mojo.h
@@ -56,6 +56,7 @@ class CONTENT_EXPORT LocalStorageContextMojo
   // Like DeleteStorage(), but also deletes storage for all sub-origins.
   void DeleteStorageForPhysicalOrigin(const url::Origin& origin);
   void Flush();
+  void FlushOriginForTesting(const url::Origin& origin);
 
   // Used by content settings to alter the behavior around
   // what data to keep and what data to discard at shutdown.
@@ -102,7 +103,7 @@ class CONTENT_EXPORT LocalStorageContextMojo
   void OnGotDatabaseVersion(leveldb::mojom::DatabaseError status,
                             const std::vector<uint8_t>& value);
   void OnConnectionFinished();
-  void DeleteAndRecreateDatabase();
+  void DeleteAndRecreateDatabase(const char* histogram_name);
   void OnDBDestroyed(bool recreate_in_memory,
                      leveldb::mojom::DatabaseError status);
 
@@ -126,6 +127,22 @@ class CONTENT_EXPORT LocalStorageContextMojo
   void OnGotStorageUsageForShutdown(std::vector<LocalStorageUsageInfo> usage);
   void OnShutdownComplete(leveldb::mojom::DatabaseError error);
 
+  void OnCommitResult(leveldb::mojom::DatabaseError error);
+  void OnReconnectedToDB();
+
+  // These values are written to logs.  New enum values can be added, but
+  // existing enums must never be renumbered or deleted and reused.
+  enum class OpenResult {
+    DIRECTORY_OPEN_FAILED = 0,
+    DATABASE_OPEN_FAILED = 1,
+    INVALID_VERSION = 2,
+    VERSION_READ_ERROR = 3,
+    SUCCESS = 4,
+    MAX
+  };
+
+  void LogDatabaseOpenResult(OpenResult result);
+
   std::unique_ptr<service_manager::Connector> connector_;
   const base::FilePath subdirectory_;
 
@@ -147,7 +164,7 @@ class CONTENT_EXPORT LocalStorageContextMojo
 
   leveldb::mojom::LevelDBServicePtr leveldb_service_;
   leveldb::mojom::LevelDBDatabaseAssociatedPtr database_;
-  bool tried_to_recreate_ = false;
+  bool tried_to_recreate_during_open_ = false;
 
   std::vector<base::OnceClosure> on_database_opened_callbacks_;
 
@@ -158,6 +175,14 @@ class CONTENT_EXPORT LocalStorageContextMojo
   // Used to access old data for migration.
   scoped_refptr<DOMStorageTaskRunner> task_runner_;
   base::FilePath old_localstorage_path_;
+
+  // Counts consecutive commit errors. If this number reaches a threshold, the
+  // whole database is thrown away.
+  int commit_error_count_ = 0;
+  bool tried_to_recover_from_commit_errors_ = false;
+
+  // Name of an extra histogram to log open results to, if not null.
+  const char* open_result_histogram_ = nullptr;
 
   base::WeakPtrFactory<LocalStorageContextMojo> weak_ptr_factory_;
 };
