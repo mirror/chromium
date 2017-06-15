328d4b79a779b93548cb6ef89cf6dc9da7c1adee
Naina Raisinghani <nainar@chromium.org>
2017-06-15 11:33:02 +1000

    Generate enum/getters/setters/mappings for resize
    Currently, the resize property is a 'storage_only' field, so
    it has no generated public getters/setters.
    
    This patch changes it to a 'keyword' field so that its getters/setters
    can be generated as well. We also generate the EResize enum and
    use the generated CSSValueID <-> EResize mappings.
    
    Diff: https://gist.github.com/nainar/de2cc565a04bae3e08b4ab5df1c981a7/revisions
    
    BUG=628043
    
    Change-Id: I939599a958396b2dc2e056afbe3d7bfe2207e4ff

--- third_party/WebKit/Source/core/style/ComputedStyleConstants.h
+++ third_party/WebKit/Source/core/style/ComputedStyleConstants.h
@@ -180,8 +180,6 @@ enum ObjectFit {
   kObjectFitScaleDown
 };
 
-enum class EResize { kNone, kBoth, kHorizontal, kVertical };
-
 enum QuoteType { OPEN_QUOTE, CLOSE_QUOTE, NO_OPEN_QUOTE, NO_CLOSE_QUOTE };
 
 enum EAnimPlayState { kAnimPlayStatePlaying, kAnimPlayStatePaused };
