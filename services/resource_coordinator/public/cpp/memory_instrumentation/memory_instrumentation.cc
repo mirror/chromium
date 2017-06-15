be4a86320929678e92afd45929b8fb316c0c56ac
Primiano Tucci <primiano@chromium.org>
2017-06-15 12:02:40 +0100

    Memory-infra: introduce helper class to interact with service
    This CL:
    1) Introduces a helper memory_instrumentation.h/.dc to
       interact with the memory-infra service.
       See design discussion on services-dev
       (https://goo.gl/ywieN0).
    2) Switches the tracing-related clients of memory-infra
       (devtools and slow reports) to use 1. 
    3) Remove the special shortcut that bypasses mojo when
       registering the memory-infra client for the browser
       process itself (crbug.com/733165).
    4) Remove a useless and hard to maintain test 
       client_process_impl_unittest.cc
    
    Upcoming CLs will get rid of the
    base::MemoryDumpManager::RequestGlobalDump() method and cleanup
    both the mojo interface and the ClientProcess library.
    
    Why this CL doesn't switch also chrome_metrics_emitter?
    Some small changes are coming next to the .mojom interface to
    have a clear split between the API to request a memory dump and
    add to the trace, and the API to request a memory dump and
    return back the results (e.g., for UMA). An API for the
    latter will be introduced in this new helper class after
    that refactoring has happened.
    
    Why removing the client_process_impl_unittest.cc?
    Right now that test covers a special test-only code-path
    of client_process_impl.cc against a mocked coordinator_impl.cc.
    The actual production code being tested is nearly zero.
    In addition, this class is more realistically covered by the
    process_memory_metrics_emitter_browsertest.cc.
    A more sensible end-to-end test to cover the memory-infra
    service will follow in upcoming CLs.
    
    BUG=733165
    
    Change-Id: I8b989e7fe17b6643ae5e2d733d4d6484b18ff507

---
+++ services/resource_coordinator/public/cpp/memory_instrumentation/memory_instrumentation.cc
@@ -0,0 +1,73 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/resource_coordinator/public/cpp/memory_instrumentation/memory_instrumentation.h"
+
+#include "base/threading/thread_local_storage.h"
+
+namespace memory_instrumentation {
+
+namespace {
+
+MemoryInstrumentation* g_instance = nullptr;
+base::ThreadLocalStorage::StaticSlot g_tls = TLS_INITIALIZER;
+
+}  // namespace
+
+// static
+void MemoryInstrumentation::CreateInstance(
+    service_manager::Connector* connector,
+    const std::string& service_name) {
+  DCHECK(!g_instance);
+  g_instance = new MemoryInstrumentation(connector, service_name);
+}
+
+// static
+MemoryInstrumentation* MemoryInstrumentation::GetInstance() {
+  return g_instance;
+}
+
+MemoryInstrumentation::MemoryInstrumentation(
+    service_manager::Connector* connector,
+    const std::string& service_name)
+    : connector_(connector), service_name_(service_name) {
+  g_tls.Initialize([](void* tls_object) {
+    delete reinterpret_cast<mojom::CoordinatorPtr*>(tls_object);
+  });
+}
+
+MemoryInstrumentation::~MemoryInstrumentation() {
+  g_instance = nullptr;
+}
+
+void MemoryInstrumentation::RequestGlobalDumpAndAppendToTrace(
+    MemoryDumpType dump_type,
+    MemoryDumpLevelOfDetail level_of_detail,
+    RequestGlobalDumpAndAppendToTraceCallback callback) {
+  const auto& coordinator = GetCoordinatorBindingForCurrentThread();
+  auto callback_adapter = [](RequestGlobalDumpAndAppendToTraceCallback callback,
+                             uint64_t dump_id, bool success,
+                             mojom::GlobalMemoryDumpPtr) {
+    if (callback)
+      callback.Run(success, dump_id);
+  };
+  base::trace_event::MemoryDumpRequestArgs args = {0, dump_type,
+                                                   level_of_detail};
+  coordinator->RequestGlobalMemoryDump(args,
+                                       base::Bind(callback_adapter, callback));
+}
+
+const mojom::CoordinatorPtr&
+MemoryInstrumentation::GetCoordinatorBindingForCurrentThread() {
+  auto* coordinator = reinterpret_cast<mojom::CoordinatorPtr*>(g_tls.Get());
+  if (!coordinator) {
+    coordinator = new mojom::CoordinatorPtr();
+    connector_->BindInterface(service_name_, mojo::MakeRequest(coordinator));
+    g_tls.Set(coordinator);
+  }
+  DCHECK(*coordinator);
+  return *coordinator;
+}
+
+}  // namespace memory_instrumentation
