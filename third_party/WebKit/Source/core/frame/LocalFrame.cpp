adc96ecb6f8e253f8fe593272699f64344771748
Joel Hockey <joelhockey@chromium.org>
2017-06-14 21:48:29 -0700

    Remove dead code WillDestroyScrollableArea from LocalFrame::Detach
    WillDestryScrollableArea was never being called here
    once view is nulled out, and it is not needed here in any
    case since it is called in LocalFrameView::Dispose which is
    always called in Document::Shutdown.
    
    Bug:
    Change-Id: I871dd58200a33c43bf1086210a8febf412aa2c32

--- third_party/WebKit/Source/core/frame/LocalFrame.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrame.cpp
@@ -427,6 +427,11 @@ void LocalFrame::Detach(FrameDetachType type) {
     performance_monitor_->Shutdown();
 
   PluginScriptForbiddenScope forbid_plugin_destructor_scripting;
+
+  // TODO: Page should take care of updating focus/scrolling instead of Frame.
+  if (page_->GetFocusController().FocusedFrame() == this)
+    page_->GetFocusController().SetFocusedFrame(nullptr);
+
   loader_.StopAllLoaders();
   // Don't allow any new child frames to load in this frame: attaching a new
   // child frame during or after detaching children results in an attached
@@ -481,10 +486,6 @@ void LocalFrame::Detach(FrameDetachType type) {
   // so page() could be null.
   if (GetPage() && GetPage()->GetFocusController().FocusedFrame() == this)
     GetPage()->GetFocusController().SetFocusedFrame(nullptr);
-
-  if (GetPage() && GetPage()->GetScrollingCoordinator() && view_)
-    GetPage()->GetScrollingCoordinator()->WillDestroyScrollableArea(
-        view_.Get());
 
   probe::frameDetachedFromParent(this);
   Frame::Detach(type);
