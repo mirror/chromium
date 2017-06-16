4dca465da94a41a8fe98139306dff43d7efafe10
Jessye Nana Davies <jessye@google.com>
2017-06-16 15:18:56 -0400

    Fixing spaces
    Removed file added by mistake
    
    
    fixed lines longer than 80 characters 2
    
    
    Fixed lines exceeding 72 characters
    
    
    fixed white spaces
    
    
    RenderWidgetHostViewCocoa gets the touch event (the Dino marquee is clickable)
    
    
    [Not for commit] Fixed whitespace for the working TouchBarView commit
    
    [Not for commit]Working touch version for custom TouchBarView; working with NSView with html pending.
    
    
    [Not for commit] Dino marquee displays in the touchbar when starting the Dino Game.
    
    Change-Id: I42fb1bc9f959f6823e64c43ea788f38f9e47decd
    
    [Not for commit] Dino marquee displays in the touchbar when starting the Dino Game.
    
    Bug:
    Change-Id: I6aca9de7330cf05b9d7f0f547ca3c442c2a69a8e

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -1768,7 +1768,12 @@ void RenderWidgetHostViewMac::OnDisplayMetricsChanged(
 - (id)initWithRenderWidgetHostViewMac:(RenderWidgetHostViewMac*)r {
   self = [super initWithFrame:NSZeroRect];
   if (self) {
-    self.acceptsTouchEvents = YES;
+    if ([self respondsToSelector:@selector(setAllowedTouchTypes:)]) {
+      self.allowedTouchTypes = NSTouchTypeMaskDirect | NSTouchTypeMaskIndirect;
+    } else {
+      self.acceptsTouchEvents = YES;
+    }
+
     editCommand_helper_.reset(new RenderWidgetHostViewMacEditCommandHelper);
     editCommand_helper_->AddEditingSelectorsToClass([self class]);
 
@@ -1829,6 +1834,11 @@ void RenderWidgetHostViewMac::OnDisplayMetricsChanged(
                            consumed:(BOOL)consumed {
   [responderDelegate_ rendererHandledGestureScrollEvent:event
                                                consumed:consumed];
+}
+
+- (void)touchesBeganWithEvent:(NSEvent*)event {
+  LOG(ERROR) << "Render widget host view mac touch";
+  [responderDelegate_ touchesBeganWithEvent:event];
 }
 
 - (BOOL)respondsToSelector:(SEL)selector {
@@ -2398,10 +2408,6 @@ void RenderWidgetHostViewMac::OnDisplayMetricsChanged(
 
 - (void)touchesMovedWithEvent:(NSEvent*)event {
   [responderDelegate_ touchesMovedWithEvent:event];
-}
-
-- (void)touchesBeganWithEvent:(NSEvent*)event {
-  [responderDelegate_ touchesBeganWithEvent:event];
 }
 
 - (void)touchesCancelledWithEvent:(NSEvent*)event {
