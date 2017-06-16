817fed26ed4206767855011187c9ae5f934cc39d
Naina Raisinghani <nainar@chromium.org>
2017-06-16 17:24:37 +1000

    Make Resize an enum class and change name to EResize.
    This patch makes Resize an enum class for type safety and changes the
    name to EResize to match the other enums
    
    BUG=684966
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I29fad6eac63e4129e15a129a51c288350b293558

--- third_party/WebKit/Source/core/style/ComputedStyleConstants.h
+++ third_party/WebKit/Source/core/style/ComputedStyleConstants.h
@@ -170,7 +170,7 @@ enum EFlexWrap { kFlexNoWrap, kFlexWrap, kFlexWrapReverse };
 enum EUserDrag { DRAG_AUTO, DRAG_NONE, DRAG_ELEMENT };
 
 // CSS3 Image Values
-enum EResize { RESIZE_NONE, RESIZE_BOTH, RESIZE_HORIZONTAL, RESIZE_VERTICAL };
+enum class EResize { kNone, kBoth, kHorizontal, kVertical };
 
 enum QuoteType { OPEN_QUOTE, CLOSE_QUOTE, NO_OPEN_QUOTE, NO_CLOSE_QUOTE };
 
