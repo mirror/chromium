0a80a25b3376373d2e9544193fbfe6d5902a318e
Jinsuk Kim <jinsukkim@chromium.org>
2017-06-16 11:22:11 +0200

    Remove public native interface ContentViewCore
    Bypass ContentViewCore for selection popup methods
    
    Some flows destined to SelectionPopupControllers are unnecessarily
    routed through ContentViewCore. This CL fixes it by passing CVC
    and routing through RenderWidgetHostView -> SelectionPopupController
    directly.
    
    This chang got pulled from a bigger refactoring
    https://crrev.com/2890173003 (still WIP).
    
    BUG=626830,626765
    
    Change-Id: I7348c9aea00cde93f976d1bae3779270826dce03

--- components/web_contents_delegate_android/android/java/src/org/chromium/components/web_contents_delegate_android/ValidationMessageBubble.java
+++ components/web_contents_delegate_android/android/java/src/org/chromium/components/web_contents_delegate_android/ValidationMessageBubble.java
@@ -16,58 +16,60 @@ import android.widget.TextView;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.annotations.CalledByNative;
-import org.chromium.content.browser.ContentViewCore;
-import org.chromium.content.browser.RenderCoordinates;
 
 /**
  * This class is an implementation of validation message bubble UI.
  */
 class ValidationMessageBubble {
+    private final View mView;
     private PopupWindow mPopup;
 
     /**
      * Creates a popup window to show the specified messages, and show it on the specified anchor
-     * rectangle.
+     * rectangle. All the values are in device pixel unit.
      *
      * If the anchor view is not in a state where a popup can be shown, this will return null.
      *
-     * @param contentViewCore The ContentViewCore object to provide various information.
-     * @param anchorX Anchor position in the CSS unit.
-     * @param anchorY Anchor position in the CSS unit.
-     * @param anchorWidth Anchor size in the CSS unit.
-     * @param anchorHeight Anchor size in the CSS unit.
+     * @param containerView A parent view for the message bubble.
+     * @param viewportWidth Viewport width.
+     * @param viewportHeight Viewport height.
+     * @param contentOffsetY Top content y offset.
+     * @param anchorX Anchor x position.
+     * @param anchorY Anchor y position.
+     * @param anchorWidth Anchor width.
+     * @param anchorHeight Anchor height.
      * @param mainText The main message. It will shown at the top of the popup window, and its font
      *                 size is larger.
      * @param subText The sub message. It will shown below the main message, and its font size is
      *                smaller.
      */
     @CalledByNative
-    private static ValidationMessageBubble createAndShowIfApplicable(
-            ContentViewCore contentViewCore, int anchorX, int anchorY, int anchorWidth,
-            int anchorHeight, String mainText, String subText) {
-        if (!canShowBubble(contentViewCore)) return null;
+    private static ValidationMessageBubble createAndShowIfApplicable(View containerView,
+            int viewportWidth, int viewportHeight, float contentOffsetY, int anchorX, int anchorY,
+            int anchorWidth, int anchorHeight, String mainText, String subText) {
+        if (!canShowBubble(containerView)) return null;
 
-        final RectF anchorPixInScreen = makePixRectInScreen(
-                contentViewCore, anchorX, anchorY, anchorWidth, anchorHeight);
-        return new ValidationMessageBubble(contentViewCore, anchorPixInScreen, mainText, subText);
+        RectF anchorPixInScreen = makeRectInScreen(
+                containerView, contentOffsetY, anchorX, anchorY, anchorWidth, anchorHeight);
+        return new ValidationMessageBubble(containerView, viewportWidth, viewportHeight,
+                contentOffsetY, anchorPixInScreen, mainText, subText);
     }
 
-    private static boolean canShowBubble(ContentViewCore contentViewCore) {
-        return contentViewCore.getContainerView() != null
-                && contentViewCore.getContainerView().getWindowToken() != null;
+    private static boolean canShowBubble(View containerView) {
+        return containerView != null && containerView.getWindowToken() != null;
     }
 
-    private ValidationMessageBubble(
-            ContentViewCore contentViewCore, RectF anchor, String mainText, String subText) {
-        final ViewGroup root = (ViewGroup) View.inflate(contentViewCore.getContext(),
-                R.layout.validation_message_bubble, null);
+    private ValidationMessageBubble(View containerView, int viewportWidth, int viewportHeight,
+            float contentOffsetY, RectF anchor, String mainText, String subText) {
+        mView = containerView;
+        ViewGroup root = (ViewGroup) View.inflate(
+                mView.getContext(), R.layout.validation_message_bubble, null);
         mPopup = new PopupWindow(root);
         updateTextViews(root, mainText, subText);
-        measure(contentViewCore.getRenderCoordinates());
-        Point origin = adjustWindowPosition(
-                contentViewCore, (int) (anchor.centerX() - getAnchorOffset()), (int) anchor.bottom);
-        mPopup.showAtLocation(
-                contentViewCore.getContainerView(), Gravity.NO_GRAVITY, origin.x, origin.y);
+        measure(viewportWidth, viewportHeight);
+        Point origin = adjustWindowPosition(viewportWidth, viewportHeight, contentOffsetY,
+                (int) (anchor.centerX() - getAnchorOffset()), (int) anchor.bottom);
+        mPopup.showAtLocation(mView, Gravity.NO_GRAVITY, origin.x, origin.y);
     }
 
     @CalledByNative
@@ -78,39 +80,38 @@ class ValidationMessageBubble {
     }
 
     /**
-     * Moves the popup window on the specified anchor rectangle.
+     * Moves the popup window on the specified anchor rectangle. All the values are in device
+     * pixel unit.
      *
-     * @param contentViewCore The ContentViewCore object to provide various information.
-     * @param anchorX Anchor position in the CSS unit.
-     * @param anchorY Anchor position in the CSS unit.
-     * @param anchorWidth Anchor size in the CSS unit.
-     * @param anchorHeight Anchor size in the CSS unit.
+     * @param viewportWidth Viewport width.
+     * @param viewportHeight Viewport height.
+     * @param contentOffsetY Content y offset from the top.
+     * @param anchorX Anchor x position.
+     * @param anchorY Anchor y position.
+     * @param anchorWidth Anchor width.
+     * @param anchorHeight Anchor height.
      */
     @CalledByNative
-    private void setPositionRelativeToAnchor(ContentViewCore contentViewCore,
-            int anchorX, int anchorY, int anchorWidth, int anchorHeight) {
-        RectF anchor = makePixRectInScreen(
-                contentViewCore, anchorX, anchorY, anchorWidth, anchorHeight);
-        Point origin = adjustWindowPosition(
-                contentViewCore, (int) (anchor.centerX() - getAnchorOffset()), (int) anchor.bottom);
+    private void setPositionRelativeToAnchor(int viewportWidth, int viewportHeight,
+            float contentOffsetY, int anchorX, int anchorY, int anchorWidth, int anchorHeight) {
+        RectF anchor = makeRectInScreen(
+                mView, contentOffsetY, anchorX, anchorY, anchorWidth, anchorHeight);
+        Point origin = adjustWindowPosition(viewportWidth, viewportHeight, contentOffsetY,
+                (int) (anchor.centerX() - getAnchorOffset()), (int) anchor.bottom);
         mPopup.update(origin.x, origin.y, mPopup.getWidth(), mPopup.getHeight());
     }
 
-    private static RectF makePixRectInScreen(ContentViewCore contentViewCore,
-            int anchorX, int anchorY, int anchorWidth, int anchorHeight) {
-        final RenderCoordinates coordinates = contentViewCore.getRenderCoordinates();
-        final float yOffset = getWebViewOffsetYPixInScreen(contentViewCore);
-        return new RectF(
-                coordinates.fromLocalCssToPix(anchorX),
-                coordinates.fromLocalCssToPix(anchorY) + yOffset,
-                coordinates.fromLocalCssToPix(anchorX + anchorWidth),
-                coordinates.fromLocalCssToPix(anchorY + anchorHeight) + yOffset);
+    private static RectF makeRectInScreen(View containerView, float contentOffsetYPix, int anchorX,
+            int anchorY, int anchorWidth, int anchorHeight) {
+        final float yOffset = getWebViewOffsetYPixInScreen(containerView, contentOffsetYPix);
+        return new RectF(anchorX, anchorY + yOffset, anchorX + anchorWidth,
+                anchorY + anchorHeight + yOffset);
     }
 
-    private static float getWebViewOffsetYPixInScreen(ContentViewCore contentViewCore) {
+    private static float getWebViewOffsetYPixInScreen(View containerView, float contentOffsetYPix) {
         int[] location = new int[2];
-        contentViewCore.getContainerView().getLocationOnScreen(location);
-        return location[1] + contentViewCore.getRenderCoordinates().getContentOffsetYPix();
+        containerView.getLocationOnScreen(location);
+        return location[1] + contentOffsetYPix;
     }
 
     private static void updateTextViews(ViewGroup root, String mainText, String subText) {
@@ -123,7 +124,7 @@ class ValidationMessageBubble {
         }
     }
 
-    private void measure(RenderCoordinates coordinates) {
+    private void measure(int viewportWidth, int viewportHeight) {
         mPopup.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
         mPopup.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
         mPopup.getContentView().setLayoutParams(
@@ -131,10 +132,8 @@ class ValidationMessageBubble {
                         RelativeLayout.LayoutParams.WRAP_CONTENT,
                         RelativeLayout.LayoutParams.WRAP_CONTENT));
         mPopup.getContentView().measure(
-                View.MeasureSpec.makeMeasureSpec(coordinates.getLastFrameViewportWidthPixInt(),
-                        View.MeasureSpec.AT_MOST),
-                View.MeasureSpec.makeMeasureSpec(coordinates.getLastFrameViewportHeightPixInt(),
-                        View.MeasureSpec.AT_MOST));
+                View.MeasureSpec.makeMeasureSpec(viewportWidth, View.MeasureSpec.AT_MOST),
+                View.MeasureSpec.makeMeasureSpec(viewportHeight, View.MeasureSpec.AT_MOST));
     }
 
     private float getAnchorOffset() {
@@ -148,11 +147,10 @@ class ValidationMessageBubble {
     /**
      * This adjusts the position if the popup protrudes the web view.
      */
-    private Point adjustWindowPosition(ContentViewCore contentViewCore, int x, int y) {
-        final RenderCoordinates coordinates = contentViewCore.getRenderCoordinates();
-        final int viewWidth = coordinates.getLastFrameViewportWidthPixInt();
-        final int viewBottom = (int) getWebViewOffsetYPixInScreen(contentViewCore)
-                + coordinates.getLastFrameViewportHeightPixInt();
+    private Point adjustWindowPosition(
+            int viewWidth, int viewHeight, float contentOffsetY, int x, int y) {
+        final int viewBottom =
+                (int) getWebViewOffsetYPixInScreen(mView, contentOffsetY) + viewHeight;
         final int width = mPopup.getContentView().getMeasuredWidth();
         final int height = mPopup.getContentView().getMeasuredHeight();
         if (x < 0) {
@@ -160,9 +158,7 @@ class ValidationMessageBubble {
         } else if (x + width > viewWidth) {
             x = viewWidth - width;
         }
-        if (y + height > viewBottom) {
-            y = viewBottom - height;
-        }
+        if (y + height > viewBottom) y = viewBottom - height;
         return new Point(x, y);
     }
 }
