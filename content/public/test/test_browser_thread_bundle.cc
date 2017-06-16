b4d88bc4fe393de28b8fb16a71322786168d91c1
Gabriel Charette <gab@chromium.org>
2017-06-16 17:30:23 -0400

    Always run after-startup-tasks in the scope of the unit test that posted them.
    Bug: 732018
    Change-Id: I318e82f3aa2969d6e338a33905ec7cd256d32d9c

--- content/public/test/test_browser_thread_bundle.cc
+++ content/public/test/test_browser_thread_bundle.cc
@@ -12,6 +12,7 @@
 #include "base/test/scoped_async_task_scheduler.h"
 #include "content/browser/browser_thread_impl.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/common/content_client.h"
 #include "content/public/test/test_browser_thread.h"
 #include "content/public/test/test_utils.h"
 
@@ -155,6 +156,10 @@ void TestBrowserThreadBundle::CreateThreads() {
   }
 
   threads_created_ = true;
+
+  // Consider startup complete such that after-startup-tasks always run in
+  // the scope of the test they were posted from (http://crbug.com/732018).
+  GetContentClient()->browser()->SetBrowserStartupIsCompleteForTesting();
 }
 
 }  // namespace content
