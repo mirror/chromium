9052c0bc3f5cffba494258d79aae57f9d3852d86
Jay Civelli <jcivelli@google.com>
2017-06-16 17:32:46 -0700

    Adding support for FDs registration.
    Bug: 702316
    Change-Id: I3d39d68440b3d16033bd6bd9ccbe6b51241325e1

--- content/app/android/child_process_service_impl.cc
+++ content/app/android/child_process_service_impl.cc
@@ -13,8 +13,6 @@
 #include "base/macros.h"
 #include "base/posix/global_descriptors.h"
 #include "jni/ChildProcessServiceImpl_jni.h"
-#include "services/service_manager/embedder/shared_file_util.h"
-#include "services/service_manager/embedder/switches.h"
 
 using base::android::JavaIntArrayToIntVector;
 using base::android::JavaParamRef;
@@ -23,10 +21,13 @@ namespace content {
 
 void RegisterFileDescriptors(JNIEnv* env,
                              const JavaParamRef<jclass>& clazz,
+                             const JavaParamRef<jobjectArray>& j_keys,
                              const JavaParamRef<jintArray>& j_ids,
                              const JavaParamRef<jintArray>& j_fds,
                              const JavaParamRef<jlongArray>& j_offsets,
                              const JavaParamRef<jlongArray>& j_sizes) {
+  std::vector<std::string> keys;
+  base::android::AppendJavaStringArrayToStringVector(env, j_keys, &keys);
   std::vector<int> ids;
   base::android::JavaIntArrayToIntVector(env, j_ids, &ids);
   std::vector<int> fds;
@@ -36,30 +37,19 @@ void RegisterFileDescriptors(JNIEnv* env,
   std::vector<int64_t> sizes;
   base::android::JavaLongArrayToInt64Vector(env, j_sizes, &sizes);
 
+  DCHECK_EQ(keys.size(), ids.size());
   DCHECK_EQ(ids.size(), fds.size());
   DCHECK_EQ(fds.size(), offsets.size());
   DCHECK_EQ(offsets.size(), sizes.size());
 
-  std::map<int, std::string> ids_to_keys;
-  std::string file_switch_value =
-      base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
-          service_manager::switches::kSharedFiles);
-  if (!file_switch_value.empty()) {
-    base::Optional<std::map<int, std::string>> ids_to_keys_from_command_line =
-        service_manager::ParseSharedFileSwitchValue(file_switch_value);
-    if (ids_to_keys_from_command_line) {
-      ids_to_keys = std::move(*ids_to_keys_from_command_line);
-    }
-  }
-
   for (size_t i = 0; i < ids.size(); i++) {
     base::MemoryMappedFile::Region region = {offsets.at(i), sizes.at(i)};
+    std::string key = keys.at(i);
     int id = ids.at(i);
     int fd = fds.at(i);
-    auto iter = ids_to_keys.find(id);
-    if (iter != ids_to_keys.end()) {
-      base::FileDescriptorStore::GetInstance().Set(iter->second,
-                                                   base::ScopedFD(fd), region);
+    if (!key.empty()) {
+      base::FileDescriptorStore::GetInstance().Set(key, base::ScopedFD(fd),
+                                                   region);
     } else {
       base::GlobalDescriptors::GetInstance()->Set(id, fd, region);
     }
