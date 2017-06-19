7dc08c19d3893194b3944007f8eefc9ff47eaa3a
Jay Civelli <jcivelli@google.com>
2017-06-19 12:30:13 -0700

    Moving ChildProcessService to base.
    Moving ChildProcessService and associated classes to base.
    
    BUG:702316
    Change-Id: If4f47ca958127cc316e42f06428af94bb42cd0aa

--- base/android/java/src/org/chromium/base/process_launcher/ChildProcessService.java
+++ base/android/java/src/org/chromium/base/process_launcher/ChildProcessService.java
@@ -1,8 +1,8 @@
-// Copyright 2012 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.content.app;
+package org.chromium.base.process_launcher;
 
 import android.app.Service;
 import android.content.Intent;
