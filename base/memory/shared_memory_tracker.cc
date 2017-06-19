a328e67ef3e2b29079a9c4c4b412ec028cf06e7c
Hajime Hoshi <hajimehoshi@chromium.org>
2017-06-19 16:45:35 +0900

    Bug fix: GUIDs for local base::SharedMemory dumps are wrong
    There are cyclic ownership edges when UserSharedMemoryBaseGUIDs()
    returns true. The cause is |local_shm_guid| is generated from a string
    'shared_memory/...' but this is same as global base::SharedMemory dumps'
    GUIDs. In the first place, |local_shm_guid| can't be generated from a
    string and instead this should be generated via MemoryAllocatorDump's
    constructor. This CL fixes SharedMemoryTracker::GetDumpGUIDForTracing
    to take PMD and changes the implementation.
    
    Bug: 604726
    Change-Id: Id6bdc95436e186c933abe846e7f2afbeca300a04

--- base/memory/shared_memory_tracker.cc
+++ base/memory/shared_memory_tracker.cc
@@ -27,15 +27,17 @@ SharedMemoryTracker* SharedMemoryTracker::GetInstance() {
 
 // static
 trace_event::MemoryAllocatorDumpGuid SharedMemoryTracker::GetDumpGUIDForTracing(
+    trace_event::ProcessMemoryDump* pmd,
     const UnguessableToken& id) {
   std::string dump_name = GetDumpNameForTracing(id);
-  return trace_event::MemoryAllocatorDumpGuid(dump_name);
+  return pmd->GetOrCreateAllocatorDump(dump_name)->guid();
 }
 
 // static
 trace_event::MemoryAllocatorDumpGuid
 SharedMemoryTracker::GetGlobalDumpGUIDForTracing(const UnguessableToken& id) {
-  return GetDumpGUIDForTracing(id);
+  std::string dump_name = GetDumpNameForTracing(id);
+  return trace_event::MemoryAllocatorDumpGuid(dump_name);
 }
 
 void SharedMemoryTracker::IncrementMemoryUsage(
@@ -75,16 +77,13 @@ bool SharedMemoryTracker::OnMemoryDump(const trace_event::MemoryDumpArgs& args,
     } else {
       dump_name = GetDumpNameForTracing(memory_guid);
     }
-    auto dump_guid = GetDumpGUIDForTracing(memory_guid);
-    // Discard duplicates that might be seen in single-process mode.
-    if (pmd->GetAllocatorDump(dump_name))
-      continue;
     trace_event::MemoryAllocatorDump* local_dump =
-        pmd->CreateAllocatorDump(dump_name);
+        pmd->GetOrCreateAllocatorDump(dump_name);
     // TODO(hajimehoshi): The size is not resident size but virtual size so far.
     // Fix this to record resident size.
     local_dump->AddScalar(trace_event::MemoryAllocatorDump::kNameSize,
                           trace_event::MemoryAllocatorDump::kUnitsBytes, size);
+    auto dump_guid = GetGlobalDumpGUIDForTracing(memory_guid);
     trace_event::MemoryAllocatorDump* global_dump =
         pmd->CreateSharedGlobalAllocatorDump(dump_guid);
     global_dump->AddScalar(trace_event::MemoryAllocatorDump::kNameSize,
