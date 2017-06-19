3ea667126c2c1d93c334b441a6c248d468662c16
Anton Vayvod <avayvod@google.com>
2017-06-19 13:48:49 -0400

    [MediaRouter,PresentationAPI] ScreenAvailability - part 3
    Remove OnScreenAvailabilityNotSupported in favor of
    ScreenAvailability::DISABLED.
    
    BUG=723032
    TEST=existing/updated tests.
    
    Change-Id: I824e832ad6454adfb4b221fd101b9eb4f65b46a3

--- content/browser/presentation/presentation_service_impl_unittest.cc
+++ content/browser/presentation/presentation_service_impl_unittest.cc
@@ -75,8 +75,10 @@ class MockPresentationServiceDelegate
       int render_process_id,
       int routing_id,
       PresentationScreenAvailabilityListener* listener) override {
-    if (!screen_availability_listening_supported_)
-      listener->OnScreenAvailabilityNotSupported();
+    if (!screen_availability_listening_supported_) {
+      listener->OnScreenAvailabilityChanged(
+          blink::mojom::ScreenAvailability::DISABLED);
+    }
 
     return AddScreenAvailabilityListener();
   }
@@ -212,7 +214,6 @@ class MockPresentationServiceClient
                void(const PresentationInfo& connection,
                     PresentationConnectionCloseReason reason,
                     const std::string& message));
-  MOCK_METHOD1(OnScreenAvailabilityNotSupported, void(const GURL& url));
   // PresentationConnectionMessage is move-only.
   void OnConnectionMessagesReceived(
       const PresentationInfo& presentation_info,
@@ -344,8 +345,9 @@ TEST_F(PresentationServiceImplTest, ListenForScreenAvailability) {
 
 TEST_F(PresentationServiceImplTest, ScreenAvailabilityNotSupported) {
   mock_delegate_.set_screen_availability_listening_supported(false);
-  EXPECT_CALL(mock_client_,
-              OnScreenAvailabilityNotSupported(presentation_url1_));
+  EXPECT_CALL(mock_client_, OnScreenAvailabilityUpdated(
+                                presentation_url1_,
+                                blink::mojom::ScreenAvailability::DISABLED));
   ListenForScreenAvailabilityAndWait(presentation_url1_, false);
   base::RunLoop().RunUntilIdle();
 }
