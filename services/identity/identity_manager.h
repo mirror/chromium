770b9ec208b255c3e6bf2393a91d78486a221bb0
Colin Blundell <blundell@chromium.org>
2017-06-19 15:01:57 +0200

    Add Observer interface to Identity Service
    This interface will eventually be a replacement for the observing of
    *all* signin-related classes that is currently done (OAuth2TokenService,
    AccountTracker, SigninManagerBase, ...). We start off simple with
    OnRefreshTokenAvailable(). We also convert one existing observer of
    OAuth2TokenService::OnRefreshTokenAvailable().
    
    Bug: 729540, 729966
    Change-Id: Id92de59ba9608820f7e06da734856a07614a9b64

--- services/identity/identity_manager.h
+++ services/identity/identity_manager.h
@@ -7,16 +7,19 @@
 
 #include "components/signin/core/browser/account_info.h"
 #include "components/signin/core/browser/profile_oauth2_token_service.h"
+#include "mojo/public/cpp/bindings/interface_ptr_set.h"
 #include "services/identity/public/cpp/account_state.h"
 #include "services/identity/public/cpp/scope_set.h"
 #include "services/identity/public/interfaces/identity_manager.mojom.h"
+#include "services/identity/public/interfaces/identity_observer.mojom.h"
 
 class AccountTrackerService;
 class SigninManagerBase;
 
 namespace identity {
 
-class IdentityManager : public mojom::IdentityManager {
+class IdentityManager : public mojom::IdentityManager,
+                        public OAuth2TokenService::Observer {
  public:
   static void Create(mojom::IdentityManagerRequest request,
                      AccountTrackerService* account_tracker,
@@ -72,6 +75,12 @@ class IdentityManager : public mojom::IdentityManager {
                       const ScopeSet& scopes,
                       const std::string& consumer_id,
                       GetAccessTokenCallback callback) override;
+  void AddObserver(mojom::IdentityObserverPtr observer) override;
+
+  void OnObserverError(mojom::IdentityObserver* observer);
+
+  // OAuth2TokenService::Observer:
+  void OnRefreshTokenAvailable(const std::string& account_id) override;
 
   // Deletes |request|.
   void AccessTokenRequestCompleted(AccessTokenRequest* request);
@@ -85,6 +94,9 @@ class IdentityManager : public mojom::IdentityManager {
 
   // The set of pending requests for access tokens.
   AccessTokenRequests access_token_requests_;
+
+  // The set of observers.
+  mojo::InterfacePtrSet<mojom::IdentityObserver> observers_;
 };
 
 }  // namespace identity
