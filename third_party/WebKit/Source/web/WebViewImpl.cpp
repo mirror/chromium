536b525f51be85f06e046b653908e493e9a22f75
Pavel Feldman <pfeldman@chromium.org>
2017-06-19 13:23:56 -0700

    Move PageOverlay and InspectorOverlayAgent from web/ to core/.
    Bug: 712963
    Change-Id: Id694978ae94a019f91a27a37b6b0f552169df728

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -91,6 +91,7 @@
 #include "core/page/FocusController.h"
 #include "core/page/FrameTree.h"
 #include "core/page/Page.h"
+#include "core/page/PageOverlay.h"
 #include "core/page/PagePopupClient.h"
 #include "core/page/PointerLockController.h"
 #include "core/page/ScopedPageSuspender.h"
@@ -170,7 +171,8 @@
 #include "public/web/WebSelection.h"
 #include "public/web/WebViewClient.h"
 #include "public/web/WebWindowFeatures.h"
-#include "web/PageOverlay.h"
+#include "web/PrerendererClientImpl.h"
+#include "web/StorageQuotaClientImpl.h"
 #include "web/WebDevToolsAgentImpl.h"
 
 #if USE(DEFAULT_RENDER_THEME)
