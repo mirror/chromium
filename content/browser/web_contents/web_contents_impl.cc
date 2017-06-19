67d35048e18d6bc712d23df6d0ca422605cc4063
Francois Doray <fdoray@chromium.org>
2017-06-19 14:48:06 -0400

    Add |needs_reload| argument to NavigationController::CopyStateFrom().
    This argument controls whether the WebContents needs to be reloaded
    when activated. Not reloading the WebContents as soon as it is
    activated will allow TabManager to implement more advanced reload
    rules (e.g. browser window is active, tab has been active for x ms).
    
    Note: Today, a WebContents is considered "active" if it is in a
    selected tab, even if the browser window is not visible.
    
    Bug: 731145
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I87a16df112752b97bcc369e34a1e1f86d18d9b99

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1607,7 +1607,7 @@ WebContents* WebContentsImpl::Clone() {
   create_params.initial_size = GetContainerBounds().size();
   WebContentsImpl* tc =
       CreateWithOpener(create_params, frame_tree_.root()->opener());
-  tc->GetController().CopyStateFrom(controller_);
+  tc->GetController().CopyStateFrom(controller_, true);
   for (auto& observer : observers_)
     observer.DidCloneToNewWebContents(this, tc);
   return tc;
