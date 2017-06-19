1b1f5af5b6cddbed7ab07dfb253052d11e67819b
Becca Hughes <beccahughes@chromium.org>
2017-06-19 11:15:21 +0100

    Add a MediaEngagementScore class to manage storing the score.
    Add a MediaEngagementScore class that manages calculating the total
    score as well as storing/reading the score for an origin from the
    content settings store. Register the setting to be synced across
    devices.
    
    BUG=731750
    
    Change-Id: I30e1ceb815c252d5f0395a8d10b905e7a6c9914b

---
+++ chrome/browser/media/media_engagement_score.cc
@@ -0,0 +1,122 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/media/media_engagement_score.h"
+
+#include "base/memory/ptr_util.h"
+#include "base/time/clock.h"
+#include "base/values.h"
+#include "chrome/browser/engagement/site_engagement_metrics.h"
+#include "components/content_settings/core/browser/host_content_settings_map.h"
+#include "components/content_settings/core/common/content_settings.h"
+#include "components/content_settings/core/common/content_settings_types.h"
+
+const char* MediaEngagementScore::kVisitsKey = "visits";
+const char* MediaEngagementScore::kMediaPlaybacksKey = "mediaPlaybacks";
+const char* MediaEngagementScore::kLastMediaPlaybackTimeKey =
+    "lastMediaPlaybackTime";
+
+const int MediaEngagementScore::kScoreMinVisits = 5;
+
+MediaEngagementScore::MediaEngagementScore(base::Clock* clock,
+                                           const GURL& origin,
+                                           HostContentSettingsMap* settings)
+    : MediaEngagementScore(
+          clock,
+          origin,
+          MediaEngagementScore::GetScoreDictForSettings(settings, origin)) {
+  settings_map_ = settings;
+}
+
+MediaEngagementScore::MediaEngagementScore(
+    base::Clock* clock,
+    const GURL& origin,
+    std::unique_ptr<base::DictionaryValue> score_dict)
+    : media_playbacks_(0),
+      visits_(0),
+      last_media_playback_time_(),
+      origin_(origin),
+      clock_(clock),
+      score_dict_(score_dict.release()) {
+  if (!score_dict_)
+    return;
+
+  score_dict_->GetInteger(kVisitsKey, &visits_);
+  score_dict_->GetInteger(kMediaPlaybacksKey, &media_playbacks_);
+
+  double internal_time;
+  if (score_dict_->GetDouble(kLastMediaPlaybackTimeKey, &internal_time))
+    last_media_playback_time_ = base::Time::FromInternalValue(internal_time);
+}
+
+MediaEngagementScore::MediaEngagementScore(MediaEngagementScore&& other) =
+    default;
+
+MediaEngagementScore::~MediaEngagementScore() = default;
+
+MediaEngagementScore& MediaEngagementScore::operator=(
+    MediaEngagementScore&& other) = default;
+
+double MediaEngagementScore::GetTotalScore() const {
+  if (visits_ < kScoreMinVisits)
+    return 0;
+  return media_playbacks_ / (double)visits_;
+}
+
+void MediaEngagementScore::Commit() {
+  DCHECK(settings_map_);
+  if (!UpdateScoreDict(score_dict_.get()))
+    return;
+
+  settings_map_->SetWebsiteSettingDefaultScope(
+      origin_, GURL(), CONTENT_SETTINGS_TYPE_MEDIA_ENGAGEMENT,
+      content_settings::ResourceIdentifier(), std::move(score_dict_));
+}
+
+bool MediaEngagementScore::UpdateScoreDict(base::DictionaryValue* score_dict) {
+  int stored_visits = 0;
+  int stored_media_playbacks = 0;
+  double stored_last_media_playback_internal = 0;
+
+  score_dict->GetInteger(kVisitsKey, &stored_visits);
+  score_dict->GetInteger(kMediaPlaybacksKey, &stored_media_playbacks);
+  score_dict->GetDouble(kLastMediaPlaybackTimeKey,
+                        &stored_last_media_playback_internal);
+
+  bool changed = stored_visits != visits() ||
+                 stored_media_playbacks != media_playbacks() ||
+                 stored_last_media_playback_internal !=
+                     last_media_playback_time_.ToInternalValue();
+  if (!changed)
+    return false;
+
+  score_dict->SetInteger(kVisitsKey, visits());
+  score_dict->SetInteger(kMediaPlaybacksKey, media_playbacks());
+  score_dict->SetDouble(kLastMediaPlaybackTimeKey,
+                        last_media_playback_time_.ToInternalValue());
+
+  return true;
+}
+
+std::unique_ptr<base::DictionaryValue>
+MediaEngagementScore::GetScoreDictForSettings(
+    const HostContentSettingsMap* settings,
+    const GURL& origin_url) {
+  if (!settings)
+    return base::MakeUnique<base::DictionaryValue>();
+
+  std::unique_ptr<base::DictionaryValue> value =
+      base::DictionaryValue::From(settings->GetWebsiteSetting(
+          origin_url, origin_url, CONTENT_SETTINGS_TYPE_MEDIA_ENGAGEMENT,
+          content_settings::ResourceIdentifier(), NULL));
+
+  if (value.get())
+    return value;
+  return base::MakeUnique<base::DictionaryValue>();
+}
+
+void MediaEngagementScore::IncrementMediaPlaybacks() {
+  media_playbacks_++;
+  last_media_playback_time_ = clock_->Now();
+}
