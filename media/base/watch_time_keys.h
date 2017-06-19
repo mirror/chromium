f9e7d67bb06226481ef1c3d389b6c375c0ffdf26
Mounir Lamouri <mlamouri@chromium.org>
2017-06-19 17:56:10 +0100

    Watch Time: report watch time based on display type.
    Different types are: inline, fullscreen and picture-in-picture.
    
    Bug: 727322
    Change-Id: I72bfa1a1f8f46079880f60cfd5b5d886875d06d6

--- media/base/watch_time_keys.h
+++ media/base/watch_time_keys.h
@@ -20,6 +20,9 @@ MEDIA_EXPORT extern const char kWatchTimeAudioEme[];
 MEDIA_EXPORT extern const char kWatchTimeAudioSrc[];
 MEDIA_EXPORT extern const char kWatchTimeAudioBattery[];
 MEDIA_EXPORT extern const char kWatchTimeAudioAc[];
+MEDIA_EXPORT extern const char kWatchTimeAudioDisplayFullscreen[];
+MEDIA_EXPORT extern const char kWatchTimeAudioDisplayInline[];
+MEDIA_EXPORT extern const char kWatchTimeAudioDisplayPictureInPicture[];
 MEDIA_EXPORT extern const char kWatchTimeAudioEmbeddedExperience[];
 MEDIA_EXPORT extern const char kWatchTimeAudioNativeControlsOn[];
 MEDIA_EXPORT extern const char kWatchTimeAudioNativeControlsOff[];
@@ -29,6 +32,9 @@ MEDIA_EXPORT extern const char kWatchTimeAudioVideoEme[];
 MEDIA_EXPORT extern const char kWatchTimeAudioVideoSrc[];
 MEDIA_EXPORT extern const char kWatchTimeAudioVideoBattery[];
 MEDIA_EXPORT extern const char kWatchTimeAudioVideoAc[];
+MEDIA_EXPORT extern const char kWatchTimeAudioVideoDisplayFullscreen[];
+MEDIA_EXPORT extern const char kWatchTimeAudioVideoDisplayInline[];
+MEDIA_EXPORT extern const char kWatchTimeAudioVideoDisplayPictureInPicture[];
 MEDIA_EXPORT extern const char kWatchTimeAudioVideoEmbeddedExperience[];
 MEDIA_EXPORT extern const char kWatchTimeAudioVideoNativeControlsOn[];
 MEDIA_EXPORT extern const char kWatchTimeAudioVideoNativeControlsOff[];
@@ -46,6 +52,7 @@ MEDIA_EXPORT extern const char
 MEDIA_EXPORT extern const char kWatchTimeFinalize[];
 MEDIA_EXPORT extern const char kWatchTimeFinalizePower[];
 MEDIA_EXPORT extern const char kWatchTimeFinalizeControls[];
+MEDIA_EXPORT extern const char kWatchTimeFinalizeDisplay[];
 
 // Count of the number of underflow events during a media session.
 MEDIA_EXPORT extern const char kWatchTimeUnderflowCount[];
@@ -69,6 +76,7 @@ MEDIA_EXPORT extern const char kRebuffersCountAudioVideoEme[];
 MEDIA_EXPORT base::flat_set<base::StringPiece> GetWatchTimeKeys();
 MEDIA_EXPORT base::flat_set<base::StringPiece> GetWatchTimePowerKeys();
 MEDIA_EXPORT base::flat_set<base::StringPiece> GetWatchTimeControlsKeys();
+MEDIA_EXPORT base::flat_set<base::StringPiece> GetWatchTimeDisplayKeys();
 
 }  // namespace media
 
