bc2688779c5aedd221e63c9dbbec137186aa3a06
Javier Fernandez <jfernandez@igalia.com>
2017-06-19 23:38:31 +0200

    [css-grid] Refactoring of flow-aware logic for grid item's margins
    We applied too much abstraction to the logic to retrieve, or to compute
    if required, the grid item's margin so it was flow-aware. However, we
    don't need to apply such abstraction when accesing data that depends
    only on the grid item's writing mode.
    
    We are already applying a flow-aware logic to determine whether we need
    to use item's logical width or height; for example, we do this during
    the track sizing algorithm when computed content-sized tracks.
    
    There is no specific bug associated to this CL, this is just a
    refactoring. Hence, no specific additional tests are required.
    
    Change-Id: I0a50d1224588e71f99699be6d7fdbe55952a1db6

--- third_party/WebKit/Source/core/layout/GridTrackSizingAlgorithm.cpp
+++ third_party/WebKit/Source/core/layout/GridTrackSizingAlgorithm.cpp
@@ -5,10 +5,13 @@
 #include "core/layout/GridTrackSizingAlgorithm.h"
 
 #include "core/layout/Grid.h"
+#include "core/layout/GridLayoutAndSizingHelperFunctions.h"
 #include "core/layout/LayoutGrid.h"
 #include "platform/LengthFunctions.h"
 
 namespace blink {
+
+using blink::GridLayoutAndSzingHelperFunctions;
 
 class GridSizingData;
 
@@ -125,31 +128,6 @@ class DefiniteSizeStrategy final : public GridTrackSizingAlgorithmStrategy {
     return false;
   }
 };
-
-// TODO(svillar): Repeated in LayoutGrid.
-LayoutUnit GridTrackSizingAlgorithmStrategy::ComputeMarginLogicalSizeForChild(
-    MarginDirection for_direction,
-    const LayoutGrid* grid,
-    const LayoutBox& child) {
-  if (!child.StyleRef().HasMargin())
-    return LayoutUnit();
-
-  bool is_row_axis = for_direction == kInlineDirection;
-  LayoutUnit margin_start;
-  LayoutUnit margin_end;
-  LayoutUnit logical_size =
-      is_row_axis ? child.LogicalWidth() : child.LogicalHeight();
-  Length margin_start_length = is_row_axis ? child.StyleRef().MarginStart()
-                                           : child.StyleRef().MarginBefore();
-  Length margin_end_length = is_row_axis ? child.StyleRef().MarginEnd()
-                                         : child.StyleRef().MarginAfter();
-  child.ComputeMarginsForDirection(
-      for_direction, grid, child.ContainingBlockLogicalWidthForContent(),
-      logical_size, margin_start, margin_end, margin_start_length,
-      margin_end_length);
-
-  return margin_start + margin_end;
-}
 
 bool GridTrackSizingAlgorithmStrategy::
     HasOverrideContainingBlockContentSizeForChild(
@@ -307,11 +285,9 @@ LayoutUnit GridTrackSizingAlgorithmStrategy::MinContentForChild(
     // FIXME: It's unclear if we should return the intrinsic width or the
     // preferred width.
     // See http://lists.w3.org/Archives/Public/www-style/2013Jan/0245.html
-    LayoutUnit margin_logical_width =
-        child.NeedsLayout() ? ComputeMarginLogicalSizeForChild(
-                                  kInlineDirection, GetLayoutGrid(), child)
-                            : child.MarginLogicalWidth();
-    return child.MinPreferredLogicalWidth() + margin_logical_width;
+    return child.MinPreferredLogicalWidth() +
+           GridLayoutAndSzingHelperFunctions::MarginLogicalWidthForChild(
+               *GetLayoutGrid(), child);
   }
 
   if (Direction() == kForColumns && !AvailableSpace()) {
@@ -344,11 +320,9 @@ LayoutUnit GridTrackSizingAlgorithmStrategy::MaxContentForChild(
     // FIXME: It's unclear if we should return the intrinsic width or the
     // preferred width.
     // See http://lists.w3.org/Archives/Public/www-style/2013Jan/0245.html
-    LayoutUnit margin_logical_width =
-        child.NeedsLayout() ? ComputeMarginLogicalSizeForChild(
-                                  kInlineDirection, GetLayoutGrid(), child)
-                            : child.MarginLogicalWidth();
-    return child.MaxPreferredLogicalWidth() + margin_logical_width;
+    return child.MaxPreferredLogicalWidth() +
+           GridLayoutAndSzingHelperFunctions::MarginLogicalWidthForChild(
+               *GetLayoutGrid(), child);
   }
 
   if (UpdateOverrideContainingBlockContentSizeForChild(child,
@@ -434,14 +408,13 @@ LayoutUnit DefiniteSizeStrategy::MinLogicalWidthForChild(
     LayoutBox& child,
     Length child_min_size,
     GridTrackSizingDirection child_inline_direction) const {
-  LayoutUnit margin_logical_width = ComputeMarginLogicalSizeForChild(
-      kInlineDirection, GetLayoutGrid(), child);
   return child.ComputeLogicalWidthUsing(
              kMinSize, child_min_size,
              OverrideContainingBlockContentSizeForChild(child,
                                                         child_inline_direction),
              GetLayoutGrid()) +
-         margin_logical_width;
+         GridLayoutAndSzingHelperFunctions::MarginLogicalWidthForChild(
+             *GetLayoutGrid(), child);
 }
 
 void DefiniteSizeStrategy::LayoutGridItemForMinSizeComputation(
