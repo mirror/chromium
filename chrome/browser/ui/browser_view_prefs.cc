2f5c131f528fd3c6da447ce4c0cfebbdb888d0e5
Tim Brown <timbrown@chromium.org>
2017-06-20 15:12:08 -0700

    Test for feature (e.g. USE_X11) not Linux
    As well as being more technically correct, this makes the jobs of
    porters to other OSes easier. It should be a NOP for all supported
    platforms.
    
    Bug: 290185
    Change-Id: Ib0528cfe431f652173768a6916b31ee51199e84f

--- chrome/browser/ui/browser_view_prefs.cc
+++ chrome/browser/ui/browser_view_prefs.cc
@@ -10,8 +10,8 @@
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 
-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-#include "ui/base/x/x11_util.h"  // nogncheck
+#if defined(USE_X11) && !defined(OS_CHROMEOS)
+#include "ui/base/x/x11_util.h"
 #endif
 
 namespace {
@@ -32,7 +32,7 @@ void RegisterBrowserViewLocalPrefs(PrefRegistrySimple* registry) {
 
 void RegisterBrowserViewProfilePrefs(
     user_prefs::PrefRegistrySyncable* registry) {
-#if !defined(OS_CHROMEOS) && defined(USE_X11)
+#if defined(USE_X11) && !defined(OS_CHROMEOS)
   registry->RegisterBooleanPref(prefs::kUseCustomChromeFrame,
                                 ui::GetCustomFramePrefDefault());
 #endif
