ca6b001fb1116c853b275046b45d5904da2ac22d
Jeremy Roman <jbroman@chromium.org>
2017-06-20 17:16:07 -0400

    Use std::atomic in WTF::ThreadSafeRefCounted.
    This is the standard idiom for atomic reference counting, and eliminates
    some unnecessary memory barriers (notably on non-x86).
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I3e3d1725e81d4f809df2b1cdf2c3def7847daac1

--- base/memory/ref_counted.cc
+++ base/memory/ref_counted.cc
@@ -10,7 +10,7 @@ namespace base {
 namespace {
 
 #if DCHECK_IS_ON()
-AtomicRefCount g_cross_thread_ref_count_access_allow_count = 0;
+AtomicRefCount g_cross_thread_ref_count_access_allow_count(0);
 #endif
 
 }  // namespace
@@ -23,14 +23,14 @@ bool RefCountedThreadSafeBase::HasOneRef() const {
 
 RefCountedThreadSafeBase::~RefCountedThreadSafeBase() {
 #if DCHECK_IS_ON()
-  DCHECK(in_dtor_) << "RefCountedThreadSafe object deleted without "
-                      "calling Release()";
+  DCHECK(in_dtor_.load(std::memory_order_acquire))
+      << "RefCountedThreadSafe object deleted without calling Release()";
 #endif
 }
 
 void RefCountedThreadSafeBase::AddRef() const {
 #if DCHECK_IS_ON()
-  DCHECK(!in_dtor_);
+  DCHECK(!in_dtor_.load(std::memory_order_acquire));
   DCHECK(!needs_adopt_ref_)
       << "This RefCounted object is created with non-zero reference count."
       << " The first reference to such a object has to be made by AdoptRef or"
@@ -41,12 +41,12 @@ void RefCountedThreadSafeBase::AddRef() const {
 
 bool RefCountedThreadSafeBase::Release() const {
 #if DCHECK_IS_ON()
-  DCHECK(!in_dtor_);
+  DCHECK(!in_dtor_.load(std::memory_order_acquire));
   DCHECK(!AtomicRefCountIsZero(&ref_count_));
 #endif
   if (!AtomicRefCountDec(&ref_count_)) {
 #if DCHECK_IS_ON()
-    in_dtor_ = true;
+    in_dtor_.store(true, std::memory_order_release);
 #endif
     return true;
   }
