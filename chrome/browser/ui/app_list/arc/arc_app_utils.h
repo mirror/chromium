6c3e59bd1bca7a35efc0468f642ea2cf91326092
khmel <khmel@google.com>
2017-06-20 09:05:48 -0700

    arc: Protect UMA arc.PlayStoreShown.TimeDelta
    With new launch mode for assistance we delay starting Play Store until
    assistance is done. This may lead to incorrect Play Store shown time
    counting. We need to protect UMA stats only to count Play Store started
    by default.
    
    TEST: Extended unit tests
    TEST: Manually on device. CrossOver works as expected. In case Play
          Store started by default, chrome://histogramms is updated
          correctly. Disable starting Play Store automatically and start
          Play Store manually after opt in. chrome://histogramms was
          not updated.
    
    Bug: b/62800156
    Change-Id: I3b2687727973f3dffa9e5395cc3bd8fa77f990d2

--- chrome/browser/ui/app_list/arc/arc_app_utils.h
+++ chrome/browser/ui/app_list/arc/arc_app_utils.h
@@ -6,6 +6,7 @@
 #define CHROME_BROWSER_UI_APP_LIST_ARC_ARC_APP_UTILS_H_
 
 #include <string>
+#include <vector>
 
 #include "base/callback.h"
 #include "components/arc/common/app.mojom.h"
@@ -27,8 +28,22 @@ extern const char kLegacyPlayStoreAppId[];
 extern const char kPlayStorePackage[];
 extern const char kPlayStoreActivity[];
 extern const char kSettingsAppId[];
+extern const char kInitialStartParam[];
 
 using CanHandleResolutionCallback = base::Callback<void(bool)>;
+
+// Represents unparsed intent.
+struct Intent {
+  Intent();
+  ~Intent();
+
+  std::string action;                     // Extracted from action.
+  std::string category;                   // Extracted from category.
+  std::string package_name;               // Extracted from component.
+  std::string activity;                   // Extracted from component.
+  int launch_flags = 0;                   // Extracted from launchFlags;
+  std::vector<std::string> extra_params;  // Other parameters not listed above.
+};
 
 // Checks if a given app should be hidden in launcher.
 bool ShouldShowInLauncher(const std::string& app_id);
@@ -45,20 +60,23 @@ bool LaunchAndroidSettingsApp(content::BrowserContext* context,
 // Launch Play Store app.
 bool LaunchPlayStoreWithUrl(const std::string& url);
 
-// Launch an app with given layout and let the system decides how big and where
-// to place it.
-// TODO (khmel) replace bool for |landscape_layout| with enum class in order
-// to prevent using another LaunchApp with different signature mistakenly.
+// Launches an app with given layout and lets the system decides how big and
+// where to place it.
 bool LaunchApp(content::BrowserContext* context,
                const std::string& app_id,
                bool landscape_layout,
                int event_flags);
 
-// Launch an app and place it at the specified coordinates.
-bool LaunchAppWithRect(content::BrowserContext* context,
-                       const std::string& app_id,
-                       const gfx::Rect& target_rect,
-                       int event_flags);
+// Launches an app with given layout and lets the system decides how big and
+// where to place it. |launch_intent| may be optionally provided to customize
+// an app launch.
+// TODO(khmel): replace bool for |landscape_layout| with enum class in order
+// to prevent using another LaunchApp with different signature mistakenly.
+bool LaunchAppWithIntent(content::BrowserContext* context,
+                         const std::string& app_id,
+                         const std::string& launch_intent,
+                         bool landscape_layout,
+                         int event_flags);
 
 // Sets task active.
 void SetTaskActive(int task_id);
@@ -97,6 +115,17 @@ bool ShowPackageInfoOnPage(const std::string& package_name,
 
 // Returns true if |id| represents either ARC app or ARC shelf group.
 bool IsArcItem(content::BrowserContext* context, const std::string& id);
+
+// Returns intent that can be used to launch an activity specified by
+// |package_name| and |activity|. |extra_params| is the list of optional
+// parameters encoded to intent.
+std::string GetLaunchIntent(const std::string& package_name,
+                            const std::string& activity,
+                            const std::vector<std::string>& extra_params);
+
+// Parses provided |intent_as_string|. Returns false if |intent_as_string|
+// cannot be parsed.
+bool ParseIntent(const std::string& intent_as_string, Intent* intent);
 
 }  // namespace arc
 
