66a0246de188bf659c20a615c3caaf2a93eca8fe
Kenneth Russell <kbr@chromium.org>
2017-06-19 19:48:53 -0700

    Check for already-deleted samplers during state restoration.
    This probably isn't the cause of the crashes seen, because the
    service-side sampler object should still be valid; it's only actually
    deleted in Sampler::~Sampler.
    
    Add DCHECKs to catch out-of-range references on the bots, in case
    there's a different bug and tests would catch it.
    
    BUG=733096
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ic073fb288ff4084614142ff802e9a93457241c6a

--- gpu/command_buffer/service/context_state.cc
+++ gpu/command_buffer/service/context_state.cc
@@ -285,13 +285,20 @@ void ContextState::RestoreSamplerBinding(GLuint unit,
                                          const ContextState* prev_state) const {
   if (!feature_info_->IsES3Capable())
     return;
+  DCHECK_LT(unit, sampler_units.size());
   const scoped_refptr<Sampler>& cur_sampler = sampler_units[unit];
-  GLuint cur_id = cur_sampler ? cur_sampler->service_id() : 0;
+  GLuint cur_id = 0;
+  if (cur_sampler && !cur_sampler->IsDeleted()) {
+    cur_id = cur_sampler->service_id();
+  }
   GLuint prev_id = 0;
   if (prev_state) {
+    DCHECK_LT(unit, prev_state->sampler_units.size());
     const scoped_refptr<Sampler>& prev_sampler =
         prev_state->sampler_units[unit];
-    prev_id = prev_sampler ? prev_sampler->service_id() : 0;
+    if (prev_sampler && !prev_sampler->IsDeleted()) {
+      prev_id = prev_sampler->service_id();
+    }
   }
   if (!prev_state || cur_id != prev_id) {
     glBindSampler(unit, cur_id);
