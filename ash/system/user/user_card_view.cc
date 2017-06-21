235a8871bec59fd19da12cce547d6ae2ef0a226a
Xiyuan Xia <xiyuan@chromium.org>
2017-06-21 13:58:50 -0700

    ash: Create UserCardView from user session info
    Use user type from user session info instead of LoginStatus when
    creating UserCardView. The user session info contains the true data
    of the user. LoginStatus is deprecated and also used to temporarily
    lock down system tray when there is system modal dialog.
    
    BUG=734011
    
    Change-Id: I295ffaa43fa8a91f0329031758c155a97468e473

--- ash/system/user/user_card_view.cc
+++ ash/system/user/user_card_view.cc
@@ -57,17 +57,18 @@ const int kUserDetailsVerticalPadding = 5;
 // and end of the user's display name in the public account user card's text.
 const base::char16 kDisplayNameMark[] = {0x2060, 0};
 
-views::View* CreateUserAvatarView(LoginStatus login_status, int user_index) {
+views::View* CreateUserAvatarView(int user_index) {
+  const mojom::UserSession* const user_session =
+      Shell::Get()->session_controller()->GetUserSession(user_index);
+
   RoundedImageView* image_view = new RoundedImageView(kTrayItemSize / 2);
-  if (login_status == LoginStatus::GUEST) {
+  if (user_session->user_info->type == user_manager::USER_TYPE_GUEST) {
     gfx::ImageSkia icon =
         gfx::CreateVectorIcon(kSystemMenuGuestIcon, kMenuIconColor);
     image_view->SetImage(icon, icon.size());
   } else {
-    SessionController* controller = Shell::Get()->session_controller();
-    image_view->SetImage(
-        controller->GetUserSession(user_index)->user_info->avatar,
-        gfx::Size(kTrayItemSize, kTrayItemSize));
+    image_view->SetImage(user_session->user_info->avatar,
+                         gfx::Size(kTrayItemSize, kTrayItemSize));
   }
 
   image_view->SetBorder(views::CreateEmptyBorder(gfx::Insets(
@@ -296,7 +297,9 @@ UserCardView::UserCardView(LoginStatus login_status, int user_index)
 
   Shell::Get()->media_controller()->AddObserver(this);
 
-  if (login_status == LoginStatus::PUBLIC)
+  const mojom::UserSession* const user_session =
+      Shell::Get()->session_controller()->GetUserSession(user_index_);
+  if (user_session->user_info->type == user_manager::USER_TYPE_PUBLIC_ACCOUNT)
     AddPublicModeUserContent();
   else
     AddUserContent(layout, login_status);
@@ -375,20 +378,21 @@ void UserCardView::OnMediaCaptureChanged(
 }
 
 void UserCardView::AddPublicModeUserContent() {
-  views::View* avatar = CreateUserAvatarView(LoginStatus::PUBLIC, 0);
+  views::View* avatar = CreateUserAvatarView(0);
   AddChildView(avatar);
   AddChildView(new PublicAccountUserDetails());
 }
 
 void UserCardView::AddUserContent(views::BoxLayout* layout,
                                   LoginStatus login_status) {
-  AddChildView(CreateUserAvatarView(login_status, user_index_));
-  SessionController* controller = Shell::Get()->session_controller();
-  base::string16 user_name_string =
-      login_status == LoginStatus::GUEST
-          ? l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_GUEST_LABEL)
-          : base::UTF8ToUTF16(controller->GetUserSession(user_index_)
-                                  ->user_info->display_name);
+  AddChildView(CreateUserAvatarView(user_index_));
+  const mojom::UserSession* const user_session =
+      Shell::Get()->session_controller()->GetUserSession(user_index_);
+  const bool is_guest =
+      user_session->user_info->type == user_manager::USER_TYPE_GUEST;
+  const base::string16 user_name_string =
+      is_guest ? l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_GUEST_LABEL)
+               : base::UTF8ToUTF16(user_session->user_info->display_name);
   user_name_ = new views::Label(user_name_string);
   user_name_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
   TrayPopupItemStyle user_name_style(
@@ -399,12 +403,11 @@ void UserCardView::AddUserContent(views::BoxLayout* layout,
   user_email_style.set_color_style(TrayPopupItemStyle::ColorStyle::INACTIVE);
   auto* user_email = new views::Label();
   base::string16 user_email_string;
-  if (login_status != LoginStatus::GUEST) {
+  if (!is_guest) {
     user_email_string =
         Shell::Get()->session_controller()->IsUserSupervised()
             ? l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_SUPERVISED_LABEL)
-            : base::UTF8ToUTF16(controller->GetUserSession(user_index_)
-                                    ->user_info->display_email);
+            : base::UTF8ToUTF16(user_session->user_info->display_email);
   }
   user_email->SetText(user_email_string);
   user_email->SetHorizontalAlignment(gfx::ALIGN_LEFT);
