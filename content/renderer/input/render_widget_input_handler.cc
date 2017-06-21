5c9c1bef324bc9d74f607c35a196ee98fd3518f7
Hayley Ferr <hayleyferr@chromium.org>
2017-06-21 15:16:51 -0400

    Bundling TouchAction bits with InputEventAcks
    The optimization would allow for one less IPC upon touch actions for
    blink. Instead of sending both a TouchAction message and an
    InputEventAck, the TouchAction bit will be bundled into the
    InputEventAck when appropriate. |handling_event_overscroll_| acted as a
    guide in the first step of the development process.
    
    Bug: 735655
    Change-Id: I8ef36e0276a29fc8c65c9d99faed31d892a23d1d

--- content/renderer/input/render_widget_input_handler.cc
+++ content/renderer/input/render_widget_input_handler.cc
@@ -197,6 +197,7 @@ RenderWidgetInputHandler::RenderWidgetInputHandler(
       widget_(widget),
       handling_input_event_(false),
       handling_event_overscroll_(nullptr),
+      handling_touch_action_(nullptr),
       handling_event_type_(WebInputEvent::kUndefined),
       context_menu_source_type_(ui::MENU_SOURCE_MOUSE),
       suppress_next_char_events_(false) {
@@ -216,6 +217,12 @@ void RenderWidgetInputHandler::HandleInputEvent(
                                                       true);
   base::AutoReset<WebInputEvent::Type> handling_event_type_resetter(
       &handling_event_type_, input_event.GetType());
+
+  // Calls into |SetTouchAction()| while handling this event will populate
+  // |touch_action|, which in turn will be bundled with the event ack.
+  std::unique_ptr<cc::TouchAction> touch_action;
+  base::AutoReset<std::unique_ptr<cc::TouchAction>*>
+      handling_touch_action_resetter(&handling_touch_action_, &touch_action);
 
   // Calls into |didOverscroll()| while handling this event will populate
   // |event_overscroll|, which in turn will be bundled with the event ack.
@@ -394,9 +401,11 @@ void RenderWidgetInputHandler::HandleInputEvent(
   TRACE_EVENT_SYNTHETIC_DELAY_END("blink.HandleInputEvent");
   if (callback) {
     std::move(callback).Run(ack_result, swap_latency_info,
-                            std::move(event_overscroll));
+                            std::move(event_overscroll),
+                            std::move(touch_action));
   } else {
     DCHECK(!event_overscroll) << "Unexpected overscroll for un-acked event";
+    DCHECK(!touch_action) << "Unexpected touch action for un-acked event";
   }
 
 #if defined(OS_ANDROID)
@@ -454,6 +463,16 @@ void RenderWidgetInputHandler::DidOverscrollFromBlink(
   }
 
   delegate_->OnDidOverscroll(*params);
+}
+
+void RenderWidgetInputHandler::TouchActionFromBlink(
+    cc::TouchAction touchAction) {
+  if (handling_touch_action_) {
+    *handling_touch_action_ = base::MakeUnique<cc::TouchAction>(touchAction);
+    return;
+  }
+
+  delegate_->OnTouchAction(touchAction);
 }
 
 }  // namespace content
