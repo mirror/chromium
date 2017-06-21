cb695aae1cd55af680374196c20a20a53ad93e81
Rouslan Solomakhin <rouslan@chromium.org>
2017-06-21 15:12:19 -0400

    Parse "supported_origins" in web payment manifest.
    If present, a valid "supported_origins" field in web payment manifest
    can be either the string "*" or a list of origins.
    
    Spec:
    https://w3c.github.io/payment-method-manifest/
    
    Design doc:
    https://docs.google.com/document/d/1izV4uC-tiRJG3JLooqY3YRLU22tYOsLTNq0P_InPJeE
    
    Bug: 735184
    Change-Id: I6e7a64ebeb2b9cd6a6f0a44077874e6417b229bd

---
+++ chrome/browser/payments/payment_manifest_parser_host_browsertest.cc
@@ -0,0 +1,82 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/payments/content/payment_manifest_parser_host.h"
+
+#include "base/memory/ptr_util.h"
+#include "base/run_loop.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace payments {
+namespace {
+
+class PaymentManifestParserHostTest : public InProcessBrowserTest {
+ public:
+  PaymentManifestParserHostTest() : all_origins_supported_(false) {}
+
+  ~PaymentManifestParserHostTest() override {}
+
+  void StartUtilityProcess() { host_.StartUtilityProcess(); }
+
+  void ParsePaymentMethodManifest(const std::string& content) {
+    base::RunLoop run_loop;
+    host_.ParsePaymentMethodManifest(
+        content,
+        base::BindOnce(
+            &PaymentManifestParserHostTest::OnPaymentMethodManifestParsed,
+            base::Unretained(this), run_loop.QuitClosure()));
+    run_loop.Run();
+  }
+
+  const std::vector<GURL>& web_app_manifest_urls() const {
+    return web_app_manifest_urls_;
+  }
+
+  const std::vector<url::Origin>& supported_origins() const {
+    return supported_origins_;
+  }
+
+  bool all_origins_supported() const { return all_origins_supported_; }
+
+ private:
+  void OnPaymentMethodManifestParsed(
+      const base::Closure& resume_test,
+      const std::vector<GURL>& web_app_manifest_urls,
+      const std::vector<url::Origin>& supported_origins,
+      bool all_origins_supported) {
+    web_app_manifest_urls_ = web_app_manifest_urls;
+    supported_origins_ = supported_origins;
+    all_origins_supported_ = all_origins_supported;
+    resume_test.Run();
+  }
+
+  PaymentManifestParserHost host_;
+  std::vector<GURL> web_app_manifest_urls_;
+  std::vector<url::Origin> supported_origins_;
+  bool all_origins_supported_;
+
+  DISALLOW_COPY_AND_ASSIGN(PaymentManifestParserHostTest);
+};
+
+IN_PROC_BROWSER_TEST_F(PaymentManifestParserHostTest, NotStarted) {
+  ParsePaymentMethodManifest("{\"supported_origins\": \"*\"}");
+
+  EXPECT_TRUE(web_app_manifest_urls().empty());
+  EXPECT_TRUE(supported_origins().empty());
+  EXPECT_FALSE(all_origins_supported());
+}
+
+IN_PROC_BROWSER_TEST_F(PaymentManifestParserHostTest, AllOriginsSupported) {
+  StartUtilityProcess();
+
+  ParsePaymentMethodManifest("{\"supported_origins\": \"*\"}");
+
+  EXPECT_TRUE(web_app_manifest_urls().empty());
+  EXPECT_TRUE(supported_origins().empty());
+  EXPECT_TRUE(all_origins_supported());
+}
+
+}  // namespace
+}  // namespace payments
