50688229aa75ba290ec624fc27105dc91283941f
Yutaka Hirano <yhirano@chromium.org>
2017-06-21 23:37:49 +0900

    Do not dispatch an opaque response for a mode: "cors" request
    When a service worker is involved, it's possible to get an opaque
    filtered response for a mode: "cors" request. We peviously
    checked it in ResourceFetcher but it's insufficient when the resource is
    shared before the response arrives.
    
    This CL instead make a CORS error when we see such response in
    DocumentThreadableLoader.
    
    Bug: 731669, 625575
    Change-Id: I65334dbe21c0e2e8aaedd6d5dd5fae762c7cb72c

--- third_party/WebKit/Source/platform/loader/fetch/ResourceFetcher.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/ResourceFetcher.cpp
@@ -975,15 +975,6 @@ bool ResourceFetcher::IsReusableAlsoForPreloading(const FetchParameters& params,
   if (request.DownloadToFile() || request.UseStreamOnResponse())
     return false;
 
-  // Never reuse opaque responses from a service worker for requests that are
-  // not no-cors. https://crbug.com/625575
-  if (existing_resource->GetResponse().WasFetchedViaServiceWorker() &&
-      existing_resource->GetResponse().ServiceWorkerResponseType() ==
-          kWebServiceWorkerResponseTypeOpaque &&
-      request.GetFetchRequestMode() != WebURLRequest::kFetchRequestModeNoCORS) {
-    return false;
-  }
-
   if (!is_static_data && !existing_resource->CanReuse(params))
     return false;
 
