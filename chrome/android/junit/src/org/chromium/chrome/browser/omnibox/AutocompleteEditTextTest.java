4127b39d8b2a635934f2afdc59387e653480f802
Changwan Ryu <changwan@chromium.org>
2017-06-22 12:35:20 -0700

    Add spannable model for omnibox inline autocomplete
    The current mechanism for inline autocomplete is based on selection
    highlight, and it does not work with OEM keyboards, nor does it work with
    major third-party keyboards because text composition gets broken when
    selection changes and on Android keyboad apps tend to use text
    composition more heavily.
    
    This CL introduces a new mechanism to present inline autocomplete
    on Android using SpannableString, allowing us to show inilne
    autocomplete even when text composition is used.
    
    The biggest huddle with this approach is deletion behavior change, and
    we will apply it only to English-only text for now.
    
    This is implemented behind a final boolean value for now, but will soon
    be followed up with a feature flag implementation.
    
    Design doc:
    https://docs.google.com/document/d/
    1PQQbCrC_Z9FnB7RUJh6_nkhkWgE87JzgT1oWxYpSDxs/edit
    
    BUG=539536
    
    Change-Id: I55c1e2791fe2cdbe645ab4eeb7b0b248e7709343

--- chrome/android/junit/src/org/chromium/chrome/browser/omnibox/AutocompleteEditTextTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/omnibox/AutocompleteEditTextTest.java
@@ -12,11 +12,9 @@ import static org.mockito.Mockito.spy;
 import android.content.Context;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.view.inputmethod.BaseInputConnection;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InOrder;
@@ -30,41 +28,60 @@ import org.chromium.testing.local.LocalRobolectricTestRunner;
 
 /**
  * A robolectric test for {@link AutocompleteEditText} class.
+ * TODO(changwan): switch to ParameterizedRobolectricTest once crbug.com/733324 is fixed.
  */
 @RunWith(LocalRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class AutocompleteEditTextTest {
-    private static final String TAG = "cr_AutocompleteEdit";
+    private static final String TAG = "cr_AutocompleteTest";
+
     private static final boolean DEBUG = false;
 
+    private boolean mUseSpannableModel;
     private InOrder mInOrder;
     private AutocompleteEditText mAutocomplete;
     private Context mContext;
     private InputConnection mInputConnection;
-    private BaseInputConnection mDummyTargetInputConnection;
-    private AutocompleteEmbedder mEmbedder;
+    private Verifier mVerifier;
 
-    // This is needed to limit the target of inOrder#verify.
-    private static class AutocompleteEmbedder {
+    // Limits the target of InOrder#verify.
+    private static class Verifier {
         public void onAutocompleteTextStateChanged(boolean updateDisplay) {
             if (DEBUG) Log.i(TAG, "onAutocompleteTextStateChanged");
         }
+
+        public void onUpdateSelection(int selStart, int selEnd) {
+            if (DEBUG) Log.i(TAG, "onUpdateSelection: [%d,%d]", selStart, selEnd);
+        }
     }
 
-    private static class TestAutocompleteEditText extends AutocompleteEditText {
-        private AutocompleteEmbedder mEmbedder;
+    private class TestAutocompleteEditText extends AutocompleteEditText {
+        private int mLastSelStart;
+        private int mLastSelEnd;
 
-        public TestAutocompleteEditText(
-                AutocompleteEmbedder embedder, Context context, AttributeSet attrs) {
+        public TestAutocompleteEditText(Context context, AttributeSet attrs) {
             super(context, attrs);
-            mEmbedder = embedder;
+        }
+
+        @Override
+        protected AutocompleteEditTextModelBase createModel() {
+            return mUseSpannableModel ? new SpannableAutocompleteEditTextModel(this)
+                                      : new AutocompleteEditTextModel(this);
         }
 
         @Override
         public void onAutocompleteTextStateChanged(boolean updateDisplay) {
-            // This function is called in super(), so mEmbedder may be null.
-            if (mEmbedder != null) {
-                mEmbedder.onAutocompleteTextStateChanged(updateDisplay);
+            // This function is called in super(), so mVerifier may be null.
+            if (mVerifier != null) {
+                mVerifier.onAutocompleteTextStateChanged(updateDisplay);
+            }
+        }
+
+        @Override
+        public void onUpdateSelectionForTesting(int selStart, int selEnd) {
+            // This function is called in super(), so mVerifier may be null.
+            if (mVerifier != null) {
+                mVerifier.onUpdateSelection(selStart, selEnd);
             }
         }
 
@@ -74,23 +91,25 @@ public class AutocompleteEditTextTest {
         }
     }
 
-    @Before
-    public void setUp() throws Exception {
-        ShadowLog.stream = System.out;
+    public AutocompleteEditTextTest() {
+        if (DEBUG) ShadowLog.stream = System.out;
+    }
+
+    private void setUp() {
         if (DEBUG) Log.i(TAG, "setUp started.");
         MockitoAnnotations.initMocks(this);
         mContext = RuntimeEnvironment.application;
-        mEmbedder = spy(new AutocompleteEmbedder());
-        mAutocomplete = spy(new TestAutocompleteEditText(mEmbedder, mContext, null));
+        mVerifier = spy(new Verifier());
+        mAutocomplete = spy(new TestAutocompleteEditText(mContext, null));
         assertNotNull(mAutocomplete);
-        // Note: this cannot catch the first {@link
-        // AutocompleteEditText#onAutocompleteTextStateChanged(boolean)}, which is caused
-        // by View constructor's call to setText().
-        mInOrder = inOrder(mEmbedder);
+        mInOrder = inOrder(mVerifier);
         mAutocomplete.onCreateInputConnection(new EditorInfo());
         mInputConnection = mAutocomplete.getInputConnection();
         assertNotNull(mInputConnection);
         mInOrder.verifyNoMoreInteractions();
+        // Feeder should call this at the beginning.
+        mAutocomplete.setIgnoreTextChangesForAutocomplete(false);
+
         if (DEBUG) Log.i(TAG, "setUp finished.");
     }
 
@@ -102,27 +121,193 @@ public class AutocompleteEditTextTest {
     }
 
     @Test
-    public void testAppend_CommitText() {
-        // Feeder should call this at the beginning.
-        mAutocomplete.setIgnoreTextChangesForAutocomplete(false);
-        // User types "h".
-        assertTrue(mInputConnection.commitText("h", 1));
-        mInOrder.verify(mEmbedder).onAutocompleteTextStateChanged(true);
+    public void testAppend_CommitTextWithSpannableModel() {
+        internalTestAppend_CommitText(true);
+    }
+
+    @Test
+    public void testAppend_CommitTextWithoutSpannableModel() {
+        internalTestAppend_CommitText(false);
+    }
+
+    @Test
+    public void testAppend_SetComposingTextWithSpannableModel() {
+        internalTestAppend_SetComposingText(true);
+    }
+
+    @Test
+    public void testAppend_SetComposingTextWithoutSpannableModel() {
+        internalTestAppend_SetComposingText(false);
+    }
+
+    @Test
+    public void testDelete_SetComposingTextWithSpannableModel() {
+        mUseSpannableModel = true;
+        setUp();
+
         // User types "hello".
-        assertTrue(mInputConnection.commitText("ello", 1));
-        mInOrder.verify(mEmbedder).onAutocompleteTextStateChanged(true);
+        assertTrue(mInputConnection.setComposingText("hello", 1));
+
+        mInOrder.verify(mVerifier).onAutocompleteTextStateChanged(true);
+        mInOrder.verify(mVerifier).onUpdateSelection(5, 5);
         mInOrder.verifyNoMoreInteractions();
+        assertEquals(true, mAutocomplete.shouldAutocomplete());
         // The controller kicks in.
         mAutocomplete.setAutocompleteText("hello", " world");
         assertTexts("hello", " world");
+        mInOrder.verifyNoMoreInteractions();
+
+        // User deletes 'o'.
+        assertTrue(mInputConnection.setComposingText("hell", 1));
+        mInOrder.verify(mVerifier).onUpdateSelection(4, 4);
+        mInOrder.verify(mVerifier).onUpdateSelection(5, 5);
+        mInOrder.verify(mVerifier).onAutocompleteTextStateChanged(false);
+        mInOrder.verifyNoMoreInteractions();
+        assertTexts("hello", "");
+    }
+
+    private void internalTestAppend_CommitText(boolean useSpannableModel) {
+        mUseSpannableModel = useSpannableModel;
+        setUp();
+
+        // User types "h".
+        assertTrue(mInputConnection.commitText("h", 1));
+        mInOrder.verify(mVerifier).onAutocompleteTextStateChanged(true);
+        mInOrder.verify(mVerifier).onUpdateSelection(1, 1);
+        mInOrder.verifyNoMoreInteractions();
+        assertTrue(mAutocomplete.shouldAutocomplete());
+
+        // The controller kicks in.
+        mAutocomplete.setAutocompleteText("h", "ello world");
+        // The non-spannable model changes selection in two steps.
+        if (!mUseSpannableModel) {
+            mInOrder.verify(mVerifier).onUpdateSelection(11, 11);
+            mInOrder.verify(mVerifier).onUpdateSelection(1, 11);
+        }
+        mInOrder.verifyNoMoreInteractions();
+
+        // User types "hello".
+        assertTrue(mInputConnection.commitText("ello", 1));
+        mInOrder.verify(mVerifier).onAutocompleteTextStateChanged(true);
+        mInOrder.verify(mVerifier).onUpdateSelection(5, 5);
+        mInOrder.verifyNoMoreInteractions();
+        // The controller kicks in.
+        mAutocomplete.setAutocompleteText("hello", " world");
+        if (!mUseSpannableModel) {
+            mInOrder.verify(mVerifier).onUpdateSelection(11, 11);
+            mInOrder.verify(mVerifier).onUpdateSelection(5, 11);
+        }
+        mInOrder.verifyNoMoreInteractions();
+        assertTexts("hello", " world");
+
+        // User types a space.
         assertTrue(mInputConnection.beginBatchEdit());
         assertTrue(mInputConnection.commitText(" ", 1));
-        assertTexts("hello ", "world");
+
+        if (mUseSpannableModel) {
+            // Under spannable model, autocomplete text is removed from the screen but internally
+            // kept until the batch edit ends.
+            assertEquals("hello ", mAutocomplete.getTextWithAutocomplete());
+            assertEquals(6, mAutocomplete.getAutocompleteLength());
+        } else {
+            assertTexts("hello ", "world");
+        }
+
         mInOrder.verifyNoMoreInteractions();
         assertTrue(mInputConnection.endBatchEdit());
-        mInOrder.verify(mEmbedder).onAutocompleteTextStateChanged(true);
+
+        // Autocomplete text gets redrawn.
+        assertTexts("hello ", "world");
+        if (mUseSpannableModel) {
+            mInOrder.verify(mVerifier).onUpdateSelection(6, 6);
+        } else {
+            mInOrder.verify(mVerifier).onUpdateSelection(6, 11);
+        }
+
         mAutocomplete.setAutocompleteText("hello ", "world");
         assertTexts("hello ", "world");
+        mInOrder.verifyNoMoreInteractions();
+    }
+
+    private void internalTestAppend_SetComposingText(boolean useSpannableModel) {
+        mUseSpannableModel = useSpannableModel;
+        setUp();
+
+        // User types "h".
+        assertTrue(mInputConnection.setComposingText("h", 1));
+
+        mInOrder.verify(mVerifier).onAutocompleteTextStateChanged(true);
+        mInOrder.verify(mVerifier).onUpdateSelection(1, 1);
+        mInOrder.verifyNoMoreInteractions();
+
+        // The old model does not allow autocompletion here.
+        assertEquals(mUseSpannableModel, mAutocomplete.shouldAutocomplete());
+        if (mUseSpannableModel) {
+            // The controller kicks in.
+            mAutocomplete.setAutocompleteText("h", "ello world");
+            assertTexts("h", "ello world");
+        } else {
+            assertTexts("h", "");
+        }
+        mInOrder.verifyNoMoreInteractions();
+
+        // User types "hello".
+        assertTrue(mInputConnection.setComposingText("hello", 1));
+        mInOrder.verify(mVerifier).onAutocompleteTextStateChanged(true);
+        mInOrder.verify(mVerifier).onUpdateSelection(5, 5);
+        mInOrder.verifyNoMoreInteractions();
+        if (mUseSpannableModel) {
+            assertTexts("hello", " world");
+        } else {
+            // The old model does not work with composition.
+            assertTexts("hello", "");
+        }
+
+        // The old model does not allow autocompletion here.
+        assertEquals(mUseSpannableModel, mAutocomplete.shouldAutocomplete());
+        if (mUseSpannableModel) {
+            // The controller kicks in.
+            mAutocomplete.setAutocompleteText("hello", " world");
+            assertTexts("hello", " world");
+        } else {
+            assertTexts("hello", "");
+        }
+        mInOrder.verifyNoMoreInteractions();
+
+        // User types a space.
+        assertTrue(mInputConnection.beginBatchEdit());
+        assertTrue(mInputConnection.finishComposingText());
+        assertTrue(mInputConnection.commitText(" ", 1));
+
+        if (mUseSpannableModel) {
+            // Under spannable model, autocomplete text is removed from the screen but internally
+            // kept until the batch edit ends.
+            assertEquals("hello ", mAutocomplete.getTextWithAutocomplete());
+            assertEquals(6, mAutocomplete.getAutocompleteLength());
+        } else {
+            assertTexts("hello ", "");
+        }
+
+        mInOrder.verifyNoMoreInteractions();
+        assertTrue(mInputConnection.endBatchEdit());
+        mInOrder.verify(mVerifier).onAutocompleteTextStateChanged(true);
+        mInOrder.verify(mVerifier).onUpdateSelection(6, 6);
+        mInOrder.verifyNoMoreInteractions();
+
+        if (mUseSpannableModel) {
+            // Autocomplete text has been drawn at endBatchEdit().
+            assertTexts("hello ", "world");
+        } else {
+            assertTexts("hello ", "");
+        }
+
+        // The old model can also autocomplete now.
+        assertTrue(mAutocomplete.shouldAutocomplete());
+        mAutocomplete.setAutocompleteText("hello ", "world");
+        assertTexts("hello ", "world");
+        if (!mUseSpannableModel) {
+            mInOrder.verify(mVerifier).onUpdateSelection(6, 11);
+        }
         mInOrder.verifyNoMoreInteractions();
     }
 }
