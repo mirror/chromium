aae7bb9326e91de42b9c10661d9d275236cd4448
Daniel Cheng <dcheng@chromium.org>
2017-06-22 02:43:52 -0700

    Remove redundant WebLocalFrame* parameter from WebFrameClient callbacks.
    WebLocalFrame and WebFrameClient should have a 1:1 relationship, which
    makes the parameter unnecessary.
    
    Bug: 361765
    Change-Id: I5462b3185698241654f26391f872200228009c66

--- third_party/WebKit/Source/modules/exported/WebEmbeddedWorkerImpl.cpp
+++ third_party/WebKit/Source/modules/exported/WebEmbeddedWorkerImpl.cpp
@@ -314,13 +314,6 @@ void WebEmbeddedWorkerImpl::LoadShadowPage() {
                        SubstituteData(buffer, "text/html", "UTF-8", KURL())));
 }
 
-void WebEmbeddedWorkerImpl::FrameDetached(WebLocalFrame* frame,
-                                          DetachType type) {
-  DCHECK(type == DetachType::kRemove && frame->Parent());
-  DCHECK(frame->FrameWidget());
-  frame->Close();
-}
-
 void WebEmbeddedWorkerImpl::DidFinishDocumentLoad() {
   DCHECK(!main_script_loader_);
   DCHECK(main_frame_);
