1f1e62d5a7c80dce1cbd2c64bb06c0801f5f81d2
Kent Tamura <tkent@chromium.org>
2017-06-22 13:27:29 +0900

    WIP, Not ready for review: Do not force layout on option selection.
    Adjust scroll position in PaintLayerScrollableArea::UpdateAfterLayout()
    
    Bug: 556801, 620408
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I2bd014b2f7950f1a51dbfa5067638013736d62dc

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -2362,6 +2362,7 @@ void Document::UpdateStyleAndLayoutIgnorePendingStylesheets(
 
   if (run_post_layout_tasks == kRunPostLayoutTasksSynchronously && View())
     View()->FlushAnyPendingPostLayoutTasks();
+  ++force_layout_count_;
 }
 
 PassRefPtr<ComputedStyle> Document::StyleForElementIgnoringPendingStylesheets(
