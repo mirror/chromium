1bca649a4d87e777c19cf34beafc9ee185d7828a
pdr <pdr@chromium.org>
2017-06-22 14:59:20 -0700

    A# Enter a description of the change. Remove Layer/LayerImpl's scroll_clip_layer and instead track scrolling bounds
    This patch removes scroll_clip_layer from Layer and LayerImpl which removes
    a major layer dependency for scrolling (e.g., with this patch, ScrollNode's
    owning_layer_id can be removed). Instead of tracking the scroll container
    bounds with a Layer, this patch tracks it explicitly as Layer/LayerImpl's
    scroll_container_bounds. If clipping is desired, a clip layer will still
    need to be added and manually kept up-to-date with the scrolling layer's
    scroll container bounds.
    
    Because the scroll container bounds are now tracked on the scrolling layer,
    scrollability (i.e., Layer::scrollable()) is based on the scroll container
    bounds being non-empty, and the scrollable bit has been removed.
    
    A TODO has been added to rename ScrollNode's scroll_clip_bounds to be
    scroll_container_bounds. The scroll_clip concept was particularily
    confusing for WebView which has a scrolling concept that does not clip.
    
    Bug: 723263
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I0ba1a15af50078d2ef52ef6deedc5ce40c80802a

--- cc/trees/layer_tree_host_unittest.cc
+++ cc/trees/layer_tree_host_unittest.cc
@@ -813,7 +813,7 @@ class LayerTreeHostTestPushNodeOwnerToNodeIdMap : public LayerTreeHostTest {
         break;
       case 2:
         // child_ should create a scroll node.
-        child_->SetScrollClipLayerId(root_->id());
+        child_->SetScrollContainerBounds(gfx::Size(1, 1));
         break;
       case 3:
         // child_ should create a clip node.
@@ -823,7 +823,7 @@ class LayerTreeHostTestPushNodeOwnerToNodeIdMap : public LayerTreeHostTest {
         // child_ should not create any property tree node.
         child_->SetMasksToBounds(false);
         child_->SetForceRenderSurfaceForTesting(false);
-        child_->SetScrollClipLayerId(Layer::INVALID_ID);
+        child_->SetScrollContainerBounds(gfx::Size(0, 0));
     }
   }
 
@@ -2742,7 +2742,7 @@ class ViewportDeltasAppliedDuringPinch : public LayerTreeHostTest {
 
     scoped_refptr<Layer> pinch = Layer::Create();
     pinch->SetBounds(gfx::Size(500, 500));
-    pinch->SetScrollClipLayerId(root_clip->id());
+    pinch->SetScrollContainerBounds(gfx::Size(200, 200));
     pinch->SetIsContainerForFixedPositionLayers(true);
     page_scale_layer->AddChild(pinch);
     root_clip->AddChild(page_scale_layer);
@@ -4766,8 +4766,8 @@ class LayerTreeHostTestElasticOverscroll : public LayerTreeHostTest {
     scoped_refptr<Layer> overscroll_elasticity_layer = Layer::Create();
     scoped_refptr<Layer> page_scale_layer = Layer::Create();
     scoped_refptr<Layer> inner_viewport_scroll_layer = Layer::Create();
-    inner_viewport_scroll_layer->SetScrollClipLayerId(
-        inner_viewport_container_layer->id());
+    inner_viewport_scroll_layer->SetScrollContainerBounds(
+        inner_viewport_container_layer->bounds());
     inner_viewport_scroll_layer->SetIsContainerForFixedPositionLayers(true);
 
     root_layer_->AddChild(inner_viewport_container_layer);
@@ -6191,7 +6191,7 @@ class LayerTreeHostTestCrispUpAfterPinchEnds : public LayerTreeHostTest {
 
     scoped_refptr<Layer> pinch = Layer::Create();
     pinch->SetBounds(gfx::Size(500, 500));
-    pinch->SetScrollClipLayerId(root_clip->id());
+    pinch->SetScrollContainerBounds(gfx::Size(500, 500));
     pinch->SetIsContainerForFixedPositionLayers(true);
     page_scale_layer->AddChild(pinch);
     root_clip->AddChild(page_scale_layer);
@@ -6498,7 +6498,7 @@ class LayerTreeHostTestContinuousDrawWhenCreatingVisibleTiles
 
     scoped_refptr<Layer> pinch = Layer::Create();
     pinch->SetBounds(gfx::Size(500, 500));
-    pinch->SetScrollClipLayerId(root_clip->id());
+    pinch->SetScrollContainerBounds(gfx::Size(500, 500));
     pinch->SetIsContainerForFixedPositionLayers(true);
     page_scale_layer->AddChild(pinch);
     root_clip->AddChild(page_scale_layer);
@@ -6904,7 +6904,7 @@ class LayerTreeTestMaskLayerForSurfaceWithContentRectNotAtOrigin
     CreateVirtualViewportLayers(root.get(), outer_viewport_scroll_layer,
                                 gfx::Size(50, 50), gfx::Size(50, 50),
                                 layer_tree_host());
-    outer_viewport_scroll_layer->scroll_clip_layer()->SetMasksToBounds(true);
+    layer_tree_host()->outer_viewport_container_layer()->SetMasksToBounds(true);
     outer_viewport_scroll_layer->AddChild(content_layer);
 
     client_.set_bounds(root->bounds());
