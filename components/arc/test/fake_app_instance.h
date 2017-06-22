40fb316aafef6199055c73ba400780412c2e1c72
khmel <khmel@google.com>
2017-06-22 11:30:35 -0700

    arc: Delay PAI flow until Voice Interaction is done.
    Before, Play Auto Install (PAI) was started right after sign-in
    completed. For Eve, we add extra component that exists on system
    image and listed in PAI configuration. In case PAI executed, and Voice
    Interaction is shown then it will be stopped. To prevent this, we need
    to start PAI after this step is completed on Eve. This CL changes
    auto-start PAI flow to manual request and introduces the lock mechanism
    for PAI flow. PAI is now started when two conditions are met. ARC
    signed in and no PAI locks.
    
    Test: Manually on device. Simulate eve environment and PAI is started
          once Voice Interaction is done or skipped. Simulate caroline
          environment and PAI works as expected.
    Bug: b/62269090
    Change-Id: I90d955f82807ea8d211734b5fb40508d56714035

--- components/arc/test/fake_app_instance.h
+++ components/arc/test/fake_app_instance.h
@@ -112,6 +112,7 @@ class FakeAppInstance : public mojom::AppInstance {
   void SetNotificationsEnabled(const std::string& package_name,
                                bool enabled) override;
   void InstallPackage(mojom::ArcPackageInfoPtr arcPackageInfo) override;
+  void StartPaiFlow() override;
 
   // Methods to reply messages.
   void SendRefreshAppList(const std::vector<mojom::AppInfo>& apps);
@@ -148,6 +149,8 @@ class FakeAppInstance : public mojom::AppInstance {
 
   int refresh_app_list_count() const { return refresh_app_list_count_; }
 
+  int start_pai_request_count() const { return start_pai_request_count_; }
+
   const std::vector<std::unique_ptr<Request>>& launch_requests() const {
     return launch_requests_;
   }
@@ -171,6 +174,8 @@ class FakeAppInstance : public mojom::AppInstance {
   mojom::AppHost* app_host_;
   // Number of RefreshAppList calls.
   int refresh_app_list_count_ = 0;
+  // Number of requests to start PAI flows.
+  int start_pai_request_count_ = 0;
   // Keeps information about launch requests.
   std::vector<std::unique_ptr<Request>> launch_requests_;
   // Keeps information about launch intents.
