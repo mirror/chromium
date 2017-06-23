de1a494cdbc8120610e6e2db5b57ddc18746be48
Louis Romero <lpromero@chromium.org>
2017-06-23 18:40:05 +0200

    Settings coordinators
    Currently, there is only one master coordinator for the entire settings. The
    screens themselves are not following the separation of layers rules.
    
    This prototype CL allows the flow where a view controller screen is ported to
    be driven by a coordinator.
    
    Before: https://drive.google.com/open?id=0Bwef-6NjeDzANXptdEhHQUI0djA
    After: https://drive.google.com/open?id=0Bwef-6NjeDzAU2JzYjEtWnB1eGM
    
    Bug:
    Change-Id: Ia1cce3c805d28dba968a5bdba537305af20c74c9

--- ios/chrome/browser/ui/settings/settings_navigation_controller.mm
+++ ios/chrome/browser/ui/settings/settings_navigation_controller.mm
@@ -302,7 +302,9 @@ initWithRootViewController:(UIViewController*)rootViewController
                   delegate:(id<SettingsNavigationControllerDelegate>)delegate {
   DCHECK(browserState);
   DCHECK(!browserState->IsOffTheRecord());
-  self = [super initWithRootViewController:rootViewController];
+  self = rootViewController
+             ? [super initWithRootViewController:rootViewController]
+             : [super init];
   if (self) {
     mainBrowserState_ = browserState;
     delegate_ = delegate;
