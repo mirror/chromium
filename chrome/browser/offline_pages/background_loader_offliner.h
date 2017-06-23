e3de0c835a5a8bb228f027360041d8097d6ffcdf
Pete Williamson <petewil@chromium.org>
2017-06-23 14:30:44 -0700

    Add a vector for tracking resource types in the BackgroundLoaderOffliner.
    As part 3 (of 3) in implementing the ResourcePercentageSignal, this
    adds a vector for tracking 3 resource types inside the
    BackgroundLoaderOffliner that are closely related to right moment
    detection.
    
    See design doc here:
    https://docs.google.com/document/d/1dxGOMsUkmxfoNUfQKqZRi4qAhWVGh9DZDiFI1arkiJ0
    Bug: 699313
    Change-Id: I440b577208956b7c6a84448609d1a3628d3725bd

--- chrome/browser/offline_pages/background_loader_offliner.h
+++ chrome/browser/offline_pages/background_loader_offliner.h
@@ -26,6 +26,20 @@ namespace offline_pages {
 class OfflinerPolicy;
 class OfflinePageModel;
 
+struct RequestSpecifics {
+ public:
+  RequestSpecifics(ResourceLoadingObserver::ResourceDataType type, bool done);
+
+ private:
+  ResourceLoadingObserver::ResourceDataType request_type;
+  bool complete;  // true if the request has completed.
+};
+
+struct RequestStats {
+  int requested;
+  int completed;
+};
+
 // An Offliner implementation that attempts client-side rendering and saving
 // of an offline page. It uses the BackgroundLoader to load the page and the
 // OfflinePageModel to save it. Only one request may be active at a time.
@@ -77,6 +91,7 @@ class BackgroundLoaderOffliner : public Offliner,
 
  private:
   friend class TestBackgroundLoaderOffliner;
+  friend class BackgroundLoaderOfflinerTest;
 
   enum SaveState { NONE, SAVING, DELETE_AFTER_SAVE };
   enum PageLoadState { SUCCESS, RETRIABLE, NONRETRIABLE };
@@ -98,6 +113,9 @@ class BackgroundLoaderOffliner : public Offliner,
 
   void DeleteOfflinePageCallback(const SavePageRequest& request,
                                  DeletePageResult result);
+
+  // Testing method to examine resource stats.
+  RequestStats* GetRequestStatsForTest() { return stats_; }
 
   std::unique_ptr<background_loader::BackgroundLoaderContents> loader_;
   // Not owned.
@@ -141,9 +159,8 @@ class BackgroundLoaderOffliner : public Offliner,
   // Callback for cancel.
   CancelCallback cancel_callback_;
 
-  // TODO(petewil): Replace with a map of all types
-  int64_t started_count_;
-  int64_t completed_count_;
+  // Holds stats for resource request status for resource types we track.
+  RequestStats stats_[ResourceDataType::RESOURCE_DATA_TYPE_COUNT];
 
   base::WeakPtrFactory<BackgroundLoaderOffliner> weak_ptr_factory_;
   DISALLOW_COPY_AND_ASSIGN(BackgroundLoaderOffliner);
