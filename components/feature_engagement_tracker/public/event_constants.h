7e766f4465e9356e1d5acd35c19d01fc0d2acb3d
Catherine Chung <catherinechung@google.com>
2017-06-23 14:57:21 -0700

    Added event constants in FeatureEngagementTracker
    Previously, the FeatureEngagementTracker was only on Android, and now
    that it is cross-platform, there must be event constants added in C++.
    The constants are currently for the New Tab in-product help.
    
    Bug: 736475
    Change-Id: I7cc677fa3af045648653368ced2f70332ec6e18a

---
+++ components/feature_engagement_tracker/public/event_constants.h
@@ -0,0 +1,25 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_FEATURE_ENGAGEMENT_TRACKER_PUBLIC_EVENT_CONSTANTS_H_
+#define COMPONENTS_FEATURE_ENGAGEMENT_TRACKER_PUBLIC_EVENT_CONSTANTS_H_
+
+namespace feature_engagement_tracker {
+
+// All the events declared below are the string names
+// of deferred onboarding events for the New Tab.
+#if defined(OS_WIN) || defined(OS_LINUX)
+// The user has opened a new tab.
+extern const char kNewTabOpened[];
+
+// The user has interacted with the omnibox.
+extern const char kOmniboxInteraction[];
+
+// The user has accumulated 2 hours of active session time (one-off event).
+extern const char kSessionTime[];
+#endif  // OS_WIN || OS_LINUX
+
+}  // namespace feature_engagement_tracker
+
+#endif  // COMPONENTS_FEATURE_ENGAGEMENT_TRACKER_PUBLIC_EVENT_CONSTANTS_H_
