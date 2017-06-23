2fafff1fbe511f34d912389b31221aae85d905be
Catherine Chung <catherinechung@google.com>
2017-06-23 13:31:39 -0700

    Added event constants in FeatureEngagementTracker
    Bug: 736475
    Change-Id: I7cc677fa3af045648653368ced2f70332ec6e18a

---
+++ components/feature_engagement_tracker/public/event_constants.h
@@ -0,0 +1,19 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_FEATURE_ENGAGEMENT_TRACKER_PUBLIC_EVENT_CONSTANTS_H_
+#define COMPONENTS_FEATURE_ENGAGEMENT_TRACKER_PUBLIC_EVENT_CONSTANTS_H_
+
+namespace feature_engagement_tracker {
+
+// All the events declared below are the string names
+// of deferred onboarding events for the New Tab.
+
+extern const char kNewTabOpened[];
+extern const char kOmniboxInteraction[];
+extern const char kSessionTime[];
+
+}  // namespace feature_engagement_tracker
+
+#endif  // COMPONENTS_FEATURE_ENGAGEMENT_TRACKER_PUBLIC_EVENT_CONSTANTS_H_
