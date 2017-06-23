9707ab0184bbc1f6f2f2c511168c635122b64941
Rune Lillesveen <rune@opera.com>
2017-06-23 15:13:46 +0200

    Implement lazy (re-)attachment of whitespace.
    BUG=349395,399816,705525,686016,648931,650168
    
    Change-Id: Id397986c49a4bc75e831da1ff8b679f2043873c3

--- third_party/WebKit/Source/core/dom/shadow/ShadowRoot.cpp
+++ third_party/WebKit/Source/core/dom/shadow/ShadowRoot.cpp
@@ -33,6 +33,7 @@
 #include "core/dom/ElementTraversal.h"
 #include "core/dom/StyleEngine.h"
 #include "core/dom/Text.h"
+#include "core/dom/WhitespaceAttacher.h"
 #include "core/dom/shadow/ElementShadow.h"
 #include "core/dom/shadow/InsertionPoint.h"
 #include "core/dom/shadow/ShadowRootRareDataV0.h"
@@ -157,20 +158,21 @@ void ShadowRoot::RecalcStyle(StyleRecalcChange change) {
   ClearChildNeedsStyleRecalc();
 }
 
-void ShadowRoot::RebuildLayoutTree(Text*& next_text_sibling) {
+void ShadowRoot::RebuildLayoutTree(WhitespaceAttacher& whitespace_attacher) {
   if (!NeedsReattachLayoutTree() && !ChildNeedsReattachLayoutTree()) {
-    SkipRebuildLayoutTree(next_text_sibling);
+    SkipRebuildLayoutTree(whitespace_attacher);
     return;
   }
 
   StyleSharingDepthScope sharing_scope(*this);
 
   ClearNeedsReattachLayoutTree();
-  RebuildChildrenLayoutTrees(next_text_sibling);
+  RebuildChildrenLayoutTrees(whitespace_attacher);
   ClearChildNeedsReattachLayoutTree();
 }
 
-void ShadowRoot::SkipRebuildLayoutTree(Text*& next_text_sibling) const {
+void ShadowRoot::SkipRebuildLayoutTree(
+    WhitespaceAttacher& whitespace_attacher) const {
   // We call this method when neither this, nor our child nodes are marked
   // for re-attachment, but the host has been marked with
   // childNeedsReattachLayoutTree. That happens when ::before or ::after needs
@@ -187,12 +189,12 @@ void ShadowRoot::SkipRebuildLayoutTree(Text*& next_text_sibling) const {
   for (Node* sibling = firstChild(); sibling;
        sibling = sibling->nextSibling()) {
     if (sibling->IsTextNode()) {
-      next_text_sibling = ToText(sibling);
+      whitespace_attacher.DidVisitText(ToText(sibling));
       return;
     }
     LayoutObject* layout_object = sibling->GetLayoutObject();
     if (layout_object && !layout_object->IsFloatingOrOutOfFlowPositioned()) {
-      next_text_sibling = nullptr;
+      whitespace_attacher.DidVisitElement(ToElement(sibling));
       return;
     }
   }
