9f07d1906c6d0a1dcb04e870684d1d9d9026dde8
Marc Treib <treib@chromium.org>
2017-06-23 11:15:52 +0200

    Disable Supervised User creation on ChromeOS
    This adds a disabled-by-default feature SupervisedUserCreation, and
    checks it on ChromeOS. A follow-up CL should check the same feature on
    desktop.
    
    Bug: 736040
    Change-Id: I8e04a5220b8a0f50b4af06eccfa36c084daa815f

--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -170,6 +170,8 @@ extern const base::Feature kSiteDetails;
 extern const base::Feature kSiteNotificationChannels;
 #endif
 
+extern const base::Feature kSupervisedUserCreation;
+
 #if defined(SYZYASAN)
 extern const base::Feature kSyzyasanDeferredFree;
 #endif
