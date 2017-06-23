305f7410121b03628cb7d3d71b875181f28770a6
danakj <danakj@chromium.org>
2017-06-23 19:34:34 -0400

    viz: Move GLHelper to viz/common/.
    This is used by anyone with access to a GLES2Interface which is not
    only in the service, or for use with the display compositor. So move
    to viz/common/. The benchmarks test helpers move into viz/common/test/.
    
    R=kylechar@chromium.org
    TBR=jam@chromium.org
    
    Bug: 722935
    Change-Id: Ie73b2f2fd73271b57beb33a3bd446333672e5d63

--- components/viz/common/test/real_gl_test_suite.cc
+++ components/viz/common/test/real_gl_test_suite.cc
@@ -2,37 +2,28 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/viz/service/display_compositor/display_compositor_test_suite.h"
+#include "components/viz/common/test/real_gl_test_suite.h"
 
-#include "base/message_loop/message_loop.h"
 #include "base/threading/thread_id_name_manager.h"
 #include "cc/test/paths.h"
 #include "ui/gl/test/gl_surface_test_support.h"
 
 namespace viz {
 
-DisplayCompositorTestSuite::DisplayCompositorTestSuite(int argc, char** argv)
+RealGLTestSuite::RealGLTestSuite(int argc, char** argv)
     : base::TestSuite(argc, argv) {}
 
-DisplayCompositorTestSuite::~DisplayCompositorTestSuite() {}
+RealGLTestSuite::~RealGLTestSuite() = default;
 
-void DisplayCompositorTestSuite::Initialize() {
+void RealGLTestSuite::Initialize() {
   base::TestSuite::Initialize();
   gl::GLSurfaceTestSupport::InitializeOneOff();
   cc::CCPaths::RegisterPathProvider();
-
-  message_loop_.reset(new base::MessageLoop);
 
   base::ThreadIdNameManager::GetInstance()->SetName(
       base::PlatformThread::CurrentId(), "Main");
 
   base::DiscardableMemoryAllocator::SetInstance(&discardable_memory_allocator_);
-}
-
-void DisplayCompositorTestSuite::Shutdown() {
-  message_loop_ = nullptr;
-
-  base::TestSuite::Shutdown();
 }
 
 }  // namespace viz
