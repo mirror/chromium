3f91b242fbd60cf3e4ccbc12d832b203dd175e77
Joseph Arhar <jarhar@google.com>
2017-06-23 09:34:58 -0700

    Move test echo logic to host extension
    Bug: 606809
    Change-Id: Ifb8ce14df492bebf98a07b5f77fe99c413292a2b

---
+++ remoting/host/test_echo_extension_session.cc
@@ -0,0 +1,44 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/test_echo_extension_session.h"
+
+#include "base/logging.h"
+#include "remoting/proto/control.pb.h"
+#include "remoting/protocol/client_stub.h"
+
+namespace {
+const char kExtensionMessageType[] = "test-echo";
+}
+
+namespace remoting {
+
+TestEchoExtensionSession::TestEchoExtensionSession(
+    protocol::ClientStub* client_stub)
+    : client_stub_(client_stub) {
+  DCHECK(client_stub_);
+}
+
+TestEchoExtensionSession::~TestEchoExtensionSession() {}
+
+bool TestEchoExtensionSession::OnExtensionMessage(
+    ClientSessionDetails* client_session_details,
+    protocol::ClientStub* client_stub,
+    const protocol::ExtensionMessage& message) {
+  if (message.type() != kExtensionMessageType) {
+    return false;
+  }
+
+  protocol::ExtensionMessage reply;
+  reply.set_type("test-echo-reply");
+  if (message.has_data()) {
+    reply.set_data(message.data().substr(0, 16));
+  }
+
+  DCHECK(client_stub_);
+  client_stub_->DeliverHostMessage(reply);
+  return true;
+}
+
+}  // namespace remoting
