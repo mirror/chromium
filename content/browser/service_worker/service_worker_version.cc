536e77aa80c160261e746efe2734a0cc2eb81780
Makoto Shimazu <shimazu@chromium.org>
2017-06-23 15:48:27 +0900

    [WIP] Creating WorkerCachedScriptsManager for script streaming
    Bug: 683037
    Change-Id: I17203eb2ba0c76230ce479a8a2ebd605e49b8a29

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -32,6 +32,7 @@
 #include "content/browser/service_worker/service_worker_client_utils.h"
 #include "content/browser/service_worker/service_worker_context_core.h"
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
+#include "content/browser/service_worker/service_worker_installed_scripts_sender.h"
 #include "content/browser/service_worker/service_worker_metrics.h"
 #include "content/browser/service_worker/service_worker_registration.h"
 #include "content/common/origin_trials/trial_token_validator.h"
@@ -783,6 +784,7 @@ void ServiceWorkerVersion::SetDevToolsAttached(bool attached) {
 
 void ServiceWorkerVersion::SetMainScriptHttpResponseInfo(
     const net::HttpResponseInfo& http_info) {
+  LOG(ERROR) << __PRETTY_FUNCTION__;
   main_script_http_info_.reset(new net::HttpResponseInfo(http_info));
 
   // Updates |origin_trial_tokens_| if it is not set yet. This happens when:
@@ -811,6 +813,7 @@ void ServiceWorkerVersion::SetTickClockForTesting(
 
 const net::HttpResponseInfo*
 ServiceWorkerVersion::GetMainScriptHttpResponseInfo() {
+  LOG(ERROR) << __PRETTY_FUNCTION__;
   return main_script_http_info_.get();
 }
 
@@ -1472,8 +1475,15 @@ void ServiceWorkerVersion::StartWorkerInternal() {
   params->is_installed = IsInstalled(status_);
   params->pause_after_download = pause_after_download_;
 
+  DCHECK(!installed_scripts_sender_);
+  installed_scripts_sender_ =
+      base::MakeUnique<ServiceWorkerInstalledScriptsSender>(
+          this, script_url(), script_cache_map()->AsWeakPtr(), context());
+  auto installed_scritps_info = installed_scripts_sender_->CreateInfoAndBind();
+
   embedded_worker_->Start(
       std::move(params), mojo::MakeRequest(&event_dispatcher_),
+      std::move(installed_scritps_info),
       base::Bind(&ServiceWorkerVersion::OnStartSentAndScriptEvaluated,
                  weak_factory_.GetWeakPtr()));
   event_dispatcher_.set_connection_error_handler(base::Bind(
