f252b952bac37aa507e056bf22150034d10e6704
Xing Liu <xingliu@chromium.org>
2017-06-24 15:45:23 -0700

    Download service: Fixed an issue in throttling config.
    Previously the throttling config is only checked at the beginning of
    ActivateMoreDownload, but we should check on every time we transit
    something to ACTIVE.
    
    Bug:
    Change-Id: I62f1bb6c8c5aa7e81dc44a09fbae0273493d53f2

--- components/download/internal/controller_impl_unittest.cc
+++ components/download/internal/controller_impl_unittest.cc
@@ -1176,9 +1176,10 @@ TEST_F(DownloadServiceControllerImplTest, ThrottlingConfigMaxRunning) {
   store_->TriggerInit(true, base::MakeUnique<std::vector<Entry>>(entries));
   store_->AutomaticallyTriggerAllFutureCallbacks(true);
 
-  // Hit the max running configuration threshold, nothing should be called.
-  EXPECT_CALL(*scheduler_, Next(_, _)).Times(0);
-  EXPECT_CALL(*scheduler_, Reschedule(_)).Times(0);
+  // Hit the max running configuration threshold, we still need to call
+  // next and reschedule for once.
+  EXPECT_CALL(*scheduler_, Next(_, _)).Times(1);
+  EXPECT_CALL(*scheduler_, Reschedule(_)).Times(1);
   driver_->MakeReady();
   task_runner_->RunUntilIdle();
 
@@ -1189,8 +1190,9 @@ TEST_F(DownloadServiceControllerImplTest, ThrottlingConfigMaxRunning) {
 // paused downloads.
 TEST_F(DownloadServiceControllerImplTest, ThrottlingConfigMaxConcurrent) {
   Entry entry1 = test::BuildBasicEntry(Entry::State::AVAILABLE);
-  Entry entry2 = test::BuildBasicEntry(Entry::State::PAUSED);
-  std::vector<Entry> entries = {entry1, entry2};
+  Entry entry2 = test::BuildBasicEntry(Entry::State::AVAILABLE);
+  Entry entry3 = test::BuildBasicEntry(Entry::State::PAUSED);
+  std::vector<Entry> entries = {entry1, entry2, entry3};
 
   EXPECT_CALL(*client_, OnServiceInitialized(_)).Times(1);
 
@@ -1205,16 +1207,23 @@ TEST_F(DownloadServiceControllerImplTest, ThrottlingConfigMaxConcurrent) {
 
   // Can have one more download due to max concurrent configuration.
   testing::InSequence seq;
+  EXPECT_EQ(Entry::State::AVAILABLE, model_->Get(entry1.guid)->state);
   EXPECT_CALL(*scheduler_, Next(_, _))
       .Times(1)
       .WillOnce(Return(model_->Get(entry1.guid)))
       .RetiresOnSaturation();
-  EXPECT_CALL(*scheduler_, Next(_, _)).Times(1).RetiresOnSaturation();
+  // |scheduler_| will poll entry2 on next time, but it should not change the
+  // state of entry2 due to max running download configuration.
+  EXPECT_CALL(*scheduler_, Next(_, _))
+      .WillOnce(Return(model_->Get(entry2.guid)));
+
   EXPECT_CALL(*scheduler_, Reschedule(_)).Times(1);
   driver_->MakeReady();
   task_runner_->RunUntilIdle();
 
   EXPECT_EQ(Entry::State::ACTIVE, model_->Get(entry1.guid)->state);
+  EXPECT_EQ(Entry::State::AVAILABLE, model_->Get(entry2.guid)->state);
+  EXPECT_EQ(Entry::State::PAUSED, model_->Get(entry3.guid)->state);
 }
 
 }  // namespace download
