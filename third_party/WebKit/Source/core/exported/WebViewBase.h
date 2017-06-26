c2b751118ef6003947b5ca196d9af10c64f44d8f
Nicholas Verne <nverne@chromium.org>
2017-06-26 18:14:51 +1000

    WIP. Create controller/ component in blink. The first non-boilerplate files in here are WebDevToolsAgentImpl.
    The controller/ is depended on by web (temporarily) and is part
    
    Bug:
    Change-Id: I7dbe02690ca3a90c08fead13468d160773fe7753

--- third_party/WebKit/Source/core/exported/WebViewBase.h
+++ third_party/WebKit/Source/core/exported/WebViewBase.h
@@ -50,7 +50,8 @@ struct WebRect;
 //
 // Once WebViewImpl is moved from web to core/exported then this class should be
 // removed and clients can once again depend on WebViewImpl.
-class WebViewBase : public WebView, public RefCounted<WebViewBase> {
+class CORE_EXPORT WebViewBase : public NON_EXPORTED_BASE(WebView),
+                                public RefCounted<WebViewBase> {
  public:
   virtual ~WebViewBase() {}
 
