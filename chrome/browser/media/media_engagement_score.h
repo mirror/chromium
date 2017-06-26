860056db3314b53e7377b72a52a2404cc93477b8
Becca Hughes <beccahughes@chromium.org>
2017-06-26 18:24:12 +0100

    Media Engagement: Expose the scores through Mojo.
    Add mojo bindings to expose the Media Engagement scores for the
    WebUI.
    
    BUG=734525
    
    Change-Id: I73800107dfa2bd58f4287397fc6a463248d20249

--- chrome/browser/media/media_engagement_score.h
+++ chrome/browser/media/media_engagement_score.h
@@ -9,6 +9,7 @@
 #include "base/memory/ptr_util.h"
 #include "base/time/clock.h"
 #include "base/values.h"
+#include "chrome/browser/media/media_engagement_details.mojom.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "url/gurl.h"
 
@@ -52,6 +53,9 @@ class MediaEngagementScore final {
 
   // Calculate the score as per the formula in the design doc.
   static double CalculateScore(int visits, int media_playbacks);
+
+  // Get a breakdown of the score that can be serialized by Mojo.
+  mojom::MediaEngagementDetails GetDetails() const;
 
   // The dictionary keys to store individual metrics. kVisitsKey will
   // store the number of visits to an origin and kMediaPlaybacksKey
