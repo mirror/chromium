9b02f32edc491f5c12afcad059ac261ef99439ca
Chris Harrelson <chrishtr@chromium.org>
2017-06-26 14:01:05 -0700

    Refactor to only need to check cull rect changes up to containing transform.
    This allows us to
    (a) simplify the PrePaintTree walk code, and make it faster
    and
    (b) makes cull rects for transforms SPv2-compatible, because we can't cull them
    out if they might end up composited
    
    Details:
    1. Stop culling out CSS transformed elements and passing along cull rects from above
    the transform. This change means that we don't need to worry about any cull rects
    of a transform or from outside of it.
    2. Simplify cull rect change detection logic in PrePaintTreeWalk and
    PaintPropertyTreeBuilder to check only for whether a clip rect changed, or was
    added/removed, between self and containing transform. This is made possible by #1.
    3. Stop optimizing out paint invalidations for empty visual rects under transform.
    This avoids paint under-invalidation failures in cases when a transform was
    previously clipped out.
    4. Remove ancestor_transformed_or_root_paint_layer from the pre-paint tree
    walk context.
    
    
    Bug: 729514
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ic4d8a382c9991c51d4b25c03cb71b406b1ee98ac

--- third_party/WebKit/Source/core/paint/PaintInvalidator.h
+++ third_party/WebKit/Source/core/paint/PaintInvalidator.h
@@ -65,6 +65,10 @@ struct CORE_EXPORT PaintInvalidatorContext {
     // don't need any invalidation. They are used as "painting subroutines"
     // for one or more other locations in SVG.
     kSubtreeNoInvalidation = 1 << 6,
+
+    // Don't skip invalidating because the previous and current visual
+    // rects were empty.
+    kInvalidateEmptyVisualRect = 1 << 7,
   };
   unsigned subtree_flags = 0;
 
