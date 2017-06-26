d02ecc3791c741007eeaf4974afc13802e037bd3
Becca Hughes <beccahughes@chromium.org>
2017-06-26 19:55:29 +0100

    Add a MediaEngagementScore class to manage storing the score.
    Add a MediaEngagementScore class that manages calculating the total
    score as well as storing/reading the score for an origin from the
    content settings store. Register the setting to be synced across
    devices.
    
    BUG=731750
    
    Change-Id: I30e1ceb815c252d5f0395a8d10b905e7a6c9914b

---
+++ chrome/browser/media/media_engagement_score.h
@@ -0,0 +1,103 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_MEDIA_MEDIA_ENGAGEMENT_SCORE_H_
+#define CHROME_BROWSER_MEDIA_MEDIA_ENGAGEMENT_SCORE_H_
+
+#include "base/macros.h"
+#include "base/memory/ptr_util.h"
+#include "base/time/clock.h"
+#include "base/values.h"
+#include "components/content_settings/core/browser/host_content_settings_map.h"
+#include "url/gurl.h"
+
+class HostContentSettingsMap;
+
+// Calculates and stores the Media Engagement Index score for a certain origin.
+class MediaEngagementScore final {
+ public:
+  MediaEngagementScore(base::Clock* clock,
+                       const GURL& origin,
+                       HostContentSettingsMap* settings);
+
+  MediaEngagementScore(base::Clock* clock,
+                       const GURL& origin,
+                       std::unique_ptr<base::DictionaryValue> score_dict);
+
+  MediaEngagementScore(MediaEngagementScore&& other);
+  ~MediaEngagementScore();
+  MediaEngagementScore& operator=(MediaEngagementScore&& other);
+
+  // Returns the total score, as per the formula.
+  double GetTotalScore() const;
+
+  // Writes the values in this score into |settings_map_|. If there are multiple
+  // instances of a score object for an origin, this could result in stale data
+  // being stored.
+  void Commit();
+
+  // Get/increment the number of visits this origin has had.
+  int visits() const { return visits_; }
+  void IncrementVisits() { visits_++; }
+
+  // Get/increment the number of media playbacks this origin has had.
+  int media_playbacks() const { return media_playbacks_; }
+  void IncrementMediaPlaybacks();
+
+  // Get the last time media was played on this origin.
+  base::Time last_media_playback_time() const {
+    return last_media_playback_time_;
+  }
+
+  // The dictionary keys to store individual metrics. kVisitsKey will
+  // store the number of visits to an origin and kMediaPlaybacksKey
+  // will store the number of media playbacks on an origin.
+  // kLastMediaPlaybackTimeKey will store the timestamp of the last
+  // media playback on an origin.
+  static const char* kVisitsKey;
+  static const char* kMediaPlaybacksKey;
+  static const char* kLastMediaPlaybackTimeKey;
+
+  // Origins with a number of visits less than this number will recieve
+  // a score of zero.
+  static const int kScoreMinVisits;
+
+ private:
+  friend class MediaEngagementScoreTest;
+
+  // The number of media playbacks this origin has had.
+  int media_playbacks_;
+
+  // The number of visits this origin has had.
+  int visits_;
+
+  // The last time media was played back on this origin.
+  base::Time last_media_playback_time_;
+
+  // The origin this score represents.
+  GURL origin_;
+
+  // A clock that can be used for testing.
+  base::Clock* clock_;
+
+  // The dictionary that represents this engagement score.
+  std::unique_ptr<base::DictionaryValue> score_dict_;
+
+  // The content settings map that will persist the score.
+  HostContentSettingsMap* settings_map_;
+
+  // Update the dictionary continaing the latest score values and return whether
+  // they have changed or not (since what was last retrieved from content
+  // settings).
+  bool UpdateScoreDict(base::DictionaryValue* score_dict);
+
+  // Get the dictionary containing the score values from content settings.
+  static std::unique_ptr<base::DictionaryValue> GetScoreDictForSettings(
+      const HostContentSettingsMap* settings,
+      const GURL& origin_url);
+
+  DISALLOW_COPY_AND_ASSIGN(MediaEngagementScore);
+};
+
+#endif  // CHROME_BROWSER_MEDIA_MEDIA_ENGAGEMENT_SCORE_H_
---
+++ chrome/browser/media/media_engagement_score_unittest.cc
@@ -0,0 +1,168 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/media/media_engagement_score.h"
+
+#include <utility>
+
+#include "base/macros.h"
+#include "base/test/simple_test_clock.h"
+#include "base/values.h"
+#include "chrome/browser/content_settings/host_content_settings_map_factory.h"
+#include "chrome/browser/engagement/site_engagement_service.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/test/base/chrome_render_view_host_test_harness.h"
+#include "chrome/test/base/testing_profile.h"
+#include "components/content_settings/core/browser/host_content_settings_map.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+namespace {
+
+base::Time GetReferenceTime() {
+  base::Time::Exploded exploded_reference_time;
+  exploded_reference_time.year = 2015;
+  exploded_reference_time.month = 1;
+  exploded_reference_time.day_of_month = 30;
+  exploded_reference_time.day_of_week = 5;
+  exploded_reference_time.hour = 11;
+  exploded_reference_time.minute = 0;
+  exploded_reference_time.second = 0;
+  exploded_reference_time.millisecond = 0;
+
+  base::Time out_time;
+  EXPECT_TRUE(
+      base::Time::FromLocalExploded(exploded_reference_time, &out_time));
+  return out_time;
+}
+
+}  // namespace
+
+class MediaEngagementScoreTest : public ChromeRenderViewHostTestHarness {
+ public:
+  MediaEngagementScoreTest() : score_(&test_clock, GURL(), nullptr) {}
+
+  void SetUp() override {
+    ChromeRenderViewHostTestHarness::SetUp();
+    test_clock.SetNow(GetReferenceTime());
+  }
+
+  base::SimpleTestClock test_clock;
+
+ protected:
+  MediaEngagementScore score_;
+
+  void VerifyScore(const MediaEngagementScore& score,
+                   int expected_visits,
+                   int expected_media_playbacks,
+                   base::Time expected_last_media_playback_time) {
+    EXPECT_EQ(expected_visits, score.visits());
+    EXPECT_EQ(expected_media_playbacks, score.media_playbacks());
+    EXPECT_EQ(expected_last_media_playback_time,
+              score.last_media_playback_time());
+  }
+
+  void UpdateScore(MediaEngagementScore* score) {
+    test_clock.SetNow(test_clock.Now() + base::TimeDelta::FromHours(1));
+
+    score->IncrementVisits();
+    score->IncrementMediaPlaybacks();
+  }
+
+  void TestScoreInitializesAndUpdates(
+      std::unique_ptr<base::DictionaryValue> score_dict,
+      int expected_visits,
+      int expected_media_playbacks,
+      base::Time expected_last_media_playback_time) {
+    std::unique_ptr<base::DictionaryValue> copy(score_dict->DeepCopy());
+    MediaEngagementScore initial_score(&test_clock, GURL(),
+                                       std::move(score_dict));
+    VerifyScore(initial_score, expected_visits, expected_media_playbacks,
+                expected_last_media_playback_time);
+
+    // Updating the score dict should return false, as the score shouldn't
+    // have changed at this point.
+    EXPECT_FALSE(initial_score.UpdateScoreDict(copy.get()));
+
+    // Increment the scores and check that the values were stored correctly.
+    UpdateScore(&initial_score);
+    EXPECT_TRUE(initial_score.UpdateScoreDict(copy.get()));
+    MediaEngagementScore updated_score(&test_clock, GURL(), std::move(copy));
+    VerifyScore(updated_score, initial_score.visits(),
+                initial_score.media_playbacks(), test_clock.Now());
+  }
+
+  void SetVisits(int visits) { score_.visits_ = visits; }
+};
+
+// Test that scores are read / written correctly from / to empty score
+// dictionaries.
+TEST_F(MediaEngagementScoreTest, EmptyDictionary) {
+  std::unique_ptr<base::DictionaryValue> dict(new base::DictionaryValue());
+  TestScoreInitializesAndUpdates(std::move(dict), 0, 0, base::Time());
+}
+
+// Test that scores are read / written correctly from / to partially empty
+// score dictionaries.
+TEST_F(MediaEngagementScoreTest, PartiallyEmptyDictionary) {
+  std::unique_ptr<base::DictionaryValue> dict(new base::DictionaryValue());
+  dict->SetInteger(MediaEngagementScore::kVisitsKey, 2);
+
+  TestScoreInitializesAndUpdates(std::move(dict), 2, 0, base::Time());
+}
+
+// Test that scores are read / written correctly from / to populated score
+// dictionaries.
+TEST_F(MediaEngagementScoreTest, PopulatedDictionary) {
+  std::unique_ptr<base::DictionaryValue> dict(new base::DictionaryValue());
+  dict->SetInteger(MediaEngagementScore::kVisitsKey, 1);
+  dict->SetInteger(MediaEngagementScore::kMediaPlaybacksKey, 2);
+  dict->SetDouble(MediaEngagementScore::kLastMediaPlaybackTimeKey,
+                  test_clock.Now().ToInternalValue());
+
+  TestScoreInitializesAndUpdates(std::move(dict), 1, 2, test_clock.Now());
+}
+
+// Test getting and commiting the score works correctly with different
+// origins.
+TEST_F(MediaEngagementScoreTest, ContentSettingsMultiOrigin) {
+  GURL url("http://www.google.com");
+
+  // Replace |score_| with one with an actual URL, and with a settings map.
+  HostContentSettingsMap* settings_map =
+      HostContentSettingsMapFactory::GetForProfile(profile());
+  score_ = MediaEngagementScore(&test_clock, url, settings_map);
+
+  // Verify the score is originally zero, try incrementing and storing
+  // the score.
+  VerifyScore(score_, 0, 0, base::Time());
+  score_.IncrementVisits();
+  UpdateScore(&score_);
+  score_.Commit();
+
+  // Now confirm the correct score is present on the same origin,
+  // but zero for a different origin.
+  GURL same_origin("http://www.google.com/page1");
+  GURL different_origin("http://www.google.co.uk");
+  score_ = MediaEngagementScore(&test_clock, url, settings_map);
+  MediaEngagementScore same_origin_score =
+      MediaEngagementScore(&test_clock, same_origin, settings_map);
+  MediaEngagementScore different_origin_score =
+      MediaEngagementScore(&test_clock, different_origin, settings_map);
+  VerifyScore(score_, 2, 1, test_clock.Now());
+  VerifyScore(same_origin_score, 2, 1, test_clock.Now());
+  VerifyScore(different_origin_score, 0, 0, base::Time());
+}
+
+// Test that the total score is calculated correctly.
+TEST_F(MediaEngagementScoreTest, TotalScoreCalculation) {
+  EXPECT_EQ(0, score_.GetTotalScore());
+  UpdateScore(&score_);
+  EXPECT_EQ(0, score_.GetTotalScore());
+  UpdateScore(&score_);
+  SetVisits(MediaEngagementScore::kScoreMinVisits);
+  double expected_score =
+      score_.media_playbacks() / (double)MediaEngagementScore::kScoreMinVisits;
+  EXPECT_EQ(expected_score, score_.GetTotalScore());
+}
