2efa368fc88e0436593351e11da0b3455080b7e9
Yutaka Hirano <yhirano@chromium.org>
2017-06-26 23:03:44 +0900

    Introduce BufferingDataPipeWriter
    This CL introduces BufferingDataPipeWriter which is conceptually the
    writer end of a data pipe accompanied with buffers. This class is planned
    to be used for fetch API support for preloading.
    
    Bug: 652228
    Change-Id: I538de868ff77f772e826345c700c654763659e03

---
+++ third_party/WebKit/Source/platform/loader/fetch/BufferingDataPipeWriterTest.cpp
@@ -0,0 +1,88 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/loader/fetch/BufferingDataPipeWriter.h"
+
+#include <memory>
+#include <random>
+
+#include "base/message_loop/message_loop.h"
+#include "platform/testing/TestingPlatformSupport.h"
+#include "platform/wtf/PtrUtil.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace blink {
+namespace {
+
+constexpr uint64_t u64(uint64_t x) {
+  return x;
+}
+
+TEST(BufferingDataPipeWriterTest, WriteMany) {
+  ScopedTestingPlatformSupport<TestingPlatformSupportWithMockScheduler>
+      platform;
+  constexpr int kCapacity = 4096;
+
+  base::MessageLoop message_loop;
+
+  std::linear_congruential_engine<
+      uint32_t, (u64(0x581f42d) << 32) + u64(0x4c957f2d), 12345, u64(1) << 48>
+      engine(99);
+
+  mojo::ScopedDataPipeProducerHandle producer;
+  mojo::ScopedDataPipeConsumerHandle consumer;
+  MojoCreateDataPipeOptions options;
+  options.struct_size = sizeof(MojoCreateDataPipeOptions);
+  options.flags = MOJO_CREATE_DATA_PIPE_OPTIONS_FLAG_NONE;
+  options.element_num_bytes = 1;
+  options.capacity_num_bytes = kCapacity;
+
+  MojoResult result = mojo::CreateDataPipe(&options, &producer, &consumer);
+  ASSERT_EQ(MOJO_RESULT_OK, result);
+
+  constexpr size_t total = kCapacity * 3;
+  constexpr size_t writing_chunk_size = 5;
+  constexpr size_t reading_chunk_size = 7;
+  std::string input;
+
+  for (size_t i = 0; i < total; ++i)
+    input += static_cast<char>(engine() % 26 + 'A');
+
+  auto writer = WTF::MakeUnique<BufferingDataPipeWriter>(
+      std::move(producer), platform->CurrentThread()->GetWebTaskRunner());
+
+  for (size_t i = 0; i < total;) {
+    size_t size = std::min(total - i, writing_chunk_size);
+    ASSERT_TRUE(writer->Write(input.data() + i, size));
+
+    i += size;
+  }
+
+  writer->Finish();
+
+  std::string output;
+
+  while (true) {
+    constexpr auto kNone = MOJO_READ_DATA_FLAG_NONE;
+    char buffer[reading_chunk_size] = {};
+    uint32_t size = reading_chunk_size;
+    result = mojo::ReadDataRaw(consumer.get(), buffer, &size, kNone);
+
+    if (result == MOJO_RESULT_SHOULD_WAIT) {
+      platform->RunUntilIdle();
+      continue;
+    }
+    if (result == MOJO_RESULT_FAILED_PRECONDITION)
+      break;
+
+    ASSERT_EQ(MOJO_RESULT_OK, result);
+    for (size_t i = 0; i < size; ++i) {
+      output += buffer[i];
+    }
+  }
+  EXPECT_EQ(output, input);
+}
+
+}  // namespace
+}  // namespace blink
