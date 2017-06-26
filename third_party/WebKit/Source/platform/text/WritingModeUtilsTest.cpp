d3f98ff3595fd3aede421611aff0a79cd17da03a
Xianzhu Wang <wangxianzhu@chromium.org>
2017-06-26 10:55:59 -0700

    Add WritingModeUtils.h to convert logical/physical directions
    Change-Id: I656169b74066aae326b3ea6a64a47bbc7372114a

---
+++ third_party/WebKit/Source/platform/text/WritingModeUtilsTest.cpp
@@ -0,0 +1,13 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/text/WritingModeUtils.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace blink {
+
+// TODO(wangxianzhu): Add tests.
+
+}  // namespace blink
