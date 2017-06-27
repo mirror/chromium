866a3298084539b53567146f7e7dd871666afddb
Dmitry Torokhov <dtor@chromium.org>
2017-06-27 16:56:24 -0700

    Add keycode definition of Assistant key
    The assistant key is not defined in USB HID usage tables (yet?), so we
    use the first available usage from the "Non-USB" range: 0xx000017.
    We will update with the correct usage code if and when we get it
    allocated.
    
    For the DOM code we are using the next available code as well: 0x0B0E
    
    Since we want to invoke the assistant via Chrome accelerator mechanism,
    we also need to plumb it as a VKEY_ASSISTANT with keycode 0x99, which is
    next unmapped keycode on Windows.
    
    There is a slight complication as the new assistant key does not have
    corresponding XKB definition, and we are still using XMB on ChromeOS,
    even though we now consume input-events directly (out "translation" from
    evdev to XMB keycodes is simply adding a constant offset to the event
    code). It is unclear whether there is value in plumbing the assistant
    key though XKB, so let's use basic map to get from Dom code to Dom key
    if XKB lookup fails, with the expectation that we are dealing with
    application launcher or another accelerator, and not a printable
    character affected by keyboard layouts.
    
    BUG=b:36735408
    
    Change-Id: I319f222dd5457224095f5a7cff37afdc0deade18

--- ui/events/ozone/layout/xkb/xkb_keyboard_layout_engine.cc
+++ ui/events/ozone/layout/xkb/xkb_keyboard_layout_engine.cc
@@ -741,7 +741,10 @@ bool XkbKeyboardLayoutEngine::Lookup(DomCode dom_code,
   xkb_keysym_t xkb_keysym;
   uint32_t character = 0;
   if (!XkbLookup(xkb_keycode, xkb_flags, &xkb_keysym, &character))
-    return false;
+    // If we do not have matching legacy Xkb keycode for the Dom code,
+    // we could be dealing with a newer application launcher or similar
+    // key. Let's see if we have a basic mapping for it.
+    return DomCodeToUsLayoutDomKey(dom_code, flags, dom_key, key_code);
 
   // Classify the keysym and convert to DOM and VKEY representations.
   if (xkb_keysym != XKB_KEY_at || (flags & EF_CONTROL_DOWN) == 0) {
