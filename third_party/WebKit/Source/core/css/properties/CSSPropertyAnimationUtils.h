ff10f45593523cb46aab8ec11bfc7d35f3f20870
Jia <jiameng@chromium.org>
2017-06-27 10:54:08 +1000

    Implement parseShorthand for shorthand properties "animation" and "transition".
    This cl contains the following changes
    * API impl of "animation" and "transition".
    * Refactored helper functions and put them into
    - CSSPropertyAnimationUtils. Some function is shared between animation and transition.
    - CSSPropertyAnimationTimingFunctionUtils.
    * Changed CSSPropertyParser to use the new API for these two shorthands.
    - Also removed functions that won't be used.
    * Build files and json to include the new API.
    
    
    Bug:668012
    Change-Id: Ic20898fbd603c5760da4b5afab86aa48edcff600

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAnimationUtils.h
@@ -0,0 +1,33 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CSSPropertyAnimationUtils_h
+#define CSSPropertyAnimationUtils_h
+
+#include "platform/heap/HeapAllocator.h"
+#include "platform/wtf/Allocator.h"
+
+namespace blink {
+
+class CSSParserContext;
+class CSSParserTokenRange;
+class CSSProperty;
+class StylePropertyShorthand;
+
+class CSSPropertyAnimationUtils {
+  STATIC_ONLY(CSSPropertyAnimationUtils);
+
+ public:
+  static bool ConsumeShorthand(const StylePropertyShorthand&,
+                               bool is_transition,
+                               bool use_legacy_parsing,
+                               bool important,
+                               CSSParserTokenRange&,
+                               const CSSParserContext&,
+                               HeapVector<CSSProperty, 256>&);
+};
+
+}  // namespace blink
+
+#endif  // CSSPropertyAnimationUtils_h
