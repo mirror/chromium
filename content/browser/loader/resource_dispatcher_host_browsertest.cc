7629cdf73e32e2b094c8e18ebc6dfcd0a2aa1f99
Pete Williamson <petewil@chromium.org>
2017-06-27 12:33:33 -0700

    Prevent Previews from interfering with Offline Pages
    Some previews can cause problems for offlining. We should disable
    those previews.  This approach uses a NavigationDelegate and a
    WebContentsDelegate to allow the background offliner to disable
    previews that could cause problems for offlining.
    
    Bug:703875
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I519fae1b6f98da99c89ff97cb4b201c60a92b176

--- content/browser/loader/resource_dispatcher_host_browsertest.cc
+++ content/browser/loader/resource_dispatcher_host_browsertest.cc
@@ -609,6 +609,7 @@ class PreviewsStateResourceDispatcherHostDelegate
         subresource_url_seen_(false),
         iframe_url_seen_(false),
         previews_state_(PREVIEWS_OFF),
+        adjusted_previews_state_(PREVIEWS_OFF),
         should_get_previews_state_called_(false) {}
 
   ~PreviewsStateResourceDispatcherHostDelegate() override {}
@@ -637,7 +638,15 @@ class PreviewsStateResourceDispatcherHostDelegate
       EXPECT_FALSE(iframe_url_seen_);
       iframe_url_seen_ = true;
     }
-    EXPECT_EQ(previews_state_, info->GetPreviewsState());
+    EXPECT_TRUE(previews_state_ == info->GetPreviewsState() ||
+                adjusted_previews_state_ == info->GetPreviewsState());
+  }
+
+  void OnResponseStarted(net::URLRequest* request,
+                         content::ResourceContext* resource_context,
+                         content::ResourceResponse* response) override {
+    const ResourceRequestInfo* info = ResourceRequestInfo::ForRequest(request);
+    previews_state_ = info->GetPreviewsState();
   }
 
   void SetDelegate() {
@@ -673,6 +682,13 @@ class PreviewsStateResourceDispatcherHostDelegate
     EXPECT_TRUE(iframe_url_seen_);
   }
 
+  PreviewsState previews_state() { return previews_state_; }
+  // This is used to let the delegate know that the previews state was adjusted
+  // elsewhere (typically by the PreviewsStateWebContentsDelegate).
+  void set_adjusted_previews_state(PreviewsState adjusted_previews_state) {
+    adjusted_previews_state_ = adjusted_previews_state;
+  }
+
  private:
   const GURL main_frame_url_;
   const GURL subresource_url_;
@@ -682,9 +698,30 @@ class PreviewsStateResourceDispatcherHostDelegate
   bool subresource_url_seen_;
   bool iframe_url_seen_;
   PreviewsState previews_state_;
+  PreviewsState adjusted_previews_state_;
   bool should_get_previews_state_called_;
 
   DISALLOW_COPY_AND_ASSIGN(PreviewsStateResourceDispatcherHostDelegate);
+};
+
+// Testing version of a web contents delegate which implements
+// AdjustPreviewsStateForNavigation, and can control the previews_state when
+// AdjustPreviewsState is called.
+class PreviewsStateWebContentsDelegate : public WebContentsDelegate {
+ public:
+  PreviewsStateWebContentsDelegate() : previews_state_(PREVIEWS_UNSPECIFIED) {}
+
+  void AdjustPreviewsStateForNavigation(
+      PreviewsState* previews_state) override {
+    *previews_state = previews_state_;
+  }
+
+  void set_previews_state(PreviewsState previews_state) {
+    previews_state_ = previews_state;
+  }
+
+ private:
+  PreviewsState previews_state_;
 };
 
 }  // namespace
@@ -727,6 +764,12 @@ class PreviewsStateResourceDispatcherHostBrowserTest
                        CheckResourcesRequested,
                    base::Unretained(delegate_.get()),
                    should_get_previews_state_called));
+  }
+
+  PreviewsState previews_state() { return delegate_->previews_state(); }
+
+  void set_adjusted_previews_state(PreviewsState adjusted_previews_state) {
+    delegate_->set_adjusted_previews_state(adjusted_previews_state);
   }
 
  private:
@@ -804,6 +847,40 @@ IN_PROC_BROWSER_TEST_F(PreviewsStateResourceDispatcherHostBrowserTest,
                                                   true);
   tab_observer.Wait();
   CheckResourcesRequested(false);
+}
+
+// Test that navigating calls GetPreviewsState with SERVER_LOFI_ON when
+// we have a web contents delegate responding to
+// AdjustPreviewsStateForNavigation.
+IN_PROC_BROWSER_TEST_F(PreviewsStateResourceDispatcherHostBrowserTest,
+                       PreviewsStateLimiting) {
+  // Navigate with ShouldEnableLoFiMode returning true.
+  Reset(SERVER_LOFI_ON | CLIENT_LOFI_ON | SERVER_LITE_PAGE_ON);
+
+  // Set up the web contents delegate to respond to
+  // AdjustPreviewsStateForNavigation, and to return a specified
+  // previews_state_to_allow.
+  PreviewsState allowed_previews_states = SERVER_LOFI_ON;
+  PreviewsStateWebContentsDelegate previews_state_web_contents_delegate;
+  previews_state_web_contents_delegate.set_previews_state(
+      allowed_previews_states);
+  shell()->web_contents()->SetDelegate(&previews_state_web_contents_delegate);
+
+  // Make sure the DispatcherHostDelegate knows about the adjusted previews
+  // state.
+  set_adjusted_previews_state(allowed_previews_states);
+
+  NavigateToURLBlockUntilNavigationsComplete(
+      shell(), embedded_test_server()->GetURL("/page_with_iframe.html"), 1);
+
+  CheckResourcesRequested(true);
+
+  // Check preview_state got limited to allowed states.  The OnResponseStarted()
+  // override should catch the preveiws state, and set the member variable, so
+  // we can check it here.  Note that we don't call CheckResourcesRequested,
+  // which would not have seen what it expected because we changed the previews
+  // state.
+  EXPECT_EQ(SERVER_LOFI_ON, previews_state());
 }
 
 namespace {
