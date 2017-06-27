ca1ee87da3cf0af529bb6eb5b24e3ce78f1432fe
Colin Blundell <blundell@chromium.org>
2017-06-27 12:59:19 +0200

    Embed Service Manager in //ios/web
    This CL adds an embedding of the Service Manager in //ios/web, such that
    it is now possible to host and connect to services in //ios/{web, chrome}.
    
    The infrastructure is based on that for //content but simplified and
    adapted to the context of //ios, where:
    - Multiprocess does not exist.
    - Connection filters are not relevant, as Chrome IPC is not relevant.
    - There is no short-term plan to migrate to using ServiceManager's main
      function.
    
    Concretely, this CL does the following things:
    (1) Adds ServiceManagerConnection as an //ios/web public interface. This
    is the gateway for connecting to services from //ios.
    (2) Adds ServiceManagerContext, which embeds an in-process ServiceManager.
    (3) Adds ServiceManagerConnectionImpl, which gets a connection to the
    ServiceManager.
    (4) Adds the ability for //ios/web and its clients to register global
    services.
    (5) Adds an Earl Grey test in //ios/web/shell that hosts the echo service
    and tests the ability to connect to that service. This test also serves
    as an example usage of the infrastructure added in this CL.
    (6) Adds shell grit resources in order to allow the shell to package
    overlays of service manifests.
    
    Support for per-user services will be added in a followup CL.
    
    Bug: 731588
    Change-Id: I90321de772a753c84fa4c28ad7021f084dd3e75a

---
+++ ios/web/shell/test/service_manager_egtest.mm
@@ -0,0 +1,67 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <EarlGrey/EarlGrey.h>
+
+#include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
+#import "ios/testing/wait_util.h"
+#include "ios/web/public/service_manager_connection.h"
+#import "ios/web/shell/test/earl_grey/shell_earl_grey.h"
+#import "ios/web/shell/test/earl_grey/web_shell_test_case.h"
+#include "services/service_manager/public/cpp/connector.h"
+#include "services/test/echo/public/interfaces/echo.mojom.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace {
+const char* kTestInput = "Livingston, I presume";
+bool gEchoStringCallbackCalled = false;
+
+void OnEchoString(echo::mojom::EchoPtr echo, const std::string& echoed_input) {
+  GREYAssert(kTestInput == echoed_input,
+             @"Unexpected string passed to echo callback: %s",
+             echoed_input.c_str());
+  gEchoStringCallbackCalled = true;
+}
+
+void WaitForEchoStringCallback() {
+  GREYCondition* condition =
+      [GREYCondition conditionWithName:@"Wait for echo string callback"
+                                 block:^BOOL {
+                                   return gEchoStringCallbackCalled;
+                                 }];
+  GREYAssert([condition waitWithTimeout:testing::kWaitForUIElementTimeout],
+             @"Failed waiting for echo callback");
+}
+}
+
+// Service Manager test cases for the web shell.
+@interface ServiceManagerTestCase : WebShellTestCase
+@end
+
+@implementation ServiceManagerTestCase
+
+// Tests that it is possible to connect to an embedded service that was
+// registered by web_shell.
+- (void)testConnectionToEmbeddedService {
+  // Connect to the echo service and bind an Echo instance.
+  echo::mojom::EchoPtr echo;
+  web::ServiceManagerConnection* connection =
+      web::ServiceManagerConnection::GetForProcess();
+  DCHECK(connection);
+  connection->GetConnector()->BindInterface("echo", mojo::MakeRequest(&echo));
+
+  // Call EchoString, making sure to keep our end of the connection alive
+  // until the callback is received.
+  echo::mojom::Echo* raw_echo = echo.get();
+  raw_echo->EchoString(kTestInput,
+                       base::BindOnce(&OnEchoString, base::Passed(&echo)));
+
+  WaitForEchoStringCallback();
+}
+
+@end
