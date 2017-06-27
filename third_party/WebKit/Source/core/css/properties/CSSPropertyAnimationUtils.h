48ef079f921e1780ee4c3e0f092dcd959cfcf436
Jia <jiameng@chromium.org>
2017-06-27 16:27:47 +1000

    Implement parseShorthand for shorthand properties "animation" and "transition".
    This cl contains the following changes
    * API impl of "animation" and "transition".
    * Refactored helper functions and put them into
    - CSSPropertyAnimationUtils. Some function is shared between animation and transition.
    - CSSPropertyAnimationTimingFunctionUtils.
    * Changed CSSPropertyParser to use the new API for these two shorthands.
    - Also removed functions that won't be used.
    * Build files and json to include the new API.
    
    
    Bug:668012
    Diff: https://gist.github.com/93e73871e2aa4edfec1e7f9807ff3980/revisions
    Change-Id: Ic20898fbd603c5760da4b5afab86aa48edcff600

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAnimationUtils.h
@@ -0,0 +1,68 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CSSPropertyAnimationUtils_h
+#define CSSPropertyAnimationUtils_h
+
+#include "core/StylePropertyShorthand.h"
+#include "core/css/CSSInitialValue.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+
+#include "platform/heap/HeapAllocator.h"
+#include "platform/wtf/Allocator.h"
+
+namespace blink {
+
+class CSSPropertyAnimationUtils {
+  STATIC_ONLY(CSSPropertyAnimationUtils);
+
+ public:
+  template <typename Func, typename... Args>
+  static bool ConsumeShorthand(const StylePropertyShorthand& shorthand,
+                               std::vector<CSSValueList*>& longhands,
+                               Func callback,
+                               CSSParserTokenRange& range,
+                               Args... args) {
+    const unsigned longhand_count = shorthand.length();
+    DCHECK_LE(longhand_count, 8u);
+    longhands.resize(longhand_count);
+
+    for (size_t i = 0; i < longhand_count; ++i)
+      longhands[i] = CSSValueList::CreateCommaSeparated();
+
+    do {
+      bool parsed_longhand[8] = {false};
+      do {
+        bool found_property = false;
+        for (size_t i = 0; i < longhand_count; ++i) {
+          if (parsed_longhand[i])
+            continue;
+
+          CSSValue* value = callback(shorthand.properties()[i], range, args...);
+          if (value) {
+            parsed_longhand[i] = true;
+            found_property = true;
+            longhands[i]->Append(*value);
+            break;
+          }
+        }
+        if (!found_property)
+          return false;
+      } while (!range.AtEnd() && range.Peek().GetType() != kCommaToken);
+
+      // TODO(timloh): This will make invalid longhands, see crbug.com/386459
+      for (size_t i = 0; i < longhand_count; ++i) {
+        if (!parsed_longhand[i])
+          longhands[i]->Append(*CSSInitialValue::Create());
+        parsed_longhand[i] = false;
+      }
+    } while (CSSPropertyParserHelpers::ConsumeCommaIncludingWhitespace(range));
+
+    return true;
+  }
+};
+
+}  // namespace blink
+
+#endif  // CSSPropertyAnimationUtils_h
