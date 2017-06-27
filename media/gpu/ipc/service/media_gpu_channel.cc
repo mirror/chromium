6e5fb8cd279e12438d88b2b5fd8bd4e4682e342e
Sunny Sachanandani <sunnyps@chromium.org>
2017-06-27 16:10:41 -0700

    gpu: Remove message filter destructor crash keys and asserts.
    Now that the crbug.com/729483 is fixed, remove the crash keys for
    message filter destructors and asserts added in the course of debugging
    that issue.
    
    TBR=jbauman,rsesek,sandersd,alokp,rockot,boliu,robertshield
    
    Bug:729483
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I1b145d1719af6e2712cc907449082d5d3e3fc40b

--- media/gpu/ipc/service/media_gpu_channel.cc
+++ media/gpu/ipc/service/media_gpu_channel.cc
@@ -75,12 +75,7 @@ class MediaGpuChannelFilter : public IPC::MessageFilter {
   }
 
  private:
-  ~MediaGpuChannelFilter() override {
-    // TODO(sunnyps): Remove once crbug.com/729483 has been resolved.
-    base::debug::SetCrashKeyToStackTrace(
-        gpu::crash_keys::kMediaGpuChannelFilterTrace,
-        base::debug::StackTrace());
-  }
+  ~MediaGpuChannelFilter() override = default;
 
   IPC::Channel* channel_;
   base::UnguessableToken channel_token_;
