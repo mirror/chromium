04a809d0d12e26ccb293e417742333d8800e609d
Jun Cai <juncai@chromium.org>
2017-06-27 11:12:13 -0700

    Add GetReading() member function to sensor_reading.*
    This CL adds a GetReading() member function at
    //device/generic_sensor/public/cpp/sensor_reading.* so that it can be
    used by both //content/renderer/device_sensors and Blink code.
    
    Bug:727788
    Change-Id: I9d03b64e5cf191738e87bbe29fd7611654f3fd5b

--- device/generic_sensor/public/cpp/sensor_reading.cc
+++ device/generic_sensor/public/cpp/sensor_reading.cc
@@ -4,6 +4,14 @@
 
 #include "device/generic_sensor/public/cpp/sensor_reading.h"
 
+#include "device/base/synchronization/shared_memory_seqlock_buffer.h"
+
+namespace {
+
+constexpr int kMaxReadAttemptsCount = 10;
+
+}  // namespace
+
 namespace device {
 
 SensorReading::SensorReading() = default;
@@ -18,6 +26,33 @@ uint64_t SensorReadingSharedBuffer::GetOffset(mojom::SensorType type) {
   return (static_cast<uint64_t>(mojom::SensorType::LAST) -
           static_cast<uint64_t>(type)) *
          sizeof(SensorReadingSharedBuffer);
+}
+
+bool SensorReadingSharedBuffer::GetReading(SensorReading* result) const {
+  int read_attempts = 0;
+  while (!TryReadFromBuffer(result)) {
+    // Only try to read this many times before failing to avoid waiting here
+    // very long in case of contention with the writer.
+    if (++read_attempts == kMaxReadAttemptsCount) {
+      // Failed to successfully read, presumably because the hardware
+      // thread was taking unusually long. Data in |result| was not updated
+      // and was simply left what was there before.
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool SensorReadingSharedBuffer::TryReadFromBuffer(
+    device::SensorReading* result) const {
+  const device::OneWriterSeqLock& seq_lock = seqlock.value();
+  auto version = seq_lock.ReadBegin();
+  auto reading_data = reading;
+  if (seq_lock.ReadRetry(version))
+    return false;
+  *result = reading;
+  return true;
 }
 
 }  // namespace device
