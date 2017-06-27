1d111377f96bb3a3969aba4bc7dfe1cbb3a660cb
Catherine Chung <catherinechung@google.com>
2017-06-27 08:24:14 -0700

    Add new tab feature engagement tracker
    To use the FeatureEngagementTracker to observe the New Tab and related 
    actions, the NewTabFeatureEngagementTracker class was created.
    
    Things that were changed in this CL:
    * Added the NewTabInProductHelp trial to the fieldtrial json
    * Added a pref to track the user's total active session time
    * Created a NewTabFeatureEngagementTracker class, which contains
    methods to notify the IPH server when events happen and if the help
    UI should be triggered.
    
    Note: This CL may be modified in the future when the frontend and
    backend of this project are combined. It will trigger a bubble dialog
    delegate view when the conditions are correct. 
    
    Related CLs:
    * The Observer for DesktopSessionDurationTracker
    https://chromium-review.googlesource.com/c/546540/
    * Event constants that are used in NewTabFeatureEngagementTracker
    https://chromium-review.googlesource.com/c/546699/
    * Implementation of the NewTabFeatureEngagementTracker
    https://chromium-review.googlesource.com/c/549074/
    
    Design Doc: go/newtab-d-o
    
    Bug: 734132
    Change-Id: I8654ef001f3fb50fbd8bb8c4f215dffcfe403762

---
+++ chrome/browser/feature_engagement_tracker/new_tab_feature_engagement_tracker.h
@@ -0,0 +1,59 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_FEATURE_ENGAGEMENT_TRACKER_NEW_TAB_FEATURE_ENGAGEMENT_TRACKER_H_
+#define CHROME_BROWSER_FEATURE_ENGAGEMENT_TRACKER_NEW_TAB_FEATURE_ENGAGEMENT_TRACKER_H_
+
+#include "base/time/time.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/feature_engagement_tracker/feature_engagement_tracker_factory.h"
+#include "chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.h"
+#include "chrome/browser/profiles/profile.h"
+#include "components/feature_engagement_tracker/public/feature_engagement_tracker.h"
+
+using metrics::DesktopSessionDurationTracker;
+
+namespace feature_engagement_tracker {
+
+class NewTabFeatureEngagementTracker
+    : public DesktopSessionDurationTracker::Observer {
+ public:
+  static void Initialize();
+  static bool IsInitialized();
+  static NewTabFeatureEngagementTracker* Get();
+
+  NewTabFeatureEngagementTracker();
+  ~NewTabFeatureEngagementTracker() override {
+    duration_tracker_->RemoveObserver(observer_.release());
+  }
+
+  // Notifies the feature engagement tracker that a new tab was opened.
+  void NotifyNewTabOpened();
+  // Notifies the feature engagement tracker that the omnibox has been used.
+  void NotifyOmniboxNavigation();
+  // Notifies the feature engagement tracker that the session time is up.
+  void NotifySessionTime();
+
+ private:
+  bool CheckSessionTime();
+  void OnSessionEnded(base::TimeDelta delta) override;
+  void UpdateSessionTime(base::TimeDelta elapsed);
+
+  int elapsed_session_time_;
+  //  duration_tracker_ is owned by NewTabFeatureEngagementTracker, and thus
+  //  lives as long as the class.
+  DesktopSessionDurationTracker* duration_tracker_;
+  //  profile_manager_ is owned by NewTabFeatureEngagementTracker, and thus
+  //  lives as long as the class.
+  ProfileManager* profile_manager_;
+  //  observer_ is released to duration_tracker_, and lives until it is
+  //  removed by duration_tracker_
+  std::unique_ptr<NewTabFeatureEngagementTracker> observer_;
+
+  DISALLOW_COPY_AND_ASSIGN(NewTabFeatureEngagementTracker);
+};
+
+}  // namespace feature_engagement_tracker
+
+#endif  // CHROME_BROWSER_FEATURE_ENGAGEMENT_TRACKER_NEW_TAB_FEATURE_ENGAGEMENT_TRACKER_H_
