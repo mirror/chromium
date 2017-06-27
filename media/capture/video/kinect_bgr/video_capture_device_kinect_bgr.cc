7a9c9926870c1f9840db4b09873cd887e0a30559
Christian Fremerey <chfremer@chromium.org>
2017-06-27 12:50:23 -0700

    WebRTC with background removal demo
    NOT INTENDED FOR LANDING
    
    This CL serves as a "branch" for development of an experimental demo for
    doing WebRTC video with background removal.
    
    Change-Id: If3fcf74dc2713b9dc79c852afd71883bcd0636e6
    Bug:

---
+++ media/capture/video/kinect_bgr/video_capture_device_kinect_bgr.cc
@@ -0,0 +1,45 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/capture/video/kinect_bgr/video_capture_device_kinect_bgr.h"
+
+namespace media {
+
+VideoCaptureDeviceKinectBgr::VideoCaptureDeviceKinectBgr() = default;
+
+VideoCaptureDeviceKinectBgr::~VideoCaptureDeviceKinectBgr() = default;
+
+// static
+void VideoCaptureDeviceKinectBgr::GetSupportedFormats(
+    VideoCaptureFormats* formats) {
+  formats->push_back(
+      VideoCaptureFormat(gfx::Size(1280, 720), 30.0f, PIXEL_FORMAT_I420));
+}
+
+void VideoCaptureDeviceKinectBgr::AllocateAndStart(
+    const VideoCaptureParams& params,
+    std::unique_ptr<VideoCaptureDevice::Client> client) {
+  NOTIMPLEMENTED();
+}
+
+void VideoCaptureDeviceKinectBgr::StopAndDeAllocate() {
+  NOTIMPLEMENTED();
+}
+
+void VideoCaptureDeviceKinectBgr::TakePhoto(TakePhotoCallback callback) {
+  NOTIMPLEMENTED();
+}
+
+void VideoCaptureDeviceKinectBgr::GetPhotoState(
+    GetPhotoStateCallback callback) {
+  NOTIMPLEMENTED();
+}
+
+void VideoCaptureDeviceKinectBgr::SetPhotoOptions(
+    mojom::PhotoSettingsPtr settings,
+    VideoCaptureDevice::SetPhotoOptionsCallback callback) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace media
