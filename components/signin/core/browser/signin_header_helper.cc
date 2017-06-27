4c46d16a0fc3ebda67f50ba1a6016ef393f79e79
David Roger <droger@google.com>
2017-06-27 11:05:42 +0200

    [Signin] Handle Google-Accounts-SignOut for Dice
    Bug:730589
    Change-Id: I334e9a884a2d40e8f614870a77c9db0e1cc67cb5

--- components/signin/core/browser/signin_header_helper.cc
+++ components/signin/core/browser/signin_header_helper.cc
@@ -90,17 +90,19 @@ SigninHeaderHelper::ParseAccountConsistencyResponseHeader(
     const std::string& header_value) {
   ResponseHeaderDictionary dictionary;
   for (const base::StringPiece& field :
-       base::SplitStringPiece(header_value, ",", base::KEEP_WHITESPACE,
+       base::SplitStringPiece(header_value, ",", base::TRIM_WHITESPACE,
                               base::SPLIT_WANT_NONEMPTY)) {
     size_t delim = field.find_first_of('=');
     if (delim == std::string::npos) {
       DLOG(WARNING) << "Unexpected Gaia header field '" << field << "'.";
       continue;
     }
-    dictionary[field.substr(0, delim).as_string()] = net::UnescapeURLComponent(
-        field.substr(delim + 1).as_string(),
-        net::UnescapeRule::PATH_SEPARATORS |
-            net::UnescapeRule::URL_SPECIAL_CHARS_EXCEPT_PATH_SEPARATORS);
+    dictionary.insert(
+        {field.substr(0, delim).as_string(),
+         net::UnescapeURLComponent(
+             field.substr(delim + 1).as_string(),
+             net::UnescapeRule::PATH_SEPARATORS |
+                 net::UnescapeRule::URL_SPECIAL_CHARS_EXCEPT_PATH_SEPARATORS)});
   }
   return dictionary;
 }
@@ -155,8 +157,14 @@ ManageAccountsParams BuildManageAccountsParams(
 }
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
-DiceResponseParams BuildDiceResponseParams(const std::string& header_value) {
-  return DiceHeaderHelper::BuildDiceResponseParams(header_value);
+DiceResponseParams BuildDiceSigninResponseParams(
+    const std::string& header_value) {
+  return DiceHeaderHelper::BuildDiceSigninResponseParams(header_value);
+}
+
+DiceResponseParams BuildDiceSignoutResponseParams(
+    const std::string& header_value) {
+  return DiceHeaderHelper::BuildDiceSignoutResponseParams(header_value);
 }
 #endif
 
