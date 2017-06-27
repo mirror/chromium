66294f186ee9ec2b51eaae3f258930e7a4db9f08
Ramin Halavati <rhalavati@chromium.org>
2017-06-27 13:58:25 +0200

    Unittest added to Network Traffic Annotation Auditor.
    Network traffic annotation auditor have no test support. A unittest is added to
    cover some tests.
    
    Bug:656607
    Bug:690323
    Change-Id: I93b05cf7ec4d89fca07166b230a4fc314fc438e7

---
+++ tools/traffic_annotation/auditor/traffic_annotation_auditor_unittest.cc
@@ -0,0 +1,25 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "tools/traffic_annotation/auditor/traffic_annotation_auditor.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using namespace testing;
+
+class TrafficAnnotationAuditorTest : public ::testing::Test {};
+
+// Tests if the two hash computation functions have the same result.
+TEST_F(TrafficAnnotationAuditorTest, HashFunctionCheck) {
+#define TEST_HASH_CODE(X)                                    \
+  EXPECT_EQ(traffic_annotation_auditor::ComputeHashValue(X), \
+            net::DefineNetworkTrafficAnnotation(X, "").unique_id_hash_code);
+
+  TEST_HASH_CODE("test")
+  TEST_HASH_CODE("unique_id")
+  TEST_HASH_CODE("123_id")
+  TEST_HASH_CODE("ID123")
+  TEST_HASH_CODE("a_unique_looooooooooooooooooooooooooooooooooooooooooooong_id")
+#undef TEST_HASH_CODE
+}
