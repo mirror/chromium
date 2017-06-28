18021d5bd7aed46703a279a64f5fd03d0b7ecda0
Siddhartha <ssid@chromium.org>
2017-06-28 16:39:13 -0700

    [memory-infra] Move the tests related to tracing observer into service/
    Move the tests related to tracing observer, peak detector and periodic
    schduler into client_process_impl_unittests.
    Removed test CallbackCalledOnFailure since it is covered by
    DumpWithoutInitializingTracing
    
    BUG=718809
    
    Change-Id: Idb2fb0ed566e54ab6f6be825f844e2ffab9b4d16

--- base/trace_event/memory_dump_manager_test_utils.h
+++ base/trace_event/memory_dump_manager_test_utils.h
@@ -8,9 +8,54 @@
 #include "base/bind.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "base/trace_event/memory_dump_request_args.h"
+#include "testing/gmock/include/gmock/gmock.h"
 
 namespace base {
 namespace trace_event {
+
+// GTest matchers for MemoryDumpRequestArgs arguments.
+MATCHER(IsDetailedDump, "") {
+  return arg.level_of_detail == MemoryDumpLevelOfDetail::DETAILED;
+}
+
+MATCHER(IsLightDump, "") {
+  return arg.level_of_detail == MemoryDumpLevelOfDetail::LIGHT;
+}
+
+MATCHER(IsBackgroundDump, "") {
+  return arg.level_of_detail == MemoryDumpLevelOfDetail::BACKGROUND;
+}
+
+class MockMemoryDumpProvider : public MemoryDumpProvider {
+ public:
+  MOCK_METHOD0(Destructor, void());
+  MOCK_METHOD2(OnMemoryDump,
+               bool(const MemoryDumpArgs& args, ProcessMemoryDump* pmd));
+  MOCK_METHOD1(PollFastMemoryTotal, void(uint64_t* memory_total));
+  MOCK_METHOD0(SuspendFastMemoryPolling, void());
+
+  MockMemoryDumpProvider();
+
+  ~MockMemoryDumpProvider() override;
+
+  bool enable_mock_destructor;
+};
+
+MockMemoryDumpProvider::MockMemoryDumpProvider()
+    : enable_mock_destructor(false) {
+  ON_CALL(*this, OnMemoryDump(testing::_, testing::_))
+      .WillByDefault(
+          testing::Invoke([](const MemoryDumpArgs& args,
+                             ProcessMemoryDump* pmd) -> bool { return true; }));
+  ON_CALL(*this, PollFastMemoryTotal(testing::_))
+      .WillByDefault(testing::Invoke(
+          [](uint64_t* memory_total) -> void { NOTREACHED(); }));
+}
+
+MockMemoryDumpProvider::~MockMemoryDumpProvider() {
+  if (enable_mock_destructor)
+    Destructor();
+}
 
 void RequestGlobalDumpForInProcessTesting(const MemoryDumpRequestArgs& args) {
   MemoryDumpManager::GetInstance()->CreateProcessDump(
