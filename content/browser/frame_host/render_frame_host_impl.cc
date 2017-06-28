91f9d3f208f5baa4038f388cb41b2654dab4e684
Dave Tapuska <dtapuska@chromium.org>
2017-06-28 15:55:41 -0400

    Teach render_host about a mojo WidgetInputHandler.
    Implement a WidgetInputHandler as a legacy interface over Chrome IPC.
    This will then allow us to dynamically swap it with a mojo backed one.
    
    This change does not provide a mojo backed implementation of this
    interface but just an implementation of the interface backed by Chrome IPC.
    The new implementation will come in another CL.
    
    BUG=722928
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I03bb42524cec501a8b76f517629e0b3e3e76bba0

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -773,11 +773,7 @@ blink::WebPageVisibilityState RenderFrameHostImpl::GetVisibilityState() {
 }
 
 bool RenderFrameHostImpl::Send(IPC::Message* message) {
-  if (IPC_MESSAGE_ID_CLASS(message->type()) == InputMsgStart) {
-    return GetRenderWidgetHost()->input_router()->SendInput(
-        base::WrapUnique(message));
-  }
-
+  DCHECK(IPC_MESSAGE_ID_CLASS(message->type()) != InputMsgStart);
   return GetProcess()->Send(message);
 }
 
@@ -3311,8 +3307,7 @@ void RenderFrameHostImpl::SetUpMojoIfNeeded() {
   if (base::FeatureList::IsEnabled(features::kMojoInputMessages)) {
     GetRemoteInterfaces()->GetInterface(&frame_input_handler_);
   } else {
-    legacy_frame_input_handler_.reset(
-        new LegacyIPCFrameInputHandler(this, routing_id_));
+    legacy_frame_input_handler_.reset(new LegacyIPCFrameInputHandler(this));
   }
 }
 
