9f069dc5898d450e10aff356ac146306ade30f11
Catherine Chung <catherinechung@google.com>
2017-06-28 15:45:44 -0700

    Add new tab feature engagement tracker
    To use the FeatureEngagementTracker to observe the New Tab and related
    actions, the NewTabFeatureEngagementTracker class was created.
    
    Things that were changed in this CL:
    * Added the NewTabInProductHelp trial to the fieldtrial json
    * Added a pref to track the user's total active session time
    * Created a NewTabFeatureEngagementTracker class, which contains
    methods to notify the IPH server when events happen and if the help
    UI should be triggered.
    
    Related CLs:
    * The Observer for DesktopSessionDurationTracker
    https://chromium-review.googlesource.com/c/546540/
    * Event constants that are used in NewTabFeatureEngagementTracker
    https://chromium-review.googlesource.com/c/546699/
    * Implementation of the NewTabFeatureEngagementTracker
    https://chromium-review.googlesource.com/c/549074/
    * Addition to fieldtrial_testing.json
    https://chromium-review.googlesource.com/c/550864/
    
    Design Doc: go/newtab-d-o
    
    Bug: 734132
    Change-Id: I8654ef001f3fb50fbd8bb8c4f215dffcfe403762

---
+++ chrome/browser/feature_engagement_tracker/new_tab_feature_engagement_tracker_unittest.cc
@@ -0,0 +1,144 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/feature_engagement_tracker/new_tab_feature_engagement_tracker.h"
+
+#include "base/test/scoped_feature_list.h"
+#include "chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.h"
+#include "chrome/common/pref_names.h"
+#include "chrome/test/base/testing_browser_process.h"
+#include "chrome/test/base/testing_profile.h"
+#include "chrome/test/base/testing_profile_manager.h"
+#include "components/feature_engagement_tracker/public/feature_constants.h"
+#include "components/feature_engagement_tracker/public/feature_engagement_tracker.h"
+#include "components/variations/variations_params_manager.h"
+#include "content/public/test/test_browser_thread_bundle.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using metrics::DesktopSessionDurationTracker;
+
+namespace feature_engagement_tracker {
+
+class MockNewTabFeatureEngagementTracker
+    : public NewTabFeatureEngagementTracker {
+ public:
+  explicit MockNewTabFeatureEngagementTracker(Profile* profile)
+      : testing_profile_(profile),
+        notify_new_tab_opened_(false),
+        notify_omnibox_navigation_(false),
+        notify_session_time_(false),
+        promo_seen_(false) {}
+
+  bool promo_seen() const {
+    return !notify_new_tab_opened_ && notify_omnibox_navigation_ &&
+           notify_session_time_;
+  }
+
+  // feature_engagement_tracker::NewTabFeatureEngagementTracker
+  Profile* GetProfile() override { return testing_profile_; }
+
+  void NotifyNewTabOpened() override { notify_new_tab_opened_ = true; }
+
+  void NotifyOmniboxNavigation() override { notify_omnibox_navigation_ = true; }
+
+  void NotifySessionTime() override { notify_session_time_ = true; }
+
+ private:
+  bool promo_seen_;
+  bool notify_new_tab_opened_;
+  bool notify_omnibox_navigation_;
+  bool notify_session_time_;
+  Profile* testing_profile_;
+
+  DISALLOW_COPY_AND_ASSIGN(MockNewTabFeatureEngagementTracker);
+};
+
+class MockFeatureEngagementTracker : public FeatureEngagementTracker {
+ public:
+  MockFeatureEngagementTracker() {}
+
+  // feature_engagement_tracker::FeatureEngagementTracker
+  bool ShouldTriggerHelpUI(const base::Feature& feature) override {
+    return true;
+  }
+};
+
+class NewTabFeatureEngagementTrackerTest : public testing::Test {
+ public:
+  NewTabFeatureEngagementTrackerTest()
+      : manager_(TestingBrowserProcess::GetGlobal()) {}
+  ~NewTabFeatureEngagementTrackerTest() override {}
+
+  // testing::Test:
+  void SetUp() override { ASSERT_TRUE(manager_.SetUp()); }
+
+  TestingProfileManager* manager() { return &manager_; }
+
+ private:
+  content::TestBrowserThreadBundle thread_bundle_;
+  TestingProfileManager manager_;
+
+  DISALLOW_COPY_AND_ASSIGN(NewTabFeatureEngagementTrackerTest);
+};
+
+TEST_F(NewTabFeatureEngagementTrackerTest, TestNotifyNewTabOpened) {
+  TestingProfile* profile = manager()->CreateTestingProfile("profile_1");
+  ASSERT_TRUE(profile);
+
+  DesktopSessionDurationTracker::Initialize();
+  MockNewTabFeatureEngagementTracker* instance =
+      new MockNewTabFeatureEngagementTracker(profile);
+
+  instance->NotifyNewTabOpened();
+
+  // If a new tab is created, then the educational promo
+  // should not be seen.
+  EXPECT_FALSE(instance->promo_seen());
+
+  delete instance;
+}
+
+TEST_F(NewTabFeatureEngagementTrackerTest, TestOmniboxNavigation) {
+  TestingProfile* profile = manager()->CreateTestingProfile("profile_2");
+  ASSERT_TRUE(profile);
+
+  DesktopSessionDurationTracker::Initialize();
+  MockNewTabFeatureEngagementTracker* instance =
+      new MockNewTabFeatureEngagementTracker(profile);
+
+  EXPECT_FALSE(instance->promo_seen());
+
+  // Two active hours have passed.
+  instance->NotifySessionTime();
+
+  instance->NotifyOmniboxNavigation();
+
+  // If two hours have passed and the user has navigated in the omnibox,
+  // then the educational promo should be seen.
+  EXPECT_TRUE(instance->promo_seen());
+
+  delete instance;
+}
+
+TEST_F(NewTabFeatureEngagementTrackerTest, TestNotifySessionTime) {
+  TestingProfile* profile = manager()->CreateTestingProfile("profile_3");
+  ASSERT_TRUE(profile);
+
+  DesktopSessionDurationTracker::Initialize();
+  MockNewTabFeatureEngagementTracker* instance_ =
+      new MockNewTabFeatureEngagementTracker(profile);
+
+  EXPECT_FALSE(instance_->promo_seen());
+
+  instance_->NotifyOmniboxNavigation();
+  instance_->NotifySessionTime();
+
+  // If the user has navigated in the omnibox and NotifySessionTime is called,
+  // then the educational promo should be seen.
+  EXPECT_TRUE(instance_->promo_seen());
+
+  delete instance_;
+}
+
+}  // namespace feature_engagement_tracker
