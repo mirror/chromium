0079ec3a74d083db7d6f419bfb52f9d9c9f29ace
khmel <khmel@google.com>
2017-06-28 11:27:24 -0700

    arc: Implement Android settings link for AOSP images.
    When ARC started without the Play Store support there is no way for
    the user to activate Android settings. We need implement corresponded
    section in Chrome settings.
    
    TEST=Manually + extra browser tests
    BUG=b/62945384
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Id630d73fd4ebffafb39666af5654e6985282cc93

--- chrome/browser/resources/settings/basic_page/basic_page.js
+++ chrome/browser/resources/settings/basic_page/basic_page.js
@@ -20,6 +20,11 @@ Polymer({
 
     showAndroidApps: Boolean,
 
+    havePlayStoreApp: Boolean,
+
+    /** @type {!AndroidAppsInfo|undefined} */
+    androidAppsInfo: Object,
+
     showChromeCleanup: {
       type: Boolean,
       value: function() {
@@ -93,6 +98,10 @@ Polymer({
     this.addEventListener('chrome-cleanup-dismissed', function(e) {
       this.showChromeCleanup = false;
     }.bind(this));
+
+    cr.addWebUIListener(
+        'android-apps-info-update', this.androidAppsInfoUpdate_.bind(this));
+    settings.AndroidAppsBrowserProxyImpl.getInstance().requestAndroidAppsInfo();
   },
 
   /**
@@ -179,13 +188,28 @@ Polymer({
   },
 
   /**
+   * @param {!AndroidAppsInfo} info
+   * @private
+   */
+  androidAppsInfoUpdate_: function(info) {
+    this.androidAppsInfo = info;
+  },
+
+  /**
    * @return {boolean}
    * @private
    */
   shouldShowAndroidApps_: function() {
     var visibility = /** @type {boolean|undefined} */ (
         this.get('pageVisibility.androidApps'));
-    return this.showAndroidApps && this.showPage_(visibility);
+    if (!this.showAndroidApps || !this.showPage_(visibility)) {
+      return false;
+    }
+
+    // Section is invisible in case we don't have the Play Store app and
+    // settings app is not yet available.
+    return this.havePlayStoreApp ||
+        (this.androidAppsInfo && this.androidAppsInfo.settingsAppAvailable);
   },
 
   /**
