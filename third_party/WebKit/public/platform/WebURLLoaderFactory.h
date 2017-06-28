eecc76c541ae2a857eca182234232cf61a01ed80
Yutaka Hirano <yhirano@chromium.org>
2017-06-28 18:24:38 +0900

    Introduce WebURLLoaderFactory
    This CL introduces WebURLLoaderFactory, a factory of WebURLLoader.
    
    Bug: 712913
    Change-Id: I1806b76d2283894d03364ec4fdf1dd2a9012bff9

---
+++ third_party/WebKit/public/platform/WebURLLoaderFactory.h
@@ -0,0 +1,28 @@
+#ifndef WebURLLoaderFactory_h
+#define WebURLLoaderFactory_h
+
+#include "WebCommon.h"
+
+#include <memory>
+
+namespace blink {
+
+class WebTaskRunner;
+class WebURLLoader;
+class WebURLRequest;
+
+// WebURLLoaderFactory is a factory of WebURLLoader.
+class WebURLLoaderFactory {
+  virtual ~WebURLLoaderFactory() = default;
+
+  // Creates a loader.
+  // This function takes a request. It should be the same request as the
+  // request given to WebURLLoader::LoadAsynchronously and
+  // WebURLLoader::LoadSynchronously.
+  virtual std::unique_ptr<WebURLLoader> CreateLoader(const WebURLRequest&,
+                                                     WebTaskRunner*) = 0;
+};
+
+}  // namespace blink
+
+#endif  // WebURLLoaderFactory_h
