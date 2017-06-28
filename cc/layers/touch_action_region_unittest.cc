7fc84600bf4ff335e0bd939f24d29b42411e8a2c
Hayley Ferr <hayleyferr@chromium.org>
2017-06-28 16:17:03 -0400

    Compute the most restricted TouchAction on cc side
    Within InputHandlerProxy::HitTestTouchEvent, |touch_action| is
    passed by pointer to EventListenerTypeForTouchStartOrMoveAt. Within
    EventListenerTypeForTouchStartOrMoveAt, GetMostRestrictedTouchAction is
    called and returns the most restricted TouchAction for the point that
    has been touched.
    
    This change will ultimately allow cc side to inform browser of the most
    restricted bit, possibly saving unecessary work from blink.
    Logic still needed: Sending the restricted TouchAction to browser.
    
    Bug: 727866
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I4777a71be0097482c13ae93406e22f072abfd9bc

---
+++ cc/layers/touch_action_region_unittest.cc
@@ -0,0 +1,79 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "cc/layers/touch_action_region.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace cc {
+namespace {
+
+TEST(TouchActionRegionTest, GetWhiteListedTouchActionMapOverlapToZero) {
+  TouchAction touch_action;
+  TouchActionRegion touch_action_region;
+  touch_action_region.Union(kTouchActionPanLeft, gfx::Rect(0, 0, 50, 50));
+  touch_action_region.Union(kTouchActionPanRight, gfx::Rect(25, 25, 25, 25));
+  touch_action =
+      touch_action_region.GetWhiteListedTouchAction(gfx::Point(10, 10));
+  // The point is only in PanLeft, so the result is PanLeft.
+  EXPECT_EQ(kTouchActionPanLeft, touch_action);
+  touch_action =
+      touch_action_region.GetWhiteListedTouchAction(gfx::Point(30, 30));
+  // The point is in both PanLeft and PanRight, and those actions have no
+  // common components, so the result is None.
+  EXPECT_EQ(kTouchActionNone, touch_action);
+  touch_action =
+      touch_action_region.GetWhiteListedTouchAction(gfx::Point(60, 60));
+  // The point is in neither PanLeft nor PanRight, so the result is Max since
+  // the default touch action is auto.
+  EXPECT_EQ(kTouchActionMax, touch_action);
+}
+
+TEST(TouchActionRegionTest, GetWhiteListedTouchActionMapOverlapToNonZero) {
+  TouchAction touch_action;
+  TouchActionRegion touch_action_region;
+  touch_action_region.Union(kTouchActionPanX, gfx::Rect(0, 0, 50, 50));
+  touch_action_region.Union(kTouchActionPanRight, gfx::Rect(25, 25, 25, 25));
+  touch_action =
+      touch_action_region.GetWhiteListedTouchAction(gfx::Point(10, 10));
+  // The point is only in PanX, so the result is PanX.
+  EXPECT_EQ(kTouchActionPanX, touch_action);
+  touch_action =
+      touch_action_region.GetWhiteListedTouchAction(gfx::Point(30, 30));
+  // The point is in both PanX and PanRight, and PanRight is a common component,
+  // so the result is PanRight.
+  EXPECT_EQ(kTouchActionPanRight, touch_action);
+  touch_action =
+      touch_action_region.GetWhiteListedTouchAction(gfx::Point(60, 60));
+  // The point is neither PanX nor PanRight, so the result is Max since the
+  // default touch action is auto.
+  EXPECT_EQ(kTouchActionMax, touch_action);
+}
+
+TEST(TouchActionRegionTest, GetWhiteListedTouchActionEmptyMap) {
+  TouchActionRegion touch_action_region;
+  TouchAction touch_action =
+      touch_action_region.GetWhiteListedTouchAction(gfx::Point(10, 10));
+  // The result is Max since the map is empty and the default touch
+  // action is auto.
+  EXPECT_EQ(kTouchActionMax, touch_action);
+}
+
+TEST(TouchActionRegionTest, GetWhiteListedTouchActionSingleMapEntry) {
+  TouchActionRegion touch_action_region;
+  TouchAction touch_action;
+  touch_action_region.Union(kTouchActionPanUp, gfx::Rect(0, 0, 50, 50));
+  touch_action =
+      touch_action_region.GetWhiteListedTouchAction(gfx::Point(10, 10));
+  // The point is only in PanUp, so the result is PanUp.
+  EXPECT_EQ(kTouchActionPanUp, touch_action);
+  touch_action =
+      touch_action_region.GetWhiteListedTouchAction(gfx::Point(60, 60));
+  // The point is not in PanUp, so the result is Max since the default touch
+  // action is auto.
+  EXPECT_EQ(kTouchActionMax, touch_action);
+}
+
+}  // namespace
+}  // namespace cc
