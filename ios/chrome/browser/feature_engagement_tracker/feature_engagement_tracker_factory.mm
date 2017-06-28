1594d9fad8d3fbc37a0a2f3de026f0aa8e7c8559
Cooper Knaak <cooperknaak@google.com>
2017-06-28 15:12:31 -0700

    [ios] FeatureEngagementTrackerFactory interface and stubs.
    Added FeatureEngagementTrackerFactory interface and implementation
    stubs. Updated BUILD.gn files to link and compile
    FeatureEngagementTrackerFactory.
    
    R=edchin@chromium.org, gchatz@chromium.org, sdefresne@chromium.org
    
    Bug: none
    Change-Id: I32cff6432e90a1d781fbe4c0b1b6efa1496ddc67

---
+++ ios/chrome/browser/feature_engagement_tracker/feature_engagement_tracker_factory.mm
@@ -0,0 +1,48 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/feature_engagement_tracker/feature_engagement_tracker_factory.h"
+
+#include "base/memory/singleton.h"
+#include "components/feature_engagement_tracker/public/feature_engagement_tracker.h"
+#include "components/keyed_service/ios/browser_state_dependency_manager.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+// static
+FeatureEngagementTrackerFactory*
+FeatureEngagementTrackerFactory::GetInstance() {
+  return base::Singleton<FeatureEngagementTrackerFactory>::get();
+}
+
+// static
+feature_engagement_tracker::FeatureEngagementTracker*
+FeatureEngagementTrackerFactory::GetForBrowserState(
+    ios::ChromeBrowserState* state) {
+  return nullptr;
+}
+
+FeatureEngagementTrackerFactory::FeatureEngagementTrackerFactory()
+    : BrowserStateKeyedServiceFactory(
+          "FeatureEngagementTracker",
+          BrowserStateDependencyManager::GetInstance()) {}
+
+FeatureEngagementTrackerFactory::~FeatureEngagementTrackerFactory() = default;
+
+std::unique_ptr<KeyedService>
+FeatureEngagementTrackerFactory::BuildServiceInstanceFor(
+    web::BrowserState* context) const {
+  return nullptr;
+}
+
+// Finds which browser state to use. If |context| is an incognito browser
+// state, it returns the non-incognito state. Thus, feature engagement events
+// are tracked even in incognito tabs.
+web::BrowserState* FeatureEngagementTrackerFactory::GetBrowserStateToUse(
+    web::BrowserState* context) const {
+  return nullptr;
+}
