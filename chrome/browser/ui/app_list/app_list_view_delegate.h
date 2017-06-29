e7b7246107feaeabf2878371d1463aa89e9cf10e
Qiang Xu <warx@chromium.org>
2017-06-29 13:49:32 -0700

    cros: Get wallpaper prominent colors for closed searchbox view
    changes:
    In crrev.com/2943333003, multiple/six wallpaper prominent colors
    are extracted in ash::WallpaperController. This CL wants to get dark
    muted color and light vibrant color for app_list::SearchBoxView.
    (1) Define remote observer mojo::WallpaperObserver for this purpose.
    (2) Make AppListViewDelegate in chrome/ as a observer, and notify
    wallpaper color changes for search box model. And add
    WallpaperProminentColorsChanged() API in SearchBoxModelObserver to
    update the search box view.
    (3) if wallpaper prominent color is available, using dark muted color
    extracted form the wallpaper for G, microphone as well as the label;
    using (light vibrant color 100% + #FFF 70%) at 100% opacity for
    searchbox background color.
    
    R: xiyuan@chromium.org, xdai@chromium.org, jamescook@chromium.org
    Bug: 733391, 732492
    TEST: tested with and without --enable-features=EnableFullscreenAppList
    flag, screenshot: https://screenshot.googleplex.com/awwbVHnjDPV
    Change-Id: I07890f48ea02518bbfd948035d9e4d4a01d56c65

--- chrome/browser/ui/app_list/app_list_view_delegate.h
+++ chrome/browser/ui/app_list/app_list_view_delegate.h
@@ -11,6 +11,7 @@
 #include <string>
 #include <vector>
 
+#include "ash/public/interfaces/wallpaper.mojom.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
@@ -21,6 +22,8 @@
 #include "components/search_engines/template_url_service_observer.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "mojo/public/cpp/bindings/associated_binding.h"
+#include "mojo/public/cpp/bindings/binding.h"
 #include "ui/app_list/app_list_view_delegate.h"
 
 namespace app_list {
@@ -41,6 +44,7 @@ class Profile;
 
 class AppListViewDelegate : public app_list::AppListViewDelegate,
                             public app_list::StartPageObserver,
+                            public ash::mojom::WallpaperObserver,
                             public HotwordClient,
                             public content::NotificationObserver,
                             public TemplateURLServiceObserver {
@@ -87,6 +91,9 @@ class AppListViewDelegate : public app_list::AppListViewDelegate,
   void OnTemplateURLServiceChanged() override;
 
  private:
+  // Callback for ash::mojom::GetWallpaperColors.
+  void OnGetWallpaperColorsCallback(const std::vector<SkColor>& colors);
+
   // Updates the speech webview and start page for the current |profile_|.
   void SetUpSearchUI();
 
@@ -98,6 +105,10 @@ class AppListViewDelegate : public app_list::AppListViewDelegate,
   void OnSpeechSoundLevelChanged(int16_t level) override;
   void OnSpeechRecognitionStateChanged(
       app_list::SpeechRecognitionState new_state) override;
+
+  // Overridden from ash::mojom::WallpaperObserver:
+  void OnWallpaperColorsChanged(
+      const std::vector<SkColor>& prominent_colors) override;
 
   // Overridden from HotwordClient:
   void OnHotwordStateChanged(bool started) override;
@@ -141,6 +152,12 @@ class AppListViewDelegate : public app_list::AppListViewDelegate,
 
   // Registers for NOTIFICATION_APP_TERMINATING to unload custom launcher pages.
   content::NotificationRegistrar registrar_;
+
+  // The binding this instance uses to implement mojom::WallpaperObserver.
+  mojo::AssociatedBinding<ash::mojom::WallpaperObserver> observer_binding_;
+
+  // Ash's mojom::WallpaperController.
+  ash::mojom::WallpaperControllerPtr wallpaper_controller_ptr_;
 
   DISALLOW_COPY_AND_ASSIGN(AppListViewDelegate);
 };
