cc42c8ca76820155268e043a4c9ba0a0e2e8feec
Sunny Sachanandani <sunnyps@chromium.org>
2017-06-29 14:32:40 -0700

    gpu: Add crash key to tell if context is virtual.
    Virtual contexts are used because of the blacklist but also dynamically
    for platform specific reasons. It's hard to reason if the context is
    virtual by looking at a crash report. This CL adds a crash key with that
    information.
    
    R=piman,alokp,rsesek
    BUG=724999
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I05333742ac98027545efeecc3e5311c390924a40

--- gpu/ipc/service/gpu_command_buffer_stub.cc
+++ gpu/ipc/service/gpu_command_buffer_stub.cc
@@ -8,6 +8,7 @@
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/debug/crash_logging.h"
 #include "base/hash.h"
 #include "base/json/json_writer.h"
 #include "base/macros.h"
@@ -35,6 +36,7 @@
 #include "gpu/command_buffer/service/service_utils.h"
 #include "gpu/command_buffer/service/sync_point_manager.h"
 #include "gpu/command_buffer/service/transfer_buffer_manager.h"
+#include "gpu/config/gpu_crash_keys.h"
 #include "gpu/ipc/common/gpu_messages.h"
 #include "gpu/ipc/service/gpu_channel.h"
 #include "gpu/ipc/service/gpu_channel_manager.h"
@@ -268,6 +270,10 @@ bool GpuCommandBufferStub::OnMessageReceived(const IPC::Message& message) {
   TRACE_EVENT1(TRACE_DISABLED_BY_DEFAULT("devtools.timeline"), "GPUTask",
                "data", DevToolsChannelData::CreateForChannel(channel()));
   FastSetActiveURL(active_url_, active_url_hash_, channel_);
+
+  // TODO(sunnyps): Should this use ScopedCrashKey instead?
+  base::debug::SetCrashKeyValue(crash_keys::kGPUGLContextIsVirtual,
+                                use_virtualized_gl_context_ ? "1" : "0");
 
   bool have_context = false;
   // Ensure the appropriate GL context is current before handling any IPC
@@ -701,6 +707,10 @@ bool GpuCommandBufferStub::Initialize(
     // group
     share_group_ = channel_->share_group();
   }
+
+  // TODO(sunnyps): Should this use ScopedCrashKey instead?
+  base::debug::SetCrashKeyValue(crash_keys::kGPUGLContextIsVirtual,
+                                use_virtualized_gl_context_ ? "1" : "0");
 
   scoped_refptr<gl::GLContext> context;
   if (use_virtualized_gl_context_ && share_group_) {
