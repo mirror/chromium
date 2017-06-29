0326b204954aae64fc61c81fe7e0d8cee74bcdac
Vladimir Levin <vmpstr@chromium.org>
2017-06-29 16:00:31 -0700

    Remove PaintShader::sk_shader() uses outside of PaintFlags.
    This patch removes the sk_shader uses from outside of the flags in order
    to restrict access and construction of the SkShader. The next patch
    here would remove SkShader construction altogether and instead store
    the needed parameters on the PaintShader. Then the shader creation can
    be restricted to raster time only (since discardable system would be
    able to use/access the images required directly from the paint shader).
    
    This would also unblock the gif animations on the compositor for the
    same reason (ie, it will be able to get a PaintImage directly from
    PaintShader).
    
    R=enne@chromium.org
    Bug:728359
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I480195299205e8a7c01298095d5662b63980f80b

--- cc/paint/paint_shader.cc
+++ cc/paint/paint_shader.cc
@@ -11,7 +11,12 @@
 namespace cc {
 
 std::unique_ptr<PaintShader> PaintShader::MakeColor(SkColor color) {
-  return base::WrapUnique(new PaintShader(nullptr, color));
+  auto shader = base::WrapUnique(new PaintShader(kColor));
+
+  // Just one color. Store it in the fallback color. Easy.
+  shader->fallback_color_ = color;
+
+  return shader;
 }
 
 std::unique_ptr<PaintShader> PaintShader::MakeLinearGradient(
@@ -23,10 +28,25 @@ std::unique_ptr<PaintShader> PaintShader::MakeLinearGradient(
     uint32_t flags,
     const SkMatrix* local_matrix,
     SkColor fallback_color) {
-  return base::WrapUnique(
-      new PaintShader(SkGradientShader::MakeLinear(points, colors, pos, count,
-                                                   mode, flags, local_matrix),
-                      fallback_color));
+  auto shader = base::WrapUnique(new PaintShader(kLinearGradient));
+
+  // There are always two points, the start and the end.
+  shader->start_point_ = points[0];
+  shader->end_point_ = points[1];
+  // There are |count| number of colors, which is at least 2.
+  DCHECK_GE(count, 2);
+  shader->colors_.assign(colors, colors + count);
+  // There are |count| number of positions, but it could be nullptr.
+  if (pos)
+    shader->positions_.assign(pos, pos + count);
+  shader->tx_ = shader->ty_ = mode;
+  // |local_matrix| can be nullptr.
+  if (local_matrix)
+    shader->local_matrix_ = *local_matrix;
+  shader->flags_ = flags;
+  shader->fallback_color_ = fallback_color;
+
+  return shader;
 }
 
 std::unique_ptr<PaintShader> PaintShader::MakeRadialGradient(
@@ -34,15 +54,30 @@ std::unique_ptr<PaintShader> PaintShader::MakeRadialGradient(
     SkScalar radius,
     const SkColor colors[],
     const SkScalar pos[],
-    int color_count,
+    int count,
     SkShader::TileMode mode,
     uint32_t flags,
     const SkMatrix* local_matrix,
     SkColor fallback_color) {
-  return base::WrapUnique(new PaintShader(
-      SkGradientShader::MakeRadial(center, radius, colors, pos, color_count,
-                                   mode, flags, local_matrix),
-      fallback_color));
+  auto shader = base::WrapUnique(new PaintShader(kRadialGradient));
+
+  shader->center_ = center;
+  shader->start_radius_ = shader->end_radius_ = radius;
+  // There are |count| number of colors, which is at least 2.
+  DCHECK_GE(count, 2);
+  shader->colors_.assign(colors, colors + count);
+  // There are |count| number of positions, but it could be nullptr.
+  if (pos)
+    shader->positions_.assign(pos, pos + count);
+  shader->tx_ = shader->ty_ = mode;
+  shader->flags_ = flags;
+  // |local_matrix| can be nullptr.
+  if (local_matrix)
+    shader->local_matrix_ = *local_matrix;
+  shader->flags_ = flags;
+  shader->fallback_color_ = fallback_color;
+
+  return shader;
 }
 
 std::unique_ptr<PaintShader> PaintShader::MakeTwoPointConicalGradient(
@@ -52,16 +87,31 @@ std::unique_ptr<PaintShader> PaintShader::MakeTwoPointConicalGradient(
     SkScalar end_radius,
     const SkColor colors[],
     const SkScalar pos[],
-    int color_count,
+    int count,
     SkShader::TileMode mode,
     uint32_t flags,
     const SkMatrix* local_matrix,
     SkColor fallback_color) {
-  return base::WrapUnique(
-      new PaintShader(SkGradientShader::MakeTwoPointConical(
-                          start, start_radius, end, end_radius, colors, pos,
-                          color_count, mode, flags, local_matrix),
-                      fallback_color));
+  auto shader = base::WrapUnique(new PaintShader(kTwoPointConicalGradient));
+
+  shader->start_point_ = start;
+  shader->end_point_ = end;
+  shader->start_radius_ = start_radius;
+  shader->end_radius_ = end_radius;
+  // There are |count| number of colors, which is at least 2.
+  DCHECK_GE(count, 2);
+  shader->colors_.assign(colors, colors + count);
+  // There are |count| number of positions, but it could be nullptr.
+  if (pos)
+    shader->positions_.assign(pos, pos + count);
+  shader->tx_ = shader->ty_ = mode;
+  // |local_matrix| can be nullptr.
+  if (local_matrix)
+    shader->local_matrix_ = *local_matrix;
+  shader->flags_ = flags;
+  shader->fallback_color_ = fallback_color;
+
+  return shader;
 }
 
 std::unique_ptr<PaintShader> PaintShader::MakeSweepGradient(
@@ -69,14 +119,26 @@ std::unique_ptr<PaintShader> PaintShader::MakeSweepGradient(
     SkScalar cy,
     const SkColor colors[],
     const SkScalar pos[],
-    int color_count,
+    int count,
     uint32_t flags,
     const SkMatrix* local_matrix,
     SkColor fallback_color) {
-  return base::WrapUnique(new PaintShader(
-      SkGradientShader::MakeSweep(cx, cy, colors, pos, color_count, flags,
-                                  local_matrix),
-      fallback_color));
+  auto shader = base::WrapUnique(new PaintShader(kTwoPointConicalGradient));
+
+  shader->center_ = SkPoint::Make(cx, cy);
+  // There are |count| number of colors, which is at least 2.
+  DCHECK_GE(count, 2);
+  shader->colors_.assign(colors, colors + count);
+  // There are |count| number of positions, but it could be nullptr.
+  if (pos)
+    shader->positions_.assign(pos, pos + count);
+  // |local_matrix| can be nullptr.
+  if (local_matrix)
+    shader->local_matrix_ = *local_matrix;
+  shader->flags_ = flags;
+  shader->fallback_color_ = fallback_color;
+
+  return shader;
 }
 
 std::unique_ptr<PaintShader> PaintShader::MakeImage(
@@ -84,8 +146,16 @@ std::unique_ptr<PaintShader> PaintShader::MakeImage(
     SkShader::TileMode tx,
     SkShader::TileMode ty,
     const SkMatrix* local_matrix) {
-  return base::WrapUnique(new PaintShader(
-      image->makeShader(tx, ty, local_matrix), SK_ColorTRANSPARENT));
+  auto shader = base::WrapUnique(new PaintShader(kImage));
+
+  shader->image_ = std::move(image);
+  shader->tx_ = tx;
+  shader->ty_ = ty;
+  // |local_matrix| can be nullptr.
+  if (local_matrix)
+    shader->local_matrix_ = *local_matrix;
+
+  return shader;
 }
 
 std::unique_ptr<PaintShader> PaintShader::MakePaintRecord(
@@ -94,22 +164,81 @@ std::unique_ptr<PaintShader> PaintShader::MakePaintRecord(
     SkShader::TileMode tx,
     SkShader::TileMode ty,
     const SkMatrix* local_matrix) {
-  return base::WrapUnique(new PaintShader(
-      SkShader::MakePictureShader(ToSkPicture(std::move(record), tile), tx, ty,
-                                  local_matrix, nullptr),
-      SK_ColorTRANSPARENT));
+  auto shader = base::WrapUnique(new PaintShader(kPaintRecord));
+
+  shader->record_ = std::move(record);
+  shader->tile_ = tile;
+  shader->tx_ = tx;
+  shader->ty_ = ty;
+
+  // |local_matrix| can be nullptr.
+  if (local_matrix)
+    shader->local_matrix_ = *local_matrix;
+
+  return shader;
 }
 
-PaintShader::PaintShader(sk_sp<SkShader> shader, SkColor fallback_color)
-    : sk_shader_(shader ? std::move(shader)
-                        : SkShader::MakeColorShader(fallback_color)) {
-  DCHECK(sk_shader_);
-}
+PaintShader::PaintShader(Type type) : shader_type_(type) {}
 PaintShader::PaintShader(const PaintShader& other) = default;
 PaintShader::PaintShader(PaintShader&& other) = default;
 PaintShader::~PaintShader() = default;
 
 PaintShader& PaintShader::operator=(const PaintShader& other) = default;
 PaintShader& PaintShader::operator=(PaintShader&& other) = default;
+
+sk_sp<SkShader> PaintShader::GetSkShader() const {
+  if (cached_shader_)
+    return cached_shader_;
+
+  switch (shader_type_) {
+    case kColor:
+      // This will be handled by the fallback check below.
+      break;
+    case kLinearGradient: {
+      SkPoint points[2] = {start_point_, end_point_};
+      cached_shader_ = SkGradientShader::MakeLinear(
+          points, colors_.data(),
+          positions_.empty() ? nullptr : positions_.data(), colors_.size(), tx_,
+          flags_, local_matrix_ ? &*local_matrix_ : nullptr);
+      break;
+    }
+    case kRadialGradient:
+      cached_shader_ = SkGradientShader::MakeRadial(
+          center_, start_radius_, colors_.data(),
+          positions_.empty() ? nullptr : positions_.data(), colors_.size(), tx_,
+          flags_, local_matrix_ ? &*local_matrix_ : nullptr);
+      break;
+    case kTwoPointConicalGradient:
+      cached_shader_ = SkGradientShader::MakeTwoPointConical(
+          start_point_, start_radius_, end_point_, end_radius_, colors_.data(),
+          positions_.empty() ? nullptr : positions_.data(), colors_.size(), tx_,
+          flags_, local_matrix_ ? &*local_matrix_ : nullptr);
+      break;
+    case kSweepGradient:
+      cached_shader_ = SkGradientShader::MakeSweep(
+          center_.x(), center_.y(), colors_.data(),
+          positions_.empty() ? nullptr : positions_.data(), colors_.size(),
+          flags_, local_matrix_ ? &*local_matrix_ : nullptr);
+      break;
+    case kImage:
+      cached_shader_ = image_->makeShader(
+          tx_, ty_, local_matrix_ ? &*local_matrix_ : nullptr);
+      break;
+    case kPaintRecord:
+      cached_shader_ = SkShader::MakePictureShader(
+          ToSkPicture(record_, tile_), tx_, ty_,
+          local_matrix_ ? &*local_matrix_ : nullptr, nullptr);
+      break;
+    case kShaderCount:
+      NOTREACHED();
+      break;
+  }
+
+  // If we didn't create a shader for whatever reason, create a fallback color
+  // one.
+  if (!cached_shader_)
+    cached_shader_ = SkShader::MakeColorShader(fallback_color_);
+  return cached_shader_;
+}
 
 }  // namespace cc
