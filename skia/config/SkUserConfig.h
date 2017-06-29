7299460340a439362639b0980ae325d7a62f8693
Florin Malita <fmalita@chromium.org>
2017-06-29 13:22:00 -0400

    Add guard for upcoming Skia change
    SK_SUPPORT_LEGACY_2PTCONICAL_GRADIENT, for
    https://skia-review.googlesource.com/c/21262.
    Change-Id: I9cd76767c71d01efc39a57177005a575c28ddff5

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -220,6 +220,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_SWIZZLE_SHADER
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_2PTCONICAL_GRADIENT
+#define SK_SUPPORT_LEGACY_2PTCONICAL_GRADIENT
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
