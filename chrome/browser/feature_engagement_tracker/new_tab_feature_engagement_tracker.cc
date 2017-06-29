f7f2c8e36f41d44ba559cf66fc5c7d41b4d63adf
Catherine Chung <catherinechung@google.com>
2017-06-28 19:22:55 -0700

    Add new tab feature engagement tracker
    To use the FeatureEngagementTracker to observe the New Tab and related
    actions, the NewTabFeatureEngagementTracker class was created.
    
    Things that were changed in this CL:
    * Added the NewTabInProductHelp trial to the fieldtrial json
    * Added a pref to track the user's total active session time
    * Created a NewTabFeatureEngagementTracker class, which contains
    methods to notify the IPH server when events happen and if the help
    UI should be triggered.
    
    Related CLs:
    * The Observer for DesktopSessionDurationTracker
    https://chromium-review.googlesource.com/c/546540/
    * Event constants that are used in NewTabFeatureEngagementTracker
    https://chromium-review.googlesource.com/c/546699/
    * Implementation of the NewTabFeatureEngagementTracker
    https://chromium-review.googlesource.com/c/549074/
    * Addition to fieldtrial_testing.json
    https://chromium-review.googlesource.com/c/550864/
    
    Design Doc: go/newtab-d-o
    
    Bug: 734132
    Change-Id: I8654ef001f3fb50fbd8bb8c4f215dffcfe403762

---
+++ chrome/browser/feature_engagement_tracker/new_tab_feature_engagement_tracker.cc
@@ -0,0 +1,121 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/feature_engagement_tracker/new_tab_feature_engagement_tracker.h"
+
+#include "base/time/time.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/feature_engagement_tracker/feature_engagement_tracker_factory.h"
+#include "chrome/browser/metrics/desktop_session_duration/desktop_session_duration_tracker.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/common/pref_names.h"
+#include "components/feature_engagement_tracker/public/event_constants.h"
+#include "components/feature_engagement_tracker/public/feature_constants.h"
+#include "components/feature_engagement_tracker/public/feature_engagement_tracker.h"
+#include "components/prefs/pref_service.h"
+#include "ui/base/fullscreen_win.h"
+
+using metrics::DesktopSessionDurationTracker;
+
+namespace {
+
+const int kTwoHoursInSeconds = 7200;
+feature_engagement_tracker::NewTabFeatureEngagementTracker* n_instance =
+    nullptr;
+
+}  // namespace
+
+namespace feature_engagement_tracker {
+
+// static
+void NewTabFeatureEngagementTracker::Initialize() {
+  n_instance = new NewTabFeatureEngagementTracker();
+}
+
+// static
+bool NewTabFeatureEngagementTracker::IsInitialized() {
+  return n_instance != nullptr;
+}
+
+// static
+NewTabFeatureEngagementTracker* NewTabFeatureEngagementTracker::Get() {
+  DCHECK(n_instance);
+  return n_instance;
+}
+
+NewTabFeatureEngagementTracker::NewTabFeatureEngagementTracker()
+    : duration_tracker_(DesktopSessionDurationTracker::Get()),
+      elapsed_session_time_(0) {
+  duration_tracker_->AddObserver(this);
+}
+
+~NewTabFeatureEngagementTracker() override {
+  duration_tracker_->RemoveObserver(this);
+}
+
+void NewTabFeatureEngagementTracker::NotifyNewTabOpened() {
+  FeatureEngagementTracker* feature_tracker =
+      FeatureEngagementTrackerFactory::GetForBrowserContext(GetProfile());
+  feature_tracker->NotifyEvent(events::kNewTabOpened);
+  if (feature_tracker->ShouldTriggerHelpUI(kIPHNewTabFeature)) {
+    ShowPromo();
+    feature_tracker->Dismissed(kIPHNewTabFeature);
+  }
+}
+
+void NewTabFeatureEngagementTracker::NotifyOmniboxNavigation() {
+  FeatureEngagementTracker* feature_tracker =
+      FeatureEngagementTrackerFactory::GetForBrowserContext(GetProfile());
+  feature_tracker->NotifyEvent(events::kOmniboxInteraction);
+  if (feature_tracker->ShouldTriggerHelpUI(kIPHNewTabFeature)) {
+    ShowPromo();
+    feature_tracker->Dismissed(kIPHNewTabFeature);
+  }
+}
+
+void NewTabFeatureEngagementTracker::NotifySessionTime() {
+  FeatureEngagementTracker* feature_tracker =
+      FeatureEngagementTrackerFactory::GetForBrowserContext(GetProfile());
+  feature_tracker->NotifyEvent(events::kSessionTime);
+  if (!ui::IsFullScreenMode()) {
+    if (feature_tracker->ShouldTriggerHelpUI(kIPHNewTabFeature)) {
+      ShowPromo();
+      feature_tracker->Dismissed(kIPHNewTabFeature);
+    }
+  }
+}
+
+Profile* NewTabFeatureEngagementTracker::GetProfile() {
+  return ProfileManager::GetLastUsedProfile();
+}
+
+void NewTabFeatureEngagementTracker::ShowPromo() {
+  // TODO(crbug.com/737830): call the promo
+}
+
+bool NewTabFeatureEngagementTracker::CheckSessionTime() {
+  return elapsed_session_time_ >= kTwoHoursInSeconds;
+}
+
+void NewTabFeatureEngagementTracker::UpdateSessionTime(
+    base::TimeDelta elapsed) {
+  PrefService* prefs = GetProfile()->GetPrefs();
+  if (!prefs->GetBoolean(prefs::kNewTabInProductHelp)) {
+    prefs->SetBoolean(prefs::kNewTabInProductHelp, true);
+    elapsed_session_time_ +=
+        prefs->GetInteger(prefs::kSessionTimeTotal) + elapsed.InSeconds();
+    prefs->SetInteger(prefs::kSessionTimeTotal, elapsed_session_time_);
+  }
+}
+
+void NewTabFeatureEngagementTracker::OnSessionEnded(base::TimeDelta delta) {
+  UpdateSessionTime(delta);
+  if (CheckSessionTime()) {
+    duration_tracker_->RemoveObserver(this);
+    NotifySessionTime();
+  }
+}
+
+}  // namespace feature_engagement_tracker
