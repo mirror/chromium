b6e2f7d6da147a8f4a9fcb4f40c71a53b9c8b6a3
Ricardo Quesada <ricardoq@chromium.org>
2017-06-30 11:16:57 -0700

    WIP: Add support for rich-content copy & paste ARC<-->Chrome
    WIP: don't merge.
    WIP: no need to review yet
    
    Supported formats:
    - plain text
    - html
    - images (transferred as PNG)
    - RTF documents
    
    BUG=????
    R=lhchavez@chromium.org
    Test=???
    
    Change-Id: I7c1757c62b0cc625b8f5a8921a9175368d689b64

--- components/arc/clipboard/arc_clipboard_bridge.h
+++ components/arc/clipboard/arc_clipboard_bridge.h
@@ -12,11 +12,14 @@
 #include "components/arc/common/clipboard.mojom.h"
 #include "components/arc/instance_holder.h"
 #include "mojo/public/cpp/bindings/binding.h"
+#include "third_party/skia/include/core/SkBitmap.h"
+#include "ui/base/clipboard/clipboard_observer.h"
 
 namespace arc {
 
 class ArcClipboardBridge
     : public ArcService,
+      public ui::ClipboardObserver,
       public InstanceHolder<mojom::ClipboardInstance>::Observer,
       public mojom::ClipboardHost {
  public:
@@ -26,16 +29,38 @@ class ArcClipboardBridge
   // InstanceHolder<mojom::ClipboardInstance>::Observer overrides.
   void OnInstanceReady() override;
 
+  // ClipboardObserver overrides
+  void OnClipboardDataChanged() override;
+
   // mojom::ClipboardHost overrides.
   void SetTextContent(const std::string& text) override;
   void GetTextContent() override;
+  void SetClipContent(mojom::ClipDataPtr clip_data) override;
+  void GetClipContent() override;
 
  private:
+  class DecodeRequest;
+
   bool CalledOnValidThread();
+  void ConvertAndSendBitmapToInstance(const SkBitmap& bitmap);
+  void ConvertBitmapAndSendFdToInstance(const SkBitmap& bitmap);
+
+  void SendImageToInstance(const std::vector<uint8_t>& png_bytes);
+  void SendFileToInstance(const std::string& mime_type,
+                          base::Optional<base::FilePath> file_path);
+  void SendHTMLToInstance(const std::string& url,
+                          const std::string& markup,
+                          const std::string& text);
+  void SendTextToInstance(const std::string& clip_text);
+  void SendEmptyToInstance();
 
   mojo::Binding<mojom::ClipboardHost> binding_;
-
   base::ThreadChecker thread_checker_;
+  uint64_t host_clip_version_;
+  uint64_t instance_clip_version_;
+  uint64_t clipboard_seq_from_instance_;
+  std::vector<base::File> files_to_delete_;
+  std::unique_ptr<DecodeRequest> decode_request_;
 
   DISALLOW_COPY_AND_ASSIGN(ArcClipboardBridge);
 };
