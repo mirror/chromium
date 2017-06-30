99e47d88b2df4e9d949eb72194cd37ffb563b896
Alexis Menard <alexis.menard@intel.com>
2017-06-30 13:15:30 -0700

    Make Interaction Media Features MQ dynamic on Android
    With detachable form factors such as Asus Transformer
    it is important to make sure the interaction media
    features are updated whenever the keyboard/trackpad combo is
    detached. This will allow content author to react to
    media query changes to adapt the user interface to
    better suit the new interaction method (often touch vs
    trackpad). The patch also handles any input changes e.g.
    USB connected mouse.
    
    The patch uses the facility landed in previous OSes and
    add a bit of Java code to listen the changes of inputs
    through the InputDeviceListener mechanism.
    
    BUG=442418
    
    Change-Id: I5c18ce4df8e6da927eede5a24f40aac10ecf1974

---
+++ ui/android/java/src/org/chromium/ui/events/devices/InputDeviceObserver.java
@@ -0,0 +1,91 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.ui.events.devices;
+
+import android.content.Context;
+import android.hardware.input.InputManager;
+import android.hardware.input.InputManager.InputDeviceListener;
+
+import org.chromium.base.ContextUtils;
+import org.chromium.base.ThreadUtils;
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.annotations.JNINamespace;
+
+/**
+ * A singleton that helps detecting changes in input devices through the interface
+ * {@link InputDeviceObserver}.
+ */
+@JNINamespace("ui")
+public class InputDeviceObserver {
+    private static InputDeviceObserver getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder {
+        private static final InputDeviceObserver INSTANCE = new InputDeviceObserver();
+    }
+
+    /**
+     * Notifies the InputDeviceObserver that an observer is attached and it
+     * should prepare itself for listening input changes.
+     */
+    @CalledByNative
+    public static void addObserver() {
+        assert ThreadUtils.runningOnUiThread();
+        getInstance().attachObserver();
+    }
+
+    /**
+     * Notifies the InputDeviceObserver that an observer has been removed.
+     */
+    @CalledByNative
+    public static void removeObserver() {
+        assert ThreadUtils.runningOnUiThread();
+        getInstance().detachObserver();
+    }
+
+    private InputManager mInputManager;
+    private InputDeviceListener mInputDeviceListener;
+    private int mObserversCounter;
+
+    private InputDeviceObserver() {
+        mInputDeviceListener = new InputDeviceListener() {
+            // Override InputDeviceListener methods
+            @Override
+            public void onInputDeviceChanged(int deviceId) {
+                nativeInputConfigurationChanged();
+            }
+
+            @Override
+            public void onInputDeviceRemoved(int deviceId) {
+                nativeInputConfigurationChanged();
+            }
+
+            @Override
+            public void onInputDeviceAdded(int deviceId) {
+                nativeInputConfigurationChanged();
+            }
+        };
+    }
+
+    private void attachObserver() {
+        if (mObserversCounter++ == 0) {
+            Context context = ContextUtils.getApplicationContext();
+            mInputManager = (InputManager) context.getSystemService(Context.INPUT_SERVICE);
+            // Register an input device listener.
+            mInputManager.registerInputDeviceListener(mInputDeviceListener, null);
+        }
+    }
+
+    private void detachObserver() {
+        assert mObserversCounter > 0;
+        if (--mObserversCounter == 0) {
+            mInputManager.unregisterInputDeviceListener(mInputDeviceListener);
+            mInputManager = null;
+        }
+    }
+
+    private native void nativeInputConfigurationChanged();
+}
