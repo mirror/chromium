7628a4ad3cc136d6e6a39fd52a5358b405998bdb
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2017-06-30 22:09:31 +0900

    Record changestamps for Team Drive changes separately.
    Each Team Drive has separate changelist and each changestamp that are
    not compatible to each other. Those need to be stored in order to get
    the necessary changelist for Team Drives' and the user's changelist.
    We store the IDs to the root directory entries that exists for each
    Team Drive, so that we can look them up in the levelDB efficiently.
    
    Bug:715355
    Change-Id: I9d7f56bbc2fc096ccda31e535c778bc2a596f410

--- components/drive/chromeos/change_list_processor.cc
+++ components/drive/chromeos/change_list_processor.cc
@@ -130,7 +130,19 @@ ChangeListProcessor::ChangeListProcessor(ResourceMetadata* resource_metadata,
 ChangeListProcessor::~ChangeListProcessor() {
 }
 
-FileError ChangeListProcessor::Apply(
+FileError ChangeListProcessor::GetTeamDriveRoot(
+    const std::string& team_drive_id,
+    ResourceEntry* out_entry) {
+  std::string local_id;
+  FileError error =
+      resource_metadata_->GetIdByResourceId(team_drive_id, &local_id);
+  if (error != FILE_ERROR_OK)
+    return error;
+  error = resource_metadata_->GetResourceEntryById(local_id, out_entry);
+  return error;
+}
+
+FileError ChangeListProcessor::ApplyUserChangeList(
     std::unique_ptr<google_apis::AboutResource> about_resource,
     std::vector<std::unique_ptr<ChangeList>> change_lists,
     bool is_delta_update) {
@@ -151,18 +163,29 @@ FileError ChangeListProcessor::Apply(
     DCHECK(!about_resource->root_folder_id().empty());
   }
 
-  ChangeListToEntryMapUMAStats uma_stats;
-  ConvertChangeListsToMap(std::move(change_lists), largest_changestamp,
-                          &uma_stats);
-
-  FileError error =
-      ApplyEntryMap(largest_changestamp, std::move(about_resource));
+  ResourceEntry root;
+  // Update the entry for "My Drive" directory with the latest changestamp.
+  FileError error = resource_metadata_->GetResourceEntryByPath(
+      util::GetDriveMyDriveRootPath(), &root);
   if (error != FILE_ERROR_OK) {
-    DLOG(ERROR) << "ApplyEntryMap failed: " << FileErrorToString(error);
+    LOG(ERROR) << "Failed to get root entry: " << FileErrorToString(error);
+    return error;
+  }
+  root.mutable_directory_specific_info()->set_changestamp(largest_changestamp);
+  root.set_resource_id(about_resource->root_folder_id());
+  error = resource_metadata_->RefreshEntry(root);
+  if (error != FILE_ERROR_OK) {
+    LOG(ERROR) << "Failed to update root entry: " << FileErrorToString(error);
     return error;
   }
 
-  // Update changestamp.
+  ChangeListToEntryMapUMAStats uma_stats;
+  error = ApplyChangeLists(std::move(change_lists), largest_changestamp,
+                           root.resource_id(), &uma_stats);
+  if (error != FILE_ERROR_OK)
+    return error;
+
+  // Update changestamp in the metadata header.
   error = resource_metadata_->SetLargestChangestamp(largest_changestamp);
   if (error != FILE_ERROR_OK) {
     DLOG(ERROR) << "SetLargestChangeStamp failed: " << FileErrorToString(error);
@@ -174,6 +197,57 @@ FileError ChangeListProcessor::Apply(
     uma_stats.UpdateFileCountUmaHistograms();
 
   return FILE_ERROR_OK;
+}
+
+FileError ChangeListProcessor::ApplyTeamDriveChangeList(
+    const std::string& team_drive_id,
+    std::vector<std::unique_ptr<ChangeList>> change_lists) {
+  DCHECK(!change_lists.empty());
+  int64_t largest_changestamp = 0;
+  // The changestamp appears in the first page of the change list.
+  // The changestamp does not appear in the full resource list.
+  largest_changestamp = change_lists[0]->largest_changestamp();
+  if (change_lists[0]->largest_changestamp() < 0) {
+    LOG(ERROR) << "Received changeList with invalid largestChangestamp: "
+               << largest_changestamp << " (team_drive_id=" << team_drive_id
+               << "). Ignored.";
+    return FILE_ERROR_FAILED;
+  }
+
+  ResourceEntry root;
+  FileError error = GetTeamDriveRoot(team_drive_id, &root);
+  if (error != FILE_ERROR_OK) {
+    LOG(ERROR) << "Failed to get Team Drive root entry: "
+               << FileErrorToString(error);
+    return error;
+  }
+
+  ChangeListToEntryMapUMAStats uma_stats;
+  error = ApplyChangeLists(std::move(change_lists), largest_changestamp,
+                           root.resource_id(), &uma_stats);
+  if (error != FILE_ERROR_OK)
+    return error;
+
+  // Update changestamp of the root entry.
+  root.mutable_directory_specific_info()->set_changestamp(largest_changestamp);
+  error = resource_metadata_->RefreshEntry(root);
+  if (error != FILE_ERROR_OK)
+    DLOG(ERROR) << "RefreshEntry failed: " << FileErrorToString(error);
+
+  return error;
+}
+
+FileError ChangeListProcessor::ApplyChangeLists(
+    std::vector<std::unique_ptr<ChangeList>> change_lists,
+    int64_t largest_changestamp,
+    const std::string& root_resource_id,
+    ChangeListToEntryMapUMAStats* uma_stats) {
+  ConvertChangeListsToMap(std::move(change_lists), largest_changestamp,
+                          uma_stats);
+  FileError error = ApplyEntryMap(root_resource_id, largest_changestamp);
+  if (error != FILE_ERROR_OK)
+    DLOG(ERROR) << "ApplyEntryMap failed: " << FileErrorToString(error);
+  return error;
 }
 
 void ChangeListProcessor::ConvertChangeListsToMap(
@@ -211,27 +285,8 @@ void ChangeListProcessor::ConvertChangeListsToMap(
 }
 
 FileError ChangeListProcessor::ApplyEntryMap(
-    int64_t changestamp,
-    std::unique_ptr<google_apis::AboutResource> about_resource) {
-  DCHECK(about_resource);
-
-  // Create the entry for "My Drive" directory with the latest changestamp.
-  ResourceEntry root;
-  FileError error = resource_metadata_->GetResourceEntryByPath(
-      util::GetDriveMyDriveRootPath(), &root);
-  if (error != FILE_ERROR_OK) {
-    LOG(ERROR) << "Failed to get root entry: " << FileErrorToString(error);
-    return error;
-  }
-
-  root.mutable_directory_specific_info()->set_changestamp(changestamp);
-  root.set_resource_id(about_resource->root_folder_id());
-  error = resource_metadata_->RefreshEntry(root);
-  if (error != FILE_ERROR_OK) {
-    LOG(ERROR) << "Failed to update root entry: " << FileErrorToString(error);
-    return error;
-  }
-
+    const std::string& root_entry_resource_id,
+    int64_t changestamp) {
   // Gather the set of changes in the old path.
   // Note that we want to notify the change in both old and new paths (suppose
   // /a/b/c is moved to /x/y/c. We want to notify both "/a/b" and "/x/y".)
@@ -323,7 +378,7 @@ FileError ChangeListProcessor::ApplyEntryMap(
       // Skip root entry in the change list. We don't expect servers to send
       // root entry, but we should better be defensive (see crbug.com/297259).
       ResourceEntryMap::iterator it = entries[i];
-      if (it->first != root.resource_id()) {
+      if (it->first != root_entry_resource_id) {
         FileError error = ApplyEntry(it->second);
         if (error != FILE_ERROR_OK) {
           LOG(ERROR) << "ApplyEntry failed: " << FileErrorToString(error)
