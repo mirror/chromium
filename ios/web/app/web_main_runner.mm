314d7c704033b9a049fab430ad7432293f27c1e0
Mike Dougherty <michaeldo@chromium.org>
2017-06-30 15:45:09 -0700

    Add additional global state to ios_global_state.
    Expands on the shared initialization to include base::AtExitManager,
    net::NetworkChangeNotifier, and base::MessageLoopForUI.
    
    With this change Cronet and ChromeWebView can successfully be used when
    built together in the framework created by
    //ios/web_view:cronet_web_view_combined.
    
    Bug: 738510
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: Iead4ba5d4f2376b09edf3f5c87a13829229b83b4

--- ios/web/app/web_main_runner.mm
+++ ios/web/app/web_main_runner.mm
@@ -11,6 +11,7 @@
 #include "base/macros.h"
 #include "base/metrics/statistics_recorder.h"
 #include "ios/web/app/web_main_loop.h"
+#include "ios/web/public/global_state/ios_global_state.h"
 #include "ios/web/public/url_schemes.h"
 #import "ios/web/public/web_client.h"
 #include "ui/base/ui_base_paths.h"
@@ -42,11 +43,9 @@ class WebMainRunnerImpl : public WebMainRunner {
     is_initialized_ = true;
     delegate_ = params.delegate;
 
-    if (params.register_exit_manager) {
-      exit_manager_.reset(new base::AtExitManager);
-    }
+    ios_global_state::Create(params.register_exit_manager, params.argc,
+                             params.argv);
 
-    base::CommandLine::Init(params.argc, params.argv);
     if (delegate_) {
       delegate_->BasicStartupComplete();
     }
@@ -95,7 +94,6 @@ class WebMainRunnerImpl : public WebMainRunner {
       delegate_->ProcessExiting();
     }
 
-    exit_manager_.reset(nullptr);
     delegate_ = nullptr;
     is_shutdown_ = true;
   }
@@ -116,7 +114,6 @@ class WebMainRunnerImpl : public WebMainRunner {
   // Used if the embedder doesn't set one.
   WebClient empty_web_client_;
 
-  std::unique_ptr<base::AtExitManager> exit_manager_;
   std::unique_ptr<WebMainLoop> main_loop_;
 
   DISALLOW_COPY_AND_ASSIGN(WebMainRunnerImpl);
