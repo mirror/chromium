90fb0b5dc653ada8a6b9a9575ddd0934a04e4972
Francois Doray <fdoray@chromium.org>
2017-06-30 16:33:42 -0400

    Compute window occlusion in TabManagerDelegate on ChromeOS.
    This CL adds code to take window occlusion into consideration when
    determining which windows are visible before discarding tabs on
    ChromeOS.
    
    With this CL, TabManager::LowMemoryKill starts asynchronous
    requests to get the list of ARC processes and the list of browser
    windows sorted by z-index (required to compute window occlusion).
    TabManager::LowMemoryKillImpl is invoked when the result of
    both requests is available.
    
    Bug: 731145
    Change-Id: Ic66a55bbbd22b7d7c73dc94e99153c4952811e19

--- chrome/browser/resource_coordinator/tab_manager_delegate_chromeos.h
+++ chrome/browser/resource_coordinator/tab_manager_delegate_chromeos.h
@@ -74,7 +74,7 @@ class TabManagerDelegate : public wm::ActivationChangeObserver,
                          aura::Window* lost_active) override;
 
   // Kills a process on memory pressure.
-  void LowMemoryKill(const TabStatsList& tab_stats);
+  void LowMemoryKill();
 
   // Returns oom_score_adj of a process if the score is cached by |this|.
   // If couldn't find the score in the cache, returns -1001 since the valid
@@ -141,7 +141,7 @@ class TabManagerDelegate : public wm::ActivationChangeObserver,
 
   // Kills a process after getting all info of tabs and apps.
   void LowMemoryKillImpl(const TabStatsList& tab_list,
-                         std::vector<arc::ArcProcess> arc_processes);
+                         const std::vector<arc::ArcProcess>& arc_processes);
 
   // Public interface to adjust OOM scores.
   void AdjustOomPriorities(const TabStatsList& tab_list,
