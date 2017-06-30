94cf39a204166e5ea36ff2cb234a2341bb076dfd
Yutaka Hirano <yhirano@chromium.org>
2017-06-30 15:47:11 +0900

    Tidy up preload related states in blink::Resource
    This CL cleans up preload related states in Resource as following:
    
     - Renames PreloadResult to PreloadState.
     - Removes preload_count_. Recent changes have enforced it only to be
       0 or 1, so I introduce kNotPreload to PreloadState.
     - Renames IncreasePreloadCount and DecreasePreloadCount to
       MarkAsPreload and MatchPreload respectively.
     - Changes the timing of kPreloadNotReferenced => kPreloadReferenced
       state transition from when a ResourceClient is added to when
       MatchPreload is called.
     - Removes PreloadReferencePolicy.
    
    Bug: 652228
    Change-Id: I6564c0eed24f19051ee49c429cf87db746414023

--- third_party/WebKit/Source/core/loader/resource/ImageResource.cpp
+++ third_party/WebKit/Source/core/loader/resource/ImageResource.cpp
@@ -109,7 +109,7 @@ class ImageResource::ImageResourceInfoImpl final
 
   void SetDecodedSize(size_t size) override { resource_->SetDecodedSize(size); }
   void WillAddClientOrObserver() override {
-    resource_->WillAddClientOrObserver(Resource::kMarkAsReferenced);
+    resource_->WillAddClientOrObserver();
   }
   void DidRemoveClientOrObserver() override {
     resource_->DidRemoveClientOrObserver();
@@ -269,7 +269,7 @@ void ImageResource::DestroyDecodedDataForFailedRevalidation() {
 
 void ImageResource::DestroyDecodedDataIfPossible() {
   GetContent()->DestroyDecodedData();
-  if (GetContent()->HasImage() && !IsPreloaded() &&
+  if (GetContent()->HasImage() && !IsUnusedPreload() &&
       GetContent()->IsRefetchableDataFromDiskCache()) {
     UMA_HISTOGRAM_MEMORY_KB("Memory.Renderer.EstimatedDroppableEncodedSize",
                             EncodedSize() / 1024);
