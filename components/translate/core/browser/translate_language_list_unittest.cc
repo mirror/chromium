164c30863cbdd77c74fbbc708a354a235c33d2bb
Jon Napper <napper@chromium.org>
2017-06-30 11:35:36 +1000

    Added missing unit tests for translate_language_list.cc
    Added missing unit tests for translate_language_list.cc
    
    Bug: 727459
    Change-Id: Ie59f5a8e9218f0f3b98be87f9575ec548ae877d6

--- components/translate/core/browser/translate_language_list_unittest.cc
+++ components/translate/core/browser/translate_language_list_unittest.cc
@@ -4,8 +4,13 @@
 
 #include "components/translate/core/browser/translate_language_list.h"
 
+#include <string>
+#include <vector>
+
+#include "base/test/scoped_command_line.h"
 #include "components/translate/core/browser/translate_download_manager.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
 
 namespace translate {
 
@@ -25,6 +30,54 @@ TEST(TranslateLanguageListTest, SetSupportedLanguages) {
   EXPECT_EQ("en", results[0]);
   EXPECT_EQ("ja", results[1]);
   manager->ResetForTesting();
+}
+
+TEST(TranslateLanguageListTest, GetLanguageCode) {
+  TranslateLanguageList language_list;
+  EXPECT_EQ(language_list.GetLanguageCode("en"), "en");
+  // Test backoff of unsupported locale.
+  EXPECT_EQ(language_list.GetLanguageCode("en-US"), "en");
+  // Test supported locale not backed off.
+  EXPECT_EQ(language_list.GetLanguageCode("zh-CN"), "zh-CN");
+}
+
+TEST(TranslateLanguageListTest, TranslateLanguageUrl) {
+  TranslateLanguageList language_list;
+
+  // Test default security origin.
+  EXPECT_EQ(language_list.TranslateLanguageUrl().spec(),
+            "https://translate.googleapis.com/translate_a/l?client=chrome");
+
+  // Test command-line security origin.
+  base::test::ScopedCommandLine scoped_command_line;
+  scoped_command_line.GetProcessCommandLine()->AppendSwitchASCII(
+      "translate-security-origin", "https://example.com");
+  EXPECT_EQ(language_list.TranslateLanguageUrl().spec(),
+            "https://example.com/translate_a/l?client=chrome");
+}
+
+TEST(TranslateLanguageListTest, IsSupportedLanguage) {
+  TranslateLanguageList language_list;
+  EXPECT_TRUE(language_list.IsSupportedLanguage("en"));
+  EXPECT_TRUE(language_list.IsSupportedLanguage("zh-CN"));
+  EXPECT_FALSE(language_list.IsSupportedLanguage("xx"));
+}
+
+TEST(TranslateLanguageListTest, GetSupportedLanguages) {
+  TranslateLanguageList language_list;
+  std::vector<std::string> languages;
+  language_list.GetSupportedLanguages(&languages);
+  // Check there are a lot of default languages.
+  EXPECT_GE(languages.size(), 100ul);
+  // Check that some very common languages are there.
+  const auto begin = languages.begin();
+  const auto end = languages.end();
+  EXPECT_NE(std::find(begin, end, "en"), end);
+  EXPECT_NE(std::find(begin, end, "es"), end);
+  EXPECT_NE(std::find(begin, end, "fr"), end);
+  EXPECT_NE(std::find(begin, end, "ru"), end);
+  EXPECT_NE(std::find(begin, end, "zh-CN"), end);
+  EXPECT_NE(std::find(begin, end, "zh-TW"), end);
 }
 
 }  // namespace translate
