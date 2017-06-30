de793142e91b539b1dcae52e952083c41d48ab33
Mike Dougherty <michaeldo@chromium.org>
2017-06-30 14:04:56 -0700

    Add additional global state to ios_global_state.
    Expands on the shared initialization to include base::AtExitManager,
    net::NetworkChangeNotifier, and base::MessageLoopForUI.
    
    With this change Cronet and ChromeWebView can successfully be used when
    built together in the framework created by
    //ios/web_view:cronet_web_view_combined.
    
    Bug: 738510
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: Iead4ba5d4f2376b09edf3f5c87a13829229b83b4

--- components/cronet/ios/cronet_environment.mm
+++ components/cronet/ios/cronet_environment.mm
@@ -27,7 +27,7 @@
 #include "components/prefs/json_pref_store.h"
 #include "components/prefs/pref_filter.h"
 #include "ios/net/cookies/cookie_store_ios.h"
-#include "ios/net/global_state/ios_global_state.h"
+#include "ios/web/public/global_state/ios_global_state.h"
 #include "ios/web/public/user_agent.h"
 #include "net/base/network_change_notifier.h"
 #include "net/cert/cert_verifier.h"
@@ -53,11 +53,6 @@
 #include "url/url_util.h"
 
 namespace {
-
-base::AtExitManager* g_at_exit_ = nullptr;
-net::NetworkChangeNotifier* g_network_change_notifier = nullptr;
-// MessageLoop on the main thread.
-base::MessageLoop* g_main_message_loop = nullptr;
 
 // Request context getter for Cronet.
 class CronetURLRequestContextGetter : public net::URLRequestContextGetter {
@@ -115,28 +110,18 @@ net::URLRequestContextGetter* CronetEnvironment::GetURLRequestContextGetter()
 void CronetEnvironment::Initialize() {
   // This method must be called once from the main thread.
   DCHECK_EQ([NSThread currentThread], [NSThread mainThread]);
-  if (!g_at_exit_)
-    g_at_exit_ = new base::AtExitManager;
 
-  ios_global_state::Create();
+  ios_global_state::Create(true, 0, nullptr);
   ios_global_state::StartTaskScheduler();
 
   url::Initialize();
-  base::CommandLine::Init(0, nullptr);
 
   // Without doing this, StatisticsRecorder::FactoryGet() leaks one histogram
   // per call after the first for a given name.
   base::StatisticsRecorder::Initialize();
 
-  // Create a message loop on the UI thread.
-  DCHECK(!base::MessageLoop::current());
-  DCHECK(!g_main_message_loop);
-  g_main_message_loop = new base::MessageLoopForUI();
-  base::MessageLoopForUI::current()->Attach();
-  // The network change notifier must be initialized so that registered
-  // delegates will receive callbacks.
-  DCHECK(!g_network_change_notifier);
-  g_network_change_notifier = net::NetworkChangeNotifier::Create();
+  ios_global_state::BuildMessageLoop();
+  ios_global_state::CreateNetworkChangeNotifier();
 }
 
 bool CronetEnvironment::StartNetLog(base::FilePath::StringType file_name,
@@ -268,7 +253,6 @@ CronetEnvironment::~CronetEnvironment() {
 
 void CronetEnvironment::InitializeOnNetworkThread() {
   DCHECK(network_io_thread_->task_runner()->BelongsToCurrentThread());
-  base::FeatureList::InitializeInstance(std::string(), std::string());
 
   static bool ssl_key_log_file_set = false;
   if (!ssl_key_log_file_set && !ssl_key_log_file_name_.empty()) {
--- ios/web/public/global_state/ios_global_state.mm
+++ ios/web/public/global_state/ios_global_state.mm
@@ -2,12 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/net/global_state/ios_global_state.h"
+#import "ios/web/public/global_state/ios_global_state.h"
 
+#include "base/at_exit.h"
+#include "base/command_line.h"
 #include "base/memory/ptr_util.h"
+#include "base/message_loop/message_loop.h"
 #include "base/task_scheduler/initialization_util.h"
 #include "components/task_scheduler_util/browser/initialization.h"
-#include "ios/net/global_state/ios_global_state_configuration.h"
+#include "ios/web/public/global_state/ios_global_state_configuration.h"
+#include "net/base/network_change_notifier.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -41,10 +45,41 @@ base::TaskScheduler::InitParams GetDefaultTaskSchedulerInitParams() {
 
 namespace ios_global_state {
 
-void Create() {
+void Create(bool register_exit_manager, int argc, const char** argv) {
+  static std::unique_ptr<base::AtExitManager> exit_manager;
+
   static dispatch_once_t once_token;
   dispatch_once(&once_token, ^{
+    if (register_exit_manager) {
+      exit_manager = base::MakeUnique<base::AtExitManager>();
+    }
+
     base::TaskScheduler::Create(TaskSchedulerName());
+
+    base::CommandLine::Init(argc, argv);
+  });
+}
+
+void BuildMessageLoop() {
+  static std::unique_ptr<base::MessageLoopForUI> main_message_loop;
+
+  static dispatch_once_t once_token;
+  dispatch_once(&once_token, ^{
+    // Create a MessageLoop if one does not already exist for the current
+    // thread.
+    if (!base::MessageLoop::current()) {
+      main_message_loop = base::MakeUnique<base::MessageLoopForUI>();
+    }
+    base::MessageLoopForUI::current()->Attach();
+  });
+}
+
+void CreateNetworkChangeNotifier() {
+  static std::unique_ptr<net::NetworkChangeNotifier> network_change_notifier;
+
+  static dispatch_once_t once_token;
+  dispatch_once(&once_token, ^{
+    network_change_notifier.reset(net::NetworkChangeNotifier::Create());
   });
 }
 
