31ef6a865a8a3fc650cbffec9c343c2ab66c6adc
Mike Dougherty <michaeldo@chromium.org>
2017-06-30 09:45:50 -0700

    Share task scheduler initialization between ios/web_view and cronet.
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: I0095d6f8d4959d1fa797f64ff45885348780bed8

---
+++ components/cronet/ios/ios_global_state_cronet_configuration.mm
@@ -0,0 +1,17 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/net/global_state/ios_global_state_configuration.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace ios_global_state {
+
+base::StringPiece TaskSchedulerName() {
+  return "CronetIos";
+}
+
+}  // namespace ios_global_state
---
+++ ios/net/global_state/ios_global_state_configuration.h
@@ -0,0 +1,17 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_NET_GLOBAL_STATE_IOS_GLOBAL_STATE_CONFIGURATION_H_
+#define IOS_NET_GLOBAL_STATE_IOS_GLOBAL_STATE_CONFIGURATION_H_
+
+#include "base/strings/string_piece.h"
+
+namespace ios_global_state {
+
+// The name to use for the creation of the global Task Scheduler.
+base::StringPiece TaskSchedulerName();
+
+}  // namespace ios_global_state
+
+#endif  // IOS_NET_GLOBAL_STATE_IOS_GLOBAL_STATE_CONFIGURATION_H_
