67f05e3c21f7ed7c7071680f5c812b6d97024806
Mike Dougherty <michaeldo@chromium.org>
2017-06-30 11:00:52 -0700

    Add additional global state to ios_global_state.
    Expands on the shared initialization to include base::AtExitManager,
    net::NetworkChangeNotifier, and base::MessageLoopForUI.
    
    With this change Cronet and ChromeWebView can successfully be used when
    built together in the framework created by
    //ios/web_view:cronet_web_view_combined.
    
    Bug: 738510
    Change-Id: Iead4ba5d4f2376b09edf3f5c87a13829229b83b4

--- ios/net/global_state/ios_global_state.h
+++ ios/net/global_state/ios_global_state.h
@@ -10,7 +10,21 @@ namespace ios_global_state {
 // Creates global state for iOS. This should be called as early as possible in
 // the application lifecycle. It is safe to call this method more than once, the
 // initialization will only be performed once.
-void Create();
+//
+// An AtExitManger will only be created if |register_exit_manager| is true. If
+// |register_exit_manager| is false, an AtExitManager must already exist before
+// calling |Create|.
+// |argc| and |argv| may be set to the command line options which were passed to
+// the application.
+//
+// Since the initialization will only be performed the first time this method is
+// called, the values of all the parameters will be ignored after the first
+// call.
+void Create(bool register_exit_manager, int argc, const char** argv);
+
+// Creates a message loop for the UI thread and attaches it. It is safe to call
+// this method more than once, the initialization will only be performed once.
+void BuildMessageLoop();
 
 // Starts a global base::TaskScheduler. This method must be called to start
 // the Task Scheduler that is created in |Create|. It is safe to call this
--- ios/net/global_state/ios_global_state.mm
+++ ios/net/global_state/ios_global_state.mm
@@ -4,10 +4,14 @@
 
 #import "ios/net/global_state/ios_global_state.h"
 
+#include "base/at_exit.h"
+#include "base/command_line.h"
 #include "base/memory/ptr_util.h"
+#include "base/message_loop/message_loop.h"
 #include "base/task_scheduler/initialization_util.h"
 #include "components/task_scheduler_util/browser/initialization.h"
 #include "ios/net/global_state/ios_global_state_configuration.h"
+#include "net/base/network_change_notifier.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -41,10 +45,35 @@ base::TaskScheduler::InitParams GetDefaultTaskSchedulerInitParams() {
 
 namespace ios_global_state {
 
-void Create() {
+void Create(bool register_exit_manager, int argc, const char** argv) {
+  static std::unique_ptr<base::AtExitManager> exit_manager;
+  static std::unique_ptr<net::NetworkChangeNotifier> network_change_notifier;
+
   static dispatch_once_t once_token;
   dispatch_once(&once_token, ^{
+    if (register_exit_manager) {
+      exit_manager = base::MakeUnique<base::AtExitManager>();
+    }
+
     base::TaskScheduler::Create(TaskSchedulerName());
+
+    base::CommandLine::Init(argc, argv);
+
+    network_change_notifier.reset(net::NetworkChangeNotifier::Create());
+  });
+}
+
+void BuildMessageLoop() {
+  static std::unique_ptr<base::MessageLoopForUI> main_message_loop;
+
+  static dispatch_once_t once_token;
+  dispatch_once(&once_token, ^{
+    // Create a MessageLoop if one does not already exist for the current
+    // thread.
+    if (!base::MessageLoop::current()) {
+      main_message_loop = base::MakeUnique<base::MessageLoopForUI>();
+    }
+    base::MessageLoopForUI::current()->Attach();
   });
 }
 
