722b1aab8fca42422b7a58f5ee3c5b6a2b466d79
Jia <jiameng@chromium.org>
2017-07-03 11:33:57 +1000

    Implement parseShorthand API for "border-image" and "-webkit-mask-box-image".
    This cl contains the following changes
    
    Bug:
    Change-Id: I7fda459f423f684e52729f9ada0dc24e3b753d78

---
+++ third_party/WebKit/Source/core/css/properties/CSSShorthandPropertyAPIBorderImage.cpp
@@ -0,0 +1,54 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSShorthandPropertyAPIBorderImage.h"
+
+#include "core/css/CSSInitialValue.h"
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+#include "core/css/properties/CSSPropertyBorderImageUtils.h"
+
+namespace blink {
+
+bool CSSShorthandPropertyAPIBorderImage::parseShorthand(
+    bool important,
+    CSSParserTokenRange& range,
+    const CSSParserContext& context,
+    const CSSParserLocalContext&,
+    HeapVector<CSSProperty, 256>& properties) {
+  CSSValue* source = nullptr;
+  CSSValue* slice = nullptr;
+  CSSValue* width = nullptr;
+  CSSValue* outset = nullptr;
+  CSSValue* repeat = nullptr;
+
+  if (CSSPropertyBorderImageUtils::ConsumeBorderImageComponents(
+          range, &context, source, slice, width, outset, repeat, false)) {
+    CSSPropertyParserHelpers::AddProperty(
+        CSSPropertyBorderImageSource, CSSPropertyBorderImage,
+        source ? *source : *CSSInitialValue::Create(), important,
+        CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+    CSSPropertyParserHelpers::AddProperty(
+        CSSPropertyBorderImageSlice, CSSPropertyBorderImage,
+        slice ? *slice : *CSSInitialValue::Create(), important,
+        CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+    CSSPropertyParserHelpers::AddProperty(
+        CSSPropertyBorderImageWidth, CSSPropertyBorderImage,
+        width ? *width : *CSSInitialValue::Create(), important,
+        CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+    CSSPropertyParserHelpers::AddProperty(
+        CSSPropertyBorderImageOutset, CSSPropertyBorderImage,
+        outset ? *outset : *CSSInitialValue::Create(), important,
+        CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+    CSSPropertyParserHelpers::AddProperty(
+        CSSPropertyBorderImageRepeat, CSSPropertyBorderImage,
+        repeat ? *repeat : *CSSInitialValue::Create(), important,
+        CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+
+    return true;
+  }
+  return false;
+}
+
+}  // namespace blink
