b68910d2e3cf4ecfed2d50f7919ebb6a7e352c99
Hector Dearman <hjd@google.com>
2017-07-03 12:25:26 +0100

    memory-infra: Start cleaning up ProcessMetricsMemoryDumpProvider
    ProcessMetricsMemoryDumpProvider uses a lot of ifdefs and is quite
    confusing. Start pulling out the code into os_metrics.cc which will
    have separate implementations for each platform.
    
    Bug: 728199
    Change-Id: I2cbc2b2e8860dce3ea1b819affb4dee2170fd5f7

---
+++ services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics_win.cc
@@ -0,0 +1,11 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics.h"
+
+namespace memory_instrumentation {
+
+void FillOSMemoryDump(base::ProcessId pid, mojom::RawOSMemDump* dump) {}
+
+}  // namespace memory_instrumentation
