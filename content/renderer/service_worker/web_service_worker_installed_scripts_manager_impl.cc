a96be8efbadea7fbc4a92a9fecfe600001810bba
Makoto Shimazu <shimazu@chromium.org>
2017-07-03 14:42:41 +0900

    ServiceWorker: add a Mojo's interface for streaming installed scripts
    This is 4th patch of script streaming project split off from
    https://chromium-review.googlesource.com/c/538477.
    This patch adds Mojo's interface to stream installed scripts to the renderer
    during launching up. This patch implements:
    - WebServiceWorkerInstalledScriptsManagerImpl::Internal as a receiver of
      streamed scripts which lives on the io thread on the renderer
    - ServiceWorkerInstalledScriptsSender as a sender of installed scripts on the
      browser process
    
    Bug: 683037
    Change-Id: I6d7ae63aea670c37fe00c0c81afceb71dd70c62a

--- content/renderer/service_worker/web_service_worker_installed_scripts_manager_impl.cc
+++ content/renderer/service_worker/web_service_worker_installed_scripts_manager_impl.cc
@@ -4,20 +4,58 @@
 
 #include "content/renderer/service_worker/web_service_worker_installed_scripts_manager_impl.h"
 
-#include "base/memory/ptr_util.h"
 #include "base/trace_event/trace_event.h"
+#include "mojo/public/cpp/bindings/strong_binding.h"
 
 namespace content {
 
+namespace {
+
+class Internal : public mojom::ServiceWorkerInstalledScriptsManager {
+ public:
+  // Called on IO
+  static void Create(mojom::ServiceWorkerInstalledScriptsManagerRequest request,
+                     base::WaitableEvent* event) {
+    TRACE_EVENT0("ServiceWorker",
+                 "ServiceWorkerInstalledScriptsManager::Internal");
+    mojo::MakeStrongBinding(base::MakeUnique<Internal>(), std::move(request));
+    event->Signal();
+  }
+
+  // Implements mojom::ServiceWorkerInstalledScriptsManager.
+  // Called on the io thread.
+  void TransferInstalledScript(
+      const GURL& script_url,
+      const std::string& encoding,
+      const std::unordered_map<std::string, std::string>& headers,
+      mojo::ScopedDataPipeConsumerHandle body,
+      mojo::ScopedDataPipeConsumerHandle meta_data) override {}
+};
+
+}  // namespace
+
 // static
 std::unique_ptr<blink::WebServiceWorkerInstalledScriptsManager>
-WebServiceWorkerInstalledScriptsManagerImpl::Create() {
+WebServiceWorkerInstalledScriptsManagerImpl::Create(
+    mojom::ServiceWorkerInstalledScriptsInfoPtr installed_scripts_info,
+    scoped_refptr<base::SingleThreadTaskRunner> io_task_runner) {
   TRACE_EVENT0("ServiceWorker",
                "WebServiceWorkerInstalledScriptsManager::Create");
-  std::vector<GURL> installed_urls;
-  return base::WrapUnique<WebServiceWorkerInstalledScriptsManagerImpl>(
-      new WebServiceWorkerInstalledScriptsManagerImpl(
-          std::move(installed_urls)));
+  auto installed_scripts_manager =
+      base::WrapUnique<WebServiceWorkerInstalledScriptsManagerImpl>(
+          new WebServiceWorkerInstalledScriptsManagerImpl(
+              std::move(installed_scripts_info->installed_urls)));
+  // TODO(shimazu): Don't use waitable event here.
+  base::WaitableEvent event(base::WaitableEvent::ResetPolicy::AUTOMATIC,
+                            base::WaitableEvent::InitialState::NOT_SIGNALED);
+  // TODO(shimazu): Pass |this| as parent
+  io_task_runner->PostTask(
+      FROM_HERE,
+      base::BindOnce(&Internal::Create,
+                     std::move(installed_scripts_info->manager_request),
+                     &event));
+  event.Wait();
+  return base::WrapUnique(installed_scripts_manager.release());
 }
 
 WebServiceWorkerInstalledScriptsManagerImpl::
