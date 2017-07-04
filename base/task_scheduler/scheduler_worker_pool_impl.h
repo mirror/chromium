0a034867be1b24711bcca31a2aef9114ea1368bd
Jeffrey He <jeffreyhe@google.com>
2017-07-04 11:46:31 -0400

    Add a lock to protect accesses to workers_.
    The SchduelerWorkPoolImpl in TaskScheduler will be changed to have a
    dynamic number of SchedulerWorkers, so workers_ can be read and written
    to from multiple threads. A lock is needed to prevent race conditions.
    
    Bug: 738104
    Change-Id: I23a49e18253a4a9a375be790be5a6735cb844323

--- base/task_scheduler/scheduler_worker_pool_impl.h
+++ base/task_scheduler/scheduler_worker_pool_impl.h
@@ -148,12 +148,16 @@ class BASE_EXPORT SchedulerWorkerPoolImpl : public SchedulerWorkerPool {
   // |workers_created_.IsSet()|).
   TimeDelta suggested_reclaim_time_;
 
-  // Synchronizes access to |idle_workers_stack_|,
-  // |idle_workers_stack_cv_for_testing_| and |num_wake_ups_before_start_|. Has
-  // |shared_priority_queue_|'s lock as its predecessor so that a worker can be
-  // pushed to |idle_workers_stack_| within the scope of a Transaction (more
-  // details in GetWork()).
+  // Synchronizes access to |idle_workers_stack_| and
+  // |num_wake_ups_before_start_|. Has |shared_priority_queue_|'s lock as
+  // its predecessor so that a worker can be pushed to |idle_workers_stack_|
+  // within the scope of a Transaction (more details in GetWork()).
   mutable SchedulerLock idle_workers_stack_lock_;
+
+  // Synchronizes accesses to |workers_| and |worker_capacity_|.
+  // Has |idle_workers_stack_lock_| as a predecessor since accesses to
+  // |idle_workers_stack_| usually occur whenever |workers_| is accessed.
+  mutable SchedulerLock workers_lock_;
 
   // Stack of idle workers. Initially, all workers are on this stack. A worker
   // is removed from the stack before its WakeUp() function is called and when
@@ -161,9 +165,6 @@ class BASE_EXPORT SchedulerWorkerPoolImpl : public SchedulerWorkerPool {
   // timeout expires, even if its WakeUp() method hasn't been called). A worker
   // is pushed on this stack when it receives nullptr from GetWork().
   SchedulerWorkerStack idle_workers_stack_;
-
-  // Signaled when all workers become idle.
-  std::unique_ptr<ConditionVariable> idle_workers_stack_cv_for_testing_;
 
   // Number of wake ups that occurred before Start(). Never modified after
   // Start() (i.e. can be read without synchronization once
