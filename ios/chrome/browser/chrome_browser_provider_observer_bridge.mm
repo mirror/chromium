7d8d800f07e33b9359e3f79660ff3b1cc6eec568
Jérôme Lebel <jlebel@chromium.org>
2017-07-04 15:45:45 +0200

    Adding ChromeBrowserProviderObserverBridge
    SigninPromoViewMediator should listen to new ChromeIdentityService and
    add or remove itself as an observer according to events, using
    ChromeBrowserProviderObserverBridge.
    
    Also adding unit tests for ChromeBrowserProviderObserverBridge.
    
    Bug:661794
    Change-Id: Iafe475d35bac21a861dfee6d48d009d265ca0bd2

---
+++ ios/chrome/browser/chrome_browser_provider_observer_bridge.mm
@@ -0,0 +1,33 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/chrome_browser_provider_observer_bridge.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+ChromeBrowserProviderObserverBridge::ChromeBrowserProviderObserverBridge(
+    id<ChromeBrowserProviderObserver> observer)
+    : observer_(observer), scoped_observer_(this) {
+  DCHECK(observer_);
+  scoped_observer_.Add(ios::GetChromeBrowserProvider());
+}
+
+ChromeBrowserProviderObserverBridge::~ChromeBrowserProviderObserverBridge() {}
+
+void ChromeBrowserProviderObserverBridge::OnChromeIdentityServiceDidChange(
+    ios::ChromeIdentityService* newIdentityService) {
+  if ([observer_
+          respondsToSelector:@selector(onChromeIdentityServiceDidChange:)])
+    [observer_ onChromeIdentityServiceDidChange:newIdentityService];
+}
+
+void ChromeBrowserProviderObserverBridge::
+    OnChromeBrowserProviderWillBeDestroyed() {
+  if ([observer_ respondsToSelector:@selector
+                 (onChromeBrowserProviderWillBeDestroyed)]) {
+    [observer_ onChromeBrowserProviderWillBeDestroyed];
+  }
+}
