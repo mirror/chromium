e13e1b71f40d2e0ee258209678c4fd34df0a631d
Kinuko Yasuda <kinuko@chromium.org>
2017-07-04 16:42:06 +0900

    Slim down ThreadableLoadingContext
    As it has multiple overlapping accessors with BaseFetchContext and/or
    ExecutionContext.
    
    Bug:
    Change-Id: I43656dbdc07f2694956f0af2e3c40b2329abf313

--- third_party/WebKit/Source/core/loader/WorkerFetchContext.h
+++ third_party/WebKit/Source/core/loader/WorkerFetchContext.h
@@ -34,10 +34,12 @@ class WorkerFetchContext final : public BaseFetchContext {
   virtual ~WorkerFetchContext();
 
   ResourceFetcher* GetResourceFetcher();
-  KURL FirstPartyForCookies() const;
-  RefPtr<WebTaskRunner> GetTaskRunner() { return loading_task_runner_; }
 
   // BaseFetchContext implementation:
+  RefPtr<WebTaskRunner> GetTaskRunner() const override {
+    return loading_task_runner_;
+  }
+  KURL GetFirstPartyForCookies() const override;
   ContentSettingsClient* GetContentSettingsClient() const override;
   Settings* GetSettings() const override;
   SubresourceFilter* GetSubresourceFilter() const override;
