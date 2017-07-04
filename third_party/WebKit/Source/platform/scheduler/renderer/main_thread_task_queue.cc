7e5a6886d61b8a6d8e60700b0647f2eb7c366e6a
Alexander Timin <altimin@chromium.org>
2017-07-04 17:00:55 +0100

    [scheduler] Use composition instead of inheritance for TaskQueueImpl.
    Make internal::TaskQueueImpl a member of TaskQueue instead of
    inheriting. This will allow us to easily subclass TaskQueue for
    different parts of the code.
    
    This patch does not change behaviour.
    
    Notable changes:
    * TaskQueueImpl now is owned by TaskQueueManager via std::unique_ptr. 
      Wrapping TaskQueue subscribes to notifications about unregistering
      (to know when TaskQueueImpl is going away) and resets pointer upon such notification.
    * TaskQueue::Observer mechanism changed (we can't issue a callback from the internals
      of the scheduler due to not having TaskQueue reference. Instead TaskQueueImpl::OnNotification
      are introduced and TaskQueue passes necessary handlers to underlying TaskQueueImpl).
      These notifications include queue wake-up changes and completed tasks for metric purposes.
    
    Bug:
    Change-Id: I61ebcdfb45d02bb44e856de3cc3caf25ab26763b

---
+++ third_party/WebKit/Source/platform/scheduler/renderer/main_thread_task_queue.cc
@@ -0,0 +1,81 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/scheduler/renderer/main_thread_task_queue.h"
+
+#include "platform/scheduler/base/task_queue_impl.h"
+#include "platform/scheduler/renderer/renderer_scheduler_impl.h"
+
+namespace blink {
+namespace scheduler {
+
+// static
+const char* MainThreadTaskQueue::NameForQueueType(
+    MainThreadTaskQueue::QueueType queue_type) {
+  switch (queue_type) {
+    case MainThreadTaskQueue::QueueType::CONTROL:
+      return "control_tq";
+    case MainThreadTaskQueue::QueueType::DEFAULT:
+      return "default_tq";
+    case MainThreadTaskQueue::QueueType::DEFAULT_LOADING:
+      return "default_loading_tq";
+    case MainThreadTaskQueue::QueueType::DEFAULT_TIMER:
+      return "default_timer_tq";
+    case MainThreadTaskQueue::QueueType::UNTHROTTLED:
+      return "unthrottled_tq";
+    case MainThreadTaskQueue::QueueType::FRAME_LOADING:
+      return "frame_loading_tq";
+    case MainThreadTaskQueue::QueueType::FRAME_TIMER:
+      return "frame_timer_tq";
+    case MainThreadTaskQueue::QueueType::FRAME_UNTHROTTLED:
+      return "frame_unthrottled_tq";
+    case MainThreadTaskQueue::QueueType::COMPOSITOR:
+      return "compositor_tq";
+    case MainThreadTaskQueue::QueueType::IDLE:
+      return "idle_tq";
+    case MainThreadTaskQueue::QueueType::TEST:
+      return "test_tq";
+    case MainThreadTaskQueue::QueueType::COUNT:
+      DCHECK(false);
+      return nullptr;
+  }
+  DCHECK(false);
+  return nullptr;
+}
+
+// static
+TaskQueue::Spec MainThreadTaskQueue::GetSpec(
+    MainThreadTaskQueue::QueueType queue_type) {
+  return TaskQueue::Spec(NameForQueueType(queue_type));
+}
+
+MainThreadTaskQueue::MainThreadTaskQueue(
+    MainThreadTaskQueue::QueueType queue_type,
+    internal::TaskQueueImpl* impl,
+    RendererSchedulerImpl* renderer_scheduler)
+    : TaskQueue(impl),
+      queue_type_(queue_type),
+      renderer_scheduler_(renderer_scheduler) {
+  impl_->SetOnTaskCompletedHandler(base::Bind(
+      &MainThreadTaskQueue::OnTaskCompleted, base::Unretained(this)));
+}
+
+MainThreadTaskQueue::~MainThreadTaskQueue() {}
+
+MainThreadTaskQueue::QueueType MainThreadTaskQueue::GetQueueType() const {
+  return queue_type_;
+}
+
+void MainThreadTaskQueue::OnTaskCompleted(base::TimeTicks start,
+                                          base::TimeTicks end) {
+  renderer_scheduler_->OnTaskCompleted(this, start, end);
+}
+
+void MainThreadTaskQueue::UnregisterTaskQueue() {
+  renderer_scheduler_->OnUnregisterTaskQueue(this);
+  TaskQueue::UnregisterTaskQueue();
+}
+
+}  // namespace scheduler
+}  // namespace blink
