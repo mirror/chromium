e92cf4239940278aaf91a5eebbc5c19cc9e3c944
Catherine Mullings <catmullings@chromium.org>
2017-07-03 18:31:30 -0700

    [Extensions] Added visibility property to chrome.contextMenu items API
    Users of the chrome.contextMenu API should be able to programmatically
    set the visiblility (shown, hidden) of context menu items on item
    creation and update.
    
    This CL introduces the |visible| property to createProperties
    (chrome.contextMenus.create) and updateProperties
    (chrome.contextMenus.udpate). Note, this change ensures that hidden
    items are not counted against
    chrome.contextMenus.ACTION_MENU_TOP_LEVEL_LIMIT.
    
    Bug: 463476
    Change-Id: I315bcf070516fe8667e51fbec7c8f539230afa23

---
+++ chrome/test/data/extensions/api_test/context_menus/item_visibility/create_item/popup.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Create two menu items, which should be visible by default.
+chrome.contextMenus.create({"title":"item1"});
+chrome.contextMenus.create({"title":"item2"});
+
+// Create a parent item with two child items -- one displayed and one hidden.
+// Hide the parent item from the menu.
+var id;
+id = chrome.contextMenus.create({title: "item3", visible: false}, function() {
+  chrome.test.assertNoLastError();
+  chrome.contextMenus.create({title: "child1", visible: true, parentId : id},
+      function() {
+        chrome.test.assertNoLastError();
+      });
+  chrome.contextMenus.create({title: "child2", visible: false, parentId : id},
+      chrome.test.callbackPass());
+});
+
+
+
