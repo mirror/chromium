280669f086f61e39ccf082019abc1ef9486a8e6f
Nicolas Dossou-gbete <dgn@chromium.org>
2017-07-05 16:11:23 +0100

    [Suggestions] Log Tile metrics when ChromeHome is enabled
    Add load task tracking to the TileGrid to allow it to record UMA when
    the TileGroup has completed all its load tasks.
    
    In addition to logging the Tile state when the group is initialised,
    we also do so when the bottom sheet opens. To support the new load
    task triggers, Tile load task tracking moved inside of the TileGroup
    and only the completion event is exposed through the delegate.
    
    Appropriately updated the non-Chrome Home code to separate non-Tile
    completion logging for Tile specific one.
    
    Bug: 735945
    Change-Id: I8582ae510fe1edda31197460bcf9db2527e22682

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/NewTabPageAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/NewTabPageAdapter.java
@@ -189,7 +189,7 @@ public class NewTabPageAdapter extends Adapter<NewTabPageViewHolder> implements 
             mSections.refreshSuggestions();
         }
 
-        if (mTileGrid != null) mTileGrid.getTileGroup().onSwitchToForeground();
+        if (mTileGrid != null) mTileGrid.getTileGroup().onSwitchToForeground(/* trackLoadTasks = */ true);
     }
 
     public int getAboveTheFoldPosition() {
