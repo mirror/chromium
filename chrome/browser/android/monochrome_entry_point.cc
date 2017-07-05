0b17fd327e9735191136d358d75a74ac7923870c
Yipeng Wang <yipengw@chromium.org>
2017-07-05 12:21:54 -0400

    Delete JNI registration calls in components.
    Bug: 683256
    Change-Id: Iaf3092bb6e984834a0940a909ac3e5642db616fc

--- chrome/browser/android/monochrome_entry_point.cc
+++ chrome/browser/android/monochrome_entry_point.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "android_webview/lib/webview_jni_onload.h"
 #include "base/android/jni_android.h"
 #include "base/android/library_loader/library_loader_hooks.h"
 #include "base/bind.h"
@@ -16,7 +15,7 @@ bool NativeInit() {
   switch (library_process_type) {
     case base::android::PROCESS_WEBVIEW:
     case base::android::PROCESS_WEBVIEW_CHILD:
-      return android_webview::OnJNIOnLoadInit();
+      return true;
       break;
     case base::android::PROCESS_BROWSER:
     case base::android::PROCESS_CHILD:
@@ -33,7 +32,6 @@ bool NativeInit() {
 // This is called by the VM when the shared library is first loaded.
 JNI_EXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   base::android::InitVM(vm);
-  base::android::SetJniRegistrationType(base::android::NO_JNI_REGISTRATION);
   base::android::SetNativeInitializationHook(NativeInit);
   return JNI_VERSION_1_4;
 }
