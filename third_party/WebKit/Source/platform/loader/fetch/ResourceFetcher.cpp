8e5173c71d7037ecb76b37c0c2d84f51ca83785d
Hiroshige Hayashizaki <hiroshige@chromium.org>
2017-07-05 16:07:08 -0700

    Forbid Add/RemoveClient() only during NotifyStartLoad() in StartLoad()
    Because RemoveClient() can be called due to Oilpan GC in some places
    in ResourceFetcher::StartLoad(), and [1] originally intended to
    forbid Add/RemoveClient/Observer() only during NotifyStartLoad().
    
    [1] https://codereview.chromium.org/2746343002
    
    Bug: 726315
    Change-Id: I30e56a193f06e07903d8227a680d556e01b71117

--- third_party/WebKit/Source/platform/loader/fetch/ResourceFetcher.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/ResourceFetcher.cpp
@@ -1385,10 +1385,8 @@ bool ResourceFetcher::StartLoad(Resource* resource) {
   ResourceLoader* loader = nullptr;
 
   {
-    // Forbids JavaScript/addClient/removeClient/revalidation until start()
+    // Forbids JavaScript/revalidation until start()
     // to prevent unintended state transitions.
-    Resource::ProhibitAddRemoveClientInScope
-        prohibit_add_remove_client_in_scope(resource);
     Resource::RevalidationStartForbiddenScope
         revalidation_start_forbidden_scope(resource);
     ScriptForbiddenIfMainThreadScope script_forbidden_scope;
@@ -1429,6 +1427,10 @@ bool ResourceFetcher::StartLoad(Resource* resource) {
 
     StorePerformanceTimingInitiatorInformation(resource);
     resource->SetFetcherSecurityOrigin(source_origin);
+
+    // NotifyStartLoad() shouldn't cause AddClient/RemoveClient().
+    Resource::ProhibitAddRemoveClientInScope
+        prohibit_add_remove_client_in_scope(resource);
 
     resource->NotifyStartLoad();
   }
