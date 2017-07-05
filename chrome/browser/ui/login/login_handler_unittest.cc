ff7a23777bafca5785de0f121ac70db45e8d9bd3
Trent Apted <tapted@chromium.org>
2017-07-05 17:27:35 +1000

    Update HTTP-Auth to latest Harmony mocks.
    With *and without* --secondary-ui-md:
     * Removes the colons from the field labels
     * Makes textfields 28px tall
     * Alters the text for insecure domains to say "Your connection to this site is not private"
     * Makes the dialog title and "Log in" button text sentence case on non-Mac/iOS
     * Removes " requires a username and password" after the domain for non-Proxy auth requests
    
    With --secondary-ui-md only:
     * Removes the close button (from all web-modal dialogs by default)
     * Moves textfields to start 144pt from the left edge of the dialog
     * Uses SECONDARY / gray text for the second line of header text
     * Adjusts layout to match the mocks
    
    Adds ui/views/harmony/layout_helper.h to encapsulate methods for creating a
    textfield stack in the standard layout.
    
    Bug: 602392
    Change-Id: I27fbc52c95d9bd8ac2a3551d916d3f161f51593f

--- chrome/browser/ui/login/login_handler_unittest.cc
+++ chrome/browser/ui/login/login_handler_unittest.cc
@@ -9,6 +9,7 @@
 #include "chrome/browser/ui/login/login_handler.h"
 #include "net/base/auth.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/test/material_design_controller_test_api.h"
 #include "url/gurl.h"
 
 namespace {
@@ -16,7 +17,7 @@ namespace {
 const char kHttpUrl[] = "http://example.com/foo/bar";
 const char kBasicAuthScheme[] = "Basic";
 const char kFooRealm[] = "Foo";
-const char kInsecureProxy[] = "Your connection to this site is not secure";
+const char kInsecureProxy[] = "Your connection to this site is not private";
 
 enum TargetType { PROXY, SERVER };
 
@@ -37,79 +38,96 @@ const struct TestCase {
     // Insecure proxy
     {kHttpUrl,
      {PROXY, kBasicAuthScheme, kFooRealm, "http://example.com"},
-     {"The proxy http://example.com requires a username and password.",
-      kInsecureProxy, "example.com:80/Foo"}},
+     {"The proxy http://example.com", kInsecureProxy, "example.com:80/Foo"}},
 
     // Insecure proxy on non-standard port
     {kHttpUrl,
      {PROXY, kBasicAuthScheme, kFooRealm, "http://example.com:8009"},
-     {"The proxy http://example.com:8009 requires a username and password.",
-      kInsecureProxy, "example.com:8009/Foo"}},
+     {"The proxy http://example.com:8009", kInsecureProxy,
+      "example.com:8009/Foo"}},
 
     // Secure proxy
     {kHttpUrl,
      {PROXY, kBasicAuthScheme, kFooRealm, "https://example.com"},
-     {"The proxy https://example.com requires a username and password.", "",
-      "example.com:443/Foo"}},
+     {"The proxy https://example.com", "", "example.com:443/Foo"}},
 
     // Secure proxy on non-standard port
     {kHttpUrl,
      {PROXY, kBasicAuthScheme, kFooRealm, "https://example.com:446"},
-     {"The proxy https://example.com:446 requires a username and password.", "",
-      "example.com:446/Foo"}},
+     {"The proxy https://example.com:446", "", "example.com:446/Foo"}},
 
     // localhost
     {kHttpUrl,
      {PROXY, kBasicAuthScheme, kFooRealm, "http://localhost:7323"},
-     {"The proxy http://localhost:7323 requires a username and password.", "",
-      "localhost:7323/Foo"}},
+     {"The proxy http://localhost:7323", "", "localhost:7323/Foo"}},
 
     // Secure server
     {"https://www.nowhere.org/dir/index.html",
      {SERVER, kBasicAuthScheme, kFooRealm, nullptr},
-     {"https://www.nowhere.org requires a username and password.", "",
-      "https://www.nowhere.org/Foo"}},
+     {"https://www.nowhere.org", "", "https://www.nowhere.org/Foo"}},
 
     // URL uses default port.
     {"https://www.nowhere.org:443/dir/index.html",
      {SERVER, kBasicAuthScheme, kFooRealm, nullptr},
-     {"https://www.nowhere.org requires a username and password.", "",
-      "https://www.nowhere.org/Foo"}},
+     {"https://www.nowhere.org", "", "https://www.nowhere.org/Foo"}},
 
     // URL uses non-default port.
     {"https://www.nowhere.org:8443/dir/index.html",
      {SERVER, kBasicAuthScheme, kFooRealm, nullptr},
-     {"https://www.nowhere.org:8443 requires a username and password.", "",
-      "https://www.nowhere.org:8443/Foo"}},
+     {"https://www.nowhere.org:8443", "", "https://www.nowhere.org:8443/Foo"}},
 
     // URL has no trailing slash.
     {"https://www.nowhere.org",
      {SERVER, kBasicAuthScheme, kFooRealm, nullptr},
-     {"https://www.nowhere.org requires a username and password.", "",
-      "https://www.nowhere.org/Foo"}},
+     {"https://www.nowhere.org", "", "https://www.nowhere.org/Foo"}},
 
     // username:password
     {"https://foo:bar@www.nowhere.org/dir/index.html",
      {SERVER, kBasicAuthScheme, kFooRealm, nullptr},
-     {"https://www.nowhere.org requires a username and password.", "",
-      "https://www.nowhere.org/Foo"}},
+     {"https://www.nowhere.org", "", "https://www.nowhere.org/Foo"}},
 
     // query
     {"https://www.nowhere.org/dir/index.html?id=965362",
      {SERVER, kBasicAuthScheme, kFooRealm, nullptr},
-     {"https://www.nowhere.org requires a username and password.", "",
-      "https://www.nowhere.org/Foo"}},
+     {"https://www.nowhere.org", "", "https://www.nowhere.org/Foo"}},
 
     // reference
     {"https://www.nowhere.org/dir/index.html#toc",
      {SERVER, kBasicAuthScheme, kFooRealm, nullptr},
-     {"https://www.nowhere.org requires a username and password.", "",
-      "https://www.nowhere.org/Foo"}},
+     {"https://www.nowhere.org", "", "https://www.nowhere.org/Foo"}},
 };
+
+enum class SecondaryUiMode { NON_MD, MD };
+
+std::string SecondaryUiModeToString(
+    const ::testing::TestParamInfo<SecondaryUiMode>& info) {
+  return info.param == SecondaryUiMode::MD ? "MD" : "NonMD";
+}
 
 }  // namespace
 
-TEST(LoginHandlerTest, DialogStringsAndRealm) {
+class LoginHandlerTest : public testing::Test,
+                         public ::testing::WithParamInterface<SecondaryUiMode> {
+ public:
+  LoginHandlerTest()
+      : md_test_api_(ui::MaterialDesignController::Mode::MATERIAL_NORMAL) {
+    md_test_api_.SetSecondaryUiMaterial(GetParam() == SecondaryUiMode::MD);
+  }
+
+  base::string16 ExpectedAuthority(bool is_proxy, const char* prefix) {
+    base::string16 str = base::ASCIIToUTF16(prefix);
+    if (is_proxy || GetParam() == SecondaryUiMode::NON_MD)
+      str += base::ASCIIToUTF16(" requires a username and password.");
+    return str;
+  }
+
+ private:
+  ui::test::MaterialDesignControllerTestAPI md_test_api_;
+
+  DISALLOW_COPY_AND_ASSIGN(LoginHandlerTest);
+};
+
+TEST_P(LoginHandlerTest, DialogStringsAndRealm) {
   scoped_refptr<net::AuthChallengeInfo> auth_info = new net::AuthChallengeInfo;
   for (const auto& test_case : kTestCases) {
     GURL request_url(test_case.request_url);
@@ -131,8 +149,9 @@ TEST(LoginHandlerTest, DialogStringsAndRealm) {
 
     LoginHandler::GetDialogStrings(request_url, *auth_info, &authority,
                                    &explanation);
-    EXPECT_STREQ(test_case.expected.authority,
-                 base::UTF16ToASCII(authority).c_str());
+    EXPECT_EQ(ExpectedAuthority(test_case.auth_info.target_type == PROXY,
+                                test_case.expected.authority),
+              authority);
     EXPECT_STREQ(test_case.expected.explanation,
                  base::UTF16ToASCII(explanation).c_str());
 
@@ -140,3 +159,9 @@ TEST(LoginHandlerTest, DialogStringsAndRealm) {
                  LoginHandler::GetSignonRealm(request_url, *auth_info).c_str());
   }
 }
+
+INSTANTIATE_TEST_CASE_P(,
+                        LoginHandlerTest,
+                        ::testing::Values(SecondaryUiMode::MD,
+                                          SecondaryUiMode::NON_MD),
+                        &SecondaryUiModeToString);
