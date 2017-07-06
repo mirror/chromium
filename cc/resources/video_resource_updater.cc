3afe12cf3d99e245ed1c89e26165a1d5fc1da9ff
Sunny Sachanandani <sunnyps@chromium.org>
2017-07-06 16:35:51 -0700

    cc: Explicitly set resource synchronization state for empty sync tokens.
    Mailbox resource sync token semantics were recently changed so that an
    empty sync token means no synchronization needed. VideoResourceUpdater
    creates resources with empty sync tokens and sync tokens are
    generated by VideoLayerImpl. That code checks the needs_sync_token state
    and does not generate sync tokens.
    
    This CL makes setting synchronization state explicit and removes
    redundant state. When a mailbox or sync token is set on a resource, an
    empty sync token means LOCALLY_USED and a non-empty sync token means
    NEEDS_WAIT. In both cases, SetSynchronized must be called either because
    we know that the non-empty sync token belongs to the compositor stream,
    or an empty sync token is used because commands are guaranteed to have
    executed in the gpu process. For clients, this means using either using
    the ScopedWriteLock classes, GenerateSyncTokenForResource methods, or
    calling SetResourceSynchronized.
    
    R=piman
    BUG=738928
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I5ca66ab689fdee2b1b78eaf2e77bfd2ef1cebe53

--- cc/resources/video_resource_updater.cc
+++ cc/resources/video_resource_updater.cc
@@ -426,12 +426,16 @@ VideoFrameExternalResources VideoResourceUpdater::CreateForSoftwarePlanes(
                      plane_resource.resource_id());
       external_resources.type = VideoFrameExternalResources::SOFTWARE_RESOURCE;
     } else {
-      // VideoResourceUpdater shares a context with the compositor so
-      // a sync token is not required.
-      TextureMailbox mailbox(plane_resource.mailbox(), gpu::SyncToken(),
+      // Set the sync token otherwise resource is assumed to be synchronized.
+      SyncTokenClientImpl client(context_provider_->ContextGL(),
+                                 gpu::SyncToken());
+      gpu::SyncToken sync_token = video_frame->UpdateReleaseSyncToken(&client);
+
+      TextureMailbox mailbox(plane_resource.mailbox(), sync_token,
                              resource_provider_->GetResourceTextureTarget(
                                  plane_resource.resource_id()));
       mailbox.set_color_space(output_color_space);
+
       external_resources.mailboxes.push_back(mailbox);
       external_resources.release_callbacks.push_back(
           base::Bind(&RecycleResource, weak_ptr_factory_.GetWeakPtr(),
@@ -529,10 +533,17 @@ VideoFrameExternalResources VideoResourceUpdater::CreateForSoftwarePlanes(
                                          resource_size_pixels);
       plane_resource.SetUniqueId(video_frame->unique_id(), i);
     }
+  }
 
+  // Set the sync token otherwise resource is assumed to be synchronized.
+  SyncTokenClientImpl client(context_provider_->ContextGL(), gpu::SyncToken());
+  gpu::SyncToken sync_token = video_frame->UpdateReleaseSyncToken(&client);
+
+  for (size_t i = 0; i < plane_resources.size(); ++i) {
+    PlaneResource& plane_resource = *plane_resources[i];
     // VideoResourceUpdater shares a context with the compositor so a
     // sync token is not required.
-    TextureMailbox mailbox(plane_resource.mailbox(), gpu::SyncToken(),
+    TextureMailbox mailbox(plane_resource.mailbox(), sync_token,
                            resource_provider_->GetResourceTextureTarget(
                                plane_resource.resource_id()));
     mailbox.set_color_space(output_color_space);
@@ -605,11 +616,10 @@ void VideoResourceUpdater::CopyPlaneTexture(
   gl->DeleteTextures(1, &src_texture_id);
 
   // Done with the source video frame texture at this point.
-  video_frame->UpdateReleaseSyncToken(&client);
+  gpu::SyncToken sync_token = video_frame->UpdateReleaseSyncToken(&client);
 
-  // VideoResourceUpdater shares a context with the compositor so a
-  // sync token is not required.
-  TextureMailbox mailbox(resource->mailbox(), gpu::SyncToken(), GL_TEXTURE_2D,
+  // Set sync token otherwise resource is assumed to be synchronized.
+  TextureMailbox mailbox(resource->mailbox(), sync_token, GL_TEXTURE_2D,
                          video_frame->coded_size(), false, false);
   mailbox.set_color_space(resource_color_space);
   external_resources->mailboxes.push_back(mailbox);
