2de9d5d26b1687d6d595b6e519fe701281bd20e4
Vaclav Brozek <vabr@chromium.org>
2017-07-06 12:26:06 +0200

    Restrict disabling of passwords_settings_egtest.mm to device
    Those tests did not flake on simulator.
    
    R=baxley@chromium.org
    BUG=739395
    
    Change-Id: If7374e610f0493d4110407742710ef0feace8f0c

--- ios/chrome/browser/ui/settings/passwords_settings_egtest.mm
+++ ios/chrome/browser/ui/settings/passwords_settings_egtest.mm
@@ -477,8 +477,12 @@ id<GREYMatcher> DeleteButton() {
 
 // Checks that deleting a password from password details view goes back to the
 // list-of-passwords view.
-// TODO(crbug.com/739395): Fix.
-- (void)DISABLED_testDeletion {
+- (void)testDeletion {
+// TODO(crbug.com/739395): Fix on device.
+#if !TARGET_IPHONE_SIMULATOR
+  EARL_GREY_TEST_DISABLED(@"Test disabled on device, failed on iPad.");
+#endif
+
   [self scopedEnablePasswordManagementAndViewingUI];
 
   // Save form to be deleted later.
@@ -525,8 +529,11 @@ id<GREYMatcher> DeleteButton() {
 }
 
 // Checks that deleting a password from password details can be cancelled.
-// TODO(crbug.com/739395): Fix.
-- (void)DISABLED_testCancelDeletion {
+- (void)testCancelDeletion {
+// TODO(crbug.com/739395): Fix on device.
+#if !TARGET_IPHONE_SIMULATOR
+  EARL_GREY_TEST_DISABLED(@"Test disabled on device, failed on iPad.");
+#endif
   [self scopedEnablePasswordManagementAndViewingUI];
 
   // Save form to be deleted later.
