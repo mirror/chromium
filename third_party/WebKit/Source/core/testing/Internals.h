d164d0a9408b4a3b678a08a282cbde3b35ca53a6
David Bokan <bokan@chromium.org>
2017-07-06 07:29:45 -0400

    [WIP] Added internal methods for controling top controls
    Bug:
    Change-Id: Iaed2d03c0e8aae4800644649bc4d8e7fce691cdc

--- third_party/WebKit/Source/core/testing/Internals.h
+++ third_party/WebKit/Source/core/testing/Internals.h
@@ -107,6 +107,8 @@ class Internals final : public GarbageCollected<Internals>,
 
   CSSStyleDeclaration* computedStyleIncludingVisitedInfo(Node*) const;
 
+  void setBrowserControlsState(float height, bool shrinksLayout);
+
   ShadowRoot* createUserAgentShadowRoot(Element* host);
 
   ShadowRoot* shadowRoot(Element* host);
