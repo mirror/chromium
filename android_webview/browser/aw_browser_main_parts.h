e7821ba16848b576269c6e45e3f0c1509aad1b51
Kyle Milka <kmilka@google.com>
2017-07-06 11:42:25 -0700

    Set up field trials within WebView
    This is part ? of ?
    
    Bug:
    Change-Id: Id76b98e5adb432ca3fa7ea4cffd7c0296f160e82

--- android_webview/browser/aw_browser_main_parts.h
+++ android_webview/browser/aw_browser_main_parts.h
@@ -7,8 +7,12 @@
 
 #include <memory>
 
+#include "android_webview/browser/aw_variations_seed_manager.h"
 #include "base/compiler_specific.h"
 #include "base/macros.h"
+#include "base/metrics/field_trial.h"
+#include "components/prefs/pref_service.h"
+#include "components/variations/platform_field_trials.h"
 #include "content/public/browser/browser_main_parts.h"
 
 namespace base {
@@ -29,12 +33,27 @@ class AwBrowserMainParts : public content::BrowserMainParts {
   int PreCreateThreads() override;
   void PreMainMessageLoopRun() override;
   bool MainMessageLoopRun(int* result_code) override;
+  void SetUpFieldTrials();
 
  private:
   // Android specific UI MessageLoop.
   std::unique_ptr<base::MessageLoop> main_message_loop_;
 
   AwContentBrowserClient* browser_client_;
+
+  // Statistical testing infrastructure for the entire application. empty until
+  // |SetupFieldTrials()| is called.
+  std::unique_ptr<base::FieldTrialList> field_trial_list_;
+
+  // Members initialized in PreMainMessageLoopRun, needed in
+  // PreMainMessageLoopRunThreadsCreated.
+  base::FilePath user_data_dir_;
+
+  // Platform specific field trial infrastructure
+  variations::PlatformFieldTrials webview_field_trials_;
+
+  // Member responsible for creating a feature list from the seed
+  std::unique_ptr<AwVariationsSeedManager> variations_seed_manager_;
 
   DISALLOW_COPY_AND_ASSIGN(AwBrowserMainParts);
 };
