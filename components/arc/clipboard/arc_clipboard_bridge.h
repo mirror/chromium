e90e796a0faeed0f367458e8209f1d63dd594f4a
Ricardo Quesada <ricardoq@chromium.org>
2017-07-06 12:03:47 -0700

    WIP: Add support for rich-content copy & paste ARC<-->Chrome
    WIP: don't merge.
    WIP: no need to review yet
    
    Supported formats:
    - plain text
    - html
    - images (transferred as PNG)
    - RTF documents
    
    BUG=????
    R=lhchavez@chromium.org
    Test=???
    
    Change-Id: I7c1757c62b0cc625b8f5a8921a9175368d689b64

--- components/arc/clipboard/arc_clipboard_bridge.h
+++ components/arc/clipboard/arc_clipboard_bridge.h
@@ -5,18 +5,23 @@
 #ifndef COMPONENTS_ARC_CLIPBOARD_ARC_CLIPBOARD_BRIDGE_H_
 #define COMPONENTS_ARC_CLIPBOARD_ARC_CLIPBOARD_BRIDGE_H_
 
+#include <memory>
 #include <string>
+#include <vector>
 
 #include "base/macros.h"
 #include "components/arc/arc_service.h"
 #include "components/arc/common/clipboard.mojom.h"
 #include "components/arc/instance_holder.h"
 #include "mojo/public/cpp/bindings/binding.h"
+#include "ui/base/clipboard/clipboard_observer.h"
 
+class SkBitmap;
 namespace arc {
 
 class ArcClipboardBridge
     : public ArcService,
+      public ui::ClipboardObserver,
       public InstanceHolder<mojom::ClipboardInstance>::Observer,
       public mojom::ClipboardHost {
  public:
@@ -26,14 +31,50 @@ class ArcClipboardBridge
   // InstanceHolder<mojom::ClipboardInstance>::Observer overrides.
   void OnInstanceReady() override;
 
+  // ClipboardObserver overrides
+  void OnClipboardDataChanged() override;
+
   // mojom::ClipboardHost overrides.
-  void SetTextContent(const std::string& text) override;
-  void GetTextContent() override;
+  void SetTextContentDeprecated(const std::string& text) override;
+  void GetTextContentDeprecated() override;
+  void SetClipContent(mojom::ClipDataPtr clip_data) override;
+  void GetClipContent(const GetClipContentCallback& callback) override;
 
  private:
-  THREAD_CHECKER(thread_checker_);
+  class DecodeRequest;
+
+  void ConvertAndSendBitmap(const GetClipContentCallback& callback,
+                            const SkBitmap& bitmap);
+  // void ConvertBitmapAndSendFd(const SkBitmap& bitmap);
+
+  void SendImage(const GetClipContentCallback& callback,
+                 const std::vector<uint8_t>& png_bytes);
+  void SendFile(const GetClipContentCallback& callback,
+                const std::string& mime_type,
+                const base::FilePath& file_path);
+  void SendHTML(const GetClipContentCallback& callback,
+                const std::string& url,
+                const std::string& markup,
+                const std::string& text);
+  void SendText(const GetClipContentCallback& callback,
+                const std::string& clip_text);
+  void SendUnsupported(const GetClipContentCallback& callback,
+                       const std::string& error_msg);
+
+  void ProcessHTML(const mojom::ClipDataPtr& clip_data);
+  void ProcessPlainText(const mojom::ClipDataPtr& clip_data);
+  void ProcessFileDescriptor(const mojom::ClipDataPtr& clip_data);
+  void ProcessBlob(const mojom::ClipDataPtr& clip_data);
+  void ProcessUnsupported(const mojom::ClipDataPtr& clip_data);
+
+  void ProcessImage(const std::vector<uint8_t>& data);
 
   mojo::Binding<mojom::ClipboardHost> binding_;
+  THREAD_CHECKER(thread_checker_);
+  uint64_t clipboard_seq_from_instance_;
+  std::vector<int> fds_to_close_;
+  std::unique_ptr<DecodeRequest> decode_request_;
+  base::WeakPtrFactory<ArcClipboardBridge> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ArcClipboardBridge);
 };
