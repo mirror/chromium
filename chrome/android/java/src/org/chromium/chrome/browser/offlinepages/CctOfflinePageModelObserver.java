146e7d484606756c414eb582e9eb378f68d7ba13
Cathy Li <chili@chromium.org>
2017-07-05 18:38:57 -0700

    [Offline pages]: Add ability to notify apps when pages attributed to them have been changed.
    Bug: 734753
    Change-Id: I1e821146ccade5ffd8c19c190fb82f305bfd9a2e

---
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/CctOfflinePageModelObserver.java
@@ -0,0 +1,10 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.offlinepages;
+
+/**
+ * Observer for offline page model events related to CCT pages.
+ */
+public class CctOfflinePageModelObserver extends OfflinePageBridge.OfflinePageModelObserver {}
