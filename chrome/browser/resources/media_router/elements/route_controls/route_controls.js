a2e10b891c23ae9638edec90143f2e59e20cf800
Takumi Fujimoto <takumif@chromium.org>
2017-07-06 15:34:47 -0700

    [Media Router] Update volume while dragging slider in WebUI route controller
    Bug:
    Change-Id: I17e7d53fcf801e1cf720de4789f7b36aaf6dd6f8

--- chrome/browser/resources/media_router/elements/route_controls/route_controls.js
+++ chrome/browser/resources/media_router/elements/route_controls/route_controls.js
@@ -301,9 +301,17 @@ Polymer({
    * @private
    */
   onVolumeChangeComplete_: function(e) {
-    this.isVolumeChanging_ = false;
     this.volumeSliderValue_ = e.target.value;
     media_router.browserApi.setCurrentMediaVolume(this.volumeSliderValue_);
+    if (this.isVolumeChanging_) {
+      // Wait for 1 second before applying external volume updates, to prevent
+      // notifications originating from this controller moving the slider knob
+      // around.
+      var that = this;
+      setTimeout(function() {
+        that.isVolumeChanging_ = false;
+      }, 1000);
+    }
   },
 
   /**
@@ -315,14 +323,14 @@ Polymer({
     this.isVolumeChanging_ = true;
     var target = /** @type {{immediateValue: number}} */ (e.target);
     this.volumeSliderValue_ = target.immediateValue;
+    media_router.browserApi.setCurrentMediaVolume(this.volumeSliderValue_);
   },
 
   /**
    * Resets the route controls. Called when the route details view is closed.
    */
   reset: function() {
-    this.routeStatus = new media_router.RouteStatus(
-        '', '', false, false, false, false, false, false, 0, 0, 0);
+    this.routeStatus = new media_router.RouteStatus();
     media_router.ui.setRouteControls(null);
   },
 
