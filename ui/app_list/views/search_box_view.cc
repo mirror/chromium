4171e78459526eb122191bdd89c3724ec35fc5c8
Maajid <maajid@chromium.org>
2017-07-05 17:54:22 -0700

    Use the magnifying glass icon instead of the Google icon for the search box when the default search provider is not Google.
    Bug:736038
    TEST: device or emulator test with --enable-features=EnableFullcreenAppList
    
    Change-Id: Ib305fbbdfea06b7f21fad2aa717af91e0f20aec0

--- ui/app_list/views/search_box_view.cc
+++ ui/app_list/views/search_box_view.cc
@@ -58,7 +58,7 @@ constexpr SkColor kHintTextColor = SkColorSetARGBMacro(0xFF, 0xA0, 0xA0, 0xA0);
 
 constexpr int kBackgroundBorderCornerRadius = 2;
 constexpr int kBackgroundBorderCornerRadiusFullscreen = 24;
-constexpr int kGoogleIconSize = 24;
+constexpr int kSearchIconSize = 24;
 constexpr int kMicIconSize = 24;
 
 // Default color used when wallpaper customized color is not available for
@@ -148,7 +148,7 @@ SearchBoxView::SearchBoxView(SearchBoxViewDelegate* delegate,
       view_delegate_(view_delegate),
       model_(nullptr),
       content_container_(new views::View),
-      google_icon_(nullptr),
+      search_icon_(nullptr),
       back_button_(nullptr),
       speech_button_(nullptr),
       search_box_(new views::Textfield),
@@ -191,10 +191,14 @@ SearchBoxView::SearchBoxView(SearchBoxViewDelegate* delegate,
   content_container_->AddChildView(back_button_);
 
   if (is_fullscreen_app_list_enabled_) {
-    google_icon_ = new views::ImageView();
-    google_icon_->SetImage(gfx::CreateVectorIcon(
-        kIcGoogleBlackIcon, kGoogleIconSize, kDefaultSearchboxColor));
-    content_container_->AddChildView(google_icon_);
+    search_icon_ = new views::ImageView();
+    const gfx::VectorIcon& icon =
+        view_delegate_->GetModel()->search_engine_is_google()
+            ? kIcGoogleBlackIcon
+            : kIcSearchEngineNotGoogleIcon;
+    search_icon_->SetImage(
+        gfx::CreateVectorIcon(icon, kSearchIconSize, kDefaultSearchboxColor));
+    content_container_->AddChildView(search_icon_);
 
     search_box_->set_placeholder_text_color(kDefaultSearchboxColor);
     search_box_->set_placeholder_text_draw_flags(
@@ -276,20 +280,23 @@ bool SearchBoxView::MoveTabFocus(bool move_backwards) {
     case FOCUS_SEARCH_BOX:
       if (move_backwards) {
         focused_view_ = back_button_ && back_button_->visible()
-            ? FOCUS_BACK_BUTTON : FOCUS_SEARCH_BOX;
+                            ? FOCUS_BACK_BUTTON
+                            : FOCUS_SEARCH_BOX;
       } else {
         focused_view_ = speech_button_ && speech_button_->visible()
-            ? FOCUS_MIC_BUTTON : FOCUS_CONTENTS_VIEW;
+                            ? FOCUS_MIC_BUTTON
+                            : FOCUS_CONTENTS_VIEW;
       }
       break;
     case FOCUS_MIC_BUTTON:
       focused_view_ = move_backwards ? FOCUS_SEARCH_BOX : FOCUS_CONTENTS_VIEW;
       break;
     case FOCUS_CONTENTS_VIEW:
-      focused_view_ = move_backwards
-          ? (speech_button_ && speech_button_->visible() ?
-              FOCUS_MIC_BUTTON : FOCUS_SEARCH_BOX)
-          : FOCUS_CONTENTS_VIEW;
+      focused_view_ =
+          move_backwards
+              ? (speech_button_ && speech_button_->visible() ? FOCUS_MIC_BUTTON
+                                                             : FOCUS_SEARCH_BOX)
+              : FOCUS_CONTENTS_VIEW;
       break;
     default:
       DCHECK(false);
@@ -346,7 +353,7 @@ void SearchBoxView::ShowBackOrGoogleIcon(bool show_back_button) {
   if (!is_fullscreen_app_list_enabled_)
     return;
 
-  google_icon_->SetVisible(!show_back_button);
+  search_icon_->SetVisible(!show_back_button);
   back_button_->SetVisible(show_back_button);
   content_container_->Layout();
 }
@@ -521,29 +528,28 @@ void SearchBoxView::WallpaperProminentColorsChanged() {
             prominent_colors.size());
   const SkColor dark_muted =
       prominent_colors[static_cast<int>(ColorProfileType::DARK_MUTED)];
-  const bool dark_muted_available = SK_ColorTRANSPARENT != dark_muted;
-  google_icon_->SetImage(gfx::CreateVectorIcon(
-      kIcGoogleBlackIcon, kGoogleIconSize,
-      dark_muted_available ? dark_muted : kDefaultSearchboxColor));
+  const SkColor search_box_color =
+      SK_ColorTRANSPARENT == dark_muted ? kDefaultSearchboxColor : dark_muted;
+  const gfx::VectorIcon& icon = model_->search_engine_is_google()
+                                    ? kIcGoogleBlackIcon
+                                    : kIcSearchEngineNotGoogleIcon;
+  search_icon_->SetImage(
+      gfx::CreateVectorIcon(icon, kSearchIconSize, search_box_color));
   speech_button_->SetImage(
       views::Button::STATE_NORMAL,
-      gfx::CreateVectorIcon(
-          kIcMicBlackIcon, kMicIconSize,
-          dark_muted_available ? dark_muted : kDefaultSearchboxColor));
-  search_box_->set_placeholder_text_color(
-      dark_muted_available ? dark_muted : kDefaultSearchboxColor);
+      gfx::CreateVectorIcon(kIcMicBlackIcon, kMicIconSize, search_box_color));
+  search_box_->set_placeholder_text_color(search_box_color);
 
   const SkColor light_vibrant =
       prominent_colors[static_cast<int>(ColorProfileType::LIGHT_VIBRANT)];
   const SkColor light_vibrant_mixed = color_utils::AlphaBlend(
       SK_ColorWHITE, light_vibrant, kLightVibrantBlendAlpha);
-  const bool light_vibrant_available = SK_ColorTRANSPARENT != light_vibrant;
-  content_container_->SetBackground(base::MakeUnique<SearchBoxBackground>(
-      light_vibrant_available ? light_vibrant_mixed
-                              : kSearchBoxBackgroundDefault));
-  search_box_->SetBackgroundColor(light_vibrant_available
-                                      ? light_vibrant_mixed
-                                      : kSearchBoxBackgroundDefault);
+  const SkColor background_color = SK_ColorTRANSPARENT == light_vibrant
+                                       ? kSearchBoxBackgroundDefault
+                                       : light_vibrant_mixed;
+  content_container_->SetBackground(
+      base::MakeUnique<SearchBoxBackground>(background_color));
+  search_box_->SetBackgroundColor(background_color);
 
   SchedulePaint();
 }
