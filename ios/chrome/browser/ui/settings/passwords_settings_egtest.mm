05ac39444450562244ce7cf5150859508b1fad84
Vaclav Brozek <vabr@chromium.org>
2017-07-06 12:34:45 +0200

    Enable all passwords EGtests
    Let's see if the recent improvements helped on iPad.
    
    BUG=739395
    
    Change-Id: I6cd8ca9bca2e5183f0c9d3e77ccf0423dd60bfd2

--- ios/chrome/browser/ui/settings/passwords_settings_egtest.mm
+++ ios/chrome/browser/ui/settings/passwords_settings_egtest.mm
@@ -478,11 +478,6 @@ id<GREYMatcher> DeleteButton() {
 // Checks that deleting a password from password details view goes back to the
 // list-of-passwords view.
 - (void)testDeletion {
-// TODO(crbug.com/739395): Fix on device.
-#if !TARGET_IPHONE_SIMULATOR
-  EARL_GREY_TEST_DISABLED(@"Test disabled on device, failed on iPad.");
-#endif
-
   [self scopedEnablePasswordManagementAndViewingUI];
 
   // Save form to be deleted later.
@@ -530,10 +525,6 @@ id<GREYMatcher> DeleteButton() {
 
 // Checks that deleting a password from password details can be cancelled.
 - (void)testCancelDeletion {
-// TODO(crbug.com/739395): Fix on device.
-#if !TARGET_IPHONE_SIMULATOR
-  EARL_GREY_TEST_DISABLED(@"Test disabled on device, failed on iPad.");
-#endif
   [self scopedEnablePasswordManagementAndViewingUI];
 
   // Save form to be deleted later.
