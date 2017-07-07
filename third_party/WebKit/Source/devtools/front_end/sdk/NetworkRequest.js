e2f3e31e0a00a3134f24ee1e6ff685aedf4bb8fb
Nathan Bruer <allada@chromium.org>
2017-07-06 18:50:28 -0700

    [Devtools] HAR format to now support h2 & quic export formats
    This patch adds the ability for HAR to show better results for h2 and
    quic requests in the httpVersion field.
    
    R=caseq
    BUG=374557
    
    Change-Id: I17a7f28be01882fb6685a0ba3ba96296a38462af

--- third_party/WebKit/Source/devtools/front_end/sdk/NetworkRequest.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/NetworkRequest.js
@@ -670,13 +670,24 @@ SDK.NetworkRequest = class extends Common.Object {
     delete this._parsedFormParameters;
   }
 
+  _filteredProtocolName() {
+    var protocol = this.protocol.toLowerCase();
+    if (protocol === 'h2')
+      return 'http/2.0';
+    return protocol.replace(/^http\/2(\.0)?\+/, 'http/2.0+');
+  }
+
   /**
    * @return {string}
    */
   requestHttpVersion() {
     var headersText = this.requestHeadersText();
-    if (!headersText)
-      return this.requestHeaderValue('version') || this.requestHeaderValue(':version') || 'unknown';
+    if (!headersText) {
+      var version = this.requestHeaderValue('version') || this.requestHeaderValue(':version');
+      if (version)
+        return version;
+      return this._filteredProtocolName();
+    }
     var firstLine = headersText.split(/\r\n/)[0];
     var match = firstLine.match(/(HTTP\/\d+\.\d+)$/);
     return match ? match[1] : 'HTTP/0.9';
@@ -823,8 +834,12 @@ SDK.NetworkRequest = class extends Common.Object {
    */
   responseHttpVersion() {
     var headersText = this._responseHeadersText;
-    if (!headersText)
-      return this.responseHeaderValue('version') || this.responseHeaderValue(':version') || 'unknown';
+    if (!headersText) {
+      var version = this.responseHeaderValue('version') || this.responseHeaderValue(':version');
+      if (version)
+        return version;
+      return this._filteredProtocolName();
+    }
     var firstLine = headersText.split(/\r\n/)[0];
     var match = firstLine.match(/^(HTTP\/\d+\.\d+)/);
     return match ? match[1] : 'HTTP/0.9';
