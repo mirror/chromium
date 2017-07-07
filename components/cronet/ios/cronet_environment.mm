4d80a8165ef8038fd14f90ac22916dadfb9bfce4
Mike Dougherty <michaeldo@chromium.org>
2017-07-06 23:19:31 -0700

    Add additional global state to ios_global_state.
    Expands on the shared initialization to include base::AtExitManager,
    net::NetworkChangeNotifier, and base::MessageLoopForUI.
    
    With this change Cronet and ChromeWebView can successfully be used when
    built together in the framework created by
    //ios/web_view:cronet_web_view_combined.
    
    Bug: 738510
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: Iead4ba5d4f2376b09edf3f5c87a13829229b83b4

--- components/cronet/ios/cronet_environment.mm
+++ components/cronet/ios/cronet_environment.mm
@@ -6,7 +6,6 @@
 
 #include <utility>
 
-#include "base/at_exit.h"
 #include "base/atomicops.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
@@ -53,11 +52,6 @@
 #include "url/url_util.h"
 
 namespace {
-
-base::AtExitManager* g_at_exit_ = nullptr;
-net::NetworkChangeNotifier* g_network_change_notifier = nullptr;
-// MessageLoop on the main thread.
-base::MessageLoop* g_main_message_loop = nullptr;
 
 // Request context getter for Cronet.
 class CronetURLRequestContextGetter : public net::URLRequestContextGetter {
@@ -115,28 +109,20 @@ net::URLRequestContextGetter* CronetEnvironment::GetURLRequestContextGetter()
 void CronetEnvironment::Initialize() {
   // This method must be called once from the main thread.
   DCHECK_EQ([NSThread currentThread], [NSThread mainThread]);
-  if (!g_at_exit_)
-    g_at_exit_ = new base::AtExitManager;
 
-  ios_global_state::Create();
+  ios_global_state::CreateParams create_params;
+  create_params.install_at_exit_manager = true;
+  ios_global_state::Create(create_params);
   ios_global_state::StartTaskScheduler();
 
   url::Initialize();
-  base::CommandLine::Init(0, nullptr);
 
   // Without doing this, StatisticsRecorder::FactoryGet() leaks one histogram
   // per call after the first for a given name.
   base::StatisticsRecorder::Initialize();
 
-  // Create a message loop on the UI thread.
-  DCHECK(!base::MessageLoop::current());
-  DCHECK(!g_main_message_loop);
-  g_main_message_loop = new base::MessageLoopForUI();
-  base::MessageLoopForUI::current()->Attach();
-  // The network change notifier must be initialized so that registered
-  // delegates will receive callbacks.
-  DCHECK(!g_network_change_notifier);
-  g_network_change_notifier = net::NetworkChangeNotifier::Create();
+  ios_global_state::BuildMessageLoop();
+  ios_global_state::CreateNetworkChangeNotifier();
 }
 
 bool CronetEnvironment::StartNetLog(base::FilePath::StringType file_name,
@@ -269,7 +255,6 @@ CronetEnvironment::~CronetEnvironment() {
 
 void CronetEnvironment::InitializeOnNetworkThread() {
   DCHECK(network_io_thread_->task_runner()->BelongsToCurrentThread());
-  base::FeatureList::InitializeInstance(std::string(), std::string());
 
   static bool ssl_key_log_file_set = false;
   if (!ssl_key_log_file_set && !ssl_key_log_file_name_.empty()) {
