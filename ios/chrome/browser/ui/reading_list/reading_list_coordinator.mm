b4e958747f328d9135bf59c8bb2b61f00845edf2
Cooper Knaak <cooperknaak@google.com>
2017-07-07 13:52:33 -0700

    [iOS] Notify FeatureEngagementTracker of Opened Reading List Item Event
    There is more than one entry point for opening a reading list item.
    selecting an item in the reading list or long-pressing the item and
    selecting an action on the action sheet. ReadingListCoordinator was
    chosen because it is where the user action is ultimately handled and
    where the user metric is recorded.
    
    R=edchin@chromium.org, gchatz@chromium.org
    
    Bug: 738164
    Change-Id: I5f8252af7521811aa02b85c71508728ee77728c4

--- ios/chrome/browser/ui/reading_list/reading_list_coordinator.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_coordinator.mm
@@ -247,6 +247,10 @@ readingListCollectionViewController:
   }
 
   base::RecordAction(base::UserMetricsAction("MobileReadingListOpen"));
+  // Send the opened reading list item event when the user clicks on an item in
+  // their reading list.
+  FeatureEngagementTrackerFactory::GetForBrowserState(self.browserState)
+      ->NotifyEvent(feature_engagement_tracker::events::kOpenedReadingListItem);
 
   [readingListCollectionViewController willBeDismissed];
 
@@ -317,6 +321,11 @@ readingListCollectionViewController:
                           openNewTabWithURL:(const GURL&)URL
                                   incognito:(BOOL)incognito {
   base::RecordAction(base::UserMetricsAction("MobileReadingListOpen"));
+  // Send the opened reading list item event when the user long presses on a
+  // reading list item and selects when of the actions in the alert (open in
+  // new tab, open in new incognito tab, or open offline version in new tab).
+  FeatureEngagementTrackerFactory::GetForBrowserState(self.browserState)
+      ->NotifyEvent(feature_engagement_tracker::events::kOpenedReadingListItem);
 
   [readingListCollectionViewController willBeDismissed];
   [self.URLLoader webPageOrderedOpen:URL
