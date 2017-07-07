303d58fdbda3d3e8f64ff253640bc5a5ba28551d
Jan Krcal <jkrcal@chromium.org>
2017-07-07 14:11:27 +0200

    [LargeIconService] Touch icons from google server upon reading
    This CL marks on-demand favicons from the Google server as accessed upon
    reading from them favicon database. In order to do so:
     - it adds a function into LargeIconService so that the clients of this
       service do not have to keep the handle to FaviconService as well;
     - it reveals icon_url info into the LargeIconImageResult struct.
    
    This is the last CL in the series for removing old on-demand favicons.
    
    Bug: 709471
    Change-Id: I92d51be4b1bed14b6e6e06636e9aa7996b1fc060

--- components/favicon/core/large_icon_service.h
+++ components/favicon/core/large_icon_service.h
@@ -102,6 +102,13 @@ class LargeIconService : public KeyedService {
       const net::NetworkTrafficAnnotationTag& traffic_annotation,
       const base::Callback<void(bool success)>& callback);
 
+  // Mark that the icon at |icon_url| was requested now. This should be called
+  // after obtaining the icon by GetLargeIcon*OrFallbackStyle() for any icon
+  // that _may_ originate from the Google favicon server (i.e. if the caller
+  // uses GetLargeIconOrFallbackStyleFromGoogleServerSkippingLocalCache()).
+  // This postpones the automatic eviction of the favicon from the database.
+  void TouchIconFromGoogleServer(const GURL& icon_url);
+
  private:
   base::CancelableTaskTracker::TaskId GetLargeIconOrFallbackStyleImpl(
       const GURL& page_url,
