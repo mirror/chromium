e726fab542cbd3583f000d0db136033770266e73
Jao-ke Chin-Lee <jchinlee@chromium.org>
2017-07-07 14:58:23 -0700

    Add memory UKMs for existing UMA metrics.
    Bug: 730783, 728707
    Change-Id: I744c73d36ca76d97eff1e202fa20ba5d4a9adb7b

--- services/metrics/public/cpp/ukm_recorder.h
+++ services/metrics/public/cpp/ukm_recorder.h
@@ -20,6 +20,7 @@
 
 class ContextualSearchRankerLoggerImpl;
 class PluginInfoMessageFilter;
+class ProcessMemoryMetricsEmitter;
 class UkmPageLoadMetricsObserver;
 
 namespace autofill {
@@ -88,6 +89,7 @@ class METRICS_EXPORT UkmRecorder {
   friend autofill::AutofillMetrics;
   friend payments::JourneyLogger;
   friend ContextualSearchRankerLoggerImpl;
+  friend ProcessMemoryMetricsEmitter;
   friend PluginInfoMessageFilter;
   friend UkmPageLoadMetricsObserver;
   friend translate::TranslateRankerImpl;
