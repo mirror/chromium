063ed9bec07210a912822ac91b6ac0d85d793641
Daniele Castagna <dcastagna@chromium.org>
2017-07-07 15:47:06 -0400

    ozone: Fail when atomic is requested but not supported.
    The current behavior of DrmDevice when drm atomic is requested but not
    supported is to fall back to legacy page flip.
    
    This caused more issues than benefits, since it makes it not clear if
    we're running with atomic or with legacy pageflip.
    
    This CL makes Chrome fail to startup when drm atomic is not available
    but reqeusted.
    
    Bug:
    Change-Id: Ic6824a2b346d51cebbde1f26b7f05507aed64a25

--- ui/ozone/platform/drm/gpu/drm_device.cc
+++ ui/ozone/platform/drm/gpu/drm_device.cc
@@ -25,7 +25,7 @@
 #include "ui/ozone/platform/drm/common/drm_util.h"
 #include "ui/ozone/platform/drm/gpu/hardware_display_plane_manager_atomic.h"
 #include "ui/ozone/platform/drm/gpu/hardware_display_plane_manager_legacy.h"
-
+#include "ui/ozone/public/ozone_switches.h"
 
 namespace ui {
 
@@ -413,9 +413,10 @@ bool DrmDevice::Initialize(bool use_atomic) {
   if (use_atomic && SetCapability(DRM_CLIENT_CAP_ATOMIC, 1))
     plane_manager_.reset(new HardwareDisplayPlaneManagerAtomic());
 
-  LOG_IF(WARNING, use_atomic && !plane_manager_)
-      << "Drm atomic requested but capabilities don't allow it. Falling back "
-         "to legacy page flip.";
+  CHECK(!use_atomic || plane_manager_)
+      << "Drm atomic requested but capabilities don't allow it. To switch to "
+         "legacy page flip remove the command line flag "
+      << switches::kEnableDrmAtomic;
   if (!plane_manager_)
     plane_manager_.reset(new HardwareDisplayPlaneManagerLegacy());
   if (!plane_manager_->Initialize(this)) {
