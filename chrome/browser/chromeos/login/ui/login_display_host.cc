e4552bfe7b473995b849cd794aad09bbe13ec55c
khmel <khmel@google.com>
2017-07-07 09:57:33 -0700

    arc: Add UMA to track first ARC app launch request.
    This tracks the time from when UI is shown to user after sign in to
    when the user activates first ARC app.
    
    TEST=Manually on device
    BUG=b/63276467
    
    Change-Id: If77dbcba01e0d17dd702887c22e60706f7311928

--- chrome/browser/chromeos/login/ui/login_display_host.cc
+++ chrome/browser/chromeos/login/ui/login_display_host.cc
@@ -4,8 +4,28 @@
 
 #include "chrome/browser/chromeos/login/ui/login_display_host.h"
 
+#include "chrome/browser/chromeos/login/ui/login_display_host_observer.h"
+
 namespace chromeos {
 
 // static
 LoginDisplayHost* LoginDisplayHost::default_host_ = nullptr;
+
+LoginDisplayHost::LoginDisplayHost() = default;
+
+LoginDisplayHost::~LoginDisplayHost() = default;
+
+void LoginDisplayHost::AddObserver(LoginDisplayHostObserver* observer) {
+  observer_list_.AddObserver(observer);
 }
+
+void LoginDisplayHost::RemoveObserver(LoginDisplayHostObserver* observer) {
+  observer_list_.RemoveObserver(observer);
+}
+
+void LoginDisplayHost::NotifyClosed() {
+  for (auto& observer : observer_list_)
+    observer.OnLoginDisplayHostClosed();
+}
+
+}  // namespace chromeos
