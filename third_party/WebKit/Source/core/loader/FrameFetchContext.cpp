9d2c2a7f4a6890a7942c5913265883709f384fa5
Tarun Bansal <tbansal@chromium.org>
2017-07-07 13:38:56 -0700

    Parse client hints lifetime header
    Also, refactor the client hints and store all client hints as enum.
    In a future CL, this ordered list of client hints would be persisted
    on the disk.
    
    Bug: 735518
    Change-Id: I5e743398ed1c3484def527543fabcfad99a0f14a

--- third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
+++ third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
@@ -786,17 +786,26 @@ void FrameFetchContext::AddClientHintsIfNecessary(
   if (!RuntimeEnabledFeatures::ClientHintsEnabled())
     return;
 
+  // TODO(tbansal): https://crbug.com/735518. Client hint should also be sent
+  // if the origin for the |request| had persisted the client hint preference
+  // in the past.
   bool should_send_device_ram =
-      GetClientHintsPreferences().ShouldSendDeviceRAM() ||
-      hints_preferences.ShouldSendDeviceRAM();
-  bool should_send_dpr = GetClientHintsPreferences().ShouldSendDPR() ||
-                         hints_preferences.ShouldSendDPR();
+      GetClientHintsPreferences().ShouldSend(kWebClientHintsTypeDeviceRam) ||
+      hints_preferences.ShouldSend(kWebClientHintsTypeDeviceRam);
+  bool should_send_dpr =
+      GetClientHintsPreferences().ShouldSend(kWebClientHintsTypeDPR) ||
+      hints_preferences.ShouldSend(kWebClientHintsTypeDPR);
   bool should_send_resource_width =
-      GetClientHintsPreferences().ShouldSendResourceWidth() ||
-      hints_preferences.ShouldSendResourceWidth();
+      GetClientHintsPreferences().ShouldSend(
+          kWebClientHintsTypeResourceWidth) ||
+      hints_preferences.ShouldSend(kWebClientHintsTypeResourceWidth);
   bool should_send_viewport_width =
-      GetClientHintsPreferences().ShouldSendViewportWidth() ||
-      hints_preferences.ShouldSendViewportWidth();
+      GetClientHintsPreferences().ShouldSend(
+          kWebClientHintsTypeViewportWidth) ||
+      hints_preferences.ShouldSend(kWebClientHintsTypeViewportWidth);
+  static_assert(
+      kWebClientHintsTypeResourceWidth + 1 == kWebClientHintsTypeNumValues,
+      "unhandled client hints");
 
   if (should_send_device_ram) {
     int64_t physical_memory = MemoryCoordinator::GetPhysicalMemoryMB();
