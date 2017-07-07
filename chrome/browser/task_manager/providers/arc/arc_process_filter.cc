f22d595201df9e05f097dec57069e12faaed5b63
Eliot Courtney <edcourtney@chromium.org>
2017-07-07 14:22:56 +0900

    [Task Manager] Implicitly white list more process states.
    If you minimise Gmail, it disappears from the task manager, so allow the
    LAST_ACTIVITY process state to be displayed for ARC processes.
    
    Additionally allow FOREGROUND_SERVICE since foreground services are user
    visible.
    
    BUG=738651
    
    Change-Id: I403b9946fbbb29b115fe1877f63bb64e3fe3b2c7

--- chrome/browser/task_manager/providers/arc/arc_process_filter.cc
+++ chrome/browser/task_manager/providers/arc/arc_process_filter.cc
@@ -35,14 +35,32 @@ bool ArcProcessFilter::ShouldDisplayProcess(
   // ProcessState::TOP: Process is hosting the current top activities
   // ProcessState::IMPORTANT_FOREGROUND: Process is important to the user
   // ProcessState::TOP_SLEEPING: Same as TOP, but while the device is sleeping
+  // ProcessState::LAST_ACTIVITY: The last activity - see crbug.com/738651.
+  // ProcessState::FOREGROUND_SERVICE: Foreground service - in Android, the user
+  //   is made aware of these via a notification.
   switch (process.process_state()) {
     case arc::mojom::ProcessState::TOP:  // Fallthrough.
     case arc::mojom::ProcessState::IMPORTANT_FOREGROUND:
     case arc::mojom::ProcessState::TOP_SLEEPING:
+    case arc::mojom::ProcessState::LAST_ACTIVITY:
+    case arc::mojom::ProcessState::FOREGROUND_SERVICE:
       return true;
-    default:
-      break;
+    case arc::mojom::ProcessState::NONEXISTENT:  // Fallthrough.
+    case arc::mojom::ProcessState::PERSISTENT:
+    case arc::mojom::ProcessState::PERSISTENT_UI:
+    case arc::mojom::ProcessState::BOUND_FOREGROUND_SERVICE:
+    case arc::mojom::ProcessState::IMPORTANT_BACKGROUND:
+    case arc::mojom::ProcessState::BACKUP:
+    case arc::mojom::ProcessState::HEAVY_WEIGHT:
+    case arc::mojom::ProcessState::SERVICE:
+    case arc::mojom::ProcessState::RECEIVER:
+    case arc::mojom::ProcessState::HOME:
+    case arc::mojom::ProcessState::CACHED_ACTIVITY:
+    case arc::mojom::ProcessState::CACHED_ACTIVITY_CLIENT:
+    case arc::mojom::ProcessState::CACHED_EMPTY:
+      return false;
   }
+  NOTREACHED();
   return false;
 }
 
