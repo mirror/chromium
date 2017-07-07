2bf16216a8389193d485cb561b058eb93cc23b1d
Sidney San Martín <sdy@chromium.org>
2017-07-07 12:14:52 -0700

    Use a custom frame view for browser windows.
    The new frame view inherits from NSThemeFrame and overrides a few methods that
    determine button and content placement and replaces some swizzling and frame
    tweaking.
    
    A manual test looks roughly like this, but I skip some tests on some OSs (e.g.
    I didn't put Accessibility Inspector on each test system):
    
      - Launch Chrome.
      - Verify that everything looks generally right.
      - Verify that the window buttons are in the same place as shipping Chrome.
      - If NSVisualEffectView is available, verify that things look right when I
        move a window around behind the browser window.
      - Verify that the window buttons respond on hover.
      - Verify that the window buttons can be clicked.
      - Close the window, open a new one, repeat the above checks.
      - Resize the window, repeat the above checks.
      - Bring the window into and out of fullscreen a couple of times and verify
        that the animations look the same as the current ones.
      - Repeat the above with "Always Show Toolbar In Full Screen" turned on/off.
      - Open an Incognito window.
      - Verify that the inactive window buttons are visible.
      - Verify that the inactive window buttons respond on hover.
      - Verify that the inactive window buttons can be clicked, but don't bring the
        window to the front on mouse down.
      - Repeat all of the above checks for the incognito window.
      - On 10.9, pay special attention to the positions of the profile picker and
        fullscreen button.
      - Navigate here: data:text/html,<script>window.onclick=()=>window.open('',
        '', 'width: 100; height: 100');</script>
      - Click the page to open a popup window. Repeat all of the above checks.
      - Turn on VoiceOver. Verify that you can navigate to and activate the window
        buttons. Turn off VoiceOver.
      - Launch Accessibility Inspector. Click the crosshair button in the toolbar
        and verify that you can use the mouse to target the window buttons.
      - On newer OSs, launch Chrome in RTL: -NSForceRightToLeftWritingDirection YES
        -AppleTextDirection YES --force-ui-direction=rtl --enable-features=MacRTL
      - On 10.12+, verify that the window buttons are on the right side of the
        window.
      - Repeat for each of macOS 10.{9..13}.
    
    
    Bug: 605219, 651287, 730679, 734713, 737206
    Change-Id: Iee9b3b03ad677255b5df50c26742551347d7d4d8

--- chrome/browser/ui/cocoa/browser_window_controller_private.mm
+++ chrome/browser/ui/cocoa/browser_window_controller_private.mm
@@ -26,7 +26,6 @@
 #import "chrome/browser/ui/cocoa/browser_window_fullscreen_transition.h"
 #import "chrome/browser/ui/cocoa/browser_window_layout.h"
 #import "chrome/browser/ui/cocoa/constrained_window/constrained_window_sheet_controller.h"
-#import "chrome/browser/ui/cocoa/custom_frame_view.h"
 #import "chrome/browser/ui/cocoa/dev_tools_controller.h"
 #import "chrome/browser/ui/cocoa/fast_resize_view.h"
 #import "chrome/browser/ui/cocoa/find_bar/find_bar_cocoa_controller.h"
@@ -60,6 +59,10 @@
 
 using content::RenderWidgetHostView;
 using content::WebContents;
+
+@interface NSView (Private)
+- (NSPoint)_fullScreenButtonOrigin;
+@end
 
 namespace {
 
