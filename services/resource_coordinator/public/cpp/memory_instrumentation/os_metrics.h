6c9faec129bd18b585d8ce543e1395d6803c5a0b
Hector Dearman <hjd@google.com>
2017-07-07 15:25:38 +0100

    memory-infra: Move mmap processing code to OSMetrics
    Bug: 728199
    Change-Id: Icf0e54c66704901e31004c3d632290def1dfb558

--- services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics.h
+++ services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics.h
@@ -5,15 +5,29 @@
 #define SERVICES_RESOURCE_COORDINATOR_PUBLIC_CPP_MEMORY_INSTRUMENTATION_OS_METRICS_H_
 
 #include "base/process/process_handle.h"
+#include "base/trace_event/process_memory_dump.h"
+#include "build/build_config.h"
 #include "services/resource_coordinator/public/cpp/resource_coordinator_export.h"
 #include "services/resource_coordinator/public/interfaces/memory_instrumentation/memory_instrumentation.mojom.h"
 
 namespace memory_instrumentation {
 
+namespace OSMetrics {
+
 mojom::RawOSMemDumpPtr SERVICES_RESOURCE_COORDINATOR_PUBLIC_CPP_EXPORT
 GetOSMemoryDump(base::ProcessId pid);
 
+bool SERVICES_RESOURCE_COORDINATOR_PUBLIC_CPP_EXPORT
+DumpProcessMemoryMaps(base::ProcessId, base::trace_event::ProcessMemoryDump*);
+
 void FillOSMemoryDump(base::ProcessId pid, mojom::RawOSMemDump* dump);
+
+#if defined(OS_LINUX) || defined(OS_ANDROID)
+void SERVICES_RESOURCE_COORDINATOR_PUBLIC_CPP_EXPORT
+SetProcSmapsForTesting(FILE*);
+#endif
+
+}  // namespace OSMetrics
 
 }  // namespace memory_instrumentation
 
