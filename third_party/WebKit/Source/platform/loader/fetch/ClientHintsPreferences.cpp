302b671caea960c6eddf1df68a7c666025be9142
Tarun Bansal <tbansal@chromium.org>
2017-07-07 13:54:17 -0700

    Parse client hints lifetime header
    Also, refactor the client hints and store all client hints as enum.
    In a future CL, this ordered list of client hints would be persisted
    on the disk.
    
    Bug: 735518
    Change-Id: I5e743398ed1c3484def527543fabcfad99a0f14a

--- third_party/WebKit/Source/platform/loader/fetch/ClientHintsPreferences.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/ClientHintsPreferences.cpp
@@ -9,18 +9,36 @@
 
 namespace blink {
 
-ClientHintsPreferences::ClientHintsPreferences()
-    : should_send_device_ram_(false),
-      should_send_dpr_(false),
-      should_send_resource_width_(false),
-      should_send_viewport_width_(false) {}
+namespace {
+
+void ParseAcceptChHeader(const String& header_value,
+                         bool enabled_types[kWebClientHintsTypeNumValues]) {
+  CommaDelimitedHeaderSet accept_client_hints_header;
+  ParseCommaDelimitedHeader(header_value, accept_client_hints_header);
+
+  enabled_types[kWebClientHintsTypeDeviceRam] =
+      RuntimeEnabledFeatures::DeviceRAMHeaderEnabled() &&
+      accept_client_hints_header.Contains("device-ram");
+  enabled_types[kWebClientHintsTypeDPR] =
+      accept_client_hints_header.Contains("dpr");
+  enabled_types[kWebClientHintsTypeResourceWidth] =
+      accept_client_hints_header.Contains("width");
+  enabled_types[kWebClientHintsTypeViewportWidth] =
+      accept_client_hints_header.Contains("viewport-width");
+
+  static_assert(
+      kWebClientHintsTypeViewportWidth + 1 == kWebClientHintsTypeNumValues,
+      "unhandled client hints");
+}
+
+}  // namespace
+
+ClientHintsPreferences::ClientHintsPreferences() : enabled_types_{false} {}
 
 void ClientHintsPreferences::UpdateFrom(
     const ClientHintsPreferences& preferences) {
-  should_send_device_ram_ = preferences.should_send_device_ram_;
-  should_send_dpr_ = preferences.should_send_dpr_;
-  should_send_resource_width_ = preferences.should_send_resource_width_;
-  should_send_viewport_width_ = preferences.should_send_viewport_width_;
+  for (size_t i = 0; i < kWebClientHintsTypeNumValues; ++i)
+    enabled_types_[i] = preferences.enabled_types_[i];
 }
 
 void ClientHintsPreferences::UpdateFromAcceptClientHintsHeader(
@@ -29,32 +47,56 @@ void ClientHintsPreferences::UpdateFromAcceptClientHintsHeader(
   if (!RuntimeEnabledFeatures::ClientHintsEnabled() || header_value.IsEmpty())
     return;
 
-  CommaDelimitedHeaderSet accept_client_hints_header;
-  ParseCommaDelimitedHeader(header_value, accept_client_hints_header);
-  if (RuntimeEnabledFeatures::DeviceRAMHeaderEnabled() &&
-      accept_client_hints_header.Contains("device-ram")) {
+  ParseAcceptChHeader(header_value, enabled_types_);
+
+  if (enabled_types_[kWebClientHintsTypeDeviceRam]) {
     if (context)
       context->CountClientHintsDeviceRAM();
-    should_send_device_ram_ = true;
   }
 
-  if (accept_client_hints_header.Contains("dpr")) {
+  if (enabled_types_[kWebClientHintsTypeDPR]) {
     if (context)
       context->CountClientHintsDPR();
-    should_send_dpr_ = true;
   }
 
-  if (accept_client_hints_header.Contains("width")) {
+  if (enabled_types_[kWebClientHintsTypeResourceWidth]) {
     if (context)
       context->CountClientHintsResourceWidth();
-    should_send_resource_width_ = true;
   }
 
-  if (accept_client_hints_header.Contains("viewport-width")) {
+  if (enabled_types_[kWebClientHintsTypeViewportWidth]) {
     if (context)
       context->CountClientHintsViewportWidth();
-    should_send_viewport_width_ = true;
   }
+
+  static_assert(
+      kWebClientHintsTypeViewportWidth + 1 == kWebClientHintsTypeNumValues,
+      "unhandled client hints");
+}
+
+void ClientHintsPreferences::UpdatePersistentHintsFromHeaders(
+    const String& accept_ch_header_value,
+    const String& accept_ch_lifetime_header_value,
+    bool enabled_types[kWebClientHintsTypeNumValues],
+    int64_t* persist_duration_seconds) {
+  *persist_duration_seconds = -1;
+
+  if (!RuntimeEnabledFeatures::ClientHintsEnabled() ||
+      !RuntimeEnabledFeatures::ClientHintsPersistenceEnabled() ||
+      accept_ch_header_value.IsEmpty() ||
+      accept_ch_lifetime_header_value.IsEmpty()) {
+    return;
+  }
+
+  bool conversion_ok = false;
+  *persist_duration_seconds =
+      accept_ch_lifetime_header_value.ToInt64Strict(&conversion_ok);
+  if (!conversion_ok || *persist_duration_seconds <= 0) {
+    *persist_duration_seconds = -1;
+    return;
+  }
+
+  ParseAcceptChHeader(accept_ch_header_value, enabled_types);
 }
 
 }  // namespace blink
