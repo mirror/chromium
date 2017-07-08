fa985d30edfbeabbf0df7a96ae19f72fc7017515
Toni Barzic <tbarzic@google.com>
2017-07-07 18:23:47 -0700

    Close lock screen app window on suspend/display turning off
    lock_screen_apps::StateController now observes power manager and
    closes any present lock screen app windows when device display is
    turned of without explicit user action (i.e. the app window will
    not be closed if the user changes screen brightness to 0) or if
    the device suspends.
    
    BUG=740236
    
    Change-Id: If40fe8110afd05fa0f3efa8bda870c2c66104f83

--- chrome/browser/chromeos/lock_screen_apps/state_controller.cc
+++ chrome/browser/chromeos/lock_screen_apps/state_controller.cc
@@ -17,6 +17,7 @@
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/chromeos_switches.h"
+#include "chromeos/dbus/dbus_thread_manager.h"
 #include "components/prefs/pref_service.h"
 #include "components/session_manager/core/session_manager.h"
 #include "content/public/common/service_manager_connection.h"
@@ -54,6 +55,7 @@ StateController::StateController()
       app_window_observer_(this),
       session_observer_(this),
       input_devices_observer_(this),
+      power_manager_client_observer_(this),
       weak_ptr_factory_(this) {
   DCHECK(!g_instance);
   DCHECK(IsEnabled());
@@ -138,6 +140,8 @@ void StateController::OnProfilesReady(Profile* primary_profile,
                            lock_screen_profile->GetOriginalProfile());
 
   input_devices_observer_.Add(ui::InputDeviceManager::GetInstance());
+  power_manager_client_observer_.Add(
+      chromeos::DBusThreadManager::Get()->GetPowerManagerClient());
   session_observer_.Add(session_manager::SessionManager::Get());
   OnSessionStateChanged();
 
@@ -201,6 +205,15 @@ void StateController::OnStylusStateChanged(ui::StylusState state) {
 
   if (state == ui::StylusState::REMOVED)
     RequestNewLockScreenNote();
+}
+
+void StateController::BrightnessChanged(int level, bool user_initiated) {
+  if (level == 0 && !user_initiated)
+    ResetNoteTakingWindowAndMoveToNextState(true /* close_window */);
+}
+
+void StateController::SuspendImminent() {
+  ResetNoteTakingWindowAndMoveToNextState(true /* close_window */);
 }
 
 extensions::AppWindow* StateController::CreateAppWindowForLockScreenAction(
