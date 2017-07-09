57f88737a2b7d809da347390830403c57dad293c
Eugene Girard <girard@chromium.org>
2017-07-08 22:22:30 -0400

    Keep 'open in incognito window' enabled, even in incognito
    Bug: 739985
    Change-Id: Ica5bc5717ae37d5f022de194315fe4bddc5f81e5

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -1503,9 +1503,9 @@ bool RenderViewContextMenu::IsCommandIdEnabled(int id) const {
 
     case IDC_CONTENT_CONTEXT_TRANSLATE:
       return IsTranslateEnabled();
-
-    case IDC_CONTENT_CONTEXT_OPENLINKNEWTAB:
     case IDC_CONTENT_CONTEXT_OPENLINKNEWWINDOW:
+      return !browser_context_->IsOffTheRecord() && params_.link_url.is_valid();
+    case IDC_CONTENT_CONTEXT_OPENLINKNEWTAB:
     case IDC_CONTENT_CONTEXT_OPENLINKINPROFILE:
       return params_.link_url.is_valid();
 
@@ -2117,7 +2117,7 @@ bool RenderViewContextMenu::IsRouteMediaEnabled() const {
 }
 
 bool RenderViewContextMenu::IsOpenLinkOTREnabled() const {
-  if (browser_context_->IsOffTheRecord() || !params_.link_url.is_valid())
+  if (!params_.link_url.is_valid())
     return false;
 
   if (!chrome::IsURLAllowedInIncognito(params_.link_url, browser_context_))
