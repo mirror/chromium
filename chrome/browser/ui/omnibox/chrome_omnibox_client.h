417508da2f460443f673253e39e0de37a98eff2a
Catherine Chung <catherinechung@google.com>
2017-07-10 12:56:29 -0700

    Add new tab feature engagement tracker
    Bug:
    Change-Id: I4fe7fa1a105c305c8b60413f509d3de2d73afd6f

--- chrome/browser/ui/omnibox/chrome_omnibox_client.h
+++ chrome/browser/ui/omnibox/chrome_omnibox_client.h
@@ -36,8 +36,8 @@ class ChromeOmniboxClient : public OmniboxClient {
   bool IsSearchResultsPage() const override;
   bool IsLoading() const override;
   bool IsPasteAndGoEnabled() const override;
-  bool IsNewTabPage(const std::string& url) const override;
-  bool IsHomePage(const std::string& url) const override;
+  bool IsNewTabPage(const GURL& url) const override;
+  bool IsHomePage(const GURL& url) const override;
   const SessionID& GetSessionID() const override;
   bookmarks::BookmarkModel* GetBookmarkModel() override;
   TemplateURLService* GetTemplateURLService() override;
