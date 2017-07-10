8cb41bff7717c1d1708aacc5b9ec0bdadf9e21b3
Michael Thiessen <mthiesse@chromium.org>
2017-07-10 15:45:21 -0400

    VR: Check focus state browser-side before sending input to renderer.
    Unfortunately we have to check focus twice before sending it to a VrDisplay,
    once in the browser to do view-level focus, then again in the renderer to do
    frame-level focus.
    
    This CL ensures browser-side focus before sending displayactivate events
    or head pose information to the WebVR page.
    
    This CL slightly changes the semantics of OnWebContentsFocused from firing
    only when an embedded fullscreen widget changes focus (wtf?) to firing any
    time any RenderWidgetHost changes focus. I believe it also used to only fire
    on aura only, but now it will fire on all platforms.
    
    Bug: 687411, 738239
    Change-Id: Ibeee427d602d7fef8ce533669b45d9cc5f8ffa03

--- content/browser/web_contents/web_contents_view_android.h
+++ content/browser/web_contents/web_contents_view_android.h
@@ -96,7 +96,6 @@ class WebContentsViewAndroid : public WebContentsView,
                      const DragEventSourceInfo& event_info,
                      RenderWidgetHostImpl* source_rwh) override;
   void UpdateDragCursor(blink::WebDragOperation operation) override;
-  void GotFocus() override;
   void TakeFocus(bool reverse) override;
 
   // ui::ViewClient implementation.
