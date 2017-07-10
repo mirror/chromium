cc1e1851a9019518fd63ebdcb5da628273780bfc
Daniel Bratell <bratell@opera.com>
2017-07-10 11:00:26 +0200

    Renamed a local class Function in a test to not collide with WTF.
    There is a Function<> template in WTF/Functional.h which is shadowed
    or collides with any local definitions of Function. This is not always
    a problem, but it caused problems in Jumbo builds so let us rename
    it to something harmless.
    
    Change-Id: Ib594b2673a1dce6b4647445d3377b1e1099849da

--- third_party/WebKit/Source/bindings/core/v8/ScriptPromiseResolverTest.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScriptPromiseResolverTest.cpp
@@ -19,16 +19,17 @@ namespace blink {
 
 namespace {
 
-class Function : public ScriptFunction {
+// Renamed because it shadows the Function<> template in WTF/Function.h.
+class TestHelperFunction : public ScriptFunction {
  public:
   static v8::Local<v8::Function> CreateFunction(ScriptState* script_state,
                                                 String* value) {
-    Function* self = new Function(script_state, value);
+    TestHelperFunction* self = new TestHelperFunction(script_state, value);
     return self->BindToV8Function();
   }
 
  private:
-  Function(ScriptState* script_state, String* value)
+  TestHelperFunction(ScriptState* script_state, String* value)
       : ScriptFunction(script_state), value_(value) {}
 
   ScriptValue Call(ScriptValue value) override {
@@ -80,8 +81,9 @@ TEST_F(ScriptPromiseResolverTest, resolve) {
   ASSERT_FALSE(promise.IsEmpty());
   {
     ScriptState::Scope scope(GetScriptState());
-    promise.Then(Function::CreateFunction(GetScriptState(), &on_fulfilled),
-                 Function::CreateFunction(GetScriptState(), &on_rejected));
+    promise.Then(
+        TestHelperFunction::CreateFunction(GetScriptState(), &on_fulfilled),
+        TestHelperFunction::CreateFunction(GetScriptState(), &on_rejected));
   }
 
   EXPECT_EQ(String(), on_fulfilled);
@@ -128,8 +130,9 @@ TEST_F(ScriptPromiseResolverTest, reject) {
   ASSERT_FALSE(promise.IsEmpty());
   {
     ScriptState::Scope scope(GetScriptState());
-    promise.Then(Function::CreateFunction(GetScriptState(), &on_fulfilled),
-                 Function::CreateFunction(GetScriptState(), &on_rejected));
+    promise.Then(
+        TestHelperFunction::CreateFunction(GetScriptState(), &on_fulfilled),
+        TestHelperFunction::CreateFunction(GetScriptState(), &on_rejected));
   }
 
   EXPECT_EQ(String(), on_fulfilled);
@@ -176,8 +179,9 @@ TEST_F(ScriptPromiseResolverTest, stop) {
   ASSERT_FALSE(promise.IsEmpty());
   {
     ScriptState::Scope scope(GetScriptState());
-    promise.Then(Function::CreateFunction(GetScriptState(), &on_fulfilled),
-                 Function::CreateFunction(GetScriptState(), &on_rejected));
+    promise.Then(
+        TestHelperFunction::CreateFunction(GetScriptState(), &on_fulfilled),
+        TestHelperFunction::CreateFunction(GetScriptState(), &on_rejected));
   }
 
   GetExecutionContext()->NotifyContextDestroyed();
@@ -316,8 +320,9 @@ TEST_F(ScriptPromiseResolverTest, resolveVoid) {
   ASSERT_FALSE(promise.IsEmpty());
   {
     ScriptState::Scope scope(GetScriptState());
-    promise.Then(Function::CreateFunction(GetScriptState(), &on_fulfilled),
-                 Function::CreateFunction(GetScriptState(), &on_rejected));
+    promise.Then(
+        TestHelperFunction::CreateFunction(GetScriptState(), &on_fulfilled),
+        TestHelperFunction::CreateFunction(GetScriptState(), &on_rejected));
   }
 
   resolver->Resolve();
@@ -340,8 +345,9 @@ TEST_F(ScriptPromiseResolverTest, rejectVoid) {
   ASSERT_FALSE(promise.IsEmpty());
   {
     ScriptState::Scope scope(GetScriptState());
-    promise.Then(Function::CreateFunction(GetScriptState(), &on_fulfilled),
-                 Function::CreateFunction(GetScriptState(), &on_rejected));
+    promise.Then(
+        TestHelperFunction::CreateFunction(GetScriptState(), &on_fulfilled),
+        TestHelperFunction::CreateFunction(GetScriptState(), &on_rejected));
   }
 
   resolver->Reject();
