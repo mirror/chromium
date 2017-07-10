06d6fe043911686bcdcf984e19fad94ddf14b51b
Kuang-che Wu <kcwu@chromium.org>
2017-07-10 16:21:40 +0800

    Revise vp9 parser fuzzer for compressed header
    This CL started to fuzz parsing of compressed header
    
    Bug: None
    Change-Id: I45f6b395425c96f4bfeb0ce83536771bfe996235

--- media/filters/vp9_parser_fuzzertest.cc
+++ media/filters/vp9_parser_fuzzertest.cc
@@ -21,6 +21,16 @@ Environment* env = new Environment();
 
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  uint8_t fuzz_type;
+  if (size < sizeof(fuzz_type))
+    return 0;
+  fuzz_type = data[0];
+  data += sizeof(fuzz_type);
+  size -= sizeof(fuzz_type);
+
+  if (fuzz_type > 2)
+    return 0;
+
   const uint8_t* ivf_payload = nullptr;
   media::IvfParser ivf_parser;
   media::IvfFileHeader ivf_file_header;
@@ -29,12 +39,12 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (!ivf_parser.Initialize(data, size, &ivf_file_header))
     return 0;
 
+  media::Vp9Parser vp9_parser(fuzz_type == 1);
   // Parse until the end of stream/unsupported stream/error in stream is found.
   while (ivf_parser.ParseNextFrame(&ivf_frame_header, &ivf_payload)) {
-    // TODO(kcwu): fuzzing with parsing_compressed_header=true.
-    media::Vp9Parser vp9_parser(false);
     media::Vp9FrameHeader vp9_frame_header;
     vp9_parser.SetStream(ivf_payload, ivf_frame_header.frame_size);
+    // TODO(kcwu): further fuzzing the case of Vp9Parser::kAwaitingRefresh.
     while (vp9_parser.ParseNextFrame(&vp9_frame_header) ==
            media::Vp9Parser::kOk) {
       // Repeat until all frames processed.
