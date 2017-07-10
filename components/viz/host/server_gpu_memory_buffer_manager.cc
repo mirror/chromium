161d45a4e9697134aca9b67dbb2ea05bedf3933b
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-07-10 16:53:42 -0400

    viz: Fix destroying gfx::GpuMemoryBuffers.
    The destruction callback of a GpuMemoryBufferImpls can be called on any
    thread[1]. So make sure the destruction callbacks hops onto the correct
    thread before mutating local data structures or sending mojo messages.
    
    [1] See doc for GpuMemoryBufferImpl::CreateFromHandle()
    https://cs.chromium.org/chromium/src/gpu/ipc/client/gpu_memory_buffer_impl.h?l=28
    
    BUG=733482
    
    Change-Id: Ifc3fecd6a8bb42aa61f3d2d9667bd0e931d7261c

--- components/viz/host/server_gpu_memory_buffer_manager.cc
+++ components/viz/host/server_gpu_memory_buffer_manager.cc
@@ -6,7 +6,7 @@
 
 #include "base/logging.h"
 #include "base/strings/stringprintf.h"
-#include "base/threading/sequenced_task_runner_handle.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "base/trace_event/process_memory_dump.h"
 #include "gpu/ipc/client/gpu_memory_buffer_impl.h"
@@ -27,7 +27,7 @@ ServerGpuMemoryBufferManager::ServerGpuMemoryBufferManager(
     : gpu_service_(gpu_service),
       client_id_(client_id),
       native_configurations_(gpu::GetNativeGpuMemoryBufferConfigurations()),
-      task_runner_(base::SequencedTaskRunnerHandle::Get()),
+      task_runner_(base::ThreadTaskRunnerHandle::Get()),
       weak_factory_(this) {}
 
 ServerGpuMemoryBufferManager::~ServerGpuMemoryBufferManager() {}
@@ -41,6 +41,8 @@ void ServerGpuMemoryBufferManager::AllocateGpuMemoryBuffer(
     gpu::SurfaceHandle surface_handle,
     base::OnceCallback<void(const gfx::GpuMemoryBufferHandle&)> callback) {
   DCHECK(task_runner_->RunsTasksInCurrentSequence());
+  if (!weak_ptr_)
+    weak_ptr_ = weak_factory_.GetWeakPtr();
   if (gpu::GetNativeGpuMemoryBufferType() != gfx::EMPTY_BUFFER) {
     const bool is_native = native_configurations_.find(std::make_pair(
                                format, usage)) != native_configurations_.end();
@@ -108,10 +110,21 @@ ServerGpuMemoryBufferManager::CreateGpuMemoryBuffer(
   wait_event.Wait();
   if (handle.is_null())
     return nullptr;
+  // The destruction callback can be called on any thread. So use an
+  // intermediate callback here as the destruction callback, which bounces off
+  // onto the |task_runner_| thread to do the real work.
+  auto real_destroy_callback =
+      base::Bind(&ServerGpuMemoryBufferManager::DestroyGpuMemoryBuffer,
+                 weak_ptr_, id, client_id_);
   return gpu::GpuMemoryBufferImpl::CreateFromHandle(
       handle, size, format, usage,
-      base::Bind(&ServerGpuMemoryBufferManager::DestroyGpuMemoryBuffer,
-                 weak_factory_.GetWeakPtr(), id, client_id_));
+      base::Bind(
+          [](scoped_refptr<base::SingleThreadTaskRunner> task_runner,
+             const gpu::GpuMemoryBufferImpl::DestructionCallback callback,
+             const gpu::SyncToken& sync_token) {
+            task_runner->PostTask(FROM_HERE, base::Bind(callback, sync_token));
+          },
+          task_runner_, real_destroy_callback));
 }
 
 void ServerGpuMemoryBufferManager::SetDestructionSyncToken(
