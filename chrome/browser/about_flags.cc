4aed69afdc9692fd19b40dc49e24725c3132eb55
Evan Stade <estade@chromium.org>
2017-07-10 16:08:49 -0600

    Delay switching reload button from reload appearance to stop appearance.
    This implements ainslie's suggestion "delay the transition from reload
    to stop for N seconds", for N = 1.35 simply because that constant is already
    in use for a similar UI update delay. I put this one behind a flag because
    it is more of a behavioral change (since the button actually does something)
    and the correct N value seems unsettled.
    
    A good site to test this on is the Chrome Web Store. When I open a new tab
    to the page for a particular in extension/theme/etc., I see 3-4 switches
    back and forth between load states. With the flag enabled, I see 0-1 toggles
    (depending on how long the initial load takes).
    
    Bug:734104
    Change-Id: Ib32ebbdc161b75b37fefe2c4ca94fd5af4b56f5f

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3157,6 +3157,14 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(chrome::android::kSpannableInlineAutocomplete)},
 #endif  // defined(OS_ANDROID)
 
+#if defined(TOOLKIT_VIEWS)
+    {"delay-reload-stop-button-change",
+     flag_descriptions::kDelayReloadStopButtonChangeName,
+     flag_descriptions::kDelayReloadStopButtonChangeDescription,
+     kOsCrOS | kOsWin | kOsLinux,
+     SINGLE_DISABLE_VALUE_TYPE(switches::kDelayReloadStopButtonChange)},
+#endif  // TOOLKIT_VIEWS
+
     // NOTE: Adding new command-line switches requires adding corresponding
     // entries to enum "LoginCustomFlags" in histograms/enums.xml. See note in
     // enums.xml and don't forget to run AboutFlagsHistogramTest unit test.
