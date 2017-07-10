a8507963fd976da0e25d6133bc8ac797ca4b9b33
Aaron Leventhal <aleventhal@chromium.org>
2017-07-10 12:00:01 -0400

    Consolidates 3 AX properties into single restrictions property for accessibility objects
    Replaces 3 previous properties with a restrictions property, which provides input restrictions associated with an object.
    No/undefined value for a control means it is enabled.
    read_only = control or group of controls that disallows input
    disabled = textbox that allows focus/selection but not input
    
    Benefits:
    - Combines many properties related to controls into one, and removes some complexity.
    - Our cross platform code now has a more universal definition of what readonly is
    - Support aria-disabled on any object, and disabled on a fieldset
    - Support aria-readonly on any control, and readonly only on text fields where it applies
    - Cleaner and more concise code overall
    
    Bug=None
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I10cbafa52a42dcb651365787d28eda863eba58f8

--- ui/views/accessibility/native_view_accessibility_base.cc
+++ ui/views/accessibility/native_view_accessibility_base.cc
@@ -69,7 +69,8 @@ const ui::AXNodeData& NativeViewAccessibilityBase::GetData() const {
   // rather than possibly crashing.
   if (!view_->GetWidget() || view_->GetWidget()->IsClosed()) {
     data_.role = ui::AX_ROLE_UNKNOWN;
-    data_.AddState(ui::AX_STATE_DISABLED);
+    data_.AddIntAttribute(ui::AX_ATTR_RESTRICTIONS,
+                          ui::AX_RESTRICTIONS_DISABLED);
     return data_;
   }
 
@@ -83,8 +84,10 @@ const ui::AXNodeData& NativeViewAccessibilityBase::GetData() const {
   if (view_->IsAccessibilityFocusable())
     data_.AddState(ui::AX_STATE_FOCUSABLE);
 
-  if (!view_->enabled())
-    data_.AddState(ui::AX_STATE_DISABLED);
+  if (!view_->enabled()) {
+    data_.AddIntAttribute(ui::AX_ATTR_RESTRICTIONS,
+                          ui::AX_RESTRICTIONS_DISABLED);
+  }
 
   if (!view_->IsDrawn())
     data_.AddState(ui::AX_STATE_INVISIBLE);
