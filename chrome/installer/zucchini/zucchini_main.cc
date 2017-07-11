407e9c6e52de845f3dc562820850e7c46d63ada0
huangs <huangs@google.com>
2017-07-11 17:37:02 -0400

    [Zucchini] Add command line processing framework with stubs.
    Bug: 729154
    Change-Id: Id5263435100dc73b2d8917ca2bcd04245ed41934

--- chrome/installer/zucchini/zucchini_main.cc
+++ chrome/installer/zucchini/zucchini_main.cc
@@ -2,6 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/command_line.h"
+#include "chrome/installer/zucchini/main_utils.h"
+
 int main(int argc, const char* argv[]) {
+  ResourceUsageTracker tracker;
+  base::CommandLine::Init(argc, argv);
+  const base::CommandLine& command_line =
+      *base::CommandLine::ForCurrentProcess();
+
+  // Instantiate Command registry and register Zucchini features.
+  CommandRegistry registry;
+  registry.Register(&kCommandGen);
+  registry.Register(&kCommandApply);
+
+  registry.RunOrExit(command_line);
+
+  if (!command_line.HasSwitch(kSwitchQuiet))
+    tracker.Print();
   return 0;
 }
