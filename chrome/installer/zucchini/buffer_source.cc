4439f7f884aefddb37c57c5391e54169029ef31c
Etienne Pierre-Doray <etiennep@chromium.org>
2017-07-11 09:22:53 -0700

    [Zucchini] Introduce buffer_source and buffer_sink.
    Cursors with convenient read and write methods to ease manipulation of
    raw data.
    
    Bug: 729154
    Change-Id: I8a7303964b02b9df7ef033de8756b09716a91eae

---
+++ chrome/installer/zucchini/buffer_source.cc
@@ -0,0 +1,49 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/installer/zucchini/buffer_source.h"
+
+#include <algorithm>
+
+namespace zucchini {
+
+BufferSource::BufferSource(ConstBufferView buffer) : ConstBufferView(buffer) {}
+
+BufferSource& BufferSource::Skip(size_type n) {
+  remove_prefix(std::min(n, Remaining()));
+  return *this;
+}
+
+bool BufferSource::CheckNextBytes(std::initializer_list<uint8_t> bytes) const {
+  if (Remaining() < bytes.size())
+    return false;
+  return std::mismatch(bytes.begin(), bytes.end(), begin()).first ==
+         bytes.end();
+}
+
+bool BufferSource::ConsumeBytes(std::initializer_list<uint8_t> bytes) {
+  if (!CheckNextBytes(bytes)) {
+    return false;
+  }
+  remove_prefix(bytes.size());
+  return true;
+}
+
+ConstBufferView BufferSource::GetRegion(size_t count) {
+  DCHECK_NE(begin(), nullptr);
+  if (Remaining() < count) {
+    ConstBufferView buffer = ConstBufferView::FromRange(begin(), end());
+    remove_prefix(size());
+    return buffer;
+  }
+  ConstBufferView buffer(begin(), count);
+  remove_prefix(count);
+  return buffer;
+}
+
+BufferSource::size_type BufferSource::Remaining() const {
+  return size();
+}
+
+}  // namespace zucchini
