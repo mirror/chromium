7079f2824166025737a9536f8ecad0b8bd60e9c1
Tom Sepez <tsepez@chromium.org>
2017-07-11 11:17:45 -0700

    Provide finer control over sandboxing in utility process.
    First patch in a series to move from a world where the sandbox
    is either on or off as controlled by a boolean to one in which
    a number of sandbox types may exist.
    
    Use the existing content::SandboxType enum for this purpose, and
    add a value for the future network process work.
    
    Change the content browser client API to do all service registrations
    through a single RegisterOutOfProcessServices() call.
    
    Change-Id: I9efc3fe2c1796118770d23d126b069c7bb9f7cf1

--- content/browser/utility_process_host_impl.cc
+++ content/browser/utility_process_host_impl.cc
@@ -168,8 +168,9 @@ void UtilityProcessHostImpl::SetExposedDir(const base::FilePath& dir) {
   exposed_dir_ = dir;
 }
 
-void UtilityProcessHostImpl::DisableSandbox() {
-  no_sandbox_ = true;
+void UtilityProcessHostImpl::SetSandboxType(SandboxType sandbox_type) {
+  no_sandbox_ = (sandbox_type == SANDBOX_TYPE_INVALID ||
+                 sandbox_type == SANDBOX_TYPE_NETWORK);
 }
 
 #if defined(OS_WIN)
