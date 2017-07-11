858df335ba8a1b02d86bd1efe37f24e54bcb70b0
Dominick Ng <dominickn@chromium.org>
2017-07-11 17:20:42 +1000

    Split native app installation monitoring out of AppBannerInfoBarDelegateAndroid.
    Currently, the Java-side AppBannerInfoBarDelegateAndroid contains
    significant logic for monitoring the progress of a native app or WebAPK
    installation. This logic would have to be duplicated in order to
    implement a non-infobar app banner.
    
    This CL moves the logic out of AppBannerInfoBarDelegateAndroid and into
    InstallerDelegate. The InfoBarDelegate becomes an observer of the
    InstallerDelegate. This is a first step to enabling a modal app install
    banner, which will also observe the InstallerDelegate in order to update
    UI as installation proceeds.
    
    BUG=739257
    
    Change-Id: Iaaf5d23e8798e3d213a714cd3194bc83d674d919

--- chrome/android/java/src/org/chromium/chrome/browser/banners/InstallerDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/banners/InstallerDelegate.java
@@ -4,102 +4,140 @@
 
 package org.chromium.chrome.browser.banners;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.SystemClock;
 
+import org.chromium.base.ApplicationStatus;
+import org.chromium.base.ContextUtils;
+import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.ui.base.WindowAndroid;
 
 /**
- * Monitors the PackageManager to see when an app has been installed.
+ * Monitors the app installation process and informs an observer of updates.
  */
-public class InstallerDelegate implements Runnable {
-    /**
-     * Callback for when the app install has completed.
-     */
+public class InstallerDelegate {
+    /** Observer methods called for the different stages of app installation. */
     public static interface Observer {
         /**
+         * Called when the installation intent completes and installation has begun.
+         * @param delegate     The delegate object sending the message.
+         * @param isInstalling true if the app is currently installing.
+         */
+        public void onInstallIntentCompleted(InstallerDelegate delegate, boolean isInstalling);
+
+        /**
          * Called when the task has finished.
-         * @param delegate Instance of the class that finished.
+         * @param delegate The delegate object sending the message.
          * @param success  Whether or not the package was successfully installed.
          */
         public void onInstallFinished(InstallerDelegate delegate, boolean success);
+
+        /**
+         * Called when the current application state changes due to an installation.
+         * @param delegate The delegate object sending the message.
+         * @param newState the new state id.
+         */
+        public void onApplicationStateChanged(InstallerDelegate delegate, int newState);
     }
 
+    /**
+     * Object to wait for the PackageManager to finish installation an app. For convenience, this is
+     * bound to an instance of InstallerDelegate, and accesses its members and methods.
+     */
+    private class InstallMonitor implements Runnable {
+        /** Timestamp of when we started monitoring. */
+        private long mTimestampStarted;
+
+        public InstallMonitor() {}
+
+        /** Begin monitoring the PackageManager to see if it completes installing the package. */
+        public void start() {
+            mTimestampStarted = SystemClock.elapsedRealtime();
+            mIsRunning = true;
+            mHandler.postDelayed(this, mMsBetweenRuns);
+        }
+
+        /** Don't call this directly; instead, call {@link #start()}. */
+        @Override
+        public void run() {
+            boolean isInstalled = InstallerDelegate.isInstalled(
+                    getPackageManager(ContextUtils.getApplicationContext()), mPackageName);
+            boolean waitedTooLong =
+                    (SystemClock.elapsedRealtime() - mTimestampStarted) > mMsMaximumWaitingTime;
+            if (isInstalled || !mIsRunning || waitedTooLong) {
+                mObserver.onInstallFinished(InstallerDelegate.this, isInstalled);
+                mIsRunning = false;
+            } else {
+                mHandler.postDelayed(this, mMsBetweenRuns);
+            }
+        }
+
+        /** Prevent rescheduling the Runnable. */
+        public void cancel() {
+            mIsRunning = false;
+        }
+    }
+
+    private static final String TAG = "cr_InstallerDelegate";
     private static final long DEFAULT_MS_BETWEEN_RUNS = 1000;
     private static final long DEFAULT_MS_MAXIMUM_WAITING_TIME = 3 * 60 * 1000;
+
+    /** PackageManager to use in place of the real one. */
+    private static PackageManager sPackageManagerForTests;
+
+    /** Monitors an installation in progress. */
+    private InstallMonitor mInstallMonitor;
 
     /** Message loop to post the Runnable to. */
     private final Handler mHandler;
 
-    /** PackageManager that the Runnable is monitoring. */
-    private final PackageManager mPackageManager;
+    /** Monitors for application state changes. */
+    private final ApplicationStatus.ApplicationStateListener mListener;
 
-    /** Object that is notified when the PackageManager has finished. */
-    private final Observer mObserver;
+    /** The name of the package currently being installed. */
+    private String mPackageName;
 
-    /** Name of the package that we need to see the PackageManager has finished installing. */
-    private final String mPackageName;
-
-    /** Whether or not the Runnable is currently looping. */
-    private boolean mIsRunning;
-
-    /** Number of milliseconds to wait between calls to run(). */
+    /** Milliseconds to wait between calls to check on the PackageManager during installation. */
     private long mMsBetweenRuns;
 
-    /** Maximum number of milliseconds to wait before giving up. */
+    /** Maximum milliseconds to wait before giving up on monitoring during installation. */
     private long mMsMaximumWaitingTime;
 
-    /** Timestamp of when we first started. */
-    private long mTimestampStarted;
+    /** The observer to inform of updates during the installation process. */
+    private Observer mObserver;
 
-    /**
-     * Constructs the InstallerDelegate.
-     * @param looper         Thread to run the Runnable on.
-     * @param packageManager Provides access to the list of installed apps.
-     * @param observer       Alerted when the package has been completely installed.
-     * @param packageName    Name of the package for the app to monitor.
-     */
-    public InstallerDelegate(
-            Looper looper, PackageManager packageManager, Observer observer, String packageName) {
-        mHandler = new Handler(looper);
-        mPackageManager = packageManager;
-        mObserver = observer;
-        mPackageName = packageName;
-        mMsBetweenRuns = DEFAULT_MS_BETWEEN_RUNS;
-        mMsMaximumWaitingTime = DEFAULT_MS_MAXIMUM_WAITING_TIME;
+    /** Whether or we are currently monitoring an installation. */
+    private boolean mIsRunning;
+
+    /** Overrides the PackageManager for testing. */
+    @VisibleForTesting
+    static void setPackageManagerForTesting(PackageManager manager) {
+        sPackageManagerForTests = manager;
     }
 
     /**
-     * Begin monitoring the PackageManager to see if it completes installing the package.
+     * Set how often the handler will check the PackageManager.
+     * @param msBetween How long to wait between executions of the Runnable.
+     * @param msMax     How long to wait before giving up.
      */
-    public void start() {
-        mTimestampStarted = SystemClock.elapsedRealtime();
-        mIsRunning = true;
-        mHandler.postDelayed(this, mMsBetweenRuns);
-    }
-
-    /**
-     * Don't call this directly; instead, call {@link #start()}.
-     */
-    @Override
-    public void run() {
-        boolean isInstalled = isInstalled();
-        boolean waitedTooLong =
-                (SystemClock.elapsedRealtime() - mTimestampStarted) > mMsMaximumWaitingTime;
-        if (isInstalled || !mIsRunning || waitedTooLong) {
-            mObserver.onInstallFinished(this, isInstalled);
-            mIsRunning = false;
-        } else {
-            mHandler.postDelayed(this, mMsBetweenRuns);
-        }
+    @VisibleForTesting
+    void setTimingForTests(long msBetween, long msMax) {
+        mMsBetweenRuns = msBetween;
+        mMsMaximumWaitingTime = msMax;
     }
 
     /**
      * Checks if the app has been installed on the system.
      * @param packageManager PackageManager to use.
-     * @param packageName Name of the package to check.
+     * @param packageName    Name of the package to check.
      * @return True if the PackageManager reports that the app is installed, false otherwise.
      */
     public static boolean isInstalled(PackageManager packageManager, String packageName) {
@@ -112,37 +150,142 @@ public class InstallerDelegate implements Runnable {
     }
 
     /**
+     * Construct an InstallerDelegate to monitor an installation.
+     * @param looper   Thread to run the monitor on.
+     * @param observer Object to inform of changes in the installation state.
+     */
+    public InstallerDelegate(Looper looper, Observer observer) {
+        mHandler = new Handler(looper);
+        mObserver = observer;
+        mListener = createApplicationStateListener();
+        ApplicationStatus.registerApplicationStateListener(mListener);
+
+        mMsBetweenRuns = DEFAULT_MS_BETWEEN_RUNS;
+        mMsMaximumWaitingTime = DEFAULT_MS_MAXIMUM_WAITING_TIME;
+    }
+
+    /**
+     * Stops all current monitoring.
+     */
+    public void destroy() {
+        if (mInstallMonitor != null) {
+            mInstallMonitor.cancel();
+            mInstallMonitor = null;
+        }
+        ApplicationStatus.unregisterApplicationStateListener(mListener);
+    }
+
+    /**
      * Checks if the app has been installed on the system.
+     * @param context     Context to use.
+     * @param packageName Name of the package to check.
      * @return True if the PackageManager reports that the app is installed, false otherwise.
      */
-    private boolean isInstalled() {
-        return isInstalled(mPackageManager, mPackageName);
+    public boolean isInstalled(Context context, String packageName) {
+        return isInstalled(getPackageManager(context), packageName);
     }
 
     /**
-     * Prevent rescheduling the Runnable.
+     * Checks to see if we are currently monitoring an installation.
+     * @return true if we are currently monitoring, false otherwise.
      */
-    public void cancel() {
-        mIsRunning = false;
-    }
-
-    /**
-     * Checks to see if the Runnable will continue scheduling itself.
-     * @return True if the runnable is still being scheduled.
-     */
-    @VisibleForTesting
-    boolean isRunning() {
+    public boolean isRunning() {
         return mIsRunning;
     }
 
     /**
-     * Set how often the handler will check the PackageManager.
-     * @param msBetween How long to wait between executions of the Runnable.
-     * @param msMax     How long to wait before giving up.
+     * Called to start monitoring an installation. Exposed to allow manual triggering in tests; this
+     * is normally called when the installation intent returns.
      */
+    void startMonitoring(String packageName) {
+        mPackageName = packageName;
+        mInstallMonitor = new InstallMonitor();
+        mInstallMonitor.start();
+    }
+
+    /** Simulates a cancellation for testing. */
     @VisibleForTesting
-    void setTimingForTests(long msBetween, long msMax) {
-        mMsBetweenRuns = msBetween;
-        mMsMaximumWaitingTime = msMax;
+    void cancel() {
+        mInstallMonitor.cancel();
+    }
+
+    /**
+     * Attempts to open the specified app in the given content. Returns true if opening was
+     * successful, otherwise returns false if the app is not installed or opening failed.
+     * @param context     The context to use
+     * @param packageName Name of the package to open.
+     */
+    public boolean openApp(Context context, String packageName) {
+        PackageManager packageManager = getPackageManager(context);
+        if (!isInstalled(packageManager, packageName)) {
+            return false;
+        }
+
+        Intent launchIntent = packageManager.getLaunchIntentForPackage(packageName);
+        if (launchIntent != null) {
+            try {
+                context.startActivity(launchIntent);
+            } catch (ActivityNotFoundException e) {
+                Log.e(TAG, "Failed to open app : %s!", packageName, e);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Attempts to install or open a native app specified by the given data.
+     * @return true if the app was opened or if installation was started successfully. false
+     * otherwise.
+     * @param tab      The current tab.
+     * @param appData  The native app data to try and open or install.
+     * @param referrer The referrer attached to the URL specifying the native app, if any.
+     */
+    public boolean installOrOpenNativeApp(Tab tab, AppData appData, String referrer) {
+        Context context = ContextUtils.getApplicationContext();
+        String packageName = appData.packageName();
+        PackageManager packageManager = getPackageManager(context);
+        if (openApp(context, packageName)) {
+            return true;
+        } else {
+            // Try installing the app.  If the installation was kicked off, return false to prevent
+            // the infobar from disappearing.
+            // The supplied referrer is the URL of the page requesting the native app banner. It
+            // may be empty depending on that page's referrer policy. If it is non-empty, attach it
+            // to the installation intent as Intent.EXTRA_REFERRER.
+            Intent installIntent = appData.installIntent();
+            if (referrer.length() > 0) installIntent.putExtra(Intent.EXTRA_REFERRER, referrer);
+            return !tab.getWindowAndroid().showIntent(
+                    installIntent, createIntentCallback(appData), null);
+        }
+    }
+
+    private WindowAndroid.IntentCallback createIntentCallback(final AppData appData) {
+        return new WindowAndroid.IntentCallback() {
+            @Override
+            public void onIntentCompleted(WindowAndroid window, int resultCode, Intent data) {
+                boolean isInstalling = resultCode == Activity.RESULT_OK;
+                if (isInstalling) {
+                    startMonitoring(appData.packageName());
+                }
+
+                mObserver.onInstallIntentCompleted(InstallerDelegate.this, isInstalling);
+            }
+        };
+    }
+
+    private ApplicationStatus.ApplicationStateListener createApplicationStateListener() {
+        return new ApplicationStatus.ApplicationStateListener() {
+            @Override
+            public void onApplicationStateChange(int newState) {
+                if (!ApplicationStatus.hasVisibleActivities()) return;
+                mObserver.onApplicationStateChanged(InstallerDelegate.this, newState);
+            }
+        };
+    }
+
+    private PackageManager getPackageManager(Context context) {
+        if (sPackageManagerForTests != null) return sPackageManagerForTests;
+        return context.getPackageManager();
     }
 }
