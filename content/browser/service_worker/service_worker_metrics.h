d9e3d45678743090eb8bf07aae5742dd5a969d99
Tsuyoshi Horo <horo@chromium.org>
2017-07-11 11:09:57 +0900

    Revive ServiceWorker.(StartFor)NavigationHintPrecision UMA
    I removed the UMA (ServiceWorker.NavigationHintPrecision) when we stopped
    "Speculatively start Service Workers on UI-events" experiment (crbug.com/616502).
    But we will start "Warm-up service worker on omnibox signal" experiment
    (crbug.com/732681).
    So this CL revive the UMA as ServiceWorker.StartForNavigationHintPrecision.
    
    Bug:740102
    Change-Id: I84034f9a3c8edb9ff25e5644d6b94c624ae00f1c

--- content/browser/service_worker/service_worker_metrics.h
+++ content/browser/service_worker/service_worker_metrics.h
@@ -190,7 +190,7 @@ class ServiceWorkerMetrics {
 
   class ScopedEventRecorder {
    public:
-    explicit ScopedEventRecorder();
+    explicit ScopedEventRecorder(EventType start_worker_purpose);
     ~ScopedEventRecorder();
 
     void RecordEventHandledStatus(EventType event, bool handled);
@@ -207,6 +207,7 @@ class ServiceWorkerMetrics {
                                         size_t fired_events);
 
     std::map<EventType, EventStat> event_stats_;
+    const EventType start_worker_purpose_;
 
     DISALLOW_COPY_AND_ASSIGN(ScopedEventRecorder);
   };
