cd8f2f4ffa51de3c31aba6637ac5eea2e065d466
Tanin Na Nakorn <tanin@google.com>
2017-07-10 18:02:59 -0700

    [Not ready] Crash-resistant URL logging
    We can't use NetLogObserver because it buffers file writing and is 
    not suitable when a crash occurs. Clusterfuzz needs a crash-resistant 
    URL logging mechanism.
    
    Bug: 715656
    Change-Id: I18e96afb5c5deb84d51691760606c79061751369

---
+++ components/url_log/url_log_unittest.cc
@@ -0,0 +1,56 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/url_log/url_log.h"
+
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/files/scoped_temp_dir.h"
+#include "base/strings/stringprintf.h"
+#include "base/test/scoped_command_line.h"
+#include "base/memory/ptr_util.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace url_log {
+
+namespace {
+
+const char kUrl[] = "file://dir/test.html";
+
+class UrlLogTest : public testing::Test {
+ public:
+  void SetUp() override {
+    ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
+    log_path_ = temp_dir_.GetPath().AppendASCII("UrlLogFile");
+  }
+
+ protected:
+  base::ScopedTempDir temp_dir_;
+  base::FilePath log_path_;
+};
+
+TEST_F(UrlLogTest, WriteUrl) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitchPath(
+      kLogUrlLog, log_path_);
+  UrlLog logger;
+  logger.Log(GURL(kUrl));
+
+  ASSERT_TRUE(base::PathExists(log_path_));
+
+  std::string input;
+  ASSERT_TRUE(base::ReadFileToString(log_path_, &input));
+
+  std::string expected = base::StringPrintf("%s\n", kUrl);
+  ASSERT_EQ(expected, input);
+}
+
+TEST_F(UrlLogTest, SkipWritingUrl) {
+  UrlLog logger;
+  logger.Log(GURL(kUrl));
+  ASSERT_FALSE(base::PathExists(log_path_));
+}
+
+}  // namespace
+
+}  // namespace url_log
