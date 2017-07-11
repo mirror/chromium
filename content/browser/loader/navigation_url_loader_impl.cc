c19a62d7df1b92dc254ece31631e15a2462a89f0
tzik <tzik@chromium.org>
2017-07-11 14:06:22 +0900

    Ban raw pointers to incomplete types as a base::Bind parameter
    Bug:
    Change-Id: If60836aa13d37e8f38fdae6e406257d9486b6c11

--- content/browser/loader/navigation_url_loader_impl.cc
+++ content/browser/loader/navigation_url_loader_impl.cc
@@ -22,6 +22,7 @@
 #include "content/public/browser/navigation_ui_data.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/stream_handle.h"
+#include "net/url_request/url_request_context_getter.h"
 
 namespace content {
 
@@ -53,7 +54,7 @@ NavigationURLLoaderImpl::NavigationURLLoaderImpl(
   BrowserThread::PostTask(
       BrowserThread::IO, FROM_HERE,
       base::Bind(&NavigationURLLoaderImplCore::Start, core_, resource_context,
-                 storage_partition->GetURLRequestContext(),
+                 base::Unretained(storage_partition->GetURLRequestContext()),
                  base::Unretained(storage_partition->GetFileSystemContext()),
                  service_worker_handle_core, appcache_handle_core,
                  base::Passed(&request_info),
