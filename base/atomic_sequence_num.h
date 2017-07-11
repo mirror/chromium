207a6b47955dbe4566328eb4de829614309986e0
tzik <tzik@chromium.org>
2017-07-11 19:33:20 +0900

    Refactor base/atomic_sequence_num
    Bug:
    Change-Id: Ia061cd4af76bbd630dc6fbc0e791b11013b0cd67

--- base/atomic_sequence_num.h
+++ base/atomic_sequence_num.h
@@ -5,55 +5,25 @@
 #ifndef BASE_ATOMIC_SEQUENCE_NUM_H_
 #define BASE_ATOMIC_SEQUENCE_NUM_H_
 
-#include "base/atomicops.h"
+#include <atomic>
+
 #include "base/macros.h"
 
 namespace base {
 
-class AtomicSequenceNumber;
-
-// Static (POD) AtomicSequenceNumber that MUST be used in global scope (or
-// non-function scope) ONLY. This implementation does not generate any static
-// initializer.  Note that it does not implement any constructor which means
-// that its fields are not initialized except when it is stored in the global
-// data section (.data in ELF). If you want to allocate an atomic sequence
-// number on the stack (or heap), please use the AtomicSequenceNumber class
-// declared below.
-class StaticAtomicSequenceNumber {
- public:
-  inline int GetNext() {
-    return static_cast<int>(
-        base::subtle::NoBarrier_AtomicIncrement(&seq_, 1) - 1);
-  }
-
- private:
-  friend class AtomicSequenceNumber;
-
-  inline void Reset() {
-    base::subtle::Release_Store(&seq_, 0);
-  }
-
-  base::subtle::Atomic32 seq_;
-};
-
-// AtomicSequenceNumber that can be stored and used safely (i.e. its fields are
-// always initialized as opposed to StaticAtomicSequenceNumber declared above).
-// Please use StaticAtomicSequenceNumber if you want to declare an atomic
-// sequence number in the global scope.
 class AtomicSequenceNumber {
  public:
-  AtomicSequenceNumber() {
-    seq_.Reset();
-  }
+  constexpr AtomicSequenceNumber() {}
 
-  inline int GetNext() {
-    return seq_.GetNext();
-  }
+  inline int GetNext() { return seq_.fetch_add(1, std::memory_order_relaxed); }
 
  private:
-  StaticAtomicSequenceNumber seq_;
+  std::atomic_int seq_{0};
+
   DISALLOW_COPY_AND_ASSIGN(AtomicSequenceNumber);
 };
+
+using StaticAtomicSequenceNumber = AtomicSequenceNumber;
 
 }  // namespace base
 
