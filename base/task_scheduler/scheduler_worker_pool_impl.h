c8e3900e4909414fd254a0b8a2fef8399c357970
Jeffrey He <jeffreyhe@google.com>
2017-07-12 10:13:59 -0400

    Create SchedulerWorkers on Demand
    Currently, all SchedulerWorkers are created in Start(). With this change, 
    one SchedulerWorker in addition to the number required based off of tasks 
    in flight will be created in Start().
    
    After that, WakeUpOneWorker() will create SchedulerWorkers as needed.
    
    Bug: 738104
    Change-Id: I9a0e2fdcfc7338e10a38e1021f22fdb7ecadfa10

--- base/task_scheduler/scheduler_worker_pool_impl.h
+++ base/task_scheduler/scheduler_worker_pool_impl.h
@@ -132,6 +132,17 @@ class BASE_EXPORT SchedulerWorkerPoolImpl : public SchedulerWorkerPool {
   // Returns true if worker thread detachment is permitted.
   bool CanWorkerDetachForTesting();
 
+  // Adds a new SchedulerWorker based on SchedulerWorkerPoolParams
+  // that were passed into Start(). SchedulerWorker::Start() must be called on
+  // the worker before it is usable. Returns the newly added worker. This cannot
+  // be called before Start(). This function should only be called under the
+  // protection of |lock_|.
+  scoped_refptr<SchedulerWorker> CreateAndRegisterSchedulerWorker();
+
+  // Performs the same function as CreateAndRegisterSchedulerWorker(), except
+  // this also calls Start() on the worker.
+  scoped_refptr<SchedulerWorker> CreateRegisterAndStartSchedulerWorker();
+
   const std::string name_;
   const ThreadPriority priority_hint_;
 
@@ -146,12 +157,14 @@ class BASE_EXPORT SchedulerWorkerPoolImpl : public SchedulerWorkerPool {
   // |workers_created_.IsSet()|).
   TimeDelta suggested_reclaim_time_;
 
-  // Synchronizes accesses to |workers_|, |idle_workers_stack_|,
-  // |idle_workers_stack_cv_for_testing_| and
+  // Synchronizes accesses to |workers_|, |worker_capacity_|,
+  // |idle_workers_stack_|, |idle_workers_stack_cv_for_testing_| and
   // |num_wake_ups_before_start_|. Has |shared_priority_queue_|'s lock as
   // its predecessor so that a worker can be pushed to |idle_workers_stack_|
   // within the scope of a Transaction (more details in GetWork()).
   mutable SchedulerLock lock_;
+
+  SchedulerBackwardCompatibility backward_compatibility_;
 
   // Stack of idle workers. Initially, all workers are on this stack. A worker
   // is removed from the stack before its WakeUp() function is called and when
@@ -176,7 +189,7 @@ class BASE_EXPORT SchedulerWorkerPoolImpl : public SchedulerWorkerPool {
   AtomicFlag worker_detachment_disallowed_;
 
 #if DCHECK_IS_ON()
-  // Set when all workers have been created.
+  // Set after all the initial workers have been created.
   AtomicFlag workers_created_;
 #endif
 
@@ -194,6 +207,10 @@ class BASE_EXPORT SchedulerWorkerPoolImpl : public SchedulerWorkerPool {
 
   TaskTracker* const task_tracker_;
   DelayedTaskManager* const delayed_task_manager_;
+
+  // Workers can be added as needed up until there are |worker_capacity_|
+  // workers.
+  size_t worker_capacity_;
 
   DISALLOW_COPY_AND_ASSIGN(SchedulerWorkerPoolImpl);
 };
