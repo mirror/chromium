05ce35f449aac4f7add951a91d1f068bba7e49de
Thoren Paulson <thoren@chromium.org>
2017-07-12 14:25:09 -0700

    Enable Widevine L3 on Android Things.
    Uses Cast's DRM instead of Android's on Android audio-only builds.
    
    BUG=internal 62721854
    TEST=Cast Spotify to cast shell
    
    Change-Id: I441357b30693e93ec2578c7066205831980d188a

--- chromecast/renderer/media/key_systems_cast.cc
+++ chromecast/renderer/media/key_systems_cast.cc
@@ -9,6 +9,7 @@
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "build/build_config.h"
+#include "chromecast/chromecast_features.h"
 #include "chromecast/media/base/key_systems_common.h"
 #include "components/cdm/renderer/android_key_systems.h"
 #include "components/cdm/renderer/widevine_key_system_properties.h"
@@ -30,7 +31,8 @@ namespace media {
 namespace {
 
 #if defined(PLAYREADY_CDM_AVAILABLE) || \
-    (defined(WIDEVINE_CDM_AVAILABLE) && !defined(OS_ANDROID))
+    (defined(WIDEVINE_CDM_AVAILABLE) && \
+     !(defined(OS_ANDROID) && !BUILDFLAG(IS_CAST_USING_CMA_BACKEND)))
 SupportedCodecs GetCastEmeSupportedCodecs() {
   SupportedCodecs codecs =
       ::media::EME_CODEC_MP4_AAC | ::media::EME_CODEC_MP4_AVC1 |
@@ -54,7 +56,7 @@ SupportedCodecs GetCastEmeSupportedCodecs() {
 
   return codecs;
 }
-#endif
+#endif  // defined(PLAYREADY_CDM_AVAILABLE) || ...
 
 #if defined(PLAYREADY_CDM_AVAILABLE)
 class PlayReadyKeySystemProperties : public ::media::KeySystemProperties {
@@ -131,7 +133,7 @@ void AddChromecastKeySystems(
 #endif  // defined(PLAYREADY_CDM_AVAILABLE)
 
 #if defined(WIDEVINE_CDM_AVAILABLE)
-#if defined(OS_ANDROID)
+#if defined(OS_ANDROID) && !BUILDFLAG(IS_CAST_USING_CMA_BACKEND)
   cdm::AddAndroidWidevine(key_systems_properties);
 #else
   using Robustness = cdm::WidevineKeySystemProperties::Robustness;
@@ -149,7 +151,7 @@ void AddChromecastKeySystems(
       // Note: On Chromecast, all CDMs may have persistent state.
       EmeFeatureSupport::ALWAYS_ENABLED,    // Persistent state.
       EmeFeatureSupport::ALWAYS_ENABLED));  // Distinctive identifier.
-#endif  // defined(OS_ANDROID)
+#endif  // defined(OS_ANDROID) && !BUILDFLAG(IS_CAST_USING_CMA_BACKEND)
 #endif  // defined(WIDEVINE_CDM_AVAILABLE)
 }
 
