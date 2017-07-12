364f3ea08e05f5ddd6cd5006e58f4543c3848c3c
Jeffrey He <jeffreyhe@google.com>
2017-07-12 14:42:33 -0400

    Remove worker detachment.
    After SchedulerWorker::Start(), a worker will always contain a thread
    until the thread exits and the worker is removed from the pool.
    
    When a SchedulerWorker's WaitableEvent timesout, instead of the worker
    detaching, the worker will timeout and be removed from the worker pool.
    
    Bug: 738104
    Change-Id: Ie99b47694014d5638d6be46a48459a5847807a44

--- base/task_scheduler/scheduler_worker_stack_unittest.cc
+++ base/task_scheduler/scheduler_worker_stack_unittest.cc
@@ -35,10 +35,11 @@ class MockSchedulerWorkerDelegate : public SchedulerWorker::Delegate {
   TimeDelta GetSleepTimeout() override {
     return TimeDelta::Max();
   }
-  bool CanDetach(SchedulerWorker* worker) override {
-    return false;
+  bool CanTimeout(SchedulerWorker* worker) override { return false; }
+  void OnTimeout() override {
+    ADD_FAILURE() << "Unexpected call to OnDetach()";
   }
-  void OnDetach() override { ADD_FAILURE() << "Unexpected call to OnDetach()"; }
+  void RemoveWorker(SchedulerWorker* worker) override {}
 };
 
 class TaskSchedulerWorkerStackTest : public testing::Test {
