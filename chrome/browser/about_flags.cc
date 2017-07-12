f53c33c71417ff2c6d65a7d9609c7763e946f6bd
Tim Song <tengs@chromium.org>
2017-07-11 17:19:58 -0700

    [EasyUnlock] Enable EasyUnlock v2 (BLE) by default.
    BUG=741214
    
    Change-Id: I2184b489044fc07de51ca3abbe4d5a5836138411

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1387,12 +1387,12 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEasyUnlockProximityDetectionName,
      flag_descriptions::kEasyUnlockProximityDetectionDescription, kOsCrOS,
      SINGLE_VALUE_TYPE(proximity_auth::switches::kEnableProximityDetection)},
-    {"enable-easy-unlock-bluetooth-low-energy-detection",
+    {"disable-easy-unlock-bluetooth-low-energy-detection",
      flag_descriptions::kEasyUnlockBluetoothLowEnergyDiscoveryName,
      flag_descriptions::kEasyUnlockBluetoothLowEnergyDiscoveryDescription,
      kOsCrOS,
-     SINGLE_VALUE_TYPE(
-         proximity_auth::switches::kEnableBluetoothLowEnergyDiscovery)},
+     SINGLE_DISABLE_VALUE_TYPE(
+         proximity_auth::switches::kDisableBluetoothLowEnergyDiscovery)},
     {"spurious-power-button-window",
      flag_descriptions::kSpuriousPowerButtonWindowName,
      flag_descriptions::kSpuriousPowerButtonWindowDescription, kOsCrOS,
