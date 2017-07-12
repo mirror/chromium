5508ef4e5fbaa8ab646640185fbe6868d9740e95
Khushal <khushalsagar@chromium.org>
2017-07-12 12:22:03 -0700

    cc: Record mip levels used for downscaling images in the renderer.
    Bug:729183
    Change-Id: I4e5b3251048d57d592bb82e933d23adab146a31d

--- cc/tiles/software_image_decode_cache.cc
+++ cc/tiles/software_image_decode_cache.cc
@@ -430,6 +430,11 @@ void SoftwareImageDecodeCache::DecodeImage(const ImageKey& key,
   if (task_type == DecodeTaskType::USE_OUT_OF_RASTER_TASKS)
     decoded_image->mark_out_of_raster();
 
+  int mip_level =
+      MipMapUtil::GetLevelForSize(key.src_rect().size(), key.target_size());
+  UMA_HISTOGRAM_CUSTOM_COUNTS("Renderer4.ImageDecodeMipLevel", mip_level + 1, 1,
+                              32, 32);
+
   decoded_images_.Put(key, std::move(decoded_image));
 }
 
