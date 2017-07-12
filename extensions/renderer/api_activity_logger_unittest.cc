nwjs/chromium.src

commit 7ab1596cf4cde44bfc15f6ba2c74f694f130b608

[Extensions Bindings] Add activity logging of custom handling
If a method is handled by a custom hook, we won't notify the browser
(where we normally would with an API request), and so the activity log
won't be updated. Add support for activity logging with native binding.

If a request is handled by a custom hook and a request is not sent to
the browser, notify the activity logger (which will send the information
along to the browser process).  This matches the logic we currently have
in the JS bindings.

Add unit tests for the same.  In addition to the unit tests, this fixes
ActivityLogApiTest.TriggerEvent with native bindings enabled.

BUG=653596

Review-Url: https://codereview.chromium.org/2962093002
Cr-Commit-Position: refs/heads/master@{#485457}

extensions/renderer/api_activity_logger.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "extensions/renderer/api_activity_logger.h"
+
 #include <stddef.h>
 
 #include <string>
@@ -11,8 +13,8 @@
 #include "content/public/renderer/render_thread.h"
 #include "extensions/common/extension_messages.h"
 #include "extensions/renderer/activity_log_converter_strategy.h"
-#include "extensions/renderer/api_activity_logger.h"
 #include "extensions/renderer/dispatcher.h"
+#include "extensions/renderer/extensions_renderer_client.h"
 #include "extensions/renderer/script_context.h"
 
 namespace extensions {
@@ -20,28 +22,45 @@ namespace extensions {
 APIActivityLogger::APIActivityLogger(ScriptContext* context,
                                      Dispatcher* dispatcher)
     : ObjectBackedNativeHandler(context), dispatcher_(dispatcher) {
-  RouteFunction("LogEvent", base::Bind(&APIActivityLogger::LogEvent,
-                                       base::Unretained(this)));
-  RouteFunction("LogAPICall", base::Bind(&APIActivityLogger::LogAPICall,
-                                         base::Unretained(this)));
+  RouteFunction("LogEvent", base::Bind(&APIActivityLogger::LogForJS,
+                                       base::Unretained(this), EVENT));
+  RouteFunction("LogAPICall", base::Bind(&APIActivityLogger::LogForJS,
+                                         base::Unretained(this), APICALL));
 }
 
 APIActivityLogger::~APIActivityLogger() {}
 
 // static
 void APIActivityLogger::LogAPICall(
-    const v8::FunctionCallbackInfo<v8::Value>& args) {
-  LogInternal(APICALL, args);
+    v8::Local<v8::Context> context,
+    const std::string& call_name,
+    const std::vector<v8::Local<v8::Value>>& arguments) {
+  const Dispatcher* dispatcher =
+      ExtensionsRendererClient::Get()->GetDispatcher();
+  if (!dispatcher ||  // dispatcher can be null in unittests.
+      !dispatcher->activity_logging_enabled()) {
+    return;
+  }
+
+  ScriptContext* script_context =
+      ScriptContextSet::GetContextByV8Context(context);
+  auto value_args = base::MakeUnique<base::ListValue>();
+  std::unique_ptr<content::V8ValueConverter> converter =
+      content::V8ValueConverter::Create();
+  ActivityLogConverterStrategy strategy;
+  converter->SetFunctionAllowed(true);
+  converter->SetStrategy(&strategy);
+  value_args->Reserve(arguments.size());
+  // TODO(devlin): This doesn't protect against custom properties, so it might
+  // not perfectly reflect the passed arguments.
+  for (const auto& arg : arguments)
+    value_args->Append(converter->FromV8Value(arg, context));
+
+  LogInternal(APICALL, script_context->GetExtensionID(), call_name,
+              std::move(value_args), std::string());
 }
 
-// static
-void APIActivityLogger::LogEvent(
-    const v8::FunctionCallbackInfo<v8::Value>& args) {
-  LogInternal(EVENT, args);
-}
-
-// static
-void APIActivityLogger::LogInternal(
+void APIActivityLogger::LogForJS(
     const CallType call_type,
     const v8::FunctionCallbackInfo<v8::Value>& args) {
   CHECK_GT(args.Length(), 2);
@@ -52,38 +71,51 @@ void APIActivityLogger::LogInternal(
   if (!dispatcher_->activity_logging_enabled())
     return;
 
-  std::string ext_id = *v8::String::Utf8Value(args[0]);
-  ExtensionHostMsg_APIActionOrEvent_Params params;
-  params.api_call = *v8::String::Utf8Value(args[1]);
-  if (args.Length() == 4)  // Extras are optional.
-    params.extra = *v8::String::Utf8Value(args[3]);
-  else
-    params.extra = "";
+  v8::Isolate* isolate = args.GetIsolate();
+  v8::HandleScope handle_scope(isolate);
+  v8::Local<v8::Context> context = isolate->GetCurrentContext();
 
-  // Get the array of api call arguments.
+  std::string extension_id = *v8::String::Utf8Value(args[0]);
+  std::string call_name = *v8::String::Utf8Value(args[1]);
+  std::string extra;
+  if (args.Length() == 4) {  // Extras are optional.
+    CHECK(args[3]->IsString());
+    extra = *v8::String::Utf8Value(args[3]);
+  }
+
+  // Get the array of call arguments.
+  auto arguments = base::MakeUnique<base::ListValue>();
   v8::Local<v8::Array> arg_array = v8::Local<v8::Array>::Cast(args[2]);
   if (arg_array->Length() > 0) {
+    arguments->Reserve(arg_array->Length());
     std::unique_ptr<content::V8ValueConverter> converter =
         content::V8ValueConverter::Create();
     ActivityLogConverterStrategy strategy;
     converter->SetFunctionAllowed(true);
     converter->SetStrategy(&strategy);
-    std::unique_ptr<base::ListValue> arg_list(new base::ListValue());
-    for (size_t i = 0; i < arg_array->Length(); ++i) {
-      arg_list->Set(
-          i,
-          converter->FromV8Value(arg_array->Get(i),
-                                 args.GetIsolate()->GetCurrentContext()));
-    }
-    params.arguments.Swap(arg_list.get());
+    for (size_t i = 0; i < arg_array->Length(); ++i)
+      arguments->Append(converter->FromV8Value(arg_array->Get(i), context));
   }
 
+  LogInternal(call_type, extension_id, call_name, std::move(arguments), extra);
+}
+
+// static
+void APIActivityLogger::LogInternal(const CallType call_type,
+                                    const std::string& extension_id,
+                                    const std::string& call_name,
+                                    std::unique_ptr<base::ListValue> arguments,
+                                    const std::string& extra) {
+  ExtensionHostMsg_APIActionOrEvent_Params params;
+  params.api_call = call_name;
+  params.arguments.Swap(arguments.get());
+  params.extra = extra;
   if (call_type == APICALL) {
     content::RenderThread::Get()->Send(
-        new ExtensionHostMsg_AddAPIActionToActivityLog(ext_id, params));
+        new ExtensionHostMsg_AddAPIActionToActivityLog(extension_id, params));
   } else if (call_type == EVENT) {
     content::RenderThread::Get()->Send(
-        new ExtensionHostMsg_AddEventToActivityLog(ext_id, params));
+        new ExtensionHostMsg_AddEventToActivityLog(extension_id, params));
   }
 }
 

extensions/renderer/api_activity_logger.h
@@ -5,12 +5,17 @@
 #ifndef EXTENSIONS_RENDERER_API_ACTIVITY_LOGGER_H_
 #define EXTENSIONS_RENDERER_API_ACTIVITY_LOGGER_H_
 
+#include <memory>
 #include <string>
+#include <vector>
 
 #include "base/macros.h"
-#include "extensions/common/features/feature.h"
 #include "extensions/renderer/object_backed_native_handler.h"
 #include "v8/include/v8.h"
+
+namespace base {
+class ListValue;
+}
 
 namespace extensions {
 class Dispatcher;
@@ -23,28 +28,31 @@ class APIActivityLogger : public ObjectBackedNativeHandler {
   APIActivityLogger(ScriptContext* context, Dispatcher* dispatcher);
   ~APIActivityLogger() override;
 
+  // Notifies the browser that an API method has been called, if and only if
+  // activity logging is enabled.
+  static void LogAPICall(v8::Local<v8::Context> context,
+                         const std::string& call_name,
+                         const std::vector<v8::Local<v8::Value>>& arguments);
+
  private:
   // Used to distinguish API calls & events from each other in LogInternal.
   enum CallType { APICALL, EVENT };
 
   // This is ultimately invoked in bindings.js with JavaScript arguments.
   //    arg0 - extension ID as a string
-  //    arg1 - API call name as a string
-  //    arg2 - arguments to the API call
+  //    arg1 - API method/Event name as a string
+  //    arg2 - arguments to the API call/event
   //    arg3 - any extra logging info as a string (optional)
-  void LogAPICall(const v8::FunctionCallbackInfo<v8::Value>& args);
+  // TODO(devlin): Does arg3 ever exist?
+  void LogForJS(const CallType call_type,
+                const v8::FunctionCallbackInfo<v8::Value>& args);
 
-  // This is ultimately invoked in bindings.js with JavaScript arguments.
-  //    arg0 - extension ID as a string
-  //    arg1 - Event name as a string
-  //    arg2 - Event arguments
-  //    arg3 - any extra logging info as a string (optional)
-  void LogEvent(const v8::FunctionCallbackInfo<v8::Value>& args);
-
-  // LogAPICall and LogEvent are really the same underneath except for
-  // how they are ultimately dispatched to the log.
-  void LogInternal(const CallType call_type,
-                   const v8::FunctionCallbackInfo<v8::Value>& args);
+  // Common implementation method for sending a logging IPC.
+  static void LogInternal(const CallType call_type,
+                          const std::string& extension_id,
+                          const std::string& call_name,
+                          std::unique_ptr<base::ListValue> arguments,
+                          const std::string& extra);
 
   Dispatcher* dispatcher_;
 

chrome/renderer/extensions/app_hooks_delegate.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/renderer/extensions/app_hooks_delegate.h"
 
+#include "extensions/renderer/api_activity_logger.h"
 #include "extensions/renderer/bindings/api_request_handler.h"
 #include "extensions/renderer/bindings/api_signature.h"
 #include "extensions/renderer/script_context_set.h"
@@ -17,11 +18,15 @@ void IsInstalledGetterCallback(
     v8::Local<v8::String> property,
     const v8::PropertyCallbackInfo<v8::Value>& info) {
   v8::HandleScope handle_scope(info.GetIsolate());
+  v8::Local<v8::Context> context = info.Holder()->CreationContext();
   ScriptContext* script_context =
-      ScriptContextSet::GetContextByV8Context(info.Holder()->CreationContext());
+      ScriptContextSet::GetContextByV8Context(context);
   DCHECK(script_context);
   auto* core =
       static_cast<AppBindingsCore*>(info.Data().As<v8::External>()->Value());
+  // Since this is more-or-less an API, log it as an API call.
+  APIActivityLogger::LogAPICall(context, "app.getIsInstalled",
+                                std::vector<v8::Local<v8::Value>>());
   info.GetReturnValue().Set(core->GetIsInstalled(script_context));
 }
 

chrome/test/data/extensions/api_test/activity_log_private/PRESUBMIT.py
@@ -15,9 +15,7 @@ import os
 def GetPathsToPrepend(input_api):
   web_dev_style_path = input_api.os_path.join(
       input_api.change.RepositoryRoot(),
-      'chrome',
-      'browser',
-      'resources')
+      'tools')
   return [input_api.PresubmitLocalPath(), web_dev_style_path]
 
 def RunWithPrependedPath(prepended_path, fn, *args):

chrome/test/data/extensions/api_test/activity_log_private/test/test.js
@@ -39,9 +39,11 @@ testCases.push({
                                'app_bindings', function response() { });
   },
   expected_activity: [
-    'app.GetDetails',
-    'app.GetIsInstalled',
-    'app.getInstallState'
+    // These API calls show up differently depending on whether native bindings
+    // are enabled.
+    /app\.[gG]etDetails/,
+    /app\.[gG]etIsInstalled/,
+    /app\.(getI|i)nstallState/,
   ]
 });
 testCases.push({
@@ -482,7 +484,11 @@ chrome.activityLogPrivate.onExtensionActivity.addListener(
         expectedCall = testCase.expected_activity[callIndx];
       }
       console.log('Logged:' + apiCall + ' Expected:' + expectedCall);
-      chrome.test.assertEq(expectedCall, apiCall);
+      // Allow either a RegExp or a strict string comparison.
+      if (expectedCall instanceof RegExp)
+        chrome.test.assertTrue(expectedCall.test(apiCall));
+      else
+        chrome.test.assertEq(expectedCall, apiCall);
 
       // Check that no real URLs are logged in incognito-mode tests.  Ignore
       // the initial call to windows.create opening the tab.

extensions/renderer/bindings/api_binding.cc
@@ -179,6 +179,7 @@ APIBinding::APIBinding(const std::string& api_name,
                        const base::ListValue* event_definitions,
                        const base::DictionaryValue* property_definitions,
                        const CreateCustomType& create_custom_type,
+                       const OnSilentRequest& on_silent_request,
                        std::unique_ptr<APIBindingHooks> binding_hooks,
                        APITypeReferenceMap* type_refs,
                        APIRequestHandler* request_handler,
@@ -187,6 +188,7 @@ APIBinding::APIBinding(const std::string& api_name,
     : api_name_(api_name),
       property_definitions_(property_definitions),
       create_custom_type_(create_custom_type),
+      on_silent_request_(on_silent_request),
       binding_hooks_(std::move(binding_hooks)),
       type_refs_(type_refs),
       request_handler_(request_handler),
@@ -570,6 +572,7 @@ void APIBinding::HandleCall(const std::string& name,
   bool invalid_invocation = false;
   v8::Local<v8::Function> custom_callback;
   bool updated_args = false;
+  int old_request_id = request_handler_->last_sent_request_id();
   {
     v8::TryCatch try_catch(isolate);
     APIBindingHooks::RequestResult hooks_result = binding_hooks_->RunHooks(
@@ -588,6 +591,14 @@ void APIBinding::HandleCall(const std::string& name,
       case APIBindingHooks::RequestResult::HANDLED:
         if (!hooks_result.return_value.IsEmpty())
           arguments->Return(hooks_result.return_value);
+
+        // TODO(devlin): This is a pretty simplistic implementation of this,
+        // but it's similar to the current JS logic. If we wanted to be more
+        // correct, we could create a RequestScope object that watches outgoing
+        // requests.
+        if (old_request_id == request_handler_->last_sent_request_id())
+          on_silent_request_.Run(context, name, argument_list);
+
         return;  // Our work here is done.
       case APIBindingHooks::RequestResult::ARGUMENTS_UPDATED:
         updated_args = true;  // Intentional fall-through.

extensions/renderer/bindings/api_binding.h
@@ -52,6 +52,12 @@ class APIBinding {
       const std::string& property_name,
       const base::ListValue* property_values)>;
 
+  // Called when a request is handled without notifying the browser.
+  using OnSilentRequest =
+      base::Callback<void(v8::Local<v8::Context>,
+                          const std::string& name,
+                          const std::vector<v8::Local<v8::Value>>& arguments)>;
+
   // The callback type for handling an API call.
   using HandlerCallback = base::Callback<void(gin::Arguments*)>;
 
@@ -64,6 +70,7 @@ class APIBinding {
              const base::ListValue* event_definitions,
              const base::DictionaryValue* property_definitions,
              const CreateCustomType& create_custom_type,
+             const OnSilentRequest& on_silent_request,
              std::unique_ptr<APIBindingHooks> binding_hooks,
              APITypeReferenceMap* type_refs,
              APIRequestHandler* request_handler,
@@ -130,6 +137,8 @@ class APIBinding {
 
   // The callback for constructing a custom type.
   CreateCustomType create_custom_type_;
+
+  OnSilentRequest on_silent_request_;
 
   // The registered hooks for this API.
   std::unique_ptr<APIBindingHooks> binding_hooks_;

mirror/chromium

commit ca4868393b9b999c97de58545863128b24ed85ba

[Extensions] Fix crash in renderer activity logging
Fix a crash in renderer activity logging introduced by revision
7ab1596cf4cde44bfc15f6ba2c74f694f130b608. Specifically,
ListValue::Set() accepts null values, but ListValue::Append does not.
Substitute Append(nullptr) with Append(base::MakeUnique<base::Value>()).
Add a regression unittest.

Bug: 740866
Change-Id: Icf72435960e7ad8f3544f8e74d44977db2f9fe4f

extensions/renderer/BUILD.gn
@@ -307,6 +307,7 @@ source_set("unit_tests") {
   sources = [
     "activity_log_converter_strategy_unittest.cc",
     "api/mojo_private/mojo_private_unittest.cc",
+    "api_activity_logger_unittest.cc",
     "api_test_base.cc",
     "api_test_base.h",
     "api_test_base_unittest.cc",

extensions/renderer/api_activity_logger.cc
@@ -19,6 +19,10 @@
 
 namespace extensions {
 
+namespace {
+bool g_log_for_testing = false;
+}
+
 APIActivityLogger::APIActivityLogger(ScriptContext* context,
                                      Dispatcher* dispatcher)
     : ObjectBackedNativeHandler(context), dispatcher_(dispatcher) {
@@ -37,8 +41,9 @@ void APIActivityLogger::LogAPICall(
     const std::vector<v8::Local<v8::Value>>& arguments) {
   const Dispatcher* dispatcher =
       ExtensionsRendererClient::Get()->GetDispatcher();
-  if (!dispatcher ||  // dispatcher can be null in unittests.
-      !dispatcher->activity_logging_enabled()) {
+  if ((!dispatcher ||  // dispatcher can be null in unittests.
+       !dispatcher->activity_logging_enabled()) &&
+      !g_log_for_testing) {
     return;
   }
 
@@ -53,11 +58,19 @@ void APIActivityLogger::LogAPICall(
   value_args->Reserve(arguments.size());
   // TODO(devlin): This doesn't protect against custom properties, so it might
   // not perfectly reflect the passed arguments.
-  for (const auto& arg : arguments)
-    value_args->Append(converter->FromV8Value(arg, context));
+  for (const auto& arg : arguments) {
+    std::unique_ptr<base::Value> converted_arg =
+        converter->FromV8Value(arg, context);
+    value_args->Append(converted_arg ? std::move(converted_arg)
+                                     : base::MakeUnique<base::Value>());
+  }
 
   LogInternal(APICALL, script_context->GetExtensionID(), call_name,
               std::move(value_args), std::string());
+}
+
+void APIActivityLogger::set_log_for_testing(bool log) {
+  g_log_for_testing = log;
 }
 
 void APIActivityLogger::LogForJS(
@@ -93,8 +106,12 @@ void APIActivityLogger::LogForJS(
     ActivityLogConverterStrategy strategy;
     converter->SetFunctionAllowed(true);
     converter->SetStrategy(&strategy);
-    for (size_t i = 0; i < arg_array->Length(); ++i)
-      arguments->Append(converter->FromV8Value(arg_array->Get(i), context));
+    for (size_t i = 0; i < arg_array->Length(); ++i) {
+      std::unique_ptr<base::Value> converted_arg =
+          converter->FromV8Value(arg_array->Get(i), context);
+      arguments->Append(converted_arg ? std::move(converted_arg)
+                                      : base::MakeUnique<base::Value>());
+    }
   }
 
   LogInternal(call_type, extension_id, call_name, std::move(arguments), extra);

extensions/renderer/api_activity_logger.h
@@ -34,6 +34,8 @@ class APIActivityLogger : public ObjectBackedNativeHandler {
                          const std::string& call_name,
                          const std::vector<v8::Local<v8::Value>>& arguments);
 
+  static void set_log_for_testing(bool log);
+
  private:
   // Used to distinguish API calls & events from each other in LogInternal.
   enum CallType { APICALL, EVENT };

extensions/renderer/api_activity_logger_unittest.cc
@@ -1,0 +1,67 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "extensions/renderer/api_activity_logger.h"
+
+#include "base/memory/ptr_util.h"
+#include "content/public/test/mock_render_thread.h"
+#include "extensions/common/extension_messages.h"
+#include "extensions/common/features/feature.h"
+#include "extensions/common/test_util.h"
+#include "extensions/renderer/bindings/api_binding_test.h"
+#include "extensions/renderer/bindings/api_binding_test_util.h"
+#include "extensions/renderer/script_context.h"
+#include "extensions/renderer/script_context_set.h"
+#include "extensions/renderer/test_extensions_renderer_client.h"
+#include "ipc/ipc_message.h"
+#include "ipc/ipc_test_sink.h"
+
+namespace extensions {
+using ActivityLoggerTest = APIBindingTest;
+
+// Regression test for crbug.com/740866.
+TEST_F(ActivityLoggerTest, DontCrashOnUnconvertedValues) {
+  content::MockRenderThread mock_render_thread;
+  TestExtensionsRendererClient client;
+  std::set<ExtensionId> extension_ids;
+  ScriptContextSet script_context_set(&extension_ids);
+
+  APIActivityLogger::set_log_for_testing(true);
+
+  v8::HandleScope handle_scope(isolate());
+  v8::Local<v8::Context> context = MainContext();
+
+  scoped_refptr<const Extension> extension = test_util::CreateEmptyExtension();
+  extension_ids.insert(extension->id());
+  const Feature::Context kContextType = Feature::BLESSED_EXTENSION_CONTEXT;
+  script_context_set.AddForTesting(base::MakeUnique<ScriptContext>(
+      context, nullptr, extension.get(), kContextType, extension.get(),
+      kContextType));
+
+  std::vector<v8::Local<v8::Value>> args = {v8::Undefined(isolate())};
+
+  IPC::TestSink& sink = mock_render_thread.sink();
+  sink.ClearMessages();
+
+  APIActivityLogger::LogAPICall(context, "someApiMethod", args);
+
+  ASSERT_EQ(1u, sink.message_count());
+  const IPC::Message* message = sink.GetMessageAt(0u);
+  ASSERT_EQ(ExtensionHostMsg_AddAPIActionToActivityLog::ID, message->type());
+  ExtensionHostMsg_AddAPIActionToActivityLog::Param full_params;
+  ASSERT_TRUE(
+      ExtensionHostMsg_AddAPIActionToActivityLog::Read(message, &full_params));
+  std::string extension_id = std::get<0>(full_params);
+  ExtensionHostMsg_APIActionOrEvent_Params params = std::get<1>(full_params);
+  EXPECT_EQ(extension->id(), extension_id);
+  ASSERT_EQ(1u, params.arguments.GetList().size());
+  EXPECT_EQ(base::Value::Type::NONE, params.arguments.GetList()[0].type());
+
+  ScriptContext* script_context = script_context_set.GetByV8Context(context);
+  script_context_set.Remove(script_context);
+  base::RunLoop().RunUntilIdle();  // Let script context destruction complete.
+  APIActivityLogger::set_log_for_testing(false);
+}
+
+}  // namespace extensions