bd1af4f64074aa43c997076757cf3735c5264638
Charles Reis <creis@chromium.org>
2017-07-12 08:11:51 -0700

    Classify cross-document replacement navigation as new.
    This ensures that we create a new NavigationEntry for them rather than
    trying to (incompletely) update an existing one.
    
    BUG=596707
    TEST=Tests still pass
    
    Change-Id: Ie58c9cd2633dbd6042ace83b75b7449911052925

--- content/browser/frame_host/navigation_controller_impl.cc
+++ content/browser/frame_host/navigation_controller_impl.cc
@@ -888,6 +888,7 @@ bool NavigationControllerImpl::RendererDidNavigate(
                                    navigation_handle);
       break;
     case NAVIGATION_TYPE_EXISTING_PAGE:
+      // TODO(creis): Try to remove this.
       details->did_replace_entry = details->is_same_document;
       RendererDidNavigateToExistingPage(rfh, params, details->is_same_document,
                                         was_restored, navigation_handle);
