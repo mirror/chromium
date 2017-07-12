af365b5d9a42d15726da08c416eb496db6bdd03e
Dan Sanders <sandersd@chromium.org>
2017-07-11 17:24:08 -0700

    media: Don't remove players from chrome://media-internals on process exit.
    As long as chrome://media-internals is open, it should preserve log
    entries. Log entries are still removed from the backing store, so these
    players will be removed if the page is reloaded.
    
    This CL also changes the trigger for backing store removal to include
    tab crashes. Previously there was no reliable way to remove a logs for a
    player from a crashed tab, which is problematic if the tab is incognito.
    
    Bug: 741132
    Change-Id: I92b152fa6578c3728ab22a45786e188d93fb87f2

--- content/browser/media/media_internals.cc
+++ content/browser/media/media_internals.cc
@@ -785,6 +785,10 @@ MediaInternals::MediaInternals()
     : can_update_(false),
       owner_ids_(),
       uma_handler_(new MediaInternalsUMAHandler(this)) {
+  // TODO(sandersd): Is there ever a relevant case where TERMINATED is sent
+  // without CLOSED also being sent?
+  registrar_.Add(this, NOTIFICATION_RENDERER_PROCESS_CLOSED,
+                 NotificationService::AllBrowserContextsAndSources());
   registrar_.Add(this, NOTIFICATION_RENDERER_PROCESS_TERMINATED,
                  NotificationService::AllBrowserContextsAndSources());
 }
@@ -795,9 +799,7 @@ void MediaInternals::Observe(int type,
                              const NotificationSource& source,
                              const NotificationDetails& details) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  DCHECK_EQ(type, NOTIFICATION_RENDERER_PROCESS_TERMINATED);
   RenderProcessHost* process = Source<RenderProcessHost>(source).ptr();
-
   uma_handler_->OnProcessTerminated(process->GetID());
   saved_events_by_process_.erase(process->GetID());
 }
