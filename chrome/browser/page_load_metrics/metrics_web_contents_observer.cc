91c9ce80f5516d92ce3b3cfe047444fb31686152
Luna Lu <loonybear@chromium.org>
2017-07-13 14:07:26 -0400

    Plumbing use counter feature usage from render to browser
    Upon receiving the features, no actions is taken upon in this CL.
    A use counter page load observer will be implemented in this CL
    https://chromium-review.googlesource.com/c/534133/
    
    which makes use of the feature usage received on the browser side.
    
    Bug: 716565
    Change-Id: I3f319782e383474dcd7a8cf18d4c56e38f292848

--- chrome/browser/page_load_metrics/metrics_web_contents_observer.cc
+++ chrome/browser/page_load_metrics/metrics_web_contents_observer.cc
@@ -586,7 +586,8 @@ MetricsWebContentsObserver::NotifyAbortedProvisionalLoadsNewNavigation(
 void MetricsWebContentsObserver::OnTimingUpdated(
     content::RenderFrameHost* render_frame_host,
     const mojom::PageLoadTiming& timing,
-    const mojom::PageLoadMetadata& metadata) {
+    const mojom::PageLoadMetadata& metadata,
+    const mojom::PageLoadFeatures& new_features) {
   // We may receive notifications from frames that have been navigated away
   // from. We simply ignore them.
   if (GetMainFrame(render_frame_host) != web_contents()->GetMainFrame()) {
@@ -623,10 +624,11 @@ void MetricsWebContentsObserver::OnTimingUpdated(
 
 void MetricsWebContentsObserver::UpdateTiming(
     const mojom::PageLoadTimingPtr timing,
-    const mojom::PageLoadMetadataPtr metadata) {
+    const mojom::PageLoadMetadataPtr metadata,
+    const mojom::PageLoadFeaturesPtr new_features) {
   content::RenderFrameHost* render_frame_host =
       page_load_metrics_binding_.GetCurrentTargetFrame();
-  OnTimingUpdated(render_frame_host, *timing, *metadata);
+  OnTimingUpdated(render_frame_host, *timing, *metadata, *new_features);
 }
 
 bool MetricsWebContentsObserver::ShouldTrackNavigation(
