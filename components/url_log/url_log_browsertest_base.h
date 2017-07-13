0197c8a519a41c4a5e9f656b8c18eff844705ede
Tanin Na Nakorn <tanin@google.com>
2017-07-13 16:44:11 -0700

    [Not ready] Crash-resistant URL logging
    We can't use NetLogObserver because it buffers file writing and is 
    not suitable when a crash occurs. Clusterfuzz needs a crash-resistant 
    URL logging mechanism.
    
    Bug: 715656
    Change-Id: I18e96afb5c5deb84d51691760606c79061751369

---
+++ components/url_log/url_log_browsertest_base.h
@@ -0,0 +1,34 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/command_line.h"
+#include "base/files/file_path.h"
+#include "base/files/scoped_temp_dir.h"
+#include "url/gurl.h"
+
+namespace url_log {
+
+class UrlLogBrowserTestBase {
+ public:
+  UrlLogBrowserTestBase();
+  ~UrlLogBrowserTestBase();
+
+  void SetUp();
+
+  void SetUpCommandLine(base::CommandLine* command_line);
+
+  GURL GetUrl(const base::FilePath& path, const std::string& child);
+
+  void WriteUrlLogTest();
+
+ protected:
+  base::ScopedTempDir temp_dir_;
+  base::FilePath log_path_;
+  base::FilePath test_data_dir_;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(UrlLogBrowserTestBase);
+};
+
+}  // namespace url_log
