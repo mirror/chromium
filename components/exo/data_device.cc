fbe878c5fda4e93adc4ac4c343569e8bb45fdb3c
Daichi Hirono <hirono@chromium.org>
2017-07-13 13:25:45 +0900

    Add wl_data_device and relatives' wrappers.
    The CL adds wrappers of wl_data_device, wl_data_source, and wl_data_offer to
    exo.
    
    BUG=b:31988797, b:28779679
    TEST=No new test since it just adds wrappers.
    
    Change-Id: I6e9ae74d00dbfb8306f2c8285176965feaf3d7cc

---
+++ components/exo/data_device.cc
@@ -0,0 +1,33 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/exo/data_device.h"
+
+#include "ash/shell.h"
+#include "base/logging.h"
+#include "base/strings/utf_string_conversions.h"
+#include "ui/aura/client/drag_drop_client.h"
+#include "ui/aura/window.h"
+#include "ui/base/dragdrop/drag_drop_types.h"
+#include "ui/base/dragdrop/os_exchange_data.h"
+
+namespace exo {
+
+DataDeviceDelegate::~DataDeviceDelegate() = default;
+
+DataDevice::DataDevice(std::unique_ptr<DataDeviceDelegate> delegate)
+    : delegate_(std::move(delegate)) {}
+
+void DataDevice::StartDrag(const DataSource* source_resource,
+                           Surface* origin_resource,
+                           Surface* icon_resource,
+                           uint32_t serial) {
+  NOTIMPLEMENTED();
+}
+
+void DataDevice::SetSelection(const DataSource& source, uint32_t serial) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace exo
---
+++ components/exo/data_source.cc
@@ -0,0 +1,22 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/exo/data_source.h"
+
+namespace exo {
+
+DataSourceDelegate::~DataSourceDelegate() = default;
+
+DataSource::DataSource(std::unique_ptr<DataSourceDelegate> delegate)
+    : delegate_(std::move(delegate)) {}
+
+void DataSource::Offer(const std::string& mime_type) {
+  NOTIMPLEMENTED();
+}
+
+void DataSource::SetActions(uint32_t dnd_actions) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace exo
