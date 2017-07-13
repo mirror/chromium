f72e0c4db0be1eab0c95e2df84c19f80c5d54f8e
Tina Quach <quacht@google.com>
2017-07-13 14:10:48 -0700

    Add accessibility tests for passwords section of Chrome Settings.
    This change includes setting up the GTest and Mocha-based a11y testing framework for Chrome WebUI
    by defining an accessibility_browsertest.js file for a11y test fixtures and including this file in the BUILD.gn.
    
    The passwords section now fires an event when the settings section has expanded--an event that is responded to in
    the added a11y test.
    
    Bug:737739
    Change-Id: I54572d16e4d6e6749a20f82b557c2804bdd2f574

---
+++ chrome/test/data/webui/settings/accessibility_browsertest.js
@@ -0,0 +1,80 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** @fileoverview Runs the Polymer Accessibility Settings tests. */
+
+/** @const {string} Path to root from chrome/test/data/webui/settings/. */
+var ROOT_PATH = '../../../../../';
+
+// Polymer BrowserTest fixture and aXe-core accessibility audit.
+GEN_INCLUDE([
+  ROOT_PATH + 'chrome/test/data/webui/polymer_browser_test_base.js',
+  ROOT_PATH + 'third_party/axe-core/axe.js',
+]);
+
+/**
+ * Run aXe-core accessibility audit, print console-friendly representation
+ * of violations to console, and fail the test .
+ */
+SettingsAccessibilityTest.runAudit = function() {
+  return new Promise(function(resolve, reject) {
+
+    var options = {
+      // Exclude rules that don't apply since we are injecting the settings-ui
+      // into an empty page.
+      'rules': {
+        'document-title': { enabled: false },
+        'html-has-lang': { enabled: false }
+      }
+    };
+
+    axe.run(document, options, function (err, results) {
+      if (err) console.log(err);
+
+      var violationCount = results.violations.length;
+
+      if (violationCount) {
+        // Pretty print out the violations detected by the audit.
+        console.log(JSON.stringify(results.violations, null, 4));
+        reject('Found ' + violationCount + ' accessibility violations.');
+      } else {
+        resolve();
+      }
+    });
+  })
+};
+
+/**
+ * Test fixture for Accessibility of the Passwords route of Chrome Settings.
+ * @constructor
+ * @extends {PolymerTest}
+ */
+function SettingsAccessibilityTest() {}
+
+SettingsAccessibilityTest.prototype = {
+  __proto__: PolymerTest.prototype,
+
+  /** @override */
+  browsePreload: 'chrome://md-settings/',
+
+  // Include files that define the mocha tests.
+  extraLibraries: PolymerTest.getLibraries(ROOT_PATH).concat([
+      'ensure_lazy_loaded.js',
+      'passwords_and_autofill_fake_data.js',
+      'passwords_a11y_test.js'
+  ]),
+
+  // TODO(hcarmona): Remove once ADT is not longer in the testing infrastructure
+  runAccessibilityChecks: false,
+
+  setUp: function() {
+    PolymerTest.prototype.setUp.call(this);
+    settings.ensureLazyLoaded();
+  },
+};
+
+TEST_F('SettingsAccessibilityTest', 'All', function() {
+  mocha.run();
+});
+
