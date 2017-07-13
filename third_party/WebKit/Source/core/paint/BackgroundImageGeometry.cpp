c3c4c92c7d6a40fb51afc3195fa1e548ed34fd0a
Emil A Eklund <eae@chromium.org>
2017-07-13 14:42:34 -0700

    Fix background image painting for table cells
    In r484771 the painting logic for table cells and rows with a background
    image was inadvertently changed as a part of a refactoring. This in turn
    led to the wrong ImageClient being used in cases where the background is
    set on the table row as opposed to the cell.
    
    The bug was introduced between patch set 3 and 4 of the original change.
    
    This CL restores the original behavior by using the correct ImageClient.
    
    TEST=fast/table/backgr_on_table_row.html
    BUG=742053
    R=pdr@chromium.org
    
    Change-Id: I7316ddf34698ba78d43067dd46610ece628ef7fe

--- third_party/WebKit/Source/core/paint/BackgroundImageGeometry.cpp
+++ third_party/WebKit/Source/core/paint/BackgroundImageGeometry.cpp
@@ -656,15 +656,15 @@ void BackgroundImageGeometry::Calculate(const LayoutBoxModelObject* container,
 }
 
 const ImageResourceObserver& BackgroundImageGeometry::ImageClient() const {
-  return box_;
+  return coordinate_offset_by_paint_rect_ ? box_ : positioning_box_;
 }
 
 const Document& BackgroundImageGeometry::ImageDocument() const {
-  return box_.GetDocument();
+  return positioning_box_.GetDocument();
 }
 
 const ComputedStyle& BackgroundImageGeometry::ImageStyle() const {
-  return box_.StyleRef();
+  return positioning_box_.StyleRef();
 }
 
 }  // namespace blink
