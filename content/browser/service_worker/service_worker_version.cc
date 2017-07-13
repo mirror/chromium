869f4e5e93a368451393fa979da5c5ba455891fe
Makoto Shimazu <shimazu@chromium.org>
2017-07-13 17:18:43 +0900

    ServiceWorker: Implement SWInstalledScriptsSender
    This is 7th patch of script streaming project split off from
    https://chromium-review.googlesource.com/c/538477.
    Design doc: 
    https://docs.google.com/document/d/1BbETxB2K1GeGUv4XIvGGforAJRRgYSNX5x4vPTGsCPU/edit
    
    This patch implements SWInstalledScriptsSender to push the installed scripts
    over mojo pipes from the browser to the renderer. The sender is started when
    ServiceWorkerVersion asks EmbeddedWorkerInstance to start the worker. That
    means that scripts will be received on the io thread immediately after binding
    the Mojo interface.
    
    Bug: 683037
    Change-Id: I5aef2cff6026ae3afa004932f77064f24ba8bfc7

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -1475,11 +1475,14 @@ void ServiceWorkerVersion::StartWorkerInternal() {
   params->pause_after_download = pause_after_download_;
 
   mojom::ServiceWorkerInstalledScriptsInfoPtr installed_scripts_info;
-  if (ServiceWorkerUtils::IsScriptStreamingEnabled()) {
+  if (ServiceWorkerUtils::IsScriptStreamingEnabled() &&
+      !pause_after_download_) {
     DCHECK(!installed_scripts_sender_);
     installed_scripts_sender_ =
-        base::MakeUnique<ServiceWorkerInstalledScriptsSender>();
+        base::MakeUnique<ServiceWorkerInstalledScriptsSender>(
+            this, script_url(), script_cache_map()->AsWeakPtr(), context());
     installed_scripts_info = installed_scripts_sender_->CreateInfoAndBind();
+    installed_scripts_sender_->Start();
   }
 
   embedded_worker_->Start(
