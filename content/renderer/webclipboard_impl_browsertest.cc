f57573216988cec6f6633521242309d5198ff029
Joshua Bell <jsbell@chromium.org>
2017-07-13 14:18:15 -0700

    Mac/Clipboard/Images - only populate bookmark if also populating HTML
    When copying an image (right click > Copy Image) we populate the
    clipboard with a bitmap and additionally with markup ("<img
    src=cat.gif>")... except on Mac where the markup confuses native apps.
    (per https://crbug.com/33016)
    
    Additionally, if an ALT attribute is present, we populate the
    clipboard with a "bookmark" (text/url pair). This is unlikely to be
    used in general, but when pasting back into Chrome it would be present
    in the transfer data as text/plain string of the URL, if-and-only-if
    there was not also a text/html string. Which is to say: on the Mac
    (per above paragraph).
    
    And web apps that are not particularly savvy (including Gmail) prefer to
    paste the string rather than the image. Result: if you copy an image
    with an ALT tag on the Mac it pastes as the URL.
    
    Mitigate this by only populating a "bookmark" if populating markup
    as well.
    
    Bug: 736439
    Change-Id: I831d785940d561d5928183b35e3c3fe27cf387ab

--- content/renderer/webclipboard_impl_browsertest.cc
+++ content/renderer/webclipboard_impl_browsertest.cc
@@ -5,6 +5,8 @@
 #include "content/renderer/webclipboard_impl.h"
 
 #include "base/strings/utf_string_conversions.h"
+#include "build/build_config.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
@@ -36,6 +38,37 @@ IN_PROC_BROWSER_TEST_F(WebClipboardImplTest, PasteRTF) {
   EXPECT_EQ(expected_title, title_watcher.WaitAndGetTitle());
 }
 
+IN_PROC_BROWSER_TEST_F(WebClipboardImplTest, ImageCopy) {
+  BrowserTestClipboardScope clipboard;
+  clipboard.SetText("");
+
+  base::string16 expected_types;
+#if !defined(OS_MACOSX)
+  // See comments in WebClipboardImpl::WriteImage for why the expected types
+  // are platform-specific.
+  expected_types = base::ASCIIToUTF16("file;image/png string;text/html");
+#else
+  expected_types = base::ASCIIToUTF16("file;image/png");
+#endif
+
+  NavigateToURL(shell(), GetTestUrl(".", "image_copy_types.html"));
+
+  shell()
+      ->web_contents()
+      ->GetMainFrame()
+      ->ExecuteJavaScriptWithUserGestureForTests(
+          base::ASCIIToUTF16("document.getElementById('copyme')"
+                             ".contentWindow.document.execCommand('copy');"));
+  content::TitleWatcher title_watcher(shell()->web_contents(), expected_types);
+
+  // Temporary, to diagnose errors on bots; is it a race or more subtle?
+  title_watcher.AlsoWaitForTitle(base::ASCIIToUTF16("-empty-"));
+  title_watcher.AlsoWaitForTitle(base::ASCIIToUTF16("wrong type"));
+  title_watcher.AlsoWaitForTitle(base::ASCIIToUTF16("no paste"));
+
+  shell()->web_contents()->Paste();
+  EXPECT_EQ(expected_types, title_watcher.WaitAndGetTitle());
+}
 }
 
 } // namespace content
