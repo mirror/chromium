27b57aa7e1a5f0eecfb99e5b14ba2bad7ee06f3f
Mike Case <mikecase@google.com>
2017-07-13 09:42:15 -0700

    Switch Android binary deps to be managed by CIPD.
    Removing b/a/update_deps/update_third_party_deps.py script.
    Replacing with a simple CIPD wrapper script.
    
    Adding cipd.ensure files for all deps (this is a file used to
    describe the package you want to download).
    
    Adding cipd.yaml files for all deps (this file was used to
    create the CIPD packages), and can be modified/used in the future
    to update these deps.
    
    CIPD gives us the advantage of having a consistent method of
    downloading/managing our binary dependencies. The goal is to move
    as much as possible from adhoc scripts over to CIPD. CIPD does
    various nice things, such as verifying the integrity of the fetched
    binary and doing smart caching. What our adhoc scripts do currently
    is very inconsistent.
    
    Bug:
    Change-Id: Iccf7defe80c8548e6929d994595e841aa4668c92

---
+++ build/cipd/cipd_wrapper.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import os
+import subprocess
+import sys
+
+RELATIVE_ENSURE_FILEPATH = {
+  'android': os.path.join('android', 'android.ensure')
+}
+
+
+def cipd_ensure(root, service_url, target_os):
+
+  ensure_file = os.path.join(
+      os.path.dirname(__file__), RELATIVE_ENSURE_FILEPATH[target_os])
+
+  depot_tools_path = os.path.join(root, 'third_party', 'depot_tools')
+
+  cipd_binary = os.path.join(depot_tools_path, 'cipd')
+  if sys.platform() in ('cygwin', 'win32'):
+    cipd_binary = os.path.join(depot_tools_path, 'cipd.bat')
+
+  return subprocess.call(
+      [cipd_binary, 'ensure',
+       '-ensure-file', ensure_file,
+       '-root', root,
+       '-service-url', service_url])
+
+
+def main():
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument(
+      '--chromium-root',
+      required=True,
+      help='Root directory for dependency.')
+  parser.add_argument(
+      '--service-url',
+      help='The url of the CIPD service.',
+      default='https://chrome-infra-packages.appspot.com')
+  parser.add_argument(
+      '--target-os',
+      required=True,
+      help='Target OS for build.')
+  args = parser.parse_args()
+  cipd_ensure(args.chromium_root, args.service_url, args.target_os)
+
+
+if __name__ == '__main__':
+  sys.exit(main())
