49423c5c879a2973abff519f771fdda72f4124fe
Tom Sepez <tsepez@chromium.org>
2017-07-13 10:50:36 -0700

    Consolidate use of sandbox_type in SandboxedProcessLauncherDelegate
    Second patch in a series to move from a world where the sandbox
    is either on or off as controlled by a boolean to one in which
    a number of sandbox types may exist.
    
    Remove window's reliance on the windows-specific ShouldSandbox()
    method and rely on the GetSandboxType() on all platforms.
    
    Bug: 715679
    Change-Id: I30ba0f0156b685cce2c2d9b24eecc3768a826267

--- components/nacl/broker/nacl_broker_listener.cc
+++ components/nacl/broker/nacl_broker_listener.cc
@@ -69,6 +69,10 @@ bool NaClBrokerListener::PreSpawnTarget(sandbox::TargetPolicy* policy) {
   return result == sandbox::SBOX_ALL_OK;
 }
 
+SandboxType NaClBrokerListener::GetSandboxType() {
+  return SANNDBOX_TYPE_NO_SANDBOX;
+}
+
 void NaClBrokerListener::OnChannelConnected(int32_t peer_pid) {
   browser_process_ = base::Process::OpenWithExtraPrivileges(peer_pid);
   CHECK(browser_process_.IsValid());
