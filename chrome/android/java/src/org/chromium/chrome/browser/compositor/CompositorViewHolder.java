66aa26869e5ead34b3561aaa6fae3c6f2b5e6904
Piotr Swigon <piotrs@chromium.org>
2017-07-13 16:42:21 +1000

    [Installed PWA] Waits for a frame swap before hiding a splash screen.
    This patch fixes the flash of white between the splash screen and the
    web content in installed web apps. This is achieved with waiting for
    a compositor frame swap before starting splash screen hiding animation.
    
    Adding callbacks for next frame swap in CompositorViewHolder might not
    be at the right level of abstraction - feedback welcome. I introduced
    a generic mechanism as we might need it different places as well.
    E.g. In CustomTabActivity the same issue is solved with a time delay,
    which I plan to change to this mechanism in a follow up patch.
    
    Existing tests for a webapp splash screen cover showing/hiding it in
    various circumstances. Writing a specific test to assert flash of white
    is fixed would be more work than it's worth it.
    
    Bug:734500
    Change-Id: I70f8bde54f251c283e260d17a291685738f9e080

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorViewHolder.java
@@ -127,6 +127,9 @@ public class CompositorViewHolder extends FrameLayout
     private int mOverlayContentWidthMeasureSpec = ContentView.DEFAULT_MEASURE_SPEC;
     private int mOverlayContentHeightMeasureSpec = ContentView.DEFAULT_MEASURE_SPEC;
 
+    // List of callbacks to be called on nearest frame swap.
+    private List<Runnable> mNextFrameSwapCallbacks;
+
     /**
      * This view is created on demand to display debugging information.
      */
@@ -598,6 +601,25 @@ public class CompositorViewHolder extends FrameLayout
 
         if (!mSkipInvalidation || pendingFrameCount == 0) flushInvalidation();
         mSkipInvalidation = !mSkipInvalidation;
+
+        runNextFrameSwapCallbacks();
+    }
+
+    /**
+     * @param nextFrameSwapCallback Callback to run on a nearest compositor frame swap.
+     */
+    public void addNextFrameSwapCallback(Runnable nextFrameSwapCallback) {
+        if (mNextFrameSwapCallbacks == null) mNextFrameSwapCallbacks = new ArrayList<>();
+        mNextFrameSwapCallbacks.add(nextFrameSwapCallback);
+    }
+
+    private void runNextFrameSwapCallbacks() {
+        List<Runnable> runnables = mNextFrameSwapCallbacks;
+        mNextFrameSwapCallbacks = null;
+        if (runnables == null) return;
+        for (Runnable r : runnables) {
+            post(r);
+        }
     }
 
     @Override
