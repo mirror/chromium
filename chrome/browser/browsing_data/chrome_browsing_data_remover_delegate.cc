2d4857ea7486fdafd7f1cde750c5536f84413013
Christian Dullweber <dullweber@chromium.org>
2017-07-13 15:55:43 +0200

    Make content settings non-filterable
    Content Settings don't need to be filterable and using
    WebsiteSettingsFilterAdapter doesn't work in some cases.
    
    Bug: 741934
    Change-Id: I765cd9683079fa7592afc3aedc0ea0f243d782ed

--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
@@ -738,7 +738,7 @@ void ChromeBrowsingDataRemoverDelegate::RemoveEmbedderData(
     for (const content_settings::ContentSettingsInfo* info : *registry) {
       map->ClearSettingsForOneTypeWithPredicate(
           info->website_settings_info()->type(), delete_begin_,
-          base::Bind(&WebsiteSettingsFilterAdapter, filter));
+          HostContentSettingsMap::PatternSourcePredicate());
     }
   }
 
