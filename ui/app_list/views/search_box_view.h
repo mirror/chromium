3549523457d7efbeb57b91bce34b4ec82cc75b6c
Maajid <maajid@chromium.org>
2017-07-13 16:20:02 -0700

    Reland: Use the magnifying glass icon instead of the Google icon for the search box when the default search provider is not Google.
    Bug:736038
    TEST:unit tested, device tested with --enable-features=EnableFullcreenAppList, ASAN and LSAN local test.
    
    The fix for the ASAN failure was to use unique_ptr for the search_box_view pointer in the unit test.

--- ui/app_list/views/search_box_view.h
+++ ui/app_list/views/search_box_view.h
@@ -110,12 +110,18 @@ class APP_LIST_EXPORT SearchBoxView : public views::View,
   // Updates the |content_container_|'s background corner radius.
   void UpdateBackground(bool search_results_state);
 
+  // Used only in the tests to get the current search icon.
+  views::ImageView* get_search_icon_for_test() { return search_icon_; }
+
  private:
   // Updates model text and selection model with current Textfield info.
   void UpdateModel();
 
   // Fires query change notification.
   void NotifyQueryChanged();
+
+  // Updates the search icon.
+  void UpdateSearchIcon(bool is_google, const SkColor& search_box_color);
 
   // Overridden from views::TextfieldController:
   void ContentsChanged(views::Textfield* sender,
@@ -146,7 +152,7 @@ class APP_LIST_EXPORT SearchBoxView : public views::View,
   AppListModel* model_;                 // Owned by the profile-keyed service.
 
   views::View* content_container_;        // Owned by views hierarchy.
-  views::ImageView* google_icon_;         // Owned by views hierarchy.
+  views::ImageView* search_icon_;         // Owned by views hierarchy.
   SearchBoxImageButton* back_button_;     // Owned by views hierarchy.
   SearchBoxImageButton* speech_button_;   // Owned by views hierarchy.
   views::Textfield* search_box_;          // Owned by views hierarchy.
