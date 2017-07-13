dd67622f6431f5a3d925b42eaa044b84db6994ea
Bernhard Bauer <bauerb@chromium.org>
2017-07-13 17:42:24 +0100

    Add unit test for <Alias> tag in OSDD files.
    Bug: 728558
    Change-Id: Ie0d82eba52ecbd0ba397c6b28f8a5bb33050a67b

--- chrome/browser/search_engines/template_url_parser_unittest.cc
+++ chrome/browser/search_engines/template_url_parser_unittest.cc
@@ -228,3 +228,21 @@ TEST_F(TemplateURLParserTest, TestPostSuggestion) {
   EXPECT_EQ(GURL("http://search.yahoo.com/favicon.ico"),
             template_url_->favicon_url());
 }
+
+TEST_F(TemplateURLParserTest, TestKeyword) {
+  ASSERT_NO_FATAL_FAILURE(ParseFile("keyword.xml", nullptr));
+  ASSERT_TRUE(template_url_.get());
+  EXPECT_EQ(ASCIIToUTF16("Example"), template_url_->short_name());
+  EXPECT_EQ("https://www.example.com/search?q={searchTerms}",
+            template_url_->url());
+  EXPECT_EQ(ASCIIToUTF16("moose"), template_url_->keyword());
+}
+
+TEST_F(TemplateURLParserTest, TestEmptyKeyword) {
+  ASSERT_NO_FATAL_FAILURE(ParseFile("empty_keyword.xml", nullptr));
+  ASSERT_TRUE(template_url_.get());
+  EXPECT_EQ(ASCIIToUTF16("Example"), template_url_->short_name());
+  EXPECT_EQ("https://www.example.com/search?q={searchTerms}",
+            template_url_->url());
+  EXPECT_EQ(ASCIIToUTF16("example.com"), template_url_->keyword());
+}
