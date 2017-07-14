ae357269a8f6b54843c263a9e8df8d31a88ea039
Jacob Dufault <jdufault@google.com>
2017-07-14 15:09:18 -0700

    cros: Animate views-based user switch.
    Bug: 719015
    Change-Id: Ie769f6bfc47aa6b6959b66bfc69768351834c47c

---
+++ ash/login/ui/login_constants.h
@@ -0,0 +1,17 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_LOGIN_UI_LOGIN_CONSTANTS_H_
+#define ASH_LOGIN_UI_LOGIN_CONSTANTS_H_
+
+namespace ash {
+
+// How long should animations that change the layout of the user run for? For
+// example, this includes the user switch animation as well as the PIN keyboard
+// show/hide animation.
+constexpr int kChangeUserAnimationDurationMs = 300;
+
+}  // namespace ash
+
+#endif  // ASH_LOGIN_UI_LOGIN_CONSTANTS_H_
