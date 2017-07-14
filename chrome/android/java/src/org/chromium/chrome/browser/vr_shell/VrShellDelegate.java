724cc078086f9eca6ad53a7f7a12dfd257700e29
Michael Thiessen <mthiesse@chromium.org>
2017-07-14 10:06:20 -0400

    VR: Don't attempt to cancel VR entry if we're already in VR.
    Headset NFC scan can pause and resume (without stopping) us while
    we're already in VR, so we stay in VR while this is happening.
    However, when we get paused we want to cancel VR entry so we don't
    enter VR while paused. This CL just adds a check to see if we're in
    VR before attempting to cancel entry.
    
    Bug: 742509
    Change-Id: Ib0cdac3c7e10e5206410835f949f318c73e9349f

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
@@ -1005,7 +1005,7 @@ public class VrShellDelegate implements ApplicationStatus.ActivityStateListener,
 
         if (mInVr) mVSyncEstimator.pause();
 
-        cancelPendingVrEntry();
+        if (!mInVr) cancelPendingVrEntry();
 
         // When the active web page has a vrdisplayactivate event handler,
         // mListeningForWebVrActivate should be set to true, which means a vrdisplayactive event
