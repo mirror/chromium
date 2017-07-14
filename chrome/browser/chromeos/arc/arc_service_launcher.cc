e0e0ad07489a1a80033571af24bbcae31e7e2ece
Hidehiko Abe <hidehiko@chromium.org>
2017-07-15 02:37:29 +0900

    Migrate ArcService to BrowserContextKeyedService part 4.
    This CL migrates ArcBootErrorNotification.
    
    BUG=672829
    TEST=Ran try.
    
    Change-Id: I8ea1f03507a4a654e035f5b9ba34819770d4b6fe

--- chrome/browser/chromeos/arc/arc_service_launcher.cc
+++ chrome/browser/chromeos/arc/arc_service_launcher.cc
@@ -99,8 +99,6 @@ void ArcServiceLauncher::Initialize() {
   arc_service_manager_->AddService(
       base::MakeUnique<ArcBluetoothBridge>(arc_bridge_service));
   arc_service_manager_->AddService(
-      base::MakeUnique<ArcBootErrorNotification>(arc_bridge_service));
-  arc_service_manager_->AddService(
       base::MakeUnique<ArcClipboardBridge>(arc_bridge_service));
   arc_service_manager_->AddService(
       base::MakeUnique<ArcCrashCollectorBridge>(arc_bridge_service));
@@ -202,6 +200,7 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   // List in lexicographical order.
   ArcAccessibilityHelperBridge::GetForBrowserContext(profile);
   ArcAuthService::GetForBrowserContext(profile);
+  ArcBootErrorNotification::GetForBrowserContext(profile);
 
   arc_service_manager_->AddService(base::MakeUnique<ArcBootPhaseMonitorBridge>(
       arc_service_manager_->arc_bridge_service(),
