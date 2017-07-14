62ba04c10ec2aa047348e52357f927a94fd8a9ba
Makoto Shimazu <shimazu@chromium.org>
2017-07-14 17:09:54 +0900

    ServiceWorker: Move OnScriptLoaded to the right place
    This is 6th patch of script streaming project split off from
    https://chromium-review.googlesource.com/c/538477.
    
    This patch:
     - Fixes where OnScriptLoaded() is called when script streaming is enabled
     - Changes to use StringBuilder in ServiceWorker in
       ServiceWorkerInstalledScriptsManager.
    
    The script streaming TRACE_EVENT is broken by this patch. I'll fix it in the
    following patch (https://crrev.com/c/566783).
    
    Bug: 683037
    Change-Id: Ice19b90bf32d007c8a25993b207f6eb5088047a7

--- content/renderer/service_worker/service_worker_context_client.h
+++ content/renderer/service_worker/service_worker_context_client.h
@@ -128,8 +128,12 @@ class ServiceWorkerContextClient : public blink::WebServiceWorkerContextClient,
 
   // Called on the main thread.
   void WorkerContextFailedToStart() override;
-  void WorkerScriptLoaded() override;
   bool HasAssociatedRegistration() override;
+
+  // Called on the main thread when the script is served from Loader or on
+  // the worker thread when the script is served via
+  // WebServiceWorkerInstalledScriptsManager.
+  void WorkerScriptLoaded() override;
 
   void WorkerContextStarted(
       blink::WebServiceWorkerContextProxy* proxy) override;
