58592e0283b6f54e0f058a0ee7c1a3526f91782e
Koji Ishii <kojii@chromium.org>
2017-07-15 01:14:17 +0900

    [LayoutNG] Add ShapeResult and orientation to NGPhysicalTextFragment
    This patch adds ShapeResult and line orientation to
    NGPhysicalTextFragment. These properties are not used today, but will be
    used in LayoutNGPaintFragments.
    
    Includes fix for AccumulateUsedFonts to use the item ShapeResult, and
    cleanup of functions that are no longer used.
    
    BUG=636993
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    
    Change-Id: Id91f177e8d9d91914144ab138f37f25cd612be2f

--- third_party/WebKit/Source/core/layout/ng/inline/ng_inline_box_state.h
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_inline_box_state.h
@@ -18,6 +18,7 @@ namespace blink {
 class NGInlineItem;
 struct NGInlineItemResult;
 class NGLineBoxFragmentBuilder;
+class ShapeResult;
 
 // Fragments that require the layout position/size of ancestor are packed in
 // this struct.
@@ -33,9 +34,9 @@ struct NGPendingPositions {
 // require ancestor position or size.
 // This is a transient object only while building line boxes in a block.
 struct NGInlineBoxState {
-  unsigned fragment_start;
-  const NGInlineItem* item;
-  const ComputedStyle* style;
+  unsigned fragment_start = 0;
+  const NGInlineItem* item = nullptr;
+  const ComputedStyle* style = nullptr;
 
   // The united metrics for the current box. This includes all objects in this
   // box, including descendants, and adjusted by placement properties such as
@@ -65,10 +66,7 @@ struct NGInlineBoxState {
 
   // Compute text metrics for a box. All text in a box share the same metrics.
   void ComputeTextMetrics(const ComputedStyle& style, FontBaseline);
-  void AccumulateUsedFonts(const NGInlineItem&,
-                           unsigned start,
-                           unsigned end,
-                           FontBaseline);
+  void AccumulateUsedFonts(const ShapeResult*, FontBaseline);
 
   // Create a box fragment for this box.
   void SetNeedsBoxFragment(bool when_empty);
