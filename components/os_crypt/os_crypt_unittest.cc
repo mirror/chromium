85fd0aa88f2d5ac856e3cb0759e31466f003a0c4
Christos Froussios <cfroussios@chromium.org>
2017-07-14 15:22:04 +0200

    Cleanup OSCrypt after initialisation
    OSCrypt only needs to read from the backend during its initialisation.
    Instances created for this initialisation can be cleaned up afterwards.
    
    To support this change, the mocking of OSCrypt can no longer be based
    on a singleton. Tests using the mocking mechanism are updated.
    
    Bug:709096
    Change-Id: Iba5b37ad45ada11b9a217dc2c388313f2371f647

--- components/os_crypt/os_crypt_unittest.cc
+++ components/os_crypt/os_crypt_unittest.cc
@@ -151,14 +151,7 @@ TEST_F(OSCryptTest, DecryptError) {
 class OSCryptConcurrencyTest : public testing::Test {
  public:
   OSCryptConcurrencyTest() {
-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-    // Mock the key storage, but not the process of getting the passwords.
-    UseMockKeyStorageForTesting(
-        []() -> KeyStorageLinux* { return OSCryptMockerLinux::GetInstance(); },
-        nullptr);
-#else
     OSCryptMocker::SetUpWithSingleton();
-#endif
   }
 
   ~OSCryptConcurrencyTest() override { OSCryptMocker::TearDown(); };
