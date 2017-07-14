0015e89940eed9ce00e17e00d0fda4ea6048d788
Lawrence Choi <lchoi@chromium.org>
2017-07-14 16:52:36 -0700

    Dynamic content detection
    Added the following flags to the screenshot benchmark:
    -dc_detect: if marked true, the benchmark will take multiple screenshots of the
    page in order to see which pixels change
    -dc_wait_time: wait time in between successive screenshots, defaults to 1 sec
    -dc_extra_screenshots: number of extra screenshots to be taken for dynamic content
    detection, defaults to 1
    -dc_threshold: maximum percentage of dynamic content pixels that the benchmark
    will tolerate, defaults to 0.5
    
    If the dc_detect flag is enabled, the benchmark will take the specified number
    of extra screenshots. For each new screenshot, the RGB values at every pixel
    are compared with those of the original screenshot. If they do not match, the
    pixel is made cyan (0, 255, 255) in the original image, and the count of dynamic
    pixels is incremented. If the percentage of dynamic content pixels is greater
    than the given threshold, the benchmark will return without saving a PNG.
    
    Bug:536285
    Change-Id: Iee8b488cdd2447ee3015eeced946f304cd6239f3

--- tools/perf/contrib/cluster_telemetry/screenshot.py
+++ tools/perf/contrib/cluster_telemetry/screenshot.py
@@ -11,22 +11,30 @@ from telemetry.page import legacy_page_test
 from telemetry.util import image_util
 
 class Screenshot(legacy_page_test.LegacyPageTest):
-  """Takes a PNG screenshot of the page."""
+  """Takes a PNG screenshot of the page. If dc_detect flag is enabled, multiple
+     screenshots will be taken, and only pixels that had the same RGB values
+     across all screenshots will be saved to the final output. Inconsistent
+     pixels will be made cyan."""
 
-  def __init__(self, png_outdir, wait_time=0):
+  def __init__(self, png_outdir, wait_time=0, dc_detect=False, dc_wait_time=1,
+               dc_extra_screenshots=1, dc_threshold=0.5):
     super(Screenshot, self).__init__()
     self._png_outdir = png_outdir
     self._wait_time = wait_time
+    self._dc_detect = dc_detect
+    self._dc_wait_time = dc_wait_time
+    self._dc_extra_screenshots = dc_extra_screenshots
+    self._dc_threshold = dc_threshold
 
   def ValidateAndMeasurePage(self, page, tab, results):
     if not tab.screenshot_supported:
       raise legacy_page_test.MeasurementFailure(
-        'Screenshotting not supported on this platform')
+        "Screenshotting not supported on this platform")
 
     try:
       tab.WaitForDocumentReadyStateToBeComplete()
     except py_utils.TimeoutException:
-      logging.warning("WaitForDocumentReadyStateToBeComplete() timeout, " +
+      logging.warning("WaitForDocumentReadyStateToBeComplete() timeout, "
                       "page: %s", page.name)
       return
 
@@ -46,11 +54,52 @@ class Screenshot(legacy_page_test.LegacyPageTest):
     outpath = outpath.replace('\\', '\\\\')
 
     screenshot = tab.Screenshot()
+    screenshot_pixels = image_util.Pixels(screenshot)
+    image_width = image_util.Width(screenshot)
+    image_height = image_util.Height(screenshot)
+    num_total_pixels = image_width * image_height
+
+    # Dynamic content flag.
+    if self._dc_detect:
+      for i in range(self._dc_extra_screenshots):
+        logging.info("Sleeping for %f seconds.", self._dc_wait_time)
+        time.sleep(self._dc_wait_time)
+
+        # After the specified wait time, take another screenshot of the page.
+        logging.info("Taking extra screenshot %d of %d.", i+1,
+                     self._dc_extra_screenshots)
+        next_screenshot = tab.Screenshot()
+        next_screenshot_pixels = image_util.Pixels(next_screenshot)
+
+        num_dynamic_pixels = 0.0
+        j = 0
+        while j < len(screenshot_pixels):
+          # If the RGB values of this screenshot do not match those of the
+          # original screenshot at a certain pixel, make the pixel cyan, and
+          # increment the count of dynamic pixels.
+          if screenshot_pixels[j] != next_screenshot_pixels[j] or \
+             screenshot_pixels[j+1] != next_screenshot_pixels[j+1] or \
+             screenshot_pixels[j+2] != next_screenshot_pixels[j+2]:
+             screenshot_pixels[j] = 0
+             screenshot_pixels[j+1] = 255
+             screenshot_pixels[j+2] = 255
+             num_dynamic_pixels += 1
+          j += 3
+
+        if (num_dynamic_pixels / num_total_pixels) > self._dc_threshold:
+          logging.error("Percentage of pixels with dynamic content: %f",
+                       num_dynamic_pixels / num_total_pixels)
+          raise legacy_page_test.MeasurementFailure("Percentage of pixels "
+            "with dynamic content is greater than threshold.")
+
+    # Convert the pixel bytearray back into an image.
+    image = image_util.FromRGBPixels(image_width, image_height,
+                                     screenshot_pixels)
 
     # TODO(lchoi): Add logging to image_util.py and/or augment error handling of
     # image_util.WritePngFile
     logging.info("Writing PNG file to %s. This may take awhile.", outpath)
     start = time.time()
-    image_util.WritePngFile(screenshot, outpath)
+    image_util.WritePngFile(image, outpath)
     logging.info("PNG file written successfully. (Took %f seconds)",
                  time.time()-start)
