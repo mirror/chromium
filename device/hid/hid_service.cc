98cfaf4f8b3edb85b072195e1dfd4c18dd63b426
Ke He <ke.he@intel.com>
2017-07-14 11:53:53 +0800

    Unify HidDeviceId type in different platforms.
    Before mojofication on Hid, we have to add typemapping for HidDeviceId first.
    HidDeviceId is defined as char[] on MacOS while as uint64_t on other platforms.
    
    In this CL, we map the PlatformHidDeviceId(actually the original HidDeviceId)
    to HidDeviceId which is type of uint64_t. All clients of HidService should
    hold the new HidDeviceId instead of the PlatformHidDeviceId.
    
    BUG=728223
    
    Change-Id: I3585c9eb0eacb02571258314c14e063adce653fc

--- device/hid/hid_service.cc
+++ device/hid/hid_service.cc
@@ -40,6 +40,7 @@ void HidService::Observer::OnDeviceRemovedCleanup(
 constexpr base::TaskTraits HidService::kBlockingTaskTraits;
 
 std::unique_ptr<HidService> HidService::Create() {
+  HidDeviceInfo::InitHidDeviceIDUniqueKey();
 #if defined(OS_LINUX) && defined(USE_UDEV)
   return base::WrapUnique(new HidServiceLinux());
 #elif defined(OS_MACOSX)
@@ -95,13 +96,15 @@ void HidService::AddDevice(scoped_refptr<HidDeviceInfo> device_info) {
   if (!base::ContainsKey(devices_, device_info->device_id())) {
     devices_[device_info->device_id()] = device_info;
 
+    HidPlatformDeviceId platform_device_id =
+        HidDeviceInfo::GetHidPlatformDeviceIdFromMap(device_info->device_id());
     HID_LOG(USER) << "HID device "
                   << (enumeration_ready_ ? "added" : "detected")
                   << ": vendorId=" << device_info->vendor_id()
                   << ", productId=" << device_info->product_id() << ", name='"
                   << device_info->product_name() << "', serial='"
                   << device_info->serial_number() << "', deviceId='"
-                  << device_info->device_id() << "'";
+                  << platform_device_id << "'";
 
     if (enumeration_ready_) {
       for (auto& observer : observer_list_)
@@ -110,11 +113,14 @@ void HidService::AddDevice(scoped_refptr<HidDeviceInfo> device_info) {
   }
 }
 
-void HidService::RemoveDevice(const HidDeviceId& device_id) {
+void HidService::RemoveDevice(const HidPlatformDeviceId& platform_device_id) {
   DCHECK(thread_checker_.CalledOnValidThread());
+  HidDeviceId device_id =
+      HidDeviceInfo::RemoveDeviceIdMappingFromMap(platform_device_id);
   DeviceMap::iterator it = devices_.find(device_id);
   if (it != devices_.end()) {
-    HID_LOG(USER) << "HID device removed: deviceId='" << device_id << "'";
+    HID_LOG(USER) << "HID device removed: deviceId='" << platform_device_id
+                  << "'";
 
     scoped_refptr<HidDeviceInfo> device = it->second;
     if (enumeration_ready_) {
