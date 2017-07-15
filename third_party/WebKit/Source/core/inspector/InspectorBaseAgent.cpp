afff1a37d9211b39f426e51dd1d2d317a7b912fa
Nicholas Verne <nverne@chromium.org>
2017-07-15 12:45:36 +1000

    Moves WebDevToolsAgentImpl to core. This required removing direct calls to modules/ agents. Instead, these agents register a callback from ModulesInitializer::Initialize
    Bug:712963
    Change-Id: I9159327618118b2a2f652ed15a76271040b5bc30

---
+++ third_party/WebKit/Source/core/inspector/InspectorBaseAgent.cpp
@@ -0,0 +1,30 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/inspector/InspectorBaseAgent.h"
+
+#include "platform/wtf/StdLibExtras.h"
+#include "platform/wtf/Vector.h"
+
+namespace blink {
+
+using SessionInitCallbackVector = Vector<InspectorAgent::SessionInitCallback>;
+SessionInitCallbackVector& GetSessionInitCallbackVector() {
+  DEFINE_THREAD_SAFE_STATIC_LOCAL(SessionInitCallbackVector,
+                                  session_init_vector, ());
+  return session_init_vector;
+}
+
+void InspectorAgent::RegisterSessionInitCallback(SessionInitCallback callback) {
+  GetSessionInitCallbackVector().push_back(callback);
+}
+
+void InspectorAgent::CallSessionInitCallbacks(InspectorSession* session,
+                                              const SessionInitArgs* args) {
+  for (auto& callback : GetSessionInitCallbackVector()) {
+    callback(session, args);
+  }
+}
+
+}  // namespace blink
