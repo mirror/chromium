d4ceb21f580e70f588a048986d619d8035adcc57
Hidehiko Abe <hidehiko@chromium.org>
2017-07-15 22:55:37 +0900

    Migrate ArcService to BrowserContextKeyedService part 13.
    This CL migrates ArcObbMounterBridge.
    
    BUG=672829
    TEST=Ran try.
    
    Change-Id: Iac02330ae8ac2c0226f81ea25f5045494290da0d

--- chrome/browser/chromeos/arc/arc_service_launcher.cc
+++ chrome/browser/chromeos/arc/arc_service_launcher.cc
@@ -107,8 +107,6 @@ void ArcServiceLauncher::Initialize() {
   arc_service_manager_->AddService(
       base::MakeUnique<ArcNetHostImpl>(arc_bridge_service));
   arc_service_manager_->AddService(
-      base::MakeUnique<ArcObbMounterBridge>(arc_bridge_service));
-  arc_service_manager_->AddService(
       base::MakeUnique<ArcPolicyBridge>(arc_bridge_service));
   arc_service_manager_->AddService(
       base::MakeUnique<ArcPowerBridge>(arc_bridge_service));
@@ -196,6 +194,7 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   ArcClipboardBridge::GetForBrowserContext(profile);
   ArcCrashCollectorBridge::GetForBrowserContext(profile);
   ArcEnterpriseReportingService::GetForBrowserContext(profile);
+  ArcObbMounterBridge::GetForBrowserContext(profile);
 
   arc_service_manager_->AddService(base::MakeUnique<ArcBootPhaseMonitorBridge>(
       arc_service_manager_->arc_bridge_service(),
