c7d995ca1332f121d6a852e8910a810007faaa33
Hidehiko Abe <hidehiko@chromium.org>
2017-07-17 04:45:13 +0900

    Migrate ArcService to BrowserContextKeyedService part 24.
    This CL migrates ArcVoiceInteraction{Framework,ArcHome}Service classes.
    
    BUG=672829
    TEST=Ran try. Ran on DUT.
    
    Change-Id: Id46d1b3ab424b340e1c28509f0d349f531de6db8

--- chrome/browser/chromeos/arc/arc_service_launcher.cc
+++ chrome/browser/chromeos/arc/arc_service_launcher.cc
@@ -36,7 +36,6 @@
 #include "chrome/browser/chromeos/arc/wallpaper/arc_wallpaper_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/ash/multi_user/multi_user_util.h"
-#include "chromeos/chromeos_switches.h"
 #include "components/arc/arc_service_manager.h"
 #include "components/arc/arc_session.h"
 #include "components/arc/arc_session_runner.h"
@@ -116,14 +115,6 @@ void ArcServiceLauncher::Initialize() {
       base::MakeUnique<ArcTtsService>(arc_bridge_service));
   arc_service_manager_->AddService(
       base::MakeUnique<ArcUserSessionService>(arc_bridge_service));
-  if (chromeos::switches::IsVoiceInteractionEnabled()) {
-    arc_service_manager_->AddService(
-        base::MakeUnique<ArcVoiceInteractionFrameworkService>(
-            arc_bridge_service));
-    arc_service_manager_->AddService(
-        base::MakeUnique<ArcVoiceInteractionArcHomeService>(
-            arc_bridge_service));
-  }
   arc_service_manager_->AddService(
       base::MakeUnique<ArcVolumeMounterBridge>(arc_bridge_service));
   arc_service_manager_->AddService(
@@ -190,6 +181,8 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   ArcPowerBridge::GetForBrowserContext(profile);
   ArcPrintService::GetForBrowserContext(profile);
   ArcProvisionNotificationService::GetForBrowserContext(profile);
+  ArcVoiceInteractionArcHomeService::GetForBrowserContext(profile);
+  ArcVoiceInteractionFrameworkService::GetForBrowserContext(profile);
 
   arc_service_manager_->AddService(base::MakeUnique<ArcBootPhaseMonitorBridge>(
       arc_service_manager_->arc_bridge_service(),
