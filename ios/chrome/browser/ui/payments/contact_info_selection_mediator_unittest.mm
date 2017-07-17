178ecbee1f745086121e6ef4f35c2b6a6b9fd564
Mathieu Perreault <mathp@chromium.org>
2017-07-17 14:54:39 -0400

    [Payments] iOS: Refactor some unit tests to have a common base
    Bug: None
    Test: pass
    Change-Id: I97c70f276ccfe7edc5aaa698893e5860bdc4ef6e

--- ios/chrome/browser/ui/payments/contact_info_selection_mediator_unittest.mm
+++ ios/chrome/browser/ui/payments/contact_info_selection_mediator_unittest.mm
@@ -5,20 +5,12 @@
 #import "ios/chrome/browser/ui/payments/contact_info_selection_mediator.h"
 
 #include "base/mac/foundation_util.h"
-#include "base/memory/ptr_util.h"
-#include "base/test/scoped_task_environment.h"
-#include "components/autofill/core/browser/autofill_profile.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
-#include "components/autofill/core/browser/test_personal_data_manager.h"
-#include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #include "ios/chrome/browser/payments/payment_request_test_util.h"
 #import "ios/chrome/browser/payments/payment_request_util.h"
-#include "ios/chrome/browser/payments/test_payment_request.h"
 #import "ios/chrome/browser/ui/payments/cells/autofill_profile_item.h"
-#import "ios/web/public/test/fakes/test_web_state.h"
-#include "testing/gtest/include/gtest/gtest.h"
+#import "ios/chrome/browser/ui/payments/payment_request_unittest_base.h"
 #include "testing/platform_test.h"
-#include "third_party/ocmock/gtest_support.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -30,54 +22,41 @@ using ::payment_request_util::GetEmailLabelFromAutofillProfile;
 using ::payment_request_util::GetPhoneNumberLabelFromAutofillProfile;
 }  // namespace
 
-class PaymentRequestContactInfoSelectionMediatorTest : public PlatformTest {
+class PaymentRequestContactInfoSelectionMediatorTest
+    : public PaymentRequestUnitTestBase,
+      public PlatformTest {
  protected:
-  PaymentRequestContactInfoSelectionMediatorTest()
-      : autofill_profile_1_(autofill::test::GetFullProfile()),
-        autofill_profile_2_(autofill::test::GetFullProfile2()),
-        chrome_browser_state_(TestChromeBrowserState::Builder().Build()) {
-    // Add testing profiles to autofill::TestPersonalDataManager.
-    personal_data_manager_.AddTestingProfile(&autofill_profile_1_);
-    personal_data_manager_.AddTestingProfile(&autofill_profile_2_);
-
-    payment_request_ = base::MakeUnique<payments::TestPaymentRequest>(
-        payment_request_test_util::CreateTestWebPaymentRequest(),
-        chrome_browser_state_.get(), &web_state_, &personal_data_manager_);
-    // Override the selected contact profile.
-    payment_request_->set_selected_contact_profile(
-        payment_request_->contact_profiles()[1]);
-  }
-
   void SetUp() override {
+    PaymentRequestUnitTestBase::SetUp();
+
+    AddAutofillProfile(autofill::test::GetFullProfile());
+    AddAutofillProfile(autofill::test::GetFullProfile2());
+
+    CreateTestPaymentRequest();
+
+    // Override the selected contact profile.
+    payment_request()->set_selected_contact_profile(
+        payment_request()->contact_profiles()[1]);
+
     mediator_ = [[ContactInfoSelectionMediator alloc]
-        initWithPaymentRequest:payment_request_.get()];
+        initWithPaymentRequest:payment_request()];
   }
 
-  ContactInfoSelectionMediator* GetMediator() { return mediator_; }
-
-  base::test::ScopedTaskEnvironment scoped_task_evironment_;
+  ContactInfoSelectionMediator* mediator() const { return mediator_; }
 
   ContactInfoSelectionMediator* mediator_;
-
-  autofill::AutofillProfile autofill_profile_1_;
-  autofill::AutofillProfile autofill_profile_2_;
-  web::TestWebState web_state_;
-  autofill::TestPersonalDataManager personal_data_manager_;
-  std::unique_ptr<TestChromeBrowserState> chrome_browser_state_;
-  std::unique_ptr<payments::TestPaymentRequest> payment_request_;
 };
 
 // Tests that the expected selectable items are created and that the index of
 // the selected item is properly set.
 TEST_F(PaymentRequestContactInfoSelectionMediatorTest, TestSelectableItems) {
-  NSArray<CollectionViewItem*>* selectable_items =
-      [GetMediator() selectableItems];
+  NSArray<CollectionViewItem*>* selectable_items = [mediator() selectableItems];
 
   // There must be two selectable items.
   ASSERT_EQ(2U, selectable_items.count);
 
   // The second item must be selected.
-  EXPECT_EQ(1U, GetMediator().selectedItemIndex);
+  EXPECT_EQ(1U, mediator().selectedItemIndex);
 
   CollectionViewItem* item_1 = [selectable_items objectAtIndex:0];
   DCHECK([item_1 isKindOfClass:[AutofillProfileItem class]]);
@@ -85,13 +64,13 @@ TEST_F(PaymentRequestContactInfoSelectionMediatorTest, TestSelectableItems) {
       base::mac::ObjCCastStrict<AutofillProfileItem>(item_1);
   EXPECT_TRUE([profile_item_1.name
       isEqualToString:GetNameLabelFromAutofillProfile(
-                          *payment_request_->contact_profiles()[0])]);
+                          *payment_request()->contact_profiles()[0])]);
   EXPECT_TRUE([profile_item_1.email
       isEqualToString:GetEmailLabelFromAutofillProfile(
-                          *payment_request_->contact_profiles()[0])]);
+                          *payment_request()->contact_profiles()[0])]);
   EXPECT_TRUE([profile_item_1.phoneNumber
       isEqualToString:GetPhoneNumberLabelFromAutofillProfile(
-                          *payment_request_->contact_profiles()[0])]);
+                          *payment_request()->contact_profiles()[0])]);
   EXPECT_EQ(nil, profile_item_1.address);
   EXPECT_EQ(nil, profile_item_1.notification);
 
@@ -101,13 +80,13 @@ TEST_F(PaymentRequestContactInfoSelectionMediatorTest, TestSelectableItems) {
       base::mac::ObjCCastStrict<AutofillProfileItem>(item_2);
   EXPECT_TRUE([profile_item_2.name
       isEqualToString:GetNameLabelFromAutofillProfile(
-                          *payment_request_->contact_profiles()[1])]);
+                          *payment_request()->contact_profiles()[1])]);
   EXPECT_TRUE([profile_item_2.email
       isEqualToString:GetEmailLabelFromAutofillProfile(
-                          *payment_request_->contact_profiles()[1])]);
+                          *payment_request()->contact_profiles()[1])]);
   EXPECT_TRUE([profile_item_2.phoneNumber
       isEqualToString:GetPhoneNumberLabelFromAutofillProfile(
-                          *payment_request_->contact_profiles()[1])]);
+                          *payment_request()->contact_profiles()[1])]);
   EXPECT_EQ(nil, profile_item_2.address);
   EXPECT_EQ(nil, profile_item_2.notification);
 }
@@ -116,28 +95,26 @@ TEST_F(PaymentRequestContactInfoSelectionMediatorTest, TestSelectableItems) {
 // selected contact profile.
 TEST_F(PaymentRequestContactInfoSelectionMediatorTest, TestNoSelectedItem) {
   // Reset the selected contact profile.
-  payment_request_->set_selected_contact_profile(nullptr);
+  payment_request()->set_selected_contact_profile(nullptr);
   mediator_ = [[ContactInfoSelectionMediator alloc]
-      initWithPaymentRequest:payment_request_.get()];
+      initWithPaymentRequest:payment_request()];
 
-  NSArray<CollectionViewItem*>* selectable_items =
-      [GetMediator() selectableItems];
+  NSArray<CollectionViewItem*>* selectable_items = [mediator() selectableItems];
 
   // There must be two selectable items.
   ASSERT_EQ(2U, selectable_items.count);
 
   // The selected item index must be invalid.
-  EXPECT_EQ(NSUIntegerMax, GetMediator().selectedItemIndex);
+  EXPECT_EQ(NSUIntegerMax, mediator().selectedItemIndex);
 }
 
 // Tests that only the requested fields are displayed.
 TEST_F(PaymentRequestContactInfoSelectionMediatorTest, TestOnlyRequestedData) {
   // Update the web_payment_request and reload the items.
-  payment_request_->web_payment_request().options.request_payer_name = false;
-  [GetMediator() loadItems];
+  payment_request()->web_payment_request().options.request_payer_name = false;
+  [mediator() loadItems];
 
-  NSArray<CollectionViewItem*>* selectable_items =
-      [GetMediator() selectableItems];
+  NSArray<CollectionViewItem*>* selectable_items = [mediator() selectableItems];
 
   CollectionViewItem* item = [selectable_items objectAtIndex:0];
   DCHECK([item isKindOfClass:[AutofillProfileItem class]]);
@@ -150,10 +127,10 @@ TEST_F(PaymentRequestContactInfoSelectionMediatorTest, TestOnlyRequestedData) {
   EXPECT_EQ(nil, profile_item.notification);
 
   // Update the web_payment_request and reload the items.
-  payment_request_->web_payment_request().options.request_payer_email = false;
-  [GetMediator() loadItems];
+  payment_request()->web_payment_request().options.request_payer_email = false;
+  [mediator() loadItems];
 
-  selectable_items = [GetMediator() selectableItems];
+  selectable_items = [mediator() selectableItems];
 
   item = [selectable_items objectAtIndex:0];
   DCHECK([item isKindOfClass:[AutofillProfileItem class]]);
@@ -165,12 +142,12 @@ TEST_F(PaymentRequestContactInfoSelectionMediatorTest, TestOnlyRequestedData) {
   EXPECT_EQ(nil, profile_item.notification);
 
   // Update the web_payment_request and reload the items.
-  payment_request_->web_payment_request().options.request_payer_name = true;
-  payment_request_->web_payment_request().options.request_payer_email = true;
-  payment_request_->web_payment_request().options.request_payer_phone = false;
-  [GetMediator() loadItems];
+  payment_request()->web_payment_request().options.request_payer_name = true;
+  payment_request()->web_payment_request().options.request_payer_email = true;
+  payment_request()->web_payment_request().options.request_payer_phone = false;
+  [mediator() loadItems];
 
-  selectable_items = [GetMediator() selectableItems];
+  selectable_items = [mediator() selectableItems];
 
   item = [selectable_items objectAtIndex:0];
   DCHECK([item isKindOfClass:[AutofillProfileItem class]]);
