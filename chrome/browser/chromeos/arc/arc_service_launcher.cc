980357a8ccb107263fe1ec8452fc090f459cfa8c
Hidehiko Abe <hidehiko@chromium.org>
2017-07-18 00:06:03 +0900

    Migrate ArcService to BrowserContextKeyedService part 26.
    This CL migrates ArcWallpaperService.
    
    BUG=672829
    TEST=Ran try.
    
    Change-Id: I2ba4846b223acab81a128097c688d5a100f5a366

--- chrome/browser/chromeos/arc/arc_service_launcher.cc
+++ chrome/browser/chromeos/arc/arc_service_launcher.cc
@@ -125,8 +125,6 @@ void ArcServiceLauncher::Initialize() {
   arc_service_manager_->AddService(
       base::MakeUnique<ArcVolumeMounterBridge>(arc_bridge_service));
   arc_service_manager_->AddService(
-      base::MakeUnique<ArcWallpaperService>(arc_bridge_service));
-  arc_service_manager_->AddService(
       base::MakeUnique<GpuArcVideoServiceHost>(arc_bridge_service));
 }
 
@@ -189,6 +187,7 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   ArcPrintService::GetForBrowserContext(profile);
   ArcProvisionNotificationService::GetForBrowserContext(profile);
   ArcTracingBridge::GetForBrowserContext(profile);
+  ArcWallpaperService::GetForBrowserContext(profile);
 
   arc_service_manager_->AddService(base::MakeUnique<ArcBootPhaseMonitorBridge>(
       arc_service_manager_->arc_bridge_service(),
