eee336696328111d485ce8516f3ca437d43d7bae
Olivier Robin <olivierrobin@chromium.org>
2017-07-17 16:25:58 +0200

    Execute commands on NTP in a completion handler.
    When creating a new tab from the NC extension, commands can be executed
    on it (focus Omnibox, launch QR Code search, launch Voice Search).
    The current process relies on async call to launch the command after the
    tab is created.
    For some scenarios (focus Omnibox while creating a new tab), this is not
    enough, and the search is cancelled by the new tab animation.
    
    Add a completion handler in the tab creation to execute the command
    the tab is created.
    
    Bug:744596
    Change-Id: I3e4688ff858e4ed70e7df62ac10e541a7375cbf1

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -1208,7 +1208,12 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
   // |chromeExecuteCommand:|.
   NSTimeInterval startTime = [NSDate timeIntervalSinceReferenceDate];
   BOOL offTheRecord = self.isOffTheRecord;
+  ProceduralBlock oldForegroundTabWasAddedCompletionBlock =
+      self.foregroundTabWasAddedCompletionBlock;
   self.foregroundTabWasAddedCompletionBlock = ^{
+    if (oldForegroundTabWasAddedCompletionBlock) {
+      oldForegroundTabWasAddedCompletionBlock();
+    }
     double duration = [NSDate timeIntervalSinceReferenceDate] - startTime;
     base::TimeDelta timeDelta = base::TimeDelta::FromSecondsD(duration);
     if (offTheRecord) {
@@ -2082,7 +2087,8 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
   return [self addSelectedTabWithURL:url
                             postData:postData
                              atIndex:[_model count]
-                          transition:transition];
+                          transition:transition
+                  tabAddedCompletion:nil];
 }
 
 - (Tab*)addSelectedTabWithURL:(const GURL&)url
@@ -2096,15 +2102,27 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
                       atIndex:(NSUInteger)position
                    transition:(ui::PageTransition)transition {
   return [self addSelectedTabWithURL:url
+                             atIndex:position
+                          transition:transition
+                  tabAddedCompletion:nil];
+}
+
+- (Tab*)addSelectedTabWithURL:(const GURL&)url
+                      atIndex:(NSUInteger)position
+                   transition:(ui::PageTransition)transition
+           tabAddedCompletion:(ProceduralBlock)tabAddedCompletion {
+  return [self addSelectedTabWithURL:url
                             postData:NULL
                              atIndex:position
-                          transition:transition];
+                          transition:transition
+                  tabAddedCompletion:tabAddedCompletion];
 }
 
 - (Tab*)addSelectedTabWithURL:(const GURL&)URL
                      postData:(TemplateURLRef::PostContent*)postData
                       atIndex:(NSUInteger)position
-                   transition:(ui::PageTransition)transition {
+                   transition:(ui::PageTransition)transition
+           tabAddedCompletion:(ProceduralBlock)tabAddedCompletion {
   if (position == NSNotFound)
     position = [_model count];
   DCHECK(position <= [_model count]);
@@ -2120,6 +2138,20 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
     params.post_data.reset(data);
     params.extra_headers.reset(@{ @"Content-Type" : contentType });
   }
+
+  if (tabAddedCompletion) {
+    ProceduralBlock oldForegroundTabWasAddedCompletionBlock =
+        self.foregroundTabWasAddedCompletionBlock;
+    self.foregroundTabWasAddedCompletionBlock = ^{
+      if (oldForegroundTabWasAddedCompletionBlock) {
+        oldForegroundTabWasAddedCompletionBlock();
+      }
+      if (tabAddedCompletion) {
+        tabAddedCompletion();
+      }
+    };
+  }
+
   Tab* tab = [_model insertTabWithLoadParams:params
                                       opener:nil
                                  openedByDOM:NO
