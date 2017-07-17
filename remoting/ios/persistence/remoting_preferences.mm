454fd242e9ec7c8a4a63190ea16916450e67b7e3
Scott Nichols <nicholss@chromium.org>
2017-07-17 10:31:58 -0700

    Working on first impl of persistant host settings.
    Change-Id: I5fe537d1af157328f47769dccd204ae9b98a6cd3

---
+++ remoting/ios/persistence/remoting_preferences.mm
@@ -0,0 +1,87 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+#import "remoting/ios/persistence/remoting_preferences.h"
+
+#import "base/mac/bind_objc_block.h"
+#import "remoting/ios/domain/host_info.h"
+#import "remoting/ios/domain/host_settings.h"
+
+#include "base/logging.h"
+
+static NSString* const kUserEmailKey =
+    @"kUserEmailKey";
+static NSString* const kHostSettingsKey =
+    @"kHostSettingsKey";
+
+@interface RemotingPreferences () {
+  NSUserDefaults* _defaults;
+}
+@end
+
+@implementation RemotingPreferences
+
+// RemotingService is a singleton.
++ (RemotingPreferences*)instance {
+  static RemotingPreferences* sharedInstance = nil;
+  static dispatch_once_t guard;
+  dispatch_once(&guard, ^{
+    sharedInstance = [[RemotingPreferences alloc] init];
+  });
+  return sharedInstance;
+}
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    _defaults = [NSUserDefaults standardUserDefaults];
+  }
+  return self;
+}
+
+#pragma mark - RemotingPreferences Implementation
+
+- (HostSettings*)settingsForHost:(NSString*)hostId {
+  NSString* key = [NSString stringWithFormat:@"%@-%@", kHostSettingsKey, hostId];
+  NSData *encodedSettings = [_defaults objectForKey:key];
+  HostSettings* settings = [NSKeyedUnarchiver unarchiveObjectWithData:encodedSettings];
+  if (settings == nil) {
+    settings = [[HostSettings alloc] init];
+    settings.hostId = hostId;
+    [self setSettings:settings forHost:hostId];
+  }
+  return settings;
+}
+
+- (void)setSettings:(HostSettings*)settings forHost:(NSString*)hostId {
+  NSString* key = [NSString stringWithFormat:@"%@-%@", kHostSettingsKey, hostId];
+  if (settings) {
+    NSData *encodedSettings = [NSKeyedArchiver archivedDataWithRootObject:settings];
+    [_defaults setObject:encodedSettings forKey:key];
+  } else {
+    return [_defaults removeObjectForKey:key];
+  }
+  [_defaults synchronize];
+}
+
+#pragma mark - Properties
+
+- (void)setActiveUserEmail:(NSString*)email {
+  if (email) {
+    [_defaults setObject:email forKey:kUserEmailKey];
+  } else {
+    [_defaults removeObjectForKey:kUserEmailKey];
+  }
+  [_defaults synchronize];
+}
+
+- (NSString*)activeUserEmail {
+  return [_defaults objectForKey:kUserEmailKey];
+}
+
+@end
