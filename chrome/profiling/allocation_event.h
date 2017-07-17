c45711866f1416e388da298382441ce8fd84b91b
Brett Wilson <brettw@chromium.org>
2017-07-17 14:16:36 -0700

    Export JSON text from the OOP heap profiler
    Change-Id: Ic9dda085032694ff1a5872e357ca2e03c0d42ad3

---
+++ chrome/profiling/allocation_event.h
@@ -0,0 +1,58 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_PROFILING_ALLOCATION_EVENT_H_
+#define CHROME_PROFILING_ALLOCATION_EVENT_H_
+
+#include <functional>
+#include <set>
+
+#include "chrome/profiling/address.h"
+#include "chrome/profiling/backtrace_storage.h"
+
+namespace profiling {
+
+// This class is copyable and assignable.
+class AllocationEvent {
+ public:
+  AllocationEvent(Address addr, size_t sz, BacktraceStorage::Key key);
+
+  // This partial initializer creates an allocation of empty size for
+  // searching purposes.
+  explicit AllocationEvent(Address addr);
+
+  Address address() const { return address_; }
+  size_t size() const { return size_; }
+  BacktraceStorage::Key backtrace_key() const { return backtrace_key_; }
+
+  // Implements < for AllocationEvents using address only.
+  struct AddressLess
+      : public std::binary_function<AllocationEvent, AllocationEvent, bool> {
+    bool operator()(const AllocationEvent& lhs,
+                    const AllocationEvent& rhs) const {
+      return lhs.address() < rhs.address();
+    }
+  };
+
+  // Implements == for AllocationEvents using address only.
+  struct AddressEqual
+      : public std::binary_function<AllocationEvent, AllocationEvent, bool> {
+    bool operator()(const AllocationEvent& lhs,
+                    const AllocationEvent& rhs) const {
+      return lhs.address() == rhs.address();
+    }
+  };
+
+ private:
+  Address address_;
+  size_t size_;
+  BacktraceStorage::Key backtrace_key_;
+};
+
+using AllocationEventSet =
+    std::set<AllocationEvent, AllocationEvent::AddressLess>;
+
+}  // namespace profiling
+
+#endif  // CHROME_PROFILING_ALLOCATION_EVENT_H_
---
+++ chrome/profiling/json_exporter.cc
@@ -0,0 +1,13 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/profiling/json_exporter.h"
+
+namespace profiling {
+
+void ExportAllocationEventSetToJSON(const BacktraceStorage* backtrace_storage,
+                                    const AllocationEventSet& set,
+                                    std::ostream& out) {}
+
+}  // namespace profiling
