5660ce2045053587413c599d849e17a8fd77a3a9
Tanin Na Nakorn <tanin@google.com>
2017-07-17 11:16:08 -0700

    Add command-line switch --log-url-log to write requested URLs to a text file.
    When in use, all URLs requested by the browser (across all contexts/profiles) are appended to a newline-separated text file.
    
    This same information could also be obtained using --log-net-log. The difference is that
     (1) --log-url-log is a much simpler file format (newline separated URLs)
     (2) --log-url-log flushes writes synchronously after each URL
    This means if a crash occurs shortly after requesting a particular URL, it is guaranteed that the URL was written to the log file (whereas --log-net-log buffers its writes, and may or may not have committed it by the time of crash).
    
    Use of this switch can cause jank on the IO thread, and hence should only be used for testing. It is intended for test tools (such as Clusterfuzz) which need a trace of the URLs being requested by the browser.
    
    Bug: 715656
    Change-Id: I18e96afb5c5deb84d51691760606c79061751369

--- chrome/browser/net/chrome_network_delegate_browsertest.cc
+++ chrome/browser/net/chrome_network_delegate_browsertest.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "base/base_paths.h"
+#include "base/command_line.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/macros.h"
 #include "base/path_service.h"
@@ -12,12 +13,18 @@
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/url_log/url_log.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_navigation_observer.h"
 #include "net/base/filename_util.h"
 #include "net/base/network_delegate.h"
 #include "url/gurl.h"
+
+namespace {
+
+const char* const kExpectedFiles[] = {"test.html", "testharness.js",
+                                      "styles.css", "child.html", "child.js"};
 
 #if defined(OS_CHROMEOS)
 
@@ -82,3 +89,55 @@ IN_PROC_BROWSER_TEST_F(ChromeNetworkDelegateBrowserTest, AccessToSymlink) {
 }
 
 #endif  // defined(OS_CHROMEOS)
+
+class ChromeNetworkDelegateUrlLogBrowserTest : public InProcessBrowserTest {
+ public:
+  ChromeNetworkDelegateUrlLogBrowserTest() {
+    PathService::Get(base::DIR_SOURCE_ROOT, &test_data_dir_);
+    test_data_dir_ = test_data_dir_.Append(FILE_PATH_LITERAL("components"));
+    test_data_dir_ = test_data_dir_.Append(FILE_PATH_LITERAL("url_log"));
+    test_data_dir_ = test_data_dir_.Append(FILE_PATH_LITERAL("test_data"));
+  }
+
+  void SetUp() override {
+    ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
+    log_path_ = temp_dir_.GetPath().AppendASCII("UrlLogFile");
+    InProcessBrowserTest::SetUp();
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    command_line->AppendSwitchPath(url_log::kLogUrlLog, log_path_);
+    command_line->AppendArg(
+        net::FilePathToFileURL(test_data_dir_.AppendASCII("test.html")).spec());
+  }
+
+ private:
+  base::ScopedTempDir temp_dir_;
+  base::FilePath log_path_;
+  base::FilePath test_data_dir_;
+
+  DISALLOW_COPY_AND_ASSIGN(ChromeNetworkDelegateUrlLogBrowserTest);
+};
+
+IN_PROC_BROWSER_TEST_F(ChromeNetworkDelegateUrlLogBrowserTest, WriteUrlLog) {
+  base::RunLoop().RunUntilIdle();
+
+  base::ThreadRestrictions::ScopedAllowIO allow_io_for_log_path;
+  EXPECT_TRUE(base::PathExists(log_path_));
+
+  std::string input;
+  EXPECT_TRUE(base::ReadFileToString(log_path_, &input));
+
+  std::vector<std::string> lines = base::SplitString(
+      input, "\n", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL);
+
+  for (const std::string& filename : kExpectedFiles) {
+    std::string full_path(
+        net::FilePathToFileURL(test_data_dir_.AppendASCII(filename)).spec());
+    EXPECT_TRUE(base::ContainsValue(lines, full_path))
+        << "Expect " << full_path << " to be in:\n"
+        << input;
+  }
+}
+
+}  // namespace
