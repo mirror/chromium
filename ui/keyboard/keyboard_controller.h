67f675b406d2fce40efc7d44d989a67fd7ce3cf6
oka@chromium.org <oka@chromium.org>
2017-07-18 18:21:24 +0900

    Remove keyboard_visible_
    Bug: 742246
    Change-Id: Ie0efac2186e44a9df043c0c93e56c93f9e95bb1b

--- ui/keyboard/keyboard_controller.h
+++ ui/keyboard/keyboard_controller.h
@@ -101,8 +101,7 @@ class KEYBOARD_EXPORT KeyboardController : public ui::InputMethodObserver,
   void Reload();
 
   // Hides virtual keyboard and notifies observer bounds change.
-  // This method hides the keyboard with a delay to avoid layout flicker
-  // when the focus of input field quickly change.
+  // This method immediately starts hiding animation.
   void HideKeyboard(HideReason reason);
 
   // Notifies the observer for contents bounds changed.
@@ -143,8 +142,8 @@ class KEYBOARD_EXPORT KeyboardController : public ui::InputMethodObserver,
   // Retrieve the active keyboard controller.
   static KeyboardController* GetInstance();
 
-  // Returns true if keyboard is currently visible.
-  bool keyboard_visible() const { return keyboard_visible_; }
+  // Returns true if keyboard is SHOWN or SHOWING state.
+  bool keyboard_visible();
 
   bool show_on_resize() const { return show_on_resize_; }
 
