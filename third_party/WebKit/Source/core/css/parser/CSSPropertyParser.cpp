7ab6da29424fdf05a57f96ffcfd2864225277303
Rob Buis <rob.buis@samsung.com>
2017-07-18 13:46:13 -0400

    Implements CSSPropertyAPI for the -webkit-locale property
    As part of Project Ribbon, separate the parsing logic for CSS
    property -webkit-locale from the parser into an API.
    
    Bug: 668012
    
    Signed-off-by: Rob Buis <rob.buis@samsung.com>
    Change-Id: I6a1e7cf419d2f425ef2ca736f0153c48025caa78

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -303,12 +303,6 @@ static CSSValue* ConsumeFontVariantList(CSSParserTokenRange& range) {
   return nullptr;
 }
 
-static CSSValue* ConsumeLocale(CSSParserTokenRange& range) {
-  if (range.Peek().Id() == CSSValueAuto)
-    return ConsumeIdent(range);
-  return ConsumeString(range);
-}
-
 static CSSFunctionValue* ConsumeFilterFunction(
     CSSParserTokenRange& range,
     const CSSParserContext* context) {
@@ -1016,9 +1010,6 @@ const CSSValue* CSSPropertyParser::ParseSingleValue(
     case CSSPropertyWebkitLogicalWidth:
     case CSSPropertyWebkitLogicalHeight:
       return CSSPropertyLengthUtils::ConsumeWidthOrHeight(range_, *context_);
-    case CSSPropertyWebkitHyphenateCharacter:
-    case CSSPropertyWebkitLocale:
-      return ConsumeLocale(range_);
     case CSSPropertyAnimationDelay:
     case CSSPropertyTransitionDelay:
       return ConsumeCommaSeparatedList(ConsumeTime, range_, kValueRangeAll);
