227ac8120f8071e0c03cb986a44bc2cf1720fc21
Daichi Hirono <hirono@chromium.org>
2017-07-18 13:06:28 +0900

    Add wl_data_device and relatives' stubs.
    The CL adds stubs of wl_data_device, wl_data_source, and wl_data_offer to
    exo.
    
    BUG=b:31988797, b:28779679
    TEST=No new test since it just adds stubs.
    
    Change-Id: I6e9ae74d00dbfb8306f2c8285176965feaf3d7cc

---
+++ components/exo/dnd_action.h
@@ -0,0 +1,14 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_EXO_DND_ACTION_H_
+#define COMPONENTS_EXO_DND_ACTION_H_
+
+namespace exo {
+
+enum class DndAction { kNone, kCopy, kMove, kAsk };
+
+}  // namespace exo
+
+#endif  // COMPONENTS_EXO_DND_ACTION_H_
