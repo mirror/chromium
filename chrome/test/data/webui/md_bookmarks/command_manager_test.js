33ade08fd949967ad5b6af819c32fce29d1c4e14
Tim Sergeant <tsergeant@chromium.org>
2017-07-18 15:27:34 +1000

    MD Bookmarks: Disable keyboard shortcuts when a dialog is open
    Even though our dialogs are modal, it was still possible to execute
    keyboard commands on the background content by clicking on the dialog
    background, which refocuses the document body and allowed commands
    to execute.
    
    Bug: 742972
    Change-Id: I01f9cb3060fd6a91a6fa5a832578f3f245712e0c

--- chrome/test/data/webui/md_bookmarks/command_manager_test.js
+++ chrome/test/data/webui/md_bookmarks/command_manager_test.js
@@ -58,6 +58,7 @@ suite('<bookmarks-command-manager>', function() {
     replaceBody(commandManager);
     document.body.appendChild(
         document.createElement('bookmarks-toast-manager'));
+    bookmarks.DialogFocusManager.instance_ = null;
   });
 
   test('Copy URL is only active for single URL items', function() {
@@ -164,7 +165,7 @@ suite('<bookmarks-command-manager>', function() {
     commandManager.assertLastCommand('redo');
   });
 
-  test.only('Show In Folder is only available during search', function() {
+  test('Show In Folder is only available during search', function() {
     store.data.selection.items = new Set(['12']);
     store.notifyObservers();
 
@@ -342,6 +343,20 @@ suite('<bookmarks-command-manager>', function() {
       commandItem[element.getAttribute('command')] = element;
     });
     MockInteractions.tap(commandItem[Command.EDIT]);
+    commandManager.assertLastCommand(null);
+  });
+
+  test('keyboard shortcuts are disabled while a dialog is open', function() {
+    assertFalse(bookmarks.DialogFocusManager.getInstance().hasOpenDialog());
+    items = new Set(['12']);
+    store.data.selection.items = items;
+    store.notifyObservers();
+
+    MockInteractions.pressAndReleaseKeyOn(document.body, '', '', 'F2');
+    commandManager.assertLastCommand(Command.EDIT);
+    assertTrue(bookmarks.DialogFocusManager.getInstance().hasOpenDialog());
+
+    MockInteractions.pressAndReleaseKeyOn(document.body, '', '', 'Delete');
     commandManager.assertLastCommand(null);
   });
 });
