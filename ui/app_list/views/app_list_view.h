7f1e2377b92c7e58111472e665d6d0913aa902ab
Alex Newcomer <newcomer@chromium.org>
2017-07-18 13:29:06 -0700

    Launcher animations!
    First round of launcher animations.
    The launcher now animates from the bottom of the screen when it is
    loaded.
    The launcher also animates between states.
    When the launcher is closed it keeps its old closing animation (for
    now).
    
    Round 2 changes:
    Linking the launcher animation with the search result animation.
    Jank improvements.
    
    BUG=735498
    
    Change-Id: I11bc0324f9b34d0257e6c08642f0bddb512fa4e4

--- ui/app_list/views/app_list_view.h
+++ ui/app_list/views/app_list_view.h
@@ -94,10 +94,6 @@ class APP_LIST_EXPORT AppListView : public views::BubbleDialogDelegateView,
   // hiding the app list when a modal dialog is being shown).
   void SetAppListOverlayVisible(bool visible);
 
-  views::Widget* search_box_widget() const { return search_box_widget_; }
-
-  SearchBoxView* search_box_view() { return search_box_view_; }
-
   // Overridden from views::View:
   gfx::Size CalculatePreferredSize() const override;
   void OnPaint(gfx::Canvas* canvas) override;
@@ -108,8 +104,6 @@ class APP_LIST_EXPORT AppListView : public views::BubbleDialogDelegateView,
   bool ShouldDescendIntoChildForEventHandling(
       gfx::NativeView child,
       const gfx::Point& location) override;
-
-  AppListMainView* app_list_main_view() { return app_list_main_view_; }
 
   // Gets the PaginationModel owned by this view's apps grid.
   PaginationModel* GetAppsPaginationModel();
@@ -122,9 +116,15 @@ class APP_LIST_EXPORT AppListView : public views::BubbleDialogDelegateView,
   // Changes the app list state.
   void SetState(AppListState new_state);
 
+  // Kicks off the proper animation for the state change.
+  void SetStateAnimation(AppListState new_state);
+
   // Changes the app list state depending on the current |app_list_state_| and
   // whether the search box is empty.
   void SetStateFromSearchBoxView(bool search_box_is_empty);
+
+  // Called when maximize mode starts and ends.
+  void OnMaximizeModeChanged(bool started);
 
   bool is_fullscreen() const {
     return app_list_state_ == FULLSCREEN_ALL_APPS ||
@@ -132,9 +132,9 @@ class APP_LIST_EXPORT AppListView : public views::BubbleDialogDelegateView,
   }
 
   AppListState app_list_state() const { return app_list_state_; }
-
-  // Called when maximize mode starts and ends.
-  void OnMaximizeModeChanged(bool started);
+  AppListMainView* app_list_main_view() { return app_list_main_view_; }
+  views::Widget* search_box_widget() const { return search_box_widget_; }
+  SearchBoxView* search_box_view() { return search_box_view_; }
 
   // Changes |app_list_state_| from |PEEKING| to |FULLSCREEN_ALL_APPS|.
   bool HandleScroll(const ui::Event* event);
@@ -221,7 +221,6 @@ class APP_LIST_EXPORT AppListView : public views::BubbleDialogDelegateView,
   bool processing_scroll_event_series_;
   // The state of the app list, controlled via SetState().
   AppListState app_list_state_;
-
   // An observer that notifies AppListView when the display has changed.
   ScopedObserver<display::Screen, display::DisplayObserver> display_observer_;
 
