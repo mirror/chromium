87e8cbdf18f81e2d9fe3365f81e891b1cc7fff1a
Hiroki Nakagawa <nhiroki@chromium.org>
2017-07-18 09:05:30 +0900

    Worker: Separate WorkerThreadStartupData for cleanup
    This CL separates WorkerThreadStartupData into 2 structs:
    GlobalScopeCreationParams for creating global scope and
    WorkerBackingThreadStartupData for initializing WorkerBackingThread.
    
    Before this CL, WorkerThreadStartupData contains both the data and it causes
    awkward initialization like [1]. This CL simplifies parameter handling and
    unblocks further refactorings like separating WorkerBackingThread initialization
    from WorkerThread.
    
    [1] https://chromium-review.googlesource.com/c/567794/
    
    Bug: 710364
    Change-Id: I3123b84faa8df29bcc0988395f5efb7906ba2341

--- third_party/WebKit/Source/core/workers/DedicatedWorkerTest.cpp
+++ third_party/WebKit/Source/core/workers/DedicatedWorkerTest.cpp
@@ -3,17 +3,19 @@
 // found in the LICENSE file.
 
 #include <memory>
+#include "bindings/core/v8/V8CacheOptions.h"
 #include "core/dom/TaskRunnerHelper.h"
 #include "core/events/MessageEvent.h"
 #include "core/inspector/ConsoleMessageStorage.h"
 #include "core/testing/DummyPageHolder.h"
 #include "core/workers/DedicatedWorkerGlobalScope.h"
 #include "core/workers/DedicatedWorkerThread.h"
+#include "core/workers/GlobalScopeCreationParams.h"
 #include "core/workers/InProcessWorkerMessagingProxy.h"
 #include "core/workers/InProcessWorkerObjectProxy.h"
+#include "core/workers/WorkerBackingThreadStartupData.h"
 #include "core/workers/WorkerInspectorProxy.h"
 #include "core/workers/WorkerThread.h"
-#include "core/workers/WorkerThreadStartupData.h"
 #include "core/workers/WorkerThreadTestHelper.h"
 #include "platform/CrossThreadFunctional.h"
 #include "platform/testing/UnitTestHelpers.h"
@@ -45,11 +47,11 @@ class DedicatedWorkerThreadForTest final : public DedicatedWorkerThread {
   }
 
   WorkerOrWorkletGlobalScope* CreateWorkerGlobalScope(
-      std::unique_ptr<WorkerThreadStartupData> startup_data) override {
+      std::unique_ptr<GlobalScopeCreationParams> creation_params) override {
     return new DedicatedWorkerGlobalScope(
-        startup_data->script_url_, startup_data->user_agent_, this,
-        time_origin_, std::move(startup_data->starter_origin_privilege_data_),
-        std::move(startup_data->worker_clients_));
+        creation_params->script_url, creation_params->user_agent, this,
+        time_origin_, std::move(creation_params->starter_origin_privilege_data),
+        std::move(creation_params->worker_clients));
   }
 
   // Emulates API use on DedicatedWorkerGlobalScope.
@@ -131,17 +133,17 @@ class InProcessWorkerMessagingProxyForTest
     CSPHeaderAndType header_and_type("contentSecurityPolicy",
                                      kContentSecurityPolicyHeaderTypeReport);
     headers->push_back(header_and_type);
-    WorkerV8Settings worker_v8_settings = WorkerV8Settings::Default();
-    worker_v8_settings.atomics_wait_mode_ =
-        WorkerV8Settings::AtomicsWaitMode::kAllow;
     InitializeWorkerThread(
-        WorkerThreadStartupData::Create(
+        WTF::MakeUnique<GlobalScopeCreationParams>(
             script_url, "fake user agent", source, nullptr /* cachedMetaData */,
             kDontPauseWorkerGlobalScopeOnStart, headers.get(),
             "" /* referrerPolicy */, security_origin_.Get(),
             nullptr /* workerClients */, kWebAddressSpaceLocal,
             nullptr /* originTrialTokens */, nullptr /* workerSettings */,
-            worker_v8_settings),
+            kV8CacheOptionsDefault),
+        WorkerBackingThreadStartupData(
+            WorkerBackingThreadStartupData::HeapLimitMode::kDefault,
+            WorkerBackingThreadStartupData::AtomicsWaitMode::kAllow),
         script_url);
   }
 
