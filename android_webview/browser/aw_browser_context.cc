308ca04bea66b082cc0374a765eb2ad4e4e647f2
Nate Fischer <ntfschr@chromium.org>
2017-07-18 16:49:11 -0700

    AW: add Safe Browsing WebUI
    This adds the chrome://safe-browsing/ WebUI page to WebView. This allows
    WebView to hardcode URLs for testing Safe Browsing. This includes the
    resources for the basic chrome://safe-browsing page.
    
    As part of this, this adds WebUI to WebView for the first time.
    
    This contains 2 changes to get things running for our tests:
    
     - Changes a DCHECK_EQ in content layer. This assumption doesn't
       hold for single process, so it crashes tests running in single
       process mode.
     - Adds more SandboxedProcessServices for the test shell. No change is
       needed for the actual WebView apk, because it already has more than
       enough SandboxedProcessServices (20).
    
    The additional resources result in the following APK size increases for
    the system_webview_google_apk target:
    
     - en-US.pak: 6896 -> 6947 (+51 bytes)
     - resources.pak: 434005 -> 436285 (+2280 bytes)
    
    Bug: 709626
    Test: run_webview_instrumentation_test_apk -f SafeBrowsingTest#testSafeBrowsingHardcodedUrls
    Change-Id: Ie312521b323a5ba5ec37f8c44eaa1f0427587e34

--- android_webview/browser/aw_browser_context.cc
+++ android_webview/browser/aw_browser_context.cc
@@ -12,6 +12,7 @@
 #include "android_webview/browser/aw_permission_manager.h"
 #include "android_webview/browser/aw_quota_manager_bridge.h"
 #include "android_webview/browser/aw_resource_context.h"
+#include "android_webview/browser/aw_web_ui_controller_factory.h"
 #include "android_webview/browser/net/aw_url_request_context_getter.h"
 #include "android_webview/common/aw_content_client.h"
 #include "base/base_paths_android.h"
@@ -208,6 +209,9 @@ void AwBrowserContext::PreMainMessageLoopRun() {
   safe_browsing_trigger_manager_ =
       base::MakeUnique<safe_browsing::TriggerManager>(
           safe_browsing_ui_manager_.get());
+
+  content::WebUIControllerFactory::RegisterFactory(
+      AwWebUIControllerFactory::GetInstance());
 }
 
 void AwBrowserContext::OnWebRestrictionsAuthorityChanged() {
