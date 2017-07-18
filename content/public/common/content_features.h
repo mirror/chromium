ade65250cb1db5d2c96e72b3bd5ebbed82fefe3a
Chris Harrelson <chrishtr@chromium.org>
2017-07-18 15:14:29 -0700

    none
    none
    
    Bug:
    Change-Id: I40f0dbddd1efc8a751fc3d48c97446e1f04bbeeb

--- content/public/common/content_features.h
+++ content/public/common/content_features.h
@@ -104,6 +104,7 @@ CONTENT_EXPORT extern const base::Feature kAndroidAutofillAccessibility;
 CONTENT_EXPORT extern const base::Feature kImeThread;
 CONTENT_EXPORT extern const base::Feature kServiceWorkerPaymentApps;
 CONTENT_EXPORT extern const base::Feature kWebNfc;
+CONTENT_EXPORT extern const base::Feature kDisablePreferCompositingToLCDText;
 #endif  // defined(OS_ANDROID)
 
 #if defined(OS_WIN)
