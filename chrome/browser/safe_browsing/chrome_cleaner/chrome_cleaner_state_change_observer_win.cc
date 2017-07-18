ce3b27302a51dca8e16b8a92d4634c567cfd1f6b
proberge <proberge@chromium.org>
2017-07-18 10:10:37 -0400

    Chrome Cleanup WebUI: Update loadTimeData as state change
    This allows the Chrome Cleanup WebUI to be consistent when the settings
    page is refreshed. It also works around https://crbug.com/740629 by
    having the state change immediately rather than the next time a settings
    page is opened.
    
    Previously, if a settings page was opened before transition to kInfected,
    refreshing the page would not show the card. However, if a new settings
    page was opened, refreshing the original one would start showing the
    card. This caused some re-layout issue in some edge cases.
    
    DO NOT SUBMIT before merging in https://chromium-review.googlesource.com/c/568399/
    
    Bug:690020
    Change-Id: Ib56c372bbe3f5b63c050abbd26bf7e6e4958eb8a

---
+++ chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_state_change_observer_win.cc
@@ -0,0 +1,70 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_state_change_observer_win.h"
+
+#include "base/threading/thread_checker.h"
+#include "content/public/browser/browser_thread.h"
+
+using content::BrowserThread;
+
+namespace safe_browsing {
+
+ChromeCleanerStateChangeObserver::ChromeCleanerStateChangeObserver(
+    const OnShowCleanupUIChangeCallback& on_show_cleanup_ui_change)
+    : on_show_cleanup_ui_change_(on_show_cleanup_ui_change),
+      cached_should_show_cleanup_in_settings_ui_(
+          ChromeCleanerController::ShouldShowCleanupInSettingsUI()),
+      cached_cleanup_powered_by_partner_(false) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  ChromeCleanerController::GetInstance()->AddObserver(this);
+}
+
+ChromeCleanerStateChangeObserver::~ChromeCleanerStateChangeObserver() {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  ChromeCleanerController::GetInstance()->RemoveObserver(this);
+}
+
+void ChromeCleanerStateChangeObserver::OnCleanupStateChange() {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+
+  bool show_cleanup = ChromeCleanerController::ShouldShowCleanupInSettingsUI();
+  bool powered_by_partner = false;
+
+  if (show_cleanup != cached_should_show_cleanup_in_settings_ui_ ||
+      powered_by_partner != cached_cleanup_powered_by_partner_) {
+    cached_should_show_cleanup_in_settings_ui_ = show_cleanup;
+    cached_cleanup_powered_by_partner_ = powered_by_partner;
+    on_show_cleanup_ui_change_.Run(show_cleanup, powered_by_partner);
+  }
+}
+
+void ChromeCleanerStateChangeObserver::OnIdle(
+    ChromeCleanerController::IdleReason idle_reason) {
+  OnCleanupStateChange();
+}
+
+void ChromeCleanerStateChangeObserver::OnScanning() {
+  OnCleanupStateChange();
+}
+
+void ChromeCleanerStateChangeObserver::OnInfected(
+    const std::set<base::FilePath>& files) {
+  OnCleanupStateChange();
+}
+
+void ChromeCleanerStateChangeObserver::OnCleaning(
+    const std::set<base::FilePath>& files) {
+  OnCleanupStateChange();
+}
+
+void ChromeCleanerStateChangeObserver::OnRebootRequired() {
+  OnCleanupStateChange();
+}
+
+void ChromeCleanerStateChangeObserver::OnLogsEnabledChanged(bool logs_enabled) {
+  OnCleanupStateChange();
+}
+
+}  // namespace safe_browsing
