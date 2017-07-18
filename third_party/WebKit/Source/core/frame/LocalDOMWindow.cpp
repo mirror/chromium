11ff6d21e0f5095dbaee85d1279c1af44409c2e3
David Bokan <bokan@chromium.org>
2017-07-18 12:18:32 -0400

    Remove inert-visual-viewport flag (enabled by default)
    This patch removes the --enable-inert-visual-viewport flag from Chrome.
    This does *not* remove the inertVisualViewport setting from Blink. This
    feature is currently on by default but it was accidentally forced to the
    off state by the existence of the command line flag. By removing the
    flag, the Blink setting controls the status of this switch - which is
    currently set to on.
    
    Note: we're not losing anything as the feature is intended to be on by
    default and we didn't have a matching --disable-inert-visual-viewport
    flag so this flag would have been useless as of M61.
    
    This patch fixes some tests that were expecting the flag to be off and
    removes others that were explicitly testing behavior of the flag when
    disabled. I also added a new test to test the new flag-on behavior.
    
    This also exposed a bug in document.rootScroller, window's scrollX
    and scrollY properties reverted to returning the FrameView's rather
    than the rootScroller's scroll offset. This was caught by the
    set-root-scroller.html test and the bug is a simple fix in
    LocalDOMWindow.cpp.
    
    Bug: 740956
    Change-Id: I249a34b4ee67dfbe8230e6bc3048871a6325e43d

--- third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
+++ third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
@@ -1063,6 +1063,8 @@ double LocalDOMWindow::scrollX() const {
 
   document()->UpdateStyleAndLayoutIgnorePendingStylesheets();
 
+  // TODO(bokan): This is wrong when the document.rootScroller is non-default.
+  // crbug.com/505516.
   double viewport_x =
       view->LayoutViewportScrollableArea()->GetScrollOffset().Width();
   return AdjustScrollForAbsoluteZoom(viewport_x, GetFrame()->PageZoomFactor());
@@ -1081,6 +1083,8 @@ double LocalDOMWindow::scrollY() const {
 
   document()->UpdateStyleAndLayoutIgnorePendingStylesheets();
 
+  // TODO(bokan): This is wrong when the document.rootScroller is non-default.
+  // crbug.com/505516.
   double viewport_y =
       view->LayoutViewportScrollableArea()->GetScrollOffset().Height();
   return AdjustScrollForAbsoluteZoom(viewport_y, GetFrame()->PageZoomFactor());
