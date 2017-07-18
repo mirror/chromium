1709fb7b390f7a77bd7f882476849ff2b750f5e1
Nicolas Dossou-gbete <dgn@chromium.org>
2017-07-18 18:48:02 +0100

    [Suggestions] Fix crash on null pointer reference
    Bug: 742056
    Change-Id: I6ae85b9583767fb85cea4d32aa655106d355b813

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -53,6 +53,7 @@ import org.chromium.content.browser.test.util.TouchCommon;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.net.test.EmbeddedTestServer;
 import org.chromium.net.test.util.TestWebServer;
+import org.chromium.policy.test.annotations.Policies;
 import org.chromium.ui.base.PageTransition;
 
 import java.io.IOException;
@@ -495,6 +496,15 @@ public class NewTabPageTest {
                 Assert.assertEquals(View.GONE, ntpView.getPlaceholder().getVisibility());
             }
         });
+    }
+
+    /** Test for https://crbug.com/742056. */
+    @Test
+    @SmallTest
+    @Policies.Add(@Policies.Item(key = "NTPContentSuggestionsEnabled", string = "false"))
+    public void testRemoteSuggestionsEnabledWhenDisabledByPolicy() {
+        Assert.assertFalse(
+                mNtp.getManagerForTesting().getSuggestionsSource().areRemoteSuggestionsEnabled());
     }
 
     private void assertThumbnailInvalidAndRecapture() {
