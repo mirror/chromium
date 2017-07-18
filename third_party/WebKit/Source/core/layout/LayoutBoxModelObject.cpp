cfcf624d536c67d43e62f15c16f2bcafa6378a50
Morten Stenshorne <mstensho@opera.com>
2017-07-18 11:00:25 +0200

    Always re-lay out when an object gets or loses a PaintLayer.
    This rids us of some crufty code, and also fixes bugs. The approach was also
    broken for multicol, because we started UpdatePaginationRecursive() in the
    middle of the tree without looking for a containing flow thread (pagination
    layer). This would result in the new layer erroneously not becoming paginated.
    
    BUG=616596
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I50ed61a7174e360259b7b786bab01cf74616fc32

--- third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
@@ -86,28 +86,6 @@ LayoutBoxModelObject* FindFirstStickyBetween(LayoutObject* from,
 }
 }  // namespace
 
-class FloatStateForStyleChange {
- public:
-  static void SetWasFloating(LayoutBoxModelObject* box_model_object,
-                             bool was_floating) {
-    was_floating_ = was_floating;
-    box_model_object_ = box_model_object;
-  }
-
-  static bool WasFloating(LayoutBoxModelObject* box_model_object) {
-    DCHECK_EQ(box_model_object, box_model_object_);
-    return was_floating_;
-  }
-
- private:
-  // Used to store state between styleWillChange and styleDidChange
-  static bool was_floating_;
-  static LayoutBoxModelObject* box_model_object_;
-};
-
-bool FloatStateForStyleChange::was_floating_ = false;
-LayoutBoxModelObject* FloatStateForStyleChange::box_model_object_ = nullptr;
-
 // The HashMap for storing continuation pointers.
 // The continuation chain is a singly linked list. As such, the HashMap's value
 // is the next pointer associated with the key.
@@ -282,8 +260,6 @@ void LayoutBoxModelObject::StyleWillChange(StyleDifference diff,
         .InvalidatePaintIncludingNonCompositingDescendants();
   }
 
-  FloatStateForStyleChange::SetWasFloating(this, IsFloating());
-
   if (HasLayer() && diff.CssClipChanged())
     Layer()->ClearClipRects();
 
@@ -296,8 +272,6 @@ void LayoutBoxModelObject::StyleDidChange(StyleDifference diff,
   bool had_transform_related_property = HasTransformRelatedProperty();
   bool had_layer = HasLayer();
   bool layer_was_self_painting = had_layer && Layer()->IsSelfPaintingLayer();
-  bool was_floating_before_style_changed =
-      FloatStateForStyleChange::WasFloating(this);
   bool was_horizontal_writing_mode = IsHorizontalWritingMode();
 
   LayoutObject::StyleDidChange(diff, old_style);
@@ -327,14 +301,8 @@ void LayoutBoxModelObject::StyleDidChange(StyleDifference diff,
   PaintLayerType type = LayerTypeRequired();
   if (type != kNoPaintLayer) {
     if (!Layer()) {
-      if (was_floating_before_style_changed && IsFloating())
-        SetChildNeedsLayout();
+      SetChildNeedsLayout();
       CreateLayerAfterStyleChange();
-      if (Parent() && !NeedsLayout()) {
-        Layer()->UpdateSize();
-        // FIXME: We should call a specialized versions of this function.
-        Layer()->UpdateLayerPositionsAfterLayout();
-      }
     }
   } else if (Layer() && Layer()->Parent()) {
     PaintLayer* parent_layer = Layer()->Parent();
@@ -346,8 +314,7 @@ void LayoutBoxModelObject::StyleDidChange(StyleDifference diff,
     Layer()->UpdateClipPath(old_style, StyleRef());
     // Calls DestroyLayer() which clears the layer.
     Layer()->RemoveOnlyThisLayerAfterStyleChange();
-    if (was_floating_before_style_changed && IsFloating())
-      SetChildNeedsLayout();
+    SetChildNeedsLayout();
     if (had_transform_related_property) {
       SetNeedsLayoutAndPrefWidthsRecalcAndFullPaintInvalidation(
           LayoutInvalidationReason::kStyleChange);
