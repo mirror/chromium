43ae9caf9ec50ba6d0476ea459a156e7397e0107
Elly Fong-Jones <ellyjones@chromium.org>
2017-07-18 10:14:59 -0400

    tab_utils: don't crash in AreAllTabsMuted on null WebContents
    If one of the selected indices supplied is not in |tab_strip|,
    this function can crash with a null dereference.
    
    Bug: 730002
    Change-Id: I18dfa17228dc061685dd62ba000d1607c6995290

--- chrome/browser/ui/tabs/tab_utils.cc
+++ chrome/browser/ui/tabs/tab_utils.cc
@@ -370,7 +370,8 @@ bool AreAllTabsMuted(const TabStripModel& tab_strip,
                      const std::vector<int>& indices) {
   for (std::vector<int>::const_iterator i = indices.begin(); i != indices.end();
        ++i) {
-    if (!tab_strip.GetWebContentsAt(*i)->IsAudioMuted())
+    content::WebContents* contents = tab_strip.GetWebContentsAt(*i);
+    if (contents && contents->IsAudioMuted())
       return false;
   }
   return true;
