ef21f54fafb38f2b1a3c4ea368e997ad1d78a5e7
Timothy Loh <timloh@chromium.org>
2017-07-19 13:02:27 +1000

    Fix navigating/closing a background tab with a permission prompt
    This patch fixed navigating/closing background tabs on Desktop that have
    shown a permission prompt. Switching tabs with an active prompt puts us
    in a state where requests_ is non-empty but we don't have a view, but
    this case wasn't correctly handled in CancelRequest().
    
    Bug: 745748
    Change-Id: I5d493288c4a54f5b23efebf98a66356fe0abedcf

--- chrome/browser/permissions/permission_request_manager.cc
+++ chrome/browser/permissions/permission_request_manager.cc
@@ -194,9 +194,9 @@ void PermissionRequestManager::CancelRequest(PermissionRequest* request) {
       continue;
 
     // We can simply erase the current entry in the request table if we aren't
-    // showing the dialog, or if we are showing it and it can accept the update.
-    bool can_erase = view_->CanAcceptRequestUpdate();
-    if (can_erase) {
+    // showing the dialog (because we switched tabs with an active prompt), or
+    // if we are showing it and it can accept the update.
+    if (!view_ || view_->CanAcceptRequestUpdate()) {
       DeleteBubble();
 
       RequestFinishedIncludingDuplicates(*requests_iter);
