4d8f78bd11d0783dc01cf3a1721adc75aa01bf7c
Luke Zielinski <lpz@chromium.org>
2017-07-19 15:22:46 -0400

    Add default values for TagAndAttributesList param.
    In preparation for launching the experiment to collect ad identifiers
    in existing SafeBrowsing reports.
    
    Hoping to also sanity check the plan for reusing this feature in the
    future if we want to change this param:
    When the trial launches, the finch config will go away and the feature
    will remain "disabled", so the code will revert to the Default value
    from this CL. If we later want to change the param, we create a new
    trial with a new param value, which will set the feature to "enabled"
    and the new value from the trial will be used instead.
    
    Bug: 674618
    Change-Id: Ibb5cd5f2b4c95ecda592701084631027582a010a

--- components/safe_browsing/renderer/threat_dom_details.cc
+++ components/safe_browsing/renderer/threat_dom_details.cc
@@ -53,10 +53,22 @@ class TagNameIs {
   std::string tag_;
 };
 
+void GetDefaultTagAndAttributeList(
+    std::vector<TagAndAttributesItem>* tag_and_attributes_list) {
+  DCHECK(tag_and_attributes_list);
+  tag_and_attributes_list->clear();
+
+  // These entries must be sorted by tag name.
+  tag_and_attributes_list->push_back(
+      (TagAndAttributesItem){"div", {"data-google-query-id", "id"}});
+  tag_and_attributes_list->push_back((TagAndAttributesItem){"iframe", {"id"}});
+}
+
 void ParseTagAndAttributeParams(
     std::vector<TagAndAttributesItem>* tag_and_attributes_list) {
   DCHECK(tag_and_attributes_list);
   if (!base::FeatureList::IsEnabled(kThreatDomDetailsTagAndAttributeFeature)) {
+    GetDefaultTagAndAttributeList(tag_and_attributes_list);
     return;
   }
   tag_and_attributes_list->clear();
@@ -232,8 +244,15 @@ bool ShouldHandleElement(
 }  // namespace
 
 TagAndAttributesItem::TagAndAttributesItem() {}
+
+TagAndAttributesItem::TagAndAttributesItem(
+    const std::string& tag_name_param,
+    const std::vector<std::string>& attributes_param)
+    : tag_name(tag_name_param), attributes(attributes_param) {}
+
 TagAndAttributesItem::TagAndAttributesItem(const TagAndAttributesItem& item)
     : tag_name(item.tag_name), attributes(item.attributes) {}
+
 TagAndAttributesItem::~TagAndAttributesItem() {}
 
 uint32_t ThreatDOMDetails::kMaxNodes = 500;
