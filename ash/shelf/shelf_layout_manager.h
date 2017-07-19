5761a2224c5079a0f9e8c4b3fcfee027fdd3366b
MinChen <minch@chromium.org>
2017-07-19 13:22:19 -0700

    Swiping up on shelf to show the app list.
    If the app list is not opened and shelf is visible, swipe up on the shelf
    should show the app list.
    If the app list is opened, swipe up on the shelf should keep the app list
    opened. Tap anywhere else of shelf should close the app list.
    
    This is the basic one currently. Will add some simple animations followed.
    (e.g, change the opacity of the searchbox and apps row by row according
    to the dragging amount)
    
    Bug:735999,742461
    Change-Id: I5eb8b6d4d006a50720050b34168849e9d57370f2

--- ash/shelf/shelf_layout_manager.h
+++ ash/shelf/shelf_layout_manager.h
@@ -121,7 +121,7 @@ class ASH_EXPORT ShelfLayoutManager
   // Processes a gesture event and updates the status of the shelf when
   // appropriate. Returns true if the gesture has been handled and it should not
   // be processed any further, false otherwise.
-  bool ProcessGestureEvent(const ui::GestureEvent& event);
+  bool ProcessGestureEvent(const ui::GestureEvent& event_in_screen);
 
   // Set an animation duration override for the show / hide animation of the
   // shelf. Specifying 0 leads to use the default.
@@ -291,9 +291,9 @@ class ASH_EXPORT ShelfLayoutManager
   bool IsShelfAutoHideForFullscreenMaximized() const;
 
   // Gesture related functions:
-  void StartGestureDrag(const ui::GestureEvent& gesture);
-  void UpdateGestureDrag(const ui::GestureEvent& gesture);
-  void CompleteGestureDrag(const ui::GestureEvent& gesture);
+  void StartGestureDrag(const ui::GestureEvent& gesture_in_screen);
+  void UpdateGestureDrag(const ui::GestureEvent& gesture_in_screen);
+  void CompleteGestureDrag(const ui::GestureEvent& gesture_in_screen);
   void CancelGestureDrag();
 
   // Returns true if the gesture is swiping up on a hidden shelf or swiping down
@@ -328,21 +328,21 @@ class ASH_EXPORT ShelfLayoutManager
   // False when neither the auto hide timer nor the timer task are running.
   bool mouse_over_shelf_when_auto_hide_timer_started_;
 
-  // Whether the fullscreen app list feature is enabled.
-  const bool is_fullscreen_app_list_enabled_;
-
   base::ObserverList<ShelfLayoutManagerObserver> observers_;
 
   // The shelf reacts to gesture-drags, and can be set to auto-hide for certain
-  // gestures. Some shelf behaviour (e.g. visibility state, background color
-  // etc.) are affected by various stages of the drag. The enum keeps track of
-  // the present status of the gesture drag.
+  // gestures. Swiping up from the shelf in maximize mode can open the
+  // fullscreen app list. Some shelf behaviour (e.g. visibility state,
+  // background color etc.) are affected by various stages of the drag. The enum
+  // keeps track of the present status of the gesture drag.
   enum GestureDragStatus {
     GESTURE_DRAG_NONE,
     GESTURE_DRAG_IN_PROGRESS,
     GESTURE_DRAG_CANCEL_IN_PROGRESS,
-    GESTURE_DRAG_COMPLETE_IN_PROGRESS
+    GESTURE_DRAG_COMPLETE_IN_PROGRESS,
+    GESTURE_DRAG_APPLIST_IN_PROGRESS,
   };
+
   GestureDragStatus gesture_drag_status_;
 
   // Tracks the amount of the drag. The value is only valid when
