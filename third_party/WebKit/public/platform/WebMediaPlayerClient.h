a64bd006331860a5fef60390b635b644b1625b71
Tommi <tommi@chromium.org>
2017-07-19 14:04:20 +0200

    Set ready state for WebRTC streams regardless of video frame having been received
    Bug:
    Change-Id: I3f6c886c32b06f6def8024ca585179ca81a6ef29

--- third_party/WebKit/public/platform/WebMediaPlayerClient.h
+++ third_party/WebKit/public/platform/WebMediaPlayerClient.h
@@ -121,6 +121,9 @@ class BLINK_PLATFORM_EXPORT WebMediaPlayerClient {
   // that the controls are currently visible.
   virtual bool HasNativeControls() = 0;
 
+  // Returns true iff the client represents an HTML <audio> element.
+  virtual bool IsAudioElement() { return false; }
+
   // Returns the current display type of the media element.
   virtual WebMediaPlayer::DisplayType DisplayType() const = 0;
 
