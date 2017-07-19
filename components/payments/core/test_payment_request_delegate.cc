94a09ef95ca4033d417a2ab0fd35722f63100d2a
Shanfeng Zhang <szhangcs@google.com>
2017-07-19 12:33:17 -0700

    Move form_parsed_timestamp out of autofill_metrics and put in form_structure.
    Fix issue of using FormInteractionsUkmLogger in AutofillManager.
    
    There's a bug for using FormInteractionsUkmLogger in AutofillManager. AutofillManager.Reset()
    may be called before logging the metrics, which will make FormParsedTimeStamp null when
    logging the metrics.
    This fix will keep FormParsedTimeStamp in form_structure and explicitly pass the FormParsedTimeStamp in the callback.
    
    Bug: 736495
    Change-Id: I0068db7bb8c3681937aac9736c1bad0d15b43928

--- components/payments/core/test_payment_request_delegate.cc
+++ components/payments/core/test_payment_request_delegate.cc
@@ -12,7 +12,13 @@ TestPaymentRequestDelegate::TestPaymentRequestDelegate(
     autofill::PersonalDataManager* personal_data_manager)
     : personal_data_manager_(personal_data_manager),
       locale_("en-US"),
-      last_committed_url_("https://shop.com") {}
+      last_committed_url_("https://shop.com"),
+      request_context_(new net::TestURLRequestContextGetter(
+          base::ThreadTaskRunnerHandle::Get())),
+      payments_client_(request_context_.get(), this),
+      full_card_request_(&autofill_client_,
+                         &payments_client_,
+                         personal_data_manager) {}
 
 TestPaymentRequestDelegate::~TestPaymentRequestDelegate() {}
 
@@ -42,7 +48,7 @@ void TestPaymentRequestDelegate::DoFullCardRequest(
     base::WeakPtr<autofill::payments::FullCardRequest::ResultDelegate>
         result_delegate) {
   if (instantaneous_full_card_request_result_) {
-    result_delegate->OnFullCardRequestSucceeded(credit_card,
+    result_delegate->OnFullCardRequestSucceeded(FullCardRequest(), credit_card,
                                                 base::ASCIIToUTF16("123"));
     return;
   }
@@ -74,7 +80,7 @@ void TestPaymentRequestDelegate::DelayFullCardRequestCompletion() {
 void TestPaymentRequestDelegate::CompleteFullCardRequest() {
   DCHECK(instantaneous_full_card_request_result_ == false);
   full_card_result_delegate_->OnFullCardRequestSucceeded(
-      full_card_request_card_, base::ASCIIToUTF16("123"));
+      FullCardRequest(), full_card_request_card_, base::ASCIIToUTF16("123"));
 }
 
 std::string TestPaymentRequestDelegate::GetAuthenticatedEmail() const {
@@ -82,6 +88,10 @@ std::string TestPaymentRequestDelegate::GetAuthenticatedEmail() const {
 }
 
 PrefService* TestPaymentRequestDelegate::GetPrefService() {
+  return nullptr;
+}
+
+IdentityProvider* TestPaymentRequestDelegate::GetIdentityProvider() {
   return nullptr;
 }
 
