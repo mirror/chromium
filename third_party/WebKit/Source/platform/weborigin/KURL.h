97aaf6b8cc3014df42aa563c94c19a4f0f8ede60
Daniel Cheng <dcheng@chromium.org>
2017-07-19 13:10:03 -0700

    Autoplay: records metrics in UKM entries.
    This is using the UKM service to record the autoplay attempts and
    the unmute results.
    
    Bug: 744659
    Change-Id: I3c5031bce230bdb7ce5379852ea1d2ec94fbaa52

--- third_party/WebKit/Source/platform/weborigin/KURL.h
+++ third_party/WebKit/Source/platform/weborigin/KURL.h
@@ -62,6 +62,8 @@ namespace WTF {
 class TextEncoding;
 }
 
+class GURL;
+
 namespace blink {
 
 struct KURLHash;
@@ -217,6 +219,11 @@ class PLATFORM_EXPORT KURL {
   bool PotentiallyDanglingMarkup() const {
     return parsed_.potentially_dangling_markup;
   }
+
+  // Returns a GURL with the same properties. This can be used in platform/ and
+  // web/. However, in core/ and modules/, this should only be used to pass
+  // a GURL to a layer that is expecting one instead of a KURL or a WebURL.
+  operator GURL() const;
 
  private:
   void Init(const KURL& base,
