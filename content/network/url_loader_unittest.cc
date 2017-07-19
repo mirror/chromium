fc110d2ecc4077f491040bfc197304144e7bb6f1
John Abd-El-Malek <jam@chromium.org>
2017-07-19 11:02:03 -0700

    Add mime sniffing to the network service.
    This doesn't handle all the cases (e.g. plugins, downloads) but these don't work yet with the network service. However it does fix ~700 new wpt tests that 
    started failing after r487304 since these generated *worker.html files weren't being served with a mime type which was causing their load to fail.
    
    BUG=746144,729849
    
    Change-Id: I232c9af1dca32f1c641736c8ccd61978096559ef

--- content/network/url_loader_unittest.cc
+++ content/network/url_loader_unittest.cc
@@ -113,6 +113,15 @@ class URLLoaderImplTest : public testing::Test {
     CHECK_EQ(data, file_contents);
   }
 
+  std::string LoadAndReturnMimeType(bool sniff, const std::string& path) {
+    TestURLLoaderClient client;
+    GURL url = test_server()->GetURL(path);
+    int32_t options =
+        sniff ? mojom::kURLLoadOptionSniffMimeType : mojom::kURLLoadOptionNone;
+    Load(url, &client, options);
+    return client.response_head().mime_type;
+  }
+
   net::EmbeddedTestServer* test_server() { return &test_server_; }
   NetworkContext* context() { return context_.get(); }
   void DestroyContext() { context_.reset(); }
@@ -189,6 +198,16 @@ TEST_F(URLLoaderImplTest, DestroyContextWithLiveRequest) {
   client.RunUntilConnectionError();
   EXPECT_FALSE(client.has_received_completion());
   EXPECT_EQ(0u, client.download_data_length());
+}
+
+TEST_F(URLLoaderImplTest, MimeSniffNotSpecified) {
+  std::string mime_type = LoadAndReturnMimeType(false, "/nocontent_type.html");
+  ASSERT_TRUE(mime_type.empty());
+}
+
+TEST_F(URLLoaderImplTest, MimeSniffSpecified) {
+  std::string mime_type = LoadAndReturnMimeType(true, "/nocontent_type.html");
+  ASSERT_EQ(std::string("text/html"), mime_type);
 }
 
 }  // namespace content
