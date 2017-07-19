1949b53b393904229779a2b8df1bc0384ce3e60b
Stuart Langley <slangley@chromium.org>
2017-07-19 14:18:33 +1000

    Move LocalFrameClientImpl to core/exported/.
    Bug:
    Change-Id: Id00a4492476ac4695606a93986517c67e08f54c7

--- third_party/WebKit/Source/web/WebFactoryImpl.cpp
+++ third_party/WebKit/Source/web/WebFactoryImpl.cpp
@@ -3,6 +3,12 @@
 // found in the LICENSE file.
 
 #include "web/WebFactoryImpl.h"
+#include "modules/audio_output_devices/HTMLMediaElementAudioOutputDevice.h"
+#include "modules/encryptedmedia/HTMLMediaElementEncryptedMedia.h"
+#include "modules/remoteplayback/HTMLMediaElementRemotePlayback.h"
+#include "modules/remoteplayback/RemotePlayback.h"
+#include "modules/serviceworkers/ServiceWorkerLinkResource.h"
+#include "public/web/WebFrameClient.h"
 #include "web/ChromeClientImpl.h"
 #include "web/WebLocalFrameImpl.h"
 #include "web/WebViewImpl.h"
@@ -38,6 +44,35 @@ WebLocalFrameBase* WebFactoryImpl::CreateWebLocalFrameBase(
     InterfaceRegistry* registry,
     WebFrame* opener) const {
   return WebLocalFrameImpl::Create(type, client, registry, opener);
+}
+
+std::unique_ptr<WebMediaPlayer> WebFactoryImpl::CreateWebMediaPlayer(
+    HTMLMediaElement& html_media_element,
+    const WebMediaPlayerSource& source,
+    WebMediaPlayerClient* client) {
+  WebLocalFrameBase* web_frame =
+      WebLocalFrameBase::FromFrame(html_media_element.GetDocument().GetFrame());
+
+  if (!web_frame || !web_frame->Client())
+    return nullptr;
+
+  HTMLMediaElementEncryptedMedia& encrypted_media =
+      HTMLMediaElementEncryptedMedia::From(html_media_element);
+  WebString sink_id(
+      HTMLMediaElementAudioOutputDevice::sinkId(html_media_element));
+  return WTF::WrapUnique(web_frame->Client()->CreateMediaPlayer(
+      source, client, &encrypted_media,
+      encrypted_media.ContentDecryptionModule(), sink_id));
+}
+
+WebRemotePlaybackClient* WebFactoryImpl::CreateWebRemotePlaybackClient(
+    HTMLMediaElement& html_media_element) {
+  return HTMLMediaElementRemotePlayback::remote(html_media_element);
+}
+
+LinkResource* WebFactoryImpl::CreateServiceWorkerLinkResource(
+    HTMLLinkElement* owner) {
+  return ServiceWorkerLinkResource::Create(owner);
 }
 
 }  // namespace blink
