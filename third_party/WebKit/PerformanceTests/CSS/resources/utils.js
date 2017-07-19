5eefdf7c12c8de90806fa85b7622f735ad382339
Naina Raisinghani <nainar@chromium.org>
2017-07-19 14:25:56 +1000

    Add testcases to PerformanceTests/CSS to test changing style using selectors
    This CL amends the utils file to add a DOM tree generator function.
    
    We then change the style of all elements in the tree and benchmark the
    time spent. This allows us to set up a benchmark for style manipulation
    done using selectors.
    
    Bug: 741970
    Change-Id: Ic95054f5a849519713c60192c245e75dac73d341

--- third_party/WebKit/PerformanceTests/CSS/resources/utils.js
+++ third_party/WebKit/PerformanceTests/CSS/resources/utils.js
@@ -15,3 +15,15 @@ function createDeepDOMTree() {
 function createShallowDOMTree() {
         createDOMTree(document.body, 10, 2);
 }
+
+function createRegularDOMTree() {
+        createDOMTree(document.body, 6, 6);
+}
+
+function applyCSSRule(rule) {
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = rule;
+    document.body.appendChild(css);
+    return css;
+}
