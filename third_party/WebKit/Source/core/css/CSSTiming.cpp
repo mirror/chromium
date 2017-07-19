dcc43e529556ed535b4e22b60c9d59891754aa02
Scott Haseley <shaseley@google.com>
2017-07-18 19:57:18 -0700

    Use swap timestamps for FP, FCP, FCP in TBM, UMA, UKM & web perf API
    Buffer swap timestamps were previously recorded but not used for either web
    perf API or internal TBM, UMA, and UKM-based metrics. This CL uses the swap
    timestamps consistently for all metrics.
    
    As part of this CL, the swap time is now stored and reported for First Text
    Paint and First Image Paint, for both consistency and so that the timestamps
    are properly ordered (i.e. FTP does not come before FP).
    
    This CL also adds 3 new UMAs to evaluate the transition:
    1) PageLoad.Internal.PaintTiming.DidSwap. The buffer swap is not guaranteed to
       occur, in which case we lose the corresponding FP or FCP or etc. This metric
       records how often the swap occurs.
    2) PageLoad.Internal.PaintTiming.SwapTimeDelta. This metric records the
       differnce between the old, renderer-based timestamp and the new swap-based
       timestamp.
    3) PageLoad.Internal.Renderer.FirstMeaningfulPaintDetector.NetworkQuietBeforeSwapPromiseReported.
       This records how often the network 2-quiet timer fired before a FMP swap
       time was reported. In cases where this happens, FMP would have already
       been reported to PaintTiming with a swap time of 0.
    
    NOTE: This change may cause regressions in telemetry perf tests for page loads.
    The swap timestamps occur *after* the renderer-based timestamps they are
    replacing, so any tests using FP, FCP, FMP, FTP, or FIP may appear to have
    regressed.
    
    Bug:738235
    Change-Id: Ie8c7c72b59b8a26c73d0cd34f3ead95a166a9959

--- third_party/WebKit/Source/core/css/CSSTiming.cpp
+++ third_party/WebKit/Source/core/css/CSSTiming.cpp
@@ -21,12 +21,12 @@ CSSTiming& CSSTiming::From(Document& document) {
 }
 
 void CSSTiming::RecordAuthorStyleSheetParseTime(double seconds) {
-  if (!paint_timing_->FirstContentfulPaint())
+  if (!paint_timing_->FirstContentfulPaintSwap())
     parse_time_before_fcp_ += seconds;
 }
 
 void CSSTiming::RecordUpdateDuration(double seconds) {
-  if (!paint_timing_->FirstContentfulPaint())
+  if (!paint_timing_->FirstContentfulPaintSwap())
     update_time_before_fcp_ += seconds;
 }
 
