38e9ba502613a455913cd8018798ea15f42fdb57
Peiyong Lin <lpy@chromium.org>
2017-07-19 10:59:06 -0700

    [GRC] Add MetricsCollector.
    This patch:
    1. implements MetricsCollector to report background frame-level heuristics
    metrics; 2. adds UMA histogram to report audibility for background tabs.
    
    BUG=731270
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I53621e5a09a0c02e0526acfc405c751b62b4188d

--- services/resource_coordinator/coordination_unit/tab_signal_generator_impl.h
+++ services/resource_coordinator/coordination_unit/tab_signal_generator_impl.h
@@ -6,6 +6,7 @@
 #define SERVICES_RESOURCE_COORDINATOR_COORDINATION_UNIT_TAB_SIGNAL_GENERATOR_IMPL_H_
 
 #include "base/macros.h"
+#include "base/time/default_tick_clock.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "mojo/public/cpp/bindings/interface_ptr_set.h"
 #include "services/resource_coordinator/coordination_unit/coordination_unit_graph_observer.h"
@@ -31,6 +32,8 @@ class TabSignalGeneratorImpl : public CoordinationUnitGraphObserver,
 
   // CoordinationUnitGraphObserver implementation.
   bool ShouldObserve(const CoordinationUnitImpl* coordination_unit) override;
+  void OnBeforeCoordinationUnitDestroyed(
+      const CoordinationUnitImpl* coordination_unit) override;
   void OnPropertyChanged(const CoordinationUnitImpl* coordination_unit,
                          const mojom::PropertyType property_type,
                          const base::Value& value) override;
@@ -39,11 +42,45 @@ class TabSignalGeneratorImpl : public CoordinationUnitGraphObserver,
       resource_coordinator::mojom::TabSignalGeneratorRequest request);
 
  private:
+  friend class TabSignalGeneratorTest;
+
+  class MetricsCollector {
+   public:
+    MetricsCollector();
+    ~MetricsCollector();
+    void CoordinationUnitRemoved(CoordinationUnitID);
+    void OnFramePropertyChanged(
+        const FrameCoordinationUnitImpl* coordination_unit,
+        const mojom::PropertyType property_type,
+        const base::Value& value);
+
+   private:
+    friend class TabSignalGeneratorTest;
+
+    struct FrameData {
+      base::TimeTicks last_blurt_time_;
+      base::TimeTicks last_invisible_time_;
+    };
+
+    // Note: |clock_| is always |&default_tick_clock_|, except during unit
+    // testing.
+    base::DefaultTickClock default_tick_clock_;
+    base::TickClock* const clock_;
+    std::map<CoordinationUnitID, FrameData> frame_data_map_;
+
+    DISALLOW_COPY_AND_ASSIGN(MetricsCollector);
+  };
+
   void OnFramePropertyChanged(
       const FrameCoordinationUnitImpl* coordination_unit,
       const mojom::PropertyType property_type,
       const base::Value& value);
 
+  MetricsCollector* metrics_collector_for_testing() {
+    return metrics_collector_.get();
+  }
+
+  std::unique_ptr<MetricsCollector> metrics_collector_;
   mojo::BindingSet<mojom::TabSignalGenerator> bindings_;
   mojo::InterfacePtrSet<mojom::TabSignalObserver> observers_;
   DISALLOW_COPY_AND_ASSIGN(TabSignalGeneratorImpl);
