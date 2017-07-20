b5f67e08a2b9045ab0a33dede33453e79e1ad937
Sergey Ulanov <sergeyu@google.com>
2017-07-20 12:37:19 -0700

    Use readdir() instead of readdir_r() in SimpleIndexFile.
    Fuchsia currently doesn't implement readdir_r(). libc implementations
    on Linux, Android and MacOS allocate dirent struct inside DIR, so
    readdir() is safe as long as DIR is not shared between multiple thread,
    i.e. readdir() is safe to use in SimpleIndexFile.
    Also readdir_r() is deprecated in glibc.
    
    Bug: 731302
    Change-Id: I748a6c39c01600dcea18d19fc8650258297af92b

--- net/disk_cache/simple/simple_index_file_posix.cc
+++ net/disk_cache/simple/simple_index_file_posix.cc
@@ -5,6 +5,7 @@
 #include "net/disk_cache/simple/simple_index_file.h"
 
 #include <dirent.h>
+#include <errno.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -35,11 +36,10 @@ bool SimpleIndexFile::TraverseCacheDirectory(
     PLOG(ERROR) << "opendir " << cache_path.value();
     return false;
   }
-  dirent entry, *result;
-  while (readdir_r(dir.get(), &entry, &result) == 0) {
-    if (!result)
-      return true;  // The traversal completed successfully.
-    const std::string file_name(result->d_name);
+  dirent* entry;
+  // errno must be set 0 before every readdir() call to detect errors.
+  while ((entry = (errno = 0, readdir(dir.get()))) != nullptr) {
+    const std::string file_name(entry->d_name);
     if (file_name == "." || file_name == "..")
       continue;
     const base::FilePath file_path = cache_path.Append(
@@ -53,8 +53,13 @@ bool SimpleIndexFile::TraverseCacheDirectory(
     entry_file_callback.Run(file_path, file_info.last_accessed,
                             file_info.last_modified, file_info.size);
   }
-  PLOG(ERROR) << "readdir_r " << cache_path.value();
-  return false;
+
+  if (errno) {
+    PLOG(ERROR) << "readdir " << cache_path.value();
+    return false;
+  }
+
+  return true;
 }
 
 }  // namespace disk_cache
