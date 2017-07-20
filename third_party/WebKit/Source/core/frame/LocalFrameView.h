ae6915f1f544f75951691aa3dfa8d969c32ae190
Patrick Noland <pnoland@google.com>
2017-07-20 11:44:05 -0700

    Scroll anchor serialization skeleton
    Plumb serialization/restore logic for the scroll anchor selector, offset, and
    simhash.
    Add declarations of SerializeAnchor, RestoreAnchor, and SerializedAnchor
    struct.
    Add dummy implementations of serialize/restore.
    Add a runtime flag.
    
    BUG=734679
    R=skobes@chromium.org,creis@chromium.org
    
    Change-Id: Ic6f1cec3a0a8cbc36f7f7fd3c2f5c5e72e723ee7

--- third_party/WebKit/Source/core/frame/LocalFrameView.h
+++ third_party/WebKit/Source/core/frame/LocalFrameView.h
@@ -275,6 +275,7 @@ class CORE_EXPORT LocalFrameView final
   void DidAttachDocument();
 
   void RestoreScrollbar();
+  bool RestoreScrollAnchor(const ScrollAnchor::SerializedAnchor);
 
   void PostLayoutTimerFired(TimerBase*);
 
