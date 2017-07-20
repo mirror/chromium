62052813cb5f96114b5c3b45caa72db163e14fdc
Derek Cheng <imcheng@chromium.org>
2017-07-20 16:16:21 -0700

    [Media Router] Misc cleanup in media_router_bindings.js
    Remove some backward compatibility logic that were put in more than a
    few milestones ago.
    
    Change-Id: I34c1959118b7c8fef8ae4e74df465f14af4db079

--- chrome/renderer/resources/extensions/media_router_bindings.js
+++ chrome/renderer/resources/extensions/media_router_bindings.js
@@ -89,9 +89,9 @@ define('media_router_bindings', [
       'custom_controller_path': route.customControllerPath,
       // Begin newly added properties, followed by the milestone they were
       // added.  The guard should be safe to remove N+2 milestones later.
-      'for_display': route.forDisplay,                               // M47
-      'is_incognito': !!route.offTheRecord,                          // M50
-      'is_offscreen_presentation': !!route.isOffscreenPresentation,  // M56
+      'for_display': route.forDisplay,
+      'is_incognito': route.offTheRecord,
+      'is_offscreen_presentation': route.isOffscreenPresentation,
       'supports_media_route_controller':
           !!route.supportsMediaRouteController  // M61
     });
@@ -617,10 +617,6 @@ define('media_router_bindings', [
    * @param {!MediaRouterHandlers} handlers
    */
   MediaRouteProvider.prototype.setHandlers = function(handlers) {
-    // TODO(mfoltz): Remove when component that supports this method is
-    // rolled out to all Chrome channels in M56.
-    if (!handlers['onBeforeInvokeHandler'])
-      handlers['onBeforeInvokeHandler'] = () => {};
     this.handlers_ = handlers;
     var requiredHandlers = [
       'stopObservingMediaRoutes',
