878698ca7610f88deac6a8618df31add66fe3365
Tina Quach <quacht@google.com>
2017-07-20 10:25:27 -0700

    Add basic accessibility tests for each settings route.
    There are many (52) routes to test within Chrome Settings, and the accessibility test
    framework should test every route or visitable page. Here, we test all routes except 
    those that have been discovered to fail/timeout/be flaky.
    
    Bug:737739
    Change-Id: If127a98861ebf64fe9d9cd8cd07848b30aaebe3e

--- chrome/test/data/webui/settings/accessibility_browsertest.js
+++ chrome/test/data/webui/settings/accessibility_browsertest.js
@@ -55,7 +55,7 @@ SettingsAccessibilityTest.prototype = {
   extraLibraries: PolymerTest.getLibraries(ROOT_PATH).concat([
       'ensure_lazy_loaded.js',
       'passwords_and_autofill_fake_data.js',
-      'passwords_a11y_test.js'
+      'passwords_accessibility_test.js'
   ]),
 
   // TODO(hcarmona): Remove once ADT is not longer in the testing infrastructure
@@ -64,10 +64,106 @@ SettingsAccessibilityTest.prototype = {
   setUp: function() {
     PolymerTest.prototype.setUp.call(this);
     settings.ensureLazyLoaded();
+
+    /**
+     * Define a basic Mocha suite for testing a route given a specific path for
+     * accessibility.
+     *
+     * @param {!string} name Name of the suite.
+     * @param {!string} path Path associated with the route
+     */
+    var defineSettingsAccessibilityTest = function(name, path) {
+      //TODO(hcarmona): Remove duplicate iron-icons so that they do not violate
+      // the duplicate ids audit rule.
+      var auditOptions = {
+        context: {
+          exclude: ['iron-iconset-svg']
+        }
+      };
+
+      suite(name, function() {
+        setup(function() {
+          settings.navigateTo(settings.routes[name]);
+          Polymer.dom.flush();
+        });
+
+        test('AccessibleWithNoChanges', function() {
+          return SettingsAccessibilityTest.runAudit(auditOptions);
+        });
+      });
+    };
+
+    // Define test for all suites
+    Object.keys(settings.routes).forEach(function(route) {
+      defineSettingsAccessibilityTest(route, settings.routes[route]);
+    });
   },
 };
 
-TEST_F('SettingsAccessibilityTest', 'All', function() {
-  mocha.run();
+// Run each mocha test in isolation (within a new TEST_F() call). ADVANCED route
+// excluded because it only serves as a parent of subpages, and should not be
+// possible to navigate to it directly.
+// TODO(quacht): Add SIGN_OUT route once test is written.
+// TODO(quacht): Add SITE_SETTINGS_DATA_DETAILS route once test is written.
+var routes = [
+  'BASIC',
+  'ABOUT',
+  'IMPORT_DATA',
+  'APPEARANCE',
+  'FONTS',
+  'DEFAULT_BROWSER',
+  'SEARCH',
+  'SEARCH_ENGINES',
+  'ON_STARTUP',
+  'STARTUP_URLS',
+  'PEOPLE',
+  'SYNC',
+  // TODO(quacht): Enable once Polymer bug involving malformed ariadescribedby
+  // is resolved.
+  // 'MANAGE_PROFILE',,
+  'CLEAR_BROWSER_DATA',
+  'PRIVACY',
+  'CERTIFICATES',
+  // TODO(quacht): Enable SITE_SETTINGS after addressing how the RegExp looks
+  // for all test suites whose name contains the expression--make it run all
+  // SITE_SETTINGS_* mocha suites and thus become flaky (timeout).
+  'SITE_SETTINGS_HANDLERS',
+  'SITE_SETTINGS_ADS',
+  'SITE_SETTINGS_AUTOMATIC_DOWNLOADS',
+  'SITE_SETTINGS_BACKGROUND_SYNC',
+  'SITE_SETTINGS_CAMERA',
+  'SITE_SETTINGS_COOKIES',
+  'SITE_SETTINGS_IMAGES',
+  'SITE_SETTINGS_JAVASCRIPT',
+  'SITE_SETTINGS_LOCATION',
+  'SITE_SETTINGS_MICROPHONE',
+  'SITE_SETTINGS_NOTIFICATIONS',
+  'SITE_SETTINGS_FLASH',
+  'SITE_SETTINGS_POPUPS',
+  'SITE_SETTINGS_UNSANDBOXED_PLUGINS',
+  'SITE_SETTINGS_MIDI_DEVICES',
+  'SITE_SETTINGS_USB_DEVICES',
+  'SITE_SETTINGS_ZOOM_LEVELS',
+  'SITE_SETTINGS_PDF_DOCUMENTS',
+  'SITE_SETTINGS_PROTECTED_CONTENT',
+  'PASSWORDS',
+  'AUTOFILL',
+  'MANAGE_PASSWORDS',
+  'LANGUAGES',
+  // TODO(quacht): Enable once Polymer bug involving malformed ariadescribedby
+  // is resolved.
+  // 'EDIT_DICTIONARY',
+  'DOWNLOADS',
+  'PRINTING',
+  'CLOUD_PRINTERS',
+  'ACCESSIBILITY',
+  // TODO(quacht): Enable once this test no longer times out.
+  // 'SYSTEM',
+  'RESET',
+  'RESET_DIALOG',
+  'TRIGGERED_RESET_DIALOG'
+ ].forEach(function(route) {
+  TEST_F('SettingsAccessibilityTest', route, function() {
+    mocha.grep(route).run();
+  });
 });
-
