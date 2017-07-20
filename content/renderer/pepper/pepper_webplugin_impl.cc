a061b78f512520a9b3092200c74a8c278e71863a
Diana Gage <drgage@google.com>
2017-07-20 12:21:02 -0700

    Add CanCut() method to check if plugin text can be cut.
    CanCut() can be utilized by the web plugin container to ask plugins if
    they can cut text. With respect to the PDF plugin, this method returns
    TRUE if focus is within a user-editable form text area and returns
    FALSE otherwise.
    
    BUG=59266
    
    Change-Id: Iefcd8314f231c7fad5d555c9981d7b38ce064441

--- content/renderer/pepper/pepper_webplugin_impl.cc
+++ content/renderer/pepper/pepper_webplugin_impl.cc
@@ -285,6 +285,12 @@ WebString PepperWebPluginImpl::SelectionAsMarkup() const {
   return WebString::FromUTF16(instance_->GetSelectedText(true));
 }
 
+bool PepperWebPluginImpl::CanCut() const {
+  if (!instance_)
+    return false;
+  return instance_->CanCut();
+}
+
 WebURL PepperWebPluginImpl::LinkAtPosition(const WebPoint& position) const {
   // Re-entrancy may cause JS to try to execute script on the plugin before it
   // is fully initialized. See: crbug.com/715747.
