137e667ece869a5638acb589c1d825b791033a77
Siddhartha <ssid@chromium.org>
2017-07-20 16:03:27 -0700

    Fix URLRequestQuicPerfTest to actually trigger memory dump
    The test was broken because enabling tracing no longer enables
    MemoryDumpManager. Directly use memory tracing api for checking the dump
    values instead of trace file analyzer.
    
    BUG=746124
    
    Change-Id: I950a1e43a63b6424afd67f0c6d30e0c50722fe24

--- base/trace_event/memory_dump_manager_test_utils.h
+++ base/trace_event/memory_dump_manager_test_utils.h
@@ -8,6 +8,8 @@
 #include "base/bind.h"
 #include "base/trace_event/memory_dump_manager.h"
 #include "base/trace_event/memory_dump_request_args.h"
+#include "base/trace_event/trace_event_argument.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
 namespace trace_event {
@@ -26,6 +28,29 @@ void InitializeMemoryDumpManagerForInProcessTesting(bool is_coordinator) {
   instance->set_dumper_registrations_ignored_for_testing(true);
   instance->Initialize(BindRepeating(&RequestGlobalDumpForInProcessTesting),
                        is_coordinator);
+}
+
+// Check if the MemoryAllocatorDump contains an attribute with the expected
+// name, type and units. The value of attribute is returned.
+std::unique_ptr<Value> CheckExpectedAttributeInDump(
+    const MemoryAllocatorDump* dump,
+    const std::string& name,
+    const char* expected_type,
+    const char* expected_units) {
+  std::unique_ptr<Value> raw_attrs =
+      dump->attributes_for_testing()->ToBaseValue();
+  DictionaryValue* args = nullptr;
+  DictionaryValue* arg = nullptr;
+  std::string arg_value;
+  const Value* out_value = nullptr;
+  EXPECT_TRUE(raw_attrs->GetAsDictionary(&args));
+  EXPECT_TRUE(args->GetDictionary(name, &arg));
+  EXPECT_TRUE(arg->GetString("type", &arg_value));
+  EXPECT_EQ(expected_type, arg_value);
+  EXPECT_TRUE(arg->GetString("units", &arg_value));
+  EXPECT_EQ(expected_units, arg_value);
+  EXPECT_TRUE(arg->Get("value", &out_value));
+  return out_value ? out_value->CreateDeepCopy() : std::unique_ptr<Value>();
 }
 
 }  // namespace trace_event
