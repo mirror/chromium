71b9c17b731b985fe5b757dd87a0985c7726f889
Boris Vidolov <borisv@chromium.org>
2017-07-20 15:22:52 -0700

    Mac support in UnixDomainSocket.
    Change-Id: Iaf58f0894e02d9b9b360fde2ad597eaf23fbc668

--- chrome/common/profiling/memlog_sender_pipe_posix.cc
+++ chrome/common/profiling/memlog_sender_pipe_posix.cc
@@ -7,7 +7,7 @@
 #include <unistd.h>
 
 #include "base/logging.h"
-#include "base/posix/unix_domain_socket_linux.h"
+#include "base/posix/unix_domain_socket_posix.h"
 #include "base/strings/string_number_conversions.h"
 #include "chrome/common/profiling/memlog_stream.h"
 
