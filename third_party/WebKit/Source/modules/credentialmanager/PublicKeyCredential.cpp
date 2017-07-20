4381c7dd6d2cba235d032351763a8100d8b6b40e
Kim Paulhamus <kpaulhamus@chromium.org>
2017-07-19 17:52:14 -0700

    Migrate webauthn to the credentialmanager module.
    This change will freeze the spec at WD05 for interop and will also update the external web platform tests.
    Spec WD05: https://www.w3.org/TR/2017/WD-webauthn-20170505/
    
    Bug:733029
    Change-Id: I4fcd01295f640c7500b815232c8f6cec5b3a2409

--- third_party/WebKit/Source/modules/credentialmanager/PublicKeyCredential.cpp
+++ third_party/WebKit/Source/modules/credentialmanager/PublicKeyCredential.cpp
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "modules/webauth/PublicKeyCredential.h"
+#include "modules/credentialmanager/PublicKeyCredential.h"
 
 namespace blink {
 
