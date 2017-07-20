737b00fc7789e59156c2183ccccbdd33561267a6
Matt Falkenhagen <falken@chromium.org>
2017-07-20 15:19:32 +0900

    service worker: Add Google Docs to targeted UMA for worker preparation type
    It'd be useful to know warm vs cold start ratio.
    
    Change-Id: Ic25b5ed997e2f5e81e8c18015ed71105d584b120

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -3093,6 +3093,9 @@ std::string ChromeContentBrowserClient::GetMetricSuffixForURL(const GURL& url) {
   if (page_load_metrics::IsGoogleSearchResultUrl(url)) {
     return "search";
   }
+  if (url.host() == "docs.google.com") {
+    return "docs";
+  }
   return std::string();
 }
 
