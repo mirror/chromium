66ec4fbb440985d6be8c6a88a0c94063bba061f4
Catherine Chung <catherinechung@google.com>
2017-07-20 13:45:00 -0700

    Notify the NewTabFeatureEngagementTracker of tab creation and omnibox navigations
    This CL uses the NewTabFeatureEngagementTracker, which you can view
    in this CL: http://crrev.com/7d54c1b52c11ef28e9a19f3c6a5a7a41a25fbf5d
    
    The New Tab FeatureEngagementTracker will be notified if:
    * The user presses the New Tab button
    * The user presses Control+T
    * The user uses the tabstrip context menu, window frame context menu,
      or the app menu to open a New tab.
    * The user uses the omnibox to start a session in the same tab.
    * The user focuses on the omnibox.
    
    Bug: 734132
    Change-Id: I5c9f3c394a2f2001b812a18e788fdc68220dcd20

--- chrome/browser/ui/tabs/tab_strip_model.cc
+++ chrome/browser/ui/tabs/tab_strip_model.cc
@@ -17,6 +17,8 @@
 #include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/defaults.h"
 #include "chrome/browser/extensions/tab_helper.h"
+#include "chrome/browser/feature_engagement_tracker/new_tab/new_tab_tracker.h"
+#include "chrome/browser/feature_engagement_tracker/new_tab/new_tab_tracker_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper_delegate.h"
@@ -906,6 +908,10 @@ void TabStripModel::ExecuteContextMenuCommand(
       UMA_HISTOGRAM_ENUMERATION("Tab.NewTab",
                                 TabStripModel::NEW_TAB_CONTEXT_MENU,
                                 TabStripModel::NEW_TAB_ENUM_COUNT);
+      feature_engagement_tracker::NewTabTrackerFactory::GetInstance()
+          ->GetForProfile(profile_)
+          ->OnNewTabOpened();
+
       delegate()->AddTabAt(GURL(), context_index + 1, true);
       break;
 
