3a06aa6f8823db38bbda146129c5e5edacf5c5be
Tarun Bansal <tbansal@chromium.org>
2017-07-20 12:57:09 -0700

    NQE: Discard transport RTT observations from private sockets.
    RTT from sockets that are expected to connect to private addresses
    is not notified back to the network quality estimator.
    
    Bug:696142
    Change-Id: Ie82765adbdbfcad0d9d33a9e07713b4724a32b97
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester

--- net/nqe/socket_watcher_factory.cc
+++ net/nqe/socket_watcher_factory.cc
@@ -17,10 +17,12 @@ namespace internal {
 SocketWatcherFactory::SocketWatcherFactory(
     scoped_refptr<base::SingleThreadTaskRunner> task_runner,
     base::TimeDelta min_notification_interval,
+    bool allow_rtt_private_address,
     OnUpdatedRTTAvailableCallback updated_rtt_observation_callback,
     base::TickClock* tick_clock)
     : task_runner_(std::move(task_runner)),
       min_notification_interval_(min_notification_interval),
+      allow_rtt_private_address_(allow_rtt_private_address),
       updated_rtt_observation_callback_(updated_rtt_observation_callback),
       tick_clock_(tick_clock) {
   DCHECK(tick_clock_);
@@ -29,9 +31,12 @@ SocketWatcherFactory::SocketWatcherFactory(
 SocketWatcherFactory::~SocketWatcherFactory() {}
 
 std::unique_ptr<SocketPerformanceWatcher>
-SocketWatcherFactory::CreateSocketPerformanceWatcher(const Protocol protocol) {
+SocketWatcherFactory::CreateSocketPerformanceWatcher(
+    const Protocol protocol,
+    const AddressList& address_list) {
   return base::MakeUnique<SocketWatcher>(
-      protocol, min_notification_interval_, task_runner_,
+      protocol, address_list, min_notification_interval_,
+      allow_rtt_private_address_, task_runner_,
       updated_rtt_observation_callback_, tick_clock_);
 }
 
