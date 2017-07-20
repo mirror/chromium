5a951649665f67a529b1c165b39c3269b0d0184d
Alex Newcomer <newcomer@chromium.org>
2017-07-20 11:31:20 -0700

    Virtual Keyboard handling in the new launcher.
    Bug:740669
    Change-Id: I25523b905ced2b5267f08bae56622aac6f8abc18

--- ui/app_list/views/search_box_view.cc
+++ ui/app_list/views/search_box_view.cc
@@ -31,6 +31,8 @@
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/shadow_value.h"
+#include "ui/keyboard/keyboard_controller.h"
+#include "ui/keyboard/keyboard_util.h"
 #include "ui/strings/grit/ui_strings.h"
 #include "ui/views/background.h"
 #include "ui/views/border.h"
@@ -145,14 +147,16 @@ class SearchBoxImageButton : public views::ImageButton {
 
 SearchBoxView::SearchBoxView(SearchBoxViewDelegate* delegate,
                              AppListViewDelegate* view_delegate,
-                             AppListView* app_list_view)
+                             AppListView* app_list_view,
+                             bool is_tablet_mode)
     : delegate_(delegate),
       view_delegate_(view_delegate),
       content_container_(new views::View),
       search_box_(new views::Textfield),
       app_list_view_(app_list_view),
       focused_view_(FOCUS_SEARCH_BOX),
-      is_fullscreen_app_list_enabled_(features::IsFullscreenAppListEnabled()) {
+      is_fullscreen_app_list_enabled_(features::IsFullscreenAppListEnabled()),
+      is_tablet_mode(is_tablet_mode_) {
   SetLayoutManager(new views::FillLayout);
   SetPreferredSize(gfx::Size(is_fullscreen_app_list_enabled_
                                  ? kPreferredWidthFullscreen
@@ -395,6 +399,19 @@ void SearchBoxView::SetSearchBoxActive(bool active) {
   search_box_->SetCursorEnabled(active);
   search_box_->SchedulePaint();
 
+  if (is_tablet_mode_) {
+    keyboard::KeyboardController* const keyboard_controller =
+        keyboard::KeyboardController::GetInstance();
+    if (!keyboard_controller || active == keyboard::IsKeyboardVisible())
+      return;
+
+    if (active)
+      keyboard_controller->ShowKeyboard(false);
+    else
+      keyboard_controller->HideKeyboard(
+          keyboard::KeyboardController::HIDE_REASON_MANUAL);
+  }
+
   if (speech_button_)
     speech_button_->SetVisible(!active);
   close_button_->SetVisible(active);
@@ -505,6 +522,13 @@ void SearchBoxView::ButtonPressed(views::Button* sender,
   } else {
     NOTREACHED();
   }
+}
+
+void SearchBoxView::OnTabletModeChanged(bool started) {
+  if (!is_fullscreen_app_list_enabled_)
+    return;
+
+  is_tablet_mode_ = started;
 }
 
 void SearchBoxView::UpdateModel() {
