1f7d9cef4e8e25f6d8d78cd4ab082befe5f5af88
Wenzhao Zang <wzang@chromium.org>
2017-07-20 15:24:28 -0700

    Add wallpaper color caching
    A wallpaper goes through at least the following stages before being
    set:
    
    |WallpaperManager|  (Keys e.g. account ID and file path are available)
            |
    |WallpaperController|   (Only image is passed, not unique ID)
            |
    |WallpaperResizer|        (Image itself is changed too)
            |
    |WallpaperCalculator|     (Nothing stays the same)
            |
    Back to |WallpaperController|
    
    For now we can assign the |WallpaperInfo| to |WallpaperController|
    for caching purpose. This method is not optimal but I believe other
    available options are more problematic, e.g.,
    
    Backup 1: Move calculation to |WallpaperManager|, but:
    1) |WallpaperView| uses the color to apply dimming when painting the
    wallpaper and it interacts closely with |WallpaperController|. If the
    calculation is moved to |WallpaperManager|, there is no way for
    |WallpaperView| to check the calculation progress.
    
    2) Wallpaper resizing 'may' influence the color calculation so we'd
    better wait until resizing is done to start color calculation.
    
    3) Moving codes to |WallpaperManager| may involve setting a timer, but
    it's hard to find a good choice of the upper limit. Currently color
    calculation is only allowed during an active session, and in theory
    we don't need to set a limit on the calculation time.
    
    4) |WallpaperManager| still needs to rely on |WallpaperController| to
    notify observers under /ash.
    
    Backup 2: Pass the ID along the path till the calculation stage, but it
    requires changing lots of APIs.
    
    Backup 3: Add an inherent attribute to the image class |ImageSkia|,
    but it's not doable under the current implementation.
    
    In addition, although Account ID and file path can both be used as key,
    since we need to initiate a calculation attempt whenever the session
    state becomes ACTIVE, only file path can distinguish between new and
    and old wallpaper.
    
    Bug:733409
    Change-Id: I55f27ee6fccddf74eafad559438dee241abb1522

--- ash/mus/wallpaper_delegate_mus.cc
+++ ash/mus/wallpaper_delegate_mus.cc
@@ -4,7 +4,7 @@
 
 #include "ash/mus/wallpaper_delegate_mus.h"
 
-#include "components/wallpaper/wallpaper_layout.h"
+#include "components/wallpaper/wallpaper_info.h"
 #include "ui/wm/core/window_animations.h"
 
 namespace ash {
