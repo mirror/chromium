46d371ca26854ac647f3d5053b3aa8626206cb79
Aaron Leventhal <aleventhal@chromium.org>
2017-07-20 09:22:39 -0400

    Remove dead code
    Bug: None
    Change-Id: I5512f6d9deab45655218557efc312adee40ee594

--- third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
@@ -2504,12 +2504,4 @@ void AXLayoutObject::AddRemoteSVGChildren() {
   }
 }
 
-bool AXLayoutObject::ElementAttributeValue(
-    const QualifiedName& attribute_name) const {
-  if (!layout_object_)
-    return false;
-
-  return EqualIgnoringASCIICase(GetAttribute(attribute_name), "true");
-}
-
 }  // namespace blink
