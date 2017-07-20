a75e87331c9f87a06af4e11759dda625486926f3
Iulia Harasim <iuliah@google.com>
2017-07-20 11:57:51 +0200

    [Android] Add SigninPromoMediator and wire promo in Settings page
    This CL adds SinginPromoMediator which is used to configure the signin
    promo. This class is then used in SigninPreference, making the promo
    visibile in the Settings page.
    
    Bug: 737743
    Change-Id: I7f5587b32a377db603cf536f8cb1cd9ba8851fdb

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/SignInPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/SignInPreference.java
@@ -4,6 +4,7 @@
 
 package org.chromium.chrome.browser.preferences;
 
+import android.accounts.Account;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.preference.Preference;
@@ -15,6 +16,7 @@ import android.view.View;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.firstrun.FirstRunSignInProcessor;
+import org.chromium.chrome.browser.firstrun.ProfileDataCache;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileDownloader;
 import org.chromium.chrome.browser.signin.AccountManagementFragment;
@@ -22,9 +24,12 @@ import org.chromium.chrome.browser.signin.AccountSigninActivity;
 import org.chromium.chrome.browser.signin.SigninAccessPoint;
 import org.chromium.chrome.browser.signin.SigninManager;
 import org.chromium.chrome.browser.signin.SigninManager.SignInAllowedObserver;
+import org.chromium.chrome.browser.signin.SigninPromoMediator;
+import org.chromium.chrome.browser.signin.SigninPromoView;
 import org.chromium.chrome.browser.sync.ProfileSyncService;
 import org.chromium.chrome.browser.sync.ProfileSyncService.SyncStateChangedListener;
 import org.chromium.chrome.browser.util.ViewUtils;
+import org.chromium.components.signin.AccountManagerHelper;
 import org.chromium.components.signin.ChromeSigninController;
 import org.chromium.components.sync.AndroidSyncSettings;
 
@@ -36,8 +41,9 @@ import org.chromium.components.sync.AndroidSyncSettings;
 public class SignInPreference extends Preference
         implements SignInAllowedObserver, ProfileDownloader.Observer,
                    AndroidSyncSettings.AndroidSyncSettingsObserver, SyncStateChangedListener {
-    private boolean mViewEnabled;
     private boolean mShowingPromo;
+    private boolean mViewEnabled;
+    private SigninPromoMediator mSigninPromoMediator;
 
     /**
      * Constructor for inflating from XML.
@@ -86,11 +92,10 @@ public class SignInPreference extends Preference
             setupSigninDisabled();
             mShowingPromo = false;
         } else if (accountName == null) {
-            setupNotSignedIn();
-
-            if (!mShowingPromo) {
-                // This user action should be recorded when message with sign-in prompt is shown
-                RecordUserAction.record("Signin_Impression_FromSettings");
+            if (SigninPromoMediator.shouldShowPromo()) {
+                setupNewPromo();
+            } else {
+                setupOldPromo();
             }
             mShowingPromo = true;
         } else {
@@ -108,24 +113,56 @@ public class SignInPreference extends Preference
     }
 
     private void setupSigninDisabled() {
+        setLayoutResource(R.layout.account_management_account_row);
         setTitle(R.string.sign_in_to_chrome);
         setSummary(R.string.sign_in_to_chrome_disabled_summary);
         setFragment(null);
         setIcon(ManagedPreferencesUtils.getManagedByEnterpriseIconId());
         setWidgetLayoutResource(0);
         setViewEnabled(false);
+        mSigninPromoMediator = null;
     }
 
-    private void setupNotSignedIn() {
+    private void setupNewPromo() {
+        setLayoutResource(R.layout.custom_preference);
+        setTitle("");
+        setSummary("");
+        setFragment(null);
+        setIcon(null);
+        setWidgetLayoutResource(R.layout.signin_promo_view);
+        setViewEnabled(true);
+
+        if (mSigninPromoMediator == null) {
+            ProfileDataCache profileDataCache =
+                    new ProfileDataCache(getContext(), Profile.getLastUsedProfile());
+            mSigninPromoMediator =
+                    new SigninPromoMediator(profileDataCache, SigninAccessPoint.SETTINGS);
+            SigninPromoMediator.recordSigninPromoImpression();
+            Account[] accounts = AccountManagerHelper.get().tryGetGoogleAccounts();
+            String defaultAccountName = accounts.length == 0 ? null : accounts[0].name;
+            mSigninPromoMediator.setAccountName(defaultAccountName);
+        }
+
+        notifyChanged();
+    }
+
+    private void setupOldPromo() {
+        setLayoutResource(R.layout.account_management_account_row);
         setTitle(R.string.sign_in_to_chrome);
         setSummary(R.string.sign_in_to_chrome_summary);
         setFragment(null);
         setIcon(AppCompatResources.getDrawable(getContext(), R.drawable.logo_avatar_anonymous));
         setWidgetLayoutResource(0);
         setViewEnabled(true);
+        mSigninPromoMediator = null;
+
+        if (!mShowingPromo) {
+            RecordUserAction.record("Signin_Impression_FromSettings");
+        }
     }
 
     private void setupSignedIn(String accountName) {
+        setLayoutResource(R.layout.account_management_account_row);
         String title = AccountManagementFragment.getCachedUserName(accountName);
         if (title == null) {
             Profile profile = Profile.getLastUsedProfile();
@@ -141,11 +178,10 @@ public class SignInPreference extends Preference
         setSummary(SyncPreference.getSyncStatusSummary(getContext()));
         setFragment(AccountManagementFragment.class.getName());
         setIcon(AccountManagementFragment.getUserPicture(getContext(), accountName));
-
         setWidgetLayoutResource(
                 SyncPreference.showSyncErrorIcon(getContext()) ? R.layout.sync_error_widget : 0);
-
         setViewEnabled(true);
+        mSigninPromoMediator = null;
     }
 
     // This just changes visual representation. Actual enabled flag in preference stays
@@ -159,9 +195,13 @@ public class SignInPreference extends Preference
     }
 
     @Override
-    protected void onBindView(View view) {
+    protected void onBindView(final View view) {
         super.onBindView(view);
         ViewUtils.setEnabledRecursive(view, mViewEnabled);
+        if (mSigninPromoMediator != null) {
+            SigninPromoView signinPromoView = view.findViewById(R.id.signin_promo_view_container);
+            mSigninPromoMediator.setupSigninPromoView(getContext(), signinPromoView, null);
+        }
     }
 
     // ProfileSyncServiceListener implementation:
---
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninPromoMediator.java
@@ -0,0 +1,173 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.signin;
+
+import android.annotation.DimenRes;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.chromium.base.ContextUtils;
+import org.chromium.base.metrics.RecordUserAction;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeFeatureList;
+import org.chromium.chrome.browser.firstrun.ProfileDataCache;
+
+import java.util.Collections;
+
+import javax.annotation.Nullable;
+
+/**
+ * A mediator for configuring the sign in promo. It sets up the sign in promo depending on the
+ * context: whether there are any Google accounts on the device which have been previously signed in
+ * or not.
+ */
+public class SigninPromoMediator {
+    /**
+     * Receives notifications when user clicks close button in the promo.
+     */
+    public interface OnDismissListener { void onDismiss(); }
+
+    private static final String SIGNIN_PROMO_IMPRESSIONS_COUNT = "signin_promo_impressions_count";
+    private static final int MAX_IMPRESSIONS = 20;
+
+    /**
+     * Determines whether the promo should be shown to the user or not.
+     * @return true if the promo is to be shown and false otherwise.
+     */
+    public static boolean shouldShowPromo() {
+        if (!ChromeFeatureList.isEnabled(ChromeFeatureList.ANDROID_SIGNIN_PROMOS)) {
+            return false;
+        }
+
+        int noOfImpressions =
+                ContextUtils.getAppSharedPreferences().getInt(SIGNIN_PROMO_IMPRESSIONS_COUNT, 0);
+        return noOfImpressions < MAX_IMPRESSIONS;
+    }
+
+    /**
+     * Records an impression for the promo.
+     */
+    public static void recordSigninPromoImpression() {
+        SharedPreferences preferences = ContextUtils.getAppSharedPreferences();
+        int noOfImpressions = preferences.getInt(SIGNIN_PROMO_IMPRESSIONS_COUNT, 0);
+        preferences.edit().putInt(SIGNIN_PROMO_IMPRESSIONS_COUNT, noOfImpressions + 1).apply();
+        RecordUserAction.record("Signin_Impression_FromSettings");
+        // TODO(iuliah): Add Signin_Impression_WithAccounts recording
+    }
+
+    private String mAccountName;
+    private final ProfileDataCache mProfileDataCache;
+    private final @AccountSigninActivity.AccessPoint int mAccessPoint;
+
+    /**
+     * Creates a new SigninPromoMediator.
+     * @param profileDataCache  The profile data cache for the latest used account on the device.
+     * @param accessPoint       Specifies the AccessPoint from which the promo is to be shown.
+     */
+    public SigninPromoMediator(
+            ProfileDataCache profileDataCache, @AccountSigninActivity.AccessPoint int accessPoint) {
+        mProfileDataCache = profileDataCache;
+        mAccessPoint = accessPoint;
+    }
+
+    /**
+     * Configures the signin promo view.
+     * @param view              The view in which the promo will be added.
+     * @param onDismissListener Listener which handles the action of dismissing the promo. A null
+     *         onDismissListener marks that the promo is not dismissible and as a result the close
+     *         button is hidden.
+     */
+    public void setupSigninPromoView(Context context, SigninPromoView view,
+            @Nullable final OnDismissListener onDismissListener) {
+        if (mAccountName == null) {
+            setupColdState(context, view);
+        } else {
+            setupHotState(context, view);
+        }
+
+        if (onDismissListener != null) {
+            view.getDismissButton().setVisibility(View.VISIBLE);
+            view.getDismissButton().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onDismissListener.onDismiss();
+                }
+            });
+        } else {
+            view.getDismissButton().setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * Sets the the default account found on the device.
+     * @param accountName       The name of the default account found on the device. Can be null if
+     *         there are no accounts signed in on the device.
+     */
+    public void setAccountName(@Nullable String accountName) {
+        mAccountName = accountName;
+    }
+
+    private void setupColdState(final Context context, SigninPromoView view) {
+        Drawable chromeIcon = context.getResources().getDrawable(R.drawable.chrome_sync_logo);
+        view.getImage().setImageDrawable(chromeIcon);
+        setImageSize(context, view, R.dimen.signin_promo_cold_state_image_size);
+
+        String signinText = context.getString(R.string.sign_in_to_chrome);
+        view.getSigninButton().setText(signinText);
+        view.getSigninButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AccountSigninActivity.startAccountSigninActivity(context, mAccessPoint);
+            }
+        });
+
+        view.getChooseAccountButton().setVisibility(View.GONE);
+    }
+
+    private void setupHotState(final Context context, SigninPromoView view) {
+        mProfileDataCache.update(Collections.singletonList(mAccountName));
+
+        Drawable accountImage = mProfileDataCache.getImage(mAccountName);
+        view.getImage().setImageDrawable(accountImage);
+        setImageSize(context, view, R.dimen.signin_promo_account_image_size);
+
+        String accountTitle = getAccountTitle();
+        String signinButtonText =
+                context.getString(R.string.signin_promo_continue_as, accountTitle);
+        view.getSigninButton().setText(signinButtonText);
+        view.getSigninButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AccountSigninActivity.startAccountSigninActivity(context, mAccessPoint);
+            }
+        });
+
+        String chooseAccountButtonText =
+                context.getString(R.string.signin_promo_choose_account, accountName);
+        view.getChooseAccountButton().setText(chooseAccountButtonText);
+        view.getChooseAccountButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AccountSigninActivity.startAccountSigninActivity(context, mAccessPoint);
+            }
+        });
+        view.getChooseAccountButton().setVisibility(View.VISIBLE);
+    }
+
+    private void setImageSize(Context context, SigninPromoView view, @DimenRes int dimenResId) {
+        ViewGroup.LayoutParams layoutParams = view.getImage().getLayoutParams();
+        layoutParams.height = context.getResources().getDimensionPixelOffset(dimenResId);
+        layoutParams.width = context.getResources().getDimensionPixelOffset(dimenResId);
+        view.getImage().setLayoutParams(layoutParams);
+    }
+
+    private String getAccountTitle() {
+        String title = mProfileDataCache.getFullName(mAccountName);
+        return title == null ? mAccountName : title;
+    }
+}
