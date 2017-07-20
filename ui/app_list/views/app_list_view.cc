81579c3313a95856b1e3880978117be852e2a380
Maajid <maajid@chromium.org>
2017-07-20 11:38:01 -0700

    Fix state transition bugs when pressing the escape key for the fullscreen app list. After this change, if escape is pressed, the following state changes occur: PEEKING->CLOSED HALF->PEEKING FULLSCREEN_ALL_APPS->CLOSED FULLSCREEN_SEARCH->FULLSCREEN_ALL_APPS
    Bug: 740662
    Test: Unit tested, device tested with --enable-features=EnableFullscreenAppList
    Change-Id: Id0820a9f07bf362fddc6e1adb3ef5648b3d2070d

--- ui/app_list/views/app_list_view.cc
+++ ui/app_list/views/app_list_view.cc
@@ -505,7 +505,6 @@ void AppListView::HandleClickOrTap() {
     case HALF:
     case FULLSCREEN_SEARCH:
       search_box_view_->ClearSearch();
-      SetState(app_list_state_ == HALF ? PEEKING : FULLSCREEN_ALL_APPS);
       break;
     case PEEKING:
     case FULLSCREEN_ALL_APPS:
@@ -882,10 +881,15 @@ void AppListView::SetState(AppListState new_state) {
   if (is_side_shelf_ || is_tablet_mode_) {
     // If side shelf or tablet mode are active, all transitions should be
     // made to the tablet mode/side shelf friendly versions.
-    if (new_state == PEEKING)
-      new_state_override = FULLSCREEN_ALL_APPS;
-    else if (new_state == HALF)
+    if (new_state == HALF) {
       new_state_override = FULLSCREEN_SEARCH;
+    } else if (new_state == PEEKING && app_list_state_ == FULLSCREEN_ALL_APPS) {
+      // If the old state was already FULLSCREEN_ALL_APPS, then we should close.
+      new_state_override = CLOSED;
+    } else if (new_state == PEEKING && app_list_state_ != FULLSCREEN_ALL_APPS) {
+      // If the old state was not FULLSCREEN_ALL_APPS, we should make it so.
+      new_state_override = FULLSCREEN_ALL_APPS;
+    }
   }
 
   gfx::Rect new_widget_bounds = fullscreen_widget_->GetWindowBoundsInScreen();
