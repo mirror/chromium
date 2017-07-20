97e34159f0a47217f1130566ee5f1e36afe20ad4
Hidehiko Abe <hidehiko@chromium.org>
2017-07-20 12:42:34 +0900

    Remove ArcService.
    Now we migrated all ArcService classes into KeyedService.
    So, clean up unused class and code.
    
    BUG=672829
    TEST=Ran trybot.
    
    Change-Id: I280cc3387f8064abbd683de4c3ff62dd586d2b41

--- components/arc/arc_service_manager.h
+++ components/arc/arc_service_manager.h
@@ -6,16 +6,9 @@
 #define COMPONENTS_ARC_ARC_SERVICE_MANAGER_H_
 
 #include <memory>
-#include <string>
-#include <type_traits>
-#include <unordered_map>
-#include <utility>
-#include <vector>
 
 #include "base/macros.h"
-#include "base/memory/ref_counted.h"
 #include "base/threading/thread_checker.h"
-#include "components/arc/arc_service.h"
 #include "components/signin/core/account_id/account_id.h"
 
 namespace content {
@@ -25,40 +18,6 @@ class BrowserContext;
 namespace arc {
 
 class ArcBridgeService;
-
-namespace internal {
-
-// If an ArcService is declared with a name, e.g.:
-//
-// class MyArcService : public ArcService {
-//  public:
-//   static const char kArcServiceName[];
-//   ...
-// };
-//
-// it can then be retrieved from ArcServiceManager in a type-safe way using
-// GetService<T>(). This two functions allow AddService() to get the name only
-// if it was provided, or use an empty string otherwise.
-//
-// Although the typename is always specified explicitly by the caller, the
-// parameter is required in order for SFINAE to work correctly.  It is not used
-// and can be nullptr, though.
-//
-// In order to avoid collisions, kArcServiceName should be the fully-qualified
-// name of the class.
-template <typename T>
-decltype(T::kArcServiceName, std::string()) GetArcServiceName(T* unused) {
-  if (strlen(T::kArcServiceName) == 0)
-    LOG(ERROR) << "kArcServiceName[] should be a fully-qualified class name.";
-  return T::kArcServiceName;
-}
-
-template <typename T>
-std::string GetArcServiceName(...) {
-  return std::string();
-}
-
-}  // namespace internal
 
 // Manages creation and destruction of services that communicate with the ARC
 // instance via the ArcBridgeService.
@@ -93,49 +52,14 @@ class ArcServiceManager {
   // class was created on.
   ArcBridgeService* arc_bridge_service();
 
-  // Adds a service to the managed services list. Returns false if another
-  // named service with that name had already been added.
-  template <typename T>
-  bool AddService(std::unique_ptr<T> service) {
-    return AddServiceInternal(internal::GetArcServiceName<T>(nullptr),
-                              std::move(service));
-  }
-
-  // Gets the named service from the managed services list. This uses SFINAE, so
-  // you can only call this function if the service specified by T provides a
-  // static member variable called kArcServiceName[] (otherwise this will not
-  // compile).
-  template <typename T>
-  T* GetService() {
-    return static_cast<T*>(GetNamedServiceInternal(T::kArcServiceName));
-  }
-
-  // Does the same as GetService(), but with the global instance. Return nullptr
-  // when the instance hasn't been created or has already been destructed.
-  template <typename T> static T* GetGlobalService() {
-    auto* service_manager = ArcServiceManager::Get();
-    if (!service_manager)
-      return nullptr;
-    return service_manager->GetService<T>();
-  }
-
   // Gets the global instance of the ARC Service Manager. This can only be
   // called on the thread that this class was created on.
   static ArcServiceManager* Get();
 
-  // Called to shut down all ARC services.
-  void Shutdown();
-
  private:
-  // Helper methods for AddService and GetService.
-  bool AddServiceInternal(const std::string& name,
-                          std::unique_ptr<ArcService> service);
-  ArcService* GetNamedServiceInternal(const std::string& name);
-
   THREAD_CHECKER(thread_checker_);
 
   std::unique_ptr<ArcBridgeService> arc_bridge_service_;
-  std::unordered_multimap<std::string, std::unique_ptr<ArcService>> services_;
 
   // This holds the pointer to the BrowserContext (practically Profile)
   // which is allowed to use ARC.
