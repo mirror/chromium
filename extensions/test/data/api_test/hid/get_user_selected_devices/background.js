a6392bd9fe3cede9542372e8f4d7e3c703e0e706
Ke He <ke.he@intel.com>
2017-07-20 16:54:53 +0800

    Map the HidPlatformDeviceId to HidDeviceId.
    Before mojofication on Hid, we have to add typemapping for HidDeviceId first.
    HidDeviceId is defined as uint64_t on MacOS while as string on other
    platforms.
    
    In this CL, we rename the original HidDeviceId to HidPlatformDeviceId, and
    define the new HidDeviceId type as uint64_t. we map the PlatformHidDeviceId
    to HidDeviceId. 
    
    All clients of HidService should hold the new HidDeviceId instead of the
    PlatformHidDeviceId.
    
    BUG=728223
    
    Change-Id: I3585c9eb0eacb02571258314c14e063adce653fc

--- extensions/test/data/api_test/hid/get_user_selected_devices/background.js
+++ extensions/test/data/api_test/hid/get_user_selected_devices/background.js
@@ -2,17 +2,30 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var device_from_user;
+var device_;
+
+function getDevice(devices) {
+  for (let i = 0; i< devices.length; i++) {
+    // HidApiTest.GetUserSelectedDevices adds/removes the device in which
+    // the productId is 0x58F1.
+    if (devices[i].productId === 0x58F1) {
+       return devices[i];
+    }
+  }
+  return undefined;
+}
 
 chrome.test.runWithUserGesture(function() {
-  chrome.hid.getDevices({}, function(devices) {
+  let filters = [{'vendorId': 0x18D1, 'productId': 0x58F0},
+                 {'vendorId': 0x18D1, 'productId': 0x58F1}];
+  chrome.hid.getDevices({'filters':filters}, function(devices) {
     chrome.test.assertNoLastError();
-    chrome.test.assertEq(0, devices.length);
-    chrome.hid.getUserSelectedDevices({ multiple: false }, function(devices) {
+    chrome.test.assertEq(3, devices.length);
+    device_ = getDevice(devices);
+    chrome.hid.getUserSelectedDevices({'multiple': false}, function(devices) {
       chrome.test.assertNoLastError();
       chrome.test.assertEq(1, devices.length);
-      device_from_user = devices[0];
-      chrome.hid.connect(device_from_user.deviceId, function(connection) {
+      chrome.hid.connect(devices[0].deviceId, function(connection) {
         chrome.test.assertNoLastError();
         chrome.hid.disconnect(connection.connectionId);
         chrome.test.sendMessage("opened_device");
@@ -22,13 +35,12 @@ chrome.test.runWithUserGesture(function() {
 });
 
 chrome.hid.onDeviceRemoved.addListener(function(deviceId) {
-  chrome.test.assertEq(device_from_user.deviceId, deviceId);
+  chrome.test.assertEq(device_.deviceId, deviceId);
   chrome.test.sendMessage("removed");
 });
 
 chrome.hid.onDeviceAdded.addListener(function(device) {
-  chrome.test.assertTrue(device_from_user.deviceId != device.deviceId);
-  chrome.test.assertEq(device_from_user.vendorId, device.vendorId);
-  chrome.test.assertEq(device_from_user.productId, device.productId);
+  chrome.test.assertTrue(device_.deviceId != device.deviceId);
+  chrome.test.assertEq(device_.vendorId, device.vendorId);
   chrome.test.sendMessage("added");
 });
