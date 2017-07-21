7875aef370c9d62f01112a082d3065768d3bbd4b
Eric Karl <ericrk@chromium.org>
2017-07-21 11:31:15 -0700

    Disable GPU timeout
    Bug:
    Change-Id: I67663a5c287d140bb2737f83fb4d9630d2b4b25d

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -689,12 +689,12 @@ void CompositorImpl::HandlePendingLayerTreeFrameSinkRequest() {
 
 #if defined(ADDRESS_SANITIZER) || defined(THREAD_SANITIZER) || \
     defined(SYZYASAN) || defined(CYGPROFILE_INSTRUMENTATION)
-  const int64_t kGpuChannelTimeoutInSeconds = 40;
+  const int64_t kGpuChannelTimeoutInSeconds = 400;
 #else
   // The GPU watchdog timeout is 15 seconds (1.5x the kGpuTimeout value due to
   // logic in GpuWatchdogThread). Make this slightly longer to give the GPU a
   // chance to crash itself before crashing the browser.
-  const int64_t kGpuChannelTimeoutInSeconds = 20;
+  const int64_t kGpuChannelTimeoutInSeconds = 400;
 #endif
 
   // Start the timer first, if the result comes synchronously, we want it to
