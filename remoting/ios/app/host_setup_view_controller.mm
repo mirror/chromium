d71002ed3f58b9d7109c0dde43c356c11386be16
Yuwei Huang <yuweih@chromium.org>
2017-07-20 18:23:06 -0700

    [CRD iOS] I18N for the host setup view
    Add i18n support for the host setup view. Also add missing strings in the
    settings menu.
    
    Bug: 747091
    Change-Id: Ideea85dc2d0d9933e39bfe9c6e9a1b00a190291c

--- remoting/ios/app/host_setup_view_controller.mm
+++ remoting/ios/app/host_setup_view_controller.mm
@@ -15,26 +15,18 @@
 #import "remoting/ios/app/host_setup_view_cell.h"
 #import "remoting/ios/app/remoting_theme.h"
 
-static NSString* const kReusableIdentifierItem = @"remotingSetupStepVCItem";
+#include "remoting/base/string_resources.h"
+#include "ui/base/l10n/l10n_util.h"
 
-// TODO(yuweih): i18n
-// Clang doesn't allow string literal concatenation in string array but
-// `git cl format` will break down long lines into multiple string literals.
-// This is to disable the warning until we move these to a better place.
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wobjc-string-concatenation"
-static NSArray<NSString*>* const kSetupSteps = @[
-  @"Go to the computer that you want to remotely access",
-  @"Visit google.com/remotedesktop from your Chrome browser",
-  @"Install the Chrome Remote Desktop software and follow the instructions to "
-   "complete the setup",
-  @"After your remote device is setup, you can access it directly from this "
-   "page",
-];
-#pragma clang diagnostic pop
+static NSString* const kReusableIdentifierItem = @"remotingSetupStepVCItem";
 
 static const CGFloat kHeaderHeight = 80.f;
 static const CGFloat kFooterHeight = 15.f;  // 80.0f for HostSetupFooterView
+
+@interface HostSetupViewController () {
+  NSArray<NSString*>* setupSteps;
+}
+@end
 
 @implementation HostSetupViewController
 
@@ -56,6 +48,10 @@ static const CGFloat kFooterHeight = 15.f;  // 80.0f for HostSetupFooterView
     [self.collectionView registerClass:[UICollectionReusableView class]
             forSupplementaryViewOfKind:UICollectionElementKindSectionFooter
                    withReuseIdentifier:UICollectionElementKindSectionFooter];
+
+    NSString* setupInstructions =
+        l10n_util::GetNSString(IDS_HOST_SETUP_INSTRUCTIONS);
+    setupSteps = [setupInstructions componentsSeparatedByString:@"\n"];
   }
   return self;
 }
@@ -73,7 +69,7 @@ static const CGFloat kFooterHeight = 15.f;  // 80.0f for HostSetupFooterView
 
 - (NSInteger)collectionView:(UICollectionView*)collectionView
      numberOfItemsInSection:(NSInteger)section {
-  return kSetupSteps.count;
+  return setupSteps.count;
 }
 
 - (UICollectionViewCell*)collectionView:(UICollectionView*)collectionView
@@ -81,7 +77,7 @@ static const CGFloat kFooterHeight = 15.f;  // 80.0f for HostSetupFooterView
   HostSetupViewCell* cell = [collectionView
       dequeueReusableCellWithReuseIdentifier:kReusableIdentifierItem
                                 forIndexPath:indexPath];
-  [cell setContentText:kSetupSteps[indexPath.item] number:indexPath.item + 1];
+  [cell setContentText:setupSteps[indexPath.item] number:indexPath.item + 1];
   return cell;
 }
 
