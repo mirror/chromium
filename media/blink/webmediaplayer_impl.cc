1dd32c9db48d749b842c9cddc8ad0e858bfc152b
Jiajia Qin <jiajia.qin@intel.com>
2017-07-21 10:06:45 +0800

    Fixed glTexSubImage2D bad dimensions issue
    When TexImageHelperHTMLVideoElement goes to TexImageImpl path,
    it will use video's natural size as the image size. However,
    we should use the visible size when uploading video data.
    
    Tested with https://github.com/KhronosGroup/WebGL/pull/2464
    
    BUG=733172
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I2265ade9bb14b5c923af634f54116df678f4c224

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -797,6 +797,16 @@ blink::WebSize WebMediaPlayerImpl::NaturalSize() const {
   return blink::WebSize(pipeline_metadata_.natural_size);
 }
 
+blink::WebSize WebMediaPlayerImpl::VisibleRect() {
+  DCHECK(main_task_runner_->BelongsToCurrentThread());
+  scoped_refptr<VideoFrame> video_frame = GetCurrentFrameFromCompositor();
+  if (!video_frame)
+    return blink::WebSize();
+
+  const gfx::Rect& visible_rect = video_frame->visible_rect();
+  return blink::WebSize(visible_rect.width(), visible_rect.height());
+}
+
 bool WebMediaPlayerImpl::Paused() const {
   DCHECK(main_task_runner_->BelongsToCurrentThread());
 
