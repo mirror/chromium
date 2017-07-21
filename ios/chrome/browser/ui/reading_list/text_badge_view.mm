30b1329eaa9bce6d54b59bd37d08e4f53d559e36
Helen Yang <helenlyang@google.com>
2017-07-21 16:15:10 -0700

    [ios] Implement TextBadgeView and integrate with Showcase
    This CL implements TextBadgeView. It sets the appearance of the
    badge and adds constraints so that subviews are centered and the
    badge stretches to fit display text. Additionally, this CL
    modifies the Showcase implementation so that it displays the text
    badge instead of a placeholder rectangle. This commit does not
    refactor NumberBadgeView.
    
    Bug: 740133
    Change-Id: I8d5db930e725e273405ce5dd580b91f38a97fc09

--- ios/chrome/browser/ui/reading_list/text_badge_view.mm
+++ ios/chrome/browser/ui/reading_list/text_badge_view.mm
@@ -4,15 +4,69 @@
 
 #import "ios/chrome/browser/ui/reading_list/text_badge_view.h"
 
+#import "ios/chrome/browser/ui/colors/MDCPalette+CrAdditions.h"
+#import "ios/third_party/material_components_ios/src/components/Typography/src/MaterialTypography.h"
+
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
+namespace {
+const CGFloat kFontSize = 10;
+const CGFloat kLabelMargin = 2.5f;
+}
+
+@interface TextBadgeView ()
+@property(nonatomic, readonly, weak) UILabel* label;
+@end
+
 @implementation TextBadgeView
+
+@synthesize text = _text;
+@synthesize label = _label;
 
 - (instancetype)initWithText:(NSString*)text {
   self = [super initWithFrame:CGRectZero];
+  _text = text;
+  if (self) {
+    UILabel* label = [[UILabel alloc] initWithFrame:CGRectZero];
+    _label = label;
+    [label setFont:[[MDCTypography fontLoader] boldFontOfSize:kFontSize]];
+    [label setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [label setTextColor:[UIColor whiteColor]];
+    [label setText:text];
+    [self addSubview:label];
+
+    [NSLayoutConstraint activateConstraints:@[
+      // Center label on badge.
+      [label.centerXAnchor constraintEqualToAnchor:self.centerXAnchor],
+      [label.centerYAnchor constraintEqualToAnchor:self.centerYAnchor],
+
+      // Make badge fit label.
+      [self.heightAnchor constraintEqualToAnchor:label.heightAnchor
+                                        constant:kLabelMargin * 2],
+      [self.widthAnchor constraintGreaterThanOrEqualToAnchor:self.heightAnchor],
+      [self.widthAnchor constraintGreaterThanOrEqualToAnchor:label.widthAnchor
+                                                    constant:kLabelMargin * 4]
+    ]];
+
+    [self setBackgroundColor:[[MDCPalette cr_bluePalette] tint500]];
+  }
   return self;
+}
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+  // Set the badge's corner radius to be one half of its height. This allows the
+  // ends of the badge to be circular.
+  self.layer.cornerRadius = self.bounds.size.height / 2.0f;
+}
+
+- (void)setText:(NSString*)text {
+  if (self.text != text) {
+    _text = text;
+    self.label.text = text;
+  }
 }
 
 @end
