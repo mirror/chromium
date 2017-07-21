699ce2b56e3cdb7e9ff40c82700d03a252b50a9c
Sam McNally <sammc@chromium.org>
2017-07-21 16:36:44 +1000

    WIP: Reconnect to prefs from mash when the active profile changes.
    Change-Id: Ib121f95fda6ed5a348b8238363f07b1d150057cf

--- ash/shell.cc
+++ ash/shell.cc
@@ -1250,6 +1250,25 @@ void Shell::OnWindowActivated(
     root_window_for_new_windows_ = gained_active->GetRootWindow();
 }
 
+void Shell::OnActiveUserSessionChanged(const AccountId& account_id) {
+  if (shell_port_->GetAshConfig() == Config::MASH &&
+      shell_delegate_->GetShellConnector()) {
+    {
+      auto pref_service = std::move(pref_service_);
+      for (auto& observer : shell_observers_)
+        observer.OnPrefServiceReady();
+    }
+
+    auto pref_registry = base::MakeRefCounted<PrefRegistrySimple>();
+    Shell::RegisterProfilePrefs(pref_registry.get());
+    prefs::ConnectToPrefService(shell_delegate_->GetShellConnector(),
+                                std::move(pref_registry),
+                                base::Bind(&Shell::OnPrefServiceInitialized,
+                                           weak_factory_.GetWeakPtr()),
+                                prefs::mojom::kForwarderServiceName);
+  }
+}
+
 void Shell::OnSessionStateChanged(session_manager::SessionState state) {
   // Initialize the shelf when a session becomes active. It's safe to do this
   // multiple times (e.g. initial login vs. multiprofile add session).
@@ -1307,7 +1326,16 @@ void Shell::OnPrefServiceInitialized(
     std::unique_ptr<::PrefService> pref_service) {
   // |pref_service_| is null if can't connect to Chrome (as happens when
   // running mash outside of chrome --mash and chrome isn't built).
+  if (pref_service_) {
+    auto old_pref_service = std::move(pref_service_);
+    for (auto& observer : shell_observers_)
+      observer.OnPrefServiceReady();
+  }
   pref_service_ = std::move(pref_service);
+  if (pref_service_) {
+    for (auto& observer : shell_observers_)
+      observer.OnPrefServiceReady();
+  }
 }
 
 void Shell::OnLocalStatePrefServiceInitialized(
