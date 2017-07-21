f8285ba3eb4c8779ee5e29cd47bc971107f2998d
Daichi Hirono <hirono@chromium.org>
2017-07-21 14:24:23 +0900

    Add DataDeviceManager class.
    DataDeviceManager manages DataDevice instances. It's used to obtain
    DataDevice's instances to notify drag events in the future CL.
    
    Bug: b:31988797
    Test: None
    Change-Id: I67d15f81a59730f0d78da3d56f120a16d84a43b3

---
+++ components/exo/data_device_manager.h
@@ -0,0 +1,37 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_EXO_DATA_DEVICE_MANAGER_H_
+#define COMPONENTS_EXO_DATA_DEVICE_MANAGER_H_
+
+#include "base/containers/flat_set.h"
+#include "base/macros.h"
+
+namespace exo {
+
+class DataDevice;
+
+enum class DndAction { kNone, kCopy, kMove, kAsk };
+
+// Object managing data transfer objects like DataDevice.
+class DataDeviceManager {
+ public:
+  explicit DataDeviceManager();
+  ~DataDeviceManager();
+
+  // Adds |data_device| to the manager.
+  void AddDataDevice(DataDevice* data_device);
+
+  // Removes |data_device| from the manager.
+  void RemoveDataDevice(DataDevice* data_device);
+
+ private:
+  base::flat_set<DataDevice*> data_devices_;
+
+  DISALLOW_COPY_AND_ASSIGN(DataDeviceManager);
+};
+
+}  // namespace exo
+
+#endif  // COMPONENTS_EXO_DATA_DEVICE_H_
