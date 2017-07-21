42f0e441f62fcc8c2c1e602332470f67b5f712f9
Aleksandr Derbenev <alex-ac@yandex-team.ru>
2017-07-21 14:39:26 +0300

    Fix std::out_of_range exception in test results tracker.
    In browser_tests and interactive_ui_tests we have a lot of tests
    prefixed with "PRE_". This prefix is handled by content test launcher.
    When test with "PRE_" test launcher meets std::out_of_range exception in
    the base::TestResultsTracker::PrintTests() method. As result test
    launcher was terminated in this case without saving summary. Also
    locations for "PRE_" tests were never reported to test launcher summary.
    
    This change fixes this exception and adds all tests locations to the
    summary.
    
    R=phajdan.jr@chromium.org
    
    Bug: 746951
    Change-Id: If6fcf28ca6077f39be317340f199465604bea930

--- base/test/launcher/test_results_tracker.h
+++ base/test/launcher/test_results_tracker.h
@@ -43,15 +43,14 @@ class TestResultsTracker {
 
   // Adds |test_name| to the set of discovered tests (this includes all tests
   // present in the executable, not necessarily run).
-  void AddTest(const std::string& test_name);
+  void AddTest(const std::string& test_name, const std::string& file, int line);
+
+  // Marks |test_name| as planned to run in this shard (test will be reported
+  // in the all_tests section of json summary).
+  void AddShardTest(const std::string& test_name);
 
   // Adds |test_name| to the set of disabled tests.
   void AddDisabledTest(const std::string& test_name);
-
-  // Adds location for the |test_name|.
-  void AddTestLocation(const std::string& test_name,
-                       const std::string& file,
-                       int line);
 
   // Adds |result| to the stored test results.
   void AddTestResult(const TestResult& result);
@@ -124,6 +123,9 @@ class TestResultsTracker {
   std::set<std::string> global_tags_;
 
   // Set of all test names discovered in the current executable.
+  std::set<std::string> all_known_tests_;
+
+  // All runned tests.
   std::set<std::string> all_tests_;
 
   std::map<std::string, CodeLocation> test_locations_;
