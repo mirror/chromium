171d5ba1dfebcc2f1a8caf28880b0f0a88140df2
Xida Chen <xidachen@chromium.org>
2017-07-21 13:43:26 -0400

    Pass the actual touch action to CC in ScrollingCoordinator
    Right now in ScrollingCoordinator::SetTouchEventTargetRects, we always
    send TouchAction::kTouchActionNone when we pass the rect to cc.
    
    This CL change the kTouchActionNone to the real value associated with
    that rect.
    
    Bug: 716668
    Change-Id: I92c450fac165c59ba01ae1ef9f037c3001d56403

--- third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
+++ third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
@@ -34,6 +34,7 @@
 #include "core/frame/Settings.h"
 #include "core/frame/VisualViewport.h"
 #include "core/html/HTMLElement.h"
+#include "core/input/TouchActionUtil.h"
 #include "core/layout/LayoutEmbeddedContent.h"
 #include "core/layout/LayoutGeometryMap.h"
 #include "core/layout/api/LayoutEmbeddedContentItem.h"
@@ -757,20 +758,29 @@ void ScrollingCoordinator::SetTouchEventTargetRects(
   // on GraphicsLayer instead of Layer, but we have no good hook into the
   // lifetime of a GraphicsLayer.
   GraphicsLayerHitTestRects graphics_layer_rects;
+  WTF::HashMap<const GraphicsLayer*, TouchAction> paint_layer_touch_action;
   for (const PaintLayer* layer : layers_with_touch_rects_) {
     if (layer->GetLayoutObject().GetFrameView() &&
         layer->GetLayoutObject().GetFrameView()->ShouldThrottleRendering()) {
       continue;
     }
+    TouchAction effective_touch_action =
+        TouchActionUtil::ComputeEffectiveTouchAction(*(layer->EnclosingNode()));
     GraphicsLayer* main_graphics_layer =
         layer->GraphicsLayerBacking(&layer->GetLayoutObject());
-    if (main_graphics_layer)
+    if (main_graphics_layer) {
       graphics_layer_rects.insert(main_graphics_layer, Vector<LayoutRect>());
+      paint_layer_touch_action.insert(main_graphics_layer,
+                                      effective_touch_action);
+    }
     GraphicsLayer* scrolling_contents_layer = layer->GraphicsLayerBacking();
     if (scrolling_contents_layer &&
-        scrolling_contents_layer != main_graphics_layer)
+        scrolling_contents_layer != main_graphics_layer) {
       graphics_layer_rects.insert(scrolling_contents_layer,
                                   Vector<LayoutRect>());
+      paint_layer_touch_action.insert(scrolling_contents_layer,
+                                      effective_touch_action);
+    }
   }
 
   layers_with_touch_rects_.clear();
@@ -789,11 +799,13 @@ void ScrollingCoordinator::SetTouchEventTargetRects(
 
   for (const auto& layer_rect : graphics_layer_rects) {
     const GraphicsLayer* graphics_layer = layer_rect.key;
+    TouchAction effective_touch_action = TouchAction::kTouchActionNone;
+    if (paint_layer_touch_action.Contains(graphics_layer))
+      effective_touch_action = paint_layer_touch_action.at(graphics_layer);
     WebVector<WebTouchInfo> touch(layer_rect.value.size());
     for (size_t i = 0; i < layer_rect.value.size(); ++i) {
       touch[i].rect = EnclosingIntRect(layer_rect.value[i]);
-      // TODO(xidachen): route the real value here
-      touch[i].touch_action = TouchAction::kTouchActionNone;
+      touch[i].touch_action = effective_touch_action;
     }
     graphics_layer->PlatformLayer()->SetTouchEventHandlerRegion(touch);
   }
