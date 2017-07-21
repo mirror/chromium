53a113e537cda13bef7142fe762153ae529c93d5
Brett Wilson <brettw@chromium.org>
2017-07-21 13:31:14 -0700

    Add process lists and dump plumbing to OOP memory logger.
    The current process list now appears in chrome://memory-internals/, and
    the dump plumbing is added through to the profiling process.
    
    The actual dumping code is still not written so this does nothing.
    
    Adds the failed file to a histogram assertion since I saw this fire once
    in my testing and did not catch the name that caused the problem.
    
    Change-Id: I5c36481d609b779a0414315390d4c681fd512ac0

--- chrome/browser/ui/webui/memory_internals_ui.cc
+++ chrome/browser/ui/webui/memory_internals_ui.cc
@@ -4,23 +4,43 @@
 
 #include "chrome/browser/ui/webui/memory_internals_ui.h"
 
+#include <iterator>
+
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
+#include "base/memory/weak_ptr.h"
+#include "base/process/process_handle.h"
+#include "base/strings/stringprintf.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiling_host/profiling_process_host.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/browser_resources.h"
+#include "content/public/browser/browser_child_process_host_iterator.h"
+#include "content/public/browser/browser_thread.h"
+#include "content/public/browser/child_process_data.h"
+#include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "content/public/browser/web_ui_message_handler.h"
+#include "content/public/common/process_type.h"
 
 namespace {
 
 // Generates one row of the returned process info.
-base::Value MakeProcessInfo(int pid, const std::string& description) {
+base::Value MakeProcessInfo(int pid, std::string description) {
   base::Value result(base::Value::Type::LIST);
   result.GetList().push_back(base::Value(pid));
-  result.GetList().push_back(base::Value(description));
+  result.GetList().push_back(base::Value(std::move(description)));
   return result;
+}
+
+// Some child processes have good descriptions and some don't, this function
+// returns the best it can given the data.
+std::string GetChildDescription(const content::ChildProcessData& data) {
+  if (!data.name.empty())
+    return base::UTF16ToUTF8(data.name);
+  return content::GetProcessTypeNameInEnglish(data.process_type);
 }
 
 content::WebUIDataSource* CreateMemoryInternalsUIHTMLSource() {
@@ -46,10 +66,16 @@ class MemoryInternalsDOMHandler : public content::WebUIMessageHandler {
   void HandleDumpProcess(const base::ListValue* args);
 
  private:
+  static void GetChildProcessesOnIOThread(
+      base::WeakPtr<MemoryInternalsDOMHandler> dom_handler);
+  void ReturnProcessListOnUIThread(std::vector<base::Value> children);
+
+  base::WeakPtrFactory<MemoryInternalsDOMHandler> weak_factory_;
+
   DISALLOW_COPY_AND_ASSIGN(MemoryInternalsDOMHandler);
 };
 
-MemoryInternalsDOMHandler::MemoryInternalsDOMHandler() {}
+MemoryInternalsDOMHandler::MemoryInternalsDOMHandler() : weak_factory_(this) {}
 
 MemoryInternalsDOMHandler::~MemoryInternalsDOMHandler() {}
 
@@ -67,20 +93,85 @@ void MemoryInternalsDOMHandler::RegisterMessages() {
 
 void MemoryInternalsDOMHandler::HandleRequestProcessList(
     const base::ListValue* args) {
+  // This is called on the UI thread, the child process iterator must run on
+  // the IO thread.
+  //
+  // TODO(brettw) Have some way to start the profiler if it's not started, or
+  // at least tell people to restart with --memlog if it's not running.
+  content::BrowserThread::PostTask(
+      content::BrowserThread::IO, FROM_HERE,
+      base::Bind(&MemoryInternalsDOMHandler::GetChildProcessesOnIOThread,
+                 weak_factory_.GetWeakPtr()));
+}
+
+void MemoryInternalsDOMHandler::HandleDumpProcess(const base::ListValue* args) {
+  profiling::ProfilingProcessHost* pph = profiling::ProfilingProcessHost::Get();
+  if (!pph)
+    return;
+
+  if (!args->is_list() || args->GetList().size() != 1)
+    return;
+  const base::Value& pid_value = args->GetList()[0];
+  if (!pid_value.is_int())
+    return;
+
+  pph->RequestProcessDump(pid_value.GetInt());
+}
+
+void MemoryInternalsDOMHandler::GetChildProcessesOnIOThread(
+    base::WeakPtr<MemoryInternalsDOMHandler> dom_handler) {
+  std::vector<base::Value> result;
+
+  // Add child processes (this does not include renderers).
+  for (content::BrowserChildProcessHostIterator iter; !iter.Done(); ++iter) {
+    // Note that ChildProcessData.id is a child ID and not an OS PID.
+    const content::ChildProcessData& data = iter.GetData();
+    base::ProcessId proc_id = base::GetProcId(data.handle);
+    std::string desc = base::StringPrintf("Process %d [%s]", proc_id,
+                                          GetChildDescription(data).c_str());
+    result.push_back(MakeProcessInfo(proc_id, std::move(desc)));
+  }
+
+  content::BrowserThread::PostTask(
+      content::BrowserThread::UI, FROM_HERE,
+      base::BindOnce(&MemoryInternalsDOMHandler::ReturnProcessListOnUIThread,
+                     dom_handler, std::move(result)));
+}
+
+void MemoryInternalsDOMHandler::ReturnProcessListOnUIThread(
+    std::vector<base::Value> children) {
   base::Value result(base::Value::Type::LIST);
-  // Return some canned data for now.
-  // TODO(brettW) hook this up.
-  result.GetList().push_back(MakeProcessInfo(11234, "Process 11234 [Browser]"));
-  result.GetList().push_back(MakeProcessInfo(11235, "Process 11235 [GPU]"));
-  result.GetList().push_back(MakeProcessInfo(11236, "Process 11236 [Rend]"));
+  std::vector<base::Value>& result_list = result.GetList();
+
+  // Add browser process.
+  base::ProcessId browser_pid = base::GetCurrentProcId();
+  result_list.push_back(MakeProcessInfo(
+      browser_pid,
+      base::StringPrintf("Process %d [Browser]", browser_pid).c_str()));
+
+  // Append renderer processes (these can only be enumerated on the UI thread).
+  auto iter = content::RenderProcessHost::AllHostsIterator();
+  while (!iter.IsAtEnd()) {
+    base::ProcessHandle renderer_handle = iter.GetCurrentValue()->GetHandle();
+    base::ProcessId renderer_pid = base::GetProcId(renderer_handle);
+    if (renderer_pid != 0) {
+      // TODO(brettw) make a better description of the process, maybe see
+      // what TaskManager does.
+      result_list.push_back(MakeProcessInfo(
+          renderer_pid,
+          base::StringPrintf("Process %d [Renderer]", renderer_pid).c_str()));
+    }
+    iter.Advance();
+  }
+
+  // Append all child processes collected on the IO thread.
+  result_list.insert(result_list.end(),
+                     std::make_move_iterator(std::begin(children)),
+                     std::make_move_iterator(std::end(children)));
 
   AllowJavascript();
   CallJavascriptFunction("returnProcessList", result);
   DisallowJavascript();
-}
-
-void MemoryInternalsDOMHandler::HandleDumpProcess(const base::ListValue* args) {
-  // TODO(brettW) hook this up.
 }
 
 }  // namespace
