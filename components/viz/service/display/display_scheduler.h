511b2d3d192bb4a4d08bb2519e6359f57a022bbc
Fady Samuel <fsamuel@chromium.org>
2017-07-23 16:11:10 -0400

    [WIP] viz: cc/surfaces => components/viz/service/surfaces
    Bug:
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I2a05631c64ab7bbc1123af4da9a31eb50fb9b4b9

--- components/viz/service/display/display_scheduler.h
+++ components/viz/service/display/display_scheduler.h
@@ -13,9 +13,9 @@
 #include "base/memory/ref_counted.h"
 #include "base/single_thread_task_runner.h"
 #include "cc/scheduler/begin_frame_source.h"
-#include "cc/surfaces/surface_observer.h"
 #include "components/viz/common/display/renderer_settings.h"
 #include "components/viz/common/surfaces/surface_id.h"
+#include "components/viz/service/surfaces/surface_observer.h"
 #include "components/viz/service/viz_service_export.h"
 
 namespace viz {
@@ -35,7 +35,7 @@ class VIZ_SERVICE_EXPORT DisplaySchedulerClient {
 };
 
 class VIZ_SERVICE_EXPORT DisplayScheduler : public cc::BeginFrameObserverBase,
-                                            public cc::SurfaceObserver {
+                                            public SurfaceObserver {
  public:
   DisplayScheduler(cc::BeginFrameSource* begin_frame_source,
                    base::SingleThreadTaskRunner* task_runner,
