b9821064a3213c0232c0d8ef3f031938bdeca4db
Koji Ishii <kojii@chromium.org>
2017-07-23 23:26:21 +0900

    [LayoutNG] Make floats push line boxes down if no break opportunities can fit
    When no break opportunities can fit in a line that has floats and/or
    exclusions, such line box should be pushed down to avoid them.
    
    A simple example is "float: left; width: 100%". This patch supports both
    100% case and other cases in slightly different code paths since
    NGLayoutOpportunityIterator handles 100% case differently.
    
    Rewind() has been used for next lines, but this is the first case where
    NGLineBreaker rewinds and process the current line again. Initializing
    states is moved to NextLine() from BreakLine() to make this possible.
    
    This patch also includes fix to avoid processing the same floats twice.
    This should have been an issue when Rewind() is used, but it was not
    handled properly before. The fix isn't very clean though, this should be
    revisited.
    
    BUG=636993
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    
    patch from issue 2955843002 at patchset 80001 (http://crrev.com/2955843002#ps80001)
    
    Change-Id: Ideae5ae00374c500f858bb992d5d3203772829a2

--- third_party/WebKit/Source/core/layout/ng/inline/ng_line_breaker.h
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_line_breaker.h
@@ -83,6 +83,7 @@ class CORE_EXPORT NGLineBreaker {
   void PrepareNextLine(NGLineInfo*);
 
   void UpdateAvailableWidth();
+  void MoveDownBelowFloats(LayoutUnit min_inline_size);
 
   void ComputeLineLocation(NGLineInfo*) const;
 
@@ -137,6 +138,9 @@ class CORE_EXPORT NGLineBreaker {
   LazyLineBreakIterator break_iterator_;
   HarfBuzzShaper shaper_;
   ShapeResultSpacing<String> spacing_;
+
+  // Keep track of handled float items. See HandleFloat().
+  unsigned handled_floats_end_item_index_ = 0;
 
   // True when current box allows line wrapping.
   bool auto_wrap_ = false;
