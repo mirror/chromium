e311e8313a52771e12329e6ee85bda16c65ab212
Wez <wez@chromium.org>
2017-07-22 17:05:00 -0700

    [DO NOT LAND] Allow LOG_DCHECK to be configured at run-time.
    Hack DCHECK log-level to be configurable at run-time, if enabled.
    
    BUG: 596231
    
    Change-Id: I390aecdba40c8a8b396d6a4a03162ff071e9c3d5

--- base/feature_list.cc
+++ base/feature_list.cc
@@ -276,6 +276,20 @@ void FeatureList::SetInstance(std::unique_ptr<FeatureList> instance) {
 
   // Note: Intentional leak of global singleton.
   g_instance = instance.release();
+
+#if DCHECK_IS_ON() && (defined(ADDRESS_SANITIZER) || defined(SYZYASAN))
+  // Update the behaviour of LOG_DCHECK to match the Feature configuration.
+  // DCHECK is also forced to be FATAL if we are running a death-test.
+  static const base::Feature kDCheckIsFatalFeature{
+      "DcheckIsFatal", base::FEATURE_DISABLED_BY_DEFAULT};
+  if (base::FeatureList::IsEnabled(kDCheckIsFatalFeature) ||
+      base::CommandLine::ForCurrentProcess()->HasSwitch(
+          "gtest_internal_run_death_test")) {
+    logging::LOG_DCHECK = logging::LOG_FATAL;
+  } else {
+    logging::LOG_DCHECK = logging::LOG_INFO;
+  }
+#endif  // DCHECK_IS_ON() && (defined(ADDRESS_SANITIZER) || defined(SYZYASAN))
 }
 
 // static
