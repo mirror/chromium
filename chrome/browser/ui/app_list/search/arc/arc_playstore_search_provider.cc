9f246aafb8b396eacab7c6a93c5ade87572cac74
Jiaquan He <hejq@google.com>
2017-07-24 11:56:46 -0700

    Add UMA for Play Store app search.
    - Record the round trip time of a Play Store app search query.
    - Record user actions of clicking into uninstalled Play Store
      apps and instant apps.
    - Record the numbers of returned uninstalled apps, instant apps,
      and their sum for every Play Store app search query.
    - Add Play Store app search result opening actions to the
      Apps.AppListSearchResultOpenType histogram.
    
    Also to distinguish canceled Play Store app search requests from
    completed ones, this commit contains a mojo API change.
    
    BUG=742595
    
    Change-Id: I94d1fcd985f123afdb5837bcd71f4aaf67323813

--- chrome/browser/ui/app_list/search/arc/arc_playstore_search_provider.cc
+++ chrome/browser/ui/app_list/search/arc/arc_playstore_search_provider.cc
@@ -6,7 +6,9 @@
 
 #include <utility>
 
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/time/time.h"
 #include "chrome/browser/ui/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/browser/ui/app_list/search/arc/arc_playstore_search_result.h"
 #include "components/arc/arc_bridge_service.h"
@@ -35,10 +37,11 @@ void ArcPlayStoreSearchProvider::Start(bool is_voice_query,
                 GetRecentAndSuggestedAppsFromPlayStore)
           : nullptr;
 
-  if (app_instance == nullptr)
+  if (app_instance == nullptr || query.empty())
     return;
 
   ClearResults();
+  query_start_time_ = base::Time::Now();
   app_instance->GetRecentAndSuggestedAppsFromPlayStore(
       UTF16ToUTF8(query), max_results_,
       base::Bind(&ArcPlayStoreSearchProvider::OnResults,
@@ -48,10 +51,32 @@ void ArcPlayStoreSearchProvider::Start(bool is_voice_query,
 void ArcPlayStoreSearchProvider::Stop() {}
 
 void ArcPlayStoreSearchProvider::OnResults(
+    bool canceled,
     std::vector<arc::mojom::AppDiscoveryResultPtr> results) {
+  size_t instant_app_count = 0;
   for (auto& result : results) {
+    if (result->is_instant_app)
+      ++instant_app_count;
     Add(base::MakeUnique<ArcPlayStoreSearchResult>(std::move(result), profile_,
                                                    list_controller_));
+  }
+  // Record user metrics.
+  if (!canceled) {
+    UMA_HISTOGRAM_TIMES("Arc.PlayStoreSearch.QueryTime",
+                        base::Time::Now() - query_start_time_);
+    query_start_time_ = base::Time();
+    // |max_results_| is initialized when this class is constructed and will
+    // never change.
+    const int kHistogramBuckets = max_results_ + 1;
+    UMA_HISTOGRAM_CUSTOM_COUNTS("Arc.PlayStoreSearch.ReturnedAppsTotal",
+                                results.size(), 0, max_results_,
+                                kHistogramBuckets);
+    UMA_HISTOGRAM_CUSTOM_COUNTS("Arc.PlayStoreSearch.ReturnedUninstalledApps",
+                                results.size() - instant_app_count, 0,
+                                max_results_, kHistogramBuckets);
+    UMA_HISTOGRAM_CUSTOM_COUNTS("Arc.PlayStoreSearch.ReturnedInstantApps",
+                                instant_app_count, 0, max_results_,
+                                kHistogramBuckets);
   }
 }
 
