faafb8580ee5f5b22fc5ad801ab12d303f227c19
Colin Blundell <blundell@chromium.org>
2017-07-24 16:15:44 +0200

    [Identity Service] Add GetAccounts() API
    This CL adds an API to the Identity Service to get a list of all
    accounts. This API has the following semantics, using the needs of
    the chrome.identity.getAccounts() extension API as a guide:
    - Returns the primary account (if any) first
    - Returns only accounts for which a refresh token is available
    
    This CL also adds the Account mojom struct to wrap AccountInfo and
    AccountState. This change is necessary in order to easily return a
    list of (AccountInfo, AccountState) pairs. A followup CL will change
    all of the IdentityManager APIs to talk in terms of Account.
    
    Finally, this CL ports the implementation of chrome.identity.getAccounts()
    to use the new Identity Service API as an initial customer and
    validation of the API.
    
    Bug: 729533, 729537
    Test: Install a Chrome extension with the identity permissions in its
    Change-Id: Ibd4ea6843fc5c78738286f280d973a4a19176f23
    manifest. Go to chrome://extensions, enable developer mode, and inspect
    the background page of the above app. At the JS console that that brings
    up, execute:
    chrome.identity.getAccounts((accounts) => {console.log(accounts[0]);} )
    Verify that the user's GAIA ID (a long string of numbers) appears rather
    than an error being reported.

--- services/identity/identity_manager.cc
+++ services/identity/identity_manager.cc
@@ -9,6 +9,7 @@
 #include "base/time/time.h"
 #include "components/signin/core/browser/account_tracker_service.h"
 #include "google_apis/gaia/google_service_auth_error.h"
+#include "services/identity/public/interfaces/account.mojom.h"
 
 namespace identity {
 
@@ -120,6 +121,30 @@ void IdentityManager::GetAccountInfoFromGaiaId(
   AccountInfo account_info = account_tracker_->FindAccountInfoByGaiaId(gaia_id);
   AccountState account_state = GetStateOfAccount(account_info);
   std::move(callback).Run(account_info, account_state);
+}
+
+void IdentityManager::GetAccounts(GetAccountsCallback callback) {
+  std::vector<mojom::AccountPtr> accounts;
+
+  std::vector<std::string> accounts_with_refresh_token =
+      token_service_->GetAccounts();
+  for (std::vector<std::string>::const_iterator it =
+           accounts_with_refresh_token.begin();
+       it != accounts_with_refresh_token.end(); ++it) {
+    AccountInfo account_info = account_tracker_->GetAccountInfo(*it);
+    AccountState account_state = GetStateOfAccount(account_info);
+
+    mojom::AccountPtr account =
+        mojom::Account::New(account_info, account_state);
+
+    if (account->state.is_primary_account) {
+      accounts.insert(accounts.begin(), std::move(account));
+    } else {
+      accounts.push_back(std::move(account));
+    }
+  }
+
+  std::move(callback).Run(std::move(accounts));
 }
 
 void IdentityManager::GetAccessToken(const std::string& account_id,
