89ff7b4fdd5c2ff51c5cee09999d41541f417d8e
Conley Owens <cco3@chromium.org>
2017-07-24 15:02:14 -0700

    Use dynamic devices in reconnect-during WebBT test
    BUG=569709
    
    Change-Id: I34e22397032aeb95a99e5ceab5697863c81c75c4

--- third_party/WebKit/LayoutTests/bluetooth/script-tests/service/reconnect-during.js
+++ third_party/WebKit/LayoutTests/bluetooth/script-tests/service/reconnect-during.js
@@ -1,25 +1,18 @@
 'use strict';
 promise_test(() => {
-  return setBluetoothFakeAdapter('HeartRateAdapter')
-    .then(() => requestDeviceWithKeyDown({
-      filters: [{services: ['heart_rate']}]}))
-    .then(device => device.gatt.connect())
-    .then(gatt => {
-      return gatt.getPrimaryService('heart_rate')
-        .then(service => {
-          let promise = assert_promise_rejects_with_message(
-            service.CALLS([
-              getCharacteristic('heart_rate_measurement')|
-              getCharacteristics()|
-              getCharacteristics('heart_rate_measurement')[UUID]
-            ]),
-            new DOMException(
-              'GATT Server is disconnected. Cannot retrieve characteristics. ' +
-              '(Re)connect first with `device.gatt.connect`.',
-              'NetworkError'));
-          gatt.disconnect();
-          return gatt.connect().then(() => promise);
-        });
+  let service;
+  return getHealthThermometerService()
+    .then(vals => ({service} = vals))
+    .then(() => service.CALLS([
+      getCharacteristic('heart_rate_measurement')|
+      getCharacteristics()|
+      getCharacteristics('heart_rate_measurement')[UUID]
+    ])
+    .then(() => assert_unreached('Got descriptor unexpectedly.'), (e) => {
+      assert_equals(e.name, 'NetworkError');
+      assert_equals(e.message,
+          'GATT Server is disconnected. Cannot retrieve characteristics. ' +
+          '(Re)connect first with `device.gatt.connect`.'));
     });
 }, 'disconnect() and connect() called during FUNCTION_NAME. Reject with ' +
    'NetworkError.');
