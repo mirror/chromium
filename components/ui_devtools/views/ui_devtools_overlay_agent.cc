71626ed8ee37f24aebcdd303e79ddf2a24971848
Thanh Pham <thanhph@chromium.org>
2017-07-24 10:37:20 -0400

    Show guide lines and pin elements when user clicks on UIElement in UI DevTools while in inspection mode.
    This cl enables users, while in inspection mode, quickly see distances
    between a pinned UIElement and a hovered element. To exit inspection
    mode, user can click on the blue box-in-arrow or press ESC key.
    
    Change-Id: I3de056e856008401bb4b1777ee66e2f588259abf
    Bug:747404

--- components/ui_devtools/views/ui_devtools_overlay_agent.cc
+++ components/ui_devtools/views/ui_devtools_overlay_agent.cc
@@ -11,6 +11,7 @@ namespace ui_devtools {
 UIDevToolsOverlayAgent::UIDevToolsOverlayAgent(UIDevToolsDOMAgent* dom_agent)
     : dom_agent_(dom_agent) {
   DCHECK(dom_agent_);
+  pinned_id = 0;
 }
 
 UIDevToolsOverlayAgent::~UIDevToolsOverlayAgent() {}
@@ -28,6 +29,7 @@ ui_devtools::protocol::Response UIDevToolsOverlayAgent::disable() {
 ui_devtools::protocol::Response UIDevToolsOverlayAgent::setInspectMode(
     const String& in_mode,
     protocol::Maybe<protocol::Overlay::HighlightConfig> in_highlightConfig) {
+  pinned_id = 0;
   if (in_mode.compare("searchForNode") == 0)
     aura::Env::GetInstance()->PrependPreTargetHandler(this);
   else if (in_mode.compare("none") == 0)
@@ -56,13 +58,31 @@ void UIDevToolsOverlayAgent::OnMouseEvent(ui::MouseEvent* event) {
   int element_id = 0;
   dom_agent_->FindElementByEventHandler(event->root_location(), &element_id);
 
-  if (event->type() == ui::ET_MOUSE_PRESSED) {
+  if (event->type() == ui::ET_MOUSE_PRESSED && (pinned_id != element_id)) {
     event->SetHandled();
-    aura::Env::GetInstance()->RemovePreTargetHandler(this);
-    if (element_id)
-      frontend()->inspectNodeRequested(element_id);
-  } else if (element_id) {
+    if (element_id) {
+      pinned_id = element_id;
+      frontend()->nodeHighlightRequested(element_id);
+      dom_agent_->HighlightNode(element_id, true);
+    }
+  } else if (element_id && !pinned_id) {
     frontend()->nodeHighlightRequested(element_id);
+    dom_agent_->HighlightNode(element_id, false);
+  }
+}
+
+void UIDevToolsOverlayAgent::OnKeyEvent(ui::KeyEvent* event) {
+  if (!dom_agent_->window_element_root())
+    return;
+
+  // Exit inspection mode by pressing ESC key.
+  if (event->key_code() == ui::KeyboardCode::VKEY_ESCAPE) {
+    aura::Env::GetInstance()->RemovePreTargetHandler(this);
+    if (pinned_id) {
+      frontend()->inspectNodeRequested(pinned_id);
+      dom_agent_->HighlightNode(pinned_id, true);
+    }
+    pinned_id = 0;
   }
 }
 
