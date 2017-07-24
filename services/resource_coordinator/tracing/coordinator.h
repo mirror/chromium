12bf58e61a05720e573139efdf6e94f45ecd98ab
Ehsan Chiniforooshan <chiniforooshan@chromium.org>
2017-07-24 16:44:29 -0400

    tracing: TracingController -> Coordinator
    This CL re-plumbs the content code to use the tracing::Coordinator
    interface instead of content::TracingControllerImpl.
    
    Summary of modifications:
    
    1- Existing tracing agents extend base::trace_event::TracingAgent. We
       modified them to also extend tracing::Agent. If this CL lands
       successfully and we don't need to revert it soon, we will delete
       the old base::trace_event::TracingAgent methods.
    
    2- content::TracingControllerImpl is modified to forward all calls to
       tracing::Coordinator via Mojo.
    
    3- Chrome tracing IPC messages between child processes and
       content::TracingControllerImpl are deleted.
    
    4- The logic for adding clock sync markers is added to
       tracing::Coordinator.
    
    5- Now that tracing agents (battor/etw/cros/arc) and chrome trace
       trace event clients are unified, there is no need for
       TracingController::TraceDataSink. It is deleted.
       TracingController::TraceDataEndpoint is used directly.
    
    Bug=640235
    
    Change-Id: I6dab520a2b53b0a56a016fccf9052e76fc99f78f

--- services/resource_coordinator/tracing/coordinator.h
+++ services/resource_coordinator/tracing/coordinator.h
@@ -13,10 +13,15 @@
 #include "base/memory/ref_counted.h"
 #include "base/sequenced_task_runner.h"
 #include "base/single_thread_task_runner.h"
+#include "base/values.h"
 #include "mojo/public/cpp/system/data_pipe.h"
 #include "services/resource_coordinator/public/interfaces/tracing/tracing.mojom.h"
 #include "services/resource_coordinator/tracing/agent_registry.h"
 #include "services/resource_coordinator/tracing/recorder.h"
+
+namespace service_manager {
+struct BindSourceInfo;
+}  // namespace service_manager
 
 namespace tracing {
 
@@ -34,7 +39,9 @@ class Coordinator : public mojom::Coordinator {
 
   Coordinator();
 
-  void BindCoordinatorRequest(mojom::CoordinatorRequest request);
+  void BindCoordinatorRequest(
+      mojom::CoordinatorRequest request,
+      const service_manager::BindSourceInfo& source_info);
 
  private:
   friend std::default_delete<Coordinator>;
@@ -45,15 +52,21 @@ class Coordinator : public mojom::Coordinator {
   // mojom::Coordinator
   void StartTracing(const std::string& config,
                     const StartTracingCallback& callback) override;
-  void StopAndFlush(mojo::ScopedDataPipeProducerHandle stream) override;
+  void StopAndFlush(mojo::ScopedDataPipeProducerHandle stream,
+                    const StopAndFlushCallback& callback) override;
   void IsTracing(const IsTracingCallback& callback) override;
   void RequestBufferUsage(const RequestBufferUsageCallback& callback) override;
   void GetCategories(const GetCategoriesCallback& callback) override;
 
   // Internal methods for collecting events from agents.
   void SendStartTracingToAgent(AgentRegistry::AgentEntry* agent_entry);
-  void OnTracingStarted(AgentRegistry::AgentEntry* agent_entry);
+  void OnTracingStarted(AgentRegistry::AgentEntry* agent_entry, bool success);
   void StopAndFlushInternal();
+  void OnRequestClockSyncMarkerResponse(AgentRegistry::AgentEntry* agent_entry,
+                                        const std::string& sync_id,
+                                        base::TimeTicks issue_ts,
+                                        base::TimeTicks issue_end_ts);
+  void StopAndFlushAfterClockSync();
   void OnRecorderDataChange(const std::string& label);
   bool StreamEventsForCurrentLabel();
   void StreamMetadata();
@@ -80,9 +93,11 @@ class Coordinator : public mojom::Coordinator {
   // If |streaming_label_| is not empty, it shows the label for which we are
   // writing chunks to the output stream.
   std::string streaming_label_;
+  std::unique_ptr<base::DictionaryValue> metadata_;
   bool stream_header_written_ = false;
   bool json_field_name_written_ = false;
   StartTracingCallback start_tracing_callback_;
+  StopAndFlushCallback stop_and_flush_callback_;
 
   // For computing trace buffer usage.
   float maximum_trace_buffer_usage_ = 0;
