a98e8137079c209ffd78bcaeea0b93fe6545806f
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2017-07-24 15:15:11 +1000

    base: Add GetWithoutFilename() and replace uses of Resolve(".") with it
    Code that interacts with manifests often needs to get an instance of a
    URL without the filename. This includes code that checks if a URL is in
    scope[1][2] and code that uses another url for the scope[3]. There is no
    such helper function in base which has led to re-implementing the
    function in many places. Furthermore, the result of calling 'url.Resolve(".")'
    is not that clear making it really easy to miss the fact that it could
    be used to get a url without the filename.
    
    To avoid this we introduce GetWithoutFilename(). We also add tests to make
    the result of calling GetWithoutFilename() clear for its users.
    
    [1] https://cs.chromium.org/chromium/src/chrome/browser/android/url_utilities.cc?l=119
    [2] https://cs.chromium.org/chromium/src/content/common/service_worker/service_worker_utils.cc?l=74
    [3] https://cs.chromium.org/chromium/src/chrome/android/java/src/org/chromium/chrome/browser/ShortcutHelper.java?q=getScopeFromUrl&l=633
    
    Change-Id: Ibb0b2a9918a20333072c407a6a25e75ea5891faf

--- url/gurl.h
+++ url/gurl.h
@@ -181,6 +181,14 @@ class URL_EXPORT GURL {
   // will be the empty URL.
   GURL GetWithEmptyPath() const;
 
+  // A helper function to return a GURL without the filename, query values, and
+  // any username and password. For example,
+  // GURL("https://www.foo.com/index.html?q=test").GetWithoutFilename().spec()
+  // will return "https://www.foo.com/".
+  // GURL("https://www.foo.com/bar/").GetWithoutFilename().spec()
+  // will return "https://www.foo.com/bar/".
+  GURL GetWithoutFilename() const;
+
   // A helper function to return a GURL containing just the scheme, host,
   // and port from a URL. Equivalent to clearing any username and password,
   // replacing the path with a slash, and clearing everything after that. If
