aaa6f4cc8c1f340687b575cb9d83fd9ea031ebe9
huangs <huangs@google.com>
2017-07-24 17:56:30 -0400

    [Zucchini] Refactor command line processing in main.
    This CL implements grt@'s post-commit suggestions from
    https://chromium-review.googlesource.com/c/567492/, notably:
    - Add and use base::AtExitManager.
    - Add OOM and crash handling.
    - Add error status and return them in main; remove exit() usage.
    - Remove static initializers for command registration.
    
    Additional changes:
    - Extract command definitions to stand-alone functions, placed inside
      new files zucchini_commands.*, so command-line parsing code will be
      out of the way once we populate these with program logic.
    - Add zucchini.h, containing enum zucchini::status::Code.
    - CommandRegistry: Inject ostream* for error output, move command
      registration from main() to CommandRegistry::RegisterAll().
    
    Bug: 729154
    Change-Id: Ia4c5d1eb0cef3249d858ec939168f3a97a3091d7

--- chrome/installer/zucchini/main_utils.cc
+++ chrome/installer/zucchini/main_utils.cc
@@ -4,18 +4,17 @@
 
 #include "chrome/installer/zucchini/main_utils.h"
 
-#include <iostream>
+#include <memory>
 
-#include "base/bind.h"
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/process/process_metrics.h"
+#include "base/time/time.h"
 #include "build/build_config.h"
 #include "chrome/installer/zucchini/io_utils.h"
 
 namespace {
 
-// Translates |command_line| arguments to a vector of base::FilePath and returns
 // the result via |fnames|. Expects exactly |expected_count|.
 bool CheckAndGetFilePathParams(const base::CommandLine& command_line,
                                size_t expected_count,
@@ -25,42 +24,61 @@ bool CheckAndGetFilePathParams(const base::CommandLine& command_line,
     return false;
 
   fnames->clear();
-  for (size_t i = 0; i < args.size(); ++i)
-    fnames->push_back(base::FilePath(args[i]));
+  fnames->reserve(args.size());
+  for (const auto& arg : args)
+    fnames->emplace_back(arg);
   return true;
 }
 
-}  // namespace
-
 /******** ResourceUsageTracker ********/
 
-ResourceUsageTracker::ResourceUsageTracker() : start_time_(base::Time::Now()) {}
+// Tracks and logs system resource usage.
+class ResourceUsageTracker {
+ public:
+  ResourceUsageTracker() = default;
+  ~ResourceUsageTracker() = default;
 
-ResourceUsageTracker::~ResourceUsageTracker() {
-  base::Time end_time = base::Time::Now();
+  // Initializes states for tracking, should be called when program starts.
+  void Start() { start_time_ = base::Time::Now(); }
+
+  // Computes and prints usage. Should be called at end of program run, e.g.,
+  // using base::AtExitManager().
+  void Finish() {
+    base::Time end_time = base::Time::Now();
 
 #if !defined(OS_MACOSX)
-  std::unique_ptr<base::ProcessMetrics> process_metrics(
-      base::ProcessMetrics::CreateProcessMetrics(
-          base::GetCurrentProcessHandle()));
+    std::unique_ptr<base::ProcessMetrics> process_metrics(
+        base::ProcessMetrics::CreateProcessMetrics(
+            base::GetCurrentProcessHandle()));
 
-  LOG(INFO) << "Zucchini.PeakPagefileUsage "
-            << process_metrics->GetPeakPagefileUsage() / 1024 << " KiB";
-  LOG(INFO) << "Zucchini.PeakWorkingSetSize "
-            << process_metrics->GetPeakWorkingSetSize() / 1024 << " KiB";
+    LOG(INFO) << "Zucchini.PeakPagefileUsage "
+              << process_metrics->GetPeakPagefileUsage() / 1024 << " KiB";
+    LOG(INFO) << "Zucchini.PeakWorkingSetSize "
+              << process_metrics->GetPeakWorkingSetSize() / 1024 << " KiB";
 #endif  // !defined(OS_MACOSX)
 
-  LOG(INFO) << "Zucchini.TotalTime " << (end_time - start_time_).InSecondsF()
-            << " s";
-}
+    LOG(INFO) << "Zucchini.TotalTime " << (end_time - start_time_).InSecondsF()
+              << " s";
+  }
+
+ private:
+  base::Time start_time_;
+};
+
+}  // namespace
+
+namespace command {
 
 /******** Command ********/
 
-Command::Command(const char* name_in,
-                 const char* usage_in,
+Command::Command(const char* const name_in,
+                 const char* const usage_in,
                  int num_args_in,
-                 Command::Fun fun_in)
-    : name(name_in), usage(usage_in), num_args(num_args_in), fun(fun_in) {}
+                 CommandFunction* command_function_in)
+    : name(name_in),
+      usage(usage_in),
+      num_args(num_args_in),
+      command_function(command_function_in) {}
 
 Command::Command(const Command&) = default;
 
@@ -68,64 +86,61 @@ Command::~Command() = default;
 
 /******** CommandRegistry ********/
 
-CommandRegistry::CommandRegistry() = default;
+CommandRegistry::CommandRegistry(std::ostream* out) : out_(out) {
+  // Register all command.
+  commands_.push_back({"gen", "-gen <old_file> <new_file> <patch_file>",
+                       kGenNumFileParams, Gen});
+  commands_.push_back({"apply", "-apply <old_file> <patch_file> <new_file>",
+                       kApplyNumFileParams, Apply});
+}
 
 CommandRegistry::~CommandRegistry() = default;
 
-void CommandRegistry::Register(const Command* command) {
-  commands_.push_back(command);
-}
-
-void CommandRegistry::RunOrExit(const base::CommandLine& command_line) {
+zucchini::status::Code CommandRegistry::Run(
+    const base::CommandLine& command_line) {
   const Command* command_use = nullptr;
-  for (const Command* command : commands_) {
-    if (command_line.HasSwitch(command->name)) {
+  for (const Command& command : commands_) {
+    if (command_line.HasSwitch(command.name)) {
       if (command_use) {        // Too many commands found.
         command_use = nullptr;  // Set to null to flag error.
         break;
       }
-      command_use = command;
+      command_use = &command;
     }
   }
 
-  // If we don't have exactly one matching command, print error and exit.
+  // If we don't have exactly one matching command, print usage and quit.
   if (!command_use) {
-    std::cerr << "Must have exactly one of:\n  [";
+    *out_ << "Must have exactly one of:\n  [";
     zucchini::PrefixSep sep(", ");
-    for (const Command* command : commands_)
-      std::cerr << sep << "-" << command->name;
-    std::cerr << "]" << std::endl;
-    PrintUsageAndExit();
+    for (const Command& command : commands_)
+      *out_ << sep << "-" << command.name;
+    *out_ << "]" << std::endl;
+    PrintUsage();
+    return zucchini::status::kStatusMissingCommand;
   }
 
+  // Try to parse filename arguments. On failure, print usage and quit.
   std::vector<base::FilePath> fnames;
-  if (CheckAndGetFilePathParams(command_line, command_use->num_args, &fnames)) {
-    command_use->fun.Run(command_line, fnames);
-  } else {
-    std::cerr << command_use->usage << std::endl;
-    PrintUsageAndExit();
+  if (!CheckAndGetFilePathParams(command_line, command_use->num_args,
+                                 &fnames)) {
+    *out_ << command_use->usage << std::endl;
+    PrintUsage();
+    return zucchini::status::kStatusMissingFiles;
   }
+
+  ResourceUsageTracker resource_usage_tracker;
+  resource_usage_tracker.Start();
+  zucchini::status::Code exit_code =
+      command_use->command_function(command_line, fnames);
+  resource_usage_tracker.Finish();
+  return exit_code;
 }
 
-void CommandRegistry::PrintUsageAndExit() {
-  std::cerr << "Usage:" << std::endl;
-  for (const Command* command : commands_)
-    std::cerr << "  zucchini " << command->usage << std::endl;
-  exit(1);
+void CommandRegistry::PrintUsage() {
+  *out_ << "Usage:" << std::endl;
+  for (const Command& command : commands_)
+    *out_ << "  zucchini " << command.usage << std::endl;
 }
 
-/******** Command Definitions ********/
-
-Command kCommandGen = {
-    "gen", "-gen <old_file> <new_file> <patch_file>", 3,
-    base::Bind([](const base::CommandLine& command_line,
-                  const std::vector<base::FilePath>& fnames) -> void {
-      // TODO(etiennep): Implement.
-    })};
-
-Command kCommandApply = {
-    "apply", "-apply <old_file> <patch_file> <new_file>", 3,
-    base::Bind([](const base::CommandLine& command_line,
-                  const std::vector<base::FilePath>& fnames) -> void {
-      // TODO(etiennep): Implement.
-    })};
+}  // namespace command
--- chrome/installer/zucchini/zucchini_main.cc
+++ chrome/installer/zucchini/zucchini_main.cc
@@ -2,9 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <iostream>
+
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/process/memory.h"
+#include "build/build_config.h"
 #include "chrome/installer/zucchini/main_utils.h"
+
+#if defined(OS_WIN)
+#include "base/win/process_startup_helper.h"
+#endif  // defined(OS_WIN)
 
 namespace {
 
@@ -18,21 +26,26 @@ void InitLogging() {
   CHECK(logging_res);
 }
 
+void InitErroHandling(const base::CommandLine& command_line) {
+  base::EnableTerminationOnHeapCorruption();
+  base::EnableTerminationOnOutOfMemory();
+#if defined(OS_WIN)
+  base::win::RegisterInvalidParamHandler();
+  base::win::SetupCRT(command_line);
+#endif  // defined(OS_WIN)
+}
+
 }  // namespace
 
 int main(int argc, const char* argv[]) {
-  ResourceUsageTracker tracker;
+  // Initialize infrastructure from base.
   base::CommandLine::Init(argc, argv);
-  InitLogging();
-
   const base::CommandLine& command_line =
       *base::CommandLine::ForCurrentProcess();
+  InitLogging();
+  InitErroHandling(command_line);
 
-  // Instantiate Command registry and register Zucchini features.
-  CommandRegistry registry;
-  registry.Register(&kCommandGen);
-  registry.Register(&kCommandApply);
-
-  registry.RunOrExit(command_line);
-  return 0;
+  // Register and dispatch commands.
+  command::CommandRegistry registry(&std::cerr);
+  return static_cast<int>(registry.Run(command_line));
 }
