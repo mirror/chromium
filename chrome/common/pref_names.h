42c1db10aaea6ed93624ecf32d0781d24f9de9ed
Sonny Sasaka <sonnysasaka@chromium.org>
2017-07-24 16:11:56 -0700

    Implement bluetooth adapter per-user power setting
    Bug: 741962
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I82c1be50e706cd99cb11611e101141b3f257aca2

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -332,6 +332,7 @@ extern const char kPinUnlockWeakPinsAllowed[];
 extern const char kEnableQuickUnlockFingerprint[];
 extern const char kInstantTetheringAllowed[];
 extern const char kInstantTetheringEnabled[];
+extern const char kBluetoothAdapterEnabled[];
 #endif  // defined(OS_CHROMEOS)
 extern const char kShowHomeButton[];
 extern const char kSpeechRecognitionFilterProfanities[];
