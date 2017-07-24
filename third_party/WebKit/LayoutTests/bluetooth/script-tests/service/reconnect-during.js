61357dbe0647099c402b74012e204464d8c6a137
Conley Owens <cco3@chromium.org>
2017-07-24 15:00:24 -0700

    bluetooth: Use dynamic devices in reconnect tests
    This change uses getHealthThermometerDeviceWithServicesDiscovered to
    ensure that the physical connection to the device remains alive while we
    reconnect the particular WebBT device.
    
    BUG=569709
    
    Change-Id: I34e22397032aeb95a99e5ceab5697863c81c75c4

--- third_party/WebKit/LayoutTests/bluetooth/script-tests/service/reconnect-during.js
+++ third_party/WebKit/LayoutTests/bluetooth/script-tests/service/reconnect-during.js
@@ -1,25 +1,29 @@
 'use strict';
 promise_test(() => {
-  return setBluetoothFakeAdapter('HeartRateAdapter')
-    .then(() => requestDeviceWithKeyDown({
-      filters: [{services: ['heart_rate']}]}))
-    .then(device => device.gatt.connect())
-    .then(gatt => {
-      return gatt.getPrimaryService('heart_rate')
-        .then(service => {
-          let promise = assert_promise_rejects_with_message(
-            service.CALLS([
-              getCharacteristic('heart_rate_measurement')|
-              getCharacteristics()|
-              getCharacteristics('heart_rate_measurement')[UUID]
-            ]),
-            new DOMException(
-              'GATT Server is disconnected. Cannot retrieve characteristics. ' +
-              '(Re)connect first with `device.gatt.connect`.',
-              'NetworkError'));
-          gatt.disconnect();
-          return gatt.connect().then(() => promise);
-        });
+  let device, service, characteristic;
+  return getHealthThermometerDeviceWithServicesDiscovered({
+      filters: [{services: [health_thermometer.name]}],
+  })
+    .then(_ => ({device, service, characteristic} = _))
+    .then(() => assert_class_string(
+        characteristic, 'BluetoothRemoteGATTCharacteristic'))
+    .then(() => {
+      let promise = assert_promise_rejects_with_message(service.CALLS([
+        getCharacteristic(measurement_interval.name)|
+        getCharacteristics()|
+        getCharacteristics(measurement_interval.name)[UUID]
+      ]), new DOMException(
+          'GATT Server is disconnected. Cannot retrieve characteristics. ' +
+          '(Re)connect first with `device.gatt.connect`.',
+          'NetworkError'));
+      // connect() guarantees on OS-level connection, but disconnect()
+      // only disconnects the current instance.
+      // getHealthThermometerDeviceWithServicesDiscovered holds another
+      // connection in an iframe, so disconnect() and connect() are certain to
+      // reconnect.  However, disconnect() will invalidate the service object so
+      // the subsequent calls made to it will fail, even after reconnecting.
+      device.gatt.disconnect();
+      return device.gatt.connect().then(() => promise);
     });
 }, 'disconnect() and connect() called during FUNCTION_NAME. Reject with ' +
    'NetworkError.');
