6d2e80f5cd6356c71a405b7f1a217de1d390fe9c
Scott Haseley <shaseley@google.com>
2017-07-24 14:24:57 -0700

    Refactoring SwapMetricsObserver to support multiple observers.
    The current SwapMetricsObserver implementaton combines how and how often swap
    metrics are updated with the client side consumption of the metrics. This CL
    splits the computation and frequency of swap metrics with observers wishing to
    use the metrics, allowing a single implementation of platform-specific
    computation while enabling multiple independent observers.
    
    Much of SwapMetricsObserver has been moved to SwapMetricsDriver, while
    SwapMetricsObserver is now a typical observer interface. SwapMetricsDrivers
    handle driving periodic metrics updates and notifying observers. In this
    implementation, there is one 1-1 relationship between drivers and observers;
    each driver has one observer scoped at its lifetime. This is because the
    driver controls the frequency at which metrics are updated, which impacts the
    observer. We assume different observers will sample at different frequencies
    or over different intervals, which requires multiple driver instances.
    
    The existing periodic experimental UMAs have been moved to
    SwapMetricsObserverUma.
    
    Bug:chromium:740625
    
    Change-Id: I66d8cdf3c06ebadba31044ff203fdd803094157f

---
+++ content/browser/memory/swap_metrics_driver_linux.cc
@@ -0,0 +1,65 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/memory/swap_metrics_driver_linux.h"
+
+#include <memory>
+
+#include "base/memory/ptr_util.h"
+#include "base/process/process_metrics.h"
+#include "base/time/time.h"
+#include "content/browser/memory/swap_metrics_driver.h"
+
+namespace content {
+
+namespace {
+
+bool HasSwap() {
+  base::SystemMemoryInfoKB memory_info;
+  if (!base::GetSystemMemoryInfo(&memory_info))
+    return false;
+  return memory_info.swap_total > 0;
+}
+
+}  // namespace
+
+// static
+std::unique_ptr<SwapMetricsDriver> SwapMetricsDriver::Create(
+    std::unique_ptr<Delegate> delegate,
+    const base::TimeDelta update_interval) {
+  return HasSwap()
+             ? base::WrapUnique<SwapMetricsDriver>(new SwapMetricsDriverLinux(
+                   std::move(delegate), update_interval))
+             : std::unique_ptr<SwapMetricsDriver>();
+}
+
+SwapMetricsDriverLinux::SwapMetricsDriverLinux(
+    std::unique_ptr<Delegate> delegate,
+    const base::TimeDelta update_interval)
+    : SwapMetricsDriver(std::move(delegate), update_interval) {}
+
+SwapMetricsDriverLinux::~SwapMetricsDriverLinux() = default;
+
+SwapMetricsUpdateResult SwapMetricsDriverLinux::UpdateMetricsInternal(
+    base::TimeDelta interval) {
+  base::SystemMemoryInfoKB memory_info;
+  if (!base::GetSystemMemoryInfo(&memory_info)) {
+    return SwapMetricsUpdateResult::kSwapMetricsUpdateFailed;
+  }
+
+  uint64_t in_counts = memory_info.pswpin - last_pswpin_;
+  uint64_t out_counts = memory_info.pswpout - last_pswpout_;
+  last_pswpin_ = memory_info.pswpin;
+  last_pswpout_ = memory_info.pswpout;
+
+  if (interval.is_zero())
+    return SwapMetricsUpdateResult::kSwapMetricsUpdateSuccess;
+
+  delegate_->OnSwapInCount(in_counts, interval);
+  delegate_->OnSwapOutCount(out_counts, interval);
+
+  return SwapMetricsUpdateResult::kSwapMetricsUpdateSuccess;
+}
+
+}  // namespace content
