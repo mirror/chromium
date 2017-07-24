56205f090f265b17cce5325e71df4a6faa376ea7
Yoshifumi Inoue <yosin@chromium.org>
2017-07-24 18:02:19 +0900

    WIP Introduce SelectionInUndoStep
    2017-07-24T17:54:58
    
    2017-07-24T17:54:53
    Bug: 747795
    Change-Id: I541fe20f9d8334fc71bbf7bb5988d3a03ae80960

---
+++ third_party/WebKit/Source/core/editing/commands/SelectionInUndoStep.h
@@ -0,0 +1,74 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SelectionInUndoStep_h
+#define SelectionInUndoStep_h
+
+#include "base/macros.h"
+#include "core/editing/Position.h"
+#include "core/editing/SelectionTemplate.h"
+#include "core/editing/VisiblePosition.h"
+#include "core/editing/VisibleSelection.h"
+#include "platform/heap/Handle.h"
+
+namespace blink {
+
+class Element;
+enum class TextAffinity;
+
+// This class represents selection in |UndoStep|.
+class SelectionInUndoStep final
+    : public GarbageCollectedFinalized<SelectionInUndoStep> {
+ public:
+  static SelectionInUndoStep From(const SelectionInDOMTree&);
+
+  SelectionInUndoStep(const SelectionInUndoStep&);
+  SelectionInUndoStep();
+
+  SelectionInUndoStep& operator=(const SelectionInUndoStep&);
+
+  bool operator==(const SelectionInUndoStep&) const;
+  bool operator!=(const SelectionInUndoStep&) const;
+
+  TextAffinity Affinity() const { return affinity_; }
+  SelectionInDOMTree AsSelection() const;
+  Position Base() const { return base_; }
+  Position Extent() const { return extent_; }
+  bool IsBaseFirst() const;
+  bool IsDirectional() const { return is_directional_; }
+
+  // TODO(editing-dev): We should rename to |ComputeStart()|.
+  Position Start() const;
+  // TODO(editing-dev): We should rename to |ComputeEnd()|.
+  Position End() const;
+
+  // TODO(editing-dev): We should move following funcitons out side
+  // |SelectionInUndoStep| class.
+  bool IsCaret() const;
+  bool IsNone() const;
+  bool IsRange() const;
+  bool IsNonOrphanedCaretOrRange() const;
+  bool IsNonOrphanedRange() const;
+  Element* RootEditableElement() const;
+  VisiblePosition VisibleStart() const;
+  VisiblePosition VisibleEnd() const;
+
+  DECLARE_TRACE();
+
+ private:
+  void EnsureStartAndEnd() const;
+
+  Position base_;
+  Position extent_;
+  TextAffinity affinity_;
+  mutable bool is_base_first_ = false;
+  bool is_directional_ = false;
+  mutable bool should_compute_is_base_first_ = false;
+};
+
+VisibleSelection CreateVisibleSelection(const SelectionInUndoStep&);
+
+}  // namespace blink
+
+#endif  // SelectionInUndoStep_h
