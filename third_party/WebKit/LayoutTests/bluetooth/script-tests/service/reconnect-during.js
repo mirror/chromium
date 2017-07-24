3535a8a5a248d59a95320af2e2fa21b2e0b31257
Conley Owens <cco3@chromium.org>
2017-07-24 15:00:24 -0700

    Use dynamic devices in reconnect-during WebBT test
    BUG=569709
    
    Change-Id: I34e22397032aeb95a99e5ceab5697863c81c75c4

--- third_party/WebKit/LayoutTests/bluetooth/script-tests/service/reconnect-during.js
+++ third_party/WebKit/LayoutTests/bluetooth/script-tests/service/reconnect-during.js
@@ -1,25 +1,25 @@
 'use strict';
 promise_test(() => {
-  return setBluetoothFakeAdapter('HeartRateAdapter')
-    .then(() => requestDeviceWithKeyDown({
-      filters: [{services: ['heart_rate']}]}))
-    .then(device => device.gatt.connect())
-    .then(gatt => {
-      return gatt.getPrimaryService('heart_rate')
-        .then(service => {
-          let promise = assert_promise_rejects_with_message(
-            service.CALLS([
-              getCharacteristic('heart_rate_measurement')|
-              getCharacteristics()|
-              getCharacteristics('heart_rate_measurement')[UUID]
-            ]),
-            new DOMException(
-              'GATT Server is disconnected. Cannot retrieve characteristics. ' +
-              '(Re)connect first with `device.gatt.connect`.',
-              'NetworkError'));
-          gatt.disconnect();
-          return gatt.connect().then(() => promise);
-        });
+  let device, fake_peripheral, service, fake_service;
+  return getHealthThermometerDeviceWithServicesDiscovered({
+      filters: [{services: [health_thermometer.name]}]
+  })
+    .then(_ => ({device, fake_peripheral} = _))
+    .then(() => fake_peripheral.addFakeService({uuid: health_thermometer.name}))
+    .then(s => (fake_service = s))
+    .then(() => device.gatt.getPrimaryService('health_thermometer'))
+    .then(s => service = s)
+    .then(() => {
+      let promise = assert_promise_rejects_with_message(service.CALLS([
+        getCharacteristic('heart_rate_measurement')|
+        getCharacteristics()|
+        getCharacteristics('heart_rate_measurement')[UUID]
+      ]), new DOMException(
+          'GATT Server is disconnected. Cannot retrieve characteristics. ' +
+          '(Re)connect first with `device.gatt.connect`.',
+          'NetworkError'));
+      device.gatt.disconnect();
+      return device.gatt.connect().then(() => promise);
     });
 }, 'disconnect() and connect() called during FUNCTION_NAME. Reject with ' +
    'NetworkError.');
