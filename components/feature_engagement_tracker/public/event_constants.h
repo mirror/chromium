5f726f31212b59ba85c40380cd30e60119bbaeed
Christopher Benson <chrisbenson@google.com>
2017-07-24 15:08:23 -0700

    Added Bookmark feature in IPH
    Changes this Patch:
    - Added event constants for Bookmarks in FeatureEngagementTracker.
    - Added the BookmarkInProductHelp trial to the fieldtrial json.
    - Created a BookmarkFeatureEngagementTracker class, which contains
    methods to notify the IPH server when events happen and if the help
    UI should be triggered.
    
    Design Doc: https://docs.google.com/document/d/18rbCr2MOM_hbdw3Swg9tIJvNUEeQpezi1vd0dDYls6g/edit#heading=h.70pdiwsg2by8
    
    Bug: 734132
    Change-Id: Ia5aa7502cc6245bb3b7eb6ef4c9148c8d098838f

--- components/feature_engagement_tracker/public/event_constants.h
+++ components/feature_engagement_tracker/public/event_constants.h
@@ -12,22 +12,31 @@ namespace feature_engagement_tracker {
 namespace events {
 
 #if defined(OS_WIN) || defined(OS_LINUX)
-// All the events declared below are the string names
-// of deferred onboarding events for the New Tab.
+// All the events declared below are the string names of deferred onboarding
+// events for the Bookmark feature.
+
+// The user has added a Bookmark (one-off event).
+extern const char kBookmarkAdded[];
+
+// The user has visited a URL more than once.
+extern const char kVisitedKnownURL[];
+
+// All the events declared below are the string names of deferred onboarding
+// events for the New Tab.
 
 // The user has interacted with the omnibox.
 extern const char kOmniboxInteraction[];
 
-// All the events declared below are the string names
-// of deferred onboarding events for the Incognito Window
+// All the events declared below are the string names of deferred onboarding
+// events for the Incognito Window.
 
 // The user has deleted browsing history.
 extern const char kHistoryDeleted[];
 // The user has opened an incognito window.
 extern const char kIncognitoWindowOpened[];
 
-// All the events declared below are the string names
-// of common deferred onboarding events
+// All the events declared below are the string names of common deferred
+// onboarding events.
 
 // The user has accumulated 2 hours of active session time (one-off event).
 extern const char kSessionTime[];
