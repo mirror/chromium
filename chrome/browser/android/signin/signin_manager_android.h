f6d6690ef84eac0398a2d2d827c1f79ce9a7d03b
Martin Sramek <msramek@chromium.org>
2017-07-25 13:46:05 +0200

    Ensure signing out deletes bookmarks only if full wipe is requested
    Due to a bug in https://codereview.chromium.org/2966763003/, bookmarks
    were being deleted when a non-managed user signed out, in which case
    only Google service workers should have been deleted.
    
    This CL fixes the problem and adds a unittest for bookmark deletion
    in both cases.
    
    Note that making bookmarks deletion testable required moving it into
    ProfileDataRemover where the rest of the deletion occurs. However,
    it happens on the same thread, and the ordering does not matter; and
    in any case, this is exactly the part is tested.
    
    Bug:747155
    Change-Id: I857e52cd4835d95e712e205c8c436adec73d1789

--- chrome/browser/android/signin/signin_manager_android.h
+++ chrome/browser/android/signin/signin_manager_android.h
@@ -94,6 +94,7 @@ class SigninManagerAndroid : public SigninManagerBase::Observer {
  private:
   FRIEND_TEST_ALL_PREFIXES(SigninManagerAndroidTest,
                            DeleteGoogleServiceWorkerCaches);
+  FRIEND_TEST_ALL_PREFIXES(SigninManagerAndroidTest, Bookmarks);
 
   ~SigninManagerAndroid() override;
 
@@ -102,9 +103,8 @@ class SigninManagerAndroid : public SigninManagerBase::Observer {
   void OnPolicyFetchDone(bool success);
 
   void OnBrowsingDataRemoverDone(
+      bool all_data,
       const base::android::ScopedJavaGlobalRef<jobject>& callback);
-
-  void ClearLastSignedInUser();
 
   void OnSigninAllowedPrefChanged();
 
