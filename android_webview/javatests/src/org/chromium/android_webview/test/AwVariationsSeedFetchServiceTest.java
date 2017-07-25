c755908dacbc377ce0fe419e67153fb62295fac0
Yiyun Yao <yiyuny@google.com>
2017-07-24 19:33:39 -0700

    Add AwVariationsConfigurationService
    Bug:
    Change-Id: Iafba941f32522bf1e54dc519ac8343f4938e9432

--- android_webview/javatests/src/org/chromium/android_webview/test/AwVariationsSeedFetchServiceTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwVariationsSeedFetchServiceTest.java
@@ -6,6 +6,7 @@ package org.chromium.android_webview.test;
 
 import android.annotation.TargetApi;
 import android.os.Build;
+import android.os.ParcelFileDescriptor;
 import android.support.test.filters.MediumTest;
 import android.test.InstrumentationTestCase;
 
@@ -13,8 +14,10 @@ import org.chromium.android_webview.AwVariationsSeedFetchService;
 import org.chromium.android_webview.AwVariationsSeedFetchService.SeedPreference;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
+import org.chromium.components.variations.firstrun.VariationsSeedFetcher;
 import org.chromium.components.variations.firstrun.VariationsSeedFetcher.SeedInfo;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -46,13 +49,16 @@ public class AwVariationsSeedFetchServiceTest extends InstrumentationTestCase {
         seedInfo.isGzipCompressed = true;
         AwVariationsSeedFetchService.storeVariationsSeed(seedInfo);
 
-        byte[] seedData = AwVariationsSeedFetchService.getVariationsSeedData();
-        assertTrue(Arrays.equals(seedData, seedInfo.seedData));
+        ParcelFileDescriptor fd =
+                AwVariationsSeedFetchService.getVariationsSeedDataFileDescriptor();
+        byte[] seedData = VariationsSeedFetcher.convertInputStreamToByteArray(
+                new FileInputStream(fd.getFileDescriptor()));
+        assertTrue(Arrays.equals(seedInfo.seedData, seedData));
 
         SeedPreference seedPreference = AwVariationsSeedFetchService.getVariationsSeedPreference();
-        assertEquals(seedPreference.signature, seedInfo.signature);
-        assertEquals(seedPreference.country, seedInfo.country);
-        assertEquals(seedPreference.date, seedInfo.date);
-        assertEquals(seedPreference.isGzipCompressed, seedInfo.isGzipCompressed);
+        assertEquals(seedInfo.signature, seedPreference.signature);
+        assertEquals(seedInfo.country, seedPreference.country);
+        assertEquals(seedInfo.date, seedPreference.date);
+        assertEquals(seedInfo.isGzipCompressed, seedPreference.isGzipCompressed);
     }
 }
