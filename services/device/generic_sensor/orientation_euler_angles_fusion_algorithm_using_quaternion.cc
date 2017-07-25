522ac4e432ff22c29761821ce69ec5bbc1bc950f
Jun Cai <juncai@chromium.org>
2017-07-25 16:05:19 -0700

    Implement device orientation fusion sensors on Android
    This CL implements device orientation fusion sensors on Android. The
    following sensor fallback is used:
    
    1. For ABSOLUTE_ORIENTATION_EULER_ANGLES we use the following fallback:
       A: ABSOLUTE_ORIENTATION_QUATERNION (if it uses TYPE_ROTATION_VECTOR
    directly)
       B: Combination of ACCELEROMETER and MAGNETOMETER
    
    2. For ABSOLUTE_ORIENTATION_QUATERNION we use the following fallback:
       A: Use TYPE_ROTATION_VECTOR directly
       B: ABSOLUTE_ORIENTATION_EULER_ANGLES
    
    3. For RELATIVE_ORIENTATION_EULER_ANGLES we use the following fallback:
       A: RELATIVE_ORIENTATION_QUATERNION (if it uses
          TYPE_GAME_ROTATION_VECTOR directly)
       B: ACCELEROMETER
    
    4. For RELATIVE_ORIENTATION_QUATERNION we use the following fallback:
       A: Use TYPE_GAME_ROTATION_VECTOR directly
       B: RELATIVE_ORIENTATION_EULER_ANGLES
    
    Bug: 733707
    Change-Id: Ic201f450487b586132952cbffae064682e93a10c

---
+++ services/device/generic_sensor/orientation_euler_angles_fusion_algorithm_using_quaternion.cc
@@ -0,0 +1,92 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/device/generic_sensor/orientation_euler_angles_fusion_algorithm_using_quaternion.h"
+
+#include <cmath>
+
+#include "services/device/generic_sensor/orientation_util.h"
+
+namespace {
+
+// Helper function to convert a quaternion to a rotation matrix. x, y, z, w
+// are values of a quaternion representing the orientation of the device in
+// 3D space. Returns a 9 element rotation matrix:
+// r[ 0]   r[ 1]   r[ 2]
+// r[ 3]   r[ 4]   r[ 5]
+// r[ 6]   r[ 7]   r[ 8]
+std::vector<double> ComputeRotationMatrixFromQuaternion(double x,
+                                                        double y,
+                                                        double z,
+                                                        double w) {
+  std::vector<double> r(9);
+
+  double sq_x = 2 * x * x;
+  double sq_y = 2 * y * y;
+  double sq_z = 2 * z * z;
+  double x_y = 2 * x * y;
+  double z_w = 2 * z * w;
+  double x_z = 2 * x * z;
+  double y_w = 2 * y * w;
+  double y_z = 2 * y * z;
+  double x_w = 2 * x * w;
+
+  r[0] = 1 - sq_y - sq_z;
+  r[1] = x_y - z_w;
+  r[2] = x_z + y_w;
+  r[3] = x_y + z_w;
+  r[4] = 1 - sq_x - sq_z;
+  r[5] = y_z - x_w;
+  r[6] = x_z - y_w;
+  r[7] = y_z + x_w;
+  r[8] = 1 - sq_x - sq_y;
+
+  return r;
+}
+
+void ComputeEulerAnglesFromQuaternion(double x,
+                                      double y,
+                                      double z,
+                                      double w,
+                                      double* alpha,
+                                      double* beta,
+                                      double* gamma) {
+  std::vector<double> rotation_matrix =
+      ComputeRotationMatrixFromQuaternion(x, y, z, w);
+  device::ComputeOrientationEulerAnglesFromRotationMatrix(rotation_matrix,
+                                                          alpha, beta, gamma);
+}
+
+}  // namespace
+
+namespace device {
+
+OrientationEulerAnglesFusionAlgorithmUsingQuaternion::
+    OrientationEulerAnglesFusionAlgorithmUsingQuaternion() {}
+
+OrientationEulerAnglesFusionAlgorithmUsingQuaternion::
+    ~OrientationEulerAnglesFusionAlgorithmUsingQuaternion() = default;
+
+bool OrientationEulerAnglesFusionAlgorithmUsingQuaternion::GetFusedData(
+    const std::vector<SensorReading>& readings,
+    SensorReading* fused_reading) {
+  // Transform the *_ORIENTATION_QUATERNION values to
+  // *_ORIENTATION_EULER_ANGLES.
+  DCHECK(readings.size() == 1);
+
+  double x = readings[0].values[0].value();
+  double y = readings[0].values[1].value();
+  double z = readings[0].values[2].value();
+  double w = readings[0].values[3].value();
+  double alpha, beta, gamma;
+  ComputeEulerAnglesFromQuaternion(x, y, z, w, &alpha, &beta, &gamma);
+  fused_reading->values[0].value() = beta;
+  fused_reading->values[1].value() = gamma;
+  fused_reading->values[2].value() = alpha;
+  fused_reading->values[3].value() = 0.0;
+
+  return true;
+}
+
+}  // namespace device
