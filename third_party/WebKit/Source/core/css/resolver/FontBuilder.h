08749c40c3f6bf3cfea032385baadfcb360f4c30
Bugs Nash <bugsnash@chromium.org>
2017-07-25 14:13:58 +1000

    Replaced usage of PassRefPtr with RefPtr in Source/core
    This patch
    - replaces most uses of PassRefPtr with RefPtr in Source/core
      (excepting cases involving other type conversions)
    - changes PassRefPtr to RefPtr in a virtual methods outside of
      Source/core that are overridden by methods changed in this patch
    
    Bug: 494719
    Change-Id: I4316587c40cd9e7ef7f75cd1179a77e4bb05bdae

--- third_party/WebKit/Source/core/css/resolver/FontBuilder.h
+++ third_party/WebKit/Source/core/css/resolver/FontBuilder.h
@@ -30,7 +30,7 @@
 #include "platform/fonts/FontDescription.h"
 #include "platform/fonts/FontVariantNumeric.h"
 #include "platform/heap/Handle.h"
-#include "platform/wtf/PassRefPtr.h"
+#include "platform/wtf/RefPtr.h"
 
 namespace blink {
 
@@ -61,8 +61,8 @@ class CORE_EXPORT FontBuilder {
   void SetSizeAdjust(const float aspect_value);
   void SetStretch(FontStretch);
   void SetFamilyDescription(const FontDescription::FamilyDescription&);
-  void SetFeatureSettings(PassRefPtr<FontFeatureSettings>);
-  void SetLocale(PassRefPtr<const LayoutLocale>);
+  void SetFeatureSettings(RefPtr<FontFeatureSettings>);
+  void SetLocale(RefPtr<const LayoutLocale>);
   void SetStyle(FontStyle);
   void SetVariantCaps(FontDescription::FontVariantCaps);
   void SetVariantLigatures(const FontDescription::VariantLigatures&);
@@ -70,7 +70,7 @@ class CORE_EXPORT FontBuilder {
   void SetTextRendering(TextRenderingMode);
   void SetKerning(FontDescription::Kerning);
   void SetFontSmoothing(FontSmoothingMode);
-  void SetVariationSettings(PassRefPtr<FontVariationSettings>);
+  void SetVariationSettings(RefPtr<FontVariationSettings>);
 
   // FIXME: These need to just vend a Font object eventually.
   void UpdateFontDescription(FontDescription&,
