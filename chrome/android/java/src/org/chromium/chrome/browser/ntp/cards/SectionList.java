171f6c8075cc2e0d8b58c45e0272c5d0909858fc
Tim Schumann <tschumann@chromium.org>
2017-07-25 10:30:17 +0200

    Take card dismissals into account when maintaining the number of seen cards.
    The recent change to issue a fetch instead of a fetch-more surfaced
    some side-effects with how already seen surfaces get updated.
    When dismissing a card, we did not adjust the number of seen cards
    which lead to a situation where the append logic thinks more cards are
    present than actually are. The result is an empty or not fully
    populated list.
    
    Bug: 728570
    Change-Id: I010c92b470f59a54fb66fd87cb60e7bbb5f74521

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/SectionList.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/SectionList.java
@@ -116,7 +116,7 @@ public class SectionList
 
         // Set the new suggestions.
         section.setStatus(categoryStatus);
-        section.appendSuggestions(suggestions, /* userRequested = */ false);
+        section.appendSuggestions(suggestions, /*sectionSizeExtended=*/false);
         return suggestions.size();
     }
 
