0f1d4f91e4b1803ae4e57dfb73cf1b797776e356
Charles Harrison <csharrison@chromium.org>
2017-07-25 08:46:10 -0400

    Remove the DocumentWriteEvaluator
    This optimization speculatively evaluates script on the main thread before it would
    otherwise be run. The javascript environment is sparse, with only a few globals like
    "location".
    
    The entire point of the evaluation is to see what the script will document.write, and
    pass *that* back into the preload scanner. This is generally used to preload ad
    script like gpt.js.
    
    The experimental results on 5% stable were good, showing 5-7% reduction of parse time on
    affected pages. However, user perceived metrics did not move as much.
    
    Bug: 705806,575850,746961
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ie61499876dc883c2efa33d880cad17b7d608a53e

--- third_party/WebKit/public/platform/WebRuntimeFeatures.h
+++ third_party/WebKit/public/platform/WebRuntimeFeatures.h
@@ -80,7 +80,6 @@ class WebRuntimeFeatures {
   BLINK_PLATFORM_EXPORT static void EnableColorCorrectRendering(bool);
   BLINK_PLATFORM_EXPORT static void EnableDatabase(bool);
   BLINK_PLATFORM_EXPORT static void EnableDecodeToYUV(bool);
-  BLINK_PLATFORM_EXPORT static void EnableDocumentWriteEvaluator(bool);
   BLINK_PLATFORM_EXPORT static void EnableExperimentalCanvasFeatures(bool);
   BLINK_PLATFORM_EXPORT static void EnableFastMobileScrolling(bool);
   BLINK_PLATFORM_EXPORT static void EnableFeaturePolicy(bool);
