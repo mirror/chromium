241b6692e653fff29d5eea93b2b13b4ac472a156
Yoshifumi Inoue <yosin@chromium.org>
2017-07-25 13:57:54 +0900

    Get rid of VisbileSelectionTemplate::start_ and end_
    2017-07-21T18:51:50
    
    2017-07-21T18:51:13
    Bug:
    Change-Id: I6a99b10540d753b37302d4b815626014dc75341d

--- third_party/WebKit/Source/core/editing/VisibleSelection.h
+++ third_party/WebKit/Source/core/editing/VisibleSelection.h
@@ -68,16 +68,16 @@ class CORE_TEMPLATE_CLASS_EXPORT VisibleSelectionTemplate {
   SelectionTemplate<Strategy> AsSelection() const;
   PositionTemplate<Strategy> Base() const { return base_; }
   PositionTemplate<Strategy> Extent() const { return extent_; }
-  PositionTemplate<Strategy> Start() const { return start_; }
-  PositionTemplate<Strategy> End() const { return end_; }
+  PositionTemplate<Strategy> Start() const;
+  PositionTemplate<Strategy> End() const;
 
   VisiblePositionTemplate<Strategy> VisibleStart() const {
     return CreateVisiblePosition(
-        start_, IsRange() ? TextAffinity::kDownstream : Affinity());
+        Start(), IsRange() ? TextAffinity::kDownstream : Affinity());
   }
   VisiblePositionTemplate<Strategy> VisibleEnd() const {
     return CreateVisiblePosition(
-        end_, IsRange() ? TextAffinity::kUpstream : Affinity());
+        End(), IsRange() ? TextAffinity::kUpstream : Affinity());
   }
   VisiblePositionTemplate<Strategy> VisibleBase() const {
     return CreateVisiblePosition(
@@ -157,10 +157,6 @@ class CORE_TEMPLATE_CLASS_EXPORT VisibleSelectionTemplate {
   PositionTemplate<Strategy> base_;
   // Where the end click happened
   PositionTemplate<Strategy> extent_;
-  // Leftmost position when expanded to respect granularity
-  PositionTemplate<Strategy> start_;
-  // Rightmost position when expanded to respect granularity
-  PositionTemplate<Strategy> end_;
 
   TextAffinity affinity_;  // the upstream/downstream affinity of the caret
 
