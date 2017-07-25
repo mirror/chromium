3ec93470ea4045bf6ab895950f809cf253705dcf
Avi Drissman <avi@chromium.org>
2017-07-25 15:40:39 -0400

    Create a UI state of "tab needs attention".
    This is a variation on the UI indication that a pinned tab has changed its title, but is triggerable from the browser.
    
    BUG=747117
    
    Change-Id: Ibda92eb1dd709e989b5de12f8d9b61188294649b

--- chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
@@ -1723,6 +1723,16 @@ NSRect FlipRectInView(NSView* view, NSRect rect) {
   [self layoutTabs];
 }
 
+- (void)tabNeedsAttentionAt:(NSInteger)modelIndex {
+  // Take closing tabs into account.
+  NSInteger index = [self indexFromModelIndex:modelIndex];
+
+  TabController* tabController = [tabArray_ objectAtIndex:index];
+  DCHECK([tabController isKindOfClass:[TabController class]]);
+
+  [tabController setNeedsAttention];
+}
+
 - (void)setFrame:(NSRect)frame ofTabView:(NSView*)view {
   NSValue* identifier = [NSValue valueWithPointer:view];
   [targetFrames_ setObject:[NSValue valueWithRect:frame]
