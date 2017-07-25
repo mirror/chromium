2be2ddb1c9ad5f17c31ecef4ed88010cf5ade2b7
Yoshifumi Inoue <yosin@chromium.org>
2017-07-25 13:28:15 +0900

    WIP ALL-IN-ONE Introduce SetSelectionData for FrameSelection::SetSelection()
    2017-07-13T16:07:48
    
    2017-07-13T15:49:49
    Bug: 742093
    Change-Id: I4f16011153340960e6b5e713c3b130c8d2d61fe2

--- third_party/WebKit/Source/core/editing/FrameSelection.h
+++ third_party/WebKit/Source/core/editing/FrameSelection.h
@@ -49,15 +49,13 @@ class GranularityStrategy;
 class GraphicsContext;
 class Range;
 class SelectionEditor;
+class SetSelectionData;
 class LayoutSelection;
 enum class SelectionModifyAlteration;
 enum class SelectionModifyDirection;
+enum class SetSelectionBy;
 class TextIteratorBehavior;
 struct PaintInvalidatorContext;
-
-enum class CursorAlignOnScroll { kIfNeeded, kAlways };
-
-enum class SetSelectionBy { kSystem = 0, kUser = 1 };
 
 enum RevealExtentOption { kRevealExtent, kDoNotRevealExtent };
 
@@ -77,28 +75,6 @@ class CORE_EXPORT FrameSelection final
   }
   ~FrameSelection();
 
-  enum SetSelectionOption {
-    kUserTriggered = 1,
-    kCloseTyping = 1 << 1,
-    kClearTypingStyle = 1 << 2,
-    kDoNotSetFocus = 1 << 3,
-    kDoNotClearStrategy = 1 << 4,
-  };
-  static_assert(kUserTriggered == static_cast<int>(SetSelectionBy::kUser),
-                "|kUserTriggered| should equal to |SetSelectionBy::kUser|");
-
-  // Union of values in SetSelectionOption and SetSelectionBy
-  typedef unsigned SetSelectionOptions;
-  static constexpr SetSelectionBy ConvertSelectionOptionsToSetSelectionBy(
-      SetSelectionOptions options) {
-    return static_cast<SetSelectionBy>(options &
-                                       static_cast<int>(SetSelectionBy::kUser));
-  }
-  static constexpr SetSelectionOptions
-  ConvertSetSelectionByToSetSelectionOptions(SetSelectionBy set_selection_by) {
-    return static_cast<SetSelectionOptions>(set_selection_by);
-  }
-
   bool IsAvailable() const { return LifecycleContext(); }
   // You should not call |document()| when |!isAvailable()|.
   Document& GetDocument() const;
@@ -117,10 +93,8 @@ class CORE_EXPORT FrameSelection final
   // layout.
   const VisibleSelection& ComputeVisibleSelectionInDOMTreeDeprecated() const;
 
-  void SetSelection(const SelectionInDOMTree&,
-                    SetSelectionOptions = kCloseTyping | kClearTypingStyle,
-                    CursorAlignOnScroll = CursorAlignOnScroll::kIfNeeded,
-                    TextGranularity = TextGranularity::kCharacter);
+  void SetSelection(const SetSelectionData&);
+  void SetSelection(const SelectionInDOMTree&);
   void SelectAll(SetSelectionBy);
   void SelectAll();
   void Clear();
@@ -131,13 +105,8 @@ class CORE_EXPORT FrameSelection final
   // functions.
   // setSelectionDeprecated() returns true if didSetSelectionDeprecated() should
   // be called.
-  bool SetSelectionDeprecated(const SelectionInDOMTree&,
-                              SetSelectionOptions = kCloseTyping |
-                                                    kClearTypingStyle,
-                              TextGranularity = TextGranularity::kCharacter);
-  void DidSetSelectionDeprecated(
-      SetSelectionOptions = kCloseTyping | kClearTypingStyle,
-      CursorAlignOnScroll = CursorAlignOnScroll::kIfNeeded);
+  bool SetSelectionDeprecated(const SetSelectionData&);
+  void DidSetSelectionDeprecated(const SetSelectionData&);
 
   // Call this after doing user-triggered selections to make it easy to delete
   // the frame you entirely selected.
