8d2e6d6ff97df898731a4fa0d12e91e3de3c8945
kylechar <kylechar@chromium.org>
2017-07-25 11:14:03 -0400

    Add viz switches.
    Add a switches file to //components/viz/common. This file will contain
    switches that control viz related behaviour, which is the same idea as
    cc switches file. Move kEnableSurfaceSynchronization initially since it
    is very much of a viz specific switch. Add kEnableSurfaceReferences
    I'm adding to turn on surface references.
    
    Since viz internally isn't supposed to use the switches, add DEPS rule
    to disallow it. The switches are instead supposed to be read by whatever
    is instantiating the viz objects and then passed in.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I52f85d0e1d4c1d3b2dfae2a050fdcfa4edd3d71a

---
+++ components/viz/common/switches.h
@@ -0,0 +1,16 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_VIZ_COMMON_SWITCHES_H_
+#define COMPONENTS_VIZ_COMMON_SWITCHES_H_
+
+namespace switches {
+
+// Keep list in alphabetical order.
+extern const char kEnableSurfaceSynchronization[];
+extern const char kEnableSurfaceReferences[];
+
+}  // namespace switches
+
+#endif  // COMPONENTS_VIZ_COMMON_SWITCHES_H_
