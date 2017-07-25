b42e1f0ee39dfc472d06050ff26491cdb135bb54
Will Chen <chenwilliam@chromium.org>
2017-07-25 14:51:09 -0700

    DevTools: port inspector-test.js API to IntegrationTestRunner.js
    Primarily a copy of inspector-test.js with JsDocs and simple clean-up
    (e.g. removing unused parameters).
    
    This keeps the API 1:1 with the old test helpers to make the automatic
    test migration simple.
    
    Bug: 667560
    Change-Id: Ia4a1ce6f2068922b7ca479965b730f667b3a749c

---
+++ third_party/WebKit/Source/devtools/front_end/data_grid_test_runner/DataGridTestRunner.js
@@ -0,0 +1,78 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview using private properties isn't a Closure violation in tests.
+ * @suppress {accessControls}
+ */
+
+/**
+ * @param {!DataGrid.DataGrid} dataGrid
+ */
+TestRunner.dumpDataGrid = function(dataGrid) {
+  TestRunner.addResult(TestRunner.dumpDataGridIntoString(dataGrid));
+};
+
+/**
+ * @param {!DataGrid.DataGrid} dataGrid
+ * @return {string}
+ */
+TestRunner.dumpDataGridIntoString = function(dataGrid) {
+  var tableElement = dataGrid.element;
+  var textRows = [];
+  var textWidths = [];
+  var rows = tableElement.getElementsByTagName('tr');
+  for (var i = 0, row; row = rows[i]; ++i) {
+    if (!row.offsetHeight || !row.textContent)
+      continue;
+    var textCols = [];
+    var cols = row.getElementsByTagName('td');
+    for (var j = 0, col; col = cols[j]; ++j) {
+      if (!col.offsetHeight)
+        continue;
+      var index = textCols.length;
+      var content = col.textContent || (col.firstChild && col.firstChild.title) || '';
+      var text = padding(col) + content;
+      textWidths[index] = Math.max(textWidths[index] || 0, text.length);
+      textCols[index] = text;
+    }
+    if (textCols.length)
+      textRows.push(textCols);
+  }
+
+  /**
+   * @param {!Node} target
+   * @return {string}
+   */
+  function padding(target) {
+    var cell = target.enclosingNodeOrSelfWithNodeName('td');
+    if (!cell.classList.contains('disclosure'))
+      return '';
+    var node = dataGrid.dataGridNodeFromNode(target);
+    var spaces = (node ? node.depth : 0) * 2;
+    return Array(spaces + 1).join(' ');
+  }
+
+  /**
+   * @param {string} text
+   * @param {string} width
+   */
+  function alignText(text, width) {
+    var spaces = width - text.length;
+    return text + Array(spaces + 1).join(' ');
+  }
+
+  var output = [];
+  for (var i = 0; i < textRows.length; ++i) {
+    var line = '';
+    for (var j = 0; j < textRows[i].length; ++j) {
+      if (j)
+        line += ' | ';
+      line += alignText(textRows[i][j], textWidths[j]);
+    }
+    line += '|';
+    output.push(line);
+  }
+  return output.join('\n');
+};
