9b3fe61a296c8f896330d2a9c509d86361a73b64
Vladimir Levin <vmpstr@chromium.org>
2017-07-26 12:53:25 -0700

    Plumb the ability to store non-SkImage in PaintImage.
    This patch adds the ability for PaintImage to be backed by
    structure other than SkImage. In this patch, the following
    changes are made:
     - Add a PaintImageBuilder for partially setting some of
       the flags for use on the final PaintImage
     - Removes ImageForCurrentFrame from blink and replaces it
       with PopulateImageForCurrentFrame which takes a paint
       image builder instead.
     - Replaces SkImages constructed from paint records to
       store paint records directly on the PaintImage
     - Renames sk_image() to GetSkImage(), which generates an
       underlying SkImage and caches it on PaintImage
     - Exposes width/height and IsLazyGenerated from PaintImage
       so that access to the underlying SkImage is limited.
    
    Follow-up work:
     - Add ability to store a decoder on PaintImage
     - Stop using GetSkImage internally for things like
       width/height.
    
    R=chrishtr@chromium.org, khushalsagar@chromium.org
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I0b95e13d437d7a99ad80e2286c2bb0c58330a9b3

--- cc/tiles/checker_image_tracker.cc
+++ cc/tiles/checker_image_tracker.cc
@@ -84,8 +84,8 @@ CheckerImagingDecision GetLoadDecision(const PaintImage& image) {
 CheckerImagingDecision GetSizeDecision(const PaintImage& image,
                                        size_t max_bytes) {
   base::CheckedNumeric<size_t> checked_size = 4;
-  checked_size *= image.sk_image()->width();
-  checked_size *= image.sk_image()->height();
+  checked_size *= image.width();
+  checked_size *= image.height();
   size_t size = checked_size.ValueOrDefault(std::numeric_limits<size_t>::max());
 
   if (size < kMinImageSizeToCheckerBytes)
@@ -304,7 +304,7 @@ bool CheckerImageTracker::ShouldCheckerImage(const DrawImage& draw_image,
 
     TRACE_EVENT1(TRACE_DISABLED_BY_DEFAULT("cc.debug"),
                  "CheckerImageTracker::CheckerImagingDecision", "image_params",
-                 ToString(image_id, image.sk_image()->uniqueID(), decision));
+                 ToString(image_id, image.GetSkImage()->uniqueID(), decision));
   }
 
   // Update the decode state from the latest image we have seen. Note that it
@@ -372,11 +372,12 @@ void CheckerImageTracker::ScheduleNextImageDecode() {
     if (it->second.policy != DecodePolicy::ASYNC)
       continue;
 
-    draw_image = DrawImage(candidate, candidate.sk_image()->bounds(),
-                           it->second.filter_quality,
-                           SkMatrix::MakeScale(it->second.scale.width(),
-                                               it->second.scale.height()),
-                           it->second.color_space);
+    draw_image = DrawImage(
+        candidate, SkIRect::MakeWH(candidate.width(), candidate.height()),
+        it->second.filter_quality,
+        SkMatrix::MakeScale(it->second.scale.width(),
+                            it->second.scale.height()),
+        it->second.color_space);
     outstanding_image_decode_.emplace(candidate);
     break;
   }
