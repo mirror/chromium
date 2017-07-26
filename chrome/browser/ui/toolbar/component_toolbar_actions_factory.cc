fd68c8914346480c6eec705bdc58ad76f21130d9
Takumi Fujimoto <takumif@chromium.org>
2017-07-25 17:52:52 -0700

    Uninstall legacy Cast extensions during ExtensionService initialization
    The Cast extension has been updated to uninstall itself when run. However, that
    would never run if the extension is disabled. So we will uninstall the extension
    from the browser side as well.
    
    This CL also removes code that was set to unload the extension if it was
    enabled.
    
    Bug:739942
    Change-Id: Iaa034b8dc04ae579fb8566ae448b5ff6f4997490

--- chrome/browser/ui/toolbar/component_toolbar_actions_factory.cc
+++ chrome/browser/ui/toolbar/component_toolbar_actions_factory.cc
@@ -17,17 +17,13 @@
 #include "extensions/browser/extension_registry.h"
 
 // static
-const char ComponentToolbarActionsFactory::kCastBetaExtensionId[] =
-    "dliochdbjfkdbacpmhlcpmleaejidimm";
-const char ComponentToolbarActionsFactory::kCastExtensionId[] =
-    "boadgeojelhgndaghljhdicfkmllpafd";
 const char ComponentToolbarActionsFactory::kMediaRouterActionId[] =
     "media_router_action";
 
-ComponentToolbarActionsFactory::ComponentToolbarActionsFactory(Profile* profile)
-    : profile_(profile) {
-  if (media_router::MediaRouterEnabled(profile_) &&
-      MediaRouterActionController::IsActionShownByPolicy(profile_)) {
+ComponentToolbarActionsFactory::ComponentToolbarActionsFactory(
+    Profile* profile) {
+  if (media_router::MediaRouterEnabled(profile) &&
+      MediaRouterActionController::IsActionShownByPolicy(profile)) {
     initial_ids_.insert(kMediaRouterActionId);
   }
 }
@@ -65,24 +61,4 @@ ComponentToolbarActionsFactory::GetComponentToolbarActionForId(
 
   NOTREACHED();
   return std::unique_ptr<ToolbarActionViewController>();
-}
-
-void ComponentToolbarActionsFactory::UnloadMigratedExtensions(
-    ExtensionService* service,
-    extensions::ExtensionRegistry* registry) {
-  // TODO(takumif): Replace the unloading of Cast and Cast Beta extensions with
-  // uninstallation.
-  UnloadExtension(service, registry, kCastExtensionId);
-  UnloadExtension(service, registry, kCastBetaExtensionId);
-}
-
-void ComponentToolbarActionsFactory::UnloadExtension(
-    ExtensionService* service,
-    extensions::ExtensionRegistry* registry,
-    const std::string& extension_id) {
-  if (registry->enabled_extensions().Contains(extension_id)) {
-    service->UnloadExtension(
-        extension_id,
-        extensions::UnloadedExtensionReason::MIGRATED_TO_COMPONENT);
-  }
 }
