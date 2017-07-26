a5161e1d90543e86521e227203c657c296f3d95a
Yoshifumi Inoue <yosin@chromium.org>
2017-07-26 12:38:03 +0900

    Get rid of VisibleSelectionTemplate::HasEditableStyle()
    This patch gets rid of |VisibleSelectionTemplate::HasEditableStyle()|to
    simplify |VisibleSelectionTemplate| as a preparation for reducing usage of
    |VisibleSelection| for improving code health.
    
    Bug: 657237

--- third_party/WebKit/Source/core/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/core/editing/FrameSelection.cpp
@@ -494,8 +494,9 @@ bool FrameSelection::ShouldPaintCaret(const LayoutBlock& block) const {
   DCHECK_GE(GetDocument().Lifecycle().GetState(),
             DocumentLifecycle::kLayoutClean);
   bool result = frame_caret_->ShouldPaintCaret(block);
-  DCHECK(!result || (ComputeVisibleSelectionInDOMTree().IsCaret() &&
-                     ComputeVisibleSelectionInDOMTree().HasEditableStyle()));
+  DCHECK(!result ||
+         (ComputeVisibleSelectionInDOMTree().IsCaret() &&
+          IsEditablePosition(ComputeVisibleSelectionInDOMTree().Start())));
   return result;
 }
 
