e4d305f1dfd3d47d8beabdfbaf4fb4dce3bf372e
Daichi Hirono <hirono@chromium.org>
2017-07-26 14:45:36 +0900

    Delivers drag and drop methods from Surface to DataDevice.
    The CL lets Surface observe drag and drop events and delegates the events to
    DataDevice thorugh DataDeviceManager.
    
    Bug:b:31988797
    Test:None
    Change-Id: I9574cd86137922d7527bd297d182a7b45e632c9e

--- components/exo/data_event_dispatcher.cc
+++ components/exo/data_event_dispatcher.cc
@@ -6,6 +6,7 @@
 
 #include "base/logging.h"
 #include "components/exo/data_device.h"
+#include "ui/base/dragdrop/drag_drop_types.h"
 
 namespace exo {
 
@@ -21,6 +22,46 @@ void DataEventDispatcher::AddDataDevice(DataDevice* data_device) {
 void DataEventDispatcher::RemoveDataDevice(DataDevice* data_device) {
   DCHECK_EQ(data_devices_.count(data_device), 1u);
   data_devices_.erase(data_device);
+}
+
+void DataEventDispatcher::OnDragEntered(Surface* surface,
+                                        const ui::DropTargetEvent& event) {
+  for (DataDevice* device : data_devices_) {
+    if (!device->CanAcceptDataEventsForSurface(surface))
+      continue;
+    device->OnDragEntered(surface, event);
+  }
+}
+
+int DataEventDispatcher::OnDragUpdated(Surface* surface,
+                                       const ui::DropTargetEvent& event) {
+  int valid_operation = ui::DragDropTypes::DRAG_NONE;
+  for (DataDevice* device : data_devices_) {
+    if (!device->CanAcceptDataEventsForSurface(surface))
+      continue;
+    valid_operation = valid_operation | device->OnDragUpdated(event);
+  }
+  return valid_operation;
+}
+
+void DataEventDispatcher::OnDragExited(Surface* surface) {
+  for (DataDevice* device : data_devices_) {
+    if (!device->CanAcceptDataEventsForSurface(surface))
+      continue;
+    device->OnDragExited();
+  }
+}
+
+int DataEventDispatcher::OnPerformDrop(Surface* surface,
+                                       const ui::DropTargetEvent& event) {
+  for (DataDevice* device : data_devices_) {
+    if (!device->CanAcceptDataEventsForSurface(surface))
+      continue;
+    device->OnPerformDrop(event);
+  }
+  // TODO(hirono): Return the correct result instead of always returning
+  // DRAG_MOVE.
+  return ui::DragDropTypes::DRAG_MOVE;
 }
 
 }  // namespace exo
