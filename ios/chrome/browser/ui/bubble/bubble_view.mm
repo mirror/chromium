1ee45c87bfc36036b1558aed5bff2d236995e585
Helen Yang <helenlyang@google.com>
2017-07-26 13:27:58 -0700

    [ios showcase] Integrate bubble with Showcase and add util methods
    This CL integrates the BubbleViewController with Showcase and adds util
    methods needed to properly configure the bubble. Additionally, this CL
    adds unit tests stubs for bubble_util methods.
    
    Bug:740145
    Change-Id: Ib6fe5ee258bedcd94d0cca6fd1c6ee86ecb0dd93

--- ios/chrome/browser/ui/bubble/bubble_view.mm
+++ ios/chrome/browser/ui/bubble/bubble_view.mm
@@ -10,6 +10,8 @@
 #error "This file requires ARC support."
 #endif
 
+CGFloat kBubbleAlignmentOffset = 10.0f;
+
 @interface BubbleView ()
 // Label containing the text displayed on the bubble.
 @property(nonatomic, weak) UILabel* label;
@@ -20,10 +22,55 @@
 @synthesize label = _label;
 
 - (instancetype)initWithText:(NSString*)text
-                   direction:(BubbleArrowDirection)arrowDirection
+              arrowDirection:(BubbleArrowDirection)direction
                    alignment:(BubbleAlignment)alignment {
   self = [super initWithFrame:CGRectZero];
+  [self setupViews];
   return self;
+}
+
+- (void)setupViews {
+  UIView* arrow = [self drawArrow];
+  [self addSubview:arrow];
+
+  UIView* background =
+      [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 200.0f, 100.0f)];
+  background.backgroundColor = [UIColor blueColor];
+  [self addSubview:background];
+
+  // add autolayout constraints
+  [NSLayoutConstraint activateConstraints:@[
+    [arrow.centerXAnchor constraintEqualToAnchor:self.trailingAnchor
+                                        constant:kBubbleAlignmentOffset],
+    [arrow.topAnchor constraintEqualToAnchor:self.topAnchor constant:8.0f],
+    [arrow.widthAnchor constraintEqualToConstant:14.0f],
+    [arrow.heightAnchor constraintEqualToConstant:10.0f],
+
+    [background.topAnchor constraintEqualToAnchor:arrow.bottomAnchor],
+    [background.trailingAnchor constraintEqualToAnchor:self.trailingAnchor],
+    [background.widthAnchor constraintEqualToConstant:200.0f],
+    [background.heightAnchor constraintEqualToConstant:100.0f],
+  ]];
+}
+
+- (UIView*)drawArrow {
+  CGFloat height = 10.0f;
+  CGFloat width = 14.0f;
+  CGRect rect = CGRectMake(0.0f, 0.0f, width, height);
+  UIView* arrow = [[UIView alloc] initWithFrame:rect];
+  UIBezierPath* path = UIBezierPath.bezierPath;
+  [path moveToPoint:CGPointMake(width / 2.0f, 0.0f)];
+  [path addLineToPoint:CGPointMake(0.0f, height)];
+  [path addLineToPoint:CGPointMake(width, height)];
+  [path closePath];
+
+  CAShapeLayer* layer = [CAShapeLayer layer];
+  layer.frame = rect;
+  layer.path = path.CGPath;
+  layer.fillColor = [UIColor blueColor].CGColor;
+  [arrow.layer addSublayer:layer];
+
+  return arrow;
 }
 
 #pragma mark - UIView overrides
