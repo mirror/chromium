d61223e65a9b973ec3f28f5c4bb2f4cf5d90ce80
Helen Li <xunjieli@chromium.org>
2017-07-26 16:09:46 -0400

    temp do not review
    Bug:
    Change-Id: If56c18c2f7e31a27d1e1735a19b1d7e9f9d6fc9a

--- tools/perf/fetch_benchmark_deps.py
+++ tools/perf/fetch_benchmark_deps.py
@@ -80,6 +80,16 @@ def FetchDepsForBenchmark(benchmark, output):
     print >> output, dep
 
 
+def GetStorySet(benchmark, user_agent=None):
+  # Create a dummy options object which hold default values that are expected
+  # by Benchmark.CreateStorySet(options) method.
+  parser = optparse.OptionParser()
+  benchmark.AddBenchmarkCommandLineArgs(parser)
+  options, _ = parser.parse_args([])
+  options.user_agent = user_agent
+  return benchmark().CreateStorySet(options)
+
+
 def main(args, output):
   parser = argparse.ArgumentParser(
          description='Fetch the dependencies of perf benchmark(s).')
@@ -101,13 +111,31 @@ def main(args, output):
       raise ValueError('No such benchmark: %s' % options.benchmark_name)
     FetchDepsForBenchmark(benchmark, output)
   else:
-    if not options.force:
-      raw_input(
-          'No benchmark name is specified. Fetching all benchmark deps. '
-          'Press enter to continue...')
+    page_sets_affected = {}
+    buckets = {}
+    story_sets = []
     for b in benchmark_finders.GetAllPerfBenchmarks():
-      print >> output, ('Fetch dependencies for benchmark %s' % b.Name())
-      FetchDepsForBenchmark(b, output)
+      story_sets.append(GetStorySet(b))
+    for b in benchmark_finders.GetAllContribBenchmarks():
+      for user_agent in ['desktop', 'mobile']:
+        story_sets.append(GetStorySet(b, user_agent))
+
+    for story_set in story_sets:
+      if story_set.archive_data_file == None or story_set.archive_data_file == '':
+        continue
+      if story_set.bucket != None:
+        if not story_set.archive_data_file in buckets:
+          buckets[story_set.archive_data_file] = []
+        buckets[story_set.archive_data_file] += [story_set.bucket]
+      if not story_set.archive_data_file in page_sets_affected:
+        page_sets_affected[story_set.archive_data_file] = []
+      page_sets_affected[story_set.archive_data_file] += [b.Name()]
+    for name, k, v in sorted([(k.split('/')[-1], k, v) for (k, v) in page_sets_affected.iteritems()]):
+      bucket_name = list(set(buckets[k]))
+      assert len(bucket_name) == 1
+      print name, ',', (' ').join(v), ',', bucket_name[0]
+      # print >> output, ('Fetch dependencies for benchmark %s' % b.Name())
+      # FetchDepsForBenchmark(b, output)
 
 if __name__ == '__main__':
   main(sys.argv[1:], sys.stdout)
