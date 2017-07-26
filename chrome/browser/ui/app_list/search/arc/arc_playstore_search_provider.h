2d7d8aa5da355c96d02781410e0485a8571dcbc4
Jiaquan He <hejq@google.com>
2017-07-26 10:59:27 -0700

    Add UMA for Play Store app search.
    - Record the round trip time of a Play Store app search query.
    - Record user actions of clicking into uninstalled Play Store
      apps and instant apps.
    - Record the numbers of returned uninstalled apps, instant apps,
      and their sum for every Play Store app search query.
    - Add Play Store app search result opening actions to the
      Apps.AppListSearchResultOpenType histogram.
    
    Also to distinguish canceled Play Store app search requests from
    completed ones, this commit contains a mojo API change.
    
    BUG=742595
    
    Change-Id: I94d1fcd985f123afdb5837bcd71f4aaf67323813

--- chrome/browser/ui/app_list/search/arc/arc_playstore_search_provider.h
+++ chrome/browser/ui/app_list/search/arc/arc_playstore_search_provider.h
@@ -29,7 +29,9 @@ class ArcPlayStoreSearchProvider : public SearchProvider {
   void Stop() override;
 
  private:
-  void OnResults(std::vector<arc::mojom::AppDiscoveryResultPtr> results);
+  void OnResults(base::TimeTicks query_start_time,
+                 arc::mojom::AppDiscoveryRequestState state,
+                 std::vector<arc::mojom::AppDiscoveryResultPtr> results);
 
   const int max_results_;
   // |profile_| is owned by ProfileInfo.
@@ -37,6 +39,7 @@ class ArcPlayStoreSearchProvider : public SearchProvider {
   // list_controller_ is owned by AppListServiceAsh and lives
   // until the service finishes.
   AppListControllerDelegate* const list_controller_;
+
   base::WeakPtrFactory<ArcPlayStoreSearchProvider> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ArcPlayStoreSearchProvider);
