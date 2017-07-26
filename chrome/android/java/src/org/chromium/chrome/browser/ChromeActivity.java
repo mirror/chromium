33b72748c70f2a3de7772d1003757f75e28fa332
Troy Hildebrandt <thildebr@chromium.org>
2017-07-26 11:10:13 -0700

    [Home] Shows bottom sheet at half state after being backgrounded for 3 hours.
    On BottomSheet init(), we set a pending sheet state to be set when the browser
    initialized callback is executed.
    
    Bug:706258
    Change-Id: I757803804d89c908424f031de0dce3ac36802e57

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeActivity.java
@@ -50,6 +50,7 @@ import org.chromium.base.DiscardableReferencePool;
 import org.chromium.base.SysUtils;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.VisibleForTesting;
+import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
@@ -147,6 +148,7 @@ import org.chromium.chrome.browser.widget.bottomsheet.BottomSheet;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheetContentController;
 import org.chromium.chrome.browser.widget.bottomsheet.EmptyBottomSheetObserver;
 import org.chromium.components.bookmarks.BookmarkId;
+import org.chromium.content.browser.BrowserStartupController;
 import org.chromium.content.browser.ContentVideoView;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content.common.ContentSwitches;
@@ -212,6 +214,12 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
     private static final String TAG = "ChromeActivity";
     private static final Rect EMPTY_RECT = new Rect();
 
+    /**
+     * Time in ms from when we last backgrounded Chrome until we show the bottom sheet at half.
+     * Time is 3 hours.
+     */
+    private static final long TIME_SINCE_BACKGROUNDED_TO_SHOW_BOTTOM_SHEET_HALF_MS = 10800000L;
+
     private static AppMenuHandlerFactory sAppMenuHandlerFactory = new AppMenuHandlerFactory() {
         @Override
         public AppMenuHandler get(
@@ -219,6 +227,9 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
             return new AppMenuHandler(activity, delegate, menuResourceId);
         }
     };
+
+    protected static final String LAST_BACKGROUNDED_TIME_MS_PREF =
+            "ChromeActivity.BackgroundTimeMs";
 
     private TabModelSelector mTabModelSelector;
     private TabModelSelectorTabObserver mTabModelSelectorTabObserver;
@@ -407,8 +418,25 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
                     ((ControlContainer) findViewById(R.id.control_container)).getView().getHeight();
             mBottomSheetContentController.init(
                     mBottomSheet, controlContainerHeight, mTabModelSelector, this);
+            maybeSetBottomSheetStateToHalfOnStartup();
         }
         ((BottomContainer) findViewById(R.id.bottom_container)).initialize(mFullscreenManager);
+    }
+
+    private void maybeSetBottomSheetStateToHalfOnStartup() {
+        BrowserStartupController.get(LibraryProcessType.PROCESS_BROWSER)
+                .addStartupCompletedObserver(new BrowserStartupController.StartupCallback() {
+                    @Override
+                    public void onSuccess(boolean alreadyStarted) {
+                        if (getTimeSinceLastBackgroundedMs()
+                                >= TIME_SINCE_BACKGROUNDED_TO_SHOW_BOTTOM_SHEET_HALF_MS) {
+                            mBottomSheet.setSheetState(BottomSheet.SHEET_STATE_HALF, true);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure() {}
+                });
     }
 
     @Override
@@ -2273,5 +2301,14 @@ public abstract class ChromeActivity extends AsyncInitializationActivity
      */
     public DiscardableReferencePool getReferencePool() {
         return mReferencePool;
+    }
+
+    /**
+     * Returns the number of milliseconds since Chrome was last backgrounded.
+     */
+    public long getTimeSinceLastBackgroundedMs() {
+        long lastBackgroundedTimeMs =
+                ContextUtils.getAppSharedPreferences().getLong(LAST_BACKGROUNDED_TIME_MS_PREF, -1);
+        return System.currentTimeMillis() - lastBackgroundedTimeMs;
     }
 }
