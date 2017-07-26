f944e82bd10d087549ad7046d47170c0bf15054e
John Mellor <johnme@chromium.org>
2017-07-26 14:40:51 +0100

    [Notifications] Resize "big picture" images to fit width
    Previously notifications shown by the message center would resize
    "big picture" images to be contained within the maximum size.
    
    However this is inconsistent with other platforms, in particular Android
    (and hence Chrome for Android) resizes these images to fit the width of
    the notification, for example:
    - Very wide images will be downscaled, shrinking the notification height
      accordingly.
    - Tall images will be scaled to fit the width then vertically
      center-cropped.
    - Small images will actually be upscaled until they fit the width (which
      may also result in them being vertically center-cropped).
    
    This patch makes the message center image scaling/cropping consistent
    with Android N+.
    
    Screenshot: https://imgur.com/a/Y69zK
    
    Bug: 662066
    Change-Id: Ic7286e593892d01fe1a15a0e1fcb7613b45fc07e

--- ui/message_center/views/proportional_image_view.cc
+++ ui/message_center/views/proportional_image_view.cc
@@ -12,34 +12,66 @@ namespace message_center {
 
 const char ProportionalImageView::kViewClassName[] = "ProportionalImageView";
 
-ProportionalImageView::ProportionalImageView(const gfx::Size& view_size) {
-  SetPreferredSize(view_size);
-}
+ProportionalImageView::ProportionalImageView(Type type) : type_(type) {}
 
-ProportionalImageView::~ProportionalImageView() {}
+ProportionalImageView::~ProportionalImageView() = default;
 
 void ProportionalImageView::SetImage(const gfx::ImageSkia& image,
+                                     const gfx::Size& max_view_size,
                                      const gfx::Size& max_image_size) {
   image_ = image;
   max_image_size_ = max_image_size;
+
+  switch (type_) {
+    case Type::FIT_WIDTH: {
+      gfx::Rect crop_rect;
+      gfx::Size draw_size;
+      CalcCropRectAndDrawSize(max_image_size_, &crop_rect, &draw_size);
+
+      int border_width_sum = max_view_size.width() - max_image_size_.width();
+      int border_height_sum = max_view_size.height() - max_image_size_.height();
+      gfx::Size view_size(draw_size.width() + border_width_sum,
+                          draw_size.height() + border_height_sum);
+      SetPreferredSize(view_size);
+      break;
+    }
+    case Type::CONTAIN: {
+      SetPreferredSize(max_view_size);
+      break;
+    }
+  }
+
   SchedulePaint();
 }
 
 void ProportionalImageView::OnPaint(gfx::Canvas* canvas) {
   views::View::OnPaint(canvas);
 
-  gfx::Size draw_size = GetImageDrawingSize();
+  if (!visible())
+    return;
+
+  gfx::Rect crop_rect;
+  gfx::Size draw_size;
+  CalcCropRectAndDrawSize(GetContentsBounds().size(), &crop_rect, &draw_size);
   if (draw_size.IsEmpty())
     return;
+
+  gfx::ImageSkia image = image_;
+
+  if (crop_rect.size() != image.size()) {
+    // Crop to the part of the image that will be visibile.
+    image = gfx::ImageSkiaOperations::ExtractSubset(image, crop_rect);
+  }
+
+  if (draw_size != image.size()) {
+    // Up/downscale the image.
+    image = gfx::ImageSkiaOperations::CreateResizedImage(
+        image, skia::ImageOperations::RESIZE_BEST, draw_size);
+  }
 
   gfx::Rect draw_bounds = GetContentsBounds();
   draw_bounds.ClampToCenteredSize(draw_size);
 
-  gfx::ImageSkia image =
-      (image_.size() == draw_size)
-          ? image_
-          : gfx::ImageSkiaOperations::CreateResizedImage(
-                image_, skia::ImageOperations::RESIZE_BEST, draw_size);
   canvas->DrawImageInt(image, draw_bounds.x(), draw_bounds.y());
 }
 
@@ -47,13 +79,58 @@ const char* ProportionalImageView::GetClassName() const {
   return kViewClassName;
 }
 
-gfx::Size ProportionalImageView::GetImageDrawingSize() {
-  if (!visible())
-    return gfx::Size();
+void ProportionalImageView::CalcCropRectAndDrawSize(
+    const gfx::Size& content_size,
+    gfx::Rect* out_crop_rect,
+    gfx::Size* out_draw_size) {
+  DCHECK(out_crop_rect);
+  DCHECK(out_draw_size);
 
   gfx::Size max_size = max_image_size_;
-  max_size.SetToMin(GetContentsBounds().size());
-  return message_center::GetImageSizeForContainerSize(max_size, image_.size());
+  max_size.SetToMin(content_size);
+  if (max_size.IsEmpty() || image_.size().IsEmpty()) {
+    DCHECK(out_draw_size->IsEmpty());
+    return;
+  }
+
+  double aspect_ratio = image_.width() / static_cast<double>(image_.height());
+
+  gfx::Size scaled_size = image_.size();
+
+  // First try fitting to width. Min height of 1 to avoid returning empty size.
+  scaled_size.SetSize(max_size.width(),
+                      std::max(0.5 + max_size.width() / aspect_ratio, 1.0));
+
+  // If type_ is FIT_WIDTH, then it's ok for the image to be taller than
+  // max_size. If instead type_ is CONTAIN, check if it's too tall, and if so
+  // fit to height instead.
+  if (type_ == Type::CONTAIN && scaled_size.height() > max_size.height()) {
+    scaled_size.SetSize(std::max(0.5 + max_size.height() * aspect_ratio, 1.0),
+                        max_size.height());
+  }
+
+  // If type_ is FIT_TO_WIDTH, scaled_size might be taller than max_image_size_.
+  *out_draw_size = scaled_size;
+  out_draw_size->SetToMin(max_image_size_);
+
+  if (*out_draw_size == scaled_size) {
+    *out_crop_rect = gfx::Rect(image_.size());
+  } else {
+    // Crop the top and bottom of the image.
+    DCHECK_EQ(type_, Type::FIT_WIDTH);
+    DCHECK_LE(scaled_size.width(), out_draw_size->width())
+        << "Only height should exceed out_draw_size";
+
+    // Total amount to crop from the top/bottom of the image.
+    float crop_height_fraction =
+        (scaled_size.height() - out_draw_size->height()) /
+        static_cast<float>(scaled_size.height());
+    int crop_height = std::lround(image_.height() * crop_height_fraction);
+    int crop_from_top = crop_height / 2;
+
+    *out_crop_rect = gfx::Rect(0, crop_from_top, image_.width(),
+                               image_.height() - crop_height);
+  }
 }
 
 }  // namespace message_center
--- ui/message_center/views/proportional_image_view.h
+++ ui/message_center/views/proportional_image_view.h
@@ -6,34 +6,42 @@
 #define UI_MESSAGE_CENTER_VIEWS_PROPORTIONAL_IMAGE_VIEW_H_
 
 #include "base/macros.h"
+#include "ui/gfx/geometry/size.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/message_center/message_center_export.h"
 #include "ui/views/view.h"
 
 namespace message_center {
 
-// ProportionalImageViews scale and center their images while preserving their
-// original proportions.
+// ProportionalImageViews scale images to kNotificationImageWidth then
+// vertically center-crops them if necessary.
 class MESSAGE_CENTER_EXPORT ProportionalImageView : public views::View {
  public:
+  enum Type { FIT_WIDTH, CONTAIN };
+
   // Internal class name.
   static const char kViewClassName[];
 
-  explicit ProportionalImageView(const gfx::Size& view_size);
+  explicit ProportionalImageView(Type type);
   ~ProportionalImageView() override;
 
-  // |image| is scaled to fit within |view_size| and |max_image_size| while
-  // maintaining its original aspect ratio. It is then centered within the view.
   void SetImage(const gfx::ImageSkia& image,
+                const gfx::Size& max_view_size,
                 const gfx::Size& max_image_size);
 
   // Overridden from views::View:
   void OnPaint(gfx::Canvas* canvas) override;
   const char* GetClassName() const override;
 
- private:
-  gfx::Size GetImageDrawingSize();
+  void CalcCropRectAndDrawSize(const gfx::Size& content_size,
+                               gfx::Rect* out_crop_rect,
+                               gfx::Size* out_draw_size);
 
+ private:
+  FRIEND_TEST_ALL_PREFIXES(NotificationViewTest, TestImageSizingFitWidth);
+  FRIEND_TEST_ALL_PREFIXES(NotificationViewMDTest, TestImageSizingFitWidth);
+
+  Type type_;
   gfx::ImageSkia image_;
   gfx::Size max_image_size_;
 
