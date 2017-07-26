3e9ad1c806345094ccd08c3ddf472b3e34bcbc9a
John Budorick <jbudorick@chromium.org>
2017-07-25 18:41:24 -0700

    [android] Stop losing tests in single-device shards.
    We're currently losing some tests in single-device shards when a
    test times out. run_tests_on_device for that shard fails, no other
    shard is available to run the remaining tests within that try, and
    the tests without @RetryOnFailure don't get rerun in the subsequent
    tries.
    
    This CL attempts to address this issue by:
     1. Only killing the shard on receiving a DeviceUnreachableError or
        an exception not emitted by devil. (The former indicates that
        we probably won't be able to use the device at all in the run,
        while the latter indicates an unexpected condition that should
        likely result in termination.) This should allow the single
        device to continue running tests within a try even if a test
        times out.
     2. Always retrying tests that didn't run.
    
    Bug: 748145
    Change-Id: Id120877e78b36443ed4cf8b979f17d52ea7d59ab

--- build/android/PRESUBMIT.py
+++ build/android/PRESUBMIT.py
@@ -63,6 +63,8 @@ def CommonChecks(input_api, output_api):
           J('pylib', 'instrumentation',
             'instrumentation_test_instance_test.py'),
           J('pylib', 'local', 'device', 'local_device_test_run_test.py'),
+          J('pylib', 'local', 'device',
+            'local_device_instrumentation_test_run_test.py'),
           J('pylib', 'results', 'json_results_test.py'),
           J('pylib', 'symbols', 'elf_symbolizer_unittest.py'),
           J('pylib', 'utils', 'decorators_test.py'),
