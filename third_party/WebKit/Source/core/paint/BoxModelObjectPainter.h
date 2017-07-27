91dcd53d318a9d1dbfe3421d5c7784b2b3261741
Emil A Eklund <eae@chromium.org>
2017-07-27 11:46:41 -0700

    WIP: Re-use BoxPainter logic with NG using inheritance
    Bug:
    Change-Id: I2d10403f955a0d18ab63b175fe250b7780225835

--- third_party/WebKit/Source/core/paint/BoxModelObjectPainter.h
+++ third_party/WebKit/Source/core/paint/BoxModelObjectPainter.h
@@ -7,9 +7,7 @@
 
 #include "core/layout/BackgroundBleedAvoidance.h"
 #include "core/paint/BoxPainterBase.h"
-#include "core/paint/RoundedInnerRectClipper.h"
 #include "platform/geometry/LayoutSize.h"
-#include "platform/graphics/GraphicsTypes.h"
 #include "platform/wtf/Allocator.h"
 
 namespace blink {
@@ -19,7 +17,7 @@ class InlineFlowBox;
 class LayoutRect;
 struct PaintInfo;
 class LayoutBoxModelObject;
-class BackgroundImageGeometry;
+class Image;
 
 // BoxModelObjectPainter is a class that can paint either a LayoutBox or a
 // LayoutInline and allows code sharing between block and inline block painting.
@@ -27,37 +25,49 @@ class BoxModelObjectPainter : public BoxPainterBase {
   STACK_ALLOCATED();
 
  public:
-  BoxModelObjectPainter(const LayoutBoxModelObject& box_model)
-      : box_model_(box_model) {}
-
-  void PaintFillLayers(const PaintInfo&,
-                       const Color&,
-                       const FillLayer&,
-                       const LayoutRect&,
-                       BackgroundImageGeometry&,
-                       BackgroundBleedAvoidance = kBackgroundBleedNone,
-                       SkBlendMode = SkBlendMode::kSrcOver);
-
-  void PaintFillLayer(const PaintInfo&,
-                      const Color&,
-                      const FillLayer&,
-                      const LayoutRect&,
-                      BackgroundBleedAvoidance,
-                      BackgroundImageGeometry&,
-                      SkBlendMode = SkBlendMode::kSrcOver,
-                      const InlineFlowBox* = nullptr,
-                      const LayoutSize& = LayoutSize());
+  BoxModelObjectPainter(const LayoutBoxModelObject& box_model,
+                        const InlineFlowBox* flow_box = nullptr,
+                        const LayoutSize& flow_box_size = LayoutSize())
+      : box_model_(box_model),
+        flow_box_(flow_box),
+        flow_box_size_(flow_box_size) {}
 
   static bool IsPaintingBackgroundOfPaintContainerIntoScrollingContentsLayer(
       const LayoutBoxModelObject*,
       const PaintInfo&);
 
- private:
-  Node* GetNode() const;
-  LayoutRectOutsets BorderOutsets(const BoxPainterBase::FillLayerInfo&) const;
-  LayoutRectOutsets PaddingOutsets(const BoxPainterBase::FillLayerInfo&) const;
+ protected:
+  BoxPainterBase::FillLayerInfo GetFillLayerInfo(
+      const Color&,
+      const FillLayer&,
+      BackgroundBleedAvoidance) const override;
 
+  void PaintFillLayerTextFillBox(GraphicsContext&,
+                                 const BoxPainterBase::FillLayerInfo&,
+                                 Image*,
+                                 SkBlendMode composite_op,
+                                 const BackgroundImageGeometry&,
+                                 const LayoutRect&,
+                                 LayoutRect scrolled_paint_rect) override;
+  LayoutRect AdjustForScrolledContent(const PaintInfo&,
+                                      const BoxPainterBase::FillLayerInfo&,
+                                      const LayoutRect&) override;
+  FloatRoundedRect GetBackgroundRoundedRect(
+      const LayoutRect&,
+      bool include_logical_left_edge,
+      bool include_logical_right_edge) const override;
+
+  Node* GetNode() const override;
+  const ComputedStyle& Style() const override;
+  const Document& GetDocument() const override;
+  const DisplayItemClient& DisplayItem() const override;
+  LayoutRectOutsets Border() const override;
+  LayoutRectOutsets Padding() const override;
+
+ private:
   const LayoutBoxModelObject& box_model_;
+  const InlineFlowBox* flow_box_;
+  const LayoutSize& flow_box_size_;
 };
 
 }  // namespace blink
