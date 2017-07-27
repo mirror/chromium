7b47bc61e31677fcb36be7ebbda5ee2cc655ccc1
Tim Volodine <timvolodine@chromium.org>
2017-07-27 16:41:52 +0100

    Move safe_browsing_db/android/ and related files to the safe_browsing component.
    Move android related code from the safe_browsing_db component to the
    safe_browsing component. This is part of the unification effort
    between the two components.
    
    BUG=668515
    
    Change-Id: Idd9b597369406729913b0d8283c35e08d3ea3f3b

--- components/safe_browsing/android/jni_registrar.cc
+++ components/safe_browsing/android/jni_registrar.cc
@@ -2,12 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/safe_browsing_db/android/jni_registrar.h"
+#include "components/safe_browsing/android/jni_registrar.h"
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_registrar.h"
 #include "base/macros.h"
-#include "components/safe_browsing_db/android/safe_browsing_api_handler_bridge.h"
+#include "components/safe_browsing/android/safe_browsing_api_handler_bridge.h"
 
 namespace safe_browsing {
 namespace android {
--- components/safe_browsing_db/remote_database_manager.cc
+++ components/safe_browsing_db/remote_database_manager.cc
@@ -11,7 +11,7 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/timer/elapsed_timer.h"
-#include "components/safe_browsing_db/safe_browsing_api_handler.h"
+#include "components/safe_browsing/safe_browsing_api_handler.h"
 #include "components/safe_browsing_db/v4_get_hash_protocol_manager.h"
 #include "components/safe_browsing_db/v4_protocol_manager_util.h"
 #include "components/variations/variations_associated_data.h"
