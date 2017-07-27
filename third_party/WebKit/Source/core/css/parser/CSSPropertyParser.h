b3ab443601d4f12ad611dd26ae9617d217ef4ed8
Jia <jiameng@chromium.org>
2017-07-27 16:05:35 +1000

    Implement parseShorthand for place-[content|items|self].
    This cl also refactors util functions and moves them from
    CSSPropertyParser to CSSPropertyPlaceUtils file.
    
    Bug:668012
    Change-Id: Ifbb4ae30df07555f0298bcd1618c23ca96b2cda5
    Diff: https://gist.github.com/784f237041c1bd419a367bfd10b71a5d/revisions

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.h
@@ -93,10 +93,6 @@ class CSSPropertyParser {
   bool ConsumeGridShorthand(bool important);
   bool ConsumeGridAreaShorthand(bool important);
 
-  bool ConsumePlaceContentShorthand(bool important);
-  bool ConsumePlaceItemsShorthand(bool important);
-  bool ConsumePlaceSelfShorthand(bool important);
-
   bool ConsumeLegacyBreakProperty(CSSPropertyID, bool important);
 
  private:
