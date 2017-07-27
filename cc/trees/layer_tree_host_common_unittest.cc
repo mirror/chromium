d33cbfe8476522395142dcd6d2dcfd4a16907f65
David Bokan <bokan@chromium.org>
2017-07-27 18:32:10 -0400

    Expand clip and pos:fixed elements for root scroller
    Bug:
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I76b4418b5735372639cd1e479e089e795cbe4516

--- cc/trees/layer_tree_host_common_unittest.cc
+++ cc/trees/layer_tree_host_common_unittest.cc
@@ -8123,6 +8123,7 @@ TEST_F(LayerTreeHostCommonTest, NodesAffectedByViewportBoundsDeltaGetUpdated) {
 
   inner_viewport_scroll_layer->SetIsContainerForFixedPositionLayers(true);
   outer_viewport_scroll_layer->SetIsContainerForFixedPositionLayers(true);
+  outer_viewport_scroll_layer->SetIsResizedByBrowserControls(true);
 
   host()->SetRootLayer(root);
   LayerTreeHost::ViewportLayers viewport_layers;
