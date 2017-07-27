30f8b829e9951663d50bc9c1c30364b1f1bf5174
Luis Rabasa <erickzul@google.com>
2017-07-27 15:29:34 -0400

    Reverted to Feature Flag only CL and moved features to chrome_features.h
    Bug:
    Change-Id: I1a4c0da04a5d3b8353b7e3f80f128799bc159d4e

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1214,6 +1214,9 @@ extern const char kAppWindowCyclingDescription[];
 extern const char kFullscreenToolbarRevealName[];
 extern const char kFullscreenToolbarRevealDescription[];
 
+extern const char kContentFullscreenName[];
+extern const char kContentFullscreenDescription[];
+
 extern const char kHostedAppsInWindowsName[];
 extern const char kHostedAppsInWindowsDescription[];
 
