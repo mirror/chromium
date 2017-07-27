d652d5f8db56b225f496ad7046b0abdd286875c3
Andrey Lushnikov <lushnikov@chromium.org>
2017-07-26 22:43:02 -0700

    DevTools: migrate inspector-protocol/css tests to a new testrunner
    This patch rewrites the inspector-protocol/css tests onto a new test
    runner.
    
    The old helper scirpts which were used to drive CSS tests are moved
    under the inspector-protocol/layout-founts/resources folder.
    
    R=dgozman
    BUG=734762
    
    Change-Id: I6950b6c6db08a61fe512af9ba2ba260e9d20f96b

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/css/css-fonts-updated-event.js
@@ -0,0 +1,14 @@
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startHTML(`<link rel='stylesheet'>some text`, 'Verifies that CSS.fontsUpdated event is sent as the web font is loaded.');
+
+setTimeout(testRunner.completeTest.bind(testRunner), 2000);
+  await dp.DOM.enable();
+  await dp.CSS.enable();
+  session.evaluate(fontURL => {
+    var link = document.querySelector('link');
+    link.href = fontURL;
+  }, testRunner.url('./resources/ahem.css'));
+  await dp.CSS.onceFontsUpdated();
+  testRunner.log('SUCCESS: CSS.FontsUpdated event received.');
+  testRunner.completeTest();
+});
