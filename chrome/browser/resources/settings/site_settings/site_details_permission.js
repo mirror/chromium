31730f1e0a65faf81048098c76deed53452df62b
Patricia Lor <patricialor@chromium.org>
2017-07-27 16:41:18 +1000

    MD Settings: Site Details shows the source of a non-user controlled setting.
    When a permission setting is controlled by something other than the user (e.g.
    enterprise policy, extensions, or embargo), describe the actual source with a
    string displayed underneath the permission in Site Details.
    
    Bug: 656758
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I017968f0b5bce6cb72143d1de5a040369499e4b2

--- chrome/browser/resources/settings/site_settings/site_details_permission.js
+++ chrome/browser/resources/settings/site_settings/site_details_permission.js
@@ -55,17 +55,31 @@ Polymer({
    * @private
    */
   siteChanged_: function(site) {
-    if (site.source == 'default') {
+    if (site.source == settings.SiteSettingSource.DEFAULT) {
       this.defaultSetting_ = site.setting;
       this.$.permission.value = settings.ContentSetting.DEFAULT;
       return;
+    } else {
+      // The default setting is unknown, so consult the C++ backend for it.
+      this.browserProxy.getDefaultValueForContentType(this.category)
+          .then((defaultValue) => {
+            this.defaultSetting_ = defaultValue.setting;
+          });
+      this.$.permission.value = site.setting;
     }
-    // The default setting is unknown, so consult the C++ backend for it.
-    this.browserProxy.getDefaultValueForContentType(this.category)
-        .then((defaultValue) => {
-          this.defaultSetting_ = defaultValue.setting;
-        });
-    this.$.permission.value = site.setting;
+
+    // Handle non-default sources.
+    if (site.source == settings.SiteSettingSource.DEFAULT ||
+        site.source == settings.SiteSettingSource.PREFERENCE) {
+      this.$.permissionItem.classList.remove('two-line');
+    } else {
+      this.$.permissionItem.classList.add('two-line');
+      this.$.permission.disabled = true;
+    }
+
+    // Users are able to override embargo, so re-enable controls if so.
+    if (site.source == settings.SiteSettingSource.EMBARGO)
+      this.$.permission.disabled = false;
   },
 
   /**
@@ -101,13 +115,14 @@ Polymer({
   /**
    * Updates the string used for this permission category's default setting.
    * @param {!settings.ContentSetting} defaultSetting Value of the default
-   *    setting for this permission category.
+   *     setting for this permission category.
    * @param {string} askString 'Ask' label, e.g. 'Ask (default)'.
    * @param {string} allowString 'Allow' label, e.g. 'Allow (default)'.
    * @param {string} blockString 'Block' label, e.g. 'Blocked (default)'.
    * @private
    */
-  defaultSettingString_(defaultSetting, askString, allowString, blockString) {
+  defaultSettingString_: function(
+      defaultSetting, askString, allowString, blockString) {
     if (defaultSetting == settings.ContentSetting.ASK ||
         defaultSetting == settings.ContentSetting.IMPORTANT_CONTENT) {
       return askString;
@@ -118,5 +133,59 @@ Polymer({
     }
     assertNotReached(
         `No string for ${this.category}'s default of ${defaultSetting}`);
+  },
+
+  /**
+   * Updates the string used to describe the source of this permission setting.
+   * Note |source| is a subproperty of |this.site|, so the
+   * permissionSourceString_ will only update when |this.site| is updated.
+   * @param {!settings.SiteSettingSource} source The source of the permission.
+   * @private
+   */
+  permissionSourceString_: function(source) {
+    var extensionSourceStrings =
+        /** @type {Object<!settings.ContentSetting, string>} */ {};
+    extensionSourceStrings[settings.ContentSetting.ALLOW] =
+        'siteSettingsSourceExtensionAllow';
+    extensionSourceStrings[settings.ContentSetting.BLOCK] =
+        'siteSettingsSourceExtensionBlock';
+    extensionSourceStrings[settings.ContentSetting.ASK] =
+        'siteSettingsSourceExtensionAsk';
+
+    var policySourceStrings =
+        /** @type {Object<!settings.ContentSetting, string>} */ {};
+    policySourceStrings[settings.ContentSetting.ALLOW] =
+        'siteSettingsSourcePolicyAllow';
+    policySourceStrings[settings.ContentSetting.BLOCK] =
+        'siteSettingsSourcePolicyBlock';
+    policySourceStrings[settings.ContentSetting.ASK] =
+        'siteSettingsSourcePolicyAsk';
+
+    if (source == settings.SiteSettingSource.EMBARGO) {
+      assert(
+          settings.ContentSetting.BLOCK == this.site.setting,
+          'Embargo is only used to block permissions.');
+      return loadTimeData.getString('siteSettingsSourceEmbargo');
+    } else if (source == settings.SiteSettingSource.EXTENSION) {
+      return loadTimeData.getString(extensionSourceStrings[this.site.setting]);
+    } else if (source == settings.SiteSettingSource.INSECURE_ORIGIN) {
+      assert(
+          settings.ContentSetting.BLOCK == this.site.setting,
+          'Permissions can only be blocked due to insecure origins.');
+      return loadTimeData.getString('siteSettingsSourceInsecureOrigin');
+    } else if (source == settings.SiteSettingSource.KILL_SWITCH) {
+      assert(
+          settings.ContentSetting.BLOCK == this.site.setting,
+          'The permissions kill switch can only be used to block permissions.');
+      return loadTimeData.getString('siteSettingsSourceKillSwitch');
+    } else if (source == settings.SiteSettingSource.POLICY) {
+      return loadTimeData.getString(policySourceStrings[this.site.setting]);
+    } else if (
+        source == settings.SiteSettingSource.DEFAULT ||
+        source == settings.SiteSettingSource.PREFERENCE) {
+      return '';
+    }
+    assertNotReached(
+        `No string for ${this.category} setting source '${source}'`);
   },
 });
