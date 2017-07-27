51bb7ef47fe6259622ba55edfe3f429091fd6a8e
Eddy Mead <meade@chromium.org>
2017-07-27 11:49:29 +1000

    Handle neutral keyframes in CSSDefaultInterpolationType correctly.
    Bug: 738636
    Change-Id: I311aa417bd0272ee392c3161b2bff4e661882541

--- third_party/WebKit/Source/core/animation/CSSDefaultInterpolationType.cpp
+++ third_party/WebKit/Source/core/animation/CSSDefaultInterpolationType.cpp
@@ -40,6 +40,11 @@ InterpolationValue CSSDefaultInterpolationType::MaybeConvertSingle(
     const InterpolationEnvironment&,
     const InterpolationValue&,
     ConversionCheckers&) const {
+  if (!ToCSSPropertySpecificKeyframe(keyframe).Value()) {
+    // Neutral keyframes have nullptr CSSValues.
+    DCHECK(keyframe.IsNeutral());
+    return nullptr;
+  }
   return InterpolationValue(
       InterpolableList::Create(0),
       CSSDefaultNonInterpolableValue::Create(
@@ -50,6 +55,7 @@ void CSSDefaultInterpolationType::Apply(
     const InterpolableValue&,
     const NonInterpolableValue* non_interpolable_value,
     InterpolationEnvironment& environment) const {
+  DCHECK(ToCSSDefaultNonInterpolableValue(non_interpolable_value)->CssValue());
   StyleBuilder::ApplyProperty(
       GetProperty().CssProperty(),
       ToCSSInterpolationEnvironment(environment).GetState(),
