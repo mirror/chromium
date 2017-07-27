e6ef9b1df954ecf721d81a41e1ec53cff19ad8a9
Karan Bhatia <karandeepb@chromium.org>
2017-07-27 13:12:02 -0700

    DNR: Introduce IndexedRule to store parsed JSON rules.
    https://chromium-review.googlesource.com/c/576199/ added the schema for
    specifying JSON rules for the Declarative Net Request API. This CL introduces
    IndexedRule to store the parsed JSON rules. In a subsequent CL, this structure
    will aid in the conversion to a flatbuffer UrlRule as specified by the
    url_pattern_index component.
    
    Doc=http://go/declarative-net-request (Internal only)
    BUG=696822
    
    Change-Id: I87b55c0266eafddf521bccb3f7d11757f47c2d7b

--- extensions/common/api/declarative_net_request/constants.h
+++ extensions/common/api/declarative_net_request/constants.h
@@ -8,6 +8,31 @@
 namespace extensions {
 namespace declarative_net_request {
 
+// The result of parsing JSON rules provided by an extension.
+enum class ParseResult {
+  SUCCESS,
+  ERROR_RESOURCE_TYPE_DUPLICATED,
+  ERROR_EMPTY_REDIRECT_RULE_PRIORITY,
+  ERROR_EMPTY_REDIRECT_URL,
+  ERROR_INVALID_RULE_ID,
+  ERROR_INVALID_REDIRECT_RULE_PRIORITY,
+  ERROR_NO_APPLICABLE_RESOURCE_TYPES,
+  ERROR_EMPTY_DOMAINS_LIST,
+  ERROR_EMPTY_RESOURCE_TYPES_LIST,
+  ERROR_EMPTY_URL_FILTER,
+  ERROR_INVALID_REDIRECT_URL,
+};
+
+// Minimum valid value of a declarative rule ID.
+constexpr int kMinValidID = 1;
+
+// Minimum valid value of a declarative rule priority.
+constexpr int kMinValidPriority = 1;
+
+// Default priority used for rules when the priority is not explicity provided
+// by an extension.
+constexpr int kDefaultPriority = 1;
+
 // Permission name.
 constexpr char kAPIPermission[] = "declarativeNetRequest";
 
