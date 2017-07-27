d2cf58b6537021631a413b64eae91e4595e7ffef
Ricardo Quesada <ricardoq@chromium.org>
2017-07-26 17:58:56 -0700

    Add support for HTML copy&paste Android<-->Chrome
    Add support for HTML copy & paste between Android and Chrome.
    Use a new way that it is easier to add more formats in the future (like FD or
    blobs).
    
    Change clipboard mojo protocol slightly
    Deprecate the old clipboard mojo callbacks.
    
    BUG=738519
    R=lhchavez@chromium.org
    R=phshah@chromium.org
    
    Change-Id: I39a0c432fb57370f8aa2279af9410344debe11e0

--- components/arc/clipboard/arc_clipboard_bridge.h
+++ components/arc/clipboard/arc_clipboard_bridge.h
@@ -5,17 +5,24 @@
 #ifndef COMPONENTS_ARC_CLIPBOARD_ARC_CLIPBOARD_BRIDGE_H_
 #define COMPONENTS_ARC_CLIPBOARD_ARC_CLIPBOARD_BRIDGE_H_
 
+#include <memory>
 #include <string>
+#include <vector>
 
 #include "base/macros.h"
 #include "components/arc/common/clipboard.mojom.h"
 #include "components/arc/instance_holder.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "mojo/public/cpp/bindings/binding.h"
+#include "ui/base/clipboard/clipboard_observer.h"
 
 namespace content {
 class BrowserContext;
 }  // namespace content
+namespace ui {
+class Clipboard;
+class ScopedClipboardWriter;
+}  // namespace ui
 
 namespace arc {
 
@@ -23,6 +30,7 @@ class ArcBridgeService;
 
 class ArcClipboardBridge
     : public KeyedService,
+      public ui::ClipboardObserver,
       public InstanceHolder<mojom::ClipboardInstance>::Observer,
       public mojom::ClipboardHost {
  public:
@@ -38,15 +46,32 @@ class ArcClipboardBridge
   // InstanceHolder<mojom::ClipboardInstance>::Observer overrides.
   void OnInstanceReady() override;
 
+  // ClipboardObserver overrides
+  void OnClipboardDataChanged() override;
+
   // mojom::ClipboardHost overrides.
-  void SetTextContent(const std::string& text) override;
-  void GetTextContent() override;
+  void SetTextContentDeprecated(const std::string& text) override;
+  void GetTextContentDeprecated() override;
+  void SetClipContent(mojom::ClipDataPtr clip_data) override;
+  void GetClipContent(const GetClipContentCallback& callback) override;
 
  private:
-  THREAD_CHECKER(thread_checker_);
+  static mojom::ClipItemPtr CreateItemHTML(ui::Clipboard* clipboard);
+  static mojom::ClipItemPtr CreateItemPlainText(ui::Clipboard* clipboard);
+
+  static void ProcessItemHTML(mojom::ClipItem* clip_item,
+                              ui::ScopedClipboardWriter* writer);
+  static void ProcessItemPlainText(mojom::ClipItem* clip_item,
+                                   ui::ScopedClipboardWriter* writer);
 
   ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
   mojo::Binding<mojom::ClipboardHost> binding_;
+
+  // clipboard sequence number. used to know whether or not a event comes from
+  // Host or Instance
+  uint64_t clipboard_seq_from_instance_;
+
+  THREAD_CHECKER(thread_checker_);
 
   DISALLOW_COPY_AND_ASSIGN(ArcClipboardBridge);
 };
