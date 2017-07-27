96a51ebd04502d861c09c711f991d77b820ade1c
Makoto Shimazu <shimazu@chromium.org>
2017-07-27 16:03:31 +0900

    ServiceWorker: Fixing traces when script streaming is enabled
    This patch fixes the order of TRACE_EVENTs in ServiceWorkerContextClient since
    the place of loading script through InstalledScriptsManager was tweaked at the
    CSP/ReferrerPolicy patch (https://crrev.com/c/578972).
    
    Bug: 683037
    Change-Id: Icf3acaba1759fa4bbd9d3924d90fd6ac8289361a

--- content/renderer/service_worker/service_worker_context_client.cc
+++ content/renderer/service_worker/service_worker_context_client.cc
@@ -583,7 +583,6 @@ ServiceWorkerContextClient::ServiceWorkerContextClient(
       service_worker_version_id_(service_worker_version_id),
       service_worker_scope_(service_worker_scope),
       script_url_(script_url),
-      is_script_streaming_(is_script_streaming),
       sender_(ChildThreadImpl::current()->thread_safe_sender()),
       main_thread_task_runner_(base::ThreadTaskRunnerHandle::Get()),
       proxy_(nullptr),
@@ -601,13 +600,9 @@ ServiceWorkerContextClient::ServiceWorkerContextClient(
   TRACE_EVENT_NESTABLE_ASYNC_BEGIN1("ServiceWorker",
                                     "ServiceWorkerContextClient", this,
                                     "script_url", script_url_.spec());
-  if (is_script_streaming_) {
-    TRACE_EVENT_NESTABLE_ASYNC_BEGIN0("ServiceWorker", "START_WORKER_THREAD",
-                                      this);
-  } else {
-    TRACE_EVENT_NESTABLE_ASYNC_BEGIN1("ServiceWorker", "LOAD_SCRIPT", this,
-                                      "Type", "ResourceLoader");
-  }
+  TRACE_EVENT_NESTABLE_ASYNC_BEGIN1(
+      "ServiceWorker", "LOAD_SCRIPT", this, "Source",
+      (is_script_streaming ? "InstalledScriptsManager" : "ResourceLoader"));
 }
 
 ServiceWorkerContextClient::~ServiceWorkerContextClient() {}
@@ -713,12 +708,8 @@ void ServiceWorkerContextClient::WorkerContextFailedToStart() {
 void ServiceWorkerContextClient::WorkerScriptLoaded() {
   (*instance_host_)->OnScriptLoaded();
   TRACE_EVENT_NESTABLE_ASYNC_END0("ServiceWorker", "LOAD_SCRIPT", this);
-  if (is_script_streaming_) {
-    TRACE_EVENT_NESTABLE_ASYNC_BEGIN0("ServiceWorker", "EVALUATE_SCRIPT", this);
-  } else {
-    TRACE_EVENT_NESTABLE_ASYNC_BEGIN0("ServiceWorker", "START_WORKER_THREAD",
-                                      this);
-  }
+  TRACE_EVENT_NESTABLE_ASYNC_BEGIN0("ServiceWorker", "START_WORKER_THREAD",
+                                    this);
 }
 
 bool ServiceWorkerContextClient::HasAssociatedRegistration() {
@@ -759,12 +750,7 @@ void ServiceWorkerContextClient::WorkerContextStarted(
   (*instance_host_)->OnThreadStarted(WorkerThread::GetCurrentId());
 
   TRACE_EVENT_NESTABLE_ASYNC_END0("ServiceWorker", "START_WORKER_THREAD", this);
-  if (is_script_streaming_) {
-    TRACE_EVENT_NESTABLE_ASYNC_BEGIN1("ServiceWorker", "LOAD_SCRIPT", this,
-                                      "Type", "InstalledScriptsManager");
-  } else {
-    TRACE_EVENT_NESTABLE_ASYNC_BEGIN0("ServiceWorker", "EVALUATE_SCRIPT", this);
-  }
+  TRACE_EVENT_NESTABLE_ASYNC_BEGIN0("ServiceWorker", "EVALUATE_SCRIPT", this);
 }
 
 void ServiceWorkerContextClient::DidEvaluateWorkerScript(bool success) {
