3d9ce5c87590ea8c46e967d831d414298084122e
Weiliang Chen <weiliangc@chromium.org>
2017-07-27 20:52:41 -0400

    cc: Add Tracing to Compositor Frame Transport from Renderer
    This adds tracing for following cases:
    - serialization and deserialization of compositor frames in IPC,
    - deserialization of compositor frames in Mojo,
    - flow event from renderer to browser on desktop,
    - instance of time elapsed between submitting compositor frame to
    receiving from renderer to browser on desktop.
    
    R=danakj@chromium.org
    
    Bug: 699121
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I6ca1adc6dc61bc28c4c241ec63a67402f2edf7cf

--- content/browser/renderer_host/render_widget_host_impl.h
+++ content/browser/renderer_host/render_widget_host_impl.h
@@ -580,7 +580,8 @@ class CONTENT_EXPORT RenderWidgetHostImpl
   // cc::mojom::CompositorFrameSink implementation.
   void SetNeedsBeginFrame(bool needs_begin_frame) override;
   void SubmitCompositorFrame(const viz::LocalSurfaceId& local_surface_id,
-                             cc::CompositorFrame frame) override;
+                             cc::CompositorFrame frame,
+                             uint64_t submit_time) override;
   void DidNotProduceFrame(const viz::BeginFrameAck& ack) override;
 
   // Signals that a frame with token |frame_token| was finished processing. If
