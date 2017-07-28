e9e28d73a2c77c81536bfa7397254db1d09a50db
Han Leon <leon.han@intel.com>
2017-07-28 19:09:46 +0800

    [DeviceService] Expose SerialDeviceEnumerator interface from Device Service
    This CL is part of servicification work for //device/serial, it:
     - creates a new mojo interface SerialDeviceEnumerator and exposes it
       from Device Service, the interface implementation wraps
       a device::SerialDeviceEnumerator to do actual work.
     - lets serial extension API impls consume the above interface instead
       of directly calling functions of device::SerialDeviceEnumerator.
    
    BUG=749514
    TEST=browser_tests SerialApiTest.*
    
    Change-Id: Ib768bb25032fcc4a0899780a2f85467f128a6800

---
+++ services/device/serial/serial_device_enumerator_impl.h
@@ -0,0 +1,40 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_DEVICE_SERIAL_SERIAL_DEVICE_ENUMERATOR_IMPL_H_
+#define SERVICES_DEVICE_SERIAL_SERIAL_DEVICE_ENUMERATOR_IMPL_H_
+
+#include <memory>
+
+#include "base/macros.h"
+#include "base/memory/ref_counted.h"
+#include "device/serial/serial_device_enumerator.h"
+#include "services/device/public/interfaces/serial.mojom.h"
+
+namespace base {
+class SequencedTaskRunner;
+}
+
+namespace device {
+
+class SerialDeviceEnumeratorImpl : public mojom::SerialDeviceEnumerator {
+ public:
+  static void Create(mojom::SerialDeviceEnumeratorRequest request);
+
+  SerialDeviceEnumeratorImpl(scoped_refptr<base::SequencedTaskRunner> runner);
+  ~SerialDeviceEnumeratorImpl() override;
+
+ private:
+  // mojom::SerialDeviceEnumerator methods:
+  void GetDevices(GetDevicesCallback callback) override;
+
+  scoped_refptr<base::SequencedTaskRunner> runner_;
+  std::unique_ptr<device::SerialDeviceEnumerator> enumerator_;
+
+  DISALLOW_COPY_AND_ASSIGN(SerialDeviceEnumeratorImpl);
+};
+
+}  // namespace device
+
+#endif  // SERVICES_DEVICE_SERIAL_SERIAL_DEVICE_ENUMERATOR_IMPL_H_
