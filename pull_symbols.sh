c1d7143d43cdccf12bd1f4a300685bdb7b8a0c10
Eric Karl <ericrk@chromium.org>
2017-07-28 14:12:19 -0700

    Use NDK r15b
    Change-Id: Ibfeb10c96206912fcd70238db3d137eca4448c57

---
+++ pull_symbols.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$1" ]; then
+       echo "Specify out/ subfolder."
+       exit 1
+fi
+
+clankium_build_dir="/usr/local/google/home/ericrk/chromium/src/out/$1"
+
+build_id=$(adb shell getprop ro.build.id)
+symbols_dir="$build_id-symbols"
+
+echo "Full symbols path: $(pwd)/$symbols_dir"
+
+if [ ! -d "$symbols_dir" ]; then
+       echo "Pulling libraries..."
+
+       mkdir -p "$symbols_dir"
+
+       mkdir "$symbols_dir/system"
+       adb pull "/system/lib" "$symbols_dir/system"
+
+       mkdir "$symbols_dir/system/bin"
+       adb pull "/system/bin/app_process32" "$symbols_dir/system/bin"
+
+       mkdir "$symbols_dir/vendor"
+       adb pull "/vendor/lib" "$symbols_dir/vendor"
+fi
+
+echo "Linking libchrome.so..."
+
+chrome_android_path="$(adb shell find /data/app -name 'org.chromium.chrome-*')"
+libchrome_dir="$symbols_dir$chrome_android_path/lib/arm"
+
+rm -rf "$libchrome_dir"
+mkdir -p "$libchrome_dir"
+ln -s "$clankium_build_dir/lib.unstripped/libchrome.so" "$libchrome_dir/libchrome.so"
