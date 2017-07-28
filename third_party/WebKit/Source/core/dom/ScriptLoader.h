41a63e1aff316168ad5ed36d6e0218d1c9d78588
Hiroshige Hayashizaki <hiroshige@chromium.org>
2017-07-28 16:14:00 -0700

    Merge ScriptLoader::ExecuteScript() into ExecuteScriptBlock()
    To make it clearer that ExecuteScriptBlock() is the single entry
    point of script execution by removing the similarly-named
    ExecuteScript(), which is now only called from ExecuteScriptBlock().
    
    No behavior changes.
    
    Bug: 686281
    Change-Id: I0e2bcdcf05cc9c3141398a3a465fa6fb268b0913

--- third_party/WebKit/Source/core/dom/ScriptLoader.h
+++ third_party/WebKit/Source/core/dom/ScriptLoader.h
@@ -175,7 +175,6 @@ class CORE_EXPORT ScriptLoader : public GarbageCollectedFinalized<ScriptLoader>,
     kShouldFireLoadEvent,
     kShouldFireNone
   };
-  WARN_UNUSED_RESULT ExecuteScriptResult ExecuteScript(const Script*);
   ExecuteScriptResult DoExecuteScript(const Script*);
   void DispatchLoadEvent();
   void DispatchErrorEvent();
