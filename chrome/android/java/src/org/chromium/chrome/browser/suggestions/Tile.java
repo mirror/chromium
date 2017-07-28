8f33052557196ee4ca41071ca2e35def8c0b1995
Nicolas Dossou-gbete <dgn@chromium.org>
2017-07-28 13:42:09 +0100

    [Suggestions] Add Section support to TileGroup
    Related to https://chromium-review.googlesource.com/c/568492/,
    stores tile data into sections. The tiles are still transmitted
    from the native side as a flat array, but when we load them we
    arrange them in a map of tiles.
    
    It should keep the current UI functioning just like before.
    
    Some of the current assumptions are affected:
    - We can have more than one tile per URL
    - We should not wait for all the tiles before we consider the tile
    load to be complete (some of the tiles will not be displayed at all)
    
    Nice to have before this can land:
    - Figure out the icon fetch and cache strategy since we can now have
    requests for the same URL multiple times
    
    Bug: 740905
    Change-Id: Ia91c3c7abb5d05a946d0b9baff95b5a05c9e3581

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/Tile.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/Tile.java
@@ -5,6 +5,7 @@
 package org.chromium.chrome.browser.suggestions;
 
 import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 /**
@@ -15,6 +16,9 @@ public class Tile implements OfflinableSuggestion {
     private final String mUrl;
     private final String mWhitelistIconPath;
     private final int mIndex;
+
+    @TileGroup.TileSection
+    private final int mSection;
 
     @TileSource
     private final int mSource;
@@ -42,6 +46,7 @@ public class Tile implements OfflinableSuggestion {
         mUrl = url;
         mWhitelistIconPath = whitelistIconPath;
         mIndex = index;
+        mSection = TileGroup.TileSection.PERSONAL; // TODO(dgn): rebase and use real data.
         mSource = source;
     }
 
@@ -50,11 +55,12 @@ public class Tile implements OfflinableSuggestion {
      * difference between the two that would require a redraw.
      * Assumes that the current tile and the old tile (if provided) both describe the same site,
      * so the URLs have to be the same.
+     *
+     * @return Whether non-transient data is different and the tile should be redrawn.
      */
-    public boolean importData(@Nullable Tile tile) {
-        if (tile == null) return true;
-
+    public boolean importData(@NonNull Tile tile) {
         assert tile.getUrl().equals(mUrl);
+        assert tile.getSection() == mSection;
 
         mType = tile.getType();
         mIcon = tile.getIcon();
@@ -157,5 +163,10 @@ public class Tile implements OfflinableSuggestion {
      */
     public void setIcon(@Nullable Drawable icon) {
         mIcon = icon;
+    }
+
+    @TileGroup.TileSection
+    public int getSection() {
+        return mSection;
     }
 }
