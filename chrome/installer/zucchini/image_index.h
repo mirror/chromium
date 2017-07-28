79e7a054cceed1c42990c2970da2dcea353eb104
Etienne Pierre-Doray <etiennep@chromium.org>
2017-07-27 18:14:44 -0700

    [Zucchini] Introduce image_index.
    This CL adds ImageIndex, which stores annotation of an image file for
    quick access to raw data and embedded references.
    Note that some functionality from EncodedView from original implemetation
    have been moved into ImageIndex. ImageIndex interface will need to be extended in
    a follow up CL to allow modification of targets based on Labels (working along with LabelManager),
    but this will only be needed later.
    
    Bug: 729154
    Change-Id: I1d5353fc8093543f2be0400361affa8b57333924

---
+++ chrome/installer/zucchini/image_index.h
@@ -0,0 +1,137 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_INSTALLER_ZUCCHINI_IMAGE_INDEX_H_
+#define CHROME_INSTALLER_ZUCCHINI_IMAGE_INDEX_H_
+
+#include <stdint.h>
+
+#include <vector>
+
+#include "base/logging.h"
+#include "base/macros.h"
+#include "base/optional.h"
+#include "chrome/installer/zucchini/buffer_view.h"
+#include "chrome/installer/zucchini/image_utils.h"
+#include "chrome/installer/zucchini/label_manager.h"
+
+namespace zucchini {
+
+class ReferenceReader;
+
+// A class that holds annotations of an image file, allowing quick access to its
+// raw and reference content. The memory overhead of storing all references is
+// relatively high, so this is only used during patch generation.
+class ImageIndex {
+ public:
+  explicit ImageIndex(ConstBufferView image,
+                      std::vector<ReferenceTypeTraits>&& traits_map);
+  ~ImageIndex();
+
+  // Inserts all references of type |type_tag| read from |ref_reader| to this
+  // index. This should be called exactly once for each reference type. If
+  // overlap bewteen any two references of any type is encountered, returns
+  // false and leaves the object in an invalid state. Otherwise, returns true.
+  bool InsertReferences(TypeTag type_tag, ReferenceReader&& ref_reader);
+
+  // Returns the number of reference type the index holds.
+  size_t TypeCount() const { return references_.size(); }
+
+  // Returns the number of target pool discovered.
+  size_t PoolCount() const { return pool_types_.size(); }
+
+  size_t LabelCount(PoolTag pool) const {
+    DCHECK_LT(pool.value(), label_count_.size());
+    return label_count_[pool.value()];
+  }
+
+  size_t LabelCount(TypeTag type) const {
+    return LabelCount(GetTraits(type).pool_tag);
+  }
+
+  // Returns traits describing references of type |type|.
+  const ReferenceTypeTraits& GetTraits(TypeTag type) const {
+    DCHECK_LT(type.value(), traits_.size());
+    return traits_[type.value()];
+  }
+
+  // Returns true if |raw_image[location]| is either:
+  // - A raw value.
+  // - The first byte of a reference.
+  bool IsToken(offset_t location) const;
+
+  // Returns true if |raw_image[location]| is part of a reference.
+  bool IsReference(offset_t location) const {
+    return GetType(location) != kNoTypeTag;
+  }
+
+  // Returns the type tag of the reference covering |location|, or kNoTypeTag if
+  // |location| is not part of a reference.
+  TypeTag GetType(offset_t location) const {
+    DCHECK_EQ(type_tags_.size(), raw_image_.size());  // Sanity check.
+    DCHECK_LT(location, size());
+    return type_tags_[location];
+  }
+
+  // Returns the raw value at |location|.
+  uint8_t GetRawValue(offset_t location) const {
+    DCHECK_LT(location, size());
+    return raw_image_[location];
+  }
+
+  // Returns the reference of type |type| covering |location|, which is required
+  // to is not part of a reference of type |type|.
+  Reference FindReference(TypeTag type, offset_t location) const;
+
+  // Returns a vector of references of type |type|, where references are sorted
+  // by their location.
+  const std::vector<Reference>& GetReferences(TypeTag type) const {
+    DCHECK_LT(type.value(), references_.size());
+    return references_[type.value()];
+  }
+
+  ConstBufferView raw_image() const { return raw_image_; }
+
+  // Returns the size of the image.
+  size_t size() const { return raw_image_.size(); }
+
+  // Replace every target represented as offset whose label is in
+  // |label_manager|  by the index of this label.
+  void LabelTargets(PoolTag pool, const BaseLabelManager& label_manager);
+
+  // Replace every target represented as offset whose label is in
+  // |label_manager| by the index of this label iff this index is also used in
+  // |reference_label_manager|.
+  void LabelAssociatedTargets(PoolTag pool,
+                              const BaseLabelManager& label_manager,
+                              const BaseLabelManager& reference_label_manager);
+
+  // Replace every target represented as an label index by its original offset,
+  // assuming that |label_manager| still holds the same labels referered to by
+  // target indices.
+  void UnlabelTargets(PoolTag pool, const BaseLabelManager& label_manager);
+
+ private:
+  const ConstBufferView raw_image_;
+
+  // Used for random access lookup of reference type, for each byte in
+  // |raw_image_|.
+  std::vector<TypeTag> type_tags_;
+
+  std::vector<ReferenceTypeTraits> traits_;
+  // For a given type, |references_[type]| stores a list of Reference instances
+  // sorted by location (file offset).
+  std::vector<std::vector<Reference>> references_;
+
+  // Enumerates type_tag for each pool.
+  std::vector<std::vector<TypeTag>> pool_types_;
+  // Number of labels for each pool.
+  std::vector<size_t> label_count_;
+
+  DISALLOW_COPY_AND_ASSIGN(ImageIndex);
+};
+
+}  // namespace zucchini
+
+#endif  // CHROME_INSTALLER_ZUCCHINI_IMAGE_INDEX_H_
---
+++ chrome/installer/zucchini/test_disassembler.h
@@ -0,0 +1,37 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_INSTALLER_ZUCCHINI_TEST_DISASSEMBLER_H_
+#define CHROME_INSTALLER_ZUCCHINI_TEST_DISASSEMBLER_H_
+
+#include <stddef.h>
+
+#include <vector>
+
+#include "base/optional.h"
+#include "chrome/installer/zucchini/disassembler.h"
+
+namespace zucchini {
+
+// A trivial ReferenceReader that reads injected references.
+class TestReferenceReader : public ReferenceReader {
+ public:
+  explicit TestReferenceReader(const std::vector<Reference>& refs)
+      : references_(refs) {}
+  ~TestReferenceReader() override = default;
+
+  base::Optional<Reference> GetNext() override {
+    if (index_ == references_.size())
+      return base::nullopt;
+    return references_[index_++];
+  }
+
+ private:
+  std::vector<Reference> references_;
+  size_t index_ = 0;
+};
+
+}  // namespace zucchini
+
+#endif  // CHROME_INSTALLER_ZUCCHINI_TEST_DISASSEMBLER_H_
