6b5f2f3f7f22ce3c91bad9146d7de056bcb0fc0b
Jian Li <jianli@chromium.org>
2017-07-28 16:02:47 -0700

    Add pipeline processing for download completed and archives import
    Bug: 701939
    Change-Id: Icbe44c4e4638621c514e698186e49e31701ee8f2

--- components/offline_pages/core/prefetch/test_prefetch_dispatcher.h
+++ components/offline_pages/core/prefetch/test_prefetch_dispatcher.h
@@ -31,6 +31,9 @@ class TestPrefetchDispatcher : public PrefetchDispatcher {
   void SetService(PrefetchService* service) override;
   void GCMOperationCompletedMessageReceived(
       const std::string& operation_name) override;
+  void DownloadCompleted(
+      const PrefetchDownloadResult& download_result) override;
+  void ImportCompleted(int64_t offline_id, bool success) override;
   void RequestFinishBackgroundTaskForTest() override;
 
   std::string latest_name_space;
