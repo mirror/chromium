4e758a0f6ff9d78aabc16a039bbb480588799c7b
yucliu <yucliu@chromium.org>
2017-07-28 12:40:20 -0700

    Serialize UnguessableToken to base::Value
    Add function to serialize UnguessableToken to hex string, so that it
    can be saved as base::Value in PrefService.
    
    It will be used in MediaDrmStorageImpl.
    
    BUG=493521
    TEST=UnguessableTokenTest
    
    Change-Id: I8185775e57dd4458f373ca89fd26a16c6daa346a

--- base/value_conversions.cc
+++ base/value_conversions.cc
@@ -7,15 +7,16 @@
 #include <stdint.h>
 
 #include <string>
+#include <vector>
 
 #include "base/files/file_path.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
+#include "base/unguessable_token.h"
 #include "base/values.h"
 
 namespace base {
-
 // |Value| internally stores strings in UTF-8, so we have to convert from the
 // system native code to UTF-8 and back.
 std::unique_ptr<Value> CreateFilePathValue(const FilePath& in_value) {
@@ -48,4 +49,34 @@ bool GetValueAsTimeDelta(const Value& value, TimeDelta* time) {
   return true;
 }
 
+std::unique_ptr<Value> CreateUnguessableTokenValue(
+    const UnguessableToken& token) {
+  uint64_t high_low[2] = {
+      token.GetHighForSerialization(), token.GetLowForSerialization(),
+  };
+
+  static_assert(sizeof(high_low) == 2 * sizeof(uint64_t),
+                "array size not match");
+
+  return MakeUnique<Value>(HexEncode(high_low, 2 * sizeof(uint64_t)));
+}
+
+bool GetValueAsUnguessableToken(const Value& value, UnguessableToken* token) {
+  if (!value.is_string()) {
+    return false;
+  }
+
+  std::vector<uint8_t> high_low_bytes;
+  if (!HexStringToBytes(value.GetString(), &high_low_bytes)) {
+    return false;
+  }
+
+  if (high_low_bytes.size() != 2 * sizeof(uint64_t)) {
+    return false;
+  }
+
+  uint64_t* high_low = reinterpret_cast<uint64_t*>(high_low_bytes.data());
+  *token = UnguessableToken::Deserialize(high_low[0], high_low[1]);
+  return true;
+}
 }  // namespace base
