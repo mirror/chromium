7ea5b7c6c7cc531e67664af5f4c527746a613716
Misha Efimov <mef@chromium.org>
2017-07-28 17:14:47 -0400

    Experiments with network service.
    Bug:
    Change-Id: I202f2415e06933cc9392d0d2a6dfb78c654965ec

--- content/network/network_service_unittest.cc
+++ content/network/network_service_unittest.cc
@@ -10,6 +10,10 @@
 #include "content/network/network_context.h"
 #include "content/network/network_service_impl.h"
 #include "content/public/common/network_service.mojom.h"
+#include "content/public/common/url_loader.mojom.h"
+#include "content/public/common/url_loader_factory.mojom.h"
+#include "content/public/test/test_url_loader_client.h"
+#include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace content {
@@ -64,6 +68,39 @@ TEST_F(NetworkServiceTest, DestroyingServiceDestroysContext) {
   // Destroying the service should destroy the context, causing a connection
   // error.
   run_loop.Run();
+}
+
+// Test shutdown in the case a NetworkContext is destroyed before the
+// NetworkService.
+TEST_F(NetworkServiceTest, CreateAndDestroyContextMef) {
+  mojom::NetworkContextPtr network_context;
+  mojom::NetworkContextParamsPtr context_params =
+      mojom::NetworkContextParams::New();
+
+  service()->CreateNetworkContext(mojo::MakeRequest(&network_context),
+                                  std::move(context_params));
+
+  mojom::URLLoaderFactoryPtr url_loader_factory;
+  network_context->CreateURLLoaderFactory(MakeRequest(&url_loader_factory), 0);
+  TestURLLoaderClient client;
+  ResourceRequest request;
+  request.url = GURL("Zhttps://www.google.com");
+  request.method = "GET";
+
+  mojom::URLLoaderPtr loader;
+  url_loader_factory->CreateLoaderAndStart(
+      mojo::MakeRequest(&loader), 2, 1, content::mojom::kURLLoadOptionNone,
+      request, client.CreateInterfacePtr(),
+      net::MutableNetworkTrafficAnnotationTag(TRAFFIC_ANNOTATION_FOR_TESTS));
+
+  client.RunUntilResponseReceived();
+  // SyncLoadResult result;
+
+  // url_loader_factory->SyncLoad(1,2, request, &result);
+
+  network_context.reset();
+  // Make sure the NetworkContext is destroyed.
+  base::RunLoop().RunUntilIdle();
 }
 
 }  // namespace
