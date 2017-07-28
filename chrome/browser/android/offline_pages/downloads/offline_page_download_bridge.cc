6bd5634182ce41f0f7642e323356a66244d02435
Cathy Li <chili@chromium.org>
2017-07-27 19:22:41 -0700

    [Offline pages]: Add ability to attribute download button clicks to apps
    Bug: 734753
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ia3cf2158295dc7243d4659938673a25ce44594d3

--- chrome/browser/android/offline_pages/downloads/offline_page_download_bridge.cc
+++ chrome/browser/android/offline_pages/downloads/offline_page_download_bridge.cc
@@ -96,7 +96,8 @@ content::WebContents* GetWebContentsFromJavaTab(
 
 void SavePageIfNotNavigatedAway(const GURL& url,
                                 const GURL& original_url,
-                                const ScopedJavaGlobalRef<jobject>& j_tab_ref) {
+                                const ScopedJavaGlobalRef<jobject>& j_tab_ref,
+                                const std::string& origin) {
   content::WebContents* web_contents = GetWebContentsFromJavaTab(j_tab_ref);
   if (!web_contents)
     return;
@@ -128,6 +129,7 @@ void SavePageIfNotNavigatedAway(const GURL& url,
       params.availability =
           RequestCoordinator::RequestAvailability::DISABLED_FOR_OFFLINER;
       params.original_url = original_url;
+      params.request_origin = origin;
       request_id = request_coordinator->SavePageLater(params);
     } else {
       DVLOG(1) << "SavePageIfNotNavigatedAway has no valid coordinator.";
@@ -152,7 +154,7 @@ void SavePageIfNotNavigatedAway(const GURL& url,
     }
     return;
   }
-  tab_helper->ObserveAndDownloadCurrentPage(client_id, request_id);
+  tab_helper->ObserveAndDownloadCurrentPage(client_id, request_id, origin);
 
   OfflinePageNotificationBridge notification_bridge;
   notification_bridge.ShowDownloadingToast();
@@ -161,9 +163,10 @@ void SavePageIfNotNavigatedAway(const GURL& url,
 void DuplicateCheckDone(const GURL& url,
                         const GURL& original_url,
                         const ScopedJavaGlobalRef<jobject>& j_tab_ref,
+                        const std::string& origin,
                         OfflinePageUtils::DuplicateCheckResult result) {
   if (result == OfflinePageUtils::DuplicateCheckResult::NOT_FOUND) {
-    SavePageIfNotNavigatedAway(url, original_url, j_tab_ref);
+    SavePageIfNotNavigatedAway(url, original_url, j_tab_ref, origin);
     return;
   }
 
@@ -174,7 +177,8 @@ void DuplicateCheckDone(const GURL& url,
   bool duplicate_request_exists =
       result == OfflinePageUtils::DuplicateCheckResult::DUPLICATE_REQUEST_FOUND;
   OfflinePageInfoBarDelegate::Create(
-      base::Bind(&SavePageIfNotNavigatedAway, url, original_url, j_tab_ref),
+      base::Bind(&SavePageIfNotNavigatedAway, url, original_url, j_tab_ref,
+                 origin),
       url, duplicate_request_exists, web_contents);
 }
 
@@ -388,7 +392,8 @@ jlong OfflinePageDownloadBridge::GetOfflineIdByGuid(
 void OfflinePageDownloadBridge::StartDownload(
     JNIEnv* env,
     const JavaParamRef<jobject>& obj,
-    const JavaParamRef<jobject>& j_tab) {
+    const JavaParamRef<jobject>& j_tab,
+    const JavaParamRef<jstring>& j_origin) {
   TabAndroid* tab = TabAndroid::GetNativeTab(env, j_tab);
   if (!tab)
     return;
@@ -400,6 +405,7 @@ void OfflinePageDownloadBridge::StartDownload(
   GURL url = web_contents->GetLastCommittedURL();
   if (url.is_empty())
     return;
+  std::string origin = ConvertJavaStringToUTF8(env, j_origin);
 
   GURL original_url =
       offline_pages::OfflinePageUtils::GetOriginalURLFromWebContents(
@@ -420,7 +426,7 @@ void OfflinePageDownloadBridge::StartDownload(
 
   OfflinePageUtils::CheckDuplicateDownloads(
       tab->GetProfile()->GetOriginalProfile(), url,
-      base::Bind(&DuplicateCheckDone, url, original_url, j_tab_ref));
+      base::Bind(&DuplicateCheckDone, url, original_url, j_tab_ref, origin));
 }
 
 void OfflinePageDownloadBridge::CancelDownload(
