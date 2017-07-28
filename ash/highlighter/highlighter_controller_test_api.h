b61eb272c67c07fa4d6c60fc7191f337685c8b25
Vladislav Kaznacheev <kaznacheev@google.com>
2017-07-28 11:04:02 -0700

    Implement prediction in HighlighterView
    Reuse FastInkPoints recently extracted from
    ash::LaserPointerView.
    
    Bug:748726
    Change-Id: I5d8651f6179440a7438a62db48a7fa867f6a1ea4

---
+++ ash/highlighter/highlighter_controller_test_api.h
@@ -0,0 +1,39 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_HIGHLIGHTER_HIGHLIGHTER_CONTROLLER_TEST_API_H_
+#define ASH_HIGHLIGHTER_HIGHLIGHTER_CONTROLLER_TEST_API_H_
+
+#include "base/macros.h"
+
+namespace ash {
+
+class FastInkPoints;
+class HighlighterController;
+class HighlighterSelectionObserver;
+
+// An api for testing the HighlighterController class.
+class HighlighterControllerTestApi {
+ public:
+  explicit HighlighterControllerTestApi(HighlighterController* instance);
+  ~HighlighterControllerTestApi();
+
+  void SetObserver(HighlighterSelectionObserver* observer);
+  void SetEnabled(bool enabled);
+  bool IsShowingHighlighter() const;
+  bool IsFadingAway() const;
+  bool IsShowingSelectionResult() const;
+  const FastInkPoints& points() const;
+  const FastInkPoints& predicted_points() const;
+
+ private:
+  HighlighterController* instance_;
+  HighlighterSelectionObserver* observer_;
+
+  DISALLOW_COPY_AND_ASSIGN(HighlighterControllerTestApi);
+};
+
+}  // namespace ash
+
+#endif  // ASH_HIGHLIGHTER_HIGHLIGHTER_CONTROLLER_TEST_API_H_
