571cd57b91da18e8b6e6b972388fcd7e5f8989f4
tzik <tzik@chromium.org>
2017-07-31 17:40:24 +0900

    Replace base::IndexSequence and its helper functions with C++14 std::index_sequence
    Change-Id: I162ec0698b4529267e26e1d648134be6b10bfcfd

--- components/login/base_screen_handler_utils.h
+++ components/login/base_screen_handler_utils.h
@@ -86,7 +86,7 @@ typename UnwrapConstRef<Arg>::Type ParseArg(const base::ListValue* args) {
 template <typename... Args, size_t... Ns>
 inline void DispatchToCallback(const base::Callback<void(Args...)>& callback,
                                const base::ListValue* args,
-                               base::IndexSequence<Ns...> indexes) {
+                               std::index_sequence<Ns...> indexes) {
   DCHECK(args);
   DCHECK_EQ(sizeof...(Args), args->GetSize());
 
@@ -96,8 +96,7 @@ inline void DispatchToCallback(const base::Callback<void(Args...)>& callback,
 template <typename... Args>
 void CallbackWrapper(const base::Callback<void(Args...)>& callback,
                      const base::ListValue* args) {
-  DispatchToCallback(callback, args,
-                     base::MakeIndexSequence<sizeof...(Args)>());
+  DispatchToCallback(callback, args, std::index_sequence_for<Args...>());
 }
 
 
