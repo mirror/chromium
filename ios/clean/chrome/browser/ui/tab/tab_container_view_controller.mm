a23765e56220fc51a625ff953937618c1bad6d5b
Mark Cogan <marq@google.com>
2017-07-31 13:17:42 -0700

    [ios clean] Fix crashes.
    This CL fixes a couple of common crashes in CSChromium.
    
    - Entering a URL on the NTP caused a crash because the webState wasn't enabled
    when it was navigating. This is fixed by setting webUsageEnabled during
    -loadGURLFromLocationBar:transition:
    
    - Tapping some buttons in the toolbar would cause a crash in the tab container
    VC's -canPerformAction:withSender:, which was creating an array with objects
    that might be nil. This method was only needed to plumb responder chain events
    into contained view controllers, which has long since been replaced by 
    dispatchers.
    
    Also, per the ObjC style guide, the exception raised in -subviewConstraints is
    replaced with a NOTREACHED().
    
    Bug:
    Change-Id: Ic22bc6decfde97530434c95f318b72cf637aaf24

--- ios/clean/chrome/browser/ui/tab/tab_container_view_controller.mm
+++ ios/clean/chrome/browser/ui/tab/tab_container_view_controller.mm
@@ -4,6 +4,7 @@
 
 #import "ios/clean/chrome/browser/ui/tab/tab_container_view_controller.h"
 
+#import "base/logging.h"
 #import "ios/clean/chrome/browser/ui/transitions/animators/swap_from_above_animator.h"
 #import "ios/clean/chrome/browser/ui/transitions/containment_transition_context.h"
 #import "ios/clean/chrome/browser/ui/transitions/containment_transitioning_delegate.h"
@@ -225,24 +226,6 @@ CGFloat kTabStripHeight = 120.0f;
   return CGRectNull;
 }
 
-#pragma mark - UIResponder
-
-// Before forwarding actions up the responder chain, give both contained
-// view controllers a chance to handle them.
-- (BOOL)canPerformAction:(SEL)action withSender:(id)sender {
-  self.actionToForward = nullptr;
-  self.forwardingTarget = nil;
-  for (UIResponder* responder in
-       @[ self.contentViewController, self.toolbarViewController ]) {
-    if ([responder canPerformAction:action withSender:sender]) {
-      self.actionToForward = action;
-      self.forwardingTarget = responder;
-      return YES;
-    }
-  }
-  return [super canPerformAction:action withSender:sender];
-}
-
 #pragma mark - NSObject method forwarding
 
 - (id)forwardingTargetForSelector:(SEL)aSelector {
@@ -261,9 +244,7 @@ CGFloat kTabStripHeight = 120.0f;
 #pragma mark - Abstract methods to be overriden by subclass
 
 - (Constraints*)subviewConstraints {
-  [NSException
-       raise:NSInternalInconsistencyException
-      format:@"You must override %@ in a subclass", NSStringFromSelector(_cmd)];
+  NOTREACHED() << "You must override -subviewConstraints in a subclass";
   return nil;
 }
 
