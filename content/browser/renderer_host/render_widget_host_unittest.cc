52bee8413b28d9de95f63addf5ea293e56796f1f
Weiliang Chen <weiliangc@chromium.org>
2017-07-31 13:09:57 -0400

    cc: Add Tracing to Compositor Frame Transport from Renderer
    This adds tracing for following cases:
    - serialization and deserialization of compositor frames in IPC,
    - deserialization of compositor frames in Mojo,
    - flow event from renderer to browser on desktop,
    - instance of time elapsed between submitting compositor frame to
    receiving from renderer to browser on desktop.
    
    R=danakj@chromium.org
    
    Bug: 699121
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I6ca1adc6dc61bc28c4c241ec63a67402f2edf7cf

--- content/browser/renderer_host/render_widget_host_unittest.cc
+++ content/browser/renderer_host/render_widget_host_unittest.cc
@@ -1435,7 +1435,8 @@ TEST_F(RenderWidgetHostTest, NewContentRenderingTimeout) {
   host_->StartNewContentRenderingTimeout(5);
   cc::CompositorFrame frame = MakeCompositorFrame(1.f, frame_size);
   frame.metadata.content_source_id = 5;
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+  host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                               base::TimeTicks());
   base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
       FROM_HERE, base::MessageLoop::QuitWhenIdleClosure(),
       TimeDelta::FromMicroseconds(20));
@@ -1449,7 +1450,8 @@ TEST_F(RenderWidgetHostTest, NewContentRenderingTimeout) {
   host_->StartNewContentRenderingTimeout(10);
   frame = MakeCompositorFrame(1.f, frame_size);
   frame.metadata.content_source_id = 9;
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+  host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                               base::TimeTicks());
   base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
       FROM_HERE, base::MessageLoop::QuitWhenIdleClosure(),
       TimeDelta::FromMicroseconds(20));
@@ -1462,7 +1464,8 @@ TEST_F(RenderWidgetHostTest, NewContentRenderingTimeout) {
   // attempt to start the timer. The timer shouldn't fire.
   frame = MakeCompositorFrame(1.f, frame_size);
   frame.metadata.content_source_id = 7;
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+  host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                               base::TimeTicks());
   host_->StartNewContentRenderingTimeout(7);
   base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
       FROM_HERE, base::MessageLoop::QuitWhenIdleClosure(),
@@ -1498,7 +1501,8 @@ TEST_F(RenderWidgetHostTest, SwapCompositorFrameWithBadSourceId) {
     cc::CompositorFrame frame = MakeCompositorFrame(1.f, frame_size);
     frame.metadata.begin_frame_ack = viz::BeginFrameAck(0, 1, true);
     frame.metadata.content_source_id = 99;
-    host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+    host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                                 base::TimeTicks());
     EXPECT_FALSE(
         static_cast<TestView*>(host_->GetView())->did_swap_compositor_frame());
     static_cast<TestView*>(host_->GetView())->reset_did_swap_compositor_frame();
@@ -1508,7 +1512,8 @@ TEST_F(RenderWidgetHostTest, SwapCompositorFrameWithBadSourceId) {
     // Test with a valid content ID as a control.
     cc::CompositorFrame frame = MakeCompositorFrame(1.f, frame_size);
     frame.metadata.content_source_id = 100;
-    host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+    host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                                 base::TimeTicks());
     EXPECT_TRUE(
         static_cast<TestView*>(host_->GetView())->did_swap_compositor_frame());
     static_cast<TestView*>(host_->GetView())->reset_did_swap_compositor_frame();
@@ -1520,7 +1525,8 @@ TEST_F(RenderWidgetHostTest, SwapCompositorFrameWithBadSourceId) {
     // the corresponding DidCommitProvisionalLoad (it's a race).
     cc::CompositorFrame frame = MakeCompositorFrame(1.f, frame_size);
     frame.metadata.content_source_id = 101;
-    host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+    host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                                 base::TimeTicks());
     EXPECT_TRUE(
         static_cast<TestView*>(host_->GetView())->did_swap_compositor_frame());
   }
@@ -2034,7 +2040,8 @@ TEST_F(RenderWidgetHostTest, FrameToken_MessageThenFrame) {
 
   cc::CompositorFrame frame = MakeCompositorFrame(1.f, frame_size);
   frame.metadata.frame_token = frame_token;
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+  host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                               base::TimeTicks());
   EXPECT_EQ(0u, host_->queued_messages_.size());
   EXPECT_EQ(1u, host_->processed_frame_messages_count());
 }
@@ -2054,7 +2061,8 @@ TEST_F(RenderWidgetHostTest, FrameToken_FrameThenMessage) {
 
   cc::CompositorFrame frame = MakeCompositorFrame(1.f, frame_size);
   frame.metadata.frame_token = frame_token;
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+  host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                               base::TimeTicks());
   EXPECT_EQ(0u, host_->queued_messages_.size());
   EXPECT_EQ(0u, host_->processed_frame_messages_count());
 
@@ -2092,13 +2100,15 @@ TEST_F(RenderWidgetHostTest, FrameToken_MultipleMessagesThenTokens) {
 
   cc::CompositorFrame frame = MakeCompositorFrame(1.f, frame_size);
   frame.metadata.frame_token = frame_token1;
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+  host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                               base::TimeTicks());
   EXPECT_EQ(1u, host_->queued_messages_.size());
   EXPECT_EQ(1u, host_->processed_frame_messages_count());
 
   frame = MakeCompositorFrame(1.f, frame_size);
   frame.metadata.frame_token = frame_token2;
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+  host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                               base::TimeTicks());
   EXPECT_EQ(0u, host_->queued_messages_.size());
   EXPECT_EQ(2u, host_->processed_frame_messages_count());
 }
@@ -2121,13 +2131,15 @@ TEST_F(RenderWidgetHostTest, FrameToken_MultipleTokensThenMessages) {
 
   cc::CompositorFrame frame = MakeCompositorFrame(1.f, frame_size);
   frame.metadata.frame_token = frame_token1;
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+  host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                               base::TimeTicks());
   EXPECT_EQ(0u, host_->queued_messages_.size());
   EXPECT_EQ(0u, host_->processed_frame_messages_count());
 
   frame = MakeCompositorFrame(1.f, frame_size);
   frame.metadata.frame_token = frame_token2;
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+  host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                               base::TimeTicks());
   EXPECT_EQ(0u, host_->queued_messages_.size());
   EXPECT_EQ(0u, host_->processed_frame_messages_count());
 
@@ -2170,7 +2182,8 @@ TEST_F(RenderWidgetHostTest, FrameToken_DroppedFrame) {
 
   cc::CompositorFrame frame = MakeCompositorFrame(1.f, frame_size);
   frame.metadata.frame_token = frame_token2;
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+  host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                               base::TimeTicks());
   EXPECT_EQ(0u, host_->queued_messages_.size());
   EXPECT_EQ(2u, host_->processed_frame_messages_count());
 }
@@ -2205,7 +2218,8 @@ TEST_F(RenderWidgetHostTest, FrameToken_RendererCrash) {
 
   cc::CompositorFrame frame = MakeCompositorFrame(1.f, frame_size);
   frame.metadata.frame_token = frame_token2;
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+  host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                               base::TimeTicks());
   EXPECT_EQ(0u, host_->queued_messages_.size());
   EXPECT_EQ(0u, host_->processed_frame_messages_count());
 
@@ -2221,7 +2235,8 @@ TEST_F(RenderWidgetHostTest, FrameToken_RendererCrash) {
 
   frame = MakeCompositorFrame(1.f, frame_size);
   frame.metadata.frame_token = frame_token3;
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame));
+  host_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                               base::TimeTicks());
   EXPECT_EQ(0u, host_->queued_messages_.size());
   EXPECT_EQ(1u, host_->processed_frame_messages_count());
 }
