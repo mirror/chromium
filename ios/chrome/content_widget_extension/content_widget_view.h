22fbabf127d1374b60b47f4b40187672af8623ee
Elodie Banel <lod@google.com>
2017-07-31 17:05:39 +0200

    Content widget UI, not including use of real data.
    The next CL will use the real data to update the UI. See screenshots
    linked below for what it looks like with this CL and test data.
    
    https://drive.google.com/a/chromium.org/file/d/0BxrfNSu8yqJqZG9reWl3T0FCclE/view?usp=sharing
    https://drive.google.com/a/chromium.org/file/d/0BxrfNSu8yqJqUkNUVlFlYW5Uckk/view?usp=sharing
    
    Bug:622743
    Change-Id: Icf8072513c202f9a0909d246c1476e2e2a55a705

--- ios/chrome/content_widget_extension/content_widget_view.h
+++ ios/chrome/content_widget_extension/content_widget_view.h
@@ -7,7 +7,27 @@
 
 #import <UIKit/UIKit.h>
 
+// View for the content widget. Shows 1 (compact)or 2 (expanded) rows of 4 most
+// visited tiles (favicon or fallback + title).
 @interface ContentWidgetView : UIView
+
+// Designated initializer, creates the widget view. |compactHeight| indicates
+// the size to use in compact display. |initiallyCompact| indicates which mode
+// to display on initialization.
+- (instancetype)initWithCompactHeight:(CGFloat)compactHeight
+                     initiallyCompact:(BOOL)compact NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+- (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
+
+// Changes the display mode of the view to compact or expanded.
+- (void)showMode:(BOOL)compact;
+
+// Gets the height of the widget.
+- (CGFloat)widgetHeight;
+
+// Updates the displayed sites.
+- (void)updateSites;
+
 @end
 
 #endif  // IOS_CHROME_CONTENT_WIDGET_EXTENSION_CONTENT_WIDGET_VIEW_H_
