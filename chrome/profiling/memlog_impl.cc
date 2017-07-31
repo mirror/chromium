dc44f8c0ed41d2e888e4fd6c5b7c09ce5a58ae49
Albert J. Wong <ajwong@chromium.org>
2017-07-31 15:52:09 -0700

    memlog: Reimagine as a Profiling Service.
    This radically changes the process broker architecture.
    
    Instead of trying to manage pipe passing by hand, this instead uses
    ServiceManager to handle lifetime of the Profiling process as well as
    passing of the handles.
    
    Major changes:
      (1) Profilng process started on demand.
      (2) Browser process logging delayed until ServiceManager has started.
      (3) Renderers request pipe from Memlog process via Memlog interface
          instead of having them passed down via the memlog-pipe flag.
      (4) The PlatformChannelPair is created in the profiling process.
    
    This allows us to leverage all the infrastructure built up for
    servicification to launch utility processes in all platforms at the
    cost of later startup. It also simplifies lifetime design as there is
    no need to worry about registering crash handlers, message loop globals,
    etc for the profiling process.
    
    Bug:
    Change-Id: Ibd1cbadd1d7c946ae442f259c3e0ae1b95acb165

---
+++ chrome/profiling/memlog_impl.cc
@@ -0,0 +1,42 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/profiling/memlog_impl.h"
+
+#include "chrome/profiling/memlog_receiver_pipe.h"
+#include "content/public/child/child_thread.h"
+#include "mojo/public/cpp/system/platform_handle.h"
+
+namespace profiling {
+
+MemlogImpl::MemlogImpl()
+    : io_runner_(content::ChildThread::Get()->GetIOTaskRunner()),
+      connection_manager_(
+          new MemlogConnectionManager(io_runner_, &backtrace_storage_),
+          DeleteOnRunner(FROM_HERE, io_runner_.get())) {
+  // The pipe code all expects an IO message loop. If somehow Mojo
+  // servicification in the future runs on a pool or something, notice this.
+}
+
+MemlogImpl::~MemlogImpl() {}
+
+void MemlogImpl::AddSender(mojo::ScopedHandle sender_pipe, int32_t sender_id) {
+  base::PlatformFile platform_file;
+  CHECK_EQ(MOJO_RESULT_OK,
+           mojo::UnwrapPlatformFile(std::move(sender_pipe), &platform_file));
+
+  // MemlogConnectionManager is deleted on the IOThread making the use of
+  // base::Unretained() safe here.
+  io_runner_->PostTask(
+      FROM_HERE,
+      base::BindOnce(&MemlogConnectionManager::OnNewConnection,
+                     base::Unretained(connection_manager_.get()),
+                     base::ScopedPlatformFile(platform_file), sender_id));
+}
+
+void MemlogImpl::DumpProcess(int32_t sender_id) {
+  LOG(ERROR) << "DumpProcess called for " << sender_id;
+}
+
+}  // namespace profiling
