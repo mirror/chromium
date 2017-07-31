4c4c45c722600cde1c725dcdf95096ffb890b6f9
Duc Bui <ducbui@google.com>
2017-07-30 17:52:16 -0700

    [GRC] Track Expected Task Queueing Duration of tabs in GRC.
    We want to track the smoothness of foreground tabs during session restore,
    which can be estimated by their ExpectedTaskQueueingDuration.
    
    This patch sends ExpectedTaskQueueingDuration metric measured in Blink
    RendererScheduler to GRC.
    
    BUG=737817
    
    Change-Id: I54be5ef1171fb885598e259ffe0580631b8e6f9c

---
+++ third_party/WebKit/Source/platform/instrumentation/resource_coordinator/RendererResourceCoordinator.cpp
@@ -0,0 +1,38 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/instrumentation/resource_coordinator/RendererResourceCoordinator.h"
+
+#include "public/platform/Platform.h"
+
+namespace blink {
+
+namespace {
+
+RendererResourceCoordinator* g_renderer_resource_coordinator = nullptr;
+
+}  // namespace
+
+// static
+void RendererResourceCoordinator::Initialize() {
+  blink::Platform* platform = Platform::Current();
+  DCHECK(platform);
+  g_renderer_resource_coordinator = new RendererResourceCoordinator(
+      platform->GetConnector(), platform->GetNavigationServiceName());
+}
+
+// static
+RendererResourceCoordinator& RendererResourceCoordinator::Get() {
+  DCHECK(g_renderer_resource_coordinator);
+  return *g_renderer_resource_coordinator;
+}
+
+RendererResourceCoordinator::RendererResourceCoordinator(
+    service_manager::Connector* connector,
+    const std::string& navigation_service_name)
+    : BlinkResourceCoordinatorBase(connector, navigation_service_name) {}
+
+RendererResourceCoordinator::~RendererResourceCoordinator() = default;
+
+}  // namespace blink
