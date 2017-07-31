01819ba86050bfbae95d77a4661595abd5143f53
Elodie Banel <lod@google.com>
2017-07-31 18:43:35 +0200

    Content widget UI, not including use of real data.
    The next CL will use the real data to update the UI. See screenshots
    linked below for what it looks like with this CL and test data.
    
    https://drive.google.com/a/chromium.org/file/d/0BxrfNSu8yqJqZG9reWl3T0FCclE/view?usp=sharing
    https://drive.google.com/a/chromium.org/file/d/0BxrfNSu8yqJqUkNUVlFlYW5Uckk/view?usp=sharing
    
    Bug:622743
    Change-Id: Icf8072513c202f9a0909d246c1476e2e2a55a705

--- ios/chrome/content_widget_extension/content_widget_view.h
+++ ios/chrome/content_widget_extension/content_widget_view.h
@@ -7,7 +7,24 @@
 
 #import <UIKit/UIKit.h>
 
+// View for the content widget. Shows 1 (compact)or 2 (expanded) rows of 4 most
+// visited tiles (favicon or fallback + title).
 @interface ContentWidgetView : UIView
+
+// The height of the widget in expanded mode.
+@property(nonatomic, readonly) CGFloat widgetExpandedHeight;
+
+// Designated initializer, creates the widget view. |compactHeight| indicates
+// the size to use in compact display. |initiallyCompact| indicates which mode
+// to display on initialization.
+- (instancetype)initWithCompactHeight:(CGFloat)compactHeight
+                     initiallyCompact:(BOOL)compact NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+- (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
+
+// Changes the display mode of the view to compact or expanded.
+- (void)showMode:(BOOL)compact;
+
 @end
 
 #endif  // IOS_CHROME_CONTENT_WIDGET_EXTENSION_CONTENT_WIDGET_VIEW_H_
--- ios/chrome/content_widget_extension/content_widget_view.mm
+++ ios/chrome/content_widget_extension/content_widget_view.mm
@@ -4,12 +4,143 @@
 
 #import "ios/chrome/content_widget_extension/content_widget_view.h"
 
-#include "base/ios/ios_util.h"
-#include "base/logging.h"
+#import "ios/chrome/browser/ui/constraints_ui_util.h"
+#import "ios/chrome/browser/ui/favicon/favicon_view.h"
+#import "ios/chrome/content_widget_extension/most_visited_tile_view.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
+namespace {
+// Spacing between tiles.
+const CGFloat kTileSpacing = 16;
+// Height of a tile row.
+const CGFloat kTileHeight = 100;
+}
+
+@interface ContentWidgetView ()
+
+// The first row of sites.
+@property(nonatomic, strong) UIView* firstRow;
+// The second row of sites.
+@property(nonatomic, strong) UIView* secondRow;
+// The height used in the compact display mode.
+@property(nonatomic) CGFloat compactHeight;
+// The first row's height constraint. Set its constant to modify the first row's
+// height.
+@property(nonatomic, strong) NSLayoutConstraint* firstRowHeightConstraint;
+
+// Sets up the widget UI for an expanded or compact appearance based on
+// |compact|.
+- (void)createUI:(BOOL)compact;
+
+// Creates the view for a row of 4 sites.
+- (UIView*)createRowOfSites;
+
+// Returns the height to use for the first row, depending on the display mode.
+- (CGFloat)firstRowHeight:(BOOL)compact;
+
+@end
+
 @implementation ContentWidgetView
+
+@synthesize firstRow = _firstRow;
+@synthesize secondRow = _secondRow;
+@synthesize compactHeight = _compactHeight;
+@synthesize firstRowHeightConstraint = _firstRowHeightConstraint;
+
+- (instancetype)initWithCompactHeight:(CGFloat)compactHeight
+                     initiallyCompact:(BOOL)compact {
+  self = [super initWithFrame:CGRectZero];
+  if (self) {
+    _compactHeight = compactHeight;
+    [self createUI:compact];
+  }
+  return self;
+}
+
+#pragma mark - properties
+
+- (CGFloat)widgetExpandedHeight {
+  // The expanded height must be at least as tall as the compact height as
+  // widgets cannot be smaller than this. The prefered height for the widget
+  // when expanded is the height of the two rows of sites + padding at top,
+  // bottom and between the two rows.
+  return MAX(2 * kTileHeight + 3 * kTileSpacing, self.compactHeight);
+}
+
+#pragma mark - UI creation
+
+- (void)createUI:(BOOL)compact {
+  _firstRow = [self createRowOfSites];
+  _secondRow = [self createRowOfSites];
+
+  [self addSubview:_firstRow];
+  [self addSubview:_secondRow];
+
+  _firstRowHeightConstraint = [_firstRow.heightAnchor
+      constraintEqualToConstant:[self firstRowHeight:compact]];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [_firstRow.topAnchor constraintEqualToAnchor:self.topAnchor],
+    [_secondRow.topAnchor constraintEqualToAnchor:_firstRow.bottomAnchor],
+    [self.leadingAnchor constraintEqualToAnchor:_firstRow.leadingAnchor],
+    [self.leadingAnchor constraintEqualToAnchor:_secondRow.leadingAnchor],
+    [self.trailingAnchor constraintEqualToAnchor:_firstRow.trailingAnchor],
+    [self.trailingAnchor constraintEqualToAnchor:_secondRow.trailingAnchor],
+    _firstRowHeightConstraint,
+  ]];
+}
+
+- (UIView*)createRowOfSites {
+  NSMutableArray<MostVisitedTileView*>* cells = [[NSMutableArray alloc] init];
+  for (int i = 0; i < 4; i++) {
+    cells[i] = [[MostVisitedTileView alloc] init];
+    cells[i].translatesAutoresizingMaskIntoConstraints = NO;
+  }
+
+  UIStackView* stack = [[UIStackView alloc] initWithArrangedSubviews:cells];
+  stack.translatesAutoresizingMaskIntoConstraints = NO;
+  stack.axis = UILayoutConstraintAxisHorizontal;
+  stack.alignment = UIStackViewAlignmentTop;
+  stack.distribution = UIStackViewDistributionEqualSpacing;
+  stack.layoutMargins = UIEdgeInsetsZero;
+  stack.spacing = kTileSpacing;
+  stack.layoutMarginsRelativeArrangement = YES;
+
+  UIView* container = [[UIView alloc] initWithFrame:CGRectZero];
+  container.translatesAutoresizingMaskIntoConstraints = NO;
+  [container addSubview:stack];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [stack.centerYAnchor constraintEqualToAnchor:container.centerYAnchor],
+    [stack.centerXAnchor constraintEqualToAnchor:container.centerXAnchor],
+    [container.heightAnchor constraintGreaterThanOrEqualToConstant:kTileHeight],
+  ]];
+
+  return container;
+}
+
+- (CGFloat)firstRowHeight:(BOOL)compact {
+  if (compact) {
+    return self.compactHeight;
+  }
+
+  CGFloat firstRowHeight = kTileHeight + 2 * kTileSpacing;
+  CGFloat secondRowHeight = kTileHeight + kTileSpacing;
+  CGFloat totalHeight = firstRowHeight + secondRowHeight;
+  if (totalHeight >= self.compactHeight) {
+    return firstRowHeight;
+  }
+
+  return self.compactHeight - secondRowHeight;
+}
+
+#pragma mark - ContentWidgetView
+
+- (void)showMode:(BOOL)compact {
+  self.firstRowHeightConstraint.constant = [self firstRowHeight:compact];
+}
+
 @end
