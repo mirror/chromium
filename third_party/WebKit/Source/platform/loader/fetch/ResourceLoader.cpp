62a06e39f391214d8887141d00637f29ef9472c2
Takeshi Yoshino <tyoshino@chromium.org>
2017-08-01 00:16:58 +0900

    Clean up console message methods on the FetchContext family classes
    - It's confusing that FetchContext has AddConsoleMessage(const String&)
      with a default argument while BaseFetchContext has one with
      ConsoleMessage* argument
    - Not to depend on the core/ constants, FetchContext has LogMessageType,
      but maybe it's clearer and not much more code if we just have methods
      for each level given the amount of combinations is small
    - Let WorkerFetchContext also print messages as
      AddConsoleMessage(ConsoleMessage*) is provided by it too
    - Unify FrameFetchContext::AddConsoleMessage(ConsoleMessage*) and
      FrameFetchContext::AddConsoleMessage(const String&, ...)
    
    Bug:
    Change-Id: Ia0bd40c50203b1101ff529eeacc830b504e85d07

--- third_party/WebKit/Source/platform/loader/fetch/ResourceLoader.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/ResourceLoader.cpp
@@ -285,7 +285,7 @@ bool ResourceLoader::WillFollowRedirect(
         resource_->SetCORSStatus(CORSStatus::kFailed);
 
         if (!unused_preload)
-          Context().AddConsoleMessage(cors_error_msg);
+          Context().AddJSErrorConsoleMessage(cors_error_msg);
 
         CancelForRedirectAccessCheckError(new_url,
                                           ResourceRequestBlockedReason::kOther);
@@ -546,7 +546,7 @@ void ResourceLoader::DidReceiveResponse(
 
     if (!resource_->IsSameOriginOrCORSSuccessful()) {
       if (!unused_preload)
-        Context().AddConsoleMessage(cors_error_msg.ToString());
+        Context().AddJSErrorConsoleMessage(cors_error_msg.ToString());
 
       HandleError(ResourceError::CancelledDueToAccessCheckError(
           response_url, ResourceRequestBlockedReason::kOther));
