bc123be8cb3ab6473da1f435264fb3d9931d569b
Duc Bui <ducbui@google.com>
2017-07-31 15:38:02 -0700

    [GRC] Send Expected Task Queueing Duration to TabManagerStatsCollector.
    We want to track the smoothness of foreground tabs during session
    restore, which can be estimated by their ExpectedTaskQueueingDuration.
    
    This patch sends ExpectedTaskQueueingDuration metric values from GRC to
    TabManagerStatsCollector.
    
    Bug:737817
    Change-Id: I07ff8aa9735c73fa3180564101cf2692c776b469

---
+++ services/resource_coordinator/coordination_unit/tab_signal_generator_impl_unittest.cc
@@ -0,0 +1,60 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/resource_coordinator/coordination_unit/tab_signal_generator_impl.h"
+#include "services/resource_coordinator/coordination_unit/coordination_unit_impl.h"
+#include "services/resource_coordinator/coordination_unit/coordination_unit_impl_unittest_util.h"
+#include "services/resource_coordinator/coordination_unit/mock_coordination_unit_graphs.h"
+#include "services/resource_coordinator/public/cpp/coordination_unit_types.h"
+#include "services/resource_coordinator/public/interfaces/coordination_unit.mojom.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace resource_coordinator {
+
+namespace {
+
+class TabSignalGeneratorImplTest : public CoordinationUnitImplTestBase {};
+
+}  // namespace
+
+class MockTabSignalGeneratorImpl : public TabSignalGeneratorImpl {
+ public:
+  // Overridden from CoordinationUnitGraphObserver.
+  bool ShouldObserve(const CoordinationUnitImpl* coordination_unit) override {
+    return coordination_unit->id().type == CoordinationUnitType::kWebContents;
+  }
+  void OnPropertyChanged(const CoordinationUnitImpl* coordination_unit,
+                         const mojom::PropertyType property_type,
+                         const base::Value& value) override {
+    ++property_changed_count_;
+  }
+
+  size_t property_changed_count() const { return property_changed_count_; }
+
+ private:
+  size_t property_changed_count_ = 0;
+};
+
+TEST_F(TabSignalGeneratorImplTest,
+       CalculateTabEQTForSingleTabWithMultipleProcesses) {
+  MockSingleTabWithMultipleProcessesCoordinationUnitGraph cu_graph;
+  MockTabSignalGeneratorImpl mock_signal_generator;
+  cu_graph.tab->AddObserver(&mock_signal_generator);
+
+  cu_graph.process->SetProperty(
+      mojom::PropertyType::kExpectedTaskQueueingDuration,
+      base::MakeUnique<base::Value>(1.0));
+  cu_graph.other_process->SetProperty(
+      mojom::PropertyType::kExpectedTaskQueueingDuration,
+      base::MakeUnique<base::Value>(10.0));
+
+  // The |other_process| is not for the main frame so its EQT values does not
+  // propagate to the tab.
+  EXPECT_EQ(1u, mock_signal_generator.property_changed_count());
+  EXPECT_EQ(base::Value(1.0),
+            cu_graph.tab->GetProperty(
+                mojom::PropertyType::kExpectedTaskQueueingDuration));
+}
+
+}  // namespace resource_coordinator
