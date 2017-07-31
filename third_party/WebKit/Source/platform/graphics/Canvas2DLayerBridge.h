9289c4d5597604dacb026c2ba123b3a8a34651fa
Xida Chen <xidachen@chromium.org>
2017-07-31 16:16:44 -0400

    Remove self_ref in Canvas2DLayerBridge
    In the current implementation of MailboxInfo, we keep a RefPtr to
    Canvas2DLayerBridge, and when the MailboxReleased function gets called,
    we do a self_ref to keep the Canvas2DLayerBridge alive.
    
    Now that the MailboxReleased function is called through a WTF::Bind to
    a WeakPtr of Canvas2DLayerBridge, we don't need the self_ref anymore.
    Also, we can remove the mailboxes_ that is kept in this class, we just
    need to remove the associated resources.
    
    Bug: None
    Change-Id: I7f087a23a7fca32272d0ad05efb66f92d9eabba8

--- third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridge.h
+++ third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridge.h
@@ -165,22 +165,16 @@ class PLATFORM_EXPORT Canvas2DLayerBridge
 
   void SetLoggerForTesting(std::unique_ptr<Logger>);
 
- private:
-  void ResetSurface();
-
-  // Callback for mailboxes given to the compositor from PrepareTextureMailbox.
-  void MailboxReleased(const gpu::Mailbox&,
-                       const gpu::SyncToken&,
-                       bool lost_resource);
-  bool IsHidden() { return is_hidden_; }
-
+// TODO(xidachen): the ImageInfo and MailboxInfo structures should not be
+// exposed. This can be fixed later on when we pass the resources that is
+// stored in the MailboxInfo instead of passing the MailboxInfo as a param
+// in the ReleaseFrameResources.
 #if USE_IOSURFACE_FOR_2D_CANVAS
   // All information associated with a CHROMIUM image.
   struct ImageInfo;
 #endif  // USE_IOSURFACE_FOR_2D_CANVAS
 
   struct MailboxInfo {
-    DISALLOW_NEW_EXCEPT_PLACEMENT_NEW();
     gpu::Mailbox mailbox_;
     sk_sp<SkImage> image_;
     RefPtr<Canvas2DLayerBridge> parent_layer_bridge_;
@@ -194,6 +188,19 @@ class PLATFORM_EXPORT Canvas2DLayerBridge
     MailboxInfo(const MailboxInfo&);
     MailboxInfo();
   };
+
+  // Callback for mailboxes given to the compositor from PrepareTextureMailbox.
+  friend void ReleaseFrameResources(
+      WeakPtr<Canvas2DLayerBridge>,
+      WeakPtr<WebGraphicsContext3DProviderWrapper>,
+      std::unique_ptr<MailboxInfo>,
+      const gpu::Mailbox&,
+      const gpu::SyncToken&,
+      bool lost_resource);
+
+ private:
+  void ResetSurface();
+  bool IsHidden() { return is_hidden_; }
 
   gpu::gles2::GLES2Interface* ContextGL();
   void StartRecording();
@@ -213,14 +220,13 @@ class PLATFORM_EXPORT Canvas2DLayerBridge
   // MailboxInfo, and prepended it to |m_mailboxs|. Returns whether the
   // mailbox was successfully prepared. |mailbox| is an out parameter only
   // populated on success.
-  bool PrepareIOSurfaceMailboxFromImage(SkImage*, viz::TextureMailbox*);
+  bool PrepareIOSurfaceMailboxFromImage(SkImage*,
+                                        MailboxInfo*,
+                                        viz::TextureMailbox*);
 
   // Creates an IOSurface-backed texture. Returns an ImageInfo, which is empty
   // on failure. The caller takes ownership of both the texture and the image.
   RefPtr<ImageInfo> CreateIOSurfaceBackedTexture();
-
-  // Releases all resources associated with a CHROMIUM image.
-  void DeleteCHROMIUMImage(RefPtr<ImageInfo>);
 
   // Releases all resources in the CHROMIUM image cache.
   void ClearCHROMIUMImageCache();
@@ -231,11 +237,9 @@ class PLATFORM_EXPORT Canvas2DLayerBridge
 
   // Returns whether the mailbox was successfully prepared from the SkImage.
   // The mailbox is an out parameter only populated on success.
-  bool PrepareMailboxFromImage(sk_sp<SkImage>, viz::TextureMailbox*);
-
-  // Resets Skia's texture bindings. This method should be called after
-  // changing texture bindings.
-  void ResetSkiaTextureBinding();
+  bool PrepareMailboxFromImage(sk_sp<SkImage>,
+                               MailboxInfo*,
+                               viz::TextureMailbox*);
 
   // Used for cloning context_provider_wrapper_ into an rvalue
   WeakPtr<WebGraphicsContext3DProviderWrapper> ContextProviderWrapper() const {
@@ -274,14 +278,6 @@ class PLATFORM_EXPORT Canvas2DLayerBridge
 
   uint32_t last_image_id_;
 
-  enum {
-    // We should normally not have more that two active mailboxes at a time,
-    // but sometimes we may have three due to the async nature of mailbox
-    // handling.
-    kMaxActiveMailboxes = 3,
-  };
-
-  Deque<MailboxInfo, kMaxActiveMailboxes> mailboxes_;
   GLenum last_filter_;
   AccelerationMode acceleration_mode_;
   OpacityMode opacity_mode_;
