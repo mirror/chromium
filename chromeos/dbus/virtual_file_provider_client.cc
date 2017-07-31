f843657bd3e20f45aa708a8d39503cef3c0be82b
Ryo Hashimoto <hashimoto@chromium.org>
2017-07-31 16:15:01 +0900

    chromeos: Add VirtualFileProvider D-Bus client
    VirtualFileProvider is a service which provides file descriptors which forward read requests to chrome.
    This is useful to let applications to read virtual files managed by chrome.
    
    BUG=740063
    TEST=build
    
    Change-Id: Idbb52590a586dbc55bba52979eec8a2790d8ac60

---
+++ chromeos/dbus/virtual_file_provider_client.cc
@@ -0,0 +1,130 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/dbus/virtual_file_provider_client.h"
+
+#include <utility>
+
+#include "base/bind.h"
+#include "base/bind_helpers.h"
+#include "base/callback.h"
+#include "dbus/bus.h"
+#include "dbus/message.h"
+#include "dbus/object_proxy.h"
+
+namespace chromeos {
+
+namespace {
+
+// TODO(hashimoto): Share these constants with the service.
+// D-Bus service constants.
+constexpr char kVirtualFileProviderInterface[] =
+    "org.chromium.VirtualFileProviderInterface";
+constexpr char kVirtualFileProviderServicePath[] =
+    "/org/chromium/VirtualFileProvider";
+constexpr char kVirtualFileProviderServiceName[] =
+    "org.chromium.VirtualFileProvider";
+
+// Method names.
+constexpr char kOpenFileMethod[] = "OpenFile";
+
+// Signal names.
+constexpr char kReadRequestSignal[] = "ReadRequest";
+
+class VirtualFileProviderClientImpl : public VirtualFileProviderClient {
+ public:
+  VirtualFileProviderClientImpl() : weak_ptr_factory_(this) {}
+  ~VirtualFileProviderClientImpl() override {}
+
+  // VirtualFileProviderClient override:
+  void OpenFile(OpenFileCallback callback) override {
+    dbus::MethodCall method_call(kVirtualFileProviderInterface,
+                                 kOpenFileMethod);
+    proxy_->CallMethod(&method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+                       base::Bind(&VirtualFileProviderClientImpl::OnOpenFile,
+                                  weak_ptr_factory_.GetWeakPtr(),
+                                  base::Passed(std::move(callback))));
+  }
+
+  void SetReadRequestHandler(const ReadRequestHandler& handler) override {
+    read_request_handler_ = handler;
+  }
+
+ protected:
+  // DBusClient override.
+  void Init(dbus::Bus* bus) override {
+    proxy_ =
+        bus->GetObjectProxy(kVirtualFileProviderServiceName,
+                            dbus::ObjectPath(kVirtualFileProviderServicePath));
+    proxy_->ConnectToSignal(
+        kVirtualFileProviderInterface, kReadRequestSignal,
+        base::Bind(&VirtualFileProviderClientImpl::OnReadRequest,
+                   weak_ptr_factory_.GetWeakPtr()),
+        base::Bind(&VirtualFileProviderClientImpl::OnSignalConnected,
+                   weak_ptr_factory_.GetWeakPtr()));
+  }
+
+ private:
+  // Handles signal connection result.
+  void OnSignalConnected(const std::string& interface,
+                         const std::string& signal,
+                         bool succeeded) {
+    LOG_IF(ERROR, !succeeded)
+        << "Signal connection failed: " << interface << "." << signal;
+  }
+
+  // Handles ReadRequest signal.
+  void OnReadRequest(dbus::Signal* signal) {
+    dbus::MessageReader reader(signal);
+    std::string id;
+    int64_t offset = 0;
+    int64_t size = 0;
+    base::ScopedFD pipe_write_end;
+    if (!reader.PopString(&id) || !reader.PopInt64(&offset) ||
+        !reader.PopInt64(&size) || !reader.PopFileDescriptor(&pipe_write_end)) {
+      LOG(ERROR) << "Invalid signal: " << signal->ToString();
+      return;
+    }
+    if (!read_request_handler_.is_null())
+      read_request_handler_.Run(id, offset, size, std::move(pipe_write_end));
+  }
+
+  // Runs the callback with OpenFile method call result.
+  void OnOpenFile(OpenFileCallback callback, dbus::Response* response) {
+    if (!response) {
+      std::move(callback).Run(std::string(), base::ScopedFD());
+      return;
+    }
+    dbus::MessageReader reader(response);
+    std::string id;
+    base::ScopedFD fd;
+    if (!reader.PopString(&id) || !reader.PopFileDescriptor(&fd)) {
+      LOG(ERROR) << "Invalid method call result.";
+      std::move(callback).Run(std::string(), base::ScopedFD());
+      return;
+    }
+    std::move(callback).Run(id, std::move(fd));
+  }
+
+  dbus::ObjectProxy* proxy_ = nullptr;
+
+  ReadRequestHandler read_request_handler_;
+
+  base::WeakPtrFactory<VirtualFileProviderClientImpl> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(VirtualFileProviderClientImpl);
+};
+
+}  // namespace
+
+VirtualFileProviderClient::VirtualFileProviderClient() {}
+
+VirtualFileProviderClient::~VirtualFileProviderClient() {}
+
+// static
+VirtualFileProviderClient* VirtualFileProviderClient::Create() {
+  return new VirtualFileProviderClientImpl();
+}
+
+}  // namespace chromeos
