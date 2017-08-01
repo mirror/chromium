887c41e53b3830b0a928e80608f80742d8a5e800
Fernando Serboncini <fserb@chromium.org>
2017-08-01 15:09:38 -0400

    Add invalidation update methods to FontSelector
    This is a simple refactor to allow more classes to accept FontSelector
    instead of CSSFontSelector.
    Most changes are due to s/FontSelector/GetFontSelector/ on StyleEngine.
    This builds up ground so we can have a WorkerFontSelector.
    
    Bug: 722393
    Change-Id: Ib4e891706d306a6fd5d4cf6d3ebb9567f4f8cbfb

---
+++ third_party/WebKit/Source/platform/fonts/FontSelectorClient.h
@@ -0,0 +1,26 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FontSelectorClient_h
+#define FontSelectorClient_h
+
+#include "platform/PlatformExport.h"
+#include "platform/heap/Handle.h"
+
+namespace blink {
+
+class FontSelector;
+
+class PLATFORM_EXPORT FontSelectorClient : public GarbageCollectedMixin {
+ public:
+  virtual ~FontSelectorClient() {}
+
+  virtual void FontsNeedUpdate(FontSelector*) = 0;
+
+  DEFINE_INLINE_VIRTUAL_TRACE() {}
+};
+
+}  // namespace blink
+
+#endif  // FontSelectorClient_h
