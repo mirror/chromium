6fb6b61dd788cffe810abae8208e8d1b4b9ce491
Steven Bennetts <stevenjb@chromium.org>
2017-08-01 15:31:15 -0600

    Elim GetBlockingPool from ios_chrome_large_icon_service_factory
    Bug: 750741
    Change-Id: I2e9d55b9e08901d9ac80bd91634936c99831b5b7

--- ios/chrome/app/spotlight/spotlight_manager_unittest.mm
+++ ios/chrome/app/spotlight/spotlight_manager_unittest.mm
@@ -8,11 +8,8 @@
 #import <Foundation/Foundation.h>
 
 #include "base/location.h"
-#include "base/message_loop/message_loop.h"
-#include "base/single_thread_task_runner.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/threading/thread_task_runner_handle.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/test/bookmark_test_helpers.h"
 #include "components/bookmarks/test/test_bookmark_client.h"
@@ -67,8 +64,7 @@ class SpotlightManagerTest : public testing::Test {
   SpotlightManagerTest() {
     model_ = bookmarks::TestBookmarkClient::CreateModel();
     large_icon_service_.reset(new favicon::LargeIconService(
-        &mock_favicon_service_, base::ThreadTaskRunnerHandle::Get(),
-        /*image_fetcher=*/nullptr));
+        &mock_favicon_service_, /*image_fetcher=*/nullptr));
     bookmarksSpotlightManager_ = [[BookmarksSpotlightManager alloc]
         initWithLargeIconService:large_icon_service_.get()
                    bookmarkModel:model_.get()];
@@ -80,7 +76,6 @@ class SpotlightManagerTest : public testing::Test {
 
   ~SpotlightManagerTest() override { [bookmarksSpotlightManager_ shutdown]; }
 
-  base::MessageLoop loop_;
   testing::StrictMock<favicon::MockFaviconService> mock_favicon_service_;
   std::unique_ptr<favicon::LargeIconService> large_icon_service_;
   base::CancelableTaskTracker cancelable_task_tracker_;
