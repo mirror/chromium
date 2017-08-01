5f5fe9d06483a544243deadc50d3aa33ade4b1f0
Nicolas Pena <npm@chromium.org>
2017-08-01 12:29:25 -0400

    EQT: Skip steps when renderer is backgrounded
    In this CL, the QueueingTimeEstimator only uses visible steps for
    calculating Expected Queueing Time. A step is considered visible if
    the renderer was never backgrounded throughout the step (excluding
    possible changes at exact start or end of step). A window may thus be
    made up of steps that are not contiguous in time. The objective of this
    CL is to start reducing the massive concentration of the UMA stats for
    Expected Queueing Time on the 0-1 ms bucket, because that is not very
    useful and not a good indicator of latency.
    
    Bug: chromium:729033
    Change-Id: I3cfd4d3268d0f2858fc77de25c08d35a7a1cfc36

--- third_party/WebKit/Source/platform/scheduler/base/queueing_time_estimator.h
+++ third_party/WebKit/Source/platform/scheduler/base/queueing_time_estimator.h
@@ -8,6 +8,7 @@
 #include "base/macros.h"
 #include "base/time/time.h"
 #include "platform/PlatformExport.h"
+#include "platform/wtf/Deque.h"
 
 #include <vector>
 
@@ -49,6 +50,8 @@ class PLATFORM_EXPORT QueueingTimeEstimator {
     void OnTopLevelTaskStarted(base::TimeTicks task_start_time);
     void OnTopLevelTaskCompleted(Client* client, base::TimeTicks task_end_time);
     void OnBeginNestedRunLoop();
+    void OnRendererBackgrounded(bool backgrounded,
+                                base::TimeTicks background_time);
 
     // |step_expected_queueing_time| is the expected queuing time of a
     // smaller window of a step's width. By combining these step EQTs through a
@@ -83,12 +86,17 @@ class PLATFORM_EXPORT QueueingTimeEstimator {
     // sliding window's step width, and the number of bins of the circular
     // buffer.
     int steps_per_window;
-    base::TimeTicks window_start_time;
+    base::TimeTicks step_start_time;
     base::TimeTicks current_task_start_time;
     RunningAverage step_queueing_times;
+    // Vector of times when |renderer_backgrounded| changes. The vector contains
+    // a pair where the first element is the time when the change occurred, and
+    // the second element says what the |renderer_backgrounded| changed to.
+    Deque<std::pair<base::TimeTicks, bool>> background_status_changes_;
 
    private:
-    bool TimePastWindowEnd(base::TimeTicks task_end_time);
+    bool CalculateNextVisibleStep();
+    bool TimePastStepEnd(base::TimeTicks task_end_time);
     bool in_nested_message_loop_ = false;
   };
 
@@ -100,6 +108,8 @@ class PLATFORM_EXPORT QueueingTimeEstimator {
   void OnTopLevelTaskStarted(base::TimeTicks task_start_time);
   void OnTopLevelTaskCompleted(base::TimeTicks task_end_time);
   void OnBeginNestedRunLoop();
+  void OnRendererBackgrounded(bool backgrounded,
+                              base::TimeTicks background_time);
 
   // Returns all state except for the current |client_|.
   const State& GetState() const { return state_; }
