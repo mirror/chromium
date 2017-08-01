c577ad20d3adc2a70cf4960b8726846734a6873f
Tetsui Ohkubo <tetsui@chromium.org>
2017-08-01 14:36:34 +0900

    Add first-party notification styling in MD notification.
    In new-style notification, first-party notification should have
    different styling compared to general web notifications.
    To do this, all system notifications should be created by
    new CreateSystemNotification. (Previously, it was not a requirement.)
    
    By using the function,
    * the display source defaults to string "ChromeOS system".
    * the notification has single accent color based on
      SystemNotificationWarningLevel.
    * gfx::VectorIcon will be passed as small_image instead of gfx::Image
      to perform icon coloring on CreateSystemNotification side.
    
    TEST=manual
    BUG=738779,726241,740807,740813
    
    Change-Id: I9e85b3f1de33b6499297f060b5305d0b02157eef

---
+++ chrome/browser/chromeos/message_center/message_center_delegate_impl.cc
@@ -0,0 +1,20 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/message_center/message_center_delegate_impl.h"
+
+#include "chrome/grit/chromium_strings.h"
+#include "ui/base/l10n/l10n_util.h"
+
+namespace chromeos {
+
+MessageCenterDelegateImpl::MessageCenterDelegateImpl() {}
+
+MessageCenterDelegateImpl::~MessageCenterDelegateImpl() = default;
+
+base::string16 MessageCenterDelegateImpl::GetProductOSName() {
+  return l10n_util::GetStringUTF16(IDS_PRODUCT_OS_NAME);
+}
+
+}  // namespace chromeos
---
+++ ui/message_center/message_center_delegate.h
@@ -0,0 +1,23 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_MESSAGE_CENTER_MESSAGE_CENTER_DELEGATE_H_
+#define UI_MESSAGE_CENTER_MESSAGE_CENTER_DELEGATE_H_
+
+#include "ui/message_center/message_center_export.h"
+
+namespace message_center {
+
+class MessageCenterDelegate {
+ public:
+  virtual ~MessageCenterDelegate() = default;
+
+  // "Chromium OS" or "Chrome OS" in the current locale.
+  // Return empty string if not on these platforms.
+  virtual base::string16 GetProductOSName() = 0;
+};
+
+}  // namespace message_center
+
+#endif  // UI_MESSAGE_CENTER_MESSAGE_CENTER_DELEGATE_H_
