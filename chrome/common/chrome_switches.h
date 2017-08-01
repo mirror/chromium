620b6b6f3a14a016d08b6068310e281f5a016fe7
dpapad <dpapad@chromium.org>
2017-08-01 16:44:05 -0700

    Delete chrome://md-policy placeholder page.
    The placeholder was landed about 2 years ago. The feature was never implemented
    and has been abandoned.
    
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ibd798e3fbd38dda05342d33d5896a4066df0bb03

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -111,7 +111,6 @@ extern const char kEnableExtensionActivityLogging[];
 extern const char kEnableExtensionActivityLogTesting[];
 extern const char kEnableFastUnload[];
 extern const char kEnableMaterialDesignFeedback[];
-extern const char kEnableMaterialDesignPolicyPage[];
 extern const char kEnableNaCl[];
 extern const char kEnableNavigationTracing[];
 extern const char kEnableNetBenchmarking[];
