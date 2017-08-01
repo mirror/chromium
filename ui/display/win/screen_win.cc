9503c36fde11943ac2a14acadc58da3cb1bf17c5
Christopher Cameron <ccameron@chromium.org>
2017-08-01 16:02:52 -0700

    hdr: Query HDR color space in the browser process

--- ui/display/win/screen_win.cc
+++ ui/display/win/screen_win.cc
@@ -82,8 +82,7 @@ std::vector<DisplayInfo> FindAndRemoveTouchingDisplayInfos(
 }
 
 Display CreateDisplayFromDisplayInfo(const DisplayInfo& display_info,
-                                     ColorProfileReader* color_profile_reader,
-                                     bool hdr_enabled) {
+                                     ColorProfileReader* color_profile_reader) {
   Display display(display_info.id());
   float scale_factor = display_info.device_scale_factor();
   display.set_device_scale_factor(scale_factor);
@@ -94,12 +93,8 @@ Display CreateDisplayFromDisplayInfo(const DisplayInfo& display_info,
                      1.0f / scale_factor));
   display.set_rotation(display_info.rotation());
   if (!Display::HasForceColorProfile()) {
-    if (hdr_enabled) {
-      display.SetColorSpaceAndDepth(gfx::ColorSpace::CreateSCRGBLinear());
-    } else {
-      display.SetColorSpaceAndDepth(
-          color_profile_reader->GetDisplayColorSpace(display_info.id()));
-    }
+    display.SetColorSpaceAndDepth(
+        color_profile_reader->GetDisplayColorSpace(display_info.id()));
   }
   return display;
 }
@@ -123,8 +118,7 @@ Display CreateDisplayFromDisplayInfo(const DisplayInfo& display_info,
 // will take precedence.
 std::vector<ScreenWinDisplay> DisplayInfosToScreenWinDisplays(
     const std::vector<DisplayInfo>& display_infos,
-    ColorProfileReader* color_profile_reader,
-    bool hdr_enabled) {
+    ColorProfileReader* color_profile_reader) {
   // Find and extract the primary display.
   std::vector<DisplayInfo> display_infos_remaining = display_infos;
   auto primary_display_iter = std::find_if(
@@ -153,8 +147,8 @@ std::vector<ScreenWinDisplay> DisplayInfosToScreenWinDisplays(
   // Layout and create the ScreenWinDisplays.
   std::vector<Display> displays;
   for (const auto& display_info : display_infos) {
-    displays.push_back(CreateDisplayFromDisplayInfo(
-        display_info, color_profile_reader, hdr_enabled));
+    displays.push_back(
+        CreateDisplayFromDisplayInfo(display_info, color_profile_reader));
   }
 
   std::unique_ptr<DisplayLayout> layout(builder.Build());
@@ -223,8 +217,7 @@ gfx::Point ScalePointRelative(const gfx::Point& from_origin,
 ScreenWin::ScreenWin() : ScreenWin(true) {}
 
 ScreenWin::ScreenWin(bool initialize)
-    : color_profile_reader_(new ColorProfileReader(this)),
-      hdr_enabled_(base::FeatureList::IsEnabled(features::kHighDynamicRange)) {
+    : color_profile_reader_(new ColorProfileReader(this)) {
   DCHECK(!g_screen_win_instance);
   g_screen_win_instance = this;
   if (initialize)
@@ -467,7 +460,7 @@ gfx::Rect ScreenWin::DIPToScreenRectInWindow(gfx::NativeView view,
 void ScreenWin::UpdateFromDisplayInfos(
     const std::vector<DisplayInfo>& display_infos) {
   screen_win_displays_ = DisplayInfosToScreenWinDisplays(
-      display_infos, color_profile_reader_.get(), hdr_enabled_);
+      display_infos, color_profile_reader_.get());
   displays_ = ScreenWinDisplaysToDisplays(screen_win_displays_);
 }
 
