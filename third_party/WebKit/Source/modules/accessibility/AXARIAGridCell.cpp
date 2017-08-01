d180a12c0258db2a2c12ff4fb89ecdde49c1a1d6
Aaron Leventhal <aleventhal@chromium.org>
2017-08-01 16:53:41 -0400

    Propagate aria-readonly value to grid cells
    Bug: 748247
    Change-Id: I7e3a45368e71e1b3cb038b16ec6639af13a1370c

--- third_party/WebKit/Source/modules/accessibility/AXARIAGridCell.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXARIAGridCell.cpp
@@ -134,4 +134,17 @@ AccessibilityRole AXARIAGridCell::ScanToDecideHeaderRole() {
   return kCellRole;
 }
 
+AXRestriction AXARIAGridCell::Restriction() const {
+  const AXObject* container = ParentTable();
+  if (!container || (container->RoleValue() != kGridRole &&
+                     container->RoleValue() != kTreeGridRole))
+    return kNone;  // Restrictions irrelevant without parent grid/treegrid.
+
+  // If this gridcell does not have it's own ARIA input restriction,
+  // use parent grid's restriction.'
+  return HasAttribute(HTMLNames::aria_readonlyAttr)
+             ? AXLayoutObject::Restriction()
+             : container->Restriction();
+}
+
 }  // namespace blink
