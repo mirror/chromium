3aaac8dcce19f138ed1797706b779c8af0990537
Vladimir Levin <vmpstr@chromium.org>
2017-08-01 11:45:04 -0700

    cc/paint: Serialize PaintShaders (mostly).
    This patch adds serialization to PaintShaders, with the exception of
    PaintImage and PaintRecord.
    
    R=enne@chromium.org
    Bug: 737629
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Idbaf512f2f0c12df8edd6bcd459af0ddaaff4126

--- cc/paint/paint_shader.h
+++ cc/paint/paint_shader.h
@@ -22,7 +22,7 @@ using PaintRecord = PaintOpBuffer;
 
 class CC_PAINT_EXPORT PaintShader : public SkRefCnt {
  public:
-  enum class Type {
+  enum class Type : uint8_t {
     kColor,
     kLinearGradient,
     kRadialGradient,
@@ -36,7 +36,7 @@ class CC_PAINT_EXPORT PaintShader : public SkRefCnt {
   // Scaling behavior dictates how a PaintRecord shader will behave. Use
   // RasterAtScale to create a picture shader. Use FixedScale to create an image
   // shader that is backed by the paint record.
-  enum class ScalingBehavior { kRasterAtScale, kFixedScale };
+  enum class ScalingBehavior : uint8_t { kRasterAtScale, kFixedScale };
 
   static sk_sp<PaintShader> MakeColor(SkColor color);
 
@@ -114,7 +114,10 @@ class CC_PAINT_EXPORT PaintShader : public SkRefCnt {
   bool IsOpaque() const;
 
  private:
+  friend class PaintShaderSerializationTest;
   friend class PaintFlags;
+  friend class PaintOpReader;
+  friend class PaintOpWriter;
 
   explicit PaintShader(Type type);
 
