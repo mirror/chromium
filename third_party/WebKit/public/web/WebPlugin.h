8102d3fcb02a0e244b078b95f6b1412736ba0758
Diana Gage <drgage@google.com>
2017-08-01 17:12:59 -0700

    Add ReplaceSelection() and Cut() methods.
    ReplaceSelection() can be utilized by the web plugin container to
    tell a plugin to replace its current text selection with another string.
    If there is no selected text, this function will append the
    replacement text after the current caret position.
    
    Cut() first copies the plugin's selected text to the clipboard, then
    calls ReplaceSelection() with an empty string to erase the selected
    text. Currently, Cut() is only invoked by the keyboard using CTRL+X.
    A later CL will use the mouse to call Cut().
    
    BUG=59266
    
    Change-Id: I7abfee38fb3a23e30c0205d5dd3cfb2ca928de5f

--- third_party/WebKit/public/web/WebPlugin.h
+++ third_party/WebKit/public/web/WebPlugin.h
@@ -160,6 +160,7 @@ class WebPlugin {
   virtual WebString SelectionAsMarkup() const { return WebString(); }
 
   virtual bool CanEditText() const { return false; }
+  virtual void ReplaceSelection(const WebString& text) {}
 
   virtual bool ExecuteEditCommand(const WebString& name) { return false; }
   virtual bool ExecuteEditCommand(const WebString& name,
