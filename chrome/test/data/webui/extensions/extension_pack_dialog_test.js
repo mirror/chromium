1b24c4a78dc7228c0f36435958e62541714410c9
Scott Chen <scottchen@chromium.org>
2017-08-01 17:32:11 -0700

    MD Extension: removes unnecessary registerTests() function
    Bug: 750842
    Change-Id: I9e178728724f41c63e59b4edc81589b4366c9df9

--- chrome/test/data/webui/extensions/extension_pack_dialog_test.js
+++ chrome/test/data/webui/extensions/extension_pack_dialog_test.js
@@ -35,70 +35,67 @@ cr.define('extension_pack_dialog_tests', function() {
     },
   };
 
-  function registerTests() {
-    suite('ExtensionPackDialogTests', function() {
-      /** @type {extensions.PackDialog} */
-      var packDialog;
+  suite('ExtensionPackDialogTests', function() {
+    /** @type {extensions.PackDialog} */
+    var packDialog;
 
-      /** @type {MockDelegate} */
-      var mockDelegate;
+    /** @type {MockDelegate} */
+    var mockDelegate;
 
-      setup(function() {
-        PolymerTest.clearBody();
-        mockDelegate = new MockDelegate();
-        packDialog = new extensions.PackDialog();
-        packDialog.delegate = mockDelegate;
-        document.body.appendChild(packDialog);
-      });
+    setup(function() {
+      PolymerTest.clearBody();
+      mockDelegate = new MockDelegate();
+      packDialog = new extensions.PackDialog();
+      packDialog.delegate = mockDelegate;
+      document.body.appendChild(packDialog);
+    });
 
-      test(assert(TestNames.Interaction), function() {
-        var dialogElement = packDialog.$$('dialog');
-        var isDialogVisible = function() {
-          var rect = dialogElement.getBoundingClientRect();
-          return rect.width * rect.height > 0;
-        };
+    test(assert(TestNames.Interaction), function() {
+      var dialogElement = packDialog.$$('dialog');
+      var isDialogVisible = function() {
+        var rect = dialogElement.getBoundingClientRect();
+        return rect.width * rect.height > 0;
+      };
 
+      expectFalse(isDialogVisible());
+      packDialog.show();
+      expectTrue(isDialogVisible());
+      expectEquals('', packDialog.$$('#root-dir').value);
+      MockInteractions.tap(packDialog.$$('#root-dir-browse'));
+      expectTrue(!!mockDelegate.rootPromise);
+      expectEquals('', packDialog.$$('#root-dir').value);
+      var kRootPath = 'this/is/a/path';
+
+      var promises = [];
+      promises.push(mockDelegate.rootPromise.promise.then(function() {
+        expectEquals(kRootPath, packDialog.$$('#root-dir').value);
+        expectEquals(kRootPath, packDialog.packDirectory_);
+      }));
+
+      expectEquals('', packDialog.$$('#key-file').value);
+      MockInteractions.tap(packDialog.$$('#key-file-browse'));
+      expectTrue(!!mockDelegate.keyPromise);
+      expectEquals('', packDialog.$$('#key-file').value);
+      var kKeyPath = 'here/is/another/path';
+
+      promises.push(mockDelegate.keyPromise.promise.then(function() {
+        expectEquals(kKeyPath, packDialog.$$('#key-file').value);
+        expectEquals(kKeyPath, packDialog.keyFile_);
+      }));
+
+      mockDelegate.rootPromise.resolve(kRootPath);
+      mockDelegate.keyPromise.resolve(kKeyPath);
+
+      return Promise.all(promises).then(function() {
+        MockInteractions.tap(packDialog.$$('.action-button'));
+        expectEquals(kRootPath, mockDelegate.rootPath);
+        expectEquals(kKeyPath, mockDelegate.keyPath);
         expectFalse(isDialogVisible());
-        packDialog.show();
-        expectTrue(isDialogVisible());
-        expectEquals('', packDialog.$$('#root-dir').value);
-        MockInteractions.tap(packDialog.$$('#root-dir-browse'));
-        expectTrue(!!mockDelegate.rootPromise);
-        expectEquals('', packDialog.$$('#root-dir').value);
-        var kRootPath = 'this/is/a/path';
-
-        var promises = [];
-        promises.push(mockDelegate.rootPromise.promise.then(function() {
-          expectEquals(kRootPath, packDialog.$$('#root-dir').value);
-          expectEquals(kRootPath, packDialog.packDirectory_);
-        }));
-
-        expectEquals('', packDialog.$$('#key-file').value);
-        MockInteractions.tap(packDialog.$$('#key-file-browse'));
-        expectTrue(!!mockDelegate.keyPromise);
-        expectEquals('', packDialog.$$('#key-file').value);
-        var kKeyPath = 'here/is/another/path';
-
-        promises.push(mockDelegate.keyPromise.promise.then(function() {
-          expectEquals(kKeyPath, packDialog.$$('#key-file').value);
-          expectEquals(kKeyPath, packDialog.keyFile_);
-        }));
-
-        mockDelegate.rootPromise.resolve(kRootPath);
-        mockDelegate.keyPromise.resolve(kKeyPath);
-
-        return Promise.all(promises).then(function() {
-          MockInteractions.tap(packDialog.$$('.action-button'));
-          expectEquals(kRootPath, mockDelegate.rootPath);
-          expectEquals(kKeyPath, mockDelegate.keyPath);
-          expectFalse(isDialogVisible());
-        });
       });
     });
-  }
+  });
 
   return {
-    registerTests: registerTests,
     TestNames: TestNames,
   };
 });
