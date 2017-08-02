f230a24fcb731cc9901e5a8ac7e5abc2cbf7246f
Gus Smith <gusss@google.com>
2017-08-02 13:42:47 -0700

    Enables WebrtcVideoEncoderGpu
    These miscellaneous changes enable the use of WebrtcVideoEncoderGpu. The
    primary changes are:
    - Constructing a WebrtcVideoEncoderGpu in WebrtcVideoStream::OnEncoderCreated
    - Adding H264 SDP parameters when updating SDP
    - Setting H264 codec-specific-parameters
    
    Change-Id: I1ea94e06127b6ec5aa43df78f023b8e33c384cea

--- remoting/protocol/webrtc_video_stream.cc
+++ remoting/protocol/webrtc_video_stream.cc
@@ -9,6 +9,7 @@
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "remoting/base/constants.h"
+#include "remoting/codec/webrtc_video_encoder_gpu.h"
 #include "remoting/codec/webrtc_video_encoder_proxy.h"
 #include "remoting/codec/webrtc_video_encoder_vpx.h"
 #include "remoting/protocol/frame_stats.h"
@@ -256,10 +257,8 @@ void WebrtcVideoStream::OnEncoderCreated(webrtc::VideoCodecType codec_type) {
     encoder_ = base::MakeUnique<WebrtcVideoEncoderProxy>(
         WebrtcVideoEncoderVpx::CreateForVP9(), encode_task_runner_);
   } else if (codec_type == webrtc::kVideoCodecH264) {
-    // TODO(gusss): Whenever the H264 encoder is ready, this is how it will be
-    // initialized.
-    // encoder_ = base::MakeUnique<WebrtcVideoEncoderProxy>(
-    //     WebrtcVideoEncoderGpu::CreateForH264(), encode_task_runner_);
+    encoder_ = base::MakeUnique<WebrtcVideoEncoderProxy>(
+        WebrtcVideoEncoderGpu::CreateForH264(), encode_task_runner_);
     NOTIMPLEMENTED();
   } else {
     LOG(FATAL) << "Unknown codec type: " << codec_type;
