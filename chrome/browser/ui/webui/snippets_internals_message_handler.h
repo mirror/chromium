596b7500f1bc1310205789fdcae5505b4ad8fb5b
gaschler <gaschler@chromium.org>
2017-08-02 15:43:58 +0200

    [NTP] Contextual in snippets-internals
    Add a section to chrome://snippets-internals to allow
    testing and fetching contextual suggestions.
    The section is only shown when flag
    ContextualSuggestionsCarousel is on.
    
    Bug: 749988
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I9cd371973c6871f93996515a1ac0eab5d3067343

--- chrome/browser/ui/webui/snippets_internals_message_handler.h
+++ chrome/browser/ui/webui/snippets_internals_message_handler.h
@@ -14,8 +14,10 @@
 #include "base/scoped_observer.h"
 #include "components/ntp_snippets/category.h"
 #include "components/ntp_snippets/category_status.h"
+#include "components/ntp_snippets/content_suggestion.h"
 #include "components/ntp_snippets/content_suggestions_service.h"
 #include "components/ntp_snippets/remote/remote_suggestions_provider.h"
+#include "components/ntp_snippets/status.h"
 #include "content/public/browser/web_ui_message_handler.h"
 
 namespace base {
@@ -59,8 +61,13 @@ class SnippetsInternalsMessageHandler
   void HandleClearCachedSuggestions(const base::ListValue* args);
   void HandleClearDismissedSuggestions(const base::ListValue* args);
   void HandleToggleDismissedSuggestions(const base::ListValue* args);
-  void ClearClassification(const base::ListValue* args);
-  void FetchRemoteSuggestionsInTheBackground(const base::ListValue* args);
+  void HandleClearClassification(const base::ListValue* args);
+  void HandleFetchRemoteSuggestionsInTheBackground(const base::ListValue* args);
+  void HandleFetchContextualSuggestions(const base::ListValue* args);
+  void OnContextualSuggestionsFetched(
+      ntp_snippets::Status status_code,
+      const GURL& url,
+      std::vector<ntp_snippets::ContentSuggestion> suggestions);
 
   void SendAllContent();
   void SendClassification();
