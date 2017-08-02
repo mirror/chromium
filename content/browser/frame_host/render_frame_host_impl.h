db8b698c1e1da505e771947cb83b1f2d4fa7cf4f
Dave Tapuska <dtapuska@chromium.org>
2017-08-02 11:55:19 -0400

    Make sure Widget mojo interface is constructed for RenderWidgetHosts.
    We require that a Widget interface exists for a RenderWidgetHost. Ensure
    that it is plumbed correctly so that in has a bound request interface
    always.
    
    BUG=722928
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ibc786aca96b2ae332e4d7ff54ecd07f10ce143db

--- content/browser/frame_host/render_frame_host_impl.h
+++ content/browser/frame_host/render_frame_host_impl.h
@@ -41,6 +41,7 @@
 #include "content/common/image_downloader/image_downloader.mojom.h"
 #include "content/common/input/input_handler.mojom.h"
 #include "content/common/navigation_params.h"
+#include "content/common/widget.mojom.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/common/javascript_dialog_type.h"
 #include "content/public/common/previews_state.h"
