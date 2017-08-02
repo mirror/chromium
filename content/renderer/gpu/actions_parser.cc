af0b4367a7b89721f3907d956bd1f64e1102af85
lanwei <lanwei@chromium.org>
2017-08-02 16:03:26 -0400

    Add PointerEnter and PointerLeave actions to PointerActionSeqence API
    In order to send PointerEnter and PointerLeave events for stylus when the
    pen is entering or leaving the proximity of the tablet in PointerEvent
    web-platform tests, we add PointerEnter and PointerLeave actions to
    PointerActionSeqence API.
    
    Bug:583413
    Change-Id: I0d04445afc9f9afc5bb02f001240c398fc5eff4e

--- content/renderer/gpu/actions_parser.cc
+++ content/renderer/gpu/actions_parser.cc
@@ -22,6 +22,8 @@ SyntheticPointerActionParams::PointerActionType ToSyntheticPointerActionType(
     return SyntheticPointerActionParams::PointerActionType::MOVE;
   if (action_type == "pointerUp")
     return SyntheticPointerActionParams::PointerActionType::RELEASE;
+  if (action_type == "pointerLeave")
+    return SyntheticPointerActionParams::PointerActionType::LEAVE;
   if (action_type == "pause")
     return SyntheticPointerActionParams::PointerActionType::IDLE;
   return SyntheticPointerActionParams::PointerActionType::NOT_INITIALIZED;
@@ -247,6 +249,7 @@ bool ActionsParser::ParseAction(
       action_param.set_button(button);
       break;
     case SyntheticPointerActionParams::PointerActionType::MOVE:
+    case SyntheticPointerActionParams::PointerActionType::LEAVE:
       action_param.set_position(gfx::PointF(position_x, position_y));
       break;
     case SyntheticPointerActionParams::PointerActionType::RELEASE:
