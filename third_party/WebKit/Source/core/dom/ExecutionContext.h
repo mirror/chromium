4212d5fb4ba71af9d4e74ea3d9c5d6b73f67df7d
Yuki Shiino <yukishiino@chromium.org>
2017-08-02 17:05:49 +0900

    v8binding: Introduces ExecutionContext::For{Function,Receiver}Object.
    Following the same manner of ScriptState::For{Function,Receiver}Object,
    this CL introduces ExecutionContext::For{Function,Receiver}Object.
    
    Also uses each of them appropriately in the generated code.  Hand-
    written code is not covered in this CL.
    
    UseCounter is not updated in this CL, although I think that it's better
    to use the receiver object's ExecutionContext in UseCounter, because
    a) the current context should be a little faster than the receiver
    object's context (I'm afraid of performance regression), b) backward
    compatibility (statistics of use counter might be affected), and c)
    UseCounter is not a user-visible behavior (it may be okay not to be
    that exact).
    
    Bug:
    Change-Id: I9b26872872a41a416cf3521b203113257835203a

--- third_party/WebKit/Source/core/dom/ExecutionContext.h
+++ third_party/WebKit/Source/core/dom/ExecutionContext.h
@@ -29,6 +29,7 @@
 #define ExecutionContext_h
 
 #include <memory>
+
 #include "core/CoreExport.h"
 #include "core/dom/ContextLifecycleNotifier.h"
 #include "core/dom/ContextLifecycleObserver.h"
@@ -40,6 +41,7 @@
 #include "platform/weborigin/ReferrerPolicy.h"
 #include "platform/wtf/Noncopyable.h"
 #include "public/platform/WebTraceLocation.h"
+#include "v8/include/v8.h"
 
 namespace blink {
 
@@ -69,6 +71,11 @@ class CORE_EXPORT ExecutionContext : public ContextLifecycleNotifier,
   DECLARE_VIRTUAL_TRACE();
 
   static ExecutionContext* From(const ScriptState*);
+
+  static ExecutionContext* ForFunctionObject(
+      const v8::FunctionCallbackInfo<v8::Value>&);
+  static ExecutionContext* ForReceiverObject(
+      const v8::FunctionCallbackInfo<v8::Value>&);
 
   virtual bool IsDocument() const { return false; }
   virtual bool IsWorkerOrWorkletGlobalScope() const { return false; }
