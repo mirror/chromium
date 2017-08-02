4e389828b48b62ba5fd066d087db84688e170879
Darren Shen <shend@chromium.org>
2017-08-02 17:06:14 +1000

    [CSSParser] Add Next/Peek for stream parsing of ConsumeDeclarationList.
    This patch adds Next/Peek methods on CSSParserTokenStream, which
    tokenizes on demand. It also offers fast path UncheckedNext/Peek methods
    that can be used when we've already called Peek().
    
    Using these new features, we can begin to convert CSSParserImpl::
    ConsumeDeclarationList to use streams (we add an overload because there
    are still other callers that use ranges). Unfortunately, using
    CSSParserObservers with streaming requires some refactoring, so for now,
    we only call ConsumeDeclarationList when we don't have an observer.
    
    Since ConsumeDeclarationList calls lower level parsing functions that
    require ranges, we convert streams to ranges using MakeRangeToEOF().
    When those lower level parsing functions modify the ranges that they
    take, we need to sync those changes with the stream using
    UpdatePositionFromRange.
    
    Future patches will continue to streamify those lower level parsing
    functions, layer by layer.
    
    This patch does not change observable parsing behaviour, and should
    have minimal performance impact: ranges are inherently faster than
    streams, but the unchecked consume/peeks should close the gap.
    
    Bug: 661854
    Change-Id: Id2732f7030d012daf213f963ed0e0506d6eed145

--- third_party/WebKit/Source/core/css/parser/CSSParserTokenStream.h
+++ third_party/WebKit/Source/core/css/parser/CSSParserTokenStream.h
@@ -6,6 +6,7 @@
 #define CSSParserTokenStream_h
 
 #include "CSSTokenizer.h"
+#include "core/css/parser/CSSParserTokenRange.h"
 
 namespace blink {
 
@@ -15,14 +16,65 @@ class CSSParserTokenStream {
 
  public:
   explicit CSSParserTokenStream(CSSTokenizer& tokenizer)
-      : tokenizer_(tokenizer) {
+      : tokenizer_(tokenizer), next_index_(0) {
     DCHECK_EQ(tokenizer.tokens_.size(), 0U);
   }
 
-  CSSParserTokenRange MakeRangeToEOF() { return tokenizer_.TokenRange(); }
+  // TODO(shend): Remove this method. We should never convert from a range to a
+  // stream.
+  void UpdatePositionFromRange(const CSSParserTokenRange& range) {
+    next_index_ = range.begin() - tokenizer_.tokens_.begin();
+  }
+
+  const CSSParserToken& Peek() const {
+    if (next_index_ == tokenizer_.tokens_.size()) {
+      if (tokenizer_.TokenizeSingle().IsEOF())
+        return g_static_eof_token;
+    }
+    DCHECK_LT(next_index_, tokenizer_.tokens_.size());
+    return tokenizer_.tokens_[next_index_];
+  }
+
+  // Can only be called after Peek() returns a non-EOF token
+  // or after AtEnd() returns false.
+  const CSSParserToken& UncheckedPeek() const {
+    DCHECK_LT(next_index_, tokenizer_.tokens_.size());
+    return tokenizer_.tokens_[next_index_];
+  }
+
+  const CSSParserToken& Consume() {
+    const CSSParserToken& token = Peek();
+    if (!token.IsEOF()) {
+      next_index_++;
+    }
+
+    DCHECK_LE(next_index_, tokenizer_.tokens_.size());
+    return token;
+  }
+
+  // Can only be called after Peek() returns a non-EOF token
+  // or after AtEnd() returns false.
+  const CSSParserToken& UncheckedConsume() {
+    DCHECK_LE(next_index_, tokenizer_.tokens_.size());
+    return tokenizer_.tokens_[next_index_++];
+  }
+
+  bool AtEnd() const { return Peek().IsEOF(); }
+
+  // Range represents all tokens from current position to EOF.
+  // Eagerly consumes all the remaining input.
+  // TODO(shend): Remove this method once we switch over to using streams
+  // completely.
+  CSSParserTokenRange MakeRangeToEOF() {
+    return tokenizer_.TokenRange().MakeSubRange(
+        tokenizer_.tokens_.begin() + next_index_, tokenizer_.tokens_.end());
+  }
+
+  void ConsumeComponentValue();
 
  private:
   CSSTokenizer& tokenizer_;
+  size_t next_index_;  // Index of next token to be consumed.
 };
 
 }  // namespace blink
