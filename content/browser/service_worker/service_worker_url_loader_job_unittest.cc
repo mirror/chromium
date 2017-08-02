dd2ddcc36d2bb7b07912c84236bf63cee5c604c6
Matt Falkenhagen <falken@chromium.org>
2017-08-02 17:30:16 +0900

    service worker: Add blob test for ServiceWorkerURLLoaderJob.
    Bug: 748416
    Change-Id: I37d13c799e61fff05bc75675a888ce2630995aa0

--- content/browser/service_worker/service_worker_url_loader_job_unittest.cc
+++ content/browser/service_worker/service_worker_url_loader_job_unittest.cc
@@ -13,9 +13,11 @@
 #include "content/public/common/resource_response.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "content/public/test/test_url_loader_client.h"
+#include "mojo/common/data_pipe_utils.h"
 #include "net/ssl/ssl_info.h"
 #include "net/test/cert_test_util.h"
 #include "net/test/test_data_directory.h"
+#include "storage/browser/blob/blob_data_builder.h"
 #include "storage/browser/blob/blob_storage_context.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/WebKit/public/platform/modules/fetch/fetch_api_request.mojom.h"
@@ -30,6 +32,64 @@ void ReceiveStartLoaderCallback(StartLoaderCallback* out_callback,
 }
 
 }  // namespace
+
+// Helper simulates a service worker handling fetch events. The response can be
+// customized via RespondWith* functions.
+class Helper : public EmbeddedWorkerTestHelper {
+ public:
+  Helper() : EmbeddedWorkerTestHelper(base::FilePath()) {}
+  ~Helper() override = default;
+
+  // Sets the helper to respond to fetch events with the specified blob.
+  void RespondWithBlob(const std::string blob_uuid, uint64_t blob_size) {
+    mode_ = Mode::kBlob;
+    blob_uuid_ = blob_uuid;
+    blob_size_ = blob_size;
+  }
+
+ protected:
+  void OnFetchEvent(
+      int embedded_worker_id,
+      int fetch_event_id,
+      const ServiceWorkerFetchRequest& request,
+      mojom::FetchEventPreloadHandlePtr preload_handle,
+      mojom::ServiceWorkerFetchResponseCallbackPtr response_callback,
+      FetchCallback finish_callback) override {
+    switch (mode_) {
+      case Mode::kDefault:
+        EmbeddedWorkerTestHelper::OnFetchEvent(
+            embedded_worker_id, fetch_event_id, request,
+            std::move(preload_handle), std::move(response_callback),
+            std::move(finish_callback));
+        break;
+      case Mode::kBlob:
+        response_callback->OnResponse(
+            ServiceWorkerResponse(
+                base::MakeUnique<std::vector<GURL>>(), 200, "OK",
+                blink::mojom::FetchResponseType::kDefault,
+                base::MakeUnique<ServiceWorkerHeaderMap>(), blob_uuid_,
+                blob_size_, nullptr /* blob */,
+                blink::kWebServiceWorkerResponseErrorUnknown, base::Time(),
+                false /* response_is_in_cache_storage */,
+                std::string() /* response_cache_storage_cache_name */,
+                base::MakeUnique<
+                    ServiceWorkerHeaderList>() /* cors_exposed_header_names */),
+            base::Time::Now());
+        std::move(finish_callback).Run(SERVICE_WORKER_OK, base::Time::Now());
+        break;
+    }
+  }
+
+ private:
+  enum class Mode { kDefault, kBlob };
+  Mode mode_;
+
+  // For Mode::kBlob
+  std::string blob_uuid_;
+  uint64_t blob_size_;
+
+  DISALLOW_COPY_AND_ASSIGN(Helper);
+};
 
 // ServiceWorkerURLLoaderJobTest is for testing the handling of requests
 // by a service worker via ServiceWorkerURLLoaderJob.
@@ -48,7 +108,7 @@ class ServiceWorkerURLLoaderJobTest
  public:
   ServiceWorkerURLLoaderJobTest()
       : thread_bundle_(TestBrowserThreadBundle::IO_MAINLOOP),
-        helper_(base::MakeUnique<EmbeddedWorkerTestHelper>(base::FilePath())) {}
+        helper_(base::MakeUnique<Helper>()) {}
   ~ServiceWorkerURLLoaderJobTest() override = default;
 
   void SetUp() override {
@@ -87,6 +147,44 @@ class ServiceWorkerURLLoaderJobTest
     return blob_context_.AsWeakPtr();
   }
 
+  // Performs a request. When this returns, |client_| will have information
+  // about the response.
+  void TestRequest() {
+    ResourceRequest request;
+    request.url = GURL("https://www.example.com/");
+    request.method = "GET";
+
+    // Start a ServiceWorkerURLLoaderJob. It should return a
+    // StartLoaderCallback.
+    StartLoaderCallback callback;
+    auto job = base::MakeUnique<ServiceWorkerURLLoaderJob>(
+        base::BindOnce(&ReceiveStartLoaderCallback, &callback), this, request,
+        GetBlobStorageContext());
+    job->ForwardToServiceWorker();
+    base::RunLoop().RunUntilIdle();
+    // TODO(falken): This will fail for fallback to network tests.
+    EXPECT_FALSE(callback.is_null());
+
+    // Start the loader. It will load the URL.
+    mojom::URLLoaderPtr loader;
+    std::move(callback).Run(mojo::MakeRequest(&loader),
+                            client_.CreateInterfacePtr());
+    client_.RunUntilComplete();
+  }
+
+  void ExpectFetchedViaServiceWorker(const ResourceResponseHead& info) {
+    EXPECT_TRUE(info.was_fetched_via_service_worker);
+    EXPECT_FALSE(info.was_fallback_required_by_service_worker);
+    EXPECT_TRUE(info.url_list_via_service_worker.empty());
+    EXPECT_EQ(blink::mojom::FetchResponseType::kDefault,
+              info.response_type_via_service_worker);
+    // TODO(falken): start and ready time should be set.
+    EXPECT_TRUE(info.service_worker_start_time.is_null());
+    EXPECT_TRUE(info.service_worker_ready_time.is_null());
+    EXPECT_FALSE(info.is_in_cache_storage);
+    EXPECT_EQ(std::string(), info.cache_storage_cache_name);
+  }
+
  protected:
   // ServiceWorkerURLLoaderJob::Delegate --------------------------------------
   void OnPrepareToRestart() override {}
@@ -105,47 +203,50 @@ class ServiceWorkerURLLoaderJobTest
   // --------------------------------------------------------------------------
 
   TestBrowserThreadBundle thread_bundle_;
-  std::unique_ptr<EmbeddedWorkerTestHelper> helper_;
+  std::unique_ptr<Helper> helper_;
   scoped_refptr<ServiceWorkerRegistration> registration_;
   scoped_refptr<ServiceWorkerVersion> version_;
   storage::BlobStorageContext blob_context_;
+  TestURLLoaderClient client_;
 };
 
 TEST_F(ServiceWorkerURLLoaderJobTest, Basic) {
-  ResourceRequest request;
-  request.url = GURL("https://www.example.com/");
-  request.method = "GET";
-
-  // Start a ServiceWorkerURLLoaderJob. It should return a StartLoaderCallback.
-  StartLoaderCallback callback;
-  auto job = base::MakeUnique<ServiceWorkerURLLoaderJob>(
-      base::BindOnce(&ReceiveStartLoaderCallback, &callback), this, request,
-      GetBlobStorageContext());
-  job->ForwardToServiceWorker();
-  base::RunLoop().RunUntilIdle();
-  EXPECT_FALSE(callback.is_null());
-
-  // Start the loader. It will load the URL.
-  TestURLLoaderClient client;
-  mojom::URLLoaderPtr loader;
-  std::move(callback).Run(mojo::MakeRequest(&loader),
-                          client.CreateInterfacePtr());
-  client.RunUntilComplete();
-  EXPECT_EQ(net::OK, client.completion_status().error_code);
-
-  // The URL should have loaded successfully via service worker.
-  const ResourceResponseHead& info = client.response_head();
+  TestRequest();
+  EXPECT_EQ(net::OK, client_.completion_status().error_code);
+  const ResourceResponseHead& info = client_.response_head();
   EXPECT_EQ(200, info.headers->response_code());
-  EXPECT_TRUE(info.was_fetched_via_service_worker);
-  EXPECT_FALSE(info.was_fallback_required_by_service_worker);
-  EXPECT_TRUE(info.url_list_via_service_worker.empty());
-  EXPECT_EQ(blink::mojom::FetchResponseType::kDefault,
-            info.response_type_via_service_worker);
-  // TODO(falken): start and ready time should be set.
-  EXPECT_TRUE(info.service_worker_start_time.is_null());
-  EXPECT_TRUE(info.service_worker_ready_time.is_null());
-  EXPECT_FALSE(info.is_in_cache_storage);
-  EXPECT_EQ(std::string(), info.cache_storage_cache_name);
+  ExpectFetchedViaServiceWorker(info);
 }
+
+TEST_F(ServiceWorkerURLLoaderJobTest, BlobResponse) {
+  // Construct the blob to respond with.
+  const char kTestData[] = "Here is sample text for the blob.";
+  std::string expected_response;
+  expected_response.reserve((sizeof(kTestData) - 1) * 1024);
+  auto blob_data = base::MakeUnique<storage::BlobDataBuilder>("blob-id:myblob");
+  for (int i = 0; i < 1024; ++i) {
+    blob_data->AppendData(kTestData);
+    expected_response += kTestData;
+  }
+  std::unique_ptr<storage::BlobDataHandle> blob_handle =
+      blob_context_.AddFinishedBlob(blob_data.get());
+  helper_->RespondWithBlob(blob_handle->uuid(), blob_handle->size());
+
+  // Perform the request.
+  TestRequest();
+  const ResourceResponseHead& info = client_.response_head();
+  EXPECT_EQ(200, info.headers->response_code());
+  ExpectFetchedViaServiceWorker(info);
+
+  // Test the body.
+  std::string response;
+  EXPECT_TRUE(client_.response_body().is_valid());
+  EXPECT_TRUE(mojo::common::BlockingCopyToString(
+      client_.response_body_release(), &response));
+  EXPECT_EQ(expected_response, response);
+}
+
+// TODO(falken): Add tests for stream response, network fallback, etc. Basically
+// everything in ServiceWorkerURLRequestJobTest.
 
 }  // namespace content
