850965aaa62f662a2e7a0cb3c4c6ef1ea483de76
Eddy Mead <meade@chromium.org>
2017-08-02 18:51:14 +1000

    Refactor CSSPropertyDescriptor structure and rename it to CSSPropertyAPI.
    Design doc (with benchmarks!):
    https://docs.google.com/document/d/1uwS_TT8VBWSQNYMnOggwqcnv6YU85FIFvD_egxPSn6o/edit
    
    This CL changes the current "struct that contains function-pointer-or-nulls"
    CSSPropertyDescriptor design to instead use virtual function dispatch and
    static constexpr for returning a pointer to a CSSPropertyAPI or subclass.
    
    
    This makes the call sites cleaner, as since there cannot be null function
    pointers any more, this allows us to write:
    
    CSSPropertyAPI::Get(property_id).ParseSingleValue(...);
    
    and avoid the if statement that we would require with the current
    implementation:
    
    CSSPropertyDescriptor& d = CSSPropertyDescriptor::Get(property_id);
    if (d.ParseSingleValue)
      d.ParseSingleValue(...);
    
    My benchmarks (details in the linked doc) show no speed penalty for doing
    this for getting an API, or for calling functions on it (in fact, it's
    probably faster). Inspecting the assembly generated from the benchmarks
    also shows that the use of constexpr correctly causes the compiler to
    avoid allocating any memory for instances of CSSPropertyAPI or subclasses,
    so function calls are still just dereferencing straight into the vtable of
    the CSSPropertyAPI subclass.
    
    
    Notes:
    - CSSPropertyAPI.h/.cpp and CSSPropertyAPICustom.cpp replace
    CSSPropertyDescriptor.
    - To avoid CSSPropertyAPICustom.cpp from getting too big, I created a
    temporary "CSSPropertyAPICustomHelper" containing the not yet
    ribbonized helper functions it requires.
    
    file moves undetected by gerrit:
    - The generator css_property_apis.py was split into two files:
    make_css_property_api.py, and make_css_property_api_headers.py.
    - CSSPropertyAPIFiles.h.tmpl was renamed to CSSPropertyAPIHeaders.h.tmpl
    
    
    Gist showing curated diff of generated files:
    CSSPropertyAPI.cpp
    CSSPropertyAPIAlignItems.h // A longhand API header
    CSSPropertyDescriptor.cpp
    CSSPropertyDescriptor.h
    CSSShorthandPropertyAPIAnimation.h // A shorthand API header
    https://gist.github.com/wilddamon/2261ef381b648f9fe3cf9ed8ac037cd3/revisions
    
    Gist showing diff between CSSPropertyAPI.cpp and the old
    CSSPropertyDescriptor.cpp:
    https://gist.github.com/wilddamon/ee1afa93637b85b4d1291b8a1d985b16/revisions
    
    Gist showing diff between CSSPropertyParser.cpp and
    CSSPropertyAPICustomHelper.cpp:
    https://gist.github.com/wilddamon/9e81c157f98ff1dbb68d43a79f89fe9c/revisions
    
    Bug: 545324
    Change-Id: Ie5465c02faa010094f78821c024f87c231bd2264

--- third_party/WebKit/Source/core/css/properties/CSSPropertyAPIFontStyle.cpp
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIFontStyle.cpp
@@ -9,9 +9,10 @@
 namespace blink {
 
 const CSSValue* CSSPropertyAPIFontStyle::ParseSingleValue(
+    CSSPropertyID,
     CSSParserTokenRange& range,
     const CSSParserContext&,
-    const CSSParserLocalContext&) {
+    const CSSParserLocalContext&) const {
   return CSSPropertyFontUtils::ConsumeFontStyle(range);
 }
 
