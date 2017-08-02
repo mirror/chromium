a3c706fe39a02b85a6ed2572f11e273541f9f926
Mihai Sardarescu <msarda@chromium.org>
2017-08-02 17:11:56 +0200

    [DICE] Add "Disconnect sync" button on dice internals.
    This CL changes the chrome://signin-dice-internals to display the "Enable sync"
    button when there is Chrome is signed out and the "Disable sync" button when
    Chrome is signed in.
    This CL also reloads the chrome://signin-dice-internals page when the
    Chrome authenticated account changes.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If9671dfaf3e23939d1b643fe1f5840d2da18eb66

--- chrome/browser/ui/webui/signin/signin_dice_internals_ui.cc
+++ chrome/browser/ui/webui/signin/signin_dice_internals_ui.cc
@@ -4,28 +4,74 @@
 
 #include "chrome/browser/ui/webui/signin/signin_dice_internals_ui.h"
 
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/ui/webui/signin/signin_dice_internals_handler.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/browser_resources.h"
 #include "components/grit/components_resources.h"
+#include "components/signin/core/browser/signin_manager.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
+#include "ui/base/webui/web_ui_util.h"
 
 SigninDiceInternalsUI::SigninDiceInternalsUI(content::WebUI* web_ui)
-    : WebUIController(web_ui) {
-  Profile* profile = Profile::FromWebUI(web_ui);
-  DCHECK(!profile->IsOffTheRecord());
+    : WebUIController(web_ui), profile_(Profile::FromWebUI(web_ui)) {
+  DCHECK(!profile_->IsOffTheRecord());
+
+  SigninManagerFactory::GetForProfile(profile_)->AddObserver(this);
 
   content::WebUIDataSource* source = content::WebUIDataSource::Create(
       chrome::kChromeUISigninDiceInternalsHost);
+  source->SetJsonPath("strings.js");
+  source->SetDefaultResource(IDR_SIGNIN_DICE_INTERNALS_HTML);
   source->AddResourcePath("signin_dice_internals.js",
                           IDR_SIGNIN_DICE_INTERNALS_JS);
-  source->SetDefaultResource(IDR_SIGNIN_DICE_INTERNALS_HTML);
-  content::WebUIDataSource::Add(profile, source);
 
+  source->AddBoolean(
+      "isSignedIn",
+      SigninManagerFactory::GetForProfile(profile_)->IsAuthenticated());
+  source->AddBoolean("isSyncAllowed", profile_->IsSyncAllowed());
+
+  base::DictionaryValue strings;
+  webui::SetLoadTimeDataDefaults(g_browser_process->GetApplicationLocale(),
+                                 &strings);
+  source->AddLocalizedStrings(strings);
+
+  content::WebUIDataSource::Add(profile_, source);
   web_ui->AddMessageHandler(
-      base::MakeUnique<SigninDiceInternalsHandler>(profile));
+      base::MakeUnique<SigninDiceInternalsHandler>(profile_));
 }
 
-SigninDiceInternalsUI::~SigninDiceInternalsUI() {}
+void SigninDiceInternalsUI::GoogleSigninSucceeded(const std::string& account_id,
+                                                  const std::string& username) {
+  std::unique_ptr<base::DictionaryValue> update(new base::DictionaryValue);
+  update->SetBoolean(
+      "isSignedIn",
+      SigninManagerFactory::GetForProfile(profile_)->IsAuthenticated());
+  update->SetBoolean("isSyncAllowed", profile_->IsSyncAllowed());
+  content::WebUIDataSource::Update(
+      profile_, chrome::kChromeUISigninDiceInternalsHost, std::move(update));
+
+  // Reload the page after the data source was updated.
+  web_ui()->CallJavascriptFunctionUnsafe("window.location.reload");
+}
+
+void SigninDiceInternalsUI::GoogleSignedOut(const std::string& account_id,
+                                            const std::string& username) {
+  std::unique_ptr<base::DictionaryValue> update(new base::DictionaryValue);
+  update->SetBoolean(
+      "isSignedIn",
+      SigninManagerFactory::GetForProfile(profile_)->IsAuthenticated());
+  update->SetBoolean("isSyncAllowed", profile_->IsSyncAllowed());
+  content::WebUIDataSource::Update(
+      profile_, chrome::kChromeUISigninDiceInternalsHost, std::move(update));
+
+  // Reload the page after the data source was updated.
+  web_ui()->CallJavascriptFunctionUnsafe("window.location.reload");
+}
+
+SigninDiceInternalsUI::~SigninDiceInternalsUI() {
+  SigninManagerFactory::GetForProfile(profile_)->RemoveObserver(this);
+}
--- chrome/browser/ui/webui/signin/signin_dice_internals_ui.h
+++ chrome/browser/ui/webui/signin/signin_dice_internals_ui.h
@@ -6,15 +6,27 @@
 #define CHROME_BROWSER_UI_WEBUI_SIGNIN_SIGNIN_DICE_INTERNALS_UI_H_
 
 #include "base/macros.h"
+#include "components/signin/core/browser/signin_manager_base.h"
 #include "content/public/browser/web_ui_controller.h"
 
+class Profile;
+
 // The implementation for the chrome://signin-dice-internals page.
-class SigninDiceInternalsUI : public content::WebUIController {
+class SigninDiceInternalsUI : public content::WebUIController,
+                              public SigninManagerBase::Observer {
  public:
   explicit SigninDiceInternalsUI(content::WebUI* web_ui);
   ~SigninDiceInternalsUI() override;
 
+  // SigninManagerBase::Observer:
+  void GoogleSigninSucceeded(const std::string& account_id,
+                             const std::string& username) override;
+  void GoogleSignedOut(const std::string& account_id,
+                       const std::string& username) override;
+
  private:
+  Profile* profile_;
+
   DISALLOW_COPY_AND_ASSIGN(SigninDiceInternalsUI);
 };
 
