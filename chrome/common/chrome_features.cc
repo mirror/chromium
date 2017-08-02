583e34a277b5809579bca44b02de9fc62ac07936
Christian Dullweber <dullweber@chromium.org>
2017-08-02 18:02:17 +0200

    Split the CBD dialog into a basic and advanced tab
    To simplify the Clear Browsing Data ui on desktop, we introduce a basic
    section with the most frequently used options and an advanced section
    showing the existing options as well as an additional "Site settings"
    checkbox.
    
    The footers are removed and their information is integrated into the
    checkbox descriptions in the basic tab.
    
    This change requires a few additions to cr_dialog to inject a tab bar
    and restrict the dialog to a fixed height.
    SettingsCheckbox requires an additional attribute to add html with links
    to the summary.
    
    https://screenshot.googleplex.com/TvDGxE77XUC.png
    https://screenshot.googleplex.com/CvF0kx7UwSY.png
    
    Bug: 725401
    Change-Id: Ic2a84d468d9e4fd74c225d8a3668d8d1663f8da6

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -102,7 +102,7 @@ const base::Feature kTabsInCbd {
 #if defined(OS_ANDROID)
       base::FEATURE_ENABLED_BY_DEFAULT
 #else
-      base::FEATURE_DISABLED_BY_DEFAULT
+      base::FEATURE_ENABLED_BY_DEFAULT
 #endif
 };
 
