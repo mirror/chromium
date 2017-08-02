1ed2cac2043f6be673796dac03594da509012567
Diana Gage <drgage@google.com>
2017-08-02 12:20:39 -0700

    Add WebPluginContainerImpl::Paste() method.
    This method retrieves the plain text currently in the copy-paste
    clipboard, and then calls WebPlugin::ReplaceSelection() with this
    string. Currently, Paste() is only invoked by the keyboard using
    CTRL+V and Shift+Insert. A later CL will use the mouse and context
    menu to call Paste().
    
    BUG=59266
    
    Change-Id: I6b26d56cfa6affb2b7e6a2b231e1e4de38f08a08

--- third_party/WebKit/Source/core/exported/WebPluginContainerImpl.cpp
+++ third_party/WebKit/Source/core/exported/WebPluginContainerImpl.cpp
@@ -393,6 +393,13 @@ void WebPluginContainerImpl::Cut() {
     web_plugin_->ReplaceSelection(WebString());
 }
 
+void WebPluginContainerImpl::Paste() {
+  // Pull text from copy-paste clipboard to send to plugin.
+  WebString text = Platform::Current()->Clipboard()->ReadPlainText(
+      WebClipboard::kBufferStandard);
+  web_plugin_->ReplaceSelection(text);
+}
+
 bool WebPluginContainerImpl::ExecuteEditCommand(const WebString& name) {
   if (web_plugin_->ExecuteEditCommand(name))
     return true;
@@ -841,34 +848,51 @@ void WebPluginContainerImpl::HandleKeyboardEvent(KeyboardEvent* event) {
 
   if (web_event.GetType() == WebInputEvent::kRawKeyDown ||
       web_event.GetType() == WebInputEvent::kKeyDown) {
-    if ((web_event.GetModifiers() & WebInputEvent::kInputModifiers) ==
-            kEditingModifier &&
-        // Only copy/cut if there's a selection, so that we only ever do
-        // this for Pepper plugins that support copying/cutting.
-        // Windowless NPAPI plugins will get the event as before.
-        web_plugin_->HasSelection()) {
-      if (web_event.windows_key_code == VKEY_C ||
-          web_event.windows_key_code == VKEY_INSERT) {
-        Copy();
-        event->SetDefaultHandled();
-        return;
-      }
+    int input_modifiers =
+        web_event.GetModifiers() & WebInputEvent::kInputModifiers;
 
-      // Ask the plugin if it can cut text before calling Cut().
-      if (web_event.windows_key_code == VKEY_X && web_plugin_->CanEditText()) {
-        Cut();
+    if (input_modifiers == kEditingModifier) {
+      // Only copy/cut if there's a selection, so that we only ever do
+      // this for Pepper plugins that support copying/cutting.
+      // Windowless NPAPI plugins will get the event as before.
+      if (web_plugin_->HasSelection()) {
+        if (web_event.windows_key_code == VKEY_C ||
+            web_event.windows_key_code == VKEY_INSERT) {
+          Copy();
+          event->SetDefaultHandled();
+          return;
+        }
+        // Ask the plugin if it can edit text before calling Cut().
+        if (web_event.windows_key_code == VKEY_X &&
+            web_plugin_->CanEditText()) {
+          Cut();
+          event->SetDefaultHandled();
+          return;
+        }
+      }
+      // Ask the plugin if it can edit text before calling Paste().
+      if (web_event.windows_key_code == VKEY_V && web_plugin_->CanEditText()) {
+        Paste();
         event->SetDefaultHandled();
         return;
       }
     }
-    // Alternate shortcut for Cut() is Shift + Delete.
-    if ((web_event.GetModifiers() & WebInputEvent::kInputModifiers) ==
-            WebInputEvent::kShiftKey &&
-        web_event.windows_key_code == VKEY_DELETE &&
-        web_plugin_->HasSelection() && web_plugin_->CanEditText()) {
-      Cut();
-      event->SetDefaultHandled();
-      return;
+
+    // Alternate shortcuts for Cut() and Paste() are Shift + Delete and Shift +
+    // Insert, respectively.
+    if (input_modifiers == WebInputEvent::kShiftKey) {
+      if (web_event.windows_key_code == VKEY_DELETE &&
+          web_plugin_->HasSelection() && web_plugin_->CanEditText()) {
+        Cut();
+        event->SetDefaultHandled();
+        return;
+      }
+      if (web_event.windows_key_code == VKEY_INSERT &&
+          web_plugin_->CanEditText()) {
+        Paste();
+        event->SetDefaultHandled();
+        return;
+      }
     }
   }
 
