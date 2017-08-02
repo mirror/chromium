1502f3029f49008f905651de85fff768256ef54d
Christopher Benson <chrisbenson@google.com>
2017-08-01 20:37:17 -0700

    NewTabButton::OnWidgetDestroying notifies IPH when promo is dismissed
    Changes on this CL
    - Moved profile() declarations to virtual TabStripController class. 
    
    Bug: 734132
    Change-Id: I1a3b5ebb8aa75f40317aef88b25ff74454fc2778

--- chrome/browser/ui/views/tabs/new_tab_button.cc
+++ chrome/browser/ui/views/tabs/new_tab_button.cc
@@ -4,8 +4,11 @@
 
 #include "chrome/browser/ui/views/tabs/new_tab_button.h"
 
+#include "chrome/browser/feature_engagement/new_tab/new_tab_tracker.h"
+#include "chrome/browser/feature_engagement/new_tab/new_tab_tracker_factory.h"
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/layout_constants.h"
+#include "chrome/browser/ui/views/tabs/browser_tab_strip_controller.h"
 #include "chrome/browser/ui/views/tabs/new_tab_promo.h"
 #include "third_party/skia/include/core/SkColorFilter.h"
 #include "third_party/skia/include/effects/SkBlurMaskFilter.h"
@@ -177,6 +180,9 @@ bool NewTabButton::GetHitTestMask(gfx::Path* mask) const {
 }
 
 void NewTabButton::OnWidgetDestroying(views::Widget* widget) {
+  feature_engagement_tracker::NewTabTrackerFactory::GetInstance()
+      ->GetForProfile(tab_strip_->controller()->GetProfile())
+      ->DismissNewTabTracker();
   new_tab_promo_observer_.Remove(widget);
   // When the promo widget is destroyed, the NewTabButton needs to be
   // recolored.
