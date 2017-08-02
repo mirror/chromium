028572d35eb4854ce7b58756a024df8ee7afc3ea
Vasilii Sukhanov <vasilii@chromium.org>
2017-08-02 15:08:48 +0200

    Handle correctly the situation when PasswordStore is destroyed on the UI thread.
    Bug:741660
    Change-Id: Ic49cb068733c159bca55e8d76eb58f70793f5fbe

--- components/password_manager/core/browser/password_store.h
+++ components/password_manager/core/browser/password_store.h
@@ -583,7 +583,8 @@ class PasswordStore : protected PasswordStoreSync,
   void InitOnBackgroundThread(
       const syncer::SyncableService::StartSyncFlare& flare);
 
-  // Deletes objest that should be destroyed on the background thread.
+  // Deletes object that should be destroyed on the background thread.
+  // WARNING: this method can be skipped on shutdown.
   void DestroyOnBackgroundThread();
 
   // The observers.
@@ -593,7 +594,10 @@ class PasswordStore : protected PasswordStoreSync,
   std::unique_ptr<AffiliatedMatchHelper> affiliated_match_helper_;
 // TODO(crbug.com/706392): Fix password reuse detection for Android.
 #if !defined(OS_ANDROID) && !defined(OS_IOS)
-  std::unique_ptr<PasswordReuseDetector> reuse_detector_;
+  // PasswordReuseDetector can be only destroyed on the background thread. It
+  // can't be owned by PasswordStore because PasswordStore can be destroyed on
+  // UI thread and DestroyOnBackgroundThread isn't guaranteed to be called.
+  PasswordReuseDetector* reuse_detector_;
 #endif
 #if defined(SYNC_PASSWORD_REUSE_DETECTION_ENABLED)
   std::unique_ptr<PasswordStoreSigninNotifier> notifier_;
