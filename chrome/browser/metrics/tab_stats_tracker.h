ec252fca751ef48d89214cd6bc464c6028c5cd5b
Sebastien Marchand <sebmarchand@chromium.org>
2017-08-03 14:54:41 -0400

    [WIP] TabStats - Add the Power observer and the base for the reporting
    Bug: 720131
    Change-Id: I7600a821389a901c1e5001a6a2fab35244423e16

--- chrome/browser/metrics/tab_stats_tracker.h
+++ chrome/browser/metrics/tab_stats_tracker.h
@@ -6,6 +6,7 @@
 #define CHROME_BROWSER_METRICS_TAB_STATS_TRACKER_H_
 
 #include "base/macros.h"
+#include "base/power_monitor/power_observer.h"
 #include "base/sequence_checker.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
@@ -17,8 +18,16 @@ namespace metrics {
 // TODO(sebmarchand): This is just a base class and doesn't do much for now,
 // finish this and document the architecture here.
 class TabStatsTracker : public TabStripModelObserver,
-                        public chrome::BrowserListObserver {
+                        public chrome::BrowserListObserver,
+                        public base::PowerObserver {
  public:
+  // The StatsReportingDelegate is responsible for delivering statistics
+  // reported by the TabStatsTracker.
+  class StatsReportingDelegate;
+
+  // An implementation of StatsReportingDelegate for reporting via UMA.
+  class UmaStatsReportingDelegate;
+
   // Creates the |TabStatsTracker| instance and initializes the
   // observers that notify it.
   static void Initialize();
@@ -47,16 +56,52 @@ class TabStatsTracker : public TabStripModelObserver,
                     content::WebContents* web_contents,
                     int index) override;
 
+  // base::PowerObserver:
+  void OnResume() override;
+
   // The total number of tabs opened across all the browsers.
   size_t total_tabs_count_;
 
   // The total number of browsers opened.
   size_t browser_count_;
 
+  // The delegate that reports the events.
+  std::unique_ptr<StatsReportingDelegate> reporting_delegate_;
+
  private:
   SEQUENCE_CHECKER(sequence_checker_);
 
   DISALLOW_COPY_AND_ASSIGN(TabStatsTracker);
+};
+
+// The reporting delegate interface.
+class TabStatsTracker::StatsReportingDelegate {
+ public:
+  StatsReportingDelegate() {}
+  virtual ~StatsReportingDelegate() {}
+
+  // Called at resume from sleep/hibernate.
+  virtual void ReportTabCountOnResume(size_t tab_count) = 0;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(StatsReportingDelegate);
+};
+
+// The default reporting delegate, which reports via UMA.
+class TabStatsTracker::UmaStatsReportingDelegate
+    : public TabStatsTracker::StatsReportingDelegate {
+ public:
+  // The number of tabs total at resume from sleep/hibernate.
+  static const char kNumberOfTabsOnResume[];
+
+  UmaStatsReportingDelegate() {}
+  ~UmaStatsReportingDelegate() override {}
+
+  // Called at resume from sleep/hibernate.
+  void ReportTabCountOnResume(size_t tab_count) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(UmaStatsReportingDelegate);
 };
 
 }  // namespace metrics
