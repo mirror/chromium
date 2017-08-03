9a7fcf3788f913758657891ee9caa85441c7d716
Gabriel Charette <gab@chromium.org>
2017-08-03 16:57:28 -0400

    Remove cleanup code from 2014 and get rid of one more call to BrowserThread::FILE
    R=torne@chromium.org
    
    Bug: 689520
    Change-Id: I376c712f7f9514ed2c1b712a0e5df2f897aad138

--- android_webview/javatests/src/org/chromium/android_webview/test/HttpCacheTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/HttpCacheTest.java
@@ -65,49 +65,4 @@ public class HttpCacheTest extends AwTestBase {
         rmrf.waitFor();
         assertFalse(dir.exists());
     }
-
-    @SmallTest
-    @Feature({"AndroidWebView"})
-    public void testLegacyHttpCacheDirIsRemovedOnStartup() throws Exception {
-        ContextUtils.initApplicationContext(
-                getInstrumentation().getTargetContext().getApplicationContext());
-        PathUtils.setPrivateDataDirectorySuffix(AwBrowserProcess.PRIVATE_DATA_DIRECTORY_SUFFIX);
-        File webViewLegacyCacheDir = new File(PathUtils.getDataDirectory(), "Cache");
-        if (!webViewLegacyCacheDir.isDirectory()) {
-            assertTrue(webViewLegacyCacheDir.mkdir());
-            assertTrue(webViewLegacyCacheDir.isDirectory());
-        }
-        File dummyCacheFile = File.createTempFile("test", null, webViewLegacyCacheDir);
-        assertTrue(dummyCacheFile.exists());
-
-        // Set up JNI bindings.
-        AwBrowserProcess.loadLibrary();
-        // No delay before removing the legacy cache files.
-        AwContentsStatics.setLegacyCacheRemovalDelayForTest(0);
-
-        startBrowserProcess();
-        final TestAwContentsClient contentClient = new TestAwContentsClient();
-        final AwTestContainerView testContainerView =
-                createAwTestContainerViewOnMainSync(contentClient);
-        final AwContents awContents = testContainerView.getAwContents();
-
-        // Do some page loading to make sure that FILE thread has processed
-        // our directory removal task.
-        TestWebServer httpServer = null;
-        try {
-            httpServer = TestWebServer.start();
-            final String pageUrl = "/page.html";
-            final String pageHtml = "<body>Hello, World!</body>";
-            final String fullPageUrl = httpServer.setResponse(pageUrl, pageHtml, null);
-            loadUrlSync(awContents, contentClient.getOnPageFinishedHelper(), fullPageUrl);
-            assertEquals(1, httpServer.getRequestCount(pageUrl));
-        } finally {
-            if (httpServer != null) {
-                httpServer.shutdown();
-            }
-        }
-
-        assertFalse(webViewLegacyCacheDir.exists());
-        assertFalse(dummyCacheFile.exists());
-    }
 }
