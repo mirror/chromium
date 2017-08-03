d1caaa1c7427754c296ae893aa08331e15d39079
Nick Harper <nharper@chromium.org>
2017-08-03 12:04:15 -0700

    Add Android ApplicationStatusListener to net::CookieMonster
    On Android, this will propagate the onStop signal to call
    CookieMonster::FlushStore. For cookie and Channel ID stores that are backed by
    sqlite databases, this will result in committing any pending changes to those
    databases.
    
    Bug: 719726
    Change-Id: Ib01551c1f500dccec2883b54760cca92d43d6adb

---
+++ chrome/browser/net/android_status_listener.cc
@@ -0,0 +1,26 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/net/android_status_listener.h"
+
+AndroidStatusListener::~AndroidStatusListener() {}
+
+AndroidStatusListener::AndroidStatusListener() {
+  app_status_listener_.reset(new base::android::ApplicationStatusListener(
+      base::Bind(&::AndroidStatusListener::OnApplicationStateChange,
+                 base::Unretained(this))));
+}
+
+void AndroidStatusListener::SetApplicationStoppedCallback(
+    base::RepeatingClosure callback) {
+  stopped_callback_ = callback;
+}
+
+void AndroidStatusListener::OnApplicationStateChange(
+    base::android::ApplicationState state) {
+  if (state == base::android::APPLICATION_STATE_HAS_STOPPED_ACTIVITIES &&
+      !stopped_callback_.is_null()) {
+    stopped_callback_.Run();
+  }
+}
