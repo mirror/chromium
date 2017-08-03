eea1058f6c3130eb4f362cf854d37b99abd4487e
gambard <gambard@chromium.org>
2017-08-03 09:29:19 +0200

    Reading List opening doesn't count for MostVisited
    When the user opens a Reading List page, it should not be taken into
    account for the computation of the Most Visited tiles.
    
    Bug:734067
    Change-Id: Iec28e8086051777ab4c43a2e53778c7bab3d54b1

---
+++ ios/chrome/browser/tabs/tab_constants.h
@@ -0,0 +1,12 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_TABS_TAB_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_TABS_TAB_CONSTANTS_H_
+
+namespace tab_constants {
+extern const char kDoNotConsiderForMostVisited[];
+}
+
+#endif  // IOS_CHROME_BROWSER_TABS_TAB_CONSTANTS_H_
