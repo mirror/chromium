19e7b879416b1b5524f104ece42fb2eaaf3c5ef5
Jia <jiameng@chromium.org>
2017-08-04 09:25:33 +1000

    Implement parseShorthand API for grid and grid-template with pairs
    This cl also refactors util functions and moves them from
    CSSPropertyParser to CSSPropertyGridUtils and moves
    functions from CSSPropertyGridTemplateAreasUtils to
    CSSPropertyGridUtils as the functions are used by multiple
    grid-related properties, and we won't have to call one util
    file from another util file.
    
    The old version of several util functions parse property values
    and also add parsed values to the parsed-property-set, which
    means these util functions need to take shorthand property id
    as an input. A major change in this cl is splitting parsing
    and adding values. Adding property values will be
    handled by property API classes. The benefits of this refactoring
    are
    (i). util functions no longer need to take shorthand id as input,
    and thus become pure util functions.
    (ii). it is now more obvious which property values are added.
    Previously property values are added at different places, but now
    they are only added in the property API files.
    
    Functions below have major changes mentioned above.
    1. ConsumeGridTemplateRowsAndAreasAndColumns
    - The old version parses and adds property values.
    - The new version only parses values and returns values to the caller.
    
    2. ConsumeGridTemplateShorthand
    - The old version parses and adds property values (by calling
    ConsumeGridTemplateRowsAndAreasAndColumns for some cases).
    - The new version only parses values and returns values to the caller.
    It is shared by both grid and grid-template APIs.
    
    3. ConsumGridShorthand
    - The old version is replaced by grid's parseShorthand method.
    - The old version parses property values both directly and indirectly
    (by calling ConsumeGridTemplateShorthand), and adds property values
    at different places.
    - The new version calls util functions for parsing only, and
    explicitly adds property values itself.
    
    Bug:668012
    Change-Id: I4808bd1f169bcad5b3d46a83fc897567f18a0e7e

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParserHelpers.h
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParserHelpers.h
@@ -8,6 +8,7 @@
 #include "core/css/CSSCustomIdentValue.h"
 #include "core/css/CSSIdentifierValue.h"
 #include "core/css/CSSPrimitiveValue.h"
+#include "core/css/CSSProperty.h"
 #include "core/css/CSSValueList.h"
 #include "core/css/parser/CSSParserMode.h"
 #include "core/css/parser/CSSParserTokenRange.h"
