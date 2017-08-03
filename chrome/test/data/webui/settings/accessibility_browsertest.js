b868c66277225747ed144ca290577a1c04880d8e
Tina Quach <quacht@google.com>
2017-08-03 14:44:05 -0700

    Add AccessibilityTest abstraction for defining tests.
    In order to make it easier to write accessibility tests, this CL
    adds the AccessibilityTest namespace, which groups all audit and mocha
    test related code so it may be reused to test other Chrome WebUI.
    
    Bug: 737739
    Change-Id: I87cc0db0670a5a17d84406d6d2e3fdd619372774

--- chrome/test/data/webui/settings/accessibility_browsertest.js
+++ chrome/test/data/webui/settings/accessibility_browsertest.js
@@ -13,20 +13,9 @@ var ROOT_PATH = '../../../../../';
 // Polymer BrowserTest fixture and aXe-core accessibility audit.
 GEN_INCLUDE([
   ROOT_PATH + 'chrome/test/data/webui/polymer_browser_test_base.js',
-  ROOT_PATH + 'chrome/test/data/webui/settings/accessibility_audit_rules.js',
+  ROOT_PATH + 'chrome/test/data/webui/settings/accessibility_audit.js',
   ROOT_PATH + 'third_party/axe-core/axe.js',
 ]);
-
-/**
- * @typedef {{
- *   rules: {
- *     'color_contrast': ({ enabled: boolean} | undefined),
- *     'aria_valid_attr': ({ enabled: boolean} | undefined),
- *   }
- * }}
- * @see https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#options-parameter
- */
-function AccessibilityAuditConfig() {}
 
 /**
  * Test fixture for Accessibility of Chrome Settings.
@@ -34,37 +23,6 @@ function AccessibilityAuditConfig() {}
  * @extends {PolymerTest}
  */
 function SettingsAccessibilityTest() {}
-
-/**
- * Run aXe-core accessibility audit, print console-friendly representation
- * of violations to console, and fail the test if there are audit failures.
- * @param {AccessibilityAuditConfig} options Dictionary disabling specific
- *    audit rules.
- * @return {Promise} A promise resolved if the accessibility audit completes
- *    with no issues, or rejected if the audit finds any accessibility issues.
- */
-SettingsAccessibilityTest.runAudit = function(options) {
-  // Ignore iron-iconset-svg elements that have duplicate ids and result in
-  // false postives from the audit.
-  var context = {exclude: ['iron-iconset-svg']};
-  options = options || {};
-
-  return new Promise(function(resolve, reject) {
-    axe.run(context, options, function(err, results) {
-      if (err)
-        reject(err);
-
-      var violationCount = results.violations.length;
-      if (violationCount) {
-        // Pretty print out the violations detected by the audit.
-        console.log(JSON.stringify(results.violations, null, 4));
-        reject('Found ' + violationCount + ' accessibility violations.');
-      } else {
-        resolve();
-      }
-    });
-  });
-};
 
 SettingsAccessibilityTest.prototype = {
   __proto__: PolymerTest.prototype,
@@ -74,8 +32,9 @@ SettingsAccessibilityTest.prototype = {
 
   // Include files that define the mocha tests.
   extraLibraries: PolymerTest.getLibraries(ROOT_PATH).concat([
-    'ensure_lazy_loaded.js', 'passwords_and_autofill_fake_data.js',
-    'passwords_a11y_test.js'
+    'ensure_lazy_loaded.js',
+    'passwords_and_autofill_fake_data.js',
+    'passwords_a11y_test.js',
   ]),
 
   // TODO(hcarmona): Remove once ADT is not longer in the testing infrastructure
@@ -91,10 +50,10 @@ SettingsAccessibilityTest.prototype = {
 // resolving the violation or after adding the violation exception framework.
 // http://crbug.com/748608
 // http://crbug.com/748632
-var rulesToSkip = ['aria-valid-attr', 'color-contrast', 'region', 'skip-link'];
+var rulesToSkip = ['color-contrast', 'region', 'skip-link'];
 
 // Define a unit test for every audit rule.
-AccessibilityAudit.ruleIds.forEach(function(ruleId) {
+AccessibilityTest.ruleIds.forEach(function(ruleId) {
   if (rulesToSkip.indexOf(ruleId) == -1) {
     // Replace hyphens, which break the build.
     var ruleName = ruleId.replace(new RegExp('-', 'g'), '_');
@@ -104,6 +63,6 @@ AccessibilityAudit.ruleIds.forEach(function(ruleId) {
           mocha.grep('MANAGE_PASSWORDS_' + ruleId).run();
         });
   }
-})
+});
 
 GEN('#endif  // defined(NDEBUG)');
