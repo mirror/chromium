6bd76e79111831b9960700fa3d45896fb87733fb
Koji Ishii <kojii@chromium.org>
2017-08-03 22:53:14 +0900

    [LayoutNG] Add style/text to NGPhysicalTextFragment for :first-line
    In preparation of :first-line support, this patch:
    
    1. Removes NGInlineNode from NGPhysicalTextFragment to manage object
       life cycle more correctly.
    2. Adds String to NGPhysicalTextFragment. The current paint patch needs
       accessing to the text content of fragments.
    3. Adds ComputedStyle to NGPhysicalFragment. This allows paint to find
       the style without knowing if it's on the first line.
       NGPhysicalFragment because inline-blocks can live on the first line.
    4. ItemIndex() is not removed yet since CreateBidiRuns() needs it. This
       should be gone when we switch to NG paint.
    5. To support 3, adds ComputedStyle to all fragment builders.
    6. Adds NGBaseFragmentBuilder since 5 is common to all fragment
       builders.
    7. In doing 6, writing mode and direction is moved to the common
       constructor as per Ian's TODO comment.
    
    Bug: 636993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I52cad8ab27336234ba3a375fa8ced5d29e41ff29

--- third_party/WebKit/Source/core/layout/ng/inline/ng_line_breaker_test.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_line_breaker_test.cc
@@ -37,7 +37,7 @@ class NGLineBreakerTest : public NGBaseLayoutAlgorithmTest {
             .ToConstraintSpace(NGWritingMode::kHorizontalTopBottom);
 
     NGFragmentBuilder container_builder(
-        NGPhysicalFragment::NGFragmentType::kFragmentBox, node);
+        node, node.Style(), space->WritingMode(), space->Direction());
     container_builder.SetBfcOffset(NGLogicalOffset{LayoutUnit(), LayoutUnit()});
 
     Vector<RefPtr<NGUnpositionedFloat>> unpositioned_floats;
