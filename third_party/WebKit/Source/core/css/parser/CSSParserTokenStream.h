f317cb0041b08126406334bbc56dfd2f639f48fb
Darren Shen <shend@chromium.org>
2017-08-04 09:03:18 +1000

    [CSSParser] Allow parsers with observers to use streams.
    Previously, we have CSSParserObservers which receive information about
    the parsing process (e.g. where a particular rule starts or ends). The
    way it worked for stylesheet parsing was that we first tokenized the
    entirety of the input, storing token information in a vector. Then,
    as we parsed, we queried the tokens for their location in the input
    string (their offset) using the vector.
    
    To get the same behaviour with streams, it's more difficult. Before,
    offset information were available prior to parsing. Now, offset
    information is only available as we parse. This requires some
    refactoring to work.
    
    In the meantime, we would like parsers with observers to still use
    streams. Hence, this patch implements a workaround: when we have
    observers, we tokenize everything before parsing as we did before.
    We then create a stream over that tokenizer (technically no longer
    a stream since everything's already tokenized) and use the same
    parsing code path. Any time we call into the observer with a range,
    we just convert the stream back into a range.
    
    This means that until we implement proper streaming with observers,
    parsing with observers will tokenize up front, whereas parsing
    without observers will tokenize on demand (mostly).
    
    For reviewers:
    - We add back the observer code that was in ConsumeDeclarationList
      but was deleted because observers didn't accept streams before.
    
    Bug: 661854
    Change-Id: Ifa53b2e7f2622edb371c74260fd24287d0fd4f89

--- third_party/WebKit/Source/core/css/parser/CSSParserTokenStream.h
+++ third_party/WebKit/Source/core/css/parser/CSSParserTokenStream.h
@@ -20,7 +20,6 @@ class CSSParserTokenStream {
  public:
   explicit CSSParserTokenStream(CSSTokenizer& tokenizer)
       : tokenizer_(tokenizer), next_index_(0) {
-    DCHECK_EQ(tokenizer.tokens_.size(), 0U);
   }
 
   // TODO(shend): Remove this method. We should never convert from a range to a
