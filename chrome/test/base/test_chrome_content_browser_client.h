231d0e57d1db8fb80204e3cf5dc76d736e28952a
Matt Menke <mmenke@chromium.org>
2017-08-03 16:22:53 -0400

    Make StoragePartition always create a NetworkContext.
    This will allow code to use NetworkContexts both when the network
    service is disabled, and when it's enabled, so the same code works with
    and without the network service.
    
    This CL also makes it so the StoragePartition always owns the
    NetworkContext, even if the context is created by the embedder.
    
    Bug: 751119
    Change-Id: If63e9f3ba75671c3fb24ce8d956e8ea2d98177dc

---
+++ chrome/test/base/test_chrome_content_browser_client.h
@@ -0,0 +1,49 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_TEST_BASE_TEST_CHROME_CONTENT_BROWSER_CLIENT_H_
+#define CHROME_TEST_BASE_TEST_CHROME_CONTENT_BROWSER_CLIENT_H_
+
+#include "base/macros.h"
+#include "chrome/browser/chrome_content_browser_client.h"
+#include "content/public/browser/content_browser_client.h"
+#include "content/public/common/network_service.mojom.h"
+
+namespace base {
+class FilePath;
+}
+
+class BrowserContext;
+
+// Subclass of ChromeContentBrowserClient for unit tests, where a TestProfile is
+// used. Unittests using the TestProfile violate some fundamental behaviors that
+// ProfileNetworkContextService relies upon.
+//
+// In particular, TestingProfiles can be created BrowserContexts. Each
+// TestingProfile has its own StoragePartition, but all TestingProfiles use the
+// original BrowserContext for BrowserContextKeyedServices, while
+// ProfileNetworkContextService expects, as a BrowserContextKeyedService, it's
+// only going to only be used for one primary StoragePartition.
+//
+// Also, TestProfile bypasses ProfileIOData, so nothing calls
+// ProfileNetworkContextService::SetUpProfileIODataMainContext.
+//
+// TODO(mmenke):  Figure how to get rid of this class.
+class TestChromeContentBrowserClient : public ChromeContentBrowserClient {
+ public:
+  TestChromeContentBrowserClient();
+  ~TestChromeContentBrowserClient() override;
+
+  // ChromeContentBrowserClient::CreateNetworkContext does not work with a
+  // TestProfile.
+  content::mojom::NetworkContextPtr CreateNetworkContext(
+      content::BrowserContext* context,
+      bool in_memory,
+      const base::FilePath& relative_partition_path) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(TestChromeContentBrowserClient);
+};
+
+#endif  // CHROME_TEST_BASE_TEST_CHROME_CONTENT_BROWSER_CLIENT_H_
