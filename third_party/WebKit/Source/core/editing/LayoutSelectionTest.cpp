851a00d508bde7bdfd75bf2ff20b40b8947460c1
Yoichi Osato <yoichio@chromium.org>
2017-08-03 16:18:09 +0900

    Traverse on flat tree to mark and invalidate kInside LayoutObject in LayoutSelection
    We mark and invalidate LayoutObject by
    - finding first/last LayoutObject by canonicalization and
    - traverse on layout tree
    However we can those operation at once by traversing flat tree.
    For that, this CL changes SelectionState::kInside marking from layout tree to flat tree.
    I will change kStart/kEnd marking.
    Core part is L517-L549, which replaces old SetSelectionState and CollectInvalidationSet.
    
    Test expectations update:
     Since we traverse on flat tree, we skip pseudo element, or list style marker.
    
    Bug: 739062,751420
    Change-Id: I0717340fb7eef2c130d1281c2c9f5d8ff9b9b586

---
+++ third_party/WebKit/Source/core/editing/LayoutSelectionTest.cpp
@@ -0,0 +1,109 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/editing/LayoutSelection.h"
+
+#include "core/editing/EditingTestBase.h"
+#include "core/editing/FrameSelection.h"
+#include "core/layout/LayoutObject.h"
+#include "platform/wtf/Assertions.h"
+
+namespace blink {
+
+static std::ostream& operator<<(std::ostream& ostream,
+                                LayoutObject* layout_object) {
+  return ostream << layout_object->GetNode()
+                 << ", state:" << layout_object->GetSelectionState()
+                 << (layout_object->ShouldInvalidateSelection()
+                         ? ", ShoudInvalidate"
+                         : ", NotInvalidate")
+                 << '\n';
+}
+
+class LayoutSelectionTest : public EditingTestBase {
+ public:
+  // Test LayoutObject's type, SelectionState and ShouldInvalidateSelection
+  // flag.
+  bool TestNextLayoutObject(bool (LayoutObject::*predicate)(void) const,
+                            SelectionState state) {
+    if (!current_)
+      current_ = GetDocument().body()->GetLayoutObject();
+    else
+      current_ = current_->NextInPreOrder();
+    if (!current_)
+      return false;
+    if (!(current_->*predicate)())
+      return false;
+    if (current_->GetSelectionState() != state)
+      return false;
+    return current_->ShouldInvalidateSelection() ==
+           (state != SelectionState::kNone);
+  }
+  LayoutObject* Current() { return current_; }
+  void PrintLayoutTreeForDebug() {
+    std::stringstream stream;
+    for (LayoutObject* runner = GetDocument().body()->GetLayoutObject(); runner;
+         runner = runner->NextInPreOrder()) {
+      stream << runner;
+    }
+    LOG(INFO) << '\n' << stream.str();
+  }
+
+ private:
+  LayoutObject* current_ = nullptr;
+};
+
+#define TEST_NEXT(predicate, state) \
+  EXPECT_TRUE(TestNextLayoutObject(predicate, state)) << Current()
+
+#define TEST_NO_NEXT_LAYOUT_OBJECT() \
+  EXPECT_EQ(Current()->NextInPreOrder(), nullptr)
+
+TEST_F(LayoutSelectionTest, TraverseLayoutObject) {
+  SetBodyContent("foo<br>bar");
+  Selection().SetSelection(SelectionInDOMTree::Builder()
+                               .SelectAllChildren(*GetDocument().body())
+                               .Build());
+  Selection().CommitAppearanceIfNeeded();
+  TEST_NEXT(&LayoutObject::IsLayoutBlock, SelectionState::kStartAndEnd);
+  TEST_NEXT(&LayoutObject::IsText, SelectionState::kStart);
+  TEST_NEXT(&LayoutObject::IsBR, SelectionState::kInside);
+  TEST_NEXT(&LayoutObject::IsText, SelectionState::kEnd);
+  TEST_NO_NEXT_LAYOUT_OBJECT();
+}
+
+TEST_F(LayoutSelectionTest, TraverseLayoutObjectTruncateVisibilityNone) {
+  SetBodyContent(
+      "<span style='visibility:hidden;'>before</span>"
+      "foo"
+      "<span style='visibility:hidden;'>after</span>");
+  Selection().SetSelection(SelectionInDOMTree::Builder()
+                               .SelectAllChildren(*GetDocument().body())
+                               .Build());
+  Selection().CommitAppearanceIfNeeded();
+  TEST_NEXT(&LayoutObject::IsLayoutBlock, SelectionState::kStartAndEnd);
+  TEST_NEXT(&LayoutObject::IsLayoutInline, SelectionState::kNone);
+  TEST_NEXT(&LayoutObject::IsText, SelectionState::kNone);
+  TEST_NEXT(&LayoutObject::IsText, SelectionState::kStartAndEnd);
+  TEST_NEXT(&LayoutObject::IsLayoutInline, SelectionState::kNone);
+  TEST_NEXT(&LayoutObject::IsText, SelectionState::kNone);
+  TEST_NO_NEXT_LAYOUT_OBJECT();
+}
+
+TEST_F(LayoutSelectionTest, TraverseLayoutObjectTruncateBR) {
+  SetBodyContent("<br><br>foo<br><br>");
+  Selection().SetSelection(SelectionInDOMTree::Builder()
+                               .SelectAllChildren(*GetDocument().body())
+                               .Build());
+  Selection().CommitAppearanceIfNeeded();
+  TEST_NEXT(&LayoutObject::IsLayoutBlock, SelectionState::kStartAndEnd);
+  TEST_NEXT(&LayoutObject::IsBR, SelectionState::kStart);
+  TEST_NEXT(&LayoutObject::IsBR, SelectionState::kInside);
+  TEST_NEXT(&LayoutObject::IsText, SelectionState::kInside);
+  TEST_NEXT(&LayoutObject::IsBR, SelectionState::kEnd);
+  TEST_NEXT(&LayoutObject::IsBR, SelectionState::kNone);
+  TEST_NO_NEXT_LAYOUT_OBJECT();
+}
+
+}  // namespace blink
