762d34d5df3934c71bb0608efbd8288b4dab793b
Daniel Vogelheim <vogelheim@chromium.org>
2017-08-03 10:18:30 +0200

    Fix preloading of resources with integrity attributes.
    Currently, a preloaded resource doesn't match a later resource request
    with an integrity=... attribute, which will call the browser to reload
    that resource. Which makes the preload pretty useless. This patch will
    match and "adopt" a preload, if the request has integrity but the
    preload doesn't.
    
    The actual fix are the few lines in ResourceFetcher. The remaining code
    is a minor refactoring that
    - moves the "cached" character-converted string into TextResource,
      (all in protected methods, so that subclasses can choose to use this
       or not)
    - thus de-dupes a tiny bit of logic between ScriptResource and
      CSSStyleSheetResource,
    - ensures that the text conversion has the same life-cycle in both of
      those resources. (Previously, CSSStyleSheetResource would always
      trigger the character conversion in CheckNotify, even if it was a
      preload and noone would actually use the data (yet). That prevent
      "adoption" of the preload in some circumstances for CSS resources.)
    
    
    Thanks to csharrison for the test case.
    
    Bug: 677022
    Change-Id: I21942be148355ed3fec904c854d4a398518acc2b

--- third_party/WebKit/Source/core/loader/resource/TextResource.cpp
+++ third_party/WebKit/Source/core/loader/resource/TextResource.cpp
@@ -26,10 +26,19 @@ void TextResource::SetEncoding(const String& chs) {
 }
 
 WTF::TextEncoding TextResource::Encoding() const {
+  // The encoding may change depending on the resource's contents, so if we
+  // haven't decoded the resource yet, we need to do so now.
+  //
+  // (Usually, Encoding() is only called after CachedDecodedText(), but some
+  // test cases set up resources so that Encoding() is called before any other
+  // decoding attempt. This claus catches those cases.)
+  if (decoded_text_cache_.IsNull() && Data())
+    CurrentDecodedText();
+
   return decoder_->Encoding();
 }
 
-String TextResource::DecodedText() const {
+String TextResource::CurrentDecodedText() const {
   DCHECK(Data());
 
   StringBuilder builder;
@@ -41,6 +50,35 @@ String TextResource::DecodedText() const {
   }
   builder.Append(decoder_->Flush());
   return builder.ToString();
+}
+
+const String& TextResource::CachedDecodedText() {
+  DCHECK(IsLoaded());
+  if (decoded_text_cache_.IsNull() && Data()) {
+    decoded_text_cache_ = AtomicString(CurrentDecodedText());
+    ClearData();
+    SetDecodedSize(DecodedSize() + DecodedTextCacheSize());
+  }
+  return decoded_text_cache_;
+}
+
+const String& TextResource::CachedDecodedText() const {
+  // TODO(vogelheim): CachedDecodedText() is logically a const operation, but
+  //                  because we lazily update decoded_text_cache_ it
+  //                  actually isn't. To avoid the const_cast<> below, we should
+  //                  either make sure callees hold a non-const instance, or
+  //                  make Resource::data_ mutable.
+  return const_cast<TextResource*>(this)->CachedDecodedText();
+}
+
+void TextResource::ClearDecodedTextCache() {
+  size_t old_size = DecodedTextCacheSize();
+  decoded_text_cache_ = AtomicString();
+  SetDecodedSize(DecodedSize() - old_size);
+}
+
+size_t TextResource::DecodedTextCacheSize() const {
+  return decoded_text_cache_.CharactersSizeInBytes();
 }
 
 }  // namespace blink
