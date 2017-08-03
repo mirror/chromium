4bdb2a785363ff4a7601d402b6e66103096d83c9
Xu Xing <xing.xu@intel.com>
2017-08-03 16:21:59 +0800

    Split ResourceProvider into LayerTreeResourceProvider and DisplayResourceProvider
    ResourceProvider manages resources shared between producer (render layer tree, browser 
    layer tree) and consumer (display compositor). The producer writes to resources and
    sends it to the consumer, the consumer receives and reads resources. These write/send
    and receive/read currently live in a single ResourceProvider. This patch intends to
    split the responsibilities in ResourceProvider into two parts: write/send in the
    LayerTreeResourceProvider, receive/read in the DisplayResourceProvider.
    
    BUG=738190
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ic7ae225334a098d8bc943cc6ff772d00dbe91362

--- cc/trees/layer_tree_impl.cc
+++ cc/trees/layer_tree_impl.cc
@@ -1312,7 +1312,7 @@ viz::ContextProvider* LayerTreeImpl::context_provider() const {
   return layer_tree_host_impl_->layer_tree_frame_sink()->context_provider();
 }
 
-ResourceProvider* LayerTreeImpl::resource_provider() const {
+LayerTreeResourceProvider* LayerTreeImpl::resource_provider() const {
   return layer_tree_host_impl_->resource_provider();
 }
 
