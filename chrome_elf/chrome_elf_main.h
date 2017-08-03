0d186ec8ec902b892e137e7dee0973347b83eeac
Sigurdur Asgeirsson <siggi@chromium.org>
2017-08-03 17:04:56 -0400

    NOT FOR SUBMISSION: Start on the grand elf unification project.
    Bug:
    Change-Id: I631a387c9f1039f6732640dc6d388287471238e8

--- chrome_elf/chrome_elf_main.h
+++ chrome_elf/chrome_elf_main.h
@@ -5,6 +5,14 @@
 #ifndef CHROME_ELF_CHROME_ELF_MAIN_H_
 #define CHROME_ELF_CHROME_ELF_MAIN_H_
 
+#include <windows.h>
+
+#include <string>
+
+namespace crash_reporter {
+struct Report;
+}
+
 // These functions are the cross-module import interface to chrome_elf.dll.
 // It is used chrome.exe, chrome.dll and other clients of chrome_elf.
 // In tests, these functions are stubbed by implementations in
@@ -21,6 +29,38 @@ void GetUserDataDirectoryThunk(wchar_t* user_data_dir,
 // happen inside DllMain().
 void SignalInitializeCrashReporting();
 void SignalChromeElf();
+
+int CrashForException(EXCEPTION_POINTERS* info);
+
+HANDLE __cdecl InjectDumpForHungInput(HANDLE process,
+                                      void* serialized_crash_keys);
+
+HANDLE __cdecl InjectDumpForHungInputNoCrashKeys(HANDLE process, int reason);
+
+void __cdecl RegisterNonABICompliantCodeRange(void* start,
+                                              size_t size_in_bytes);
+
+void __cdecl UnregisterNonABICompliantCodeRange(void* start);
+
+void GetCrashReportsImpl(const crash_reporter::Report** reports,
+                         size_t* report_count);
+
+void SetMetricsClientId(const char* client_id);
+
+void SetUploadConsentImpl(bool consent);
+
+// NOTE: This function is used by SyzyASAN to annotate crash reports. If you
+// change the name or signature of this function you will break SyzyASAN
+// instrumented releases of Chrome. Please contact syzygy-team@chromium.org
+// before doing so! See also http://crbug.com/567781.
+void SetCrashKeyValueImpl(const wchar_t* key, const wchar_t* value);
+
+void ClearCrashKeyValueImpl(const wchar_t* key);
+
+// This helper is invoked by code in chrome.dll to request a single crash report
+// upload. See CrashUploadListCrashpad.
+// TODO(siggi): std::string across module boundaries seems ... impertinent.
+void RequestSingleCrashUploadImpl(const std::string& local_id);
 
 }  // extern "C"
 
--- components/crash/content/app/crashpad.cc
+++ components/crash/content/app/crashpad.cc
@@ -24,6 +24,8 @@
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
+// DO NOT SUBMIT - TODO(siggi): figure declaration crap.
+#include "chrome_elf/chrome_elf_main.h"
 #include "components/crash/content/app/crash_reporter_client.h"
 #include "third_party/crashpad/crashpad/client/crash_report_database.h"
 #include "third_party/crashpad/crashpad/client/crashpad_client.h"
@@ -319,7 +321,7 @@ extern "C" {
 // be consistent with
 // crash_reporter::GetCrashReporterClient()->GetCollectStatsConsent(), but it's
 // not enforced to avoid blocking startup code on synchronizing them.
-void __declspec(dllexport) __cdecl SetUploadConsentImpl(bool consent) {
+void SetUploadConsentImpl(bool consent) {
   crash_reporter::SetUploadConsent(consent);
 }
 
@@ -327,20 +329,18 @@ void __declspec(dllexport) __cdecl SetUploadConsentImpl(bool consent) {
 // change the name or signature of this function you will break SyzyASAN
 // instrumented releases of Chrome. Please contact syzygy-team@chromium.org
 // before doing so! See also http://crbug.com/567781.
-void __declspec(dllexport) __cdecl SetCrashKeyValueImpl(const wchar_t* key,
-                                                        const wchar_t* value) {
+void SetCrashKeyValueImpl(const wchar_t* key, const wchar_t* value) {
   crash_reporter::SetCrashKeyValue(base::UTF16ToUTF8(key),
                                    base::UTF16ToUTF8(value));
 }
 
-void __declspec(dllexport) __cdecl ClearCrashKeyValueImpl(const wchar_t* key) {
+void ClearCrashKeyValueImpl(const wchar_t* key) {
   crash_reporter::ClearCrashKey(base::UTF16ToUTF8(key));
 }
 
 // This helper is invoked by code in chrome.dll to request a single crash report
 // upload. See CrashUploadListCrashpad.
-void __declspec(dllexport)
-    RequestSingleCrashUploadImpl(const std::string& local_id) {
+void RequestSingleCrashUploadImpl(const std::string& local_id) {
   crash_reporter::RequestSingleCrashUpload(local_id);
 }
 }  // extern "C"
