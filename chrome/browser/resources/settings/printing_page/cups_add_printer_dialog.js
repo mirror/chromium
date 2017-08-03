ac0c5d49b8d410b97c85546bc63e28b8ea546f35
Sean Kau <skau@chromium.org>
2017-08-03 12:17:06 -0700

    Record printer installation metrics for automatic and manual installs.
    Record IPP make and model information, USB information, and chosen
    PPD data for printers installed from the printer setup page.
    
    Bug: 740270
    Change-Id: I08c34e54694de3eadf3647d16ff052f1ef848eb6

--- chrome/browser/resources/settings/printing_page/cups_add_printer_dialog.js
+++ chrome/browser/resources/settings/printing_page/cups_add_printer_dialog.js
@@ -436,10 +436,13 @@ Polymer({
         'showDiscoveryDialog_');
   },
 
-  /** @private */
-  addPrinter_: function() {
+  /**
+   * @param {!SetupMethod} setupMethod
+   * @private
+   */
+  addPrinter_: function(setupMethod) {
     settings.CupsPrintersBrowserProxyImpl.getInstance().addCupsPrinter(
-        this.newPrinter);
+        setupMethod, this.newPrinter);
   },
 
   /** @private */
@@ -462,7 +465,7 @@ Polymer({
     // Add the printer if it's configurable. Otherwise, forward to the
     // manufacturer dialog.
     if (this.newPrinter.printerAutoconf) {
-      this.addPrinter_();
+      this.addPrinter_(SetupMethod.AUTOMATIC);
     } else {
       this.switchToManufacturerDialog_();
     }
@@ -485,11 +488,11 @@ Polymer({
     if (this.previousDialog_ == AddPrinterDialogs.DISCOVERY) {
       this.configuringDialogTitle =
           loadTimeData.getString('addPrintersNearbyTitle');
-      this.addPrinter_();
+      this.addPrinter_(SetupMethod.AUTOMATIC);
     } else if (this.previousDialog_ == AddPrinterDialogs.MANUFACTURER) {
       this.configuringDialogTitle =
           loadTimeData.getString('selectManufacturerAndModelTitle');
-      this.addPrinter_();
+      this.addPrinter_(SetupMethod.MANUAL);
     } else if (this.previousDialog_ == AddPrinterDialogs.MANUALLY) {
       this.configuringDialogTitle =
           loadTimeData.getString('addPrintersManuallyTitle');
