1a14c6fb74309e9875efdd6f36e28f63ff6e6d4b
Marijn Kruisselbrink <mek@chromium.org>
2017-08-04 11:15:57 -0700

    WIP: Add typemaps for MessagePortMessage and further reduce copies in MessagePort and BCChannel.
    Bug:
    Change-Id: I6cb6849352f7726cb9628b0156a507e08745fabe

--- third_party/WebKit/common/message_port/message_port_message.h
+++ third_party/WebKit/common/message_port/message_port_message.h
@@ -7,14 +7,17 @@
 
 #include <vector>
 #include "mojo/public/cpp/bindings/array_traits_carray.h"
-#include "mojo/public/cpp/system/message_pipe.h"
+#include "third_party/WebKit/common/common_export.h"
+#include "third_party/WebKit/common/message_port/message_port.h"
 
 namespace blink_common {
 
 // This struct represents messages as they are posted over a message port. This
 // type can be serialized as a content::mojom::MessagePortMessage struct.
-struct MessagePortMessage {
+struct BLINK_COMMON_EXPORT MessagePortMessage {
   MessagePortMessage();
+  MessagePortMessage(MessagePortMessage&&);
+  MessagePortMessage& operator=(MessagePortMessage&&);
   ~MessagePortMessage();
 
   // To reduce copies when serializing |encoded_message| does not have to point
@@ -26,7 +29,7 @@ struct MessagePortMessage {
   std::vector<uint8_t> owned_encoded_message;
 
   // Any ports being transfered as part of this message.
-  std::vector<mojo::ScopedMessagePipeHandle> ports;
+  std::vector<MessagePort> ports;
 };
 
 }  // namespace blink_common
