520e20edc03d79b3f406547b127be122b29577f1
Deepanjan Roy <dproy@chromium.org>
2017-08-04 15:15:50 -0400

    [Not ready to land] Adds Consistently Interactive Detector
    This is a proof of concept of Consistently Interactive implementation in
    chromium. Seeking feedback for general approach.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I8c4706a5a068bdfd48cc5acd75ec199f753d2c8c

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -192,6 +192,7 @@
 #include "core/loader/DocumentLoader.h"
 #include "core/loader/FrameFetchContext.h"
 #include "core/loader/FrameLoader.h"
+#include "core/loader/InteractiveDetector.h"
 #include "core/loader/NavigationScheduler.h"
 #include "core/loader/PrerendererClient.h"
 #include "core/loader/appcache/ApplicationCacheHost.h"
@@ -5637,6 +5638,11 @@ void Document::FinishedParsing() {
 
     frame->Loader().FinishedParsing();
 
+    InteractiveDetector* interactive_detector(InteractiveDetector::From(*this));
+    if (interactive_detector) {
+      interactive_detector->OnDomContentLoadedEnd(
+          document_timing_.DomContentLoadedEventEnd());
+    }
     TRACE_EVENT_INSTANT1("devtools.timeline", "MarkDOMContent",
                          TRACE_EVENT_SCOPE_THREAD, "data",
                          InspectorMarkLoadEvent::Data(frame));
