e6d9bcf4dde49371bf277a07e022472093a17b01
Tina Quach <quacht@google.com>
2017-08-03 17:57:07 -0700

    Add AccessibilityTest abstraction for defining tests.
    In order to make it easier to write accessibility tests, this CL
    adds the AccessibilityTest namespace, which groups all audit and mocha
    test related code so it may be reused to test other Chrome WebUI.
    
    Bug: 737739
    Change-Id: I87cc0db0670a5a17d84406d6d2e3fdd619372774

--- chrome/test/data/webui/settings/passwords_a11y_test.js
+++ chrome/test/data/webui/settings/passwords_a11y_test.js
@@ -4,70 +4,72 @@
 
 /** @fileoverview Suite of accessibility tests for the passwords page. */
 
-// Define a mocha suite for every route-rule combination.
-for (let ruleId of AccessibilityAudit.ruleIds) {
-  suite('MANAGE_PASSWORDS_' + ruleId, function() {
-    var passwordsSection = null;
-    var passwordManager = null;
+AccessibilityTest.define({
+  /** @override */
+  name: 'MANAGE_PASSWORDS',
+  /** @type {PasswordManager} */
+  passwordManager: null,
+  /** @type {PasswordsSectionElement}*/
+  passwordsSection: null,
+  /** @override */
+  setup: function() {
+    return new Promise(function(resolve) {
+      // Reset to a blank page.
+      PolymerTest.clearBody();
 
-    /** @type {AccessibilityAuditConfig} **/
-    var auditOptions = {runOnly: {type: 'rule', values: [ruleId]}};
+      // Set the URL to be that of specific route to load upon injecting
+      // settings-ui. Simply calling settings.navigateTo(route) prevents
+      // use of mock APIs for fake data.
+      window.history.pushState(
+          'object or string', 'Test', settings.routes.MANAGE_PASSWORDS.path);
 
-    setup(function() {
-      return new Promise(function(resolve) {
-        // Reset to a blank page.
-        PolymerTest.clearBody();
+      PasswordManagerImpl.instance_ = new TestPasswordManager();
+      passwordManager = PasswordManagerImpl.instance_;
 
-        // Set the URL to be that of specific route to load upon injecting
-        // settings-ui. Simply calling settings.navigateTo(route) prevents
-        // use of mock APIs for fake data.
-        window.history.pushState(
-            'object or string', 'Test', settings.routes.MANAGE_PASSWORDS.path);
+      var settingsUi = document.createElement('settings-ui');
 
-        PasswordManagerImpl.instance_ = new TestPasswordManager();
-        passwordManager = PasswordManagerImpl.instance_;
+      // The settings section will expand to load the MANAGE_PASSWORDS route
+      // (based on the URL set above) once the settings-ui element is attached
+      settingsUi.addEventListener('settings-section-expanded', function() {
+        // Passwords section should be loaded before setup is complete.
+        passwordsSection = settingsUi.$$('settings-main')
+                               .$$('settings-basic-page')
+                               .$$('settings-passwords-and-forms-page')
+                               .$$('passwords-section');
+        assertTrue(!!passwordsSection);
 
-        var settingsUi = document.createElement('settings-ui');
+        assertEquals(passwordManager, passwordsSection.passwordManager_);
 
-        // The settings section will expand to load the MANAGE_PASSWORDS route
-        // (based on the URL set above) once the settings-ui element is attached
-        settingsUi.addEventListener('settings-section-expanded', function() {
-          // Passwords section should be loaded before setup is complete.
-          passwordsSection = settingsUi.$$('settings-main')
-                                 .$$('settings-basic-page')
-                                 .$$('settings-passwords-and-forms-page')
-                                 .$$('passwords-section');
-
-          assertTrue(!!passwordsSection);
-
-          assertEquals(passwordManager, passwordsSection.passwordManager_);
-
-          resolve();
-        });
-
-        document.body.appendChild(settingsUi);
+        resolve();
       });
+
+      document.body.appendChild(settingsUi);
     });
+  },
 
-    test('Accessible with 0 passwords', function() {
-      assertEquals(0, passwordsSection.savedPasswords.length);
-      return SettingsAccessibilityTest.runAudit(auditOptions);
-    });
+  /** @override */
+  tests: {
+    'Accessible with 0 passwords': function() {
+      return new Promise(function(resolve) {
+        assertEquals(0, this.passwordsSection.savedPasswords.length);
+        resolve();
+      });
+    },
+    'Accessible with 100 passwords': function() {
+      return new Promise(function(resolve) {
+        var fakePasswords = [];
+        for (var i = 0; i < 100; i++) {
+          fakePasswords.push(FakeDataMaker.passwordEntry());
+        }
 
-    test('Accessible with 100 passwords', function() {
-      var fakePasswords = [];
-      for (var i = 0; i < 100; i++) {
-        fakePasswords.push(FakeDataMaker.passwordEntry());
-      }
+        // Set list of passwords.
+        this.passwordManager.lastCallback.addSavedPasswordListChangedListener(
+            fakePasswords);
+        Polymer.dom.flush();
 
-      // Set list of passwords.
-      passwordManager.lastCallback.addSavedPasswordListChangedListener(
-          fakePasswords);
-      Polymer.dom.flush();
-
-      assertEquals(100, passwordsSection.savedPasswords.length);
-
-      return SettingsAccessibilityTest.runAudit(auditOptions);
-    });
-  });
-};
+        assertEquals(100, this.passwordsSection.savedPasswords.length);
+        resolve();
+      });
+    },
+  }
+});
