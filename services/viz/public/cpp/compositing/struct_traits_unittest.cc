a85be78022e3fb379f9eff489a1910349491f484
Saman Sami <samans@chromium.org>
2017-08-04 15:08:24 -0400

    viz: Move SurfaceInfo's mojom from cc/ipc to services/viz
    Bug:722935
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Icbcfda264a4272048877a5e9dfb25c0ea5e5b16a

--- services/viz/public/cpp/compositing/struct_traits_unittest.cc
+++ services/viz/public/cpp/compositing/struct_traits_unittest.cc
@@ -5,10 +5,17 @@
 #include <utility>
 
 #include "base/message_loop/message_loop.h"
+#include "cc/ipc/frame_sink_id_struct_traits.h"
+#include "cc/ipc/local_surface_id_struct_traits.h"
+#include "cc/ipc/surface_id_struct_traits.h"
 #include "components/viz/common/resources/resource_settings.h"
+#include "components/viz/common/surfaces/surface_info.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "services/viz/public/cpp/compositing/resource_settings_struct_traits.h"
+#include "services/viz/public/cpp/compositing/surface_info_struct_traits.h"
+#include "services/viz/public/interfaces/compositing/surface_info.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/gfx/geometry/mojo/geometry_struct_traits.h"
 
 namespace viz {
 
@@ -34,6 +41,24 @@ TEST_F(StructTraitsTest, ResourceSettings) {
             output.use_gpu_memory_buffer_resources);
   EXPECT_EQ(input.buffer_to_texture_target_map,
             output.buffer_to_texture_target_map);
+}
+
+TEST_F(StructTraitsTest, SurfaceInfo) {
+  const SurfaceId surface_id(
+      FrameSinkId(1234, 4321),
+      LocalSurfaceId(5678,
+                     base::UnguessableToken::Deserialize(143254, 144132)));
+  constexpr float device_scale_factor = 1.234;
+  constexpr gfx::Size size(987, 123);
+
+  SurfaceInfo input(surface_id, device_scale_factor, size);
+  SurfaceInfo output;
+  mojom::SurfaceInfo::Deserialize(mojom::SurfaceInfo::Serialize(&input),
+                                  &output);
+
+  EXPECT_EQ(input.id(), output.id());
+  EXPECT_EQ(input.size_in_pixels(), output.size_in_pixels());
+  EXPECT_EQ(input.device_scale_factor(), output.device_scale_factor());
 }
 
 }  // namespace
