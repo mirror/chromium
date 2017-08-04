102215eb1a321f519e42122a917417312346e899
Karan Bhatia <karandeepb@chromium.org>
2017-08-03 17:20:48 -0700

    DNR: Introduce FlatRulesetIndexer to index rules to flatbuffer format.
    https://chromium-review.googlesource.com/c/595223 introduced the the flatbuffer
    schema to store rules for the Declarative Net Request API. This CL introduces
    FlatRulesetIndexer which takes in IndexedRule(s) and converts them to flatbuffer
    format. In a subsequent CL, this will be used to persist rules provided by
    extensions as part of its manifest.
    
    Doc=http://go/declarative-net-request (Internal only)
    BUG=696822
    
    Change-Id: I9496c381e45681ee18ee21f74a60fa6b8ed4db32

--- extensions/common/api/declarative_net_request/indexed_rule.cc
+++ extensions/common/api/declarative_net_request/indexed_rule.cc
@@ -241,6 +241,8 @@ std::vector<std::string> CanonicalizeDomains(
 
 IndexedRule::IndexedRule() = default;
 IndexedRule::~IndexedRule() = default;
+IndexedRule::IndexedRule(IndexedRule&&) = default;
+IndexedRule& IndexedRule::operator=(IndexedRule&&) = default;
 
 ParseResult CreateIndexedRule(std::unique_ptr<dnr_api::Rule> parsed_rule,
                               IndexedRule* indexed_rule) {
