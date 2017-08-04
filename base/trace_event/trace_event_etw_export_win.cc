6a6e56ace8a1c6aaee2501f9261835ebafc541da
Nico Weber <thakis@chromium.org>
2017-08-04 13:52:11 -0400

    cross gn, reloaded
    gerrit version of https://codereview.chromium.org/1183633003/#ps720001
    
    0. (Linux-only)
    
    on linux, need to create a case-insensitive mount for the SDK (for uuid.lib vs Uuid.lib etc):
    
      # Win SDK is 2GB, make a 3GB image.
      fallocate -l 3g vs2017.disk
      mkfs.vfat vs2017.disk
      mkdir third_party/depot_tools/win_toolchain/vs_files
      sudo mount vs2017.disk third_party/depot_tools/win_toolchain/vs_files -t vfat -o loop,owner,group,umask=000
    
    1. Download SDK (Googlers only)
      cd path/to/chrome/src
      # Authenticate yourself.
      # Follow instructions, enter 0 as project id.
      download_from_google_storage --config
      # Actually download the thing
      GYP_MSVS_VERSION=2017 build/vs_toolchain.py update --force
    
    $ gn args out/gnwin
    target_cpu = "x64"
    target_os = "win"
    is_clang = true
    enable_nacl = false  # makes a few things easier for now (see bug)
    # On Mac, we don't bundle lld with clang, so there also need:
    # Not needed on linux unless you want to use a custom clang.
    clang_use_chrome_plugins = false  # for local clang builds
    clang_base_path = getenv("HOME") + "/src/llvm-build"
    
    ninja -C out/gnwin/ ../../v8/src/api.cc^
    
    BUG=495204
    also needs mc.exe for the etw provider in base and for the one remoting file.
    also needs mt.exe for some manifest stuff
    also needs rc.exe in quite a few places (see https://codereview.chromium.org/2628573003/ , https://github.com/nico/hack/blob/master/res/rc.cc)
    also midl.exe
    nacl build calls cl.exe to build .S files and ml64 to build .asm files :-/
    one .asm file in crashpad also needs ml64
    
    thakis@thakis:~/src/chrome/src/third_party/angle$ git diff
    diff --git a/src/libGLESv2.gypi b/src/libGLESv2.gypi
    index 3e7d9f8..c5ff631 100644
    --- a/src/libGLESv2.gypi
    +++ b/src/libGLESv2.gypi
    @@ -623,14 +623,14 @@
                 'libGLESv2/global_state.h',
                 'libGLESv2/libGLESv2.cpp',
                 'libGLESv2/libGLESv2.def',
    -            'libGLESv2/libGLESv2.rc',
    +            #'libGLESv2/libGLESv2.rc',
                 'libGLESv2/resource.h',
             ],
             'libegl_sources':
             [
                 'libEGL/libEGL.cpp',
                 'libEGL/libEGL.def',
    -            'libEGL/libEGL.rc',
    +            #'libEGL/libEGL.rc',
                 'libEGL/resource.h',
             ],
         },
    
    Bug: 495204
    Change-Id: Ic522107f9653d7f08f1f1792fe6e599789d33e8b

--- base/trace_event/trace_event_etw_export_win.cc
+++ base/trace_event/trace_event_etw_export_win.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#if 0
 #include "base/trace_event/trace_event_etw_export_win.h"
 
 #include <stddef.h>
@@ -376,3 +377,4 @@ void TraceEventETWExport::UpdateETWKeyword() {
 }
 }  // namespace trace_event
 }  // namespace base
+#endif
