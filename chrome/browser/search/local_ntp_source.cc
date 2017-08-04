b9e4fdcbec99be99171f0b4055c3f92e8c0fa903
Ondrej Skopek <oskopek@google.com>
2017-08-04 10:33:53 +0200

    Added flag for Voice search on the local NTP.
    Includes basic localization resources and stubs
    for porting voice search code from the remote NTP
    to the local NTP.
    
    Bug: 583291
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I152f41e55ed78d3c6b6e1aa35ffe623f9f5e9341

--- chrome/browser/search/local_ntp_source.cc
+++ chrome/browser/search/local_ntp_source.cc
@@ -67,9 +67,11 @@ const struct Resource{
 } kResources[] = {
     {kMainHtmlFilename, kLocalResource, "text/html"},
     {"local-ntp.js", IDR_LOCAL_NTP_JS, "application/javascript"},
+    {"voice.js", IDR_LOCAL_NTP_VOICE_JS, "application/javascript"},
     {kConfigDataFilename, kLocalResource, "application/javascript"},
     {kThemeCSSFilename, kLocalResource, "text/css"},
     {"local-ntp.css", IDR_LOCAL_NTP_CSS, "text/css"},
+    {"voice.css", IDR_LOCAL_NTP_VOICE_CSS, "text/css"},
     {"images/close_3_mask.png", IDR_CLOSE_3_MASK, "image/png"},
     {"images/ntp_default_favicon.png", IDR_NTP_DEFAULT_FAVICON, "image/png"},
     {kOneGoogleBarScriptFilename, kLocalResource, "text/javascript"},
@@ -116,6 +118,10 @@ std::string GetConfigData(bool is_google) {
   base::DictionaryValue config_data;
   config_data.Set("translatedStrings", GetTranslatedStrings(is_google));
   config_data.SetBoolean("isGooglePage", is_google);
+
+  bool is_voice_search_enabled =
+      base::FeatureList::IsEnabled(features::kVoiceSearchOnLocalNtp);
+  config_data.SetBoolean("isVoiceSearchEnabled", is_voice_search_enabled);
 
   // Serialize the dictionary.
   std::string js_text;
@@ -290,7 +296,8 @@ void LocalNtpSource::StartDataRequest(
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
   if (command_line->HasSwitch(switches::kLocalNtpReload)) {
     if (stripped_path == "local-ntp.html" || stripped_path == "local-ntp.js" ||
-        stripped_path == "local-ntp.css") {
+        stripped_path == "local-ntp.css" || stripped_path == "voice.js" ||
+        stripped_path == "voice.css") {
       base::ReplaceChars(stripped_path, "-", "_", &stripped_path);
       local_ntp::SendLocalFileResource(stripped_path, callback);
       return;
@@ -312,6 +319,11 @@ void LocalNtpSource::StartDataRequest(
         base::StringPrintf(kIntegrityFormat, LOCAL_NTP_JS_INTEGRITY);
     base::ReplaceFirstSubstringAfterOffset(&html, 0, "{{LOCAL_NTP_INTEGRITY}}",
                                            local_ntp_integrity);
+
+    std::string local_ntp_voice_integrity =
+        base::StringPrintf(kIntegrityFormat, VOICE_JS_INTEGRITY);
+    base::ReplaceFirstSubstringAfterOffset(
+        &html, 0, "{{LOCAL_NTP_VOICE_INTEGRITY}}", local_ntp_voice_integrity);
     callback.Run(base::RefCountedString::TakeString(&html));
     return;
   }
@@ -387,11 +399,11 @@ std::string LocalNtpSource::GetContentSecurityPolicyScriptSrc() const {
 #endif  // !defined(GOOGLE_CHROME_BUILD)
 
   return base::StringPrintf(
-      "script-src 'strict-dynamic' 'sha256-%s' 'sha256-%s';",
+      "script-src 'strict-dynamic' 'sha256-%s' 'sha256-%s' 'sha256-%s';",
       GetIntegritySha256Value(
           GetConfigData(default_search_provider_is_google_io_thread_))
           .c_str(),
-      LOCAL_NTP_JS_INTEGRITY);
+      LOCAL_NTP_JS_INTEGRITY, VOICE_JS_INTEGRITY);
 }
 
 std::string LocalNtpSource::GetContentSecurityPolicyChildSrc() const {
