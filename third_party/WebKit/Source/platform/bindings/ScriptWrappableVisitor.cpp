4025fbe8e7969826c4404ecc0c8607d626163582
Michael Lippautz <mlippautz@chromium.org>
2017-08-04 11:21:01 +0200

    [wrapper-tracing] Switch to conservative Dijkstra barrier
    On assignment 'x.a = y' during incremental marking the Dijkstra barrier
    suggests checking the color of 'x' and only mark 'y' if 'x' is marked.
    
    Since checking 'x' is expensive in the current setting, as it requires
    either a backpointer or expensive lookup logic due to large objects and
    multiple inheritance, just assume that 'x' is black. We assume here that
    since an object 'x' is referenced for a write, it will generally also be
    alive in the current GC cycle.
    
    As a side-effect this CL also
    - Remove the need for explicit construction of TraceWrapperMember.
    - Fixes swap() performance when tracing is off.
    - Enable the use of more TraceWrapperMember where we previously used
      a manual write barrier.
    
    Bug:
    Change-Id: Ic283a2eaac1323aa3019e08ba9fa330900bdd0aa

--- third_party/WebKit/Source/platform/bindings/ScriptWrappableVisitor.cpp
+++ third_party/WebKit/Source/platform/bindings/ScriptWrappableVisitor.cpp
@@ -224,18 +224,14 @@ void ScriptWrappableVisitor::MarkWrappersInAllWorlds(
 
 void ScriptWrappableVisitor::WriteBarrier(
     v8::Isolate* isolate,
-    const void* src_object,
     const TraceWrapperV8Reference<v8::Value>* dst_object) {
   if (!src_object || !dst_object || dst_object->IsEmpty() ||
       !ThreadState::Current()->WrapperTracingInProgress()) {
     return;
   }
-  // We only require a write barrier if |srcObject|  is already marked. Note
-  // that this implicitly disables the write barrier when the GC is not
-  // active as object will not be marked in this case.
-  if (!HeapObjectHeader::FromPayload(src_object)->IsWrapperHeaderMarked()) {
-    return;
-  }
+
+  // Conservatively assume that the source object containing |dst_object| is
+  // marked.
   CurrentVisitor(isolate)->MarkWrapper(
       &(const_cast<TraceWrapperV8Reference<v8::Value>*>(dst_object)->Get()));
 }
@@ -310,6 +306,12 @@ void ScriptWrappableVisitor::PerformCleanup(v8::Isolate* isolate) {
 
 WrapperVisitor* ScriptWrappableVisitor::CurrentVisitor(v8::Isolate* isolate) {
   return V8PerIsolateData::From(isolate)->GetScriptWrappableVisitor();
+}
+
+WrapperVisitor* ScriptWrappableVisitor::CurrentVisitor() {
+  const ThreadState* thread_state = ThreadState::Current();
+  DCHECK(thread_state);
+  return CurrentVisitor(thread_state->GetIsolate());
 }
 
 }  // namespace blink
