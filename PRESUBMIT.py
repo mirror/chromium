e7b321d7112b7c71cbfb959733b6ffb6c496d132
Robert Liao <robliao@chromium.org>
2017-08-04 15:46:29 -0700

    Make the RunLoop Presubmit Error Messages a Tuple
    The message is expected to be a tuple, otherwise one character per
    line is output like so:
    Banned functions were used.
        [FILENAME]:100:
          P
          l
          e
          a
          s
          e
    
    BUG=752670
    
    Change-Id: I8b91f3b236155d0ac2f037a97f3ba1289ec4c381

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -411,8 +411,8 @@ _BANNED_CPP_FUNCTIONS = (
     (
       'MessageLoop::QuitWhenIdleClosure',
       (
-        'MessageLoop::QuitWhenIdleClosure is deprecated. Please migrate to'
-        'Runloop.'
+        'MessageLoop::QuitWhenIdleClosure is deprecated. Please migrate to',
+        'Runloop.',
       ),
       True,
       (),
@@ -420,8 +420,8 @@ _BANNED_CPP_FUNCTIONS = (
     (
       'RunLoop::QuitCurrent',
       (
-        'Please migrate away from RunLoop::QuitCurrent*() methods. Use member'
-        'methods of a specific RunLoop instance instead.'
+        'Please migrate away from RunLoop::QuitCurrent*() methods. Use member',
+        'methods of a specific RunLoop instance instead.',
       ),
       True,
       (),
