939e10b86fefc6335077ab6a62145f38ac15515e
Anantanarayanan Iyengar <ananta@chromium.org>
2017-08-03 19:19:49 -0700

    Add support for updating the AppCache using the network URL loader.
    This patch adds the implementation for the methods of the AppCacheUpdateJob::UpdateURLLoaderRequest
    class added in the last iteration.
    
    Following changes:
    1. Bring back the helper classes from mojo for facilitating copying between a mojo buffer and net.
       These classes are MojoToNetPendingBuffer and MojoToNetIOBuffer.
    
    2. The UpdateRequestBase::Read function does not take an IOBuffer anymore. Instead the
       subclasses (URLRequest and URLLoader) pass the IOBuffer to AppCacheUpdateJob::URLFetcher::OnReadCompleted()
    
    3. AppCacheUpdateJob::URLFetcher does not maintain an IOBuffer anymore. The buffers are maintained by the
       request subclasses.
    
    4. The AppCacheUpdateJob::UpdateURLRequest::Read implicitly invokes the fetchers  OnReadCompleted() function
       if the read is synchronously completed. It always returns ERR_IO_PENDING though.
    
    5. The AppCacheServiceImpl class now saves away the URLLoaderFactoryGetter class. This
       enables the updater based on URLLoader to retrieve the network service from there.
    
    BUG=715632
    TEST=Verified by manual testing on the w3schools AppCache test page and http://webdbg.com/test/appcache/
         The entries get setup correctly and the page loads in offline
    
    Change-Id: Ic7c42471589aff33c68de9af6bafd0c881e5b4db

--- content/browser/appcache/appcache_update_url_loader_request.cc
+++ content/browser/appcache/appcache_update_url_loader_request.cc
@@ -4,6 +4,7 @@
 
 #include "content/browser/appcache/appcache_update_url_loader_request.h"
 #include "content/browser/appcache/appcache_update_url_fetcher.h"
+#include "net/http/http_response_info.h"
 #include "net/url_request/url_request_context.h"
 
 namespace content {
@@ -11,93 +12,111 @@ namespace content {
 AppCacheUpdateJob::UpdateURLLoaderRequest::~UpdateURLLoaderRequest() {}
 
 void AppCacheUpdateJob::UpdateURLLoaderRequest::Start() {
-  NOTIMPLEMENTED();
+  mojom::URLLoaderClientPtr client;
+  client_binding_.Bind(mojo::MakeRequest(&client));
+
+  loader_factory_getter_->GetNetworkFactory()->get()->CreateLoaderAndStart(
+      mojo::MakeRequest(&url_loader_), -1, -1, 0, request_, std::move(client),
+      net::MutableNetworkTrafficAnnotationTag(GetTrafficAnnotation()));
 }
 
 void AppCacheUpdateJob::UpdateURLLoaderRequest::SetExtraRequestHeaders(
     const net::HttpRequestHeaders& headers) {
-  NOTIMPLEMENTED();
+  request_.headers = headers.ToString();
 }
 
 GURL AppCacheUpdateJob::UpdateURLLoaderRequest::GetURL() const {
-  NOTIMPLEMENTED();
-  return GURL();
-}
-
-GURL AppCacheUpdateJob::UpdateURLLoaderRequest::GetOriginalURL() const {
-  NOTIMPLEMENTED();
-  return GURL();
+  return request_.url;
 }
 
 void AppCacheUpdateJob::UpdateURLLoaderRequest::SetLoadFlags(int flags) {
-  NOTIMPLEMENTED();
+  request_.load_flags = flags;
 }
 
 int AppCacheUpdateJob::UpdateURLLoaderRequest::GetLoadFlags() const {
-  NOTIMPLEMENTED();
-  return 0;
+  return request_.load_flags;
 }
 
 std::string AppCacheUpdateJob::UpdateURLLoaderRequest::GetMimeType() const {
-  NOTIMPLEMENTED();
-  return std::string();
+  return response_->mime_type;
 }
 
 void AppCacheUpdateJob::UpdateURLLoaderRequest::SetFirstPartyForCookies(
     const GURL& first_party_for_cookies) {
-  NOTIMPLEMENTED();
+  request_.first_party_for_cookies = first_party_for_cookies;
 }
 
 void AppCacheUpdateJob::UpdateURLLoaderRequest::SetInitiator(
     const base::Optional<url::Origin>& initiator) {
-  NOTIMPLEMENTED();
+  request_.request_initiator = initiator;
 }
 
 net::HttpResponseHeaders*
 AppCacheUpdateJob::UpdateURLLoaderRequest::GetResponseHeaders() const {
-  NOTIMPLEMENTED();
-  return nullptr;
+  return response_->headers.get();
 }
 
 int AppCacheUpdateJob::UpdateURLLoaderRequest::GetResponseCode() const {
-  NOTIMPLEMENTED();
+  if (response_->headers)
+    return response_->headers->response_code();
   return 0;
 }
 
 net::HttpResponseInfo
 AppCacheUpdateJob::UpdateURLLoaderRequest::GetResponseInfo() const {
-  NOTIMPLEMENTED();
-  return net::HttpResponseInfo();
+  return *http_response_info_;
 }
 
-const net::URLRequestContext*
-AppCacheUpdateJob::UpdateURLLoaderRequest::GetRequestContext() const {
-  NOTIMPLEMENTED();
-  return nullptr;
-}
-
-int AppCacheUpdateJob::UpdateURLLoaderRequest::Read(net::IOBuffer* buf,
-                                                    int max_bytes) {
-  NOTIMPLEMENTED();
-  return 0;
+void AppCacheUpdateJob::UpdateURLLoaderRequest::Read() {
+  read_requested_ = true;
+  // Initiate a read from the pipe if we have not done so.
+  MaybeStartReading();
 }
 
 int AppCacheUpdateJob::UpdateURLLoaderRequest::Cancel() {
-  NOTIMPLEMENTED();
+  client_binding_.Close();
+  url_loader_ = nullptr;
   return 0;
+}
+
+// TODO(ananta/michaeln)
+// Add support in the URLLoader to return information on whether the http stack
+// is configured to ignore certificate errors.
+bool AppCacheUpdateJob::UpdateURLLoaderRequest::ShouldIgnoreCertificateErrors()
+    const {
+  return false;
 }
 
 void AppCacheUpdateJob::UpdateURLLoaderRequest::OnReceiveResponse(
     const ResourceResponseHead& response_head,
     const base::Optional<net::SSLInfo>& ssl_info,
     mojom::DownloadedTempFilePtr downloaded_file) {
-  NOTIMPLEMENTED();
+  response_.reset(new ResourceResponseHead());
+  *response_ = response_head;
+
+  // TODO(ananta/michaeln)
+  // Populate other fields in the HttpResponseInfo class. It would be good to
+  // have a helper function which populates the HttpResponseInfo structure from
+  // the ResourceResponseHead structure.
+  http_response_info_.reset(new net::HttpResponseInfo());
+  if (ssl_info.has_value())
+    http_response_info_->ssl_info = *ssl_info;
+  http_response_info_->headers = response_head.headers;
+  http_response_info_->was_fetched_via_spdy =
+      response_head.was_fetched_via_spdy;
+  http_response_info_->was_alpn_negotiated = response_head.was_alpn_negotiated;
+  http_response_info_->alpn_negotiated_protocol =
+      response_head.alpn_negotiated_protocol;
+  http_response_info_->connection_info = response_head.connection_info;
+  http_response_info_->socket_address = response_head.socket_address;
+  fetcher_->OnResponseStarted(net::OK);
 }
 
 void AppCacheUpdateJob::UpdateURLLoaderRequest::OnReceiveRedirect(
     const net::RedirectInfo& redirect_info,
     const ResourceResponseHead& response_head) {
-  NOTIMPLEMENTED();
+  *response_ = response_head;
+  fetcher_->OnReceivedRedirect(redirect_info);
 }
 
 void AppCacheUpdateJob::UpdateURLLoaderRequest::OnDataDownloaded(
@@ -115,7 +134,6 @@ void AppCacheUpdateJob::UpdateURLLoaderRequest::OnUploadProgress(
 
 void AppCacheUpdateJob::UpdateURLLoaderRequest::OnReceiveCachedMetadata(
     const std::vector<uint8_t>& data) {
-  NOTIMPLEMENTED();
 }
 
 void AppCacheUpdateJob::UpdateURLLoaderRequest::OnTransferSizeUpdated(
@@ -125,17 +143,99 @@ void AppCacheUpdateJob::UpdateURLLoaderRequest::OnTransferSizeUpdated(
 
 void AppCacheUpdateJob::UpdateURLLoaderRequest::OnStartLoadingResponseBody(
     mojo::ScopedDataPipeConsumerHandle body) {
-  NOTIMPLEMENTED();
+  handle_ = std::move(body);
+  // Initiate a read from the pipe if we have a pending Read() request.
+  MaybeStartReading();
 }
 
 void AppCacheUpdateJob::UpdateURLLoaderRequest::OnComplete(
     const ResourceRequestCompletionStatus& status) {
-  NOTIMPLEMENTED();
+  response_status_ = status;
+  // We inform the URLFetcher about a failure only here. For the success case
+  // OnResponseCompleted() is invoked URLFetcher::OnReadCompleted().
+  if (status.error_code != net::OK)
+    fetcher_->OnResponseCompleted(status.error_code);
 }
 
 AppCacheUpdateJob::UpdateURLLoaderRequest::UpdateURLLoaderRequest(
-    net::URLRequestContext* request_context,
+    URLLoaderFactoryGetter* loader_factory_getter,
     const GURL& url,
-    URLFetcher* fetcher) {}
+    int buffer_size,
+    URLFetcher* fetcher)
+    : fetcher_(fetcher),
+      loader_factory_getter_(loader_factory_getter),
+      client_binding_(this),
+      buffer_(new net::IOBuffer(buffer_size)),
+      buffer_size_(buffer_size),
+      handle_watcher_(FROM_HERE, mojo::SimpleWatcher::ArmingPolicy::MANUAL),
+      read_requested_(false) {
+  request_.url = url;
+  request_.method = "GET";
+}
+
+int AppCacheUpdateJob::UpdateURLLoaderRequest::OnDataAvailable(
+    size_t num_bytes) {
+  int bytes_copied = std::min<int>(buffer_size_, num_bytes);
+
+  scoped_refptr<MojoToNetIOBuffer> buffer =
+      new MojoToNetIOBuffer(pending_read_.get());
+
+  read_requested_ = false;
+  fetcher_->OnReadCompleted(buffer.get(), bytes_copied);
+  return bytes_copied;
+}
+
+void AppCacheUpdateJob::UpdateURLLoaderRequest::OnDataComplete() {
+  DCHECK(response_status_.error_code == net::OK);
+  fetcher_->OnReadCompleted(buffer_.get(), 0);
+}
+
+void AppCacheUpdateJob::UpdateURLLoaderRequest::StartReading(
+    MojoResult unused) {
+  // Wait for the caller to initiate a read.
+  if (!read_requested_)
+    return;
+
+  uint32_t bytes_read = 0;
+  MojoResult result =
+      MojoToNetPendingBuffer::BeginRead(&handle_, &pending_read_, &bytes_read);
+  if (result == MOJO_RESULT_SHOULD_WAIT) {
+    handle_watcher_.ArmOrNotify();
+    return;
+  }
+
+  if (result == MOJO_RESULT_BUSY)
+    return;
+
+  if (result == MOJO_RESULT_FAILED_PRECONDITION) {
+    OnDataComplete();
+    return;
+  }
+
+  if (result != MOJO_RESULT_OK) {
+    response_status_.error_code = net::ERR_FAILED;
+    OnComplete(response_status_);
+    return;
+  }
+
+  int bytes_copied = OnDataAvailable(bytes_read);
+  handle_ = pending_read_->Complete(bytes_copied);
+}
+
+void AppCacheUpdateJob::UpdateURLLoaderRequest::MaybeStartReading() {
+  if (!handle_.is_valid() || !read_requested_)
+    return;
+
+  if (handle_watcher_.IsWatching()) {
+    handle_watcher_.ArmOrNotify();
+    return;
+  }
+
+  handle_watcher_.Watch(
+      handle_.get(), MOJO_HANDLE_SIGNAL_READABLE,
+      base::Bind(&AppCacheUpdateJob::UpdateURLLoaderRequest::StartReading,
+                 base::Unretained(this)));
+  StartReading(0);
+}
 
 }  // namespace content
