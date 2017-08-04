fd60bc54cd43d2ef127a325c8398f5ad07944dc7
Charles Harrison <csharrison@chromium.org>
2017-08-04 13:46:27 -0400

    New mojo service for exposing FirstPaint per RenderFrameHost
    This service hooks directly in Blink's PaintTiming class which computes
    the first paint, and uses the same timestamp as exposed to the
    web platform.
    
    A followup will plumb the data to WebContentsObservers, and formally
    deprecate WebContentsObserver::DidFirstVisuallyNonEmptyPaint.
    
    Bug: TODO
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I67618fcdbca9670ff37d5f03f113a0f0dd65b62c

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -2853,6 +2853,10 @@ void RenderFrameHostImpl::RunCreateWindowCompleteCallback(
 }
 
 void RenderFrameHostImpl::RegisterMojoInterfaces() {
+  registry_->AddInterface(
+      base::Bind(&RenderFrameHostImpl::CreateFrameBrokerService,
+                 weak_ptr_factory_.GetWeakPtr()));
+
 #if !defined(OS_ANDROID)
   // The default (no-op) implementation of InstalledAppProvider. On Android, the
   // real implementation is provided in Java.
@@ -3713,6 +3717,10 @@ void RenderFrameHostImpl::GetInterfaceProvider(
                               std::move(interfaces), std::move(provider));
 }
 
+void RenderFrameHostImpl::OnFirstPaint(base::TimeDelta time_to_first_paint) {
+  delegate_->OnFirstPaintInFrame(this, time_to_first_paint);
+}
+
 #if BUILDFLAG(USE_EXTERNAL_POPUP_MENU)
 #if defined(OS_MACOSX)
 
@@ -4248,5 +4256,10 @@ void RenderFrameHostImpl::ForwardGetInterfaceToRenderFrame(
   GetRemoteInterfaces()->GetInterface(interface_name, std::move(pipe));
 }
 #endif
+
+void RenderFrameHostImpl::CreateFrameBrokerService(
+    blink::mojom::FrameBrokerRequest request) {
+  frame_broker_binding_.AddBinding(this, std::move(request));
+}
 
 }  // namespace content
