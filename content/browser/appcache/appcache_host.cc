5761aca27d090d0969087736ff89053b91e43f93
Mark Pilgrim <pilgrim@chromium.org>
2017-08-04 15:46:28 -0400

    Appcache: BindOnce refactoring
    Make AppCacheDispatcherHost::GetStatusCallback a OnceCallback and convert
    references from Bind to BindOnce.
    
    Bug: 714018
    Change-Id: I54035dd88155671d14721f5ba45f27ceefa63f3e

--- content/browser/appcache/appcache_host.cc
+++ content/browser/appcache/appcache_host.cc
@@ -210,13 +210,13 @@ bool AppCacheHost::MarkAsForeignEntry(const GURL& document_url,
   return true;
 }
 
-void AppCacheHost::GetStatusWithCallback(const GetStatusCallback& callback,
+void AppCacheHost::GetStatusWithCallback(GetStatusCallback callback,
                                          void* callback_param) {
   DCHECK(pending_start_update_callback_.is_null() &&
          pending_swap_cache_callback_.is_null() &&
          pending_get_status_callback_.is_null());
 
-  pending_get_status_callback_ = callback;
+  pending_get_status_callback_ = std::move(callback);
   pending_callback_param_ = callback_param;
   if (is_selection_pending())
     return;
@@ -227,7 +227,8 @@ void AppCacheHost::GetStatusWithCallback(const GetStatusCallback& callback,
 void AppCacheHost::DoPendingGetStatus() {
   DCHECK_EQ(false, pending_get_status_callback_.is_null());
 
-  pending_get_status_callback_.Run(GetStatus(), pending_callback_param_);
+  std::move(pending_get_status_callback_)
+      .Run(GetStatus(), pending_callback_param_);
   pending_get_status_callback_.Reset();
   pending_callback_param_ = NULL;
 }
@@ -259,7 +260,8 @@ void AppCacheHost::DoPendingStartUpdate() {
     }
   }
 
-  pending_start_update_callback_.Run(success, pending_callback_param_);
+  std::move(pending_start_update_callback_)
+      .Run(success, pending_callback_param_);
   pending_start_update_callback_.Reset();
   pending_callback_param_ = NULL;
 }
@@ -295,7 +297,7 @@ void AppCacheHost::DoPendingSwapCache() {
     }
   }
 
-  pending_swap_cache_callback_.Run(success, pending_callback_param_);
+  std::move(pending_swap_cache_callback_).Run(success, pending_callback_param_);
   pending_swap_cache_callback_.Reset();
   pending_callback_param_ = NULL;
 }
