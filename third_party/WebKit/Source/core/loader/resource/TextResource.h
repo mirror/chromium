3dd5e0f49e89fb9edd5b7fb08658af57ef9a18be
Daniel Vogelheim <vogelheim@chromium.org>
2017-08-04 10:27:46 +0200

    Fix preloading of resources with integrity attributes.
    Currently, a preloaded resource doesn't match a later resource request
    with an integrity=... attribute, which will call the browser to reload
    that resource. Which makes the preload pretty useless. This patch will
    match and "adopt" a preload, if the request has integrity but the
    preload doesn't.
    
    The actual fix are the few lines in ResourceFetcher. The remaining code
    is a minor refactoring that
    - moves the "cached" character-converted string into TextResource,
      (all in protected methods, so that subclasses can choose to use this
       or not)
    - thus de-dupes a tiny bit of logic between ScriptResource and
      CSSStyleSheetResource,
    - ensures that the text conversion has the same life-cycle in both of
      those resources. (Previously, CSSStyleSheetResource would always
      trigger the character conversion in CheckNotify, even if it was a
      preload and noone would actually use the data (yet). That prevent
      "adoption" of the preload in some circumstances for CSS resources.)
    
    
    Thanks to csharrison for the test case.
    
    Bug: 677022
    Change-Id: I21942be148355ed3fec904c854d4a398518acc2b

--- third_party/WebKit/Source/core/loader/resource/TextResource.h
+++ third_party/WebKit/Source/core/loader/resource/TextResource.h
@@ -15,10 +15,6 @@ namespace blink {
 
 class CORE_EXPORT TextResource : public Resource {
  public:
-  // Returns the decoded data in text form. The data has to be available at
-  // call time.
-  String DecodedText() const;
-
   WTF::TextEncoding Encoding() const override;
 
   void SetEncodingForTest(const String& encoding) { SetEncoding(encoding); }
@@ -30,10 +26,26 @@ class CORE_EXPORT TextResource : public Resource {
                const TextResourceDecoderOptions&);
   ~TextResource() override;
 
+  // Returns a copy of the data received so far in text form.
+  // If the resource hasn't completed loading yet, this will be an incomplete
+  // representation of the resource.
+  String CurrentDecodedText() const;
+
+  // Returns the resource data in text form. This ensures that only one copy
+  // and text conversion happen. This will delete the original data.
+  // Requires that the resource has loaded.
+  // These methods are protected so that subclasses have full control over
+  // whether and when these operations are performed.
+  const String& CachedDecodedText() const;
+  const String& CachedDecodedText();
+  void ClearDecodedTextCache();
+  size_t DecodedTextCacheSize() const;
+
   void SetEncoding(const String&) override;
 
  private:
   std::unique_ptr<TextResourceDecoder> decoder_;
+  String decoded_text_cache_;
 };
 
 }  // namespace blink
