4227f4ee9fc1d3926bfa27ab006522cc6f47b4e3
Christopher Benson <chrisbenson@google.com>
2017-08-03 17:09:36 -0700

    Refactor of Trackers in Feature Engagement Tracker
    Changes on this CL:
    - Preemptively refactor out of NewTabTracker into a generic FeatureTracker
    - Moves all active session time tracking into FeatureTracker, so that time isn't double counted with multiple features being tracker.
    
    Bug: 734132
    Change-Id: I172ce15a6ffa2430b7814224cb3a0b57c0570913

---
+++ chrome/browser/feature_engagement/feature_tracker.h
@@ -0,0 +1,58 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_FEATURE_ENGAGEMENT_FEATURE_TRACKER_H_
+#define CHROME_BROWSER_FEATURE_ENGAGEMENT_FEATURE_TRACKER_H_
+
+#include "base/scoped_observer.h"
+#include "chrome/browser/feature_engagement/session_duration_updater.h"
+#include "components/keyed_service/core/keyed_service.h"
+
+namespace feature_engagement {
+
+class Tracker;
+
+// The FeatureTracker provides a backend for displaying in-product help for the
+// various features.
+class FeatureTracker : public SessionDurationUpdater::Observer,
+                       public KeyedService {
+ public:
+  FeatureTracker(Tracker* tracker,
+                 SessionDurationUpdater* session_duration_updater);
+
+ protected:
+  ~FeatureTracker() override;
+
+  // Returns whether the active session time of a user has elapsed more than the
+  // required active sessiom time for the feature.
+  virtual bool HasEnoughSessionTimeElapsed(const int total_session_time) = 0;
+  // Alerts the feature tracker that the session time is up.
+  virtual void OnSessionTimeMet() = 0;
+  virtual Tracker* GetFeatureTracker();
+
+ private:
+  // Adds the SessionDurationUpdater observer.
+  void AddSessionDurationObserver();
+  // Removes the SessionDurationUpdater observer.
+  void RemoveSessionDurationObserver();
+  // SessionDurationUpdater::Observer:
+  void OnSessionEnded(const int total_session_time) override;
+
+  // Singleton instance of a KeyedService.
+  Tracker* const tracker_;
+
+  // Observes the DesktopSessionDurationTracker and notifies when a desktop
+  // session starts and ends.
+  ScopedObserver<SessionDurationUpdater, SessionDurationUpdater::Observer>
+      session_duration_observer_;
+
+  // Singleton instance of KeyedService.
+  SessionDurationUpdater* session_duration_updater_;
+
+  DISALLOW_COPY_AND_ASSIGN(FeatureTracker);
+};
+
+}  // namespace feature_engagement
+
+#endif  // CHROME_BROWSER_FEATURE_ENGAGEMENT_FEATURE_TRACKER_H_
