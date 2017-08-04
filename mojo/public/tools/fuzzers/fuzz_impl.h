6b24369827f6aff3dcc684dd26880cc59d12e543
Tim Becker <tjbecker@google.com>
2017-08-04 13:16:49 -0700

    Message Dumper / Corpus Generator for Mojo Fuzzer
    Adds a standalone executable which will dump raw mojo messages to the
    provided directory, which is used to generate a corpus for the
    mojo_parse_message_fuzzer.
    
    R=ochang@chromium.org
    
    Bug:
    Change-Id: Id94348b6fbbdf1e1222ed6f46d1ec062eb6c7409

---
+++ mojo/public/tools/fuzzers/fuzz_impl.h
@@ -0,0 +1,28 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/public/tools/fuzzers/fuzz.mojom.h"
+
+class FuzzImpl : public fuzz::mojom::FuzzInterface {
+ public:
+  explicit FuzzImpl(fuzz::mojom::FuzzInterfaceRequest request);
+  ~FuzzImpl() override;
+
+  void FuzzBasic() override;
+  void FuzzBasicResp(FuzzBasicRespCallback callback) override;
+  void FuzzBasicSyncResp(FuzzBasicSyncRespCallback callback) override;
+  void FuzzArgs(fuzz::mojom::FuzzStructPtr a,
+                fuzz::mojom::FuzzStructPtr b) override;
+
+  void FuzzArgsResp(fuzz::mojom::FuzzStructPtr a,
+                    fuzz::mojom::FuzzStructPtr b,
+                    FuzzArgsRespCallback callback) override;
+  void FuzzArgsSyncResp(fuzz::mojom::FuzzStructPtr a,
+                        fuzz::mojom::FuzzStructPtr b,
+                        FuzzArgsSyncRespCallback callback) override;
+
+  /* Expose the binding to the fuzz harness. */
+  mojo::Binding<FuzzInterface> binding_;
+};
