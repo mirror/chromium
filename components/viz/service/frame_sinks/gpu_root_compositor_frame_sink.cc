90d4bdfff2efcc3288fbcc9fe6851853405dfe00
Gary Klassen <gklassen@chromium.org>
2017-08-04 09:45:43 -0400

    Add HitTestRegionList to SubmitCompositorFrame
    We need a way to send HitTestRegionList data to the HitTestAggregator
    with each call to SubmitCompositorFrame.
    
    This change adds HitTestRegionList as an optional argument to
    SubmitCompositorFrame.  The HitTestRegionList data is currently
    optional and empty - this will be added in future CLs.
    
    Bug: 732399
    Change-Id: I291bf67b36a3e3addf59a28fb64b58f2dc5527ce

--- components/viz/service/frame_sinks/gpu_root_compositor_frame_sink.cc
+++ components/viz/service/frame_sinks/gpu_root_compositor_frame_sink.cc
@@ -79,7 +79,9 @@ void GpuRootCompositorFrameSink::SetNeedsBeginFrame(bool needs_begin_frame) {
 
 void GpuRootCompositorFrameSink::SubmitCompositorFrame(
     const LocalSurfaceId& local_surface_id,
-    cc::CompositorFrame frame) {
+    cc::CompositorFrame frame,
+    mojom::HitTestRegionListPtr hit_test_region_list) {
+  // TODO(gklassen): Route hit-test data to the appropriate HitTestAggregator.
   if (!support_->SubmitCompositorFrame(local_surface_id, std::move(frame))) {
     compositor_frame_sink_binding_.Close();
     OnClientConnectionLost();
