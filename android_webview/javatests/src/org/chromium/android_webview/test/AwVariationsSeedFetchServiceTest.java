7e9fd5940e5614a49ed275e6ec78ed5a07a9e045
Yiyun Yao <yiyuny@google.com>
2017-08-05 10:57:20 -0700

    Add AwVariationsConfigurationService and WebView logic interact with it
    The CL add the AwVariationsConfigurationService(VCS) which the WebViews on the
    system will bind to and ask for Finch seed. When the seed request comes, it will
    first check if the seed data in the service directory is expired(service seed).
    If the service seed is not expired, the VCS will return the seed immediately,
    otherwise, the service will schedule a job to the AwVariationsSeedFetchService.
    When the new seed is downloaded, the VCS will be notified and send back the
    seed to the WebViews. The seed data on the WebView side will be stored in the
    host app's directory under 'WebView_Variations' and like the service side, seed
    data and its related header fields will be stored separately. Because the
    functionality is just a prototype, the code will be landed behind a flag
    'enable-webview-finch'.
    
    Bug:733857
    Change-Id: Iafba941f32522bf1e54dc519ac8343f4938e9432

--- android_webview/javatests/src/org/chromium/android_webview/test/AwVariationsSeedFetchServiceTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwVariationsSeedFetchServiceTest.java
@@ -6,8 +6,10 @@ package org.chromium.android_webview.test;
 
 import android.annotation.TargetApi;
 import android.os.Build;
+import android.os.ParcelFileDescriptor;
 import android.support.test.filters.MediumTest;
 import android.test.InstrumentationTestCase;
+import android.util.Base64;
 
 import org.chromium.android_webview.AwVariationsSeedFetchService;
 import org.chromium.android_webview.AwVariationsSeedFetchService.SeedPreference;
@@ -15,13 +17,14 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.components.variations.firstrun.VariationsSeedFetcher.SeedInfo;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 
 /**
- * Instrumentation tests for AwVariationsSeedFetchService.
+ * Instrumentation tests for AwVariationsSeedFetchService on Android L and above.
  */
-@MinAndroidSdkLevel(Build.VERSION_CODES.LOLLIPOP) // Android versions under L don't have JobService.
+@MinAndroidSdkLevel(Build.VERSION_CODES.LOLLIPOP)
 @TargetApi(Build.VERSION_CODES.LOLLIPOP) // JobService requires API level 21.
 public class AwVariationsSeedFetchServiceTest extends InstrumentationTestCase {
     protected void setUp() throws Exception {
@@ -46,13 +49,18 @@ public class AwVariationsSeedFetchServiceTest extends InstrumentationTestCase {
         seedInfo.isGzipCompressed = true;
         AwVariationsSeedFetchService.storeVariationsSeed(seedInfo);
 
-        byte[] seedData = AwVariationsSeedFetchService.getVariationsSeedData();
-        assertTrue(Arrays.equals(seedData, seedInfo.seedData));
+        ParcelFileDescriptor fd =
+                AwVariationsSeedFetchService.getVariationsSeedDataFileDescriptor();
+        BufferedReader reader = new BufferedReader(new FileReader(fd.getFileDescriptor()));
+        String encodedSeedData = reader.readLine();
+        assertEquals(Base64.encodeToString(seedInfo.seedData, Base64.NO_WRAP), encodedSeedData);
 
         SeedPreference seedPreference = AwVariationsSeedFetchService.getVariationsSeedPreference();
-        assertEquals(seedPreference.signature, seedInfo.signature);
-        assertEquals(seedPreference.country, seedInfo.country);
-        assertEquals(seedPreference.date, seedInfo.date);
-        assertEquals(seedPreference.isGzipCompressed, seedInfo.isGzipCompressed);
+        assertEquals(seedInfo.signature, seedPreference.signature);
+        assertEquals(seedInfo.country, seedPreference.country);
+        assertEquals(seedInfo.date, seedPreference.date);
+        assertEquals(seedInfo.isGzipCompressed, seedPreference.isGzipCompressed);
+
+        AwVariationsSeedFetchService.closeStream(reader);
     }
 }
