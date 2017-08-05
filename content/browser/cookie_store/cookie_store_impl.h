d426fe5ec08af3e73b8690c2a640f7a746fc92ea
Victor Costan <pwnall@chromium.org>
2017-08-04 18:50:42 -0700

    C++ skeleton for the Async Cookies API.
    Bug:
    Change-Id: I4328d9b92422d6d10f04ebe72e6461a71969a857

---
+++ content/browser/cookie_store/cookie_store_impl.h
@@ -0,0 +1,30 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/macros.h"
+#include "mojo/public/cpp/bindings/binding.h"
+#include "third_party/WebKit/public/platform/modules/cookie_store/cookie_store.mojom.h"
+
+namespace content {
+
+class CONTENT_EXPORT CookieStoreImpl
+    : NON_EXPORTED_BASE(public blink::mojom::CookieStore) {
+ public:
+  void GetAll(const GURL& url,
+              const GURL& first_party_for_cookies,
+              const std::string& name,
+              blink::mojom::CookieStoreGetOptionsPtr options,
+              blink::mojom::CookieStore::GetAllCallback callback) override;
+
+  void Set(const GURL& url,
+           const GURL& first_party_for_cookies,
+           const std::string& name,
+           const std::string& value,
+           blink::mojom::CookieStoreSetOptionsPtr options) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(CookieStoreImpl);
+};
+
+}  // namespace content
