5877b96346c9ee9b554ef8b2771682441c5d33b3
Robert Hogan <robhogan@gmail.com>
2017-08-06 12:15:49 +0100

    List markers should only avoid floats on their own line.
    We used to update the offset of list markers after we had laid out the line
    but this resulted in the list marker avoiding floats that came
    after it in layout (https://codereview.chromium.org/2553793003).
    
    PositionListMarker() always happens after a layout of the list item so it's reasonable
    to always update the offset when laying one out. We can do better than this
    though and only update the offset when we know there's a float to avoid, so do
    that instead. The layout of a list marker consists simply of finding its offset
    and updating its margins so it's cheap enough to treat a layout as an update to
    the offset.
    
    Bug: 726583, 753099
    Change-Id: I5aca82ae654f1ddece448fc89563f90b801d1110

--- third_party/WebKit/Source/core/layout/LayoutListMarker.cpp
+++ third_party/WebKit/Source/core/layout/LayoutListMarker.cpp
@@ -135,10 +135,15 @@ void LayoutListMarker::Paint(const PaintInfo& paint_info,
   ListMarkerPainter(*this).Paint(paint_info, paint_offset);
 }
 
-void LayoutListMarker::UpdateLayout() {
-  DCHECK(NeedsLayout());
-  LayoutAnalyzer::Scope analyzer(*this);
+void LayoutListMarker::SetLocationAndUpdateOverflowControlsIfNeeded(
+    const LayoutPoint& location) {
+  LayoutUnit old_logical_top = LogicalTop();
+  LayoutBox::SetLocationAndUpdateOverflowControlsIfNeeded(location);
+  if (LogicalTop() != old_logical_top)
+    UpdateLineOffset();
+}
 
+void LayoutListMarker::UpdateLineOffset() {
   LayoutUnit block_offset = LogicalTop();
   for (LayoutBox* o = ParentBox(); o && o != ListItem(); o = o->ParentBox()) {
     block_offset += o->LogicalTop();
@@ -150,6 +155,13 @@ void LayoutListMarker::UpdateLayout() {
     line_offset_ = ListItem()->LogicalRightOffsetForLine(
         block_offset, kDoNotIndentText, LayoutUnit());
   }
+}
+
+void LayoutListMarker::UpdateLayout() {
+  DCHECK(NeedsLayout());
+  LayoutAnalyzer::Scope analyzer(*this);
+
+  UpdateLineOffset();
   if (IsImage()) {
     UpdateMarginsAndContent();
     LayoutSize image_size(ImageBulletSize());
