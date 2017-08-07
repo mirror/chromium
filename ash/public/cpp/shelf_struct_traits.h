ee57428347193f1636cff36cdd4514394087e0c1
Michael Wasserman <msw@chromium.org>
2017-08-07 14:08:14 -0700

    mash: Add shelf context menu support.
    Chrome serializes its shelf item context menus for Ash.
    Ash displays the menus and reports invocations to Chrome.
    
    Add an ash-side ShelfContextMenu class that supports:
    -Local menu items to modify shelf and wallpaper settings.
    -Proxies Chrome's menu items via mojo structs/interfaces.
    TODO: Explicitly check if preferences are modifiable.
    
    Extend mojom::ShelfItemDelegate::ExecuteCommand and MenuItem:
    -Add a flag to denote the command's origin (context/app menu).
    -Allow 64-bit int command and radio-group ids; null images.
    
    Add ash::ShelfItemDelegate context menu handling:
    -Add ShelfAction::SHELF_ACTION_SHOW_CONTEXT_MENU.
    -GetContextMenu() is overridden by Chrome's subclasses.
    -WillShowContextMenu() handles item right-click selection.
    -ExecuteContextMenuCommand() handles context menu invocation.
    
    Wire up chrome's launcher item subclass context menus.
    Make minor launcher context menu class simplifications.
    Add a ShelfWindowWatcher context menu item for closing.
    
    Add SessionController::is_running_in_app_mode and Chrome init.
    (Ash stifles menus if Chrome is running in a kiosk/app mode)
    
    Convert ShelfButtonPressedMetricTracker from Button to View.
    (to support AfterItemSelected via ShowContextMenuForView)
    
    Remove mus, ash_shell, test, and desktop context menu classes.
    Remove ash::ShellDelegate::CreateContextMenu.
    Remove ash::ShelfAlignmentMenu.
    Add unit tests, flip TestWallpaperDelegate::CanOpenSetWallpaperPage.
    
    Bug: 640693
    Change-Id: I3afb93095b4042dca57d16844c5b05308f06a384

--- ash/public/cpp/shelf_struct_traits.h
+++ ash/public/cpp/shelf_struct_traits.h
@@ -28,6 +28,8 @@ struct EnumTraits<ash::mojom::ShelfAction, ash::ShelfAction> {
         return ash::mojom::ShelfAction::WINDOW_MINIMIZED;
       case ash::SHELF_ACTION_APP_LIST_SHOWN:
         return ash::mojom::ShelfAction::APP_LIST_SHOWN;
+      case ash::SHELF_ACTION_SHOW_CONTEXT_MENU:
+        return ash::mojom::ShelfAction::SHOW_CONTEXT_MENU;
     }
     NOTREACHED();
     return ash::mojom::ShelfAction::NONE;
@@ -50,78 +52,8 @@ struct EnumTraits<ash::mojom::ShelfAction, ash::ShelfAction> {
       case ash::mojom::ShelfAction::APP_LIST_SHOWN:
         *out = ash::SHELF_ACTION_APP_LIST_SHOWN;
         return true;
-    }
-    NOTREACHED();
-    return false;
-  }
-};
-
-template <>
-struct EnumTraits<ash::mojom::ShelfAlignment, ash::ShelfAlignment> {
-  static ash::mojom::ShelfAlignment ToMojom(ash::ShelfAlignment input) {
-    switch (input) {
-      case ash::SHELF_ALIGNMENT_BOTTOM:
-        return ash::mojom::ShelfAlignment::BOTTOM;
-      case ash::SHELF_ALIGNMENT_LEFT:
-        return ash::mojom::ShelfAlignment::LEFT;
-      case ash::SHELF_ALIGNMENT_RIGHT:
-        return ash::mojom::ShelfAlignment::RIGHT;
-      case ash::SHELF_ALIGNMENT_BOTTOM_LOCKED:
-        return ash::mojom::ShelfAlignment::BOTTOM_LOCKED;
-    }
-    NOTREACHED();
-    return ash::mojom::ShelfAlignment::BOTTOM;
-  }
-
-  static bool FromMojom(ash::mojom::ShelfAlignment input,
-                        ash::ShelfAlignment* out) {
-    switch (input) {
-      case ash::mojom::ShelfAlignment::BOTTOM:
-        *out = ash::SHELF_ALIGNMENT_BOTTOM;
-        return true;
-      case ash::mojom::ShelfAlignment::LEFT:
-        *out = ash::SHELF_ALIGNMENT_LEFT;
-        return true;
-      case ash::mojom::ShelfAlignment::RIGHT:
-        *out = ash::SHELF_ALIGNMENT_RIGHT;
-        return true;
-      case ash::mojom::ShelfAlignment::BOTTOM_LOCKED:
-        *out = ash::SHELF_ALIGNMENT_BOTTOM_LOCKED;
-        return true;
-    }
-    NOTREACHED();
-    return false;
-  }
-};
-
-template <>
-struct EnumTraits<ash::mojom::ShelfAutoHideBehavior,
-                  ash::ShelfAutoHideBehavior> {
-  static ash::mojom::ShelfAutoHideBehavior ToMojom(
-      ash::ShelfAutoHideBehavior input) {
-    switch (input) {
-      case ash::SHELF_AUTO_HIDE_BEHAVIOR_ALWAYS:
-        return ash::mojom::ShelfAutoHideBehavior::ALWAYS;
-      case ash::SHELF_AUTO_HIDE_BEHAVIOR_NEVER:
-        return ash::mojom::ShelfAutoHideBehavior::NEVER;
-      case ash::SHELF_AUTO_HIDE_ALWAYS_HIDDEN:
-        return ash::mojom::ShelfAutoHideBehavior::HIDDEN;
-    }
-    NOTREACHED();
-    return ash::mojom::ShelfAutoHideBehavior::NEVER;
-  }
-
-  static bool FromMojom(ash::mojom::ShelfAutoHideBehavior input,
-                        ash::ShelfAutoHideBehavior* out) {
-    switch (input) {
-      case ash::mojom::ShelfAutoHideBehavior::ALWAYS:
-        *out = ash::SHELF_AUTO_HIDE_BEHAVIOR_ALWAYS;
-        return true;
-      case ash::mojom::ShelfAutoHideBehavior::NEVER:
-        *out = ash::SHELF_AUTO_HIDE_BEHAVIOR_NEVER;
-        return true;
-      case ash::mojom::ShelfAutoHideBehavior::HIDDEN:
-        *out = ash::SHELF_AUTO_HIDE_ALWAYS_HIDDEN;
+      case ash::mojom::ShelfAction::SHOW_CONTEXT_MENU:
+        *out = ash::SHELF_ACTION_SHOW_CONTEXT_MENU;
         return true;
     }
     NOTREACHED();
