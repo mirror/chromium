caa3ac37a109af11191f52c4911492a25f408686
Sammie Quon <sammiequon@google.com>
2017-08-07 11:32:26 -0700

    stylus: Disable tools on entering lock.
    Disable active tool on entering lock screen. Users will have to choose again when they re-enter. Also --ash-force-stylus should take priority of has seen stylus.
    
    Test: ash_unittests "PaletteTrayTest*" with and without --mash
    Bug: 751221
    Change-Id: I483bbae620cb8facb690b9059d95b62765538201

--- ash/system/palette/palette_utils.cc
+++ ash/system/palette/palette_utils.cc
@@ -19,13 +19,25 @@
 namespace ash {
 namespace palette_utils {
 
+namespace {
+// If true the device performs as if it is hardware reports that it is stylus
+// capable.
+bool g_stylus_capable_for_testing = false;
+}  // namespace
+
+bool HasForcedStylusInput() {
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kAshForceEnableStylusTools);
+}
+
 bool HasStylusInput() {
   // Allow the user to force enable or disable by passing a switch. If both are
   // present, enabling takes precedence over disabling.
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kAshForceEnableStylusTools)) {
+  if (HasForcedStylusInput())
     return true;
-  }
+
+  if (g_stylus_capable_for_testing)
+    return true;
 
   // Check to see if the hardware reports it is stylus capable.
   for (const ui::TouchscreenDevice& device :
@@ -66,6 +78,10 @@ bool PaletteContainsPointInScreen(const gfx::Point& point) {
 bool HasInternalStylus() {
   return base::CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kHasInternalStylus);
+}
+
+void SetIsStylusCapableForTesting() {
+  g_stylus_capable_for_testing = true;
 }
 
 }  // namespace palette_utils
