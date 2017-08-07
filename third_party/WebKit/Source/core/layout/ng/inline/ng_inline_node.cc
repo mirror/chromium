a4bfd354ae03b0e5ae172b53c0261367a3129957
Koji Ishii <kojii@chromium.org>
2017-08-08 01:56:15 +0900

    first-line
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: If83b594d57d3fd56f748096f506aba059d4cfffa

--- third_party/WebKit/Source/core/layout/ng/inline/ng_inline_node.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_inline_node.cc
@@ -351,6 +351,18 @@ NGInlineItemRange NGInlineNode::Items(unsigned start, unsigned end) {
   return NGInlineItemRange(&MutableData().items_, start, end);
 }
 
+const Vector<NGInlineItem>* NGInlineNode::FirstLineItems() const {
+  return Data().first_line_items_.get();
+}
+
+const Vector<NGInlineItem>& NGInlineNode::Items(bool is_first_line) const {
+  if (!is_first_line)
+    return Items();
+  if (const Vector<NGInlineItem>* first_line_items = FirstLineItems())
+    return *first_line_items;
+  return Items();
+}
+
 void NGInlineNode::InvalidatePrepareLayout() {
   ToLayoutNGBlockFlow(GetLayoutBlockFlow())->ResetNGInlineNodeData();
   MutableData().text_content_ = String();
@@ -444,13 +456,49 @@ void NGInlineNode::SegmentText() {
 
 void NGInlineNode::ShapeText() {
   // TODO(eae): Add support for shaping latin-1 text?
-  MutableData().text_content_.Ensure16Bit();
-  const String& text_content = Data().text_content_;
+  NGInlineNodeData& data = MutableData();
+  data.text_content_.Ensure16Bit();
+  const String& text_content = data.text_content_;
+  Vector<NGInlineItem>& items = data.items_;
+  ShapeText(text_content, &items);
 
+  DCHECK(!data.first_line_items_);
+  LayoutObject* layout_object = GetLayoutObject();
+  if (!layout_object->GetDocument().GetStyleEngine().UsesFirstLineRules())
+    return;
+  DLOG(INFO) << "This document has first line rules";
+
+  const ComputedStyle* block_style = layout_object->Style();
+  const ComputedStyle* first_line_style = layout_object->FirstLineStyle();
+  if (block_style == first_line_style)
+    return;
+  DLOG(INFO) << "This node has first line style";
+
+  auto first_line_items = WTF::MakeUnique<Vector<NGInlineItem>>();
+  first_line_items->AppendVector(items);
+  for (auto& item : *first_line_items) {
+    if (item.style_) {
+      DCHECK(item.layout_object_);
+      item.style_ = item.layout_object_->FirstLineStyle();
+    }
+  }
+
+  const Font& font = block_style->GetFont();
+  const Font& first_line_font = first_line_style->GetFont();
+  if (&font != &first_line_font && font != first_line_font) {
+    DLOG(INFO) << "This node use different font for first line";
+    ShapeText(text_content, first_line_items.get());
+  }
+
+  data.first_line_items_ = std::move(first_line_items);
+}
+
+void NGInlineNode::ShapeText(const String& text_content,
+                             Vector<NGInlineItem>* items) {
   // Shape each item with the full context of the entire node.
   HarfBuzzShaper shaper(text_content.Characters16(), text_content.length());
   ShapeResultSpacing<String> spacing(text_content);
-  for (auto& item : MutableData().items_) {
+  for (auto& item : *items) {
     if (item.Type() != NGInlineItem::kText)
       continue;
 
@@ -622,6 +670,7 @@ void NGInlineNode::CopyFragmentDataToLayoutBox(
         baseline - line_metrics.ascent, baseline + line_metrics.descent,
         line_top, baseline + max_with_leading.descent);
 
+    line_info.SetFirstLine(false);
     bidi_runs.DeleteRuns();
     positions_for_bidi_runs.clear();
     positions.clear();
