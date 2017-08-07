4699d4c484a70a3f14367d5cc48f3f902d311621
Vadym Doroshenko <dvadym@chromium.org>
2017-08-07 14:29:26 +0200

    [Bling Password Manager] Send request to browser on input onFocus
    Not Finished
    
    Bug:
    Change-Id: Ie47f6335c6c7681dadfef7b53276007527e400c4

--- ios/chrome/browser/passwords/password_controller.mm
+++ ios/chrome/browser/passwords/password_controller.mm
@@ -104,6 +104,8 @@ enum class PasswordInfoBarType { SAVE, UPDATE };
 - (void)findPasswordFormsWithCompletionHandler:
     (void (^)(const std::vector<autofill::PasswordForm>&))completionHandler;
 
+- (void)findPasswordFormsAndSendThemToBrowser;
+
 // Finds the currently submitted password form and calls |completionHandler|
 // with the populated data structure. |found| is YES if the current form was
 // found successfully, NO otherwise. |completionHandler| cannot be nil.
@@ -268,6 +270,8 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
 
   // Bridge to observe WebState from Objective-C.
   std::unique_ptr<web::WebStateObserverBridge> webStateObserverBridge_;
+
+  BOOL sent_request_to_store_;
 }
 
 @synthesize isWebStateDestroyed = isWebStateDestroyed_;
@@ -310,6 +314,7 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
             instanceOfClass:[JsPasswordManager class]]);
     webStateObserverBridge_.reset(
         new web::WebStateObserverBridge(webState, self));
+    sent_request_to_store_ = NO;
   }
   return self;
 }
@@ -343,6 +348,7 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
   passwordManagerDriver_.reset();
   passwordManager_.reset();
   passwordManagerClient_.reset();
+  sent_request_to_store_ = NO;
 }
 
 - (void)findAndFillPasswordForms:(NSString*)username
@@ -373,6 +379,7 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
 - (void)webState:(web::WebState*)webState didLoadPageWithSuccess:(BOOL)success {
   // Clear per-page state.
   formData_.reset();
+  sent_request_to_store_ = NO;
 
   // Retrieve the identity of the page. In case the page might be malicous,
   // returns early.
@@ -393,13 +400,7 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
         didFinishPasswordFormExtraction:std::vector<autofill::PasswordForm>()];
   }
 
-  // Read all password forms from the page and send them to the password
-  // manager.
-  __weak PasswordController* weakSelf = self;
-  [self findPasswordFormsWithCompletionHandler:^(
-            const std::vector<autofill::PasswordForm>& forms) {
-    [weakSelf didFinishPasswordFormExtraction:forms];
-  }];
+  [self findPasswordFormsAndSendThemToBrowser];
 }
 
 - (void)webState:(web::WebState*)webState
@@ -445,6 +446,16 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
     std::vector<autofill::PasswordForm> forms;
     [weakSelf getPasswordForms:&forms fromFormsJSON:jsonString pageURL:pageURL];
     completionHandler(forms);
+  }];
+}
+
+- (void)findPasswordFormsAndSendThemToBrowser {
+  // Read all password forms from the page and send them to the password
+  // manager.
+  __weak PasswordController* weakSelf = self;
+  [self findPasswordFormsWithCompletionHandler:^(
+            const std::vector<autofill::PasswordForm>& forms) {
+    [weakSelf didFinishPasswordFormExtraction:forms];
   }];
 }
 
@@ -564,6 +575,7 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
       }
     }
 
+    sent_request_to_store_ = YES;
     // Invoke the password manager callback to autofill password forms
     // on the loaded page.
     passwordManager_->OnPasswordFormsParsed(passwordManagerDriver_.get(),
@@ -601,6 +613,11 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
                                   webState:(web::WebState*)webState
                          completionHandler:
                              (SuggestionsAvailableCompletion)completion {
+  if (!sent_request_to_store_ && [type isEqual:@"focus"]) {
+    [self findPasswordFormsAndSendThemToBrowser];
+    completion(NO);
+    return;
+  }
   if (!formData_ || !GetPageURLAndCheckTrustLevel(webState, nullptr)) {
     completion(NO);
     return;
--- ios/chrome/browser/passwords/password_controller_unittest.mm
+++ ios/chrome/browser/passwords/password_controller_unittest.mm
@@ -48,6 +48,7 @@
 using autofill::PasswordForm;
 using autofill::PasswordFormFillData;
 using testing::Return;
+using testing::_;
 
 namespace {
 
@@ -242,7 +243,7 @@ class PasswordControllerTest : public web::WebTestWithWebState {
   // PasswordController for testing.
   PasswordController* passwordController_;
 
-  scoped_refptr<password_manager::PasswordStore> store_;
+  scoped_refptr<password_manager::MockPasswordStore> store_;
 };
 
 struct PasswordFormTestData {
@@ -1364,4 +1365,50 @@ TEST_F(PasswordControllerTest, HTTPSPassword) {
       web_state()->GetNavigationManager()->GetLastCommittedItem()->GetSSL();
   EXPECT_FALSE(ssl_status.content_status &
                web::SSLStatus::DISPLAYED_PASSWORD_FIELD_ON_HTTP);
+}
+
+//
+TEST_F(PasswordControllerTest, FillingDynamicallyAddedFormsOnFocus) {
+  LoadHtml(kHtmlWithoutPasswordForm);
+
+  NSString* kAddFormDynamicallyScript =
+      @"var dynamicForm = document.createElement('form');"
+       "dynamicForm.setAttribute('name', 'dynamic_form');"
+       "var inputUsername = document.createElement('input');"
+       "inputUsername.setAttribute('type', 'text');"
+       "inputUsername.setAttribute('id', 'username');"
+       "var inputPassword = document.createElement('input');"
+       "inputPassword.setAttribute('type', 'password');"
+       "inputPassword.setAttribute('id', 'password');"
+       "var submitButton = document.createElement('input');"
+       "submitButton.setAttribute('type', 'submit');"
+       "submitButton.setAttribute('value', 'Submit');"
+       "dynamicForm.appendChild(inputUsername);"
+       "dynamicForm.appendChild(inputPassword);"
+       "dynamicForm.appendChild(submitButton);"
+       "document.body.appendChild(dynamicForm);";
+
+  ExecuteJavaScript(kAddFormDynamicallyScript);
+  WaitForBackgroundTasks();
+
+  NSString* kSetUsernameInFocusScript =
+      @"document.getElementById('username').focus();";
+
+  bool block_was_called = false;
+  ON_CALL(*store_, GetLogins(_, _))
+      .WillByDefault(testing::Invoke(
+          [&block_was_called](
+              const password_manager::PasswordStore::FormDigest&,
+              password_manager::PasswordStoreConsumer*) {
+            block_was_called = true;
+          }));
+  bool* p_block_was_called = &block_was_called;
+  EXPECT_CALL(*store_, GetLogins(_, _));
+
+  ExecuteJavaScript(kSetUsernameInFocusScript);
+  WaitForBackgroundTasks();
+
+  base::test::ios::WaitUntilCondition(^bool() {
+    return *p_block_was_called;
+  });
 }
