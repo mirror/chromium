05a8b10403d7365a06349b28699cddbd96eb1acb
Alex Newcomer <newcomer@chromium.org>
2017-08-07 09:42:09 -0700

    cros:Remove launcher movement with side shelf
    When the shelf is oriented on the side, the launcher should not move or
    be draggable.
    
    Disabled both show and hide animations when side shelf is detected.
    Disabled touch drag when side shelf is detected.
    
    Bug:750664
    Change-Id: Ic48079aa15e1b7d1037b0a9a06b0cb0b55d6ccb6

--- ash/app_list/app_list_presenter_delegate_unittest.cc
+++ ash/app_list/app_list_presenter_delegate_unittest.cc
@@ -252,6 +252,29 @@ TEST_F(AppListPresenterDelegateTest, TinyDisplay) {
   EXPECT_GE(app_list_view_top, kMinimalAppListMargin);
 }
 
+// Tests that the app list is not draggable in side shelf alignment.
+TEST_F(FullscreenAppListPresenterDelegateTest, SideShelfAlignmentDragDisabled) {
+  SetShelfAlignment(ShelfAlignment::SHELF_ALIGNMENT_LEFT);
+  app_list_presenter_impl()->Show(GetPrimaryDisplayId());
+  const app_list::AppListView* app_list = app_list_presenter_impl()->GetView();
+  EXPECT_TRUE(app_list->is_fullscreen());
+  EXPECT_EQ(app_list::AppListView::FULLSCREEN_ALL_APPS,
+            app_list->app_list_state());
+
+  // Drag the widget across the screen over an arbitrary 100Ms, this would
+  // normally result in the app list transitioning to PEEKING but will now
+  // result in no state change.
+  ui::test::EventGenerator& generator = GetEventGenerator();
+  generator.GestureScrollSequence(gfx::Point(10, 10), gfx::Point(10, 900),
+                                  base::TimeDelta::FromMilliseconds(100), 10);
+  EXPECT_EQ(app_list::AppListView::FULLSCREEN_ALL_APPS,
+            app_list->app_list_state());
+
+  // Tap the app list body. This should still close the app list.
+  generator.GestureTapAt(GetPointOutsideSearchbox());
+  EXPECT_EQ(app_list::AppListView::CLOSED, app_list->app_list_state());
+}
+
 // Tests that the app list initializes in fullscreen with side shelf alignment
 // and that the state transitions via text input act properly.
 TEST_F(FullscreenAppListPresenterDelegateTest,
