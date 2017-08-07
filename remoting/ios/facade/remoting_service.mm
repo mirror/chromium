2ece35575762978958476e19e4c48be18a011200
Yuwei Huang <yuweih@chromium.org>
2017-08-07 12:14:05 -0700

    [CRD iOS] Cancel old hostlist fetch when switching accounts
    Previously when a new fetch request comes while the old request is still
    in progress, we ignore the second request. This is not ideal if the second
    request comes after the user switches account, in this case we may show
    the host list of the previous account for the current account.
    
    This CL fixes that by making HostListFetcher canceling previous fetch
    request when the user switches account.
    
    Change-Id: Id339784a147d223d3000f8220a0d79291eb27e67

--- remoting/ios/facade/remoting_service.mm
+++ remoting/ios/facade/remoting_service.mm
@@ -42,7 +42,7 @@ NSString* const kUserInfo = @"kUserInfo";
 
 @interface RemotingService ()<RemotingAuthenticationDelegate> {
   id<RemotingAuthentication> _authentication;
-  remoting::HostListFetcher* _hostListFetcher;
+  std::unique_ptr<remoting::HostListFetcher> _hostListFetcher;
   remoting::IosClientRuntimeDelegate* _clientRuntimeDelegate;
 }
 @end
@@ -66,7 +66,6 @@ NSString* const kUserInfo = @"kUserInfo";
   self = [super init];
   if (self) {
     _hosts = nil;
-    _hostListFetcher = nil;
     // TODO(yuweih): Maybe better to just cancel the previous request.
     _hostListState = HostListStateNotFetched;
     // TODO(nicholss): This might need a pointer back to the service.
@@ -85,19 +84,26 @@ NSString* const kUserInfo = @"kUserInfo";
   }
   [self setHostListState:HostListStateFetching];
   if (!_hostListFetcher) {
-    _hostListFetcher = new remoting::HostListFetcher(
-        remoting::ChromotingClientRuntime::GetInstance()->url_requester());
+    _hostListFetcher.reset(new remoting::HostListFetcher(
+        remoting::ChromotingClientRuntime::GetInstance()->url_requester()));
   }
   _hostListFetcher->RetrieveHostlist(
       base::SysNSStringToUTF8(accessToken),
       base::BindBlockArc(^(int responseCode,
                            const std::vector<remoting::HostInfo>& hostlist) {
+        if (responseCode != net::HTTP_OK) {
+          LOG(WARNING) << "Failed to fetch host list. Response code: "
+                       << responseCode;
+          if (responseCode == net::HTTP_UNAUTHORIZED) {
+            [[RemotingService instance].authentication logout];
+          }
+          // TODO(nicholss): There are more |responseCode|s that we might want
+          // to trigger on, look into that later.
 
-        if (responseCode == net::HTTP_UNAUTHORIZED) {
-          [[RemotingService instance].authentication logout];
+          [self setHostListState:HostListStateNotFetched];
+          _hosts = nil;
+          return;
         }
-        // TODO(nicholss): There are more |responseCode|s that we might want to
-        // trigger on, look into that later.
 
         NSMutableArray<HostInfo*>* hosts =
             [NSMutableArray arrayWithCapacity:hostlist.size()];
@@ -150,6 +156,9 @@ NSString* const kUserInfo = @"kUserInfo";
 
 - (void)userDidUpdate:(UserInfo*)user {
   NSDictionary* userInfo = nil;
+  if (_hostListFetcher) {
+    _hostListFetcher->CancelFetch();
+  }
   [self setHostListState:HostListStateNotFetched];
   if (user) {
     userInfo = [NSDictionary dictionaryWithObject:user forKey:kUserInfo];
