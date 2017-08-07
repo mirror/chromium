c719f3e533e2d0a20e2ac0932db838e381820fdf
Koji Ishii <kojii@chromium.org>
2017-08-07 09:14:17 +0900

    [LayoutNG] Add style/text to NGPhysicalTextFragment for :first-line
    In preparation of :first-line support, this patch:
    
    1. Removes NGInlineNode from NGPhysicalTextFragment to manage object
       life cycle more correctly.
    2. Adds text content String to NGPhysicalTextFragment. The current paint
       patch needs accessing to the text content of fragments.
    3. Adds ComputedStyle to NGPhysicalFragment. This allows paint to find
       the style without knowing if it's on the first line.
       NGPhysicalFragment because inline-blocks can live on the first line.
    4. ItemIndex() is not removed yet since CreateBidiRuns() needs it. This
       should be gone when we switch to NG paint.
    5. To support 3, adds ComputedStyle to all fragment builders.
    6. Adds NGBaseFragmentBuilder since 5 is common to all fragment
       builders.
    7. In doing 6, writing mode and direction is moved to the common
       constructor as per Ian's TODO comment.
    
    Bug: 636993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I52cad8ab27336234ba3a375fa8ced5d29e41ff29

---
+++ third_party/WebKit/Source/core/layout/ng/ng_base_fragment_builder.cc
@@ -0,0 +1,42 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/layout/ng/ng_base_fragment_builder.h"
+
+#include "core/layout/ng/ng_constraint_space.h"
+#include "core/layout/ng/ng_layout_result.h"
+
+namespace blink {
+
+NGBaseFragmentBuilder::NGBaseFragmentBuilder(const ComputedStyle& style,
+                                             NGWritingMode writing_mode,
+                                             TextDirection direction)
+    : style_(&style), writing_mode_(writing_mode), direction_(direction) {
+  DCHECK(&style);
+}
+
+NGBaseFragmentBuilder::NGBaseFragmentBuilder(NGWritingMode writing_mode,
+                                             TextDirection direction)
+    : writing_mode_(writing_mode), direction_(direction) {}
+
+NGBaseFragmentBuilder& NGBaseFragmentBuilder::SetStyle(
+    const ComputedStyle& style) {
+  DCHECK(&style);
+  style_ = &style;
+  return *this;
+}
+
+NGBaseFragmentBuilder& NGBaseFragmentBuilder::SetWritingMode(
+    NGWritingMode writing_mode) {
+  writing_mode_ = writing_mode;
+  return *this;
+}
+
+NGBaseFragmentBuilder& NGBaseFragmentBuilder::SetDirection(
+    TextDirection direction) {
+  direction_ = direction;
+  return *this;
+}
+
+}  // namespace blink
