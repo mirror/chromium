5d050619131eebc47ad0b0514f5a1484aa28e42a
Michael Wasserman <msw@chromium.org>
2017-08-07 10:56:41 -0700

    mash: Move shelf prefs from Chrome to Ash.
    Move some of Chrome's shelf pref names to ash_pref_names.
    Move some shelf pref code to ash/public/cpp (used by Ash and Chrome).
    Remove now unused mojo interface functions and enum types.
    
    Make context menus modify prefs instead of setting values directly.
    (this is really the only code that should set the shelf pref values)
    
    Add SessionController::is_running_in_app_mode and Chrome init.
    (Ash needs to hide the shelf in Chrome's kiosk/app modes)
    
    Move Shelf's WindowTreeHostManager::Observer code to ShelfController.
    (use one observer, not per-display observers; share SetShelf*FromPrefs)
    Construct ShelfController in Shell::Init() to support Shell::Get() use.
    Init/update prefs & PrefRegistar in OnActiveUserPrefServiceChanged.
    Minor refactoring; add some basic unit tests.
    
    Bug: 723085
    Test: No Chrome OS shelf alignment/auto-hide regressions.
    Change-Id: I109ee314093fac10402b50629a95171d88e2e26f

--- ash/shell.cc
+++ ash/shell.cc
@@ -338,6 +338,7 @@ void Shell::RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
 void Shell::RegisterProfilePrefs(PrefRegistrySimple* registry) {
   LogoutButtonTray::RegisterProfilePrefs(registry);
   NightLightController::RegisterProfilePrefs(registry);
+  ShelfController::RegisterProfilePrefs(registry);
 }
 
 views::NonClientFrameView* Shell::CreateDefaultNonClientFrameView(
@@ -623,7 +624,6 @@ Shell::Shell(std::unique_ptr<ShellDelegate> shell_delegate,
       media_controller_(base::MakeUnique<MediaController>()),
       new_window_controller_(base::MakeUnique<NewWindowController>()),
       session_controller_(base::MakeUnique<SessionController>()),
-      shelf_controller_(base::MakeUnique<ShelfController>()),
       shell_delegate_(std::move(shell_delegate)),
       shutdown_controller_(base::MakeUnique<ShutdownController>()),
       system_tray_controller_(base::MakeUnique<SystemTrayController>()),
@@ -851,6 +851,8 @@ Shell::~Shell() {
 
 void Shell::Init(const ShellInitParams& init_params) {
   const Config config = shell_port_->GetAshConfig();
+
+  shelf_controller_ = base::MakeUnique<ShelfController>();
 
   if (NightLightController::IsFeatureEnabled())
     night_light_controller_ = base::MakeUnique<NightLightController>();
