4b21ab1e243249fe822e1d740fc4846b324fd854
Changwan Ryu <changwan@chromium.org>
2017-08-07 10:37:31 -0700

    fix dispatchKeyEvent()
    add logs
    
    Bug:
    Change-Id: I743d3062ed825a49c736b7d99a924eb2fac9c6a2

--- chrome/android/junit/src/org/chromium/chrome/browser/omnibox/AutocompleteEditTextTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/omnibox/AutocompleteEditTextTest.java
@@ -40,7 +40,7 @@ import org.chromium.testing.local.LocalRobolectricTestRunner;
 public class AutocompleteEditTextTest {
     private static final String TAG = "cr_AutocompleteTest";
 
-    private static final boolean DEBUG = false;
+    private static final boolean DEBUG = true;
 
     @Rule
     public Features.Processor mProcessor = new Features.Processor();
