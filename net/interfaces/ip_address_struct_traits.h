b8b902a64aa4c92091fe882756960ea8a8dbd992
Bin Zhao <zhaobin@chromium.org>
2017-08-07 12:31:48 -0700

    [net_interfaces] rename address to address_bytes in ip_address.mojom
    ip_endpoint.mojom has field "IPAddress address". When referencing underlying IPAddress in .js file,
    we use: ip_endpoint.address.address.join('.'); The naming is a little confusing. Change address to
    address_types in ip_address.mojom instead.
    
    Bug:749917
    Change-Id: I4158a867303449c9f21b7e1e1c1792d9c2ccabd6

--- net/interfaces/ip_address_struct_traits.h
+++ net/interfaces/ip_address_struct_traits.h
@@ -12,7 +12,8 @@
 namespace mojo {
 template <>
 struct StructTraits<net::interfaces::IPAddressDataView, net::IPAddress> {
-  static const std::vector<uint8_t> address(const net::IPAddress& ip_address) {
+  static const std::vector<uint8_t> address_bytes(
+      const net::IPAddress& ip_address) {
     // TODO(rch): avoid creating a vector here.
     return ip_address.CopyBytesToVector();
   }
