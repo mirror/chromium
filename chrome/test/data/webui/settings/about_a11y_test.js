27a70d90af7149d9a29220469a96effd559bebd1
Tina Quach <quacht@google.com>
2017-08-08 12:00:40 -0700

    Add A11y Tests for BASIC and ABOUT Settings routes.
    In adding these tests, the framework has been restructured such that
    there is a test fixture per route. In addition, some improvements were
    made to AccessibilityTest to enable the easy addition of tests.
    
    Bug: 737739
    Change-Id: Ie02b3c09d03137d9bb35c4be0dea4787961e0959

---
+++ chrome/test/data/webui/settings/about_a11y_test.js
@@ -0,0 +1,34 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Define accessibility tests for the ABOUT route.
+ */
+
+AccessibilityTest.define({
+  /** @override */
+  name: 'ABOUT',
+
+  /** @override */
+  setup: function() {
+    settings.router.navigateTo(settings.routes.ABOUT);
+    Polymer.dom.flush();
+  },
+
+  /** @override */
+  tests: {
+    'Accessible with No Changes': function() {}
+  },
+  violationFilter: {
+    // TODO(quacht): remove this exception once the color contrast issue is
+    // solved.
+    // http://crbug.com/748608
+    'color-contrast': function(nodeResult) {
+      return nodeResult.element.id == 'prompt';
+    },
+   'aria-valid-attr': function(nodeResult) {
+      return nodeResult.element.hasAttribute('aria-active-attribute');
+    },
+  }
+});
