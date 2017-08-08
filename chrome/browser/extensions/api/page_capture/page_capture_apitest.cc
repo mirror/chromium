2b03e2819a792a3a79b15d5dd4170e3be059b1da
Istiaque Ahmed <lazyboy@chromium.org>
2017-08-07 17:23:41 -0700

    storage::ScopedFile: Demonstrate that BLOCK_ON_SHUTDOWN is necessary.
    Bug:
    Change-Id: Ia4ac6566a28b418b76ac88d5058f7995fd03341f

--- chrome/browser/extensions/api/page_capture/page_capture_apitest.cc
+++ chrome/browser/extensions/api/page_capture/page_capture_apitest.cc
@@ -13,6 +13,11 @@
 #include "content/public/test/test_utils.h"
 #include "extensions/browser/extension_dialog_auto_confirm.h"
 #include "net/dns/mock_host_resolver.h"
+#include "storage/browser/blob/scoped_file.h"
+#include "base/threading/thread_restrictions.h"
+#include "chrome/browser/download/download_prefs.h"
+#include "base/task_scheduler/post_task.h"
+#include "chrome/browser/ui/browser_commands.h"
 
 #if defined(OS_CHROMEOS)
 #include "chromeos/login/login_state.h"
@@ -64,6 +69,89 @@ IN_PROC_BROWSER_TEST_F(ExtensionPageCaptureApiTest, SaveAsMHTML) {
   // the contents.
   base::ThreadRestrictions::ScopedAllowIO allow_io;
   ASSERT_FALSE(base::PathExists(delegate.temp_file_));
+}
+
+
+// Notes: This test has nothing to with page_capture, I've putting
+// it here because I was already changing this file :)
+IN_PROC_BROWSER_TEST_F(ExtensionPageCaptureApiTest,
+                       PRE_TestScopedFileWithoutBlockingShutdown) {
+  LOG(INFO) << "Run PRE_TestScopedFileWithoutBlockingShutdown";
+  Profile* profile = browser()->profile();
+  base::ThreadRestrictions::ScopedAllowIO scoped_allow_io;
+  base::ScopedTempDir temp_download_dir;
+  ASSERT_TRUE(temp_download_dir.CreateUniqueTempDir());
+  base::FilePath download_path = temp_download_dir.GetPath();
+  LOG(INFO) << "Setting download_path to " << download_path.value();
+  DownloadPrefs::FromBrowserContext(profile)->SetDownloadPath(
+      download_path);
+  base::FilePath hello_file(
+      temp_download_dir.GetPath().AppendASCII("hello.txt"));
+  ASSERT_EQ(3,
+            base::WriteFile(hello_file, "cat", 3));
+  // IMPORTANT: Leak the temp dir so that the test teardown
+  // doesn't delete the directory. This will let us test ScopedFile that wraps
+  // hello_file.
+  temp_download_dir.Take();  // Intentional leak. IMPORTANT.
+
+  scoped_refptr<base::SequencedTaskRunner> task_runner =
+      base::CreateSequencedTaskRunnerWithTraits(
+        {base::MayBlock(),
+          base::TaskPriority::BACKGROUND,
+          // THE BUG IS HERE.
+          // Must use TaskShutdownBehavior::BLOCK_SHUTDOWN.
+          base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
+          //base::TaskShutdownBehavior::BLOCK_SHUTDOWN});
+
+  // T1: Run a task that takes a while.
+  auto run_big_task = []() {
+    LOG(INFO) << "BEG run_big_task";
+    int j = 0;
+    std::string s = "test";
+    for (int64_t i = 0; i < (1LL<<27); ++i) {
+      s = "testing";
+      s += (i % 10) > 5 ? 'a' : 'b';
+      j = j + 2;
+    }
+    LOG(INFO) << "End run_big_task";
+  };
+  task_runner->PostTask(
+      FROM_HERE, base::BindOnce(run_big_task));
+  storage::ScopedFile scoped_file(
+      hello_file,
+      storage::ScopedFile::DELETE_ON_SCOPE_OUT,
+      task_runner);
+  // T2: |scoped_file| going out of scope task.
+
+  // |task_runner| is sequenced task runner and we post T1 before T2.
+  // T1 will take longer and since |task_runner| doesn't block on shutdown, the
+  // ScopedFile destruction will not happen.
+  // Therefore |TestScopedFile| will still see "hello_file".
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionPageCaptureApiTest,
+                       TestScopedFileWithoutBlockingShutdown) {
+  LOG(INFO) << "Run ScopedFile";
+  Profile* profile = browser()->profile();
+  base::ThreadRestrictions::ScopedAllowIO scoped_allow_io;
+  base::ScopedTempDir temp_download_dir;
+  base::FilePath download_path =
+      DownloadPrefs::FromBrowserContext(profile)->DownloadPath();
+  LOG(INFO) << "download_path = " << download_path.value();
+  EXPECT_TRUE(temp_download_dir.Set(download_path));
+  base::FilePath hello_file(
+      temp_download_dir.GetPath().AppendASCII("hello.txt"));
+
+  // We should not have seen this file still, ScopedFile in
+  // TestScopedFileWithoutBlockingShutdown should have deleted the file.
+  // However since TestScopedFileWithoutBlockingShutdown didn't use
+  // BLOCK_SHUTDOWN for its |task_runner|, |run_big_task| didn't let ScopedFile
+  // destruction code have a chance to run.
+  ASSERT_FALSE(base::PathExists(hello_file));
+
+  //std::string hello_contents;
+  //ASSERT_TRUE(base::ReadFileToString(hello_file, &hello_contents));
+  //ASSERT_EQ(std::string("cat"), hello_contents);
 }
 
 #if defined(OS_CHROMEOS)
