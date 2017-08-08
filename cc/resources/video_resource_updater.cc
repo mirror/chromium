07842a5c7b7b4b0ce74f76b0d4d9d2e507130477
Sunny Sachanandani <sunnyps@chromium.org>
2017-08-07 18:59:56 -0700

    cc: Allocate resources on worker context.
    Moves resource allocation for GL and GMB resources from compositor
    context to worker context. The allocation is performed lazily in the
    resource lock.
    
    Contains other cleanup in ResourceProvider:
    1. Bind textures to images immediately on creation rather than lazily.
       Get rid of dirty_image and bound_image_id. BindForSampling doesn't
       need to bind texture image.
    2. Rename:
       * LazyCreate -> CreateTexture
       * CreateMailboxAndBindResources -> CreateMailbox (does not bind)
       * CreateTexture/Bitmap -> CreateGpuResource/BitmapResource
    3. Use the mailbox for both async worker context and legacy code path.
       Remove the async worker context parameter from GL resource lock.
    
    R=piman
    BUG=514819
    
    Change-Id: I4f3bf1aaa80d994efcefa26143b588fdce1946a3

--- cc/resources/video_resource_updater.cc
+++ cc/resources/video_resource_updater.cc
@@ -255,10 +255,9 @@ VideoResourceUpdater::AllocateResource(const gfx::Size& plane_size,
     gpu::gles2::GLES2Interface* gl = context_provider_->ContextGL();
 
     gl->GenMailboxCHROMIUM(mailbox.name);
-    ResourceProvider::ScopedWriteLockGL lock(resource_provider_, resource_id,
-                                             false);
+    ResourceProvider::ScopedWriteLockGL lock(resource_provider_, resource_id);
     gl->ProduceTextureDirectCHROMIUM(
-        lock.texture_id(),
+        lock.GetTexture(),
         resource_provider_->GetResourceTextureTarget(resource_id),
         mailbox.name);
   }
@@ -598,7 +597,7 @@ void VideoResourceUpdater::CopyPlaneTexture(
   resource->add_ref();
 
   ResourceProvider::ScopedWriteLockGL lock(resource_provider_,
-                                           resource->resource_id(), false);
+                                           resource->resource_id());
   DCHECK_EQ(
       resource_provider_->GetResourceTextureTarget(resource->resource_id()),
       (GLenum)GL_TEXTURE_2D);
@@ -609,7 +608,7 @@ void VideoResourceUpdater::CopyPlaneTexture(
   uint32_t src_texture_id = gl->CreateAndConsumeTextureCHROMIUM(
       mailbox_holder.texture_target, mailbox_holder.mailbox.name);
   gl->CopySubTextureCHROMIUM(
-      src_texture_id, 0, GL_TEXTURE_2D, lock.texture_id(), 0, 0, 0, 0, 0,
+      src_texture_id, 0, GL_TEXTURE_2D, lock.GetTexture(), 0, 0, 0, 0, 0,
       output_plane_resource_size.width(), output_plane_resource_size.height(),
       false, false, false);
   gl->DeleteTextures(1, &src_texture_id);
