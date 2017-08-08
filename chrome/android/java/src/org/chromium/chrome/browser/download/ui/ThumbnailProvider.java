4f2c449309415172592f6bf990d8102755561d4c
Angela S <angelashao@google.com>
2017-08-08 13:38:15 -0700

    chrome/android: Add disk cache for download home thumbnails.
    We want to cache the thumbnails in download home to the disk in addition
    to the existing memory cache. This patch separates the memory cache from
    the thumbnail generation logic in ThumbnailProviderImpl and creates a
    new class ThumbnailProviderDiskStorage for disk caching.
    
    BUG=746488
    
    Change-Id: Ia7470695e594ce1bf642650110bafd64059e1fb4

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/ThumbnailProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/ThumbnailProvider.java
@@ -8,16 +8,25 @@ import android.graphics.Bitmap;
 
 /** Provides thumbnails that represent different files. */
 public interface ThumbnailProvider {
-    /** Used to request the retrieval of a thumbnail. */
+    /**
+     * Used to request the retrieval of a thumbnail. The requested thumbnail should be cached if it
+     * is implemented by {@link DownloadItemView}.
+     */
     public static interface ThumbnailRequest {
         /** Local storage path to the file. */
         String getFilePath();
 
-        /** Called when a thumbnail is ready. */
-        void onThumbnailRetrieved(String filePath, Bitmap thumbnail);
+        /** Content ID that uniquely identifies the file. */
+        String getContentId();
 
-        /** The size of the thumbnail to be retrieved. */
+        /** Called when a requested thumbnail is ready. */
+        void onThumbnailRetrieved(String contentId, Bitmap thumbnail);
+
+        /** The width (pixel) of the thumbnail to be retrieved. */
         int getIconSize();
+
+        /** Determines whether to cache the thumbnail to disk. */
+        boolean shouldCacheToDisk();
     }
 
     /** Destroys the class. */
@@ -30,6 +39,13 @@ public interface ThumbnailProvider {
      * @param request Parameters that describe the thumbnail being retrieved.
      */
     void getThumbnail(ThumbnailRequest request);
+
+    /**
+     * Removes the thumbnails (different sizes) with the given contentId from disk (if disk-cached).
+     * @param contentId The content ID of the thumbnail to remove.
+     * @return Whether
+     */
+    boolean removeThumbnailsFromDisk(String contentId);
 
     /** Removes a particular request from the pending queue. */
     void cancelRetrieval(ThumbnailRequest request);
