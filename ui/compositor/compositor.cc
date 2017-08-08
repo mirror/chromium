7aa3427d13e117285f694d968ec82b6280680baf
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-08-08 03:05:32 -0400

    mus: Reset compositor's LocalSurfaceId when size changes.
    It is necessary to update the LocalSurfaceId of the compositor when its
    size changes when running in mus. For a size-change initiated at the
    client (e.g. a Widget's size is changed, with --mash), the updated
    LocalSurfaceId is received asynchronously from the window server. Until
    then, defer commits in the compositor by setting an invalid
    LocalSurfaceId.
    
    BUG=733694
    
    Change-Id: Idb3a58dd899887beacd2126b45cff2b3df1d542a

--- ui/compositor/compositor.cc
+++ ui/compositor/compositor.cc
@@ -325,9 +325,10 @@ void Compositor::SetLatencyInfo(const ui::LatencyInfo& latency_info) {
 
 void Compositor::SetScaleAndSize(float scale, const gfx::Size& size_in_pixel) {
   DCHECK_GT(scale, 0);
-  if (!size_in_pixel.IsEmpty()) {
+  if (!size_in_pixel.IsEmpty() && size_ != size_in_pixel) {
     size_ = size_in_pixel;
     host_->SetViewportSize(size_in_pixel);
+    host_->SetLocalSurfaceId(viz::LocalSurfaceId());
     root_web_layer_->SetBounds(size_in_pixel);
     // TODO(fsamuel): Get rid of ContextFactoryPrivate.
     if (context_factory_private_)
