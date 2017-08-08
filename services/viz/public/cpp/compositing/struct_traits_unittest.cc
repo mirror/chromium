7b4c63110edc255c128e53309589013e92c8e4ef
Gary Klassen <gklassen@chromium.org>
2017-08-08 09:56:44 -0400

    viz: Move ReturnedResource mojom from cc to viz
    Move ReturnedResource mojom and associated files from cc to viz as part
    of viz restructuring.
    
    Bug: 722935
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Id7e02cdf2efdcec08a81c98dd4ad4d2b7b1ddb65

--- services/viz/public/cpp/compositing/struct_traits_unittest.cc
+++ services/viz/public/cpp/compositing/struct_traits_unittest.cc
@@ -5,9 +5,13 @@
 #include <utility>
 
 #include "base/message_loop/message_loop.h"
+#include "cc/quads/render_pass.h"
 #include "components/viz/common/resources/resource_settings.h"
+#include "components/viz/common/resources/returned_resource.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "services/viz/public/cpp/compositing/resource_settings_struct_traits.h"
+#include "services/viz/public/cpp/compositing/returned_resource_struct_traits.h"
+#include "services/viz/public/interfaces/compositing/returned_resource.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace viz {
@@ -15,6 +19,22 @@ namespace viz {
 namespace {
 
 using StructTraitsTest = testing::Test;
+
+// Test StructTrait serialization and deserialization for copyable type. |input|
+// will be serialized and then deserialized into |output|.
+template <class MojomType, class Type>
+void SerializeAndDeserialize(const Type& input, Type* output) {
+  MojomType::Deserialize(MojomType::Serialize(&input), output);
+}
+
+// Test StructTrait serialization and deserialization for move only type.
+// |input| will be serialized and then deserialized into |output|.
+template <class MojomType, class Type>
+void SerializeAndDeserialize(Type&& input, Type* output) {
+  MojomType::Deserialize(MojomType::Serialize(&input), output);
+}
+
+}  // namespace
 
 TEST_F(StructTraitsTest, ResourceSettings) {
   constexpr size_t kArbitrarySize = 32;
@@ -26,8 +46,8 @@ TEST_F(StructTraitsTest, ResourceSettings) {
       DefaultBufferToTextureTargetMapForTesting();
 
   ResourceSettings output;
-  mojom::ResourceSettings::Deserialize(
-      mojom::ResourceSettings::Serialize(&input), &output);
+  SerializeAndDeserialize<mojom::ResourceSettings>(input, &output);
+
   EXPECT_EQ(input.texture_id_allocation_chunk_size,
             output.texture_id_allocation_chunk_size);
   EXPECT_EQ(input.use_gpu_memory_buffer_resources,
@@ -36,6 +56,31 @@ TEST_F(StructTraitsTest, ResourceSettings) {
             output.buffer_to_texture_target_map);
 }
 
-}  // namespace
+TEST_F(StructTraitsTest, ReturnedResource) {
+  const cc::RenderPassId id = 1337u;
+  const gpu::CommandBufferNamespace command_buffer_namespace = gpu::IN_PROCESS;
+  const int32_t extra_data_field = 0xbeefbeef;
+  const gpu::CommandBufferId command_buffer_id(
+      gpu::CommandBufferId::FromUnsafeValue(0xdeadbeef));
+  const uint64_t release_count = 0xdeadbeefdead;
+  const gpu::SyncToken sync_token(command_buffer_namespace, extra_data_field,
+                                  command_buffer_id, release_count);
+  const int count = 1234;
+  const bool lost = true;
+
+  ReturnedResource input;
+  input.id = id;
+  input.sync_token = sync_token;
+  input.count = count;
+  input.lost = lost;
+
+  ReturnedResource output;
+  SerializeAndDeserialize<mojom::ReturnedResource>(input, &output);
+
+  EXPECT_EQ(id, output.id);
+  EXPECT_EQ(sync_token, output.sync_token);
+  EXPECT_EQ(count, output.count);
+  EXPECT_EQ(lost, output.lost);
+}
 
 }  // namespace viz
