869458342a70e5c4c1204b2de6066cef6a5d82e2
Weiliang Chen <weiliangc@chromium.org>
2017-08-08 16:16:47 -0400

    cc: Add Tracing to Compositor Frame Transport from Renderer
    This adds tracing for following cases:
    - serialization and deserialization of compositor frames in IPC,
    - deserialization of compositor frames in Mojo,
    - flow event from renderer to browser on desktop,
    - instance of time elapsed between submitting compositor frame to
    receiving from renderer to browser on desktop.
    
    R=danakj@chromium.org
    
    Bug: 699121
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I6ca1adc6dc61bc28c4c241ec63a67402f2edf7cf

--- components/viz/service/frame_sinks/gpu_compositor_frame_sink.cc
+++ components/viz/service/frame_sinks/gpu_compositor_frame_sink.cc
@@ -36,7 +36,8 @@ void GpuCompositorFrameSink::SetNeedsBeginFrame(bool needs_begin_frame) {
 void GpuCompositorFrameSink::SubmitCompositorFrame(
     const LocalSurfaceId& local_surface_id,
     cc::CompositorFrame frame,
-    mojom::HitTestRegionListPtr hit_test_region_list) {
+    mojom::HitTestRegionListPtr hit_test_region_list,
+    uint64_t submit_time) {
   // TODO(gklassen): Route hit-test data to the appropriate HitTestAggregator.
   if (!support_->SubmitCompositorFrame(local_surface_id, std::move(frame))) {
     compositor_frame_sink_binding_.CloseWithReason(
