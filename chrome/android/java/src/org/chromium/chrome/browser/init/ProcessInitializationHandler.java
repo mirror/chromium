b57f14e7bb1bd7c8da479579895364719e3511d6
Eric Karl <ericrk@chromium.org>
2017-08-08 13:27:17 -0700

    Log EGL Shader Cache Size on Android
    On Android, EGL provides a shader cache which duplicates Chrome's shader
    cache. In order to determine how much memory is being wasted, this CL
    reads the EGL cache's cache file and logs its size to UMA. This is done
    on each startup.
    
    Bug: 750723
    Change-Id: Ie8d4bbe2b01b651b0e11ec898d819aed605e00e7

--- chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.SystemClock;
 import android.support.annotation.WorkerThread;
 import android.view.View;
@@ -402,6 +403,13 @@ public class ProcessInitializationHandler {
                 BackgroundTaskSchedulerFactory.getScheduler().checkForOSUpgrade(application);
             }
         });
+
+        deferredStartupHandler.addDeferredTask(new Runnable() {
+            @Override
+            public void run() {
+                logEGLShaderCacheSizeHistogram();
+            }
+        });
     }
 
     private void initChannelsAsync() {
@@ -642,5 +650,58 @@ public class ProcessInitializationHandler {
             boolean match = systemLocale.getLanguage().equalsIgnoreCase(keyboardLanguage);
             RecordHistogram.recordBooleanHistogram("InputMethod.MatchesSystemLanguage", match);
         }
+    }
+
+    /**
+     * Logs a histogram with the size of the Android EGL shader cache.
+     */
+    private static void logEGLShaderCacheSizeHistogram() {
+        Context cacheContext;
+        // On Android N+ this cache is stored in the protected storage context.
+        if (Build.VERSION.SDK_INT >= 24) {
+            cacheContext =
+                    ContextUtils.getApplicationContext().createDeviceProtectedStorageContext();
+        } else {
+            cacheContext = ContextUtils.getApplicationContext();
+        }
+        // Make cacheContext final to pass to the AsyncTask.
+        final Context finalCacheContext = cacheContext;
+
+        // Must log async, as we're doing a file access.
+        new AsyncTask<Void, Void, Void>() {
+            // Record file sizes between 1-2560KB. Expected range is 1-2048KB, so this gives
+            // us a bit of buffer. These values cannot be changed, as doing so will alter
+            // histogram bucketing and confuse the dashboard.
+            private static final int MIN_CACHE_FILE_SIZE_KB = 1;
+            private static final int MAX_CACHE_FILE_SIZE_KB = 2560;
+
+            @Override
+            protected Void doInBackground(Void... unused) {
+                File codeCacheDir = finalCacheContext.getCodeCacheDir();
+                if (codeCacheDir == null) {
+                    return null;
+                }
+                // This filename is defined in core/java/android/view/HardwareRenderer.java in
+                // Android's platform/frameworks/base since ICS.
+                File cacheFile = new File(codeCacheDir, "com.android.opengl.shaders_cache");
+                if (!cacheFile.exists()) {
+                    return null;
+                }
+                long cacheFileSizeKb = cacheFile.length() / 1024;
+                // Clamp size to [minFileSizeKb, maxFileSizeKb). This also guarantees that the
+                // int-cast below is safe.
+                if (cacheFileSizeKb < MIN_CACHE_FILE_SIZE_KB) {
+                    cacheFileSizeKb = MIN_CACHE_FILE_SIZE_KB;
+                }
+                if (cacheFileSizeKb >= MAX_CACHE_FILE_SIZE_KB) {
+                    cacheFileSizeKb = MAX_CACHE_FILE_SIZE_KB - 1;
+                }
+                String histogramName = "Memory.Experimental.Browser.EGLShaderCacheSize.Android";
+                RecordHistogram.recordCustomCountHistogram(histogramName, (int) cacheFileSizeKb,
+                        MIN_CACHE_FILE_SIZE_KB, MAX_CACHE_FILE_SIZE_KB, 50);
+                return null;
+            }
+        }
+                .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 }
