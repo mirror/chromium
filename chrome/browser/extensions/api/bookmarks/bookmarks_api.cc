c986f189668cb0cab09048c3270eee3e243329b5
Xing Liu <xingliu@chromium.org>
2017-08-07 20:10:46 -0700

    Unicode characters on linux can bypass file name filtering.
    Some unicode special characters in the file path should be filtered,
    such as,
    http://unicode.org/reports/tr9/#Directional_Formatting_Characters
    
    On Linux, the OS doesn't specify the file system encoding, the filtering
    function to generate file path only parses the string as ASCII, but the
    input string is actually already UTF8. So illegal unicode chars will
    bypass the filtering.
    
    
    Bug:686503
    Change-Id: I2cdd6b183391c77c5a4af86f1710eb02c9fa7d57

--- chrome/browser/extensions/api/bookmarks/bookmarks_api.cc
+++ chrome/browser/extensions/api/bookmarks/bookmarks_api.cc
@@ -87,7 +87,7 @@ base::FilePath GetDefaultFilepathForBookmarkExport() {
                                  base::TimeFormatShortDateNumeric(time));
 #endif
 
-  base::i18n::ReplaceIllegalCharactersInPath(&filename, '_');
+  base::i18n::ReplaceIllegalCharactersInPath(&filename, '_', true);
 
   base::FilePath default_path;
   PathService::Get(chrome::DIR_USER_DOCUMENTS, &default_path);
