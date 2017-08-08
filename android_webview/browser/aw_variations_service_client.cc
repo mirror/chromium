b9f6c136553ae04f6ddd8259a6dae4318068af27
Kyle Milka <kmilka@google.com>
2017-08-07 17:00:45 -0700

    Set up field trials within WebView
    This is part 6 of 6 for FieldTrial refactoring for WebView
    
    Part 1: https://chromium-review.googlesource.com/c/562098/
    Part 2: https://chromium-review.googlesource.com/c/561920
    Part 3: https://chromium-review.googlesource.com/c/561922/
    Part 4: https://chromium-review.googlesource.com/c/561980/
    Part 5: https://chromium-review.googlesource.com/c/562417/
    Part 6: https://chromium-review.googlesource.com/c/562021/
    
    This CL makes use of previous refactoring work to actually
    set up field trials within WebView. Several field are added
    to AwBrowserMainParts that are then passed into
    VariationsSeedManager's SetupFieldTrials().
    
    BUG=678288
    Change-Id: Id76b98e5adb432ca3fa7ea4cffd7c0296f160e82

---
+++ android_webview/browser/aw_variations_service_client.cc
@@ -0,0 +1,51 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "android_webview/browser/aw_variations_service_client.h"
+
+#include "base/bind.h"
+#include "build/build_config.h"
+#include "components/version_info/version_info.h"
+
+namespace {
+
+// Gets the version number to use for variations seed simulation. Must be called
+// on a thread where IO is allowed.
+base::Version GetVersionForSimulation() {
+  return base::Version(version_info::GetVersionNumber());
+}
+
+}  // namespace
+
+AwVariationsServiceClient::AwVariationsServiceClient() {}
+
+AwVariationsServiceClient::~AwVariationsServiceClient() {}
+
+std::string AwVariationsServiceClient::GetApplicationLocale() {
+  return "";
+}
+
+base::Callback<base::Version(void)>
+AwVariationsServiceClient::GetVersionForSimulationCallback() {
+  return base::Bind(&GetVersionForSimulation);
+}
+
+net::URLRequestContextGetter*
+AwVariationsServiceClient::GetURLRequestContext() {
+  return nullptr;
+}
+
+network_time::NetworkTimeTracker*
+AwVariationsServiceClient::GetNetworkTimeTracker() {
+  return nullptr;
+}
+
+version_info::Channel AwVariationsServiceClient::GetChannel() {
+  return version_info::Channel::DEV;
+}
+
+bool AwVariationsServiceClient::OverridesRestrictParameter(
+    std::string* parameter) {
+  return false;
+}
