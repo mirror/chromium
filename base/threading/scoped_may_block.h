ca7bd853f785654ae9dd1b5f7057637162023f5a
Jeffrey He <jeffreyhe@google.com>
2017-08-08 15:26:56 -0400

    Add ScopedMayBlock and BlockingObserver classes
    The ScopedMayBlock class is used to annotate code that may block. It
    informs the BlockingObserver object for the current thread in TLS by
    calling BlockingObserver::ThreadBlocked() when ScopedMayBlock is created
    and BlockingObserver::ThreadUnblocked() when destroyed.
    
    Bug: 738104
    Change-Id: I965932a058a0ac22e465e2afa2f85b515c35a439

---
+++ base/threading/scoped_may_block.h
@@ -0,0 +1,36 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_THREADING_SCOPED_MAY_BLOCK_H
+#define BASE_THREADING_SCOPED_MAY_BLOCK_H
+
+#include "base/base_export.h"
+
+namespace base {
+
+// This class should be instantiated in every scope where a blocking call is
+// made.
+class BASE_EXPORT ScopedMayBlock {
+ public:
+  ScopedMayBlock();
+  ~ScopedMayBlock();
+};
+
+namespace internal {
+
+// Interface for an observer to be informed when a thread enters or exits
+// the scope of a ScopedMayBlock object.
+class BASE_EXPORT BlockingObserver {
+ public:
+  virtual void BlockingScopeEntered() = 0;
+  virtual void BlockingScopeExited() = 0;
+};
+
+void SetBlockingObserverForCurrentThread(BlockingObserver* blocking_observer);
+
+}  // namespace internal
+
+}  // namespace base
+
+#endif  // BASE_THREADING_SCOPED_MAY_BLOCK_H
