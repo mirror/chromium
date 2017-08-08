d490845257ec745a006a6fdbe4c04cc15d99e03f
oka@chromium.org <oka@chromium.org>
2017-08-08 10:10:00 +0900

    Implement virtualKeyboard.restrictFeatures properly
    * API usage
    With proper IME extension side update, this change allows Kiosk app to
    update the following IME configs:
    - auto complete
    - auto correct
    - handwriting
    - spell_check
    - voice_input
    through chrome.virtualKeyboard.restrictFeatures API.
    
    For IME extension,
    - 'features' which chrome.virtualKeyboardPrivate.getKeyboardConfig returns
    contains these configs in the form of 'autocomplete-enabled'.
    - chrome.virtualKeyboardPrivate.onKeyboardConfigChanged event, which
    is fired on the config change, has been added.
    
    * Construct
    - keyboard_util.cc manages the config as a global variable. Using global state
    is not ideal but should work, because its change menas the device is under
    Kiosk app's control anyway.
    - ChromeVirtualKeyboardDelegate contains the actual logic to construct the
    APIs' return value.
    - All the other files are needed accomodation and wiring, or tests.
    
    Bug: 710242
    Change-Id: Ie00d6545c5e189bf6132ed6278698f2f0e7d433a

--- ui/keyboard/keyboard_controller.cc
+++ ui/keyboard/keyboard_controller.cc
@@ -737,6 +737,11 @@ void KeyboardController::
   ui_->EnsureCaretInWorkArea();
 }
 
+void KeyboardController::NotifyKeyboardConfigChanged() {
+  for (KeyboardControllerObserver& observer : observer_list_)
+    observer.OnKeyboardConfigChanged();
+}
+
 void KeyboardController::HideAnimationFinished() {
   if (state_ != KeyboardControllerState::HIDING)
     return;
