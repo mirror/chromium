c2e61e4e46379a9b2cc2b51d00e39385384b5d94
Tina Quach <quacht@google.com>
2017-08-08 10:53:59 -0700

    Add ability to filter accessibility audit results.
    The aXe-audit sometimes has false positives, this CL allows exceptions
    to be filtered. It also does some refactoring and adds the aXe
    audit's type definitions so they may be referenced in documentation.
    
    Bug: 737739, 748632
    Change-Id: Id7b9e626007b80e64261d36cdbd532b3f93e72b9

--- chrome/test/data/webui/settings/passwords_a11y_test.js
+++ chrome/test/data/webui/settings/passwords_a11y_test.js
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/** @fileoverview Suite of accessibility tests for the passwords page. */
+/**
+ * @fileoverview Define accessibility tests for the MANAGE_PASSWORDS route.
+ */
 
 AccessibilityTest.define({
   /** @override */
@@ -65,6 +67,17 @@ AccessibilityTest.define({
       Polymer.dom.flush();
 
       assertEquals(10, this.passwordsSection.savedPasswords.length);
+    },
+  },
+  violationFilter: {
+    // TODO(quacht): remove this exception once the color contrast issue is
+    // solved.
+    // http://crbug.com/748608
+    'color-contrast': function(nodeResult) {
+      return nodeResult.element.id == 'prompt';
+    },
+   'aria-valid-attr': function(nodeResult) {
+      return nodeResult.element.hasAttribute('aria-active-attribute');
     },
   }
 });
