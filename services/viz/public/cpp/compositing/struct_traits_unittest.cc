b2131ae87bf3830d1390d3ebbb89872aad09d551
Saman Sami <samans@chromium.org>
2017-08-08 10:49:23 -0400

    viz: Move SurfaceSequence's mojom from cc/ipc to services/viz
    Bug:722935
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ibaff586055b80c63b6666efe010b3f8a5dfae4a5

--- services/viz/public/cpp/compositing/struct_traits_unittest.cc
+++ services/viz/public/cpp/compositing/struct_traits_unittest.cc
@@ -5,9 +5,12 @@
 #include <utility>
 
 #include "base/message_loop/message_loop.h"
+#include "cc/ipc/frame_sink_id_struct_traits.h"
 #include "components/viz/common/resources/resource_settings.h"
 #include "mojo/public/cpp/bindings/binding_set.h"
 #include "services/viz/public/cpp/compositing/resource_settings_struct_traits.h"
+#include "services/viz/public/cpp/compositing/surface_sequence_struct_traits.h"
+#include "services/viz/public/interfaces/compositing/surface_sequence.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace viz {
@@ -34,6 +37,19 @@ TEST_F(StructTraitsTest, ResourceSettings) {
             output.use_gpu_memory_buffer_resources);
   EXPECT_EQ(input.buffer_to_texture_target_map,
             output.buffer_to_texture_target_map);
+}
+
+TEST_F(StructTraitsTest, SurfaceSequence) {
+  const FrameSinkId frame_sink_id(2016, 1234);
+  const uint32_t sequence = 0xfbadbeef;
+
+  SurfaceSequence input(frame_sink_id, sequence);
+  SurfaceSequence output;
+  mojom::SurfaceSequence::Deserialize(mojom::SurfaceSequence::Serialize(&input),
+                                      &output);
+
+  EXPECT_EQ(frame_sink_id, output.frame_sink_id);
+  EXPECT_EQ(sequence, output.sequence);
 }
 
 }  // namespace
