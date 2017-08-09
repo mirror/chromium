1a73cd608284a631d874bd9896886d348d3800b6
Charles Harrison <csharrison@chromium.org>
2017-08-09 11:35:18 -0400

    New mojo service for exposing FirstPaint per RenderFrameHost
    This service hooks directly in Blink's PaintTiming class which computes
    the first paint, and uses the same timestamp as exposed to the
    web platform.
    
    A followup will plumb the data to WebContentsObservers, and formally
    deprecate WebContentsObserver::DidFirstVisuallyNonEmptyPaint.
    
    Bug: 738464
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I67618fcdbca9670ff37d5f03f113a0f0dd65b62c

--- third_party/WebKit/Source/core/paint/PaintTiming.cpp
+++ third_party/WebKit/Source/core/paint/PaintTiming.cpp
@@ -10,6 +10,7 @@
 #include "core/dom/Document.h"
 #include "core/frame/LocalDOMWindow.h"
 #include "core/frame/LocalFrame.h"
+#include "core/frame/LocalFrameClient.h"
 #include "core/frame/LocalFrameView.h"
 #include "core/loader/DocumentLoader.h"
 #include "core/loader/ProgressTracker.h"
@@ -20,7 +21,9 @@
 #include "platform/Histogram.h"
 #include "platform/WebFrameScheduler.h"
 #include "platform/instrumentation/tracing/TraceEvent.h"
+#include "platform/wtf/Time.h"
 #include "public/platform/WebLayerTreeView.h"
+#include "public/platform/frame_broker.mojom-blink.h"
 
 namespace blink {
 
@@ -169,6 +172,10 @@ void PaintTiming::SetFirstPaint(double stamp) {
   TRACE_EVENT_INSTANT1("loading,rail,devtools.timeline", "firstPaint",
                        TRACE_EVENT_SCOPE_PROCESS, "frame", GetFrame());
   RegisterNotifySwapTime(PaintEvent::kFirstPaint);
+
+  auto timing = GetSupplementable()->Loader()->GetTiming();
+  GetFrame()->GetFrameBroker()->OnFirstPaint(
+      WTF::TimeDelta::FromSecondsD(stamp - timing.NavigationStart()));
 }
 
 void PaintTiming::SetFirstContentfulPaint(double stamp) {
