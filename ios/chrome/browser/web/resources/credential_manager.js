866257d15cdda24d9816eab759064b456ec8eb36
Tomasz Garbus <tgarbus@chromium.org>
2017-08-09 12:26:51 +0200

    navigator.credentials methods send message to host
    navigator.credentials methods get, store, preventSilentAccess send
    message to host, consisting of command name, unique ID and options
    dictionary. A Promise is created and returned. Its executor function
    stores resolver and rejecter functions in __gCrWeb['credentialManager']
    namespace, so that they can be accessed by their IDs.
    
    Example flow for method calls is as follows:
    1. website invokes navigator.credentials.get(options)
    2. __gCrWeb['credentialManager'].invokeOnHost_() sends a message:
    {   command: "navigator.credentials.get",
      requestId: 5,
      mediation: options.mediation
    }
    3. A Promise is created and returned, |resolve| and |reject|
    methods are stored in __gCrWeb['credentialManager'].resolvers_ and
    rejecters_.
    4. On the app-side |credential| is obtained from PasswordStore and
    browser executes Javascript like
    "__gCrWeb['credentialManager'].resolvers_[requestId](credential)"
    
    
    Bug: 435046
    Change-Id: I9365c21759bb8dd6bd769fc93a399d444f091f84

--- ios/chrome/browser/web/resources/credential_manager.js
+++ ios/chrome/browser/web/resources/credential_manager.js
@@ -18,14 +18,152 @@
 // Namespace for credential management. __gCrWeb must have already
 // been defined.
 __gCrWeb['credentialManager'] = {
+
+  /**
+   * Used to apply unique requestId fields to messages sent to host.
+   * Those IDs can be later used to call a corresponding resolver/rejecter.
+   * @private {number}
+   */
+  nextId_: 0,
+
+  /**
+   * Stores the functions for resolving Promises returned by
+   * navigator.credentials method calls. A resolver for a call with
+   * requestId: |id| is stored at resolvers_[id].
+   * @type {!Object<number, function(?Credential)|function()>}
+   * @private
+   */
+  resolvers_: {},
+
+  /**
+   * Stores the functions for rejecting Promises returned by
+   * navigator.credentials method calls. A rejecter for a call with
+   * requestId: |id| is stored at rejecters_[id].
+   * @type {!Object<number, function(?Error)>}
+   * @private
+   */
+  rejecters_: {}
 };
+
+/**
+ * Creates and returns a Promise with given |requestId|. The Promise's executor
+ * function stores resolver and rejecter functions in
+ * __gCrWeb['credentialManager'] under the key |requestId| so they can be called
+ * from the host after executing app side code.
+ * @param {number} requestId The number assigned to newly created Promise.
+ * @return {!Promise<?Credential>|!Promise<!Credential>|!Promise<void>}
+ *     The created Promise.
+ * @private
+ */
+__gCrWeb['credentialManager'].createPromise_ = function(requestId) {
+  return new Promise(function(resolve, reject) {
+    __gCrWeb['credentialManager'].resolvers_[requestId] = resolve;
+    __gCrWeb['credentialManager'].rejecters_[requestId] = reject;
+  });
+}
+
+/**
+ * Sends a message to the app side, invoking |command| with the given |options|.
+ * @param {string} command The name of the invoked command.
+ * @param {Object} options A dictionary of additional properties to forward to
+ *     the app.
+ * @return {!Promise<?Credential>|!Promise<!Credential>|!Promise<void>}
+ *     A promise to be returned by the calling method.
+ * @private
+ */
+__gCrWeb['credentialManager'].invokeOnHost_ = function(command, options) {
+  var requestId = __gCrWeb['credentialManager'].nextId_++;
+  var message = {
+    'command': command,
+    'requestId': requestId
+  };
+  if (options) {
+    Object.keys(options).forEach(function(key) {
+      message[key] = options[key];
+    });
+  }
+  __gCrWeb.message.invokeOnHost(message);
+  return __gCrWeb['credentialManager'].createPromise_(requestId);
+}
 
 /**
  * The Credential interface, for more information see
  * https://w3c.github.io/webappsec-credential-management/#the-credential-interface
  * @constructor
  */
-function Credential() {}
+function Credential() {
+  /** @type {string} */
+  this.id;
+  /** @type {string} */
+  this.type;
+}
+
+/**
+ * PasswordCredential interace, for more information see
+ * https://w3c.github.io/webappsec-credential-management/#passwordcredential-interface
+ * @extends {Credential}
+ * @constructor
+ */
+// TODO(crbug.com/435046) tgarbus: Implement two constructors: from
+// HTMLFormElement and from PasswordCredentialData
+function PasswordCredential() {
+  /** @type {string} */
+  this.name;
+  /** @type {string} */
+  this.iconURL;
+  /** @type {string} */
+  this.password;
+}
+
+/**
+ * FederatedCredential interface, for more information see
+ * https://w3c.github.io/webappsec-credential-management/#federatedcredential-interface
+ * @extends {Credential}
+ * @constructor
+ */
+// TODO(crbug.com/435046) tgarbus: Implement constructor taking
+// FederatedCredentialInit
+function FederatedCredential() {
+  /** @type {string} */
+  this.name;
+  /** @type {string} */
+  this.iconURL;
+  /** @type {string} */
+  this.provider;
+  /** @type {?string} */
+  this.protocol;
+}
+
+/**
+ * CredentialData dictionary
+ * https://w3c.github.io/webappsec-credential-management/#dictdef-credentialdata
+ * @typedef {{id: string}}
+ */
+var CredentialData;
+
+/**
+ * PasswordCredentialData used for constructing PasswordCredential objects
+ * https://w3c.github.io/webappsec-credential-management/#dictdef-passwordcredentialdata
+ * @typedef {{
+ *     id: string,
+ *     name: string,
+ *     iconURL: string,
+ *     password: string
+ * }}
+ */
+var PasswordCredentialData;
+
+/**
+ * FederatedCredentialInit used for constructing FederatedCredential objects
+ * https://w3c.github.io/webappsec-credential-management/#dictdef-federatedcredentialinit
+ * @typedef {{
+ *     id: string,
+ *     name: string,
+ *     iconURL: string,
+ *     provider: string
+ * }}
+ */
+var FederatedCredentialInit;
 
 /**
  * The CredentialRequestOptions dictionary, for more information see
@@ -35,11 +173,46 @@ function Credential() {}
 var CredentialRequestOptions;
 
 /**
+ * The FederatedCredentialRequestOptions dictionary, for more information see
+ * https://w3c.github.io/webappsec-credential-management/#dictdef-federatedcredentialrequestoptions
+ * @typedef {{
+ *     providers: {!Array<string>},
+ *     protocols: {!Array<string>}
+ * }}
+ * @extends {CredentialRequestOptions}
+ */
+var FederatedCredentialRequestOptions;
+
+/**
  * The CredentialCreationOptions dictionary, for more information see
  * https://w3c.github.io/webappsec-credential-management/#credentialcreationoptions-dictionary
  * @typedef {Object}
  */
 var CredentialCreationOptions;
+
+Credential.prototype.serialize = function() {
+  var serialized = {
+    'id': this.id,
+    'type': this.type
+  }
+}
+
+PasswordCredential.prototype.serialize = function() {
+  var serialized = Credential.prototype.serialize.call(this);
+  serialized.name = this.name;
+  serialized.iconURL = this.iconURL;
+  serialized.password = this.password;
+  return serialized;
+}
+
+FederatedCredential.prototype.serialize = function() {
+  var serialized = Credential.prototype.serialize.call(this);
+  serialized.name = this.name;
+  serialized.iconURL = this.iconURL;
+  serialized.provider = this.provider;
+  serialized.protocol = this.protocol;
+  return serialized;
+}
 
 /**
  * Implements the public Credential Management API. For more information, see
@@ -57,8 +230,8 @@ function CredentialsContainer() {}
  *     of the request.
  */
 CredentialsContainer.prototype.get = function(options) {
-  // TODO(crbug.com/435046) tgarbus: Implement |get| as JS invoking
-  // |get| on host.
+  return __gCrWeb['credentialManager'].invokeOnHost_(
+      'navigator.credentials.get', options);
 };
 
 /**
@@ -69,8 +242,9 @@ CredentialsContainer.prototype.get = function(options) {
  *     of the request.
  */
 CredentialsContainer.prototype.store = function(credential) {
-  // TODO(crbug.com/435046) tgarbus: Implement |store| as JS invoking
-  // |store| on host.
+  var serialized_credential = credential.serialize();
+  return __gCrWeb['credentialManager'].invokeOnHost_(
+      'navigator.credentials.store', serialized_credential);
 };
 
 /**
@@ -80,8 +254,8 @@ CredentialsContainer.prototype.store = function(credential) {
  *     of the request.
  */
 CredentialsContainer.prototype.preventSilentAccess = function() {
-  // TODO(crbug.com/435046) tgarbus: Implement |preventSilentAccess| as JS
-  // invoking |preventSilentAccess| on host.
+  return __gCrWeb['credentialManager'].invokeOnHost_(
+      'navigator.credentials.preventSilentAccess');
 };
 
 /**
