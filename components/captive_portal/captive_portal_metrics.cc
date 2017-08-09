f79b17fb57ae17a3fa79b16cf4ac49c31a7f7b41
Mike Dougherty <michaeldo@chromium.org>
2017-08-09 13:38:43 -0700

    Move captive portal metrics enum to captive portal component.
    Bug: None
    Change-Id: I9f4d3f3130c41badf3f74ed911f2c7808c1eedb4

---
+++ components/captive_portal/captive_portal_metrics.cc
@@ -0,0 +1,15 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/captive_portal/captive_portal_metrics.h"
+
+namespace captive_portal {
+
+void CaptivePortalMetrics::LogCaptivePortalBlockingPageEvent(
+    CaptivePortalBlockingPageEvent event) {
+  UMA_HISTOGRAM_ENUMERATION("interstitial.captive_portal", event,
+                            CAPTIVE_PORTAL_BLOCKING_PAGE_EVENT_COUNT);
+}
+
+}  // namespace captive_portal
