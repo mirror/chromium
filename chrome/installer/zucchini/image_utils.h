9b72ab4c3ff72d847f1872e42c25093b2b0a1455
Etienne Pierre-Doray <etiennep@chromium.org>
2017-08-09 09:45:06 -0700

    [Zucchini] Add zucchini API declaration; implement MainGen and MainApply.
    This includes:
    - Add Generate, GenerateRaw and Apply in zucchini.h
    - Implement MainGen and MainApply using these functions
    
    Please ignore changes regarding BufferRegion, which is introduced
    in 606739.
    
    Bug: 729154
    Change-Id: I6024c7b3431199f70fa4bebe2e198a3e94e30f0c

--- chrome/installer/zucchini/image_utils.h
+++ chrome/installer/zucchini/image_utils.h
@@ -8,6 +8,7 @@
 #include <stdint.h>
 
 #include "base/optional.h"
+#include "chrome/installer/zucchini/buffer_view.h"
 #include "chrome/installer/zucchini/typed_value.h"
 
 namespace zucchini {
@@ -133,6 +134,12 @@ enum ExecutableType : uint32_t {
 // |exe_type| can be kExeTypeNoOp, in which case the Element descibes a region
 // of raw data.
 struct Element {
+  Element() = default;
+  constexpr Element(ExecutableType exe_type, offset_t offset, offset_t length)
+      : exe_type(exe_type), offset(offset), length(length) {}
+  constexpr explicit Element(const BufferRegion& region)
+      : exe_type(kExeTypeNoOp), offset(region.offset), length(region.size) {}
+
   ExecutableType exe_type;
   offset_t offset;
   offset_t length;
