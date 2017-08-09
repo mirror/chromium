f755accb181f3b4ce13744ad972ac9c7211cd7cc
Nicolas Dossou-gbete <dgn@chromium.org>
2017-08-09 16:20:52 +0100

    [Suggestions] Expose and use Site Exploration flags and sections
    Bug: 740905
    Change-Id: I2629ed14593d88e3436119559ab84e05a9a9ce2e

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1056,6 +1056,9 @@ extern const char kNtpCondensedLayoutDescription[];
 extern const char kNtpCondensedTileLayoutName[];
 extern const char kNtpCondensedTileLayoutDescription[];
 
+extern const char kSitesExplorationName[];
+extern const char kSitesExplorationDescription[];
+
 extern const char kNtpGoogleGInOmniboxName[];
 extern const char kNtpGoogleGInOmniboxDescription[];
 
