a7779f4192f2206a2e69c95a2aa595b09966c609
Emil A Eklund <eae@chromium.org>
2017-08-09 14:22:46 -0400

    [LayoutNG] Add basic support for painting from fragment tree
    Add very basic support for painting LayoutNG fragments directly from the
    NG fragment tree for inline layout instead of synthesizing LayoutObjects
    and painting from the legacy tree. Behind the new LayoutNGPaintFragments
    runtime enabled feature for now to allow us to iterate on it before it's
    ready to turn on by default. The new flag is expected to be short lived.
    
    Use these flags: --enable-blink-features=LayoutNG,LayoutNGPaintFragments
    and optionally --enable-slimming-paint-v2 to activate fragment painting.
    
    Bug: 714962
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I5e51988900220be419f4bcf9803c1fbfb8a2b008

---
+++ third_party/WebKit/Source/core/paint/ng/ng_block_flow_painter.cc
@@ -0,0 +1,31 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/paint/ng/ng_block_flow_painter.h"
+
+#include "core/layout/ng/inline/ng_physical_line_box_fragment.h"
+#include "core/layout/ng/layout_ng_block_flow.h"
+#include "core/layout/ng/ng_physical_box_fragment.h"
+#include "core/paint/PaintInfo.h"
+#include "core/paint/ng/ng_box_fragment_painter.h"
+#include "platform/RuntimeEnabledFeatures.h"
+
+namespace blink {
+
+void NGBlockFlowPainter::PaintContents(const PaintInfo& paint_info,
+                                       const LayoutPoint& paint_offset) {
+  RefPtr<const NGPhysicalBoxFragment> box_fragment = block_.RootFragment();
+  if (box_fragment)
+    PaintBoxFragment(*box_fragment, paint_info, paint_offset);
+}
+
+void NGBlockFlowPainter::PaintBoxFragment(const NGPhysicalBoxFragment& fragment,
+                                          const PaintInfo& paint_info,
+                                          const LayoutPoint& paint_offset) {
+  PaintInfo ng_paint_info(paint_info);
+  NGBoxFragmentPainter box_painter(fragment);
+  box_painter.Paint(ng_paint_info, paint_offset);
+}
+
+}  // namespace blink
