44821b5c9fe020b8c1b24d77f4c25cbc2a43040c
Eric Lawrence <elawrence@chromium.org>
2017-08-09 11:22:34 -0500

    Add an Explanation when security downgraded by non-secure field editing
    When a non-secure field edit downgrades the page's SecurityLevel to
    HTTP_SHOW_WARNING, add an explanation to |neutral_explanations|. The
    explanation is shown in the Developer Tools' Security panel.
    
    Bug: 720094
    Change-Id: Ibcd29d850fadeee418f2a741a72e17731f8b511e

--- components/security_state/content/content_utils_unittest.cc
+++ components/security_state/content/content_utils_unittest.cc
@@ -4,6 +4,7 @@
 
 #include "components/security_state/content/content_utils.h"
 
+#include <string>
 #include <vector>
 
 #include "base/command_line.h"
@@ -525,6 +526,16 @@ TEST(SecurityStateContentUtilsTest, HTTPWarning) {
   security_style = GetSecurityStyle(security_info, &explanations);
   EXPECT_EQ(blink::kWebSecurityStyleNeutral, security_style);
   EXPECT_EQ(2u, explanations.neutral_explanations.size());
+
+  // Verify that three explanations are shown when the Incognito, FormNotSecure,
+  // and Insecure Field Edit flags are all set.
+  explanations.neutral_explanations.clear();
+  security_info.displayed_credit_card_field_on_http = true;
+  security_info.incognito_downgraded_security_level = true;
+  security_info.field_edit_downgraded_security_level = true;
+  security_style = GetSecurityStyle(security_info, &explanations);
+  EXPECT_EQ(blink::kWebSecurityStyleNeutral, security_style);
+  EXPECT_EQ(3u, explanations.neutral_explanations.size());
 }
 
 // Tests that an explanation is provided if a certificate is missing a
