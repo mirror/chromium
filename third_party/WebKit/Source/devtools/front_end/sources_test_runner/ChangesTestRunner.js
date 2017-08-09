aad3f562b8c2b3db2ee983e6ebf350dbd1a16a7d
Will Chen <chenwilliam@chromium.org>
2017-08-08 17:42:48 -0700

    DevTools: migrate integration test helpers
    Most of this CL was generated by migrate_helpers.js which automatically
    transforms our test helpers into the new format.
    
    This enables us to:
    1) Try mass-converting as many tests right away
    2) Allow new tests to be written for all domains
    
    Bug: 667560
    Change-Id: Id8e863c098ba815c219324387ad83eb518340b0f

---
+++ third_party/WebKit/Source/devtools/front_end/sources_test_runner/ChangesTestRunner.js
@@ -0,0 +1,8 @@
+// Copyright 2017 The Chromium Authors. All
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview using private properties isn't a Closure violation in tests.
+ * @suppress {accessControls}
+ */
