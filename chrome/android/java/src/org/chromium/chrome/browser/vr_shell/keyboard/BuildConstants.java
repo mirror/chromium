42150dc6f3922be58137981dedf1f103dbbfafd7
Yash Malik <ymalik@google.com>
2017-08-09 14:53:48 -0400

    Add keyboard support
    Based on: https://codereview.chromium.org/2798633002/
    
    Bug:
    Change-Id: I82b4b136ff734b49b1addd18de9c26bf06b568f8

---
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/keyboard/BuildConstants.java
@@ -0,0 +1,14 @@
+package org.chromium.chrome.browser.vr_shell.keyboard;
+
+/**
+ * Miscellaneous build-related constants.
+ */
+public class BuildConstants {
+    /**
+     * The version number of the keyboard API. A local copy of this class should
+     * be built into both the client and the SDK. Then at SDK load time, the
+     * version numbers can be compared to make sure the client and SDK have
+     * compatible APIs.
+     */
+    public static final long API_VERSION = 1;
+}
