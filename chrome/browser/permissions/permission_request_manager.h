8139bc7db53e47b71f38de72b6d3d58a0881d202
Timothy Loh <timloh@chromium.org>
2017-08-09 19:12:44 +1000

    [WIP] Don't show modal permission prompts for background tabs
    This patch fixes modal permission prompts to not appear for background
    tabs. On desktop we already do this in the PermissionRequestManager with
    the tab_can_show_prompts_ bool, so we just re-use it on Android too.
    
    This plumbing on Android is mostly from https://crrev.com/2781803002
    where we previously deleted this logic because it didn't seem necessary
    but we didn't consider the modal dialog case. The permissions code
    differs from that CL as we still avoid deleting the PermissionPrompt.
    
    Bug: 751025
    Change-Id: Ie52ca6e5985334b807dd6aa91c9f1aca94846429

--- chrome/browser/permissions/permission_request_manager.h
+++ chrome/browser/permissions/permission_request_manager.h
@@ -75,14 +75,9 @@ class PermissionRequestManager
   // at which time the caller is free to delete the request.
   void CancelRequest(PermissionRequest* request);
 
-  // Temporarily hides the bubble, and destroys the prompt UI surface. Any
-  // existing requests will be reshown when DisplayPendingRequests is called
-  // (e.g. when switching tabs away and back to a page with a prompt).
-  // TODO(timloh): Rename this to something more fitting (e.g. TabSwitchedAway).
-  void HideBubble();
-
   // Will show a permission bubble if there is a pending permission request on
   // the web contents that the PermissionRequestManager belongs to.
+  // TODO(timloh): Remove this from the public API.
   void DisplayPendingRequests();
 
   // Will reposition the bubble (may change parent if necessary).
@@ -135,6 +130,11 @@ class PermissionRequestManager
   void DocumentLoadedInFrame(
       content::RenderFrameHost* render_frame_host) override;
   void WebContentsDestroyed() override;
+  // Prompts are only shown when a tab is showing. On switching tabs with an
+  // active prompt, we destroy the prompt on desktop but not Android as the
+  // infobar system handles it for us and modals prevent tab switching.
+  void WasShown() override;
+  void WasHidden() override;
 
   // PermissionPrompt::Delegate:
   const std::vector<PermissionRequest*>& Requests() override;
@@ -191,7 +191,7 @@ class PermissionRequestManager
   std::unique_ptr<PermissionPrompt> view_;
   // We only show prompts when both of these are true. On Desktop, we hide any
   // active prompt on tab switching, while on Android we let the infobar system
-  // handle it.
+  // handle it and modals prevent tab switching.
   bool main_frame_has_fully_loaded_;
   bool tab_can_show_prompts_;
 
