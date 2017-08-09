cdc69b249c4844b4424cd555f2c4da99361de9d1
Michael Wasserman <msw@chromium.org>
2017-08-09 15:21:48 -0700

    mash: Add shelf context menu support.
    Chrome serializes its shelf item context menus for Ash.
    Ash displays the menus and reports invocations to Chrome.
    
    Add an ash-side ShelfContextMenu class that supports:
    -Local menu items to set shelf & wallpaper prefs/settings.
    -Proxies Chrome's menu items via mojo structs/interfaces.
    -Also used for wallpaper/desktop context menus (via RWC).
    
    Extend mojom::ShelfItemDelegate::ExecuteCommand, MenuItem, etc.:
    -Denote the command's origin (context/app menu) and display id.
    -Allow 64-bit int command and radio-group ids; null images.
    -Add ShelfAction::SHELF_ACTION_SHOW_CONTEXT_MENU.
    
    Add common ash::ShelfItemDelegate context menu handling:
    -GetContextMenu() is overridden by delegate subclasses.
    -WillShowContextMenu() handles item right-click selection.
    -ExecuteContextMenuCommand() handles context menu invocation.
    
    Wire up chrome's launcher item subclass context menus.
    Make minor launcher context menu class simplifications.
    Add a ShelfWindowWatcher context menu item for closing.
    
    Convert ShelfButtonPressedMetricTracker from Button to View.
    (to support AfterItemSelected via ShowContextMenuForView)
    
    Remove mus, ash_shell, test, and desktop context menu classes.
    Remove ShellDelegate::CreateContextMenu and ash::ShelfAlignmentMenu.
    Inline CanChangeShelfAlignment; move CanUserModifyShelfAutoHideBehavior.
    Add unit tests, flip TestWallpaperDelegate::CanOpenSetWallpaperPage.
    
    Bug: 640693, 753028
    Test: No Chrome OS shelf/desktop context menu behavior changes.
    Change-Id: I3afb93095b4042dca57d16844c5b05308f06a384

--- ash/public/cpp/shelf_item_delegate.h
+++ ash/public/cpp/shelf_item_delegate.h
@@ -17,6 +17,10 @@
 
 class AppWindowLauncherItemController;
 
+namespace ui {
+class MenuModel;
+}
+
 namespace ash {
 
 using MenuItemList = std::vector<mojom::MenuItemPtr>;
@@ -44,8 +48,20 @@ class ASH_PUBLIC_EXPORT ShelfItemDelegate : public mojom::ShelfItemDelegate {
   // Returns items for the application menu; used for convenience and testing.
   virtual MenuItemList GetAppMenuItems(int event_flags);
 
+  // Returns the context menu model; used to serialize existing ShelfItem menus.
+  virtual std::unique_ptr<ui::MenuModel> GetContextMenu(int64_t display_id);
+
   // Returns nullptr if class is not AppWindowLauncherItemController.
   virtual AppWindowLauncherItemController* AsAppWindowLauncherItemController();
+
+  // Runs the callback to show the shelf item's context menu on right-click.
+  // Returns true if the menu will be shown and no further action is needed.
+  bool WillShowContextMenu(const ui::Event* event,
+                           int64_t display_id,
+                           ItemSelectedCallback* callback);
+
+  // Attempts to execute a context menu command; returns true if it was run.
+  bool ExecuteContextMenuCommand(int64_t command_id, int32_t event_flags);
 
  private:
   // The shelf id; empty if there is no app associated with the item.
@@ -60,6 +76,9 @@ class ASH_PUBLIC_EXPORT ShelfItemDelegate : public mojom::ShelfItemDelegate {
 
   // Set to true if the launcher item image has been set by the controller.
   bool image_set_by_controller_;
+
+  // The context menu model that was last shown for the associated shelf item.
+  std::unique_ptr<ui::MenuModel> context_menu_;
 
   DISALLOW_COPY_AND_ASSIGN(ShelfItemDelegate);
 };
