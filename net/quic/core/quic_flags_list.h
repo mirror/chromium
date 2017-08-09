0cb0935f9d2c1f7bf545d13f036535498792e774
Ryan Hamilton <rch@chromium.org>
2017-08-09 11:54:01 -0700

    Remove unused QUIC flags.
    Change-Id: I7d24535430c1f375c04a5bcfe2801de969c9addc

--- net/quic/core/quic_flags_list.h
+++ net/quic/core/quic_flags_list.h
@@ -117,12 +117,6 @@ QUIC_FLAG(bool, FLAGS_quic_reloadable_flag_quic_enable_random_padding, true)
 // If enabled, use refactored stream creation methods.
 QUIC_FLAG(bool, FLAGS_quic_reloadable_flag_quic_refactor_stream_creation, false)
 
-// If true, GFEs generate and validate source address token using the actual
-// client IP for proxied session.
-QUIC_FLAG(bool,
-          FLAGS_quic_reloadable_flag_quic_use_client_address_for_stk_in_proxy,
-          true)
-
 // If true, export a varz mapping QUIC non 0-rtt handshake with corresponding
 // frontend service.
 QUIC_FLAG(bool, FLAGS_quic_reloadable_flag_quic_account_handshake, false)
@@ -136,9 +130,6 @@ QUIC_FLAG(bool, FLAGS_quic_reloadable_flag_quic_enable_pcc, false)
 
 // If true, enable QUIC v40.
 QUIC_FLAG(bool, FLAGS_quic_enable_version_40, false)
-
-// If true, use the more CPU efficient bandwidth sampler datastructure.
-QUIC_FLAG(bool, FLAGS_quic_reloadable_flag_quic_faster_bandwidth_sampler, true)
 
 // In QUIC, QuicSession gets notified when stream frames are acked, discarded or
 // retransmitted.
