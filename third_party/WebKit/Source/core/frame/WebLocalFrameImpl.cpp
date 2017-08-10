88438d8e9e0f84b4485d86295ad23498115cc29a
Diana Gage <drgage@google.com>
2017-08-09 21:42:07 -0700

    Add edit commands to plugin context menu.
    This CL adds support for executing "Cut", "Paste", and
    "PasteAndMatchStyle" commands via the mouse and context menu in pepper
    plugins. The plugin's context menu is built differently when focus is
    within an editable text area: editable items are added as options and
    regular page items are not shown.
    
    Currently, the "SelectAll" command is disabled because it has not yet
    been implemented, and spell check and writing direction sub menus are
    not shown because support has not been added for them for plugins.
    
    BUG=59266
    
    Change-Id: Ib181f6dea29be3f524e5e19252ff12e34488ee16

--- third_party/WebKit/Source/core/frame/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/core/frame/WebLocalFrameImpl.cpp
@@ -1033,10 +1033,12 @@ bool WebLocalFrameImpl::ExecuteCommand(const WebString& name) {
     command = command.Substring(0, command.length() - 1);
 
   Node* plugin_lookup_context_node =
-      context_menu_node_ && name == "Copy" ? context_menu_node_ : nullptr;
+      context_menu_node_ ? context_menu_node_ : nullptr;
+
   WebPluginContainerImpl* plugin_container =
       GetFrame()->GetWebPluginContainer(plugin_lookup_context_node);
-  if (plugin_container && plugin_container->ExecuteEditCommand(name))
+  if (plugin_container && plugin_container->SupportsCommand(name) &&
+      plugin_container->ExecuteEditCommand(name))
     return true;
 
   return GetFrame()->GetEditor().ExecuteCommand(command);
