ed98caf67457af811756681f14cd4a6d806dbcf4
Irmak Kavasoğlu <irmakk@google.com>
2017-08-10 15:57:17 +0200

    Enabled username correction by default
    Enabled username correction feature by default and updated tests accordingly.
    
    Bug:734965
    Change-Id: I2c4a61b2d791075193650e49179e24ee6d0c8969

--- chrome/browser/ui/cocoa/passwords/save_pending_password_view_controller_unittest.mm
+++ chrome/browser/ui/cocoa/passwords/save_pending_password_view_controller_unittest.mm
@@ -64,7 +64,7 @@ TEST_F(SavePendingPasswordViewControllerTest,
 }
 
 TEST_F(SavePendingPasswordViewControllerTest,
-       EditButtonExistsWhenUsernameCorrectionEnabled) {
+       EditButtonExistsIfUsernameCorrectionEnabled) {
   base::test::ScopedFeatureList feature_list;
   feature_list.InitAndEnableFeature(
       password_manager::features::kEnableUsernameCorrection);
@@ -75,7 +75,10 @@ TEST_F(SavePendingPasswordViewControllerTest,
 }
 
 TEST_F(SavePendingPasswordViewControllerTest,
-       EditButtonShouldNotExistByDefault) {
+       EditButtonShouldNotExistIfUsernameCorrectionDisabled) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndDisableFeature(
+      password_manager::features::kEnableUsernameCorrection);
   SetUpSavePendingState(false);
   EXPECT_FALSE(controller().editButton);
 }
@@ -188,7 +191,10 @@ TEST_F(SavePendingPasswordViewControllerTest,
 }
 
 TEST_F(SavePendingPasswordViewControllerTest,
-       ShouldNotShowPasswordRowWhenUsernameEmpty) {
+       ShouldNotShowPasswordRowWhenUsernameEmptyAndUsernameCorrectionDisabled) {
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndDisableFeature(
+      password_manager::features::kEnableUsernameCorrection);
   SetUpSavePendingState(true);
   EXPECT_FALSE([controller() createPasswordView]);
 }
