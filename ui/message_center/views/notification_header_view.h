87b75b5f14d19cd2b1b7bee7dfdb488315469b8a
Tetsui Ohkubo <tetsui@chromium.org>
2017-08-10 11:38:47 +0900

    Fix header text corruption in MD notification.
    MD notifiction header label did not show properly when ripple animation
    was running. According to InkDropContainerView description, this ugly
    text rendering happens when paining labels to an opaque canvas with
    subpixel rendering. This can be avoided by using InkDropContainerView.
    
    TEST=manual
    BUG=752422
    
    Change-Id: I21e861e345de6039c142c028386701ebb7875068

--- ui/message_center/views/notification_header_view.h
+++ ui/message_center/views/notification_header_view.h
@@ -48,6 +48,8 @@ class NotificationHeaderView : public views::CustomButton {
   std::unique_ptr<views::InkDropRipple> CreateInkDropRipple() const override;
   std::unique_ptr<views::InkDropHighlight> CreateInkDropHighlight()
       const override;
+  void AddInkDropLayer(ui::Layer* ink_drop_layer) override;
+  void RemoveInkDropLayer(ui::Layer* ink_drop_layer) override;
 
   views::ImageView* expand_button() { return expand_button_; }
   views::ImageButton* settings_button() { return settings_button_; }
@@ -60,6 +62,7 @@ class NotificationHeaderView : public views::CustomButton {
 
   SkColor accent_color_ = message_center::kNotificationDefaultAccentColor;
 
+  views::InkDropContainerView* ink_drop_container_ = nullptr;
   views::Label* app_name_view_ = nullptr;
   views::Label* summary_text_divider_ = nullptr;
   views::Label* summary_text_view_ = nullptr;
