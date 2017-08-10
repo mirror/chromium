8f6e4e8cef3c5762a21ce578bf89ce656a161941
Tarun Bansal <tbansal@chromium.org>
2017-08-10 11:25:16 -0700

    netinfo DNR
    Bug:
    Change-Id: I1c229fe430927e066bb409fab2691ce9265e1160

--- third_party/WebKit/LayoutTests/netinfo/resources/netinfo_common.js
+++ third_party/WebKit/LayoutTests/netinfo/resources/netinfo_common.js
@@ -6,11 +6,17 @@ var initialDownlinkMax = 1.0;
 var newConnectionType = "ethernet";
 var newDownlinkMax = 2.0;
 var initialEffectiveType = "3g";
-var initialRtt = 25.0;
-var initialDownlink = 10.0;
+var initialRtt = 50.0;
+// Up to 10% noise  may be added to rtt and downlink. Use 11% as the buffer
+// below to include any mismatches due to floating point calculations.
+var initialRttMaxNoise = initialRtt * 0.11;
+var initialDownlink = 5.0;
+var initialDownlinkMaxNoise = initialDownlink * 0.11;
 var newEffectiveType = "4g";
 var newRtt = 50.0;
-var newDownlink = 30.0;
+var newRttMaxNoise = newRtt * 0.11;
+var newDownlink = 10.0;
+var newDownlinkMaxNoise = newDownlink * 0.11;
 
 // Suppress connection messages information from the host.
 if (window.internals) {
@@ -25,4 +31,16 @@ if (window.internals) {
 
 function isTypeOnline(type) {
     return type != 'none';
+}
+
+function verifyOnChange(data, type, downlinkMax, effectiveType, rtt, downlink) {
+    var parsed = data.toString().split(",");
+    if(parsed[0] != type)
+        testFailed("type mismatch");
+    if(parsed[2] != effectiveType)
+        testFailed("effectiveType mismatch");
+    if(parsed[3] != rtt)
+        testFailed("rtt mismatch");
+    if(parsed[4] != downlink)
+        testFailed("downlink mismatch");
 }
