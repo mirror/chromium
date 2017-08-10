bdeeb646319fa30f5e3da6a004c4e131dd67406a
Yutaka Hirano <yhirano@chromium.org>
2017-08-10 20:13:42 +0900

    Avoid nested BodyStreamBuffer::ProcessData call
    ReadableStream's controller's enqueue method may call pull
    synchronously. Before this CL it led to a nested ProcessData call which
    confused internal states. This CL suppresses such recursive calls.
    
    Bug: 752400
    Change-Id: I8bcfde9b2ca83d596107178ea92096546c8be8a9

--- third_party/WebKit/Source/modules/fetch/BodyStreamBuffer.cpp
+++ third_party/WebKit/Source/modules/fetch/BodyStreamBuffer.cpp
@@ -19,6 +19,7 @@
 #include "platform/bindings/V8ThrowException.h"
 #include "platform/blob/BlobData.h"
 #include "platform/network/EncodedFormData.h"
+#include "platform/wtf/AutoReset.h"
 
 namespace blink {
 
@@ -209,7 +210,8 @@ ScriptPromise BodyStreamBuffer::pull(ScriptState* script_state) {
   if (stream_needs_more_)
     return ScriptPromise::CastUndefined(script_state);
   stream_needs_more_ = true;
-  ProcessData();
+  if (!in_process_data_)
+    ProcessData();
   return ScriptPromise::CastUndefined(script_state);
 }
 
@@ -311,6 +313,9 @@ void BodyStreamBuffer::CancelConsumer() {
 
 void BodyStreamBuffer::ProcessData() {
   DCHECK(consumer_);
+  DCHECK(!in_process_data_);
+
+  AutoReset<bool> auto_reset(&in_process_data_, true);
   while (stream_needs_more_) {
     const char* buffer = nullptr;
     size_t available = 0;
@@ -318,7 +323,9 @@ void BodyStreamBuffer::ProcessData() {
     if (result == BytesConsumer::Result::kShouldWait)
       return;
     DOMUint8Array* array = nullptr;
+    std::string s;
     if (result == BytesConsumer::Result::kOk) {
+      s = std::string(buffer, available);
       array = DOMUint8Array::Create(
           reinterpret_cast<const unsigned char*>(buffer), available);
       result = consumer_->EndRead(available);
