3bf0534f24bd23213376b39ae041c449159485ec
Hajime Hoshi <hajimehoshi@chromium.org>
2017-08-10 16:11:28 +0900

    Refactoring: Remove an unused argument from CreateSharedMemoryOwnershipEdge
    Now we have switched the CreateSharedMemoryOwnershipEdge implementation
    not to use |client_global_dump_guid|, let's remove this.
    
    Bug: 604726
    Change-Id: I11114760c0c9e4c1b97a071d9ca33bb337a40d6d

--- components/viz/host/server_gpu_memory_buffer_manager.cc
+++ components/viz/host/server_gpu_memory_buffer_manager.cc
@@ -164,11 +164,8 @@ bool ServerGpuMemoryBufferManager::OnMemoryDump(
       uint64_t client_tracing_process_id = ClientIdToTracingId(client_id);
 
       if (buffer_info.type == gfx::SHARED_MEMORY_BUFFER) {
-        auto shared_buffer_guid = gfx::GetSharedMemoryGUIDForTracing(
-            client_tracing_process_id, buffer_id);
-        pmd->CreateSharedMemoryOwnershipEdge(dump->guid(), shared_buffer_guid,
-                                             buffer_info.shared_memory_guid,
-                                             0 /* importance */);
+        pmd->CreateSharedMemoryOwnershipEdge(
+            dump->guid(), buffer_info.shared_memory_guid, 0 /* importance */);
       } else {
         auto shared_buffer_guid = gfx::GetGenericSharedGpuMemoryGUIDForTracing(
             client_tracing_process_id, buffer_id);
