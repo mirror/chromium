121facdd8d197be0386bfea2d74cf3e908a61220
Joseph Arhar <jarhar@google.com>
2017-08-10 13:20:30 -0700

    Add FileTransferProxy and FileTransferProxyFactory
    FileTransferProxy is an interface to be used by
    FileTransferMessageHandler for writing files to disk.
    FileTransferProxyFactory creates FileTransferProxies by passing required
    context for file IO, as shown in the Linux implementation in a
    subsequent change.
    
    Bug:
    Change-Id: I91c7dba22cb65eacbd2ad018b29fe3b124ea6b64

---
+++ remoting/host/file_transfer_proxy_factory.cc
@@ -0,0 +1,13 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/file_transfer_proxy_factory.h"
+
+namespace remoting {
+
+FileTransferProxyFactory::FileTransferProxyFactory() = default;
+
+FileTransferProxyFactory::~FileTransferProxyFactory() = default;
+
+}  // namespace remoting
