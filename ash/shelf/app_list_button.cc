0688c5f280c731cda5766ca1ca243b00d643710d
Sammie Quon <sammiequon@google.com>
2017-08-10 12:50:55 -0700

    shelf: App list button flashes under shelf before animating.
    When moving from tablet mode to normal with a couple maximized windows. The app list button bounces to the bottom before animating as expected. This is because the back button is only displayed when it is tablet mode, or animating. In this case, it is considered as no longer in tablet mode, but the animation has yet to start (maybe because of reattaching former window states) so the app list button circle does a quick "bounce".
    
    Bug:
    Test: ash_unittest "ShelfViewTest"
    Change-Id: I8edfc4d18d0d46ec5f235f9929000bae1bc308cc

--- ash/shelf/app_list_button.cc
+++ ash/shelf/app_list_button.cc
@@ -295,7 +295,7 @@ void AppListButton::PaintButtonContents(gfx::Canvas* canvas) {
   bg_flags.setAntiAlias(true);
   bg_flags.setStyle(cc::PaintFlags::kFill_Style);
 
-  if (is_tablet_mode || current_animation_value > 0.0) {
+  if (is_tablet_mode || shelf_view_->is_tablet_mode_animation_running()) {
     // Draw the tablet mode app list background. It will look something like
     // [1] when the shelf is horizontal and [2] when the shelf is vertical,
     // where 1. is the back button and 2. is the app launcher circle.
@@ -343,14 +343,27 @@ void AppListButton::PaintButtonContents(gfx::Canvas* canvas) {
     gfx::ImageSkia back_button =
         CreateVectorIcon(kShelfBackIcon, SK_ColorTRANSPARENT);
 
-    // Fades the back button in or out. The button is opaque when we are in
-    // tablet mode and the animation has finished.
-    int opacity = current_animation_value > 0.0
-                      ? static_cast<int>((is_tablet_mode
-                                              ? current_animation_value
-                                              : 1.0 - current_animation_value) *
-                                         255.0)
-                      : 255;
+    // Fades the back button icon in or out.
+    // There are three cases/steps:
+    // 1. Enter/exit tablet mode. |is_tablet_mode_animation_running()| is true
+    // at this point, but there is a bit of delay before the app list button
+    // actually starts animating (ie. |current_animation_value| <= 0.0|). At
+    // this point |is_tablet_mode| is the new value, but since the animation has
+    // yet to start, the opacity should match the old value of |is_tablet_mode|
+    // (ie. !is_tablet_mode).
+    // 2. App list button has started animating (ie. |current_animation_value| >
+    // 0.0). Animate 0->255 when entering tablet mode, or 255->0 when exiting.
+    // 3. We are in fully in tablet mode and done animations (the check about
+    // prevents us from being fully in non-tablet mode and done animations), the
+    // button is opaque.
+    int opacity = is_tablet_mode ? 0 : 255;
+    if (current_animation_value > 0.0) {
+      opacity = static_cast<int>(current_animation_value * 255.0);
+      opacity = is_tablet_mode ? opacity : (255 - opacity);
+    } else if (!shelf_view_->is_tablet_mode_animation_running()) {
+      opacity = 255;
+    }
+
     canvas->DrawImageInt(back_button, back_center.x() - back_button.width() / 2,
                          back_center.y() - back_button.height() / 2, opacity);
   } else {
@@ -410,8 +423,7 @@ gfx::Point AppListButton::GetAppListButtonCenterPoint() const {
   const bool is_tablet_mode = Shell::Get()
                                   ->tablet_mode_controller()
                                   ->IsTabletModeWindowManagerEnabled();
-  const bool is_animating =
-      shelf_view_->GetAppListButtonAnimationCurrentValue() > 0.0;
+  const bool is_animating = shelf_view_->is_tablet_mode_animation_running();
 
   ShelfAlignment alignment = shelf_->alignment();
   if (alignment == SHELF_ALIGNMENT_BOTTOM ||
