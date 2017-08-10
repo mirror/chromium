ecac0bd58b4df8ada13e3e04b96fbddb4ca80047
Jeffrey He <jeffreyhe@google.com>
2017-08-10 16:45:53 -0400

    Change ScopedMayBlock to be ScopedWillBlock
    ScopedWillBlock will be added first to SchedulerWorkerPoolImpl instead of
    ScopedMayBlock because there are clearer use cases for ScopedWillBlock
    (in particular, blocking on DNS requests).
    
    Bug: 754265
    Change-Id: I8c7c70408b2dafec2eee8255c6a5b7edf247ad1e

--- base/threading/scoped_may_block.h
+++ base/threading/scoped_may_block.h
@@ -6,30 +6,37 @@
 #define BASE_THREADING_SCOPED_MAY_BLOCK_H
 
 #include "base/base_export.h"
+#include "base/logging.h"
 
 namespace base {
 
-// This class should be instantiated in every scope where a blocking call is
-// made.
+// This class can be instantiated before a blocking call is made. Instantiation
+// will hint the BlockingObserver for this thread about the scope of the
+// blocking operation.
 //
-// Instantiation will hint the BlockingObserver for this thread about the
-// scope of the blocking operation. In particular, on TaskScheduler owned
-// threads, this will allow the thread to be replaced in its pool if the
-// blocking scope doesn't expire shortly.
-class BASE_EXPORT ScopedMayBlock {
+// In particular, on TaskScheduler owned threads, this will allow the thread to
+// be replaced in its pool.
+class BASE_EXPORT ScopedWillBlock {
  public:
-  ScopedMayBlock();
-  ~ScopedMayBlock();
+  ScopedWillBlock();
+  ~ScopedWillBlock();
 };
 
 namespace internal {
 
 // Interface for an observer to be informed when a thread enters or exits
-// the scope of a ScopedMayBlock object.
+// the scope of a ScopedWillBlock object.
 class BASE_EXPORT BlockingObserver {
  public:
   virtual void BlockingScopeEntered() = 0;
   virtual void BlockingScopeExited() = 0;
+
+ private:
+#if DCHECK_IS_ON()
+  bool in_blocked_scope_ = false;
+
+  friend class base::ScopedWillBlock;
+#endif
 };
 
 void SetBlockingObserverForCurrentThread(BlockingObserver* blocking_observer);
