93c1e156b3d5f44494b6c1e5e2328d8efbb6e2bd
Allen Bauer <kylixrd@chromium.org>
2017-08-10 08:34:45 -0700

    Fixed TableViewTest to work in MD mode.
    Bug: 713030
    Change-Id: I6aa86b54e4e5fccadb38d26afae70a70faec897d

--- ui/views/controls/table/table_view_unittest.cc
+++ ui/views/controls/table/table_view_unittest.cc
@@ -220,6 +220,15 @@ class TableViewTest : public ViewsTestBase {
   }
 
   void TearDown() override {
+    // Under Material mode, the focus is represented by a separate "focus ring"
+    // control that is inserted into and removed from the parent control list.
+    // During the destruction process, while the child controls are iterated in
+    // order to be destroyed, the control which has focus will cause the focus
+    // manager to remove the focus ring. This leads to the control list being
+    // changed while also being iterrated, triggering a DCHECK().
+    // Closing the widget first will ensure the focus is cleared prior to
+    // initiating the destruction process.
+    widget_->Close();
     widget_.reset();
     ViewsTestBase::TearDown();
   }
