ee795ce97bb436a803a2aac4dd976a4bc015e5d4
Tarun Bansal <tbansal@chromium.org>
2017-08-09 22:14:08 -0700

    p2s
    Bug:
    Change-Id: I1c229fe430927e066bb409fab2691ce9265e1160

--- third_party/WebKit/LayoutTests/netinfo/resources/netinfo_common.js
+++ third_party/WebKit/LayoutTests/netinfo/resources/netinfo_common.js
@@ -6,11 +6,15 @@ var initialDownlinkMax = 1.0;
 var newConnectionType = "ethernet";
 var newDownlinkMax = 2.0;
 var initialEffectiveType = "3g";
-var initialRtt = 25.0;
-var initialDownlink = 10.0;
+var initialRtt = 50.0;
+var initialRttMaxNoise = 0;
+var initialDownlink = 5.0;
+var initialDownlinkMaxNoise = 0.5;
 var newEffectiveType = "4g";
 var newRtt = 50.0;
-var newDownlink = 30.0;
+var newRttMaxNoise = 0;
+var newDownlink = 10.0;
+var newDownlinkMaxNoise = 1.0;
 
 // Suppress connection messages information from the host.
 if (window.internals) {
@@ -25,4 +29,16 @@ if (window.internals) {
 
 function isTypeOnline(type) {
     return type != 'none';
+}
+
+function verifyOnChange(data, type, downlinkMax, effectiveType, rtt, downlink) {
+    var parsed = data.toString().split(",");
+    if(parsed[0] != type)
+        testFailed("type mismatch");
+    if(parsed[2] != effectiveType)
+        testFailed("effectiveType mismatch");
+    if(Math.abs(parsed[3] -rtt) > 0.2 *rtt)
+        testFailed("rtt mismatch");
+    if(Math.abs(parsed[4] -downlink) > 0.2 *downlink)
+        testFailed("downlink mismatch");
 }
