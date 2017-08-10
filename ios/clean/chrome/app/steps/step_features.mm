2f5f7b07feac0f6fa2c51b64a57f6d4c20215eed
Olivier Robin <olivierrobin@chromium.org>
2017-08-10 16:00:09 +0200

    Refactor startup sequence for Clean Skeleton
    Implement a new startup sequence based on dependencies.
    Each step provides one feature and can depend on a list of feature.
    All dependencies run before the features that depend on it.
    
    Bug:
    Change-Id: I0c5a21f060553625d628ab849d1ad4c0cdbdb5f7

---
+++ ios/clean/chrome/app/steps/step_features.mm
@@ -0,0 +1,19 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/clean/chrome/app/steps/step_features.h"
+
+namespace step_features {
+
+NSString* const kProviders = @"providers";
+NSString* const kBundleAndDefaults = @"bundle-and-defualts";
+NSString* const kChromeMainStarted = @"chrome-main-started";
+NSString* const kChromeMainStopped = @"chrome-main-stopped";
+NSString* const kForeground = @"foreground";
+NSString* const kBrowserState = @"browser-state";
+NSString* const kBrowserStateInitialized = @"browser-state-init";
+NSString* const kMainWindow = @"mainWindow";
+NSString* const kRootCoordinatorStarted = @"rootCoordinatorStarted";
+
+}  // namespace step_features
