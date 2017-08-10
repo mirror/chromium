5e7790e2fad03eb4543392101b18c078b6e7bccd
Michael Wasserman <msw@chromium.org>
2017-08-10 10:14:16 -0700

    mash: Add shelf context menu support.
    Chrome serializes its shelf item context menus for Ash.
    Ash displays the menus and reports invocations to Chrome.
    
    Add an ash-side ShelfContextMenu class that supports:
    -Local menu items to set shelf & wallpaper prefs/settings.
    -Proxies Chrome's menu items via mojo structs/interfaces.
    -Also used for wallpaper/desktop context menus (via RWC).
    
    Extend mojom::ShelfItemDelegate::ExecuteCommand, MenuItem, etc.:
    -Denote the command's origin (context/app menu) and display id.
    -Allow 64-bit int command and radio-group ids; null images.
    -Add ShelfAction::SHELF_ACTION_SHOW_CONTEXT_MENU.
    
    Add common ash::ShelfItemDelegate context menu handling:
    -GetContextMenu() is overridden by delegate subclasses.
    -WillShowContextMenu() handles item right-click selection.
    -ExecuteContextMenuCommand() handles context menu invocation.
    
    Wire up chrome's launcher item subclass context menus.
    Make minor launcher context menu class simplifications.
    Add a ShelfWindowWatcher context menu item for closing.
    
    Convert ShelfButtonPressedMetricTracker from Button to View.
    (to support AfterItemSelected via ShowContextMenuForView)
    
    Remove mus, ash_shell, test, and desktop context menu classes.
    Remove ShellDelegate::CreateContextMenu and ash::ShelfAlignmentMenu.
    Inline CanChangeShelfAlignment; move CanUserModifyShelfAutoHideBehavior.
    Add unit tests, flip TestWallpaperDelegate::CanOpenSetWallpaperPage.
    
    Bug: 640693, 753028
    Test: No Chrome OS shelf/desktop context menu behavior changes.
    Change-Id: I3afb93095b4042dca57d16844c5b05308f06a384

--- ash/public/cpp/shelf_item_delegate.cc
+++ ash/public/cpp/shelf_item_delegate.cc
@@ -4,9 +4,57 @@
 
 #include "ash/public/cpp/shelf_item_delegate.h"
 
+#include "ui/base/models/menu_model.h"
+
 namespace ash {
 
-ShelfItemDelegate::ShelfItemDelegate(const ash::ShelfID& shelf_id)
+namespace {
+
+// Get the mojo menu item type from the MenuModel enum value.
+mojom::MenuItem::Type GetItemTypeForMojo(ui::MenuModel::ItemType type) {
+  switch (type) {
+    case ui::MenuModel::TYPE_COMMAND:
+      return mojom::MenuItem::Type::ITEM;
+    case ui::MenuModel::TYPE_CHECK:
+      return mojom::MenuItem::Type::CHECK;
+    case ui::MenuModel::TYPE_RADIO:
+      return mojom::MenuItem::Type::RADIO;
+    case ui::MenuModel::TYPE_SEPARATOR:
+      return mojom::MenuItem::Type::SEPARATOR;
+    case ui::MenuModel::TYPE_BUTTON_ITEM:
+      NOTIMPLEMENTED() << "Button menu items are not supported.";
+      return mojom::MenuItem::Type::ITEM;
+    case ui::MenuModel::TYPE_SUBMENU:
+      return mojom::MenuItem::Type::SUBMENU;
+  }
+  NOTREACHED();
+  return mojom::MenuItem::Type::ITEM;
+}
+
+// Get a serialized list of mojo MenuItemPtr objects to transport a menu model.
+// NOTE: This does not support button items, some separator types, sublabels,
+// minor text, dynamic items, label fonts, accelerators, visibility, etc.
+MenuItemList GetMenuItemsForMojo(ui::MenuModel* model) {
+  DCHECK(model);
+  MenuItemList items;
+  for (int i = 0; i < model->GetItemCount(); ++i) {
+    mojom::MenuItemPtr item(mojom::MenuItem::New());
+    item->type = GetItemTypeForMojo(model->GetTypeAt(i));
+    item->command_id = model->GetCommandIdAt(i);
+    item->label = model->GetLabelAt(i);
+    item->checked = model->IsItemCheckedAt(i);
+    item->enabled = model->IsEnabledAt(i);
+    item->radio_group_id = model->GetGroupIdAt(i);
+    if (item->type == mojom::MenuItem::Type::SUBMENU)
+      item->submenu = GetMenuItemsForMojo(model->GetSubmenuModelAt(i));
+    items.push_back(std::move(item));
+  }
+  return items;
+}
+
+}  // namespace
+
+ShelfItemDelegate::ShelfItemDelegate(const ShelfID& shelf_id)
     : shelf_id_(shelf_id), binding_(this), image_set_by_controller_(false) {}
 
 ShelfItemDelegate::~ShelfItemDelegate() {}
@@ -21,9 +69,43 @@ MenuItemList ShelfItemDelegate::GetAppMenuItems(int event_flags) {
   return MenuItemList();
 }
 
+std::unique_ptr<ui::MenuModel> ShelfItemDelegate::GetContextMenu(
+    int64_t display_id) {
+  return nullptr;
+}
+
 AppWindowLauncherItemController*
 ShelfItemDelegate::AsAppWindowLauncherItemController() {
   return nullptr;
+}
+
+bool ShelfItemDelegate::WillShowContextMenu(const ui::Event* event,
+                                            int64_t display_id,
+                                            ItemSelectedCallback* callback) {
+  // Help delegate subclasses show a context menu on right-click events.
+  if (event && (event->type() == ui::ET_POINTER_DOWN &&
+                event->flags() == ui::EF_RIGHT_MOUSE_BUTTON)) {
+    context_menu_ = GetContextMenu(display_id);
+    if (context_menu_) {
+      std::move(*callback).Run(SHELF_ACTION_SHOW_CONTEXT_MENU,
+                               GetMenuItemsForMojo(context_menu_.get()));
+      return true;
+    }
+  }
+  return false;
+}
+
+bool ShelfItemDelegate::ExecuteContextMenuCommand(int64_t command_id,
+                                                  int32_t event_flags) {
+  DCHECK(context_menu_);
+  // Help delegate subclasses execute context menu items, may be on a sub-menu.
+  ui::MenuModel* model = context_menu_.get();
+  int index = -1;
+  if (!ui::MenuModel::GetModelAndIndexForCommandId(command_id, &model, &index))
+    return false;
+
+  model->ActivatedAt(index, event_flags);
+  return true;
 }
 
 }  // namespace ash
