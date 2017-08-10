4c98792d0b91c85621b6e4e1168efdd58d61a36e
Andrew Grieve <agrieve@chromium.org>
2017-08-10 15:17:17 -0400

    Android: Add support for Java's try-with-resources via Desugar.
    Bug: 751776
    Change-Id: Ibb33dc5dae89d3f0eb9b44145c0aabbae2e3c21f

--- build/android/gyp/desugar.py
+++ build/android/gyp/desugar.py
@@ -9,6 +9,7 @@ import os
 import shutil
 import sys
 import tempfile
+import zipfile
 
 from util import build_utils
 
@@ -18,26 +19,41 @@ _SRC_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__),
 _DESUGAR_JAR_PATH = os.path.normpath(os.path.join(
     _SRC_ROOT, 'third_party', 'bazel', 'desugar', 'Desugar.jar'))
 
+_DESUGAR_RUNTIME_PREFIX = 'com/google/devtools/build/android/desugar/runtime'
+
 
 def _OnStaleMd5(input_jar, output_jar, classpath, bootclasspath_entry):
-  cmd = [
-      'java',
-      '-jar',
-      _DESUGAR_JAR_PATH,
-      '--input',
-      input_jar,
-      '--bootclasspath_entry',
-      bootclasspath_entry,
-      '--output',
-      output_jar,
-      # Disable try-with-resources due to proguard duplicate zip entry error
-      # TODO(zpeng): Enable try-with-resources with
-      #    desugar_try_with_resources_omit_runtime_classes
-      '--desugar_try_with_resources_if_needed=false',
-  ]
-  for path in classpath:
-    cmd += ['--classpath_entry', path]
-  build_utils.CheckOutput(cmd, print_stdout=False)
+  with tempfile.NamedTemporaryFile(delete=False) as tmp_out:
+    cmd = [
+        'java',
+        '-jar',
+        _DESUGAR_JAR_PATH,
+        '--input',
+        input_jar,
+        '--bootclasspath_entry',
+        bootclasspath_entry,
+        '--output',
+        tmp_out.name,
+    ]
+    for path in classpath:
+      cmd += ['--classpath_entry', path]
+    build_utils.CheckOutput(cmd, print_stdout=False)
+    with zipfile.ZipFile(tmp_out.name) as tmp_zip:
+      all_infos = tmp_zip.infolist()
+      non_desugar_infos = [
+          x for x in all_infos
+          if not x.filename.startswith(_DESUGAR_RUNTIME_PREFIX)]
+      # No try-with-resource files, just move the jar.
+      if len(non_desugar_infos) == len(all_infos):
+        shutil.move(tmp_out.name, output_jar)
+      else:
+        # Strip try-with-resources files from being included in every single
+        # .jar. Instead, they are included once via
+        # //third_party/bazel/desugar:desugar_runtime_java.
+        with zipfile.ZipFile(output_jar, 'w') as out_zip:
+          for info in non_desugar_infos:
+            out_zip.writestr(info, tmp_zip.read(info))
+        os.unlink(tmp_out.name)
 
 
 def main():
