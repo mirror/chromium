45008e779b02de51a696a4e71bed9902e2cb6250
Tina Quach <quacht@google.com>
2017-08-10 15:09:27 -0700

    Add a11y test for EDIT_DICTIONARY route
    Bug: 737739
    Change-Id: I15b489895ba80515c181a9e5df25125491e44e77

---
+++ chrome/test/data/webui/settings/edit_dictionary_a11y_test.js
@@ -0,0 +1,44 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Define accessibility tests for the EDIT_DICTIONARY route.
+ */
+
+/** @const {string} Path to root from chrome/test/data/webui/settings/. */
+var ROOT_PATH = '../../../../../';
+
+// SettingsAccessibilityTest fixture.
+GEN_INCLUDE([
+  ROOT_PATH + 'chrome/test/data/webui/settings/accessibility_browsertest.js',
+]);
+
+AccessibilityTest.define('SettingsAccessibilityTest', {
+  /** @override */
+  name: 'EDIT_DICTIONARY',
+  /** @override */
+  setup: function() {
+    settings.navigateTo(settings.routes.EDIT_DICTIONARY);
+    Polymer.dom.flush();
+  },
+  /** @override */
+  tests: {
+    'Accessible with No Changes': function() {}
+  },
+  /** @override */
+  violationFilter: {
+    'aria-valid-attr': function(nodeResult) {
+      return nodeResult.element.hasAttribute('aria-active-attribute');
+    },
+    'aria-valid-attr-value': function(nodeResult) {
+      var describerId = nodeResult.element.getAttribute('aria-describedby');
+      return  describerId == 'cloudPrintersSecondary' ||
+          (describerId === '' && nodeResult.element.id == 'input');
+    },
+    'button-name': function(nodeResult) {
+      var node = nodeResult.element;
+      return node.classList.contains('icon-expand-more');
+    },
+  },
+});
