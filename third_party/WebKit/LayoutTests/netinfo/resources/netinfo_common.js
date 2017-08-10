182d9df0117f355ce67bf4a33863724638e6582a
Tarun Bansal <tbansal@chromium.org>
2017-08-10 00:23:13 -0700

    p2s
    Bug:
    Change-Id: I1c229fe430927e066bb409fab2691ce9265e1160

--- third_party/WebKit/LayoutTests/netinfo/resources/netinfo_common.js
+++ third_party/WebKit/LayoutTests/netinfo/resources/netinfo_common.js
@@ -6,11 +6,17 @@ var initialDownlinkMax = 1.0;
 var newConnectionType = "ethernet";
 var newDownlinkMax = 2.0;
 var initialEffectiveType = "3g";
-var initialRtt = 25.0;
-var initialDownlink = 10.0;
+var initialRtt = 50.0;
+// Up to 10% noise  may be added to rtt and downlink. Use 11% as the buffer
+// below to include any mismatches due to floating point calculations.
+var initialRttMaxNoise = initialRtt * 0.11;
+var initialDownlink = 5.0;
+var initialDownlinkMaxNoise = initialDownlink * 0.11;
 var newEffectiveType = "4g";
 var newRtt = 50.0;
-var newDownlink = 30.0;
+var newRttMaxNoise = newRtt * 0.11;
+var newDownlink = 10.0;
+var newDownlinkMaxNoise = newDownlink * 0.11;
 
 // Suppress connection messages information from the host.
 if (window.internals) {
@@ -25,4 +31,20 @@ if (window.internals) {
 
 function isTypeOnline(type) {
     return type != 'none';
+}
+
+function verifyOnChange(data, type, downlinkMax, effectiveType, rtt, downlink) {
+    var parsed = data.toString().split(",");
+    if(parsed[0] != type)
+        testFailed("type mismatch");
+    if(parsed[2] != effectiveType)
+        testFailed("effectiveType mismatch");
+    // Up to 10% noise  may be added to rtt and downlink. rtt or downlink from
+    // two different hosts may have a difference of up to 20%. Use 21% as the
+    // buffer below to include any mismatches due to floating point
+    // calculations.
+    if(Math.abs(parsed[3] -rtt) > 0.21 *rtt)
+        testFailed("rtt mismatch");
+    if(Math.abs(parsed[4] -downlink) > 0.21 *downlink)
+        testFailed("downlink mismatch");
 }
