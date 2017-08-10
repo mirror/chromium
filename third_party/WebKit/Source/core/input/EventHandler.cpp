3c2c7956e09f0863a62a34e92abfb9b02f09e97d
Xiaocheng Hu <xiaochengh@chromium.org>
2017-08-10 15:09:21 -0700

    Prune ComputeVisibleSelectionInDOMTreeDeprecated from EventHandler::ShouldShowIBeamForNode
    This patch prunes a call site of the deprecated function by switching to
    use SelectionInDOMTree instead of VisibleSelection.
    
    Since the call site also checks HasEditableStyle(), to ensure that the
    elimination of the layout update doesn't cause side effect, this patch
    also adds a DCHECK of clean layout at the beginning of the function.
    
    Bug: 698633
    Change-Id: Ib9826a4a3bba2ccd3fce96fc8a23ac53be4df9c5

--- third_party/WebKit/Source/core/input/EventHandler.cpp
+++ third_party/WebKit/Source/core/input/EventHandler.cpp
@@ -376,6 +376,9 @@ bool EventHandler::ShouldShowIBeamForNode(const Node* node,
   if (!node)
     return false;
 
+  // Clean layout is needed for accessing layout tree and checking editability.
+  DCHECK(!NeedsLayoutTreeUpdate(*node));
+
   if (LayoutObject* layout_object = node->GetLayoutObject()) {
     PaintLayer* layer = layout_object->EnclosingLayer();
     if (layer->GetScrollableArea() &&
@@ -389,16 +392,12 @@ bool EventHandler::ShouldShowIBeamForNode(const Node* node,
   }
 
   // If a drag may be starting or we're capturing mouse events for a particular
-  // node, don't treat this as a selection. Note calling
-  // ComputeVisibleSelectionInDOMTreeDeprecated may update layout.
+  // node, don't treat this as a selection.
   const bool mouse_selection =
-      !capturing_mouse_events_node_ &&
-      mouse_event_manager_->MousePressed() &&
+      !capturing_mouse_events_node_ && mouse_event_manager_->MousePressed() &&
       GetSelectionController().MouseDownMayStartSelect() &&
       !mouse_event_manager_->MouseDownMayStartDrag() &&
-      !frame_->Selection()
-           .ComputeVisibleSelectionInDOMTreeDeprecated()
-           .IsNone();
+      !frame_->Selection().GetSelectionInDOMTree().IsNone();
   const bool mouse_selects_link = mouse_selection && result.IsOverLink();
 
   return mouse_selects_link || HasEditableStyle(*node);
