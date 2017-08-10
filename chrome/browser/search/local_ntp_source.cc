3f5a448608b64261c0b7c4c9e77329d521c9b0c4
Ondrej Skopek <oskopek@google.com>
2017-08-10 14:03:25 +0200

    [Local NTP Voice] Google base URL + query encoding.
    Use the correct Google base URL for search queries.
    Encode the query before issuing a search.
    
    Bug: 583291
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I2942797480416b8ff95758acd7bebcd5b9e89bff

--- chrome/browser/search/local_ntp_source.cc
+++ chrome/browser/search/local_ntp_source.cc
@@ -34,6 +34,7 @@
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/browser_resources.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/search_engines/search_terms_data.h"
 #include "components/search_engines/template_url_service.h"
 #include "components/search_engines/template_url_service_observer.h"
 #include "components/strings/grit/components_strings.h"
@@ -135,10 +136,11 @@ std::unique_ptr<base::DictionaryValue> GetTranslatedStrings(bool is_google) {
 }
 
 // Returns a JS dictionary of configuration data for the local NTP.
-std::string GetConfigData(bool is_google) {
+std::string GetConfigData(bool is_google, std::string google_base_url) {
   base::DictionaryValue config_data;
   config_data.Set("translatedStrings", GetTranslatedStrings(is_google));
   config_data.SetBoolean("isGooglePage", is_google);
+  config_data.SetString("googleBaseUrl", google_base_url);
 
   bool is_voice_search_enabled =
       base::FeatureList::IsEnabled(features::kVoiceSearchOnLocalNtp);
@@ -245,6 +247,7 @@ class LocalNtpSource::GoogleSearchProviderTracker
 
 LocalNtpSource::LocalNtpSource(Profile* profile)
     : profile_(profile),
+      google_base_url_(""),
       one_google_bar_service_(nullptr),
       one_google_bar_service_observer_(this),
       default_search_provider_is_google_(false),
@@ -271,6 +274,9 @@ LocalNtpSource::LocalNtpSource(Profile* profile)
                    base::Unretained(this)));
     DefaultSearchProviderIsGoogleChanged(
         google_tracker_->DefaultSearchProviderIsGoogle());
+
+    google_base_url_ =
+        template_url_service->search_terms_data().GoogleBaseURLValue();
   }
 }
 
@@ -289,7 +295,7 @@ void LocalNtpSource::StartDataRequest(
   std::string stripped_path = StripParameters(path);
   if (stripped_path == kConfigDataFilename) {
     std::string config_data_js =
-        GetConfigData(default_search_provider_is_google_);
+        GetConfigData(default_search_provider_is_google_, google_base_url_);
     callback.Run(base::RefCountedString::TakeString(&config_data_js));
     return;
   }
@@ -331,9 +337,10 @@ void LocalNtpSource::StartDataRequest(
                            .GetRawDataResource(IDR_LOCAL_NTP_HTML)
                            .as_string();
     std::string config_integrity = base::StringPrintf(
-        kIntegrityFormat, GetIntegritySha256Value(
-                              GetConfigData(default_search_provider_is_google_))
-                              .c_str());
+        kIntegrityFormat,
+        GetIntegritySha256Value(
+            GetConfigData(default_search_provider_is_google_, google_base_url_))
+            .c_str());
     base::ReplaceFirstSubstringAfterOffset(&html, 0, "{{CONFIG_INTEGRITY}}",
                                            config_integrity);
     std::string local_ntp_integrity =
@@ -422,7 +429,8 @@ std::string LocalNtpSource::GetContentSecurityPolicyScriptSrc() const {
   return base::StringPrintf(
       "script-src 'strict-dynamic' 'sha256-%s' 'sha256-%s' 'sha256-%s';",
       GetIntegritySha256Value(
-          GetConfigData(default_search_provider_is_google_io_thread_))
+          GetConfigData(default_search_provider_is_google_io_thread_,
+                        google_base_url_))
           .c_str(),
       LOCAL_NTP_JS_INTEGRITY, VOICE_JS_INTEGRITY);
 }
