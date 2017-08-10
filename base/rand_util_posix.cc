c79f2c4a801261711d0dd85e9bd2ffa3cbb2e7d8
Ken Rockot <rockot@chromium.org>
2017-08-09 17:31:59 -0700

    TLS cache for base::RandBytes on POSIX
    We make lots of calls to this function for small numbers of bytes and
    the cost can add up. This implements a TLS cache for random data so that
    small calls are typically much faster.
    
    BUG=754004
    
    Change-Id: I73b42981cc2b8deb0688174b07c7435e871f25d2

--- base/rand_util_posix.cc
+++ base/rand_util_posix.cc
@@ -13,9 +13,15 @@
 #include "base/files/file_util.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
+#include "base/macros.h"
 #include "base/posix/eintr_wrapper.h"
+#include "base/threading/thread_local.h"
 
 namespace {
+
+// The number of random bytes to cache in TLS when reading small chunks of
+// random numbers.
+constexpr size_t kTlsCacheSize = 4096;
 
 // We keep the file descriptor for /dev/urandom around so we don't need to
 // reopen it (which is expensive), and since we may not even be able to reopen
@@ -45,6 +51,60 @@ class URandomFd {
 
 base::LazyInstance<URandomFd>::Leaky g_urandom_fd = LAZY_INSTANCE_INITIALIZER;
 
+class RandomnessCache {
+ public:
+  RandomnessCache() : urandom_fd_(g_urandom_fd.Pointer()->fd()) {}
+  ~RandomnessCache() = default;
+
+  void GetRandomBytes(void* output, size_t output_length) {
+    size_t num_cached_bytes = num_cached_bytes_remaining();
+    if (output_length <= num_cached_bytes) {
+      memcpy(output, next_cached_bytes(), output_length);
+      cache_index_ += output_length;
+      return;
+    }
+
+    // Copy the remainder of whatever's cached.
+    memcpy(output, next_cached_bytes(), num_cached_bytes);
+    cache_index_ = kTlsCacheSize;
+
+    size_t additional_bytes_needed = output_length - num_cached_bytes;
+    if (additional_bytes_needed < kTlsCacheSize) {
+      // If the number of bytes we're asking for is smaller than the cache size,
+      // go ahead and repopulate the cache now, copying the first
+      // |additional_bytes_needed| to the caller.
+      const bool success = base::ReadFromFD(
+          urandom_fd_, reinterpret_cast<char*>(&cache_[0]), kTlsCacheSize);
+      CHECK(success);
+      cache_index_ = additional_bytes_needed;
+      memcpy(static_cast<uint8_t*>(output) + num_cached_bytes, cache_,
+             additional_bytes_needed);
+    } else {
+      // Otherwise just read from the FD directly and leave the cache empty.
+      const bool success = base::ReadFromFD(
+          urandom_fd_, static_cast<char*>(output) + num_cached_bytes,
+          additional_bytes_needed);
+      CHECK(success);
+    }
+  }
+
+ private:
+  size_t num_cached_bytes_remaining() const {
+    return kTlsCacheSize - cache_index_;
+  }
+
+  const void* next_cached_bytes() const { return &cache_[cache_index_]; }
+
+  const int urandom_fd_;
+  uint8_t cache_[kTlsCacheSize];
+  size_t cache_index_ = kTlsCacheSize;
+
+  DISALLOW_COPY_AND_ASSIGN(RandomnessCache);
+};
+
+base::LazyInstance<base::ThreadLocalPointer<RandomnessCache>>::Leaky
+    g_tls_cache;
+
 }  // namespace
 
 namespace base {
@@ -57,10 +117,12 @@ uint64_t RandUint64() {
 }
 
 void RandBytes(void* output, size_t output_length) {
-  const int urandom_fd = g_urandom_fd.Pointer()->fd();
-  const bool success =
-      ReadFromFD(urandom_fd, static_cast<char*>(output), output_length);
-  CHECK(success);
+  RandomnessCache* cache = g_tls_cache.Get().Get();
+  if (!cache) {
+    cache = new RandomnessCache;
+    g_tls_cache.Get().Set(cache);
+  }
+  cache->GetRandomBytes(output, output_length);
 }
 
 int GetUrandomFD(void) {
