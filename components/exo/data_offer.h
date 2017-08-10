85dc83e2116d38cdf8c061c86964649f722df77e
Daichi Hirono <hirono@chromium.org>
2017-08-10 12:05:40 +0900

    Implement DataOffer methods to update properties
    The CL implement DataOffer.Set... methods which update the property of
    DataOffer and send update events to the client.
    
    BUG=b:31988797
    TEST=DataOfferTest.SendEvents
    Change-Id: I84854698ef7e7f1b0ca56f21446c346d57eab3e0

--- components/exo/data_offer.h
+++ components/exo/data_offer.h
@@ -13,6 +13,10 @@
 #include "base/macros.h"
 #include "ui/base/class_property.h"
 
+namespace ui {
+class OSExchangeData;
+}
+
 namespace exo {
 
 class DataOfferDelegate;
@@ -21,7 +25,7 @@ enum class DndAction;
 // Object representing transferred data offered to a client.
 class DataOffer : public ui::PropertyHandler {
  public:
-  explicit DataOffer(DataOfferDelegate* delegate);
+  DataOffer(DataOfferDelegate* delegate);
   ~DataOffer();
 
   // Accepts one of the offered mime types.
@@ -38,8 +42,17 @@ class DataOffer : public ui::PropertyHandler {
   void SetActions(const base::flat_set<DndAction>& dnd_actions,
                   DndAction preferred_action);
 
+  // Sets drop data.
+  void SetDropData(const ui::OSExchangeData& data);
+
+  // Sets source actions.
+  void SetSourceActions(const base::flat_set<DndAction>& source_actions);
+
  private:
   DataOfferDelegate* const delegate_;
+  base::flat_set<std::string> mime_types_;
+  base::flat_set<DndAction> source_actions_;
+  DndAction dnd_action_;
 
   DISALLOW_COPY_AND_ASSIGN(DataOffer);
 };
