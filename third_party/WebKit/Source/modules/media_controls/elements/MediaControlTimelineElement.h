0d48f5e8e97aebe43d0a7faec07211fe11bdd2cf
Becca Hughes <beccahughes@chromium.org>
2017-08-10 13:42:39 +0100

    Media Controls: Replace painter with CSS.
    Replace MediaControlPainter with CSS. Add some classes to some
    elements to allow styling by state.
    
    BUG=746872
    
    Change-Id: I1c226a85ff133b9bf440925b238abf7810c9482a

--- third_party/WebKit/Source/modules/media_controls/elements/MediaControlTimelineElement.h
+++ third_party/WebKit/Source/modules/media_controls/elements/MediaControlTimelineElement.h
@@ -5,7 +5,7 @@
 #ifndef MediaControlTimelineElement_h
 #define MediaControlTimelineElement_h
 
-#include "modules/media_controls/elements/MediaControlInputElement.h"
+#include "modules/media_controls/elements/MediaControlSegmentedTrackElement.h"
 #include "modules/media_controls/elements/MediaControlTimelineMetrics.h"
 
 namespace blink {
@@ -13,7 +13,8 @@ namespace blink {
 class Event;
 class MediaControlsImpl;
 
-class MediaControlTimelineElement final : public MediaControlInputElement {
+class MediaControlTimelineElement final
+    : public MediaControlSegmentedTrackElement {
  public:
   explicit MediaControlTimelineElement(MediaControlsImpl&);
 
@@ -40,7 +41,12 @@ class MediaControlTimelineElement final : public MediaControlInputElement {
   // simplicity; deliberately ignores pinch zoom's pageScaleFactor).
   int TimelineWidth();
 
+  void RenderBarSegments();
+
   MediaControlTimelineMetrics metrics_;
+
+  double current_time_;
+  double duration_;
 };
 
 }  // namespace blink
