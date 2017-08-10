b8976754eaa605aaf687f174243a1b6b6a01a052
Saman Sami <samans@chromium.org>
2017-08-10 15:40:57 -0400

    viz: Move RenderPass's mojom from cc/ipc to services/viz
    Bug: 722935
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I851700cf9fe8af40c045151615c263afed6234f3

--- services/viz/public/cpp/compositing/render_pass_struct_traits.h
+++ services/viz/public/cpp/compositing/render_pass_struct_traits.h
@@ -7,15 +7,15 @@
 
 #include "base/logging.h"
 #include "cc/ipc/quads_struct_traits.h"
-#include "cc/ipc/render_pass.mojom-shared.h"
 #include "cc/quads/render_pass.h"
+#include "services/viz/public/interfaces/compositing/render_pass.mojom-shared.h"
 #include "ui/gfx/ipc/color/gfx_param_traits.h"
 #include "ui/gfx/mojo/transform_struct_traits.h"
 
 namespace mojo {
 
 template <>
-struct StructTraits<cc::mojom::RenderPassDataView,
+struct StructTraits<viz::mojom::RenderPassDataView,
                     std::unique_ptr<cc::RenderPass>> {
   static cc::RenderPassId id(const std::unique_ptr<cc::RenderPass>& input) {
     DCHECK(input->id);
@@ -71,7 +71,7 @@ struct StructTraits<cc::mojom::RenderPassDataView,
     return input->quad_list;
   }
 
-  static bool Read(cc::mojom::RenderPassDataView data,
+  static bool Read(viz::mojom::RenderPassDataView data,
                    std::unique_ptr<cc::RenderPass>* out);
 };
 
