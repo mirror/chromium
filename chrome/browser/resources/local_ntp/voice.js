507010424159aabfcc252e085f67d0d23af422ce
Ondrej Skopek <oskopek@google.com>
2017-08-10 11:18:41 +0200

    [Local NTP Voice] Google base URL + query encoding.
    Use the correct Google base URL for search queries.
    Encode the query before issuing a search.
    
    Bug: 583291
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I2942797480416b8ff95758acd7bebcd5b9e89bff

--- chrome/browser/resources/local_ntp/voice.js
+++ chrome/browser/resources/local_ntp/voice.js
@@ -222,6 +222,13 @@ voice.speech.finalResult_;
 
 
 /**
+ * Base URL for sending queries to Search. Includes trailing forward slash.
+ * @private {string}
+ */
+voice.speech.googleBaseUrl_;
+
+
+/**
  * The ID for the idle timer.
  * @private {number}
  */
@@ -263,7 +270,8 @@ voice.speech.init = function(configData) {
         'Trying to re-initialize speech when not in UNINITIALIZED state.');
   }
 
-  view.init(speech.handleClickEvent_);
+  speech.googleBaseUrl_ = configData.googleBaseUrl;
+
   // Set translations map.
   speech.messages = {
     audioError: configData.translatedStrings.audioError,
@@ -284,6 +292,7 @@ voice.speech.init = function(configData) {
     waiting: configData.translatedStrings.waiting,
   };
 
+  view.init(speech.handleClickEvent_);
   speech.initWebkitSpeech_();
   speech.reset_();
 };
@@ -641,9 +650,15 @@ voice.speech.submitSpeechQuery_ = function() {
   }
 
   // Getting the speech.speechQuery_ needs to happen before stopping speech.
-  // TODO(oskopek): Encode (and wrap) the speech.speechQuery_.
-  var queryUrl = `https://google.com/search?hl=${getChromeUILanguage()}` +
-      `&q=${speech.speechQuery_}&gs_ivs=1`;
+  /* @const */ var encodedQuery =
+      encodeURIComponent(speech.speechQuery_).replace(/%20/g, '+');
+  /* @const */ var queryUrl = `${speech.googleBaseQuery_}search` +
+      // Actual query.
+      `?q=${encodedQuery}` +
+      // Localization of the search result page.
+      `&hl=${getChromeUILanguage()}` +
+      // Add a parameter to indicate that this request is a voice search.
+      `&gs_ivs=1`;
 
   speech.stop_();
   window.location.href = queryUrl;
