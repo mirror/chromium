c03b0eb5fc733e9046809825c2db859639a2831c
Dominic Cooney <dominicc@chromium.org>
2017-08-10 15:15:22 +0900

    Roll libexpat to 2.2.3
    Bug: 735752
    Change-Id: Ic4b85154f2183b4735b03dad03061f233d84be7a

--- third_party/expat/files/lib/winconfig.h
+++ third_party/expat/files/lib/winconfig.h
@@ -10,14 +10,18 @@
 #ifndef WINCONFIG_H
 #define WINCONFIG_H
 
-#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
-#endif
 #include <windows.h>
 #undef WIN32_LEAN_AND_MEAN
 
 #include <memory.h>
 #include <string.h>
+
+
+#if defined(HAVE_EXPAT_CONFIG_H)  /* e.g. MinGW */
+# include <expat_config.h>
+#else  /* !defined(HAVE_EXPAT_CONFIG_H) */
+
 
 #define XML_NS 1
 #define XML_DTD 1
@@ -28,5 +32,9 @@
 
 /* Windows has memmove() available. */
 #define HAVE_MEMMOVE
+
+
+#endif /* !defined(HAVE_EXPAT_CONFIG_H) */
+
 
 #endif /* ndef WINCONFIG_H */
