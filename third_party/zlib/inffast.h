44da90aea2eb3ef8c452105a38082adf9cce3701
Noel Gordon <noel@chromium.org>
2017-08-10 14:29:51 +1000

    zlib measurement patch
    http://bit.ly/2vopRXB suggests -O2 at least is needed to get fewer
    instructions in zlib holder reader code, so add that (optimize) to
    zlib BUILD.gn here.
    
    Update the ImageDecodeBench() with a rate-based benchmark. Do some
    number of runs (11), each run does a constant number of decodes in
    proportion with the width/height and report minimum and median run
    time, and decode rate in MB/s (output side).
    
    Bug:None
    Change-Id: I1f41fd8ab28b6a07dbb09418b32c392b1b8df680

--- third_party/zlib/inffast.h
+++ third_party/zlib/inffast.h
@@ -8,4 +8,43 @@
    subject to change. Applications should only use zlib.h.
  */
 
+#if !defined(ZLIB_64_BIT_READS)
+
+#define ZLIB_INTERNAL_INFLATE_FAST_MIN_LEFT 258
+#define ZLIB_INTERNAL_INFLATE_FAST_MIN_HAVE 6
+
+#else
+
+/* __x86_64__ is used by Clang and GCC. _M_X64 is used by MSVC.
+   __ppc64__ is used by Clang. __PPC64__ is used by GCC.
+   __aarch64__ is used by Clang and GCC.
+ */
+#if defined(__x86_64__) || defined(_M_X64)
+#define ZLIB_INTERNAL_HAVE_64_BIT_UNALIGNED_LOADS 1
+#define ZLIB_INTERNAL_HAVE_64_BIT_UNALIGNED_LOADS_LITTLE_ENDIAN 1
+#endif
+
+#if defined(__ppc64__) || defined(__PPC__64) || defined(__aarch64__)
+#define ZLIB_INTERNAL_HAVE_64_BIT_UNALIGNED_LOADS 1
+#if defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+#define ZLIB_INTERNAL_HAVE_64_BIT_UNALIGNED_LOADS_LITTLE_ENDIAN 1
+#endif
+#endif
+
+/* If using 64 bit loads, round MIN_LEFT and MIN_HAVE up to a multiple of 8. */
+
+#ifdef ZLIB_INTERNAL_HAVE_64_BIT_UNALIGNED_LOADS
+#define ZLIB_INTERNAL_INFLATE_FAST_MIN_LEFT 264
+#else
+#define ZLIB_INTERNAL_INFLATE_FAST_MIN_LEFT 258
+#endif
+
+#ifdef ZLIB_INTERNAL_HAVE_64_BIT_UNALIGNED_LOADS_LITTLE_ENDIAN
+#define ZLIB_INTERNAL_INFLATE_FAST_MIN_HAVE 8
+#else
+#define ZLIB_INTERNAL_INFLATE_FAST_MIN_HAVE 6
+#endif
+
+#endif
+
 void ZLIB_INTERNAL inflate_fast OF((z_streamp strm, unsigned start));
