27c2dbd44c2fbc6f80adb2bce7c375242b8669b3
Istiaque Ahmed <lazyboy@chromium.org>
2017-08-11 11:03:01 -0700

    [TaskScheduler] Make PackExtensionJob run on sequences.
    It used to run on FILE thread, which is deprecated.
    
    This CL also removes ref-counting from PackExtensionJob, as
    PackExtensionJob::Client should be responsible for keeping
    PackExtensionJob alive.
    
    Change set_asynchronous(bool) -> set_synchronous as async is
    the default, and set_synchronous was always called with false.
    
    Bug: 689520
    Change-Id: I87143df26e7adf28bbbe6fd379a38d17d07cb967

--- chrome/browser/extensions/api/developer_private/developer_private_api.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_api.cc
@@ -902,12 +902,14 @@ void DeveloperPrivatePackDirectoryFunction::OnPackSuccess(
       PackExtensionJob::StandardSuccessMessage(crx_file, pem_file));
   response.status = developer::PACK_STATUS_SUCCESS;
   Respond(OneArgument(response.ToValue()));
+  pack_job_.reset();
   Release();  // Balanced in Run().
 }
 
 void DeveloperPrivatePackDirectoryFunction::OnPackFailure(
     const std::string& error,
     ExtensionCreator::ErrorType error_type) {
+  pack_job_.reset();
   developer::PackDirectoryResponse response;
   response.message = error;
   if (error_type == ExtensionCreator::kCRXExists) {
@@ -919,6 +921,7 @@ void DeveloperPrivatePackDirectoryFunction::OnPackFailure(
     response.status = developer::PACK_STATUS_ERROR;
   }
   Respond(OneArgument(response.ToValue()));
+  pack_job_.reset();
   Release();  // Balanced in Run().
 }
 
@@ -958,8 +961,8 @@ ExtensionFunction::ResponseAction DeveloperPrivatePackDirectoryFunction::Run() {
 
   AddRef();  // Balanced in OnPackSuccess / OnPackFailure.
 
-  // TODO(devlin): Why is PackExtensionJob ref-counted?
-  pack_job_ = new PackExtensionJob(this, root_directory, key_file, flags);
+  pack_job_ =
+      base::MakeUnique<PackExtensionJob>(this, root_directory, key_file, flags);
   pack_job_->Start();
   return RespondLater();
 }
