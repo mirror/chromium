87cdd27573a7391839026e8cf72285e909f74278
Andrey Lushnikov <lushnikov@chromium.org>
2017-08-10 18:00:53 -0700

    DevTools: introduce Browser.getVersion protocol method
    This patch:
    - introduces Browser.getVersion protocol method
    - adds Browser domain for the browser target
    
    R=dgozman, pfeldman
    
    Change-Id: I9a306d415db5e39ca05f10c8f6204f942b6b6aba

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/browser/browser-version.js
@@ -0,0 +1,14 @@
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startBlank('Tests resolving blob to UUID through IO domain.');
+  var response = await dp.Browser.getVersion();
+  check('protocol_version', /^\d+\.\d+$/); // e.g. 1.2
+  check('revision', /^@[0-9abcdef]+$/); // e.g. @7d9cc1464f836e6d8c1ab9396a48c656df153d58
+  check('user_agent', /Chrome/); // e.g. Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.77.34.5 Safari/537.36
+  check('v8_version', /^\d+\.\d+\.\d+$/); // e.g. 6.2.196
+  testRunner.completeTest();
+
+  function check(fieldName, regex) {
+    var status = regex.test(response.result[fieldName]) ? 'OK' : 'FAIL';
+    testRunner.log(`version.${fieldName}: ${status}`);
+  }
+})
