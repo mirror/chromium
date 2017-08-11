fcb15bb0506bf51b0e1f318ace930398bd65190d
Darren Shen <shend@chromium.org>
2017-08-11 14:17:43 +1000

    [CSSparser] Implement stream block parsing for ConsumeFontFaceRule.
    This patch implements the stream parsing of CSS blocks (tokens wrapped
    by '(' or '{' or '['). Stream block parsing solves the following issue:
    
    When we parse with ranges, we consume a block in its entirety and then
    pass that block to another function which will do a second pass over
    that range. This means that even if the block contained invalid syntax,
    the whole block will still be consumed. This allows us to continue
    parsing the rest of the stylesheet without issues (error recovery).
    
    If we simply substituted ranges with streams in this case, we would
    get a different result. When we encounter an error, the rest of the
    block would not be consumed yet.
    
    To fix this, we conceptually change these streams to be 'block streams',
    meaning that they can only parse the inner contents of a single block.
    This means it is an a runtime error to consume a token representing
    an open block, and the stream ends at EOF or if the next token closes
    the block.
    
    To parse nested blocks, you must create a BlockGuard. Upon creation,
    the BlockGuard would advance the stream past the open block token.
    When the BlockGuard goes out of scope, error recovery kicks in and
    we ensure that the rest of the block is consumed using the BlockGuard
    destructor.
    
    We test this on ConsumeFontFaceRule. Unfortunately, some juggling is
    required so that we can have both a block range (for the other parsing
    calls) and a block stream. We had to overload ConsumeFontFaceRule in
    the meantime, but the range overload will be removed in the future.
    
    Bug: 661854
    Change-Id: I4dcc7e20c8eb62c23b589277556960dcb85eca70

--- third_party/WebKit/Source/core/css/parser/CSSParserTokenStream.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSParserTokenStream.cpp
@@ -19,15 +19,30 @@ const CSSParserToken& CSSParserTokenStream::ConsumeIncludingWhitespace() {
   return result;
 }
 
-void CSSParserTokenStream::UncheckedConsumeComponentValue() {
-  unsigned nesting_level = 0;
+void CSSParserTokenStream::UncheckedConsumeComponentValue(
+    unsigned nesting_level) {
   do {
-    const CSSParserToken& token = UncheckedConsume();
+    const CSSParserToken& token = UncheckedConsumeInternal();
     if (token.GetBlockType() == CSSParserToken::kBlockStart)
       nesting_level++;
     else if (token.GetBlockType() == CSSParserToken::kBlockEnd)
       nesting_level--;
-  } while (nesting_level && !AtEnd());
+  } while (nesting_level && !PeekInternal().IsEOF());
+}
+
+const CSSParserToken& CSSParserTokenStream::UncheckedConsumeInternal() {
+  DCHECK_LE(next_index_, tokenizer_.tokens_.size());
+  return tokenizer_.tokens_[next_index_++];
+}
+
+const CSSParserToken& CSSParserTokenStream::PeekInternal() const {
+  if (next_index_ == tokenizer_.tokens_.size()) {
+    // Reached end of token buffer, but might not be end of input.
+    if (tokenizer_.TokenizeSingle().IsEOF())
+      return g_static_eof_token;
+  }
+  DCHECK_LT(next_index_, tokenizer_.tokens_.size());
+  return UncheckedPeek();
 }
 
 }  // namespace blink
