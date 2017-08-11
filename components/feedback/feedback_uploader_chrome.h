f50db754041030d1b221925d7574b9724493a099
Ahmed Fakhry <afakhry@google.com>
2017-08-10 18:23:27 -0700

    Send OAuth token with Feedback report
    Add the authentication header to the feedback report upload request
    to help validate the identity of the user. Make sure only one report
    is sent at a time.
    
    BUG=724676
    
    Change-Id: I3adccc07d93e34ed94b2cb2ae9dd835dcdfac2d7

--- components/feedback/feedback_uploader_chrome.h
+++ components/feedback/feedback_uploader_chrome.h
@@ -8,6 +8,7 @@
 #include "base/macros.h"
 #include "components/feedback/feedback_uploader.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "google_apis/gaia/oauth2_token_service.h"
 
 namespace content {
 class BrowserContext;
@@ -15,19 +16,34 @@ class BrowserContext;
 
 namespace feedback {
 
-class FeedbackUploaderChrome : public FeedbackUploader,
+class FeedbackUploaderChrome : public OAuth2TokenService::Consumer,
+                               public FeedbackUploader,
                                public KeyedService {
  public:
   FeedbackUploaderChrome(
       content::BrowserContext* context,
       scoped_refptr<base::SingleThreadTaskRunner> task_runner);
+  ~FeedbackUploaderChrome() override;
 
  private:
+  // OAuth2TokenService::Consumer:
+  void OnGetTokenSuccess(const OAuth2TokenService::Request* request,
+                         const std::string& access_token,
+                         const base::Time& expiration_time) override;
+  void OnGetTokenFailure(const OAuth2TokenService::Request* request,
+                         const GoogleServiceAuthError& error) override;
+
   // feedback::FeedbackUploader:
   void DispatchReport(scoped_refptr<FeedbackReport> report) override;
 
+  void SendReport(const std::string& access_token);
+
   // Browser context this uploader was created for.
   content::BrowserContext* context_;
+
+  scoped_refptr<FeedbackReport> report_;
+
+  std::unique_ptr<OAuth2TokenService::Request> access_token_request_;
 
   DISALLOW_COPY_AND_ASSIGN(FeedbackUploaderChrome);
 };
