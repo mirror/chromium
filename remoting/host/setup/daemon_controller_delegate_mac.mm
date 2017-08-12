105f5fcae0b19673fd9d8c8e1c27232355c72441
kelvinp@chromium.org <kelvinp@chromium.org>
2017-08-11 17:27:17 -0700

    Change default value for crash reporting opt-in to true
    Change-Id: Ie84c6e94d372ee3ac00fd4567be19aae32fc1faa

--- remoting/host/setup/daemon_controller_delegate_mac.mm
+++ remoting/host/setup/daemon_controller_delegate_mac.mm
@@ -261,7 +261,7 @@ DaemonController::UsageStatsConsent
 DaemonControllerDelegateMac::GetUsageStatsConsent() {
   DaemonController::UsageStatsConsent consent;
   consent.supported = true;
-  consent.allowed = false;
+  consent.allowed = true;
   // set_by_policy is not yet supported.
   consent.set_by_policy = false;
 
