2a67d4b93f41ee644399ee6e3c766bb41437d1a4
Daniele Castagna <dcastagna@chromium.org>
2017-08-12 00:45:35 -0400

    ui: Add gfx::BufferUsage::SCANOUT_VDA_WRITE.
    This adds a buffer usage that produces buffers that can be written from
    hardware video decoder, and can be read by the compositor or, possibly,
    by the display controller.
    
    This is needed for formats like NV12 when the content is produced
    by VAAPI decoders and the buffers are then sent to the compositor.
    
    Additionally, if a KMS framebuffer can't be added for the new buffer,
    the allocation will still succeed, and the buffer can still be used
    for compositing, but not as an HW overlay.
    
    Bug: 683347
    Change-Id: Iab5c23c3e15fc235a375c9889085dda9d4816862

--- ui/gfx/mojo/buffer_types_struct_traits.h
+++ ui/gfx/mojo/buffer_types_struct_traits.h
@@ -128,6 +128,8 @@ struct EnumTraits<gfx::mojom::BufferUsage, gfx::BufferUsage> {
         return gfx::mojom::BufferUsage::SCANOUT;
       case gfx::BufferUsage::SCANOUT_CPU_READ_WRITE:
         return gfx::mojom::BufferUsage::SCANOUT_CPU_READ_WRITE;
+      case gfx::BufferUsage::SCANOUT_VDA_WRITE:
+        return gfx::mojom::BufferUsage::SCANOUT_VDA_WRITE;
       case gfx::BufferUsage::GPU_READ_CPU_READ_WRITE:
         return gfx::mojom::BufferUsage::GPU_READ_CPU_READ_WRITE;
       case gfx::BufferUsage::GPU_READ_CPU_READ_WRITE_PERSISTENT:
@@ -148,6 +150,8 @@ struct EnumTraits<gfx::mojom::BufferUsage, gfx::BufferUsage> {
       case gfx::mojom::BufferUsage::SCANOUT_CPU_READ_WRITE:
         *out = gfx::BufferUsage::SCANOUT_CPU_READ_WRITE;
         return true;
+      case gfx::mojom::BufferUsage::SCANOUT_VDA_WRITE:
+        *out = gfx::BufferUsage::SCANOUT_VDA_WRITE;
       case gfx::mojom::BufferUsage::GPU_READ_CPU_READ_WRITE:
         *out = gfx::BufferUsage::GPU_READ_CPU_READ_WRITE;
         return true;
