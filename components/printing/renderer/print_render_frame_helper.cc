5adfffb32ecdd685740c218207b2e92eb233b281
rbpotter <rbpotter@chromium.org>
2017-08-11 17:06:33 -0700

    Fix PDF flicker in background of Print Preview
    The source PDF flickers in the top left corner of the page behind
    Print Preview any time the preview is regenerated. Fix this by not
    resizing the PDF's frame in PrintRenderFrameHelper.
    
    Bug: 739973
    Change-Id: I5431d291c9c0f65a50a425867cf66295b6225096

--- components/printing/renderer/print_render_frame_helper.cc
+++ components/printing/renderer/print_render_frame_helper.cc
@@ -790,6 +790,11 @@ void PrepareFrameAndViewForPrint::ResizeForPrinting() {
   if (!frame())
     return;
 
+  // PDFs do not need to be resized. Resizing them causes a flicker in the
+  // top left corner behind the preview. See crbug.com/739973.
+  if (PrintingNodeOrPdfFrame(frame(), node_to_print_))
+    return;
+
   // Backup size and offset if it's a local frame.
   blink::WebView* web_view = frame_.view();
   if (blink::WebFrame* web_frame = web_view->MainFrame()) {
@@ -908,6 +913,10 @@ void PrepareFrameAndViewForPrint::CallOnReady() {
 
 void PrepareFrameAndViewForPrint::RestoreSize() {
   if (!frame())
+    return;
+
+  // Do not restore PDFs since they are not resized.
+  if (PrintingNodeOrPdfFrame(frame(), node_to_print_))
     return;
 
   blink::WebView* web_view = frame_.GetFrame()->View();
