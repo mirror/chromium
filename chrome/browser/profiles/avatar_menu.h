858eaaf7f2c7e1f3fabcdd99fa1a149b59f64c44
Xi Cheng <chengx@chromium.org>
2017-08-13 21:23:01 -0700

    Decorate Chrome icon in the taskbar correctly with the Gaia picture
    Each profile has an icon, either one of built-in icons from Chrome, or from a G+
    profile image (Gaia image). In the multi-profile scenario, the Chrome icon in the
    taskbar is decorated with the profile icon.
    
    There have been reports that when a profile's icon is from a G+ profile image, it
    doesn't show up to decorate the Chrome icon sometimes. Instead, the built-in icon
    shows up.
    
    Here is the reason why this happens. The G+ profile image is read from disk
    asynchronously during launching Chrome. Meanwhile, the program thinks there's no
    such image so it uses the built-in icon to decorate the Chrome icon instead.
    
    The correct behavior for the Gaia image decoration should be avoid displaying
    anything until we've tried to load the image. If loading fails (because there is
    no image) then we should fall back to a built-in icon. This CL implements this.
    
    More code details and how this bug was introduced are documented in
    https://docs.google.com/document/d/1f40dVO3ScbjQzqdh9n6c5CZEmhNZXR66HBSV1Ix_8-M/edit#
    
    Bug:654499
    Change-Id: I3716e7fc9eb28cf88cf7c53d356df4fe127f0671

--- chrome/browser/profiles/avatar_menu.h
+++ chrome/browser/profiles/avatar_menu.h
@@ -87,6 +87,14 @@ class AvatarMenu :
     base::FilePath profile_path;
   };
 
+  // The load status of an avatar image.
+  enum class ImageLoadStatus {
+    SUCCESS = 0,
+    USE_DEFAULT_AS_GAIA_LOADING,
+    USE_DEFAULT_AS_GAIA_MISSING,
+    FAIL_AS_PROFILE_DELETED
+  };
+
   // Constructor. |observer| can be NULL. |browser| can be NULL and a new one
   // will be created if an action requires it.
   AvatarMenu(ProfileAttributesStorage* profile_storage,
@@ -98,9 +106,11 @@ class AvatarMenu :
   static bool ShouldShowAvatarMenu();
 
   // Sets |image| to the avatar corresponding to the profile at |profile_path|.
-  // For built-in profile avatars, returns the non-high res version.
-  static void GetImageForMenuButton(const base::FilePath& profile_path,
-                                    gfx::Image* image);
+  // For built-in profile avatars, returns the non-high res version. Returns the
+  // image load status.
+  static ImageLoadStatus GetImageForMenuButton(
+      const base::FilePath& profile_path,
+      gfx::Image* image);
 
   // Opens a Browser with the specified profile in response to the user
   // selecting an item. If |always_create| is true then a new window is created
