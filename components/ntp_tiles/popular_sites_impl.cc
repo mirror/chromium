88fe92a1cd0dea809f53ebe4aac2a2c9b581e939
Friedrich Horschig <fhorschig@chromium.org>
2017-08-14 12:39:59 +0200

    Add tile source for baked-in popular sites
    We often have problems differentiating popular sites from baked-in
    content. The new source will help with that in testing, metrics and
    debugging.
    
    Bug: 742414
    Change-Id: Ifd3df54ee086422e0b47304b0e67a9bf4982a206

--- components/ntp_tiles/popular_sites_impl.cc
+++ components/ntp_tiles/popular_sites_impl.cc
@@ -128,6 +128,10 @@ PopularSites::SitesVector ParseSiteList(const base::ListValue& list) {
                        GURL(large_icon_url), GURL(thumbnail_url));
     item->GetInteger("default_icon_resource",
                      &sites.back().default_icon_resource);
+    int source;
+    if (item->GetInteger("source", &source)) {
+      sites.back().source = static_cast<TileSource>(source);
+    }
   }
   return sites;
 }
@@ -211,6 +215,10 @@ std::unique_ptr<base::ListValue> DefaultPopularSites() {
           ResourceBundle::GetSharedInstance().GetRawDataResource(
               IDR_DEFAULT_POPULAR_SITES_JSON)));
   DCHECK(sites);
+  for (base::Value& site : *sites) {
+    base::DictionaryValue& dict = static_cast<base::DictionaryValue&>(site);
+    dict.SetInteger("source", static_cast<int>(TileSource::BAKED_IN_SITE));
+  }
 #if defined(GOOGLE_CHROME_BUILD)
   int index = 0;
   for (int icon_resource :
@@ -237,6 +245,7 @@ PopularSites::Site::Site(const base::string16& title,
       favicon_url(favicon_url),
       large_icon_url(large_icon_url),
       thumbnail_url(thumbnail_url),
+      source(TileSource::POPULAR),
       default_icon_resource(-1) {}
 
 PopularSites::Site::Site(const Site& other) = default;
