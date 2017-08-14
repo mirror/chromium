d158cc055e91669edbacf67765888f374cab52d2
Eric Stevenson <estevenson@chromium.org>
2017-08-14 15:00:09 -0400

    Android: Support alternate BuildHooks implementations.
    This CL introduces a way for targets to provide alternate
    implementations of BuildHooks.java. The default implementation is still
    used if no other implementation is supplied to an android_apk target
    via build_hooks_impl_deps.
    
    Bug: 695304, 672945
    Change-Id: Iaaf13a4797e2035bcf8bd954d13f4160dbd8608b

--- build/android/buildhooks/java/org/chromium/build/BuildHooks.java
+++ build/android/buildhooks/java/org/chromium/build/BuildHooks.java
@@ -5,27 +5,50 @@
 package org.chromium.build;
 
 /**
- * This class is inserted in build, all Java targets have dependence on it.
+ * This class is inserted in build, all non-testonly Java targets have dependence on it. Used for
+ * hooks needed when bytecode rewriting. Static convenience methods are used to minimize the amount
+ * of code required to be manually generated when bytecode rewriting.
+ *
+ * This class contains default implementations for all methods - these are used when no other
+ * implementation is supplied to an android_apk target (via build_hooks_impl_deps).
  */
-public class BuildHooks {
-    private static Callback<AssertionError> sAssertCallback;
+public abstract class BuildHooks {
+    private static BuildHooksImpl sInstance;
+
+    private static BuildHooks get() {
+        if (sInstance == null) {
+            sInstance = new BuildHooksImpl();
+        }
+        return sInstance;
+    }
+
+    private Callback<AssertionError> mAssertCallback;
+
     /**
      * Handle AssertionError, decide whether throw or report without crashing base on gn arg.
      * This method is inserted to handle any assert failure by java_assertion_enabler.
      */
-    public static void assertFailureHandler(AssertionError assertionError) {
-        if (sAssertCallback != null) {
-            sAssertCallback.run(assertionError);
+    protected void assertFailureHandlerImpl(AssertionError assertionError) {
+        if (mAssertCallback != null) {
+            mAssertCallback.run(assertionError);
         } else {
             throw assertionError;
         }
+    }
+
+    public static void assertFailureHandler(AssertionError assertionError) {
+        get().assertFailureHandlerImpl(assertionError);
     }
 
     /**
      * Set the callback function that handles assert failure.
      * This should be called from attachBaseContext.
      */
+    protected void setAssertCallbackImpl(Callback<AssertionError> callback) {
+        mAssertCallback = callback;
+    }
+
     public static void setAssertCallback(Callback<AssertionError> callback) {
-        sAssertCallback = callback;
+        get().setAssertCallbackImpl(callback);
     }
 }
