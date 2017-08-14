1539230b04bcdb4e67e58184891156b5c2c2d11b
Scott Graham <scottmg@chromium.org>
2017-08-14 09:30:43 -0700

    fuchsia: Ensure image root common prefix is actually a directory
    In making a small repro binary, the dependencies ended up being:
    ['.../out/fuchd/libbase_something.so', '.../out/fuchd/libc.so'].
    
    The intention for common_prefix was to be the common path, but by using
    os.path.commonprefix, we can end up with '.../out/fuchd/lib' in this
    unlikely circumstance. Patch this up, and assert that we really got a
    directory.
    
    Bug: 738275
    Change-Id: I73d7993afaa248e23b280828749adfd711ceecc2

--- build/fuchsia/runner_common.py
+++ build/fuchsia/runner_common.py
@@ -129,6 +129,10 @@ def BuildBootfs(output_directory, runtime_deps, bin_name, child_args,
   common_prefix = '/'
   if len(locations_to_add) > 1:
     common_prefix = os.path.commonprefix(locations_to_add)
+  if not os.path.isdir(common_prefix):
+    common_prefix = os.path.split(common_prefix)[0]
+  assert os.path.isdir(common_prefix)
+
   target_source_pairs = zip(
       [MakeTargetImageName(common_prefix, output_directory, loc)
        for loc in locations_to_add],
