57031c152e6076e6b7d1aafd076267398d3c4682
Nicolas Dossou-gbete <dgn@chromium.org>
2017-08-14 14:18:53 +0100

    [Suggestions] Add modern styles for sign in promo
    Adds styles to be used for modern cards and uses them in the
    existing one and for the new style of sign in promo. Additionally,
    sign in promos are now at the top in Modern.
    
    Bug: 754186
    Change-Id: I58600912a0c39385a502c7daaaf37cc614f882ab

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/snippets/ArticleSnippetsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/snippets/ArticleSnippetsTest.java
@@ -28,6 +28,7 @@ import org.chromium.base.annotations.SuppressFBWarnings;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RetryOnFailure;
+import org.chromium.base.test.util.ScreenShooter;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.base.test.util.parameter.CommandLineParameter;
 import org.chromium.chrome.R;
@@ -41,6 +42,7 @@ import org.chromium.chrome.browser.favicon.LargeIconBridge;
 import org.chromium.chrome.browser.ntp.ContextMenuManager;
 import org.chromium.chrome.browser.ntp.ContextMenuManager.TouchEnabledDelegate;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageAdapter;
+import org.chromium.chrome.browser.ntp.cards.SignInPromo;
 import org.chromium.chrome.browser.ntp.cards.SuggestionsCategoryInfo;
 import org.chromium.chrome.browser.offlinepages.OfflinePageBridge;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -112,6 +114,7 @@ public class ArticleSnippetsTest {
 
     private FrameLayout mContentView;
     private SnippetArticleViewHolder mSuggestion;
+    private SignInPromo.ViewHolder mSigninPromo;
 
     private UiConfig mUiConfig;
 
@@ -251,6 +254,35 @@ public class ArticleSnippetsTest {
             }
         });
         mRenderTestRule.render(mSuggestion.itemView, "download_snippet_thumbnail");
+    }
+
+    @Test
+    @MediumTest
+    @Feature({"ArticleSnippets", "RenderTest"})
+    @ScreenShooter.Directory("Signin Promo")
+    @CommandLineParameter({"", "enable-features=" + ChromeFeatureList.CHROME_HOME_MODERN_LAYOUT})
+    public void testSigninPromo() throws IOException {
+        ThreadUtils.runOnUiThreadBlocking(() -> {
+            mContentView = new FrameLayout(mActivityTestRule.getActivity());
+            mUiConfig = new UiConfig(mContentView);
+
+            mActivityTestRule.getActivity().setContentView(mContentView);
+
+            mRecyclerView = new SuggestionsRecyclerView(mActivityTestRule.getActivity());
+            TouchEnabledDelegate touchEnabledDelegate =
+                    enabled -> mRecyclerView.setTouchEnabled(enabled);
+            ContextMenuManager contextMenuManager =
+                    new ContextMenuManager(mActivityTestRule.getActivity(),
+                            mUiDelegate.getNavigationDelegate(), touchEnabledDelegate);
+            mRecyclerView.init(mUiConfig, contextMenuManager);
+            mRecyclerView.setAdapter(mAdapter);
+
+            mSigninPromo = new SignInPromo.ViewHolder(mRecyclerView, contextMenuManager, mUiConfig);
+            mSigninPromo.onBindViewHolder(new SignInPromo(mUiDelegate));
+            mContentView.addView(mSigninPromo.itemView);
+        });
+
+        mRenderTestRule.render(mSigninPromo.itemView, "signin_promo");
     }
 
     private void setupTestData(Bitmap thumbnail) {
