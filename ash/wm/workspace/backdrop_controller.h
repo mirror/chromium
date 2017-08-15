57ce9a991843d1c8ead61e8c7c4d3e5d679ad7b1
MinChen <minch@chromium.org>
2017-08-14 18:55:38 -0700

    Disable backdrop if fullscreen app list is opened in tablet mode.
    If fullscreen app list is opened, the shelf should change to be transparent.
    But since there is a fullscreen backdrop window in tablet mode. The shelf area
    is still black after app list is opened.
    
    Change:
    Add OnAppListVisibilityChanged(*) in BackdropController. Hide backdrop if app list
    is opened, update it if app list is dismissed.
    
    Bug: 752383
    Change-Id: Ic1be66eb5cb9b0a4d126d8e8d78aa2f229b824b8

--- ash/wm/workspace/backdrop_controller.h
+++ ash/wm/workspace/backdrop_controller.h
@@ -62,6 +62,8 @@ class BackdropController : public ShellObserver, public AccessibilityObserver {
   void OnOverviewModeEnded() override;
   void OnSplitViewModeStarting() override;
   void OnSplitViewModeEnded() override;
+  void OnAppListVisibilityChanged(bool shown,
+                                  aura::Window* root_window) override;
 
   // AccessibilityObserver:
   void OnAccessibilityModeChanged(
@@ -85,6 +87,15 @@ class BackdropController : public ShellObserver, public AccessibilityObserver {
   // Hide the backdrop window.
   void Hide();
 
+  // Plus one to |force_hidden_counter_| and then update backdrop state.
+  void AddForceHidden();
+
+  // Minus one to |force_hidden_counter_| and then update backdrop state.
+  void RemoveForceHidden();
+
+  // Hide or update backdrop according to the counter.
+  void HideOrUpdateBackdropToCounter();
+
   // The backdrop which covers the rest of the screen.
   views::Widget* backdrop_ = nullptr;
 
@@ -103,9 +114,9 @@ class BackdropController : public ShellObserver, public AccessibilityObserver {
   // If true, the |RestackOrHideWindow| might recurse.
   bool in_restacking_ = false;
 
-  // True to temporarily hide the backdrop. Used in
-  // overview mode.
-  bool force_hidden_ = false;
+  // Hide the backdrop if the counter is larger than 0. The counter is
+  // maintained by overview mode, split view and app list visibility state.
+  int force_hidden_counter_ = 0;
 
   DISALLOW_COPY_AND_ASSIGN(BackdropController);
 };
