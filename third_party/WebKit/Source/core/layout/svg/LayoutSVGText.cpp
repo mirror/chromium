f7c3a10d39d3864a54ea5d5f8b9231f75699c2e7
Stephen Chenney <schenney@chromium.org>
2017-08-15 12:16:47 -0400

    Update SVG Text positioning values on metrics change
    When font metrics change it can change the positioning
    of child tspan elements within an SVG text element. So
    update positions when the font changes.
    
    R=fs@opera.com
    BUG=755518
    
    Change-Id: I6a8686e4bcdd14e820eba8770aa9c682146b467c

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGText.cpp
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGText.cpp
@@ -201,6 +201,7 @@ void LayoutSVGText::UpdateLayout() {
   // update them before updating the layout attributes.
   if (needs_text_metrics_update_) {
     UpdateFontAndMetrics(*this);
+    needs_positioning_values_update_ = true;
     needs_text_metrics_update_ = false;
     update_parent_boundaries = true;
   }
