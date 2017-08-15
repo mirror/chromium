f0db5129ccf36384dc28f3e2f3e532aa2b299828
Antonio Gomes <tonikitoo@igalia.com>
2017-08-14 23:35:23 -0400

    Factor Services related logic out of BrowserProcessPlatforPart{chromeos}
    CL introduces a BrowserProcessPlatformPartMus class, factors the MUS logic
    in ::RegisterInProcessServices out of BrowserProcessPlatforPart{chromeos}
    into it, and makes the later inherit from the former.
    
    For ChromeOS there is no behavior change (both --mash and --mus continue
    with the same process/service model), but this refactor allows platforms
    other than ChromeOS more easily benefit from the same in-process UI as
    chromeos/mus, e.g. Ozone+Mus+Linux.
    
    TEST=None
    BUG=722527
    
    Change-Id: Ic7a24778b3c57cc0c8620b83bcf555a5a98f5bf8

--- chrome/browser/browser_process_platform_part_chromeos.h
+++ chrome/browser/browser_process_platform_part_chromeos.h
@@ -12,7 +12,7 @@
 #include "base/containers/flat_set.h"
 #include "base/macros.h"
 #include "base/sequence_checker.h"
-#include "chrome/browser/browser_process_platform_part_base.h"
+#include "chrome/browser/browser_process_platform_part_mus.h"
 
 namespace chromeos {
 class ChromeSessionManager;
@@ -37,13 +37,12 @@ class BrowserPolicyConnectorChromeOS;
 }
 
 namespace ui {
-class ImageCursorsSet;
 class InputDeviceControllerClient;
 }
 
 class ScopedKeepAlive;
 
-class BrowserProcessPlatformPart : public BrowserProcessPlatformPartBase {
+class BrowserProcessPlatformPart : public BrowserProcessPlatformPartMus {
  public:
   BrowserProcessPlatformPart();
   ~BrowserProcessPlatformPart() override;
@@ -99,13 +98,9 @@ class BrowserProcessPlatformPart : public BrowserProcessPlatformPartBase {
   void StartTearDown() override;
   std::unique_ptr<policy::BrowserPolicyConnector> CreateBrowserPolicyConnector()
       override;
-  void RegisterInProcessServices(
-      content::ContentBrowserClient::StaticServiceMap* services) override;
 
   chromeos::system::SystemClock* GetSystemClock();
   void DestroySystemClock();
-
-  void DestroyImageCursorsSet();
 
   void AddCompatibleCrOSComponent(const std::string& name);
 
@@ -142,9 +137,6 @@ class BrowserProcessPlatformPart : public BrowserProcessPlatformPartBase {
   std::unique_ptr<ScopedKeepAlive> keep_alive_;
 
   base::flat_set<std::string> compatible_cros_components_;
-
-  // Used by the UI Service.
-  std::unique_ptr<ui::ImageCursorsSet> image_cursors_set_;
 
 #if defined(USE_OZONE)
   std::unique_ptr<ui::InputDeviceControllerClient>
