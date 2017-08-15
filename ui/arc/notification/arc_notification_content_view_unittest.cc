10ec8e8baf3ad85a0446f3dc5c0e3f1fbe3e57c0
Evan Stade <estade@chromium.org>
2017-08-14 19:48:44 -0600

    Remove a couple ui/arc -> ui/message_center dependencies.
    arc doesn't need to know about PaddedButton or ToastContentsView.
    
    Bug: 755413
    Change-Id: I89e02cad77134d8fbf651b2f64ed17704b5731f0

--- ui/arc/notification/arc_notification_content_view_unittest.cc
+++ ui/arc/notification/arc_notification_content_view_unittest.cc
@@ -218,8 +218,7 @@ class ArcNotificationContentViewTest : public views::ViewsTestBase {
     auto* control_buttons_view =
         GetArcNotificationContentView()->control_buttons_view_;
     ASSERT_TRUE(control_buttons_view);
-    message_center::PaddedButton* close_button =
-        control_buttons_view->close_button();
+    views::Button* close_button = control_buttons_view->close_button();
     ASSERT_NE(nullptr, close_button);
     control_buttons_view->ButtonPressed(close_button, dummy_event);
   }
