10345e0379bcef7e34338ce2eee339b6a1d38860
Xi Han <hanxi@google.com>
2017-08-15 14:05:48 -0400

    New WebApk Install UI [3/4]
    This CL includes the following UI changes:
    1) Add to homescreen from menu will show a dialog and won't trigger an infobar.
    2) After user clicks "Add" button from the app banner, the banner
       disappears.
    3) Show install-in-progress notification.
    
    It simplifies existing logic:
    1) Simplify the logic to show the dialog in AddToHomescreenManager and data
       fetcher.
    3) Remove unnecessary logic for adding from menu in AppbanerInfobarDelegate.
    
    See video: https://drive.google.com/open?id=0B7zEF5GgyYmpcERZSnFtZjdhMms
    
    Bug:752338
    Change-Id: Ie2a173f4bedb52382448a6a7fa383c2621dc7cc3

--- chrome/browser/android/webapk/webapk_install_service.cc
+++ chrome/browser/android/webapk/webapk_install_service.cc
@@ -4,10 +4,16 @@
 
 #include "chrome/browser/android/webapk/webapk_install_service.h"
 
+#include "base/android/jni_android.h"
+#include "base/android/jni_string.h"
 #include "base/bind.h"
+#include "base/memory/ptr_util.h"
+#include "chrome/browser/android/shortcut_helper.h"
 #include "chrome/browser/android/shortcut_info.h"
 #include "chrome/browser/android/webapk/webapk_install_service_factory.h"
 #include "chrome/browser/android/webapk/webapk_installer.h"
+#include "jni/WebApkInstallService_jni.h"
+#include "ui/gfx/android/java_bitmap.h"
 
 // static
 WebApkInstallService* WebApkInstallService::Get(
@@ -26,19 +32,27 @@ bool WebApkInstallService::IsInstallInProgress(const GURL& web_manifest_url) {
   return installs_.count(web_manifest_url);
 }
 
-void WebApkInstallService::InstallAsync(const ShortcutInfo& shortcut_info,
+void WebApkInstallService::InstallAsync(content::WebContents* web_contents,
+                                        const ShortcutInfo& shortcut_info,
                                         const SkBitmap& primary_icon,
                                         const SkBitmap& badge_icon,
-                                        const FinishCallback& finish_callback) {
-  DCHECK(!IsInstallInProgress(shortcut_info.manifest_url));
+                                        webapk::InstallSource install_source) {
+  if (IsInstallInProgress(shortcut_info.manifest_url)) {
+    ShortcutHelper::ShowWebApkInstallInProgressToast();
+    return;
+  }
 
+  webapk::TrackInstallSource(install_source);
   installs_.insert(shortcut_info.manifest_url);
 
+  ShowInstallNotification(shortcut_info, primary_icon, std::string());
+
+  auto observer = base::MakeUnique<Observer>(web_contents);
   WebApkInstaller::InstallAsync(
       browser_context_, shortcut_info, primary_icon, badge_icon,
       base::Bind(&WebApkInstallService::OnFinishedInstall,
-                 weak_ptr_factory_.GetWeakPtr(), shortcut_info.manifest_url,
-                 finish_callback));
+                 weak_ptr_factory_.GetWeakPtr(), base::Passed(&observer),
+                 shortcut_info, primary_icon));
 }
 
 void WebApkInstallService::UpdateAsync(
@@ -53,11 +67,61 @@ void WebApkInstallService::UpdateAsync(
 }
 
 void WebApkInstallService::OnFinishedInstall(
-    const GURL& web_manifest_url,
-    const FinishCallback& finish_callback,
+    std::unique_ptr<Observer> web_contents_observer,
+    const ShortcutInfo& shortcut_info,
+    const SkBitmap& primary_icon,
     WebApkInstallResult result,
     bool relax_updates,
     const std::string& webapk_package_name) {
-  finish_callback.Run(result, relax_updates, webapk_package_name);
-  installs_.erase(web_manifest_url);
+  installs_.erase(shortcut_info.manifest_url);
+
+  if (result == WebApkInstallResult::SUCCESS) {
+    ShowInstallNotification(shortcut_info, primary_icon, webapk_package_name);
+    return;
+  }
+
+  JNIEnv* env = base::android::AttachCurrentThread();
+  base::android::ScopedJavaLocalRef<jstring> java_manifest_url =
+      base::android::ConvertUTF8ToJavaString(env,
+                                             shortcut_info.manifest_url.spec());
+  Java_WebApkInstallService_cancelNotification(env, java_manifest_url);
+
+  content::WebContents* web_contents = web_contents_observer->web_contents();
+  if (web_contents == nullptr)
+    return;
+
+  // If the install didn't definitely fail, we don't add a shortcut. This
+  // could happen if Play was busy with another install and this one is still
+  // queued (and hence might succeed in the future).
+  ShortcutHelper::AddToLauncherWithSkBitmap(web_contents, shortcut_info,
+                                            primary_icon);
+}
+
+// static
+void WebApkInstallService::ShowInstallNotification(
+    const ShortcutInfo& shortcut_info,
+    const SkBitmap& primary_icon,
+    const std::string& webapk_package_name) {
+  JNIEnv* env = base::android::AttachCurrentThread();
+  base::android::ScopedJavaLocalRef<jstring> java_manifest_url =
+      base::android::ConvertUTF8ToJavaString(env,
+                                             shortcut_info.manifest_url.spec());
+  base::android::ScopedJavaLocalRef<jstring> java_title =
+      base::android::ConvertUTF16ToJavaString(env, shortcut_info.short_name);
+  base::android::ScopedJavaLocalRef<jstring> java_url =
+      base::android::ConvertUTF8ToJavaString(env, shortcut_info.url.spec());
+  base::android::ScopedJavaLocalRef<jobject> java_primary_icon =
+      gfx::ConvertToJavaBitmap(&primary_icon);
+
+  if (webapk_package_name.empty()) {
+    Java_WebApkInstallService_showInstallStartNotification(
+        env, java_manifest_url, java_title, java_url, java_primary_icon);
+    return;
+  }
+
+  base::android::ScopedJavaLocalRef<jstring> java_webapk_package =
+      base::android::ConvertUTF8ToJavaString(env, webapk_package_name);
+  Java_WebApkInstallService_showInstalledNotification(
+      env, java_webapk_package, java_manifest_url, java_title, java_url,
+      java_primary_icon);
 }
