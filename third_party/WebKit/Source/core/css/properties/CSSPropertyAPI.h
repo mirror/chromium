cd600f4923f3b0b1c11c777d49dd28f58f5e7f8a
Renee Wright <rjwright@chromium.org>
2017-08-15 12:18:05 +1000

    [Ribbon][WIP] Move CSSPropertyMetadata::IsInterpolableProperty to CSSPropertyAPI
    This is the first method from CSSPropertyMetadata to be ribbonized.
    
    Depends on chromium-review.googlesource.com/c/603029
    
    Bug: 751373
    Change-Id: Id2ee483afbc39b3d3edb7b20af2b09a64f4fb795

--- third_party/WebKit/Source/core/css/properties/CSSPropertyAPI.h
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPI.h
@@ -31,6 +31,8 @@ class CSSPropertyAPI {
                               const CSSParserContext&,
                               bool use_legacy_parsing,
                               HeapVector<CSSProperty, 256>&) const;
+
+  virtual bool IsInterpolableProperty() const { return false; }
 };
 
 }  // namespace blink
