f38d1d0ba886f3cfa5f8ebcb37a4712e0af4cf5e
Zhenyao Mo <zmo@chromium.org>
2017-08-15 13:26:30 -0700

    Hook up driver bug workaround's platform dependent disabled extensions.
    BUG=744658
    TEST=gl_unittests
    R=piman@chromium.org,kbr@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ic511c7dcde2af29adb90ec30e3f82c25152a5924

--- ui/gl/generate_bindings.py
+++ ui/gl/generate_bindings.py
@@ -2438,6 +2438,8 @@ def GenerateHeader(file, functions, set_name,
 #ifndef UI_GL_GL_BINDINGS_AUTOGEN_%(name)s_H_
 #define UI_GL_GL_BINDINGS_AUTOGEN_%(name)s_H_
 
+#include <string>
+
 namespace gl {
 
 class GLContext;
@@ -2474,6 +2476,9 @@ class GLContext;
  public:
   %(name)sApi();
   virtual ~%(name)sApi();
+
+  virtual void SetDisabledExtensions(
+      const std::string& disabled_extensions) {}
 
 """ % {'name': set_name.upper()})
   for func in functions:
@@ -2687,9 +2692,12 @@ namespace gl {
       if options.validate_bindings:
         if not 'is_optional' in func or not func['is_optional']:
           file.write('DCHECK(fn.%sFn);\n' % known_as)
-      file.write('}\n')
+      file.write('} ')
       i += 1
       first_version = False
+    file.write("""else {\n
+    fn.%sFn = nullptr;\n
+  }\n""" % known_as)
 
   # TODO(jmadill): make more robust
   def IsClientExtensionFunc(func):
