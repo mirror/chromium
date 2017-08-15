4fd8bbda50aa16abc8809958067397b0d0241cbb
Shuhei Takahashi <nya@chromium.org>
2017-08-15 18:02:13 +0900

    Update the extension list to watch for Android N.
    Raw images and QuickTime images are added.
    
    Bug: 753524
    Test: unit_tests
    Test: Downloaded .nef image is shown in Media View
    Change-Id: Ifc9b0f5a4fe8ff5280e26a3b565010815ce403e6

--- chrome/browser/chromeos/arc/downloads_watcher/arc_downloads_watcher_service_unittest.cc
+++ chrome/browser/chromeos/arc/downloads_watcher/arc_downloads_watcher_service_unittest.cc
@@ -9,6 +9,16 @@
 
 namespace arc {
 
+TEST(ArcDownloadsWatcherServiceTest, AndroidSupportedMediaExtensionsSorted) {
+  const auto less_comparator = [](const char* a, const char* b) {
+    return strcmp(a, b) < 0;
+  };
+  EXPECT_TRUE(std::is_sorted(
+      kAndroidSupportedMediaExtensions,
+      kAndroidSupportedMediaExtensions + kAndroidSupportedMediaExtensionsSize,
+      less_comparator));
+}
+
 TEST(ArcDownloadsWatcherServiceTest, HasAndroidSupportedMediaExtension) {
   EXPECT_TRUE(HasAndroidSupportedMediaExtension(
       base::FilePath(FILE_PATH_LITERAL("/tmp/kitten.3g2"))));
