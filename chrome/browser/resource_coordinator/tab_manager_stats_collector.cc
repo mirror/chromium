54b2df81c066c960c2302c50ff68fdb021a9ec74
Duc Bui <ducbui@google.com>
2017-08-15 20:49:00 -0700

    [TabManager] Record EQT metrics
    This CL records Task Expected Queueing Time UMA in
    TabManagerStatsCollector.
    
    Bug: 737817
    Change-Id: I16b1a46b02a419ed5c9f2241d84a44c6a73fff20

--- chrome/browser/resource_coordinator/tab_manager_stats_collector.cc
+++ chrome/browser/resource_coordinator/tab_manager_stats_collector.cc
@@ -79,6 +79,22 @@ void TabManagerStatsCollector::RecordSwitchToTab(
   }
 }
 
+void TabManagerStatsCollector::RecordExpectedTaskQueueingDuration(
+    content::WebContents* contents,
+    int64_t queueing_time_milliseconds) {
+  if (tab_manager_->IsSessionRestoreLoadingTabs() && contents->IsVisible()) {
+    UMA_HISTOGRAM_TIMES(
+        kHistogramSessionRestoreForegroundTabExpectedTaskQueueingDuration,
+        base::TimeDelta::FromMilliseconds(queueing_time_milliseconds));
+  }
+
+  if (tab_manager_->IsLoadingBackgroundTabs() && contents->IsVisible()) {
+    UMA_HISTOGRAM_TIMES(
+        kHistogramBackgroundTabOpenForegroundTabExpectedTaskQueueingDuration,
+        base::TimeDelta::FromMilliseconds(queueing_time_milliseconds));
+  }
+}
+
 void TabManagerStatsCollector::OnSessionRestoreStartedLoadingTabs() {
   DCHECK(!is_session_restore_loading_tabs_);
   if (session_restore_swap_metrics_driver_)
@@ -135,5 +151,16 @@ void TabManagerStatsCollector::OnSessionRestoreUpdateMetricsFailed() {
   // metrics for session restore.
   session_restore_swap_metrics_driver_.reset();
 }
+
+// static
+const char TabManagerStatsCollector::
+    kHistogramSessionRestoreForegroundTabExpectedTaskQueueingDuration[] =
+        "TabManager.SessionRestore.ForegroundTab.ExpectedTaskQueueingDuration";
+
+// static
+const char TabManagerStatsCollector::
+    kHistogramBackgroundTabOpenForegroundTabExpectedTaskQueueingDuration[] =
+        "TabManager.BackgroundTabOpen.ForegroundTab."
+        "ExpectedTaskQueueingDuration";
 
 }  // namespace resource_coordinator
