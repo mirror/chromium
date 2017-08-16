63392d4c1b5f86d29858154edbaa8ea70765007e
Peter K. Lee <pkl@chromium.org>
2017-08-15 18:48:41 -0700

    Adds a feature flag to disable mailto: URL rewrite
    This feature defaults to ENABLED. Adding a feature flag allows a single
    change of default from ENABLED to DISABLED if we ever need to turn off
    this feature. Although a recompilation is necessary, this leaves the
    code much cleaner since this feature is not expected to be turned off.
    
    Bug: 755571
    Change-Id: I4d3189a93c7a0361a89d9f45f3bb482484ac98c1

---
+++ ios/chrome/browser/web/features.h
@@ -0,0 +1,16 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_WEB_FEATURES_H_
+#define IOS_CHROME_BROWSER_WEB_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace ios {
+
+extern const base::Feature kMailtoUrlRewriting;
+
+}  // namespace ios
+
+#endif  // IOS_CHROME_BROWSER_WEB_FEATURES_H_
