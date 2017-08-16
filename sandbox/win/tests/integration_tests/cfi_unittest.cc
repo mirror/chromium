331402c7bd98adafeee518f18a5bebbffa404dcc
Penny MacNeil <pennymac@chromium.org>
2017-08-16 15:21:22 -0700

    [Windows Sandbox Tests]Fix up CFG test.
    Remove tests dependencies on MS assembly.  Too fragile.
    
    Analyze our own running binary instead.
    
    Test=sbox_integration_tests.exe, CFGSupportTests.*
    BUG=751809
    
    Cq-Include-Trybots: master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: Ia973871ee9413763ceac30f7d3a6825e7ea40ba1

--- sandbox/win/tests/integration_tests/cfi_unittest.cc
+++ sandbox/win/tests/integration_tests/cfi_unittest.cc
@@ -3,50 +3,45 @@
 // found in the LICENSE file.
 #include <windows.h>
 
-#include "base/command_line.h"
-#include "base/files/file_util.h"
-#include "base/process/launch.h"
-#include "base/test/test_timeouts.h"
-#include "base/win/windows_version.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+extern "C" IMAGE_DOS_HEADER __ImageBase;
 
 namespace sandbox {
 // ASLR must be enabled for CFG to be enabled.  As ASLR is disabled in debug
 // builds, so must be CFG.
 #if defined(NDEBUG)
 
-// Make sure Microsoft binaries, that are compiled with CFG enabled, catch
-// a hook and throw an exception.
-// - If this test fails, the expected CFG exception did NOT happen.  This
-//   indicates a build system change that has disabled Chrome process-wide CFG.
-TEST(CFGSupportTests, MsIndirectFailure) {
-  // CFG is only supported on >= Win8.1 Update 3.
-  // Not checking for update, since test infra is updated and it would add
-  // a lot of complexity.
-  if (base::win::GetVersion() < base::win::VERSION_WIN8_1)
-    return;
+// This test analyzes our own running binary to ensure MS CFG is enabled.
+// (Not instrumented into our code, just enabled for the process system DLLs.)
+TEST(CFGSupportTests, BinaryCheck) {
+  ASSERT_EQ(0x5A4D, __ImageBase.e_magic);
 
-  const wchar_t* exe_filename = L"cfi_unittest_exe.exe";
-  const wchar_t* sys_dll_test = L"1";
+  PIMAGE_NT_HEADERS nt_headers = reinterpret_cast<PIMAGE_NT_HEADERS>(
+      reinterpret_cast<char*>(&__ImageBase) + __ImageBase.e_lfanew);
+  ASSERT_EQ(static_cast<DWORD>(IMAGE_NT_SIGNATURE), nt_headers->Signature);
 
-  base::CommandLine cmd_line = base::CommandLine::FromString(exe_filename);
-  cmd_line.AppendArgNative(sys_dll_test);
+  ASSERT_EQ(static_cast<WORD>(IMAGE_NT_OPTIONAL_HDR_MAGIC),
+            nt_headers->OptionalHeader.Magic);
+  PIMAGE_OPTIONAL_HEADER optional_header =
+      reinterpret_cast<PIMAGE_OPTIONAL_HEADER>(&nt_headers->OptionalHeader);
 
-  base::Process proc =
-      base::LaunchProcess(cmd_line, base::LaunchOptionsForTest());
-  ASSERT_TRUE(proc.IsValid());
+  // Check 1: Ensure the CFG DllCharacteristics flag is set.
+  ASSERT_TRUE(optional_header->DllCharacteristics &
+              IMAGE_DLLCHARACTERISTICS_GUARD_CF);
 
-  int exit_code = 0;
-  if (!proc.WaitForExitWithTimeout(TestTimeouts::action_timeout(),
-                                   &exit_code)) {
-    // Timeout while waiting.  Try to cleanup.
-    proc.Terminate(1, false);
-    ADD_FAILURE();
-    return;
-  }
+  PIMAGE_DATA_DIRECTORY load_config =
+      &(optional_header->DataDirectory[IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG]);
+  PIMAGE_LOAD_CONFIG_DIRECTORY load_config_struct =
+      reinterpret_cast<PIMAGE_LOAD_CONFIG_DIRECTORY>(
+          reinterpret_cast<char*>(optional_header->ImageBase) +
+          load_config->VirtualAddress);
 
-  // CFG security check failure.
-  ASSERT_EQ(STATUS_STACK_BUFFER_OVERRUN, static_cast<DWORD>(exit_code));
+  // Check 2: Ensure the Guard CF function count is > 0.
+  ASSERT_TRUE(load_config_struct->GuardCFFunctionCount > 0);
+  ASSERT_TRUE(load_config_struct->GuardFlags & IMAGE_GUARD_CF_INSTRUMENTED);
+
+  return;
 }
 
 #endif  // defined(NDEBUG)
