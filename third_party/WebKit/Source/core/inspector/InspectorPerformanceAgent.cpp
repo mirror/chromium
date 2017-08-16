e7bb26f96023efe6de664d097f4179ad3f4c59b2
Alexei Filippov <alph@chromium.org>
2017-08-16 10:35:12 -0700

    DevTools: Add RecalcStyle, Layout & JS timings to Performance.metrics
    BUG=751892

--- third_party/WebKit/Source/core/inspector/InspectorPerformanceAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorPerformanceAgent.cpp
@@ -7,6 +7,7 @@
 #include "core/frame/LocalFrame.h"
 #include "core/inspector/InspectedFrames.h"
 #include "core/paint/PaintTiming.h"
+#include "core/probe/CoreProbes.h"
 #include "platform/InstanceCounters.h"
 #include "platform/wtf/dtoa/utils.h"
 
@@ -74,6 +75,9 @@ Response InspectorPerformanceAgent::getMetrics(
   AppendMetric(
       result.get(), "RecalcStyleCount",
       InstanceCounters::CounterValue(InstanceCounters::kRecalcStyleCounter));
+  AppendMetric(result.get(), "LayoutDuration", layout_duration_);
+  AppendMetric(result.get(), "RecalcStyleDuration", recalc_style_duration_);
+  AppendMetric(result.get(), "ScriptDuration", script_duration_);
 
   // Performance timings.
   Document* document = inspected_frames_->Root()->GetDocument();
@@ -96,6 +100,42 @@ void InspectorPerformanceAgent::ConsoleTimeStamp(const String& title) {
   std::unique_ptr<protocol::Array<protocol::Performance::Metric>> metrics;
   getMetrics(&metrics);
   GetFrontend()->metrics(std::move(metrics), title);
+}
+
+void InspectorPerformanceAgent::Will(const probe::CallFunction& probe) {
+  if (!script_call_depth_++)
+    probe.CaptureStartTime();
+}
+
+void InspectorPerformanceAgent::Did(const probe::CallFunction& probe) {
+  if (!--script_call_depth_)
+    script_duration_ += probe.Duration();
+}
+
+void InspectorPerformanceAgent::Will(const probe::ExecuteScript& probe) {
+  if (!script_call_depth_++)
+    probe.CaptureStartTime();
+}
+
+void InspectorPerformanceAgent::Did(const probe::ExecuteScript& probe) {
+  if (!--script_call_depth_)
+    script_duration_ += probe.Duration();
+}
+
+void InspectorPerformanceAgent::Will(const probe::RecalculateStyle& probe) {
+  probe.CaptureStartTime();
+}
+
+void InspectorPerformanceAgent::Did(const probe::RecalculateStyle& probe) {
+  recalc_style_duration_ += probe.Duration();
+}
+
+void InspectorPerformanceAgent::Will(const probe::UpdateLayout& probe) {
+  probe.CaptureStartTime();
+}
+
+void InspectorPerformanceAgent::Did(const probe::UpdateLayout& probe) {
+  layout_duration_ += probe.Duration();
 }
 
 DEFINE_TRACE(InspectorPerformanceAgent) {
