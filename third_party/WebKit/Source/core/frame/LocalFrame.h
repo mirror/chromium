007517f34b90773e6ad9b28fbb919958182cbc57
Yutaka Hirano <yhirano@chromium.org>
2017-08-16 20:33:32 +0900

    Keep renderer alive for "keepalive" requests
    wip
    
    Bug: 695939
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I0dd5ee2c1dfd4e694dfcf2f2f62485add7dec249

--- third_party/WebKit/Source/core/frame/LocalFrame.h
+++ third_party/WebKit/Source/core/frame/LocalFrame.h
@@ -87,6 +87,7 @@ class WebFrameScheduler;
 class WebPluginContainerImpl;
 class WebTaskRunner;
 class WebURLLoader;
+class WebURLRequestsTracker;
 
 extern template class CORE_EXTERN_TEMPLATE_EXPORT Supplement<LocalFrame>;
 
@@ -253,6 +254,7 @@ class CORE_EXPORT LocalFrame final : public Frame,
 
   std::unique_ptr<WebURLLoader> CreateURLLoader(const ResourceRequest&,
                                                 WebTaskRunner*);
+  std::unique_ptr<WebURLRequestsTracker> IssueRequestsTracker();
 
   bool IsInert() const { return is_inert_; }
 
