a61c4e22f771363585d9a8da0b74bd571b0edd38
Reza.Zakerinasab <zakerinasab@chromium.org>
2017-08-16 16:36:04 -0400

    Revert "Turn on color correct rendering for canvas" for PixelDiff testing
    This is a revert for turning on color correct rendering for canvas. This
    revert is created to get passed to Pixel Diff to see if the original patch
    has changed the output of the Chromium.
    
    Bug: 733274
    Change-Id: I839ddb30dcadc10ff250567abd5f46edbc1c83e3

--- third_party/WebKit/Source/core/html/ImageData.cpp
+++ third_party/WebKit/Source/core/html/ImageData.cpp
@@ -319,21 +319,13 @@ ImageData* ImageData::Create(NotShared<DOMUint8ClampedArray> data,
   return new ImageData(IntSize(width, height), data.View());
 }
 
-bool ColorManagementEnabled(const ImageDataColorSettings& color_settings) {
-  if (CanvasColorParams::ColorCorrectRenderingInAnyColorSpace())
-    return true;
-  if (CanvasColorParams::ColorCorrectRenderingInSRGBOnly() &&
-      color_settings.colorSpace() == kSRGBCanvasColorSpaceName)
-    return true;
-  return false;
-}
-
 ImageData* ImageData::CreateImageData(
     unsigned width,
     unsigned height,
     const ImageDataColorSettings& color_settings,
     ExceptionState& exception_state) {
-  if (!ColorManagementEnabled(color_settings))
+  if (!RuntimeEnabledFeatures::ColorCanvasExtensionsEnabled() &&
+      !RuntimeEnabledFeatures::ColorCorrectRenderingEnabled())
     return nullptr;
 
   if (!ImageData::ValidateConstructorArguments(
@@ -357,7 +349,7 @@ ImageData* ImageData::CreateImageData(ImageDataArray& data,
                                       unsigned height,
                                       ImageDataColorSettings& color_settings,
                                       ExceptionState& exception_state) {
-  if (!ColorManagementEnabled(color_settings))
+  if (!RuntimeEnabledFeatures::ColorCanvasExtensionsEnabled())
     return nullptr;
 
   DOMArrayBufferView* buffer_view = nullptr;
@@ -709,7 +701,7 @@ DOMArrayBufferBase* ImageData::BufferBase() const {
 }
 
 CanvasColorParams ImageData::GetCanvasColorParams() {
-  if (!ColorManagementEnabled(color_settings_))
+  if (!RuntimeEnabledFeatures::ColorCanvasExtensionsEnabled())
     return CanvasColorParams();
   CanvasColorSpace color_space =
       ImageData::GetCanvasColorSpace(color_settings_.colorSpace());
