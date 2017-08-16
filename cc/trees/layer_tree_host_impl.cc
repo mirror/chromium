95c5d2653bb568fc631fbba8a86fd58e4882e63f
chaopeng <chaopeng@chromium.org>
2017-08-16 11:12:17 -0400

    ScrollBegin and ScrollEnd should call paired in same layer
    Bug: 755576
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I20d1e2ef2b6628ce90f9a551cd7dbb0b73968d35

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -2848,7 +2848,15 @@ InputHandler::ScrollStatus LayerTreeHostImpl::ScrollBeginImpl(
     scroll_status.main_thread_scrolling_reasons =
         MainThreadScrollingReason::kNoScrollingLayer;
     return scroll_status;
+  } else if (touchpad_and_wheel_scroll_latching_enabled_) {
+    // TODO(chaopeng) ScrollBegin and ScrollEnd will be paired after
+    // TouchpadAndWheelScrollLatching land.
+    ScrollbarAnimationController* scrollbar_animation_controller =
+        ScrollbarAnimationControllerForElementId(scrolling_node->element_id);
+    if (scrollbar_animation_controller)
+      scrollbar_animation_controller->DidScrollBegin();
   }
+
   scroll_status.thread = SCROLL_ON_IMPL_THREAD;
   mutator_host_->ScrollAnimationAbort();
 
@@ -2958,6 +2966,15 @@ InputHandler::ScrollStatus LayerTreeHostImpl::ScrollBegin(
                                     kScrollerSizeBucketCount);
       }
     }
+  }
+
+  // TODO(chaopeng) ScrollBegin and ScrollEnd will be paired after
+  // TouchpadAndWheelScrollLatching land.
+  if (touchpad_and_wheel_scroll_latching_enabled_ && scrolling_node) {
+    ScrollbarAnimationController* scrollbar_animation_controller =
+        ScrollbarAnimationControllerForElementId(scrolling_node->element_id);
+    if (scrollbar_animation_controller)
+      scrollbar_animation_controller->DidScrollBegin();
   }
 
   if (scroll_on_main_thread) {
@@ -3637,6 +3654,18 @@ void LayerTreeHostImpl::ScrollEnd(ScrollState* scroll_state) {
 
   DistributeScrollDelta(scroll_state);
   browser_controls_offset_manager_->ScrollEnd();
+
+  // TODO(chaopeng) ScrollBegin and ScrollEnd will be paired after
+  // TouchpadAndWheelScrollLatching land.
+  if (touchpad_and_wheel_scroll_latching_enabled_) {
+    if (ScrollNode* scrolling_node = CurrentlyScrollingNode()) {
+      ScrollbarAnimationController* scrollbar_animation_controller =
+          ScrollbarAnimationControllerForElementId(scrolling_node->element_id);
+      if (scrollbar_animation_controller)
+        scrollbar_animation_controller->DidScrollEnd();
+    }
+  }
+
   ClearCurrentlyScrollingNode();
 }
 
