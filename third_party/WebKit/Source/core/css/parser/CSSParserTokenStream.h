c2236abb1cb23d37442614fbb7bbcb9c85e678b8
Darren Shen <shend@chromium.org>
2017-08-16 18:14:14 +1000

    [CSSParser] Add offset information to CSSParserTokenStream.
    One of the benefits of streaming parser is that we can get token offset
    information (where a token starts in the input string) on the fly.
    This patch adds CSSParserTokenStream::Offset() which returns the index
    in the input string of the next token to be consumed. Naively, we would
    implement this by querying CSSTokenizerParserInputStream::Offset().
    However, CSSTokenizerInputStream::Offset() might be ahead of
    CSSParserTokenStream::Offset() because peeking a token would advance
    CSSTokenizerInputStream but not CSSParserTokenStream, so some extra
    logic is required to handle this.
    
    Bug: 661854
    Change-Id: I67ceaeb4cb16345454b1ccf92c103401996fd298

--- third_party/WebKit/Source/core/css/parser/CSSParserTokenStream.h
+++ third_party/WebKit/Source/core/css/parser/CSSParserTokenStream.h
@@ -132,6 +132,9 @@ class CORE_EXPORT CSSParserTokenStream {
     return Iterator(next_index_);
   }
 
+  // Get the index of the character in the original string to be consumed next.
+  size_t Offset() const { return offset_; }
+
   void ConsumeWhitespace();
   CSSParserToken ConsumeIncludingWhitespace();
   void UncheckedConsumeComponentValue(unsigned nesting_level = 0);
@@ -153,12 +156,14 @@ class CORE_EXPORT CSSParserTokenStream {
     if (!token.IsEOF())
       next_index_++;
 
+    offset_ = tokenizer_.input_.Offset();
     DCHECK_LE(next_index_, tokenizer_.CurrentSize());
     return token;
   }
 
   const CSSParserToken& UncheckedConsumeInternal() {
     DCHECK_LE(next_index_, tokenizer_.CurrentSize());
+    offset_ = tokenizer_.input_.Offset();
     return tokenizer_.tokens_[next_index_++];
   }
 
@@ -166,6 +171,7 @@ class CORE_EXPORT CSSParserTokenStream {
 
   CSSTokenizer& tokenizer_;
   size_t next_index_;  // Index of next token to be consumed.
+  size_t offset_ = 0;
 };
 
 }  // namespace blink
