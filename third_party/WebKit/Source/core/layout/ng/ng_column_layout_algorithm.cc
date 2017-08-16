7d985a259a60c47693270624d8c04b885b7d9dbc
Morten Stenshorne <mstensho@opera.com>
2017-08-16 13:01:19 +0200

    [LayoutNG] Bring back native support for multicol.
    Still missing: Copy data back into the LayoutObject tree.
    
    Also brought back some of the unit tests. They needed some changes, because of
    the following:
    
    1. We now create a fragment for each column, from an anonymous constraint
    space. I.e. we have one more level of fragments, and it's the column fragments
    that have their inline offset shifted to the correct column, rather than
    setting it on the first child block, which is what we did last time these tests
    worked.
    
    2. Mistakes introduced when converting test to using setBodyInnerHTML() rather
    than building the layout trees programatically:
    https://codereview.chromium.org/2725773002
    
    3. New bugs. :)
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I4df85d684f0ddb8f7f0f75d15230a1ab61e9f9a0

--- third_party/WebKit/Source/core/layout/ng/ng_column_layout_algorithm.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_column_layout_algorithm.cc
@@ -4,16 +4,99 @@
 
 #include "core/layout/ng/ng_column_layout_algorithm.h"
 
+#include "core/layout/ng/inline/ng_baseline.h"
+#include "core/layout/ng/ng_block_layout_algorithm.h"
+#include "core/layout/ng/ng_constraint_space_builder.h"
+#include "core/layout/ng/ng_length_utils.h"
+#include "core/layout/ng/ng_out_of_flow_layout_part.h"
+
 namespace blink {
 
 NGColumnLayoutAlgorithm::NGColumnLayoutAlgorithm(NGBlockNode node,
                                                  NGConstraintSpace* space,
                                                  NGBreakToken* break_token)
-    : NGBlockLayoutAlgorithm(node, space, ToNGBlockBreakToken(break_token)){};
+    : NGLayoutAlgorithm(node, space, ToNGBlockBreakToken(break_token)) {}
 
 RefPtr<NGLayoutResult> NGColumnLayoutAlgorithm::Layout() {
-  LOG(FATAL) << "Not implemented";
-  return nullptr;
+  Optional<MinMaxSize> min_max_size;
+  if (NeedMinMaxSize(ConstraintSpace(), Style()))
+    min_max_size = ComputeMinMaxSize();
+  NGBoxStrut border_scrollbar_padding = CalculateBorderScrollbarPadding(
+      ConstraintSpace(), Style(), Node().GetLayoutObject());
+  NGLogicalSize border_box_size =
+      CalculateBorderBoxSize(ConstraintSpace(), Style(), min_max_size);
+  content_box_size_ =
+      CalculateContentBoxSize(border_box_size, border_scrollbar_padding);
+
+  // TODO(mstensho): Implement column balancing.
+  DCHECK_NE(Style().GetColumnFill(), EColumnFill::kBalance)
+      << "Column balancing is not yet implemented";
+  // Auto height also implies column balancing.
+  DCHECK_NE(border_box_size.block_size, NGSizeIndefinite)
+      << "Column balancing is not yet implemented";
+
+  container_builder_.SetSize(border_box_size);
+
+  RefPtr<NGConstraintSpace> child_space = CreateConstraintSpaceForColumns();
+  NGWritingMode writing_mode = ConstraintSpace().WritingMode();
+  RefPtr<NGBlockBreakToken> break_token = BreakToken();
+  NGLogicalSize overflow;
+  LayoutUnit column_inline_offset;
+  LayoutUnit column_inline_progression =
+      child_space->AvailableSize().inline_size + ResolveUsedColumnGap(Style());
+
+  do {
+    // Lay out one column. Each column will become a fragment.
+    NGBlockLayoutAlgorithm child_algorithm(Node(), child_space.Get(),
+                                           break_token.Get());
+    RefPtr<NGLayoutResult> result = child_algorithm.Layout();
+    RefPtr<NGPhysicalBoxFragment> column(
+        ToNGPhysicalBoxFragment(result->PhysicalFragment().Get()));
+
+    NGLogicalOffset logical_offset(column_inline_offset, LayoutUnit());
+    container_builder_.AddChild(column, logical_offset);
+
+    NGLogicalSize size = NGBoxFragment(writing_mode, column.Get()).Size();
+    NGLogicalOffset end_offset =
+        logical_offset + NGLogicalOffset(size.inline_size, size.block_size);
+
+    overflow.inline_size =
+        std::max(overflow.inline_size, end_offset.inline_offset);
+    overflow.block_size =
+        std::max(overflow.block_size, end_offset.block_offset);
+
+    column_inline_offset += column_inline_progression;
+    break_token = ToNGBlockBreakToken(column->BreakToken());
+  } while (break_token && !break_token->IsFinished());
+
+  container_builder_.SetOverflowSize(overflow);
+
+  NGOutOfFlowLayoutPart(ConstraintSpace(), Style(), &container_builder_).Run();
+
+  // TODO(mstensho): Propagate baselines.
+
+  return container_builder_.ToBoxFragment();
+}
+
+RefPtr<NGConstraintSpace>
+NGColumnLayoutAlgorithm::CreateConstraintSpaceForColumns() const {
+  NGConstraintSpaceBuilder space_builder(ConstraintSpace().WritingMode());
+  NGLogicalSize adjusted_size = content_box_size_;
+  adjusted_size.inline_size =
+      ResolveUsedColumnInlineSize(adjusted_size.inline_size, Style());
+  space_builder.SetAvailableSize(adjusted_size);
+  space_builder.SetPercentageResolutionSize(adjusted_size);
+
+  if (NGBaseline::ShouldPropagateBaselines(Node()))
+    space_builder.AddBaselineRequests(ConstraintSpace().BaselineRequests());
+
+  space_builder.SetFragmentationType(kFragmentColumn);
+  space_builder.SetFragmentainerSpaceAvailable(adjusted_size.block_size);
+  space_builder.SetIsNewFormattingContext(true);
+  space_builder.SetIsAnonymous(true);
+
+  return space_builder.ToConstraintSpace(
+      FromPlatformWritingMode(Style().GetWritingMode()));
 }
 
 }  // namespace Blink
