8804a8100e638bed7dcabbba59e4beb196293c87
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-08-16 13:23:36 -0700

    [Extensions Bindings] Add more histograms
    Add histograms for the amount of time taken to update bindings for a
    given context (split by context type) and for the amount of time taken
    to instantiate a new full API binding natively (which is mostly matched
    by Extensions.ApiBindingGenerationTime for JS bindings).
    
    Bug: 653596
    Change-Id: Id20229c2b972ef75db5c4dac8fe39fe0b09af49d

--- extensions/renderer/extension_bindings_system.cc
+++ extensions/renderer/extension_bindings_system.cc
@@ -4,6 +4,7 @@
 
 #include "extensions/renderer/extension_bindings_system.h"
 
+#include "base/metrics/histogram_macros.h"
 #include "extensions/common/manifest_constants.h"
 #include "extensions/common/manifest_handlers/externally_connectable.h"
 #include "extensions/renderer/renderer_extension_registry.h"
@@ -28,5 +29,67 @@ bool ExtensionBindingsSystem::IsRuntimeAvailableToContext(
 const char* ExtensionBindingsSystem::kWebAvailableFeatures[] = {
     "app", "webstore", "dashboardPrivate",
 };
+
+void ExtensionBindingsSystem::LogUpdateBindingsForContextTime(
+    Feature::Context context_type,
+    base::TimeDelta elapsed) {
+  switch (context_type) {
+    case Feature::UNSPECIFIED_CONTEXT:
+      break;
+    case Feature::WEB_PAGE_CONTEXT:
+      UMA_HISTOGRAM_CUSTOM_COUNTS(
+          "Extensions.Bindings.UpdateBindingsForContextTime.WebPageContext",
+          elapsed.InMicroseconds(),
+          1, 10000000, 100);
+      break;
+    case Feature::BLESSED_WEB_PAGE_CONTEXT:
+      UMA_HISTOGRAM_CUSTOM_COUNTS(
+          "Extensions.Bindings.UpdateBindingsForContextTime."
+              "BlessedWebPageContext",
+          elapsed.InMicroseconds(),
+          1, 10000000, 100);
+      break;
+    case Feature::SERVICE_WORKER_CONTEXT:
+      UMA_HISTOGRAM_CUSTOM_COUNTS(
+          "Extensions.Bindings.UpdateBindingsForContextTime."
+              "ServiceWorkerContext",
+          elapsed.InMicroseconds(),
+          1, 10000000, 100);
+      break;
+    case Feature::BLESSED_EXTENSION_CONTEXT:
+      UMA_HISTOGRAM_CUSTOM_COUNTS(
+          "Extensions.Bindings.UpdateBindingsForContextTime."
+              "BlessedExtensionContext",
+          elapsed.InMicroseconds(),
+          1, 10000000, 100);
+      break;
+    case Feature::LOCK_SCREEN_EXTENSION_CONTEXT:
+      UMA_HISTOGRAM_CUSTOM_COUNTS(
+          "Extensions.Bindings.UpdateBindingsForContextTime."
+              "LockScreenExtensionContext",
+          elapsed.InMicroseconds(),
+          1, 10000000, 100);
+      break;
+    case Feature::UNBLESSED_EXTENSION_CONTEXT:
+      UMA_HISTOGRAM_CUSTOM_COUNTS(
+          "Extensions.Bindings.UpdateBindingsForContextTime."
+              "UnblessedExtensionContext",
+          elapsed.InMicroseconds(),
+          1, 10000000, 100);
+      break;
+    case Feature::CONTENT_SCRIPT_CONTEXT:
+      UMA_HISTOGRAM_CUSTOM_COUNTS(
+          "Extensions.Bindings.UpdateBindingsForContextTime."
+              "ContentScriptContext",
+          elapsed.InMicroseconds(),
+          1, 10000000, 100);
+      break;
+    case Feature::WEBUI_CONTEXT:
+      UMA_HISTOGRAM_CUSTOM_COUNTS(
+          "Extensions.Bindings.UpdateBindingsForContextTime.WebUIContext",
+          elapsed.InMicroseconds(),
+          1, 10000000, 100);
+  }
+}
 
 }  // namespace extensions
