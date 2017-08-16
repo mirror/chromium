59f98985499cd4dae152db1bbf5905f61aa584ba
Peiyong Lin <lpy@chromium.org>
2017-08-15 17:01:31 -0700

    [GRC] Report first favicon updated UMA for background tabs.
    This patch adds UMA histogram to report first favicon update happen for
    background tabs, histogram records the time from when the tab becomes
    background to when the favicon is updated.
    
    BUG=731270
    
    Change-Id: I8c429bc18903fb509cba559d6adee19e73e889fd

--- chrome/browser/resource_coordinator/resource_coordinator_web_contents_observer.cc
+++ chrome/browser/resource_coordinator/resource_coordinator_web_contents_observer.cc
@@ -127,6 +127,7 @@ void ResourceCoordinatorWebContentsObserver::DidFinishNavigation(
 
   if (navigation_handle->IsInMainFrame()) {
     UpdateUkmRecorder(navigation_handle->GetURL());
+    ResetFlag();
   }
 
   content::RenderFrameHost* render_frame_host =
@@ -144,14 +145,28 @@ void ResourceCoordinatorWebContentsObserver::DidFinishNavigation(
 void ResourceCoordinatorWebContentsObserver::TitleWasSet(
     content::NavigationEntry* entry,
     bool explicit_set) {
-  // Ignore first time title updated event, since it happens as part of loading
-  // process.
-  if (!first_time_title_updated_) {
-    first_time_title_updated_ = true;
+  // Ignore update when the tab is in foreground.
+  if (web_contents()->IsVisible())
+    return;
+  if (!first_time_title_set_) {
+    first_time_title_set_ = true;
     return;
   }
   tab_resource_coordinator_->SendEvent(
       resource_coordinator::mojom::Event::kTitleUpdated);
+}
+
+void ResourceCoordinatorWebContentsObserver::DidUpdateFaviconURL(
+    const std::vector<content::FaviconURL>& candidates) {
+  // Ignore update when the tab is in foreground.
+  if (web_contents()->IsVisible())
+    return;
+  if (!first_time_favicon_set_) {
+    first_time_favicon_set_ = true;
+    return;
+  }
+  tab_resource_coordinator_->SendEvent(
+      resource_coordinator::mojom::Event::kFaviconUpdated);
 }
 
 void ResourceCoordinatorWebContentsObserver::UpdateUkmRecorder(
@@ -166,4 +181,9 @@ void ResourceCoordinatorWebContentsObserver::UpdateUkmRecorder(
   ukm::UkmRecorder::Get()->UpdateSourceURL(ukm_source_id_, url);
   tab_resource_coordinator_->SetProperty(
       resource_coordinator::mojom::PropertyType::kUKMSourceId, ukm_source_id_);
+}
+
+void ResourceCoordinatorWebContentsObserver::ResetFlag() {
+  first_time_title_set_ = false;
+  first_time_favicon_set_ = false;
 }
