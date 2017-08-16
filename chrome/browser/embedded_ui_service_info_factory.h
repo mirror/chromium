33006765fb92f0898797469b7c43214193f69901
Antonio Gomes <tonikitoo@igalia.com>
2017-08-16 14:20:26 -0400

    Factor Services related logic out of BrowserProcessPlatforPart{chromeos}
    CL introduces a EmbeddedUIServiceInfoFactory factory, and factors the UI
    service logic in BrowserProcessPlatforPart{chromeos}::RegisterInProcessServices
    into.
    
    For ChromeOS there is no behavior change (both --mash and --mus continue
    with the same process/service model), but this refactor allows platforms
    other than ChromeOS (e.g. Ozone+Mus+Linux) more easily benefit from the
    same in-process UI as chromeos/mus.
    
    TEST=None
    BUG=722527
    
    Change-Id: Ic7a24778b3c57cc0c8620b83bcf555a5a98f5bf8

---
+++ chrome/browser/embedded_ui_service_info_factory.h
@@ -0,0 +1,17 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_EMBEDDED_UI_SERVICE_INFO_FACTORY_H_
+#define CHROME_BROWSER_EMBEDDED_UI_SERVICE_INFO_FACTORY_H_
+
+#include "services/service_manager/embedder/embedded_service_info.h"
+#include "services/ui/common/image_cursors_set.h"
+
+// This standalone function works as a factory for creating
+// EmbeddedServiceInfo for running the UI service as a thread.
+// It is kept separated so it can be reused.
+service_manager::EmbeddedServiceInfo CreateEmbeddedUIServiceInfo(
+    base::WeakPtr<ui::ImageCursorsSet> image_cursors_set_weak_ptr);
+
+#endif  // CHROME_BROWSER_EMBEDDED_UI_SERVICE_INFO_FACTORY_H_
