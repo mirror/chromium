7d5300f8af360a5ada103d259f423aa5601d6f5b
Ryan Landay <rlanday@chromium.org>
2017-08-16 13:34:44 -0700

    Fix composition marker painting in bi-directional text
    Composition marker painting is currently broken for LTR text in an RTL text box
    and vice-versa. This CL fixes these cases. The layout test baseline changes in
    this CL show examples of these cases.
    
    Bug: 754443
    Change-Id: I8baf26c81d06593e32db8d70b0cf8e0e2b39c7dc

--- third_party/WebKit/Source/core/paint/InlineTextBoxPainter.h
+++ third_party/WebKit/Source/core/paint/InlineTextBoxPainter.h
@@ -79,9 +79,15 @@ class InlineTextBoxPainter {
 
   void PaintStyleableMarkerUnderline(GraphicsContext&,
                                      const LayoutPoint& box_origin,
-                                     const StyleableMarker&);
-  unsigned MarkerPaintStart(const DocumentMarker&);
-  unsigned MarkerPaintEnd(const DocumentMarker&);
+                                     const StyleableMarker&,
+                                     const ComputedStyle&,
+                                     const Font&);
+  struct MarkerOffsets {
+    unsigned start;
+    unsigned end;
+  };
+  MarkerOffsets MarkerPaintStartAndEnd(const DocumentMarker&);
+
   bool ShouldPaintTextBox(const PaintInfo&);
   void ExpandToIncludeNewlineForSelection(LayoutRect&);
   LayoutObject& InlineLayoutObject() const;
