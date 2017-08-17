d0af17a59e88d7aa8025a53e3942893e9bab0105
Hirokazu Honda <hiroh@chromium.org>
2017-08-17 09:14:38 +0900

    Refactor ChromeArcVDA and GPUArcVDA based on new VDA-like IPC design
    BUG=chromium:745457
    
    Change-Id: I591626d651d1b7bed0a03f55fdc64dae93d196c9

--- chrome/gpu/arc_video_decode_accelerator.h
+++ chrome/gpu/arc_video_decode_accelerator.h
@@ -1,4 +1,4 @@
-// Copyright 2016 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,46 +9,31 @@
 
 #include "base/files/scoped_file.h"
 #include "components/arc/video_accelerator/video_accelerator.h"
+#include "media/base/video_codecs.h"
+#include "media/base/video_types.h"
+#include "media/video/picture.h"
+#include "ui/gfx/geometry/size.h"
 
 namespace chromeos {
 namespace arc {
 
-enum HalPixelFormatExtension {
-  // The pixel formats defined in Android but are used here. They are defined
-  // in "system/core/include/system/graphics.h"
-  HAL_PIXEL_FORMAT_BGRA_8888 = 5,
-  HAL_PIXEL_FORMAT_YCbCr_420_888 = 0x23,
-
-  // The following formats are not defined in Android, but used in
-  // ArcVideoDecodeAccelerator to identify the input format.
-  HAL_PIXEL_FORMAT_H264 = 0x34363248,
-  HAL_PIXEL_FORMAT_VP8 = 0x00385056,
-  HAL_PIXEL_FORMAT_VP9 = 0x00395056,
+// Information of the shared memory bitstream buffer.
+struct BitstreamBuffer {
+  int32_t bitstream_id;
+  base::ScopedFD ashmem_fd;
+  uint32_t offset;
+  uint32_t bytes_used;
 };
 
-enum PortType {
-  PORT_INPUT = 0,
-  PORT_OUTPUT = 1,
-  PORT_COUNT = 2,
-};
+// Format specification of the picture buffer request.
+struct PictureBufferFormat {
+  uint32_t pixel_format;
+  uint32_t buffer_size;
 
-struct BufferMetadata {
-  int64_t timestamp = 0;  // in microseconds
-  uint32_t bytes_used = 0;
-};
-
-struct VideoFormat {
-  uint32_t pixel_format = 0;
-  uint32_t buffer_size = 0;
-
-  // Minimum number of buffers required to decode/encode the stream.
-  uint32_t min_num_buffers = 0;
-  uint32_t coded_width = 0;
-  uint32_t coded_height = 0;
-  uint32_t crop_left = 0;
-  uint32_t crop_width = 0;
-  uint32_t crop_top = 0;
-  uint32_t crop_height = 0;
+  // minimal number of buffers required to process the video.
+  uint32_t min_num_buffers;
+  uint32_t coded_width;
+  uint32_t coded_height;
 };
 
 // The IPC interface between Android and Chromium for video decoding. Input
@@ -69,78 +54,67 @@ class ArcVideoDecodeAccelerator {
     RESULT_MAX = 6,
   };
 
-  struct Config {
-    size_t num_input_buffers = 0;
-    uint32_t input_pixel_format = 0;
-    // TODO(owenlin): Add output_pixel_format. For now only the native pixel
-    //                format of each VDA on Chromium is supported.
-  };
-
-  // The callbacks of the ArcVideoDecodeAccelerator. The user of this class
-  // should implement this interface.
+  // The callbacks of the ArcVideoDecodeAccelerator similar
+  // to media::VideoDecodeAccelerator::Client.
+  // The user of this class should implement this interface.
   class Client {
    public:
     virtual ~Client() {}
 
-    // Called when an asynchronous error happens. The errors in Initialize()
-    // will not be reported here, but will be indicated by a return value
-    // there.
-    virtual void OnError(Result error) = 0;
+    // Callback to tell client how many and what size of buffers to provide.
+    // by the VDA, or PIXEL_FORMAT_UNKNOWN if the underlying platform handles
+    // |format| indicates what format the decoded frames will be produced in
+    // by the VDA, or PIXEL_FORMAT_UNKNOWN if the underlying platform handles
+    // this transparently.
+    virtual void ProvidePictureBuffers(uint32_t requested_num_of_buffers,
+                                       media::VideoPixelFormat format,
+                                       uint32_t textures_per_buffer,
+                                       const gfx::Size& dimensions,
+                                       uint32_t texture_target) = 0;
 
-    // Called when a buffer with the specified |index| and |port| has been
-    // processed and is no longer used in the accelerator. For input buffers,
-    // the Client may fill them with new content. For output buffers, indicates
-    // they are ready to be consumed by the client.
-    virtual void OnBufferDone(PortType port,
-                              uint32_t index,
-                              const BufferMetadata& metadata) = 0;
+    // Called when decoded pictures is ready to be displayed.
+    virtual void PictureReady(const media::Picture& picture) = 0;
 
-    // Called when the output format has changed or the output format
-    // becomes available at beginning of the stream after initial parsing.
-    virtual void OnOutputFormatChanged(const VideoFormat& format) = 0;
-
-    // Called as a completion notification for Reset().
-    virtual void OnResetDone() = 0;
+    // Called as a notification that decoded has decoded the end of the current
+    // bitstream buffer.
+    virtual void NotifyEndOfBitstreamBuffer(int32_t bitstream_buffer) = 0;
 
     // Called as a completion notification for Flush().
-    virtual void OnFlushDone() = 0;
+    virtual void NotifyFlushDone() = 0;
+
+    // Called as a completion notification for Reset().
+    virtual void NotifyResetDone() = 0;
+
+    // Called when an asynchronous error happens. The errors in Initialize()
+    // will not be reported here, but will be indicated by a return value there.
+    virtual void NotifyError(Result error) = 0;
   };
 
-  // Initializes the ArcVideoDecodeAccelerator with specific configuration. This
-  // must be called before any other methods. This call is synchronous and
-  // returns SUCCESS iff initialization is successful.
-  virtual Result Initialize(const Config& config, Client* client) = 0;
+  // Initializes the ArcVideoDecodeAccelerator with video codec profile.
+  // This must be called before any other methods.
+  // This call is synchronous and returns SUCCESS
+  // iff initialization is successful.
+  virtual Result Initialize(media::VideoCodecProfile profile,
+                            Client* client) = 0;
 
-  // Assigns a shared memory to be used for the accelerator at the specified
-  // port and index. A buffer must be successfully bound before it can be passed
-  // to the accelerator via UseBuffer(). Already bound buffers may be reused
-  // multiple times without additional bindings.
-  virtual void BindSharedMemory(PortType port,
-                                uint32_t index,
-                                base::ScopedFD ashmem_fd,
-                                off_t offset,
-                                size_t length) = 0;
+  // Decodes given bitstream buffer that contains at most one frame.
+  // BitstreamBuffer needs to be passed as right value, because
+  // |bitstream_buffer.ashmem_fd| should be released when its file descriptor
+  // is passed to VDA.
+  virtual void Decode(chromeos::arc::BitstreamBuffer&& bitstream_buffer) = 0;
 
-  // Assigns a buffer to be used for the accelerator at the specified
-  // port and index. A buffer must be successfully bound before it can be
-  // passed to the accelerator via UseBuffer(). Already bound buffers may be
-  // reused multiple times without additional bindings.
-  virtual void BindDmabuf(
-      PortType port,
-      uint32_t index,
-      base::ScopedFD dmabuf_fd,
-      const std::vector<::arc::VideoFramePlane>& planes) = 0;
+  // Sets the number of output buffers.
+  virtual void AssignPictureBuffers(uint32_t count) = 0;
 
-  // Passes a buffer to the accelerator. For input buffer, the accelerator
-  // will process it. For output buffer, the accelerator will output content
-  // to it.
-  virtual void UseBuffer(PortType port,
-                         uint32_t index,
-                         const BufferMetadata& metadata) = 0;
+  // Import a handle as backing memory for picture buffer associated with
+  // |picture_id|. The handle is created with |ashmem_fd| and |planes|.
+  virtual void ImportBufferForPicture(
+      int32_t picture_id,
+      base::ScopedFD ashmem_fd,
+      std::vector<::arc::VideoFramePlane> planes) = 0;
 
-  // Sets the number of output buffers. When it fails, Client::OnError() will
-  // be called.
-  virtual void SetNumberOfOutputBuffers(size_t number) = 0;
+  // Reuse the picture buffer associated with |picture_id|.
+  virtual void ReusePictureBuffer(int32_t picture_id) = 0;
 
   // Resets the accelerator. When it is done, Client::OnResetDone() will
   // be called. Afterwards, all buffers won't be accessed by the accelerator
