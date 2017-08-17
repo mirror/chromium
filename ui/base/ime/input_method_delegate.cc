bd681f7f1e7dc4ec12adb12e20a6b9e924097e99
Thanh Pham <thanhph@chromium.org>
2017-08-16 20:23:38 -0400

    Make DispatchKeyEventPostIME() asynchronous.
    This change:
    - Adds an overloaded version of 
    InputMethodDelegate::DispatchKeyEventPostIME() with a callback option.
    - Uses the async version of DispatchKeyEventPostIME() in 
    InputMethodChromeOS.
    - Adds ui.mojom.TextInputClient.DispatchKeyEventPostIME().
    
    This is because DispatchKeyEventPostIME() isn't really *Post* IME, and
    calls to it are sometimes intervened with the logic in
    InputMethodChromeOS to provide KeyDown and KeyUp events in addition to
    KeyPress events to textfields.
    
    This CL calls ui.mojom.TextInputClient.DispatchKeyEventPostIME() in
    these cases and continues the logic after the dispatch finishes in the
    client side.
    
    Bug:723792
    Change-Id: I70b3048f836cb195c8676dab1c40bcf28dd0c773

---
+++ ui/base/ime/input_method_delegate.cc
@@ -0,0 +1,12 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/base/ime/input_method_delegate.h"
+
+#include "base/callback.h"
+#include "ui/events/event.h"
+
+namespace ui {
+namespace internal {}  // namespace internal
+}  // namespace ui
