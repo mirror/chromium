126b806dc3c25a624434f506730f554543e4e3fa
jdoerrie <jdoerrie@chromium.org>
2017-08-17 15:55:17 +0200

    Allow only explicit copies of base::Value
    This change deletes base::Values's copy constructor and copy assignment
    operator and introduces base::Value::Clone. It is the purpose of this
    change to disallow implicit copies while still keeping the possibility
    to make explicit copies.
    
    Bug: 646113, 742306
    Change-Id: Idf91382a8dfad1a2b398958e5107b67686696475

--- chromeos/network/onc/onc_merger.cc
+++ chromeos/network/onc/onc_merger.cc
@@ -414,14 +414,13 @@ class MergeToAugmented : public MergeToEffective {
         new base::DictionaryValue);
 
     if (values.active_setting) {
-      augmented_value->SetWithoutPathExpansion(
-          ::onc::kAugmentationActiveSetting,
-          base::MakeUnique<base::Value>(*values.active_setting));
+      augmented_value->SetKey(::onc::kAugmentationActiveSetting,
+                              values.active_setting->Clone());
     }
 
     if (!which_effective.empty()) {
-      augmented_value->SetStringWithoutPathExpansion(
-          ::onc::kAugmentationEffectiveSetting, which_effective);
+      augmented_value->SetKey(::onc::kAugmentationEffectiveSetting,
+                              base::Value(which_effective));
     }
 
     // Prevent credentials from being forwarded in cleartext to
@@ -430,25 +429,21 @@ class MergeToAugmented : public MergeToEffective {
     bool is_credential = onc::FieldIsCredential(*signature_, key);
     if (!is_credential) {
       if (values.user_policy) {
-        augmented_value->SetWithoutPathExpansion(
-            ::onc::kAugmentationUserPolicy,
-            base::MakeUnique<base::Value>(*values.user_policy));
+        augmented_value->SetKey(::onc::kAugmentationUserPolicy,
+                                values.user_policy->Clone());
       }
       if (values.device_policy) {
-        augmented_value->SetWithoutPathExpansion(
-            ::onc::kAugmentationDevicePolicy,
-            base::MakeUnique<base::Value>(*values.device_policy));
+        augmented_value->SetKey(::onc::kAugmentationDevicePolicy,
+                                values.device_policy->Clone());
       }
     }
     if (values.user_setting) {
-      augmented_value->SetWithoutPathExpansion(
-          ::onc::kAugmentationUserSetting,
-          base::MakeUnique<base::Value>(*values.user_setting));
+      augmented_value->SetKey(::onc::kAugmentationUserSetting,
+                              values.user_setting->Clone());
     }
     if (values.shared_setting) {
-      augmented_value->SetWithoutPathExpansion(
-          ::onc::kAugmentationSharedSetting,
-          base::MakeUnique<base::Value>(*values.shared_setting));
+      augmented_value->SetKey(::onc::kAugmentationSharedSetting,
+                              values.shared_setting->Clone());
     }
     if (HasUserPolicy() && values.user_editable) {
       augmented_value->SetKey(::onc::kAugmentationUserEditable,
