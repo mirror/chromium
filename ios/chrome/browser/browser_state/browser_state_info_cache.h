00694393063454d6b5730074b167b703d76cdb8f
jdoerrie <jdoerrie@chromium.org>
2017-08-17 19:09:24 +0200

    Allow only explicit copies of base::Value
    This change deletes base::Values's copy constructor and copy assignment
    operator and introduces base::Value::Clone. It is the purpose of this
    change to disallow implicit copies while still keeping the possibility
    to make explicit copies.
    
    Bug: 646113, 742306
    Change-Id: Idf91382a8dfad1a2b398958e5107b67686696475

--- ios/chrome/browser/browser_state/browser_state_info_cache.h
+++ ios/chrome/browser/browser_state/browser_state_info_cache.h
@@ -65,9 +65,7 @@ class BrowserStateInfoCache {
   const base::DictionaryValue* GetInfoForBrowserStateAtIndex(
       size_t index) const;
   // Saves the browser state info to a cache.
-  void SetInfoForBrowserStateAtIndex(
-      size_t index,
-      std::unique_ptr<base::DictionaryValue> info);
+  void SetInfoForBrowserStateAtIndex(size_t index, base::DictionaryValue info);
 
   std::string CacheKeyFromBrowserStatePath(
       const base::FilePath& browser_state_path) const;
