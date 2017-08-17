9d5a1f2f43aedac0a9d1f91fc19757fa41927ecc
Nick Peterson <nrpeter@google.com>
2017-08-16 20:58:19 -0700

    Prevent dispatch of chrome.webNavigation events when the website is protected by policy.
    Bug: 624649
    Change-Id: Ib203773e0f54e16e96e9df18aba6e1eed38c57b7

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -26,6 +26,7 @@
 #include "chrome/browser/extensions/api/web_navigation/web_navigation_api.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/extensions/extension_with_management_policy_apitest.h"
 #include "chrome/browser/loader/chrome_resource_dispatcher_host_delegate.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/renderer_context_menu/render_view_context_menu_test_util.h"
@@ -485,6 +486,15 @@ IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, ReferenceFragment) {
 IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, SimpleLoad) {
   ASSERT_TRUE(StartEmbeddedTestServer());
   ASSERT_TRUE(RunExtensionTest("webnavigation/simpleLoad")) << message_;
+}
+
+IN_PROC_BROWSER_TEST_F(ExtensionApiTestWithManagementPolicy, PolicySimpleLoad) {
+  {
+    ExtensionManagementPolicyUpdater pref(&policy_provider_);
+    pref.AddRuntimeBlockedHost("*", "*://example.com");
+  }
+  ASSERT_TRUE(StartEmbeddedTestServer());
+  ASSERT_TRUE(RunExtensionTest("webnavigation/policySimpleLoad")) << message_;
 }
 
 // Flaky on Windows and Mac. See http://crbug.com/477480 (Windows) and
