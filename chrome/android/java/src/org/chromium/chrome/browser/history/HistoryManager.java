5060b9c3837f75c9d0b2a9cea031d62e82436f23
Becky Zhou <huayinz@chromium.org>
2017-08-16 17:03:56 -0700

    Hide history info button on a scrolled position when exit from search
    Bug: 756249
    Change-Id: I1615f2a3f2a687c1904401c6a02e3bc4c5b0083a

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManager.java
@@ -74,6 +74,8 @@ public class HistoryManager implements OnMenuItemClickListener, SignInStateObser
 
     private boolean mIsSearching;
     private boolean mShouldShowInfoHeader;
+    private boolean mItemPositionReady;
+
     /**
      * Creates a new HistoryManager.
      * @param activity The Activity associated with the HistoryManager.
@@ -142,12 +144,17 @@ public class HistoryManager implements OnMenuItemClickListener, SignInStateObser
         mRecyclerView.addOnScrollListener(new OnScrollListener() {
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                // This is added to make sure visibility of info button is independent to
+                // info header's position in RecyclerView before onScrolled is called. Otherwise
+                // the info button may be incorrectly shown.
+                mItemPositionReady = true;
+
                 LinearLayoutManager layoutManager =
                         (LinearLayoutManager) recyclerView.getLayoutManager();
                 // Show info button if available if first visible position is close to info header;
                 // otherwise hide info button.
-                mToolbar.updateInfoMenuItem(infoHeaderIsVisible() && shouldShowInfoButton(),
-                        shouldShowInfoHeaderIfAvailable());
+                mToolbar.updateInfoMenuItem(
+                        shouldShowInfoButton(), shouldShowInfoHeaderIfAvailable());
 
                 if (!mHistoryAdapter.canLoadMoreItems()) return;
                 // Load more items if the scroll position is close to the bottom of the list.
@@ -420,8 +427,13 @@ public class HistoryManager implements OnMenuItemClickListener, SignInStateObser
      * @return True if info menu item should be shown on history toolbar, false otherwise.
      */
     boolean shouldShowInfoButton() {
-        return mHistoryAdapter.hasPrivacyDisclaimers() && mHistoryAdapter.getItemCount() > 0
-                && !mToolbar.isSearching() && !mSelectionDelegate.isSelectionEnabled();
+        LinearLayoutManager layoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+        boolean headerIsVisible =
+                !mItemPositionReady || (layoutManager.findFirstVisibleItemPosition() == 0);
+
+        return headerIsVisible && mHistoryAdapter.hasPrivacyDisclaimers()
+                && mHistoryAdapter.getItemCount() > 0 && !mToolbar.isSearching()
+                && !mSelectionDelegate.isSelectionEnabled();
     }
 
     /**
@@ -430,14 +442,6 @@ public class HistoryManager implements OnMenuItemClickListener, SignInStateObser
      */
     boolean shouldShowInfoHeaderIfAvailable() {
         return mShouldShowInfoHeader;
-    }
-
-    /**
-     * @return True if info header is visible on history page.
-     */
-    boolean infoHeaderIsVisible() {
-        LinearLayoutManager layoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
-        return (layoutManager.findFirstVisibleItemPosition() == 0);
     }
 
     @Override
