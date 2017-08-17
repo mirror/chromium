3efc91e3071513e8be2820ceac835b966ab6f1a0
Brett Wilson <brettw@chromium.org>
2017-08-17 15:28:39 -0700

    Open select file dialog for saving memlogs.
    When saving a dump in the out-of-process heap profiler, this now opens a save dialog so the user can select a file name.
    
    The behavior on Android is unchanged pending study on what the save dialog experience looks like.
    
    Change-Id: I67eb90da6e8a7ac2ae4eb4a46a30138da4e7688d

--- chrome/browser/profiling_host/profiling_process_host.cc
+++ chrome/browser/profiling_host/profiling_process_host.cc
@@ -5,11 +5,9 @@
 #include "chrome/browser/profiling_host/profiling_process_host.h"
 
 #include "base/command_line.h"
-#include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task_scheduler/post_task.h"
 #include "build/build_config.h"
-#include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/profiling/constants.mojom.h"
 #include "chrome/common/profiling/profiling_constants.h"
@@ -129,7 +127,8 @@ ProfilingProcessHost* ProfilingProcessHost::GetInstance() {
       base::LeakySingletonTraits<ProfilingProcessHost>>::get();
 }
 
-void ProfilingProcessHost::RequestProcessDump(base::ProcessId pid) {
+void ProfilingProcessHost::RequestProcessDump(base::ProcessId pid,
+                                              const base::FilePath& dest) {
   if (!connector_) {
     LOG(ERROR)
         << "Requesting process dump when profiling process hasn't started.";
@@ -138,7 +137,7 @@ void ProfilingProcessHost::RequestProcessDump(base::ProcessId pid) {
   base::PostTaskWithTraits(
       FROM_HERE, {base::TaskPriority::USER_VISIBLE, base::MayBlock()},
       base::BindOnce(&ProfilingProcessHost::GetOutputFileOnBlockingThread,
-                     base::Unretained(this), pid));
+                     base::Unretained(this), pid, dest));
 }
 
 void ProfilingProcessHost::MakeConnector(
@@ -177,13 +176,11 @@ void ProfilingProcessHost::LaunchAsService() {
                          data_channel.PassClientHandle().release().handle)));
 }
 
-void ProfilingProcessHost::GetOutputFileOnBlockingThread(base::ProcessId pid) {
-  base::FilePath user_data_dir;
-  PathService::Get(chrome::DIR_USER_DATA, &user_data_dir);
-  base::FilePath output_path = user_data_dir.AppendASCII("memlog_dump.json.gz");
-  base::File file(output_path,
+void ProfilingProcessHost::GetOutputFileOnBlockingThread(
+    base::ProcessId pid,
+    const base::FilePath& dest) {
+  base::File file(dest,
                   base::File::FLAG_CREATE_ALWAYS | base::File::FLAG_WRITE);
-
   content::BrowserThread::PostTask(
       content::BrowserThread::IO, FROM_HERE,
       base::BindOnce(&ProfilingProcessHost::HandleDumpProcessOnIOThread,
