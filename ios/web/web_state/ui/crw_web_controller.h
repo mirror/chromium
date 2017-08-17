faeb3a5dabc011cf346c46db8fc588483036fe9c
Danyao Wang <danyao@google.com>
2017-08-17 15:55:07 -0400

    [Nav Experiment] Move |-goToItemAtIndex| to NavigationManagerImpl.
    Common logic that does not depend on CRWSessionController is moved to
    NavigationManagerImpl. The rest is pushed into GoToIndexImpl() in the
    sub classes so either session controller or WKBackForwardList is used.
    
    Bug: 734150, 661316
    Change-Id: I9959e89e3b3314f784d25ad6c59e62a504d9dded

--- ios/web/web_state/ui/crw_web_controller.h
+++ ios/web/web_state/ui/crw_web_controller.h
@@ -185,9 +185,6 @@ class WebStateImpl;
 // used when deliberately pre-triggering a load without displaying.
 - (void)triggerPendingLoad;
 
-// Navigates to the item at the given |index|.
-- (void)goToItemAtIndex:(int)index;
-
 // Executes |script| in the web view, registering user interaction.
 - (void)executeUserJavaScript:(NSString*)script
             completionHandler:(web::JavaScriptResultBlock)completion;
@@ -211,6 +208,15 @@ class WebStateImpl;
 // TODO(stuartmorgan): This is public only temporarily; once refactoring is
 // complete it will be handled internally.
 - (void)restoreStateFromHistory;
+
+// Updates the HTML5 history state of the page using the current NavigationItem.
+// For same-document navigations and navigations affected by
+// window.history.[push/replace]State(), the URL and serialized state object
+// will be updated to the current NavigationItem's values.  A popState event
+// will be triggered for all same-document navigations.  Additionally, a
+// hashchange event will be triggered for same-document navigations where the
+// only difference between the current and previous URL is the fragment.
+- (void)updateHTML5HistoryState;
 
 // Notifies the CRWWebController that it has been shown.
 - (void)wasShown;
