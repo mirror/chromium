5c19b61e21db85e48cde3dd70ea81a4f327f5d32
Karan Bhatia <karandeepb@chromium.org>
2017-08-17 15:51:35 -0700

    Extensions: Change how ManagementPolicy::UserMayLoad is handled.
    Currently when an enabled extension is blocked by ManagementPolicy::UserMayLoad,
    it is unloaded without its preferences being removed. Also, ExtensionPrefs shows
    the state of such extensions as enabled. This can lead to bugs where unloaded
    policy blacklisted extensions can have side-effects. Also,
    ManagementPolicy::UserMayLoad is not checked while enabling a disabled
    extension. This can cause blacklisted extensions to become enabled.
    
    This CL brings the following changes:
      - A new disabled reason DISABLE_BLOCKED_BY_POLICY is introduced.
      - When an enabled extension fails ManagementPolicy::UserMayLoad, it is
        disabled with DISABLE_BLOCKED_BY_POLICY.
      - Hence extensions which fail UserMayLoad checks are now visible to the user 
        on chrome://extensions page, instead of silently remaining installed.
      - They are re-enabled automatically when they satisfy the policy. 
      - ManagementPolicy::MustRemainDisabled also checks
        ManagementPolicy::UserMayLoad now. This leads to cleaner code at call sites
        and helps avoid inconsistent checking of the management policy.
      - Some cases in which disabled policy blacklisted extensions could be 
        re-enabled are fixed.
    
    BUG=709264, 754448, 461747
    TEST=Install EvilProxy (https://chrome.google.com/webstore/detail/evilproxy/jndopnbadbiahpmbnfadmmadjhpjbmon). 
    Ensure navigating to tools.google.com causes a 404. Add the extension to policy
    blacklist. Ensure navigating to tools.google.com works fine. Restart browser. 
    Ensure navigating to tools.google.com works fine.
    TEST=Install EvilProxy. Ensure navigating to tools.google.com causes a 404. 
    Close browser. Add the extension to policy blacklist. Start browser. Ensure 
    navigating to tools.google.com works fine. 
    TEST=Install EvilProxy. Add the extension to policy blacklist. Close browser.
    Remove extension from policy blacklist. Start browser. Ensure navigating to 
    tools.google.com causes a 404.
    TEST=Install EvilProxy. Add the extension to policy blacklist. Ensure navigating
    to tools.google.com works fine. Remove extension from policy blacklist. Ensure
    navigating to tools.google.com causes a 404.
    TEST=Install EvilProxy. Disable the extension from chrome://extensions page. Add
    it to policy blacklist. Ensure it can't be enabled from chrome://extensions.
    
    Change-Id: Ia25d7a2a49bd1c006a435c26397c834ffa5ae632

--- extensions/browser/management_policy.cc
+++ extensions/browser/management_policy.cc
@@ -88,6 +88,12 @@ bool ManagementPolicy::MustRemainEnabled(const Extension* extension,
 bool ManagementPolicy::MustRemainDisabled(const Extension* extension,
                                           Extension::DisableReason* reason,
                                           base::string16* error) const {
+  if (!UserMayLoad(extension, error)) {
+    if (reason)
+      *reason = Extension::DISABLE_BLOCKED_BY_POLICY;
+    return true;
+  }
+
   for (ProviderList::const_iterator it = providers_.begin();
        it != providers_.end(); ++it)
     if ((*it)->MustRemainDisabled(extension, reason, error))
