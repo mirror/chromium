306fd153e9fb54269da75d574e04d681e3065cf3
Jacob Dufault <jdufault@google.com>
2017-08-17 14:08:25 -0700

    cros: Allow tab/shift+tab to navigate to status area.
    Bug: 719015
    Change-Id: Ibbe5b0aac317c5daa161329c3cd85c54bda1c4dd

--- ash/login/ui/lock_screen_sanity_unittest.cc
+++ ash/login/ui/lock_screen_sanity_unittest.cc
@@ -7,7 +7,10 @@
 #include "ash/login/ui/login_test_base.h"
 #include "ash/login/ui/login_test_utils.h"
 #include "ash/public/cpp/config.h"
+#include "ash/session/test_session_controller_client.h"
+#include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
+#include "ash/system/tray/system_tray.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/events/test/event_generator.h"
 #include "ui/views/focus/focus_manager.h"
@@ -16,6 +19,20 @@ using ::testing::_;
 using LockScreenSanityTest = ash::LoginTestBase;
 
 namespace ash {
+namespace {
+
+// Returns true if |view| or any child of it has focus.
+bool HasFocusInAnyChildView(views::View* view) {
+  if (view->HasFocus())
+    return true;
+  for (int i = 0; i < view->child_count(); ++i) {
+    if (HasFocusInAnyChildView(view->child_at(i)))
+      return true;
+  }
+  return false;
+}
+
+}  // namespace
 
 // Verifies that the password input box has focus.
 TEST_F(LockScreenSanityTest, PasswordIsInitiallyFocused) {
@@ -54,6 +71,62 @@ TEST_F(LockScreenSanityTest, PasswordSubmitCallsLockScreenClient) {
   generator.PressKey(ui::KeyboardCode::VKEY_A, 0);
   generator.PressKey(ui::KeyboardCode::VKEY_RETURN, 0);
   base::RunLoop().RunUntilIdle();
+}
+
+// Verifies that tabbing from the lock screen will eventually focus the status
+// area. Then, a shift tab will go back to the lock screen.
+TEST_F(LockScreenSanityTest, TabGoesFromLockToStatusAreaAndBackToLock) {
+  // The status area will not focus out unless we are on the lock screen. See
+  // StatusAreaWidgetDelegate::ShouldFocusOut.
+  GetSessionControllerClient()->SetSessionState(
+      session_manager::SessionState::LOCKED);
+
+  // Create lock screen.
+  auto* lock = new LockContentsView(data_dispatcher());
+  SetUserCount(1);
+  ShowWidgetWithContent(lock);
+  views::View* system_tray =
+      Shell::Get()->GetPrimarySystemTray()->GetWidget()->GetContentsView();
+
+  // Lock has focus.
+  EXPECT_TRUE(HasFocusInAnyChildView(lock));
+  EXPECT_FALSE(HasFocusInAnyChildView(system_tray));
+
+  // Tab (eventually) goes the system tray.
+  for (int i = 0; i < 50; ++i) {
+    GetEventGenerator().PressKey(ui::KeyboardCode::VKEY_TAB, 0);
+    if (!HasFocusInAnyChildView(lock))
+      break;
+  }
+  EXPECT_FALSE(HasFocusInAnyChildView(lock));
+  EXPECT_TRUE(HasFocusInAnyChildView(system_tray));
+
+  // A single shift+tab brings focus back to the lock screen.
+  GetEventGenerator().PressKey(ui::KeyboardCode::VKEY_TAB, ui::EF_SHIFT_DOWN);
+  EXPECT_TRUE(HasFocusInAnyChildView(lock));
+  EXPECT_FALSE(HasFocusInAnyChildView(system_tray));
+}
+
+// Verifies that shift-tabbing from the lock screen will go to the shelf.
+TEST_F(LockScreenSanityTest, ShiftTabGoesToShelf) {
+  // TODO(jdufault|wzang): This test is incomplete. It should verify that tab
+  // goes back to the lock screen. Doing so most likely requires that we are in
+  // a lock screen session state. However, moving to lock screen session state
+  // will currently disable/hide the shelf. See crbug.com/701394.
+
+  auto* lock = new LockContentsView(data_dispatcher());
+  SetUserCount(1);
+  ShowWidgetWithContent(lock);
+  auto* shelf = GetPrimaryShelf()->shelf_widget()->GetContentsView();
+
+  // Lock screen has focus.
+  EXPECT_TRUE(HasFocusInAnyChildView(lock));
+  EXPECT_FALSE(HasFocusInAnyChildView(shelf));
+
+  // A single shift+tab brings focus to the shelf.
+  GetEventGenerator().PressKey(ui::KeyboardCode::VKEY_TAB, ui::EF_SHIFT_DOWN);
+  EXPECT_FALSE(HasFocusInAnyChildView(lock));
+  EXPECT_TRUE(HasFocusInAnyChildView(shelf));
 }
 
 }  // namespace ash
