456dc61720e9cb01a1dd734a0067289d03051c20
Kurt Horimoto <kkhorimoto@chromium.org>
2017-08-16 20:25:35 -0700

    [iOS] Created BrowserCoordinatorObserver.
    This observer protocol allows objects to register to be notified about
    BrowserCoordinator lifecycle events.
    
    Bug: none
    Change-Id: Ia90b165bf0974f68afa6f8e31e7feff41880756e

--- ios/chrome/browser/ui/coordinators/browser_coordinator.mm
+++ ios/chrome/browser/ui/coordinators/browser_coordinator.mm
@@ -4,14 +4,19 @@
 
 #import "ios/chrome/browser/ui/coordinators/browser_coordinator.h"
 
+#import "base/ios/block_types.h"
 #import "base/logging.h"
 #import "ios/chrome/browser/ui/coordinators/browser_coordinator+internal.h"
+#import "ios/chrome/browser/ui/coordinators/browser_coordinator_observer.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
 @interface BrowserCoordinator ()
+// The observers added to this object.
+@property(nonatomic, strong)
+    NSMutableSet<id<BrowserCoordinatorObserver>>* observers;
 // Child coordinators owned by this object.
 @property(nonatomic, strong)
     NSMutableSet<BrowserCoordinator*>* childCoordinators;
@@ -19,11 +24,18 @@
 @property(nonatomic, readwrite, weak) BrowserCoordinator* parentCoordinator;
 @property(nonatomic, readwrite) BOOL started;
 @property(nonatomic, readwrite) BOOL overlaying;
+
+// Sends |selector| to observers.
+- (void)notifyObservers:(SEL)selector;
+// Waits until any current transition animations have finished before sending
+// |selector| to observers.
+- (void)notifyObserversAfterTransition:(SEL)selector;
+
 @end
 
 @implementation BrowserCoordinator
-
 @synthesize browser = _browser;
+@synthesize observers = _observers;
 @synthesize childCoordinators = _childCoordinators;
 @synthesize parentCoordinator = _parentCoordinator;
 @synthesize started = _started;
@@ -31,9 +43,20 @@
 
 - (instancetype)init {
   if (self = [super init]) {
+    _observers = [NSMutableSet set];
     _childCoordinators = [NSMutableSet set];
   }
   return self;
+}
+
+- (void)dealloc {
+  for (BrowserCoordinator* child in self.children) {
+    [self removeChildCoordinator:child];
+  }
+  for (id<BrowserCoordinatorObserver> observer in self.observers) {
+    [observer browserCoordinatorWasDestroyed:self];
+  }
+  DCHECK(!self.observers.count);
 }
 
 #pragma mark - Public API
@@ -43,7 +66,10 @@
     return;
   }
   self.started = YES;
+  [self notifyObservers:@selector(browserCoordinatorDidStart:)];
   [self.parentCoordinator childCoordinatorDidStart:self];
+  [self notifyObserversAfterTransition:@selector
+        (browserCoordinatorConsumerDidStart:)];
 }
 
 - (void)stop {
@@ -55,11 +81,45 @@
   for (BrowserCoordinator* child in self.children) {
     [child stop];
   }
+  [self notifyObservers:@selector(browserCoordinatorDidStop:)];
+  [self notifyObserversAfterTransition:@selector
+        (browserCoordinatorConsumerDidStop:)];
 }
 
-- (void)dealloc {
-  for (BrowserCoordinator* child in self.children) {
-    [self removeChildCoordinator:child];
+- (void)addObserver:(id<BrowserCoordinatorObserver>)observer {
+  DCHECK(observer);
+  [self.observers addObject:observer];
+}
+
+- (void)removeObserver:(id<BrowserCoordinatorObserver>)observer {
+  DCHECK(observer);
+  [self.observers removeObject:observer];
+}
+
+#pragma mark -
+
+- (void)notifyObservers:(SEL)selector {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+  for (id<BrowserCoordinatorObserver> observer in self.observers) {
+    if ([observer respondsToSelector:selector])
+      [observer performSelector:selector withObject:self];
+  }
+#pragma clang diagnostic pop
+}
+
+- (void)notifyObserversAfterTransition:(SEL)selector {
+  id<UIViewControllerTransitionCoordinator> transitionCoordinator =
+      self.viewController.transitionCoordinator;
+  if (transitionCoordinator) {
+    __weak BrowserCoordinator* weakSelf = self;
+    [transitionCoordinator
+        animateAlongsideTransition:nil
+                        completion:^(id context) {
+                          [weakSelf notifyObservers:selector];
+                        }];
+  } else {
+    [self notifyObservers:selector];
   }
 }
 
