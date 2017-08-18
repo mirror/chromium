58fbec11f04fd213e4b19e076f389f7428004992
Ivan Sandrk <isandrk@google.com>
2017-08-18 14:43:22 +0200

    Migrate CloudPolicyManager to use TaskScheduler
    Bug:689520,729082
    Change-Id: Ia7fe12ba44dec0c107f4ddaf3f1e9b0f5aa55095

--- components/policy/core/common/configuration_policy_provider_test.h
+++ components/policy/core/common/configuration_policy_provider_test.h
@@ -11,16 +11,12 @@
 #include "base/callback_forward.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/message_loop/message_loop.h"
+#include "base/test/scoped_task_environment.h"
 #include "build/build_config.h"
 #include "components/policy/core/common/policy_types.h"
 #include "components/policy/core/common/schema.h"
 #include "components/policy/core/common/schema_registry.h"
 #include "testing/gtest/include/gtest/gtest.h"
-
-#if defined(OS_POSIX)
-#include "base/files/file_descriptor_watcher_posix.h"
-#endif
 
 namespace base {
 class DictionaryValue;
@@ -58,12 +54,7 @@ class PolicyTestBase : public testing::Test {
 
   SchemaRegistry schema_registry_;
 
-  // Needed by FilePathWatcher, which is used by ConfigDirPolicyLoader and
-  // PolicyLoaderMac.
-  base::MessageLoopForIO loop_;
-#if defined(OS_POSIX)
-  base::FileDescriptorWatcher file_descriptor_watcher_;
-#endif
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(PolicyTestBase);
