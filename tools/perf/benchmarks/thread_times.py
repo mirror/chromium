f83877bdd49f3c70dd79b1c50e3cd7abfb11d48c
Ned Nguyen <nednguyen@google.com>
2017-08-17 20:15:52 -0400

    Add vmiura@ as owner of thread_times.key_mobile_sites_smooth
    Change-Id: I14ed0e251c2fccd3b5700560a3050e728f918737

--- tools/perf/benchmarks/thread_times.py
+++ tools/perf/benchmarks/thread_times.py
@@ -78,7 +78,8 @@ class ThreadTimesKeyHitTestCases(_ThreadTimes):
 
 
 @benchmark.Enabled('android')
-class ThreadTimesFastPathMobileSites(_ThreadTimes):
+@benchmark.Owner(emails=['vmiura@chromium.org'])
+class ThreadTimesKeyMobileSitesSmooth(_ThreadTimes):
   """Measures timeline metrics while performing smoothness action on
   key mobile sites labeled with fast-path tag.
   http://www.chromium.org/developers/design-documents/rendering-benchmarks"""
