5493ac4cd331865e56da4bc19ec3ac48619aba92
Luna Lu <loonybear@chromium.org>
2017-08-18 15:47:21 -0400

    Support complete feature policy syntax in iframe attribute
    This requies update current syntax to adop CSP syntax, for more details please
    refer to the doc: https://docs.google.com/document/d/1903nztB5tHN_8g_GDtNT9KkkjRAuid1eaS8kT99mlDs/edit
    
    For consistency, feature policy http header policy will also be using the same
    syntax.
    
    Bug:726739
    Change-Id: Iac71b7ef971b58fa1f125b13d6e472b15450cb47

--- third_party/WebKit/Source/platform/feature_policy/FeaturePolicy.cpp
+++ third_party/WebKit/Source/platform/feature_policy/FeaturePolicy.cpp
@@ -8,6 +8,7 @@
 #include "platform/json/JSONValues.h"
 #include "platform/network/HTTPParsers.h"
 #include "platform/weborigin/SecurityOrigin.h"
+#include "platform/wtf/BitVector.h"
 #include "platform/wtf/PtrUtil.h"
 
 namespace blink {
@@ -15,66 +16,85 @@ namespace blink {
 WebParsedFeaturePolicy ParseFeaturePolicy(const String& policy,
                                           RefPtr<SecurityOrigin> origin,
                                           Vector<String>* messages) {
-  return ParseFeaturePolicy(policy, origin, messages,
-                            GetDefaultFeatureNameMap());
+  return ParseContainerPolicy(policy, origin, RefPtr<SecurityOrigin>(),
+                              messages, GetDefaultFeatureNameMap());
 }
 
-WebParsedFeaturePolicy ParseFeaturePolicy(const String& policy,
-                                          RefPtr<SecurityOrigin> origin,
-                                          Vector<String>* messages,
-                                          const FeatureNameMap& feature_names) {
+Vector<WebParsedFeaturePolicyDeclaration> ParseContainerPolicy(
+    const String& policy,
+    RefPtr<SecurityOrigin> self_origin,
+    RefPtr<SecurityOrigin> src_origin,
+    Vector<String>* messages) {
+  return ParseContainerPolicy(policy, self_origin, src_origin, messages,
+                              GetDefaultFeatureNameMap());
+}
+
+Vector<WebParsedFeaturePolicyDeclaration> ParseContainerPolicy(
+    const String& policy,
+    RefPtr<SecurityOrigin> self_origin,
+    RefPtr<SecurityOrigin> src_origin,
+    Vector<String>* messages,
+    const FeatureNameMap& feature_names) {
   Vector<WebParsedFeaturePolicyDeclaration> whitelists;
+  BitVector features_specified(
+      static_cast<int>(WebFeaturePolicyFeature::LAST_FEATURE));
 
-  // Use a reasonable parse depth limit; the actual maximum depth is only going
-  // to be 4 for a valid policy, but we'll give the featurePolicyParser a chance
-  // to report more specific errors, unless the string is really invalid.
-  std::unique_ptr<JSONArray> policy_items = ParseJSONHeader(policy, 50);
-  if (!policy_items) {
-    if (messages)
-      messages->push_back("Unable to parse header.");
-    return whitelists;
-  }
-
-  for (size_t i = 0; i < policy_items->size(); ++i) {
-    JSONObject* item = JSONObject::Cast(policy_items->at(i));
-    if (!item) {
-      if (messages)
-        messages->push_back("Policy is not an object.");
-      continue;  // Array element is not an object; skip
-    }
-
-    for (size_t j = 0; j < item->size(); ++j) {
-      JSONObject::Entry entry = item->at(j);
-      if (!feature_names.Contains(entry.first))
-        continue;  // Unrecognized feature; skip
-      WebFeaturePolicyFeature feature = feature_names.at(entry.first);
-      JSONArray* targets = JSONArray::Cast(entry.second);
-      if (!targets) {
-        if (messages)
-          messages->push_back("Whitelist is not an array of strings.");
+  // RFC2616, section 4.2 specifies that headers appearing multiple times can be
+  // combined with a comma. Walk the header string, and parse each comma
+  // separated chunk as a separate header.
+  Vector<String> policy_items;
+  // policy_items = [ policy *( "," [ policy ] ) ]
+  policy.Split(',', policy_items);
+  for (const String& item : policy_items) {
+    Vector<String> entry_list;
+    // entry_list = [ entry *( ";" [ entry ] ) ]
+    item.Split(';', entry_list);
+    for (const String& entry : entry_list) {
+      // Split removes extra whitespaces by default
+      //     "name value1 value2" or "name".
+      Vector<String> tokens;
+      entry.Split(' ', tokens);
+      if (!feature_names.Contains(tokens[0])) {
+        messages->push_back("Unrecognized feature: '" + tokens[0] + "'.");
         continue;
       }
 
+      WebFeaturePolicyFeature feature = feature_names.at(tokens[0]);
+      // If a policy has already been specified for the current feature, drop
+      // the new policy.
+      if (features_specified.QuickGet(static_cast<int>(feature)))
+        continue;
+
       WebParsedFeaturePolicyDeclaration whitelist;
       whitelist.feature = feature;
+      features_specified.QuickSet(static_cast<int>(feature));
       Vector<WebSecurityOrigin> origins;
-      String target_string;
-      for (size_t j = 0; j < targets->size(); ++j) {
-        if (targets->at(j)->AsString(&target_string)) {
-          if (EqualIgnoringASCIICase(target_string, "self")) {
-            if (!origin->IsUnique())
-              origins.push_back(origin);
-          } else if (target_string == "*") {
-            whitelist.matches_all_origins = true;
-          } else {
-            WebSecurityOrigin target_origin =
-                WebSecurityOrigin::CreateFromString(target_string);
-            if (!target_origin.IsNull() && !target_origin.IsUnique())
-              origins.push_back(target_origin);
-          }
+      // If a policy entry has no (optional) values, only valid syntax for allow
+      // attribute (e,g, allow="feature_name1; feature_name2 value"), enable the
+      // feature for src origin.
+      if (tokens.size() == 1) {
+        DCHECK(src_origin);
+        origins.push_back(src_origin);
+      }
+
+      for (size_t i = 1; i < tokens.size(); i++) {
+        if (EqualIgnoringASCIICase(tokens[i], "'self'")) {
+          origins.push_back(self_origin);
+        } else if (EqualIgnoringASCIICase(tokens[i], "'src'")) {
+          origins.push_back(src_origin);
+        } else if (EqualIgnoringASCIICase(tokens[i], "'none'")) {
+          continue;
+        } else if (tokens[i] == "*") {
+          whitelist.matches_all_origins = true;
+          break;
         } else {
-          if (messages)
-            messages->push_back("Whitelist is not an array of strings.");
+          WebSecurityOrigin target_origin =
+              WebSecurityOrigin::CreateFromString(tokens[i]);
+          if (!target_origin.IsNull() && !target_origin.IsUnique()) {
+            origins.push_back(target_origin);
+          } else {
+            messages->push_back("Unrecognized origin: '" + tokens[i] + "'.");
+          }
         }
       }
       whitelist.origins = origins;
--- third_party/WebKit/Source/platform/feature_policy/FeaturePolicy.h
+++ third_party/WebKit/Source/platform/feature_policy/FeaturePolicy.h
@@ -23,23 +23,38 @@ namespace blink {
 typedef HashMap<String, WebFeaturePolicyFeature> FeatureNameMap;
 PLATFORM_EXPORT const FeatureNameMap& GetDefaultFeatureNameMap();
 
-// Converts a JSON feature policy string into a vector of whitelists, one for
+// Converts a feature policy string into a vector of whitelists, one for
 // each feature specified. Unrecognized features are filtered out. If |messages|
-// is not null, then any errors in the input will cause an error message to be
+// is not null, then any message in the input will cause a warning message to be
 // appended to it.
+// Example of a feature policy string:
+//     "vibrate a.com b.com; fullscreen 'none'; payment 'self', payment *".
 PLATFORM_EXPORT WebParsedFeaturePolicy
 ParseFeaturePolicy(const String& policy,
                    RefPtr<SecurityOrigin>,
                    Vector<String>* messages);
 
-// Converts a JSON feature policy string into a vector of whitelists (see
-// comments above), with an explicit FeatureNameMap. This method is primarily
-// used for testing.
-PLATFORM_EXPORT WebParsedFeaturePolicy
-ParseFeaturePolicy(const String& policy,
-                   RefPtr<SecurityOrigin>,
-                   Vector<String>* messages,
-                   const FeatureNameMap& feature_names);
+// Converts a contianer policy string into a vector of whitelists, given self
+// and src origins provided, one for each feature specified. Unrecognized
+// features are filtered out. If |messages| is not null, then any message in the
+// input will cause as warning message to be appended to it.
+// Example of a feature policy string:
+//     "vibrate a.com 'src'; fullscreen 'none'; payment 'self', payment *".
+PLATFORM_EXPORT Vector<WebParsedFeaturePolicyDeclaration> ParseContainerPolicy(
+    const String& policy,
+    RefPtr<SecurityOrigin>,
+    RefPtr<SecurityOrigin>,
+    Vector<String>* messages,
+    const FeatureNameMap& feature_names);
+
+// Converts a feature policy string into a vector of whitelists (see comments
+// above), with an explicit FeatureNameMap. This method is primarily used for
+// testing.
+PLATFORM_EXPORT Vector<WebParsedFeaturePolicyDeclaration> ParseContainerPolicy(
+    const String& policy,
+    RefPtr<SecurityOrigin>,
+    RefPtr<SecurityOrigin>,
+    Vector<String>* messages);
 
 // Verifies whether feature policy is enabled and |feature| is supported in
 // feature policy.
