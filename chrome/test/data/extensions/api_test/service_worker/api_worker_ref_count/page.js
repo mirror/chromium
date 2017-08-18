3851a32448b9fceb2ea4253baca27780014df31a
Istiaque Ahmed <lazyboy@chromium.org>
2017-08-17 18:00:09 -0700

    Deflake and enable ServiceWorkerTest.WorkerRefCount.
    There are two fixes:
      - JS should have sent SUCCESS message after two messages from worker, it
        incorrectly sent SUCCESS on each of those messages. Fix it.
      - Decrement ref count message initiated from renderer/ asynchronously
        reaches IO thread. Previously GetWorkerRefCount could have inspected
        the ref count even before the IPC reaches IO thread on the browser/.
        Make sure we wait for those IPCs to arrive in the browser/ process.
    
    Bug: 702126
    Change-Id: I67a10f4371b4f9831ec2bb74b126f89ccc190f48

--- chrome/test/data/extensions/api_test/service_worker/api_worker_ref_count/page.js
+++ chrome/test/data/extensions/api_test/service_worker/api_worker_ref_count/page.js
@@ -16,6 +16,7 @@ window.runServiceWorker = function() {
   });
 };
 
+var numWorkerReplyReceived = 0;
 window.testSendMessage = function() {
   if (worker == null) {
     chrome.test.sendMessage(FAILURE_MESSAGE);
@@ -24,7 +25,10 @@ window.testSendMessage = function() {
   var channel = new MessageChannel();
   channel.port1.onmessage = function(e) {
     if (e.data == 'Worker reply: Hello world') {
-      chrome.test.sendMessage('SUCCESS');
+      ++numWorkerReplyReceived;
+      if (numWorkerReplyReceived == 2) {
+        chrome.test.sendMessage('SUCCESS');
+      }
     } else {
       chrome.test.sendMessage(FAILURE_MESSAGE);
     }
