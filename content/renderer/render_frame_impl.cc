cde65f43dc7cc53a2fedf80947b658d2bb4ccd0f
Kinuko Yasuda <kinuko@chromium.org>
2017-08-18 18:28:50 +0900

    Implement ServiceWorkerSubresourceLoader
    This patch:
    - Creates SWSubresourceLoader in CreateURLLoader for SW-controlled clients
    - The SubresourceLoader intercepts requests and directly dispatches Fetch events to BrowserSideSWEventDispatcher
    - The BrowserSideSWEventDispatcher then forwards the events to renderer-side SWEventDispatcher
    
    Bug: 715640
    Change-Id: I3cf7e8cbca4648560a7849ed7e30b463e8f67ca5

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -3534,10 +3534,12 @@ void RenderFrameImpl::DidCreateDocumentLoader(
   if (document_loader->GetServiceWorkerNetworkProvider())
     return;
 
+  RenderThreadImpl* render_thread = RenderThreadImpl::current();
   document_loader->SetServiceWorkerNetworkProvider(
       ServiceWorkerNetworkProvider::CreateForNavigation(
           routing_id_, navigation_state->request_params(), frame_,
-          content_initiated));
+          content_initiated,
+          render_thread ? GetDefaultURLLoaderFactoryGetter() : nullptr));
 }
 
 void RenderFrameImpl::DidStartProvisionalLoad(
