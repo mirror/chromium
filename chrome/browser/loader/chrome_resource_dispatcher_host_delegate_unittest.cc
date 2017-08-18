07dbf9a84d460afab0990b7a1caac99d353d1c37
Megan Jablonski <megjablon@google.com>
2017-08-17 18:42:57 -0700

    Enable the NetworkTimeTracker on Android. StartTimeFetch when lite pages are enabled.
    Allow on demand queries of the NetworkTimeTracker on Android. When lite
    pages are enabled for a page load, start a network time fetch.
    
    Bug: 748843
    Change-Id: I546fa2f237837739a3850fd6c59a4174f0befcfe

--- chrome/browser/loader/chrome_resource_dispatcher_host_delegate_unittest.cc
+++ chrome/browser/loader/chrome_resource_dispatcher_host_delegate_unittest.cc
@@ -6,17 +6,30 @@
 
 #include <memory>
 
+#include "base/run_loop.h"
+#include "base/test/simple_test_clock.h"
+#include "base/test/simple_test_tick_clock.h"
+#include "base/time/time.h"
 #include "chrome/browser/loader/chrome_navigation_data.h"
 #include "chrome/browser/loader/chrome_resource_dispatcher_host_delegate.h"
+#include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile_manager.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_data.h"
+#include "components/network_time/network_time_test_utils.h"
+#include "components/network_time/network_time_tracker.h"
+#include "components/prefs/testing_pref_service.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_data.h"
+#include "content/public/test/test_browser_thread.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "net/base/request_priority.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+#include "net/test/embedded_test_server/http_response.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_context.h"
+#include "net/url_request/url_request_test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
@@ -74,4 +87,86 @@ TEST_F(ChromeResourceDispatcherHostDelegateTest,
       static_cast<ChromeNavigationData*>(
           delegate->GetNavigationData(fake_request.get()));
   EXPECT_FALSE(chrome_navigation_data->GetDataReductionProxyData());
+}
+
+class ChromeResourceDispatcherHostDelegateTimeTrackerTest
+    : public ChromeRenderViewHostTestHarness {
+ public:
+  ChromeResourceDispatcherHostDelegateTimeTrackerTest()
+      : clock_(new base::SimpleTestClock),
+        tick_clock_(new base::SimpleTestTickClock),
+        test_server_(new net::EmbeddedTestServer),
+        field_trial_test_(new network_time::FieldTrialTest()),
+        profile_manager_(
+            new TestingProfileManager(TestingBrowserProcess::GetGlobal())) {
+    SetThreadBundleOptions(content::TestBrowserThreadBundle::REAL_IO_THREAD);
+    network_time::NetworkTimeTracker::RegisterPrefs(pref_service_.registry());
+  }
+  ~ChromeResourceDispatcherHostDelegateTimeTrackerTest() override {}
+
+  void SetUp() override {
+    ChromeRenderViewHostTestHarness::SetUp();
+    ASSERT_TRUE(profile_manager_->SetUp());
+    tracker_.reset(new network_time::NetworkTimeTracker(
+        std::unique_ptr<base::Clock>(clock_),
+        std::unique_ptr<base::TickClock>(tick_clock_), &pref_service_,
+        new net::TestURLRequestContextGetter(
+            content::BrowserThread::GetTaskRunnerForThread(
+                content::BrowserThread::IO))));
+    // Do this to be sure that |is_null| returns false.
+    clock_->Advance(base::TimeDelta::FromDays(111));
+    tick_clock_->Advance(base::TimeDelta::FromDays(222));
+  }
+
+  void TearDown() override { ChromeRenderViewHostTestHarness::TearDown(); }
+
+  void WaitForUIThread() {
+    base::RunLoop run_loop;
+    content::BrowserThread::PostTask(content::BrowserThread::UI, FROM_HERE,
+                                     run_loop.QuitClosure());
+    run_loop.Run();
+  }
+
+  network_time::NetworkTimeTracker* tracker() { return tracker_.get(); }
+  net::EmbeddedTestServer* test_server() { return test_server_.get(); }
+  network_time::FieldTrialTest* field_trial_test() {
+    return field_trial_test_.get();
+  }
+
+ private:
+  base::SimpleTestClock* clock_;
+  base::SimpleTestTickClock* tick_clock_;
+
+  TestingPrefServiceSimple pref_service_;
+  std::unique_ptr<network_time::NetworkTimeTracker> tracker_;
+  std::unique_ptr<net::EmbeddedTestServer> test_server_;
+  std::unique_ptr<network_time::FieldTrialTest> field_trial_test_;
+  // Set up TestingProfileManager for extensions::UserScriptListener.
+  std::unique_ptr<TestingProfileManager> profile_manager_;
+};
+
+TEST_F(ChromeResourceDispatcherHostDelegateTimeTrackerTest, TimeQueryStarted) {
+  std::unique_ptr<ChromeResourceDispatcherHostDelegate> delegate(
+      new ChromeResourceDispatcherHostDelegate());
+
+  base::Time network_time;
+  base::TimeDelta uncertainty;
+  EXPECT_EQ(network_time::NetworkTimeTracker::NETWORK_TIME_NO_SYNC_ATTEMPT,
+            tracker()->GetNetworkTime(&network_time, &uncertainty));
+
+  // Enable network time queries.
+  test_server()->RegisterRequestHandler(
+      base::Bind(&network_time::GoodTimeResponseHandler));
+  EXPECT_TRUE(test_server()->Start());
+  tracker()->SetTimeServerURLForTesting(test_server()->GetURL("/"));
+  field_trial_test()->SetNetworkQueriesWithVariationsService(
+      true, 0.0, network_time::NetworkTimeTracker::FETCHES_ON_DEMAND_ONLY);
+  delegate->StartNetworkTimeTrackerFetch(tracker());
+  WaitForUIThread();
+
+  tracker()->WaitForFetchForTesting(123123123);
+  base::RunLoop().RunUntilIdle();
+
+  EXPECT_EQ(network_time::NetworkTimeTracker::NETWORK_TIME_AVAILABLE,
+            tracker()->GetNetworkTime(&network_time, &uncertainty));
 }
