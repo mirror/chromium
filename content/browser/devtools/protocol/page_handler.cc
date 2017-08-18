a5228446ee18a92bea97f1d95b5ce7f4fdee12e8
David Vallet <dvallet@chromium.org>
2017-08-18 13:16:02 +1000

    initial version of the headless download manager delegate
    patch from issue 2886693002 at patchset 100001 (http://crrev.com/2886693002#ps100001)
    Following fixes:
    - Added Deny/Allow behavior tests, fixed setting deny by default
    - Changed using TaskScheduler for FILE thread post tasks, since use of BrowserThread::FILE is deprecated
    - Merged setDownloadBehavior and setDownloadDirectory into setDownloadBehaviour with downloadPath optional parameter.
    - Additional cleanup
    
    
    
    BUG=696481
    
    Change-Id: Ib4385e478e89219062241983a6ceb523bf476d09

--- content/browser/devtools/protocol/page_handler.cc
+++ content/browser/devtools/protocol/page_handler.cc
@@ -22,13 +22,17 @@
 #include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "content/browser/devtools/devtools_session.h"
+#include "content/browser/devtools/protocol/devtools_download_manager_delegate.h"
+#include "content/browser/devtools/protocol/devtools_download_manager_helper.h"
 #include "content/browser/devtools/protocol/emulation_handler.h"
 #include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/browser/renderer_host/render_widget_host_view_base.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/browser/web_contents/web_contents_view.h"
 #include "content/common/view_messages.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/download_manager.h"
 #include "content/public/browser/javascript_dialog_manager.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
@@ -611,10 +615,50 @@ Response PageHandler::BringToFront() {
   return Response::InternalError();
 }
 
+Response PageHandler::SetDownloadBehavior(const std::string& behavior,
+                                          Maybe<std::string> download_path) {
+  WebContentsImpl* web_contents = GetWebContents();
+  if (!web_contents)
+    return Response::InternalError();
+
+  OverrideDownloadDelegate();
+  // Ensure that there is one helper attached. If there's already one we reuse
+  // it.
+  DevToolsDownloadManagerHelper::CreateForWebContents(web_contents);
+  DevToolsDownloadManagerHelper* download_helper =
+      DevToolsDownloadManagerHelper::FromWebContents(web_contents);
+
+  DevToolsDownloadManagerHelper::DownloadBehavior download_behavior =
+      DevToolsDownloadManagerHelper::DownloadBehavior::DENY;
+  if (behavior == "allow")
+    download_behavior = DevToolsDownloadManagerHelper::DownloadBehavior::ALLOW;
+  if (behavior == "default")
+    download_behavior =
+        DevToolsDownloadManagerHelper::DownloadBehavior::DEFAULT;
+  download_helper->SetDownloadBehavior(download_behavior);
+  if (download_path.isJust())
+    download_helper->SetDownloadPath(download_path.fromJust());
+
+  return Response::OK();
+}
+
 WebContentsImpl* PageHandler::GetWebContents() {
   return host_ ?
       static_cast<WebContentsImpl*>(WebContents::FromRenderFrameHost(host_)) :
       nullptr;
+}
+
+void PageHandler::OverrideDownloadDelegate() {
+  WebContentsImpl* web_contents = GetWebContents();
+  if (!web_contents)
+    return;
+  content::BrowserContext* browser_context = web_contents->GetBrowserContext();
+  if (!browser_context)
+    return;
+
+  content::DownloadManager* download_manager =
+      content::BrowserContext::GetDownloadManager(browser_context);
+  DevToolsDownloadManagerDelegate::TakeOver(download_manager);
 }
 
 void PageHandler::NotifyScreencastVisibility(bool visible) {
