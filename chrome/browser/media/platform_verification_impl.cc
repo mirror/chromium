0eb6740ea69789d3f25421a7b2a84f378bde29da
John Rummell <jrummell@chromium.org>
2017-08-18 16:23:21 -0700

    Add GetStorageId() to PlatformVerification
    Add mojo implementation of GetStorageId(). As it will be supported on multiple
    platforms, move the current PlatformVerification out of the chromeos directory.
    The existing ChallengePlatform() is only available on ChromeOS, so only
    include it with ChromeOS builds, and simply return failure if called on
    other platforms.
    
    BUG=478960
    TEST=future CL
    
    Change-Id: Ib79bd5815d6d03eb1dd2011baeff68ca860f7f4c

--- chrome/browser/media/platform_verification_impl.cc
+++ chrome/browser/media/platform_verification_impl.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/attestation/platform_verification_impl.h"
+#include "chrome/browser/media/platform_verification_impl.h"
 
 #include <utility>
 
@@ -10,9 +10,6 @@
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_contents.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
-
-namespace chromeos {
-namespace attestation {
 
 using media::mojom::PlatformVerification;
 
@@ -30,12 +27,14 @@ void PlatformVerificationImpl::Create(
 
 PlatformVerificationImpl::PlatformVerificationImpl(
     content::RenderFrameHost* render_frame_host)
-    : render_frame_host_(render_frame_host), weak_factory_(this) {
+    : weak_factory_(this) {
   DCHECK(render_frame_host);
+#if defined(OS_CHROMEOS)
+  render_frame_host_ = render_frame_host;
+#endif
 }
 
-PlatformVerificationImpl::~PlatformVerificationImpl() {
-}
+PlatformVerificationImpl::~PlatformVerificationImpl() {}
 
 void PlatformVerificationImpl::ChallengePlatform(
     const std::string& service_id,
@@ -44,16 +43,28 @@ void PlatformVerificationImpl::ChallengePlatform(
   DVLOG(2) << __FUNCTION__;
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
+#if defined(OS_CHROMEOS)
   if (!platform_verification_flow_.get())
-    platform_verification_flow_ = new PlatformVerificationFlow();
+    platform_verification_flow_ =
+        base::MakeRefCounted<chromeos::attestation::PlatformVerificationFlow>();
 
   platform_verification_flow_->ChallengePlatformKey(
       content::WebContents::FromRenderFrameHost(render_frame_host_), service_id,
       challenge,
       base::Bind(&PlatformVerificationImpl::OnPlatformChallenged,
                  weak_factory_.GetWeakPtr(), base::Passed(&callback)));
+#else
+  // Not supported, so return failure.
+  std::move(callback).Run(false, std::string(), std::string(), std::string());
+#endif  // defined(OS_CHROMEOS)
 }
 
+void PlatformVerificationImpl::GetStorageId(GetStorageIdCallback callback) {
+  // TODO(jrummell): Implement this. http://crbug.com/478960
+  std::move(callback).Run(std::vector<uint8_t>());
+}
+
+#if defined(OS_CHROMEOS)
 void PlatformVerificationImpl::OnPlatformChallenged(
     ChallengePlatformCallback callback,
     Result result,
@@ -63,7 +74,7 @@ void PlatformVerificationImpl::OnPlatformChallenged(
   DVLOG(2) << __FUNCTION__ << ": " << result;
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
-  if (result != PlatformVerificationFlow::SUCCESS) {
+  if (result != chromeos::attestation::PlatformVerificationFlow::SUCCESS) {
     DCHECK(signed_data.empty());
     DCHECK(signature.empty());
     DCHECK(platform_key_certificate.empty());
@@ -78,6 +89,4 @@ void PlatformVerificationImpl::OnPlatformChallenged(
   std::move(callback).Run(true, signed_data, signature,
                           platform_key_certificate);
 }
-
-}  // namespace attestation
-}  // namespace chromeos
+#endif  // defined(OS_CHROMEOS)
