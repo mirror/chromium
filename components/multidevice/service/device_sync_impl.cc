e2f78ca6b8de55856bb1a11fa4cbb6842dcc911a
Regan Hsu <hsuregan@google.com>
2017-08-18 15:35:10 -0700

    [CrOS MultiDevice] Device Sync Multidevice API and associated unit test
    Bug:
    Change-Id: Iee8859dfdb54ba45a0d206784075b3d68d929e8b

---
+++ components/multidevice/service/device_sync_impl.cc
@@ -0,0 +1,33 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/multidevice/service/device_sync_impl.h"
+
+#include "base/logging.h"
+
+namespace multidevice {
+
+DeviceSyncImpl::DeviceSyncImpl(
+    std::unique_ptr<service_manager::ServiceContextRef> service_ref)
+    : service_ref_(std::move(service_ref)) {}
+
+DeviceSyncImpl::~DeviceSyncImpl() {}
+
+void DeviceSyncImpl::ForceEnrollmentNow() {
+  LOG(ERROR) << "ForceEnrollmentNow called!";
+  // listeners_.ForAllPtrs([](device_sync::mojom::DeviceSyncObserverPtr*
+  // listener) { LOG(ERROR) << "c'mon now";
+  //   //listener->OnEnrollmentFinished(true /* success */);
+  // });
+}
+
+void DeviceSyncImpl::ForceSyncNow() {}
+
+void DeviceSyncImpl::AddObserver(
+    device_sync::mojom::DeviceSyncObserverPtr observer) {
+  LOG(ERROR) << "AddObserver called!";
+  // listeners_.AddPtr(std::move(observer));
+}
+
+}  // namespace multidevice
