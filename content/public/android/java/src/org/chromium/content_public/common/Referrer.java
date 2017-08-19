bc1e081342359373f59f5b8e734bdd8b661d867b
Yusuf Ozuysal <yusufo@google.com>
2017-08-18 17:35:55 -0700

    Changes to policy around referrers in external intents
    - Add a java class for WebReferrerPolicy so that values can be reflected
    trutfully wrt Blink equivalents
    - Add a new extra to define referral policy the client wants to add.
    - Add a way to include https:// referrers if the client app can be
    verified to have a 1st party relationship with the referrer domain.
    
    BUG=725921
    
    Change-Id: Ib3a0dfdabefe5669646fffac5924a675db7cc5eb

--- content/public/android/java/src/org/chromium/content_public/common/Referrer.java
+++ content/public/android/java/src/org/chromium/content_public/common/Referrer.java
@@ -4,16 +4,20 @@
 
 package org.chromium.content_public.common;
 
+import org.chromium.blink_public.web.WebReferrerPolicy;
+
 /**
  * Container that holds together a referrer URL along with the referrer policy set on the
  * originating frame. This corresponds to native content/public/common/referrer.h.
  */
 public class Referrer {
+    // TODO(yusufo): Remove these constants now that org.chromium.blink_public.web.WebReferrerPolicy
+    // exists.
     /** @see blink::WebReferrerPolicyAlways */
-    public static final int REFERRER_POLICY_ALWAYS = 0;
+    public static final int REFERRER_POLICY_ALWAYS = WebReferrerPolicy.WEB_REFERRER_POLICY_ALWAYS;
 
     /** @see blink::WebReferrerPolicyDefault */
-    public static final int REFERRER_POLICY_DEFAULT = 1;
+    public static final int REFERRER_POLICY_DEFAULT = WebReferrerPolicy.WEB_REFERRER_POLICY_DEFAULT;
 
     private final String mUrl;
     private final int mPolicy;
