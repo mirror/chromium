4a44a92fded4be00b515267aabb86bc8c5966691
Rob Buis <rob.buis@samsung.com>
2017-08-21 13:39:10 -0400

    Implements CSSPropertyAPI for clip/origin longhand properties
    As part of Project Ribbon, separate the parsing logic for CSS properties
    -webkit-mask-clip, -webkit-mask-origin, -webkit-background-clip and
    -webkit-background-origin from the parser into an API.
    
    Bug: 668012
    
    Signed-off-by: Rob Buis <rob.buis@samsung.com>
    Change-Id: I049f06489c33321e8a316b5eccaf7a77c9a80585

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIWebkitOrigin.cpp
@@ -0,0 +1,22 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSPropertyAPIWebkitOrigin.h"
+
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+
+namespace blink {
+
+const CSSValue* CSSPropertyAPIWebkitOrigin::ParseSingleValue(
+    CSSPropertyID,
+    CSSParserTokenRange& range,
+    const CSSParserContext&,
+    const CSSParserLocalContext&) const {
+  return CSSPropertyParserHelpers::ConsumeCommaSeparatedList(
+      CSSPropertyParserHelpers::ConsumePrefixedBackgroundBox, range,
+      false /* allow_text_value */);
+}
+
+}  // namespace blink
