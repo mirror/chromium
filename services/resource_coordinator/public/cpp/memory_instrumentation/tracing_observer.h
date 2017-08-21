32483420dbf0751437aed624a349c527a0754b28
Hector Dearman <hjd@google.com>
2017-08-21 17:44:22 +0100

    memory-infra: Remove dumping on behalf of other processes
    This removes the ability for ProcessDumpProviders to dump on
    behalf of other processes. This feature was supported for
    collecting OS statistics for many processes from the browser
    process which was necessary on some platforms due to sandboxing.
    
    Now that OS statistics are collected separately anyway we don't need the
    feature and is the cause of unnecessary complexity (not to mention the
    service refactoring has slightly broken it already).
    
    Bug: 728199
    Change-Id: I702cfedd62dc795076e566344494db69e013fbd9

--- services/resource_coordinator/public/cpp/memory_instrumentation/tracing_observer.h
+++ services/resource_coordinator/public/cpp/memory_instrumentation/tracing_observer.h
@@ -28,7 +28,6 @@ class SERVICES_RESOURCE_COORDINATOR_PUBLIC_CPP_EXPORT TracingObserver
   void OnTraceLogDisabled() override;
 
   bool AddDumpToTraceIfEnabled(const base::trace_event::MemoryDumpRequestArgs&,
-                               const base::ProcessId,
                                const base::trace_event::ProcessMemoryDump*);
   bool AddOsDumpToTraceIfEnabled(
       const base::trace_event::MemoryDumpRequestArgs&,
