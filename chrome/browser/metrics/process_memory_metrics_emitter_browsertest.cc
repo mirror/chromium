ac94e8f0ce39ef2d5c95ca573f43f54feee984dc
Steven Holte <holte@google.com>
2017-08-21 12:00:53 -0700

    Convert ResourceCoordinator to NavigationId UKMs
    Bug: 754895
    Change-Id: I9a7d8b8021e942e8ed3ae90f3dae5d640768bfd6

--- chrome/browser/metrics/process_memory_metrics_emitter_browsertest.cc
+++ chrome/browser/metrics/process_memory_metrics_emitter_browsertest.cc
@@ -156,22 +156,6 @@ class ProcessMemoryMetricsEmitterTest : public InProcessBrowserTest {
  protected:
   std::unique_ptr<ukm::TestAutoSetUkmRecorder> test_ukm_recorder_;
 
-  void CheckUkmSourcesWithUrl(const GURL& url, size_t count) {
-    std::vector<const ukm::UkmSource*> sources =
-        test_ukm_recorder_->GetSourcesForUrl(url.spec().c_str());
-
-    // There should be at least |count|, and not more than 2 * |count| renderers
-    // with this URL.
-    EXPECT_GE(sources.size(), count) << "Expected at least " << count
-                                     << " renderers with url: " << url.spec();
-    EXPECT_LE(sources.size(), 2 * count);
-
-    // Each one should have renderer type.
-    for (const ukm::UkmSource* source : sources) {
-      EXPECT_TRUE(ProcessHasTypeForSource(source, ProcessType::RENDERER));
-    }
-  }
-
   void CheckAllUkmSources(size_t metric_count = 1u) {
     const std::map<ukm::SourceId, std::unique_ptr<ukm::UkmSource>>& sources =
         test_ukm_recorder_->GetSources();
@@ -259,7 +243,6 @@ IN_PROC_BROWSER_TEST_F(ProcessMemoryMetricsEmitterTest,
   run_loop.Run();
 
   CheckAllMemoryMetrics(histogram_tester, 1);
-  CheckUkmSourcesWithUrl(url1, 1);
   CheckAllUkmSources();
 }
 
@@ -316,7 +299,6 @@ IN_PROC_BROWSER_TEST_F(ProcessMemoryMetricsEmitterTest,
                   MemoryDumpType::EXPLICITLY_TRIGGERED))));
 
   CheckAllMemoryMetrics(histogram_tester, 1);
-  CheckUkmSourcesWithUrl(url1, 1);
   CheckAllUkmSources();
 }
 
@@ -345,6 +327,5 @@ IN_PROC_BROWSER_TEST_F(ProcessMemoryMetricsEmitterTest, MAYBE_FetchThreeTimes) {
   run_loop.Run();
 
   CheckAllMemoryMetrics(histogram_tester, count);
-  CheckUkmSourcesWithUrl(url1, 1);
   CheckAllUkmSources(count);
 }
