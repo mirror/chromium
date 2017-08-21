98a3caef1f1bb4da55db72f480a645412cb7675a
Tomasz Garbus <tgarbus@chromium.org>
2017-08-21 10:08:04 +0200

    Created CredentialManager class which binds JS messages with CredentialManagerImpl
    CredentialManager instance should be owned by PasswordController if
    the feature is enabled.
    Implemented HandleScriptCommand method, which can be registered
    as a script command callback for "credentials" command prefix.
    It parses the dictionary received from JavaScript side and calls
    Get/Store/PreventSilentAccess on CredentialManagerImpl.
    
    Bug: 435048
    Change-Id: I3f3eacb4dbd0e3ef6f0afa280d7046810b56a587

---
+++ ios/chrome/browser/passwords/credential_manager_util.cc
@@ -0,0 +1,136 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/passwords/credential_manager_util.h"
+
+bool CredentialManager::ParseMediationRequirement(
+    const base::DictionaryValue& json,
+    CredentialMediationRequirement* mediation) {
+  std::string mediation_str;
+  if (!json.HasKey("mediation")) {
+    *mediation = CredentialMediationRequirement::kOptional;
+    return true;
+  }
+  if (!json.GetString("mediation", &mediation_str)) {
+    // Dictionary contains |mediation| field but it is not a valid string.
+    return false;
+  }
+  if (mediation_str == "silent") {
+    *mediation = CredentialMediationRequirement::kSilent;
+    return true;
+  }
+  if (mediation_str == "required") {
+    *mediation = CredentialMediationRequirement::kRequired;
+    return true;
+  }
+  if (mediation_str == "optional") {
+    *mediation = CredentialMediationRequirement::kOptional;
+    return true;
+  }
+  // |mediation| value is not in {"silent", "required", "optional"}.
+  // Dictionary is invalid then.
+  return false;
+}
+
+bool CredentialManager::ParseIncludePasswords(const base::DictionaryValue& json,
+                                              bool* include_passwords) {
+  if (!json.HasKey("password")) {
+    *include_passwords = false;
+    return true;
+  }
+  if (!json.GetBoolean("password", include_passwords)) {
+    // Dictionary contains |password| field but it is not a boolean. That means
+    // dictionary is invalid.
+    return false;
+  }
+  return true;
+}
+
+bool CredentialManager::ParseFederations(const base::DictionaryValue& json,
+                                         std::vector<GURL>* federations) {
+  federations->clear();
+  if (!json.HasKey("providers")) {
+    // No |providers| list.
+    return true;
+  }
+  const base::ListValue* lst = nullptr;
+  if (!json.GetList("providers", &lst)) {
+    // Dictionary has |providers| field but it is not a list. That means
+    // dictionary is invalid.
+    return false;
+  }
+  for (size_t i = 0; i < lst->GetSize(); i++) {
+    std::string s;
+    if (!lst->GetString(i, &s)) {
+      // Element of |providers| is invalid string.
+      return false;
+    }
+    GURL gurl(s);
+    if (gurl.is_valid()) {
+      // Skip the invalid URLs. See
+      // https://w3c.github.io/webappsec-credential-management/#provider-identification
+      federations->push_back(GURL(s));
+    }
+  }
+  return true;
+}
+
+bool CredentialManager::ParseCredentialType(const base::DictionaryValue& json,
+                                            CredentialType* credential_type) {
+  std::string str;
+  if (!json.GetString("type", &str)) {
+    // Credential must contain |type|
+    return false;
+  }
+  if (str == "PasswordCredential") {
+    *credential_type = CredentialType::CREDENTIAL_TYPE_PASSWORD;
+    return true;
+  }
+  if (str == "FederatedCredential") {
+    *credential_type = CredentialType::CREDENTIAL_TYPE_FEDERATED;
+    return true;
+  }
+  return false;
+}
+
+bool CredentialManager::ParseCredentialDictionary(
+    const base::DictionaryValue& json,
+    CredentialInfo* credential) {
+  if (!json.GetString("id", &credential->id)) {
+    // |id| is required.
+    return false;
+  }
+  json.GetString("name", &credential->name);
+  std::string iconURL;
+  if (json.GetString("iconURL", &iconURL) && !iconURL.empty()) {
+    credential->icon = GURL(iconURL);
+    if (!credential->icon.is_valid() ||
+        !web::IsOriginSecure(credential->icon)) {
+      // |iconURL| is either not a valid URL or not a secure URL.
+      return false;
+    }
+  }
+  if (!ParseCredentialType(json, &credential->type)) {
+    // Credential has invalid |type|
+    return false;
+  }
+  if (credential->type == CredentialType::CREDENTIAL_TYPE_PASSWORD) {
+    if (!json.GetString("password", &credential->password) ||
+        credential->password.empty()) {
+      // |password| field is required for PasswordCredential.
+      return false;
+    }
+  }
+  if (credential->type == CredentialType::CREDENTIAL_TYPE_FEDERATED) {
+    std::string federation;
+    json.GetString("provider", &federation);
+    if (!GURL(federation).is_valid()) {
+      // |provider| field must be a valid URL. See
+      // https://w3c.github.io/webappsec-credential-management/#provider-identification
+      return false;
+    }
+    credential->federation = url::Origin(GURL(federation));
+  }
+  return true;
+}
