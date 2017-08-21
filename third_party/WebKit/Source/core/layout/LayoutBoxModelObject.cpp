07de4d79d402f0e4185c23d934f81a68b49fcd36
Xianzhu Wang <wangxianzhu@chromium.org>
2017-08-21 15:27:20 -0700

    Invalidate on change of stacked status
    Change of stacked status may cause change of PaintLayer's
    CompositingContainer which may have painted a cached subsequence
    containing the object.
    
    BUG=756522
    
    Change-Id: I6d0fa32254cedaf6a5d0a9271a6b01cc8c11d4c8

--- third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBoxModelObject.cpp
@@ -226,22 +226,31 @@ void LayoutBoxModelObject::WillBeDestroyed() {
 
 void LayoutBoxModelObject::StyleWillChange(StyleDifference diff,
                                            const ComputedStyle& new_style) {
-  // This object's layer may begin or cease to be a stacking context, in which
-  // case the paint invalidation container of this object and descendants may
-  // change. Thus we need to invalidate paint eagerly for all such children.
-  // PaintLayerCompositor::paintInvalidationOnCompositingChange() doesn't work
-  // for the case because we can only see the new paintInvalidationContainer
-  // during compositing update.
-  if (!RuntimeEnabledFeatures::SlimmingPaintV2Enabled() && Style() &&
-      Style()->IsStackingContext() != new_style.IsStackingContext() &&
-      // InvalidatePaintIncludingNonCompositingDescendants() requires this.
+  // This object's layer may begin or cease to be stacked or stacking context,
+  // in which case the paint invalidation container of this object and
+  // descendants may change. Thus we need to invalidate paint eagerly for all
+  // such children. PaintLayerCompositor::paintInvalidationOnCompositingChange()
+  // doesn't work for the case because we can only see the new
+  // paintInvalidationContainer during compositing update.
+  // For SPv2, change of stacked/stacking context status may cause change of the
+  // PaintLayer's CompositingContainer, we need to eagerly invalidate the
+  // current compositing container which may have painted a cached subsequence
+  // containing this object.
+  if (Style() &&
+      (Style()->IsStacked() != new_style.IsStacked() ||
+       Style()->IsStackingContext() != new_style.IsStackingContext()) &&
+      // ObjectPaintInvalidator requires this.
       IsRooted()) {
-    // The following disablers are valid because we need to invalidate based on
-    // the current status.
-    DisableCompositingQueryAsserts compositing_disabler;
-    DisablePaintInvalidationStateAsserts paint_disabler;
-    ObjectPaintInvalidator(*this)
-        .InvalidatePaintIncludingNonCompositingDescendants();
+    if (RuntimeEnabledFeatures::SlimmingPaintV2Enabled()) {
+      ObjectPaintInvalidator(*this).SlowSetPaintingLayerNeedsRepaint();
+    } else {
+      // The following disablers are valid because we need to invalidate based
+      // on the current status.
+      DisableCompositingQueryAsserts compositing_disabler;
+      DisablePaintInvalidationStateAsserts paint_disabler;
+      ObjectPaintInvalidator(*this)
+          .InvalidatePaintIncludingNonCompositingDescendants();
+    }
   }
 
   if (HasLayer() && diff.CssClipChanged())
