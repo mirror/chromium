e1ad24e93c1f8d8bb03e035bed8a69682ee58bce
Troy Hildebrandt <thildebr@chromium.org>
2017-08-21 14:01:25 -0700

    [Home] Fork the Android support library bottom navigation menu.
    Brings the bottom nav menu into a new third_party directory, so we can
    make local modifications for Chrome Home.
    
    Bug:738492
    Change-Id: I0855cf54a7a03db6b8fdcf928ff47e90415a666a

---
+++ chrome/android/third_party/bottom_nav/java/src/org/chromium/chrome/browser/widget/bottomsheet/base/BottomNavigationAnimationHelperIcs.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.chromium.chrome.browser.widget.bottomsheet.base;
+
+import android.support.design.internal.TextScale;
+import android.support.transition.AutoTransition;
+import android.support.transition.TransitionManager;
+import android.support.transition.TransitionSet;
+import android.support.v4.view.animation.FastOutSlowInInterpolator;
+import android.view.ViewGroup;
+
+/** Forked from android.support.design.internal.BottomNavigationAnimationHelperIcs */
+public class BottomNavigationAnimationHelperIcs extends BottomNavigationAnimationHelperBase {
+    private static final long ACTIVE_ANIMATION_DURATION_MS = 115L;
+
+    private final TransitionSet set;
+
+    BottomNavigationAnimationHelperIcs() {
+        set = new AutoTransition();
+        set.setOrdering(TransitionSet.ORDERING_TOGETHER);
+        set.setDuration(ACTIVE_ANIMATION_DURATION_MS);
+        set.setInterpolator(new FastOutSlowInInterpolator());
+        TextScale textScale = new TextScale();
+        set.addTransition(textScale);
+    }
+
+    void beginDelayedTransition(ViewGroup view) {
+        TransitionManager.beginDelayedTransition(view, set);
+    }
+}
