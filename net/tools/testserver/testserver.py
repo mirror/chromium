de7607f7f98380f894410cb870e8f514f7c9f523
Sergey Ulanov <sergeyu@google.com>
2017-08-21 14:36:33 -0700

    Add TestServerConfig to allow testserver outside localhost
    Previuosly testserver was always assumed to be running on localhost
    even when started with RemoteTestServer. This CL adds TestServerConfig
    that allows to pass to the tests configuratgion for the test server.
    The config specifies how the tests should connect to the test spawner
    and to the test server itself. The config also replaces the mechanism
    that was used to pass spawner's port number on Android (through
    net-test-server-ports file).
    
    Bug: 731302
    Change-Id: Ib2e07e6d293c52164b12b73dfd5ebe13d89d018d

--- net/tools/testserver/testserver.py
+++ net/tools/testserver/testserver.py
@@ -1920,7 +1920,7 @@ class ServerRunner(testserver_base.TestServerRunner):
 
           (pem_cert_and_key, intermediate_cert_der) = \
               minica.GenerateCertKeyAndIntermediate(
-                  subject = "127.0.0.1",
+                  subject = self.options.hostname,
                   ca_issuers_url = ("http://%s:%d/ca_issuers" %
                                     (host, self.__ocsp_server.server_port)),
                   serial = self.options.cert_serial)
@@ -1992,9 +1992,9 @@ class ServerRunner(testserver_base.TestServerRunner):
                 self.options.ocsp_produced)
 
           (pem_cert_and_key, ocsp_der) = minica.GenerateCertKeyAndOCSP(
-              subject = "127.0.0.1",
+              subject = self.options.hostname,
               ocsp_url = ("http://%s:%d/ocsp" %
-                  (host, self.__ocsp_server.server_port)),
+                  (self.options.hostname, self.__ocsp_server.server_port)),
               ocsp_states = ocsp_states,
               ocsp_dates = ocsp_dates,
               ocsp_produced = ocsp_produced,
@@ -2162,6 +2162,11 @@ class ServerRunner(testserver_base.TestServerRunner):
     self.option_parser.add_option('--https', action='store_true',
                                   dest='https', help='Specify that https '
                                   'should be used.')
+    self.option_parser.add_option('--hostname', default='127.0.0.1',
+                                  dest='hostname',
+                                  help='Hostname that the tests use to '
+                                  'connect to the server. Used when '
+                                  'generating certificates.')
     self.option_parser.add_option('--cert-and-key-file',
                                   dest='cert_and_key_file', help='specify the '
                                   'path to the file containing the certificate '
