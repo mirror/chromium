14dd7f33d216103589e3fcfbe834a88f7878bf90
Shuhei Takahashi <nya@chromium.org>
2017-08-21 16:02:58 +0900

    recent: Improve logic to gather recent files.
    Formerly, RecentModel simply merged recent file lists returned from
    various sources without filtering them at all, and it was
    RecentSource's responsibility to limit a recent file list to certain
    size and exclude too old files.
    
    However this naive approach is not intuitive to users; for example,
    a file modified yesterday might not be shown if there are many other
    files modified today on the same source, while a file modified last
    month can be shown if it is on a different source.
    
    With this patch, Recent view will show the first 1000 items of the
    complete list of all covered files (possibly containing millions of
    items) sorted by descending order of last modified time.
    
    To compute this list, we query each source for 1000 recently modified
    files, merge the lists, and take only the first 1000 items. This
    requires RecentModel to know the last modified time of files returned
    from sources, so RecentSource::GetRecentFiles() is modified to return
    std::vector<RecentFile> (where RecentFile is essentially a pair of
    FileSystemURL and last modified time), instead of
    std::vector<FileSystemURL>.
    
    Bug: 742722
    Test: unit_tests
    Change-Id: I193e837a7eb28f9475c834e05489f6e6dd957f1d

--- chrome/browser/chromeos/fileapi/recent_model.h
+++ chrome/browser/chromeos/fileapi/recent_model.h
@@ -6,6 +6,7 @@
 #define CHROME_BROWSER_CHROMEOS_FILEAPI_RECENT_MODEL_H_
 
 #include <memory>
+#include <queue>
 #include <vector>
 
 #include "base/callback_forward.h"
@@ -16,6 +17,7 @@
 #include "base/optional.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "chrome/browser/chromeos/fileapi/recent_file.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "storage/browser/fileapi/file_system_url.h"
 
@@ -35,9 +37,8 @@ extern const size_t kMaxFilesFromSingleSource;
 // All member functions must be called on the UI thread.
 class RecentModel : public KeyedService {
  public:
-  using RecentFileList = std::vector<storage::FileSystemURL>;
   using GetRecentFilesCallback =
-      base::OnceCallback<void(const RecentFileList& files)>;
+      base::OnceCallback<void(const std::vector<RecentFile>& files)>;
 
   ~RecentModel() override;
 
@@ -48,9 +49,11 @@ class RecentModel : public KeyedService {
   static std::unique_ptr<RecentModel> CreateForTest(
       std::vector<std::unique_ptr<RecentSource>> sources);
 
-  // Returns a list of recent files by querying sources. Results are internally
-  // cached for better performance.
-  void GetRecentFiles(RecentContext context, GetRecentFilesCallback callback);
+  // Returns a list of recent files by querying sources.
+  // Files are sorted by descending order of last modified time.
+  // Results might be internally cached for better performance.
+  void GetRecentFiles(const RecentContext& context,
+                      GetRecentFilesCallback callback);
 
   // KeyedService overrides:
   void Shutdown() override;
@@ -64,14 +67,15 @@ class RecentModel : public KeyedService {
   explicit RecentModel(Profile* profile);
   explicit RecentModel(std::vector<std::unique_ptr<RecentSource>> sources);
 
-  void OnGetRecentFiles(RecentFileList files);
+  void OnGetRecentFiles(const RecentContext& context,
+                        std::vector<RecentFile> files);
   void OnGetRecentFilesCompleted();
   void ClearCache();
 
   std::vector<std::unique_ptr<RecentSource>> sources_;
 
-  // Cached RecentFileList.
-  base::Optional<RecentFileList> cached_files_ = base::nullopt;
+  // Cached RecentFile list.
+  base::Optional<std::vector<RecentFile>> cached_files_ = base::nullopt;
 
   // Timer to clear the cache.
   base::OneShotTimer cache_clear_timer_;
@@ -87,7 +91,8 @@ class RecentModel : public KeyedService {
   int num_inflight_sources_ = 0;
 
   // Intermediate container of recent files while building a list.
-  RecentFileList intermediate_files_;
+  std::priority_queue<RecentFile, std::vector<RecentFile>, RecentFileComparator>
+      intermediate_files_;
 
   base::WeakPtrFactory<RecentModel> weak_ptr_factory_;
 
