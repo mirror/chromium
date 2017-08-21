8d0b17d880c4bf65c0a7dfa1d621ea894b3f15bc
yiyix <yiyix@chromium.org>
2017-08-21 00:19:12 -0400

    add needs_blending
    add needs_blending
    
    Bug:
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I6fa880fa0ecdaae6ffb294b2e40542bf3bc287df

--- components/exo/surface.cc
+++ components/exo/surface.cc
@@ -705,12 +705,13 @@ void Surface::AppendContentsToFrame(const gfx::Point& origin,
       } else if (state_.opaque_region.isRect()) {
         opaque_rect = gfx::SkIRectToRect(state_.opaque_region.getBounds());
       }
+      bool needs_blending = !opaque_rect.Contains(quad_rect);
 
       texture_quad->SetNew(
-          quad_state, quad_rect, opaque_rect, quad_rect, current_resource_.id,
-          true /* premultiplied_alpha */, uv_top_left, uv_bottom_right,
-          SK_ColorTRANSPARENT /* background_color */, vertex_opacity,
-          false /* y_flipped */, false /* nearest_neighbor */,
+          quad_state, quad_rect, opaque_rect, quad_rect, needs_blending,
+          current_resource_.id, true /* premultiplied_alpha */, uv_top_left,
+          uv_bottom_right, SK_ColorTRANSPARENT /* background_color */,
+          vertex_opacity, false /* y_flipped */, false /* nearest_neighbor */,
           state_.only_visible_on_secure_output);
       if (current_resource_.is_overlay_candidate)
         texture_quad->set_resource_size_in_pixels(current_resource_.size);
