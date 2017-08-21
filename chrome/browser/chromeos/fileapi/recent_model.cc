0f2b964dc4f6da858e6572947cc4e1d90b05cfff
Shuhei Takahashi <nya@chromium.org>
2017-08-21 16:02:58 +0900

    recent: Improve logic to gather recent files.
    Formerly, RecentModel simply merged recent file lists returned from
    various sources without filtering them, and it was RecentSource's
    responsibility to limit a recent file list to certain size and exclude
    too old files.
    
    TODO(nya): Explain more
    
    Bug: 742722
    Test: unit_tests
    Change-Id: I193e837a7eb28f9475c834e05489f6e6dd957f1d

--- chrome/browser/chromeos/fileapi/recent_model.cc
+++ chrome/browser/chromeos/fileapi/recent_model.cc
@@ -18,6 +18,7 @@
 #include "chrome/browser/chromeos/fileapi/recent_context.h"
 #include "chrome/browser/chromeos/fileapi/recent_download_source.h"
 #include "chrome/browser/chromeos/fileapi/recent_drive_source.h"
+#include "chrome/browser/chromeos/fileapi/recent_file.h"
 #include "chrome/browser/chromeos/fileapi/recent_model_factory.h"
 #include "chrome/browser/chromeos/fileapi/recent_source.h"
 #include "content/public/browser/browser_thread.h"
@@ -71,7 +72,7 @@ RecentModel::~RecentModel() {
   DCHECK(sources_.empty());
 }
 
-void RecentModel::GetRecentFiles(RecentContext context,
+void RecentModel::GetRecentFiles(const RecentContext& context,
                                  GetRecentFilesCallback callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
@@ -102,9 +103,9 @@ void RecentModel::GetRecentFiles(RecentContext context,
   }
 
   for (const auto& source : sources_) {
-    source->GetRecentFiles(context,
-                           base::BindOnce(&RecentModel::OnGetRecentFiles,
-                                          weak_ptr_factory_.GetWeakPtr()));
+    source->GetRecentFiles(
+        context, base::BindOnce(&RecentModel::OnGetRecentFiles,
+                                weak_ptr_factory_.GetWeakPtr(), context));
   }
 }
 
@@ -116,14 +117,17 @@ void RecentModel::Shutdown() {
   sources_.clear();
 }
 
-void RecentModel::OnGetRecentFiles(RecentFileList files) {
+void RecentModel::OnGetRecentFiles(const RecentContext& context,
+                                   std::vector<RecentFile> files) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   DCHECK_LT(0, num_inflight_sources_);
 
-  intermediate_files_.insert(intermediate_files_.end(),
-                             std::make_move_iterator(files.begin()),
-                             std::make_move_iterator(files.end()));
+  for (const auto& file : files)
+    intermediate_files_.emplace(file);
+
+  while (intermediate_files_.size() > kMaxFilesFromSingleSource)
+    intermediate_files_.pop();
 
   --num_inflight_sources_;
   if (num_inflight_sources_ == 0)
@@ -137,8 +141,12 @@ void RecentModel::OnGetRecentFilesCompleted() {
   DCHECK(!cached_files_.has_value());
   DCHECK(!build_start_time_.is_null());
 
-  cached_files_ = RecentFileList();
-  cached_files_.value().swap(intermediate_files_);
+  cached_files_ = std::vector<RecentFile>();
+  while (!intermediate_files_.empty()) {
+    cached_files_.value().emplace_back(intermediate_files_.top());
+    intermediate_files_.pop();
+  }
+  std::reverse(cached_files_.value().begin(), cached_files_.value().end());
 
   DCHECK(cached_files_.has_value());
   DCHECK(intermediate_files_.empty());
