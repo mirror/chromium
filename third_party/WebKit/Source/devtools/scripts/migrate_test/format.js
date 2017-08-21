1b0dcc222d32f88c7359b2c43ec40ce901cd67a0
Will Chen <chenwilliam@chromium.org>
2017-08-21 11:08:58 -0700

    DevTools: format tests - inspector/console (part 1)
    Bug: 667560
    Change-Id: I02c0fef9c64c8d0846f97a12f28caa308b9d3eaa

---
+++ third_party/WebKit/Source/devtools/scripts/migrate_test/format.js
@@ -0,0 +1,97 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const path = require('path');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const prettier = require('prettier');
+var Linter = require('eslint').Linter;
+
+const utils = require('../utils');
+
+function main() {
+  // walkSync('../../../../LayoutTests/http/tests/inspector', formatFile);
+  walkSync('../../../../LayoutTests/http/tests/inspector-enabled', formatFile);
+  walkSync('../../../../LayoutTests/http/tests/inspector-unit', formatFile);
+  walkSync('../../../../LayoutTests/inspector-enabled', formatFile);
+  // walkSync('../../../../LayoutTests/inspector', formatFile);
+  walkSync('../../../../LayoutTests/inspector/console', formatFile);
+}
+
+function walkSync(currentDirPath, process) {
+  fs.readdirSync(currentDirPath).forEach(function(name) {
+    if (name === 'resources') {
+      return;
+    }
+    let filePath = path.join(currentDirPath, name);
+    let stat = fs.statSync(filePath);
+    if (stat.isFile() && (filePath.endsWith('.html'))) {
+      try {
+        process(filePath);
+      } catch (err) {
+        console.log(filePath, 'has err:', err);
+      }
+    } else if (stat.isDirectory()) {
+      walkSync(filePath, process);
+    }
+  });
+}
+
+function formatFile(filePath) {
+  const regex = /<script>([\S\s]*?)<\/script>/;
+  let scriptCount = 0;
+  const scripts = [];
+
+  let htmlContents = fs.readFileSync(filePath, 'utf-8');
+
+  while (htmlContents.match(regex)) {
+    extractScriptContents();
+  }
+
+  for (var i = 0; i < scripts.length; i++) {
+    prettyPrint(i, filePath);
+  }
+
+  fs.writeFileSync(filePath, htmlContents);
+
+  function extractScriptContents() {
+    scripts.push(htmlContents.match(regex)[1]);
+    htmlContents = htmlContents.replace(regex, sentinel(scriptCount));
+    scriptCount++;
+  }
+
+  function prettyPrint(i) {
+    let contents = scripts[i];
+    let linter = new Linter();
+
+    let result = linter.verifyAndFix(contents, {
+      envs: ['browser', 'mocha'],
+      useEslintrc: false,
+      parserOptions: {
+        ecmaVersion: 8,
+      },
+      rules: {
+        curly: [2, 'all'],
+      }
+    });
+    if (result.messages.length) {
+      console.log('Issue with eslint', result.messages, 'for file: ', filePath);
+      process.exit(1);
+    }
+    contents = result.output;
+    const formatted = prettier
+                          .format(contents, {tabWidth: 2, printWidth: 120, singleQuote: true})
+                          // Need to escape $ symbol
+                          .split('$')
+                          .join('$$');
+
+    htmlContents = htmlContents.replace(sentinel(i), '<script>\n' + formatted + '</script>');
+  }
+
+  function sentinel(n) {
+    return '@@SUPER_SECRET_SCRIPT_' + n + '@@';
+  }
+}
+
+main();
