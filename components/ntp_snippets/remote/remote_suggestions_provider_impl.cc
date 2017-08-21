8628dd474b154f0999ab22ba14e71ee6537b861f
Vitalii Iarko <vitaliii@chromium.org>
2017-08-21 11:57:32 +0200

    [NTP] Allow triggering suggestion notifications easier for testing.
    Previously, triggering a suggestion notification for testing was quite a
    nondeterministic process, which rarely worked (at least for me).
    
    In this CL:
    
    1) A button to clean notification state is added to snippets internals.
    It hides existing notifications, clears auto opt out (consecutive
    ignored count) and quota (notifications sent and last date) prefs.
    
    2) The button to fetch in the background now has 2 seconds delay.
    
    3) Another about::flags option to force notifications client side. This
    is done by simply overwriting notification info after receiving
    suggestions.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I575271dc1dac3dde9a3ed549fcd4ad67f2471130

--- components/ntp_snippets/remote/remote_suggestions_provider_impl.cc
+++ components/ntp_snippets/remote/remote_suggestions_provider_impl.cc
@@ -187,6 +187,20 @@ bool IsSignedOutUsersSubscriptionForPushedSuggestionsEnabled() {
       kEnableSignedOutUsersSubscriptionForPushedSuggestionsDefault);
 }
 
+// Whether notification info is overriden for fetched suggestions. Note that
+// this param does not overwrite other switches which could disable these
+// notifications.
+const bool kForceFetchedSuggestionsNotificationsDefault = false;
+const char kForceFetchedSuggestionsNotificationsParamName[] =
+    "force_fetched_suggestions_notifications";
+
+bool ShouldForceFetchedSuggestionsNotifications() {
+  return base::GetFieldTrialParamByFeatureAsBool(
+      ntp_snippets::kNotificationsFeature,
+      kForceFetchedSuggestionsNotificationsParamName,
+      kForceFetchedSuggestionsNotificationsDefault);
+}
+
 template <typename SuggestionPtrContainer>
 std::unique_ptr<std::vector<std::string>> GetSuggestionIDVector(
     const SuggestionPtrContainer& suggestions) {
@@ -735,11 +749,17 @@ void RemoteSuggestionsProviderImpl::OnFetchFinished(
     return;
   }
 
-  if (!IsFetchedSuggestionsNotificationsEnabled()) {
-    if (fetched_categories) {
-      for (FetchedCategory& fetched_category : *fetched_categories) {
-        for (std::unique_ptr<RemoteSuggestion>& suggestion :
-             fetched_category.suggestions) {
+  if (fetched_categories) {
+    const base::Time deadline = clock_->Now() + base::TimeDelta::FromDays(7);
+    for (FetchedCategory& fetched_category : *fetched_categories) {
+      for (std::unique_ptr<RemoteSuggestion>& suggestion :
+           fetched_category.suggestions) {
+        if (ShouldForceFetchedSuggestionsNotifications() &&
+            IsFetchedSuggestionsNotificationsEnabled()) {
+          suggestion->set_should_notify(true);
+          suggestion->set_notification_deadline(deadline);
+        }
+        if (!IsFetchedSuggestionsNotificationsEnabled()) {
           suggestion->set_should_notify(false);
         }
       }
