58368d7869f1a7635ab296c17b6e1c75a1491b09
Mike West <mkwst@chromium.org>
2017-08-22 01:35:24 +0200

    Secure contexts: remove user-data-dir requirement.
    Requiring developers to create a new profile in order to debug features
    that need secure contexts is overkill. Let's loosen that requirement.
    
    Bug: 757631
    Change-Id: Ic8edf5172f11f6e2bdff94b9d45b1bd9fbbacf7d

--- chrome/common/secure_origin_whitelist.cc
+++ chrome/common/secure_origin_whitelist.cc
@@ -11,13 +11,11 @@
 
 std::vector<GURL> GetSecureOriginWhitelist() {
   std::vector<GURL> origins;
-  // If kUnsafelyTreatInsecureOriginAsSecure option is given and
-  // kUserDataDir is present, add the given origins as trustworthy
-  // for whitelisting.
+  // If kUnsafelyTreatInsecureOriginAsSecure option is given, then treat the
+  // value as a comma-separated list of origins:
   const base::CommandLine& command_line =
       *base::CommandLine::ForCurrentProcess();
-  if (command_line.HasSwitch(switches::kUnsafelyTreatInsecureOriginAsSecure) &&
-      command_line.HasSwitch(switches::kUserDataDir)) {
+  if (command_line.HasSwitch(switches::kUnsafelyTreatInsecureOriginAsSecure)) {
     std::string origins_str = command_line.GetSwitchValueASCII(
         switches::kUnsafelyTreatInsecureOriginAsSecure);
     for (const std::string& origin : base::SplitString(
