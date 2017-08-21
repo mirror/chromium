a6e18495d2ad1154350c87a1e83825d514500bee
Tomasz Garbus <tgarbus@chromium.org>
2017-08-21 15:19:02 +0200

    Created CredentialManager class which binds JS messages with CredentialManagerImpl
    CredentialManager instance should be owned by PasswordController if
    the feature is enabled.
    Implemented HandleScriptCommand method, which can be registered
    as a script command callback for "credentials" command prefix.
    It parses the dictionary received from JavaScript side and calls
    Get/Store/PreventSilentAccess on CredentialManagerImpl.
    
    Bug: 435048
    Change-Id: I3f3eacb4dbd0e3ef6f0afa280d7046810b56a587

---
+++ ios/chrome/browser/passwords/credential_manager_util.h
@@ -0,0 +1,33 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PASSWORDS_CREDENTIAL_MANAGER_UTIL_H_
+#define IOS_CHROME_BROWSER_PASSWORDS_CREDENTIAL_MANAGER_UTIL_H_
+
+#include "base/values.h"
+#include "components/password_manager/core/common/credential_manager_types.h"
+
+// Return value of Parse* methods below is false if |json| is invalid, which
+// means it is missing required fields, contains fields of wrong type or
+// unexpected values. Otherwise return value is true.
+// Parses |mediation| field of JavaScript object CredentialRequestOptions.
+bool ParseMediationRequirement(
+    const base::DictionaryValue& json,
+    password_manager::CredentialMediationRequirement* mediation);
+// Parses |password| field of JavaScript object CredentialRequestOptions.
+bool ParseIncludePasswords(const base::DictionaryValue& json,
+                           bool* include_passwords);
+// Parses |providers| field of JavaScript object
+// FederatedCredentialRequestOptions into list of GURLs.
+bool ParseFederations(const base::DictionaryValue& json,
+                      std::vector<GURL>* federations);
+// Parses |type| field from JavaScript Credential object into CredentialType.
+bool ParseCredentialType(const base::DictionaryValue& json,
+                         password_manager::CredentialType* credential_type);
+// Parses dictionary representing JavaScript Credential object into
+// CredentialInfo.
+bool ParseCredentialDictionary(const base::DictionaryValue& json,
+                               password_manager::CredentialInfo* credential);
+
+#endif  // IOS_CHROME_BROWSER_PASSWORDS_CREDENTIAL_MANAGER_UTIL_H_
