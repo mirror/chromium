3127d9fc7683dc789e7ebd1a7233b6c0935c40ef
Renee Wright <rjwright@chromium.org>
2017-08-22 15:22:41 +1000

    [Ribbon] Move CSSPropertyMetadata::IsInheritedProperty to CSSPropertyAPI
    This is the second method from CSSPropertyMetadata to be ribbonized.
    
    Depends on chromium-review.googlesource.com/c/603194
    
    Diff of CSSPropertyAPI.cpp, CSSPropertyAPIFontSize.h, CSSPropertyMetadata.cpp
    https://gist.github.com/rjwright/c9d4ff584010fabdc0c718f26fab8ce1/revisions
    
    Bug: 751373
    Change-Id: Ia6e832cf3d173b02b47e24c9203438e21a415d93

--- third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
@@ -63,6 +63,7 @@
 #include "core/css/StylePropertySet.h"
 #include "core/css/StyleRuleImport.h"
 #include "core/css/StyleSheetContents.h"
+#include "core/css/properties/CSSPropertyAPI.h"
 #include "core/css/resolver/AnimatedStyleBuilder.h"
 #include "core/css/resolver/CSSVariableResolver.h"
 #include "core/css/resolver/MatchResult.h"
@@ -1514,7 +1515,7 @@ void StyleResolver::ApplyAllProperty(
     // When hitting matched properties' cache, only inherited properties will be
     // applied.
     if (inherited_only &&
-        !CSSPropertyMetadata::IsInheritedProperty(property_id))
+        !CSSPropertyAPI::Get(property_id).IsInheritedProperty())
       continue;
 
     StyleBuilder::ApplyProperty(property_id, state, all_value);
