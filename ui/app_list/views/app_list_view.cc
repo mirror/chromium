fa5adbc4bdf77d32a5046adbada12e2b09c852bb
Weidong Guo <weidongg@chromium.org>
2017-08-22 13:39:56 -0700

    cros: Fix crash showing fullscreen app list in mash
    Reason for crash:
    DesktopNativeWidgetAura::InitNativeWidget binds current widget with the
    root window's kDesktopNativeWidgetAuraKey for desktop build. But the
    DCHECK in DesktopScreenPositionClient::SetBounds() expects current
    widget's root window's kDesktopNativeWidgetAuraKey is not set or is not
    set to the current widget.
    
    Changes:
    Set bounds widget's native view in parent window's coordinates instead
    of screen coordinates to avoid calling Window::SetBoundsInScreen()
    which triggers DesktopScreenPositionClient::SetBounds().
    
    BUG=757573
    
    Change-Id: Id627572256fd315ae4609d347a7999cecb2ee0f1

--- ui/app_list/views/app_list_view.cc
+++ ui/app_list/views/app_list_view.cc
@@ -47,6 +47,7 @@
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/views_delegate.h"
 #include "ui/views/widget/widget.h"
+#include "ui/wm/core/coordinate_conversion.h"
 #include "ui/wm/core/shadow_types.h"
 
 using wallpaper::ColorProfileType;
@@ -475,11 +476,13 @@ void AppListView::InitializeFullscreen(gfx::NativeView parent,
   app_list_overlay_view_params.layer_type = ui::LAYER_SOLID_COLOR;
   fullscreen_widget_->Init(app_list_overlay_view_params);
 
-  // Set bounds directly in screen coordinates to avoid screen position
-  // controller setting bounds in the display where the widget has the largest
+  // Set native view's bounds directly to avoid screen position controller
+  // setting bounds in the display where the widget has the largest
   // intersection.
-  fullscreen_widget_->GetNativeView()->SetBoundsInScreen(
-      app_list_overlay_view_bounds, GetDisplayNearestView());
+  gfx::NativeView native_view = fullscreen_widget_->GetNativeView();
+  ::wm::ConvertRectFromScreen(native_view->parent(),
+                              &app_list_overlay_view_bounds);
+  native_view->SetBounds(app_list_overlay_view_bounds);
 
   overlay_view_ = new AppListOverlayView(0 /* no corners */);
 
@@ -1155,8 +1158,9 @@ void AppListView::UpdateYPositionAndOpacity(int y_position_in_screen,
     app_list_y_position_in_screen_ =
         std::max(y_position_in_screen, GetDisplayNearestView().bounds().y());
     new_widget_bounds.set_y(app_list_y_position_in_screen_);
-    fullscreen_widget_->GetNativeView()->SetBoundsInScreen(
-        new_widget_bounds, GetDisplayNearestView());
+    gfx::NativeView native_view = fullscreen_widget_->GetNativeView();
+    ::wm::ConvertRectFromScreen(native_view->parent(), &new_widget_bounds);
+    native_view->SetBounds(new_widget_bounds);
   }
 
   DraggingLayout();
