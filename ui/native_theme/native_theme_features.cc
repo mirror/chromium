34108f93fa15eee450cf0492a1a314561adacf5d
chaopeng <chaopeng@chromium.org>
2017-08-22 15:46:41 -0400

    Add 2 UX experimental features for Overlay Scrollbar
    experimental features:
    1. Show all scrollbars for any scroll update
    2. Show scrollbar when mouse enter
    
    This 2 experimental features will add behind flags:
    1. OverlayScrollbarFlashWhenAnyScrollUpdate
    2. OverlayScrollbarFlashWhenMouseEnter
    
    Bug: 746342
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I1e99af493a12c813a77225836a421d0e72b5dc68

--- ui/native_theme/native_theme_features.cc
+++ ui/native_theme/native_theme_features.cc
@@ -20,12 +20,22 @@ constexpr base::FeatureState kOverlayScrollbarFeatureState =
 const base::Feature kOverlayScrollbar{"OverlayScrollbar",
                                       kOverlayScrollbarFeatureState};
 
+// Experiment: Enables will flash all scrollbars in page when any scroll update.
+const base::Feature kOverlayScrollbarFlashWhenAnyScrollUpdate{
+    "OverlayScrollbarFlashWhenAnyScrollUpdate",
+    base::FEATURE_DISABLED_BY_DEFAULT};
+
 }  // namespace features
 
 namespace ui {
 
 bool IsOverlayScrollbarEnabled() {
   return base::FeatureList::IsEnabled(features::kOverlayScrollbar);
+}
+
+bool OverlayScrollbarFlashWhenAnyScrollUpdate() {
+  return base::FeatureList::IsEnabled(
+      features::kOverlayScrollbarFlashWhenAnyScrollUpdate);
 }
 
 }  // namespace ui
