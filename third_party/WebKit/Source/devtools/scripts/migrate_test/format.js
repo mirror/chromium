8599cd774b0ea9ac37a0731b395c1a5e8213f53e
Will Chen <chenwilliam@chromium.org>
2017-08-21 18:11:13 -0700

    DevTools: format tests (part 1)
    Bug: 667560
    Change-Id: I02c0fef9c64c8d0846f97a12f28caa308b9d3eaa

---
+++ third_party/WebKit/Source/devtools/scripts/migrate_test/format.js
@@ -0,0 +1,112 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var childProcess = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const recast = require('recast');
+var Linter = require('eslint').Linter;
+
+const utils = require('../utils');
+
+const TEMP_JS_FILE = path.resolve(__dirname, 'temp.js');
+const TEST_FUNCTION_SENTINEL = 'TEST_FUNCTION_SENTINEL();';
+
+function main() {
+  // walkSync('../../../../LayoutTests/http/tests/inspector');
+  walkSync('../../../../LayoutTests/http/tests/inspector-enabled');
+  walkSync('../../../../LayoutTests/http/tests/inspector-unit');
+  walkSync('../../../../LayoutTests/inspector-enabled');
+  // walkSync('../../../../LayoutTests/inspector');
+  walkSync('../../../../LayoutTests/inspector/console');
+}
+
+function walkSync(currentDirPath) {
+  fs.readdirSync(currentDirPath).forEach(function(name) {
+    if (name === 'resources') {
+      return;
+    }
+    let filePath = path.join(currentDirPath, name);
+    let stat = fs.statSync(filePath);
+    if (stat.isFile() && (filePath.endsWith('.html'))) {
+      try {
+        formatFile(filePath);
+      } catch (err) {
+        console.log(filePath, 'has err:', err);
+      }
+    } else if (stat.isDirectory()) {
+      walkSync(filePath);
+    }
+  });
+}
+
+function formatFile(filePath) {
+  let htmlContents = fs.readFileSync(filePath, 'utf-8');
+  const scriptContents = htmlContents.match(/<script.*?>([\S\s]*?)<\/script>/g);
+
+  let functionNode;
+  for (const script of scriptContents) {
+    const regex = /<script.*?>([\S\s]*?)<\/script>/
+    const innerScript = script.match(regex)[1];
+    if (!innerScript)
+      continue;
+    const ast = recast.parse(innerScript);
+    const sentinelNode = createExpressionNode(TEST_FUNCTION_SENTINEL);
+
+    let index =
+        ast.program.body.findIndex(n => n.type === 'VariableDeclaration' && n.declarations[0].id.name === 'test');
+    if (index > -1) {
+      functionNode = ast.program.body[index];
+      ast.program.body[index] = sentinelNode;
+    }
+
+    index = ast.program.body.findIndex(n => n.type === 'FunctionDeclaration' && n.id.name === 'test');
+    if (index > -1) {
+      functionNode = ast.program.body[index];
+      ast.program.body[index] = sentinelNode;
+    }
+    htmlContents = htmlContents.replace(innerScript, recast.print(ast).code);
+  }
+
+  if (!functionNode) {
+    console.log('ERROR with: ', filePath);
+    return;
+  }
+
+  let linter = new Linter();
+  let result = linter.verifyAndFix(recast.print(functionNode).code, {
+    envs: ['browser'],
+    useEslintrc: false,
+    parserOptions: {
+      ecmaVersion: 8,
+    },
+    rules: {
+      semi: 2,
+    }
+  });
+  if (result.messages.length) {
+    console.log('Issue with eslint', result.messages, 'for file: ', filePath);
+    process.exit(1);
+  }
+  fs.writeFileSync(TEMP_JS_FILE, result.output);
+  const formattedTestcode = shellOutput(`clang-format ${TEMP_JS_FILE} --style=FILE`)
+                                // Need to escape $ symbol
+                                .split('$')
+                                .join('$$');
+  fs.unlinkSync(TEMP_JS_FILE);
+  const newContents = htmlContents.replace(TEST_FUNCTION_SENTINEL, formattedTestcode);
+
+  fs.writeFileSync(filePath, newContents);
+}
+
+main();
+
+function shellOutput(command) {
+  return childProcess.execSync(command).toString();
+}
+
+function createExpressionNode(code) {
+  return recast.parse(code).program.body[0];
+}
