193e4a2c2eeae2bff08bb1c7d52f5768214c34ca
Sahel Sharify <sahel@chromium.org>
2017-08-22 17:10:25 -0400

    DispatchMouseEvent adds wheel phase info before sending wheel events.
    When wheel scroll latching and async wheel events are enabled, phase
    information gets added to WebMouseWheelEvents when they are generated
    from native events and before routing them. So, when wheel events are
    added to the Wheel event queue, it is expected for them to have the
    phase information. DispatchMouseEvent directly generates web events and
    forwards them skipping the creation from native events and routing them.
    This changes DispatchMouseEvent to add the phase information before
    forwarding the generated wheel events.
    
    To test the change locally run the following test with --enable-features=
    TouchpadAndWheelScrollLatching, AsyncWheelEvents runtime flag. The test
    would crash without applying this cl.
    
    Bug:756524, 526463
    test:inspector-protocol/input/dispatchMouseEvent.js
    Change-Id: I864a33b08e6398bac04da5092df16ac1eae7a0be

--- content/browser/devtools/protocol/input_handler.cc
+++ content/browser/devtools/protocol/input_handler.cc
@@ -19,6 +19,7 @@
 #include "content/common/input/synthetic_pinch_gesture_params.h"
 #include "content/common/input/synthetic_smooth_scroll_gesture_params.h"
 #include "content/common/input/synthetic_tap_gesture_params.h"
+#include "content/public/common/content_features.h"
 #include "ui/events/base_event_utils.h"
 #include "ui/events/blink/web_input_event_traits.h"
 #include "ui/events/gesture_detection/gesture_provider_config_helper.h"
@@ -247,8 +248,7 @@ InputHandler::InputHandler()
       input_queued_(false),
       page_scale_factor_(1.0),
       last_id_(0),
-      weak_factory_(this) {
-}
+      weak_factory_(this) {}
 
 InputHandler::~InputHandler() {
 }
@@ -453,7 +453,11 @@ void InputHandler::DispatchMouseEvent(
     }
     wheel_event->delta_x = static_cast<float>(-delta_x.fromJust());
     wheel_event->delta_y = static_cast<float>(-delta_y.fromJust());
-    wheel_event->dispatch_type = blink::WebInputEvent::kBlocking;
+    if (base::FeatureList::IsEnabled(
+            features::kTouchpadAndWheelScrollLatching)) {
+      wheel_event->phase = blink::WebMouseWheelEvent::kPhaseBegan;
+      wheel_event->dispatch_type = blink::WebInputEvent::kBlocking;
+    }
   } else {
     mouse_event.reset(new blink::WebMouseEvent(type, modifiers, timestamp));
   }
@@ -474,10 +478,28 @@ void InputHandler::DispatchMouseEvent(
   host_->GetRenderWidgetHost()->Focus();
   input_queued_ = false;
   pending_mouse_callbacks_.push_back(std::move(callback));
-  if (wheel_event)
+  if (wheel_event) {
     host_->GetRenderWidgetHost()->ForwardWheelEvent(*wheel_event);
-  else
+
+    if (base::FeatureList::IsEnabled(
+            features::kTouchpadAndWheelScrollLatching)) {
+      // Send a synthetic wheel event with phaseEnded to finish scrolling.
+      blink::WebMouseWheelEvent* wheel_end_event =
+          new blink::WebMouseWheelEvent(blink::WebInputEvent::kMouseWheel,
+                                        modifiers, timestamp);
+      wheel_end_event->delta_x = 0;
+      wheel_end_event->delta_y = 0;
+      wheel_end_event->phase = blink::WebMouseWheelEvent::kPhaseEnded;
+      wheel_end_event->dispatch_type = blink::WebInputEvent::kEventNonBlocking;
+      wheel_end_event->SetPositionInWidget(x * page_scale_factor_,
+                                           y * page_scale_factor_);
+      wheel_end_event->SetPositionInScreen(x * page_scale_factor_,
+                                           y * page_scale_factor_);
+      host_->GetRenderWidgetHost()->ForwardWheelEvent(*wheel_end_event);
+    }
+  } else {
     host_->GetRenderWidgetHost()->ForwardMouseEvent(*mouse_event);
+  }
   if (!input_queued_) {
     pending_mouse_callbacks_.back()->sendSuccess();
     pending_mouse_callbacks_.pop_back();
