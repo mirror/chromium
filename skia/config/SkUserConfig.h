f9447983123f2d1cc9ef4a4c791f82afbffedb46
Aleksandr Derbenev <alex-ac@yandex-team.ru>
2017-08-22 15:27:30 +0300

    Redirect SkASSERT to chromium log when dchecks enabled.
    Without SK_ABORT defined SkAssertf goes to chromium logging as DCHECK,
    but SkASSERT does not. This change makes SkASSERT and SkAssertf both
    call base/logging. It allows test-launcher to report these asserts to
    test launch summary.
    
    R=bungeman@google.com
    
    Bug:
    Change-Id: I34781f43c63eb5c79194a06a14e18e512d1950a3

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -141,6 +141,12 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
     "%s:%d: failed assertion \"%s\"\n", \
     __FILE__, __LINE__, #cond); } } while (false)
 
+#define SK_ABORT(message)                                                \
+  do {                                                                   \
+    SkDebugf_FileLine(__FILE__, __LINE__, true, "%s:%d: %s\n", __FILE__, \
+                      __LINE__, message);                                \
+  } while (false)
+
 #if !defined(ANDROID)   // On Android, we use the skia default settings.
 #define SK_A32_SHIFT    24
 #define SK_R32_SHIFT    16
