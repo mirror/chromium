1949e466e215d20efa928f7b6bd355246c46e884
Vitalii Iarko <vitaliii@chromium.org>
2017-08-22 11:21:08 +0200

    [NTP::Push] Clear prefs if feature is disabled.
    This ensures that we do not store any data after the experiment.
    
    Bug:757413
    Change-Id: I5c32a755acb8a1092047f45415e3ccf0453b109e

--- components/ntp_snippets/breaking_news/breaking_news_gcm_app_handler.cc
+++ components/ntp_snippets/breaking_news/breaking_news_gcm_app_handler.cc
@@ -317,6 +317,7 @@ void BreakingNewsGCMAppHandler::OnSendAcknowledged(
   NOTREACHED() << "BreakingNewsGCMAppHandler doesn't send GCM messages.";
 }
 
+// static
 void BreakingNewsGCMAppHandler::RegisterProfilePrefs(
     PrefRegistrySimple* registry) {
   registry->RegisterStringPref(prefs::kBreakingNewsGCMSubscriptionTokenCache,
@@ -325,6 +326,13 @@ void BreakingNewsGCMAppHandler::RegisterProfilePrefs(
                               /*default_value=*/0);
   registry->RegisterInt64Pref(prefs::kBreakingNewsGCMLastForcedSubscriptionTime,
                               /*default_value=*/0);
+}
+
+// static
+void BreakingNewsGCMAppHandler::ClearProfilePrefs(PrefService* pref_service) {
+  pref_service->ClearPref(prefs::kBreakingNewsGCMSubscriptionTokenCache);
+  pref_service->ClearPref(prefs::kBreakingNewsGCMLastTokenValidationTime);
+  pref_service->ClearPref(prefs::kBreakingNewsGCMSubscriptionTokenCache);
 }
 
 void BreakingNewsGCMAppHandler::OnJsonSuccess(
