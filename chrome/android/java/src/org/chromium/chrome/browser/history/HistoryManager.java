8bf0d9562c7f2f3f3a2a51e4247430556ff812ca
Becky Zhou <huayinz@chromium.org>
2017-08-22 15:06:50 -0700

    Hide history info button on a scrolled position when exit from search
    Bug: 756249
    Change-Id: I1615f2a3f2a687c1904401c6a02e3bc4c5b0083a

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryManager.java
@@ -75,6 +75,7 @@ public class HistoryManager implements OnMenuItemClickListener, SignInStateObser
 
     private boolean mIsSearching;
     private boolean mShouldShowInfoHeader;
+
     /**
      * Creates a new HistoryManager.
      * @param activity The Activity associated with the HistoryManager.
@@ -149,8 +150,8 @@ public class HistoryManager implements OnMenuItemClickListener, SignInStateObser
                         (LinearLayoutManager) recyclerView.getLayoutManager();
                 // Show info button if available if first visible position is close to info header;
                 // otherwise hide info button.
-                mToolbar.updateInfoMenuItem(infoHeaderIsVisible() && shouldShowInfoButton(),
-                        shouldShowInfoHeaderIfAvailable());
+                mToolbar.updateInfoMenuItem(
+                        shouldShowInfoButton(), shouldShowInfoHeaderIfAvailable());
 
                 if (!mHistoryAdapter.canLoadMoreItems()) return;
                 // Load more items if the scroll position is close to the bottom of the list.
@@ -423,8 +424,15 @@ public class HistoryManager implements OnMenuItemClickListener, SignInStateObser
      * @return True if info menu item should be shown on history toolbar, false otherwise.
      */
     boolean shouldShowInfoButton() {
-        return mHistoryAdapter.hasPrivacyDisclaimers() && mHistoryAdapter.getItemCount() > 0
-                && !mToolbar.isSearching() && !mSelectionDelegate.isSelectionEnabled();
+        LinearLayoutManager layoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+        // This is checking less than 0 to take into account NO_POSITION from
+        // findFirstVisibleItemPosition. Otherwise, the menu buttons will be incorrectly drawn
+        // in Chrome Home possibly due to android bug.
+        boolean headerIsVisible = (layoutManager.findFirstVisibleItemPosition() <= 0);
+
+        return headerIsVisible && mHistoryAdapter.hasPrivacyDisclaimers()
+                && mHistoryAdapter.getItemCount() > 0 && !mToolbar.isSearching()
+                && !mSelectionDelegate.isSelectionEnabled();
     }
 
     /**
@@ -433,14 +441,6 @@ public class HistoryManager implements OnMenuItemClickListener, SignInStateObser
      */
     boolean shouldShowInfoHeaderIfAvailable() {
         return mShouldShowInfoHeader;
-    }
-
-    /**
-     * @return True if info header is visible on history page.
-     */
-    boolean infoHeaderIsVisible() {
-        LinearLayoutManager layoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
-        return (layoutManager.findFirstVisibleItemPosition() == 0);
     }
 
     @Override
