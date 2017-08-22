9c78700ffa924c5488332cfd53bd363bd428e48a
Sylvain Defresne <sdefresne@chromium.org>
2017-08-22 10:45:26 +0200

    Refactor restoring of foreign navigations in NavigationManager.
    Add a Restore method to NavigationManager that uses the same API as
    the corresponding method of NavigationController on content/. This
    method loads a list of NavigationItem in an empty NavigationManager.
    
    Add a helper method to session_utils to create a WebState and load
    a list of NavigationItem and use it in the different code path that
    load a foreign session.
    
    Remove the -loadSessionTab: from Tab interface, and instead create
    a new WebState the helper method and insert it into the WebStateList
    or replace the currently active WebState (depending on whether the
    session loading should happen in the current tab or in a new tab).
    
    Remove old NavigationManagerImpl API to reset the session controller,
    remove use of NavigationManagerImpl API in favour of the new public
    NavigationManager API, remove Tab -didReplaceSessionHistory (no tab
    has its history replaced anymore, instead a new tab is created).
    
    Bug: 620465,620083,619992
    Change-Id: Ib5f54cbf2d66e06a9af223525e4a32c822cb85fb

--- ios/chrome/browser/sessions/session_util.mm
+++ ios/chrome/browser/sessions/session_util.mm
@@ -5,9 +5,15 @@
 #include "ios/chrome/browser/sessions/session_util.h"
 
 #include "base/files/file_path.h"
+#include "base/logging.h"
 #include "base/strings/sys_string_conversions.h"
+#include "components/sessions/core/session_types.h"
+#include "components/sessions/ios/ios_serialized_navigation_builder.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/sessions/session_service_ios.h"
-#include "ios/web/public/browser_state.h"
+#include "ios/web/public/navigation_item.h"
+#import "ios/web/public/navigation_manager.h"
+#import "ios/web/public/web_state/web_state.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -16,11 +22,27 @@
 namespace session_util {
 
 // Deletes the file containing the commands for the last session.
-void DeleteLastSession(web::BrowserState* browser_state) {
+void DeleteLastSession(ios::ChromeBrowserState* browser_state) {
   NSString* state_path =
       base::SysUTF8ToNSString(browser_state->GetStatePath().AsUTF8Unsafe());
   [[SessionServiceIOS sharedService]
       deleteLastSessionFileInDirectory:state_path];
+}
+
+std::unique_ptr<web::WebState> CreateWebStateWithNavigationEntries(
+    ios::ChromeBrowserState* browser_state,
+    int selected_navigation,
+    const std::vector<sessions::SerializedNavigationEntry>& navigations) {
+  DCHECK_GE(selected_navigation, 0);
+  DCHECK_LT(static_cast<size_t>(selected_navigation), navigations.size());
+
+  web::WebState::CreateParams params(browser_state);
+  auto web_state = web::WebState::Create(params);
+  web_state->GetNavigationManager()->Restore(
+      selected_navigation,
+      sessions::IOSSerializedNavigationBuilder::ToNavigationItems(navigations));
+  web_state->SetWebUsageEnabled(true);
+  return web_state;
 }
 
 }  // namespace session_util
