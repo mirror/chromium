2ac65f5e0ca73bc693ebc119ec3606b5de000452
Dan Elphick <delphick@chromium.org>
2017-08-22 17:17:44 +0100

    Convert BackgroundFetch to use GUIDs
    In preparation for converting to use the DownloadService, change the
    interface between the BackgroundFetchDelegateProxy and the
    DownloadManager to match what will be ultimately used. This means
    converts to use GUIDs to identify on-going downloads.
    
    
    Fix the MockDownloadManager to actually use the supplied GUID.
    
    Change-Id: I9e67ffee5957bfc58f8276c912d3914dc39fa9db

--- content/browser/background_fetch/background_fetch_request_info.cc
+++ content/browser/background_fetch/background_fetch_request_info.cc
@@ -4,10 +4,9 @@
 
 #include "content/browser/background_fetch/background_fetch_request_info.h"
 
-#include <string>
-
 #include "base/strings/string_util.h"
 #include "base/threading/sequenced_task_runner_handle.h"
+#include "content/browser/background_fetch/background_fetch_response.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/download_item.h"
 #include "net/http/http_response_headers.h"
@@ -26,50 +25,43 @@ BackgroundFetchRequestInfo::~BackgroundFetchRequestInfo() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 }
 
-void BackgroundFetchRequestInfo::PopulateDownloadStateOnUI(
-    DownloadItem* download_item,
-    DownloadInterruptReason download_interrupt_reason) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+void BackgroundFetchRequestInfo::PopulateWithResponse(
+    std::unique_ptr<const BackgroundFetchResponse> response) {
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
   DCHECK(!download_state_populated_);
 
-  download_guid_ = download_item->GetGuid();
-  download_state_ = download_item->GetState();
+  url_chain_ = response->url_chain;
+  response_time_ = response->response_time;
+
+  // deal with bad guids/aborts/etc
 
   // The response code, text and headers all are stored in the
   // net::HttpResponseHeaders object, shared by the |download_item|.
-  if (download_item->GetResponseHeaders()) {
-    const auto& headers = download_item->GetResponseHeaders();
+  response_code_ = response->headers->response_code();
+  response_text_ = response->headers->GetStatusText();
 
-    response_code_ = headers->response_code();
-    response_text_ = headers->GetStatusText();
+  size_t iter = 0;
+  std::string name, value;
 
-    size_t iter = 0;
-    std::string name, value;
+  while (response->headers->EnumerateHeaderLines(&iter, &name, &value))
+    response_headers_[base::ToLowerASCII(name)] = value;
 
-    while (headers->EnumerateHeaderLines(&iter, &name, &value))
-      response_headers_[base::ToLowerASCII(name)] = value;
-  }
+  response_data_populated_ = true;
 }
 
-void BackgroundFetchRequestInfo::SetDownloadStatePopulated() {
+void BackgroundFetchRequestInfo::SetPathAndSize(const base::FilePath& path,
+                                                uint64_t size) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   download_state_populated_ = true;
+
+  file_path_ = path;
+  file_size_ = size;
 }
 
-void BackgroundFetchRequestInfo::PopulateResponseFromDownloadItemOnUI(
-    DownloadItem* download_item) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  DCHECK(!response_data_populated_);
-
-  url_chain_ = download_item->GetUrlChain();
-  file_path_ = download_item->GetTargetFilePath();
-  file_size_ = download_item->GetReceivedBytes();
-  response_time_ = download_item->GetEndTime();
-}
-
-void BackgroundFetchRequestInfo::SetResponseDataPopulated() {
+void BackgroundFetchRequestInfo::SetFailureReason(FailureReason reason) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  response_data_populated_ = true;
+
+  failure_reason_ = reason;
 }
 
 int BackgroundFetchRequestInfo::GetResponseCode() const {
