13101a9750c4991b942ad3ca5a92333106a1fba5
Vladimir Levin <vmpstr@chromium.org>
2017-08-22 11:17:20 -0700

    viz: Extract resource_format and utils into a separate component.
    This patch introduces //components/viz/common:resource_format which only
    contains the resource format and associated utils. The top level common
    component publicly depends on this.
    
    This means that other components can still simply pull in common and get
    the resource format as well, but it also allows components to only
    depend on resource_format, thus avoiding pulling in other common deps.
    
    The patch also cleans up some comments and includes in the resource
    format area
    
    R=fsamuel@chromium.org, piman@chromium.org
    
    Change-Id: Ia1cab35bce015e02290d8a71ef70cbe26a1a6c43

--- components/viz/common/resources/resource_format_utils.h
+++ components/viz/common/resources/resource_format_utils.h
@@ -9,16 +9,24 @@
 #include "components/viz/common/viz_common_export.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
 #include "third_party/skia/include/gpu/GrTypes.h"
+#include "ui/gfx/buffer_types.h"
 
 namespace viz {
 
 VIZ_COMMON_EXPORT SkColorType
 ResourceFormatToClosestSkColorType(ResourceFormat format);
 VIZ_COMMON_EXPORT int BitsPerPixel(ResourceFormat format);
-VIZ_COMMON_EXPORT GLenum GLDataType(ResourceFormat format);
-VIZ_COMMON_EXPORT GLenum GLDataFormat(ResourceFormat format);
-VIZ_COMMON_EXPORT GLenum GLInternalFormat(ResourceFormat format);
-VIZ_COMMON_EXPORT GLenum GLCopyTextureInternalFormat(ResourceFormat format);
+
+// The following functions use unsigned int instead of GLenum, since including
+// third_party/khronos/GLES2/gl2.h causes redefinition errors as
+// macros/functions defined in it conflict with macros/functions defined in
+// ui/gl/gl_bindings.h. See http://crbug.com/512833 for more information.
+VIZ_COMMON_EXPORT unsigned int GLDataType(ResourceFormat format);
+VIZ_COMMON_EXPORT unsigned int GLDataFormat(ResourceFormat format);
+VIZ_COMMON_EXPORT unsigned int GLInternalFormat(ResourceFormat format);
+VIZ_COMMON_EXPORT unsigned int GLCopyTextureInternalFormat(
+    ResourceFormat format);
+
 VIZ_COMMON_EXPORT gfx::BufferFormat BufferFormat(ResourceFormat format);
 VIZ_COMMON_EXPORT bool IsResourceFormatCompressed(ResourceFormat format);
 VIZ_COMMON_EXPORT bool DoesResourceFormatSupportAlpha(ResourceFormat format);
