044c382e7efdf137ee8c16881db9c1eb266bc6e6
Tsuyoshi Horo <horo@chromium.org>
2017-08-22 16:26:42 +0900

    Stop using WorkerURLLoaderFactoryProvider
    I introduced WorkerURLLoaderFactoryProvider in e612058 to:
     1) Get the URLLoaderFactory in the worker thread.
     2) Register the WorkerFetchContextImpl in a (dedicated or shared) worker thread
        as a ServiceWorkerWorkerClient to the parent document's ServiceWorkerProviderHost 
        in the browser process to receive SetControllerServiceWorker() method call.
    
    But now we can get the URLLoaderFactory in the main thread and make a clone of
    it and provide that to WorkerFetchContextImpl.
    So this cl deletes WorkerURLLoaderFactoryProvider and use the Clone method of
    URLLoaderFactory.
    
    And also this cl changes the caller of SetControllerServiceWorker() from the
    ServiceWorkerProviderHost in the browser process to ServiceWorkerProviderContext
    in the main thread of the renderer process.
    
    OffMainThreadFetch was disabled when NetworkService is enabled because 
    WorkerURLLoaderFactoryProvider can't a URLLoaderFactory. But we can enable it
    now.
    
    
    Bug:756571
    Change-Id: Ib2a549ce23dc0a612a2df88c1f45af1383192ad3

--- content/child/child_url_loader_factory_getter.cc
+++ content/child/child_url_loader_factory_getter.cc
@@ -4,7 +4,29 @@
 
 #include "content/child/child_url_loader_factory_getter.h"
 
+#include "base/feature_list.h"
+#include "content/public/common/content_features.h"
+
 namespace content {
+
+ChildURLLoaderFactoryGetter::Info::Info(
+    mojom::URLLoaderFactoryPtrInfo network_loader_factory_info,
+    mojom::URLLoaderFactoryPtrInfo blob_loader_factory_info)
+    : network_loader_factory_info_(std::move(network_loader_factory_info)),
+      blob_loader_factory_info_(std::move(blob_loader_factory_info)) {}
+
+ChildURLLoaderFactoryGetter::Info::~Info() = default;
+
+scoped_refptr<ChildURLLoaderFactoryGetter>
+ChildURLLoaderFactoryGetter::Info::Bind() {
+  DCHECK(network_loader_factory_info_.is_valid());
+  mojom::URLLoaderFactoryPtr network_loader_factory;
+  mojom::URLLoaderFactoryPtr blob_loader_factory;
+  network_loader_factory.Bind(std::move(network_loader_factory_info_));
+  blob_loader_factory.Bind(std::move(blob_loader_factory_info_));
+  return base::MakeRefCounted<ChildURLLoaderFactoryGetter>(
+      std::move(network_loader_factory), std::move(blob_loader_factory));
+}
 
 ChildURLLoaderFactoryGetter::ChildURLLoaderFactoryGetter(
     PossiblyAssociatedURLLoaderFactory network_loader_factory,
@@ -12,12 +34,35 @@ ChildURLLoaderFactoryGetter::ChildURLLoaderFactoryGetter(
     : network_loader_factory_(std::move(network_loader_factory)),
       blob_loader_factory_getter_(std::move(blob_loader_factory_getter)) {}
 
+ChildURLLoaderFactoryGetter::ChildURLLoaderFactoryGetter(
+    PossiblyAssociatedURLLoaderFactory network_loader_factory,
+    PossiblyAssociatedURLLoaderFactory blob_loader_factory)
+    : network_loader_factory_(std::move(network_loader_factory)),
+      blob_loader_factory_(std::move(blob_loader_factory)) {}
+
+std::unique_ptr<ChildURLLoaderFactoryGetter::Info>
+ChildURLLoaderFactoryGetter::GetClonedInfo() {
+  mojom::URLLoaderFactoryPtrInfo network_loader_factory_info;
+  GetNetworkLoaderFactory()->Clone(
+      mojo::MakeRequest(&network_loader_factory_info));
+
+  mojom::URLLoaderFactoryPtrInfo blob_loader_factory_info;
+  GetBlobLoaderFactory()->Clone(mojo::MakeRequest(&blob_loader_factory_info));
+
+  return base::MakeUnique<Info>(std::move(network_loader_factory_info),
+                                std::move(blob_loader_factory_info));
+}
+
 mojom::URLLoaderFactory*
 ChildURLLoaderFactoryGetter::GetNetworkLoaderFactory() {
   return network_loader_factory_.get();
 }
 
 mojom::URLLoaderFactory* ChildURLLoaderFactoryGetter::GetBlobLoaderFactory() {
+  if (!base::FeatureList::IsEnabled(features::kNetworkService)) {
+    return GetNetworkLoaderFactory();
+  }
+
   if (!blob_loader_factory_) {
     DCHECK(!blob_loader_factory_getter_.is_null());
     blob_loader_factory_ = std::move(blob_loader_factory_getter_).Run();
