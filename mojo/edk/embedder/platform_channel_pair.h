df2a3ea1817e5132af5f541ceb325eb894d4e29e
Kevin Marshall <kmarshall@chromium.org>
2017-08-23 11:49:30 -0700

    Fuchsia: Add logic for Mojo handle-passing to child processes.
    This CL implements helper methods for serializing PlatformHandles as
    strings outside the context of PlatformChannelPairs.
    
    Bug: 746674, 750938
    Change-Id: I22f76e77e4c549bd52c7bce9f8921328beb88411

--- mojo/edk/embedder/platform_channel_pair.h
+++ mojo/edk/embedder/platform_channel_pair.h
@@ -87,9 +87,15 @@ class MOJO_SYSTEM_IMPL_EXPORT PlatformChannelPair {
       base::CommandLine* command_line,
       HandlePassingInformation* handle_passing_info) const;
 
-  // Like above, but returns a string instead of changing the command line.
-  std::string PrepareToPassClientHandleToChildProcessAsString(
-      HandlePassingInformation* handle_passing_info) const;
+#if defined(OS_FUCHSIA)
+  // Like above, but accepts a caller-supplied client |handle|.
+  // TODO(wez): Consider incorporating this call into other platform
+  // implementations.
+  static void PrepareToPassHandleToChildProcess(
+      const PlatformHandle& handle,
+      base::CommandLine* command_line,
+      HandlePassingInformation* handle_passing_info);
+#endif
 
   // To be called once the child process has been successfully launched, to do
   // any cleanup necessary.
