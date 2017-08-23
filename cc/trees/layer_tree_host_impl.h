fb4c94c038e5e1639e9f5edfa77f126e88d94ed3
chaopeng <chaopeng@chromium.org>
2017-08-23 14:21:30 -0400

    Add UX experimental feature flash all scrollbars for any scroll update
    This patch introduce an UX experimental feature flash all scrollbars for any
    scroll update for overlay scrollbar. This feature is behind a flag:
    OverlayScrollbarFlashWhenAnyScrollUpdate.
    
    Bug: 746342
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I1e99af493a12c813a77225836a421d0e72b5dc68

--- cc/trees/layer_tree_host_impl.h
+++ cc/trees/layer_tree_host_impl.h
@@ -338,6 +338,7 @@ class CC_EXPORT LayerTreeHostImpl
   void UnregisterScrollbarAnimationController(ElementId scroll_element_id);
   ScrollbarAnimationController* ScrollbarAnimationControllerForElementId(
       ElementId scroll_element_id) const;
+  void FlashAllScrollbars(bool did_scroll);
 
   DrawMode GetDrawMode() const;
 
