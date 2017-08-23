a0ecc4c0b151b852dd2a1564ba3cde9463eb2763
Marc Treib <treib@chromium.org>
2017-08-23 14:00:12 +0200

    Cleanup: Remove unused parts of Instant
    After https://chromium-review.googlesource.com/623649 (removing
    InstantSearchPrerenderer), parts of Instant have become unused. This CL
    removed unused plumbing and types. The actual API endpoints (in
    searchbox_api.js) are left in place for now; those will be removed in a
    followup.
    
    Bug: 627747
    Change-Id: If792081ab7ebe476e78a1df39e408c26123cae8b

--- chrome/browser/ui/search/search_tab_helper.cc
+++ chrome/browser/ui/search/search_tab_helper.cc
@@ -165,22 +165,6 @@ void SearchTabHelper::OmniboxFocusChanged(OmniboxFocusState state,
     ipc_router_.SetInputInProgress(IsInputInProgress());
 }
 
-void SearchTabHelper::NavigationEntryUpdated() {
-  if (!is_search_enabled_)
-    return;
-
-  UpdateMode(/*update_origin=*/false);
-}
-
-void SearchTabHelper::SetSuggestionToPrefetch(
-    const InstantSuggestion& suggestion) {
-  ipc_router_.SetSuggestionToPrefetch(suggestion);
-}
-
-void SearchTabHelper::Submit(const EmbeddedSearchRequestParams& params) {
-  ipc_router_.Submit(params);
-}
-
 void SearchTabHelper::OnTabActivated() {
   ipc_router_.OnTabActivated();
 
