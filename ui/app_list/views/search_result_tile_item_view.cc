9c05a50e0cf13556e38f16fc623842c4f985631a
Qiang Xu <warx@chromium.org>
2017-08-22 17:54:33 -0700

    app_list: make app search result title two-line display for installed apps
    changes:
    - Add SetMaxLines/max_lines api in views::Label.
    - When app search result is installed result type, make its title
    two-line display.
    - Changing the padding between icon and title from 6 to 5, makes the
    bottom line title not truncated.
    
    screenshots:
    before the change: https://screenshot.googleplex.com/phPK7h0iDKx
    after the change: https://screenshot.googleplex.com/FCqVx5UJ4No
    
    Test: tested with fullscreen app list flag
    Bug: 754891, 756164
    Change-Id: I13cabc419a6070af607d49c5bd09e75c1e07dfff

--- ui/app_list/views/search_result_tile_item_view.cc
+++ ui/app_list/views/search_result_tile_item_view.cc
@@ -24,7 +24,7 @@ namespace {
 
 constexpr int kSearchTileWidth = 80;
 constexpr int kSearchTileTopPadding = 4;
-constexpr int kSearchTitleSpacing = 6;
+constexpr int kSearchTitleSpacing = 5;
 constexpr int kSearchPriceSize = 37;
 constexpr int kSearchRatingSize = 26;
 constexpr int kSearchRatingStarSize = 12;
@@ -138,6 +138,17 @@ void SearchResultTileItemView::SetSearchResult(SearchResult* item) {
           views::CreateSolidBackground(kCardBackgroundColorFullscreen));
       title()->SetFontList(base_font.DeriveWithSizeDelta(1));
       title()->SetEnabledColor(kSearchTitleColor);
+    }
+
+    if (item_->display_type() == SearchResult::DISPLAY_TILE &&
+        item_->result_type() == SearchResult::RESULT_INSTALLED_APP) {
+      title()->SetMultiLine(true);
+      title()->SetMaxLines(2);
+    } else {
+      title()->SetMultiLine(false);
+      // Reset max lines to make title() display only limited by the available
+      // view height.
+      title()->SetMaxLines(0);
     }
   }
 
