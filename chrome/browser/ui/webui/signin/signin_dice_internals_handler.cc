be491da540f39aa488c5014c85728871df549043
Mihai Sardarescu <msarda@chromium.org>
2017-08-23 15:52:30 +0200

    [DICE] Add "Disconnect sync" button on dice internals.
    This CL changes the chrome://signin-dice-internals to display the "Enable sync"
    button when there is Chrome is signed out and the "Disable sync" button when
    Chrome is signed in.
    This CL also reloads the chrome://signin-dice-internals page when the
    Chrome authenticated account changes.
    
    Bug: 733226
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If9671dfaf3e23939d1b643fe1f5840d2da18eb66

--- chrome/browser/ui/webui/signin/signin_dice_internals_handler.cc
+++ chrome/browser/ui/webui/signin/signin_dice_internals_handler.cc
@@ -12,19 +12,31 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/sync/one_click_signin_sync_starter.h"
+#include "chrome/common/url_constants.h"
+#include "content/public/browser/web_ui_data_source.h"
 
 SigninDiceInternalsHandler::SigninDiceInternalsHandler(Profile* profile)
     : profile_(profile) {
   DCHECK(profile_);
   DCHECK(!profile_->IsOffTheRecord());
+  SigninManagerFactory::GetForProfile(profile_)->AddObserver(this);
 }
 
-SigninDiceInternalsHandler::~SigninDiceInternalsHandler() {}
+SigninDiceInternalsHandler::~SigninDiceInternalsHandler() {
+  SigninManagerFactory::GetForProfile(profile_)->RemoveObserver(this);
+}
 
 void SigninDiceInternalsHandler::RegisterMessages() {
   web_ui()->RegisterMessageCallback(
+      "initialized", base::Bind(&SigninDiceInternalsHandler::HandleInitialized,
+                                base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
       "enableSync", base::Bind(&SigninDiceInternalsHandler::HandleEnableSync,
                                base::Unretained(this)));
+}
+void SigninDiceInternalsHandler::HandleInitialized(
+    const base::ListValue* args) {
+  AllowJavascript();
 }
 
 void SigninDiceInternalsHandler::HandleEnableSync(const base::ListValue* args) {
@@ -55,4 +67,29 @@ void SigninDiceInternalsHandler::HandleEnableSync(const base::ListValue* args) {
   OneClickSigninSyncStarter::Callback callback;
   new OneClickSigninSyncStarter(profile_, browser, gaia_id, email,
                                 web_ui()->GetWebContents(), callback);
+}
+
+void SigninDiceInternalsHandler::GoogleSigninSucceeded(
+    const std::string& account_id,
+    const std::string& username) {
+  UpdateDataSourceAndReload();
+}
+
+void SigninDiceInternalsHandler::GoogleSignedOut(const std::string& account_id,
+                                                 const std::string& username) {
+  UpdateDataSourceAndReload();
+}
+
+void SigninDiceInternalsHandler::UpdateDataSourceAndReload() {
+  std::unique_ptr<base::DictionaryValue> update(new base::DictionaryValue);
+  update->SetBoolean(
+      "isSignedIn",
+      SigninManagerFactory::GetForProfile(profile_)->IsAuthenticated());
+  update->SetBoolean("isSyncAllowed", profile_->IsSyncAllowed());
+  content::WebUIDataSource::Update(
+      profile_, chrome::kChromeUISigninDiceInternalsHost, std::move(update));
+
+  // Reload the page after the data source was updated.
+  if (IsJavascriptAllowed())
+    CallJavascriptFunction("window.location.reload");
 }
