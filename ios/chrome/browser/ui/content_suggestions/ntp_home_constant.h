952a17dcc208c6e5eaddf30e08c8826553b264ea
Gauthier Ambard <gambard@chromium.org>
2017-08-23 13:32:16 +0200

    Add NTP impressions metrics
    This CL adds metrics recording for types of NTP impressions.
    
    Bug:691979
    Change-Id: I9c512d81abb25993c714c7d4346918eb1922caea

--- ios/chrome/browser/ui/content_suggestions/ntp_home_constant.h
+++ ios/chrome/browser/ui/content_suggestions/ntp_home_constant.h
@@ -9,6 +9,17 @@
 
 namespace ntp_home {
 
+// Type of NTP opened, for UMA report. It should be treated as append-only.
+// These match tools/metrics/histograms/enums.xml.
+typedef NS_ENUM(NSUInteger, IOSNTPImpression) {
+  // The NTP only displays the local suggestions.
+  LOCAL_SUGGESTIONS = 0,
+  // The NTP displays local and remote suggestions.
+  REMOTE_SUGGESTIONS = 1,
+  // Add new enum above ENUM_MAX.
+  COUNT
+};
+
 // Returns the accessibility identifier used by the fake omnibox.
 NSString* FakeOmniboxAccessibilityID();
 
