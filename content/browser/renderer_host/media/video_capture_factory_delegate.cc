827c4aa7814266bab2281f5c9cf5fcaa3fb2f0f0
Christian Fremerey <chfremer@chromium.org>
2017-08-23 15:54:32 -0700

    [Video Capture Service] Do no launch service when doing non-device capture
    This CL makes launching the service more lazy, so that it does not get
    launched before actual capture from a device is requested.
    
    Bug: 755315
    Change-Id: I9eab267b6728b9ef2065ee56eff40bc8a37ee1a5

---
+++ content/browser/renderer_host/media/video_capture_factory_delegate.cc
@@ -0,0 +1,33 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/renderer_host/media/video_capture_factory_delegate.h"
+
+namespace content {
+
+VideoCaptureFactoryDelegate::VideoCaptureFactoryDelegate(
+    video_capture::mojom::DeviceFactoryPtr* device_factory,
+    base::OnceClosure destruction_cb)
+    : device_factory_(device_factory),
+      destruction_cb_(std::move(destruction_cb)) {}
+
+VideoCaptureFactoryDelegate::~VideoCaptureFactoryDelegate() {
+  base::ResetAndReturn(&destruction_cb_).Run();
+}
+
+void VideoCaptureFactoryDelegate::CreateDevice(
+    const std::string& device_id,
+    ::video_capture::mojom::DeviceRequest device_request,
+    video_capture::mojom::DeviceFactory::CreateDeviceCallback callback) {
+  if (!device_factory_->is_bound()) {
+    // This can happen when the ServiceVideoCaptureProvider owning
+    // |device_factory_| loses connection to the service process and resets
+    // |device_factory_|.
+    CHECK(false);
+  }
+  (*device_factory_)
+      ->CreateDevice(device_id, std::move(device_request), std::move(callback));
+}
+
+}  // namespace content
