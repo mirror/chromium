b123226280085865458c5207e6d8762ddd0a0fbd
Francois Doray <fdoray@chromium.org>
2017-08-23 11:16:50 -0400

    Add WindowOcclusionTracker to BrowserProcess.
    With this CL, a WindowOcclusionTracker initialized in
    ChromeBrowserMainParts::PreBrowserStart() is accessible via
    BrowserProcess::GetWindowOcclusionTracker().
    
    Design doc: https://docs.google.com/a/google.com/document/d/1USXrTpwzk-5A9ds3FkTmFCzJGVJJtCM4VLPw2UdpGS0/edit?usp=sharing
    
    Bug: 738387
    Change-Id: I874f4814596fb5fa72ad89a9f77cdbb4275419a4

--- chrome/browser/browser_process.h
+++ chrome/browser/browser_process.h
@@ -117,6 +117,7 @@ class RapporServiceImpl;
 
 namespace resource_coordinator {
 class TabManager;
+class WindowOcclusionTracker;
 }
 
 namespace safe_browsing {
@@ -283,6 +284,10 @@ class BrowserProcess {
   virtual network_time::NetworkTimeTracker* network_time_tracker() = 0;
 
   virtual gcm::GCMDriver* gcm_driver() = 0;
+
+  // Returns the WindowOcclusionTracker, if supported on this platform.
+  virtual resource_coordinator::WindowOcclusionTracker*
+  GetWindowOcclusionTracker() = 0;
 
   // Returns the tab manager. On non-supported platforms, this returns null.
   virtual resource_coordinator::TabManager* GetTabManager() = 0;
