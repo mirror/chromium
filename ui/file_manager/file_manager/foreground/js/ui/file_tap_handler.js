a620b037f48a8b6c863b928e73ce40de6d976af4
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2017-08-23 10:52:14 +0900

    Enable opening context menu by two-finger tap.
    The context menu has once disabled by 606068 when we assigned long-tap
    to enter the selection mode.
    
    Bug:755757
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I282901d190e63c264eb6238abd34bf0cff107a66

--- ui/file_manager/file_manager/foreground/js/ui/file_tap_handler.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_tap_handler.js
@@ -106,13 +106,17 @@ FileTapHandler.prototype.handleTouchEvents = function(event, index, callback) {
     case 'touchstart':
       // Only process single touches.  If there is already a touch happening, or
       // two simultaneous touches then just ignore them.
-      if (event.touches.length > 1)
+      if (event.touches.length > 1) {
         // Note that we could cancel an active touch here.  That would make
         // simultaneous touch behave similar to near-simultaneous. However, if
         // the user is dragging something, an accidental second touch could be
         // quite disruptive if it cancelled their drag.  Better to just ignore
-        // it.
+        // it
+
+        // Invalidate current touch to distinguish it from normal tap.
+        this.tapStarted_ = false;
         return;
+      }
 
       // It's still possible there could be an active "touch" if the user is
       // simultaneously using a mouse and a touch input.
@@ -140,8 +144,6 @@ FileTapHandler.prototype.handleTouchEvents = function(event, index, callback) {
       break;
 
     case 'touchmove':
-      assert(this.findActiveTouch_(event.touches), 'Missing touchEnd');
-
       var touch = this.findActiveTouch_(event.changedTouches);
       if (!touch)
         break;
