6b4320b3ce30465ac1fcc003a22c83b0c7a6d901
kelvinp@chromium.org <kelvinp@chromium.org>
2017-08-23 14:27:44 -0700

    [Chromoting] Reports click through rate of migration messages.
    Reports the following metrics:
    1. The number of times a message is shown for a specific migration
       phase.
    2. The number of times a message is dismissed for a specific migration
       phase.
    3. The number of times the upsell URL is clicked for a specific migration
       phase.
    
    Bug: 756674
    Change-Id: I38916dcc66a4cab579eb2314c18b63b27302db04

--- remoting/webapp/crd/js/butter_bar_unittest.js
+++ remoting/webapp/crd/js/butter_bar_unittest.js
@@ -6,6 +6,21 @@
 
 'use strict';
 
+var logWriterSpy;
+
+function verifyLog(assert, index, expectedEvent, expectedPhase) {
+  var actual = logWriterSpy.getCall(index).args[0];
+  assert.equal(
+      actual.type, remoting.ChromotingEvent.Type.CHROMOTING_DOT_COM_MIGRATION,
+      'ChromotingEvent.type == CHROMOTING_DOT_COM_MIGRATION');
+  assert.equal(
+      actual.chromoting_dot_com_migration.event, expectedEvent,
+      'ChromotingEvent.chromoting_dot_com_migration.event matched.');
+  assert.equal(
+      actual.chromoting_dot_com_migration.phase, expectedPhase,
+      'ChromotingEvent.chromoting_dot_com_migration.phase matched');
+}
+
 QUnit.module('ButterBar', {
   beforeEach: function() {
     var fixture = document.getElementById('qunit-fixture');
@@ -18,7 +33,8 @@ QUnit.module('ButterBar', {
         '    </a>' +
         '  </p>' +
         '</div>';
-    this.butterBar = new remoting.ButterBar();
+    logWriterSpy = sinon.spy();
+    this.butterBar = new remoting.ButterBar(logWriterSpy);
     chrome.storage = {
       sync: {
         get: sinon.stub(),
@@ -137,6 +153,8 @@ QUnit.test(
     'should be shown and should not update local storage if it has already ' +
     'shown, the timeout has not elapsed and it has not been dismissed',
     function(assert) {
+  var MigrationPhase = remoting.ChromotingEvent.ChromotingDotComMigration.Phase;
+  var MigrationEvent = remoting.ChromotingEvent.ChromotingDotComMigration.Event;
   this.currentMessage = 0;
   chrome.storage.sync.get.callsArgWith(1, {
     "message-state": {
@@ -149,6 +167,9 @@ QUnit.test(
   return this.butterBar.init().then(() => {
     assert.ok(this.butterBar.root_.hidden == false);
     assert.ok(!chrome.storage.sync.set.called);
+    verifyLog(
+        assert, 0, MigrationEvent.DEPRECATION_NOTICE_IMPRESSION,
+        MigrationPhase.BETA)
   });
 });
 
@@ -164,6 +185,7 @@ QUnit.test('should stay hidden if the timeout has elapsed', function(assert) {
   this.now = remoting.ButterBar.kTimeout_+ 1;
   return this.butterBar.init().then(() => {
     assert.ok(this.butterBar.root_.hidden == true);
+    sinon.assert.notCalled(logWriterSpy);
   });
 });
 
@@ -180,11 +202,14 @@ QUnit.test('should stay hidden if it was previously dismissed',
   });
   return this.butterBar.init().then(() => {
     assert.ok(this.butterBar.root_.hidden == true);
+    sinon.assert.notCalled(logWriterSpy);
   });
 });
 
 
 QUnit.test('should be shown if the index has increased', function(assert) {
+  var MigrationPhase = remoting.ChromotingEvent.ChromotingDotComMigration.Phase;
+  var MigrationEvent = remoting.ChromotingEvent.ChromotingDotComMigration.Event;
   this.currentMessage = 1;
   chrome.storage.sync.get.callsArgWith(1, {
     "message-state": {
@@ -196,21 +221,33 @@ QUnit.test('should be shown if the index has increased', function(assert) {
   this.now = remoting.ButterBar.kTimeout_ + 1;
   return this.butterBar.init().then(() => {
     assert.ok(this.butterBar.root_.hidden == false);
+    verifyLog(
+        assert, 0, MigrationEvent.DEPRECATION_NOTICE_IMPRESSION,
+        MigrationPhase.STABLE)
   });
 });
 
 QUnit.test('should be red and not dismissable for the final message',
            function(assert) {
+  var MigrationPhase = remoting.ChromotingEvent.ChromotingDotComMigration.Phase;
+  var MigrationEvent = remoting.ChromotingEvent.ChromotingDotComMigration.Event;
+
   this.currentMessage = 3;
   chrome.storage.sync.get.callsArgWith(1, {});
   return this.butterBar.init().then(() => {
     assert.ok(this.butterBar.root_.hidden == false);
     assert.ok(this.butterBar.dismiss_.hidden == true);
     assert.ok(this.butterBar.root_.classList.contains('red'));
+    verifyLog(
+        assert, 0, MigrationEvent.DEPRECATION_NOTICE_IMPRESSION,
+        MigrationPhase.DEPRECATION_2)
   });
 });
 
 QUnit.test('dismiss button updates local storage', function(assert) {
+  var MigrationPhase = remoting.ChromotingEvent.ChromotingDotComMigration.Phase;
+  var MigrationEvent = remoting.ChromotingEvent.ChromotingDotComMigration.Event;
+
   this.currentMessage = 0;
   chrome.storage.sync.get.callsArgWith(1, {});
   return this.butterBar.init().then(() => {
@@ -225,6 +262,31 @@ QUnit.test('dismiss button updates local storage', function(assert) {
                          "timestamp": 0,
                        }
                      }]);
+    verifyLog(
+        assert, 0, MigrationEvent.DEPRECATION_NOTICE_IMPRESSION,
+        MigrationPhase.BETA)
+    verifyLog(
+        assert, 1, MigrationEvent.DEPRECATION_NOTICE_DISMISSAL,
+        MigrationPhase.BETA)
+  });
+});
+
+QUnit.test('clicks on upsell URL are reported', function(assert) {
+  var MigrationPhase = remoting.ChromotingEvent.ChromotingDotComMigration.Phase;
+  var MigrationEvent = remoting.ChromotingEvent.ChromotingDotComMigration.Event;
+
+  this.currentMessage = 2;
+  chrome.storage.sync.get.callsArgWith(1, {});
+  return this.butterBar.init().then(() => {
+    this.clock = sinon.useFakeTimers(0);
+    // Invoke the link.
+    this.butterBar.root_.getElementsByTagName('a')[0].click();
+    verifyLog(
+        assert, 0, MigrationEvent.DEPRECATION_NOTICE_IMPRESSION,
+        MigrationPhase.DEPRECATION_1)
+    verifyLog(
+        assert, 1, MigrationEvent.DEPRECATION_NOTICE_CLICKED,
+        MigrationPhase.DEPRECATION_1)
   });
 });
 
