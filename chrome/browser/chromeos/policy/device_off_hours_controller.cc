c4cfeadbe205d7114164539b2b84eeaa5133ccec
Daria Yakovleva <yakovleva@google.com>
2017-08-23 21:00:22 +0200

    Add converter for DeviceOffHoursProto
    Add converter from DeviceOffHoursProto
    to DictionaryValue which is used in
    device settings provider and
    device policy decoder
    
    Bug: 739713
    Change-Id: I3ddd2a605a19a0d357ccc145636e2c90edf08a68

---
+++ chrome/browser/chromeos/policy/device_off_hours_controller.cc
@@ -0,0 +1,135 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/policy/device_off_hours_controller.h"
+
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "base/logging.h"
+#include "base/memory/ptr_util.h"
+#include "base/time/time.h"
+
+namespace em = enterprise_management;
+
+namespace policy {
+namespace {
+
+const int kMillisecondsInDay = base::TimeDelta::FromDays(1).InMilliseconds();
+
+// WeeklyTime class contains weekday and time in milliseconds.
+struct WeeklyTime {
+  // Number of weekday (1 = Monday, 2 = Tuesday, etc.)
+  int weekday;
+  // Time of day in milliseconds from the beginning of the day.
+  int milliseconds;
+
+  WeeklyTime(int weekday, int milliseconds)
+      : weekday(weekday), milliseconds(milliseconds) {}
+
+  std::unique_ptr<base::DictionaryValue> ToValue() const {
+    auto weekly_time = base::MakeUnique<base::DictionaryValue>();
+    weekly_time->SetInteger("weekday", weekday);
+    weekly_time->SetInteger("time", milliseconds);
+    return weekly_time;
+  }
+};
+
+// Time interval struct, interval = [start, end]
+struct Interval {
+  std::unique_ptr<WeeklyTime> start;
+  std::unique_ptr<WeeklyTime> end;
+
+  Interval(Interval& interval)
+      : start(std::move(interval.start)), end(std::move(interval.end)) {}
+
+  Interval(const WeeklyTime& start, const WeeklyTime& end)
+      : start(base::MakeUnique<WeeklyTime>(start)),
+        end(base::MakeUnique<WeeklyTime>(end)) {}
+
+  std::unique_ptr<base::DictionaryValue> ToValue() {
+    auto interval = base::MakeUnique<base::DictionaryValue>();
+    interval->SetDictionary("start", start->ToValue());
+    interval->SetDictionary("end", end->ToValue());
+    return interval;
+  }
+};
+
+// Get WeeklyTime structure from WeeklyTimeProto and put it to |weekly_time|
+// Return false if |weekly_time| won't correct
+std::unique_ptr<WeeklyTime> GetWeeklyTime(
+    const em::WeeklyTimeProto& container) {
+  if (!container.has_weekday() ||
+      container.weekday() == em::WeeklyTimeProto::DAY_OF_WEEK_UNSPECIFIED) {
+    LOG(ERROR) << "Day of week in interval is absent or unspecified.";
+    return nullptr;
+  }
+  if (!container.has_time()) {
+    LOG(ERROR) << "Time in interval is absent.";
+    return nullptr;
+  }
+  int time_of_day = container.time();
+  if (!(time_of_day >= 0 && time_of_day < kMillisecondsInDay)) {
+    LOG(ERROR) << "Invalid time value: " << time_of_day
+               << ", the value should be in [0; " << kMillisecondsInDay << ").";
+    return nullptr;
+  }
+  return base::MakeUnique<WeeklyTime>(container.weekday(), time_of_day);
+}
+
+// Get and return list of time intervals from DeviceOffHoursProto structure
+std::vector<std::unique_ptr<Interval>> GetIntervals(
+    const em::DeviceOffHoursProto& container) {
+  std::vector<std::unique_ptr<Interval>> intervals;
+  for (const auto& entry : container.interval()) {
+    if (!entry.has_start() || !entry.has_end()) {
+      LOG(WARNING) << "Miss interval without start or/and end.";
+      continue;
+    }
+    auto start = GetWeeklyTime(entry.start());
+    auto end = GetWeeklyTime(entry.end());
+    if (start && end) {
+      intervals.push_back(base::MakeUnique<Interval>(*start, *end));
+    }
+  }
+  return intervals;
+}
+
+std::vector<std::string> GetIgnoredPolicies(
+    const em::DeviceOffHoursProto& container) {
+  std::vector<std::string> ignored_policies;
+  for (const auto& entry : container.ignored_policy()) {
+    ignored_policies.push_back(entry);
+  }
+  return ignored_policies;
+}
+
+}  // namespace
+
+namespace off_hours {
+
+std::unique_ptr<base::DictionaryValue> ConvertToValue(
+    const em::DeviceOffHoursProto& container) {
+  if (!container.has_timezone())
+    return nullptr;
+  auto off_hours = base::MakeUnique<base::DictionaryValue>();
+  off_hours->SetString("timezone", container.timezone());
+  std::vector<std::unique_ptr<Interval>> intervals = GetIntervals(container);
+  auto intervals_value = base::MakeUnique<base::ListValue>();
+  for (size_t i = 0; i < intervals.size(); i++) {
+    intervals_value->Append(intervals[i]->ToValue());
+  }
+  off_hours->SetList("intervals", std::move(intervals_value));
+  std::vector<std::string> ignored_policies = GetIgnoredPolicies(container);
+  auto ignored_policies_value = base::MakeUnique<base::ListValue>();
+  for (const auto& policy : ignored_policies) {
+    ignored_policies_value->AppendString(policy);
+  }
+  off_hours->SetList("ignored_policies", std::move(ignored_policies_value));
+  return off_hours;
+}
+
+}  // namespace off_hours
+}  // namespace policy
---
+++ chrome/browser/chromeos/policy/device_off_hours_controller.h
@@ -0,0 +1,32 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_POLICY_DEVICE_OFF_HOURS_CONTROLLER_H_
+#define CHROME_BROWSER_CHROMEOS_POLICY_DEVICE_OFF_HOURS_CONTROLLER_H_
+
+#include <memory>
+
+#include "base/values.h"
+#include "chrome/browser/chromeos/policy/proto/chrome_device_policy.pb.h"
+
+namespace policy {
+namespace off_hours {
+
+// Return DictionaryValue in format:
+// { "timezone" : string,
+//   "intervals" : list of Intervals,
+//   "ignored_policies" : string list }
+// Interval dictionary format:
+// { "start" : WeeklyTime,
+//   "end" : WeeklyTime }
+// WeeklyTime dictionary format:
+// { "weekday" : int,
+//   "time" : int }
+std::unique_ptr<base::DictionaryValue> ConvertToValue(
+    const enterprise_management::DeviceOffHoursProto& container);
+
+}  // namespace off_hours
+}  // namespace policy
+
+#endif  // CHROME_BROWSER_CHROMEOS_POLICY_DEVICE_OFF_HOURS_CONTROLLER_H_
