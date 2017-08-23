824a3bd587763df78cc6ef32efa01e77b2020b4a
Kevin Marshall <kmarshall@chromium.org>
2017-08-23 14:36:04 -0700

    Fuchsia: implement sandbox support via Fuchsia launch clone policy.
    This CL adds a function "GetClonePolicyForSandbox()" which returns
    the set of clone flags needed to achieve the level of isolation
    appropriate for a SandboxType.
    
    Calling LaunchProcess() without setting the clone policy will result
    in the child process being spawned with the same capabilities as its
    parent, so as to not break the existing call sites of LaunchProcess().
    
    
    Bug: 750938
    Change-Id: I09033892246a93322970b24ab79d107b103c1671

---
+++ content/common/sandbox_policy_fuchsia.h
@@ -0,0 +1,13 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/public/common/sandbox_type.h"
+
+namespace content {
+
+// Returns a flag that represents the capabilities that should be granted to a
+// sandboxed process of type |type|.
+uint32_t GetClonePolicyForSandbox(content::SandboxType type);
+
+}  // namespace content
