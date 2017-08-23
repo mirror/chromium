d482348a53a1c827ab091060ebdff482a8f93478
Troy Hildebrandt <thildebr@chromium.org>
2017-08-23 13:35:37 -0700

    [Home] Bring forked Android bottom navigation widget into the build.
    Modifies the package the classes live in, and brings the code into the
    build.
    
    Bug:738492
    Change-Id: Ic8bf750bb48e316c19bfce497053c511fe6d63b3

--- chrome/android/third_party/widget_bottomsheet_base/java/src/org/chromium/chrome/browser/widget/bottomsheet/base/ThemeUtils.java
+++ chrome/android/third_party/widget_bottomsheet_base/java/src/org/chromium/chrome/browser/widget/bottomsheet/base/ThemeUtils.java
@@ -14,13 +14,15 @@
  * limitations under the License.
  */
 
-package android.support.design.widget;
+package org.chromium.chrome.browser.widget.bottomsheet.base;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.support.design.R;
 
-class ThemeUtils {
+/**
+ * Formed from android.support.design.widget.ThemeUtils.
+ */
+public class ThemeUtils {
     private static final int[] APPCOMPAT_CHECK_ATTRS = {
             android.support.v7.appcompat.R.attr.colorPrimary};
 
