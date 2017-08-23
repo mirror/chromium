f6b99eaf07300910ce9473679cb31c828cae6c06
Geoff Lang <geofflang@chromium.org>
2017-08-23 16:10:59 -0400

    Only create the passthrough command decoder if it is supported.
    Check the available EGL extensions to determine if it is possible to
    support the passthrough command decoder and only create one if it is
    both requested and supportable.
    
    BUG=602688
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I6a38402088148a000d57cf05b8d7a70d32e60838

--- gpu/command_buffer/tests/gl_manager.cc
+++ gpu/command_buffer/tests/gl_manager.cc
@@ -306,8 +306,10 @@ void GLManager::InitializeWithCommandLine(
     GpuDriverBugWorkarounds gpu_driver_bug_workaround(&command_line);
     scoped_refptr<gles2::FeatureInfo> feature_info =
         new gles2::FeatureInfo(command_line, gpu_driver_bug_workaround);
+    // Always mark the passthrough command decoder as supported so that tests do
+    // not unexpectedly use the wrong command decoder
     context_group = new gles2::ContextGroup(
-        gpu_preferences_, mailbox_manager_, nullptr /* memory_tracker */,
+        gpu_preferences_, true, mailbox_manager_, nullptr /* memory_tracker */,
         translator_cache_.get(), &completeness_cache_, feature_info,
         options.bind_generates_resource, &image_manager_, options.image_factory,
         nullptr /* progress_reporter */, GpuFeatureInfo(),
@@ -333,19 +335,17 @@ void GLManager::InitializeWithCommandLine(
     context_ = scoped_refptr<gl::GLContext>(new gpu::GLContextVirtual(
         share_group_.get(), base_context_->get(), decoder_->AsWeakPtr()));
     ASSERT_TRUE(context_->Initialize(
-        surface_.get(),
-        GenerateGLContextAttribs(attribs, context_group->gpu_preferences())));
+        surface_.get(), GenerateGLContextAttribs(attribs, context_group)));
   } else {
     if (real_gl_context) {
       context_ = scoped_refptr<gl::GLContext>(new gpu::GLContextVirtual(
           share_group_.get(), real_gl_context, decoder_->AsWeakPtr()));
       ASSERT_TRUE(context_->Initialize(
-          surface_.get(),
-          GenerateGLContextAttribs(attribs, context_group->gpu_preferences())));
+          surface_.get(), GenerateGLContextAttribs(attribs, context_group)));
     } else {
       context_ = gl::init::CreateGLContext(
           share_group_.get(), surface_.get(),
-          GenerateGLContextAttribs(attribs, context_group->gpu_preferences()));
+          GenerateGLContextAttribs(attribs, context_group));
     }
   }
   ASSERT_TRUE(context_.get() != NULL) << "could not create GL context";
