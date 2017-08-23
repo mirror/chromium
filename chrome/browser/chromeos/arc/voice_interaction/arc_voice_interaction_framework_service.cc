128d3c8596669a8aac0f9894d4ea471b896353c6
Xiaohui Chen <xiaohuic@google.com>
2017-08-23 14:35:41 -0700

    Remove time barrier to start voice interaction
    Remove arbitrary time barrier to start voice interaction. It is
    causing perceivable user keyboard events drops.
    
    BUG=b:64866190
    TEST=build and flash locally, see keyboard more responsive
    
    Change-Id: Ic055868913a3be5e4b1b29623a803381c425652a

--- chrome/browser/chromeos/arc/voice_interaction/arc_voice_interaction_framework_service.cc
+++ chrome/browser/chromeos/arc/voice_interaction/arc_voice_interaction_framework_service.cc
@@ -540,14 +540,6 @@ bool ArcVoiceInteractionFrameworkService::InitiateUserInteraction() {
   }
 
   auto start_time = base::TimeTicks::Now();
-  if ((start_time - user_interaction_start_time_) <
-          kAllowedTimeSinceUserInteraction &&
-      context_request_remaining_count_) {
-    // If next request starts too soon and there is an active session in action,
-    // we should drop it.
-    VLOG(1) << "Rejected voice interaction request.";
-    return false;
-  }
   user_interaction_start_time_ = start_time;
   context_request_remaining_count_ = kContextRequestMaxRemainingCount;
   return true;
