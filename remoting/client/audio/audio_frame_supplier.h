1434fd7bfa71099f714a369315808394de5b6db2
Scott Nichols <nicholss@chromium.org>
2017-08-23 10:03:45 -0700

    Adding interfaces for future client audio work.
    Bug:611181
    Change-Id: I91f38c489f7b3d9791c50ae9e7ee64a9aad1fb68

---
+++ remoting/client/audio/audio_frame_supplier.h
@@ -0,0 +1,42 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_CLIENT_AUDIO_AUDIO_FRAME_SUPPLIER_H_
+#define REMOTING_CLIENT_AUDIO_AUDIO_FRAME_SUPPLIER_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <list>
+#include <memory>
+
+#include "base/callback.h"
+#include "base/macros.h"
+#include "remoting/proto/audio.pb.h"
+
+namespace remoting {
+
+// This class provides an interface to request audio frames of a given size
+// and a way to ask about the currently buffered audio data.
+// Audio Pipeline Context:
+// Stream -> Decode -> Stream Consumer -> Buffer -> [Frame Provider] -> Play
+class AudioFrameSupplier {
+ public:
+  AudioFrameSupplier() {}
+  virtual ~AudioFrameSupplier() {}
+  virtual uint32_t GetAudioFrame(void* buffer, uint32_t buffer_size) = 0;
+
+  // Methods to describe buffered data.
+  virtual AudioPacket::SamplingRate GetBufferedSamplingRate() = 0;
+  virtual AudioPacket::Channels GetBufferedChannels() = 0;
+  virtual AudioPacket::BytesPerSample GetBufferedByesPerSample() = 0;
+  virtual uint32_t GetBytesPerFrame() = 0;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(AudioFrameSupplier);
+};
+
+}  // namespace remoting
+
+#endif  // REMOTING_CLIENT_AUDIO_AUDIO_FRAME_SUPPLIER_H_
