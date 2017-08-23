ed4463fb049b87d68b69e43351ee5aeb6ed67224
Ke He <ke.he@intel.com>
2017-08-23 11:05:44 +0800

    Remove HidService::Observer::OnDeviceRemovedCleanup()
    DevicePermissionsManager has implicit dependency on HidDeviceManager.
    Now we make that dependency explict with removing the OnDeviceRemovedCleanup().
    
    The HidService::GetDevices(callback) is changed to run the callback
    synchronously.
    
    BUG=728223
    
    Change-Id: Iefe114a9cc3eb0dfcfd776caed1f49c548ae2c18

--- device/hid/hid_service.cc
+++ device/hid/hid_service.cc
@@ -32,10 +32,6 @@ void HidService::Observer::OnDeviceRemoved(
     scoped_refptr<HidDeviceInfo> device_info) {
 }
 
-void HidService::Observer::OnDeviceRemovedCleanup(
-    scoped_refptr<HidDeviceInfo> device_info) {
-}
-
 // static
 constexpr base::TaskTraits HidService::kBlockingTaskTraits;
 
@@ -53,16 +49,15 @@ std::unique_ptr<HidService> HidService::Create() {
 
 void HidService::GetDevices(const GetDevicesCallback& callback) {
   DCHECK(thread_checker_.CalledOnValidThread());
-  if (enumeration_ready_) {
-    std::vector<scoped_refptr<HidDeviceInfo>> devices;
-    for (const auto& map_entry : devices_) {
-      devices.push_back(map_entry.second);
-    }
+
+  bool was_empty = pending_enumerations_.empty();
+  pending_enumerations_.push_back(callback);
+  if (enumeration_ready_ && was_empty) {
     base::ThreadTaskRunnerHandle::Get()->PostTask(
-        FROM_HERE, base::Bind(callback, devices));
-  } else {
-    pending_enumerations_.push_back(callback);
+        FROM_HERE,
+        base::Bind(&HidService::RunPendingEnumerations, GetWeakPtr()));
   }
+  enumeration_ready_ = false;
 }
 
 void HidService::AddObserver(HidService::Observer* observer) {
@@ -127,27 +122,27 @@ void HidService::RemoveDevice(const HidPlatformDeviceId& platform_device_id) {
         observer.OnDeviceRemoved(device);
     }
     devices_.erase(device_guid);
-    if (enumeration_ready_) {
-      for (auto& observer : observer_list_)
-        observer.OnDeviceRemovedCleanup(device);
-    }
   }
 }
 
-void HidService::FirstEnumerationComplete() {
-  enumeration_ready_ = true;
-
+void HidService::RunPendingEnumerations() {
   if (!pending_enumerations_.empty()) {
     std::vector<scoped_refptr<HidDeviceInfo>> devices;
     for (const auto& map_entry : devices_) {
       devices.push_back(map_entry.second);
     }
 
-    for (const GetDevicesCallback& callback : pending_enumerations_) {
+    std::vector<GetDevicesCallback> callbacks;
+    callbacks.swap(pending_enumerations_);
+    for (const auto& callback : callbacks)
       callback.Run(devices);
-    }
-    pending_enumerations_.clear();
   }
+
+  enumeration_ready_ = true;
+}
+
+void HidService::FirstEnumerationComplete() {
+  RunPendingEnumerations();
 }
 
 std::string HidService::FindDeviceIdByPlatformDeviceId(
