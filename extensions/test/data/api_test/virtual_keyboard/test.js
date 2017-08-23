2afa93af281a4909b50b7b7473085b279cadc8a8
oka@chromium.org <oka@chromium.org>
2017-08-23 15:40:05 +0900

    Implement virtualKeyboard.restrictFeatures properly
    * API usage
    With proper IME extension side update, this change allows Kiosk app to
    update the following IME configs:
    - auto complete
    - auto correct
    - handwriting
    - spell_check
    - voice_input
    through chrome.virtualKeyboard.restrictFeatures API.
    
    For IME extension,
    - 'features' which chrome.virtualKeyboardPrivate.getKeyboardConfig returns
    contains these configs in the form of 'autocomplete-enabled'.
    - chrome.virtualKeyboardPrivate.onKeyboardConfigChanged event, which
    is fired on the config change, has been added.
    
    * Construct
    - keyboard_util.cc manages the config as a global variable. Using global state
    is not ideal but should work, because if config change happened, it means the
    device is under Kiosk app's control anyway.
    - ChromeVirtualKeyboardDelegate contains the actual logic to construct the
    APIs' return value.
    - All the other files are needed accommodation and wiring, or tests.
    
    Bug: 710242
    Test: Added a kiosk test app and run it in a browser_test,
        KioskVirtualKeyboardTest .
    Change-Id: Ie00d6545c5e189bf6132ed6278698f2f0e7d433a

--- extensions/test/data/api_test/virtual_keyboard/test.js
+++ extensions/test/data/api_test/virtual_keyboard/test.js
@@ -3,44 +3,49 @@
 // found in the LICENSE file.
 
 chrome.test.runTests([
-    function setRestrictedKeyboard() {
-      chrome.virtualKeyboard.restrictFeatures({
-        autoCompleteEnabled: true,
-        autoCorrectEnabled: true,
-        spellCheckEnabled: true,
-        voiceInputEnabled: true,
-        handwritingEnabled: true
-      }, chrome.test.callbackPass(function() {
+  function setRestrictedKeyboard() {
+    chrome.virtualKeyboard.restrictFeatures(
+        {
+          autoCompleteEnabled: true,
+          autoCorrectEnabled: true,
+          spellCheckEnabled: true,
+          voiceInputEnabled: true,
+          handwritingEnabled: true
+        },
+        chrome.test.callbackPass(function() {
           chrome.test.sendMessage('restrictedChanged',
               function(featuresEnabled) {
                 chrome.test.assertEq('true', featuresEnabled);
                 chrome.test.succeed();
               });
-          }));
-    },
-    function setNotRestrictedKeyboard() {
-      chrome.virtualKeyboard.restrictFeatures({
-        autoCompleteEnabled: false,
-        autoCorrectEnabled: false,
-        spellCheckEnabled: false,
-        voiceInputEnabled: false,
-        handwritingEnabled: false
-      }, chrome.test.callbackPass(function() {
-        chrome.test.sendMessage('restrictedChanged',
-            function(featuresEnabled) {
-              chrome.test.assertEq('false', featuresEnabled);
-              chrome.test.succeed();
-            });
         }));
-    },
-    function differentEnabledStates() {
-      var kError = 'All features are expected to have the same enabled state.';
-      chrome.virtualKeyboard.restrictFeatures({
-        autoCompleteEnabled: true,
-        autoCorrectEnabled: false,
-        spellCheckEnabled: true,
-        voiceInputEnabled: false,
-        handwritingEnabled: true
-      }, chrome.test.callbackFail(kError));
-    },
+  },
+  function setNotRestrictedKeyboard() {
+    chrome.virtualKeyboard.restrictFeatures(
+        {
+          autoCompleteEnabled: false,
+          autoCorrectEnabled: false,
+          spellCheckEnabled: false,
+          voiceInputEnabled: false,
+          handwritingEnabled: false
+        },
+        chrome.test.callbackPass(function() {
+          chrome.test.sendMessage(
+              'restrictedChanged',
+              chrome.test.callbackPass(function(featuresEnabled) {
+                chrome.test.assertEq('false', featuresEnabled);
+              }));
+        }));
+  },
+  function differentEnabledStates() {
+    chrome.virtualKeyboard.restrictFeatures(
+        {
+          autoCompleteEnabled: true,
+          autoCorrectEnabled: false,
+          spellCheckEnabled: true,
+          voiceInputEnabled: false,
+          handwritingEnabled: true
+        },
+        chrome.test.callbackPass());
+  },
 ]);
