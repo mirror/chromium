52e569198658bfd0d3973f81815275adf6c5550c
Bettina Dea <bdea@chromium.org>
2017-08-23 15:14:42 -0700

    Default to using the "x_minutes" field tiral param for feature promos.
    Previously each feature tracker hard coded the session duration. However,
    for testers (i.e. UX testers), it will be hard for them to wait for such
    a long time (in this case, it's 2 hours for new tab promo). Thus it would
    be better to add an extra param in the config "x_minutes" which will mitigate
    this problem. Also refactored some functionality that can be moved to the 
    feature_tracker. (ShouldShowPromo)
    
    Bug:734132
    Change-Id: Ic7593f3137d50a718eff72c0ec4807bc05847746

--- chrome/browser/feature_engagement/feature_tracker.cc
+++ chrome/browser/feature_engagement/feature_tracker.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/browser/feature_engagement/feature_tracker.h"
 
+#include "base/metrics/field_trial_params.h"
 #include "base/time/time.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
 #include "components/feature_engagement/public/event_constants.h"
@@ -34,6 +35,10 @@ bool FeatureTracker::IsObserving() {
   return session_duration_observer_.IsObserving(session_duration_updater_);
 }
 
+bool FeatureTracker::ShouldShowPromo(const base::Feature& feature) {
+  return GetTracker()->ShouldTriggerHelpUI(feature);
+}
+
 Tracker* FeatureTracker::GetTracker() const {
   return TrackerFactory::GetForBrowserContext(profile_);
 }
@@ -43,6 +48,20 @@ void FeatureTracker::OnSessionEnded(base::TimeDelta total_session_time) {
     OnSessionTimeMet();
     RemoveSessionDurationObserver();
   }
+}
+
+int FeatureTracker::GetSessionTimeRequiredToShowForFeature(
+    const base::Feature& feature,
+    int defaultRequiredTime) {
+  if (!has_retrieved_field_trial_minutes) {
+    field_trial_minutes_value =
+        base::GetFieldTrialParamValueByFeature(feature, "x_minutes");
+    has_retrieved_field_trial_minutes = true;
+  }
+
+  return field_trial_minutes_value.empty()
+             ? defaultRequiredTime
+             : std::stoi(field_trial_minutes_value, nullptr);
 }
 
 bool FeatureTracker::HasEnoughSessionTimeElapsed(
