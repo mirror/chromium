1b08e8a2b12b2e84fb2df8abece38a2c0f9ff6d3
chaopeng <chaopeng@chromium.org>
2017-08-22 21:22:54 -0400

    Add UX experimental feature flash all scrollbars for any scroll update
    This patch introduce an UX experimental feature flash all scrollbars for any
    scroll update for overlay scrollbar. This feature is behind a flag:
    OverlayScrollbarFlashWhenAnyScrollUpdate.
    
    Bug: 746342
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I1e99af493a12c813a77225836a421d0e72b5dc68

--- cc/trees/layer_tree_impl.cc
+++ cc/trees/layer_tree_impl.cc
@@ -860,11 +860,17 @@ void LayerTreeImpl::DidUpdatePageScale() {
   // Viewport scrollbar sizes depend on the page scale factor.
   SetScrollbarGeometriesNeedUpdate();
 
-  if (IsActiveTree() && layer_tree_host_impl_->ViewportMainScrollLayer()) {
-    if (ScrollbarAnimationController* controller =
-            layer_tree_host_impl_->ScrollbarAnimationControllerForElementId(
-                OuterViewportScrollLayer()->element_id()))
-      controller->DidScrollUpdate();
+  if (IsActiveTree()) {
+    if (settings().scrollbar_flash_when_after_scroll_update) {
+      layer_tree_host_impl_->FlashAllScrollbars(true);
+      return;
+    }
+    if (layer_tree_host_impl_->ViewportMainScrollLayer()) {
+      if (ScrollbarAnimationController* controller =
+              layer_tree_host_impl_->ScrollbarAnimationControllerForElementId(
+                  OuterViewportScrollLayer()->element_id()))
+        controller->DidScrollUpdate();
+    }
   }
 }
 
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -573,6 +573,9 @@ extern const char kOriginTrialsDescription[];
 extern const char kOverlayScrollbarsName[];
 extern const char kOverlayScrollbarsDescription[];
 
+extern const char kOverlayScrollbarsFlashWhenAnyScrollUpdateName[];
+extern const char kOverlayScrollbarsFlashWhenAnyScrollUpdateDescription[];
+
 extern const char kOverscrollHistoryNavigationName[];
 extern const char kOverscrollHistoryNavigationDescription[];
 extern const char kOverscrollHistoryNavigationSimpleUi[];
