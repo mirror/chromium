1bf59291511ea0487b973c9e5a8c4c53cf7791b5
Bettina Dea <bdea@chromium.org>
2017-08-23 15:47:26 -0700

    Added Bookmark feature in IPH.
    Changes in this Patch:
    - Added event and feature constants for Bookmarks.
    - Added the BookmarkInProductHelp trial to the fieldtrial json.
    - Created a BookmarkTracker class, which contains methods to notify the IPH server
    when events happen and if the help UI should be triggered.
    - Created a BookmarkTrackerFactory class and registered in the chrome browser main parts.
    - Added unit tests for BookmarkTracker event logging and expected functionality.
    
    Design Doc: https://docs.google.com/a/chromium.org/document/d/1j_npx4R1V8IhWY1bHCwvAVpzWUbAl26tlY5jpJ_Rdgc/edit?usp=sharing
    
    This is following the intern's, Chris, CL that wasn't submitted:
    https://chromium-review.googlesource.com/c/581872
    
    Bug:734132
    Change-Id: Ifc4b4e07ce5f2427f9210dac739df67637cfd66e

--- components/feature_engagement/public/feature_list.h
+++ components/feature_engagement/public/feature_list.h
@@ -55,6 +55,7 @@ DEFINE_VARIATION_PARAM(kIPHChromeHomeExpandFeature, "IPH_ChromeHomeExpand");
 DEFINE_VARIATION_PARAM(kIPHMediaDownloadFeature, "IPH_MediaDownload");
 #endif  // defined(OS_ANDROID)
 #if defined(OS_WIN) || defined(OS_LINUX)
+DEFINE_VARIATION_PARAM(kIPHBookmarkFeature, "IPH_Bookmark");
 DEFINE_VARIATION_PARAM(kIPHIncognitoWindowFeature, "IPH_IncognitoWindow");
 DEFINE_VARIATION_PARAM(kIPHNewTabFeature, "IPH_NewTab");
 #endif  // defined(OS_WIN) || defined(OS_LINUX)
@@ -80,6 +81,7 @@ constexpr flags_ui::FeatureEntry::FeatureVariation
         VARIATION_ENTRY(kIPHChromeHomeExpandFeature),
         VARIATION_ENTRY(kIPHMediaDownloadFeature),
 #elif defined(OS_WIN) || defined(OS_LINUX)
+        VARIATION_ENTRY(kIPHBookmarkFeature),
         VARIATION_ENTRY(kIPHIncognitoWindowFeature),
         VARIATION_ENTRY(kIPHNewTabFeature),
 #elif defined(OS_IOS)
