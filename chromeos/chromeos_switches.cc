89b9ddf7431146641ecbd14fb31aae6a12ddb8a4
Yue Li <updowndota@chromium.org>
2017-08-23 15:08:20 -0700

    Error message when voice interaciton has unsupported language
    - Show a toast with error message when voice interaction has unsupported
      locales.
    - Update CrOS swithces for voice interaction.
    
    BUG=b/64692482
    
    Change-Id: I1d5394175ea83ae26f9fdd5d13f890ed52d63aa6

--- chromeos/chromeos_switches.cc
+++ chromeos/chromeos_switches.cc
@@ -580,23 +580,31 @@ bool IsCellularFirstDevice() {
   return base::CommandLine::ForCurrentProcess()->HasSwitch(kCellularFirst);
 }
 
-bool IsVoiceInteractionEnabled() {
-  // TODO(updowndota): Add DCHECK here to make sure the value never changes
-  // after all the use case for this method has been moved into user session.
+bool IsVoiceInteractionLocalesSupported() {
+  // Make sure the locale value does not change.
+  static const std::string kLocale = icu::Locale::getDefault().getName();
+  DCHECK_EQ(kLocale, icu::Locale::getDefault().getName());
 
   // Disable voice interaction for non-supported locales.
-  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  std::string locale = icu::Locale::getDefault().getName();
-  if (locale != ULOC_US && locale != ULOC_UK && locale != ULOC_CANADA &&
-      command_line
+  if (kLocale != ULOC_US && kLocale != ULOC_UK && kLocale != ULOC_CANADA &&
+      base::CommandLine::ForCurrentProcess()
               ->GetSwitchValueASCII(
                   chromeos::switches::kVoiceInteractionLocales)
-              .find(locale) == std::string::npos) {
+              .find(kLocale) == std::string::npos) {
     return false;
   }
+  return true;
+}
 
+bool IsVoiceInteractionFlagsEnabled() {
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
   return command_line->HasSwitch(kEnableVoiceInteraction) ||
          base::FeatureList::IsEnabled(kVoiceInteractionFeature);
+}
+
+bool IsVoiceInteractionEnabled() {
+  return IsVoiceInteractionLocalesSupported() &&
+         IsVoiceInteractionFlagsEnabled();
 }
 
 }  // namespace switches
