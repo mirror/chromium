538dc3c2c22a17ffcf4ddac642fa67c055823e2e
Matthew Jones <mdjones@chromium.org>
2017-08-24 09:31:37 -0700

    [Home] Fix sheet content transition background color
    The background color of the bottom sheet's content should be white for
    the modern design.
    
    BUG=738478
    
    Change-Id: I27eb8fc06f8a80ff84e04edd4e7cecf38a76dd69

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheet.java
@@ -708,6 +708,10 @@ public class BottomSheet
 
         mBottomSheetContentContainer = (FrameLayout) findViewById(R.id.bottom_sheet_content);
 
+        if (FeatureUtilities.isChromeHomeModernEnabled()) {
+            mBottomSheetContentContainer.setBackgroundColor(Color.WHITE);
+        }
+
         // Listen to height changes on the root.
         root.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
             private int mPreviousKeyboardHeight;
@@ -1009,7 +1013,7 @@ public class BottomSheet
         // Temporarily make the background of the toolbar holder a solid color so the transition
         // doesn't appear to show a hole in the toolbar.
         int colorId = content.isIncognitoThemedContent() ? R.color.incognito_primary_color
-                                                         : R.color.modern_light_grey;
+                                                         : android.R.color.white;
         mToolbarHolder.setBackgroundColor(ApiCompatibilityUtils.getColor(getResources(), colorId));
         mBottomSheetContentContainer.setBackgroundColor(
                 ApiCompatibilityUtils.getColor(getResources(), colorId));
