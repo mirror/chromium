eea4467e84d34dcda1a5e99709ff898879de7723
Sergey Ulanov <sergeyu@google.com>
2017-08-24 16:34:40 -0700

    Add TestServerConfig to allow testserver outside localhost
    Previuosly testserver was always assumed to be running on localhost
    even when started with RemoteTestServer. This CL adds TestServerConfig
    that allows to pass to the tests configuratgion for the test server.
    The config specifies how the tests should connect to the test spawner
    and to the test server itself. The config also replaces the mechanism
    that was used to pass spawner's port number on Android (through
    net-test-server-ports file).
    
    Bug: 731302
    Change-Id: Ib2e07e6d293c52164b12b73dfd5ebe13d89d018d

--- net/test/spawned_test_server/base_test_server.cc
+++ net/test/spawned_test_server/base_test_server.cc
@@ -26,6 +26,7 @@
 #include "net/cert/x509_certificate.h"
 #include "net/dns/host_resolver.h"
 #include "net/log/net_log_with_source.h"
+#include "net/test/spawned_test_server/test_server_config.h"
 #include "url/gurl.h"
 
 namespace net {
@@ -34,6 +35,8 @@ namespace {
 
 std::string GetHostname(BaseTestServer::Type type,
                         const BaseTestServer::SSLOptions& options) {
+  TestServerConfig* config = TestServerConfig::Get();
+
   if (BaseTestServer::UsingSSL(type)) {
     if (options.server_certificate ==
             BaseTestServer::SSLOptions::CERT_MISMATCHED_NAME ||
@@ -42,12 +45,12 @@ std::string GetHostname(BaseTestServer::Type type,
       // For |CERT_MISMATCHED_NAME|, return a different hostname string
       // that resolves to the same hostname. For
       // |CERT_COMMON_NAME_IS_DOMAIN|, the certificate is issued for
-      // "localhost" instead of "127.0.0.1".
-      return "localhost";
+      // domain name instead of the address.
+      return config->name();
     }
   }
 
-  return "127.0.0.1";
+  return config->address().ToString();
 }
 
 std::string GetClientCertType(SSLClientCertType type) {
@@ -249,13 +252,13 @@ std::string BaseTestServer::SSLOptions::GetOCSPProducedArgument() const {
 }
 
 BaseTestServer::BaseTestServer(Type type) : type_(type) {
-  Init(GetHostname(type, ssl_options_));
+  Init();
 }
 
 BaseTestServer::BaseTestServer(Type type, const SSLOptions& ssl_options)
     : ssl_options_(ssl_options), type_(type) {
   DCHECK(UsingSSL(type));
-  Init(GetHostname(type, ssl_options));
+  Init();
 }
 
 BaseTestServer::~BaseTestServer() {}
@@ -420,8 +423,8 @@ scoped_refptr<X509Certificate> BaseTestServer::GetCertificate() const {
   return certs_in_file[0];
 }
 
-void BaseTestServer::Init(const std::string& host) {
-  host_port_pair_ = HostPortPair(host, 0);
+void BaseTestServer::Init() {
+  host_port_pair_ = HostPortPair(GetHostname(type_, ssl_options_), 0);
 
   // TODO(battre) Remove this after figuring out why the TestServer is flaky.
   // http://crbug.com/96594
@@ -489,7 +492,7 @@ void BaseTestServer::CleanUpWhenStoppingServer() {
 bool BaseTestServer::GenerateArguments(base::DictionaryValue* arguments) const {
   DCHECK(arguments);
 
-  arguments->SetString("host", host_port_pair_.host());
+  arguments->SetString("hostname", host_port_pair_.host());
   arguments->SetInteger("port", host_port_pair_.port());
   arguments->SetString("data-dir", document_root_.value());
 
