a6a3e08e6e72c4687022b86eb8558cb926077343
Yixin Wang <wangyix@chromium.org>
2017-08-24 13:56:46 -0700

    Add support for parsing IETF format of alt-svc header advertising QUIC
    Change-Id: Idb25c187d402edc5e6c180c529fc3eede2c416bf

--- net/spdy/core/spdy_alt_svc_wire_format.cc
+++ net/spdy/core/spdy_alt_svc_wire_format.cc
@@ -33,6 +33,84 @@ bool ParsePositiveIntegerImpl(SpdyStringPiece::const_iterator c,
   return (c == end && *value > 0);
 }
 
+const char VALUE_TO_HEX_DIGIT[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                                     '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+// Will not include leading zeros. Example:
+// Input:   0xCDAB2301
+// Output:  "123ABCD"
+SpdyString LittleEndianUIntToHexString(uint32_t input) {
+  char hex_digits[8];
+  for (int i = 0; i < 8; i += 2) {
+    uint32_t lower_nibble = input & 0xF;
+    input >>= 4;
+    uint32_t upper_nibble = input & 0xF;
+    input >>= 4;
+    DCHECK_LT(lower_nibble, 16u);
+    DCHECK_LT(upper_nibble, 16u);
+    hex_digits[i] = VALUE_TO_HEX_DIGIT[upper_nibble];
+    hex_digits[i + 1] = VALUE_TO_HEX_DIGIT[lower_nibble];
+  }
+  // Trim leading zeros except for last digit.
+  int i = 0;
+  for (; hex_digits[i] == '0' && i < 7; ++i)
+    ;
+  return SpdyString(&(hex_digits[i]), 8 - i);
+}
+
+const char HEX_DIGIT_TO_VALUE[256] = {
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    0,    1,    2,    3,    4,    5,    6,    7,    8,    9,    -128, -128,
+    -128, -128, -128, -128, -128, 10,   11,   12,   13,   14,   15,   -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, 10,   11,   12,   13,   14,   15,   -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128, -128,
+    -128, -128, -128, -128};
+
+// Input must be 1 to 8 characters long. Example:
+// Input:  "123ABCD"
+// Output: 0xCDAB2301
+bool LittleEndianUIntFromHexString(const std::string& hex_string,
+                                   uint32_t* output) {
+  DCHECK(!hex_string.empty());
+  DCHECK_LE(hex_string.size(), 8u);
+  *output = 0;
+  int i = hex_string.size() - 1;
+  while (i >= 1) {
+    int upper_nibble = HEX_DIGIT_TO_VALUE[(int)hex_string[i - 1]];
+    int lower_nibble = HEX_DIGIT_TO_VALUE[(int)hex_string[i]];
+    int byte_value = upper_nibble << 4 | lower_nibble;
+    if (byte_value < 0) {
+      return false;
+    }
+    *output = (*output << 8) | byte_value;
+    i -= 2;
+  }
+  if (i == 0) {
+    int lower_nibble = HEX_DIGIT_TO_VALUE[(int)hex_string[i]];
+    if (lower_nibble < 0) {
+      return false;
+    }
+    *output = (*output << 8) | lower_nibble;
+  }
+  return true;
+}
+
 }  // namespace
 
 SpdyAltSvcWireFormat::AlternativeService::AlternativeService() {}
@@ -76,6 +154,9 @@ bool SpdyAltSvcWireFormat::ParseHeaderFieldValue(
         !PercentDecode(c, percent_encoded_protocol_id_end, &protocol_id)) {
       return false;
     }
+    // Check for IETF format for advertising QUIC:
+    // h2q=":49288";quic=51303338;quic=51303334
+    const bool is_ietf_format_quic = (protocol_id.compare("h2q") == 0);
     c = percent_encoded_protocol_id_end;
     if (c == value.end()) {
       return false;
@@ -146,7 +227,7 @@ bool SpdyAltSvcWireFormat::ParseHeaderFieldValue(
         if (!ParsePositiveInteger32(parameter_value_begin, c, &max_age)) {
           return false;
         }
-      } else if (parameter_name.compare("v") == 0) {
+      } else if (!is_ietf_format_quic && parameter_name.compare("v") == 0) {
         // Version is a comma separated list of positive integers enclosed in
         // quotation marks.  Since it can contain commas, which are not
         // delineating alternative service entries, |parameters_end| and |c| can
@@ -177,6 +258,31 @@ bool SpdyAltSvcWireFormat::ParseHeaderFieldValue(
             return false;
           }
         }
+      } else if (is_ietf_format_quic && parameter_name.compare("quic") == 0) {
+        // IETF format for advertising QUIC. Version is hex encoding of QUIC
+        // version tag. Hex-encoded string should not include leading "0x" or
+        // leading zeros, unless the version is just "0".
+        // Example for advertising QUIC versions "Q038" and "Q034":
+        // h2q=":49288";quic=51303338;quic=51303334
+        if (*parameter_value_begin == '0' && c != parameter_value_begin + 1) {
+          return false;
+        }
+        // Versions will be stored as the little-endian uint32_t value of the
+        // version tag. Example: QUIC version "Q038", which is advertised as:
+        // h2q=":49288";quic=51303338
+        // ... will be stored in |versions| as 0x38333051.
+        uint32_t quic_version_tag;
+        if (!LittleEndianUIntFromHexString(
+                std::string(parameter_value_begin, c), &quic_version_tag)) {
+          return false;
+        }
+        version.push_back(quic_version_tag);
+        /*uint32_t quic_version_tag_reversed;
+        if (!base::HexStringToUInt(std::string(parameter_value_begin, c),
+                                   &quic_version_tag_reversed)) {
+          return false;
+        }
+        version.push_back(ReverseUIntByteOrder(quic_version_tag_reversed));*/
       }
     }
     altsvc_vector->emplace_back(protocol_id, host, port, max_age, version);
@@ -198,6 +304,8 @@ SpdyString SpdyAltSvcWireFormat::SerializeHeaderFieldValue(
     if (!value.empty()) {
       value.push_back(',');
     }
+    // Check for IETF format for advertising QUIC.
+    const bool is_ietf_format_quic = (altsvc.protocol_id.compare("h2q") == 0);
     // Percent escape protocol id according to
     // http://tools.ietf.org/html/rfc7230#section-3.2.6.
     for (char c : altsvc.protocol_id) {
@@ -243,15 +351,24 @@ SpdyString SpdyAltSvcWireFormat::SerializeHeaderFieldValue(
       SpdyStringAppendF(&value, "; ma=%d", altsvc.max_age);
     }
     if (!altsvc.version.empty()) {
-      value.append("; v=\"");
-      for (VersionVector::const_iterator it = altsvc.version.begin();
-           it != altsvc.version.end(); ++it) {
-        if (it != altsvc.version.begin()) {
-          value.append(",");
+      if (is_ietf_format_quic) {
+        for (VersionVector::const_iterator it = altsvc.version.begin();
+             it != altsvc.version.end(); ++it) {
+          value.append("; quic=");
+          uint32_t quic_version_tag = *it;
+          value.append(LittleEndianUIntToHexString(quic_version_tag));
         }
-        SpdyStringAppendF(&value, "%d", *it);
+      } else {
+        value.append("; v=\"");
+        for (VersionVector::const_iterator it = altsvc.version.begin();
+             it != altsvc.version.end(); ++it) {
+          if (it != altsvc.version.begin()) {
+            value.append(",");
+          }
+          SpdyStringAppendF(&value, "%d", *it);
+        }
+        value.append("\"");
       }
-      value.append("\"");
     }
   }
   return value;
