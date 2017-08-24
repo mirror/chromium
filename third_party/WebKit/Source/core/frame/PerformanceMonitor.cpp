d055db4fa749a94957dad9ce315e6fc95254de13
Liquan (Max) Gu <maxlg@chromium.org>
2017-08-24 14:02:26 -0400

    Long tasks v2: add probe for V8 Execute Script
    This CL is to implement probe::V8ExecuteScript as an effort to attribute long tasks with the subtasks 'script-run'. 'script-run' corresponds to V8ScriptRunner::RunCompileScript() and v8ScriptRunner::EvaluateModule().
    
    The probe records the url of culprit script, and finally become entries in the attribution of long task performance entries.
    
    This feature is temporarily hidden behind a command line flag "LongTaskV2".
    
    Bug: 757858
    Change-Id: Idacbfeb2221a7dc5c5cc0f8807863af7c736a21c

--- third_party/WebKit/Source/core/frame/PerformanceMonitor.cpp
+++ third_party/WebKit/Source/core/frame/PerformanceMonitor.cpp
@@ -173,10 +173,24 @@ void PerformanceMonitor::Did(const probe::UpdateLayout& probe) {
 
 void PerformanceMonitor::Will(const probe::ExecuteScript& probe) {
   WillExecuteScript(probe.context);
+
+  probe.CaptureStartTime();
 }
 
 void PerformanceMonitor::Did(const probe::ExecuteScript& probe) {
   DidExecuteScript();
+
+  if (!enabled_ || !thresholds_[kLongTask])
+    return;
+
+  double v8_execute_script_duration = probe.Duration();
+  if (v8_execute_script_duration <= kLongTaskSubTaskThresholdInSeconds)
+    return;
+  std::unique_ptr<SubTaskAttribution> sub_task_attribution =
+      SubTaskAttribution::Create(
+          String("script-run"), probe.context->Url().GetString(),
+          probe.CaptureStartTime(), v8_execute_script_duration);
+  sub_task_attributions_.push_back(std::move(sub_task_attribution));
 }
 
 void PerformanceMonitor::Will(const probe::CallFunction& probe) {
