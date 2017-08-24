a3e4a2f1512bcd49277195948d9547ad2ec4c1d1
galinap <galinap@google.com>
2017-08-24 23:08:10 +0100

    [Site Explore] Override SiteExploreViewPager.onMeasure to measure height of children
    We need to override SiteExploreViewPager.onMeasure because ViewPager does not 
    properly handle wrap_content as its height measurement. It sets the height to 0.
    This is because different pages might have different heights and the ViewPager 
    doesn't know which one to choose.
    We know that the SiteExploreViewPager will hold a TabLayout and a TileGridLayout 
    at all times, so we can calculate its height explicitly.
    
    Bug:7084
    Change-Id: I1263b98999a4f19e3647d0bf0e8b9dbb5f1b210e

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SiteExploreViewPager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SiteExploreViewPager.java
@@ -20,23 +20,49 @@ public class SiteExploreViewPager extends ViewPager {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        View tabLayout = getChildAt(0);
-        tabLayout.measure(
-                widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-        int tabLayoutHeight = tabLayout.getMeasuredHeight();
+        // We need to override this method because ViewPager does not properly handle wrap_content
+        // as its height measurement. It relies on the container view to give it a height and
+        // based on that value will crop its children. So, we will need to measure how much space
+        // the ViewPager will need to show its contents.
+        // We know that this SiteExploreViewPager will hold a TabLayout and a TileGridLayout at all
+        // times, so we can calculate what height it will need to be.
 
-        int maxChildHeight = 0;
-        for (int i = 1; i < getChildCount(); i++) {
+        // We need to call this to let the ViewPager initialize its children pages first. This will
+        // call ViewPagerAdapter.initializeItem(), which will add the pages. However, this
+        // does not measure the heights of the children - it sets their height depending on its
+        // own height, which is 0. That's why we need to measure the children's heights
+        // explicitly after that.
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int neededHeight = 0;
+
+        // Get the required height from the view pager's children. ViewPager decors (e.g. TabLayout)
+        // and ViewPager pages are both ViewPager children. The decors come first. We add up the
+        // heights of all decors and the height of the first page to get the required height for the
+        // view pager. We assume that all pages have the same height.
+        for (int i = 0; i < getChildCount(); i++) {
             View child = getChildAt(i);
             child.measure(
                     widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-            int childHeight = child.getMeasuredHeight();
-            if (childHeight > maxChildHeight) maxChildHeight = childHeight;
+
+            neededHeight += child.getMeasuredHeight();
+
+            // Break the loop when we see the first page (non decor view).
+            if (!((ViewPager.LayoutParams) child.getLayoutParams()).isDecor) {
+                break;
+            }
         }
 
-        heightMeasureSpec =
-                MeasureSpec.makeMeasureSpec(maxChildHeight + tabLayoutHeight, MeasureSpec.EXACTLY);
+        // Take into account vertical padding.
+        neededHeight += getPaddingTop() + getPaddingBottom();
 
+        // Set the height of the ViewPager to the sum of the height of the TabLayout, the height
+        // of one of its pages and any vertical padding.
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(neededHeight, MeasureSpec.EXACTLY);
+
+        // TODO(galinap): Restrict view pager's width to tile_grid_layout_max_width in NTP.
+
+        // Super method has to be called again so the new specs are treated as exact measurements.
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 }
