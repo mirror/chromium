84490b15a0ba59e1fca3c7f672a448abc50c7a1c
Ke He <ke.he@intel.com>
2017-08-24 15:55:09 +0800

    Add hid.mojom, re-define the device::HidDeviceInfo.
    This is a prepare CL for the mojofication in next step.
    
    1) Add hid.mojom in which we define the device::mojom::HidDeviceInfo.
    
    2) Re-define the device::HidDeviceInfo, the device::HidDeviceInfo will be
       internal and won't exposed to client(after mojofication).
       Remove the HidDeviceInfoLinux.
    
    3) Let all clients of HidService use device::mojom::HidDeviceInfo. Remove
       the scoped_refptr<HidDeviceInfo> in client side, client should own
       the device::mojom::HidDeviceInfo instead of sharing the same one.
    
    4) Remove the HidService::GetDeviceInfo() which is only used by
       HidDeviceManager. HidDeviceManager should keep the DeviceInfo in its map.
    
    5) Change the tests accordingly, make sure all the tests pass.
    
    BUG=728223
    
    Remove HidService::Observer::OnDeviceRemovedCleanup()
    
    DevicePermissionsManager has implicit dependency on HidDeviceManager.
    Now we make that dependency explict with removing the OnDeviceRemovedCleanup().
    
    BUG=728223
    
    Change-Id: I83a0d96ae4c1a8c3b4919c2b126399a10515601f

--- device/hid/hid_service.h
+++ device/hid/hid_service.h
@@ -14,11 +14,13 @@
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/single_thread_task_runner.h"
 #include "base/task_scheduler/task_traits.h"
 #include "base/threading/thread_checker.h"
 #include "device/hid/hid_device_info.h"
+#include "device/hid/public/interfaces/hid.mojom.h"
 
 namespace device {
 
@@ -30,20 +32,15 @@ class HidService {
  public:
   class Observer {
    public:
-    virtual void OnDeviceAdded(scoped_refptr<HidDeviceInfo> info);
+    virtual void OnDeviceAdded(device::mojom::HidDeviceInfoPtr info);
     // Notifies all observers that a device is being removed, called before
     // removing the device from HidService. Observers should not depend on the
     // order in which they are notified of the OnDeviceRemove event.
-    virtual void OnDeviceRemoved(scoped_refptr<HidDeviceInfo> info);
-    // Notifies all observers again, after having first notified all observers
-    // with OnDeviceRemoved and removed the device from internal structures.
-    // Each observer must not depend on any other observers' awareness of the
-    // device as they could be cleaned up in any order.
-    virtual void OnDeviceRemovedCleanup(scoped_refptr<HidDeviceInfo> info);
+    virtual void OnDeviceRemoved(device::mojom::HidDeviceInfoPtr info);
   };
 
   using GetDevicesCallback =
-      base::Callback<void(const std::vector<scoped_refptr<HidDeviceInfo>>&)>;
+      base::Callback<void(const std::vector<device::mojom::HidDeviceInfoPtr>&)>;
   using ConnectCallback =
       base::Callback<void(scoped_refptr<HidConnection> connection)>;
 
@@ -63,9 +60,6 @@ class HidService {
   void AddObserver(Observer* observer);
   void RemoveObserver(Observer* observer);
 
-  scoped_refptr<HidDeviceInfo> GetDeviceInfo(
-      const std::string& device_guid) const;
-
   // Opens a connection to a device. The callback will be run with null on
   // failure.
   virtual void Connect(const std::string& device_guid,
@@ -78,6 +72,7 @@ class HidService {
 
   HidService();
 
+  virtual base::WeakPtr<HidService> GetWeakPtr() = 0;
   void AddDevice(scoped_refptr<HidDeviceInfo> info);
   void RemoveDevice(const HidPlatformDeviceId& platform_device_id);
   void FirstEnumerationComplete();
@@ -87,6 +82,7 @@ class HidService {
   base::ThreadChecker thread_checker_;
 
  private:
+  void RunPendingEnumerations();
   std::string FindDeviceIdByPlatformDeviceId(
       const HidPlatformDeviceId& platform_device_id);
 
