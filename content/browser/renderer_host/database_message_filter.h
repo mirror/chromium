7690ac6e51ca26c84eaa13d953a570b2ab55ffbf
Stuart Langley <slangley@chromium.org>
2017-08-24 10:04:27 +1000

    Mojofy the DatabaseMsg IPC used for WebDatabase calls from browser to renderer.
    This CL is a straight forward conversion of the DatabaseMsg IPC calls to mojo.
    As the name content::Database is already taken the mojo interface was called
    WebDatabase and the implementation WebDatabaseImpl. Once onion souped it is
    likely that this class will replace the existing blink::WebDatabase public
    interface.
    
    The mojo conversion is slightly complicated as the setup and teardown of the
    client side binding needs to occur on different threads to where the interface
    is called.
    
    Bug: 757277
    Change-Id: Idd6ae95fcd34732751aad2a6c5a7759c591f04e7

--- content/browser/renderer_host/database_message_filter.h
+++ content/browser/renderer_host/database_message_filter.h
@@ -9,6 +9,7 @@
 
 #include "base/containers/hash_tables.h"
 #include "base/strings/string16.h"
+#include "content/common/database.mojom.h"
 #include "content/public/browser/browser_message_filter.h"
 #include "ipc/ipc_platform_file.h"
 #include "storage/browser/database/database_tracker.h"
@@ -24,7 +25,8 @@ namespace content {
 class DatabaseMessageFilter : public BrowserMessageFilter,
                               public storage::DatabaseTracker::Observer {
  public:
-  explicit DatabaseMessageFilter(storage::DatabaseTracker* db_tracker);
+  explicit DatabaseMessageFilter(int process_id,
+                                 storage::DatabaseTracker* db_tracker);
 
   // BrowserMessageFilter implementation.
   void OnChannelClosing() override;
@@ -93,6 +95,17 @@ class DatabaseMessageFilter : public BrowserMessageFilter,
                           IPC::Message* reply_msg,
                           int reschedule_count);
 
+  // Callback to ensure we close the connection to the WebDatabase mojo
+  // interface on the db_tracker task runner.
+  void CloseOnDatabaseTracker();
+
+  // Helper function to get the mojo interface for the WebDatabase on the
+  // render process. Will establish the connection if it does not already exist.
+  content::mojom::WebDatabase& GetWebDatabase();
+
+  // The render process ID that we use to bind to the correct render process.
+  const int process_id_;
+
   // The database tracker for the current browser context.
   scoped_refptr<storage::DatabaseTracker> db_tracker_;
 
@@ -102,6 +115,9 @@ class DatabaseMessageFilter : public BrowserMessageFilter,
 
   // Keeps track of all DB connections opened by this renderer
   storage::DatabaseConnections database_connections_;
+
+  // Allows us to call render process to notify change in database state.
+  content::mojom::WebDatabasePtr database_provider_;
 };
 
 }  // namespace content
