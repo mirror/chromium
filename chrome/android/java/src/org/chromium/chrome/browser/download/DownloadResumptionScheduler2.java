0263e5d3a5e5857d3bb8cd0ff97b903b6f316a20
Joy Ming <jming@chromium.org>
2017-08-24 15:55:38 -0700

    Separate all refactor code for downloads as a foreground service.
    The refactor that makes downloads a foreground service for all versions
    of Android is still in progress. In order to maintain stability for M62
    branch point, this CL separates all changes related to this refactor
    into repeated classes (ie. DownloadsNotificationService2) or new classes
    that will not affect the stability of M62. 
    
    The versions without the numbers (ie. DownloadsNotificationService)
    should be the "old" version, before the downloads as a foreground
    refactor (approx commit 16e22136cb71398b57c282ac4a75ef0de834eac4). The
    versions with the numbers (ie. DownloadNotificationService2) is a
    snapshot of the most current state in the downloads foreground refactor
    (approx commit db5b34d2ad8a7e934c083bbc14e350924dbc00cf). There is
    currently no branch point between the two versions.
    
    Bug:747563
    Change-Id: Ie552e81fa9e04217d03730e62b62433df61ba26d

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadResumptionScheduler2.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadResumptionScheduler2.java
@@ -1,11 +1,11 @@
 // Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 package org.chromium.chrome.browser.download;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
 
 import com.google.android.gms.gcm.GcmNetworkManager;
 import com.google.android.gms.gcm.OneoffTask;
@@ -15,43 +15,50 @@ import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.SuppressFBWarnings;
 import org.chromium.chrome.browser.ChromeBackgroundService;
+
 /**
- * Class for scheduing download resumption tasks.
+ * Class for scheduling download resumption tasks.
  */
-public class DownloadResumptionScheduler {
+public class DownloadResumptionScheduler2 {
     public static final String TASK_TAG = "DownloadResumption";
     private static final String TAG = "DownloadScheduler";
     private static final int ONE_DAY_IN_SECONDS = 24 * 60 * 60;
     private final Context mContext;
+    private final DownloadNotificationService2 mDownloadNotificationService;
     @SuppressLint("StaticFieldLeak")
-    private static DownloadResumptionScheduler sDownloadResumptionScheduler;
+    private static DownloadResumptionScheduler2 sDownloadResumptionScheduler;
+
     @SuppressFBWarnings("LI_LAZY_INIT")
-    public static DownloadResumptionScheduler getDownloadResumptionScheduler(Context context) {
+    public static DownloadResumptionScheduler2 getDownloadResumptionScheduler(Context context) {
         assert context == context.getApplicationContext();
         if (sDownloadResumptionScheduler == null) {
-            sDownloadResumptionScheduler = new DownloadResumptionScheduler(context);
+            sDownloadResumptionScheduler = new DownloadResumptionScheduler2(context);
         }
         return sDownloadResumptionScheduler;
     }
-    protected DownloadResumptionScheduler(Context context) {
+
+    protected DownloadResumptionScheduler2(Context context) {
         mContext = context;
+        mDownloadNotificationService = DownloadNotificationService2.getInstance();
     }
+
     /**
      * For tests only: sets the DownloadResumptionScheduler.
-     * @param service An instance of DownloadResumptionScheduler.
+     * @param scheduler An instance of DownloadResumptionScheduler.
      */
     @VisibleForTesting
-    public static void setDownloadResumptionScheduler(DownloadResumptionScheduler scheduler) {
+    public static void setDownloadResumptionScheduler(DownloadResumptionScheduler2 scheduler) {
         sDownloadResumptionScheduler = scheduler;
     }
+
     /**
      * Schedules a future task to start download resumption.
      * @param allowMeteredConnection Whether download resumption can start if connection is metered.
      */
     public void schedule(boolean allowMeteredConnection) {
         GcmNetworkManager gcmNetworkManager = GcmNetworkManager.getInstance(mContext);
-        int networkType = allowMeteredConnection
-                ? Task.NETWORK_STATE_CONNECTED : Task.NETWORK_STATE_UNMETERED;
+        int networkType = allowMeteredConnection ? Task.NETWORK_STATE_CONNECTED
+                                                 : Task.NETWORK_STATE_UNMETERED;
         OneoffTask task = new OneoffTask.Builder()
                                   .setService(ChromeBackgroundService.class)
                                   .setExecutionWindow(0, ONE_DAY_IN_SECONDS)
@@ -66,6 +73,7 @@ public class DownloadResumptionScheduler {
             Log.e(TAG, "unable to schedule resumption task.", e);
         }
     }
+
     /**
      * Cancels a download resumption task if it is scheduled.
      */
@@ -73,13 +81,11 @@ public class DownloadResumptionScheduler {
         GcmNetworkManager gcmNetworkManager = GcmNetworkManager.getInstance(mContext);
         gcmNetworkManager.cancelTask(TASK_TAG, ChromeBackgroundService.class);
     }
+
     /**
      * Start browser process and resumes all interrupted downloads.
      */
     public void handleDownloadResumption() {
-        // Fire an intent to the DownloadNotificationService so that it will handle download
-        // resumption.
-        Intent intent = new Intent(DownloadNotificationService.ACTION_DOWNLOAD_RESUME_ALL);
-        DownloadNotificationService.startDownloadNotificationService(mContext, intent);
+        mDownloadNotificationService.resumeAllPendingDownloads();
     }
 }
--- chrome/android/javatests/src/org/chromium/chrome/browser/download/MockDownloadNotificationService.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/MockDownloadNotificationService.java
@@ -1,33 +1,58 @@
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 package org.chromium.chrome.browser.download;
 
-import static org.chromium.chrome.browser.notifications.NotificationConstants.DEFAULT_NOTIFICATION_ID;
-
 import android.app.Notification;
-import android.content.Intent;
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.util.Pair;
 
 import org.chromium.base.ThreadUtils;
-import org.chromium.chrome.browser.util.IntentUtils;
 import org.chromium.components.offline_items_collection.ContentId;
 import org.chromium.components.offline_items_collection.OfflineItem.Progress;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.concurrent.Callable;
 /**
  * Mock class to DownloadNotificationService for testing purpose.
  */
 public class MockDownloadNotificationService extends DownloadNotificationService {
     private final List<Integer> mNotificationIds = new ArrayList<Integer>();
     private boolean mPaused = false;
-    private int mLastNotificationId = DEFAULT_NOTIFICATION_ID;
-
-    List<String> mResumedDownloads = new ArrayList<>();
-
+    private Context mContext;
+    private int mLastNotificationId;
+    void setContext(Context context) {
+        mContext = context;
+    }
+    @Override
+    public void stopForegroundInternal(boolean killNotification) {
+        if (!useForegroundService()) return;
+        if (killNotification) mNotificationIds.clear();
+    }
+    @Override
+    public void startForegroundInternal() {}
+    @Override
+    public void cancelOffTheRecordDownloads() {
+        super.cancelOffTheRecordDownloads();
+        mPaused = true;
+    }
+    @Override
+    boolean hasDownloadNotificationsInternal(int notificationIdToIgnore) {
+        if (!useForegroundService()) return false;
+        // Cancelling notifications here is synchronous, so we don't really have to worry about
+        // {@code notificationIdToIgnore}, but address it properly anyway.
+        if (mNotificationIds.size() == 1 && notificationIdToIgnore != -1) {
+            return !mNotificationIds.contains(notificationIdToIgnore);
+        }
+        return !mNotificationIds.isEmpty();
+    }
+    @Override
+    void updateSummaryIconInternal(
+            int removedNotificationId, Pair<Integer, Notification> addedNotification) {}
+    @Override
+    void cancelSummaryNotification() {}
     @Override
     void updateNotification(int id, Notification notification) {
         if (!mNotificationIds.contains(id)) {
@@ -35,77 +60,68 @@ public class MockDownloadNotificationService extends DownloadNotificationService
             mLastNotificationId = id;
         }
     }
-
     public boolean isPaused() {
         return mPaused;
     }
-
     public List<Integer> getNotificationIds() {
         return mNotificationIds;
     }
-
-    public int getLastNotificationId() {
-        return mLastNotificationId;
-    }
-
     @Override
     public void cancelNotification(int notificationId, ContentId id) {
         super.cancelNotification(notificationId, id);
         mNotificationIds.remove(Integer.valueOf(notificationId));
     }
-
+    public int getLastAddedNotificationId() {
+        return mLastNotificationId;
+    }
+    @Override
+    public Context getApplicationContext() {
+        return mContext == null ? super.getApplicationContext() : mContext;
+    }
     @Override
     public int notifyDownloadSuccessful(final ContentId id, final String filePath,
             final String fileName, final long systemDownloadId, final boolean isOffTheRecord,
             final boolean isSupportedMimeType, final boolean isOpenable, final Bitmap icon,
             final String originalUrl, final String referrer) {
-        return ThreadUtils.runOnUiThreadBlockingNoException(
-                () -> MockDownloadNotificationService.super.notifyDownloadSuccessful(id, filePath,
+        return ThreadUtils.runOnUiThreadBlockingNoException(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return MockDownloadNotificationService.super.notifyDownloadSuccessful(id, filePath,
                         fileName, systemDownloadId, isOffTheRecord, isSupportedMimeType, isOpenable,
-                        icon, originalUrl, referrer));
+                        icon, originalUrl, referrer);
+            }
+        });
     }
-
     @Override
     public void notifyDownloadProgress(final ContentId id, final String fileName,
             final Progress progress, final long bytesReceived, final long timeRemainingInMillis,
             final long startTime, final boolean isOffTheRecord,
             final boolean canDownloadWhileMetered, final boolean isTransient, final Bitmap icon) {
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> MockDownloadNotificationService.super.notifyDownloadProgress(id, fileName,
-                        progress,
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                MockDownloadNotificationService.super.notifyDownloadProgress(id, fileName, progress,
                         bytesReceived, timeRemainingInMillis, startTime, isOffTheRecord,
-                        canDownloadWhileMetered, isTransient, icon));
+                        canDownloadWhileMetered, isTransient, icon);
+            }
+        });
     }
-
-    @Override
-    void notifyDownloadPaused(ContentId id, String fileName, boolean isResumable,
-            boolean isAutoResumable, boolean isOffTheRecord, boolean isTransient, Bitmap icon,
-            boolean hasUserGesture) {
-        ThreadUtils.runOnUiThreadBlocking(
-                ()
-                        -> MockDownloadNotificationService.super.notifyDownloadPaused(id, fileName,
-                                isResumable, isAutoResumable, isOffTheRecord, isTransient, icon,
-                                hasUserGesture));
-    }
-
     @Override
     public void notifyDownloadFailed(final ContentId id, final String fileName, final Bitmap icon) {
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> MockDownloadNotificationService.super.notifyDownloadFailed(id, fileName,
-                        icon));
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                MockDownloadNotificationService.super.notifyDownloadFailed(id, fileName, icon);
+            }
+        });
     }
-
     @Override
-    public void notifyDownloadCanceled(final ContentId id, boolean hasUserGesture) {
-        ThreadUtils.runOnUiThreadBlocking(
-                ()
-                        -> MockDownloadNotificationService.super.notifyDownloadCanceled(
-                                id, hasUserGesture));
-    }
-
-    @Override
-    void resumeDownload(Intent intent) {
-        mResumedDownloads.add(IntentUtils.safeGetStringExtra(intent, EXTRA_DOWNLOAD_CONTENTID_ID));
+    public void notifyDownloadCanceled(final ContentId id) {
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                MockDownloadNotificationService.super.notifyDownloadCanceled(id);
+            }
+        });
     }
 }
-
