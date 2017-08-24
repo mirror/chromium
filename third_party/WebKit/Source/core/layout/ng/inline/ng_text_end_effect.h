5c4a808f53be11a15f3f3f9740695c00cad6a6f6
Koji Ishii <kojii@chromium.org>
2017-08-24 16:15:56 +0900

    WIP: hyphen
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I1bcf0c156f56c0f76246b533cd7e2ce90112aa5e

---
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_text_end_effect.h
@@ -0,0 +1,19 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NGTextEndEffect_h
+#define NGTextEndEffect_h
+
+namespace blink {
+
+enum class NGTextEndEffect {
+  kNone,
+  kHyphen,
+
+  // When adding new values, ensure NGPhysicalTextFragment has enough bits.
+};
+
+}  // namespace blink
+
+#endif  // NGTextEndEffect_h
