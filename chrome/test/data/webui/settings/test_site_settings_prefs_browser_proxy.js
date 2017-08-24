72b6852264a8d1123de89a6cce9e8ae3add2527f
Patti <patricialor@chromium.org>
2017-08-25 07:32:48 +1000

    MD Settings: Site Details now shows protected content setting on ChromeOS.
    The Protected Media Identifier / Protected Content setting is missing from Site
    Details. Add it.
    
    Bug: 656758, 647399
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I03078200f3533d6271c8fd76bcb53f2e3dbb9833

--- chrome/test/data/webui/settings/test_site_settings_prefs_browser_proxy.js
+++ chrome/test/data/webui/settings/test_site_settings_prefs_browser_proxy.js
@@ -12,8 +12,9 @@ var SiteSettingsPref;
 
 /**
  * An example empty pref.
- * TODO(patricialor): Use the values from settings.ContentSettingsTypes (see
- * site_settings/constants.js) as the keys for these instead.
+ * TODO(https://crbug.com/742706): Use the values from
+ * settings.ContentSettingsTypes (see site_settings/constants.js) as the keys
+ * for these instead.
  * @type {SiteSettingsPref}
  */
 var prefsEmpty = {
@@ -31,6 +32,7 @@ var prefsEmpty = {
     plugins: {},
     images: {},
     popups: {},
+    protectedContent: {},
     unsandboxed_plugins: {},
   },
   exceptions: {
@@ -47,6 +49,7 @@ var prefsEmpty = {
     plugins: [],
     images: [],
     popups: [],
+    protectedContent: [],
     unsandboxed_plugins: [],
   },
 };
@@ -262,6 +265,9 @@ class TestSiteSettingsPrefsBrowserProxy extends TestBrowserProxy {
     } else if (
         contentType == settings.ContentSettingsTypes.UNSANDBOXED_PLUGINS) {
       pref = this.prefs_.defaults.unsandboxed_plugins;
+    } else if (
+        contentType == settings.ContentSettingsTypes.PROTECTED_CONTENT) {
+      pref = this.prefs_.defaults.protectedContent;
     } else {
       console.log('getDefault received unknown category: ' + contentType);
     }
