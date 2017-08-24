2b278ff850cdbc885241f405bc7d916617bcdd39
galinap <galinap@google.com>
2017-08-24 11:45:46 +0100

    [Site Explore] Fix bug in Chrome Home where first pull up shows empty section
    Override onMeasure properly.
    We need to override this method because ViewPager does not properly handle wrap_content
    as its height measurement. This is because different pages might have different heights
    and the ViewPager doesn't know which one to choose.
    We know that this SiteExploreViewPager will hold a TabLayout and a TileGridLayout at all
    times, so we can calculate its height explicitly.
    
    Bug:
    Change-Id: I1263b98999a4f19e3647d0bf0e8b9dbb5f1b210e

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SiteExploreViewPager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SiteExploreViewPager.java
@@ -20,23 +20,37 @@ public class SiteExploreViewPager extends ViewPager {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // We need to override this method because ViewPager does not properly handle wrap_content
+        // as its height measurement. This is because different pages might have different heights
+        // and the ViewPager doesn't know which one to choose.
+        // We know that this SiteExploreViewPager will hold a TabLayout and a TileGridLayout at all
+        // times, so we can calculate its height explicitly.
+
+        // Need to call this to let the ViewPager initialize its children pages.
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        // Get the height of the tab layout.
         View tabLayout = getChildAt(0);
         tabLayout.measure(
                 widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
         int tabLayoutHeight = tabLayout.getMeasuredHeight();
 
-        int maxChildHeight = 0;
-        for (int i = 1; i < getChildCount(); i++) {
-            View child = getChildAt(i);
-            child.measure(
+        // Get the height of the first of its pages. We assume that all pages will have the same
+        // height.
+        View pageView = getChildAt(1);
+        int pageHeight = 0;
+        if (pageView != null) {
+            pageView.measure(
                     widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
-            int childHeight = child.getMeasuredHeight();
-            if (childHeight > maxChildHeight) maxChildHeight = childHeight;
+            pageHeight = pageView.getMeasuredHeight();
         }
 
+        // Set the height of the ViewPager to the sum of the height of the TabLayout and the height
+        // of one of its pages.
         heightMeasureSpec =
-                MeasureSpec.makeMeasureSpec(maxChildHeight + tabLayoutHeight, MeasureSpec.EXACTLY);
+                MeasureSpec.makeMeasureSpec(pageHeight + tabLayoutHeight, MeasureSpec.EXACTLY);
 
+        // Need to call this again to get the true height measurements.
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 }
