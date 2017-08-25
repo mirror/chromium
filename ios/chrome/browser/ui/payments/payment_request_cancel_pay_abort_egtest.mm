2f3218a920e2ca55b905ea22ebe577c9f43c4b4f
Mohamad Ahmadi <mahmadi@chromium.org>
2017-08-25 14:52:33 -0400

    [Payment Request] Fixes broken tests for ios_chrome_payments_egtests target
    Bug:758513
    Change-Id: If666eecc27e51f8ce5ed6cfa49b01a862eaf0e1f

--- ios/chrome/browser/ui/payments/payment_request_cancel_pay_abort_egtest.mm
+++ ios/chrome/browser/ui/payments/payment_request_cancel_pay_abort_egtest.mm
@@ -195,7 +195,8 @@ const char kNoShippingPage[] =
 
   // Tap the Confirm button.
   [[EarlGrey
-      selectElementWithMatcher:ButtonWithAccessibilityLabelId(IDS_ACCNAME_OK)]
+      selectElementWithMatcher:ButtonWithAccessibilityLabelId(
+                                   IDS_AUTOFILL_CARD_UNMASK_CONFIRM_BUTTON)]
       performAction:grey_tap()];
 
   // Confirm that the Payment Request UI is not showing.
@@ -222,7 +223,8 @@ const char kNoShippingPage[] =
 
   // Confirm the error.
   [[EarlGrey
-      selectElementWithMatcher:ButtonWithAccessibilityLabelId(IDS_ACCNAME_OK)]
+      selectElementWithMatcher:ButtonWithAccessibilityLabelId(
+                                   IDS_AUTOFILL_CARD_UNMASK_CONFIRM_BUTTON)]
       performAction:grey_tap()];
 
   // Confirm that the Payment Request UI is not showing.
