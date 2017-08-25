72437ed2666f44fb200456a54e9363838895dfd2
Sean Kau <skau@chromium.org>
2017-08-25 10:36:03 -0700

    Define the bulk enterprise printer policy.
    Enterprise printer configurations will now be distributed as a file
    encoded as a protocol buffer and a policy indicating what printers
    are allowed.
    
    Bug:758680
    Change-Id: If2d4063f6d5dea13a743bf9309db85b24ff4efe7

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -586,6 +586,15 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kNativePrinters,
     prefs::kRecommendedNativePrinters,
     base::Value::Type::LIST },
+  { key::kNativePrintersBulkAccessMode,
+    prefs::kRecommendedNativePrintersAccessMode,
+    base::Value::Type::INTEGER },
+  { key::kNativePrintersBulkBlacklist,
+    prefs::kRecommendedNativePrintersBlacklist,
+    base::Value::Type::LIST },
+  { key::kNativePrintersBulkWhitelist,
+    prefs::kRecommendedNativePrintersWhitelist,
+    base::Value::Type::LIST },
 #endif  // defined(OS_CHROMEOS)
 
 // Metrics reporting is controlled by a platform specific policy for ChromeOS
@@ -1052,6 +1061,8 @@ std::unique_ptr<ConfigurationPolicyHandlerList> BuildHandlerList(
       base::MakeUnique<ExternalDataPolicyHandler>(key::kDeviceWallpaperImage));
   handlers->AddHandler(
       base::MakeUnique<ExternalDataPolicyHandler>(key::kWallpaperImage));
+  handlers->AddHandler(base::MakeUnique<ExternalDataPolicyHandler>(
+      key::kNativePrintersBulkConfiguration));
   handlers->AddHandler(base::WrapUnique(new SimpleSchemaValidatingPolicyHandler(
       key::kSessionLocales, NULL, chrome_schema, SCHEMA_STRICT,
       SimpleSchemaValidatingPolicyHandler::RECOMMENDED_ALLOWED,
