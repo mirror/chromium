d1f060b57814239c401ce936baa35f69b045300f
Akari Asai <akariasai@google.com>
2017-08-25 11:41:22 +0900

    Converts basic-iteration.js to gtest.
    Converted WebKit/LayoutTests/editing/text-iterator/script-tests/basic-iteration.js
    to gtest and added the corresponding tests to WebKit/Source/core/editing/iterators/
    TextIteratorTest.cpp, since there are no good reasons using layout test for verifying
    TextIterator.
    
    Bug:753657
    Change-Id: I392b13da25829ab312d82eb7c0dcb6404a5de300

--- third_party/WebKit/Source/core/editing/iterators/TextIteratorTest.cpp
+++ third_party/WebKit/Source/core/editing/iterators/TextIteratorTest.cpp
@@ -941,4 +941,65 @@ TEST_F(TextIteratorTest, VisitsDisplayContentsChildren) {
   EXPECT_EQ("[Hello, ][text][iterator.]", Iterate<FlatTree>());
 }
 
+TEST_F(TextIteratorTest, BasicIterationEmptyContent) {
+  SetBodyContent("");
+  EXPECT_EQ("", Iterate<DOMTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationSingleCharacter) {
+  SetBodyContent("a");
+  EXPECT_EQ("[a]", Iterate<DOMTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationSingleDiv) {
+  SetBodyContent("<div>a</div>");
+  EXPECT_EQ("[a]", Iterate<DOMTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationMultipleDivs) {
+  SetBodyContent("<div>a</div><div>b</div>");
+  EXPECT_EQ("[a][\n][b]", Iterate<DOMTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationMultipleDivsWithStyle) {
+  static const char* input = "<div style='line-height: 18px; min-height: 436px; '>"
+                                "debugging this note"
+                              "</div>";
+  SetBodyContent(input);
+  EXPECT_EQ("[debugging this note]", Iterate<DOMTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationMultipleDivsWithChildren) {
+  static const char* input = "<div>Hello<div><br><span></span></div></div>";
+  SetBodyContent(input);
+  EXPECT_EQ("[Hello][\n][\n]", Iterate<DOMTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationOnChildrenWithStyle) {
+  SetBodyContent(
+      "<div style='left:22px'>"
+      "</div>"
+      "\n\t\t"
+      "<div style='left:26px'>"
+      "</div>"
+      "\n\n\t\t"
+      "<div>"
+        "\n\t\t\t"
+        "<div>"
+          "\n\t\t\t\t"
+          "<div>"
+            "\n\t\t\t\t\t"
+            "<div contenteditable style='line-height: 20px; min-height: 580px; '>"
+              "hey"
+            "</div>"
+            "\n\t\t\t\t"
+          "</div>"
+          "\n\t\t\t"
+        "</div>"
+        "\n\t\t"
+      "</div>"
+      "\n\t\n");
+  EXPECT_EQ("[hey]", Iterate<DOMTree>());
+}
+
 }  // namespace blink
