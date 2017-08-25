092d3672eeb9f3e502033db796971bb24d37cdcd
Makoto Shimazu <shimazu@chromium.org>
2017-08-25 16:55:11 +0900

    ServiceWorker: Fix metrics for script streaming
    This patch is to set FinishedReason::kSuccess if there is no installed script.
    UpdateStatus is added for ensuring to update both of |state_| and
    |finished_reason_|.
    Also, this patch adds recording CountReadResponseResult for recording errors of
    disk cache which used to be recorded in ServiceWorkerReadFromCacheJob.
    
    Bug: 754139, 683037
    Change-Id: I9e3141892aca9f88d2ed1a56b27976dc3a985e57

--- content/browser/service_worker/service_worker_installed_scripts_sender.h
+++ content/browser/service_worker/service_worker_installed_scripts_sender.h
@@ -71,9 +71,11 @@ class CONTENT_EXPORT ServiceWorkerInstalledScriptsSender {
       uint64_t meta_data_size);
   void OnHttpInfoRead(scoped_refptr<HttpResponseInfoIOBuffer> http_info);
   void OnFinishSendingScript();
-  void OnAbortSendingScript(FinishedReason status);
+  void OnAbortSendingScript(FinishedReason reason);
 
   const GURL& CurrentSendingURL();
+
+  void UpdateState(State state, FinishedReason reason);
 
   ServiceWorkerVersion* owner_;
   const GURL main_script_url_;
