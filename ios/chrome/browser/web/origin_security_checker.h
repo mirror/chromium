5cbbd7b397e1b713ba4b7b647ddee47697a087ee
Tomasz Garbus <tgarbus@chromium.org>
2017-08-25 13:53:18 +0200

    Implemented IsContextSecure function in credential_manager_util.mm
    This CL implements IsContextSecure function, which must be called
    by CredentialManager before handling any callback it receives from
    JavaScript. Changes:
    1. Moved ios/chrome/browser/passwords/origin_security_checker.* to
    ios/chrome/browser/web in order to avoid code duplication.
    2. Implemented IsContextSecure function, which checks if context is
    secure for WebState's content. It uses previously mentioned
    OriginSecurityChecker and IOSSecurityStateTabHelper.
    3. Created CredentialManagerTest, a unittest file for
    CredentialManager. It uses WebState, and when CredentialManager
    methods are implemented, they will require WebState too. In order to
    avoid code duplication for setting up WebState with secure context,
    IsContextSecure is tested in CredentialManagerTest.
    
    Bug: 435048
    Change-Id: If1c8e6fbedeef250535999270826ba34389c787b

---
+++ ios/chrome/browser/web/origin_security_checker.h
@@ -0,0 +1,23 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PAYMENTS_ORIGIN_SECURITY_CHECKER_H_
+#define IOS_CHROME_BROWSER_PAYMENTS_ORIGIN_SECURITY_CHECKER_H_
+
+#include "base/macros.h"
+#include "components/security_state/core/security_state.h"
+
+class GURL;
+
+namespace web {
+
+// Returns true for a valid |url| from a secure context. This check is a
+// superset of the web::IsOriginSecure() and url.is_valid() by making
+// sure the URL does not have a data URI scheme. This may eventually get more
+// complicated and require looking at whether the origin is opaque, etc.
+bool IsContextSecure(const GURL& url);
+
+}  // namespace web
+
+#endif  // IOS_CHROME_BROWSER_PAYMENTS_ORIGIN_SECURITY_CHECKER_H_
