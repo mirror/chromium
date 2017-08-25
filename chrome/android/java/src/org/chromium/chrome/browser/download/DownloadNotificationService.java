d4341c02b1be87966db1f74ec2b34bb8a345881d
Joy Ming <jming@chromium.org>
2017-08-25 16:31:50 -0700

    Reimplement DownloadResumptionScheduler.
    This is part of the larger refactor to make downloads a foreground
    service on all versions of Android. During the refactor, the logic for
    DownloadResumptionScheduler was temporarily removed. This CL
    reimplements DownloadResumptionScheduler by scheduling a job when the
    DownloadForegroundService has been destroyed and updating the
    resumption attempts when the resumption attempt has been completed and
    when there is user interaction.
    
    Bug:755771,758688,747563
    Change-Id: I2e312295a5df4a7dfab5bfb3a447cb14f0b452a6

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadNotificationService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadNotificationService.java
@@ -4,6 +4,8 @@
 
 package org.chromium.chrome.browser.download;
 
+import static org.chromium.chrome.browser.download.DownloadBroadcastManager.getServiceDelegate;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
@@ -19,14 +21,18 @@ import android.graphics.drawable.shapes.OvalShape;
 import android.text.TextUtils;
 
 import org.chromium.base.ApiCompatibilityUtils;
+import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.VisibleForTesting;
+import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.notifications.NotificationUmaTracker;
 import org.chromium.chrome.browser.notifications.channels.ChannelDefinitions;
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.offline_items_collection.ContentId;
 import org.chromium.components.offline_items_collection.LegacyHelpers;
 import org.chromium.components.offline_items_collection.OfflineItem.Progress;
+import org.chromium.content.browser.BrowserStartupController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -60,8 +66,10 @@ public class DownloadNotificationService {
             "Chrome.NotificationBundleIconIdExtra";
     /** Notification Id starting value, to avoid conflicts from IDs used in prior versions. */
     private static final int STARTING_NOTIFICATION_ID = 1000000;
-
     private static final String KEY_NEXT_DOWNLOAD_NOTIFICATION_ID = "NextDownloadNotificationId";
+
+    private static final int MAX_RESUMPTION_ATTEMPT_LEFT = 5;
+    private static final String KEY_AUTO_RESUMPTION_ATTEMPT_LEFT = "ResumptionAttemptLeft";
 
     @VisibleForTesting
     final List<ContentId> mDownloadsInProgress = new ArrayList<ContentId>();
@@ -487,10 +495,21 @@ public class DownloadNotificationService {
      * already in progress, do nothing.
      */
     void resumeAllPendingDownloads() {
+        Context context = ContextUtils.getApplicationContext();
+
+        // Limit the number of auto resumption attempts in case Chrome falls into a vicious cycle.
+        DownloadResumptionScheduler.getDownloadResumptionScheduler(context).cancelTask();
+        int numAutoResumptionAtemptLeft = getResumptionAttemptLeft();
+        if (numAutoResumptionAtemptLeft > 0) {
+            numAutoResumptionAtemptLeft--;
+            updateResumptionAttemptLeft(numAutoResumptionAtemptLeft);
+        }
+
+        // Go through and check which downloads to resume.
         List<DownloadSharedPreferenceEntry> entries = mDownloadSharedPreferenceHelper.getEntries();
         for (int i = 0; i < entries.size(); ++i) {
             DownloadSharedPreferenceEntry entry = entries.get(i);
-            if (!canResumeDownload(ContextUtils.getApplicationContext(), entry)) continue;
+            if (!canResumeDownload(context, entry)) continue;
             if (mDownloadsInProgress.contains(entry.id)) continue;
 
             Intent intent = new Intent();
@@ -524,5 +543,117 @@ public class DownloadNotificationService {
         editor.putInt(KEY_NEXT_DOWNLOAD_NOTIFICATION_ID, mNextNotificationId);
         editor.apply();
         return notificationId;
+    }
+
+    /**
+     * Helper method to update the remaining number of background resumption attempts left.
+     */
+    static void updateResumptionAttemptLeft(int numAutoResumptionAttemptLeft) {
+        SharedPreferences sharedPrefs = ContextUtils.getAppSharedPreferences();
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.putInt(KEY_AUTO_RESUMPTION_ATTEMPT_LEFT, numAutoResumptionAttemptLeft);
+        editor.apply();
+    }
+
+    /**
+     * Helper method to get the remaining number of background resumption attempts left.
+     */
+    static int getResumptionAttemptLeft() {
+        SharedPreferences sharedPrefs = ContextUtils.getAppSharedPreferences();
+        return sharedPrefs.getInt(KEY_AUTO_RESUMPTION_ATTEMPT_LEFT, MAX_RESUMPTION_ATTEMPT_LEFT);
+    }
+
+    /**
+     * Helper method to clear the remaining number of background resumption attempts left.
+     */
+    static void clearResumptionAttemptLeft() {
+        SharedPreferences sharedPrefs = ContextUtils.getAppSharedPreferences();
+        SharedPreferences.Editor editor = sharedPrefs.edit();
+        editor.remove(KEY_AUTO_RESUMPTION_ATTEMPT_LEFT);
+        editor.apply();
+    }
+
+    /**
+     * Called when the foreground service has been restarted.
+     */
+    void handleForegroundServiceRestarted() {
+        updateNotificationsForShutdown();
+        resumeAllPendingDownloads();
+    }
+
+    /**
+     * Called when foreground service is unexpectedly destroyed.
+     */
+    void handleForegroundServiceDestroyed() {
+        updateNotificationsForShutdown();
+        rescheduleDownloads();
+    }
+
+    private void updateNotificationsForShutdown() {
+        cancelOffTheRecordDownloads();
+        List<DownloadSharedPreferenceEntry> entries = mDownloadSharedPreferenceHelper.getEntries();
+        for (DownloadSharedPreferenceEntry entry : entries) {
+            if (entry.isOffTheRecord) continue;
+            // Move all regular downloads to pending.  Don't propagate the pause because
+            // if native is still working and it triggers an update, then the service will be
+            // restarted.
+            notifyDownloadPaused(entry.id, entry.fileName, !entry.isOffTheRecord, true,
+                    entry.isOffTheRecord, entry.isTransient, null, false);
+        }
+    }
+
+    private void cancelOffTheRecordDownloads() {
+        boolean cancelActualDownload =
+                BrowserStartupController.get(LibraryProcessType.PROCESS_BROWSER)
+                        .isStartupSuccessfullyCompleted()
+                && Profile.getLastUsedProfile().hasOffTheRecordProfile();
+
+        List<DownloadSharedPreferenceEntry> entries = mDownloadSharedPreferenceHelper.getEntries();
+        List<DownloadSharedPreferenceEntry> copies =
+                new ArrayList<DownloadSharedPreferenceEntry>(entries);
+        for (DownloadSharedPreferenceEntry entry : copies) {
+            if (!entry.isOffTheRecord) continue;
+            ContentId id = entry.id;
+            notifyDownloadCanceled(id, false);
+            if (cancelActualDownload) {
+                DownloadServiceDelegate delegate = getServiceDelegate(id);
+                delegate.cancelDownload(id, true);
+                delegate.destroyServiceDelegate();
+            }
+        }
+    }
+
+    private void rescheduleDownloads() {
+        Context context = ContextUtils.getApplicationContext();
+
+        // Cancel any existing task.  If we have any downloads to resume we'll reschedule another.
+        DownloadResumptionScheduler.getDownloadResumptionScheduler(context).cancelTask();
+        List<DownloadSharedPreferenceEntry> entries = mDownloadSharedPreferenceHelper.getEntries();
+        if (entries.isEmpty()) return;
+
+        boolean scheduleAutoResumption = false;
+        boolean allowMeteredConnection = false;
+        for (int i = 0; i < entries.size(); ++i) {
+            DownloadSharedPreferenceEntry entry = entries.get(i);
+            if (entry.isAutoResumable) {
+                scheduleAutoResumption = true;
+                if (entry.canDownloadWhileMetered) {
+                    allowMeteredConnection = true;
+                    break;
+                }
+            }
+        }
+
+        if (scheduleAutoResumption && getResumptionAttemptLeft() > 0) {
+            DownloadResumptionScheduler.getDownloadResumptionScheduler(context).schedule(
+                    allowMeteredConnection);
+        }
+    }
+
+    void handleForegroundServiceTaskRemoved() {
+        // If we've lost all Activities, cancel the off the record downloads.
+        if (ApplicationStatus.isEveryActivityDestroyed()) {
+            cancelOffTheRecordDownloads();
+        }
     }
 }
