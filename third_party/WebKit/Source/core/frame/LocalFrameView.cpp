c785415f0790ce374dbeab0fb03b41a137509705
Philip Rogers <pdr@chromium.org>
2017-08-25 18:50:32 -0700

    Set DidScroll callback (WebScrollLayerClient) once in PAC
    The DidScroll callback was stored once per ScrollPaintPropertyNode but
    is static for all of PaintArtifactCompositor (PAC). This patch moves
    the WebScrollLayerClient to PaintArtifactCompositor and removes it from
    ScrollPaintPropertyNode. The WebScrollLayerClient is stored on the Page
    which owns PaintArtifactCompositor, so there are no lifetime issues. This
    also removes one more difference between blink and cc property trees.
    
    Bug: 758360
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I3bf5700b360f1ff7983fc4923f1a39a759b6646c

--- third_party/WebKit/Source/core/frame/LocalFrameView.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrameView.cpp
@@ -3387,13 +3387,15 @@ void LocalFrameView::PushPaintArtifactToCompositor(
   TRACE_EVENT0("blink", "LocalFrameView::pushPaintArtifactToCompositor");
 
   DCHECK(RuntimeEnabledFeatures::SlimmingPaintV2Enabled());
+  DCHECK(frame_->GetSettings()->GetAcceleratedCompositingEnabled());
 
   Page* page = GetFrame().GetPage();
   if (!page)
     return;
 
   if (!paint_artifact_compositor_) {
-    paint_artifact_compositor_ = PaintArtifactCompositor::Create();
+    paint_artifact_compositor_ =
+        PaintArtifactCompositor::Create(*page->GetScrollingCoordinator());
     page->GetChromeClient().AttachRootLayer(
         paint_artifact_compositor_->GetWebLayer(), &GetFrame());
   }
