3539f05b7f7fcfd9bc37af9689f7eb224a537418
Thanh Pham <thanhph@chromium.org>
2017-08-27 13:20:48 -0400

    Make DispatchKeyEventPostIME() asynchronous.
    This change:
    - Uses the async version of DispatchKeyEventPostIME() in 
    InputMethodChromeOS.
    - Adds ui.mojom.TextInputClient.DispatchKeyEventPostIME().
    
    This is because DispatchKeyEventPostIME() isn't really *Post* IME, and
    calls to it are sometimes intervened with the logic in
    InputMethodChromeOS to provide KeyDown and KeyUp events in addition to
    KeyPress events to textfields.
    
    This CL calls ui.mojom.TextInputClient.DispatchKeyEventPostIME() in
    these cases and continues the logic after the dispatch finishes in the
    client side.
    
    Bug:723792
    Change-Id: I70b3048f836cb195c8676dab1c40bcf28dd0c773

--- ui/base/ime/input_method_chromeos.h
+++ ui/base/ime/input_method_chromeos.h
@@ -48,12 +48,18 @@ class UI_BASE_IME_EXPORT InputMethodChromeOS : public InputMethodBase {
                               CompositionText* out_composition) const;
 
   // Process a key returned from the input method.
-  virtual ui::EventDispatchDetails ProcessKeyEventPostIME(ui::KeyEvent* event,
-                                                          bool handled)
-      WARN_UNUSED_RESULT;
+  virtual ui::EventDispatchDetails ProcessKeyEventPostIME(
+      ui::KeyEvent* event,
+      std::unique_ptr<AckCallback> ack_callback,
+      bool skip_process_filtered,
+      bool handled) WARN_UNUSED_RESULT;
 
   // Resets context and abandon all pending results and key events.
   void ResetContext();
+
+  ui::EventDispatchDetails DispatchKeyEventPostIME(
+      ui::KeyEvent* event,
+      std::unique_ptr<AckCallback> ack_callback) const WARN_UNUSED_RESULT;
 
  private:
   class PendingKeyEvent;
@@ -75,12 +81,26 @@ class UI_BASE_IME_EXPORT InputMethodChromeOS : public InputMethodBase {
   // A VKEY_PROCESSKEY may be dispatched to the EventTargets.
   // It returns the result of whether the event has been stopped propagation
   // when dispatching post IME.
-  ui::EventDispatchDetails ProcessFilteredKeyPressEvent(ui::KeyEvent* event)
-      WARN_UNUSED_RESULT;
+  ui::EventDispatchDetails ProcessFilteredKeyPressEvent(
+      ui::KeyEvent* event,
+      std::unique_ptr<AckCallback> ack_callback) WARN_UNUSED_RESULT;
+
+  void PostProcessFilteredKeyPressEvent(
+      ui::KeyEvent* event,
+      TextInputClient* prev_client,
+      std::unique_ptr<AckCallback> ack_callback,
+      bool stopped_propagation);
 
   // Processes a key event that was not filtered by the input method.
-  ui::EventDispatchDetails ProcessUnfilteredKeyPressEvent(ui::KeyEvent* event)
-      WARN_UNUSED_RESULT;
+  ui::EventDispatchDetails ProcessUnfilteredKeyPressEvent(
+      ui::KeyEvent* event,
+      std::unique_ptr<AckCallback> ack_callback) WARN_UNUSED_RESULT;
+
+  void PostProcessUnfilteredKeyPressEvent(
+      ui::KeyEvent* event,
+      TextInputClient* prev_client,
+      std::unique_ptr<AckCallback> ack_callback,
+      bool stopped_propagation);
 
   // Sends input method result caused by the given key event to the focused text
   // input client.
