7e7ff416de5f5148dd5f8d7f737cd26932db5275
Ke He <ke.he@intel.com>
2017-08-28 15:43:38 +0800

    Add hid.mojom, re-define the device::HidDeviceInfo.
    This is a prepare CL for the mojofication in next step.
    In this CL we change:
    
    1) Add hid.mojom in which we define the device::mojom::HidDeviceInfo.
    
    2) Re-define the device::HidDeviceInfo which wraps the
       device::mojom::HidDeviceInfo. (Because platform_id is in different type in
       different platform, we cannot totally replace the old native HidDeviceInfo
       struct by device::mojom::HidDeviceInfo). The device::HidDeviceInfo will be
       internal and won't exposed to client(after mojofication).
       Remove the unnecessary HidDeviceInfoLinux.
    
    3) Let all clients of HidService use device::mojom::HidDeviceInfo. Remove
       the scoped_refptr<HidDeviceInfo> in client side, client should own
       the device::mojom::HidDeviceInfo instead of sharing the same one.
    
    4) Remove the HidService::GetDeviceInfo() which is only used by
       HidDeviceManager. HidDeviceManager should keep the DeviceInfo in its map.
    
    5) Change the tests accordingly, make sure all the tests pass.
    
    TODO: 1) move client library files into device/hid/public/cpp
          2) define mojo interfaces, then do mojofication.
          3) host HidService by DeviceService instead of DeviceClient.
          4) make sure all test cases pass after mojofication.
    
    BUG=728223
    
    Change-Id: I83a0d96ae4c1a8c3b4919c2b126399a10515601f

--- extensions/browser/api/hid/hid_api.h
+++ extensions/browser/api/hid/hid_api.h
@@ -12,6 +12,7 @@
 
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "device/hid/public/interfaces/hid.mojom.h"
 #include "extensions/browser/api/api_resource_manager.h"
 #include "extensions/browser/api/hid/hid_connection_resource.h"
 #include "extensions/browser/api/hid/hid_device_manager.h"
@@ -20,7 +21,6 @@
 
 namespace device {
 class HidConnection;
-class HidDeviceInfo;
 }  // namespace device
 
 namespace net {
@@ -62,7 +62,7 @@ class HidGetUserSelectedDevicesFunction : public UIThreadExtensionFunction {
   ResponseAction Run() override;
 
   void OnDevicesChosen(
-      const std::vector<scoped_refptr<device::HidDeviceInfo>>& devices);
+      const std::vector<device::mojom::HidDeviceInfoPtr>& devices);
 
   std::unique_ptr<DevicePermissionsPrompt> prompt_;
 
