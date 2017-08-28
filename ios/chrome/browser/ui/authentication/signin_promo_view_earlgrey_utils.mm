8487aa9be5f7d7cddbda6ef3251f03e596bde3ea
Jérôme Lebel <jlebel@chromium.org>
2017-08-28 20:30:46 +0200

    Adding UI tests for sign-in promo in bookmarks
    Updating the current test for the sign-in promo to match the cold state
    sign-in promo, and to check that only the primary button is visible.
    
    Adding a warm state sign-in promo test, to check if both buttons are
    visible, and check the behavior of the secondary button.
    
    Adding a test to check that sign-in promo is not shown after being
    displayed 19 times.
    
    Bug: 661794
    Change-Id: Idd2870ca0b1ba618398dead05555b4613ec6986e

---
+++ ios/chrome/browser/ui/authentication/signin_promo_view_earlgrey_utils.mm
@@ -0,0 +1,60 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/authentication/signin_promo_view_earlgrey_utils.h"
+
+#import <EarlGrey/EarlGrey.h>
+
+#import "ios/chrome/test/earl_grey/chrome_matchers.h"
+
+using chrome_test_util::PrimarySignInButton;
+using chrome_test_util::SecondarySignInButton;
+
+@implementation SignPromoViewEarlgreyUtils
+
+// Checks that the sign-in promo view is visible using the right mode.
++ (void)checkSigninPromoVisibleWithMode:(SigninPromoViewMode)mode {
+  [[EarlGrey
+      selectElementWithMatcher:grey_allOf(
+                                   grey_accessibilityID(kSigninPromoViewId),
+                                   grey_sufficientlyVisible(), nil)]
+      assertWithMatcher:grey_notNil()];
+  [[EarlGrey
+      selectElementWithMatcher:grey_allOf(PrimarySignInButton(),
+                                          grey_sufficientlyVisible(), nil)]
+      assertWithMatcher:grey_notNil()];
+  switch (mode) {
+    case SigninPromoViewModeColdState:
+      [[EarlGrey
+          selectElementWithMatcher:grey_allOf(SecondarySignInButton(),
+                                              grey_sufficientlyVisible(), nil)]
+          assertWithMatcher:grey_nil()];
+      break;
+    case SigninPromoViewModeWarmState:
+      [[EarlGrey
+          selectElementWithMatcher:grey_allOf(SecondarySignInButton(),
+                                              grey_sufficientlyVisible(), nil)]
+          assertWithMatcher:grey_notNil()];
+      break;
+  }
+}
+
+// Checks that the sign-in promo view is not visible.
++ (void)checkSigninPromoNotVisible {
+  [[EarlGrey
+      selectElementWithMatcher:grey_allOf(
+                                   grey_accessibilityID(kSigninPromoViewId),
+                                   grey_sufficientlyVisible(), nil)]
+      assertWithMatcher:grey_nil()];
+  [[EarlGrey
+      selectElementWithMatcher:grey_allOf(PrimarySignInButton(),
+                                          grey_sufficientlyVisible(), nil)]
+      assertWithMatcher:grey_nil()];
+  [[EarlGrey
+      selectElementWithMatcher:grey_allOf(SecondarySignInButton(),
+                                          grey_sufficientlyVisible(), nil)]
+      assertWithMatcher:grey_nil()];
+}
+
+@end
