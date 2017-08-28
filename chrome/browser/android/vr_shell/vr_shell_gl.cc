c9e308763166e241ad2c5a1d97b39bc45a998b7f
Michael Thiessen <mthiesse@chromium.org>
2017-08-28 11:31:03 -0400

    VR: Introduce a timeout for WebVR to produce a frame before dropping back to browsing.
    Bug: 713730
    Change-Id: Ic65bb52b2b02fd8e06685a286e9aa2b7744a1bea

--- chrome/browser/android/vr_shell/vr_shell_gl.cc
+++ chrome/browser/android/vr_shell/vr_shell_gl.cc
@@ -87,6 +87,9 @@ static constexpr float kMinAppButtonGestureAngleRad = 0.25;
 static constexpr base::TimeDelta kWebVRFenceCheckTimeout =
     base::TimeDelta::FromMicroseconds(2000);
 
+static constexpr int kWebVrInitialFrameTimeoutSeconds = 5;
+static constexpr int kWebVrSequentialFrameTimeoutSeconds = 1;
+
 // Provides the direction the head is looking towards as a 3x1 unit vector.
 gfx::Vector3dF GetForwardVector(const gfx::Transform& head_pose) {
   // Same as multiplying the inverse of the rotation component of the matrix by
@@ -368,6 +371,7 @@ void VrShellGl::ConnectPresentingService(
   closePresentationBindings();
   submit_client_.Bind(std::move(submit_client_info));
   binding_.Bind(std::move(request));
+  ScheduleWebVrFrameTimeout();
 }
 
 void VrShellGl::OnContentFrameAvailable() {
@@ -393,6 +397,30 @@ void VrShellGl::OnWebVRFrameAvailable() {
   browser_->OnWebVrFrameAvailable();
 
   DrawFrame(frame_index);
+  if (web_vr_mode_) {
+    ++webvr_frames_received_;
+    ScheduleWebVrFrameTimeout();
+  } else {
+    webvr_frame_timeout_.Cancel();
+  }
+}
+
+void VrShellGl::ScheduleWebVrFrameTimeout() {
+  webvr_frame_timeout_.Reset(
+      base::Bind(&VrShellGl::OnWebVrFrameTimedOut, base::Unretained(this)));
+  // Allow a webVR page to show a splash screen (up to 60 frames worth) before
+  // expecting the page to draw frames consistently.
+  if (webvr_frames_received_ > 0 && webvr_frames_received_ < 60)
+    return;
+  int delay = (webvr_frames_received_ == 0)
+                  ? kWebVrInitialFrameTimeoutSeconds
+                  : kWebVrSequentialFrameTimeoutSeconds;
+  task_runner_->PostDelayedTask(FROM_HERE, webvr_frame_timeout_.callback(),
+                                base::TimeDelta::FromSeconds(delay));
+}
+
+void VrShellGl::OnWebVrFrameTimedOut() {
+  browser_->OnWebVrTimedOut();
 }
 
 void VrShellGl::GvrInit(gvr_context* gvr_api) {
@@ -1050,26 +1078,36 @@ void VrShellGl::OnPause() {
   vsync_helper_.CancelVSyncRequest();
   controller_->OnPause();
   gvr_api_->PauseTracking();
+  webvr_frame_timeout_.Cancel();
 }
 
 void VrShellGl::OnResume() {
   gvr_api_->RefreshViewerProfile();
   gvr_api_->ResumeTracking();
   controller_->OnResume();
-  if (ready_to_draw_) {
-    vsync_helper_.CancelVSyncRequest();
-    OnVSync(base::TimeTicks::Now());
-  }
+  if (!ready_to_draw_)
+    return;
+  vsync_helper_.CancelVSyncRequest();
+  OnVSync(base::TimeTicks::Now());
+  if (web_vr_mode_ && submit_client_)
+    ScheduleWebVrFrameTimeout();
 }
 
 void VrShellGl::SetWebVrMode(bool enabled) {
   web_vr_mode_ = enabled;
 
+  if (web_vr_mode_ && submit_client_) {
+    ScheduleWebVrFrameTimeout();
+  } else {
+    webvr_frame_timeout_.Cancel();
+    webvr_frames_received_ = 0;
+  }
+
   if (cardboard_) {
     browser_->ToggleCardboardGamepad(enabled);
   }
 
-  if (!enabled) {
+  if (!web_vr_mode_) {
     closePresentationBindings();
   }
 }
@@ -1235,6 +1273,7 @@ void VrShellGl::CreateVRDisplayInfo(
 }
 
 void VrShellGl::closePresentationBindings() {
+  webvr_frame_timeout_.Cancel();
   submit_client_.reset();
   if (!callback_.is_null()) {
     // When this Presentation provider is going away we have to respond to
