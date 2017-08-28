3af0791f516571674e27dc829fbbeca4fbf7bf9c
Akari Asai <akariasai@google.com>
2017-08-28 17:59:34 +0900

    Converts basic-iteration.js to gtest.
    Converted WebKit/LayoutTests/editing/text-iterator/script-tests/basic-iteration.js
    to gtest and added the corresponding tests to WebKit/Source/core/editing/iterators/
    TextIteratorTest.cpp, since there are no good reasons using layout test for verifying
    TextIterator.
    
    Bug:753657
    Change-Id: I392b13da25829ab312d82eb7c0dcb6404a5de300

--- third_party/WebKit/Source/core/editing/iterators/TextIteratorTest.cpp
+++ third_party/WebKit/Source/core/editing/iterators/TextIteratorTest.cpp
@@ -28,10 +28,11 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "core/editing/iterators/TextIterator.h"
-
+#include "core/dom/Document.h"
 #include "core/editing/EditingTestBase.h"
+#include "core/editing/iterators/TextIterator.h"
 #include "core/frame/LocalFrameView.h"
+#include "core/html/TextControlElement.h"
 
 namespace blink {
 
@@ -939,6 +940,92 @@ TEST_F(TextIteratorTest, VisitsDisplayContentsChildren) {
 
   EXPECT_EQ("[Hello, ][text][iterator.]", Iterate<DOMTree>());
   EXPECT_EQ("[Hello, ][text][iterator.]", Iterate<FlatTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationEmptyContent) {
+  SetBodyContent("");
+  EXPECT_EQ("", Iterate<DOMTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationSingleCharacter) {
+  SetBodyContent("a");
+  EXPECT_EQ("[a]", Iterate<DOMTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationSingleDiv) {
+  SetBodyContent("<div>a</div>");
+  EXPECT_EQ("[a]", Iterate<DOMTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationMultipleDivs) {
+  SetBodyContent("<div>a</div><div>b</div>");
+  EXPECT_EQ("[a][\n][b]", Iterate<DOMTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationMultipleDivsWithStyle) {
+  static const char* input =
+      "<div style='line-height: 18px; min-height: 436px; '>"
+        "debugging this note"
+      "</div>";
+  SetBodyContent(input);
+  EXPECT_EQ("[debugging this note]", Iterate<DOMTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationMultipleDivsWithChildren) {
+  static const char* input = "<div>Hello<div><br><span></span></div></div>";
+  SetBodyContent(input);
+  EXPECT_EQ("[Hello][\n][\n]", Iterate<DOMTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationOnChildrenWithStyle) {
+  SetBodyContent(
+      "<div style='left:22px'>"
+      "</div>"
+      "\t\t\n"
+      "<div style='left:26px'>"
+      "</div>"
+      "\t\t\n\n"
+      "<div>"
+        "\t\t\t\n"
+        "<div>"
+          "\t\t\t\t\n"
+          "<div>"
+            "\t\t\t\t\t\n"
+            "<div contenteditable style='line-height: 20px; min-height: 580px; '>"
+              "hey"
+            "</div>"
+            "\t\t\t\t\n"
+          "</div>"
+          "\t\t\t\n"
+        "</div>"
+        "\t\t\n"
+      "</div>"
+      "\n\t\n");
+  EXPECT_EQ("[hey]", Iterate<DOMTree>());
+}
+
+TEST_F(TextIteratorTest, BasicIterationInput) {
+  static const char* input = "<input id='a' value='b'>";
+  SetBodyContent(input);
+  TextControlElement* input_element =
+      (TextControlElement*)GetDocument().getElementById("a");
+  const Element* inner_editor = input_element->InnerEditorElement();
+  const Position start = Position::FirstPositionInNode(*inner_editor);
+  const Position end = Position::LastPositionInNode(*inner_editor);
+  EXPECT_EQ("[b]", IteratePartial<DOMTree>(start, end));
+}
+
+TEST_F(TextIteratorTest, BasicIterationInputiWithBr) {
+  static const char* input = "<input id='a' value='b'>";
+  SetBodyContent(input);
+  TextControlElement* input_element =
+      (TextControlElement*)GetDocument().getElementById("a");
+  Element* inner_editor = input_element->InnerEditorElement();
+  Element* br = GetDocument().createElement("br");
+  inner_editor->AppendChild(br);
+  const Position start = Position::FirstPositionInNode(*inner_editor);
+  const Position end = Position::LastPositionInNode(*inner_editor);
+  EXPECT_EQ("[b]", IteratePartial<DOMTree>(start, end));
 }
 
 }  // namespace blink
