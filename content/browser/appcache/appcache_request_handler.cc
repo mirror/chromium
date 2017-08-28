ac2a3b09fb0a7b1ef530ff06d9ba20aacba47a28
Anantanarayanan Iyengar <ananta@chromium.org>
2017-08-28 12:41:31 -0700

    Ensure that redirected subresource requests in the AppCache URLLoader can be served out of the cache.
    Subresource requests handled by the AppCacheSubresourceURLFactory can be redirected. The redirected
    URL could potentially be served out of the AppCache. This patch provides support for the same.
    
    Changes as below:
    1. AppCacheURLLoaderJob now maintains a weak pointer to the AppCacheSubresourceURLFactory instance. This
       is passed to the AppCacheRequestHandler::MaybeCreateSubresourceLoader function and from there to the
       job.
    
    2. When the AppCacheURLLoaderJob receives a redirect notification via a call to URLLoaderClient::OnReceiveRedirect(),
       we remember the redirect information. When the caller invokes FollowRedirect(), we try to load the request out
       of the AppCache via a call to the AppCacheRequestHandler::MaybeCreateSubresourceLoader() function.
    
    3. The AppCacheSubresourceURLFactory class supports weak pointers and provides a method Restart() which is called by
       the AppCacheURLLoaderJob when its FollowRedirect() method is called.
    
    
    BUG=715632
    TEST=Covered by content browser test AppCacheNetworkServiceBrowserTest.VerifyRedirectedSubresourceAppCacheLoad
    
    Change-Id: Id73749254d7c81d0c0fded65fb6185b7e913f76f

--- content/browser/appcache/appcache_request_handler.cc
+++ content/browser/appcache/appcache_request_handler.cc
@@ -248,9 +248,11 @@ void AppCacheRequestHandler::MaybeCompleteCrossSiteTransferInOldProcess(
 
 AppCacheJob* AppCacheRequestHandler::MaybeCreateSubresourceLoader(
     std::unique_ptr<SubresourceLoadInfo> subresource_load_info,
-    URLLoaderFactoryGetter* loader_factory_getter) {
+    URLLoaderFactoryGetter* loader_factory_getter,
+    AppCacheSubresourceURLFactory* subresource_factory) {
   DCHECK(!is_main_resource());
   DCHECK(base::FeatureList::IsEnabled(features::kNetworkService));
+  job_ = nullptr;
 
   subresource_load_info_ = std::move(subresource_load_info);
   network_url_loader_factory_getter_ = loader_factory_getter;
@@ -261,8 +263,8 @@ AppCacheJob* AppCacheRequestHandler::MaybeCreateSubresourceLoader(
 
   AppCacheURLLoaderJob* loader_job = job->AsURLLoaderJob();
   // The job takes ownership of the handler.
-  loader_job->set_request_handler(
-      std::unique_ptr<AppCacheRequestHandler>(this));
+  loader_job->SetRequestHandlerAndFactory(
+      std::unique_ptr<AppCacheRequestHandler>(this), subresource_factory);
   return job;
 }
 
--- content/browser/appcache/appcache_subresource_url_factory.h
+++ content/browser/appcache/appcache_subresource_url_factory.h
@@ -17,8 +17,10 @@ namespace content {
 
 class AppCacheHost;
 class AppCacheJob;
+class AppCacheRequestHandler;
 class AppCacheServiceImpl;
 class URLLoaderFactoryGetter;
+struct SubresourceLoadInfo;
 
 // Implements the URLLoaderFactory mojom for AppCache subresource requests.
 class CONTENT_EXPORT AppCacheSubresourceURLFactory
@@ -48,6 +50,15 @@ class CONTENT_EXPORT AppCacheSubresourceURLFactory
                                 traffic_annotation) override;
   void Clone(mojom::URLLoaderFactoryRequest request) override;
 
+  base::WeakPtr<AppCacheSubresourceURLFactory> GetWeakPtr();
+
+  // Called bythe AppCacheURLLoaderJob to restart the request during a
+  // redirect. We attempt to serve the request out of the AppCache and if
+  // that fails we go to the network.
+  void Restart(const net::RedirectInfo& redirect_info,
+               std::unique_ptr<AppCacheRequestHandler> subresource_handler,
+               std::unique_ptr<SubresourceLoadInfo> subresource_load_info);
+
  private:
   friend class AppCacheNetworkServiceBrowserTest;
 
@@ -60,6 +71,16 @@ class CONTENT_EXPORT AppCacheSubresourceURLFactory
   // actual error.
   void NotifyError(mojom::URLLoaderClientPtr client, int error_code);
 
+  // Forwards the request to the network loader.
+  void DeliverNetworkResponse(mojom::URLLoaderRequest url_loader_request,
+                              int32_t routing_id,
+                              int32_t request_id,
+                              uint32_t options,
+                              const ResourceRequest& request,
+                              mojom::URLLoaderClientPtr client,
+                              const net::MutableNetworkTrafficAnnotationTag&
+                                  traffic_annotation);
+
   // Mojo bindings.
   mojo::BindingSet<mojom::URLLoaderFactory> bindings_;
 
@@ -68,6 +89,8 @@ class CONTENT_EXPORT AppCacheSubresourceURLFactory
   scoped_refptr<URLLoaderFactoryGetter> default_url_loader_factory_getter_;
 
   base::WeakPtr<AppCacheHost> appcache_host_;
+
+  base::WeakPtrFactory<AppCacheSubresourceURLFactory> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(AppCacheSubresourceURLFactory);
 };
