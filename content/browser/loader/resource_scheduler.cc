ec10a2e42dd74415e4992e73d62b0106eda50bd0
Josh Karlin <jkarlin@chromium.org>
2017-08-28 13:12:40 -0400

    [ResourceScheduler] Disable H2 throttling by default
    Bug: 655585
    Change-Id: Ie97d8c4a93b1e36ecb7e9b15e3a93524d24fd501

--- content/browser/loader/resource_scheduler.cc
+++ content/browser/loader/resource_scheduler.cc
@@ -41,7 +41,7 @@ namespace {
 // HTTP/1.1 resources are. Disabling this appears to have negative performance
 // impact, see https://crbug.com/655585.
 const base::Feature kPrioritySupportedRequestsDelayable{
-    "PrioritySupportedRequestsDelayable", base::FEATURE_ENABLED_BY_DEFAULT};
+    "PrioritySupportedRequestsDelayable", base::FEATURE_DISABLED_BY_DEFAULT};
 
 // When enabled, low-priority H2 and QUIC requests are throttled, but only
 // when the parser is in head.
