446f0942e76d40929d3ea798a5bb83371fe6a4a8
Sam McNally <sammc@chromium.org>
2017-08-28 18:03:29 +1000

    Remove NOTIFICATION_RESOURCE_RECEIVED_REDIRECT.
    Migrate WebViewGuest from NOTIFICATION_RESOURCE_RECEIVED_REDIRECT and
    NOTIFICATION_LOAD_COMPLETED_MAIN_FRAME to the corresponding
    WebContentsObserver methods and remove its use of NotificationService.
    
    Bug: 170921
    Change-Id: Id2067b929528b96929fd965365b5fc9e23050438

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -3455,12 +3455,6 @@ void WebContentsImpl::DidGetRedirectForResourceRequest(
   const ResourceRedirectDetails& details) {
   for (auto& observer : observers_)
     observer.DidGetRedirectForResourceRequest(details);
-
-  // TODO(avi): Remove. http://crbug.com/170921
-  NotificationService::current()->Notify(
-      NOTIFICATION_RESOURCE_RECEIVED_REDIRECT,
-      Source<WebContents>(this),
-      Details<const ResourceRedirectDetails>(&details));
 }
 
 void WebContentsImpl::NotifyWebContentsFocused(
