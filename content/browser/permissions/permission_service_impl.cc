416002fe004c32d8256fa42c68ef85ffbadbb8c3
Dominic Mazzoni <dmazzoni@chromium.org>
2017-08-28 13:33:11 -0700

    PermissionServiceImpl should handle BrowserContext being null.
    Both PermissionServiceContext and PermissionServiceImpl were assuming
    that there must always be a valid BrowserContext, but crash logs reveal
    that this isn't always true. GetBrowserContext() should legitimately
    return nullptr during teardown.
    
    In addition, when showing an interstitial page (for example
    a security warning for bad SSL), render_frame_host_ is valid
    but web_contents() legitimately returns nullptr.
    
    Also, since the lifetime of PermissionServiceImpl is different than
    RenderFrameHostImpl, and because separate mojo channels aren't
    guaranteed to be ordered, it's possible that PermissionServiceImpl
    gets a mojo request to request a permission after the RFHI is
    destroyed.
    
    Bug: 759379
    Change-Id: Ib7537392bddf23d325e5e5fe1262fe986ae6b85c

--- content/browser/permissions/permission_service_impl.cc
+++ content/browser/permissions/permission_service_impl.cc
@@ -179,10 +179,13 @@ PermissionServiceImpl::PermissionServiceImpl(PermissionServiceContext* context)
     : context_(context), weak_factory_(this) {}
 
 PermissionServiceImpl::~PermissionServiceImpl() {
-  DCHECK(context_->GetBrowserContext());
+  BrowserContext* browser_context = context_->GetBrowserContext();
+  // May be null during teardown.
+  if (!browser_context)
+    return;
 
   PermissionManager* permission_manager =
-      context_->GetBrowserContext()->GetPermissionManager();
+      browser_context->GetPermissionManager();
   if (!permission_manager)
     return;
 
@@ -219,7 +222,10 @@ void PermissionServiceImpl::RequestPermissions(
   // any UI, we want to still return something relevant so the current
   // permission status is returned for each permission.
   BrowserContext* browser_context = context_->GetBrowserContext();
-  DCHECK(browser_context);
+  // May be null during teardown.
+  if (!browser_context)
+    return;
+
   if (!context_->render_frame_host() ||
       !browser_context->GetPermissionManager()) {
     std::vector<PermissionStatus> result(permissions.size());
@@ -335,7 +341,10 @@ PermissionStatus PermissionServiceImpl::GetPermissionStatusFromType(
     PermissionType type,
     const url::Origin& origin) {
   BrowserContext* browser_context = context_->GetBrowserContext();
-  DCHECK(browser_context);
+  // May be null during teardown.
+  if (!browser_context)
+    return PermissionStatus::DENIED;
+
   if (!browser_context->GetPermissionManager() ||
       !AllowedByFeaturePolicy(context_->render_frame_host(), type)) {
     return PermissionStatus::DENIED;
@@ -352,7 +361,10 @@ PermissionStatus PermissionServiceImpl::GetPermissionStatusFromType(
 void PermissionServiceImpl::ResetPermissionStatus(PermissionType type,
                                                   const url::Origin& origin) {
   BrowserContext* browser_context = context_->GetBrowserContext();
-  DCHECK(browser_context);
+  // May be null during teardown.
+  if (!browser_context)
+    return;
+
   if (!browser_context->GetPermissionManager())
     return;
 
