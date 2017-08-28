cf16d95e4ff8bb5a5e7ecc623db64d0aea1c829b
Shivani Sharma <shivanisha@chromium.org>
2017-08-28 11:19:53 -0400

    [subresource filter] Renderer side activation of indirect special url navigations.
    Some times its possible that a special url navigation changes its process from 
    its parent process to the initiator in case the navigation is done after the frame
    was initially created. This CL makes the change such that the special url frame is
    able to inherit the activation state of its initiator, in that case by traversing
    the frame tree and stopping at the first frame which is local to this frame.
    
    Using --site-per-process with the tests is able to simulate this scenario.
    
    Bug: 739777
    Change-Id: I380f929c1d9a3eb280969d252c37068d74be6a74

--- components/subresource_filter/content/renderer/subresource_filter_agent.h
+++ components/subresource_filter/content/renderer/subresource_filter_agent.h
@@ -61,8 +61,9 @@ class SubresourceFilterAgent
       const DocumentLoadStatistics& statistics);
 
  private:
-  // Assumes that the parent will be in a local frame relative to this one, upon
-  // construction.
+  // Traverses the tree upwards from |render_frame|'s parent to find the first
+  // frame that is local relative to this one and returns the activation state
+  // of that frame.
   static ActivationState GetParentActivationState(
       content::RenderFrame* render_frame);
 
