b76d3e7f9cbaa7e91eb4fb1182f01f9dd40f73dd
Yuri Wiitala <miu@chromium.org>
2017-08-28 16:28:37 -0700

    Video Capture: Support for populating frames via writable Mojo shmem.
    To support the migration of tab capture into the VIZ component, it was
    necessary to expand the pool's GetHandleForInterProcessTransit()
    functionality to provide a writable Mojo shared memory handle (before it
    was read-only).
    
    In addition, to avoid mapping memory twice in the same process (where
    the video frames are being produced), this change adds a "lazy mapping"
    scheme to media::SharedBufferMemoryTracker. It will only map the shmem
    while there are outstanding VideoCaptureBufferHandles requiring that the
    mapping exists, and not before. However, this change requires that the
    VCBHandle subclass (SBMHandle) notify SBMTracker when its lifetime ends,
    so the SBMTracker can unmap memory after memory access is no longer
    needed. To simplify the code structure around all this, SBMHandle has
    been brought inside SBMTracker as a private inner class.
    
    Bug: 754872
    Change-Id: Id77e9be1151d2fd6482223e9698f541afe504a73

--- media/capture/video/shared_memory_buffer_tracker.cc
+++ media/capture/video/shared_memory_buffer_tracker.cc
@@ -4,19 +4,26 @@
 
 #include "media/capture/video/shared_memory_buffer_tracker.h"
 
+#include "base/logging.h"
 #include "base/memory/ptr_util.h"
-#include "media/capture/video/shared_memory_buffer_handle.h"
-#include "mojo/public/cpp/system/platform_handle.h"
+#include "ui/gfx/geometry/size.h"
 
 namespace media {
 
 SharedMemoryBufferTracker::SharedMemoryBufferTracker()
-    : VideoCaptureBufferTracker() {}
+    : VideoCaptureBufferTracker(), map_ref_count_(0) {}
+
+SharedMemoryBufferTracker::~SharedMemoryBufferTracker() {
+  // If the tracker is being destroyed, there must be no outstanding
+  // Handles. If this DCHECK() triggers, it means that either there is a logic
+  // flaw in VideoCaptureBufferPoolImpl, or a client did not delete all of its
+  // owned VideoCaptureBufferHandles before calling Pool::ReliquishXYZ().
+  DCHECK_EQ(map_ref_count_, 0);
+}
 
 bool SharedMemoryBufferTracker::Init(const gfx::Size& dimensions,
                                      VideoPixelFormat format,
-                                     VideoPixelStorage storage_type,
-                                     base::Lock* lock) {
+                                     VideoPixelStorage storage_type) {
   DVLOG(2) << __func__ << "allocating ShMem of " << dimensions.ToString();
   set_dimensions(dimensions);
   // |dimensions| can be 0x0 for trackers that do not require memory backing.
@@ -27,25 +34,66 @@ bool SharedMemoryBufferTracker::Init(const gfx::Size& dimensions,
                      .ImageAllocationSize();
   if (!mapped_size_)
     return true;
-  return shared_memory_.CreateAndMapAnonymous(mapped_size_);
+  DCHECK_EQ(map_ref_count_, 0);
+  return shared_memory_.CreateAnonymous(mapped_size_);
 }
 
 std::unique_ptr<VideoCaptureBufferHandle>
 SharedMemoryBufferTracker::GetMemoryMappedAccess() {
-  return base::MakeUnique<SharedMemoryBufferHandle>(&shared_memory_,
-                                                    mapped_size_);
+  bool need_to_map_memory;
+  {
+    base::AutoLock lock(map_ref_count_lock_);
+    DCHECK_GE(map_ref_count_, 0);
+    ++map_ref_count_;
+    need_to_map_memory = (map_ref_count_ == 1);
+  }
+  if (need_to_map_memory)
+    CHECK(shared_memory_.Map(mapped_size_));
+
+  return std::make_unique<Handle>(this);
 }
 
-mojo::ScopedSharedBufferHandle
-SharedMemoryBufferTracker::GetHandleForTransit() {
+mojo::ScopedSharedBufferHandle SharedMemoryBufferTracker::GetHandleForTransit(
+    bool read_only) {
   return mojo::WrapSharedMemoryHandle(
       base::SharedMemory::DuplicateHandle(shared_memory_.handle()),
-      mapped_size_, false /* read_only */);
+      mapped_size_, read_only);
 }
 
 base::SharedMemoryHandle
 SharedMemoryBufferTracker::GetNonOwnedSharedMemoryHandleForLegacyIPC() {
   return shared_memory_.handle();
+}
+
+void SharedMemoryBufferTracker::OnHandleDestroyed() {
+  bool need_to_unmap_memory;
+  {
+    base::AutoLock lock(map_ref_count_lock_);
+    DCHECK_GT(map_ref_count_, 0);
+    --map_ref_count_;
+    need_to_unmap_memory = (map_ref_count_ == 0);
+  }
+  if (need_to_unmap_memory)
+    CHECK(shared_memory_.Unmap());
+}
+
+SharedMemoryBufferTracker::Handle::Handle(SharedMemoryBufferTracker* owner)
+    : owner_(owner) {}
+
+SharedMemoryBufferTracker::Handle::~Handle() {
+  owner_->OnHandleDestroyed();
+}
+
+size_t SharedMemoryBufferTracker::Handle::mapped_size() const {
+  return owner_->mapped_size_;
+}
+
+uint8_t* SharedMemoryBufferTracker::Handle::data() const {
+  return static_cast<uint8_t*>(owner_->shared_memory_.memory());
+}
+
+const uint8_t* SharedMemoryBufferTracker::Handle::const_data() const {
+  return static_cast<const uint8_t*>(owner_->shared_memory_.memory());
 }
 
 }  // namespace media
