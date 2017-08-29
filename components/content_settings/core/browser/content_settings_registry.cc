dcec189434f4ff296a9790a5fb104b7855fb016e
Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
2017-08-29 15:31:17 +0300

    Register CONTENT_SETTINGS_TYPE_SENSORS in ContentSettingsRegistry
    Otherwise `navigator.permissions.revoke({ name: "accelerometer" })`
    is causing crash.
    
    Bug:759426
    Change-Id: I6c465caf717e13d0330527cdd702b979f7294a79

--- components/content_settings/core/browser/content_settings_registry.cc
+++ components/content_settings/core/browser/content_settings_registry.cc
@@ -327,6 +327,15 @@ void ContentSettingsRegistry::Init() {
            WebsiteSettingsRegistry::DESKTOP |
                WebsiteSettingsRegistry::PLATFORM_ANDROID,
            ContentSettingsInfo::INHERIT_IF_LESS_PERMISSIVE);
+
+  Register(CONTENT_SETTINGS_TYPE_SENSORS, "sensors-guard",
+           CONTENT_SETTING_ALLOW, WebsiteSettingsInfo::UNSYNCABLE,
+           WhitelistedSchemes(),
+           ValidSettings(CONTENT_SETTING_ALLOW, CONTENT_SETTING_BLOCK),
+           WebsiteSettingsInfo::TOP_LEVEL_ORIGIN_ONLY_SCOPE,
+           WebsiteSettingsRegistry::DESKTOP |
+               WebsiteSettingsRegistry::PLATFORM_ANDROID,
+           ContentSettingsInfo::INHERIT_IF_LESS_PERMISSIVE);
 }
 
 void ContentSettingsRegistry::Register(
