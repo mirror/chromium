abbea83820b60cdad00b7d7093eaa0c11061caf7
Matthew Cary <mattcary@chromium.org>
2017-08-29 13:26:41 +0200

    CCT: export load timing and network quality estimation.
    Expose load timing and network quality estimation to a Custom Tabs client via
    the AndroidPageLoadMetricsObserver.
    
    Bug: 739612,749711
    Change-Id: Ic15600f0360d89b36e2bdd7277cc45c539294b6a

--- chrome/browser/page_load_metrics/observers/android_page_load_metrics_observer.h
+++ chrome/browser/page_load_metrics/observers/android_page_load_metrics_observer.h
@@ -9,6 +9,7 @@
 
 #include "base/macros.h"
 #include "chrome/browser/page_load_metrics/page_load_metrics_observer.h"
+#include "net/nqe/network_quality_estimator.h"
 
 namespace content {
 class WebContents;
@@ -23,15 +24,25 @@ class AndroidPageLoadMetricsObserver
   explicit AndroidPageLoadMetricsObserver(content::WebContents* web_contents);
 
   // page_load_metrics::PageLoadMetricsObserver:
+  ObservePolicy OnStart(content::NavigationHandle* navigation_handle,
+                        const GURL& currently_committed_url,
+                        bool started_in_foreground) override;
   void OnFirstContentfulPaintInPage(
       const page_load_metrics::mojom::PageLoadTiming& timing,
       const page_load_metrics::PageLoadExtraInfo& extra_info) override;
   void OnLoadEventStart(
       const page_load_metrics::mojom::PageLoadTiming& timing,
       const page_load_metrics::PageLoadExtraInfo& info) override;
+  void OnLoadedResource(const page_load_metrics::ExtraRequestCompleteInfo&
+                            extra_request_complete_info) override;
 
  private:
   content::WebContents* web_contents_;
+
+  bool did_dispatch_on_main_resource_ = false;
+
+  net::NetworkQualityEstimator::NetworkQualityProvider*
+      network_quality_provider_ = nullptr;
 
   DISALLOW_COPY_AND_ASSIGN(AndroidPageLoadMetricsObserver);
 };
