544f8de52722167335677b15ec645e667407dfa1
edchin <edchin@chromium.org>
2017-08-29 10:52:31 -0700

    [ios clean] Tab strip and bottom toolbar experiment
    This CL adds the new tab strip behind an experiment flag, and refactors
    the bottom toolbar experiment. Both of these experiments affect the
    tab coordinator.
    
    Goals were:
    - Only access the experiment flag in one place, so there's a single
    point to turn on or off the feature.
    - Use only one or two conditionals in a given class.
    - Minimize the permutations of subclasses (this CL has at most two
    subclasses per super class).
    - Use either an injected flag, configuration object, or subclassing
    as best fitting for the situation.
    
    Highlights:
    - Subclassing was used for the Tab Strip experiment.
    - A simple conditional was used for the Bottom Toolbar experiment. 
    
    Bug: 686770
    Change-Id: Id1799eb776218b6c293279e6d15c80b882fe6c3e

---
+++ ios/clean/chrome/browser/ui/tab/tab_features.cc
@@ -0,0 +1,16 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/clean/chrome/browser/ui/tab/tab_features.h"
+
+namespace tab {
+namespace features {
+
+const base::Feature kTabStrip{"TabStrip", base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kBottomToolbar{"BottomToolbar",
+                                   base::FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace features
+}  // namespace tab
---
+++ ios/clean/chrome/browser/ui/tab/tab_features.h
@@ -0,0 +1,22 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CLEAN_CHROME_BROWSER_UI_TAB_TAB_FEATURES_H_
+#define IOS_CLEAN_CHROME_BROWSER_UI_TAB_TAB_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace tab {
+namespace features {
+
+// Used to control whether the new tab strip is used.
+extern const base::Feature kTabStrip;
+
+// Used to control whether the toolbar is placed at the bottom.
+extern const base::Feature kBottomToolbar;
+
+}  // namespace features
+}  // namespace tab
+
+#endif  // IOS_CLEAN_CHROME_BROWSER_UI_TAB_TAB_FEATURES_H_
