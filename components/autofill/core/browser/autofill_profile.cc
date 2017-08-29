3781a710b58c492f7202e39a9138d49156b13dc5
sebsg <sebsg@chromium.org>
2017-08-29 11:35:43 -0400

    [Autofill] Add function to compute the profile's validity bitfield.
    Bug: 748223
    Change-Id: If6d3305f086bb33e6cb47a8939654727f253ed2b

--- components/autofill/core/browser/autofill_profile.cc
+++ components/autofill/core/browser/autofill_profile.cc
@@ -191,6 +191,17 @@ void GetFieldsForDistinguishingProfiles(
   }
 }
 
+// The order is important to ensure a consistent bitfield value. New values
+// should be added at the end NOT at the start or middle.
+static std::vector<ServerFieldType> supported_types_for_validation = {
+    ADDRESS_HOME_COUNTRY,
+    ADDRESS_HOME_STATE,
+    ADDRESS_HOME_ZIP,
+    ADDRESS_HOME_CITY,
+    ADDRESS_HOME_DEPENDENT_LOCALITY,
+    EMAIL_ADDRESS,
+    PHONE_HOME_WHOLE_NUMBER};
+
 }  // namespace
 
 AutofillProfile::AutofillProfile(const std::string& guid,
@@ -715,18 +726,21 @@ void AutofillProfile::SetValidityState(ServerFieldType type,
 }
 
 bool AutofillProfile::IsValidationSupportedForType(ServerFieldType type) {
-  switch (type) {
-    case ADDRESS_HOME_STATE:
-    case ADDRESS_HOME_ZIP:
-    case ADDRESS_HOME_COUNTRY:
-    case ADDRESS_HOME_CITY:
-    case ADDRESS_HOME_DEPENDENT_LOCALITY:
-    case EMAIL_ADDRESS:
-    case PHONE_HOME_WHOLE_NUMBER:
-      return true;
-    default:
-      return false;
+  return std::find(supported_types_for_validation.begin(),
+                   supported_types_for_validation.end(),
+                   type) != supported_types_for_validation.end();
+}
+
+int AutofillProfile::GetValidityBitfieldValue() {
+  int validity_value = 0;
+  size_t field_type_shift = 0;
+  for (ServerFieldType supported_type : supported_types_for_validation) {
+    DCHECK(GetValidityState(supported_type) != UNSUPPORTED);
+    validity_value |= GetValidityState(supported_type) << field_type_shift;
+    field_type_shift += kValidityBitsPerType;
   }
+
+  return validity_value;
 }
 
 base::string16 AutofillProfile::GetInfoImpl(
