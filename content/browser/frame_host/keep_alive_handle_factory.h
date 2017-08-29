4c89541d8bcc83803bf8d6c1849e405f9318d7ed
Yutaka Hirano <yhirano@chromium.org>
2017-08-29 18:45:55 +0900

    Keep renderer alive for "keepalive" requests
    In order to support "keepalive" property in the fetch spec, we need to
    keep a renderer process alive while such requests are pending. This CL
    introduces "KeepAliveRendererForKeepaliveRequests" feature. When it
    is enabled, blink::ResourceFetcher prolongs the renderer process'
    lifetime with WebURLRequestsTracker. The tracker will be deactivated
    when one of the following conditions holds:
    
     - All pending requests finish.
     - A certain time period (default: 30sec) passes.
    
    The feature is disabled by default.
    
    Bug: 695939
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I0dd5ee2c1dfd4e694dfcf2f2f62485add7dec249

---
+++ content/browser/frame_host/keep_alive_handle_factory.h
@@ -0,0 +1,38 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_RENDERER_HOST_REQUESTS_TRACKER_IMPL_HOLDER_H_
+#define CONTENT_BROWSER_RENDERER_HOST_REQUESTS_TRACKER_IMPL_HOLDER_H_
+
+#include "base/memory/ref_counted.h"
+#include "content/common/frame.mojom.h"
+
+namespace content {
+
+class RenderProcessHost;
+
+// A KeepAliveHandleFactory creates mojom::KeepAliveHandle. Each created
+// handle prolongs the associated renderer's lifetime by using
+// RenderProcessHost's KeepAliveRefCounts while alive.
+// When a certain time passes after the factory is destroyed, all created
+// handles are invalidated.
+class KeepAliveHandleFactory final {
+ public:
+  KeepAliveHandleFactory(RenderProcessHost* process_host);
+  ~KeepAliveHandleFactory();
+
+  void Create(mojom::KeepAliveHandleRequest request);
+
+ private:
+  class KeepAliveHandleImpl;
+  class Context;
+
+  scoped_refptr<Context> context_;
+
+  DISALLOW_COPY_AND_ASSIGN(KeepAliveHandleFactory);
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_RENDERER_HOST_REQUESTS_TRACKER_IMPL_HOLDER_H_
