3aa19a99c33925dee4b84ef0d4d8803f3072f75c
Xianzhu Wang <wangxianzhu@chromium.org>
2017-08-28 22:34:56 -0700

    Let SPv2 produce layer tree more similar to SPv1
    Change-Id: Ic0617e8e56ecdc53a5a67175236ffd5f577af2b9

--- third_party/WebKit/Source/platform/graphics/compositing/ContentLayerClientImpl.h
+++ third_party/WebKit/Source/platform/graphics/compositing/ContentLayerClientImpl.h
@@ -10,6 +10,7 @@
 #include "platform/PlatformExport.h"
 #include "platform/graphics/GraphicsLayerClient.h"
 #include "platform/graphics/paint/PaintChunk.h"
+#include "platform/wtf/HashMap.h"
 #include "platform/wtf/Noncopyable.h"
 #include "platform/wtf/Vector.h"
 
@@ -46,7 +47,16 @@ class PLATFORM_EXPORT ContentLayerClientImpl : public cc::ContentLayerClient {
 
   void SetTracksRasterInvalidations(bool);
 
-  std::unique_ptr<JSONObject> LayerAsJSON(LayerTreeFlags);
+  struct LayerAsJSONContext {
+    LayerAsJSONContext(LayerTreeFlags flags) : flags(flags) {}
+
+    const LayerTreeFlags flags;
+    int next_transform_id = 1;
+    std::unique_ptr<JSONArray> transforms_json;
+    HashMap<const TransformPaintPropertyNode*, int> transform_id_map;
+    HashMap<int, int> rendering_context_map;
+  };
+  std::unique_ptr<JSONObject> LayerAsJSON(LayerAsJSONContext&) const;
 
   scoped_refptr<cc::PictureLayer> UpdateCcPictureLayer(
       const PaintArtifact&,
@@ -94,6 +104,8 @@ class PLATFORM_EXPORT ContentLayerClientImpl : public cc::ContentLayerClient {
   void InvalidateRasterForOldChunk(const PaintChunkInfo&,
                                    PaintInvalidationReason);
   void InvalidateRasterForWholeLayer();
+
+  int GetTransformIdForLayerAsJSON(LayerAsJSONContext&) const;
 
   scoped_refptr<cc::PictureLayer> cc_picture_layer_;
   scoped_refptr<cc::DisplayItemList> cc_display_item_list_;
