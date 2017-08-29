3d96beab0a9cbe21172167eaa6147996b9ff4f7f
Jialiu Lin <jialiul@chromium.org>
2017-08-29 13:05:14 -0700

    [macOS] add password reuse buttons to page info
    A follow-up CL of https://chromium-review.googlesource.com/602867
    The new site identity section includes a title, a description,
    a "Change Password" button, and a "Site is legitimate" button.
    
    This new page info section is gated by finch feature and is not
    wired up yet.
    
    Bug:741138,760207
    Change-Id: I8291a3b6ba3fab33a7c73fedb33600acf63a8d96

--- components/safe_browsing/features.cc
+++ components/safe_browsing/features.cc
@@ -24,7 +24,7 @@ const base::Feature kAdSamplerTriggerFeature{"SafeBrowsingAdSamplerTrigger",
 
 const base::Feature kGoogleBrandedPhishingWarning{
     "PasswordProtectionGoogleBrandedPhishingWarning",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kLocalDatabaseManagerEnabled{
     "SafeBrowsingV4LocalDatabaseManagerEnabled",
