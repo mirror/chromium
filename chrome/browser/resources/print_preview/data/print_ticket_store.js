021914028bee7520efecbbd644314295ef85bed5
rbpotter <rbpotter@chromium.org>
2017-08-29 15:02:58 -0700

    Print Preview: Move fit to page checkbox into scaling section
    Per UX advice, move fit to page checkbox into scaling section since
    the two controls are related. Change behavior so that fit to page is
    immediately unchecked when scaling field is modified. Also disable
    print button if an invalid scaling value is currently displayed.
    
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I2b31cb1872a59effe433a19a8b526f6824328141

--- chrome/browser/resources/print_preview/data/print_ticket_store.js
+++ chrome/browser/resources/print_preview/data/print_ticket_store.js
@@ -414,16 +414,18 @@ cr.define('print_preview', function() {
       return this.isTicketValidForPreview() &&
           (!this.copies_.isCapabilityAvailable() || this.copies_.isValid()) &&
           (!this.pageRange_.isCapabilityAvailable() ||
-           this.pageRange_.isValid());
+           this.pageRange_.isValid()) &&
+          (!this.scaling_.isCapabilityAvailable() || this.scaling_.isValid());
     },
 
     /** @return {boolean} Whether the ticket is valid for preview generation. */
     isTicketValidForPreview: function() {
       return (
-          !this.marginsType_.isCapabilityAvailable() ||
-          !this.marginsType_.isValueEqual(
-              print_preview.ticket_items.MarginsTypeValue.CUSTOM) ||
-          this.customMargins_.isValid());
+          (!this.marginsType_.isCapabilityAvailable() ||
+           !this.marginsType_.isValueEqual(
+               print_preview.ticket_items.MarginsTypeValue.CUSTOM) ||
+           this.customMargins_.isValid()) &&
+          (!this.scaling_.isCapabilityAvailable() || this.scaling_.isValid()));
     },
 
     /**
