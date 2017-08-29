8885ff792b1a65e9396c1c2347218f3948e57478
Albert J. Wong <ajwong@chromium.org>
2017-08-29 16:55:11 -0700

    static_assert if HAS_OUT_OF_PROC_TEST_RUNNER in IN_PROC_BROWSER_TEST_*
    Without this, failing to declare the macro results in a really confusing
    compile error that says the fixture class is pure virtual.
    
    Bug: none.
    Change-Id: I2dc8d17efe9591d1a9b8776fd45a404190829660

--- content/public/test/browser_test.h
+++ content/public/test/browser_test.h
@@ -91,6 +91,13 @@
 #define IN_PROC_BROWSER_TEST_P(test_case_name, test_name) \
   IN_PROC_BROWSER_TEST_P_(test_case_name, test_name)
 
+#else
+
+#define IN_PROC_BROWSER_TEST_F(test_fixture, test_name) \
+  static_assert(false, "HAS_OUT_OF_PROC_TEST_RUNNER not defined"); if (false)
+#define IN_PROC_BROWSER_TEST_P(test_case_name, test_name) \
+  static_assert(false, "HAS_OUT_OF_PROC_TEST_RUNNER not defined"); if (false)
+
 #endif  // defined(HAS_OUT_OF_PROC_TEST_RUNNER)
 
 #endif  // CONTENT_PUBLIC_TEST_BROWSER_TEST_H_
