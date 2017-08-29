3b53c46ec62029069070a61084884a05462a53c7
Reilly Grant <reillyg@chromium.org>
2017-08-29 13:16:58 -0700

    Support URLLoaderThrottles with synchronous XHR
    This change restores support for synchronous XMLHttpRequest with the
    network service enabled by adding the concept of detaching a
    URLLoaderThrottle from the thread on which it was created and later
    attaching it to the thread on which the request is being processed.
    
    To make this work for the Safe Browsing throttle a Clone message was
    added to the SafeBrowsing interface, allowing a new pipe, bound to the
    thread on which the request is being process, to be created.
    
    Bug: 754827
    Change-Id: Ifd212061e95a7f49d3e6238ccc6182231692989c

--- components/safe_browsing/renderer/websocket_sb_handshake_throttle_unittest.cc
+++ components/safe_browsing/renderer/websocket_sb_handshake_throttle_unittest.cc
@@ -54,6 +54,8 @@ class FakeSafeBrowsing : public mojom::SafeBrowsing {
     run_loop_.Quit();
   }
 
+  void Clone(mojom::SafeBrowsingRequest request) override { NOTREACHED(); }
+
   void RunUntilCalled() { run_loop_.Run(); }
 
   int32_t render_frame_id_;
