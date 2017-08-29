9339c056a988a3c98d8004cbe1132e561b3cd105
Joel Hockey <joelhockey@chromium.org>
2017-08-29 04:06:40 -0700

    DOM Distiller Web Agent runs and invokes mojo call to browser.
    * Web Agents api in WebKit/webagents
    * agent implemented in components/dom_distiller/
    * agent.cc takes WebLocalFrame in ctor and casts to
      WebLocalFrameImpl so it can access LocalFrame.
    
    Bug:
    Change-Id: I0fdfd0b16b0bbf806a03e034f6c33f41bd48c3af

---
+++ third_party/WebKit/webagents/event_target.cc
@@ -0,0 +1,17 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/WebKit/webagents/event_target.h"
+
+#include "core/dom/events/EventTarget.h"
+
+namespace webagents {
+
+EventTarget::EventTarget(blink::EventTarget* target) : event_target_(target) {
+  DCHECK(target);
+}
+
+EventTarget::~EventTarget() {}
+
+}  // namespace webagents
