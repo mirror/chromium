c40dacd52c9632cda502e985ac02d7cdc454ce6c
Daniel Cheng <dcheng@chromium.org>
2017-08-28 21:56:58 -0700

    Add a new typemap for base::Value using value semantics
    base::Value is now a value type: thus, it's preferred to
    treat it as such rather than using a std::unique_ptr to
    own it. To allow incremental migration of existing Mojo
    interfaces, this CL clones the current struct traits for
    base::Value into //base/mojo, updating it to use value
    semantics. New code should prefer the struct definitions
    in //base/mojo.
    
    The primary complexity is because base::DictionaryValue
    still manages its internal values with std::unique_ptr.
    In order to ensure efficient serialization, this CL
    retains the legacy DictionaryValue mojo struct and its
    corresponding MapTraits. In order to discourage further
    use of the DictionaryValue mojo struct, it is now named
    InternalDictValueDoNotUse. Once base::DictionaryValue
    uses value semantics internally, this legacy struct will
    be removed.
    
    Bug: 646113
    Change-Id: I330fed8f592063ddd88a633a01222cce1e09e504

---
+++ base/mojom/values_struct_traits.cc
@@ -0,0 +1,95 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/mojom/values_struct_traits.h"
+
+#include <memory>
+#include <utility>
+
+#include "base/memory/ptr_util.h"
+#include "base/strings/string_piece.h"
+
+namespace mojo {
+
+bool StructTraits<base::mojom::DictionaryValueDataView, base::Value>::Read(
+    base::mojom::DictionaryValueDataView data,
+    base::Value* value_out) {
+  mojo::MapDataView<mojo::StringDataView, base::mojom::ValueDataView> view;
+  data.GetStorageDataView(&view);
+  std::vector<base::Value::DictStorage::value_type> dict_storage;
+  dict_storage.reserve(view.size());
+  for (size_t i = 0; i < view.size(); ++i) {
+    base::StringPiece key;
+    auto value = base::MakeUnique<base::Value>();
+    if (!view.keys().Read(i, &key) || !view.values().Read(i, value.get()))
+      return false;
+    dict_storage.emplace_back(key.as_string(), std::move(value));
+  }
+  *value_out = base::Value(base::Value::DictStorage(std::move(dict_storage),
+                                                    base::KEEP_LAST_OF_DUPES));
+  return true;
+}
+
+bool StructTraits<base::mojom::ListValueDataView, base::Value>::Read(
+    base::mojom::ListValueDataView data,
+    base::Value* value_out) {
+  mojo::ArrayDataView<base::mojom::ValueDataView> view;
+  data.GetStorageDataView(&view);
+  std::vector<base::Value> list_storage(view.size());
+  for (size_t i = 0; i < view.size(); ++i) {
+    if (!view.Read(i, &list_storage[i]))
+      return false;
+  }
+  *value_out = base::Value(std::move(list_storage));
+  return true;
+}
+
+bool UnionTraits<base::mojom::ValueDataView, base::Value>::Read(
+    base::mojom::ValueDataView data,
+    base::Value* value_out) {
+  switch (data.tag()) {
+    case base::mojom::ValueDataView::Tag::NULL_VALUE: {
+      *value_out = base::Value();
+      return true;
+    }
+    case base::mojom::ValueDataView::Tag::BOOL_VALUE: {
+      *value_out = base::Value(data.bool_value());
+      return true;
+    }
+    case base::mojom::ValueDataView::Tag::INT_VALUE: {
+      *value_out = base::Value(data.int_value());
+      return true;
+    }
+    case base::mojom::ValueDataView::Tag::DOUBLE_VALUE: {
+      *value_out = base::Value(data.double_value());
+      return true;
+    }
+    case base::mojom::ValueDataView::Tag::STRING_VALUE: {
+      base::StringPiece string_piece;
+      if (!data.ReadStringValue(&string_piece))
+        return false;
+      *value_out = base::Value(string_piece);
+      return true;
+    }
+    case base::mojom::ValueDataView::Tag::BINARY_VALUE: {
+      mojo::ArrayDataView<uint8_t> binary_data_view;
+      data.GetBinaryValueDataView(&binary_data_view);
+      const char* data_pointer =
+          reinterpret_cast<const char*>(binary_data_view.data());
+      std::vector<char> binary_storage(data_pointer,
+                                       data_pointer + binary_data_view.size());
+      *value_out = base::Value(std::move(binary_storage));
+      return true;
+    }
+    case base::mojom::ValueDataView::Tag::DICTIONARY_VALUE: {
+      return data.ReadDictionaryValue(value_out);
+    }
+    case base::mojom::ValueDataView::Tag::LIST_VALUE: {
+      return data.ReadListValue(value_out);
+    }
+  }
+  return false;
+}
+
+}  // namespace mojo
