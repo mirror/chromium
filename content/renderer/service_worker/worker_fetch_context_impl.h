2a171e13cd8b8ca589b3de90d213aeadec8225be
Takashi Toyoshima <toyoshim@chromium.org>
2017-08-30 17:52:57 +0900

    make ChildURLLoaderFactoryGetter be in content/public
    mojom::URLLoaderFactory is going to be a standard way to make a network
    request, but there is no way to obtain mojom::URLLoaderFactory instance
    in chrome/{child,renderer} today.
    
    This patch moves ChildURLLoaderFactoryGetter into content/public/child,
    and promotes RenderFrameImpl::GetDefaultURLLoaderFactoryGetter interface
    to public interface of RenderFrame.
    
    Bug: 752028
    Change-Id: I7abecd06a534e8aad0d402647471c0556583abb2

--- content/renderer/service_worker/worker_fetch_context_impl.h
+++ content/renderer/service_worker/worker_fetch_context_impl.h
@@ -8,6 +8,7 @@
 #include "content/child/child_url_loader_factory_getter.h"
 #include "content/common/service_worker/service_worker_provider.mojom.h"
 #include "content/common/service_worker/service_worker_types.h"
+#include "content/public/child/child_url_loader_factory_getter.h"
 #include "content/public/common/service_worker_modes.h"
 #include "content/public/common/url_loader_factory.mojom.h"
 #include "ipc/ipc_message.h"
