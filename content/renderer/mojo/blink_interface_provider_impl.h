193c299238e5b11dcb7caa589ba84a27469f91a0
Nico Weber <thakis@chromium.org>
2017-08-29 20:06:24 -0400

    Fix some -Wdelete-non-virtual-dtor warnings.
    The warning was kind-of-disabled but https://reviews.llvm.org/D37235
    is bringing it back, so we need to clean up our code again.
    
    This fixes several (likely benign) bugs where we used to not call the dtors
    of subclasses because we deleted them through a pointer to their superclass
    without the superclass having a virtual dtor.
    
    The warning fires on `delete var` if the type of var has virtual methods
    (suggesting it can be subclasses, and var can in theory point to a subclass)
    but it has no virtual destructor (meaning the subclass's destructor won't be
    called).
    
    There are several possible fixes:
    * If the type only has virtual methods because it itself overrides a few
      methods (due to it implementing some interface) but it's not supposed to
      be subclassed, just mark the class final. That fixes the warning without
      a behavior change.
    * If the type really has subclasses, give it a virtual dtor, so that subclass
      dtors are called correctly.
    
    Bug: 673171
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I80a8c19bd85fe814d9905f0b7c94f02027a5372b

--- content/renderer/mojo/blink_interface_provider_impl.h
+++ content/renderer/mojo/blink_interface_provider_impl.h
@@ -23,7 +23,7 @@ namespace content {
 
 // An implementation of blink::InterfaceProvider that forwards to a
 // service_manager::InterfaceProvider.
-class BlinkInterfaceProviderImpl : public blink::InterfaceProvider {
+class BlinkInterfaceProviderImpl final : public blink::InterfaceProvider {
  public:
   explicit BlinkInterfaceProviderImpl(
       base::WeakPtr<service_manager::Connector> connector);
