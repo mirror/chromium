c33637c26fe44985c4f79427e3ee5aaa4ab02e14
Alice Boxhall <aboxhall@chromium.org>
2017-08-30 17:09:01 +1000

    [Devtools] Tweaks to what nodes are shown and synced in the Accessibility tree
    Bug: 625500
    Change-Id: I36e4144ea8a66e430dd7c3b0b9010b900dd7398d

--- third_party/WebKit/Source/devtools/front_end/accessibility/AXBreadcrumbsPane.js
+++ third_party/WebKit/Source/devtools/front_end/accessibility/AXBreadcrumbsPane.js
@@ -73,10 +73,17 @@ Accessibility.AXBreadcrumbsPane = class extends Accessibility.AccessibilitySubPa
 
     this._setPreselectedBreadcrumb(this._inspectedNodeBreadcrumb);
 
-    for (var child of axNode.children()) {
-      var childBreadcrumb = new Accessibility.AXBreadcrumb(child, depth, false);
-      this._inspectedNodeBreadcrumb.appendChild(childBreadcrumb);
+    function append(parentBreadcrumb, axNode, localDepth) {
+      var childBreadcrumb = new Accessibility.AXBreadcrumb(axNode, localDepth, false);
+      parentBreadcrumb.appendChild(childBreadcrumb);
+
+      // In most cases there will be no children here, but there are some special cases.
+      for (var child of axNode.children())
+        append(childBreadcrumb, child, localDepth + 1);
     }
+
+    for (var child of axNode.children())
+      append(this._inspectedNodeBreadcrumb, child, depth);
 
     this._selectedByUser = false;
   }
