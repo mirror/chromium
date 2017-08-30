feab97ae5fa44f88cac1a452dcb80096f3113f36
Regan Hsu <hsuregan@google.com>
2017-08-29 21:22:14 -0700

    first set of funcs
    Bug:
    Change-Id: I2b5bc4dabe652c739910ec3dd76466f589ee3014

---
+++ components/multidevice/service/public/interfaces/device_sync_struct_traits.h
@@ -0,0 +1,47 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DEVICE_SYNC_MOJO_STRUCT_TRAITS_H_
+#define DEVICE_SYNC_MOJO_STRUCT_TRAITS_H_
+
+#include "components/cryptauth/proto/cryptauth_api.pb.h"
+#include "components/cryptauth/remote_device.h"
+#include "components/multidevice/service/public/interfaces/device_sync.mojom.h"
+#include "mojo/public/cpp/bindings/struct_traits.h"
+
+namespace mojo {
+
+template <>
+class StructTraits<device_sync::mojom::BeaconSeedDataView,
+                   cryptauth::BeaconSeed> {
+ public:
+  static std::string data(const cryptauth::BeaconSeed& beacon_seed);
+  static base::TimeTicks start_time(const cryptauth::BeaconSeed& beacon_seed);
+  static base::TimeTicks end_time(const cryptauth::BeaconSeed& beacon_seed);
+  static bool Read(device_sync::mojom::BeaconSeedDataView data,
+                   cryptauth::BeaconSeed* out);
+};
+
+template <>
+class StructTraits<device_sync::mojom::RemoteDeviceDataView,
+                   cryptauth::RemoteDevice> {
+ public:
+  static std::string public_key(const cryptauth::RemoteDevice& remote_device);
+  static std::string device_id(const cryptauth::RemoteDevice& remote_device);
+  static std::string user_id(const cryptauth::RemoteDevice& remote_device);
+  static std::string device_name(const cryptauth::RemoteDevice& remote_device);
+  static std::string persistent_symmetric_key(
+      const cryptauth::RemoteDevice& remote_device);
+  static bool unlock_key(const cryptauth::RemoteDevice& remote_device);
+  static bool mobile_hotspot_supported(
+      const cryptauth::RemoteDevice& remote_device);
+  static std::vector<cryptauth::BeaconSeed> beacon_seeds(
+      const cryptauth::RemoteDevice& remote_device);
+  static bool Read(device_sync::mojom::RemoteDeviceDataView data,
+                   cryptauth::RemoteDevice* out_remote_device);
+};
+
+}  // namespace mojo
+
+#endif  // DEVICE_SYNC_MOJO_STRUCT_TRAITS_H_
