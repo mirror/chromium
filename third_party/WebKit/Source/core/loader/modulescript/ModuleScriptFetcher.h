1e449b6b68e596d34ff69db007779858473b7379
Hiroki Nakagawa <nhiroki@chromium.org>
2017-08-30 14:18:21 +0900

    ES6 Modules: Change ModuleScriptFetcher to pure-virtual interface
    For cleanup, this CL changes ModuleScriptFetcher to pure-virtual interface, and
    makes {Document,Worklet}ModuleScriptFetcher directly inherit it.
    
    Before this CL, ModuleScriptFetcher is a concrete class used for module loading
    on the Document context, and WorkletModuleScriptFetcher inherits
    ModuleScriptFetcher for the Worklet context. This worked well until [1] because
    WorkeltModuleScriptFetcher depended on functions of ModuleScriptFetcher (e.g.,
    implementation of ResourceOwner). However, after the change,
    WorkletModuleScriptFetcher no longer depends on them and doesn't have to
    inherit it.
    
    To simplify the code, this CL...
    - changes ModuleScriptFetcher to pure-virtual interface.
    - introduces DocumentModuleScriptFetcher as implementation of
      ModuleScriptFetcher for the Document context. This is almost the same as the
      previous ModuleScriptFetcher.
    - makes WorkletModuleScriptFetcher directly inherit ModuleScriptFetcher as
      opposed to DocumentModuleScriptFetcher.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/623044
    
    Bug: 727194
    Change-Id: I42631f5c21d8473b9a121883b4a5b027c56089cd

--- third_party/WebKit/Source/core/loader/modulescript/ModuleScriptFetcher.h
+++ third_party/WebKit/Source/core/loader/modulescript/ModuleScriptFetcher.h
@@ -5,13 +5,9 @@
 #ifndef ModuleScriptFetcher_h
 #define ModuleScriptFetcher_h
 
-#include "core/dom/Modulator.h"
+#include "core/CoreExport.h"
 #include "core/loader/modulescript/ModuleScriptCreationParams.h"
-#include "core/loader/resource/ScriptResource.h"
 #include "platform/loader/fetch/FetchParameters.h"
-#include "platform/loader/fetch/ResourceFetcher.h"
-#include "platform/loader/fetch/ResourceOwner.h"
-#include "platform/weborigin/SecurityOrigin.h"
 #include "platform/wtf/Optional.h"
 
 namespace blink {
@@ -22,11 +18,7 @@ class ConsoleMessage;
 // (via ScriptResource::Fetch). Then, it keeps track of the fetch progress by
 // being a ResourceOwner. Finally, it returns its client a fetched resource as
 // ModuleScriptCreationParams.
-class CORE_EXPORT ModuleScriptFetcher
-    : public GarbageCollectedFinalized<ModuleScriptFetcher>,
-      public ResourceOwner<ScriptResource> {
-  USING_GARBAGE_COLLECTED_MIXIN(ModuleScriptFetcher);
-
+class CORE_EXPORT ModuleScriptFetcher : public GarbageCollectedMixin {
  public:
   class CORE_EXPORT Client : public GarbageCollectedMixin {
    public:
@@ -35,30 +27,7 @@ class CORE_EXPORT ModuleScriptFetcher
         ConsoleMessage* error_message) = 0;
   };
 
-  ModuleScriptFetcher(const FetchParameters&,
-                      ResourceFetcher*,
-                      Client*);
-
-  // 'virtual' for custom fetch.
-  virtual void Fetch();
-
-  // Implements ScriptResourceClient
-  void NotifyFinished(Resource*) final;
-  String DebugName() const final { return "ModuleScriptFetcher"; }
-
-  const FetchParameters& GetFetchParams() const { return fetch_params_; }
-
-  DECLARE_TRACE();
-
- protected:
-  virtual void Finalize(const WTF::Optional<ModuleScriptCreationParams>&,
-                        ConsoleMessage* error_message);
-
- private:
-  FetchParameters fetch_params_;
-  Member<ResourceFetcher> fetcher_;
-  Member<Client> client_;
-  bool was_fetched_ = false;
+  virtual void Fetch(FetchParameters&) = 0;
 };
 
 }  // namespace blink
