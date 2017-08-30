21cface921977df356ebf25dbeac1a58d933a061
Derek Cheng <imcheng@chromium.org>
2017-08-30 16:42:32 -0700

    [Media Router Android] Parse Cast Presentation URLs.
    Also added check for valid autojoin policy for legacy and new URL
    formats.
    
    Bug: 757358
    Change-Id: If99b46e9f0fd692e2665b5148c6ac7949b2fcb0f

--- chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/MediaSource.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/MediaSource.java
@@ -22,6 +22,8 @@ public class MediaSource {
     public static final String AUTOJOIN_TAB_AND_ORIGIN_SCOPED = "tab_and_origin_scoped";
     public static final String AUTOJOIN_ORIGIN_SCOPED = "origin_scoped";
     public static final String AUTOJOIN_PAGE_SCOPED = "page_scoped";
+    private static final String AUTOJOIN_POLICIES = {AUTOJOIN_CUSTOM_CONTROLLER_SCOPED,
+            AUTOJOIN_TAB_AND_ORIGIN_SCOPED, AUTOJOIN_ORIGIN_SCOPED, AUTOJOIN_PAGE_SCOPED};
 
     private static final String CAST_SOURCE_ID_SEPARATOR = "/";
     private static final String CAST_SOURCE_ID_APPLICATION_ID = "__castAppId__";
@@ -37,6 +39,26 @@ public class MediaSource {
         "audio_in",
         "multizone_group"
     };
+
+    /**
+     * The scheme for Cast Presentation URLs.
+     */
+    private static final String CAST_URL_SCHEME = "cast";
+
+    /**
+     * The query parameter key for Cast Client ID in a Cast Presentation URL.
+     */
+    private static final String CAST_URL_CLIENT_ID = "clientId";
+
+    /**
+     * The query parameter key for autojoin policy in a Cast Presentation URL.
+     */
+    private static final String CAST_URL_AUTOJOIN_POLICY = "autoJoinPolicy";
+
+    /**
+     * The query parameter key for app capabilities in a Cast Presentation URL.
+     */
+    private static final String CAST_URL_CAPABILITIES = "capabilities";
 
     /**
      * The original presentation URL that the {@link MediaSource} object was created from.
@@ -76,28 +98,10 @@ public class MediaSource {
         assert sourceId != null;
 
         Uri sourceUri = Uri.parse(sourceId);
-
-        String uriFragment = sourceUri.getFragment();
-        if (uriFragment == null) return null;
-
-        String[] parameters = uriFragment.split(CAST_SOURCE_ID_SEPARATOR);
-
-        String applicationId = extractParameter(parameters, CAST_SOURCE_ID_APPLICATION_ID);
-        if (applicationId == null) return null;
-
-        String[] capabilities = null;
-        int capabilitiesIndex = applicationId.indexOf(CAST_APP_CAPABILITIES_PREFIX);
-        if (capabilitiesIndex != -1) {
-            capabilities = extractCapabilities(applicationId.substring(capabilitiesIndex));
-            if (capabilities == null) return null;
-
-            applicationId = applicationId.substring(0, capabilitiesIndex);
+        if (CAST_URL_SCHEME.equals(sourceUri.getScheme())) {
+            return fromCastUrl(sourceId, sourceUri);
         }
-
-        String clientId = extractParameter(parameters, CAST_SOURCE_ID_CLIENT_ID);
-        String autoJoinPolicy = extractParameter(parameters, CAST_SOURCE_ID_AUTOJOIN_POLICY);
-
-        return new MediaSource(sourceId, applicationId, clientId, autoJoinPolicy, capabilities);
+        return fromLegacyUrl(sourceId, sourceUri);
     }
 
     /**
@@ -196,5 +200,73 @@ public class MediaSource {
             if (!supportedCapabilities.contains(capability)) return null;
         }
         return capabilities;
+    }
+
+    /**
+     * Helper method to create a MediaSource object from a Cast (cast:) presentation URL.
+     * @param sourceId the source id for the Cast media source.
+     * @param sourceUri the parsed Uri object for sourceId.
+     * @return an initialized media source if the uri is a valid Cast presentation URL, null
+     * otherwise.
+     */
+    @Nullable
+    private static MediaSource fromCastUrl(String sourceId, Uri sourceUri) {
+        String applicationId = sourceUri.getPath();
+        if (applicationId == null) return null;
+
+        String clientId = sourceUri.getQueryParameter(CAST_URL_CLIENT_ID);
+        String autoJoinPolicy = sourceUri.getQueryParameter(CAST_URL_AUTOJOIN_POLICY);
+        if (autoJoinPolicy != null && !Arrays.asList(AUTOJOIN_POLICIES).contains(autoJoinPolicy)) {
+            return null;
+        }
+
+        String[] capabilities = null;
+        String capabilitesParam = sourceUri.getQueryParameter(CAST_URL_CAPABILITIES);
+        if (capabilitesParam != null) {
+            capabilities = capabilitiesParam.split(CAST_APP_CAPABILITIES_SEPARATOR);
+            List<String> supportedCapabilities = Arrays.asList(CAST_APP_CAPABILITIES);
+            for (String capability : capabilities) {
+                if (!supportedCapabilities.contains(capability)) return null;
+            }
+        }
+
+        return new MediaSource(sourceId, applicationId, clientId, autoJoinPolicy, capabilities);
+    }
+
+    /**
+     * @deprecated Legacy Cast Presentation URLs are deprecated in favor of cast: URLs. This method
+     * will be removed when we drop support for legacy URLs (crbug.com/757358).
+     * Helper method to create a MediaSource object from a legacy (https:) presentation URL.
+     * @param sourceId the source id for the Cast media source.
+     * @param sourceUri the parsed Uri object for sourceId.
+     * @return an initialized media source if the uri is a valid https presentation URL, null
+     * otherwise.
+     */
+    @Deprecated
+    @Nullable
+    private static MediaSource fromLegacyUrl(String sourceId, Uri sourceUri) {
+        String uriFragment = sourceUri.getFragment();
+        if (uriFragment == null) return null;
+
+        String[] parameters = uriFragment.split(CAST_SOURCE_ID_SEPARATOR);
+
+        String applicationId = extractParameter(parameters, CAST_SOURCE_ID_APPLICATION_ID);
+        if (applicationId == null) return null;
+
+        String[] capabilities = null;
+        int capabilitiesIndex = applicationId.indexOf(CAST_APP_CAPABILITIES_PREFIX);
+        if (capabilitiesIndex != -1) {
+            capabilities = extractCapabilities(applicationId.substring(capabilitiesIndex));
+            if (capabilities == null) return null;
+
+            applicationId = applicationId.substring(0, capabilitiesIndex);
+        }
+
+        String clientId = extractParameter(parameters, CAST_SOURCE_ID_CLIENT_ID);
+        String autoJoinPolicy = extractParameter(parameters, CAST_SOURCE_ID_AUTOJOIN_POLICY);
+        if (autoJoinPolicy != null && !Arrays.asList(AUTOJOIN_POLICIES).contains(autoJoinPolicy)) {
+            return null;
+        }
+        return new MediaSource(sourceId, applicationId, clientId, autoJoinPolicy, capabilities);
     }
 }
