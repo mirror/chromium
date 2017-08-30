778f1214215d069dcc6d2ea2850dd11cdec2f4fa
Ian Vollick <vollick@chromium.org>
2017-08-30 15:55:38 -0400

    [vr] Remove Kick/End visibility
    Turns out that these are not necessary in the current UI. We can bring
    this functionality back later when it's required.
    
    Bug:None
    Change-Id: Ia3b627118706dbb88836c39489378375b259ca33

--- chrome/browser/vr/elements/transience_manager.h
+++ chrome/browser/vr/elements/transience_manager.h
@@ -15,14 +15,10 @@ namespace vr {
 
 class TransienceManager {
  public:
-  TransienceManager(UiElement* element,
-                    float opacity_when_enabled,
-                    const base::TimeDelta& timeout);
+  TransienceManager(UiElement* element, const base::TimeDelta& timeout);
   virtual ~TransienceManager() = default;
 
-  void SetEnabled(bool enabled);
-  void KickVisibilityIfEnabled();
-  void EndVisibilityIfEnabled();
+  void SetVisible(bool visible);
 
  private:
   void StartTimer();
@@ -31,9 +27,8 @@ class TransienceManager {
   void Hide();
 
   UiElement* element_;
-  float opacity_when_enabled_;
   base::TimeDelta timeout_;
-  bool enabled_ = false;
+  bool visible_ = false;
   base::OneShotTimer visibility_timer_;
 };
 
