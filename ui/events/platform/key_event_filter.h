efba4b2ba3a9aafd8008f9859a571e9112143e04
Zijie He <zijiehe@chromium.org>
2017-08-30 14:56:06 -0700

    [Keyboard-Lock] Add KeyEventFilter and PlatformKeyEventFilter
    According to the design, X11EventSource needs to use PlatformKeyEventFilter to
    decide whether the keyboard event should be forwarded to regular event route. So
    PlatformKeyEventFilter needs to be placed under //ui/events/ or
    //ui/events/platform to let //ui/events/platform/x11 depend on it.
    Meanwhile KeyEventFilter is needed by PlatformKeyEventFilter, the later one only
    forwards the requests to KeyEventFilter::OnKeyDown() and
    KeyEventFilter::OnKeyUp() functions. So KeyEventFilter interface also needs to
    stay in the same folder. To ensure the performance, KeyEventFilter processes
    ui::KeyboardCode in integer form.
    
    Considering PlatformKeyEventFilter consumes ui::PlatformEvent,
    //ui/events/platform looks like a better place for it.
    
    Both KeyEventFilter and PlatformKeyEventFilter are used in //ui/events only, so
    they do not need to be declared with EVENTS_EXPORT.
    
    This is part of Keyboard-Lock feature. Current implementation follows the
    prototype at https://codereview.chromium.org/2879033002/.
    
    Bug: chromium:680809
    Change-Id: Ibec674b69a335566916a4b8009c536a459e5def2

---
+++ ui/events/platform/key_event_filter.h
@@ -0,0 +1,24 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_EVENTS_PLATFORM_KEY_EVENT_FILTER_H_
+#define UI_EVENTS_PLATFORM_KEY_EVENT_FILTER_H_
+
+namespace ui {
+
+class KeyEvent;
+
+// An interface to receive and process a key event.
+class KeyEventFilter {
+ public:
+  KeyEventFilter() = default;
+  virtual ~KeyEventFilter() = default;
+
+  // Returns true if |event| has been consumed already.
+  virtual bool OnKeyEvent(const ui::KeyEvent& event) = 0;
+};
+
+}  // namespace ui
+
+#endif  // UI_EVENTS_PLATFORM_KEY_EVENT_FILTER_H_
