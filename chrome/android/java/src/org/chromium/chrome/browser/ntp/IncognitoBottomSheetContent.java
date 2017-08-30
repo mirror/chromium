013786b5426729db6cd812e357b7ce950a81ab4f
Troy Hildebrandt <thildebr@chromium.org>
2017-08-30 10:46:11 -0700

    [Home] Match non-MD incognito NTP page styling with the MD version.
    When using Chrome Home, this brings the MD incognito NTP styling to the
    non-MD version, leaving the original intact when not using Chrome Home.
    
    Bug:735294
    Change-Id: I048103f3194159ea5c9858b4dd5a36d68d33b4bb

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/IncognitoBottomSheetContent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/IncognitoBottomSheetContent.java
@@ -5,9 +5,13 @@
 package org.chromium.chrome.browser.ntp;
 
 import android.app.Activity;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewTreeObserver.OnScrollChangedListener;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -37,7 +41,7 @@ public class IncognitoBottomSheetContent extends IncognitoNewTabPage implements 
                             mIncognitoNewTabPageView.getResources(), R.color.toolbar_shadow_color),
                 FadingShadow.POSITION_TOP);
 
-        initTextViewColors();
+        initTextViews();
 
         // Hide the incognito image from the Chrome Home NTP pages.
         ImageView incognitoSplash =
@@ -63,7 +67,7 @@ public class IncognitoBottomSheetContent extends IncognitoNewTabPage implements 
         });
     }
 
-    private void initTextViewColors() {
+    private void initTextViews() {
         final int locationBarLightHintTextColor = ApiCompatibilityUtils.getColor(
                 mIncognitoNewTabPageView.getResources(), R.color.locationbar_light_hint_text);
         final int googleBlueColor = ApiCompatibilityUtils.getColor(
@@ -97,9 +101,25 @@ public class IncognitoBottomSheetContent extends IncognitoNewTabPage implements 
             final TextView newTabIncognitoMessageView =
                     (TextView) mIncognitoNewTabPageView.findViewById(
                             R.id.new_tab_incognito_message);
+            final LinearLayout newTabIncognitoContainer =
+                    (LinearLayout) mIncognitoNewTabPageView.findViewById(
+                            R.id.new_tab_incognito_container);
 
             incognitoNtpHeaderView.setTextColor(locationBarLightHintTextColor);
+
             newTabIncognitoMessageView.setTextColor(locationBarLightHintTextColor);
+            newTabIncognitoMessageView.setLineSpacing(spToPx(6), 1.0f);
+            LayoutParams layoutParams = (LayoutParams) newTabIncognitoMessageView.getLayoutParams();
+            layoutParams.bottomMargin = dpToPx(32);
+
+            newTabIncognitoContainer.setPadding(dpToPx(16),
+                    newTabIncognitoContainer.getPaddingTop(),
+                    newTabIncognitoContainer.getPaddingRight(),
+                    newTabIncognitoContainer.getPaddingBottom());
+            newTabIncognitoContainer.setGravity(Gravity.START);
+
+            learnMoreView.setPadding(0, 0, 0, 0);
+            learnMoreView.setAllCaps(false);
         }
     }
 
@@ -145,5 +165,21 @@ public class IncognitoBottomSheetContent extends IncognitoNewTabPage implements 
     @Override
     public boolean applyDefaultTopPadding() {
         return true;
+    }
+
+    /**
+     * Converts a dp value to a px value.
+     */
+    private int dpToPx(int dp) {
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+                getContentView().getResources().getDisplayMetrics()));
+    }
+
+    /*
+     * Converts an sp value to a px value.
+     */
+    private float spToPx(int sp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, sp,
+                getContentView().getResources().getDisplayMetrics());
     }
 }
