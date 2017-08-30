96c462f815f13e9fc0b9a1e2f6cf109e62234ad0
Regan Hsu <hsuregan@google.com>
2017-08-30 16:50:20 -0700

    [CrOS MultiDevice] First set of DeviceSync API functions.
    The DeviceSync API syncs metadata about other devices tied to a given Google account.
    It contacts the existing CryptAuth back-end to enroll the current device
    and sync down new data about other devices.
    
    Bug:752273
    Change-Id: I27ad165f68adab29a6afe8ae277dbfa46f1ffef3

---
+++ components/multidevice/service/multidevice_service.cc
@@ -0,0 +1,59 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/multidevice/service/multidevice_service.h"
+
+#include "components/multidevice/service/device_sync_impl.h"
+#include "components/cryptauth/remote_device_provider.h"
+#include "mojo/public/cpp/bindings/strong_binding.h"
+#include "services/service_manager/public/cpp/service_context.h"
+
+namespace multidevice {
+
+//TODO(hsuregan): Remove when whereabouts of parameters can be determined.
+MultiDeviceService::MultiDeviceService()
+    : MultiDeviceService(nullptr, nullptr, nullptr, nullptr, nullptr) {}
+
+MultiDeviceService::MultiDeviceService(
+    std::unique_ptr<service_manager::Connector> connector,
+    std::unique_ptr<cryptauth::CryptAuthGCMManager> gcm_manager,
+    std::unique_ptr<cryptauth::CryptAuthDeviceManager> device_manager,
+    std::unique_ptr<cryptauth::SecureMessageDelegateFactory> secure_message_delegate_factory,
+    std::unique_ptr<cryptauth::CryptAuthEnrollmentManager> enrollment_manager)
+    : connector_(std::move(connector)),
+      gcm_manager_(std::move(gcm_manager)),
+      device_manager_(std::move(device_manager)),
+      enrollment_manager_(std::move(enrollment_manager)),
+      secure_message_delegate_factory_(std::move(secure_message_delegate_factory)),
+      weak_ptr_factory_(this) {}
+
+MultiDeviceService::~MultiDeviceService() {}
+
+void MultiDeviceService::OnStart() {
+  ref_factory_ = base::MakeUnique<service_manager::ServiceContextRefFactory>(
+      base::Bind(&service_manager::ServiceContext::RequestQuit,
+                 base::Unretained(context())));
+  registry_.AddInterface<device_sync::mojom::DeviceSync>(
+      base::Bind(&MultiDeviceService::CreateDeviceSyncImpl,
+                 base::Unretained(this), ref_factory_.get()));
+}
+
+void MultiDeviceService::OnBindInterface(
+    const service_manager::BindSourceInfo& source_info,
+    const std::string& interface_name,
+    mojo::ScopedMessagePipeHandle interface_pipe) {
+  registry_.BindInterface(interface_name, std::move(interface_pipe));
+}
+
+void MultiDeviceService::CreateDeviceSyncImpl(
+    service_manager::ServiceContextRefFactory* ref_factory,
+    device_sync::mojom::DeviceSyncRequest request) {
+  mojo::MakeStrongBinding(
+      base::MakeUnique<DeviceSyncImpl>(
+          ref_factory->CreateRef(), connector_.get(), gcm_manager_.get(),
+          device_manager_.get(), enrollment_manager_.get(), secure_message_delegate_factory_.get()),
+      std::move(request));
+}
+
+}  // namespace multidevice
---
+++ components/multidevice/service/multidevice_service.h
@@ -0,0 +1,66 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_MULTIDEVICE_SERVICE_H_
+#define COMPONENTS_MULTIDEVICE_SERVICE_H_
+
+#include "base/memory/weak_ptr.h"
+#include "components/cryptauth/cryptauth_device_manager.h"
+#include "components/cryptauth/cryptauth_enrollment_manager.h"
+#include "components/cryptauth/cryptauth_gcm_manager.h"
+#include "components/multidevice/service/public/interfaces/device_sync.mojom.h"
+#include "services/service_manager/public/cpp/binder_registry.h"
+#include "services/service_manager/public/cpp/connector.h"
+#include "services/service_manager/public/cpp/service.h"
+#include "services/service_manager/public/cpp/service_context_ref.h"
+
+namespace cryptauth {
+  class SecureMessageDelegateFactory;
+} // cryptauth
+
+namespace multidevice {
+
+// This class provides convenient APIs for creating connections
+// between multiple devices tied to a single Google Account, and sending as well
+// as recieving messages through these connections.
+class MultiDeviceService : public service_manager::Service {
+ public:
+  // TODO(hsuregan): Remove when gcm_manager, device_manager, and
+  // enrollment_manager can be integrated.
+  MultiDeviceService();
+
+  MultiDeviceService(
+      std::unique_ptr<service_manager::Connector> connector,
+      std::unique_ptr<cryptauth::CryptAuthGCMManager> gcm_manager,
+      std::unique_ptr<cryptauth::CryptAuthDeviceManager> device_manager,
+      std::unique_ptr<cryptauth::SecureMessageDelegateFactory> secure_message_delegate_factory,
+      std::unique_ptr<cryptauth::CryptAuthEnrollmentManager>
+          enrollment_manager);
+
+  ~MultiDeviceService() override;
+
+  // service_manager::Service:
+  void OnStart() override;
+  void OnBindInterface(const service_manager::BindSourceInfo& source_info,
+                       const std::string& interface_name,
+                       mojo::ScopedMessagePipeHandle interface_pipe) override;
+
+ private:
+  void CreateDeviceSyncImpl(
+      service_manager::ServiceContextRefFactory* ref_factory,
+      device_sync::mojom::DeviceSyncRequest request);
+
+  std::unique_ptr<service_manager::Connector> connector_;
+  std::unique_ptr<cryptauth::CryptAuthGCMManager> gcm_manager_;
+  std::unique_ptr<cryptauth::CryptAuthDeviceManager> device_manager_;
+  std::unique_ptr<cryptauth::CryptAuthEnrollmentManager> enrollment_manager_;
+  std::unique_ptr<cryptauth::SecureMessageDelegateFactory> secure_message_delegate_factory_;
+  std::unique_ptr<service_manager::ServiceContextRefFactory> ref_factory_;
+  service_manager::BinderRegistry registry_;
+  base::WeakPtrFactory<MultiDeviceService> weak_ptr_factory_;
+};
+
+}  // namespace multidevice
+
+#endif  // COMPONENTS_MULTIDEVICE_SERVICE_H_
