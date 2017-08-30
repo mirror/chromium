237d0a79f47c58e8f7d17e0256d885f870272c79
Karan Bhatia <karandeepb@chromium.org>
2017-08-29 18:41:00 -0700

    DNR: Index and persist manifest declarative rules for unpacked extensions.
    This CL builds on https://chromium-
    review.googlesource.com/c/chromium/src/+/625481 and adds supports for indexing
    and persisting the manifest declarative rules on each load of unpacked
    extensions.
    
    Doc=http://go/declarative-net-request (Internal only)
    BUG=696822
    
    Change-Id: I58afc054915b1cbbb97420edca5149a343a40fce

--- extensions/common/api/declarative_net_request/constants.h
+++ extensions/common/api/declarative_net_request/constants.h
@@ -11,6 +11,31 @@ namespace declarative_net_request {
 // Permission name.
 constexpr char kAPIPermission[] = "declarativeNetRequest";
 
+// Minimum valid value of a declarative rule ID.
+constexpr int kMinValidID = 1;
+
+// Minimum valid value of a declarative rule priority.
+constexpr int kMinValidPriority = 1;
+
+// Default priority used for rules when the priority is not explicity provided
+// by an extension.
+constexpr int kDefaultPriority = 1;
+
+// Keys used in rules.
+constexpr char kIDKey[] = "id";
+constexpr char kPriorityKey[] = "priority";
+constexpr char kRuleConditionKey[] = "condition";
+constexpr char kRuleActionKey[] = "action";
+constexpr char kUrlFilterKey[] = "urlFilter";
+constexpr char kIsUrlFilterCaseSensitiveKey[] = "isUrlFilterCaseSensitive";
+constexpr char kDomainsKey[] = "domains";
+constexpr char kExcludedDomainsKey[] = "excludedDomains";
+constexpr char kResourceTypesKey[] = "resourceTypes";
+constexpr char kExcludedResourceTypesKey[] = "excludedResourceTypes";
+constexpr char kDomainTypeKey[] = "domainType";
+constexpr char kRuleActionTypeKey[] = "type";
+constexpr char kRedirectUrlKey[] = "redirectUrl";
+
 }  // namespace declarative_net_request
 }  // namespace extensions
 
