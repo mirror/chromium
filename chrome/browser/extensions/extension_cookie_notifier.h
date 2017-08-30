0e6f18d28bad2ee6e3b01ff24284cb3822ecc05b
Randy Smith <rdsmith@chromium.org>
2017-08-30 11:57:36 -0400

    Remove CookieMonsterDelegate usage for tracking cookie changes, switching to registration interface.  This will allow cleaner conversion of cookie stores to Mojo.
    Bug: 721395
    Change-Id: Iaa4e13316a06781d9f37dad56e482ebec4921266
    R=mmenke@chromium.org

---
+++ chrome/browser/extensions/extension_cookie_notifier.h
@@ -0,0 +1,43 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_EXTENSIONS_EXTENSION_COOKIE_NOTIFIER_H_
+#define CHROME_BROWSER_EXTENSIONS_EXTENSION_COOKIE_NOTIFIER_H_
+
+#include <memory>
+#include <vector>
+
+#include "base/macros.h"
+#include "net/cookies/cookie_store.h"
+
+class Profile;
+
+// Sends cookie-change notifications on the UI thread via
+// chrome::NOTIFICATION_COOKIE_CHANGED_FOR_EXTENSIONS.
+// Constructed on the UI thread, lives and (if ever used) is destructed
+// on the IO thread.
+class ExtensionCookieNotifier {
+ public:
+  explicit ExtensionCookieNotifier(Profile* profile);
+  ~ExtensionCookieNotifier();
+
+  // Add a CookieStore for which cookie notifications will be transmitted.
+  // This store will be monitored until this object is destructed; i.e.
+  // |*store| must outlive this object.
+  // Must be called on the IO thread.
+  void AddStore(net::CookieStore* store);
+
+ private:
+  // net::CookieStore::CookieChangedCallback implementation.
+  void OnCookieChanged(const net::CanonicalCookie& cookie,
+                       net::CookieStore::ChangeCause cause);
+
+  Profile* profile_;
+  std::vector<std::unique_ptr<net::CookieStore::CookieChangedSubscription>>
+      subscriptions_;
+
+  DISALLOW_COPY_AND_ASSIGN(ExtensionCookieNotifier);
+};
+
+#endif  // CHROME_BROWSER_EXTENSIONS_EXTENSION_COOKIE_NOTIFIER_H_
