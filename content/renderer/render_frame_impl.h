5946a4d1ee4851f816bcfa4755c5451dfadc301a
Balazs Engedy <engedy@chromium.org>
2017-08-30 17:16:36 +0200

    Mojofy DidCommitProvisionalLoad.
    Refactor FrameHostMsg_DidCommitProvisionalLoad from being a legacy
    IPC message into a method on mojom::FrameHost, a frame-scoped,
    Channel-associated interface. There is no change to functionality.
    
    Bug: 729021
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I794a8f1cb433a82e2c1d9351630116b584f8fcf9

--- content/renderer/render_frame_impl.h
+++ content/renderer/render_frame_impl.h
@@ -706,7 +706,7 @@ class CONTENT_EXPORT RenderFrameImpl
                  mojom::FrameHostInterfaceBrokerPtr frame_host);
 
   // Virtual so the test render frame can flush the interface.
-  virtual mojom::FrameHostAssociatedPtr GetFrameHost();
+  virtual mojom::FrameHost* GetFrameHost();
 
   void BindFrameBindingsControl(
       mojom::FrameBindingsControlAssociatedRequest request);
@@ -1468,6 +1468,7 @@ class CONTENT_EXPORT RenderFrameImpl
 
   service_manager::BindSourceInfo browser_info_;
 
+  mojom::FrameHostAssociatedPtr frame_host_ptr_;
   mojo::BindingSet<service_manager::mojom::InterfaceProvider>
       interface_provider_bindings_;
 
