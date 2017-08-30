77309d2d5dcf75f55b0f6b96082e4cdac55e9f06
Mike Baxley <baxley@chromium.org>
2017-08-30 15:09:44 -0700

    Remove the matcher InterstitialContainingText.
    This matcher that waits has been replaced by a ChromeEarlGrey
    method that waits for the condition to become true. No more callsites
    use this.
    
    Bug: 700067
    Change-Id: I8ba9c2a922704ec4eb2701c07b9ada1fa17050d8

--- ios/web/public/test/earl_grey/web_view_matchers.mm
+++ ios/web/public/test/earl_grey/web_view_matchers.mm
@@ -55,9 +55,6 @@ enum ImageState {
   IMAGE_STATE_LOADED,
 };
 
-// Script that returns document.body as a string.
-char kGetDocumentBodyJavaScript[] =
-    "document.body ? document.body.textContent : null";
 // Script that tests presence of css selector.
 char kTestCssSelectorJavaScriptTemplate[] = "!!document.querySelector(\"%s\");";
 
@@ -251,28 +248,6 @@ id<GREYMatcher> Interstitial(WebState* web_state) {
 
   return grey_allOf(
       WebViewInWebState(web_state),
-      [[GREYElementMatcherBlock alloc] initWithMatchesBlock:matches
-                                           descriptionBlock:describe],
-      nil);
-}
-
-id<GREYMatcher> InterstitialContainingText(NSString* text,
-                                           WebState* web_state) {
-  MatchesBlock matches = ^BOOL(WKWebView* view) {
-    return WaitUntilConditionOrTimeout(testing::kWaitForUIElementTimeout, ^{
-      NSString* script = base::SysUTF8ToNSString(kGetDocumentBodyJavaScript);
-      id body = ExecuteScriptOnInterstitial(web_state, script);
-      return [body containsString:text] ? true : false;
-    });
-  };
-
-  DescribeToBlock describe = ^(id<GREYDescription> description) {
-    [description appendText:@"interstitial containing "];
-    [description appendText:text];
-  };
-
-  return grey_allOf(
-      Interstitial(web_state),
       [[GREYElementMatcherBlock alloc] initWithMatchesBlock:matches
                                            descriptionBlock:describe],
       nil);
