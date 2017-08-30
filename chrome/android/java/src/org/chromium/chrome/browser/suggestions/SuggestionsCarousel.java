c7539f11fbf3501b7ef9ebf98c5c83c04c3090fb
galinap <galinap@google.com>
2017-08-30 13:43:33 +0100

    [Suggestions] Add logs for contextual suggestion interactions
    This CL adds user action recording of the contextual suggestions
    carousel.
    
    Bug:715477
    Change-Id: I543ad445d2bd1d248f4d905c0f8bbdebfd02450f

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsCarousel.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsCarousel.java
@@ -7,21 +7,24 @@ package org.chromium.chrome.browser.suggestions;
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
+import android.content.Context;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.ViewGroup;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ntp.cards.ImpressionTracker;
 import org.chromium.chrome.browser.ntp.cards.ItemViewType;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageViewHolder;
 import org.chromium.chrome.browser.ntp.cards.NodeVisitor;
 import org.chromium.chrome.browser.ntp.cards.OptionalLeaf;
 import org.chromium.chrome.browser.ntp.snippets.SnippetArticle;
 import org.chromium.chrome.browser.widget.displaystyle.UiConfig;
+import org.chromium.ui.widget.Toast;
 
 import java.util.Collections;
-import java.util.List;
 
 /**
  * This is an optional item in Chrome Home in the bottom sheet. The carousel is a {@link
@@ -33,17 +36,43 @@ import java.util.List;
  */
 public class SuggestionsCarousel extends OptionalLeaf {
     private final SuggestionsCarouselAdapter mAdapter;
+    private final SuggestionsUiDelegate mUiDelegate;
     private String mCurrentContextUrl;
 
     public SuggestionsCarousel(UiConfig uiConfig, SuggestionsUiDelegate uiDelegate) {
         mAdapter = new SuggestionsCarouselAdapter(uiConfig, uiDelegate);
+        mUiDelegate = uiDelegate;
+
         setVisibilityInternal(true);
+    }
+
+    /**
+     * Removes any suggestions that might be present in the carousel.
+     */
+    public void clearSuggestions() {
+        mCurrentContextUrl = null;
+        mAdapter.setSuggestions(Collections.<SnippetArticle>emptyList());
+    }
+
+    /**
+     * Fetches new suggestions if necessary.
+     */
+    public void refresh(Context context, final String newUrl) {
+        // Do nothing if there are already suggestions in the carousel for the new context.
+        if (TextUtils.equals(newUrl, mCurrentContextUrl)) return;
+
+        String text = "Fetching contextual suggestions...";
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+
+        mUiDelegate.getSuggestionsSource().fetchContextualSuggestions(
+                newUrl, (contextualSuggestions) -> {
+                    mCurrentContextUrl = newUrl;
+                    mAdapter.setSuggestions(contextualSuggestions);
+                });
     }
 
     @Override
     protected void onBindViewHolder(NewTabPageViewHolder holder) {
-        assert holder.itemView instanceof RecyclerView;
-
         ((RecyclerView) holder.itemView).setAdapter(mAdapter);
     }
 
@@ -58,62 +87,48 @@ public class SuggestionsCarousel extends OptionalLeaf {
     }
 
     /**
-     * Set suggestions which will be displayed in the carousel.
-     *
-     * @param url The URL for which these suggestions were generated.
-     * @param newSuggestions The contextual suggestions for the URL.
-     */
-    public void newContextualSuggestionsAvailable(String url, List<SnippetArticle> newSuggestions) {
-        setVisibilityInternal(!newSuggestions.isEmpty());
-        mCurrentContextUrl = url;
-        mAdapter.setSuggestions(newSuggestions);
-    }
-
-    /**
-     * @return The URL for which the carousel has suggestions.
-     */
-    public String getCurrentCarouselContextUrl() {
-        return mCurrentContextUrl;
-    }
-
-    /**
-     * Removes any suggestions that might be present in the carousel.
-     */
-    public void clearSuggestions() {
-        mCurrentContextUrl = null;
-        mAdapter.setSuggestions(Collections.<SnippetArticle>emptyList());
-    }
-
-    /**
-     * Creates a view holder for a horizontal recycler view.
-     *
-     * @param parent The container for the recycler view.
-     * @return The created recycler view.
-     */
-    public NewTabPageViewHolder createViewHolder(ViewGroup parent) {
-        return new ViewHolder(parent);
-    }
-
-    /**
      * View holder for the {@link SuggestionsCarousel}.
      */
-    private static class ViewHolder extends NewTabPageViewHolder {
+    public static class ViewHolder extends NewTabPageViewHolder {
+        private ImpressionTracker mImpressionTracker;
+
         public ViewHolder(ViewGroup parentView) {
             super(new RecyclerView(parentView.getContext()));
 
+            setUpRecyclerView();
+
+            // The impression tracker will record metrics only once per bottom sheet opened.
+            mImpressionTracker = new ImpressionTracker(itemView, () -> {
+                SuggestionsMetrics.recordContextualSuggestionsCarouselShown();
+                mImpressionTracker.reset(null);
+            });
+        }
+
+        private void setUpRecyclerView() {
             RecyclerView recyclerView = (RecyclerView) itemView;
+            recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                    // The dragging state guarantees that the scroll event was caused by the user.
+                    if (newState != RecyclerView.SCROLL_STATE_DRAGGING) return;
+
+                    // Record only the first time the user scrolls the carousel after it was shown.
+                    SuggestionsMetrics.recordContextualSuggestionsCarouselScrolled();
+                    recyclerView.removeOnScrollListener(this);
+                }
+            });
 
             ViewGroup.LayoutParams params =
                     new RecyclerView.LayoutParams(MATCH_PARENT, WRAP_CONTENT);
             recyclerView.setLayoutParams(params);
 
             // Make the recycler view scroll horizontally.
-            LinearLayoutManager layoutManager = new LinearLayoutManager(parentView.getContext());
+            LinearLayoutManager layoutManager = new LinearLayoutManager(recyclerView.getContext());
             layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
             recyclerView.setLayoutManager(layoutManager);
             recyclerView.setClipToPadding(false);
 
-            float spaceBetweenCards = parentView.getContext().getResources().getDimension(
+            float spaceBetweenCards = recyclerView.getContext().getResources().getDimension(
                     R.dimen.contextual_carousel_space_between_cards);
             int recyclerViewMarginEnd = (int) Math.floor(spaceBetweenCards);
             ApiCompatibilityUtils.setPaddingRelative(recyclerView, 0, 0, recyclerViewMarginEnd, 0);
