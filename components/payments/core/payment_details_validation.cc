5190f7c8e88c603356b653d9a920ee4ba242a189
Mohamad Ahmadi <mahmadi@chromium.org>
2017-08-30 11:50:13 -0400

    [Payment Request] Moves payment_details_validation.* from content/ to core/
    Bug:602666
    Change-Id: I617a9a78c77fcfa6cfc3f767db65fd5173740f5f

--- components/payments/core/payment_details_validation.cc
+++ components/payments/core/payment_details_validation.cc
@@ -2,13 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/payments/content/payment_details_validation.h"
+#include "components/payments/core/payment_details_validation.h"
 
 #include <set>
 #include <vector>
 
+#include "components/payments/core/payment_details.h"
 #include "components/payments/core/payments_validators.h"
-#include "third_party/WebKit/public/platform/modules/payments/payment_request.mojom.h"
 
 namespace payments {
 namespace {
@@ -16,47 +16,43 @@ namespace {
 // Validates ShippingOption or PaymentItem, which happen to have identical
 // fields, except for "id", which is present only in ShippingOption.
 template <typename T>
-bool validateShippingOptionOrPaymentItem(
-    const T& item,
-    const payments::mojom::PaymentItemPtr& total,
-    std::string* error_message) {
-  if (!item->amount) {
+bool validateShippingOptionOrPaymentItem(const T& item,
+                                         const PaymentItem& total,
+                                         std::string* error_message) {
+  if (!item.amount) {
     *error_message = "Currency amount required";
     return false;
   }
-
-  if (item->amount->currency.empty()) {
+  if (item.amount->currency.empty()) {
     *error_message = "Currency code required";
     return false;
   }
 
-  if (item->amount->value.empty()) {
+  if (item.amount->value.empty()) {
     *error_message = "Currency value required";
     return false;
   }
 
-  if (item->amount->currency_system.empty()) {
+  if (item.amount->currency_system.empty()) {
     *error_message = "Currency system can't be empty";
     return false;
   }
 
   if (!payments::PaymentsValidators::IsValidCurrencyCodeFormat(
-          item->amount->currency, item->amount->currency_system,
-          error_message)) {
+          item.amount->currency, item.amount->currency_system, error_message)) {
     return false;
   }
 
-  if (!payments::PaymentsValidators::IsValidAmountFormat(item->amount->value,
+  if (!payments::PaymentsValidators::IsValidAmountFormat(item.amount->value,
                                                          error_message)) {
     return false;
   }
   return true;
 }
 
-bool validateDisplayItems(
-    const std::vector<payments::mojom::PaymentItemPtr>& items,
-    const payments::mojom::PaymentItemPtr& total,
-    std::string* error_message) {
+bool validateDisplayItems(const std::vector<PaymentItem>& items,
+                          const PaymentItem& total,
+                          std::string* error_message) {
   for (const auto& item : items) {
     if (!validateShippingOptionOrPaymentItem(item, total, error_message))
       return false;
@@ -64,22 +60,21 @@ bool validateDisplayItems(
   return true;
 }
 
-bool validateShippingOptions(
-    const std::vector<payments::mojom::PaymentShippingOptionPtr>& options,
-    const payments::mojom::PaymentItemPtr& total,
-    std::string* error_message) {
+bool validateShippingOptions(const std::vector<PaymentShippingOption>& options,
+                             const PaymentItem& total,
+                             std::string* error_message) {
   std::set<std::string> uniqueIds;
   for (const auto& option : options) {
-    if (option->id.empty()) {
+    if (option.id.empty()) {
       *error_message = "ShippingOption id required";
       return false;
     }
 
-    if (uniqueIds.find(option->id) != uniqueIds.end()) {
+    if (uniqueIds.find(option.id) != uniqueIds.end()) {
       *error_message = "Duplicate shipping option identifiers are not allowed";
       return false;
     }
-    uniqueIds.insert(option->id);
+    uniqueIds.insert(option.id);
 
     if (!validateShippingOptionOrPaymentItem(option, total, error_message))
       return false;
@@ -88,8 +83,8 @@ bool validateShippingOptions(
 }
 
 bool validatePaymentDetailsModifiers(
-    const std::vector<payments::mojom::PaymentDetailsModifierPtr>& modifiers,
-    const payments::mojom::PaymentItemPtr& total,
+    const std::vector<PaymentDetailsModifier>& modifiers,
+    const PaymentItem& total,
     std::string* error_message) {
   if (modifiers.empty()) {
     *error_message = "Must specify at least one payment details modifier";
@@ -97,29 +92,24 @@ bool validatePaymentDetailsModifiers(
   }
 
   for (const auto& modifier : modifiers) {
-    if (!modifier->method_data) {
-      *error_message = "Method data required";
-      return false;
-    }
-
-    if (modifier->method_data->supported_methods.empty()) {
+    if (modifier.method_data.supported_methods.empty()) {
       *error_message = "Must specify at least one payment method identifier";
       return false;
     }
 
-    if (modifier->total) {
-      if (!validateShippingOptionOrPaymentItem(modifier->total, total,
+    if (modifier.total) {
+      if (!validateShippingOptionOrPaymentItem(*modifier.total, total,
                                                error_message))
         return false;
 
-      if (modifier->total->amount->value[0] == '-') {
+      if (modifier.total->amount->value[0] == '-') {
         *error_message = "Total amount value should be non-negative";
         return false;
       }
     }
 
-    if (modifier->additional_display_items.size()) {
-      if (!validateDisplayItems(modifier->additional_display_items, total,
+    if (modifier.additional_display_items.size()) {
+      if (!validateDisplayItems(modifier.additional_display_items, total,
                                 error_message)) {
         return false;
       }
@@ -130,37 +120,38 @@ bool validatePaymentDetailsModifiers(
 
 }  // namespace
 
-bool validatePaymentDetails(const mojom::PaymentDetailsPtr& details,
+bool validatePaymentDetails(const PaymentDetails& details,
                             std::string* error_message) {
-  if (details->total) {
-    if (!validateShippingOptionOrPaymentItem(details->total, details->total,
-                                             error_message))
+  if (details.total) {
+    if (!validateShippingOptionOrPaymentItem(*details.total, *details.total,
+                                             error_message)) {
       return false;
+    }
 
-    if (details->total->amount->value[0] == '-') {
+    if (details.total->amount->value[0] == '-') {
       *error_message = "Total amount value should be non-negative";
       return false;
     }
   }
 
-  if (details->display_items.size()) {
-    if (!validateDisplayItems(details->display_items, details->total,
+  if (details.display_items.size()) {
+    if (!validateDisplayItems(details.display_items, *details.total,
                               error_message))
       return false;
   }
 
-  if (details->shipping_options.size()) {
-    if (!validateShippingOptions(details->shipping_options, details->total,
+  if (details.shipping_options.size()) {
+    if (!validateShippingOptions(details.shipping_options, *details.total,
                                  error_message))
       return false;
   }
 
-  if (details->modifiers.size()) {
-    if (!validatePaymentDetailsModifiers(details->modifiers, details->total,
+  if (details.modifiers.size()) {
+    if (!validatePaymentDetailsModifiers(details.modifiers, *details.total,
                                          error_message))
       return false;
   }
-  if (!PaymentsValidators::IsValidErrorMsgFormat(details->error, error_message))
+  if (!PaymentsValidators::IsValidErrorMsgFormat(details.error, error_message))
     return false;
   return true;
 }
