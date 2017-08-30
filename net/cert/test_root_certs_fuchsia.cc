a7b764f1f14f80e804324fb9ad4e0d737f6c35d3
Sergey Ulanov <sergeyu@google.com>
2017-08-30 12:37:24 -0700

    Implement TestRootCerts and add SystemTrustStore stub for Fuchsia.
    1. Updated TestRootCerts to store list of temporary certs in memory.
    2. Added SystemTrustStoreFuchsia. Currently it contains only certs
       added to TestRootCerts. Will extend it to load system certs in a
       separate CL.
    
    Bug: 731302
    Change-Id: Id6e1c526f92f1cc256fbf8545c8b6b20190f288b

--- net/cert/test_root_certs_fuchsia.cc
+++ net/cert/test_root_certs_fuchsia.cc
@@ -6,30 +6,39 @@
 
 #include "base/location.h"
 #include "base/logging.h"
+#include "net/cert/internal/cert_errors.h"
+#include "net/cert/internal/parsed_certificate.h"
+#include "net/cert/internal/system_trust_store.h"
 #include "net/cert/x509_certificate.h"
+#include "net/cert/x509_util.h"
 
 namespace net {
 
 bool TestRootCerts::Add(X509Certificate* certificate) {
-  // TODO(fuchsia): Implement this.
-  NOTIMPLEMENTED();
-  return false;
+  std::string der_encoded;
+  if (!X509Certificate::GetDEREncoded(certificate->os_cert_handle(),
+                                      &der_encoded)) {
+    LOG(FATAL) << "GetDEREncoded() failed.";
+  }
+  CertErrors err;
+  auto parsed =
+      ParsedCertificate::Create(net::x509_util::CreateCryptoBuffer(der_encoded),
+                                ParseCertificateOptions(), &err);
+  CHECK(parsed) << err.ToDebugString();
+  temporary_roots_.push_back(parsed);
+  return true;
 }
 
 void TestRootCerts::Clear() {
-  // TODO(fuchsia): Implement this.
-  NOTIMPLEMENTED();
-  empty_ = true;
+  temporary_roots_.clear();
 }
 
 bool TestRootCerts::IsEmpty() const {
-  return empty_;
+  return temporary_roots_.empty();
 }
 
 TestRootCerts::~TestRootCerts() {}
 
-void TestRootCerts::Init() {
-  empty_ = true;
-}
+void TestRootCerts::Init() {}
 
 }  // namespace net
