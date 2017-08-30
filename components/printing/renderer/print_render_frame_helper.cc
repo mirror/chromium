44ef64adae8eade1dc2a1095cf90e8653de90a50
Kent Tamura <tkent@chromium.org>
2017-08-30 15:53:36 +0900

    Prepare to support beforeprint/afterprint events.
    https://html.spec.whatwg.org/multipage/timers-and-user-prompts.html#printing
    
    * Blink
    Add empty implementation of WebLocalFrame::DispatchBeforePrintEvent()
    and DispatchAfterPrintEvent().
    They should be called before and after pairs of PrintBegin() and
    PrintEnd().
    Note that we usually calls a pair of PrintBegin() and PrintEnd()
    multiple times to print a page. So we shouldn't dispatch beforeprint/
    afterprint events in PrintBegin() and PrintEnd().
    
    * For PRINT_PREVIEW
    PrintRenderFrameHelper::RequestPrintPreview() calls
    DispatchBeforePrintEvent(). Add a new message to tell that print preview
    dialog is closed, and its message handler calls
    DispatchAfterPrintEvent().
    
    If "Print using system dialog..." in the preview dialog is
    clicked, we don't send the message.
    PrintRenderFrameHelper::OnPrintForSystemDialog() is called in
    this case, and it doesn't call DispatchBeforePrintEvent(), but
    calls DispatchAfterPrintEvent().
    
    * For BASIC_PRINTING
    PrintRenderFrameHelper::OnPrintPages() and ScriptedPrint() calls
    DispatchBeforePrintEvent() before Print(), and
    DispatchAfterPrintEvent() after Print().
    
    This CL shouldn't have any user-visible behavior changes.
    
    Bug: 218205
    Change-Id: I536358441990f4afb3739ce4b3015d0e3347b11d

--- components/printing/renderer/print_render_frame_helper.cc
+++ components/printing/renderer/print_render_frame_helper.cc
@@ -1044,7 +1044,11 @@ void PrintRenderFrameHelper::ScriptedPrint(bool user_initiated) {
 #endif
   } else {
 #if BUILDFLAG(ENABLE_BASIC_PRINTING)
+    web_frame->DispatchBeforePrintEvent();
+    if (!web_frame->View())
+      return;
     Print(web_frame, blink::WebNode(), true /* is_scripted? */);
+    web_frame->DispatchAfterPrintEvent();
 #endif
   }
   // WARNING: |this| may be gone at this point. Do not do any more work here and
@@ -1074,6 +1078,8 @@ bool PrintRenderFrameHelper::OnMessageReceived(const IPC::Message& message) {
     IPC_MESSAGE_HANDLER(PrintMsg_InitiatePrintPreview, OnInitiatePrintPreview)
     IPC_MESSAGE_HANDLER(PrintMsg_PrintPreview, OnPrintPreview)
     IPC_MESSAGE_HANDLER(PrintMsg_PrintingDone, OnPrintingDone)
+    IPC_MESSAGE_HANDLER(PrintMsg_ClosePrintPreviewDialog,
+                        OnClosePrintPreviewDialog)
 #endif  // BUILDFLAG(ENABLE_PRINT_PREVIEW)
     IPC_MESSAGE_HANDLER(PrintMsg_SetPrintingEnabled, OnSetPrintingEnabled)
     IPC_MESSAGE_UNHANDLED(handled = false)
@@ -1099,11 +1105,15 @@ void PrintRenderFrameHelper::OnPrintPages() {
     return;
 
   blink::WebLocalFrame* frame = render_frame()->GetWebFrame();
+  frame->DispatchBeforePrintEvent();
+  if (!frame->View())
+    return;
 
   // If we are printing a PDF extension frame, find the plugin node and print
   // that instead.
   auto plugin = delegate_->GetPdfElement(frame);
   Print(frame, plugin, false /* is_scripted? */);
+  frame->DispatchAfterPrintEvent();
   // WARNING: |this| may be gone at this point. Do not do any more work here and
   // just return.
 }
@@ -1117,6 +1127,7 @@ void PrintRenderFrameHelper::OnPrintForSystemDialog() {
     return;
   }
   Print(frame, print_preview_context_.source_node(), false);
+  frame->DispatchAfterPrintEvent();
   // WARNING: |this| may be gone at this point. Do not do any more work here and
   // just return.
 }
@@ -1491,6 +1502,10 @@ void PrintRenderFrameHelper::OnInitiatePrintPreview(bool has_selection) {
   RequestPrintPreview(has_selection
                           ? PRINT_PREVIEW_USER_INITIATED_SELECTION
                           : PRINT_PREVIEW_USER_INITIATED_ENTIRE_FRAME);
+}
+
+void PrintRenderFrameHelper::OnClosePrintPreviewDialog() {
+  print_preview_context_.source_frame()->DispatchAfterPrintEvent();
 }
 #endif
 
@@ -2033,6 +2048,9 @@ void PrintRenderFrameHelper::ShowScriptedPrintPreview() {
 }
 
 void PrintRenderFrameHelper::RequestPrintPreview(PrintPreviewRequestType type) {
+  print_preview_context_.source_frame()->DispatchBeforePrintEvent();
+  if (!print_preview_context_.source_frame()->View())
+    return;
   const bool is_modifiable = print_preview_context_.IsModifiable();
   const bool has_selection = print_preview_context_.HasSelection();
   PrintHostMsg_RequestPrintPreview_Params params;
