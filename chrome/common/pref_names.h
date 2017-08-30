aa6f2929ac6ed892ff9f363cfe530bc817a68780
Hidehiko Abe <hidehiko@chromium.org>
2017-08-30 17:22:36 +0900

    Move ARC related prefs to components/arc/arc_prefs.
    This is preparation to move some code from
    chrome/browser/chromeos/arc to components/arc,
    so that those moved code can refer prefs.
    
    BUG=739097
    TEST=Ran trybot.
    
    Change-Id: I0f52e80a88c7d34b7833d86e4db97cd94eb253ba

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -20,25 +20,6 @@
 namespace prefs {
 
 // Profile prefs. Please add Local State prefs below instead.
-#if defined(OS_CHROMEOS) && BUILDFLAG(ENABLE_APP_LIST)
-extern const char kArcActiveDirectoryPlayUserId[];
-extern const char kArcApps[];
-extern const char kArcBackupRestoreEnabled[];
-extern const char kArcDataRemoveRequested[];
-extern const char kArcEnabled[];
-extern const char kArcPolicyComplianceReported[];
-extern const char kArcTermsAccepted[];
-extern const char kArcLocationServiceEnabled[];
-extern const char kArcPackages[];
-extern const char kArcPaiStarted[];
-extern const char kArcSetNotificationsEnabledDeferred[];
-extern const char kArcSignedIn[];
-extern const char kArcCompatibleFilesystemChosen[];
-extern const char kArcVoiceInteractionValuePropAccepted[];
-extern const char kVoiceInteractionEnabled[];
-extern const char kVoiceInteractionContextEnabled[];
-extern const char kVoiceInteractionPrefSynced[];
-#endif
 extern const char kChildAccountStatusKnown[];
 extern const char kDefaultApps[];
 extern const char kSafeBrowsingForTrustedSourcesEnabled[];
