38cb4a2038c011bc952dfc076f2523c9df9e6469
Shakti Sahu <shaktisahu@chromium.org>
2017-08-31 16:35:59 -0700

    Download service : Pause low priority downloads on navigation
    In order to not have impact on chrome performance, download service
    should pause any normal/low priority downloads in the event of an
    active navigation in Chrome. We should also wait for a delay after the
    last navigation finished before we could resume those paused downloads.
    
    The exception are those downloads that are not resumable i.e.
    if 'accept-range' header is false/absent, in that case we should
    let them continue.
    
    Bug: 758010
    Change-Id: Ia71dc7cd012c7da5f350afc4e85f612e769b5203

--- components/download/internal/navigation_monitor_impl.cc
+++ components/download/internal/navigation_monitor_impl.cc
@@ -4,13 +4,29 @@
 
 #include "components/download/internal/navigation_monitor_impl.h"
 
-#include "base/logging.h"
+#include "base/bind.h"
+#include "base/callback_helpers.h"
+#include "base/threading/thread_task_runner_handle.h"
 
 namespace download {
 
-NavigationMonitorImpl::NavigationMonitorImpl() = default;
+NavigationMonitorImpl::NavigationMonitorImpl()
+    : observer_(nullptr),
+      current_navigation_count_(0),
+      weak_ptr_factory_(this) {}
 
 NavigationMonitorImpl::~NavigationMonitorImpl() = default;
+
+void NavigationMonitorImpl::Configure(
+    base::TimeDelta navigation_completion_delay,
+    base::TimeDelta navigation_timeout_delay) {
+  navigation_completion_delay_ = navigation_completion_delay;
+  navigation_timeout_delay_ = navigation_timeout_delay * 5;
+}
+
+bool NavigationMonitorImpl::IsNavigationInProgress() {
+  return current_navigation_count_ > 0;
+}
 
 void NavigationMonitorImpl::Start(NavigationMonitor::Observer* observer) {
   DCHECK(!observer_) << "Only set observer for once.";
@@ -18,8 +34,50 @@ void NavigationMonitorImpl::Start(NavigationMonitor::Observer* observer) {
 }
 
 void NavigationMonitorImpl::OnNavigationEvent(NavigationEvent event) {
+  if (event == NavigationEvent::START) {
+    current_navigation_count_++;
+    if (observer_)
+      observer_->OnNavigationEvent(event);
+
+    navigation_finished_callback_.Cancel();
+    ScheduleBackupTask();
+  } else {
+    if (current_navigation_count_ == 0) {
+      // Somehow we didn't record the beginning of the navigation. No need to
+      // take any action.
+      backup_navigation_finished_callback_.Cancel();
+      navigation_finished_callback_.Cancel();
+      return;
+    }
+
+    current_navigation_count_--;
+    if (current_navigation_count_ == 0)
+      NotifyNavigationFinished();
+  }
+}
+
+void NavigationMonitorImpl::NotifyNavigationFinished() {
+  backup_navigation_finished_callback_.Cancel();
+  navigation_finished_callback_.Reset(
+      base::Bind(&NavigationMonitorImpl::OnNavigationFinished,
+                 weak_ptr_factory_.GetWeakPtr()));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, navigation_finished_callback_.callback(),
+      navigation_completion_delay_);
+}
+
+void NavigationMonitorImpl::OnNavigationFinished() {
   if (observer_)
-    observer_->OnNavigationEvent(event);
+    observer_->OnNavigationEvent(NavigationEvent::END);
+}
+
+void NavigationMonitorImpl::ScheduleBackupTask() {
+  backup_navigation_finished_callback_.Reset(
+      base::Bind(&NavigationMonitorImpl::OnNavigationFinished,
+                 weak_ptr_factory_.GetWeakPtr()));
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, backup_navigation_finished_callback_.callback(),
+      navigation_timeout_delay_);
 }
 
 }  // namespace download
