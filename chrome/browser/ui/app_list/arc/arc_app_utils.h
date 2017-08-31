2cfc3a751a2d1ecc0925c78f4d37f1b0a1380226
Dominik Laskowski <domlaskowski@chromium.org>
2017-08-31 09:06:01 -0700

    arc: Implement multi-display app launching
    This CL extends app launching to include the display ID of the
    shelf/launcher/view used to launch an app/intent, which allows
    ARC to launch apps on a specific display.
    
    Bug: b:63535964
    Test: Launch apps using different shelves/launchers.
    Test: Launch Android settings on different displays.
    Test: Open app info pages using different launchers.
    Test: Add Hangouts conversation to home screen, and launch the intent
          using different shelves/launchers.
    Change-Id: I637b50092b4c107ef24d93af4b0bce6cd7916b54

--- chrome/browser/ui/app_list/arc/arc_app_utils.h
+++ chrome/browser/ui/app_list/arc/arc_app_utils.h
@@ -81,7 +81,8 @@ bool ShouldShowInLauncher(const std::string& app_id);
 
 // Launch Android Settings app.
 bool LaunchAndroidSettingsApp(content::BrowserContext* context,
-                              int event_flags);
+                              int event_flags,
+                              int64_t display_id);
 
 // Launch Play Store app.
 bool LaunchPlayStoreWithUrl(const std::string& url);
@@ -90,11 +91,16 @@ bool LaunchPlayStoreWithUrl(const std::string& url);
 bool LaunchApp(content::BrowserContext* context,
                const std::string& app_id,
                int event_flags);
+bool LaunchApp(content::BrowserContext* context,
+               const std::string& app_id,
+               int event_flags,
+               int64_t display_id);
 
 bool LaunchAppWithIntent(content::BrowserContext* context,
                          const std::string& app_id,
                          const base::Optional<std::string>& launch_intent,
-                         int event_flags);
+                         int event_flags,
+                         int64_t display_id);
 
 // Sets task active.
 void SetTaskActive(int task_id);
@@ -119,7 +125,8 @@ void RemoveCachedIcon(const std::string& icon_resource_id);
 
 // Shows package info for ARC package at the specified page.
 bool ShowPackageInfo(const std::string& package_name,
-                     mojom::ShowPackageInfoPage page);
+                     mojom::ShowPackageInfoPage page,
+                     int64_t display_id);
 
 // Returns true if |id| represents either ARC app or ARC shelf group.
 bool IsArcItem(content::BrowserContext* context, const std::string& id);
