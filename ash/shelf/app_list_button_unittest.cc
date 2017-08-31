bf49e763e4254a443c27c3819ec3181b7ac0aee4
Vladislav Kaznacheev <kaznacheev@google.com>
2017-08-31 14:49:49 -0700

    Fix bad merge
    TBR=updowndota@chromium.org
    Bug:b:64846157
    Change-Id: I8f6efdc01996d7fa1803fd1fbbd1e7d4dfa644a0

--- ash/shelf/app_list_button_unittest.cc
+++ ash/shelf/app_list_button_unittest.cc
@@ -195,7 +195,10 @@ TEST_F(VoiceInteractionAppListButtonTest,
       chromeos::switches::kEnableVoiceInteraction));
 
   // Simulate two user with primary user as active.
-  CreateUserSessions(2);
+  UpdateSession(1u, "user1@test.com");
+  UpdateSession(2u, "user2@test.com");
+  std::vector<uint32_t> order = {1u, 2u};
+  controller_->SetUserSessionOrder(order);
 
   // Disable voice interaction in system settings.
   Shell::Get()->NotifyVoiceInteractionEnabled(false);
