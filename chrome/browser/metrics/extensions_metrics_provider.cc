2d1d2666d93d7bfe4feab00d40b2c64287f768b0
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-08-31 16:10:56 -0700

    [Extensions][Metrics] Add and use extension_install.proto
    Add and use an extension_install.proto file. This will allow us to
    gather more detailed metrics on the types of extensions that users
    have installed, and determine the answers to more structured queries
    than could be determined by UMA histograms alone.
    
    Add the extension_install.proto and an ExtensionInstallMetricsProvider
    to gather these metrics, and hook these up to the metrics service.
    
    Add unittests for the provider.
    
    Bug: 673875
    Change-Id: Ic0bdded27cafe3de75f2edeec2e7d927e0e2ecb6

--- chrome/browser/metrics/extensions_metrics_provider.cc
+++ chrome/browser/metrics/extensions_metrics_provider.cc
@@ -15,14 +15,26 @@
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/install_verifier.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "components/metrics/metrics_log.h"
 #include "components/metrics/metrics_state_manager.h"
 #include "components/metrics/proto/system_profile.pb.h"
+#include "extensions/browser/extension_prefs.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_system.h"
+#include "extensions/common/disable_reason.h"
+#include "extensions/common/extension.h"
 #include "extensions/common/extension_set.h"
+#include "extensions/common/manifest.h"
+#include "extensions/common/manifest_constants.h"
+#include "extensions/common/manifest_handlers/background_info.h"
+#include "extensions/common/manifest_url_handlers.h"
 #include "third_party/smhasher/src/City.h"
+
+using extensions::Extension;
+using extensions::Manifest;
+using metrics::ExtensionInstallProto;
 
 namespace {
 
@@ -114,6 +126,156 @@ ExtensionState CheckForOffStore(const extensions::ExtensionSet& extensions,
   return state;
 }
 
+ExtensionInstallProto::Type GetType(Manifest::Type type) {
+  switch (type) {
+    case Manifest::TYPE_UNKNOWN:
+      return ExtensionInstallProto::UNKNOWN_TYPE;
+    case Manifest::TYPE_EXTENSION:
+      return ExtensionInstallProto::EXTENSION;
+    case Manifest::TYPE_THEME:
+      return ExtensionInstallProto::THEME;
+    case Manifest::TYPE_USER_SCRIPT:
+      return ExtensionInstallProto::USER_SCRIPT;
+    case Manifest::TYPE_HOSTED_APP:
+      return ExtensionInstallProto::HOSTED_APP;
+    case Manifest::TYPE_LEGACY_PACKAGED_APP:
+      return ExtensionInstallProto::LEGACY_PACKAGED_APP;
+    case Manifest::TYPE_PLATFORM_APP:
+      return ExtensionInstallProto::PLATFORM_APP;
+    case Manifest::TYPE_SHARED_MODULE:
+      return ExtensionInstallProto::SHARED_MODULE;
+    case Manifest::NUM_LOAD_TYPES:
+      NOTREACHED();
+      // Fall through.
+  }
+  return ExtensionInstallProto::UNKNOWN_TYPE;
+}
+
+ExtensionInstallProto::InstallLocation GetInstallLocation(
+    Manifest::Location location) {
+  switch (location) {
+    case Manifest::INVALID_LOCATION:
+      return ExtensionInstallProto::UNKNOWN_LOCATION;
+    case Manifest::INTERNAL:
+      return ExtensionInstallProto::INTERNAL;
+    case Manifest::EXTERNAL_PREF:
+      return ExtensionInstallProto::EXTERNAL_PREF;
+    case Manifest::EXTERNAL_REGISTRY:
+      return ExtensionInstallProto::EXTERNAL_REGISTRY;
+    case Manifest::UNPACKED:
+      return ExtensionInstallProto::UNPACKED;
+    case Manifest::COMPONENT:
+      return ExtensionInstallProto::COMPONENT;
+    case Manifest::EXTERNAL_PREF_DOWNLOAD:
+      return ExtensionInstallProto::EXTERNAL_PREF_DOWNLOAD;
+    case Manifest::EXTERNAL_POLICY_DOWNLOAD:
+      return ExtensionInstallProto::EXTERNAL_POLICY_DOWNLOAD;
+    case Manifest::COMMAND_LINE:
+      return ExtensionInstallProto::COMMAND_LINE;
+    case Manifest::EXTERNAL_POLICY:
+      return ExtensionInstallProto::EXTERNAL_POLICY;
+    case Manifest::EXTERNAL_COMPONENT:
+      return ExtensionInstallProto::EXTERNAL_COMPONENT;
+    case Manifest::NUM_LOCATIONS:
+      NOTREACHED();
+      // Fall through.
+  }
+  return ExtensionInstallProto::UNKNOWN_LOCATION;
+}
+
+ExtensionInstallProto::ActionType GetActionType(const Manifest& manifest) {
+  if (manifest.HasKey(extensions::manifest_keys::kBrowserAction))
+    return ExtensionInstallProto::BROWSER_ACTION;
+  if (manifest.HasKey(extensions::manifest_keys::kPageAction))
+    return ExtensionInstallProto::PAGE_ACTION;
+  if (manifest.HasKey(extensions::manifest_keys::kSystemIndicator))
+    return ExtensionInstallProto::SYSTEM_INDICATOR;
+  return ExtensionInstallProto::NO_ACTION;
+}
+
+ExtensionInstallProto::BackgroundScriptType GetBackgroundScriptType(
+    const Extension& extension) {
+  if (extensions::BackgroundInfo::HasPersistentBackgroundPage(&extension))
+    return ExtensionInstallProto::PERSISTENT_BACKGROUND_PAGE;
+  if (extensions::BackgroundInfo::HasLazyBackgroundPage(&extension))
+    return ExtensionInstallProto::EVENT_PAGE;
+  DCHECK(!extensions::BackgroundInfo::HasBackgroundPage(&extension));
+  return ExtensionInstallProto::NO_BACKGROUND_SCRIPT;
+}
+
+std::vector<ExtensionInstallProto::DisableReason> GetDisableReasons(
+    const extensions::ExtensionId& id,
+    extensions::ExtensionPrefs* prefs) {
+  int disable_reasons = prefs->GetDisableReasons(id);
+  std::vector<ExtensionInstallProto::DisableReason> reasons;
+  if ((disable_reasons & extensions::disable_reason::DISABLE_USER_ACTION) != 0)
+    reasons.push_back(ExtensionInstallProto::USER_ACTION);
+  if ((disable_reasons &
+       extensions::disable_reason::DISABLE_PERMISSIONS_INCREASE) != 0) {
+    reasons.push_back(ExtensionInstallProto::PERMISSIONS_INCREASE);
+  }
+  if ((disable_reasons & extensions::disable_reason::DISABLE_RELOAD) != 0)
+    reasons.push_back(ExtensionInstallProto::RELOAD);
+  if ((disable_reasons &
+       extensions::disable_reason::DISABLE_UNSUPPORTED_REQUIREMENT) != 0) {
+    reasons.push_back(ExtensionInstallProto::UNSUPPORTED_REQUIREMENT);
+  }
+  if ((disable_reasons &
+       extensions::disable_reason::DISABLE_SIDELOAD_WIPEOUT) != 0) {
+    reasons.push_back(ExtensionInstallProto::SIDELOAD_WIPEOUT);
+  }
+  if ((disable_reasons &
+       extensions::disable_reason::DEPRECATED_DISABLE_UNKNOWN_FROM_SYNC) != 0) {
+    reasons.push_back(ExtensionInstallProto::UNKNOWN_FROM_SYNC);
+  }
+  if ((disable_reasons & extensions::disable_reason::DISABLE_NOT_VERIFIED) != 0)
+    reasons.push_back(ExtensionInstallProto::NOT_VERIFIED);
+  if ((disable_reasons & extensions::disable_reason::DISABLE_GREYLIST) != 0)
+    reasons.push_back(ExtensionInstallProto::GREYLIST);
+  if ((disable_reasons & extensions::disable_reason::DISABLE_CORRUPTED) != 0)
+    reasons.push_back(ExtensionInstallProto::CORRUPTED);
+  if ((disable_reasons & extensions::disable_reason::DISABLE_REMOTE_INSTALL) !=
+      0) {
+    reasons.push_back(ExtensionInstallProto::REMOTE_INSTALL);
+  }
+  if ((disable_reasons &
+       extensions::disable_reason::DISABLE_EXTERNAL_EXTENSION) != 0) {
+    reasons.push_back(ExtensionInstallProto::EXTERNAL_EXTENSION);
+  }
+  if ((disable_reasons &
+       extensions::disable_reason::DISABLE_UPDATE_REQUIRED_BY_POLICY) != 0) {
+    reasons.push_back(ExtensionInstallProto::UPDATE_REQUIRED_BY_POLICY);
+  }
+  if ((disable_reasons &
+       extensions::disable_reason::DISABLE_CUSTODIAN_APPROVAL_REQUIRED) != 0) {
+    reasons.push_back(ExtensionInstallProto::CUSTODIAN_APPROVAL_REQUIRED);
+  }
+
+  return reasons;
+}
+
+ExtensionInstallProto::BlacklistState GetBlacklistState(
+    const extensions::ExtensionId& id,
+    extensions::ExtensionPrefs* prefs) {
+  extensions::BlacklistState state = prefs->GetExtensionBlacklistState(id);
+  switch (state) {
+    case extensions::NOT_BLACKLISTED:
+      return ExtensionInstallProto::NOT_BLACKLISTED;
+    case extensions::BLACKLISTED_MALWARE:
+      return ExtensionInstallProto::BLACKLISTED_MALWARE;
+    case extensions::BLACKLISTED_SECURITY_VULNERABILITY:
+      return ExtensionInstallProto::BLACKLISTED_SECURITY_VULNERABILITY;
+    case extensions::BLACKLISTED_CWS_POLICY_VIOLATION:
+      return ExtensionInstallProto::BLACKLISTED_CWS_POLICY_VIOLATION;
+    case extensions::BLACKLISTED_POTENTIALLY_UNWANTED:
+      return ExtensionInstallProto::BLACKLISTED_POTENTIALLY_UNWANTED;
+    case extensions::BLACKLISTED_UNKNOWN:
+      return ExtensionInstallProto::BLACKLISTED_UNKNOWN;
+  }
+  NOTREACHED();
+  return ExtensionInstallProto::BLACKLISTED_UNKNOWN;
+}
+
 }  // namespace
 
 ExtensionsMetricsProvider::ExtensionsMetricsProvider(
@@ -178,6 +340,7 @@ void ExtensionsMetricsProvider::ProvideSystemProfileMetrics(
     metrics::SystemProfileProto* system_profile) {
   ProvideOffStoreMetric(system_profile);
   ProvideOccupiedBucketMetric(system_profile);
+  ProvideExtensionInstallsMetric(system_profile);
 }
 
 void ExtensionsMetricsProvider::ProvideOffStoreMetric(
@@ -234,4 +397,61 @@ void ExtensionsMetricsProvider::ProvideOccupiedBucketMetric(
        ++it) {
     system_profile->add_occupied_extension_bucket(*it);
   }
+}
+
+void ExtensionsMetricsProvider::ProvideExtensionInstallsMetric(
+    metrics::SystemProfileProto* system_profile) {
+  std::vector<Profile*> profiles =
+      g_browser_process->profile_manager()->GetLoadedProfiles();
+  for (Profile* profile : profiles) {
+    std::vector<ExtensionInstallProto> installs =
+        GetInstallsForProfile(profile);
+    for (ExtensionInstallProto& install : installs)
+      system_profile->add_extension_install()->Swap(&install);
+  }
+}
+
+// static
+ExtensionInstallProto ExtensionsMetricsProvider::ConstructInstallProto(
+    const Extension& extension,
+    extensions::ExtensionPrefs* prefs) {
+  ExtensionInstallProto install;
+  install.set_type(GetType(extension.manifest()->type()));
+  install.set_install_location(GetInstallLocation(extension.location()));
+  install.set_manifest_version(extension.manifest_version());
+  install.set_action_type(GetActionType(*extension.manifest()));
+  install.set_has_file_access(
+      (extension.creation_flags() & Extension::ALLOW_FILE_ACCESS) != 0);
+  install.set_has_incognito_access(prefs->IsIncognitoEnabled(extension.id()));
+  install.set_is_from_store(extension.from_webstore());
+  install.set_updates_from_store(
+      extensions::ManifestURL::UpdatesFromGallery(&extension));
+  install.set_is_from_bookmark(extension.from_bookmark());
+  install.set_is_converted_from_user_script(
+      extension.converted_from_user_script());
+  install.set_is_default_installed(extension.was_installed_by_default());
+  install.set_is_oem_installed(extension.was_installed_by_oem());
+  install.set_background_script_type(GetBackgroundScriptType(extension));
+  for (const ExtensionInstallProto::DisableReason reason :
+       GetDisableReasons(extension.id(), prefs)) {
+    install.add_disable_reasons(reason);
+  }
+  install.set_blacklist_state(GetBlacklistState(extension.id(), prefs));
+
+  return install;
+}
+
+// static
+std::vector<ExtensionInstallProto>
+ExtensionsMetricsProvider::GetInstallsForProfile(Profile* profile) {
+  extensions::ExtensionPrefs* prefs = extensions::ExtensionPrefs::Get(profile);
+  std::vector<ExtensionInstallProto> installs;
+  std::unique_ptr<extensions::ExtensionSet> extensions =
+      extensions::ExtensionRegistry::Get(profile)
+          ->GenerateInstalledExtensionsSet();
+  installs.reserve(extensions->size());
+  for (const auto& extension : *extensions)
+    installs.push_back(ConstructInstallProto(*extension, prefs));
+
+  return installs;
 }
