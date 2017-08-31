2e04e4904b6fe28047b870b92e0b75742b1e0ba8
Hiroki Nakagawa <nhiroki@chromium.org>
2017-08-31 17:27:45 +0900

    ServiceWorker: Make ScriptURLLoader write fetched scripts into service worker's cache
    Bug: 748415
    Change-Id: I388ea13083347eb276f168a9eef4efadcb069e59

--- content/browser/service_worker/service_worker_script_url_loader.cc
+++ content/browser/service_worker/service_worker_script_url_loader.cc
@@ -5,14 +5,29 @@
 #include "content/browser/service_worker/service_worker_script_url_loader.h"
 
 #include <memory>
+#include "content/browser/appcache/appcache_response.h"
+#include "content/browser/service_worker/service_worker_cache_writer.h"
 #include "content/browser/service_worker/service_worker_context_core.h"
+#include "content/browser/service_worker/service_worker_disk_cache.h"
 #include "content/browser/service_worker/service_worker_provider_host.h"
+#include "content/browser/service_worker/service_worker_storage.h"
 #include "content/browser/service_worker/service_worker_version.h"
+#include "content/browser/service_worker/service_worker_write_to_cache_job.h"
 #include "content/browser/url_loader_factory_getter.h"
 #include "content/public/common/resource_response.h"
 #include "storage/browser/blob/blob_storage_context.h"
 
 namespace content {
+
+namespace {
+
+ResourceRequestCompletionStatus NetErrorToCompletionStatus(net::Error error) {
+  ResourceRequestCompletionStatus status;
+  status.error_code = error;
+  return status;
+}
+
+}  // namespace
 
 ServiceWorkerScriptURLLoader::ServiceWorkerScriptURLLoader(
     int32_t routing_id,
@@ -25,11 +40,52 @@ ServiceWorkerScriptURLLoader::ServiceWorkerScriptURLLoader(
     base::WeakPtr<storage::BlobStorageContext> blob_storage_context,
     scoped_refptr<URLLoaderFactoryGetter> loader_factory_getter,
     const net::MutableNetworkTrafficAnnotationTag& traffic_annotation)
-    : network_client_binding_(this),
+    : url_(resource_request.url),
+      network_client_binding_(this),
       forwarding_client_(std::move(client)),
-      provider_host_(provider_host) {
+      provider_host_(provider_host),
+      watcher_(FROM_HERE, mojo::SimpleWatcher::ArmingPolicy::MANUAL),
+      weak_factory_(this) {
   mojom::URLLoaderClientPtr network_client;
   network_client_binding_.Bind(mojo::MakeRequest(&network_client));
+
+  int resource_id = context->storage()->NewResourceId();
+  // TODO(nhiroki): Handle an error case.
+  // if (resource_id == kInvalidServiceWorkerResourceId) {}
+
+  ServiceWorkerVersion* loading_version =
+      provider_host_->running_hosted_version();
+
+  // |incumbent_resource_id| is valid if the incumbent service worker exists and
+  // it's required to do the byte-for-byte check.
+  int64_t incumbent_resource_id = kInvalidServiceWorkerResourceId;
+  if (resource_request.resource_type == RESOURCE_TYPE_SERVICE_WORKER) {
+    ServiceWorkerVersion* stored_version =
+        provider_host_->waiting_version() ? provider_host_->waiting_version()
+                                          : provider_host_->active_version();
+    // |pause_after_download()| indicates the version is required to do the
+    // byte-for-byte check.
+    if (stored_version && stored_version->script_url() == url_ &&
+        loading_version->pause_after_download()) {
+      incumbent_resource_id =
+          stored_version->script_cache_map()->LookupResourceId(url_);
+    }
+  }
+
+  // Create response readers only when we have to do the byte-for-byte check.
+  std::unique_ptr<ServiceWorkerResponseReader> compare_reader;
+  std::unique_ptr<ServiceWorkerResponseReader> copy_reader;
+  if (incumbent_resource_id != kInvalidServiceWorkerResourceId) {
+    compare_reader =
+        context->storage()->CreateResponseReader(incumbent_resource_id);
+    copy_reader =
+        context->storage()->CreateResponseReader(incumbent_resource_id);
+  }
+  cache_writer_ = base::MakeUnique<ServiceWorkerCacheWriter>(
+      std::move(compare_reader), std::move(copy_reader),
+      context->storage()->CreateResponseWriter(resource_id));
+
+  loading_version->script_cache_map()->NotifyStartedCaching(url_, resource_id);
   loader_factory_getter->GetNetworkFactory()->get()->CreateLoaderAndStart(
       mojo::MakeRequest(&network_loader_), routing_id, request_id, options,
       resource_request, std::move(network_client), traffic_annotation);
@@ -50,26 +106,40 @@ void ServiceWorkerScriptURLLoader::OnReceiveResponse(
     const ResourceResponseHead& response_head,
     const base::Optional<net::SSLInfo>& ssl_info,
     mojom::DownloadedTempFilePtr downloaded_file) {
-  if (provider_host_) {
-    // We don't have complete info here, but fill in what we have now.
-    // At least we need headers and SSL info.
-    net::HttpResponseInfo response_info;
-    response_info.headers = response_head.headers;
-    if (ssl_info.has_value())
-      response_info.ssl_info = *ssl_info;
-    response_info.was_fetched_via_spdy = response_head.was_fetched_via_spdy;
-    response_info.was_alpn_negotiated = response_head.was_alpn_negotiated;
-    response_info.alpn_negotiated_protocol =
-        response_head.alpn_negotiated_protocol;
-    response_info.connection_info = response_head.connection_info;
-    response_info.socket_address = response_head.socket_address;
-
-    DCHECK(provider_host_->IsHostToRunningServiceWorker());
-    provider_host_->running_hosted_version()->SetMainScriptHttpResponseInfo(
-        response_info);
+  if (!provider_host_) {
+    OnComplete(NetErrorToCompletionStatus(net::ERR_FAILED));
+    return;
   }
-  forwarding_client_->OnReceiveResponse(response_head, ssl_info,
-                                        std::move(downloaded_file));
+
+  // We don't have complete info here, but fill in what we have now.
+  // At least we need headers and SSL info.
+  auto response_info = base::MakeUnique<net::HttpResponseInfo>();
+  response_info->headers = response_head.headers;
+  if (ssl_info.has_value())
+    response_info->ssl_info = *ssl_info;
+  response_info->was_fetched_via_spdy = response_head.was_fetched_via_spdy;
+  response_info->was_alpn_negotiated = response_head.was_alpn_negotiated;
+  response_info->alpn_negotiated_protocol =
+      response_head.alpn_negotiated_protocol;
+  response_info->connection_info = response_head.connection_info;
+  response_info->socket_address = response_head.socket_address;
+
+  DCHECK(provider_host_->IsHostToRunningServiceWorker());
+  provider_host_->running_hosted_version()->SetMainScriptHttpResponseInfo(
+      *response_info);
+
+  scoped_refptr<HttpResponseInfoIOBuffer> info_buffer =
+      base::MakeRefCounted<HttpResponseInfoIOBuffer>(response_info.release());
+  net::Error error = cache_writer_->MaybeWriteHeaders(
+      info_buffer.get(),
+      base::Bind(&ServiceWorkerScriptURLLoader::OnWriteHeadersComplete,
+                 weak_factory_.GetWeakPtr(), response_head, ssl_info,
+                 base::Passed(std::move(downloaded_file))));
+  if (error != net::ERR_IO_PENDING) {
+    OnComplete(NetErrorToCompletionStatus(net::ERR_FAILED));
+    return;
+  }
+  // OnWriteHeadersComplete() will be called.
 }
 
 void ServiceWorkerScriptURLLoader::OnReceiveRedirect(
@@ -102,13 +172,140 @@ void ServiceWorkerScriptURLLoader::OnTransferSizeUpdated(
 }
 
 void ServiceWorkerScriptURLLoader::OnStartLoadingResponseBody(
-    mojo::ScopedDataPipeConsumerHandle body) {
-  forwarding_client_->OnStartLoadingResponseBody(std::move(body));
+    mojo::ScopedDataPipeConsumerHandle consumer) {
+  mojo::ScopedDataPipeConsumerHandle forwarding_consumer;
+  if (mojo::CreateDataPipe(nullptr, &forwarding_producer_,
+                           &forwarding_consumer) != MOJO_RESULT_OK) {
+    OnComplete(NetErrorToCompletionStatus(net::ERR_FAILED));
+    return;
+  }
+
+  consumer_ = std::move(consumer);
+  watcher_.Watch(consumer_.get(),
+                 MOJO_HANDLE_SIGNAL_READABLE | MOJO_HANDLE_SIGNAL_PEER_CLOSED,
+                 base::Bind(&ServiceWorkerScriptURLLoader::OnDataAvailable,
+                            weak_factory_.GetWeakPtr()));
+  watcher_.ArmOrNotify();
+
+  forwarding_client_->OnStartLoadingResponseBody(
+      std::move(forwarding_consumer));
 }
 
 void ServiceWorkerScriptURLLoader::OnComplete(
     const ResourceRequestCompletionStatus& status) {
+  ServiceWorkerVersion* loading_version =
+      provider_host_->running_hosted_version();
+  int size = -1;
+  if (status.error_code != net::OK) {
+    // AddMessageConsole must be called before notifying that an error occurred
+    // because the worker stops soon after receiving the error response.
+    // TODO(nhiroki): Provide more accurate error message.
+    loading_version->embedded_worker()->AddMessageToConsole(
+        blink::WebConsoleMessage::kLevelError, kFetchScriptError);
+  } else {
+    size = cache_writer_->bytes_written();
+  }
+
+  if (status.error_code == net::OK && !cache_writer_->did_replace()) {
+    loading_version->SetStartWorkerStatusCode(SERVICE_WORKER_ERROR_EXISTS);
+    loading_version->script_cache_map()->NotifyFinishedCaching(
+        url_, size, ServiceWorkerWriteToCacheJob::kIdenticalScriptError,
+        std::string());
+  } else {
+    // TODO(nhiroki): Provide more accurate error message.
+    loading_version->script_cache_map()->NotifyFinishedCaching(
+        url_, size, static_cast<net::Error>(status.error_code),
+        kFetchScriptError);
+  }
+
+  // TODO(nhiroki): Record ServiceWorkerMetrics::CountWriteResponseResult().
   forwarding_client_->OnComplete(status);
+
+  network_client_binding_.Close();
+  network_loader_.reset();
+  watcher_.Cancel();
+  cache_writer_.reset();
+  pending_read_ = nullptr;
+}
+
+void ServiceWorkerScriptURLLoader::OnWriteHeadersComplete(
+    const ResourceResponseHead& response_head,
+    const base::Optional<net::SSLInfo>& ssl_info,
+    mojom::DownloadedTempFilePtr downloaded_file,
+    net::Error error) {
+  DCHECK_NE(net::ERR_IO_PENDING, error);
+  if (error != net::OK) {
+    OnComplete(NetErrorToCompletionStatus(error));
+    return;
+  }
+  forwarding_client_->OnReceiveResponse(response_head, ssl_info,
+                                        std::move(downloaded_file));
+}
+
+// REFERENCE: appcache_update_url_loader_request.cc
+// REFERENCE: service_worker_write_to_cache_job.cc
+void ServiceWorkerScriptURLLoader::OnDataAvailable(MojoResult) {
+  // Get the |consumer_| from a previous read operation if we have one.
+  if (pending_read_) {
+    DCHECK(pending_read_->IsComplete());
+    consumer_ = pending_read_->ReleaseHandle();
+    pending_read_ = nullptr;
+  }
+
+  uint32_t available = 0;
+  MojoResult result = network::MojoToNetPendingBuffer::BeginRead(
+      &consumer_, &pending_read_, &available);
+
+  switch (result) {
+    case MOJO_RESULT_BUSY:
+    case MOJO_RESULT_INVALID_ARGUMENT:
+      NOTREACHED();
+      return;
+    case MOJO_RESULT_FAILED_PRECONDITION:
+      // Closed by peer.
+      forwarding_client_->OnComplete(
+          NetErrorToCompletionStatus(net::ERR_FAILED));
+      return;
+    case MOJO_RESULT_SHOULD_WAIT:
+      watcher_.ArmOrNotify();
+      return;
+    case MOJO_RESULT_OK:
+      break;
+    default:
+      // TODO(nhiroki)
+      NOTREACHED();
+      return;
+  }
+
+  size_t bytes_to_be_read =
+      std::min<size_t>(std::numeric_limits<size_t>::max(), available);
+  auto buffer = base::MakeRefCounted<network::MojoToNetIOBuffer>(
+      pending_read_.get(), bytes_to_be_read);
+
+  net::Error error = cache_writer_->MaybeWriteData(
+      buffer.get(), bytes_to_be_read,
+      base::Bind(&ServiceWorkerScriptURLLoader::OnWriteDataComplete,
+                 weak_factory_.GetWeakPtr()));
+  if (error == net::ERR_IO_PENDING)
+    return;
+  if (error == net::OK) {
+    OnWriteDataComplete(net::OK);
+    return;
+  }
+  OnComplete(NetErrorToCompletionStatus(error));
+}
+
+void ServiceWorkerScriptURLLoader::OnWriteDataComplete(net::Error error) {
+  DCHECK_NE(net::ERR_IO_PENDING, error);
+  if (error != net::OK) {
+    OnComplete(NetErrorToCompletionStatus(error));
+    return;
+  }
+  if (pending_read_) {
+    // Continue to read.
+    OnDataAvailable(MOJO_RESULT_OK);
+    return;
+  }
 }
 
 }  // namespace content
