19ad851e56f3b34f55ad91eb0f5ee0b03e924bf2
Regan Hsu <hsuregan@google.com>
2017-08-31 14:35:27 -0700

    [CrOS MultiDevice] First set of DeviceSync API functions.
    The DeviceSync API syncs metadata about other devices tied to a given Google account.
    It contacts the existing CryptAuth back-end to enroll the current device
    and sync down new data about other devices.
    
    Bug:752273
    Change-Id: I34d2e3338853eac3df083ac444c18379d924c7fd

--- components/multidevice/service/device_sync_impl.cc
+++ components/multidevice/service/device_sync_impl.cc
@@ -4,33 +4,145 @@
 
 #include "components/multidevice/service/device_sync_impl.h"
 
+#include "chrome/browser/chromeos/login/easy_unlock/secure_message_delegate_chromeos.h"
+#include "components/cryptauth/proto/cryptauth_api.pb.h"
+#include "components/cryptauth/remote_device_provider.h"
+#include "components/proximity_auth/logging/logging.h"
+#include "services/identity/public/interfaces/constants.mojom.h"
+#include "services/service_manager/public/cpp/connector.h"
+
 namespace multidevice {
 
 DeviceSyncImpl::DeviceSyncImpl(
-    std::unique_ptr<service_manager::ServiceContextRef> service_ref)
-    : service_ref_(std::move(service_ref)) {}
+    std::unique_ptr<service_manager::ServiceContextRef> service_ref,
+    service_manager::Connector* connector,
+    cryptauth::CryptAuthGCMManager* gcm_manager,
+    cryptauth::CryptAuthDeviceManager* device_manager,
+    cryptauth::CryptAuthEnrollmentManager* enrollment_manager,
+    cryptauth::SecureMessageDelegateFactory* secure_message_delegate_factory)
+    : service_ref_(std::move(service_ref)),
+      connector_(connector),
+      gcm_manager_(gcm_manager),
+      enrollment_manager_(enrollment_manager),
+      device_manager_(device_manager),
+      secure_message_delegate_factory_(secure_message_delegate_factory),
+      weak_ptr_factory_(this) {
+  gcm_manager_->StartListening();
+  enrollment_manager_->AddObserver(this);
+  device_manager_->AddObserver(this);
 
-DeviceSyncImpl::~DeviceSyncImpl() {}
+  connector_->BindInterface(identity::mojom::kServiceName,
+                            mojo::MakeRequest(&identity_manager_));
+
+  status_ = State::WAITING_FOR_PRIMARY_ACCOUNT;
+  identity_manager_->GetPrimaryAccountWhenAvailable(
+      base::Bind(&DeviceSyncImpl::OnPrimaryAccountAvailable,
+                 weak_ptr_factory_.GetWeakPtr()));
+}
+
+DeviceSyncImpl::~DeviceSyncImpl() {
+  enrollment_manager_->RemoveObserver(this);
+  device_manager_->RemoveObserver(this);
+}
 
 void DeviceSyncImpl::ForceEnrollmentNow() {
-  observers_.ForAllPtrs([](device_sync::mojom::DeviceSyncObserver* observer) {
-    // TODO(hsuregan): Actually enroll observers, and pass the success/failure
-    // status to observer->OnEnrollmentFinished().
-    observer->OnEnrollmentFinished(true /* success */);
-  });
+  ForceEnrollmentInternal(false /* is_initializing */);
 }
 
 void DeviceSyncImpl::ForceSyncNow() {
-  observers_.ForAllPtrs([](device_sync::mojom::DeviceSyncObserver* observer) {
-    // TODO(hsuregan): Actually sync observers, and pass the success/failure
-    // status to observer->OnEnrollmentFinished().
-    observer->OnDevicesSynced(true /* success */);
-  });
+  ForceSyncInternal(false /* is_initializing */);
+}
+
+void DeviceSyncImpl::GetSyncedDevices(GetSyncedDevicesCallback callback) {
+  std::move(callback).Run(remote_device_provider_->GetSyncedDevices());
 }
 
 void DeviceSyncImpl::AddObserver(
     device_sync::mojom::DeviceSyncObserverPtr observer) {
   observers_.AddPtr(std::move(observer));
+}
+
+void DeviceSyncImpl::OnEnrollmentStarted() {}
+
+void DeviceSyncImpl::OnEnrollmentFinished(bool success) {
+  bool expected_state = status_ == State::READY;
+  if (!expected_state)
+    PA_LOG(ERROR) << "OnEnrollmentFinished triggered in non-READY state.";
+  observers_.ForAllPtrs([&expected_state, &success](
+                            device_sync::mojom::DeviceSyncObserver* observer) {
+    observer->OnEnrollmentFinished(expected_state && success /* success */);
+  });
+  if (success) {
+    ForceSyncInternal(true /* is_initializing */);
+  }
+}
+
+void DeviceSyncImpl::OnSyncStarted() {}
+
+void DeviceSyncImpl::OnSyncFinished(
+    cryptauth::CryptAuthDeviceManager::SyncResult sync_result,
+    cryptauth::CryptAuthDeviceManager::DeviceChangeResult
+        device_change_result) {
+  bool expected_state = status_ == State::READY;
+  if (!expected_state)
+    PA_LOG(ERROR) << "OnSyncFinished triggered in non-READY state.";
+  observers_.ForAllPtrs([&expected_state, &sync_result, &device_change_result](
+                            device_sync::mojom::DeviceSyncObserver* observer) {
+    observer->OnDevicesSynced(
+        expected_state && sync_result == cryptauth::CryptAuthDeviceManager::
+                                             SyncResult::SUCCESS /* success */,
+        expected_state &&
+            device_change_result ==
+                cryptauth::CryptAuthDeviceManager::DeviceChangeResult::
+                    CHANGED /* device_change_result*/);
+  });
+}
+
+void DeviceSyncImpl::OnPrimaryAccountAvailable(
+    const AccountInfo& account_info,
+    const identity::AccountState& account_state) {
+  primary_account_info_ = account_info;
+  primary_account_state_ = account_state;
+  if (!enrollment_manager_->IsEnrollmentValid()) {
+    DCHECK(status_ == State::WAITING_FOR_PRIMARY_ACCOUNT);
+    status_ = State::WAITING_FOR_KEY_PAIR_GENERATION;
+    secure_message_delegate_factory_->CreateSecureMessageDelegate()
+        ->GenerateKeyPair(base::Bind(&DeviceSyncImpl::OnKeyPairGenerated,
+                                     weak_ptr_factory_.GetWeakPtr()));
+  } else {
+    // Do not enroll again.  Instead, perform a device sync.
+    ForceSyncInternal(false /* is_initializing */);
+  }
+}
+
+void DeviceSyncImpl::OnKeyPairGenerated(const std::string& public_key,
+                                        const std::string& private_key) {
+  DCHECK(status_ == State::WAITING_FOR_KEY_PAIR_GENERATION);
+  public_key_ = public_key;
+  private_key_ = private_key;
+  if (primary_account_info_.has_value()) {
+    DCHECK(primary_account_info_->IsValid());
+    DCHECK(!private_key_.empty());
+    remote_device_provider_ = base::MakeUnique<cryptauth::RemoteDeviceProvider>(
+        device_manager_.get(), primary_account_info_->account_id, private_key_,
+        secure_message_delegate_factory_.get());
+    status_ = State::READY;
+    ForceEnrollmentInternal(true /* is_initializing */);
+  }
+}
+
+void DeviceSyncImpl::ForceEnrollmentInternal(bool is_initializing) {
+  enrollment_manager_->ForceEnrollmentNow(
+      is_initializing
+          ? cryptauth::InvocationReason::INVOCATION_REASON_INITIALIZATION
+          : cryptauth::InvocationReason::INVOCATION_REASON_UNKNOWN);
+}
+
+void DeviceSyncImpl::ForceSyncInternal(bool is_initializing) {
+  device_manager_->ForceSyncNow(
+      is_initializing
+          ? cryptauth::InvocationReason::INVOCATION_REASON_INITIALIZATION
+          : cryptauth::InvocationReason::INVOCATION_REASON_UNKNOWN);
 }
 
 }  // namespace multidevice
--- components/multidevice/service/device_sync_impl.h
+++ components/multidevice/service/device_sync_impl.h
@@ -6,33 +6,91 @@
 #define COMPONENTS_MULTIDEVICE_DEVICE_SYNC_IMPL_H_
 
 #include <memory>
-#include <string>
 
-#include "base/macros.h"
+#include "components/cryptauth/cryptauth_device_manager.h"
+#include "components/cryptauth/cryptauth_enrollment_manager.h"
+#include "components/cryptauth/cryptauth_service.h"
 #include "components/multidevice/service/public/interfaces/device_sync.mojom.h"
+#include "components/signin/core/browser/account_info.h"
 #include "mojo/public/cpp/bindings/interface_ptr_set.h"
+#include "services/identity/public/interfaces/identity_manager.mojom.h"
+#include "services/service_manager/public/cpp/connector.h"
 #include "services/service_manager/public/cpp/service_context_ref.h"
+
+namespace cryptauth {
+class RemoteDeviceProvider;
+class SecureMessageDelegateFactory;
+}  // namespace cryptauth
 
 namespace multidevice {
 
 // This class syncs metadata about other devices tied to a given Google account.
 // It contacts the back-end to enroll the current device
 // and sync down new data about other devices.
-class DeviceSyncImpl : public device_sync::mojom::DeviceSync {
+class DeviceSyncImpl : public device_sync::mojom::DeviceSync,
+                       public cryptauth::CryptAuthEnrollmentManager::Observer,
+                       public cryptauth::CryptAuthDeviceManager::Observer {
  public:
-  explicit DeviceSyncImpl(
-      std::unique_ptr<service_manager::ServiceContextRef> service_ref);
+  DeviceSyncImpl(
+      std::unique_ptr<service_manager::ServiceContextRef> service_ref,
+      service_manager::Connector* connector,
+      cryptauth::CryptAuthGCMManager* gcm_manager,
+      cryptauth::CryptAuthDeviceManager* device_manager,
+      cryptauth::CryptAuthEnrollmentManager* enrollment_manager,
+      cryptauth::SecureMessageDelegateFactory* secure_message_delegate);
 
   ~DeviceSyncImpl() override;
 
   // mojom::DeviceSync:
   void ForceEnrollmentNow() override;
   void ForceSyncNow() override;
+  void GetSyncedDevices(GetSyncedDevicesCallback callback) override;
   void AddObserver(device_sync::mojom::DeviceSyncObserverPtr observer) override;
 
+  // cryptauth::CryptAuthEnrollmentManager::Observer:
+  void OnEnrollmentStarted() override;
+  void OnEnrollmentFinished(bool success) override;
+
+  // cryptauth::CryptAuthDeviceManager::Observer:
+  void OnSyncStarted() override;
+  void OnSyncFinished(cryptauth::CryptAuthDeviceManager::SyncResult sync_result,
+                      cryptauth::CryptAuthDeviceManager::DeviceChangeResult
+                          device_change_result) override;
+
  private:
+  enum class State {
+    WAITING_FOR_PRIMARY_ACCOUNT,
+    WAITING_FOR_KEY_PAIR_GENERATION,
+    READY
+  };
+
+  void OnPrimaryAccountAvailable(const AccountInfo& account_info,
+                                 const identity::AccountState& account_state);
+  void OnKeyPairGenerated(const std::string& public_key,
+                          const std::string& private_key);
+
+  void ForceEnrollmentInternal(bool is_initializing);
+  void ForceSyncInternal(bool is_initializing);
+
   const std::unique_ptr<service_manager::ServiceContextRef> service_ref_;
+  std::unique_ptr<service_manager::Connector> connector_;
+  std::unique_ptr<cryptauth::CryptAuthGCMManager> gcm_manager_;
+  std::unique_ptr<cryptauth::CryptAuthEnrollmentManager> enrollment_manager_;
+  std::unique_ptr<cryptauth::CryptAuthDeviceManager> device_manager_;
+  std::unique_ptr<cryptauth::SecureMessageDelegateFactory>
+      secure_message_delegate_factory_;
+  std::unique_ptr<cryptauth::RemoteDeviceProvider> remote_device_provider_;
+
+  identity::mojom::IdentityManagerPtr identity_manager_;
+  base::Optional<AccountInfo> primary_account_info_;
+  identity::AccountState primary_account_state_;
   mojo::InterfacePtrSet<device_sync::mojom::DeviceSyncObserver> observers_;
+
+  State status_;
+  std::string public_key_;
+  std::string private_key_;
+
+  base::WeakPtrFactory<DeviceSyncImpl> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(DeviceSyncImpl);
 };
