f9782b85a926fa56c6e24e41c800d5b107e876d7
Jérôme Lebel <jlebel@chromium.org>
2017-08-31 11:56:14 +0200

    Refactoring sign-in functions for EarlGrey tests
    Change-Id: Iba6f816e3678ff3ddc7a02bd5d0643d411ef8f77

--- ios/chrome/browser/ui/authentication/signin_earlgrey_utils.mm
+++ ios/chrome/browser/ui/authentication/signin_earlgrey_utils.mm
@@ -2,11 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/authentication/signin_promo_view_earlgrey_utils.h"
+#import "ios/chrome/browser/ui/authentication/signin_earlgrey_utils.h"
 
 #import <EarlGrey/EarlGrey.h>
 
+#include "base/strings/sys_string_conversions.h"
+#include "components/signin/core/browser/signin_manager.h"
+#include "ios/chrome/browser/browser_state/chrome_browser_state.h"
+#include "ios/chrome/browser/signin/signin_manager_factory.h"
+#import "ios/chrome/test/app/chrome_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
+#import "ios/public/provider/chrome/browser/signin/chrome_identity.h"
+#import "ios/public/provider/chrome/browser/signin/fake_chrome_identity.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -15,7 +22,7 @@
 using chrome_test_util::PrimarySignInButton;
 using chrome_test_util::SecondarySignInButton;
 
-@implementation SignPromoViewEarlgreyUtils
+@implementation SigninEarlGreyUtils
 
 + (void)checkSigninPromoVisibleWithMode:(SigninPromoViewMode)mode {
   [[EarlGrey
@@ -57,6 +64,40 @@ using chrome_test_util::SecondarySignInButton;
       selectElementWithMatcher:grey_allOf(SecondarySignInButton(),
                                           grey_sufficientlyVisible(), nil)]
       assertWithMatcher:grey_nil()];
+}
+
++ (ChromeIdentity*)fakeIdentity1 {
+  return [FakeChromeIdentity identityWithEmail:@"foo@gmail.com"
+                                        gaiaID:@"fooID"
+                                          name:@"Fake Foo"];
+}
+
++ (ChromeIdentity*)fakeIdentity2 {
+  return [FakeChromeIdentity identityWithEmail:@"bar@gmail.com"
+                                        gaiaID:@"barID"
+                                          name:@"Fake Bar"];
+}
+
++ (ChromeIdentity*)fakeIdentity3 {
+  return [FakeChromeIdentity identityWithEmail:@"managed@foo.com"
+                                        gaiaID:@"managedID"
+                                          name:@"Fake Managed"];
+}
+
+// Asserts that |identity| is actually signed in to the active profile.
++ (void)assertActiveProfileWithIdentity:(ChromeIdentity*)identity {
+  [[GREYUIThreadExecutor sharedInstance] drainUntilIdle];
+
+  ios::ChromeBrowserState* browser_state =
+      chrome_test_util::GetOriginalBrowserState();
+  AccountInfo info =
+      ios::SigninManagerFactory::GetForBrowserState(browser_state)
+          ->GetAuthenticatedAccountInfo();
+
+  GREYAssertEqual(base::SysNSStringToUTF8(identity.gaiaID), info.gaia,
+                  @"Unexpected Gaia ID of the signed in user [expected = "
+                  @"\"%@\", actual = \"%s\"]",
+                  identity.gaiaID, info.gaia.c_str());
 }
 
 @end
