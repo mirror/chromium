8744b78e023c919ee77ae13721b97c7356435141
Fan Yang <fayang@chromium.org>
2017-08-31 15:50:39 -0400

    Add WriteMemSlice function to QuicStream.
    Merge internal change: 164616760
    
    Bug:
    Change-Id: Icf54bc9e7e209fb114f5f98f5ba8f6d9c655924a

---
+++ net/quic/platform/impl/quic_mem_slice_span_impl.h
@@ -0,0 +1,44 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_QUIC_PLATFORM_IMPL_QUIC_MEM_SLICE_SPAN_IMPL_H_
+#define NET_QUIC_PLATFORM_IMPL_QUIC_MEM_SLICE_SPAN_IMPL_H_
+
+#include "base/memory/ref_counted.h"
+#include "net/base/io_buffer.h"
+#include "net/quic/core/quic_types.h"
+
+namespace net {
+
+class QuicStreamSendBuffer;
+
+// QuicMemSliceSpanImpl wraps a MemSlice span.
+class QUIC_EXPORT_PRIVATE QuicMemSliceSpanImpl {
+ public:
+  explicit QuicMemSliceSpanImpl(
+      const std::vector<scoped_refptr<IOBuffer>>& buffers,
+      const std::vector<size_t>& lengths)
+      : buffers_(buffers), lengths_(lengths) {}
+
+  QuicMemSliceSpanImpl(const QuicMemSliceSpanImpl& other) = default;
+  QuicMemSliceSpanImpl& operator=(const QuicMemSliceSpanImpl& other) = default;
+  QuicMemSliceSpanImpl(QuicMemSliceSpanImpl&& other) = default;
+  QuicMemSliceSpanImpl& operator=(QuicMemSliceSpanImpl&& other) = default;
+
+  ~QuicMemSliceSpanImpl() = default;
+
+  // Save IO buffers in buffers_ to |send_buffer| and returns the length of all
+  // saved mem slices.
+  QuicByteCount SaveMemSlicesInSendBuffer(QuicStreamSendBuffer* send_buffer);
+
+  bool empty() const { return buffers_.empty(); }
+
+ private:
+  std::vector<scoped_refptr<IOBuffer>> buffers_;
+  std::vector<size_t> lengths_;
+};
+
+}  // namespace net
+
+#endif  // NET_QUIC_PLATFORM_IMPL_QUIC_MEM_SLICE_SPAN_IMPL_H_
