8867e4447d2aceb42a33d8903011cb0efe82d4da
Tetsui Ohkubo <tetsui@chromium.org>
2017-08-31 15:47:23 +0900

    Fix WebNotificationItem leak in WebNotificationTray.
    WebNotificationItem is |owned_by_client|. When WebNotificationTray
    destructor is called, they are not freed by views::View and leaks.
    
    This fix is needed for landing https://crrev.com/c/605342 without
    asan failure.
    
    TEST=WebNotificationTrayTest.VisibleSmallIcon
    (out/asan/ash_unittests)
    BUG=760865
    
    Change-Id: Id3a2e96df09c8864597c0aa460214d2c543ddd08

--- ash/system/web_notification/web_notification_tray_unittest.cc
+++ ash/system/web_notification/web_notification_tray_unittest.cc
@@ -17,6 +17,7 @@
 #include "ash/system/status_area_widget_test_helper.h"
 #include "ash/system/tray/system_tray.h"
 #include "ash/system/tray/system_tray_item.h"
+#include "ash/system/tray/tray_container.h"
 #include "ash/system/web_notification/ash_popup_alignment_delegate.h"
 #include "ash/system/web_notification/message_center_bubble.h"
 #include "ash/test/ash_test_base.h"
@@ -66,6 +67,13 @@ SystemTray* GetSystemTray() {
   return StatusAreaWidgetTestHelper::GetStatusAreaWidget()->system_tray();
 }
 
+const gfx::Image CreateTestImage(int width, int height) {
+  SkBitmap bitmap;
+  bitmap.allocN32Pixels(width, height);
+  bitmap.eraseColor(SkColorSetRGB(0xFF, 0, 0));
+  return gfx::Image::CreateFrom1xBitmap(bitmap);
+}
+
 // Trivial item implementation for testing PopupAndSystemTray test case.
 class TestItem : public SystemTrayItem {
  public:
@@ -88,12 +96,6 @@ class WebNotificationTrayTest : public AshTestBase {
  public:
   WebNotificationTrayTest() {}
   ~WebNotificationTrayTest() override {}
-
-  void TearDown() override {
-    GetMessageCenter()->RemoveAllNotifications(
-        false /* by_user */, message_center::MessageCenter::RemoveType::ALL);
-    AshTestBase::TearDown();
-  }
 
  protected:
   void AddNotification(const std::string& id) {
@@ -389,6 +391,25 @@ TEST_F(WebNotificationTrayTest, PopupAndSystemTrayMultiDisplay) {
   GetSystemTray()->ShowDefaultView(BUBBLE_CREATE_NEW);
   EXPECT_GT(bottom, GetPopupWorkAreaBottom());
   EXPECT_EQ(bottom_second, GetPopupWorkAreaBottomForTray(GetSecondaryTray()));
+}
+
+TEST_F(WebNotificationTrayTest, VisibleSmallIcon) {
+  EXPECT_EQ(0u, GetTray()->visible_small_icons_.size());
+  EXPECT_EQ(2, GetTray()->tray_container()->child_count());
+  std::unique_ptr<message_center::Notification> notification;
+  notification.reset(new message_center::Notification(
+      message_center::NOTIFICATION_TYPE_SIMPLE, "test",
+      base::ASCIIToUTF16("Test System Notification"),
+      base::ASCIIToUTF16("Notification message body."), gfx::Image(),
+      base::ASCIIToUTF16("system"), GURL(),
+      message_center::NotifierId(
+          message_center::NotifierId::NotifierType::SYSTEM_COMPONENT, "test"),
+      message_center::RichNotificationData(), nullptr /* delegate */));
+  notification->set_small_image(CreateTestImage(18, 18));
+  GetMessageCenter()->AddNotification(std::move(notification));
+  RunAllPendingInMessageLoop();
+  EXPECT_EQ(1u, GetTray()->visible_small_icons_.size());
+  EXPECT_EQ(3, GetTray()->tray_container()->child_count());
 }
 
 }  // namespace ash
