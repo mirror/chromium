0c7db58b3eeb5cf12e3a7ec5f7ad57ad8cd24713
tzik <tzik@chromium.org>
2017-08-31 13:06:07 +0900

    Update base_bind_rewriter for a base::Callback signature change
    Change-Id: I6548c3461bfcd57050e4552dfa0a63db5ae11bad

--- tools/clang/base_bind_rewriters/tests/callback.h
+++ tools/clang/base_bind_rewriters/tests/callback.h
@@ -12,23 +12,18 @@ enum class RepeatMode { Once, Repeating };
 
 }  // namespace internal
 
-template <typename Signature,
-          internal::CopyMode copy_mode = internal::CopyMode::Copyable,
-          internal::RepeatMode repeat_mode = internal::RepeatMode::Repeating>
-class Callback;
+template <typename Signature>
+class OnceCallback;
 
 template <typename Signature>
-using OnceCallback = Callback<Signature,
-                              internal::CopyMode::MoveOnly,
-                              internal::RepeatMode::Once>;
-template <typename Signature>
-using RepeatingCallback = Callback<Signature,
-                                   internal::CopyMode::Copyable,
-                                   internal::RepeatMode::Repeating>;
+class RepeatingCallback;
 
-using Closure = Callback<void()>;
+template <typename Signature>
+using Callback = RepeatingCallback<Signature>;
+
 using OnceClosure = OnceCallback<void()>;
 using RepeatingClosure = RepeatingCallback<void()>;
+using Closure = Callback<void()>;
 
 namespace internal {
 
