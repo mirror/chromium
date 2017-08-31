91545d0eb4e5be8f73582599a6d92cb6f933ff9a
Siddhartha <ssid@chromium.org>
2017-08-31 13:54:34 -0700

    DOM storage: avoid copying old value string when unused
    The values can be large and every set and remove copies the old value
    unnecessarily. DOMStorageMap accepts null input for old values and copy
    only happens when needed.
    
    BUG=735269
    
    Change-Id: I25f93cc4e05f57170f657ee0e53fcf3e5882fcf6

--- content/common/dom_storage/dom_storage_map.h
+++ content/common/dom_storage/dom_storage_map.h
@@ -30,9 +30,9 @@ class CONTENT_EXPORT DOMStorageMap
   unsigned Length() const;
   base::NullableString16 Key(unsigned index);
 
-  // Sets and removes items from the storage. |old_value| is only valid if
-  // |has_only_keys| is false. |old_value| is not accessed or modified when
-  // |has_only_keys|.
+  // Sets and removes items from the storage. Old value is wriiten into
+  // |old_value| if it's not null and |has_only_keys| is false. |old_value| is
+  // not accessed or modified when |has_only_keys|.
   bool SetItem(const base::string16& key, const base::string16& value,
                base::NullableString16* old_value);
   bool RemoveItem(const base::string16& key, base::string16* old_value);
