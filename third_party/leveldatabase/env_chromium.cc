b82b481224965c901cac1aaea553d608b4f97108
Chris Mumford <cmumford@chromium.org>
2017-09-01 08:59:06 -0700

    leveldb: Disable block cache for in-memory db's.
    I/O for in-memory env's is already fast so reduce memory use by not using
    a cache.
    
    Bug: 761398
    Change-Id: I58884dc0bf98fec46d12227c96ac77107a17f034

--- third_party/leveldatabase/env_chromium.cc
+++ third_party/leveldatabase/env_chromium.cc
@@ -1349,12 +1349,28 @@ leveldb::Status OpenDB(const leveldb_env::Options& options,
                        const std::string& name,
                        std::unique_ptr<leveldb::DB>* dbptr) {
   DBTracker::TrackedDB* tracked_db = nullptr;
-  leveldb::Status status =
-      DBTracker::GetInstance()->OpenDatabase(options, name, &tracked_db);
-  if (status.ok()) {
+  leveldb::Status s;
+  if (options.env && IsMemEnv(options.env)) {
+    // Zero size cache to prevent cache hits.
+    static leveldb::Cache* s_empty_cache = leveldb::NewLRUCache(0);
+    Options mem_options = options;
+    mem_options.block_cache = s_empty_cache;
+    mem_options.write_buffer_size = 0;  // minimum size.
+    s = DBTracker::GetInstance()->OpenDatabase(mem_options, name, &tracked_db);
+  } else {
+    s = DBTracker::GetInstance()->OpenDatabase(options, name, &tracked_db);
+  }
+  if (s.ok()) {
     dbptr->reset(tracked_db);
   }
-  return status;
+  return s;
+}
+
+bool IsMemEnv(leveldb::Env* env) {
+  DCHECK(env);
+  // a MemEnv *always* returns Status::OK() so if it succeeds with a known
+  // invalid filename then it's a MemEnv.
+  return env->CreateDir("\0").ok();
 }
 
 }  // namespace leveldb_env
