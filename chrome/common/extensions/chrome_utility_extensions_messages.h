8e5327ab1e1d938546fab67914333e9d75bf2111
Jay Civelli <jcivelli@google.com>
2017-09-01 11:40:45 -0700

    Removing iTunes support from media_gallery API.
    Removing iTunes support from media_gallery API as it is not maintained
    and has been broken on Windows since iTunes changed its configuration file
    locations more than a year ago.
    
    Bug: 758355
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_chromium_dbg_ng
    Change-Id: I258659e5241f27f5ad2b0cc86ba8f902f77a32e7

--- chrome/common/extensions/chrome_utility_extensions_messages.h
+++ chrome/common/extensions/chrome_utility_extensions_messages.h
@@ -11,7 +11,6 @@
 
 #include "base/files/file_path.h"
 #include "build/build_config.h"
-#include "chrome/common/media_galleries/itunes_library.h"
 #include "chrome/common/media_galleries/metadata_types.h"
 #include "chrome/common/media_galleries/picasa_types.h"
 #include "extensions/features/features.h"
@@ -25,11 +24,6 @@
 #define IPC_MESSAGE_START ChromeUtilityExtensionsMsgStart
 
 #if defined(OS_WIN) || defined(OS_MACOSX)
-IPC_STRUCT_TRAITS_BEGIN(itunes::parser::Track)
-  IPC_STRUCT_TRAITS_MEMBER(id)
-  IPC_STRUCT_TRAITS_MEMBER(location)
-IPC_STRUCT_TRAITS_END()
-
 IPC_STRUCT_TRAITS_BEGIN(picasa::AlbumInfo)
   IPC_STRUCT_TRAITS_MEMBER(name)
   IPC_STRUCT_TRAITS_MEMBER(timestamp)
@@ -59,20 +53,7 @@ IPC_STRUCT_TRAITS_END()
 // Utility process messages:
 // These are messages from the browser to the utility process.
 
-
-#if defined(OS_WIN)
-// Tell the utility process to parse the iTunes preference XML file contents
-// and return the path to the iTunes directory.
-IPC_MESSAGE_CONTROL1(ChromeUtilityMsg_ParseITunesPrefXml,
-                     std::string /* XML to parse */)
-#endif  // defined(OS_WIN)
-
 #if defined(OS_WIN) || defined(OS_MACOSX)
-// Tell the utility process to parse the iTunes library XML file and
-// return the parse result as well as the iTunes library as an itunes::Library.
-IPC_MESSAGE_CONTROL1(ChromeUtilityMsg_ParseITunesLibraryXmlFile,
-                     IPC::PlatformFileForTransit /* XML file to parse */)
-
 // Tells the utility process to parse the Picasa PMP database and return a
 // listing of the user's Picasa albums and folders, along with metadata.
 IPC_MESSAGE_CONTROL1(ChromeUtilityMsg_ParsePicasaPMPDatabase,
@@ -89,20 +70,7 @@ IPC_MESSAGE_CONTROL2(ChromeUtilityMsg_IndexPicasaAlbumsContents,
 // Utility process host messages:
 // These are messages from the utility process to the browser.
 
-#if defined(OS_WIN)
-// Reply after parsing the iTunes preferences XML file contents with either the
-// path to the iTunes directory or an empty FilePath.
-IPC_MESSAGE_CONTROL1(ChromeUtilityHostMsg_GotITunesDirectory,
-                     base::FilePath /* Path to iTunes library */)
-#endif  // defined(OS_WIN)
-
 #if defined(OS_WIN) || defined(OS_MACOSX)
-// Reply after parsing the iTunes library XML file with the parser result and
-// an itunes::Library data structure.
-IPC_MESSAGE_CONTROL2(ChromeUtilityHostMsg_GotITunesLibrary,
-                     bool /* Parser result */,
-                     itunes::parser::Library /* iTunes library */)
-
 // Reply after parsing the Picasa PMP Database with the parser result and a
 // listing of the user's Picasa albums and folders, along with metadata.
 IPC_MESSAGE_CONTROL3(ChromeUtilityHostMsg_ParsePicasaPMPDatabase_Finished,
