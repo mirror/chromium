f6ddd798dd4c8a5795fe1ad9bb9e980a91cdbcad
kylechar <kylechar@chromium.org>
2017-09-01 12:06:48 -0400

    viz: Unify [mojom::]CompositorFrameSink[Support].
    This CL makes CompositorFrameSinkSupport into a
    mojom::CompositorFrameSink. CompositorFrameSinkSupport already
    implements the interface so this is a trivial change.
    
    This CL also replaces CompositorFrameSinkSupportClient with
    mojom::CompositorFrameSinkClient. The two interfaces were almost
    identical, minus WillDrawSurface() method, which was only used in
    DelegatedFrameHost. WillDrawSurface() is replaced with a callback.
    
    This greatly simplifies [Root]CompostiorFrameSinkImpl. Instead of having
    a CompositorFrameSinkSupport and delegating calls in/out, it just
    inherits from it. CompositorFrameSinkImpl only needs to hold the
    Binding/InterfacePtr.
    
    This CL also cleans up test implementations of
    CompositorFrameSink[Support]Client.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ib4f1fd6a22badce9ae77a7bb484c6ab465e4d7c5

--- components/viz/service/frame_sinks/root_compositor_frame_sink_impl.cc
+++ components/viz/service/frame_sinks/root_compositor_frame_sink_impl.cc
@@ -8,7 +8,6 @@
 
 #include "base/command_line.h"
 #include "components/viz/service/display/display.h"
-#include "components/viz/service/frame_sinks/compositor_frame_sink_support.h"
 #include "components/viz/service/frame_sinks/frame_sink_manager_impl.h"
 
 namespace viz {
@@ -21,18 +20,17 @@ RootCompositorFrameSinkImpl::RootCompositorFrameSinkImpl(
     mojom::CompositorFrameSinkAssociatedRequest request,
     mojom::CompositorFrameSinkClientPtr client,
     mojom::DisplayPrivateAssociatedRequest display_private_request)
-    : support_(
-          CompositorFrameSinkSupport::Create(this,
-                                             frame_sink_manager,
-                                             frame_sink_id,
-                                             true /* is_root */,
-                                             true /* needs_sync_points */)),
+    : CompositorFrameSinkSupport(client.get(),
+                                 frame_sink_id,
+                                 true /* is_root */,
+                                 true /* needs_sync_points */),
       display_begin_frame_source_(std::move(begin_frame_source)),
       display_(std::move(display)),
       client_(std::move(client)),
       compositor_frame_sink_binding_(this, std::move(request)),
       display_private_binding_(this, std::move(display_private_request)),
       hit_test_aggregator_(this) {
+  Init(frame_sink_manager);
   DCHECK(display_begin_frame_source_);
   compositor_frame_sink_binding_.set_connection_error_handler(
       base::Bind(&RootCompositorFrameSinkImpl::OnClientConnectionLost,
@@ -43,7 +41,7 @@ RootCompositorFrameSinkImpl::RootCompositorFrameSinkImpl(
 }
 
 RootCompositorFrameSinkImpl::~RootCompositorFrameSinkImpl() {
-  support_->frame_sink_manager()->UnregisterBeginFrameSource(
+  frame_sink_manager()->UnregisterBeginFrameSource(
       display_begin_frame_source_.get());
 }
 
@@ -74,25 +72,18 @@ void RootCompositorFrameSinkImpl::SetLocalSurfaceId(
   display_->SetLocalSurfaceId(local_surface_id, scale_factor);
 }
 
-void RootCompositorFrameSinkImpl::SetNeedsBeginFrame(bool needs_begin_frame) {
-  support_->SetNeedsBeginFrame(needs_begin_frame);
-}
-
 void RootCompositorFrameSinkImpl::SubmitCompositorFrame(
     const LocalSurfaceId& local_surface_id,
     cc::CompositorFrame frame,
     mojom::HitTestRegionListPtr hit_test_region_list,
     uint64_t submit_time) {
   // TODO(gklassen): send |hit_test_region_list| to |support_|.
-  if (!support_->SubmitCompositorFrame(local_surface_id, std::move(frame))) {
+  if (!CompositorFrameSinkSupport::SubmitCompositorFrame(local_surface_id,
+                                                         std::move(frame))) {
+    DLOG(ERROR) << "SubmitCompositorFrame failed for " << local_surface_id;
     compositor_frame_sink_binding_.Close();
     OnClientConnectionLost();
   }
-}
-
-void RootCompositorFrameSinkImpl::DidNotProduceFrame(
-    const BeginFrameAck& begin_frame_ack) {
-  support_->DidNotProduceFrame(begin_frame_ack);
 }
 
 void RootCompositorFrameSinkImpl::OnAggregatedHitTestRegionListUpdated(
@@ -100,15 +91,15 @@ void RootCompositorFrameSinkImpl::OnAggregatedHitTestRegionListUpdated(
     uint32_t active_handle_size,
     mojo::ScopedSharedBufferHandle idle_handle,
     uint32_t idle_handle_size) {
-  support_->frame_sink_manager()->OnAggregatedHitTestRegionListUpdated(
-      support_->frame_sink_id(), std::move(active_handle), active_handle_size,
+  frame_sink_manager()->OnAggregatedHitTestRegionListUpdated(
+      frame_sink_id(), std::move(active_handle), active_handle_size,
       std::move(idle_handle), idle_handle_size);
 }
 
 void RootCompositorFrameSinkImpl::SwitchActiveAggregatedHitTestRegionList(
     uint8_t active_handle_index) {
-  support_->frame_sink_manager()->SwitchActiveAggregatedHitTestRegionList(
-      support_->frame_sink_id(), active_handle_index);
+  frame_sink_manager()->SwitchActiveAggregatedHitTestRegionList(
+      frame_sink_id(), active_handle_index);
 }
 
 void RootCompositorFrameSinkImpl::DisplayOutputSurfaceLost() {
@@ -124,36 +115,8 @@ void RootCompositorFrameSinkImpl::DisplayWillDrawAndSwap(
 
 void RootCompositorFrameSinkImpl::DisplayDidDrawAndSwap() {}
 
-void RootCompositorFrameSinkImpl::DidReceiveCompositorFrameAck(
-    const std::vector<ReturnedResource>& resources) {
-  if (client_)
-    client_->DidReceiveCompositorFrameAck(resources);
-}
-
-void RootCompositorFrameSinkImpl::OnBeginFrame(const BeginFrameArgs& args) {
-  hit_test_aggregator_.Swap();
-  if (client_)
-    client_->OnBeginFrame(args);
-}
-
-void RootCompositorFrameSinkImpl::OnBeginFramePausedChanged(bool paused) {
-  if (client_)
-    client_->OnBeginFramePausedChanged(paused);
-}
-
-void RootCompositorFrameSinkImpl::ReclaimResources(
-    const std::vector<ReturnedResource>& resources) {
-  if (client_)
-    client_->ReclaimResources(resources);
-}
-
-void RootCompositorFrameSinkImpl::WillDrawSurface(
-    const LocalSurfaceId& local_surface_id,
-    const gfx::Rect& damage_rect) {}
-
 void RootCompositorFrameSinkImpl::OnClientConnectionLost() {
-  support_->frame_sink_manager()->OnClientConnectionLost(
-      support_->frame_sink_id());
+  frame_sink_manager()->OnClientConnectionLost(frame_sink_id());
 }
 
 }  // namespace viz
