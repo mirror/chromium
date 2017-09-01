d024cc35c045e6145f72de3fce455f96498fd6ab
Takashi Toyoshima <toyoshim@chromium.org>
2017-09-01 16:19:02 +0900

    ResourceFetcherImpl: explicitly complete the request on cancellation
    With NetworkService is enabled, OnComplete is not called on resetting
    the URLLoader. This patch changes to set |completed_| even on Cancel(),
    and do nothing if OnComplete() is called after the Cancel() call.
    
    Bug: 760581
    Change-Id: I7250a9741b281edd0a4926146ed4fb8d9aba0d14

--- content/public/renderer/resource_fetcher.h
+++ content/public/renderer/resource_fetcher.h
@@ -31,8 +31,9 @@ class CONTENT_EXPORT ResourceFetcher {
 
   // This will be called asynchronously after the URL has been fetched,
   // successfully or not.  If there is a failure, response and data will both be
-  // empty.  |response| and |data| are both valid until the URLFetcher instance
-  // is destroyed.
+  // empty.  |response| and |data| are both valid until the ResourceFetcher
+  // instance is destroyed.  The callback will not be called if ResourceFetcher
+  // instance is destroyed before completion or cancellation.
   typedef base::Callback<void(const blink::WebURLResponse& response,
                               const std::string& data)> Callback;
 
@@ -50,7 +51,8 @@ class CONTENT_EXPORT ResourceFetcher {
                          const std::string& value) = 0;
 
   // Starts the request using the specified frame.  Calls |callback| when
-  // done.
+  // done.  If ResourceFetcher instance is destructed without completion or
+  // cancellation, |callback| will be disposed without being called.
   virtual void Start(blink::WebLocalFrame* frame,
                      blink::WebURLRequest::RequestContext request_context,
                      const Callback& callback) = 0;
@@ -59,7 +61,7 @@ class CONTENT_EXPORT ResourceFetcher {
   // timeout.  Must be called after a request is started.
   virtual void SetTimeout(const base::TimeDelta& timeout) = 0;
 
-  // Manually cancel the request.
+  // Manually cancel the request.  Calls |callback| with a null WebURLResponse.
   virtual void Cancel() = 0;
 };
 
