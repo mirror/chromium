030593377ba58dacfa0b74cfe6829969b06bd518
Pavol Marko <pmarko@chromium.org>
2017-09-01 11:11:58 +0200

    Default (unset by admin) migration policy for EDU
    If the admin left the policy for migration unset and the device is
    enrolled in an EDU domain, the server will send to the client
    kAskForEcryptfsArcUsers = 5 value for that policy.
    
    When the client receives the kAskForEcryptfsArcUsers policy value,
    it must check two things:
    1. If the device had ARC M in M60 (specified by the flag
    kArcTransitionMToNRequired)
    2. Arc is enabled by poicy.
    
    If both conditions apply, then user should be asked if the migration
    has to be performed. Otherwise the migration is disabled for this
    policy value.
    
    This CL was originally owned by igorcov@ and tracked in CL:645352.
    
    BUG=747907
    TEST=Manual, unit_tests --gtest_filter=ArcMigration*
    
    Change-Id: I668e5d4e33afa94607aa6db790c06e6f9ee61879

--- chrome/browser/chromeos/arc/arc_util.cc
+++ chrome/browser/chromeos/arc/arc_util.cc
@@ -9,6 +9,7 @@
 #include <set>
 
 #include "base/callback.h"
+#include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
@@ -22,6 +23,7 @@
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
+#include "chromeos/chromeos_switches.h"
 #include "components/arc/arc_util.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_manager/known_user.h"
@@ -134,7 +136,7 @@ bool IsArcAllowedForProfile(const Profile* profile) {
   }
 
   if (!IsArcCompatibleFileSystemUsedForProfile(profile) &&
-      !IsArcMigrationAllowedForProfile(profile)) {
+      !IsArcMigrationAllowedByPolicyForProfile(profile)) {
     VLOG(1) << "Incompatible encryption and migration forbidden.";
     return false;
   }
@@ -162,13 +164,27 @@ bool IsArcAllowedForProfile(const Profile* profile) {
   return true;
 }
 
-bool IsArcMigrationAllowedForProfile(const Profile* profile) {
+bool IsArcMigrationAllowedByPolicyForProfile(const Profile* profile) {
   if (!profile || !profile->GetPrefs()->IsManagedPreference(
                       prefs::kEcryptfsMigrationStrategy)) {
     return true;
   }
 
-  return profile->GetPrefs()->GetInteger(prefs::kEcryptfsMigrationStrategy) !=
+  int migration_strategy =
+      profile->GetPrefs()->GetInteger(prefs::kEcryptfsMigrationStrategy);
+  // |kAskForEcryptfsArcUsers| value is received only if the device is in EDU
+  // and admin left the migration policy unset. In this case only a group of
+  // devices that had ARC M available are allowed to migrate, provided ARC is
+  // enabled.
+  if (migration_strategy ==
+      static_cast<int>(
+          arc::policy_util::EcryptfsMigrationAction::kAskForEcryptfsArcUsers)) {
+    return profile->GetPrefs()->GetBoolean(prefs::kArcEnabled) &&
+           base::CommandLine::ForCurrentProcess()->HasSwitch(
+               chromeos::switches::kArcTransitionMigrationRequired);
+  }
+
+  return migration_strategy !=
          static_cast<int>(
              arc::policy_util::EcryptfsMigrationAction::kDisallowMigration);
 }
