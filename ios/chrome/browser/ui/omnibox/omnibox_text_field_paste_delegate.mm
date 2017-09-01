19e7885c151c9f81cbad6e2090dd1eb7b19c77af
stkhapugin@chromium.org <stkhapugin@chromium.org>
2017-09-01 11:51:01 +0200

    Omnibox paste delegate.
    Introduce a paste delegate for omnibox on iOS 11 in order to avoid
    repeated pastes of the copied URL.
    
    Bug: 756855
    Change-Id: I78ff6d1bd6c4486446d7f7c9b32c3a1487152a65

---
+++ ios/chrome/browser/ui/omnibox/omnibox_text_field_paste_delegate.mm
@@ -0,0 +1,28 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/omnibox/omnibox_text_field_paste_delegate.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation OmniboxTextFieldPasteDelegate
+
+- (NSAttributedString*)textPasteConfigurationSupporting:
+    (id<UITextPasteConfigurationSupporting>)textPasteConfigurationSupporting
+    combineItemAttributedStrings:(NSArray<NSAttributedString*>*)itemStrings
+                        forRange:(UITextRange*)textRange
+    API_AVAILABLE(ios(11.0)) {
+  // Return only one item string to avoid repetition, for example when there are
+  // both a URL and a string in the pasteboard.
+  NSAttributedString* string = [itemStrings firstObject];
+  if (!string) {
+    string = [[NSAttributedString alloc] init];
+  }
+
+  return string;
+}
+
+@end
