3982c49cb30eedd879a38d37e8e0a4225c8c9885
galinap <galinap@google.com>
2017-09-01 09:54:01 +0100

    [Contextual Suggestions] Use a clean url for contextual suggestion fetch
    Bug:745660
    Change-Id: Iab7680e8e704f3de6fb4ed88be9e26c0fda3deca

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetContent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetContent.java
@@ -302,7 +302,7 @@ public class SuggestionsBottomSheetContent implements BottomSheet.BottomSheetCon
         // Context has changed, so we want to remove any old suggestions from the carousel.
         mSuggestionsCarousel.clearSuggestions();
 
-        String text = String.format(Locale.US, "Fetching contextual suggestions...");
+        String text = "Fetching contextual suggestions...";
         Toast.makeText(mRecyclerView.getContext(), text, Toast.LENGTH_SHORT).show();
         mSuggestionsUiDelegate.getSuggestionsSource().fetchContextualSuggestions(
                 url, new Callback<List<SnippetArticle>>() {
@@ -310,8 +310,8 @@ public class SuggestionsBottomSheetContent implements BottomSheet.BottomSheetCon
                     public void onResult(List<SnippetArticle> contextualSuggestions) {
                         mSuggestionsCarousel.newContextualSuggestionsAvailable(
                                 url, contextualSuggestions);
-                        String text = "Fetched " + contextualSuggestions.size()
-                                + " contextual suggestions.";
+                        String text = String.format(Locale.US, "Fetched %d contextual suggestions.",
+                                contextualSuggestions.size());
                         Toast.makeText(mRecyclerView.getContext(), text, Toast.LENGTH_SHORT).show();
                     }
                 });
