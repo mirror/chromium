5f3a840ef0519c358a4e0036f2e377184d3aef10
rbpotter <rbpotter@chromium.org>
2017-09-01 19:19:09 -0700

    Refactor PDF printing code out of PrintPreviewHandler
    Remove all the save to PDF code from PrintPreviewHandler and create a
    new PdfPrinterHandler class that inherits from PrinterHandler.
    
    Bug: None
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ied9fb7c2b11eb1588dacf182d5a1e8d251981e6f

--- chrome/browser/ui/webui/print_preview/pdf_printer_handler_win_unittest.cc
+++ chrome/browser/ui/webui/print_preview/pdf_printer_handler_win_unittest.cc
@@ -7,6 +7,8 @@
 #include <commdlg.h>
 #include <windows.h>
 
+#include "base/memory/ref_counted.h"
+#include "base/memory/ref_counted_memory.h"
 #include "base/run_loop.h"
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/printing/print_preview_dialog_controller.h"
@@ -15,9 +17,11 @@
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/webui/print_preview/pdf_printer_handler.h"
 #include "chrome/browser/ui/webui/print_preview/print_preview_ui.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/shell_dialogs/select_file_dialog_win.h"
 #include "ui/shell_dialogs/select_file_policy.h"
@@ -26,16 +30,20 @@ using content::WebContents;
 
 namespace {
 
-class FakePrintPreviewHandler;
+class FakePdfPrinterHandler;
 bool GetOpenFileNameImpl(OPENFILENAME* ofn);
-bool GetSaveFileNameImpl(FakePrintPreviewHandler* handler, OPENFILENAME* ofn);
+bool GetSaveFileNameImpl(FakePdfPrinterHandler* handler, OPENFILENAME* ofn);
 
-class FakePrintPreviewHandler : public PrintPreviewHandler {
+void EmptyPrintCallback(bool success, const base::Value& error) {}
+
+class FakePdfPrinterHandler : public PdfPrinterHandler {
  public:
-  explicit FakePrintPreviewHandler(content::WebUI* web_ui)
-      : init_called_(false), save_failed_(false) {
-    set_web_ui(web_ui);
-  }
+  FakePdfPrinterHandler(Profile* profile,
+                        content::WebContents* contents,
+                        printing::StickySettings* sticky_settings)
+      : PdfPrinterHandler(profile, contents, sticky_settings),
+        init_called_(false),
+        save_failed_(false) {}
 
   void FileSelected(const base::FilePath& path,
                     int index,
@@ -50,8 +58,9 @@ class FakePrintPreviewHandler : public PrintPreviewHandler {
     run_loop_.Quit();
   }
 
-  void StartPrintToPdf() {
-    PrintToPdf();
+  void StartPrintToPdf(const base::string16& job_title) {
+    StartPrint("", "", job_title, "", gfx::Size(), nullptr,
+               base::Bind(&EmptyPrintCallback));
     run_loop_.Run();
   }
 
@@ -75,7 +84,7 @@ class FakePrintPreviewHandler : public PrintPreviewHandler {
     select_file_dialog_->SelectFile(
         ui::SelectFileDialog::SELECT_SAVEAS_FILE, base::string16(),
         default_filename, &file_type_info, 0, base::FilePath::StringType(),
-        platform_util::GetTopLevel(web_ui()->GetWebContents()->GetNativeView()),
+        platform_util::GetTopLevel(preview_web_contents_->GetNativeView()),
         nullptr);
   }
 
@@ -85,15 +94,15 @@ class FakePrintPreviewHandler : public PrintPreviewHandler {
 };
 
 // Hook function to cancel the dialog when it is successfully initialized.
-UINT_PTR CALLBACK PrintPreviewHandlerTestHookFunction(HWND hdlg,
-                                                      UINT message,
-                                                      WPARAM wparam,
-                                                      LPARAM lparam) {
+UINT_PTR CALLBACK PdfPrinterHandlerTestHookFunction(HWND hdlg,
+                                                    UINT message,
+                                                    WPARAM wparam,
+                                                    LPARAM lparam) {
   if (message != WM_INITDIALOG)
     return 0;
   OPENFILENAME* ofn = reinterpret_cast<OPENFILENAME*>(lparam);
-  FakePrintPreviewHandler* handler =
-      reinterpret_cast<FakePrintPreviewHandler*>(ofn->lCustData);
+  FakePdfPrinterHandler* handler =
+      reinterpret_cast<FakePdfPrinterHandler*>(ofn->lCustData);
   handler->set_init_called();
   PostMessage(GetParent(hdlg), WM_COMMAND, MAKEWPARAM(IDCANCEL, 0), 0);
   return 1;
@@ -103,20 +112,20 @@ bool GetOpenFileNameImpl(OPENFILENAME* ofn) {
   return ::GetOpenFileName(ofn);
 }
 
-bool GetSaveFileNameImpl(FakePrintPreviewHandler* handler, OPENFILENAME* ofn) {
+bool GetSaveFileNameImpl(FakePdfPrinterHandler* handler, OPENFILENAME* ofn) {
   // Modify ofn so that the hook function will be called.
   ofn->Flags |= OFN_ENABLEHOOK;
-  ofn->lpfnHook = PrintPreviewHandlerTestHookFunction;
+  ofn->lpfnHook = PdfPrinterHandlerTestHookFunction;
   ofn->lCustData = reinterpret_cast<LPARAM>(handler);
   return ::GetSaveFileName(ofn);
 }
 
 }  // namespace
 
-class PrintPreviewHandlerTest : public PrintPreviewTest {
+class PdfPrinterHandlerTest : public PrintPreviewTest {
  public:
-  PrintPreviewHandlerTest() : preview_ui_(nullptr) {}
-  ~PrintPreviewHandlerTest() override {}
+  PdfPrinterHandlerTest() : preview_ui_(nullptr) {}
+  ~PdfPrinterHandlerTest() override {}
 
   void SetUp() override {
     PrintPreviewTest::SetUp();
@@ -140,39 +149,33 @@ class PrintPreviewHandlerTest : public PrintPreviewTest {
     print_view_manager->PrintPreviewNow(initiator->GetMainFrame(), false);
     WebContents* preview_dialog =
         controller->GetOrCreatePreviewDialog(initiator);
-    ASSERT_TRUE(preview_dialog);
-    preview_ui_ = static_cast<PrintPreviewUI*>(
-        preview_dialog->GetWebUI()->GetController());
-    ASSERT_TRUE(preview_ui_);
-
-    preview_handler_ =
-        base::MakeUnique<FakePrintPreviewHandler>(preview_dialog->GetWebUI());
+    pdf_printer_ = base::MakeUnique<FakePdfPrinterHandler>(
+        Profile::FromBrowserContext(preview_dialog->GetBrowserContext()),
+        preview_dialog, nullptr);
   }
 
-  std::unique_ptr<FakePrintPreviewHandler> preview_handler_;
+  std::unique_ptr<FakePdfPrinterHandler> pdf_printer_;
   PrintPreviewUI* preview_ui_;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(PrintPreviewHandlerTest);
+  DISALLOW_COPY_AND_ASSIGN(PdfPrinterHandlerTest);
 };
 
-TEST_F(PrintPreviewHandlerTest, TestSaveAsPdf) {
+TEST_F(PdfPrinterHandlerTest, TestSaveAsPdf) {
   CreateUIAndHandler();
-  preview_ui_->SetInitiatorTitle(L"111111111111111111111.html");
-  preview_handler_->StartPrintToPdf();
-  EXPECT_TRUE(preview_handler_->init_called());
-  EXPECT_TRUE(preview_handler_->save_failed());
+  pdf_printer_->StartPrintToPdf(L"111111111111111111111.html");
+  EXPECT_TRUE(pdf_printer_->init_called());
+  EXPECT_TRUE(pdf_printer_->save_failed());
 }
 
-TEST_F(PrintPreviewHandlerTest, TestSaveAsPdfLongFileName) {
+TEST_F(PdfPrinterHandlerTest, TestSaveAsPdfLongFileName) {
   CreateUIAndHandler();
-  preview_ui_->SetInitiatorTitle(
+  pdf_printer_->StartPrintToPdf(
       L"11111111111111111111111111111111111111111111111111111111111111111111111"
       L"11111111111111111111111111111111111111111111111111111111111111111111111"
       L"11111111111111111111111111111111111111111111111111111111111111111111111"
       L"11111111111111111111111111111111111111111111111111111111111111111111111"
       L"1111111111111111111111111111111111111111111111111.html");
-  preview_handler_->StartPrintToPdf();
-  EXPECT_TRUE(preview_handler_->init_called());
-  EXPECT_TRUE(preview_handler_->save_failed());
+  EXPECT_TRUE(pdf_printer_->init_called());
+  EXPECT_TRUE(pdf_printer_->save_failed());
 }
