2e9558bd655e9be3971c2d762fe7cf25b5628634
Alex Chu <alexchu@google.com>
2017-09-01 19:09:49 -0700

    Remove references to android.webkit.WebSettings.LayoutAlgorithm from AwContents.
    Change-Id: Id4092f3588713e76419c87afa5dabec0168e5bd5

--- android_webview/glue/java/src/com/android/webview/chromium/Converters.java
+++ android_webview/glue/java/src/com/android/webview/chromium/Converters.java
@@ -43,4 +43,36 @@ final class Converters {
                 throw new IllegalArgumentException("Unsupported value: " + value);
         }
     }
+
+    @org.chromium.android_webview.AwSettings.LayoutAlgorithm
+    public static int toAwLayoutAlgorithm(android.webkit.WebSettings.LayoutAlgorithm value) {
+        switch (value) {
+            case NARROW_COLUMNS:
+                return org.chromium.android_webview.AwSettings.NARROW_COLUMNS;
+            case NORMAL:
+                return org.chromium.android_webview.AwSettings.NORMAL;
+            case SINGLE_COLUMN:
+                return org.chromium.android_webview.AwSettings.SINGLE_COLUMN;
+            case TEXT_AUTOSIZING:
+                return org.chromium.android_webview.AwSettings.TEXT_AUTOSIZING;
+            default:
+                throw new IllegalArgumentException("Unsupported value: " + value);
+        }
+    }
+
+    public static android.webkit.WebSettings.LayoutAlgorithm fromAwLayoutAlgorithm(
+            @org.chromium.android_webview.AwSettings.LayoutAlgorithm int value) {
+        switch (value) {
+            case org.chromium.android_webview.AwSettings.NARROW_COLUMNS:
+                return android.webkit.WebSettings.LayoutAlgorithm.NARROW_COLUMNS;
+            case org.chromium.android_webview.AwSettings.NORMAL:
+                return android.webkit.WebSettings.LayoutAlgorithm.NORMAL;
+            case org.chromium.android_webview.AwSettings.SINGLE_COLUMN:
+                return android.webkit.WebSettings.LayoutAlgorithm.SINGLE_COLUMN;
+            case org.chromium.android_webview.AwSettings.TEXT_AUTOSIZING:
+                return android.webkit.WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING;
+            default:
+                throw new IllegalArgumentException("Unsupported value: " + value);
+        }
+    }
 }
