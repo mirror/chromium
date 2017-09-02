9626b87b424504e1be391ae398295a20cd1e9362
Alex Chu <alexchu@google.com>
2017-09-01 19:10:14 -0700

    Remove references to android.webkit.WebSettings.ZoomDensity from AwContents.
    Change-Id: Iceea304e4d28017e294d840ca5cd31ef152f3c0d

---
+++ android_webview/glue/java/src/com/android/webview/chromium/Converters.java
@@ -0,0 +1,12 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package com.android.webview.chromium;
+
+import android.content.Intent;
+
+/** A utility that converts between AwContents types and android.webkit types. */
+final class Converters {
+    private Converters() {}
+}
