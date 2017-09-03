c48ca8c95375f720eb86f6cab73b3165c506c324
Darin Fisher <darin@chromium.org>
2017-09-02 17:57:45 -0700

    Use Mojo IPC to connect to shared workers
    Bug:612308
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I8f065cec16673061104817b1c4b41d55e2d12203

--- content/renderer/shared_worker/shared_worker_repository.cc
+++ content/renderer/shared_worker/shared_worker_repository.cc
@@ -4,15 +4,17 @@
 
 #include "content/renderer/shared_worker/shared_worker_repository.h"
 
+#include "content/child/webmessageportchannel_impl.h"
 #include "content/common/view_messages.h"
 #include "content/renderer/render_frame_impl.h"
-#include "content/renderer/shared_worker/websharedworker_proxy.h"
+#include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/WebKit/public/web/WebSharedWorkerConnectListener.h"
 
 namespace content {
 
-SharedWorkerRepository::SharedWorkerRepository(RenderFrameImpl* render_frame)
-    : render_frame_(render_frame){};
+SharedWorkerRepository::SharedWorkerRepository(
+    service_manager::InterfaceProvider* interface_provider)
+    : interface_provider_(interface_provider) {}
 
 SharedWorkerRepository::~SharedWorkerRepository() = default;
 
@@ -21,37 +23,64 @@ void SharedWorkerRepository::Connect(
     const blink::WebString& name,
     DocumentID document_id,
     const blink::WebString& content_security_policy,
-    blink::WebContentSecurityPolicyType security_policy_type,
+    blink::WebContentSecurityPolicyType content_security_policy_type,
     blink::WebAddressSpace creation_address_space,
-    blink::WebSharedWorkerCreationContextType creation_context_type,
+    blink::mojom::SharedWorkerCreationContextType creation_context_type,
     bool data_saver_enabled,
     std::unique_ptr<blink::WebMessagePortChannel> channel,
     std::unique_ptr<blink::WebSharedWorkerConnectListener> listener) {
-  documents_with_workers_.insert(document_id);
+  // Lazy bind the connector.
+  if (!connector_)
+    interface_provider_->GetInterface(mojo::MakeRequest(&connector_));
 
-  ViewHostMsg_CreateWorker_Params params;
-  params.url = url;
-  params.name = name.Utf16();
-  params.content_security_policy = content_security_policy.Utf16();
-  params.security_policy_type = security_policy_type;
-  params.document_id = document_id;
-  params.render_frame_route_id = render_frame_->GetRoutingID();
-  params.creation_address_space = creation_address_space;
-  params.creation_context_type = creation_context_type;
-  params.data_saver_enabled = data_saver_enabled;
-  ViewHostMsg_CreateWorker_Reply reply;
+  mojom::SharedWorkerInfoPtr info(mojom::SharedWorkerInfo::New(
+      url, name.Utf8(), content_security_policy.Utf8(),
+      content_security_policy_type, creation_address_space,
+      creation_context_type, data_saver_enabled));
 
-  // This proxy will self-destruct when a worker is destroyed.
-  new WebSharedWorkerProxy(std::move(listener), params, std::move(channel));
+  mojom::SharedWorkerClientPtr client;
+  AddWorker(document_id,
+            mojo::MakeStrongBinding(
+                std::make_unique<SharedWorkerClientImpl>(std::move(listener)),
+                mojo::MakeRequest(&client)));
+
+  connector_->Connect(std::move(info), std::move(client),
+                      static_cast<WebMessagePortChannelImpl*>(channel.get())
+                          ->ReleaseMessagePort()
+                          .ReleaseHandle());
 }
 
-void SharedWorkerRepository::DocumentDetached(DocumentID document) {
-  std::set<DocumentID>::iterator iter = documents_with_workers_.find(document);
-  if (iter != documents_with_workers_.end()) {
-    // Notify the browser process that the document has shut down.
-    render_frame_->Send(new ViewHostMsg_DocumentDetached(document));
-    documents_with_workers_.erase(iter);
+void SharedWorkerRepository::DocumentDetached(DocumentID document_id) {
+  // Delete any associated SharedWorkerClientImpls, which will signal, via the
+  // dropped mojo connection, disinterest in the associated shared worker.
+  auto iter = client_map_.find(document_id);
+  if (iter != client_map_.end()) {
+    for (auto client : iter->second) {
+      if (client)
+        client->Close();
+    }
+    client_map_.erase(iter);
   }
+}
+
+void SharedWorkerRepository::AddWorker(
+    DocumentID document_id,
+    mojo::StrongBindingPtr<mojom::SharedWorkerClient> client) {
+  std::pair<ClientMap::iterator, bool> result =
+      client_map_.insert(std::make_pair(document_id, ClientList()));
+  ClientList& clients = result.first->second;
+
+  // Prune the list of any dead clients. We expect the size of this list to
+  // be pretty small.
+  for (auto iter = clients.begin(); iter != clients.end();) {
+    if (!iter->get()) {
+      iter = clients.erase(iter);
+    } else {
+      ++iter;
+    }
+  }
+
+  clients.push_back(client);
 }
 
 }  // namespace content
