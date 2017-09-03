1d7daf253fcce2da784562396fe700df6eb73fe0
Chris Palmer <palmer@chromium.org>
2017-09-03 14:16:42 -0700

    Attempt a new kind of lock for Partition Alloc.
    The SpinLock strategy is spinning too much, in certain contexts. A base::Lock
    causes a significant perf regression in various benchmarks. This new strategy
    attempts to get the benefits of an atomic int and falls back to
    base::WaitableEvent under contention.
    
    BUG=758695
    
    Change-Id: I4827bcb6475cb950d48ab0c12ba29d031f717552

--- base/allocator/partition_allocator/spin_event_lock_unittest.cc
+++ base/allocator/partition_allocator/spin_event_lock_unittest.cc
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/allocator/partition_allocator/spin_lock.h"
-
 #include <memory>
+#include "base/allocator/partition_allocator/event_lock.h"
+#include "base/allocator/partition_allocator/spin_lock.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/threading/thread.h"
@@ -14,7 +14,8 @@ namespace base {
 
 static const size_t kBufferSize = 16;
 
-static subtle::SpinLock g_lock;
+static subtle::SpinLock g_spin_lock;
+static subtle::EventLock g_event_lock;
 
 static void FillBuffer(volatile char* buffer, char fill_pattern) {
   for (size_t i = 0; i < kBufferSize; ++i)
@@ -34,14 +35,16 @@ static void ChangeAndCheckBuffer(volatile char* buffer) {
   FillBuffer(buffer, '!');
 }
 
-static void ThreadMain(volatile char* buffer) {
+template <typename LockType>
+static void ThreadMain(volatile char* buffer, LockType* lock) {
   for (int i = 0; i < 500000; ++i) {
-    subtle::SpinLock::Guard guard(g_lock);
+    typename LockType::Guard guard(*lock);
     ChangeAndCheckBuffer(buffer);
   }
 }
 
-TEST(SpinLockTest, Torture) {
+template <typename LockType>
+static void TortureLock(LockType* lock) {
   char shared_buffer[kBufferSize];
 
   Thread thread1("thread1");
@@ -51,11 +54,19 @@ TEST(SpinLockTest, Torture) {
   thread2.StartAndWaitForTesting();
 
   thread1.task_runner()->PostTask(
-      FROM_HERE,
-      BindOnce(&ThreadMain, Unretained(static_cast<char*>(shared_buffer))));
+      FROM_HERE, BindOnce(&ThreadMain<LockType>,
+                          Unretained(static_cast<char*>(shared_buffer)), lock));
   thread2.task_runner()->PostTask(
-      FROM_HERE,
-      BindOnce(&ThreadMain, Unretained(static_cast<char*>(shared_buffer))));
+      FROM_HERE, BindOnce(&ThreadMain<LockType>,
+                          Unretained(static_cast<char*>(shared_buffer)), lock));
+}
+
+TEST(SpinLockTest, Torture) {
+  TortureLock<subtle::SpinLock>(&g_spin_lock);
+}
+
+TEST(EventLockTest, Torture) {
+  TortureLock<subtle::EventLock>(&g_event_lock);
 }
 
 }  // namespace base
