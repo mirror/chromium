e058b09719839311a323a2ab5dbd3c64ae6ce713
Chris Mumford <cmumford@chromium.org>
2017-09-05 14:41:56 -0700

    leveldb: Moved shared block cache functions into leveldb::chrome.
    Change-Id: I3a3df8f623556898ca57ca9f22fcc32aadd130e7

---
+++ third_party/leveldatabase/leveldb_chromium.cc
@@ -0,0 +1,70 @@
+// Copyright 2017 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#include "third_party/leveldatabase/leveldb_chromium.h"
+
+#include <memory>
+#include "base/sys_info.h"
+
+namespace leveldb {
+namespace chrome {
+
+namespace {
+
+size_t DefaultBlockCacheSize() {
+  if (base::SysInfo::IsLowEndDevice())
+    return 1 << 20;  // 1MB
+  else
+    return 8 << 20;  // 8MB
+}
+
+class Globals {
+ public:
+  static Globals* Get();
+
+  Globals() : browser_block_cache_(NewLRUCache(DefaultBlockCacheSize())) {
+    if (!base::SysInfo::IsLowEndDevice())
+      web_block_cache_.reset(NewLRUCache(DefaultBlockCacheSize()));
+  }
+
+  Cache* web_block_cache() const {
+    if (web_block_cache_)
+      return web_block_cache_.get();
+    return browser_block_cache();
+  }
+
+  Cache* browser_block_cache() const { return browser_block_cache_.get(); }
+
+ private:
+  ~Globals() {}
+
+  std::unique_ptr<Cache> web_block_cache_;      // NULL on low end devices
+  std::unique_ptr<Cache> browser_block_cache_;  // Never NULL
+
+  DISALLOW_COPY_AND_ASSIGN(Globals);
+};
+
+// static
+Globals* GetGlobals() {
+  static Globals* globals = new Globals();
+  return globals;
+}
+
+}  // namespace
+
+// Returns a separate (from the default) block cache for use by web APIs.
+// This must be used when opening the databases accessible to Web-exposed APIs,
+// so rogue pages can't mount a denial of service attack by hammering the block
+// cache. Without separate caches, such an attack might slow down Chrome's UI to
+// the point where the user can't close the offending page's tabs.
+Cache* GetSharedWebBlockCache() {
+  return GetGlobals()->web_block_cache();
+}
+
+Cache* GetSharedBrowserBlockCache() {
+  return GetGlobals()->browser_block_cache();
+}
+
+}  // namespace chrome
+}  // namespace leveldb
