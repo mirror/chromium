6f60a73eda560f46adc0a8d26841493b9d9bbafb
Tom Anderson <thomasanderson@chromium.org>
2017-09-05 16:29:50 -0700

    Add merge_deb_dependencies target
    This CL adds a script to merge dependency lists and an associated
    target to merge all automatically genreated debian deps.
    
    Redording the deps into a file and merging them is preferred to
    recalculating and merging because running dpkg-shlibdeps is expensive
    (can take ~2s for the chrome binary).
    
    BUG=758654
    R=thestig@chromium.org
    
    Change-Id: I196c0e8df6540dba126069bade992a832085d9f3

---
+++ chrome/installer/linux/debian/merge-package-versions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import sys
+
+import package_version_interval
+
+if len(sys.argv) < 3:
+  print 'Usage: output_deps_file input1_deps_file input2_deps_file ...'
+  sys.exit(1)
+
+output_filename = sys.argv[1]
+input_filenames = sys.argv[2:]
+
+package_intervals = {}
+for input_filename in input_filenames:
+  for line in open(input_filename):
+    line = line.rstrip('\n')
+    (package, interval) = package_version_interval.parse_dep(line)
+    if package in package_intervals:
+      interval = interval.intersect(package_intervals[package])
+    package_intervals[package] = interval
+
+with open(output_filename, 'w') as output_file:
+  output_file.write(package_version_interval.format_package_intervals(
+      package_intervals))
