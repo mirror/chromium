738ae1d139e42b77b128d54beed0d40b34aa1a51
Alexander Semashko <ahest@yandex-team.ru>
2017-09-05 12:51:45 +0300

    Add net error pages to content shell.
    Currently content shell shows a blank page on error. In
    content_browsertests the (unexpected, I guess) effect is that
    NavigateToURL returns true even when the page was not loaded. Also with
    error pages it will be easier to understand what's happening during
    debugging. 
    
    Bug:
    Change-Id: I4c051c87df6c86b7c333af933b34ac3bf879ec3e

--- content/browser/tracing/memory_tracing_browsertest.cc
+++ content/browser/tracing/memory_tracing_browsertest.cc
@@ -154,7 +154,7 @@ class MemoryTracingTest : public ContentBrowserTest {
   }
 
   void Navigate(Shell* shell) {
-    NavigateToURL(shell, GetTestUrl("", "title.html"));
+    NavigateToURL(shell, GetTestUrl("", "title1.html"));
   }
 
   MOCK_METHOD2(OnMemoryDumpDone, void(uint32_t request_index, bool successful));
