7a79e156fc6dd67d68edd24d36ef7a2dc17cbf59
Emi Morikawa <emim@google.com>
2017-09-05 11:31:20 +0900

    Factor out common part between ServiceWorkerSubresourceLoader and ServiceWorkerURLLoaderJob
    Make new class "ServiceWorkerLoaderHelpers". there are helper functions for service worker classes that use URLLoader. 
    
    Bug: 758879
    Change-Id: I286336903b6b750ee094947264b163e6debd4995

---
+++ content/common/service_worker/service_worker_loader_helpers.h
@@ -0,0 +1,36 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_COMMON_SERVICE_WORKER_SERVICE_WORKER_LOADER_HELPERS_H_
+#define CONTENT_COMMON_SERVICE_WORKER_SERVICE_WORKER_LOADER_HELPERS_H_
+
+#include "content/common/service_worker/service_worker_types.h"
+
+namespace content {
+
+struct ResourceRequest;
+struct ResourceResponseHead;
+
+// This class factor out common part between ServiceWorkerSubresourceLoader and
+// ServiceWorkerURLLoaderJob.
+class ServiceWorkerLoaderHelpers {
+ public:
+  static std::unique_ptr<ServiceWorkerFetchRequest> CreateFetchRequest(
+      const ResourceRequest& request);
+
+  static void SaveResponseHead(const ServiceWorkerResponse& response,
+                               ResourceResponseHead* out_head);
+  // Generates and populates |out_head->headers|.
+  static void SaveResponseHeaders(const int status_code,
+                                  const std::string& status_text,
+                                  const ServiceWorkerHeaderMap& headers,
+                                  ResourceResponseHead* out_head);
+  // Populates |out_head| (except for headers) with given |response|.
+  static void SaveResponseInfo(const ServiceWorkerResponse& response,
+                               ResourceResponseHead* out_head);
+};
+
+}  // namespace content
+
+#endif  // CONTENT_COMMON_SERVICE_WORKER_SERVICE_WORKER_LOADER_HELPERS_H_
