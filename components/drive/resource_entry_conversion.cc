1c611a0ae68bf4a6d6fc3d1ee9a5f708e27c746c
Shuhei Takahashi <nya@chromium.org>
2017-09-05 14:08:37 +0900

    recent: Store modifiedByMeDate in local Drive database.
    This timestamp will be used in Recent root.
    
    Bug: 758677
    Test: unit_tests
    Test: Disconnect from Internet, copy a file from Downloads to Drive, and makes sure the new file is shown in Recent
    Change-Id: I51e85ce10d1777e1a0c0bb928c3da92faae376fb

--- components/drive/resource_entry_conversion.cc
+++ components/drive/resource_entry_conversion.cc
@@ -134,6 +134,9 @@ bool ConvertFileResourceToResourceEntry(
       file_specific_info->set_image_rotation(image_rotation);
   }
 
+  converted.set_modification_by_me_date(
+      input.modified_by_me_date().ToInternalValue());
+
   out_entry->Swap(&converted);
   swap(*out_parent_resource_id, parent_resource_id);
   return true;
