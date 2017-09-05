460bc0ffde4ae5acb45cf48eb959e9adbb8aa5b9
Brett Wilson <brettw@chromium.org>
2017-09-05 15:55:07 -0700

    Remove tracked_objects::ScopedTracker instantiations.
    Removes the ScopedTracker calls since we are deleting this infrastructure.
    This saves about 25KB in an official build.
    
    Design doc:
    https://docs.google.com/document/d/1cibE5nAG9sdjVMfs-MkvrMtp43i9MLGqM_uNusLqiBY
    
    Bug:762311
    Change-Id: Id00f88d4b7c8c14d51b20d9b53dc233189bdb049

--- content/browser/renderer_host/media/media_devices_manager.cc
+++ content/browser/renderer_host/media/media_devices_manager.cc
@@ -263,19 +263,9 @@ void MediaDevicesManager::StartMonitoring() {
 #if defined(OS_MACOSX)
 void MediaDevicesManager::StartMonitoringOnUIThread() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  // TODO(erikchen): Remove ScopedTracker below once crbug.com/458404 is fixed.
-  tracked_objects::ScopedTracker tracking_profile1(
-      FROM_HERE_WITH_EXPLICIT_FUNCTION(
-          "458404 MediaDevicesManager::GetBrowserMainLoop"));
   BrowserMainLoop* browser_main_loop = content::BrowserMainLoop::GetInstance();
   if (!browser_main_loop)
     return;
-
-  // TODO(erikchen): Remove ScopedTracker below once crbug.com/458404 is
-  // fixed.
-  tracked_objects::ScopedTracker tracking_profile3(
-      FROM_HERE_WITH_EXPLICIT_FUNCTION(
-          "458404 MediaDevicesManager::DeviceMonitorMac::StartMonitoring"));
   browser_main_loop->device_monitor_mac()->StartMonitoring();
 }
 #endif
