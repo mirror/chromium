83cd5d6d3888514bea1b259889a2414d4f1b5c75
Jinsuk Kim <jinsukkim@chromium.org>
2017-09-05 14:03:23 +0900

    DO NOT SUBMIT: Android: Javascript API to WebContents
    Moves Javascript API in CVC to WebContents.  All the APIs are
    left intact but only changed the exposure surface.
    
    Bug: 598880
    Change-Id: I62f3a86e49a6e2aeb2f1a055357505e184186772

--- content/browser/web_contents/web_contents_android.h
+++ content/browser/web_contents/web_contents_android.h
@@ -23,6 +23,7 @@ class GURL;
 
 namespace content {
 
+class GinJavaBridgeDispatcherHost;
 class WebContentsImpl;
 
 // Android wrapper around WebContents that provides safer passage from java and
@@ -39,6 +40,10 @@ class CONTENT_EXPORT WebContentsAndroid
   base::android::ScopedJavaLocalRef<jobject> GetJavaObject();
 
   // Methods called from Java
+  void SetRetainedJavascriptObjects(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& obj,
+      const base::android::JavaParamRef<jobject>& retained_javascript_objects);
   base::android::ScopedJavaLocalRef<jobject> GetTopLevelNativeWindow(
       JNIEnv* env,
       const base::android::JavaParamRef<jobject>& obj);
@@ -217,6 +222,23 @@ class CONTENT_EXPORT WebContentsAndroid
       JNIEnv* env,
       const base::android::JavaParamRef<jobject>& obj);
 
+  void SetAllowJavascriptInterfacesInspection(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& obj,
+      jboolean allow);
+
+  void AddJavascriptInterface(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& /* obj */,
+      const base::android::JavaParamRef<jobject>& object,
+      const base::android::JavaParamRef<jstring>& name,
+      const base::android::JavaParamRef<jclass>& safe_annotation_clazz);
+
+  void RemoveJavascriptInterface(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& /* obj */,
+      const base::android::JavaParamRef<jstring>& name);
+
   void SetMediaSession(
       const base::android::ScopedJavaLocalRef<jobject>& j_media_session);
 
@@ -239,6 +261,9 @@ class CONTENT_EXPORT WebContentsAndroid
   WebContentsImpl* web_contents_;
   NavigationControllerAndroid navigation_controller_;
   base::android::ScopedJavaGlobalRef<jobject> obj_;
+
+  // Manages injecting Java objects.
+  scoped_refptr<GinJavaBridgeDispatcherHost> java_bridge_dispatcher_host_;
 
   base::WeakPtrFactory<WebContentsAndroid> weak_factory_;
 
