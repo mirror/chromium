978aa96bd75028d303b8ed44f54328cdd7233804
DongJun Kim <djmix.kim@samsung.com>
2017-09-05 14:34:02 +0900

    Change default value of aria-valuemax on some roles
    According from spec [1],
    scrollbar, separator, or slider should have a default
    max of 100 if not otherwise specified.
    
    Now, we will return 100 as default when calling
    AXNodeObject::MaxValueForRange() on above roles.
    
    [1] https://www.w3.org/WAI/ARIA/wiki/ARIA_1.1_Changes
    
    BUG=688581
    
    Change-Id: I687889d03034d7d38dc33c7af64ea184a4f4e5fb

--- third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
@@ -1895,7 +1895,15 @@ float AXNodeObject::MaxValueForRange() const {
   if (isHTMLMeterElement(GetNode()))
     return toHTMLMeterElement(*GetNode()).max();
 
-  return 0.0;
+  switch (AriaRoleAttribute()) {
+    case kScrollBarRole:
+    case kSplitterRole:
+    case kSliderRole:
+      // In ARIA 1.1, the default value for aria-valuemax changed to 100.
+      return 100.0;
+    default:
+      return 0.0;
+  }
 }
 
 float AXNodeObject::MinValueForRange() const {
