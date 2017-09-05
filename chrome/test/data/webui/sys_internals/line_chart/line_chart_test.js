78a505cbca10e40b7a76a9c2a1d726b325837160
Chung-Sheng Wu <chungsheng@google.com>
2017-09-05 15:09:20 +0800

    [CrOS] Show system information via Chrome Internal Page
    Shows information like CPU usage, Memory status, Zram status.
    
    This patch adds the implementation for the Internal Page frontend.
    Handle the data from the backend, and then show them on a line chart.
    
    Bug: 752002
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ifd7a1aadb95693dce2c88202486b951e94d86513

---
+++ chrome/test/data/webui/sys_internals/line_chart/line_chart_test.js
@@ -0,0 +1,13 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var LineChartTest = LineChartTest || {};
+
+LineChartTest.LineChart = function() {
+  test('LineChart unit test', function() {
+    assertTrue(true);
+  });
+
+  mocha.run();
+}
