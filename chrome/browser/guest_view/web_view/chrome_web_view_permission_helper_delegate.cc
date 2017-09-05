e642cf9b3d530adbf98c1a25691f2c9fa2abc9ab
Nicholas Verne <nverne@chromium.org>
2017-09-05 12:20:13 +1000

    mojo-ify plugin placeholder renderer->host messages
    This CL introduces PluginHost, PluginAuthHost and PluginRenderer.
     
    mojo interfaces. PluginHost and PluginAuthHost are for messages sent 
    to the host. 
    PluginRenderer is for messages sent from host to renderer. 
    
    We pass PluginRenderer interfaces to the host when the renderer needs
    new plugins to be downloaded.
    
    
    Bug: 577685
    Change-Id: Iabf90d2f87703f5d84506edef7d9a27d90022735

--- chrome/browser/guest_view/web_view/chrome_web_view_permission_helper_delegate.cc
+++ chrome/browser/guest_view/web_view/chrome_web_view_permission_helper_delegate.cc
@@ -34,8 +34,8 @@ void CallbackWrapper(const base::Callback<void(bool)>& callback,
 ChromeWebViewPermissionHelperDelegate::ChromeWebViewPermissionHelperDelegate(
     WebViewPermissionHelper* web_view_permission_helper)
     : WebViewPermissionHelperDelegate(web_view_permission_helper),
-      weak_factory_(this) {
-}
+      plugin_auth_host_bindings_(web_contents(), this),
+      weak_factory_(this) {}
 
 ChromeWebViewPermissionHelperDelegate::~ChromeWebViewPermissionHelperDelegate()
 {}
@@ -45,12 +45,6 @@ bool ChromeWebViewPermissionHelperDelegate::OnMessageReceived(
     const IPC::Message& message,
     content::RenderFrameHost* render_frame_host) {
   IPC_BEGIN_MESSAGE_MAP(ChromeWebViewPermissionHelperDelegate, message)
-    IPC_MESSAGE_HANDLER(ChromeViewHostMsg_BlockedOutdatedPlugin,
-                        OnBlockedOutdatedPlugin)
-    IPC_MESSAGE_HANDLER(ChromeViewHostMsg_BlockedUnauthorizedPlugin,
-                        OnBlockedUnauthorizedPlugin)
-    IPC_MESSAGE_HANDLER(ChromeViewHostMsg_CouldNotLoadPlugin,
-                        OnCouldNotLoadPlugin)
     IPC_MESSAGE_HANDLER(ChromeViewHostMsg_RemovePluginPlaceholderHost,
                         OnRemovePluginPlaceholderHost)
     IPC_MESSAGE_HANDLER(ChromeViewHostMsg_OpenPDF, OnOpenPDF)
@@ -60,7 +54,7 @@ bool ChromeWebViewPermissionHelperDelegate::OnMessageReceived(
   return true;
 }
 
-void ChromeWebViewPermissionHelperDelegate::OnBlockedUnauthorizedPlugin(
+void ChromeWebViewPermissionHelperDelegate::BlockedUnauthorizedPlugin(
     const base::string16& name,
     const std::string& identifier) {
   const char kPluginName[] = "name";
@@ -78,15 +72,6 @@ void ChromeWebViewPermissionHelperDelegate::OnBlockedUnauthorizedPlugin(
       true /* allowed_by_default */);
   base::RecordAction(
       base::UserMetricsAction("WebView.Guest.PluginLoadRequest"));
-}
-
-void ChromeWebViewPermissionHelperDelegate::OnCouldNotLoadPlugin(
-    const base::FilePath& plugin_path) {
-}
-
-void ChromeWebViewPermissionHelperDelegate::OnBlockedOutdatedPlugin(
-    int placeholder_id,
-    const std::string& identifier) {
 }
 
 void ChromeWebViewPermissionHelperDelegate::OnRemovePluginPlaceholderHost(
