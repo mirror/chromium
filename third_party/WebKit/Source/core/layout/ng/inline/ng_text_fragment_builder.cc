3e8bbc277913aab46945d926683d26e4a6ea3193
Koji Ishii <kojii@chromium.org>
2017-09-05 16:55:00 +0900

    [LayoutNG] Compute visual rect for text fragments
    This patch adds the initial part of computing visual rects of fragments.
    
    Visual rects are computed when fragments are built, but they need to be
    updated when scale/zoom changes because of font hinting. This patch adds
    NGPhysicalFragment::UpdateVisualRect() for this purpose, though it's not
    plumbed from the style change code path yet.
    
    Computing glyph overflow is supported, but not all CSS properties that
    cause visual overflow are not supported yet.
    
    Copying visual overflow back to the LayoutObject tree is not supported
    yet.
    
    BUG=636993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I762e06d9631f4a5075c074b8af37b095d749c472

--- third_party/WebKit/Source/core/layout/ng/inline/ng_text_fragment_builder.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_text_fragment_builder.cc
@@ -60,10 +60,12 @@ RefPtr<NGPhysicalTextFragment> NGTextFragmentBuilder::ToTextFragment(
     unsigned index,
     unsigned start_offset,
     unsigned end_offset) {
-  return AdoptRef(new NGPhysicalTextFragment(
+  RefPtr<NGPhysicalTextFragment> fragment = AdoptRef(new NGPhysicalTextFragment(
       node_.GetLayoutObject(), Style(), node_.Text(), index, start_offset,
       end_offset, size_.ConvertToPhysical(WritingMode()),
       ToLineOrientation(WritingMode()), end_effect_, std::move(shape_result_)));
+  fragment->UpdateVisualRect();
+  return fragment;
 }
 
 }  // namespace blink
