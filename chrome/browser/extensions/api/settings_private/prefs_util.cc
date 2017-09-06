4280ee7c9194c463e88c7779fd8f8134028f4770
Oren Blasberg <orenb@google.com>
2017-09-05 18:33:39 -0700

    Add a real pref for SMS Connect and update settings UI to use it.
    Bug: 718617
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I5dfa501f31a8ba0abd7341bdb60a4874fb1c2cde

--- chrome/browser/extensions/api/settings_private/prefs_util.cc
+++ chrome/browser/extensions/api/settings_private/prefs_util.cc
@@ -380,6 +380,11 @@ const PrefsUtil::TypedPrefMap& PrefsUtil::GetWhitelistedKeys() {
       settings_private::PrefType::PREF_TYPE_NUMBER;
   (*s_whitelist)[::prefs::kLanguageXkbAutoRepeatInterval] =
       settings_private::PrefType::PREF_TYPE_NUMBER;
+
+  // Multidevice settings.
+  (*s_whitelist)[::prefs::kSmsConnectEnabled] =
+      settings_private::PrefType::PREF_TYPE_BOOLEAN;
+
 #else
   (*s_whitelist)[::prefs::kAcceptLanguages] =
       settings_private::PrefType::PREF_TYPE_STRING;
