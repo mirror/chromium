3a380069d801ae79d0c44c76bcf0f4674ca9c075
Jinsuk Kim <jinsukkim@chromium.org>
2017-09-06 15:28:34 +0900

    Remove ContentViewCoreObserver 2/2
    RenderWidgetHostViewAndroid was observing ContentViewCore destruction
    directly through an interface ContentViewCoreObserver, which is
    a layering violation. This CL addresses the issue by introducing
    ContentDestructionObserver that decouples RWHVA from ContentViewCore.
    CDO instance is created for a RWHVA when it is linked to a certain
    WebContents, and notifies it of the event by way of
    WebContentsObserver::WebContentsDestroyed which can also be used
    to detect the destruction of ContentViewCore.
    
    Bug: 626765
    Change-Id: Iadd03512f4529506a5dc5576ef0e0820264d4513

---
+++ content/browser/android/content_destruction_observer.cc
@@ -0,0 +1,45 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/android/content_destruction_observer.h"
+
+namespace content {
+// static
+void ContentDestructionObserver::Create(WebContents* web_contents,
+                                        RenderWidgetHostViewAndroid* rwhva) {
+  // Owns itself, and gets destroyed together with WebContents.
+  new ContentDestructionObserver(web_contents, rwhva);
+}
+
+void ContentDestructionObserver::RenderViewHostChanged(
+    RenderViewHost* old_host,
+    RenderViewHost* new_host) {
+  auto* new_view = new_host ? static_cast<RenderWidgetHostViewAndroid*>(
+                                  new_host->GetWidget()->GetView())
+                            : nullptr;
+  if (new_view != rwhva_) {
+    // The WebContents got paired with a new RenderWidgetHostView. No need to
+    // signal the destruction of the current WebContents any more. Delete
+    // itself.
+    delete this;
+  }
+}
+
+void ContentDestructionObserver::WebContentsDestroyed() {
+  rwhva_->OnContentDestroyed();
+  delete this;
+}
+
+void ContentDestructionObserver::RenderWidgetHostViewDestroyed(
+    RenderWidgetHostViewAndroid* rwhva) {
+  // RWHVA is going away. No need to signal the destruction of the current
+  // WebContents any more. Delete itself.
+  delete this;
+}
+
+ContentDestructionObserver::~ContentDestructionObserver() {
+  rwhva_->RemoveDestructionObserver(this);
+}
+
+}  // namespace content
