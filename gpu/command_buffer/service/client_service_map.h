aacae1d07d72004522eba0dd11c04f912c38cad3
Geoff Lang <geofflang@chromium.org>
2017-09-06 17:18:46 -0400

    Use a flat arrays to store client/service ID mappings for small IDs.
    Store the mapping of client ID to service ID in an array for IDs less
    than 16k.  This greatly speeds up ID lookups for the general case, from
    a 8% hotspot to <1% on the MotionMark WebGL benchmark.
    
    Change-Id: I81401c8e87bdb885a0b773050c930a94bcb3fa45

--- gpu/command_buffer/service/client_service_map.h
+++ gpu/command_buffer/service/client_service_map.h
@@ -7,6 +7,7 @@
 
 #include <limits>
 #include <unordered_map>
+#include <vector>
 
 namespace gpu {
 
@@ -15,19 +16,42 @@ namespace gles2 {
 template <typename ClientType, typename ServiceType>
 class ClientServiceMap {
  public:
-  ClientServiceMap() : client_to_service_() {}
+  ClientServiceMap()
+      : client_to_service_array_(kInitialFlaArraySize, invalid_service_id()),
+        client_to_service_map_() {}
 
   void SetIDMapping(ClientType client_id, ServiceType service_id) {
-    DCHECK(client_to_service_.find(client_id) == client_to_service_.end());
     DCHECK(service_id != invalid_service_id());
-    client_to_service_[client_id] = service_id;
+    if (client_id < kMaxFlatArraySize) {
+      if (client_id >= client_to_service_array_.size()) {
+        // Resize the array to the next power of 2 above client_id
+        size_t new_size = powf(2, ceilf(log2f(client_id + 1)));
+        DCHECK(new_size <= kMaxFlatArraySize);
+        client_to_service_array_.resize(new_size, invalid_service_id());
+      }
+      DCHECK(client_to_service_array_[client_id] == invalid_service_id());
+      client_to_service_array_[client_id] = service_id;
+    } else {
+      DCHECK(client_to_service_map_.find(client_id) ==
+             client_to_service_map_.end());
+      client_to_service_map_[client_id] = service_id;
+    }
   }
 
   void RemoveClientID(ClientType client_id) {
-    client_to_service_.erase(client_id);
+    if (client_id < kMaxFlatArraySize) {
+      if (client_id < client_to_service_array_.size()) {
+        client_to_service_array_[client_id] = invalid_service_id();
+      }
+    } else {
+      client_to_service_map_.erase(client_id);
+    }
   }
 
-  void Clear() { client_to_service_.clear(); }
+  void Clear() {
+    client_to_service_map_.clear();
+    client_to_service_array_.clear();
+  }
 
   bool GetServiceID(ClientType client_id, ServiceType* service_id) const {
     if (client_id == 0) {
@@ -36,12 +60,22 @@ class ClientServiceMap {
       }
       return true;
     }
-    auto iter = client_to_service_.find(client_id);
-    if (iter != client_to_service_.end()) {
-      if (service_id) {
-        *service_id = iter->second;
+    if (client_id < kMaxFlatArraySize) {
+      if (client_id < client_to_service_array_.size() &&
+          client_to_service_array_[client_id] != invalid_service_id()) {
+        if (service_id) {
+          *service_id = client_to_service_array_[client_id];
+        }
+        return true;
       }
-      return true;
+    } else {
+      auto iter = client_to_service_map_.find(client_id);
+      if (iter != client_to_service_map_.end()) {
+        if (service_id) {
+          *service_id = iter->second;
+        }
+        return true;
+      }
     }
     return false;
   }
@@ -61,7 +95,16 @@ class ClientServiceMap {
       }
       return true;
     }
-    for (auto mapping : client_to_service_) {
+    for (size_t client_id_idx = 0;
+         client_id_idx < client_to_service_array_.size(); client_id_idx++) {
+      if (client_to_service_array_[client_id_idx] == service_id) {
+        if (client_id) {
+          *client_id = client_id_idx;
+        }
+        return true;
+      }
+    }
+    for (const auto& mapping : client_to_service_map_) {
       if (mapping.second == service_id) {
         if (client_id) {
           *client_id = mapping.first;
@@ -76,13 +119,28 @@ class ClientServiceMap {
     return std::numeric_limits<ServiceType>::max();
   }
 
-  typedef typename std::unordered_map<ClientType, ServiceType>::const_iterator
-      const_iterator;
-  const_iterator begin() const { return client_to_service_.begin(); }
-  const_iterator end() const { return client_to_service_.end(); }
+  template <typename FunctionType>
+  void ForEach(FunctionType func) const {
+    for (size_t client_id_idx = 0;
+         client_id_idx < client_to_service_array_.size(); client_id_idx++) {
+      if (client_to_service_array_[client_id_idx] != invalid_service_id()) {
+        func(client_id_idx, client_to_service_array_[client_id_idx]);
+      }
+    }
+    for (const auto& mapping : client_to_service_map_) {
+      func(mapping.first, mapping.second);
+    }
+  }
 
  private:
-  std::unordered_map<ClientType, ServiceType> client_to_service_;
+  std::vector<ServiceType> client_to_service_array_;
+  std::unordered_map<ClientType, ServiceType> client_to_service_map_;
+
+  // Start with 32 maximum elements in the map, which can grow.
+  static constexpr size_t kInitialFlaArraySize = 0x20;
+
+  // Experimental testing suggests that 16k is a reasonable upper limit.
+  static constexpr size_t kMaxFlatArraySize = 0x4000;
 };
 
 }  // namespace gles2
