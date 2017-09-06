e5e49df675bfee751246c8585581aff171371e36
Reilly Grant <reillyg@chromium.org>
2017-09-06 10:40:27 -0700

    Open WebSocket through loading context in dedicated worker
    Dedicated workers always share a process with the document that created
    them and this, rather than a shadow-page, is the loading context that
    they use. By opening WebSockets through this loading context user
    actions related to the frame in which this document is loaded, such as
    clicking through an SSL certificate warning interstitial, apply to
    connections from the worker as well.
    
    Bug: 760708
    Change-Id: Ibde562f36c667e419b5636ea572cf0b646262173

---
+++ net/data/websocket/connect_check_worker.js
@@ -0,0 +1,13 @@
+onmessage = () => {
+  var protocol = location.protocol.replace('http', 'ws');
+  var url = protocol + '//' + location.host + '/echo-with-no-extension';
+  var ws = new WebSocket(url);
+
+  ws.onopen = () => {
+    postMessage('PASS');
+  }
+
+  ws.onclose = () => {
+    postMessage('FAIL');
+  }
+}
