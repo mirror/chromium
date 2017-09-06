1a1b277c6862bd2ab0ea253e51b3eecbef8203b8
Chung-Sheng Wu <chungsheng@google.com>
2017-09-06 17:20:39 +0800

    Chrome Internal Page to show system information
    Shows information like CPU usage, Memory status, Zram status.
    
    Bug: 752002
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I879d5563e74f1371a39aa31f691f673cb79c8d0e

---
+++ chrome/test/data/webui/sys_internals/line_chart/unit_label_test.js
@@ -0,0 +1,36 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var LineChartTest = LineChartTest || {};
+
+LineChartTest.UnitLabel = function() {
+  suite('LineChart Label unit test', function() {
+    suiteSetup(function() {
+      TEST_MEM_UNIT = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+      TEST_MEM_UNITBASE = 1024;
+      testLabel = new LineChart.UnitLabel(TEST_MEM_UNIT, TEST_MEM_UNITBASE);
+    });
+
+    test('getSuitableUnit', function() {
+      const testValue1 = 1024 * 1024 * 1024 * 1024 * 5;
+      const result1 = LineChart.UnitLabel.getSuitableUnit(
+          testValue1, TEST_MEM_UNIT, TEST_MEM_UNITBASE);
+      assertDeepEquals(result1, {value: 5, unitIdx: 4});
+
+      const testValue2 = 1024 * 1024 * 1023;
+      const result2 = LineChart.UnitLabel.getSuitableUnit(
+          testValue2, TEST_MEM_UNIT, TEST_MEM_UNITBASE);
+      assertDeepEquals(result2, {value: 1023, unitIdx: 2});
+
+      const testValue3 = 1024 * 1024 * 1024 * 1024 * 1024 * 1024;
+      const result3 = LineChart.UnitLabel.getSuitableUnit(
+          testValue3, TEST_MEM_UNIT, TEST_MEM_UNITBASE);
+      assertDeepEquals(result3, {value: 1024, unitIdx: 5});
+    });
+
+    test
+  });
+
+  mocha.run();
+};
