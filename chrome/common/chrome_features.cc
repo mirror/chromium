7481262484d2e07aafe4c89e7f7dad3c3517f661
Claudio Magni <claudiomagni@chromium.org>
2017-09-06 14:27:36 +1000

    Change the new Accept-Language Headers logic to default-enabled.
    See design doc: https://docs.google.com/document/d/10eGUww_2Ufv-YyGwnmr9ke_89Q6By_94v02FM_NTU24
    
    The main logic was introduced in https://chromium-review.googlesource.com/c/chromium/src/+/625496
    
    Bug: 737232
    Change-Id: I8f22cbba99a571edc1f95248ebb6ba8b5806d535
    Reviewed-on: https://chromium-review.googlesource.com/647047
    Reviewed-by: Dimitri Glazkov <dglazkov@chromium.org>
    Commit-Queue: Claudio M <claudiomagni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499388}
    (cherry picked from commit 6ddc1c3486a8c1bf76b69268984bb05409a2fb31)
    
    Resolved conflicts in features file.
    
    Change-Id: I3e5b61a21f5dee147a25d996927260089e060025

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -52,6 +52,114 @@ const base::Feature kMaterialDesignHistoryFeature {
   "MaterialDesignHistory", base::FEATURE_DISABLED_BY_DEFAULT
 };
 
+<<<<<<< HEAD
+=======
+#if !defined(OS_ANDROID)
+// Enables media content bitstream remoting, an optimization that can activate
+// during Cast Tab Mirroring.
+const base::Feature kMediaRemoting{"MediaRemoting",
+                                   base::FEATURE_DISABLED_BY_DEFAULT};
+
+// If enabled, replaces the <extensionview> controller in the route details view
+// of the Media Router dialog with the controller bundled with the WebUI
+// resources.
+const base::Feature kMediaRouterUIRouteController{
+    "MediaRouterUIRouteController", base::FEATURE_DISABLED_BY_DEFAULT};
+#endif  // !defined(OS_ANDROID)
+
+// Enables or disables modal permission prompts.
+const base::Feature kModalPermissionPrompts{"ModalPermissionPrompts",
+                                            base::FEATURE_DISABLED_BY_DEFAULT};
+
+#if defined(OS_WIN)
+// Enables or disables the ModuleDatabase backend for the conflicts UI.
+const base::Feature kModuleDatabase{"ModuleDatabase",
+                                    base::FEATURE_DISABLED_BY_DEFAULT};
+#endif
+
+#if defined(OS_CHROMEOS)
+// Enables or disables multidevice features and corresponding UI on Chrome OS.
+const base::Feature kMultidevice{"Multidevice",
+                                 base::FEATURE_DISABLED_BY_DEFAULT};
+#endif
+
+// Enables the use of native notification centers instead of using the Message
+// Center for displaying the toasts.
+#if BUILDFLAG(ENABLE_NATIVE_NOTIFICATIONS)
+#if defined(OS_MACOSX) || defined(OS_ANDROID)
+const base::Feature kNativeNotifications{"NativeNotifications",
+                                         base::FEATURE_ENABLED_BY_DEFAULT};
+#else
+const base::Feature kNativeNotifications{"NativeNotifications",
+                                         base::FEATURE_DISABLED_BY_DEFAULT};
+#endif
+#endif  // BUILDFLAG(ENABLE_NATIVE_NOTIFICATIONS)
+
+#if BUILDFLAG(ENABLE_NATIVE_WINDOW_NAV_BUTTONS)
+const base::Feature kNativeWindowNavButtons{"NativeWindowNavButtons",
+                                            base::FEATURE_ENABLED_BY_DEFAULT};
+#endif  // BUILDFLAG(ENABLE_NATIVE_WINDOW_NAV_BUTTONS)
+
+const base::Feature kNetworkPrediction{"NetworkPrediction",
+                                       base::FEATURE_ENABLED_BY_DEFAULT};
+
+// If enabled, the list of content suggestions on the New Tab page will contain
+// pages that the user downloaded for later use.
+const base::Feature kOfflinePageDownloadSuggestionsFeature{
+    "NTPOfflinePageDownloadSuggestions", base::FEATURE_ENABLED_BY_DEFAULT};
+
+#if !defined(OS_ANDROID)
+// Enables or disabled the OneGoogleBar on the local NTP.
+const base::Feature kOneGoogleBarOnLocalNtp{"OneGoogleBarOnLocalNtp",
+                                            base::FEATURE_DISABLED_BY_DEFAULT};
+#endif
+
+// Adds the base language code to the Language-Accept headers if at least one
+// corresponding language+region code is present in the user preferences.
+// For example: "en-US, fr-FR" --> "en-US, en, fr-FR, fr".
+const base::Feature kUseNewAcceptLanguageHeader{
+    "UseNewAcceptLanguageHeader", base::FEATURE_ENABLED_BY_DEFAULT};
+
+// Enables Permissions Blacklisting via Safe Browsing.
+const base::Feature kPermissionsBlacklist{
+    "PermissionsBlacklist", base::FEATURE_DISABLED_BY_DEFAULT};
+
+// Disables PostScript generation when printing to PostScript capable printers
+// and instead sends Emf files.
+#if defined(OS_WIN)
+const base::Feature kDisablePostScriptPrinting{
+    "DisablePostScriptPrinting", base::FEATURE_DISABLED_BY_DEFAULT};
+#endif
+
+#if BUILDFLAG(ENABLE_PLUGINS)
+// Prefer HTML content by hiding Flash from the list of plugins.
+// https://crbug.com/626728
+const base::Feature kPreferHtmlOverPlugins{"PreferHtmlOverPlugins",
+                                           base::FEATURE_ENABLED_BY_DEFAULT};
+#endif
+
+#if defined(OS_CHROMEOS)
+// The lock screen will be preloaded so it is instantly available when the user
+// locks the Chromebook device.
+const base::Feature kPreloadLockScreen{"PreloadLockScreen",
+                                       base::FEATURE_ENABLED_BY_DEFAULT};
+#endif
+
+#if BUILDFLAG(ENABLE_PRINT_PREVIEW) && !defined(OS_WIN) && !defined(OS_MACOSX)
+// Enables the Print as Image feature in print preview.
+const base::Feature kPrintPdfAsImage{"PrintPdfAsImage",
+                                     base::FEATURE_ENABLED_BY_DEFAULT};
+#endif
+
+// Enables or disables push subscriptions keeping Chrome running in the
+// background when closed.
+const base::Feature kPushMessagingBackgroundMode{
+    "PushMessagingBackgroundMode", base::FEATURE_DISABLED_BY_DEFAULT};
+
+// Enables support for Minimal-UI PWA display mode.
+const base::Feature kPwaMinimalUi{"PwaMinimalUi",
+                                  base::FEATURE_DISABLED_BY_DEFAULT};
+
 #if defined(OS_CHROMEOS)
 // Runtime flag that indicates whether this leak detector should be enabled in
 // the current instance of Chrome.
