8dc7b6a9e9cf2d38af731a1c326dbb640e87f4d0
Tim Brown <timbrown@chromium.org>
2017-09-06 16:42:54 -0700

    Make static initializer test fail the build immediately
    The old sizes.py perf test used to check for performance regressions.
    This is now handled elsewhere, and the remaining SI check is quick
    enough that we can immediately fail the build on an SI increase, rather
    than running the test later.
    
    This CL also cleans up some of the cruft from moving the test around in
    the past.
    
    Bug: 572393
    Change-Id: I960cb8fec63e25c489e8c4e90f670c7e35dd4fd6

--- tools/linux/dump-static-initializers.py
+++ tools/linux/dump-static-initializers.py
@@ -2,7 +2,6 @@
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """Dump functions called by static intializers in a Linux Release binary.
 
 Usage example:
@@ -30,28 +29,32 @@ import sys
 
 # A map of symbol => informative text about it.
 NOTES = {
-  '__cxa_atexit@plt': 'registers a dtor to run at exit',
-  'std::__ioinit': '#includes <iostream>, use <ostream> instead',
+    '__cxa_atexit@plt': 'registers a dtor to run at exit',
+    'std::__ioinit': '#includes <iostream>, use <ostream> instead',
 }
 
 # Determine whether this is a git checkout (as opposed to e.g. svn).
 IS_GIT_WORKSPACE = (subprocess.Popen(
     ['git', 'rev-parse'], stderr=subprocess.PIPE).wait() == 0)
 
+
 class Demangler(object):
   """A wrapper around c++filt to provide a function to demangle symbols."""
+
   def __init__(self, toolchain):
-    self.cppfilt = subprocess.Popen([toolchain + 'c++filt'],
-                                    stdin=subprocess.PIPE,
-                                    stdout=subprocess.PIPE)
+    self.cppfilt = subprocess.Popen(
+        [toolchain + 'c++filt'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
   def Demangle(self, sym):
     """Given mangled symbol |sym|, return its demangled form."""
     self.cppfilt.stdin.write(sym + '\n')
     return self.cppfilt.stdout.readline().strip()
 
+
 # Matches for example: "cert_logger.pb.cc", capturing "cert_logger".
 protobuf_filename_re = re.compile(r'(.*)\.pb\.cc$')
+
+
 def QualifyFilenameAsProto(filename):
   """Attempt to qualify a bare |filename| with a src-relative path, assuming it
   is a protoc-generated file.  If a single match is found, it is returned.
@@ -63,14 +66,16 @@ def QualifyFilenameAsProto(filename):
     return filename
   basename = match.groups(0)
   gitlsfiles = subprocess.Popen(
-    ['git', 'ls-files', '--', '*/%s.proto' % basename],
-    stdout=subprocess.PIPE)
+      ['git', 'ls-files', '--',
+       '*/%s.proto' % basename],
+      stdout=subprocess.PIPE)
   candidate = filename
   for line in gitlsfiles.stdout:
     if candidate != filename:
-      return filename # Multiple hits, can't help.
+      return filename  # Multiple hits, can't help.
     candidate = line.strip()
   return candidate
+
 
 # Regex matching the substring of a symbol's demangled text representation most
 # likely to appear in a source file.
@@ -79,6 +84,8 @@ def QualifyFilenameAsProto(filename):
 # picks up any ::-qualification and the last fragment picks up a suffix that
 # starts with an opener.
 symbol_code_name_re = re.compile(r'^(?:[^(<[]*::)?([^:(<[]*).*?$')
+
+
 def QualifyFilename(filename, symbol):
   """Given a bare filename and a symbol that occurs in it, attempt to qualify
   it with a src-relative path.  If more than one file matches, return the
@@ -90,8 +97,9 @@ def QualifyFilename(filename, symbol):
     return filename
   symbol = match.group(1)
   gitgrep = subprocess.Popen(
-    ['git', 'grep', '-l', symbol, '--', '*/%s' % filename],
-    stdout=subprocess.PIPE)
+      ['git', 'grep', '-l', symbol, '--',
+       '*/%s' % filename],
+      stdout=subprocess.PIPE)
   candidate = filename
   for line in gitgrep.stdout:
     if candidate != filename:  # More than one candidate; return bare filename.
@@ -99,9 +107,12 @@ def QualifyFilename(filename, symbol):
     candidate = line.strip()
   return candidate
 
+
 # Regex matching nm output for the symbols we're interested in.
 # See test_ParseNmLine for examples.
 nm_re = re.compile(r'(\S+) (\S+) t (?:_ZN12)?_GLOBAL__(?:sub_)?I_(.*)')
+
+
 def ParseNmLine(line):
   """Given a line of nm output, parse static initializers as a
   (file, start, size) tuple."""
@@ -113,15 +124,14 @@ def ParseNmLine(line):
 
 def test_ParseNmLine():
   """Verify the nm_re regex matches some sample lines."""
-  parse = ParseNmLine(
-    '0000000001919920 0000000000000008 t '
-    '_ZN12_GLOBAL__I_safe_browsing_service.cc')
+  parse = ParseNmLine('0000000001919920 0000000000000008 t '
+                      '_ZN12_GLOBAL__I_safe_browsing_service.cc')
   assert parse == ('safe_browsing_service.cc', 26319136, 8), parse
 
-  parse = ParseNmLine(
-    '00000000026b9eb0 0000000000000024 t '
-    '_GLOBAL__sub_I_extension_specifics.pb.cc')
+  parse = ParseNmLine('00000000026b9eb0 0000000000000024 t '
+                      '_GLOBAL__sub_I_extension_specifics.pb.cc')
   assert parse == ('extension_specifics.pb.cc', 40607408, 36), parse
+
 
 # Just always run the test; it is fast enough.
 test_ParseNmLine()
@@ -129,21 +139,27 @@ test_ParseNmLine()
 
 def ParseNm(toolchain, binary):
   """Given a binary, yield static initializers as (file, start, size) tuples."""
-  nm = subprocess.Popen([toolchain + 'nm', '-S', binary],
-                        stdout=subprocess.PIPE)
+  nm = subprocess.Popen(
+      [toolchain + 'nm', '-S', binary], stdout=subprocess.PIPE)
   for line in nm.stdout:
     parse = ParseNmLine(line)
     if parse:
       yield parse
 
+
 # Regex matching objdump output for the symbols we're interested in.
 # Example line:
 #     12354ab:  (disassembly, including <FunctionReference>)
 disassembly_re = re.compile(r'^\s+[0-9a-f]+:.*<(\S+)>')
+
+
 def ExtractSymbolReferences(toolchain, binary, start, end):
   """Given a span of addresses, returns symbol references from disassembly."""
-  cmd = [toolchain + 'objdump', binary, '--disassemble',
-         '--start-address=0x%x' % start, '--stop-address=0x%x' % end]
+  cmd = [
+      toolchain + 'objdump', binary, '--disassemble',
+      '--start-address=0x%x' % start,
+      '--stop-address=0x%x' % end
+  ]
   objdump = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 
   refs = set()
@@ -158,23 +174,32 @@ def ExtractSymbolReferences(toolchain, binary, start, end):
       if ref.startswith('.LC') or ref.startswith('_DYNAMIC'):
         # Ignore these, they are uninformative.
         continue
-      if ref.startswith('_GLOBAL__I_'):
+      if re.match('_GLOBAL__(?:sub_)?I_', ref):
         # Probably a relative jump within this function.
         continue
       refs.add(ref)
 
   return sorted(refs)
 
+
 def main():
   parser = optparse.OptionParser(usage='%prog [option] filename')
-  parser.add_option('-d', '--diffable', dest='diffable',
-                    action='store_true', default=False,
-                    help='Prints the filename on each line, for more easily '
-                         'diff-able output. (Used by sizes.py)')
-  parser.add_option('-t', '--toolchain-prefix', dest='toolchain',
-                    action='store', default='',
-                    help='Toolchain prefix to append to all tool invocations '
-                         '(nm, objdump).')
+  parser.add_option(
+      '-d',
+      '--diffable',
+      dest='diffable',
+      action='store_true',
+      default=False,
+      help='Prints the filename on each line, for more easily '
+      'diff-able output. (Used by sizes.py)')
+  parser.add_option(
+      '-t',
+      '--toolchain-prefix',
+      dest='toolchain',
+      action='store',
+      default='',
+      help='Toolchain prefix to append to all tool invocations '
+      '(nm, objdump).')
   opts, args = parser.parse_args()
   if len(args) != 1:
     parser.error('missing filename argument')
@@ -201,7 +226,7 @@ def main():
       ref_output.append('[empty ctor, but it still has cost on gcc <4.6]')
     else:
       for ref in ExtractSymbolReferences(opts.toolchain, binary, addr,
-                                         addr+size):
+                                         addr + size):
         initializer_count += 1
 
         ref = demangler.Demangle(ref)
@@ -235,6 +260,7 @@ def main():
                                                        file_count)
 
   return 0
+
 
 if '__main__' == __name__:
   sys.exit(main())
