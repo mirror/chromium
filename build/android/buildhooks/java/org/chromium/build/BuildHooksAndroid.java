5c4e1403c930b477c7f0019b4cc2882950dec66e
Eric Stevenson <estevenson@chromium.org>
2017-09-06 11:34:34 -0400

    Android: Add bytecode rewriting for custom resources.
    This CL adds support for modying resources before they are used. This is
    done by changing the bytecode of places where regular resources are
    used to use custom methods in BuildHooksAndroid. More specifically, we
    rewrite all classes that meet the following three conditions:
      * Descendant of Context
      * Direct subclass of a framework class
      * Not a subclass of ContextWrapper
    
    Rewriting existing methods currently isn't supported (we don't have
    any places where this is required yet); this CL just adds support for
    adding methods.
    
    This CL also introduces a number of helper classes/methods that
    provide utilities for verifying the above criteria.
    
    Currently, custom resources aren't enabled so the added methods just
    call their respective super methods (i.e. a no-op). This bytecode
    transformation is performed for all targets that require android that
    haven't opted out.
    
    A couple of other necessary changes in this CL:
      * Added lint support for java_binary targets (opt-in)
      * Added an extra wrapper script for java_binary scripts
    
    Bug: 762157
    Change-Id: I038075681afec1ed2dddbcfe1e981b2bf8a084f3

--- build/android/buildhooks/java/org/chromium/build/BuildHooksAndroid.java
+++ build/android/buildhooks/java/org/chromium/build/BuildHooksAndroid.java
@@ -4,6 +4,8 @@
 
 package org.chromium.build;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.content.res.Resources;
 
 /**
@@ -21,14 +23,56 @@ public abstract class BuildHooksAndroid {
 
     /**
      * Hook to provide custom resources.
-     * @param resources fallback resources to use if custom resources aren't available.
-     * @return custom resources.
      */
-    public static Resources getResources(Resources resources) {
-        return sInstance.getResourcesImpl(resources);
+    public static Resources getResources(Context context) {
+        return sInstance.getResourcesImpl(context);
     }
 
-    protected Resources getResourcesImpl(Resources resources) {
-        return resources;
+    protected Resources getResourcesImpl(Context context) {
+        return null;
     }
+
+    public static AssetManager getAssets(Context context) {
+        return sInstance.getAssetsImpl(context);
+    }
+
+    protected AssetManager getAssetsImpl(Context context) {
+        return null;
+    }
+
+    public static Resources.Theme getTheme(Context context) {
+        return sInstance.getThemeImpl(context);
+    }
+
+    protected Resources.Theme getThemeImpl(Context context) {
+        return null;
+    }
+
+    public static void setTheme(int theme, Context context) {
+        sInstance.setThemeImpl(theme, context);
+    }
+
+    protected void setThemeImpl(int theme, Context context) {}
+
+    public static Context createConfigurationContext(Context context) {
+        return sInstance.createConfigurationContextImpl(context);
+    }
+
+    protected Context createConfigurationContextImpl(Context context) {
+        return null;
+    }
+
+    public static boolean isEnabled() {
+        return sInstance.isEnabledImpl();
+    }
+
+    protected boolean isEnabledImpl() {
+        return false;
+    }
+
+    public static void init() {
+        sInstance.initImpl();
+    }
+
+    protected void initImpl() {}
 }
