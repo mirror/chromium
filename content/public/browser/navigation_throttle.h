95ef382e25206b6313bb6a28d9dc012ca4d1e389
Lucas Garron <lgarron@chromium.org>
2017-09-06 02:37:19 -0700

    Committed interstitial throttle cert info plumbing (2/2): Add a WillFailRequest() method to NavigationThrottle.
    This exposes methods from //content (in NavigationThrottle and NavigationHandle)
    that will be used in the follow-up CL at crrev.com/c/621236
    
    Bug: 751941
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I9a597851d018e3e42d884c185ba2c619b9d31923

--- content/public/browser/navigation_throttle.h
+++ content/public/browser/navigation_throttle.h
@@ -17,7 +17,9 @@ class CONTENT_EXPORT NavigationThrottle {
   // This is returned to the NavigationHandle to allow the navigation to
   // proceed, or to cancel it.
   enum ThrottleCheckResult {
-    // The navigation proceeds uninterrupted.
+    // The action proceeds. This can either mean the navigation continues (e.g.
+    // for WillStartRequest) or that the navigation halts (e.g. for
+    // WillFailRequest).
     PROCEED,
 
     // Defers the navigation until the NavigationThrottle calls
@@ -72,6 +74,15 @@ class CONTENT_EXPORT NavigationThrottle {
   // implementer need to destroy the WebContents, it should return CANCEL,
   // CANCEL_AND_IGNORE or DEFER and perform the destruction asynchronously.
   virtual ThrottleCheckResult WillRedirectRequest();
+
+  // Called when a request will fail
+  //
+  // The implementer is responsible for ensuring that the WebContents this
+  // throttle is associated with remain alive during the duration of this
+  // method. Failing to do so will result in use-after-free bugs. Should the
+  // implementer need to destroy the WebContents, it should return CANCEL,
+  // CANCEL_AND_IGNORE or DEFER and perform the destruction asynchronously.
+  virtual ThrottleCheckResult WillFailRequest();
 
   // Called when a response's headers and metadata are available.
   //
