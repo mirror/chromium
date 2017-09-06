9de1bf777bdb39ef0e5378e174c586b979218805
Max Morin <maxmorin@chromium.org>
2017-09-06 16:17:24 +0200

    Introduce AudioInputDelegate.
    WIP
    
    Bug: 653861
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I79bd4ab455fd4a2f35582fa8d650d7c8e2ab13a9

--- media/audio/audio_output_delegate.cc
+++ media/audio/audio_output_delegate.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "audio_output_delegate.h"
+#include "media/audio/audio_output_delegate.h"
 
 media::AudioOutputDelegate::EventHandler::EventHandler() {}
 media::AudioOutputDelegate::EventHandler::~EventHandler() {}
