4d890e7bcd3d29d3e2d9cc611d0cbc092dc6a2df
Tom Anderson <thomasanderson@chromium.org>
2017-09-07 12:07:20 -0700

    Add targets for calculating and merging RPM dependencies
    BUG=758654
    R=thestig@chromium.org

--- chrome/installer/linux/rpm/calculate_package_deps.py
+++ chrome/installer/linux/rpm/calculate_package_deps.py
@@ -12,11 +12,12 @@ import os
 import subprocess
 import sys
 
-if len(sys.argv) != 3:
-  print 'Usage: %s binary_path dep_file' % sys.argv[0]
+if len(sys.argv) < 3:
+  print 'Usage: %s binary_path dep_file [shlib1 shlib2 ...]' % sys.argv[0]
   sys.exit(1)
 binary = os.path.abspath(sys.argv[1])
 dep_filename = sys.argv[2]
+bundled_shlibs = [os.path.basename(file) for file in sys.argv[3:]]
 
 proc = subprocess.Popen(['/usr/lib/rpm/find-requires'], stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -26,7 +27,7 @@ if exit_code != 0:
   print 'find-requires failed with exit code ' + str(exit_code)
   print 'stderr was ' + stderr
   sys.exit(1)
-requires = stdout.rstrip().split('\n')
+requires = stdout.split('\n')
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 distro_package_provides = json.load(open(os.path.join(
@@ -35,6 +36,10 @@ distro_package_provides = json.load(open(os.path.join(
 ret_code = 0
 for distro in distro_package_provides:
   for requirement in requires:
+    if requirement == '':
+      continue
+    if any([requirement.startswith(shlib) for shlib in bundled_shlibs]):
+      continue
     if requirement not in distro_package_provides[distro]:
       print >> sys.stderr, (
           'Unexpected new dependency %s on distro %s caused by binary %s' % (
