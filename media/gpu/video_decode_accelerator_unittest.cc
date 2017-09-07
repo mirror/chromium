9e8d34c8963bc38a77d753ecf5107b162f144ec0
Owen Lin <owenlin@google.com>
2017-09-07 13:16:42 +0800

    rendering_helper: Use Offscreen GL surface.
    The goal is to make vda_unittest less dependent on the platforms.
    
    BUG=753273
    TEST=Run test on a CrOS device.
    
    Change-Id: I6e3319dd329996c879d251f49bdfcc0fe84b542f

--- media/gpu/video_decode_accelerator_unittest.cc
+++ media/gpu/video_decode_accelerator_unittest.cc
@@ -124,9 +124,6 @@ const base::FilePath::CharType* g_output_log = NULL;
 // The value is set by the switch "--rendering_fps".
 double g_rendering_fps = 60;
 
-// The value is set by the switch "--rendering_warm_up".
-int g_rendering_warm_up = 0;
-
 // The value is set by the switch "--num_play_throughs". The video will play
 // the specified number of times. In different test cases, we have different
 // values for |num_play_throughs|. This setting will override the value. A
@@ -1219,8 +1216,6 @@ void VideoDecodeAcceleratorTest::TearDown() {
       FROM_HERE, base::Bind(&RenderingHelper::UnInitialize,
                             base::Unretained(&rendering_helper_), &done));
   done.Wait();
-
-  rendering_helper_.TearDown();
 }
 
 void VideoDecodeAcceleratorTest::ParseAndReadTestVideoData(
@@ -1292,8 +1287,6 @@ void VideoDecodeAcceleratorTest::UpdateTestVideoFileParams(
 
 void VideoDecodeAcceleratorTest::InitializeRenderingHelper(
     const RenderingHelperParams& helper_params) {
-  rendering_helper_.Setup();
-
   base::WaitableEvent done(base::WaitableEvent::ResetPolicy::AUTOMATIC,
                            base::WaitableEvent::InitialState::NOT_SIGNALED);
   g_env->GetRenderingTaskRunner()->PostTask(
@@ -1406,7 +1399,6 @@ TEST_P(VideoDecodeAcceleratorParamTest, TestSimpleDecode) {
 
   RenderingHelperParams helper_params;
   helper_params.rendering_fps = g_rendering_fps;
-  helper_params.warm_up_iterations = g_rendering_warm_up;
   helper_params.render_as_thumbnails = render_as_thumbnails;
   if (render_as_thumbnails) {
     // Only one decoder is supported with thumbnail rendering
@@ -1415,6 +1407,7 @@ TEST_P(VideoDecodeAcceleratorParamTest, TestSimpleDecode) {
     helper_params.thumbnail_size = kThumbnailSize;
   }
 
+  helper_params.num_windows = num_concurrent_decoders;
   // First kick off all the decoders.
   for (size_t index = 0; index < num_concurrent_decoders; ++index) {
     TestVideoFile* video_file =
@@ -1439,10 +1432,6 @@ TEST_P(VideoDecodeAcceleratorParamTest, TestSimpleDecode) {
             render_as_thumbnails);
 
     clients_[index] = std::move(client);
-    helper_params.window_sizes.push_back(
-        render_as_thumbnails
-            ? kThumbnailsPageSize
-            : gfx::Size(video_file->width, video_file->height));
   }
 
   InitializeRenderingHelper(helper_params);
@@ -1786,8 +1775,7 @@ TEST_F(VideoDecodeAcceleratorTest, TestDecodeTimeMedian) {
       test_video_files_[0]->profile, g_fake_decoder, true,
       std::numeric_limits<int>::max(), kWebRtcDecodeCallsPerSecond, false));
   RenderingHelperParams helper_params;
-  helper_params.window_sizes.push_back(
-      gfx::Size(test_video_files_[0]->width, test_video_files_[0]->height));
+  helper_params.num_windows = 1;
   InitializeRenderingHelper(helper_params);
   CreateAndStartDecoder(clients_[0].get(), notes_[0].get());
   ClientState last_state = WaitUntilDecodeFinish(notes_[0].get());
@@ -1815,8 +1803,7 @@ TEST_F(VideoDecodeAcceleratorTest, NoCrash) {
       test_video_files_[0]->profile, g_fake_decoder, true,
       std::numeric_limits<int>::max(), 0, false));
   RenderingHelperParams helper_params;
-  helper_params.window_sizes.push_back(
-      gfx::Size(test_video_files_[0]->width, test_video_files_[0]->height));
+  helper_params.num_windows = 1;
   InitializeRenderingHelper(helper_params);
   CreateAndStartDecoder(clients_[0].get(), notes_[0].get());
   WaitUntilDecodeFinish(notes_[0].get());
@@ -1896,8 +1883,7 @@ int main(int argc, char** argv) {
       continue;
     }
     if (it->first == "rendering_warm_up") {
-      std::string input(it->second.begin(), it->second.end());
-      LOG_ASSERT(base::StringToInt(input, &media::g_rendering_warm_up));
+      // TODO(owenlin): Remove this after autotest stop using it.
       continue;
     }
     // TODO(owenlin): Remove this flag once it is not used in autotest.
