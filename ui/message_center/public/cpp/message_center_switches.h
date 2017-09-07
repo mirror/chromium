087c95b096891f1dc21d99a5849823c725b10a58
Evan Stade <estade@chromium.org>
2017-09-07 12:15:57 -0600

    Create public cpp directory for Message Center, move constants and flags there.
    Bug: 755413
    Change-Id: I8d804a160e33b8667942aa723e031beb3d67bf8e

---
+++ ui/message_center/public/cpp/message_center_switches.h
@@ -0,0 +1,35 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_MESSAGE_CENTER_PUBLIC_CPP_MESSAGE_CENTER_SWITCHES_H_
+#define UI_MESSAGE_CENTER_PUBLIC_CPP_MESSAGE_CENTER_SWITCHES_H_
+
+#include "ui/message_center/public/cpp/message_center_public_export.h"
+
+namespace message_center {
+
+// Returns if new style notification is enabled, i.e. NotificationViewMD is
+// used instead of NotificationView.
+bool MESSAGE_CENTER_PUBLIC_EXPORT IsNewStyleNotificationEnabled();
+
+namespace switches {
+
+MESSAGE_CENTER_PUBLIC_EXPORT extern const char
+    kEnableMessageCenterAlwaysScrollUpUponNotificationRemoval[];
+
+// Flag to enable or disable notification changes while the message center
+// opens.  Possible values are "" (meant default), "enabled" or "disabled".
+// This flag will be removed once the feature gets stable.
+MESSAGE_CENTER_PUBLIC_EXPORT extern const char kMessageCenterChangesWhileOpen[];
+
+MESSAGE_CENTER_PUBLIC_EXPORT extern const char
+    kEnableMessageCenterNewStyleNotification[];
+MESSAGE_CENTER_PUBLIC_EXPORT extern const char
+    kDisableMessageCenterNewStyleNotification[];
+
+}  // namespace switches
+
+}  // namespace message_center
+
+#endif  // UI_MESSAGE_CENTER_PUBLIC_CPP_MESSAGE_CENTER_SWITCHES_H_
