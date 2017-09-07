37de7e9f7b31f8adc5f921741deb5d4038fdcd59
Ira Burak <iburak@google.com>
2017-09-07 13:19:10 -0400

    CL for testing changes. Bug:
    Change-Id: I6bd2764d1aaec2b0343a8e21ac167b130a13e762

--- base/metrics/statistics_recorder.cc
+++ base/metrics/statistics_recorder.cc
@@ -15,6 +15,7 @@
 #include "base/metrics/histogram_snapshot_manager.h"
 #include "base/metrics/metrics_hashes.h"
 #include "base/metrics/persistent_histogram_allocator.h"
+#include "base/metrics/record_histogram_checker.h"
 #include "base/stl_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
@@ -34,6 +35,8 @@ bool HistogramNameLesser(const base::HistogramBase* a,
 
 namespace base {
 
+bool StatisticsRecorder::record_checker_enabled_ = true;
+
 StatisticsRecorder::~StatisticsRecorder() {
   DCHECK(histograms_);
   DCHECK(ranges_);
@@ -45,6 +48,7 @@ StatisticsRecorder::~StatisticsRecorder() {
   callbacks_ = existing_callbacks_.release();
   ranges_ = existing_ranges_.release();
   providers_ = existing_providers_.release();
+  record_checker_ = existing_record_checker_.release();
 }
 
 // static
@@ -444,6 +448,25 @@ void StatisticsRecorder::UninitializeForTesting() {
 }
 
 // static
+void StatisticsRecorder::SetRecordChecker(
+    std::unique_ptr<RecordHistogramChecker> record_checker) {
+  if (!record_checker_) {
+    record_checker_ = record_checker.release();
+  }
+}
+
+// static
+void StatisticsRecorder::SetRecordCheckerEnabled(bool enabled) {
+  record_checker_enabled_ = enabled;
+}
+
+// static
+bool StatisticsRecorder::ShouldRecordHistogram(uint64_t histogram_hash) {
+  return !(record_checker_enabled_ && record_checker_ &&
+           !record_checker_->ShouldRecord(histogram_hash));
+}
+
+// static
 std::vector<HistogramBase*> StatisticsRecorder::GetKnownHistograms(
     bool include_persistent) {
   std::vector<HistogramBase*> known;
@@ -485,11 +508,13 @@ StatisticsRecorder::StatisticsRecorder() {
   existing_callbacks_.reset(callbacks_);
   existing_ranges_.reset(ranges_);
   existing_providers_.reset(providers_);
+  existing_record_checker_.reset(record_checker_);
 
   histograms_ = new HistogramMap;
   callbacks_ = new CallbackMap;
   ranges_ = new RangesMap;
   providers_ = new HistogramProviders;
+  record_checker_ = nullptr;
 
   InitLogOnShutdownWithoutLock();
 }
@@ -503,21 +528,23 @@ void StatisticsRecorder::InitLogOnShutdownWithoutLock() {
 
 // static
 void StatisticsRecorder::Reset() {
-
   std::unique_ptr<HistogramMap> histograms_deleter;
   std::unique_ptr<CallbackMap> callbacks_deleter;
   std::unique_ptr<RangesMap> ranges_deleter;
   std::unique_ptr<HistogramProviders> providers_deleter;
+  std::unique_ptr<RecordHistogramChecker> record_checker_deleter;
   {
     base::AutoLock auto_lock(lock_.Get());
     histograms_deleter.reset(histograms_);
     callbacks_deleter.reset(callbacks_);
     ranges_deleter.reset(ranges_);
     providers_deleter.reset(providers_);
+    record_checker_deleter.reset(record_checker_);
     histograms_ = nullptr;
     callbacks_ = nullptr;
     ranges_ = nullptr;
     providers_ = nullptr;
+    record_checker_ = nullptr;
   }
   // We are going to leak the histograms and the ranges.
 }
@@ -538,6 +565,8 @@ StatisticsRecorder::CallbackMap* StatisticsRecorder::callbacks_ = nullptr;
 StatisticsRecorder::RangesMap* StatisticsRecorder::ranges_ = nullptr;
 // static
 StatisticsRecorder::HistogramProviders* StatisticsRecorder::providers_;
+// static
+RecordHistogramChecker* StatisticsRecorder::record_checker_ = nullptr;
 // static
 base::LazyInstance<base::Lock>::Leaky StatisticsRecorder::lock_ =
     LAZY_INSTANCE_INITIALIZER;
