98dc5e9bb99af34c1543ac5f41dde2d6bf647b0a
Peter Beverloo <peter@chromium.org>
2017-09-07 19:50:07 +0100

    Notification handlers should use base::Optional<>
    Not all arguments may always be available. Use base::Optional<> on the
    affected values to represent that state.
    
    This CL also fixes an issue where the WebNotificationDelegate, which
    bridges between the message center delegates and the new notification
    handlers, did not appropriately forward the by_user flag.
    
    BUG=760608
    
    Change-Id: I80853df2960753bbe858f5daa41441f9d354ee25

--- chrome/browser/extensions/api/notifications/extension_notification_handler_unittest.cc
+++ chrome/browser/extensions/api/notifications/extension_notification_handler_unittest.cc
@@ -69,7 +69,7 @@ TEST_F(ExtensionNotificationHandlerTest, CloseHandler) {
   TestExtensionNotificationHandler handler;
   handler.SetTestExpectations(kChromeExtensionId, "notifications.onClosed", 2);
   handler.OnClose(profile.get(), kChromeExtensionOrigin, kChromeNotificationId,
-                  false);
+                  false /* by_user */);
 }
 
 TEST_F(ExtensionNotificationHandlerTest, ClickHandler) {
@@ -80,7 +80,7 @@ TEST_F(ExtensionNotificationHandlerTest, ClickHandler) {
   TestExtensionNotificationHandler handler;
   handler.SetTestExpectations(kChromeExtensionId, "notifications.onClicked", 1);
   handler.OnClick(profile.get(), kChromeExtensionOrigin, kChromeNotificationId,
-                  -1, base::NullableString16() /* reply */);
+                  base::nullopt /* action_index */, base::nullopt /* reply */);
 }
 
 TEST_F(ExtensionNotificationHandlerTest, ClickHandlerButton) {
@@ -92,7 +92,7 @@ TEST_F(ExtensionNotificationHandlerTest, ClickHandlerButton) {
   handler.SetTestExpectations(kChromeExtensionId,
                               "notifications.onButtonClicked", 2);
   handler.OnClick(profile.get(), kChromeExtensionOrigin, kChromeNotificationId,
-                  1, base::NullableString16() /* reply */);
+                  1 /* action_index */, base::nullopt /* reply */);
 }
 
 }  // namespace extensions
