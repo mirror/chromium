c04fbd8c65b993f703c74486bed976337f7ee0df
Tibor Goldschwendt <tiborg@chromium.org>
2017-09-07 15:06:24 -0400

    [vr] Add frame cycle times telemetry test
    The benchmark added here uses the metric from:
    https://codereview.chromium.org/3010693002/
    
    This CL is based on
    https://chromium-review.googlesource.com/c/chromium/src/+/567604
    and was started with leilei@. Credit is due.
    
    Bug: 726906
    Change-Id: I0336f060e3b82efca4dded41929c18bbf1bf8504

--- tools/perf/contrib/vr_benchmarks/vr_page_sets/webvr_sample_pages.py
+++ tools/perf/contrib/vr_benchmarks/vr_page_sets/webvr_sample_pages.py
@@ -1,33 +1,22 @@
 # Copyright 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-import os
-from telemetry.page import page as page_module
+
 from telemetry import story
-from contrib.vr_benchmarks.vr_page_sets import (shared_android_vr_page_state
-                                                as vr_state)
+from contrib.vr_benchmarks.vr_page_sets.vr_sample_page import VrSamplePage
 
 
-SAMPLE_DIR = os.path.join(os.path.dirname(__file__),
-                          '..', '..', '..', '..', '..',
-                          'chrome', 'test', 'data', 'vr',
-                          'webvr_info', 'samples')
+class WebVrSamplePage(VrSamplePage):
 
-
-class WebVrSamplePage(page_module.Page):
-  def __init__(self, get_parameters, page_set):
-    url = 'test-slow-render.html'
-    if get_parameters:
-      url += '?' + '&'.join(get_parameters)
-    name = url.replace('.html', '')
-    url = 'file://' + os.path.join(SAMPLE_DIR, url)
+  def __init__(self, page_set, get_parameters):
     super(WebVrSamplePage, self).__init__(
-        url=url, page_set=page_set, name=name,
-        shared_page_state_class=vr_state.SharedAndroidVrPageState)
+        sample_page='test-slow-render',
+        page_set=page_set,
+        get_parameters=get_parameters)
 
   def RunPageInteractions(self, action_runner):
-      action_runner.TapElement(selector='canvas[id="webgl-canvas"]')
-      action_runner.MeasureMemory(True)
+    action_runner.TapElement(selector='canvas[id="webgl-canvas"]')
+    action_runner.MeasureMemory(True)
 
 
 class WebVrSamplePageSet(story.StorySet):
@@ -37,20 +26,32 @@ class WebVrSamplePageSet(story.StorySet):
     super(WebVrSamplePageSet, self).__init__()
 
     # Standard sample app with no changes
-    self.AddStory(WebVrSamplePage(['canvasClickPresents=1',
-                                   'renderScale=1'], self))
+    self.AddStory(
+        WebVrSamplePage(self, [
+            'canvasClickPresents=1',
+            'renderScale=1',
+        ]))
     # Increased render scale
-    self.AddStory(WebVrSamplePage(['canvasClickPresents=1',
-                                   'renderScale=1.5'], self))
+    self.AddStory(
+        WebVrSamplePage(self, [
+            'canvasClickPresents=1',
+            'renderScale=1.5',
+        ]))
     # Default render scale, increased load
-    self.AddStory(WebVrSamplePage(['canvasClickPresents=1',
-                                   'renderScale=1',
-                                   'heavyGpu=1',
-                                   'cubeScale=0.2',
-                                   'workTime=5'], self))
+    self.AddStory(
+        WebVrSamplePage(self, [
+            'canvasClickPresents=1',
+            'renderScale=1',
+            'heavyGpu=1',
+            'cubeScale=0.2',
+            'workTime=5',
+        ]))
     # Further increased load
-    self.AddStory(WebVrSamplePage(['canvasClickPresents=1',
-                                   'renderScale=1',
-                                   'heavyGpu=1',
-                                   'cubeScale=0.3',
-                                   'workTime=10'], self))
+    self.AddStory(
+        WebVrSamplePage(self, [
+            'canvasClickPresents=1',
+            'renderScale=1',
+            'heavyGpu=1',
+            'cubeScale=0.3',
+            'workTime=10',
+        ]))
