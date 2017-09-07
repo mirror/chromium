4ea38a071a0c495e1293b08985264cdc0bc1e25f
Biao She <bshe@chromium.org>
2017-09-07 16:49:15 -0400

    Recursively check if elements are rendered with correct opacity
    Bug: 761013
    Change-Id: Ife4a8cef1c088a9e2ec445ca2a4588de13524577

--- chrome/browser/vr/ui_scene_manager_unittest.cc
+++ chrome/browser/vr/ui_scene_manager_unittest.cc
@@ -62,6 +62,9 @@ std::set<UiElementName> kHitTestableElements = {
     kLoadingIndicator,
     kCloseButton,
 };
+std::set<UiElementName> kSpecialOpacityElements = {
+    kScreenDimmer,
+};
 
 static constexpr float kTolerance = 1e-5;
 
@@ -80,6 +83,45 @@ void CheckHitTestableRecursive(UiElement* element) {
   for (const auto& child : element->children()) {
     CheckHitTestableRecursive(child.get());
   }
+}
+
+void VerifyChildElementsRendererOpacity(UiElement* parent) {
+  for (auto& child : parent->children()) {
+    FakeUiElementRenderer renderer;
+    child->Render(&renderer, kProjMatrix);
+
+    // We skip opacity verification for the following two cases:
+    // 1. |renderer| is not called in |Render| function (for example UiElement).
+    // 2. The element has special opacity (for example ScreenDimmer).
+    if (!renderer.called() || kSpecialOpacityElements.find(child->name()) !=
+                                  kSpecialOpacityElements.end()) {
+      continue;
+    }
+    EXPECT_FLOAT_EQ(renderer.opacity(), child->computed_opacity())
+        << "element name: " << child->name();
+  }
+}
+
+void CheckChildRendererOpacityRecursive(UiSceneManagerTest* test,
+                                        UiElement* element) {
+  if (element->children().size() == 0)
+    return;
+
+  // Make sure all elements are visible.
+  element->SetOpacity(1.0);
+  for (auto& child : element->children()) {
+    child->SetOpacity(0.8);
+  }
+  test->AnimateBy(MsToDelta(0));
+  VerifyChildElementsRendererOpacity(element);
+
+  // Change parent opacity.
+  element->SetOpacity(0.5);
+  test->AnimateBy(MsToDelta(10));
+  VerifyChildElementsRendererOpacity(element);
+
+  for (auto& child : element->children())
+    CheckChildRendererOpacityRecursive(test, child.get());
 }
 
 }  // namespace
@@ -619,49 +661,15 @@ TEST_F(UiSceneManagerTest, DontPropagateContentBoundsOnNegligibleChange) {
   manager_->OnProjMatrixChanged(kProjMatrix);
 }
 
-TEST_F(UiSceneManagerTest, ApplyParentOpacity) {
+TEST_F(UiSceneManagerTest, RendererUsesCorrectOpacity) {
   MakeManager(kNotInCct, kNotInWebVr);
-  FakeUiElementRenderer renderer;
-  auto grid_element = base::MakeUnique<Grid>();
-  grid_element->set_draw_phase(kPhaseForeground);
-  Grid* grid = grid_element.get();
-  scene_->AddUiElement(k2dBrowsingContentGroup, std::move(grid_element));
-  auto rect_element = base::MakeUnique<Rect>();
-  rect_element->set_draw_phase(kPhaseForeground);
-  Rect* rect = rect_element.get();
-  scene_->AddUiElement(k2dBrowsingContentGroup, std::move(rect_element));
 
-  ContentElement* content_quad =
+  ContentElement* content_element =
       static_cast<ContentElement*>(scene_->GetUiElementByName(kContentQuad));
-  content_quad->set_texture_id(1);
-  TexturedElement* textured_element =
-      static_cast<TexturedElement*>(scene_->GetUiElementByName(kExitPrompt));
-  textured_element->SetVisible(true);
-  textured_element->SetInitializedForTesting();
+  content_element->set_texture_id(1);
+  TexturedElement::SetInitializedForTesting();
 
-  AnimateBy(MsToDelta(0));
-  content_quad->Render(&renderer, kProjMatrix);
-  EXPECT_FLOAT_EQ(renderer.texture_opacity(), content_quad->computed_opacity());
-  textured_element->Render(&renderer, kProjMatrix);
-  EXPECT_FLOAT_EQ(renderer.texture_opacity(),
-                  textured_element->computed_opacity());
-  grid->Render(&renderer, kProjMatrix);
-  EXPECT_FLOAT_EQ(renderer.grid_opacity(), grid->computed_opacity());
-  rect->Render(&renderer, kProjMatrix);
-  EXPECT_FLOAT_EQ(renderer.grid_opacity(), rect->computed_opacity());
-
-  // Change parent opacity.
-  scene_->GetUiElementByName(k2dBrowsingContentGroup)->SetOpacity(0.2);
-  AnimateBy(MsToDelta(0));
-  content_quad->Render(&renderer, kProjMatrix);
-  EXPECT_FLOAT_EQ(renderer.texture_opacity(), content_quad->computed_opacity());
-  textured_element->Render(&renderer, kProjMatrix);
-  EXPECT_FLOAT_EQ(renderer.texture_opacity(),
-                  textured_element->computed_opacity());
-  grid->Render(&renderer, kProjMatrix);
-  EXPECT_FLOAT_EQ(renderer.grid_opacity(), grid->computed_opacity());
-  rect->Render(&renderer, kProjMatrix);
-  EXPECT_FLOAT_EQ(renderer.grid_opacity(), rect->computed_opacity());
+  CheckChildRendererOpacityRecursive(this, &scene_->root_element());
 }
 
 }  // namespace vr
