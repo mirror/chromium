2b82c65c63d34cf1e5f958a3dd9bcd2adb8b0d03
Eric Roman <eroman@chromium.org>
2017-09-07 11:46:19 -0700

    Rename "parse_ocsp" files to "ocsp".
    It already contains non-parsing code, and I would like to put verification code inside too.
    
    Change-Id: Ia80696faf23ad1281818af39be325b60acbda21c

--- net/tools/update_ios_bundle_data.py
+++ net/tools/update_ios_bundle_data.py
@@ -47,7 +47,7 @@ net_unittest_bundle_data_globs = [
     "data/filter_unittests/*",
     "data/name_constraints_unittest/*.pem",
     "data/parse_certificate_unittest/*.pem",
-    "data/parse_ocsp_unittest/*.pem",
+    "data/ocsp_unittest/*.pem",
     "data/test.html",
     "data/url_request_unittest/*",
     "data/verify_certificate_chain_unittest/**/*.pem",
