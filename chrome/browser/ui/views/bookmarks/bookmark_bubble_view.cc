c5e0e8ba87c9242a13eaf60d88deec36c2d4726c
Trent Apted <tapted@chromium.org>
2017-09-07 14:19:58 +1000

    Use the Harmony textfield stack in the add bookmark bubble.
    And remove colons from the field labels (both for Harmony
    and non-Harmony).
    
    The non-Harmony DISTANCE_CONTROL_LIST_VERTICAL also changes
    from 20px (DISTANCE_UNRELATED_CONTROL_VERTICAL) to 8px
    (DISTANCE_RELATED_CONTROL_VERTICAL). This avoids excessive
    vertical padding between the fields. The result is that the
    non-Harmony http-auth and add bookmark dialogs fields get
    more compact, which looks OK.
    
    Bug: 726187
    Change-Id: I16785ab33294fd3c9c8bcc659360c955c68abcd7

--- chrome/browser/ui/views/bookmarks/bookmark_bubble_view.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bubble_view.cc
@@ -15,6 +15,7 @@
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/sync/sync_promo_ui.h"
 #include "chrome/browser/ui/views/harmony/chrome_layout_provider.h"
+#include "chrome/browser/ui/views/harmony/textfield_layout.h"
 #include "chrome/browser/ui/views/sync/bubble_sync_promo_view.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
@@ -41,25 +42,6 @@
 using base::UserMetricsAction;
 using bookmarks::BookmarkModel;
 using bookmarks::BookmarkNode;
-
-namespace {
-
-// This combobox prevents any lengthy content from stretching the bubble view.
-class UnsizedCombobox : public views::Combobox {
- public:
-  explicit UnsizedCombobox(ui::ComboboxModel* model) : views::Combobox(model) {}
-  ~UnsizedCombobox() override {}
-
-  // views::Combobox:
-  gfx::Size CalculatePreferredSize() const override {
-    return gfx::Size(0, views::Combobox::CalculatePreferredSize().height());
-  }
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(UnsizedCombobox);
-};
-
-}  // namespace
 
 BookmarkBubbleView* BookmarkBubbleView::bookmark_bubble_ = nullptr;
 
@@ -298,43 +280,21 @@ void BookmarkBubbleView::Init() {
   bookmark_contents_view_ = new views::View();
   GridLayout* layout = GridLayout::CreateAndInstall(bookmark_contents_view_);
 
-  // This column set is used for the labels and textfields.
   constexpr int kColumnId = 0;
-  constexpr float kFixed = 0.f;
-  constexpr float kStretchy = 1.f;
-  views::ColumnSet* cs = layout->AddColumnSet(kColumnId);
-  ChromeLayoutProvider* provider = ChromeLayoutProvider::Get();
-
-  cs->AddColumn(provider->GetControlLabelGridAlignment(), GridLayout::CENTER,
-                kFixed, GridLayout::USE_PREF, 0, 0);
-  cs->AddPaddingColumn(kFixed, provider->GetDistanceMetric(
-                                   DISTANCE_UNRELATED_CONTROL_HORIZONTAL));
-  cs->AddColumn(GridLayout::FILL, GridLayout::CENTER, kStretchy,
-                GridLayout::USE_PREF, 0, 0);
-
-  layout->StartRow(kFixed, kColumnId);
-  views::Label* label = new views::Label(
-      l10n_util::GetStringUTF16(IDS_BOOKMARK_BUBBLE_NAME_LABEL));
-  layout->AddView(label);
-
-  name_field_ = new views::Textfield();
+  ConfigureTextfieldStack(layout, kColumnId);
+  name_field_ = AddFirstTextfieldRow(
+      layout, l10n_util::GetStringUTF16(IDS_BOOKMARK_BUBBLE_NAME_LABEL),
+      kColumnId);
   name_field_->SetText(GetBookmarkName());
   name_field_->SetAccessibleName(
       l10n_util::GetStringUTF16(IDS_BOOKMARK_AX_BUBBLE_NAME_LABEL));
-  layout->AddView(name_field_);
 
-  layout->StartRowWithPadding(
-      kFixed, kColumnId, kFixed,
-      provider->GetDistanceMetric(views::DISTANCE_RELATED_CONTROL_VERTICAL));
-  views::Label* combobox_label = new views::Label(
-      l10n_util::GetStringUTF16(IDS_BOOKMARK_BUBBLE_FOLDER_LABEL));
-  layout->AddView(combobox_label);
-
-  parent_combobox_ = new UnsizedCombobox(&parent_model_);
+  parent_combobox_ = AddComboboxRow(
+      layout, l10n_util::GetStringUTF16(IDS_BOOKMARK_BUBBLE_FOLDER_LABEL),
+      &parent_model_, kColumnId);
   parent_combobox_->set_listener(this);
   parent_combobox_->SetAccessibleName(
       l10n_util::GetStringUTF16(IDS_BOOKMARK_AX_BUBBLE_FOLDER_LABEL));
-  layout->AddView(parent_combobox_);
 
   AddChildView(bookmark_contents_view_);
 }
