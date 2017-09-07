97ad21717e16bf3090384daf7272430e0805e325
Yutaka Hirano <yhirano@chromium.org>
2017-09-07 21:04:34 +0900

    [DO NOT COMMIT] Disable KeepAliveRendererForKeepaliveRequests
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I9be3af8059131393c4ac8903a4543fcae824ff14

--- content/browser/renderer_host/render_process_host_browsertest.cc
+++ content/browser/renderer_host/render_process_host_browsertest.cc
@@ -698,59 +698,5 @@ IN_PROC_BROWSER_TEST_F(RenderProcessHostTest,
     rph->RemoveObserver(this);
 }
 
-IN_PROC_BROWSER_TEST_F(RenderProcessHostTest, KeepAliveRendererProcess) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeatureWithParameters(
-      features::kKeepAliveRendererForKeepaliveRequests,
-      {std::make_pair("timeout_in_sec", "30")});
-
-  embedded_test_server()->RegisterRequestHandler(
-      base::BindRepeating(HandleBeacon));
-  ASSERT_TRUE(embedded_test_server()->Start());
-  RenderProcessHostImpl* rph = static_cast<RenderProcessHostImpl*>(
-      shell()->web_contents()->GetMainFrame()->GetProcess());
-
-  host_destructions_ = 0;
-  process_exits_ = 0;
-  rph->AddObserver(this);
-
-  NavigateToURL(shell(), embedded_test_server()->GetURL("/send-beacon.html"));
-  base::TimeTicks start = base::TimeTicks::Now();
-  NavigateToURL(shell(), GURL("data:text/html,<p>hello</p>"));
-
-  WaitUntilProcessExits(1);
-
-  EXPECT_LT(base::TimeTicks::Now() - start, base::TimeDelta::FromSeconds(30));
-  if (!host_destructions_)
-    rph->RemoveObserver(this);
-}
-
-IN_PROC_BROWSER_TEST_F(RenderProcessHostTest, KeepAliveRendererProcess_Hung) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeatureWithParameters(
-      features::kKeepAliveRendererForKeepaliveRequests,
-      {std::make_pair("timeout_in_sec", "1")});
-
-  embedded_test_server()->RegisterRequestHandler(
-      base::BindRepeating(HandleHungBeacon));
-  ASSERT_TRUE(embedded_test_server()->Start());
-  RenderProcessHostImpl* rph = static_cast<RenderProcessHostImpl*>(
-      shell()->web_contents()->GetMainFrame()->GetProcess());
-
-  host_destructions_ = 0;
-  process_exits_ = 0;
-  rph->AddObserver(this);
-
-  NavigateToURL(shell(), embedded_test_server()->GetURL("/send-beacon.html"));
-  base::TimeTicks start = base::TimeTicks::Now();
-  NavigateToURL(shell(), GURL("data:text/html,<p>hello</p>"));
-
-  WaitUntilProcessExits(1);
-
-  EXPECT_GE(base::TimeTicks::Now() - start, base::TimeDelta::FromSeconds(1));
-  if (!host_destructions_)
-    rph->RemoveObserver(this);
-}
-
 }  // namespace
 }  // namespace content
