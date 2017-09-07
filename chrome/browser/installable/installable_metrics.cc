5c1d82dfdf80ce1c0b93186987eb2f9ffa9c18c8
Dominick Ng <dominickn@chromium.org>
2017-09-07 15:59:47 +1000

    Add metrics to determine how often the A2HS dialog times out.
    This CL adds precise metrics to the add to homescreen dialog on Android,
    which currently has a strict 4 second timeout for fetching a site's
    manifest and icon, and for checking its installability status. The
    intention of the metric is to determine how often PWAs trigger a timeout
    in the dialog at a) the manifest and icon (step 1), and b) checking
    installability (step 2).
    
    The metric is recorded on the InstallableManager, which knows when sites
    are and are not PWAs. To ensure that we fully complete the check as
    often as possible, a timeout during the manifest + icon fetch will
    trigger the installability check in the background. This additional
    check will be:
      a) queued if there is some other work already happening
      b) entirely device local if it is run (checking SW and the already
         downloaded icon)
      c) cancelled if the user navigates away from the page or closes the
         tab
    
    BUG=692630
    
    Change-Id: If0b0e904f4d81b0736ea144f583350416940180c

--- chrome/browser/installable/installable_metrics.cc
+++ chrome/browser/installable/installable_metrics.cc
@@ -6,15 +6,165 @@
 
 #include "base/metrics/histogram_macros.h"
 
-void InstallableMetrics::RecordMenuOpenHistogram(
-    InstallabilityCheckStatus status) {
+namespace {
+
+void RecordMenuOpenHistogram(InstallabilityCheckStatus status) {
   UMA_HISTOGRAM_ENUMERATION("Webapp.InstallabilityCheckStatus.MenuOpen", status,
                             InstallabilityCheckStatus::COUNT);
 }
 
-void InstallableMetrics::RecordMenuItemAddToHomescreenHistogram(
-    InstallabilityCheckStatus status) {
+void RecordMenuItemAddToHomescreenHistogram(InstallabilityCheckStatus status) {
   UMA_HISTOGRAM_ENUMERATION(
       "Webapp.InstallabilityCheckStatus.MenuItemAddToHomescreen", status,
       InstallabilityCheckStatus::COUNT);
+}
+
+void RecordAddToHomescreenHistogram(AddToHomescreenTimeoutStatus status) {
+  UMA_HISTOGRAM_ENUMERATION(
+      "Webapp.InstallabilityCheckStatus.AddToHomescreenTimeout", status,
+      AddToHomescreenTimeoutStatus::COUNT);
+}
+
+}  // anonymous namespace
+
+InstallableMetrics::InstallableMetrics()
+    : status_(InstallabilityCheckStatus::NOT_STARTED),
+      menu_open_count_(0),
+      menu_item_add_to_homescreen_count_(0),
+      add_to_homescreen_manifest_timeout_count_(0),
+      add_to_homescreen_installability_timeout_count_(0) {}
+
+InstallableMetrics::~InstallableMetrics() {}
+
+void InstallableMetrics::Start() {
+  if (status_ == InstallabilityCheckStatus::NOT_STARTED)
+    status_ = InstallabilityCheckStatus::NOT_COMPLETED;
+}
+
+InstallabilityCheckStatus InstallableMetrics::GetStatusForTesting() const {
+  return status_;
+}
+
+void InstallableMetrics::RecordMenuOpen() {
+  if (is_pwa_check_complete())
+    RecordMenuOpenHistogram(status_);
+  else
+    ++menu_open_count_;
+}
+
+void InstallableMetrics::RecordMenuItemAddToHomescreen() {
+  if (is_pwa_check_complete())
+    RecordMenuItemAddToHomescreenHistogram(status_);
+  else
+    ++menu_item_add_to_homescreen_count_;
+}
+
+void InstallableMetrics::RecordAddToHomescreenNoTimeout() {
+  // For there to be no timeout, we must have successfully completed the
+  // installability check, or failed early. We could still be in the
+  // NOT_COMPLETED state since we don't do the full installability check if
+  // WebAPKs are disabled.
+  switch (status_) {
+    case InstallabilityCheckStatus::COMPLETE_PROGRESSIVE_WEB_APP:
+      RecordAddToHomescreenHistogram(
+          AddToHomescreenTimeoutStatus::NO_TIMEOUT_PROGRESSIVE_WEB_APP);
+      break;
+    case InstallabilityCheckStatus::COMPLETE_NON_PROGRESSIVE_WEB_APP:
+    case InstallabilityCheckStatus::NOT_COMPLETED:
+      RecordAddToHomescreenHistogram(
+          AddToHomescreenTimeoutStatus::NO_TIMEOUT_NON_PROGRESSIVE_WEB_APP);
+      break;
+    case InstallabilityCheckStatus::NOT_STARTED:
+    case InstallabilityCheckStatus::IN_PROGRESS_NON_PROGRESSIVE_WEB_APP:
+    case InstallabilityCheckStatus::IN_PROGRESS_PROGRESSIVE_WEB_APP:
+    case InstallabilityCheckStatus::COUNT:
+      NOTREACHED();
+  }
+}
+
+void InstallableMetrics::RecordAddToHomescreenManifestAndIconTimeout() {
+  ++add_to_homescreen_manifest_timeout_count_;
+}
+
+void InstallableMetrics::RecordAddToHomescreenInstallabilityTimeout() {
+  ++add_to_homescreen_installability_timeout_count_;
+}
+
+void InstallableMetrics::RecordQueuedMetrics(bool check_passed) {
+  // Don't do anything if we've already finished the PWA check.
+  if (is_pwa_check_complete())
+    return;
+
+  status_ = check_passed
+                ? InstallabilityCheckStatus::COMPLETE_PROGRESSIVE_WEB_APP
+                : InstallabilityCheckStatus::COMPLETE_NON_PROGRESSIVE_WEB_APP;
+
+  // Compute what the status would have been for any queued calls to
+  // Record{MenuOpen,MenuItemAddToHomescreen}Histogram.
+  InstallabilityCheckStatus prev_status =
+      check_passed
+          ? InstallabilityCheckStatus::IN_PROGRESS_PROGRESSIVE_WEB_APP
+          : InstallabilityCheckStatus::IN_PROGRESS_NON_PROGRESSIVE_WEB_APP;
+  for (; menu_open_count_ > 0; --menu_open_count_)
+    RecordMenuOpenHistogram(prev_status);
+
+  for (; menu_item_add_to_homescreen_count_ > 0;
+       --menu_item_add_to_homescreen_count_) {
+    RecordMenuItemAddToHomescreenHistogram(prev_status);
+  }
+
+  // Record the status for any queued calls to RecordAddToHomescreen*.
+  auto manifest_status = AddToHomescreenTimeoutStatus::
+      TIMEOUT_MANIFEST_FETCH_NON_PROGRESSIVE_WEB_APP;
+  auto installability_status = AddToHomescreenTimeoutStatus::
+      TIMEOUT_INSTALLABILITY_CHECK_NON_PROGRESSIVE_WEB_APP;
+
+  if (status_ == InstallabilityCheckStatus::COMPLETE_PROGRESSIVE_WEB_APP) {
+    manifest_status = AddToHomescreenTimeoutStatus::
+        TIMEOUT_MANIFEST_FETCH_PROGRESSIVE_WEB_APP;
+    installability_status = AddToHomescreenTimeoutStatus::
+        TIMEOUT_INSTALLABILITY_CHECK_PROGRESSIVE_WEB_APP;
+  }
+
+  for (; add_to_homescreen_manifest_timeout_count_ > 0;
+       --add_to_homescreen_manifest_timeout_count_) {
+    RecordAddToHomescreenHistogram(manifest_status);
+  }
+
+  for (; add_to_homescreen_installability_timeout_count_ > 0;
+       --add_to_homescreen_installability_timeout_count_) {
+    RecordAddToHomescreenHistogram(installability_status);
+  }
+}
+
+void InstallableMetrics::RecordMetricsOnNavigationAndReset() {
+  // We may have reset when the counts are nonzero and |status_| is one of
+  // NOT_STARTED or NOT_COMPLETED. If we completed, then these values cannot be
+  // anything except 0.
+  for (; menu_open_count_ > 0; --menu_open_count_)
+    RecordMenuOpenHistogram(status_);
+
+  for (; menu_item_add_to_homescreen_count_ > 0;
+       --menu_item_add_to_homescreen_count_) {
+    RecordMenuItemAddToHomescreenHistogram(status_);
+  }
+
+  for (; add_to_homescreen_manifest_timeout_count_ > 0;
+       --add_to_homescreen_manifest_timeout_count_) {
+    RecordAddToHomescreenHistogram(
+        AddToHomescreenTimeoutStatus::TIMEOUT_MANIFEST_FETCH_UNKNOWN);
+  }
+
+  for (; add_to_homescreen_installability_timeout_count_ > 0;
+       --add_to_homescreen_installability_timeout_count_) {
+    RecordAddToHomescreenHistogram(
+        AddToHomescreenTimeoutStatus::TIMEOUT_INSTALLABILITY_CHECK_UNKNOWN);
+  }
+
+  status_ = InstallabilityCheckStatus::NOT_STARTED;
+}
+
+bool InstallableMetrics::is_pwa_check_complete() const {
+  return status_ == InstallabilityCheckStatus::COMPLETE_PROGRESSIVE_WEB_APP ||
+         status_ == InstallabilityCheckStatus::COMPLETE_NON_PROGRESSIVE_WEB_APP;
 }
--- chrome/browser/installable/installable_metrics.h
+++ chrome/browser/installable/installable_metrics.h
@@ -16,11 +16,52 @@ enum class InstallabilityCheckStatus {
   COUNT,
 };
 
+// This enum backs a UMA histogram and must be treated as append-only.
+enum class AddToHomescreenTimeoutStatus {
+  NO_TIMEOUT_NON_PROGRESSIVE_WEB_APP,
+  NO_TIMEOUT_PROGRESSIVE_WEB_APP,
+  TIMEOUT_MANIFEST_FETCH_NON_PROGRESSIVE_WEB_APP,
+  TIMEOUT_MANIFEST_FETCH_PROGRESSIVE_WEB_APP,
+  TIMEOUT_MANIFEST_FETCH_UNKNOWN,
+  TIMEOUT_INSTALLABILITY_CHECK_NON_PROGRESSIVE_WEB_APP,
+  TIMEOUT_INSTALLABILITY_CHECK_PROGRESSIVE_WEB_APP,
+  TIMEOUT_INSTALLABILITY_CHECK_UNKNOWN,
+  COUNT,
+};
+
 class InstallableMetrics {
  public:
-  static void RecordMenuOpenHistogram(InstallabilityCheckStatus status);
-  static void RecordMenuItemAddToHomescreenHistogram(
-      InstallabilityCheckStatus status);
+  InstallableMetrics();
+  ~InstallableMetrics();
+
+  void RecordMenuOpen();
+  void RecordMenuItemAddToHomescreen();
+  void RecordAddToHomescreenNoTimeout();
+  void RecordAddToHomescreenManifestAndIconTimeout();
+  void RecordAddToHomescreenInstallabilityTimeout();
+  void RecordQueuedMetrics(bool check_passed);
+
+  void RecordMetricsOnNavigationAndReset();
+
+  void Start();
+  InstallabilityCheckStatus GetStatusForTesting() const;
+
+ private:
+  bool is_pwa_check_complete() const;
+
+  // Whether or not the current page is a PWA.
+  InstallabilityCheckStatus status_;
+
+  // Counts for the number of queued requests of the menu and add to homescreen
+  // menu item there have been whilst the installable check is awaiting
+  // completion. Used for metrics recording.
+  int menu_open_count_;
+  int menu_item_add_to_homescreen_count_;
+
+  // Counts for the number of times the add to homescreen dialog times out at
+  // different stages of the installable check.
+  int add_to_homescreen_manifest_timeout_count_;
+  int add_to_homescreen_installability_timeout_count_;
 };
 
 #endif  // CHROME_BROWSER_INSTALLABLE_INSTALLABLE_METRICS_H_
