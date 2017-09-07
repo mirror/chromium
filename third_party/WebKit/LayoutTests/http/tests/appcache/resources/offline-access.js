86455226ffc934e09264d4e41343abdebdbe8551
Michael Nordman <michaeln@google.com>
2017-09-07 14:53:13 -0700

    [AppCache] Delete flaky VerifyRedirectedSubresourceAppCacheLoad browser test.
    Instead, slightly modify an existing layout test (offline-access.html) to provide test coverage.
    
    Bug: 762381
    Change-Id: I1ed7636dd4223fdffa0555d3b64323b353437139

--- third_party/WebKit/LayoutTests/http/tests/appcache/resources/offline-access.js
+++ third_party/WebKit/LayoutTests/http/tests/appcache/resources/offline-access.js
@@ -4,14 +4,23 @@ function test()
     applicationCache.onnoupdate = function() { log("noupdate") }
     applicationCache.onerror = function() { log("error") }
 
-    try {
+  try {
+        var cachedResourceUrl = "/resources/network-simulator.php?path=/appcache/resources/simple.txt";
+        var redirectToCachedResourceUrl = "/resources/redirect.php?url=" + cachedResourceUrl;
+
         var req = new XMLHttpRequest;
-        req.open("GET", "/resources/network-simulator.php?path=/appcache/resources/simple.txt", false);
+        req.open("GET", cachedResourceUrl, false);
         req.send(null);
-        if (req.responseText == "Hello, World!")
-            parent.postMessage("done", "*");
-        else
-            alert("FAIL, unexpected response: " + ex);
+        if (req.responseText != "Hello, World!")
+            throw "wrong response text for cachedResourceUrl";
+
+        req = new XMLHttpRequest;
+        req.open("GET", redirectToCachedResourceUrl, false);
+        req.send(null);
+        if (req.responseText != "Hello, World!")
+            throw "wrong response text for redirectToCachedResourceUrl";
+
+        parent.postMessage("done", "*");
     } catch (ex) {
         alert("FAIL, unexpected error: " + ex);
     }
