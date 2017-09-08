d90edb47be236a1a45f3cbbcc661c1e852b8e3aa
Zijie He <zijiehe@chromium.org>
2017-09-08 16:58:58 -0700

    Implement Process::IsRunning
    This change implements bool Process::IsRunning() const function to check whether
    a process is still running.
    It uses WaitForExitWithTimeout() to test if the process has been exited already.
    But there are some small tweaks needed in WaitForExitWithTimeout()
    implementations on different platforms to ensure the cross-platform consistency.
    
    This function can help to track the lifetime of a process, it's helpful when
    measuring the metrics (ProcessMetrics) of several processes: a stopped process
    can be removed from the set without needing extra synchronizating menchanism.
    
    Bug: chromium:650926
    Change-Id: I48802949efcca0796e948442a144e85041027918

--- base/process/process_unittest.cc
+++ base/process/process_unittest.cc
@@ -287,6 +287,25 @@ TEST_F(ProcessTest, SetProcessBackgroundedSelf) {
   EXPECT_EQ(old_priority, new_priority);
 }
 
+TEST_F(ProcessTest, CurrentProcessIsRunning) {
+  EXPECT_TRUE(Process::Current().IsRunning());
+}
+
+#if defined(OS_MACOSX)
+// On Mac OSX, we can detect whether a non-child process is running.
+TEST_F(ProcessTest, PredefinedProcessIsRunning) {
+  // Process 1 is the /sbin/launchd, it should be always running.
+  EXPECT_TRUE(Process::Open(1).IsRunning());
+}
+#endif
+
+TEST_F(ProcessTest, ChildProcessIsRunning) {
+  Process process(SpawnChild("SleepyChildProcess"));
+  EXPECT_TRUE(process.IsRunning());
+  process.Terminate(0, true);
+  EXPECT_FALSE(process.IsRunning());
+}
+
 #if defined(OS_CHROMEOS)
 
 // Tests that the function IsProcessBackgroundedCGroup() can parse the contents
