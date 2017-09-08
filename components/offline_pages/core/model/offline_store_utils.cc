48ad9fcbacda87cd3113cf7328e15bf1113203ff
Yafei Duan <romax@chromium.org>
2017-09-08 15:37:32 -0700

    [Offline Pages] Implementing create archive task.
    Implementing CreateArchiveTask, which is a task for starting the archive
    creation but will not wait for the completion of the process, in order
    to support multiple archivers working in parallel. The creator of the
    task has to provide a CreateArchiveTaskCallback, which will be
    processing the arguments and decide whether the creation is successful,
    so that it can create AddPageTask for next steps.
    Also duplicating most of the tests related with archive creation from
    the OfflinePageModelImpl unit tests.
    
    Bug: 753595
    Change-Id: I915daab56a260ccaa9c028c0ea95c2ff0646d461

---
+++ components/offline_pages/core/model/offline_store_utils.cc
@@ -0,0 +1,18 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+#include "components/offline_pages/core/model/offline_store_utils.h"
+
+#include <limits>
+
+#include "base/rand_util.h"
+
+namespace offline_pages {
+
+// static
+int64_t OfflineStoreUtils::GenerateOfflineId() {
+  return base::RandGenerator(std::numeric_limits<int64_t>::max()) + 1;
+}
+
+}  // namespace offline_pages
