2daa4a1d9213247e5982ff3338ad0127bb8128ab
Conley Owens <cco3@chromium.org>
2017-09-08 15:37:08 -0700

    bluetooth: Add AllResponsesConsumed to test API
    This change adds an AllResponsesConsumed method to the test interface.
    This ensures that the calling code consumes all responses set with the
    following methods:
    - SetNextGATTConnectionResponse
    - SetNextGATTDiscoveryResponse
    - SetNextReadCharacteristicResponse
    - SetNextWriteCharacteristicResponse
    - SetNextSubscribeToNotificationsResponse
    - SetNextUnsubscribeFromNotificationsResponse
    - SetNextReadDescriptorResponse
    - SetNextWriteDescriptorResponse
    
    BUG=569709
    
    Change-Id: Ic19c2e26ce3aaaf8fe928d3819114037a1641277

--- device/bluetooth/test/fake_central.cc
+++ device/bluetooth/test/fake_central.cc
@@ -72,6 +72,14 @@ void FakeCentral::SetNextGATTDiscoveryResponse(
   std::move(callback).Run(true);
 }
 
+bool FakeCentral::AllResponsesConsumed() {
+  return std::all_of(devices_.begin(), devices_.end(), [](const auto& e) {
+    FakePeripheral* fake_peripheral =
+        static_cast<FakePeripheral*>(e.second.get());
+    return fake_peripheral->AllResponsesConsumed();
+  });
+}
+
 void FakeCentral::SimulateGATTDisconnection(
     const std::string& address,
     SimulateGATTDisconnectionCallback callback) {
