61ad84e4324fec14cb68fbf0d735c01dfeaf06a7
Prashant Malani <pmalani@google.com>
2017-09-07 22:18:40 -0700

    MIDI ArcBridge proxy mojo interface.
    ARC MIDI uses this to connect to the midis daemon running in
    Chrome OS. It takes a Mojo connection request over the ArcBridge,
    and then uses D-Bus (for sending across a message handle) to initiate
    a Mojo connection with that daemon. The daemon then returns a 2nd Mojo
    interface (with the actual Midis client calls) back to the proxy,
    which in turn sends it to the Android side. At present, the Connect()
    call which should return the 2nd Mojo interface handle has not yet
    been implemented.
    
    It retains the initial Mojo connection with the daemon is maintained,
    for future ARC sessions, and tears down when the daemon dies.
    
    This implementation is a based on the similar implementation used by
    ArcOemCrypto.
    
    BUG=chromium:701005
    
    Change-Id: Icc8c937f743ddcc3deca85446c303408b17ec759

--- chrome/browser/chromeos/arc/arc_service_launcher.cc
+++ chrome/browser/chromeos/arc/arc_service_launcher.cc
@@ -48,6 +48,7 @@
 #include "components/arc/ime/arc_ime_service.h"
 #include "components/arc/intent_helper/arc_intent_helper_bridge.h"
 #include "components/arc/metrics/arc_metrics_service.h"
+#include "components/arc/midis/arc_midis_bridge.h"
 #include "components/arc/net/arc_net_host_impl.h"
 #include "components/arc/obb_mounter/arc_obb_mounter_bridge.h"
 #include "components/arc/power/arc_power_bridge.h"
@@ -144,6 +145,7 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   ArcIntentHelperBridge::GetForBrowserContext(profile);
   ArcKioskBridge::GetForBrowserContext(profile);
   ArcMetricsService::GetForBrowserContext(profile);
+  ArcMidisBridge::GetForBrowserContext(profile);
   ArcNetHostImpl::GetForBrowserContext(profile);
   ArcNotificationManager::GetForBrowserContext(profile);
   ArcObbMounterBridge::GetForBrowserContext(profile);
