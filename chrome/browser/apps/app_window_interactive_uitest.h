6a2d2d905d50fc07dd0b8a4489e22cd620c4b0d4
Ivan Sandrk <isandrk@google.com>
2017-09-08 20:08:22 +0200

    Display a toast when entering immersive fullscreen in public session
    More details can be found in the design doc: https://docs.google.com/document/d/1UZtS12mz5lP1KZRvQiWdGLphGazTEqBCY9EdC9vciFo
    
    Bug:chromium:741069
    Change-Id: Icdbf2ac7cbb9d2fb9cb823b555c9e267a0aac67c

---
+++ chrome/browser/apps/app_window_interactive_uitest.h
@@ -0,0 +1,45 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_APPS_APP_WINDOW_INTERACTIVE_UITEST_H_
+#define CHROME_BROWSER_APPS_APP_WINDOW_INTERACTIVE_UITEST_H_
+
+#include <string>
+
+#include "base/macros.h"
+#include "chrome/browser/apps/app_browsertest_util.h"
+#include "extensions/browser/app_window/native_app_window.h"
+#include "ui/events/keycodes/keyboard_codes.h"
+
+using extensions::NativeAppWindow;
+
+// Helper class that has to be created on the stack to check if the fullscreen
+// setting of a NativeWindow has changed since the creation of the object.
+class FullscreenChangeWaiter {
+ public:
+  explicit FullscreenChangeWaiter(NativeAppWindow* window);
+
+  void Wait();
+
+ private:
+  NativeAppWindow* window_;
+  bool initial_fullscreen_state_;
+
+  DISALLOW_COPY_AND_ASSIGN(FullscreenChangeWaiter);
+};
+
+class AppWindowInteractiveTest : public extensions::PlatformAppBrowserTest {
+ public:
+  bool RunAppWindowInteractiveTest(const char* testName);
+
+  bool SimulateKeyPress(ui::KeyboardCode key);
+
+  // This method will wait until the application is able to ack a key event.
+  void WaitUntilKeyFocus();
+
+  // This test is a method so that we can test with each frame type.
+  void TestOuterBoundsHelper(const std::string& frame_type);
+};
+
+#endif  // CHROME_BROWSER_APPS_APP_WINDOW_INTERACTIVE_UITEST_H_
