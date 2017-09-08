82846fea588de5b65710048384393458c2c7bcaf
erikchen <erikchen@chromium.org>
2017-09-08 16:05:55 -0700

    Support heap profiling for GPU process.
    This CL renames the memlog mode kBrowser to be kMinimal, and kMinimal now
    profiles both the browser and GPU processes. In kMinimal or kAll memlog mode,
    the GPU process will upload a report if private memory footprint exceeds 1 GB.
    
    Bug:
    Change-Id: Iae1b6cb101ecafcdc7724c0ffda4c35fd9704a56

--- chrome/browser/profiling_host/profiling_process_host.cc
+++ chrome/browser/profiling_host/profiling_process_host.cc
@@ -122,9 +122,11 @@ void ProfilingProcessHost::Unregister() {
 
 void ProfilingProcessHost::BrowserChildProcessLaunchedAndConnected(
     const content::ChildProcessData& data) {
-  // Ignore newly launched child process if only profiling the browser.
-  if (mode_ == Mode::kBrowser)
+  // In minimal mode, only profile the GPU process.
+  if (mode_ == Mode::kMinimal &&
+      data.process_type != content::ProcessType::PROCESS_TYPE_GPU) {
     return;
+  }
 
   if (!content::BrowserThread::CurrentlyOn(content::BrowserThread::IO)) {
     content::BrowserThread::GetTaskRunnerForThread(content::BrowserThread::IO)
@@ -146,6 +148,7 @@ void ProfilingProcessHost::BrowserChildProcessLaunchedAndConnected(
       mojo::MakeRequest(&memlog_client);
   BindInterface(host->GetHost(), std::move(request));
   base::ProcessId pid = base::GetProcId(data.handle);
+  LOG(ERROR) << "start profiling: " << pid;
   SendPipeToProfilingService(std::move(memlog_client), pid);
 }
 
@@ -153,8 +156,9 @@ void ProfilingProcessHost::Observe(
     int type,
     const content::NotificationSource& source,
     const content::NotificationDetails& details) {
-  // Ignore newly launched renderer if only profiling the browser.
-  if (mode_ == Mode::kBrowser)
+  // Ignore newly launched renderer if only profiling a minimal set of
+  // processes.
+  if (mode_ == Mode::kMinimal)
     return;
 
   if (type != content::NOTIFICATION_RENDERER_PROCESS_CREATED)
@@ -183,15 +187,6 @@ void ProfilingProcessHost::Observe(
 bool ProfilingProcessHost::OnMemoryDump(
     const base::trace_event::MemoryDumpArgs& args,
     base::trace_event::ProcessMemoryDump* pmd) {
-  // Dump the browser process, which happens to be this process.
-  if (GetCurrentMode() == Mode::kBrowser) {
-    memlog_->DumpProcessForTracing(
-        base::Process::Current().Pid(),
-        base::BindOnce(&ProfilingProcessHost::OnDumpProcessForTracingCallback,
-                       base::Unretained(this)));
-    return true;
-  }
-
   // Attempt to dump all processes. Some of these processes will not be profiled
   // [e.g. utility processes, including the profiling process]. The profiling
   // process will gracefully handle these failures.
@@ -274,8 +269,8 @@ ProfilingProcessHost::Mode ProfilingProcessHost::GetCurrentMode() {
             switches::kMemlog);
     if (mode == switches::kMemlogModeAll)
       return Mode::kAll;
-    if (mode == switches::kMemlogModeBrowser)
-      return Mode::kBrowser;
+    if (mode == switches::kMemlogModeMinimal)
+      return Mode::kMinimal;
 
     DLOG(ERROR) << "Unsupported value: \"" << mode << "\" passed to --"
                 << switches::kMemlog;
