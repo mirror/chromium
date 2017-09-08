e193ef1c9833054df893d28325c7a150e73c7dd6
Dominik Laskowski <domlaskowski@chromium.org>
2017-09-08 13:00:49 -0700

    arc: Implement multi-display app launching
    This CL extends app launching to include the display ID of the
    shelf/launcher/view used to launch an app/intent, which allows
    ARC to launch apps on a specific display.
    
    Bug: b:63535964
    Test: Launch apps using different shelves/launchers.
    Test: Launch Android settings on different displays.
    Test: Open app info pages using different launchers.
    Test: Add Hangouts conversation to home screen, and launch the intent
          using different shelves/launchers.
    Change-Id: I637b50092b4c107ef24d93af4b0bce6cd7916b54

--- chrome/browser/ui/app_list/app_list_controller_delegate.h
+++ chrome/browser/ui/app_list/app_list_controller_delegate.h
@@ -5,6 +5,7 @@
 #ifndef CHROME_BROWSER_UI_APP_LIST_APP_LIST_CONTROLLER_DELEGATE_H_
 #define CHROME_BROWSER_UI_APP_LIST_APP_LIST_CONTROLLER_DELEGATE_H_
 
+#include <cstdint>
 #include <string>
 
 #include "chrome/common/extensions/extension_constants.h"
@@ -55,6 +56,9 @@ class AppListControllerDelegate {
 
   // Gets app list window.
   virtual gfx::NativeWindow GetAppListWindow() = 0;
+
+  // Gets display ID of app list window.
+  int64_t GetAppListDisplayId();
 
   // Gets the content bounds of the app info dialog of the app list in the
   // screen coordinates. On platforms that do not use views, this returns a 0x0
