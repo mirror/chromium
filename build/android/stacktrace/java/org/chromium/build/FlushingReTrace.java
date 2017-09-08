19c370d7192fa7aa0227b694a9c0c8d6e5264ddf
Andrew Grieve <agrieve@chromium.org>
2017-09-08 13:54:49 -0400

    Android: Update java_deobfuscate to match more lines
    Also adds a test, and a --logcat flag for better matching of logcat
    lines.
    
    Bug: 713710
    Change-Id: Ie21030b3c4e68d02cfb2e0d65d2c4c4f00624a80

--- build/android/stacktrace/java/org/chromium/build/FlushingReTrace.java
+++ build/android/stacktrace/java/org/chromium/build/FlushingReTrace.java
@@ -20,25 +20,68 @@ import proguard.retrace.ReTrace;
  *  2. Disables output buffering
  */
 public class FlushingReTrace {
-    // This regex is based on the one from:
-    // http://proguard.sourceforge.net/manual/retrace/usage.html.
-    // But with the "at" part changed to "(?::|\bat)", to account for lines like:
-    //     06-22 13:58:02.895  4674  4674 E THREAD_STATE:     bLA.a(PG:173)
-    // And .*=%c\s* added as the second subpattern to account for lines like:
-    //     INSTRUMENTATION_STATUS: class=bNs
-    // Normal stack trace lines look like:
-    // java.lang.RuntimeException: Intentional Java Crash
-    //     at org.chromium.chrome.browser.tab.Tab.handleJavaCrash(Tab.java:682)
-    //     at org.chromium.chrome.browser.tab.Tab.loadUrl(Tab.java:644)
-    private static final String LINE_PARSE_REGEX =
-            "(?:.*?(?::|\\bat)\\s+%c\\.%m\\s*\\(%s(?::%l)?\\)\\s*)|"
-            + "(?:.*=%c\\s*)|"
-            + "(?:(?:.*?[:\"]\\s+)?%c(?::.*)?)";
+    // Prefixed to LINE_PARSE_REGEX when --logcat is passed.
+    private static final String LOGCAT_PREFIX = ".*?: ";
+
+    // Note: Order of these sub-patterns defines their precedence.
+    // Note: Deobfuscation of methods without the presense of line numbers basically never works.
+    // There is a test for these pattern at //build/android/stacktrace/java_deobfuscate_test.py
+    private static final String LINE_PARSE_REGEX = "(?:"
+            // Based on default ReTrace regex, but with "at" changed to to allow :
+            // E.g.: 06-22 13:58:02.895  4674  4674 E THREAD_STATE:     bLA.a(PG:173)
+            // Normal stack trace lines look like:
+            // \tat org.chromium.chrome.browser.tab.Tab.handleJavaCrash(Tab.java:682)
+            + "(?:.*?(?::|\\bat)\\s+%c\\.%m\\s*\\(%s(?::%l)?\\))|"
+            // E.g.: VFY: unable to resolve new-instance 3810 (LSome/Framework/Class;) in Lfoo/Bar;
+            + "(?:.*L%C;.*)|"
+            // E.g.: END SomeTestClass#someMethod
+            + "(?:.*?%c#%m.*?)|"
+            // E.g.: The member "Foo.bar"
+            // E.g.: The class "Foobar"
+            + "(?:.*?\"%c\\.%m\".*)|"
+            + "(?:.*?\"%c\".*)|"
+            // E.g.: java.lang.RuntimeException: Intentional Java Crash
+            + "(?:%c:.*)|"
+            // All lines that end with a class / class+method:
+            // E.g.: The class: Foo
+            // E.g.: INSTRUMENTATION_STATUS: class=Foo
+            // E.g.: NoClassDefFoundError: SomeFrameworkClass in isTestClass for Foo
+            // E.g.: Could not find class 'SomeFrameworkClass', referenced from method Foo.bar
+            // E.g.: Could not find method SomeFrameworkMethod, referenced from method Foo.bar
+            + "(?:.*(?:=|:\\s*|\\s+)%c\\.%m)|"
+            + "(?:.*(?:=|:\\s*|\\s+)%c)"
+            + ")";
+
+    private static void usage() {
+        System.err.println("Usage: echo $OBFUSCATED_CLASS | java_deobfuscate Foo.apk.mapping");
+        System.err.println("Usage: java_deobfuscate --logcat Foo.apk.mapping < foo.log");
+        System.err.println("Note: Deobfuscation of symbols outside the context of stack "
+                + "traces will work only when lines match the regular expression defined "
+                + "in FlushingReTrace.java.");
+        System.err.println("Also: Deobfuscation of method names without associated line "
+                + "numbers does not seem to work.");
+        System.exit(1);
+    }
 
     public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Usage: java_deobfuscate Foo.apk.map < foo.log > bar.log");
-            System.exit(1);
+        if (args.length == 0 || args.length > 2) {
+            usage();
+        }
+        String mappingPath = null;
+        boolean logcat = false;
+        for (String arg : args) {
+            if ("--help".equals(arg)) {
+                usage();
+            } else if ("--logcat".equals(arg)) {
+                logcat = true;
+            } else if (arg.startsWith("-") || mappingPath != null) {
+                usage();
+            } else {
+                mappingPath = arg;
+            }
+        }
+        if (mappingPath == null) {
+            usage();
         }
 
         File mappingFile = new File(args[0]);
@@ -52,7 +95,11 @@ public class FlushingReTrace {
                     new PrintWriter(new OutputStreamWriter(System.out, "UTF-8"), autoFlush);
 
             boolean verbose = false;
-            new ReTrace(LINE_PARSE_REGEX, verbose, mappingFile).retrace(reader, writer);
+            String lineParseRegex = LINE_PARSE_REGEX;
+            if (logcat) {
+                lineParseRegex = LOGCAT_PREFIX + LINE_PARSE_REGEX;
+            }
+            new ReTrace(lineParseRegex, verbose, mappingFile).retrace(reader, writer);
         } catch (IOException ex) {
             // Print a verbose stack trace.
             ex.printStackTrace();
---
+++ build/android/stacktrace/java_deobfuscate_test.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+#
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Tests for java_deobfuscate."""
+
+import argparse
+import subprocess
+import sys
+import tempfile
+
+
+TEST_MAP = """\
+this.was.Deobfuscated -> FOO:
+    int[] FontFamily -> a
+    1:3:void someMethod(int,android.os.Bundle):65:65 -> bar
+"""
+
+TEST_DATA = """\
+Here is a FOO
+Here is a FOO baz
+Here is a "FOO" baz
+Here is a "FOO.bar" baz
+Here it is: FOO
+Here it is: FOO.bar
+Here is a FOO.bar
+Here is a FOO.bar baz
+END FOO#bar
+new-instance 3810 (LSome/Framework/Class;) in LFOO;
+FOO: Error message
+\tat FOO.bar(PG:1)
+""".splitlines(True)
+
+EXPECTED_OUTPUT = """\
+Here is a this.was.Deobfuscated
+Here is a FOO baz
+Here is a "this.was.Deobfuscated" baz
+Here is a "this.was.Deobfuscated.someMethod" baz
+Here it is: this.was.Deobfuscated
+Here it is: this.was.Deobfuscated.someMethod
+Here is a this.was.Deobfuscated.someMethod
+Here is a FOO.bar baz
+END this.was.Deobfuscated#someMethod
+new-instance 3810 (LSome/Framework/Class;) in Lthis/was/Deobfuscated;
+this.was.Deobfuscated: Error message
+\tat this.was.Deobfuscated.someMethod(Deobfuscated.java:65)
+""".splitlines(True)
+
+
+def _RunTest(bin_path, map_file, logcat):
+  cmd = [bin_path, map_file]
+  payload = TEST_DATA
+  expected_output = EXPECTED_OUTPUT
+  if logcat:
+    cmd.append('--logcat')
+    payload = ['1:2:3 LogTag: ' + x for x in payload]
+    expected_output = ['1:2:3 LogTag: ' + x for x in expected_output]
+
+  proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+  actual_output = proc.communicate(''.join(payload))[0].splitlines(True)
+  any_unexpected_failures = False
+  for actual, expected in zip(actual_output, expected_output):
+    if actual == expected:
+      sys.stdout.write('Good: ' + actual)
+    elif actual.replace('bar', 'someMethod') == expected:
+      # TODO(agrieve): Fix ReTrace's ability to deobfuscated methods.
+      sys.stdout.write('Bug:  ' + actual)
+    else:
+      sys.stdout.write('Bad:  ' + actual)
+      any_unexpected_failures = True
+  return not any_unexpected_failures
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('path_to_java_deobfuscate')
+  args = parser.parse_args()
+
+  with tempfile.NamedTemporaryFile() as map_file:
+    map_file.write(TEST_MAP)
+    map_file.flush()
+
+    result1 = _RunTest(args.path_to_java_deobfuscate, map_file.name, False)
+    result2 = _RunTest(args.path_to_java_deobfuscate, map_file.name, True)
+  passed = result1 and result2
+  print 'Result:', 'PASS' if passed else 'FAIL'
+  sys.exit(int(not passed))
+
+
+if __name__ == '__main__':
+  main()
