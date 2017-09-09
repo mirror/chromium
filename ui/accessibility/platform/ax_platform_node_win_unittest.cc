2500c24f39412f96923ddc445fbf909a0c6ad4d0
Doug Turner <dougt@chromium.org>
2017-09-08 17:45:24 -0700

    Migrate get_caretOffset from BrowserAccessiblity to AXPlatformNode
    Bug: 703369
    Change-Id: If672c2f92fa5f74db6e89af266827e706d43c40e

--- ui/accessibility/platform/ax_platform_node_win_unittest.cc
+++ ui/accessibility/platform/ax_platform_node_win_unittest.cc
@@ -2207,4 +2207,46 @@ TEST_F(AXPlatformNodeWinTest, TestIAccessibleTextGetNCharacters) {
   EXPECT_EQ(4, count);
 }
 
+TEST_F(AXPlatformNodeWinTest, TestIAccessibleTextGetCaretOffset) {
+  AXNodeData text_field_node;
+  text_field_node.id = 1;
+  text_field_node.role = AX_ROLE_TEXT_FIELD;
+  text_field_node.state = 1 << AX_STATE_EDITABLE;
+  text_field_node.state |= 1 << AX_STATE_SELECTED;
+  text_field_node.SetValue("Hi");
+
+  Init(text_field_node);
+  ScopedComPtr<IAccessible2> ia2_text_field =
+      ToIAccessible2(GetRootIAccessible());
+  ScopedComPtr<IAccessibleText> text_field;
+  ia2_text_field.CopyTo(text_field.GetAddressOf());
+  ASSERT_NE(nullptr, text_field.Get());
+
+  LONG offset;
+  EXPECT_HRESULT_SUCCEEDED(text_field->get_caretOffset(&offset));
+  ASSERT_EQ(0, offset);
+}
+
+TEST_F(AXPlatformNodeWinTest, TestIAccessibleTextGetCaretOffsetSelected) {
+  AXNodeData text_field_node;
+  text_field_node.id = 1;
+  text_field_node.role = AX_ROLE_TEXT_FIELD;
+  text_field_node.state = 1 << AX_STATE_EDITABLE;
+  text_field_node.state |= 1 << AX_STATE_SELECTED;
+  text_field_node.AddIntAttribute(ui::AX_ATTR_TEXT_SEL_START, 1);
+  text_field_node.AddIntAttribute(ui::AX_ATTR_TEXT_SEL_END, 2);
+  text_field_node.SetValue("Hi");
+
+  Init(text_field_node);
+  ScopedComPtr<IAccessible2> ia2_text_field =
+      ToIAccessible2(GetRootIAccessible());
+  ScopedComPtr<IAccessibleText> text_field;
+  ia2_text_field.CopyTo(text_field.GetAddressOf());
+  ASSERT_NE(nullptr, text_field.Get());
+
+  LONG offset;
+  EXPECT_HRESULT_SUCCEEDED(text_field->get_caretOffset(&offset));
+  ASSERT_EQ(2, offset);
+}
+
 }  // namespace ui
