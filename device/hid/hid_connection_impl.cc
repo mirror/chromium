daf96034ca81e3cd95ac23e995c13a3374a02690
Ke He <ke.he@intel.com>
2017-09-09 08:37:40 +0800

    Mojofication on //device/hid. Convert //extensions to use hid mojo interface.
    In this CL:
    1) Add the HidManager and HidConnection interfaces in hid.mojom, and
       implements those mojo interfaces in //device/hid.
    
    2) Convert the clients in //extensions to use hid mojo interfaces.
    
    3) Change the type of HidUsageAndPage::usage_page to uint16_t.
    
    4) Rewrite the hid_apitest base on the new added mojo interfaces.
    
    TODO:
    1) Move client library files into device/hid/public/cpp.
    2) Host HidService by DeviceService instead of DeviceClient.
    3) Mojofy //device/u2f Or just move it into DeviceService.
    
    BUG=728223
    
    Change-Id: I1de1b5211f7ef37f54d4bc18c7995a3b47cc7da2

---
+++ device/hid/hid_connection_impl.cc
@@ -0,0 +1,107 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "device/hid/hid_connection_impl.h"
+
+#include "base/bind.h"
+#include "base/memory/ptr_util.h"
+
+namespace device {
+
+HidConnectionImpl::HidConnectionImpl(
+    scoped_refptr<device::HidConnection> connection)
+    : hid_connection_(std::move(connection)), weak_factory_(this) {}
+
+HidConnectionImpl::~HidConnectionImpl() {
+  DCHECK(hid_connection_);
+
+  // Close |hid_connection_| on destruction because this class is owned by a
+  // mojo::StrongBinding and will be destroyed when the pipe is closed.
+  hid_connection_->Close();
+}
+
+void HidConnectionImpl::Read(ReadCallback callback) {
+  DCHECK(hid_connection_);
+  hid_connection_->Read(base::BindOnce(&HidConnectionImpl::OnRead,
+                                       weak_factory_.GetWeakPtr(),
+                                       std::move(callback)));
+}
+
+void HidConnectionImpl::OnRead(ReadCallback callback,
+                               bool success,
+                               scoped_refptr<net::IOBuffer> buffer,
+                               size_t size) {
+  if (!success) {
+    std::move(callback).Run(false, base::nullopt);
+    return;
+  }
+  DCHECK(buffer);
+
+  std::vector<uint8_t> data(buffer->data(), buffer->data() + size);
+  std::move(callback).Run(true, data);
+}
+
+void HidConnectionImpl::Write(const std::vector<uint8_t>& buffer,
+                              WriteCallback callback) {
+  DCHECK(hid_connection_);
+
+  auto io_buffer = base::MakeRefCounted<net::IOBufferWithSize>(buffer.size());
+
+  const char* data = reinterpret_cast<const char*>(buffer.data());
+  memcpy(io_buffer->data(), data, buffer.size());
+
+  hid_connection_->Write(
+      io_buffer, io_buffer->size(),
+      base::BindOnce(&HidConnectionImpl::OnWrite, weak_factory_.GetWeakPtr(),
+                     std::move(callback)));
+}
+
+void HidConnectionImpl::OnWrite(WriteCallback callback, bool success) {
+  std::move(callback).Run(success);
+}
+
+void HidConnectionImpl::GetFeatureReport(uint8_t report_id,
+                                         GetFeatureReportCallback callback) {
+  DCHECK(hid_connection_);
+  hid_connection_->GetFeatureReport(
+      report_id,
+      base::BindOnce(&HidConnectionImpl::OnGetFeatureReport,
+                     weak_factory_.GetWeakPtr(), std::move(callback)));
+}
+
+void HidConnectionImpl::OnGetFeatureReport(GetFeatureReportCallback callback,
+                                           bool success,
+                                           scoped_refptr<net::IOBuffer> buffer,
+                                           size_t size) {
+  if (!success) {
+    std::move(callback).Run(false, base::nullopt);
+    return;
+  }
+  DCHECK(buffer);
+
+  std::vector<uint8_t> data(buffer->data(), buffer->data() + size);
+  std::move(callback).Run(true, data);
+}
+
+void HidConnectionImpl::SendFeatureReport(const std::vector<uint8_t>& buffer,
+                                          SendFeatureReportCallback callback) {
+  DCHECK(hid_connection_);
+
+  auto io_buffer = base::MakeRefCounted<net::IOBufferWithSize>(buffer.size());
+
+  const char* data = reinterpret_cast<const char*>(buffer.data());
+  memcpy(io_buffer->data(), data, buffer.size());
+
+  hid_connection_->SendFeatureReport(
+      io_buffer, io_buffer->size(),
+      base::BindOnce(&HidConnectionImpl::OnSendFeatureReport,
+                     weak_factory_.GetWeakPtr(), std::move(callback)));
+}
+
+void HidConnectionImpl::OnSendFeatureReport(SendFeatureReportCallback callback,
+                                            bool success) {
+  std::move(callback).Run(success);
+}
+
+}  // namespace device
