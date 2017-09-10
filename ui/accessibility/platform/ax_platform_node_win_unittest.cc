bec6a48a373d7f1ef40f3c7e9a5367604ce1cc1e
Doug Turner <dougt@chromium.org>
2017-09-10 00:30:36 -0700

    Migrate addSelection from BrowserAccessiblity to AXPlatformNode
    Bug: 703369
    Change-Id: I749030e605eb30d3ee0785885d20c510e5f91f69

--- ui/accessibility/platform/ax_platform_node_win_unittest.cc
+++ ui/accessibility/platform/ax_platform_node_win_unittest.cc
@@ -2181,4 +2181,33 @@ TEST_F(AXPlatformNodeWinTest, TestIAccessible2GetLocalizedExtendedRole) {
   EXPECT_STREQ(L"extended role", role);
 }
 
+TEST_F(AXPlatformNodeWinTest, TestIAccessibleTextSetSelected) {
+  AXNodeData text_field_node;
+  text_field_node.id = 1;
+  text_field_node.role = AX_ROLE_TEXT_FIELD;
+  text_field_node.state = 1 << AX_STATE_EDITABLE;
+  text_field_node.state |= 1 << AX_STATE_SELECTED;
+  text_field_node.SetValue("Hi");
+
+  Init(text_field_node);
+  ScopedComPtr<IAccessible2> ia2_text_field =
+      ToIAccessible2(GetRootIAccessible());
+  ScopedComPtr<IAccessibleText> text_field;
+  ia2_text_field.CopyTo(text_field.GetAddressOf());
+  ASSERT_NE(nullptr, text_field.Get());
+
+  LONG start_offset, end_offset;
+  EXPECT_HRESULT_SUCCEEDED(
+      text_field->get_selection(0, &start_offset, &end_offset));
+  ASSERT_EQ(0, start_offset);
+  ASSERT_EQ(0, end_offset);
+
+  EXPECT_HRESULT_SUCCEEDED(text_field->addSelection(1, 2));
+
+  EXPECT_HRESULT_SUCCEEDED(
+      text_field->get_selection(0, &start_offset, &end_offset));
+  ASSERT_EQ(1, start_offset);
+  ASSERT_EQ(2, end_offset);
+}
+
 }  // namespace ui
