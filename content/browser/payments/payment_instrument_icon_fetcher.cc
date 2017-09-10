1f95930eab4fdb62c569eb867b648a45ba1bbd56
Jinho Bang <jinho.bang@samsung.com>
2017-09-11 04:22:44 +0900

    PaymentHandler: Refactor PaymentInstrumentIconFetcher class.
    The current PaymentInstrumentIconFetcher class is too complicated and can not
    ensure thread-safety due to misuse some APIs. So, this patch refactors the class
    to use ManifestIconDownloader internally. After this patch, we can remove much
    duplicated codes and resolve some crashes in debug mode as well.
    
    Bug: 752836
    Change-Id: I04c01d433d093ae138148e6aa2849bc1713c1c85

--- content/browser/payments/payment_instrument_icon_fetcher.cc
+++ content/browser/payments/payment_instrument_icon_fetcher.cc
@@ -6,160 +6,113 @@
 
 #include "base/base64.h"
 #include "base/bind_helpers.h"
+#include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/browser/storage_partition_impl.h"
+#include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/browser_thread.h"
-#include "content/public/common/service_manager_connection.h"
-#include "net/base/load_flags.h"
-#include "net/http/http_status_code.h"
-#include "net/traffic_annotation/network_traffic_annotation.h"
-#include "services/data_decoder/public/cpp/decode_image.h"
-#include "services/service_manager/public/cpp/connector.h"
+#include "content/public/browser/manifest_icon_downloader.h"
+#include "content/public/browser/manifest_icon_selector.h"
+#include "content/public/common/manifest.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/image/image.h"
 
 namespace content {
-
 namespace {
 
-net::NetworkTrafficAnnotationTag g_traffic_annotation =
-    net::DefineNetworkTrafficAnnotation("payment_instrument_icon_fetcher", R"(
-        semantics {
-          sender: "Web Payments"
-          description:
-            "Chromium downloads payment instrument icons when registering "
-            "payment instruments."
-          trigger:
-            "When user navigates to a website to register web payment apps."
-          data:
-            "URL of the required icon to fetch. No user information is sent."
-          destination: WEBSITE
-        }
-        policy {
-          cookies_allowed: NO
-          setting:
-            "This feature cannot be disabled in settings. Users can refuse to "
-            "install web payment apps."
-          policy_exception_justification: "Not implemented."
-        })");
+// TODO(zino): Choose appropriate icon size dynamically on different platforms.
+// Here we choose a large ideal icon size to be big enough for all platforms.
+// Note that we only scale down for this icon size but not scale up.
+const int kPaymentAppIdealIconSize = 0xFFFF;
+const int kPaymentAppMinimumIconSize = 0;
 
 }  // namespace
 
-PaymentInstrumentIconFetcher::PaymentInstrumentIconFetcher()
-    : checking_image_object_index_(0), weak_ptr_factory_(this) {}
-PaymentInstrumentIconFetcher::~PaymentInstrumentIconFetcher() {}
-
 void PaymentInstrumentIconFetcher::Start(
-    const std::vector<payments::mojom::ImageObjectPtr>& image_objects,
     scoped_refptr<ServiceWorkerContextWrapper> service_worker_context,
+    const std::vector<payments::mojom::ImageObjectPtr>& image_objects,
     PaymentInstrumentIconFetcherCallback callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
-  for (const auto& obj : image_objects) {
-    image_objects_.emplace_back(payments::mojom::ImageObject::New(obj->src));
+  std::vector<Manifest::Icon> icons;
+  for (const auto& image_object : image_objects) {
+    Manifest::Icon icon;
+    icon.src = image_object->src;
+    icon.sizes.emplace_back(gfx::Size());
+    icon.purpose.emplace_back(Manifest::Icon::IconPurpose::ANY);
+    icons.emplace_back(icon);
   }
-  DCHECK_GT(image_objects_.size(), 0U);
 
-  callback_ = std::move(callback);
+  GURL icon_url = ManifestIconSelector::FindBestMatchingIcon(
+      icons, kPaymentAppIdealIconSize, kPaymentAppMinimumIconSize,
+      Manifest::Icon::IconPurpose::ANY);
+
+  if (icon_url.is_empty()) {
+    std::move(callback).Run(std::string());
+    return;
+  }
+
+  std::unique_ptr<std::vector<std::pair<int, int>>> provider_hosts =
+      service_worker_context->GetProviderHostIds(icon_url.GetOrigin());
 
   BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
-      base::BindOnce(&PaymentInstrumentIconFetcher::StartFromUIThread,
-                     weak_ptr_factory_.GetWeakPtr(),
-                     std::move(service_worker_context)));
+      base::BindOnce(&PaymentInstrumentIconFetcher::StartOnUI, icon_url,
+                     std::move(provider_hosts), std::move(callback)));
 }
 
-void PaymentInstrumentIconFetcher::StartFromUIThread(
-    scoped_refptr<ServiceWorkerContextWrapper> service_worker_context) {
+void PaymentInstrumentIconFetcher::StartOnUI(
+    const GURL& icon_url,
+    std::unique_ptr<std::vector<std::pair<int, int>>> provider_hosts,
+    PaymentInstrumentIconFetcherCallback callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  url_request_context_getter_ =
-      service_worker_context->storage_partition()->GetURLRequestContext();
-  if (!url_request_context_getter_) {
-    PostCallbackToIOThread(std::string());
-    return;
+  for (const auto& host : *provider_hosts) {
+    RenderFrameHostImpl* render_frame_host =
+        RenderFrameHostImpl::FromID(host.first, host.second);
+    if (!render_frame_host)
+      continue;
+
+    WebContentsImpl* web_content = static_cast<WebContentsImpl*>(
+        WebContents::FromRenderFrameHost(render_frame_host));
+    if (!web_content || web_content->IsHidden() ||
+        icon_url.GetOrigin().spec().compare(
+            web_content->GetLastCommittedURL().GetOrigin().spec()) != 0) {
+      continue;
+    }
+
+    bool can_download_icon = ManifestIconDownloader::Download(
+        web_content, icon_url, kPaymentAppIdealIconSize,
+        kPaymentAppMinimumIconSize,
+        base::Bind(&PaymentInstrumentIconFetcher::OnIconFetched,
+                   base::Passed(std::move(callback))));
+    if (can_download_icon)
+      return;
   }
 
-  FetchIcon();
+  // Failed to fetch icon.
+  BrowserThread::PostTask(BrowserThread::IO, FROM_HERE,
+                          base::BindOnce(std::move(callback), std::string()));
 }
 
-void PaymentInstrumentIconFetcher::FetchIcon() {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
-
-  if (checking_image_object_index_ >= image_objects_.size()) {
-    PostCallbackToIOThread(std::string());
-    return;
-  }
-
-  GURL* icon_src_url = &(image_objects_[checking_image_object_index_]->src);
-  if (!icon_src_url->is_valid()) {
-    checking_image_object_index_++;
-    FetchIcon();
-    return;
-  }
-
-  fetcher_ = net::URLFetcher::Create(*icon_src_url, net::URLFetcher::GET, this,
-                                     g_traffic_annotation);
-  fetcher_->SetRequestContext(url_request_context_getter_.get());
-  fetcher_->SetLoadFlags(net::LOAD_DO_NOT_SEND_COOKIES |
-                         net::LOAD_DO_NOT_SAVE_COOKIES);
-  fetcher_->SetStopOnRedirect(true);
-  fetcher_->Start();
-}
-
-void PaymentInstrumentIconFetcher::OnURLFetchComplete(
-    const net::URLFetcher* source) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
-
-  DCHECK_EQ(fetcher_.get(), source);
-  std::unique_ptr<net::URLFetcher> free_fetcher = std::move(fetcher_);
-
-  std::string data;
-  if (!(source->GetStatus().is_success() &&
-        source->GetResponseCode() == net::HTTP_OK &&
-        source->GetResponseAsString(&data))) {
-    checking_image_object_index_++;
-    FetchIcon();
-    return;
-  }
-
-  service_manager::mojom::ConnectorRequest connector_request;
-  std::unique_ptr<service_manager::Connector> connector =
-      service_manager::Connector::Create(&connector_request);
-  content::ServiceManagerConnection::GetForProcess()
-      ->GetConnector()
-      ->BindConnectorRequest(std::move(connector_request));
-
-  std::vector<uint8_t> image_data(data.begin(), data.end());
-  data_decoder::DecodeImage(
-      connector.get(), image_data, data_decoder::mojom::ImageCodec::DEFAULT,
-      false, data_decoder::kDefaultMaxSizeInBytes, gfx::Size(),
-      base::BindOnce(&PaymentInstrumentIconFetcher::DecodeImageCallback,
-                     weak_ptr_factory_.GetWeakPtr()));
-}
-
-void PaymentInstrumentIconFetcher::DecodeImageCallback(const SkBitmap& bitmap) {
+void PaymentInstrumentIconFetcher::OnIconFetched(
+    PaymentInstrumentIconFetcherCallback callback,
+    const SkBitmap& bitmap) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   if (bitmap.drawsNothing()) {
-    checking_image_object_index_++;
-    FetchIcon();
+    BrowserThread::PostTask(BrowserThread::IO, FROM_HERE,
+                            base::BindOnce(std::move(callback), std::string()));
     return;
   }
 
   gfx::Image decoded_image = gfx::Image::CreateFrom1xBitmap(bitmap);
   scoped_refptr<base::RefCountedMemory> raw_data = decoded_image.As1xPNGBytes();
-  std::string base_64;
+  std::string encoded_data;
   base::Base64Encode(
       base::StringPiece(raw_data->front_as<char>(), raw_data->size()),
-      &base_64);
-
-  PostCallbackToIOThread(base_64);
-}
-
-void PaymentInstrumentIconFetcher::PostCallbackToIOThread(
-    const std::string& decoded_data) {
+      &encoded_data);
   BrowserThread::PostTask(BrowserThread::IO, FROM_HERE,
-                          base::BindOnce(std::move(callback_), decoded_data));
+                          base::BindOnce(std::move(callback), encoded_data));
 }
 
 }  // namespace content
