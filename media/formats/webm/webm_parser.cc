6942f8dbc638eea6950e20e76f97a798a399bd44
Matt Wolenetz <wolenetz@chromium.org>
2017-09-11 10:04:04 -0700

    MSE: Change IsSiblingOrAncestor DCHECK failure to parse error
    The context of this DCHECK strongly implies that the stream is malformed
    if the DCHECK failed; the conditions in the DCHECK were not enforced by
    callers either. This change makes the DCHECK's failing condition instead
    be a parse error (in Release builds, too.)
    
    BUG=763574
    
    Change-Id: Ifd022d5e1edf471673382759e0cab9beaef4bc05

--- media/formats/webm/webm_parser.cc
+++ media/formats/webm/webm_parser.cc
@@ -985,14 +985,15 @@ bool WebMListParser::OnListEnd() {
 }
 
 bool WebMListParser::IsSiblingOrAncestor(int id_a, int id_b) const {
-  DCHECK((id_a == kWebMIdSegment) || (id_a == kWebMIdCluster));
-
   if (id_a == kWebMIdCluster) {
     // kWebMIdCluster siblings.
     for (size_t i = 0; i < arraysize(kSegmentIds); i++) {
       if (kSegmentIds[i].id_ == id_b)
         return true;
     }
+  } else {
+    // |id_a| must be either kWebMIdCluster or kWebMIdSegment.
+    return false;
   }
 
   // kWebMIdSegment siblings.
