f81408ef41af1162b4c57de9de21cd1d6fb225f9
Pavel Kalinnikov <pkalinnikov@chromium.org>
2017-09-11 17:51:49 +0200

    [u2f] Implement BLE frames and fragments.
    Bug: 763303
    Change-Id: I0d9734c91fd1de5b16d2ae738b4a414acc55646d

--- device/u2f/u2f_command_type.h
+++ device/u2f/u2f_command_type.h
@@ -14,17 +14,21 @@ namespace device {
 //
 // Each request sent to a device results in a response of *the same* type sent
 // back, unless there was an error in which case a CMD_ERROR is returned.
-//
-// TODO(pkalinnikov): Merge BLE-specific commands to this type.
 enum class U2fCommandType : uint8_t {
+  UNDEFINED = 0x00,
+
   // Sends arbitrary data to the device which echoes the same data back.
   CMD_PING = 0x81,
-  // Encapsulates a U2F protocol raw message.
+  // KEEPALIVE is used only in responses from BLE authenticators.
+  CMD_KEEPALIVE = 0x82,
+  // MSG command encapsulates a U2F raw message. It is used both for requests,
+  // and the corresponding responses.
   CMD_MSG = 0x83,
   // Requests a unique channel from a USB/HID device.
   CMD_INIT = 0x86,
   // Instructs a USB/HID authenticator to show the user that it is active.
   CMD_WINK = 0x88,
+
   // Used as a response in case an error occurs during a request.
   CMD_ERROR = 0xBF,
 };
