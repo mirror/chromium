c919aff59ea2dae5b2ae6270431a762496d32314
Koji Ishii <kojii@chromium.org>
2017-09-11 17:20:33 +0900

    breakspace
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Ib85bbc7aa8fe8528c6673cf1c30042f36239c481

--- third_party/WebKit/Source/platform/text/TextBreakIterator.h
+++ third_party/WebKit/Source/platform/text/TextBreakIterator.h
@@ -86,7 +86,14 @@ enum class LineBreakType {
   kKeepAll,
 };
 
+enum class BreakSpaceType {
+  kBefore,
+  kBeforeSpace,
+  kAfter,
+};
+
 PLATFORM_EXPORT std::ostream& operator<<(std::ostream&, LineBreakType);
+PLATFORM_EXPORT std::ostream& operator<<(std::ostream&, BreakSpaceType);
 
 class PLATFORM_EXPORT LazyLineBreakIterator final {
   STACK_ALLOCATED();
@@ -218,10 +225,8 @@ class PLATFORM_EXPORT LazyLineBreakIterator final {
   // When 'white-space:pre-wrap', or when in editing, leaging/trailing spaces
   // need to be preserved, and this optimization needs to be disabled. This mode
   // is compatible with UAX#14/ICU. http://unicode.org/reports/tr14/
-  bool BreakAfterSpace() const { return break_after_space_; }
-  void SetBreakAfterSpace(bool break_after_space) {
-    break_after_space_ = break_after_space;
-  }
+  BreakSpaceType BreakSpace() const { return break_space_; }
+  void SetBreakSpace(BreakSpaceType break_space) { break_space_ = break_space; }
 
   inline bool IsBreakable(int pos,
                           int& next_breakable,
@@ -261,7 +266,7 @@ class PLATFORM_EXPORT LazyLineBreakIterator final {
     cached_prior_context_length_ = 0;
   }
 
-  template <typename CharacterType, LineBreakType, bool>
+  template <typename CharacterType, LineBreakType, BreakSpaceType>
   int NextBreakablePosition(int pos, const CharacterType* str) const;
   template <typename CharacterType, LineBreakType>
   int NextBreakablePosition(int pos, const CharacterType* str) const;
@@ -278,7 +283,7 @@ class PLATFORM_EXPORT LazyLineBreakIterator final {
   mutable const UChar* cached_prior_context_;
   mutable unsigned cached_prior_context_length_;
   LineBreakType break_type_;
-  bool break_after_space_ = false;
+  BreakSpaceType break_space_ = BreakSpaceType::kBefore;
 };
 
 // Iterates over "extended grapheme clusters", as defined in UAX #29.
