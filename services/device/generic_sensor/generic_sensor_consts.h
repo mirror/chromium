1d805f5cd4728a476ec08307b64094ab0af4b2c5
Jun Cai <juncai@chromium.org>
2017-09-11 11:32:05 -0700

    Move radian and degree conversion constants to generic_sensor_consts.h
    Currently there are radian and degree conversion constants in both
    generic_sensor_consts.h and orientation_util.h. This CL moves them to
    generic_sensor_consts.h.
    
    Bug: None
    Change-Id: I7de69b7e9536bb003158ab9d3611ec9a621211fc

--- services/device/generic_sensor/generic_sensor_consts.h
+++ services/device/generic_sensor/generic_sensor_consts.h
@@ -5,16 +5,20 @@
 #ifndef SERVICES_DEVICE_GENERIC_SENSOR_GENERIC_SENSOR_CONSTS_H_
 #define SERVICES_DEVICE_GENERIC_SENSOR_GENERIC_SENSOR_CONSTS_H_
 
-#define _USE_MATH_DEFINES
-#include <math.h>
+#define _USE_MATH_DEFINES  // For VC++ to get M_PI. This has to be first.
+
+#include <cmath>
 
 namespace device {
 
 // Required for conversion from G/s^2 to m/s^2
 constexpr double kMeanGravity = 9.80665;
 
-// Required for conversion from deg to rad
-constexpr double kRadiansInDegrees = M_PI / 180.0;
+// Conversion ratio from radians to degrees.
+constexpr double kRadiansToDegrees = 180.0 / M_PI;
+
+// Conversion ratio from degrees to radians.
+constexpr double kDegreesToRadians = M_PI / 180.0;
 
 // Required for conversion from Gauss to uT.
 constexpr double kMicroteslaInGauss = 100.0;
