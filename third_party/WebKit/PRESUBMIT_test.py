d5ed0e1f4a4dffd10c6d9b7d30cccfb42d481a9e
Daniel Cheng <dcheng@chromium.org>
2017-09-11 10:45:51 -0700

    Reformat PRESUBMIT_test.py to be in standard Google Python style
    Also deletes the various presubmit style checks relating to Python.
    As a bonus, fixes PRESUBMIT_test.py so it actually runs too.
    
    Bug: 763980
    Change-Id: I1f629673ce550adc17eacea098cf71e7d6be9436

--- third_party/WebKit/PRESUBMIT_test.py
+++ third_party/WebKit/PRESUBMIT_test.py
@@ -1,69 +1,80 @@
-# disable camel case warning
-# pylint: disable=C0103
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Note: running this test requires installing the package python-mock.
 import PRESUBMIT
 
 import mock
+import os.path
 import subprocess
+import sys
 import unittest
 
-from PRESUBMIT_test_mocks import MockInputApi, MockOutputApi, MockAffectedFile  # pylint: disable=F0401
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
+
+from PRESUBMIT_test_mocks import MockInputApi
+from PRESUBMIT_test_mocks import MockOutputApi
+from PRESUBMIT_test_mocks import MockAffectedFile
 
 
 class Capture(object):
-    """
-    Class to capture a call argument that can be tested later on.
-    """
-    def __init__(self):
-        self.value = None
+  """
+  Class to capture a call argument that can be tested later on.
+  """
 
-    def __eq__(self, other):
-        self.value = other
-        return True
+  def __init__(self):
+    self.value = None
+
+  def __eq__(self, other):
+    self.value = other
+    return True
 
 
 class PresubmitTest(unittest.TestCase):
 
-    @mock.patch('subprocess.Popen')
-    def testCheckChangeOnUploadWithWebKitAndChromiumFiles(self, _):
-        """
-        This verifies that CheckChangeOnUpload will only call check-webkit-style
-        on WebKit files.
-        """
-        diff_file_webkit_h = ['some diff']
-        diff_file_chromium_h = ['another diff']
-        mock_input_api = MockInputApi()
-        mock_input_api.files = [MockAffectedFile('FileWebkit.h',
-                                                 diff_file_webkit_h),
-                                MockAffectedFile('file_chromium.h',
-                                                 diff_file_chromium_h)]
-        # Access to a protected member _CheckStyle
-        # pylint: disable=W0212
-        PRESUBMIT._CheckStyle(mock_input_api, MockOutputApi())
-        capture = Capture()
-        # pylint: disable=E1101
-        subprocess.Popen.assert_called_with(capture, stderr=-1)
-        self.assertEqual(4, len(capture.value))
-        self.assertEqual('../../FileWebkit.h', capture.value[3])
+  @mock.patch('subprocess.Popen')
+  def testCheckChangeOnUploadWithWebKitAndChromiumFiles(self, _):
+    """
+    This verifies that CheckChangeOnUpload will only call check-webkit-style
+    on WebKit files.
+    """
+    diff_file_webkit_h = ['some diff']
+    diff_file_chromium_h = ['another diff']
+    mock_input_api = MockInputApi()
+    mock_input_api.files = [
+        MockAffectedFile('FileWebkit.h', diff_file_webkit_h),
+        MockAffectedFile('file_chromium.h', diff_file_chromium_h)
+    ]
+    # Access to a protected member _CheckStyle
+    # pylint: disable=W0212
+    PRESUBMIT._CheckStyle(mock_input_api, MockOutputApi())
+    capture = Capture()
+    # pylint: disable=E1101
+    subprocess.Popen.assert_called_with(capture, stderr=-1)
+    self.assertEqual(4, len(capture.value))
+    self.assertEqual('../../FileWebkit.h', capture.value[3])
 
-    @mock.patch('subprocess.Popen')
-    def testCheckChangeOnUploadWithEmptyAffectedFileList(self, _):
-        """
-        This verifies that CheckChangeOnUpload will skip calling
-        check-webkit-style if the affected file list is empty.
-        """
-        diff_file_chromium1_h = ['some diff']
-        diff_file_chromium2_h = ['another diff']
-        mock_input_api = MockInputApi()
-        mock_input_api.files = [MockAffectedFile('first_file_chromium.h',
-                                                 diff_file_chromium1_h),
-                                MockAffectedFile('second_file_chromium.h',
-                                                 diff_file_chromium2_h)]
-        # Access to a protected member _CheckStyle
-        # pylint: disable=W0212
-        PRESUBMIT._CheckStyle(mock_input_api, MockOutputApi())
-        # pylint: disable=E1101
-        subprocess.Popen.assert_not_called()
+  @mock.patch('subprocess.Popen')
+  def testCheckChangeOnUploadWithEmptyAffectedFileList(self, _):
+    """
+    This verifies that CheckChangeOnUpload will skip calling
+    check-webkit-style if the affected file list is empty.
+    """
+    diff_file_chromium1_h = ['some diff']
+    diff_file_chromium2_h = ['another diff']
+    mock_input_api = MockInputApi()
+    mock_input_api.files = [
+        MockAffectedFile('first_file_chromium.h', diff_file_chromium1_h),
+        MockAffectedFile('second_file_chromium.h', diff_file_chromium2_h)
+    ]
+    # Access to a protected member _CheckStyle
+    # pylint: disable=W0212
+    PRESUBMIT._CheckStyle(mock_input_api, MockOutputApi())
+    # pylint: disable=E1101
+    subprocess.Popen.assert_not_called()
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
