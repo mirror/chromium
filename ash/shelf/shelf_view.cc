8900d866429eef91e17a2a9798db6fb2de67b290
MinChen <minch@chromium.org>
2017-09-11 13:36:06 -0700

    Make shelf item can be dragged when context menu is opened.
    Current context menu of shelf,
    1) Allow dragging before context menu is created.
    2) No dragging after context menu is opened.
    
    In order to keep the same as Android OS,
    1) Allow dragging during the same press (don't release the finger)
    after context menu is opened.
    2) Close the context menu once start dragging.
    
    Changes in this cl,
    1. Add |SEND_UNHANDLED_GESTURES_TO_OWNER| in MenuRunner. This means the
    owner hopes that menu can help send the unhandled gesture events to the
    owner instead of dropping them. e.g, the context menu of the shelf
    items, and this is only true if shelf is not autohide.
    2. Transfer events from owner to menu host if the owner needs gesture
    events. vice verse. e.g. Shelf items hope can be dragged after long
    press which will create the context menu.
    3. Handle the LONG_TAP events on shelf items. Since gesture events
    been transferred from shelf to MenuHost, but we don't want it will show
    the context menu again.
    
    This is an cl moved from rietveld to here, check the history
    https://codereview.chromium.org/2876203003/
    
    Bug: 710601, 748253
    Change-Id: I7b39cce9f7e54922c318f1ac6398a21f219045da

--- ash/shelf/shelf_view.cc
+++ ash/shelf/shelf_view.cc
@@ -672,6 +672,8 @@ bool ShelfView::ShouldEventActivateButton(View* view, const ui::Event& event) {
 void ShelfView::PointerPressedOnButton(views::View* view,
                                        Pointer pointer,
                                        const ui::LocatedEvent& event) {
+  if (IsShowingMenu())
+    launcher_menu_runner_->Cancel();
   if (drag_view_)
     return;
 
@@ -1733,6 +1735,7 @@ void ShelfView::AfterItemSelected(
 void ShelfView::AfterGetContextMenuItems(
     const ShelfID& shelf_id,
     const gfx::Point& point,
+    views::View* source,
     ui::MenuSourceType source_type,
     std::vector<mojom::MenuItemPtr> menu_items) {
   context_menu_id_ = shelf_id;
@@ -1740,7 +1743,7 @@ void ShelfView::AfterGetContextMenuItems(
   ShowMenu(base::MakeUnique<ShelfContextMenuModel>(
                std::move(menu_items), model_->GetShelfItemDelegate(shelf_id),
                display_id),
-           nullptr /* source */, point, true /* context_menu */, source_type,
+           source, point, true /* context_menu */, source_type,
            nullptr /* ink_drop */);
 }
 
@@ -1788,7 +1791,7 @@ void ShelfView::ShowContextMenuForView(views::View* source,
   model_->GetShelfItemDelegate(item->id)->GetContextMenuItems(
       display_id, base::Bind(&ShelfView::AfterGetContextMenuItems,
                              weak_factory_.GetWeakPtr(), item->id,
-                             context_menu_point, source_type));
+                             context_menu_point, source, source_type));
 }
 
 void ShelfView::ShowMenu(std::unique_ptr<ui::MenuModel> menu_model,
@@ -1807,6 +1810,15 @@ void ShelfView::ShowMenu(std::unique_ptr<ui::MenuModel> menu_model,
   if (context_menu)
     run_types |=
         views::MenuRunner::CONTEXT_MENU | views::MenuRunner::FIXED_ANCHOR;
+
+  // Selected shelf items with context menu opened can only be dragged if the
+  // shelf is not in auto-hide.
+  const ShelfItem* item = ShelfItemForView(source);
+  if (context_menu && item && item->type != TYPE_APP_LIST &&
+      shelf()->auto_hide_behavior() == SHELF_AUTO_HIDE_BEHAVIOR_NEVER) {
+    run_types |= views::MenuRunner::SEND_GESTURE_EVENTS_TO_OWNER;
+  }
+
   launcher_menu_runner_.reset(
       new views::MenuRunner(menu_model_adapter_->CreateMenu(), run_types));
 
@@ -1859,10 +1871,6 @@ void ShelfView::ShowMenu(std::unique_ptr<ui::MenuModel> menu_model,
 
 void ShelfView::OnMenuClosed(views::InkDrop* ink_drop) {
   context_menu_id_ = ShelfID();
-
-  // Hide the hide overflow bubble after showing a context menu for its items.
-  if (owner_overflow_bubble_)
-    owner_overflow_bubble_->Hide();
 
   closing_event_time_ = launcher_menu_runner_->closing_event_time();
 
