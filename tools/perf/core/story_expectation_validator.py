96966e350a6840b79847f32e6a3032b09ecb3022
rnephew <rnephew@chromium.org>
2017-09-11 10:40:24 -0700

    [Telemetry] Add ability to print disabled stories to story_expectation_validator
    Requires https://codereview.chromium.org/3008203003/
    commit=false
    
    Bug: 723636
    Change-Id: I3bda0037af48c78fd7b80912f4a84d0542a782e8

--- tools/perf/core/story_expectation_validator.py
+++ tools/perf/core/story_expectation_validator.py
@@ -3,7 +3,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """Script to check validity of StoryExpectations."""
+
+import argparse
+import json
 import os
+import pprint
 
 from core import benchmark_finders
 from core import path_util
@@ -39,8 +43,53 @@ def validate_story_names(benchmarks):
     assert not failed_stories, 'Incorrect story names: %s' % str(failed_stories)
 
 
+def ListDisabledStories(benchmarks, path):
+  # Creates a dictionary of the format:
+  # {
+  #   'benchmark_name1' : {
+  #     'story_1': [
+  #       {'conditions': conditions, 'reason': reason},
+  #       ...
+  #     ],
+  #     ...
+  #   },
+  #   ...
+  # }
+  disables = {}
+  for benchmark in benchmarks:
+    name = benchmark.Name()
+    disables[name] = {}
+    expectations = benchmark().GetExpectations().AsDict()['stories']
+    for story in expectations:
+      for conditions, reason in  expectations[story]:
+        if not disables[name].get(story):
+          disables[name][story] = []
+          conditions_str = [str(a) for a in conditions]
+        disables[name][story].append((conditions_str, reason))
+  if path:
+    print 'Disabled tests information saved to %s' % path
+    with open(path, 'w') as f:
+      json.dump(disables, f)
+  else:
+    pp = pprint.PrettyPrinter()
+    pp.pprint(disables)
+
+
 def main(args):
-  del args  # unused
+  parser = argparse.ArgumentParser(
+      description=('Tests if disabled stories exist.'))
+  parser.add_argument(
+      '--list', action='store_true', default=False,
+      help=('Prints list of disabled stories.'))
+  parser.add_argument(
+      '--output-file',
+       help='If --list is set, this will be the file the json is saved to.'
+  )
+  options = parser.parse_args(args)
+
   benchmarks = benchmark_finders.GetAllBenchmarks()
-  validate_story_names(benchmarks)
+  if options.list:
+    ListDisabledStories(benchmarks, options.output_file)
+  else:
+    validate_story_names(benchmarks)
   return 0
