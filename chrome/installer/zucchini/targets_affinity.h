fd4276aa5e0492b75d675e806aa5f177340589b3
Etienne Pierre-Doray <etiennep@chromium.org>
2017-09-11 19:52:58 -0400

    [Zucchini] Introduce targets affinity.
    This CL adds TargetsAffinity and unittests to represent and work with
    affinity between targets. This is meant to replace use of
    MakeNewTargetsFromEquivalenceMap() for target association and labelling,
    and is being integrated in a follow-up CL (630558).
    
    Bug: 760614
    Change-Id: I9cec412b067f146807adb4aa00c9c1eda21bec55

---
+++ chrome/installer/zucchini/targets_affinity.h
@@ -0,0 +1,58 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_INSTALLER_ZUCCHINI_TARGETS_AFFINITY_H_
+#define CHROME_INSTALLER_ZUCCHINI_TARGETS_AFFINITY_H_
+
+#include <stddef.h>
+
+#include <vector>
+
+#include "chrome/installer/zucchini/image_utils.h"
+
+namespace zucchini {
+
+class EquivalenceMap;
+
+// Computes and stores affinity between old and new targets for a single target
+// pool.
+class TargetsAffinity {
+ public:
+  TargetsAffinity();
+  ~TargetsAffinity();
+
+  // Infers affinity between |old_targets| and |new_targets| using similarities
+  // described by |equivalence_map|, and updates internal state for retrieval of
+  // affinity metrics. Both |old_targets| and |new_targets| are targets in the
+  // same pool and are sorted in ascending order.
+  void InferFromSimilarities(const EquivalenceMap& equivalence_map,
+                             const std::vector<offset_t>& old_targets,
+                             const std::vector<offset_t>& new_targets);
+
+  // Assigns labels to targets based on associations previously inferred.
+  // Label 0 is assigned to targets that can not be associated. Labels for old
+  // targets are written to |old_labels| and labels for new targets are written
+  // to |new_labels|. Returns the upper bound on assigned labels.
+  size_t AssignLabels(double min_affinity,
+                      std::vector<offset_t>* old_labels,
+                      std::vector<offset_t>* new_labels);
+
+  // Returns the affinity metric between targets identified by |old_keys| and
+  // |new_keys|. Affinity > 0 means an association is likely, < 0 means
+  // incompatible association, and 0 means neither targets have been associated.
+  double AffinityBetween(offset_t old_keys, offset_t new_keys) const;
+
+ private:
+  struct Association {
+    offset_t other = 0;
+    double affinity = 0.0;
+  };
+
+  std::vector<Association> forward_association_;
+  std::vector<Association> backward_association_;
+};
+
+}  // namespace zucchini
+
+#endif  // CHROME_INSTALLER_ZUCCHINI_TARGETS_AFFINITY_H_
