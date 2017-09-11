b4a820b7caa9ce3d33ca280581872bf95c59acfb
ericwilligers <ericwilligers@chromium.org>
2017-09-11 23:33:21 +1000

    CSS: Retire runtime flag CSS3TextDecorations
    Support for text decoration properties shipped to stable in Chrome 57.
    
    The runtime flag is no longer needed.
    
    BUG=165462
    
    Change-Id: I68bfb1c902fa5d51de8040493d3e12ab6b61349d

--- third_party/WebKit/Source/core/editing/EditingStyle.cpp
+++ third_party/WebKit/Source/core/editing/EditingStyle.cpp
@@ -69,11 +69,7 @@ namespace blink {
 using namespace cssvalue;
 
 static const CSSPropertyID& TextDecorationPropertyForEditing() {
-  static const CSSPropertyID kProperty =
-      RuntimeEnabledFeatures::CSS3TextDecorationsEnabled()
-          ? CSSPropertyTextDecorationLine
-          : CSSPropertyTextDecoration;
-  return kProperty;
+  return CSSPropertyTextDecorationLine;
 }
 
 // Editing style properties must be preserved during editing operation.
@@ -106,8 +102,7 @@ static const Vector<CSSPropertyID>& AllEditingProperties() {
     CSSProperty::FilterEnabledCSSPropertiesIntoVector(
         kStaticEditingProperties, WTF_ARRAY_LENGTH(kStaticEditingProperties),
         properties);
-    if (RuntimeEnabledFeatures::CSS3TextDecorationsEnabled())
-      properties.erase(properties.Find(CSSPropertyTextDecoration));
+    properties.erase(properties.Find(CSSPropertyTextDecoration));
   }
   return properties;
 }
@@ -864,10 +859,9 @@ bool EditingStyle::ConflictsWithInlineStyleOfElement(
       if (!conflicting_properties)
         return true;
       conflicting_properties->push_back(CSSPropertyTextDecoration);
-      // Because text-decoration expands to text-decoration-line when CSS3
-      // Text Decoration is enabled, we also state it as conflicting.
-      if (RuntimeEnabledFeatures::CSS3TextDecorationsEnabled())
-        conflicting_properties->push_back(CSSPropertyTextDecorationLine);
+      // Because text-decoration expands to text-decoration-line,
+      // we also state it as conflicting.
+      conflicting_properties->push_back(CSSPropertyTextDecorationLine);
       if (extracted_style)
         extracted_style->SetProperty(
             TextDecorationPropertyForEditing(),
