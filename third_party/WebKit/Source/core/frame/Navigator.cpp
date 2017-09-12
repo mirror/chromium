ce2812359f6c5014d62e1b9a1c67bda254b3a6cc
Tarun Bansal <tbansal@chromium.org>
2017-09-12 15:48:39 -0700

    Move some of the memory coordinator code to third_party/Webkit/common
    This is a pure refactor CL and does not introduce any functionality change.
    Moving some of the code to t_p/Webkit/common makes it possible to access
    that code in chrome/browser.
    
    In the next CL, chrome/browser/chrome_content_browser_client.cc
    would call GetApproximatedDeviceMemory() to attach required
    client hints to the navigation requests.
    
    Bug:764513
    Change-Id: I96e5d812b8f46e83601d02c6c48f1a37919f8041

--- third_party/WebKit/Source/core/frame/Navigator.cpp
+++ third_party/WebKit/Source/core/frame/Navigator.cpp
@@ -34,6 +34,7 @@
 #include "core/page/Page.h"
 #include "platform/Language.h"
 #include "platform/MemoryCoordinator.h"
+#include "third_party/WebKit/common/memory_coordinator/physical_memory_coordinator.h"
 
 namespace blink {
 
@@ -52,7 +53,7 @@ String Navigator::vendor() const {
 }
 
 float Navigator::deviceMemory() const {
-  return MemoryCoordinator::GetApproximatedDeviceMemory();
+  return PhysicalMemoryCoordinator::GetApproximatedDeviceMemory();
 }
 
 String Navigator::vendorSub() const {
