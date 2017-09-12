dc1cec972974440b9ecde3a583b2974eac67959e
Gauthier Ambard <gambard@chromium.org>
2017-09-12 15:27:09 +0200

    [ios clean] Add incognito style
    This CL adds the incognito style to the toolbar, using configuration
    objects.
    
    Bug:762578
    Change-Id: I09e598a99b20cb5e4c08e0d56c4ef0a0b1847790

---
+++ ios/clean/chrome/browser/ui/toolbar/toolbar_configuration.h
@@ -0,0 +1,37 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CLEAN_CHROME_BROWSER_UI_TOOLBAR_TOOLBAR_CONFIGURATION_H_
+#define IOS_CLEAN_CHROME_BROWSER_UI_TOOLBAR_TOOLBAR_CONFIGURATION_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/clean/chrome/browser/ui/toolbar/toolbar_style.h"
+
+// Toolbar configuration object giving access to styling elements.
+@interface ToolbarConfiguration : NSObject
+
+- (instancetype)initWithStyle:(ToolbarStyle)style NS_DESIGNATED_INITIALIZER;
+- (instancetype)init NS_UNAVAILABLE;
+
+@property(nonatomic, assign) ToolbarStyle style;
+
+// Background color of the toolbar.
+- (UIColor*)backgroundColor;
+
+// Background color of the omnibox.
+- (UIColor*)omniboxBackgroundColor;
+
+// Border color of the omnibox.
+- (UIColor*)omniboxBorderColor;
+
+// Color of the title of the buttons for the normal state.
+- (UIColor*)buttonTitleNormalColor;
+
+// Color of the title of the buttons for the highlighted state.
+- (UIColor*)buttonTitleHighlightedColor;
+
+@end
+
+#endif  // IOS_CLEAN_CHROME_BROWSER_UI_TOOLBAR_TOOLBAR_CONFIGURATION_H_
