036912e60e29818b8c25679b7c39448c976e9ad9
Daichi Hirono <hirono@chromium.org>
2017-09-12 11:44:02 +0900

    Add unittests for DataDevice
    Bug: 750997
    Test: exo_unittests --gtest_filter=DataDeviceTest.*
    Change-Id: I83c41477a945e363c70560595fe3a079ed62c2cb

--- components/exo/data_offer.cc
+++ components/exo/data_offer.cc
@@ -12,11 +12,14 @@
 #include "components/exo/data_offer_observer.h"
 #include "components/exo/file_helper.h"
 #include "ui/base/clipboard/clipboard.h"
+#include "ui/base/dragdrop/file_info.h"
 #include "ui/base/dragdrop/os_exchange_data.h"
 #include "url/gurl.h"
 
 namespace exo {
 namespace {
+
+constexpr char kUriListSeparator[] = "\r\n";
 
 class RefCountedString16 : public base::RefCountedMemory {
  public:
@@ -53,7 +56,8 @@ void WriteFileDescriptor(base::ScopedFD fd,
 DataOffer::DataOffer(DataOfferDelegate* delegate) : delegate_(delegate) {}
 
 DataOffer::~DataOffer() {
-  delegate_->OnDataOfferDestroying(this);
+  if (delegate_)
+    delegate_->OnDataOfferDestroying(this);
   for (DataOfferObserver& observer : observers_) {
     observer.OnDataOfferDestroying(this);
   }
@@ -90,13 +94,15 @@ void DataOffer::Finish() {
 void DataOffer::SetActions(const base::flat_set<DndAction>& dnd_actions,
                            DndAction preferred_action) {
   dnd_action_ = preferred_action;
-  delegate_->OnAction(preferred_action);
+  if (delegate_)
+    delegate_->OnAction(preferred_action);
 }
 
 void DataOffer::SetSourceActions(
     const base::flat_set<DndAction>& source_actions) {
   source_actions_ = source_actions;
-  delegate_->OnSourceActions(source_actions);
+  if (delegate_)
+    delegate_->OnSourceActions(source_actions);
 }
 
 void DataOffer::SetDropData(FileHelper* file_helper,
@@ -111,17 +117,23 @@ void DataOffer::SetDropData(FileHelper* file_helper,
     }
   }
   if (data.HasFile()) {
-    base::FilePath path;
-    if (data.GetFilename(&path)) {
-      GURL url;
-      if (file_helper->ConvertPathToUrl(path, &url)) {
-        base::string16 url_string = base::UTF8ToUTF16(url.spec());
-        drop_data_.emplace(
-            file_helper->GetMimeTypeForUriList(),
-            RefCountedString16::TakeString(std::move(url_string)));
+    std::vector<ui::FileInfo> files;
+    if (data.GetFilenames(&files)) {
+      base::string16 url_list;
+      for (const auto& info : files) {
+        GURL url;
+        if (file_helper->ConvertPathToUrl(info.path, &url)) {
+          if (!url_list.empty())
+            url_list += base::UTF8ToUTF16(kUriListSeparator);
+          url_list += base::UTF8ToUTF16(url.spec());
+        }
       }
+      drop_data_.emplace(file_helper->GetMimeTypeForUriList(),
+                         RefCountedString16::TakeString(std::move(url_list)));
     }
   }
+  if (!delegate_)
+    return;
   for (const auto& pair : drop_data_) {
     delegate_->OnOffer(pair.first);
   }
