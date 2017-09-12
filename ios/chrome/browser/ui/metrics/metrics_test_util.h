fb41d4301795c83f06d7419003eb72bba37fa774
Gregory Chatzinoff <gchatz@chromium.org>
2017-09-11 18:28:52 -0700

    Metrics Use Case
    WIP
    
    
    Bug:none
    Change-Id: Ifa7b1aa7c27835ec6e11df07382c673ab747fab9

---
+++ ios/chrome/browser/ui/metrics/metrics_test_util.h
@@ -0,0 +1,20 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_METRICS_METRICS_TEST_UTIL_H_
+#define IOS_CHROME_BROWSER_UI_METRICS_METRICS_TEST_UTIL_H_
+
+#import <Foundation/Foundation.h>
+
+namespace metrics_test_util {
+
+// Constructs an NSInvocation for an instance method defined by
+// |selector| in |protocol|.
+NSInvocation* GetInvocationForProtocolInstanceMethod(Protocol* protocol,
+                                                     SEL selector,
+                                                     BOOL isRequiredMethod);
+
+}  // namespace metrics_test_util
+
+#endif  // IOS_CHROME_BROWSER_UI_METRICS_METRICS_TEST_UTIL_H_
