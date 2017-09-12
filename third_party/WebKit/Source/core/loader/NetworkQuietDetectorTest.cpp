1241ad79491f58f8f6df41dd6b2a951f46c47430
Peiyong Lin <lpy@chromium.org>
2017-09-12 16:23:43 -0700

    Use network idleness signal from Performance Monitor.
    Previously we have a dedicated NetworkQuietDetector to detect network
    idleness signal and send it to services/resource_coordinator, however,
    such signal uses a timer, thus it can't handle cases like, if a task is
    being processed for a long time, network requests won't be able to be
    fired, thus when the task is finished processing the network idleness
    signal will be fired before network requests are fired, which results in
    inaccurate network idleness signal.
    
    So, in this patch, we reuse the network idleness signal from Performance
    Monitor, which avoids using timer and takes task process time into
    account by subtracting the task time.
    
    BUG=730098, 749789
    
    Change-Id: Ied003129cd5ddf7c1ebb93019723e11e9a31be04

--- third_party/WebKit/Source/core/loader/NetworkQuietDetectorTest.cpp
+++ third_party/WebKit/Source/core/loader/NetworkQuietDetectorTest.cpp
@@ -1,15 +1,11 @@
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 #include "core/loader/NetworkQuietDetector.h"
-
 #include "core/testing/DummyPageHolder.h"
 #include "platform/testing/TestingPlatformSupport.h"
 #include "testing/gtest/include/gtest/gtest.h"
-
 namespace blink {
-
 class NetworkQuietDetectorTest : public ::testing::Test {
  protected:
   void SetUp() override {
