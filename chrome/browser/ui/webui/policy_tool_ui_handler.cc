df5904293afbdb57327ed070a04c1ea1a2d775c4
Anton Urusov <urusant@google.com>
2017-09-12 12:04:49 -0400

    Improved invalid JSON error Bug:
    Change-Id: Icd40b3dbf14334317ebd13c634d33f876148f52d

---
+++ chrome/browser/ui/webui/policy_tool_ui_handler.cc
@@ -0,0 +1,241 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <iostream>
+
+#include "chrome/browser/ui/webui/policy_tool_ui_handler.h"
+
+#include "base/files/file_enumerator.h"
+#include "base/files/file_util.h"
+#include "base/json/json_reader.h"
+#include "base/json/json_writer.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/task_scheduler/post_task.h"
+#include "build/build_config.h"
+#include "chrome/browser/profiles/profile.h"
+
+namespace {
+
+base::FilePath::StringType StringToFilePath(const std::string& value) {
+#if defined(OS_WIN)
+  return base::UTF8ToUTF16(value);
+#else
+  return value;
+#endif
+}
+
+}  // namespace
+
+PolicyToolUIHandler::PolicyToolUIHandler() : callback_weak_ptr_factory_(this) {}
+
+PolicyToolUIHandler::~PolicyToolUIHandler() {}
+
+void PolicyToolUIHandler::RegisterMessages() {
+  // Set directory for storing sessions.
+  sessions_dir_ = Profile::FromWebUI(web_ui())->GetPath().Append(
+      FILE_PATH_LITERAL("Policy sessions"));
+
+  web_ui()->RegisterMessageCallback(
+      "initialized", base::Bind(&PolicyToolUIHandler::HandleInitializedAdmin,
+                                base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+      "loadSession", base::Bind(&PolicyToolUIHandler::HandleLoadSession,
+                                base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+      "updateSession", base::Bind(&PolicyToolUIHandler::HandleUpdateSession,
+                                  base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+      "enableLogging", base::Bind(&PolicyToolUIHandler::HandleEnableLogging,
+                                  base::Unretained(this)));
+}
+
+void PolicyToolUIHandler::OnJavascriptDisallowed() {
+  callback_weak_ptr_factory_.InvalidateWeakPtrs();
+}
+
+base::FilePath PolicyToolUIHandler::GetSessionPath(
+    const base::FilePath::StringType& name) const {
+  return sessions_dir_.Append(name).AddExtension(FILE_PATH_LITERAL("json"));
+}
+
+base::ListValue PolicyToolUIHandler::GetSessionsList() {
+  base::FileEnumerator enumerator(sessions_dir_, false,
+                                  base::FileEnumerator::FILES,
+                                  FILE_PATH_LITERAL("*.json"));
+  // A vector of session names and their last access times.
+  using Session = std::pair<base::Time, base::FilePath::StringType>;
+  std::vector<Session> sessions;
+  for (base::FilePath name = enumerator.Next(); !name.empty();
+       name = enumerator.Next()) {
+    base::File::Info info;
+    base::GetFileInfo(name, &info);
+    sessions.push_back(
+        {info.last_accessed, name.BaseName().RemoveExtension().value()});
+  }
+  // Sort the sessions by the the time of last access in decreasing order.
+  std::sort(sessions.begin(), sessions.end(), std::greater<Session>());
+
+  // Convert sessions to the list containing only names.
+  base::ListValue session_names;
+  for (Session session : sessions) {
+    session_names.GetList().push_back(base::Value(session.second));
+  }
+  return session_names;
+}
+
+void PolicyToolUIHandler::SetDefaultSessionName() {
+  base::ListValue sessions = GetSessionsList();
+  if (sessions.GetSize() == 0U) {
+    // If there are no sessions, fallback to the default session name.
+    session_name_ = FILE_PATH_LITERAL("policy");
+  } else {
+    session_name_ = StringToFilePath(sessions.GetList()[0].GetString());
+  }
+}
+
+std::string PolicyToolUIHandler::ReadOrCreateFileCallback() {
+  // Create sessions directory, if it doesn't exist yet.
+  // If unable to create a directory, just silently return a dictionary
+  // indicating that logging was unsuccessful.
+  // TODO(urusant): add a possibility to disable logging to disk in similar
+  // cases.
+  if (!base::CreateDirectory(sessions_dir_)) {
+    is_logging_enabled_ = false;
+  }
+
+  // Initialize session name if it is not initialized yet.
+  if (session_name_.empty())
+    SetDefaultSessionName();
+
+  const base::FilePath session_path = GetSessionPath(session_name_);
+
+  // Check if the file for the current session already exists. If not, create it
+  // and put an empty dictionary in it.
+  base::File session_file(session_path, base::File::Flags::FLAG_CREATE |
+                                            base::File::Flags::FLAG_WRITE);
+
+  // If unable to open the file, just return an empty dictionary.
+  if (session_file.created()) {
+    session_file.WriteAtCurrentPos("{}", 2);
+    return "{}";
+  }
+  session_file.Close();
+
+  // Check that the file exists by now. If it doesn't, it means that at least
+  // one of the filesystem operations wasn't successful. In this case, return
+  // a dictionary indicating that logging was unsuccessful. Potentially this can
+  // also be the place to disable logging to disk.
+  if (!PathExists(session_path)) {
+    is_logging_enabled_ = false;
+  }
+  // Read file contents.
+  std::string contents;
+  base::ReadFileToString(session_path, &contents);
+
+  // Touch the file to remember the last session.
+  base::TouchFile(session_path, base::Time::Now(), base::Time::Now());
+
+  return contents;
+}
+
+void PolicyToolUIHandler::OnFileRead(const std::string& contents) {
+  // If the logging is disabled, send a message about that to the UI.
+  if (!is_logging_enabled_) {
+    CallJavascriptFunction("policy.Page.disableLogging");
+  } else {
+    std::unique_ptr<base::DictionaryValue> value =
+        base::DictionaryValue::From(base::JSONReader::Read(contents));
+
+    // If contents is not a properly formed JSON string, we alert the user about
+    // it and send an empty dictionary instead. Note that the broken session
+    // file would be overrided when the user edits something, so any manual
+    // changes that made it invalid will be lost.
+    // TODO(urusant): do it in a smarter way, e.g. revert to a previous session
+    // or a new session with a generated name.
+    if (!value) {
+      value = base::MakeUnique<base::DictionaryValue>();
+      CallJavascriptFunction("policy.Page.disableEditing");
+      return;
+    }
+    CallJavascriptFunction("policy.Page.setPolicyValues", *value);
+  }
+}
+
+void PolicyToolUIHandler::ImportFile() {
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_VISIBLE},
+      base::BindOnce(&PolicyToolUIHandler::ReadOrCreateFileCallback,
+                     base::Unretained(this)),
+      base::BindOnce(&PolicyToolUIHandler::OnFileRead,
+                     callback_weak_ptr_factory_.GetWeakPtr()));
+}
+
+void PolicyToolUIHandler::HandleInitializedAdmin(const base::ListValue* args) {
+  DCHECK_EQ(0U, args->GetSize());
+  AllowJavascript();
+  is_logging_enabled_ = true;
+  SendPolicyNames();
+  ImportFile();
+}
+
+bool PolicyToolUIHandler::IsValidSessionName(
+    const base::FilePath::StringType& name) const {
+  // Check if the session name is valid, which means that it doesn't use
+  // filesystem navigation (e.g. ../ or nested folder).
+  base::FilePath session_path = GetSessionPath(name);
+  return !session_path.empty() && session_path.DirName() == sessions_dir_;
+}
+
+void PolicyToolUIHandler::HandleLoadSession(const base::ListValue* args) {
+  DCHECK_EQ(1U, args->GetSize());
+  base::FilePath::StringType new_session_name =
+      StringToFilePath(args->GetList()[0].GetString());
+  if (!IsValidSessionName(new_session_name)) {
+    ShowErrorMessageToUser("errorInvalidSessionName");
+    return;
+  }
+  session_name_ = new_session_name;
+  ImportFile();
+}
+
+bool PolicyToolUIHandler::DoUpdateSession(const std::string& contents) {
+  return base::WriteFile(sessions_dir_.Append(session_name_)
+                             .AddExtension(FILE_PATH_LITERAL(".json")),
+                         contents.c_str(),
+                         contents.size()) == static_cast<int>(contents.size());
+}
+
+void PolicyToolUIHandler::OnSessionUpdated(bool is_successful) {
+  if (!is_successful) {
+    is_logging_enabled_ = false;
+    AllowJavascript();
+    CallJavascriptFunction("policy.Page.disableLogging");
+  }
+}
+
+void PolicyToolUIHandler::HandleUpdateSession(const base::ListValue* args) {
+  DCHECK(is_logging_enabled_);
+  DCHECK_EQ(1U, args->GetSize());
+
+  const base::DictionaryValue* policy_values = nullptr;
+  args->GetDictionary(0, &policy_values);
+  DCHECK(policy_values);
+  std::string converted_values;
+  base::JSONWriter::Write(*policy_values, &converted_values);
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::BACKGROUND},
+      base::BindOnce(&PolicyToolUIHandler::DoUpdateSession,
+                     base::Unretained(this), converted_values),
+      base::BindOnce(&PolicyToolUIHandler::OnSessionUpdated,
+                     callback_weak_ptr_factory_.GetWeakPtr()));
+}
+
+void PolicyToolUIHandler::HandleEnableLogging(const base::ListValue* args) {
+  is_logging_enabled_ = true;
+}
+
+void PolicyToolUIHandler::ShowErrorMessageToUser(
+    const std::string& message_name) {
+  CallJavascriptFunction("policy.showErrorMessage", base::Value(message_name));
+}
