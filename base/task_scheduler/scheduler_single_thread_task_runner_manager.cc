ee5d0f9a9582bda48f19b713aab0c171325e854a
Francois Doray <fdoray@chromium.org>
2017-09-12 18:30:30 -0400

    Throttle execution of TaskPriority::BACKGROUND tasks.
    With this CL, no background task is scheduled when there are
    pending foreground tasks and it is possible to set a maximum
    number of background tasks that can be scheduled concurrently.
    
    The next step will be to merge the background and foreground
    pools on platforms that don't support thread priorities.
    Code from this CL will ensure that background tasks never
    use all available threads in a pool.
    
    Bug: 728208
    Change-Id: Ide746d3a876e78d1c9f9211a69d49f892b6e1a92

--- base/task_scheduler/scheduler_single_thread_task_runner_manager.cc
+++ base/task_scheduler/scheduler_single_thread_task_runner_manager.cc
@@ -64,6 +64,11 @@ class SchedulerWorkerDelegate : public SchedulerWorker::Delegate {
   SchedulerWorkerDelegate(const std::string& thread_name)
       : thread_name_(thread_name) {}
 
+  void set_worker(SchedulerWorker* worker) {
+    DCHECK(!worker_);
+    worker_ = worker;
+  }
+
   // SchedulerWorker::Delegate:
   void OnMainEntry(SchedulerWorker* worker) override {
     thread_ref_checker_.Set();
@@ -87,6 +92,7 @@ class SchedulerWorkerDelegate : public SchedulerWorker::Delegate {
       return;
 
     DCHECK_EQ(sequence, sequence_);
+    DCHECK(!has_work_);
     has_work_ = true;
   }
 
@@ -111,6 +117,12 @@ class SchedulerWorkerDelegate : public SchedulerWorker::Delegate {
     }
   }
 
+  void OnCanScheduleSequence(scoped_refptr<Sequence> sequence) override {
+    DCHECK(worker_);
+    ReEnqueueSequence(std::move(sequence));
+    worker_->WakeUp();
+  }
+
   // SchedulerWorkerDelegate:
 
   // Consumers should release their sequence reference as soon as possible to
@@ -122,6 +134,14 @@ class SchedulerWorkerDelegate : public SchedulerWorker::Delegate {
 
  private:
   const std::string thread_name_;
+
+  // The SchedulerWorker that has |this| as a delegate. Must be set
+  // synchronously right after creating a SchedulerWorker using |this| as
+  // delegate (to ensure it is set before the first OnCanScheduleSequence()
+  // which can occur before OnMainEntry() if PostTaskNow()'s
+  // WillScheduleSequence() is initially held up and released quickly
+  // thereafter).
+  SchedulerWorker* worker_ = nullptr;
 
   // Synchronizes access to |sequence_| and |has_work_|.
   SchedulerLock sequence_lock_;
@@ -310,8 +330,12 @@ class SchedulerSingleThreadTaskRunnerManager::SchedulerSingleThreadTaskRunner
 
     const bool sequence_was_empty = sequence->PushTask(std::move(task));
     if (sequence_was_empty) {
-      GetDelegate()->ReEnqueueSequence(std::move(sequence));
-      worker_->WakeUp();
+      sequence = outer_->task_tracker_->WillScheduleSequence(
+          std::move(sequence), GetDelegate());
+      if (sequence) {
+        GetDelegate()->ReEnqueueSequence(std::move(sequence));
+        worker_->WakeUp();
+      }
     }
   }
 
@@ -504,9 +528,13 @@ SchedulerSingleThreadTaskRunnerManager::CreateAndRegisterSchedulerWorker(
     ThreadPriority priority_hint) {
   lock_.AssertAcquired();
   int id = next_worker_id_++;
-  workers_.emplace_back(make_scoped_refptr(new SchedulerWorker(
-      priority_hint, CreateSchedulerWorkerDelegate<DelegateType>(name, id),
-      task_tracker_)));
+  std::unique_ptr<SchedulerWorkerDelegate> delegate =
+      CreateSchedulerWorkerDelegate<DelegateType>(name, id);
+  SchedulerWorkerDelegate* delegate_raw = delegate.get();
+  scoped_refptr<SchedulerWorker> worker = MakeRefCounted<SchedulerWorker>(
+      priority_hint, std::move(delegate), task_tracker_);
+  delegate_raw->set_worker(worker.get());
+  workers_.emplace_back(std::move(worker));
   return workers_.back().get();
 }
 
