663a6dd677413ebaca01a0d18bb0a2c90b937316
Shuhei Takahashi <nya@chromium.org>
2017-09-12 14:26:03 +0900

    recent: Store modifiedByMeDate in local Drive database.
    In order to trigger refresh of local Drive metadata, DB version is
    incremented from 14 to 15.
    
    New timestamps will be used in Recent root.
    
    Bug: 758677
    Test: unit_tests
    Change-Id: I51e85ce10d1777e1a0c0bb928c3da92faae376fb

--- components/drive/chromeos/file_system/copy_operation.h
+++ components/drive/chromeos/file_system/copy_operation.h
@@ -57,6 +57,8 @@ class CopyOperation {
   // with a target of |dest_file_path|.
   // If |preserve_last_modified| is set to true, this tries to preserve
   // last modified time stamp. This is supported only on Drive API v2.
+  // However last_modified_by_me timestamp will not preserved and set to
+  // the same timestamp as last_modified.
   // Invokes |callback| when finished with the result of the operation.
   // |callback| must not be null.
   void Copy(const base::FilePath& src_file_path,
