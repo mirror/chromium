c932faf260cc04435c4307cfc79d5d53b7482c83
Jenny Zhang <jennyz@chromium.org>
2017-09-12 12:32:50 -0700

    Tweak app long press animation effect to make it smooth.
    Scale up the AppListItemView when user press on it with mouse or finger, replace it with drag/drop proxy when user starts to drag it. By the end of drop, replace the proxy with enlarged AppListItemView and scale it back to original size. This change makes the transition between AppListItemView icon and proxy icon looks un-noticable to user.
    
    Bug:760339
    Change-Id: Ib8567cd37df3f64aa61b2506ed561ff7296acf79

--- ui/app_list/app_list_constants.h
+++ ui/app_list/app_list_constants.h
@@ -91,6 +91,9 @@ APP_LIST_EXPORT extern const int kTileIconSize;
 
 APP_LIST_EXPORT extern const SkColor kIconColor;
 
+APP_LIST_EXPORT extern const float kDragDropAppIconScale;
+APP_LIST_EXPORT extern const int kDragDropAppIconScaleTransitionInMs;
+
 APP_LIST_EXPORT extern const size_t kNumStartPageTiles;
 APP_LIST_EXPORT extern const size_t kNumStartPageTilesFullscreen;
 APP_LIST_EXPORT extern const size_t kMaxSearchResults;
--- ui/app_list/views/app_list_item_view.cc
+++ ui/app_list/views/app_list_item_view.cc
@@ -98,6 +98,7 @@ AppListItemView::AppListItemView(AppsGridView* apps_grid_view,
     title_->SetFontList(base_font.DeriveWithSizeDelta(1));
     title_->SetHorizontalAlignment(gfx::ALIGN_CENTER);
     title_->SetEnabledColor(kGridTitleColorFullscreen);
+    SetPaintToLayer();
   } else {
     const gfx::FontList& font_list = GetFontList();
     title_->SetFontList(font_list);
@@ -167,27 +168,73 @@ void AppListItemView::SetUIState(UIState ui_state) {
   SchedulePaint();
 }
 
+void AppListItemView::ScaleAppIcon(bool scale_up) {
+  // Set the initial state before the animation starts.
+  const gfx::Rect bounds(layer()->bounds().size());
+  gfx::Transform transform =
+      gfx::GetScaleTransform(bounds.CenterPoint(), kDragDropAppIconScale);
+  if (scale_up)
+    layer()->SetTransform(gfx::Transform());
+  else
+    layer()->SetTransform(transform);
+
+  ui::ScopedLayerAnimationSettings settings(layer()->GetAnimator());
+  settings.SetTransitionDuration(
+      base::TimeDelta::FromMilliseconds((kDragDropAppIconScaleTransitionInMs)));
+  if (scale_up)
+    layer()->SetTransform(transform);
+  else
+    layer()->SetTransform(gfx::Transform());
+
+  SchedulePaint();
+}
+
 void AppListItemView::SetTouchDragging(bool touch_dragging) {
   if (touch_dragging_ == touch_dragging)
     return;
+
   touch_dragging_ = touch_dragging;
+
+  if (touch_dragging) {
+    ScaleAppIcon(true);
+  } else {
+    apps_grid_view_->EndDrag(false);
+    ScaleAppIcon(false);
+  }
+
   SetState(STATE_NORMAL);
   SetUIState(touch_dragging_ ? UI_STATE_DRAGGING : UI_STATE_NORMAL);
 }
 
+void AppListItemView::SetMouseDragging(bool mouse_dragging) {
+  if (mouse_dragging_ == mouse_dragging)
+    return;
+
+  mouse_dragging_ = mouse_dragging;
+
+  if (mouse_dragging_) {
+    ScaleAppIcon(true);
+  } else {
+    apps_grid_view_->EndDrag(false);
+    ScaleAppIcon(false);
+    mouse_drag_proxy_created_ = false;
+  }
+
+  SetState(STATE_NORMAL);
+  SetUIState(mouse_dragging_ ? UI_STATE_DRAGGING : UI_STATE_NORMAL);
+}
+
 void AppListItemView::OnMouseDragTimer() {
-  DCHECK(apps_grid_view_->IsDraggedView(this));
-  apps_grid_view_->StartDragAndDropHostDragAfterLongPress(AppsGridView::MOUSE);
-  SetUIState(UI_STATE_DRAGGING);
+  // Show scaled up app icon to indicate draggable state.
+  SetMouseDragging(true);
 }
 
 void AppListItemView::OnTouchDragTimer(
     const gfx::Point& tap_down_location,
     const gfx::Point& tap_down_root_location) {
-  DCHECK(apps_grid_view_->IsDraggedView(this));
+  // Show scaled up app icon to indicate draggable state.
   apps_grid_view_->InitiateDrag(this, AppsGridView::TOUCH, tap_down_location,
                                 tap_down_root_location);
-  apps_grid_view_->StartDragAndDropHostDragAfterLongPress(AppsGridView::TOUCH);
   SetTouchDragging(true);
 }
 
@@ -420,24 +467,36 @@ bool AppListItemView::OnKeyPressed(const ui::KeyEvent& event) {
 
 void AppListItemView::OnMouseReleased(const ui::MouseEvent& event) {
   Button::OnMouseReleased(event);
-  apps_grid_view_->EndDrag(false);
+  SetMouseDragging(false);
 }
 
 bool AppListItemView::OnMouseDragged(const ui::MouseEvent& event) {
   Button::OnMouseDragged(event);
   if (apps_grid_view_->IsDraggedView(this)) {
-    // If the drag is no longer happening, it could be because this item
-    // got removed, in which case this item has been destroyed. So, bail out
-    // now as there will be nothing else to do anyway as
-    // apps_grid_view_->dragging() will be false.
-    if (!apps_grid_view_->UpdateDragFromItem(AppsGridView::MOUSE, event))
+    if (mouse_drag_proxy_created_) {
+      // Update the drag location of the drag proxy if it has been created.
+      // If the drag is no longer happening, it could be because this item
+      // got removed, in which case this item has been destroyed. So, bail out
+      // now as there will be nothing else to do anyway as
+      // apps_grid_view_->dragging() will be false.
+      if (!apps_grid_view_->UpdateDragFromItem(AppsGridView::MOUSE, event))
+        return true;
+    } else if (mouse_dragging_) {
+      // Create the drag proxy as soon as the mouse_drag_timer_ fires.
+      apps_grid_view_->StartDragAndDropHostDragAfterLongPress(
+          AppsGridView::MOUSE);
+      mouse_drag_proxy_created_ = true;
+    } else {
+      // Do not move the app before the |mouse_drag_timer_| fires.
+      mouse_drag_timer_.Stop();
       return true;
+    }
   }
 
   if (!apps_grid_view_->IsSelectedView(this))
     apps_grid_view_->ClearAnySelectedView();
 
-  // Shows dragging UI when it's confirmed without waiting for the timer.
+  // Show dragging UI when it's confirmed without waiting for the timer.
   if (ui_state_ != UI_STATE_DRAGGING &&
       apps_grid_view_->dragging() &&
       apps_grid_view_->IsDraggedView(this)) {
@@ -450,10 +509,13 @@ bool AppListItemView::OnMouseDragged(const ui::MouseEvent& event) {
 void AppListItemView::OnGestureEvent(ui::GestureEvent* event) {
   switch (event->type()) {
     case ui::ET_GESTURE_SCROLL_BEGIN:
-      if (touch_dragging_)
+      if (touch_dragging_) {
+        apps_grid_view_->StartDragAndDropHostDragAfterLongPress(
+            AppsGridView::TOUCH);
         event->SetHandled();
-      else
+      } else {
         touch_drag_timer_.Stop();
+      }
       break;
     case ui::ET_GESTURE_LONG_PRESS:
       event->SetHandled();
@@ -468,7 +530,6 @@ void AppListItemView::OnGestureEvent(ui::GestureEvent* event) {
     case ui::ET_SCROLL_FLING_START:
       if (touch_dragging_) {
         SetTouchDragging(false);
-        apps_grid_view_->EndDrag(false);
         event->SetHandled();
       }
       break;
@@ -495,12 +556,10 @@ void AppListItemView::OnGestureEvent(ui::GestureEvent* event) {
     case ui::ET_GESTURE_END:
       touch_drag_timer_.Stop();
       SetTouchDragging(false);
-      apps_grid_view_->EndDrag(false);
       break;
     case ui::ET_GESTURE_TWO_FINGER_TAP:
       if (touch_dragging_) {
         SetTouchDragging(false);
-        apps_grid_view_->EndDrag(false);
       } else {
         touch_drag_timer_.Stop();
       }
