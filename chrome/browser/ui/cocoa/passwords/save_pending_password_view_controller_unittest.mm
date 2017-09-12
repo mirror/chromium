315f608d00e0306656e58a3ef2039b106e77d966
Irmak Kavasoglu <irmakk@google.com>
2017-09-12 13:46:02 +0200

    Password selection works on linux platform
    This cl completes the password selection feature functionality for
    linux/chromeos/windows platforms. Now we are actually saving the
    selected password when user clicks save button.
    
    Bug:753806
    Change-Id: Ia61c2f49bf43b4c7fc456cb99ed0eb5582e89df5

--- chrome/browser/ui/cocoa/passwords/save_pending_password_view_controller_unittest.mm
+++ chrome/browser/ui/cocoa/passwords/save_pending_password_view_controller_unittest.mm
@@ -63,7 +63,8 @@ TEST_F(SavePendingPasswordViewControllerTest,
   EXPECT_CALL(
       *ui_controller(),
       SavePassword(
-          GetModelAndCreateIfNull()->pending_password().username_value));
+          GetModelAndCreateIfNull()->pending_password().username_value,
+          GetModelAndCreateIfNull()->pending_password().password_value));
   EXPECT_CALL(*ui_controller(), NeverSavePassword()).Times(0);
   [controller().saveButton performClick:nil];
 
@@ -204,7 +205,7 @@ TEST_F(SavePendingPasswordViewControllerTest,
 TEST_F(SavePendingPasswordViewControllerTest,
        ShouldNeverAndDismissWhenNeverClicked) {
   SetUpSavePendingState(false);
-  EXPECT_CALL(*ui_controller(), SavePassword(_)).Times(0);
+  EXPECT_CALL(*ui_controller(), SavePassword(_, _)).Times(0);
   EXPECT_CALL(*ui_controller(), NeverSavePassword());
   [controller().neverButton performClick:nil];
 
@@ -213,7 +214,7 @@ TEST_F(SavePendingPasswordViewControllerTest,
 
 TEST_F(SavePendingPasswordViewControllerTest, ShouldDismissWhenCrossClicked) {
   SetUpSavePendingState(false);
-  EXPECT_CALL(*ui_controller(), SavePassword(_)).Times(0);
+  EXPECT_CALL(*ui_controller(), SavePassword(_, _)).Times(0);
   EXPECT_CALL(*ui_controller(), NeverSavePassword()).Times(0);
   [controller().closeButton performClick:nil];
 
@@ -239,7 +240,7 @@ TEST_F(SavePendingPasswordViewControllerTest, CloseBubbleAndHandleClick) {
   // A user may press mouse down, some navigation closes the bubble, mouse up
   // still sends the action.
   SetUpSavePendingState(false);
-  EXPECT_CALL(*ui_controller(), SavePassword(_)).Times(0);
+  EXPECT_CALL(*ui_controller(), SavePassword(_, _)).Times(0);
   EXPECT_CALL(*ui_controller(), NeverSavePassword()).Times(0);
   [delegate() setModel:nil];
   [controller().neverButton performClick:nil];
