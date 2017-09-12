77d0cc3846df16987ae08f785042ba972ec29d71
mariannet <mariannet@google.com>
2017-09-12 15:22:27 +0900

    When deleting files in check-select-mode, the mode will now switch back to single select mode.
    Before:
    When deleting files in check-select-mode, the mode will stay in check-select,
    the selected files would be at similar indices to the deleted files.
    After:
    The mode now changes to single select mode.
    
    Bug: 742689
    Test: manual tests
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I3bf102cf2582e91085690577b08e2bed66f5d621

--- ui/file_manager/file_manager/foreground/js/ui/file_list_selection_model.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_list_selection_model.js
@@ -38,8 +38,36 @@ FileListSelectionModel.prototype.getCheckSelectMode = function() {
 };
 
 /**
- * Handles change event to update isCheckSelectMode_ BEFORE the change event is
- * dispatched to other listeners.
+ * @override
+ * Changes to single-select mode if all selected files get deleted.
+ */
+FileListSelectionModel.prototype.adjustToReordering = function(permutation) {
+  this.beginChange();
+  // Look at the old state.
+  var oldSelectedItemsCount = this.selectedIndexes.length;
+  var oldLeadIndex = this.leadIndex;
+  var newSelectedItemsCount = this.selectedIndexes
+                                  .map(function(oldIndex) {
+                                    return permutation[oldIndex];
+                                  })
+                                  .filter(function(index) {
+                                    return index != -1;
+                                  })
+                                  .length;
+  // Call the superclass function.
+  cr.ui.ListSelectionModel.prototype.adjustToReordering.call(this, permutation);
+  // Remove selection and focus if all items got deleted.
+  if (oldSelectedItemsCount && !newSelectedItemsCount && this.length_ &&
+      oldLeadIndex != -1) {
+    this.isCheckSelectMode_ = false;
+    this.selectedIndexes = [];
+  }
+  this.endChange();
+};
+
+/**
+ * Handles change event to update isCheckSelectMode_ BEFORE the change event
+ * is dispatched to other listeners.
  * @param {!Event} event Event object of 'change' event.
  * @private
  */
