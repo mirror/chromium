41a022b8d0199ac45699b6f0f9e4623596b6557f
Tomasz Garbus <tgarbus@chromium.org>
2017-09-12 15:29:11 +0200

    Add CredentialManager instance to PasswordController
    This CL adds an instance of CredentialManager to PasswordController:
     * In PasswordController's Init function, it is checked whether the
       feature is enabled, and then (and only then), CredentialManager is
       instantiated and owned by PasswordController. CredentialManager then
       registers a script command callback for "credentials" command prefix.
     * CredentialManager instance owned by PasswordController is destroyed
       in PasswordController::detach(), which is invoked when WebState is
       being destroyed. CredentialManager then removes script command
       callback.
    
    Bug: 435048
    Change-Id: Ibad9c09f8c8f0868d940bd0912a861b6120dd1c8

--- ios/chrome/browser/passwords/password_controller.mm
+++ ios/chrome/browser/passwords/password_controller.mm
@@ -33,6 +33,8 @@
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/experimental_flags.h"
 #include "ios/chrome/browser/infobars/infobar_manager_impl.h"
+#include "ios/chrome/browser/passwords/credential_manager.h"
+#include "ios/chrome/browser/passwords/credential_manager_features.h"
 #import "ios/chrome/browser/passwords/ios_chrome_save_password_infobar_delegate.h"
 #import "ios/chrome/browser/passwords/ios_chrome_update_password_infobar_delegate.h"
 #import "ios/chrome/browser/passwords/js_password_manager.h"
@@ -264,6 +266,7 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
   std::unique_ptr<PasswordGenerationManager> passwordGenerationManager_;
   std::unique_ptr<PasswordManagerClient> passwordManagerClient_;
   std::unique_ptr<PasswordManagerDriver> passwordManagerDriver_;
+  std::unique_ptr<credential_manager::CredentialManager> credentialManager_;
   PasswordGenerationAgent* passwordGenerationAgent_;
 
   __weak JsPasswordManager* passwordJsManager_;
@@ -321,6 +324,13 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
     webStateObserverBridge_.reset(
         new web::WebStateObserverBridge(webState, self));
     sent_request_to_store_ = NO;
+
+    if (base::FeatureList::IsEnabled(
+            credential_manager::features::kCredentialManager)) {
+      credentialManager_ =
+          std::make_unique<credential_manager::CredentialManager>(
+              passwordManagerClient_.get(), webState_);
+    }
   }
   return self;
 }
@@ -354,6 +364,7 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
   passwordManagerDriver_.reset();
   passwordManager_.reset();
   passwordManagerClient_.reset();
+  credentialManager_.reset();
 }
 
 #pragma mark -
