9b61e7547f9850183a5d1e0217b3188928e7837a
Olivier Robin <olivierrobin@chromium.org>
2017-09-12 16:37:14 +0200

    Do not allow saving empty sites.
    Bug: 762517
    Change-Id: I66c248f00524900e2c240acb94bc1ed4b5654d5f

--- ios/chrome/browser/ui/ntp/ntp_tile_saver.mm
+++ ios/chrome/browser/ui/ntp/ntp_tile_saver.mm
@@ -72,6 +72,8 @@ void WriteToDiskIfComplete(NSDictionary<NSURL*, NTPTile*>* tiles,
     if (!tile.faviconFetched) {
       return;
     }
+    // Any fetched tile must have a file name or a fallback monogram.
+    DCHECK(tile.faviconFileName || tile.fallbackMonogram);
   }
 
   ReplaceSavedFavicons(faviconsURL);
@@ -130,12 +132,14 @@ void SaveMostVisitedToDisk(const ntp_tiles::NTPTilesVector& mostVisitedData,
         [tmpFaviconURL URLByAppendingPathComponent:faviconFileName];
 
     void (^faviconImageBlock)(UIImage*) = ^(UIImage* favicon) {
-      tile.faviconFetched = YES;
       NSData* imageData = UIImagePNGRepresentation(favicon);
       if ([imageData writeToURL:fileURL atomically:YES]) {
+        // If the saving the image is not possible, the best thing to do is to
+        // keep the previous tile.
+        tile.faviconFetched = YES;
         tile.faviconFileName = faviconFileName;
+        WriteToDiskIfComplete(tiles, faviconsURL);
       }
-      WriteToDiskIfComplete(tiles, faviconsURL);
     };
 
     void (^fallbackBlock)(UIColor*, UIColor*, BOOL) =
@@ -146,6 +150,8 @@ void SaveMostVisitedToDisk(const ntp_tiles::NTPTilesVector& mostVisitedData,
           tile.fallbackIsDefaultColor = isDefaultColor;
           tile.fallbackMonogram = base::SysUTF16ToNSString(
               favicon::GetFallbackIconText(net::GURLWithNSURL(tile.URL)));
+          DCHECK(tile.fallbackMonogram && tile.fallbackTextColor &&
+                 tile.fallbackBackgroundColor);
           WriteToDiskIfComplete(tiles, faviconsURL);
         };
 
