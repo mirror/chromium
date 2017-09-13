ea9b01f95bd2662ce92db41b94078deb73a47fc1
Fadi Meawad <fmeawad@chromium.org>
2017-09-13 15:18:32 -0700

    [Prototype][Memory UKM] Add time since last backgrounded
    DO NOT LAND.
    
    Bug:chromium:764098
    Change-Id: I7e6c61c47d00845e8cac59fa03c350dd52e4dd93

--- services/resource_coordinator/coordination_unit/coordination_unit_introspector_impl.cc
+++ services/resource_coordinator/coordination_unit/coordination_unit_introspector_impl.cc
@@ -9,11 +9,13 @@
 #include "base/process/process_handle.h"
 #include "services/resource_coordinator/coordination_unit/coordination_unit_impl.h"
 #include "services/resource_coordinator/coordination_unit/frame_coordination_unit_impl.h"
+#include "services/resource_coordinator/coordination_unit/web_contents_coordination_unit_impl.h"
 #include "services/service_manager/public/cpp/bind_source_info.h"
 
 namespace resource_coordinator {
 
-CoordinationUnitIntrospectorImpl::CoordinationUnitIntrospectorImpl() = default;
+CoordinationUnitIntrospectorImpl::CoordinationUnitIntrospectorImpl()
+    : clock_(&default_tick_clock_) {}
 
 CoordinationUnitIntrospectorImpl::~CoordinationUnitIntrospectorImpl() = default;
 
@@ -42,6 +44,15 @@ void CoordinationUnitIntrospectorImpl::GetProcessToURLMap(
               &ukm_source_id)) {
         process_info->ukm_source_ids.push_back(ukm_source_id);
       }
+      if (web_contents_data_map_[web_contents_cu->id()].is_visible) {
+        process_info->time_since_last_visible.push_back(-1);
+      } else {
+        auto now = clock_->NowTicks();
+        auto duration =
+            now -
+            web_contents_data_map_[web_contents_cu->id()].last_invisible_time;
+        process_info->time_since_last_visible.push_back(duration.InSeconds());
+      }
     }
     process_infos.push_back(std::move(process_info));
   }
@@ -52,6 +63,36 @@ void CoordinationUnitIntrospectorImpl::BindToInterface(
     resource_coordinator::mojom::CoordinationUnitIntrospectorRequest request,
     const service_manager::BindSourceInfo& source_info) {
   bindings_.AddBinding(this, std::move(request));
+}
+
+bool CoordinationUnitIntrospectorImpl::ShouldObserve(
+    const CoordinationUnitImpl* coordination_unit) {
+  return coordination_unit->id().type == CoordinationUnitType::kWebContents;
+}
+
+void CoordinationUnitIntrospectorImpl::OnWebContentsPropertyChanged(
+    const WebContentsCoordinationUnitImpl* web_contents_cu,
+    const mojom::PropertyType property_type,
+    int64_t value) {
+  const auto web_contents_cu_id = web_contents_cu->id();
+  if (property_type == mojom::PropertyType::kVisible) {
+    if (value) {
+      // The web contents becomes visible again, clear all record in order to
+      // report metrics when web contents becomes invisible next time.
+      web_contents_data_map_[web_contents_cu_id].is_visible = true;
+      return;
+    }
+    web_contents_data_map_[web_contents_cu_id].is_visible = false;
+    web_contents_data_map_[web_contents_cu_id].last_invisible_time =
+        clock_->NowTicks();
+  }
+}
+
+void CoordinationUnitIntrospectorImpl::OnBeforeCoordinationUnitDestroyed(
+    const CoordinationUnitImpl* coordination_unit) {
+  if (coordination_unit->id().type == CoordinationUnitType::kWebContents) {
+    web_contents_data_map_.erase(coordination_unit->id());
+  }
 }
 
 }  // namespace resource_coordinator
