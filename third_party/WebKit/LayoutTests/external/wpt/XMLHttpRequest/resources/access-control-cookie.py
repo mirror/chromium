63745d3cd569e4d696eabfd7162363a4c0a2078c
Austin James Ahlstrom <aahlstrom@google.com>
2017-09-13 15:29:26 +0900

    Porting access-control-preflight-request-must-not-contain-cookie to WPT
    Bug: 745385
    Change-Id: I83212ba1afd5ba11e0bbf59e8446f3b8a0d833d9

---
+++ third_party/WebKit/LayoutTests/external/wpt/XMLHttpRequest/resources/access-control-cookie.py
@@ -0,0 +1,19 @@
+import datetime
+import time
+
+def main(request, response):
+    cookie_name = request.GET.first("cookie_name", "")
+
+    response.headers.set("Cache-Control", "no-store")
+    response.headers.set("Access-Control-Allow-Origin", request.headers.get("origin"))
+    response.headers.set("Access-Control-Allow-Credentials", "true")
+
+    for cookie in request.cookies:
+        # Set cookie to expire yesterday
+        response.set_cookie(cookie_name, "deleted", expires=
+                            datetime.datetime.now() - datetime.timedelta(days=1))
+
+    if cookie_name:
+        # Set cookie to expire tomorrow
+        response.set_cookie(cookie_name, "COOKIE", expires=
+                            datetime.datetime.now() + datetime.timedelta(days=1))
