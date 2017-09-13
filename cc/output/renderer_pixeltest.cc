0f81d9d205713647f696d59be1f0a6af2051e353
Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
2017-09-13 18:08:52 +0300

    [media]: Replace LUMINANCE_F16 by R16_EXT for 9/10-bit h264 videos.
    LUMINANCE_F16 has following issues:
    1. GL_LUMINANCE (as well as GL_ALPHA) is deprecated.
    2. GpuMemoryBuffer cannot support LUMINANCE_F16.
    3. LUMINANCE_F16 requires cpu int-to-float conversion.
    
    This CL introduces a media switch "kUseR16Texture" feature to use R16_EXT.
    
    If we get better power/performance numbers, we can enable R16_EXT
    by default.
    
    R16_EXT is :
    + intuitive
    + filterable
    + no int to half float CPU conversion
    + GpuMemoryBuffer can support R16_EXT
    
    Bug: 445071, 624436
    Change-Id: I9390d15c1e3b9bb5e1f2825d7338a1b0dca1e8c5
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel

--- cc/output/renderer_pixeltest.cc
+++ cc/output/renderer_pixeltest.cc
@@ -17,6 +17,7 @@
 #include "cc/test/fake_raster_source.h"
 #include "cc/test/fake_recording_source.h"
 #include "cc/test/pixel_test.h"
+#include "cc/test/test_in_process_context_provider.h"
 #include "components/viz/service/display/gl_renderer.h"
 #include "gpu/command_buffer/client/gles2_interface.h"
 #include "media/base/video_frame.h"
@@ -1304,9 +1305,35 @@ class VideoGLRendererPixelTest : public GLRendererPixelTest {
   std::unique_ptr<VideoResourceUpdater> video_resource_updater_;
 };
 
+enum class HighbitTexture {
+  Y8,
+  R16_EXT,
+};
+
 class VideoGLRendererPixelHiLoTest
     : public VideoGLRendererPixelTest,
-      public ::testing::WithParamInterface<bool> {};
+      public ::testing::WithParamInterface<
+          ::testing::tuple<bool, HighbitTexture>> {
+ public:
+  void SetSupportHighbitTexture(HighbitTexture texture) {
+    TestInProcessContextProvider* context_provider =
+        GetTestInProcessContextProvider();
+    switch (texture) {
+      case HighbitTexture::Y8:
+        break;
+      case HighbitTexture::R16_EXT:
+        context_provider->SetSupportTextureNorm16(true);
+        video_resource_updater_->SetUseR16ForTesting(true);
+        break;
+    }
+  }
+
+ private:
+  TestInProcessContextProvider* GetTestInProcessContextProvider() {
+    return static_cast<TestInProcessContextProvider*>(
+        output_surface_->context_provider());
+  }
+};
 
 TEST_P(VideoGLRendererPixelHiLoTest, SimpleYUVRect) {
   gfx::Rect rect(this->device_viewport_size_);
@@ -1320,7 +1347,10 @@ TEST_P(VideoGLRendererPixelHiLoTest, SimpleYUVRect) {
   SharedQuadState* shared_state =
       CreateTestSharedQuadState(gfx::Transform(), rect, pass.get());
 
-  bool highbit = GetParam();
+  const bool highbit = testing::get<0>(GetParam());
+  const HighbitTexture format = testing::get<1>(GetParam());
+  SetSupportHighbitTexture(format);
+
   CreateTestYUVVideoDrawQuad_Striped(
       shared_state, media::PIXEL_FORMAT_YV12, media::COLOR_SPACE_SD_REC601,
       false, highbit, gfx::RectF(0.0f, 0.0f, 1.0f, 1.0f), pass.get(),
@@ -1349,7 +1379,10 @@ TEST_P(VideoGLRendererPixelHiLoTest, ClippedYUVRect) {
   SharedQuadState* shared_state =
       CreateTestSharedQuadState(gfx::Transform(), viewport, pass.get());
 
-  bool highbit = GetParam();
+  const bool highbit = testing::get<0>(GetParam());
+  const HighbitTexture format = testing::get<1>(GetParam());
+  SetSupportHighbitTexture(format);
+
   CreateTestYUVVideoDrawQuad_Striped(
       shared_state, media::PIXEL_FORMAT_YV12, media::COLOR_SPACE_SD_REC601,
       false, highbit, gfx::RectF(0.0f, 0.0f, 1.0f, 1.0f), pass.get(),
@@ -1418,7 +1451,12 @@ TEST_F(VideoGLRendererPixelTest, SimpleYUVRectBlack) {
 }
 
 // First argument (test case prefix) is intentionally left empty.
-INSTANTIATE_TEST_CASE_P(, VideoGLRendererPixelHiLoTest, ::testing::Bool());
+INSTANTIATE_TEST_CASE_P(
+    ,
+    VideoGLRendererPixelHiLoTest,
+    testing::Combine(testing::Bool(),
+                     testing::Values(HighbitTexture::Y8,
+                                     HighbitTexture::R16_EXT)));
 
 TEST_F(VideoGLRendererPixelTest, SimpleYUVJRect) {
   gfx::Rect rect(this->device_viewport_size_);
