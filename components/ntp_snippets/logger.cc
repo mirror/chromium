2c6217c8ce02d4d51112e53f6dbeef4c8eeeb883
Vitalii Iarko <vitaliii@chromium.org>
2017-09-13 17:56:40 +0200

    [NTP] Add custom debug logger and scheduler/article provider logging.
    Previously it was difficult to debug user reported bugs, because we
    often did not have enough information. This CL adds a custom logger
    (accessible through snippets-internals) as well as Scheduler and
    Article Provider fetch related logging. All the logging is behind
    a feature and disabled by default.
    
    Bug:762596
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ib939565a66035da81118717a887183523c32e8ae

---
+++ components/ntp_snippets/logger.cc
@@ -0,0 +1,70 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/ntp_snippets/logger.h"
+
+#include <vector>
+
+#include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
+#include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
+#include "components/ntp_snippets/features.h"
+
+namespace ntp_snippets {
+
+namespace {
+
+// Variation parameter for maximal number of the most recent log items for the
+// logger to store.
+const char kMaxItemsCountParamName[] = "max_items_count";
+const int kMaxItemsCountDefault = 1000;
+
+int GetMaxItemsCount() {
+  return base::GetFieldTrialParamByFeatureAsInt(kContentSuggestionsDebugLog,
+                                                kMaxItemsCountParamName,
+                                                kMaxItemsCountDefault);
+}
+
+}  // namespace
+
+Logger::Logger() = default;
+Logger::~Logger() = default;
+
+void Logger::Log(const base::Location& from_here, const std::string& message) {
+  if (!base::FeatureList::IsEnabled(kContentSuggestionsDebugLog)) {
+    return;
+  }
+
+  logged_items_.emplace_back(message, base::Time::Now(), from_here);
+  if (static_cast<int>(logged_items_.size()) > GetMaxItemsCount()) {
+    logged_items_.pop_front();
+  }
+}
+
+std::string Logger::GetHumanReadableLog() const {
+  std::vector<std::string> message_strings;
+  for (const LogItem& item : logged_items_) {
+    message_strings.push_back(item.ToString());
+  }
+  return base::JoinString(message_strings, /*separator=*/"\n");
+}
+
+Logger::LogItem::LogItem(const std::string& message,
+                         const base::Time time,
+                         const base::Location& from_where)
+    : message(message), time(time), from_where(from_where) {}
+
+std::string Logger::LogItem::ToString() const {
+  base::Time::Exploded exploded;
+  time.UTCExplode(&exploded);
+  std::string time_string = base::StringPrintf(
+      "%04d-%02d-%02d %02d:%02d:%02d.%03d UTC", exploded.year, exploded.month,
+      exploded.day_of_month, exploded.hour, exploded.minute, exploded.second,
+      exploded.millisecond);
+  return base::StringPrintf("%s %s %s", time_string.c_str(),
+                            from_where.ToString().c_str(), message.c_str());
+}
+
+}  // namespace ntp_snippets
