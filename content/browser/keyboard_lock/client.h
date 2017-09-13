231d8a35324e3bd91dc45c01805979875c4855b1
Zijie He <zijiehe@chromium.org>
2017-09-13 13:44:59 -0700

    Prototype: System keyboard lock
    A workable prototype to use ui::KeyEvent instead of int + bool.
    Comparing to the previous prototype (https://codereview.chromium.org/2879033002/), this one focuses on
      - A better client / host / observer / platform_hook pattern to be able to adapt to other scenarios, e.g. Mus.
        - A client of the keyboard lock only needs to
          1) create a host and choose a platform hook
          2) define a client to receive the key events
          3) observe the focus of target
          everything else is working inside of the shared logic under //ui/aura/keyboard_lock/.
        - content/browser is the first client of the keyboard-lock component, see changes under content/browser/keyboard_lock about how to use the component in browser scenario.
      - A SpecialKeyDetector to help detect long-pressing / repeat-pressing of Escape.
      - Resolving issues of dependency.
      - Remove some unused multi-threading protection logic.
    This prototype is still sending key events to RenderWidgetHost, we can discuss the change once we start working on this part: it impacts a very small component of the entire feature.
    
    I have also spent some time to investigate the possibility of moving all the platform dependent low-level keyboard procedure logic into the platform message loop layer, i.e. moving the logic of PlatformHookWin into HWNDMessageHandler. I won't say it's impossible, but extremely complex:
      - Extra and unnecessary forwarding logic are needed to send the requests (reserved keys) to and receive key events all the way between blink and platform-dependent window
      - We also need to notify two Browser instances when moving WebContents instances between them: reserved keys of a web page should not be reset after its tab has been moved from one browser window to another.
    Though moving StateKeeper into WebContents can handle the issue, it blocks sharing the browser keyboard lock logic with Mus.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I82c99b3464070362a631affd7e406b3642b5487b

---
+++ content/browser/keyboard_lock/client.h
@@ -0,0 +1,42 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_KEYBOARD_LOCK_CLIENT_H_
+#define CONTENT_BROWSER_KEYBOARD_LOCK_CLIENT_H_
+
+#include "ui/aura/keyboard_lock/client.h"
+#include "ui/aura/keyboard_lock/long_press_detector.h"
+
+#include "base/logging.h"
+
+namespace content {
+class RenderWidgetHost;
+class WebContents;
+
+namespace keyboard_lock {
+
+class Client final
+    : public ui::aura::keyboard_lock::Client,
+      public ui::aura::keyboard_lock::LongPressDetector::Delegate {
+ public:
+  static std::unique_ptr<ui::aura::keyboard_lock::Client> Create(
+      content::WebContents* web_contents);
+
+  ~Client() override;
+
+  void OnKeyEvent(const ui::KeyEvent& event) override;
+
+ private:
+  Client(content::WebContents* web_contents);
+
+  void LongPressDetected() override;
+
+  content::WebContents* const contents_;
+  content::RenderWidgetHost* const host_;
+};
+
+}  // namespace keyboard_lock
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_KEYBOARD_LOCK_CLIENT_H_
