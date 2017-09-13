2a92202e79dc5e6ac12aefe093014edbddd5325d
spqchan <spqchan@chromium.org>
2017-09-13 18:01:30 -0400

    [Mac] Password Reuse Dialog
    Implement the Password Reuse dialog for Cocoa and
    hooked it up to ChromePasswordProtectionService.
    
    Bug:741138
    Change-Id: I77ae7bb2d026a35aad88734f226e2b262dbce9a0

---
+++ chrome/browser/ui/cocoa/password_reuse_warning_dialog_cocoa.h
@@ -0,0 +1,35 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_COCOA_PASSWORD_REUSE_WARNING_DIALOG_COCOA_H_
+#define CHROME_BROWSER_UI_COCOA_PASSWORD_REUSE_WARNING_DIALOG_COCOA_H_
+
+#import <Cocoa/Cocoa.h>
+
+#include "chrome/browser/safe_browsing/chrome_password_protection_service.h"
+#include "chrome/browser/ui/cocoa/constrained_window/constrained_window_mac.h"
+
+@class PasswordReuseWarningViewController;
+
+class PasswordReuseWarningDialogCocoa : ConstrainedWindowMacDelegate {
+ public:
+  PasswordReuseWarningDialogCocoa(content::WebContents* web_contents,
+                                  safe_browsing::OnWarningDone callback);
+
+  ~PasswordReuseWarningDialogCocoa();
+
+  void OnChangePassword();
+  void OnIgnore();
+
+ private:
+  void OnConstrainedWindowClosed(ConstrainedWindowMac* window) override;
+
+  safe_browsing::OnWarningDone callback_;
+  base::scoped_nsobject<PasswordReuseWarningViewController> controller_;
+  std::unique_ptr<ConstrainedWindowMac> window_;
+
+  DISALLOW_COPY_AND_ASSIGN(PasswordReuseWarningDialogCocoa);
+};
+
+#endif  // CHROME_BROWSER_UI_COCOA_PASSWORD_REUSE_WARNING_DIALOG_COCOA_H_
