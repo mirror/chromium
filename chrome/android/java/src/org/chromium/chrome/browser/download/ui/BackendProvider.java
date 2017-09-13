0c29b234f3f25926b3f063cf5acf7f65e96b068d
Shakti Sahu <shaktisahu@chromium.org>
2017-09-13 11:49:01 -0700

    Implement OfflineContentProvider by offline pages
    This CL makes offline pages backend implement OfflineContentProvider.
    
    Details:
    1- DownloadUIAdapter implements OfflineContentProvider in native.
    The methods for pause/resume/cancel etc are moved from
    OfflinePageDownloadBridge to here.
    2- DownloadUIAdapter creates OfflineItem instead of DownloadUIItem.
    DownloadUIItem is removed. Usages that were replaced include notification
    bridge and recent_tabs.
    3- OfflinePageDownloadBridge contains only startDownload method.
    4- DownloadHistoryAdapter implements the OfflineContentProvider::Observer
    instead of OfflinePageDownloadBridge::Observer.
    5- pause/resume/cancel/remove are routed to RequestCoordinator right away.
    
    Bug:730892
    Change-Id: I196f1838ab7dc66ae5af3860a4ea69327eab947f

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/BackendProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/BackendProvider.java
@@ -4,16 +4,9 @@
 
 package org.chromium.chrome.browser.download.ui;
 
-import android.content.ComponentName;
-import android.support.annotation.Nullable;
-
 import org.chromium.chrome.browser.download.DownloadItem;
 import org.chromium.chrome.browser.download.DownloadManagerService;
-import org.chromium.chrome.browser.offlinepages.downloads.OfflinePageDownloadBridge;
-import org.chromium.chrome.browser.offlinepages.downloads.OfflinePageDownloadItem;
 import org.chromium.chrome.browser.widget.selection.SelectionDelegate;
-
-import java.util.List;
 
 /**
  * Provides classes that need to be interacted with by the {@link DownloadHistoryAdapter}.
@@ -47,41 +40,8 @@ public interface BackendProvider {
         void updateLastAccessTime(String downloadGuid, boolean isOffTheRecord);
     }
 
-    /** Interacts with the Offline Pages backend. */
-    public static interface OfflinePageDelegate {
-        /** See {@link OfflinePageDownloadBridge#addObserver}. */
-        void addObserver(OfflinePageDownloadBridge.Observer observer);
-
-        /** See {@link OfflinePageDownloadBridge#removeObserver}. */
-        void removeObserver(OfflinePageDownloadBridge.Observer observer);
-
-        /** See {@link OfflinePageDownloadBridge#getAllItems}. */
-        List<OfflinePageDownloadItem> getAllItems();
-
-        /** See {@link OfflinePageDownloadBridge#openItem}. */
-        void openItem(String guid, @Nullable ComponentName componentName);
-
-        /** See {@link OfflinePageDownloadBridge#pauseDownload} */
-        void pauseDownload(String guid);
-
-        /** See {@link OfflinePageDownloadBridge#resumeDownload} */
-        void resumeDownload(String guid);
-
-        /** See {@link OfflinePageDownloadBridge#cancelDownload} */
-        void cancelDownload(String guid);
-
-        /** See {@link OfflinePageDownloadBridge#deleteItem}. */
-        void deleteItem(String guid);
-
-        /** See {@link OfflinePageDownloadBridge#destroy}. */
-        void destroy();
-    }
-
     /** Returns the {@link DownloadDelegate} that works with the Downloads backend. */
     DownloadDelegate getDownloadDelegate();
-
-    /** Returns the {@link OfflinePageDelegate} that works with the Offline Pages backend. */
-    OfflinePageDelegate getOfflinePageBridge();
 
     /** Returns the {@link ThumbnailProvider} that gets thumbnails for files. */
     ThumbnailProvider getThumbnailProvider();
