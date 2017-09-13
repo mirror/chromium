080a0d31e58d2407c667911775a4bbe67c65c0f6
Carlos Knippschild <carlosk@chromium.org>
2017-09-13 15:46:36 -0700

    [Offline Pages]: Add PretchStoreSchema for schema creation and upgrade.
    WIP
    
    Bug: 701939
    Change-Id: Ica8c2905bbb4f9bf76721c83e69d2b61044fa95d

---
+++ components/offline_pages/core/prefetch/store/prefetch_store_schema.cc
@@ -0,0 +1,192 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/offline_pages/core/prefetch/store/prefetch_store_schema.h"
+
+#include "base/bind.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/location.h"
+#include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
+#include "base/sequenced_task_runner.h"
+#include "base/task_runner_util.h"
+#include "base/threading/thread_task_runner_handle.h"
+#include "components/offline_pages/core/offline_store_types.h"
+#include "components/offline_pages/core/prefetch/store/prefetch_store_utils.h"
+#include "sql/connection.h"
+#include "sql/meta_table.h"
+#include "sql/statement.h"
+#include "sql/transaction.h"
+
+namespace offline_pages {
+
+// static
+const int PrefetchStoreSchema::kCurrentVersion = 2;
+
+// static
+const int PrefetchStoreSchema::kCompatibleVersion = 1;
+
+namespace {
+
+// TODO(carlosk): propose improvements to MetaTable so that getters and setters
+// of version information allow the caller to know about errors. Then remove
+// these internal values from here.
+
+// From MetaTable internals.
+const char kVersionKey[] = "version";
+const char kCompatibleVersionKey[] = "last_compatible_version";
+
+const int kVersionError = -1;
+
+// bool SetVersionNumber(sql::MetaTable* meta_table, int version) {
+//   return meta_table->SetValue(kVersionKey, version);
+// }
+
+// bool SetCompatibleVersionNumber(sql::MetaTable* meta_table, int version) {
+//   return meta_table->SetValue(kCompatibleVersionKey, version);
+// }
+
+int GetVersionNumber(sql::MetaTable* meta_table) {
+  int version;
+  if (meta_table->GetValue(kVersionKey, &version))
+    return version;
+  return kVersionError;
+}
+
+int GetCompatibleVersionNumber(sql::MetaTable* meta_table) {
+  int version;
+  if (meta_table->GetValue(kCompatibleVersionKey, &version))
+    return version;
+  return kVersionError;
+}
+
+// Name of the table with prefetch items.
+const char kItemsTableName[] = "prefetch_items";
+const char kItemsTempTableName[] = "prefetch_items_temp";
+const char kQuotaTableName[] = "prefetch_downloader_quota";
+const char kQuotaTempTableName[] = "prefetch_downloader_quota_temp";
+
+// IMPORTANT #1: when making changes to these columns please also reflect them
+// into:
+// - PrefetchItem: update existing fields and all method implementations
+//   (operator=, operator<<, ToString, etc).
+// - PrefetchItemTest, PrefetchStoreTestUtil: update test related code to cover
+//   the changed set of columns and PrefetchItem members.
+// - MockPrefetchItemGenerator: so that its generated items consider all fields.
+// IMPORTANT #2: the ordering of column types is important in SQLite 3 tables to
+// simplify data retrieval. Columns with fixed length types must come first and
+// variable length types must come later.
+static const char kItemsTableCreationSql[] =
+    "CREATE TABLE prefetch_items "
+    // Fixed length columns come first.
+    "(offline_id INTEGER PRIMARY KEY NOT NULL,"
+    " state INTEGER NOT NULL DEFAULT 0,"
+    " generate_bundle_attempts INTEGER NOT NULL DEFAULT 0,"
+    " get_operation_attempts INTEGER NOT NULL DEFAULT 0,"
+    " download_initiation_attempts INTEGER NOT NULL DEFAULT 0,"
+    " archive_body_length INTEGER_NOT_NULL DEFAULT -1,"
+    " creation_time INTEGER NOT NULL,"
+    " freshness_time INTEGER NOT NULL,"
+    " error_code INTEGER NOT NULL DEFAULT 0,"
+    " file_size INTEGER NOT NULL DEFAULT -1,"
+    // Variable length columns come later.
+    " guid VARCHAR NOT NULL DEFAULT '',"
+    " client_namespace VARCHAR NOT NULL DEFAULT '',"
+    " client_id VARCHAR NOT NULL DEFAULT '',"
+    " requested_url VARCHAR NOT NULL DEFAULT '',"
+    " final_archived_url VARCHAR NOT NULL DEFAULT '',"
+    " operation_name VARCHAR NOT NULL DEFAULT '',"
+    " archive_body_name VARCHAR NOT NULL DEFAULT '',"
+    " title VARCHAR NOT NULL DEFAULT '',"
+    " file_path VARCHAR NOT NULL DEFAULT ''"
+    ")";
+
+static const char kQuotaTableCreationSql[] =
+    "CREATE TABLE prefetch_downloader_quota "
+    "(quota_id INTEGER PRIMARY KEY NOT NULL DEFAULT 1,"
+    " update_time INTEGER NOT NULL,"
+    " available_quota INTEGER NOT NULL DEFAULT 0)";
+
+bool CreatePrefetchItemsTable(sql::Connection* db, bool temporary) {
+  const char* table_name = temporary ? kItemsTempTableName : kItemsTableName;
+  if (db->DoesTableExist(table_name))
+    return true;
+  // TODO temp table support
+  return db->Execute(kItemsTableCreationSql);
+}
+
+bool CreatePrefetchQuotaTable(sql::Connection* db, bool temporary) {
+  const char* table_name = temporary ? kQuotaTempTableName : kQuotaTableName;
+  if (db->DoesTableExist(table_name))
+    return true;
+  // TODO temp table support
+  return db->Execute(kQuotaTableCreationSql);
+}
+
+bool CreateLatestSchema(sql::Connection* db) {
+  sql::Transaction transaction(db);
+  if (!transaction.Begin())
+    return false;
+
+  if (!CreatePrefetchItemsTable(db, false) ||
+      !CreatePrefetchQuotaTable(db, false)) {
+    return false;
+  }
+
+  // This would be a great place to add indices when we need them.
+  return transaction.Commit();
+}
+
+}  // namespace
+
+PrefetchStoreSchema::PrefetchStoreSchema(sql::Connection* db) : db_(db){};
+
+PrefetchStoreSchema::~PrefetchStoreSchema() = default;
+
+bool PrefetchStoreSchema::CreateOrUpgradeIfNeeded() {
+  DCHECK_GE(kCurrentVersion, kCompatibleVersion);
+  sql::MetaTable meta_table;
+  if (!meta_table.Init(db_, kCurrentVersion, kCompatibleVersion) ||
+      !CreateLatestSchema(db_)) {
+    return false;
+  }
+
+  const int compatible_version = GetCompatibleVersionNumber(&meta_table);
+  const int current_version = GetVersionNumber(&meta_table);
+  if (current_version == kVersionError || compatible_version == kVersionError)
+    return false;
+  DCHECK_GE(current_version, compatible_version);
+
+  // Stored database version is newer and incompatible with what the current
+  // running code supports (Chrome was downgraded).
+  if (compatible_version > kCurrentVersion)
+    return false;
+
+  // Database is already at the latest version.
+  if (current_version == kCurrentVersion)
+    return true;
+
+  // Schema upgrade code starts here. Be very careful when authoring this code
+  // as it must robust to any sort of inconsistent schema state.
+  // Note: A series of if-else blocks was chosen to allow for more flexibility
+  // in the upgrade logic than a single switch-case block would.
+
+  if (current_version <= 1) {
+    // Raze and recreate the latest schema version and return immediately.
+    meta_table.Reset();
+    return db_->Raze() && CreateLatestSchema(db_) &&
+           meta_table.Init(db_, kCurrentVersion, kCompatibleVersion);
+  }
+
+  // Returns false when an error happens or if |from_version| is unknown.
+  return true;
+}
+
+// static
+const char* PrefetchStoreSchema::GetItemTableCreationSqlForTesting() {
+  return kItemsTableCreationSql;
+}
+
+}  // namespace offline_pages
