a1841139f63d446d361a813bee02f65ae3a0bfb8
Matt Wolenetz <wolenetz@chromium.org>
2017-09-13 10:54:23 -0700

    SmokeTests: Allow comments and require Tools OWNERS to review changes
    To help prevent accidental SmokeTests coverage regressions like that
    fixed in bug 763528, this change allows '#' prefixed comments in
    SmokeTests, and also makes changes to SmokeTests require Tools OWNERS
    review.
    
    BUG=764556,763528
    
    Change-Id: I612fb2b67fbe9f0a99107c1079b79a4b61338944

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
@@ -921,6 +921,16 @@ class Port(object):
         """
         return self.skipped_due_to_smoke_tests(test) or self.skipped_in_never_fix_tests(test)
 
+    def _tests_from_file_contents(self, file_contents):
+        tests = []
+        for line in file_contents.split('\n'):
+            line = line.strip()
+            line = line.rstrip('/')  # Best to normalize directory names to not include the trailing slash.
+            if line.startswith('#') or not len(line):
+                continue
+            tests.append(line)
+        return tests
+
     def skipped_due_to_smoke_tests(self, test):
         """Checks if the test is skipped based on the set of Smoke tests.
 
@@ -930,8 +940,11 @@ class Port(object):
         if not self.default_smoke_test_only():
             return False
         smoke_test_filename = self.path_to_smoke_tests_file()
-        return (self._filesystem.exists(smoke_test_filename) and
-                test not in self._filesystem.read_text_file(smoke_test_filename))
+        if not self._filesystem.exists(smoke_test_filename):
+            return False
+        smoke_tests_file_contents = self._filesystem.read_text_file(smoke_test_filename)
+        smoke_tests = self._tests_from_file_contents(smoke_tests_file_contents)
+        return test not in smoke_tests
 
     def path_to_smoke_tests_file(self):
         return self._filesystem.join(self.layout_tests_dir(), 'SmokeTests')
@@ -960,16 +973,6 @@ class Port(object):
     def path_to_never_fix_tests_file(self):
         return self._filesystem.join(self.layout_tests_dir(), 'NeverFixTests')
 
-    def _tests_from_skipped_file_contents(self, skipped_file_contents):
-        tests_to_skip = []
-        for line in skipped_file_contents.split('\n'):
-            line = line.strip()
-            line = line.rstrip('/')  # Best to normalize directory names to not include the trailing slash.
-            if line.startswith('#') or not len(line):
-                continue
-            tests_to_skip.append(line)
-        return tests_to_skip
-
     def _expectations_from_skipped_files(self, skipped_file_paths):
         # TODO(qyearsley): Remove this if there are no more "Skipped" files.
         tests_to_skip = []
@@ -980,7 +983,7 @@ class Port(object):
                 continue
             _log.debug('Using Skipped file: %s', filename)
             skipped_file_contents = self._filesystem.read_text_file(filename)
-            tests_to_skip.extend(self._tests_from_skipped_file_contents(skipped_file_contents))
+            tests_to_skip.extend(self._tests_from_file_contents(skipped_file_contents))
         return tests_to_skip
 
     @memoized
