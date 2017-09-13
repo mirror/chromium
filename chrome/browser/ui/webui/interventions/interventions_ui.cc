406276097bd72e1356d27a9a3403b18d89c1f14f
Thanh Le <thanhdle@chromium.org>
2017-09-13 09:08:58 -0700

    Adding an empty chrome://interventions-internals page
    This page will add new functionality for debugging interventions in chrome, but for now it shows "Interventions -- Comming soon!".
    Bug: 764408
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I2761a0d1fcf653b2ead0f972e9e85900604f8527

---
+++ chrome/browser/ui/webui/interventions/interventions_ui.cc
@@ -0,0 +1,35 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/webui/interventions/interventions_ui.h"
+
+#include <string>
+
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/common/url_constants.h"
+#include "chrome/grit/browser_resources.h"
+#include "content/public/browser/web_ui_data_source.h"
+
+namespace {
+
+content::WebUIDataSource* CreateInterventionsHTMLSource() {
+  content::WebUIDataSource* source =
+      content::WebUIDataSource::Create(chrome::kChromeUIInterventionsHost);
+
+  source->SetDefaultResource(IDR_INTERVENTIONS_INDEX_HTML);
+  source->UseGzip(std::unordered_set<std::string>());
+  return source;
+}
+
+}  // namespace
+
+InterventionsUI::InterventionsUI(content::WebUI* web_ui)
+    : WebUIController(web_ui) {
+  // Set up the chrome://interventions/ source.
+  Profile* profile = Profile::FromWebUI(web_ui);
+  content::WebUIDataSource::Add(profile, CreateInterventionsHTMLSource());
+
+  // TODO(thanhdle): Add a previews message handler.
+  // crbug.com/764409
+}
