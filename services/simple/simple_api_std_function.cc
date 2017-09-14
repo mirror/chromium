24eb8defdcdcd599b08f234990a09058577fafe0
David 'Digit' Turner <digit@chromium.org>
2017-09-14 15:54:24 +0200

    Experimental Mojo program and benchmark.
    DO NOT SUBMIT - This is an experiment.
    
    For more details (and preliminary results, see README.chromium).
    
    BUG=None
    
    Change-Id: I360821be5df63de782252f9efb489750b7d408c1

---
+++ services/simple/simple_api_std_function.cc
@@ -0,0 +1,48 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <functional>
+#include "simple_api.h"
+
+namespace {
+
+// Implementation of the simple:: classes using a std::function<> instance.
+// This shall have similar performance to the base::Callback version but
+// does not depend on //base.
+
+class MyMath : public simple::Math {
+ public:
+  MyMath() : callback_(&DoAdd) {}
+
+  int32_t Add(int32_t x, int32_t y) override { return callback_(x, y); }
+
+  static int32_t DoAdd(int32_t x, int32_t y) { return x + y; }
+
+ private:
+  std::function<int32_t(int32_t, int32_t)> callback_;
+};
+
+class MyEcho : public simple::Echo {
+ public:
+  MyEcho() : callback_(&DoPing) {}
+
+  std::string Ping(const std::string& str) override { return callback_(str); }
+
+  static std::string DoPing(const std::string& str) { return str; }
+
+ private:
+  std::function<std::string(const std::string&)> callback_;
+};
+
+}  // namespace
+
+simple::Math* createMath() {
+  return new MyMath();
+}
+simple::Echo* createEcho() {
+  return new MyEcho();
+}
+const char* createAbstract() {
+  return "Invoke same std::function<> instance on each method call.";
+}
