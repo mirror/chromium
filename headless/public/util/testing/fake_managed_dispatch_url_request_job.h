b6266afd6b235bcec0a3727f6b3f14a859791229
Eric Seckler <eseckler@chromium.org>
2017-09-14 13:00:07 +0100

    headless: Add VirtualTimeController.
    The VirtualTimeController allows interleaving of repeating tasks with
    the progression of virtual time.
    
    Bug: none
    Change-Id: Ibd81b75e58a83887c22e4555202913f8dfbfc81b

--- headless/public/util/testing/fake_managed_dispatch_url_request_job.h
+++ headless/public/util/testing/fake_managed_dispatch_url_request_job.h
@@ -10,6 +10,7 @@
 
 #include "base/macros.h"
 #include "base/strings/stringprintf.h"
+#include "headless/public/headless_export.h"
 #include "headless/public/util/managed_dispatch_url_request_job.h"
 #include "net/base/net_errors.h"
 
