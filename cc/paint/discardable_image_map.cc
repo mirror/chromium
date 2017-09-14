3afc16caca7c7eb8b7f0360fb1552c8e3d14c415
Khushal <khushalsagar@chromium.org>
2017-09-14 13:19:53 -0700

    images: Move animation of images to cc.
    Currently animating an image requires an invalidation and repaint, to
    record the next frame of animation in an updated DisplayItemList from
    blink to cc. This change eliminates repaints for these animations by
    ticking them in the compositor and selecting the frame index at playback
    time during raster, instead of the value stored in the recording. Doing
    this requires the following:
    
    1) An ImageAnimationController class is added to track the frame index
    for an image used on the active/pending tree and to schedule an
    invalidation when the animation needs to be advanced.
    
    2) The frame_index to use for an image is added to DrawImage for the
    image decode caches. When creating a raster task for a tile, a
    map of image_id_to_frame_index_ to use for an image is stored in the
    PlaybackImageProvider, based on the tree of this tile.
    
    3) The animation state for an image is persisted in the controller across
    commits. This is necessary to ensure that the animation is resumed from
    the last frame displayed, even if the image is no longer being painted.
    Subsequent changes will remove this tracking on navigations, or update
    it if explicitly reset on the main thread.
    
    The following improvements in smoothness and thread_times result for
    https://giphy.com/search/60-fps on Android Nexus5 from this change:
    
    BEFORE
    thread_total_all_cpu_time_per_frame: 64.023 ms
    thread_total_fast_path_cpu_time_per_frame: 24.411 ms
    thread_renderer_main_cpu_time_per_frame: 9.866 ms
    thread_renderer_compositor_cpu_time_per_frame: 5.513 ms
    thread_raster_cpu_time_per_frame: 29.376 ms
    
    avg_surface_fps: 28.250
    mean_frame_time: 35.523 ms
    percentage_smooth: 41.495
    jank_count: 95
    max_frame_delay: 9.250
    
    AFTER
    thread_total_all_cpu_time_per_frame: 42.568 ms
    thread_total_fast_path_cpu_time_per_frame: 21.003 ms
    thread_renderer_main_cpu_time_per_frame: 1.614 ms
    thread_renderer_compositor_cpu_time_per_frame: 4.826 ms
    thread_raster_cpu_time_per_frame: 19.695 ms
    
    avg_surface_fps: 36.5
    mean_frame_time: 27.302 ms
    percentage_smooth: 60.045
    jank_count: 73
    max_frame_delay: 7.250
    
    R= chrishtr@chromium.org, enne@chromium.org, vmpstr@chromium.org
    Bug:735741
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I4dc018d4fc2aa82962d05f24d88ae0e73b39f7ca

--- cc/paint/discardable_image_map.cc
+++ cc/paint/discardable_image_map.cc
@@ -229,9 +229,10 @@ class DiscardableImageGenerator {
           paint_image.GetFrameMetadata(), paint_image.repetition_count());
     }
 
-    image_set_.emplace_back(
-        DrawImage(std::move(paint_image), src_irect, filter_quality, matrix),
-        image_rect);
+    size_t frame_index = paint_image.frame_index();
+    image_set_.emplace_back(DrawImage(std::move(paint_image), src_irect,
+                                      filter_quality, matrix, frame_index),
+                            image_rect);
   }
 
   // This canvas is used only for tracking transform/clip/filter state from the
