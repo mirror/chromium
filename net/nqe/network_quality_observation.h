edfcc3c6a2bfff9061518b797bac736dcaa18a25
Tarun Bansal <tbansal@chromium.org>
2017-09-14 10:31:13 -0700

    Refactor Observation struct as a class
    No functional change. This CL changes Onbservation struct
    to a class.
    
    Bug:
    Change-Id: I841051c9c163c6b57759859b8ddf059056808570

--- net/nqe/network_quality_observation.h
+++ net/nqe/network_quality_observation.h
@@ -8,6 +8,7 @@
 #include <stdint.h>
 
 #include "base/optional.h"
+#include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "net/base/net_export.h"
 #include "net/nqe/network_quality_estimator_util.h"
@@ -23,7 +24,8 @@ namespace internal {
 // or throughput), along with the time the observation was made. Observations
 // can be made at several places in the network stack, thus the observation
 // source is provided as well.
-struct NET_EXPORT_PRIVATE Observation {
+class NET_EXPORT_PRIVATE Observation {
+ public:
   Observation(int32_t value,
               base::TimeTicks timestamp,
               const base::Optional<int32_t>& signal_strength,
@@ -36,23 +38,52 @@ struct NET_EXPORT_PRIVATE Observation {
               const base::Optional<IPHash>& host);
 
   Observation(const Observation& other);
+  Observation& operator=(const Observation& other);
 
   ~Observation();
 
   // Value of the observation.
-  const int32_t value;
+  int32_t value() const {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+    return value_;
+  }
 
   // Time when the observation was taken.
-  const base::TimeTicks timestamp;
+  base::TimeTicks timestamp() const {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+    return timestamp_;
+  }
 
   // Signal strength when the observation was taken.
-  const base::Optional<int32_t> signal_strength;
+  base::Optional<int32_t> signal_strength() const {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+    return signal_strength_;
+  }
 
   // The source of the observation.
-  const NetworkQualityObservationSource source;
+  NetworkQualityObservationSource source() const {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+    return source_;
+  }
 
   // A unique identifier for the remote host which was used for the measurement.
-  const base::Optional<IPHash> host;
+  base::Optional<IPHash> host() const {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+    return host_;
+  }
+
+ private:
+  int32_t value_;
+
+  base::TimeTicks timestamp_;
+
+  base::Optional<int32_t> signal_strength_;
+
+  NetworkQualityObservationSource source_;
+
+  base::Optional<IPHash> host_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
 };
 
 }  // namespace internal
