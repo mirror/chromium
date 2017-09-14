6959e76c290d8846812b8b0c715c038f83149e04
Chan <chanli@chromium.org>
2017-09-14 14:30:10 -0700

    Add new submit rule for Findit auto revert.
    Added a customized submit rule for Findit auto-revert:
    If Findit created and tries to commit the change and the change is a pure revert of another one, the change is submittable.

---
+++ rules.pl
@@ -0,0 +1,34 @@
+% Reuse default logic.
+submit_rule(X) :-
+    gerrit:default_submit(X),
+    !.
+
+% Special submit rule on Findit auto-reverts: A change is submittable if
+% 1. If Findit created the change,
+% 2. If Findit wants to commit the change,
+% 3. If the change is a pure revert of another change.
+submit_rule(submit(A, R)) :-
+    created_and_committed_by_findit(A),
+    commit_is_pure_revert(R).
+
+% Findit is CL author and now it tries to commit the CL.
+created_and_committed_by_findit(A) :-
+    gerrit:commit_author(Id, 'Findit', 'findit-for-me@appspot.gserviceaccount.com'),
+    gerrit:current_user(Id),
+    !,
+    A = label('Authored-and-Committed-by-Findit', ok(Id)).
+
+created_and_committed_by_findit(A) :-
+    A = label('Authored-and-Committed-by-Findit', need(_)).
+
+% This commit is pure revert of another commit.
+commit_is_pure_revert(R) :-
+    gerrit:pure_revert(1),
+    !,
+    gerrit:commit_author(A),
+    R = label('Is-Pure-Revert', ok(A)).
+
+commit_is_pure_revert(R) :-
+    gerrit:pure_revert(U),
+    U \= 1,
+    R = label('Is-Pure-Revert', need(_)).
