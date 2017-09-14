69ef68b030d79fa579a82dab63da96752b3e2378
Siddhartha <ssid@chromium.org>
2017-09-14 15:44:05 -0700

    Local storage: Add area cache on renderer side
    On each navigation the local storage database is cleared and created
    again with a mojo message to browser to load the whole database from
    disk. To avoid that, keep a cache of the database in renderer.
    
    BUG=743187
    
    Change-Id: Ic91144aebd5e1948bbbb1a50b58025a4dd7592c7

--- content/renderer/dom_storage/local_storage_cached_areas.cc
+++ content/renderer/dom_storage/local_storage_cached_areas.cc
@@ -4,31 +4,58 @@
 
 #include "content/renderer/dom_storage/local_storage_cached_areas.h"
 
+#include "base/sys_info.h"
 #include "content/renderer/dom_storage/local_storage_cached_area.h"
 
 namespace content {
+namespace {
+const size_t kTotalCacheLimitInBytesLowEnd = 1 * 1024 * 1024;
+const size_t kTotalCacheLimitInBytes = 5 * 1024 * 1024;
+}  // namespace
 
 LocalStorageCachedAreas::LocalStorageCachedAreas(
     mojom::StoragePartitionService* storage_partition_service)
-    : storage_partition_service_(storage_partition_service) {}
+    : storage_partition_service_(storage_partition_service),
+      total_cache_limit_(base::SysInfo::AmountOfPhysicalMemoryMB() < 1024
+                             ? kTotalCacheLimitInBytesLowEnd
+                             : kTotalCacheLimitInBytes) {}
 
-LocalStorageCachedAreas::~LocalStorageCachedAreas() {
-}
+LocalStorageCachedAreas::~LocalStorageCachedAreas() {}
 
 scoped_refptr<LocalStorageCachedArea> LocalStorageCachedAreas::GetCachedArea(
     const url::Origin& origin) {
-  if (cached_areas_.find(origin) == cached_areas_.end()) {
-    cached_areas_[origin] = new LocalStorageCachedArea(
-        origin, storage_partition_service_, this);
+  auto it = cached_areas_.find(origin);
+  if (it == cached_areas_.end()) {
+    ClearAreasIfNeeded();
+    it = cached_areas_
+             .emplace(origin, new LocalStorageCachedArea(
+                                  origin, storage_partition_service_, this))
+             .first;
   }
-
-  return make_scoped_refptr(cached_areas_[origin]);
+  return it->second;
 }
 
-void LocalStorageCachedAreas::CacheAreaClosed(
-    LocalStorageCachedArea* cached_area) {
-  DCHECK(cached_areas_.find(cached_area->origin()) != cached_areas_.end());
-  cached_areas_.erase(cached_area->origin());
+size_t LocalStorageCachedAreas::TotalCacheSize() const {
+  size_t total = 0;
+  for (const auto& it : cached_areas_)
+    total += it.second.get()->area_memory_used();
+  return total;
+}
+
+void LocalStorageCachedAreas::ClearAreasIfNeeded() {
+  size_t total_cache_size = TotalCacheSize();
+  while (total_cache_size > total_cache_limit_) {
+    auto it = cached_areas_.begin();
+    for (; it != cached_areas_.end(); ++it) {
+      if (it->second->HasOneRef()) {
+        total_cache_size -= it->second->area_memory_used();
+        cached_areas_.erase(it);
+        break;
+      }
+    }
+    if (it == cached_areas_.end())
+      return;
+  }
 }
 
 }  // namespace content
