0df4909bd82b4157fc3954d56a10386281e8169c
Sergey Shekyan <shekyan@gmail.com>
2017-09-15 10:46:27 -0700

    Introduce NavigatorAutomationInformation interface
    Introduce NavigatorAutomationInformation interface
    that indicates if navigator is controlled by automation.
    It is guarded by RuntimeEnabled and exists only if AutomationConrolled feature is set.
    https://w3c.github.io/webdriver/webdriver-spec.html#dom-navigatorautomationinformation
    
    Bug: 723900
    Change-Id: I7a93767ab8673d8ba1dda26fa44bac71ea15ac3d

--- content/public/common/content_switches.h
+++ content/public/common/content_switches.h
@@ -108,6 +108,7 @@ CONTENT_EXPORT extern const char kDomAutomationController[];
 extern const char kDisable2dCanvasClipAntialiasing[];
 CONTENT_EXPORT extern const char kDumpBlinkRuntimeCallStats[];
 CONTENT_EXPORT extern const char kEnableAggressiveDOMStorageFlushing[];
+CONTENT_EXPORT extern const char kEnableAutomation[];
 CONTENT_EXPORT extern const char kEnablePreferCompositingToLCDText[];
 CONTENT_EXPORT extern const char kEnableBlinkFeatures[];
 CONTENT_EXPORT extern const char kEnableBackgroundFetchPersistence[];
