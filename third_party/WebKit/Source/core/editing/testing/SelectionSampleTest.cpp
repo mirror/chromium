df36dccb22c268fe77d5caa65fcebb785e98dd1c
Akari Asai <akariasai@google.com>
2017-09-15 16:14:55 +0900

    Add ConvertTemplatesToShadowRoots() to SelectionSample.cpp.
    Bug:
    Change-Id: If24c4d0edd4bf4f622b5a2a9bf568a25f63a5374

--- third_party/WebKit/Source/core/editing/testing/SelectionSampleTest.cpp
+++ third_party/WebKit/Source/core/editing/testing/SelectionSampleTest.cpp
@@ -207,4 +207,25 @@ TEST_F(SelectionSampleTest, SerializeVoidElementBR) {
       << "When BR has child nodes, it is not void element.";
 }
 
+TEST_F(SelectionSampleTest, ConvertTemplatesToShadowRoots) {
+  SetBodyContent(
+      "<div id='d1'>^foo|"
+      "<template data-mode='open'>"
+      "<div>d1-1</div>"
+      "<slot name='d1-s1'></slot>"
+      "<div>d1-2</div>"
+      "</template>"
+      "<div slot='d1-s1'>d2</div>"
+      "</div>");
+  Element* body = GetDocument().body();
+  Element* d1 = body->getElementById("d1");
+  HTMLElement* converted =
+      SelectionSample::ConvertTemplatesToShadowRootsForTesring(
+          ToHTMLElement(d1));
+  ShadowRoot* shadow_root = converted->ShadowRootIfV1();
+  EXPECT_EQ(true, shadow_root->IsShadowRoot());
+  EXPECT_EQ("<div>d1-1</div><slot name=\"d1-s1\"></slot><div>d1-2</div>",
+            shadow_root->innerHTML());
+}
+
 }  // namespace blink
