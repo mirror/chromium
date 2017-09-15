b1bd4e511f8f21e4e9634cb041efcefcc33d133c
Maks Orlovich <morlovich@chromium.org>
2017-09-15 09:46:55 -0400

    HttpServer: adjust QueuedWriteIOBuffer to new queue
    In particular, don't assume pointer stability.
    
    This is a first cut for fix, something more ellegant
    may be possible in a less local manner.
    
    Bug: 764320
    Change-Id: Ie260aa1cef4a42869ae2ab46601d9e1143f1d5d8

--- net/server/http_connection_unittest.cc
+++ net/server/http_connection_unittest.cc
@@ -327,5 +327,30 @@ TEST(HttpConnectionTest, QueuedWriteIOBuffer_TotalSizeLimit) {
   EXPECT_EQ(kDataLength * 4 - kConsumedLength, buffer->total_size());
 }
 
+TEST(HttpConnectionTest, QueuedWriteIOBuffer_QueueMoveData) {
+  // This is a regression test that makes sure that QueuedWriteIOBuffer deals
+  // with base::queue's semantics differences vs. std::queue right.
+  scoped_refptr<HttpConnection::QueuedWriteIOBuffer> buffer(
+      new HttpConnection::QueuedWriteIOBuffer());
+
+  // We append a short string to make it fit within any short string
+  // optimization, so that if the underlying queue moves the std::string,
+  // the data should change.
+  buffer->Append("abcdefgh");
+
+  // Read part of it, to make sure this handles the case of data() pointing
+  // to something other than start of string right.
+  buffer->DidConsume(3);
+  EXPECT_EQ("defgh", base::StringPiece(buffer->data(), 5));
+
+  // Now append a whole bunch of other things to make the underlying queue
+  // grow, and likely need to move stuff around in memory.
+  for (int i = 0; i < 256; ++i)
+    buffer->Append("some other string data");
+
+  // data() should still be right (but possibly different).
+  EXPECT_EQ("defgh", base::StringPiece(buffer->data(), 5));
+}
+
 }  // namespace
 }  // namespace net
