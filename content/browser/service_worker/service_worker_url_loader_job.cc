f037ded1e1e8117496f336366d481ad64270d8b4
Tsuyoshi Horo <horo@chromium.org>
2017-09-15 20:35:51 +0900

    Support redirect for main resource navigation with S13nServiceWorker.
    I locally tested these redirect patterns in WPT's navigation-redirect.https.html
    with --enable-features=NetworkService flag, after registering the SW without the
    flag.
    
      - Normal redirect to same-origin scope.
      - Normal redirect to other-origin scope.
      - SW-fallbacked redirect to same-origin out-scope.
      - SW-fallbacked redirect to same-origin same-scope.
      - SW-fallbacked redirect to same-origin other-scope.
      - SW-fallbacked redirect to other-origin out-scope.
      - SW-fallbacked redirect to other-origin in-scope.
      - SW-generated redirect to same-origin out-scope.
      - SW-generated redirect to same-origin same-scope.
      - SW-generated redirect to same-origin other-scope.
      - SW-generated redirect to other-origin out-scope.
      - SW-generated redirect to other-origin in-scope.
    
    But other patters in navigation-redirect.https.html failed. It is because there
    is another issue in handling manual redirect mode requests in NetworkService.
    
    BUG=764224
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I1f4b602e6dd4887aabac770fa2b9d4a6dfb3aeb8

--- content/browser/service_worker/service_worker_url_loader_job.cc
+++ content/browser/service_worker/service_worker_url_loader_job.cc
@@ -18,6 +18,48 @@
 
 namespace content {
 
+namespace {
+
+// TODO(horo): We should have shared logic with
+// net::URLRequestJob::ComputeRedirectInfo()
+std::unique_ptr<net::RedirectInfo> ComputeRedirectInfo(
+    const ResourceRequest& original_request,
+    const ResourceResponseHead& response_head) {
+  std::string new_location;
+  if (!response_head.headers->IsRedirect(&new_location))
+    return std::unique_ptr<net::RedirectInfo>();
+
+  int status = response_head.headers->response_code();
+  const std::string& method = original_request.method;
+  const GURL& url = original_request.url;
+  const GURL location = url.Resolve(new_location);
+
+  std::unique_ptr<net::RedirectInfo> redirect_info =
+      base::MakeUnique<net::RedirectInfo>();
+  redirect_info->status_code = status;
+  redirect_info->new_method =
+      ((status == 303 && method != "HEAD") ||
+       ((status == 301 || status == 302) && method == "POST"))
+          ? "GET"
+          : method;
+  if (url.is_valid() && url.has_ref() && !location.has_ref()) {
+    GURL::Replacements replacements;
+    replacements.SetRef(url.spec().data(),
+                        url.parsed_for_possibly_invalid_spec().ref);
+    redirect_info->new_url = location.ReplaceComponents(replacements);
+  } else {
+    redirect_info->new_url = location;
+  }
+  // TODO(horo): Check the first part URL policy.
+  redirect_info->new_site_for_cookies = redirect_info->new_url;
+  // TODO(horo): Set redirect_info->new_referrer_policy by checking
+  // Referrer-Policy header value.
+  // TODO(horo): Set redirect_info->referred_token_binding_host.
+  return redirect_info;
+}
+
+}  // namespace
+
 // This class waits for completion of a stream response from the service worker.
 // It calls ServiceWorkerURLLoader::CommitComplete() upon completion of the
 // response.
@@ -68,7 +110,11 @@ ServiceWorkerURLLoaderJob::ServiceWorkerURLLoaderJob(
       blob_storage_context_(blob_storage_context),
       blob_client_binding_(this),
       binding_(this),
-      weak_factory_(this) {}
+      weak_factory_(this) {
+  // TODO(horo): Is there the right place to set these time?
+  response_head_.load_timing.request_start = base::TimeTicks::Now();
+  response_head_.load_timing.request_start_time = base::Time::Now();
+}
 
 ServiceWorkerURLLoaderJob::~ServiceWorkerURLLoaderJob() {}
 
@@ -153,6 +199,9 @@ void ServiceWorkerURLLoaderJob::StartRequest() {
           resource_request_, url_loader_factory_getter_.get(),
           base::BindOnce(&base::DoNothing /* TODO(crbug/762357): metrics? */));
   response_head_.service_worker_start_time = base::TimeTicks::Now();
+  // TODO(horo): Is there the right place to set these time?
+  response_head_.load_timing.send_start = base::TimeTicks::Now();
+  response_head_.load_timing.send_end = base::TimeTicks::Now();
   fetch_dispatcher_->Run();
 }
 
@@ -183,6 +232,21 @@ void ServiceWorkerURLLoaderJob::DeliverErrorResponse() {
     CommitResponseHeaders();
   }
   CommitCompleted(net::ERR_FAILED);
+}
+
+bool ServiceWorkerURLLoaderJob::MayBeCommitRedirect() {
+  DCHECK_GT(status_, Status::kNotStarted);
+  DCHECK_LT(status_, Status::kCompleted);
+
+  std::unique_ptr<net::RedirectInfo> redirect_info =
+      ComputeRedirectInfo(resource_request_, response_head_);
+  if (!redirect_info)
+    return false;
+
+  response_head_.load_timing.receive_headers_end = base::TimeTicks::Now();
+  response_head_.encoded_data_length = 0;
+  url_loader_client_->OnReceiveRedirect(*redirect_info, response_head_);
+  return true;
 }
 
 void ServiceWorkerURLLoaderJob::DidPrepareFetchEvent(
@@ -262,6 +326,9 @@ void ServiceWorkerURLLoaderJob::StartResponse(
 
   response_head_.did_service_worker_navigation_preload =
       did_navigation_preload_;
+
+  if (MayBeCommitRedirect())
+    return;
 
   // Handle a stream response body.
   if (!body_as_stream.is_null() && body_as_stream->stream.is_valid()) {
