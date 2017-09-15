79c0e079492f548ee7b9acb6009968d32caf9f00
Noel Gordon <noel@chromium.org>
2017-09-16 00:44:50 +1000

    zlib adler_smid.c
    Add SSSE3 implementation of the adler32 checksum, suitable for
    both large workloads, and small workloads commonly seen during
    PNG image decoding. Leave a slot for a NEON implementation.
    
    Speed is comparable to the serial adler32 computation but near
    64 bytes of input data the SSSE3 code path begins to be faster
    than the serial path: 3x faster at 256 bytes of input data, to
    ~8x faster for 1M of input data [1].
    
    For the PNG 140 image corpus, PNG decoding speed is ~8% faster
    on the average on the various machines tested (see bug).
    
    Update x86.{c,h} to runtime detect SSSE3 support and use it to
    enable the adler32_simd code path and update inflate.c to call
    x86_check_features(). Update the name mangler file names.h for
    the new symbols added, add FIXME notes about simd.patch.
    
    Update BUILD.gn to put the code in its own source set, and add
    it conditionally to the zlib library build rule. Add FIXME re:
    NEON support and an ADLER32_SIMD_SSSE3 define for x86 and x64.
    
    No change in behavior, covered by many existing tests.
    
    [1] I compared using 32 and 48 as the break point to enter the
    vector code using image_decode_bench (over the PNG 140 corpus)
    but they regressed results compared to using 64.
    
    Bug:762564
    Change-Id: I14a39940ae113b5a67ba70a99c3741e289b1796b

---
+++ third_party/zlib/adler32_simd.h
@@ -0,0 +1,12 @@
+/* adler32_simd.h
+ *
+ * For conditions of distribution and use, see copyright notice in zlib.h
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+
+uint32_t ZLIB_INTERNAL adler32_simd_(
+    uint32_t adler,
+    const unsigned char *buf,
+    size_t len);
