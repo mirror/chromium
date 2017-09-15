9f02bf6dda3085b6d5d9e8a8f8cc8b2012695584
Khushal <khushalsagar@chromium.org>
2017-09-15 15:30:59 -0700

    blink: Fix bugs in blink Image metadata tracking.
    This change fixes the following bugs in BitmapImage and Image.
    
    1) The repetition count in BitmapImage is assumed to only be known once
    a complete loop of an animation has been completed. This is an
    incorrect assumption, since the repetition count is locked once all
    encoded data for the image is available.
    
    2) The completion state of the PaintImage created by Image overrides is
    set based on whether CurrentFrameIsFullyReceived. This method should
    return whether all data to completely decode the current frame is
    available. This is incorrect since the state should be set based on
    whether the complete data for the image has been received. In addition,
    since the method is not overridden by any of the Image subclasses, the
    value defaults to false in all cases. Fix this by setting the state
    correctly in all image overrides.
    
    3) When creating a PaintImageGenerator for a PaintImage, the
    DeferredImageDecoder records the actual duration for a frame. For the
    purpose of animating this image, this value is overridden to account
    for cases where the duration < 10ms. Ensure that this overridden value
    is added to the generator's FrameMetadata.
    
    Bug:735741
    Change-Id: I93918f5306b39c6d71b1f1e36dc448263052f3e3

--- third_party/WebKit/Source/core/svg/graphics/SVGImage.cpp
+++ third_party/WebKit/Source/core/svg/graphics/SVGImage.cpp
@@ -317,6 +317,7 @@ void SVGImage::DrawForContainer(PaintCanvas* canvas,
 PaintImage SVGImage::PaintImageForCurrentFrame() {
   PaintImageBuilder builder;
   InitPaintImageBuilder(builder);
+  builder.set_completion_state(completion_state());
   PopulatePaintRecordForCurrentFrameForContainer(builder, NullURL(), Size());
   return builder.TakePaintImage();
 }
@@ -719,6 +720,7 @@ Image::SizeAvailability SVGImage::DataChanged(bool all_data_received) {
 
   if (!all_data_received)
     return page_ ? kSizeAvailable : kSizeUnavailable;
+  all_data_received_ = true;
 
   CHECK(!page_);
 
