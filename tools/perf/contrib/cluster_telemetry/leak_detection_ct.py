f49a9f35a70d8eb3947f0e36304be47b6a13ff4e
Yuzu Saijo <yuzus@chromium.org>
2017-09-15 11:34:58 +0900

    NOT FOR REVIEW | Add Cluster Telemetry benchmark & Telemetry benchmark for leak detection
    Bug:
    Change-Id: I51223b7fdfc6d56830315317a70b2a2ddcc1d78e

---
+++ tools/perf/contrib/cluster_telemetry/leak_detection_ct.py
@@ -0,0 +1,44 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from benchmarks import leak_detection as ld
+
+from contrib.cluster_telemetry import ct_benchmarks_util
+from contrib.cluster_telemetry import page_set
+
+from telemetry import story
+
+
+# pylint: disable=protected-access
+class LeakDetectionClusterTelemetry(ld.MemoryLeakDetectionBenchmark):
+
+  options = {'upload_results': True}
+
+  @classmethod
+  def AddBenchmarkCommandLineArgs(cls, parser):
+    super(LeakDetectionClusterTelemetry, cls).AddBenchmarkCommandLineArgs(parser)
+    ct_benchmarks_util.AddBenchmarkCommandLineArgs(parser)
+
+  def CreateStorySet(self, options):
+    def RunNavigateSteps(self, action_runner):
+        action_runner.Navigate('about:blank')
+        action_runner.PrepareForLeakDetection()
+        action_runner.MeasureMemory(True)
+        action_runner.Navigate(self.url)
+        py_utils.WaitFor(action_runner.tab.HasReachedQuiescence, timeout=30)
+        action_runner.Navigate('about:blank')
+        action_runner.PrepareForLeakDetection()
+        action_runner.MeasureMemory(True)
+    return page_set.CTPageSet(
+      options.urls_list, options.user_agent, options.archive_data_file,
+      run_page_interaction_callback=RunNavigateSteps)
+
+  @classmethod
+  def Name(cls):
+    return 'leak_detection.cluster_telemetry'
+
+  def GetExpectations(self):
+    class StoryExpectations(story.expectations.StoryExpectations):
+      def SetExpectations(self):
+        pass # Not tests disabled.
+    return StoryExpectations()
