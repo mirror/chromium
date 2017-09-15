c048281a57d955ae79bc5ef184c178d1cbcd7f6d
Vitalii Iarko <vitaliii@chromium.org>
2017-09-15 14:58:19 +0200

    [NTP::Push] Ignore and report received messages when not listening.
    Previously, the handler just crashed when it was not listening and a
    message was received. Unfortunately, the metric to track received
    messages is reported after that code, so we do not observe these
    messages in our data.
    
    In this CL:
    
    1) Ignore received messages when not listening.
    
    2) Report whether the handler is listening when a message is received
    (in addition to whether it contains pushed news).
    
    Bug:763520
    Change-Id: Ib35962d0584aeb39bf43cf2ca2e84a5efba99359

--- components/ntp_snippets/breaking_news/breaking_news_gcm_app_handler.cc
+++ components/ntp_snippets/breaking_news/breaking_news_gcm_app_handler.cc
@@ -304,7 +304,15 @@ void BreakingNewsGCMAppHandler::OnMessage(const std::string& app_id,
   DCHECK_EQ(app_id, kBreakingNewsGCMAppID);
 
   gcm::MessageData::const_iterator it = message.data.find(kPushedNewsKey);
-  if (it != message.data.end()) {
+  bool contains_pushed_news = (it != message.data.end());
+  metrics::OnMessageReceived(IsListening(), contains_pushed_news);
+
+  if (!IsListening()) {
+    DLOG(WARNING) << "Received a pushed messaged while not listening.";
+    return;
+  }
+
+  if (contains_pushed_news) {
     const std::string& news = it->second;
     parse_json_callback_.Run(
         news,
@@ -316,8 +324,6 @@ void BreakingNewsGCMAppHandler::OnMessage(const std::string& app_id,
     LOG(WARNING)
         << "Receiving pushed content failure: Breaking News ID missing.";
   }
-
-  metrics::OnMessageReceived(/*contains_pushed_news=*/it != message.data.end());
 }
 
 void BreakingNewsGCMAppHandler::OnMessagesDeleted(const std::string& app_id) {
@@ -363,6 +369,11 @@ void BreakingNewsGCMAppHandler::ClearProfilePrefs(PrefService* pref_service) {
 void BreakingNewsGCMAppHandler::OnJsonSuccess(
     std::unique_ptr<base::Value> content) {
   DCHECK(content);
+
+  if (!IsListening()) {
+    return;
+  }
+
   std::vector<FetchedCategory> fetched_categories;
   if (!JsonToCategories(*content, &fetched_categories,
                         /*fetch_time=*/base::Time::Now())) {
