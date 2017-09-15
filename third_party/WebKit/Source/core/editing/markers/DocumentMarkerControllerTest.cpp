2e3cfc608b9adea99b5262efccad876667cc0ed6
Ryan Landay <rlanday@chromium.org>
2017-09-15 12:05:34 -0700

    Add SuggestionMarkerProperties
    This CL introduces a builder pattern for
    DocumentMarkerController::AddSuggestionMarker() and SuggestionMarker's
    constructor, to make it easier to read call sites where we construct or add
    SuggestionMarkers.
    Change-Id: I46d4d891b9cb28e672562259a25e40731cd1febf

--- third_party/WebKit/Source/core/editing/markers/DocumentMarkerControllerTest.cpp
+++ third_party/WebKit/Source/core/editing/markers/DocumentMarkerControllerTest.cpp
@@ -37,6 +37,7 @@
 #include "core/dom/Text.h"
 #include "core/editing/EphemeralRange.h"
 #include "core/editing/markers/SuggestionMarker.h"
+#include "core/editing/markers/SuggestionMarkerProperties.h"
 #include "core/editing/testing/EditingTestBase.h"
 #include "core/html/HTMLElement.h"
 #include "core/testing/DummyPageHolder.h"
@@ -446,13 +447,11 @@ TEST_F(DocumentMarkerControllerTest, SuggestionMarkersHaveUniqueTags) {
   Node* text = div->firstChild();
 
   MarkerController().AddSuggestionMarker(
-      EphemeralRange(Position(text, 0), Position(text, 1)), Vector<String>(),
-      Color::kBlack, Color::kBlack, StyleableMarker::Thickness::kThick,
-      Color::kBlack);
+      EphemeralRange(Position(text, 0), Position(text, 1)),
+      SuggestionMarkerProperties());
   MarkerController().AddSuggestionMarker(
-      EphemeralRange(Position(text, 0), Position(text, 1)), Vector<String>(),
-      Color::kBlack, Color::kBlack, StyleableMarker::Thickness::kThick,
-      Color::kBlack);
+      EphemeralRange(Position(text, 0), Position(text, 1)),
+      SuggestionMarkerProperties());
 
   EXPECT_EQ(2u, MarkerController().Markers().size());
   EXPECT_NE(ToSuggestionMarker(MarkerController().Markers()[0])->Tag(),
