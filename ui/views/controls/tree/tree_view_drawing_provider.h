fd4fa57a2259b24088fcb77aa7d3a94f5e446149
Elly Fong-Jones <ellyjones@chromium.org>
2017-09-15 16:58:57 -0400

    views: factor out TreeView drawing options
    Currently, it's difficult for consumers of TreeView to customize its visuals.
    All the painting methods are private and there's no easy way to subclass
    TreeView to change their behavior. This change:
    
    1) Adds an explicit extension point for TreeView painting, named
       TreeView::DrawingProvider,
    2) Moves some of the existing painting logic into the base DrawingProvider
       implementation,
    3) Adds a sample DrawingProvider to TreeViewExample
    
    This will be required to customize TreeView behavior in the collected cookies
    dialog (https://crbug.com/610428).
    
    Bug: 610428
    Change-Id: Ia1c1c41f3f54065b30ed276f7ee8c390aab3d0d6

---
+++ ui/views/controls/tree/tree_view_drawing_provider.h
@@ -0,0 +1,38 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_VIEWS_CONTROLS_TREE_TREE_VIEW_DRAWING_PROVIDER_H_
+#define UI_VIEWS_CONTROLS_TREE_TREE_VIEW_DRAWING_PROVIDER_H_
+
+#include "base/strings/string16.h"
+#include "third_party/skia/include/core/SkColor.h"
+#include "ui/views/views_export.h"
+
+namespace ui {
+class TreeModelNode;
+}
+
+namespace views {
+
+class TreeView;
+
+class VIEWS_EXPORT TreeViewDrawingProvider {
+ public:
+  TreeViewDrawingProvider();
+  virtual ~TreeViewDrawingProvider();
+
+  virtual SkColor GetBackgroundColorForNode(TreeView* tree_view,
+                                            ui::TreeModelNode* node);
+  virtual SkColor GetTextColorForNode(TreeView* tree_view,
+                                      ui::TreeModelNode* node);
+
+  // The auxiliary text for a node is descriptive text drawn on the trailing end
+  // of the node's row in the treeview.
+  virtual base::string16 GetAuxiliaryTextForNode(TreeView* tree_view,
+                                                 ui::TreeModelNode* node);
+};
+
+}  // namespace views
+
+#endif  // UI_VIEWS_CONTROLS_TREE_TREE_VIEW_DISPLAY_PROVIDER_H_
