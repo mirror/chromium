9117d28d14f48e3e8d75a98ac13cebf4eb0b6560
Bettina Dea <bdea@chromium.org>
2017-09-14 17:53:17 -0700

    Make new tab promo bubble inactive.
    Previously, the new tab promo bubble would pop up and take off the focus
    off of the omnibox and place it on the promo bubble. This interrupts
    users as the user has to click back on the omnibox (making it two
    clicks). Thus by making it inactive, it prevents interruptions to users.
    
    However, there were some more additional bugs due to the bubble now
    being inactive.
    
    Because it was inactive, it didn't register clicks anymore. When a user
    clicks on the new tab button or opens a new tab the promo didn't
    disappear
    and instead would block on the new tab. Thus, now every time a new tab
    is
    open, we have to call manually call CloseBubble to close the bubble.
    
    Another bug was that due to the new tab promo bubble using anchor_rect
    for the BubbleDialogDelegateView, whenever the window was resized when
    the bubble was still showing, the bubble would not move with the
    resizing window so it would just float on air in the screen. I changed
    this to using anchor_view instead and tested it on Linux.
    
    Bug:734132
    Change-Id: I60d1f0a0ed8bb4c192aa2856f01a4baa7b495039

--- chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
@@ -366,6 +366,13 @@ void BrowserTabStripController::CreateNewTab() {
   feature_engagement::NewTabTrackerFactory::GetInstance()
       ->GetForProfile(browser_view_->browser()->profile())
       ->OnNewTabOpened();
+  if (feature_engagement::NewTabTrackerFactory::GetInstance()
+          ->GetForProfile(browser_view_->browser()->profile())
+          ->CloseBubble()) {
+    feature_engagement::NewTabTrackerFactory::GetInstance()
+        ->GetForProfile(browser_view_->browser()->profile())
+        ->OnPromoClosed();
+  }
 #endif
   model_->delegate()->AddTabAt(GURL(), -1, true);
 }
