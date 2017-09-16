de8e10623fe6efb27fa91807846a89a7e540fa8a
Koji Ishii <kojii@chromium.org>
2017-09-16 20:08:59 +0900

    ceilwidth
    Bug:
    Change-Id: I9ce93f053acd08ca578037e9b61f10c40a3f5d70

--- third_party/WebKit/Source/core/layout/LayoutBlockFlowLine.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlowLine.cpp
@@ -543,9 +543,9 @@ static inline void SetLogicalWidthForTextRun(
 
   const Font& font = layout_text.Style(line_info.IsFirstLine())->GetFont();
 
-  LayoutUnit hyphen_width;
+  float hyphen_width = 0;
   if (ToInlineTextBox(run->box_)->HasHyphen())
-    hyphen_width = LayoutUnit(layout_text.HyphenWidth(font, run->Direction()));
+    hyphen_width = layout_text.HyphenWidth(font, run->Direction());
 
   float measured_width = 0;
   FloatRect glyph_bounds;
@@ -631,7 +631,8 @@ static inline void SetLogicalWidthForTextRun(
 
   glyph_overflow.SetFromBounds(glyph_bounds, font, measured_width);
 
-  run->box_->SetLogicalWidth(LayoutUnit(measured_width) + hyphen_width);
+  run->box_->SetLogicalWidth(
+      LayoutUnit::FromFloatCeil(measured_width + hyphen_width));
   if (!fallback_fonts.IsEmpty()) {
     DCHECK(run->box_->IsText());
     GlyphOverflowAndFallbackFontsMap::ValueType* it =
