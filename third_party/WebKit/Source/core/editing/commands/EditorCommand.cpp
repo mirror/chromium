ab78744bd884010228479e20ddada18a0c542919
tanvir.rizvi <tanvir.rizvi@samsung.com>
2017-09-18 17:09:28 +0530

    Update queryCommandSupported for cut/copy command per spec
    Currently queryCommandSupported for cut and copy always
    returns true, even when execCommand for same fails,
    As per the recent spec updates[1] queryCommandSupported
    should return false if the environment do not support.
    The spec says as "All commands defined in this specification
    must be supported, except optionally the copy command,
    the cut command, and/or the paste command."
    Copy, cut and paste command may/maynot be enabled based on the
    environment.
    
    [1] https://w3c.github.io/editing/execCommand.html#supported-commands
    
    Bug: 757140
    Change-Id: If6792ff8793c908e98b4a70781e0a5241162440c

--- third_party/WebKit/Source/core/editing/commands/EditorCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/EditorCommand.cpp
@@ -2071,6 +2071,13 @@ static bool PasteSupported(LocalFrame* frame) {
   return frame->GetEditor().Client().CanPaste(frame, default_value);
 }
 
+static bool CopyCutSupported(LocalFrame* frame) {
+  const Settings* const settings = frame->GetSettings();
+  const bool default_value =
+      settings && settings->GetJavaScriptCanAccessClipboard();
+  return frame->GetEditor().Client().CanCopyCut(frame, default_value);
+}
+
 static bool SupportedFromMenuOrKeyBinding(LocalFrame*) {
   return false;
 }
@@ -2449,13 +2456,13 @@ static const EditorInternalCommand* InternalCommand(
       {WebEditingCommandType::kBold, ExecuteToggleBold, Supported,
        EnabledInRichlyEditableText, StateBold, ValueStateOrNull,
        kNotTextInsertion, kDoNotAllowExecutionWhenDisabled},
-      {WebEditingCommandType::kCopy, ExecuteCopy, Supported, EnabledCopy,
+      {WebEditingCommandType::kCopy, ExecuteCopy, CopyCutSupported, EnabledCopy,
        StateNone, ValueStateOrNull, kNotTextInsertion,
        kAllowExecutionWhenDisabled},
       {WebEditingCommandType::kCreateLink, ExecuteCreateLink, Supported,
        EnabledInRichlyEditableText, StateNone, ValueStateOrNull,
        kNotTextInsertion, kDoNotAllowExecutionWhenDisabled},
-      {WebEditingCommandType::kCut, ExecuteCut, Supported, EnabledCut,
+      {WebEditingCommandType::kCut, ExecuteCut, CopyCutSupported, EnabledCut,
        StateNone, ValueStateOrNull, kNotTextInsertion,
        kAllowExecutionWhenDisabled},
       {WebEditingCommandType::kDefaultParagraphSeparator,
