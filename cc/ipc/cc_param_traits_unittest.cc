a91832c94390b6249f64fe497f0ab0d730ac36e8
Fady Samuel <fsamuel@chromium.org>
2017-09-17 22:51:06 -0400

    WIP: Use default background color if no fallback specified.
    Bug:
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I367f7eab7b0189d438926dea407076ef6007f0a4

--- cc/ipc/cc_param_traits_unittest.cc
+++ cc/ipc/cc_param_traits_unittest.cc
@@ -391,7 +391,7 @@ TEST_F(CCParamTraitsTest, AllQuads) {
   surface_in->SetAll(shared_state3_in, arbitrary_rect2,
                      arbitrary_rect1_inside_rect2, arbitrary_bool1,
                      arbitrary_surface_id, viz::SurfaceDrawQuadType::PRIMARY,
-                     nullptr);
+                     SK_ColorWHITE, nullptr);
   pass_cmp->CopyFromAndAppendDrawQuad(surface_in);
 
   TextureDrawQuad* texture_in =
