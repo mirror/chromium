360ec898041782f2cb3323254a4a2ebd88b1d15f
Thanh Le <thanhdle@chromium.org>
2017-09-18 15:57:10 -0700

    Try mojo to communicate between Javascript and C++.
    Print messages to chrome://interventions-internals.
    
    Bug: 764413
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Id943304b28cd6fb64f0e1ed72a12e392d7cfda8e

---
+++ chrome/browser/resources/interventions_internals/index.js
@@ -0,0 +1,42 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(function() {
+// Connection to the InterventionsInternalsPageHandler instance running in the
+// browser process.
+let pageHandler = null;
+
+function getPreviewsEnabled() {
+  let node = document.createElement('p');
+
+  pageHandler.getPreviewsEnabled()
+      .then(function(response) {
+        let message = 'Previews enabled: ';
+
+        if (response.enabled) {
+          console.log("Enabled");
+          message += 'Enabled';
+        } else {
+          console.log("Enabled");
+          message += 'Disabled';
+        }
+
+        $('log-message').appendChild(node);
+        node.appendChild(document.createTextNode(message));
+      })
+      .catch(function(error) {
+        let message = document.createTextNode(error.message);
+        $('log-message').appendChild(node);
+        node.appendChild(message);
+      });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  pageHandler = new mojom.InterventionsInternalsPageHandlerPtr;
+  Mojo.bindInterface(
+      mojom.InterventionsInternalsPageHandler.name,
+      mojo.makeRequest(pageHandler).handle);
+  getPreviewsEnabled();
+});
+})();
---
+++ chrome/browser/ui/webui/interventions_internals/interventions_internals_page_handler.cc
@@ -0,0 +1,16 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/webui/interventions_internals/interventions_internals_page_handler.h"
+
+InterventionsInternalsPageHandler::InterventionsInternalsPageHandler(
+    mojom::InterventionsInternalsPageHandlerRequest request)
+    : binding_(this, std::move(request)) {}
+
+InterventionsInternalsPageHandler::~InterventionsInternalsPageHandler() {}
+
+void InterventionsInternalsPageHandler::GetPreviewsEnabled(
+    GetPreviewsEnabledCallback callback) {
+  std::move(callback).Run(true);
+}
