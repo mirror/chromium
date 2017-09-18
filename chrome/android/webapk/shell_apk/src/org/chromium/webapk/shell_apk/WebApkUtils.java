7b912e5d8fdaf187d33286c941d23bd1a5501964
Ran Ji <ranj@chromium.org>
2017-09-18 11:08:46 -0400

    Rewrite intent url to start url if intent url does not match scope
    Bug:763380
    Change-Id: I84cebeac83b2920cf9171fcc5bffff3b650c99ee

--- chrome/android/webapk/shell_apk/src/org/chromium/webapk/shell_apk/WebApkUtils.java
+++ chrome/android/webapk/shell_apk/src/org/chromium/webapk/shell_apk/WebApkUtils.java
@@ -130,32 +130,32 @@ public class WebApkUtils {
      * scheme and host name in this case, and append orginal intent url if |loggedIntentUrlParam| is
      * set.
      */
-    public static String rewriteIntentUrlIfNecessary(String startUrl, Bundle metadata) {
-        String returnUrl = startUrl;
+    public static String rewriteIntentUrlIfNecessary(String intentStartUrl, Bundle metadata) {
         String scopeUrl = metadata.getString(WebApkMetaDataKeys.SCOPE);
-        if (!TextUtils.isEmpty(scopeUrl)) {
-            Uri parsedStartUrl = Uri.parse(startUrl);
-            Uri parsedScope = Uri.parse(scopeUrl);
+        String startUrl = metadata.getString(WebApkMetaDataKeys.START_URL);
 
-            if (!parsedStartUrl.getScheme().equals(parsedScope.getScheme())
-                    || !parsedStartUrl.getEncodedAuthority().equals(
-                               parsedScope.getEncodedAuthority())) {
-                Uri.Builder returnUrlBuilder =
-                        parsedStartUrl.buildUpon()
-                                .scheme(parsedScope.getScheme())
-                                .encodedAuthority(parsedScope.getEncodedAuthority());
-
-                String loggedIntentUrlParam =
-                        metadata.getString(WebApkMetaDataKeys.LOGGED_INTENT_URL_PARAM);
-                if (loggedIntentUrlParam != null && !TextUtils.isEmpty(loggedIntentUrlParam)) {
-                    String orginalUrl = Uri.encode(startUrl).toString();
-                    returnUrlBuilder.appendQueryParameter(loggedIntentUrlParam, orginalUrl);
-                }
-
-                returnUrl = returnUrlBuilder.build().toString();
-            }
+        if (TextUtils.isEmpty(scopeUrl) || isUrlInScope(intentStartUrl, scopeUrl)) {
+            return intentStartUrl;
         }
-        return returnUrl;
+
+        String loggedIntentUrlParam =
+                metadata.getString(WebApkMetaDataKeys.LOGGED_INTENT_URL_PARAM);
+        if (TextUtils.isEmpty(loggedIntentUrlParam)) {
+            return startUrl;
+        }
+
+        Uri.Builder returnUrlBuilder = Uri.parse(startUrl).buildUpon();
+        returnUrlBuilder.appendQueryParameter(
+                loggedIntentUrlParam, Uri.encode(intentStartUrl).toString());
+        return returnUrlBuilder.toString();
+    }
+
+    private static boolean isUrlInScope(String url, String scopeUrl) {
+        Uri parsedUrl = Uri.parse(url);
+        Uri parsedScopeUrl = Uri.parse(scopeUrl);
+        return parsedUrl.getScheme().equals(parsedScopeUrl.getScheme())
+                && parsedUrl.getHost().equals(parsedScopeUrl.getHost())
+                && parsedUrl.getPath().startsWith(parsedScopeUrl.getPath());
     }
 
     /**
