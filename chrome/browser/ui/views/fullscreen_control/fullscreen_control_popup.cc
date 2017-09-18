c27fca4288beefe6d6fa9bf493e1151cfee91f46
Yuwei Huang <yuweih@chromium.org>
2017-09-18 14:41:27 -0700

    [Fullscreen Control] Finish up the fullscreen control UI and add the animation class
    This CL finishes up the fullscreen control UI. It is a transparent-black
    circular button with a close (X) icon on the middle. It is used both as an
    indicator for exiting fullscreen when the keyboard lock reserves the ESC
    key and an actual button to exit fullscreen for touch devices.
    
    This CL also implements a FullscreenControlPopup class to show the
    indicator with a dropdown animation. We could potentially switch to use
    DropdownBarHost to show the animation once MacViews is ready.
    
    This is the UX spec (Google internal):
    https://docs.google.com/presentation/d/11D86S87NZK5sAMLhc8WSV9pk_VWkjMAjvnN08uUD5po/edit#slide=id.g23756e43b7_3_0
    
    Bug: 758456
    Change-Id: I44dc2d0944386f5f6f5e607831c5ea4e38364357

---
+++ chrome/browser/ui/views/fullscreen_control/fullscreen_control_popup.cc
@@ -0,0 +1,97 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/fullscreen_control/fullscreen_control_popup.h"
+
+#include "base/bind.h"
+#include "chrome/browser/ui/views/fullscreen_control/fullscreen_control_view.h"
+#include "ui/compositor/scoped_layer_animation_settings.h"
+#include "ui/views/widget/widget.h"
+
+namespace {
+
+// Offsets with respect to the top y coordinate of the parent widget.
+constexpr int kFinalOffset = 45;
+
+constexpr float kInitialOpacity = 0.1f;
+constexpr float kFinalOpacity = 1.f;
+
+constexpr int kSlideInDurationMs = 300;
+constexpr int kSlideOutDurationMs = 150;
+
+// Creates a Widget containing an FullscreenControlView.
+std::unique_ptr<views::Widget> CreatePopupWidget(gfx::NativeView parent_view,
+                                                 FullscreenControlView* view) {
+  // Initialize the popup.
+  std::unique_ptr<views::Widget> popup = std::make_unique<views::Widget>();
+  views::Widget::InitParams params(views::Widget::InitParams::TYPE_POPUP);
+  params.opacity = views::Widget::InitParams::TRANSLUCENT_WINDOW;
+  params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
+  params.parent = parent_view;
+  params.accept_events = false;
+  popup->Init(params);
+  popup->SetContentsView(view);
+
+  return popup;
+}
+
+}  // namespace
+
+FullscreenControlPopup::FullscreenControlPopup(
+    gfx::NativeView parent_view,
+    const base::RepeatingClosure& on_button_pressed)
+    : control_view_(new FullscreenControlView(on_button_pressed)),
+      popup_(CreatePopupWidget(parent_view, control_view_)),
+      animation_(new gfx::SlideAnimation(this)) {
+  animation_->Reset(0);
+}
+
+FullscreenControlPopup::~FullscreenControlPopup() {}
+
+void FullscreenControlPopup::Show(const gfx::Rect& parent_bounds_in_screen) {
+  parent_bounds_in_screen_ = parent_bounds_in_screen;
+
+  animation_->SetSlideDuration(kSlideInDurationMs);
+  animation_->Show();
+
+  // The default animation progress is 0. Call it once here then show the popup
+  // to prevent potential flickering.
+  AnimationProgressed(animation_.get());
+  popup_->Show();
+}
+
+void FullscreenControlPopup::Hide(bool animated) {
+  if (!animated) {
+    animation_->Reset(0);
+    popup_->SetOpacity(0.f);
+    popup_->Hide();
+    return;
+  }
+
+  animation_->SetSlideDuration(kSlideOutDurationMs);
+  animation_->Hide();
+}
+
+void FullscreenControlPopup::AnimationProgressed(
+    const gfx::Animation* animation) {
+  float opacity = static_cast<float>(
+      animation_->CurrentValueBetween(kInitialOpacity, kFinalOpacity));
+  popup_->SetOpacity(opacity);
+  int initial_offset = -control_view_->GetPreferredSize().height();
+  int current_y = parent_bounds_in_screen_.y() +
+                  animation_->CurrentValueBetween(initial_offset, kFinalOffset);
+  gfx::Point origin(parent_bounds_in_screen_.CenterPoint().x() -
+                        control_view_->GetPreferredSize().width() / 2,
+                    current_y);
+  popup_->SetBounds({origin, control_view_->GetPreferredSize()});
+}
+
+void FullscreenControlPopup::AnimationEnded(const gfx::Animation* animation) {
+  if (animation_->GetCurrentValue() == 0.0) {
+    // It's the end of the reversed animation. Just hide the popup in this case.
+    popup_->Hide();
+    return;
+  }
+  AnimationProgressed(animation);
+}
