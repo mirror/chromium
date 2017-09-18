445ff1dcb71b2f1b307299a44f545d755f356d0c
Nick Carter <nick@chromium.org>
2017-09-18 14:12:24 -0700

    Introduce TableModelObserver::OnItemsMoved() and enhance ListSelectionModel::Move()
    In TableModelObserver, support a OnItemsMoved() operation which indicates
    that a number of rows have been reordered in the model. In a subsequent CL,
    the TaskManagerTableModel will emit these, with the goal being to
    keep a tab properly selected during cross-process navigation.
    
    In TableView and its unittest, handle the OnItemsMoved() operation, by
    updating the ListSelectionModel as appropriate.
    
    In ListSelectionModel, Add a |length| parameter to Move, and provide
    an efficient implementation of this. Implement IncrementFrom() and
    DecrementFrom() in terms of the Move() operation. Add unittests that
    exercise Move().
    
    Bug: 527455
    Change-Id: I448bc18b2a3efbe6b2acb78d73ee350212bbf485

--- ui/base/models/list_selection_model.cc
+++ ui/base/models/list_selection_model.cc
@@ -15,12 +15,15 @@ namespace ui {
 // static
 const int ListSelectionModel::kUnselectedIndex = -1;
 
-static void IncrementFromImpl(int index, int* value) {
+namespace {
+
+void IncrementFromImpl(int index, int* value) {
   if (*value >= index)
     (*value)++;
 }
 
-static bool DecrementFromImpl(int index, int* value) {
+// Returns true if |value| should be erased from its container.
+bool DecrementFromImpl(int index, int* value) {
   if (*value == index) {
     *value = ListSelectionModel::kUnselectedIndex;
     return true;
@@ -29,6 +32,30 @@ static bool DecrementFromImpl(int index, int* value) {
     (*value)--;
   return false;
 }
+
+void MoveToLowerIndexImpl(int old_start,
+                          int new_start,
+                          int length,
+                          int* value) {
+  DCHECK_LE(new_start, old_start);
+  DCHECK_GE(length, 0);
+  // When a range of items moves to a lower index, the only affected indices
+  // are those in the interval [new_start, old_start + length).
+  if (new_start <= *value && *value < old_start + length) {
+    if (*value < old_start) {
+      // The items originally in the interval [new_start, old_start) see
+      // |length| many items inserted before them, so their indices increase.
+      *value += length;
+    } else {
+      // The items originally in the interval [old_start, old_start + length)
+      // are shifted downward by (old_start - new_start) many spots, so
+      // their indices decrease.
+      *value -= (old_start - new_start);
+    }
+  }
+}
+
+}  // namespace
 
 ListSelectionModel::ListSelectionModel()
     : active_(kUnselectedIndex),
@@ -39,7 +66,7 @@ ListSelectionModel::~ListSelectionModel() {
 }
 
 void ListSelectionModel::IncrementFrom(int index) {
-  // Shift the selection to account for the newly inserted tab.
+  // Shift the selection to account for a newly inserted item at |index|.
   for (SelectedIndices::iterator i = selected_indices_.begin();
        i != selected_indices_.end(); ++i) {
     IncrementFromImpl(index, &(*i));
@@ -112,24 +139,55 @@ void ListSelectionModel::AddSelectionFromAnchorTo(int index) {
   }
 }
 
-void ListSelectionModel::Move(int from, int to) {
-  DCHECK_NE(to, from);
-  bool was_anchor = from == anchor_;
-  bool was_active = from == active_;
-  bool was_selected = IsSelected(from);
-  if (to < from) {
-    IncrementFrom(to);
-    DecrementFrom(from + 1);
-  } else {
-    DecrementFrom(from);
-    IncrementFrom(to);
+void ListSelectionModel::Move(int from, int to, int length) {
+  // |length| many items are moving from index |from| to index |to|.
+  DCHECK_NE(from, to);
+  DCHECK_GT(length, 0);
+
+  // Remap move-to-higher-index operations to the equivalent move-to-lower-index
+  // operation. As an example, the permutation "ABCDEFG" -> "CDEFABG" can be
+  // thought of either as shifting 'AB' higher by 4, or by shifting 'CDEF' lower
+  // by 2.
+  if (to > from) {
+    Move(from + length, from, to - from);
+    return;
   }
-  if (was_active)
-    active_ = to;
-  if (was_anchor)
-    anchor_ = to;
-  if (was_selected)
-    AddIndexToSelection(to);
+
+  // We know that from > to, so this is a move to a lower index. Start by
+  // transforming |anchor_| and |active_|.
+  MoveToLowerIndexImpl(from, to, length, &anchor_);
+  MoveToLowerIndexImpl(from, to, length, &active_);
+
+  // When a range of items moves to a lower index, the affected items are those
+  // in the interval [to, from + length). Search within |selected_indices_| for
+  // indices that fall into that range.
+  auto low =
+      std::lower_bound(selected_indices_.begin(), selected_indices_.end(), to);
+  auto high = std::lower_bound(low, selected_indices_.end(), from + length);
+
+  // The items originally in the interval [to, from) will see |length| many
+  // items inserted before them, so their indices increase.
+  auto middle = std::lower_bound(low, high, from);
+  int pivot_value = to + length;
+  for (auto it = low; it != middle; ++it) {
+    (*it) += length;
+    DCHECK(pivot_value <= (*it) && (*it) < (from + length));
+  }
+
+  // The items originally in the interval [from, from + length) are shifted
+  // downward by (from - to) many spots, so their indices decrease.
+  for (auto it = middle; it != high; ++it) {
+    (*it) -= (from - to);
+    DCHECK(to <= (*it) && (*it) < pivot_value);
+  }
+
+  // Reorder the ranges [low, middle), and [middle, high) so that the elements
+  // in [middle, high) appear first, followed by [low, middle). This suffices to
+  // restore the sort condition on |selected_indices_|, because each range is
+  // still sorted piecewise, and |pivot_value| is a lower bound for elements in
+  // [low, middle), and an upper bound for [middle, high).
+  std::rotate(low, middle, high);
+  DCHECK(std::is_sorted(selected_indices_.begin(), selected_indices_.end()));
 }
 
 void ListSelectionModel::Clear() {
