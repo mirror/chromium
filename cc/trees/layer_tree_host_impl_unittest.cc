23bf2cbc0111677113362a9b2217d6089d73bdd0
Fady Samuel <fsamuel@chromium.org>
2017-09-18 03:51:37 -0400

    Simplify transparent backgrounds
    Prior to this CL, there were two ways to specify transparent backgrounds:
    
    1. LayerTreeHost::SetHasTransparentBackground(true)
    2. LayerTreeHost::SetBackgroundColor(SK_ColorTRANSPARENT)
    
    The former avoided filling the background of the LayerTreeHost with
    SolidColorDrawQuads, while the latter would cause transparent gutter
    in DelegatedFrameHost, and fill the background with *transparent*
    SolidColorDrawQuads.
    
    This CL unifies this into 2. Setting SK_ColorTRANSPARENT (or any alpha less than
    SK_AlphaOPAQUE (0xff) as the background color will now avoid generating extra
    SolidColorDrawQuads.
    
    Bug: 669444
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Iaca9a230ba42cbd8cf70b8fb833af5e8b5e145ea

--- cc/trees/layer_tree_host_impl_unittest.cc
+++ cc/trees/layer_tree_host_impl_unittest.cc
@@ -8659,7 +8659,6 @@ TEST_F(LayerTreeHostImplTest, HasTransparentBackground) {
   host_impl_->active_tree()->set_background_color(SK_ColorWHITE);
 
   // Verify one quad is drawn when transparent background set is not set.
-  host_impl_->active_tree()->set_has_transparent_background(false);
   TestFrameData frame;
   EXPECT_EQ(DRAW_SUCCESS, host_impl_->PrepareToDraw(&frame));
   {
@@ -8675,7 +8674,7 @@ TEST_F(LayerTreeHostImplTest, HasTransparentBackground) {
   host_impl_->SetFullViewportDamage();
 
   // Verify no quads are drawn when transparent background is set.
-  host_impl_->active_tree()->set_has_transparent_background(true);
+  host_impl_->active_tree()->set_background_color(SK_ColorTRANSPARENT);
   host_impl_->SetFullViewportDamage();
   EXPECT_EQ(DRAW_SUCCESS, host_impl_->PrepareToDraw(&frame));
   {
