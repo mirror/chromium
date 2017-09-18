4185122405f393b4f991874e297bdced192c8198
Guido Urdaneta <guidou@chromium.org>
2017-09-18 19:40:28 +0200

    Wire MediaStreamTrack.applyConstraints() to content
    Most of the functionality for MediaStreamTracks is currently
    implemented in content.
    This CL makes applyConstraints available in content without
    actually implementing the functionality.
    The only behavior changes this CL introduces slight
    differences in the conditions required to pass constraints
    to the ImageCapture implementation.
    
    This CL also provides a TestRunner implementation of applyConstraints
    and a corresponding LayoutTest.
    
    This CL is to be followed by CLs implementing the applyConstraints
    functionality in content as well as browser and Web Platform tests.
    
    Bug: 338503
    Change-Id: I99800626ac704a9e73cf1c655418d4682defd0c1

---
+++ third_party/WebKit/public/web/WebApplyConstraintsRequest.h
@@ -0,0 +1,57 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WebApplyConstraintsRequest_h
+#define WebApplyConstraintsRequest_h
+
+#include "public/platform/WebCommon.h"
+#include "public/platform/WebPrivatePtr.h"
+
+namespace blink {
+
+class ApplyConstraintsRequest;
+class WebMediaConstraints;
+class WebMediaStreamTrack;
+class WebString;
+
+class BLINK_EXPORT WebApplyConstraintsRequest {
+ public:
+  WebApplyConstraintsRequest(const WebApplyConstraintsRequest& request) {
+    Assign(request);
+  }
+  ~WebApplyConstraintsRequest();
+
+  WebApplyConstraintsRequest& operator=(
+      const WebApplyConstraintsRequest& other) {
+    Assign(other);
+    return *this;
+  }
+
+  bool operator==(const WebApplyConstraintsRequest& other) const;
+
+  bool IsNull() const { return private_.IsNull(); }
+
+  WebMediaStreamTrack Track() const;
+  WebMediaConstraints Constraints() const;
+
+  void RequestSucceeded();
+  void RequestFailed(const WebString& constraint, const WebString& message);
+
+  // TODO(guidou): Remove this method. http://crbug.com/338503
+  void RequestNotSupported(const WebString& message);
+
+#if INSIDE_BLINK
+  WebApplyConstraintsRequest() = default;
+  explicit WebApplyConstraintsRequest(ApplyConstraintsRequest*);
+#endif
+
+ private:
+  void Assign(const WebApplyConstraintsRequest&);
+
+  WebPrivatePtr<ApplyConstraintsRequest> private_;
+};
+
+}  // namespace blink
+
+#endif  // WebApplyConstraintsRequest_h
