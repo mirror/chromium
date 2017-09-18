f64cabce8b24b352ebdf4680546e02ebb4ccae0b
Ian Vollick <vollick@chromium.org>
2017-09-18 11:40:13 -0400

    [vr] Create a background for the splash screen text
    Previously, we'd relied on the skybox to provide this background, but
    this is incorrect.
    
    In order to get the splash screen text to render in front of the
    background, I've generalized the notion of "draw phase" and have used
    it to gather collections of renderable elements in vr_shell_gl
    to pass to the UiRenderer.
    
    I've also added the notion of a RenderableElement to help elide and
    ignore elements that could never draw, regardless of their visibility.
    
    Bug:763552
    Change-Id: If94d3945e8f1c8ca2ff7b9b449139fc845271699

--- chrome/browser/vr/elements/draw_phase.h
+++ chrome/browser/vr/elements/draw_phase.h
@@ -8,16 +8,15 @@
 namespace vr {
 
 // Each draw phase is rendered independently in the order specified below.
-//
-// TODO(vollick): once we've established the element hierarchy, only elements in
-// the 2D browsing foreground need to be depth sorted. We should rewrite our
-// sorting logic at that point to leverage the hierarchy and delete this enum.
 enum DrawPhase : int {
   // kPhaseNone is to be used for elements that do not draw. Eg, layouts.
-  kPhaseBackground = 0,
+  // NB: We make queries based on ranges of values here, so do reorder.
+  kPhaseNone = 0,
+  kPhaseBackground,
   kPhaseFloorCeiling,
   kPhaseForeground,
-  kPhaseNone,
+  kPhaseOverlayBackground,
+  kPhaseOverlayForeground,
 };
 
 }  // namespace vr
