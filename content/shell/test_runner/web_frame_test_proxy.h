78ccc3081249141a7d3e220e601fbe1e655cd626
Lukasz Anforowicz <lukasza@chromium.org>
2017-09-18 11:54:45 -0700

    Remove |TestRunner::will_navigate_| - no longer needed by PlzNavigate.
    This CL reverts r412572 - |TestRunner::will_navigate_| is no longer
    needed, because right now PlzNavigate creates a dummy provisional
    DocumentLoader at the start of a renderer-initiated navigation - the
    presence of a provisional DocumentLoader is taken into account when
    computing when to stop the layout TestRunner.
    
    Bug: 765752
    Change-Id: Ia50aabbba25937777c6c966ec8e056d4014bd971

--- content/shell/test_runner/web_frame_test_proxy.h
+++ content/shell/test_runner/web_frame_test_proxy.h
@@ -153,14 +153,6 @@ class WebFrameTestProxy : public Base, public WebFrameTestProxyBase {
     test_client()->DidFinishLoad();
   }
 
-  void DidNavigateWithinPage(const blink::WebHistoryItem& history_item,
-                             blink::WebHistoryCommitType commit_type,
-                             bool content_initiated) override {
-    Base::DidNavigateWithinPage(history_item, commit_type, content_initiated);
-    test_client()->DidNavigateWithinPage(history_item, commit_type,
-                                         content_initiated);
-  }
-
   void DidStopLoading() override {
     Base::DidStopLoading();
     test_client()->DidStopLoading();
@@ -245,11 +237,6 @@ class WebFrameTestProxy : public Base, public WebFrameTestProxyBase {
       return policy;
 
     return Base::DecidePolicyForNavigation(info);
-  }
-
-  void DidStartLoading(bool to_different_document) override {
-    Base::DidStartLoading(to_different_document);
-    test_client()->DidStartLoading(to_different_document);
   }
 
   void WillStartUsingPeerConnectionHandler(
