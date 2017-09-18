0c23abf7ab3a58946dea8847d83aad8766790db2
Xiaohan Wang <xhwang@chromium.org>
2017-09-18 10:53:59 -0700

    Allow sched_getaffinity in utility sandbox on Linux
    This syscall is allowed in the PPAPI sandbox but not in the utility
    sandbox. As we are moving the Widevine CDM from the PPAPI process to the
    utility process (using mojo), we need this API to be allowed in the
    utility sandbox as well.
    
    BUG=765840
    TEST=Encrypted media browser tests that need this pass now.
    
    Change-Id: Ib2015ecfdfd7f47844d13c3c469c244da9cfb95c

--- content/common/sandbox_linux/bpf_utility_policy_linux.cc
+++ content/common/sandbox_linux/bpf_utility_policy_linux.cc
@@ -42,6 +42,7 @@ ResultExpr UtilityProcessPolicy::EvaluateSyscall(int sysno) const {
     case __NR_mremap:  // https://crbug.com/546204
     case __NR_pread64:
     case __NR_pwrite64:
+    case __NR_sched_getaffinity:
     case __NR_sysinfo:
     case __NR_times:
     case __NR_uname:
