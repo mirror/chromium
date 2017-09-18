242e7c494b8c10b2cc2b68457b0a9bbed75a2a76
Olivier Robin <olivierrobin@chromium.org>
2017-09-18 17:55:38 +0200

    Fix flaky WaitableEventWatcherTest/MultipleWatchersManual
    Test is flaky on iOS release running on iOS 11.
    Doing one runloop is not enough for the event to be fired.
    Example of failure:
    https://uberchromegw.corp.google.com/i/official.ios/builders/ios-canary/builds/763/steps/base_unittests%20%28iPhone%207%20iOS%2011.0%29%20on%20iOS-11.0
    https://uberchromegw.corp.google.com/i/official.ios/builders/ios-canary/builds/762/steps/base_unittests%20%28iPhone%207%20iOS%2011.0%29%20on%20iOS-11.0
    
    Bug: None
    Change-Id: I7bf97fc5f59be9cbf0fc63a95d1b23ca549939cc

--- base/synchronization/waitable_event_watcher_unittest.cc
+++ base/synchronization/waitable_event_watcher_unittest.cc
@@ -200,27 +200,35 @@ TEST_P(WaitableEventWatcherTest, MultipleWatchersManual) {
 
   int counter1 = 0;
   int counter2 = 0;
+  int remaining_threads = 2;
 
-  auto callback = [](RunLoop* run_loop, int* counter, WaitableEvent* event) {
+  auto callback = [](RunLoop* run_loop, int* counter, int* remaining_threads,
+                     WaitableEvent* event) {
     ++(*counter);
-    run_loop->QuitWhenIdle();
+    --(*remaining_threads);
+    if (*remaining_threads == 0) {
+      run_loop->QuitWhenIdle();
+    }
   };
 
   RunLoop run_loop;
 
   WaitableEventWatcher watcher1;
   watcher1.StartWatching(
-      &event, BindOnce(callback, Unretained(&run_loop), Unretained(&counter1)));
+      &event, BindOnce(callback, Unretained(&run_loop), Unretained(&counter1),
+                       Unretained(&remaining_threads)));
 
   WaitableEventWatcher watcher2;
   watcher2.StartWatching(
-      &event, BindOnce(callback, Unretained(&run_loop), Unretained(&counter2)));
+      &event, BindOnce(callback, Unretained(&run_loop), Unretained(&counter2),
+                       Unretained(&remaining_threads)));
 
   event.Signal();
   run_loop.Run();
 
   EXPECT_EQ(1, counter1);
   EXPECT_EQ(1, counter2);
+  EXPECT_EQ(0, remaining_threads);
   EXPECT_TRUE(event.IsSignaled());
 }
 
