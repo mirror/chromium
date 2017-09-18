3aedb8b6beaf49dc47374ff10a20b0f40526d835
Mike West <mkwst@chromium.org>
2017-09-18 15:46:32 +0200

    WIP: Explore adding some "safe" string types.
    After talking with some folks about xtof's [1], I think there's something
    there that browsers are uniquely positioned to help out with. This patch
    starts sketching out what a set of safe types might look like, and what
    it might feel like for a developer to lock themselves out of raw string
    assignments to typical XSS sinks like `location.href` or `el.innerHTML`.
    
    [1]: https://research.google.com/pubs/pub42934.html
    
    Bug: 739170
    Change-Id: I65e5add5cc7fcbc17f97ca8b737d7a3e1d8cad3b

--- third_party/WebKit/Source/core/editing/InputMethodControllerTest.cpp
+++ third_party/WebKit/Source/core/editing/InputMethodControllerTest.cpp
@@ -288,21 +288,21 @@ TEST_F(InputMethodControllerTest, SetCompositionKeepingStyle) {
   // Subtract a character.
   Controller().SetComposition(String("12345789"), ime_text_spans, 8, 8);
   EXPECT_STREQ("abc1<b>2</b>3457<b>8</b>9d<b>e</b>f",
-               div->innerHTML().Utf8().data());
+               div->InnerHTMLAsString().Utf8().data());
   EXPECT_EQ(11u, Controller().GetSelectionOffsets().Start());
   EXPECT_EQ(11u, Controller().GetSelectionOffsets().End());
 
   // Append a character.
   Controller().SetComposition(String("123456789"), ime_text_spans, 9, 9);
   EXPECT_STREQ("abc1<b>2</b>34567<b>8</b>9d<b>e</b>f",
-               div->innerHTML().Utf8().data());
+               div->InnerHTMLAsString().Utf8().data());
   EXPECT_EQ(12u, Controller().GetSelectionOffsets().Start());
   EXPECT_EQ(12u, Controller().GetSelectionOffsets().End());
 
   // Subtract and append characters.
   Controller().SetComposition(String("123hello789"), ime_text_spans, 11, 11);
   EXPECT_STREQ("abc1<b>2</b>3hello7<b>8</b>9d<b>e</b>f",
-               div->innerHTML().Utf8().data());
+               div->InnerHTMLAsString().Utf8().data());
 }
 
 TEST_F(InputMethodControllerTest, SetCompositionWithEmojiKeepingStyle) {
@@ -320,11 +320,13 @@ TEST_F(InputMethodControllerTest, SetCompositionWithEmojiKeepingStyle) {
   // surrogate pair to the previous one.
   Controller().SetComposition(String::FromUTF8("\xF0\x9F\x8F\xAB"),
                               ime_text_spans, 2, 2);
-  EXPECT_STREQ("<b>\xF0\x9F\x8F\xAB</b>", div->innerHTML().Utf8().data());
+  EXPECT_STREQ("<b>\xF0\x9F\x8F\xAB</b>",
+               div->InnerHTMLAsString().Utf8().data());
 
   Controller().SetComposition(String::FromUTF8("\xF0\x9F\x8F\xA0"),
                               ime_text_spans, 2, 2);
-  EXPECT_STREQ("<b>\xF0\x9F\x8F\xA0</b>", div->innerHTML().Utf8().data());
+  EXPECT_STREQ("<b>\xF0\x9F\x8F\xA0</b>",
+               div->InnerHTMLAsString().Utf8().data());
 }
 
 TEST_F(InputMethodControllerTest,
@@ -345,11 +347,11 @@ TEST_F(InputMethodControllerTest,
   Controller().SetComposition(String::FromUTF8("\xE0\xB0\x83\xE0\xB0\x83"),
                               ime_text_spans, 2, 2);
   EXPECT_STREQ("<b>\xE0\xB0\x83\xE0\xB0\x83</b>",
-               div->innerHTML().Utf8().data());
+               div->InnerHTMLAsString().Utf8().data());
 
   Controller().SetComposition(String::FromUTF8("\xE0\xB0\x83"), ime_text_spans,
                               1, 1);
-  EXPECT_STREQ("<b>\xE0\xB0\x83</b>", div->innerHTML().Utf8().data());
+  EXPECT_STREQ("<b>\xE0\xB0\x83</b>", div->InnerHTMLAsString().Utf8().data());
 }
 
 TEST_F(InputMethodControllerTest, FinishComposingTextKeepingStyle) {
@@ -364,10 +366,12 @@ TEST_F(InputMethodControllerTest, FinishComposingTextKeepingStyle) {
   Controller().SetCompositionFromExistingText(ime_text_spans, 3, 12);
 
   Controller().SetComposition(String("123hello789"), ime_text_spans, 11, 11);
-  EXPECT_STREQ("abc1<b>2</b>3hello7<b>8</b>9", div->innerHTML().Utf8().data());
+  EXPECT_STREQ("abc1<b>2</b>3hello7<b>8</b>9",
+               div->InnerHTMLAsString().Utf8().data());
 
   Controller().FinishComposingText(InputMethodController::kKeepSelection);
-  EXPECT_STREQ("abc1<b>2</b>3hello7<b>8</b>9", div->innerHTML().Utf8().data());
+  EXPECT_STREQ("abc1<b>2</b>3hello7<b>8</b>9",
+               div->InnerHTMLAsString().Utf8().data());
 }
 
 TEST_F(InputMethodControllerTest, CommitTextKeepingStyle) {
@@ -382,7 +386,8 @@ TEST_F(InputMethodControllerTest, CommitTextKeepingStyle) {
   Controller().SetCompositionFromExistingText(ime_text_spans, 3, 12);
 
   Controller().CommitText(String("123789"), ime_text_spans, 0);
-  EXPECT_STREQ("abc1<b>2</b>37<b>8</b>9", div->innerHTML().Utf8().data());
+  EXPECT_STREQ("abc1<b>2</b>37<b>8</b>9",
+               div->InnerHTMLAsString().Utf8().data());
 }
 
 TEST_F(InputMethodControllerTest, InsertTextWithNewLine) {
@@ -393,7 +398,7 @@ TEST_F(InputMethodControllerTest, InsertTextWithNewLine) {
                                        Color(255, 0, 0), false, 0));
 
   Controller().CommitText(String("hello\nworld"), ime_text_spans, 0);
-  EXPECT_STREQ("hello<div>world</div>", div->innerHTML().Utf8().data());
+  EXPECT_STREQ("hello<div>world</div>", div->InnerHTMLAsString().Utf8().data());
 }
 
 TEST_F(InputMethodControllerTest, InsertTextWithNewLineIncrementally) {
@@ -403,11 +408,11 @@ TEST_F(InputMethodControllerTest, InsertTextWithNewLineIncrementally) {
   Vector<ImeTextSpan> ime_text_spans;
   Controller().CommitText("a", ime_text_spans, 0);
   Controller().SetComposition("bcd", ime_text_spans, 0, 2);
-  EXPECT_STREQ("abcd", div->innerHTML().Utf8().data());
+  EXPECT_STREQ("abcd", div->InnerHTMLAsString().Utf8().data());
 
   Controller().CommitText(String("bcd\nefgh\nijkl"), ime_text_spans, 0);
   EXPECT_STREQ("abcd<div>efgh</div><div>ijkl</div>",
-               div->innerHTML().Utf8().data());
+               div->InnerHTMLAsString().Utf8().data());
 }
 
 TEST_F(InputMethodControllerTest, SelectionOnConfirmExistingText) {
@@ -1533,14 +1538,14 @@ TEST_F(InputMethodControllerTest, WhitespaceFixup) {
 
   // The space at the beginning of the string should have been converted to an
   // nbsp
-  EXPECT_STREQ("&nbsp;text blah", div->innerHTML().Utf8().data());
+  EXPECT_STREQ("&nbsp;text blah", div->InnerHTMLAsString().Utf8().data());
 
   // Delete "blah"
   Controller().SetCompositionFromExistingText(empty_ime_text_spans, 6, 10);
   Controller().CommitText(String(""), empty_ime_text_spans, 0);
 
   // The space at the end of the string should have been converted to an nbsp
-  EXPECT_STREQ("&nbsp;text&nbsp;", div->innerHTML().Utf8().data());
+  EXPECT_STREQ("&nbsp;text&nbsp;", div->InnerHTMLAsString().Utf8().data());
 }
 
 TEST_F(InputMethodControllerTest, CommitEmptyTextDeletesSelection) {
@@ -1830,7 +1835,7 @@ TEST_F(InputMethodControllerTest,
   Controller().SetCompositionFromExistingText(empty_ime_text_spans, 13, 25);
   Controller().CommitText(String("content"), empty_ime_text_spans, 0);
 
-  EXPECT_STREQ("This is some content", div->innerHTML().Utf8().data());
+  EXPECT_STREQ("This is some content", div->InnerHTMLAsString().Utf8().data());
 
   // Verify marker was removed
   EXPECT_EQ(0u, GetDocument().Markers().Markers().size());
@@ -1853,7 +1858,7 @@ TEST_F(InputMethodControllerTest,
   Controller().SetCompositionFromExistingText(empty_ime_text_spans, 13, 25);
   Controller().CommitText(String("content"), empty_ime_text_spans, 0);
 
-  EXPECT_STREQ("This is some content", div->innerHTML().Utf8().data());
+  EXPECT_STREQ("This is some content", div->InnerHTMLAsString().Utf8().data());
 
   // Verify marker is under "some "
   EXPECT_EQ(1u, GetDocument().Markers().Markers().size());
