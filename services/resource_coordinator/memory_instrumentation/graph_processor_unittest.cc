1a208c85e0e09f8924a72fc0b591636611d2acc1
Lalit Maganti <lalitm@chromium.org>
2017-09-19 22:04:59 +0100

    [DO NOT SUBMIT] Add graph processing to memory instrumentation service
    Bug:
    Change-Id: I614e33d78c2e5431a3774310bfc4525f84127231

---
+++ services/resource_coordinator/memory_instrumentation/graph_processor_unittest.cc
@@ -0,0 +1,50 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/resource_coordinator/memory_instrumentation/graph_processor.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace memory_instrumentation {
+
+using base::ProcessId;
+using base::trace_event::HeapProfilerSerializationState;
+using base::trace_event::MemoryAllocatorDump;
+using base::trace_event::MemoryDumpArgs;
+using base::trace_event::MemoryDumpLevelOfDetail;
+using base::trace_event::ProcessMemoryDump;
+
+TEST(GraphProcessorTest, Process) {
+  std::map<ProcessId, ProcessMemoryDump> process_dumps;
+
+  MemoryDumpArgs dump_args = {MemoryDumpLevelOfDetail::DETAILED};
+  ProcessMemoryDump pmd(new HeapProfilerSerializationState, dump_args);
+
+  auto* allocator_dump = pmd.CreateAllocatorDump("test1/test2/test3");
+  allocator_dump->AddScalar(MemoryAllocatorDump::kNameSize,
+                            MemoryAllocatorDump::kUnitsBytes, 10);
+
+  process_dumps.emplace(0, std::move(pmd));
+
+  auto global_dump = ProcessGlobalDump(std::move(process_dumps));
+  for (auto& id_to_dump : global_dump->container_dumps()) {
+    auto* root = id_to_dump.second->dump_root();
+
+    auto* first_child = root->GetChild("test1");
+    ASSERT_TRUE(first_child != nullptr);
+
+    auto* second_child = first_child->GetChild("test2");
+    ASSERT_TRUE(second_child != nullptr);
+
+    auto* third_child = second_child->GetChild("test3");
+    ASSERT_TRUE(third_child != nullptr);
+
+    ASSERT_EQ(third_child->entries().size(), 1ul);
+
+    auto size = third_child->entries().find(MemoryAllocatorDump::kNameSize);
+    ASSERT_EQ(10ul, size->second.value_uint64);
+  }
+}
+
+}  // namespace memory_instrumentation
