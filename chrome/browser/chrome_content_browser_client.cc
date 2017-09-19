53492950c4def2beaf163c2eb89436be70fe2753
Jay Civelli <jcivelli@google.com>
2017-09-19 14:34:49 -0700

    Servicifying PWGRasterConverter.
    Changing PWGRasterConverterHelper to use a new service and relying on
    the service manager for creating the child process instead of using
    UtilityProcessHost.
    This is part of the effort to deprecate the use of UtilityProcessHost.
    
    Bug: 758355
    Change-Id: I7746dbab8485bdbe2310e2abe934e35106b59e40

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -395,6 +395,9 @@
 
 #if BUILDFLAG(ENABLE_PRINTING)
 #include "components/printing/service/public/interfaces/pdf_compositor.mojom.h"
+#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
+#include "chrome/common/printing/pdf_to_pwg_raster_converter.mojom.h"
+#endif
 #endif
 
 #if BUILDFLAG(ENABLE_MOJO_MEDIA)
@@ -3003,6 +3006,12 @@ void ChromeContentBrowserClient::RegisterOutOfProcessServices(
 #if BUILDFLAG(ENABLE_PRINTING)
   (*services)[printing::mojom::kServiceName] =
       base::ASCIIToUTF16("PDF Compositor Service");
+
+#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
+  (*services)[printing::mojom::kUtilityServiceName] =
+      l10n_util::GetStringUTF16(IDS_UTILITY_PROCESS_PWG_RASTER_CONVERTOR_NAME);
+#endif
+
 #endif
 
   (*services)[profiling::mojom::kServiceName] =
