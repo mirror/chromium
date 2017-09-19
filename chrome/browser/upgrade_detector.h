592c2be038fd278d3ada61bb6c48449d99f82f27
spqchan <spqchan@chromium.org>
2017-09-19 14:28:56 -0700

    Provide Metrics on the Browser's Upgrade State
    Add metrics that tracks if the user's browser is outdated.
    The metric contains details on the upgrade level, which is
    represented by the upgrade detector's
    "UpgradeAnnoyanceLevel".
    
    Tests: upgrade_metrics_provider_unittest.cc
    
    Bug:766246
    Change-Id: Ic5a6f8c72a46fda24cb4b00db6380406d460d2ef

--- chrome/browser/upgrade_detector.h
+++ chrome/browser/upgrade_detector.h
@@ -29,6 +29,8 @@ class UpgradeObserver;
 class UpgradeDetector {
  public:
   // The Homeland Security Upgrade Advisory System.
+  // These values are logged in an histogram and shouldn't be renumbered or
+  // removed.
   enum UpgradeNotificationAnnoyanceLevel {
     UPGRADE_ANNOYANCE_NONE = 0,  // What? Me worry?
     UPGRADE_ANNOYANCE_LOW,       // Green.
@@ -36,6 +38,7 @@ class UpgradeDetector {
     UPGRADE_ANNOYANCE_HIGH,      // Red.
     UPGRADE_ANNOYANCE_SEVERE,    // Orange.
     UPGRADE_ANNOYANCE_CRITICAL,  // Red exclamation mark.
+    UPGRADE_ANNOYANCE_COUNT
   };
 
   // Returns the singleton implementation instance.
@@ -166,6 +169,7 @@ class UpgradeDetector {
  private:
   FRIEND_TEST_ALL_PREFIXES(AppMenuModelTest, Basics);
   FRIEND_TEST_ALL_PREFIXES(SystemTrayClientTest, UpdateTrayIcon);
+  friend class UpgradeMetricsProviderTest;
 
   // Initiates an Idle check. See IdleCallback below.
   void CheckIdle();
