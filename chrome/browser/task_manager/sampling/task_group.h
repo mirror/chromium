c9f4bc3c9dbf212530a3d434d8af34f63fd482a9
Nick Carter <nick@chromium.org>
2017-09-19 12:52:44 -0700

    Fix TaskManager selection bug by supporting atomic add/remove in TaskManagerImpl
    Bug 527455 is a longstanding class of issues where selection in the
    TaskManager not always preserved when a tab navigates cross-process.
    
    The problem was that selection only existed at the TableView layer,
    and the sort order was determined by the TaskManagerImpl layer,
    with the TaskManagerTableModel, sandwiched between the other two,
    only seeing a series of add/remove operations.
    
    In the worst case, the TaskManagerImpl could decide to completely
    reorder its rows, in which case the TaskManagerTableModel wouldn't
    correctly adjust the permutation, potentially randomizing the
    state of the ListSelectionModel.
    
    There are three intertangled things happening in this change:
    
     - Move responsibility for maintaining a "model order" sorting
       from TaskManagerImpl to TaskManagerTableModel. This is
       a more natural arrangement, since ordering is essentially the
       main value-add of TaskManagerTableModel. No other
       consumers ever cared about the logical ordering of tasks. The
       TaskManagerImpl still helps by providing a stable sort
       key (which it can store in the TaskGroup directly) that
       translates to the logical ordering. By requiring
       TaskManagerTableModel to maintain its own list of tasks
       sorted according to that key, it is able to emit a
       coherent stream indices to feed to TableModelObserver events.
    
       The newly implemented model order is very similar to,
       but not quite the same as, the old ordering. The difference
       stems from the fact that once placed into the order, a
       process won't move, unless all of its tasks are removed and
       re-registered. For example, if a process starts out as
       a subframe process, it'll be placed in the table order just
       below the process for its main frame. If a second tab
       does a main-frame navigation to the subframe process,
       we used to move the existing subframe row to the "natural"
       spot for the tab #2; now, we'll just insert tab #2
       wherever subframe #1 was. The result is a more stable
       ordering.
    
     - Since TaskManagerImpl no longer needs to supply
       a model-order sorted_task_ids_ list, TaskManagerImpl::GetTaskIdsList()
       now returns all tasks in task_id order, which allows a different
       organization of the primary data structures of TaskManagerImpl.
       As an additional refinement, the treatment of "pid zero" TaskGroups
       changes slightly: a Task with a null PID always gets its own
       TaskGroup, and such task groups do not show up in
       task_groups_by_proc_id_. This allows the temporary (pid-zero)
       task to show up in the right row for the model, even if
       two tabs are in this process-launching state simultaneously.
    
     - TaskManagerObserver is amended to support the atomic Replace (i.e., add/remove)
       operation, and the TaskManagerImpl forwards through such events when
       emitted by the TaskProviderObserver. In the TaskManagerTableModel,
       TaskManagerObserver::TaskReplaced() are translated to
       TableModelObserver::OnItemsMoved() operations. The semantic
       information of cross-process navigation is now preserved
       end-to-end through five interfaces:
    
         WebContentsObserver::RenderFrameHostChanged(RFH*, RFH*)
         TaskProviderObserver::TaskReplaced(Task*, Task*) ->
         TaskManagerObserver::OnTaskReplaced(TaskId, TaskId) ->
         TableModelObserver::OnItemsMoved(int, int, int)
         ListSelectionModel::Move(int, int, int)
    
       Thereby fixing bug 527455, hopefully once and for all.
    
    A couple end-to-end browser tests are added (previously,
    they'd fail) that validate the new behavior.
    
    Bug: 527455
    Change-Id: Ic91180cfc011b28bc8a1a4b44a8254ef0976ef8f

--- chrome/browser/task_manager/sampling/task_group.h
+++ chrome/browser/task_manager/sampling/task_group.h
@@ -19,6 +19,7 @@
 #include "build/build_config.h"
 #include "chrome/browser/task_manager/providers/task.h"
 #include "chrome/browser/task_manager/sampling/task_group_sampler.h"
+#include "chrome/browser/task_manager/task_manager_interface.h"
 #include "chrome/browser/task_manager/task_manager_observer.h"
 
 namespace gpu {
@@ -33,9 +34,13 @@ class SharedSampler;
 // process. This class lives on the UI thread.
 class TaskGroup {
  public:
+  // A key by which TaskGroups may be sorted.
+  using SortKey = TaskManagerInterface::ProcessSortKey;
+
   TaskGroup(
       base::ProcessHandle proc_handle,
       base::ProcessId proc_id,
+      const SortKey& sort_key,
       const base::Closure& on_background_calculations_done,
       const scoped_refptr<SharedSampler>& shared_sampler,
       const scoped_refptr<base::SequencedTaskRunner>& blocking_pool_runner);
@@ -63,6 +68,10 @@ class TaskGroup {
 
   const base::ProcessHandle& process_handle() const { return process_handle_; }
   const base::ProcessId& process_id() const { return process_id_; }
+
+  const TaskManagerInterface::ProcessSortKey& sort_key() const {
+    return sort_key_;
+  }
 
   const std::vector<Task*>& tasks() const { return tasks_; }
   size_t num_tasks() const { return tasks().size(); }
@@ -136,8 +145,11 @@ class TaskGroup {
   void OnBackgroundRefreshTypeFinished(int64_t finished_refresh_type);
 
   // The process' handle and ID.
-  base::ProcessHandle process_handle_;
-  base::ProcessId process_id_;
+  const base::ProcessHandle process_handle_;
+  const base::ProcessId process_id_;
+
+  // The key that controls the model ordering of this group.
+  const SortKey sort_key_;
 
   // This is a callback into the TaskManagerImpl to inform it that the
   // background calculations for this TaskGroup has finished.
