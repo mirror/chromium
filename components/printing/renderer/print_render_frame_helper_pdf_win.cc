22b4af01e1ef2e2f3b781648e22c760871f1f4d9
Wei Li <weili@chromium.org>
2017-09-18 23:31:40 -0700

    Enable returning different doc types for printing
    To support OOPIF printing, we need to return document in different
    format than PDF. Meanwhile, PDF format is still needed for printing a
    PDF document, which is rendered in a plugin process. So this change list
    adds the message field to return the document type, also we simplify the
    process by using a uniform data returning function.
    
    BUG=455764
    
    Change-Id: I056bd7c47a57cb36b11466649895fcac6954d7b9

--- components/printing/renderer/print_render_frame_helper_pdf_win.cc
+++ components/printing/renderer/print_render_frame_helper_pdf_win.cc
@@ -42,12 +42,13 @@ bool PrintRenderFrameHelper::PrintPagesNative(blink::WebLocalFrame* frame,
 
   PrintHostMsg_DidPrintPage_Params printed_page_params;
   if (!CopyMetafileDataToSharedMem(metafile,
-                                   &printed_page_params.metafile_data_handle)) {
+                                   &printed_page_params.metafile_data_handle,
+                                   &printed_page_params.data_size,
+                                   &printed_page_params.printed_doc_type)) {
     return false;
   }
 
   printed_page_params.content_area = params.params.printable_area;
-  printed_page_params.data_size = metafile.GetDataSize();
   printed_page_params.document_cookie = params.params.document_cookie;
   printed_page_params.page_size = params.params.page_size;
 
