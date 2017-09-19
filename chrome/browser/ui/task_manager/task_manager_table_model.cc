256584e8dca9a45c244ec3e327e5ac198fff4639
Nick Carter <nick@chromium.org>
2017-09-19 12:52:44 -0700

    Fix TaskManager selection bug by supporting atomic add/remove in TaskManagerImpl
    Bug 527455, is a longstanding class of issues where selection in the
    TaskManager not always preserved when a tab navigates cross-process.
    
    The problem was that selection only existed at the TableView layer,
    and the sort order was determined by the TaskManagerImpl layer,
    with the TaskManagerTableModel, sandwiched between the other two,
    only seeing a series of add/remove operations.
    
    In the worst case, the TaskManagerImpl could decide to completely
    reorder its rows, in which case the TaskManagerTableModel wouldn't
    correctly adjust the permutation, potentially randomizing the
    state of the ListSelectionModel.
    
    There are three intertangled things happening in this change:
    
     - Move responsibility for maintaining a "model order" sorting
       from TaskManagerImpl to TaskManagerTableModel. This is
       a more natural arrangement, since ordering is essentially the
       main value-add of TaskManagerTableModel. No other
       consumers ever cared about the logical ordering of tasks. The
       TaskManagerImpl still helps by providing a stable sort
       key (which it can store in the TaskGroup directly) that
       translates to the logical ordering. By requiring
       TaskManagerTableModel to maintain its own list of tasks
       sorted according to that key, it is able to emit a
       coherent stream indices to feed to TableModelObserver events.
    
       The newly implemented model order is very similar to,
       but not quite the same as, the old ordering. The difference
       stems from the fact that once placed into the order, a
       process won't move, unless all of its tasks are removed and
       re-registered. For example, if a process starts out as
       a subframe process, it'll be placed in the table order just
       below the process for its main frame. If a second tab
       does a main-frame navigation to the subframe process,
       we used to move the existing subframe row to the "natural"
       spot for the tab #2; now, we'll just insert tab #2
       wherever subframe #1 was. The result is a more stable
       ordering.
    
     - Since TaskManagerImpl no longer needs to supply
       a model-order sorted_task_ids_ list, TaskManagerImpl::GetTaskIdsList()
       now returns all tasks in task_id order, which allows a different
       organization of the primary data structures of TaskManagerImpl.
       As an additional refinement, the treatment of "pid zero" TaskGroups
       changes slightly: a Task with a null PID always gets its own
       TaskGroup, and such task groups do not show up in
       task_groups_by_proc_id_. This allows the temporary (pid-zero)
       task to show up in the right row for the model, even if
       two tabs are in this process-launching state simultaneously.
    
     - TaskManagerObserver is amended to support the atomic Replace (i.e., add/remove)
       operation, and the TaskManagerImpl forwards through such events when
       emitted by the TaskProviderObserver. In the TaskManagerTableModel,
       TaskManagerObserver::TaskReplaced() are translated to
       TableModelObserver::OnItemsMoved() operations. The semantic
       information of of cross-process navigation is now preserved
       end-to-end through five interfaces:
    
         WebContentsObserver::RenderFrameHostChanged(RFH*, RFH*)
         TaskProviderObserver::TaskReplaced(Task*, Task*) ->
         TaskManagerObserver::OnTaskReplaced(TaskId, TaskId) ->
         TableModelObserver::OnItemsMoved(int, int, int)
         ListSelectionModel::Move(int, int, int)
    
       Thereby fixing bug 527455, hopefully once and for all.
    
    A couple end-to-end browser tests are added (previously,
    they'd fail) that validate the new behavior.
    
    Bug: 527455
    Change-Id: Ic91180cfc011b28bc8a1a4b44a8254ef0976ef8f

--- chrome/browser/ui/task_manager/task_manager_table_model.cc
+++ chrome/browser/ui/task_manager/task_manager_table_model.cc
@@ -659,34 +659,57 @@ void TaskManagerTableModel::GetRowsGroupRange(int row_index,
 }
 
 void TaskManagerTableModel::OnTaskAdded(TaskId id) {
-  // For the table view scrollbar to behave correctly we must inform it that
-  // a new task has been added.
-
-  // We will get a newly sorted list from the task manager as opposed to just
-  // adding |id| to |tasks_| because we want to keep |tasks_| sorted by proc IDs
-  // and then by Task IDs.
-  tasks_ = observed_task_manager()->GetTaskIdsList();
+  int index = FindInsertionRow(id);
+  tasks_.insert(tasks_.begin() + index, id);
 
   if (table_model_observer_) {
-    std::vector<TaskId>::difference_type index =
-        std::find(tasks_.begin(), tasks_.end(), id) - tasks_.begin();
-    table_model_observer_->OnItemsAdded(static_cast<int>(index), 1);
+    table_model_observer_->OnItemsAdded(index, 1);
   }
 }
 
 void TaskManagerTableModel::OnTaskToBeRemoved(TaskId id) {
-  auto index = std::find(tasks_.begin(), tasks_.end(), id);
-  if (index == tasks_.end())
-    return;
-  auto removed_index = index - tasks_.begin();
-  tasks_.erase(index);
+  int index = FindExistingRow(id);
+  DCHECK(index != -1);
+  tasks_.erase(tasks_.begin() + index);
   if (table_model_observer_)
-    table_model_observer_->OnItemsRemoved(removed_index, 1);
+    table_model_observer_->OnItemsRemoved(index, 1);
+}
+
+void TaskManagerTableModel::OnTaskReplaced(TaskId old_task_id,
+                                           TaskId new_task_id) {
+  int old_index = FindExistingRow(old_task_id);
+  int new_index = FindInsertionRow(new_task_id);
+  DCHECK(old_index != -1);
+
+  tasks_[old_index] = new_task_id;
+  if (new_index > old_index)
+    new_index--;  // Adjust for effect of removal.
+
+  if (new_index < old_index)
+    MoveTasks(old_index, 1, new_index);
+  else if (new_index > old_index)
+    MoveTasks(old_index + 1, new_index - old_index, old_index);
+
+  if (table_model_observer_) {
+    table_model_observer_->OnItemsChanged(new_index, 1);
+  }
+}
+
+void TaskManagerTableModel::MoveTasks(int old_index,
+                                      int length,
+                                      int new_index) {
+  DCHECK(new_index < old_index);  // Only support left-rotation.
+  std::rotate(tasks_.begin() + new_index, tasks_.begin() + old_index,
+              tasks_.begin() + old_index + length);
+
+  if (table_model_observer_) {
+    table_model_observer_->OnItemsMoved(old_index, length, new_index);
+  }
 }
 
 void TaskManagerTableModel::OnTasksRefreshed(
     const TaskIdList& task_ids) {
-  tasks_ = task_ids;
+  DCHECK(task_ids.size() == tasks_.size());
   OnRefresh();
 }
 
@@ -900,12 +923,8 @@ void TaskManagerTableModel::ToggleColumnVisibility(int column_id) {
 
 int TaskManagerTableModel::GetRowForWebContents(
     content::WebContents* web_contents) {
-  TaskId task_id =
-      observed_task_manager()->GetTaskIdForWebContents(web_contents);
-  auto index = std::find(tasks_.begin(), tasks_.end(), task_id);
-  if (index == tasks_.end())
-    return -1;
-  return static_cast<int>(index - tasks_.begin());
+  return FindExistingRow(
+      observed_task_manager()->GetTaskIdForWebContents(web_contents));
 }
 
 void TaskManagerTableModel::StartUpdating() {
@@ -931,11 +950,42 @@ void TaskManagerTableModel::OnRefresh() {
 }
 
 bool TaskManagerTableModel::IsTaskFirstInGroup(int row_index) const {
+  // Group rows together if they're adjacent and both have the same
+  // valid process ID. Do not group rows together with a null PID, since
+  // those rows reflect activities that that are still launching.
   if (row_index == 0)
     return true;
 
-  return observed_task_manager()->GetProcessId(tasks_[row_index - 1]) !=
-      observed_task_manager()->GetProcessId(tasks_[row_index]);
+  auto row_pid = observed_task_manager()->GetProcessId(tasks_[row_index]);
+  auto prev_pid = observed_task_manager()->GetProcessId(tasks_[row_index - 1]);
+  return row_pid != prev_pid || row_pid == base::kNullProcessId;
+}
+
+int TaskManagerTableModel::FindInsertionRow(TaskId id) {
+  TaskManagerInterface* tm = observed_task_manager();
+  // First, find the range items for the sort key of this task's process.
+  auto primary_ordering = [tm](TaskId a, TaskId b) {
+    return tm->GetProcessSortKey(a) < tm->GetProcessSortKey(b);
+  };
+  auto process_range =
+      std::equal_range(tasks_.begin(), tasks_.end(), id, primary_ordering);
+
+  // Among the rows for this process, put the subframe tasks last, then order by
+  // tab ID.
+  auto secondary_ordering = [tm](TaskId a, TaskId b) {
+    return std::make_tuple(tm->HasParentTask(a), tm->GetTabId(a)) <
+           std::make_tuple(tm->HasParentTask(b), tm->GetTabId(b));
+  };
+  auto insertion_point = std::upper_bound(
+      process_range.first, process_range.second, id, secondary_ordering);
+  return base::checked_cast<int>(insertion_point - tasks_.begin());
+}
+
+int TaskManagerTableModel::FindExistingRow(TaskId task_id) {
+  auto index = std::find(tasks_.begin(), tasks_.end(), task_id);
+  if (index == tasks_.end())
+    return -1;
+  return base::checked_cast<int>(index - tasks_.begin());
 }
 
 }  // namespace task_manager
