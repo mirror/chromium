92313ea20406196748c4b0510cca5b31a8f10aef
Yuwei Huang <yuweih@chromium.org>
2017-09-18 17:23:18 -0700

    [Fullscreen Control] Finish up the fullscreen control UI and add the animation class
    This CL finishes up the fullscreen control UI. It is a transparent-black
    circular button with a close (X) icon on the middle. It is used both as an
    indicator for exiting fullscreen when the keyboard lock reserves the ESC
    key and an actual button to exit fullscreen for touch devices.
    
    This CL also implements a FullscreenControlPopup class to show the
    indicator with a dropdown animation. We could potentially switch to use
    DropdownBarHost to show the animation once MacViews is ready.
    
    This is the UX spec (Google internal):
    https://docs.google.com/presentation/d/11D86S87NZK5sAMLhc8WSV9pk_VWkjMAjvnN08uUD5po/edit#slide=id.g23756e43b7_3_0
    
    Bug: 758456
    Change-Id: I44dc2d0944386f5f6f5e607831c5ea4e38364357

---
+++ chrome/browser/ui/views/fullscreen_control/fullscreen_control_popup.h
@@ -0,0 +1,72 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_FULLSCREEN_CONTROL_FULLSCREEN_CONTROL_POPUP_H_
+#define CHROME_BROWSER_UI_VIEWS_FULLSCREEN_CONTROL_FULLSCREEN_CONTROL_POPUP_H_
+
+#include <memory>
+
+#include "base/callback_forward.h"
+#include "base/macros.h"
+#include "ui/gfx/animation/animation_delegate.h"
+#include "ui/gfx/animation/slide_animation.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/native_widget_types.h"
+
+namespace views {
+class Widget;
+}  // namespace views
+
+class FullscreenControlView;
+
+// FullscreenControlPopup is a helper class that holds a FullscreenControlView
+// and allow showing and hiding the view with a drop down animation.
+//
+// TODO(yuweih): We might be able to use the DropdownBarHost for the animation
+// once Mac starts using views. In that case we will need to change the
+// animation curve and hook the opacity animation into DropdownBarHost.
+class FullscreenControlPopup : public gfx::AnimationDelegate {
+ public:
+  FullscreenControlPopup(gfx::NativeView parent_view,
+                         const base::RepeatingClosure& on_button_pressed);
+  ~FullscreenControlPopup() override;
+
+  // Shows the indicator with an animation that drops it off the top of
+  // |parent_view|.
+  // parent_bounds_in_screen: The bounds of |parent_view| in the reference frame
+  // of the screen.
+  void Show(const gfx::Rect& parent_bounds_in_screen);
+
+  // Hides the indicator. If |animated| is true, the indicator will be hidden by
+  // the reversed animation of Show(), i.e. the indicator flies to the top of
+  // |parent_widget|.
+  void Hide(bool animated);
+
+  // Returns the final bounds of the control view when it is fully shown.
+  // Returns empty bounds if the popup is not visible.
+  gfx::Rect GetFinalBounds() const;
+
+  bool IsAnimating() const;
+  bool IsVisible() const;
+
+  FullscreenControlView* fullscreen_control_view() { return control_view_; }
+
+ private:
+  // gfx::AnimationDelegate:
+  void AnimationProgressed(const gfx::Animation* animation) override;
+  void AnimationEnded(const gfx::Animation* animation) override;
+
+  gfx::Rect CalculateBounds(int y_offset) const;
+
+  FullscreenControlView* const control_view_;
+  std::unique_ptr<views::Widget> const popup_;
+  std::unique_ptr<gfx::SlideAnimation> const animation_;
+
+  // The bounds is empty when the popup is not showing.
+  gfx::Rect parent_bounds_in_screen_;
+
+  DISALLOW_COPY_AND_ASSIGN(FullscreenControlPopup);
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_FULLSCREEN_CONTROL_FULLSCREEN_CONTROL_POPUP_H_
