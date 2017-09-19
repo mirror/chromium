9c50fa8fd54f13c496a8f9758a81c772e962f7e9
Tsuyoshi Horo <horo@chromium.org>
2017-09-19 15:47:59 +0900

    [not for review] CKECK to find redirecting tests
    Bug:
    Change-Id: I5309dd23e087f8774e887893a9dd16e806e30d88

--- content/browser/service_worker/service_worker_url_request_job.cc
+++ content/browser/service_worker/service_worker_url_request_job.cc
@@ -682,6 +682,7 @@ void ServiceWorkerURLRequestJob::DidDispatchFetchEvent(
     blink::mojom::ServiceWorkerStreamHandlePtr body_as_stream,
     storage::mojom::BlobPtr body_as_blob,
     const scoped_refptr<ServiceWorkerVersion>& version) {
+  CHECK(!net::HttpResponseHeaders::IsRedirectResponseCode(response.status_code));
   // Do not clear |fetch_dispatcher_| if it has dispatched a navigation preload
   // request to keep the mojom::URLLoader related objects in it, because the
   // preload response might still need to be streamed even after calling
