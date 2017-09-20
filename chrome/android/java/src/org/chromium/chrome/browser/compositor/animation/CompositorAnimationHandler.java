18f3515c3f3d0efc83cc21ebab58e8327837948e
Matthew Jones <mdjones@chromium.org>
2017-09-20 15:47:50 -0700

    Replace ChromeAnimation instances in Contextual Search
    This change replaces some of the instances of ChromeAnimation in the
    Contextual Search feature with CompositorAnimators.
    
    This change also adds the ability to put the CompositorAnimationHandler
    into 'testing mode' where all started animations are immediately ended.
    
    BUG=750381
    
    Change-Id: I2319679605395ff8a8f14b4844c8cac0a2546133

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/animation/CompositorAnimationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/animation/CompositorAnimationHandler.java
@@ -35,6 +35,9 @@ public class CompositorAnimationHandler {
      */
     private boolean mWasUpdateRequestedForAnimationStart;
 
+    /** Whether or not testing mode is enabled. In this mode, animations end immediately. */
+    private boolean mIsInTestingMode;
+
     /**
      * Default constructor.
      * @param host A {@link LayoutUpdateHost} responsible for requesting frames when an animation
@@ -63,6 +66,9 @@ public class CompositorAnimationHandler {
             mUpdateHost.requestUpdate();
             mWasUpdateRequestedForAnimationStart = true;
         }
+
+        // If in testing mode, immediately push and update and end the animation.
+        if (mIsInTestingMode) pushUpdate(animator.getDuration());
     }
 
     /**
@@ -102,5 +108,13 @@ public class CompositorAnimationHandler {
     @VisibleForTesting
     public int getActiveAnimationCount() {
         return mAnimators.size();
+    }
+
+    /**
+     * Enable testing mode. This causes any animations to end immediately.
+     */
+    @VisibleForTesting
+    public void enableTestingMode() {
+        mIsInTestingMode = true;
     }
 }
