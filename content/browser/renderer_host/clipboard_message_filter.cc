5c5469ebe9b4986351e69ac9414296c8c8a28fbc
Herb Derby <herb@google.com>
2017-09-20 13:25:08 -0400

    Remove use of sk_64_isS32
    This call is being removed from the API.
    
    Change-Id: Ic1f23ff997bd6d06e6212bfb2995fe308f93d969

--- content/browser/renderer_host/clipboard_message_filter.cc
+++ content/browser/renderer_host/clipboard_message_filter.cc
@@ -292,7 +292,7 @@ void ClipboardMessageFilter::OnWriteImage(ui::ClipboardType clipboard_type,
 
   // Make sure the size is representable as a signed 32-bit int, so
   // SkBitmap::getSize() won't be truncated.
-  if (!sk_64_isS32(bitmap.computeSize64()))
+  if (!bitmap.getSafeSize())
     return;
 
   if (!bitmap_buffer->Map(bitmap.getSize()))
