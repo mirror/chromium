5e57f5ad46edcb74d6adc2226c3ca4670e87ea34
Tom Sepez <tsepez@chromium.org>
2017-09-20 16:40:05 -0700

    Introducing sandbox/policy directory.
    Future home of much of content/common/sandbox*.
    
    First step in extracting the policy logic outside of content
    so it may be used by other services.  We then move the sandbox type
    enum here, since it is just a name and doesn't imply an actual
    dependence on content.
    
    Create a top-level sandbox_unittests, replacing sandbox_mac_unittests
    and sandbox_linux_unittests.
    
    Bug: 708738
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I8ad9adc336fb1da53283b088f0c6840429407ee5

--- sandbox/sandbox_export.h
+++ sandbox/sandbox_export.h
@@ -5,10 +5,6 @@
 #ifndef SANDBOX_SANDBOX_EXPORT_H_
 #define SANDBOX_SANDBOX_EXPORT_H_
 
-#if defined(WIN32)
-#error "sandbox_export.h does not support WIN32."
-#endif
-
 #if defined(COMPONENT_BUILD)
 
 #if defined(SANDBOX_IMPLEMENTATION)
