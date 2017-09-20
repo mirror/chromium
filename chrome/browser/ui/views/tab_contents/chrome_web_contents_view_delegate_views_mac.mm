cc4d22b19964f4cdac53c426b113c9afe46c2744
jongkwon.lee <jongkwon.lee@navercorp.com>
2017-09-20 10:44:18 +0900

    Support MacViews focus in WebContentsViewMac
    WebContentsViewMac was handling focus related jobs for Cocoa only.
    To support MacViewsBrowser, WebContentsViewMac now delegates focus
    related jobs to its |delegate_|.
    
    Bug: 710506
    Change-Id: I9969c92bdf9e3d17ce185ecf4bfb67e165bea41e

--- chrome/browser/ui/views/tab_contents/chrome_web_contents_view_delegate_views_mac.mm
+++ chrome/browser/ui/views/tab_contents/chrome_web_contents_view_delegate_views_mac.mm
@@ -6,13 +6,21 @@
 
 #include "chrome/browser/ui/sad_tab_helper.h"
 #include "chrome/browser/ui/views/sad_tab_view.h"
+#include "chrome/browser/ui/views/tab_contents/chrome_web_contents_view_focus_helper.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/ui_features.h"
+#include "ui/views/focus/focus_manager.h"
+#include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
 
 ChromeWebContentsViewDelegateViewsMac::ChromeWebContentsViewDelegateViewsMac(
     content::WebContents* web_contents)
-    : ChromeWebContentsViewDelegateMac(web_contents) {}
+    : ChromeWebContentsViewDelegateMac(web_contents),
+      focus_helper_(
+          base::MakeUnique<ChromeWebContentsViewFocusHelper>(web_contents)) {}
+
+ChromeWebContentsViewDelegateViewsMac::
+    ~ChromeWebContentsViewDelegateViewsMac() {}
 
 void ChromeWebContentsViewDelegateViewsMac::SizeChanged(const gfx::Size& size) {
   SadTabHelper* sad_tab_helper = SadTabHelper::FromWebContents(web_contents());
@@ -21,6 +29,18 @@ void ChromeWebContentsViewDelegateViewsMac::SizeChanged(const gfx::Size& size) {
   SadTabView* sad_tab = static_cast<SadTabView*>(sad_tab_helper->sad_tab());
   if (sad_tab)
     sad_tab->GetWidget()->SetBounds(gfx::Rect(size));
+}
+
+bool ChromeWebContentsViewDelegateViewsMac::Focus() {
+  return focus_helper_->Focus();
+}
+
+void ChromeWebContentsViewDelegateViewsMac::StoreFocus() {
+  focus_helper_->StoreFocus();
+}
+
+void ChromeWebContentsViewDelegateViewsMac::RestoreFocus() {
+  focus_helper_->RestoreFocus();
 }
 
 #if BUILDFLAG(MAC_VIEWS_BROWSER)
