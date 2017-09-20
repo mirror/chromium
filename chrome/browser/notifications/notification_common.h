c8a63a890cf047ab0fee61f24807bd15cdd4483b
Evan Stade <estade@chromium.org>
2017-09-20 12:06:17 -0600

    Move Notification::service_worker_scope into a new notification metadata class.
    It seems confusing to let Notification become cluttered by extra fields
    that are type specific.
    
    Bug: 763426
    Change-Id: I202f10cca7b2637fd696897616cadf1f0c44e93f

--- chrome/browser/notifications/notification_common.h
+++ chrome/browser/notifications/notification_common.h
@@ -6,6 +6,7 @@
 #define CHROME_BROWSER_NOTIFICATIONS_NOTIFICATION_COMMON_H_
 
 #include "base/feature_list.h"
+#include "url/gurl.h"
 
 namespace features {
 
@@ -20,6 +21,7 @@ class BrowserContext;
 
 class GURL;
 class Profile;
+struct PersistentNotificationMetadata;
 
 // Shared functionality for both in page and persistent notification
 class NotificationCommon {
@@ -42,6 +44,12 @@ class NotificationCommon {
     TYPE_MAX = EXTENSION
   };
 
+  // A struct that contains extra data about a notification specific to one of
+  // the above types.
+  struct Metadata {
+    Type type;
+  };
+
   // Open the Notification settings screen when clicking the right button.
   // TODO(miguelg) have it take a Profile instead once NotificationObjectProxy
   // is updated.
@@ -51,6 +59,15 @@ class NotificationCommon {
   // Whether a web notification should be displayed when chrome is in full
   // screen mode.
   static bool ShouldDisplayOnFullScreen(Profile* profile, const GURL& origin);
+};
+
+// Metadata for PERSISTENT notifications.
+struct PersistentNotificationMetadata : public NotificationCommon::Metadata {
+  PersistentNotificationMetadata();
+
+  static const PersistentNotificationMetadata* From(const Metadata* metadata);
+
+  GURL service_worker_scope;
 };
 
 #endif  // CHROME_BROWSER_NOTIFICATIONS_NOTIFICATION_COMMON_H_
