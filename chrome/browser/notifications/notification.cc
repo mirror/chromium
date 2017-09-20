ad7bfdb0254d81c7a50da89c481d35b75b330f56
Evan Stade <estade@chromium.org>
2017-09-20 12:41:24 -0600

    Move Notification::service_worker_scope into a new notification metadata class.
    It seems confusing to let Notification become cluttered by extra fields
    that are type specific.
    
    Bug: 763426
    Change-Id: I202f10cca7b2637fd696897616cadf1f0c44e93f

--- chrome/browser/notifications/notification.cc
+++ chrome/browser/notifications/notification.cc
@@ -33,13 +33,11 @@ Notification::Notification(const std::string& id,
                            const Notification& notification)
     : message_center::Notification(id, notification),
       tag_(notification.tag()),
-      service_worker_scope_(notification.service_worker_scope()),
       delegate_(notification.delegate_) {}
 
 Notification::Notification(const Notification& notification)
     : message_center::Notification(notification),
       tag_(notification.tag()),
-      service_worker_scope_(notification.service_worker_scope()),
       delegate_(notification.delegate_) {}
 
 Notification::~Notification() {}
@@ -47,7 +45,6 @@ Notification::~Notification() {}
 Notification& Notification::operator=(const Notification& notification) {
   message_center::Notification::operator=(notification);
   tag_ = notification.tag();
-  service_worker_scope_ = notification.service_worker_scope();
   delegate_ = notification.delegate_;
   return *this;
 }
