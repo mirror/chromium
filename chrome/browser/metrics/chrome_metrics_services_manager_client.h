92f9bf04a9e99699a7f890e3ab6db6100560b802
Robert Kaplow <rkaplow@chromium.org>
2017-09-20 17:25:46 -0400

    typo
    UKM sampling
    
    Bug:
    Change-Id: Ibea39c5a94c36d8c7c67672ee7fc380f50fa7acb

--- chrome/browser/metrics/chrome_metrics_services_manager_client.h
+++ chrome/browser/metrics/chrome_metrics_services_manager_client.h
@@ -22,6 +22,7 @@ class PrefService;
 namespace metrics {
 class EnabledStateProvider;
 class MetricsStateManager;
+extern const base::Feature kMetricsReportingFeature;
 }
 
 namespace version_info {
@@ -72,6 +73,7 @@ class ChromeMetricsServicesManagerClient
   CreateEntropyProvider() override;
   net::URLRequestContextGetter* GetURLRequestContext() override;
   bool IsMetricsReportingEnabled() override;
+  bool IsMetricsReportingEnabledIgnoringFeature() override;
 
 #if defined(OS_WIN)
   // On Windows, the client controls whether Crashpad can upload crash reports.
