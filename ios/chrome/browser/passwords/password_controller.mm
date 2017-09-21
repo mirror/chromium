bc4965847468487ae7e1e19b3b882cd02fde993d
Tomasz Garbus <tgarbus@chromium.org>
2017-09-21 14:29:19 +0200

    (WIP)[UI] Notification saying "Signing in as ..." + EG test case
    This CL introduces a notification view appearing on the bottom
    of the screen when user is automatically logged in. It also
    includes an EG test for this behavior.
    
    Here is a screenshot with the notification:
    https://screenshot.googleplex.com/267O2QU93YA.png
    
    iPhone X screenshots:
    vertical: https://screenshot.googleplex.com/0bitXQFqbR2.png
    horizontal: https://screenshot.googleplex.com/v0i7GbVwdBL.png
    
    Bug: 435048
    Change-Id: Ic871148a970c36e8ce89922319720637a54cabbc

--- ios/chrome/browser/passwords/password_controller.mm
+++ ios/chrome/browser/passwords/password_controller.mm
@@ -14,11 +14,14 @@
 
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
+#include "base/mac/bind_objc_block.h"
 #include "base/mac/foundation_util.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string16.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "base/values.h"
 #include "components/autofill/core/common/password_form.h"
 #include "components/autofill/core/common/password_form_fill_data.h"
@@ -38,6 +41,7 @@
 #import "ios/chrome/browser/passwords/ios_chrome_save_password_infobar_delegate.h"
 #import "ios/chrome/browser/passwords/ios_chrome_update_password_infobar_delegate.h"
 #import "ios/chrome/browser/passwords/js_password_manager.h"
+#import "ios/chrome/browser/passwords/notify_auto_signin_view_controller.h"
 #import "ios/chrome/browser/passwords/password_form_filler.h"
 #import "ios/chrome/browser/passwords/password_generation_agent.h"
 #include "ios/chrome/browser/sync/ios_chrome_profile_sync_service_factory.h"
@@ -61,6 +65,9 @@ using password_manager::PasswordManagerDriver;
 namespace {
 // Types of password infobars to display.
 enum class PasswordInfoBarType { SAVE, UPDATE };
+
+// Duration for notify user auto-sign in dialog being displayed.
+constexpr int kNotifyAutoSigninDuration = 3;  // seconds
 }
 
 @interface PasswordController ()
@@ -275,12 +282,19 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
   // Bridge to observe WebState from Objective-C.
   std::unique_ptr<web::WebStateObserverBridge> webStateObserverBridge_;
 
+  // Timer for hiding "Signing in as ..." notification.
+  base::OneShotTimer notifyAutoSigninTimer_;
+
   // True indicates that a request for credentials has been sent to the password
   // store.
   BOOL sent_request_to_store_;
 }
 
 @synthesize isWebStateDestroyed = isWebStateDestroyed_;
+
+@synthesize delegate = delegate_;
+
+@synthesize notifyAutoSigninViewController = notifyAutoSigninViewController_;
 
 - (instancetype)initWithWebState:(web::WebState*)webState
              passwordsUiDelegate:(id<PasswordsUiDelegate>)delegate {
@@ -404,6 +418,11 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
 
 #pragma mark -
 #pragma mark CRWWebStateObserver
+
+// If Tab was hidden, hide auto sign-in notification.
+- (void)webStateWasHidden:(web::WebState*)webState {
+  [self hideAutosigninNotification];
+}
 
 - (void)webState:(web::WebState*)webState didLoadPageWithSuccess:(BOOL)success {
   // Clear per-page state.
@@ -703,6 +722,44 @@ bool GetPageURLAndCheckTrustLevel(web::WebState* web_state, GURL* page_url) {
     (std::unique_ptr<PasswordFormManager>)formToUpdate {
   [self showInfoBarForForm:std::move(formToUpdate)
                infoBarType:PasswordInfoBarType::UPDATE];
+}
+
+// Hides auto sign-in notification. Removes the view from superview and destroys
+// the controller.
+// TODO(crbug.com/435048): Animate disappearance.
+- (void)hideAutosigninNotification {
+  [notifyAutoSigninViewController_ willMoveToParentViewController:nil];
+  [notifyAutoSigninViewController_.view removeFromSuperview];
+  [notifyAutoSigninViewController_ removeFromParentViewController];
+  notifyAutoSigninViewController_ = nil;
+}
+
+// Shows auto sign-in notification and schedules hiding it after 3 seconds.
+// TODO(crbug.com/435048): Animate appearance.
+- (void)showAutosigninNotification:
+    (std::unique_ptr<autofill::PasswordForm>)formSignedIn {
+  if (!webStateObserverBridge_ || !webStateObserverBridge_->web_state())
+    return;
+
+  // If a notification is already being displayed, hides the old one, then shows
+  // the new one.
+  if (notifyAutoSigninViewController_) {
+    notifyAutoSigninTimer_.Stop();
+    [self hideAutosigninNotification];
+  }
+
+  // Creates view controller then shows the subview.
+  notifyAutoSigninViewController_ = [[NotifyUserAutoSigninViewController alloc]
+      initWithUserId:base::SysUTF16ToNSString(formSignedIn->username_value)];
+  [delegate_ displaySignInNotification:notifyAutoSigninViewController_];
+
+  // Hides notification after 3 seconds.
+  __weak PasswordController* weakSelf = self;
+  notifyAutoSigninTimer_.Start(
+      FROM_HERE, base::TimeDelta::FromSeconds(kNotifyAutoSigninDuration),
+      base::BindBlockArc(^{
+        [weakSelf hideAutosigninNotification];
+      }));
 }
 
 #pragma mark -
