5f77d212391068d56cf541a45f047c46f85139d7
Tomasz Garbus <tgarbus@chromium.org>
2017-09-21 13:02:52 +0200

    (WIP)[UI] Notification saying "Signing in as ..." + EG test case
    This CL introduces a notification view appearing on the bottom
    of the screen when user is automatically logged in. It also
    includes an EG test for this behavior.
    
    Here is a screenshot with the notification:
    https://screenshot.googleplex.com/267O2QU93YA.png
    
    iPhone X screenshots:
    vertical: https://screenshot.googleplex.com/ORrB6N7HsFy.png
    horizontal: https://screenshot.googleplex.com/qapmtAhwLPY.png
    
    Bug: 435048
    Change-Id: Ic871148a970c36e8ce89922319720637a54cabbc

--- ios/chrome/browser/passwords/password_tab_helper.h
+++ ios/chrome/browser/passwords/password_tab_helper.h
@@ -13,6 +13,7 @@
 @protocol FormInputAccessoryViewProvider;
 @protocol FormSuggestionProvider;
 @class PasswordController;
+@protocol PasswordControllerDelegate;
 @protocol PasswordFormFiller;
 @protocol PasswordsUiDelegate;
 
@@ -47,6 +48,8 @@ class PasswordTabHelper : public web::WebStateObserver,
 
   // Returns the PasswordGenerationManager owned by the PasswordController.
   password_manager::PasswordGenerationManager* GetPasswordGenerationManager();
+
+  void SetPasswordControllerDelegate(id<PasswordControllerDelegate> delegate);
 
  private:
   PasswordTabHelper(web::WebState* web_state,
