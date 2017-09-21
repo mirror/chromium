e82b3529ad31464257e8e3b4ee2422939baea12e
Nate Chapin <japhet@chromium.org>
2017-09-21 11:17:47 -0700

    Move the Resource canellation timer to ResourceLoader, where it can get a per-frame task queue
    Also, the timer in RawResource::MatchPreloads
    
    Expose the WebTaskRunner for loading on FrameFetchContext and wire it up for WorkerFetchContext,
    which enables us to get the ResourceFetcher constructor back down to one parameter and removes
    the need for thread and null checks when handling task runners in platform/loader/fetch/
    
    Bug:
    Change-Id: I36a0bd8f1a9758b51a33badf8e93a84880618600

--- third_party/WebKit/Source/platform/loader/fetch/Resource.h
+++ third_party/WebKit/Source/platform/loader/fetch/Resource.h
@@ -235,7 +235,7 @@ class PLATFORM_EXPORT Resource : public GarbageCollectedFinalized<Resource>,
 
   void MarkAsPreload();
   // Returns true if |this| resource is matched with the given parameters.
-  virtual bool MatchPreload(const FetchParameters&);
+  virtual bool MatchPreload(const FetchParameters&, WebTaskRunner*);
 
   bool CanReuseRedirectChain() const;
   bool MustRevalidateDueToCacheHeaders() const;
@@ -408,8 +408,6 @@ class PLATFORM_EXPORT Resource : public GarbageCollectedFinalized<Resource>,
   class CachedMetadataHandlerImpl;
   class ServiceWorkerResponseCachedMetadataHandler;
 
-  void CancelTimerFired(TimerBase*);
-
   void RevalidationSucceeded(const ResourceResponse&);
   void RevalidationFailed();
 
@@ -477,7 +475,6 @@ class PLATFORM_EXPORT Resource : public GarbageCollectedFinalized<Resource>,
 
   double response_timestamp_;
 
-  TaskRunnerTimer<Resource> cancel_timer_;
   TaskHandle async_finish_pending_clients_task_;
 
   ResourceRequest resource_request_;
