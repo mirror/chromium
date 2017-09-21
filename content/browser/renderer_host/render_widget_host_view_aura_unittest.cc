1d03110124ee97a879fbb64189aee00e2cf0d9cd
sahel <sahel@chromium.org>
2017-09-20 22:06:49 -0400

    Wheel end event must have its own timestamp.
    The wheel end event generated by Mouse_wheel_phase_handler used to have
    the timestamp of the last wheel event. This caused a 100ms+ latency to
    handling this event since it is released 100ms after the last wheel
    event. This cl adds proper timestamp to the synthetic wheel end event.
    
    Bug:759458
    Change-Id: Iebfe9638cb401618d178a5b4caa4d4108b1048ca

--- content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
+++ content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
@@ -1956,6 +1956,8 @@ TEST_F(RenderWidgetHostViewAuraWheelScrollLatchingEnabledTest,
   view_->OnScrollEvent(&scroll1);
   input_event = GetInputEventFromMessage(*sink_->GetMessageAt(0));
   wheel_event = static_cast<const WebMouseWheelEvent*>(input_event);
+  base::TimeTicks wheel_event_timestamp =
+      ui::EventTimeStampFromSeconds(wheel_event->TimeStampSeconds());
   EXPECT_EQ(WebMouseWheelEvent::kPhaseChanged, wheel_event->phase);
   SendInputEventACK(blink::WebInputEvent::kMouseWheel,
                     INPUT_EVENT_ACK_STATE_NOT_CONSUMED);
@@ -1967,6 +1969,7 @@ TEST_F(RenderWidgetHostViewAuraWheelScrollLatchingEnabledTest,
   EXPECT_EQ(2U, gesture_event->data.scroll_update.delta_y);
   SendInputEventACK(WebInputEvent::kGestureScrollUpdate,
                     INPUT_EVENT_ACK_STATE_CONSUMED);
+
   sink_->ClearMessages();
 
   // Let the MouseWheelPhaseHandler::mouse_wheel_end_dispatch_timer_ fire. A
@@ -1978,10 +1981,13 @@ TEST_F(RenderWidgetHostViewAuraWheelScrollLatchingEnabledTest,
   base::RunLoop().Run();
 
   input_event = GetInputEventFromMessage(*sink_->GetMessageAt(0));
-  wheel_event = static_cast<const WebMouseWheelEvent*>(input_event);
-  EXPECT_EQ(WebMouseWheelEvent::kPhaseEnded, wheel_event->phase);
-  EXPECT_EQ(0U, wheel_event->delta_x);
-  EXPECT_EQ(0U, wheel_event->delta_y);
+  const WebMouseWheelEvent* wheel_end_event =
+      static_cast<const WebMouseWheelEvent*>(input_event);
+  EXPECT_EQ(WebMouseWheelEvent::kPhaseEnded, wheel_end_event->phase);
+  EXPECT_EQ(0U, wheel_end_event->delta_x);
+  EXPECT_EQ(0U, wheel_end_event->delta_y);
+  EXPECT_GT(ui::EventTimeStampFromSeconds(wheel_end_event->TimeStampSeconds()),
+            wheel_event_timestamp);
   SendInputEventACK(blink::WebInputEvent::kMouseWheel,
                     INPUT_EVENT_ACK_STATE_NOT_CONSUMED);
 
