d7270c4d0e3bb9e770db8eb683109d5c8eb40161
Dan Beam <dbeam@chromium.org>
2017-09-20 18:53:37 -0700

    DO NOT COMMIT (WIP): Remove DirectionDelegate for a simpler implementation
    R=dpapad@chromium.org
    BUG=715509
    
    Change-Id: Ib85bfd34c5ea57ef6ee0b6fc5509a1bb0a38ac6b

--- chrome/test/data/webui/settings/rtl_tests.js
+++ chrome/test/data/webui/settings/rtl_tests.js
@@ -2,28 +2,26 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-cr.define('settings_rtl_tests', function() {
-  /** @implements {settings.DirectionDelegate} */
-  function TestDirectionDelegate(isRtl) {
-    /** @override */
-    this.isRtl = function() { return isRtl; };
-  }
+suite('settings drawer panel RTL tests', function() {
+  setup(function() {
+    PolymerTest.clearBody();
+  });
 
-  suite('settings drawer panel RTL tests', function() {
-    setup(function() {
-      PolymerTest.clearBody();
-    });
+  test('test i18n processing flips drawer panel', function() {
+    var loadTimeData = new LoadTimeData;
+    loadTimeData.data_ = {'textdirection': 'ltr'};
 
-    test('test i18n processing flips drawer panel', function() {
-      var ui = document.createElement('settings-ui');
-      var appDrawer = ui.$.drawer;
-      assertEquals('left', appDrawer.align);
+    var ui = document.createElement('settings-ui');
+    document.body.appendChild(ui);
+    Polymer.dom.flush();
+    assertEquals('left', ui.$.drawer.align);
+    ui.remove();
 
-      ui.directionDelegate = new TestDirectionDelegate(true /* isRtl */);
-      assertEquals('right', appDrawer.align);
-
-      ui.directionDelegate = new TestDirectionDelegate(false /* isRtl */);
-      assertEquals('left', appDrawer.align);
-    });
+    loadTimeData.overrideValues({'textdirection': 'rtl'});
+    ui = document.createElement('settings-ui');
+    document.body.appendChild(ui);
+    Polymer.dom.flush();
+    assertEquals('right', ui.$.drawer.align);
+    ui.remove();
   });
 });
