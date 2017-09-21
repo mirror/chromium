fc1576c1cef6ecc7d0155547050e7a610a884a5d
Thanh Le <thanhdle@chromium.org>
2017-09-21 15:38:13 -0700

    Use mojo to communicate between Javascript and C++.
    Displays basic previews status on chrome://interventions-internals. Right now it
    is only displaying Offline Previews' status. Functionalities and tests for C++ code will be
    in future CLs.
    
    Added tests for the basic JavaScript method for displaying previews'
    enabled/disabled status.
    
    Bug: 764413
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Id943304b28cd6fb64f0e1ed72a12e392d7cfda8e

---
+++ chrome/browser/resources/interventions_internals/index.js
@@ -0,0 +1,52 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+cr.define('interventions_internals', function() {
+  let pageHandler_ = null;
+
+  function init(pageHandler) {
+    pageHandler_ = pageHandler;
+  }
+
+  function getPreviewsEnabled() {
+    if (pageHandler_.getPreviewsEnabled == null) {
+      return;
+    }
+    pageHandler_.getPreviewsEnabled()
+        .then(function(response) {
+          let message = 'OfflinePreviews: ';
+          message += response.enabled ? 'Enabled' : 'Disabled';
+          $('OfflinePreviews').textContent = message;
+        })
+        .catch(function(error) {
+          node.textContent = error.message;
+        });
+  }
+
+  return {
+    init: init,
+    getPreviewsEnabled: getPreviewsEnabled,
+  };
+});
+
+window.setupFn = window.setupFn || function() {
+  return Promise.resolve();
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  let pageHandler = null;
+
+  window.setupFn().then(function() {
+    if (window.testPageHandler) {
+      pageHandler = window.testPageHandler;
+    } else {
+      pageHandler = new mojom.InterventionsInternalsPageHandlerPtr;
+      Mojo.bindInterface(
+          mojom.InterventionsInternalsPageHandler.name,
+          mojo.makeRequest(pageHandler).handle);
+    }
+    interventions_internals.init(pageHandler);
+    interventions_internals.getPreviewsEnabled();
+  });
+});
