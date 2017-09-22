e0e2b31c1416435b8868e86e4f8db65c9fde3693
Tatiana Gornak <melandory@chromium.org>
2017-09-22 10:59:55 +0200

    Saved passwords on Android: icons should be grey
    This CL changes the icon stored in the repo from blue to black and
    makes color of icon in settings grey (black with 54% alpha)
    
    Bug: 763953
    Change-Id: I0a49ccd3c7a0f0eddd9b23ae59f5534c03e0ac89

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/password/PasswordEntryEditor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/password/PasswordEntryEditor.java
@@ -382,7 +382,7 @@ public class PasswordEntryEditor extends Fragment {
     private void displayPassword() {
         getActivity().getWindow().setFlags(LayoutParams.FLAG_SECURE, LayoutParams.FLAG_SECURE);
 
-        changeHowPasswordIsDisplayed(R.drawable.ic_visibility_off,
+        changeHowPasswordIsDisplayed(R.drawable.ic_visibility_off_black,
                 InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD,
                 R.string.password_entry_editor_hide_stored_password);
         RecordHistogram.recordEnumeratedHistogram(
@@ -391,7 +391,7 @@ public class PasswordEntryEditor extends Fragment {
     }
 
     private void hidePassword() {
-        changeHowPasswordIsDisplayed(R.drawable.ic_visibility,
+        changeHowPasswordIsDisplayed(R.drawable.ic_visibility_black,
                 InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD,
                 R.string.password_entry_editor_view_stored_password);
         RecordHistogram.recordEnumeratedHistogram(
