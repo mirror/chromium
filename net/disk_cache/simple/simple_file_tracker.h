f7d86f5d2dc940b3a7f0a34a2ae8290380b4b920
Maks Orlovich <morlovich@chromium.org>
2017-09-22 11:13:52 -0400

    RFC: simple_cache: Prototype the SimpleFileTracker, port everything but Doom to it.
    The idea here is that SimpleFileTracker is a global object which keeps track of 
    everything SimpleCache opened, so that it can close some files if it's using way 
    too many FDs, and then re-open when needed again. This does everything but the
    actual closing/reopening and some changes to Doom needed to make it safe, as a 
    discussion/transitional step. 
    
    Bug: 636400
    Change-Id: Ida05dbdf0db42926bcea003adbb0f4509059f9b9

--- net/disk_cache/simple/simple_file_tracker.h
+++ net/disk_cache/simple/simple_file_tracker.h
@@ -6,15 +6,28 @@
 #define NET_DISK_CACHE_SIMPLE_SIMPLE_FILE_TRACKER_H_
 
 #include <stdint.h>
+#include <algorithm>
+#include <memory>
+#include <unordered_map>
 
+#include "base/files/file.h"
 #include "base/macros.h"
+#include "base/memory/ref_counted.h"
+#include "base/synchronization/lock.h"
 #include "net/base/net_export.h"
+#include "net/disk_cache/simple/simple_entry_format.h"
 
 namespace disk_cache {
 
-// This will eventually help keep track of what files the backend has open, so
-// we don't use an unreasonable number of FDs. Right now it just holds some
-// types we've started using in preparation.
+class SimpleSynchronousEntry;
+
+// This keeps track of all the files SimpleCache has open, across all the
+// backend instancess, in order to prevent us from running out of file
+// descriptors.
+// TODO(morlovich): Actually implement closing and re-opening of things if we
+// run out.
+//
+// This class is thread-safe.
 class NET_EXPORT_PRIVATE SimpleFileTracker {
  public:
   enum class SubFile { FILE_0, FILE_1, FILE_SPARSE };
@@ -34,6 +47,94 @@ class NET_EXPORT_PRIVATE SimpleFileTracker {
     // presently in use here.
     uint32_t doom_generation;
   };
+
+  SimpleFileTracker();
+  ~SimpleFileTracker();
+
+  // Established |file| as what's backing |subfile| for |key|. This is intended
+  // to be called when SimpleSynchronousEntry first sets up the file to transfer
+  // its ownership to SimpleFileTracker. Any Register() call must be eventually
+  // followed by a corresponding Close() call before the |owner| is destroyed.
+  void Register(const SimpleSynchronousEntry* owner,
+                const EntryFileKey& key,
+                SubFile subfile,
+                base::File file);
+
+  // Lends out a file to SimpleSynchronousEntry for use. SimpleFileTracker
+  // will ensure that it doesn't close the FD until Release() is called
+  // with the same parameters.
+  base::File* Acquire(const SimpleSynchronousEntry* owner,
+                      const EntryFileKey& key,
+                      SubFile subfile);
+  void Release(const SimpleSynchronousEntry* owner,
+               const EntryFileKey& key,
+               SubFile subfile);
+
+  // Tells SimpleFileTracker that SimpleSynchronousEntry will not be interested
+  // in the file further at all, so it can be closed and forgotten about.
+  // It's OK to call this before Release (in which case the effect takes place
+  // after the Release call).
+  void Close(const SimpleSynchronousEntry* owner,
+             const EntryFileKey& key,
+             SubFile file);
+
+ private:
+  struct TrackedFiles {
+    // We can potentially run through this state machine multiple times for
+    // FILE_1, as that's often missing, so SimpleSynchronousEntry can sometimes
+    // close and remove the file for an empty stream, then re-open it on actual
+    // data.
+    enum State {
+      TF_NO_REGISTRATION = 0,
+      TF_REGISTERED = 1,
+      TF_ACQUIRED = 2,
+      TF_ACQUIRED_PENDING_CLOSE = 3,
+    };
+
+    TrackedFiles() {
+      std::fill(state, state + kSimpleEntrySubFileCount, TF_NO_REGISTRATION);
+    }
+
+    bool NoActiveRegistrations() const {
+      for (State s : state)
+        if (s != TF_NO_REGISTRATION)
+          return false;
+      return true;
+    }
+
+    // We use pointers to SimpleSynchronousEntry two ways:
+    // 1) As opaque keys. This is handy as it avoids having to compare paths in
+    //    case multiple backends use the same key. Since we access the
+    //    bookkeeping under |lock_|
+    //
+    // 2) To get info on the caller of our operation.
+    //    Accessing |owner| from any other TrackedFiles would be unsafe (as it
+    //    may be doing its own thing in a different thread).
+    const SimpleSynchronousEntry* owner;
+    EntryFileKey key;
+
+    // Some of these may be !IsValid(), if they are not open.
+    // Note that these are stored indirect since we hand out pointers to these,
+    // and we don't want those to become invalid if some other thread appends
+    // things here.
+    std::unique_ptr<base::File> files[kSimpleEntrySubFileCount];
+
+    State state[kSimpleEntrySubFileCount];
+  };
+
+  // *ok will be set on whether the entry was found or not.
+  std::vector<TrackedFiles>::iterator Find(const SimpleSynchronousEntry* owner,
+                                           const EntryFileKey& key,
+                                           bool* ok);
+
+  // Handles state transition of closing file (when we are not deferring it),
+  // and moves the file out.
+  std::unique_ptr<base::File> PrepareClose(
+      std::vector<TrackedFiles>::iterator owners_files,
+      int file_index);
+
+  base::Lock lock_;
+  std::unordered_map<uint64_t, std::vector<TrackedFiles>> tracked_files_;
 
   DISALLOW_COPY_AND_ASSIGN(SimpleFileTracker);
 };
