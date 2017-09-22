0f2390b2fcc0ea8cb102dde43e6ebcf6112f1c25
Eric Lawrence <elawrence@chromium.org>
2017-09-22 12:19:12 -0500

    Prevent smuggling of Javascript urls in Omnibox paste
    Prevent circumvention of checks that ensure javascript:
    URLs cannot be pasted in the omnibox by ensuring that
    leading control and whitespace characters are trimmed.
    
    Bug: 631847
    Test: components_unittests OmniboxViewTest.*
    Change-Id: I74897e0e96e363f365c5c1e678820737063e7369

--- components/omnibox/browser/omnibox_view.cc
+++ components/omnibox/browser/omnibox_view.cc
@@ -7,6 +7,8 @@
 
 #include "components/omnibox/browser/omnibox_view.h"
 
+#include <algorithm>
+#include <memory>
 #include <utility>
 
 #include "base/strings/string16.h"
@@ -26,13 +28,32 @@
 base::string16 OmniboxView::StripJavascriptSchemas(const base::string16& text) {
   const base::string16 kJsPrefix(
       base::ASCIIToUTF16(url::kJavaScriptScheme) + base::ASCIIToUTF16(":"));
-  base::string16 out(text);
-  while (base::StartsWith(out, kJsPrefix,
-                          base::CompareCase::INSENSITIVE_ASCII)) {
-    base::TrimWhitespace(out.substr(kJsPrefix.length()), base::TRIM_LEADING,
-                         &out);
+
+  // Find the index of the first character that isn't whitespace, a control
+  // character, or a part of a JavaScript: scheme.
+  size_t trimbefore = 0;
+  for (size_t i = 0; i < text.size(); ++i) {
+    if (base::IsUnicodeWhitespace(text[i]) || (text[i] < 0x20))
+      continue;
+    if (!base::EqualsCaseInsensitiveASCII(text.substr(i, kJsPrefix.length()),
+                                          kJsPrefix))
+      break;
+    trimbefore = i + kJsPrefix.length();
+    i = trimbefore - 1;
   }
-  return out;
+
+  // If we found any "JavaScript:" schemes in the text, return the text starting
+  // at the first non-whitespace/control character after the last instance of
+  // the scheme.
+  if (trimbefore) {
+    while (trimbefore < text.size() &&
+           (base::IsUnicodeWhitespace(text[trimbefore]) ||
+            (text[trimbefore] < 0x20)))
+      ++trimbefore;
+    return text.substr(trimbefore);
+  }
+
+  return text;
 }
 
 // static
