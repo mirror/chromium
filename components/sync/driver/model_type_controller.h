d96b89b0ad241d42f24dc7d6c261d9517fb278fc
Gang Wu <gangwu@chromium.org>
2017-09-22 10:35:21 -0700

    [USS] Implement controller for typed url
    This is the final CL for typed url implementation, this CL implement
    controller for typed url, and bridge Instantiation.
    
    Bug:726158
    Change-Id: Iddb32b91b2907c3986e613dec866abab100cb5ba

--- components/sync/driver/model_type_controller.h
+++ components/sync/driver/model_type_controller.h
@@ -51,9 +51,14 @@ class ModelTypeController : public DataTypeController {
   void GetStatusCounters(const StatusCountersCallback& callback) override;
   void RecordMemoryUsageHistogram() override;
 
+ protected:
+  void ReportModelError(const ModelError& error);
+
+  // The sync client, which provides access to this type's ModelTypeSyncBridge.
+  SyncClient* const sync_client_;
+
  private:
   void RecordStartFailure(ConfigureResult result) const;
-  void ReportModelError(const ModelError& error);
 
   // If the DataType controller is waiting for models to load, once the models
   // are loaded this function should be called to let the base class
@@ -72,10 +77,8 @@ class ModelTypeController : public DataTypeController {
 
   // Post the given task that requires the bridge object to run to the model
   // thread, where the bridge lives.
-  void PostBridgeTask(const base::Location& location, const BridgeTask& task);
-
-  // The sync client, which provides access to this type's ModelTypeSyncBridge.
-  SyncClient* const sync_client_;
+  virtual void PostBridgeTask(const base::Location& location,
+                              const BridgeTask& task);
 
   // The thread the model type lives on.
   scoped_refptr<base::SingleThreadTaskRunner> model_thread_;
