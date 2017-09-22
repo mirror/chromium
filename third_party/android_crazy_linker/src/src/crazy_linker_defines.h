1bb5c774f34adb9d1ac39ad5f674fc8936224bbd
Andrew Grieve <agrieve@chromium.org>
2017-09-21 20:42:33 -0400

    Android: Compressing packed relocations for chrome_public_apk
    Deflate achieves 94% compression on packed relocations, shrinking
    libchrome.so's relocations from 350kb -> 20kb.
    
    This change will likely make patch updates a bit bigger, but no more
    than 20kb bigger.
    
    This change adds a -z flag to the relocation packer that enables
    compression, and updates the crazy linker to be able to handle
    compressed packed reloactions.
    
    I plan to upstream these changes to AOSP at some point.
    
    On a Nexus 5, library load time is not any worse:
    Cold load before this change: 343ms, 326ms, 316ms
    Cold load after this change: 315ms, 308ms, 325ms
    Warm load before this change: 292ms, 225ms, 295ms
    Warm load after this change: 228ms, 257ms, 272ms
    
    Timings from the existing "Time to load native libraries:" logcat.
    "Cold load" means: echo 3 > /proc/sys/vm/drop_caches
    
    We currently use the crazy linker on Android pre-M, which means only
    chrome_public_apk is be able to enable compression for now. However,
    chrome_modern_public_apk will likely be moved to crazy linker as well
    (bug 719977), at which point we can enable compression there as well.
    
    Bug: 681933
    Change-Id: Id5afa29b1364b85b8b664fbf08bc95edf2a08f48

---
+++ third_party/android_crazy_linker/src/src/crazy_linker_defines.h
@@ -0,0 +1,7 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#if defined(__aarch64__) || defined(__x86_64__)
+#define USE_RELA 1
+#endif
