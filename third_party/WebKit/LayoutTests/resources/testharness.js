5438ffaaf5d22032d4019d8ed0c3cd5b99dd19bf
Conley Owens <cco3@chromium.org>
2017-09-22 11:20:58 -0700

    bluetooth: Use new mojo bindings in LayoutTests
    This change updates our mojo bindings in javascript to use the new
    `mojo_bindings.js` library.  This is important for our migration to Web
    Platform Tests, because we don't intend to use `mojo-helpers.js` in
    wpt/.
    
    BUG=509038,699569
    
    Change-Id: I1309f57a9811802d36649daca4c780e21c3d0d0d

--- third_party/WebKit/LayoutTests/resources/testharness.js
+++ third_party/WebKit/LayoutTests/resources/testharness.js
@@ -13,6 +13,13 @@ policies and contribution forms [3].
 /* Documentation: http://web-platform-tests.org/writing-tests/testharness-api.html
  * (../docs/_writing-tests/testharness-api.md) */
 
+
+
+
+
+
+
+
 (function ()
 {
     var debug = false;
