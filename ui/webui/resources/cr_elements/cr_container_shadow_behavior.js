690b8e56c1714a124553db0fff4e36c645993912
dpapad <dpapad@chromium.org>
2017-09-22 12:06:25 -0700

    MD Extensions: Remove paper-header-panel, fix drop shadow effect.
     - Extracted IntersectionObserver logic from Settings to a new reusable Behavior.
     - Removed paper-header-panel usage from MD Extensions
     - Using the new Behavior from MD Extensions.
    
    Bug:751758
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I5679c3297b1f8c85d68a1d7538c3055dcc62c5e9

---
+++ ui/webui/resources/cr_elements/cr_container_shadow_behavior.js
@@ -0,0 +1,46 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview ContainerShadowBehavior holds logic for showing a drop shadow
+ * near the top of a container element, when the content has scrolled.
+ *
+ * Elements using this behavior are expected to define the following three DOM
+ * nodes:
+ *  - #container: The element being scrolled.
+ *  - #dropShadow: The element holding the drop shadow itself.
+ *  - #intersectionProbe: The element used to determine when to show the shadow.
+ *
+ *  A 'has-shadow' attribute is automatically added/removed to #dropShadow by
+ *  this behavior while scrolling, as necessary.
+ */
+
+/** @polymerBehavior */
+var ContainerShadowBehavior = {
+  /** @private {?IntersectionObserver} */
+  intersectionObserver_: null,
+
+  /** @override */
+  attached: function() {
+    // Setup drop shadow logic.
+    var callback = entries => {
+      this.$.dropShadow.classList.toggle(
+          'has-shadow', entries[entries.length - 1].intersectionRatio == 0);
+    };
+
+    this.intersectionObserver_ = new IntersectionObserver(
+        callback,
+        /** @type {IntersectionObserverInit} */ ({
+          root: this.$.container,
+          threshold: 0,
+        }));
+    this.intersectionObserver_.observe(this.$.intersectionProbe);
+  },
+
+  /** @override */
+  detached: function() {
+    this.intersectionObserver_.disconnect();
+    this.intersectionObserver_ = null;
+  },
+};
