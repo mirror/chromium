f359f3f9e97d42fc6ee0fe7cd8b7659f26b43ab6
Lucas Garron <lgarron@chromium.org>
2017-09-22 20:22:50 -0700

    Remove `overridable` parameter from ContentBrowserClient::AllowCertificateError() and its overrides.
    Note that this change does not update the similar parameter in
    WebClient::AllowCertificateError() on iOS.
    
    Bug: 768105
    Change-Id: I8214a89b6497108dea3dfe636735c865041a0673

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -252,7 +252,6 @@ void ContentBrowserClient::AllowCertificateError(
     const net::SSLInfo& ssl_info,
     const GURL& request_url,
     ResourceType resource_type,
-    bool overridable,
     bool strict_enforcement,
     bool expired_previous_decision,
     const base::Callback<void(CertificateRequestResultType)>& callback) {
