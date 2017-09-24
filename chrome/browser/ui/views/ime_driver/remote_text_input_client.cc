a46d25f81ba8b8fdcdb7cc2318c6b8ab252c7ec5
Thanh Pham <thanhph@chromium.org>
2017-09-23 21:10:26 -0400

    Implement IsCandidatePopUpOpen() in InputMethodMus.
    Client InputMethodMus needs a way to know if there's candidate pop up
    window which is created in different process, i.e., Chrome. We can
    provide SetCandidateWindowVisible() interface in ime.mojom to allow
    RemoteTextInputClient in Chrome process to send visible property of
    candidate window pop up to InputMethodMus in client process.
    
    Bug:637416
    Change-Id: I5c5785972ba90d6ec2e6ceabc9e29043ee226761

--- chrome/browser/ui/views/ime_driver/remote_text_input_client.cc
+++ chrome/browser/ui/views/ime_driver/remote_text_input_client.cc
@@ -8,7 +8,6 @@
 #define NOTIMPLEMENTED_POLICY 5
 
 #include "chrome/browser/ui/views/ime_driver/remote_text_input_client.h"
-
 #include "base/strings/utf_string_conversions.h"
 
 RemoteTextInputClient::RemoteTextInputClient(
@@ -23,7 +22,9 @@ RemoteTextInputClient::RemoteTextInputClient(
       text_input_mode_(text_input_mode),
       text_direction_(text_direction),
       text_input_flags_(text_input_flags),
-      caret_bounds_(caret_bounds) {}
+      caret_bounds_(caret_bounds) {
+  ui::IMEBridge::Get()->SetCandidateWindowHandler(this);
+}
 
 RemoteTextInputClient::~RemoteTextInputClient() {}
 
@@ -34,6 +35,12 @@ void RemoteTextInputClient::SetTextInputType(
 
 void RemoteTextInputClient::SetCaretBounds(const gfx::Rect& caret_bounds) {
   caret_bounds_ = caret_bounds;
+}
+
+void RemoteTextInputClient::UpdateLookupTable(
+    const ui::CandidateWindow& candidate_window,
+    bool visible) {
+  remote_client_->SetCandidateWindowVisible(visible);
 }
 
 void RemoteTextInputClient::SetCompositionText(
