dab339d202af87b976cc0848519983ffa9dbad17
Doug Turner <dougt@chromium.org>
2017-09-25 06:07:28 -0700

    Migrate calculation of hypertext from BrowserAccessibility to AXPlatformNode.
    Bug: 703369
    Change-Id: Icbab14a1d81201cde6d21a9ccf73e6ac9bd0286a

--- ui/accessibility/platform/ax_platform_node_base.cc
+++ ui/accessibility/platform/ax_platform_node_base.cc
@@ -542,4 +542,17 @@ base::string16 AXPlatformNodeBase::GetText() {
   return GetInnerText();
 }
 
+base::string16 AXPlatformNodeBase::GetValue() {
+  base::string16 value = GetString16Attribute(ui::AX_ATTR_VALUE);
+
+  // Some screen readers like Jaws and VoiceOver require a
+  // value to be set in text fields with rich content, even though the same
+  // information is available on the children.
+  if (value.empty() && (IsSimpleTextControl() || IsRichTextControl()) &&
+      !IsNativeTextControl())
+    return GetInnerText();
+
+  return value;
+}
+
 }  // namespace ui
