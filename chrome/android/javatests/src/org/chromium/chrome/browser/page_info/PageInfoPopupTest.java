6d94b58465d8a26409a0128b9ab347304d778d41
Patti <patricialor@chromium.org>
2017-09-25 15:36:31 +1000

    Android Page Info: Add switches to non-default permissions shown in Page Info.
    Make it easier to toggle permissions that have previously been granted or denied
    on Android by adding a toggle directly to Page Info.
    
    Manual test - Navigate to https://permission.site and grant or deny a few
    permissions there (including allowing location). Then turn off location on your
    device. Observe that when you click the lock icon next to the URL, it will show
    a list of permissions that you granted/denied permission to, plus location with
    extra blue text underneath. Verify tapping the switches next to them work and
    that the blue text disappears when blocking location. Also make sure that
    tapping the blue text under location will bring up the Android settings app for
    turning on device location.
    
    Change-Id: I0ab8c177c2378dfeb10f22e9980103de9604e97a

--- chrome/android/javatests/src/org/chromium/chrome/browser/page_info/PageInfoPopupTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/page_info/PageInfoPopupTest.java
@@ -4,8 +4,14 @@
 
 package org.chromium.chrome.browser.page_info;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
+import android.support.v7.widget.SwitchCompat;
+import android.view.View;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,10 +20,16 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.RetryOnFailure;
+import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeSwitches;
+import org.chromium.chrome.browser.ContentSettingsType;
+import org.chromium.chrome.browser.preferences.website.ContentSetting;
 import org.chromium.chrome.test.ChromeActivityTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.content.browser.test.util.Criteria;
+import org.chromium.content.browser.test.util.CriteriaHelper;
+import org.chromium.net.test.EmbeddedTestServer;
 
 /**
  * Tests for PageInfoPopup.
@@ -28,6 +40,21 @@ public class PageInfoPopupTest {
     @Rule
     public ChromeActivityTestRule<ChromeActivity> mActivityTestRule =
             new ChromeActivityTestRule<>(ChromeActivity.class);
+    private PageInfoPopup mPageInfo;
+    private EmbeddedTestServer mTestServer;
+
+    @Before
+    public void setUp() throws InterruptedException {
+        mActivityTestRule.startMainActivityFromLauncher();
+        mTestServer = EmbeddedTestServer.createAndStartServer(
+                InstrumentationRegistry.getInstrumentation().getContext());
+        mActivityTestRule.loadUrl(mTestServer.getURL("/chrome/test/data/android/simple.html"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mTestServer.stopAndDestroyServer();
+    }
 
     /**
      * Tests that PageInfoPopup can be instantiated and shown.
@@ -37,13 +64,108 @@ public class PageInfoPopupTest {
     @Feature({"PageInfoPopup"})
     @RetryOnFailure
     public void testShow() throws InterruptedException {
-        mActivityTestRule.startMainActivityOnBlankPage();
         ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
                 PageInfoPopup.show(mActivityTestRule.getActivity(),
                         mActivityTestRule.getActivity().getActivityTab(), null,
                         PageInfoPopup.OPENED_FROM_MENU);
+            }
+        });
+    }
+
+    /**
+     * Tests that by default, PageInfoPopup doesn't show any permissions.
+     */
+    @Test
+    @MediumTest
+    @Feature({"PageInfoPopup"})
+    @RetryOnFailure
+    public void testNoPermissionsShownByDefault() throws InterruptedException {
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                PageInfoPopup mPageInfo = new PageInfoPopup(mActivityTestRule.getActivity(),
+                        mActivityTestRule.getActivity().getActivityTab(), null, null);
+                View permissionsList =
+                        mPageInfo.mContainer.findViewById(R.id.page_info_permissions_list);
+                Assert.assertEquals(View.GONE, permissionsList.getVisibility());
+            }
+        });
+    }
+
+    /**
+     * Tests that PageInfoPopup will show permissions if they are set to non-default values.
+     */
+    @Test
+    @MediumTest
+    @Feature({"PageInfoPopup"})
+    @RetryOnFailure
+    public void testNonDefaultPermissionsAreShown() throws InterruptedException {
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                mPageInfo = new PageInfoPopup(mActivityTestRule.getActivity(),
+                        mActivityTestRule.getActivity().getActivityTab(), null, null);
+                mPageInfo.nativeOnSitePermissionChanged(mPageInfo.mNativePageInfoPopup,
+                        ContentSettingsType.CONTENT_SETTINGS_TYPE_NOTIFICATIONS,
+                        ContentSetting.ALLOW.toInt());
+            }
+        });
+
+        CriteriaHelper.pollUiThread(new Criteria() {
+            @Override
+            public boolean isSatisfied() {
+                if (mPageInfo == null) return false;
+
+                return mPageInfo.mPermissionsList.getVisibility() == View.VISIBLE
+                        && mPageInfo.mDisplayedPermissions.size() == 1
+                        && mPageInfo.mDisplayedPermissions.get(0).name.equals("Notifications");
+            }
+        });
+    }
+
+    /**
+     * Tests that tapping a permission switch on the PageInfoPopup will change the permission.
+     */
+    @Test
+    @MediumTest
+    @Feature({"PageInfoPopup"})
+    @RetryOnFailure
+    public void testTogglingPermission() throws InterruptedException {
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                mPageInfo = new PageInfoPopup(mActivityTestRule.getActivity(),
+                        mActivityTestRule.getActivity().getActivityTab(), null, null);
+                mPageInfo.nativeOnSitePermissionChanged(mPageInfo.mNativePageInfoPopup,
+                        ContentSettingsType.CONTENT_SETTINGS_TYPE_MEDIASTREAM_MIC,
+                        ContentSetting.ALLOW.toInt());
+            }
+        });
+
+        CriteriaHelper.pollUiThread(new Criteria() {
+            @Override
+            public boolean isSatisfied() {
+                if (mPageInfo == null) return false;
+
+                SwitchCompat permissionSwitch = (SwitchCompat) mPageInfo.mContainer.findViewById(
+                        R.id.page_info_permission_switch);
+                if (permissionSwitch == null) return false;
+
+                permissionSwitch.setChecked(false);
+                return true;
+            }
+        });
+
+        CriteriaHelper.pollUiThread(new Criteria() {
+            @Override
+            public boolean isSatisfied() {
+                if (mPageInfo == null) return false;
+
+                return mPageInfo.mDisplayedPermissions.size() == 1
+                        && mPageInfo.mDisplayedPermissions.get(0).name.equals("Microphone")
+                        && mPageInfo.mDisplayedPermissions.get(0).setting == ContentSetting.BLOCK;
             }
         });
     }
