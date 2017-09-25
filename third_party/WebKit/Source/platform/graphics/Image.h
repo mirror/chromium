ccec28976c8166dc45444adfc4532e27675c11ec
Ramin Halavati <rhalavati@chromium.org>
2017-09-25 12:53:08 +0200

    Basic image content based approach added to HighContrastImagePolicy.
    To correctly decide if an image should be inverted or not for high-
    contrast view, a very simple classifier is added under kFilterSmart
    HighContrastImagePolicy and the decision on an image is stored in the
    blink::Image objects for next references.
    
    Bug:685242
    Change-Id: I47242c237e7f34291bba53a7a02a9d6c9ef632a1

--- third_party/WebKit/Source/platform/graphics/Image.h
+++ third_party/WebKit/Source/platform/graphics/Image.h
@@ -30,6 +30,7 @@
 #include "platform/PlatformExport.h"
 #include "platform/SharedBuffer.h"
 #include "platform/geometry/IntRect.h"
+#include "platform/graphics/GraphicsTypes.h"
 #include "platform/graphics/ImageAnimationPolicy.h"
 #include "platform/graphics/ImageObserver.h"
 #include "platform/graphics/ImageOrientation.h"
@@ -225,6 +226,17 @@ class PLATFORM_EXPORT Image : public ThreadSafeRefCounted<Image> {
     return nullptr;
   }
 
+  HighContrastClassification GetHighContrastClassification() {
+    return high_contrast_classification_;
+  }
+
+  // High contrast classification result is cached to be consistent and have
+  // higher performance for future paints.
+  void SetHighContrastClassification(
+      const HighContrastClassification high_contrast_classification) {
+    high_contrast_classification_ = high_contrast_classification;
+  }
+
  protected:
   Image(ImageObserver* = 0, bool is_multipart = false);
 
@@ -266,6 +278,7 @@ class PLATFORM_EXPORT Image : public ThreadSafeRefCounted<Image> {
   WeakPersistent<ImageObserver> image_observer_;
   PaintImage::Id stable_image_id_;
   const bool is_multipart_;
+  HighContrastClassification high_contrast_classification_;
 };
 
 #define DEFINE_IMAGE_TYPE_CASTS(typeName)                          \
