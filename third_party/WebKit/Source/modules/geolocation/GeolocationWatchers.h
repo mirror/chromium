4050e26acbe9305b431cc41e00f27d4e6a573ea5
Sayaka Takayama <stakayama@google.com>
2017-09-25 16:20:03 +0900

    V8binding: changed PositionCallback from callback interface to callback function
    PositionCallback is defined as callback function by W3C standards.
    So I changed PositionCallback from callback interface to callback function.
    Also, I declared and defined references for PositionCallback as TraceWrapper.
    
    Bug:569301,658073
    Change-Id: If2575ec348850881beccb208d2d233e2561525f1

--- third_party/WebKit/Source/modules/geolocation/GeolocationWatchers.h
+++ third_party/WebKit/Source/modules/geolocation/GeolocationWatchers.h
@@ -5,18 +5,20 @@
 #ifndef GeolocationWatchers_h
 #define GeolocationWatchers_h
 
+#include "platform/bindings/TraceWrapperMember.h"
 #include "platform/heap/Handle.h"
 
 namespace blink {
 
 class GeoNotifier;
 
-class GeolocationWatchers {
+class GeolocationWatchers : public TraceWrapperBase {
   DISALLOW_NEW();
 
  public:
   GeolocationWatchers() {}
   DECLARE_TRACE();
+  DECLARE_TRACE_WRAPPERS();
 
   bool Add(int id, GeoNotifier*);
   GeoNotifier* Find(int id);
@@ -29,8 +31,8 @@ class GeolocationWatchers {
   void GetNotifiersVector(HeapVector<Member<GeoNotifier>>&) const;
 
  private:
-  typedef HeapHashMap<int, Member<GeoNotifier>> IdToNotifierMap;
-  typedef HeapHashMap<Member<GeoNotifier>, int> NotifierToIdMap;
+  typedef HeapHashMap<int, TraceWrapperMember<GeoNotifier>> IdToNotifierMap;
+  typedef HeapHashMap<TraceWrapperMember<GeoNotifier>, int> NotifierToIdMap;
 
   IdToNotifierMap id_to_notifier_map_;
   NotifierToIdMap notifier_to_id_map_;
