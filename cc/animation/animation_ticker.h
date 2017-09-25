153cc5e5bf37f6547194bbdd18e215d747912d3c
Majid Valipour <majidvp@chromium.org>
2017-09-25 15:28:22 -0400

    Send state from cc to animation worklet and back.
    The patch introduces the followings:
     - cc::AnimationPlayer::TimingModel a simple transformation from 
      TimeTicks->TimeDelta which is used before animation player wants to
      tick its animation. This provides the hook for WorkletAnimationPlayer
      to control its own local time and ignore that of the timeline.
     - cc::WrokletAnimationPlayer: A type of animation player that uses
       a managed timing model and has a name.
     - Animator{Input,Output}State: structs that are used to shuttle data
       to/from cc to animation worklet.
     - Update Mutate to accept input state and modify setNeedsMutate to 
       accept a boolean needs_mutate and an optional mutation state.
     - Collect input state in AnimationHost when sending mutate signal to
       AnimationWorklet and then apply mutation state once we receive the
       result back.
    -  compositorworkler/EffectProxy.idl just a placeholder when animator
       callback can set a local time which is then plumbed back to the
       actual player in cc.
    
    
    This is still WIP. Please only review general direction.
    The patch will be upstreamed in smaller pieces once we 
    are happy. :)
    
    
    
    
    Bug:
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ifd7f3e80220ccef5d21892648f6dbd9527b58725

--- cc/animation/animation_ticker.h
+++ cc/animation/animation_ticker.h
@@ -38,6 +38,11 @@ struct PropertyAnimationState;
 // ElementAnimations - there is only one ElementAnimations for a given target.
 class CC_ANIMATION_EXPORT AnimationTicker {
  public:
+  class AnimationTimeProvider {
+   public:
+    virtual base::TimeTicks GetTimeForAnimation(const Animation&) const = 0;
+  };
+
   explicit AnimationTicker(AnimationPlayer* animation_player);
   ~AnimationTicker();
 
@@ -71,7 +76,8 @@ class CC_ANIMATION_EXPORT AnimationTicker {
   void AttachElement(ElementId element_id);
   void DetachElement();
 
-  void Tick(base::TimeTicks monotonic_time);
+  void Tick(base::TimeTicks monotonic_time,
+            const AnimationTimeProvider* tick_provider);
   static void TickAnimation(base::TimeTicks monotonic_time,
                             Animation* animation,
                             AnimationTarget* target);
