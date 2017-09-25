9a5e84f1b4453a862f46b757eff4023f8c7eae52
Jun Cai <juncai@chromium.org>
2017-09-25 13:54:24 -0700

    Set unavailable sensor data field Not-A-Number
    Currently the generic_sensor code doesn't provide a mechanism to mark
    which sensor data field is unavailable. This CL uses Not-A-Number to
    mark that.
    
    Bug: 768473
    Change-Id: I7debdfde015cd5bec83c01f4a01b781a419e74e7

--- services/device/generic_sensor/relative_orientation_euler_angles_fusion_algorithm_using_accelerometer.cc
+++ services/device/generic_sensor/relative_orientation_euler_angles_fusion_algorithm_using_accelerometer.cc
@@ -39,7 +39,7 @@ void ComputeRelativeOrientationFromAccelerometer(double acceleration_x,
   // Also note that alpha can't be provided but it's assumed to be always zero.
   // This is necessary in order to provide enough information to solve
   // the equations.
-  *alpha_in_degrees = 0.0;
+  *alpha_in_degrees = NAN;
   *beta_in_degrees =
       kRadiansToDegrees * std::atan2(-acceleration_y, acceleration_z);
   *gamma_in_degrees =
