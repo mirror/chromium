78104c732e81e624f1b50e4bff174f35ac492bf4
Joshua Bell <jsbell@chromium.org>
2017-09-25 16:14:35 -0700

    Web Locks API
    https://github.com/inexorabletash/web-locks
    
    Bug: 161072
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: If31fdff852f7e922a61d42687d813a61cb559dbe

---
+++ third_party/WebKit/LayoutTests/http/tests/locks/resources/worker.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Map of id => function that releases a lock.
+
+const held = new Map();
+
+self.addEventListener('message', e => {
+  switch (e.data.op) {
+  case 'request':
+    self.requestLock(e.data.scope, e.data.mode)
+      .then(lock => {
+        let release;
+        const promise = new Promise(r => { release = r; });
+        held.set(e.data.id, release);
+        lock.waitUntil(promise);
+        self.postMessage({ack: 'request', id: e.data.id});
+      });
+    break;
+
+  case 'release':
+    held.get(e.data.id)();
+    held.delete(e.data.id);
+    self.postMessage({ack: 'release', id: e.data.id});
+    break;
+  }
+});
--- third_party/WebKit/Source/modules/ModulesInitializer.cpp
+++ third_party/WebKit/Source/modules/ModulesInitializer.cpp
@@ -28,6 +28,7 @@
 #include "modules/EventModulesNames.h"
 #include "modules/EventTargetModulesNames.h"
 #include "modules/IndexedDBNames.h"
+#include "modules/LocksNames.h"
 #include "modules/accessibility/AXObjectCacheImpl.h"
 #include "modules/accessibility/InspectorAccessibilityAgent.h"
 #include "modules/app_banner/AppBannerController.h"
@@ -96,13 +97,14 @@ void ModulesInitializer::Initialize() {
   const unsigned kModulesStaticStringsCount =
       EventNames::EventModulesNamesCount +
       EventTargetNames::EventTargetModulesNamesCount +
-      IndexedDBNames::IndexedDBNamesCount;
+      LocksNames::LocksNamesCount + IndexedDBNames::IndexedDBNamesCount;
   StringImpl::ReserveStaticStringsCapacityForSize(kModulesStaticStringsCount);
 
   EventNames::initModules();
   EventTargetNames::initModules();
   Document::RegisterEventFactory(EventModulesFactory::Create());
   ModuleBindingsInitializer::Init();
+  LocksNames::init();
   IndexedDBNames::init();
   AXObjectCache::Init(AXObjectCacheImpl::Create);
   DraggedIsolatedFileSystem::Init(
