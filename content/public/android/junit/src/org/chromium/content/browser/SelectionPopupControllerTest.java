d968a8e0411af40f624c9c0ecf5bf4fa7b50a5d4
Shimi Zhang <ctzsm@chromium.org>
2017-09-25 15:21:16 -0700

    [Smart Selection] Fix selection menu flakiness
    Current Smart Selection workflow:
    
    1) When receiving showSelectionMenu() call in SelectionPopupController,
    it asks SmartSelectionClient to do classification and suggestion.
    2) A callback onClassified() in SelectionPopupController is called when
    received classification/suggestion result.
    3) onClassified() will call adjustSelectionByCharacterOffset() if
    necessary.
    4) When selection range expands, SelectionPopupController will receive
    a onSelectionEvent() call, then it triggers selection menu if
    SELECTION_HANDLES_MOVED event comes.
    
    This workflow has one flaw since SELECTION_HANDLES_* events are not
    very reliable for this use case. Because under certain condition the
    original long press caused selection and the later selection expansion
    event can be draw at once. In this case, there is only one
    SELECTION_HANDLES_SHOWN event without a following
    SELECTION_HANDLES_MOVED event will happen.
    
    This CL changed 4), let adjustSelectionByCharacterOffset() triggers
    another showSelectionMenu() call and handle the last selection menu
    event from that point. It removes the dependency on
    SELECTION_HANDLES_* events, therefore resolved the bug.
    
    Bug: 763201
    Change-Id: I3688fbb0a31e3794486cc9eab162f4392ab6ff80

--- content/public/android/junit/src/org/chromium/content/browser/SelectionPopupControllerTest.java
+++ content/public/android/junit/src/org/chromium/content/browser/SelectionPopupControllerTest.java
@@ -4,17 +4,28 @@
 
 package org.chromium.content.browser;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.when;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.view.ActionMode;
 import android.view.View;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowLog;
 
 import org.chromium.base.test.util.Feature;
 import org.chromium.content_public.browser.WebContents;
@@ -33,8 +44,13 @@ public class SelectionPopupControllerTest {
     WebContents mWebContents;
     View mView;
     RenderCoordinates mRenderCoordinates;
+    ActionMode mActionMode;
+    PackageManager mPackageManager;
 
     class MySelectionClient implements SelectionClient {
+        private SelectionClient.Result mResult;
+        private SelectionClient.ResultCallback mResultCallback;
+
         @Override
         public void onSelectionChanged(String selection) {}
 
@@ -49,31 +65,153 @@ public class SelectionPopupControllerTest {
 
         @Override
         public boolean requestSelectionPopupUpdates(boolean shouldSuggest) {
-            return false;
+            mResultCallback.onClassified(mResult);
+            return true;
         }
 
         @Override
         public void cancelAllRequests() {}
+
+        public void setResult(SelectionClient.Result result) {
+            mResult = result;
+        }
+
+        public void setResultCallback(SelectionClient.ResultCallback callback) {
+            mResultCallback = callback;
+        }
     }
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+        ShadowLog.stream = System.out;
 
         mContext = Mockito.mock(Context.class);
         mWindowAndroid = Mockito.mock(WindowAndroid.class);
         mWebContents = Mockito.mock(WebContents.class);
         mView = Mockito.mock(View.class);
         mRenderCoordinates = Mockito.mock(RenderCoordinates.class);
+        mActionMode = Mockito.mock(ActionMode.class);
+        mPackageManager = Mockito.mock(PackageManager.class);
+
+        when(mContext.getPackageManager()).thenReturn(mPackageManager);
 
         mController = SelectionPopupController.createForTesting(
                 mContext, mWindowAndroid, mWebContents, mView, mRenderCoordinates);
     }
 
     @Test
-    @Feature({"SmartSelection"})
+    @Feature({"TextInput", "SmartSelection"})
     public void testSmartSelectionAdjustSelectionRange() {
-        assertNotNull(mController);
+        InOrder order = inOrder(mWebContents, mView);
+
+        // Long press "Amphitheatre" in "1600 Amphitheatre Parkway".
+        SelectionClient.Result result = new SelectionClient.Result();
+        result.startAdjust = -5;
+        result.endAdjust = 8;
+        result.label = "Maps";
+
+        // Setup SelectionClient for SelectionPopupController.
         MySelectionClient client = new MySelectionClient();
+        client.setResult(result);
+        client.setResultCallback(mController.getResultCallback());
+        mController.setSelectionClient(client);
+
+        // Long press triggered showSelectionMenu() call.
+        mController.showSelectionMenu(0, 0, 0, 0, /* isEditable = */ true,
+                /* isPasswordType = */ false, "Amphitheatre", /* canSelectAll = */ true,
+                /* canRichlyEdit = */ true, /* shouldSuggest = */ true,
+                /* fromSelectionAdjustment = */ false);
+
+        // adjustSelectionByCharacterOffset() should be called.
+        order.verify(mWebContents)
+                .adjustSelectionByCharacterOffset(result.startAdjust, result.endAdjust, true);
+        assertFalse(mController.isActionModeValid());
+
+        when(mView.startActionMode(any(FloatingActionModeCallback.class), anyInt()))
+                .thenReturn(mActionMode);
+
+        // Call showSelectionMenu again, which is adjustSelectionByCharacterOffset triggered.
+        mController.showSelectionMenu(0, 0, 0, 0, /* isEditable = */ true,
+                /* isPasswordType = */ false, "1600 Amphitheatre Parkway",
+                /* canSelectAll = */ true,
+                /* canRichlyEdit = */ true, /* shouldSuggest = */ true,
+                /* fromSelectionAdjustment = */ true);
+
+        order.verify(mView).startActionMode(
+                isA(FloatingActionModeCallback.class), eq(ActionMode.TYPE_FLOATING));
+        assertTrue(mController.isActionModeValid());
+    }
+
+    @Test
+    @Feature({"TextInput", "SmartSelection"})
+    public void testSmartSelectionMultiRangeAdjustment() {
+        InOrder order = inOrder(mWebContents, mView);
+
+        // Longpress "Amphitheatre" in "1600 Amphitheatre Parkway".
+        SelectionClient.Result result = new SelectionClient.Result();
+        result.startAdjust = -5;
+        result.endAdjust = 8;
+        result.label = "Maps";
+
+        // Set SelectionClient for SelectionPopupController.
+        MySelectionClient client = new MySelectionClient();
+        client.setResult(result);
+        client.setResultCallback(mController.getResultCallback());
+        mController.setSelectionClient(client);
+
+        // Long press triggered showSelectionMenu() call.
+        mController.showSelectionMenu(0, 0, 0, 0, /* isEditable = */ true,
+                /* isPasswordType = */ false, "Amphitheatre", /* canSelectAll = */ true,
+                /* canRichlyEdit = */ true, /* shouldSuggest = */ true,
+                /* fromSelectionAdjustment = */ false);
+
+        // adjustSelectionByCharacterOffset() should be called.
+        order.verify(mWebContents)
+                .adjustSelectionByCharacterOffset(result.startAdjust, result.endAdjust, true);
+        assertFalse(mController.isActionModeValid());
+
+        // Long press "Mountain" in "585 Franklin Street, Mountain View, CA 94041".
+        SelectionClient.Result newResult = new SelectionClient.Result();
+        newResult.startAdjust = -21;
+        newResult.endAdjust = 15;
+        newResult.label = "Maps";
+
+        client.setResult(newResult);
+
+        // Another long press triggered showSelectionMenu() call.
+        mController.showSelectionMenu(0, 0, 0, 0, /* isEditable = */ true,
+                /* isPasswordType = */ false, "Mountain", /* canSelectAll = */ true,
+                /* canRichlyEdit = */ true, /* shouldSuggest = */ true,
+                /* fromSelectionAdjustment = */ false);
+        order.verify(mWebContents)
+                .adjustSelectionByCharacterOffset(newResult.startAdjust, newResult.endAdjust, true);
+        assertFalse(mController.isActionModeValid());
+
+        when(mView.startActionMode(any(FloatingActionModeCallback.class), anyInt()))
+                .thenReturn(mActionMode);
+
+        // First adjustSelectionByCharacterOffset() triggered.
+        mController.showSelectionMenu(0, 0, 0, 0, /* isEditable = */ true,
+                /* isPasswordType = */ false, "1600 Amphitheatre Parkway",
+                /* canSelectAll = */ true,
+                /* canRichlyEdit = */ true, /* shouldSuggest = */ true,
+                /* fromSelectionAdjustment = */ true);
+
+        SelectionClient.Result returnResult = mController.getClassificationResult();
+        assertTrue(returnResult.startAdjust == newResult.startAdjust
+                && returnResult.endAdjust == newResult.endAdjust
+                && returnResult.label == newResult.label);
+
+        // Second adjustSelectionByCharacterOffset() triggered.
+        mController.showSelectionMenu(0, 0, 0, 0, /* isEditable = */ true,
+                /* isPasswordType = */ false, "585 Franklin Street, Mountain View, CA 94041",
+                /* canSelectAll = */ true,
+                /* canRichlyEdit = */ true, /* shouldSuggest = */ true,
+                /* fromSelectionAdjustment = */ true);
+
+        order.verify(mView).startActionMode(
+                isA(FloatingActionModeCallback.class), eq(ActionMode.TYPE_FLOATING));
+        assertTrue(mController.isActionModeValid());
     }
 }
