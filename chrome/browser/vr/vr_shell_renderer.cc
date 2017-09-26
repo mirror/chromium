e14fbf460e9efc729e84b082de4c851c1b86b9fb
Ian Vollick <vollick@chromium.org>
2017-09-26 14:14:24 -0400

    [vr] Update the loading indicator via databinding
    This CL does a number of things. It introduces vr::Model, which is
    owned by vr::Ui. The model does not contain much yet (just a bit of
    state related to the loading indicator), but the goal is for it to
    house all our browser state).
    
    In order to have vr::Ui poke the model rather than having the
    BrowserUiInterface calls get routed directly to the UiSceneManager, I
    also made the vr::Ui implement both BrowserUiInterface and UiInterface.
    
    Given that we have rounded rects as a primitive, this change also
    allowed us to avoid using textures at all for the loading indicator.
    
    Bug:768512
    Change-Id: I2c5ff1bba6be7280aeadb53b000e0acb30fdc502

--- chrome/browser/vr/vr_shell_renderer.cc
+++ chrome/browser/vr/vr_shell_renderer.cc
@@ -1099,6 +1099,11 @@ void VrShellRenderer::DrawTexturedQuad(int texture_data_handle,
                                        float opacity,
                                        gfx::SizeF element_size,
                                        float corner_radius) {
+  // TODO(vollick): handle drawing this degenerate situation crbug.com/768922
+  if (corner_radius * 2.0 > element_size.width() ||
+      corner_radius * 2.0 > element_size.height()) {
+    return;
+  }
   TexturedQuadRenderer* renderer = texture_location == kTextureLocationExternal
                                        ? GetExternalTexturedQuadRenderer()
                                        : GetTexturedQuadRenderer();
