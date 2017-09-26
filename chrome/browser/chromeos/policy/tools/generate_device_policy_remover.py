3900cb349fd7f801fb065f7df2b1b89f51930ca8
Daria Yakovleva <yakovleva@google.com>
2017-09-26 18:25:36 +0200

    [DeviceOffHours] Change |ignored_policies| values in proto
    Change |ignored_policies| values from names in
    ChromeDeviceSettingsProto to IDs in the same proto because IDs
    are never changed.
    
    Bug: 739713
    Change-Id: I88b55c37f59445e7daf22e73b594cb7eae0bc011

--- chrome/browser/chromeos/policy/tools/generate_device_policy_remover.py
+++ chrome/browser/chromeos/policy/tools/generate_device_policy_remover.py
@@ -42,17 +42,20 @@ def main():
                'namespace em = enterprise_management;\n\n'
                'void RemovePolicies(\n'
                '    em::ChromeDeviceSettingsProto* policies,\n'
-               '    const std::vector<std::string>& '
-               'input_policies_to_remove) {\n'
-               '  std::vector<std::string> policies_to_remove'
-               '(input_policies_to_remove);\n'
-               '  std::sort(policies_to_remove.begin(),'
-               ' policies_to_remove.end());\n')
+               '    const std::vector<int>& '
+               'input_policies_proto_tags_to_remove) {\n'
+               '  std::vector<int> policies_proto_tags_to_remove'
+               '(input_policies_proto_tags_to_remove);\n'
+               '  std::sort(policies_proto_tags_to_remove.begin(),'
+               ' policies_proto_tags_to_remove.end());\n')
     for field in ChromeDeviceSettingsProto.DESCRIPTOR.fields:
-      file.write('  if (std::binary_search(policies_to_remove.begin(),\n'
-                 '                         policies_to_remove.end(),\n'
-                 '                         "{name}"))\n'
-                 '    policies->clear_{name}();\n'.format(name=field.name))
+      file.write('  if (std::binary_search('
+                 'policies_proto_tags_to_remove.begin(),\n'
+                 '                         '
+                 'policies_proto_tags_to_remove.end(),\n'
+                 '                         {id}))\n'
+                 '    policies->clear_{name}();\n'.format(id=field.number,
+                                                          name=field.name))
     file.write('}\n}\n')
   return 0
 
