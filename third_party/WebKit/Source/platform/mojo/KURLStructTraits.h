989a255ed360d8477fc92f3ac687b7987e8d7e77
Daniel Cheng <dcheng@chromium.org>
2017-09-25 21:19:22 -0700

    Ensure that invalid URLs roundtrip through IPC.
    Sending an invalid URL shouldn't result in getting a valid, but empty,
    GURL on the other end.
    
    Bug: 166486, 571784
    Change-Id: Ia369e3d8faa8c6fed07ec8681cae932262d9c6c5

--- third_party/WebKit/Source/platform/mojo/KURLStructTraits.h
+++ third_party/WebKit/Source/platform/mojo/KURLStructTraits.h
@@ -5,6 +5,7 @@
 #ifndef KURLStructTraits_h
 #define KURLStructTraits_h
 
+#include "platform/PlatformExport.h"
 #include "platform/weborigin/KURL.h"
 #include "platform/wtf/text/WTFString.h"
 #include "url/mojo/url.mojom-blink.h"
@@ -13,29 +14,18 @@
 namespace mojo {
 
 template <>
-struct StructTraits<url::mojom::blink::Url::DataView, ::blink::KURL> {
-  static WTF::String url(const ::blink::KURL& blinkUrl) {
-    if (!blinkUrl.IsValid() ||
-        blinkUrl.GetString().length() > url::kMaxURLChars) {
+struct PLATFORM_EXPORT
+    StructTraits<url::mojom::blink::Url::DataView, ::blink::KURL> {
+  static bool is_valid(const ::blink::KURL& url) { return url.IsValid(); }
+  static const WTF::String& spec(const ::blink::KURL& url) {
+    if (!url.IsValid() || url.GetString().length() > url::kMaxURLChars) {
       return g_empty_string;
     }
 
-    return blinkUrl.GetString();
+    return url.GetString();
   }
-  static bool Read(url::mojom::blink::Url::DataView data, ::blink::KURL* out) {
-    WTF::String urlString;
-    if (!data.ReadUrl(&urlString))
-      return false;
 
-    if (urlString.length() > url::kMaxURLChars)
-      return false;
-
-    *out = ::blink::KURL(::blink::KURL(), urlString);
-    if (!urlString.IsEmpty() && !out->IsValid())
-      return false;
-
-    return true;
-  }
+  static bool Read(url::mojom::blink::Url::DataView, ::blink::KURL* out);
 };
 }
 
