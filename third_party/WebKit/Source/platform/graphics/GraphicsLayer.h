7ff6c6bcab6767264d8bae0a9f42ad9301528e18
Peter Mayo <petermayo@chromium.org>
2017-09-26 11:05:06 -0400

    Remove mutable_properties from layers
    These could/should have gone away with CompositorProxy, but now that
    CompositorWorker is gone too, they really do stick out as zombie code.
    
    Bug: 768852
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I3eb1b3fd0f8806ffd1bed4689d6547308ecc6438

--- third_party/WebKit/Source/platform/graphics/GraphicsLayer.h
+++ third_party/WebKit/Source/platform/graphics/GraphicsLayer.h
@@ -271,8 +271,6 @@ class PLATFORM_EXPORT GraphicsLayer : public cc::LayerClient,
   void SetElementId(const CompositorElementId&);
   CompositorElementId GetElementId() const;
 
-  void SetCompositorMutableProperties(uint32_t);
-
   WebContentLayerClient& WebContentLayerClientForTesting() { return *this; }
 
   // DisplayItemClient methods
