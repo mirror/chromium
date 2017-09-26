440de0386a21c84c13cb110b6950334c2ba6618d
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2017-09-26 12:37:47 -0700

    [DevTools] postpone inspector interruptions during CreateContext
    Otherwise crash can occur.
    
    Bug:chromium:757778
    Change-Id: Idbe43f15bf620af41738e6e27c7a5ddf5bbf3067
    R=pfeldman@chromium.org

--- third_party/WebKit/Source/core/inspector/InspectorTaskRunner.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorTaskRunner.cpp
@@ -19,8 +19,22 @@ InspectorTaskRunner::IgnoreInterruptsScope::~IgnoreInterruptsScope() {
   task_runner_->ignore_interrupts_ = was_ignoring_;
 }
 
+InspectorTaskRunner::PostponeInterruptsScope::PostponeInterruptsScope(
+    InspectorTaskRunner* task_runner,
+    v8::Isolate* isolate)
+    : isolate_(isolate),
+      interupt_requested_(task_runner->interupt_requested_),
+      task_runner_(task_runner),
+      ignore_interrupts_scope_(task_runner) {}
+
+InspectorTaskRunner::PostponeInterruptsScope::~PostponeInterruptsScope() {
+  if (interupt_requested_ && !task_runner_->interupt_requested_) {
+    task_runner_->InterruptAndRunAllTasksDontWait(isolate_);
+  }
+}
+
 InspectorTaskRunner::InspectorTaskRunner()
-    : ignore_interrupts_(false), killed_(false) {}
+    : interupt_requested_(false), ignore_interrupts_(false), killed_(false) {}
 
 InspectorTaskRunner::~InspectorTaskRunner() {}
 
@@ -58,6 +72,7 @@ void InspectorTaskRunner::Kill() {
 
 void InspectorTaskRunner::InterruptAndRunAllTasksDontWait(
     v8::Isolate* isolate) {
+  interupt_requested_ = true;
   isolate->RequestInterrupt(&V8InterruptCallback, this);
 }
 
@@ -72,6 +87,7 @@ void InspectorTaskRunner::RunAllTasksDontWait() {
 
 void InspectorTaskRunner::V8InterruptCallback(v8::Isolate*, void* data) {
   InspectorTaskRunner* runner = static_cast<InspectorTaskRunner*>(data);
+  runner->interupt_requested_ = false;
   if (runner->ignore_interrupts_)
     return;
   runner->RunAllTasksDontWait();
