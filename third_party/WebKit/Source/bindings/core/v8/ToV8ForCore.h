469cf597d2cd92f7fee9b9c54a7a0f8dc0990f8e
Daniel Bratell <bratell@opera.com>
2017-09-26 10:18:58 +0200

    Remove a small header by including it in ToV8ForCore.h
    A bit of cleanup after a recent commit that added an unnecessary header
    file.
    
    Bug: 745732
    Change-Id: I1a90f22c9301ab60a09ea2fec9020c5da431460b

--- third_party/WebKit/Source/bindings/core/v8/ToV8ForCore.h
+++ third_party/WebKit/Source/bindings/core/v8/ToV8ForCore.h
@@ -10,6 +10,7 @@
 
 #include "bindings/core/v8/IDLDictionaryBase.h"
 #include "bindings/core/v8/ScriptValue.h"
+#include "bindings/core/v8/V8BindingForTesting.h"
 #include "bindings/core/v8/V8NodeFilterCondition.h"
 #include "core/dom/Node.h"
 #include "core/typed_arrays/ArrayBufferViewHelpers.h"
@@ -78,6 +79,10 @@ template <typename T>
 inline ScriptValue ScriptValue::From(ScriptState* script_state, T&& value) {
   return ScriptValue(script_state, ToV8(std::forward<T>(value), script_state));
 }
+
+template <typename T>
+v8::Local<v8::Value> ToV8(V8TestingScope* scope, T value) {
+  return blink::ToV8(value, scope->GetContext()->Global(), scope->GetIsolate());
 
 }  // namespace blink
 
