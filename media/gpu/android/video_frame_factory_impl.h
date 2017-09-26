42d4132dee8d4c6c4cc7a8ca80812257f4a63398
Chris Watkins <watk@chromium.org>
2017-09-26 20:51:57 +1000

    media: Stop calling EOS DecodeCBs after Reset in MediaCodecVideoDecoder
    Previously it was possible that eos_decode_cb_ would be run after a
    Reset(). Now we guarantee that neither VideoFrame outputs nor the EOS
    DecodeCB will run after a Reset(). This is implemented by replacing the
    reset_generation_ int with WeakPtrs. The WeakPtrs are contained within
    VideoFrameFactoryImpl and it exposes a CancelPendingCallbacks(). This
    makes it easy to cancel callbacks on Reset() or during Destroy() and
    EnterTerminalState().
    
    This also moves GpuVideoFrameFactory to the cc file because it doesn't
    need to be in the header.
    
    Bug: 660942
    Change-Id: I03897fcbfff5eb4bd267fc3de1558bd9b5f0a2bd

--- media/gpu/android/video_frame_factory_impl.h
+++ media/gpu/android/video_frame_factory_impl.h
@@ -19,92 +19,49 @@
 #include "ui/gl/gl_bindings.h"
 
 namespace media {
-class GpuVideoFrameFactory;
+using FrameAndReleaseCb =
+    std::pair<scoped_refptr<VideoFrame>, VideoFrameFactory::ReleaseMailboxCB>;
 
 // VideoFrameFactoryImpl creates CodecOutputBuffer backed VideoFrames and tries
 // to eagerly render them to their surface to release the buffers back to the
-// decoder as soon as possible. It's not thread safe, but it internally posts
-// calls to GpuVideoFrameFactory on the gpu thread.
+// decoder as soon as possible. It's not thread safe; it should be created, used
+// and destructed on a single sequence. It's implemented by proxying calls
+// to a helper class hosted on the gpu thread.
 class MEDIA_GPU_EXPORT VideoFrameFactoryImpl : public VideoFrameFactory {
  public:
-  VideoFrameFactoryImpl();
+  // |get_stub_cb| will be run on |gpu_task_runner|.
+  VideoFrameFactoryImpl(
+      scoped_refptr<base::SingleThreadTaskRunner> gpu_task_runner,
+      GetStubCb get_stub_cb);
   ~VideoFrameFactoryImpl() override;
 
-  void Initialize(scoped_refptr<base::SingleThreadTaskRunner> gpu_task_runner,
-                  GetStubCb get_stub_cb,
-                  InitCb init_cb) override;
+  void Initialize(InitCb init_cb) override;
   void CreateVideoFrame(std::unique_ptr<CodecOutputBuffer> output_buffer,
                         scoped_refptr<SurfaceTextureGLOwner> surface_texture,
                         base::TimeDelta timestamp,
                         gfx::Size natural_size,
                         OutputWithReleaseMailboxCB output_cb) override;
+  void RunAfterPendingVideoFrames(base::Closure closure) override;
+  void CancelPendingCallbacks() override;
 
  private:
+  // These member functions intercept the reponses from GpuSide and forward them
+  // to the caller, but allow us to cancel all pending callbacks by invalidating
+  // |weak_factory_|'s pointers.
+  void OnInitializeDone(InitCb init_cb,
+                        scoped_refptr<SurfaceTextureGLOwner> surface_texture);
+  void OnVideoFrameCreated(OutputWithReleaseMailboxCB output_cb,
+                           FrameAndReleaseCb frame_and_release_cb);
+  void RunClosure(base::Closure closure);
+
+  // The gpu thread side of the implementation.
+  class GpuSide;
+  std::unique_ptr<GpuSide> gpu_side_;
   scoped_refptr<base::SingleThreadTaskRunner> gpu_task_runner_;
-  std::unique_ptr<GpuVideoFrameFactory> gpu_video_frame_factory_;
+  GetStubCb get_stub_cb_;
+  base::WeakPtrFactory<VideoFrameFactoryImpl> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(VideoFrameFactoryImpl);
-};
-
-// GpuVideoFrameFactory is an implementation detail of VideoFrameFactoryImpl. It
-// may be created on any thread but only accessed on the gpu thread thereafter.
-class GpuVideoFrameFactory
-    : public gpu::GpuCommandBufferStub::DestructionObserver {
- public:
-  GpuVideoFrameFactory();
-  ~GpuVideoFrameFactory() override;
-
-  scoped_refptr<SurfaceTextureGLOwner> Initialize(
-      VideoFrameFactory::GetStubCb get_stub_cb);
-
-  // Creates a VideoFrame and returns it via posting |output_cb| to
-  // |task_runner|.
-  void CreateVideoFrame(
-      std::unique_ptr<CodecOutputBuffer> output_buffer,
-      scoped_refptr<SurfaceTextureGLOwner> surface_texture,
-      base::TimeDelta timestamp,
-      gfx::Size natural_size,
-      VideoFrameFactory::OutputWithReleaseMailboxCB output_cb,
-      scoped_refptr<base::SingleThreadTaskRunner> task_runner);
-
- private:
-  // Creates a TextureRef and VideoFrame.
-  void CreateVideoFrameInternal(
-      std::unique_ptr<CodecOutputBuffer> output_buffer,
-      scoped_refptr<SurfaceTextureGLOwner> surface_texture,
-      base::TimeDelta timestamp,
-      gfx::Size natural_size,
-      scoped_refptr<VideoFrame>* video_frame_out,
-      scoped_refptr<gpu::gles2::TextureRef>* texture_ref_out);
-
-  void OnWillDestroyStub() override;
-
-  // Clears |texture_refs_|. Makes the gl context current.
-  void ClearTextureRefs();
-
-  // Removes |ref| from texture_refs_. Makes the gl context current.
-  // |token| is ignored because MojoVideoDecoderService guarantees that it has
-  // already passed by the time we get the callback.
-  void DropTextureRef(gpu::gles2::TextureRef* ref, const gpu::SyncToken& token);
-
-  // Removes |image| from |images_|.
-  void OnImageDestructed(CodecImage* image);
-
-  // Outstanding images that should be considered for early rendering.
-  std::vector<CodecImage*> images_;
-
-  // Outstanding TextureRefs that are still referenced by a mailbox VideoFrame.
-  // They're kept alive until their mailboxes are released (or |this| is
-  // destructed).
-  std::map<gpu::gles2::TextureRef*, scoped_refptr<gpu::gles2::TextureRef>>
-      texture_refs_;
-  gpu::GpuCommandBufferStub* stub_;
-
-  // A helper for creating textures. Only valid while |stub_| is valid.
-  std::unique_ptr<GLES2DecoderHelper> decoder_helper_;
-  base::WeakPtrFactory<GpuVideoFrameFactory> weak_factory_;
-
-  DISALLOW_COPY_AND_ASSIGN(GpuVideoFrameFactory);
 };
 
 namespace internal {
@@ -151,7 +108,6 @@ void MEDIA_GPU_EXPORT MaybeRenderEarly(std::vector<Image*>* image_vector_ptr) {
 }
 
 }  // namespace internal
-
 }  // namespace media
 
 #endif  // MEDIA_GPU_ANDROID_VIDEO_FRAME_FACTORY_IMPL_
