12628b98a764bab7d0f9d0c72d0e78faa4942d5a
Yuzu Saijo <yuzus@chromium.org>
2017-09-26 16:34:31 +0900

    NOT FOR REVIEW | Add Cluster Telemetry benchmark & Telemetry benchmark for leak detection
    Bug:
    Change-Id: I51223b7fdfc6d56830315317a70b2a2ddcc1d78e

--- components/autofill/content/renderer/autofill_agent.cc
+++ components/autofill/content/renderer/autofill_agent.cc
@@ -193,10 +193,7 @@ void AutofillAgent::DidCommitProvisionalLoad(bool is_new_navigation,
     // (crbug/753071).
     std::string group_name =
         base::FieldTrialList::FindFullName("FixDocumentLeakInAutofillAgent");
-    if (base::StartsWith(group_name, "enabled",
-                         base::CompareCase::INSENSITIVE_ASCII)) {
-      element_.Reset();
-    }
+    element_.Reset();
 
     form_cache_.Reset();
     submitted_forms_.clear();
