851f12ef28365bf8d297ed3ab56aaf4f9657eb4e
Olga Sharonova <olka@chromium.org>
2017-09-26 02:30:58 +0200

    mojom::AudioSystemInfo implementaiton in AudioService
    and media::AudioSystem on top of it.
    
    WIP
    
    * //services/audio infrastructure (.gn files need fixes)
    * Mojo interfaces and struct traits
    * AudioService
    * mojom::AudioSystemInfo implementaion on top of AudioManager
    * client-side media::AudioSystem implementation on top of
    mojom::AudioSystemInfo - draft
    * Unit tests missing.
    
    May need to be split into parts.
    
    Bug:740943
    Change-Id: I3323b993b6954d5bae044958d6903c826bde9dc5

---
+++ services/audio/service_main.cc
@@ -0,0 +1,12 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/audio/audio_service.h"
+#include "services/service_manager/public/c/main.h"
+#include "services/service_manager/public/cpp/service_runner.h"
+
+MojoResult ServiceMain(MojoHandle service_request_handle) {
+  return service_manager::ServiceRunner(new audio::AudioService(nullptr))
+      .Run(service_request_handle);
+}
