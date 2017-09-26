b819a37f4cd8642749670a50ccb8befc9bbb12f2
Raymes Khoury <raymes@chromium.org>
2017-09-26 16:42:23 +1000

    Fix tests that fail when feature policy is enabled
    This CL fixes tests that fail when Feature Policy is enabled.
    Specifically:
    
    1) media_stream_ui_proxy_unittest.cc doesn't use real RFHs when
    exercising code. FP checks require that. This CL adds a check to
    media_stream_ui_proxy.cc to ensure that when these tests are running
    the FP is simply ignored.
    
    2) media_stream_dispatcher_host_unittest.cc also doesn't use real RFHs.
    FakeMediaStreamUIProxy is modified so that the test can inform the
    MediaStreamUIProxy about this so that FP checks are ignored in that
    case. Because a new FakeMediaStreamUIProxy is created per request and
    some tests involve multiple requests, MediaStreamManager is modified
    such that a factory function is passed to create the appropriate
    FakeMediaStreamUIProxy.
    
    BUG=689802
    
    Change-Id: Icb7b566af131ad4996c5e2c136759058495c3430

--- content/browser/renderer_host/media/media_stream_manager.cc
+++ content/browser/renderer_host/media/media_stream_manager.cc
@@ -411,12 +411,18 @@ MediaStreamManager::MediaStreamManager(
     media::AudioSystem* audio_system,
     scoped_refptr<base::SingleThreadTaskRunner> audio_task_runner,
     std::unique_ptr<VideoCaptureProvider> video_capture_provider)
-    : audio_system_(audio_system),
+    : audio_system_(audio_system) {
 #if defined(OS_WIN)
-      video_capture_thread_("VideoCaptureThread"),
+  video_capture_thread_ = "VideoCaptureThread";
 #endif
-      use_fake_ui_(base::CommandLine::ForCurrentProcess()->HasSwitch(
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kUseFakeUIForMediaStream)) {
+    fake_ui_factory_ = base::Bind([] {
+      return std::make_unique<FakeMediaStreamUIProxy>(
+          /*tests_use_fake_render_frame_hosts=*/false);
+    });
+  }
+
   DCHECK(audio_system_);
 
   if (!video_capture_provider) {
@@ -981,9 +987,10 @@ void MediaStreamManager::PostRequestToUI(
   // If using the fake UI, it will just auto-select from the available devices.
   // The fake UI doesn't work for desktop sharing requests since we can't see
   // its devices from here; always use the real UI for such requests.
-  if (use_fake_ui_ && request->video_type() != MEDIA_DESKTOP_VIDEO_CAPTURE) {
+  if (fake_ui_factory_ &&
+      request->video_type() != MEDIA_DESKTOP_VIDEO_CAPTURE) {
     if (!fake_ui_)
-      fake_ui_.reset(new FakeMediaStreamUIProxy());
+      fake_ui_ = fake_ui_factory_.Run();
 
     MediaStreamDevices devices = ConvertToMediaStreamDevices(
         request->audio_type(), enumeration[MEDIA_DEVICE_TYPE_AUDIO_INPUT]);
@@ -1441,10 +1448,10 @@ void MediaStreamManager::OnResume() {
 }
 
 void MediaStreamManager::UseFakeUIForTests(
-    std::unique_ptr<FakeMediaStreamUIProxy> fake_ui) {
+    base::Callback<std::unique_ptr<FakeMediaStreamUIProxy>(void)>
+        fake_ui_factory) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  use_fake_ui_ = true;
-  fake_ui_ = std::move(fake_ui);
+  fake_ui_factory_ = fake_ui_factory;
 }
 
 void MediaStreamManager::RegisterNativeLogCallback(
