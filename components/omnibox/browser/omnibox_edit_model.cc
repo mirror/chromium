383685ceed77b5818f0d66623b264767ee0ff9fe
Mark Pearson <mpearson@chromium.org>
2017-09-26 14:35:57 -0700

    Measure Usage of Omnibox Suggestions by Scheme
    Bug: 766438
    Change-Id: Ib0627398de070fb53a3239b7c8bf665071c9d8b0

--- components/omnibox/browser/omnibox_edit_model.cc
+++ components/omnibox/browser/omnibox_edit_model.cc
@@ -19,6 +19,7 @@
 #include "base/strings/utf_string_conversions.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/metrics/proto/omnibox_event.pb.h"
+#include "components/navigation_metrics/navigation_metrics.h"
 #include "components/omnibox/browser/autocomplete_classifier.h"
 #include "components/omnibox/browser/autocomplete_match_type.h"
 #include "components/omnibox/browser/autocomplete_provider.h"
@@ -657,6 +658,20 @@ void OmniboxEditModel::OpenMatch(AutocompleteMatch match,
         SEARCH_ENGINE_OTHER;
     UMA_HISTOGRAM_ENUMERATION("Omnibox.SearchEngineType", search_engine_type,
                               SEARCH_ENGINE_MAX);
+  } else {
+    // |match| is a URL navigation, not a search.
+    // For logging the below histogram, only record uses that depend on the
+    // omnibox suggestion system.  That is, exclude omnibox URL interactions
+    // that are treated as reloads or link-following (i.e., cut-and-paste of
+    // URLs).
+    if (!ui::PageTransitionTypeIncludingQualifiersIs(
+            match.transition, ui::PAGE_TRANSITION_RELOAD) &&
+        !ui::PageTransitionTypeIncludingQualifiersIs(
+            match.transition, ui::PAGE_TRANSITION_LINK)) {
+      DCHECK(ui::PageTransitionTypeIncludingQualifiersIs(
+          match.transition, ui::PAGE_TRANSITION_TYPED));
+      navigation_metrics::RecordOmniboxURLNavigation(match.destination_url);
+    }
   }
 
   if (disposition != WindowOpenDisposition::NEW_BACKGROUND_TAB) {
