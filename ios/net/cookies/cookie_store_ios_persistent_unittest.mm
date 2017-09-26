b720b198661c0fde6e7a21c92c54ff7bb3e53a0e
Mohammad Refaat <mrefaat@chromium.org>
2017-09-26 10:43:41 -0700

    Convert SystemCookieStore to Async.
    * Converts SystemCookieStore to Async
    * Add Async functions to NSHTTPSystemCookieStore and keep the synchronous
    functions.
    * Move creation time management to systemCookieStore.
    * Update cookie_store_ios_presistent_unittest & 
    cookie_store_ios_unittests to account for SystemCookieStore changes 
    * (As CookieStoreIOS is using systemCookieStore internally).
    
    Another CL is in progress to add WKSystemCookieStore & unit_tests for 
    systemCookieStore (to handle async methods) with template for both 
    NSHTTP & WK systemCookieStore.
    
    Bug: 759229, 759227, 767948
    Change-Id: I1c24c82540f4781a0bec77ebfa4a861ca7433391

--- ios/net/cookies/cookie_store_ios_persistent_unittest.mm
+++ ios/net/cookies/cookie_store_ios_persistent_unittest.mm
@@ -56,6 +56,7 @@ class CookieStoreIOSPersistentTest : public testing::Test {
  public:
   CookieStoreIOSPersistentTest()
       : kTestCookieURL("http://foo.google.com/bar"),
+        cookie_store_ios_client_(base::MakeUnique<TestCookieStoreIOSClient>()),
         backend_(new net::TestPersistentCookieStore),
         store_(
             base::MakeUnique<net::CookieStoreIOSPersistent>(backend_.get())) {
@@ -85,6 +86,7 @@ class CookieStoreIOSPersistentTest : public testing::Test {
 
  protected:
   base::MessageLoop loop_;
+  ScopedTestingCookieStoreIOSClient cookie_store_ios_client_;
   scoped_refptr<net::TestPersistentCookieStore> backend_;
   std::unique_ptr<net::CookieStoreIOS> store_;
   std::unique_ptr<net::CookieStore::CookieChangedSubscription>
