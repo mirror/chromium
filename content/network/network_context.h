7b68d6d2d5ab650a2033466429fefc14e5fbdbf8
Victor Costan <pwnall@chromium.org>
2017-09-25 18:40:55 -0700

    Movie cookie_manager.mojom to //services/network.
    Having the interface definition in the network service makes it usable
    from Blink.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ie851209458949f85c0da4395f3ce22e414dd18b7

--- content/network/network_context.h
+++ content/network/network_context.h
@@ -79,7 +79,7 @@ class CONTENT_EXPORT NetworkContext : public mojom::NetworkContext {
                               uint32_t process_id) override;
   void HandleViewCacheRequest(const GURL& url,
                               mojom::URLLoaderClientPtr client) override;
-  void GetCookieManager(mojom::CookieManagerRequest request) override;
+  void GetCookieManager(network::mojom::CookieManagerRequest request) override;
 
   // Called when the associated NetworkServiceImpl is going away. Guaranteed to
   // destroy NetworkContext's URLRequestContext.
