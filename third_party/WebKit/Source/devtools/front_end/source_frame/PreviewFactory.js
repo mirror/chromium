3d9304352ab8e7c45fe65d4875e518336a297617
Nathan Bruer <allada@chromium.org>
2017-09-26 16:12:55 -0700

    [Devtools] Regression: Fixed rendering of HTML and JSON in network
    * Fixed bug where network preview would not render JSON if it was
    fetched and and had an error code.
    
    * Fixed bug where network preview would try to render as  HTML
    agressively if requested via XHR.
    
    R=pfeldman,eostroukhov
    BUG=765286,767393,763921,760278
    
    Change-Id: Ice108ab173ce08eb949e0ee7d12c1ef664cf8d48

--- third_party/WebKit/Source/devtools/front_end/source_frame/PreviewFactory.js
+++ third_party/WebKit/Source/devtools/front_end/source_frame/PreviewFactory.js
@@ -10,30 +10,58 @@ SourceFrame.PreviewFactory = class {
    */
   static async createPreview(provider, mimeType) {
     var content = await provider.requestContent();
+    return this.createPreviewWithContentData(provider, {error: null, content: content, encoded: false}, mimeType);
+  }
+
+  /**
+   * @param {!Common.ContentProvider} originalProvider
+   * @param {!SDK.NetworkRequest.ContentData} contentData
+   * @param {string} mimeType
+   * @returns {!Promise<?UI.Widget>}
+   */
+  static async createPreviewWithContentData(originalProvider, contentData, mimeType) {
+    if (contentData.content === null || contentData.error)
+      return new UI.EmptyWidget(Common.UIString('Failed to load resource data'));
+
+    try {
+      var content = (contentData.encoded ? atob(contentData.content || '') : contentData.content) || '';
+    } catch (e) {
+      console.error('Could not decode content: ' + e);
+      return new UI.EmptyWidget(Common.UIString('Failed to decode response'));
+    }
+    mimeType = mimeType.replace(/;.*/, '');  // remove charset.
+
     if (!content)
       return new UI.EmptyWidget(Common.UIString('Nothing to preview'));
-
-    var parsedXML = SourceFrame.XMLView.parseXML(content, mimeType);
-    if (parsedXML)
-      return SourceFrame.XMLView.createSearchableView(parsedXML);
 
     // We support non-strict JSON parsing by parsing an AST tree which is why we offload it to a worker.
     var parsedJSON = await SourceFrame.JSONView.parseJSON(content);
     if (parsedJSON && typeof parsedJSON.data === 'object')
       return SourceFrame.JSONView.createSearchableView(/** @type {!SourceFrame.ParsedJSON} */ (parsedJSON));
 
-    var resourceType = provider.contentType() || Common.resourceTypes.Other;
-
-    if (resourceType.isTextType()) {
-      var highlighterType = mimeType.replace(/;.*/, '');  // remove charset
-      return SourceFrame.ResourceSourceFrame.createSearchableView(provider, highlighterType);
+    // We can assume the status code has been set already because fetching contentData should wait for request to be
+    // finished.
+    var whitelist = new Set(['text/html', 'text/plain', 'application/xhtml+xml']);
+    if (whitelist.has(mimeType)) {
+      var dataURL = Common.ContentProvider.contentAsDataURL(
+          content, mimeType, contentData.encoded, contentData.encoded ? 'utf-8' : null);
+      return dataURL ? new SourceFrame.HTMLView(dataURL) : null;
     }
+
+    var parsedXML = SourceFrame.XMLView.parseXML(content, mimeType);
+    if (parsedXML)
+      return SourceFrame.XMLView.createSearchableView(parsedXML);
+
+    var resourceType = originalProvider.contentType() || Common.resourceTypes.Other;
+
+    if (resourceType.isTextType())
+      return SourceFrame.ResourceSourceFrame.createSearchableView(originalProvider, mimeType);
 
     switch (resourceType) {
       case Common.resourceTypes.Image:
-        return new SourceFrame.ImageView(mimeType, provider);
+        return new SourceFrame.ImageView(mimeType, originalProvider);
       case Common.resourceTypes.Font:
-        return new SourceFrame.FontView(mimeType, provider);
+        return new SourceFrame.FontView(mimeType, originalProvider);
     }
     return null;
   }
