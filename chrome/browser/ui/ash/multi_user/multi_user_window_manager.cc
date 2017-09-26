b32de796cba42670691073263979a159481b63e3
James Cook <jamescook@chromium.org>
2017-09-26 13:45:02 -0700

    WIP: Eliminate ash::ShellDelegate::IsMultiProfilesEnabled
    WIP AddUserSessionPolicy ALLOWED
    
    
    default to add session policy allowed
    
    
    really fix CLC tests
    
    
    fix ChromeLauncherController tests
    
    
    back to the crappy MultiUserWindowManager check
    
    
    remove spam
    
    
    ash unit tests pass
    
    
    Fix inactive user notification test
    
    
    All the features work, but the tests fail.
    
    
    mroe stuff
    
    
    add some TODOs
    
    
    more hacks
    
    
    InactiveUserNotificationBlocker test passes
    
    
    rip out delegate, TrayUserTest fails
    
    
    kill SessionControllerClient
    
    
    moar, fix MultiUserWindowManager
    
    
    remove calls
    
    
    wip
    
    Bug:
    Change-Id: Ib1c0bcd5da3f9e9c32641b29e95385e696cc415a

--- chrome/browser/ui/ash/multi_user/multi_user_window_manager.cc
+++ chrome/browser/ui/ash/multi_user/multi_user_window_manager.cc
@@ -5,22 +5,34 @@
 #include "chrome/browser/ui/ash/multi_user/multi_user_window_manager.h"
 
 #include "ash/multi_profile_uma.h"
-#include "ash/shell.h"
-#include "ash/shell_delegate.h"
 #include "base/logging.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/ash/ash_util.h"
 #include "chrome/browser/ui/ash/multi_user/multi_user_window_manager_chromeos.h"
 #include "chrome/browser/ui/ash/multi_user/multi_user_window_manager_stub.h"
+#include "chrome/browser/ui/ash/session_controller_client.h"
 #include "components/signin/core/account_id/account_id.h"
 #include "components/user_manager/user_info.h"
 #include "components/user_manager/user_manager.h"
 
-namespace {
-chrome::MultiUserWindowManager* g_instance = NULL;
-}  // namespace
+using user_manager::UserManager;
 
 namespace chrome {
+namespace {
+
+MultiUserWindowManager* g_instance = nullptr;
+
+//JAMES CanAddMultiProfileUsers
+// bool IsMultiProfileAllowed() {
+//   // return ash::Shell::Get()->session_controller()->IsMultiProfileAvailable();
+//   if (UserManager::Get()->GetLoggedInUsers().size() > 1)
+//     return true;
+
+//   return SessionControllerClient::GetAddUserSessionPolicy() ==
+//          ash::AddUserSessionPolicy::ALLOWED;
+// }
+
+}  // namespace
 
 // Caching the current multi profile mode to avoid expensive detection
 // operations.
@@ -33,6 +45,7 @@ MultiUserWindowManager* MultiUserWindowManager::GetInstance() {
   return g_instance;
 }
 
+//JAMES I think this is called during login, during shelf creation, from CLC
 MultiUserWindowManager* MultiUserWindowManager::CreateInstance() {
   DCHECK(!g_instance);
   multi_user_mode_ = MULTI_PROFILE_MODE_OFF;
@@ -40,7 +53,8 @@ MultiUserWindowManager* MultiUserWindowManager::CreateInstance() {
       ash::MultiProfileUMA::SESSION_SINGLE_USER_MODE;
   // TODO(crbug.com/557406): Enable this component in Mash.
   if (!ash_util::IsRunningInMash() &&
-      ash::Shell::Get()->shell_delegate()->IsMultiProfilesEnabled()) {
+      SessionControllerClient::IsMultiProfileAvailable()) {
+    LOG(ERROR) << "JAMES making MUWMChromeOS";
     MultiUserWindowManagerChromeOS* manager =
         new MultiUserWindowManagerChromeOS(
             user_manager::UserManager::Get()->GetActiveUser()->GetAccountId());
@@ -48,6 +62,8 @@ MultiUserWindowManager* MultiUserWindowManager::CreateInstance() {
     manager->Init();
     multi_user_mode_ = MULTI_PROFILE_MODE_ON;
     mode = ash::MultiProfileUMA::SESSION_SEPARATE_DESKTOP_MODE;
+  } else {
+    LOG(ERROR) << "JAMES NOT MAKING MUWM!";
   }
   ash::MultiProfileUMA::RecordSessionMode(mode);
 
@@ -64,7 +80,7 @@ MultiUserWindowManager::GetMultiProfileMode() {
   return multi_user_mode_;
 }
 
-// satic
+// static
 bool MultiUserWindowManager::ShouldShowAvatar(aura::Window* window) {
   // Note: In case of the M-31 mode the window manager won't exist.
   if (GetMultiProfileMode() == MULTI_PROFILE_MODE_ON) {
