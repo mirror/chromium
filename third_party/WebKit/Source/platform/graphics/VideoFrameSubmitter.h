939b6391e576ec2909b2a6049e02947bd5383578
CJ DiMeglio <lethalantidote@chromium.org>
2017-09-26 13:36:59 -0700

    Obtains media ContextProvider for use in VideoFrameResourceProvider.
    This work is helpful in the cc::Surface for Videos work, as it allows
    easier access to this ContextProvider when obtaining Resources.
    
    Bug:746182
    Change-Id: Ie68c180a047abd36885ac5a6f70bd13b0e92af43

--- third_party/WebKit/Source/platform/graphics/VideoFrameSubmitter.h
+++ third_party/WebKit/Source/platform/graphics/VideoFrameSubmitter.h
@@ -22,7 +22,9 @@ class PLATFORM_EXPORT VideoFrameSubmitter
     : public WebVideoFrameSubmitter,
       public viz::mojom::blink::CompositorFrameSinkClient {
  public:
-  explicit VideoFrameSubmitter(cc::VideoFrameProvider*);
+  VideoFrameSubmitter(
+      cc::VideoFrameProvider*,
+      const base::Callback<void(base::Callback<void(viz::ContextProvider*)>)>&);
 
   ~VideoFrameSubmitter() override;
 
@@ -64,7 +66,9 @@ class PLATFORM_EXPORT VideoFrameSubmitter
   mojo::Binding<viz::mojom::blink::CompositorFrameSinkClient> binding_;
   viz::LocalSurfaceIdAllocator local_surface_id_allocator_;
   viz::LocalSurfaceId current_local_surface_id_;
-  VideoFrameResourceProvider resource_provider_;
+  const base::Callback<void(base::Callback<void(viz::ContextProvider*)>)>&
+      context_provider_callback_;
+  std::unique_ptr<VideoFrameResourceProvider> resource_provider_;
 
   bool is_rendering_;
 
