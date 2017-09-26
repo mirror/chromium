1944c47c3f53d57a6b8198a6b2377f335441fa84
Simon Que <sque@chromium.org>
2017-09-26 18:21:25 -0400

    feedbackPrivate: Add rate limiter for log source accesses
    This factors out the rate limiting of access to log sources into a
    separate module.
    
    In the future, we can update the rate limiting policy by updating only
    the rate limiter.
    
    BUG=768967
    
    Change-Id: Ic897112ad2adc58adac9e57ed95b424c532ae42e

---
+++ extensions/browser/api/feedback_private/log_source_access_rate_limiter.cc
@@ -0,0 +1,46 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "extensions/browser/api/feedback_private/log_source_access_rate_limiter.h"
+
+#include "base/time/default_tick_clock.h"
+
+namespace extensions {
+
+LogSourceAccessRateLimiter::LogSourceAccessRateLimiter(
+    size_t max_num_accesses,
+    const base::TimeDelta& recharge_period)
+    : max_num_accesses_(max_num_accesses),
+      recharge_period_(recharge_period),
+      recharge_counter_(recharge_period * max_num_accesses),
+      tick_clock_(&default_tick_clock_) {}
+
+LogSourceAccessRateLimiter::~LogSourceAccessRateLimiter() {}
+
+bool LogSourceAccessRateLimiter::TryAccess() {
+  if (recharge_period_.is_zero())
+    return true;
+
+  if (last_access_time_.is_null())
+    last_access_time_ = tick_clock_->NowTicks();
+
+  // First, attempt to recharge.
+  const base::TimeTicks now = tick_clock_->NowTicks();
+  recharge_counter_ += now - last_access_time_;
+  last_access_time_ = now;
+
+  // Did |num_accesses_| max out?
+  if (recharge_counter_ > recharge_period_ * max_num_accesses_) {
+    recharge_counter_ = recharge_period_ * max_num_accesses_;
+  }
+
+  // Is an immediate access available?
+  if (recharge_counter_ < recharge_period_)
+    return false;
+
+  recharge_counter_ -= recharge_period_;
+  return true;
+}
+
+}  // namespace extensions
