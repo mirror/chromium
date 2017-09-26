676c5e87101af061cf98837ce7573f2d109c404f
Biao She <bshe@chromium.org>
2017-09-26 11:19:56 -0400

    [Code cleanup]Rename view_proj_matrix to model_view_proj_matrix when appropriate
    Bug: None
    Change-Id: I17162ce72e6047e0d26613fad6a041703e8f2698

--- chrome/browser/vr/elements/full_screen_rect.h
+++ chrome/browser/vr/elements/full_screen_rect.h
@@ -18,7 +18,7 @@ class FullScreenRect : public Rect {
   ~FullScreenRect() override;
 
   void Render(UiElementRenderer* renderer,
-              const gfx::Transform& view_proj_matrix) const final;
+              const gfx::Transform& model_view_proj_matrix) const final;
 
   DISALLOW_COPY_AND_ASSIGN(FullScreenRect);
 };
