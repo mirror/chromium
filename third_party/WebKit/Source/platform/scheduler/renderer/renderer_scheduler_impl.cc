0c9ebbac09309168801505030909542df2c845eb
Alexander Timin <altimin@chromium.org>
2017-09-27 20:52:53 +0900

    [scheduler] Record task duration per frame type.
    Record main thread task duration for per-frame queues split by frame
    status (offscreen / onscreen / background / cross-origin etc).
    
    R=skyostil@chromium.org,rkaplow@chromium.org
    
    Change-Id: I9389f88d6b4487199439fa36074a0b421ae591ff

--- third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.cc
+++ third_party/WebKit/Source/platform/scheduler/renderer/renderer_scheduler_impl.cc
@@ -1926,8 +1926,7 @@ void RendererSchedulerImpl::OnTaskCompleted(MainThreadTaskQueue* queue,
   task_queue_throttler()->OnTaskRunTimeReported(queue, start, end);
 
   // TODO(altimin): Per-page metrics should also be considered.
-  main_thread_only().metrics_helper.RecordTaskMetrics(queue->queue_type(),
-                                                      start, end);
+  main_thread_only().metrics_helper.RecordTaskMetrics(queue, start, end);
 }
 
 void RendererSchedulerImpl::OnBeginNestedRunLoop() {
