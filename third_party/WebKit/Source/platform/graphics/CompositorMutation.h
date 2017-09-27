b4e2cea0ee27e84d9599436f4822d1a5736f428b
Peter Mayo <petermayo@chromium.org>
2017-09-26 20:19:00 -0400

    Remove CompositorMutation
    These could/should have gone away with CompositorProxy, but now that
    cc/trees/mutable_properties is gone too, they really do stick out as
    zombie code.
    
    Bug: 768852
    Change-Id: Ib33a0243acbbd504b7649d72ae89278459c45d26

--- third_party/WebKit/Source/platform/graphics/CompositorMutation.h
+++ third_party/WebKit/Source/platform/graphics/CompositorMutation.h
@@ -6,7 +6,6 @@
 #define CompositorMutation_h
 
 #include <memory>
-#include "platform/graphics/CompositorMutableProperties.h"
 #include "platform/wtf/HashMap.h"
 #include "third_party/skia/include/core/SkMatrix44.h"
 
@@ -14,35 +13,10 @@ namespace blink {
 
 class CompositorMutation {
  public:
-  void SetOpacity(float opacity) {
-    mutated_flags_ |= CompositorMutableProperty::kOpacity;
-    opacity_ = opacity;
-  }
-  void SetScrollLeft(float scroll_left) {
-    mutated_flags_ |= CompositorMutableProperty::kScrollLeft;
-    scroll_left_ = scroll_left;
-  }
-  void SetScrollTop(float scroll_top) {
-    mutated_flags_ |= CompositorMutableProperty::kScrollTop;
-    scroll_top_ = scroll_top;
-  }
-  void SetTransform(const SkMatrix44& transform) {
-    mutated_flags_ |= CompositorMutableProperty::kTransform;
-    transform_ = transform;
-  }
-
-  bool IsOpacityMutated() const {
-    return mutated_flags_ & CompositorMutableProperty::kOpacity;
-  }
-  bool IsScrollLeftMutated() const {
-    return mutated_flags_ & CompositorMutableProperty::kScrollLeft;
-  }
-  bool IsScrollTopMutated() const {
-    return mutated_flags_ & CompositorMutableProperty::kScrollTop;
-  }
-  bool IsTransformMutated() const {
-    return mutated_flags_ & CompositorMutableProperty::kTransform;
-  }
+  bool IsOpacityMutated() const { return mutated_flags_ & 4; }
+  bool IsScrollLeftMutated() const { return mutated_flags_ & 1; }
+  bool IsScrollTopMutated() const { return mutated_flags_ & 2; }
+  bool IsTransformMutated() const { return mutated_flags_ & 8; }
 
   float Opacity() const { return opacity_; }
   float ScrollLeft() const { return scroll_left_; }
