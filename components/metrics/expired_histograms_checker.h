c84485c6fefa58cc83eab7cfcb98c6748aa4df98
Ira Burak <iburak@google.com>
2017-09-27 10:31:12 -0400

    Use a Finch study to configure a set of expired histograms
    Bug:764030
    Change-Id: Ifd0946bf020458ef85f40ea35d81dcb9b93bc86f

--- components/metrics/expired_histograms_checker.h
+++ components/metrics/expired_histograms_checker.h
@@ -7,6 +7,9 @@
 
 #include <stdint.h>
 
+#include <string>
+#include <vector>
+
 #include "base/macros.h"
 #include "base/metrics/record_histogram_checker.h"
 
@@ -21,11 +24,25 @@ class ExpiredHistogramsChecker final : public base::RecordHistogramChecker {
   ~ExpiredHistogramsChecker() override;
 
   // Checks if the given |histogram_hash| corresponds to an expired histogram.
-  bool ShouldRecord(uint64_t histogram_hash) const override;
+  bool ShouldRecord(uint64_t histogram_hash) override;
 
  private:
+  // Loads parameters from variation server for enabling/disabling recording
+  // particular histograms regardless of their expiry date.
+  // Safe for multiple calls.
+  void LoadVariationParameters();
+
+  // An array with hashes of expired histograms.
   const uint64_t* const array_;
+  // Size of array_.
   const size_t size_;
+
+  // Hashes of the histograms that were affected by the variation parameters.
+  std::vector<uint64_t> enabled_histograms_;
+  std::vector<uint64_t> disabled_histograms_;
+
+  // True iff variation parameters were loaded.
+  bool variation_parameters_loaded_;
 
   DISALLOW_COPY_AND_ASSIGN(ExpiredHistogramsChecker);
 };
