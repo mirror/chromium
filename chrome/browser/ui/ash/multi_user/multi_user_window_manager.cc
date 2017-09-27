6acc1a46b5646a7d5634ab32e4e278a0d8f384e5
James Cook <jamescook@chromium.org>
2017-09-27 13:24:37 -0700

    cros: Eliminate MultiUserWindowManager::MultiUserMode
    There's only one mode (split desktops). The uninitialized state isn't
    needed anymore.  It was added a long time ago as an optimization to
    avoid looking up flag values and those flags no longer exist.
    
    Bug: none
    Test: unit_tests, browser_tests, manually test teleporting windows between user sessions and verify avatar icons appear in the frames
    Change-Id: Icc660ca2af047b39066ae501a5c6dc6a28f7aa8f

--- chrome/browser/ui/ash/multi_user/multi_user_window_manager.cc
+++ chrome/browser/ui/ash/multi_user/multi_user_window_manager.cc
@@ -20,12 +20,6 @@ namespace {
 MultiUserWindowManager* g_instance = nullptr;
 }  // namespace
 
-// Caching the current multi profile mode to avoid expensive detection
-// operations.
-MultiUserWindowManager::MultiProfileMode
-    MultiUserWindowManager::multi_user_mode_ =
-        MultiUserWindowManager::MULTI_PROFILE_MODE_UNINITIALIZED;
-
 // static
 MultiUserWindowManager* MultiUserWindowManager::GetInstance() {
   return g_instance;
@@ -33,7 +27,6 @@ MultiUserWindowManager* MultiUserWindowManager::GetInstance() {
 
 MultiUserWindowManager* MultiUserWindowManager::CreateInstance() {
   DCHECK(!g_instance);
-  multi_user_mode_ = MULTI_PROFILE_MODE_OFF;
   ash::MultiProfileUMA::SessionMode mode =
       ash::MultiProfileUMA::SESSION_SINGLE_USER_MODE;
   // TODO(crbug.com/557406): Enable this component in Mash. The object itself
@@ -45,7 +38,6 @@ MultiUserWindowManager* MultiUserWindowManager::CreateInstance() {
             user_manager::UserManager::Get()->GetActiveUser()->GetAccountId());
     g_instance = manager;
     manager->Init();
-    multi_user_mode_ = MULTI_PROFILE_MODE_ON;
     mode = ash::MultiProfileUMA::SESSION_SEPARATE_DESKTOP_MODE;
   }
   ash::MultiProfileUMA::RecordSessionMode(mode);
@@ -58,30 +50,22 @@ MultiUserWindowManager* MultiUserWindowManager::CreateInstance() {
 }
 
 // static
-MultiUserWindowManager::MultiProfileMode
-MultiUserWindowManager::GetMultiProfileMode() {
-  return multi_user_mode_;
-}
-
-// static
 bool MultiUserWindowManager::ShouldShowAvatar(aura::Window* window) {
-  // Note: In case of the M-31 mode the window manager won't exist.
-  if (GetMultiProfileMode() == MULTI_PROFILE_MODE_ON) {
-    // If the window is shown on a different desktop than the user, it should
-    // have the avatar icon
-    MultiUserWindowManager* instance = GetInstance();
-    return !instance->IsWindowOnDesktopOfUser(window,
-                                              instance->GetWindowOwner(window));
-  }
-  return false;
+  // Only show avatars when multiple users are signed in.
+  if (user_manager::UserManager::Get()->GetLoggedInUsers().size() < 2)
+    return false;
+
+  // Show the avatar icon if the window is on a different desktop than the
+  // window's owner's desktop.
+  return !g_instance->IsWindowOnDesktopOfUser(
+      window, g_instance->GetWindowOwner(window));
 }
 
 // static
 void MultiUserWindowManager::DeleteInstance() {
   DCHECK(g_instance);
   delete g_instance;
-  g_instance = NULL;
-  multi_user_mode_ = MULTI_PROFILE_MODE_UNINITIALIZED;
+  g_instance = nullptr;
 }
 
 void MultiUserWindowManager::SetInstanceForTest(
@@ -89,7 +73,6 @@ void MultiUserWindowManager::SetInstanceForTest(
   if (g_instance)
     DeleteInstance();
   g_instance = instance;
-  multi_user_mode_ = MULTI_PROFILE_MODE_ON;
 }
 
 }  // namespace chrome
