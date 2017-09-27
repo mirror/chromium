9a80d6aebb13123f955e37de3f4a731fcbf6834d
Lalit Maganti <lalitm@chromium.org>
2017-09-27 14:23:37 +0100

    [DO NOT SUBMIT] Process graph edges in memory instrumentation graph processor
    Bug:
    Change-Id: I5e6202fbfc71407075262ae9a610ea86e6bdd903

--- services/resource_coordinator/memory_instrumentation/graph.h
+++ services/resource_coordinator/memory_instrumentation/graph.h
@@ -20,6 +20,7 @@ namespace memory_instrumentation {
 class GlobalDumpGraph {
  public:
   class Node;
+  class Edge;
 
   // Graph of dumps either associated with a process or with
   // the shared space.
@@ -93,6 +94,13 @@ class GlobalDumpGraph {
     void AddEntry(std::string name, Entry::ScalarUnits units, uint64_t value);
     void AddEntry(std::string name, std::string value);
 
+    // Adds an edge which indicates that this node is owned by
+    // another node.
+    void AddOwnedByEdge(Edge* edge);
+
+    // Sets the edge indicates that this node owns another node.
+    void SetOwnsEdge(Edge* edge);
+
     const GlobalDumpGraph::Process* dump_graph() const { return dump_graph_; }
     const std::map<std::string, Entry>& entries() const { return entries_; }
 
@@ -101,7 +109,31 @@ class GlobalDumpGraph {
     std::map<std::string, Entry> entries_;
     std::map<std::string, Node*> children_;
 
+    GlobalDumpGraph::Edge* owns_edge_;
+    std::vector<GlobalDumpGraph::Edge*> owned_by_edges_;
+
     DISALLOW_COPY_AND_ASSIGN(Node);
+  };
+
+  // An edge in the dump graph which indicates ownership between the
+  // source and target nodes.
+  class Edge {
+   public:
+    Edge(GlobalDumpGraph::Node* source,
+         GlobalDumpGraph::Node* target,
+         int priority);
+
+    GlobalDumpGraph::Node* source() const { return source_; }
+    GlobalDumpGraph::Node* target() const { return target_; }
+    int priority() { return priority_; }
+    bool is_weak() { return weak_; }
+    void set_weak(bool weak) { weak_ = weak; }
+
+   private:
+    GlobalDumpGraph::Node* const source_;
+    GlobalDumpGraph::Node* const target_;
+    const int priority_;
+    bool weak_;
   };
 
   using ProcessDumpGraphMap =
@@ -115,6 +147,10 @@ class GlobalDumpGraph {
   // Creates a container for all the dump graphs for the process given
   // by the given |process_id|.
   GlobalDumpGraph::Process* CreateGraphForProcess(base::ProcessId process_id);
+
+  // Adds an edge in the dump graph with the given source and target nodes
+  // and edge priority.
+  bool AddNodeOwnershipEdge(Node* owner, Node* owned, int priority);
 
   const GuidNodeMap& nodes_by_guid() const { return nodes_by_guid_; }
   GlobalDumpGraph::Process* shared_memory_graph() const {
@@ -130,6 +166,7 @@ class GlobalDumpGraph {
   Node* CreateNode(GlobalDumpGraph::Process* dump_graph);
 
   std::forward_list<Node> all_nodes_;
+  std::forward_list<Edge> all_edges_;
   GuidNodeMap nodes_by_guid_;
   std::unique_ptr<GlobalDumpGraph::Process> shared_memory_graph_;
   ProcessDumpGraphMap process_dump_graphs_;
