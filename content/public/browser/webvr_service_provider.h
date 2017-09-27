04632ad95552e21f0c3fe6a9fb0148172fe2df71
Michael Thiessen <mthiesse@chromium.org>
2017-09-27 15:19:53 -0400

    Move VrServiceImpl and VrDeviceManager into chrome/browser/vr
    This CL moves VrServiceImpl and VrDeviceManager into chrome/browser/vr.
    No functionality changes yet. This is part of a larger refactoring of
    focus and device access management into chrome/browser/vr.
    
    Bug: 768923
    Change-Id: I38085259ccdd2e7d32a022e5a1b35cfa9718dad2

--- content/public/browser/webvr_service_provider.h
+++ content/public/browser/webvr_service_provider.h
@@ -1,4 +1,4 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -30,7 +30,7 @@ class WebvrServiceProvider {
       void(int, int, mojo::InterfaceRequest<device::mojom::VRService>)>;
 
   CONTENT_EXPORT static void SetWebvrServiceCallback(
-      BindWebvrServiceCallback callback);
+      const BindWebvrServiceCallback& callback);
 
  private:
   WebvrServiceProvider() = default;
