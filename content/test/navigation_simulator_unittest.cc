0f3ff18e5273de7cf5d19f1f6ff09adc182dd343
Lucas Garron <lgarron@chromium.org>
2017-09-27 15:45:14 -0700

    Committed interstitial throttle cert info plumbing (2/2): Add a WillFailRequest() method to NavigationThrottle.
    Bug: 751941
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I9a597851d018e3e42d884c185ba2c619b9d31923

--- content/test/navigation_simulator_unittest.cc
+++ content/test/navigation_simulator_unittest.cc
@@ -19,10 +19,35 @@
 #include "content/test/test_render_frame_host.h"
 #include "content/test/test_render_view_host.h"
 #include "content/test/test_web_contents.h"
+#include "net/base/net_errors.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
 namespace content {
+
+// A version of CancellingNavigationThrottle that also allows us to observe when
+// WillFailRequest() is called.
+class NavigationSimulatorTestThrottle : public CancellingNavigationThrottle {
+ public:
+  NavigationSimulatorTestThrottle(NavigationHandle* handle,
+                                  CancelTime cancel_time,
+                                  ResultSynchrony sync,
+                                  const base::Closure& on_will_fail_request)
+      : CancellingNavigationThrottle(handle, cancel_time, sync),
+        on_will_fail_request_(on_will_fail_request) {}
+
+  NavigationThrottle::ThrottleCheckResult WillFailRequest() override {
+    on_will_fail_request_.Run();
+    return CancellingNavigationThrottle::WillFailRequest();
+  }
+
+  const char* GetNameForLogging() override {
+    return "NavigationSimulatorTestThrottle";
+  }
+
+ private:
+  base::Closure on_will_fail_request_;
+};
 
 class NavigationSimulatorTest
     : public RenderViewHostImplTestHarness,
@@ -50,18 +75,26 @@ class NavigationSimulatorTest
 
   void DidStartNavigation(content::NavigationHandle* handle) override {
     handle->RegisterThrottleForTesting(
-        base::MakeUnique<CancellingNavigationThrottle>(handle, cancel_time_,
-                                                       sync_));
+        base::MakeUnique<NavigationSimulatorTestThrottle>(
+            handle, cancel_time_, sync_,
+            base::Bind(
+                &NavigationSimulatorTest::OnWillFailRequestCalledOnThrottle,
+                base::Unretained(this))));
   }
 
   void DidFinishNavigation(content::NavigationHandle* handle) override {
     did_finish_navigation_ = true;
   }
 
+  void OnWillFailRequestCalledOnThrottle() {
+    will_fail_request_called_on_throttle_ = true;
+  }
+
   CancellingNavigationThrottle::CancelTime cancel_time_;
   CancellingNavigationThrottle::ResultSynchrony sync_;
   std::unique_ptr<NavigationSimulator> simulator_;
   bool did_finish_navigation_ = false;
+  bool will_fail_request_called_on_throttle_ = false;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(NavigationSimulatorTest);
@@ -107,6 +140,45 @@ INSTANTIATE_TEST_CASE_P(
                           CancellingNavigationThrottle::WILL_REDIRECT_REQUEST,
                           CancellingNavigationThrottle::WILL_PROCESS_RESPONSE,
                           CancellingNavigationThrottle::NEVER),
+        ::testing::Values(CancellingNavigationThrottle::SYNCHRONOUS,
+                          CancellingNavigationThrottle::ASYNCHRONOUS)));
+
+// Create a version of the test class for parameterized testing.
+typedef NavigationSimulatorTest NavigationSimulatorTestCancelFail;
+
+// Test canceling the simulated navigation.
+TEST_P(NavigationSimulatorTestCancelFail, Fail) {
+  simulator_->Start();
+  simulator_->Fail(net::ERR_CERT_DATE_INVALID);
+  EXPECT_TRUE(will_fail_request_called_on_throttle_);
+  EXPECT_EQ(NavigationThrottle::CANCEL,
+            simulator_->GetLastThrottleCheckResult());
+}
+
+INSTANTIATE_TEST_CASE_P(
+    Fail,
+    NavigationSimulatorTestCancelFail,
+    ::testing::Combine(
+        ::testing::Values(CancellingNavigationThrottle::WILL_FAIL_REQUEST),
+        ::testing::Values(CancellingNavigationThrottle::SYNCHRONOUS,
+                          CancellingNavigationThrottle::ASYNCHRONOUS)));
+
+// Create a version of the test class for parameterized testing.
+typedef NavigationSimulatorTest NavigationSimulatorTestCancelFailErrAborted;
+
+// Test canceling the simulated navigation with net::ERR_ABORTED, which should
+// not call WillFailRequest on the throttle.
+TEST_P(NavigationSimulatorTestCancelFailErrAborted, Fail) {
+  simulator_->Start();
+  simulator_->Fail(net::ERR_ABORTED);
+  EXPECT_FALSE(will_fail_request_called_on_throttle_);
+}
+
+INSTANTIATE_TEST_CASE_P(
+    Fail,
+    NavigationSimulatorTestCancelFailErrAborted,
+    ::testing::Combine(
+        ::testing::Values(CancellingNavigationThrottle::WILL_FAIL_REQUEST),
         ::testing::Values(CancellingNavigationThrottle::SYNCHRONOUS,
                           CancellingNavigationThrottle::ASYNCHRONOUS)));
 
