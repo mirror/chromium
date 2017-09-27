a66c8ae6d38534f8ec6c7b7155b074657e880aed
Dan Elphick <delphick@chromium.org>
2017-09-27 15:22:03 +0100

    Convert BackgroundFetchDelegateImpl to use DownloadService
    Creates a new download::Client, BackgroundFetchDownloadClient, which
    forwards calls to the BackgroundFetchDelegateImpl.
    
    Bug: 758562
    Change-Id: I8fa30334b41713b1e1d9952448c521ae547bfc2d

--- content/browser/background_fetch/background_fetch_delegate_proxy.cc
+++ content/browser/background_fetch/background_fetch_delegate_proxy.cc
@@ -30,10 +30,15 @@ class BackgroundFetchDelegateProxy::Core
     // Some BrowserContext implementations return nullptr for their delegate
     // implementation and the feature should be disabled in that case.
     if (delegate_)
-      delegate_->SetDelegateClient(GetWeakPtrOnUI());
+      delegate_->SetDelegateClient(this);
   }
 
-  ~Core() override { DCHECK_CURRENTLY_ON(BrowserThread::UI); }
+  ~Core() override {
+    DCHECK_CURRENTLY_ON(BrowserThread::UI);
+
+    if (delegate_)
+      delegate_->SetDelegateClient(nullptr);
+  }
 
   base::WeakPtr<Core> GetWeakPtrOnUI() {
     DCHECK_CURRENTLY_ON(BrowserThread::UI);
@@ -46,8 +51,8 @@ class BackgroundFetchDelegateProxy::Core
     DCHECK_CURRENTLY_ON(BrowserThread::UI);
     DCHECK(request);
 
-    // TODO(crbug/757760): This can be nullptr, is if the delegate has shut
-    // down, in which case we need to make sure this is retried when the browser
+    // TODO(crbug/757760): This can be nullptr, if the delegate has shut down,
+    // in which case we need to make sure this is retried when the browser
     // restarts.
     if (!delegate_)
       return;
@@ -108,6 +113,8 @@ class BackgroundFetchDelegateProxy::Core
   void OnDownloadComplete(
       const std::string& guid,
       std::unique_ptr<BackgroundFetchResult> result) override;
+  void OnDownloadFailed(const std::string& guid,
+                        BackgroundFetchDelegate::FailureReason reason) override;
   void OnDownloadStarted(
       const std::string& guid,
       std::unique_ptr<content::BackgroundFetchResponse> response) override;
@@ -140,6 +147,12 @@ void BackgroundFetchDelegateProxy::Core::OnDownloadComplete(
       BrowserThread::IO, FROM_HERE,
       base::BindOnce(&BackgroundFetchDelegateProxy::OnDownloadComplete,
                      io_parent_, guid, std::move(result)));
+}
+
+void BackgroundFetchDelegateProxy::Core::OnDownloadFailed(
+    const std::string& guid,
+    BackgroundFetchDelegate::FailureReason reason) {
+  // TODO(delphick): do something with this
 }
 
 void BackgroundFetchDelegateProxy::Core::OnDownloadStarted(
