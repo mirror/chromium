b7772a55447064d2282207a627d267cf6d05e90c
yoshiki iguchi <yoshiki@chromium.org>
2017-09-27 10:47:19 +0900

    Remove the junk on expanding notification
    The default background of notification has vertical gray line for some reason
    and the background sometimes appears during animation for a short time. This
    is not good look so this CL overdraws it with a blank background.
    
    Bug: none
    Change-Id: Icf046a8e66c2091b0698850391871087a01118ef

--- ui/arc/notification/arc_notification_content_view.cc
+++ ui/arc/notification/arc_notification_content_view.cc
@@ -559,14 +559,19 @@ void ArcNotificationContentView::Layout() {
 void ArcNotificationContentView::OnPaint(gfx::Canvas* canvas) {
   views::NativeViewHost::OnPaint(canvas);
 
-  // Bail if there is a |surface_| or no item or no snapshot image.
-  if (surface_ || !item_ || item_->GetSnapshot().isNull())
-    return;
-  const gfx::Rect contents_bounds = GetContentsBounds();
-  canvas->DrawImageInt(item_->GetSnapshot(), 0, 0, item_->GetSnapshot().width(),
-                       item_->GetSnapshot().height(), contents_bounds.x(),
-                       contents_bounds.y(), contents_bounds.width(),
-                       contents_bounds.height(), false);
+  if (!surface_ && item_ && !item_->GetSnapshot().isNull()) {
+    // Bail if there is a |surface_| or no item or no snapshot image.
+    const gfx::Rect contents_bounds = GetContentsBounds();
+    const float scale = static_cast<float>(contents_bounds.width()) /
+                        item_->GetSnapshot().width();
+    canvas->DrawImageInt(
+        item_->GetSnapshot(), 0, 0, item_->GetSnapshot().width(),
+        item_->GetSnapshot().height(), contents_bounds.x(), contents_bounds.y(),
+        contents_bounds.width(), item_->GetSnapshot().height() * scale, false);
+  } else {
+    // Draw a blank background otherwise.
+    canvas->DrawColor(SK_ColorWHITE);
+  }
 }
 
 void ArcNotificationContentView::OnMouseEntered(const ui::MouseEvent&) {
