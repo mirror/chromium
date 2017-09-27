ead5c4b7fa25665b93aa3e974d5bf1e23426905d
Tomasz Garbus <tgarbus@chromium.org>
2017-09-27 15:43:21 +0200

    Show suppressed notify autosign-in snackbar once tab is shown.
    navigator.credentials.get() may be called from inactive tab. As of
    change 665119, showing the snackbar would be just suppressed.
    
    This CL stores PasswordForm, for which notification should be shown
    and, once tab is active again, shows the delayed notification.
    
    Also included an Earl Grey test case for this behavior.
    
    Here is a short screen recording from manual testing:
    https://drive.google.com/open?id=0B3hBvGfYBfe2Qml1X0pQRkJJQkE
    
    Bug: 435048
    Change-Id: I1ff3cf6f3858126d32f17d09ec9117b8c5d1a96e

--- ios/chrome/browser/passwords/credential_manager_egtest.mm
+++ ios/chrome/browser/passwords/credential_manager_egtest.mm
@@ -15,7 +15,9 @@
 #include "ios/chrome/browser/passwords/credential_manager_features.h"
 #include "ios/chrome/browser/passwords/ios_chrome_password_store_factory.h"
 #import "ios/chrome/test/app/chrome_test_util.h"
+#import "ios/chrome/test/app/tab_test_util.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
+#import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/testing/wait_util.h"
@@ -77,8 +79,7 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
   [super tearDown];
 }
 
-// Tests that notification saying "Signing is as ..." appears on auto sign-in.
-- (void)testNotificationAppearsOnAutoSignIn {
+- (void)loadSimplePageAndStoreACredential {
   // Loads simple page. It is on localhost so it is considered a secure context.
   const GURL URL = self.testServer->GetURL("/example");
   [ChromeEarlGrey loadURL:URL];
@@ -111,6 +112,11 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
   passwordCredentialForm.signon_realm = passwordCredentialForm.origin.spec();
   passwordCredentialForm.scheme = autofill::PasswordForm::SCHEME_HTML;
   passwordStore->AddLogin(passwordCredentialForm);
+}
+
+// Tests that notification saying "Signing is as ..." appears on auto sign-in.
+- (void)testNotificationAppearsOnAutoSignIn {
+  [self loadSimplePageAndStoreACredential];
 
   // Call get() from JavaScript.
   NSError* error = nil;
@@ -139,14 +145,57 @@ std::unique_ptr<net::test_server::HttpResponse> StandardResponse(
   // Wait for the notification to disappear.
   ConditionBlock waitForDisappearance = ^{
     NSError* error = nil;
-    [[EarlGrey selectElementWithMatcher:matcher] assertWithMatcher:grey_nil()
-                                                             error:&error];
+    [[EarlGrey selectElementWithMatcher:matcher]
+        assertWithMatcher:grey_sufficientlyVisible()
+                    error:&error];
     return error == nil;
   };
   // Ensures that notification disappears after time limit.
   GREYAssert(testing::WaitUntilConditionOrTimeout(kDisappearanceTimeout,
                                                   waitForDisappearance),
              @"Notification did not disappear");
+}
+
+// Tests that when navigator.credentials.get() was called from inactive tab, the
+// autosign-in notification appears once tab becomes active.
+- (void)testNotificationAppearsWhenTabIsActive {
+  [self loadSimplePageAndStoreACredential];
+
+  // Get WebState before switching the tab.
+  web::WebState* webState = chrome_test_util::GetCurrentWebState();
+
+  // Open new tab.
+  [ChromeEarlGreyUI openNewTab];
+  [ChromeEarlGrey waitForMainTabCount:2];
+
+  // Execute JavaScript from inactive tab.
+  webState->ExecuteJavaScript(
+      base::UTF8ToUTF16("typeof navigator.credentials.get({password: true})"));
+
+  // Matches the UILabel by its accessibilityLabel.
+  id<GREYMatcher> matcher = chrome_test_util::StaticTextWithAccessibilityLabel(
+      @"Signing in as johndoe@example.com");
+  // Wait for notification to appear.
+  ConditionBlock waitForAppearance = ^{
+    NSError* error = nil;
+    [[EarlGrey selectElementWithMatcher:matcher]
+        assertWithMatcher:grey_sufficientlyVisible()
+                    error:&error];
+    return error == nil;
+  };
+
+  // Check that notification doesn't appear in current tab.
+  GREYAssertFalse(testing::WaitUntilConditionOrTimeout(
+                      testing::kWaitForUIElementTimeout, waitForAppearance),
+                  @"Notification appeared in wrong tab");
+
+  // Switch to previous tab.
+  chrome_test_util::SelectTabAtIndexInCurrentMode(0);
+
+  // Check that the notification has appeared.
+  GREYAssert(testing::WaitUntilConditionOrTimeout(
+                 testing::kWaitForUIElementTimeout, waitForAppearance),
+             @"Notification did not appear");
 }
 
 @end
