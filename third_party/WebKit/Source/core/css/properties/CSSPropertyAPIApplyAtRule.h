d97567d5f0b567e5b098fadad3d1693cdb3eebee
Renee Wright <rjwright@chromium.org>
2017-09-27 19:27:41 +1000

    [Ribbon] Keep a CSSPropertyID in each CSSPropertyAPI
    This is needed for moving CSSProperty::ResolveDirectionAwareProperty
    into CSSPropertyAPI. It also allows backwards compatibility when we
    start passing around CSSPropertyAPI& instead of CSSPropertyID.
    
    This was originally written by meade@. That CL is here
    
    https://chromium-review.googlesource.com/c/chromium/src/+/662079
    
    I am re-uploading it because I need it & meade@ is away.
    
    Bug: 545324
    Change-Id: Iea3bd09d7a067070c1fb7981c62d4288e75ac65b

--- third_party/WebKit/Source/core/css/properties/CSSPropertyAPIApplyAtRule.h
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIApplyAtRule.h
@@ -5,12 +5,20 @@
 #ifndef CSSPropertyAPIApplyAtRule_h
 #define CSSPropertyAPIApplyAtRule_h
 
+#include "build/build_config.h"
 #include "core/css/properties/CSSPropertyAPI.h"
+#include "platform/RuntimeEnabledFeatures.h"
 
 namespace blink {
 
 class CSSPropertyAPIApplyAtRule : public CSSPropertyAPI {
  public:
+#if !defined(OS_WIN) || !defined(COMPILER_MSVC)
+  constexpr CSSPropertyAPIApplyAtRule(CSSPropertyID id) : CSSPropertyAPI(id) {}
+#else
+  CSSPropertyAPIApplyAtRule(CSSPropertyID id) : CSSPropertyAPI(id) {}
+#endif
+
   bool IsEnabled() const override {
     return RuntimeEnabledFeatures::CSSApplyAtRulesEnabled();
   }
