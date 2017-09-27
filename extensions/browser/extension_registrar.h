a40bc30eb47b01431f056b3eb9a1e0696e7bb20e
Michael Giuffrida <michaelpg@chromium.org>
2017-09-27 13:13:24 -0700

    Move more ExtensionService code to ExtensionRegistrar
    Adds several functions to ExtensionRegistrar:
    * AddExtension
    * RemoveExtension
    * ReplaceReloadedExtension
    * IsExtensionEnabled
    
    EnableExtension and DisableExtension have been augmented to do most of
    the work ExtensionService used to do here, and delegate the remaining
    work to the new ExtensionRegistrar::Delegate. The delegate can query
    policy, update the profile, and other things still part of //chrome.
    
    Design doc: https://goo.gl/trZKep (Google-internal).
    First CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/671206
    
    Change-Id: I7e5f43b458fd722137c0593b826a4b98d115387a

--- extensions/browser/extension_registrar.h
+++ extensions/browser/extension_registrar.h
@@ -5,6 +5,8 @@
 #ifndef EXTENSIONS_BROWSER_EXTENSION_REGISTRAR_H_
 #define EXTENSIONS_BROWSER_EXTENSION_REGISTRAR_H_
 
+#include <memory>
+
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
@@ -17,33 +19,57 @@ class BrowserContext;
 namespace extensions {
 
 class Extension;
+class ExtensionPrefs;
 class ExtensionRegistry;
 class ExtensionSystem;
 class RendererStartupHelper;
 
 // ExtensionRegistrar drives the stages of registering and unregistering
-// extensions for a BrowserContext.
-// TODO(michaelpg): Add functionality for reloading and terminating extensions.
+// extensions for a BrowserContext. It uses the ExtensionRegistry to track
+// extension states. Other classes may query the ExtensionRegistry directly,
+// but eventually only ExtensionRegistrar will be able to make changes to it.
 class ExtensionRegistrar {
  public:
-  explicit ExtensionRegistrar(content::BrowserContext* browser_context);
+  // Delegate for embedder-specific functionality like policy and permissions.
+  class Delegate {
+   public:
+    Delegate() = default;
+    virtual ~Delegate() = default;
+
+    // Handles updating the browser context when an extension is activated
+    // (becomes enabled).
+    virtual void PostActivateExtension(
+        scoped_refptr<const Extension> extension) = 0;
+
+    // Handles updating the browser context when an enabled extension is
+    // deactivated (whether disabled or removed).
+    virtual void PostDeactivateExtension(
+        scoped_refptr<const Extension> extension) = 0;
+
+    // Returns true if the extension is allowed to be enabled or disabled,
+    // respectively.
+    virtual bool CanEnableExtension(const Extension* extension) = 0;
+    virtual bool CanDisableExtension(const Extension* extension) = 0;
+
+    // Returns true if the extension should be blocked.
+    virtual bool ShouldBlockExtension(const Extension* extension) = 0;
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(Delegate);
+  };
+
+  explicit ExtensionRegistrar(content::BrowserContext* browser_context,
+                              Delegate* delegate);
   virtual ~ExtensionRegistrar();
 
-  // Marks |extension| enabled and notifies other components about it.
-  // TODO(michaelpg): Move other steps and checks for enabling the extension
-  // from ExtensionService::EnableExtension into here.
-  void EnableExtension(scoped_refptr<const Extension> extension);
-
-  // Marks |extension| disabled and notifies other components that it is
-  // disabled. The ExtensionRegistry retains a reference to it in
-  // disabled_extensions().
-  // TODO(michaelpg): Move other steps for disabling the extension from
-  // ExtensionService::DisableExtension into here.
-  void DisableExtension(scoped_refptr<const Extension> extension);
+  // Adds the extension to the ExtensionRegistry. The extension will be added to
+  // the enabled, disabled, blacklisted or blocked set. If the extension is
+  // added as enabled, it will be activated.
+  void AddExtension(scoped_refptr<const Extension> extension);
 
   // Removes |extension| from the extension system by deactivating it if it is
-  // enabled and removing references to it from the ExtensionRegistry's enabled
-  // or disabled sets.
+  // enabled and removing references to it from the ExtensionRegistry's
+  // enabled or disabled sets.
   // Note: Extensions will not be removed from other sets (terminated,
   // blacklisted or blocked). ExtensionService handles that, since it also adds
   // it to those sets. TODO(michaelpg): Make ExtensionRegistrar the sole mutator
@@ -51,7 +77,32 @@ class ExtensionRegistrar {
   void RemoveExtension(const ExtensionId& extension_id,
                        UnloadedExtensionReason reason);
 
+  // If the extension is disabled, marks it as enabled and activates it for use.
+  // Otherwise, simply updates the ExtensionPrefs. (Blacklisted or blocked
+  // extensions cannot be enabled.)
+  // Returns the extension if it was activated.
+  const Extension* EnableExtension(const ExtensionId& extension_id);
+
+  // Marks |extension| as disabled and deactivates it. The ExtensionRegistry
+  // retains a reference to it, so it can be enabled later.
+  void DisableExtension(const ExtensionId& extension_id, int disable_reasons);
+
+  // Given an extension that was disabled for reloading, completes the reload
+  // by replacing the old extension with the new version and enabling it.
+  // Returns true on success.
+  bool ReplaceReloadedExtension(scoped_refptr<const Extension> extension);
+
+  // TODO(michaelpg): Add methods for blacklisting and blocking extensions.
+
+  // Returns true if the extension is enabled (including terminated), or if it
+  // is not loaded but isn't explicitly disabled in preferences.
+  bool IsExtensionEnabled(const ExtensionId& extension_id) const;
+
  private:
+  // Activates |extension| by marking it enabled and notifying other components
+  // about it.
+  void ActivateExtension(const Extension* extension);
+
   // Triggers the unloaded notifications to deactivate an extension.
   void DeactivateExtension(const Extension* extension,
                            UnloadedExtensionReason reason);
@@ -69,9 +120,12 @@ class ExtensionRegistrar {
 
   // We are owned by the ExtensionSystem, which guarantees that these objects
   // outlive us.
+  content::BrowserContext* browser_context_;
+  Delegate* delegate_;
   ExtensionSystem* const extension_system_;
+  ExtensionPrefs* extension_prefs_;
   ExtensionRegistry* const registry_;
-  extensions::RendererStartupHelper* renderer_helper_;
+  RendererStartupHelper* renderer_helper_;
 
   base::WeakPtrFactory<ExtensionRegistrar> weak_factory_;
 
