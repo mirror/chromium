d8ce23b3ff85bde1aaac5d1f533f98a49d7fb73b
David Reveman <reveman@chromium.org>
2017-09-26 20:37:28 -0400

    Change Picture: Motion Stills
    This takes advantage of the animated image support added
    recently to allow users to capture short looping high-quality
    mini videos as avatar picture. Videos are created using the
    following parameters:
    
    - 10 photos taken over the interval of 1 second.
    - Stitched together in a seamless forwards/backwards loop.
    - Playback at 20 FPS.
    - Final animation time is 1 second.
    - Playback is 2X the speed of capture. 
    
    The profile picture UI has been updated to support capture of
    motion stills by allowing the user to switch between photo and
    video capture by clicking a button in the camera element.
    
    Bug: 763735
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ia921646cdfa649cbe7294d521f5d29f0bddc3035

--- ui/webui/resources/cr_elements/chromeos/cr_picture/cr_camera.js
+++ ui/webui/resources/cr_elements/chromeos/cr_picture/cr_camera.js
@@ -13,15 +13,39 @@
  * Dimensions for camera capture.
  * @const
  */
-var CAPTURE_SIZE = {height: 576, width: 576};
+var CAPTURE_SIZE = {width: 576, height: 576};
+
+/**
+ * Interval between frames for camera capture (milliseconds).
+ * @const
+ */
+var CAPTURE_INTERVAL_MS = 1000 / 10;
+
+/**
+ * Duration of camera capture (milliseconds).
+ * @const
+ */
+var CAPTURE_DURATION_MS = 1000;
 
 Polymer({
   is: 'cr-camera',
 
+  behaviors: [CrPngBehavior],
+
   properties: {
     /** Strings provided by host */
-    flipPhotoLabel: String,
     takePhotoLabel: String,
+    switchModeLabel: String,
+
+    /**
+     * True if currently in video mode.
+     * @private {boolean}
+     */
+    videomode: {
+      type: Boolean,
+      value: false,
+      reflectToAttribute: true,
+    },
 
     /**
      * True when the camera is actually streaming video. May be false even when
@@ -32,20 +56,10 @@ Polymer({
       type: Boolean,
       value: false,
     },
-
-    /**
-     * True if the photo is currently marked flipped.
-     * @private {boolean}
-     */
-    isFlipped_: {
-      type: Boolean,
-      value: true,
-    },
   },
 
   /** @override */
   attached: function() {
-    this.$.userImageStreamCrop.classList.toggle('flip-x', this.isFlipped_);
     this.$.cameraVideo.addEventListener('canplay', function() {
       this.cameraOnline_ = true;
     }.bind(this));
@@ -65,17 +79,68 @@ Polymer({
   takePhoto: function() {
     if (!this.cameraOnline_)
       return;
-    var canvas =
-        /** @type {HTMLCanvasElement} */ (document.createElement('canvas'));
-    canvas.width = CAPTURE_SIZE.width;
-    canvas.height = CAPTURE_SIZE.height;
-    this.captureFrame_(
-        this.$.cameraVideo,
-        /** @type {!CanvasRenderingContext2D} */ (canvas.getContext('2d')));
 
-    var photoDataUrl = this.isFlipped_ ? this.flipFrame_(canvas) :
-                                         canvas.toDataURL('image/png');
-    this.fire('photo-taken', {photoDataUrl: photoDataUrl});
+    /** Pre-allocate all frames needed for capture. */
+    var captureSize = this.getCaptureSize_();
+    var numFramesToCapture =
+        this.videomode ? CAPTURE_DURATION_MS / CAPTURE_INTERVAL_MS : 1;
+    var allocatedFrames = [];
+    for (var i = 0; i < numFramesToCapture; ++i) {
+      var canvas =
+          /** @type {!HTMLCanvasElement} */ (document.createElement('canvas'));
+      canvas.width = captureSize.width;
+      canvas.height = captureSize.height;
+      var ctx =
+          /** @type {!CanvasRenderingContext2D} */ (
+              canvas.getContext('2d', {alpha: false}));
+      // Flip frame horizontally.
+      ctx.translate(captureSize.width, 0);
+      ctx.scale(-1.0, 1.0);
+      allocatedFrames.push(canvas);
+    }
+
+    /** Start capturing frames at an interval. */
+    var capturedFrames = [];
+    this.$.userImageStreamCrop.classList.add('capture');
+    var interval = setInterval(function() {
+      var canvas = allocatedFrames.pop();
+      var ctx =
+          /** @type {!CanvasRenderingContext2D} */ (
+              canvas.getContext('2d', {alpha: false}));
+      this.captureFrame_(this.$.cameraVideo, ctx);
+      capturedFrames.push(canvas);
+
+      /** Stop capturing frames when all allocated frames have been consumed. */
+      if (allocatedFrames.length == 0) {
+        this.$.userImageStreamCrop.classList.remove('capture');
+        clearInterval(interval);
+
+        /** Encode captured frames. */
+        var encodedFrames = [];
+        for (var i = 0; i < capturedFrames.length; ++i)
+          encodedFrames.push(capturedFrames[i].toDataURL('image/png'));
+
+        /** No need for further processing if we captured a single frame. */
+        if (encodedFrames.length == 1) {
+          this.fire('photo-taken', {photoDataUrl: encodedFrames[0]});
+          return;
+        }
+
+        /** Create forward/backward image sequence. */
+        var forwardBackwardImageSequence = [];
+        for (var i = 0; i < encodedFrames.length; ++i)
+          forwardBackwardImageSequence.push(encodedFrames[i]);
+        for (var i = encodedFrames.length - 2; i > 0; --i)
+          forwardBackwardImageSequence.push(encodedFrames[i]);
+
+        /** Convert image sequence to animated PNG and fire 'photo-taken' event.
+         */
+        this.fire('photo-taken', {
+          photoDataUrl: CrPngBehavior.convertImageSequenceToPng(
+              forwardBackwardImageSequence)
+        });
+      }
+    }.bind(this), CAPTURE_INTERVAL_MS);
   },
 
   /** Tries to start the camera stream capture. */
@@ -123,13 +188,25 @@ Polymer({
   },
 
   /**
-   * Flip the live camera stream.
+   * Switch between photo and video mode.
    * @private
    */
-  onTapFlipPhoto_: function() {
-    this.isFlipped_ = !this.isFlipped_;
-    this.$.userImageStreamCrop.classList.toggle('flip-x', this.isFlipped_);
-    this.fire('photo-flipped', this.isFlipped_);
+  onTapSwitchMode_: function() {
+    this.videomode = !this.videomode;
+    this.fire('switch-mode', this.videomode);
+  },
+
+  /**
+   * Returns capture size based on current camera mode.
+   * @return {{width: number, height: number}} The capture size.
+   * @private
+   */
+  getCaptureSize_: function() {
+    /** Reduce capture size when in video mode. */
+    if (this.videomode) {
+      return {height: CAPTURE_SIZE.height / 2, width: CAPTURE_SIZE.width / 2};
+    }
+    return CAPTURE_SIZE;
   },
 
   /**
@@ -142,42 +219,26 @@ Polymer({
   captureFrame_: function(video, ctx) {
     var width = video.videoWidth;
     var height = video.videoHeight;
-    if (width < CAPTURE_SIZE.width || height < CAPTURE_SIZE.height) {
+    var captureSize = this.getCaptureSize_();
+    if (width < captureSize.width || height < captureSize.height) {
       console.error(
           'Video capture size too small: ' + width + 'x' + height + '!');
     }
     var src = {};
-    if (width / CAPTURE_SIZE.width > height / CAPTURE_SIZE.height) {
+    if (width / captureSize.width > height / captureSize.height) {
       // Full height, crop left/right.
       src.height = height;
-      src.width = height * CAPTURE_SIZE.width / CAPTURE_SIZE.height;
+      src.width = height * captureSize.width / captureSize.height;
     } else {
       // Full width, crop top/bottom.
       src.width = width;
-      src.height = width * CAPTURE_SIZE.height / CAPTURE_SIZE.width;
+      src.height = width * captureSize.height / captureSize.width;
     }
     src.x = (width - src.width) / 2;
     src.y = (height - src.height) / 2;
     ctx.drawImage(
-        video, src.x, src.y, src.width, src.height, 0, 0, CAPTURE_SIZE.width,
-        CAPTURE_SIZE.height);
-  },
-
-  /**
-   * Flips frame horizontally.
-   * @param {!(HTMLImageElement|HTMLCanvasElement|HTMLVideoElement)} source
-   *     Frame to flip.
-   * @return {string} Flipped frame as data URL.
-   */
-  flipFrame_: function(source) {
-    var canvas = document.createElement('canvas');
-    canvas.width = CAPTURE_SIZE.width;
-    canvas.height = CAPTURE_SIZE.height;
-    var ctx = canvas.getContext('2d');
-    ctx.translate(CAPTURE_SIZE.width, 0);
-    ctx.scale(-1.0, 1.0);
-    ctx.drawImage(source, 0, 0);
-    return canvas.toDataURL('image/png');
+        video, src.x, src.y, src.width, src.height, 0, 0, captureSize.width,
+        captureSize.height);
   },
 });
 
