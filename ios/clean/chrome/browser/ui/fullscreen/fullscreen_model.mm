bec8496e64a27a222dba9d51830b33185d8c37e5
Kurt Horimoto <kkhorimoto@chromium.org>
2017-09-27 14:07:27 -0700

    [iOS Clean] Implement fullscreen support in new architecture.
    Fullscreen functionality is split across several classes:
    - FullscreenController: This is the main entry point into fullscreen.
      It sets up helper objects to handle the calculation and exposes an
      observer API that can be used to communicate calculated changes back
      to the coordinator/UI layers.
    - FullscreenModel: The main model object for fullscreen.  This takes
      broadcasted UI state and calculates how much of the toolbar should
      be visible.
    - FullscreenBroadcastForwarder: An object that listens for broadcasted
      UI changes and forwards that data into FullscreenModel.
    - FullscreenWebStateObserver: A WebStateObserver that updates the model
      for navigation events.
    
    Bug: 769089
    Change-Id: I97400a9e9c98d51de56791cdf3e98fc7c8115ff0

---
+++ ios/clean/chrome/browser/ui/fullscreen/fullscreen_model.mm
@@ -0,0 +1,109 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/clean/chrome/browser/ui/fullscreen/fullscreen_model.h"
+
+#include <algorithm>
+
+#include "base/logging.h"
+#import "ios/chrome/browser/ui/uikit_ui_util.h"
+#import "ios/clean/chrome/browser/ui/fullscreen/fullscreen_controller.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+FullscreenModel::FullscreenModel(FullscreenController* controller)
+    : controller_(controller) {
+  DCHECK(controller_);
+}
+
+bool FullscreenModel::IsToolbarFullyVisible() const {
+  return CGFloatEquals(progress(), 1.0);
+}
+
+bool FullscreenModel::IsToolbarFullyHidden() const {
+  return CGFloatEquals(progress(), 0.0);
+}
+
+void FullscreenModel::IncrementDisabledCounter() {
+  if (++disabled_counter_ == 1U)
+    controller_->OnFullscreenDisabled();
+}
+
+void FullscreenModel::DecrementDisabledCounter() {
+  DCHECK_GT(disabled_counter_, 0U);
+  if (!--disabled_counter_)
+    controller_->OnFullscreenEnabled();
+}
+
+void FullscreenModel::ResetForNavigation() {
+  SetProgress(1.0);
+  scrolling_ = false;
+  update_base_offset_ = true;
+}
+
+void FullscreenModel::AnimationEndedWithProgress(CGFloat progress) {
+  SetProgress(progress, false);
+  if (!locked_)
+    UpdateBaseOffset();
+}
+
+void FullscreenModel::SetToolbarHeight(CGFloat toolbar_height) {
+  DCHECK_GE(toolbar_height, 0.0);
+  toolbar_height_ = toolbar_height;
+  SetProgress(1.0);
+}
+
+void FullscreenModel::SetYContentOffset(CGFloat y_content_offset) {
+  y_content_offset_ = y_content_offset;
+
+  if (update_base_offset_) {
+    UpdateBaseOffset();
+    update_base_offset_ = false;
+  }
+
+  if (scrolling_) {
+    CGFloat delta = base_offset_ - y_content_offset_;
+    SetProgress(1.0 + delta / toolbar_height_);
+  } else if (!locked_) {
+    // If the base offset is not locked, update it for programmatic scrolls.
+    base_offset_ = y_content_offset;
+  }
+}
+
+void FullscreenModel::SetScrollViewIsScrolling(bool scrolling) {
+  if (scrolling_ == scrolling)
+    return;
+  scrolling_ = scrolling;
+  if (!scrolling_)
+    controller_->OnScrollEventEnded();
+}
+
+void FullscreenModel::SetScrollViewIsDragging(bool dragging) {
+  if (dragging_ == dragging)
+    return;
+  dragging_ = dragging;
+  if (dragging_) {
+    update_base_offset_ = true;
+  }
+}
+
+void FullscreenModel::SetBaseOffsetLocked(bool locked) {
+  locked_ = locked;
+}
+
+void FullscreenModel::SetProgress(CGFloat progress, bool notify_observers) {
+  progress = std::min(static_cast<CGFloat>(1.0), progress);
+  progress = std::max(static_cast<CGFloat>(0.0), progress);
+  if (CGFloatEquals(progress_, progress))
+    return;
+  progress_ = progress;
+  if (notify_observers)
+    controller_->OnProgressUpdated();
+}
+
+void FullscreenModel::UpdateBaseOffset() {
+  base_offset_ = y_content_offset_ - (1.0 - progress_) * toolbar_height_;
+}
---
+++ ios/clean/chrome/browser/ui/fullscreen/fullscreen_ui_updater.mm
@@ -0,0 +1,34 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/clean/chrome/browser/ui/fullscreen/fullscreen_ui_updater.h"
+
+#import "ios/clean/chrome/browser/ui/fullscreen/fullscreen_ui_element.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+FullscreenUIUpdater::FullscreenUIUpdater(id<FullscreenUIElement> ui_element)
+    : ui_element_(ui_element) {}
+
+void FullscreenUIUpdater::FullscreenProgressUpdated(
+    FullscreenController* controller,
+    CGFloat progress) {
+  [ui_element_ updateForFullscreenProgress:progress];
+}
+
+void FullscreenUIUpdater::FullscreenDisabled(FullscreenController* controller) {
+  [ui_element_ updateForFullscreenEnabled:false];
+}
+
+void FullscreenUIUpdater::FullscreenEnabled(FullscreenController* controller) {
+  [ui_element_ updateForFullscreenEnabled:true];
+}
+
+void FullscreenUIUpdater::FullscreenScrollEventEnded(
+    FullscreenController* controller,
+    FullscreenScrollEndAnimator* animator) {
+  [ui_element_ finishFullscreenScrollWithAnimator:animator];
+}
