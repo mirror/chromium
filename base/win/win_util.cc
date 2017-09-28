434f6d7f9f62f9ba7c002f5e19bf1efda6a85619
Peter Boström <pbos@chromium.org>
2017-09-28 14:49:22 -0700

    Enable high-DPI support for browser_tests.
    Permits browser_tests to look right on high-DPI monitors. This is
    especially important for the UI team as browser_tests can be invoked to
    summon dialogs that are not easy to get to, to see if they look right.
    
    Without high-DPI support Windows will upscale the normal-DPI version of
    Chromium and everything will look blurry.
    
    BUG=chromium:769430
    Change-Id: Ia3cd12a7ec92f06302f51c234e047500ad35cefe

--- base/win/win_util.cc
+++ base/win/win_util.cc
@@ -89,6 +89,35 @@ POWER_PLATFORM_ROLE GetPlatformRole() {
   return PowerDeterminePlatformRoleEx(POWER_PLATFORM_ROLE_V2);
 }
 
+// Method used for Windows 8.1 and later.
+bool SetProcessDpiAwarenessWrapper(PROCESS_DPI_AWARENESS value) {
+  typedef HRESULT(WINAPI * SetProcessDpiAwarenessPtr)(PROCESS_DPI_AWARENESS);
+  SetProcessDpiAwarenessPtr set_process_dpi_awareness_func =
+      reinterpret_cast<SetProcessDpiAwarenessPtr>(GetProcAddress(
+          GetModuleHandleA("user32.dll"), "SetProcessDpiAwarenessInternal"));
+  if (set_process_dpi_awareness_func) {
+    HRESULT hr = set_process_dpi_awareness_func(value);
+    if (SUCCEEDED(hr)) {
+      VLOG(1) << "SetProcessDpiAwareness succeeded.";
+      return true;
+    } else if (hr == E_ACCESSDENIED) {
+      LOG(ERROR) << "Access denied error from SetProcessDpiAwareness. "
+                    "Function called twice, or manifest was used.";
+    }
+  }
+  return false;
+}
+
+// This function works for Windows 7 and 8.0. Win8.1 must use
+// SetProcessDpiAwareness[Wrapper].
+BOOL SetProcessDPIAwareWrapper() {
+  typedef BOOL(WINAPI * SetProcessDPIAwarePtr)(VOID);
+  SetProcessDPIAwarePtr set_process_dpi_aware_func =
+      reinterpret_cast<SetProcessDPIAwarePtr>(
+          GetProcAddress(GetModuleHandleA("user32.dll"), "SetProcessDPIAware"));
+  return set_process_dpi_aware_func && set_process_dpi_aware_func();
+}
+
 }  // namespace
 
 // Uses the Windows 10 WRL API's to query the current system state. The API's
@@ -670,6 +699,19 @@ bool IsProcessPerMonitorDpiAware() {
     }
   }
   return per_monitor_dpi_aware == PerMonitorDpiAware::PER_MONITOR_DPI_AWARE;
+}
+
+void EnableHighDPISupport() {
+  // Enable per-monitor DPI for Win10 or above instead of Win8.1 since Win8.1
+  // does not have EnableChildWindowDpiMessage, necessary for correct non-client
+  // area scaling across monitors.
+  bool allowed_platform = base::win::GetVersion() >= base::win::VERSION_WIN10;
+  PROCESS_DPI_AWARENESS process_dpi_awareness =
+      allowed_platform ? PROCESS_PER_MONITOR_DPI_AWARE
+                       : PROCESS_SYSTEM_DPI_AWARE;
+  if (!SetProcessDpiAwarenessWrapper(process_dpi_awareness)) {
+    SetProcessDPIAwareWrapper();
+  }
 }
 
 }  // namespace win
