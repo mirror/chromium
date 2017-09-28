f48ed3334cb1fa70b78bbbf23d2863011e4275bd
Colin Blundell <blundell@chromium.org>
2017-09-28 15:31:12 +0200

    Port chrome.identity.onSignInChanged impl away from AccountTracker
    The AccountTracker class is deprecated, with an eye toward complete
    removal (see details in crbug.com/729590). This CL ports the
    implementation of the chrome.identity.onSignInChanged event away from
    being backed by AccountTracker, changing it to instead be backed
    directly by SigninManager and ProfileOAuth2TokenService (the classes
    that AccountTracker is itself internally backed by in the IdentityAPI
    context). We preserve the semantics that were precisely defined in
    https://chromium-review.googlesource.com/c/593654.
    
    This change is an incremental step toward introducing the ability to
    observe signin changes via the Identity Service and changing
    this implementation to be backed by the Identity Service. As the
    Identity Service is itself backed directly by SigninManager and
    ProfileOAuth2TokenService, making this intermediate step will make the
    transformation to come a smaller jump.
    
    Bug: 729589, 72590
    Test: Install a Chrome extension with the identity permissions in
    Change-Id: I99dd48d34b380067ac34a2207effd9d3279191fd
    its manifest. Go to chrome://extensions, enable developer mode, and
    inspect the background page of the above app. At the JS console that
    that brings up, execute:
    chrome.identity.getAuthToken({interactive: true}, (token) => {console.log(token);} )
    Sign out of the browser. Verify that you receive a callback at the console.

--- chrome/browser/extensions/api/identity/identity_api.h
+++ chrome/browser/extensions/api/identity/identity_api.h
@@ -27,10 +27,9 @@
 #include "chrome/browser/extensions/api/identity/identity_remove_cached_auth_token_function.h"
 #include "chrome/browser/extensions/api/identity/web_auth_flow.h"
 #include "chrome/browser/extensions/chrome_extension_function.h"
-#include "components/signin/core/browser/profile_identity_provider.h"
+#include "components/signin/core/browser/signin_manager.h"
 #include "extensions/browser/browser_context_keyed_api_factory.h"
 #include "extensions/browser/event_router.h"
-#include "google_apis/gaia/account_tracker.h"
 #include "google_apis/gaia/oauth2_mint_token_flow.h"
 #include "google_apis/gaia/oauth2_token_service.h"
 
@@ -72,7 +71,8 @@ class IdentityTokenCacheValue {
 };
 
 class IdentityAPI : public BrowserContextKeyedAPI,
-                    public gaia::AccountTracker::Observer {
+                    public OAuth2TokenService::Observer,
+                    public SigninManagerBase::Observer {
  public:
   typedef std::map<ExtensionTokenKey, IdentityTokenCacheValue> CachedTokens;
 
@@ -96,19 +96,10 @@ class IdentityAPI : public BrowserContextKeyedAPI,
   void Shutdown() override;
   static BrowserContextKeyedAPIFactory<IdentityAPI>* GetFactoryInstance();
 
-  // gaia::AccountTracker::Observer implementation:
-  void OnAccountSignInChanged(const gaia::AccountIds& ids,
-                              bool is_signed_in) override;
-
   std::unique_ptr<base::CallbackList<void()>::Subscription>
   RegisterOnShutdownCallback(const base::Closure& cb) {
     return on_shutdown_callback_list_.Add(cb);
   }
-
-  // TODO(blundell): Eliminate this method once this class is no longer using
-  // AccountTracker.
-  // Makes |account_tracker_| aware of this account.
-  void SetAccountStateForTesting(const std::string& account_id, bool signed_in);
 
   // Callback that is used in testing contexts to test the implementation of
   // the chrome.identity.onSignInChanged event. Note that the passed-in Event is
@@ -126,15 +117,33 @@ class IdentityAPI : public BrowserContextKeyedAPI,
   static const char* service_name() { return "IdentityAPI"; }
   static const bool kServiceIsNULLWhileTesting = true;
 
+  // OAuth2TokenService::Observer implementation.
+  void OnRefreshTokenAvailable(const std::string& account_id) override;
+  void OnRefreshTokenRevoked(const std::string& account_id) override;
+
+  // SigninManagerBase::Observer:
+  void GoogleSigninSucceeded(const std::string& account_id,
+                             const std::string& username) override;
+  void GoogleSignedOut(const std::string& account_id,
+                       const std::string& username) override;
+
+  // Fires the chrome.identity.onSignInChanged event.
+  void FireOnAccountSignInChanged(const std::string& account_id,
+                                  bool is_signed_in);
+
   content::BrowserContext* browser_context_;
   IdentityMintRequestQueue mint_queue_;
   CachedTokens token_cache_;
-  ProfileIdentityProvider profile_identity_provider_;
-  gaia::AccountTracker account_tracker_;
 
   OnSignInChangedCallback on_signin_changed_callback_for_testing_;
 
   base::CallbackList<void()> on_shutdown_callback_list_;
+
+  // True if there is a primary account.
+  bool primary_account_is_available_;
+
+  // Maps account IDs to Gaia IDs.
+  std::map<std::string, std::string> account_id_to_gaia_id_;
 };
 
 template <>
