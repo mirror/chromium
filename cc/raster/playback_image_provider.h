bb2d8f76d1d466914e11a1f4ca2476e90c2a732a
Adrienne Walker <enne@chromium.org>
2017-09-28 10:43:40 -0700

    [wip] perf tryjob revert PBIP changes
    Change-Id: I4f788d054853da8175ee4928be3516ae41e7a5b3

--- cc/raster/playback_image_provider.h
+++ cc/raster/playback_image_provider.h
@@ -22,13 +22,10 @@ class ImageDecodeCache;
 //    only be used for lazy generated images.
 class CC_EXPORT PlaybackImageProvider : public ImageProvider {
  public:
-  PlaybackImageProvider(
-      bool skip_all_images,
-      PaintImageIdFlatSet images_to_skip,
-      std::vector<DrawImage> at_raster_images,
-      ImageDecodeCache* cache,
-      const gfx::ColorSpace& taget_color_space,
-      base::flat_map<PaintImage::Id, size_t> image_to_current_frame_index);
+  PlaybackImageProvider(bool skip_all_images,
+                        PaintImageIdFlatSet images_to_skip,
+                        ImageDecodeCache* cache,
+                        const gfx::ColorSpace& taget_color_space);
   ~PlaybackImageProvider() override;
 
   void BeginRaster() override;
@@ -45,11 +42,9 @@ class CC_EXPORT PlaybackImageProvider : public ImageProvider {
   bool skip_all_images_;
   bool in_raster_ = false;
   PaintImageIdFlatSet images_to_skip_;
-  std::vector<DrawImage> at_raster_images_;
   std::vector<ImageProvider::ScopedDecodedDrawImage> decoded_at_raster_;
   ImageDecodeCache* cache_;
   gfx::ColorSpace target_color_space_;
-  base::flat_map<PaintImage::Id, size_t> image_to_current_frame_index_;
 
   DISALLOW_COPY_AND_ASSIGN(PlaybackImageProvider);
 };
