97e3fa38e9f38eed76f8b022632957386d09bc3e
Dmitry Skiba <dskiba@chromium.org>
2017-09-28 16:34:22 -0700

    Avoid ChromeLauncherActivity when opening Chrome via launcher.
    This CL introduces .LauncherMain activity alias that maps MAIN intents
    with LAUNCHER or DEFAULT categories to ChromeTabbedActivity.
    
    The CL refactors intent dispatching logic from ChromeLauncherActivity
    into LaunchIntentDispatcher, and (indirectly) calls it from
    ChromeTabbedActivity.onCreate() to support multi-window cases.
    
    As a result, MAIN + LAUNCHER / DEFAULT intents can now only do the
    following:
    
    1. Start ChromeTabbedActivity, or its proper multi-window equivalent.
    2. Migrate from document mode.
    3. Start FRE.
    
    To get the old behavior of implicit MAIN intents, callers should
    explicitly specify the activity:
    (1) either as "com.google.android.apps.chrome.Main" (externally), or
    (2) as ChromeLauncherActivity.class (internally).
    
    All of this trouble is worth it, because it cuts unnecessary middleman
    when starting Chrome, and reduces startup time by at least ~100ms on
    512MiB Android Go devices.
    
    Bug:761606
    Change-Id: If06a2b918f9eee6302c57fb28fc82f25f054902a

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -63,7 +63,6 @@ import org.chromium.chrome.browser.compositor.layouts.OverviewModeBehavior.Overv
 import org.chromium.chrome.browser.compositor.layouts.phone.StackLayout;
 import org.chromium.chrome.browser.cookies.CookiesFetcher;
 import org.chromium.chrome.browser.device.DeviceClassManager;
-import org.chromium.chrome.browser.document.ChromeLauncherActivity;
 import org.chromium.chrome.browser.document.DocumentUtils;
 import org.chromium.chrome.browser.dom_distiller.ReaderModeManager;
 import org.chromium.chrome.browser.download.DownloadUtils;
@@ -216,6 +215,10 @@ public class ChromeTabbedActivity
      * crbug.com/706258
      */
     private static final long TIME_SINCE_BACKGROUNDED_TO_SHOW_BOTTOM_SHEET_HALF_MS = 10800000L;
+
+    // Name of the ChromeTabbedActivity alias that handles MAIN + DEFAULT/LAUNCHER intents.
+    private static final String MAIN_LAUNCHER_ACTIVITY_NAME =
+            "com.google.android.apps.chrome.LauncherMain";
 
     private final ActivityStopMetrics mActivityStopMetrics;
     private final MainIntentBehaviorMetrics mMainIntentMetrics;
@@ -372,6 +375,18 @@ public class ChromeTabbedActivity
         mActivityStopMetrics = new ActivityStopMetrics();
         mMainIntentMetrics = new MainIntentBehaviorMetrics(this);
         mAppIndexingUtil = new AppIndexingUtil();
+    }
+
+    @Override
+    protected @LaunchIntentDispatcher.Action int maybeDispatchLaunchIntent(Intent intent) {
+        if (getClass().equals(ChromeTabbedActivity.class)) {
+            // ChromeTabbedActivity can be started via an explicit intent from inside of Chrome,
+            // via an implicit MAIN intent, or via a MAIN/LAUNCHER intent. In either case call
+            // dispatchToTabbedActivity(), which will either do nothing, or start a different
+            // multiwindow-related TabbedActivity.
+            return new LaunchIntentDispatcher(this, intent).dispatchToTabbedActivity();
+        }
+        return super.maybeDispatchLaunchIntent(intent);
     }
 
     @Override
@@ -1300,8 +1315,8 @@ public class ChromeTabbedActivity
          */
         private void openNewTab(String url, String referer, String headers,
                 String externalAppId, Intent intent, boolean forceNewTab) {
-            boolean isAllowedToReturnToExternalApp = IntentUtils.safeGetBooleanExtra(intent,
-                    ChromeLauncherActivity.EXTRA_IS_ALLOWED_TO_RETURN_TO_PARENT, true);
+            boolean isAllowedToReturnToExternalApp = IntentUtils.safeGetBooleanExtra(
+                    intent, LaunchIntentDispatcher.EXTRA_IS_ALLOWED_TO_RETURN_TO_PARENT, true);
 
             // Create a new tab.
             Tab newTab =
@@ -2108,6 +2123,12 @@ public class ChromeTabbedActivity
         for (ActivityManager.AppTask task : appTasks) {
             if (task.getTaskInfo() == null || task.getTaskInfo().baseActivity == null) continue;
             String baseActivity = task.getTaskInfo().baseActivity.getClassName();
+            // Contrary to the documentation task.getTaskInfo().baseActivity for the .LauncherMain
+            // activity alias is the alias itself, and not the implementation. Filed b/66729258;
+            // for now translate the alias manually.
+            if (baseActivity.equals(MAIN_LAUNCHER_ACTIVITY_NAME)) {
+                baseActivity = ChromeTabbedActivity.class.getName();
+            }
             if (baseActivity.equals(otherWindowActivityClass.getName())) {
                 otherActivityTask = task;
             }
