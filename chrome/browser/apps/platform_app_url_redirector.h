73fbdd44acfc4fc6d100c38d26b467b9088f1536
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2017-09-28 17:12:28 +1000

    desktop-pwas: Separate Bookmark Apps redirection from Platform Apps redirection
    Introduces BookmarkAppNavigationThrottle which will eventually defer
    navigations so that app windows are not opened in the background.
    
    Besides not inhering from InterceptNavigationThrottle,
    BookmarkAppNavigationThrottle performs two checks in WillStartRequest
    that AppUrlRedirector performs in MaybCreateThrottleFor():
    
    1. Scheme is HTTP or HTTPS
    2. URL is handled by an app
    
    This ensures that, once we implement WillRedirectRequest,
    our NavigationThrottle is run for all URLs in the navigation not only
    the first one.
    
    Bug: 764626
    Change-Id: I8a52a8f6bf120e05141952feaccfcf696c821b5e

--- chrome/browser/apps/platform_app_url_redirector.h
+++ chrome/browser/apps/platform_app_url_redirector.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_APPS_APP_URL_REDIRECTOR_H_
-#define CHROME_BROWSER_APPS_APP_URL_REDIRECTOR_H_
+#ifndef CHROME_BROWSER_APPS_PLATFORM_APP_URL_REDIRECTOR_H_
+#define CHROME_BROWSER_APPS_PLATFORM_APP_URL_REDIRECTOR_H_
 
 #include <memory>
 
@@ -15,16 +15,16 @@ class NavigationHandle;
 class NavigationThrottle;
 }
 
-// This class creates navigation throttles that redirect URLs to apps that have
-// a matching URL handler in the 'url_handlers' manifest key. Note that this is
-// a UI thread class.
-class AppUrlRedirector {
+// This class creates navigation throttles that redirect URLs to platform apps
+// that have a matching URL handler in the 'url_handlers' manifest key. Note
+// that this is a UI thread class.
+class PlatformAppUrlRedirector {
  public:
   static std::unique_ptr<content::NavigationThrottle> MaybeCreateThrottleFor(
       content::NavigationHandle* handle);
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(AppUrlRedirector);
+  DISALLOW_COPY_AND_ASSIGN(PlatformAppUrlRedirector);
 };
 
-#endif  // CHROME_BROWSER_APPS_APP_URL_REDIRECTOR_H_
+#endif  // CHROME_BROWSER_APPS_PLATFORM_APP_URL_REDIRECTOR_H_
