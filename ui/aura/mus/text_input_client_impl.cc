34fea2022701b7c4aac619321907ed76e1026e8a
Thanh Pham <thanhph@chromium.org>
2017-09-28 16:50:45 -0400

    Implement IsCandidatePopUpOpen() in InputMethodMus.
    Client InputMethodMus needs a way to know if there's candidate pop up
    window which is created in different process, i.e., Chrome. We can
    provide SetCandidateWindowVisible() interface in ime.mojom to allow
    RemoteTextInputClient in Chrome process to send visible property of
    candidate window pop up to InputMethodMus in client process.
    
    Bug:637416
    Change-Id: I5c5785972ba90d6ec2e6ceabc9e29043ee226761

--- ui/aura/mus/text_input_client_impl.cc
+++ ui/aura/mus/text_input_client_impl.cc
@@ -20,6 +20,10 @@ TextInputClientImpl::TextInputClientImpl(
 
 TextInputClientImpl::~TextInputClientImpl() {}
 
+bool TextInputClientImpl::is_candidate_window_pop_up_visible() const {
+  return is_candidate_window_pop_up_visible_;
+}
+
 ui::mojom::TextInputClientPtr TextInputClientImpl::CreateInterfacePtrAndBind() {
   ui::mojom::TextInputClientPtr ptr;
   binding_.Bind(mojo::MakeRequest(&ptr));
@@ -56,6 +60,10 @@ void TextInputClientImpl::DispatchKeyEventPostIME(
     if (callback && !callback.is_null())
       std::move(callback).Run(event->stopped_propagation());
   }
+}
+
+void TextInputClientImpl::SetCandidateWindowVisible(bool visible) {
+  is_candidate_window_pop_up_visible_ = visible;
 }
 
 }  // namespace aura
