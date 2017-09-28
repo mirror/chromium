d0462b47e2f8c31b548c03c32d854b11cdd0448a
Henrik Boström <hbos@chromium.org>
2017-09-28 12:19:21 +0200

    PC.setRemoteDescription: Use track-based callbacks over stream-based.
    WIP
    
    Bug: 741619, 741618, 738929, 705901, webrtc:7933
    Change-Id: I75c18b889e1d4d5827c53bf6dd627f495ca22b51

--- content/renderer/media/webrtc/rtc_rtp_receiver.cc
+++ content/renderer/media/webrtc/rtc_rtp_receiver.cc
@@ -18,14 +18,28 @@ uintptr_t RTCRtpReceiver::getId(
 
 RTCRtpReceiver::RTCRtpReceiver(
     rtc::scoped_refptr<webrtc::RtpReceiverInterface> webrtc_rtp_receiver,
-    std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef> track_adapter)
+    std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef> track_adapter,
+    std::vector<std::unique_ptr<WebRtcMediaStreamAdapterMap::AdapterRef>>
+        stream_adapters)
     : webrtc_rtp_receiver_(std::move(webrtc_rtp_receiver)),
-      track_adapter_(std::move(track_adapter)) {
+      track_adapter_(std::move(track_adapter)),
+      stream_adapters_(std::move(stream_adapters)) {
   DCHECK(webrtc_rtp_receiver_);
   DCHECK(track_adapter_);
 }
 
 RTCRtpReceiver::~RTCRtpReceiver() {}
+
+std::unique_ptr<RTCRtpReceiver> RTCRtpReceiver::ShallowCopy() const {
+  std::vector<std::unique_ptr<WebRtcMediaStreamAdapterMap::AdapterRef>>
+      stream_adapter_copies(stream_adapters_.size());
+  for (size_t i = 0; i < stream_adapters_.size(); ++i) {
+    stream_adapter_copies[i] = stream_adapters_[i]->Copy();
+  }
+  return base::MakeUnique<RTCRtpReceiver>(webrtc_rtp_receiver_,
+                                          track_adapter_->Copy(),
+                                          std::move(stream_adapter_copies));
+}
 
 uintptr_t RTCRtpReceiver::Id() const {
   return getId(webrtc_rtp_receiver_.get());
@@ -34,6 +48,13 @@ uintptr_t RTCRtpReceiver::Id() const {
 const blink::WebMediaStreamTrack& RTCRtpReceiver::Track() const {
   DCHECK(track_adapter_->webrtc_track() == webrtc_rtp_receiver_->track());
   return track_adapter_->web_track();
+}
+
+blink::WebVector<blink::WebMediaStream> RTCRtpReceiver::Streams() const {
+  blink::WebVector<blink::WebMediaStream> web_streams(stream_adapters_.size());
+  for (size_t i = 0; i < stream_adapters_.size(); ++i)
+    web_streams[i] = stream_adapters_[i]->adapter().web_stream();
+  return web_streams;
 }
 
 blink::WebVector<std::unique_ptr<blink::WebRTCRtpContributingSource>>
@@ -51,6 +72,15 @@ const webrtc::MediaStreamTrackInterface& RTCRtpReceiver::webrtc_track() const {
   DCHECK(track_adapter_->webrtc_track() == webrtc_rtp_receiver_->track());
   DCHECK(track_adapter_->webrtc_track());
   return *track_adapter_->webrtc_track();
+}
+
+bool RTCRtpReceiver::HasStream(
+    const webrtc::MediaStreamInterface* webrtc_stream) const {
+  for (const auto& stream_adapter : stream_adapters_) {
+    if (webrtc_stream == stream_adapter->adapter().webrtc_stream().get())
+      return true;
+  }
+  return false;
 }
 
 }  // namespace content
