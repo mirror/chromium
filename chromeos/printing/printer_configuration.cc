990915672f0ca66fd5a26f0b189ab35c92fe4944
Sean Kau <skau@chromium.org>
2017-09-27 19:49:00 -0700

    Resolve zeroconf printer IPs in PrinterConfigurer.
    It's possible that we have to setup a printer before
    ZeroconfPrinterDetector has finished its scan.  Notably, when a printer
    is pre-selected in the PrintPreview dialog.  To avoid installing printers
    with uris we can't resolve, resolve the IP in configurer.  As a bonus,
    we now deselect the printer if it's not immedeately usable.
    
    Bug: 769469
    Change-Id: I96e07edd9a2eaf199f093408bbb6b8cf583fe851

--- chromeos/printing/printer_configuration.cc
+++ chromeos/printing/printer_configuration.cc
@@ -8,8 +8,44 @@
 
 #include "base/guid.h"
 #include "base/strings/string_piece.h"
+#include "base/strings/string_util.h"
+#include "net/base/ip_endpoint.h"
+#include "url/url_constants.h"
 
 namespace chromeos {
+
+namespace {
+
+// Separator = ://
+constexpr size_t kSeparatorLen = 3U;
+
+// Returns the index of the first character representing the hostname in |uri|.
+// Returns npos if the start of the hostname is not found.
+size_t HostnameStart(base::StringPiece uri) {
+  size_t scheme_separator_start = uri.find(url::kStandardSchemeSeparator);
+  if (scheme_separator_start == base::StringPiece::npos) {
+    return base::StringPiece::npos;
+  }
+  return scheme_separator_start + kSeparatorLen;
+}
+
+base::StringPiece HostAndPort(base::StringPiece uri) {
+  size_t hostname_start = HostnameStart(uri);
+  if (hostname_start == base::StringPiece::npos) {
+    return "";
+  }
+
+  size_t hostname_end = uri.find("/", hostname_start);
+  if (hostname_end == base::StringPiece::npos) {
+    // No trailing slash.  Use end of string.
+    hostname_end = uri.size();
+  }
+
+  DCHECK_GT(hostname_end, hostname_start);
+  return uri.substr(hostname_start, hostname_end - hostname_start);
+}
+
+}  // namespace
 
 Printer::Printer() : source_(SRC_USER_PREFS) {
   id_ = base::GenerateGUID();
@@ -28,6 +64,38 @@ Printer::~Printer() {}
 
 bool Printer::IsIppEverywhere() const {
   return ppd_reference_.autoconf;
+}
+
+bool Printer::RequiresIpResolution() const {
+  return base::StartsWith(id_, "zeroconf-", base::CompareCase::SENSITIVE) &&
+         effective_uri_.empty();
+}
+
+net::HostPortPair Printer::GetHostAndPort() const {
+  if (uri_.empty()) {
+    return net::HostPortPair();
+  }
+
+  return net::HostPortPair::FromString(HostAndPort(uri_).as_string());
+}
+
+std::string Printer::ReplaceHostAndPort(const net::IPEndPoint& ip) const {
+  if (uri_.empty()) {
+    return "";
+  }
+
+  base::StringPiece uri_piece(uri_);
+  size_t hostname_start = HostnameStart(uri_);
+  if (hostname_start == base::StringPiece::npos) {
+    return "";
+  }
+  size_t host_port_len = HostAndPort(uri_).length();
+  std::string effective_uri = uri_piece.substr(0, hostname_start).as_string();
+  effective_uri.append(ip.ToString());
+  effective_uri.append(
+      uri_piece.substr(hostname_start + host_port_len).as_string());
+
+  return effective_uri;
 }
 
 Printer::PrinterProtocol Printer::GetProtocol() const {
