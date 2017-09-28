16d2b881359e33ed68aef807d09ef3698ffaceee
Renee Wright <rjwright@chromium.org>
2017-09-28 23:35:54 +1000

    [Ribbon] add comments to hand-written Variable and ApplyAtRule headers
    Bug:
    Change-Id: Ic43a790f715ffe3bc36de1daf058910201b15a0b

--- third_party/WebKit/Source/core/css/properties/CSSPropertyAPIVariable.h
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIVariable.h
@@ -2,6 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// This header is hand-written, whereas most CSSPropertyAPI subclass
+// headers are generate by
+// core/css/properties/templates/CSSPropertyAPISubclass.h.tmpl. The
+// typical CSSPropertyAPI header is generated from an object in
+// core/css/CSSProperties.json5, but adding an entry there for
+// CSSPropertyAPIVariable causes problems elsewhere.
+// CSSPropertyAPIVariable is not really a CSS property, but it is
+// convient to treat it as one some of the time. The same is true of
+// CSSPropertyApplyAtRule, which also has a hand-written
+// CSSPropertyAPI header.
 #ifndef CSSPropertyAPIVariable_h
 #define CSSPropertyAPIVariable_h
 
