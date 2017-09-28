d66bc892b80e7262311e9d1a527a9c50ee63d811
Megan Jablonski <megjablon@google.com>
2017-09-28 11:21:49 -0700

    Add Memex button in Clank behind an off-by-default flag
    When the feature is enabled, clicking on the tab switcher button
    will load Memex in the current tab. The original tab switcher
    will still be accessible by long clicking on the button.
    
    Bug: 768952
    Change-Id: Ib4fae97b752eb8e0c7f8c272a0f1f60e3ea86441

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -62,6 +62,7 @@ const base::Feature* kFeaturesExposedToJava[] = {
     &kChromeHomeDoodle,
     &kChromeHomeExpandButton,
     &kChromeHomeSwipeLogic,
+    &kChromeMemexFeature,
     &kContentSuggestionsLargeThumbnail,
     &kContentSuggestionsScrollToLoad,
     &kContentSuggestionsSettings,
@@ -174,6 +175,9 @@ const base::Feature kChromeHomeExpandButton{"ChromeHomeExpandButton",
 
 const base::Feature kChromeHomeSwipeLogic{"ChromeHomeSwipeLogic",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
+
+const base::Feature kChromeMemexFeature{"ChromeMemex",
+                                        base::FEATURE_DISABLED_BY_DEFAULT};
 
 const base::Feature kContentSuggestionsLargeThumbnail{
     "ContentSuggestionsLargeThumbnail", base::FEATURE_ENABLED_BY_DEFAULT};
