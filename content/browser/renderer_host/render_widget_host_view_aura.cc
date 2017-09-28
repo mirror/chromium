f3a8171fd29d9c939a894fedc82815adf9b821f8
Scott Violet <sky@chromium.org>
2017-09-28 14:26:17 -0700

    chromeos: Gets OOPIF working for mus
    The flow is:
    1. browser requests WindowTreeClient from the RenderWidget of the
      frame to embed.
    2. browser send a message to the parent RenderFrameProxy with the
       WindowTreeClient.
    3. RenderFrameProxy forwards to RendererWindowTreeClient to create the
       child window and embed the WindowTreeClient in it.
    
    BUG=758387
    TEST=none
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I0296e802c0f754e463c65492f38ec5a68df30eba

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -1910,16 +1910,9 @@ void RenderWidgetHostViewAura::CreateAuraWindow(aura::client::WindowType type) {
   if (!IsMus())
     return;
 
-  // Connect to the renderer, pass it a WindowTreeClient interface request
-  // and embed that client inside our mus window.
-  mojom::RenderWidgetWindowTreeClientFactoryPtr factory;
-  BindInterface(host_->GetProcess(), &factory);
-
-  ui::mojom::WindowTreeClientPtr window_tree_client;
-  factory->CreateWindowTreeClientForRenderWidget(
-      host_->GetRoutingID(), mojo::MakeRequest(&window_tree_client));
+  // Embed the renderer into the Window.
   aura::WindowPortMus::Get(window_)->Embed(
-      std::move(window_tree_client),
+      GetWindowTreeClientFromRenderer(),
       ui::mojom::kEmbedFlagEmbedderInterceptsEvents,
       base::Bind(&EmbedCallback));
 }
