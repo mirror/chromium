55c1022ea5b4f1938f1e0aeba172d8789d91ca6c
Daniel Park <danielpark@chromium.org>
2017-09-28 15:55:08 -0700

    [Chrome Home Survey]
    Creates survey info bar
    
    Shows an invitation to take a survey
     if Chrome Home was enabled > 1 week.
    Adds clearing cache function.
    
    Bug:754499
    Change-Id: I53064f181fcd8832a5524eb4d7d38841531c84e7

---
+++ chrome/android/java/src/org/chromium/chrome/browser/survey/ChromeHomeSurveyController.java
@@ -0,0 +1,52 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.survey;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import org.chromium.base.CommandLine;
+import org.chromium.base.ContextUtils;
+import org.chromium.base.StrictModeContext;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.components.variations.VariationsAssociatedData;
+
+/**
+ * Class that controls if and when to show surveys related to Chrome Home.
+ */
+public class ChromeHomeSurveyController {
+    private static final String TRIAL_NAME = "ChromeHome";
+    private static final String PARAM_NAME = "survey_id";
+    private static final long ONE_WEEK_IN_MILLIS = 604800000L;
+
+    public void kickoffSurveyProcess(Tab tab, Context context) {
+        android.util.Log.d("INJAE", "ChromeHomeSurveyController#initSurveyController");
+        SurveyController surveyController = SurveyController.getInstance();
+        if (tab != null && context != null && userQualifiesForSurvey()) {
+            CommandLine instance = CommandLine.getInstance();
+            String surveyId;
+            if (instance.hasSwitch(PARAM_NAME)) {
+                surveyId = instance.getSwitchValue(PARAM_NAME);
+            }
+            surveyId = VariationsAssociatedData.getVariationParamValue(TRIAL_NAME, PARAM_NAME);
+            if (surveyId == null) return;
+            surveyController.initController(
+                    context, tab.getWebContents(), surveyId, "", true, R.drawable.chrome_sync_logo);
+            SurveyController.getInstance().downloadSurvey(context);
+        }
+    }
+
+    private boolean userQualifiesForSurvey() {
+        try (StrictModeContext unused = StrictModeContext.allowDiskReads()) {
+            SharedPreferences sharedPreferences = ContextUtils.getAppSharedPreferences();
+            long earliestLoggedDate = sharedPreferences.getLong(
+                    ChromePreferenceManager.CHROME_HOME_SHARED_PREFERENCES_KEY, Long.MAX_VALUE);
+            if (System.currentTimeMillis() - earliestLoggedDate > ONE_WEEK_IN_MILLIS) return true;
+        }
+        return false;
+    }
+}
