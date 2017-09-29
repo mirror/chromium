81f388cd4fbb0516b74559c5280cdc7635180cb1
Toni Barzic <tbarzic@google.com>
2017-09-28 19:50:47 -0700

    Add note action app launcher to views based lock screen
    Introduces NoteActionLaunchButton - a view for launching lock screen
    note from the lock screen.
    The control is implemented in two parts:
     *  BackgroundView, which contains the view background - a bubble
        (quarter of a circle centered in top right view corner), which
        is resized with animation on hover/focus.
     *  ActionButton - the note ImageButton painted on top of the
        background view, which handles user interactions with the
        control.
    
    LockContentsView layout is updated to accommodate the new element.
    The whole contents view layout manager is changed to FillLayout, and
    NoteActionLaunchButton is then resized/positioned during Layout (to
    top right corner of the lock screen). The goal was to minimize the
    action control's influence on the rest of the lock screen layout.
    
    The view containing the authentication controls (which used to be the
    whole lock contents view) is wrapped with another view (main_view_)
    that is given the same layout manager as lock contents view used to
    have. Existing AddChildView calls have been changed to
    main_view_->AddChildView.
    
    BUG=746596
    
    Change-Id: I152b9299757a4ee32c156db04aed7ff24428d656

--- ash/login/ui/lock_debug_view.h
+++ ash/login/ui/lock_debug_view.h
@@ -41,6 +41,7 @@ class LockDebugView : public views::View, public views::ButtonListener {
 
   views::View* debug_;
   views::View* toggle_blur_;
+  views::View* toggle_note_action_;
   views::View* add_user_;
   views::View* remove_user_;
 
