7647021596a10e414cf2bdc3ccbdd652dd60b7a9
Pramod B S <pramod.bs@samsung.com>
2017-09-29 10:58:02 +0530

    bluetooth: Hide False characterstics properties in bluetooth-internals
    Modified the redraw function in object fieldset
    to draw only valid parameters.
    
    Bug: 704549
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I4db0a7bd2a3bab63aa5bf01dd6c968d9598f3494

--- chrome/browser/resources/bluetooth_internals/object_fieldset.js
+++ chrome/browser/resources/bluetooth_internals/object_fieldset.js
@@ -63,27 +63,30 @@ cr.define('object_fieldset', function() {
       this.innerHTML = '';
 
       Object.keys(this.value).forEach(function(propName) {
-        var name = this.nameMap_[propName] || propName;
         var value = this.value[propName];
-        var newField = document.createElement('div');
-        newField.classList.add('status');
+        if (value) {
+          var name = this.nameMap_[propName] || propName;
 
-        var nameDiv = document.createElement('div');
-        nameDiv.textContent = name + ':';
-        newField.appendChild(nameDiv);
+          var newField = document.createElement('div');
+          newField.classList.add('status');
 
-        var valueDiv = document.createElement('div');
-        valueDiv.dataset.field = propName;
+          var nameDiv = document.createElement('div');
+          nameDiv.textContent = name + ':';
+          newField.appendChild(nameDiv);
 
-        if (typeof(value) === 'boolean') {
-          valueDiv.classList.add('toggle-status');
-          valueDiv.classList.toggle('checked', value);
-        } else {
-          valueDiv.textContent = String(value);
+          var valueDiv = document.createElement('div');
+          valueDiv.dataset.field = propName;
+
+          if (typeof(value) === 'boolean') {
+            valueDiv.classList.add('toggle-status');
+            valueDiv.classList.toggle('checked', value);
+          } else {
+            valueDiv.textContent = String(value);
+          }
+
+          newField.appendChild(valueDiv);
+          this.appendChild(newField);
         }
-
-        newField.appendChild(valueDiv);
-        this.appendChild(newField);
       }, this);
     },
   };
