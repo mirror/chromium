a655d4337f518704bf3b7fe8e31e856f15162d0d
David Jacobo <djacobo@google.com>
2017-09-29 13:45:06 -0700

    Add entry on settings to manage app preferences.
    Adding a way for the user to manage their app preferences via
    chrome://settings, this new entry is hosted on the Handlers section and
    it points to Settings->Apps->Configure apps->App links on ARC.
    
    Bug: 678141
    Test: Clicking "Manage Play app links" on chrome://settings opens ARC settings.
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I4f975ec105f84c3656861f3a7e0e4879405f9f4c

--- chrome/browser/resources/settings/site_settings/protocol_handlers.js
+++ chrome/browser/resources/settings/site_settings/protocol_handlers.js
@@ -59,6 +59,17 @@ Polymer({
     /* Labels for the toggle on/off positions. */
     toggleOffLabel: String,
     toggleOnLabel: String,
+
+    androidAppsInfo: {
+      playStoreEnabled: {
+        type: Boolean,
+        value: false,
+      },
+      settingsAppAvailable: {
+        type: Boolean,
+        value: false,
+      },
+    },
   },
 
   /** @override */
@@ -71,6 +82,28 @@ Polymer({
         'setIgnoredProtocolHandlers',
         this.setIgnoredProtocolHandlers_.bind(this));
     this.browserProxy.observeProtocolHandlers();
+  },
+
+  /** @override */
+  attached: function() {
+    if (settings.AndroidAppsBrowserProxyImpl) {
+      cr.addWebUIListener(
+          'android-apps-info-update', this.androidAppsInfoUpdate_.bind(this));
+    }
+  },
+
+  /**
+   * @param {androidAppsInfo} info - keeps track of ARC flags.
+   * @private
+   */
+  androidAppsInfoUpdate_: function(info) {
+    this.androidAppsInfo = info;
+    if (!this.androidAppsInfo || !this.androidAppsInfo.playStoreEnabled ||
+        !this.androidAppsInfo.settingsAppAvailable) {
+      this.$.manageApps.hidden = true;
+    } else {
+      this.$.manageApps.hidden = false;
+    }
   },
 
   /**
@@ -177,5 +210,13 @@ Polymer({
         .showAt(
             /** @type {!Element} */ (
                 Polymer.dom(/** @type {!Event} */ (event)).localTarget));
-  }
+  },
+
+  /**
+   * Opens an activity to handle App links (preferred apps).
+   * @private
+   */
+  onManageAndroidAppsTap_: function() {
+    this.browserProxy.showAndroidManageAppLinks();
+  },
 });
