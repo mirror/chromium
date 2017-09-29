5458e77c94666e0f7bc1fc6679ecf9542f2bf741
Bill Budge <bbudge@chromium.org>
2017-09-29 15:55:47 -0700

    [Memory] Adds ASLR handling for OS'es and Architectures supported by V8.
    - Refactors GetRandomPageBase to make it easier to understand.
    - Adds handling for architectures and OS'es needed by V8.
    
    Bug: chromium:756050
    Change-Id: I3d27aeb9e243cfdda005662dc6df710bdbcabaaa

--- base/allocator/partition_allocator/address_space_randomization_unittest.cc
+++ base/allocator/partition_allocator/address_space_randomization_unittest.cc
@@ -1,24 +1,20 @@
-// Copyright 2014 The Chromium Authors. All rights reserved.
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/allocator/partition_allocator/address_space_randomization.h"
 
 #include "base/allocator/partition_allocator/page_allocator.h"
-#include "base/allocator/partition_allocator/spin_lock.h"
-#include "base/lazy_instance.h"
+#include "base/bit_cast.h"
+#include "base/bits.h"
+#include "base/sys_info.h"
 #include "build/build_config.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 #if defined(OS_WIN)
 #include <windows.h>
 #include "base/win/windows_version.h"
-#else
-#include <sys/time.h>
-#include <unistd.h>
-#endif
-
 // VersionHelpers.h must be included after windows.h.
-#if defined(OS_WIN)
 #include <VersionHelpers.h>
 #endif
 
@@ -26,62 +22,8 @@ namespace base {
 
 namespace {
 
-// This is the same PRNG as used by tcmalloc for mapping address randomness;
-// see http://burtleburtle.net/bob/rand/smallprng.html
-struct ranctx {
-  subtle::SpinLock lock;
-  bool initialized;
-  uint32_t a;
-  uint32_t b;
-  uint32_t c;
-  uint32_t d;
-};
-
-#define rot(x, k) (((x) << (k)) | ((x) >> (32 - (k))))
-
-uint32_t ranvalInternal(ranctx* x) {
-  uint32_t e = x->a - rot(x->b, 27);
-  x->a = x->b ^ rot(x->c, 17);
-  x->b = x->c + x->d;
-  x->c = x->d + e;
-  x->d = e + x->a;
-  return x->d;
-}
-
-#undef rot
-
-uint32_t ranval(ranctx* x) {
-  subtle::SpinLock::Guard guard(x->lock);
-  if (UNLIKELY(!x->initialized)) {
-    x->initialized = true;
-    char c;
-    uint32_t seed = static_cast<uint32_t>(reinterpret_cast<uintptr_t>(&c));
-    uint32_t pid;
-    uint32_t usec;
-#if defined(OS_WIN)
-    pid = GetCurrentProcessId();
-    SYSTEMTIME st;
-    GetSystemTime(&st);
-    usec = static_cast<uint32_t>(st.wMilliseconds * 1000);
-#else
-    pid = static_cast<uint32_t>(getpid());
-    struct timeval tv;
-    gettimeofday(&tv, 0);
-    usec = static_cast<uint32_t>(tv.tv_usec);
-#endif
-    seed ^= pid;
-    seed ^= usec;
-    x->a = 0xf1ea5eed;
-    x->b = x->c = x->d = seed;
-    for (int i = 0; i < 20; ++i) {
-      (void)ranvalInternal(x);
-    }
-  }
-  uint32_t ret = ranvalInternal(x);
-  return ret;
-}
-
-static LazyInstance<ranctx>::Leaky s_ranctx = LAZY_INSTANCE_INITIALIZER;
+// NOTE: These are copied from address_space_randomization.cc and must be
+// kept in sync with the declarations there.
 
 #define ASLR_ADDRESS(mask) ((mask)&kPageAllocationGranularityBaseMask);
 #define ASLR_MASK(bits) \
@@ -192,51 +134,38 @@ static const uintptr_t kASLROffset = ASLR_ADDRESS(0x20000000UL);
 
 }  // namespace
 
-// Calculates a random preferred mapping address. In calculating an address, we
-// balance good ASLR against not fragmenting the address space too badly.
-void* GetRandomPageBase() {
-  uintptr_t random = static_cast<uintptr_t>(ranval(s_ranctx.Pointer()));
-
-#if defined(ARCH_CPU_64_BITS)
-  random <<= 32UL;
-  random |= static_cast<uintptr_t>(ranval(s_ranctx.Pointer()));
-
-#if defined(OS_WIN)
-  // Windows >= 8.1 has the full 47 bits. Use them where available.
-  static bool windows_81 = false;
-  static bool windows_81_initialized = false;
-  if (!windows_81_initialized) {
-    windows_81 = IsWindows8Point1OrGreater();
-    windows_81_initialized = true;
+TEST(AddressSpaceRandomizationTest, GetRandomPageBase) {
+  uintptr_t mask = kASLRMask;
+#if defined(ARCH_CPU_64_BITS) && defined(OS_WIN)
+  if (!IsWindows8Point1OrGreater()) {
+    mask = kASLRMaskBefore8_10;
   }
-  if (!windows_81) {
-    random &= kASLRMaskBefore8_10;
-  } else {
-    random &= kASLRMask;
+#endif
+  // Sample the first 100 addresses.
+  std::set<uintptr_t> addresses;
+  uintptr_t address_logical_sum = 0;
+  uintptr_t address_logical_product = static_cast<uintptr_t>(-1);
+  for (int i = 0; i < 100; i++) {
+    uintptr_t address = reinterpret_cast<uintptr_t>(base::GetRandomPageBase());
+    // Test that address is in range.
+    EXPECT_LE(kASLROffset, address);
+    EXPECT_GE(mask + kASLROffset, address);
+    // Test that address is page aligned.
+    EXPECT_EQ(0ULL, (address & kPageAllocationGranularityOffsetMask));
+    // Test that address is unique (no collisions in 100 tries)
+    CHECK_EQ(0ULL, addresses.count(address));
+    addresses.insert(address);
+    // Sum and product to test randomness at each bit position, below.
+    address -= kASLROffset;
+    address_logical_sum |= address;
+    address_logical_product &= address;
   }
-  random += kASLROffset;
-#else   // defined(OS_POSIX)
-  random &= kASLRMask;
-  random += kASLROffset;
-#endif  // defined(OS_POSIX)
-#else   // defined(ARCH_CPU_32_BITS)
-#if defined(OS_WIN)
-  // On win32 host systems the randomization plus huge alignment causes
-  // excessive fragmentation. Plus most of these systems lack ASLR, so the
-  // randomization isn't buying anything. In that case we just skip it.
-  // TODO(jschuh): Just dump the randomization when HE-ASLR is present.
-  static BOOL isWow64 = -1;
-  if (isWow64 == -1 && !IsWow64Process(GetCurrentProcess(), &isWow64))
-    isWow64 = FALSE;
-  if (!isWow64)
-    return nullptr;
-#endif  // defined(OS_WIN)
-  random &= kASLRMask;
-  random += kASLROffset;
-#endif  // defined(ARCH_CPU_32_BITS)
-
-  DCHECK_EQ(0UL, (random & kPageAllocationGranularityOffsetMask));
-  return reinterpret_cast<void*>(random);
+  // All bits in address_logical_sum should be set, since the likelihood of
+  // never setting any of the bits is 1 / (2 ^ 100) with a good RNG. Likewise,
+  // all bits in address_logical_product should be cleared, except for the
+  // bits in kASLROffset.
+  EXPECT_EQ(mask, address_logical_sum);
+  EXPECT_EQ(0UL, address_logical_product);
 }
 
 }  // namespace base
