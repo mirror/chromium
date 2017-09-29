fee49704026a09871020207dde508c1f25afc5b5
Peter Mayo <petermayo@chromium.org>
2017-09-29 15:07:29 -0400

    Remove CompositorMutation
    These could/should have gone away with CompositorProxy, but now that
    cc/trees/mutable_properties is gone too, they really do stick out as
    zombie code.
    
    Bug: 768852
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ib33a0243acbbd504b7649d72ae89278459c45d26

--- third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
@@ -1255,7 +1255,6 @@ void StyleResolver::ApplyAnimatedStandardProperties(
     } else if (interpolation.IsTransitionInterpolation()) {
       ToTransitionInterpolation(interpolation).Apply(state);
     } else {
-      // TODO(alancutter): Move CustomCompositorAnimations off AnimatableValues.
       ToLegacyStyleInterpolation(interpolation).Apply(state);
     }
   }
