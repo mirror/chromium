282047d74af3421721e23c3a3e883b717de2069e
Fady Samuel <fsamuel@chromium.org>
2017-09-29 12:52:48 -0400

    ViewHostMsg_UpdateRect => ViewHostMsg_Resize_ACK
    While fixing up resize for surface synchronization, I noticed that we're
    STILL using IPC naming from the old software rendering path days. Gone are
    the days of updating rects on a backing store in the browser process.
    
    This CL uses naming that better (perhaps imperfectly) matches what's going
    on today. ViewHostMsg_UpdateRect is really used as a throttling ACK for resizes
    or repaints.
    
    This CL renames mentions of UpdateRect and backing stores.
    
    Bug: 672962
    Change-Id: Ice2e44c3726e80da29b6d173e5f436b34f2792c3

--- content/common/resize_params.h
+++ content/common/resize_params.h
@@ -53,8 +53,8 @@ struct CONTENT_EXPORT ResizeParams {
   // The display mode.
   blink::WebDisplayMode display_mode;
 
-  // If set, requests the renderer to reply with a ViewHostMsg_UpdateRect
-  // with the ViewHostMsg_UpdateRect_Flags::IS_RESIZE_ACK bit set in flags.
+  // If set, requests the renderer to reply with a ViewHostMsg_Resize_ACK
+  // with the ViewHostMsg_Resize_ACK_Flags::IS_RESIZE_ACK bit set in flags.
   bool needs_resize_ack;
 };
 
