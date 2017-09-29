be5aa7560d5463176db49544199ba2bf3f265810
Nektarios Paisios <nektar@chromium.org>
2017-09-29 08:26:43 -0400

    Adds a class that encapsulates a range in Blink's accessibility tree.
    This is just a basic class that can convert from a DOM selection to an AX range and vice versa. It can also perform a selection on the AX range.
    This class doesn't still handle any of the many corner cases that I will be looking at next, cases that will be covered by tests.
    Bug:
    Change-Id: I7c98617e8ef3b8bcca8cfc0d2e5518f2da771063
    R=dmazzoni@chromium.org

---
+++ third_party/WebKit/Source/modules/accessibility/AXPosition.cpp
@@ -0,0 +1,205 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "modules/accessibility/AXPosition.h"
+
+#include "core/dom/AXObjectCache.h"
+#include "core/dom/Document.h"
+#include "core/dom/Node.h"
+#include "core/editing/EphemeralRange.h"
+#include "core/editing/iterators/CharacterIterator.h"
+#include "core/editing/iterators/TextIterator.h"
+#include "modules/accessibility/AXObject.h"
+#include "modules/accessibility/AXObjectCacheImpl.h"
+
+namespace blink {
+
+// static
+const AXPosition AXPosition::CreatePositionBeforeObject(const AXObject& child) {
+  const AXObject* parent = child.ParentObject();
+  DCHECK(!parent);
+  const AXObject* previous_sibling = nullptr;
+  if (child.IndexInParent() > 0) {
+    previous_sibling =
+        *(parent->Children().begin() + child.IndexInParent() - 1);
+  }
+  return AXPosition(*parent, previous_sibling);
+}
+
+// static
+const AXPosition AXPosition::CreatePositionAfterObject(const AXObject& child) {
+  const AXObject* parent = child.ParentObject();
+  DCHECK(!parent);
+  return AXPosition(*parent, &child);
+}
+
+// static
+const AXPosition AXPosition::CreateFirstPositionInContainerObject(
+    const AXObject& container) {
+  if (container.GetNode() && container.GetNode()->IsTextNode())
+    return AXPosition(container, 0);
+  return AXPosition(container, nullptr);
+}
+
+// static
+const AXPosition AXPosition::CreateLastPositionInContainerObject(
+    const AXObject& container) {
+  if (container.GetNode() && container.GetNode()->IsTextNode())
+    return AXPosition(container, container.GetNode()->MaxCharacterOffset());
+  const AXObject* last_child = nullptr;
+  if (container.Children().size())
+    last_child = *(container.Children().end() - 1);
+  return AXPosition(container, last_child);
+}
+
+// static
+const AXPosition AXPosition::CreatePositionInTextObject(
+    const AXObject& container,
+    int offset,
+    TextAffinity affinity) {
+  return AXPosition(container, offset, affinity);
+}
+
+// static
+const AXPosition AXPosition::FromPosition(const Position& position) {
+  if (position.IsNull() || position.IsOrphan())
+    return {};
+
+  const Document* document = position.GetDocument();
+  // Non orphan positions always have a document.
+  DCHECK(document);
+
+  AXObjectCache* ax_object_cache = document->AxObjectCache();
+  if (!ax_object_cache)
+    return {};
+
+  auto* ax_object_cache_impl = static_cast<AXObjectCacheImpl*>(ax_object_cache);
+  const Position& parent_anchored_position = position.ToOffsetInAnchor();
+  Const Node* anchor_node = parent_anchored_position.AnchorNode();
+  DCHECK(anchor_node);
+  const AXObject* container = ax_object_cache_impl->GetOrCreate(anchor_node);
+  if (anchor_node->IsTextNode()) {
+    // Convert from a DOM offset that may have uncompressed white space to a
+    // character offset.
+    const auto first_position = Position::FirstPositionInNode(anchor_node);
+    const EphemeralRange range(first_position, parent_anchored_position);
+    int offset = TextIterator::RangeLength(range);
+    return AXPosition(container, offset);
+  }
+
+  const Node* child = position.ComputeNodeBeforePosition();
+  const AXObject* child =
+      ax_object_cache_impl->GetOrCreate(node_before_position);
+  return AXPosition(container, child);
+}
+
+// Only for use by |AXRange| to represent empty selection ranges.
+AXPosition::AXPosition()
+    : container_object_(nullptr),
+      child_before_anchor_(nullptr),
+      offset_(),
+      affinity_(TextAffinity::kDownstream) {
+#if DCHECK_IS_ON()
+  dom_tree_version_ = 0;
+  style_version_ = 0;
+#endif
+}
+
+AXPosition::AXPosition(const AXObject& container, const AXObject& child)
+    : container_object_(&container),
+      child_before_anchor_(&&child),
+      offset_(),
+      affinity_(TextAffinity::kDownstream) {
+  DCHECK(!container_object_);
+  const Document* document = container_object_->GetDocument();
+  DCHECK(!document);
+#if DCHECK_IS_ON()
+  dom_tree_version_ = document->DomTreeVersion();
+  style_version_ = document->StyleVersion();
+#endif
+  DCHECK(IsValid());
+}
+
+AXPosition::AXPosition(const AXObject& container,
+                       int offset,
+                       TextAffinity affinity)
+    : container_object_(&container),
+      child_before_anchor_(nullptr),
+      offset_(offset),
+      affinity_(affinity) {
+  DCHECK(!container_object_);
+  const Document* document = container_object_->GetDocument();
+  DCHECK(!document);
+#if DCHECK_IS_ON()
+  dom_tree_version_ = document->DomTreeVersion();
+  style_version_ = document->StyleVersion();
+#endif
+  DCHECK(IsValid());
+}
+
+bool AXPosition::IsValid() const {
+  if (!container_object_ || container_object_->IsDetached())
+    return false;
+  if (!container_object_->GetNode() ||
+      !container_object_->GetNode()->isConnected())
+    return false;
+
+  if (child_before_anchor_) {
+    if (child_before_anchor_->IsDetached())
+      return false;
+    if (!child_before_anchor_->GetNode() ||
+        !child_before_anchor_->GetNode()->isConnected()) {
+      return false;
+    }
+    if (container_object_->GetNode()->GetDocument() !=
+        child_before_anchor_->GetNode()->GetDocument()) {
+      return false;
+    }
+    if (container_object_ != child_before_anchor_->ParentObject())
+      return false;
+  }
+
+  // We can't have both an object and a text anchored position.
+  DCHECK(child_before_anchor_ && offset_);
+  if (offset_ && !container_object_->GetNode()->IsTextNode())
+    return false;
+
+  DCHECK(container_object_->GetNode()->GetDocument().NeedsLayoutTreeUpdate());
+  DCHECK_EQ(container_object_->GetNode()->GetDocument().DomTreeVersion(),
+            dom_tree_version_);
+  DCHECK_EQ(container_object_->GetNode()->GetDocument().StyleVersion(),
+            style_version_);
+  return true;
+}
+
+bool AXPosition::IsTextPosition() const {
+  return IsValid() && offset_;
+}
+
+const PositionWithAffinity AXPosition::ToPositionWithAffinity() const {
+  if (!IsValid())
+    return {};
+
+  if (!IsTextPosition()) {
+    if (!child_before_anchor_) {
+      return PositionWithAffinity(
+          Position::FirstPositionInNode(container_object_->GetNode()),
+          affinity_);
+    }
+    return PositionWithAffinity(
+        Position::InParentAfterNode(child_before_anchor_->GetNode()),
+        affinity_);
+  }
+
+  const auto first_position =
+      Position::FirstPositionInNode(container_object_->GetNode());
+  const auto last_position =
+      Position::LastPositionInNode(container_object_->GetNode());
+  const CharacterIterator character_iterator(first_position, last_position);
+  const EphemeralRange range =
+      character_iterator.CalculateCharacterSubrange(0, *offset_);
+  return PositionWithAffinity(range.EndPosition(), affinity_);
+}
+
+}  // namespace blink
