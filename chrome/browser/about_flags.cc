1d40b106549e690b56ae4880bcf09392106b8109
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2017-09-28 17:53:53 -0700

    Add setting UI to toggle Reader Mode for Accessibility
    The user-facing name is "Simplified View".
    
    Bug:
    Change-Id: I50aff7a3e6d936c7f3277a8f378ab7b4ac28db7a

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3523,6 +3523,13 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(features::kAsyncDns)},
 #endif  // defined(OS_ANDROID)
 
+#if defined(OS_ANDROID)
+    {"allow-reader-for-accessibility",
+     flag_descriptions::kAllowReaderForAccessibilityName,
+     flag_descriptions::kAllowReaderForAccessibilityDescription, kOsAndroid,
+     FEATURE_VALUE_TYPE(chrome::android::kAllowReaderForAccessibility)},
+#endif  // defined(OS_ANDROID)
+
     // NOTE: Adding new command-line switches requires adding corresponding
     // entries to enum "LoginCustomFlags" in histograms/enums.xml. See note in
     // enums.xml and don't forget to run AboutFlagsHistogramTest unit test.
