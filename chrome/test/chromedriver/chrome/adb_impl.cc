cc96beeeef4efe8d7da9999dffe7f0e47d726931
Isaac Murchie <murchieisaac@gmail.com>
2017-09-29 11:09:21 -0400

    [ChromeDriver] Fix Chromedriver functioning for Android O
    In Android O "ps" only returns the user's processes (generally just the "ps"
    command itself). Run both the bare "ps" command and with the flag "-A" to
    handle Android O.
    
    Move to using ps && ps -A for chromedriver process inspection
    
    Bug: chromedriver:2013

--- chrome/test/chromedriver/chrome/adb_impl.cc
+++ chrome/test/chromedriver/chrome/adb_impl.cc
@@ -199,7 +199,10 @@ Status AdbImpl::GetPidByName(const std::string& device_serial,
                              const std::string& process_name,
                              int* pid) {
   std::string response;
-  Status status = ExecuteHostShellCommand(device_serial, "ps", &response);
+  // on Android O `ps` returns only user processes, so also try with `-A` flag.
+  Status status = ExecuteHostShellCommand(device_serial, "ps && ps -A",
+      &response);
+
   if (!status.IsOk())
     return status;
 
@@ -256,4 +259,3 @@ Status AdbImpl::ExecuteHostShellCommand(
       "host:transport:" + device_serial + "|shell:" + shell_command,
       response);
 }
-
