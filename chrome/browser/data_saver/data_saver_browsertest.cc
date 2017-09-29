04fd741560366dbb4c3aec0d05b4efc3f30f4b88
Tarun Bansal <tbansal@chromium.org>
2017-09-28 23:27:35 -0700

    Add save-data header holdback field trial experiment.
    When Chrome is in save-data header holdback experiment, then
    save-data header is not sent to origins, and the render processes
    are not notified of the data saver state.
    
    Also, add save-data header to HTTPNames.json5.
    
    Bug:698367
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ib8c28f0399376969bf28b0933c9560d67a22a934

--- chrome/browser/data_saver/data_saver_browsertest.cc
+++ chrome/browser/data_saver/data_saver_browsertest.cc
@@ -12,6 +12,7 @@
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/prefs/pref_service.h"
+#include "content/public/common/content_features.h"
 #include "content/public/test/browser_test_base.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
@@ -48,6 +49,41 @@ IN_PROC_BROWSER_TEST_F(DataSaverBrowserTest, DataSaverEnabled) {
 IN_PROC_BROWSER_TEST_F(DataSaverBrowserTest, DataSaverDisabled) {
   ASSERT_TRUE(embedded_test_server()->Start());
   EnableDataSaver(false);
+  VerifySaveDataHeader("None");
+}
+
+class DataSaverHoldbackBrowserTest : public InProcessBrowserTest {
+ protected:
+  void SetUp() override {
+    scoped_feature_list_.InitAndEnableFeature(features::kDataSaverHoldback);
+    InProcessBrowserTest::SetUp();
+  }
+
+  void EnableDataSaver(bool enabled) {
+    PrefService* prefs = browser()->profile()->GetPrefs();
+    prefs->SetBoolean(prefs::kDataSaverEnabled, enabled);
+    content::RunAllPendingInMessageLoop();
+  }
+
+  void VerifySaveDataHeader(const std::string& expected_header_value) {
+    ui_test_utils::NavigateToURL(
+        browser(), embedded_test_server()->GetURL("/echoheader?Save-Data"));
+    std::string header_value;
+    EXPECT_TRUE(content::ExecuteScriptAndExtractString(
+        browser()->tab_strip_model()->GetActiveWebContents(),
+        "window.domAutomationController.send(document.body.textContent);",
+        &header_value));
+    EXPECT_EQ(expected_header_value, header_value);
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+IN_PROC_BROWSER_TEST_F(DataSaverHoldbackBrowserTest,
+                       DataSaverEnabledWithHoldbackEnabled) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+  EnableDataSaver(true);
   VerifySaveDataHeader("None");
 }
 
