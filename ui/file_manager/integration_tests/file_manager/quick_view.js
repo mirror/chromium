69344fbc61f9f496070be7306ff698493605b42f
mariannet <mariannet@google.com>
2017-09-29 11:13:03 +0900

    Add a test that opens quick view, closes it, and opens it again on a folder.
    Originally, this was supposed to be a regression test for bug 762699, but this
    test can't detect the bug...
    Test that first opens quick view, closes it, then opens it again.
    
    Bug: 762699
    Test: this adds a test
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ib3d8ac961dc4fd57a1ec28e53378a5ba92b073f8

--- ui/file_manager/integration_tests/file_manager/quick_view.js
+++ ui/file_manager/integration_tests/file_manager/quick_view.js
@@ -4,12 +4,10 @@
 
 'use strict';
 
-function openQuickViewSteps(appId) {
+function openQuickViewSteps(appId, filename = 'My Desktop Background.png') {
   return [
     function(results) {
-      // Select an image file.
-      remoteCall.callRemoteTestUtil(
-          'selectFile', appId, ['My Desktop Background.png'], this.next);
+      remoteCall.callRemoteTestUtil('selectFile', appId, [filename], this.next);
     },
     function(results) {
       chrome.test.assertTrue(results);
@@ -40,24 +38,7 @@ function openQuickViewSteps(appId) {
       chrome.test.assertEq(1, results.length);
       // Check Quick View dialog is displayed.
       chrome.test.assertEq('block', results[0].styles.display);
-
       checkIfNoErrorsOccured(this.next);
-    },
-    function() {
-      // Wait until Quick View is displayed.
-      repeatUntil(function() {
-        return remoteCall
-            .callRemoteTestUtil(
-                'deepQueryAllElements', appId,
-                [['#quick-view', '#dialog'], null, ['display']])
-            .then(function(results) {
-              if (results.length === 0 ||
-                  results[0].styles.display === 'none') {
-                return pending('Quick View is not opened yet.');
-              };
-              return results;
-            });
-      }).then(this.next);
     },
   ];
 }
@@ -104,5 +85,17 @@ testcase.closeQuickView = function() {
   setupAndWaitUntilReady(null, RootPath.DOWNLOADS).then(function(results) {
     StepsRunner.run(openQuickViewSteps(results.windowId)
                         .concat(closeQuickViewSteps(results.windowId)));
+  });
+};
+
+/**
+ * Open quick view, close it again, and try to open it again for folders.
+ */
+testcase.openQuickViewForFoldersAfterClose = function() {
+  setupAndWaitUntilReady(null, RootPath.DOWNLOADS).then(function(results) {
+    StepsRunner.run(
+        openQuickViewSteps(results.windowId, 'My Desktop Background.png')
+            .concat(closeQuickViewSteps(results.windowId))
+            .concat(openQuickViewSteps(results.windowId, 'photos')));
   });
 };
