570abfaf64ed05da56f1c744beb6bc0b86bdec47
John Chen <johnchen@chromium.org>
2017-09-28 20:27:37 -0700

    [ChromeDriver] Fix userAgent for emulated devices
    The userAgent strings associated with some emulated mobile devices
    contain substring "%s", which should be replaced with Chrome version.
    
    Bug: chromedriver:1883
    Change-Id: I475a9a20e72008956bc201b3677cb60f6632e57c

--- chrome/test/chromedriver/embed_mobile_devices_in_cpp.py
+++ chrome/test/chromedriver/embed_mobile_devices_in_cpp.py
@@ -23,9 +23,29 @@ import cpp_source
 def main():
   parser = optparse.OptionParser()
   parser.add_option(
+      '', '--version-file', type='string', help='Path to Chrome version file')
+  parser.add_option(
       '', '--directory', type='string', default='.',
       help='Path to directory where the cc/h files should be created')
   options, args = parser.parse_args()
+
+  # The device userAgent string may contain '%s', which should be replaced with
+  # current Chrome version. First we read the version file.
+  if options.version_file == None:
+    print 'Missing required parameter --version-file'
+    return 1
+  version_parts = ['MAJOR', 'MINOR', 'BUILD', 'PATCH']
+  version = []
+  version_file = open(options.version_file, 'r')
+  for part in version_parts:
+    # The version file should have 4 lines, with format like MAJOR=63
+    components = version_file.readline().split('=')
+    if len(components) != 2 or components[0].strip() != part:
+      print 'Bad version file'
+      return 1
+    version.append(components[1].strip())
+  # Join parts of version together using '.' as separator
+  version = '.'.join(version)
 
   devices = {}
   file_name = args[0]
@@ -37,7 +57,7 @@ def main():
     if extension['type'] == 'emulated-device':
       device = extension['device']
       devices[device['title']] = {
-        'userAgent': device['user-agent'],
+        'userAgent': device['user-agent'].replace('%s', version),
         'width': device['screen']['vertical']['width'],
         'height': device['screen']['vertical']['height'],
         'deviceScaleFactor': device['screen']['device-pixel-ratio'],
