dfa24b468efd91165769427b88e382545d12aee1
Scott Chen <scottchen@chromium.org>
2017-09-29 14:05:20 -0700

    MD Extensions: Add devtools in error-console.
    This CL ports over the runtime error stack-trace and dev-view button
    that existed in the old extensions page.
    
    Bug: 768609
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I80fc989ca0b3bcc8af7483a2f475e673e2476674

--- chrome/browser/resources/md_extensions/error_page.js
+++ chrome/browser/resources/md_extensions/error_page.js
@@ -11,22 +11,39 @@ cr.define('extensions', function() {
   'use strict';
 
   /** @interface */
-  const ErrorPageDelegate = function() {};
-
-  ErrorPageDelegate.prototype = {
+  class ErrorPageDelegate {
     /**
      * @param {string} extensionId
-     * @param {!Array<number>=} opt_errorIds
-     * @param {chrome.developerPrivate.ErrorType=} opt_type
+     * @param {!Array<number>=} errorIds
+     * @param {chrome.developerPrivate.ErrorType=} type
      */
-    deleteErrors: assertNotReached,
+    deleteErrors(extensionId, errorIds, type) {}
 
     /**
      * @param {chrome.developerPrivate.RequestFileSourceProperties} args
      * @return {!Promise<!chrome.developerPrivate.RequestFileSourceResponse>}
      */
-    requestFileSource: assertNotReached,
-  };
+    requestFileSource(args) {}
+
+    /**
+     * @param {!chrome.developerPrivate.OpenDevToolsProperties} args
+     */
+    openDevTools(args) {}
+  }
+
+  /**
+   * Get the url relative to the main extension url. If the url is
+   * unassociated with the extension, this will be the full url.
+   * @param {string} url
+   * @param {?(ManifestError|RuntimeError)} error
+   * @return {string}
+   */
+  function getRelativeUrl(url, error) {
+    const fullUrl = 'chrome-extension://' + error.extensionId + '/';
+    return url.substring(0, fullUrl.length) == fullUrl ?
+        url.substring(fullUrl.length) :
+        url;
+  }
 
   const ErrorPage = Polymer({
     is: 'extensions-error-page',
@@ -40,6 +57,25 @@ cr.define('extensions', function() {
 
       /** @private {?(ManifestError|RuntimeError)} */
       selectedError_: Object,
+
+      /** @private {?chrome.developerPrivate.StackFrame}*/
+      selectedStackFrame_: {
+        type: Object,
+        value: function() {
+          return null;
+        },
+      },
+
+      /** @private */
+      isRuntimeError_: Boolean,
+
+      /** @private */
+      stackTraceExpanded_: {
+        type: Boolean,
+        value: function() {
+          return false;
+        },
+      },
     },
 
     observers: [
@@ -54,9 +90,8 @@ cr.define('extensions', function() {
      */
     observeDataChanges_: function() {
       assert(this.data);
-      const e = this.data.manifestErrors[0] || this.data.runtimeErrors[0];
-      if (e)
-        this.selectedError_ = e;
+      this.selectedError_ =
+          this.data.manifestErrors[0] || this.data.runtimeErrors[0] || null;
     },
 
     /**
@@ -117,6 +152,17 @@ cr.define('extensions', function() {
      * @private
      */
     onSelectedErrorChanged_: function() {
+      // Reset values that only apply if an error is selected, and the error's
+      // type is "RUNTIME".
+      this.isRuntimeError_ = false;
+      this.selectedStackFrame_ = null;
+      this.shownStackTrace_ = [];
+
+      if (!this.selectedError_) {
+        this.$['code-section'].code = null;
+        return;
+      }
+
       const error = this.selectedError_;
       const args = {
         extensionId: error.extensionId,
@@ -129,6 +175,9 @@ cr.define('extensions', function() {
           args.manifestSpecific = error.manifestSpecific;
           break;
         case chrome.developerPrivate.ErrorType.RUNTIME:
+          this.isRuntimeError_ = true;
+          this.shownStackTrace_ = this.selectedError_.stackTrace;
+          this.selectedStackFrame_ = this.shownStackTrace_[0];
           // slice(1) because pathname starts with a /.
           args.pathSuffix = new URL(error.source).pathname.slice(1);
           args.lineNumber = error.stackTrace && error.stackTrace[0] ?
@@ -139,6 +188,97 @@ cr.define('extensions', function() {
       this.delegate.requestFileSource(args).then(code => {
         this.$['code-section'].code = code;
       });
+    },
+
+    /**
+     * The description is a human-readable summation of the frame, in the
+     * form "<relative_url>:<line_number> (function)", e.g.
+     * "myfile.js:25 (myFunction)".
+     * @param {!chrome.developerPrivate.StackFrame} frame
+     * @return {string}
+     * @private
+     */
+    getStackTraceLabel_: function(frame) {
+      let description = getRelativeUrl(frame.url, this.selectedError_) + ':' +
+          frame.lineNumber;
+
+      if (frame.functionName) {
+        const functionName = frame.functionName == '(anonymous function)' ?
+            loadTimeData.getString('anonymousFunction') :
+            frame.functionName;
+        description += ' (' + functionName + ')';
+      }
+
+      return description;
+    },
+
+    /** @private */
+    getExpandedClass_: function() {
+      return this.stackTraceExpanded_ ? 'expanded' : '';
+    },
+
+    /**
+     * @param {chrome.developerPrivate.StackFrame} frame
+     * @return {string}
+     * @private
+     */
+    getStackFrameClass_: function(frame) {
+      return frame == this.selectedStackFrame_ ? 'selected' : '';
+    },
+
+    /**
+     * This function is used to determine whether or not we want to show a
+     * stack frame. We don't want to show code from internal scripts.
+     * @param {string} url
+     * @return {boolean}
+     * @private
+     */
+    shouldDisplayFrame_: function(url) {
+      // All our internal scripts are in the 'extensions::' namespace.
+      return !/^extensions::/.test(url);
+    },
+
+    /**
+     * @param {!Event} e
+     * @private
+     */
+    onStackFrameTap_: function(e) {
+      const frame = (/** @type {!{model:Object}} */ (e)).model.item;
+
+      this.selectedStackFrame_ = frame;
+
+      this.delegate
+          .requestFileSource({
+            extensionId: this.selectedError_.extensionId,
+            message: this.selectedError_.message,
+            pathSuffix: getRelativeUrl(frame.url, this.selectedError_),
+            lineNumber: frame.lineNumber,
+          })
+          .then(code => {
+            this.$['code-section'].code = code;
+          });
+    },
+
+    /** @private */
+    onDevToolButtonTap_: function() {
+      // This guarantees renderProcessId and renderViewId.
+      assert(
+          this.selectedError_.type ==
+          chrome.developerPrivate.ErrorType.RUNTIME);
+      assert(this.selectedStackFrame_);
+
+      this.delegate.openDevTools({
+        renderProcessId: this.selectedError_.renderProcessId,
+        renderViewId: this.selectedError_.renderViewId,
+        url: this.selectedStackFrame_.url,
+        lineNumber: this.selectedStackFrame_.lineNumber || 0,
+        columnNumber: this.selectedStackFrame_.columnNumber || 0,
+      });
+    },
+
+    /** @private */
+    onStackTraceTap_: function() {
+      this.stackTraceExpanded_ = !this.stackTraceExpanded_;
     },
 
     /**
