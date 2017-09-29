082331e1ea93c8ccdb38dbbed36e3ef63c0067a5
jkereliuk <kereliuk@chromium.org>
2017-09-29 12:31:04 -0700

    [ChromeDriver] Key type actions for the actions API
    This CL contains the key type actions. Follow up CLs for tests and
    pointer/pause actions
    
    spec: https://w3c.github.io/webdriver/webdriver-spec.html#actions
    
    Bug:chromedriver:1897
    Change-Id: I135df58d97a45494e51a5ac09dfce021f7d094d3

---
+++ chrome/test/chromedriver/window_commands_unittest.cc
@@ -0,0 +1,19 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/test/chromedriver/window_commands.h"
+
+#include <memory>
+#include <string>
+
+#include "base/values.h"
+#include "chrome/test/chromedriver/chrome/status.h"
+#include "chrome/test/chromedriver/session.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+TEST(WindowCommandsTest, ProcessInputActionSequenceNone) {}
+
+TEST(WindowCommandsTest, ProcessInputActionSequenceKey) {}
+
+TEST(WindowCommandsTest, ProcessInputActionSequencePointer) {}
