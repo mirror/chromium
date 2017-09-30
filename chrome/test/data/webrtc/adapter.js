95b2d7d619d356e31dbc615f55ddfed9346c9440
Philip Jägenstedt <foolip@chromium.org>
2017-09-30 13:05:49 +0200

    Remove internal use of webkitMediaStream and webkitRTCPeerConnection
    This is a no-op change that makes it easier to later remove them.
    
    Bug: 690582
    Change-Id: I8f4074102dff3ef87b35d239f06ce2c93246d88d

--- chrome/test/data/webrtc/adapter.js
+++ chrome/test/data/webrtc/adapter.js
@@ -97,9 +97,6 @@ if (navigator.mozGetUserMedia) {
     return iceServer;
   };
 
-  // The RTCPeerConnection object.
-  RTCPeerConnection = webkitRTCPeerConnection;
-
   // Get UserMedia (only difference is the prefix).
   // Code from Adam Barth.
   getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
@@ -120,27 +117,6 @@ if (navigator.mozGetUserMedia) {
   reattachMediaStream = function(to, from) {
     to.src = from.src;
   };
-
-  // The representation of tracks in a stream is changed in M26.
-  // Unify them for earlier Chrome versions in the coexisting period.
-  if (!webkitMediaStream.prototype.getVideoTracks) {
-    webkitMediaStream.prototype.getVideoTracks = function() {
-      return this.videoTracks;
-    };
-    webkitMediaStream.prototype.getAudioTracks = function() {
-      return this.audioTracks;
-    };
-  }
-
-  // New syntax of getXXXStreams method in M26.
-  if (!webkitRTCPeerConnection.prototype.getLocalStreams) {
-    webkitRTCPeerConnection.prototype.getLocalStreams = function() {
-      return this.localStreams;
-    };
-    webkitRTCPeerConnection.prototype.getRemoteStreams = function() {
-      return this.remoteStreams;
-    };
-  }
 } else {
   console.log("Browser does not appear to be WebRTC-capable");
 }
