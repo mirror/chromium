941d10ac65296a9bb594a1be10f4fe8469787395
Ian Vollick <vollick@chromium.org>
2017-10-01 09:37:47 -0400

    [vr] Fix banding on gradients
    This CL introduces some very cheesy noise to the gradient colors that
    eliminates the banding artifacts.
    
    Bug:770549
    Change-Id: Ief55e597bb5802a541db41027d4d7644cefebd3c

--- chrome/browser/vr/vr_shell_renderer.cc
+++ chrome/browser/vr/vr_shell_renderer.cc
@@ -340,7 +340,11 @@ static constexpr char const* kGradientQuadFragmentShader = SHADER(
     float mask = smoothstep(1.0 + u_CornerScale,
         1.0 - u_CornerScale,
         length(v_CornerPosition));
-    gl_FragColor = color * u_Opacity * mask;
+    // Perturb the color to avoid banding.
+    float noise_channel = fract(dot(v_Position.xy, vec2(12345.67, 2345.45)));
+    noise_channel = (noise_channel - 0.5) / 255.0;
+    vec4 noise = vec4(noise_channel, noise_channel, noise_channel, 1);
+    gl_FragColor = (color + noise) * u_Opacity * mask;
   }
 );
 
@@ -386,7 +390,12 @@ static constexpr char const* kGradientGridFragmentShader = SHADER(
           grid_color.xyz + bg_color.xyz * (1.0 - opacity),
           opacity + bg_color.w * (1.0 - opacity));
     } else {
-      gl_FragColor = bg_color;
+      // Perturb the color to avoid banding.
+      float noise_channel =
+          fract(dot(v_GridPosition.xy, vec2(12345.67, 2345.45)));
+      noise_channel = (noise_channel - 0.5) / 255.0;
+      vec4 noise = vec4(noise_channel, noise_channel, noise_channel, 1);
+      gl_FragColor = bg_color + noise;
     }
   }
 );
