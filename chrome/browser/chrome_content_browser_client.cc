2b7086831fd7dbaabceb25422a794159b2a76aec
Lei Zhang <thestig@chromium.org>
2017-10-02 16:44:51 -0700

    Refer to ui::ResourceBundle with namespace in chrome/
    ResourceBundle became ui::ResourceBundle in r72038 but many callers
    still refer to it without the namespace.
    
    Change-Id: I7a150f17f4b150cb7035627063588331b12aea57

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1876,7 +1876,7 @@ std::string ChromeContentBrowserClient::GetAcceptLangs(
 }
 
 const gfx::ImageSkia* ChromeContentBrowserClient::GetDefaultFavicon() {
-  ResourceBundle& rb = ResourceBundle::GetSharedInstance();
+  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
   return rb.GetNativeImageNamed(IDR_DEFAULT_FAVICON).ToImageSkia();
 }
 
@@ -3086,7 +3086,7 @@ void ChromeContentBrowserClient::RegisterOutOfProcessServices(
 
 std::unique_ptr<base::Value>
 ChromeContentBrowserClient::GetServiceManifestOverlay(base::StringPiece name) {
-  ResourceBundle& rb = ResourceBundle::GetSharedInstance();
+  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
   int id = -1;
   if (name == content::mojom::kBrowserServiceName)
     id = IDR_CHROME_CONTENT_BROWSER_MANIFEST_OVERLAY;
