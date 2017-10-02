f5a5d1022a81f807dc4c895f27ca24df0c49e823
Thanh Le <thanhdle@chromium.org>
2017-10-02 14:29:12 -0700

    Create PreviewsLog object that will be used to post log messages on chrome://interventions-internals.
    Bug: 764409
    Change-Id: I6ad2181cb389c67dc39220340d136cf13996cc7c

---
+++ components/previews/core/previews_log.h
@@ -0,0 +1,79 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PREVIEWS_CORE_PREVIEWS_LOG_H_
+#define COMPONENTS_PREVIEWS_CORE_PREVIEWS_LOG_H_
+
+#include <list>
+#include <string>
+
+#include "base/macros.h"
+#include "base/sequence_checker.h"
+#include "base/time/time.h"
+#include "components/previews/core/previews_experiments.h"
+#include "url/gurl.h"
+
+namespace previews {
+
+// Records information about previews and interventions events. The class only
+// keeps the recent event logs.
+class PreviewsLogger {
+ public:
+  // Information needed for a log message. This information will be used to
+  // display log messages on chrome://interventions-internals.
+  struct MessageLog {
+    MessageLog(const std::string& event_type,
+               const std::string& event_description,
+               const GURL& url,
+               base::Time time);
+
+    MessageLog(const MessageLog& other);
+
+    const std::string event_type;
+    const std::string event_description;
+    const GURL url;
+    const base::Time time;
+  };
+
+  // Information about a preview navigation.
+  struct PreviewNavigation {
+    PreviewNavigation(const GURL& url,
+                      PreviewsType type,
+                      bool opt_out,
+                      base::Time time)
+        : url(url), type(type), opt_out(opt_out), time(time) {}
+
+    const GURL url;
+    const PreviewsType type;
+    const bool opt_out;
+    const base::Time time;
+  };
+
+  PreviewsLogger();
+  virtual ~PreviewsLogger();
+
+  std::vector<MessageLog> log_messages() const;
+
+  // Add MessageLog using the given information. Pop out the oldest log if the
+  // size of |log_messages_| grows larger than a threshold.
+  void LogMessage(const std::string& event_type,
+                  const std::string& event_description,
+                  const GURL& url,
+                  base::Time time);
+
+  // Convert |navigation| to a MessageLog, and add that message to
+  // |log_messages_|. Virtual for testing.
+  virtual void LogPreviewNavigation(const PreviewNavigation& navigation);
+
+  // Collection of recorded log messages.
+  std::list<MessageLog> log_messages_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  DISALLOW_COPY_AND_ASSIGN(PreviewsLogger);
+};
+
+}  // namespace previews
+
+#endif  // COMPONENTS_PREVIEWS_CORE_PREVIEWS_LOG_H
