fb2a526167bf0c7cc7bbda848f68ff38a397ca97
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-10-02 13:45:33 -0400

    [wip] Very wip for using ui::GpuMain from GpuChildThread.
    Bug:
    Change-Id: I87a40121ee5881f64046dab46fd6558925ce1d06

--- content/gpu/gpu_child_thread.cc
+++ content/gpu/gpu_child_thread.cc
@@ -130,74 +130,41 @@ class QueueingConnectionFilter : public ConnectionFilter {
 
 }  // namespace
 
-GpuChildThread::GpuChildThread(
-    std::unique_ptr<gpu::GpuWatchdogThread> watchdog_thread,
-    bool dead_on_arrival,
-    const gpu::GPUInfo& gpu_info,
-    const gpu::GpuFeatureInfo& gpu_feature_info,
-    DeferredMessages deferred_messages)
-    : GpuChildThread(GetOptions(),
-                     std::move(watchdog_thread),
-                     dead_on_arrival,
-                     false /* in_browser_process */,
-                     gpu_info,
-                     gpu_feature_info) {
-  deferred_messages_ = std::move(deferred_messages);
+GpuChildThread::GpuChildThread(std::unique_ptr<gpu::GpuInit> gpu_init,
+                               ui::GpuMain::LogMessages log_messages)
+    : ChildThreadImpl(GetOptions()),
+      gpu_main_(this, std::move(gpu_init)),
+      in_browser_process_(false),
+      weak_factory_(this) {
+  gpu_main_.SetLogMessagesForHost(std::move(log_messages));
 }
 
 GpuChildThread::GpuChildThread(const InProcessChildThreadParams& params,
-                               const gpu::GPUInfo& gpu_info,
-                               const gpu::GpuFeatureInfo& gpu_feature_info)
-    : GpuChildThread(ChildThreadImpl::Options::Builder()
-                         .InBrowserProcess(params)
-                         .AutoStartServiceManagerConnection(false)
-                         .ConnectToBrowser(true)
-                         .Build(),
-                     nullptr /* watchdog_thread */,
-                     false /* dead_on_arrival */,
-                     true /* in_browser_process */,
-                     gpu_info,
-                     gpu_feature_info) {}
-
-GpuChildThread::GpuChildThread(
-    const ChildThreadImpl::Options& options,
-    std::unique_ptr<gpu::GpuWatchdogThread> gpu_watchdog_thread,
-    bool dead_on_arrival,
-    bool in_browser_process,
-    const gpu::GPUInfo& gpu_info,
-    const gpu::GpuFeatureInfo& gpu_feature_info)
-    : ChildThreadImpl(options),
-      dead_on_arrival_(dead_on_arrival),
-      in_browser_process_(in_browser_process),
-      gpu_service_(
-          new viz::GpuServiceImpl(gpu_info,
-                                  std::move(gpu_watchdog_thread),
-                                  ChildProcess::current()->io_task_runner(),
-                                  gpu_feature_info)),
-      gpu_main_binding_(this),
-      weak_factory_(this) {
-  if (in_browser_process_) {
-    DCHECK(base::CommandLine::ForCurrentProcess()->HasSwitch(
-               switches::kSingleProcess) ||
-           base::CommandLine::ForCurrentProcess()->HasSwitch(
-               switches::kInProcessGPU));
-  }
-  gpu_service_->set_in_host_process(in_browser_process_);
-}
+                               std::unique_ptr<gpu::GpuInit> gpu_init)
+    : ChildThreadImpl(ChildThreadImpl::Options::Builder()
+                          .InBrowserProcess(params)
+                          .AutoStartServiceManagerConnection(false)
+                          .ConnectToBrowser(true)
+                          .Build()),
+      gpu_main_(this, std::move(gpu_init)),
+      in_browser_process_(true),
+      weak_factory_(this) {}
 
 GpuChildThread::~GpuChildThread() {
 }
 
 void GpuChildThread::Init(const base::Time& process_start_time) {
-  gpu_service_->set_start_time(process_start_time);
+  //  gpu_service_->set_start_time(process_start_time);
 
-#if defined(OS_ANDROID)
   // When running in in-process mode, this has been set in the browser at
   // ChromeBrowserMainPartsAndroid::PreMainMessageLoopRun().
-  if (!in_browser_process_)
+  if (!in_browser_process_) {
+#if defined(OS_ANDROID)
     media::SetMediaDrmBridgeClient(
         GetContentClient()->GetMediaDrmBridgeClient());
 #endif
+  }
+
   AssociatedInterfaceRegistry* associated_registry = &associated_interfaces_;
   associated_registry->AddInterface(base::Bind(
       &GpuChildThread::CreateGpuMainService, base::Unretained(this)));
@@ -220,7 +187,7 @@ void GpuChildThread::Init(const base::Time& process_start_time) {
 
 void GpuChildThread::CreateGpuMainService(
     ui::mojom::GpuMainAssociatedRequest request) {
-  gpu_main_binding_.Bind(std::move(request));
+  gpu_main_.BindAssociated(std::move(request));
 }
 
 bool GpuChildThread::Send(IPC::Message* msg) {
@@ -240,41 +207,20 @@ void GpuChildThread::OnAssociatedInterfaceRequest(
     ChildThreadImpl::OnAssociatedInterfaceRequest(name, std::move(handle));
 }
 
-void GpuChildThread::CreateGpuService(
-    viz::mojom::GpuServiceRequest request,
-    ui::mojom::GpuHostPtr gpu_host,
-    const gpu::GpuPreferences& gpu_preferences,
-    mojo::ScopedSharedBufferHandle activity_flags) {
-  gpu_service_->UpdateGPUInfoFromPreferences(gpu_preferences);
-  for (const LogMessage& log : deferred_messages_)
-    gpu_host->RecordLogMessage(log.severity, log.header, log.message);
-  deferred_messages_.clear();
+gpu::SyncPointManager* GpuChildThread::GetSyncPointManager() {
+  return GetContentClient()->gpu()
+             ? GetContentClient()->gpu()->GetSyncPointManager()
+             : nullptr;
+}
 
-  if (dead_on_arrival_) {
-    LOG(ERROR) << "Exiting GPU process due to errors during initialization";
-    gpu_service_.reset();
-    gpu_host->DidFailInitialize();
-    base::RunLoop::QuitCurrentWhenIdleDeprecated();
-    return;
-  }
+base::WaitableEvent* GpuChildThread::GetShutDownEvent() {
+  return ChildProcess::current()->GetShutDownEvent();
+}
 
-  // Bind should happen only if initialization succeeds (i.e. not dead on
-  // arrival), because otherwise, it can receive requests from the host while in
-  // an uninitialized state.
-  gpu_service_->Bind(std::move(request));
-  gpu::SyncPointManager* sync_point_manager = nullptr;
-  // Note SyncPointManager from ContentGpuClient cannot be owned by this.
-  if (GetContentClient()->gpu())
-    sync_point_manager = GetContentClient()->gpu()->GetSyncPointManager();
-  gpu_service_->InitializeWithHost(
-      std::move(gpu_host),
-      gpu::GpuProcessActivityFlags(std::move(activity_flags)),
-      sync_point_manager, ChildProcess::current()->GetShutDownEvent());
-  CHECK(gpu_service_->media_gpu_channel_manager());
-
+void GpuChildThread::OnGpuServiceConnection(viz::GpuServiceImpl* gpu_service) {
   // Only set once per process instance.
   service_factory_.reset(new GpuServiceFactory(
-      gpu_service_->media_gpu_channel_manager()->AsWeakPtr()));
+      gpu_service->media_gpu_channel_manager()->AsWeakPtr()));
 
 #if defined(OS_ANDROID)
   gpu_service_->media_gpu_channel_manager()->SetOverlayFactory(
@@ -282,15 +228,10 @@ void GpuChildThread::CreateGpuService(
 #endif
 
   if (GetContentClient()->gpu())  // NULL in tests.
-    GetContentClient()->gpu()->GpuServiceInitialized(gpu_preferences);
+    GetContentClient()->gpu()->GpuServiceInitialized(
+        gpu_service->gpu_preferences());
 
   release_pending_requests_closure_.Run();
-}
-
-void GpuChildThread::CreateFrameSinkManager(
-    viz::mojom::FrameSinkManagerRequest request,
-    viz::mojom::FrameSinkManagerClientPtr client) {
-  NOTREACHED();
 }
 
 void GpuChildThread::BindServiceFactoryRequest(
