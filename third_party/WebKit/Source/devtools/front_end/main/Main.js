38499a66f615b1925475c8a576437f2444825e70
Will Chen <chenwilliam@chromium.org>
2017-10-02 12:20:09 -0700

    DevTools: test path for experiment in new test framework
    The legacy test framework has a mechanism for enabling experiments at the startup
    of DevTools, and this brings parity in the new test framework.
    
    In the long-term, there should be a better solution as this is very
    confusing for first-time DevTools test contributors and is a code smell.
    
    Bug: 667560
    Change-Id: I230453e16b3d06f6ee9ada99c0beb793f244d8c5

--- third_party/WebKit/Source/devtools/front_end/main/Main.js
+++ third_party/WebKit/Source/devtools/front_end/main/Main.js
@@ -145,7 +145,7 @@ Main.Main = class {
     Runtime.experiments.cleanUpStaleExperiments();
 
     if (Host.isUnderTest(prefs)) {
-      var testPath = JSON.parse(prefs['testPath'] || '""');
+      var testPath = Runtime.queryParam('test') || JSON.parse(prefs['testPath'] || '""');
       // Enable experiments for testing.
       if (testPath.indexOf('accessibility/') !== -1)
         Runtime.experiments.enableForTest('accessibilityInspection');
