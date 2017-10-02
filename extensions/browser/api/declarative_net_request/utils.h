a4004d9d123620dd0f5ecc81381f76e1d25a8776
Karan Bhatia <karandeepb@chromium.org>
2017-10-02 13:16:16 -0700

    DNR: Save indexed ruleset checksum as part of Extension preferences.
    This CL adds a new extension preference to save the checksum of the indexed
    ruleset for the Declarative Net Request API. This preference is saved during
    extension installation for packed and loading for unpacked extensions. The
    checksum is useful to determine whether an indexed ruleset exists for an
    extension and to verify the integrity of the ruleset on disk.
    
    Doc=http://go/declarative-net-request (Internal only)
    BUG=696822
    
    Change-Id: I092cc9c113a7ba61562314462f8fcd74bd8a8b11

--- extensions/browser/api/declarative_net_request/utils.h
+++ extensions/browser/api/declarative_net_request/utils.h
@@ -19,12 +19,17 @@ struct InstallWarning;
 namespace declarative_net_request {
 
 // Indexes and persists |rules| for |extension|. In case of an error, returns
-// false and populates |error|.
+// false and populates |error|. On success, returns |ruleset_checksum|, which
+// is a checksum of the persisted indexed ruleset file.
 // Note: This must be called on a thread where file IO is allowed.
 bool IndexAndPersistRules(const base::ListValue& rules,
                           const Extension& extension,
                           std::string* error,
-                          std::vector<InstallWarning>* warnings);
+                          std::vector<InstallWarning>* warnings,
+                          int* ruleset_checksum);
+
+// Returns the ruleset checksum of the passed |data| buffer.
+int GetRulesetChecksumForTesting(const uint8_t* data, size_t size);
 
 }  // namespace declarative_net_request
 }  // namespace extensions
