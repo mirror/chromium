90bc05f3a9afe247061750f639bc1abaa45736cc
John Wu <jzw@chromium.org>
2017-10-02 16:11:14 -0700

    Public API for signing in to ChromeWebView
    Bug:753660
    Change-Id: I52b05e132bde91651fc4c8cae944fc650812d72e

--- ios/web_view/internal/web_view_browser_state.mm
+++ ios/web_view/internal/web_view_browser_state.mm
@@ -12,11 +12,13 @@
 #include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
 #include "base/threading/thread_restrictions.h"
+#include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/in_memory_pref_store.h"
 #include "components/prefs/json_pref_store.h"
 #include "components/prefs/pref_filter.h"
 #include "components/prefs/pref_service_factory.h"
+#include "components/signin/ios/browser/active_state_manager.h"
 #include "components/translate/core/browser/translate_pref_names.h"
 #include "components/translate/core/browser/translate_prefs.h"
 #include "ios/web/public/web_thread.h"
@@ -66,6 +68,10 @@ WebViewBrowserState::WebViewBrowserState(bool off_the_record)
   prefs_ = factory.Create(pref_registry.get());
 
   base::ThreadRestrictions::SetIOAllowed(wasIOAllowed);
+
+  ActiveStateManager* active_state_manager =
+      ActiveStateManager::FromBrowserState(this);
+  active_state_manager->SetActive(true);
 }
 
 WebViewBrowserState::~WebViewBrowserState() = default;
@@ -102,6 +108,9 @@ void WebViewBrowserState::RegisterPrefs(
                                     l10n_util::GetLocaleOverride());
   pref_registry->RegisterBooleanPref(prefs::kEnableTranslate, true);
   translate::TranslatePrefs::RegisterProfilePrefs(pref_registry);
+
+  BrowserStateDependencyManager::GetInstance()
+      ->RegisterBrowserStatePrefsForServices(this, pref_registry);
 }
 
 }  // namespace ios_web_view
