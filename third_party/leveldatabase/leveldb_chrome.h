419a5a93f182457d2179c39d1a59313d3eb4d547
Chris Mumford <cmumford@chromium.org>
2017-10-02 10:46:35 -0700

    leveldb: Created a shared library.
    The Chromium leveldb related code now has static variables, but
    is in a static library. This change moves this code into a component
    to avoid having multiple copies which confuses the memory-infra
    reporting statistics.
    
    Bug: 764810
    Change-Id: I895b553923ce06cf6788688975ec430da7f2655f

--- third_party/leveldatabase/leveldb_chrome.h
+++ third_party/leveldatabase/leveldb_chrome.h
@@ -6,16 +6,25 @@
 #define THIRD_PARTY_LEVELDATABASE_LEVELDB_CHROME_H_
 
 #include "leveldb/cache.h"
+#include "leveldb/export.h"
+#include "third_party/leveldatabase/src/db/filename.h"
 
 namespace leveldb_chrome {
 
 // Return the shared leveldb block cache for web APIs. The caller *does not*
 // own the returned instance.
-extern leveldb::Cache* GetSharedWebBlockCache();
+LEVELDB_EXPORT extern leveldb::Cache* GetSharedWebBlockCache();
 
 // Return the shared leveldb block cache for browser (non web) APIs. The caller
 // *does not* own the returned instance.
-extern leveldb::Cache* GetSharedBrowserBlockCache();
+LEVELDB_EXPORT extern leveldb::Cache* GetSharedBrowserBlockCache();
+
+// If filename is a leveldb file, store the type of the file in *type.
+// The number encoded in the filename is stored in *number.  If the
+// filename was successfully parsed, returns true.  Else return false.
+LEVELDB_EXPORT extern bool ParseFileName(const std::string& filename,
+                                         uint64_t* number,
+                                         leveldb::FileType* type);
 
 }  // namespace leveldb_chrome
 
