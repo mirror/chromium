9adc665deba72f85be3df5d9f4c4e49af14e57ee
Wei Li <weili@chromium.org>
2017-10-02 15:07:15 -0700

    Add access to direct write font proxy in pdf compositor service
    Pdf compositor service needs to access direct write font proxy to
    be able to use skia to draw properly on Windows. This CL move font
    proxy init header to public so more processes (including ours)
    can use it, also it adds font proxy initialization in pdf
    compositor service.
    
    BUG=455764
    
    Change-Id: Ia7bf3c7804ae0fea671087a53cf177615ca4d59e

--- components/printing/service/public/cpp/pdf_compositor_service_factory.cc
+++ components/printing/service/public/cpp/pdf_compositor_service_factory.cc
@@ -4,16 +4,25 @@
 
 #include "components/printing/service/public/cpp/pdf_compositor_service_factory.h"
 
+#include "build/build_config.h"
 #include "components/printing/service/pdf_compositor_service.h"
 #include "content/public/utility/utility_thread.h"
 #include "third_party/WebKit/public/platform/WebImageGenerator.h"
 #include "third_party/skia/include/core/SkGraphics.h"
+
+#if defined(OS_WIN)
+#include "content/public/child/dwrite_font_proxy_init_win.h"
+#endif
 
 namespace printing {
 
 std::unique_ptr<service_manager::Service> CreatePdfCompositorService(
     const std::string& creator) {
   content::UtilityThread::Get()->EnsureBlinkInitialized();
+#if defined(OS_WIN)
+  // Initialize direct write font proxy so skia can use it.
+  content::InitializeDWriteFontProxy();
+#endif
   // Hook up blink's codecs so skia can call them.
   SkGraphics::SetImageGeneratorFromEncodedDataFactory(
       blink::WebImageGenerator::CreateAsSkImageGenerator);
--- content/child/dwrite_font_proxy/dwrite_font_proxy_init_win.cc
+++ content/child/dwrite_font_proxy/dwrite_font_proxy_init_win.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/child/dwrite_font_proxy/dwrite_font_proxy_init_win.h"
+#include "content/public/child/dwrite_font_proxy_init_win.h"
 
 #include <dwrite.h>
 
