2d2d7ab3dd89343ebdaba69c460c67d04807bb5f
erikchen <erikchen@chromium.org>
2017-10-02 13:58:19 -0700

    Switch task manager to use the memory_instrumentation interface.
    The memory_instrumentation interface on the content_browser service exposes
    cross-platform, consistent memory metrics that should also be used by the task
    manager.
    
    This CL adds a new column to the task manager: Memory Footprint. See
    https://docs.google.com/document/d/1PZyRzChnvkUNUB85Op46aqkFXuAGUJi751DJuB6O40g/edit#
    for more details.
    
    Bug: 756276
    Change-Id: I68fdd6868a231840a1a8401dcd7ec50ffb0af068

--- chrome/browser/task_manager/sampling/task_group.cc
+++ chrome/browser/task_manager/sampling/task_group.cc
@@ -89,6 +89,7 @@ TaskGroup::TaskGroup(
       expected_on_bg_done_flags_(kBackgroundRefreshTypesMask),
       current_on_bg_done_flags_(0),
       platform_independent_cpu_usage_(0.0),
+      memory_footprint_(-1),
       gpu_memory_(-1),
       memory_state_(base::MemoryState::UNKNOWN),
       per_process_network_usage_rate_(-1),
