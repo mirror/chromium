2f3216d56f44e4ab89d65af68e1527f0ecfba6bd
Leonard Grey <lgrey@chromium.org>
2017-10-02 11:12:39 -0400

    [Mac] Add system share menu
    NB: This is continued from https://codereview.chromium.org/2950403002/
    Landing it behind a flag now to catch M63 feature freeze.
    
    Replaces the "Email Link to Page" item in the file menu with a submenu
    that allows sharing via macOS Sharing Extensions. The previous e-mail
    command is superseded by the Mail sharing service, which inherits the
    key command.
    
    Bug: 465302
    Change-Id: Ie3422389118d6146df5cc2fbe8610046e3b6c563

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1365,6 +1365,9 @@ extern const char kHostedAppsInWindowsDescription[];
 extern const char kMacRTLName[];
 extern const char kMacRTLDescription[];
 
+extern const char kMacSystemShareMenuName[];
+extern const char kMacSystemShareMenuDescription[];
+
 extern const char kMacTouchBarName[];
 extern const char kMacTouchBarDescription[];
 
