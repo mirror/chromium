f449e4082bd5b7f5f1e08ba9407922949e3b7e73
Anton Urusov <urusant@google.com>
2017-10-02 13:29:05 -0400

    Improved invalid JSON error
    Bug:
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Icd40b3dbf14334317ebd13c634d33f876148f52d

--- chrome/browser/resources/policy_tool.js
+++ chrome/browser/resources/policy_tool.js
@@ -4,9 +4,19 @@
 
 // Override some methods of policy.Page.
 
+/**
+ * Disables editing policy values by hiding the main section and showing an
+ * error message instead.
+ */
+policy.Page.disableEditing = function() {
+  $('disable-editing-error').hidden = false;
+  $('main-section').hidden = true;
+};
+
 /** @override */
 policy.Page.setPolicyValues = function(values) {
   var page = this.getInstance();
+  page.enableEditing();
   var table = page.policyTables['chrome'];
   table.setPolicyValues(values.chromePolicies || {});
   if (values.hasOwnProperty('extensionPolicies')) {
@@ -58,9 +68,18 @@ policy.Page.prototype.initialize = function() {
     }
   };
 
+  $('enable-editing').onclick = () => {
+    this.enableEditing();
+    chrome.send('resetSession');
+  };
   // Notify the browser that the page has loaded, causing it to send the
   // list of all known policies and the values from the default session.
   chrome.send('initialized');
+};
+
+policy.Page.prototype.enableEditing = function() {
+  $('main-section').hidden = false;
+  $('disable-editing-error').hidden = true;
 };
 
 /**
