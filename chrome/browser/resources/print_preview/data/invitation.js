01c06900464a622b647945339fc184b41a9e0c48
dpapad <dpapad@chromium.org>
2017-10-02 13:51:36 -0700

    Print Preview cleanup: Convert various classes to ES6 syntax, part1.
    Bug:770851
    Change-Id: I2b724b3922c90feee8c0c93850964e4b155dab77

--- chrome/browser/resources/print_preview/data/invitation.js
+++ chrome/browser/resources/print_preview/data/invitation.js
@@ -5,86 +5,85 @@
 cr.define('print_preview', function() {
   'use strict';
 
-  /**
-   * Printer sharing invitation data object.
-   * @param {string} sender Text identifying invitation sender.
-   * @param {string} receiver Text identifying invitation receiver. Empty in
-   *     case of a personal invitation. Identifies a group or domain in case
-   *     of an invitation received by a group manager.
-   * @param {!print_preview.Destination} destination Shared destination.
-   * @param {!Object} aclEntry JSON representation of the ACL entry this
-   *     invitation was sent to.
-   * @param {string} account User account this invitation is sent for.
-   * @constructor
-   */
-  function Invitation(sender, receiver, destination, aclEntry, account) {
+  /* Printer sharing invitation data object. */
+  class Invitation {
     /**
-     * Text identifying invitation sender.
-     * @private {string}
+     * @param {string} sender Text identifying invitation sender.
+     * @param {string} receiver Text identifying invitation receiver. Empty in
+     *     case of a personal invitation. Identifies a group or domain in case
+     *     of an invitation received by a group manager.
+     * @param {!print_preview.Destination} destination Shared destination.
+     * @param {!Object} aclEntry JSON representation of the ACL entry this
+     *     invitation was sent to.
+     * @param {string} account User account this invitation is sent for.
      */
-    this.sender_ = sender;
+    constructor(sender, receiver, destination, aclEntry, account) {
+      /**
+       * Text identifying invitation sender.
+       * @private {string}
+       */
+      this.sender_ = sender;
 
-    /**
-     * Text identifying invitation receiver. Empty in case of a personal
-     * invitation. Identifies a group or domain in case of an invitation
-     * received by a group manager.
-     * @private {string}
-     */
-    this.receiver_ = receiver;
+      /**
+       * Text identifying invitation receiver. Empty in case of a personal
+       * invitation. Identifies a group or domain in case of an invitation
+       * received by a group manager.
+       * @private {string}
+       */
+      this.receiver_ = receiver;
 
-    /**
-     * Shared destination.
-     * @private {!print_preview.Destination}
-     */
-    this.destination_ = destination;
+      /**
+       * Shared destination.
+       * @private {!print_preview.Destination}
+       */
+      this.destination_ = destination;
 
-    /**
-     * JSON representation of the ACL entry this invitation was sent to.
-     * @private {!Object}
-     */
-    this.aclEntry_ = aclEntry;
+      /**
+       * JSON representation of the ACL entry this invitation was sent to.
+       * @private {!Object}
+       */
+      this.aclEntry_ = aclEntry;
 
-    /**
-     * Account this invitation is sent for.
-     * @private {string}
-     */
-    this.account_ = account;
-  }
+      /**
+       * Account this invitation is sent for.
+       * @private {string}
+       */
+      this.account_ = account;
+    }
 
-  Invitation.prototype = {
     /** @return {string} Text identifying invitation sender. */
-    get sender() {
+    sender() {
       return this.sender_;
-    },
+    }
 
     /** @return {string} Text identifying invitation receiver. */
-    get receiver() {
+    receiver() {
       return this.receiver_;
-    },
+    }
 
     /**
      * @return {boolean} Whether this user acts as a manager for a group of
      * users.
      */
-    get asGroupManager() {
+    asGroupManager() {
       return !!this.receiver_;
-    },
+    }
 
     /** @return {!print_preview.Destination} Shared destination. */
-    get destination() {
+    destination() {
       return this.destination_;
-    },
+    }
 
     /** @return {string} Scope (account) this invitation was sent to. */
-    get scopeId() {
+    scopeId() {
       return this.aclEntry_['scope'] || '';
-    },
+    }
 
     /** @return {string} Account this invitation is sent for. */
-    get account() {
+    account() {
       return this.account_;
     }
-  };
+  }
 
   // Export
   return {Invitation: Invitation};
