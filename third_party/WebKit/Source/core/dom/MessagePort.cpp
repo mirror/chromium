be17440b5958081950ae8877ffd8d11fe66a00a0
Marijn Kruisselbrink <mek@chromium.org>
2017-10-02 11:45:39 -0700

    Directly serialize messageport messages to/from SerializedScriptValue.
    This reduces the number of copies of the data to its minimum (of one
    copy from the SSV into the message pipe, and one copy from the message
    pipe into the SSV).
    
    In future CLs these same struct traits will also be used to optimize
    other postMessage APIs.
    
    Bug: 769907
    Change-Id: Iae57c33dd9baa06fe8860b64a43846f2962e319a

--- third_party/WebKit/Source/core/dom/MessagePort.cpp
+++ third_party/WebKit/Source/core/dom/MessagePort.cpp
@@ -30,6 +30,7 @@
 #include "bindings/core/v8/ExceptionState.h"
 #include "bindings/core/v8/serialization/SerializedScriptValue.h"
 #include "bindings/core/v8/serialization/SerializedScriptValueFactory.h"
+#include "core/dom/BlinkMessagePortMessageStructTraits.h"
 #include "core/dom/ExceptionCode.h"
 #include "core/dom/ExecutionContext.h"
 #include "core/dom/TaskRunnerHelper.h"
@@ -68,6 +69,9 @@ void MessagePort::postMessage(ScriptState* script_state,
   DCHECK(GetExecutionContext());
   DCHECK(!IsNeutered());
 
+  BlinkMessagePortMessage msg;
+  msg.message = message;
+
   // Make sure we aren't connected to any of the passed-in ports.
   for (unsigned i = 0; i < ports.size(); ++i) {
     if (ports[i] == this) {
@@ -77,15 +81,13 @@ void MessagePort::postMessage(ScriptState* script_state,
       return;
     }
   }
-  WebVector<MessagePortChannel> channels = MessagePort::DisentanglePorts(
+  msg.ports = MessagePort::DisentanglePorts(
       ExecutionContext::From(script_state), ports, exception_state);
   if (exception_state.HadException())
     return;
 
-  StringView wire_data = message->GetWireData();
-  channel_.PostMessage(
-      reinterpret_cast<const uint8_t*>(wire_data.Characters8()),
-      wire_data.length(), channels.ReleaseVector());
+  channel_.PostMojoMessage(
+      mojom::blink::MessagePortMessage::SerializeAsMessage(&msg));
 }
 
 MessagePortChannel MessagePort::Disentangle() {
@@ -154,18 +156,18 @@ bool MessagePort::TryGetMessage(RefPtr<SerializedScriptValue>& message,
   if (IsNeutered())
     return false;
 
-  std::vector<uint8_t> message_data;
-  std::vector<MessagePortChannel> channels_vector;
-  if (!channel_.GetMessage(&message_data, &channels_vector))
+  mojo::Message mojo_message;
+  if (!channel_.GetMojoMessage(&mojo_message))
     return false;
 
-  channels.Shrink(0);
-  channels.ReserveCapacity(channels_vector.size());
-  channels.AppendRange(std::make_move_iterator(channels_vector.begin()),
-                       std::make_move_iterator(channels_vector.end()));
+  BlinkMessagePortMessage msg;
+  if (!mojom::blink::MessagePortMessage::DeserializeFromMessage(
+          std::move(mojo_message), &msg)) {
+    return false;
+  }
 
-  message = SerializedScriptValue::Create(
-      reinterpret_cast<const char*>(message_data.data()), message_data.size());
+  message = std::move(msg.message);
+  channels = std::move(msg.ports);
   return true;
 }
 
