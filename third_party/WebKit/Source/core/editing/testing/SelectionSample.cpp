2a8e3d4ae4e0672aaca8460db59e701ab4e6a7d2
Yoshifumi Inoue <yosin@chromium.org>
2017-10-02 17:41:35 +0900

    2017-10-02T17:41:18
    2017-10-02T17:38:32
    Bug:
    Change-Id: I0a66af714b65cb0aa856a3a9ce8eb4eea6b100ca

--- third_party/WebKit/Source/core/editing/testing/SelectionSample.cpp
+++ third_party/WebKit/Source/core/editing/testing/SelectionSample.cpp
@@ -367,4 +367,10 @@ std::string SelectionSample::GetSelectionText(
   return Serializer<EditingStrategy>(selection).Serialize(root);
 }
 
+std::string SelectionSample::GetSelectionText(
+    const ContainerNode& root,
+    const SelectionInFlatTree& selection) {
+  return Serializer<EditingInFlatTreeStrategy>(selection).Serialize(root);
+}
+
 }  // namespace blink
