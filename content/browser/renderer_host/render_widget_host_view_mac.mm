ad01dacb44f79724ea7a1dea263079c2024424f6
Ken Buchanan <kenrb@chromium.org>
2017-10-02 14:14:38 -0400

    Plumb HitTestRegionList through content to CompositorFrameSinkSupport
    This patch causes a HitTestRegionListPtr struct pointer (that is
    supplied by the renderer process via SubmitCompositorFrame) to the
    RenderWidgetHostImpl to be passed along to the
    CompositorFrameSinkSupport. When the renderer process starts
    generating hit test regions, the browser can use these instead of
    compositor Surfaces for determining the target of a given mouse or
    touch event.
    
    Viz service hit testing is currently only being developed for Aura
    platforms, so the plumbing is currently only through
    RenderWidgetHostViews that can be instantiated in Aura.
    
    This patch also adds a new method to DelegatedFrameHost that will
    use Viz hit testing when the internal implementation is finished.
    
    Bug:750755
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ic34d73cb2823978bb3473711d4e17d5ef70ac931

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -1436,7 +1436,8 @@ void RenderWidgetHostViewMac::DidCreateNewRendererCompositorFrameSink(
 
 void RenderWidgetHostViewMac::SubmitCompositorFrame(
     const viz::LocalSurfaceId& local_surface_id,
-    viz::CompositorFrame frame) {
+    viz::CompositorFrame frame,
+    viz::mojom::HitTestRegionListPtr hit_test_region_list) {
   TRACE_EVENT0("browser", "RenderWidgetHostViewMac::OnSwapCompositorFrame");
 
   last_frame_root_background_color_ = frame.metadata.root_background_color;
