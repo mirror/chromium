d67a4a921ce842a8c8237148a8eb79f7f173fdae
Wei Li <weili@chromium.org>
2017-10-02 16:01:51 -0700

    Setup proper sandbox policy for pdf compositor service
    Pdf compositor service is a utility service that uses skia to render
    and generate pdf document. Thus it needs to access local font system.
    On Linux, it needs 'uname' to get user agent. So, on Windows and Mac,
    it resembles ppapi process in term of sandbox policy. On Linux, it is
    closer to utility process's sandbox requirements. To make its
    requirements clear, we create a new service sandbox type for it, and
    apply appropriate policy on different platforms.
    
    BUG=455764
    
    Change-Id: I7c7aeb40e5e0b09fe29887532c06d2abc04969d6

--- services/service_manager/tests/sandbox/sandbox_type_unittest.cc
+++ services/service_manager/tests/sandbox/sandbox_type_unittest.cc
@@ -64,6 +64,11 @@ TEST(SandboxTypeTest, Utility) {
   command_line6.AppendSwitchASCII(switches::kServiceSandboxType, "bogus");
   EXPECT_EQ(SANDBOX_TYPE_UTILITY, SandboxTypeFromCommandLine(command_line6));
 
+  base::CommandLine command_line7(command_line);
+  SetCommandLineFlagsForSandboxType(&command_line7, SANDBOX_TYPE_PDFCOMPOSITOR);
+  EXPECT_EQ(SANDBOX_TYPE_PDFCOMPOSITOR,
+            SandboxTypeFromCommandLine(command_line7));
+
   command_line.AppendSwitch(switches::kNoSandbox);
   EXPECT_EQ(SANDBOX_TYPE_NO_SANDBOX, SandboxTypeFromCommandLine(command_line));
 }
