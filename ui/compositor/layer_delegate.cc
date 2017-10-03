bb8565e30a6c89c4056652412ab04e59970e0e34
Francois Doray <fdoray@chromium.org>
2017-10-03 14:29:11 -0400

    Add ui::LayerDelegate::OnLayerTransform(ing|ed).
    Bug:
    Change-Id: I2b2796b1b6d64e194a036b7533c61b70ffab6f67

--- ui/compositor/layer_delegate.cc
+++ ui/compositor/layer_delegate.cc
@@ -8,6 +8,10 @@ namespace ui {
 
 void LayerDelegate::OnLayerBoundsChanged(const gfx::Rect& old_bounds) {}
 
+void LayerDelegate::OnLayerTransforming() {}
+
+void LayerDelegate::OnLayerTransformed() {}
+
 void LayerDelegate::OnLayerOpacityChanged(float old_opacity,
                                           float new_opacity) {}
 
