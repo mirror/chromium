6ae32ce61cbfdd0672d6c6eedae3f26ebb7c9751
tzik <tzik@chromium.org>
2017-10-03 18:14:56 +0900

    Remove RefPtr::LeakRef
    This converts RefPtr::LeakRef from a method to a free function, so that
    scoped_refptr don't need to have it after the unification of WTF::RefPtr
    and scoped_refptr.
    
    Bug: 763844
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I7203a55ecab6daa853e93d0b467e89a135c3891d

--- third_party/WebKit/public/platform/WebPrivatePtr.h
+++ third_party/WebKit/public/platform/WebPrivatePtr.h
@@ -107,7 +107,9 @@ class PtrStorageImpl<T,
         strongOrWeak == WebPrivatePtrStrength::kNormal,
         "Ref-counted classes do not support weak WebPrivatePtr<> references");
     Release();
-    ptr_ = val.LeakRef();
+    val->AddRef();
+    ptr_ = val.get();
+    val = nullptr;
   }
 
   void Assign(const PtrStorageImpl& other) {
