5541a10a1188c2c12222f684f14bcf3eb201d307
Ella Ge <eirage@chromium.org>
2017-10-03 15:23:20 -0400

    Fractional MouseEvent
    The last patch of fractional MouseEvent.
    
    Remove truncation in mouseEvent coordinate. 
    Add Layouttests for fractional pointer event coordinates.
    
    Bug:456625
    Change-Id: I036f4953857907daf3131fd260331dc9dd2e4874

--- third_party/WebKit/Source/core/events/MouseEvent.h
+++ third_party/WebKit/Source/core/events/MouseEvent.h
@@ -132,21 +132,11 @@ class CORE_EXPORT MouseEvent : public UIEventWithKeyState {
 
   // Note that these values are adjusted to counter the effects of zoom, so that
   // values exposed via DOM APIs are invariant under zooming.
-  // TODO(eirage): remove coordinates getter override when mouse event is
-  // fractional;
-  virtual double screenX() const {
-    return static_cast<int>(screen_location_.X());
-  }
-  virtual double screenY() const {
-    return static_cast<int>(screen_location_.Y());
-  }
+  double screenX() const { return screen_location_.X(); }
+  double screenY() const { return screen_location_.Y(); }
 
-  virtual double clientX() const {
-    return static_cast<int>(client_location_.X());
-  }
-  virtual double clientY() const {
-    return static_cast<int>(client_location_.Y());
-  }
+  double clientX() const { return client_location_.X(); }
+  double clientY() const { return client_location_.Y(); }
 
   int movementX() const { return movement_delta_.X(); }
   int movementY() const { return movement_delta_.Y(); }
@@ -157,8 +147,8 @@ class CORE_EXPORT MouseEvent : public UIEventWithKeyState {
   int offsetX();
   int offsetY();
 
-  virtual double pageX() const { return static_cast<int>(page_location_.X()); }
-  virtual double pageY() const { return static_cast<int>(page_location_.Y()); }
+  double pageX() const { return page_location_.X(); }
+  double pageY() const { return page_location_.Y(); }
 
   double x() const { return clientX(); }
   double y() const { return clientY(); }
@@ -215,12 +205,6 @@ class CORE_EXPORT MouseEvent : public UIEventWithKeyState {
 
   void ReceivedTarget() override;
 
-  // TODO(eirage): Move these coordinates to private when MouseEvent is
-  // fractional.
-  DoublePoint screen_location_;
-  DoublePoint client_location_;
-  DoublePoint page_location_;
-
  private:
   friend class MouseEventDispatchMediator;
   void InitMouseEventInternal(const AtomicString& type,
@@ -244,8 +228,11 @@ class CORE_EXPORT MouseEvent : public UIEventWithKeyState {
   void ComputePageLocation();
   void ComputeRelativePosition();
 
+  DoublePoint screen_location_;
+  DoublePoint client_location_;
   DoublePoint movement_delta_;
 
+  DoublePoint page_location_;
   DoublePoint layer_location_;
   DoublePoint offset_location_;
   DoublePoint absolute_location_;
