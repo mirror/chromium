60bb9a4b5758652ef5dd61b56775afc857ca82da
Owen Min <zmin@chromium.org>
2017-10-03 16:13:18 -0400

    TemplateWriter: checks supported version on each platform
    TemplateWriter.IsPolicySupported and
    TemplateWriter.IsPolicySupportedOnPlatform are now checking whether the
    currect version is supported or not. It will affect the generation of
    html documentation, adm/admx file, etc.
    
    TEST: components/policy/tools/template_writers/test_suite_all.py
    Bug: 770874
    
    
    Change-Id: I6fcecfaae5c87197b7829bb102afc407d2120779

--- components/policy/tools/template_writers/writers/template_writer.py
+++ components/policy/tools/template_writers/writers/template_writer.py
@@ -75,11 +75,15 @@ class TemplateWriter(object):
       return False
 
     if '*' in self.platforms:
-      # Currently chrome_os is only catched here.
-      return True
+      for supported_on in policy['supported_on']:
+        if self._IsVersionSupported(supported_on):
+          return True
+      return False
+
     for supported_on in policy['supported_on']:
       for supported_on_platform in supported_on['platforms']:
-        if supported_on_platform in self.platforms:
+        if (supported_on_platform in self.platforms and
+            self._IsVersionSupported(supported_on)):
           return True
     return False
 
@@ -111,10 +115,10 @@ class TemplateWriter(object):
       return False
 
     for supported_on in policy['supported_on']:
-      if platform in supported_on['platforms'] and \
-          (not product or product in supported_on['product']):
+      if (platform in supported_on['platforms'] and
+          (not product or product in supported_on['product']) and
+          self._IsVersionSupported(supported_on)):
         return True
-
     return False
 
   def IsCrOSManagementSupported(self, policy, management):
@@ -136,8 +140,25 @@ class TemplateWriter(object):
 
     Returns: The Chromium version string or None if it has not been set.'''
 
-    if 'version' in self.config:
-      return self.config['version']
+    return self.config.get('version', None)
+
+  def _GetChromiumMajorVersion(self):
+    ''' Returns the major version of Chromium if it exists
+    in config.
+    '''
+    return self.config.get('major_version', None)
+
+  def _IsVersionSupported(self, supported_on):
+    '''Checks whether the policy is supoorted on current version'''
+    major_version = self._GetChromiumMajorVersion()
+    if not major_version:
+      return True
+
+    since_version = supported_on.get('since_version', None)
+    until_version = supported_on.get('until_version', None)
+
+    return ((not since_version or int(since_version) <= major_version) and
+            (not until_version or int(until_version) >= major_version))
 
   def _GetPoliciesForWriter(self, group):
     '''Filters the list of policies in the passed group that are supported by
