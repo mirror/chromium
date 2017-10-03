200bd22e0459890154feace4e8a55be2d9409dff
Nektarios Paisios <nektar@chromium.org>
2017-10-03 05:26:06 -0400

    Adds a class that encapsulates a range in Blink's accessibility tree.
    This is just a basic class that can convert from a DOM selection to an AX range and vice versa. It can also perform a selection on the AX range.
    This class doesn't still handle any of the many corner cases that I will be looking at next, cases that will be covered by tests.
    Bug:
    Change-Id: I7c98617e8ef3b8bcca8cfc0d2e5518f2da771063
    R=dmazzoni@chromium.org

---
+++ third_party/WebKit/Source/modules/accessibility/AXRange.cpp
@@ -0,0 +1,115 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "modules/accessibility/AXRange.h"
+
+#include "core/dom/Document.h"
+#include "core/editing/FrameSelection.h"
+#include "core/frame/LocalFrame.h"
+#include "modules/accessibility/AXObject.h"
+
+namespace blink {
+
+AXRange::Builder& AXRange::Builder::SetBase(const AXPosition& base) {
+  DCHECK(base.IsValid());
+  range_.base_ = base;
+  return *this;
+}
+
+AXRange::Builder& AXRange::Builder::SetBase(const Position& base) {
+  const auto ax_base = AXPosition::FromPosition(base);
+  DCHECK(ax_base.IsValid());
+  range_.base_ = ax_base;
+  return *this;
+}
+
+AXRange::Builder& AXRange::Builder::SetExtent(const AXPosition& extent) {
+  DCHECK(extent.IsValid());
+  range_.extent_ = extent;
+  return *this;
+}
+
+AXRange::Builder& AXRange::Builder::SetExtent(const Position& extent) {
+  const auto ax_extent = AXPosition::FromPosition(extent);
+  DCHECK(ax_extent.IsValid());
+  range_.extent_ = ax_extent;
+  return *this;
+}
+
+AXRange::Builder& AXRange::Builder::SetRange(
+    const SelectionInDOMTree& selection) {
+  if (selection.IsNone())
+    return *this;
+
+  range_.base_ = AXPosition::FromPosition(selection.Base());
+  range_.extent_ = AXPosition::FromPosition(selection.Extent());
+  return *this;
+}
+
+const AXRange AXRange::Builder::Build() {
+  if (!range_.Base().IsValid() || !range_.Extent().IsValid())
+    return {};
+
+  const Document* document = range_.Base().ContainerObject()->GetDocument();
+  DCHECK(document);
+  DCHECK(document->NeedsLayoutTreeUpdate());
+#if DCHECK_IS_ON()
+  range_.dom_tree_version_ = document->DomTreeVersion();
+  range.style_version_ = document->StyleVersion();
+#endif
+  return range_;
+}
+
+AXRange::AXRange() : base_(), extent_() {
+#if DCHECK_IS_ON()
+  dom_tree_version_ = 0;
+  style_version_ = 0;
+#endif
+}
+
+bool AXRange::IsValid() const {
+  if (!base_.IsValid() || !extent_.IsValid())
+    return false;
+  // We don't support ranges that span across documents.
+  if (base_.ContainerObject()->GetDocument() !=
+      extent_.ContainerObject()->GetDocument())
+    return false;
+  DCHECK(!base_.ContainerObject()->GetDocument()->NeedsLayoutTreeUpdate());
+  DCHECK_EQ(base_.ContainerObject()->GetDocument()->DomTreeVersion(),
+            dom_tree_version_);
+  DCHECK_EQ(base_.ContainerObject()->GetDocument()->StyleVersion(),
+            style_version_);
+  return true;
+}
+
+const SelectionInDOMTree AXRange::AsSelection() const {
+  if (!IsValid())
+    return {};
+
+  const auto dom_base = base_.ToPositionWithAffinity();
+  const auto dom_extent = extent_.ToPositionWithAffinity();
+  SelectionInDOMTree::Builder selection_builder;
+  selection_builder.SetBaseAndExtent(dom_base.GetPosition(),
+                                     dom_extent.GetPosition());
+  selection_builder.SetAffinity(extent_.Affinity());
+  return selection_builder.Build();
+}
+
+void AXRange::Select() {
+  if (!IsValid())
+    return;
+
+  const SelectionInDOMTree selection = AsSelection();
+  DCHECK(selection.AssertValid());
+  Document* document = selection.Base().GetDocument();
+  if (!document)
+    return;
+  LocalFrame* frame = document->GetFrame();
+  if (!frame)
+    return;
+  FrameSelection& frame_selection = frame->Selection();
+  frame_selection.SetSelection(selection);
+}
+
+}  // namespace blink
---
+++ third_party/WebKit/Source/modules/accessibility/AXRange.h
@@ -0,0 +1,92 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef AXRange_h
+#define AXRange_h
+
+#include <base/logging.h>
+#include <base/optional.h>
+#include <stdint.h>
+
+#include "core/editing/SelectionTemplate.h"
+#include "core/editing/TextAffinity.h"
+#include "modules/accessibility/AXPosition.h"
+#include "platform/wtf/Allocator.h"
+
+namespace blink {
+
+class AXRange final {
+  DISALLOW_NEW_EXCEPT_PLACEMENT_NEW();
+
+ public:
+  class Builder;
+
+  AXRange(const AXRange&) = default;
+  AXRange& operator=(const AXRange&) = default;
+  ~AXRange() = default;
+
+  const AXPosition Base() const { return base_; }
+  const AXPosition Extent() const { return extent_; }
+
+  // The range is invalid if either the anchor or the focus position is invalid,
+  // or if the positions are in two separate documents.
+  bool IsValid() const;
+
+  // Determines if the range only refers to text offsets under the current
+  // object.
+  bool IsSimple() const {
+    return IsValid() && base_.ContainerObject() == extent_.ContainerObject();
+  }
+
+  const SelectionInDOMTree AsSelection() const;
+
+  // Tries to set the selection to this range.
+  void Select();
+
+ private:
+  AXRange();
+
+  // The |AXPosition| where the range starts.
+  const AXPosition base_;
+
+  // The |AXPosition| where the range ends.
+  const AXPosition extent_;
+
+#if DCHECK_IS_ON()
+  uint64_t dom_tree_version_;
+  uint64_t style_version_;
+#endif
+
+  friend class Builder;
+};
+
+class AXRange::Builder final {
+  STACK_ALLOCATED();
+
+ public:
+  Builder() = default;
+  ~Builder() = default;
+  Builder& SetBase(const AXPosition&);
+  Builder& SetBase(const Position&);
+  Builder& SetExtent(const AXPosition&);
+  Builder& SetExtent(const Position&);
+  Builder& SetRange(const SelectionInDOMTree&);
+  const AXRange Build();
+
+ private:
+  AXRange range_;
+};
+
+bool operator==(const AXRange& a, const AXRange& b) {
+  DCHECK(!a.IsValid() || !b.IsValid());
+  return a.Base() == b.Base() && a.Extent() == b.Extent();
+}
+
+bool operator!=(const AXRange& a, const AXRange& b) {
+  return !(a == b);
+}
+
+}  // namespace blink
+
+#endif  // AXRange_h
