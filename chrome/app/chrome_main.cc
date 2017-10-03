b8ae28f1fb72ad0eed96d58db833c4b1f286b087
Ilya Sherman <isherman@chromium.org>
2017-10-03 16:54:42 -0700

    [Sampling Profiler] Start sampling as early during initialization as possible.
    This will give us visibility into the performance of the earliest parts of
    startup as well as the later parts, which already have coverage.
    
    R=asvitkine@chromium.org
    
    Bug: 700083
    Change-Id: I2fd2e76c00ca46754a57574dc3713ee641494a66

--- chrome/app/chrome_main.cc
+++ chrome/app/chrome_main.cc
@@ -9,6 +9,7 @@
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "chrome/app/chrome_main_delegate.h"
+#include "chrome/app/profiler/stack_sampling_profiler_launcher.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/features.h"
 #include "content/public/app/content_main.h"
@@ -92,7 +93,11 @@ int ChromeMain(int argc, const char** argv) {
   const base::CommandLine* command_line(base::CommandLine::ForCurrentProcess());
   ALLOW_UNUSED_LOCAL(command_line);
 
-  // Chrome-specific process modes.
+  // Start the sampling profiler as early as possible – namely, once the command
+  // line data is available.
+  StartStackSamplingProfilerIfEnabled();
+
+// Chrome-specific process modes.
 #if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_WIN)
   if (command_line->HasSwitch(switches::kHeadless)) {
 #if defined(OS_MACOSX)
