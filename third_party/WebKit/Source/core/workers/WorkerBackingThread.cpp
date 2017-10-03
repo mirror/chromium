8fc3b3ceef8d8c4c78576445155b8715998c47b0
Hitoshi Yoshida <peria@chromium.org>
2017-10-03 19:20:23 +0900

    Make V8 workable without snapshot_blob.bin
    Bug:
    Change-Id: I853a6fe5b2d0eaed19232ba5c73da3d073ceefcb

--- third_party/WebKit/Source/core/workers/WorkerBackingThread.cpp
+++ third_party/WebKit/Source/core/workers/WorkerBackingThread.cpp
@@ -76,9 +76,8 @@ void WorkerBackingThread::InitializeOnBackingThread(
   backing_thread_->InitializeOnThread();
 
   DCHECK(!isolate_);
-  // Use nullptr for |external_reference_table|, since it's used for the context
-  // snapshot feature which workers don't use.
-  intptr_t* external_reference_table = nullptr;
+  const intptr_t* external_reference_table =
+      V8ContextSnapshot::GetReferenceTable();
   isolate_ = V8PerIsolateData::Initialize(
       backing_thread_->PlatformThread().GetWebTaskRunner(),
       external_reference_table,
