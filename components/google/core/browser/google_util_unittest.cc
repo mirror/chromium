d636af88cdd4a82bb91e9f07b696204690e3d4bd
Quentin Fiard <qfiard@google.com>
2017-10-03 19:28:49 +0200

    Gracefully handle non-standard Google base URLs in GetGoogleCountryCode
    Used in https://chrome-internal-review.googlesource.com/c/clank/internal/apps/+/349564
    
    Bug: 703497
    Change-Id: I9ae7dfd840a116e5d93b6d12ab5f1e6d478af17b

--- components/google/core/browser/google_util_unittest.cc
+++ components/google/core/browser/google_util_unittest.cc
@@ -452,3 +452,25 @@ TEST(GoogleUtilTest, YoutubeDomains) {
                                   google_util::DISALLOW_SUBDOMAIN,
                                   google_util::DISALLOW_NON_STANDARD_PORTS));
 }
+
+TEST(GoogleUtilTest, GetGoogleCountryCode) {
+  EXPECT_EQ("us",
+            google_util::GetGoogleCountryCode(GURL("http://www.google.com")));
+  EXPECT_EQ("fr",
+            google_util::GetGoogleCountryCode(GURL("http://www.google.fr")));
+
+// Extracting the country code from an invalid real Google URL is fatal in
+// debug builds.
+#if DEBUG
+  EXPECT_DEATH_IF_SUPPORTED(
+      google_util::GetGoogleCountryCode(GURL("http://localhost")), "");
+#else
+  EXPECT_EQ("us", google_util::GetGoogleCountryCode(GURL("http://localhost")));
+#endif
+
+  // Extracting the country code from an invalid flag-specified Google URL is
+  // always gracefully accepted.
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kGoogleBaseURL, "http://localhost");
+  EXPECT_EQ("us", google_util::GetGoogleCountryCode(GURL("http://localhost")));
+}
