107c42c1d61d14c4155be274bdacb0f6653bc9cf
Tom Sepez <tsepez@chromium.org>
2017-10-03 13:54:53 -0700

    Make windows sandbox_init_win take sandbox_type parameter.
    This is another step towards normalizing (somewhat) the APIs
    across the various platforms.
    
    Change-Id: I86279c2f5c26c9c1bd0b7586b7ac882c3667f6e5

--- content/app/sandbox_helper_win.cc
+++ content/app/sandbox_helper_win.cc
@@ -13,16 +13,15 @@ void InitializeSandboxInfo(sandbox::SandboxInterfaceInfo* info) {
   info->broker_services = sandbox::SandboxFactory::GetBrokerServices();
   if (!info->broker_services) {
     info->target_services = sandbox::SandboxFactory::GetTargetServices();
-  } else {
-    // Ensure the proper mitigations are enforced for the browser process.
-    sandbox::ApplyProcessMitigationsToCurrentProcess(
-        sandbox::MITIGATION_DEP |
-        sandbox::MITIGATION_DEP_NO_ATL_THUNK |
-        sandbox::MITIGATION_HARDEN_TOKEN_IL_POLICY);
-    // Note: these mitigations are "post-startup".  Some mitigations that need
-    // to be enabled sooner (e.g. MITIGATION_EXTENSION_POINT_DISABLE) are done
-    // so in Chrome_ELF.
+    return;
   }
+  // Ensure the proper mitigations are enforced for the browser process.
+  // Note: these mitigations are "post-startup".  Some mitigations that need
+  // to be enabled sooner (e.g. MITIGATION_EXTENSION_POINT_DISABLE) are done
+  // so in Chrome_ELF.
+  sandbox::ApplyProcessMitigationsToCurrentProcess(
+      sandbox::MITIGATION_DEP | sandbox::MITIGATION_DEP_NO_ATL_THUNK |
+      sandbox::MITIGATION_HARDEN_TOKEN_IL_POLICY);
 }
 
 }  // namespace content
