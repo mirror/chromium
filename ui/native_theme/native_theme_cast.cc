c365c223a97185ff91de1ed60840ce333915b7c4
Trent Apted <tapted@chromium.org>
2017-10-03 15:08:54 +1100

    NativeThemeCast
    cl format
    
    
    NativeThemeCast?
    
    Bug:
    Change-Id: Ib74f4f7c54505aef6107e7e1b54649ae4baf5a64

---
+++ ui/native_theme/native_theme_cast.cc
@@ -0,0 +1,41 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/gfx/color_palette.h"
+#include "ui/native_theme/native_theme_aura.h"
+
+namespace ui {
+namespace {
+
+class NativeThemeCast : public NativeThemeAura {
+ public:
+  NativeThemeCast() : NativeThemeAura(false /* use_overlay_scrollbars */) {}
+
+  SkColor GetSystemColor(ColorId color_id) const override {
+    // Colors that Cast uses as part of its native UI.
+    switch (color_id) {
+      // Also used for progress bars.
+      case ui::NativeTheme::kColorId_ProminentButtonColor:
+        return gfx::kGoogleBlue500;
+      case ui::NativeTheme::kColorId_ProminentButtonColor:
+        return gfx::kGoogleRed700;
+      default:
+        break;
+    }
+
+    return NativeThemeAura::GetSystemColor(color_id);
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NativeThemeCast);
+};
+
+}  // namespace
+
+NativeTheme* NativeTheme::GetInstanceForNativeUi() {
+  CR_DEFINE_STATIC_LOCAL(NativeThemeCast, instance, ());
+  return &instance;
+}
+
+}  // namespace ui
