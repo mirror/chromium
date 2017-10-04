5598b2f81ffc63e3cc97ac6750d11bead99bb55d
Mitsuru Oshima <oshima@chromium.org>
2017-10-04 08:34:52 -0700

    Add --force-remote-shell-scale to specify the remote-shell client's scale
    The VM was using ARC_UI_SCALE, ARC_LCD_DENSITY which has been
    deprecated, so it needs a way to specify the density used by on ARC++.
    
    Bug: b/66963637
    Test: --remote-shell-scale=2.0 on 1.0x device shoud use 1.5 dsf on Android
    and the size is about the same ratio of 2x device like samus.
    
    Change-Id: I4d73583d3fbb5957110ab6ed97c5cbbe7bfb92b7

--- components/exo/wayland/server.cc
+++ components/exo/wayland/server.cc
@@ -42,12 +42,14 @@
 #include "ash/shell.h"
 #include "base/bind.h"
 #include "base/cancelable_callback.h"
+#include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/macros.h"
 #include "base/memory/free_deleter.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/weak_ptr.h"
 #include "base/message_loop/message_loop.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/threading/thread.h"
@@ -148,6 +150,21 @@ void SetImplementation(wl_resource* resource,
                        std::unique_ptr<T> user_data) {
   wl_resource_set_implementation(resource, implementation, user_data.release(),
                                  DestroyUserData<T>);
+}
+
+// Returns a scale factor to be uesd by a remote shell client.
+float GetDefaultDeviceScaleFactor() {
+  // A flag used by VM to emulate a device scale for a partiular board.
+  constexpr char kRemoteShellScaleSwitch[] = "force-remote-shell-scale";
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(kRemoteShellScaleSwitch)) {
+    std::string value =
+        command_line->GetSwitchValueASCII(kRemoteShellScaleSwitch);
+    double scale = 1.0;
+    if (base::StringToDouble(value, &scale))
+      return scale;
+  }
+  return WMHelper::GetInstance()->GetDefaultDeviceScaleFactor();
 }
 
 // Convert a timestamp to a time value that can be used when interfacing
@@ -2162,8 +2179,7 @@ class WaylandRemoteShell : public WMHelper::TabletModeObserver,
                        : ZCR_REMOTE_SHELL_V1_LAYOUT_MODE_WINDOWED;
 
     if (wl_resource_get_version(remote_shell_resource_) >= 8) {
-      float scale_factor =
-          WMHelper::GetInstance()->GetDefaultDeviceScaleFactor();
+      float scale_factor = GetDefaultDeviceScaleFactor();
       // Send using 16.16 fixed point.
       const int kDecimalBits = 24;
       int32_t fixed_scale =
@@ -2189,9 +2205,9 @@ class WaylandRemoteShell : public WMHelper::TabletModeObserver,
   std::unique_ptr<ShellSurface> CreateShellSurface(
       Surface* surface,
       int container,
-      bool scale_by_default_device_scale_factor) {
-    return display_->CreateRemoteShellSurface(
-        surface, container, scale_by_default_device_scale_factor);
+      float default_device_scale_factor) {
+    return display_->CreateRemoteShellSurface(surface, container,
+                                              default_device_scale_factor);
   }
 
   std::unique_ptr<NotificationSurface> CreateNotificationSurface(
@@ -2436,11 +2452,13 @@ void remote_shell_get_remote_surface(wl_client* client,
                                      wl_resource* surface,
                                      uint32_t container) {
   WaylandRemoteShell* shell = GetUserDataAs<WaylandRemoteShell>(resource);
-  bool scale_by_default_scale_factor = wl_resource_get_version(resource) >= 8;
+  float default_scale_factor = wl_resource_get_version(resource) >= 8
+                                   ? GetDefaultDeviceScaleFactor()
+                                   : 1.0f;
 
   std::unique_ptr<ShellSurface> shell_surface = shell->CreateShellSurface(
       GetUserDataAs<Surface>(surface), RemoteSurfaceContainer(container),
-      scale_by_default_scale_factor);
+      default_scale_factor);
   if (!shell_surface) {
     wl_resource_post_error(resource, ZCR_REMOTE_SHELL_V1_ERROR_ROLE,
                            "surface has already been assigned a role");
