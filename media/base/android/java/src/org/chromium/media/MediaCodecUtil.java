b4dc9607f307d57ddf5dd6b98d4168e62d54db8d
Sandeep Vijayasekar <sandv@chromium.org>
2017-10-04 11:28:32 -0700

    Pass MediaCrypto to createDecoder() for secure audio decoder
    BUG=727918
    BUG=Internal b/63354167
    TEST=Audio works with secure audio decoder
    
    Change-Id: I32d8a88db8ca74b0f3dbc1a8aae7b42c5e9c60a3

--- media/base/android/java/src/org/chromium/media/MediaCodecUtil.java
+++ media/base/android/java/src/org/chromium/media/MediaCodecUtil.java
@@ -12,6 +12,7 @@ import android.media.MediaCodecInfo.CodecCapabilities;
 import android.media.MediaCodecInfo.CodecProfileLevel;
 import android.media.MediaCodecInfo.VideoCapabilities;
 import android.media.MediaCodecList;
+import android.media.MediaCrypto;
 import android.media.MediaFormat;
 import android.os.Build;
 
@@ -279,8 +280,20 @@ class MediaCodecUtil {
      * @return CodecCreationInfo object
      */
     static CodecCreationInfo createDecoder(String mime, int codecType) {
+        return createDecoder(mime,codecType,null);
+    }
+
+    /**
+     * Creates MediaCodec decoder.
+     * @param mime MIME type of the media.
+     * @param codecType Type of codec to create.
+     * @param mediaCrypto Crypto of the media.
+     * @return CodecCreationInfo object
+     */
+    static CodecCreationInfo createDecoder(String mime, int codecType, MediaCrypto mediaCrypto) {
         // Always return a valid CodecCreationInfo, its |mediaCodec| field will be null
         // if we cannot create the codec.
+
         CodecCreationInfo result = new CodecCreationInfo();
 
         assert result.mediaCodec == null;
@@ -319,6 +332,17 @@ class MediaCodecUtil {
                             codecSupportsAdaptivePlayback(insecureCodec, mime);
                     insecureCodec.release();
                 }
+
+                result.mediaCodec = MediaCodec.createByCodecName(decoderName + ".secure");
+
+                // Special check for secure audio decoder crbug.com/727918
+            } else if (mime.startsWith("audio") && mediaCrypto != null
+                    && mediaCrypto.requiresSecureDecoderComponent(mime)) {
+                String decoderName = getDefaultCodecName(mime, MediaCodecDirection.DECODER, false);
+                MediaCodec insecureCodec = MediaCodec.createByCodecName(decoderName);
+                result.supportsAdaptivePlayback =
+                        codecSupportsAdaptivePlayback(insecureCodec, mime);
+                insecureCodec.release();
                 result.mediaCodec = MediaCodec.createByCodecName(decoderName + ".secure");
             } else {
                 if (codecType == CodecType.SOFTWARE) {
