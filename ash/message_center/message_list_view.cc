83e987d517416deaef39fd239ed34cad074d2a56
yoshiki iguchi <yoshiki@chromium.org>
2017-10-05 03:13:30 +0900

    Not remove views in OnBoundsAnimatorDone after clearing all
    This patch changes the deletion timing of views on clearing all notification.
    
    Previously they are removed in OnBoundsAnimatorDone().　This patch makes it not removing views, because it is the cause of the crash (see crbug.com/762803). Even with this change, the views are moved in the RemoveNotification method eventually.
    
    This patch also skips setting reposition target during clearing all.
    
    Bug: 692527
    Change-Id: Ibf55195e109d6fbe96d01717ddd774a77de5e2b4

--- ash/message_center/message_list_view.cc
+++ ash/message_center/message_list_view.cc
@@ -340,14 +340,12 @@ void MessageListView::OnBoundsAnimatorDone(views::BoundsAnimator* animator) {
 
   if (clear_all_started_) {
     clear_all_started_ = false;
-    // TODO(yoshiki): we shouldn't touch views in OnAllNotificationsCleared().
-    // Or rename it to like OnAllNotificationsClearing().
     for (auto& observer : observers_)
       observer.OnAllNotificationsCleared();
 
-    // Need to update layout after deleting the views.
-    if (!deleted_when_done_.empty())
-      need_update = true;
+    // Just return here if new animation is initiated in the above observers.
+    if (animator_.IsAnimating())
+      return;
   }
 
   // None of these views should be deleted.
@@ -520,7 +518,11 @@ bool MessageListView::AnimateChild(views::View* child,
                                    int height,
                                    bool animate_on_move) {
   gfx::Rect child_area = GetContentsBounds();
-  if (adding_views_.find(child) != adding_views_.end()) {
+  if (base::ContainsValue(clearing_all_views_, child)) {
+    // This view is waiting for animation by clear-all. Do not initiate
+    // animateion here.
+    return false;
+  } else if (adding_views_.find(child) != adding_views_.end()) {
     child->SetBounds(child_area.right(), top, child_area.width(), height);
     animator_.AnimateViewTo(
         child, gfx::Rect(child_area.x(), top, child_area.width(), height));
@@ -552,9 +554,6 @@ void MessageListView::AnimateClearingOneNotification() {
   gfx::Rect new_bounds = child->bounds();
   new_bounds.set_x(new_bounds.right() + kMarginBetweenItems);
   animator_.AnimateViewTo(child, new_bounds);
-
-  // Deleting the child after animation.
-  deleted_when_done_.insert(child);
 
   // Schedule to start sliding out next notification after a short delay.
   if (!clearing_all_views_.empty()) {
--- ash/message_center/message_list_view_unittest.cc
+++ ash/message_center/message_list_view_unittest.cc
@@ -164,12 +164,18 @@ class MessageListViewTest : public AshTestBase,
     }
   }
 
+  bool is_on_all_notifications_cleared_called() const {
+    return is_on_all_notifications_cleared_called_;
+  }
+
  private:
   // MockNotificationView::Test override
   void RegisterCall(CallType type) override {}
 
   // MessageListView::Observer override
-  void OnAllNotificationsCleared() override {}
+  void OnAllNotificationsCleared() override {
+    is_on_all_notifications_cleared_called_ = true;
+  }
 
   // MessageCenterController override:
   void ClickOnNotification(const std::string& notification_id) override {}
@@ -193,6 +199,8 @@ class MessageListViewTest : public AshTestBase,
   std::unique_ptr<views::Widget> widget_;
   // MessageListView to be tested.
   std::unique_ptr<MessageListView> message_list_view_;
+
+  bool is_on_all_notifications_cleared_called_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(MessageListViewTest);
 };
@@ -453,7 +461,7 @@ TEST_F(MessageListViewTest, ClearAllClosableNotifications) {
 
   RunPendingAnimations();
 
-  EXPECT_EQ(0, message_list_view()->child_count());
+  EXPECT_TRUE(is_on_all_notifications_cleared_called());
 }
 
 // Regression test for crbug.com/713983
@@ -475,13 +483,9 @@ TEST_F(MessageListViewTest, RemoveWhileClearAll) {
                    message_center::RichNotificationData(), nullptr));
 
   message_list_view()->AddNotificationAt(notification_view1, 0);
-  EXPECT_EQ(1, message_list_view()->child_count());
-
   RunPendingAnimations();
 
   message_list_view()->AddNotificationAt(notification_view2, 1);
-  EXPECT_EQ(2, message_list_view()->child_count());
-
   RunPendingAnimations();
 
   // Call RemoveNotification()
@@ -494,7 +498,7 @@ TEST_F(MessageListViewTest, RemoveWhileClearAll) {
       message_list_view()->bounds());
 
   RunPendingAnimations();
-  EXPECT_EQ(0, message_list_view()->child_count());
+  EXPECT_TRUE(is_on_all_notifications_cleared_called());
 }
 
 }  // namespace ash
