9cd6f220a8949cb7d5cba6dc222221007dbe154c
Dave Schuyler <dschuyler@chromium.org>
2017-10-03 18:12:24 -0700

    [MD settings] cookies, clear filter after removing items
    This CL adds a listener to the settings subpage that will allow setting
    the filter value. The filter may be cleared by setting the value to ''.
    
    Bug: 770423
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If4ac8e4bb1f1b38224b52e970f549293ff9b5e9d

--- chrome/test/data/webui/settings/settings_subpage_test.js
+++ chrome/test/data/webui/settings/settings_subpage_test.js
@@ -4,9 +4,26 @@
 
 cr.define('settings_subpage', function() {
   suite('SettingsSubpage', function() {
-    test('navigates to parent when there is no history', function() {
+    setup(function() {
       PolymerTest.clearBody();
+    });
 
+    test('clear search', function() {
+      var subpage = document.createElement('settings-subpage');
+      // Having a searchLabel will create the settings-subpage-search.
+      subpage.searchLabel = 'test';
+      document.body.appendChild(subpage);
+      Polymer.dom.flush();
+      var searchElement =
+          subpage.$$('settings-subpage-search').$$('#searchInput');
+      assertTrue(!!searchElement);
+      searchElement.value = 'Hello';
+      subpage.fire('clear-subpage-search');
+      Polymer.dom.flush();
+      assertEquals('', searchElement.value);
+    });
+
+    test('navigates to parent when there is no history', function() {
       // Pretend that we initially started on the CERTIFICATES route.
       window.history.replaceState(
           undefined, '', settings.routes.CERTIFICATES.path);
@@ -21,8 +38,6 @@ cr.define('settings_subpage', function() {
     });
 
     test('navigates to any route via window.back()', function(done) {
-      PolymerTest.clearBody();
-
       settings.navigateTo(settings.routes.BASIC);
       settings.navigateTo(settings.routes.SYNC);
       assertEquals(settings.routes.SYNC, settings.getCurrentRoute());
