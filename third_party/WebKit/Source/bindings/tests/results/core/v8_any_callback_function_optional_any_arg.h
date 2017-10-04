58adc16fe62ff553a9fad7847e3e97492cb3dcbd
Sayaka Takayama <stakayama@google.com>
2017-10-04 10:49:04 +0900

    v8binding: added CallbackFunctionBase: base class for callback functions
    Callback function classes generated by callback_function.h.tmpl have
    some common objects and each of classes don't have to have them.
    CallbackFunctionBase is a base class for callback function classes and
    has objects which used to be common for them.
    
    Bug:658073
    Change-Id: I7a71cf9813b53edbd7b7141ca647508d88c85510

--- third_party/WebKit/Source/bindings/tests/results/core/v8_any_callback_function_optional_any_arg.h
+++ third_party/WebKit/Source/bindings/tests/results/core/v8_any_callback_function_optional_any_arg.h
@@ -19,31 +19,24 @@
 #include "platform/bindings/TraceWrapperV8Reference.h"
 #include "platform/heap/Handle.h"
 #include "platform/wtf/text/WTFString.h"
+#include "platform/bindings/CallbackFunctionBase.h"
 
 namespace blink {
 
 class ScriptState;
 
-class CORE_EXPORT V8AnyCallbackFunctionOptionalAnyArg final : public GarbageCollectedFinalized<V8AnyCallbackFunctionOptionalAnyArg>, public TraceWrapperBase {
+class CORE_EXPORT V8AnyCallbackFunctionOptionalAnyArg final : public CallbackFunctionBase {
  public:
   static V8AnyCallbackFunctionOptionalAnyArg* Create(ScriptState*, v8::Local<v8::Value> callback);
 
   ~V8AnyCallbackFunctionOptionalAnyArg() = default;
 
-  DEFINE_INLINE_TRACE() {}
   DECLARE_TRACE_WRAPPERS();
 
   bool call(ScriptWrappable* scriptWrappable, ScriptValue optionalAnyArg, ScriptValue& returnValue);
 
-  v8::Local<v8::Function> v8Value(v8::Isolate* isolate) {
-    return callback_.NewLocal(isolate);
-  }
-
  private:
   V8AnyCallbackFunctionOptionalAnyArg(ScriptState*, v8::Local<v8::Function>);
-
-  RefPtr<ScriptState> script_state_;
-  TraceWrapperV8Reference<v8::Function> callback_;
 };
 
 template <>
--- third_party/WebKit/Source/bindings/tests/results/core/v8_void_callback_function_dictionary_arg.cc
+++ third_party/WebKit/Source/bindings/tests/results/core/v8_void_callback_function_dictionary_arg.cc
@@ -31,13 +31,10 @@ V8VoidCallbackFunctionDictionaryArg* V8VoidCallbackFunctionDictionaryArg::Create
 }
 
 V8VoidCallbackFunctionDictionaryArg::V8VoidCallbackFunctionDictionaryArg(ScriptState* scriptState, v8::Local<v8::Function> callback)
-    : script_state_(scriptState),
-    callback_(scriptState->GetIsolate(), this, callback) {
-  DCHECK(!callback_.IsEmpty());
-}
+    : CallbackFunctionBase(scriptState, callback) {}
 
 DEFINE_TRACE_WRAPPERS(V8VoidCallbackFunctionDictionaryArg) {
-  visitor->TraceWrappers(callback_.Cast<v8::Value>());
+  CallbackFunctionBase::TraceWrappers(visitor);
 }
 
 bool V8VoidCallbackFunctionDictionaryArg::call(ScriptWrappable* scriptWrappable, const TestDictionary& arg) {
