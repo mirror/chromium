c81722bf2412b96f81d281f85d152834b8ae576d
Peiyong Lin <lpy@chromium.org>
2017-10-04 13:26:32 -0700

    Merge network idleness signal with Performance Monitor.
    Previously we have a dedicated detector to detect network idleness signal and
    send it to services/resource_coordinator, however, it uses a timer, thus it
    can't handle cases like, if a task is being processed for a long time, network
    requests won't be able to be fired, thus when the task is finished processing
    the network idleness signal will be fired before network requests are fired,
    which results in inaccurate network idleness signal.
    
    So, in this patch, we excluded task processing time in network idle time
    accumulation, and merged Performance Monitor life cycle report into the current
    detector.
    
    Minor: renamed kNetworkIdle to kNetworkAlmostIdle, renamed class/file name to
    IdlenessDetector.
    
    BUG=730098, 749789
    
    Change-Id: Ied003129cd5ddf7c1ebb93019723e11e9a31be04

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -187,6 +187,7 @@
 #include "core/loader/DocumentLoader.h"
 #include "core/loader/FrameFetchContext.h"
 #include "core/loader/FrameLoader.h"
+#include "core/loader/IdlenessDetector.h"
 #include "core/loader/NavigationScheduler.h"
 #include "core/loader/PrerendererClient.h"
 #include "core/loader/TextResourceDecoderBuilder.h"
@@ -5797,6 +5798,7 @@ void Document::FinishedParsing() {
                          TRACE_EVENT_SCOPE_THREAD, "data",
                          InspectorMarkLoadEvent::Data(frame));
     probe::domContentLoadedEventFired(frame);
+    frame->GetIdlenessDetector()->DomContentLoadedEventFired();
   }
 
   // Schedule dropping of the ElementDataCache. We keep it alive for a while
