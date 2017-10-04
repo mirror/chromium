b42363c961313a5e486c4936d777005aed593df6
Daniel Park <danielpark@chromium.org>
2017-10-03 17:48:54 -0700

    [Chrome Home Survey] Creates & hooks up survey info bar
    Shows an invitation to take a survey if Chrome Home was enabled
       for longer than 1 week.
    Creates a generic survey controller for future surveys.
    Creates a Chrome Home specific controller.
    
    Bug:754499
    Change-Id: I53064f181fcd8832a5524eb4d7d38841531c84e7

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -97,6 +97,7 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.signin.SigninPromoUtil;
 import org.chromium.chrome.browser.snackbar.undo.UndoBarController;
 import org.chromium.chrome.browser.suggestions.SuggestionsEventReporterBridge;
+import org.chromium.chrome.browser.survey.ChromeHomeSurveyController;
 import org.chromium.chrome.browser.tab.BrowserControlsVisibilityDelegate;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabDelegateFactory;
@@ -513,6 +514,8 @@ public class ChromeTabbedActivity
             } else {
                 preferenceManager.setPromosSkippedOnFirstStart(true);
             }
+
+            ChromeHomeSurveyController.initialize(this, mTabModelSelectorImpl);
 
             super.finishNativeInitialization();
         } finally {
