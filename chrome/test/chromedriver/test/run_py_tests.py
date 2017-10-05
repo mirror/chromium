62930192472a7271d309df95b896eb8a3a9771c8
John Chen <johnchen@chromium.org>
2017-10-05 15:52:58 -0700

    [ChromeDriver] Fix test case testDeviceName
    Fix test case MobileEmulationCapabilityTest.testDeviceName in Python
    ChromeDriver tests, which was broken by https://crrev.com/c/689966.
    
    Bug: chromedriver:2062
    Change-Id: I974852fcd5cd9308bada6c1614cf33d40d343f00

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -2104,7 +2104,7 @@ class MobileEmulationCapabilityTest(ChromeDriverBaseTest):
     body_tag = driver.FindElement('tag name', 'body')
     self.assertEqual(
         'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) '
-        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Mobile '
+        'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3227.0 Mobile '
         'Safari/537.36',
         body_tag.GetText())
 
