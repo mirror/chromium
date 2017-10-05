5ffc13eb718528a5559223ae89c82aa585b74175
Renjie Liu <renjieliu@chromium.org>
2017-10-06 10:43:49 +1100

    Init implementation for automatically generate cpp code.
    1) Read s2 cellid with lang code data from adminregionsdata.csv;
    2) Genearte cpp code to look up language code.
    3) Since S2 library is not ready yet, so the lookup logic is not done.
    
    The generated code will live under <OUT_DIR>/gen/components/language/util
    
    I will add a unit test later.
    
    Bug:
    Change-Id: I198488adce31def9741663adf57d59df45a45159

---
+++ components/language/content/util/language_code_locator.h
@@ -0,0 +1,22 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace language {
+
+class LanguageCodeLocator {
+ public:
+  LanguageCodeLocator();
+
+  // Find the language code given a coordinate.
+  std::vector<std::string> GetLanguageCode(double lat, double lng) const;
+
+ private:
+  std::unordered_map<string, string> district_lang_;
+};
+
+}  // namespace language
