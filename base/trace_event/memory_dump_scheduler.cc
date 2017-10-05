3a01b9ebda0aacd5dfc5bcf5c9b109e00c167ab5
Lalit Maganti <lalitm@chromium.org>
2017-10-05 20:23:19 +0100

    memory-infra: delay first periodic dump by 200 ms
    This is a temporary hack until a more correct solution is implemented.
    This is so that the upcoming stable does not have the bug specified.
    
    Bug: 770151
    Change-Id: I7e4f5c7ad5c41620e3ed667af059498aaa9ad041

--- base/trace_event/memory_dump_scheduler.cc
+++ base/trace_event/memory_dump_scheduler.cc
@@ -74,9 +74,12 @@ void MemoryDumpScheduler::StartInternal(MemoryDumpScheduler::Config config) {
   heavy_dump_rate_ = heavy_dump_period_ms / min_period_ms;
 
   // Trigger the first dump immediately.
-  SequencedTaskRunnerHandle::Get()->PostTask(
+  // TODO(lalitm): this is a tempoarary hack to delay the first scheduled dump
+  // so that tracing messages can propogate. See crbug.com/770151.
+  SequencedTaskRunnerHandle::Get()->PostDelayedTask(
       FROM_HERE,
-      BindOnce(&MemoryDumpScheduler::Tick, Unretained(this), ++generation_));
+      BindOnce(&MemoryDumpScheduler::Tick, Unretained(this), ++generation_),
+      TimeDelta::FromMilliseconds(200));
 }
 
 void MemoryDumpScheduler::StopInternal() {
