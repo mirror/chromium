303d2f6c34f25e0358114db120ebc77020eff97b
Bence Béky <bnc@chromium.org>
2017-10-05 10:09:29 -0400

    Avoid WrapUnique(new) in //net.
    I structured this cleanup CL into multiple patch sets hoping that that
    would make it easier to review.  (They are kinda too small for individual
    CLs.)
    
    There is no bug for most of this CL, except for the Patch set 5 part of it.
    
    Patch set 1:
    Except for befriended factory methods that need to call new directly,
    and except for shared code, use make_unique instead of WrapUnique:
    * base::WrapUnique(new T) => std::make_unique<T>()
    * base::WrapUnique(new T()) => std::make_unique<T>()
    * base::WrapUnique(new T(args...)) => std::make_unique<T>(args...)
    because WrapUnique(new) is just silly, see
    https://groups.google.com/a/chromium.org/forum/#!topic/chromium-dev/iQgMedVA8-k.
    
    Patch set 2:
    Change NULL to nullptr in constructor arguments passed to make_unique at
    two places, because otherwise the compiler refuses to compile on the
    basis that it cannot convert long to T*.  (Funny that new does not have
    this issue.)
    
    Patch set 3:
    Change one WrapUnique(uniqueptr.release()) pattern to std::move().
    
    (Patch set 4: edit commit message.)
    
    Patch set 5:
    Change base::MakeUnique to std::make_unique at six occurrences, see
    https://crrev.com/c/616016.
    
    Patch set 6:
    Include "ptr_util.h" (for WrapUnique) and <memory> (for make_unique)
    exactly where necessary.
    
    (Patch set 7: edit commit message.)
    
    Patch set 8:
    Revert WrapUnique(new) to make_unique change in one more friend class
    because of private constructor.
    
    (Patch set 9: edit commit message.)
    
    Change-Id: I77e1f1b2cc0889090e486bf592f0d95f76d543bf
    
    Bug: 755727
    Change-Id: I77e1f1b2cc0889090e486bf592f0d95f76d543bf

--- net/disk_cache/entry_unittest.cc
+++ net/disk_cache/entry_unittest.cc
@@ -4441,8 +4441,8 @@ TEST_F(DiskCacheEntryTest, SimpleCacheCreateCollision) {
 class DiskCacheSimplePrefetchTest : public DiskCacheEntryTest {
  public:
   DiskCacheSimplePrefetchTest()
-      : field_trial_list_(base::MakeUnique<base::FieldTrialList>(
-            base::MakeUnique<base::MockEntropyProvider>())) {}
+      : field_trial_list_(std::make_unique<base::FieldTrialList>(
+            std::make_unique<base::MockEntropyProvider>())) {}
 
   enum { kEntrySize = 1024 };
 
