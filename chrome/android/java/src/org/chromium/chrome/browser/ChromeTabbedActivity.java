f3ffcd09eb779f2aec0891b43fd3173109915689
Matthew Jones <mdjones@chromium.org>
2017-10-05 08:22:50 -0700

    [Home] Convert promo flag to show once on cold-start
    Previously the Chrome Home promo flag forced the promo to show each
    time Chrome started. This change alters the meaning of that flag to
    show it once on cold start if Chrome Home is disabled. Once the promo
    has been shown and a decision (enable/disable) has been made, the
    promo will not be shown again on startup unless data is cleared.
    
    BUG=767738
    
    Change-Id: I926ab31284d3dc87b689bc3d53f6e69275279045

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -485,11 +485,8 @@ public class ChromeTabbedActivity
                 if (!SigninPromoUtil.launchSigninPromoIfNeeded(this)) {
                     if (!DataReductionPromoScreen.launchDataReductionPromo(
                                 this, currentModel.isIncognito())) {
-                        // TODO(mdjones): Refine this triggering logic when promo is complete:
-                        // crbug.com/767738
-                        if (ChromeFeatureList.isEnabled(ChromeFeatureList.CHROME_HOME_PROMO)) {
-                            ChromeHomePromoDialog chDialog = new ChromeHomePromoDialog(this);
-                            chDialog.show();
+                        if (FeatureUtilities.shouldShowChromeHomePromoForStartup()) {
+                            new ChromeHomePromoDialog(this).show();
                         } else if (getBottomSheet() != null) {
                             getBottomSheet().showHelpBubbleIfNecessary();
                         }
@@ -1540,9 +1537,7 @@ public class ChromeTabbedActivity
                         if (getBottomSheet() != null
                                 && ChromeFeatureList.isEnabled(
                                            ChromeFeatureList.CHROME_HOME_PROMO)) {
-                            ChromeHomePromoDialog chDialog =
-                                    new ChromeHomePromoDialog(ChromeTabbedActivity.this);
-                            chDialog.show();
+                            new ChromeHomePromoDialog(ChromeTabbedActivity.this).show();
                             return;
                         }
 
