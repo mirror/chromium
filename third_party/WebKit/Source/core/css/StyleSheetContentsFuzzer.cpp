3f46f5d2e2de5e5308376fc48b0d3b7ff9590454
Naina Raisinghani <nainar@chromium.org>
2017-10-05 13:57:16 +1100

    Change styleSheet_contents_fuzzer to fuzz in HTMLQuirksMode too
    This should increase coverage by allowing the fuzz to produce test
    cases in quirks mode too. HTMLQuirksMode has it's own issues in our
    code base and getting fuzzer to run these tests would be great.
    
    Change-Id: I0ea188d480d7bc605e0ddf62b665ba88ffde9b45

--- third_party/WebKit/Source/core/css/StyleSheetContentsFuzzer.cpp
+++ third_party/WebKit/Source/core/css/StyleSheetContentsFuzzer.cpp
@@ -5,18 +5,25 @@
 #include "core/css/StyleSheetContents.h"
 
 #include "platform/testing/BlinkFuzzerTestSupport.h"
+#include "platform/testing/FuzzedDataProvider.h"
 #include "platform/wtf/text/WTFString.h"
+
+using namespace blink;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   static blink::BlinkFuzzerTestSupport test_support =
       blink::BlinkFuzzerTestSupport();
-  blink::CSSParserContext* context =
-      blink::CSSParserContext::Create(blink::kHTMLStandardMode);
+
+  FuzzedDataProvider provider(data, size);
+  bool is_strict_mode = provider.ConsumeBool();
+  blink::CSSParserContext* context = blink::CSSParserContext::Create(
+      is_strict_mode ? blink::kHTMLStandardMode : blink::kHTMLQuirksMode);
   blink::StyleSheetContents* styleSheet =
       blink::StyleSheetContents::Create(context);
 
   styleSheet->ParseString(String::FromUTF8WithLatin1Fallback(
-      reinterpret_cast<const char*>(data), size));
+      reinterpret_cast<const char*>(provider.ConsumeRemainingBytes().data()),
+      size));
 
 #if defined(ADDRESS_SANITIZER)
   // LSAN needs unreachable objects to be released to avoid reporting them
