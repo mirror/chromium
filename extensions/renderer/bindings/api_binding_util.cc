f3c1bf033cc9ca445f74e2316ffd9e65178c79eb
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-10-05 12:30:01 -0700

    stronger context invalidation
    Bug:
    Change-Id: Ie835ec2bebc24f9767bb645015bb6f07c114d227

--- extensions/renderer/bindings/api_binding_util.cc
+++ extensions/renderer/bindings/api_binding_util.cc
@@ -5,10 +5,40 @@
 #include "extensions/renderer/bindings/api_binding_util.h"
 
 #include "base/logging.h"
+#include "base/supports_user_data.h"
 #include "build/build_config.h"
+#include "gin/per_context_data.h"
 
 namespace extensions {
 namespace binding {
+
+namespace {
+
+const char kInvalidatedContextFlagKey[] = "extension_invalidated_context";
+// An empty struct that just serves as a flag for invalidated contexts. If the
+// context has an instance of this struct, it has been invalidated.
+struct InvalidatedContextFlag : public base::SupportsUserData::Data {
+  // This space intentionally left blank.
+};
+
+}  // namespace
+
+bool IsContextValid(v8::Local<v8::Context> context) {
+  gin::PerContextData* per_context_data = gin::PerContextData::From(context);
+  if (!per_context_data)
+    return false;
+  auto* data = static_cast<InvalidatedContextFlag*>(
+      per_context_data->GetUserData(kInvalidatedContextFlagKey));
+  return !data;
+}
+
+void InvalidateContext(v8::Local<v8::Context> context) {
+  gin::PerContextData* per_context_data = gin::PerContextData::From(context);
+  if (!per_context_data)
+    return;  // Context is already invalidated at a higher level.
+  per_context_data->SetUserData(kInvalidatedContextFlagKey,
+                                std::make_unique<InvalidatedContextFlag>());
+}
 
 std::string GetPlatformString() {
 #if defined(OS_CHROMEOS)
