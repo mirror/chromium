8b296a33ab8beb314111774a365c24de558650ad
Olga Sharonova <olka@chromium.org>
2017-10-05 14:14:15 +0200

    UMA for stream format used for AudioOutputStreamProxy creation
    Bug:771157
    Change-Id: I02047d77bdc3a3918493dafe6158e594a9581e08

--- media/audio/audio_manager_base.cc
+++ media/audio/audio_manager_base.cc
@@ -9,6 +9,7 @@
 #include "base/command_line.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/threading/thread_task_runner_handle.h"
@@ -43,6 +44,19 @@ std::unique_ptr<AudioDebugRecorder> GetNullptrAudioDebugRecorder(
     const AudioParameters& params) {
   return nullptr;
 }
+
+// This enum must match the numbering for AudioOutputProxyStreamFormat in
+// enums.xml. Do not reorder or remove items, only add new items before
+// STREAM_FORMAT_MAX.
+enum StreamFormat {
+  STREAM_FORMAT_INVALID = 0,
+  STREAM_FORMAT_BITSTREAM = 1,
+  STREAM_FORMAT_PCM_LINEAR = 2,
+  STREAM_FORMAT_PCM_LOW_LATENCY = 3,
+  STREAM_FORMAT_PCM_LOW_LATENCY_FALLBACK_TO_FAKE = 4,
+  STREAM_FORMAT_FAKE = 5,
+  STREAM_FORMAT_MAX = 5,
+};
 
 }  // namespace
 
@@ -236,6 +250,12 @@ AudioOutputStream* AudioManagerBase::MakeAudioOutputStreamProxy(
     const std::string& device_id) {
   CHECK(GetTaskRunner()->BelongsToCurrentThread());
 
+  if (!params.IsValid()) {
+    UMA_HISTOGRAM_ENUMERATION("Media.AudioOutputStreamProxy.StreamFormat",
+                              STREAM_FORMAT_INVALID, STREAM_FORMAT_MAX + 1);
+    return nullptr;
+  }
+
   // If the caller supplied an empty device id to select the default device,
   // we fetch the actual device id of the default device so that the lookup
   // will find the correct device regardless of whether it was opened as
@@ -264,7 +284,15 @@ AudioOutputStream* AudioManagerBase::MakeAudioOutputStreamProxy(
         GetPreferredOutputStreamParameters(output_device_id, params);
 
     // Ensure we only pass on valid output parameters.
-    if (!output_params.IsValid()) {
+    if (output_params.IsValid()) {
+      if (params.effects() != output_params.effects()) {
+        // Turn off effects that weren't requested.
+        output_params.set_effects(params.effects() & output_params.effects());
+      }
+      UMA_HISTOGRAM_ENUMERATION("Media.AudioOutputStreamProxy.StreamFormat",
+                                STREAM_FORMAT_PCM_LOW_LATENCY,
+                                STREAM_FORMAT_MAX + 1);
+    } else {
       // We've received invalid audio output parameters, so switch to a mock
       // output device based on the input parameters.  This may happen if the OS
       // provided us junk values for the hardware configuration.
@@ -278,11 +306,30 @@ AudioOutputStream* AudioManagerBase::MakeAudioOutputStreamProxy(
       // Tell the AudioManager to create a fake output device.
       output_params = params;
       output_params.set_format(AudioParameters::AUDIO_FAKE);
-    } else if (params.effects() != output_params.effects()) {
-      // Turn off effects that weren't requested.
-      output_params.set_effects(params.effects() & output_params.effects());
+      UMA_HISTOGRAM_ENUMERATION("Media.AudioOutputStreamProxy.StreamFormat",
+                                STREAM_FORMAT_PCM_LOW_LATENCY_FALLBACK_TO_FAKE,
+                                STREAM_FORMAT_MAX + 1);
     }
+
     output_params.set_latency_tag(params.latency_tag());
+
+  } else {
+    switch (output_params.format()) {
+      case AudioParameters::AUDIO_PCM_LINEAR:
+        UMA_HISTOGRAM_ENUMERATION("Media.AudioOutputStreamProxy.StreamFormat",
+                                  STREAM_FORMAT_PCM_LINEAR,
+                                  STREAM_FORMAT_MAX + 1);
+        break;
+      case AudioParameters::AUDIO_FAKE:
+        UMA_HISTOGRAM_ENUMERATION("Media.AudioOutputStreamProxy.StreamFormat",
+                                  STREAM_FORMAT_FAKE, STREAM_FORMAT_MAX + 1);
+        break;
+      default:
+        DCHECK(output_params.IsBitstreamFormat());
+        UMA_HISTOGRAM_ENUMERATION("Media.AudioOutputStreamProxy.StreamFormat",
+                                  STREAM_FORMAT_BITSTREAM,
+                                  STREAM_FORMAT_MAX + 1);
+    }
   }
 
   std::unique_ptr<DispatcherParams> dispatcher_params =
