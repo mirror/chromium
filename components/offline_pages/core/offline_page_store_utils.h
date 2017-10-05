6c8688ca51da2cff45eede465baa8f01079c82c7
Yafei Duan <romax@chromium.org>
2017-10-05 13:59:03 -0700

    [Offline Pages] Reorganizing StoreUtils and ModelUtils.
    Reorganizing OfflinePageStoreUtils and OfflinePageModelUtils.
    The criteria are:
    - The store-related utility functions go into OPStoreUtils, like the
      previous time-related To/FromDatabaseTime. This patch is adding
      To/FromDatabaseFilePath, doing the translation between strings and
      FilePaths.
    - The other utility functions which are used by new tasks for taskified
      Offline Page Model are organized into OPModelUtils. And this should
      also be used later in the new taskified OPM.
    
    Bug: 770878
    Change-Id: I64d54211e0107e637cf3e9095d32f199fafc19a5

---
+++ components/offline_pages/core/offline_page_store_utils.h
@@ -0,0 +1,36 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_OFFLINE_PAGES_CORE_OFFLINE_PAGE_STORE_UTILS_H_
+#define COMPONENTS_OFFLINE_PAGES_CORE_OFFLINE_PAGE_STORE_UTILS_H_
+
+#include <stdint.h>
+
+#include <string>
+
+namespace base {
+class Time;
+class FilePath;
+}  // namespace base
+
+namespace offline_pages {
+
+namespace store_utils {
+
+// Time conversion methods for the time format recommended for offline pages
+// projects for database storage: elapsed time in microseconds since the Windows
+// epoch.
+int64_t ToDatabaseTime(base::Time time);
+base::Time FromDatabaseTime(int64_t serialized_time);
+
+// File path conversion methods for the file path format recommended for offline
+// pages projects for database storage: UTF8 string.
+std::string ToDatabaseFilePath(const base::FilePath& file_path);
+base::FilePath FromDatabaseFilePath(const std::string& file_path_string);
+
+}  // namespace store_utils
+
+}  // namespace offline_pages
+
+#endif  // COMPONENTS_OFFLINE_PAGES_CORE_OFFLINE_PAGE_STORE_UTILS_H_
