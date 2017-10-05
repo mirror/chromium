703768878bc8549b2fbb2456c1f31917a7613fe9
Gregory Chatzinoff <gchatz@chromium.org>
2017-10-04 21:36:16 -0700

    WIP
    Bug:none
    Change-Id: I1f82cb4c17dd1a0aac7033054574710241fa9aa9

--- ios/chrome/browser/ui/signin_interaction/signin_interaction_controller.h
+++ ios/chrome/browser/ui/signin_interaction/signin_interaction_controller.h
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_AUTHENTICATION_SIGNIN_INTERACTION_CONTROLLER_H_
-#define IOS_CHROME_BROWSER_UI_AUTHENTICATION_SIGNIN_INTERACTION_CONTROLLER_H_
+#ifndef IOS_CHROME_BROWSER_UI_SIGNIN_INTERACTION_SIGNIN_INTERACTION_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_UI_SIGNIN_INTERACTION_SIGNIN_INTERACTION_CONTROLLER_H_
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 #import "base/ios/block_types.h"
 #include "components/signin/core/browser/signin_metrics.h"
@@ -16,11 +16,14 @@ namespace ios {
 class ChromeBrowserState;
 }
 @class ChromeIdentity;
+@protocol SigninInteractionPresenting;
 
 // Interaction controller for sign-in related operations. This class is mainly a
 // proxy for |ChromeSigninViewController|, calling directly
 // |ChromeIdentityInteractionManager| for the no-accounts case.
 @interface SigninInteractionController : NSObject
+
+// All method comments need to be updated.
 
 // Designated initializer.
 // * |browserState| is the current browser state
@@ -29,8 +32,7 @@ class ChromeBrowserState;
 //   part of the presented view controllers stack
 // * |accessPoint| represents the access point that initiated the sign-in.
 - (instancetype)initWithBrowserState:(ios::ChromeBrowserState*)browserState
-            presentingViewController:(UIViewController*)presentingViewController
-               isPresentedOnSettings:(BOOL)isPresentedOnSettings
+                presentationProvider:(id<SigninInteractionPresenting>)presenter
                          accessPoint:(signin_metrics::AccessPoint)accessPoint
                          promoAction:(signin_metrics::PromoAction)promoAction
                           dispatcher:(id<ApplicationCommands>)dispatcher;
@@ -61,4 +63,4 @@ class ChromeBrowserState;
 
 @end
 
-#endif  // IOS_CHROME_BROWSER_UI_AUTHENTICATION_SIGNIN_INTERACTION_CONTROLLER_H_
+#endif  // IOS_CHROME_BROWSER_UI_SIGNIN_INTERACTION_SIGNIN_INTERACTION_CONTROLLER_H_
