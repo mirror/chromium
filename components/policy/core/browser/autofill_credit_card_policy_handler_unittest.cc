b8d94fe617c55668d2e7e285e1946906f63c05b8
Cait Phillips <caitkp@chromium.org>
2017-10-05 17:28:42 -0400

    Disable Sync for Wallet if credit card autofill is disabled by policy
    This is a follow up CL to
    https://chromium-review.googlesource.com/c/chromium/src/+/666059,
    which ensures that no credit card data is synced if autofill for credit
    cards has been disabled by policy.
    
    Bug: 700812
    Change-Id: Ie458e9fdc4ee52308b23b862cedb531bdeb2fb22

---
+++ components/policy/core/browser/autofill_credit_card_policy_handler_unittest.cc
@@ -0,0 +1,85 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/policy/core/browser/autofill_credit_card_policy_handler.h"
+#include "base/memory/ptr_util.h"
+#include "base/values.h"
+#include "components/autofill/core/common/autofill_pref_names.h"
+#include "components/policy/core/common/policy_map.h"
+#include "components/policy/core/common/policy_types.h"
+#include "components/policy/policy_constants.h"
+#include "components/prefs/pref_value_map.h"
+#include "components/sync/base/pref_names.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace policy {
+
+// Test cases for the Autofill credit card policy setting.
+class AutofillCreditCardPolicyHandlerTest : public testing::Test {};
+
+TEST_F(AutofillCreditCardPolicyHandlerTest, Default) {
+  PolicyMap policy;
+  PrefValueMap prefs;
+  AutofillCreditCardPolicyHandler handler;
+  handler.ApplyPolicySettings(policy, &prefs);
+  EXPECT_FALSE(
+      prefs.GetValue(autofill::prefs::kAutofillCreditCardEnabled, NULL));
+  EXPECT_FALSE(prefs.GetValue(syncer::prefs::kSyncAutofillWallet, NULL));
+  EXPECT_FALSE(
+      prefs.GetValue(syncer::prefs::kSyncAutofillWalletMetadata, NULL));
+}
+
+TEST_F(AutofillCreditCardPolicyHandlerTest, Enabled) {
+  PolicyMap policy;
+  policy.Set(key::kAutofillCreditCardEnabled, POLICY_LEVEL_MANDATORY,
+             POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD,
+             base::MakeUnique<base::Value>(true), nullptr);
+  PrefValueMap prefs;
+  AutofillCreditCardPolicyHandler handler;
+  handler.ApplyPolicySettings(policy, &prefs);
+
+  // Enabling Autofill for credit cards should not set the prefs.
+  EXPECT_FALSE(
+      prefs.GetValue(autofill::prefs::kAutofillCreditCardEnabled, NULL));
+  EXPECT_FALSE(prefs.GetValue(syncer::prefs::kSyncAutofillWallet, NULL));
+  EXPECT_FALSE(
+      prefs.GetValue(syncer::prefs::kSyncAutofillWalletMetadata, NULL));
+}
+
+TEST_F(AutofillCreditCardPolicyHandlerTest, Disabled) {
+  PolicyMap policy;
+  policy.Set(key::kAutofillCreditCardEnabled, POLICY_LEVEL_MANDATORY,
+             POLICY_SCOPE_USER, POLICY_SOURCE_CLOUD,
+             base::MakeUnique<base::Value>(false), nullptr);
+  PrefValueMap prefs;
+  AutofillCreditCardPolicyHandler handler;
+  handler.ApplyPolicySettings(policy, &prefs);
+
+  // Disabling Autofill for credit cards should switch the prefs to managed.
+  const base::Value* value = NULL;
+  EXPECT_TRUE(
+      prefs.GetValue(autofill::prefs::kAutofillCreditCardEnabled, &value));
+  ASSERT_TRUE(value);
+  bool autofill_credt_card_enabled = true;
+  bool result = value->GetAsBoolean(&autofill_credt_card_enabled);
+  ASSERT_TRUE(result);
+  EXPECT_FALSE(autofill_credt_card_enabled);
+
+  EXPECT_TRUE(prefs.GetValue(syncer::prefs::kSyncAutofillWallet, &value));
+  ASSERT_TRUE(value);
+  bool wallet_enabled = true;
+  result = value->GetAsBoolean(&wallet_enabled);
+  ASSERT_TRUE(result);
+  EXPECT_FALSE(wallet_enabled);
+
+  EXPECT_TRUE(
+      prefs.GetValue(syncer::prefs::kSyncAutofillWalletMetadata, &value));
+  ASSERT_TRUE(value);
+  bool wallet_metadata_enabled = true;
+  result = value->GetAsBoolean(&wallet_metadata_enabled);
+  ASSERT_TRUE(result);
+  EXPECT_FALSE(wallet_metadata_enabled);
+}
+
+}  // namespace policy
