508ab17c3a020fc1eaddf9071d2be865d96549d9
Fady Samuel <fsamuel@chromium.org>
2017-10-04 20:23:53 -0400

    WIP: Determine fallback strategy in SurfaceAggregator Bug:
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I381428d7eb4d2d16761fb6b562e0bc1915ab2185

--- components/viz/common/quads/surface_draw_quad.cc
+++ components/viz/common/quads/surface_draw_quad.cc
@@ -5,6 +5,7 @@
 #include "components/viz/common/quads/surface_draw_quad.h"
 
 #include "base/logging.h"
+#include "base/optional.h"
 #include "base/trace_event/trace_event_argument.h"
 #include "base/values.h"
 
@@ -12,36 +13,38 @@ namespace viz {
 
 SurfaceDrawQuad::SurfaceDrawQuad() = default;
 
-void SurfaceDrawQuad::SetNew(const SharedQuadState* shared_quad_state,
-                             const gfx::Rect& rect,
-                             const gfx::Rect& visible_rect,
-                             const SurfaceId& surface_id,
-                             SurfaceDrawQuadType surface_draw_quad_type,
-                             SkColor default_background_color,
-                             SurfaceDrawQuad* fallback_quad) {
+SurfaceDrawQuad::SurfaceDrawQuad(const SurfaceDrawQuad& other) = default;
+
+SurfaceDrawQuad::~SurfaceDrawQuad() = default;
+
+void SurfaceDrawQuad::SetNew(
+    const SharedQuadState* shared_quad_state,
+    const gfx::Rect& rect,
+    const gfx::Rect& visible_rect,
+    const SurfaceId& surface_id,
+    const base::Optional<SurfaceId>& fallback_surface_id,
+    SkColor default_background_color) {
   bool needs_blending = true;
   DrawQuad::SetAll(shared_quad_state, DrawQuad::SURFACE_CONTENT, rect,
                    visible_rect, needs_blending);
   this->surface_id = surface_id;
-  this->surface_draw_quad_type = surface_draw_quad_type;
+  this->fallback_surface_id = fallback_surface_id;
   this->default_background_color = default_background_color;
-  this->fallback_quad = fallback_quad;
 }
 
-void SurfaceDrawQuad::SetAll(const SharedQuadState* shared_quad_state,
-                             const gfx::Rect& rect,
-                             const gfx::Rect& visible_rect,
-                             bool needs_blending,
-                             const SurfaceId& surface_id,
-                             SurfaceDrawQuadType surface_draw_quad_type,
-                             SkColor default_background_color,
-                             SurfaceDrawQuad* fallback_quad) {
+void SurfaceDrawQuad::SetAll(
+    const SharedQuadState* shared_quad_state,
+    const gfx::Rect& rect,
+    const gfx::Rect& visible_rect,
+    bool needs_blending,
+    const SurfaceId& surface_id,
+    const base::Optional<SurfaceId>& fallback_surface_id,
+    SkColor default_background_color) {
   DrawQuad::SetAll(shared_quad_state, DrawQuad::SURFACE_CONTENT, rect,
                    visible_rect, needs_blending);
   this->surface_id = surface_id;
-  this->surface_draw_quad_type = surface_draw_quad_type;
+  this->fallback_surface_id = fallback_surface_id;
   this->default_background_color = default_background_color;
-  this->fallback_quad = fallback_quad;
 }
 
 const SurfaceDrawQuad* SurfaceDrawQuad::MaterialCast(const DrawQuad* quad) {
--- components/viz/test/surface_hittest_test_helpers.cc
+++ components/viz/test/surface_hittest_test_helpers.cc
@@ -56,8 +56,7 @@ void CreateSurfaceDrawQuad(RenderPass* pass,
   SurfaceDrawQuad* surface_quad =
       pass->CreateAndAppendDrawQuad<SurfaceDrawQuad>();
   surface_quad->SetNew(pass->shared_quad_state_list.back(), quad_rect,
-                       quad_rect, surface_id, SurfaceDrawQuadType::PRIMARY,
-                       SK_ColorWHITE, nullptr);
+                       quad_rect, surface_id, SurfaceId(), SK_ColorWHITE);
 }
 
 void CreateRenderPass(int render_pass_id,
