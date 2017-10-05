3d90c11e594a43a7a483ae51aa6da576a1d6df36
Makoto Shimazu <shimazu@chromium.org>
2017-10-05 14:19:29 +0900

    ServiceWorkerInstalledScriptsManager handles all installed scripts
    As a step of removing SWReadFromCacheJob, this patch adds a path to get the same
    installed scripts multiple times. WebServiceWorkerInstalledScriptsManager asks
    the browser if the script has already been taken, and the browser side
    counterpart (SWInstalledScriptsSender) sends the script after streaming all of
    scripts. This is not optimal for sites which importing the same script multiple
    times, but it simplifies the code.
    
    Bug: 756312
    Change-Id: I8d8c03fe8488367b5aa1b8d2c02e13a5f25e676e

--- third_party/WebKit/Source/core/workers/InstalledScriptsManager.h
+++ third_party/WebKit/Source/core/workers/InstalledScriptsManager.h
@@ -53,14 +53,11 @@ class InstalledScriptsManager {
   // installed.
   virtual bool IsScriptInstalled(const KURL& script_url) const = 0;
 
-  enum class ScriptStatus { kSuccess, kTaken, kFailed };
+  enum class ScriptStatus { kSuccess, kFailed };
   // Used on the worker thread. GetScriptData() can provide a script for the
   // |script_url| only once. When GetScriptData returns
   // - ScriptStatus::kSuccess: the script has been received correctly. Sets the
   //                           script to |out_script_data|.
-  // - ScriptStatus::kTaken: the script has been served from this manager
-  //                         (i.e. the same script is read more than once).
-  //                         |out_script_data| is left as is.
   // - ScriptStatus::kFailed: an error happened while receiving the script from
   //                          the browser process.
   //                         |out_script_data| is left as is.
