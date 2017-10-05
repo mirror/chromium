34d3955b6809c78029258c2d6fd0085c6facae56
Etienne Pierre-Doray <etiennep@chromium.org>
2017-10-05 12:10:32 -0400

    [Zucchini] Refactor ImageIndex to use TargetsAffinity.
    This CL refactors several components to use target affinities
    and to avoid mutation of ImageIndex. Mainly:
    
    - ImageIndex becomes immutable after initialization and always 
      stores references as IndirectReference. Also:
      - TypeInfo becomes ReferenceSet with better encapsulation.
      - PoolInfo becomes TargetPool and also stores a list of targets.
      - LabelTargets()/LabelAssociatedTargets()/UnlabelTargets() are 
        removed.
    - EncodedView uses labels directly to compute Projection(), using
      lookups in a label table.
      - Introduce EncodedView::PoolInfo to holds data.
    
    Note that LabelManager is still being used to minimize changes, even
    though most of its functionality are stripped down. It will be
    remove as a follow-up CL (627188) that simplifies reference delta.
    
    
    Experimental CL.
    
    Bug: 729154
    Change-Id: Ia8aaa7e5f287480c987d65a5dc24b01e7d1622dd

---
+++ chrome/installer/zucchini/reference_set.cc
@@ -0,0 +1,56 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/installer/zucchini/reference_set.h"
+
+#include <algorithm>
+#include <iterator>
+
+#include "base/logging.h"
+#include "base/macros.h"
+#include "chrome/installer/zucchini/target_pool.h"
+
+namespace zucchini {
+
+ReferenceSet::ReferenceSet(const ReferenceTypeTraits& traits,
+                           const TargetPool& target_pool)
+    : traits_(traits), target_pool_(target_pool) {}
+ReferenceSet::ReferenceSet(ReferenceSet&&) = default;
+ReferenceSet::~ReferenceSet() = default;
+
+void ReferenceSet::InsertReferences(ReferenceReader&& ref_reader) {
+  DCHECK(references_.empty());
+  for (auto ref = ref_reader.GetNext(); ref.has_value();
+       ref = ref_reader.GetNext()) {
+    references_.push_back(
+        {ref->location, target_pool_.KeyForOffset(ref->target)});
+  }
+}
+
+void ReferenceSet::InsertReferences(const std::vector<Reference>& refs) {
+  DCHECK(references_.empty());
+  DCHECK(std::is_sorted(refs.begin(), refs.end(),
+                        [](const Reference& a, const Reference& b) {
+                          return a.location < b.location;
+                        }));
+  std::transform(refs.begin(), refs.end(), std::back_inserter(references_),
+                 [&](const Reference& ref) {
+                   return IndirectReference{
+                       ref.location, target_pool_.KeyForOffset(ref.target)};
+                 });
+}
+
+IndirectReference ReferenceSet::at(offset_t location) const {
+  auto pos = std::upper_bound(references_.begin(), references_.end(), location,
+                              [](offset_t a, const IndirectReference& ref) {
+                                return a < ref.location;
+                              });
+
+  DCHECK(pos != references_.begin());
+  --pos;
+  DCHECK_LT(location, pos->location + width());
+  return *pos;
+}
+
+}  // namespace zucchini
