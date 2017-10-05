d492690a8a2e4b509c396837c38b88a1a95fc74d
Matt Mueller <mattm@chromium.org>
2017-10-04 18:18:32 -0700

    Allow UTF-8 in PrintableStrings inside client certificate DNs.
    Bug: 770323
    Change-Id: I684c92fb407276a46bd83b7520e02fe6a113a828

--- net/cert/x509_cert_types.h
+++ net/cert/x509_cert_types.h
@@ -38,9 +38,17 @@ struct NET_EXPORT CertPrincipal {
   ~CertPrincipal();
 
 #if BUILDFLAG(USE_BYTE_CERTS)
+  // Configures handling of PrintableString values in the DistinguishedName. Do
+  // not use non-default handling without consulting //net owners.
+  enum class PrintableStringHandling { kDefault, kAsUTF8Hack };
+
   // Parses a BER-format DistinguishedName.
   // TODO(mattm): change this to take a der::Input.
-  bool ParseDistinguishedName(const void* ber_name_data, size_t length);
+  bool ParseDistinguishedName(
+      const void* ber_name_data,
+      size_t length,
+      PrintableStringHandling printable_string_handling =
+          PrintableStringHandling::kDefault);
 #endif
 
   // Returns a name that can be used to represent the issuer.  It tries in this
