db96043ad078d2a1af53a3779ec6ba96e7d68319
Fernando Serboncini <fserb@chromium.org>
2017-10-05 11:12:43 -0400

    Makes Fetcher part of ExecutionContext
    This is needed so we can share font loading code between Document and
    Worker.
    
    We need to keep the const-ness from Document's Fetcher(), since this 
    is required everywhere. Unfortunately, Worker's fetcher is lazy and
    mutable. So we have to hack around it.
    
    Bug: 722511
    Change-Id: I6b8332f0f0ac98ad892c8d57e59511abc7c23214

--- third_party/WebKit/Source/core/inspector/WorkerInspectorController.cpp
+++ third_party/WebKit/Source/core/inspector/WorkerInspectorController.cpp
@@ -77,7 +77,7 @@ void WorkerInspectorController::ConnectFrontend(int session_id) {
                                         nullptr, session->V8Session()));
   if (thread_->GlobalScope()->IsWorkerGlobalScope() &&
       RuntimeEnabledFeatures::OffMainThreadFetchEnabled()) {
-    DCHECK(ToWorkerGlobalScope(thread_->GlobalScope())->GetResourceFetcher());
+    DCHECK(ToWorkerGlobalScope(thread_->GlobalScope())->Fetcher());
     session->Append(InspectorNetworkAgent::CreateForWorker(
         ToWorkerGlobalScope(thread_->GlobalScope())));
   }
--- third_party/WebKit/Source/core/workers/WorkerOrWorkletGlobalScope.cpp
+++ third_party/WebKit/Source/core/workers/WorkerOrWorkletGlobalScope.cpp
@@ -60,13 +60,16 @@ void WorkerOrWorkletGlobalScope::CountDeprecation(WebFeature feature) {
   ReportingProxy().CountDeprecation(feature);
 }
 
-ResourceFetcher* WorkerOrWorkletGlobalScope::GetResourceFetcher() {
+ResourceFetcher* WorkerOrWorkletGlobalScope::Fetcher() const {
   DCHECK(RuntimeEnabledFeatures::OffMainThreadFetchEnabled());
   DCHECK(!IsMainThreadWorkletGlobalScope());
   if (resource_fetcher_)
     return resource_fetcher_;
-  WorkerFetchContext* fetch_context = WorkerFetchContext::Create(*this);
-  resource_fetcher_ = ResourceFetcher::Create(fetch_context);
+  WorkerOrWorkletGlobalScope* mutable_scope =
+      const_cast<WorkerOrWorkletGlobalScope*>(this);
+  WorkerFetchContext* fetch_context =
+      WorkerFetchContext::Create(*mutable_scope);
+  mutable_scope->resource_fetcher_ = ResourceFetcher::Create(fetch_context);
   return resource_fetcher_;
 }
 
