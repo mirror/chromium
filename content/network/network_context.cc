ff46a95d9e979df7f1c6db972c5492db5792fd52
Andrey Kosyakov <caseq@chromium.org>
2017-10-05 14:07:32 -0700

    Move throttling to network service
    This moves the contents of content/common/devtools to content/network/throttling,
    removing DevTools from the class names.
    This also removes NetworkConditions from the public interface of ThrottlihgController,
    replacing it with 4 function parameters to simplify future migration to network service.
    
    Bug: 721408
    Change-Id: I0dc5b70eceb9976af623662a21d730260e6bbba8

--- content/network/network_context.cc
+++ content/network/network_context.cc
@@ -21,11 +21,11 @@
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/pref_service_factory.h"
-#include "content/common/devtools/devtools_network_transaction_factory.h"
 #include "content/network/cache_url_loader.h"
 #include "content/network/http_server_properties_pref_delegate.h"
 #include "content/network/network_service_impl.h"
 #include "content/network/network_service_url_loader_factory_impl.h"
+#include "content/network/throttling/throttling_network_transaction_factory.h"
 #include "content/network/url_loader_impl.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_switches.h"
@@ -218,7 +218,7 @@ std::unique_ptr<net::URLRequestContext> NetworkContext::MakeURLRequestContext(
   builder.SetCreateHttpTransactionFactoryCallback(
       base::BindOnce([](net::HttpNetworkSession* session)
                          -> std::unique_ptr<net::HttpTransactionFactory> {
-        return std::make_unique<DevToolsNetworkTransactionFactory>(session);
+        return std::make_unique<ThrottlingNetworkTransactionFactory>(session);
       }));
   ApplyContextParamsToBuilder(&builder, network_context_params);
 
