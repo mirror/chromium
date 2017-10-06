31dfb9339d06e6afaa05f9027eacd822d18b3532
Gabriel Charette <gab@chromium.org>
2017-10-06 14:57:20 -0400

    [Tracker] [Reland] Remove all remaining deprecated Web/BrowserThreads!!
    Merged with FILE thread CL which wasn't ready then but is ready now:
    https://chromium-review.googlesource.com/c/chromium/src/+/685276
    
    NOTE: This CL is a tracker for the reland in smaller pieces to track
    down what caused crbug.com/768886 and forced its revert.
    
    (BrowserThread::PROCESS_LAUNCHER migration was put on hold because of
    Android specific requirements around process launching)
    
    Remaining usage of TestBrowserThreadBundle::REAL_DBFILE_THREAD could simply
    be removed as all of the product code was already migrated to TaskScheduler
    which always uses real threads in unit tests.
    
    Bug: 689520, 365909, 752144
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: Id70b722aa35e8568795d5aac16947392ac1597be
    
    NOPRESUBMIT=True (for content/browser/browser_thread_impl.cc:211)
    
    Initial CL: https://chromium-review.googlesource.com/610880 (patchset 1 of this CL)
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503732}
    
    Reverted: https://chromium-review.googlesource.com/c/chromium/src/+/689254
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504939}
    
    Change-Id: I3661674ff89033baf055b860b8c16fc08b1cc391

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -316,7 +316,7 @@ _BANNED_CPP_FUNCTIONS = (
       (),
     ),
     (
-      r'/(WebThread|BrowserThread)::(FILE|FILE_USER_BLOCKING|DB|CACHE)',
+      'BrowserThread::FILE',
       (
         'The non-UI/IO BrowserThreads are deprecated, please migrate this',
         'code to TaskScheduler. See https://goo.gl/mDSxKl for details.',
