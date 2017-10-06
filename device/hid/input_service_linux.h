0ce9c0039cfabbc579b193a43bd0dcb0f0bf82d1
Ke He <ke.he@intel.com>
2017-10-06 10:43:29 +0800

    InputServiceLinux mojofication.
    Mojofy the InputServiceLinux, convert all its clients to use mojo
    interfaces.
    
    Removed the useless InputServiceProxy, rewrite the FakeInputServiceLinux
    to make sure the browsertests fit for using mojo.
    
    Make the InputServiceLinux owned by Device Service.
    
    BUG=676944
    
    Save connected devices in HidDetectionScreen.
    
    This is a preparation CL for mojofication on InputServiceLinux.
    
    Before this CL, the HidDetectionScreen always query the InputServiceProxy
    for connected devices. As a part of InputServiceLinux mojofication,
    the InputServiceProxy will be removed. All clients will be converted to
    use the InputDeviceManager mojo Interfaces.
    
    In HidDetectionScreen, to avoid calling mojo interface frequently and to
    make the logic of creating/deleting the InterfacePtr<InputDeviceManager>
    more simple, it should get and maintain the list of devices instead of
    always doing query.
    
    BUG=676944
    
    Change-Id: I518b9a322ca162a861a241261f780d065e3e946e

--- device/hid/input_service_linux.h
+++ device/hid/input_service_linux.h
@@ -12,30 +12,25 @@
 
 #include "base/compiler_specific.h"
 #include "base/macros.h"
-#include "base/observer_list.h"
 #include "base/threading/thread_checker.h"
 #include "device/hid/public/interfaces/input_service.mojom.h"
+#include "mojo/public/cpp/bindings/binding_set.h"
+#include "mojo/public/cpp/bindings/interface_ptr_set.h"
 
 namespace device {
 
 // This class provides information and notifications about
 // connected/disconnected input/HID devices. This class is *NOT*
 // thread-safe and all methods must be called from the FILE thread.
-// TODO(ke.he@intel.com): We'll move the InputServiceLinux into
-// device service, and run it in the FILE thread.
-class InputServiceLinux {
+class InputServiceLinux : public device::mojom::InputDeviceManager {
  public:
   using DeviceMap = std::map<std::string, device::mojom::InputDeviceInfoPtr>;
 
-  class Observer {
-   public:
-    virtual ~Observer() {}
-    virtual void OnInputDeviceAdded(device::mojom::InputDeviceInfoPtr info) = 0;
-    virtual void OnInputDeviceRemoved(const std::string& id) = 0;
-  };
-
   InputServiceLinux();
-  virtual ~InputServiceLinux();
+  ~InputServiceLinux() override;
+
+  // Create the InputServiceLinux instance by GetInstance(), bind the request.
+  static void Create(device::mojom::InputDeviceManagerRequest request);
 
   // Returns the InputServiceLinux instance for the current process. Creates one
   // if none has been set.
@@ -51,12 +46,13 @@ class InputServiceLinux {
   // current process. |service| will never be deleted.
   static void SetForTesting(std::unique_ptr<InputServiceLinux> service);
 
-  void AddObserver(Observer* observer);
-  void RemoveObserver(Observer* observer);
+  void AddBinding(device::mojom::InputDeviceManagerRequest request);
 
-  // Returns list of all currently connected input/hid devices.
-  virtual void GetDevices(
-      std::vector<device::mojom::InputDeviceInfoPtr>* devices);
+  // device::mojom::InputDeviceManager implementation:
+  void GetDevicesAndSetClient(
+      device::mojom::InputDeviceManagerClientAssociatedPtrInfo client,
+      GetDevicesCallback callback) override;
+  void GetDevices(GetDevicesCallback callback) override;
 
  protected:
   void AddDevice(device::mojom::InputDeviceInfoPtr info);
@@ -65,10 +61,12 @@ class InputServiceLinux {
   bool CalledOnValidThread() const;
 
   DeviceMap devices_;
-  base::ObserverList<Observer> observers_;
 
  private:
   base::ThreadChecker thread_checker_;
+  mojo::BindingSet<device::mojom::InputDeviceManager> bindings_;
+  mojo::AssociatedInterfacePtrSet<device::mojom::InputDeviceManagerClient>
+      clients_;
 
   DISALLOW_COPY_AND_ASSIGN(InputServiceLinux);
 };
