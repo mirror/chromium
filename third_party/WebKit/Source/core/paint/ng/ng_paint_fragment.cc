e542e9bb49234af75d0556b13a12721365889292
Koji Ishii <kojii@chromium.org>
2017-10-06 17:06:01 +0900

    visualrect
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I694e5ac6b655732f9f99bbceccb8fe10e385dbe7

--- third_party/WebKit/Source/core/paint/ng/ng_paint_fragment.cc
+++ third_party/WebKit/Source/core/paint/ng/ng_paint_fragment.cc
@@ -4,6 +4,7 @@
 
 #include "core/paint/ng/ng_paint_fragment.h"
 
+#include "core/layout/ng/inline/ng_physical_text_fragment.h"
 #include "core/layout/ng/ng_physical_container_fragment.h"
 #include "core/layout/ng/ng_physical_fragment.h"
 #include "platform/wtf/PtrUtil.h"
@@ -13,23 +14,54 @@ namespace blink {
 NGPaintFragment::NGPaintFragment(RefPtr<const NGPhysicalFragment> fragment)
     : physical_fragment_(std::move(fragment)) {
   DCHECK(physical_fragment_);
-  PopulateDescendants();
 }
 
 // Populate descendant NGPaintFragment from NGPhysicalFragment tree.
 void NGPaintFragment::PopulateDescendants() {
+  DCHECK(children_.IsEmpty());
+
   if (PhysicalFragment().IsContainer()) {
     const NGPhysicalContainerFragment& fragment =
         ToNGPhysicalContainerFragment(PhysicalFragment());
     children_.ReserveCapacity(fragment.Children().size());
     for (const auto& child_fragment : fragment.Children()) {
       auto child = WTF::MakeUnique<NGPaintFragment>(child_fragment);
+      child->PopulateDescendants();
       children_.push_back(std::move(child));
     }
   }
-  // TODO(kojii): Do some stuff to accumulate visual rects and convert to paint
-  // coordinates.
-  SetVisualRect(PhysicalFragment().LocalVisualRect());
+
+  UpdateVisualRect();
+}
+
+void NGPaintFragment::UpdateVisualRect() {
+  const NGPhysicalFragment& fragment = PhysicalFragment();
+  switch (fragment.Type()) {
+    case NGPhysicalFragment::kFragmentBox:
+      LayoutObject* layout_object = fragment.GetLayoutObject();
+      DCHECK(layout_object);
+      SetVisualRect(layout_object->VisualRect());
+      return;
+
+    case NGPhysicalFragment::kFragmentText: {
+      LayoutObject* layout_object = fragment.GetLayoutObject();
+      DCHECK(layout_object);
+      LayoutRect visual_rect = fragment.LocalVisualRect();
+      visual_rect.MoveBy(layout_object->PaintOffset());
+      SetVisualRect(visual_rect);
+      return;
+    }
+
+    case NGPhysicalFragment::kFragmentLineBox: {
+      LayoutRect visual_rect;
+      for (const auto& child : children_) {
+        visual_rect.Unite(child->VisualRect());
+      }
+      SetVisualRect(visual_rect);
+      return;
+    }
+  }
+  NOTREACHED();
 }
 
 }  // namespace blink
