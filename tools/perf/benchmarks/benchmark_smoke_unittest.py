7dd6c030e582b3d91f2893e5fa5eb21f6f2b07b5
Ben Hayden <benjhayden@chromium.org>
2017-10-06 10:08:48 -0700

    Smoke test number of values produced by telemetry benchmarks.
    Depends on:
    https://chromium-review.googlesource.com/c/catapult/+/704055
    
    https://github.com/catapult-project/catapult/issues/3951
    
    Change-Id: I5ae9351f5e934f4379efe2f96c0c86e7cfae5f0e

--- tools/perf/benchmarks/benchmark_smoke_unittest.py
+++ tools/perf/benchmarks/benchmark_smoke_unittest.py
@@ -30,7 +30,7 @@ from benchmarks import speedometer
 from benchmarks import v8_browsing
 
 
-def SmokeTestGenerator(benchmark, num_pages=1):
+def SmokeTestGenerator(benchmark, num_pages=1, max_values=1000):
   """Generates a benchmark that includes first N pages from pageset.
 
   Args:
@@ -70,6 +70,17 @@ def SmokeTestGenerator(benchmark, num_pages=1):
     benchmark_module.AddCommandLineArgs(parser)
     benchmark.SetArgumentDefaults(parser)
     options.MergeDefaultValues(parser.get_default_values())
+
+    # Prevent benchmarks from accidentally trying to upload too much data to
+    # the chromeperf dashboard. The number of values uploaded is equal to (the
+    # average number of values produced by a single page) * (1 + (the number
+    # of pages)). The "1 + " accounts for values summarized across all pages.
+    # We can approximate "the average number of values produced by a single
+    # page" as the number of values produced by the first page. pageset_repeat
+    # doesn't matter because values are summarized across repetitions before
+    # uploading.
+    story_set = benchmark().CreateStorySet(options)
+    benchmark.MAX_NUM_VALUES = max_values / len(story_set.stories)
 
     benchmark.ProcessCommandLineArgs(None, options)
     benchmark_module.ProcessCommandLineArgs(None, options)
