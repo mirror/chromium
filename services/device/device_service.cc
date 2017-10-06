591182229663df3da188db3a3e7a0c560903f451
Ke He <ke.he@intel.com>
2017-10-06 10:43:29 +0800

    NOT for review, debuging browsertest failure

--- services/device/device_service.cc
+++ services/device/device_service.cc
@@ -35,6 +35,10 @@
 #include "services/device/vibration/vibration_manager_impl.h"
 #endif
 
+#if defined(OS_LINUX) && defined(USE_UDEV)
+#include "device/hid/input_service_linux.h"
+#endif
+
 namespace device {
 
 #if defined(OS_ANDROID)
@@ -154,6 +158,15 @@ void DeviceService::BindNFCProviderRequest(mojom::NFCProviderRequest request) {
 void DeviceService::BindVibrationManagerRequest(
     mojom::VibrationManagerRequest request) {
   VibrationManagerImpl::Create(std::move(request));
+}
+#endif
+
+#if defined(OS_LINUX) && defined(USE_UDEV)
+void DeviceService::BindInputDeviceManagerRequest(
+    mojom::InputDeviceManagerRequest request) {
+  file_task_runner_->PostTask(
+      FROM_HERE,
+      base::BindOnce(&InputServiceLinux::Create, std::move(request)));
 }
 #endif
 
