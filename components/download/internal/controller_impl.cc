9b7b11283706446b8eccf5f0c02bb65d15837bdf
Xing Liu <xingliu@chromium.org>
2017-10-06 12:19:27 -0700

    Download service: Fix an issue that downloads started too early.
    This CL adds a delay to wait for network stack to be ready.
    
    After download service database loaded, the service and its client can
    issue download request. However, net::HostResolverImpl needs to wait
    for a kernel call to get the ip address and it will shake off all the
    requests after getting the new ip address.
    
    This can happen after download requests are sent, and
    net::ERROR_NETWORK_CHANGED is reported to download core.
    
    
    Bug: 771252, 769426
    Change-Id: I4b50d5cbe905d3ea71d5cf8200270b5582f790ce

--- components/download/internal/controller_impl.cc
+++ components/download/internal/controller_impl.cc
@@ -579,13 +579,8 @@ void ControllerImpl::AttemptToFinalizeSetup() {
 
   controller_state_ = State::READY;
 
-  UpdateDriverStates();
-
   KillTimedOutDownloads();
   NotifyServiceOfStartup();
-
-  // Pull the initial straw if active downloads haven't reach maximum.
-  ActivateMoreDownloads();
 }
 
 void ControllerImpl::HandleUnrecoverableSetup() {
@@ -1044,6 +1039,7 @@ void ControllerImpl::KillTimedOutDownloads() {
 }
 
 void ControllerImpl::ActivateMoreDownloads() {
+  LOG(ERROR) << "@@@ " << __func__;
   if (controller_state_ != State::READY)
     return;
 
