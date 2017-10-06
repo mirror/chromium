a1b88157989773cb66c9c3bf7cda246b694e9080
Scott Chen <scottchen@chromium.org>
2017-10-06 12:07:25 -0700

    MD Extensions: use css grid instead of iron-list for item-lists.
    This fixes various bug for item-list rendering, and slightly increases
    performance for window or item resizing.
    
    This CL also added a restriction of maximun 3 columns for the grid, whereas
    iron-list was rendering as many as it could fit on the same row.
    
    As a side effect of removing iron-list, we're no longer doing lazy-loading
    for large lists. Testing with 80 elements, this approach still has a faster
    first-useful-paint load-time than iron-list. However, scrolling does use
    slightly more CPU than iron-list. Both have the same CPU usage when resizing.
    
    Bug: 769863, 769573
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ib74a5b5e132dd5473ae4a8ca176dddc5946c4db4

--- chrome/browser/resources/md_extensions/item.js
+++ chrome/browser/resources/md_extensions/item.js
@@ -100,7 +100,6 @@ cr.define('extensions', function() {
         assert(this.data);
         idElement.innerHTML = this.i18n('itemId', this.data.id);
       }
-      this.fire('extension-item-size-changed', {item: this.data});
     },
 
     /**
