7bfe51306a2c7a092e0ead8bba929265a02eacfb
Shimi Zhang <ctzsm@chromium.org>
2017-10-06 15:02:42 -0700

    [Smart Selection] Instrumentation test for reset.
    The test for http://crrev/c/653432.
    
    Bug: 703884
    Change-Id: I9ffdf0e16a34e9fc410e71593f625426cb32ac78

--- content/public/android/javatests/src/org/chromium/content/browser/ContentViewCoreSelectionTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ContentViewCoreSelectionTest.java
@@ -76,6 +76,11 @@ public class ContentViewCoreSelectionTest {
 
         @Override
         public boolean requestSelectionPopupUpdates(boolean shouldSuggest) {
+            if (!shouldSuggest) {
+                mResult = new SelectionClient.Result();
+                mResult.startAdjust = 0;
+                mResult.endAdjust = 0;
+            }
             ThreadUtils.postOnUiThread(() -> mResultCallback.onClassified(mResult));
             return true;
         }
@@ -340,10 +345,46 @@ public class ContentViewCoreSelectionTest {
         DOMUtils.longPressNode(mContentViewCore, "smart_selection");
         waitForSelectActionBarVisible(true);
 
+        Assert.assertEquals(
+                "1600 Amphitheatre Parkway", mSelectionPopupController.getSelectedText());
+
         SelectionClient.Result returnResult = mSelectionPopupController.getClassificationResult();
         Assert.assertEquals(-5, returnResult.startAdjust);
         Assert.assertEquals(8, returnResult.endAdjust);
         Assert.assertEquals("Maps", returnResult.label);
+    }
+
+    @Test
+    @MediumTest
+    @Feature({"TextInput", "SmartSelection"})
+    public void testSmartSelectionReset() throws Throwable {
+        SelectionClient.Result result = new SelectionClient.Result();
+        result.startAdjust = -5;
+        result.endAdjust = 8;
+        result.label = "Maps";
+
+        TestSelectionClient client = new TestSelectionClient();
+        client.setResult(result);
+        client.setResultCallback(mSelectionPopupController.getResultCallback());
+
+        mSelectionPopupController.setSelectionClient(client);
+
+        DOMUtils.longPressNode(mContentViewCore, "smart_selection");
+        waitForSelectActionBarVisible(true);
+
+        Assert.assertEquals(
+                "1600 Amphitheatre Parkway", mSelectionPopupController.getSelectedText());
+
+        SelectionClient.Result returnResult = mSelectionPopupController.getClassificationResult();
+        Assert.assertEquals(-5, returnResult.startAdjust);
+        Assert.assertEquals(8, returnResult.endAdjust);
+        Assert.assertEquals("Maps", returnResult.label);
+
+        DOMUtils.clickNode(mContentViewCore, "smart_selection");
+
+        CriteriaHelper.pollUiThread(Criteria.equals(
+                0, () -> mSelectionPopupController.getClassificationResult().startAdjust));
+        Assert.assertEquals("Amphitheatre", mSelectionPopupController.getSelectedText());
     }
 
     @Test
