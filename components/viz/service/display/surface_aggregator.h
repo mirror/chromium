e2d70ed75a92c563b77d6a0109f1383422e91401
Fady Samuel <fsamuel@chromium.org>
2017-10-06 19:14:20 -0400

    Determine fallback strategy in SurfaceAggregator
    Prior to this CL, the parent (embedder) client submitted a fallback
    SurfaceDrawQuad in addition to the primary SurfaceDrawQuad indicating
    what surface to use in the event that a primary surface is unavailable.
    This added complexity in the layer compositor, forced the layer
    compositor to submit more information in a CompositorFrame, and made
    it possible for the embedder to place the fallback in an entirely
    different part of the CompositorFrame which is more open-ended of
    an API than is desirable. It was also possible to specify multiple
    fallbacks when they weren't supported at the time.
    
    This CL simplifies the system somewhat by reducing the chain of fallback
    SurfaceDrawQuads to a single primary SurfaceDrawQuad with a fallback
    surface ID. Note that this CL does not yet handle device scale factor
    and stretch_content_to_fill_bounds correctly for fallbacks. Those
    will be introduced in subsequent CLs.
    
    Tests were updated accordingly.
    
    Bug: 672962
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I381428d7eb4d2d16761fb6b562e0bc1915ab2185

--- components/viz/service/display/surface_aggregator.h
+++ components/viz/service/display/surface_aggregator.h
@@ -101,13 +101,48 @@ class VIZ_SERVICE_EXPORT SurfaceAggregator {
                            const SurfaceId& surface_id);
 
   void HandleSurfaceQuad(const SurfaceDrawQuad* surface_quad,
-                         const gfx::Rect& primary_rect,
                          const gfx::Transform& target_transform,
                          const ClipData& clip_rect,
                          RenderPass* dest_pass,
                          bool ignore_undamaged,
                          gfx::Rect* damage_rect_in_quad_space,
                          bool* damage_rect_in_quad_space_valid);
+
+  void HandleSurface(Surface* surface,
+                     const SharedQuadState* source_sqs,
+                     const gfx::Rect& rect,
+                     const gfx::Rect& visible_rect,
+                     const gfx::Transform& target_transform,
+                     const ClipData& clip_rect,
+                     RenderPass* dest_pass,
+                     bool ignore_undamaged,
+                     gfx::Rect* damage_rect_in_quad_space,
+                     bool* damage_rect_in_quad_space_valid);
+
+  void EmitFallbackSurface(const SurfaceDrawQuad* primary_surface_quad,
+                           const gfx::Transform& target_transform,
+                           const ClipData& clip_rect,
+                           RenderPass* dest_pass,
+                           bool ignore_undamaged,
+                           gfx::Rect* damage_rect_in_quad_space,
+                           bool* damage_rect_in_quad_space_valid);
+
+  void EmitDefaultBackgroundColorQuad(const SurfaceDrawQuad* surface_quad,
+                                      const gfx::Transform& target_transform,
+                                      const ClipData& clip_rect,
+                                      RenderPass* dest_pass);
+
+  void EmitGutterQuadsIfNecessary(
+      const gfx::Rect& primary_rect,
+      const gfx::Rect& fallback_rect,
+      const SharedQuadState* primary_shared_quad_state,
+      const gfx::Transform& target_transform,
+      const ClipData& clip_rect,
+      SkColor background_color,
+      RenderPass* dest_pass);
+
+  void ReportMissingFallbackSurface(const SurfaceId& fallback_surface_id,
+                                    const Surface* fallback_surface);
 
   SharedQuadState* CopySharedQuadState(const SharedQuadState* source_sqs,
                                        const gfx::Transform& target_transform,
