b8ab453745cd5c450ef77179c63666287abb118a
Sergey Ulanov <sergeyu@chromium.org>
2017-10-06 11:00:00 -0700

    Make cast_shell buildable for Fuchsia
    1. Added ifdefs around crash-reporting, bug 753619.
    2. Added CastContentWindowFuchsia.
    3. Disabled CastDisplayConfigurator.
    4. Disabled NetworkChangeNotifierFactoryCast.
    
    Bug: 707031, 753619
    Change-Id: Id48af74ccdb003230233a6061bbf49412a571ce6

--- chromecast/app/cast_main_delegate.h
+++ chromecast/app/cast_main_delegate.h
@@ -37,9 +37,9 @@ class CastMainDelegate : public content::ContentMainDelegate {
   int RunProcess(
       const std::string& process_type,
       const content::MainFunctionParams& main_function_params) override;
-#if !defined(OS_ANDROID)
+#if !defined(OS_ANDROID) && !defined(OS_FUCHSIA)
   void ZygoteForked() override;
-#endif  // !defined(OS_ANDROID)
+#endif  // !defined(OS_ANDROID) && !defined(OS_FUCHSIA)
   content::ContentBrowserClient* CreateContentBrowserClient() override;
   content::ContentRendererClient* CreateContentRendererClient() override;
   content::ContentUtilityClient* CreateContentUtilityClient() override;
