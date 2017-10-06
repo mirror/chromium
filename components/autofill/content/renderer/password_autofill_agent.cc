c28bc89747613e899fd7cbf4632d576a01dfd38b
nikhil <nikhil.sahni@samsung.com>
2017-10-06 11:04:19 +0530

    Username and password are not autofilled on schwab.com
    Username and password are not autofilled on schwab.com due to login
    form is hosted in an iframe on a different domain.
    Earlier check was that all parent frames have the same security
    origin, which is changed now to PSL-matching origins.
    
    BUG=770181
    
    Change-Id: Idd44aa5a31aaf8ea159e1f7149043ace657be6b0

--- components/autofill/content/renderer/password_autofill_agent.cc
+++ components/autofill/content/renderer/password_autofill_agent.cc
@@ -37,6 +37,7 @@
 #include "content/public/renderer/navigation_state.h"
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_view.h"
+#include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/WebKit/public/platform/WebInputEvent.h"
 #include "third_party/WebKit/public/platform/WebSecurityOrigin.h"
@@ -493,6 +494,35 @@ bool FillUserNameAndPassword(
   return true;
 }
 
+// Supporting function for IsPublicSuffixDomainMatch
+std::string GetRegistryControlledDomain(const GURL& signon_realm) {
+  return net::registry_controlled_domains::GetDomainAndRegistry(
+      signon_realm,
+      net::registry_controlled_domains::INCLUDE_PRIVATE_REGISTRIES);
+}
+
+// Supporting function for FillFormOnPasswordReceived
+bool IsPublicSuffixDomainMatch(const std::string& url1,
+                               const std::string& url2) {
+  GURL gurl1(url1);
+  GURL gurl2(url2);
+
+  if (!gurl1.is_valid() || !gurl2.is_valid())
+    return false;
+
+  if (gurl1 == gurl2)
+    return true;
+
+  std::string domain1(GetRegistryControlledDomain(gurl1));
+  std::string domain2(GetRegistryControlledDomain(gurl2));
+
+  if (domain1.empty() || domain2.empty())
+    return false;
+
+  return gurl1.scheme() == gurl2.scheme() && domain1 == domain2 &&
+         gurl1.port() == gurl2.port();
+}
+
 // Attempts to fill |username_element| and |password_element| with the
 // |fill_data|. Will use the data corresponding to the preferred username,
 // unless the |username_element| already has a value set. In that case,
@@ -518,7 +548,9 @@ bool FillFormOnPasswordReceived(
 
   while (cur_frame->Parent()) {
     cur_frame = cur_frame->Parent();
-    if (!bottom_frame_origin.Equals(cur_frame->GetSecurityOrigin().ToString()))
+    if (!IsPublicSuffixDomainMatch(
+            bottom_frame_origin.Ascii(),
+            cur_frame->GetSecurityOrigin().ToString().Ascii()))
       return false;
   }
 
