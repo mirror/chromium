0e154f885b329d058bc62a0d5c5630379dfe6681
Varun Khaneja <vakh@chromium.org>
2017-10-06 15:03:09 -0700

    Send database update notification from PVer4 code
    Bug: 771697
    Change-Id: If920aeed593d958a48419b36d676a3be999bace1

---
+++ components/safe_browsing/db/notification_types.h
@@ -0,0 +1,32 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SAFE_BROWSING_DB_NOTIFICATION_TYPES_H_
+#define COMPONENTS_SAFE_BROWSING_DB_NOTIFICATION_TYPES_H_
+
+// This file describes various types used to describe and filter notifications
+// that pass through the NotificationService.
+//
+// Only notifications specific to Safe Browsing should be here.
+namespace safe_browsing {
+
+enum NotificationType {
+  NOTIFICATION_SAFE_BROWSING_START = 0,
+
+  // General -----------------------------------------------------------------
+
+  // Special signal value to represent an interest in all notifications.
+  // Not valid when posting a notification.
+  NOTIFICATION_ALL = NOTIFICATION_SAFE_BROWSING_START,
+
+  // A safe browsing database update completed.  Source is the
+  // SafeBrowsingService and the details are none.
+  NOTIFICATION_SAFE_BROWSING_UPDATE_COMPLETE,
+
+  NOTIFICATION_SAFE_BROWSING_END,
+};
+
+}  // namespace safe_browsing
+
+#endif  // COMPONENTS_SAFE_BROWSING_DB_NOTIFICATION_TYPES_H_
