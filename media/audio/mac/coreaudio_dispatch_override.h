8450ee0c9d2ffd7528e6c573d25dce34ed345eb3
Oskar Sundbom <ossu@chromium.org>
2017-10-06 15:50:26 +0200

    Serialize AUHAL Pause/Resume calls to workaround missing callbacks error
    We do this by interposing dispatch_get_global_queue() and returning a
    serial queue for the cases where we're called from two functions in
    CoreAudio.
    
    To avoid the costs introduced by interposing this globally, and in a separate dylib, the code uses the private dyld_dynamic_interpose API.
    If that API is not available on the system (OSX < 10.10), the code does no interposing and the workaround is disabled.
    
    Bug: chromium:772410
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie39e46fa5dee029b05ada190556853c5f6d5c658

---
+++ media/audio/mac/coreaudio_dispatch_override.h
@@ -0,0 +1,12 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_AUDIO_MAC_COREAUDIO_DISPATCH_OVERRIDE_H_
+#define MEDIA_AUDIO_MAC_COREAUDIO_DISPATCH_OVERRIDE_H_
+
+namespace media {
+bool initialize_coreaudio_dispatch_override();
+}
+
+#endif /* MEDIA_AUDIO_MAC_COREAUDIO_DISPATCH_OVERRIDE_H_ */
