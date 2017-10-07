4458506d02dbe44aad8f8c635d623cd719991abd
Scott Chen <scottchen@chromium.org>
2017-10-06 17:30:56 -0700

    MD Extensions: redirect to root when hitting unsupported routes
    Bug: 772575
    Change-Id: Ie3e7c94ea6fc5cd093c3d1664a3e714116815332

--- chrome/browser/resources/md_extensions/extensions.js
+++ chrome/browser/resources/md_extensions/extensions.js
@@ -4,6 +4,15 @@
 
 (function() {
 'use strict';
+
+// Going to any unsupported routes should redirect to chrome://extensions/
+// TODO(scottchen): there might be a better place to do this once manager
+//     and service become less coupled.
+if (!extensions.navigation.isRouteSupported()) {
+  location.pathname = '/';
+  return;
+}
+
 const manager = /** @type {extensions.Manager} */ (
     document.querySelector('extensions-manager'));
 manager.readyPromiseResolver.promise.then(function() {
