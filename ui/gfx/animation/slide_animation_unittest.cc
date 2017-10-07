9c80b9b0a02d625bda98e4f745e3181a671bc796
Alex Newcomer <newcomer@chromium.org>
2017-10-06 17:03:52 -0700

    cros: Pagination grid animations changes
    Pagination grid animations should be constant
    regardless of whether they are partially completed.
    
    I created a new flavor of SlideAnimation for this, which
    ensures that the duration is dampened if the animation
    is partway complete.
    
    Also I added a new unit test for this new flavor of SlideAnimation.
    
    Bug: 771262
    Change-Id: Ibb40799cb6d15b04e1e2f9f36ac224c3d28daf13

--- ui/gfx/animation/slide_animation_unittest.cc
+++ ui/gfx/animation/slide_animation_unittest.cc
@@ -18,10 +18,23 @@ namespace gfx {
 ////////////////////////////////////////////////////////////////////////////////
 // SlideAnimationTest
 class SlideAnimationTest : public testing::Test {
+ public:
+  void RunAnimationFor(base::TimeDelta duration) {
+    base::TimeTicks now = base::TimeTicks::Now();
+    animation_api_->SetStartTime(now);
+    animation_api_->Step(now + duration);
+  }
+
+  std::unique_ptr<AnimationTestApi> animation_api_;
+  std::unique_ptr<SlideAnimation> slide_animation_;
+
  protected:
   SlideAnimationTest()
       : scoped_task_environment_(
-            base::test::ScopedTaskEnvironment::MainThreadType::UI) {}
+            base::test::ScopedTaskEnvironment::MainThreadType::UI) {
+    slide_animation_.reset(new SlideAnimation(nullptr));
+    animation_api_.reset(new AnimationTestApi(slide_animation_.get()));
+  }
 
  private:
   base::test::ScopedTaskEnvironment scoped_task_environment_;
@@ -90,6 +103,47 @@ TEST_F(SlideAnimationTest, DontNotifyOnDelete) {
   // Make sure the delegate wasn't notified.
   EXPECT_FALSE(delegate.finished());
   EXPECT_FALSE(delegate.canceled());
+}
+
+// Tests that animations which are started partway and have a dampening factor
+// of 1 progress properly.
+TEST_F(SlideAnimationTest,
+       AnimationWithPartialProgressAndDefaultDampeningFactor) {
+  slide_animation_->SetTweenType(Tween::LINEAR);
+  const double duration = 100;
+  slide_animation_->SetSlideDuration(duration);
+  slide_animation_->Show();
+  // Advance the animation to halfway done.
+  RunAnimationFor(base::TimeDelta::FromMilliseconds(duration / 2.0));
+  EXPECT_EQ(0.5, slide_animation_->GetCurrentValue());
+  // Reverse the animation.
+  slide_animation_->Hide();
+
+  // Run for 10ms, the animation should step to 0.4.
+  RunAnimationFor(base::TimeDelta::FromMilliseconds(10));
+
+  EXPECT_EQ(0.4, slide_animation_->GetCurrentValue());
+}
+
+// Tests that animations which are started partway and have a dampening factor
+// of >1 progress properly.
+TEST_F(SlideAnimationTest,
+       AnimationWithPartialProgressAndNonDefaultDampeningFactor) {
+  slide_animation_->SetTweenType(Tween::LINEAR);
+  const double duration = 100;
+  slide_animation_->SetDampeningValue(3.0);
+  slide_animation_->SetSlideDuration(duration);
+  slide_animation_->Show();
+  // Advance the animation to halfway done.
+  RunAnimationFor(base::TimeDelta::FromMilliseconds(duration / 2.0));
+  EXPECT_EQ(0.5, slide_animation_->GetCurrentValue());
+  // Reverse the animation.
+  slide_animation_->Hide();
+
+  // Run the animation for 8ms.
+  RunAnimationFor(base::TimeDelta::FromMilliseconds(8));
+
+  EXPECT_FLOAT_EQ(0.25999039, slide_animation_->GetCurrentValue());
 }
 
 }  // namespace gfx
