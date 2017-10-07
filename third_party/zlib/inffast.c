96bf10fe0caad4c5b83966d5d7e03676a5767c3f
Nigel Tao <nigeltao@chromium.org>
2017-10-07 17:35:57 +1100

    Speed zlib inflate_fast up to 1.5x on x86_64.
    There are two specific techniques that speed up zlib decompression.
    
    One is that, when performing "copy LENGTH bytes from previously
    decompressed output, DIST bytes ago", that copy of LENGTH bytes can
    often be done 8 bytes at a time, instead of 1 byte at a time. This
    technique requires 64-bit unaligned loads and stores. On x86_64, this
    can give up to a 1.3x speed up.
    
    Two is that, when filling the bit buffer for Huffman decoding, that
    buffer can be filled 8 bytes at a time, instead of 1 byte at a time.
    This technique requires 64-bit unaligned and also little-endian loads.
    On x86_64, this can give up to a 1.2x speed up.
    
    Together, this change can give up to a 1.5x speed up.
    
    The upstream pull request is
    https://github.com/madler/zlib/pull/292
    
    Zlib decompression microbenchmarks across an 11 file test corpus improve
    by between 1.1x and 1.5x. The details are in
    https://bugs.chromium.org/p/chromium/issues/detail?id=760853#c26
    
    This zlib change should affect things like HTTP de-compression and PNG
    decoding time. For PNG decoding,
    https://bugs.chromium.org/p/chromium/issues/detail?id=760853#c21
    summarizes many measurements as "the average speed-up is 2%, some images
    have large gains >> 15%".
    
    Bug:760853
    Change-Id: I6b7b446020e88e1860fffc6b0498083272db2e1a

--- third_party/zlib/inffast.c
+++ third_party/zlib/inffast.c
@@ -12,6 +12,44 @@
 #  pragma message("Assembler code may have bugs -- use at your own risk")
 #else
 
+#if defined(INFLATE_FAST64_PULLBITS)
+#include <stdint.h>
+#include <string.h>
+
+/* PULLBITS_48 pulls at least 48 bits into the bit accumulator.
+
+   The three-line memcpy_tmp dance here is a C standards compliant way of type
+   punning a uint8_t* as a uint64_t*. It is essentially:
+        hold |= *((uint64_t *)(in)) << bits;
+   The memcpy function call looks superficially inefficient, but modern
+   compilers like clang and gcc will generate efficient code.
+
+   The fact that in and bits are incremented by 6 and 48 instead of 8 and 64
+   is discussed below, in the comment above "uint64_t hold".
+ */
+#define PULLBITS_48() \
+    do { \
+        uint64_t memcpy_tmp; \
+        memcpy(&memcpy_tmp, in, 8); \
+        hold |= memcpy_tmp << bits; \
+        in += 6; \
+        bits += 48; \
+    } while (0)
+
+#else /* INFLATE_FAST64_PULLBITS */
+
+/* PULLBITS_8 pulls exactly 8 bits into the bit accumulator.
+
+   TODO: replace "hold += etc" with "hold |= etc", in PULLBITS_8, to be
+   consistent with PULLBITS_48. This is only a comment for now so that the
+   commit that introduced this comment has no effect whatsoever when
+   INFLATE_FAST64_PULLBITS is undefined. */
+#define PULLBITS_8() \
+    hold += (unsigned long)(*in++) << bits; \
+    bits += 8;
+
+#endif /* INFLATE_FAST64_PULLBITS */
+
 /*
    Decode literal, length, and distance codes and write out the resulting
    literal and match bytes until either not enough input or output is
@@ -27,6 +65,7 @@
         strm->avail_out >= INFLATE_FAST_MIN_LEFT
         start >= strm->avail_out
         state->bits < 8
+        (state->hold >> state->bits) == 0
 
    On return, state->mode is one of:
 
@@ -41,6 +80,10 @@
       and 13 bits for the distance extra.  This totals 48 bits, or six bytes.
       Therefore if strm->avail_in >= 6, then there is enough input to avoid
       checking for available input while decoding.
+
+    - On some architectures, it can be noticably faster (e.g. up to 1.15x faster
+      on x86_64) to load from strm->next_in 64 bits, or 8 bytes, at a time, so
+      INFLATE_FAST_MIN_HAVE == 8. This requires a little endian architecture.
 
     - The maximum bytes that a single length/distance pair can output is 258
       bytes, which is the maximum length that can be coded.  inflate_fast()
@@ -64,7 +107,54 @@ unsigned start;         /* inflate()'s starting value for strm->avail_out */
     unsigned whave;             /* valid bytes in the window */
     unsigned wnext;             /* window write index */
     unsigned char FAR *window;  /* allocated sliding window, if wsize != 0 */
-    unsigned long hold;         /* local strm->hold */
+
+    /* hold is a local copy of strm->hold. By default, hold satisfies the same
+       invariants that strm->hold does, namely that (hold >> bits) == 0. This
+       invariant is kept by loading bits into hold one byte at a time, like:
+
+       hold |= next_byte_of_input << bits; in++; bits += 8;
+
+       If we need to ensure that bits >= 15 then this code snippet is simply
+       repeated. Over one iteration of the outermost do/while loop, this
+       happens up to six times (48 bits of input), as described in the NOTES
+       above.
+
+       However, on some little endian architectures, it can be noticably faster
+       to load 64 bits once instead of 8 bits six times:
+
+       if (bits <= 16) {
+         hold |= next_8_bytes_of_input << bits; in += 6; bits += 48;
+       }
+
+       Unlike the simpler one byte load, shifting the next_8_bytes_of_input
+       by bits will overflow and lose those high bits, up to 2 bytes' worth.
+       The conservative estimate is therefore that we have read only 6 bytes
+       (48 bits). Again, as per the NOTES above, 48 bits is sufficient for the
+       rest of the iteration, and we will not need to load another 8 bytes.
+
+       Inside this function, we no longer satisfy (hold >> bits) == 0, but
+       this is not problematic, even if that overflow does not land on an 8 bit
+       byte boundary. Those excess bits will eventually shift down lower as the
+       Huffman decoder consumes input, and when new input bits need to be loaded
+       into the bits variable, the same input bits will be or'ed over those
+       existing bits. A bitwise or is idempotent: (a | b | b) equals (a | b).
+       Note that we therefore write that load operation as "hold |= etc" and not
+       "hold += etc".
+
+       Outside that loop, at the end of the function, hold is bitwise and'ed
+       with (1<<bits)-1 to drop those excess bits so that, on function exit, we
+       keep the invariant that (state->hold >> state->bits) == 0.
+
+       TODO: rename the bits variable to nbits, so that this block comment
+       is less confusing when discussing bits (the variable) and bits (one
+       eighth of a byte).
+       */
+#ifdef INFLATE_FAST64_PULLBITS
+    uint64_t hold;
+#else
+    unsigned long hold;
+#endif
+
     unsigned bits;              /* local strm->bits */
     code const FAR *lcode;      /* local strm->lencode */
     code const FAR *dcode;      /* local strm->distcode */
@@ -102,10 +192,12 @@ unsigned start;         /* inflate()'s starting value for strm->avail_out */
        input data or output space */
     do {
         if (bits < 15) {
-            hold += (unsigned long)(*in++) << bits;
-            bits += 8;
-            hold += (unsigned long)(*in++) << bits;
-            bits += 8;
+#ifdef INFLATE_FAST64_PULLBITS
+            PULLBITS_48();
+#else
+            PULLBITS_8();
+            PULLBITS_8();
+#endif
         }
         here = lcode[hold & lmask];
       dolen:
@@ -124,8 +216,11 @@ unsigned start;         /* inflate()'s starting value for strm->avail_out */
             op &= 15;                           /* number of extra bits */
             if (op) {
                 if (bits < op) {
-                    hold += (unsigned long)(*in++) << bits;
-                    bits += 8;
+#ifdef INFLATE_FAST64_PULLBITS
+                    PULLBITS_48();
+#else
+                    PULLBITS_8();
+#endif
                 }
                 len += (unsigned)hold & ((1U << op) - 1);
                 hold >>= op;
@@ -133,10 +228,12 @@ unsigned start;         /* inflate()'s starting value for strm->avail_out */
             }
             Tracevv((stderr, "inflate:         length %u\n", len));
             if (bits < 15) {
-                hold += (unsigned long)(*in++) << bits;
-                bits += 8;
-                hold += (unsigned long)(*in++) << bits;
-                bits += 8;
+#ifdef INFLATE_FAST64_PULLBITS
+                PULLBITS_48();
+#else
+                PULLBITS_8();
+                PULLBITS_8();
+#endif
             }
             here = dcode[hold & dmask];
           dodist:
@@ -148,12 +245,14 @@ unsigned start;         /* inflate()'s starting value for strm->avail_out */
                 dist = (unsigned)(here.val);
                 op &= 15;                       /* number of extra bits */
                 if (bits < op) {
-                    hold += (unsigned long)(*in++) << bits;
-                    bits += 8;
+#ifdef INFLATE_FAST64_PULLBITS
+                    PULLBITS_48();
+#else
+                    PULLBITS_8();
                     if (bits < op) {
-                        hold += (unsigned long)(*in++) << bits;
-                        bits += 8;
+                        PULLBITS_8();
                     }
+#endif
                 }
                 dist += (unsigned)hold & ((1U << op) - 1);
 #ifdef INFLATE_STRICT
--- third_party/zlib/inffast.h
+++ third_party/zlib/inffast.h
@@ -8,6 +8,80 @@
    subject to change. Applications should only use zlib.h.
  */
 
+/* The TRY_INFLATE_FAST64 macro is set at configuration time, to enable testing
+   if certain optimization techniques are applicable, on modern CPUs and modern
+   compilers.
+
+   One technique is INFLATE_FAST64_PULLBITS, which implements inffast.c's
+   PULLBITS via 8 byte loads instead of 1 byte loads. It requires:
+     - INFLATE_FAST64_UNALIGNED_LOADS_LITTLE_ENDIAN and
+       INFLATE_FAST64_INLINE_MEMCPY, or
+     - FORCE_INFLATE_FAST64_PULLBITS.
+ */
+#ifdef TRY_INFLATE_FAST64
+
+/* The INFLATE_FAST64_PULLBITS code shows consistent improvement on x86_64 and
+   is automatically enabled there. In theory, that optimization technique could
+   also apply to AArch64 and PowerPC64, amongst others. In practice, there is
+   possibly wider variation in how such hardware performs with unaligned loads,
+   so it is disabled by default.
+
+   Zlib builders can opt in to configuring with FORCE_INFLATE_FAST64_PULLBITS
+   after running their own benchmarks on the hardware they care about to confirm
+   the performance improvement.
+
+   For reference:
+   __x86_64__ is used by Clang and GCC. _M_X64 is used by MSVC.
+   __ppc64__ is used by Clang. __PPC64__ is used by GCC.
+   __aarch64__ is used by Clang and GCC.
+ */
+#if defined(__x86_64__) || defined(_M_X64)
+#if defined(__BYTE_ORDER__) && (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+#define INFLATE_FAST64_UNALIGNED_LOADS_LITTLE_ENDIAN 1
+#endif
+#endif
+
+/* The INFLATE_FAST64_PULLBITS code loads bits into the bit accumulator
+   (called hold) 8 bytes (64 bits) at a time instead of 1 byte at a time. The
+   code itself doesn't do this, but conceptually, it puns a uint8_t* pointer
+   (called in) as a uint64_t* pointer:
+        hold |= *((uint64_t *)(in)) << bits;
+   bits (the variable) is the number of bits (one eighth of a byte) that is
+   currently held in hold.
+
+   Technically speaking, this is undefined behavior according to the C
+   specification. Instead, what the code actually does is to use an equivalent
+   but standards compliant technique:
+        uint64_t memcpy_tmp;
+        memcpy(&memcpy_tmp, in, 8);
+        hold |= memcpy_tmp << bits;
+
+   This load happens inside the inflate_fast inner loop. Having this perform
+   well depends on the C compiler being able to recognize that memcpy to a
+   temporary as a single load instruction instead of a literal function call.
+
+   Zlib is expected to work well across a variety of C compilers. We therefore
+   disable this optimization by default, and whitelist specific compilers:
+   Clang, GCC and MSC (Microsoft). Note that Clang also #define's __GNUC__ as
+   it aims to be a drop-in replacement for GCC.
+
+   Once again, zlib builders can also opt in via FORCE_INFLATE_FAST64_PULLBITS.
+ */
+#if defined(__GNUC__) || defined(_MSC_VER)
+#define INFLATE_FAST64_INLINE_MEMCPY
+#endif
+
+#if defined(INFLATE_FAST64_UNALIGNED_LOADS_LITTLE_ENDIAN) && \
+    defined(INFLATE_FAST64_INLINE_MEMCPY)
+#define INFLATE_FAST64_PULLBITS 1
+#endif
+
+#endif /* TRY_INFLATE_FAST64 */
+
+#ifdef FORCE_INFLATE_FAST64_PULLBITS
+#define INFLATE_FAST64_PULLBITS 1
+#endif
+
 /* INFLATE_FAST_MIN_LEFT is the minimum number of output bytes that are left,
    so that we can call inflate_fast safely with only one up front bounds check.
    One length-distance code pair can copy up to 258 bytes.
@@ -20,6 +94,11 @@
    bits each) plus any additional bits (up to 5 for length and 13 for distance)
    can require reading up to 48 bits, or 6 bytes.
  */
+#if defined(INFLATE_FAST64_PULLBITS)
+/* Round up to a multiple of 8. */
+#define INFLATE_FAST_MIN_HAVE 8
+#else
 #define INFLATE_FAST_MIN_HAVE 6
+#endif
 
 void ZLIB_INTERNAL inflate_fast OF((z_streamp strm, unsigned start));
