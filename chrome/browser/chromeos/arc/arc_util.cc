a2550dc9e3f87cb8ce502891fb89dee8fa2c6200
khmel <khmel@google.com>
2017-10-06 17:22:47 -0700

    arc: Disable ARC on non-Google clients.
    It is possible to make a Chromium build with custom client id that
    defines non-Google build.  In this case auth code cannot be requested
    and ARC fails with server communication  error. This CL disables ARC
    on such builds.
    
    Test: Manually on device. Build image with mozilla client id. Without
          this CL this leads to SERVER_COMMUNICATION_ERROR on attempt to
          use ARC. With this CL ARC UI is not available and do not
          start. If I redefine client to Chrome again using oauth2-client*
          flags or build Chrome build, then ARC is available again and can
          be started without error.
    Bug: b/67410968
    Change-Id: I6bcfcb8c5a184a52df74020552404ff86b05051b

--- chrome/browser/chromeos/arc/arc_util.cc
+++ chrome/browser/chromeos/arc/arc_util.cc
@@ -31,6 +31,7 @@
 #include "components/user_manager/known_user.h"
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
+#include "google_apis/google_api_keys.h"
 
 namespace arc {
 
@@ -38,6 +39,9 @@ namespace {
 
 constexpr char kLsbReleaseArcVersionKey[] = "CHROMEOS_ARC_ANDROID_SDK_VERSION";
 constexpr char kAndroidMSdkVersion[] = "23";
+
+// Used as client id on bot tests.
+constexpr char kDummyClientId[] = "dummytoken";
 
 // Contains set of profiles for which decline reson was already reported.
 base::LazyInstance<std::set<base::FilePath>>::DestructorAtExit
@@ -222,6 +226,14 @@ bool IsArcAllowedForProfile(const Profile* profile) {
   if (!user_flow || !user_flow->CanStartArc()) {
     VLOG_IF(1, IsReportingFirstTimeForProfile(profile))
         << "ARC is not allowed in the current user flow.";
+    return false;
+  }
+
+  if (!google_apis::IsGoogleChromeAPIKeyUsed() &&
+      google_apis::GetOAuth2ClientID(google_apis::CLIENT_MAIN) !=
+          kDummyClientId) {
+    VLOG_IF(1, IsReportingFirstTimeForProfile(profile))
+        << "ARC is not supported for non-google client.";
     return false;
   }
 
