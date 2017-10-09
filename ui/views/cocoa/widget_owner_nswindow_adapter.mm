cb23479bd776928e8ae8832d9a9764f7ce5eb952
Trent Apted <tapted@chromium.org>
2017-10-09 14:22:00 +1100

    MacViews: Watch NSWindowDidDeminiaturizeNotification for NSWindow parents
    For views::Widgets with non-Widget parent windows, an adapter currently
    just watches for NSWindowDidChangeOcclusionStateNotification on the
    parent window when deciding to show a "visble" Widget that had hidden
    parent window. For a deminiaturize on the parent, this triggers, but it
    triggers before -[NSWindow isVisible] return YES for the parent.
    
    To fix, also watch NSWindowDidDeminiaturizeNotification.
    
    Bug: 771885
    Change-Id: I094a090198d1107bf0ef597d4d2e606634b7dc54

--- ui/views/cocoa/widget_owner_nswindow_adapter.mm
+++ ui/views/cocoa/widget_owner_nswindow_adapter.mm
@@ -73,6 +73,15 @@ WidgetOwnerNSWindowAdapter::WidgetOwnerNSWindowAdapter(
          selector:@selector(windowDidChangeOcclusionState:)
              name:NSWindowDidChangeOcclusionStateNotification
            object:anchor_window_];
+
+  // On a deminiaturize, the occlusion state change occurs before -[NSWindow
+  // isVisible] returns YES. So observe NSWindowDidDeminiaturizeNotification to
+  // check again at the end of the deminiaturize animation.
+  [[NSNotificationCenter defaultCenter]
+      addObserver:observer_bridge_
+         selector:@selector(windowDidChangeOcclusionState:)
+             name:NSWindowDidDeminiaturizeNotification
+           object:anchor_window_];
 }
 
 void WidgetOwnerNSWindowAdapter::OnWindowWillClose() {
