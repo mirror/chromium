54f9c55a0bd115e05c97082c6ad93c131c843303
Marijn Kruisselbrink <mek@chromium.org>
2017-10-09 14:32:13 -0700

    Add typemaps for MessagePortMessage mojo struct.
    This typemaps the mojom MessagePortMessage struct to either
    blink::MessagePortMessage or blink::BlinkMessagePortMessage, and uses
    that type in the BroadcastChannel mojo interface.
    
    By itself this doesn't gain us much (in particular since BroadcastChannel
    doesn't even support transferring ports as part of its messages), but in
    a follow-up CL MessagePortMessage will be expended with Blob handles to
    keep blobs that are part of the message alive, at which point sharing the
    same serialization logic will make code a lot simpler.
    
    Other follow-up patches will use the same struct for other postMessage
    APIs/IPCs/mojo interfaces.
    
    Bug: 740744, 351753
    Change-Id: I6cb6849352f7726cb9628b0156a507e08745fabe

--- third_party/WebKit/common/message_port/message_port_message.h
+++ third_party/WebKit/common/message_port/message_port_message.h
@@ -8,14 +8,17 @@
 #include <vector>
 
 #include "base/containers/span.h"
-#include "mojo/public/cpp/system/message_pipe.h"
+#include "third_party/WebKit/common/common_export.h"
+#include "third_party/WebKit/common/message_port/message_port_channel.h"
 
 namespace blink {
 
 // This struct represents messages as they are posted over a message port. This
 // type can be serialized as a blink::mojom::MessagePortMessage struct.
-struct MessagePortMessage {
+struct BLINK_COMMON_EXPORT MessagePortMessage {
   MessagePortMessage();
+  MessagePortMessage(MessagePortMessage&&);
+  MessagePortMessage& operator=(MessagePortMessage&&);
   ~MessagePortMessage();
 
   // To reduce copies when serializing |encoded_message| does not have to point
@@ -27,7 +30,7 @@ struct MessagePortMessage {
   std::vector<uint8_t> owned_encoded_message;
 
   // Any ports being transfered as part of this message.
-  std::vector<mojo::ScopedMessagePipeHandle> ports;
+  std::vector<MessagePortChannel> ports;
 };
 
 }  // namespace blink
