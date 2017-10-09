404ebb910de578712773036c1cc6e63536c2d848
k2.nagaraju <k2.nagaraju@samsung.com>
2017-10-09 15:03:12 +0530

    Clean up storage of overflow data in AudioInputSyncWriter
    Bug:768562
    Change-Id: Ied472734f32e09ff0aa15d54dcde3f74eb6b2b80

--- content/browser/renderer_host/media/audio_input_sync_writer.h
+++ content/browser/renderer_host/media/audio_input_sync_writer.h
@@ -164,13 +164,8 @@ class CONTENT_EXPORT AudioInputSyncWriter
   // It should ideally be rare, but we need to guarantee that the data arrives
   // since audio processing such as echo cancelling requires that to perform
   // properly.
-  std::vector<std::unique_ptr<media::AudioBus>> overflow_buses_;
-  struct OverflowParams {
-    double volume;
-    bool key_pressed;
-    base::TimeTicks capture_time;
-  };
-  base::circular_deque<OverflowParams> overflow_params_;
+  struct OverflowData;
+  std::vector<OverflowData*> overflow_data_;
 
   DISALLOW_IMPLICIT_CONSTRUCTORS(AudioInputSyncWriter);
 };
