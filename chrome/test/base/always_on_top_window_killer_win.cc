9276cf28b2fc15075b2ea75418865654e9ff545c
Greg Thompson <grt@chromium.org>
2017-10-09 12:18:06 +0200

    Save a snapshot of always-on-top windows if requested.
    "interactive_ui_tests.exe --snapshot-output-dir=DIR" will cause
    snapshots named "ss_HWND_PID_TID.png" to be written to the specified
    output directory.
    
    BUG=764415
    R=sky@chromium.org
    
    Change-Id: I5a7645c22a996ae4a84ade2d9d42c302a9fd66d8

--- chrome/test/base/always_on_top_window_killer_win.cc
+++ chrome/test/base/always_on_top_window_killer_win.cc
@@ -6,33 +6,112 @@
 
 #include <Windows.h>
 
+#include <ios>
 #include <string>
+#include <vector>
 
+#include "base/command_line.h"
+#include "base/files/file.h"
+#include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/process/process.h"
+#include "base/strings/stringprintf.h"
+#include "third_party/skia/include/core/SkBitmap.h"
+#include "ui/display/win/screen_win.h"
+#include "ui/gfx/codec/png_codec.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/image/image.h"
+#include "ui/snapshot/snapshot_win.h"
 
 namespace {
 
-const char kDialogFoundBeforeTest[] =
+constexpr char kDialogFoundBeforeTest[] =
     "There is an always on top dialog on the desktop. This was most likely "
     "caused by a previous test and may cause this test to fail. Trying to "
     "close it.";
 
-const char kDialogFoundPostTest[] =
+constexpr char kDialogFoundPostTest[] =
     "There is an always on top dialog on the desktop after running this test. "
     "This was most likely caused by this test and may cause future tests to "
     "fail, trying to close it.";
 
-const char kWindowFoundBeforeTest[] =
+constexpr char kWindowFoundBeforeTest[] =
     "There is an always on top window on the desktop before running the test. "
     "This may have been caused by a previous test and may cause this test to "
     "fail, class-name=";
 
-const char kWindowFoundPostTest[] =
+constexpr char kWindowFoundPostTest[] =
     "There is an always on top window on the desktop after running the test. "
     "This may have been caused by this test or a previous test and may cause "
     "flake, class-name=";
+
+constexpr char kSnapshotOutputDir[] = "snapshot-output-dir";
+
+// Saves a snapshot of |hwnd|'s client area to a file in |output_dir|, returning
+// the path to the file if created. An empty path is returned if no new snapshot
+// is created. The name of the file is generated based on the tuple (|hwnd|,
+// |process_id|, |thread_id|). No new snapshot is created if a snapshot for a
+// given tuple already exists.
+base::FilePath SaveSnapshot(HWND hwnd,
+                            DWORD process_id,
+                            DWORD thread_id,
+                            const base::FilePath& output_dir) {
+  // Create the output file.
+  base::FilePath output_path(output_dir.Append(base::FilePath(
+      base::StringPrintf(L"ss_%u_%u_%u.png", hwnd, process_id, thread_id))));
+  base::File file(output_path, base::File::FLAG_CREATE |
+                                   base::File::FLAG_WRITE |
+                                   base::File::FLAG_SHARE_DELETE |
+                                   base::File::FLAG_CAN_DELETE_ON_CLOSE);
+  if (!file.IsValid()) {
+    if (file.error_details() == base::File::FILE_ERROR_EXISTS) {
+      LOG(INFO) << "Skipping window snapshot since it is already present: "
+                << output_path.BaseName();
+    } else {
+      LOG(ERROR) << "Failed to create snapshot output file \"" << output_path
+                 << "\" with error " << file.error_details();
+    }
+    return base::FilePath();
+  }
+
+  // Delete the output file in case of any error.
+  file.DeleteOnClose(true);
+
+  // Take the snapshot.
+  gfx::Image image;
+  RECT tmp_rect;
+  ::GetClientRect(hwnd, &tmp_rect);
+  gfx::Rect bounds(display::win::ScreenWin::DIPToClientRect(
+      hwnd, gfx::Rect(gfx::Rect(tmp_rect).size())));
+  if (!ui::internal::GrabHwndSnapshot(hwnd, bounds, bounds, &image) ||
+      image.IsEmpty()) {
+    LOG(ERROR) << "Failed to grab snapshot of window " << std::hex << hwnd;
+    return base::FilePath();
+  }
+
+  // Encode it.
+  std::vector<unsigned char> encoded;
+  if (!gfx::PNGCodec::EncodeBGRASkBitmap(*image.ToSkBitmap(), false,
+                                         &encoded)) {
+    LOG(ERROR) << "Failed to PNG encode window snapshot.";
+    return base::FilePath();
+  }
+
+  // Write it to disk.
+  const int to_write = base::checked_cast<int>(encoded.size());
+  int written =
+      file.WriteAtCurrentPos(reinterpret_cast<char*>(encoded.data()), to_write);
+  if (written != to_write) {
+    LOG(ERROR) << "Failed to write entire snapshot to file";
+    return base::FilePath();
+  }
+
+  // Keep the output file.
+  file.DeleteOnClose(false);
+  return output_path;
+}
 
 BOOL CALLBACK AlwaysOnTopWindowProc(HWND hwnd, LPARAM l_param) {
   const BOOL kContinueIterating = TRUE;
@@ -57,8 +136,10 @@ BOOL CALLBACK AlwaysOnTopWindowProc(HWND hwnd, LPARAM l_param) {
         // interactive ui tests at least every 12 hours we're going with the
         // simple for now.
         CloseWindow(hwnd);
-      } else if (class_name != L"Button" && class_name != L"Shell_TrayWnd") {
-        // 'Button' is the start button, and 'Shell_TrayWnd' the taskbar.
+      } else if (class_name != L"Button" && class_name != L"Shell_TrayWnd" &&
+                 class_name != L"Shell_SecondaryTrayWnd") {
+        // 'Button' is the start button, 'Shell_TrayWnd' the taskbar, and
+        // 'Shell_SecondaryTrayWnd' is the taskbar on non-primary displays.
         //
         // These windows may be problematic as well, but in theory tests should
         // not be creating an always on top window that outlives the test. Log
@@ -87,6 +168,20 @@ BOOL CALLBACK AlwaysOnTopWindowProc(HWND hwnd, LPARAM l_param) {
                    << class_name << " process_id=" << process_id
                    << " thread_id=" << thread_id
                    << " process_path=" << process_path;
+
+        // Save a snapshot of the window if the process was run with an output
+        // directory.
+        base::FilePath output_dir =
+            base::CommandLine::ForCurrentProcess()->GetSwitchValuePath(
+                kSnapshotOutputDir);
+        if (!output_dir.empty()) {
+          base::FilePath snapshot_file =
+              SaveSnapshot(hwnd, process_id, thread_id, output_dir);
+          if (!snapshot_file.empty()) {
+            LOG(ERROR) << "Wrote snapshot to file " << snapshot_file;
+          }
+        }
+
         return kContinueIterating;
       }
     }
