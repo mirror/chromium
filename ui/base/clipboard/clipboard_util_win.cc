e333c00b7eac4033d6b298506cdcf3adb5f216df
Bruce Dawson <brucedawson@chromium.org>
2017-10-09 16:51:14 -0700

    Avoid std::string when char[] will do
    Running tools\win\static_initializers showed several atexit destructors
    that were triggered for static local std::string variables. These aren't
    as bad as globals with constructors but they still have unnecessary cost
    because they are thread-safe initialized and are registered for atexit
    destruction. Since static const char[] easily replaces them we might as
    well use it.
    
    Bug: 341941
    Change-Id: I03e36ce56852c77947b41fa5efdd0c14e27485f7

--- ui/base/clipboard/clipboard_util_win.cc
+++ ui/base/clipboard/clipboard_util_win.cc
@@ -457,11 +457,11 @@ void ClipboardUtil::CFHtmlExtractMetadata(const std::string& cf_html,
                                           size_t* fragment_end) {
   // Obtain base_url if present.
   if (base_url) {
-    static std::string src_url_str("SourceURL:");
+    static const char src_url_str[] = "SourceURL:";
     size_t line_start = cf_html.find(src_url_str);
     if (line_start != std::string::npos) {
       size_t src_end = cf_html.find("\n", line_start);
-      size_t src_start = line_start + src_url_str.length();
+      size_t src_start = line_start + arraysize(src_url_str) - 1;
       if (src_end != std::string::npos && src_start != std::string::npos) {
         *base_url = cf_html.substr(src_start, src_end - src_start);
         base::TrimWhitespaceASCII(*base_url, base::TRIM_ALL, base_url);
@@ -480,18 +480,20 @@ void ClipboardUtil::CFHtmlExtractMetadata(const std::string& cf_html,
   }
   size_t tag_start = cf_html.find("<!--StartFragment", markup_start);
   if (tag_start == std::string::npos) {
-    static std::string start_fragment_str("StartFragment:");
+    static const char start_fragment_str[] = "StartFragment:";
     size_t start_fragment_start = cf_html.find(start_fragment_str);
     if (start_fragment_start != std::string::npos) {
-      *fragment_start = static_cast<size_t>(atoi(cf_html.c_str() +
-          start_fragment_start + start_fragment_str.length()));
+      *fragment_start =
+          static_cast<size_t>(atoi(cf_html.c_str() + start_fragment_start +
+                                   arraysize(start_fragment_str) - 1));
     }
 
-    static std::string end_fragment_str("EndFragment:");
+    static const char end_fragment_str[] = "EndFragment:";
     size_t end_fragment_start = cf_html.find(end_fragment_str);
     if (end_fragment_start != std::string::npos) {
-      *fragment_end = static_cast<size_t>(atoi(cf_html.c_str() +
-          end_fragment_start + end_fragment_str.length()));
+      *fragment_end =
+          static_cast<size_t>(atoi(cf_html.c_str() + end_fragment_start +
+                                   arraysize(end_fragment_str) - 1));
     }
   } else {
     *fragment_start = cf_html.find('>', tag_start) + 1;
