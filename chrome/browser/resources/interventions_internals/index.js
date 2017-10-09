b3cc6de377b4d317ab79e9df5cd212288cdf07fd
Thanh Le <thanhdle@chromium.org>
2017-10-09 13:48:04 -0700

    Posting Previews navigations to chrome://interventions-internals.
    Bug: 764418
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Id7f79ef2295b9d22eccc12c8ea46812f09373eca

--- chrome/browser/resources/interventions_internals/index.js
+++ chrome/browser/resources/interventions_internals/index.js
@@ -2,6 +2,53 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/** @constructor */
+let InterventionsInternalPageImpl = function(request) {
+  this.binding_ =
+      new mojo.Binding(mojom.InterventionsInternalsPage, this, request);
+};
+
+InterventionsInternalPageImpl.prototype = {
+  /**
+   * Post a new log message to the web page.
+   * E.g. "dd/MM/yyyy hh:mm:ss: [type] some description [url.com]"
+   *
+   * @override
+   * @param {!MessageLog} log The new log message recorded by
+   * PreviewsLogger.
+   */
+  logNewMessage: function(log) {
+    let logsTable = $('message-logs-table');
+    let tableRow = document.createElement('tr');
+    tableRow.setAttribute('class', 'log-message');
+
+    let timeTd = document.createElement('td');
+    let date = new Date(log.time);
+    timeTd.textContent = date.toISOString();
+    timeTd.setAttribute('class', 'log-time');
+    tableRow.appendChild(timeTd);
+
+    let typeTd = document.createElement('td');
+    typeTd.setAttribute('class', 'log-type');
+    typeTd.textContent = log.type;
+    tableRow.appendChild(typeTd);
+
+    let descriptionTd = document.createElement('td');
+    descriptionTd.setAttribute('class', 'log-description');
+    descriptionTd.textContent = log.description;
+    tableRow.appendChild(descriptionTd);
+
+    // TODO(thanhdle): Truncate url and show full url when user clicks on it.
+    // crbug.com/773019
+    let urlTd = document.createElement('td');
+    urlTd.setAttribute('class', 'log-url');
+    urlTd.textContent = log.url.url;
+    tableRow.appendChild(urlTd);
+
+    logsTable.appendChild(tableRow);
+  },
+};
+
 cr.define('interventions_internals', () => {
   let pageHandler = null;
 
@@ -10,11 +57,17 @@ cr.define('interventions_internals', () => {
     getPreviewsEnabled();
   }
 
+  /**
+   * Retrieves the statuses of previews (i.e. Offline, LoFi, AMP Redirection),
+   * and posts them on chrome://intervention-internals.
+   */
   function getPreviewsEnabled() {
     pageHandler.getPreviewsEnabled()
         .then((response) => {
-          let statuses = $('previewsStatuses');
+          let statuses = $('previews-statuses');
 
+          // TODO(thanhdle): The statues are not printed in alphabetic order of
+          // the key. crbug.com/772458
           response.statuses.forEach((value, key) => {
             let message = value.description + ': ';
             message += value.enabled ? 'Enabled' : 'Disabled';
@@ -43,6 +96,7 @@ window.setupFn = window.setupFn || function() {
 
 document.addEventListener('DOMContentLoaded', () => {
   let pageHandler = null;
+  let pageImpl = null;
 
   window.setupFn().then(() => {
     if (window.testPageHandler) {
@@ -52,7 +106,13 @@ document.addEventListener('DOMContentLoaded', () => {
       Mojo.bindInterface(
           mojom.InterventionsInternalsPageHandler.name,
           mojo.makeRequest(pageHandler).handle);
+
+      // Set up client side mojo interface.
+      let client = new mojom.InterventionsInternalsPagePtr;
+      pageImpl = new InterventionsInternalPageImpl(mojo.makeRequest(client));
+      pageHandler.setClientPage(client);
     }
+
     interventions_internals.init(pageHandler);
   });
 });
