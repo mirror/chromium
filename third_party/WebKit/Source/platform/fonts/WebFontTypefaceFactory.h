f43ada56ab52b9fcd36feb7f8229f21d8f35d3c5
Dominik Röttsches <drott@chromium.org>
2017-10-09 13:38:50 +0300

    WIP FreeType for CBDT/CBLC, sbix and CFF2 fonts
    Change-Id: I6be1e92bf730094458764c98af22fb26b51db5ad

---
+++ third_party/WebKit/Source/platform/fonts/WebFontTypefaceFactory.h
@@ -0,0 +1,43 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/skia/include/ports/SkFontMgr.h"
+
+#if defined(OS_WIN) || defined(OS_MACOSX)
+#include "third_party/skia/include/ports/SkFontMgr_empty.h"
+#endif
+
+namespace blink {
+
+// Decided which Skia Fontmanager to use for instantiating a web font. In the
+// regular case, this would be default font manager used for the platform.
+// However, for variable font, color bitmap font formats and CFF2 fonts we want
+// to use FreeType on Windows and Mac.
+class WebFontTypefaceFactory {
+ public:
+  static bool CreateTypeface(const sk_sp<SkData>, sk_sp<SkTypeface>&);
+
+  // TODO(drott): This should be going away in favor of a new API on SkTypeface:
+  // https://bugs.chromium.org/p/skia/issues/detail?id=7121
+  static sk_sp<SkFontMgr> FontManagerForVariations();
+
+ private:
+  // These values are written to logs.  New enum values can be added, but
+  // existing enums must never be renumbered or deleted and reused.
+  enum WebFontInstantiationResult {
+    kErrorInstantiatingVariableFont = 0,
+    kSuccessConventionalWebFont = 1,
+    kSuccessVariableWebFont = 2,
+    kSuccessCbdtCblcColorFont = 3,
+    kSuccessCff2Font = 4,
+    kMaxWebFontInstantiationResult = 5
+  };
+
+  static sk_sp<SkFontMgr> DefaultFontManager();
+  static sk_sp<SkFontMgr> FreeTypeFontManager();
+
+  static void ReportWebFontInstantiationResult(WebFontInstantiationResult);
+};
+
+}  // namespace blink
---
+++ third_party/WebKit/Source/platform/fonts/opentype/FontFormatCheck.h
@@ -0,0 +1,26 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FontFormatCheck_h
+#define FontFormatCheck_h
+
+#include "platform/wtf/Vector.h"
+#include "third_party/skia/include/core/SkTypeface.h"
+
+namespace blink {
+
+class FontFormatCheck {
+ public:
+  FontFormatCheck(sk_sp<SkTypeface>);
+  bool IsVariableFont();
+  bool IsCbdtCblcColorFont();
+  bool IsCff2OutlineFont();
+
+ private:
+  Vector<SkFontTableTag> table_tags_;
+};
+
+}  // namespace blink
+
+#endif
