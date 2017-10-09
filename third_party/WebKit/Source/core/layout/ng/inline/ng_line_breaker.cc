76eb8f734dd91b317efa63f412ee5f96c0122ad7
Koji Ishii <kojii@chromium.org>
2017-10-10 00:58:55 +0900

    [LayoutNG] Implement 'text-overflow: ellipsis'
    This patch implements 'text-overflow: ellipsis' for
    LayoutNGPaintFragments. For LayoutNG without NGPaint, truncation
    works but ellipsis is not painted, and that tests do not pass.
    
    The algorithm is different from the current layout engine that
    LayoutNG computes line break with the space for ellipsis.
    NGInlineLayoutAlgorithm then appends the ellipsis to the line box.
    This difference changes the result of LTR/RTL mixed string to match
    to Edge behavior.
    
    Bug:636993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I1a76b770986e8669e0d2a94f6cee51f83acd9cd1

--- third_party/WebKit/Source/core/layout/ng/inline/ng_line_breaker.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_line_breaker.cc
@@ -123,8 +123,14 @@ bool NGLineBreaker::NextLine(const NGLogicalOffset& content_offset,
 
   // TODO(kojii): There are cases where we need to PlaceItems() without creating
   // line boxes. These cases need to be reviewed.
-  if (line_.should_create_line_box)
+  if (line_.should_create_line_box) {
+    if (!line_.CanFit() &&
+        node_.GetLayoutBlockFlow()->ShouldTruncateOverflowingText()) {
+      TruncateOverflowingText(line_info);
+    }
+
     ComputeLineLocation(line_info);
+  }
 
   return true;
 }
@@ -663,8 +669,13 @@ NGLineBreaker::LineBreakState NGLineBreaker::HandleCloseTag(
 // At this point, item_results does not fit into the current line, and there
 // are no break opportunities in item_results.back().
 void NGLineBreaker::HandleOverflow(NGLineInfo* line_info) {
+  HandleOverflow(line_info, line_.AvailableWidth(), false);
+}
+
+void NGLineBreaker::HandleOverflow(NGLineInfo* line_info,
+                                   LayoutUnit available_width,
+                                   bool force_break_anywhere) {
   NGInlineItemResults* item_results = &line_info->Results();
-  LayoutUnit available_width = line_.AvailableWidth();
   LayoutUnit width_to_rewind = line_.position - available_width;
   DCHECK_GT(width_to_rewind, 0);
 
@@ -677,8 +688,10 @@ void NGLineBreaker::HandleOverflow(NGLineInfo* line_info) {
 
     // Try to break after this item.
     if (i < item_results->size() - 1 && !item_result->prohibit_break_after) {
-      if (width_to_rewind <= 0)
+      if (width_to_rewind <= 0) {
+        line_.position = available_width + width_to_rewind;
         return Rewind(line_info, i + 1);
+      }
       break_before = i + 1;
     }
 
@@ -688,19 +701,26 @@ void NGLineBreaker::HandleOverflow(NGLineInfo* line_info) {
     DCHECK(item_result->item);
     const NGInlineItem& item = *item_result->item;
     if (item.Type() == NGInlineItem::kText && next_width_to_rewind < 0 &&
-        !item_result->no_break_opportunities_inside) {
+        (!item_result->no_break_opportunities_inside || force_break_anywhere)) {
       // When the text fits but its right margin does not, the break point
       // must not be at the end.
       LayoutUnit item_available_width =
           std::min(-next_width_to_rewind, item_result->inline_size - 1);
       SetCurrentStyle(*item.Style());
+      if (force_break_anywhere)
+        break_iterator_.SetBreakType(LineBreakType::kBreakCharacter);
       BreakText(item_result, item, item_available_width, *line_info);
       if (item_result->inline_size <= item_available_width) {
         DCHECK(item_result->end_offset < item.EndOffset() ||
                (item_result->end_offset == item.EndOffset() &&
                 item_result->has_hanging_spaces));
         DCHECK(!item_result->prohibit_break_after);
-        return Rewind(line_info, i + 1);
+        line_.position =
+            available_width + next_width_to_rewind + item_result->inline_size;
+        DCHECK_LE(i + 1, item_results->size());
+        if (i + 1 < item_results->size())
+          Rewind(line_info, i + 1);
+        return;
       }
     }
 
@@ -758,6 +778,37 @@ void NGLineBreaker::Rewind(NGLineInfo* line_info, unsigned new_end) {
   item_results->Shrink(new_end);
 
   line_info->SetIsLastLine(false);
+}
+
+// Truncate overflowing text and append ellipsis.
+void NGLineBreaker::TruncateOverflowingText(NGLineInfo* line_info) {
+  // The ellipsis is styled according to the line style.
+  const Font& font = line_info->LineStyle().GetFont();
+  const SimpleFontData* font_data = font.PrimaryFont();
+  DCHECK(font_data);
+  String ellipsis =
+      font_data && font_data->GlyphForCharacter(kHorizontalEllipsisCharacter)
+          ? String(&kHorizontalEllipsisCharacter, 1)
+          : String(u"...");
+  HarfBuzzShaper shaper(ellipsis.Characters16(), ellipsis.length());
+  RefPtr<ShapeResult> shape_result =
+      shaper.Shape(&font, line_info->BaseDirection());
+
+  // Truncate the line to (available_width - ellipsis_width) using 'line-break:
+  // anywhere'.
+  unsigned saved_item_index = item_index_;
+  unsigned saved_offset = offset_;
+  HandleOverflow(line_info,
+                 line_.AvailableWidth() - shape_result->SnappedWidth(), true);
+
+  // Restore item_index/offset to before HandleOverflow().
+  item_index_ = saved_item_index;
+  offset_ = saved_offset;
+
+  // The ellipsis should appear at the logical end of the line.
+  // This is stored seprately from other results so that it can be appended
+  // after bidi reorder.
+  line_info->SetLineEndShapeResult(std::move(shape_result));
 }
 
 void NGLineBreaker::SetCurrentStyle(const ComputedStyle& style) {
