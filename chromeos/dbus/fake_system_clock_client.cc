f9307ca5f860b0e8c6bae0dbd63f693e8c6b9796
Daria Yakovleva <yakovleva@google.com>
2017-10-09 16:28:38 +0200

    Add time synchronization to SystemClockClient
    Add time update observer to DeviceOffHoursController.
    Add dbus call to check local time synchronization
    with the network time.
    Add check time synchronization to DeviceOffHoursController.
    SystemClockLastSyncInfo is got from here: https://chromium.googlesource.com/chromiumos/platform2/+/ed23f71c21cbe9bb01ee1418592b89ebbd55c208/login_manager/session_manager_impl.cc#963
    
    Bug: 739713
    Change-Id: I23fccae4f597993929e5d78eb3749212973e779c

--- chromeos/dbus/fake_system_clock_client.cc
+++ chromeos/dbus/fake_system_clock_client.cc
@@ -3,11 +3,13 @@
 // found in the LICENSE file.
 
 #include "chromeos/dbus/fake_system_clock_client.h"
+#include "base/bind.h"
+#include "base/message_loop/message_loop.h"
+#include "base/single_thread_task_runner.h"
 
 namespace chromeos {
 
-FakeSystemClockClient::FakeSystemClockClient() {
-}
+FakeSystemClockClient::FakeSystemClockClient() : weak_ptr_factory_(this) {}
 
 FakeSystemClockClient::~FakeSystemClockClient() {
 }
@@ -16,19 +18,38 @@ void FakeSystemClockClient::Init(dbus::Bus* bus) {
 }
 
 void FakeSystemClockClient::AddObserver(Observer* observer) {
+  observers_.AddObserver(observer);
 }
 
 void FakeSystemClockClient::RemoveObserver(Observer* observer) {
+  observers_.RemoveObserver(observer);
 }
 
 bool FakeSystemClockClient::HasObserver(const Observer* observer) const {
-  return false;
+  return observers_.HasObserver(observer);
 }
 
 void FakeSystemClockClient::SetTime(int64_t time_in_seconds) {}
 
 bool FakeSystemClockClient::CanSetTime() {
   return true;
+}
+
+void FakeSystemClockClient::GetLastSyncInfo(
+    const GetLastSyncInfoCallback& callback) {
+  base::MessageLoop::current()->task_runner()->PostTask(
+      FROM_HERE, base::BindOnce(&FakeSystemClockClient::OnGetLastSyncInfo,
+                                weak_ptr_factory_.GetWeakPtr(), callback));
+}
+
+void FakeSystemClockClient::NotifyObserversSystemClockUpdated() {
+  for (auto& observer : observers_)
+    observer.SystemClockUpdated();
+}
+
+void FakeSystemClockClient::OnGetLastSyncInfo(
+    const GetLastSyncInfoCallback& callback) {
+  callback.Run(network_synchronized_);
 }
 
 }  // namespace chromeos
