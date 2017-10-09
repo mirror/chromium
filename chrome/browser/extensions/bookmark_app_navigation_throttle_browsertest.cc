601ef9b0e9f471affde0fa7ead6e82b89877d46a
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2017-10-09 14:04:58 +1100

    content/test: Introduce TestNavigationFinishedObserver
    Bug:
    Change-Id: Ic11098dd77dcdca9e3f144c9afea9eff90727348

--- chrome/browser/extensions/bookmark_app_navigation_throttle_browsertest.cc
+++ chrome/browser/extensions/bookmark_app_navigation_throttle_browsertest.cc
@@ -25,6 +25,7 @@
 #include "content/public/common/context_menu_params.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_frame_navigation_observer.h"
+#include "content/public/test/test_navigation_finished_observer.h"
 #include "content/public/test/test_utils.h"
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/notification_types.h"
@@ -71,8 +72,7 @@ content::RenderFrameHost* GetIFrame(content::WebContents* web_contents) {
 void ClickLinkAndWait(content::WebContents* web_contents,
                       const GURL& target_url,
                       LinkTarget target) {
-  ui_test_utils::UrlLoadObserver url_observer(
-      target_url, content::NotificationService::AllSources());
+  content::TestNavigationFinishedObserver observer(target_url);
   std::string script = base::StringPrintf(
       "(() => {"
       "const link = document.createElement('a');"
@@ -85,7 +85,7 @@ void ClickLinkAndWait(content::WebContents* web_contents,
       target_url.spec().c_str(),
       target == LinkTarget::SELF ? "_self" : "_blank");
   ASSERT_TRUE(content::ExecuteScript(web_contents, script));
-  url_observer.Wait();
+  observer.Wait();
 }
 
 // Creates a <form> element with a |target_url| action and |method| method. Adds
@@ -103,8 +103,7 @@ void SubmitFormAndWait(content::WebContents* web_contents,
     ASSERT_TRUE(target_url.query().empty());
   }
 
-  ui_test_utils::UrlLoadObserver url_observer(
-      target_url, content::NotificationService::AllSources());
+  content::TestNavigationFinishedObserver observer(target_url);
   std::string script = base::StringPrintf(
       "(() => {"
       "const form = document.createElement('form');"
@@ -119,15 +118,14 @@ void SubmitFormAndWait(content::WebContents* web_contents,
       target_url.spec().c_str(),
       method == net::URLFetcher::RequestType::POST ? "post" : "get");
   ASSERT_TRUE(content::ExecuteScript(web_contents, script));
-  url_observer.Wait();
+  observer.Wait();
 }
 
 // Uses |params| to navigate to a URL. Blocks until the URL is loaded.
 void NavigateToURLAndWait(chrome::NavigateParams* params) {
-  ui_test_utils::UrlLoadObserver url_observer(
-      params->url, content::NotificationService::AllSources());
+  content::TestNavigationFinishedObserver observer(params->url);
   ui_test_utils::NavigateToURL(params);
-  url_observer.Wait();
+  observer.Wait();
 }
 
 // Wrapper so that we can use base::Bind with NavigateToURL.
@@ -193,14 +191,13 @@ class BookmarkAppNavigationThrottleBrowserTest : public ExtensionBrowserTest {
 
   Browser* OpenTestBookmarkApp() {
     GURL app_url = embedded_test_server()->GetURL(kAppUrlPath);
-    ui_test_utils::UrlLoadObserver url_observer(
-        app_url, content::NotificationService::AllSources());
+    content::TestNavigationFinishedObserver observer(app_url);
 
     OpenApplication(AppLaunchParams(
         profile(), test_bookmark_app_, extensions::LAUNCH_CONTAINER_WINDOW,
         WindowOpenDisposition::CURRENT_TAB, SOURCE_CHROME_INTERNAL));
 
-    url_observer.Wait();
+    observer.Wait();
 
     return chrome::FindLastActive();
   }
@@ -601,8 +598,7 @@ IN_PROC_BROWSER_TEST_F(BookmarkAppNavigationThrottleBrowserTest,
   GURL initial_url = initial_tab->GetLastCommittedURL();
 
   const GURL in_scope_url = embedded_test_server()->GetURL(kInScopeUrlPath);
-  ui_test_utils::UrlLoadObserver url_observer(
-      in_scope_url, content::NotificationService::AllSources());
+  content::TestNavigationFinishedObserver observer(in_scope_url);
   content::ContextMenuParams params;
   params.page_url = initial_url;
   params.link_url = in_scope_url;
@@ -610,7 +606,7 @@ IN_PROC_BROWSER_TEST_F(BookmarkAppNavigationThrottleBrowserTest,
   menu.Init();
   menu.ExecuteCommand(IDC_CONTENT_CONTEXT_OPENLINKOFFTHERECORD,
                       0 /* event_flags */);
-  url_observer.Wait();
+  observer.Wait();
 
   Browser* incognito_browser = chrome::FindLastActive();
   EXPECT_EQ(incognito_browser->profile(), profile()->GetOffTheRecordProfile());
