f6a5c8f084927e143c612e301843425857e4dc8d
Gustav Sennton <gsennton@google.com>
2017-10-09 21:37:41 +0100

    [test] Remove use of deprecated DownloadManager constant.
    DownloadManager.COLUMN_LOCAL_FILENAME was deprecated in Nougat. This CL
    removes the use of that constant, by fetching the filename of a
    downloaded file using the DownloadManager.COLUMN_TITLE constant.
    
    BUG=765498
    
    Change-Id: I2a8eb008a76c5d8f6e41140dd1a4fc762da14eb7

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTestRule.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTestRule.java
@@ -76,7 +76,7 @@ public class DownloadTestRule extends ChromeActivityTestRule<ChromeActivity> {
         cursor.moveToFirst();
         boolean result = false;
         while (!cursor.isAfterLast()) {
-            if (fullPath.equals(getPathFromCursor(cursor))) {
+            if (fileName.equals(getFileNameFromCursor(cursor))) {
                 if (expectedContents != null) {
                     FileInputStream stream = new FileInputStream(new File(fullPath));
                     byte[] data = new byte[expectedContents.getBytes().length];
@@ -118,15 +118,13 @@ public class DownloadTestRule extends ChromeActivityTestRule<ChromeActivity> {
         cursor.moveToFirst();
         while (!cursor.isAfterLast()) {
             long id = cursor.getLong(idColumnIndex);
-            String fileName = getPathFromCursor(cursor);
+            String fileName = getFileNameFromCursor(cursor);
             manager.remove(id);
 
-            if (fileName != null) { // Somehow fileName can be null for some entries.
-                // manager.remove does not remove downloaded file.
-                File localFile = new File(fileName);
-                if (localFile.exists()) {
-                    localFile.delete();
-                }
+            // manager.remove does not remove downloaded file.
+            File localFile = new File(DOWNLOAD_DIRECTORY, fileName);
+            if (localFile.exists()) {
+                localFile.delete();
             }
 
             cursor.moveToNext();
@@ -137,26 +135,8 @@ public class DownloadTestRule extends ChromeActivityTestRule<ChromeActivity> {
     /**
      * Retrieve the path of the downloaded file from a DownloadManager cursor.
      */
-    private String getPathFromCursor(Cursor cursor) {
-        int columnId = cursor.getColumnIndex("local_filename");
-        return cursor.getString(columnId);
-    }
-
-    private String getPathForDownload(long downloadId) {
-        DownloadManager manager =
-                (DownloadManager) getActivity().getSystemService(Context.DOWNLOAD_SERVICE);
-        DownloadManager.Query query = new DownloadManager.Query();
-        query.setFilterById(downloadId);
-        Cursor cursor = null;
-        try {
-            cursor = manager.query(query);
-            if (!cursor.moveToFirst()) {
-                return null;
-            }
-            return getPathFromCursor(cursor);
-        } finally {
-            if (cursor != null) cursor.close();
-        }
+    private String getFileNameFromCursor(Cursor cursor) {
+        return cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_TITLE));
     }
 
     private String mLastDownloadFilePath;
