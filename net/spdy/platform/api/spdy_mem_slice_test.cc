a5d00bcbb8fc4fb1902c7b91d97b361907df4053
Dianna Hu <diannahu@chromium.org>
2017-10-10 10:26:41 -0400

    Add SpdyMemSlice. Not used and not protected.
    This CL lands server change 170485623 by yasong
    and 171215460 by diannahu.
    
    BUG=488484
    
    Change-Id: I74f8b358de54466724ccd341b1e5c1d92436cba4

---
+++ net/spdy/platform/api/spdy_mem_slice_test.cc
@@ -0,0 +1,48 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/spdy/platform/api/spdy_mem_slice.h"
+
+#include <utility>
+
+#include "net/spdy/platform/api/spdy_test.h"
+
+namespace net {
+namespace test {
+namespace {
+
+class SpdyMemSliceTest : public SpdyTest {
+ public:
+  SpdyMemSliceTest() {
+    size_t length = 1024;
+    slice_ = SpdyMemSlice(length);
+    orig_data_ = slice_.data();
+    orig_length_ = slice_.length();
+  }
+
+  SpdyMemSlice slice_;
+  const char* orig_data_;
+  size_t orig_length_;
+};
+
+TEST_F(SpdyMemSliceTest, MoveConstruct) {
+  SpdyMemSlice moved(std::move(slice_));
+  EXPECT_EQ(moved.data(), orig_data_);
+  EXPECT_EQ(moved.length(), orig_length_);
+  EXPECT_EQ(nullptr, slice_.data());
+  EXPECT_EQ(0u, slice_.length());
+}
+
+TEST_F(SpdyMemSliceTest, MoveAssign) {
+  SpdyMemSlice moved;
+  moved = std::move(slice_);
+  EXPECT_EQ(moved.data(), orig_data_);
+  EXPECT_EQ(moved.length(), orig_length_);
+  EXPECT_EQ(nullptr, slice_.data());
+  EXPECT_EQ(0u, slice_.length());
+}
+
+}  // namespace
+}  // namespace test
+}  // namespace net
