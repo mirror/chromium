a04ab12b504d54e1ff7640027a67ceb4185e222e
Lalit Maganti <lalitm@chromium.org>
2017-10-10 15:13:24 +0100

    memory-infra: mark nodes as weak if corresponding dumps are weak
    Bug: 768373
    Change-Id: I6926d906ee8bf4dcdb8e57c9bbcb46615102dd9f

--- services/resource_coordinator/memory_instrumentation/graph_unittest.cc
+++ services/resource_coordinator/memory_instrumentation/graph_unittest.cc
@@ -42,12 +42,16 @@ TEST(ProcessTest, CreateAndFindNode) {
   GlobalDumpGraph global_dump_graph;
   Process graph(&global_dump_graph);
 
-  Node* first = graph.CreateNode(MemoryAllocatorDumpGuid(1), "simple/test/1");
-  Node* second = graph.CreateNode(MemoryAllocatorDumpGuid(2), "simple/test/2");
-  Node* third = graph.CreateNode(MemoryAllocatorDumpGuid(3), "simple/other/1");
-  Node* fourth = graph.CreateNode(MemoryAllocatorDumpGuid(4), "complex/path");
-  Node* fifth =
-      graph.CreateNode(MemoryAllocatorDumpGuid(5), "complex/path/child/1");
+  Node* first =
+      graph.CreateNode(MemoryAllocatorDumpGuid(1), "simple/test/1", false);
+  Node* second =
+      graph.CreateNode(MemoryAllocatorDumpGuid(2), "simple/test/2", false);
+  Node* third =
+      graph.CreateNode(MemoryAllocatorDumpGuid(3), "simple/other/1", false);
+  Node* fourth =
+      graph.CreateNode(MemoryAllocatorDumpGuid(4), "complex/path", false);
+  Node* fifth = graph.CreateNode(MemoryAllocatorDumpGuid(5),
+                                 "complex/path/child/1", false);
 
   ASSERT_EQ(graph.FindNode("simple/test/1"), first);
   ASSERT_EQ(graph.FindNode("simple/test/2"), second);
