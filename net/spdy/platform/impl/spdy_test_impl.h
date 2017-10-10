6e91e9f465b66ab2f89a35c8e6c9311d7e3e2dd1
Dianna Hu <diannahu@chromium.org>
2017-10-10 11:32:08 -0400

    Add SpdyMemSlice. Not used and not protected.
    This CL lands server change 170485623 by yasong
    and 171215460 by diannahu.
    
    BUG=488484
    
    Change-Id: I74f8b358de54466724ccd341b1e5c1d92436cba4

---
+++ net/spdy/platform/impl/spdy_test_impl.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_SPDY_PLATFORM_IMPL_SPDY_TEST_IMPL_H_
+#define NET_SPDY_PLATFORM_IMPL_SPDY_TEST_IMPL_H_
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+using SpdyTestImpl = ::testing::Test;
+
+template <class T>
+using SpdyTestWithParamImpl = ::testing::TestWithParam<T>;
+
+#endif  // NET_SPDY_PLATFORM_IMPL_SPDY_TEST_IMPL_H_
