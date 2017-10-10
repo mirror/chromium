9de1cef9e82b890348f9924f33a66362339cc3b2
Lalit Maganti <lalitm@chromium.org>
2017-10-10 16:58:08 +0100

    memory-infra: mark nodes transitively as weak
    Bug: 768373
    Change-Id: I9e61aa7231e68d15fcf3e0d72e8ed938545d0638

--- services/resource_coordinator/memory_instrumentation/graph.h
+++ services/resource_coordinator/memory_instrumentation/graph.h
@@ -40,6 +40,8 @@ class GlobalDumpGraph {
     // if no such node exists in the provided |graph|.
     GlobalDumpGraph::Node* FindNode(base::StringPiece path);
 
+    GlobalDumpGraph::Node* root() { return root_; }
+
    private:
     GlobalDumpGraph* const global_graph_;
     GlobalDumpGraph::Node* root_;
@@ -107,8 +109,9 @@ class GlobalDumpGraph {
     bool is_explicit() const { return explicit_; }
     void set_explicit(bool explicit_node) { explicit_ = explicit_node; }
     GlobalDumpGraph::Edge* owns_edge() const { return owns_edge_; }
-    const std::vector<GlobalDumpGraph::Edge*>& owned_by_edges() const {
-      return owned_by_edges_;
+    std::map<std::string, Node*>* children() { return &children_; }
+    std::vector<GlobalDumpGraph::Edge*>* owned_by_edges() {
+      return &owned_by_edges_;
     }
     const GlobalDumpGraph::Process* dump_graph() const { return dump_graph_; }
     const std::map<std::string, Entry>& entries() const { return entries_; }
