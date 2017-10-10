92a5c54406bc9ca65c278deb0bc22437aac2d387
Peng Huang <penghuang@chromium.org>
2017-10-10 12:55:29 -0400

    Enable arc video encode and decoder accelerator in mus.
    Bug: 769755
    Change-Id: Id7580a385c5203e80bc9a15980853fef6e33ac26

--- chrome/browser/embedded_ui_service_info_factory.cc
+++ chrome/browser/embedded_ui_service_info_factory.cc
@@ -5,11 +5,74 @@
 #include "chrome/browser/embedded_ui_service_info_factory.h"
 
 #include "base/message_loop/message_loop.h"
+#include "chrome/gpu/gpu_arc_video_decode_accelerator.h"
+#include "chrome/gpu/gpu_arc_video_encode_accelerator.h"
+#include "components/arc/common/video_decode_accelerator.mojom.h"
+#include "components/arc/common/video_encode_accelerator.mojom.h"
 #include "content/public/browser/discardable_shared_memory_manager.h"
+#include "content/public/common/service_manager_connection.h"
+#include "mojo/public/cpp/bindings/strong_binding.h"
+#include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/ui/public/interfaces/constants.mojom.h"
 #include "services/ui/service.h"
 
 namespace {
+
+class UIService : public service_manager::ForwardingService {
+ public:
+  explicit UIService(const ui::Service::InProcessConfig* config)
+      : service_manager::ForwardingService(&ui_service_), ui_service_(config) {}
+  ~UIService() override {}
+
+ private:
+  void OnStart() override {
+    registry_.AddInterface<arc::mojom::VideoDecodeAccelerator>(base::Bind(
+        &UIService::BindVideoDecodeAcceleratorRequest, base::Unretained(this)));
+    registry_.AddInterface<arc::mojom::VideoEncodeAccelerator>(base::Bind(
+        &UIService::BindVideoEncodeAcceleratorRequest, base::Unretained(this)));
+    service_manager::ForwardingService::OnStart();
+  }
+
+  void OnBindInterface(const service_manager::BindSourceInfo& source_info,
+                       const std::string& interface_name,
+                       mojo::ScopedMessagePipeHandle interface_pipe) override {
+    if (registry_.TryBindInterface(interface_name, &interface_pipe))
+      return;
+    service_manager::ForwardingService::OnBindInterface(
+        source_info, interface_name, std::move(interface_pipe));
+  }
+
+  bool OnServiceManagerConnectionLost() override {
+    return service_manager::ForwardingService::OnServiceManagerConnectionLost();
+  }
+
+  void BindVideoDecodeAcceleratorRequest(
+      arc::mojom::VideoDecodeAcceleratorRequest request) {
+    mojo::MakeStrongBinding(
+        base::MakeUnique<chromeos::arc::GpuArcVideoDecodeAccelerator>(
+            gpu_preferences_),
+        std::move(request));
+  }
+
+  void BindVideoEncodeAcceleratorRequest(
+      arc::mojom::VideoEncodeAcceleratorRequest request) {
+    mojo::MakeStrongBinding(
+        base::MakeUnique<chromeos::arc::GpuArcVideoEncodeAccelerator>(
+            gpu_preferences_),
+        std::move(request));
+  }
+
+  ui::Service ui_service_;
+  service_manager::BinderRegistry registry_;
+
+  // TODO(penghuang): Correctly initialize gpu::GpuPreferences (like it is
+  // initialized
+  // in GpuProcessHost::Init()).
+
+  gpu::GpuPreferences gpu_preferences_;
+
+  DISALLOW_COPY_AND_ASSIGN(UIService);
+};
 
 std::unique_ptr<service_manager::Service> CreateEmbeddedUIService(
     const scoped_refptr<base::SingleThreadTaskRunner>& task_runner,
@@ -19,7 +82,7 @@ std::unique_ptr<service_manager::Service> CreateEmbeddedUIService(
   config.resource_runner = task_runner;
   config.image_cursors_set_weak_ptr = image_cursors_set_weak_ptr;
   config.memory_manager = memory_manager;
-  return base::MakeUnique<ui::Service>(&config);
+  return std::make_unique<UIService>(&config);
 }
 
 }  // namespace
