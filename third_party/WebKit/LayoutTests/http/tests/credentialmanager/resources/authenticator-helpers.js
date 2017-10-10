5e717bd6f9a907ec5aecd37657cf0a668d32db7e
Kim Paulhamus <kpaulhamus@chromium.org>
2017-10-10 14:42:28 -0700

    Hook WebAuthN to //device/u2f to perform credential registration.
    This change implements the browser-side of authenticator.mojom to
    build and send a U2F-style request to //device/u2f to talk to a
    hardware authenticator. This also adds a utility class to map the u2f
    response from U2fDevice to a CTAP response per the FIDO2 and WebAuthN specs.
    
    Bug: 664630
    Change-Id: I7d5f9e2308c45fdd8ca9463a8a78c2f88d9aeb80

--- third_party/WebKit/LayoutTests/http/tests/credentialmanager/resources/authenticator-helpers.js
+++ third_party/WebKit/LayoutTests/http/tests/credentialmanager/resources/authenticator-helpers.js
@@ -16,6 +16,7 @@ class MockAuthenticator {
 
   // Returns a PublicKeyCredentialInfo to the client.
   async makeCredential(options) {
+    console.log("makeCredential mock was called with options: ", options);
     var info = null;
     if (this.status_ == webauth.mojom.AuthenticatorStatus.SUCCESS) {
       let response = new webauth.mojom.AuthenticatorResponse(
@@ -39,7 +40,7 @@ class MockAuthenticator {
 
   // Resets state of mock Authenticator.
   reset() {
-    this.status_ = webauth.mojom.AuthenticatorStatus.NOT_IMPLEMENTED;
+    this.status_ = webauth.mojom.AuthenticatorStatus.UNKNOWN_ERROR;
     this.id_ = null;
     this.rawId_ = new Uint8Array(0);
     this.clientDataJson_ = new Uint8Array(0);
