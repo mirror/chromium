0abd76be005451477cb34d8f3b7e5acb0ebf8bc7
Will Chen <chenwilliam@chromium.org>
2017-10-10 13:40:37 -0700

    Layout Tests: add --watch to quickly re-run the same test
    Oftentimes, a developer is fixing a failing test and needs to re-run
    it several times in a row as they iterate on it. Right now, the startup
    time of the layout test python test harness is ~2.5 seconds on a Linux workstation.
    
    Typically in a test framework, the watch mode is triggered upon a change in the test
    file, but watching filesystem changes is platform-specific and could be buggier
    whereas asking the user to retry a test is simple to implement and covers more
    cases (e.g. they might be changing production code to make a layout test pass).
    
    Usage: --watch or -w
    
    Example:
    ```
    Found 1 test; running 1, skipping 0.
    Running 1 content_shell.                                                                         
    
    Finished running tests
    Interactive watch mode: (q)uit (r)etry
    Command: r
    Running 1 content_shell.
    ```
    
    Bug: 770751
    Change-Id: I0a5e0648717ee6031dd804490677fb8576a58174

--- third_party/WebKit/Tools/Scripts/webkitpy/common/system/user_mock.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/system/user_mock.py
@@ -29,6 +29,7 @@
 import logging
 
 _log = logging.getLogger(__name__)
+canned_responses = ['Mock user response']
 
 
 class MockUser(object):
@@ -38,7 +39,12 @@ class MockUser(object):
 
     @classmethod
     def prompt(cls, message, repeat=1, raw_input=raw_input):
-        return 'Mock user response'
+        return canned_responses.pop(0)
+
+    @classmethod
+    def set_canned_responses(cls, responses):
+        global canned_responses
+        canned_responses = responses
 
     @classmethod
     def prompt_with_list(cls, list_title, list_items, can_choose_multiple=False, raw_input=raw_input):
