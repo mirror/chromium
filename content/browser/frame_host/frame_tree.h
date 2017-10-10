e8d105c7191b0ccc786d0f75206426b74616a627
Balazs Engedy <engedy@chromium.org>
2017-10-10 17:32:54 +0200

    Provide InterfaceProvider to RFs at creation time.
    Every RenderFrameHost exposes an InterfaceProvider to its corresponding
    RenderFrame, which is used by content/renderer/, Blink and WebUI JS code
    to access frame-scoped services implemented by the RFH. Previously, the
    InterfaceProvider was retrieved asynchronously by the RenderFrame through
    the FrameHostInterfaceBroker shortly after its construction.
    
    This CL removes the FrameHostInterfaceBroker interface entirely, and
    instead provides the InterfaceProvider interface to RenderFrames at
    construction time. Depending on how a RenderFrame is created, the client
    end of the interface is plumbed through one of the following routes:
    
     1) For main frames whose creation is triggered from the browser side,
        the RenderViewHost takes care of sending down the client end of
        the InterfaceProvider interface as part of CreateViewParams and
        plumbing it to the main RenderFrame.
    
     2) For main frames whose creation is triggered from the renderer side
        using window.open, the client end of the InterfaceProvider interface
        is sent down as part of CreateNewWindowReply and plumbed to the new
        main RenderFrame.
    
     3) For child frames, the client end of the InterfaceProvider interface
        is sent down as a naked MessagePipeHandle in the response to the
        synchronous FrameHostMsg_CreateChildFrame legacy IPC.
    
     4) For out-of-process child frames, the RenderFrameHost takes care of
        sending down the client end of InterfaceProvider interface to the
        renderer as part of CreateFrameParams.
    
    Bug: 729021
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I1b3d113266763a1351018f19b7d7e584fd058a76

--- content/browser/frame_host/frame_tree.h
+++ content/browser/frame_host/frame_tree.h
@@ -17,6 +17,7 @@
 #include "base/macros.h"
 #include "content/browser/frame_host/frame_tree_node.h"
 #include "content/common/content_export.h"
+#include "services/service_manager/public/interfaces/interface_provider.mojom.h"
 
 namespace content {
 
@@ -43,6 +44,8 @@ class RenderWidgetHostDelegate;
 class CONTENT_EXPORT FrameTree {
  public:
   class NodeRange;
+  using InterfaceProviderRequest =
+      service_manager::mojom::InterfaceProviderRequest;
 
   class CONTENT_EXPORT NodeIterator
       : public std::iterator<std::forward_iterator_tag, FrameTreeNode> {
@@ -121,9 +124,14 @@ class CONTENT_EXPORT FrameTree {
   // Adds a new child frame to the frame tree. |process_id| is required to
   // disambiguate |new_routing_id|, and it must match the process of the
   // |parent| node. Otherwise no child is added and this method returns false.
+  // |interfaces_request| is the request end of the InterfaceProvider
+  // interface through which the child RenderFrame can access Mojo services
+  // exposed by the corresponding RenderFrameHost. The caller takes care of
+  // sending the client end of the interface down to the RenderFrame.
   bool AddFrame(FrameTreeNode* parent,
                 int process_id,
                 int new_routing_id,
+                InterfaceProviderRequest interfaces_request,
                 blink::WebTreeScopeType scope,
                 const std::string& frame_name,
                 const std::string& frame_unique_name,
