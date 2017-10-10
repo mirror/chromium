756c87dc1b851c147bffccbcab7792380d08bb16
Thanh Le <thanhdle@chromium.org>
2017-10-10 10:08:19 -0700

    Posting Previews navigations to chrome://interventions-internals.
    Screenshots of the basic ui:
    https://drive.google.com/open?id=0B_MBksOU85S3R0R0ano1OVF0eXc
    https://drive.google.com/open?id=0B_MBksOU85S3eW1vcUtYaW5LdzQ
    
    Bug: 764418
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Id7f79ef2295b9d22eccc12c8ea46812f09373eca

--- components/previews/core/previews_logger_unittest.cc
+++ components/previews/core/previews_logger_unittest.cc
@@ -2,13 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/previews/core/previews_log.h"
+#include "components/previews/core/previews_logger.h"
 
 #include <vector>
 
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/time/time.h"
+#include "components/previews/core/previews_logger_observer.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace previews {
@@ -18,7 +19,37 @@ namespace {
 const char kPreviewsNavigationEventType[] = "Navigation";
 const size_t kMaximumMessageLogs = 10;
 
-}  // namespace
+// Mock class to test correct MessageLog is passed back to the
+// mojo::InterventionsInternalsPagePtr.
+class TestPreviewsLoggerObserver : public PreviewsLoggerObserver {
+ public:
+  TestPreviewsLoggerObserver()
+      : new_message_log_added_called_(0), message_(nullptr) {}
+
+  ~TestPreviewsLoggerObserver() override {}
+
+  // PreviewsLoggerObserver:
+  void OnNewMessageLogAdded(
+      const PreviewsLogger::MessageLog& message) override {
+    new_message_log_added_called_++;
+    message_ = base::MakeUnique<PreviewsLogger::MessageLog>(message);
+  }
+
+  // Check if the mocked OnNewMessageLogAdded is called.
+  size_t OnNewMessageLogAddedCalls() const {
+    return new_message_log_added_called_;
+  }
+
+  // Expose the passed in MessageLog for testing.
+  PreviewsLogger::MessageLog* message() const { return message_.get(); }
+
+ private:
+  // Keep count of the number of time OnNewMessageLogAdded is called.
+  size_t new_message_log_added_called_;
+
+  // The passed in MessageLog in OnNewMessageLogAdded.
+  std::unique_ptr<PreviewsLogger::MessageLog> message_;
+};
 
 class PreviewsLoggerTest : public testing::Test {
  public:
@@ -115,5 +146,57 @@ TEST_F(PreviewsLoggerTest, PreviewsLoggerOnlyKeepsCertainNumberOfMessageLogs) {
 
   EXPECT_EQ(kMaximumMessageLogs, logger_->log_messages().size());
 }
+
+TEST_F(PreviewsLoggerTest, ObserverIsNotifiedOfHistoricalMessagesWhenAdded) {
+  std::unique_ptr<TestPreviewsLoggerObserver> observer =
+      base::MakeUnique<TestPreviewsLoggerObserver>();
+
+  const PreviewsLogger::MessageLog expected_messages[] = {
+      PreviewsLogger::MessageLog("Event_a", "Some description a",
+                                 GURL("http://www.url_a.com/url_a"),
+                                 base::Time::Now()),
+      PreviewsLogger::MessageLog("Event_b", "Some description b",
+                                 GURL("http://www.url_b.com/url_b"),
+                                 base::Time::Now()),
+      PreviewsLogger::MessageLog("Event_c", "Some description c",
+                                 GURL("http://www.url_c.com/url_c"),
+                                 base::Time::Now()),
+  };
+
+  for (auto message : expected_messages) {
+    logger_->LogMessage(message.event_type, message.event_description,
+                        message.url, message.time);
+  }
+  logger_->AddAndNotifyObserver(observer.get());
+
+  const size_t expected_times = 3;
+  EXPECT_EQ(expected_times, observer->OnNewMessageLogAddedCalls());
+}
+
+TEST_F(PreviewsLoggerTest, ObserversOnNewMessageIsCalledWithCorrectParams) {
+  TestPreviewsLoggerObserver observers[3];
+
+  const size_t number_of_obs = 3;
+  for (size_t i = 0; i < number_of_obs; i++) {
+    logger_->AddAndNotifyObserver(&observers[i]);
+  }
+
+  std::string type = "Event";
+  std::string description = "Some description";
+  GURL url("http://www.url_.com/url_");
+  base::Time now = base::Time::Now();
+  logger_->LogMessage(type, description, url, now);
+
+  const size_t expected_times = 1;
+  for (size_t i = 0; i < number_of_obs; i++) {
+    EXPECT_EQ(expected_times, observers[i].OnNewMessageLogAddedCalls());
+    EXPECT_EQ(type, observers[i].message()->event_type);
+    EXPECT_EQ(description, observers[i].message()->event_description);
+    EXPECT_EQ(url, observers[i].message()->url);
+    EXPECT_EQ(now, observers[i].message()->time);
+  }
+}
+
+}  // namespace
 
 }  // namespace previews
