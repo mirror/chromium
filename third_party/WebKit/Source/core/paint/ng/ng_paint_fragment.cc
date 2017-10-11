d7200bac45349a64c491d7b9961acb96b97205bb
Koji Ishii <kojii@chromium.org>
2017-10-11 16:34:57 +0900

    [LayoutNG] Compute NGPaintFragment::VisualRect
    This patch changes NGPaintFragment::VisualRect() to use the computed
    results from PrePaint.
    
    This fixes VisualRect() for block-level boxes. Inline-level boxes,
    including text boxes, are computed too, but only after invalidations.
    This needs more work.
    
    This patch turns 7 tests in fast/text to pass with
    LayoutNGPaintFragments flag enabled.
    
    Design doc:
    https://docs.google.com/a/chromium.org/document/d/1qzuRg7pMDxHXbOfVXyH7ybGVhX_z4Jt2kOJSDT11jiY/edit?usp=sharing
    
    Bug:714962
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I694e5ac6b655732f9f99bbceccb8fe10e385dbe7

--- third_party/WebKit/Source/core/paint/ng/ng_paint_fragment.cc
+++ third_party/WebKit/Source/core/paint/ng/ng_paint_fragment.cc
@@ -4,6 +4,9 @@
 
 #include "core/paint/ng/ng_paint_fragment.h"
 
+#include "core/layout/LayoutObject.h"
+#include "core/layout/ng/geometry/ng_physical_rect.h"
+#include "core/layout/ng/inline/ng_physical_text_fragment.h"
 #include "core/layout/ng/ng_physical_container_fragment.h"
 #include "core/layout/ng/ng_physical_fragment.h"
 #include "platform/wtf/PtrUtil.h"
@@ -13,24 +16,81 @@ namespace blink {
 NGPaintFragment::NGPaintFragment(RefPtr<const NGPhysicalFragment> fragment)
     : physical_fragment_(std::move(fragment)) {
   DCHECK(physical_fragment_);
-  PopulateDescendants();
 }
 
-// Populate descendant NGPaintFragment from NGPhysicalFragment tree.
-void NGPaintFragment::PopulateDescendants() {
-  if (PhysicalFragment().IsContainer()) {
-    const NGPhysicalContainerFragment& fragment =
-        ToNGPhysicalContainerFragment(PhysicalFragment());
-    children_.ReserveCapacity(fragment.Children().size());
-    for (const auto& child_fragment : fragment.Children()) {
-      auto child = WTF::MakeUnique<NGPaintFragment>(child_fragment);
-      children_.push_back(std::move(child));
+void NGPaintFragment::UpdateVisualRect() {
+  if (!children_.IsEmpty())
+    return;
+
+  UpdateVisualRect({nullptr});
+}
+
+void NGPaintFragment::UpdateVisualRect(const UpdateContext& context) {
+  const NGPhysicalFragment& fragment = PhysicalFragment();
+  switch (fragment.Type()) {
+    case NGPhysicalFragment::kFragmentBox: {
+      // For box fragments, LayoutBox has the correct LocalVisualRect() and
+      // that VisualRect() is set correctly by PaintInvalidator.
+      // Copy the VisualRect from the LayoutObject.
+      LayoutObject* layout_object = fragment.GetLayoutObject();
+      DCHECK(layout_object);
+      if (layout_object == context.parent_box) {
+        // If this is an anonymous fragment; i.e., does not exist in
+        // LayoutObject tree, it shouldn't paint anything.
+        // Use the same VisualRect() as its parent box.
+        SetVisualRect(layout_object->VisualRect());
+        PopulateDescendants(
+            {layout_object, context.offset_to_parent_box + fragment.Offset()});
+      } else {
+        // TODO(kojii): For LayoutInline, LocalVisualRect() walks InlineBox
+        // tree, and thus incorrect at this point.
+        SetVisualRect(layout_object->VisualRect());
+        PopulateDescendants({layout_object});
+      }
+      return;
+    }
+
+    case NGPhysicalFragment::kFragmentText: {
+      // TODO(kojii): This code computes VisualRect for this text fragment, but
+      // 1) does not update LayoutText::VisualRect(), and 2) is too late because
+      // paint invalidation is done.
+      NGPhysicalRect visual_rect =
+          ToNGPhysicalTextFragment(fragment).LocalVisualRect();
+      DCHECK(context.parent_box);
+      visual_rect.location +=
+          fragment.Offset() + context.offset_to_parent_box +
+          NGPhysicalOffset(context.parent_box->PaintOffset());
+      SetVisualRect(visual_rect.ToLayoutRect());
+      return;
+    }
+
+    case NGPhysicalFragment::kFragmentLineBox: {
+      // Line box fragments do not paint anything.
+      NGPhysicalOffset offset =
+          context.offset_to_parent_box + fragment.Offset();
+      SetVisualRect({offset.left, offset.top, fragment.Size().width,
+                     fragment.Size().height});
+      PopulateDescendants({context.parent_box,
+                           context.offset_to_parent_box + fragment.Offset()});
+      return;
     }
   }
-  // TODO(kojii): Do some stuff to accumulate visual rects and convert to paint
-  // coordinates.
-  // TODO(kojii): This is still quite worng, revisit in following patch.
-  SetVisualRect({{}, PhysicalFragment().Size().ToLayoutSize()});
+  NOTREACHED();
+}
+
+// Populate descendants from NGPhysicalFragment tree.
+void NGPaintFragment::PopulateDescendants(const UpdateContext& context) {
+  DCHECK(children_.IsEmpty());
+  DCHECK(PhysicalFragment().IsContainer());
+
+  const NGPhysicalContainerFragment& fragment =
+      ToNGPhysicalContainerFragment(PhysicalFragment());
+  children_.ReserveCapacity(fragment.Children().size());
+  for (const auto& child_fragment : fragment.Children()) {
+    auto child = WTF::MakeUnique<NGPaintFragment>(child_fragment);
+    child->UpdateVisualRect(context);
+    children_.push_back(std::move(child));
+  }
 }
 
 }  // namespace blink
