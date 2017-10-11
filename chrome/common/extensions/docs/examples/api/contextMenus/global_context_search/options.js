a728e964a9e91e50b01e89a5123cc4e06a546578
crystallambert@chromium.org <crystallambert@chromium.org>
2017-10-11 10:54:31 -0700

    Sample extension that works with context menus, non persistent background script s, and options page. Calls chrome.contextMenu.remove() and chrome.runtime.onInstalled listener event.
    Bug:
    Change-Id: Iaa785dabe474dc94bb09f02fa87359700f63ddf4

---
+++ chrome/common/extensions/docs/examples/api/contextMenus/global_context_search/options.js
@@ -0,0 +1,45 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function createForm() {
+  let removed = []
+  chrome.storage.sync.get(['removedContextMenu'], function(list) {
+    if (list.removedContextMenu) {
+      removed = list.removedContextMenu;
+    } else {
+      removed.push('null');
+    }
+    let form = document.getElementById('form');
+    for (let key of Object.keys(countryLocales)) {
+      let div = document.createElement('div');
+      let checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.checked = true;
+      if (removed.includes(key)){
+          checkbox.checked = false;
+      }
+      checkbox.name = key;
+      checkbox.value = countryLocales[key];
+      let span = document.createElement('span');
+      span.textContent = countryLocales[key];
+      div.appendChild(checkbox);
+      div.appendChild(span);
+      form.appendChild(div);
+    }
+  });
+}
+
+createForm();
+
+document.getElementById('optionsSubmit').onclick = function() {
+  let checkboxes = document.getElementsByTagName('input');
+  let removed = [];
+    for(i=0; i<checkboxes.length; i++) {
+      if (checkboxes[i].checked == false) {
+        removed.push(checkboxes[i].name);
+        }
+      }
+  chrome.storage.sync.set({removedContextMenu: removed});
+  window.close();
+}
