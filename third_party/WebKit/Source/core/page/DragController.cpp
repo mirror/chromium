49862c3ecdeeeefd1eb5f80991d5a58805586603
Hugo Holgersson <hugoh@vewd.com>
2017-10-11 11:20:51 +0200

    Make links draggable despite 'user-select: text' styling
    Problem:
     When a Text node, "hey" is within a link that is within
     |user-select: text|,
    
       <div style="user-select: text"><a href="www">hey</a></div>
    
     The link text, "hey" should be draggable despite
     |user-select: text|.
    
    Background:
     The bug surfaced because Node::CanStartSelection
     returns true for any node that is within a |user-select: text|
     sub tree. That made DragController::DraggableNode make
     the wrong decision (it returned nullptr for start_node = "hey").
    
    Solution:
     With a new helper, IsDraggableText we check if a dragged
     Text node is a draggable.
    
    Note:
     There can only be one Text node in the ancestor chain
     (because Text nodes cannot have children) so
     IsDraggableText will only be called once.
    
    Bug: 771573
    Change-Id: Id482cf0888cd428d0bc9a12bff535743c7138df8

--- third_party/WebKit/Source/core/page/DragController.cpp
+++ third_party/WebKit/Source/core/page/DragController.cpp
@@ -781,6 +781,22 @@ bool DragController::TryDHTMLDrag(DragData* drag_data,
   return true;
 }
 
+bool IsDraggableText(const Node& node) {
+  if (!node.IsTextNode())
+    return false;
+
+  // Editable elements loose their draggability,
+  // see https://github.com/whatwg/html/issues/3114.
+  if (HasEditableStyle(node))
+    return false;
+
+  for (Node& node : NodeTraversal::InclusiveAncestorsOf(node)) {
+    if (node.IsHTMLElement() && ToHTMLElement(&node)->draggable())
+      return true;
+  }
+  return false;
+}
+
 Node* DragController::DraggableNode(const LocalFrame* src,
                                     Node* start_node,
                                     const IntPoint& drag_origin,
@@ -804,11 +820,11 @@ Node* DragController::DraggableNode(const LocalFrame* src,
       // skip over them for the purposes of finding a draggable node.
       continue;
     }
-    if (drag_type != kDragSourceActionSelection && node->IsTextNode() &&
+    if (drag_type != kDragSourceActionSelection && !IsDraggableText(*node) &&
         node->CanStartSelection()) {
-      // In this case we have a click in the unselected portion of text. If this
-      // text is selectable, we want to start the selection process instead of
-      // looking for a parent to try to drag.
+      // We have a click in an unselected, selectable text that is not
+      // draggable... so we want to start the selection process instead
+      // of looking for a parent to try to drag.
       return nullptr;
     }
     if (node->IsElementNode()) {
