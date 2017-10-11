3ff95b9de8dd81dc51b8bd3f4759b0de70e73e9c
Pete Williamson <petewil@chromium.org>
2017-10-11 14:40:13 -0700

    Report very old tasks from all states.
    Due to potential bugs, it could come to pass that we have tasks
    that are waiting on an event that they missed or isn't going to
    happen.  If we find any tasks like that, report on them once they
    are more than one week old.
    
    Bug: 701939
    Change-Id: Ia67c31579e8bfbce5b52d04afb517b881f49af30

--- components/offline_pages/core/prefetch/stale_entry_finalizer_task.cc
+++ components/offline_pages/core/prefetch/stale_entry_finalizer_task.cc
@@ -7,6 +7,7 @@
 #include <array>
 
 #include "base/bind.h"
+#include "base/metrics/histogram_macros.h"
 #include "components/offline_pages/core/offline_store_utils.h"
 #include "components/offline_pages/core/prefetch/prefetch_dispatcher.h"
 #include "components/offline_pages/core/prefetch/prefetch_downloader.h"
@@ -113,6 +114,23 @@ bool FinalizeFutureItems(PrefetchItemState state,
   return statement.Run();
 }
 
+// If there is a bug in our code, an item might be stuck in the queue waiting
+// on an event that didn't happen.  If so, report that item.
+void ReportStuckItems(base::Time now, sql::Connection* db) {
+  static constexpr char kSql[] =
+      "SELECT state FROM prefetch_items"
+      " WHERE creation_time < ?";
+  const int64_t earliest_valid_creation_time =
+      store_utils::ToDatabaseTime(now - base::TimeDelta::FromDays(7));
+  sql::Statement statement(db->GetCachedStatement(SQL_FROM_HERE, kSql));
+  statement.BindInt64(0, earliest_valid_creation_time);
+
+  while (statement.Step()) {
+    UMA_HISTOGRAM_SPARSE_SLOWLY("OfflinePages.Prefetching.StuckItemState",
+                                statement.ColumnInt(0));
+  }
+}
+
 Result FinalizeStaleEntriesSync(StaleEntryFinalizerTask::NowGetter now_getter,
                                 sql::Connection* db) {
   if (!db)
@@ -139,6 +157,10 @@ Result FinalizeStaleEntriesSync(StaleEntryFinalizerTask::NowGetter now_getter,
     if (!FinalizeFutureItems(state, now, db))
       return Result::NO_MORE_WORK;
   }
+
+  // Items could also be stuck in a non-expirable state due to a bug, clean them
+  // too.
+  ReportStuckItems(now, db);
 
   Result result = Result::MORE_WORK_NEEDED;
   if (!MoreWorkInQueue(db))
