ca90c33771481f3d42cb72219846950a01bb28b0
Dmitry Titov <dimich@chromium.org>
2017-10-10 20:09:19 -0700

    Add UMA collection for the prefetch daily stats: - was enabled for prefetch and Zine and received new URLs for prefetch - Prefetched a page - Opened a prefetched page The data is collected during a day and accumulated in daily persistent counters that are periodically reported to UMA.
    Bug:701939
    Change-Id: I1a67f8bf0daeca3022485293daca7cdfffefeba4

--- components/ntp_tiles/popular_sites_impl.cc
+++ components/ntp_tiles/popular_sites_impl.cc
@@ -11,6 +11,7 @@
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
+#include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
