44104e226fe7b5890c44b4159ebdcaa5ce546421
Yoland Yan <yolandyan@chromium.org>
2017-10-11 21:46:56 +0000

    Revert "Reland "VR: Move focus/secure origin checks to chrome/browser/vr/service""
    This reverts commit 7f6e637b7411df4addc7477b22adbe62ba8c4dc3.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Reland "VR: Move focus/secure origin checks to chrome/browser/vr/service"
    > 
    > Original change's description:
    > > Revert "VR: Move focus/secure origin checks to chrome/browser/vr/service"
    > > 
    > > This reverts commit de44e2eadbf08ab3dd285236cd88a8907b4b6869.
    > > 
    > > Reason for revert: Possible culprit for tree closure on Android Arm64 Builder (dbg) Build #46484
    > > 
    > > https://build.chromium.org/p/chromium.linux/builders/Android%20Arm64%20Builder%20%28dbg%29/builds/46484
    > > 
    > > 
    > > 
    > > Original change's description:
    > > > VR: Move focus/secure origin checks to chrome/browser/vr/service
    > > > 
    > > > This CL makes focus and secure origin checks for VR presentation
    > > > cross-platform by moving them out of the Android VrShellDelegate to
    > > > the VrDisplayHost in chrome/browser/vr/service/.
    > > > 
    > > > There are no functionality changes here.
    > > > 
    > > > https://chromium-review.googlesource.com/c/chromium/src/+/698276
    > > > must land before this CL.
    > > > 
    > > > 
    > > > Bug: 768923
    > > > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    > > > Change-Id: I1358b289b24e0b14649574e28c63d07baab1f456
    > > > Reviewed-on: https://chromium-review.googlesource.com/698374
    > > > Reviewed-by: Avi Drissman <avi@chromium.org>
    > > > Reviewed-by: Yash Malik <ymalik@chromium.org>
    > > > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > > > Cr-Commit-Position: refs/heads/master@{#507680}
    > > 
    > > TBR=avi@chromium.org,mthiesse@chromium.org,ymalik@chromium.org
    > > 
    > > Change-Id: I220d02d5eb0f551104be46902f32866f2959d321
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Bug: 768923
    > > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    > > Reviewed-on: https://chromium-review.googlesource.com/709675
    > > Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    > > Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#507694}
    > 
    > TBR=avi@chromium.org,mthiesse@chromium.org,ymalik@chromium.org,sebsg@chromium.org
    > 
    > Bug: 768923
    > Change-Id: Id566997946afe4fc396cb02c46f3ac9473d67ed8
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    > Reviewed-on: https://chromium-review.googlesource.com/709956
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#508035}
    
    TBR=avi@chromium.org,mthiesse@chromium.org,ymalik@chromium.org,sebsg@chromium.org
    
    Change-Id: Ibc5ca7707a072a73731536007cb9bc893d9d37c8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 768923
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -2961,8 +2961,9 @@ void RenderFrameHostImpl::RegisterMojoInterfaces() {
   registry_->AddInterface(base::Bind(&SharedWorkerConnectorImpl::Create,
                                      process_->GetID(), routing_id_));
 
-  registry_->AddInterface<device::mojom::VRService>(base::Bind(
-      &WebvrServiceProvider::BindWebvrService, base::Unretained(this)));
+  registry_->AddInterface<device::mojom::VRService>(
+      base::Bind(&WebvrServiceProvider::BindWebvrService, GetProcess()->GetID(),
+                 GetRoutingID()));
 
   if (RendererAudioOutputStreamFactoryContextImpl::UseMojoFactories()) {
     registry_->AddInterface(base::BindRepeating(
