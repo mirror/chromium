939a506689a708d1e64660e0fd65076edec82b56
Maxim Kolosovskiy <kolos@chromium.org>
2017-10-11 23:23:33 +0200

    [Password Manager] Don't show the fallback for saving if credentials is already saved
    Bug: 725883
    Change-Id: I64b4729a1db3c450fc532403bb9764e2e8adcbb4

--- components/password_manager/core/browser/password_manager.cc
+++ components/password_manager/core/browser/password_manager.cc
@@ -485,12 +485,17 @@ void PasswordManager::ShowManualFallbackForSaving(
   }
   ProvisionallySaveManager(password_form, matched_manager, nullptr);
 
-  DCHECK(provisional_save_manager_);
-  bool is_update = IsPasswordUpdate(*provisional_save_manager_);
+  // Show the fallback if a promp or confirmation bubble should be available.
   bool has_generated_password =
       provisional_save_manager_->has_generated_password();
-  client_->ShowManualFallbackForSaving(std::move(provisional_save_manager_),
-                                       has_generated_password, is_update);
+  if (ShouldPromptUserToSavePassword() || has_generated_password) {
+    DCHECK(provisional_save_manager_);
+    bool is_update = IsPasswordUpdate(*provisional_save_manager_);
+    client_->ShowManualFallbackForSaving(std::move(provisional_save_manager_),
+                                         has_generated_password, is_update);
+  } else {
+    provisional_save_manager_.reset();
+  }
 }
 
 void PasswordManager::HideManualFallbackForSaving() {
