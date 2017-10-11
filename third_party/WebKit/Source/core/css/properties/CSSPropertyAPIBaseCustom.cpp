50ecf2f1f2cc84f35a9c711d077e484d891ee885
Renee Wright <rjwright@chromium.org>
2017-10-11 15:34:38 +1100

    [Ribbon] Refactor ResolveDirectionAwareProperty
    This CL makes a few changes.
    
    - Breaks up ResolveToPhysicalProperty into side-specific methods.
    
    - Adds a logical_side and shorthand_for_physical member to the property
    objects in CSSProperties.json5
    
    - Uses these json memebers to generate ResolveDirectionAwareProperty
    for the cases where we use a shorthand for ResolvePhysicalProperty
    (which allowed me to delete some of the cpp files)
    
    We can keep some of these changes, all of them, or none of them.
    
    Gist diff of generated files
    https://gist.github.com/rjwright/b74a02f851d18099c90e58bc61a5294f/revisions
    
    Bug:545324
    Change-Id: Ic7c5f07f7403ecc3fbde29083ec89743915b6599

--- third_party/WebKit/Source/core/css/properties/CSSPropertyAPIBaseCustom.cpp
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIBaseCustom.cpp
@@ -10,117 +10,6 @@
 
 namespace blink {
 
-namespace {
-
-CSSPropertyID ResolveToPhysicalProperty(
-    TextDirection direction,
-    WritingMode writing_mode,
-    LogicalBoxSide logical_side,
-    const StylePropertyShorthand& shorthand) {
-  if (direction == TextDirection::kLtr) {
-    if (IsHorizontalWritingMode(writing_mode)) {
-      // The common case. The logical and physical box sides match.
-      // Left = Start, Right = End, Before = Top, After = Bottom
-      return shorthand.properties()[logical_side];
-    }
-
-    if (IsFlippedLinesWritingMode(writing_mode)) {
-      // Start = Top, End = Bottom, Before = Left, After = Right.
-      switch (logical_side) {
-        case kStartSide:
-          return shorthand.properties()[kTopSide];
-        case kEndSide:
-          return shorthand.properties()[kBottomSide];
-        case kBeforeSide:
-          return shorthand.properties()[kLeftSide];
-        default:
-          return shorthand.properties()[kRightSide];
-      }
-    }
-
-    // Start = Top, End = Bottom, Before = Right, After = Left
-    switch (logical_side) {
-      case kStartSide:
-        return shorthand.properties()[kTopSide];
-      case kEndSide:
-        return shorthand.properties()[kBottomSide];
-      case kBeforeSide:
-        return shorthand.properties()[kRightSide];
-      default:
-        return shorthand.properties()[kLeftSide];
-    }
-  }
-
-  if (IsHorizontalWritingMode(writing_mode)) {
-    // Start = Right, End = Left, Before = Top, After = Bottom
-    switch (logical_side) {
-      case kStartSide:
-        return shorthand.properties()[kRightSide];
-      case kEndSide:
-        return shorthand.properties()[kLeftSide];
-      case kBeforeSide:
-        return shorthand.properties()[kTopSide];
-      default:
-        return shorthand.properties()[kBottomSide];
-    }
-  }
-
-  if (IsFlippedLinesWritingMode(writing_mode)) {
-    // Start = Bottom, End = Top, Before = Left, After = Right
-    switch (logical_side) {
-      case kStartSide:
-        return shorthand.properties()[kBottomSide];
-      case kEndSide:
-        return shorthand.properties()[kTopSide];
-      case kBeforeSide:
-        return shorthand.properties()[kLeftSide];
-      default:
-        return shorthand.properties()[kRightSide];
-    }
-  }
-
-  // Start = Bottom, End = Top, Before = Right, After = Left
-  switch (logical_side) {
-    case kStartSide:
-      return shorthand.properties()[kBottomSide];
-    case kEndSide:
-      return shorthand.properties()[kTopSide];
-    case kBeforeSide:
-      return shorthand.properties()[kRightSide];
-    default:
-      return shorthand.properties()[kLeftSide];
-  }
-}
-
-CSSPropertyID ResolveToPhysicalProperty(WritingMode writing_mode,
-                                        LogicalExtent logical_side,
-                                        const CSSPropertyID* properties) {
-  if (IsHorizontalWritingMode(writing_mode))
-    return properties[logical_side];
-  return logical_side == kLogicalWidth ? properties[1] : properties[0];
-}
-
-}  // namespace
-
-const CSSPropertyAPI& CSSPropertyAPI::ResolveToPhysicalPropertyAPI(
-    TextDirection direction,
-    WritingMode writing_mode,
-    LogicalBoxSide logical_side,
-    const StylePropertyShorthand& shorthand) {
-  CSSPropertyID id = ResolveToPhysicalProperty(direction, writing_mode,
-                                               logical_side, shorthand);
-  return Get(id);
-}
-
-const CSSPropertyAPI& CSSPropertyAPI::ResolveToPhysicalPropertyAPI(
-    WritingMode writing_mode,
-    LogicalExtent logical_side,
-    const CSSPropertyID* properties) {
-  CSSPropertyID id =
-      ResolveToPhysicalProperty(writing_mode, logical_side, properties);
-  return Get(id);
-}
-
 const StylePropertyShorthand& CSSPropertyAPI::BorderDirections() {
   static const CSSPropertyID kProperties[4] = {
       CSSPropertyBorderTop, CSSPropertyBorderRight, CSSPropertyBorderBottom,
@@ -129,6 +18,71 @@ const StylePropertyShorthand& CSSPropertyAPI::BorderDirections() {
       StylePropertyShorthand, border_directions,
       (CSSPropertyBorder, kProperties, WTF_ARRAY_LENGTH(kProperties)));
   return border_directions;
+}
+
+const CSSPropertyAPI& CSSPropertyAPI::ResolveAfterToPhysicalPropertyAPI(
+    TextDirection direction,
+    WritingMode writing_mode,
+    const StylePropertyShorthand& shorthand) {
+  const CSSPropertyID* shorthand_properties = shorthand.properties();
+  if (IsHorizontalWritingMode(writing_mode))
+    return Get(shorthand_properties[kBottomSide]);
+  if (IsFlippedLinesWritingMode(writing_mode))
+    return Get(shorthand_properties[kRightSide]);
+  return Get(shorthand_properties[kLeftSide]);
+}
+
+const CSSPropertyAPI& CSSPropertyAPI::ResolveBeforeToPhysicalPropertyAPI(
+    TextDirection direction,
+    WritingMode writing_mode,
+    const StylePropertyShorthand& shorthand) {
+  const CSSPropertyID* shorthand_properties = shorthand.properties();
+  if (IsHorizontalWritingMode(writing_mode))
+    return Get(shorthand_properties[kTopSide]);
+  if (IsFlippedLinesWritingMode(writing_mode))
+    return Get(shorthand_properties[kLeftSide]);
+  return Get(shorthand_properties[kRightSide]);
+}
+
+const CSSPropertyAPI& CSSPropertyAPI::ResolveEndToPhysicalPropertyAPI(
+    TextDirection direction,
+    WritingMode writing_mode,
+    const StylePropertyShorthand& shorthand) {
+  const CSSPropertyID* shorthand_properties = shorthand.properties();
+  if (direction == TextDirection::kLtr) {
+    if (IsHorizontalWritingMode(writing_mode))
+      return Get(shorthand_properties[kRightSide]);
+    return Get(shorthand_properties[kBottomSide]);
+  }
+  if (IsHorizontalWritingMode(writing_mode))
+    return Get(shorthand_properties[kLeftSide]);
+  return Get(shorthand_properties[kTopSide]);
+}
+
+const CSSPropertyAPI& CSSPropertyAPI::ResolveStartToPhysicalPropertyAPI(
+    TextDirection direction,
+    WritingMode writing_mode,
+    const StylePropertyShorthand& shorthand) {
+  const CSSPropertyID* shorthand_properties = shorthand.properties();
+  if (direction == TextDirection::kLtr) {
+    if (IsHorizontalWritingMode(writing_mode))
+      return Get(shorthand_properties[kLeftSide]);
+    return Get(shorthand_properties[kTopSide]);
+  }
+  if (IsHorizontalWritingMode(writing_mode))
+    return Get(shorthand_properties[kRightSide]);
+  return Get(shorthand_properties[kBottomSide]);
+}
+
+const CSSPropertyAPI& CSSPropertyAPI::ResolveToPhysicalPropertyAPI(
+    WritingMode writing_mode,
+    LogicalExtent logical_side,
+    const CSSPropertyID* properties) {
+  if (IsHorizontalWritingMode(writing_mode))
+    return Get(properties[logical_side]);
+  CSSPropertyID id =
+      logical_side == kLogicalWidth ? properties[1] : properties[0];
+  return Get(id);
 }
 
 }  // namespace blink
