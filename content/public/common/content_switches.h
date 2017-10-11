57c32d6ea2dfaa05e7aa2399bf61061cd425ab21
Tom Sepez <tsepez@chromium.org>
2017-10-10 17:10:34 -0700

    Move StartSandboxedProcess to sandbox_init_win.cc
    Removes some content dependencies from sandbox_win.cc, making it easier
    to move out from under content.
    
    The only functional changes are to use sandbox_type rather than
    process_type in SetJobMemoryLimit(), and to use a bool expression
    in IsWin32kLockdownEnabled().
    
    Change-Id: Ica7a9781f03d21a367fe6976be648a2f4f9b8ff2

--- content/public/common/content_switches.h
+++ content/public/common/content_switches.h
@@ -311,7 +311,6 @@ CONTENT_EXPORT extern const char kPrefetchArgumentOther[];
 // like renderers, etc.
 CONTENT_EXPORT extern const char kDeviceScaleFactor[];
 CONTENT_EXPORT extern const char kDisableLegacyIntermediateWindow[];
-CONTENT_EXPORT extern const char kDisableWin32kLockDown[];
 CONTENT_EXPORT extern const char kEnableAcceleratedVpxDecode[];
 CONTENT_EXPORT extern const char kEnableWin7WebRtcHWH264Decoding[];
 // Switch to pass the font cache shared memory handle to the renderer.
