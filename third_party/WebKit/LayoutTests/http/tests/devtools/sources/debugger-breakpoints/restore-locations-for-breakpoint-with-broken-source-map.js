86f558f1f9ec9eaa96733f2d60a5065867223c55
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2017-10-12 16:22:45 -0700

    [DevTools] update location on stub UISourceCode removed
    .. otherwise it's possible for locations to stuck with :sourcemap url
    if sourceMap failed to attach.
    
    Bug:chromium:740387
    Change-Id: Ic45f789c1bb9b73494de9f53ab3d02b14a398bd9
    R=lushnikov@chromium.org

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/sources/debugger-breakpoints/restore-locations-for-breakpoint-with-broken-source-map.js
@@ -0,0 +1,51 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function test() {
+  TestRunner.addResult('Checks that we update breakpoint location on source map loading error.');
+  await TestRunner.loadModule('sources_test_runner');
+  await TestRunner.showPanel('sources');
+
+  await SourcesTestRunner.startDebuggerTestPromise();
+
+  TestRunner.evaluateInPageAnonymously(`function foo() {
+  console.log(42);
+}
+//# sourceMappingURL=${TestRunner.url('../resources/a.js.map')}
+//# sourceURL=foo.js`);
+
+  let sourceFrame = await new Promise(resolve => SourcesTestRunner.showScriptSource('a.ts', resolve));
+  SourcesTestRunner.toggleBreakpoint(sourceFrame, 1, false);
+  await SourcesTestRunner.waitBreakpointSidebarPane(true);
+  SourcesTestRunner.dumpBreakpointSidebarPane();
+
+  await new Promise(resolve => TestRunner.reloadPage(resolve));
+
+  let origLoad = Host.ResourceLoader.load;
+  let stopRequest;
+  Host.ResourceLoader.load = function(){
+    let url = arguments[0];
+    if (url.endsWith('a.js.map')) {
+      stopRequest = () => arguments[2](404);
+      return;
+    }
+    return origLoad.apply(this, arguments);
+  }
+
+  await TestRunner.evaluateInPageAnonymously(`function foo() {
+  console.log(42);
+}
+//# sourceMappingURL=${TestRunner.url('../resources/a.js.map')}
+//# sourceURL=foo.js`);
+
+  await SourcesTestRunner.waitBreakpointSidebarPane(true);
+  SourcesTestRunner.dumpBreakpointSidebarPane();
+  stopRequest();
+  sourceFrame = await new Promise(resolve => SourcesTestRunner.showScriptSource('foo.js', resolve));
+  // Set another breakpoint to always force waitBreakpointSidebarPane resolving.
+  SourcesTestRunner.toggleBreakpoint(sourceFrame, 2, false);
+  await SourcesTestRunner.waitBreakpointSidebarPane(true);
+  SourcesTestRunner.dumpBreakpointSidebarPane();
+  SourcesTestRunner.completeDebuggerTest();
+})();
