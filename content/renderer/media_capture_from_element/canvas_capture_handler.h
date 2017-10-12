a8ad16d7fb7b68c30b4a50997069387f314ab5e0
Emircan Uysaler <emircan@chromium.org>
2017-10-11 17:16:24 -0700

    Use async GPU readbacks in canvas.captureStream()
    Bug:727385
    Change-Id: I8ed44b4419c8222062792ccb104bfb4b8c8e6bb3

--- content/renderer/media_capture_from_element/canvas_capture_handler.h
+++ content/renderer/media_capture_from_element/canvas_capture_handler.h
@@ -25,6 +25,10 @@
 
 class SkImage;
 
+namespace viz {
+class GLHelper;
+}  // namespace viz
+
 namespace content {
 
 // CanvasCaptureHandler acts as the link between Blink side HTMLCanvasElement
@@ -60,7 +64,6 @@ class CONTENT_EXPORT CanvasCaptureHandler final
       const media::VideoCapturerSource::RunningCallback& running_callback);
   void RequestRefreshFrame();
   void StopVideoCapture();
-  blink::WebSize GetSourceSize() const { return size_; }
 
  private:
   // A VideoCapturerSource instance is created, which is responsible for handing
@@ -73,7 +76,18 @@ class CONTENT_EXPORT CanvasCaptureHandler final
       const scoped_refptr<base::SingleThreadTaskRunner>& io_task_runner,
       blink::WebMediaStreamTrack* track);
 
-  void CreateNewFrame(const SkImage* image);
+  void DeliverNewFrame(const SkImage* image);
+  void DeliverAsYUVFrame(bool isOpaque,
+                         bool flip,
+                         const uint8_t* source_ptr,
+                         const gfx::Size& image_size,
+                         SkColorType source_color_type);
+  void ReadPixelsAsync(const scoped_refptr<media::VideoFrame> temp_frame,
+                       const gfx::Size& image_size,
+                       bool isOpaque,
+                       bool flip,
+                       bool success);
+
   void AddVideoCapturerSourceToVideoTrack(
       std::unique_ptr<media::VideoCapturerSource> source,
       blink::WebMediaStreamTrack* web_track);
@@ -85,13 +99,8 @@ class CONTENT_EXPORT CanvasCaptureHandler final
   media::VideoCaptureFormat capture_format_;
   bool ask_for_new_frame_;
 
-  const blink::WebSize size_;
-  gfx::Size last_size;
-  std::vector<uint8_t> temp_data_;
-  size_t temp_data_stride_;
-  SkImageInfo image_info_;
   media::VideoFramePool frame_pool_;
-
+  std::unique_ptr<viz::GLHelper> gl_helper_;
   scoped_refptr<media::VideoFrame> last_frame_;
 
   const scoped_refptr<base::SingleThreadTaskRunner> io_task_runner_;
