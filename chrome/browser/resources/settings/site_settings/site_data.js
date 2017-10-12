93af746c8a5303143a46a78bc75c04d668799d43
Dave Schuyler <dschuyler@chromium.org>
2017-10-12 15:55:13 -0700

    [MD settings] further improve time to display local data
    This CL transfers the local data to JS in chunks
    to allow faster initial display and interactivity during the transfer.
    
    Bug: 730777
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I75d1728a84cc262b040e4ed3959a46914b48a040

--- chrome/browser/resources/settings/site_settings/site_data.js
+++ chrome/browser/resources/settings/site_settings/site_data.js
@@ -72,6 +72,12 @@ Polymer({
   /** @private {settings.LocalDataBrowserProxy} */
   browserProxy_: null,
 
+  /** @private {number} */
+  extendSiteListHandle_: 0,
+
+  /** @private {number} */
+  extendSiteListAnimationHandle_: 0,
+
   /** @override */
   ready: function() {
     this.browserProxy_ = settings.LocalDataBrowserProxyImpl.getInstance();
@@ -90,6 +96,22 @@ Polymer({
     if (currentRoute == settings.routes.SITE_SETTINGS_SITE_DATA) {
       this.browserProxy_.reloadCookies().then(this.updateSiteList_.bind(this));
     }
+  },
+
+  /**
+   * Clear the sites list and stop gathering list data.
+   * @private
+   */
+  clearExtendSiteList_: function() {
+    window.cancelIdleCallback(this.extendSiteListHandle_);
+    this.extendSiteListHandle_ = 0;  // Unnecessary, just being clean.
+    window.cancelAnimationFrame(this.extendSiteListAnimationHandle_);
+    this.extendSiteListAnimationHandle_ = 0;  // Unnecessary.
+    this.sites = [];
+    // Queue the |sites| reset.
+    window.requestAnimationFrame(() => {
+      this.sites = [];
+    });
   },
 
   /**
@@ -123,16 +145,39 @@ Polymer({
   },
 
   /**
-   * Gather all the site data.
+   * Recursively add elements to the site list until all the sites are listed.
+   * @private
+   */
+  extendSiteList_: function() {
+    // A small |count| will have too much IPC overhead. A large |count| may
+    // cause UI stutter on low-end machines. Suggested values: 50 to 200.
+    let count = 100;  // Arbitrary.
+    this.browserProxy_.getDisplayList(this.filter, this.sites.length, count)
+        .then((listInfo) => {
+          // The last chunk (terminator) will have no items in the list.
+          if (!listInfo.items.length) {
+            this.fire('site-data-list-complete');
+            return;
+          }
+          // Don't modify the DOM during idle, do it in next animation frame.
+          this.extendSiteListAnimationHandle_ =
+              window.requestAnimationFrame(function(listInfo) {
+                this.push.bind(this, 'sites').apply(null, listInfo.items);
+              }.bind(this, listInfo));
+          // Get the next chunk.
+          this.extendSiteListHandle_ =
+              window.requestIdleCallback(this.extendSiteList_.bind(this));
+        });
+  },
+
+  /**
+   * Kick off a chain of |extendSiteList_| calls to gather all the site data.
+   * Warning: Do not call more than once in the same update cycle.
    * @private
    */
   updateSiteList_: function() {
-    this.browserProxy_
-        .getDisplayList(this.filter, 0 /* start */, -1 /* count */)
-        .then((listInfo) => {
-          this.sites = listInfo.items;
-          this.fire('site-data-list-complete');
-        });
+    this.clearExtendSiteList_();
+    this.extendSiteList_();
   },
 
   /**
@@ -174,9 +219,7 @@ Polymer({
   onConfirmDelete_: function() {
     this.$.confirmDeleteDialog.close();
     if (this.filter.length == 0) {
-      this.browserProxy_.removeAll().then(() => {
-        this.sites = [];
-      });
+      this.browserProxy_.removeAll().then(this.clearExtendSiteList_.bind(this));
     } else {
       this.browserProxy_.removeShownItems();
       // We just deleted all items found by the filter, let's reset the filter.
