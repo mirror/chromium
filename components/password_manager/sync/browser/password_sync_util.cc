c44db6fa2b2145f484fa85e0f10502ead6b61b41
Vadym Doroshenko <dvadym@chromium.org>
2017-10-12 13:30:08 +0200

    Update sync password hash on sync password change
    Bug: 767621
    Change-Id: I90c5deda2a765e15e1165dc391f18fe803364405

--- components/password_manager/sync/browser/password_sync_util.cc
+++ components/password_manager/sync/browser/password_sync_util.cc
@@ -13,6 +13,12 @@
 using autofill::PasswordForm;
 using url::Origin;
 
+namespace {
+
+const char kGoogleChangePasswordSignonRealm[] = "https://myaccount.google.com/";
+
+}  // namespace
+
 namespace password_manager {
 namespace sync_util {
 
@@ -35,7 +41,8 @@ bool IsSyncAccountCredential(const autofill::PasswordForm& form,
                              const syncer::SyncService* sync_service,
                              const SigninManagerBase* signin_manager) {
   const Origin gaia_origin(GaiaUrls::GetInstance()->gaia_url().GetOrigin());
-  if (!Origin(GURL(form.signon_realm)).IsSameOriginWith(gaia_origin)) {
+  if (!Origin(GURL(form.signon_realm)).IsSameOriginWith(gaia_origin) &&
+      form.signon_realm != kGoogleChangePasswordSignonRealm) {
     return false;
   }
 
