dc71b5f9324c1f8595b09a6d0d4896e451eb1caf
Zhenyao Mo <zmo@chromium.org>
2017-10-12 16:21:39 -0700

    Add functions to encode GpuPreferences to string and decode it from string.
    BUG=774157
    TEST=gpu_unittests
    R=piman@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ib4a410443d192cbda369a64d4b59cefb23444f9c

--- gpu/command_buffer/service/gpu_preferences.cc
+++ gpu/command_buffer/service/gpu_preferences.cc
@@ -4,7 +4,9 @@
 
 #include "gpu/command_buffer/service/gpu_preferences.h"
 
-#include "base/sys_info.h"
+#include <cstring>
+
+#include "base/base64.h"
 
 namespace gpu {
 
@@ -13,5 +15,26 @@ GpuPreferences::GpuPreferences() = default;
 GpuPreferences::GpuPreferences(const GpuPreferences& other) = default;
 
 GpuPreferences::~GpuPreferences() {}
+
+std::string GpuPreferences::ToString() const {
+  std::string encoded;
+  base::Base64Encode(
+      base::StringPiece(reinterpret_cast<const char*>(this), sizeof(*this)),
+      &encoded);
+  return encoded;
+}
+
+// static
+bool GpuPreferences::FromString(const std::string& data,
+                                GpuPreferences* output_prefs) {
+  DCHECK(output_prefs);
+  std::string decoded;
+  if (!base::Base64Decode(data, &decoded))
+    return false;
+  if (decoded.size() != sizeof(*output_prefs))
+    return false;
+  memcpy(output_prefs, decoded.data(), decoded.size());
+  return true;
+}
 
 }  // namespace gpu
