ccac3a2da69343540205bfde0e8483af709782f7
Bettina Dea <bdea@chromium.org>
2017-10-11 18:20:17 -0700

    Encapsulate all new tab IPH promo/UI.
    Previously, the new tab in-product help promo was invoked from the
    new_tab_button.cc file, which is not good as it's an integral class that
    is used in Chrome. Thus, I moved all the UI functionality (i.e.
    ShowPromo, CloseBubble) into the new_tab_tracker itself to encapsulate
    it which is much more clean than how it was previously implemented.
    
    Bug:734132
    Change-Id: Ic723f3afdbc9c93c181ae1877df3a50bd1c4f28a

--- chrome/browser/ui/views/feature_promos/incognito_window_promo_bubble_view.h
+++ chrome/browser/ui/views/feature_promos/incognito_window_promo_bubble_view.h
@@ -6,7 +6,9 @@
 #define CHROME_BROWSER_UI_VIEWS_FEATURE_PROMOS_INCOGNITO_WINDOW_PROMO_BUBBLE_VIEW_H_
 
 #include "base/macros.h"
+#include "chrome/browser/feature_engagement/feature_promo_bubble.h"
 #include "chrome/browser/ui/views/feature_promos/feature_promo_bubble_view.h"
+#include "ui/views/widget/widget_observer.h"
 
 // The IncognitoWindowPromoBubbleView is a bubble anchored to the right of the
 // App Menu Button. It draws users' attention to the App Menu Button. It is
@@ -16,14 +18,28 @@ class IncognitoWindowPromoBubbleView : public FeaturePromoBubbleView {
   // Returns a raw pointer that is owned by its native widget.
   static IncognitoWindowPromoBubbleView* CreateOwned(views::View* anchor_view);
 
+  // FeaturePromoBubble:
+  void ShowPromoBubble() override;
+  void ClosePromoBubble() override;
+
+  ~IncognitoWindowPromoBubbleView() override;
+
  private:
   // Anchors the bubble to |anchor_view|. The bubble widget and promo are
   // owned by their native widget.
   explicit IncognitoWindowPromoBubbleView(views::View* anchor_view);
-  ~IncognitoWindowPromoBubbleView() override;
 
   // Returns the string ID to display in the promo.
   int GetStringSpecifier();
+
+  // views::WidgetObserver:
+  void OnWidgetDestroying(views::Widget* widget) override;
+
+  IncognitoWindowPromoBubbleView* incognito_promo_ = nullptr;
+
+  // Observes the |incognito_promo_|'s Widget. Used to tell whether the promo
+  // is open and is called back when it closes.
+  ScopedObserver<views::Widget, WidgetObserver> incognito_promo_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(IncognitoWindowPromoBubbleView);
 };
