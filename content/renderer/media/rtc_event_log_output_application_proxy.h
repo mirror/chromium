b37b6b68b69b39d9512c65dd411cef0f8cf7138a
Elad Alon <eladalon@chromium.org>
2017-10-12 16:24:43 +0200

    RtcEventLogOutputApplicationProxy
    WIP
    
    Bug:TODO
    Change-Id: Ibb6a8fb5edb243d0cabcd79e9db2a0452ddb22e4

---
+++ content/renderer/media/rtc_event_log_output_application_proxy.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_RENDERER_MEDIA_RTC_EVENT_LOG_OUTPUT_APPLICATION_PROXY_H_
+#define CONTENT_RENDERER_MEDIA_RTC_EVENT_LOG_OUTPUT_APPLICATION_PROXY_H_
+
+namespace content {}  // namespace content
+
+#endif  // CONTENT_RENDERER_MEDIA_RTC_EVENT_LOG_OUTPUT_APPLICATION_PROXY_H_
