48dd20dc878b6ee3041cec1b650f721465f203da
Bettina Dea <bdea@chromium.org>
2017-10-11 18:42:50 -0700

    Remove dependency on ui/views directory from browser directory.
    1. Previously the browser directory depended on the ui/views directory
    which causes a circular dependency. This CL creates a platform
    independent interface of a feature_promo that can be used in the
    browser/feature_engagement directory which will avoid the circular
    dependency.
    
    2. Encapsulate all new tab IPH promo/UI.
    Previously, the new tab in-product help promo was invoked from the
    new_tab_button.cc file, which is not good as it's an integral class that
    is used in Chrome. Thus, I moved all the UI functionality (i.e.
    ShowPromo, CloseBubble) into the new_tab_tracker itself to encapsulate
    it which is much more clean than how it was previously implemented.
    
    Bug:734132
    Change-Id: Ic723f3afdbc9c93c181ae1877df3a50bd1c4f28a

---
+++ chrome/browser/feature_engagement/feature_promo_bubble.h
@@ -0,0 +1,35 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_FEATURE_ENGAGEMENT_FEATURE_PROMO_BUBBLE_H_
+#define CHROME_BROWSER_FEATURE_ENGAGEMENT_FEATURE_PROMO_BUBBLE_H_
+
+#include "chrome/browser/ui/toolbar/app_menu_icon_controller.h"
+
+// FeaturePromoBubble is an interface implemented by an object that provides an
+// in-product help feature promo bubble widget on the UI.
+class FeaturePromoBubble {
+ public:
+  FeaturePromoBubble() {}
+  virtual ~FeaturePromoBubble() {}
+
+  virtual void ShowPromoBubble() = 0;
+
+  virtual void ClosePromoBubble() = 0;
+};
+
+namespace feature_engagement {
+
+// Gets the severity of app menu button of the last active browser.
+AppMenuIconController::Severity GetAppMenuButtonSeverity();
+
+// Shows an incognito window in product help feature promo bubble.
+FeaturePromoBubble* CreateIncognitoFeaturePromoBubble();
+
+// Create a new tab in product help feature promo bubble.
+FeaturePromoBubble* CreateNewTabFeaturePromoBubble();
+
+}  // namespace feature_engagement
+
+#endif  // CHROME_BROWSER_FEATURE_ENGAGEMENT_FEATURE_PROMO_BUBBLE_H_
--- chrome/browser/ui/views/feature_promos/new_tab_promo_bubble_view.h
+++ chrome/browser/ui/views/feature_promos/new_tab_promo_bubble_view.h
@@ -6,6 +6,7 @@
 #define CHROME_BROWSER_UI_VIEWS_FEATURE_PROMOS_NEW_TAB_PROMO_BUBBLE_VIEW_H_
 
 #include "base/macros.h"
+#include "chrome/browser/feature_engagement/feature_promo_bubble.h"
 #include "chrome/browser/ui/views/feature_promos/feature_promo_bubble_view.h"
 
 // The NewTabPromoBubbleView is a bubble anchored to the right of the New Tab
@@ -16,6 +17,10 @@ class NewTabPromoBubbleView : public FeaturePromoBubbleView {
   // Returns a raw pointer that is owned by its native widget.
   static NewTabPromoBubbleView* CreateOwned(views::View* anchor_view);
 
+  // FeaturePromoBubble:
+  void ShowPromoBubble() override;
+  void ClosePromoBubble() override;
+
  private:
   // Anchors the bubble to |anchor_view|. The bubble widget and promo are
   // owned by their native widget.
@@ -24,6 +29,15 @@ class NewTabPromoBubbleView : public FeaturePromoBubbleView {
 
   // Returns the string ID to display in the promo.
   static int GetStringSpecifier();
+
+  // views::WidgetObserver:
+  void OnWidgetDestroying(views::Widget* widget) override;
+
+  NewTabPromoBubbleView* new_tab_promo_ = nullptr;
+
+  // Observes the |new_tab_promo_|'s Widget. Used to tell whether the promo
+  // is open and is called back when it closes.
+  ScopedObserver<views::Widget, WidgetObserver> new_tab_promo_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(NewTabPromoBubbleView);
 };
