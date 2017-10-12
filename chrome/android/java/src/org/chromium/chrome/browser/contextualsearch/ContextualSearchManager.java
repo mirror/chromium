0c45bfb4fcdf7a36509e157d68710f3ac5d353c9
Donn Denman <donnd@google.com>
2017-10-12 11:29:46 -0700

    [TTS] Fix a race with Smart Selection.
    Introduces a fix for issue 773330 where gathering
    surrounding text on the page is requested multiple times
    but only a single call is supported.
    
    Disables the gathering of surrounding text for longpress
    when Smart Selection is active. Instead we send just the
    current selection to icing for the assistant to use.
    
    Also fix a logic error in whether surrounding text can be
    sent to icing or not.
    
    BUG=773330, 770502
    
    Change-Id: I752271efecb27d931d3fc25d46a4d66ad9649875

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchManager.java
@@ -1517,6 +1517,19 @@ public class ContextualSearchManager
                 };
 
                 boolean isTap = mSelectionController.getSelectionType() == SelectionType.TAP;
+                if (!isTap && mDoSuppressContextualSearchForSmartSelection && mContext != null) {
+                    // If Smart Selection is active we need to work around a race
+                    // condition gathering surrounding text.  See issue 773330.
+                    // Instead we just return the selection which is good enough for the assistant.
+                    mInternalStateController.notifyStartingWorkOn(
+                            InternalState.GATHERING_SURROUNDINGS);
+                    String currentSelection = mSelectionController.getSelectedText();
+                    mContext.setSurroundingText(getBaseWebContents(), currentSelection);
+                    mInternalStateController.notifyFinishedWorkOn(
+                            InternalState.GATHERING_SURROUNDINGS);
+                    return;
+                }
+
                 if (isTap && mPolicy.shouldPreviousTapResolve()) {
                     mContext.setResolveProperties(
                             mPolicy.getHomeCountry(mActivity), mPolicy.maySendBasePageUrl());
@@ -1643,7 +1656,6 @@ public class ContextualSearchManager
                     // progress or may have completed and the Bar is being shown.
                     hideContextualSearch(StateChangeReason.UNKNOWN);
                     // TODO(donnd): add user action:
-                    // RecordUserAction.record("ContextualSearch.SmartSelectSuppressed");
                 } else {
                     showContextualSearch(StateChangeReason.TEXT_SELECT_LONG_PRESS);
                 }
