762233a8dcb009c8beb7fc6ad81ef8b9403b1160
Paul Jensen <pauljensen@chromium.org>
2017-10-13 09:38:07 -0400

    test for gerrit copy detection
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I7a60fc726666664c14c6beea8b83d14bbf8ff085

--- components/cronet/tools/cronet_c_generator.py
+++ components/cronet/tools/cronet_c_generator.py
@@ -1,6 +1,6 @@
 # Copyright 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
-# found  in the LICENSE file.
+# found in the LICENSE file.
 
 """Generates C++ source files from a mojom.Module."""
 
