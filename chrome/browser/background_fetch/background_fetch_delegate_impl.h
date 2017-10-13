a6475c6cf2e8f394d963aab67122abf48b1ac613
Dan Elphick <delphick@chromium.org>
2017-10-13 09:56:59 +0100

    Dump of current work on adding a notification including BackgroundFetchDelegate API changes
    Change-Id: I48ffa20dce3a6443bd219136a80dfe65f22f7375

--- chrome/browser/background_fetch/background_fetch_delegate_impl.h
+++ chrome/browser/background_fetch/background_fetch_delegate_impl.h
@@ -5,23 +5,34 @@
 #ifndef CHROME_BROWSER_BACKGROUND_FETCH_BACKGROUND_FETCH_DELEGATE_IMPL_H_
 #define CHROME_BROWSER_BACKGROUND_FETCH_BACKGROUND_FETCH_DELEGATE_IMPL_H_
 
+#include <map>
 #include <memory>
+#include <set>
 #include <string>
 #include <vector>
 
 #include "base/memory/weak_ptr.h"
 #include "components/download/public/download_params.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/offline_items_collection/core/offline_content_provider.h"
 #include "content/public/browser/background_fetch_delegate.h"
+#include "url/origin.h"
+
 class Profile;
 
 namespace download {
 class DownloadService;
 }  // namespace download
 
+namespace offline_items_collection {
+class OfflineContentAggregator;
+}
+
 // Implementation of BackgroundFetchDelegate using the DownloadService.
-class BackgroundFetchDelegateImpl : public content::BackgroundFetchDelegate,
-                                    public KeyedService {
+class BackgroundFetchDelegateImpl
+    : public content::BackgroundFetchDelegate,
+      public offline_items_collection::OfflineContentProvider,
+      public KeyedService {
  public:
   explicit BackgroundFetchDelegateImpl(Profile* profile);
 
@@ -31,7 +42,15 @@ class BackgroundFetchDelegateImpl : public content::BackgroundFetchDelegate,
   void Shutdown() override;
 
   // BackgroundFetchDelegate implementation:
-  void DownloadUrl(const std::string& guid,
+  void CreateDownloadJob(
+      const std::string& jobId,
+      const std::string& title,
+      const url::Origin& origin,
+      int completed_parts,
+      int total_parts,
+      const std::vector<std::string>& current_guids) override;
+  void DownloadUrl(const std::string& jobId,
+                   const std::string& guid,
                    const std::string& method,
                    const GURL& url,
                    const net::NetworkTrafficAnnotationTag& traffic_annotation,
@@ -49,17 +68,67 @@ class BackgroundFetchDelegateImpl : public content::BackgroundFetchDelegate,
                            const base::FilePath& path,
                            uint64_t size);
 
+  // OfflineContentProvider implementation:
+  bool AreItemsAvailable() override;
+  void OpenItem(const offline_items_collection::ContentId& id) override;
+  void RemoveItem(const offline_items_collection::ContentId& id) override;
+  void CancelDownload(const offline_items_collection::ContentId& id) override;
+  void PauseDownload(const offline_items_collection::ContentId& id) override;
+  void ResumeDownload(const offline_items_collection::ContentId& id,
+                      bool has_user_gesture) override;
+  const offline_items_collection::OfflineItem* GetItemById(
+      const offline_items_collection::ContentId& id) override;
+  OfflineItemList GetAllItems() override;
+  void GetVisualsForItem(const offline_items_collection::ContentId& id,
+                         const VisualsCallback& callback) override;
+  void AddObserver(Observer* observer) override;
+  void RemoveObserver(Observer* observer) override;
+
   base::WeakPtr<BackgroundFetchDelegateImpl> GetWeakPtr() {
     return weak_ptr_factory_.GetWeakPtr();
   }
 
  private:
+  struct JobDetails {
+    JobDetails(JobDetails&&) = default;
+    JobDetails(const std::string& title,
+               const url::Origin& origin,
+               int completed_parts,
+               int total_parts);
+    ~JobDetails();
+
+    std::string title;
+    const url::Origin origin;
+    int completed_parts;
+    const int total_parts;
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(JobDetails);
+  };
+
+  offline_items_collection::OfflineItem CreateOfflineItem(
+      const std::string& job_id,
+      const JobDetails& details);
+
   void OnDownloadReceived(const std::string& guid,
                           download::DownloadParams::StartResult result);
 
   // The BackgroundFetchDelegateImplFactory depends on the
   // DownloadServiceFactory, so |download_service_| should outlive |this|.
   download::DownloadService* download_service_;
+
+  // Map from individual download GUIDs to job ids.
+  std::map<std::string, std::string> file_download_job_map_;
+
+  // Map from job ids to the details of the job.
+  std::map<std::string, JobDetails> job_details_map_;
+
+  offline_items_collection::OfflineContentAggregator*
+      offline_content_aggregator_;
+
+  std::set<Observer*> observers_;
+
+  OfflineItemList items_;
 
   base::WeakPtrFactory<BackgroundFetchDelegateImpl> weak_ptr_factory_;
 
