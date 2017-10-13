3060930a9fe54813756ff5bb8623ce8b0dcb8090
Keishi Hattori <keishi@chromium.org>
2017-10-13 15:04:26 +0900

    Oilpan: move arenas_ from ThreadState to ThreadHeap
    Bug:
    Change-Id: Ib734628157daf7fbe9f6386f22d473532ee32483

--- third_party/WebKit/Source/platform/heap/HeapPage.cpp
+++ third_party/WebKit/Source/platform/heap/HeapPage.cpp
@@ -343,7 +343,7 @@ Address BaseArena::AllocateLargeObject(size_t allocation_size,
   // TODO(sof): should need arise, support eagerly finalized large objects.
   CHECK(ArenaIndex() != BlinkGC::kEagerSweepArenaIndex);
   LargeObjectArena* large_object_arena = static_cast<LargeObjectArena*>(
-      GetThreadState()->Arena(BlinkGC::kLargeObjectArenaIndex));
+      GetThreadState()->Heap().Arena(BlinkGC::kLargeObjectArenaIndex));
   Address large_object = large_object_arena->AllocateLargeObjectPage(
       allocation_size, gc_info_index);
   ASAN_MARK_LARGE_VECTOR_CONTAINER(this, large_object);
@@ -608,7 +608,7 @@ void NormalPageArena::TakeFreelistSnapshot(const String& dump_name) {
 }
 
 void NormalPageArena::AllocatePage() {
-  GetThreadState()->ShouldFlushHeapDoesNotContainCache();
+  GetThreadState()->Heap().ShouldFlushHeapDoesNotContainCache();
   PageMemory* page_memory =
       GetThreadState()->Heap().GetFreePagePool()->Take(ArenaIndex());
 
@@ -999,7 +999,7 @@ Address LargeObjectArena::DoAllocateLargeObjectPage(size_t allocation_size,
   large_object_size += kAllocationGranularity;
 #endif
 
-  GetThreadState()->ShouldFlushHeapDoesNotContainCache();
+  GetThreadState()->Heap().ShouldFlushHeapDoesNotContainCache();
   PageMemory* page_memory = PageMemory::Allocate(
       large_object_size, GetThreadState()->Heap().GetRegionTree());
   Address large_object_address = page_memory->WritableStart();
