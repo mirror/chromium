323184f8b04fbd4631901d8bfdd0e92f7000c8c8
Khushal <khushalsagar@chromium.org>
2017-10-13 10:03:38 -0700

    images: Plumb async decode veto for src changes to cc.
    If the image content for an image element is changed, displaying the
    updated image asynchronously can result in visual content flickering.
    Plumb this state change using the DecodingMode on PaintImage to cc.
    Subsequent patches will ensure that these images are not decoded async
    on the pending tree by cc, to avoid flickering when replacing active
    tree content. The decode may still be done async if the image only
    exists on the active tree.
    
    R=chrishtr@chromium.org, vmpstr@chromium.org
    Bug:740953
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I1d89eb170e26f7348a247cd93d85f62efef6d40d

--- third_party/WebKit/Source/core/paint/compositing/CompositedLayerMapping.cpp
+++ third_party/WebKit/Source/core/paint/compositing/CompositedLayerMapping.cpp
@@ -2819,8 +2819,9 @@ void CompositedLayerMapping::UpdateImageContents() {
 
   Node* node = image_layout_object.GetNode();
   Image::ImageDecodingMode decode_mode =
-      IsHTMLImageElement(node) ? ToHTMLImageElement(node)->GetDecodingMode()
-                               : Image::kUnspecifiedDecode;
+      IsHTMLImageElement(node)
+          ? ToHTMLImageElement(node)->GetDecodingMode(image->paint_image_id())
+          : Image::kUnspecifiedDecode;
 
   // This is a no-op if the layer doesn't have an inner layer for the image.
   graphics_layer_->SetContentsToImage(
