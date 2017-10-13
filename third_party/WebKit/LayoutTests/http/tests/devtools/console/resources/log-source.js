71f6f7de452b0d9a7f9e63b2bd0b76a03647af6f
Erik Luo <luoe@chromium.org>
2017-10-13 15:03:49 -0700

    DevTools: switch list to treeoutline for log management UI
    This CL pivots the log management UI to use a tree instead of a list.
    - Introduces SidebarLevelByUrlGroups, which contain SidebarItems
    - Adding a single message will let groups create SidebarItem children
    - Sidebar's applyFilter lets Groups choose which children to apply to
    
    Some messages have no 'url' such as evaluations, and will be addressed
    in a future CL.
    
    Styles will be introduced in a followup.
    
    Bug: 685283
    Change-Id: I0335138c89ba3d4ff81dcbb3a88787e8ac0fa395

--- third_party/WebKit/LayoutTests/http/tests/devtools/console/resources/log-source.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console/resources/log-source.js
@@ -2,3 +2,8 @@ function log2()
 {
     console.log.apply(console, arguments);
 }
+
+function warn2()
+{
+    console.warn.apply(console, arguments);
+}
--- third_party/WebKit/Source/devtools/front_end/console/ConsoleSidebar.js
+++ third_party/WebKit/Source/devtools/front_end/console/ConsoleSidebar.js
@@ -2,50 +2,38 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * @implements {UI.ListDelegate<!Console.ConsoleFilter>}
- */
 Console.ConsoleSidebar = class extends UI.VBox {
   constructor() {
     super(true);
-    this.registerRequiredCSS('console/consoleSidebar.css');
-    this.setMinimumSize(50, 0);
-
-    /** @type {!UI.ListModel<!Console.ConsoleFilter>} */
-    this._filters = new UI.ListModel();
-    /** @type {!UI.ListControl<!Console.ConsoleFilter>} */
-    this._list = new UI.ListControl(this._filters, this, UI.ListMode.EqualHeightItems);
-    this._list.element.classList.add('list');
-    this.contentElement.appendChild(this._list.element);
-
-    this._allFilter =
-        new Console.ConsoleFilter(Common.UIString('All'), [], null, Console.ConsoleFilter.allLevelsFilterValue());
-    this._filters.replaceAll([this._allFilter]);
-    this._list.selectItem(this._filters.at(0));
-    this._lastSelectedItem = this._list.selectedItem();
-
-    /** @type {!Map<string, !Console.ConsoleFilter>} */
-    this._contextFilters = new Map();
-    /** @type {!Set<!Console.ConsoleFilter>} */
-    this._pendingFiltersToAdd = new Set();
-    this._pendingClear = false;
-
+    this.setMinimumSize(125, 0);
     this._enabled = Runtime.experiments.isEnabled('logManagement');
+
+    this._tree = new UI.TreeOutlineInShadow();
+    this._tree.registerRequiredCSS('console/consoleSidebar.css');
+    this._tree.addEventListener(UI.TreeOutline.Events.ElementSelected, this._selectionChanged.bind(this));
+    this.contentElement.appendChild(this._tree.element);
+    /** @type {?Console.SidebarItem} */
+    this._selectedTreeElement = null;
+
+    var Levels = ConsoleModel.ConsoleMessage.MessageLevel;
+    var filters = [
+      new Console.ConsoleFilter(Common.UIString('message'), [], null, Console.ConsoleFilter.allLevelsFilterValue()),
+      new Console.ConsoleFilter(Levels.Error, [], null, Console.ConsoleFilter.singleLevelMask(Levels.Error)),
+      new Console.ConsoleFilter(Levels.Warning, [], null, Console.ConsoleFilter.singleLevelMask(Levels.Warning)),
+      new Console.ConsoleFilter(Levels.Info, [], null, Console.ConsoleFilter.singleLevelMask(Levels.Info)),
+      new Console.ConsoleFilter(Levels.Verbose, [], null, Console.ConsoleFilter.singleLevelMask(Levels.Verbose))
+    ];
+    this._groups = filters.map(filter => new Console.SidebarLevelByUrlGroup(filter));
+    this._groups.forEach(group => this._tree.appendChild(group));
+    this._groups[0].expand();
+    this._groups[0].select();
   }
 
   /**
    * @override
    */
   wasShown() {
-    // ListControl's viewport does not update when hidden.
-    this._list.viewportResized();
-  }
-
-  /**
-   * @override
-   */
-  onResize() {
-    this._list.viewportResized();
+    this._tree.focus();
   }
 
   /**
@@ -54,28 +42,13 @@ Console.ConsoleSidebar = class extends UI.VBox {
   onMessageAdded(message) {
     if (!this._enabled)
       return;
-    this._allFilter[Console.ConsoleSidebar._filterIsDirtySymbol] = true;
-
-    var context = message.context;
-    if (context) {
-      var filter = this._contextFilters.get(context);
-      if (!filter) {
-        var parsedFilter = {key: Console.ConsoleFilter.FilterType.Context, text: context, negative: false};
-        filter = new Console.ConsoleFilter(context, [parsedFilter], null);
-        this._contextFilters.set(context, filter);
-        this._pendingFiltersToAdd.add(filter);
-      } else {
-        filter[Console.ConsoleSidebar._filterIsDirtySymbol] = true;
-      }
-    }
+    this._groups.forEach(group => group.onMessageAdded(message));
   }
 
   clear() {
     if (!this._enabled)
       return;
-    this._contextFilters.clear();
-    this._pendingFiltersToAdd.clear();
-    this._pendingClear = true;
+    this._groups.forEach(group => group.clear());
   }
 
   /**
@@ -83,100 +56,19 @@ Console.ConsoleSidebar = class extends UI.VBox {
    * @return {boolean}
    */
   applyFilters(viewMessage) {
-    var passed = true;
-    for (var filter of this._filters) {
-      var passedFilter = filter.applyFilter(viewMessage);
-      if (filter === this._list.selectedItem())
-        passed = passedFilter;
-    }
-    for (var filter of this._pendingFiltersToAdd)
-      filter.applyFilter(viewMessage);
-    return passed;
-  }
-
-  resetItemCounters() {
-    for (var filter of this._filters) {
-      filter.resetCounters();
-      filter[Console.ConsoleSidebar._filterIsDirtySymbol] = true;
-    }
-  }
-
-  refresh() {
     if (!this._enabled)
-      return;
-    if (this._pendingClear) {
-      this._filters.replaceAll([this._allFilter]);
-      this._list.selectItem(this._filters.at(0));
-      this._pendingClear = false;
-    }
-    // Refresh counters for stale groups.
-    for (var filter of this._filters) {
-      if (filter[Console.ConsoleSidebar._filterIsDirtySymbol]) {
-        this._list.refreshItem(filter);
-        delete filter[Console.ConsoleSidebar._filterIsDirtySymbol];
-      }
-    }
-
-    // Add new groups.
-    if (this._pendingFiltersToAdd.size > 0) {
-      this._filters.replaceRange(this._filters.length, this._filters.length, Array.from(this._pendingFiltersToAdd));
-      this._pendingFiltersToAdd.clear();
-    }
-  }
-
-  /**
-   * @override
-   * @param {!Console.ConsoleFilter} item
-   * @return {!Element}
-   */
-  createElementForItem(item) {
-    var element = createElementWithClass('div', 'context-item');
-    element.createChild('div', 'name').textContent = item.name;
-    element.title = item.name;
-    var counters = element.createChild('div', 'counters');
-    if (item.errorCount)
-      counters.createChild('span', 'error-count').textContent = item.errorCount > 99 ? '99+' : item.errorCount;
-    if (item.warningCount)
-      counters.createChild('span', 'warning-count').textContent = item.warningCount > 99 ? '99+' : item.warningCount;
-    if (item.infoCount)
-      counters.createChild('span', 'info-count').textContent = item.infoCount > 99 ? '99+' : item.infoCount;
-    return element;
-  }
-
-  /**
-   * @override
-   * @param {!Console.ConsoleFilter} item
-   * @return {number}
-   */
-  heightForItem(item) {
-    return 28;
-  }
-
-  /**
-   * @override
-   * @param {!Console.ConsoleFilter} item
-   * @return {boolean}
-   */
-  isItemSelectable(item) {
+      return true;
+    if (this._selectedTreeElement)
+      return this._selectedTreeElement.applyFilter(viewMessage);
     return true;
   }
 
   /**
-   * @override
-   * @param {?Console.ConsoleFilter} from
-   * @param {?Console.ConsoleFilter} to
-   * @param {?Element} fromElement
-   * @param {?Element} toElement
+   * @param {!Common.Event} event
    */
-  selectedItemChanged(from, to, fromElement, toElement) {
-    if (fromElement)
-      fromElement.classList.remove('selected');
-    if (toElement)
-      toElement.classList.add('selected');
-    var shouldNotify = this._lastSelectedItem !== to;
-    this._lastSelectedItem = to;
-    if (shouldNotify)
-      this.dispatchEventToListeners(Console.ConsoleSidebar.Events.FilterSelected, to);
+  _selectionChanged(event) {
+    this._selectedTreeElement = /** @type {!Console.SidebarItem} */ (event.data);
+    this.dispatchEventToListeners(Console.ConsoleSidebar.Events.FilterSelected);
   }
 };
 
@@ -185,5 +77,59 @@ Console.ConsoleSidebar.Events = {
   FilterSelected: Symbol('FilterSelected')
 };
 
-/** @type {symbol} */
-Console.ConsoleSidebar._filterIsDirtySymbol = Symbol('filterIsDirty');
+Console.SidebarItem = class extends UI.TreeElement {
+  /**
+   * @param {!Console.ConsoleFilter} filter
+   * @param {string=} tooltip
+   */
+  constructor(filter, tooltip) {
+    super(filter.name);
+    this._filter = filter;
+    if (tooltip)
+      this.tooltip = tooltip;
+  }
+
+  /**
+   * @param {!Console.ConsoleViewMessage} viewMessage
+   * @return {boolean}
+   */
+  applyFilter(viewMessage) {
+    return this._filter.applyFilter(viewMessage);
+  }
+};
+
+Console.SidebarLevelByUrlGroup = class extends Console.SidebarItem {
+  /**
+   * @param {!Console.ConsoleFilter} filter
+   */
+  constructor(filter) {
+    super(filter);
+    this.setExpandable(true);
+    /** @type {!Map<string, !Console.SidebarItem>} */
+    this._urlItems = new Map();
+  }
+
+  /**
+   * @param {!ConsoleModel.ConsoleMessage} message
+   */
+  onMessageAdded(message) {
+    var url = message.url;
+    if (!url || !this._filter.levelsMask[/** @type {string} */ (message.level)] || this._urlItems.get(url))
+      return;
+
+    var parsedFilter = {key: Console.ConsoleFilter.FilterType.Url, text: url, negative: false};
+    var levelsMask = {};
+    for (var level of Object.keys(this._filter.levelsMask))
+      levelsMask[level] = this._filter.levelsMask[level];
+    var parsedURL = url.asParsedURL();
+    var filter = new Console.ConsoleFilter(parsedURL ? parsedURL.displayName : url, [parsedFilter], null, levelsMask);
+    var child = new Console.SidebarItem(filter, url);
+    this._urlItems.set(url, child);
+    this.appendChild(child);
+  }
+
+  clear() {
+    this._urlItems.clear();
+    this.removeChildren();
+  }
+};
