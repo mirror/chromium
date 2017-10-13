b335976ca21729b9d8cf8d5d3a031321a0640f18
Lucas Garron <lgarron@chromium.org>
2017-10-13 16:19:36 -0700

    Committed Interstitials: expand NavigationRequest::OnRequestFailed() path to allow optional error page html (2/2)
    Plumb failed request error page data URL from NavigationThrottle to RenderFrameImpl.
    
    Bug: 751949
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I43d7329a8dc6e479fb9c281f2efbe832f5341eb0

--- content/browser/frame_host/navigation_handle_impl_browsertest.cc
+++ content/browser/frame_host/navigation_handle_impl_browsertest.cc
@@ -5,9 +5,11 @@
 #include "base/command_line.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/weak_ptr.h"
+#include "base/strings/stringprintf.h"
 #include "content/browser/frame_host/debug_urls.h"
 #include "content/browser/frame_host/navigation_handle_impl.h"
 #include "content/browser/web_contents/web_contents_impl.h"
+#include "content/public/browser/navigation_throttle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/common/bindings_policy.h"
@@ -16,7 +18,6 @@
 #include "content/public/common/content_switches.h"
 #include "content/public/common/request_context_type.h"
 #include "content/public/common/url_constants.h"
-#include "content/public/test/browser_side_navigation_test_utils.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
@@ -1992,6 +1993,39 @@ IN_PROC_BROWSER_TEST_F(NavigationHandleImplDownloadBrowserTest,
   EXPECT_FALSE(observer.is_download());
   EXPECT_TRUE(observer.is_error());
   EXPECT_TRUE(observer.was_redirected());
+}
+
+IN_PROC_BROWSER_TEST_F(NavigationHandleImplBrowserTest,
+                       ThrottleFailureWithErrorPageContent) {
+  if (!IsBrowserSideNavigationEnabled())
+    return;
+
+  net::EmbeddedTestServer https_server(net::EmbeddedTestServer::TYPE_HTTPS);
+  https_server.SetSSLConfig(net::EmbeddedTestServer::CERT_MISMATCHED_NAME);
+  ASSERT_TRUE(https_server.Start());
+  GURL url(https_server.GetURL("/title1.html"));
+
+  const char body_text_content[] = "some plain text content";
+  NavigationThrottle::ThrottleCheckResult cancel_result = {
+      NavigationThrottle::CANCEL, net::ERR_CERT_COMMON_NAME_INVALID,
+      base::StringPrintf("<html><body>%s</body><html>", body_text_content)};
+
+  NavigationHandleObserver observer(shell()->web_contents(), url);
+  TestNavigationThrottleInstaller installer(
+      shell()->web_contents(), NavigationThrottle::PROCEED,
+      NavigationThrottle::PROCEED, cancel_result, NavigationThrottle::PROCEED);
+
+  EXPECT_FALSE(observer.has_committed());
+  EXPECT_FALSE(observer.is_error());
+
+  EXPECT_FALSE(NavigateToURL(shell(), url));
+
+  std::string result = "";
+  const std::string javascript =
+      "domAutomationController.send(document.body.textContent)";
+  content::RenderFrameHost* rfh = shell()->web_contents()->GetMainFrame();
+  ASSERT_TRUE(content::ExecuteScriptAndExtractString(rfh, javascript, &result));
+  EXPECT_EQ(std::string(body_text_content), result);
 }
 
 }  // namespace content
