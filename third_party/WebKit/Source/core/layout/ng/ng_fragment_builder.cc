a08da2d98f17a2d5ea2a37dfd6fd3ca2e3998471
Ian Kilpatrick <ikilpatrick@chromium.org>
2017-10-13 11:29:23 -0700

    [LayoutNG] Create NGContainerFragmentBuilder.
    This captures the common methods that are needed between
    NGFragmentBuilder, and NGLineBoxFragmentBuilder.
    
    I didn't place them into NGBaseFragmentBuilder, as text doesn't really
    need them.
    
    Bug: 635619
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I5fe83a94021890bbe0a6a4e096a1aa2893138899

--- third_party/WebKit/Source/core/layout/ng/ng_fragment_builder.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_fragment_builder.cc
@@ -17,7 +17,7 @@ NGFragmentBuilder::NGFragmentBuilder(NGLayoutInputNode node,
                                      RefPtr<const ComputedStyle> style,
                                      NGWritingMode writing_mode,
                                      TextDirection direction)
-    : NGBaseFragmentBuilder(style, writing_mode, direction),
+    : NGContainerFragmentBuilder(style, writing_mode, direction),
       node_(node),
       layout_object_(node.GetLayoutObject()),
       did_break_(false) {}
@@ -26,20 +26,15 @@ NGFragmentBuilder::NGFragmentBuilder(LayoutObject* layout_object,
                                      RefPtr<const ComputedStyle> style,
                                      NGWritingMode writing_mode,
                                      TextDirection direction)
-    : NGBaseFragmentBuilder(style, writing_mode, direction),
+    : NGContainerFragmentBuilder(style, writing_mode, direction),
       node_(nullptr),
       layout_object_(layout_object),
       did_break_(false) {}
 
 NGFragmentBuilder::~NGFragmentBuilder() {}
 
-NGFragmentBuilder& NGFragmentBuilder::SetSize(const NGLogicalSize& size) {
-  size_ = size;
-  return *this;
-}
-
-NGFragmentBuilder& NGFragmentBuilder::SetBlockSize(LayoutUnit size) {
-  size_.block_size = size;
+NGFragmentBuilder& NGFragmentBuilder::SetBlockSize(LayoutUnit block_size) {
+  block_size_ = block_size;
   return *this;
 }
 
@@ -49,20 +44,7 @@ NGFragmentBuilder& NGFragmentBuilder::SetIntrinsicBlockSize(
   return *this;
 }
 
-NGFragmentBuilder& NGFragmentBuilder::AddChild(
-    RefPtr<NGLayoutResult> child,
-    const NGLogicalOffset& child_offset) {
-  // Collect child's out of flow descendants.
-  for (const NGOutOfFlowPositionedDescendant& descendant :
-       child->OutOfFlowPositionedDescendants()) {
-    oof_positioned_candidates_.push_back(
-        NGOutOfFlowPositionedCandidate{descendant, child_offset});
-  }
-
-  return AddChild(child->PhysicalFragment(), child_offset);
-}
-
-NGFragmentBuilder& NGFragmentBuilder::AddChild(
+NGContainerFragmentBuilder& NGFragmentBuilder::AddChild(
     RefPtr<NGPhysicalFragment> child,
     const NGLogicalOffset& child_offset) {
   switch (child->Type()) {
@@ -86,10 +68,7 @@ NGFragmentBuilder& NGFragmentBuilder::AddChild(
       break;
   }
 
-  children_.push_back(std::move(child));
-  offsets_.push_back(child_offset);
-
-  return *this;
+  return NGContainerFragmentBuilder::AddChild(std::move(child), child_offset);
 }
 
 NGFragmentBuilder& NGFragmentBuilder::AddBreakBeforeChild(
@@ -120,11 +99,6 @@ NGFragmentBuilder& NGFragmentBuilder::PropagateBreak(
   return *this;
 }
 
-NGFragmentBuilder& NGFragmentBuilder::SetBfcOffset(const NGBfcOffset& offset) {
-  bfc_offset_ = offset;
-  return *this;
-}
-
 NGFragmentBuilder& NGFragmentBuilder::AddOutOfFlowChildCandidate(
     NGBlockNode child,
     const NGLogicalOffset& child_offset) {
@@ -143,8 +117,8 @@ NGFragmentBuilder& NGFragmentBuilder::AddOutOfFlowChildCandidate(
 void NGFragmentBuilder::AddOutOfFlowLegacyCandidate(
     NGBlockNode node,
     const NGStaticPosition& static_position) {
-  DCHECK_GE(size_.inline_size, LayoutUnit());
-  DCHECK_GE(size_.block_size, LayoutUnit());
+  DCHECK_GE(inline_size_, LayoutUnit());
+  DCHECK_GE(block_size_, LayoutUnit());
 
   NGOutOfFlowPositionedDescendant descendant{node, static_position};
   // Need 0,0 physical coordinates as child offset. Because offset
@@ -155,21 +129,21 @@ void NGFragmentBuilder::AddOutOfFlowLegacyCandidate(
       if (IsLtr(Direction()))
         zero_offset = NGLogicalOffset();
       else
-        zero_offset = NGLogicalOffset(size_.inline_size, LayoutUnit());
+        zero_offset = NGLogicalOffset(inline_size_, LayoutUnit());
       break;
     case kVerticalRightLeft:
     case kSidewaysRightLeft:
       if (IsLtr(Direction()))
-        zero_offset = NGLogicalOffset(LayoutUnit(), size_.block_size);
+        zero_offset = NGLogicalOffset(LayoutUnit(), block_size_);
       else
-        zero_offset = NGLogicalOffset(size_.inline_size, size_.block_size);
+        zero_offset = NGLogicalOffset(inline_size_, block_size_);
       break;
     case kVerticalLeftRight:
     case kSidewaysLeftRight:
       if (IsLtr(Direction()))
         zero_offset = NGLogicalOffset();
       else
-        zero_offset = NGLogicalOffset(size_.inline_size, LayoutUnit());
+        zero_offset = NGLogicalOffset(inline_size_, LayoutUnit());
       break;
   }
   oof_positioned_candidates_.push_back(
@@ -182,9 +156,9 @@ void NGFragmentBuilder::GetAndClearOutOfFlowDescendantCandidates(
 
   descendant_candidates->ReserveCapacity(oof_positioned_candidates_.size());
 
-  DCHECK_GE(size_.inline_size, LayoutUnit());
-  DCHECK_GE(size_.block_size, LayoutUnit());
-  NGPhysicalSize builder_physical_size{size_.ConvertToPhysical(WritingMode())};
+  DCHECK_GE(inline_size_, LayoutUnit());
+  DCHECK_GE(block_size_, LayoutUnit());
+  NGPhysicalSize builder_physical_size{Size().ConvertToPhysical(WritingMode())};
 
   for (NGOutOfFlowPositionedCandidate& candidate : oof_positioned_candidates_) {
     NGPhysicalOffset child_offset = candidate.child_offset.ConvertToPhysical(
@@ -209,18 +183,6 @@ void NGFragmentBuilder::GetAndClearOutOfFlowDescendantCandidates(
   oof_positioned_candidates_.clear();
 }
 
-NGFragmentBuilder& NGFragmentBuilder::AddOutOfFlowDescendant(
-    NGOutOfFlowPositionedDescendant descendant) {
-  oof_positioned_descendants_.push_back(descendant);
-  return *this;
-}
-
-NGFragmentBuilder& NGFragmentBuilder::SetExclusionSpace(
-    std::unique_ptr<const NGExclusionSpace> exclusion_space) {
-  exclusion_space_ = std::move(exclusion_space);
-  return *this;
-}
-
 void NGFragmentBuilder::AddBaseline(NGBaselineRequest request,
                                     LayoutUnit offset) {
 #if DCHECK_IS_ON()
@@ -233,7 +195,7 @@ void NGFragmentBuilder::AddBaseline(NGBaselineRequest request,
 RefPtr<NGLayoutResult> NGFragmentBuilder::ToBoxFragment() {
   DCHECK_EQ(offsets_.size(), children_.size());
 
-  NGPhysicalSize physical_size = size_.ConvertToPhysical(WritingMode());
+  NGPhysicalSize physical_size = Size().ConvertToPhysical(WritingMode());
 
   NGPhysicalOffsetRect contents_visual_rect({}, physical_size);
   for (size_t i = 0; i < children_.size(); ++i) {
