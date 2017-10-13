2b395a00cccaccd1fb31d6dc96d811fbaeaf94eb
Chris Harrelson <chrishtr@chromium.org>
2017-10-13 16:59:42 -0700

    Fully implement paint invalidation for fragments.
    Bug: 648274
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ia318bf84f4d7d22743dc26661772c8677fec80cf

--- third_party/WebKit/Source/core/frame/LocalFrameViewTest.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrameViewTest.cpp
@@ -234,8 +234,9 @@ TEST_P(LocalFrameViewTest, UpdateLifecyclePhasesForPrintingDetachedFrame) {
             ChildDocument().Lifecycle().GetState());
   if (RuntimeEnabledFeatures::RootLayerScrollingEnabled()) {
     auto* child_layout_view = ChildDocument().GetLayoutView();
-    ASSERT_TRUE(child_layout_view->FirstFragment());
-    EXPECT_TRUE(child_layout_view->FirstFragment()->PaintProperties());
+    EXPECT_TRUE(child_layout_view->FirstFragment()
+                    .GetRarePaintData()
+                    ->PaintProperties());
   } else {
     EXPECT_TRUE(ChildDocument().View()->PreTranslation());
   }
