21638fe51665dc3b05d032c1afd3e3eef8a1cb45
Sergey Ulanov <sergeyu@chromium.org>
2017-10-12 18:13:05 -0700

    Move desktop CMA backend to a separate derectory.
    Previously all files used to build CMA backend on desktop builds of
    cast_shell had "_default" suffix and were in the common backend
    directory. Moved them to a separate directory and renamed them to use
    "_desktop" suffix.
    
    Change-Id: I26cb1791f3a9b8dfd8c133f1ff84739f79df5a6c

--- chromecast/media/cma/backend/desktop/video_decoder_desktop.h
+++ chromecast/media/cma/backend/desktop/video_decoder_desktop.h
@@ -1,9 +1,9 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROMECAST_MEDIA_CMA_BACKEND_AUDIO_DECODER_DEFAULT_H_
-#define CHROMECAST_MEDIA_CMA_BACKEND_AUDIO_DECODER_DEFAULT_H_
+#ifndef CHROMECAST_MEDIA_CMA_BACKEND_VIDEO_DECODER_DESKTOP_H_
+#define CHROMECAST_MEDIA_CMA_BACKEND_VIDEO_DECODER_DESKTOP_H_
 
 #include <memory>
 
@@ -14,34 +14,32 @@
 namespace chromecast {
 namespace media {
 
-class MediaSinkDefault;
+class MediaSinkDesktop;
 
-class AudioDecoderDefault : public MediaPipelineBackend::AudioDecoder {
+class VideoDecoderDesktop : public MediaPipelineBackend::VideoDecoder {
  public:
-  AudioDecoderDefault();
-  ~AudioDecoderDefault() override;
+  VideoDecoderDesktop();
+  ~VideoDecoderDesktop() override;
 
   void Start(base::TimeDelta start_pts);
   void Stop();
   void SetPlaybackRate(float rate);
   base::TimeDelta GetCurrentPts();
 
-  // MediaPipelineBackend::AudioDecoder implementation:
+  // MediaPipelineBackend::VideoDecoder implementation:
   void SetDelegate(Delegate* delegate) override;
   MediaPipelineBackend::BufferStatus PushBuffer(
       CastDecoderBuffer* buffer) override;
   void GetStatistics(Statistics* statistics) override;
-  bool SetConfig(const AudioConfig& config) override;
-  bool SetVolume(float multiplier) override;
-  RenderingDelay GetRenderingDelay() override;
+  bool SetConfig(const VideoConfig& config) override;
 
  private:
   Delegate* delegate_;
-  std::unique_ptr<MediaSinkDefault> sink_;
-  DISALLOW_COPY_AND_ASSIGN(AudioDecoderDefault);
+  std::unique_ptr<MediaSinkDesktop> sink_;
+  DISALLOW_COPY_AND_ASSIGN(VideoDecoderDesktop);
 };
 
 }  // namespace media
 }  // namespace chromecast
 
-#endif  // CHROMECAST_MEDIA_CMA_BACKEND_AUDIO_DECODER_DEFAULT_H_
+#endif  // CHROMECAST_MEDIA_CMA_BACKEND_VIDEO_DECODER_DESKTOP_H_
