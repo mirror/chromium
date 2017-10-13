3eef4a0f5ae76db89922d02487ed32e2aa6553ff
Ehsan Chiniforooshan <chiniforooshan@chromium.org>
2017-10-13 12:49:34 -0400

    Tracing: set service process names
    Tested by running chrome --mash and capturing a trace in
    chrome://tracing. The trace viewer shows actual process names instead
    of the generic "Process (pid ????)" name.
    
    Change-Id: I0327a1b4d0dfba20699cc71dc420f0ee4035775b

--- content/utility/utility_service_factory.cc
+++ content/utility/utility_service_factory.cc
@@ -8,6 +8,7 @@
 
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/trace_event/trace_log.h"
 #include "build/build_config.h"
 #include "content/child/child_process.h"
 #include "content/network/network_service_impl.h"
@@ -21,6 +22,7 @@
 #include "media/media_features.h"
 #include "services/data_decoder/data_decoder_service.h"
 #include "services/data_decoder/public/interfaces/constants.mojom.h"
+#include "services/service_manager/public/interfaces/service.mojom.h"
 #include "services/shape_detection/public/interfaces/constants.mojom.h"
 #include "services/shape_detection/shape_detection_service.h"
 #include "services/video_capture/public/interfaces/constants.mojom.h"
@@ -111,6 +113,14 @@ UtilityServiceFactory::UtilityServiceFactory()
     : network_registry_(base::MakeUnique<service_manager::BinderRegistry>()) {}
 
 UtilityServiceFactory::~UtilityServiceFactory() {}
+
+void UtilityServiceFactory::CreateService(
+    service_manager::mojom::ServiceRequest request,
+    const std::string& name) {
+  base::trace_event::TraceLog::GetInstance()->SetProcessName(
+      ("Service: " + name).c_str());
+  ServiceFactory::CreateService(std::move(request), name);
+}
 
 void UtilityServiceFactory::RegisterServices(ServiceMap* services) {
   GetContentClient()->utility()->RegisterServices(services);
