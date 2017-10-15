8b28cf6fccb025c690f3140139adc1c62e0ff999
junweifu <junwei.fu@intel.com>
2017-10-15 11:06:00 +0800

    Move common functions to winrt util
    Shape Detection will use WRL to access Windows Runtime components,
    WrlStaticsFactory, CreateIBufferFromData and GetPointerToBufferData
    function also need to be used in CL[1], so move these common functions
    to winrt util.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/708336
    
    BUG=767021
    
    Change-Id: I054e273dc14fea648d1fed444d189ecd5e4292ca

--- base/win/core_winrt_util.cc
+++ base/win/core_winrt_util.cc
@@ -5,6 +5,12 @@
 #include "base/win/core_winrt_util.h"
 
 #include <roapi.h>
+#include <robuffer.h>
+#include <string.h>
+
+#include "base/strings/string_util.h"
+#include "base/win/scoped_comptr.h"
+#include "base/win/scoped_hstring.h"
 
 namespace {
 
@@ -32,6 +38,8 @@ decltype(&::RoGetActivationFactory) GetRoGetActivationFactoryFunction() {
 namespace base {
 namespace win {
 
+using namespace ABI::Windows::Storage::Streams;
+
 bool ResolveCoreWinRTDelayload() {
   // TODO(finnur): Add AssertIOAllowed once crbug.com/770193 is fixed.
 
@@ -54,6 +62,46 @@ HRESULT RoActivateInstance(HSTRING class_id, IInspectable** instance) {
   if (!activate_instance_func)
     return E_FAIL;
   return activate_instance_func(class_id, instance);
+}
+
+HRESULT GetPointerToBufferData(IBuffer* buffer, uint8_t** out) {
+  *out = nullptr;
+
+  ScopedComPtr<Windows::Storage::Streams::IBufferByteAccess> buffer_byte_access;
+  HRESULT hr = buffer->QueryInterface(IID_PPV_ARGS(&buffer_byte_access));
+  if (FAILED(hr))
+    return hr;
+
+  // Lifetime of the pointing buffer is controlled by the buffer object.
+  return buffer_byte_access->Buffer(out);
+}
+
+HRESULT CreateIBufferFromData(const uint8_t* data,
+                              UINT32 length,
+                              IBuffer** buffer) {
+  ScopedComPtr<IBufferFactory> buffer_factory;
+  HRESULT hr = base::win::GetActivationFactory<
+      IBufferFactory, RuntimeClass_Windows_Storage_Streams_Buffer>(
+      buffer_factory.GetAddressOf());
+  if (FAILED(hr))
+    return hr;
+
+  hr = buffer_factory->Create(length, buffer);
+  if (FAILED(hr))
+    return hr;
+
+  hr = (*buffer)->put_Length(length);
+  if (FAILED(hr))
+    return hr;
+
+  uint8_t* p_buffer_data;
+  hr = GetPointerToBufferData(*buffer, &p_buffer_data);
+  if (FAILED(hr))
+    return hr;
+
+  memcpy(p_buffer_data, data, length);
+
+  return S_OK;
 }
 
 }  // namespace win
