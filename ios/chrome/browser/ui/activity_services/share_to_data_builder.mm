0f0ff61fe550ba45bf69f78dbd6d27354936e84d
Sylvain Defresne <sdefresne@chromium.org>
2017-10-15 11:52:38 -0700

    Inline call to Tab -originalTitle.
    There was only one call to Tab -originalTitle, so inline it and
    remove the property from Tab.
    
    Bug: none
    Change-Id: Idc574da6818df54eac30df23afb636444b119456

--- ios/chrome/browser/ui/activity_services/share_to_data_builder.mm
+++ ios/chrome/browser/ui/activity_services/share_to_data_builder.mm
@@ -5,9 +5,12 @@
 #include "ios/chrome/browser/ui/activity_services/share_to_data_builder.h"
 
 #include "base/logging.h"
+#import "base/strings/sys_string_conversions.h"
 #include "ios/chrome/browser/tabs/tab.h"
 #include "ios/chrome/browser/ui/activity_services/chrome_activity_item_thumbnail_generator.h"
 #include "ios/chrome/browser/ui/activity_services/share_to_data.h"
+#import "ios/web/public/navigation_item.h"
+#import "ios/web/public/navigation_manager.h"
 #import "ios/web/public/web_state/web_state.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -17,6 +20,7 @@
 namespace activity_services {
 
 ShareToData* ShareToDataForTab(Tab* tab) {
+  DCHECK(tab);
   // For crash documented in crbug.com/503955, tab.url which is being passed
   // as a reference parameter caused a crash due to invalid address which
   // which suggests that |tab| may be deallocated along the way. Check that
@@ -24,17 +28,27 @@ ShareToData* ShareToDataForTab(Tab* tab) {
   // tab is being closed.
   if (!tab.webState)
     return nil;
-  DCHECK(tab);
   // If the original page title exists, it is expected to match the tab title.
   // If this ever changes, then a decision has to be made on which one should
   // be used for sharing.
-  DCHECK(!tab.originalTitle || [tab.originalTitle isEqualToString:tab.title]);
+  BOOL isOriginalTitle = NO;
+  DCHECK(tab.webState->GetNavigationManager());
+  web::NavigationItem* lastCommittedItem =
+      tab.webState->GetNavigationManager()->GetLastCommittedItem();
+  if (lastCommittedItem) {
+    const base::string16& originalTitle = lastCommittedItem->GetTitle();
+    if (!originalTitle.empty()) {
+      DCHECK(
+          [tab.title isEqualToString:base::SysUTF16ToNSString(originalTitle)]);
+      isOriginalTitle = YES;
+    }
+  }
   BOOL isPagePrintable = [tab viewForPrinting] != nil;
   ThumbnailGeneratorBlock thumbnailGenerator =
       activity_services::ThumbnailGeneratorForTab(tab);
   return [[ShareToData alloc] initWithURL:tab.webState->GetVisibleURL()
                                     title:tab.title
-                          isOriginalTitle:(tab.originalTitle != nil)
+                          isOriginalTitle:isOriginalTitle
                           isPagePrintable:isPagePrintable
                        thumbnailGenerator:thumbnailGenerator];
 }
