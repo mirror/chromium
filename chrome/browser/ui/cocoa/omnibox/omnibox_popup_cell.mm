98137356c702168730e246c26d975e4f25b6c35e
Nico Weber <thakis@chromium.org>
2017-10-16 17:54:35 -0400

    mac: Remove deprecated ObjCPropertyReleaser.
    No intended behavior change.
    
    Bug: none
    Change-Id: I2ccdeb903e7500acbc40c1098339b6e8ab5a0f0f

--- chrome/browser/ui/cocoa/omnibox/omnibox_popup_cell.mm
+++ chrome/browser/ui/cocoa/omnibox/omnibox_popup_cell.mm
@@ -12,7 +12,7 @@
 #include "base/feature_list.h"
 #include "base/i18n/rtl.h"
 #include "base/mac/foundation_util.h"
-#include "base/mac/objc_property_releaser.h"
+#include "base/mac/objc_release_properties.h"
 #include "base/mac/scoped_nsobject.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/strings/string_number_conversions.h"
@@ -358,9 +358,7 @@ NSAttributedString* CreateClassifiedAttributedString(
 
 }  // namespace
 
-@interface OmniboxPopupCellData () {
-  base::mac::ObjCPropertyReleaser propertyReleaser_OmniboxPopupCellData_;
-}
+@interface OmniboxPopupCellData ()
 @end
 
 @interface OmniboxPopupCell ()
@@ -441,6 +439,11 @@ NSAttributedString* CreateClassifiedAttributedString(
                                                 [OmniboxPopupCellData class]);
   }
   return self;
+}
+
+- (void)dealloc {
+  base::mac::ReleaseProperties(self);
+  [super dealloc];
 }
 
 - (instancetype)copyWithZone:(NSZone*)zone {
