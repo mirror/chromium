c5bd11924e3951ed8d17602df9d3ce4617a829cb
Elad Alon <eladalon@chromium.org>
2017-10-16 10:42:15 +0200

    RtcEventLogOutputApplicationProxy
    WIP
    
    Bug:TODO
    Change-Id: Ibb6a8fb5edb243d0cabcd79e9db2a0452ddb22e4

---
+++ content/renderer/media/rtc_event_log_output_sink_proxy.cc
@@ -0,0 +1,25 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/logging.h"
+#include "content/renderer/media/rtc_event_log_output_sink_proxy.h"
+
+namespace content {
+
+RtcEventLogOutputSinkProxy::RtcEventLogOutputSinkProxy(
+    RtcEventLogOutputSink* sink)
+    : sink_(sink) {
+  CHECK(sink_);
+}
+
+bool RtcEventLogOutputSinkProxy::IsActive() const {
+  return true;  // Active until the proxy is destroyed.
+}
+
+bool RtcEventLogOutputSinkProxy::Write(const std::string& output) {
+  sink_->OnRtcEventLogWrite(output);
+  return true;
+}
+
+}  // namespace content
