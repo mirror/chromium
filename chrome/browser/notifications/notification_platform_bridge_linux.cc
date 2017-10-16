cd9c2313373a66e9372c159a1baebec7b1d3bdec
Finnur Thorarinsson <finnur@chromium.org>
2017-10-16 18:08:46 +0000

    Work in progress, not ready for review/checkin.
    Bug:
    Change-Id: I2926dc33b489f150a75f97792266e1a31a4ea5a2

--- chrome/browser/notifications/notification_platform_bridge_linux.cc
+++ chrome/browser/notifications/notification_platform_bridge_linux.cc
@@ -115,13 +115,6 @@ base::string16 CreateNotificationTitle(const Notification& notification) {
   return title;
 }
 
-gfx::Image DeepCopyImage(const gfx::Image& image) {
-  if (image.IsEmpty())
-    return gfx::Image();
-  std::unique_ptr<gfx::ImageSkia> image_skia(image.CopyImageSkia());
-  return gfx::Image(*image_skia);
-}
-
 void EscapeUnsafeCharacters(std::string* message) {
   // Canonical's notification development guidelines recommends only
   // escaping the '&', '<', and '>' characters:
@@ -300,14 +293,8 @@ class NotificationPlatformBridgeLinuxImpl
     // notification and its images.  Wrap the notification in a
     // unique_ptr to transfer ownership of the notification (and the
     // non-thread-safe reference counts) to the task runner thread.
-    auto notification_copy = std::make_unique<Notification>(notification);
-    notification_copy->set_icon(DeepCopyImage(notification_copy->icon()));
-    notification_copy->set_image(body_images_supported_.value()
-                                     ? DeepCopyImage(notification_copy->image())
-                                     : gfx::Image());
-    notification_copy->set_small_image(gfx::Image());
-    for (size_t i = 0; i < notification_copy->buttons().size(); i++)
-      notification_copy->SetButtonIcon(i, gfx::Image());
+    auto notification_copy = Notification::DeepCopy(
+        notification, body_images_supported_.value(), false, false);
 
     PostTaskToTaskRunnerThread(base::BindOnce(
         &NotificationPlatformBridgeLinuxImpl::DisplayOnTaskRunner, this,
@@ -499,11 +486,12 @@ class NotificationPlatformBridgeLinuxImpl
   }
 
   // Makes the "Notify" call to D-Bus.
-  void DisplayOnTaskRunner(NotificationCommon::Type notification_type,
-                           const std::string& notification_id,
-                           const std::string& profile_id,
-                           bool is_incognito,
-                           std::unique_ptr<Notification> notification) {
+  void DisplayOnTaskRunner(
+      NotificationCommon::Type notification_type,
+      const std::string& notification_id,
+      const std::string& profile_id,
+      bool is_incognito,
+      std::unique_ptr<message_center::Notification> notification) {
     DCHECK(task_runner_->RunsTasksInCurrentSequence());
     NotificationData* data =
         FindNotificationData(notification_id, profile_id, is_incognito);
