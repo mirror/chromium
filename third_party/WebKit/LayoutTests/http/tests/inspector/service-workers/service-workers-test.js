e5c4d7d89054621fca8c910a3da6abd5e4baaed9
Tsuyoshi Horo <horo@chromium.org>
2017-10-16 18:18:53 +0900

    Check "Bypass for network" flag in DocumentThreadableLoader
    There is a bug that CORS requests can't work at all when "Bypass for network"
    in DevTools is enabled.
    
    Currently the CORS logic is implemented in DocumentThreadableLoader in the
    renderer process. If the page is not controlled by a service worker,
    DocumentThreadableLoader sets the Origin header for the CORS request and sends
    the request to the browser process. And the browser process sends the request to
    the network server.
    
    But if the page is controlled by a service worker, DocumentThreadableLoader just
    sends the original request to the browser process. And the browser process sends 
    the request to the service worker. If the service worker doesn't call
    FetchEvent.respondWith(), the browser process returns a "FallbackRequired"
    response to the DocumentThreadableLoader. And DocumentThreadableLoader will
    start the CORS logic such as setting the Origin header.
    
    When "Bypass for network" is enabled, InspectorNetworkAgent::WillSendRequest()
    sets the request service worker mode to None. So the browser process doesn't
    pass the request to the service worker.
    
    But InspectorNetworkAgent::WillSendRequest() is called just before the renderer
    process sends the request to the browser process. So the request which should be
    sent to the service worker will directly go to the network server.
    
    This CL adds a check of the bypass flag in DocumentThreadableLoader, so that the
    CORS logic will be correctly executed.
    
    Bug: 771742
    Change-Id: I923fc9f9e2e361aea3d25ace653c138707d9a682

--- third_party/WebKit/LayoutTests/http/tests/inspector/service-workers/service-workers-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/service-workers/service-workers-test.js
@@ -1,3 +1,33 @@
 var initialize_ServiceWorkersTest = function() {
     InspectorTest.preloadModule('application_test_runner');
 }
+
+function loadIframe(url, frame_id) {
+  let callback;
+  let promise = new Promise((fulfill) => callback = fulfill);
+  let frame = document.createElement('iframe');
+  if (frame_id !== undefined) {
+    frame.id = frame_id;
+  }
+  frame.src = url;
+  frame.onload = callback;
+  document.body.appendChild(frame);
+  return promise;
+}
+
+function unloadIframe(frame_id) {
+  document.getElementById(frame_id).remove();
+}
+
+function waitForActivated(worker) {
+  if (worker.state === 'activated')
+    return Promise.resolve();
+  if (worker.state === 'redundant')
+    return Promise.reject(new Error('The worker is redundant'));
+  return new Promise(resolve => {
+      worker.addEventListener('statechange', () => {
+          if (worker.state === 'activated')
+            resolve();
+        });
+    });
+}
