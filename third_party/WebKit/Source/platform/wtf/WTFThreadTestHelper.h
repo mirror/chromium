357f6c130ef27425f6a8f2178c854cbc291399c1
Peter Mayo <petermayo@chromium.org>
2017-10-16 10:01:18 -0400

    Add a not-main-thread testing helper
    There are pieces of threaded code where classes are intended to run
    on particular threads, and they are littered with
    "DCHECK(!IsMainThread)" but simple unittests to exercise the logic
    don't gain benefit from these checks.  This simple little helper
    can make it easy to pretend to step off the main thread in a unit
    test.  Hopefully when there's less friction to writing unittests
    there will be more unittests.
    
    BUG=None
    
    Change-Id: I4a4f3667f59878696f635b864273fcd92182735a

---
+++ third_party/WebKit/Source/platform/wtf/WTFThreadTestHelper.h
@@ -0,0 +1,30 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WTFThreadTestHelper_h
+#define WTFThreadTestHelper_h
+// TODO(petermayo): Add an automatic rule to prevent including this outside
+// of unittests.
+
+#include "platform/wtf/Threading.h"
+#include "platform/wtf/WTF.h"
+
+namespace WTF {
+
+// While this class is on the stack the value of IsMainThread will be inverted.
+// This is for use in unittesting only.  For example where the thread
+// sctructure is not set up, but our classes may be DCHECKing it for product
+// stability.
+class NotOnMainThread {
+ public:
+  NotOnMainThread();
+  ~NotOnMainThread();
+
+ private:
+  ThreadIdentifier old_main_;
+};
+
+}  // namespace WTF
+
+#endif  // WTF_WTFThreadTestHelper_h
