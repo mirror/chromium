d2cddb7d3d86e572e6ac21d91d7e9371102c9fec
Robert Ogden <robertogden@chromium.org>
2017-10-16 11:28:36 -0700

    Add test for hijacked secure proxy connections
    Also adds basic framework for a connection emulation server for future use.
    
    Bug: 766866
    Change-Id: I8c34a80d65cb614f4833d962b9cafbe02b0075b2

---
+++ tools/chrome_proxy/webdriver/proxy_connection.py
@@ -0,0 +1,35 @@
+ # Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import sys
+import traceback
+
+import common
+from common import TestDriver
+from common import IntegrationTest
+from emulation_server import InvalidTLSHandler
+
+
+class ProxyConnection(IntegrationTest):
+
+  def testTLSInjectionAfterHandshake(self):
+    port = common.GetOpenPort()
+    with TestDriver() as t:
+      t.AddChromeArg('--enable-spdy-proxy-auth')
+      t.AddChromeArg(
+        '--data-reduction-proxy-http-proxies=https://localhost:%d' % port)
+      t.AddChromeArg(
+        '-force-fieldtrials=DataReductionProxyConfigService/Disabled')
+      t.UseEmulationServer(InvalidTLSHandler, port=port)
+
+      t.LoadURL('http://check.googlezip.net/test.html')
+      responses = t.GetHTTPResponses()
+      # Expect responses with a bypass on a bad proxy. If the test failed, the
+      # next assertion will fail because there will be no responses.
+      self.assertEqual(2, len(responses))
+      for response in responses:
+        self.assertNotHasChromeProxyViaHeader(response)
+
+if __name__ == '__main__':
+  IntegrationTest.RunAllTests()
