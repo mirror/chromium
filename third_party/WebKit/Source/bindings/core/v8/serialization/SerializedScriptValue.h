2382c228f1b488d88c4038d26d3c5f6f9731c8cc
Jeremy Roman <jbroman@chromium.org>
2017-10-16 17:04:23 -0400

    Have SerializedScriptValue::GetWireData return a base::span.
    StringView wasn't an ideal type, since this is binary data, not character data.
    
    Bug: 770862
    Change-Id: I24108f10f8171815d52704d028a93fd01e2f44cf

--- third_party/WebKit/Source/bindings/core/v8/serialization/SerializedScriptValue.h
+++ third_party/WebKit/Source/bindings/core/v8/serialization/SerializedScriptValue.h
@@ -33,6 +33,7 @@
 
 #include <memory>
 
+#include "base/containers/span.h"
 #include "bindings/core/v8/NativeValueTraits.h"
 #include "bindings/core/v8/ScriptValue.h"
 #include "bindings/core/v8/serialization/Transferables.h"
@@ -146,8 +147,8 @@ class CORE_EXPORT SerializedScriptValue
   String ToWireString() const;
   void ToWireBytes(Vector<char>&) const;
 
-  StringView GetWireData() const {
-    return StringView(data_buffer_.get(), data_buffer_size_);
+  base::span<const uint8_t> GetWireData() const {
+    return {data_buffer_.get(), data_buffer_size_};
   }
 
   // Deserializes the value (in the current context). Returns a null value in
