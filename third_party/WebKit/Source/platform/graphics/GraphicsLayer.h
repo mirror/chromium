8213125f567f83da8a1e5bde4c61e8aa9abf86ac
Xianzhu Wang <wangxianzhu@chromium.org>
2017-10-16 12:11:44 -0700

    Enable PaintChunk-based raster invalidation for SPv175
    - Change SlimmingPaintV2Enabled guarding raster invalidation code
      to SlimmingPaintV175Enabled
    
    - Let raster under-invalidation checking work for SPv175 
    
    Bug: 771643
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I7f42db48a655dad433d9f02474f2dbf5a38a66ad

--- third_party/WebKit/Source/platform/graphics/GraphicsLayer.h
+++ third_party/WebKit/Source/platform/graphics/GraphicsLayer.h
@@ -56,6 +56,7 @@
 
 namespace blink {
 
+class CompositedLayerRasterInvalidator;
 class CompositorFilterOperations;
 class CompositedLayerRasterInvalidator;
 class Image;
@@ -340,7 +341,9 @@ class PLATFORM_EXPORT GraphicsLayer : public cc::LayerClient,
 
   sk_sp<PaintRecord> CaptureRecord();
 
+  Vector<const PaintChunk*> AllChunkPointers() const;
   CompositedLayerRasterInvalidator& EnsureRasterInvalidator();
+  void SetNeedsDisplayInRectInternal(const IntRect&);
 
   GraphicsLayerClient* client_;
 
