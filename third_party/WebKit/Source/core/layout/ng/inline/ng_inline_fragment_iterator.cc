9a80d29c0e1b7e092e72abf035992893fc38def6
Koji Ishii <kojii@chromium.org>
2017-10-17 05:16:39 +0900

    vrinline
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I55c607a40fcf705c46162577fd9a1255f3048da7

---
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_inline_fragment_iterator.cc
@@ -0,0 +1,68 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/layout/ng/inline/ng_inline_fragment_iterator.h"
+
+#include "core/layout/LayoutObject.h"
+#include "core/layout/ng/ng_physical_box_fragment.h"
+#include "core/layout/ng/ng_physical_container_fragment.h"
+
+namespace blink {
+
+NGInlineFragmentIterator::NGInlineFragmentIterator(
+    const NGPhysicalContainerFragment* container,
+    const LayoutObject* target)
+    : target_(target) {
+  DCHECK(container);
+  DCHECK(target);
+  container_stack_.push_back(ContainerStackEntry{container, 0, {}});
+  MoveToNextIfCurrentIsNotValid();
+}
+
+void NGInlineFragmentIterator::operator++() {
+  if (!container_stack_.IsEmpty()) {
+    container_stack_.back().child_index++;
+    MoveToNextIfCurrentIsNotValid();
+  }
+}
+
+NGInlineFragmentIterator::Result NGInlineFragmentIterator::operator*() const {
+  DCHECK(!container_stack_.IsEmpty());
+  const ContainerStackEntry& container = container_stack_.back();
+  const NGPhysicalFragment& fragment =
+      *container.fragment->Children()[container.child_index];
+  return {fragment, fragment.Offset() + container.offset_to_container_box};
+}
+
+void NGInlineFragmentIterator::MoveToNextIfCurrentIsNotValid() {
+  for (; !container_stack_.IsEmpty();) {
+    ContainerStackEntry& container = container_stack_.back();
+    const auto& children = container.fragment->Children();
+    for (unsigned child_index = container.child_index;; child_index++) {
+      if (child_index >= children.size()) {
+        container_stack_.pop_back();
+        break;
+      }
+
+      const NGPhysicalFragment& child = *children[child_index];
+      const LayoutObject* child_layout_object = child.GetLayoutObject();
+      if (child_layout_object == target_) {
+        container.child_index = child_index;
+        return;
+      }
+
+      if (child.IsContainer() &&
+          !(child.IsBox() &&
+            ToNGPhysicalBoxFragment(child).IsBlockLayoutBoundary())) {
+        container.child_index = child_index + 1;
+        container_stack_.push_back(ContainerStackEntry{
+            ToNGPhysicalContainerFragment(&child), 0,
+            child.Offset() + container.offset_to_container_box});
+        break;
+      }
+    }
+  }
+}
+
+}  // namespace blink
