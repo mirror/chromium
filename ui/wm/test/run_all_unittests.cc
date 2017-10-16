7bfcadd88f020ee70157bf10dc566d54209c1426
Mike Reed <reed@google.com>
2017-10-16 14:20:25 -0400

    remove obsolete skia flags
    The x86 flag is just dead code
    
    The blur flag removal enables our special-case rrect blur algo, which differs a tiny bit from path, hence the rebaseline.
    
    Bug:
    Change-Id: Ife83eed40cc741658d0df311441ba0986a0659ae

--- ui/wm/test/run_all_unittests.cc
+++ ui/wm/test/run_all_unittests.cc
@@ -9,6 +9,7 @@
 #include "base/macros.h"
 #include "base/path_service.h"
 #include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/test_discardable_memory_allocator.h"
 #include "base/test/test_suite.h"
 #include "mojo/edk/embedder/embedder.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -30,6 +31,9 @@ class WMTestSuite : public base::TestSuite {
     base::FilePath ui_test_pak_path;
     ASSERT_TRUE(PathService::Get(ui::UI_TEST_PAK, &ui_test_pak_path));
     ui::ResourceBundle::InitSharedInstanceWithPakPath(ui_test_pak_path);
+
+    base::DiscardableMemoryAllocator::SetInstance(
+        &discardable_memory_allocator_);
 
     env_ = aura::Env::CreateInstance();
   }
