2f0fee3146c7a9c86d4338a443eac48037c8e72d
Edman Anjos <edmanp@google.com>
2017-10-16 20:16:23 +0200

    Fix mojom java constant name generation on strings with numbers
    NameToComponent would behave incorrectly in strings with digits such as
    "SHA1", where it produced "SH_A1" instead of the expected "SHA1". This
    CL fixes the regexes to account for digits in the input.
    
    Bug: NA
    Change-Id: Ic924a1fdf098f9b506ce664d0d8067cc8109d8b2

---
+++ mojo/android/javatests/src/org/chromium/mojo/bindings/NameGeneratorTest.java
@@ -0,0 +1,69 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.mojo.bindings;
+
+import android.support.test.filters.SmallTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.mojo.bindings.test.mojom.sample.NameGeneratorConstants;
+import org.chromium.mojo.bindings.test.mojom.sample.SupportedCases;
+
+/**
+ * Test mojom constant names generated for java.
+ */
+@RunWith(BaseJUnit4ClassRunner.class)
+public class NameGeneratorTest {
+    @Test
+    @SmallTest
+    public void testLowerCamelCase() {
+        Assert.assertTrue(classHasField(SupportedCases.class, "LOWER_CAMEL_CASE"));
+    }
+
+    @Test
+    @SmallTest
+    public void testUpperCamelCase() {
+        Assert.assertTrue(classHasField(SupportedCases.class, "UPPER_CAMEL_CASE"));
+    }
+
+    @Test
+    @SmallTest
+    public void testSnakeCase() {
+        Assert.assertTrue(classHasField(SupportedCases.class, "SNAKE_CASE"));
+    }
+
+    @Test
+    @SmallTest
+    public void testMacroCase() {
+        Assert.assertTrue(classHasField(SupportedCases.class, "MACRO_CASE"));
+    }
+
+    @Test
+    @SmallTest
+    public void testHungarianNotation() {
+        Assert.assertTrue(classHasField(SupportedCases.class, "HUNGARIAN_NOTATION"));
+    }
+
+    @Test
+    @SmallTest
+    public void testNames() {
+        Assert.assertTrue(classHasField(NameGeneratorConstants.class, "PAD_RSA_PKCS1_1_5_SIGN"));
+        Assert.assertTrue(classHasField(NameGeneratorConstants.class, "DIGEST_SHA1"));
+        Assert.assertTrue(classHasField(NameGeneratorConstants.class, "E2E_INTEGRATION"));
+        Assert.assertTrue(classHasField(NameGeneratorConstants.class, "U_N_D_E_F_I_N_E_D"));
+    }
+
+    private static <T> boolean classHasField(Class<T> clazz, String fieldName) {
+        try {
+            clazz.getField(fieldName);
+            return true;
+        } catch (NoSuchFieldException e) {
+            return false;
+        }
+    }
+}
