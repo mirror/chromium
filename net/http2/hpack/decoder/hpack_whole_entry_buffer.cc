f976e5fc19438c083549090a3ad3c9c2da9c1cb4
David 'Digit' Turner <digit@chromium.org>
2017-10-16 17:43:36 +0200

    Avoid sending binary data to the log.
    The HTTP/2 hpack decoder used to send binary packet data directly
    to the log when --verbose=2 (or greater) is being used. This is bad
    for a couple of reasons:
    
    - Totally not human-friendly, as logs should be.
    - Sometimes messing with the user's terminal for Linux and OS X builds.
    - Crashing the adb logcat wrapper very rapidly at runtime for
      Android builds, making it impossible to get all logs.
    
    This patch fixes the issue by outputting an hexadecimal dump of the
    packet bytes instead. Which look like, on Android like:
    
    10-16 16:08:21.893 22307 22380 V chromium: [VERBOSE1:hpack_entry_decoder.cc(134)] kStartDecodingValue: db->Remaining=1043
    10-16 16:08:21.893 22307 22380 V chromium: [VERBOSE2:hpack_whole_entry_buffer.cc(215)] HpackWholeEntryBuffer::OnValueStart: huffman_encoded=true,  len=22
    10-16 16:08:21.893 22307 22380 V chromium: [VERBOSE2:hpack_decoder_string_buffer.cc(75)] HpackDecoderStringBuffer::OnStart
    10-16 16:08:21.893 22307 22380 V chromium: [VERBOSE2:hpack_whole_entry_buffer.cc(229)] HpackWholeEntryBuffer::OnValueData: len=22
    10-16 16:08:21.893 22307 22380 V chromium: data: d0 7a be 94 0b 8a 6a 22 54 10 02 ea 81 6a e0 1e |.z....j"T....j..|
    10-16 16:08:21.893 22307 22380 V chromium: data: b8 20 a9 8b 46 ff                               |. ..F.          |
    
    BUG=None
    R=bnc@chromium.org,gavinp@chromium.org
    
    Change-Id: I6a50c07cf21f3cbbed4b5be1403dc0c5d491ba99

--- net/http2/hpack/decoder/hpack_whole_entry_buffer.cc
+++ net/http2/hpack/decoder/hpack_whole_entry_buffer.cc
@@ -7,7 +7,131 @@
 #include "base/logging.h"
 #include "base/trace_event/memory_usage_estimator.h"
 
+#include <string.h>
+#include <algorithm>
+
 namespace net {
+
+namespace {
+
+// Convenience class to dump binary data as human-friendly lines of hexadecimal
+// dump, which look like:
+//
+// 62 3a 90 c1 64 62 33 d9 33 05 91 88 ea 43 04 a0 |b:..db3.3....C..|
+// c5 a9 2b 5b 5c 83 49 58 25 06 2d 49 b6 b9 06 92 |..+[\.IX%.-I....|
+// ad 5d 87 91 61 da 85 c7 6a 4f 6d 72 0d 25 41 6c |.]..a...jOmr.%Al|
+// ee 5b 18 10 1d fa 2d 14 32 0b 8d 34 cf          |.[....-.2..4.   |
+//
+// Usage is the following:
+//
+//  1/ Create new HexDumper instance, passing a pointer to the data and
+//     its size to the constructor.
+//
+//  2/ Optional: Call GetLineCount() to get the number of lines of output
+//     that will be generated.
+//
+//  3/ Call HasNextLine() and GetNextLine() to iterate over the generated
+//     output.
+//
+// E.g.:
+//      HexDumper dumper(data, len);
+//      while (dumper.HasNextLine()) {
+//         std::cout << dumper.GetNextLine() << "\n";
+//      }
+//
+class HexDumper {
+ public:
+  // Default constructor.
+  HexDumper() = default;
+
+  // Constructor with explicit |data| and |data_len| parameters.
+  HexDumper(const void* data, size_t data_len)
+      : data_(static_cast<const char*>(data)),
+        data_end_(static_cast<const char*>(data) + data_len) {}
+
+  // Constructor that takes a base::StringPiece, or anything that converts
+  // to it.
+  HexDumper(base::StringPiece str) : HexDumper(str.data(), str.size()) {}
+
+  // Reset the dumper to another piece of data.
+  void Reset(const void* data, size_t data_len) {
+    data_ = static_cast<const char*>(data);
+    data_end_ = data_ + data_len;
+  }
+
+  // Return true iff there is another line of output.
+  bool HasNextLine() const { return data_ < data_end_; }
+
+  // Return the number of lines of output remaining.
+  size_t GetLineCount() const {
+    return (data_end_ - data_ + kMaxDumpWidth - 1) / kMaxDumpWidth;
+  }
+
+  // Return
+  base::StringPiece GetNextLine() {
+    size_t avail =
+        std::min(static_cast<size_t>(data_end_ - data_), kMaxDumpWidth);
+    if (avail == 0) {
+      return {nullptr, 0};
+    }
+    char* hex_line = line_buffer_;
+    char* char_line = line_buffer_ + kMaxDumpWidth * 3 + 1;
+    for (size_t n = 0; n < avail; ++n) {
+      static const char kHexDigits[] = "0123456789abcdef";
+      uint8_t val = static_cast<uint8_t>(data_[n]);
+      hex_line[n * 3] = kHexDigits[(val >> 4) & 15];
+      hex_line[n * 3 + 1] = kHexDigits[val & 15];
+      hex_line[n * 3 + 2] = ' ';
+      char_line[n] = (val >= 32 && val < 127) ? static_cast<char>(val) : '.';
+    }
+    for (size_t n = avail; n < kMaxDumpWidth; ++n) {
+      hex_line[n * 3] = ' ';
+      hex_line[n * 3 + 1] = ' ';
+      hex_line[n * 3 + 2] = ' ';
+      char_line[n] = ' ';
+    }
+    char_line[-1] = '|';
+    char_line[kMaxDumpWidth] = '|';
+    char_line[kMaxDumpWidth + 1] = '\0';
+
+    data_ += avail;
+
+    return {line_buffer_, kMaxDumpWidth * 4 + 2};
+  }
+
+ private:
+  static constexpr size_t kMaxDumpWidth = 16;
+  const char* data_ = nullptr;
+  const char* data_end_ = nullptr;
+  char line_buffer_[kMaxDumpWidth * 4 + 3];
+};
+
+// Perform a human-friendly hexadecimal dump of an array of bytes.
+// |prefix| will be pre-pended to every output line, and cannot be nullptr.
+// |data| and |len| describe the input bytes to be dumped.
+// This generates a new string that looks like the following (using \n as
+// a line separator):
+//
+// <prefix>62 3a 90 c1 64 62 33 d9 33 05 91 88 ea 43 04 a0 |b:..db3.3....C..|
+// <prefix>c5 a9 2b 5b 5c 83 49 58 25 06 2d 49 b6 b9 06 92 |..+[\.IX%.-I....|
+// <prefix>ad 5d 87 91 61 da 85 c7 6a 4f 6d 72 0d 25 41 6c |.]..a...jOmr.%Al|
+// <prefix>ee 5b 18 10 1d fa 2d 14 32 0b 8d 34 cf          |.[....-.2..4.   |
+//
+static std::string HexDump(const char* prefix, const char* data, size_t len) {
+  std::string result;
+  HexDumper dumper(data, len);
+  while (dumper.HasNextLine()) {
+    auto line = dumper.GetNextLine();
+    if (!result.empty()) {
+      result.append(1, '\n');
+    }
+    result.append(prefix);
+    result.append(line.data(), line.size());
+  }
+  return result;
+}
+
+}  // namespace
 
 HpackWholeEntryBuffer::HpackWholeEntryBuffer(HpackWholeEntryListener* listener,
                                              size_t max_string_size_bytes)
@@ -65,8 +189,8 @@ void HpackWholeEntryBuffer::OnNameStart(bool huffman_encoded, size_t len) {
 }
 
 void HpackWholeEntryBuffer::OnNameData(const char* data, size_t len) {
-  DVLOG(2) << "HpackWholeEntryBuffer::OnNameData: len=" << len
-           << "\n data: " << Http2StringPiece(data, len);
+  DVLOG(2) << "HpackWholeEntryBuffer::OnNameData: len=" << len << "\n"
+           << HexDump("data: ", data, len);
   DCHECK_EQ(maybe_name_index_, 0u);
   if (!error_detected_ && !name_.OnData(data, len)) {
     ReportError("Error decoding HPACK entry name.");
@@ -96,8 +220,8 @@ void HpackWholeEntryBuffer::OnValueStart(bool huffman_encoded, size_t len) {
 }
 
 void HpackWholeEntryBuffer::OnValueData(const char* data, size_t len) {
-  DVLOG(2) << "HpackWholeEntryBuffer::OnValueData: len=" << len
-           << "\n data: " << Http2StringPiece(data, len);
+  DVLOG(2) << "HpackWholeEntryBuffer::OnValueData: len=" << len << "\n"
+           << HexDump("data: ", data, len);
   if (!error_detected_ && !value_.OnData(data, len)) {
     ReportError("Error decoding HPACK entry value.");
   }
