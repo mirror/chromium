582cc91490191a6f443e5dddfe1c14c1caef56e0
Francois Doray <fdoray@chromium.org>
2017-10-16 10:04:32 -0400

    AssertBaseSyncPrimitivesAllowed() in Process::WaitForExit*().
    Bug: 766678
    Change-Id: I1d5cc98984b84a412492db5f83c8db4b20396367

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -5,6 +5,8 @@
 #ifndef BASE_THREADING_THREAD_RESTRICTIONS_H_
 #define BASE_THREADING_THREAD_RESTRICTIONS_H_
 
+#include <string>
+
 #include "base/base_export.h"
 #include "base/gtest_prod_util.h"
 #include "base/logging.h"
@@ -187,8 +189,10 @@ class ScopedAllowBlockingForTesting {
   DISALLOW_COPY_AND_ASSIGN(ScopedAllowBlockingForTesting);
 };
 
-// "Waiting on a //base sync primitive" refers to calling
-// base::WaitableEvent::*Wait* or base::ConditionVariable::*Wait*.
+// "Waiting on a //base sync primitive" refers to calling one of these methods:
+// - base::WaitableEvent::*Wait*
+// - base::ConditionVariable::*Wait*
+// - base::Process::WaitForExit*
 
 // Disallows waiting on a //base sync primitive on the current thread.
 INLINE_IF_DCHECK_IS_OFF void DisallowBaseSyncPrimitives()
@@ -197,11 +201,15 @@ INLINE_IF_DCHECK_IS_OFF void DisallowBaseSyncPrimitives()
 // ScopedAllowBaseSyncPrimitives(ForTesting)(OutsideBlockingScope) allow waiting
 // on a //base sync primitive within a scope where this is normally disallowed.
 //
-// Avoid using this. Instead of waiting on a WaitableEvent or a
-// ConditionVariable, put the work that should happen after the wait in a
-// callback and post that callback from where the WaitableEvent or
-// ConditionVariable would have been signaled. If something needs to be
-// scheduled after many tasks have executed, use base::BarrierClosure.
+// Avoid using this.
+//
+// Instead of waiting on a WaitableEvent or a ConditionVariable, put the work
+// that should happen after the wait in a callback and post that callback from
+// where the WaitableEvent or ConditionVariable would have been signaled. If
+// something needs to be scheduled after many tasks have executed, use
+// base::BarrierClosure.
+//
+// On Windows, join processes asynchronously using base::win::ObjectWatcher.
 
 // This can only be used in a scope where blocking is allowed.
 class BASE_EXPORT ScopedAllowBaseSyncPrimitives {
@@ -218,6 +226,7 @@ class BASE_EXPORT ScopedAllowBaseSyncPrimitives {
   friend class leveldb::LevelDBMojoProxy;
   friend class media::BlockingUrlProtocol;
   friend class net::OCSPScopedAllowBaseSyncPrimitives;
+  friend std::string GetLinuxDistro();
 
   ScopedAllowBaseSyncPrimitives() EMPTY_BODY_IF_DCHECK_IS_OFF;
   ~ScopedAllowBaseSyncPrimitives() EMPTY_BODY_IF_DCHECK_IS_OFF;
