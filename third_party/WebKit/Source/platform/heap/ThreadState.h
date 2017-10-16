3a8da1ef000e12c8d0cae32d2bc683fc9837d173
Keishi Hattori <keishi@chromium.org>
2017-10-16 19:35:10 +0900

    Refactor SafePointScope related code into StackRoots
    Bug:
    Change-Id: I90fc6107e77f08869e574b5d8a493f521b218677

--- third_party/WebKit/Source/platform/heap/ThreadState.h
+++ third_party/WebKit/Source/platform/heap/ThreadState.h
@@ -52,11 +52,9 @@ class Isolate;
 
 namespace blink {
 
-class BasePage;
 class GarbageCollectedMixinConstructorMarkerBase;
 class PersistentNode;
 class PersistentRegion;
-class BaseArena;
 class ThreadHeap;
 class ThreadState;
 class Visitor;
@@ -224,6 +222,7 @@ class PLATFORM_EXPORT ThreadState {
   bool CheckThread() const { return thread_ == CurrentThread(); }
 
   ThreadHeap& Heap() const { return *heap_; }
+  ThreadIdentifier ThreadId() const { return thread_; }
 
   // When ThreadState is detaching from non-main thread its
   // heap is expected to be empty (because it is going away).
@@ -286,7 +285,6 @@ class PLATFORM_EXPORT ThreadState {
   // - isInGC() returns true between 1) and 3).
   // - isSweepingInProgress() returns true while any sweeping operation is
   //   running.
-  void MakeConsistentForGC();
   void MarkPhasePrologue(BlinkGC::StackState,
                          BlinkGC::GCType,
                          BlinkGC::GCReason);
@@ -296,12 +294,6 @@ class PLATFORM_EXPORT ThreadState {
   void CompleteSweep();
   void PreSweep(BlinkGC::GCType);
   void PostSweep();
-  // makeConsistentForMutator() drops marks from marked objects and rebuild
-  // free lists. This is called after taking a snapshot and before resuming
-  // the executions of mutators.
-  void MakeConsistentForMutator();
-
-  void Compact();
 
   // Support for disallowing allocation. Mainly used for sanity
   // checks asserts.
@@ -371,57 +363,7 @@ class PLATFORM_EXPORT ThreadState {
 
   void FlushHeapDoesNotContainCacheIfNeeded();
 
-  // Safepoint related functionality.
-  //
-  // When a thread attempts to perform GC it needs to stop all other threads
-  // that use the heap or at least guarantee that they will not touch any
-  // heap allocated object until GC is complete.
-  //
-  // We say that a thread is at a safepoint if this thread is guaranteed to
-  // not touch any heap allocated object or any heap related functionality until
-  // it leaves the safepoint.
-  //
-  // Notice that a thread does not have to be paused if it is at safepoint it
-  // can continue to run and perform tasks that do not require interaction
-  // with the heap. It will be paused if it attempts to leave the safepoint and
-  // there is a GC in progress.
-  //
-  // Each thread that has ThreadState attached must:
-  //   - periodically check if GC is requested from another thread by calling a
-  //     safePoint() method;
-  //   - use SafePointScope around long running loops that have no safePoint()
-  //     invocation inside, such loops must not touch any heap object;
-  //
-  // Check if GC is requested by another thread and pause this thread if this is
-  // the case.  Can only be called when current thread is in a consistent state.
   void SafePoint(BlinkGC::StackState);
-
-  // Mark current thread as running inside safepoint.
-  void EnterSafePoint(BlinkGC::StackState, void*);
-  void LeaveSafePoint();
-
-  void RecordStackEnd(intptr_t* end_of_stack) { end_of_stack_ = end_of_stack; }
-  NO_SANITIZE_ADDRESS void CopyStackUntilSafePointScope();
-
-  // Get one of the heap structures for this thread.
-  // The thread heap is split into multiple heap parts based on object types
-  // and object sizes.
-  BaseArena* Arena(int arena_index) const {
-    DCHECK_LE(0, arena_index);
-    DCHECK_LT(arena_index, BlinkGC::kNumberOfArenas);
-    return arenas_[arena_index];
-  }
-
-#if DCHECK_IS_ON()
-  // Infrastructure to determine if an address is within one of the
-  // address ranges for the Blink heap. If the address is in the Blink
-  // heap the containing heap page is returned.
-  BasePage* FindPageFromAddress(Address);
-  BasePage* FindPageFromAddress(const void* pointer) {
-    return FindPageFromAddress(
-        reinterpret_cast<Address>(const_cast<void*>(pointer)));
-  }
-#endif
 
   // A region of PersistentNodes allocated on the given thread.
   PersistentRegion* GetPersistentRegion() const {
@@ -449,14 +391,6 @@ class PLATFORM_EXPORT ThreadState {
     Vector<size_t> live_size;
     Vector<size_t> dead_size;
   };
-
-  size_t ObjectPayloadSizeForTesting();
-
-  void ShouldFlushHeapDoesNotContainCache() {
-    should_flush_heap_does_not_contain_cache_ = true;
-  }
-
-  bool IsAddressInHeapDoesNotContainCache(Address);
 
   void RegisterTraceDOMWrappers(
       v8::Isolate* isolate,
@@ -495,57 +429,6 @@ class PLATFORM_EXPORT ThreadState {
       gc_mixin_marker_ = nullptr;
     }
   }
-
-  // vectorBackingArena() returns an arena that the vector allocation should
-  // use.  We have four vector arenas and want to choose the best arena here.
-  //
-  // The goal is to improve the succession rate where expand and
-  // promptlyFree happen at an allocation point. This is a key for reusing
-  // the same memory as much as possible and thus improves performance.
-  // To achieve the goal, we use the following heuristics:
-  //
-  // - A vector that has been expanded recently is likely to be expanded
-  //   again soon.
-  // - A vector is likely to be promptly freed if the same type of vector
-  //   has been frequently promptly freed in the past.
-  // - Given the above, when allocating a new vector, look at the four vectors
-  //   that are placed immediately prior to the allocation point of each arena.
-  //   Choose the arena where the vector is least likely to be expanded
-  //   nor promptly freed.
-  //
-  // To implement the heuristics, we add an arenaAge to each arena. The arenaAge
-  // is updated if:
-  //
-  // - a vector on the arena is expanded; or
-  // - a vector that meets the condition (*) is allocated on the arena
-  //
-  //   (*) More than 33% of the same type of vectors have been promptly
-  //       freed since the last GC.
-  //
-  BaseArena* VectorBackingArena(size_t gc_info_index) {
-    DCHECK(CheckThread());
-    size_t entry_index = gc_info_index & kLikelyToBePromptlyFreedArrayMask;
-    --likely_to_be_promptly_freed_[entry_index];
-    int arena_index = vector_backing_arena_index_;
-    // If likely_to_be_promptly_freed_[entryIndex] > 0, that means that
-    // more than 33% of vectors of the type have been promptly freed
-    // since the last GC.
-    if (likely_to_be_promptly_freed_[entry_index] > 0) {
-      arena_ages_[arena_index] = ++current_arena_ages_;
-      vector_backing_arena_index_ =
-          ArenaIndexOfVectorArenaLeastRecentlyExpanded(
-              BlinkGC::kVector1ArenaIndex, BlinkGC::kVector4ArenaIndex);
-    }
-    DCHECK(IsVectorArenaIndex(arena_index));
-    return arenas_[arena_index];
-  }
-  BaseArena* ExpandedVectorBackingArena(size_t gc_info_index);
-  static bool IsVectorArenaIndex(int arena_index) {
-    return BlinkGC::kVector1ArenaIndex <= arena_index &&
-           arena_index <= BlinkGC::kVector4ArenaIndex;
-  }
-  void AllocationPointAdjusted(int arena_index);
-  void PromptlyFreed(size_t gc_info_index);
 
   void AccumulateSweepingTime(double time) {
     accumulated_sweeping_time_ += time;
@@ -607,15 +490,9 @@ class PLATFORM_EXPORT ThreadState {
  private:
   template <typename T>
   friend class PrefinalizerRegistration;
-  enum SnapshotType { kHeapSnapshot, kFreelistSnapshot };
 
   ThreadState();
   ~ThreadState();
-
-  void ClearSafePointScopeMarker() {
-    safe_point_stack_copy_.clear();
-    safe_point_scope_marker_ = nullptr;
-  }
 
   // shouldSchedule{Precise,Idle}GC and shouldForceConservativeGC
   // implement the heuristics that are used to determine when to collect
@@ -660,23 +537,9 @@ class PLATFORM_EXPORT ThreadState {
 
   void EagerSweep();
 
-#if defined(ADDRESS_SANITIZER)
-  void PoisonEagerArena();
-  void PoisonAllHeaps();
-#endif
-
-  void RemoveAllPages();
-
   void InvokePreFinalizers();
 
-  void TakeSnapshot(SnapshotType);
-  void ClearArenaAges();
-  int ArenaIndexOfVectorArenaLeastRecentlyExpanded(int begin_arena_index,
-                                                   int end_arena_index);
-
   void ReportMemoryToV8();
-
-  friend class SafePointScope;
 
   friend class BlinkGCObserver;
 
@@ -705,11 +568,7 @@ class PLATFORM_EXPORT ThreadState {
   ThreadIdentifier thread_;
   std::unique_ptr<PersistentRegion> persistent_region_;
   BlinkGC::StackState stack_state_;
-  intptr_t* start_of_stack_;
-  intptr_t* end_of_stack_;
 
-  void* safe_point_scope_marker_;
-  Vector<Address> safe_point_stack_copy_;
   bool sweep_forbidden_;
   size_t no_allocation_count_;
   size_t gc_forbidden_count_;
@@ -717,14 +576,8 @@ class PLATFORM_EXPORT ThreadState {
   double accumulated_sweeping_time_;
   bool object_resurrection_forbidden_;
 
-  BaseArena* arenas_[BlinkGC::kNumberOfArenas];
-  int vector_backing_arena_index_;
-  size_t arena_ages_[BlinkGC::kNumberOfArenas];
-  size_t current_arena_ages_;
-
   GarbageCollectedMixinConstructorMarkerBase* gc_mixin_marker_;
 
-  bool should_flush_heap_does_not_contain_cache_;
   GCState gc_state_;
 
   using PreFinalizerCallback = bool (*)(void*);
@@ -757,15 +610,6 @@ class PLATFORM_EXPORT ThreadState {
   // Count that controls scoped disabling of persistent registration.
   size_t disabled_static_persistent_registration_;
 #endif
-
-  // Ideally we want to allocate an array of size |gcInfoTableMax| but it will
-  // waste memory. Thus we limit the array size to 2^8 and share one entry
-  // with multiple types of vectors. This won't be an issue in practice,
-  // since there will be less than 2^8 types of objects in common cases.
-  static const int kLikelyToBePromptlyFreedArraySize = (1 << 8);
-  static const int kLikelyToBePromptlyFreedArrayMask =
-      kLikelyToBePromptlyFreedArraySize - 1;
-  std::unique_ptr<int[]> likely_to_be_promptly_freed_;
 
   size_t reported_memory_to_v8_;
 
