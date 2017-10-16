a1097a414c48e1dfe0fe6b47dbe3de71f9dd08f7
kylechar <kylechar@chromium.org>
2017-10-16 18:03:36 -0400

    Replace BrowserGpuChannelFactory.
    WIP test.
    
    Bug:
    Change-Id: I550f05402fa117099cadce846c432eb55b3ccef2

---
+++ content/browser/gpu/browser_gpu_client.cc
@@ -0,0 +1,52 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/gpu/browser_gpu_client.h"
+
+#include <utility>
+
+#include "content/public/browser/browser_thread.h"
+
+namespace content {
+namespace {
+
+constexpr int kGpuClientTracingId = 1;
+
+BrowserGpuClient* g_browser_gpu_client = nullptr;
+
+}  // namespace
+
+// static
+void BrowserGpuClient::Create(int browser_client_id) {
+  LOG(ERROR) << "BrowserGpuClient::Create";
+  DCHECK(!g_browser_gpu_client);
+  g_browser_gpu_client = new BrowserGpuClient(browser_client_id);
+}
+
+// static
+void BrowserGpuClient::Destroy() {
+  LOG(ERROR) << "BrowserGpuClient::Destroy";
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  // DCHECK(g_browser_gpu_client);
+  delete g_browser_gpu_client;
+}
+
+// static
+BrowserGpuClient* BrowserGpuClient::Get() {
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
+  DCHECK(g_browser_gpu_client);
+  return g_browser_gpu_client;
+}
+
+void BrowserGpuClient::BindGpuRequest(ui::mojom::GpuRequest request) {
+  gpu_client_.Add(std::move(request));
+}
+
+BrowserGpuClient::BrowserGpuClient(int browser_client_id)
+    : gpu_memory_buffer_manager_(browser_client_id, kGpuClientTracingId),
+      gpu_client_(browser_client_id, true /* privileged */) {}
+
+BrowserGpuClient::~BrowserGpuClient() {}
+
+}  // namespace content
