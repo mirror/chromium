e9ff331d23b502ee8a8b1ce583f577b32b8ec54c
Thanh Pham <thanhph@chromium.org>
2017-10-17 16:50:53 -0400

    IME for Mus: Send TextInputClient information to IMEDriver.
    Client sends text range, selection range, and composition range to
    IMEDriver when initializing a session. Client can also update selection
    range and delete range.
    
    Bug:631527
    Change-Id: I762445575c9baee86fc7298efa944ad23a84d4f1

--- ui/views/controls/prefix_selector.h
+++ ui/views/controls/prefix_selector.h
@@ -45,6 +45,11 @@ class VIEWS_EXPORT PrefixSelector : public ui::TextInputClient {
                                      gfx::Rect* rect) const override;
   bool HasCompositionText() const override;
   bool GetTextRange(gfx::Range* range) const override;
+  bool GetTextAndSelectionRange(
+      base::OnceCallback<void(bool,
+                              const gfx::Range&,
+                              const base::string16&,
+                              const gfx::Range&)> callback) const override;
   bool GetCompositionTextRange(gfx::Range* range) const override;
   bool GetSelectionRange(gfx::Range* range) const override;
   bool SetSelectionRange(const gfx::Range& range) override;
