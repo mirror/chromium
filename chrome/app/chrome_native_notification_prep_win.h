f2c21b3a8620af06c04c64a1af07941297875200
Xi Cheng <chengx@chromium.org>
2017-10-17 13:57:55 -0700

    Register Chrome with Windows 10 for native notification support
    Bug:734095
    Change-Id: Ic220c72a15eb24f395b5173339e606dd5ffe66bb

---
+++ chrome/app/chrome_native_notification_prep_win.h
@@ -0,0 +1,10 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_APP_CHROME_NATIVE_NOTIFICATION_PREP_WIN_H_
+#define CHROME_APP_CHROME_NATIVE_NOTIFICATION_PREP_WIN_H_
+
+void PrepareForNativeNotification();
+
+#endif  // CHROME_APP_CHROME_NATIVE_NOTIFICATION_PREP_WIN_H_
