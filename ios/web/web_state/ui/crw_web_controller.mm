2b5318ff7d1fdbe4128ba50aefc344f7cf8956cb
Eugene But <eugenebut@google.com>
2017-10-17 16:44:55 -0700

    Do not call triggerPendingLoad from -[CRWWebController view].
    - Loading URL is quite unexpected side effect
    - Not every caller of this API actually need to load the page
      sometimes they just need to know view bounds and loading
      a URL would be the waste of resources
    - triggerPendingLoad may display placeholder overlay. Placeholder overlay
      presentation should not be a part of ios/web and should be done
      in chrome layer explicitly, not as a side effect of displaying the view
    
    Bug:
    Change-Id: Ibb64c588241d1718ed2eb91623305eacb127d401

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -1360,9 +1360,6 @@ const NSTimeInterval kSnapshotOverlayTransition = 0.5;
 }
 
 - (UIView*)view {
-  // Kick off the process of lazily creating the view and starting the load if
-  // necessary; this creates _containerView if it doesn't exist.
-  [self triggerPendingLoad];
   DCHECK(_containerView);
   return _containerView;
 }
