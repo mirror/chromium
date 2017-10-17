7182501025f46a5a1f63f54db1804dadc39ee9b1
Sean O'Brien <seobrien@google.com>
2017-10-17 10:06:00 -0700

    Blink: Snap page scale to 100% on trackpad pinch end
    If a trackpad pinch ends at scale level 105% or lower, snap to 100% with
    a scale animation.
    
    BUG=769934
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I6d7e185951900f11afadf80c7eeea16f8a4156b6

--- ui/events/blink/input_handler_proxy_unittest.cc
+++ ui/events/blink/input_handler_proxy_unittest.cc
@@ -167,7 +167,7 @@ class MockInputHandler : public cc::InputHandler {
   MOCK_METHOD0(PinchGestureBegin, void());
   MOCK_METHOD2(PinchGestureUpdate,
                void(float magnify_delta, const gfx::Point& anchor));
-  MOCK_METHOD0(PinchGestureEnd, void());
+  MOCK_METHOD2(PinchGestureEnd, void(const gfx::Point& anchor, bool snap));
 
   MOCK_METHOD0(SetNeedsAnimateInput, void());
 
@@ -916,7 +916,7 @@ TEST_P(InputHandlerProxyTest, GesturePinch) {
   VERIFY_AND_RESET_MOCKS();
 
   gesture_.SetType(WebInputEvent::kGesturePinchEnd);
-  EXPECT_CALL(mock_input_handler_, PinchGestureEnd());
+  EXPECT_CALL(mock_input_handler_, PinchGestureEnd(gfx::Point(9, 6), true));
   EXPECT_EQ(expected_disposition_, input_handler_->HandleInputEvent(gesture_));
 
   VERIFY_AND_RESET_MOCKS();
@@ -1017,7 +1017,7 @@ TEST_P(InputHandlerProxyTest, GesturePinchAfterScrollOnMainThread) {
   VERIFY_AND_RESET_MOCKS();
 
   gesture_.SetType(WebInputEvent::kGesturePinchEnd);
-  EXPECT_CALL(mock_input_handler_, PinchGestureEnd());
+  EXPECT_CALL(mock_input_handler_, PinchGestureEnd(gfx::Point(9, 6), true));
   EXPECT_EQ(expected_disposition_, input_handler_->HandleInputEvent(gesture_));
 
   // After the pinch gesture ends, they should go to back to the main
@@ -3820,7 +3820,7 @@ TEST_P(InputHandlerProxyEventQueueTest, VSyncAlignedGestureScrollPinchScroll) {
   // Two |GesturePinchUpdate| will be coalesced.
   EXPECT_CALL(mock_input_handler_,
               PinchGestureUpdate(0.7f, gfx::Point(13, 17)));
-  EXPECT_CALL(mock_input_handler_, PinchGestureEnd());
+  EXPECT_CALL(mock_input_handler_, PinchGestureEnd(gfx::Point(), true));
 
   HandleGestureEvent(WebInputEvent::kGestureScrollUpdate, -30);
   HandleGestureEvent(WebInputEvent::kGestureScrollEnd);
