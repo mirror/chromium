da463cca47d3b9bdcedaf0d6fc469b87107f0b0f
Alex Moshchuk <alexmos@chromium.org>
2017-10-17 23:56:00 -0700

    Use speculative RFH with PlzNavigate when changing accessibility mode.
    Bug:
    Change-Id: I1c4653f684ebc08584f00fac34b0c2f1aae00181

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1063,7 +1063,9 @@ void WebContentsImpl::SetAccessibilityMode(ui::AXMode mode) {
   for (FrameTreeNode* node : frame_tree_.Nodes()) {
     UpdateAccessibilityModeOnFrame(node->current_frame_host());
     RenderFrameHost* pending_frame_host =
-        node->render_manager()->pending_frame_host();
+        IsBrowserSideNavigationEnabled()
+            ? node->render_manager()->speculative_frame_host()
+            : node->render_manager()->pending_frame_host();
     if (pending_frame_host)
       UpdateAccessibilityModeOnFrame(pending_frame_host);
   }
