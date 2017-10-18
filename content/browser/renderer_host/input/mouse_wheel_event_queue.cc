630d74deeaf3c7773239001e00c39dd8b51282b5
Sahel Sharify <sahel@chromium.org>
2017-10-18 18:29:47 -0400

    Uma metric added to record latched vs non-latched wheel scrolling.
    Bug:774324
    Test:MouseWheelEventQueueTest.WheelScrollingWasLatchedHistogramCheck/*
    Change-Id: I243d693a9d70b40f764577b2b7b94b894c515ba0

--- content/browser/renderer_host/input/mouse_wheel_event_queue.cc
+++ content/browser/renderer_host/input/mouse_wheel_event_queue.cc
@@ -184,7 +184,11 @@ void MouseWheelEventQueue::ProcessMouseWheelAck(
         // Wheel event with phaseBegan must have non-zero deltas.
         DCHECK(needs_update);
         send_wheel_events_async_ = true;
+        UMA_HISTOGRAM_BOOLEAN("WheelScrolling.WasLatched", false);
         SendScrollBegin(scroll_update, false);
+      } else if (needs_update) {
+        UMA_HISTOGRAM_BOOLEAN("WheelScrolling.WasLatched",
+                              !needs_scroll_begin_);
       }
 
       if (needs_update) {
@@ -225,6 +229,7 @@ void MouseWheelEventQueue::ProcessMouseWheelAck(
         }
 
         if (needs_update) {
+          UMA_HISTOGRAM_BOOLEAN("WheelScrolling.WasLatched", false);
           ui::LatencyInfo latency = ui::LatencyInfo(ui::SourceEventType::WHEEL);
           latency.AddLatencyNumber(
               ui::INPUT_EVENT_LATENCY_GENERATE_SCROLL_UPDATE_FROM_MOUSE_WHEEL,
