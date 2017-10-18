764ce7f359cdbdf6e646423f3fb769e64abd0ae6
Bo Liu <boliu@chromium.org>
2017-10-18 13:58:49 -0700

    Test for webview gc with accessibility
    Bug: 773659
    Change-Id: I999e0b841f41230d1cfa629ca097fd8299fafded

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -1742,6 +1742,7 @@ public class ContentViewCore implements AccessibilityStateChangeListener, Displa
             mWebContentsAccessibility = WebContentsAccessibility.create(mContext, mContainerView,
                     mWebContents, mRenderCoordinates, mShouldSetAccessibilityFocusOnPageLoad);
             mWebContentsAccessibility.enable();
+            return mWebContentsAccessibility.getAccessibilityNodeProvider();
         }
         return null;
     }
