4a5c22780ab04281b8f9b4ca61c64ed55c9acfff
Thomas Tangl <tangltom@chromium.org>
2017-10-18 14:28:17 +0200

    EditableProfilePhoto ~> BadgedProfilePhoto
    The class EditableProfilePhoto was renamed to BadgedProfilePhoto and was
    slightly modified because it did not and will not be editable. The class 
    was moved to a separate file, so it easier to maintain and extend (which 
    will happen in the implementation for the DICe UI). 
    
    Bug:
    Change-Id: I25fb7b1f3bf9f168ca23a90390365b13679c4eb6

---
+++ chrome/browser/ui/views/profiles/badged_profile_photo.h
@@ -0,0 +1,48 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_PROFILES_BADGED_PROFILE_PHOTO_H_
+#define CHROME_BROWSER_UI_VIEWS_PROFILES_BADGED_PROFILE_PHOTO_H_
+
+#include <stddef.h>
+
+#include "ui/views/controls/button/label_button.h"
+
+class BadgedProfilePhoto : public views::LabelButton {
+ public:
+  enum BadgeType {
+    // No badge
+    BADGE_TYPE_NONE,
+    BADGE_TYPE_SUPERVISED,
+    BADGE_TYPE_CHILD,
+  };
+
+  BadgedProfilePhoto(views::ButtonListener* listener,
+                     BadgeType badge_type,
+                     const gfx::Image& icon);
+
+  void PaintChildren(const views::PaintInfo& paint_info) override;
+
+  // The space between the right/bottom edge of the profile badge and the
+  // right/bottom edge of the profile icon.
+  static const int kBadgeSpacing = 4;
+
+ private:
+  // views::Button:
+  void StateChanged(ButtonState old_state) override;
+
+  void OnFocus() override;
+
+  void OnBlur() override;
+
+  // Image that is shown when hovering over the image button. Can be NULL if
+  // the photo isn't allowed to be edited (e.g. for guest profiles).
+  views::ImageView* photo_overlay_;
+
+  BadgeType badge_type_;
+
+  DISALLOW_COPY_AND_ASSIGN(BadgedProfilePhoto);
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_PROFILES_BADGED_PROFILE_PHOTO_H_
