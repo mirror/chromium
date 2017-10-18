06c70b28e081f4eda564679651b582d1b2a0ab68
Francois Doray <fdoray@chromium.org>
2017-10-18 09:28:51 -0400

    Enable TabManagerTest.DiscardTabsWithMinimizedAndOccludedWindows.
    Issues in this test should have been fixed by
    https://chromium-review.googlesource.com/c/chromium/src/+/695324
    
    Bug: 772839
    Change-Id: I5abe02046be2dcbcb17768c3533f23237aa47422

--- chrome/browser/resource_coordinator/tab_manager_browsertest.cc
+++ chrome/browser/resource_coordinator/tab_manager_browsertest.cc
@@ -822,16 +822,8 @@ Browser* CreateBrowserWithTabs(int num_tabs) {
 
 }  // namespace
 
-// Flaky on Linux.  Times out on Windows debug builds. http://crbug.com/772839.
-#if defined(OS_LINUX) || (defined(OS_WIN) && !defined(NDEBUG))
-#define MAYBE_DiscardTabsWithMinimizedAndOccludedWindows \
-  DISABLED_DiscardTabsWithMinimizedAndOccludedWindows
-#else
-#define MAYBE_DiscardTabsWithMinimizedAndOccludedWindows \
-  DiscardTabsWithMinimizedAndOccludedWindows
-#endif
 IN_PROC_BROWSER_TEST_F(TabManagerTest,
-                       MAYBE_DiscardTabsWithMinimizedAndOccludedWindows) {
+                       DiscardTabsWithMinimizedAndOccludedWindows) {
   TabManager* tab_manager = g_browser_process->GetTabManager();
 
   // Disable the protection of recent tabs.
