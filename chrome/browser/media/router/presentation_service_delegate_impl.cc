59ead2df1a0dfe73e76c15d53d26079e50fc0a8e
Takumi Fujimoto <takumif@chromium.org>
2017-10-17 23:31:56 -0700

    [Media Router] Local MRP
    This CL implements the WiredDisplayMediaRouteProvider, which is responsible for making wired displays available as media sinks.
    
    Bug:758005
    Change-Id: I0c882826a5e3c79bf58a2d9dd49a3e070ef2e861

--- chrome/browser/media/router/presentation_service_delegate_impl.cc
+++ chrome/browser/media/router/presentation_service_delegate_impl.cc
@@ -33,6 +33,8 @@
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/common/presentation_info.h"
+#include "ui/display/display.h"
+#include "ui/display/screen.h"
 #include "url/gurl.h"
 
 #if !defined(OS_ANDROID)
@@ -238,6 +240,8 @@ void PresentationFrame::ConnectToPresentation(
     auto* offscreen_presentation_manager =
         OffscreenPresentationManagerFactory::GetOrCreateForWebContents(
             web_contents_);
+    LOG(ERROR) << "register offscreen presentation controller for "
+               << presentation_info.presentation_url;
     offscreen_presentation_manager->RegisterOffscreenPresentationController(
         presentation_info, render_frame_host_id_,
         std::move(controller_connection_ptr),
@@ -313,6 +317,9 @@ PresentationServiceDelegateImpl::PresentationServiceDelegateImpl(
     : web_contents_(web_contents),
       router_(MediaRouterFactory::GetApiForBrowserContext(
           web_contents_->GetBrowserContext())),
+      offscreen_presentation_manager_(
+          OffscreenPresentationManagerFactory::GetOrCreateForWebContents(
+              web_contents_)),
       weak_factory_(this) {
   DCHECK(web_contents_);
   DCHECK(router_);
@@ -419,6 +426,7 @@ void PresentationServiceDelegateImpl::OnJoinRouteResponse(
 
 void PresentationServiceDelegateImpl::OnStartPresentationSucceeded(
     const RenderFrameHostId& render_frame_host_id,
+    int64_t display_id,
     content::PresentationConnectionCallback success_cb,
     const content::PresentationInfo& new_presentation_info,
     const MediaRoute& route) {
@@ -427,7 +435,16 @@ void PresentationServiceDelegateImpl::OnStartPresentationSucceeded(
            << ", presentation URL: " << new_presentation_info.presentation_url
            << ", presentation ID: " << new_presentation_info.presentation_id;
   AddPresentation(render_frame_host_id, new_presentation_info, route);
+  offscreen_presentation_manager_->RemovePendingController(display_id);
   std::move(success_cb).Run(new_presentation_info);
+}
+
+void PresentationServiceDelegateImpl::OnStartPresentationFailed(
+    int64_t display_id,
+    content::PresentationConnectionErrorCallback error_cb,
+    const content::PresentationError& error) {
+  offscreen_presentation_manager_->RemovePendingController(display_id);
+  std::move(error_cb).Run(error);
 }
 
 void PresentationServiceDelegateImpl::AddPresentation(
@@ -469,14 +486,27 @@ void PresentationServiceDelegateImpl::StartPresentation(
     return;
   }
 
+  // TODO: this
+  auto* offscreen_presentation_manager =
+      OffscreenPresentationManagerFactory::GetOrCreateForWebContents(
+          web_contents_);
+  const int64_t display_id =
+      display::Screen::GetScreen()
+          ->GetDisplayNearestView(web_contents_->GetNativeView())
+          .id();
+  offscreen_presentation_manager->AddPendingController(display_id);
+
   MediaRouterDialogController* controller =
       MediaRouterDialogController::GetOrCreateForWebContents(web_contents_);
   if (!controller->ShowMediaRouterDialogForPresentation(
           request,
           base::BindOnce(
               &PresentationServiceDelegateImpl::OnStartPresentationSucceeded,
-              GetWeakPtr(), render_frame_host_id, std::move(success_cb)),
-          std::move(error_cb))) {
+              GetWeakPtr(), render_frame_host_id, display_id,
+              std::move(success_cb)),
+          base::BindOnce(
+              &PresentationServiceDelegateImpl::OnStartPresentationFailed,
+              GetWeakPtr(), display_id, std::move(error_cb)))) {
     LOG(ERROR)
         << "StartPresentation failed: unable to create Media Router dialog.";
   }
