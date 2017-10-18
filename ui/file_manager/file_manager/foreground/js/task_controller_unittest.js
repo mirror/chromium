69357604311d8916bde4564356fd25ac8a186443
Klemen Kozjek <klemenko@google.com>
2017-10-18 16:16:06 +0900

    Zip Archiver: Use two switches to toggle zip/unzip feature
    As a result, we can enable/disable just zip or unzip feature
    from Zip Archiver extension.
    
    Bug: 359837
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I42488235d7523a55a66cedd376540070108c3ae0

--- ui/file_manager/file_manager/foreground/js/task_controller_unittest.js
+++ ui/file_manager/file_manager/foreground/js/task_controller_unittest.js
@@ -2,6 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+window.chrome = {
+  commandLinePrivate: {
+    hasSwitch: function(name, callback) {
+      callback(false);
+    }
+  }
+};
+
 window.metrics = {
   recordEnum: function() {}
 };
