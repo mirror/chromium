d4a126b55a53666419dda7caac1ed2ff3705dddd
Tom Sepez <tsepez@chromium.org>
2017-10-18 16:21:21 -0700

    Move sandbox_win.cc to services/service_manager/sandbox/win
    Bug: 708738
    Change-Id: I57fb3c3878aaa97207c85c1c1200b22707a138d6

---
+++ services/service_manager/sandbox/sandbox.h
@@ -0,0 +1,44 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICE_MANAGER_SANDBOX_SANDBOX_H_
+#define SERVICE_MANAGER_SANDBOX_SANDBOX_H_
+
+#include "build/build_config.h"
+#include "services/service_manager/sandbox/export.h"
+#include "services/service_manager/sandbox/sandbox_type.h"
+
+#if defined(OS_MACOSX)
+#include "base/callback.h"
+#include "base/files/file_path.h"
+#endif  // defined(OS_MACOSX)
+
+namespace sandbox {
+class SandboxInterfaceInfo;
+}  // namespace sandbox
+
+namespace service_manager {
+
+// Interface to the service manager sandboxes across the various platforms.
+class SERVICE_MANAGER_SANDBOX_EXPORT Sandbox {
+ public:
+#if defined(OS_LINUX)
+// TODO(tsepez): move linux code here.
+#endif  // defined(OS_LINUX)
+
+#if defined(OS_MACOSX)
+  static bool Initialize(service_manager::SandboxType sandbox_type,
+                         const base::FilePath& allowed_dir,
+                         base::OnceClosure hook);
+#endif  // defined(OS_MACOSX)
+
+#if defined(OS_WIN)
+  static bool Initialize(service_manager::SandboxType sandbox_type,
+                         sandbox::SandboxInterfaceInfo* sandbox_info);
+#endif  // defined(OS_WIN)
+};
+
+}  // namespace service_manager
+
+#endif  // SERVICE_MANAGER_SANDBOX_SANDBOX_H_
