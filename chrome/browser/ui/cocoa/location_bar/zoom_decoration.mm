c087a7f4471a8a4fe3336b8c2329c0852190d168
Trent Apted <tapted@chromium.org>
2017-10-18 22:15:04 +1100

    Split toolkit-views dialog plumbing on Mac into "Pilot" and "All" categories.
    Pilot dialogs will be put behind --secondary-ui-md.
    Other dialogs get a separate, chrome://flags/#show-all-dialogs-with-views-toolkit
    
    This allows us to roll out Harmony UI to Mac in stages on Mac, where it is a
    major change.
    
    A side-effect of this CL means that with just pilot dialogs, some Cocoa
    dialogs will be anchored differently, even though they still use an arrow.
    
    The first round of pilot dialogs are:
     - Add Bookmark (bubble)
     - HTTP Auth (tab modal)
     - Upgrade chrome (window modal)
     - Zoom (bubble)
    
    Bug: 775848
    Change-Id: If127171ebeb9f626219036aa9696d7626b81d989

--- chrome/browser/ui/cocoa/location_bar/zoom_decoration.mm
+++ chrome/browser/ui/cocoa/location_bar/zoom_decoration.mm
@@ -20,14 +20,19 @@
 #include "components/zoom/zoom_controller.h"
 #include "ui/base/cocoa/cocoa_base_utils.h"
 #include "ui/base/l10n/l10n_util_mac.h"
-#include "ui/base/material_design/material_design_controller.h"
 #import "ui/gfx/mac/coordinate_conversion.h"
+
+namespace {
+bool UseViews() {
+  return chrome::ShowPilotDialogsWithViewsToolkit();
+}
+}  // namespace
 
 ZoomDecoration::ZoomDecoration(LocationBarViewMac* owner)
     : owner_(owner), bubble_(nullptr), vector_icon_(nullptr) {}
 
 ZoomDecoration::~ZoomDecoration() {
-  if (ui::MaterialDesignController::IsSecondaryUiMaterial()) {
+  if (UseViews()) {
     CloseBubble();
     return;
   }
@@ -84,7 +89,7 @@ void ZoomDecoration::ShowBubble(BOOL auto_close) {
   const NSRect frame =
       [[field cell] frameForDecoration:this inFrame:[field bounds]];
 
-  if (ui::MaterialDesignController::IsSecondaryUiMaterial()) {
+  if (UseViews()) {
     NSWindow* window = [web_contents->GetNativeView() window];
     if (!window) {
       // The tab isn't active right now.
@@ -111,7 +116,7 @@ void ZoomDecoration::ShowBubble(BOOL auto_close) {
 }
 
 void ZoomDecoration::CloseBubble() {
-  if (ui::MaterialDesignController::IsSecondaryUiMaterial()) {
+  if (UseViews()) {
     chrome::CloseZoomBubbleViews();
     return;
   }
@@ -135,7 +140,7 @@ void ZoomDecoration::UpdateUI(zoom::ZoomController* zoom_controller,
 
   tooltip_.reset([tooltip_string retain]);
 
-  if (ui::MaterialDesignController::IsSecondaryUiMaterial())
+  if (UseViews())
     chrome::RefreshZoomBubbleViews();
   else
     [bubble_ onZoomChanged];
@@ -159,9 +164,7 @@ bool ZoomDecoration::IsAtDefaultZoom() const {
 }
 
 bool ZoomDecoration::IsBubbleShown() const {
-  return (ui::MaterialDesignController::IsSecondaryUiMaterial() &&
-          chrome::IsZoomBubbleViewsShown()) ||
-         bubble_;
+  return (UseViews() && chrome::IsZoomBubbleViewsShown()) || bubble_;
 }
 
 bool ZoomDecoration::ShouldShowDecoration() const {
@@ -180,8 +183,7 @@ bool ZoomDecoration::OnMousePressed(NSRect frame, NSPoint location) {
   } else {
     // With Material Design enabled the zoom bubble is no longer auto-closed
     // when activated with a mouse click.
-    const BOOL auto_close =
-        !ui::MaterialDesignController::IsSecondaryUiMaterial();
+    const BOOL auto_close = !UseViews();
     ShowBubble(auto_close);
   }
   return true;
@@ -196,7 +198,7 @@ content::WebContents* ZoomDecoration::GetWebContents() {
 }
 
 void ZoomDecoration::OnClose() {
-  if (!ui::MaterialDesignController::IsSecondaryUiMaterial()) {
+  if (!UseViews()) {
     bubble_.delegate = nil;
     bubble_ = nil;
   }
