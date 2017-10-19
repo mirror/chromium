8617610408fcff9470f4fb8739a9920c650797f2
Rohit Rao <rohitrao@google.com>
2017-10-19 11:02:01 -0400

    [ios] Adds an animator to drive the transition from BVC to TabSwitcher.
    BUG=768563
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Iec923c55746e264dced1d275132c207ffc3876a6

---
+++ ios/chrome/browser/ui/main/transitions/bvc_container_to_tab_switcher_animator.h
@@ -0,0 +1,15 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_MAIN_TRANSITIONS_BVC_CONTAINER_TO_TAB_SWITCHER_ANIMATOR_H_
+#define IOS_CHROME_BROWSER_UI_MAIN_TRANSITIONS_BVC_CONTAINER_TO_TAB_SWITCHER_ANIMATOR_H_
+
+#import <UIKit/UIKit.h>
+
+@interface BVCContainerToTabSwitcherAnimator
+    : NSObject<UIViewControllerAnimatedTransitioning>
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_MAIN_TRANSITIONS_BVC_CONTAINER_TO_TAB_SWITCHER_ANIMATOR_H_
