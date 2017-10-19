19f3ed13168e04765977652ff4c6b2f6b14f8d7f
Ramin Halavati <rhalavati@chromium.org>
2017-10-19 14:32:36 +0200

    Adding background detection to high contrast image classifier.
    To correctly classify images to those suitable for color inversion and
    those not, we need to detect if some part of the image is static
    background and ignore it while sampling from the image.
    This is because frequently there are images with very large static
    background and some small foreground. Sampling uniformly from the whole
    image results in very few samples from foreground and most samples from
    the large background, and this skews the classifier towards inversion,
    while the decision must be taken based on the foreground area.
    
    Bug: 685242
    Change-Id: I5b7bd9d2ed1b52913684e7ba2fdc6a2fd0e4a46a

--- third_party/WebKit/Source/platform/graphics/HighContrastImageClassifier.h
+++ third_party/WebKit/Source/platform/graphics/HighContrastImageClassifier.h
@@ -7,14 +7,16 @@
 
 #include <vector>
 
+#include "base/rand_util.h"
 #include "platform/graphics/GraphicsTypes.h"
 #include "platform/graphics/Image.h"
+#include "third_party/WebKit/Source/platform/geometry/IntRect.h"
 
 namespace blink {
 
 class PLATFORM_EXPORT HighContrastImageClassifier {
  public:
-  HighContrastImageClassifier() = default;
+  HighContrastImageClassifier();
   ~HighContrastImageClassifier() = default;
 
   // Decides if a high contrast filter should be applied to the image or not.
@@ -25,26 +27,61 @@ class PLATFORM_EXPORT HighContrastImageClassifier {
     return ComputeImageFeatures(image, features);
   }
 
+  void SetRandomGeneratorForTesting() { use_testing_random_generator_ = true; }
+
  private:
+  enum class ColorMode { kColor = 0, kGrayscale = 1 };
+
   // Computes the features vector for a given image.
   bool ComputeImageFeatures(Image&, std::vector<float>*);
 
   // Converts image to SkBitmap and returns true if successful.
   bool GetBitmap(Image&, SkBitmap*);
 
-  // Extracts sample pixels and transparency ratio from the given bitmap.
-  void GetSamples(const SkBitmap&, std::vector<SkColor>*, float*);
+  // Extracts sample pixels, transparency ratio, and background ratio from the
+  // given bitmap.
+  void GetSamples(const SkBitmap&, std::vector<SkColor>*, float*, float*);
 
-  // Computes the features, given sampled pixels and transparency ratio.
+  // Computes the features, given sampled pixels, transparency ratio, and
+  // background ratio.
   void GetFeatures(const std::vector<SkColor>&,
+                   const float,
                    const float,
                    std::vector<float>*);
 
   // Makes a decision about image given its features.
   HighContrastClassification ClassifyImage(const std::vector<float>&);
 
-  // Receives sampled pixels, and returns the number of color buckets.
-  int CountColorBuckets(const std::vector<SkColor>&);
+  // Receives sampled pixels and color mode, and returns the ratio of color
+  // buckets count to all possible color buckets.
+  float ComputeColorBucketsRatio(const std::vector<SkColor>&, const ColorMode);
+
+  // Gets the specified number of samples from a block of the image, and returns
+  // the samples and the number of transparent pixels.
+  void GetBlockSamples(const SkBitmap&,
+                       const IntRect&,
+                       const int,
+                       std::vector<SkColor>*,
+                       int*);
+
+  // Given sampled pixels from a block of image and the number of transparent
+  // pixels, decides if a block is part of background or or not.
+  bool IsBlockBackground(const std::vector<SkColor>&, const int);
+
+  // Returns a random number in range [min, max).
+  int GetRandomInt(const int min, const int max) {
+    if (use_testing_random_generator_) {
+      testing_random_generator_seed_ *= 7;
+      testing_random_generator_seed_ += 15485863;
+      testing_random_generator_seed_ %= 256205689;
+      return min + testing_random_generator_seed_ % (max - min);
+    }
+
+    return base::RandInt(min, max - 1);
+  }
+
+  bool use_testing_random_generator_;
+  int testing_random_generator_seed_;
 };
 
 }  // namespace blink
