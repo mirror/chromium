ef08a7f373d685780f714ae0030ad116db92df34
Matt Menke <mmenke@chromium.org>
2017-10-18 23:43:44 -0400

    Add test, fix build
    Bug: 754007
    Change-Id: Ifab3624ea083b4e1e00beaa66e274ab8d0656496

--- chrome/browser/net/chrome_mojo_proxy_resolver_factory.cc
+++ chrome/browser/net/chrome_mojo_proxy_resolver_factory.cc
@@ -30,12 +30,88 @@ void BindConnectorOnUIThread(service_manager::mojom::ConnectorRequest request) {
 
 }  // namespace
 
+// ProxyResolverFactory proxy class that tracks the number of ProxyResolvers it
+// creates. Need a separate class for each factory to know how many
+// ProxyResolvers were in use in the case of a network process crash.
+class ChromeMojoProxyResolverFactory::ResolverFactoryChannel
+    : public proxy_resolver::mojom::ProxyResolverFactory {
+ public:
+  ResolverFactoryChannel(
+      ChromeMojoProxyResolverFactory* parent_factory,
+      proxy_resolver::mojom::ProxyResolverFactoryRequest request)
+      : parent_factory_(parent_factory), binding_(this, std::move(request)) {
+    binding_.set_connection_error_handler(base::BindOnce(
+        &ResolverFactoryChannel::OnConnectionError, base::Unretained(this)));
+  }
+
+  ~ResolverFactoryChannel() override {
+    if (has_proxy_resolvers())
+      parent_factory_->OnNumProxyResolversChanged();
+  }
+
+  bool has_proxy_resolvers() const { return num_proxy_resolvers_ > 0; }
+
+ private:
+  // net::interfaces::ProxyResolverFactory implementation:
+
+  void CreateResolver(
+      const std::string& pac_script,
+      proxy_resolver::mojom::ProxyResolverRequest req,
+      proxy_resolver::mojom::ProxyResolverFactoryRequestClientPtr client)
+      override {
+    // Always increment number of resolvers, even on errors. The caller will
+    // inform this class of any connection errors, including those that happen
+    // when utility process creation fails.
+    num_proxy_resolvers_++;
+    parent_factory_->OnNumProxyResolversChanged();
+
+    if (!parent_factory_->ResolverFactory()) {
+      // If factory creation failed, just destroy |req|, which should cause a
+      // connection error on the other end.
+      return;
+    }
+
+    parent_factory_->ResolverFactory()->CreateResolver(
+        pac_script, std::move(req), std::move(client));
+  }
+
+  void OnResolverDestroyed() override {
+    // Can't call OnResolverDestroyed() on the utility process's
+    // ProxyResolverFactory - if the utility processes crashes and is restarted,
+    // it's unclear if this count is for the old or new utility process. This
+    // could be worked around, but since the utility process's factory doesn't
+    // care, doesn't seem worth the effort.
+
+    // Don't bother protecting this from becoming less than 0 - at worse,
+    // underflow here can keep the service process alive, but just keeping
+    // around a ProxyResolver will do the same thing.
+    num_proxy_resolvers_--;
+    parent_factory_->OnNumProxyResolversChanged();
+  }
+
+  void OnConnectionError() { parent_factory_->OnConnectionClosed(this); }
+
+  ChromeMojoProxyResolverFactory* parent_factory_;
+  mojo::Binding<proxy_resolver::mojom::ProxyResolverFactory> binding_;
+  int num_proxy_resolvers_ = 0;
+
+  DISALLOW_COPY_AND_ASSIGN(ResolverFactoryChannel);
+};
+
 // static
 ChromeMojoProxyResolverFactory* ChromeMojoProxyResolverFactory::GetInstance() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
   return base::Singleton<
       ChromeMojoProxyResolverFactory,
       base::LeakySingletonTraits<ChromeMojoProxyResolverFactory>>::get();
+}
+
+proxy_resolver::mojom::ProxyResolverFactoryPtr
+ChromeMojoProxyResolverFactory::CreateFactoryInterface() {
+  proxy_resolver::mojom::ProxyResolverFactoryPtr factory;
+  channels_.insert(std::make_unique<ResolverFactoryChannel>(
+      this, mojo::MakeRequest(&factory)));
+  return factory;
 }
 
 ChromeMojoProxyResolverFactory::ChromeMojoProxyResolverFactory()
@@ -45,30 +121,6 @@ ChromeMojoProxyResolverFactory::ChromeMojoProxyResolverFactory()
 
 ChromeMojoProxyResolverFactory::~ChromeMojoProxyResolverFactory() {
   DCHECK(thread_checker_.CalledOnValidThread());
-}
-
-std::unique_ptr<base::ScopedClosureRunner>
-ChromeMojoProxyResolverFactory::CreateResolver(
-    const std::string& pac_script,
-    mojo::InterfaceRequest<proxy_resolver::mojom::ProxyResolver> req,
-    proxy_resolver::mojom::ProxyResolverFactoryRequestClientPtr client) {
-  DCHECK(thread_checker_.CalledOnValidThread());
-  if (!resolver_factory_)
-    CreateFactory();
-
-  if (!resolver_factory_) {
-    // If factory creation failed, close |req|'s message pipe, which should
-    // cause a connection error.
-    req = nullptr;
-    return nullptr;
-  }
-  idle_timer_.Stop();
-  num_proxy_resolvers_++;
-  resolver_factory_->CreateResolver(pac_script, std::move(req),
-                                    std::move(client));
-  return base::MakeUnique<base::ScopedClosureRunner>(
-      base::Bind(&ChromeMojoProxyResolverFactory::OnResolverDestroyed,
-                 base::Unretained(this)));
 }
 
 void ChromeMojoProxyResolverFactory::SetFactoryIdleTimeoutForTests(
@@ -93,43 +145,58 @@ void ChromeMojoProxyResolverFactory::InitServiceManagerConnector() {
       base::Bind(&BindConnectorOnUIThread, base::Passed(&request)));
 }
 
-void ChromeMojoProxyResolverFactory::CreateFactory() {
+proxy_resolver::mojom::ProxyResolverFactory*
+ChromeMojoProxyResolverFactory::ResolverFactory() {
   DCHECK(thread_checker_.CalledOnValidThread());
-  DCHECK(!resolver_factory_);
 
-  InitServiceManagerConnector();
+  if (!resolver_factory_) {
+    InitServiceManagerConnector();
 
-  service_manager_connector_->BindInterface(
-      proxy_resolver::mojom::kProxyResolverServiceName,
-      mojo::MakeRequest(&resolver_factory_));
+    service_manager_connector_->BindInterface(
+        proxy_resolver::mojom::kProxyResolverServiceName,
+        mojo::MakeRequest(&resolver_factory_));
 
-  resolver_factory_.set_connection_error_handler(base::Bind(
-      &ChromeMojoProxyResolverFactory::DestroyFactory, base::Unretained(this)));
+    resolver_factory_.set_connection_error_handler(
+        base::Bind(&ChromeMojoProxyResolverFactory::DestroyFactory,
+                   base::Unretained(this)));
+  }
+  return resolver_factory_.get();
 }
 
 void ChromeMojoProxyResolverFactory::DestroyFactory() {
   resolver_factory_.reset();
 }
 
-void ChromeMojoProxyResolverFactory::OnResolverDestroyed() {
-  DCHECK(thread_checker_.CalledOnValidThread());
-  DCHECK_GT(num_proxy_resolvers_, 0u);
-  if (--num_proxy_resolvers_ == 0) {
-    // When all proxy resolvers have been destroyed, the proxy resolver factory
-    // is no longer needed. However, new proxy resolvers may be created
-    // shortly after being destroyed (e.g. due to a network change).
-    //
-    // On desktop, where a utility process is used, if the utility process is
-    // shut down immediately, this would cause unnecessary process churn, so
-    // wait for an idle timeout before shutting down the proxy resolver utility
-    // process.
-    idle_timer_.Start(FROM_HERE, factory_idle_timeout_, this,
-                      &ChromeMojoProxyResolverFactory::OnIdleTimeout);
-  }
-}
-
 void ChromeMojoProxyResolverFactory::OnIdleTimeout() {
   DCHECK(thread_checker_.CalledOnValidThread());
-  DCHECK_EQ(num_proxy_resolvers_, 0u);
   DestroyFactory();
+}
+
+void ChromeMojoProxyResolverFactory::OnConnectionClosed(
+    ResolverFactoryChannel* binding) {
+  auto it = std::find_if(
+      channels_.begin(), channels_.end(),
+      [binding](
+          const std::unique_ptr<ResolverFactoryChannel>& factory_binding) {
+        return factory_binding.get() == binding;
+      });
+  DCHECK(it != channels_.end());
+  channels_.erase(it);
+}
+
+void ChromeMojoProxyResolverFactory::OnNumProxyResolversChanged() {
+  bool has_proxy_resolvers = false;
+  for (const auto& binding : channels_) {
+    if (binding->has_proxy_resolvers()) {
+      has_proxy_resolvers = true;
+      break;
+    }
+  }
+
+  if (has_proxy_resolvers) {
+    idle_timer_.Start(FROM_HERE, factory_idle_timeout_, this,
+                      &ChromeMojoProxyResolverFactory::OnIdleTimeout);
+  } else {
+    idle_timer_.Stop();
+  }
 }
--- chrome/browser/net/chrome_mojo_proxy_resolver_factory.h
+++ chrome/browser/net/chrome_mojo_proxy_resolver_factory.h
@@ -14,7 +14,8 @@
 #include "base/threading/thread_checker.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
-#include "content/public/network/mojo_proxy_resolver_factory.h"
+#include "mojo/public/cpp/bindings/binding.h"
+#include "services/proxy_resolver/public/interfaces/proxy_resolver.mojom.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "services/service_manager/public/interfaces/connector.mojom.h"
 
@@ -26,17 +27,13 @@ struct DefaultSingletonTraits;
 // A factory used to create connections to Mojo proxy resolver services.  On
 // Android, the proxy resolvers will run in the browser process, and on other
 // platforms, they'll all be run in the same utility process.
-class ChromeMojoProxyResolverFactory
-    : public content::MojoProxyResolverFactory {
+class ChromeMojoProxyResolverFactory {
  public:
   static ChromeMojoProxyResolverFactory* GetInstance();
 
-  // Overridden from proxy_content::MojoProxyResolverFactory:
-  std::unique_ptr<base::ScopedClosureRunner> CreateResolver(
-      const std::string& pac_script,
-      mojo::InterfaceRequest<proxy_resolver::mojom::ProxyResolver> req,
-      proxy_resolver::mojom::ProxyResolverFactoryRequestClientPtr client)
-      override;
+  // Creates a new net::interfaces::ProxyResolverFactoryPtr, which proxies
+  // ProxyResolver creation through the ChromeMojoProxyResolverFactory.
+  proxy_resolver::mojom::ProxyResolverFactoryPtr CreateFactoryInterface();
 
   // Used by tests to override the default timeout used when no resolver is
   // connected before disconnecting the factory (and causing the service to
@@ -45,32 +42,45 @@ class ChromeMojoProxyResolverFactory
 
  private:
   friend struct base::DefaultSingletonTraits<ChromeMojoProxyResolverFactory>;
+
+  // Each ResolverFactoryChannel manages a single mojo connection, tracking the
+  // number of live resolvers it has issued.
+  class ResolverFactoryChannel;
+
   ChromeMojoProxyResolverFactory();
-  ~ChromeMojoProxyResolverFactory() override;
+  ~ChromeMojoProxyResolverFactory();
 
   // Initializes the ServiceManager's connector if it hasn't been already.
   void InitServiceManagerConnector();
 
-  // Creates the proxy resolver factory. On desktop, creates a new utility
-  // process before creating it out of process. On Android, creates it on the
-  // current thread.
-  void CreateFactory();
+  // Returns the the proxy resolver factory, creating it if needed. On desktop,
+  // creates a new utility process before creating it out of process. On
+  // Android, creates it on the current thread.
+  proxy_resolver::mojom::ProxyResolverFactory* ResolverFactory();
 
   // Destroys |resolver_factory_|.
   void DestroyFactory();
-
-  // Invoked each time a proxy resolver is destroyed.
-  void OnResolverDestroyed();
 
   // Invoked once an idle timeout has elapsed after all proxy resolvers are
   // destroyed.
   void OnIdleTimeout();
 
+  // Called when a ResolverFactoryChannel's connection is closed. Destroys
+  // |binding|.
+  void OnConnectionClosed(ResolverFactoryChannel* binding);
+
+  // Should be called when the number of ProxyResolvers changes. If no
+  // ProxyResolvers exist, starts a timeout to shut down the utility process.
+  // Otherwise, stops the shutdown timer if its running.
+  void OnNumProxyResolversChanged();
+
+  // The real ProxyResolverFactory that, on desktop platforms, lives in a
+  // utility process.
   proxy_resolver::mojom::ProxyResolverFactoryPtr resolver_factory_;
 
   std::unique_ptr<service_manager::Connector> service_manager_connector_;
 
-  size_t num_proxy_resolvers_ = 0;
+  std::set<std::unique_ptr<ResolverFactoryChannel>> channels_;
 
   base::OneShotTimer idle_timer_;
 
