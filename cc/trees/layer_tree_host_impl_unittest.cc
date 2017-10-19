aabcdfed7f2afb06a7334893534b6e91ab56d70d
Khushal <khushalsagar@chromium.org>
2017-10-18 23:57:50 -0700

    cc: Use Region for tracking positions of images in DiscardableImageMap.
    The position tracking for images maintains a union of rects for all
    instances of this image. This means that if the same image is used by
    multiple elements, an image invalidation invalidates a unioned rect for
    all these images. In the case of animated images this can lead to
    continuous expensive repaints of offscreen content.
    
    In order to avoid this, maintain a Region instead of a unioned rect for
    images in the DiscardableImageMap. But clamp the vector size
    maintained to 256. The reason for reverting to rect from region was a
    regression from the overhead of this tracking on motion mark
    anonimeter.
    
    R=chrishtr@chromium.org, enne@chromium.org, vmpstr@chromium.org
    Bug:772881
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ie868f66d473625003b398ea6d55ae0f40440193f

--- cc/trees/layer_tree_host_impl_unittest.cc
+++ cc/trees/layer_tree_host_impl_unittest.cc
@@ -12970,8 +12970,10 @@ TEST_F(LayerTreeHostImplTest, CheckerImagingTileInvalidation) {
     else
       EXPECT_FALSE(tile->HasRasterTask());
   }
-  Region expected_invalidation(
-      raster_source->GetRectForImage(checkerable_image.stable_id()));
+  Region expected_invalidation =
+      raster_source->GetDisplayItemList()
+          ->discardable_image_map()
+          .GetRegionForImage(checkerable_image.stable_id());
   EXPECT_EQ(expected_invalidation, *(root->GetPendingInvalidation()));
 }
 
