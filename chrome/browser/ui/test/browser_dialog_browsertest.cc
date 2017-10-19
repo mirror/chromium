3bbb1f6c3223a1ef91c6dcb3b163f4b52030df44
Trent Apted <tapted@chromium.org>
2017-10-19 18:00:26 +1100

    Fix TestBrowserDialog --interactive.
    It started failing in r482709 since more robust checks were added for
    checking for test result output files.
    
    To fix, remove switches::kTestLauncherOutput from the subprocess
    command line so it doesn't try to create test result output.
    
    Bug: 776265
    Change-Id: Ib0406b77fb7aa90cddf712331342526fa555491b

--- chrome/browser/ui/test/browser_dialog_browsertest.cc
+++ chrome/browser/ui/test/browser_dialog_browsertest.cc
@@ -53,7 +53,14 @@ TEST(BrowserDialogTest, Invoke) {
   ASSERT_NE(it, dialog_cases.end()) << "Dialog '" << dialog_name
                                     << "' not found.";
 
-  base::CommandLine command(invoker);
+  // Don't create test output for the subprocess (the paths will conflict).
+  base::CommandLine::StringVector argv = invoker.argv();
+  std::string ascii(switches::kTestLauncherOutput);
+  base::CommandLine::StringType native_switch(ascii.begin(), ascii.end());
+  base::EraseIf(argv, [native_switch](const auto& arg) -> bool {
+    return arg.find(native_switch) != arg.npos;  // Substring search.
+  });
+  base::CommandLine command(argv);
 
   // Replace TestBrowserDialog.Invoke with |dialog_name|.
   command.AppendSwitchASCII(base::kGTestFilterFlag, dialog_name);
