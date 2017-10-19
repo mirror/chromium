3c74f8bbcd3d7e79955ea83b3c3d2430ff164363
Joy Ming <jming@chromium.org>
2017-10-19 15:18:00 -0700

    Make flicker less frequent.
    This is part of a larger refactor to make downloads a foreground
    service on all versions of Android. Before this CL, on Android API <= 24
    the downloads notification would flicker on pause and on completion as a
    hack to disassociate the notification from the foreground service.
    However, this leads to a lot of flickering. This CL reduces the amount
    of flickering to be only in the cases of completion and when the
    notification that is pinned to the foreground is switched. Basically,
    this means that the notification will flicker in all cases of completion
    (which is the same as before) and fewer cases of pause, only when the
    user has another download running, which is less likely anyways.
    
    Bug:771802,747563
    Change-Id: Ic0d30cb7d91726a7015a6db9b0dc6e113140fd54

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadForegroundServiceManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadForegroundServiceManager.java
@@ -116,7 +116,7 @@ public class DownloadForegroundServiceManager {
         // Stop the foreground service.
         // In the pending case, this will stop the foreground immediately after it was started.
         if (!isActive(downloadUpdate.mDownloadStatus)) {
-            stopAndUnbindService(downloadUpdate.mDownloadStatus == DownloadStatus.CANCEL);
+            stopAndUnbindService(downloadUpdate.mDownloadStatus);
             cleanDownloadUpdateQueue();
             return;
         }
@@ -238,28 +238,46 @@ public class DownloadForegroundServiceManager {
     /** Helper code to stop and unbind service. */
 
     @VisibleForTesting
-    void stopAndUnbindService(boolean isCancelled) {
-        mIsServiceBound = false;
+    void stopAndUnbindService(DownloadStatus downloadStatus) {
+        // Pause: only try to detach, do not kill notification.
+        // Complete/failed: try to detach, if that doesn't work, kill.
+        // Cancel: don't even try to detach, just kill.
+        boolean detachNotification = downloadStatus == DownloadStatus.PAUSE
+                || downloadStatus == DownloadStatus.COMPLETE
+                || downloadStatus == DownloadStatus.FAIL;
+        boolean killNotification = downloadStatus == DownloadStatus.CANCEL
+                || downloadStatus == DownloadStatus.COMPLETE
+                || downloadStatus == DownloadStatus.FAIL;
 
+        mIsServiceBound = false;
         if (mBoundService != null) {
-            stopAndUnbindServiceInternal(isCancelled);
+            stopAndUnbindServiceInternal(detachNotification, killNotification);
             mBoundService = null;
         }
 
-        // If the download isn't cancelled,  need to relaunch the notification so it is no longer
+        // If the download is completed,  need to relaunch the notification so it is no longer
         // pinned to the foreground service.
-        if (!isCancelled && Build.VERSION.SDK_INT < 24) {
+        if ((downloadStatus == DownloadStatus.COMPLETE || downloadStatus == DownloadStatus.FAIL)
+                && Build.VERSION.SDK_INT < 24) {
             relaunchPinnedNotification();
         }
-        mPinnedNotificationId = INVALID_NOTIFICATION_ID;
+
+        // Only remove the pinned notification if it was killed or detached.
+        if ((detachNotification && Build.VERSION.SDK_INT >= 24) || killNotification) {
+            mPinnedNotificationId = INVALID_NOTIFICATION_ID;
+        }
     }
 
     @VisibleForTesting
-    void stopAndUnbindServiceInternal(boolean isCancelled) {
-        mBoundService.stopDownloadForegroundService(isCancelled);
+    void stopAndUnbindServiceInternal(boolean detachNotification, boolean killNotification) {
+        mBoundService.stopDownloadForegroundService(detachNotification, killNotification);
         ContextUtils.getApplicationContext().unbindService(mConnection);
-        DownloadForegroundServiceObservers.removeObserver(
-                DownloadNotificationServiceObserver.class);
+
+        // Only remove the observer if the notification has been detached or killed.
+        if ((detachNotification && Build.VERSION.SDK_INT >= 24) || killNotification) {
+            DownloadForegroundServiceObservers.removeObserver(
+                    DownloadNotificationServiceObserver.class);
+        }
     }
 
     /** Helper code for testing. */
