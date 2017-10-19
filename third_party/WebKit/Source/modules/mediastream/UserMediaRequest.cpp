c16ab3f35d201b9fd136d488ddcd848240a9b785
Luna Lu <loonybear@chromium.org>
2017-10-19 10:39:58 -0400

    Feature Policy Onion Soup
    STEP 1 (Patch 1/2):
    - Moved source files content/common/feature_policy/feature_policy.cc, .h
      into third_party/WebKit/common/feature_policy/
    - Moved third_party/webkit/public/platform/WebFeaturePolicyFeature.h
      into third_party/common/feature_policy/feature_policy_feature.h
    - Migrated everything into blink namespace
    - Fixed BUILD.gn files
    - Modified DEPS files temporarily
    
    STEP 2:
    - Rename WebFeaturePolicyFeature to FeaturePolicyFeature
    for f in $(git grep -l -w "WebFeaturePolicyFeature"); do
      sed -i 's/::WebFeaturePolicyFeature::/::FeaturePolicyFeature::/g' "$f"
      sed -i 's/::WebFeaturePolicyFeature/::FeaturePolicyFeature/g' "$f"
      sed -i 's/WebFeaturePolicyFeature::/FeaturePolicyFeature::/g' "$f"
      sed -i 's/ WebFeaturePolicyFeature / FeaturePolicyFeature /g' "$f"
      sed -i 's/WebFeaturePolicyFeature /FeaturePolicyFeature /g' "$f"
      sed -i 's/<WebFeaturePolicyFeature>/<FeaturePolicyFeature>/g' "$f"
      sed -i 's/ WebFeaturePolicyFeature>/ FeaturePolicyFeature>/g' "$f"
      sed -i 's/(WebFeaturePolicyFeature)/(FeaturePolicyFeature)/g' "$f"
      sed -i 's/WebFeaturePolicyFeature,/FeaturePolicyFeature,/g' "$f"
      sed -i 's/ WebFeaturePolicyFeature;/ FeaturePolicyFeature;/g' "$f"
      sed -i 's/WebFeaturePolicyFeature)/FeaturePolicyFeature)/g' "$f"
    done
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_chromium_dbg_ng;master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ief3b2edce924e0d74dd3c2ee082469e6fe29d0d8

--- third_party/WebKit/Source/modules/mediastream/UserMediaRequest.cpp
+++ third_party/WebKit/Source/modules/mediastream/UserMediaRequest.cpp
@@ -36,6 +36,7 @@
 #include "bindings/core/v8/Dictionary.h"
 #include "bindings/core/v8/ExceptionMessages.h"
 #include "bindings/core/v8/ExceptionState.h"
+#include "common/feature_policy/feature_policy_feature.h"
 #include "core/dom/Document.h"
 #include "core/dom/ExceptionCode.h"
 #include "core/dom/SpaceSplitString.h"
@@ -48,7 +49,6 @@
 #include "modules/mediastream/UserMediaController.h"
 #include "platform/mediastream/MediaStreamCenter.h"
 #include "platform/mediastream/MediaStreamDescriptor.h"
-#include "public/platform/WebFeaturePolicyFeature.h"
 
 namespace blink {
 
