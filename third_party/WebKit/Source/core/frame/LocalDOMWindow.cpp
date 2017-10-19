08480b49ec35a02970db0bf04624b562139693a2
Philip Rogers <pdr@chromium.org>
2017-10-19 16:39:25 -0700

    [trybots only] withpatch run for https://chromium-review.googlesource.com/c/chromium/src/+/729467
    Do not commit.
    
    Change-Id: Ic178d7f1810ab44237ad6baa1809628a6e51af14

--- third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
+++ third_party/WebKit/Source/core/frame/LocalDOMWindow.cpp
@@ -984,7 +984,9 @@ FloatSize LocalDOMWindow::GetViewportSize(
   return GetFrame()->IsMainFrame() &&
                  !page->GetSettings().GetInertVisualViewport()
              ? FloatSize(page->GetVisualViewport().VisibleRect().Size())
-             : FloatSize(view->VisibleContentRect(scrollbar_inclusion).Size());
+             : FloatSize(view->LayoutViewportScrollableArea()
+                             ->VisibleContentRect(scrollbar_inclusion)
+                             .Size());
 }
 
 int LocalDOMWindow::innerHeight() const {
