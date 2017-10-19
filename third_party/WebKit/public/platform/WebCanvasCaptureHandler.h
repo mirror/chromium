f03bd9286d1c010d7e9d91940ccaf41b2758e989
Emircan Uysaler <emircan@chromium.org>
2017-10-18 17:11:58 -0700

    Use viz::GLHelper::ReadbackTextureAsync() in canvas.captureStream()
    This CL replaces the existing blocking call to SkImage::readPixels() to using viz::GLHelper::ReadbackTextureAsync() for texture backed images. Using this we no longer block main render thread when waiting for readback. If it fails for any reason, we still have a fall back path using the earlier methods. 
    
    Bug:727385
    Change-Id: I8ed44b4419c8222062792ccb104bfb4b8c8e6bb3

--- third_party/WebKit/public/platform/WebCanvasCaptureHandler.h
+++ third_party/WebKit/public/platform/WebCanvasCaptureHandler.h
@@ -7,15 +7,21 @@
 
 #include "WebCommon.h"
 
+#include "third_party/skia/include/core/SkRefCnt.h"
+
 class SkImage;
 
 namespace blink {
+
+class WebGraphicsContext3DProvider;
 
 // Platform interface of a CanvasCaptureHandler.
 class BLINK_PLATFORM_EXPORT WebCanvasCaptureHandler {
  public:
   virtual ~WebCanvasCaptureHandler() = default;
-  virtual void SendNewFrame(const SkImage*) {}
+  virtual void SendNewFrame(
+      sk_sp<SkImage>,
+      blink::WebGraphicsContext3DProvider* context_provider) {}
   virtual bool NeedsNewFrame() const { return false; }
 };
 
