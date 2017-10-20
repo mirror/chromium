86c46112904c8d8b5d9d8b7c139e2679f44c183b
dhausknecht <dhausknecht@google.com>
2017-10-20 16:23:50 +0200

    OriginManifestURLLoaderThrottle loads Origin Manifest file on navigation.
    The class is added as part of the implementation of the Origin Manifest mechanism (https://groups.google.com/a/chromium.org/d/topic/blink-dev/mdkHs4jybG4/discussion).
    This patch requires patch https://chromium-review.googlesource.com/c/chromium/src/+/709595.
    
    Bug: 751996
    Change-Id: I9d048fae469bd6b07f5bcc02e9893f61fee41d84

--- chrome/browser/origin_manifest/origin_manifest_url_loader_throttle.cc
+++ chrome/browser/origin_manifest/origin_manifest_url_loader_throttle.cc
@@ -46,85 +46,84 @@ OriginManifestURLLoaderThrottle::~OriginManifestURLLoaderThrottle() {}
 void OriginManifestURLLoaderThrottle::WillStartRequest(
     const content::ResourceRequest& request,
     bool* defer) {
-  origin_.reset(new url::Origin(request.url));
   *defer = false;
 }
 
 void OriginManifestURLLoaderThrottle::WillRedirectRequest(
     const net::RedirectInfo& redirect_info,
     bool* defer) {
-  origin_.reset(new url::Origin(redirect_info.new_url));
   *defer = false;
 }
 
 void OriginManifestURLLoaderThrottle::WillProcessResponse(
+    const GURL& response_url,
     const content::ResourceResponseHead& response_head,
     bool* defer) {
   std::string version;
-  if (response_head.headers.get() &&
-      response_head.headers->EnumerateHeader(nullptr, "Sec-Origin-Manifest",
-                                             &version)) {
-    if (!SanitizeOriginManifestVersion(version, &version)) {
-      *defer = false;
-      return;
-    }
-
-    // TODO(dhausknecht) this way of fetching works for now but seems
-    // non-optimal. I am sure there is a better way.
-    // Build the ResourceRequest
-    std::string path =
-        std::string("originmanifest/").append(version).append(".json");
-    url::Replacements<char> replacements;
-    replacements.SetPath(path.c_str(), url::Component(0, path.length()));
-    content::ResourceRequest res_request;
-    res_request.url = origin_->GetURL().ReplaceComponents(replacements);
-
-    // Get the URLLoaderFactory
-    // TODO(dhausknecht) move the following three lines to header file
-    network_service_ = content::NetworkService::Create();
-    content::mojom::NetworkContextParamsPtr context_params =
-        content::mojom::NetworkContextParams::New();
-    context_params->http_cache_enabled = false;
-    network_service_->CreateNetworkContext(mojo::MakeRequest(&network_context_),
-                                           std::move(context_params));
-    // TODO(dhausknecht) 0 for the process ID is probably not what we want
-    network_context_->CreateURLLoaderFactory(
-        mojo::MakeRequest(&url_loader_factory_), 0);
-
-    // Create the traffic annotation
-    net::NetworkTrafficAnnotationTag traffic_annotation =
-        net::DefineNetworkTrafficAnnotation("origin_manifest_loader", R"(
-          semantics {
-            sender: "Origin Manifest URL Loader Throttle"
-            description:
-              "Fetches the Origin Manifest with a given version from an origin."
-            trigger:
-              "In case the Origin Manifest with a given version does not "
-              "exist in the Origin Manifest Store, it is fetched from the "
-              "origin at a well-known location."
-            data:
-              "None, the URL itself contains the origin and Origin Manifest "
-              "version"
-            destination: OTHER
-          }
-          policy {
-            cookies_allowed: NO
-            setting: "This feature cannot be disabled by settings. Server "
-              "opt-in or out of this mechanism."
-            policy_exception_justification:
-              "Not implemented, considered not useful."
-          })");
-
-    url_loader_ = content::SimpleURLLoader::Create();
-    url_loader_->DownloadToString(
-        res_request, url_loader_factory_.get(), traffic_annotation,
-        base::BindOnce(&OriginManifestURLLoaderThrottle::OnBodyAsString,
-                       base::Unretained(this)),
-        url_loader_->kMaxBoundedStringDownloadSize);
-
-    *defer = true;
+  if (!response_head.headers.get() &&
+      !response_head.headers->EnumerateHeader(nullptr, "Sec-Origin-Manifest",
+                                              &version) &&
+      !SanitizeOriginManifestVersion(version, &version)) {
+    *defer = false;
+    return;
   }
-  *defer = false;
+
+  // TODO(dhausknecht) this way of fetching works for now but seems
+  // non-optimal. I am sure there is a better way.
+
+  // Build the ResourceRequest
+  std::string path = std::string(".well-known/originmanifest/")
+                         .append(version)
+                         .append(".json");
+  url::Replacements<char> replacements;
+  replacements.SetPath(path.c_str(), url::Component(0, path.length()));
+  content::ResourceRequest res_request;
+  res_request.url =
+      url::Origin(response_url).GetURL().ReplaceComponents(replacements);
+
+  // Get the URLLoaderFactory
+  network_service_ = content::NetworkService::Create();
+  content::mojom::NetworkContextParamsPtr context_params =
+      content::mojom::NetworkContextParams::New();
+  context_params->http_cache_enabled = false;
+  network_service_->CreateNetworkContext(mojo::MakeRequest(&network_context_),
+                                         std::move(context_params));
+  // TODO(dhausknecht) 0 for the process ID is probably not what we want
+  network_context_->CreateURLLoaderFactory(
+      mojo::MakeRequest(&url_loader_factory_), 0);
+
+  // Create the traffic annotation
+  net::NetworkTrafficAnnotationTag traffic_annotation =
+      net::DefineNetworkTrafficAnnotation("origin_manifest_loader", R"(
+        semantics {
+          sender: "Origin Manifest URL Loader Throttle"
+          description:
+            "Fetches the Origin Manifest with a given version from an origin."
+          trigger:
+            "In case the Origin Manifest with a given version does not "
+            "exist in the Origin Manifest Store, it is fetched from the "
+            "origin at a well-known location."
+          data:
+            "None, the URL itself contains the origin and Origin Manifest "
+            "version"
+          destination: OTHER
+        }
+        policy {
+          cookies_allowed: NO
+          setting: "This feature cannot be disabled by settings. Server "
+            "opt-in or out of this mechanism."
+          policy_exception_justification:
+            "Not implemented, considered not useful."
+        })");
+
+  url_loader_ = content::SimpleURLLoader::Create();
+  url_loader_->DownloadToString(
+      res_request, url_loader_factory_.get(), traffic_annotation,
+      base::BindOnce(&OriginManifestURLLoaderThrottle::OnBodyAsString,
+                     base::Unretained(this)),
+      url_loader_->kMaxBoundedStringDownloadSize);
+
+  *defer = true;
 }
 
 void OriginManifestURLLoaderThrottle::OnBodyAsString(
