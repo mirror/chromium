9d3f4a12c42b3f3bd165b8aab2de4e9ddd7cb8aa
Leonard Grey <lgrey@chromium.org>
2017-10-20 12:55:31 -0400

    [Mac] Add histograms for native sharing success/failure
    Bug: 465302
    Change-Id: I7a620ed005a3c677be83d37287e51f65d746d892

--- chrome/browser/ui/cocoa/share_menu_controller.mm
+++ chrome/browser/ui/cocoa/share_menu_controller.mm
@@ -6,6 +6,7 @@
 
 #include "base/mac/foundation_util.h"
 #include "base/mac/mac_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/sys_string_conversions.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/ui/browser.h"
@@ -23,6 +24,11 @@
 #include "ui/gfx/image/image.h"
 #include "ui/snapshot/snapshot.h"
 
+// Private method, used to identify instantiated services.
+@interface NSSharingService (ExposeName)
+- (id)name;
+@end
+
 namespace {
 
 NSString* const kExtensionPrefPanePath =
@@ -34,7 +40,54 @@ NSString* const kOpenSharingSubpaneActionValue = @"revealExtensionPoint";
 NSString* const kOpenSharingSubpaneProtocolKey = @"protocol";
 NSString* const kOpenSharingSubpaneProtocolValue = @"com.apple.share-services";
 
+// The following two services don't have convenient NSSharingServiceName*
+// constants.
+NSString* const kRemindersSharingServiceName =
+    @"com.apple.reminders.RemindersShareExtension";
+NSString* const kNotesSharingServiceName = @"com.apple.Notes.SharingExtension";
+
+}  //  namespace
+
+namespace mac_share_menu {
+
+namespace {
+
+const struct {
+  NSString* name;
+  ShareServiceUMA histogram_value;
+} kServiceNameMap[] = {
+    {NSSharingServiceNameComposeEmail, ShareServiceUMA::kEmail},
+    {NSSharingServiceNameComposeMessage, ShareServiceUMA::kMessage},
+    {kNotesSharingServiceName, ShareServiceUMA::kNote},
+    {kRemindersSharingServiceName, ShareServiceUMA::kReminder},
+    {NSSharingServiceNamePostOnTwitter, ShareServiceUMA::kTwitter},
+    {NSSharingServiceNamePostOnFacebook, ShareServiceUMA::kFacebook},
+    {NSSharingServiceNamePostOnLinkedIn, ShareServiceUMA::kLinkedIn},
+    {NSSharingServiceNamePostOnSinaWeibo, ShareServiceUMA::kSinaWeibo},
+    {NSSharingServiceNamePostOnTencentWeibo, ShareServiceUMA::kTencentWeibo}};
+
+ShareServiceUMA UMAValueForSharingService(NSSharingService* service) {
+  for (const auto& mapping : kServiceNameMap) {
+    if ([mapping.name isEqualToString:[service name]]) {
+      return mapping.histogram_value;
+    }
+  }
+  return ShareServiceUMA::kUnknownCustom;
+}
+
+void RecordShareToUMA(NSSharingService* service, bool is_success) {
+  ShareServiceUMA service_value = UMAValueForSharingService(service);
+  if (is_success) {
+    UMA_HISTOGRAM_ENUMERATION("NativeMacShare.Success", service_value,
+                              ShareServiceUMA::kShareServiceCount);
+  } else {
+    UMA_HISTOGRAM_ENUMERATION("NativeMacShare.Failure", service_value,
+                              ShareServiceUMA::kShareServiceCount);
+  }
+}
 }  // namespace
+
+}  // namespace mac
 
 @implementation ShareMenuController {
   // The following three ivars are provided to the system via NSSharingService
@@ -65,11 +118,10 @@ NSString* const kOpenSharingSubpaneProtocolValue = @"com.apple.share-services";
   // to fetch sharing services that can handle the NSURL type.
   NSArray* services = [NSSharingService
       sharingServicesForItems:@[ [NSURL URLWithString:@"https://google.com"] ]];
-  NSSharingService* readingListService = [NSSharingService
-      sharingServiceNamed:NSSharingServiceNameAddToSafariReadingList];
   for (NSSharingService* service in services) {
     // Don't include "Add to Reading List".
-    if ([service isEqual:readingListService])
+    if ([[service name]
+            isEqualToString:NSSharingServiceNameAddToSafariReadingList])
       continue;
     NSMenuItem* item = [self menuItemForService:service];
     [item setEnabled:canShare];
@@ -91,14 +143,14 @@ NSString* const kOpenSharingSubpaneProtocolValue = @"com.apple.share-services";
 
 - (void)sharingService:(NSSharingService*)service
          didShareItems:(NSArray*)items {
-  // TODO(lgrey): Add an UMA stat.
+  mac_share_menu::RecordShareToUMA(service, true);
   [self clearTransitionData];
 }
 
 - (void)sharingService:(NSSharingService*)service
     didFailToShareItems:(NSArray*)items
                   error:(NSError*)error {
-  // TODO(lgrey): Add an UMA stat.
+  mac_share_menu::RecordShareToUMA(service, false);
   [self clearTransitionData];
 }
 
