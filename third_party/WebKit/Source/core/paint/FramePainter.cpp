ce62546d174613ac688c055cf90706d55a23c25a
Stefan Zager <szager@chromium.org>
2017-10-19 17:45:30 -0700

    [root layer scrolls] Fix printing
    When printing, the root layer should not have scrollbars, and it should
    not clip its overflow.
    
    Bug: 774716,774716
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I4d5ecd8cf944a5870568dd08d5a6081bce2b5900
    
    Enable root layer scrolling for try jobs 2017-10-19T17-36-44-0700
    
    ** DO NOT LAND **
    
    BUG=417782
    
    Change-Id: I0c062036753b0465fc4893af9b67cf27547946ff

--- third_party/WebKit/Source/core/paint/FramePainter.cpp
+++ third_party/WebKit/Source/core/paint/FramePainter.cpp
@@ -168,9 +168,12 @@ void FramePainter::PaintContents(GraphicsContext& context,
   // TODO(jchaffraix): GlobalPaintFlags should be const during a paint
   // phase. Thus we should set this flag upfront (crbug.com/510280).
   GlobalPaintFlags local_paint_flags = global_paint_flags;
-  if (document->Printing())
+  PaintLayerFlags root_layer_paint_flags = 0;
+  if (document->Printing()) {
     local_paint_flags |=
         kGlobalPaintFlattenCompositingLayers | kGlobalPaintPrinting;
+    root_layer_paint_flags = kPaintLayerPaintingOverflowContents;
+  }
 
   PaintLayer* root_layer = layout_view->Layer();
 
@@ -186,7 +189,8 @@ void FramePainter::PaintContents(GraphicsContext& context,
       root_layer->GetLayoutObject().GetFrame());
   context.SetDeviceScaleFactor(device_scale_factor);
 
-  layer_painter.Paint(context, LayoutRect(rect), local_paint_flags);
+  layer_painter.Paint(context, LayoutRect(rect), local_paint_flags,
+                      root_layer_paint_flags);
 
   if (root_layer->ContainsDirtyOverlayScrollbars())
     layer_painter.PaintOverlayScrollbars(context, LayoutRect(rect),
