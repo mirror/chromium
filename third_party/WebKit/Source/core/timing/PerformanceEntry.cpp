1681f47fff74bc1b6f76b08b054fafe782b09fa2
Liquan (Max) Gu <maxlg@chromium.org>
2017-10-20 16:36:11 -0400

    [PerformanceEntry] Resolve race condition in indexing
    ClusterFuzz has detected a data race in blink::PerformanceEntry::PerformanceEntry(crbug/776813).
    
    By observation, the original design has race condition when multiple threads create PerformanceEntry at the same time, with max_index being a shared variable.
    
    The new impl use atomic int which applies an atomic lock to the shared variable, to avoid the race condition.
    
    Bug: 776813
    Change-Id: Ibd251a020ddbb6c9e4496b0e5d197ed19b40658f

--- third_party/WebKit/Source/core/timing/PerformanceEntry.cpp
+++ third_party/WebKit/Source/core/timing/PerformanceEntry.cpp
@@ -36,7 +36,7 @@
 namespace blink {
 
 namespace {
-static size_t max_index = 0;
+static std::atomic_int g_next_index(0);
 }
 
 PerformanceEntry::PerformanceEntry(const String& name,
@@ -47,9 +47,8 @@ PerformanceEntry::PerformanceEntry(const String& name,
       entry_type_(entry_type),
       start_time_(start_time),
       duration_(finish_time - start_time),
-      entry_type_enum_(ToEntryTypeEnum(entry_type)) {
-  index_ = ++max_index;
-}
+      entry_type_enum_(ToEntryTypeEnum(entry_type)),
+      index_(g_next_index.fetch_add(1, std::memory_order_relaxed)) {}
 
 PerformanceEntry::~PerformanceEntry() {}
 
