52864f9dd124e6b06f972179ca625a60c9bb4b63
Mikel Astiz <mastiz@chromium.org>
2017-10-20 11:41:11 +0200

    Update favicon mappings for page URL DB lookup hits
    Even prior to this patch, when a page is loaded, if there is no cached
    favicon in history, the images are downloaded, processed and stored in
    history. These icons get associated to all page URLs in case of a
    redirect chain, as well as redirect-like in-page navigations (fragment
    navigations, replaceState(), etc.).
    
    However, for database hits (that is, the landing page's URL is known
    in the favicon cache), we don't do any writes, unless the favicon has
    expired (is considered too old and hence the icons are redownloaded).
    
    This situation should be rather rare (e.g. user typed the landing page's
    URL first, and later the redirecting page's multiple times until it
    surfaced in the NTP). However, if it does reproduce, it doesn't go away
    until the favicon expires (1 week later).
    
    Bug: 741340
    Change-Id: I69cb38008b61fad3a9e8385d9801ae0d39dcfc59

--- chrome/browser/ui/tab_helpers.cc
+++ chrome/browser/ui/tab_helpers.cc
@@ -203,7 +203,6 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
   data_use_measurement::DataUseWebContentsObserver::CreateForWebContents(
       web_contents);
   ExternalProtocolObserver::CreateForWebContents(web_contents);
-  favicon::CreateContentFaviconDriverForWebContents(web_contents);
   FindTabHelper::CreateForWebContents(web_contents);
 
   Profile* profile =
@@ -214,6 +213,7 @@ void TabHelpers::AttachTabHelpers(WebContents* web_contents) {
   history::WebContentsTopSitesObserver::CreateForWebContents(
       web_contents, TopSitesFactory::GetForProfile(profile).get());
   HistoryTabHelper::CreateForWebContents(web_contents);
+  favicon::CreateContentFaviconDriverForWebContents(web_contents);
   InfoBarService::CreateForWebContents(web_contents);
   InstallableManager::CreateForWebContents(web_contents);
   metrics::RendererUptimeWebContentsObserver::CreateForWebContents(
