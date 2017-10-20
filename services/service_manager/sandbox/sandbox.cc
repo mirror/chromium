b7f4685d3dc068e668b7e28fd956ed70b133be0d
Tom Sepez <tsepez@chromium.org>
2017-10-20 09:56:36 -0700

    Move sandbox_win.cc to services/service_manager/sandbox/win
    Bug: 708738
    Change-Id: I57fb3c3878aaa97207c85c1c1200b22707a138d6

--- services/service_manager/sandbox/sandbox.cc
+++ services/service_manager/sandbox/sandbox.cc
@@ -10,6 +10,10 @@
 #include "services/service_manager/sandbox/mac/sandbox_mac.h"
 #endif  // defined(OS_MACOSX)
 
+#if defined(OS_WIN)
+#include "services/service_manager/sandbox/mac/sandbox_win.h"
+#endif  // defined(OS_WIN)
+
 namespace service_manager {
 
 #if defined(OS_MACOSX)
@@ -27,5 +31,31 @@ bool Sandbox::Initialize(service_manager::SandboxType sandbox_type,
   return SandboxMac::Enable(sandbox_type, allowed_dir);
 }
 #endif  // defined(OS_MACOSX)
+
+#if defined(OS_WIN)
+bool Sandbox::Initialize(service_manager::SandboxType sandbox_type,
+                         sandbox::SandboxInterfaceInfo* sandbox_info) {
+  sandbox::BrokerServices* broker_services = sandbox_info->broker_services;
+  if (broker_services) {
+    if (!InitBrokerServices(broker_services))
+      return false;
+
+    // IMPORTANT: This piece of code needs to run as early as possible in the
+    // process because it will initialize the sandbox broker, which requires the
+    // process to swap its window station. During this time all the UI will be
+    // broken. This has to run before threads and windows are created.
+    if (!service_manager::IsUnsandboxedSandboxType(sandbox_type)) {
+      // Precreate the desktop and window station used by the renderers.
+      scoped_refptr<sandbox::TargetPolicy> policy =
+          broker_services->CreatePolicy();
+      sandbox::ResultCode result = policy->CreateAlternateDesktop(true);
+      CHECK(sandbox::SBOX_ERROR_FAILED_TO_SWITCH_BACK_WINSTATION != result);
+    }
+    return true;
+  }
+  return service_manager::IsUnsandboxedSandboxType(sandbox_type) ||
+         InitTargetServices(sandbox_info->target_services);
+}
+#endif  // defined(OS_WIN)
 
 }  // namespace service_manager
