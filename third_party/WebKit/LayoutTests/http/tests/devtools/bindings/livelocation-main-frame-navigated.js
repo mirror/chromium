e964931a584148acd7ee259d460c202ec42d52bd
Will Chen <chenwilliam@chromium.org>
2017-10-19 17:28:33 -0700

    DevTools: migrate tests in bindings/, audits/
    This converts a batch of tests from the old test framework (html file)
    into the new test framework (js file).
    
    Most of it was done through an automated transformation script, with
    a small amount of manual clean-up.
    
    Bug: 667560
    Change-Id: I29a616e9db34e23e1f5514661725813836975682

--- third_party/WebKit/LayoutTests/http/tests/devtools/bindings/livelocation-main-frame-navigated.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/bindings/livelocation-main-frame-navigated.js
@@ -1,11 +1,10 @@
-<html>
-<head>
-<script src="../../inspector/inspector-test.js"></script>
-<script src="../../inspector/bindings/bindings-test.js"></script>
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-<script>
-
-async function test() {
+(async function test() {
+  TestRunner.addResult('Verify that debugger live location gets updated.');
+  await TestRunner.loadModule('bindings_test_runner');
   TestRunner.markStep('attachFrame');
   await Promise.all([
     BindingsTestRunner.attachFrame('frame', './resources/sourcemap-frame.html'),
@@ -22,19 +21,5 @@ async function test() {
   BindingsTestRunner.dumpLocation(jsLiveLocation);
   BindingsTestRunner.dumpLocation(cssLiveLocation);
 
-  TestRunner.markStep('navigateBack');
-  var originalUrl = 'http://127.0.0.1:8000/devtools/bindings/bindings-main-frame-navigated.html';
-  await TestRunner.navigatePromise(originalUrl);
-
   TestRunner.completeTest();
-}
-
-</script>
-
-</head>
-<body onload="runTest()">
-<p>
-Verify that debugger live location gets updated.
-</p>
-</body>
-</html>
+})();
