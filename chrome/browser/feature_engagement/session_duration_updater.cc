ff1d8d59edc8bcf987907afe9d2d31067eeb891f
Bettina Dea <bdea@chromium.org>
2017-10-19 17:28:05 -0700

    Add observed session time perf for each in-product help feature.
    There are many things going on in this CL:
    1. Added checking current elapsed session time when checking promo trigger.
    2. Added observed session time perf for each in product help feature
    3. Changed session duration updater to take in a session time perf and remove
    the session duration updater so that we can have multiple instances of it.
    4. Make sure that the feature trackers are not lazy loaded.
    5. Previously the session time was tracked using minutes which wasn't accurate
    if session times were only active for seconds which would automatically
    round down to 0 minutes which meant session time prefs would take a while
    before increasing.
    
    1:
    Previously, the perf for each desktop In-Product Help session time
    requirements was only populated when session is ended. This is through
    the session_duration_updater. However, that means when ShouldTriggerPromo
    is called, because the session time requirement has not been notified to
    the field trial params, it would always return false if the user is in a
    middle of a session where the cumulative session time of the previous
    sessions have not met the session time requirement. This causes a delay
    and wouldn't show the promo until the next session which does not meet
    the In-Product Help.
    
    Thus, I added an ElapsedTimer in the session_duration_updater to keep
    track of the current elapsed time of the current session. When calling
    the ShouldTriggerPromo method, it would first check the current elapsed
    time of the session and add it to what's recorded in the
    kObservedSessionTime perf to see if the current session has met the
    requirement instead of waiting until the session ended.
    
    2:
    Previously there was only one session time perf (kObservedSessionTimePerf).
    If more than one in-product help feature exists, having one perf would not
    work, since it never resets and we wouldn't know when to reset it. Thus,
    we need to create a perf for each feature. This leads to 3.
    
    3:
    To be able to track all multiple perfs simulatenously and also be flexibly
    when releasing new feature experiments, we need to change session duration
    updater to take in an observed session time perf. By doing this, we need to
    remove the factory and have the trackers their own session duration updater.
    This also means that the tracker factories have to make sure to register the
    profile prefs.
    
    4:
    Previously, all of the updaters, trackers, and factories were lazy loaded,
    which meant that current active session time was never tracked and wouldn't
    be loaded until a trigger event for a feature happened.
    
    5:
    I changed all the session time prefs to be int64 instead of integer as we are
    now tracking by seconds instead of minutes.
    
    Bug:734132
    Change-Id: I3c36471d9e36ec60e125045c5f7e272e39bf565b

--- chrome/browser/feature_engagement/session_duration_updater.cc
+++ chrome/browser/feature_engagement/session_duration_updater.cc
@@ -6,26 +6,36 @@
 
 #include "base/observer_list.h"
 #include "base/time/time.h"
-#include "chrome/browser/feature_engagement/tracker_factory.h"
+#include "base/timer/elapsed_timer.h"
+#include "base/values.h"
 #include "chrome/common/pref_names.h"
-#include "components/feature_engagement/public/event_constants.h"
-#include "components/feature_engagement/public/feature_constants.h"
-#include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
+#include "components/prefs/scoped_user_pref_update.h"
 
 namespace feature_engagement {
 
-SessionDurationUpdater::SessionDurationUpdater(PrefService* pref_service)
-    : duration_tracker_observer_(this), pref_service_(pref_service) {
+SessionDurationUpdater::SessionDurationUpdater(
+    PrefService* pref_service,
+    const char* observed_session_time_perf_key)
+    : duration_tracker_observer_(this),
+      pref_service_(pref_service),
+      observed_session_time_perf_key_(observed_session_time_perf_key) {
   AddDurationTrackerObserver();
 }
 
 SessionDurationUpdater::~SessionDurationUpdater() = default;
 
-// static
-void SessionDurationUpdater::RegisterProfilePrefs(
-    user_prefs::PrefRegistrySyncable* registry) {
-  registry->RegisterIntegerPref(prefs::kObservedSessionTime, 0);
+base::TimeDelta SessionDurationUpdater::GetActiveSessionElapsedTime() {
+  const base::DictionaryValue* dict =
+      pref_service_->GetDictionary(prefs::kObservedSessionTime);
+  const double stored_value =
+      dict->HasKey(observed_session_time_perf_key_)
+          ? dict->FindKey(observed_session_time_perf_key_)->GetDouble()
+          : 0L;
+  base::TimeDelta elapsed_time = base::TimeDelta::FromSeconds(stored_value);
+  return !elapsed_timer_ || !is_session_active_
+             ? elapsed_time
+             : elapsed_time + elapsed_timer_->Elapsed();
 }
 
 void SessionDurationUpdater::AddObserver(Observer* observer) {
@@ -35,8 +45,13 @@ void SessionDurationUpdater::AddObserver(Observer* observer) {
   // DesktopSessionDurationTracker if another feature is added after
   // SessionDurationUpdater was removed.
   if (!duration_tracker_observer_.IsObserving(
-          metrics::DesktopSessionDurationTracker::Get()))
+          metrics::DesktopSessionDurationTracker::Get())) {
+    if (!is_session_active_) {
+      elapsed_timer_.reset(new base::ElapsedTimer());
+      is_session_active_ = true;
+    }
     AddDurationTrackerObserver();
+  }
 }
 
 void SessionDurationUpdater::RemoveObserver(Observer* observer) {
@@ -44,8 +59,16 @@ void SessionDurationUpdater::RemoveObserver(Observer* observer) {
   // If all the observer Features have removed themselves due to their active
   // time limits have been reached, the SessionDurationUpdater removes itself
   // as an observer of DesktopSessionDurationTracker.
-  if (!observer_list_.might_have_observers())
+  if (!observer_list_.might_have_observers()) {
+    elapsed_timer_.reset();
+    is_session_active_ = false;
     RemoveDurationTrackerObserver();
+  }
+}
+
+void SessionDurationUpdater::OnSessionStarted(base::TimeTicks session_start) {
+  elapsed_timer_.reset(new base::ElapsedTimer());
+  is_session_active_ = true;
 }
 
 void SessionDurationUpdater::OnSessionEnded(base::TimeDelta elapsed) {
@@ -58,12 +81,22 @@ void SessionDurationUpdater::OnSessionEnded(base::TimeDelta elapsed) {
 
   base::TimeDelta elapsed_session_time;
 
-  elapsed_session_time +=
-      base::TimeDelta::FromMinutes(
-          pref_service_->GetInteger(prefs::kObservedSessionTime)) +
-      elapsed;
-  pref_service_->SetInteger(prefs::kObservedSessionTime,
-                            elapsed_session_time.InMinutes());
+  const base::DictionaryValue* dict =
+      pref_service_->GetDictionary(prefs::kObservedSessionTime);
+  const double stored_value =
+      dict->HasKey(observed_session_time_perf_key_)
+          ? dict->FindKey(observed_session_time_perf_key_)->GetDouble()
+          : 0L;
+
+  elapsed_session_time += base::TimeDelta::FromSeconds(stored_value) + elapsed;
+
+  DictionaryPrefUpdate update(pref_service_, prefs::kObservedSessionTime);
+  update->SetKey(
+      observed_session_time_perf_key_,
+      base::Value(static_cast<double>(elapsed_session_time.InSeconds())));
+
+  elapsed_timer_.reset();
+  is_session_active_ = false;
 
   for (Observer& observer : observer_list_)
     observer.OnSessionEnded(elapsed_session_time);
