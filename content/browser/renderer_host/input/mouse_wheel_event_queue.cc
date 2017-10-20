1799218c3011f4cd71d97bf0429193a1d649aa2b
Sahel Sharify <sahel@chromium.org>
2017-10-20 12:07:45 -0400

    Uma metric added to record latched vs non-latched wheel scrolling.
    Bug:774324
    Test:MouseWheelEventQueueTest.WheelScrollingWasLatchedHistogramCheck/*
    Change-Id: I243d693a9d70b40f764577b2b7b94b894c515ba0

--- content/browser/renderer_host/input/mouse_wheel_event_queue.cc
+++ content/browser/renderer_host/input/mouse_wheel_event_queue.cc
@@ -184,7 +184,10 @@ void MouseWheelEventQueue::ProcessMouseWheelAck(
         // Wheel event with phaseBegan must have non-zero deltas.
         DCHECK(needs_update);
         send_wheel_events_async_ = true;
+        RecordLatchingUmaMetric(false);
         SendScrollBegin(scroll_update, false);
+      } else if (needs_update) {
+        RecordLatchingUmaMetric(!needs_scroll_begin_);
       }
 
       if (needs_update) {
@@ -225,6 +228,7 @@ void MouseWheelEventQueue::ProcessMouseWheelAck(
         }
 
         if (needs_update) {
+          RecordLatchingUmaMetric(false);
           ui::LatencyInfo latency = ui::LatencyInfo(ui::SourceEventType::WHEEL);
           latency.AddLatencyNumber(
               ui::INPUT_EVENT_LATENCY_GENERATE_SCROLL_UPDATE_FROM_MOUSE_WHEEL,
@@ -341,6 +345,10 @@ void MouseWheelEventQueue::SendScrollBegin(
   needs_scroll_end_ = true;
   client_->ForwardGestureEventWithLatencyInfo(
       scroll_begin, ui::LatencyInfo(ui::SourceEventType::WHEEL));
+}
+
+void MouseWheelEventQueue::RecordLatchingUmaMetric(bool latched) {
+  UMA_HISTOGRAM_BOOLEAN("WheelScrolling.WasLatched", latched);
 }
 
 }  // namespace content
