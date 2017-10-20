de78d7c78609efcf55fc4d03a523dea090aa8aed
erikchen <erikchen@chromium.org>
2017-10-20 14:14:36 -0700

    Working on 10s backoff.
    Bug:
    Change-Id: Ieeacfdac990178c0e0d04295bb33d555675c8b47

--- chrome/common/profiling/memlog_allocator_shim.cc
+++ chrome/common/profiling/memlog_allocator_shim.cc
@@ -75,8 +75,14 @@ class SendBuffer {
 
  private:
   void SendCurrentBuffer() {
-    g_sender_pipe->Send(buffer_, used_);
+    MemlogSenderPipe::Result result = g_sender_pipe->Send(buffer_, used_);
     used_ = 0;
+    if (result == MemlogSenderPipe::Result::kError)
+      StopAllocatorShimDangerous();
+    if (result == MemlogSenderPipe::Result::kTimeout) {
+      StopAllocatorShimDangerous();
+      // TODO(erikchen): Emit a histogram.
+    }
   }
 
   base::Lock lock_;
@@ -91,10 +97,13 @@ SendBuffer* g_send_buffers = nullptr;
 
 // "address" is the address in question, which is used to select which send
 // buffer to use.
-void DoSend(const void* address, const void* data, size_t size) {
+void DoSend(const void* address,
+            const void* data,
+            size_t size,
+            SendBuffer* send_buffers) {
   base::trace_event::AllocationRegister::AddressHasher hasher;
   int bin_to_use = hasher(address) % kNumSendBuffers;
-  g_send_buffers[bin_to_use].Send(data, size);
+  send_buffers[bin_to_use].Send(data, size);
 }
 
 #if BUILDFLAG(USE_ALLOCATOR_SHIM)
@@ -250,13 +259,17 @@ void StopAllocatorShimDangerous() {
     g_hook_gc_alloc(nullptr);
     g_hook_gc_free(nullptr);
   }
+
+  if (g_sender_pipe)
+    g_sender_pipe->Close();
 }
 
 void AllocatorShimLogAlloc(AllocatorType type,
                            void* address,
                            size_t sz,
                            const char* context) {
-  if (!g_send_buffers)
+  SendBuffer* send_buffers = g_send_buffers;
+  if (!send_buffers)
     return;
   if (UNLIKELY(g_prevent_reentrancy.Pointer()->Get()))
     return;
@@ -308,14 +321,15 @@ void AllocatorShimLogAlloc(AllocatorType type,
       message_end += context_len;
     }
 
-    DoSend(address, message, message_end - message);
+    DoSend(address, message, message_end - message, send_buffers);
   }
 
   g_prevent_reentrancy.Pointer()->Set(false);
 }
 
 void AllocatorShimLogFree(void* address) {
-  if (!g_send_buffers)
+  SendBuffer* send_buffers = g_send_buffers;
+  if (!send_buffers)
     return;
   if (UNLIKELY(g_prevent_reentrancy.Pointer()->Get()))
     return;
@@ -326,17 +340,18 @@ void AllocatorShimLogFree(void* address) {
     free_packet.op = kFreePacketType;
     free_packet.address = (uint64_t)address;
 
-    DoSend(address, &free_packet, sizeof(FreePacket));
+    DoSend(address, &free_packet, sizeof(FreePacket), send_buffers);
   }
 
   g_prevent_reentrancy.Pointer()->Set(false);
 }
 
 void AllocatorShimFlushPipe(uint32_t barrier_id) {
-  if (!g_send_buffers)
+  SendBuffer* send_buffers = g_send_buffers;
+  if (!send_buffers)
     return;
   for (int i = 0; i < kNumSendBuffers; i++)
-    g_send_buffers[i].Flush();
+    send_buffers[i].Flush();
 
   BarrierPacket barrier;
   barrier.barrier_id = barrier_id;
