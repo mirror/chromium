d7ab6b1a57e2779f4cc9faeda612d28f898b5f8a
Koji Ishii <kojii@chromium.org>
2017-10-20 16:35:14 +0900

    [LayoutNG] Keep child NGLayoutResult in NGLineBoxFragmentBuilder
    This patch changes NGLineBoxFragmentBuilder to keep child
    NGLayoutResult until it finalizes children offsets and descendant
    box tree structure.
    
    This is needed to give the final offsets (relative to the line box)
    to e.g., out of flow descendants.
    
    Bug:635619
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I850fec48bc32262437965bec7cd3cbf1df2e660d

--- third_party/WebKit/Source/core/layout/ng/inline/ng_line_box_fragment_builder.h
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_line_box_fragment_builder.h
@@ -29,13 +29,6 @@ class CORE_EXPORT NGLineBoxFragmentBuilder final
 
   NGLogicalSize Size() const final;
 
-  void MoveChildrenInBlockDirection(LayoutUnit);
-  void MoveChildrenInBlockDirection(LayoutUnit, unsigned start, unsigned end);
-
-  Vector<RefPtr<NGPhysicalFragment>>& MutableChildren() { return children_; }
-  const Vector<NGLogicalOffset>& Offsets() const { return offsets_; }
-  Vector<NGLogicalOffset>& MutableOffsets() { return offsets_; }
-
   void SetMetrics(const NGLineHeightMetrics&);
   const NGLineHeightMetrics& Metrics() const { return metrics_; }
 
@@ -44,6 +37,57 @@ class CORE_EXPORT NGLineBoxFragmentBuilder final
   // Set the break token for the fragment to build.
   // A finished break token will be attached if not set.
   void SetBreakToken(RefPtr<NGInlineBreakToken>);
+
+  // A data struct to keep NGLayoutResult until the box tree structures and
+  // child offsets are finalized.
+  // Callers can add to the fragment builder in a batch once finalized.
+  struct Child {
+    RefPtr<NGLayoutResult> layout_result;
+    RefPtr<NGPhysicalFragment> fragment;
+    NGLogicalOffset offset;
+
+    const NGPhysicalFragment* PhysicalFragment() const;
+  };
+
+  class ChildList {
+    STACK_ALLOCATED();
+
+   public:
+    ChildList() {}
+    void operator=(ChildList&& other) {
+      children_ = std::move(other.children_);
+    }
+
+    Child& operator[](unsigned i) { return children_[i]; }
+
+    unsigned size() const { return children_.size(); }
+    bool IsEmpty() const { return children_.IsEmpty(); }
+    void ReserveInitialCapacity(unsigned capacity) {
+      children_.ReserveInitialCapacity(capacity);
+    }
+
+    using iterator = Vector<Child, 16>::iterator;
+    using const_iterator = Vector<Child, 16>::const_iterator;
+    iterator begin() { return children_.begin(); }
+    iterator end() { return children_.end(); }
+    const_iterator begin() const { return children_.begin(); }
+    const_iterator end() const { return children_.end(); }
+
+    void AddChild(RefPtr<NGLayoutResult>, const NGLogicalOffset&);
+    void AddChild(RefPtr<NGPhysicalFragment>, const NGLogicalOffset&);
+    // nullptr child is a placeholder until enclosing inline boxes are closed
+    // and we know the final box structure and their positions. This variant
+    // helps to avoid needing static_cast when adding a nullptr.
+    void AddChild(std::nullptr_t, const NGLogicalOffset&);
+
+    void MoveInBlockDirection(LayoutUnit);
+    void MoveInBlockDirection(LayoutUnit, unsigned start, unsigned end);
+
+   private:
+    Vector<Child, 16> children_;
+  };
+
+  void AddChildren(const ChildList&);
 
   // Creates the fragment. Can only be called once.
   RefPtr<NGLayoutResult> ToLineBoxFragment();
