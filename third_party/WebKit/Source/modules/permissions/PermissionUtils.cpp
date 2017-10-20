f00ee864a6d9953b93e91b22009ebc288d775b75
Gary Kacmarcik <garykac@chromium.org>
2017-10-19 17:03:21 -0700

    Initial renderer support for Clipboard Permission
    Bug: 677564
    Change-Id: If944340769c0192a9e123b7fc4b57b22b0619cac

--- third_party/WebKit/Source/modules/permissions/PermissionUtils.cpp
+++ third_party/WebKit/Source/modules/permissions/PermissionUtils.cpp
@@ -4,6 +4,8 @@
 
 #include "modules/permissions/PermissionUtils.h"
 
+#include <utility>
+
 #include "core/dom/Document.h"
 #include "core/dom/ExecutionContext.h"
 #include "core/frame/LocalFrame.h"
@@ -49,6 +51,17 @@ PermissionDescriptorPtr CreateMidiPermissionDescriptor(bool sysex) {
   midi_extension->sysex = sysex;
   descriptor->extension = mojom::blink::PermissionDescriptorExtension::New();
   descriptor->extension->set_midi(std::move(midi_extension));
+  return descriptor;
+}
+
+PermissionDescriptorPtr CreateClipboardPermissionDescriptor(
+    PermissionName name,
+    bool allowWithoutGesture) {
+  auto descriptor = CreatePermissionDescriptor(name);
+  auto clipboard_extension = mojom::blink::ClipboardPermissionDescriptor::New();
+  clipboard_extension->allowWithoutGesture = allowWithoutGesture;
+  descriptor->extension = mojom::blink::PermissionDescriptorExtension::New();
+  descriptor->extension->set_clipboard(std::move(clipboard_extension));
   return descriptor;
 }
 
