9d06060a728b90b8b62a81462f239378768aa33d
Ian Clelland <iclelland@google.com>
2017-10-20 13:14:14 -0400

    Track CSP-set sandbox flags separately from frame owner flags
    Bug: 740556
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: If696428160446e636eaa1a9fa3ef7e067b0c8922

--- content/test/test_render_frame_host.cc
+++ content/test/test_render_frame_host.cc
@@ -275,7 +275,7 @@ void TestRenderFrameHost::SimulateFeaturePolicyHeader(
   header[0].feature = feature;
   header[0].matches_all_origins = false;
   header[0].origins = whitelist;
-  OnDidSetFeaturePolicyHeader(header);
+  OnDidSetFramePolicyHeaders(blink::WebSandboxFlags::kNone, header);
 }
 
 void TestRenderFrameHost::SendNavigate(int nav_entry_id,
@@ -537,4 +537,9 @@ void TestRenderFrameHost::SimulateWillStartRequest(
       true /* user_gesture */, transition, false /* is_external_protocol */);
 }
 
+void TestRenderFrameHost::SendFramePolicy(
+    blink::WebSandboxFlags sandbox_flags,
+    const ParsedFeaturePolicyHeader& declared_policy) {
+  OnDidSetFramePolicyHeaders(sandbox_flags, declared_policy);
+}
 }  // namespace content
