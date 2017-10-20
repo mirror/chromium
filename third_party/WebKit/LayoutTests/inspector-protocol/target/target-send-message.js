5c5a7225d0d201c67ae0005e9b90306355650b2a
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2017-10-19 17:45:23 -0700

    [DevTools] better errors for Target.sendMessageToTarget
    Methods should generate notification with error on invalid
    message.
    
    inspector_protocol rolled to bdfbb48cdc1a1ccd7dc9456506feab46c8ffe90c:
    - [inspector_protocol] added UberDispatcher::getCommandName
    
    Bug:chromium:775105
    Change-Id: I47be11a4b1203781472a83c8ae38566a286f1e4d
    R=dgozman@chromium.org

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/target/target-send-message.js
@@ -0,0 +1,66 @@
+(async function(testRunner) {
+  let {page, session, dp} = await testRunner.startBlank('Tests sendMessage with invalid message.');
+
+  dp.Target.onDetachedFromTarget(msg => {
+    testRunner.log(msg);
+    testRunner.completeTest();
+  });
+
+  dp.Target.onTargetDestroyed(msg => {
+    testRunner.log(msg);
+    testRunner.completeTest();
+  });
+
+  await dp.Target.setAutoAttach({autoAttach: true, waitForDebuggerOnStart: false});
+  let p = dp.Target.onceAttachedToTarget();
+  session.evaluate(`
+    window.worker = new Worker('${testRunner.url('../resources/worker.js')}');
+  `);
+  let {params:{sessionId}} = await p;
+
+  p = dp.Target.onceReceivedMessageFromTarget();
+  testRunner.log('JSON syntax error..');
+  dp.Target.sendMessageToTarget({
+    sessionId: sessionId,
+    message: "{\"method\":\"Runtime.evaluate\",\"params\":{\"expression\":\"1\", awaitPromise: true},\"id\":1}"
+  });
+  let {params:{message}} = await p;
+  testRunner.log(message);
+
+  testRunner.log('JSON with primitive value..');
+  p = dp.Target.onceReceivedMessageFromTarget();
+  dp.Target.sendMessageToTarget({
+    sessionId: sessionId,
+    message: "42"
+  });
+  ({params:{message}} = await p);
+  testRunner.log(message);
+
+  testRunner.log('JSON without method property..');
+  p = dp.Target.onceReceivedMessageFromTarget();
+  dp.Target.sendMessageToTarget({
+    sessionId: sessionId,
+    message: "{}"
+  });
+  ({params:{message}} = await p);
+  testRunner.log(message);
+
+  testRunner.log('JSON without id property..');
+  p = dp.Target.onceReceivedMessageFromTarget();
+  dp.Target.sendMessageToTarget({
+    sessionId: sessionId,
+    message: "{\"method\":\"Runtime.evaluate\",\"params\":{\"expression\":\"42\"}}"
+  });
+  ({params:{message}} = await p);
+  testRunner.log(message);
+
+  testRunner.log('Valid JSON..');
+  p = dp.Target.onceReceivedMessageFromTarget();
+  dp.Target.sendMessageToTarget({
+    sessionId: sessionId,
+    message: "{\"method\":\"Runtime.evaluate\",\"params\":{\"expression\":\"42\"},\"id\":1}"
+  });
+  ({params:{message}} = await p);
+  testRunner.log(message);
+  testRunner.completeTest();
+})
