327da960e91e782fc3c31182bf550ddba6bb67f3
liberato@chromium.org <liberato@chromium.org>
2017-10-21 11:12:16 -0700

    SequencedObject draft.
    Bug: 774273
    Change-Id: I3042fa840fd0d317de298dcb91a4c3e402bcdf45

---
+++ media/base/sequenced_scoped_ref_ptr.h
@@ -0,0 +1,157 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_BASE_SEQUENCD_SCOPED_REF_PTR_H_
+#define MEDIA_BASE_SEQUENCD_SCOPED_REF_PTR_H_
+
+#include "base/memory/ptr_util.h"
+#include "base/sequenced_task_runner.h"
+#include "base/threading/sequenced_task_runner_handle.h"
+#include "media/base/sequenced_object.h"
+#include "media/base/sequenced_weak_ptr.h"
+
+namespace media {
+
+// Manage the lifetime of an instance of |T| on some other task runner.  |T|
+// will be constructed and destroyed on an impl task runner that's provided once
+// all references are dropped.
+//
+// The idea is that, often, one runs on sequence A, but wants to create / own
+// an object that runs on sequence B.  A SequencedScopedRefPtr lets you do
+// that easily:
+//
+//   struct Goo : public SequencedObject<Goo> {};
+//
+//   struct Foo {
+//     Foo(scoped_refptr<Sequenced...> goo_task_runner_) :
+//       goo_(goo_task_runner_,
+//            /* optional: any args for Foo(...) */ ) {}
+//
+//     SequencedScopedRefPtr<Goo> goo_owner_;
+//   };
+//
+// Goo will be constructed on |goo_task_runner_|, and destroyed there after
+// |goo_owner_| is destroyed.  You may call:
+//
+//   goo_owner_.GetSequencedWeakPtr()
+//
+// to get a SWP to talk to |goo_|.  Notice that you don't get access to a Goo*,
+// so it's impossible to refer to the underlying Goo object on the wrong thread.
+//
+// Also note that you have no idea if the instance of Goo has been constructed
+// yet, but that's okay.  It's guaranteed that construction is posted to the
+// impl thread during construction of the remote owner, and the weak pointer
+// will be valid by then.
+//
+// Remember that, from the point of view of the Goo instance, the sequence that
+// on which |goo_owner_| is constructed is a remote sequence.
+//
+// It is legal to copy and/or move a SequencedScopedRefPtr between threads once
+// construction is complete.  The underlying object will be retained until the
+// last copy is destroyed.
+//
+// TODO(liberato): I don't like this name.  Maybe SequencedObjectSharedOwner?
+// SequencedObjectSharedRef?  SequencedScopedRefPtr?
+template <typename T>
+class SequencedScopedRefPtr {
+ public:
+  // |impl_task_runner| is the local task runner on which |t_| will live.
+  template <typename... Args>
+  SequencedScopedRefPtr(
+      scoped_refptr<base::SequencedTaskRunner> impl_task_runner,
+      Args... args)
+      : record_(base::SequencedTaskRunnerHandle::Get()),
+        weak_ref_(new typename SequencedObject<T>::InternalWeakRef) {
+    // Post construction of |t_|.
+    record_->impl_task_runner->PostTask(
+        FROM_HERE,
+        base::BindOnce(
+            &SequencedScopedRefPtr<T>::ConstructOnImplSequence<Args...>,
+            record_.get(), weak_ref_, args...));
+  }
+
+  // If you've created an object that should be owned on the current thread, you
+  // may assign ownership to us.  This is useful to transfer ownership to some
+  // other object.
+  SequencedScopedRefPtr(std::unique_ptr<T> t)
+      : record_(new OwnerRecord(std::move(t),
+                                base::SequencedTaskRunnerHandle::Get())) {
+    // We don't need to create our own weak ref, since |t| already has
+    // one.  Just use that one.  Remember, we're on the right thread else this
+    // constructor shouldn't be used.
+    weak_ref_ = record_->t->GetWeakRef();
+  }
+
+  ~SequencedScopedRefPtr() {}
+
+  SequencedWeakPtr<T> GetSequencedWeakPtr() {
+    // Note that |weak_ref_| might not be filled in yet, but that's okay.  It
+    // will be filled in before it can be used on |impl_task_runner|, since we
+    // already posted construction there.
+    return SequencedWeakPtr<T>(record_->impl_task_runner, weak_ref_);
+  }
+
+ private:
+  // This owns the remote object, and guarantees that it will be destructed on
+  // the proper thread.
+  struct OwnerRecord : public base::RefCountedThreadSafe<OwnerRecord> {
+    OwnerRecord(scoped_refptr<base::SequencedTaskRunner> impl)
+        : impl_task_runner(std::move(impl)) {}
+
+    OwnerRecord(std::unique_ptr<T> t0,
+                scoped_refptr<base::SequencedTaskRunner> impl)
+        : t(std::move(t0)), impl_task_runner(std::move(impl)) {}
+
+    virtual ~OwnerRecord() {
+      // Post destruction of |t_|.
+      // It would be nice if we couldn't DeleteSoon a SequencedObject, but
+      // it seems to evade our detection.  So, for now, we skip wrapping this in
+      // something that would hide it.
+      if (t)
+        impl_task_runner->DeleteSoon(FROM_HERE, std::move(t));
+    }
+
+    // |t| may be modified only on the impl thread with a strong ref to |this|
+    // held.  It may also be modified from any thread from our destructor.
+    // Of course, dereferencing |t| is only allowed on the impl sequence.
+    std::unique_ptr<T> t;
+
+    // This is set during construction, and may be read from any thread.
+    const scoped_refptr<base::SequencedTaskRunner> impl_task_runner;
+
+    // NOTE: we could keep weak_ref here too, but it doesn't matter since it's
+    // already a scoped_refptr.
+  };
+
+  // Run on the impl sequence to actually construct |t_|, and return it to us
+  // via |wp| on the orignal thread.
+  template <typename... Args>
+  static void ConstructOnImplSequence(
+      scoped_refptr<base::SequencedTaskRunner> owner_task_runner,
+      scoped_refptr<OwnerRecord> record,
+      scoped_refptr<typename SequencedObject<T>::InternalWeakRef> weak_ref,
+      Args... args) {
+    OwnerRecord ptr;
+    // It is safe to do this, since only the impl thread may access |t|.
+    // One exception is the destuction of |record|, but since we hold a ref to
+    // it, that can't happen.
+    record->t = base::MakeUnique<T>(args...);
+
+    // Fill in |weak_ref| for any refs that we've already sent out.
+    weak_ref->ptr = record->t.get();
+
+    // Register |weak_ref_| so that |ptr.t| clears it on destruction.
+    record->t->RegisterWeakRef(weak_ref.get());
+  }
+
+  // Our reference to the shared owner.
+  scoped_refptr<OwnerRecord> record_;
+
+  // Weak ref to |t_|.  It is created here, but its pointer is filled in later.
+  scoped_refptr<typename SequencedObject<T>::InternalWeakRef> weak_ref_;
+};
+
+}  // namespace media
+
+#endif  // MEDIA_BASE_SEQUENCD_SCOPED_REF_PTR_H_
---
+++ media/base/sequenced_unique_ptr.h
@@ -0,0 +1,38 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_BASE_SEQUENCED_UNIQUE_PTR_H_
+#define MEDIA_BASE_SEQUENCED_UNIQUE_PTR_H_
+
+#include "media/base/sequenced_scoped_ref_ptr.h"
+
+namespace media {
+
+// We're exactly the same as a SequencedScopedRefPtr, except that we are
+// move-only.  We inherit privately to prevent casting and copying.
+template <typename T>
+class SequencedUniquePointer : private SequencedScopedRefPtr<T> {
+ public:
+  template <typename... Args>
+  SequencedUniquePointer(
+      scoped_refptr<base::SequencedTaskRunner> impl_task_runner,
+      Args... args)
+      : SequencedScopedRefPtr<T>(std::move(impl_task_runner),
+                                 std::move(args)...) {}
+
+  SequencedUniquePointer(std::unique_ptr<T> t)
+      : SequencedScopedRefPtr<T>(std::move(t)) {}
+
+  SequencedWeakPtr<T> GetSequencedWeakPtr() {
+    return SequencedUniquePointer<T>::GetSequencedWeakPtr();
+  }
+
+ private:
+  // TODO(liberato): we should allow assign, i suppose.
+  DISALLOW_COPY_AND_ASSIGN(SequencedUniquePointer);
+};
+
+}  // namespace media
+
+#endif  // MEDIA_BASE_SEQUENCED_UNIQUE_PTR_H_
