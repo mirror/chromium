07ab6c550b1d13c6a295c6628627a352733b531d
Lily Houghton <lilyhoughton@chromium.org>
2017-10-23 14:01:38 -0400

    [Cronet] preliminary performance testing
    Add initial performance testing to Cronet.  There are currently still some wrinkles to iron out, namely that it crashes and behaves strangely under a couple different circumstances.  The most effective way to run it correctly is using the counting profiler in the debug build, as the release build crashes immediately for unknown reasons, and running the debug build normally appears to trigger this bug: https://stackoverflow.com/questions/40371536/nsurlsession-causing-exc-bad-access
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I64f4a24baf8fe92d2d9d865752cbff4bcb2df12c

--- components/cronet/ios/test/cronet_perf_test_base.h
+++ components/cronet/ios/test/cronet_perf_test_base.h
@@ -28,15 +28,15 @@ class Thread;
 
 // NSURLSessionDataDelegate delegate implementation used by the tests to
 // wait for a response and check its status.
-@interface TestDelegate : NSObject<NSURLSessionDataDelegate>
+@interface PerfTestDelegate : NSObject<NSURLSessionDataDelegate>
 
 // Error the request this delegate is attached to failed with, if any.
 @property(retain, atomic) NSError* error;
 
 // Contains total amount of received data.
-@property(readonly) long totalBytesReceived;
+@property(readonly) NSMutableDictionary* totalBytesReceivedPerTask;
 
-@property(readonly) long expectedContentLength;
+@property(readonly) NSMutableDictionary* expectedContentLengthPerTask;
 
 // Resets the delegate, so it can be used again for another request.
 - (void)reset;
@@ -47,7 +47,7 @@ class Thread;
 /// Waits for a single request to complete.
 
 /// @return  |NO| if the request didn't complete and the method timed-out.
-- (BOOL)waitForDone;
+- (BOOL)waitForDone:(NSURLSessionDataTask*)task;
 
 @end
 
@@ -60,7 +60,7 @@ namespace cronet {
 
 // A base class that should be extended by all other Cronet tests.
 // The class automatically starts and stops the test QUIC server.
-class CronetTestBase : public ::testing::Test {
+class CronetPerfTestBase : public ::testing::Test {
  protected:
   static bool CalculatePublicKeySha256(const net::X509Certificate& cert,
                                        net::HashValue* out_hash_value);
@@ -74,8 +74,8 @@ class CronetTestBase : public ::testing::Test {
 
   ::testing::AssertionResult IsResponseSuccessful();
 
-  TestDelegate* delegate_;
-};  // class CronetTestBase
+  PerfTestDelegate* delegate_;
+};  // class CronetPerfTestBase
 
 }  // namespace cronet
 
