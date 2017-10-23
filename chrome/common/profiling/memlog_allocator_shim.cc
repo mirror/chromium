13ce778ccadfebe65ed789ea998249cfa22c6200
erikchen <erikchen@chromium.org>
2017-10-23 14:44:19 -0700

    OOP HP: Add 10 second timeout.
    This CL changes the MemlogSenderPipe to use a synchronous, non-blocking write
    with a 10-second timeout rather than a synchronous, blocking write. The latter
    could cause a process to stop functioning correctly if the pipe is not cleared
    in a timely fashion. After a timeout, the allocator shim is shut down.
    
    This requires the OS pipe to have slightly different characteristics than
    allowed by PlatformChannelPair. On Windows, we want the pipe to have a 64k
    buffer rather than a 4k buffer. For performance reasons, on macOS and Linux, we
    want to use a pipe rather than a socket. This CL changes the AllocatorShim to
    create its own pipes, rather than using mojo::edk::PlatformChannelPair.
    
    This CL updates the tear down logic in memlog_allocator_shim.cc to be thread
    safe, and to close its end of the pipe on error.
    
    Bug:777546
    Change-Id: Ieeacfdac990178c0e0d04295bb33d555675c8b47

--- chrome/common/profiling/memlog_allocator_shim.cc
+++ chrome/common/profiling/memlog_allocator_shim.cc
@@ -7,6 +7,7 @@
 #include "base/allocator/allocator_shim.h"
 #include "base/allocator/features.h"
 #include "base/allocator/partition_allocator/partition_alloc.h"
+#include "base/atomicops.h"
 #include "base/compiler_specific.h"
 #include "base/debug/debugging_flags.h"
 #include "base/debug/stack_trace.h"
@@ -29,15 +30,12 @@ using base::allocator::AllocatorDispatch;
 
 MemlogSenderPipe* g_sender_pipe = nullptr;
 
-// Matches the native buffer size on the pipe.
-// On Windows and Linux, the default pipe buffer size is 65536.
-// On macOS, the default pipe buffer size is 16 * 1024, but grows to 64 * 1024
-// for large writes.
-constexpr int kSendBufferSize = 65536;
-
 // Prime since this is used like a hash table. Numbers of this magnitude seemed
 // to provide sufficient parallelism to avoid lock overhead in ad-hoc testing.
 constexpr int kNumSendBuffers = 17;
+
+// If writing to the MemlogSenderPipe ever takes longer than 10s, just give up.
+constexpr int kTimeoutMs = 10000;
 
 // Functions set by a callback if the GC heap exists in the current process.
 // This function pointers can be used to hook or unhook the oilpan allocations.
@@ -54,13 +52,13 @@ base::LazyInstance<base::ThreadLocalBoolean>::Leaky g_prevent_reentrancy =
 
 class SendBuffer {
  public:
-  SendBuffer() : buffer_(new char[kSendBufferSize]) {}
+  SendBuffer() : buffer_(new char[MemlogSenderPipe::kPipeSize]) {}
   ~SendBuffer() { delete[] buffer_; }
 
   void Send(const void* data, size_t sz) {
     base::AutoLock lock(lock_);
 
-    if (used_ + sz > kSendBufferSize)
+    if (used_ + sz > MemlogSenderPipe::kPipeSize)
       SendCurrentBuffer();
 
     memcpy(&buffer_[used_], data, sz);
@@ -75,8 +73,15 @@ class SendBuffer {
 
  private:
   void SendCurrentBuffer() {
-    g_sender_pipe->Send(buffer_, used_);
+    MemlogSenderPipe::Result result =
+        g_sender_pipe->Send(buffer_, used_, kTimeoutMs);
     used_ = 0;
+    if (result == MemlogSenderPipe::Result::kError)
+      StopAllocatorShimDangerous();
+    if (result == MemlogSenderPipe::Result::kTimeout) {
+      StopAllocatorShimDangerous();
+      // TODO(erikchen): Emit a histogram. https://crbug.com/777546.
+    }
   }
 
   base::Lock lock_;
@@ -87,14 +92,22 @@ class SendBuffer {
   DISALLOW_COPY_AND_ASSIGN(SendBuffer);
 };
 
-SendBuffer* g_send_buffers = nullptr;
+// When the allocator shim is enabled, this variable holds a pointer to an array
+// of SendBuffers. When the allocator shim is disabled, this variable holds
+// nullptr. When the allocator shim is disabled, we do not destroy the array of
+// SendBuffers. As such, we do not need barriers to prevent reordering, we only
+// need to ensure that writing/reading from this variable is atomic.
+base::subtle::AtomicWord g_send_buffers = 0;
 
 // "address" is the address in question, which is used to select which send
 // buffer to use.
-void DoSend(const void* address, const void* data, size_t size) {
+void DoSend(const void* address,
+            const void* data,
+            size_t size,
+            SendBuffer* send_buffers) {
   base::trace_event::AllocationRegister::AddressHasher hasher;
   int bin_to_use = hasher(address) % kNumSendBuffers;
-  g_send_buffers[bin_to_use].Send(data, size);
+  send_buffers[bin_to_use].Send(data, size);
 }
 
 #if BUILDFLAG(USE_ALLOCATOR_SHIM)
@@ -222,7 +235,9 @@ void InitAllocatorShim(MemlogSenderPipe* sender_pipe) {
   // Must be done before hooking any functions that make stack traces.
   base::debug::EnableInProcessStackDumping();
 
-  g_send_buffers = new SendBuffer[kNumSendBuffers];
+  base::subtle::NoBarrier_Store(&g_send_buffers,
+                                reinterpret_cast<base::subtle::AtomicWord>(
+                                    new SendBuffer[kNumSendBuffers]));
   g_sender_pipe = sender_pipe;
 
 #if BUILDFLAG(USE_ALLOCATOR_SHIM)
@@ -242,7 +257,11 @@ void InitAllocatorShim(MemlogSenderPipe* sender_pipe) {
 }
 
 void StopAllocatorShimDangerous() {
-  g_send_buffers = nullptr;
+  // This pointer is leaked on purpose to avoid races. The contention caused by
+  // adding a lock adds too much overhead.
+  base::subtle::NoBarrier_Store(
+      &g_send_buffers, reinterpret_cast<base::subtle::AtomicWord>(nullptr));
+
   base::PartitionAllocHooks::SetAllocationHook(nullptr);
   base::PartitionAllocHooks::SetFreeHook(nullptr);
 
@@ -250,13 +269,18 @@ void StopAllocatorShimDangerous() {
     g_hook_gc_alloc(nullptr);
     g_hook_gc_free(nullptr);
   }
+
+  if (g_sender_pipe)
+    g_sender_pipe->Close();
 }
 
 void AllocatorShimLogAlloc(AllocatorType type,
                            void* address,
                            size_t sz,
                            const char* context) {
-  if (!g_send_buffers)
+  SendBuffer* send_buffers = reinterpret_cast<SendBuffer*>(
+      base::subtle::NoBarrier_Load(&g_send_buffers));
+  if (!send_buffers)
     return;
   if (UNLIKELY(g_prevent_reentrancy.Pointer()->Get()))
     return;
@@ -266,7 +290,7 @@ void AllocatorShimLogAlloc(AllocatorType type,
     constexpr size_t max_message_size = sizeof(AllocPacket) +
                                         kMaxStackEntries * sizeof(uint64_t) +
                                         kMaxContextLen;
-    static_assert(max_message_size < kSendBufferSize,
+    static_assert(max_message_size < MemlogSenderPipe::kPipeSize,
                   "We can't have a message size that exceeds the pipe write "
                   "buffer size.");
     char message[max_message_size];
@@ -308,14 +332,16 @@ void AllocatorShimLogAlloc(AllocatorType type,
       message_end += context_len;
     }
 
-    DoSend(address, message, message_end - message);
+    DoSend(address, message, message_end - message, send_buffers);
   }
 
   g_prevent_reentrancy.Pointer()->Set(false);
 }
 
 void AllocatorShimLogFree(void* address) {
-  if (!g_send_buffers)
+  SendBuffer* send_buffers = reinterpret_cast<SendBuffer*>(
+      base::subtle::NoBarrier_Load(&g_send_buffers));
+  if (!send_buffers)
     return;
   if (UNLIKELY(g_prevent_reentrancy.Pointer()->Get()))
     return;
@@ -326,21 +352,28 @@ void AllocatorShimLogFree(void* address) {
     free_packet.op = kFreePacketType;
     free_packet.address = (uint64_t)address;
 
-    DoSend(address, &free_packet, sizeof(FreePacket));
+    DoSend(address, &free_packet, sizeof(FreePacket), send_buffers);
   }
 
   g_prevent_reentrancy.Pointer()->Set(false);
 }
 
 void AllocatorShimFlushPipe(uint32_t barrier_id) {
-  if (!g_send_buffers)
+  SendBuffer* send_buffers = reinterpret_cast<SendBuffer*>(
+      base::subtle::NoBarrier_Load(&g_send_buffers));
+  if (!send_buffers)
     return;
   for (int i = 0; i < kNumSendBuffers; i++)
-    g_send_buffers[i].Flush();
+    send_buffers[i].Flush();
 
   BarrierPacket barrier;
   barrier.barrier_id = barrier_id;
-  g_sender_pipe->Send(&barrier, sizeof(barrier));
+  MemlogSenderPipe::Result result =
+      g_sender_pipe->Send(&barrier, sizeof(barrier), kTimeoutMs);
+  if (result != MemlogSenderPipe::Result::kSuccess) {
+    StopAllocatorShimDangerous();
+    // TODO(erikchen): Emit a histogram. https://crbug.com/777546.
+  }
 }
 
 void SetGCHeapAllocationHookFunctions(SetGCAllocHookFunction hook_alloc,
--- chrome/common/profiling/memlog_sender_pipe.h
+++ chrome/common/profiling/memlog_sender_pipe.h
@@ -7,10 +7,57 @@
 
 #include "build/build_config.h"
 
-#if defined(OS_WIN)
-#include "chrome/common/profiling/memlog_sender_pipe_win.h"
-#else
-#include "chrome/common/profiling/memlog_sender_pipe_posix.h"
-#endif
+#include "base/files/platform_file.h"
+#include "base/macros.h"
+#include "base/synchronization/lock.h"
+#include "mojo/edk/embedder/scoped_platform_handle.h"
+
+namespace profiling {
+
+class MemlogSenderPipe {
+ public:
+  struct Pipes {
+    Pipes();
+    Pipes(Pipes&&);
+    mojo::edk::ScopedPlatformHandle sender;
+    mojo::edk::ScopedPlatformHandle receiver;
+    DISALLOW_COPY_AND_ASSIGN(Pipes);
+  };
+
+  // Matches the native buffer size on the pipe.
+  // On Windows and Linux, the default pipe buffer size is 64 * 1024.
+  // On macOS, the default pipe buffer size is 16 * 1024, but grows to 64 * 1024
+  // for large writes.
+  static constexpr size_t kPipeSize = 64 * 1024;
+
+  // Returns a pair of newly created pipes. Must be called from a privileged
+  // process. The sender-pipe is non-blocking and has a buffer size of
+  // |kPipeSize|.
+  static Pipes CreatePipes();
+
+  explicit MemlogSenderPipe(base::ScopedPlatformFile file);
+  ~MemlogSenderPipe();
+
+  enum class Result { kSuccess, kTimeout, kError };
+
+  // Attempts to atomically write all the |data| into the pipe. kError is
+  // returned on failure, kTimeout after |timeout_ms| milliseconds.
+  Result Send(const void* data, size_t sz, int timeout_ms);
+
+  // Closes the underlying pipe.
+  void Close();
+
+ private:
+  base::ScopedPlatformFile file_;
+
+  // All calls to Send() are wrapped in a Lock, since the size of the data might
+  // be larger than the maximum atomic write size of a pipe on Posix [PIPE_BUF].
+  // On Windows, ::WriteFile() is not thread-safe.
+  base::Lock lock_;
+
+  DISALLOW_COPY_AND_ASSIGN(MemlogSenderPipe);
+};
+
+}  // namespace profiling
 
 #endif  // CHROME_COMMON_PROFILING_MEMLOG_SENDER_PIPE_H_
