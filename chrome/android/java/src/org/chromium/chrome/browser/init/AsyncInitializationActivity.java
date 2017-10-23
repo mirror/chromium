dedfda323a7a67d7fc8a0589283f82bb8ba6a227
Dmitry Skiba <dskiba@chromium.org>
2017-10-23 13:18:27 -0700

    Don't animate when aborting CTA launch.
    Sometimes there is a visual glitch when ChromeTabbedActivity aborts
    after starting another activity. See the bug for one example. That
    is because abortLaunch() function disables exit animation for CTA
    window, and cancels currently running CTA window animation midway,
    creating something that is perceived as a flash.
    
    This CL replaces exit animation for CTA and enter animation for the
    next activity with 'no_anim' animation, which doesn't animate, but
    keeps both windows visible for a short time,
    
    
    
    Bug:776456
    Change-Id: I4a8de790087c4f3f2711999c2255198f311b6b7e

--- chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/AsyncInitializationActivity.java
@@ -307,7 +307,7 @@ public abstract class AsyncInitializationActivity extends AppCompatActivity impl
     private void abortLaunch() {
         super.onCreate(null);
         ApiCompatibilityUtils.finishAndRemoveTask(this);
-        overridePendingTransition(R.anim.activity_open_enter, 0);
+        overridePendingTransition(R.anim.no_anim, R.anim.no_anim);
     }
 
     /**
