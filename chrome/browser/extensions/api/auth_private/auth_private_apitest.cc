7bd020b2c6ea9b4178d6e67c0cf9e10aced5fc4b
Achuith Bhandarkar <achuith@chromium.org>
2017-10-23 15:46:53 +0200

    Copy auth-related functions from autotestPrivate to authPrivate.
    Bug: 777356
    Test: browser test
    Change-Id: I20a16a887bf3e37471f1c6601b9b29cf27eaee86

---
+++ chrome/browser/extensions/api/auth_private/auth_private_apitest.cc
@@ -0,0 +1,17 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "build/build_config.h"
+#include "chrome/browser/extensions/api/auth_private/auth_private_api.h"
+#include "chrome/browser/extensions/extension_apitest.h"
+
+#if defined(OS_CHROMEOS)
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, AuthPrivate) {
+  // Turn on testing mode so we don't kill the browser.
+  extensions::AuthPrivateAPI::GetFactoryInstance()
+      ->Get(browser()->profile())
+      ->set_test_mode(true);
+  ASSERT_TRUE(RunComponentExtensionTest("auth_private")) << message_;
+}
+#endif
