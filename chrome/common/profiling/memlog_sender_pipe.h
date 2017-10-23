a1595d6bb729ddc67c521368f72d02dea9f38a10
Erik Chen <erikchen@chromium.org>
2017-10-23 12:15:39 -0700

    Working on 10s backoff.
    Bug:
    Change-Id: Ieeacfdac990178c0e0d04295bb33d555675c8b47

--- chrome/common/profiling/memlog_sender_pipe.h
+++ chrome/common/profiling/memlog_sender_pipe.h
@@ -7,10 +7,54 @@
 
 #include "build/build_config.h"
 
-#if defined(OS_WIN)
-#include "chrome/common/profiling/memlog_sender_pipe_win.h"
+#include "base/files/platform_file.h"
+#include "base/macros.h"
+#include "base/synchronization/lock.h"
+#include "mojo/edk/embedder/scoped_platform_handle.h"
+
+namespace profiling {
+
+class MemlogSenderPipe {
+ public:
+  struct Pipes {
+    Pipes();
+    Pipes(Pipes&&);
+    mojo::edk::ScopedPlatformHandle sender;
+    mojo::edk::ScopedPlatformHandle receiver;
+    DISALLOW_COPY_AND_ASSIGN(Pipes);
+  };
+
+  // Returns a pair of newly created pipes. Must be called from a privileged
+  // process. The sender-pipe is non-blocking and has a buffer size of 64kb.
+  static Pipes CreatePipes();
+
+  explicit MemlogSenderPipe(base::ScopedPlatformFile file);
+  ~MemlogSenderPipe();
+
+  enum class Result { kSuccess, kTimeout, kError };
+
+  // Attempts to atomically write all the |data| into the pipe. kError is
+  // returned on failure, kTimeout after |timeout_ms| milliseconds.
+  Result Send(const void* data, size_t sz, int timeout_ms);
+
+  // Closes the underlying pipe.
+  void Close();
+
+ private:
+#if defined(OS_WIN) || defined(OS_MACOSX)
+  base::ScopedPlatformFile file_;
 #else
-#include "chrome/common/profiling/memlog_sender_pipe_posix.h"
+  mojo::edk::ScopedPlatformHandle handle_;
 #endif
+
+  // All calls to Send() are wrapped in a Lock, since the size of the data might
+  // be larger than the maximum atomic write size of a pipe on Posix [PIPE_BUF].
+  // On Windows, ::WriteFile() is not thread-safe.
+  base::Lock lock_;
+
+  DISALLOW_COPY_AND_ASSIGN(MemlogSenderPipe);
+};
+
+}  // namespace profiling
 
 #endif  // CHROME_COMMON_PROFILING_MEMLOG_SENDER_PIPE_H_
--- chrome/common/profiling/memlog_sender_pipe_posix.cc
+++ chrome/common/profiling/memlog_sender_pipe_posix.cc
@@ -2,8 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/common/profiling/memlog_sender_pipe_posix.h"
+#include "chrome/common/profiling/memlog_sender_pipe.h"
 
+#include <poll.h>
 #include <unistd.h>
 
 #include "base/logging.h"
@@ -11,31 +12,116 @@
 #include "chrome/common/profiling/memlog_stream.h"
 #include "mojo/edk/embedder/platform_channel_utils_posix.h"
 
+#if defined(OS_MACOSX)
+#include "base/posix/eintr_wrapper.h"
+#endif
+
 namespace profiling {
 
+MemlogSenderPipe::Pipes MemlogSenderPipe::CreatePipes() {
+#if defined(OS_MACOSX
+  // On macOS, we create a pipe() rather than a socketpair(). This causes
+  // writes to be much more performant.
+  // https://bugs.chromium.org/p/chromium/issues/detail?id=776435
+  int fds[2];
+  pipe(fds);
+  PCHECK(fcntl(fds[0], F_SETFL, O_NONBLOCK) == 0);
+  PCHECK(fcntl(fds[1], F_SETFL, O_NONBLOCK) == 0);
+  PCHECK(fcntl(fds[0], F_SETNOSIGPIPE, 1) == 0);
+  PCHECK(fcntl(fds[1], F_SETNOSIGPIPE, 1) == 0);
+
+  profiling_service_->AddProfilingClient(
+      pid, std::move(client), mojo::WrapPlatformFile(fds[1]),
+      mojo::WrapPlatformFile(fds[0]), process_type);
+#else
+  // Writes to the data_channel must be atomic to ensure that the profiling
+  // process can demux the messages. We accomplish this by making writes
+  // synchronous, and protecting the write() itself with a Lock.
+  // On Windows, this calls CreateNamedPipeW with output and input buffer size
+  // suggestions of 4096. Perhaps this should be increased to 65536.
+  mojo::edk::PlatformChannelPair data_channel(false /* client_is_blocking */);
+
+  // Passes the client_for_profiling directly to the profiling process.
+  // The client process can not start sending data until the pipe is ready,
+  // so talking to the client is done in the AddSender completion callback.
+  //
+}
+
+#if defined(OS_MACOSX)
+MemlogSenderPipe::MemlogSenderPipe(base::ScopedPlatformFile file)
+    : file_(std::move(file)) {}
+#else
 MemlogSenderPipe::MemlogSenderPipe(base::ScopedPlatformFile file)
     : handle_(mojo::edk::PlatformHandle(file.release())) {}
+#endif
 
 MemlogSenderPipe::~MemlogSenderPipe() {
 }
 
-bool MemlogSenderPipe::Connect() {
-  // In posix-land, the pipe is just handed to us already connected.
-  return true;
+MemlogSenderPipe::Result MemlogSenderPipe::Send(const void* data,
+                                                size_t sz,
+                                                int timeout_ms) {
+  base::AutoLock lock(lock_);
+
+  // This can happen if Close() was called on another thread, while this thread
+  // was already waiting to call MemlogSenderPipe::Send().
+  if (!handle_.is_valid())
+    return Result::kError;
+
+  int size = static_cast<int>(sz);
+  base::Time start_time;
+  while (size > 0) {
+#if defined(OS_MACOSX)
+    int r = HANDLE_EINTR(write(handle_.get().handle, data, size));
+#else
+    int r = mojo::edk::PlatformChannelWrite(handle_.get(), data, size);
+#endif
+
+    // On success!
+    if (r != -1) {
+      DCHECK_LE(r, size);
+      size -= r;
+      data = static_cast<const char*>(data) + r;
+      continue;
+    }
+
+    // An error is either irrecoverable, or an I/O delay. Wait at most
+    // timeout_ms seconds for the pipe to clear.
+    int cached_errno = errno;
+    if (cached_errno != EAGAIN && cached_errno != EWOULDBLOCK)
+      return Result::kError;
+
+    // Set the start time, if it hasn't already been set.
+    base::Time now = base::Time::Now();
+    if (start_time.is_null())
+      start_time = now;
+
+    // Calculate time left.
+    int64_t time_left_ms =
+        ((start_time + base::TimeDelta::FromMilliseconds(timeout_ms)) - now)
+            .InMilliseconds();
+    if (time_left_ms <= 0)
+      return Result::kTimeout;
+
+    // Wait for the pipe to be writeable.
+    struct pollfd pfd = {handle_.get().handle, POLLOUT, 0};
+    int poll_result = poll(&pfd, 1, static_cast<int>(time_left_ms));
+    if (poll_result == 0)
+      return Result::kTimeout;
+    if (poll_result == -1)
+      return Result::kError;
+
+    // If POLLOUT isn't returned, the pipe isn't writeable.
+    DCHECK_EQ(poll_result, 1);
+    if (!(pfd.revents & POLLOUT))
+      return Result::kError;
+  }
+  return Result::kSuccess;
 }
 
-bool MemlogSenderPipe::Send(const void* data, size_t sz) {
+void MemlogSenderPipe::Close() {
   base::AutoLock lock(lock_);
-  int size = static_cast<int>(sz);
-  while (size > 0) {
-    int r = mojo::edk::PlatformChannelWrite(handle_.get(), data, size);
-    if (r == -1)
-      return false;
-    DCHECK_LE(r, size);
-    size -= r;
-    data = static_cast<const char*>(data) + r;
-  }
-  return true;
+  handle_.reset();
 }
 
 }  // namespace profiling
