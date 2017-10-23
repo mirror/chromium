cf32b161cebf84602cea9c00bcb239d0b670dc97
Vaclav Brozek <vabr@chromium.org>
2017-10-23 16:42:47 +0200

    Separate authentication logic from PasswordManagerPresenter
    Currently, PasswordManagerPresenter offers OS-level reauthentication to
    be used before revealing passwords in settings. This reauthentication
    has a platform independent logic concerning time spans of
    reauthentication attempt validity, and platform dependent OS calls to
    show appropriate UI. This is all inside one method within
    PasswordManagerPresenter. That makes it hard to test both the
    time-handling logic, and the fact that the rest of
    PasswordManagerPresenter invokes reauthentication as needed.
    
    Therefore, this CL separates that logic into a class,
    PasswordAccessAuthenticator, which contains precisely the
    platform-independent logic. An object of that class gets a callback to
    invoke the OS-dependent UI calls, which allows mocking it. Therefore the
    logic itself becomes easily testable, and also PasswordManagerPresenter
    can be tested for using reauthentication.
    
    Bug: 700003
    Change-Id: Ie694895c9e730be095712b95e64f320601fc0f8f

---
+++ chrome/browser/ui/passwords/password_access_authenticator.cc
@@ -0,0 +1,41 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/passwords/password_access_authenticator.h"
+
+#include "base/metrics/histogram_macros.h"
+#include "base/time/default_clock.h"
+#include "components/password_manager/core/browser/password_manager_metrics_util.h"
+
+PasswordAccessAuthenticator::PasswordAccessAuthenticator(
+    base::RepeatingCallback<bool()> os_reauth_call)
+    : clock_(std::make_unique<base::DefaultClock>()),
+      os_reauth_call_(std::move(os_reauth_call)) {}
+
+PasswordAccessAuthenticator::~PasswordAccessAuthenticator() = default;
+
+// TODO(crbug.com/327331): Trigger Re-Auth after closing and opening the
+// settings tab.
+bool PasswordAccessAuthenticator::EnsureUserIsAuthenticated() {
+  const bool can_skip_reauth = last_authentication_time_.has_value() &&
+                               clock_->Now() - *last_authentication_time_ <=
+                                   base::TimeDelta::FromSeconds(60);
+  if (can_skip_reauth) {
+    UMA_HISTOGRAM_ENUMERATION(
+        "PasswordManager.ReauthToAccessPasswordInSettings",
+        password_manager::metrics_util::REAUTH_SKIPPED,
+        password_manager::metrics_util::REAUTH_COUNT);
+    return true;
+  }
+
+  bool authenticated = os_reauth_call_.Run();
+  if (authenticated)
+    last_authentication_time_ = clock_->Now();
+  UMA_HISTOGRAM_ENUMERATION(
+      "PasswordManager.ReauthToAccessPasswordInSettings",
+      authenticated ? password_manager::metrics_util::REAUTH_SUCCESS
+                    : password_manager::metrics_util::REAUTH_FAILURE,
+      password_manager::metrics_util::REAUTH_COUNT);
+  return authenticated;
+}
---
+++ chrome/browser/ui/passwords/password_access_authenticator.h
@@ -0,0 +1,59 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_PASSWORDS_PASSWORD_ACCESS_AUTHENTICATOR_H_
+#define CHROME_BROWSER_UI_PASSWORDS_PASSWORD_ACCESS_AUTHENTICATOR_H_
+
+#include <memory>
+#include <utility>
+
+#include "base/callback.h"
+#include "base/macros.h"
+#include "base/optional.h"
+#include "base/time/clock.h"
+#include "base/time/time.h"
+
+// This class takes care of reauthentication used for accessing passwords
+// through the settings page. It is used on all platforms but iOS (see
+// //ios/chrome/browser/ui/settings/reauthentication_module.* for iOS).
+class PasswordAccessAuthenticator {
+ public:
+  // |os_reauth_call| is passed to |os_reauth_call_|, see the latter for
+  // explanation.
+  explicit PasswordAccessAuthenticator(
+      base::RepeatingCallback<bool()> os_reauth_call);
+
+  ~PasswordAccessAuthenticator();
+
+  // Returns whether the user is able to pass the authentication challenge,
+  // which is represented by |os_reauth_call_| returning true. A successful
+  // result of |os_reauth_call_| is cached for 60 seconds.
+  bool EnsureUserIsAuthenticated();
+
+  // Use this in tests to mock the OS-level reauthentication.
+  void set_os_reauth_call_for_testing(
+      base::RepeatingCallback<bool()> os_reauth_call) {
+    os_reauth_call_ = std::move(os_reauth_call);
+  }
+
+  // Use this to manipulate time in tests.
+  void set_clock_for_testing(std::unique_ptr<base::Clock> clock) {
+    clock_ = std::move(clock);
+  }
+
+ private:
+  // The last time the user was successfully authenticated.
+  base::Optional<base::Time> last_authentication_time_;
+
+  // Used to measure the time since the last authentication.
+  std::unique_ptr<base::Clock> clock_;
+
+  // Used to directly present the authentication challenge (such as the login
+  // prompt) to the user.
+  base::RepeatingCallback<bool()> os_reauth_call_;
+
+  DISALLOW_COPY_AND_ASSIGN(PasswordAccessAuthenticator);
+};
+
+#endif  // CHROME_BROWSER_UI_PASSWORDS_PASSWORD_ACCESS_AUTHENTICATOR_H_
