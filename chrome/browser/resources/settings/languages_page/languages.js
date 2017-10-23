811d6ca4d4c3f346ffc2845d2e29646dac43fbe9
Claudio Magni <claudiomagni@chromium.org>
2017-10-23 18:36:44 +1100

    Added C++ API to move a language inside the list in Settings.
    Currently, the logic that moves a language is completely handled in JS.
    This change moves the logic to C++, into TranslatePrefs, which allows for
    more extensive testing and better separation between UI and
    preferences/settings.
    
    This change is not behind a flag, because it's re-implementing the same
    behavior and should be transparent for the user.
    The only actual change is to fix a bug present in the current JS code.
    
    Bug: 746771
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I042e417b0c6ef3563cab132c548d73dd046f7de1

--- chrome/browser/resources/settings/languages_page/languages.js
+++ chrome/browser/resources/settings/languages_page/languages.js
@@ -619,29 +619,15 @@ Polymer({
     if (!CrSettingsPrefs.isInitialized)
       return;
 
-    var languageCodes =
-        this.getPref(preferredLanguagesPrefName).value.split(',');
+    var tempArray = Array.from(this.enabledLanguageSet_);
+    var listStr = tempArray.join(',');
 
-    var originalIndex = languageCodes.indexOf(languageCode);
-    var newIndex = originalIndex;
-    var direction = Math.sign(offset);
-    var distance = Math.abs(offset);
-
-    // Step over the distance to find the target index.
-    while (distance > 0) {
-      newIndex += direction;
-      if (newIndex < 0 || newIndex >= languageCodes.length)
-        return;
-
-      // Skip over non-enabled languages, since they don't appear in the list
-      // (but we don't want to remove them).
-      if (this.enabledLanguageSet_.has(languageCodes[newIndex]))
-        distance--;
+    if (offset < 0) {
+      this.languageSettingsPrivate_.moveLanguageUp(languageCode, listStr);
+    } else if (offset > 0) {
+      this.languageSettingsPrivate_.moveLanguageDown(languageCode, listStr);
     }
 
-    languageCodes[originalIndex] = languageCodes[newIndex];
-    languageCodes[newIndex] = languageCode;
-    this.setPrefValue(preferredLanguagesPrefName, languageCodes.join(','));
   },
 
   /**
@@ -652,15 +638,9 @@ Polymer({
     if (!CrSettingsPrefs.isInitialized)
       return;
 
-    var languageCodes =
-        this.getPref(preferredLanguagesPrefName).value.split(',');
-    var originalIndex = languageCodes.indexOf(languageCode);
-    assert(originalIndex != -1);
-
-    languageCodes.splice(originalIndex, 1);
-    languageCodes.unshift(languageCode);
-
-    this.setPrefValue(preferredLanguagesPrefName, languageCodes.join(','));
+    var tempArray = Array.from(this.enabledLanguageSet_);
+    var listStr = tempArray.join(',');
+    this.languageSettingsPrivate_.moveLanguageToTheTop(languageCode, listStr);
   },
 
   /**
