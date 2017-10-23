b229ebadc79047185a889890fa8a1520446d78b6
Patti <patricialor@chromium.org>
2017-10-23 11:38:21 +1100

    Views/Harmony: Add views::HoverButton for use in the ProfileChooserView.
    Extract the BackgroundColorHoverButton from the ProfileChooserView and turn it
    into a proper Views control which is compliant with the Harmony spec. This
    prepares the views::HoverButton control to be used in Harmony Page Info.
    
    Bug: 535074
    Change-Id: Idd964d17868a7057d5c18b1b96c6d150b7a6d866

---
+++ ui/views/controls/button/hover_button.h
@@ -0,0 +1,63 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_VIEWS_CONTROLS_BUTTON_HOVER_BUTTON_H_
+#define UI_VIEWS_CONTROLS_BUTTON_HOVER_BUTTON_H_
+
+#include "ui/views/controls/button/label_button.h"
+
+namespace views {
+
+// A button that allows for setting a background color when hovered over.
+class VIEWS_EXPORT HoverButton : public LabelButton {
+ public:
+  // Creates a single line hover button with no icon.
+  HoverButton(ButtonListener* button_listener, const base::string16& text);
+
+  // Creates a single line hover button with an icon.
+  HoverButton(ButtonListener* button_listener,
+              const gfx::ImageSkia& icon,
+              const base::string16& text);
+
+  // Creates a double line hover button with a custom view in place of the icon.
+  // Passing an empty |subtitle| will adjust the |title| text to be vertically
+  // centered to |icon_view|.
+  HoverButton(ButtonListener* button_listener,
+              views::View* icon_view,
+              const base::string16& title,
+              const base::string16& subtitle);
+
+  // Overrides the main label associated with this button. If unset, label()
+  // will be used instead. |label| should be drawn over this button, but it is
+  // not necessary that it be a child view.
+  // void set_title(Label* label) { title_ = label; }
+
+  // Sets a secondary label associated with this button. |label| should be drawn
+  // over this button, but it is not necessary that it be a child view.
+  // void set_subtitle(Label* label) { subtitle_ = label; }
+
+  ~HoverButton() override {}
+
+  void SetSubtitleElideBehavior(gfx::ElideBehavior elide_behavior);
+
+ private:
+  // View:
+  void OnFocus() override;
+  void OnBlur() override;
+  void OnNativeThemeChanged(const ui::NativeTheme* theme) override;
+
+  // Button:
+  void StateChanged(ButtonState old_state) override;
+
+  void UpdateColors();
+
+  Label* title_;
+  Label* subtitle_;
+
+  DISALLOW_COPY_AND_ASSIGN(HoverButton);
+};
+
+}  // namespace views
+
+#endif  // UI_VIEWS_CONTROLS_BUTTON_HOVER_BUTTON_H_
