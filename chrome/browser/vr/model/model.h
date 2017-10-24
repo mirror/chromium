3c70328a09c5fd93c9d293e085d6652fe00d5b04
Ian Vollick <vollick@chromium.org>
2017-10-24 14:41:40 -0400

    [vr] Add WebVR timeout affordance
    With this change, if WebVR content fails to produce frames
    after entering presentation, we eventually show a spinner
    (after 2s) and then present a prompt giving the user the
    option of exiting (after 5s).
    
    Bug:None
    Change-Id: I7a7ddffce46f969e43d91cffee6a0936cf7566d9

--- chrome/browser/vr/model/model.h
+++ chrome/browser/vr/model/model.h
@@ -7,9 +7,26 @@
 
 namespace vr {
 
+// As we wait for WebVR frames, we may pass through the following states.
+enum WebVrTimeoutState {
+  // We are not awaiting a WebVR frame.
+  kWebVrNoTimeoutPending,
+  kWebVrAwaitingFirstFrame,
+  // We are awaiting a WebVR frame, and we will soon exceed the amount of time
+  // that we're willing to wait. In this state, it could be appropriate to show
+  // an affordance to the user to let them know that WebVR is delayed.
+  kWebVrTimeoutImminent,
+  // In this case the time allotted for waiting for the first WebVR frame has
+  // been entirely exceeded.
+  kWebVrTimedOut,
+};
+
 struct Model {
   bool loading = false;
   float load_progress = 0.0f;
+
+  WebVrTimeoutState web_vr_timeout_state = kWebVrNoTimeoutPending;
+  bool started_for_autopresentation = false;
 };
 
 }  // namespace vr
