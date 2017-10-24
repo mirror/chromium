957040bd55ca8d177b24087934e04d983969d9da
Adam Parker <amp@chromium.org>
2017-10-24 10:48:29 -0700

    [Views] Tab capture fullscreen UI: Render page at video resolution.
    This change adds a native view size property to the native view host and resizes
    the local view based on that size if it doesn't match the native view size when
    capturing content while fullscreen in tab.
    
    The visible UI changes are subtle, resolution changes (resizing the window etc)
    will no longer affect the captured render resolution while still providing a
    usable scaled view locally.
    
    The user impact will be a significant quality improvement for the captured
    video as rendered.  Previously the captured content would be based on the
    the content scaled to the current view which was likely much lower than the
    possible render resolution and would often be scaled up again.
    
    After this change the capture output will be based on the original video
    resolution and will not be scaled down for the local view before hand (it will
    be scaled down for the local view if the capture size is larger).
    
    Derived from an unpublished prototype by miu@
    https://codereview.chromium.org/2654413002/
    
    Bug:516188,692790
    Change-Id: Ie681137f2fe684074bb5e719d289c213c0b1e4a7

--- ui/views/controls/native/native_view_host_aura.cc
+++ ui/views/controls/native/native_view_host_aura.cc
@@ -69,7 +69,8 @@ class NativeViewHostAura::ClippingWindowDelegate : public aura::WindowDelegate {
 NativeViewHostAura::NativeViewHostAura(NativeViewHost* host)
     : host_(host),
       clipping_window_delegate_(new ClippingWindowDelegate()),
-      clipping_window_(clipping_window_delegate_.get()) {
+      clipping_window_(clipping_window_delegate_.get()),
+      original_transform_changed_(false) {
   // Set the type so descendant views (including popups) get positioned
   // appropriately.
   clipping_window_.SetType(aura::client::WINDOW_TYPE_CONTROL);
@@ -98,6 +99,8 @@ void NativeViewHostAura::AttachNativeView() {
   host_->native_view()->AddObserver(this);
   host_->native_view()->SetProperty(views::kHostViewKey,
       static_cast<View*>(host_));
+  original_transform_ = host_->native_view()->transform();
+  original_transform_changed_ = false;
   AddClippingWindow();
   InstallMask();
 }
@@ -109,6 +112,8 @@ void NativeViewHostAura::NativeViewDetaching(bool destroyed) {
     host_->native_view()->RemoveObserver(this);
     host_->native_view()->ClearProperty(views::kHostViewKey);
     host_->native_view()->ClearProperty(aura::client::kHostWindowKey);
+    if (original_transform_changed_)
+      host_->native_view()->SetTransform(original_transform_);
     host_->native_view()->Hide();
     if (host_->native_view()->parent())
       Widget::ReparentNativeView(host_->native_view(), NULL);
@@ -164,22 +169,36 @@ void NativeViewHostAura::UninstallClip() {
   clip_rect_.reset();
 }
 
-void NativeViewHostAura::ShowWidget(int x, int y, int w, int h) {
-  int width = w;
-  int height = h;
+void NativeViewHostAura::ShowWidget(int x,
+                                    int y,
+                                    int w,
+                                    int h,
+                                    int render_w,
+                                    int render_h) {
   if (host_->fast_resize()) {
     gfx::Point origin(x, y);
     views::View::ConvertPointFromWidget(host_, &origin);
     InstallClip(origin.x(), origin.y(), w, h);
-    width = host_->native_view()->bounds().width();
-    height = host_->native_view()->bounds().height();
+    render_w = host_->native_view()->bounds().width();
+    render_h = host_->native_view()->bounds().height();
+  } else {
+    gfx::Transform transform = original_transform_;
+    if (w > 0 && h > 0 && render_w > 0 && render_h > 0) {
+      transform.Scale(static_cast<SkMScalar>(w) / render_w,
+                      static_cast<SkMScalar>(h) / render_h);
+    }
+    // Only set the transform when it is actually different.
+    if (transform != host_->native_view()->transform()) {
+      host_->native_view()->SetTransform(transform);
+      original_transform_changed_ = true;
+    }
   }
+
   clipping_window_.SetBounds(clip_rect_ ? *clip_rect_
                                         : gfx::Rect(x, y, w, h));
-
   gfx::Point clip_offset = clipping_window_.bounds().origin();
   host_->native_view()->SetBounds(
-      gfx::Rect(x - clip_offset.x(), y - clip_offset.y(), width, height));
+      gfx::Rect(x - clip_offset.x(), y - clip_offset.y(), render_w, render_h));
   host_->native_view()->Show();
   clipping_window_.Show();
 }
