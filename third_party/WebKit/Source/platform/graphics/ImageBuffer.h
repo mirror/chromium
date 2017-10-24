df4ae0d75ee02eb5376a3b844fd9847f1590e23f
tzik <tzik@chromium.org>
2017-10-24 17:18:41 +0900

    Replace WTF::RefPtr with scoped_refptr in platform/graphics
    scoped_refptr is allowed in Blink now, and WTF::RefPtr is an alias of
    scoped_refptr for migration. This CL inlines the alias to the RefPtr
    users in platform/graphics/ to remove RefPtr.
    
    Bug: 763844
    Change-Id: I777f44a6098e9cbf3834d769517d94ba7402f36d

--- third_party/WebKit/Source/platform/graphics/ImageBuffer.h
+++ third_party/WebKit/Source/platform/graphics/ImageBuffer.h
@@ -155,7 +155,7 @@ class PLATFORM_EXPORT ImageBuffer {
 
   void NotifySurfaceInvalid();
 
-  RefPtr<StaticBitmapImage> NewImageSnapshot(
+  scoped_refptr<StaticBitmapImage> NewImageSnapshot(
       AccelerationHint = kPreferNoAcceleration,
       SnapshotReason = kSnapshotReasonUnknown) const;
 
