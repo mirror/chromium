d573dba9b021e72a9244b6053d5c943826f033b2
Dan Sanders <sandersd@chromium.org>
2017-10-24 16:41:03 -0700

    Move source_buffer_platform.h to //media/base/demuxer_memory_limit.h
    This allows the memory limit used by FFmpegDemuxer to be shared with the
    MP4 format parsers.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I2aea47ebe5954d5f68223116411d5612a647bf8f

---
+++ media/base/demuxer_memory_limit.h
@@ -0,0 +1,21 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_BASE_DEMUXER_MEMORY_LIMIT_H_
+#define MEDIA_BASE_DEMUXER_MEMORY_LIMIT_H_
+
+#include <stddef.h>
+
+#include "media/base/media_export.h"
+
+namespace media {
+
+// The maximum amount of data (in bytes) a demuxer can keep in memory, for a
+// particular track type.
+extern const size_t kDemuxerAudioMemoryLimit;
+extern const size_t kDemuxerVideoMemoryLimit;
+
+}  // namespace media
+
+#endif  // MEDIA_BASE_DEMUXER_MEMORY_LIMIT_H_
