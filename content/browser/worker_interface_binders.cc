256dbb1ff1be142d8a4c3b7d3633d88433ac0137
Sam McNally <sammc@chromium.org>
2017-10-24 13:26:57 +1100

    Add a shared BinderRegistry for workers.
    Add content::BindWorkerInterface(), a function to bind an interface
    requested by a web worker. BindWorkerInterface() forwards to a shared
    BinderRegistryWithArgs<RenderProcessHost*, const url::Origin&>.
    
    Bug: 734210
    Change-Id: I7add53ac091f5c5bfcdeb67c935fb138e0b99173

---
+++ content/browser/worker_interface_binders.cc
@@ -0,0 +1,62 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/worker_interface_binders.h"
+
+#include <utility>
+
+#include "content/public/browser/browser_thread.h"
+#include "services/service_manager/public/cpp/binder_registry.h"
+#include "url/origin.h"
+
+namespace content {
+namespace {
+
+// A container of parametized and unparameterized BinderRegistries for
+// content-layer interfaces exposed to web workers.
+class WorkerInterfaceBinders {
+ public:
+  WorkerInterfaceBinders() {
+    // TODO(sammc): Populate |parameterized_binder_registry_| and
+    // |binder_registry_|.
+  }
+
+  // Bind an interface request |interface_pipe| for |interface_name| received
+  // from a web worker with origin |origin| hosted in the renderer |host|.
+  void BindInterface(const std::string& interface_name,
+                     mojo::ScopedMessagePipeHandle interface_pipe,
+                     RenderProcessHost* host,
+                     const url::Origin& origin) {
+    if (parameterized_binder_registry_.TryBindInterface(
+            interface_name, &interface_pipe, host, origin)) {
+      return;
+    }
+
+    if (binder_registry_.TryBindInterface(interface_name, &interface_pipe))
+      return;
+
+    // TODO(sammc): Forward unhandled requests to the embedder.
+  }
+
+ private:
+  service_manager::BinderRegistryWithArgs<RenderProcessHost*,
+                                          const url::Origin&>
+      parameterized_binder_registry_;
+  service_manager::BinderRegistry binder_registry_;
+};
+
+}  // namespace
+
+void BindWorkerInterface(const std::string& interface_name,
+                         mojo::ScopedMessagePipeHandle interface_pipe,
+                         RenderProcessHost* host,
+                         const url::Origin& origin) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+
+  CR_DEFINE_STATIC_LOCAL(WorkerInterfaceBinders, binders, ());
+  binders.BindInterface(interface_name, std::move(interface_pipe), host,
+                        origin);
+}
+
+}  // namespace content
