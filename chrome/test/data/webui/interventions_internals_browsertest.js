ffcec3e180942a3ffa142b286d7605a85ffc31b0
Thanh Le <thanhdle@chromium.org>
2017-10-24 15:24:48 -0700

    Reverse the logging order in chrome://interventions-internals.
    Add a remove logs button the clear out all log messages.
    
    Bug: 778027
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I700ec60d5a47cfa839e43abab376c6db038e2847

--- chrome/test/data/webui/interventions_internals_browsertest.js
+++ chrome/test/data/webui/interventions_internals_browsertest.js
@@ -148,7 +148,7 @@ TEST_F('InterventionsInternalsUITest', 'LogNewMessage', function() {
 
     logs.forEach((log, index) => {
       let expectedTime = new Date(log.time).toISOString();
-      let row = rows[index];
+      let row = rows[logs.length - index - 1];  // Expecting reversed order.
 
       expectEquals(expectedTime, row.querySelector('.log-time').textContent);
       expectEquals(log.type, row.querySelector('.log-type').textContent);
