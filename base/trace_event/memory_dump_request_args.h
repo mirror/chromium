3d51ddb1708cca28586e2d3b31bf7e597664fc1f
Lalit Maganti <lalitm@chromium.org>
2017-10-24 14:52:55 +0100

    memory-infra: move SUMMARY_ONLY from type to level of detail
    As with VM_REGIONS_ONLY, SUMMARY_ONLY never belonged in the type enum as
    its meaning refers how much information should be returned to the caller
    of the memory instrumentation service.
    
    Move the enum member and change all relevant references. Eventually, we
    plan to remove this member all together as its purpose will be subsumed
    by one of the other levels of detail but for now this is a good stepping
    stone.
    
    Bug: 775041
    Bug: 777820
    Change-Id: I43504be0cd2f864b38dfd607b7d08ada156ed7ca

--- base/trace_event/memory_dump_request_args.h
+++ base/trace_event/memory_dump_request_args.h
@@ -31,8 +31,7 @@ enum class MemoryDumpType {
   PERIODIC_INTERVAL,     // Dumping memory at periodic intervals.
   EXPLICITLY_TRIGGERED,  // Non maskable dump request.
   PEAK_MEMORY_USAGE,     // Dumping memory at detected peak total memory usage.
-  SUMMARY_ONLY,          // Calculate just the summary & don't add to the trace.
-  LAST = SUMMARY_ONLY
+  LAST = PEAK_MEMORY_USAGE
 };
 
 // Tells the MemoryDumpProvider(s) how much detailed their dumps should be.
@@ -41,10 +40,13 @@ enum class MemoryDumpType {
 enum class MemoryDumpLevelOfDetail : uint32_t {
   FIRST,
 
+  // Calculate just the summary.
+  SUMMARY_ONLY = FIRST,
+
   // For background tracing mode. The dump time is quick, and typically just the
   // totals are expected. Suballocations need not be specified. Dump name must
   // contain only pre-defined strings and string arguments cannot be added.
-  BACKGROUND = FIRST,
+  BACKGROUND,
 
   // For the levels below, MemoryDumpProvider instances must guarantee that the
   // total size reported in the root node is consistent. Only the granularity of
