c2827af691b0d715b7021fda528f9e78d773565f
Koji Ishii <kojii@chromium.org>
2017-10-24 16:03:04 +0900

    invalidate
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Id0d11e1138b876a77c160a54379deecfc36ad0aa

--- third_party/WebKit/Source/core/layout/ng/inline/ng_inline_node.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_inline_node.cc
@@ -382,12 +382,27 @@ void NGInlineNode::InvalidatePrepareLayout() {
   DCHECK(!IsPrepareLayoutFinished());
 }
 
-void NGInlineNode::PrepareLayout() {
+void NGInlineNode::PrepareLayoutIfNeeded() {
+  LayoutBlockFlow* block_flow = GetLayoutBlockFlow();
+  if (IsPrepareLayoutFinished()) {
+    // LayoutObject::NeedsLayout() includes NeedsPositionedMovementLayout(),
+    // which doesn't require PrepareLayout().
+    if (!(block_flow->SelfNeedsLayout() ||
+          block_flow->NormalChildNeedsLayout() ||
+          block_flow->PosChildNeedsLayout() ||
+          block_flow->NeedsSimplifiedNormalFlowLayout()))
+      return;
+
+    block_flow->ResetNGInlineNodeData();
+  }
+
   // Scan list of siblings collecting all in-flow non-atomic inlines. A single
   // NGInlineNode represent a collection of adjacent non-atomic inlines.
   CollectInlines();
   SegmentText();
   ShapeText();
+
+  block_flow->ClearNeedsLayout();
 }
 
 const NGOffsetMappingResult& NGInlineNode::ComputeOffsetMappingIfNeeded() {
@@ -543,6 +558,8 @@ void NGInlineNode::ShapeTextForFirstLineIfNeeded() {
 scoped_refptr<NGLayoutResult> NGInlineNode::Layout(
     const NGConstraintSpace& constraint_space,
     NGBreakToken* break_token) {
+  PrepareLayoutIfNeeded();
+
   NGInlineLayoutAlgorithm algorithm(*this, constraint_space,
                                     ToNGInlineBreakToken(break_token));
   return algorithm.Layout();
@@ -589,6 +606,8 @@ static LayoutUnit ComputeContentSize(NGInlineNode node,
 }
 
 MinMaxSize NGInlineNode::ComputeMinMaxSize() {
+  PrepareLayoutIfNeeded();
+
   // Run line breaking with 0 and indefinite available width.
 
   // TODO(kojii): There are several ways to make this more efficient and faster
@@ -613,6 +632,7 @@ MinMaxSize NGInlineNode::ComputeMinMaxSize() {
 }
 
 NGLayoutInputNode NGInlineNode::NextSibling() {
+  PrepareLayoutIfNeeded();
   return NGBlockNode(Data().next_sibling_);
 }
 
