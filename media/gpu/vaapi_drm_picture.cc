77a7bc8988889fd3290dc8b31cdcc901259d7687
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-10-24 12:43:37 -0400

    Move VAAPI static methods to impl classes
    This CL moves the factory method VaapiPicture::Create to the two impl 
    classes vaapi_{drm,tfp}_picture, instead of having a single one with 
    #ifdefs.
    
    Also in the process moved VaapiPicture::GetGLTextureTarget() which
    was in a similar situation, made a member const, inlined AllowOverlay()  
    and changed LOG with DLOG()s in a file because:
     "prefer DVLOG(1) to other logging methods" [1]),.
    
    [1] https://chromium.googlesource.com/chromium/src/+/master/styleguide/c++/c++.md#logging
    
    Bug:778093
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I632acf8193ce052781b54ebed5d4957028635780

--- media/gpu/vaapi_drm_picture.cc
+++ media/gpu/vaapi_drm_picture.cc
@@ -138,4 +138,25 @@ bool VaapiDrmPicture::AllowOverlay() const {
   return true;
 }
 
+// static
+linked_ptr<VaapiPicture> VaapiPicture::CreatePicture(
+    const scoped_refptr<VaapiWrapper>& vaapi_wrapper,
+    const MakeGLContextCurrentCallback& make_context_current_cb,
+    const BindGLImageCallback& bind_image_cb,
+    int32_t picture_buffer_id,
+    const gfx::Size& size,
+    uint32_t texture_id,
+    uint32_t client_texture_id) {
+  linked_ptr<VaapiPicture> picture;
+  picture.reset(new VaapiDrmPicture(vaapi_wrapper, make_context_current_cb,
+                                    bind_image_cb, picture_buffer_id, size,
+                                    texture_id, client_texture_id));
+  return picture;
+}
+
+// static
+uint32_t VaapiPicture::GetGLTextureTarget() {
+  return GL_TEXTURE_EXTERNAL_OES;
+}
+
 }  // namespace media
