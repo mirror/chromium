99e8d0708da3a92fd6b618dbf67fdd987ed79642
Koji Ishii <kojii@chromium.org>
2017-10-24 15:19:18 +0900

    invalidate
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Id0d11e1138b876a77c160a54379deecfc36ad0aa

--- third_party/WebKit/Source/core/layout/ng/inline/ng_line_breaker_test.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_line_breaker_test.cc
@@ -29,8 +29,7 @@ class NGLineBreakerTest : public NGBaseLayoutAlgorithmTest {
                                          LayoutUnit available_width) {
     DCHECK(node);
 
-    if (!node.IsPrepareLayoutFinished())
-      node.PrepareLayout();
+    node.PrepareLayoutIfNeeded();
 
     scoped_refptr<NGConstraintSpace> space =
         NGConstraintSpaceBuilder(
