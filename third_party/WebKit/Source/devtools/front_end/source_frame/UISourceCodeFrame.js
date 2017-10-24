03d8a64f0b6ffea228028696c2b1aa68510fe1de
Nathan Bruer <allada@chromium.org>
2017-10-23 18:02:22 -0700

    [Devtools] Fixed bug where iframes were not editable with overrides
    R=pfeldman
    Bug=None
    
    Change-Id: I3b4f3b85e5629a3fdd8110c337494210fbf5f424

--- third_party/WebKit/Source/devtools/front_end/source_frame/UISourceCodeFrame.js
+++ third_party/WebKit/Source/devtools/front_end/source_frame/UISourceCodeFrame.js
@@ -160,8 +160,7 @@ SourceFrame.UISourceCodeFrame = class extends SourceFrame.SourceFrame {
     var networkPersistenceProject = Persistence.networkPersistenceManager.activeProject();
     if (this._uiSourceCode.project().type() === Workspace.projectTypes.Network && networkPersistenceProject) {
       var projectDomain = Persistence.networkPersistenceManager.domainForProject(networkPersistenceProject);
-      var urlDomainPath = this._uiSourceCode.url().replace(/^https?:\/\//, '');
-      if (projectDomain && urlDomainPath.startsWith(projectDomain + '/'))
+      if (projectDomain)
         return true;
     }
     return this._uiSourceCode.contentType() !== Common.resourceTypes.Document;
