b729f23389cfecea22e481e56ee144ba0523ac61
Mark Pilgrim <pilgrim@chromium.org>
2017-10-24 14:49:07 -0400

    Migrate BitmapFetcher to SimpleURLLoader
    BUG=773295
    
    Change-Id: Ifbe46b3976024ae5f6ba47a7d8324580106f34aa

--- chrome/browser/extensions/webstore_install_helper.cc
+++ chrome/browser/extensions/webstore_install_helper.cc
@@ -29,16 +29,15 @@ WebstoreInstallHelper::WebstoreInstallHelper(
     const std::string& id,
     const std::string& manifest,
     const GURL& icon_url,
-    net::URLRequestContextGetter* context_getter)
+    content::mojom::URLLoaderFactory* loader_factory)
     : delegate_(delegate),
       id_(id),
       manifest_(manifest),
       icon_url_(icon_url),
-      context_getter_(context_getter),
+      loader_factory_(loader_factory),
       icon_decode_complete_(false),
       manifest_parse_complete_(false),
-      parse_error_(Delegate::UNKNOWN_ERROR) {
-}
+      parse_error_(Delegate::UNKNOWN_ERROR) {}
 
 WebstoreInstallHelper::~WebstoreInstallHelper() {}
 
@@ -87,8 +86,8 @@ void WebstoreInstallHelper::Start() {
     icon_fetcher_.reset(
         new chrome::BitmapFetcher(icon_url_, this, traffic_annotation));
     icon_fetcher_->Init(
-        context_getter_, std::string(),
-        net::URLRequest::CLEAR_REFERRER_ON_TRANSITION_FROM_SECURE_TO_INSECURE,
+        loader_factory_, std::string(),
+        blink::kWebReferrerPolicyNoReferrerWhenDowngradeOriginWhenCrossOrigin,
         net::LOAD_DO_NOT_SAVE_COOKIES | net::LOAD_DO_NOT_SEND_COOKIES);
     icon_fetcher_->Start();
   }
