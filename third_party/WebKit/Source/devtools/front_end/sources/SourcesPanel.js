d02fab21e23801f6f10d32027e7bf28a12fe3bde
Joel Einbinder <einbinder@chromium.org>
2017-10-23 18:50:07 -0700

    DevTools: Diff from estimated original network content
    BUG=720160
    
    Change-Id: I234f1461bf0418844dd42f7556567bdb2728ef55

--- third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
+++ third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
@@ -699,13 +699,6 @@ Sources.SourcesPanel = class extends UI.Panel {
   }
 
   /**
-   * @param {!Workspace.UISourceCode} uiSourceCode
-   */
-  _showLocalHistory(uiSourceCode) {
-    Sources.RevisionHistoryView.showHistory(uiSourceCode);
-  }
-
-  /**
    * @override
    * @param {!Event} event
    * @param {!UI.ContextMenu} contextMenu
