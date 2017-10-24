95d1506bea61174a2ca701502bbdfae5e89a8fa1
Yoichi Osato <yoichio@chromium.org>
2017-10-24 11:25:39 +0900

    async assert_selection
    Bug: 679977
    Change-Id: Ib0c5ddad170e4ca107b30f481d85c8888873fefa

--- third_party/WebKit/LayoutTests/editing/assert_selection.js
+++ third_party/WebKit/LayoutTests/editing/assert_selection.js
@@ -66,6 +66,8 @@ const kIFrameBorderSize = 2;
 
 /** @const @type {string} */
 const kTextArea = 'TEXTAREA';
+/** @const @type {string} */
+const kAsync = 'async';
 
 class Traversal {
   /**
@@ -900,7 +902,7 @@ function commonPrefixOf(str1, str2) {
  * @return {!Sample}
  */
 function assertSelection(
-    passedInputText, tester, passedExpectedText, opt_options = {}) {
+    passedInputText, tester, passedExpectedText, opt_options = {}, resolve) {
   const kDescription = 'description';
   const kDumpAs = 'dumpAs';
   const kRemoveSampleIfSucceeded = 'removeSampleIfSucceeded';
@@ -937,15 +939,8 @@ function assertSelection(
 
   checkExpectedText(expectedText);
   const sample = new Sample(inputText);
-  if (typeof(tester) === 'function') {
-    tester.call(window, sample.selection);
-  } else if (typeof(tester) === 'string') {
-    const strings = tester.split(/ (.+)/);
-    sample.document.execCommand(strings[0], false, strings[1]);
-  } else {
-    throw new Error(`Invalid tester: ${tester}`);
-  }
 
+  let confirm = () => {
   /** @type {!Traversal} */
   const traversal = (() => {
     switch (dumpAs) {
@@ -975,6 +970,20 @@ function assertSelection(
     `\t expected ${expectedText},\n` +
     `\t but got  ${actualText},\n` +
     `\t sameupto ${commonPrefixOf(expectedText, actualText)}`);
+  };
+
+  if (options[kAsync]) {
+    return tester.call(window, sample.selection).then(confirm).then(resolve);
+  }
+  if (typeof(tester) === 'function') {
+    tester.call(window, sample.selection);
+  } else if (typeof(tester) === 'string') {
+    const strings = tester.split(/ (.+)/);
+    sample.document.execCommand(strings[0], false, strings[1]);
+  } else {
+    throw new Error(`Invalid tester: ${tester}`);
+  }
+  return confirm();
 }
 
 /**
@@ -989,8 +998,15 @@ function selectionTest(inputText, tester, expectedText, opt_options,
   const description = typeof(opt_options) === 'string' ? opt_options
                                                        : opt_description;
   const options = typeof(opt_options) === 'string' ? undefined : opt_options;
+  if (options && options[kAsync]) {
+    promise_test(() => new Promise(resolve => {
+      assertSelection(inputText, tester, expectedText, options, resolve) }),
+      description);
+    return;
+  }
   test(() => assertSelection(inputText, tester, expectedText, options),
        description);
+
 }
 
 // Export symbols
