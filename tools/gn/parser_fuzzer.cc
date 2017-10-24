f84cb27e3077baffb60c2a958f43abc0c3314220
Penny MacNeil <pennymac@chromium.org>
2017-10-24 13:21:08 -0700

    [GN fuzzer] Stack overflow fix.
    Fuzzathon 2017.
    
    Bug:648076,749793,773426,768111,754972,734401,734200
    Change-Id: Ic608c5a374252809443a879ad4e2ddf8f6184697

--- tools/gn/parser_fuzzer.cc
+++ tools/gn/parser_fuzzer.cc
@@ -9,10 +9,37 @@
 #include "tools/gn/source_file.h"
 #include "tools/gn/tokenizer.h"
 
+namespace {
+
+enum { kMaxContentDepth = 100 };
+
+// Some auto generated input is too unreasonable for fuzzing GN.
+// We see stack overflow when the parser hits a really deeply nested input.
+bool SanityCheckContent(std::string& content) {
+  int depth = 0;
+  for (auto&& character : content) {
+    if (character == '{')
+      ++depth;
+    else if (character == '}')
+      --depth;
+
+    if (depth >= kMaxContentDepth)
+      return false;
+  }
+
+  return true;
+}
+
+}  // namespace
+
 extern "C" int LLVMFuzzerTestOneInput(const unsigned char* data, size_t size) {
+  std::string content(reinterpret_cast<const char*>(data), size);
+  if (!SanityCheckContent(content))
+    return 0;
+
   SourceFile source;
   InputFile input(source);
-  input.SetContents(std::string(reinterpret_cast<const char*>(data), size));
+  input.SetContents(content);
 
   Err err;
   std::vector<Token> tokens = Tokenizer::Tokenize(&input, &err);
