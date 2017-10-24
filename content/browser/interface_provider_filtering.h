44d2344b02efa8f96f1d1e417d1580243f6dbfac
Sam McNally <sammc@chromium.org>
2017-10-24 11:10:49 +1100

    Add InterfaceProviderSpecs for web workers.
    For each worker host, when providing the worker with an
    InterfaceProvider, request the service_manager::Connector filter the
    interfaces. Add stub InterfaceProviderSpecs for each worker type.
    
    Add a helper function FilterRendererExposedInterfaces() to avoid
    copy-pasting the code looking-up the Connector for a RenderProcessHost
    ID and use it to filter interface requests to worker and frame hosts.
    
    Bug: 734210
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I3eb803ffeafaef6848d7a9d8fdb9cbccf701cc2e

---
+++ content/browser/interface_provider_filtering.h
@@ -0,0 +1,30 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_INTERFACE_PROVIDER_FILTERING_H_
+#define CONTENT_BROWSER_INTERFACE_PROVIDER_FILTERING_H_
+
+#include "base/strings/string_piece.h"
+#include "services/service_manager/public/interfaces/interface_provider.mojom.h"
+
+namespace content {
+
+// Requests that service manager filter requests to an InterfaceProvider
+// received from the renderer process with ID |process_id|, running an
+// execution context of the type corresponding to |spec|. |request| is the
+// InterfaceProviderRequest from the renderer; the return value is an equivalent
+// InterfaceProviderRequest after filtering by service manager.
+//
+// If |process_id| does not refer to a renderer process or if that renderer's
+// BrowserContext does not have a Connector, the connection is broken instead;
+// that is, |request| and the InterfacePtr corresponding to the returned request
+// are both closed.
+service_manager::mojom::InterfaceProviderRequest
+FilterRendererExposedInterfaces(
+    const char* spec,
+    int process_id,
+    service_manager::mojom::InterfaceProviderRequest request);
+
+}  // namespace content
+#endif  // CONTENT_BROWSER_INTERFACE_PROVIDER_FILTERING_H_
