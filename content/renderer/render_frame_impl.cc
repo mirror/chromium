1a42a6c16ab81548fa920ed57cbb23952e0af602
Lucas Garron <lgarron@chromium.org>
2017-10-24 13:59:07 -0700

    Committed Interstitials: expand NavigationRequest::OnRequestFailed() path to allow optional error page html (2/2)
    Plumb failed request error page data URL from NavigationThrottle to RenderFrameImpl.
    
    Bug: 751949
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I43d7329a8dc6e479fb9c281f2efbe832f5341eb0

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -2560,7 +2560,8 @@ void RenderFrameImpl::LoadNavigationErrorPage(
     const WebURLRequest& failed_request,
     const WebURLError& error,
     bool replace,
-    HistoryEntry* entry) {
+    HistoryEntry* entry,
+    const base::Optional<std::string>& error_page_content) {
   blink::WebFrameLoadType frame_load_type =
       entry ? blink::WebFrameLoadType::kBackForward
             : blink::WebFrameLoadType::kStandard;
@@ -2585,8 +2586,12 @@ void RenderFrameImpl::LoadNavigationErrorPage(
   }
 
   std::string error_html;
-  GetContentClient()->renderer()->GetNavigationErrorStrings(
-      this, failed_request, error, &error_html, nullptr);
+  if (error_page_content.has_value()) {
+    error_html = error_page_content.value();
+  } else {
+    GetContentClient()->renderer()->GetNavigationErrorStrings(
+        this, failed_request, error, &error_html, nullptr);
+  }
   LoadNavigationErrorPageInternal(error_html, GURL(kUnreachableWebDataURL),
                                   error.unreachable_url, replace,
                                   frame_load_type, history_item);
@@ -3657,7 +3662,8 @@ void RenderFrameImpl::DidReceiveServerRedirectForProvisionalLoad() {
 
 void RenderFrameImpl::DidFailProvisionalLoad(
     const blink::WebURLError& error,
-    blink::WebHistoryCommitType commit_type) {
+    blink::WebHistoryCommitType commit_type,
+    const base::Optional<std::string>& error_page_content) {
   TRACE_EVENT1("navigation,benchmark,rail",
                "RenderFrameImpl::didFailProvisionalLoad", "id", routing_id_);
   // Note: It is important this notification occur before DidStopLoading so the
@@ -3704,7 +3710,8 @@ void RenderFrameImpl::DidFailProvisionalLoad(
   }
 
   // Load an error page.
-  LoadNavigationErrorPage(failed_request, error, replace, nullptr);
+  LoadNavigationErrorPage(failed_request, error, replace, nullptr,
+                          error_page_content);
 }
 
 void RenderFrameImpl::DidCommitProvisionalLoad(
@@ -5358,7 +5365,8 @@ void RenderFrameImpl::OnFailedNavigation(
     const CommonNavigationParams& common_params,
     const RequestNavigationParams& request_params,
     bool has_stale_copy_in_cache,
-    int error_code) {
+    int error_code,
+    base::Optional<std::string> error_page_content) {
   DCHECK(IsBrowserSideNavigationEnabled());
   bool is_reload =
       FrameMsg_Navigate_Type::IsReload(common_params.navigation_type);
@@ -5438,8 +5446,10 @@ void RenderFrameImpl::OnFailedNavigation(
   // notification.
   bool had_provisional_document_loader = frame_->GetProvisionalDocumentLoader();
   if (request_params.nav_entry_id == 0) {
-    DidFailProvisionalLoad(error, replace ? blink::kWebHistoryInertCommit
-                                          : blink::kWebStandardCommit);
+    DidFailProvisionalLoad(
+        error,
+        replace ? blink::kWebHistoryInertCommit : blink::kWebStandardCommit,
+        error_page_content);
     if (!weak_this)
       return;
   }
@@ -5448,8 +5458,8 @@ void RenderFrameImpl::OnFailedNavigation(
   // GetProvisionalDocumentLoader(), LoadNavigationErrorPage wasn't called, so
   // do it now.
   if (request_params.nav_entry_id != 0 || !had_provisional_document_loader) {
-    LoadNavigationErrorPage(failed_request, error, replace,
-                            history_entry.get());
+    LoadNavigationErrorPage(failed_request, error, replace, history_entry.get(),
+                            error_page_content);
     if (!weak_this)
       return;
   }
