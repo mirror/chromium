b15be29c29112bf0b6a3d4777231f85650720119
Arthur Hemery <ahemery@chromium.org>
2017-10-24 17:04:48 +0200

    Navigation: Moving has_user_gesture to CommonNavigationParams
    has_user_gesture is now only in CommonNavigationParams instead of being
    part of both BeginNavigationParams and RequestNavigationParams.
    
    This also solved a problem where has_user_gesture was not set in the
    same way in begin and requests params on Android leading to a DCHECK
    failure under specific circumstances.
    
    Bug:773755
    Change-Id: I4d22176f5376000e16a1b3bc491f969308290e83

--- content/public/test/render_view_test.cc
+++ content/public/test/render_view_test.cc
@@ -528,7 +528,8 @@ void RenderViewTest::Reload(const GURL& url) {
       FrameMsg_UILoadMetricsReportType::NO_REPORT, GURL(), GURL(),
       PREVIEWS_UNSPECIFIED, base::TimeTicks::Now(), "GET", nullptr,
       base::Optional<SourceLocation>(),
-      CSPDisposition::CHECK /* should_check_main_world_csp */);
+      CSPDisposition::CHECK /* should_check_main_world_csp */,
+      false /* has_user_gesture */);
   RenderViewImpl* impl = static_cast<RenderViewImpl*>(view_);
   TestRenderFrame* frame =
       static_cast<TestRenderFrame*>(impl->GetMainRenderFrame());
@@ -668,7 +669,8 @@ void RenderViewTest::GoToOffset(int offset,
       base::TimeTicks(), FrameMsg_UILoadMetricsReportType::NO_REPORT, GURL(),
       GURL(), PREVIEWS_UNSPECIFIED, base::TimeTicks::Now(), "GET", nullptr,
       base::Optional<SourceLocation>(),
-      CSPDisposition::CHECK /* should_check_main_world_csp */);
+      CSPDisposition::CHECK /* should_check_main_world_csp */,
+      false /* has_user_gesture */);
   RequestNavigationParams request_params;
   request_params.page_state = state;
   request_params.nav_entry_id = pending_offset + 1;
