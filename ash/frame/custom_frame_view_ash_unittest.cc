6fc1a796b97fc2df94092c1f367cac14b3a2e641
Mitsuru Oshima <oshima@chromium.org>
2017-10-24 14:33:26 -0700

    Add back button to frame.
    This will be used in two scenarios:
    1) back button for v1 apps. This is currently behind --ash-enable-v1-back-button.
     This currently does not take the history into account and will be addressed in a separate CL.
    2) back button for ARC++ apps. This is not hooked up yet and will be handled in a followup CL.
    
    The icon data is just placeholder and will be updated with correct assets.
    
    BUG=749713, b/33693796
    TEST=covered by unit tests.
    
    Change-Id: I4925ade99337004a4bf3a6c6da53f5109ed68360

--- ash/frame/custom_frame_view_ash_unittest.cc
+++ ash/frame/custom_frame_view_ash_unittest.cc
@@ -313,4 +313,52 @@ TEST_F(CustomFrameViewAshTest, OpeningAppsInTabletMode) {
       delegate->GetCustomFrameViewTopBorderHeight());
 }
 
+TEST_F(CustomFrameViewAshTest, BackButton) {
+  CustomFrameTestWidgetDelegate* delegate = new CustomFrameTestWidgetDelegate;
+  views::Widget* widget = new views::Widget();
+  views::Widget::InitParams params;
+  params.context = CurrentContext();
+  params.delegate = delegate;
+  widget->Init(params);
+  widget->Show();
+
+  CustomFrameViewAsh* custom_frame_view = delegate->custom_frame_view();
+  HeaderView* header_view =
+      static_cast<HeaderView*>(custom_frame_view->GetHeaderView());
+  EXPECT_FALSE(header_view->back_button());
+  custom_frame_view->SetBackButtonStatus(/*show=*/true, /*enabled=*/false);
+  EXPECT_TRUE(header_view->back_button());
+  EXPECT_FALSE(header_view->back_button()->enabled());
+  custom_frame_view->SetBackButtonStatus(/*show=*/true, /*enabled=*/true);
+  EXPECT_TRUE(header_view->back_button());
+  EXPECT_TRUE(header_view->back_button()->enabled());
+  custom_frame_view->SetBackButtonStatus(/*show=*/false, /*enabled=*/true);
+  EXPECT_FALSE(header_view->back_button());
+}
+
+TEST_F(CustomFrameViewAshTest, FrameVisibility) {
+  CustomFrameTestWidgetDelegate* delegate = new CustomFrameTestWidgetDelegate;
+  views::Widget* widget = new views::Widget();
+  views::Widget::InitParams params;
+  params.bounds = gfx::Rect(10, 10, 200, 100);
+  params.context = CurrentContext();
+  params.delegate = delegate;
+  widget->Init(params);
+  widget->Show();
+
+  CustomFrameViewAsh* custom_frame_view = delegate->custom_frame_view();
+  EXPECT_EQ(gfx::Size(200, 67), widget->client_view()->GetLocalBounds().size());
+
+  custom_frame_view->SetVisible(false);
+  widget->GetRootView()->Layout();
+  EXPECT_EQ(gfx::Size(200, 100),
+            widget->client_view()->GetLocalBounds().size());
+  EXPECT_FALSE(widget->non_client_view()->frame_view()->visible());
+
+  custom_frame_view->SetVisible(true);
+  widget->GetRootView()->Layout();
+  EXPECT_EQ(gfx::Size(200, 67), widget->client_view()->GetLocalBounds().size());
+  EXPECT_TRUE(widget->non_client_view()->frame_view()->visible());
+}
+
 }  // namespace ash
