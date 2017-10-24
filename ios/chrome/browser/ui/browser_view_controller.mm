192bf13c04778d45e47ffd69b50e67a77786c6a3
edchin <edchin@chromium.org>
2017-10-24 09:29:37 -0400

    [ios] Remove -topPresentedViewController from -showSettings
    MainController's -topPresentedViewController is deprecated. It
    performs a walk from the bottom of the VC hierarchy to the top
    most VC. But this walk is not necessary when the presenting VC
    is known.
        
    This CL plumbs a baseViewController (either tab switcher or
    BVC) to the tools menu so that the presenting VC is known
    when calling -showSettings.
        
    This CL also plumbs a baseViewController into the 
    KeyCommandsProvider.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I54bfee925c8b760e3b596d09cf3466506f46e464

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -3831,6 +3831,7 @@ bubblePresenterForFeature:(const base::Feature&)feature
   }
   return [self.keyCommandsProvider
       keyCommandsForConsumer:self
+          baseViewController:self
                   dispatcher:self.dispatcher
                  editingText:![self isFirstResponder]];
 }
@@ -4160,7 +4161,8 @@ bubblePresenterForFeature:(const base::Feature&)feature
   [self updateFindBar:NO shouldFocus:NO];
 
   ToolsMenuConfiguration* configuration =
-      [[ToolsMenuConfiguration alloc] initWithDisplayView:[self view]];
+      [[ToolsMenuConfiguration alloc] initWithDisplayView:[self view]
+                                       baseViewController:self];
   configuration.requestStartTime =
       showToolsMenuPopupRequestDate.timeIntervalSinceReferenceDate;
   if ([_model count] == 0)
