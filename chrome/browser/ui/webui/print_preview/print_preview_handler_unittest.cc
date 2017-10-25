85b8d68a9c00b0241537bdcbaf3a91e497897a03
rbpotter <rbpotter@chromium.org>
2017-10-25 15:14:36 -0700

    Use the PDF in PrintPreviewDataStore for local printers
    To improve consistency with other printer types and avoid a second
    round of rendering, use the SkPDF stored in the print preview data
    store for local printers.
    
    Bug:  None
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I7d3423efb8b473ef862491b203bb18eb172e6458

--- chrome/browser/ui/webui/print_preview/print_preview_handler_unittest.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_handler_unittest.cc
@@ -77,7 +77,7 @@ base::Value GetPrintTicket(printing::PrinterType type, bool cloud) {
   bool is_privet_printer = !cloud && type == printing::kPrivetPrinter;
   bool is_extension_printer = !cloud && type == printing::kExtensionPrinter;
   base::Value::DictStorage ticket;
-
+  ticket["pageRange"] = std::make_unique<base::Value>();  // entire document
   // Letter
   base::Value::DictStorage media_size;
   media_size["is_default"] = std::make_unique<base::Value>(true);
@@ -507,8 +507,8 @@ TEST_F(PrintPreviewHandlerTest, Print) {
     handler()->HandlePrint(&args);
 
     // Verify correct PrinterHandler was called or that no handler was requested
-    // for local and cloud printers.
-    if (cloud || type == printing::kLocalPrinter) {
+    // for cloud printers.
+    if (cloud) {
       EXPECT_TRUE(handler()->NotCalled());
     } else {
       EXPECT_TRUE(handler()->CalledOnlyForType(type));
