4be79b00fec6bc15f9ad9bd25a6cc1eee01b5bff
Sophie Chang <sophiechang@chromium.org>
2017-10-25 15:17:28 -0700

    Add optimization_guide component
    The component will contain code for consuming and using server hints for
    what optimizations to perform on a page load to improve data savings
    and/or performance.
    
    This is in //components rather than //chrome, as it will be consumed by
    //components/previews.
    
    Bug:777982
    Change-Id: I3af83401fd63008c799bafe98128511db5256d61

---
+++ components/optimization_guide/core/optimization_hints_reader.cc
@@ -0,0 +1,34 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/optimization_guide/core/optimization_hints_reader.h"
+
+#include "base/files/file.h"
+
+namespace optimization_guide {
+
+UnindexedHintsInfo::UnindexedHintsInfo() = default;
+UnindexedHintsInfo::~UnindexedHintsInfo() = default;
+
+OptimizationHintsReader::OptimizationHintsReader() {}
+
+OptimizationHintsReader::~OptimizationHintsReader() {}
+
+bool OptimizationHintsReader::ReadHints(
+    const UnindexedHintsInfo& unindexed_hints_info) {
+  if (!unindexed_hints_info.hints_version.IsValid()) {
+    return false;
+  }
+  base::File unindexed_hints_file(
+      unindexed_hints_info.hints_path,
+      base::File::FLAG_OPEN | base::File::FLAG_READ);
+  if (!unindexed_hints_file.IsValid()) {
+    VLOG(1) << "Failed opening unindexed hints.";
+    return false;
+  }
+  // TODO(crbug.com/777892): Actually index hints.
+  return true;
+}
+
+}  // namespace optimization_guide
