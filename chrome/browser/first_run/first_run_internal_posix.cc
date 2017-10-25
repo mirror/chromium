0dc6d8657b2b233bb9f79efb0392243fd9080bd4
Trent Apted <tapted@chromium.org>
2017-10-25 19:03:52 +1100

    Add --force-first-run-dialog, regression tests for r511312.
    Chrome has a modal first-run dialog displayed during browser startup
    on Linux and Mac, before the main MessageLoop begins. It is supressed
    under many scenarios, and completely disabled in Chromium builds,
    making it very difficult to test (even manually).
    
    Before r511312 Chrome would upload a crash dump if a SIGTERM or similar
    was received while the dialog was showing on Mac.
    
    This CL adds --force-first-run-dialog which works in official and non-
    official builds for testing the dialog manually. Adds a regression
    test for r511312 to test signal handling while this dialog is showing.
    This also gives some bot coverage of the dialog code.
    
    Consolidates repeated and redundant logic leading up to the dialog
    invocation, allowing Linux/Mac to share the --force-first-run-dialog
    logic as well.
    
    Bug: 777660
    Change-Id: I523966003eda01d43f15d0217e075e11838da38a

--- chrome/browser/first_run/first_run_internal_posix.cc
+++ chrome/browser/first_run/first_run_internal_posix.cc
@@ -11,8 +11,10 @@
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/first_run/first_run.h"
 #include "chrome/browser/first_run/first_run_dialog.h"
+#include "chrome/browser/metrics/metrics_reporting_state.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/installer/util/google_update_settings.h"
 #include "chrome/installer/util/master_preferences.h"
 #include "components/metrics/metrics_pref_names.h"
@@ -21,26 +23,69 @@
 #include "components/startup_metric_utils/browser/startup_metric_utils.h"
 
 namespace first_run {
+
+void (*g_before_show_first_run_dialog_hook_for_testing)();
+
 namespace internal {
+namespace {
+
+#if !defined(OS_CHROMEOS)
+// Launch the first run dialog only for certain builds, and only if the user has
+// not already set preferences.
+bool ShouldShowFirstRunDialog(bool* update_metrics_reporting) {
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kForceFirstRunDialog)) {
+    return true;
+  }
+
+#if !defined(GOOGLE_CHROME_BUILD)
+  // On non-official builds, only --force-first-run-dialog will show the dialog.
+  return false;
+#endif
+
+  base::FilePath local_state_path;
+  PathService::Get(chrome::FILE_LOCAL_STATE, &local_state_path);
+  if (base::PathExists(local_state_path))
+    return false;
+
+  if (!internal::IsOrganicFirstRun())
+    return false;
+
+  // The purpose of the dialog is to ask the user to enable stats and crash
+  // reporting. This setting may be controlled through configuration management
+  // in enterprise scenarios. If that is the case, skip the dialog entirely, as
+  // it's not worth bothering the user for only the default browser question
+  // (which is likely to be forced in enterprise deployments anyway).
+  if (IsMetricsReportingPolicyManaged())
+    return false;
+
+  *update_metrics_reporting = true;
+  return true;
+}
+#endif  // !OS_CHROMEOS
+
+}  // namespace
 
 void DoPostImportPlatformSpecificTasks(Profile* profile) {
 #if !defined(OS_CHROMEOS)
-  base::FilePath local_state_path;
-  PathService::Get(chrome::FILE_LOCAL_STATE, &local_state_path);
-  bool local_state_file_exists = base::PathExists(local_state_path);
-  // Launch the first run dialog only for certain builds, and only if the user
-  // has not already set preferences.
-  if (internal::IsOrganicFirstRun() && !local_state_file_exists) {
-    if (ShowFirstRunDialog(profile)) {
-      bool is_opt_in = first_run::IsMetricsReportingOptIn();
-      metrics::RecordMetricsReportingDefaultState(
-          g_browser_process->local_state(),
-          is_opt_in ? metrics::EnableMetricsDefault::OPT_IN
-                    : metrics::EnableMetricsDefault::OPT_OUT);
-      startup_metric_utils::SetNonBrowserUIDisplayed();
-    }
+  bool update_metrics_reporting = false;
+  if (!ShouldShowFirstRunDialog(&update_metrics_reporting))
+    return;
+
+  if (g_before_show_first_run_dialog_hook_for_testing)
+    g_before_show_first_run_dialog_hook_for_testing();
+
+  ShowFirstRunDialog(profile);
+  startup_metric_utils::SetNonBrowserUIDisplayed();
+
+  if (update_metrics_reporting) {
+    bool is_opt_in = first_run::IsMetricsReportingOptIn();
+    metrics::RecordMetricsReportingDefaultState(
+        g_browser_process->local_state(),
+        is_opt_in ? metrics::EnableMetricsDefault::OPT_IN
+                  : metrics::EnableMetricsDefault::OPT_OUT);
   }
-#endif
+#endif  // !OS_CHROMEOS
 }
 
 bool IsFirstRunSentinelPresent() {
