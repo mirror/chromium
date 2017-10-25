af7e6e3ccf3a4950476f4a8250a4491e93a8492d
Jonathan Metzman <metzman@chromium.org>
2017-10-25 08:49:18 -0700

    Add CSS fuzzer that uses libprotobuf-mutator (WIP).
    Bug: 771641
    Change-Id: I90faad57bc1499b7cd8a4d6240b710911d8661bd

---
+++ third_party/WebKit/Source/core/css/parser/CSSParserFuzzer.cpp
@@ -0,0 +1,42 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/StyleSheetContents.h"
+#include "core/css/parser/CSSParser.h"
+#include "platform/testing/BlinkFuzzerTestSupport.h"
+#include "platform/wtf/text/WTFString.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static blink::BlinkFuzzerTestSupport test_support =
+      blink::BlinkFuzzerTestSupport();
+
+  // const static blink::CSSParserMode kParserModeMap[] = {
+  //     blink::kHTMLStandardMode,    blink::kHTMLQuirksMode,
+  //     blink::kSVGAttributeMode,    blink::kCSSViewportRuleMode,
+  //     blink::kCSSFontFaceRuleMode, blink::kUASheetMode};
+
+  if (size <= 5)
+    return 0;
+
+  // blink::CSSParserMode mode = kParserModeMap[data[0] % 6];
+  blink::CSSParserMode mode = blink::kHTMLQuirksMode;
+
+  // blink::CSSParserContext::SelectorProfile selector_profile = ;
+
+  // if (data[1] % 2)
+  //   selector_profile = blink::CSSParserContext::kDynamicProfile;
+  // else
+  //   selector_profile = blink::CSSParserContext::kStaticProfile;
+
+  blink::CSSParserContext* context = blink::CSSParserContext::Create(
+      mode, blink::CSSParserContext::kStaticProfile);
+  blink::StyleSheetContents* style_sheet =
+      blink::StyleSheetContents::Create(context);
+  // bool defer_property_parsing = data[4] % 2;
+  bool defer_property_parsing = false;
+  WTF::String style_sheet_string(data, size);
+  blink::CSSParser::ParseSheet(context, style_sheet, style_sheet_string,
+                               defer_property_parsing);
+  return 0;
+}
