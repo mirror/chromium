a7699dc2dc60bf217fa89d9e3e66e45770c9321a
Thanh Le <thanhdle@chromium.org>
2017-10-25 16:43:37 -0700

    Display estimated effective connection type of chrome://interventions-internals.
    The page is notified when there's a change in effective connection type.
    
    Bug: 778409
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ied4a037d2d94c52c3ef9d76b1984227fe9610d11

--- chrome/browser/ui/webui/interventions_internals/interventions_internals_ui.cc
+++ chrome/browser/ui/webui/interventions_internals/interventions_internals_ui.cc
@@ -6,6 +6,7 @@
 
 #include <string>
 
+#include "chrome/browser/net/nqe/ui_network_quality_estimator_service_factory.h"
 #include "chrome/browser/previews/previews_service.h"
 #include "chrome/browser/previews/previews_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -52,16 +53,22 @@ InterventionsInternalsUI::InterventionsInternalsUI(content::WebUI* web_ui)
     content::WebUIDataSource::Add(profile, GetUnsupportedSource());
     return;
   }
-
   content::WebUIDataSource::Add(profile, GetSource());
   logger_ = previews_service->previews_ui_service()->previews_logger();
+  ui_nqe_service_ =
+      UINetworkQualityEstimatorServiceFactory::GetForProfile(profile);
 }
 
-InterventionsInternalsUI::~InterventionsInternalsUI() {}
+InterventionsInternalsUI::~InterventionsInternalsUI() {
+  DCHECK(page_handler_);
+  ui_nqe_service_->RemoveEffectiveConnectionTypeObserver(page_handler_.get());
+}
 
 void InterventionsInternalsUI::BindUIHandler(
     mojom::InterventionsInternalsPageHandlerRequest request) {
   DCHECK(logger_);
+  DCHECK(ui_nqe_service_);
   page_handler_.reset(
       new InterventionsInternalsPageHandler(std::move(request), logger_));
+  ui_nqe_service_->AddEffectiveConnectionTypeObserver(page_handler_.get());
 }
