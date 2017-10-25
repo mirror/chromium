6d781110c51c6260f93b7025cac7e9f43aa2b494
Muyuan Li <muyuanli@chromium.org>
2017-10-25 12:41:40 -0700

    Map SPEECH_INPUT_TOGGLE key to start assistant on ChromeOS.
    BUG=655745
    TEST=Hit assistant key on headphone jack and observe
      assistant is launched.
    
    Change-Id: If041c5da94ef6986054dddb63701b780d17abb65

--- ash/accelerators/accelerator_controller.cc
+++ ash/accelerators/accelerator_controller.cc
@@ -614,7 +614,8 @@ bool CanHandleStartVoiceInteraction() {
   return chromeos::switches::IsVoiceInteractionFlagsEnabled();
 }
 
-void HandleToggleVoiceInteraction(const ui::Accelerator& accelerator) {
+void HandleStartVoiceInteraction(const ui::Accelerator& accelerator,
+                                  bool voice_mode) {
   if (accelerator.IsCmdDown() && accelerator.key_code() == ui::VKEY_SPACE) {
     base::RecordAction(
         base::UserMetricsAction("VoiceInteraction.Started.Search_Space"));
@@ -624,6 +625,9 @@ void HandleToggleVoiceInteraction(const ui::Accelerator& accelerator) {
   } else if (accelerator.key_code() == ui::VKEY_ASSISTANT) {
     base::RecordAction(
         base::UserMetricsAction("VoiceInteraction.Started.Assistant"));
+  } else if (accelerator.key_code() == ui::VKEY_VOICE_COMMAND) {
+    base::RecordAction(base::UserMetricsAction(
+        "VoiceInteraction.Started.VoiceCommandKey"));
   }
 
   // Show a toast if the active user is not primary.
@@ -649,7 +653,10 @@ void HandleToggleVoiceInteraction(const ui::Accelerator& accelerator) {
     return;
   }
 
-  Shell::Get()->app_list()->ToggleVoiceInteractionSession();
+  if (voice_mode)
+    Shell::Get()->app_list()->StartVoiceInteractionSession();
+  else
+    Shell::Get()->app_list()->ToggleVoiceInteractionSession();
 }
 
 void HandleSuspend() {
@@ -1070,7 +1077,10 @@ bool AcceleratorController::CanPerformAction(
       return accelerators::IsInternalDisplayZoomEnabled();
     case SHOW_STYLUS_TOOLS:
       return CanHandleShowStylusTools();
-    case START_VOICE_INTERACTION:
+    case START_VOICE_INTERACTION_TEXT_COMMAND:
+    case START_VOICE_INTERACTION_VOICE_COMMAND:
+      // Start assistant in voice command mode shares same condition as starting
+      // from assistant key.
       return CanHandleStartVoiceInteraction();
     case SWAP_PRIMARY_DISPLAY:
       return display::Screen::GetScreen()->GetNumDisplays() > 1;
@@ -1324,8 +1334,11 @@ void AcceleratorController::PerformAction(AcceleratorAction action,
     case SHOW_TASK_MANAGER:
       HandleShowTaskManager();
       break;
-    case START_VOICE_INTERACTION:
-      HandleToggleVoiceInteraction(accelerator);
+    case START_VOICE_INTERACTION_TEXT_COMMAND:
+      HandleStartVoiceInteraction(accelerator, false);
+      break;
+    case START_VOICE_INTERACTION_VOICE_COMMAND:
+      HandleStartVoiceInteraction(accelerator, true);
       break;
     case SUSPEND:
       HandleSuspend();
