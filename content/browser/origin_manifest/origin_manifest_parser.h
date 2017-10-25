cac6cbea26bf5b0d6c22f3e5af880209969572d7
dhausknecht <dhausknecht@google.com>
2017-10-25 15:12:30 +0200

    Origin Manifest parser
    It simply generates a Origin Manifest object from a string.
    
    The class is added as part of the implementation of the Origin Manifest mechanism (https://groups.google.com/a/chromium.org/d/topic/blink-dev/mdkHs4jybG4/discussion).
    This patch requires patch https://chromium-review.googlesource.com/c/chromium/src/+/725725.
    
    Bug: 751996
    Change-Id: I04270c8043c29970e4ff104ecc2e225bfb557ac0

---
+++ content/browser/origin_manifest/origin_manifest_parser.h
@@ -0,0 +1,56 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_ORIGIN_MANIFEST_ORIGIN_MANIFEST_PARSER_H_
+#define CONTENT_BROWSER_ORIGIN_MANIFEST_ORIGIN_MANIFEST_PARSER_H_
+
+#include <string>
+
+#include "base/gtest_prod_util.h"
+#include "base/values.h"
+#include "content/common/content_export.h"
+#include "third_party/WebKit/common/origin_manifest/origin_manifest.h"
+
+namespace base {
+class Value;
+}
+
+namespace content {
+
+class CONTENT_EXPORT OriginManifestParser {
+ public:
+  static std::unique_ptr<blink::OriginManifest> Parse(std::string json);
+
+ private:
+  enum class DirectiveType {
+    kContentSecurityPolicy,
+    kUnknown,
+  };
+
+  ~OriginManifestParser() = delete;
+  void* operator new(size_t) = delete;
+  void* operator new(size_t, void*) = delete;
+
+  static DirectiveType GetDirectiveType(const std::string& str);
+
+  static void ParseContentSecurityPolicy(blink::OriginManifest* const om,
+                                         base::Value value);
+
+  static blink::OriginManifest::ContentSecurityPolicyType GetCSPDisposition(
+      const std::string& json);
+  static blink::OriginManifest::ActivationType GetCSPActivationType(
+      const bool value);
+
+  FRIEND_TEST_ALL_PREFIXES(OriginManifestParserTest, GetDirectiveType);
+  FRIEND_TEST_ALL_PREFIXES(OriginManifestParserTest,
+                           ParseContentSecurityPolicy);
+  FRIEND_TEST_ALL_PREFIXES(OriginManifestParserTest, GetCSPDisposition);
+  FRIEND_TEST_ALL_PREFIXES(OriginManifestParserTest, GetCSPActivationType);
+
+  DISALLOW_IMPLICIT_CONSTRUCTORS(OriginManifestParser);
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_ORIGIN_MANIFEST_ORIGIN_MANIFEST_PARSER_H_
