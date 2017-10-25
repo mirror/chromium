70b5f5d77b1e280270f5e1672a5ba72b42448474
chaopeng <chaopeng@chromium.org>
2017-10-25 12:17:16 -0400

    Fix TestNonCompositedOverlayScrollbarsFade in ASAN and TSAN
    TestNonCompositedOverlayScrollbarsFade fails because we give it a 5ms fadeout
    delay, then it fadeout before the first check. In this patch we increase the
    number to 20ms.
    
    Bug: 776684
    Change-Id: Ic658a5fd386592f84f18023c37d96b00ab75173c

--- third_party/WebKit/Source/core/layout/ScrollbarsTest.cpp
+++ third_party/WebKit/Source/core/layout/ScrollbarsTest.cpp
@@ -930,22 +930,14 @@ static void DisableCompositing(WebSettings* settings) {
   settings->SetPreferCompositingToLCDTextEnabled(false);
 }
 
-#if defined(THREAD_SANITIZER) || defined(ADDRESS_SANITIZER)
-#define DISABLE_ON_TSAN_AND_ASAN(test_name) DISABLED_##test_name
-#else
-#define DISABLE_ON_TSAN_AND_ASAN(test_name) test_name
-#endif  // defined(THREAD_SANITIZER)
-
 // Make sure overlay scrollbars on non-composited scrollers fade out and set
 // the hidden bit as needed.
-// TODO(crbug.com/776684): Fix and re-enable on TSAN and ASAN.
-TEST_P(ScrollbarsTest,
-       DISABLE_ON_TSAN_AND_ASAN(TestNonCompositedOverlayScrollbarsFade)) {
+TEST_P(ScrollbarsTest, TestNonCompositedOverlayScrollbarsFade) {
   FrameTestHelpers::WebViewHelper web_view_helper;
   WebViewImpl* web_view_impl = web_view_helper.Initialize(
       nullptr, nullptr, nullptr, &DisableCompositing);
 
-  constexpr double kMockOverlayFadeOutDelayMs = 5.0;
+  constexpr double kMockOverlayFadeOutDelayMs = 20.0;
   constexpr TimeDelta kMockOverlayFadeOutDelay =
       TimeDelta::FromMillisecondsD(kMockOverlayFadeOutDelayMs);
 
