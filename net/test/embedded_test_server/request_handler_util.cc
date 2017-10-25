63c20f46fbbae5765527c29f7e38e74ee86fbf77
Jian Li <jianli@chromium.org>
2017-10-25 16:00:47 -0700

    Trigger downloading MHTML from remote server, instead of loading it
    To pass the offline page info to ResourceRequestInfo,
    OfflinePageNavigationUIData will be added as part of NavigationUIData.
    Setting OfflinePageNavigationUIData is done in
    OfflinePageRequestJob::OnOfflineFilePathAvailable.
    
    ChromeResourceDispatcherHostDelegate::ShouldForceDownloadResource will
    check this bit in order to decide whether to treat it as download.
    
    Most of changes to unittest files are just cosmetic changes due to
    adding one parameter to ResourceRequestInfo::AllocateForTesting.
    OfflinePageRquestJobTest is updated to cehck if
    OfflinePageNavigationUIData is present or not.
    
    Bug: 777019
    Change-Id: Ib90388780f8d1b47226419bf514a30cd1eacbfae

--- net/test/embedded_test_server/request_handler_util.cc
+++ net/test/embedded_test_server/request_handler_util.cc
@@ -60,6 +60,8 @@ std::string GetContentType(const base::FilePath& path) {
     return "audio/wav";
   if (path.MatchesExtension(FILE_PATH_LITERAL(".xml")))
     return "text/xml";
+  if (path.MatchesExtension(FILE_PATH_LITERAL(".mhtml")))
+    return "multipart/related";
   if (path.MatchesExtension(FILE_PATH_LITERAL(".html")) ||
       path.MatchesExtension(FILE_PATH_LITERAL(".htm"))) {
     return "text/html";
