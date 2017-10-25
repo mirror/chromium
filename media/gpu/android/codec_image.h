78104feed1df68b9342017e692a1dcc19e6c44ab
Sunny Sachanandani <sunnyps@chromium.org>
2017-10-24 17:26:16 -0700

    cc: Use CHROMIUM_texture_storage_image for GMB backed texture resources.
    Use TexStorage2DImageCHROMIUM to allocate service side anonymous
    GpuMemoryBuffers instead of allocating GMBs client side. Add a new
    texture hint that can be used to allocate textures this way, and use it
    for overlay resources in GLRenderer.
    
    R=piman,ccameron
    BUG=768108
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I1564dcb4d444b0b9c77e0486f38fd7ddea3da351

--- media/gpu/android/codec_image.h
+++ media/gpu/android/codec_image.h
@@ -45,6 +45,7 @@ class MEDIA_GPU_EXPORT CodecImage : public gpu::gles2::GLStreamTextureImage {
                             gfx::OverlayTransform transform,
                             const gfx::Rect& bounds_rect,
                             const gfx::RectF& crop_rect) override;
+  void SetColorSpace(const gfx::ColorSpace& color_space) override {}
   void Flush() override {}
   void OnMemoryDump(base::trace_event::ProcessMemoryDump* pmd,
                     uint64_t process_tracing_id,
