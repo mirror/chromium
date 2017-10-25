acf94bd26e8096b966b0569f2ffce582ac0d05dd
oka@chromium.org <oka@chromium.org>
2017-10-25 18:23:00 +0900

    Remove hack: text type is password when keyboard feature is restricted
    Bug: 710242
    Test: manually
    Change-Id: Ic5381d6927529b476a17c252cf42d6a626a427a8

--- chrome/browser/extensions/api/input_ime/input_ime_api.cc
+++ chrome/browser/extensions/api/input_ime/input_ime_api.cc
@@ -204,17 +204,6 @@ bool ImeObserver::HasListener(const std::string& event_name) const {
 
 std::string ImeObserver::ConvertInputContextType(
     ui::IMEEngineHandlerInterface::InputContext input_context) {
-  // This is a hack, but tricking the virtual keyboard to think the
-  // current input context is password will disable all keyboard features
-  // that are not supported in restricted keyboard mode.
-  // TODO(oka): Remove this hack once VK extension starts to honor the
-  // virtualKeyboardPrivate.GetKeyboardConfig parameters.
-  if (IsKeyboardRestricted() &&
-      input_context.type != ui::TEXT_INPUT_TYPE_TELEPHONE &&
-      input_context.type != ui::TEXT_INPUT_TYPE_NUMBER) {
-    return "password";
-  }
-
   std::string input_context_type = "text";
   switch (input_context.type) {
     case ui::TEXT_INPUT_TYPE_SEARCH:
