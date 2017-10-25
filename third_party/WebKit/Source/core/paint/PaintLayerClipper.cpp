aadfc9f23e7d62ca9daf58a6b8b2119e01f25f55
Chris Harrelson <chrishtr@chromium.org>
2017-10-24 22:02:05 -0700

    Add defensive code against missing fragments in one more case.
    Bug:774946
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ic2eeb76c7bd26003b7666ea947cb3a2e60fc50ab

--- third_party/WebKit/Source/core/paint/PaintLayerClipper.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerClipper.cpp
@@ -558,8 +558,14 @@ void PaintLayerClipper::CalculateBackgroundClipRect(
     const ClipRectsContext& context,
     ClipRect& output) const {
   if (use_geometry_mapper_) {
-    CalculateBackgroundClipRectWithGeometryMapper(
-        context, *layer_.GetLayoutObject().FirstFragment(), output);
+    auto* fragment_data = layer_.GetLayoutObject().FirstFragment();
+    DCHECK(fragment_data);
+    // TODO(chrishtr): find the root cause of not having a fragment and fix
+    // it.
+    if (!fragment_data)
+      return;
+    CalculateBackgroundClipRectWithGeometryMapper(context, *fragment_data,
+                                                  output);
     return;
   }
   DCHECK(layer_.Parent());
