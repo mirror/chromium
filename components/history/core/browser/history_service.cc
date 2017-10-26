15db33640d317e0e0fd3509b8626e49d93df447e
Mikel Astiz <mastiz@chromium.org>
2017-10-26 15:32:09 +0200

    Refactor enum favicon_base::IconType
    No behavioral changes, and most diffs are trivial.
    
    The change includes:
    - Adoption of enum class for stronger type safety.
    - Adoption of consecutive enum values (avoid "holes"), which improves
      Mojo validation and simplifies integration with UMA.
    - Backward-compatibility in persistency layers (ThumbnailDatabase).
    - Adoption of a container to represent bitsets which makes APIs less
      subtle.
    
    I've considered introducing a home-grown container for IconTypeSet (e.g.
    based on std::bitset) but decided against for code simplicity. Besides,
    as of today, base::small_set does not exist, so we're using flat_set.
    
    This change should also help prevent future issues with C++17 causing
    undefined behavior for unlisted enum values.
    
    Bug: 778159
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I64986271937bea040265f5d52e664be175b0fa85

--- components/history/core/browser/history_service.cc
+++ components/history/core/browser/history_service.cc
@@ -522,7 +522,7 @@ base::CancelableTaskTracker::TaskId HistoryService::GetFavicon(
 
 base::CancelableTaskTracker::TaskId HistoryService::GetFaviconsForURL(
     const GURL& page_url,
-    int icon_types,
+    const favicon_base::IconTypeSet& icon_types,
     const std::vector<int>& desired_sizes,
     const favicon_base::FaviconResultsCallback& callback,
     base::CancelableTaskTracker* tracker) {
@@ -540,7 +540,7 @@ base::CancelableTaskTracker::TaskId HistoryService::GetFaviconsForURL(
 
 base::CancelableTaskTracker::TaskId HistoryService::GetLargestFaviconForURL(
     const GURL& page_url,
-    const std::vector<int>& icon_types,
+    const std::vector<favicon_base::IconTypeSet>& icon_types,
     int minimum_size_in_pixels,
     const favicon_base::FaviconRawBitmapCallback& callback,
     base::CancelableTaskTracker* tracker) {
