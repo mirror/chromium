cf0f03d82b497ca2f4956a2cabae4b9479226858
Ivan Sandrk <isandrk@google.com>
2017-10-26 19:59:37 +0200

    Introduce lockWindowFullscreenPrivate and enable creation of locked fullscreen windows
    Design doc @ go/locked-fullscreen-mode
    
    Bug: chromium:755696
    Change-Id: I78b774a40993b5672f24a2e1ae2f8a8aaa4eed8a

---
+++ chrome/test/data/extensions/api_test/window_open/locked_fullscreen/test.js
@@ -0,0 +1,10 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  function openLockedFullscreenWindow() {
+    chrome.windows.create({url: 'about:blank', state: 'locked'},
+      chrome.test.callbackPass(function(win) {}));
+  }
+]);
