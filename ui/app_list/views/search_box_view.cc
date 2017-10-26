38d78a5eced88bc1318de46b82d37379099fe39a
Xiaohui Chen <xiaohuic@google.com>
2017-10-26 16:14:44 -0700

    Remove hotword related code
    Bug: 761426
    Test: Build and run locally, used browser, settings, voice search
    Test: on new tab and no feature seems broken.
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I647a19ec174442ddab5c4c322bfd34c8fc9a75bf

--- ui/app_list/views/search_box_view.cc
+++ ui/app_list/views/search_box_view.cc
@@ -983,22 +983,11 @@ void SearchBoxView::SpeechRecognitionButtonPropChanged() {
           gfx::CreateVectorIcon(kIcMicBlackIcon, kMicIconSize,
                                 kDefaultSearchboxColor));
     }
-    // TODO(warx): consider removing on_tooltip as it is not accessible due to
-    // the overlap of speech UI.
-    if (view_delegate_->GetSpeechUI()->state() ==
-        SPEECH_RECOGNITION_HOTWORD_LISTENING) {
-      if (!is_fullscreen_app_list_enabled_) {
-        speech_button_->SetImage(views::Button::STATE_NORMAL,
-                                 &speech_button_prop->on_icon);
-      }
-      speech_button_->SetTooltipText(speech_button_prop->on_tooltip);
-    } else {
-      if (!is_fullscreen_app_list_enabled_) {
-        speech_button_->SetImage(views::Button::STATE_NORMAL,
-                                 &speech_button_prop->off_icon);
-      }
-      speech_button_->SetTooltipText(speech_button_prop->off_tooltip);
+    if (!is_fullscreen_app_list_enabled_) {
+      speech_button_->SetImage(views::Button::STATE_NORMAL,
+                               &speech_button_prop->off_icon);
     }
+    speech_button_->SetTooltipText(speech_button_prop->off_tooltip);
   } else {
     if (speech_button_) {
       // Deleting a view will detach it from its parent.
