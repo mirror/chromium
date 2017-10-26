cf0c7a4975e5eeccbe9d2b859c94fab8f90bfdaf
Sammie Quon <sammiequon@google.com>
2017-10-25 17:09:45 -0700

    tablet mode: Hide selection when dragging in overview and prevent overview window from clipping immersive moded tablet windows.
    Fixes  two of overview mode introduced by tablet mode work.
    
    - Do not allow the top of the settings app to get trimmed.
    - Hide the selection widget while dragging.
    
    Test: manual
    Bug: 777652
    Change-Id: I9b8313cbd255378cf51c7919269491e85190f449

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_ash.cc
@@ -287,9 +287,12 @@ void BrowserNonClientFrameViewAsh::Layout() {
   if (profile_indicator_icon())
     LayoutProfileIndicatorIcon();
   BrowserNonClientFrameView::Layout();
+
+  bool has_top_view_inset = !browser_view()->IsTabStripVisible();
+  // There are no insets if tablet mode is enabled and the auto hide title bars
+  // feature is enabled.
   frame()->GetNativeWindow()->SetProperty(
-      aura::client::kTopViewInset,
-      browser_view()->IsTabStripVisible() ? 0 : GetTopInset(true));
+      aura::client::kTopViewInset, has_top_view_inset ? GetTopInset(true) : 0);
 }
 
 const char* BrowserNonClientFrameViewAsh::GetClassName() const {
@@ -454,8 +457,15 @@ void BrowserNonClientFrameViewAsh::LayoutProfileIndicatorIcon() {
 }
 
 bool BrowserNonClientFrameViewAsh::ShouldPaint() const {
-  if (!frame()->IsFullscreen())
-    return true;
+  if (!frame()->IsFullscreen()) {
+    // For tablet mode, non-normal browsers will be in immersive mode, so they
+    // may or may not need to have their headers painted.
+    if (!TabletModeClient::Get()->tablet_mode_enabled() ||
+        !TabletModeClient::Get()->auto_hide_title_bars() ||
+        browser_view()->IsBrowserTypeNormal()) {
+      return true;
+    }
+  }
 
   // We need to paint when the top-of-window views are revealed in immersive
   // fullscreen.
