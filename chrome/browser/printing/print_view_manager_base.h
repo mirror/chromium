d6d3abae43eb216f6acdb6da4c57fdb4642416ec
rbpotter <rbpotter@chromium.org>
2017-10-26 10:35:10 -0700

    Use the PDF in PrintPreviewDataStore for local printers
    To improve consistency with other printer types and avoid a second
    round of rendering, use the SkPDF stored in the print preview data
    store for local printers.
    
    Bug:  None
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I7d3423efb8b473ef862491b203bb18eb172e6458

--- chrome/browser/printing/print_view_manager_base.h
+++ chrome/browser/printing/print_view_manager_base.h
@@ -23,7 +23,7 @@
 struct PrintHostMsg_DidPrintPage_Params;
 
 namespace base {
-class SharedMemory;
+class RefCountedBytes;
 }
 
 namespace content {
@@ -36,6 +36,8 @@ class JobEventDetails;
 class PrintJob;
 class PrintJobWorkerOwner;
 class PrintQueriesQueue;
+class PrintedDocument;
+class PrinterQuery;
 
 // Base class for managing the print commands for a WebContents.
 class PrintViewManagerBase : public content::NotificationObserver,
@@ -49,6 +51,26 @@ class PrintViewManagerBase : public content::NotificationObserver,
   // this function. Returns false if printing is impossible at the moment.
   virtual bool PrintNow(content::RenderFrameHost* rfh);
 #endif  // ENABLE_BASIC_PRINTING
+
+#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
+  // Prints the document in |print_data| with settings specified in
+  // |job_settings|. Runs callback with an error string on failure and with an
+  // empty string if the print job is started successfully. |rfh| and |rph|
+  // should be the render frame host and render process host for the print
+  // preview initiator contents respectively.
+  // On MacOS, |print_data| should contain only the first page of data to print
+  // and |get_page_callback| will be called to obtain the other pages.
+  using PrintCallback = base::OnceCallback<void(const base::Value& error)>;
+  using GetPageCallback =
+      base::RepeatingCallback<scoped_refptr<base::RefCountedBytes>(int page)>;
+  void PrintForPrintPreview(
+      PrintCallback callback,
+      std::unique_ptr<base::DictionaryValue> job_settings,
+      int page_count,
+      const scoped_refptr<base::RefCountedBytes>& print_data,
+      content::RenderFrameHost* rfh,
+      const GetPageCallback& get_page_callback);
+#endif
 
   // Whether printing is enabled or not.
   void UpdatePrintingEnabled();
@@ -93,15 +115,46 @@ class PrintViewManagerBase : public content::NotificationObserver,
   void OnShowInvalidPrinterSettingsError();
   void OnDidPrintPage(const PrintHostMsg_DidPrintPage_Params& params);
 
-  // Handle extra tasks once a page or doc is printed.
-  void UpdateForPrintedPage(const PrintHostMsg_DidPrintPage_Params& params,
-                            bool has_valid_page_data,
-                            std::unique_ptr<base::SharedMemory> shared_buf);
-
   // IPC message handlers for service.
   void OnComposePdfDone(const PrintHostMsg_DidPrintPage_Params& params,
                         mojom::PdfCompositor::Status status,
                         mojo::ScopedSharedBufferHandle handle);
+
+// Helpers for PrintForPrintPreview();
+#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
+  void CreateQueryWithSettings(
+      content::RenderFrameHost* rfh,
+      std::unique_ptr<base::DictionaryValue> job_settings,
+      base::OnceCallback<void(scoped_refptr<printing::PrinterQuery>)> callback);
+
+  void OnPrintSettingsDone(
+      PrintCallback callback,
+      int page_count,
+      const scoped_refptr<base::RefCountedBytes>& print_data,
+      const GetPageCallback& get_page_callback,
+      scoped_refptr<printing::PrinterQuery> printer_query);
+
+  void StartLocalPrintJob(
+      PrintCallback callback,
+      int page_count,
+      const scoped_refptr<base::RefCountedBytes>& print_data,
+      scoped_refptr<printing::PrinterQuery> printer_query,
+      const GetPageCallback& get_page_callback);
+
+#if defined(OS_MACOSX)
+  // Callback that runs when the next page of print data has been obtained
+  // from the printer handler.
+  void OnGotPrintData(PrintCallback callback,
+                      const GetPageCallback& get_page_callback,
+                      const std::vector<int>& page_nums,
+                      size_t index,
+                      scoped_refptr<printing::PrinterQuery> printer_query,
+                      const gfx::Size& page_size,
+                      const gfx::Rect& content_area,
+                      const gfx::Point& offsets,
+                      scoped_refptr<base::RefCountedBytes> print_data);
+#endif  // defined(OS_MACOSX)
+#endif  // BUILDFLAG(ENABLE_PRINT_PREVIEW)
 
   // Processes a NOTIFY_PRINT_JOB_EVENT notification.
   void OnNotifyPrintJobEvent(const JobEventDetails& event_details);
@@ -110,6 +163,25 @@ class PrintViewManagerBase : public content::NotificationObserver,
   // No-op if no print job is pending. Returns true if at least one page has
   // been requested to the renderer.
   bool RenderAllMissingPagesNow();
+
+  // Checks that synchronization is correct and a print query exists for
+  // |cookie|. If so, returns the document associated with the cookie.
+  PrintedDocument* GetDocument(int cookie);
+
+  // Whether the metafile handle must be valid.
+  bool MetafileMustBeValid();
+
+  // Starts printing page |page_num| of a document with data given in
+  // |print_data|. |print_data| must successfully initialize a metafile if
+  // |metafile_must_be_valid| is true. |document| is the printed document
+  // associated with the print job.
+  bool PrintPage(int page_num,
+                 const gfx::Size& page_size,
+                 const gfx::Rect& content_area,
+                 const gfx::Point& offsets,
+                 const scoped_refptr<base::RefCountedBytes>& print_data,
+                 bool metafile_must_be_valid,
+                 PrintedDocument* document);
 
   // Quits the current message loop if these conditions hold true: a document is
   // loaded and is complete and waiting_for_pages_to_be_rendered_ is true. This
