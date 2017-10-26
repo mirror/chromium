bf8c85658480209e0db56a0f5ec41889a41a3e17
Nathan Bruer <allada@chromium.org>
2017-10-26 14:17:29 -0700

    [Devtools] Requests can now be intercepted on response
    Adds the ability in protocol to intercept requests on the response
    instead of the request. This will allow us to patch requests instead of
    just feed in the raw data.
    
    R=pfeldman,dgozman,caseq
    BUG=754371
    
    Change-Id: I1cded22b8ce4e6bd2e41a81143d024085c8315af

---
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/network/request-interception-on-response.js
@@ -0,0 +1,54 @@
+(async function(testRunner) {
+  var {page, session, dp} = await testRunner.startBlank(
+      `Tests interception blocking, modification of network fetches.`);
+
+  var InterceptionHelper = await testRunner.loadScript('../resources/interception-test.js');
+  var helper = new InterceptionHelper(testRunner, session);
+
+  var headersMaskList = new Set(['date', 'server', 'last-modified', 'etag', 'keep-alive']);
+
+  var requestInterceptedDict = {
+    'simple-iframe.html': event => {
+      testRunner.log('Request Intercepted: ' + event.params.request.url.split('/').pop());
+      testRunner.log('  responseCode: ' + event.params.responseCode);
+      testRunner.log('  responseHeaders:');
+      for (var headerName of Object.keys(event.params.responseHeaders)) {
+        var headerValue = event.params.responseHeaders[headerName];
+        if (headersMaskList.has(headerName.toLowerCase()))
+          headerValue = '<Masked>';
+        testRunner.log(`    ${headerName}: ${headerValue}`);
+      }
+      testRunner.log('  responseBody:');
+      testRunner.log(atob(event.params.responseBody));
+      helper.modifyRequest(event, {rawResponse: btoa('<html>\n<body>This content was rewritten!</body>\n</html>')});
+      testRunner.log('');
+    }
+  };
+
+  await helper.startInterceptionTest(requestInterceptedDict, Infinity, true);
+
+  var requestId = '';
+  session.protocol.Network.onRequestWillBeSent(event => {
+    testRunner.log('RequestWillBeSent Event fired for url: ' + event.params.request.url.split('/').pop());
+    if (requestId)
+      throw "requestId already set";
+    requestId = event.params.requestId;
+  });
+  await new Promise(resolve => {
+    session.protocol.Network.onResponseReceived(event => {
+      testRunner.log('ResponseReceived Event fired');
+      resolve();
+    });
+    session.evaluate(`
+      var iframe = document.createElement('iframe');
+      iframe.src = '${testRunner.url('./resources/simple-iframe.html')}';
+      document.body.appendChild(iframe);
+    `);
+  });
+
+  var result = await session.protocol.Network.getResponseBody({requestId: requestId});
+  testRunner.log('Body content received by renderer:');
+  testRunner.log(result.result.base64Encoded ? atob(result.result.body) : result.result.body);
+
+  testRunner.completeTest();
+})
