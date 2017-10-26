e98b8093f628324c2f1f737bc5e787ff50560198
Steve Kobes <skobes@chromium.org>
2017-10-26 13:16:50 -0700

    Implement 'try-flag trigger'.
    Bug: 755401
    Change-Id: Ica41e281b77f3b3625e65f883aedd394470c2a6d

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/try_flag.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/try_flag.py
@@ -29,6 +29,7 @@ import sys
 
 from webkitpy.common.host import Host
 from webkitpy.common.net.git_cl import GitCL
+from webkitpy.common.path_finder import PathFinder
 from webkitpy.layout_tests.models.test_configuration import TestConfiguration
 from webkitpy.layout_tests.models.test_configuration import TestConfigurationConverter
 from webkitpy.layout_tests.models.test_expectations import TestExpectationLine
@@ -36,11 +37,17 @@ from webkitpy.layout_tests.models.test_expectations import TestExpectations
 from webkitpy.layout_tests.models.test_expectations import TestExpectationsModel
 
 
-BUILDERS = {
+BUILDER_CONFIGS = {
     'linux_chromium_rel_ng': TestConfiguration('Linux', '', 'release'),
     'mac_chromium_rel_ng': TestConfiguration('Mac', '', 'release'),
     'win7_chromium_rel_ng': TestConfiguration('Win', '', 'release')
 }
+BUILDER_MASTERS = {
+    'linux_chromium_rel_ng': 'tryserver.chromium.linux',
+    'mac_chromium_rel_ng': 'tryserver.chromium.mac',
+    'win7_chromium_rel_ng': 'tryserver.chromium.win7'
+}
+FLAG_FILE = 'additional-driver-flag.setting'
 
 
 class TryFlag(object):
@@ -51,10 +58,27 @@ class TryFlag(object):
         self._git_cl = git_cl
         self._expectations_model = TestExpectationsModel()
         self._test_configuration_converter = TestConfigurationConverter(
-            set(BUILDERS.values()))
+            set(BUILDER_CONFIGS.values()))
+        self._filesystem = self._host.filesystem
+        self._path_finder = PathFinder(self._filesystem)
+        self._git = self._host.git()
+
+    def _set_flag(self, flag):
+        filesystem = self._filesystem
+        path = filesystem.join(self._path_finder.layout_tests_dir(), FLAG_FILE)
+        filesystem.write_text_file(path, flag + '\n')
+        self._git.add_list([path])
+        self._git.commit_locally_with_message(
+            'Flag try job: force %s for run-webkit-tests.' % flag)
 
     def trigger(self):
-        print 'TODO: implement "trigger"'
+        flag = self._args.flag
+        if flag:
+            self._set_flag(flag)
+            self._git_cl.run(['upload', '--bypass-hooks', '-f',
+                              '-t', 'Flag try job for %s.' % flag])
+        for builder, master in BUILDER_MASTERS.iteritems():
+            self._git_cl.trigger_try_jobs([builder], master)
 
     def _create_expectation_line(self, result, test_configuration):
         test_name = result.test_name()
@@ -78,18 +102,19 @@ class TryFlag(object):
     def _process_result(self, build, result):
         if not result.did_run_as_expected():
             self._expectations_model.add_expectation_line(
-                self._create_expectation_line(result,
-                                              BUILDERS[build.builder_name]),
+                self._create_expectation_line(
+                    result,
+                    BUILDER_CONFIGS[build.builder_name]),
                 model_all_expectations=True)
 
     def update(self):
         self._host.print_('Fetching results...')
         # TODO: Get jobs from the _tryflag branch. Current branch for now.
-        jobs = self._git_cl.latest_try_jobs(BUILDERS.keys())
+        jobs = self._git_cl.latest_try_jobs(BUILDER_CONFIGS.keys())
         buildbot = self._host.buildbot
         for build in sorted(jobs.keys()):
             self._host.print_('-- %s: %s/results.html' % (
-                BUILDERS[build.builder_name].version,
+                BUILDER_CONFIGS[build.builder_name].version,
                 buildbot.results_url(build.builder_name, build.build_number)))
             results = buildbot.fetch_results(build, True)
             results.for_each_test(
@@ -129,6 +154,8 @@ def parse_args(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('action', help='"trigger" or "update"')
     parser.add_argument('--bug', help='crbug number for expectation lines')
+    parser.add_argument('--flag',
+                        help='flag to force-enable in run-webkit-tests')
     return parser.parse_args(argv)
 
 
