231b7215b1df4a0f66b603f97bcb0cbafc54b452
Ella Ge <eirage@chromium.org>
2017-10-26 11:39:33 -0400

    Fractional MouseEvent
    This is the last patch of fractional MouseEvent.
    
    Add a non-web exposed feature flag for fractional coordinates in ME.
    Add a experimental flag to expose fractional coordinates
    for PointerEvent of type "mouse"
    Add Layouttests for fractional pointer event coordinates.
    
    Intent to implement and ship for expose fractions for PE
    type mouse: 
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/ssfd-_V26Ls
    
    Bug:456625, 773813
    Change-Id: I036f4953857907daf3131fd260331dc9dd2e4874

--- third_party/WebKit/Source/core/events/PointerEvent.h
+++ third_party/WebKit/Source/core/events/PointerEvent.h
@@ -39,33 +39,45 @@ class CORE_EXPORT PointerEvent final : public MouseEvent {
   bool IsMouseEvent() const override;
   bool IsPointerEvent() const override;
 
-  // TODO(eirage): crbug.com/773813 Make PointerEvents of type mouse have
-  // fractional coordiantes
+  // TODO(eirage): Remove these override of coordinates getter when
+  // fractional mouseevent flag is removed.
   double screenX() const override {
-    return (pointer_type_ == "mouse") ? static_cast<int>(screen_location_.X())
-                                      : screen_location_.X();
+    return (!RuntimeEnabledFeatures::FractionalMouseTypePointerEventEnabled() &&
+            pointer_type_ == "mouse")
+               ? static_cast<int>(screen_location_.X())
+               : screen_location_.X();
   }
   double screenY() const override {
-    return (pointer_type_ == "mouse") ? static_cast<int>(screen_location_.Y())
-                                      : screen_location_.Y();
+    return (!RuntimeEnabledFeatures::FractionalMouseTypePointerEventEnabled() &&
+            pointer_type_ == "mouse")
+               ? static_cast<int>(screen_location_.Y())
+               : screen_location_.Y();
   }
 
   double clientX() const override {
-    return (pointer_type_ == "mouse") ? static_cast<int>(client_location_.X())
-                                      : client_location_.X();
+    return (!RuntimeEnabledFeatures::FractionalMouseTypePointerEventEnabled() &&
+            pointer_type_ == "mouse")
+               ? static_cast<int>(client_location_.X())
+               : client_location_.X();
   }
   double clientY() const override {
-    return (pointer_type_ == "mouse") ? static_cast<int>(client_location_.Y())
-                                      : client_location_.Y();
+    return (!RuntimeEnabledFeatures::FractionalMouseTypePointerEventEnabled() &&
+            pointer_type_ == "mouse")
+               ? static_cast<int>(client_location_.Y())
+               : client_location_.Y();
   }
 
   double pageX() const override {
-    return (pointer_type_ == "mouse") ? static_cast<int>(page_location_.X())
-                                      : page_location_.X();
+    return (!RuntimeEnabledFeatures::FractionalMouseTypePointerEventEnabled() &&
+            pointer_type_ == "mouse")
+               ? static_cast<int>(page_location_.X())
+               : page_location_.X();
   }
   double pageY() const override {
-    return (pointer_type_ == "mouse") ? static_cast<int>(page_location_.Y())
-                                      : page_location_.Y();
+    return (!RuntimeEnabledFeatures::FractionalMouseTypePointerEventEnabled() &&
+            pointer_type_ == "mouse")
+               ? static_cast<int>(page_location_.Y())
+               : page_location_.Y();
   }
 
   EventDispatchMediator* CreateMediator() override;
