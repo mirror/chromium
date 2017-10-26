5e933b12497158381d7e112b253eb17596068e0f
Mounir Lamouri <mlamouri@chromium.org>
2017-10-26 16:32:24 +0100

    Media Engagement: record metrics related to data clearing.
    This is adding a user action metric that records when site data are
    cleared and a histogram that record the reduction in MEI score when
    history is cleared.
    
    Bug: 778701
    Change-Id: Iec07a068a9230b15b8fa52aeed00ab9ef573dfe2

--- chrome/browser/media/media_engagement_service_unittest.cc
+++ chrome/browser/media/media_engagement_service_unittest.cc
@@ -347,6 +347,7 @@ TEST_F(MediaEngagementServiceTest, CleanupOriginsOnHistoryDeletion) {
   EXPECT_TRUE(GetActualScore(origin4));
 
   {
+    base::HistogramTester histogram_tester;
     MediaEngagementChangeWaiter waiter(profile());
 
     base::CancelableTaskTracker task_tracker;
@@ -368,9 +369,18 @@ TEST_F(MediaEngagementServiceTest, CleanupOriginsOnHistoryDeletion) {
     ExpectScores(origin3, 0.0, 1, 0, TimeNotSet());
     ExpectScores(origin4, 0.4, MediaEngagementScore::kScoreMinVisits, 2,
                  TimeNotSet());
+
+
+    histogram_tester.ExpectTotalCount(
+      MediaEngagementService::kHistogramURLsDeletedScoreReductionName, 3);
+    histogram_tester.ExpectBucketCount(
+      MediaEngagementService::kHistogramURLsDeletedScoreReductionName, 0, 2);
+    histogram_tester.ExpectBucketCount(
+      MediaEngagementService::kHistogramURLsDeletedScoreReductionName, 8, 1);
   }
 
   {
+    base::HistogramTester histogram_tester;
     MediaEngagementChangeWaiter waiter(profile());
 
     // Expire origin1b.
@@ -392,9 +402,15 @@ TEST_F(MediaEngagementServiceTest, CleanupOriginsOnHistoryDeletion) {
     ExpectScores(origin3, 0.0, 1, 0, TimeNotSet());
     ExpectScores(origin4, 0.4, MediaEngagementScore::kScoreMinVisits, 2,
                  TimeNotSet());
+
+    histogram_tester.ExpectTotalCount(
+      MediaEngagementService::kHistogramURLsDeletedScoreReductionName, 1);
+    histogram_tester.ExpectBucketCount(
+      MediaEngagementService::kHistogramURLsDeletedScoreReductionName, 17, 1);
   }
 
   {
+    base::HistogramTester histogram_tester;
     MediaEngagementChangeWaiter waiter(profile());
 
     // Expire origin3.
@@ -418,6 +434,11 @@ TEST_F(MediaEngagementServiceTest, CleanupOriginsOnHistoryDeletion) {
     ExpectScores(origin3, 0.0, 0, 0, TimeNotSet());
     ExpectScores(origin4, 0.4, MediaEngagementScore::kScoreMinVisits, 2,
                  TimeNotSet());
+
+    histogram_tester.ExpectTotalCount(
+      MediaEngagementService::kHistogramURLsDeletedScoreReductionName, 1);
+    histogram_tester.ExpectBucketCount(
+      MediaEngagementService::kHistogramURLsDeletedScoreReductionName, 0, 1);
   }
 }
 
