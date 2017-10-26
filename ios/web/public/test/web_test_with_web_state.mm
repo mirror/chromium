131bafe2af16c03ccbab65a2c248a87dada5b4d1
Sylvain Defresne <sdefresne@chromium.org>
2017-10-25 17:51:44 -0700

    Allow WebStateObserver to observe N WebStates [7/N].
    Convert test WebStateObserver subclasses to directly manage
    the registration with WebState instead of dependending on
    the deprecated code from WebStateObserver.
    
    Bug: 775684
    Change-Id: I2c209d662794d2b1d7bbeb09e377f03f359e184f
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet

--- ios/web/public/test/web_test_with_web_state.mm
+++ ios/web/public/test/web_test_with_web_state.mm
@@ -60,8 +60,15 @@ void WebTestWithWebState::LoadHtml(NSString* html, const GURL& url) {
   // Sets MIME type to "text/html" once navigation is committed.
   class MimeTypeUpdater : public WebStateObserver {
    public:
-    explicit MimeTypeUpdater(WebState* web_state)
-        : WebStateObserver(web_state) {}
+    explicit MimeTypeUpdater(WebState* web_state) : web_state_(web_state) {
+      web_state_->AddObserver(this);
+    }
+    ~MimeTypeUpdater() override {
+      if (web_state_) {
+        web_state_->RemoveObserver(this);
+        web_state_ = nullptr;
+      }
+    }
     // WebStateObserver overrides:
     void NavigationItemCommitted(WebState* web_state,
                                  const LoadCommittedDetails&) override {
@@ -72,6 +79,16 @@ void WebTestWithWebState::LoadHtml(NSString* html, const GURL& url) {
       // checking if MIME type is in fact HTML.
       static_cast<WebStateImpl*>(web_state)->SetContentsMimeType("text/html");
     }
+    void WebStateDestroyed(WebState* web_state) override {
+      DCHECK_EQ(web_state_, web_state);
+      web_state_->RemoveObserver(this);
+      web_state_ = nullptr;
+    }
+
+   private:
+    WebState* web_state_;
+
+    DISALLOW_COPY_AND_ASSIGN(MimeTypeUpdater);
   };
   MimeTypeUpdater mime_type_updater(web_state());
 
