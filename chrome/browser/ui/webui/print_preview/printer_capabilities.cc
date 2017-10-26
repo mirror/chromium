d0dc60fe34ef3004cac0bd9e1f844233b557a759
jdoerrie <jdoerrie@chromium.org>
2017-10-26 11:59:45 +0200

    Add DCHECKs to base::Value Path API
    This change adds DCHECKs to the base::Value Path API to prevent mis-use.
    Ideally this checks should be static asserts, but this is currently not
    easily do-able.
    
    Bug: 646113
    Change-Id: I04700486d41a425a1b75c0fdd93f21da7009e05a

--- chrome/browser/ui/webui/print_preview/printer_capabilities.cc
+++ chrome/browser/ui/webui/print_preview/printer_capabilities.cc
@@ -229,13 +229,13 @@ std::unique_ptr<base::DictionaryValue> ValidateCddForPrintPreview(
     const base::DictionaryValue& cdd) {
   auto validated_cdd =
       base::DictionaryValue::From(base::Value::ToUniquePtrValue(cdd.Clone()));
-  const base::Value* caps = cdd.FindPath({kPrinter});
+  const base::Value* caps = cdd.FindKey(kPrinter);
   if (!caps || !caps->is_dict())
     return validated_cdd;
-  validated_cdd->RemovePath({kPrinter});
+  validated_cdd->RemoveKey(kPrinter);
   auto out_caps = std::make_unique<base::DictionaryValue>();
   for (const auto& capability : caps->DictItems()) {
-    const auto& path = capability.first;
+    const auto& key = capability.first;
     const base::Value& value = capability.second;
 
     const base::Value* list = nullptr;
@@ -244,11 +244,11 @@ std::unique_ptr<base::DictionaryValue> ValidateCddForPrintPreview(
     else if (value.is_list())
       list = &value;
     if (!list) {
-      out_caps->SetPath({path}, value.Clone());
+      out_caps->SetKey(key, value.Clone());
       continue;
     }
 
-    bool is_vendor_capability = path == kVendorCapabilityKey;
+    bool is_vendor_capability = key == kVendorCapabilityKey;
     base::Value out_list = GetFilteredList(
         list, is_vendor_capability ? VendorCapabilityInvalid : ValueIsNull);
     if (out_list.GetList().empty())  // leave out empty lists.
@@ -264,17 +264,17 @@ std::unique_ptr<base::DictionaryValue> ValidateCddForPrintPreview(
         base::Value* options_dict = vendor_option.FindPathOfType(
             {kSelectCapKey}, base::Value::Type::DICTIONARY);
         const base::Value* options_list =
-            options_dict->FindPathOfType({kOptionKey}, base::Value::Type::LIST);
-        options_dict->SetPath({kOptionKey},
-                              GetFilteredList(options_list, ValueIsNull));
+            options_dict->FindKeyOfType(kOptionKey, base::Value::Type::LIST);
+        options_dict->SetKey(kOptionKey,
+                             GetFilteredList(options_list, ValueIsNull));
       }
     }
     if (value.is_dict()) {
       base::Value option_dict(base::Value::Type::DICTIONARY);
       option_dict.SetKey(kOptionKey, std::move(out_list));
-      out_caps->SetKey(path, std::move(option_dict));
+      out_caps->SetKey(key, std::move(option_dict));
     } else {
-      out_caps->SetPath({path}, std::move(out_list));
+      out_caps->SetKey(key, std::move(out_list));
     }
   }
   validated_cdd->SetDictionary(kPrinter, std::move(out_caps));
