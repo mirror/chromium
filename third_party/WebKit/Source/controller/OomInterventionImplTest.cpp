8bf24992a637c3aa12e0b38c5eea49b2de816a27
Kenichi Ishibashi <bashi@chromium.org>
2017-10-26 14:50:40 +0900

    Add OomIntervention mojo interface and its implementation
    This CL adds a mojo interface and its implementation for
    OOM-intervention. BlinkInitializer adds the interface to the
    main frame's InterfaceRegistry. The impl class pauses pages
    when it receives OnNearOomDetected() message and resumes pages
    when it receives OnInterventionDeclined() message. Clients of
    the interface doesn't exist yet except for tests. Follow-up CLs
    will add a client in the browser process.
    
    Bug: 776665
    Change-Id: Ic7be386b9637ddb90a6c25bb5605a584f08e9e8b

---
+++ third_party/WebKit/Source/controller/OomInterventionImplTest.cpp
@@ -0,0 +1,48 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "controller/OomInterventionImpl.h"
+
+#include "core/exported/WebViewImpl.h"
+#include "core/frame/FrameTestHelpers.h"
+#include "core/frame/LocalFrame.h"
+#include "core/frame/WebLocalFrameImpl.h"
+#include "core/page/Page.h"
+#include "core/testing/DummyPageHolder.h"
+#include "services/service_manager/public/cpp/interface_provider.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace blink {
+
+namespace {
+
+void CheckStatus(mojom::blink::OomInterventionStatus expected,
+                 mojom::blink::OomInterventionStatus actual) {
+  EXPECT_EQ(expected, actual);
+}
+
+}  // namespace
+
+class OomInterventionImplTest : public ::testing::Test {
+ protected:
+  FrameTestHelpers::WebViewHelper web_view_helper_;
+};
+
+TEST_F(OomInterventionImplTest, DetectedAndDeclined) {
+  WebViewImpl* web_view = web_view_helper_.InitializeAndLoad("about::blank");
+  Page* page = web_view->MainFrameImpl()->GetFrame()->GetPage();
+  EXPECT_FALSE(page->Paused());
+
+  OomInterventionImpl intervention;
+
+  intervention.OnNearOomDetected(ConvertToBaseCallback(
+      WTF::Bind(&CheckStatus, mojom::blink::OomInterventionStatus::ACTIVE)));
+  EXPECT_TRUE(page->Paused());
+
+  intervention.OnInterventionDeclined(ConvertToBaseCallback(
+      WTF::Bind(&CheckStatus, mojom::blink::OomInterventionStatus::INACTIVE)));
+  EXPECT_FALSE(page->Paused());
+}
+
+}  // namespace blink
