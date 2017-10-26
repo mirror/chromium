1975eefd8e6fb0162ab35a4c584423b27ab69974
Xiaocheng Hu <xiaochengh@chromium.org>
2017-10-26 15:22:24 -0700

    [LayoutNG] Change Start/EndOfNext/LastNonCollapsedCharacter to take Position
    This patch changes the APIs to take Position parameters to better support
    mapping non-text anchored positions to LayoutNG canonical text.
    
    To better cope with the modified APIs, this patch also adds a helper method
    LayoutText::CaretOffsetForPosition() for converting Position back to caret
    offset. It also helps consolidating overrides of CaretMin/MaxOffset in
    LayoutTextFragment, by overriding CaretOffsetForPosition() instead.
    
    Bug: 699017
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Iff7e4e3388e70e8ee7d8412f7d62a829ebc9efb7

--- third_party/WebKit/Source/core/layout/LayoutText.h
+++ third_party/WebKit/Source/core/layout/LayoutText.h
@@ -206,6 +206,10 @@ class CORE_EXPORT LayoutText : public LayoutObject {
   // TODO(layout-dev): Fix it when text-transform changes text length.
   virtual Position PositionForCaretOffset(unsigned) const;
 
+  // Returns the offset in the |text_| string that corresponds to the given
+  // position in DOM; Returns nullopt is the position is not in this LayoutText.
+  virtual Optional<unsigned> CaretOffsetForPosition(const Position&) const;
+
   // Returns true if the offset (0-based in the |text_| string) is next to a
   // non-collapsed non-linebreak character, or before a forced linebreak (<br>,
   // or segment break in node with style white-space: pre/pre-line/pre-wrap).
