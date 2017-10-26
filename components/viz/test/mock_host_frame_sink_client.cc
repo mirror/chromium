ab07c12716794e5f5aafcc1b2b0276880c1f0428
kylechar <kylechar@chromium.org>
2017-10-26 10:24:02 -0400

    Verify FrameSinkIds registered.
    Add a DCHECK to enforce that FrameSinkIds are registered before doing
    anything with them.
    
    Bug:
    Change-Id: Ie197b655ecb1b1eabb5da56ca92fd590e2dfa450

---
+++ components/viz/test/mock_host_frame_sink_client.cc
@@ -0,0 +1,13 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/viz/test/mock_host_frame_sink_client.h"
+
+namespace viz {
+
+MockHostFrameSinkClient::MockHostFrameSinkClient() = default;
+
+MockHostFrameSinkClient::~MockHostFrameSinkClient() = default;
+
+}  // namespace viz
