eec9394a454a5e7fc25c750a33801022b47ccff0
Sidney San Martín <sdy@chromium.org>
2017-10-26 14:55:08 -0400

    [Mac] Use the browser window for layer detachment in fullscreen (FSLP).
    In the past, Chrome has use a secondary NSWindow to show fullscreen
    video in a way that lets macOS "detach" the video and use much less
    power for graphics than it would otherwise. There are currently a couple
    of bugs in this code and it's disabled for High Sierra.
    
    From discussions with Apple, it looks like the browser window itself
    wasn't being considered for detachment because of the status bubble
    window which is always on screen, just hidden, because if a Chrome
    window was in another Space, the user would be switched to that space if
    the status bubble showed itself in response to network activity
    (crbug/31821). However, this no longer seems to happen on macOS 10.9+,
    so it should be safe to orderOut: the status bubble when it's not in
    use.
    
    This change always |Detach()|es the status bubble when it's hidden and
    removes the FSLP coordinator and window.
    
    Bug: 742691, 644133
    Change-Id: Ia59ca2d2541e1484efaf8e7982d9f015a448e080

--- ui/accelerated_widget_mac/accelerated_widget_mac.mm
+++ ui/accelerated_widget_mac/accelerated_widget_mac.mm
@@ -13,7 +13,6 @@
 #include "base/message_loop/message_loop.h"
 #include "base/trace_event/trace_event.h"
 #include "third_party/skia/include/core/SkCanvas.h"
-#include "ui/accelerated_widget_mac/fullscreen_low_power_coordinator.h"
 #include "ui/base/cocoa/animation_utils.h"
 #include "ui/gfx/geometry/dip_util.h"
 #include "ui/gl/scoped_cgl.h"
@@ -38,19 +37,12 @@ AcceleratedWidgetMac::AcceleratedWidgetMac() : view_(nullptr) {
   // Disable the fade-in animation as the layers are added.
   ScopedCAActionDisabler disabler;
 
-  // Add a flipped transparent layer as a child, so that we don't need to
-  // fiddle with the position of sub-layers -- they will always be at the
-  // origin.
+  // Add a flipped layer so that we don't need to fiddle with the position of
+  // sub-layers — they will always be at the origin.
   flipped_layer_.reset([[CALayer alloc] init]);
   [flipped_layer_ setGeometryFlipped:YES];
   [flipped_layer_ setAnchorPoint:CGPointMake(0, 0)];
   [flipped_layer_
-      setAutoresizingMask:kCALayerWidthSizable|kCALayerHeightSizable];
-
-  fslp_flipped_layer_.reset([[CALayer alloc] init]);
-  [fslp_flipped_layer_ setGeometryFlipped:YES];
-  [fslp_flipped_layer_ setAnchorPoint:CGPointMake(0, 0)];
-  [fslp_flipped_layer_
       setAutoresizingMask:kCALayerWidthSizable|kCALayerHeightSizable];
 
   // Use a sequence number as the accelerated widget handle that we can use
@@ -72,14 +64,12 @@ void AcceleratedWidgetMac::SetNSView(AcceleratedWidgetMacNSView* view) {
   // Disable the fade-in animation as the view is added.
   ScopedCAActionDisabler disabler;
 
-  DCHECK(!fslp_coordinator_);
   DCHECK(view && !view_);
   view_ = view;
 
   CALayer* background_layer = [view_->AcceleratedWidgetGetNSView() layer];
   DCHECK(background_layer);
   [flipped_layer_ setBounds:[background_layer bounds]];
-  [fslp_flipped_layer_ setBounds:[background_layer bounds]];
   [background_layer addSublayer:flipped_layer_];
 }
 
@@ -87,42 +77,14 @@ void AcceleratedWidgetMac::ResetNSView() {
   if (!view_)
     return;
 
-  if (fslp_coordinator_) {
-    fslp_coordinator_->WillLoseAcceleratedWidget();
-    DCHECK(!fslp_coordinator_);
-  }
-
   // Disable the fade-out animation as the view is removed.
   ScopedCAActionDisabler disabler;
 
+  [flipped_layer_ setSublayers:nil];
   [flipped_layer_ removeFromSuperlayer];
-  [content_layer_ removeFromSuperlayer];
-  [io_surface_layer_ removeFromSuperlayer];
-  content_layer_.reset();
-  io_surface_layer_.reset();
 
   last_swap_size_dip_ = gfx::Size();
   view_ = NULL;
-}
-
-void AcceleratedWidgetMac::SetFullscreenLowPowerCoordinator(
-    FullscreenLowPowerCoordinator* coordinator) {
-  DCHECK(coordinator);
-  DCHECK(!fslp_coordinator_);
-  fslp_coordinator_ = coordinator;
-}
-
-void AcceleratedWidgetMac::ResetFullscreenLowPowerCoordinator() {
-  DCHECK(fslp_coordinator_);
-  fslp_coordinator_ = nullptr;
-}
-
-CALayer* AcceleratedWidgetMac::GetFullscreenLowPowerLayer() const {
-  return fslp_flipped_layer_;
-}
-
-bool AcceleratedWidgetMac::MightBeInFullscreenLowPowerMode() const {
-  return fslp_coordinator_;
 }
 
 bool AcceleratedWidgetMac::HasFrameOfSize(
@@ -164,33 +126,21 @@ void AcceleratedWidgetMac::GotCALayerFrame(
   }
   ScopedCAActionDisabler disabler;
   last_swap_size_dip_ = gfx::ConvertSizeToDIP(scale_factor, pixel_size);
-  if (fullscreen_low_power_layer_valid)
-    [fslp_flipped_layer_ setFrame:gfx::Rect(last_swap_size_dip_).ToCGRect()];
+  CALayer* flipped_layer = flipped_layer_.get();
 
-  // Ensure that the content is in the CALayer hierarchy, and update fullscreen
-  // low power state.
-  if (content_layer_ != content_layer) {
-    [flipped_layer_ addSublayer:content_layer];
-    [content_layer_ removeFromSuperlayer];
-    content_layer_ = content_layer;
-  }
-  if (fullscreen_low_power_layer_ != fullscreen_low_power_layer) {
-    if (fslp_coordinator_) {
-      fslp_coordinator_->WillLoseAcceleratedWidget();
-      DCHECK(!fslp_coordinator_);
+  if (fullscreen_low_power_layer_valid) {
+    if (![fullscreen_low_power_layer superlayer]) {
+      flipped_layer.backgroundColor = CGColorGetConstantColor(kCGColorBlack);
+      flipped_layer.sublayers = @[ fullscreen_low_power_layer ];
     }
-    [fslp_flipped_layer_ addSublayer:fullscreen_low_power_layer];
-    [fullscreen_low_power_layer_ removeFromSuperlayer];
-    fullscreen_low_power_layer_ = fullscreen_low_power_layer;
+  } else {
+    if (![content_layer superlayer]) {
+      flipped_layer.backgroundColor = nil;
+      flipped_layer.sublayers = @[ content_layer ];
+    }
   }
-  if (fslp_coordinator_)
-    fslp_coordinator_->SetLowPowerLayerValid(fullscreen_low_power_layer_valid);
 
-  // Ensure the IOSurface is removed.
-  if (io_surface_layer_) {
-    [io_surface_layer_ removeFromSuperlayer];
-    io_surface_layer_.reset();
-  }
+  io_surface_layer_.reset();
   view_->AcceleratedWidgetSwapCompleted();
 }
 
@@ -211,7 +161,8 @@ void AcceleratedWidgetMac::GotIOSurfaceFrame(
     io_surface_layer_.reset([[CALayer alloc] init]);
     [io_surface_layer_ setContentsGravity:kCAGravityTopLeft];
     [io_surface_layer_ setAnchorPoint:CGPointMake(0, 0)];
-    [flipped_layer_ addSublayer:io_surface_layer_];
+    [flipped_layer_ setBackgroundColor:nil];
+    [flipped_layer_ setSublayers:@[ io_surface_layer_ ]];
   }
   id new_contents = static_cast<id>(io_surface.get());
   if (new_contents && new_contents == [io_surface_layer_ contents])
@@ -223,11 +174,6 @@ void AcceleratedWidgetMac::GotIOSurfaceFrame(
   if ([io_surface_layer_ contentsScale] != scale_factor)
     [io_surface_layer_ setContentsScale:scale_factor];
 
-  // Ensure that the content layer is removed.
-  if (content_layer_) {
-    [content_layer_ removeFromSuperlayer];
-    content_layer_.reset();
-  }
   view_->AcceleratedWidgetSwapCompleted();
 }
 
