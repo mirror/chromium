92e47ec1f56095ff6004db5343406a6897285043
Ran Ji <ranj@chromium.org>
2017-10-27 14:44:01 -0400

    Add build arg "report_java_assert" to report java assert failure
    If this arg is set to true, Chrome will report java assertion
    failures (without crashing).
    
    Bug:672945
    Change-Id: I671ad4a3ef3a66d3a50005552aeb2a37f56fef22

--- build/android/buildhooks/java/org/chromium/build/BuildHooks.java
+++ build/android/buildhooks/java/org/chromium/build/BuildHooks.java
@@ -8,10 +8,27 @@ package org.chromium.build;
  * All Java targets that support android have dependence on this class.
  */
 public abstract class BuildHooks {
+    private static Callback<AssertionError> sAssertCallback;
+
     /**
      * This method is inserted to handle any assert failure by java_assertion_enabler.
      */
     public static void assertFailureHandler(AssertionError assertionError) {
-        throw assertionError;
+        if (sAssertCallback != null) {
+            sAssertCallback.run(assertionError);
+        } else {
+            //  It is vital that when assertions are enabled on a Canary, that we set this handler
+            //  first thing, and that we don't ship any other apks that don't set the callback in
+            //  their application classes (e.g. webview)
+            throw assertionError;
+        }
+    }
+
+    /**
+     * Set the callback function that handles assert failure.
+     * This should be called from attachBaseContext.
+     */
+    public static void setAssertCallback(Callback<AssertionError> callback) {
+        sAssertCallback = callback;
     }
 }
