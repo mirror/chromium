b64869425a4e92d2c97ed48e13e7a0f6dccf2ef8
Tsuyoshi Horo <horo@chromium.org>
2017-10-27 13:25:10 +0900

    [POC] Create a full V8 code cache in ServiceWorker's install event handler.
    Bug: 768705
    Change-Id: I0b64a5c545980cb49405ab5a60217dda0ac48c3a

--- third_party/WebKit/LayoutTests/http/tests/devtools/service-workers/resources/v8-cache-worker.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/service-workers/resources/v8-cache-worker.js
@@ -1,13 +1,17 @@
 const cacheName = 'c8-cache-test';
 const scriptName = 'v8-cache-script.js';
 
+let continue_install;
+
 self.addEventListener('install', (event) => {
     event.waitUntil(
-      caches.open(cacheName)
-        .then((cache) => {
-          return cache.add(new Request(scriptName));
-        })
-    );
+      new Promise((resolve) => { continue_install = resolve; })
+        .then(() => caches.open(cacheName))
+        .then((cache) => cache.add(new Request(scriptName))));
+  });
+
+self.addEventListener('message', (event) => {
+    continue_install();
   });
 
 self.addEventListener('fetch', (event) => {
