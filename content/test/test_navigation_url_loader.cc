d423173ba68e6304e60475698e051efbddaf5ceb
Kinuko Yasuda <kinuko@chromium.org>
2017-10-27 11:30:37 +0900

    Make navigation request handlers pass subresource loader params as a struct
    ServiceWorker wants to pass several params for subresource-loader setup
    in the renderer that are not URLLoaderFactory, because it needs to create
    URLLoaderFactory in the renderer. Currently we pass them via a
    separate Mojo call but it causes racy situation and makes tests flaky.
    
    This change introduces SubresourceLoaderParams struct (that has
    URLLoaderFactoryInfo as a member) and pass it along from navigation request
    handlers instead of URLLoaderFactoryInfo.
    
    This CL also slightly changes how WebUI subresource loader factory is passed;
    to make it share more code with AppCache etc.
    
    Bug: 778898
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Iafe0e2f7bd8f6c500c91bde3c46dff3d4d7dd8c8

--- content/test/test_navigation_url_loader.cc
+++ content/test/test_navigation_url_loader.cc
@@ -75,10 +75,10 @@ void TestNavigationURLLoader::CallOnResponseStarted(
           ->GetProcess()
           ->GetID();
   GlobalRequestID global_id(child_id, ++request_id);
-  delegate_->OnResponseStarted(
-      response, std::move(body), mojo::ScopedDataPipeConsumerHandle(),
-      SSLStatus(), std::move(navigation_data), global_id, false, false,
-      mojom::URLLoaderFactoryPtrInfo());
+  delegate_->OnResponseStarted(response, std::move(body),
+                               mojo::ScopedDataPipeConsumerHandle(),
+                               SSLStatus(), std::move(navigation_data),
+                               global_id, false, false, base::nullopt);
 }
 
 TestNavigationURLLoader::~TestNavigationURLLoader() {}
