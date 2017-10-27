01b521e8b6985ac10ce87bce899d755da4288c28
Maks Orlovich <morlovich@chromium.org>
2017-10-27 13:44:12 -0400

    Working on Vary;* changes.
    Right now this just add tests for Vary:* coverage.
    
    One of them currently fails since we incorrectly use Last-Modified
    for validation there.
    
    Bug: 778681
    Change-Id: Iccf7fa950eb838b3cc0a2c5f6f8f7d1e26e831f7

--- net/http/http_vary_data.cc
+++ net/http/http_vary_data.cc
@@ -15,28 +15,30 @@
 
 namespace net {
 
-HttpVaryData::HttpVaryData() : is_valid_(false) {
-}
+HttpVaryData::HttpVaryData() : vary_status_(VARY_NONE) {}
 
 bool HttpVaryData::Init(const HttpRequestInfo& request_info,
                         const HttpResponseHeaders& response_headers) {
   base::MD5Context ctx;
   base::MD5Init(&ctx);
 
-  is_valid_ = false;
+  vary_status_ = VARY_NONE;
   bool processed_header = false;
 
   // Feed the MD5 context in the order of the Vary header enumeration.  If the
   // Vary header repeats a header name, then that's OK.
   //
-  // If the Vary header contains '*' then we should not construct any vary data
-  // since it is all usurped by a '*'.  See section 13.6 of RFC 2616.
+  // If the Vary header contains '*' then we can just summarize it as VARY_STAR
+  // and not have to worry about the specific headers.  See section 13.6 of
+  // RFC 2616.
   //
   size_t iter = 0;
   std::string name = "vary", request_header;
   while (response_headers.EnumerateHeader(&iter, name, &request_header)) {
-    if (request_header == "*")
-      return false;
+    if (request_header == "*") {
+      vary_status_ = VARY_STAR;
+      return true;
+    }
     AddField(request_info, request_header, &ctx);
     processed_header = true;
   }
@@ -45,33 +47,60 @@ bool HttpVaryData::Init(const HttpRequestInfo& request_info,
     return false;
 
   base::MD5Final(&request_digest_, &ctx);
-  return is_valid_ = true;
+  vary_status_ = VARY_SPECIFIC;
+  return true;
 }
 
-bool HttpVaryData::InitFromPickle(base::PickleIterator* iter) {
-  is_valid_ = false;
+bool HttpVaryData::InitFromPickle(base::PickleIterator* iter,
+                                  PickleType pickle_type) {
+  vary_status_ = VARY_NONE;
+
   const char* data;
+  if (pickle_type != PICKLETYPE_HISTORICAL) {
+    if (!iter->ReadBytes(&data, 1))
+      return false;
+    // Should be in range, and not be VARY_NONE.
+    if (*data <= VARY_NONE || *data > LAST_VARY_STATUS)
+      return false;
+    vary_status_ = static_cast<VaryStatus>(*data);
+    if (vary_status_ == VARY_STAR)
+      return true;
+  }
+
   if (iter->ReadBytes(&data, sizeof(request_digest_))) {
+    vary_status_ = VARY_SPECIFIC;
     memcpy(&request_digest_, data, sizeof(request_digest_));
-    return is_valid_ = true;
+    return true;
   }
   return false;
 }
 
 void HttpVaryData::Persist(base::Pickle* pickle) const {
-  DCHECK(is_valid());
-  pickle->WriteBytes(&request_digest_, sizeof(request_digest_));
+  DCHECK_NE(vary_status_, VARY_NONE);
+  char status_for_pickling = static_cast<char>(vary_status_);
+  pickle->WriteBytes(&status_for_pickling, 1);
+  if (vary_status_ != VARY_STAR)
+    pickle->WriteBytes(&request_digest_, sizeof(request_digest_));
 }
 
 bool HttpVaryData::MatchesRequest(
     const HttpRequestInfo& request_info,
     const HttpResponseHeaders& cached_response_headers) const {
+  if (vary_status() == VARY_STAR)
+    return false;
+
   HttpVaryData new_vary_data;
   if (!new_vary_data.Init(request_info, cached_response_headers)) {
     // This case can happen if |this| was loaded from a cache that was populated
     // by a build before crbug.com/469675 was fixed.
     return false;
   }
+
+  // If the cached entry was written before we supported Vary:* here,
+  // new_vary_data may be the only thing aware of it.
+  if (new_vary_data.vary_status() == VARY_STAR)
+    return false;
+
   return memcmp(&new_vary_data.request_digest_, &request_digest_,
                 sizeof(request_digest_)) == 0;
 }
