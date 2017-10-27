874eb275d9031de401db2c841578191e92d0db39
Peter Beverloo <peter@chromium.org>
2017-10-27 18:15:24 +0100

    [NOT FOR COMMIT] NotificationData stuff
    Change-Id: I61bffe7a357b206bdcf654e4ec5dc2004b993b16

--- chrome/browser/notifications/notification_platform_bridge_win.cc
+++ chrome/browser/notifications/notification_platform_bridge_win.cc
@@ -45,32 +45,6 @@ typedef winfoundtn::ITypedEventHandler<
     winui::Notifications::ToastDismissedEventArgs*>
     ToastDismissedHandler;
 
-// Hold related data for a notification.
-struct NotificationData {
-  NotificationData(NotificationCommon::Type notification_type,
-                   const std::string& notification_id,
-                   const std::string& profile_id,
-                   bool incognito,
-                   const GURL& origin_url)
-      : notification_type(notification_type),
-        notification_id(notification_id),
-        profile_id(profile_id),
-        incognito(incognito),
-        origin_url(origin_url) {}
-
-  // Same parameters used by NotificationPlatformBridge::Display().
-  NotificationCommon::Type notification_type;
-  const std::string notification_id;
-  const std::string profile_id;
-  const bool incognito;
-
-  // A copy of the origin_url from the underlying message_center::Notification.
-  // Used to pass back to NotificationDisplayService.
-  const GURL origin_url;
-
-  DISALLOW_COPY_AND_ASSIGN(NotificationData);
-};
-
 namespace {
 
 // Templated wrapper for winfoundtn::GetActivationFactory().
@@ -143,57 +117,6 @@ HRESULT GetToastNotification(
   return S_OK;
 }
 
-// Perform |operation| on a notification once the profile has been loaded.
-void ProfileLoadedCallback(NotificationCommon::Operation operation,
-                           NotificationCommon::Type notification_type,
-                           const std::string& origin,
-                           const std::string& notification_id,
-                           const base::Optional<int>& action_index,
-                           const base::Optional<base::string16>& reply,
-                           const base::Optional<bool>& by_user,
-                           Profile* profile) {
-  if (!profile)
-    return;
-
-  auto* display_service =
-      NotificationDisplayServiceFactory::GetForProfile(profile);
-  display_service->ProcessNotificationOperation(operation, notification_type,
-                                                origin, notification_id,
-                                                action_index, reply, by_user);
-}
-
-void ForwardNotificationOperationOnUiThread(
-    NotificationCommon::Operation operation,
-    NotificationCommon::Type notification_type,
-    const std::string& origin,
-    const std::string& notification_id,
-    const std::string& profile_id,
-    bool incognito) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  if (!g_browser_process)
-    return;
-
-  g_browser_process->profile_manager()->LoadProfile(
-      profile_id, incognito,
-      base::Bind(&ProfileLoadedCallback, operation, notification_type, origin,
-                 notification_id, base::nullopt /*action_index*/,
-                 base::nullopt /*reply*/, base::nullopt /*by_user*/));
-}
-
-void ForwardNotificationOperation(NotificationData* data,
-                                  NotificationCommon::Operation operation) {
-  if (!data)
-    return;
-
-  content::BrowserThread::PostTask(
-      content::BrowserThread::UI, FROM_HERE,
-      base::Bind(&ForwardNotificationOperationOnUiThread, operation,
-                 data->notification_type, data->origin_url.spec(),
-                 data->notification_id, data->profile_id, data->incognito));
-}
-
-}  // namespace
-
 // static
 NotificationPlatformBridge* NotificationPlatformBridge::Create() {
   return new NotificationPlatformBridgeWin();
@@ -215,14 +138,6 @@ void NotificationPlatformBridgeWin::Display(
     std::unique_ptr<NotificationCommon::Metadata> metadata) {
   // TODO(finnur): Move this to a RoInitialized thread, as per crbug.com/761039.
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-
-  NotificationData* data = FindNotificationData(
-      notification_id, profile_id, notification.origin_url(), incognito);
-  if (!data) {
-    data = new NotificationData(notification_type, notification_id, profile_id,
-                                incognito, notification.origin_url());
-    notifications_.emplace(data, base::WrapUnique(data));
-  }
 
   mswr::ComPtr<winui::Notifications::IToastNotificationManagerStatics>
       toast_manager;
@@ -306,16 +221,6 @@ HRESULT NotificationPlatformBridgeWin::OnActivated(
     IInspectable* /* inspectable */) {
   // TODO(chengx): We need to write profile id and incognito information into
   // the toast, so that we can retrieve them from |notification| here.
-  std::string notification_id = "";
-  std::string profile_id = "";
-  bool incognito = false;
-  GURL origin_url;
-
-  NotificationData* data =
-      FindNotificationData(notification_id, profile_id, origin_url, incognito);
-  if (data)
-    ForwardNotificationOperation(data, NotificationCommon::CLICK);
-
   return S_OK;
 }
 
@@ -324,34 +229,5 @@ HRESULT NotificationPlatformBridgeWin::OnDismissed(
     winui::Notifications::IToastDismissedEventArgs* /* args */) {
   // TODO(chengx): We need to write profile_id and incognito information into
   // the toast, so that we can retrieve them from |notification| here.
-  std::string notification_id = "";
-  std::string profile_id = "";
-  bool incognito = false;
-  GURL origin_url;
-
-  NotificationData* data =
-      FindNotificationData(notification_id, profile_id, origin_url, incognito);
-  if (data) {
-    ForwardNotificationOperation(data, NotificationCommon::CLOSE);
-    notifications_.erase(data);
-  }
-
   return S_OK;
-}
-
-NotificationData* NotificationPlatformBridgeWin::FindNotificationData(
-    const std::string& notification_id,
-    const std::string& profile_id,
-    const GURL& origin_url,
-    bool incognito) {
-  for (const auto& item : notifications_) {
-    NotificationData* data = item.first;
-    if (data->notification_id == notification_id &&
-        data->profile_id == profile_id && data->origin_url == origin_url &&
-        data->incognito == incognito) {
-      return data;
-    }
-  }
-
-  return nullptr;
 }
--- chrome/browser/notifications/notification_platform_bridge_win.h
+++ chrome/browser/notifications/notification_platform_bridge_win.h
@@ -6,14 +6,9 @@
 #define CHROME_BROWSER_NOTIFICATIONS_NOTIFICATION_PLATFORM_BRIDGE_WIN_H_
 
 #include <windows.ui.notifications.h>
-#include <string>
-#include <unordered_map>
 
 #include "base/macros.h"
 #include "chrome/browser/notifications/notification_platform_bridge.h"
-#include "url/gurl.h"
-
-struct NotificationData;
 
 // Implementation of the NotificationPlatformBridge for Windows 10 Anniversary
 // Edition and beyond, delegating display of notifications to the Action Center.
@@ -46,24 +41,8 @@ class NotificationPlatformBridgeWin : public NotificationPlatformBridge {
       ABI::Windows::UI::Notifications::IToastNotification* notification,
       ABI::Windows::UI::Notifications::IToastDismissedEventArgs* args);
 
-  // Returns a notification with properties |notification_id|, |profile_id|,
-  // |origin_url| and |incognito| if found in notifications_. Returns nullptr if
-  // not found.
-  NotificationData* FindNotificationData(const std::string& notification_id,
-                                         const std::string& profile_id,
-                                         const GURL& origin_url,
-                                         bool incognito);
-
   // Whether the required functions from combase.dll have been loaded.
   bool com_functions_initialized_;
-
-  // Stores the set of Notifications in a session.
-  // A std::set<std::unique_ptr<T>> doesn't work well because e.g.,
-  // std::set::erase(T) would require a std::unique_ptr<T> argument, so the data
-  // would get double-destructed.
-  template <typename T>
-  using UnorderedUniqueSet = std::unordered_map<T*, std::unique_ptr<T>>;
-  UnorderedUniqueSet<NotificationData> notifications_;
 
   DISALLOW_COPY_AND_ASSIGN(NotificationPlatformBridgeWin);
 };
