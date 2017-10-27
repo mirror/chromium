1bc0aabac023ce3d621754f76bb54581160a41b8
Kevin Cernekee <cernekee@chromium.org>
2017-10-27 16:30:08 -0700

    Revert "Hide ARC VPN behind a feature flag"
    This reverts commit b7f96a5e6d6b07dde77de608e680d6c51658e4b0.
    The ARC VPN feature will go live in M64.
    
    BUG=696865
    TEST=manually verify that the feature still works
    
    Change-Id: I28da4ca60d1f08a8e830a42210d9289b9683d538

--- components/arc/arc_features.h
+++ components/arc/arc_features.h
@@ -14,7 +14,6 @@ namespace arc {
 // Please keep alphabetized.
 extern const base::Feature kBootCompletedBroadcastFeature;
 extern const base::Feature kNativeBridgeExperimentFeature;
-extern const base::Feature kVpnFeature;
 
 }  // namespace arc
 
