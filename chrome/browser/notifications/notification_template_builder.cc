7ea4ec66121c60ce9d6b98e3296d29d1c310832a
Finnur Thorarinsson <finnur@chromium.org>
2017-10-27 15:02:06 +0000

    Win10 Native Notifications: Support images.
    Introduce a class that supports hanging on to temporary
    files long enough for the OS to pick them up (and clean
    them up after restart of Chrome if it fails the first
    time around). Necessary because Windows doesn't
    guarantee temp file deletion on shutdown/restart. 
    
    Bug: 734095
    Change-Id: I2926dc33b489f150a75f97792266e1a31a4ea5a2

--- chrome/browser/notifications/notification_template_builder.cc
+++ chrome/browser/notifications/notification_template_builder.cc
@@ -4,13 +4,16 @@
 
 #include "chrome/browser/notifications/notification_template_builder.h"
 
+#include "base/files/file_path.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/notifications/temp_file_keeper.h"
 #include "components/url_formatter/elide_url.h"
 #include "third_party/libxml/chromium/libxml_utils.h"
 #include "ui/message_center/notification.h"
+#include "url/gurl.h"
 #include "url/origin.h"
 
 namespace {
@@ -26,17 +29,29 @@ const char kBindingElementTemplateAttribute[] = "template";
 const char kButtonIndex[] = "buttonIndex=";
 const char kContent[] = "content";
 const char kForeground[] = "foreground";
+const char kHero[] = "hero";
+const char kHintCrop[] = "hint-crop";
+const char kHintCropCircle[] = "circle";
+const char kImageElement[] = "image";
+const char kImageUri[] = "imageUri";
 const char kInputElement[] = "input";
 const char kInputId[] = "id";
 const char kInputType[] = "type";
 const char kPlaceholderContent[] = "placeHolderContent";
 const char kPlacement[] = "placement";
+const char kPlacementAppLogoOverride[] = "appLogoOverride";
+const char kSrc[] = "src";
 const char kText[] = "text";
-const char kUserResponse[] = "userResponse";
 const char kTextElement[] = "text";
 const char kToastElement[] = "toast";
 const char kToastElementLaunchAttribute[] = "launch";
+const char kUserResponse[] = "userResponse";
 const char kVisualElement[] = "visual";
+
+// Prefixes for filenames.
+const wchar_t kPrefixImage[] = L"Image_";
+const wchar_t kPrefixButton[] = L"Button_";
+const wchar_t kPrefixIcon[] = L"Icon_";
 
 // Name of the template used for default Chrome notifications.
 const char kDefaultTemplate[] = "ToastGeneric";
@@ -49,6 +64,7 @@ const char kXmlVersionHeader[] = "<?xml version=\"1.0\"?>\n";
 // static
 std::unique_ptr<NotificationTemplateBuilder> NotificationTemplateBuilder::Build(
     const std::string& notification_id,
+    TempFileKeeper& temp_file_keeper,
     const message_center::Notification& notification) {
   std::unique_ptr<NotificationTemplateBuilder> builder =
       base::WrapUnique(new NotificationTemplateBuilder);
@@ -62,18 +78,19 @@ std::unique_ptr<NotificationTemplateBuilder> NotificationTemplateBuilder::Build(
   builder->StartBindingElement(kDefaultTemplate);
 
   // Content for the toast template.
-  builder->WriteTextElement("1", base::UTF16ToUTF8(notification.title()),
+  builder->WriteTextElement(base::UTF16ToUTF8(notification.title()),
                             TextType::NORMAL);
-  builder->WriteTextElement("2", base::UTF16ToUTF8(notification.message()),
+  builder->WriteTextElement(base::UTF16ToUTF8(notification.message()),
                             TextType::NORMAL);
-  builder->WriteTextElement("3",
-                            builder->FormatOrigin(notification.origin_url()),
+  builder->WriteTextElement(builder->FormatOrigin(notification.origin_url()),
                             TextType::ATTRIBUTION);
+  builder->WriteIconElement(notification, temp_file_keeper);
+  builder->WriteImageElement(notification, temp_file_keeper);
 
   builder->EndBindingElement();
   builder->EndVisualElement();
 
-  builder->AddActions(notification.buttons());
+  builder->AddActions(notification, temp_file_keeper);
 
   builder->EndToastElement();
 
@@ -136,8 +153,7 @@ void NotificationTemplateBuilder::EndBindingElement() {
   xml_writer_->EndElement();
 }
 
-void NotificationTemplateBuilder::WriteTextElement(const std::string& id,
-                                                   const std::string& content,
+void NotificationTemplateBuilder::WriteTextElement(const std::string& content,
                                                    TextType text_type) {
   xml_writer_->StartElement(kTextElement);
   if (text_type == TextType::ATTRIBUTION)
@@ -146,8 +162,46 @@ void NotificationTemplateBuilder::WriteTextElement(const std::string& id,
   xml_writer_->EndElement();
 }
 
+void NotificationTemplateBuilder::WriteIconElement(
+    const message_center::Notification& notification,
+    TempFileKeeper& temp_file_keeper) {
+  gfx::Image icon = notification.icon();
+  if (icon.IsEmpty())
+    return;
+
+  base::FilePath path = temp_file_keeper.RegisterTemporaryImage(
+      icon, notification.origin_url(), kPrefixIcon);
+  if (!path.empty()) {
+    xml_writer_->StartElement(kImageElement);
+    xml_writer_->AddAttribute(kPlacement, kPlacementAppLogoOverride);
+    xml_writer_->AddAttribute(kSrc, base::UTF16ToUTF8(path.value()));
+    xml_writer_->AddAttribute(kHintCrop, kHintCropCircle);
+    xml_writer_->EndElement();
+  }
+}
+
+void NotificationTemplateBuilder::WriteImageElement(
+    const message_center::Notification& notification,
+    TempFileKeeper& temp_file_keeper) {
+  gfx::Image image = notification.image();
+  if (image.IsEmpty())
+    return;
+
+  base::FilePath path = temp_file_keeper.RegisterTemporaryImage(
+      image, notification.origin_url(), kPrefixImage);
+  if (!path.empty()) {
+    xml_writer_->StartElement(kImageElement);
+    xml_writer_->AddAttribute(kPlacement, kHero);
+    xml_writer_->AddAttribute(kSrc, base::UTF16ToUTF8(path.value()));
+    xml_writer_->EndElement();
+  }
+}
+
 void NotificationTemplateBuilder::AddActions(
-    const std::vector<message_center::ButtonInfo>& buttons) {
+    const message_center::Notification& notification,
+    TempFileKeeper& temp_file_keeper) {
+  const std::vector<message_center::ButtonInfo>& buttons =
+      notification.buttons();
   if (!buttons.size())
     return;
 
@@ -174,7 +228,7 @@ void NotificationTemplateBuilder::AddActions(
 
   for (size_t i = 0; i < buttons.size(); ++i) {
     const auto& button = buttons[i];
-    WriteActionElement(button, i);
+    WriteActionElement(button, i, notification.origin_url(), temp_file_keeper);
   }
 
   EndActionsElement();
@@ -190,13 +244,22 @@ void NotificationTemplateBuilder::EndActionsElement() {
 
 void NotificationTemplateBuilder::WriteActionElement(
     const message_center::ButtonInfo& button,
-    int index) {
-  // TODO(finnur): Implement button images (imageUri).
-
+    int index,
+    const GURL& origin,
+    TempFileKeeper& temp_file_keeper) {
   xml_writer_->StartElement(kActionElement);
   xml_writer_->AddAttribute(kActivationType, kForeground);
-  xml_writer_->AddAttribute(kContent, base::UTF16ToUTF8(button.title).c_str());
+  xml_writer_->AddAttribute(kContent, base::UTF16ToUTF8(button.title));
   std::string param = std::string(kButtonIndex) + base::IntToString(index);
-  xml_writer_->AddAttribute(kArguments, param.c_str());
+  xml_writer_->AddAttribute(kArguments, param);
+
+  if (!button.icon.IsEmpty()) {
+    base::FilePath path = temp_file_keeper.RegisterTemporaryImage(
+        button.icon, origin, kPrefixButton);
+    if (!path.empty()) {
+      xml_writer_->AddAttribute(kImageUri, base::UTF16ToUTF8(path.value()));
+    }
+  }
+
   xml_writer_->EndElement();
 }
