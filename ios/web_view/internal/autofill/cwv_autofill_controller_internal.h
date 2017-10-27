b0f0440b4f939b0ef2687c507994a00f5abbc6da
John Z Wu <jzw@chromium.org>
2017-10-27 11:01:02 -0700

    Internal headers for //ios/web_view autofill API.
    Bug:763797
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ied77e8d14806431b24962bcd5ee5b05f9faf2b9d

---
+++ ios/web_view/internal/autofill/cwv_autofill_controller_internal.h
@@ -0,0 +1,46 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_VIEW_INTERNAL_AUTOFILL_CWV_AUTOFILL_CONTROLLER_INTERNAL_H_
+#define IOS_WEB_VIEW_INTERNAL_AUTOFILL_CWV_AUTOFILL_CONTROLLER_INTERNAL_H_
+
+#import "ios/web_view/public/cwv_autofill_controller.h"
+
+@class AutofillAgent;
+@class JsAutofillManager;
+
+namespace web {
+class WebState;
+class WebStateObserverBridge;
+}  // namespace web
+
+namespace autofill {
+class AutofillClientIOS;
+class AutofillManager;
+}  // namespace autofill
+
+@interface CWVAutofillController ()
+
+// The |webState| which this autofill controller should observe.
+@property(nonatomic, assign) web::WebState* webState;
+
+// Bridge to observe the |webState|.
+@property(nonatomic, readonly)
+    web::WebStateObserverBridge* webStateObserverBridge;
+
+// Autofill client associated with |webState|.
+@property(nonatomic, readonly) autofill::AutofillClientIOS* autofillClient;
+
+// Autofill agent associated with |webState|.
+@property(nonatomic, readonly) AutofillAgent* autofillAgent;
+
+// Autofill manager associated with |webState|.
+@property(nonatomic, readonly) autofill::AutofillManager* autofillManager;
+
+// Exposes injected javascript methods for autofill.
+@property(nonatomic, readonly) JsAutofillManager* JSAutofillManager;
+
+@end
+
+#endif  // IOS_WEB_VIEW_INTERNAL_AUTOFILL_CWV_AUTOFILL_CONTROLLER_INTERNAL_H_
