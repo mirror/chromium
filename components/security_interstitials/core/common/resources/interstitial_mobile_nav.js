acca43b80f7dc9aa9c879369a10ab964d3d82643
Edward Jung <edwardjung@chromium.org>
2017-10-27 13:59:51 +0100

    Interstitial tweaks for small screen / low bit depth screens
    Bug: 755714
    Change-Id: Ia90afd5ab621fd0c0c9781cb13f7a9e003c053a0

--- components/security_interstitials/core/common/resources/interstitial_mobile_nav.js
+++ components/security_interstitials/core/common/resources/interstitial_mobile_nav.js
@@ -42,6 +42,8 @@ function onResize() {
 }
 
 function setupMobileNav() {
+  // Check for screens with a low bit colour depth.
+  document.body.classList.toggle('low-color-depth', screen.colorDepth < 24);
   window.addEventListener('resize', onResize);
   onResize();
 }
