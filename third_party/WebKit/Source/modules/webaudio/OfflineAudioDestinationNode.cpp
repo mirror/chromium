cb6e57adc0301a00c16bfdcc7745206e4563c684
Hajime Hoshi <hajimehoshi@chromium.org>
2017-10-27 17:15:21 +0900

    Add DCHECK(IsMainThread()) at Document::GetTaskRunner()
    Bug: 696905
    Change-Id: If247c92bbefb23b447a3d2a9097e512540c4bbba

--- third_party/WebKit/Source/modules/webaudio/OfflineAudioDestinationNode.cpp
+++ third_party/WebKit/Source/modules/webaudio/OfflineAudioDestinationNode.cpp
@@ -26,7 +26,6 @@
 #include "modules/webaudio/OfflineAudioDestinationNode.h"
 
 #include <algorithm>
-#include "core/dom/TaskRunnerHelper.h"
 #include "modules/webaudio/AudioNodeInput.h"
 #include "modules/webaudio/AudioNodeOutput.h"
 #include "modules/webaudio/AudioWorkletMessagingProxy.h"
@@ -59,6 +58,11 @@ OfflineAudioDestinationHandler::OfflineAudioDestinationHandler(
 
   SetInternalChannelCountMode(kExplicit);
   SetInternalChannelInterpretation(AudioBus::kSpeakers);
+
+  if (Context()->GetExecutionContext()) {
+    task_runner_ = Context()->GetExecutionContext()->GetTaskRunner(
+        TaskType::kMediaElementEvent);
+  }
 }
 
 scoped_refptr<OfflineAudioDestinationHandler>
@@ -250,13 +254,11 @@ void OfflineAudioDestinationHandler::SuspendOfflineRendering() {
   DCHECK(!IsMainThread());
 
   // The actual rendering has been suspended. Notify the context.
-  if (Context()->GetExecutionContext()) {
-    TaskRunnerHelper::Get(TaskType::kMediaElementEvent,
-                          Context()->GetExecutionContext())
-        ->PostTask(BLINK_FROM_HERE,
-                   CrossThreadBind(
-                       &OfflineAudioDestinationHandler::NotifySuspend,
-                       WrapRefCounted(this), Context()->CurrentSampleFrame()));
+  if (task_runner_) {
+    task_runner_->PostTask(
+        BLINK_FROM_HERE,
+        CrossThreadBind(&OfflineAudioDestinationHandler::NotifySuspend,
+                        WrapRefCounted(this), Context()->CurrentSampleFrame()));
   }
 }
 
@@ -265,12 +267,10 @@ void OfflineAudioDestinationHandler::FinishOfflineRendering() {
 
   // The actual rendering has been completed. Notify the context.
   if (Context()->GetExecutionContext()) {
-    TaskRunnerHelper::Get(TaskType::kMediaElementEvent,
-                          Context()->GetExecutionContext())
-        ->PostTask(
-            BLINK_FROM_HERE,
-            CrossThreadBind(&OfflineAudioDestinationHandler::NotifyComplete,
-                            WrapRefCounted(this)));
+    task_runner_->PostTask(
+        BLINK_FROM_HERE,
+        CrossThreadBind(&OfflineAudioDestinationHandler::NotifyComplete,
+                        WrapRefCounted(this)));
   }
 }
 
