a19b0e74317366191932d98c152ee34aba0c2497
Ryan Harrison <rharrison@chromium.org>
2017-10-27 10:25:33 -0400

    Fetch executable path for Mac in helper
    The current implementation for Mac crashes, since /proc/ doesn't exist and thus
    the path being searched for. This leads to attempting to create a string of
    length 0.
    
    This adds in Mac specific logic for fetching this information.
    
    BUG=chromium:773183
    
    Change-Id: I57bc2530ee79a51c0caad42ec31f640f73c7a7d4

--- pdf/pdfium/fuzzers/pdfium_fuzzer_helper.cc
+++ pdf/pdfium/fuzzers/pdfium_fuzzer_helper.cc
@@ -8,6 +8,11 @@
 
 #include <assert.h>
 #include <limits.h>
+
+#ifdef __APPLE__
+#include <mach-o/dyld.h>
+#endif
+
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -87,10 +92,23 @@ std::string ProgramPath() {
   return std::string(path, res);
 #else
   char* path = new char[PATH_MAX + 1];
-  assert(path);
+  if (!path)
+    return std::string();
+
+  std::string result;
+#ifdef __APPLE__
+  uint32_t sz = PATH_MAX + 1;
+  if (!_NSGetExecutablePath(path, &sz)) {
+    char* resolved_path = realpath(path, nullptr);
+    result = std::string(resolved_path);
+    free(resolved_path);
+  }
+#else
   ssize_t sz = readlink("/proc/self/exe", path, PATH_MAX);
-  assert(sz > 0);
-  std::string result(path, sz);
+  if (sz > 0)
+    result = std::string(path, sz);
+#endif
+
   delete[] path;
   return result;
 #endif
