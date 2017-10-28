ee06bb6c0f1f5e604fdbf9785440d0a9bda6313a
Gyuyoung Kim <gyuyoung.kim@lge.com>
2017-10-28 14:14:35 +0900

    Introduce NGLayoutTest to use ScopedLayoutNGForTest for testing
    This CL removes calls to RuntimeEnabledFeatures:::SetLayoutNGEnabled and instead uses scoped
    settings setters to ensure test settings aren't used in next tests. To use that, this CL
    introduces NGLayoutTest that inherits RenderingTest and ScopedLayoutNGForTest. Then child NGFooClass
    inherit it instead of RenderingTest.
    
    Bug: 769541
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I6f73077a694dd10750e458cd2bc619005cce1a42

--- third_party/WebKit/Source/core/layout/ng/inline/ng_inline_node_test.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_inline_node_test.cc
@@ -4,7 +4,6 @@
 
 #include "core/layout/ng/inline/ng_inline_node.h"
 
-#include "core/layout/LayoutTestHelper.h"
 #include "core/layout/ng/inline/ng_inline_layout_algorithm.h"
 #include "core/layout/ng/inline/ng_physical_line_box_fragment.h"
 #include "core/layout/ng/inline/ng_physical_text_fragment.h"
@@ -12,6 +11,7 @@
 #include "core/layout/ng/ng_constraint_space.h"
 #include "core/layout/ng/ng_constraint_space_builder.h"
 #include "core/layout/ng/ng_layout_result.h"
+#include "core/layout/ng/ng_layout_test.h"
 #include "core/layout/ng/ng_physical_box_fragment.h"
 #include "core/style/ComputedStyle.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -63,17 +63,15 @@ class NGInlineNodeForTest : public NGInlineNode {
   using NGInlineNode::ShapeText;
 };
 
-class NGInlineNodeTest : public RenderingTest {
+class NGInlineNodeTest : public NGLayoutTest {
  protected:
   void SetUp() override {
     RenderingTest::SetUp();
-    RuntimeEnabledFeatures::SetLayoutNGEnabled(true);
     style_ = ComputedStyle::Create();
     style_->GetFont().Update(nullptr);
   }
 
   void TearDown() override {
-    RuntimeEnabledFeatures::SetLayoutNGEnabled(false);
     RenderingTest::TearDown();
   }
 
