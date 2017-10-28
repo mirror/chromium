615bdcdb44eb0be4fef9b57490a4df28abfabcbe
Ella Ge <eirage@chromium.org>
2017-10-27 23:00:40 -0400

    Enable prevent scrolling in Element.focus()
    Implement Focusptions for focus() under a experimental flag.
    FocusOptions has a boolean preventScroll (default false) to
    prevent scrolling.
    
    spec proposal: https://github.com/whatwg/html/pull/2787#issuecomment-338193107
    tests:
    https://github.com/w3c/web-platform-tests/pull/7915
    https://github.com/w3c/web-platform-tests/pull/7917
    
    Intent to implement: https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/QeiK3lAXcmI
    
    Bug:734166
    Change-Id: I47c1ebd9aebfc48064f9712d11c4d07ab8fabfa4

--- third_party/WebKit/Source/core/html/forms/HTMLTextAreaElement.cpp
+++ third_party/WebKit/Source/core/html/forms/HTMLTextAreaElement.cpp
@@ -247,7 +247,8 @@ bool HTMLTextAreaElement::ShouldShowFocusRingOnMouseFocus() const {
 }
 
 void HTMLTextAreaElement::UpdateFocusAppearance(
-    SelectionBehaviorOnFocus selection_behavior) {
+    SelectionBehaviorOnFocus selection_behavior,
+    const FocusOptions& options) {
   switch (selection_behavior) {
     case SelectionBehaviorOnFocus::kReset:  // Fallthrough.
     case SelectionBehaviorOnFocus::kRestore:
@@ -256,8 +257,10 @@ void HTMLTextAreaElement::UpdateFocusAppearance(
     case SelectionBehaviorOnFocus::kNone:
       return;
   }
-  if (GetDocument().GetFrame())
-    GetDocument().GetFrame()->Selection().RevealSelection();
+  if (!options.preventScroll()) {
+    if (GetDocument().GetFrame())
+      GetDocument().GetFrame()->Selection().RevealSelection();
+  }
 }
 
 void HTMLTextAreaElement::DefaultEventHandler(Event* event) {
