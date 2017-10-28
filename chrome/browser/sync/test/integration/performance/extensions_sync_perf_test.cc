25e1c866f3c2ac18fb926aeb7181cff2ec73dee6
Lei Zhang <thestig@chromium.org>
2017-10-28 08:51:27 -0700

    Get rid of most std::vector<int>::iterator usage.
    Most std::vector iteration can just be range-based For loops.
    
    Change-Id: Ibe18f8a088a27f41dbe0e38c40787bbcef28a7e2

--- chrome/browser/sync/test/integration/performance/extensions_sync_perf_test.cc
+++ chrome/browser/sync/test/integration/performance/extensions_sync_perf_test.cc
@@ -55,12 +55,11 @@ void ExtensionsSyncPerfTest::AddExtensions(int profile, int num_extensions) {
 
 void ExtensionsSyncPerfTest::UpdateExtensions(int profile) {
   std::vector<int> extensions = GetInstalledExtensions(GetProfile(profile));
-  for (std::vector<int>::iterator it = extensions.begin();
-       it != extensions.end(); ++it) {
-    if (IsExtensionEnabled(GetProfile(profile), *it)) {
-      DisableExtension(GetProfile(profile), *it);
+  for (int extension : extensions) {
+    if (IsExtensionEnabled(GetProfile(profile), extension)) {
+      DisableExtension(GetProfile(profile), extension);
     } else {
-      EnableExtension(GetProfile(profile), *it);
+      EnableExtension(GetProfile(profile), extension);
     }
   }
 }
@@ -71,9 +70,8 @@ int ExtensionsSyncPerfTest::GetExtensionCount(int profile) {
 
 void ExtensionsSyncPerfTest::RemoveExtensions(int profile) {
   std::vector<int> extensions = GetInstalledExtensions(GetProfile(profile));
-  for (std::vector<int>::iterator it = extensions.begin();
-       it != extensions.end(); ++it) {
-    UninstallExtension(GetProfile(profile), *it);
+  for (int extension : extensions) {
+    UninstallExtension(GetProfile(profile), extensions);
   }
 }
 
