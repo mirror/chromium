f4df2b9eaad81168765cac8db7f7cae6083fb6dc
Finnur Thorarinsson <finnur@chromium.org>
2017-10-29 22:20:25 +0000

    Win10 Native Notifications: Support images.
    Introduce a class that supports hanging on to temporary
    files long enough for the OS to pick them up (and clean
    them up after restart of Chrome if it fails the first
    time around). Necessary because Windows doesn't
    guarantee temp file deletion on shutdown/restart. 
    
    Bug: 734095
    Change-Id: I2926dc33b489f150a75f97792266e1a31a4ea5a2

--- base/files/file_util_win.cc
+++ base/files/file_util_win.cc
@@ -340,6 +340,14 @@ FILE* CreateAndOpenTemporaryFileInDir(const FilePath& dir, FilePath* path) {
 
 bool CreateTemporaryFileInDir(const FilePath& dir, FilePath* temp_file) {
   AssertBlockingAllowed();
+  return CreateTemporaryFileWithPrefixInDir(dir, FilePath::StringType(),
+                                            temp_file);
+}
+
+bool CreateTemporaryFileWithPrefixInDir(const FilePath& dir,
+                                        const FilePath::StringType& prefix,
+                                        FilePath* temp_file) {
+  AssertBlockingAllowed();
 
   // Use GUID instead of ::GetTempFileName() to generate unique file names.
   // "Due to the algorithm used to generate file names, GetTempFileName can
@@ -354,7 +362,8 @@ bool CreateTemporaryFileInDir(const FilePath& dir, FilePath* temp_file) {
   // Although it is nearly impossible to get a duplicate name with GUID, we
   // still use a loop here in case it happens.
   for (int i = 0; i < 100; ++i) {
-    temp_name = dir.Append(ASCIIToUTF16(base::GenerateGUID()) + L".tmp");
+    temp_name =
+        dir.Append(prefix + ASCIIToUTF16(base::GenerateGUID()) + L".tmp");
     File file(temp_name,
               File::FLAG_CREATE | File::FLAG_READ | File::FLAG_WRITE);
     if (file.IsValid()) {
