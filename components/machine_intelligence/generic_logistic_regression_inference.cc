041fcd51ea5c1ce6af707eea739913a6e657e626
Philippe Hamel <hamelphi@chromium.org>
2017-10-30 11:27:20 -0400

    Fix GenericLogisticRegressionInference includes.
    Bug: 759161
    Change-Id: I199c7604eb38fc5a1b8107710eeaa27f8e71b299

--- components/machine_intelligence/generic_logistic_regression_inference.cc
+++ components/machine_intelligence/generic_logistic_regression_inference.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <cmath>
+
 #include "components/machine_intelligence/generic_logistic_regression_inference.h"
 
 #include "base/logging.h"
