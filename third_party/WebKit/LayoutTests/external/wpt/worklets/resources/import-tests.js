c7aa84c07f07757ed3b6cdb7e18086a8d391b957
Hiroki Nakagawa <nhiroki@chromium.org>
2017-10-30 10:52:40 +0900

    Worklet: Make WorkletGlobalScope inherit parent document's ReferrerPolicy
    Bug: 773921
    Change-Id: Id8b9aa6649bdd1c2851f8d64af230b24ebf1e78e

--- third_party/WebKit/LayoutTests/external/wpt/worklets/resources/import-tests.js
+++ third_party/WebKit/LayoutTests/external/wpt/worklets/resources/import-tests.js
@@ -1,3 +1,14 @@
+function openWindow(url) {
+  return new Promise(resolve => {
+      let win = window.open(url, '_blank');
+      add_completion_callback(() => win.close());
+      window.onmessage = e => {
+        assert_equals(e.data, 'LOADED');
+        resolve(win);
+      };
+    });
+}
+
 // Runs a series of tests related to importing scripts on a worklet.
 //
 // Usage:
@@ -111,4 +122,52 @@ function runImportTests(worklet) {
                                worklet.addModule(kScriptURL));
     }, 'Importing a cross origin resource without the ' +
        'Access-Control-Allow-Origin header should reject the given promise');
+
+    promise_test(() => {
+        const kWindowURL = "resources/referrer-window.html" +
+                           "?policy=no-referrer" +
+                           "&pipe=header(Referrer-Policy,no-referrer)";
+        return openWindow(kWindowURL).then(win => {
+            let promise = new Promise(resolve => window.onmessage = resolve);
+            win.postMessage('ADD_MODULE', '*');
+            return promise;
+        }).then(msg_event => assert_equals(msg_event.data, 'RESOLVED'));
+    }, 'Importing a script from a page that has "no-referrer" referrer ' +
+       'policy should not send referrer.');
+
+    promise_test(() => {
+        const kWindowURL = "resources/referrer-window.html" +
+                           "?policy=origin" +
+                           "&pipe=header(Referrer-Policy,origin)";
+        return openWindow(kWindowURL).then(win => {
+            let promise = new Promise(resolve => window.onmessage = resolve);
+            win.postMessage('ADD_MODULE', '*');
+            return promise;
+        }).then(msg_event => assert_equals(msg_event.data, 'RESOLVED'));
+    }, 'Importing a script from a page that has "origin" referrer policy ' +
+       'should send only an origin as referrer.');
+
+    promise_test(() => {
+        const kWindowURL = "resources/referrer-window.html" +
+                           "?policy=same-origin" +
+                           "&pipe=header(Referrer-Policy,same-origin)";
+        return openWindow(kWindowURL).then(win => {
+            let promise = new Promise(resolve => window.onmessage = resolve);
+            win.postMessage('ADD_MODULE', '*');
+            return promise;
+        }).then(msg_event => assert_equals(msg_event.data, 'RESOLVED'));
+    }, 'Importing a same-origin script from a page that has "same-origin" ' +
+       'referrer policy should send referrer.');
+
+    promise_test(() => {
+        const kWindowURL = "resources/referrer-window.html" +
+                           "?policy=same-origin&cors=true" +
+                           "&pipe=header(Referrer-Policy,same-origin)";
+        return openWindow(kWindowURL).then(win => {
+            let promise = new Promise(resolve => window.onmessage = resolve);
+            win.postMessage('ADD_MODULE', '*');
+            return promise;
+        }).then(msg_event => assert_equals(msg_event.data, 'RESOLVED'));
+    }, 'Importing a remote-origin script from a page that has "same-origin" ' +
+       'referrer policy should not send referrer.');
 }
