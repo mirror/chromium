7e56118bfe19c557a9fc81536fdc14e501e38a79
Darren Shen <shend@chromium.org>
2017-10-30 14:33:49 +1100

    [css-typed-om] Implement CSSMathNegate and CSSMathInvert.
    This patch adds the new CSSMathValue subclasses CSSMathNegate and
    CSSMathInvert.
    
    Bug: 776173
    Change-Id: I7183df9c269db6eb799f93d39c0d8b1e541f7991

---
+++ third_party/WebKit/Source/core/css/cssom/CSSMathNegate.h
@@ -0,0 +1,46 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CSSMathNegate_h
+#define CSSMathNegate_h
+
+#include "core/css/cssom/CSSMathValue.h"
+
+namespace blink {
+
+// Represents the negation of a CSSNumericValue.
+// See CSSMathNegate.idl for more information about this class.
+class CORE_EXPORT CSSMathNegate : public CSSMathValue {
+  WTF_MAKE_NONCOPYABLE(CSSMathNegate);
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  // The constructor defined in the IDL.
+  static CSSMathNegate* Create(const CSSNumberish& arg) {
+    return new CSSMathNegate(arg);
+  }
+
+  String getOperator() const final { return "negate"; }
+
+  void value(CSSNumberish& value) { value = value_; }
+  void setValue(const CSSNumberish& value) { value_ = value; }
+
+  // From CSSStyleValue.
+  StyleValueType GetType() const final { return CSSStyleValue::kNegateType; }
+
+  void Trace(Visitor* visitor) override {
+    visitor->Trace(value_);
+    CSSMathValue::Trace(visitor);
+  }
+
+ private:
+  explicit CSSMathNegate(const CSSNumberish& value)
+      : CSSMathValue(), value_(value) {}
+
+  CSSNumberish value_;
+};
+
+}  // namespace blink
+
+#endif  // CSSMathNegate_h
