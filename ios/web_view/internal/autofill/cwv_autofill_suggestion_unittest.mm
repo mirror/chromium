8ad603f75faa880f747bd213cf54b787d730bd57
John Z Wu <jzw@chromium.org>
2017-10-30 12:44:21 -0700

    Autofill suggestion implementation. Includes unit tests.
    Bug:763797
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I9ba29392c1b16a64cadf4792cdb7b2a947ee04eb

---
+++ ios/web_view/internal/autofill/cwv_autofill_suggestion_unittest.mm
@@ -0,0 +1,41 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/web_view/internal/autofill/cwv_autofill_suggestion_internal.h"
+
+#import <Foundation/Foundation.h>
+
+#import "components/autofill/ios/browser/form_suggestion.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#import "testing/gtest_mac.h"
+#include "testing/platform_test.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace ios_web_view {
+
+using CWVAutofillSuggestionTest = PlatformTest;
+
+// Tests CWVAutofillSuggestion initialization.
+TEST_F(CWVAutofillSuggestionTest, Initialization) {
+  NSString* formName = @"TestFormName";
+  NSString* fieldName = @"TestFieldName";
+  FormSuggestion* formSuggestion =
+      [FormSuggestion suggestionWithValue:@"TestValue"
+                       displayDescription:@"TestDisplayDescription"
+                                     icon:@"TestIcon"
+                               identifier:0];
+  CWVAutofillSuggestion* suggestion =
+      [[CWVAutofillSuggestion alloc] initWithFormSuggestion:formSuggestion
+                                                   formName:formName
+                                                  fieldName:fieldName];
+  EXPECT_NSEQ(suggestion.formName, formName);
+  EXPECT_NSEQ(suggestion.fieldName, fieldName);
+  EXPECT_NSEQ(suggestion.displayDescription, formSuggestion.displayDescription);
+  EXPECT_NSEQ(suggestion.value, formSuggestion.value);
+}
+
+}  // namespace ios_web_view
