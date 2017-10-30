f18c01683d6ff64eed7b840edf5133c3007bac4f
Peter Mayo <petermayo@chromium.org>
2017-10-30 13:40:22 -0400

    [animation-worklet] Move AnimationWorklet to a separate backing thread
    This version introduces the backing thread, but does not plumb the
    monotonic time, since the Mutate call currently doesn't use it.
    It uses a completion event to do the animation synchronously; this
    lines up with the division of tasks outlined in the task issues.
    
    Bug: 731727
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ia774e53b5bf34e0cef5d7f0dd9d02fc3b27f4b20

--- third_party/WebKit/Source/modules/animationworklet/AnimationWorkletGlobalScopeTest.cpp
+++ third_party/WebKit/Source/modules/animationworklet/AnimationWorkletGlobalScopeTest.cpp
@@ -187,7 +187,7 @@ class AnimationWorkletGlobalScopeTest : public ::testing::Test {
     state.animations = {test_animation_state};
 
     std::unique_ptr<CompositorMutatorOutputState> output =
-        global_scope->Mutate(state);
+        global_scope->MutateInternal(state);
     EXPECT_TRUE(output);
 
     ScriptValue constructed_after =
@@ -238,7 +238,7 @@ class AnimationWorkletGlobalScopeTest : public ::testing::Test {
     state.animations = {test_animation_state};
 
     std::unique_ptr<CompositorMutatorOutputState> output =
-        global_scope->Mutate(state);
+        global_scope->MutateInternal(state);
     EXPECT_TRUE(output);
 
     EXPECT_EQ(output->animations.size(), 1ul);
@@ -248,7 +248,7 @@ class AnimationWorkletGlobalScopeTest : public ::testing::Test {
     // Passing a new empty input state should cause the worklet to remove the
     // previously constructed animator.
     CompositorMutatorInputState empty_state;
-    output = global_scope->Mutate(empty_state);
+    output = global_scope->MutateInternal(empty_state);
     EXPECT_TRUE(output);
     EXPECT_EQ(output->animations.size(), 0ul);
     EXPECT_EQ(global_scope->GetAnimatorsSizeForTest(), 0u);
