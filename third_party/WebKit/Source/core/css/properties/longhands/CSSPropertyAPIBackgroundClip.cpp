4d7c26703996370dc5ed9b5efcfa7e70a91f6e03
Bugs Nash <bugsnash@chromium.org>
2017-10-31 09:25:30 +1100

    Made the -webkit-background-clip property an alias for background-clip.
    Made the -webkit-background-clip property an alias for the
    background-clip property using the standard alias_for code path. This
    reduces that places that need to know about the alias.
    
    This patch also splits the following grouped property APIs into single
    property APIs. This is required because -webkit-background-clip has
    different parsing logic from background-clip and this is now handled
    in the standard way in the property API for background-clip.
    - CSSPropertyAPIWebkitClip changed to single property API
      CSSPropertyAPIWebkitMaskClip
    - CSSPropertyAPIBackgroundBox split into single property APIs
     - CSSPropertyAPIBackgroundClip (handles alias logic)
     - CSSPropertyAPIBackgroundOrigin
    
    Diff of generated files:
    https://gist.github.com/BugsNash/1650bbd85e5d089a5b1f812e6a71fb07/revisions
    Ignore ComputedStyleBase changes in generated files diff, these are not
    due to my change but due to a non-deterministic bug.
    
    Bug: 410112
    Change-Id: I3f4e8d88e51508ec1bf528419da01fb5b2536976

--- third_party/WebKit/Source/core/css/properties/longhands/CSSPropertyAPIBackgroundClip.cpp
+++ third_party/WebKit/Source/core/css/properties/longhands/CSSPropertyAPIBackgroundClip.cpp
@@ -2,17 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "core/css/properties/longhands/CSSPropertyAPIBackgroundBox.h"
+#include "core/css/properties/longhands/CSSPropertyAPIBackgroundClip.h"
 
+#include "core/css/parser/CSSParserLocalContext.h"
 #include "core/css/parser/CSSPropertyParserHelpers.h"
 #include "core/css/properties/CSSPropertyBackgroundUtils.h"
 
 namespace blink {
 
-const CSSValue* CSSPropertyAPIBackgroundBox::ParseSingleValue(
+const CSSValue* CSSPropertyAPIBackgroundClip::ParseSingleValue(
     CSSParserTokenRange& range,
     const CSSParserContext&,
-    const CSSParserLocalContext&) const {
+    const CSSParserLocalContext& local_context) const {
+  if (local_context.UseAliasParsing()) {
+    return CSSPropertyParserHelpers::ConsumeCommaSeparatedList(
+        CSSPropertyBackgroundUtils::ConsumePrefixedBackgroundBox, range,
+        AllowTextValue::kAllowed);
+  }
   return CSSPropertyParserHelpers::ConsumeCommaSeparatedList(
       CSSPropertyBackgroundUtils::ConsumeBackgroundBox, range);
 }
