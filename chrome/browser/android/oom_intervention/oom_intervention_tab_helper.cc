3ccf84d22c2c87529288120af2fc4110d8755e66
Kenichi Ishibashi <bashi@chromium.org>
2017-10-31 08:37:55 +0900

    Add OomInterventionTabHelper
    This CL adds OomInterventionTabHelper which is attached to a WebContent
    when the "OomIntervention" feature flag is enabled. When the WebContent
    is a foreground tab, the helper tries to detect near-OOM situation by
    using NearOomMonitor.
    
    Currently the helper doesn't initiate any intervention (e.g. pause
    JavaScript execution) but records some metrics. Metrics include:
    1) Elapsed time between near-OOM detection and a crash/process
       termination/page navigation.
    2) The number of crashes of OOM protected renderers without near-OOM
       detection.
    3) The number of renderer termination without near-OOM detection.
    4) The number of navigations without near-OOM detection.
    
    1 will be used to examine true/false positives (near-OOM is detected,
    is it a success estimation or not?)
    2, 3, 4 will be used to examine true/false negatives (near-OOM is not
    detected, isn't it overlooked?)
    
    Bug: 776665
    Change-Id: I439ef7abeb90ba09df41e7fbb7a61e1599d2edc7

---
+++ chrome/browser/android/oom_intervention/oom_intervention_tab_helper.cc
@@ -0,0 +1,140 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/android/oom_intervention/oom_intervention_tab_helper.h"
+
+#include "base/metrics/histogram_macros.h"
+#include "content/public/browser/navigation_handle.h"
+#include "content/public/browser/render_frame_host.h"
+#include "content/public/browser/web_contents.h"
+
+DEFINE_WEB_CONTENTS_USER_DATA_KEY(OomInterventionTabHelper);
+
+namespace {
+
+// This enum is associated with UMA. Values must be kept in sync with enums.xml
+// and must not be renumbered/reused.
+enum class NearOomMonitoringEndReason {
+  OOM_PROTECTED_CRASH = 0,
+  RENDERER_GONE = 1,
+  NAVIGATION = 2,
+  COUNT,
+};
+
+void RecordNearOomMonitoringEndReason(NearOomMonitoringEndReason reason) {
+  UMA_HISTOGRAM_ENUMERATION(
+      "Memory.Experimental.OomIntervention.NearOomMonitoringEndReason", reason,
+      NearOomMonitoringEndReason::COUNT);
+}
+
+}  // namespace
+
+// static
+bool OomInterventionTabHelper::IsEnabled() {
+  NearOomMonitor* monitor = NearOomMonitor::GetInstance();
+  return monitor && monitor->IsRunning();
+}
+
+OomInterventionTabHelper::OomInterventionTabHelper(
+    content::WebContents* web_contents)
+    : content::WebContentsObserver(web_contents) {}
+
+OomInterventionTabHelper::~OomInterventionTabHelper() = default;
+
+void OomInterventionTabHelper::WebContentsDestroyed() {
+  StopMonitoring();
+}
+
+void OomInterventionTabHelper::RenderProcessGone(
+    base::TerminationStatus status) {
+  if (near_oom_detected_time_ && web_contents()->IsVisible()) {
+    base::TimeDelta elapsed_time =
+        base::TimeTicks::Now() - near_oom_detected_time_.value();
+    if (status == base::TERMINATION_STATUS_OOM_PROTECTED) {
+      UMA_HISTOGRAM_MEDIUM_TIMES(
+          "Memory.Experimental.OomIntervention."
+          "OomProtectedCrashAfterDetectionTime",
+          elapsed_time);
+    } else {
+      UMA_HISTOGRAM_MEDIUM_TIMES(
+          "Memory.Experimental.OomIntervention."
+          "RendererGoneAfterDetectionTime",
+          elapsed_time);
+    }
+    near_oom_detected_time_.reset();
+  } else if (subscription_) {
+    if (status == base::TERMINATION_STATUS_OOM_PROTECTED) {
+      RecordNearOomMonitoringEndReason(
+          NearOomMonitoringEndReason::OOM_PROTECTED_CRASH);
+    } else {
+      RecordNearOomMonitoringEndReason(
+          NearOomMonitoringEndReason::RENDERER_GONE);
+    }
+  }
+}
+
+void OomInterventionTabHelper::DidStartNavigation(
+    content::NavigationHandle* navigation_handle) {
+  if (!navigation_handle->IsInMainFrame())
+    return;
+
+  // Filter out a navigation if the navigation happens without changing
+  // document.
+  if (navigation_handle->IsSameDocument())
+    return;
+
+  // Filter out the first navigation.
+  if (!navigation_started_)
+    return;
+
+  navigation_started_ = true;
+  if (near_oom_detected_time_) {
+    // near-OOM was detected.
+    base::TimeDelta elapsed_time =
+        base::TimeTicks::Now() - near_oom_detected_time_.value();
+    UMA_HISTOGRAM_MEDIUM_TIMES(
+        "Memory.Experimental.OomIntervention."
+        "NavigatedAfterDetectionTime",
+        elapsed_time);
+    near_oom_detected_time_.reset();
+  } else if (subscription_) {
+    // Monitoring but near-OOM hasn't been detected.
+    RecordNearOomMonitoringEndReason(NearOomMonitoringEndReason::NAVIGATION);
+  }
+
+  StartMonitoringIfNeeded();
+}
+
+void OomInterventionTabHelper::WasShown() {
+  StartMonitoringIfNeeded();
+}
+
+void OomInterventionTabHelper::WasHidden() {
+  StopMonitoring();
+}
+
+void OomInterventionTabHelper::StartMonitoringIfNeeded() {
+  if (subscription_)
+    return;
+
+  if (near_oom_detected_time_)
+    return;
+
+  if (!web_contents()->IsVisible())
+    return;
+
+  subscription_ = NearOomMonitor::GetInstance()->RegisterCallback(base::Bind(
+      &OomInterventionTabHelper::OnNearOomDetected, base::Unretained(this)));
+}
+
+void OomInterventionTabHelper::StopMonitoring() {
+  subscription_.reset();
+}
+
+void OomInterventionTabHelper::OnNearOomDetected() {
+  DCHECK(web_contents()->IsVisible());
+  DCHECK(!near_oom_detected_time_);
+  near_oom_detected_time_ = base::TimeTicks::Now();
+  subscription_.reset();
+}
