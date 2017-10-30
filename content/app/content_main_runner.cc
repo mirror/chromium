71116665078569824f220884afe1d4d51d5db7ce
Hitoshi Yoshida <peria@chromium.org>
2017-10-30 13:59:46 +0900

    Drop snapshot_blob.bin if v8_context_snapshot.bin is available
    Copyed from 696443
    
    Bug:
    Change-Id: Ifb7fb8451504c240a686b1a284fa34e51553cdb6

--- content/app/content_main_runner.cc
+++ content/app/content_main_runner.cc
@@ -170,6 +170,7 @@ void InitializeFieldTrialAndFeatureList(
 }
 
 void LoadV8ContextSnapshotFile() {
+#if defined(USE_V8_CONTEXT_SNAPSHOT)
 #if defined(OS_POSIX) && !defined(OS_MACOSX)
   base::FileDescriptorStore& file_descriptor_store =
       base::FileDescriptorStore::GetInstance();
@@ -181,14 +182,15 @@ void LoadV8ContextSnapshotFile() {
                                                     region.size);
     return;
   }
-#endif  // OS
+#endif  // OS_POSIX && !OS_MACOSX
 #if !defined(CHROME_MULTIPLE_DLL_BROWSER)
   gin::V8Initializer::LoadV8ContextSnapshot();
 #endif
+#endif  // USE_V8_CONTEXT_SNAPSHOT
 }
 
 void LoadV8SnapshotFile() {
-#if defined(V8_USE_EXTERNAL_STARTUP_DATA)
+#if defined(V8_USE_EXTERNAL_STARTUP_DATA) && !defined(USE_V8_CONTEXT_SNAPSHOT)
 #if defined(OS_POSIX) && !defined(OS_MACOSX)
   base::FileDescriptorStore& file_descriptor_store =
       base::FileDescriptorStore::GetInstance();
