dd37dc02db2626751ad879f98e023da2343f30c6
Luna Lu <loonybear@chromium.org>
2017-10-31 10:52:27 -0400

    Feature Policy Onion Soup (1)
    STEP 1 (Patch 1/2):
    - Moved source files content/common/feature_policy/feature_policy.cc, .h
      into third_party/WebKit/common/feature_policy/
    - Moved third_party/WebKit/public/platform/WebFeaturePolicyFeature.h
      into third_party/WebKit/common/feature_policy/feature_policy_feature.h
    - Migrated everything into blink namespace
    - Fixed BUILD.gn files
    - Modified DEPS files temporarily
    
    STEP 2 (Patch 3):
    - Rename WebFeaturePolicyFeature to FeaturePolicyFeature
    - UNDONE STEP 2 ==> moved it to a separate CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/742421
    
    STEP 3:
    - Replace WebFeaturePolicy class by FeaturePolicy
    - Replace reference to WebFeaturePolicy.h by feature_policy.h
    
    STEP 4:
    - Get rid of Web* types (WebVector, WebString, etc)
    
    STEP 5:
    - Use url::Origin instead of SecurityOrigin / WebSecurityOrigin
    
    STEP 6:
    - Remove unnecessary layer in Platform.h (previously needed for type conversion)
    - UNDONE STEP 6 ==> moved it to a separate CL
    https://chromium-review.googlesource.com/c/chromium/src/+/742601
    
    ---------------------------------------------------------------------------------------
    
    STEP 7:
    - Undo STEP 2
    
    STEP 8:
    - Undo STEP 6
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_chromium_dbg_ng;master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ief3b2edce924e0d74dd3c2ee082469e6fe29d0d8

--- content/browser/frame_host/render_frame_host_impl.h
+++ content/browser/frame_host/render_frame_host_impl.h
@@ -98,7 +98,6 @@ namespace content {
 class AssociatedInterfaceProviderImpl;
 class AssociatedInterfaceRegistryImpl;
 class LegacyIPCFrameInputHandler;
-class FeaturePolicy;
 class FrameTree;
 class FrameTreeNode;
 class GeolocationServiceImpl;
@@ -612,7 +611,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
   void ResetLoadingState();
 
   // Returns the feature policy which should be enforced on this RenderFrame.
-  FeaturePolicy* feature_policy() { return feature_policy_.get(); }
+  blink::FeaturePolicy* feature_policy() { return feature_policy_.get(); }
 
   // Clears any existing policy and constructs a new policy for this frame,
   // based on its parent frame.
@@ -775,7 +774,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
   void OnDidChangeOpener(int32_t opener_routing_id);
   void OnDidChangeName(const std::string& name, const std::string& unique_name);
   void OnDidSetFeaturePolicyHeader(
-      const ParsedFeaturePolicyHeader& parsed_header);
+      const blink::ParsedFeaturePolicy& parsed_header);
 
   // A new set of CSP |policies| has been added to the document.
   void OnDidAddContentSecurityPolicies(
@@ -1302,7 +1301,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
   int enabled_bindings_ = 0;
 
   // Tracks the feature policy which has been set on this frame.
-  std::unique_ptr<FeaturePolicy> feature_policy_;
+  std::unique_ptr<blink::FeaturePolicy> feature_policy_;
 
 #if defined(OS_ANDROID)
   // An InterfaceProvider for Java-implemented interfaces that are scoped to
