0c8a8f1165b6e77f2e38f141765011afb5c71148
Luna Lu <loonybear@chromium.org>
2017-10-31 15:22:37 -0400

    Add ToUrlOrigin() / FromUrlOrigin() conversion helper to blink::SecurityOrigin
    Change-Id: Id5e50fbd4a1d9e2cf76bb48c50da340615005739

--- third_party/WebKit/Source/platform/weborigin/SecurityOrigin.cpp
+++ third_party/WebKit/Source/platform/weborigin/SecurityOrigin.cpp
@@ -42,6 +42,7 @@
 #include "platform/wtf/StdLibExtras.h"
 #include "platform/wtf/text/StringBuilder.h"
 #include "platform/wtf/text/StringUTF8Adaptor.h"
+#include "platform/wtf/text/WTFString.h"
 #include "url/url_canon.h"
 #include "url/url_canon_ip.h"
 
@@ -194,6 +195,18 @@ scoped_refptr<SecurityOrigin> SecurityOrigin::CreateUnique() {
   scoped_refptr<SecurityOrigin> origin = WTF::AdoptRef(new SecurityOrigin());
   DCHECK(origin->IsUnique());
   return origin;
+}
+
+url::Origin SecurityOrigin::ToUrlOrigin() {
+  return url::Origin::CreateFromNormalizedTupleWithSuborigin(
+      StringUTF8Adaptor(protocol_).AsStringPiece(),
+      StringUTF8Adaptor(host_).AsStringPiece(), effective_port_,
+      StringUTF8Adaptor(suborigin_.GetName()).AsStringPiece());
+}
+
+scoped_refptr<SecurityOrigin> SecurityOrigin::FromUrlOrigin(
+    const url::Origin& origin) {
+  return CreateFromString(String::FromUTF8(origin.Serialize().c_str()));
 }
 
 scoped_refptr<SecurityOrigin> SecurityOrigin::IsolatedCopy() const {
