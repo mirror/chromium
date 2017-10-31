3d877002e8f1e81c4c5d0dcde55b395895984f91
Saman Sami <samans@chromium.org>
2017-10-31 11:49:22 -0400

    Implement a video detector in Viz
    Implement a video detector in Viz. Works with --mash, --mus,
    --enable-viz, and no flags.
    
    Change-Id: I5098492edaf5cce08f9ba6c50a5d7482eb739426
    BUG:775035

---
+++ services/ui/ws/video_detector_impl.h
@@ -0,0 +1,37 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_UI_WS_VIDEO_DETECTOR_IMPL_H_
+#define SERVICES_UI_WS_VIDEO_DETECTOR_IMPL_H_
+
+#include "mojo/public/cpp/bindings/binding_set.h"
+#include "services/ui/public/interfaces/video_detector.mojom.h"
+#include "services/viz/public/interfaces/compositing/video_detector_observer.mojom.h"
+
+namespace viz {
+class HostFrameSinkManager;
+}
+
+namespace ui {
+namespace ws {
+
+class VideoDetectorImpl : public mojom::VideoDetector {
+ public:
+  VideoDetectorImpl(viz::HostFrameSinkManager* host_frame_sink_manager);
+  ~VideoDetectorImpl() override;
+
+  void AddBinding(mojom::VideoDetectorRequest request);
+
+  // mojom::VideoDetector implementation.
+  void AddObserver(viz::mojom::VideoDetectorObserverPtr observer) override;
+
+ private:
+  viz::HostFrameSinkManager* host_frame_sink_manager_;
+  mojo::BindingSet<mojom::VideoDetector> binding_set_;
+};
+
+}  // namespace ws
+}  // namespace ui
+
+#endif  // SERVICES_UI_WS_VIDEO_DETECTOR_IMPL_H_
