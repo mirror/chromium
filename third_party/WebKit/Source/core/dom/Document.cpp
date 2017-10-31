a66fc82c114684a8c1ed513a038616f496fe1392
Hiroshige Hayashizaki <hiroshige@chromium.org>
2017-10-31 16:16:06 -0700

    Make Document::Create() take non-const Document reference
    Bug: 779730
    Change-Id: I10fd0ada6c3718dff178b842ccc9e1a888faaaa1

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -557,11 +557,10 @@ class Document::NetworkStateObserver final
       online_observer_handle_;
 };
 
-Document* Document::Create(const Document& document) {
-  Document* new_document =
-      new Document(DocumentInit::Create()
-                       .WithContextDocument(const_cast<Document*>(&document))
-                       .WithURL(BlankURL()));
+Document* Document::Create(Document& document) {
+  Document* new_document = new Document(
+      DocumentInit::Create().WithContextDocument(&document).WithURL(
+          BlankURL()));
   new_document->SetSecurityOrigin(document.GetSecurityOrigin());
   new_document->SetContextFeatures(document.GetContextFeatures());
   return new_document;
