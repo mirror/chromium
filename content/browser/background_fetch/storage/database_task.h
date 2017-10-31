ae770eb81058971b625f98f28e4fbe1ed8578ee9
Dan Elphick <delphick@chromium.org>
2017-10-31 17:06:26 +0000

    [BackgroundFetch] Pull out DatabaseTask and subclasses
    Move DatabaseTask and each of its subclasses into
    content/browser/background_fetch/storage and put them in the
    background_fetch namespace.
    
    To prevent circular dependencies move
    BackgroundFetchDataManager::Controller out of BackgroundFetchDataManager
    and rename it to BackgroundFetchDatabaseClient.
    Change-Id: I4c1fb87f5ee41fb09d31c8354d3a57f0d90f5d26

---
+++ content/browser/background_fetch/storage/database_task.h
@@ -0,0 +1,98 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_BACKGROUND_FETCH_STORAGE_DATABASE_TASK_H_
+#define CONTENT_BROWSER_BACKGROUND_FETCH_STORAGE_DATABASE_TASK_H_
+
+#include <memory>
+#include <set>
+#include <string>
+
+#include "base/logging.h"
+#include "base/macros.h"
+#include "content/browser/background_fetch/background_fetch_registration_id.h"
+#include "third_party/WebKit/public/platform/modules/background_fetch/background_fetch.mojom.h"
+
+// Service Worker DB UserData schema
+// =================================
+// Design doc:
+// https://docs.google.com/document/d/1-WPPTP909Gb5PnaBOKP58tPVLw2Fq0Ln-u1EBviIBns/edit
+//
+// - Each key will be stored twice by the Service Worker DB, once as a
+//   "REG_HAS_USER_DATA:", and once as a "REG_USER_DATA:" - see
+//   content/browser/service_worker/service_worker_database.cc for details.
+// - Integer values are serialized as a string by base::Int*ToString().
+//
+// key: "bgfetch_active_registration_unique_id_" + <std::string 'developer_id'>
+// value: <std::string 'unique_id'>
+//
+// key: "bgfetch_registration_" + <std::string 'unique_id'>
+// value: <std::string 'serialized content::proto::BackgroundFetchRegistration'>
+//
+// key: "bgfetch_request_" + <std::string 'registration unique_id'>
+//          + "_" + <int 'request_index'>
+// value: <TODO: FetchAPIRequest serialized as a string>
+//
+// key: "bgfetch_pending_request_"
+//          + <int64_t 'registration_creation_microseconds_since_unix_epoch'>
+//          + "_" + <std::string 'registration unique_id'>
+//          + "_" + <int 'request_index'>
+// value: ""
+
+namespace content {
+
+class BackgroundFetchDataManager;
+class BackgroundFetchDatabaseClient;
+class ServiceWorkerContextWrapper;
+
+// Note that this also handles non-error cases where the NONE is NONE.
+using HandleBackgroundFetchErrorCallback =
+    base::OnceCallback<void(blink::mojom::BackgroundFetchError)>;
+
+namespace background_fetch {
+
+// A DatabaseTask is an asynchronous "transaction" that needs to read/write the
+// Service Worker Database.
+//
+// Only one DatabaseTask can run at once per StoragePartition, and no other code
+// reads/writes Background Fetch keys, so each task effectively has an exclusive
+// lock, except that core Service Worker code may delete all keys for a
+// ServiceWorkerRegistration or the entire database at any time.
+class DatabaseTask {
+ public:
+  virtual ~DatabaseTask() = default;
+
+  virtual void Start() = 0;
+
+ protected:
+  explicit DatabaseTask(BackgroundFetchDataManager* data_manager)
+      : data_manager_(data_manager) {
+    DCHECK(data_manager_);
+  }
+
+  // Each task MUST call this once finished, even if exceptions occur, to
+  // release their lock and allow the next task to execute.
+  void Finished();
+
+  BackgroundFetchDatabaseClient* GetController(const std::string& unique_id);
+
+  void AddDatabaseTask(std::unique_ptr<DatabaseTask> task);
+
+  ServiceWorkerContextWrapper* service_worker_context();
+
+  std::set<std::string>& ref_counted_unique_ids();
+
+  BackgroundFetchDataManager* data_manager() { return data_manager_; }
+
+ private:
+  BackgroundFetchDataManager* data_manager_;  // Owns this.
+
+  DISALLOW_COPY_AND_ASSIGN(DatabaseTask);
+};
+
+}  // namespace background_fetch
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_BACKGROUND_FETCH_STORAGE_DATABASE_TASK_H_
