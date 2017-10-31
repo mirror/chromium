a24edfd93b993ea681f4039690993997fbcc6a74
David Roger <droger@chromium.org>
2017-10-31 17:09:25 +0100

    [signin] Parse "verified" bit from the ListAccounts Gaia endpoint
    Gaia will add a "verified" bit for accounts in the ListAccounts
    endpoint. This CL parses it, but does not use it yet.
    
    Bug: 780041
    Change-Id: Ie3b1610b4d6496bcb9d19f9b5887e90aba5167f2

--- google_apis/gaia/gaia_auth_util.h
+++ google_apis/gaia/gaia_auth_util.h
@@ -25,13 +25,13 @@ struct ListedAccount {
   std::string email;
   std::string gaia_id;
   std::string raw_email;
-  bool valid;
-  bool signed_out;
+  bool valid = 1;
+  bool signed_out = 0;
+  bool verified = 1;
 
   ListedAccount();
   ListedAccount(const ListedAccount& other);
   ~ListedAccount();
-  bool operator==(const ListedAccount& other) const;
 };
 
 // Perform basic canonicalization of |email_address|, taking into account that
