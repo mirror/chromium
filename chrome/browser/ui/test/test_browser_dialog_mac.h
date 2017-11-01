747ef088f01e55c1fb7e9d3366a14b68f4a40895
Vasiliy Tsukanov <palar@yandex-team.ru>
2017-11-01 16:51:25 +0300

    Fixed TestBrowserDialog use of depricated funcion.
    Bug:
    Change-Id: I3aed198d5a6ddb0b776f60c2358701953630078c

---
+++ chrome/browser/ui/test/test_browser_dialog_mac.h
@@ -0,0 +1,14 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef CHROME_BROWSER_UI_TEST_TEST_BROWSER_DIALOG_MAC_H_
+#define CHROME_BROWSER_UI_TEST_TEST_BROWSER_DIALOG_MAC_H_
+
+namespace test {
+// Platform dependant fixture for TestBrowserDialog.
+
+void TestBrowserDialogFixture();
+
+}  // namespace test
+
+#endif  // CHROME_BROWSER_UI_TEST_TEST_BROWSER_DIALOG_MAC_H_
