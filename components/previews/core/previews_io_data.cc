6c406b16e5e9460ac7c1c9f7d93927163e9a9ac8
Doug Arnett <dougarnett@chromium.org>
2017-11-01 15:01:24 -0700

    Adds new PreviewsOptimizationGuide whitelist interface
    Bug: 780298
    Change-Id: Ia369ee168a5f297515048944f2339c7fcebb904f

--- components/previews/core/previews_io_data.cc
+++ components/previews/core/previews_io_data.cc
@@ -122,6 +122,11 @@ void PreviewsIOData::SetPreviewsBlacklistForTesting(
   previews_black_list_ = std::move(previews_back_list);
 }
 
+void PreviewsIOData::SetPreviewsOptimizationGuideForTesting(
+    std::unique_ptr<PreviewsOptimizationGuide> previews_opt_guide) {
+  previews_opt_guide_ = std::move(previews_opt_guide);
+}
+
 void PreviewsIOData::LogPreviewNavigation(const GURL& url,
                                           bool opt_out,
                                           PreviewsType type,
@@ -221,6 +226,8 @@ bool PreviewsIOData::ShouldAllowPreviewAtECT(
     return false;
   }
 
+  // Check provided blacklist, if any. This type of blacklist was added for
+  // Finch provided blacklist for Client LoFi.
   if (std::find(host_blacklist_from_server.begin(),
                 host_blacklist_from_server.end(), request.url().host_piece()) !=
       host_blacklist_from_server.end()) {
@@ -228,6 +235,22 @@ bool PreviewsIOData::ShouldAllowPreviewAtECT(
         PreviewsEligibilityReason::HOST_BLACKLISTED_BY_SERVER, request.url(),
         base::Time::Now(), type);
     return false;
+  }
+
+  // Check any provided optimization guidance from the server. This data is
+  // provided through ComponentUpdater (at least originally).
+  if (params::AreOptimizationHintsEnabled()) {
+    // Optimization hints are configured. Check specific previews types that
+    // use them.
+    // TODO(dougarnett): Generalize at some point.
+    if (type == PreviewsType::NOSCRIPT) {
+      // Ensure the URL is whitelisted for NoScript.
+      if (!previews_opt_guide_ ||
+          !previews_opt_guide_->IsWhitelisted(request, type)) {
+        // TODO(dougarnett): Add and log eligibility reason for this.
+        return false;
+      }
+    }
   }
 
   LogPreviewDecisionMade(PreviewsEligibilityReason::ALLOWED, request.url(),
--- components/previews/core/previews_io_data_unittest.cc
+++ components/previews/core/previews_io_data_unittest.cc
@@ -92,6 +92,20 @@ class TestPreviewsBlackList : public PreviewsBlackList {
   PreviewsEligibilityReason status_;
 };
 
+// Stub class of PreviewsOptimizationGuide to control IsWhitelisted outcome
+// when testing PreviewsIOData.
+class TestPreviewsOptimizationGuide : public PreviewsOptimizationGuide {
+ public:
+  TestPreviewsOptimizationGuide() {}
+  ~TestPreviewsOptimizationGuide() override {}
+
+  // PreviewsOptimizationGuide:
+  bool IsWhitelisted(const net::URLRequest& request,
+                     PreviewsType type) const override {
+    return request.url().host().compare("whitelisted.example.com") == 0;
+  }
+};
+
 // Stub class of PreviewsUIService to test logging functionalities in
 // PreviewsIOData.
 class TestPreviewsUIService : public PreviewsUIService {
@@ -207,6 +221,13 @@ class TestPreviewsIOData : public PreviewsIOData {
   // |blacklist| into |this|.
   void InjectTestBlacklist(std::unique_ptr<PreviewsBlackList> blacklist) {
     SetPreviewsBlacklistForTesting(std::move(blacklist));
+  }
+
+  // Expose the injecting previews optimization guide method from
+  // PreviewsIOData, and inject |guide| into |this|.
+  void InjectTestPreviewsOptimizationGuide(
+      std::unique_ptr<TestPreviewsOptimizationGuide> guide) {
+    SetPreviewsOptimizationGuideForTesting(std::move(guide));
   }
 
  private:
@@ -632,6 +653,40 @@ TEST_F(PreviewsIODataTest, NoScriptAllowedByFeature) {
   base::HistogramTester histogram_tester;
   EXPECT_TRUE(io_data()->ShouldAllowPreviewAtECT(
       *CreateHttpsRequest(), PreviewsType::NOSCRIPT,
+      previews::params::GetECTThresholdForPreview(
+          previews::PreviewsType::NOSCRIPT),
+      std::vector<std::string>()));
+  histogram_tester.ExpectUniqueSample(
+      "Previews.EligibilityReason.NoScript",
+      static_cast<int>(PreviewsEligibilityReason::ALLOWED), 1);
+}
+
+TEST_F(PreviewsIODataTest, NoScriptAllowedByFeatureWithWhitelist) {
+  InitializeUIService();
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitWithFeatures(
+      {features::kNoScriptPreviews, features::kOptimizationHints}, {});
+
+  std::unique_ptr<TestPreviewsOptimizationGuide> guide =
+      base::MakeUnique<TestPreviewsOptimizationGuide>();
+  io_data()->InjectTestPreviewsOptimizationGuide(std::move(guide));
+
+  network_quality_estimator()->set_effective_connection_type(
+      net::EFFECTIVE_CONNECTION_TYPE_2G);
+
+  base::HistogramTester histogram_tester;
+
+  // First verify no preview for non-whitelisted url.
+  EXPECT_FALSE(io_data()->ShouldAllowPreviewAtECT(
+      *CreateHttpsRequest(), PreviewsType::NOSCRIPT,
+      previews::params::GetECTThresholdForPreview(
+          previews::PreviewsType::NOSCRIPT),
+      std::vector<std::string>()));
+
+  // Now verify preview for whitelisted url.
+  EXPECT_TRUE(io_data()->ShouldAllowPreviewAtECT(
+      *CreateRequestWithURL(GURL("https://whitelisted.example.com")),
+      PreviewsType::NOSCRIPT,
       previews::params::GetECTThresholdForPreview(
           previews::PreviewsType::NOSCRIPT),
       std::vector<std::string>()));
