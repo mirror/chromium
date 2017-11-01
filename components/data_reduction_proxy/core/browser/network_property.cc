8f1b4bb3c16f4ebf2c737c9b35ac72e777a3ec39
Tarun Bansal <tbansal@chromium.org>
2017-11-01 16:54:20 -0700

    Add network property struct to store the state of data reduction proxies.
    Bug:779219
    Change-Id: I861b4fdc4f01a3948fc12241c6eabc09c58e53e4

--- components/data_reduction_proxy/core/browser/network_property.cc
+++ components/data_reduction_proxy/core/browser/network_property.cc
@@ -16,6 +16,10 @@ static const char kInsecureProxiesState[] = "insecure_proxies_state=";
 
 namespace data_reduction_proxy {
 
+NetworkProperty::NetworkProperty()
+    : NetworkProperty(static_cast<ProxyState>(PROXY_STATE_FLAG_ALLOWED),
+                      static_cast<ProxyState>(PROXY_STATE_FLAG_ALLOWED)) {}
+
 NetworkProperty::NetworkProperty(ProxyState secure_proxies_state,
                                  ProxyState insecure_proxies_state)
     : secure_proxies_state_(secure_proxies_state),
@@ -26,8 +30,8 @@ NetworkProperty::NetworkProperty(const std::string& serialized) {
       serialized, ",", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
 
   // Initialize with the default values.
-  secure_proxies_state_ = PROXY_STATE_ALLOWED;
-  insecure_proxies_state_ = PROXY_STATE_ALLOWED;
+  secure_proxies_state_ = PROXY_STATE_FLAG_ALLOWED;
+  insecure_proxies_state_ = PROXY_STATE_FLAG_ALLOWED;
 
   DCHECK_EQ(2u, tokens.size());
 
@@ -38,8 +42,8 @@ NetworkProperty::NetworkProperty(const std::string& serialized) {
       int value = -1;
       base::StringToInt(token.substr(arraysize(kSecureProxiesState) - 1),
                         &value);
-      DCHECK(value >= 0 && value <= PROXY_STATE_LAST);
-      if (value >= 0 && value <= PROXY_STATE_LAST)
+      DCHECK(value >= 0 && value <= kMaxProxyState);
+      if (value >= 0 && value <= kMaxProxyState)
         secure_proxies_state_ = static_cast<ProxyState>(value);
     }
 
@@ -49,8 +53,8 @@ NetworkProperty::NetworkProperty(const std::string& serialized) {
       int value = -1;
       base::StringToInt(token.substr(arraysize(kInsecureProxiesState) - 1),
                         &value);
-      DCHECK(value >= 0 && value <= PROXY_STATE_LAST);
-      if (value >= 0 && value <= PROXY_STATE_LAST)
+      DCHECK(value >= 0 && value <= kMaxProxyState);
+      if (value >= 0 && value <= kMaxProxyState)
         insecure_proxies_state_ = static_cast<ProxyState>(value);
     }
   }
@@ -58,21 +62,69 @@ NetworkProperty::NetworkProperty(const std::string& serialized) {
 
 NetworkProperty::NetworkProperty(const NetworkProperty& other) = default;
 
+NetworkProperty::~NetworkProperty() {}
+
 NetworkProperty& NetworkProperty::operator=(const NetworkProperty& other) =
     default;
 
 std::string NetworkProperty::ToString() const {
+  DCHECK_LE(0, secure_proxies_state_);
+  DCHECK_GE(kMaxProxyState, secure_proxies_state_);
+  DCHECK_LE(0, insecure_proxies_state_);
+  DCHECK_GE(kMaxProxyState, insecure_proxies_state_);
+
   // Store the properties as comma separated tokens.
   return kSecureProxiesState + base::IntToString(secure_proxies_state_) + "," +
          kInsecureProxiesState + base::IntToString(insecure_proxies_state_);
 }
 
-ProxyState NetworkProperty::secure_proxies_state() const {
-  return secure_proxies_state_;
+bool NetworkProperty::IsProxyAllowed(bool secure_proxy) const {
+  ProxyState proxy_state =
+      secure_proxy ? secure_proxies_state_ : insecure_proxies_state_;
+  return proxy_state == PROXY_STATE_FLAG_ALLOWED;
 }
 
-ProxyState NetworkProperty::insecure_proxies_state() const {
-  return insecure_proxies_state_;
+bool NetworkProperty::IsSecureProxyDisallowedByCarrier() const {
+  return secure_proxies_state_ & PROXY_STATE_FLAG_DISALLOWED_BY_CARRIER;
+}
+
+void NetworkProperty::SetIsSecureProxyDisallowedByCarrier(
+    bool disallowed_by_carrier) {
+  if (IsSecureProxyDisallowedByCarrier() == disallowed_by_carrier)
+    return;
+
+  secure_proxies_state_ ^= (PROXY_STATE_FLAG_DISALLOWED_BY_CARRIER);
+}
+
+bool NetworkProperty::IsCaptivePortal() const {
+  return secure_proxies_state_ &
+         PROXY_STATE_FLAG_DISALLOWED_DUE_TO_CAPTIVE_PORTAL;
+}
+
+void NetworkProperty::SetIsCaptivePortal(bool is_captive_portal) {
+  if (IsCaptivePortal() == is_captive_portal)
+    return;
+  secure_proxies_state_ ^= (PROXY_STATE_FLAG_DISALLOWED_DUE_TO_CAPTIVE_PORTAL);
+}
+
+bool NetworkProperty::HasWarmupURLProbeFailed(bool secure_proxy) const {
+  ProxyState proxy_state =
+      secure_proxy ? secure_proxies_state_ : insecure_proxies_state_;
+  return proxy_state & PROXY_STATE_FLAG_DISALLOWED_DUE_TO_WARMUP_PROBE_FAILURE;
+}
+
+void NetworkProperty::SetHasWarmupURLProbeFailed(bool secure_proxy,
+                                                 bool warmup_url_probe_failed) {
+  if (HasWarmupURLProbeFailed(secure_proxy) == warmup_url_probe_failed)
+    return;
+
+  if (secure_proxy) {
+    secure_proxies_state_ ^=
+        (PROXY_STATE_FLAG_DISALLOWED_DUE_TO_WARMUP_PROBE_FAILURE);
+  } else {
+    insecure_proxies_state_ ^=
+        (PROXY_STATE_FLAG_DISALLOWED_DUE_TO_WARMUP_PROBE_FAILURE);
+  }
 }
 
 }  // namespace data_reduction_proxy
