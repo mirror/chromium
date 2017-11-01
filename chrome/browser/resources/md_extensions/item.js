93b30e1bd81ff813dbd75382ac828280efec5cb5
dpapad <dpapad@chromium.org>
2017-10-31 19:46:48 -0700

    MD Extensions: Reduce the amount of hidden rendered <paper-button>s.
    Before this CL, for 30 total extensions, there were
    paper-button instances: 153, hidden 94
    
    After this CL
    paper-button instances: 63, hidden 4
    
    This improves FMP time by ~12% (686ms -> 603ms, on Linux).
    
    Bug: 764126
    Change-Id: I203334058ad93d5a5decfd81f7583fb92a41c8ce

--- chrome/browser/resources/md_extensions/item.js
+++ chrome/browser/resources/md_extensions/item.js
@@ -104,9 +104,8 @@ cr.define('extensions', function() {
      * @return {boolean}
      * @private
      */
-    computeErrorsHidden_: function() {
-      return !this.data.manifestErrors.length &&
-          !this.data.runtimeErrors.length;
+    shouldShowErrorsButton_: function() {
+      return this.data.manifestErrors.length || this.data.runtimeErrors.length;
     },
 
     /** @private */
