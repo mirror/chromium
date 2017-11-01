aaeb55f007039467afc5536d2fdd948b138cfac3
sahel <sahel@chromium.org>
2017-11-01 12:13:27 -0400

    Added scroll latency metrics including both update and begin.
    Event.Latency.ScrollBegin.Wheel.* metrics measure the latency numbers
    for first GSU events of every scroll sequence and
    Event.Latency.ScrollUpdate.Wheel.* metrics measure the latency numbers
    for non-first GSU events of every scroll sequence. Reasoning about the
    performance impact of wheel_scroll_latching and async_wheel_events by
    using these existing metrics is hard since when latching is disabled
    all GSU latencies are measured by ScrollBegin metrics while when it is
    disabled latency for latched GSU events are measured by ScrollUpdate.
    This causes different counts for different groups of finch trial.
    This cl adds Event.Latency.Scroll.Wheel.* metrics to include all GSU
    events in a scroll sequence. This metric will have equal sizes in all
    finch groups.
    
    Bug:526463
    Test:RenderWidgetHostLatencyTrackerTest.TestWheelToScrollHistograms,
    Change-Id: I500ef8e12c9e4cf7277022623a2476fc67ec1386
    RenderWidgetHostLatencyTrackerTest.TestWheelToFirstScrollHistograms

--- ui/latency/latency_tracker.cc
+++ ui/latency/latency_tracker.cc
@@ -25,6 +25,22 @@ std::string LatencySourceEventTypeToInputModalityString(
   }
 }
 
+// This UMA metric tracks the time from when the original wheel event is created
+// to when the scroll gesture results in final frame swap. All scroll events are
+// included in this metric.
+void RecordUmaEventLatencyScrollWheelTimeToScrollUpdateSwapBegin2Histogram(
+    const ui::LatencyInfo::LatencyComponent& start,
+    const ui::LatencyInfo::LatencyComponent& end) {
+  CONFIRM_VALID_TIMING(start, end);
+
+  const char histogram_name[] =
+      "Event.Latency.Scroll.Wheel.TimeToScrollUpdateSwapBegin2";
+  UMA_HISTOGRAM_CUSTOM_COUNTS(
+      histogram_name,
+      (end.last_event_time - start.first_event_time).InMicroseconds(), 1,
+      1000000, 100);
+}
+
 }  // namespace
 
 void LatencyTracker::OnGpuSwapBuffersCompleted(const LatencyInfo& latency) {
@@ -109,6 +125,11 @@ void LatencyTracker::ComputeEndToEndLatencyHistograms(
             ".TimeToScrollUpdateSwapBegin2",
         original_component, gpu_swap_begin_component);
 
+    if (input_modality == "Wheel") {
+      RecordUmaEventLatencyScrollWheelTimeToScrollUpdateSwapBegin2Histogram(
+          original_component, gpu_swap_begin_component);
+    }
+
     ReportRapporScrollLatency("Event.Latency.ScrollBegin." + input_modality +
                                   ".TimeToScrollUpdateSwapBegin2",
                               original_component, gpu_swap_begin_component);
@@ -129,6 +150,11 @@ void LatencyTracker::ComputeEndToEndLatencyHistograms(
         "Event.Latency.ScrollUpdate." + input_modality +
             ".TimeToScrollUpdateSwapBegin2",
         original_component, gpu_swap_begin_component);
+
+    if (input_modality == "Wheel") {
+      RecordUmaEventLatencyScrollWheelTimeToScrollUpdateSwapBegin2Histogram(
+          original_component, gpu_swap_begin_component);
+    }
 
     ReportRapporScrollLatency("Event.Latency.ScrollUpdate." + input_modality +
                                   ".TimeToScrollUpdateSwapBegin2",
@@ -169,6 +195,12 @@ void LatencyTracker::ComputeEndToEndLatencyHistograms(
       "Event.Latency." + scroll_name + "." + input_modality +
           ".TimeToHandled2_" + thread_name,
       original_component, rendering_scheduled_component);
+
+  if (input_modality == "Wheel") {
+    UMA_HISTOGRAM_SCROLL_LATENCY_LONG_2(
+        "Event.Latency.Scroll.Wheel.TimeToHandled2_" + thread_name,
+        original_component, rendering_scheduled_component);
+  }
 
   LatencyInfo::LatencyComponent renderer_swap_component;
   if (!latency.FindLatency(ui::INPUT_EVENT_LATENCY_RENDERER_SWAP_COMPONENT, 0,
