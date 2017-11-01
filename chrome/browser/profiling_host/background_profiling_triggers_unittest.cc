4d5202681cbe5812dbe15e96a0021dfcffae6b38
Albert J. Wong <ajwong@chromium.org>
2017-11-01 14:39:50 -0700

    OOPHP: Add 3 new modes of process profiling.
    The new modes are
      Browser process only
      Gpu process only
      RendererSampling (semi-randomly samples a single renderer process in a chrome run).
    
    Bug: 777984
    Change-Id: Ibb78d516450a23e35e2195a85b35ff87f565bb7a

--- chrome/browser/profiling_host/background_profiling_triggers_unittest.cc
+++ chrome/browser/profiling_host/background_profiling_triggers_unittest.cc
@@ -11,42 +11,16 @@
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace profiling {
-
-namespace {
-
-const uint32_t kProcessMallocTriggerKb = 2 * 1024 * 1024;  // 2 Gig
-
 using GlobalMemoryDumpPtr = memory_instrumentation::mojom::GlobalMemoryDumpPtr;
 using ProcessMemoryDumpPtr =
     memory_instrumentation::mojom::ProcessMemoryDumpPtr;
 using OSMemDumpPtr = memory_instrumentation::mojom::OSMemDumpPtr;
 using ProcessType = memory_instrumentation::mojom::ProcessType;
 
-class MockProfilingProcessHost : public ProfilingProcessHost {
- public:
-  MockProfilingProcessHost() {}
-  ~MockProfilingProcessHost() override {}
+namespace profiling {
+namespace {
 
-  void SetMode(Mode mode) { ProfilingProcessHost::SetMode(mode); }
-};
-
-class TestBackgroundProfilingTriggers : public BackgroundProfilingTriggers {
- public:
-  TestBackgroundProfilingTriggers() : BackgroundProfilingTriggers(&host_) {}
-  void OnReceivedMemoryDump(bool success, GlobalMemoryDumpPtr ptr) override {
-    BackgroundProfilingTriggers::OnReceivedMemoryDump(success, std::move(ptr));
-  }
-
-  void TriggerMemoryReportForProcess(base::ProcessId pid) override {
-    pids_.insert(pid);
-  }
-
-  void Reset() { pids_.clear(); }
-
-  std::set<base::ProcessId> pids_;
-  MockProfilingProcessHost host_;
-};
+constexpr uint32_t kProcessMallocTriggerKb = 2 * 1024 * 1024;  // 2 Gig
 
 OSMemDumpPtr GetFakeOSMemDump(uint32_t resident_set_kb,
                               uint32_t private_footprint_kb) {
@@ -80,56 +54,134 @@ GlobalMemoryDumpPtr GetLargeMemoryDump() {
   PopulateMetrics(dump, 3, ProcessType::RENDERER, 1, 1);
   PopulateMetrics(dump, 4, ProcessType::GPU, kProcessMallocTriggerKb,
                   kProcessMallocTriggerKb);
+  PopulateMetrics(dump, 5, ProcessType::OTHER, kProcessMallocTriggerKb,
+                  kProcessMallocTriggerKb);
+  PopulateMetrics(dump, 6, ProcessType::RENDERER, kProcessMallocTriggerKb,
+                  kProcessMallocTriggerKb);
   return dump;
 }
 
-}  // namespace.
+}  // namespace
 
-TEST(BackgroungProfilingTriggersTest, OnReceivedMemoryDump) {
-  TestBackgroundProfilingTriggers triggers;
+class TestBackgroundProfilingTriggers : public BackgroundProfilingTriggers {
+ public:
+  explicit TestBackgroundProfilingTriggers(ProfilingProcessHost* host)
+      : BackgroundProfilingTriggers(host) {}
 
-  // Validate triggers for browser processes only.
-  triggers.host_.SetMode(ProfilingProcessHost::Mode::kMinimal);
+  using BackgroundProfilingTriggers::OnReceivedMemoryDump;
+
+  void Reset() { pids_.clear(); }
+  const std::set<base::ProcessId>& pids() { return pids_; }
+
+ private:
+  void TriggerMemoryReportForProcess(base::ProcessId pid) override {
+    pids_.insert(pid);
+  }
+
+  std::set<base::ProcessId> pids_;
+};
+
+class BackgroundProfilingTriggersTest : public testing::Test {
+ public:
+  BackgroundProfilingTriggersTest() : triggers_(&host_) {}
+
+  void SetMode(ProfilingProcessHost::Mode mode) { host_.SetMode(mode); }
+
+ protected:
+  ProfilingProcessHost host_;
+  TestBackgroundProfilingTriggers triggers_;
+};
+
+// Ensures:
+//  * robust to empty memory dumps.
+//  * does not trigger if below a size threshold.
+TEST_F(BackgroundProfilingTriggersTest, OnReceivedMemoryDump_EmptyCases) {
+  SetMode(ProfilingProcessHost::Mode::kAll);
 
   GlobalMemoryDumpPtr dump_empty(
       memory_instrumentation::mojom::GlobalMemoryDump::New());
-  triggers.OnReceivedMemoryDump(true, std::move(dump_empty));
-  EXPECT_TRUE(triggers.pids_.empty());
+  triggers_.OnReceivedMemoryDump(true, std::move(dump_empty));
+  EXPECT_TRUE(triggers_.pids().empty());
+  triggers_.Reset();
 
-  // A small browser process doesn't trigger a report.
   GlobalMemoryDumpPtr dump_browser(
       memory_instrumentation::mojom::GlobalMemoryDump::New());
   PopulateMetrics(dump_browser, 1, ProcessType::BROWSER, 1, 1);
-  triggers.OnReceivedMemoryDump(true, std::move(dump_browser));
-  EXPECT_TRUE(triggers.pids_.empty());
+  triggers_.OnReceivedMemoryDump(true, std::move(dump_browser));
+  EXPECT_TRUE(triggers_.pids().empty());
+  triggers_.Reset();
 
-  // A larger browser and GPU process must trigger a report. Renderers are not
-  // reported.
-  triggers.OnReceivedMemoryDump(true, GetLargeMemoryDump());
-  EXPECT_THAT(triggers.pids_, testing::ElementsAre(1, 4));
-  triggers.Reset();
-
-  // A small gpu process doesn't trigger a report.
   GlobalMemoryDumpPtr dump_gpu(
       memory_instrumentation::mojom::GlobalMemoryDump::New());
   PopulateMetrics(dump_gpu, 1, ProcessType::GPU, 1, 1);
-  triggers.OnReceivedMemoryDump(true, std::move(dump_gpu));
-  EXPECT_TRUE(triggers.pids_.empty());
+  triggers_.OnReceivedMemoryDump(true, std::move(dump_gpu));
+  EXPECT_TRUE(triggers_.pids().empty());
+  triggers_.Reset();
 
-  // Validate triggers for all processes.
-  triggers.host_.SetMode(ProfilingProcessHost::Mode::kAll);
+  GlobalMemoryDumpPtr dump_renderer(
+      memory_instrumentation::mojom::GlobalMemoryDump::New());
+  PopulateMetrics(dump_renderer, 1, ProcessType::RENDERER, 1, 1);
+  triggers_.OnReceivedMemoryDump(true, std::move(dump_renderer));
+  EXPECT_TRUE(triggers_.pids().empty());
+  triggers_.Reset();
 
-  // Both browser and renderer must be reported.
-  triggers.OnReceivedMemoryDump(true, GetLargeMemoryDump());
-  EXPECT_THAT(triggers.pids_, testing::ElementsAre(1, 2, 4));
-  triggers.Reset();
+  GlobalMemoryDumpPtr dump_other(
+      memory_instrumentation::mojom::GlobalMemoryDump::New());
+  PopulateMetrics(dump_other, 1, ProcessType::OTHER, 1, 1);
+  triggers_.OnReceivedMemoryDump(true, std::move(dump_other));
+  EXPECT_TRUE(triggers_.pids().empty());
+  triggers_.Reset();
+}
 
-  // Validate triggers when off.
-  triggers.host_.SetMode(ProfilingProcessHost::Mode::kNone);
+// kNone mode shold trigger nothing.
+TEST_F(BackgroundProfilingTriggersTest, OnReceivedMemoryDump_ModeNone) {
+  SetMode(ProfilingProcessHost::Mode::kNone);
 
-  triggers.OnReceivedMemoryDump(true, GetLargeMemoryDump());
-  EXPECT_TRUE(triggers.pids_.empty());
-  triggers.Reset();
+  triggers_.OnReceivedMemoryDump(true, GetLargeMemoryDump());
+  EXPECT_TRUE(triggers_.pids().empty());
+}
+
+// kAll mode only reports everything over the large threshold.
+TEST_F(BackgroundProfilingTriggersTest, OnReceivedMemoryDump_ModeAll) {
+  SetMode(ProfilingProcessHost::Mode::kAll);
+
+  // Third process is a small renderer, and thus skipped.
+  // Fifth process is type OTHER which is not handled currently and also
+  // skipped.
+  triggers_.OnReceivedMemoryDump(true, GetLargeMemoryDump());
+  EXPECT_THAT(triggers_.pids(), testing::ElementsAre(1, 2, 4, 6));
+}
+
+// kMinimal mode only reports browser and gpu processes.
+TEST_F(BackgroundProfilingTriggersTest, OnReceivedMemoryDump_ModeMinimal) {
+  SetMode(ProfilingProcessHost::Mode::kMinimal);
+
+  triggers_.OnReceivedMemoryDump(true, GetLargeMemoryDump());
+  EXPECT_THAT(triggers_.pids(), testing::ElementsAre(1, 4));
+}
+
+// kBrowser mode only reports browser.
+TEST_F(BackgroundProfilingTriggersTest, OnReceivedMemoryDump_ModeBrowser) {
+  SetMode(ProfilingProcessHost::Mode::kBrowser);
+
+  triggers_.OnReceivedMemoryDump(true, GetLargeMemoryDump());
+  EXPECT_THAT(triggers_.pids(), testing::ElementsAre(1));
+}
+
+// kGpu mode only reports gpu.
+TEST_F(BackgroundProfilingTriggersTest, OnReceivedMemoryDump_ModeGpu) {
+  SetMode(ProfilingProcessHost::Mode::kGpu);
+
+  triggers_.OnReceivedMemoryDump(true, GetLargeMemoryDump());
+  EXPECT_THAT(triggers_.pids(), testing::ElementsAre(4));
+}
+
+// kRendererSampling mode only the single profiled renderer.
+TEST_F(BackgroundProfilingTriggersTest,
+       DISABLED_OnReceivedMemoryDump_ModeRendererSampling) {
+  SetMode(ProfilingProcessHost::Mode::kRendererSampling);
+  // TODO(ajwong): RendererSampling is not correctly hooked into the triggering
+  // logic. This is a placeholder test.
 }
 
 }  // namespace profiling
