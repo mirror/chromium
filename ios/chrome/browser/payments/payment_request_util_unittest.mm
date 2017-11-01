7686cb9e6e2ef68c6b471a160d35e142ff5134f8
Mohamad Ahmadi <mahmadi@chromium.org>
2017-11-01 11:44:25 -0400

    [PR] Refactors utility methods from mediators to payment_request_util::
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I90bc25d622cc0a9248298dfb21f42de4e7ab4f53

--- ios/chrome/browser/payments/payment_request_util_unittest.mm
+++ ios/chrome/browser/payments/payment_request_util_unittest.mm
@@ -7,17 +7,156 @@
 #include "base/json/json_writer.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
+#include "components/autofill/core/browser/autofill_profile.h"
+#include "components/autofill/core/browser/autofill_test_utils.h"
+#include "components/autofill/core/browser/credit_card.h"
+#include "components/autofill/core/browser/field_types.h"
 #include "components/payments/core/basic_card_response.h"
 #include "components/payments/core/payment_address.h"
 #include "components/payments/core/payment_response.h"
+#include "ios/chrome/browser/payments/payment_request_unittest_base.h"
+#import "ios/chrome/browser/payments/test_payment_request.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "testing/platform_test.h"
 
 namespace payment_request_util {
 
-class PaymentRequestUtilTest : public testing::Test {
+class PaymentRequestUtilTest : public PaymentRequestUnitTestBase,
+                               public PlatformTest {
  protected:
   PaymentRequestUtilTest() {}
+
+  void SetUp() override { PaymentRequestUnitTestBase::SetUp(); }
 };
+
+// Tests that payment_request_util::RequestContactInfo returns true if payer's
+// name, phone number, or email address are requested and false otherwise.
+TEST_F(PaymentRequestUtilTest, RequestContactInfo) {
+  payments::WebPaymentRequest web_payment_request;
+
+  web_payment_request.options.request_payer_name = true;
+  web_payment_request.options.request_payer_phone = true;
+  web_payment_request.options.request_payer_email = true;
+  payments::TestPaymentRequest payment_request1(web_payment_request,
+                                                browser_state(), web_state(),
+                                                personal_data_manager());
+  EXPECT_TRUE(RequestContactInfo(payment_request1));
+
+  web_payment_request.options.request_payer_name = false;
+  payments::TestPaymentRequest payment_request2(web_payment_request,
+                                                browser_state(), web_state(),
+                                                personal_data_manager());
+  EXPECT_TRUE(RequestContactInfo(payment_request2));
+
+  web_payment_request.options.request_payer_phone = false;
+  payments::TestPaymentRequest payment_request3(web_payment_request,
+                                                browser_state(), web_state(),
+                                                personal_data_manager());
+  EXPECT_TRUE(RequestContactInfo(payment_request3));
+
+  web_payment_request.options.request_payer_email = false;
+  payments::TestPaymentRequest payment_request4(web_payment_request,
+                                                browser_state(), web_state(),
+                                                personal_data_manager());
+  EXPECT_FALSE(RequestContactInfo(payment_request4));
+}
+
+// Tests that payment_request_util::RequestShipping returns true if
+// requestShipping of the PaymentOptions is true.
+TEST_F(PaymentRequestUtilTest, RequestShipping) {
+  payments::WebPaymentRequest web_payment_request;
+
+  web_payment_request.options.request_shipping = true;
+  payments::TestPaymentRequest payment_request1(web_payment_request,
+                                                browser_state(), web_state(),
+                                                personal_data_manager());
+  EXPECT_TRUE(RequestShipping(payment_request1));
+
+  web_payment_request.options.request_shipping = false;
+  payments::TestPaymentRequest payment_request2(web_payment_request,
+                                                browser_state(), web_state(),
+                                                personal_data_manager());
+  EXPECT_FALSE(RequestShipping(payment_request2));
+}
+
+// Tests the output of payment_request_util::CanPay.
+TEST_F(PaymentRequestUtilTest, CanPay) {
+  payments::WebPaymentRequest web_payment_request;
+  payments::PaymentMethodData method_datum;
+  method_datum.supported_methods.push_back("basic-card");
+  method_datum.supported_networks.push_back("visa");
+  web_payment_request.method_data.push_back(method_datum);
+
+  // No selected payment method.
+  payments::TestPaymentRequest payment_request1(web_payment_request,
+                                                browser_state(), web_state(),
+                                                personal_data_manager());
+  EXPECT_FALSE(CanPay(payment_request1));
+
+  autofill::AutofillProfile profile = autofill::test::GetFullProfile();
+  // Make the profile incomplete by removing the name and the phone number so
+  // that it is not selected as the default shipping address or contact info.
+  profile.SetInfo(autofill::AutofillType(autofill::NAME_FULL), base::string16(),
+                  "en-US");
+  profile.SetInfo(autofill::AutofillType(autofill::PHONE_HOME_WHOLE_NUMBER),
+                  base::string16(), "en-US");
+  autofill::CreditCard card = autofill::test::GetCreditCard();  // Visa.
+  card.set_billing_address_id(profile.guid());
+  AddAutofillProfile(std::move(profile));
+  AddCreditCard(std::move(card));
+
+  // Has a selected payment method.
+  payments::TestPaymentRequest payment_request2(web_payment_request,
+                                                browser_state(), web_state(),
+                                                personal_data_manager());
+  EXPECT_TRUE(CanPay(payment_request2));
+
+  // No selected contact info.
+  web_payment_request.options.request_payer_phone = true;
+  payments::TestPaymentRequest payment_request3(web_payment_request,
+                                                browser_state(), web_state(),
+                                                personal_data_manager());
+  EXPECT_FALSE(CanPay(payment_request3));
+
+  profiles()[0]->SetInfo(
+      autofill::AutofillType(autofill::PHONE_HOME_WHOLE_NUMBER),
+      base::ASCIIToUTF16("16502111111"), "en-US");
+
+  // Has a selected contact info.
+  payments::TestPaymentRequest payment_request4(web_payment_request,
+                                                browser_state(), web_state(),
+                                                personal_data_manager());
+  EXPECT_TRUE(CanPay(payment_request4));
+
+  // No selected shipping address.
+  web_payment_request.options.request_shipping = true;
+  payments::TestPaymentRequest payment_request5(web_payment_request,
+                                                browser_state(), web_state(),
+                                                personal_data_manager());
+  EXPECT_FALSE(CanPay(payment_request5));
+
+  profiles()[0]->SetInfo(autofill::AutofillType(autofill::NAME_FULL),
+                         base::ASCIIToUTF16("John Doe"), "en-US");
+
+  // Has a selected shipping address, but no selected shipping option.
+  payments::TestPaymentRequest payment_request6(web_payment_request,
+                                                browser_state(), web_state(),
+                                                personal_data_manager());
+  EXPECT_FALSE(CanPay(payment_request6));
+
+  std::vector<payments::PaymentShippingOption> shipping_options;
+  payments::PaymentShippingOption option;
+  option.id = "1";
+  option.selected = true;
+  shipping_options.push_back(std::move(option));
+  web_payment_request.details.shipping_options = std::move(shipping_options);
+
+  // Has a selected shipping address and a selected shipping option.
+  payments::TestPaymentRequest payment_request7(web_payment_request,
+                                                browser_state(), web_state(),
+                                                personal_data_manager());
+  EXPECT_TRUE(CanPay(payment_request7));
+}
 
 // Tests that serializing a default PaymentResponse yields the expected result.
 TEST_F(PaymentRequestUtilTest,
