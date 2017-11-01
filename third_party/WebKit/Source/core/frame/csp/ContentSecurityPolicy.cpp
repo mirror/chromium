525828db2abded86bad68973302b3cf3e5d20736
Andy Paicu <andypaicu@chromium.org>
2017-11-01 02:12:58 +0100

    Modified the report-uri logic to not respect base elements
    When a relative URL is specified for report-uri, we should not respect
    any base elements.
    
    Spec: https://w3c.github.io/webappsec-csp/#report-violation
    
    Bug: 777350
    Change-Id: I1ca513d056047bcbe71f5faaefcd800d9f70363e

--- third_party/WebKit/Source/core/frame/csp/ContentSecurityPolicy.cpp
+++ third_party/WebKit/Source/core/frame/csp/ContentSecurityPolicy.cpp
@@ -1049,10 +1049,6 @@ const KURL ContentSecurityPolicy::Url() const {
   return execution_context_->Url();
 }
 
-KURL ContentSecurityPolicy::CompleteURL(const String& url) const {
-  return execution_context_->CompleteURL(url);
-}
-
 void ContentSecurityPolicy::EnforceSandboxFlags(SandboxFlags mask) {
   sandbox_mask_ |= mask;
 }
@@ -1331,10 +1327,15 @@ void ContentSecurityPolicy::PostViolationReport(
         DCHECK(!context_frame ||
                GetDirectiveType(violation_data.effectiveDirective()) ==
                    DirectiveType::kFrameAncestors);
-        KURL url = context_frame
-                       ? frame->GetDocument()->CompleteURLWithOverride(
-                             report_endpoint, KURL(violation_data.blockedURI()))
-                       : CompleteURL(report_endpoint);
+        KURL url =
+            context_frame
+                ? frame->GetDocument()->CompleteURLWithOverride(
+                      report_endpoint, KURL(violation_data.blockedURI()))
+                // We use the FallbackBaseURL to ensure that we don't
+                // respect base elements when determining the report
+                // endpoint URL
+                : frame->GetDocument()->CompleteURLWithOverride(
+                      report_endpoint, frame->GetDocument()->FallbackBaseURL());
         PingLoader::SendViolationReport(
             frame, url, report,
             PingLoader::kContentSecurityPolicyViolationReport);
