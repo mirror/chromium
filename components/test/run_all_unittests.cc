dacfcba688994fc6d5c8608d0f1d93b9a57d497b
Wei Li <weili@chromium.org>
2017-11-01 11:27:03 -0700

    Add printing service unit test to components unittests
    Make components unittests to be able to use and test services by
    setting proper catalog information. Also add printing service unit
    test into components unittests so it can be tested on bots.
    
    BUG=455764
    
    Change-Id: Ib3e2c59c490e65b2204786993eb880aafa0c239e

--- components/test/run_all_unittests.cc
+++ components/test/run_all_unittests.cc
@@ -2,8 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/test/test_suite.h"
 #include "components/test/components_test_suite.h"
 
+#if defined(HAS_SERVICE_IN_UNIT_TEST)
+#include "components/test/components_unittests_catalog_source.h"
+#include "services/catalog/catalog.h"
+#endif
+
 int main(int argc, char** argv) {
+  base::TestSuite test_suite(argc, argv);
+#if defined(HAS_SERVICE_IN_UNIT_TEST)
+  catalog::Catalog::SetDefaultCatalogManifest(
+      components::CreateUnittestsCatalog());
+#endif
+
   return base::LaunchUnitTests(argc, argv, GetLaunchCallback(argc, argv));
 }
