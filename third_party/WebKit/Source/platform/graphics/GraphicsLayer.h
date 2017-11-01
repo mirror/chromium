fe4fa4133b47137d3d197ce6d46200c8f11fe5f4
Francois Doray <fdoray@chromium.org>
2017-11-01 10:16:56 -0400

    Revert "Add ui::LayerDelegate::OnLayerTransformed()."
    This reverts commit faa13f8c8516dd027f5fc5a6ba984099ff330d05.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ie5bad1b35836e788d6e911c27e641d8d041fcdb4

--- third_party/WebKit/Source/platform/graphics/GraphicsLayer.h
+++ third_party/WebKit/Source/platform/graphics/GraphicsLayer.h
@@ -262,7 +262,6 @@ class PLATFORM_EXPORT GraphicsLayer : public cc::LayerClient,
   void didUpdateMainThreadScrollingReasons() override;
   void didChangeScrollbarsHidden(bool) override;
   void DidChangeLayerOpacity(float, float) override {}
-  void DidChangeLayerTransform() override {}
 
   PaintController& GetPaintController() const;
 
