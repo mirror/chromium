9580584103675be18acbae24d21523717b1f26f6
Nicolas Dossou-Gbété <dgn@chromium.org>
2017-11-01 21:18:08 +0000

    Revert "🏡 Disable the ChromeHome dependent doodle fetching"
    This reverts commit 44d6990cb200cf0193064c09243ab5c85dae19cb.
    
    Reason for revert: Was hotfix intended for merging on m63,
    but we are considering merging the real fix instead. Reverting
    as this would make the real fix fail merging.
    
    Original change's description:
    > 🏡 Disable the ChromeHome dependent doodle fetching
    > 
    > The LogoServiceFactory caches the UI at creation, but since it can
    > now change at runtime, this can cause displaying Doodles on classic
    > New Tab Pages with ChromeHome's white background.
    > 
    > As temporary workaround for M63 that won't display Doodles in
    > ChromeHome, we simply stop configuring the LogoServiceFactory to
    > fetch ChromeHome specific Doodles.
    > 
    > Bug: 776725
    > Change-Id: I1b2df2250f597671057aa10c86e5a1fa2177d8fa
    > Reviewed-on: https://chromium-review.googlesource.com/743901
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#512527}
    
    TBR=treib@chromium.org,dgn@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 776725
    Change-Id: I36ed94be9a6a9af11c01dd7c015af07292017b7a

--- chrome/browser/search_provider_logos/logo_service_factory.cc
+++ chrome/browser/search_provider_logos/logo_service_factory.cc
@@ -14,8 +14,16 @@
 #include "components/search_provider_logos/logo_service_impl.h"
 #include "net/url_request/url_request_context_getter.h"
 
+#if defined(OS_ANDROID)
+#include "chrome/browser/android/feature_utilities.h"
+#endif
+
 using search_provider_logos::LogoService;
 using search_provider_logos::LogoServiceImpl;
+
+#if defined(OS_ANDROID)
+using chrome::android::GetIsChromeHomeEnabled;
+#endif  // defined(OS_ANDROID)
 
 namespace {
 
@@ -49,12 +57,7 @@ KeyedService* LogoServiceFactory::BuildServiceInstanceFor(
   Profile* profile = static_cast<Profile*>(context);
   DCHECK(!profile->IsOffTheRecord());
 #if defined(OS_ANDROID)
-  // TODO(https://crbug.com/776725): Update LogoServiceImpl to request the
-  // latest value instead of caching it. Depending on the UI mode (ChromeHome
-  // enabled or not), the background where the logo is used changes from gray to
-  // white. This value can change at runtime, so we should stop using a value
-  // determined at factory instantiation time.
-  bool use_gray_background = true;
+  bool use_gray_background = !GetIsChromeHomeEnabled();
 #else
   bool use_gray_background = false;
 #endif
