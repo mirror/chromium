09c05c57a41798b93e3756631ec7ac0627170627
tanvir.rizvi <tanvir.rizvi@samsung.com>
2017-11-02 17:52:38 +0530

    [Informal] move SelectionTemplate:is_directional to FrameSelection
    This patch is all in one patch to get rid of is_directional_ from
    SelectionTemplate and move it to FrameSelection.
    
    BUG=740804
    
    Change-Id: I29970ef81eb634b3b82bb7ac2daee06e17d95368

--- third_party/WebKit/Source/core/html/forms/TextControlElement.cpp
+++ third_party/WebKit/Source/core/html/forms/TextControlElement.cpp
@@ -444,7 +444,6 @@ bool TextControlElement::SetSelectionRange(
                         : start_position)
           .Extend(direction == kSelectionHasBackwardDirection ? start_position
                                                               : end_position)
-          .SetIsDirectional(direction != kSelectionHasNoDirection)
           .Build(),
       SetSelectionOptions::Builder()
           .SetShouldCloseTyping(true)
@@ -630,7 +629,6 @@ SelectionInDOMTree TextControlElement::Selection() const {
   if (!inner_text->HasChildren()) {
     return SelectionInDOMTree::Builder()
         .Collapse(Position(inner_text, 0))
-        .SetIsDirectional(selectionDirection() != "none")
         .Build();
   }
 
@@ -658,7 +656,6 @@ SelectionInDOMTree TextControlElement::Selection() const {
 
   return SelectionInDOMTree::Builder()
       .SetBaseAndExtent(Position(start_node, start), Position(end_node, end))
-      .SetIsDirectional(selectionDirection() != "none")
       .Build();
 }
 
