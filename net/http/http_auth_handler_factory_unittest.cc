97b2724961ca6d3d132fbcc9e3b0575b9ad943c1
Bence Béky <bnc@chromium.org>
2017-11-02 10:52:39 -0400

    DO NOT CHECK IN.  Running perf benchmark.
    Revert "Deprecate ERR_INVALID_SPDY_STREAM net error code."
    
    This reverts commit b22c2f1ddd9301b8ad2886d057fa45b574c35e31.
    
    Bug: 780187
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I8e606b7af5ad330fff550a56180bf490b4d89b15

--- net/http/http_auth_handler_factory_unittest.cc
+++ net/http/http_auth_handler_factory_unittest.cc
@@ -53,15 +53,15 @@ TEST(HttpAuthHandlerFactoryTest, RegistryFactory) {
   SSLInfo null_ssl_info;
   HttpAuthHandlerRegistryFactory registry_factory;
   GURL gurl("www.google.com");
-  const int kBasicReturnCode = -1;
+  const int kBasicReturnCode = ERR_INVALID_SPDY_STREAM;
   MockHttpAuthHandlerFactory* mock_factory_basic =
       new MockHttpAuthHandlerFactory(kBasicReturnCode);
 
-  const int kDigestReturnCode = -2;
+  const int kDigestReturnCode = ERR_PAC_SCRIPT_FAILED;
   MockHttpAuthHandlerFactory* mock_factory_digest =
       new MockHttpAuthHandlerFactory(kDigestReturnCode);
 
-  const int kDigestReturnCodeReplace = -3;
+  const int kDigestReturnCodeReplace = ERR_SYN_REPLY_NOT_RECEIVED;
   MockHttpAuthHandlerFactory* mock_factory_digest_replace =
       new MockHttpAuthHandlerFactory(kDigestReturnCodeReplace);
 
