c03bea3ed1c1932c75d62d6d44746cdd2b501597
junweifu <junwei.fu@intel.com>
2017-11-02 16:55:00 +0800

    ShapeDetection: Create Face Detector Async
    Create Face Detector with Face Detector factory that is async.
    Add a template class represents an asynchronous operation which
    returns a result upon completion.
    Add unittest to test the result.
    
    Split original large CL[1] up in smaller subpatches including this CL.
    Link Face Detection demo[2] here.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/708336
    [2] https://codepen.io/miguelao/pen/PmJWro
    
    BUG=767021
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: Id5b19115f10d7c96da191d7421a626741b075266

--- services/shape_detection/face_detection_impl_win.h
+++ services/shape_detection/face_detection_impl_win.h
@@ -5,8 +5,10 @@
 #ifndef SERVICES_SHAPE_DETECTION_FACE_DETECTION_IMPL_WIN_H_
 #define SERVICES_SHAPE_DETECTION_FACE_DETECTION_IMPL_WIN_H_
 
+#include <windows.foundation.h>
 #include <wrl/client.h>
 
+#include "detection_utils_win.h"
 #include "services/shape_detection/public/interfaces/facedetection.mojom.h"
 
 class SkBitmap;
@@ -16,17 +18,29 @@ namespace Windows {
 namespace Media {
 namespace FaceAnalysis {
 struct IFaceDetectorStatics;
+struct FaceDetector;
+struct IFaceDetector;
 }  // namespace FaceAnalysis
 }  // namespace Media
 }  // namespace Windows
 }  // namespace ABI
 
+enum AsyncOperationStatus {
+  ASYNC_OPERATING = 1,
+  ASYNC_FAILED,
+  ASYNC_IDLE,
+};
+
 namespace shape_detection {
+
+using ABI::Windows::Foundation::IAsyncOperation;
 
 class FaceDetectionImplWin : public mojom::FaceDetection {
  public:
   using IFaceDetectorStatics =
       ABI::Windows::Media::FaceAnalysis::IFaceDetectorStatics;
+  using FaceDetector = ABI::Windows::Media::FaceAnalysis::FaceDetector;
+  using IFaceDetector = ABI::Windows::Media::FaceAnalysis::IFaceDetector;
 
   static std::unique_ptr<FaceDetectionImplWin> Create();
 
@@ -39,6 +53,19 @@ class FaceDetectionImplWin : public mojom::FaceDetection {
 
  private:
   Microsoft::WRL::ComPtr<IFaceDetectorStatics> face_detector_factory_;
+
+  HRESULT CreateFaceDetector();
+  void OnFaceDetectorCreated(
+      Microsoft::WRL::ComPtr<IAsyncOperation<FaceDetector*>> async_op);
+  std::unique_ptr<AsyncOperation<FaceDetector>> async_create_detector_ops_;
+
+  Microsoft::WRL::ComPtr<IFaceDetector> face_detector_;
+
+  FRIEND_TEST_ALL_PREFIXES(FaceDetectionImplWinTest, CreateFaceDetectorFailed);
+  friend class FaceDetectionImplWinTest;
+  void SetAsyncStatus(AsyncOperationStatus status) { async_status_ = status; }
+  AsyncOperationStatus AsyncStatus() { return async_status_; }
+  AsyncOperationStatus async_status_;
 
   DISALLOW_COPY_AND_ASSIGN(FaceDetectionImplWin);
 };
