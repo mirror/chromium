34c9b9d9b1c32d11fc048870b87c345e1bad4439
Stuart Langley <slangley@chromium.org>
2017-11-02 11:39:51 +1100

    Change custom data type from map to unordered_map in preperation for mojofication.
    clipboard_messages currently defines a CustomDataMap as:
    
    typedef std::map<base::string16, base::string16> CustomDataMap;
    
    When this moves to mojo is will become an unordered_map - this work is to make this
    change now so we can make sure it doesn't introduce and side effects.
    
    Bug: 775830
    Change-Id: I6bb8fc854027e3735bb0f5afd831a411bba82f8f

--- ui/base/clipboard/custom_data_helper_unittest.cc
+++ ui/base/clipboard/custom_data_helper_unittest.cc
@@ -17,12 +17,12 @@ namespace ui {
 namespace {
 
 void PrepareEmptyTestData(base::Pickle* pickle) {
-  std::map<base::string16, base::string16> data;
+  std::unordered_map<base::string16, base::string16> data;
   WriteCustomDataToPickle(data, pickle);
 }
 
 void PrepareTestData(base::Pickle* pickle) {
-  std::map<base::string16, base::string16> data;
+  std::unordered_map<base::string16, base::string16> data;
   data.insert(std::make_pair(ASCIIToUTF16("abc"), base::string16()));
   data.insert(std::make_pair(ASCIIToUTF16("de"), ASCIIToUTF16("1")));
   data.insert(std::make_pair(ASCIIToUTF16("f"), ASCIIToUTF16("23")));
@@ -54,7 +54,7 @@ TEST(CustomDataHelperTest, EmptyReadMap) {
   base::Pickle pickle;
   PrepareEmptyTestData(&pickle);
 
-  std::map<base::string16, base::string16> result;
+  std::unordered_map<base::string16, base::string16> result;
   ReadCustomDataIntoMap(pickle.data(), pickle.size(), &result);
   EXPECT_EQ(0u, result.size());
 }
@@ -70,7 +70,8 @@ TEST(CustomDataHelperTest, ReadTypes) {
   expected.push_back(ASCIIToUTF16("abc"));
   expected.push_back(ASCIIToUTF16("de"));
   expected.push_back(ASCIIToUTF16("f"));
-  EXPECT_EQ(expected, types);
+  EXPECT_EQ(expected.size(), types.size());
+  EXPECT_TRUE(std::equal(expected.begin(), expected.end(), types.begin()));
 }
 
 TEST(CustomDataHelperTest, ReadSingleType) {
@@ -101,10 +102,10 @@ TEST(CustomDataHelperTest, ReadMap) {
   base::Pickle pickle;
   PrepareTestData(&pickle);
 
-  std::map<base::string16, base::string16> result;
+  std::unordered_map<base::string16, base::string16> result;
   ReadCustomDataIntoMap(pickle.data(), pickle.size(), &result);
 
-  std::map<base::string16, base::string16> expected;
+  std::unordered_map<base::string16, base::string16> expected;
   expected.insert(std::make_pair(ASCIIToUTF16("abc"), base::string16()));
   expected.insert(std::make_pair(ASCIIToUTF16("de"), ASCIIToUTF16("1")));
   expected.insert(std::make_pair(ASCIIToUTF16("f"), ASCIIToUTF16("23")));
@@ -137,7 +138,7 @@ TEST(CustomDataHelperTest, BadReadTypes) {
 
 TEST(CustomDataHelperTest, BadPickle) {
   base::string16 result_data;
-  std::map<base::string16, base::string16> result_map;
+  std::unordered_map<base::string16, base::string16> result_map;
 
   base::Pickle malformed;
   malformed.WriteUInt32(1000);
