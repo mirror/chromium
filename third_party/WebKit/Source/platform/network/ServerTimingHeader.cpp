8ffb53a49bdc7d9b7f5de1cfdd67640097c00dd2
Charles Vazac <cvazac@akamai.com>
2017-11-02 14:13:19 -0500

    Update Server Timing header parsing in accordance with spec change
    See https://w3c.github.io/server-timing/#the-server-timing-header-field
    Parameters for a metric in the Server-Timing header are now be named.
    Old:
    Server-Timing: db = 123; "database access"
    
    New:
    Server-Timing: db; duration=123; description="database access"
    
    Bug:702760
    Change-Id: Ic544448898a72f9762ba054b69e68f63bb5c76b0

---
+++ third_party/WebKit/Source/platform/network/ServerTimingHeader.cpp
@@ -0,0 +1,24 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/network/ServerTimingHeader.h"
+
+namespace blink {
+
+void ServerTimingHeader::SetParam(StringView name, String value) {
+  String _name = name.ToString().LowerASCII();
+  if (_name == "duration") {
+    if (!durationSet_) {
+      duration_ = value.ToDouble();
+      durationSet_ = true;
+    }
+  } else if (_name == "description") {
+    if (!descriptionSet_) {
+      description_ = value;
+      descriptionSet_ = true;
+    }
+  }
+}
+
+}  // namespace blink
