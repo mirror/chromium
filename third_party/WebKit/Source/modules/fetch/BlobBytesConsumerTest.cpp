ef351329a634d0293819a1f0eef3b7e7b33431c8
Yutaka Hirano <yhirano@chromium.org>
2017-11-02 21:01:24 +0900

    Make WebURLError non-nullable
    This CL makes WebURLError and ResourceError non-nullable so that users
    don't have to worry whether an "error" truly represents an error or
    it is actually not an error at all.
    
    Consequently I'm removing WebURLError::Domain::kEmpty in this CL.
    
    Bug: 748491
    Change-Id: I83879a8170f30e64465ffcf817b0aad1366868b5

--- third_party/WebKit/Source/modules/fetch/BlobBytesConsumerTest.cpp
+++ third_party/WebKit/Source/modules/fetch/BlobBytesConsumerTest.cpp
@@ -91,7 +91,7 @@ class SyncErrorTestThreadableLoader : public ThreadableLoader {
 
   void Start(const ResourceRequest& request) override {
     is_started_ = true;
-    client_->DidFail(ResourceError());
+    client_->DidFail(ResourceError::Failure(NullURL()));
   }
 
   void OverrideTimeout(unsigned long timeout_milliseconds) override {
@@ -186,7 +186,7 @@ TEST_F(BlobBytesConsumerTest, FailLoading) {
   EXPECT_EQ(PublicState::kReadableOrWaiting, consumer->GetPublicState());
 
   int num_on_state_change_called = client->NumOnStateChangeCalled();
-  consumer->DidFail(ResourceError());
+  consumer->DidFail(ResourceError::Failure(NullURL()));
 
   EXPECT_EQ(num_on_state_change_called + 1, client->NumOnStateChangeCalled());
   EXPECT_EQ(PublicState::kErrored, consumer->GetPublicState());
@@ -216,7 +216,7 @@ TEST_F(BlobBytesConsumerTest, FailLoadingAfterResponseReceived) {
   EXPECT_EQ(Result::kShouldWait, consumer->BeginRead(&buffer, &available));
   EXPECT_EQ(PublicState::kReadableOrWaiting, consumer->GetPublicState());
 
-  consumer->DidFail(ResourceError());
+  consumer->DidFail(ResourceError::Failure(NullURL()));
   EXPECT_EQ(num_on_state_change_called + 2, client->NumOnStateChangeCalled());
   EXPECT_EQ(PublicState::kErrored, consumer->GetPublicState());
   EXPECT_EQ(Result::kError, consumer->BeginRead(&buffer, &available));
@@ -238,7 +238,7 @@ TEST_F(BlobBytesConsumerTest, FailAccessControlCheck) {
   EXPECT_EQ(PublicState::kReadableOrWaiting, consumer->GetPublicState());
 
   int num_on_state_change_called = client->NumOnStateChangeCalled();
-  consumer->DidFail(ResourceError());
+  consumer->DidFail(ResourceError::Failure(NullURL()));
   EXPECT_EQ(num_on_state_change_called + 1, client->NumOnStateChangeCalled());
 
   EXPECT_EQ(PublicState::kErrored, consumer->GetPublicState());
