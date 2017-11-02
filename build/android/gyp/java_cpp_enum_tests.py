ef8dfddfa8a8e5c93bdd7752c7d105ad9e7703e2
Mohamed Heikal <mheikal@google.com>
2017-11-02 09:43:31 -0400

    CamelCase prefixes now stripped by java_cpp_enum.py
    java_cpp_enum.py which converts from cpp to java enums currently only
    strips SHOUT_CASE prefixes. This change should allow it to also strip
    CamelCase prefixes.
    
    Bug: 711139
    Change-Id: I3b81467d39e374e50320448b3b0073cb69d8f883

--- build/android/gyp/java_cpp_enum_tests.py
+++ build/android/gyp/java_cpp_enum_tests.py
@@ -376,10 +376,10 @@ public @interface ClassName {
     definitions = HeaderParser(test_data).ParseDefinitions()
     definition = definitions[0]
     self.assertEqual(
-        collections.OrderedDict([('ENUM_TEST_A', '1'), ('ENUM_TEST_B', '2')]),
+        collections.OrderedDict([('A', '1'), ('B', '2')]),
         definition.entries)
     self.assertEqual(
-        collections.OrderedDict([('ENUM_TEST_B', 'comment for ENUM_TEST_B.')]),
+        collections.OrderedDict([('B', 'comment for B.')]),
         definition.comments)
 
     definition = definitions[1]
