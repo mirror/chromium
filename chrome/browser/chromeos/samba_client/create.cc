b8d38dd3b8250e798ab0b9e79e349efc14e98b07
Bailey Berro <baileyberro@chromium.org>
2017-11-02 00:44:08 -0700

    This is a stub implementation of the SmbProvidedFileSystem class that future CL's will implement.
    Bug:chromium:778791
    Change-Id: I72d82b3e137ce9010565a7a121f89e36ee623402

---
+++ chrome/browser/chromeos/samba_client/create.cc
@@ -0,0 +1,26 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/samba_client/create.h"
+
+#include <stddef.h>
+
+#include "base/memory/ptr_util.h"
+#include "chrome/browser/chromeos/file_system_provider/provided_file_system.h"
+#include "chrome/browser/chromeos/file_system_provider/provided_file_system_info.h"
+#include "chrome/browser/chromeos/file_system_provider/provided_file_system_interface.h"
+#include "chrome/browser/chromeos/file_system_provider/throttled_file_system.h"
+
+namespace chromeos {
+namespace samba_client {
+
+using file_system_provider::ProvidedFileSystemInterface;
+
+std::unique_ptr<ProvidedFileSystemInterface> CreateSambaProvidedFileSystem(
+    const file_system_provider::ProvidedFileSystemInfo& file_system_info) {
+  return base::MakeUnique<SmbProvidedFileSystem>(file_system_info);
+}
+
+}  // namespace samba_client
+}  // namespace chromeos
