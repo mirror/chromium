1d59a97c0e505a416f6af8db21154490bbcf1c84
Sam McNally <sammc@chromium.org>
2017-11-02 17:08:42 +1100

    Unify the mojo and C++ ManifestManager interfaces.
    ManifestManager will be moved from content/renderer into blink. Once
    this is done, its clients in the renderer will no longer be able to
    access it directly; they will instead use mojo. In preparation for this,
    migrate ManifestDebugInfo to be a mojo struct, add it to the mojo
    interface and migrate the renderer-process clients to use the mojo
    interface.
    
    Bug: 704441
    Change-Id: Ib95ab9370f657987811cb85c9287d2de7f38e125

--- content/renderer/installedapp/related_apps_fetcher.cc
+++ content/renderer/installedapp/related_apps_fetcher.cc
@@ -6,14 +6,14 @@
 
 #include "base/bind.h"
 #include "content/public/common/manifest.h"
-#include "content/renderer/manifest/manifest_debug_info.h"
-#include "content/renderer/manifest/manifest_manager.h"
 #include "third_party/WebKit/public/platform/WebString.h"
 #include "third_party/WebKit/public/platform/modules/installedapp/WebRelatedApplication.h"
+#include "third_party/WebKit/public/platform/modules/manifest/manifest.mojom.h"
 
 namespace content {
 
-RelatedAppsFetcher::RelatedAppsFetcher(ManifestManager* manifest_manager)
+RelatedAppsFetcher::RelatedAppsFetcher(
+    blink::mojom::ManifestManager* manifest_manager)
     : manifest_manager_(manifest_manager) {}
 
 RelatedAppsFetcher::~RelatedAppsFetcher() {}
@@ -22,7 +22,7 @@ void RelatedAppsFetcher::GetManifestRelatedApplications(
     std::unique_ptr<blink::WebCallbacks<
         const blink::WebVector<blink::WebRelatedApplication>&,
         void>> callbacks) {
-  manifest_manager_->GetManifest(
+  manifest_manager_->RequestManifest(
       base::BindOnce(&RelatedAppsFetcher::OnGetManifestForRelatedApplications,
                      base::Unretained(this), base::Passed(&callbacks)));
 }
@@ -32,20 +32,22 @@ void RelatedAppsFetcher::OnGetManifestForRelatedApplications(
         const blink::WebVector<blink::WebRelatedApplication>&,
         void>> callbacks,
     const GURL& /*url*/,
-    const Manifest& manifest,
-    const ManifestDebugInfo& /*manifest_debug_info*/) {
+    const base::Optional<Manifest>& manifest,
+    blink::mojom::ManifestDebugInfoPtr debug_info) {
   std::vector<blink::WebRelatedApplication> related_apps;
-  for (const auto& relatedApplication : manifest.related_applications) {
-    blink::WebRelatedApplication webRelatedApplication;
-    webRelatedApplication.platform =
-        blink::WebString::FromUTF16(relatedApplication.platform);
-    webRelatedApplication.id =
-        blink::WebString::FromUTF16(relatedApplication.id);
-    if (!relatedApplication.url.is_empty()) {
-      webRelatedApplication.url =
-          blink::WebString::FromUTF8(relatedApplication.url.spec());
+  if (manifest) {
+    for (const auto& related_application : manifest->related_applications) {
+      blink::WebRelatedApplication web_related_application;
+      web_related_application.platform =
+          blink::WebString::FromUTF16(related_application.platform);
+      web_related_application.id =
+          blink::WebString::FromUTF16(related_application.id);
+      if (!related_application.url.is_empty()) {
+        web_related_application.url =
+            blink::WebString::FromUTF8(related_application.url.spec());
+      }
+      related_apps.push_back(std::move(web_related_application));
     }
-    related_apps.push_back(std::move(webRelatedApplication));
   }
   callbacks->OnSuccess(std::move(related_apps));
 }
