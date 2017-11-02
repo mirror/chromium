ff42f7e9c70ccf5c8b84e8073611d00c02857aa8
Khushal <khushalsagar@chromium.org>
2017-11-01 17:09:54 -0700

    cc: Add PaintFilter for PaintRecord backed SkImageFilters.
    SkImageFilters can be backed with SkPictures which can not be
    serialized cross-process due to security constraints. Supporting these
    filters is necessary for OOP raster. This change introduces a
    PaintRecord backed SkImageFilter which replicates the behaviour of
    SkPictureImageFilter and packes the PaintRecord directly instead.
    
    R=enne@chromium.org, reed@chromium.org
    Bug:777636
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I5e6c05850f87a81f3a59817a8558c43f5b22ee64

---
+++ third_party/WebKit/Source/platform/graphics/paint/PaintFilter.h
@@ -0,0 +1,14 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PaintFilter_h
+#define PaintFilter_h
+
+#include "cc/paint/paint_filter.h"
+
+namespace blink {
+using cc::PaintFilter;
+}
+
+#endif  // PaintFilter_h
