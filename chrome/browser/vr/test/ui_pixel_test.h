49b59a810d74c1fd9501bfa882d05ec03ad944c1
bsheedy <bsheedy@chromium.org>
2017-11-03 16:33:41 -0700

    WIP Integrate VR pixel gtest with gold
    Bug:
    Change-Id: I694891d97e88358d96530061743bcec36b1e1c6c

--- chrome/browser/vr/test/ui_pixel_test.h
+++ chrome/browser/vr/test/ui_pixel_test.h
@@ -5,6 +5,8 @@
 #ifndef CHROME_BROWSER_VR_TEST_UI_PIXEL_TEST_H_
 #define CHROME_BROWSER_VR_TEST_UI_PIXEL_TEST_H_
 
+#include "base/files/file_path.h"
+#include "base/md5.h"
 #include "chrome/browser/vr/test/gl_test_environment.h"
 #include "chrome/browser/vr/test/mock_browser_interface.h"
 #include "chrome/browser/vr/test/mock_content_input_delegate.h"
@@ -18,6 +20,15 @@
 
 namespace vr {
 
+// Base class for GTest-based VR pixel tests. Tests inheriting from this class
+// are meant to be used alongside the Skia Gold image diff service and the
+// Python wrapper at //testing/scripts/run_gold_test.py. See that file and
+// https://skia.org/dev/testing/skiagold for more specifics on Gold and its
+// integration with these tests.
+
+// The general test flow is to render the VR browser and compare the output to
+// golden hashes provided by Gold. If a difference is detected, the test is
+// failed and the image uploaded to Gold for diffing.
 class UiPixelTest : public testing::Test {
  public:
   UiPixelTest();
@@ -26,8 +37,11 @@ class UiPixelTest : public testing::Test {
   void TearDown() override;
 
  protected:
+  // Generates VR browser UI state using the given states, but does not draw
   void MakeUi(const UiInitialState& ui_initial_state,
               const ToolbarState& toolbar_state);
+  // Draws the UI into the frame buffer using the current UI state and provided
+  // laser/controller/view state. Only renders the left eye.
   void DrawUi(const gfx::Vector3dF& laser_direction,
               const gfx::Point3F& laser_origin,
               UiInputManager::ButtonState button_state,
@@ -35,10 +49,21 @@ class UiPixelTest : public testing::Test {
               const gfx::Transform& controller_transform,
               const gfx::Transform& view_matrix,
               const gfx::Transform& proj_matrix);
+  // Saves whatever is currently in the frame buffer to a new SkBitmap
   std::unique_ptr<SkBitmap> SaveCurrentFrameBufferToSkBitmap();
+  // Calculates the MD5 digest of the given SkBitmap's pixel data
+  std::unique_ptr<base::MD5Digest> CalculateMd5FromSkBitmap(
+      const SkBitmap& bitmap);
+  // Captures the current frame buffer and compares its hash to the list of
+  // golden hashes. Saves the frame buffer to a file and fails the test if the
+  // generated hash is not in the list.
+  void CaptureAndCompareFrameBufferToGolden();
+  // Saves the given SkBitmap to a PNG file at the specified path.
   bool SaveSkBitmapToPng(const SkBitmap& bitmap, const std::string& filename);
 
  private:
+  static constexpr char kOutputDirSwitch[] = "image-output-directory";
+  base::FilePath image_output_directory_;
   std::unique_ptr<GlTestEnvironment> gl_test_environment_;
   std::unique_ptr<MockBrowserInterface> browser_;
   std::unique_ptr<MockContentInputDelegate> content_input_delegate_;
