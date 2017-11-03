c4cc2ef91e4526c865ee919e15770323d0aabf91
rbpotter <rbpotter@chromium.org>
2017-11-02 19:57:42 -0700

    Print Preview: Add print header contents
    Bug: 773928
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I3f2d1b172716b9e4df702b79ea1448bdd3896438

--- chrome/browser/resources/print_preview/new/app.js
+++ chrome/browser/resources/print_preview/new/app.js
@@ -2,6 +2,47 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+cr.exportPath('print_preview_new');
+
+/**
+ * @typedef {{
+ *   previewLoading: boolean,
+ *   previewFailed: boolean,
+ *   cloudPrintError: string,
+ *   privetExtensionError: string,
+ *   invalidSettings: boolean,
+ *   destinationId: string,
+ *   copies: number,
+ *   pageRange: !Array<number>,
+ *   duplex: boolean,
+ *   printTicketInvalid: boolean
+ * }}
+ */
+print_preview_new.Model;
+
 Polymer({
   is: 'print-preview-app',
+
+  properties: {
+    /**
+     * @type {!print_preview_new.Model}
+     * @private
+     */
+    model_: {
+      type: Object,
+      notify: true,
+      value: {
+        previewLoading: false,
+        previewFailed: false,
+        cloudPrintError: '',
+        privetExtensionError: '',
+        invalidSettings: false,
+        destinationId: 'Foo Printer',
+        copies: 1,
+        pageRange: [1],
+        duplex: false,
+        printTicketInvalid: false
+      },
+    },
+  }
 });
