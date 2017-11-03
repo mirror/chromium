156e7cc178a63284be20f75d25b93d7e5cafcbb6
Sophie Chang <sophiechang@chromium.org>
2017-11-03 16:39:09 -0700

    Add implementation for OptimizationGuideService
    This validates the hints protobuf file and sends a notification when the hints have been validated and processed.
    
    Bug:777892
    Change-Id: I72ca225a0d327f11849f0cd3f3198fe6c2abb06b

--- components/optimization_guide/optimization_guide_service.cc
+++ components/optimization_guide/optimization_guide_service.cc
@@ -7,8 +7,23 @@
 #include <string>
 
 #include "base/bind.h"
+#include "base/files/file.h"
+#include "base/files/file_util.h"
+#include "base/task_scheduler/post_task.h"
+#include "components/optimization_guide/notification_types.h"
+#include "content/public/browser/notification_details.h"
+#include "content/public/browser/notification_service.h"
+#include "content/public/browser/notification_source.h"
 
 namespace optimization_guide {
+
+namespace {
+
+// Version "0" corresponds to no processed version. By service conventions,
+// we represent it as a dotted triple.
+const char kNullVersion[] = "0.0.0";
+
+}  // namespace
 
 UnindexedHintsInfo::UnindexedHintsInfo(const base::Version& hints_version,
                                        const base::FilePath& hints_path)
@@ -20,11 +35,17 @@ OptimizationGuideService::OptimizationGuideService(
     const scoped_refptr<base::SingleThreadTaskRunner> io_thread_task_runner)
     : background_task_runner_(base::CreateSequencedTaskRunnerWithTraits(
           {base::MayBlock(), base::TaskPriority::BACKGROUND})),
-      io_thread_task_runner_(io_thread_task_runner) {
+      io_thread_task_runner_(io_thread_task_runner),
+      latest_processed_version_(kNullVersion) {
   DETACH_FROM_SEQUENCE(sequence_checker_);
 }
 
 OptimizationGuideService::~OptimizationGuideService() {}
+
+void OptimizationGuideService::SetLatestProcessedVersionForTesting(
+    base::Version version) {
+  latest_processed_version_ = version;
+}
 
 void OptimizationGuideService::ReadAndIndexHints(
     const UnindexedHintsInfo& unindexed_hints_info) {
@@ -37,14 +58,37 @@ void OptimizationGuideService::ReadAndIndexHints(
 void OptimizationGuideService::ReadAndIndexHintsInBackground(
     const UnindexedHintsInfo& unindexed_hints_info) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (!unindexed_hints_info.hints_version.IsValid()) {
+
+  if (!unindexed_hints_info.hints_version.IsValid())
+    return;
+  if (latest_processed_version_.CompareTo(unindexed_hints_info.hints_version) >=
+      0)
+    return;
+  if (unindexed_hints_info.hints_path.empty())
+    return;
+  std::string binary_pb;
+  if (!base::ReadFileToString(unindexed_hints_info.hints_path, &binary_pb))
+    return;
+
+  std::unique_ptr<Configuration> new_config = std::make_unique<Configuration>();
+  if (!new_config->ParseFromString(binary_pb)) {
+    DVLOG(1) << "Failed parsing proto";
     return;
   }
-  if (unindexed_hints_info.hints_path.empty()) {
-    return;
-  }
-  // TODO(crbug.com/77892): Actually process hints here and dispatch indexed
-  // hints to IO thread.
+  latest_processed_version_ = unindexed_hints_info.hints_version;
+  io_thread_task_runner_->PostTask(
+      FROM_HERE,
+      base::Bind(&OptimizationGuideService::DispatchHintsOnIOThread,
+                 base::Unretained(this), base::Passed(std::move(new_config))));
+}
+
+void OptimizationGuideService::DispatchHintsOnIOThread(
+    std::unique_ptr<Configuration> config) {
+  DCHECK(io_thread_task_runner_->BelongsToCurrentThread());
+
+  content::NotificationService::current()->Notify(
+      NOTIFICATION_HINTS_READY, content::Source<OptimizationGuideService>(this),
+      content::Details<Configuration>(config.release()));
 }
 
 }  // namespace optimization_guide
