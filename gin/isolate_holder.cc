e24f79e931850b9c61055dfa66489d48ec2e62bf
Siddhartha <ssid@chromium.org>
2017-11-03 16:39:51 -0700

    V8 dump provider should be registered on isolate thread
    The V8 dump provider is currently registered on the thread that
    IsolateHolder is created on. The isolate should be accessed on the task
    runner provided rather than the current thread.
    Make sure the IsolateHolder always gets a task runner.
    
    
    Bug: 762723
    Change-Id: I9674690362fc0287d21baa27428b5093cb5146a7

--- gin/isolate_holder.cc
+++ gin/isolate_holder.cc
@@ -15,6 +15,7 @@
 #include "base/message_loop/message_loop.h"
 #include "base/single_thread_task_runner.h"
 #include "base/sys_info.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "gin/debug_impl.h"
 #include "gin/function_template.h"
@@ -70,7 +71,8 @@ IsolateHolder::IsolateHolder(
   }
   isolate_ = v8::Isolate::New(params);
 
-  SetUp(std::move(task_runner));
+  SetUp(task_runner ? std::move(task_runner)
+                    : base::ThreadTaskRunnerHandle::Get());
 }
 
 IsolateHolder::IsolateHolder(const intptr_t* reference_table,
@@ -88,7 +90,7 @@ IsolateHolder::IsolateHolder(const intptr_t* reference_table,
       new v8::SnapshotCreator(reference_table, existing_blob));
   isolate_ = snapshot_creator_->GetIsolate();
 
-  SetUp(nullptr);
+  SetUp(base::ThreadTaskRunnerHandle::Get());
 }
 
 IsolateHolder::~IsolateHolder() {
@@ -140,11 +142,13 @@ void IsolateHolder::EnableIdleTasks(
 
 void IsolateHolder::SetUp(
     scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
+  DCHECK(task_runner);
   v8::ArrayBuffer::Allocator* allocator = g_array_buffer_allocator;
   CHECK(allocator) << "You need to invoke gin::IsolateHolder::Initialize first";
   isolate_data_.reset(
       new PerIsolateData(isolate_, allocator, access_mode_, task_runner));
-  isolate_memory_dump_provider_.reset(new V8IsolateMemoryDumpProvider(this));
+  isolate_memory_dump_provider_.reset(
+      new V8IsolateMemoryDumpProvider(this, task_runner));
 #if defined(OS_WIN)
   {
     void* code_range;
