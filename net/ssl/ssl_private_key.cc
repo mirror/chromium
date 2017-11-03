8e32f47b09fc6d519ef21d8b0b096b2cea290547
David Benjamin <davidben@chromium.org>
2017-11-03 18:58:44 -0400

    Add RSA-PSS support to Linux, MacOS, and Windows SSLPrivateKey.
    Android support will be done separately. It likely involves us switching
    from SignDigest to Sign.
    
    TODO - TEST MAC MANUALLY
    
    Bug: 752345
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Idaeb84e9c10e93efce634170e4cea528d6cbe109

--- net/ssl/ssl_private_key.cc
+++ net/ssl/ssl_private_key.cc
@@ -10,9 +10,23 @@
 
 namespace net {
 
-std::vector<uint16_t> SSLPrivateKey::DefaultPreferences(int type) {
+std::vector<uint16_t> SSLPrivateKey::DefaultPreferences(int type,
+                                                        bool supports_pss) {
   switch (type) {
     case EVP_PKEY_RSA:
+      if (supports_pss) {
+        return {
+            SSL_SIGN_RSA_PKCS1_SHA512, SSL_SIGN_RSA_PKCS1_SHA384,
+            SSL_SIGN_RSA_PKCS1_SHA256, SSL_SIGN_RSA_PKCS1_SHA1,
+
+            // Order PSS last so we preferentially use the more conservative
+            // optio. While the platform APIs may support RSA-PSS, the key may
+            // not. Ideally the SSLPrivateKey would query this, but smartcards
+            // often do not support such queries well.
+            SSL_SIGN_RSA_PSS_SHA512, SSL_SIGN_RSA_PSS_SHA384,
+            SSL_SIGN_RSA_PSS_SHA256,
+        };
+      }
       return {
           SSL_SIGN_RSA_PKCS1_SHA512, SSL_SIGN_RSA_PKCS1_SHA384,
           SSL_SIGN_RSA_PKCS1_SHA256, SSL_SIGN_RSA_PKCS1_SHA1,
