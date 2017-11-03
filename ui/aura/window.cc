42ba3daa1e539d4e32e1ef24507df45a202e60eb
Francois Doray <fdoray@chromium.org>
2017-11-03 16:44:19 -0400

    Add ui::PropertyChangeReason argument to ui::LayerDelegate methods.
    With this CL,
    ui::LayerDelegate::OnLayer(BoundsChanged|OpacityChanged|Transformed)()
    have a new argument that indicates whether the property was set
    directly or by an animation.
    
    The goal is to pass this argument to aura::WindowObserver so that
    the upcoming aura::WindowOcclusionTracker can know when window
    animations start without calling
    ui::LayerAnimator::IsAnimatingOnePropertyOf().
    
    Bug: 738387
    Change-Id: I35bf33de5eb7dee50864ef8ad8258c18fe8945f5

--- ui/aura/window.cc
+++ ui/aura/window.cc
@@ -714,7 +714,7 @@ void Window::SetBoundsInternal(const gfx::Rect& new_bounds) {
   // changed notification from the layer (this typically happens after animating
   // hidden). We must notify ourselves.
   if (layer()->delegate() != this)
-    OnLayerBoundsChanged(old_bounds);
+    OnLayerBoundsChanged(old_bounds, ui::PropertyChangeReason::SET);
 }
 
 void Window::SetVisible(bool visible) {
@@ -1055,7 +1055,8 @@ void Window::OnDelegatedFrameDamage(const gfx::Rect& damage_rect_in_dip) {
     observer.OnDelegatedFrameDamage(this, damage_rect_in_dip);
 }
 
-void Window::OnLayerBoundsChanged(const gfx::Rect& old_bounds) {
+void Window::OnLayerBoundsChanged(const gfx::Rect& old_bounds,
+                                  ui::PropertyChangeReason reason) {
   bounds_ = layer()->bounds();
 
   // Use |bounds_| as that is the bounds before any animations, which is what
@@ -1070,9 +1071,9 @@ void Window::OnLayerBoundsChanged(const gfx::Rect& old_bounds) {
     observer.OnWindowBoundsChanged(this, old_bounds, bounds_);
 }
 
-void Window::OnLayerOpacityChanged(float old_opacity, float new_opacity) {
+void Window::OnLayerOpacityChanged(ui::PropertyChangeReason reason) {
   for (WindowObserver& observer : observers_)
-    observer.OnWindowOpacityChanged(this, old_opacity, new_opacity);
+    observer.OnWindowOpacityChanged(this);
 }
 
 bool Window::CanAcceptEvent(const ui::Event& event) {
