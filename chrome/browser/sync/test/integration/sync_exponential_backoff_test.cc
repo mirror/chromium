2d17776eb0e2c247cf44246713f01efb9e548e68
Helen Li <xunjieli@chromium.org>
2017-11-03 15:16:07 -0400

    Refactor sync_manager_impl.h to use NetworkChangeObserver
    IPAddressObserver and ConnectionTypeObserver are deprecated. This CL
    modifies SyncManagerImpl to use NetworkChangeObserver instead.
    
    Note that when device is coming online, a CONNECTION_NONE will always
    be sent before the actual connection type, so I modified
    OnConnectionStatusChange to take into account the connection type before retry.
    
    Bug: 754695
    Change-Id: I25dd585a84f1c6736546e931f19596e9788b6c15

--- chrome/browser/sync/test/integration/sync_exponential_backoff_test.cc
+++ chrome/browser/sync/test/integration/sync_exponential_backoff_test.cc
@@ -79,7 +79,7 @@ IN_PROC_BROWSER_TEST_F(SyncExponentialBackoffTest, OfflineToOnline) {
   // Trigger network change notification and remember time when it happened.
   // Ensure that scheduler runs canary job immediately.
   GetFakeServer()->EnableNetwork();
-  net::NetworkChangeNotifier::NotifyObserversOfConnectionTypeChangeForTests(
+  net::NetworkChangeNotifier::NotifyObserversOfNetworkChangeForTests(
       net::NetworkChangeNotifier::CONNECTION_ETHERNET);
 
   base::Time network_notification_time = base::Time::Now();
