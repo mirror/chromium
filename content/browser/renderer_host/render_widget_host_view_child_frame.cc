b8e71070caff65dfe0843ec3f72db1337aa39018
Scott Violet <sky@chromium.org>
2017-11-03 12:02:30 -0700

    chromeos: gets WebViewGuest working with mus
    With mus we want the renderer doing the embedding to create the window
    on the window server and embed the guest renderer in it. This is done
    by having the browser obtain the guest WindowTreeClient, call
    ScheduleEmbed(), pass the token to the renderer doing the embedding
    which then calls embed using the token (done using MusEmbeddedFrame).
    
    With this flow the guest WebContents doesn't need to create an aura::Window,
    that is effectively done by the renderer.
    
    My understanding is all this code will go away within the next
    quarter, so I didn't try to isolate the USE_AURA ifdefs particularly
    well.
    
    BUG=755440
    TEST=none
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I3efb44c02e829b46c95e595c76216c9ef61b45a4

--- content/browser/renderer_host/render_widget_host_view_child_frame.cc
+++ content/browser/renderer_host/render_widget_host_view_child_frame.cc
@@ -25,6 +25,7 @@
 #include "content/browser/browser_plugin/browser_plugin_guest.h"
 #include "content/browser/compositor/surface_utils.h"
 #include "content/browser/gpu/compositor_util.h"
+#include "content/browser/mus_util.h"
 #include "content/browser/renderer_host/frame_connector_delegate.h"
 #include "content/browser/renderer_host/input/touch_selection_controller_client_child_frame.h"
 #include "content/browser/renderer_host/render_view_host_impl.h"
