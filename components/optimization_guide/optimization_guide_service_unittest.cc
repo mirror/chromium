b9d17363697c28fb595065a362d10af78c9e1477
Sophie Chang <sophiechang@chromium.org>
2017-11-03 12:46:17 -0700

    Add implementation for OptimizationGuideService
    This validates the hints protobuf file and sends a notification when the hints have been validated and processed.
    
    Bug:77892
    Change-Id: I72ca225a0d327f11849f0cd3f3198fe6c2abb06b

---
+++ components/optimization_guide/optimization_guide_service_unittest.cc
@@ -0,0 +1,179 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/optimization_guide/optimization_guide_service.h"
+
+#include <memory>
+
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/files/scoped_temp_dir.h"
+#include "base/macros.h"
+#include "base/memory/ptr_util.h"
+#include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
+#include "base/version.h"
+#include "components/optimization_guide/notification_types.h"
+#include "components/optimization_guide/proto/hints.pb.h"
+#include "content/public/browser/notification_details.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
+#include "content/public/browser/notification_source.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace optimization_guide {
+
+class TestNotificationObserver : public content::NotificationObserver {
+ public:
+  TestNotificationObserver(OptimizationGuideService* optimization_guide_service)
+      : optimization_guide_service_(optimization_guide_service) {
+    registrar_.Add(
+        this, NOTIFICATION_HINTS_READY,
+        content::Source<OptimizationGuideService>(optimization_guide_service_));
+  }
+
+  void Observe(int notification_type,
+               const content::NotificationSource& source,
+               const content::NotificationDetails& details) override {
+    ASSERT_EQ(notification_type, NOTIFICATION_HINTS_READY);
+    ASSERT_EQ(optimization_guide_service_,
+              content::Source<OptimizationGuideService>(source).ptr());
+    received_notification_ = true;
+    received_config_ = content::Details<Configuration>(details).ptr();
+  }
+
+  bool received_notification() { return received_notification_; }
+
+  const Configuration* received_config() { return received_config_; }
+
+ private:
+  OptimizationGuideService* optimization_guide_service_;
+  bool received_notification_;
+  Configuration* received_config_;
+
+  content::NotificationRegistrar registrar_;
+
+  DISALLOW_COPY_AND_ASSIGN(TestNotificationObserver);
+};
+
+class OptimizationGuideServiceTest : public testing::Test {
+ public:
+  OptimizationGuideServiceTest()
+      : optimization_guide_service_(
+            scoped_task_environment_.GetMainThreadTaskRunner()) {}
+
+  ~OptimizationGuideServiceTest() override {}
+
+  void SetUp() override { ASSERT_TRUE(temp_dir_.CreateUniqueTempDir()); }
+
+  OptimizationGuideService* optimization_guide_service() {
+    return &optimization_guide_service_;
+  }
+
+  void UpdateHints(base::Version version, base::FilePath filePath) {
+    UnindexedHintsInfo info(version, filePath);
+    optimization_guide_service_.ReadAndIndexHints(info);
+  }
+
+  void WriteConfigToFile(base::FilePath filePath, const Configuration& config) {
+    std::string serialized_config;
+    ASSERT_TRUE(config.SerializeToString(&serialized_config));
+    ASSERT_EQ(static_cast<int32_t>(serialized_config.length()),
+              base::WriteFile(filePath, serialized_config.data(),
+                              serialized_config.length()));
+  }
+
+  base::FilePath temp_dir() { return temp_dir_.GetPath(); }
+
+ protected:
+  void RunUntilIdle() {
+    scoped_task_environment_.RunUntilIdle();
+    base::RunLoop().RunUntilIdle();
+  }
+
+ private:
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
+  base::ScopedTempDir temp_dir_;
+
+  OptimizationGuideService optimization_guide_service_;
+
+  DISALLOW_COPY_AND_ASSIGN(OptimizationGuideServiceTest);
+};
+
+TEST_F(OptimizationGuideServiceTest, InvalidVersionIgnored) {
+  TestNotificationObserver observer(optimization_guide_service());
+  UpdateHints(base::Version(""), base::FilePath("blah"));
+
+  RunUntilIdle();
+
+  EXPECT_FALSE(observer.received_notification());
+}
+
+TEST_F(OptimizationGuideServiceTest, PastVersionIgnored) {
+  optimization_guide_service()->SetLatestProcessedVersionForTesting(
+      base::Version("2.0.0"));
+
+  base::FilePath filePath = temp_dir().Append("somefile.pb");
+  Configuration config;
+  Hint* hint = config.add_hints();
+  hint->set_key("google.com");
+  ASSERT_NO_FATAL_FAILURE(WriteConfigToFile(filePath, config));
+
+  TestNotificationObserver observer(optimization_guide_service());
+  UpdateHints(base::Version("1.0.0"), filePath);
+
+  RunUntilIdle();
+
+  EXPECT_FALSE(observer.received_notification());
+}
+
+TEST_F(OptimizationGuideServiceTest, EmptyFileNameIgnored) {
+  TestNotificationObserver observer(optimization_guide_service());
+  UpdateHints(base::Version("1.0.0"), base::FilePath(""));
+
+  RunUntilIdle();
+
+  EXPECT_FALSE(observer.received_notification());
+}
+
+TEST_F(OptimizationGuideServiceTest, InvalidFileIgnored) {
+  TestNotificationObserver observer(optimization_guide_service());
+  UpdateHints(base::Version("1.0.0"), base::FilePath("garbage"));
+
+  RunUntilIdle();
+
+  EXPECT_FALSE(observer.received_notification());
+}
+
+TEST_F(OptimizationGuideServiceTest, NotAConfigInFileIgnored) {
+  base::FilePath filePath = temp_dir().Append("somefile.pb");
+  ASSERT_EQ(static_cast<int32_t>(3), base::WriteFile(filePath, "boo", 3));
+
+  TestNotificationObserver observer(optimization_guide_service());
+  UpdateHints(base::Version("1.0.0"), filePath);
+
+  RunUntilIdle();
+
+  EXPECT_FALSE(observer.received_notification());
+}
+
+TEST_F(OptimizationGuideServiceTest, HappyCase) {
+  base::FilePath filePath = temp_dir().Append("somefile.pb");
+  Configuration config;
+  Hint* hint = config.add_hints();
+  hint->set_key("google.com");
+  ASSERT_NO_FATAL_FAILURE(WriteConfigToFile(filePath, config));
+
+  TestNotificationObserver observer(optimization_guide_service());
+  UpdateHints(base::Version("1.0.0"), filePath);
+
+  RunUntilIdle();
+
+  EXPECT_TRUE(observer.received_notification());
+  const Configuration* received_config = observer.received_config();
+  ASSERT_EQ(1, received_config->hints_size());
+  ASSERT_EQ("google.com", received_config->hints()[0].key());
+}
+
+}  // namespace optimization_guide
