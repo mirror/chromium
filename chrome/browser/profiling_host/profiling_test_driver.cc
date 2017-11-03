6d89a3deed32f852f9e0f0165c358ffdd7f7eefa
Erik Chen <erikchen@chromium.org>
2017-11-03 10:07:33 -0700

    compile working. all targets present.
    more fixes.
    
    
    minor changes.
    
    
    more changes.
    
    
    wip
    
    
    OOPHP: Add 3 new modes of process profiling.
    
    The new modes are
      Browser process only
      Gpu process only
      RendererSampling (semi-randomly samples a single renderer process in a chrome run).
    
    Change-Id: Ie06f639de6a67fe56de3666e8dbf9d4d21cf7816

---
+++ chrome/browser/profiling_host/profiling_test_driver.cc
@@ -0,0 +1,11 @@
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/profiling_host/profiling_test_driver.h"
+
+namespace profiling {
+
+ProfilingTestDriver::ProfilingTestDriver() {}
+ProfilingTestDriver::~ProfilingTestDriver() {}
+
+}  // namespace profiling
---
+++ chrome/browser/profiling_host/profiling_test_driver.h
@@ -0,0 +1,18 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PROFILING_HOST_PROFILING_TEST_DRIVER_H_
+#define CHROME_BROWSER_PROFILING_HOST_PROFILING_TEST_DRIVER_H_
+
+namespace profiling {
+
+class ProfilingTestDriver {
+public:
+  ProfilingTestDriver();
+  ~ProfilingTestDriver();
+};
+
+}  // namespace profiling
+
+#endif  // CHROME_BROWSER_PROFILING_HOST_PROFILING_TEST_DRIVER_H_
