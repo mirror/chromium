1d71bf776d67dd6a204020c2f07203657bcf2907
Doug Arnett <dougarnett@chromium.org>
2017-11-03 15:56:03 -0700

    Fixes previews_browsertest flakiness wrt NoScript feature initialization
    Adds test subclass that now enable NoScriptPreviews via SetUpCommandLine
    Bug: 779934
    Change-Id: I077e4c9ddac17694dd02ba641d3bc1b8da833316

--- chrome/browser/previews/previews_browsertest.cc
+++ chrome/browser/previews/previews_browsertest.cc
@@ -37,11 +37,6 @@ class PreviewsBrowserTest : public InProcessBrowserTest {
     cmd->AppendSwitchASCII("force-effective-connection-type", "Slow-2G");
   }
 
-  void EnableNoScriptPreviews() {
-    scoped_feature_list_.InitAndEnableFeature(
-        previews::features::kNoScriptPreviews);
-  }
-
   const GURL& test_url() const { return test_url_; }
 
   bool noscript_css_requested() const { return noscript_css_requested_; }
@@ -78,18 +73,23 @@ IN_PROC_BROWSER_TEST_F(PreviewsBrowserTest, NoScriptPreviewsDisabled) {
   EXPECT_FALSE(noscript_css_requested());
 }
 
-// Flaky on Win/Mac. See crbug.com/779934 for detail.
-#if defined(OS_MACOSX) || defined(OS_WIN)
-#define MAYBE_NoScriptPreviewsEnabled DISABLED_NoScriptPreviewsEnabled
-#else
-#define MAYBE_NoScriptPreviewsEnabled NoScriptPreviewsEnabled
-#endif
+// This test class enables NoScriptPreviews via command line addition.
+class PreviewsNoScriptBrowserTest : public PreviewsBrowserTest {
+ public:
+  PreviewsNoScriptBrowserTest() {}
+
+  ~PreviewsNoScriptBrowserTest() override {}
+
+  void SetUpCommandLine(base::CommandLine* cmd) override {
+    PreviewsBrowserTest::SetUpCommandLine(cmd);
+    cmd->AppendSwitchASCII("enable-features", "NoScriptPreviews");
+  }
+};
 
 // Loads a webpage that has both script and noscript tags and also requests
 // a script resource. Verifies that the noscript tag is evaluated and the
 // script resource is not loaded.
-IN_PROC_BROWSER_TEST_F(PreviewsBrowserTest, MAYBE_NoScriptPreviewsEnabled) {
-  EnableNoScriptPreviews();
+IN_PROC_BROWSER_TEST_F(PreviewsNoScriptBrowserTest, NoScriptPreviewsEnabled) {
   ui_test_utils::NavigateToURL(browser(), test_url());
 
   // Verify loaded noscript tag triggered css resource but not js one.
