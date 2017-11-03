1901f5df04363c48cb27f74c48918e954127056b
Xida Chen <xidachen@chromium.org>
2017-11-03 14:33:37 -0400

    Temporary disable main thread compositable animation UMA
    In AnimationHost::SetAnimationCounts(), we have a DCHECK to make sure
    that main_thread_animations_count_ is always >=
    main_thread_compositable_animations_count_. It appears that this could
    fail in certain cases. In particular, right now this is causing constant
    crash when chrome cast team is running some testing with debug version.
    
    Due to the severity of this bug, this CL temporary set the main thread
    compositable animations to be always 0 so that it won't report anything.
    I will further investigate for the root cause.
    
    Bug: 781305
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I9a4a4f4111b24ef5e5e3d87fd9588402dde142bd

--- third_party/WebKit/Source/core/animation/DocumentTimeline.cpp
+++ third_party/WebKit/Source/core/animation/DocumentTimeline.cpp
@@ -193,11 +193,8 @@ void DocumentTimeline::DocumentTimelineTiming::Trace(blink::Visitor* visitor) {
 
 size_t DocumentTimeline::MainThreadCompositableAnimationsCount() const {
   size_t main_thread_compositable_animations_count = 0;
-  for (Animation* animation : animations_needing_update_) {
-    if (animation->IsNonCompositedCompositable() &&
-        animation->PlayStateInternal() != Animation::kFinished)
-      main_thread_compositable_animations_count++;
-  }
+  // TODO(xidachen): Restore the calculation here. It is temporary commented out
+  // due to crbug.com/781305.
   return main_thread_compositable_animations_count;
 }
 
