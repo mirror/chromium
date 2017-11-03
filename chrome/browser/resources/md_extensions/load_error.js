e70ed7b4fe35791a80ba43a6b88c965b11b83368
dpapad <dpapad@chromium.org>
2017-11-03 13:39:06 -0700

    MD Extensions: Decouple Service and Manager.
    Service and Manager classes used to depend on each other, making
    their initialization timing complicated.
    
    This CL moves more logic into Manager, such that the dependency cycle
    is broken, Service no longer depends on Manager. As a result of this
    simplification, there is now no need to keep track of both the aggregate
    list (previously Service#extensions_) as well as the two sublists
    (Manager#apps and Manager#extensions). Manger now only keeps track of
    the sublists, and some unnecessary linear searching has been eliminated.
    
    Bug:None
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I3bdc0e9b630cc1adf98034ee4fda862b17dbfbfa

--- chrome/browser/resources/md_extensions/load_error.js
+++ chrome/browser/resources/md_extensions/load_error.js
@@ -10,6 +10,7 @@ cr.define('extensions', function() {
     /**
      * Attempts to load the previously-attempted unpacked extension.
      * @param {string} retryId
+     * @return {!Promise}
      */
     retryLoadUnpacked(retryId) {}
   }
@@ -38,8 +39,11 @@ cr.define('extensions', function() {
 
     /** @private */
     onRetryTap_: function() {
-      this.delegate.retryLoadUnpacked(this.loadError.retryGuid);
       this.close();
+      this.delegate.retryLoadUnpacked(this.loadError.retryGuid)
+          .catch(loadError => {
+            this.fire('load-error', loadError);
+          });
     },
 
     /** @private */
