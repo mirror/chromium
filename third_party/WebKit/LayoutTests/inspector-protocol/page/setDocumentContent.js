4edac0a04c8211eb3defaffad7110c8bb3adba83
Andrey Lushnikov <lushnikov@chromium.org>
2017-11-03 16:08:24 -0700

    DevTools: change Page.setDocumentContent to issue a navigation
    This patch changes Page.setDocumentContent to issue a navigation instead
    of settings content for the existing document.
    
    This allows clients to track resource loading with lifecycle events.
    
    This patch:
    - adds options "url" parameter for the Page.setDocumentContent method
      that allows to set document URL
    - starts returning loaderId that will be used to load resources
      references by the new document content
    
    R=pfeldman
    
    Change-Id: Icd30f9fbe632e48e1801f3e8d66da3d2e2e48681

--- third_party/WebKit/LayoutTests/inspector-protocol/page/setDocumentContent.js
+++ third_party/WebKit/LayoutTests/inspector-protocol/page/setDocumentContent.js
@@ -5,14 +5,21 @@
   await dp.Page.enable();
   testRunner.log('Page enabled');
 
-  var resourceTreeResponse = await dp.Page.getResourceTree();
-  var mainFrameId = resourceTreeResponse.result.frameTree.frame.id;
+  var frameTreeResponse = await dp.Page.getFrameTree();
+  var mainFrameId = frameTreeResponse.result.frameTree.frame.id;
   testRunner.log('Main Frame obtained');
 
   testRunner.log('Document content before: ' + await session.evaluate(() => document.documentElement.outerHTML));
-  await dp.Page.setDocumentContent({frameId: mainFrameId, html: '<div>こんにちは世界</div>'});
+  await dp.Page.setDocumentContent({
+    frameId: mainFrameId,
+    html: '<div>こんにちは世界</div>',
+    url: 'https://google.com'
+  });
+  // Wait for navigation to happen.
+  await dp.Page.onceFrameNavigated();
   testRunner.log('Document content set');
   testRunner.log('Document content after: ' + await session.evaluate(() => document.documentElement.outerHTML));
+  testRunner.log('    Document URL after: ' + await session.evaluate(() => window.location.href));
 
   testRunner.completeTest();
 })
