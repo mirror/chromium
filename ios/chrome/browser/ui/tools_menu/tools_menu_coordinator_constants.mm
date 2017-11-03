be11233f96436a2e0f8d2ffbb6df7124bfcbc08e
Peter Laurens <peterlaurens@google.com>
2017-11-03 13:58:56 -0700

    Box ToolsPopupController with a Coordinator class.
    ToolsPopupController is fully boxed within ToolsPopupCoordinator
    which operates with the rest of the application mostly through
    a simple command protocol for use with a CommandDispatcher, and
    some configuration providers that can configure specific details
    of the tools menu and the manner in which its presented.
    
    ToolsPopupCoordinator is created and owned by the two classes which
    previously triggered a popup menu via ToolsPopupController, the BVC
    and StackViewController.
    
    There is now also a full set of presentation notifications that are
    sent for the popup UI (willShow, didShow, willHide, didHide) and they
    are now only sent from the coordinator class.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I5ac85aaf2c4f3ed20bf2dabd4bbaf1f78aa17d19

---
+++ ios/chrome/browser/ui/tools_menu/tools_menu_coordinator_constants.mm
@@ -0,0 +1,14 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/tools_menu/tools_menu_coordinator_constants.h"
+
+NSString* const kToolsMenuWillShowNotification =
+    @"kToolsMenuWillShowNotification";
+NSString* const kToolsMenuWillHideNotification =
+    @"kToolsMenuWillHideNotification";
+NSString* const kToolsMenuDidShowNotification =
+    @"kToolsMenuDidShowNotification";
+NSString* const kToolsMenuDidHideNotification =
+    @"kToolsMenuDidHideNotification";
