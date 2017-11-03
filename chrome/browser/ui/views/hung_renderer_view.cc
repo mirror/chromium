9f5675b0bf05840cf19c92f784dd753523f061f0
Dave Tapuska <dtapuska@chromium.org>
2017-11-03 14:35:55 -0400

    Fix hung renderer dialog crashing currently responsive renderers.
    We were getting crash reports for renderers that are currently responsive.
    UMA metric added in https://chromium-review.googlesource.com/c/chromium/src/+/739644
    
    It appears that a navigation can change the view in the WebContents and
    it could now be pointing at a responsive renderer. Listen for the
    RenderViewHostChange callback and if the new host doesn't exist or is
    currently responsive remove the tab from the model; otherwise update
    the label.
    
    BUG=615090
    
    Change-Id: Ib9cf15a8337a2053d045ae33b06c9209f47b469d

--- chrome/browser/ui/views/hung_renderer_view.cc
+++ chrome/browser/ui/views/hung_renderer_view.cc
@@ -153,6 +153,10 @@ void HungPagesTableModel::TabDestroyed(WebContentsObserverImpl* tab) {
   // WARNING: we've likely been deleted.
 }
 
+void HungPagesTableModel::TabUpdated(WebContentsObserverImpl* tab) {
+  delegate_->TabUpdated();
+}
+
 HungPagesTableModel::WebContentsObserverImpl::WebContentsObserverImpl(
     HungPagesTableModel* model, WebContents* tab)
     : content::WebContentsObserver(tab),
@@ -162,6 +166,19 @@ HungPagesTableModel::WebContentsObserverImpl::WebContentsObserverImpl(
 void HungPagesTableModel::WebContentsObserverImpl::RenderProcessGone(
     base::TerminationStatus status) {
   model_->TabDestroyed(this);
+}
+
+void HungPagesTableModel::WebContentsObserverImpl::RenderViewHostChanged(
+    content::RenderViewHost* old_host,
+    content::RenderViewHost* new_host) {
+  // If |new_host| is currently responsive dismiss this dialog, otherwise
+  // let the model know the tab has been updated. Updating the tab will
+  // dismiss the current dialog but restart the hung renderer timeout.
+  if (!new_host->GetWidget()->IsCurrentlyUnresponsive()) {
+    model_->TabDestroyed(this);
+    return;
+  }
+  model_->TabUpdated(this);
 }
 
 void HungPagesTableModel::WebContentsObserverImpl::WebContentsDestroyed() {
@@ -399,6 +416,11 @@ bool HungRendererDialogView::ShouldUseCustomFrame() const {
 
 ///////////////////////////////////////////////////////////////////////////////
 // HungRendererDialogView, HungPagesTableModel::Delegate overrides:
+
+void HungRendererDialogView::TabUpdated() {
+  // Simulate clicking Wait.
+  Accept();
+}
 
 void HungRendererDialogView::TabDestroyed() {
   GetWidget()->Close();
