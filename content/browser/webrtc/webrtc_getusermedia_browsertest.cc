0792c9ddbec3ab4a22d232cd2d0a53be041aaf10
Guido Urdaneta <guidou@chromium.org>
2017-11-04 19:28:40 +0100

    Serialize MediaStreamTrack.stop() with getUserMedia()
    Bug: 778039
    Change-Id: Id7e0ee38e40265fd599dc6dd0712f0b667d2e1a5

--- content/browser/webrtc/webrtc_getusermedia_browsertest.cc
+++ content/browser/webrtc/webrtc_getusermedia_browsertest.cc
@@ -935,4 +935,12 @@ IN_PROC_BROWSER_TEST_F(WebRtcGetUserMediaBrowserTest,
   ExecuteJavascriptAndWaitForOk("applyConstraintsNonDevice()");
 }
 
+IN_PROC_BROWSER_TEST_F(WebRtcGetUserMediaBrowserTest,
+                       ConcurrentGetUserMediaStop) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+  GURL url(embedded_test_server()->GetURL("/media/getusermedia.html"));
+  NavigateToURL(shell(), url);
+  ExecuteJavascriptAndWaitForOk("concurrentGetUserMediaStop()");
+}
+
 }  // namespace content
