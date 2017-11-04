ebd69fe3ce6abeeb7965e7e7108bb229bb257b41
Eugene But <eugenebut@chromium.org>
2017-11-04 12:20:58 -0700

    Added TODO to remove WebStateObserver::InterstitialDismissed callback.
    This callback is needed because DidChangeVisibleSecurityState and
    DidFinishNavigation callbacks are not called when the user dismisses the
    interstitial via "Back To Safety" interstitial button.
    
    InterstitialDismissed is too specific and should be removed.
    
    Bug: 781553
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ia233836316c9a40f8b9bc64fb61d0c0f9dd9ad5c

--- ios/chrome/browser/tabs/tab.mm
+++ ios/chrome/browser/tabs/tab.mm
@@ -1013,6 +1013,9 @@ void TabInfoBarObserver::OnInfoBarReplaced(infobars::InfoBar* old_infobar,
 }
 
 - (void)webStateDidDismissInterstitial:(web::WebState*)webState {
+  // This callback is the only existing signal to get Omnibox updated as
+  // DidFinishNavigation are not called when the user dismisses the interstitial
+  // via "Back To Safety" interstitial button.
   [_parentTabModel notifyTabChanged:self];
 }
 
