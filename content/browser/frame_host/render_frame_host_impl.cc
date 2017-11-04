317b09c46ceee26fbfcb017107c95a2e2ce048b8
Ke He <ke.he@intel.com>
2017-11-04 12:35:11 +0800

    Add GeolocationContext mojo interface.
    This CL is one step of the Geolocation Servicification project.
    Before moving geolocation core to //services, both //content and //devtools
    shouldn't call the device::GeolocationContext directly, instead, they should
    call via mojo.
    
    1) Add GeolocationContext mojo interface.
    2) Host the GeolocationContext by Device Service.
    3) Change devtool to call the GeolocationContext mojo interface.
    
    My test steps in Ubuntu-16.04: 
    1) Build out a executable chrome, you need to obtain a Google API key
    and provide it to Chromium either at build time or at runtime. See how
    to do in https://www.chromium.org/developers/how-tos/api-keys
    
    2) I setup a SimpleHTTPServer by python, add the test pages like:
    <script type="text/javascript">
    if (navigator.geolocation)
    {  navigator.geolocation.getCurrentPosition(
         function( position ) {  
    	show_msg( position );
         },
         function( err ) {
            alert('error!')
         })
    }
    else
       alert( 'your browser doesn't support Geolocation' );
    	  
    function show_msg( position )
    {   var lat = position.coords.latitude;
        var lng = position.coords.longitude;
        alert( "latitude:" + lat + "，longitude" + lng );
    }
    </script>
    
    3) Visit the test page, verify chrome can get the Geo-position.
    4) In DevTools, choose 'Console', press "ESC" then the 'Sensors' panel
    shows, change the 'Geolocation' dropbox value, verify the Geo-position is
    overrided.
    
    BUG=725063
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ic92e3d4b10db763e6eb9dec2fa59fd0240469592

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -2960,8 +2960,7 @@ void RenderFrameHostImpl::RegisterMojoInterfaces() {
       GetProcess()->GetBrowserContext()->GetPermissionManager();
 
   if (delegate_) {
-    device::GeolocationContext* geolocation_context =
-        delegate_->GetGeolocationContext();
+    auto* geolocation_context = delegate_->GetGeolocationContext();
     if (geolocation_context && permission_manager) {
       geolocation_service_.reset(new GeolocationServiceImpl(
           geolocation_context, permission_manager, this));
