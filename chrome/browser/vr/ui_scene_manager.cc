2be1a8985d9415b91ee53527c3cbdeea4688b0e5
Ian Vollick <vollick@chromium.org>
2017-11-04 00:16:57 -0400

    [vr] Factor ContentInputDelegate out of VrShellGl
    With this change, the ContentInputDelegate interface has been converted
    into a class and its functionality moved from VrShellGl into that class
    implementation.
    
    Bug:778374
    Change-Id: I0bf43d568d4ef2c1c986201aa890a947ae1da0ab

--- chrome/browser/vr/ui_scene_manager.cc
+++ chrome/browser/vr/ui_scene_manager.cc
@@ -1161,7 +1161,7 @@ void UiSceneManager::OnAppButtonClicked() {
 }
 
 void UiSceneManager::OnAppButtonGesturePerformed(
-    UiInterface::Direction direction) {}
+    PlatformController::SwipeDirection direction) {}
 
 void UiSceneManager::SetFullscreen(bool fullscreen) {
   if (fullscreen_ == fullscreen)
