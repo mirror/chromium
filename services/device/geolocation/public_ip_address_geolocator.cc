40333572569cc15cb7c4e418e80659cf17549836
Andrew Moylan <amoylan@chromium.org>
2017-11-06 02:10:11 +1100

    PublicIpAddressGeolocator service implementation
    This CL adds public-IP-only geolocation as described in
    the design doc:
    https://docs.google.com/document/d/1oSQM3GEMTL1raWLYv8S6_Yyt93FF2zQbANI4bKJ6LDs
    
    The Device Service hosts this, in the following way:
    * It provides a PublicIpAddressGeolocationProvider interface that
      has a method to bind a request for a device::mojom::Geolocation.
    * It fulfils these requests with a PublicIpAddressGeolocator.
    * The PublicIpAddressGeolocator instance(s) all talk to a centralized
      PublicIpAddressLocationNotifier that issues IP-geolocation requests.
    
    PublicIpAddressGeolocationProvider is exposed via the Service Manager
    to the Browser, which will use it to implement GeoLanguageProvider,
    as described in this design doc:
    https://docs.google.com/document/d/18WqVHz5F9vaUiE32E8Ge6QHmku2QSJKvlqB9JjnIM-g
    
    The PublicIpAddresGeolocator service implementation reuses the
    NetworkLocationRequest class from the existing Geolocation service
    (which providesweb-platform geolocation) in //device/geolocation. Aside
    from that the implementation is separate from the web-platform
    implementation of device::mojom::Geolocation.
    
    This CL creates //services/device/geolocation/ and includes a "+net"
    DEPS entry. (This "+net" dependency, among others, will eventually be
    needed when all of //device/geolocation eventually moves into
    //services/device/geolocation as part of Geolocation Servicification.)
    
    Tests:
    * Basic test for Device Service -> PublicIpAddressGeolocationProvider
      -> PublicIpAddressGeolocator -> QueryNextPosition usage.
    * Detailed tests of PublicIpAddressGeolocationNotifier which has the
      bulk of the logic.
    
    Bug: 718694, 769158
    Change-Id: I692f8b18b459be970c8dde1cf70b8c0277d89d50

---
+++ services/device/geolocation/public_ip_address_geolocator.cc
@@ -0,0 +1,61 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/device/geolocation/public_ip_address_geolocator.h"
+
+#include "services/device/geolocation/public_ip_address_location_notifier.h"
+
+namespace device {
+
+// static
+void PublicIpAddressGeolocator::Create(
+    mojom::GeolocationRequest request,
+    const base::WeakPtr<PublicIpAddressLocationNotifier> notifier) {
+  // Create strong binding of request to a new implementation object.
+  const mojo::StrongBindingPtr<mojom::Geolocation> binding =
+      mojo::MakeStrongBinding(
+          std::make_unique<PublicIpAddressGeolocator>(notifier),
+          std::move(request));
+
+  // Give the implementation a weak reference to its binding.
+  static_cast<PublicIpAddressGeolocator*>(binding->impl())->binding_ = binding;
+}
+
+PublicIpAddressGeolocator::PublicIpAddressGeolocator(
+    const base::WeakPtr<PublicIpAddressLocationNotifier> notifier)
+    : last_updated_timestamp_(), notifier_(notifier), weak_ptr_factory_(this) {}
+
+PublicIpAddressGeolocator::~PublicIpAddressGeolocator() {}
+
+void PublicIpAddressGeolocator::QueryNextPosition(
+    QueryNextPositionCallback callback) {
+  if (query_next_position_callback_) {
+    mojo::ReportBadMessage(
+        "Overlapping calls to QueryNextPosition are prohibited.");
+    binding_->Close();
+  }
+
+  if (!notifier_)
+    return;
+
+  // Request the next position after the latest one we received.
+  notifier_->QueryNextPositionAfterTimestamp(
+      last_updated_timestamp_,
+      base::BindOnce(&PublicIpAddressGeolocator::OnPositionUpdate,
+                     weak_ptr_factory_.GetWeakPtr()));
+
+  // Retain the callback to use if/when we get a new position.
+  query_next_position_callback_ = std::move(callback);
+}
+
+// Low/high accuracy toggle is ignored by this implementation.
+void PublicIpAddressGeolocator::SetHighAccuracy(bool /* high_accuracy */) {}
+
+void PublicIpAddressGeolocator::OnPositionUpdate(
+    const mojom::Geoposition& position) {
+  last_updated_timestamp_ = base::Time::FromDoubleT(position.timestamp);
+  std::move(query_next_position_callback_).Run(position.Clone());
+}
+
+}  // namespace device
