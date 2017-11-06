58e1ff78fddbc2d351be4971ed81b9571d837ed9
junweifu <junwei.fu@intel.com>
2017-11-06 13:57:19 +0800

    ShapeDetection: Create Face Detector Async
    Create Face Detector with Face Detector factory that is async.
    Add a template class represents an asynchronous operation which
    returns a result upon completion.
    Add unittest to test the result.
    
    Split original large CL[1] up in smaller subpatches including this CL.
    Link Face Detection demo[2] here.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/708336
    [2] https://codepen.io/miguelao/pen/PmJWro
    
    BUG=767021
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: Id5b19115f10d7c96da191d7421a626741b075266

--- services/shape_detection/face_detection_impl_win.cc
+++ services/shape_detection/face_detection_impl_win.cc
@@ -16,6 +16,8 @@
 
 namespace shape_detection {
 
+using ABI::Windows::Foundation::IAsyncOperation;
+using ABI::Windows::Media::FaceAnalysis::FaceDetector;
 using base::win::ScopedHString;
 using base::win::GetActivationFactory;
 
@@ -44,7 +46,7 @@ std::unique_ptr<FaceDetectionImplWin> FaceDetectionImplWin::Create() {
   }
 
   Microsoft::WRL::ComPtr<IFaceDetectorStatics> factory;
-  const HRESULT hr = GetActivationFactory<
+  HRESULT hr = GetActivationFactory<
       IFaceDetectorStatics,
       RuntimeClass_Windows_Media_FaceAnalysis_FaceDetector>(&factory);
   if (FAILED(hr)) {
@@ -55,9 +57,21 @@ std::unique_ptr<FaceDetectionImplWin> FaceDetectionImplWin::Create() {
 
   boolean is_supported = FALSE;
   factory->get_IsSupported(&is_supported);
-  return (is_supported == FALSE)
-             ? nullptr
-             : std::make_unique<FaceDetectionImplWin>(std::move(factory));
+  if (is_supported == FALSE) {
+    DLOG(ERROR) << "FaceDetector isn't supported on the current device.";
+    return nullptr;
+  }
+
+  // Create a instance of FaceDetector asynchronously.
+  auto impl = std::make_unique<FaceDetectionImplWin>(std::move(factory));
+  hr = impl->CreateFaceDetector();
+  if (FAILED(hr)) {
+    DLOG(ERROR) << "Async Create FaceDetector failed: "
+                << logging::SystemErrorCodeToString(hr);
+    return nullptr;
+  }
+
+  return impl;
 }
 
 FaceDetectionImplWin::FaceDetectionImplWin(
@@ -69,6 +83,58 @@ FaceDetectionImplWin::~FaceDetectionImplWin() = default;
 void FaceDetectionImplWin::Detect(const SkBitmap& bitmap,
                                   DetectCallback callback) {
   DCHECK_EQ(kN32_SkColorType, bitmap.colorType());
+
+  if (async_status_ == AsyncOperationStatus::ASYNC_OPERATING) {
+    // TODO(junwei.fu): Save |callback| and |bitmap| for detecting seriality.
+  } else if (async_status_ == AsyncOperationStatus::ASYNC_IDLE) {
+    // TODO(junwei.fu): Detect face async.
+  } else if (async_status_ == AsyncOperationStatus::ASYNC_FAILED) {
+    // Return face 0.
+    std::move(callback).Run(std::vector<mojom::FaceDetectionResultPtr>());
+  }
+}
+
+// Create a instance of FaceDetector asynchronously.
+HRESULT FaceDetectionImplWin::CreateFaceDetector() {
+  Microsoft::WRL::ComPtr<IAsyncOperation<FaceDetector*>> async_op;
+  HRESULT hr = face_detector_factory_->CreateAsync(&async_op);
+  if (FAILED(hr)) {
+    DLOG(ERROR) << "Create FaceDetector failed: "
+                << logging::SystemErrorCodeToString(hr);
+    return hr;
+  }
+
+  // The once callback will not be called if this object is deleted,
+  // so we can use Unretained to bind the callback.
+  auto async_operation = AsyncOperation<FaceDetector>::Create(
+      base::BindOnce(&FaceDetectionImplWin::OnFaceDetectorCreated,
+                     base::Unretained(this)),
+      std::move(async_op));
+  if (!async_operation)
+    return E_FAIL;
+
+  async_create_detector_ops_ = std::move(async_operation);
+  async_status_ = AsyncOperationStatus::ASYNC_OPERATING;
+
+  return S_OK;
+}
+
+void FaceDetectionImplWin::OnFaceDetectorCreated(
+    Microsoft::WRL::ComPtr<IAsyncOperation<FaceDetector*>> async_op) {
+  const HRESULT hr =
+      async_op ? async_op->GetResults(face_detector_.GetAddressOf()) : E_FAIL;
+  if (FAILED(hr)) {
+    DLOG(ERROR) << "GetResults failed: "
+                << logging::SystemErrorCodeToString(hr);
+    async_status_ = AsyncOperationStatus::ASYNC_FAILED;
+    return;
+  }
+
+  async_create_detector_ops_.reset();
+
+  // The callback only for unittests if async Face Detector is succeeded.
+  if (!callback_for_testing_.is_null())
+    std::move(callback_for_testing_).Run();
 }
 
 }  // namespace shape_detection
