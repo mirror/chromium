ce651a4acfe7e4d1bf9d6d2b250a80cc502d9c9b
Randy Smith <rdsmith@chromium.org>
2017-11-05 22:04:11 -0500

    Refactor use of StoragePartition::ClearData* methods.
    This refactor is to remove the passed URLRequestContextGetter as an
    argument, preperatory to shifting cookie deletion in StoragePartitionImpl
    over to Mojo interfaces.
    
    Bug: 721395
    Change-Id: I4568cb5ffd98591605a7c5d8b3850c5d8cd5d9b7

--- chrome/browser/extensions/data_deleter.cc
+++ chrome/browser/extensions/data_deleter.cc
@@ -19,6 +19,8 @@
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/manifest_handlers/app_isolation_info.h"
+#include "net/cookies/cookie_store.h"
+#include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_getter.h"
 
 using base::WeakPtr;
@@ -29,6 +31,54 @@ using content::StoragePartition;
 namespace extensions {
 
 namespace {
+
+// Provide a callback wrapper that delays calling the passed callback
+// until the wrapper has been called a number of times specified in the
+// constructor.  This class owns itself and will delete itself after the
+// specified number of calls.
+class CallbackWrapper {
+ public:
+  CallbackWrapper(base::OnceClosure callback, int wraps)
+      : callback_(std::move(callback)), wraps_(wraps) {}
+
+  base::Closure WrappingCallback() {
+    return base::Bind(&CallbackWrapper::WrappingFunction,
+                      base::Unretained(this));
+  }
+
+ private:
+  void WrappingFunction() {
+    DCHECK_GT(wraps_, 0);
+    if (--wraps_ == 0) {
+      std::move(callback_).Run();
+      delete this;
+    }
+  }
+
+  base::OnceClosure callback_;
+  int wraps_;
+};
+
+bool DoesCookieMatchHost(const std::string& host,
+                         const net::CanonicalCookie& cookie) {
+  return cookie.IsHostCookie() && cookie.IsDomainMatch(host);
+}
+
+void OnClearedCookies(base::OnceClosure callback, uint32_t /* num_deleted */) {
+  BrowserThread::PostTask(BrowserThread::UI, FROM_HERE, std::move(callback));
+}
+
+// TODO: Trace out the scoped refptr requirements.
+void ClearCookiesOnIOThread(scoped_refptr<net::URLRequestContextGetter> context,
+                            GURL origin,
+                            base::OnceClosure ui_callback) {
+  net::CookieStore* cookie_store =
+      context->GetURLRequestContext()->cookie_store();
+  cookie_store->DeleteAllCreatedBetweenWithPredicateAsync(
+      base::Time(), base::Time::Max(),
+      base::Bind(&DoesCookieMatchHost, origin.host()),
+      base::BindOnce(&OnClearedCookies, std::move(ui_callback)));
+}
 
 // Helper function that deletes data of a given |storage_origin| in a given
 // |partition|.
@@ -50,12 +100,27 @@ void DeleteOrigin(Profile* profile,
     // preserve this code path without checking for isolation because it's
     // simpler than special casing.  This code should go away once we merge
     // the various URLRequestContexts (http://crbug.com/159193).
+
+    // Self-owned.
+    CallbackWrapper* wrapper = new CallbackWrapper(callback, 2);
+
     partition->ClearDataForOrigin(
         ~StoragePartition::REMOVE_DATA_MASK_SHADER_CACHE,
-        StoragePartition::QUOTA_MANAGED_STORAGE_MASK_ALL,
-        origin,
-        profile->GetRequestContextForExtensions(),
-        callback);
+        StoragePartition::QUOTA_MANAGED_STORAGE_MASK_ALL, origin,
+        profile->GetRequestContextForExtensions(), wrapper->WrappingCallback());
+
+    // Delete cookies separately from other data so that the request context
+    // for extensions doesn't need to be passed into the StoragePartition.
+    // TODO(rdsmith): Mojoify this call and get rid of the thread hopping.
+    BrowserThread::PostTask(
+        BrowserThread::IO, FROM_HERE,
+        base::BindOnce(
+            &ClearCookiesOnIOThread,
+            // Safe not to refcount because deletion of the
+            // request context getter happens on the IO thread and
+            // thus must occur after the execution of this PostTask.
+            base::WrapRefCounted(profile->GetRequestContextForExtensions()),
+            origin, wrapper->WrappingCallback()));
   } else {
     // We don't need to worry about the media request context because that
     // shares the same cookie store as the main request context.
