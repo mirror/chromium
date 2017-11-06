52da1fed418caa926ee3246e4ed939c383035569
Finnur Thorarinsson <finnur@chromium.org>
2017-11-06 16:36:03 +0000

    Win10 Native Notifications: Support images.
    Introduce a class that supports hanging on to temporary
    files long enough for the OS to pick them up (and clean
    them up after restart of Chrome if it fails the first
    time around). Necessary because Windows doesn't
    guarantee temp file deletion on shutdown/restart.
    
    Bug: 734095
    Change-Id: I2926dc33b489f150a75f97792266e1a31a4ea5a2

--- chrome/browser/notifications/notification_platform_bridge_win.cc
+++ chrome/browser/notifications/notification_platform_bridge_win.cc
@@ -5,25 +5,32 @@
 #include "chrome/browser/notifications/notification_platform_bridge_win.h"
 
 #include <activation.h>
+#include <windows.ui.notifications.h>
 #include <wrl/client.h>
 #include <wrl/event.h>
 #include <wrl/wrappers/corewrappers.h>
 #include <memory>
 #include <set>
+#include <unordered_map>
 #include <utility>
 
 #include "base/bind.h"
 #include "base/logging.h"
 #include "base/strings/string16.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/win/core_winrt_util.h"
 #include "base/win/scoped_hstring.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/notifications/notification_common.h"
 #include "chrome/browser/notifications/notification_display_service_factory.h"
+#include "chrome/browser/notifications/notification_image_retainer.h"
 #include "chrome/browser/notifications/notification_template_builder.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/installer/util/install_util.h"
 #include "chrome/installer/util/shell_util.h"
+#include "components/version_info/channel.h"
 #include "content/public/browser/browser_thread.h"
 #include "ui/message_center/notification.h"
 
@@ -36,14 +43,6 @@ namespace winxml = ABI::Windows::Data::Xml;
 
 using base::win::ScopedHString;
 using message_center::RichNotificationData;
-
-typedef winfoundtn::ITypedEventHandler<winui::Notifications::ToastNotification*,
-                                       IInspectable*>
-    ToastActivatedHandler;
-typedef winfoundtn::ITypedEventHandler<
-    winui::Notifications::ToastNotification*,
-    winui::Notifications::ToastDismissedEventArgs*>
-    ToastDismissedHandler;
 
 // Hold related data for a notification.
 struct NotificationData {
@@ -72,6 +71,14 @@ struct NotificationData {
 };
 
 namespace {
+
+typedef winfoundtn::ITypedEventHandler<winui::Notifications::ToastNotification*,
+                                       IInspectable*>
+    ToastActivatedHandler;
+typedef winfoundtn::ITypedEventHandler<
+    winui::Notifications::ToastNotification*,
+    winui::Notifications::ToastDismissedEventArgs*>
+    ToastDismissedHandler;
 
 // Templated wrapper for winfoundtn::GetActivationFactory().
 template <unsigned int size, typename T>
@@ -199,9 +206,211 @@ NotificationPlatformBridge* NotificationPlatformBridge::Create() {
   return new NotificationPlatformBridgeWin();
 }
 
+class NotificationPlatformBridgeWinImpl
+    : public base::RefCountedThreadSafe<NotificationPlatformBridgeWinImpl>,
+      public content::BrowserThread::DeleteOnThread<
+          content::BrowserThread::IO> {
+ public:
+  explicit NotificationPlatformBridgeWinImpl(
+      base::SequencedTaskRunner* task_runner)
+      : image_retainer_(
+            std::make_unique<NotificationImageRetainer>(task_runner)),
+        task_runner_(task_runner) {
+    DCHECK(task_runner_);
+    com_functions_initialized_ =
+        base::win::ResolveCoreWinRTDelayload() &&
+        ScopedHString::ResolveCoreWinRTStringDelayload();
+  }
+
+  void Display(NotificationCommon::Type notification_type,
+               const std::string& notification_id,
+               const std::string& profile_id,
+               bool incognito,
+               const message_center::Notification& notification,
+               std::unique_ptr<NotificationCommon::Metadata> metadata) {
+    // TODO(finnur): Move this to a RoInitialized thread, as per
+    // crbug.com/761039.
+    DCHECK(task_runner_->RunsTasksInCurrentSequence());
+
+    NotificationData* data = FindNotificationData(
+        notification_id, profile_id, notification.origin_url(), incognito);
+    if (!data) {
+      data =
+          new NotificationData(notification_type, notification_id, profile_id,
+                               incognito, notification.origin_url());
+      notifications_.emplace(data, base::WrapUnique(data));
+    }
+
+    if (!notifier_.Get() && FAILED(InitializeToastNotifier())) {
+      LOG(ERROR) << "Unable to initialize toast notifier";
+      return;
+    }
+
+    std::unique_ptr<NotificationTemplateBuilder> notification_template =
+        NotificationTemplateBuilder::Build(
+            notification_id, image_retainer_.get(), profile_id, notification);
+    mswr::ComPtr<winui::Notifications::IToastNotification> toast;
+    HRESULT hr = GetToastNotification(*notification_template, &toast);
+    if (FAILED(hr)) {
+      LOG(ERROR) << "Unable to get a toast notification";
+      return;
+    }
+
+    auto activated_handler = mswr::Callback<ToastActivatedHandler>(
+        this, &NotificationPlatformBridgeWinImpl::OnActivated);
+    EventRegistrationToken activated_token;
+    hr = toast->add_Activated(activated_handler.Get(), &activated_token);
+    if (FAILED(hr)) {
+      LOG(ERROR) << "Unable to add toast activated event handler";
+      return;
+    }
+
+    auto dismissed_handler = mswr::Callback<ToastDismissedHandler>(
+        this, &NotificationPlatformBridgeWinImpl::OnDismissed);
+    EventRegistrationToken dismissed_token;
+    hr = toast->add_Dismissed(dismissed_handler.Get(), &dismissed_token);
+    if (FAILED(hr)) {
+      LOG(ERROR) << "Unable to add toast dismissed event handler";
+      return;
+    }
+
+    hr = notifier_->Show(toast.Get());
+    if (FAILED(hr))
+      LOG(ERROR) << "Unable to display the notification";
+  }
+
+  void Close(const std::string& profile_id,
+             const std::string& notification_id) {
+    DCHECK(task_runner_->RunsTasksInCurrentSequence());
+    // TODO(peter): Implement the ability to close notifications.
+  }
+
+  void GetDisplayed(const std::string& profile_id,
+                    bool incognito,
+                    const GetDisplayedNotificationsCallback& callback) const {
+    DCHECK(task_runner_->RunsTasksInCurrentSequence());
+    auto displayed_notifications = std::make_unique<std::set<std::string>>();
+    content::BrowserThread::PostTask(
+        content::BrowserThread::UI, FROM_HERE,
+        base::Bind(callback, base::Passed(&displayed_notifications),
+                   false /* supports_synchronization */));
+  }
+
+  void SetReadyCallback(
+      NotificationPlatformBridge::NotificationBridgeReadyCallback callback) {
+    DCHECK(task_runner_->RunsTasksInCurrentSequence());
+    std::move(callback).Run(com_functions_initialized_);
+  }
+
+ private:
+  friend class base::RefCountedThreadSafe<NotificationPlatformBridgeWinImpl>;
+
+  ~NotificationPlatformBridgeWinImpl() = default;
+
+  HRESULT OnActivated(winui::Notifications::IToastNotification* notification,
+                      IInspectable* /* inspectable */) {
+    // TODO(chengx): We need to write profile id and incognito information into
+    // the toast, so that we can retrieve them from |notification| here.
+    std::string notification_id = "";
+    std::string profile_id = "";
+    bool incognito = false;
+    GURL origin_url;
+
+    NotificationData* data = FindNotificationData(notification_id, profile_id,
+                                                  origin_url, incognito);
+    if (data)
+      ForwardNotificationOperation(data, NotificationCommon::CLICK);
+
+    return S_OK;
+  }
+
+  HRESULT OnDismissed(
+      winui::Notifications::IToastNotification* notification,
+      winui::Notifications::IToastDismissedEventArgs* /* args */) {
+    // TODO(chengx): We need to write profile_id and incognito information into
+    // the toast, so that we can retrieve them from |notification| here.
+    std::string notification_id = "";
+    std::string profile_id = "";
+    bool incognito = false;
+    GURL origin_url;
+
+    NotificationData* data = FindNotificationData(notification_id, profile_id,
+                                                  origin_url, incognito);
+    if (data) {
+      ForwardNotificationOperation(data, NotificationCommon::CLOSE);
+      notifications_.erase(data);
+    }
+
+    return S_OK;
+  }
+
+  // Returns a notification with properties |notification_id|, |profile_id|,
+  // |origin_url| and |incognito| if found in notifications_. Returns nullptr if
+  // not found.
+  NotificationData* FindNotificationData(const std::string& notification_id,
+                                         const std::string& profile_id,
+                                         const GURL& origin_url,
+                                         bool incognito) {
+    for (const auto& item : notifications_) {
+      NotificationData* data = item.first;
+      if (data->notification_id == notification_id &&
+          data->profile_id == profile_id && data->origin_url == origin_url &&
+          data->incognito == incognito) {
+        return data;
+      }
+    }
+
+    return nullptr;
+  }
+
+  HRESULT InitializeToastNotifier() {
+    mswr::ComPtr<winui::Notifications::IToastNotificationManagerStatics>
+        toast_manager;
+    HRESULT hr = CreateActivationFactory(
+        RuntimeClass_Windows_UI_Notifications_ToastNotificationManager,
+        toast_manager.GetAddressOf());
+    if (FAILED(hr)) {
+      LOG(ERROR) << "Unable to create the ToastNotificationManager";
+      return hr;
+    }
+
+    base::string16 browser_model_id =
+        ShellUtil::GetBrowserModelId(InstallUtil::IsPerUserInstall());
+    ScopedHString application_id = ScopedHString::Create(browser_model_id);
+    hr = toast_manager->CreateToastNotifierWithId(application_id.get(),
+                                                  &notifier_);
+    if (FAILED(hr))
+      LOG(ERROR) << "Unable to create the ToastNotifier";
+    return hr;
+  }
+
+  // Stores the set of Notifications in a session.
+  // A std::set<std::unique_ptr<T>> doesn't work well because e.g.,
+  // std::set::erase(T) would require a std::unique_ptr<T> argument, so the data
+  // would get double-destructed.
+  template <typename T>
+  using UnorderedUniqueSet = std::unordered_map<T*, std::unique_ptr<T>>;
+  UnorderedUniqueSet<NotificationData> notifications_;
+
+  // Whether the required functions from combase.dll have been loaded.
+  bool com_functions_initialized_;
+
+  // An object that keeps temp files alive long enough for Windows to pick up.
+  std::unique_ptr<NotificationImageRetainer> image_retainer_;
+
+  // The task runner this object runs on.
+  scoped_refptr<base::SequencedTaskRunner> task_runner_;
+
+  // The ToastNotifier to use to communicate with the Action Center.
+  mswr::ComPtr<winui::Notifications::IToastNotifier> notifier_;
+
+  DISALLOW_COPY_AND_ASSIGN(NotificationPlatformBridgeWinImpl);
+};
+
 NotificationPlatformBridgeWin::NotificationPlatformBridgeWin() {
-  com_functions_initialized_ = base::win::ResolveCoreWinRTDelayload() &&
-                               ScopedHString::ResolveCoreWinRTStringDelayload();
+  task_runner_ = base::CreateSequencedTaskRunnerWithTraits(
+      {base::MayBlock(), base::TaskPriority::USER_BLOCKING});
+  impl_ = new NotificationPlatformBridgeWinImpl(task_runner_.get());
 }
 
 NotificationPlatformBridgeWin::~NotificationPlatformBridgeWin() = default;
@@ -210,148 +419,50 @@ void NotificationPlatformBridgeWin::Display(
     NotificationCommon::Type notification_type,
     const std::string& notification_id,
     const std::string& profile_id,
-    bool incognito,
+    bool is_incognito,
     const message_center::Notification& notification,
     std::unique_ptr<NotificationCommon::Metadata> metadata) {
-  // TODO(finnur): Move this to a RoInitialized thread, as per crbug.com/761039.
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
-  NotificationData* data = FindNotificationData(
-      notification_id, profile_id, notification.origin_url(), incognito);
-  if (!data) {
-    data = new NotificationData(notification_type, notification_id, profile_id,
-                                incognito, notification.origin_url());
-    notifications_.emplace(data, base::WrapUnique(data));
-  }
+  // Make a deep copy of the notification as its resources cannot safely
+  // be passed between threads.
+  auto notification_copy = message_center::Notification::DeepCopy(
+      notification, /*include_body_image=*/true, /*include_small_image=*/true,
+      /*include_icon_images=*/true);
 
-  mswr::ComPtr<winui::Notifications::IToastNotificationManagerStatics>
-      toast_manager;
-  HRESULT hr = CreateActivationFactory(
-      RuntimeClass_Windows_UI_Notifications_ToastNotificationManager,
-      toast_manager.GetAddressOf());
-  if (FAILED(hr)) {
-    LOG(ERROR) << "Unable to create the ToastNotificationManager";
-    return;
-  }
-
-  base::string16 browser_model_id =
-      ShellUtil::GetBrowserModelId(InstallUtil::IsPerUserInstall());
-  ScopedHString application_id = ScopedHString::Create(browser_model_id);
-  mswr::ComPtr<winui::Notifications::IToastNotifier> notifier;
-  hr =
-      toast_manager->CreateToastNotifierWithId(application_id.get(), &notifier);
-  if (FAILED(hr)) {
-    LOG(ERROR) << "Unable to create the ToastNotifier";
-    return;
-  }
-
-  std::unique_ptr<NotificationTemplateBuilder> notification_template =
-      NotificationTemplateBuilder::Build(notification_id, notification);
-  mswr::ComPtr<winui::Notifications::IToastNotification> toast;
-  hr = GetToastNotification(*notification_template, &toast);
-  if (FAILED(hr)) {
-    LOG(ERROR) << "Unable to get a toast notification";
-    return;
-  }
-
-  auto activated_handler = mswr::Callback<ToastActivatedHandler>(
-      this, &NotificationPlatformBridgeWin::OnActivated);
-  EventRegistrationToken activated_token;
-  hr = toast->add_Activated(activated_handler.Get(), &activated_token);
-  if (FAILED(hr)) {
-    LOG(ERROR) << "Unable to add toast activated event handler";
-    return;
-  }
-
-  auto dismissed_handler = mswr::Callback<ToastDismissedHandler>(
-      this, &NotificationPlatformBridgeWin::OnDismissed);
-  EventRegistrationToken dismissed_token;
-  hr = toast->add_Dismissed(dismissed_handler.Get(), &dismissed_token);
-  if (FAILED(hr)) {
-    LOG(ERROR) << "Unable to add toast dismissed event handler";
-    return;
-  }
-
-  hr = notifier->Show(toast.Get());
-  if (FAILED(hr))
-    LOG(ERROR) << "Unable to display the notification";
+  PostTaskToTaskRunnerThread(base::BindOnce(
+      &NotificationPlatformBridgeWinImpl::Display, impl_, notification_type,
+      notification_id, profile_id, is_incognito, *notification_copy,
+      base::Passed(&metadata)));
 }
 
 void NotificationPlatformBridgeWin::Close(const std::string& profile_id,
                                           const std::string& notification_id) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  // TODO(peter): Implement the ability to close notifications.
+  PostTaskToTaskRunnerThread(
+      base::BindOnce(&NotificationPlatformBridgeWinImpl::Close, impl_,
+                     notification_id, profile_id));
 }
 
 void NotificationPlatformBridgeWin::GetDisplayed(
     const std::string& profile_id,
     bool incognito,
     const GetDisplayedNotificationsCallback& callback) const {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  auto displayed_notifications = std::make_unique<std::set<std::string>>();
-  content::BrowserThread::PostTask(
-      content::BrowserThread::UI, FROM_HERE,
-      base::Bind(callback, base::Passed(&displayed_notifications),
-                 false /* supports_synchronization */));
+  PostTaskToTaskRunnerThread(
+      base::BindOnce(&NotificationPlatformBridgeWinImpl::GetDisplayed, impl_,
+                     profile_id, incognito, callback));
 }
 
 void NotificationPlatformBridgeWin::SetReadyCallback(
     NotificationBridgeReadyCallback callback) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  std::move(callback).Run(com_functions_initialized_);
+  PostTaskToTaskRunnerThread(
+      base::BindOnce(&NotificationPlatformBridgeWinImpl::SetReadyCallback,
+                     impl_, base::Passed(&callback)));
 }
 
-HRESULT NotificationPlatformBridgeWin::OnActivated(
-    winui::Notifications::IToastNotification* notification,
-    IInspectable* /* inspectable */) {
-  // TODO(chengx): We need to write profile id and incognito information into
-  // the toast, so that we can retrieve them from |notification| here.
-  std::string notification_id = "";
-  std::string profile_id = "";
-  bool incognito = false;
-  GURL origin_url;
-
-  NotificationData* data =
-      FindNotificationData(notification_id, profile_id, origin_url, incognito);
-  if (data)
-    ForwardNotificationOperation(data, NotificationCommon::CLICK);
-
-  return S_OK;
-}
-
-HRESULT NotificationPlatformBridgeWin::OnDismissed(
-    winui::Notifications::IToastNotification* notification,
-    winui::Notifications::IToastDismissedEventArgs* /* args */) {
-  // TODO(chengx): We need to write profile_id and incognito information into
-  // the toast, so that we can retrieve them from |notification| here.
-  std::string notification_id = "";
-  std::string profile_id = "";
-  bool incognito = false;
-  GURL origin_url;
-
-  NotificationData* data =
-      FindNotificationData(notification_id, profile_id, origin_url, incognito);
-  if (data) {
-    ForwardNotificationOperation(data, NotificationCommon::CLOSE);
-    notifications_.erase(data);
-  }
-
-  return S_OK;
-}
-
-NotificationData* NotificationPlatformBridgeWin::FindNotificationData(
-    const std::string& notification_id,
-    const std::string& profile_id,
-    const GURL& origin_url,
-    bool incognito) {
-  for (const auto& item : notifications_) {
-    NotificationData* data = item.first;
-    if (data->notification_id == notification_id &&
-        data->profile_id == profile_id && data->origin_url == origin_url &&
-        data->incognito == incognito) {
-      return data;
-    }
-  }
-
-  return nullptr;
+void NotificationPlatformBridgeWin::PostTaskToTaskRunnerThread(
+    base::OnceClosure closure) const {
+  bool success = task_runner_->PostTask(FROM_HERE, std::move(closure));
+  DCHECK(success);
 }
