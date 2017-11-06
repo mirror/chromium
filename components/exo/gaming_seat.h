ed5f8c1b18c5a552fe8029f281befddeb74fea50
Mitsuru Oshima <oshima@chromium.org>
2017-11-06 07:05:22 -0800

    WM Helper
    Bug:
    Change-Id: Ia13d4eb195c8ebcf96986c8474566b332a065d49

--- components/exo/gaming_seat.h
+++ components/exo/gaming_seat.h
@@ -23,7 +23,8 @@ class GamepadDelegate;
 // This class represents one gaming seat. It uses /device/gamepad or
 // ozone/gamepad as backend and notifies corresponding GamepadDelegate of any
 // gamepad changes.
-class GamingSeat : public WMHelper::FocusObserver, public ui::GamepadObserver {
+class GamingSeat : public aura::client::FocusChangeObserver,
+                   public ui::GamepadObserver {
  public:
   // This class will monitor gamepad connection changes and manage gamepad
   // returned by gaming_seat_delegate.
--- components/exo/wayland/server.cc
+++ components/exo/wayland/server.cc
@@ -88,6 +88,7 @@
 #include "ui/base/class_property.h"
 #include "ui/base/hit_test.h"
 #include "ui/base/ui_features.h"
+#include "ui/compositor/compositor_vsync_manager.h"
 #include "ui/display/display_observer.h"
 #include "ui/display/manager/managed_display_info.h"
 #include "ui/display/screen.h"
@@ -98,6 +99,7 @@
 #include "ui/views/widget/widget_observer.h"
 #include "ui/wm/core/coordinate_conversion.h"
 #include "ui/wm/core/window_animations.h"
+#include "ui/wm/public/activation_change_observer.h"
 
 #if defined(USE_OZONE)
 #include <drm_fourcc.h>
@@ -2164,8 +2166,8 @@ const struct zcr_notification_surface_v1_interface
 
 // Implements remote shell interface and monitors workspace state needed
 // for the remote shell interface.
-class WaylandRemoteShell : public WMHelper::TabletModeObserver,
-                           public WMHelper::ActivationObserver,
+class WaylandRemoteShell : public ash::TabletModeObserver,
+                           public wm::ActivationChangeObserver,
                            public display::DisplayObserver {
  public:
   WaylandRemoteShell(Display* display, wl_resource* remote_shell_resource)
@@ -2257,8 +2259,9 @@ class WaylandRemoteShell : public WMHelper::TabletModeObserver,
   }
   void OnTabletModeEnded() override {}
 
-  // Overridden from WMHelper::ActivationObserver:
-  void OnWindowActivated(aura::Window* gained_active,
+  // Overridden from wm::ActivationChangeObserver:
+  void OnWindowActivated(ActivationReason reason,
+                         aura::Window* gained_active,
                          aura::Window* lost_active) override {
     SendActivated(gained_active, lost_active);
   }
@@ -2632,7 +2635,7 @@ void bind_aura_shell(wl_client* client,
 // vsync_timing_interface:
 
 // Implements VSync timing interface by monitoring updates to VSync parameters.
-class VSyncTiming final : public WMHelper::VSyncObserver {
+class VSyncTiming final : public ui::CompositorVSyncManager::Observer {
  public:
   ~VSyncTiming() override {
     WMHelper::GetInstance()->RemoveVSyncObserver(this);
