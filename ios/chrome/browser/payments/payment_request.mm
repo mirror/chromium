58b3f7dd50b49763750b50431729443b6fed2a21
gogerald <gogerald@google.com>
2017-11-06 15:10:24 -0500

    [Payments] Filtering modifiers based on capabilities
    Bug:777470
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I9a143c2db2a1bb9989feb2464b25182bb4c5b702

--- ios/chrome/browser/payments/payment_request.mm
+++ ios/chrome/browser/payments/payment_request.mm
@@ -316,8 +316,10 @@ const PaymentDetailsModifier* PaymentRequest::GetApplicableModifier(
                                 &unused_payment_method_identifiers);
 
     if (selected_instrument->IsValidForModifier(
-            modifier.method_data.supported_methods, supported_networks,
-            supported_types, !modifier.method_data.supported_types.empty())) {
+            modifier.method_data.supported_methods,
+            !modifier.method_data.supported_networks.empty(),
+            supported_networks, !modifier.method_data.supported_types.empty(),
+            supported_types)) {
       return &modifier;
     }
   }
