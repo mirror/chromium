1d3841e47e78701951b4947f4449055b609aa235
Peiyong Lin <lpy@chromium.org>
2017-11-06 12:55:03 -0800

    [GRC] Decouple PageSignalReceiver from TabManager.
    Previously PageSignalReceiver depends on TabManager because it calls directly to
    TabManager::WebContentsData and TabManager::TabStatsCollector. TabManager also
    depends on PageSignalReceiver directly or indirectly. To make PageSignalReceiver
    decoupled from TabManger, we need to introduce a layering that
    PageSignalReceiver calls into but not part of TabManager.
    
    Thus, this patch:
    
    1. Decoupled PageSignalReceiver from TabManager by adding PageSignalObserver
    API;
    2. Add [Add|Remove]Observer APIs in PageSignalReceiver;
    3. Implemented a TabManager::ResourceCoordinatorSignalObserver which implements
    PageSignalObserver APIs.
    
    BUG=775691
    
    Change-Id: I8c111ed36bbdb39c69d3568e5f7396dcd01c4ad7

--- chrome/browser/resource_coordinator/tab_manager.cc
+++ chrome/browser/resource_coordinator/tab_manager.cc
@@ -35,6 +35,7 @@
 #include "chrome/browser/resource_coordinator/background_tab_navigation_throttle.h"
 #include "chrome/browser/resource_coordinator/resource_coordinator_web_contents_observer.h"
 #include "chrome/browser/resource_coordinator/tab_manager_observer.h"
+#include "chrome/browser/resource_coordinator/tab_manager_resource_coordinator_signal_observer.h"
 #include "chrome/browser/resource_coordinator/tab_manager_stats_collector.h"
 #include "chrome/browser/resource_coordinator/tab_manager_web_contents_data.h"
 #include "chrome/browser/sessions/session_restore.h"
@@ -241,10 +242,15 @@ TabManager::TabManager()
 #endif
   browser_tab_strip_tracker_.Init();
   session_restore_observer_.reset(new TabManagerSessionRestoreObserver(this));
+  if (PageSignalReceiver::IsEnabled()) {
+    resource_coordinator_signal_observer_.reset(
+        new ResourceCoordinatorSignalObserver());
+  }
   stats_collector_.reset(new TabManagerStatsCollector());
 }
 
 TabManager::~TabManager() {
+  resource_coordinator_signal_observer_.reset();
   Stop();
 }
 
