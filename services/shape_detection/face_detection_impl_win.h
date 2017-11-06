147be7c77f5171e77f49a8e3e1df8c0214cfefab
junweifu <junwei.fu@intel.com>
2017-11-06 16:35:12 +0800

    ShapeDetection: Create Face Detector Async
    Create Face Detector with Face Detector factory that is async.
    Add a template class represents an asynchronous operation which
    returns a result upon completion.
    Add unittest to test the result.
    
    Split original large CL[1] up in smaller subpatches including this CL.
    Link Face Detection demo[2] here.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/708336
    [2] https://codepen.io/miguelao/pen/PmJWro
    
    BUG=767021
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: Id5b19115f10d7c96da191d7421a626741b075266

--- services/shape_detection/face_detection_impl_win.h
+++ services/shape_detection/face_detection_impl_win.h
@@ -5,8 +5,10 @@
 #ifndef SERVICES_SHAPE_DETECTION_FACE_DETECTION_IMPL_WIN_H_
 #define SERVICES_SHAPE_DETECTION_FACE_DETECTION_IMPL_WIN_H_
 
+#include <windows.foundation.h>
 #include <wrl/client.h>
 
+#include "detection_utils_win.h"
 #include "services/shape_detection/public/interfaces/facedetection.mojom.h"
 
 class SkBitmap;
@@ -15,7 +17,9 @@ namespace ABI {
 namespace Windows {
 namespace Media {
 namespace FaceAnalysis {
-struct IFaceDetectorStatics;
+interface IFaceDetectorStatics;
+interface IFaceDetector;
+class FaceDetector;
 }  // namespace FaceAnalysis
 }  // namespace Media
 }  // namespace Windows
@@ -23,10 +27,14 @@ struct IFaceDetectorStatics;
 
 namespace shape_detection {
 
+using ABI::Windows::Foundation::IAsyncOperation;
+
 class FaceDetectionImplWin : public mojom::FaceDetection {
  public:
   using IFaceDetectorStatics =
       ABI::Windows::Media::FaceAnalysis::IFaceDetectorStatics;
+  using FaceDetector = ABI::Windows::Media::FaceAnalysis::FaceDetector;
+  using IFaceDetector = ABI::Windows::Media::FaceAnalysis::IFaceDetector;
 
   static std::unique_ptr<FaceDetectionImplWin> Create();
 
@@ -34,11 +42,28 @@ class FaceDetectionImplWin : public mojom::FaceDetection {
       Microsoft::WRL::ComPtr<IFaceDetectorStatics> factory);
   ~FaceDetectionImplWin() override;
 
+  // mojom::FaceDetection:
   void Detect(const SkBitmap& bitmap,
               mojom::FaceDetection::DetectCallback callback) override;
 
  private:
+  friend class FaceDetectionImplWinTest;
+  FRIEND_TEST_ALL_PREFIXES(FaceDetectionImplWinTest, CreateFaceDetectorFailed);
+  FRIEND_TEST_ALL_PREFIXES(FaceDetectionImplWinTest,
+                           CreateFaceDetectorSucceeded);
+
+  HRESULT CreateFaceDetector();
+  void OnFaceDetectorCreated(
+      Microsoft::WRL::ComPtr<IAsyncOperation<FaceDetector*>> async_op);
+
   Microsoft::WRL::ComPtr<IFaceDetectorStatics> face_detector_factory_;
+  std::unique_ptr<AsyncOperation<FaceDetector>> async_create_detector_ops_;
+  Microsoft::WRL::ComPtr<IFaceDetector> face_detector_;
+
+  AsyncOperationStatus async_status_;
+
+  // The callback for unittests.
+  base::OnceClosure callback_for_testing_;
 
   DISALLOW_COPY_AND_ASSIGN(FaceDetectionImplWin);
 };
--- services/shape_detection/face_detection_impl_win_unittest.cc
+++ services/shape_detection/face_detection_impl_win_unittest.cc
@@ -4,10 +4,16 @@
 
 #include "services/shape_detection/face_detection_impl_win.h"
 
+#include "base/test/scoped_task_environment.h"
 #include "base/win/scoped_com_initializer.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace shape_detection {
+
+ACTION_P(RunClosure, closure) {
+  closure.Run();
+}
 
 class FaceDetectionImplWinTest : public testing::Test {
  protected:
@@ -17,12 +23,59 @@ class FaceDetectionImplWinTest : public testing::Test {
     ASSERT_TRUE(scoped_com_initializer_->Succeeded());
   }
 
+ public:
+  void CreateDetectorCallback() {
+    // Create Face detector succeeded.
+    Detection();
+  }
+
+  void DetectFailedCallback(
+      std::vector<mojom::FaceDetectionResultPtr> results) {
+    EXPECT_EQ(0u, results.size());
+    Detection();
+  }
+
+  MOCK_METHOD0(Detection, void(void));
+
  private:
   std::unique_ptr<base::win::ScopedCOMInitializer> scoped_com_initializer_;
+
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 };
 
 TEST_F(FaceDetectionImplWinTest, CreateAndDestroy) {
   auto impl = FaceDetectionImplWin::Create();
+}
+
+TEST_F(FaceDetectionImplWinTest, CreateFaceDetectorSucceeded) {
+  auto impl = FaceDetectionImplWin::Create();
+  if (!impl)
+    return;
+
+  base::RunLoop run_loop;
+  EXPECT_CALL(*this, Detection()).WillOnce(RunClosure(run_loop.QuitClosure()));
+  impl->callback_for_testing_ =
+      base::BindOnce(&FaceDetectionImplWinTest::CreateDetectorCallback,
+                     base::Unretained(this));
+  run_loop.Run();
+}
+
+TEST_F(FaceDetectionImplWinTest, CreateFaceDetectorFailed) {
+  auto impl = FaceDetectionImplWin::Create();
+  if (!impl)
+    return;
+
+  // Set failed status when creating face detector.
+  SkBitmap bitmap;
+  bitmap.setInfo(SkImageInfo::MakeN32Premul(100, 100));
+  impl->async_status_ = AsyncOperationStatus::ASYNC_FAILED;
+
+  base::RunLoop run_loop;
+  EXPECT_CALL(*this, Detection()).WillOnce(RunClosure(run_loop.QuitClosure()));
+  impl->Detect(bitmap,
+               base::BindOnce(&FaceDetectionImplWinTest::DetectFailedCallback,
+                              base::Unretained(this)));
+  run_loop.Run();
 }
 
 }  // namespace shape_detection
