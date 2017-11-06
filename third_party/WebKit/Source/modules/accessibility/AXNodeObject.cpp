c4219afda98db48c4fa04803d0b48d8832199c2c
Aaron Leventhal <aleventhal@chromium.org>
2017-11-05 20:56:17 -0500

    Address AutoSetSize/AutoPosInSet crash when children are still changing.
    Do not compute setsize/posinset info when parent's children need updating anyway.
    The call to the parent's Children() method can cause the current child to detach 
    from the old parent and get a new parent, which wreaks havoc. Future changes should
    address performance issues with this code, see crbug.com/781651.
    
    Bug: 773413
    Change-Id: Iff8fce096e7304dce9f119728cdd76ed1a1b63ac

--- third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXNodeObject.cpp
@@ -1416,7 +1416,12 @@ int AXNodeObject::SetSize() const {
 
 int AXNodeObject::AutoPosInSet() const {
   AXObject* parent = ParentObject();
-  if (!parent)
+
+  // Do not continue if the children will need updating soon, because
+  // the calculation requires all the siblings to remain stable.
+  if (!parent || parent->NeedsToUpdateChildren())
+    return 0;
+  if (parent->NeedsToUpdateChildren())
     return 0;
 
   int pos_in_set = 1;
@@ -1449,7 +1454,10 @@ int AXNodeObject::AutoPosInSet() const {
 
 int AXNodeObject::AutoSetSize() const {
   AXObject* parent = ParentObject();
-  if (!parent)
+
+  // Do not continue if the children will need updating soon, because
+  // the calculation requires all the siblings to remain stable.
+  if (!parent || parent->NeedsToUpdateChildren())
     return 0;
 
   int set_size = AutoPosInSet();
