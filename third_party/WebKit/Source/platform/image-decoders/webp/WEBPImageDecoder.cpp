161fa0975e6c3568f57a472eef02f066b4774c3d
Reza.Zakerinasab <zakerinasab@chromium.org>
2017-11-07 14:45:26 -0500

    Add DCHECK to color conversion API call sites
    This change adds DCHECKs to SkColorSpaceXform::apply() call sites in color managed canvas code.
    
    Bug:771714
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: If10a5908937565f1acfb5f6730730fbcf360ef7e

--- third_party/WebKit/Source/platform/image-decoders/webp/WEBPImageDecoder.cpp
+++ third_party/WebKit/Source/platform/image-decoders/webp/WEBPImageDecoder.cpp
@@ -352,12 +352,13 @@ void WEBPImageDecoder::ApplyPostProcessing(size_t frame_index) {
         SkColorSpaceXform::kBGRA_8888_ColorFormat;
     const SkColorSpaceXform::ColorFormat kDstFormat =
         SkColorSpaceXform::kRGBA_8888_ColorFormat;
+    bool color_converison_successful = false;
     for (int y = decoded_height_; y < decoded_height; ++y) {
       const int canvas_y = top + y;
       uint8_t* row = reinterpret_cast<uint8_t*>(buffer.GetAddr(left, canvas_y));
-      xform->apply(kDstFormat, row, kSrcFormat, row, width,
-                   kUnpremul_SkAlphaType);
-
+      color_converison_successful = xform->apply(
+          kDstFormat, row, kSrcFormat, row, width, kUnpremul_SkAlphaType);
+      DCHECK(color_converison_successful);
       uint8_t* pixel = row;
       for (int x = 0; x < width; ++x, pixel += 4) {
         const int canvas_x = left + x;
