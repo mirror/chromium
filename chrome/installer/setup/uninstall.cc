2e351049f7c1c1269b213f7827ccec806f9aa28f
Xi Cheng <chengx@chromium.org>
2017-11-06 16:07:01 -0800

    Register a COM server with the OS to handle native notifications
    This is one prerequisite for Chrome to use the Windows 10 new toast notification
    features. The registered COM server will help launch Chrome when an user clicks
    the toast notifications when Chrome is not running. 
    
    Bug: 734095
    Change-Id: I751ce45c2a594da28e3437e76164ad128886fd6e

--- chrome/installer/setup/uninstall.cc
+++ chrome/installer/setup/uninstall.cc
@@ -7,6 +7,8 @@
 #include "chrome/installer/setup/uninstall.h"
 
 #include <windows.h>
+
+#include <objbase.h>
 #include <stddef.h>
 #include <stdint.h>
 
@@ -637,6 +639,14 @@ bool DeleteChromeRegistrationKeys(const InstallerState& installer_state,
   // would otherwise try to figure out the currently installed suffix).
   reg_app_id.append(install_static::GetBaseAppId() + browser_entry_suffix);
   InstallUtil::DeleteRegistryKey(root, reg_app_id, WorkItem::kWow64Default);
+
+  // Delete Software\Classes\CLSID.
+  wchar_t toast_activator_clsid[MAX_PATH];
+  ::StringFromGUID2(install_static::GetToastActivatorClsid(),
+                    toast_activator_clsid, arraysize(toast_activator_clsid));
+  base::string16 reg_key(L"Software\\Classes\\CLSID\\");
+  reg_key.append(toast_activator_clsid).append(L"\\LocalServer32");
+  InstallUtil::DeleteRegistryKey(root, reg_key, WorkItem::kWow64Default);
 
   // Delete all Start Menu Internet registrations that refer to this Chrome.
   {
