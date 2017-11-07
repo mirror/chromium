7d64e36bcb013a45d0ac132547278f65220bf70f
Marc Treib <treib@chromium.org>
2017-11-07 18:44:08 +0100

    Local NTP OneGoogleBar: Use position: absolute instead of fixed
    This makes the OGB scroll normally, instead of sticking to the top.
    
    Bug: 782218
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I5948bba3a6c618ef48df9e22dd0ba1cd2ab025ed

--- chrome/browser/search/one_google_bar/one_google_bar_fetcher_impl.cc
+++ chrome/browser/search/one_google_bar/one_google_bar_fetcher_impl.cc
@@ -174,7 +174,13 @@ GURL OneGoogleBarFetcherImpl::AuthenticatedURLFetcher::GetApiUrl() const {
   GURL api_url = google_base_url_.Resolve(api_url_override_.value_or(kApiPath));
 
   // Add the "hl=" parameter.
-  return net::AppendQueryParameter(api_url, "hl", application_locale_);
+  api_url = net::AppendQueryParameter(api_url, "hl", application_locale_);
+
+  // Add the "async=" parameter. We can't use net::AppendQueryParameter for
+  // this because we need the ":" to remain unescaped.
+  GURL::Replacements replacements;
+  replacements.SetQueryStr(api_url.query() + "&async=fixed:0");
+  return api_url.ReplaceComponents(replacements);
 }
 
 std::string
