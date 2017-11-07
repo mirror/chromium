6021978b7c8067717c455a177d53b7988145cbb5
Marc Treib <treib@chromium.org>
2017-11-07 18:19:42 +0100

    Doodle logging via /async/ddllog
    Bug: 781309
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Iac285baf48c04251a749e5ed06201c2e63d568c3

--- components/search_provider_logos/google_logo_api.cc
+++ components/search_provider_logos/google_logo_api.cc
@@ -180,6 +180,9 @@ std::unique_ptr<EncodedLogo> ParseDoodleLogoResponse(
   logo->metadata.on_click_url = ParseUrl(*ddljson, "target_url", base_url);
   ddljson->GetString("alt_text", &logo->metadata.alt_text);
 
+  logo->metadata.cta_log_url = ParseUrl(*ddljson, "cta_log_url", base_url);
+  logo->metadata.log_url = ParseUrl(*ddljson, "log_url", base_url);
+
   ddljson->GetString("fingerprint", &logo->metadata.fingerprint);
 
   base::TimeDelta time_to_live;
