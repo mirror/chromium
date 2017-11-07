abb84bc48d32c00f72a2230c44f7e7c38216a2f8
Peiyong Lin <lpy@chromium.org>
2017-11-06 19:48:37 -0800

    [GRC]Add global clock ResourceCoordinatorClock.
    Previously when we need to simulate clock ticks in tests, we need each
    indivisual components like PageCoordinationUnitImpl to carry its own tick
    clock, and we have to create helper functions to set the clock properly among
    different components. This is fragile and redudant. This patch introduces a
    ResourceCoordinatorClock which provides 4 helper methods to manipulate clock in
    testing across components.
    
    There's also a recent patch adding timing functions to
    chrome/browser/resource_coordinator:
    https://chromium-review.googlesource.com/c/chromium/src/+/723674
    
    BUG=775691
    
    Change-Id: I05e5d1fd688aaa35c8d8abcc4707439f00d9c958

--- services/resource_coordinator/observers/metrics_collector.cc
+++ services/resource_coordinator/observers/metrics_collector.cc
@@ -5,6 +5,7 @@
 #include "services/resource_coordinator/observers/metrics_collector.h"
 
 #include "base/metrics/histogram_macros.h"
+#include "services/resource_coordinator/clock.h"
 #include "services/resource_coordinator/coordination_unit/coordination_unit_manager.h"
 #include "services/resource_coordinator/coordination_unit/frame_coordination_unit_impl.h"
 #include "services/resource_coordinator/coordination_unit/page_coordination_unit_impl.h"
@@ -51,8 +52,7 @@ size_t GetNumCoresidentTabs(const PageCoordinationUnitImpl* page_cu) {
 }
 
 MetricsCollector::MetricsCollector()
-    : clock_(&default_tick_clock_),
-      max_ukm_cpu_usage_measurements_(kDefaultMaxCPUUsageMeasurements) {
+    : max_ukm_cpu_usage_measurements_(kDefaultMaxCPUUsageMeasurements) {
   UpdateWithFieldTrialParams();
 }
 
@@ -91,7 +91,7 @@ void MetricsCollector::OnFramePropertyChanged(
   if (property_type == mojom::PropertyType::kAudible) {
     bool audible = static_cast<bool>(value);
     if (!audible) {
-      frame_data.last_audible_time = clock_->NowTicks();
+      frame_data.last_audible_time = ResourceCoordinatorClock::NowTicks();
       return;
     }
     auto* page_cu = frame_cu->GetPageCoordinationUnit();
@@ -101,7 +101,7 @@ void MetricsCollector::OnFramePropertyChanged(
     }
     // Audio is considered to have started playing if the page has never
     // previously played audio, or has been silent for at least one minute.
-    auto now = clock_->NowTicks();
+    auto now = ResourceCoordinatorClock::NowTicks();
     if (frame_data.last_audible_time + kMaxAudioSlientTimeout < now) {
       MetricsReportRecord& record =
           metrics_report_record_map_.find(page_cu->id())->second;
@@ -197,10 +197,6 @@ void MetricsCollector::OnPageEventReceived(
         true, page_cu->TimeSinceLastVisibilityChange(),
         coordination_unit_manager().ukm_recorder());
   }
-}
-
-void MetricsCollector::SetClockForTest(base::TickClock* test_clock) {
-  const_cast<base::TickClock*&>(clock_) = test_clock;
 }
 
 bool MetricsCollector::ShouldReportMetrics(
