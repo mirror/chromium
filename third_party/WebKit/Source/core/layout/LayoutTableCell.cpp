f94d73668f51d8d785a942a5c2d7593308cdefde
Renee Wright <rjwright@chromium.org>
2017-11-07 15:17:30 +1100

    [WIP] Rename CSSPropertyAPI to CSSProperty and add namespaces
    Bug: 779928
    Change-Id: Ia70e660f7fa7c4b1076e369f56342c16efd1ef67

--- third_party/WebKit/Source/core/layout/LayoutTableCell.cpp
+++ third_party/WebKit/Source/core/layout/LayoutTableCell.cpp
@@ -514,7 +514,7 @@ bool LayoutTableCell::IsInEndColumn() const {
 
 CSSPropertyID LayoutTableCell::ResolveBorderProperty(
     CSSPropertyID property) const {
-  return CSSPropertyAPI::Get(property).ResolveDirectionAwareProperty(
+  return CSSProperty::Get(property).ResolveDirectionAwareProperty(
       TableStyle().Direction(), TableStyle().GetWritingMode());
 }
 
