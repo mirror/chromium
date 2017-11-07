233d9cc156e6a06d616697a6a2e7bb601ff558d9
Peter Laurens <peterlaurens@google.com>
2017-11-07 11:41:53 -0800

    Box ToolsPopupController with a Coordinator class.
    ToolsPopupController is fully boxed within ToolsPopupCoordinator
    which operates with the rest of the application mostly through
    a simple command protocol for use with a CommandDispatcher, and
    some configuration providers that can configure specific details
    of the tools menu and the manner in which its presented.
    
    ToolsPopupCoordinator is created and owned by the two classes which
    previously triggered a popup menu via ToolsPopupController, the BVC
    and StackViewController.
    
    There is now also a full set of presentation notifications that are
    sent for the popup UI (willShow, didShow, willHide, didHide) and they
    are now only sent from the coordinator class.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I5ac85aaf2c4f3ed20bf2dabd4bbaf1f78aa17d19

--- ios/chrome/browser/ui/tools_menu/tools_menu_constants.h
+++ ios/chrome/browser/ui/tools_menu/tools_menu_constants.h
@@ -7,6 +7,68 @@
 
 #import <Foundation/Foundation.h>
 
+@class ToolsMenuCoordinator, ToolsMenuConfiguration, UIButton;
+
+// Notification that the tools menu will be shown.
+extern NSString* const kToolsMenuWillShowNotification;
+// Notification that the tools menu will dismiss.
+extern NSString* const kToolsMenuWillHideNotification;
+// Notification that the tools menu did show.
+extern NSString* const kToolsMenuDidShowNotification;
+// Notification that the tools menu did dismiss.
+extern NSString* const kToolsMenuDidHideNotification;
+
+// A protocol which allows details of the presentation of the tools menu to be
+// configured.
+@protocol ToolsMenuCoordinatorPresentationProvider<NSObject>
+@optional
+// Returns the button being used to present and dismiss the tools menu,
+// if applicable.
+- (UIButton*)presentingButtonForToolsMenuCoordinator:
+    (ToolsMenuCoordinator*)coordinator;
+@end
+
+// A protocol which allows objects to be able to request information about
+// the presentation state of the tools UI without having to have full
+// knowledge of the ToolsMenuCoordinator class.
+@protocol ToolsMenuCoordinatorPresentationStateProtocol<NSObject>
+// Returns whether the tools menu on screen and being presented to the user.
+- (BOOL)isShowingToolsMenu;
+@end
+
+// A protocol that describes a set of methods which may configure a
+// ToolsMenuCoordinator. Most of the configuration of the coordinator may be
+// achieved through the required ToolsMenuConfiguration object, but some
+// optional minor elements such as bookmark highlights are also independently
+// configurable.
+@protocol ToolsMenuCoordinatorConfigurationProvider<NSObject>
+// Returns a ToolsMenuConfiguration object describing the desired configuration
+// of the tools menu.
+- (ToolsMenuConfiguration*)menuConfigurationForToolsMenuCoordinator:
+    (ToolsMenuCoordinator*)coordinator;
+@optional
+// If implemented, may influence how the tools menu shows the bookmark
+// indicator in the tools UI.
+- (BOOL)shouldHighlightBookmarkButtonForToolsMenuCoordinator:
+    (ToolsMenuCoordinator*)coordinator;
+// If implemented, may influence the presence of the find bar in the tools
+// UI.
+- (BOOL)shouldShowFindBarForToolsMenuCoordinator:
+    (ToolsMenuCoordinator*)coordinator;
+// If implemented,may influence the presence of the share menu in the tools
+// UI.
+- (BOOL)shouldShowShareMenuForToolsMenuCoordinator:
+    (ToolsMenuCoordinator*)coordinator;
+// If implemented, may influence how the tools menu shows page-reload related
+// UI.
+- (BOOL)isTabLoadingForToolsMenuCoordinator:(ToolsMenuCoordinator*)coordinator;
+// If implemented, allows the delegate to be informed of an imminent
+// presentation of the tools menu. The delegate may choose to dismiss other
+// presented UI or otherwise configure itself for the menu presentation.
+- (void)prepareForToolsMenuPresentationByCoordinator:
+    (ToolsMenuCoordinator*)coordinator;
+@end
+
 // New Tab item accessibility Identifier.
 extern NSString* const kToolsMenuNewTabId;
 // New incognito Tab item accessibility Identifier.
