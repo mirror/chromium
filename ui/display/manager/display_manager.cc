afb88480397ee72d4cfa382194de203720f0aaea
Ahmed Fakhry <afakhry@google.com>
2017-11-07 13:37:08 -0800

    CL_3: Hookup the display APIs to the unified matrix support
    BUG=752767
    TEST=
    
    Change-Id: I55d498ca0fd203c635036eb4bfb9eaa074723d77

--- ui/display/manager/display_manager.cc
+++ ui/display/manager/display_manager.cc
@@ -1115,6 +1115,11 @@ void DisplayManager::SetUnifiedDesktopMatrix(
   SetDefaultMultiDisplayModeForCurrentDisplays(UNIFIED);
 }
 
+const UnifiedDesktopLayoutMatrix&
+DisplayManager::GetCurrentUnifiedDesktopMatrix() const {
+  return current_matrix_;
+}
+
 const Display* DisplayManager::GetPrimaryMirroringDisplayForUnifiedDesktop()
     const {
   if (!IsInUnifiedMode())
