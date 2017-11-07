f5d320b0776132b899c6ba1de094800bfe442485
Joy Ming <jming@chromium.org>
2017-11-07 14:18:22 -0800

    In_progress_cache : Initial draft
    Details:
    1- Stores info into proto format and writes to a file.
    2- The cache is instantiated & owned by ChromeDownloadManagerDelegate.
    3- The entry is created when download starts and will be deleted as
    soon as the download completes.
    
    This is continued from this CL by shaktisahu@:
    https://chromium-review.googlesource.com/c/chromium/src/+/731403.
    
    Bug:778425
    Change-Id: I6bd3f6df2648ac7b812c464b572492fb670d32bb

---
+++ components/download/downloader/in_progress/download_entry.cc
@@ -0,0 +1,15 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/download/downloader/in_progress/download_entry.h"
+
+namespace download {
+
+DownloadEntry::DownloadEntry() = default;
+
+DownloadEntry::DownloadEntry(const DownloadEntry& other) = default;
+
+DownloadEntry::~DownloadEntry() = default;
+
+}  // namespace download
