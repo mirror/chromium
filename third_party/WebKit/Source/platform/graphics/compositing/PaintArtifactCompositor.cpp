f87eb060608d87156c867badea5c11e7fe6a2863
Xianzhu Wang <wangxianzhu@chromium.org>
2017-11-07 10:15:32 -0800

    Verbose logs for paint debugging
    The code for these logs is compiled when DCHECK is on. The logs can be
    controlled using the --vmodule command line:
    - Module "*/paint/*"
      =1: paint property trees
      =2: =1 plus
        paint layer trees
        paint controller debug data without paint records (also for "*/graphics/*")
      =3: =2 plus
        (debug build only) paint controller debug data with paint records (also for "*/graphics/*")
        (debug build only) layout trees
    - Module "*/graphics/*"
      =2: graphics layer trees (spv1*) or composited layers (spv175+) with less verbosity
      =3: =2 trees with full verbosity
      and paint controller debug data as described in "*/paint/*"
    
    Sample usage:
    out/Release/content_shell --vmodule="*/paint/*=2,*/graphics/*=3"
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I71039a295d699f42d3a8e3ddcabd1ef05ce5af67

--- third_party/WebKit/Source/platform/graphics/compositing/PaintArtifactCompositor.cpp
+++ third_party/WebKit/Source/platform/graphics/compositing/PaintArtifactCompositor.cpp
@@ -766,6 +766,13 @@ void PaintArtifactCompositor::Update(
     chunk.raster_invalidation_rects.clear();
     chunk.raster_invalidation_tracking.clear();
   }
+
+#if DCHECK_IS_ON()
+  if (VLOG_IS_ON(2)) {
+    LayerTreeFlags flags = VLOG_IS_ON(3) ? 0xffffffff : 0;
+    DVLOG(2) << LayersAsJSON(flags)->ToPrettyJSONString().Utf8().data();
+  }
+#endif
 }
 
 std::unique_ptr<WebLayer>
@@ -781,12 +788,13 @@ PaintArtifactCompositor::ExtraDataForTesting::ScrollHitTestWebLayerAt(
       scroll_hit_test_layers[index].get());
 }
 
-#ifndef NDEBUG
+#if DCHECK_IS_ON()
 void PaintArtifactCompositor::ShowDebugData() {
-  LOG(ERROR) << LayersAsJSON(kLayerTreeIncludesDebugInfo)
-                    ->ToPrettyJSONString()
-                    .Utf8()
-                    .data();
+  LOG(INFO) << LayersAsJSON(kLayerTreeIncludesDebugInfo |
+                            kLayerTreeIncludesPaintInvalidations)
+                   ->ToPrettyJSONString()
+                   .Utf8()
+                   .data();
 }
 #endif
 
