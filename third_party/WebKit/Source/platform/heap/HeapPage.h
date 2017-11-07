035a551c74c153edd6851447cc59e7ae40372d77
Keishi Hattori <keishi@chromium.org>
2017-11-07 18:06:07 +0900

    Oilpan: Re enable HeapObjectHeader integrity check in release builds
    r466556 accidentally disabled HeapObjectHeader integrity check on release builds (introduced in r450598)
    
    Bug:
    Change-Id: I73f2034b9bd27dce712b97743589173409b347f1

--- third_party/WebKit/Source/platform/heap/HeapPage.h
+++ third_party/WebKit/Source/platform/heap/HeapPage.h
@@ -890,7 +890,7 @@ NO_SANITIZE_ADDRESS inline bool HeapObjectHeader::IsValid() const {
 
 NO_SANITIZE_ADDRESS inline void HeapObjectHeader::CheckHeader() const {
 #if defined(ARCH_CPU_64_BITS)
-  DCHECK(IsValid());
+  CHECK(IsValid());
 #endif
 }
 
