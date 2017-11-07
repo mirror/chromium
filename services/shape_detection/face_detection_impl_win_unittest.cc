e2ef53627af7deef72661bd423ed4f1c3b1d24dc
junweifu <junwei.fu@intel.com>
2017-11-07 16:35:57 +0800

    ShapeDetection: Create Face Detector Async on windows 10
    Create Face Detector with Face Detector factory that is async.
    Add a template class represents an asynchronous operation which
    returns a result upon completion.
    Add unittest to test the result.
    
    Split original large CL[1] up in smaller subpatches including this CL.
    Link Face Detection demo[2] here.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/708336
    [2] https://codepen.io/miguelao/pen/PmJWro
    
    BUG=767021
    
    Cq-Include-Trybots: master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: Id5b19115f10d7c96da191d7421a626741b075266

--- services/shape_detection/face_detection_impl_win_unittest.cc
+++ services/shape_detection/face_detection_impl_win_unittest.cc
@@ -4,6 +4,7 @@
 
 #include "services/shape_detection/face_detection_impl_win.h"
 
+#include "base/test/scoped_task_environment.h"
 #include "base/win/scoped_com_initializer.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -17,12 +18,57 @@ class FaceDetectionImplWinTest : public testing::Test {
     ASSERT_TRUE(scoped_com_initializer_->Succeeded());
   }
 
+ public:
+  void CreateDetectorSucceeded(base::Closure quite_closure) {
+    // Create Face detector succeeded.
+    quite_closure.Run();
+  }
+
+  void DetectFailedCallback(
+      base::Closure quite_closure,
+      std::vector<mojom::FaceDetectionResultPtr> results) {
+    EXPECT_EQ(0u, results.size());
+
+    quite_closure.Run();
+  }
+
  private:
   std::unique_ptr<base::win::ScopedCOMInitializer> scoped_com_initializer_;
+
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 };
 
 TEST_F(FaceDetectionImplWinTest, CreateAndDestroy) {
   auto impl = FaceDetectionImplWin::Create();
+}
+
+TEST_F(FaceDetectionImplWinTest, CreateFaceDetectorSucceeded) {
+  auto impl = FaceDetectionImplWin::Create();
+  if (!impl)
+    return;
+
+  base::RunLoop run_loop;
+  impl->callback_for_testing_ =
+      base::BindOnce(&FaceDetectionImplWinTest::CreateDetectorSucceeded,
+                     base::Unretained(this), run_loop.QuitClosure());
+  run_loop.Run();
+}
+
+TEST_F(FaceDetectionImplWinTest, CreateFaceDetectorFailed) {
+  auto impl = FaceDetectionImplWin::Create();
+  if (!impl)
+    return;
+
+  // Set failed status when creating face detector.
+  SkBitmap bitmap;
+  bitmap.setInfo(SkImageInfo::MakeN32Premul(100, 100));
+  impl->async_status_ = AsyncOperationStatus::KFailed;
+
+  base::RunLoop run_loop;
+  impl->Detect(bitmap,
+               base::BindOnce(&FaceDetectionImplWinTest::DetectFailedCallback,
+                              base::Unretained(this), run_loop.QuitClosure()));
+  run_loop.Run();
 }
 
 }  // namespace shape_detection
