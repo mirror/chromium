5166ebfdf95d15080ff031e57acf6df12b9324b4
josephlolak <joseph.lolak@samsung.com>
2017-11-07 10:16:19 +0530

    Fix for incorrect currenttime if media duration is infinity
    Returned last played time if media duration is infinity.
    Added check to trigger "ended" event if media duration is infinity.
    
    Bug:766732,409280
    Change-Id: Ic5b2554cb7fddd6938309f08dfd5746bc89c56b4

--- third_party/WebKit/Source/core/html/media/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/media/HTMLMediaElement.cpp
@@ -3103,8 +3103,10 @@ void HTMLMediaElement::TimeChanged() {
   // When the current playback position reaches the end of the media resource
   // when the direction of playback is forwards, then the user agent must follow
   // these steps:
-  if (!std::isnan(dur) && dur && now >= dur &&
-      GetDirectionOfPlayback() == kForward) {
+  if ((GetWebMediaPlayer() && GetWebMediaPlayer()->IsEnded() &&
+       std::isinf(dur)) ||
+      (!std::isnan(dur) && dur && now >= dur &&
+       GetDirectionOfPlayback() == kForward)) {
     // If the media element has a loop attribute specified
     if (Loop()) {
       //  then seek to the earliest possible position of the media resource and
