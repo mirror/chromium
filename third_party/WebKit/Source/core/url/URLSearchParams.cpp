da24315024ae812cb4855b71162d2dccad1c61e7
Yutaka Hirano <yhirano@chromium.org>
2017-11-07 13:07:48 +0900

    [URL] Do not run update steps when URLSearchParam is created
    See https://github.com/whatwg/url/issues/350 and
    https://github.com/whatwg/url/issues/18.
    
    Bug: 776344
    Change-Id: I7881add2fc5b858874da42cef7b14a9e8ee10262

--- third_party/WebKit/Source/core/url/URLSearchParams.cpp
+++ third_party/WebKit/Source/core/url/URLSearchParams.cpp
@@ -82,14 +82,16 @@ URLSearchParams* URLSearchParams::Create(const Vector<Vector<String>>& init,
     }
     instance->AppendWithoutUpdate(pair[0], pair[1]);
   }
-  instance->RunUpdateSteps();
   return instance;
 }
 
-URLSearchParams::URLSearchParams(const String& query_string, DOMURL* url_object)
-    : url_object_(url_object) {
+URLSearchParams::URLSearchParams(const String& query_string,
+                                 DOMURL* url_object) {
   if (!query_string.IsEmpty())
     SetInput(query_string);
+  // Set |url_object_| here in order not to run its update steps in the previous
+  // step.
+  url_object_ = url_object;
 }
 
 URLSearchParams* URLSearchParams::Create(
@@ -100,7 +102,6 @@ URLSearchParams* URLSearchParams::Create(
     return instance;
   for (const auto& item : init)
     instance->AppendWithoutUpdate(item.first, item.second);
-  instance->RunUpdateSteps();
   return instance;
 }
 
