01da6a37beaaefdd671328c649d582c19c87ba3d
Ian Vollick <vollick@chromium.org>
2017-11-07 16:10:04 -0500

    [vr] Invisible elements shouldn't cause scene dirtiness
    Previously, any animated element would cause dirtiness when updated,
    forcing the scene to be redrawn. Similarly, updating bindings would
    also cause dirtiness, even if the element was invisible. With this
    change, I've ensured that invisible elements do not cause dirtiness.
    
    Bug:776521
    Change-Id: I1d2e733ce9c48ff98356f273c6d73d8c53bf06f1

--- chrome/browser/vr/elements/ui_element.h
+++ chrome/browser/vr/elements/ui_element.h
@@ -61,8 +61,8 @@ class UiElement : public cc::AnimationTarget {
 
   enum UpdatePhase {
     kDirty = 0,
-    kUpdatedAnimations,
     kUpdatedBindings,
+    kUpdatedAnimations,
     kUpdatedComputedOpacity,
     kUpdatedTexturesAndSizes,
     kUpdatedLayout,
@@ -223,8 +223,7 @@ class UiElement : public cc::AnimationTarget {
     return bindings_;
   }
 
-  // Return true if any bindings has updates.
-  bool UpdateBindings();
+  void UpdateBindings();
 
   gfx::Point3F GetCenter() const;
   gfx::Vector3dF GetNormal() const;
@@ -271,7 +270,7 @@ class UiElement : public cc::AnimationTarget {
 
   virtual gfx::Transform LocalTransform() const;
 
-  void UpdateComputedOpacityRecursive();
+  void UpdateComputedOpacity();
   void UpdateWorldSpaceTransformRecursive();
 
   std::vector<std::unique_ptr<UiElement>>& children() { return children_; }
@@ -302,6 +301,14 @@ class UiElement : public cc::AnimationTarget {
   // this is ignored (say for head-locked elements that draw in screen space),
   // then this function should return false.
   virtual bool IsWorldPositioned() const;
+
+  bool updated_bindings_this_frame() const {
+    return updated_bindings_this_frame_;
+  }
+
+  bool updated_visiblity_this_frame() const {
+    return updated_visibility_this_frame_;
+  }
 
   std::string DebugName() const;
 
@@ -349,6 +356,13 @@ class UiElement : public cc::AnimationTarget {
 
   // The computed opacity, incorporating opacity of parent objects.
   float computed_opacity_ = 1.0f;
+
+  // Returns true if the last call to UpdateBindings had any effect.
+  bool updated_bindings_this_frame_ = false;
+
+  // Return true if the last call to UpdateComputedOpacity had any effect on
+  // visibility.
+  bool updated_visibility_this_frame_ = false;
 
   // If anchoring is specified, the translation will be relative to the
   // specified edge(s) of the parent, rather than the center.  A parent object
