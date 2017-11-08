f39e2ba4c7d0d6bea89084879a804ffc03a3fba2
Jochen Eisinger <jochen@chromium.org>
2017-11-08 23:09:26 +0100

    When cancelling parsing of a document, try to transition to completed
    With browser side navigations, we might cancel parsing because a new
    navigation started, which in turn might fail. In that case, the original
    document should be in a defined state
    
    BUG=714373
    R=japhet@chromium.org
    
    Change-Id: Idf09c46a2dde8ce1107d8c8d1d414e5fa06987da

--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -1468,6 +1468,7 @@ void FrameLoader::StartLoad(FrameLoadRequest& frame_load_request,
   if (had_placeholder_client_document_loader)
     provisional_document_loader_->SetSentDidFinishLoad();
   frame_->GetDocument()->CancelParsing();
+  frame_->GetDocument()->CheckCompleted();
   DetachDocumentLoader(provisional_document_loader_);
 
   // beforeunload fired above, and detaching a DocumentLoader can fire events,
