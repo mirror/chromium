35f75571f18c92b9ea1de0b45d56b0e273da14f8
danakj <danakj@chromium.org>
2017-11-08 16:52:43 -0500

    When calling UseRenderPass() to restore, ensure it runs.
    UseRenderPass() has two use cases, either to set up a render pass
    as the framebuffer for drawing at the start of the draw, or to restore
    us back to that state after making some intermediate change to which
    framebuffer is bound.
    
    UseRenderPass() has some optimizations where it can return false and
    not do anything, if it detects that we don't want to draw the render
    pass. The first use case, in DrawRenderPass() makes use of this return
    value to avoid drawing anything.
    
    Other callers to UseRenderPass() do not check the return value because
    we're already drawing the pass and they are trying to restore state. If
    it early outs for these it would be unexpected and produce bugs.
    
    Currently DecideRenderPassAllocationsForFrame modifies render passes to
    try avoid such bugs, but there are other ways to encounter the bug. So
    instead, tell UseRenderPass() explicitly what behaviour each caller wants.
    
    Also, ensure that UseRenderPass() is called after using a
    ScopedUseGrContext when drawing a RenderPassDrawQuad. Currently on errors
    it would early out before restoring the framebuffer, which could cause
    bugs for the next quads in the RenderPass. This was introduced when the
    UseRenderPass() call was moved out of the ~ScopedUseGrContext()
    destructor in https://codereview.chromium.org/2203033005.
    
    This all came up via https://chromium-review.googlesource.com/c/chromium/src/+/757027#message-b6e905925e834419ff448e2093a392053d5d321a
    
    R=enne@chromium.org, wutao@chromium.org
    
    Bug: 782044, 738190, 782042
    Change-Id: Ib4662c5fee3f326642add12bbfe3d51e51dff1c9

--- components/viz/service/display/gl_renderer.cc
+++ components/viz/service/display/gl_renderer.cc
@@ -1124,11 +1124,20 @@ void GLRenderer::DrawRenderPassQuadInternal(
       params->quad->shared_quad_state->quad_to_target_transform;
   if (!InitializeRPDQParameters(params))
     return;
+
   UpdateRPDQShadersForBlending(params);
-  if (!UpdateRPDQWithSkiaFilters(params))
-    return;
+  bool can_draw = UpdateRPDQWithSkiaFilters(params);
+  // The above calls use ScopedUseGrContext which can change the bound
+  // framebuffer, so we need to restore it for the current RenderPass.
   UseRenderPass(current_frame()->current_render_pass);
+  // As part of restoring the framebuffer, we call SetViewport directly, rather
+  // than through PrepareSurfaceForPass. PrepareSurfaceForPass also clears the
+  // surface, which is not desired when restoring.
   SetViewport();
+
+  if (!can_draw)
+    return;
+
   UpdateRPDQTexturesForSampling(params);
   UpdateRPDQBlendMode(params);
   ChooseRPDQProgram(params);
@@ -2861,7 +2870,7 @@ void GLRenderer::BindFramebufferToOutputSurface() {
   }
 }
 
-bool GLRenderer::BindFramebufferToTexture(const cc::ScopedResource* texture) {
+void GLRenderer::BindFramebufferToTexture(const cc::ScopedResource* texture) {
   DCHECK(texture->id());
 
   // Explicitly release lock, otherwise we can crash when try to lock
@@ -2903,7 +2912,6 @@ bool GLRenderer::BindFramebufferToTexture(const cc::ScopedResource* texture) {
   } else {
     SetStencilEnabled(false);
   }
-  return true;
 }
 
 void GLRenderer::SetScissorTestRect(const gfx::Rect& scissor_rect) {
