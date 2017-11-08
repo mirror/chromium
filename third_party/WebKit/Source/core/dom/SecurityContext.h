60fe8775a2af0023147825860621a3b7a9f7109c
Hiroshige Hayashizaki <hiroshige@chromium.org>
2017-11-08 11:20:52 -0800

    Make most of SecurityOrigin references const
    This CL only adds |const| to SecurityOrigin-related types and thus
    doesn't change the behavior.
    
    This CL replaces most of
    - WebPrivatePtr<SecurityOrigin> -> WebPrivatePtr<const SecurityOrigin>
    - scoped_refptr<SecurityOrigin> -> scoped_refptr<const SecurityOrigin>
    - SecurityOrigin* -> const SecurityOrigin*
    - SecurityOrigin& -> const SecurityOrigin&
    
    Most of the modifications are done mechanically by PCRE:
    s/(WebPrivatePtr<|scoped_refptr<|((?<!const)\s+)|\(|^)((::)?blink::)?SecurityOrigin([&\*>])/$1const $3SecurityOrigin$5/g
    Except for the following exceptions:
    
    [Memo for reviewers]
    PatchSet 15 has all mechanical changes applied, and
    I removed some |const| in PatchSet 16.
    See diffs between PatchSet 15 and PatchSet 16 for the list of exceptions
    for constifying.
    I'll update the commit message with the list of exceptions later.
    
    =====
    Obsolete memo below:
    
    - core/dom/SecurityContext.h
    - core/inspector/InspectorPageAgent.cpp
    - platform/weborigin/SecurityOrigin.h
    - platform/weborigin/SecurityOrigin.cpp
    
    Some unit tests:
      - core/dom/DocumentTest.cpp
      - modules/webdatabase/QuotaTrackerTest.cpp
      - platform/weborigin/SecurityOriginTest.cpp
      - core/page/scrolling/RootScrollerTest.cpp
      - core/exported/WebFrameTest.cpp
        WebSecurityOrigin constructor is added explicitly, because the
        implicit conversion from scoped_refptr<SecurityOrigin> to
        scoped_refptr<const SecurityOrigin> then to WebSecurityOrigin
        was not inferred by the compiler.
    
    Memo:
    
    SecurityOrigin::Create.*() and IsolatedCopy()
    
    SecurityContext::SetSecurityOrigin()
    SecurityContext::security_origin_
    MutableWebSecurityOrigin
    
    For SecurityOrigin sharing:
    - Document::UpdateSecurityOrigin()
    - RemoteSecurityContext::SetReplicatedOrigin()
    
    Blob-related:
    - URLRegistry::RegisterURL() and its overrides
    - BlobRegistry::RegisterPublicBlobURL()
    - SaveToOriginMap() and BlobURLOriginMap in BlobRegistry.cpp
    - GetOriginFromMap() in SecurityOrigin.cpp
    - URLSecurityOriginMap::GetOrigin() and its override
    - ExecutionContext::GetMutableSecurityOrigin()
    - SecurityContext::GetMutableSecurityOrigin()
    
    Other:
    - UpdateSecurityOriginFromDocument in LocalWindowProxy.cpp
    - third_party/WebKit/Source/core/html/PublicURLManager.cpp
    - InspectorPageAgent.cpp
    
    Bug: 779730
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie4a1a5cc2add20c1c1b9d0d6d5dff9ea9ec46240

--- third_party/WebKit/Source/core/dom/SecurityContext.h
+++ third_party/WebKit/Source/core/dom/SecurityContext.h
@@ -57,7 +57,9 @@ class CORE_EXPORT SecurityContext : public GarbageCollectedMixin {
 
   using InsecureNavigationsSet = HashSet<unsigned, WTF::AlreadyHashed>;
 
-  SecurityOrigin* GetSecurityOrigin() const { return security_origin_.get(); }
+  const SecurityOrigin* GetSecurityOrigin() const {
+    return security_origin_.get();
+  }
   ContentSecurityPolicy* GetContentSecurityPolicy() const {
     return content_security_policy_.Get();
   }
