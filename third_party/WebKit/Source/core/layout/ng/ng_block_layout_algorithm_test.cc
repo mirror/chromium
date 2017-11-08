b1540aeaaf2e4cc667327e27f93b1501ac8fa389
Gyuyoung Kim <gyuyoung.kim@lge.com>
2017-11-08 17:24:25 +0900

    Make NGFooLayoutAlgorithmTest to use ScopedLayoutNGFragmentCachingForTest
    NGColumnLayoutAlgorithmTest and NGBlockLayoutAlgorithmTest have been using
    RuntimeEnabledFeatures::SetLayoutNGFragmentCachingEnabled for testing. This cl
    removes the use of RuntimeEnabledFeatures, instead make them to use
    ScopedLayoutNGPaintFragmentsForTest.
    
    Bug: 769541
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I4794372fedbe4df6bdc0167487b95b4295970e2a

--- third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm_test.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm_test.cc
@@ -18,7 +18,7 @@
 #include "core/layout/ng/ng_physical_box_fragment.h"
 #include "core/layout/ng/ng_physical_fragment.h"
 #include "core/style/ComputedStyle.h"
-#include "platform/runtime_enabled_features.h"
+#include "platform/testing/RuntimeEnabledFeaturesTestHelpers.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
 namespace blink {
@@ -27,12 +27,14 @@ namespace {
 using ::testing::ElementsAre;
 using ::testing::Pointee;
 
-class NGBlockLayoutAlgorithmTest : public NGBaseLayoutAlgorithmTest {
+class NGBlockLayoutAlgorithmTest
+    : public NGBaseLayoutAlgorithmTest,
+      private ScopedLayoutNGFragmentCachingForTest {
+ public:
+  NGBlockLayoutAlgorithmTest() : ScopedLayoutNGFragmentCachingForTest(false) {}
+
  protected:
   void SetUp() override {
-    // Make sure to reset this, except for the one test that needs it.
-    RuntimeEnabledFeatures::SetLayoutNGFragmentCachingEnabled(false);
-
     NGBaseLayoutAlgorithmTest::SetUp();
     style_ = ComputedStyle::Create();
   }
@@ -89,7 +91,7 @@ TEST_F(NGBlockLayoutAlgorithmTest, FixedSize) {
 }
 
 TEST_F(NGBlockLayoutAlgorithmTest, Caching) {
-  RuntimeEnabledFeatures::SetLayoutNGFragmentCachingEnabled(true);
+  ScopedLayoutNGFragmentCachingForTest layout_ng_fragment_caching(true);
 
   SetBodyInnerHTML(R"HTML(
     <div id="box" style="width:30px; height:40px"></div>
