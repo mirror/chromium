8e7b7ab33bef141f68e7f19a29740b33403c7d4c
huangs <huangs@google.com>
2017-11-08 13:26:32 -0500

    [Debugging] Example of Windows test failure timing out due to modal dialog.
    DO_NOT_SUBMIT
    
    Bug:
    Change-Id: I7058be6e05700835276140f18b6498294c6c2ef1

---
+++ blah/blah_unittest.cc
@@ -0,0 +1,18 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <vector>
+
+#include "base/logging.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+TEST(BlahTest, Success) {
+  std::vector<int> v;
+  int* ptr = &v[0];
+  EXPECT_EQ(ptr, ptr);
+}
+
+}  // namespace
