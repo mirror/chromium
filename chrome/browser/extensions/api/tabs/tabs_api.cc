95809c0ea7b011c1622bb05b654725b7775fddcc
Ivan Sandrk <isandrk@google.com>
2017-11-08 16:29:16 +0100

    Introduce lockWindowFullscreenPrivate and enable creation of locked fullscreen windows
    Design doc @ go/locked-fullscreen-mode
    
    Bug: chromium:755696
    Change-Id: I78b774a40993b5672f24a2e1ae2f8a8aaa4eed8a

--- chrome/browser/extensions/api/tabs/tabs_api.cc
+++ chrome/browser/extensions/api/tabs/tabs_api.cc
@@ -95,6 +95,12 @@
 #include "ui/base/models/list_selection_model.h"
 #include "ui/base/ui_base_types.h"
 
+#if defined(OS_CHROMEOS)
+#include "ash/public/cpp/window_properties.h"
+#include "ash/public/interfaces/window_pin_type.mojom.h"
+#include "ui/aura/window.h"
+#endif
+
 using content::BrowserThread;
 using content::NavigationController;
 using content::NavigationEntry;
@@ -211,6 +217,8 @@ ui::WindowShowState ConvertToWindowShowState(windows::WindowState state) {
       return ui::SHOW_STATE_MAXIMIZED;
     case windows::WINDOW_STATE_FULLSCREEN:
       return ui::SHOW_STATE_FULLSCREEN;
+    case windows::WINDOW_STATE_LOCKED:
+      return ui::SHOW_STATE_FULLSCREEN;
     case windows::WINDOW_STATE_NONE:
       return ui::SHOW_STATE_DEFAULT;
   }
@@ -234,6 +242,7 @@ bool IsValidStateForWindowsCreateFunction(
              !is_panel;
     case windows::WINDOW_STATE_MAXIMIZED:
     case windows::WINDOW_STATE_FULLSCREEN:
+    case windows::WINDOW_STATE_LOCKED:
       // If maximised/fullscreen, default focused state should be focused.
       return !(create_data->focused && !*create_data->focused) && !has_bound &&
              !is_panel;
@@ -243,6 +252,18 @@ bool IsValidStateForWindowsCreateFunction(
       return true;
   }
   NOTREACHED();
+  return true;
+}
+
+bool CheckLockedStatePermission(
+    const Extension* extension, windows::WindowState state) {
+#if defined(OS_CHROMEOS)
+  if (state == windows::WINDOW_STATE_LOCKED &&
+      !extension->permissions_data()->HasAPIPermission(
+          APIPermission::kLockWindowFullscreenPrivate)) {
+    return false;
+  }
+#endif
   return true;
 }
 
@@ -560,11 +581,23 @@ ExtensionFunction::ResponseAction WindowsCreateFunction::Run() {
   }
   create_params.initial_show_state = ui::SHOW_STATE_NORMAL;
   if (create_data && create_data->state) {
+    if (!CheckLockedStatePermission(extension(), create_data->state)) {
+      return RespondNow(
+          Error(keys::kMissingLockWindowFullscreenPrivatePermission));
+    }
     create_params.initial_show_state =
         ConvertToWindowShowState(create_data->state);
   }
 
   Browser* new_window = new Browser(create_params);
+
+#if defined(OS_CHROMEOS)
+  if (create_data && create_data->state == windows::WINDOW_STATE_LOCKED) {
+    aura::Window* window = new_window->window()->GetNativeWindow();
+    window->SetProperty(
+        ash::kWindowPinTypeKey, ash::mojom::WindowPinType::TRUSTED_PINNED);
+  }
+#endif
 
   for (const GURL& url : urls) {
     chrome::NavigateParams navigate_params(new_window, url,
@@ -641,6 +674,11 @@ ExtensionFunction::ResponseAction WindowsUpdateFunction::Run() {
   // state (crbug.com/703733).
   ReportRequestedWindowState(params->update_info.state);
 
+  if (!CheckLockedStatePermission(extension(), params->update_info.state)) {
+    return RespondNow(
+        Error(keys::kMissingLockWindowFullscreenPrivatePermission));
+  }
+
   ui::WindowShowState show_state =
       ConvertToWindowShowState(params->update_info.state);
 
@@ -659,6 +697,14 @@ ExtensionFunction::ResponseAction WindowsUpdateFunction::Run() {
       if (controller->window()->IsMinimized() ||
           controller->window()->IsMaximized())
         controller->window()->Restore();
+#if defined(OS_CHROMEOS)
+      if (params->update_info.state == windows::WINDOW_STATE_LOCKED) {
+        aura::Window* window = controller->window()->GetNativeWindow();
+        window->SetProperty(
+            ash::kWindowPinTypeKey, ash::mojom::WindowPinType::TRUSTED_PINNED);
+        break;
+      }
+#endif
       controller->SetFullscreenMode(true, extension()->url());
       break;
     case ui::SHOW_STATE_NORMAL:
@@ -739,6 +785,17 @@ ExtensionFunction::ResponseAction WindowsRemoveFunction::Run() {
                                            &controller, &error)) {
     return RespondNow(Error(error));
   }
+
+#if defined(OS_CHROMEOS)
+  aura::Window* window = controller->window()->GetNativeWindow();
+  ash::mojom::WindowPinType type = window->GetProperty(ash::kWindowPinTypeKey);
+  if (type == ash::mojom::WindowPinType::TRUSTED_PINNED &&
+      !extension()->permissions_data()->HasAPIPermission(
+          APIPermission::kLockWindowFullscreenPrivate)) {
+    return RespondNow(
+        Error(keys::kMissingLockWindowFullscreenPrivatePermission));
+  }
+#endif
 
   WindowController::Reason reason;
   if (!controller->CanClose(&reason)) {
---
+++ chrome/test/data/extensions/api_test/locked_fullscreen/without_permission/test.js
@@ -0,0 +1,30 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const error_msg = 'Cannot lock window to fullscreen or close a locked fullscreen window without lockWindowFullscreenPrivate manifest permission';
+
+openLockedFullscreenWindow = function() {
+  chrome.windows.create({url: 'about:blank', state: 'locked'},
+    chrome.test.callbackFail(error_msg));
+};
+
+updateWindowToLockedFullscreen = function() {
+  chrome.windows.getCurrent(null, function(window) {
+    chrome.windows.update(window.id, {state: 'locked'},
+      chrome.test.callbackFail(error_msg))});
+};
+
+const tests = {
+  openLockedFullscreenWindow: openLockedFullscreenWindow,
+  updateWindowToLockedFullscreen: updateWindowToLockedFullscreen,
+};
+
+window.onload = function() {
+  chrome.test.getConfig(function(config) {
+    if (config.customArg in tests)
+      chrome.test.runTests([tests[config.customArg]]);
+    else
+      chrome.test.fail('Test "' + config.customArg + '"" doesnt exist!');
+  });
+};
