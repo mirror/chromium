5e06018936996b623abf898b4fb57a29c3a896f6
Adrian Elder <aelder@chromium.org>
2017-11-08 17:00:27 -0500

    Implement webrtcLoggingPrivate.getLogsDirectory
    This follows the pattern used by
    chrome.runtime.getPackageDirectoryEntry to return a read-only
    DirectoryEntry for the "WebRTC Logs" directory.
    
    If the directory doesn't exist yet, calling this function will
    create it, using the same method that other webrtcLoggingPrivate
    methods do.
    
    Unlike other functions in chrome.webrtcLoggingPrivate, this method
    shouldn't be called by the Hangouts component extension on the behalf
    of some web front end, so it grants access to the render process of
    the extension that invoked it rather than looking up some other render
    process.
    
    The general "webrtcLoggingPrivate" whitelist is overridden with
    "webrtcLoggingPrivate.getLogsDirectory" API permissions to only 
    permit access from internal hotrod apps. This is specifically not
    exposed to the Hangouts component extension that's included in Chrome.
    
    Bug: 775961
    Change-Id: I6e4b462d4af2b9aaba9822a13ba31f18d52c5f32

---
+++ chrome/browser/extensions/api/webrtc_logging_private/webrtc_logging_private_browsertest.cc
@@ -0,0 +1,15 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/apps/app_browsertest_util.h"
+
+using WebrtcLoggingPrivateApiBrowserTest = extensions::PlatformAppBrowserTest;
+
+IN_PROC_BROWSER_TEST_F(WebrtcLoggingPrivateApiBrowserTest,
+                       TestGetLogsDirectory) {
+  ASSERT_TRUE(
+      RunPlatformAppTest("api_test/webrtc_logging_private/"
+                         "get_logs_directory"))
+      << message_;
+}
