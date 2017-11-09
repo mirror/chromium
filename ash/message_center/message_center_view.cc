ef0cd901fe414c46368603d427b11f2199bb0b61
Aga Wronska <agawronska@chromium.org>
2017-11-08 17:08:02 -0800

    Add spoken feedback to the notification center dialog on the lock screen.
    Make message center view focusable, so it gets focus when shown even if there
    are no buttons displayed.
    Set button bar title as the accessiblity name of message center view.
    Button bar title describes the content of message center in a relevant
    way:
    * 'Notifications' when message center is enabled.
    * Hint to unlock device to see notifications on the lock screen.
    Bug:763180
    Change-Id: I292725ba3a4aceae1f6ccedfd4d18c04bfa5807a

--- ash/message_center/message_center_view.cc
+++ ash/message_center/message_center_view.cc
@@ -17,6 +17,7 @@
 #include "base/message_loop/message_loop.h"
 #include "base/stl_util.h"
 #include "build/build_config.h"
+#include "ui/accessibility/ax_node_data.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/animation/slide_animation.h"
 #include "ui/gfx/canvas.h"
@@ -139,6 +140,7 @@ MessageCenterView::MessageCenterView(MessageCenter* message_center,
   message_center_->AddObserver(this);
   set_notify_enter_exit_on_child(true);
   SetBackground(views::CreateSolidBackground(kBackgroundColor));
+  SetFocusBehavior(views::View::FocusBehavior::ALWAYS);
 
   button_bar_ = new MessageCenterButtonBar(
       this, message_center, initially_settings_visible, GetButtonBarTitle());
@@ -356,6 +358,11 @@ void MessageCenterView::OnMouseExited(const ui::MouseEvent& event) {
 
   message_list_view_->ResetRepositionSession();
   Update(true /* animate */);
+}
+
+void MessageCenterView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
+  node_data->role = ui::AX_ROLE_DIALOG;
+  node_data->SetName(GetButtonBarTitle());
 }
 
 void MessageCenterView::OnNotificationAdded(const std::string& id) {
