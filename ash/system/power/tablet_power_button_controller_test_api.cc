87ebcb4889e9143910a6872a9fd18d03ff82a339
Toni Barzic <tbarzic@google.com>
2017-11-09 14:24:07 -0800

    Control lock screen note state from power_button_display_controller
    Update power_button_display_controller to
     *  launch lock screen note when stylus is removed
     *  close lock screen note when screen is forced off
        (e.g. due to power button press)
    And remove code that currently does this from
    lock_screen_apps/state_controller.
    
    The main goal of this is to better handle lock screen app launch when
    stylus is ejected with display forced off. This action should perform
    two actions:
     1. turn the display on
     2. launch the lock screen note (if lock screen note action is enabled)
    Doing this at the same time can cause a flash of lock screen UI before
    the lock screen note app window is shown. With logic for launching
    the note moved to power_button_display_controller, it's possible to
    delay changing display state until the lock screen app window becomes
    visible, which should be long enough to prevent lock screen UI flash.
    
    BUG=767711
    
    Change-Id: Ie47f546edb51659bb82ba4618b069569a2d188c6

--- ash/system/power/tablet_power_button_controller_test_api.cc
+++ ash/system/power/tablet_power_button_controller_test_api.cc
@@ -4,6 +4,7 @@
 
 #include "ash/system/power/tablet_power_button_controller_test_api.h"
 
+#include "ash/system/power/lock_screen_note_controller.h"
 #include "ash/system/power/power_button_display_controller.h"
 #include "ash/system/power/tablet_power_button_controller.h"
 
@@ -26,6 +27,17 @@ bool TabletPowerButtonControllerTestApi::TriggerShutdownTimeout() {
 
   base::Closure task = controller_->shutdown_timer_.user_task();
   controller_->shutdown_timer_.Stop();
+  task.Run();
+  return true;
+}
+
+bool TabletPowerButtonControllerTestApi::TriggerLockScreenNoteLaunchTimeout() {
+  base::Timer* timer = &controller_->display_controller_
+                            ->lock_screen_note_controller_->launch_timer_;
+  if (!timer->IsRunning())
+    return false;
+  base::Closure task = timer->user_task();
+  timer->Stop();
   task.Run();
   return true;
 }
