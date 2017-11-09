440d0a4cec044f365b5983dd81456731690f340f
Sophie Chang <sophiechang@chromium.org>
2017-11-09 11:48:38 -0800

    Process hints and plumb optimization guide service through to previews
    Bug:777892
    Change-Id: I671f0dff35ca40e1a229df5ce42df95184378355

---
+++ components/previews/content/previews_optimization_guide_hints.h
@@ -0,0 +1,109 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PREVIEWS_CONTENT_PREVIEWS_OPTIMIZATION_GUIDE_HINTS_H_
+#define COMPONENTS_PREVIEWS_CONTENT_PREVIEWS_OPTIMIZATION_GUIDE_HINTS_H_
+
+#include <map>
+#include <set>
+
+#include "base/macros.h"
+#include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
+#include "base/sequence_checker.h"
+#include "base/sequenced_task_runner.h"
+#include "base/single_thread_task_runner.h"
+#include "components/optimization_guide/optimization_guide_service.h"
+#include "components/optimization_guide/optimization_guide_service_observer.h"
+#include "components/previews/content/previews_optimization_guide.h"
+#include "components/previews/core/previews_experiments.h"
+#include "components/url_matcher/url_matcher.h"
+
+class GURL;
+
+namespace net {
+class URLRequest;
+}  // namespace net
+
+namespace optimization_guide {
+namespace proto {
+class Configuration;
+}  // namespace proto
+}  // namespace optimization_guide
+
+namespace previews {
+
+// A PreviewsOptimizationGuide implementation that makes decisions guided by
+// hints received from the OptimizationGuideService.
+class PreviewsOptimizationGuideHints
+    : public PreviewsOptimizationGuide,
+      public optimization_guide::OptimizationGuideServiceObserver {
+ public:
+  PreviewsOptimizationGuideHints(
+      optimization_guide::OptimizationGuideService* optimization_guide_service,
+      const scoped_refptr<base::SingleThreadTaskRunner>& io_task_runner);
+
+  ~PreviewsOptimizationGuideHints() override;
+
+  // PreviewsOptimizationGuide implementation:
+  bool IsWhitelisted(const net::URLRequest& request,
+                     PreviewsType type) const override;
+
+  // optimization_guide::OptimizationGuideServiceObserver implementation:
+  void OnHintsProcessed(
+      const optimization_guide::proto::Configuration& config) override;
+
+ private:
+  // Holds previews hints extracted from the configuration sent by the
+  // Optimization Guide Service.
+  class Hints {
+   public:
+    ~Hints();
+
+    // Creates a Hints instance from the provided configuration.
+    static std::unique_ptr<Hints> CreateFromConfig(
+        const optimization_guide::proto::Configuration& config);
+
+    // Whether the URL is whitelisted for the given previews type.
+    bool IsWhitelisted(const GURL& url, PreviewsType type);
+
+   private:
+    Hints();
+
+    // The URLMatcher used to match whether a URL has any hints associated with
+    // it.
+    url_matcher::URLMatcher url_matcher_;
+
+    // A map from the condition set ID to the preview types it is whitelisted by
+    // the server for.
+    std::map<url_matcher::URLMatcherConditionSet::ID, std::set<PreviewsType>>
+        whitelist_;
+  };
+
+  // Updates the hints to the latest hints sent by the Component Updater.
+  void UpdateHints(std::unique_ptr<Hints> hints);
+
+  // The OptimizationGuideService that this guide is listening to, not owned by
+  // this class.
+  optimization_guide::OptimizationGuideService* optimization_guide_service_;
+
+  // Runner for IO thread tasks.
+  scoped_refptr<base::SingleThreadTaskRunner> io_task_runner_;
+
+  // Background thread where hints processing should be performed.
+  scoped_refptr<base::SequencedTaskRunner> background_task_runner_;
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  // The current hints used for this optimization guide.
+  std::unique_ptr<Hints> hints_;
+
+  // Used to get |weak_ptr_| to self on the IO thread.
+  base::WeakPtrFactory<PreviewsOptimizationGuideHints> io_weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(PreviewsOptimizationGuideHints);
+};
+
+}  // namespace previews
+
+#endif  // COMPONENTS_PREVIEWS_CONTENT_PREVIEWS_OPTIMIZATION_GUIDE_HINTS_H_
