2f493ea5c38e05d57e9e3aa4e82327357a6bd756
David Roger <droger@chromium.org>
2017-11-09 11:35:35 +0100

    [signin] Report Dice migration state even if migration is not enabled
    The Dice migration state (i.e. if the profile is consistent and can be
    migrated now) even when the Dice migration is not actually enabled.
    This is useful for the "Prepare Migration" step, where we try to make the
    profiles consistent without actually performing the migration.
    
    Bug: 781761
    Change-Id: I179dca5d7ee370434df26143edeefd0f34faa3a5

--- components/signin/core/browser/account_reconcilor.cc
+++ components/signin/core/browser/account_reconcilor.cc
@@ -39,6 +39,18 @@ const char kSource[] = "ChromiumAccountReconcilor";
 const char kDiceMigrationOnStartupPref[] =
     "signin.AccountReconcilor.kDiceMigrationOnStartup";
 
+const char kDiceMigrationStatusHistogram[] = "Signin.DiceMigrationStatus";
+
+// Used for UMA histogram kDiceMigrationStatusHistogram.
+// Do not remove or re-order values.
+enum class DiceMigrationStatus {
+  kEnabled,
+  kDisabledReadyForMigration,
+  kDisabledNotReadyForMigration,
+
+  kDiceMigrationStatusCount
+};
+
 class AccountEqualToFunc {
  public:
   explicit AccountEqualToFunc(const gaia::ListedAccount& account)
@@ -97,15 +109,22 @@ AccountReconcilor::AccountReconcilor(
       reconcile_on_unblock_(false) {
   VLOG(1) << "AccountReconcilor::AccountReconcilor";
   PrefService* prefs = client_->GetPrefs();
-  if (ShouldMigrateToDiceOnStartup(is_new_profile)) {
+  bool is_ready_for_dice = IsReadyForDiceMigration(is_new_profile);
+  if (is_ready_for_dice && signin::IsDiceMigrationEnabled()) {
     DCHECK(prefs);
     if (!signin::IsDiceEnabledForProfile(prefs))
       VLOG(1) << "Profile is migrating to Dice";
     signin::MigrateProfileToDice(prefs);
     DCHECK(signin::IsDiceEnabledForProfile(prefs));
   }
-  UMA_HISTOGRAM_BOOLEAN("Signin.DiceEnabledForProfile",
-                        signin::IsDiceEnabledForProfile(prefs));
+  UMA_HISTOGRAM_ENUMERATION(
+      kDiceMigrationStatusHistogram,
+      signin::IsDiceEnabledForProfile(prefs)
+          ? DiceMigrationStatus::kEnabled
+          : (is_ready_for_dice
+                 ? DiceMigrationStatus::kDisabledReadyForMigration
+                 : DiceMigrationStatus::kDisabledNotReadyForMigration),
+      DiceMigrationStatus::kDiceMigrationStatusCount);
 }
 
 AccountReconcilor::~AccountReconcilor() {
@@ -756,10 +775,9 @@ void AccountReconcilor::UnblockReconcile() {
   }
 }
 
-bool AccountReconcilor::ShouldMigrateToDiceOnStartup(bool is_new_profile) {
-  return signin::IsDiceMigrationEnabled() &&
-         (is_new_profile ||
-          client_->GetPrefs()->GetBoolean(kDiceMigrationOnStartupPref));
+bool AccountReconcilor::IsReadyForDiceMigration(bool is_new_profile) {
+  return is_new_profile ||
+         client_->GetPrefs()->GetBoolean(kDiceMigrationOnStartupPref);
 }
 
 // static
