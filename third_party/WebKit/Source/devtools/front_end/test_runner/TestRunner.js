c562e045e02fb40b73e5807c197574188cf97457
Will Chen <chenwilliam@chromium.org>
2017-11-08 16:08:11 -0800

    DevTools: automatically migrate elements/{accessibility|edit} tests
    This converts a batch of tests from the old test framework (html file)
    into the new test framework (js file).
    
    Most of it was done through an automated transformation script, with
    a small amount of manual clean-up.
    
    Bug: 667560
    Change-Id: Ib2e2837fe91e7ed38be5cc2acfececfed81c805f

--- third_party/WebKit/Source/devtools/front_end/test_runner/TestRunner.js
+++ third_party/WebKit/Source/devtools/front_end/test_runner/TestRunner.js
@@ -471,11 +471,30 @@ TestRunner.deprecatedRunAfterPendingDispatches = function(callback) {
  * @return {!Promise<undefined>}
  */
 TestRunner.loadHTML = function(html) {
+  return TestRunner._loadHTML(html, true);
+};
+
+/**
+ * This doesn't set a base tag because it causes certain document.write
+ * calls to have unexpected results
+ * @param {string} html
+ * @return {!Promise<undefined>}
+ */
+TestRunner.forceLoadHTML = function(html) {
+  return TestRunner._loadHTML(html, false);
+};
+
+/**
+ * @param {string} html
+ * @param {boolean} includeBaseTag
+ * @return {!Promise<undefined>}
+ */
+TestRunner._loadHTML = function(html, includeBaseTag) {
   var testPath = TestRunner.url();
-  if (!html.includes('<base'))
+  if (!html.includes('<base') && includeBaseTag)
     html = `<base href="${testPath}">` + html;
   html = html.replace(/'/g, '\\\'').replace(/\n/g, '\\n');
-  return TestRunner.evaluateInPageAnonymously(`document.write('${html}');document.close();`);
+  return TestRunner.evaluateInPageAnonymously(`document.write(\`${html}\`);document.close();`);
 };
 
 /**
