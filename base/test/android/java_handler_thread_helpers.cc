dc812a21ed81be3cbd2c9e3c7314916deaf6f289
Michael Thiessen <mthiesse@chromium.org>
2017-11-09 15:33:19 -0500

    ALooper implementation for MessagePumpForUI::Schedule(Delayed)Work
    This CL switches the Android MessagePumpForUI from queuing tasks on
    the Android MessageQueue, to signalling File Descriptors added to the
    ALooper's epoll implementation.
    
    Ordering between events queued in Java and C++ is not necessarily the
    order in which they were added, but this was already not the case on
    Android; this CL simply takes better advantage of that. Ordering for
    events queued in Java and C++ are still correctly ordered with respect
    to other events queued in Java and C++, respectively.
    
    Some semblance of fairness between Java and C++ tasks is guaranteed by
    alternating between executing single Java and C++ tasks when both are
    queued up.
    
    A finch kill switch and control group will be implemented for this new
    MessagePumpForUI implementation, but setting up a Feature for code that
    runs this early in Chrome startup is difficult, and I would like to
    start baking this change in Chromium as soon as possible. *If the kill
    switch is not implemented before branch cut, I will simply revert to
    the old behaviour on the branch.*
    
    Performance Improvements (measured locally using Tracing):
    On a Pixel running Android N:
    Average CPU duration for old MessagePumpForUI::ScheduleWork: 0.042ms
    Average CPU duration for new MessagePumpForUI::ScheduleWork: 0.009ms
    
    On an original Nexus 7 running Android K:
    Average CPU duration for old MessagePumpForUI::ScheduleWork: 0.050ms
    Average CPU duration for new MessagePumpForUI::ScheduleWork: 0.010ms
    
    These measurements were from ~5000 samples of PostTask recorded while
    scrolling a page. Less noisy and more representative numbers will be
    measured using a finch control group that continues using the old
    implementation.
    
    Bug: 780100
    Change-Id: Icca7443d62154e4b516a70f8afffb1882000b555

--- base/test/android/java_handler_thread_helpers.cc
+++ base/test/android/java_handler_thread_helpers.cc
@@ -4,13 +4,27 @@
 
 #include "base/test/android/java_handler_thread_helpers.h"
 
+#include <jni.h>
+
 #include "base/android/java_handler_thread.h"
+#include "base/android/jni_android.h"
+#include "base/android/scoped_java_ref.h"
 #include "base/message_loop/message_loop.h"
 #include "base/synchronization/waitable_event.h"
 #include "jni/JavaHandlerThreadHelpers_jni.h"
 
 namespace base {
 namespace android {
+
+namespace {
+
+static RepeatingCallback<void(bool)> java_task_run_callback;
+
+}  // namespace
+
+static void OnJavaTaskRun(JNIEnv* env, const JavaParamRef<jclass>& clazz) {
+  java_task_run_callback.Run(true);
+}
 
 // static
 std::unique_ptr<JavaHandlerThread> JavaHandlerThreadHelpers::CreateJavaFirst() {
@@ -33,6 +47,18 @@ bool JavaHandlerThreadHelpers::IsExceptionTestException(
     ScopedJavaLocalRef<jthrowable> exception) {
   JNIEnv* env = AttachCurrentThread();
   return Java_JavaHandlerThreadHelpers_isExceptionTestException(env, exception);
+}
+
+// static
+void JavaHandlerThreadHelpers::SetOnJavaTaskRunCallback(
+    const RepeatingCallback<void(bool)>& callback) {
+  java_task_run_callback = callback;
+}
+
+// static
+void JavaHandlerThreadHelpers::PostJavaTask() {
+  JNIEnv* env = AttachCurrentThread();
+  Java_JavaHandlerThreadHelpers_postJavaTask(env);
 }
 
 }  // namespace android
