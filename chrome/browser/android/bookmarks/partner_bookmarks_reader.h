0917e6e509647456b888de78a0fc5a641d48dee8
Troy Hildebrandt <thildebr@chromium.org>
2017-11-09 08:35:26 -0800

    Fetch partner bookmarks favicon from cache or server if not provided.
    Adds ability for PartnerBookmarksReader to fetch cached favicons from
    the ThumbnailDatabase if available, or reach out to a Google server to
    prefetch and cache the favicon without the user visiting the site.
    
    Adds a callback from native back into the Java code to get the result
    of the favicon fetching. This is so we can cache the results to avoid
    retrying failed favicon retrievals every time we load the partner
    bookmarks. The cache will come shortly in another CL.
    
    Bug:778805,780042
    Change-Id: I7a1f7f18008d17f452504037118aa0c5bb0eb3a5

--- chrome/browser/android/bookmarks/partner_bookmarks_reader.h
+++ chrome/browser/android/bookmarks/partner_bookmarks_reader.h
@@ -13,8 +13,32 @@
 #include "base/macros.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 
+namespace favicon {
+class LargeIconService;
+}
+
 class PartnerBookmarksShim;
 class Profile;
+
+#define PARTNER_BOOKMARKS_MINIMUM_FAVICON_SIZE_PX 16
+
+namespace partnerbookmarks {
+
+// GENERATED_JAVA_ENUM_PACKAGE: org.chromium.chrome.browser.partnerbookmarks
+enum class FaviconFetchResult {
+  // Successfully fetched a favicon from cache or server.
+  SUCCESS = 0,
+  // Received a server error fetching the favicon.
+  FAILURE_SERVER_ERROR = 1,
+  // The icon service was unavailable.
+  FAILURE_ICON_SERVICE_UNAVAILABLE = 2,
+  // There was nothing in the cache, but we opted out of retrieving from server.
+  FAILURE_NOT_IN_CACHE = 3,
+};
+
+using FaviconFetchedCallback = base::OnceCallback<void(FaviconFetchResult)>;
+
+}  // namespace partnerbookmarks
 
 // Generates a partner bookmark hierarchy and handles submitting the results to
 // the global PartnerBookmarksShim.
@@ -35,14 +59,44 @@ class PartnerBookmarksReader {
       jboolean is_folder,
       jlong parent_id,
       const base::android::JavaParamRef<jbyteArray>& favicon,
-      const base::android::JavaParamRef<jbyteArray>& touchicon);
+      const base::android::JavaParamRef<jbyteArray>& touchicon,
+      jboolean fetch_uncached_favicons_from_server,
+      // Callback<FaviconFetchResult>
+      const base::android::JavaParamRef<jobject>& j_callback);
   void PartnerBookmarksCreationComplete(
       JNIEnv* env,
       const base::android::JavaParamRef<jobject>& obj);
 
+  favicon::LargeIconService* GetLargeIconService();
+  void GetFavicon(const GURL& page_url,
+                  Profile* profile,
+                  bool fallback_to_server,
+                  partnerbookmarks::FaviconFetchedCallback callback);
+  void GetFaviconImpl(const GURL& page_url,
+                      Profile* profile,
+                      bool fallback_to_server,
+                      partnerbookmarks::FaviconFetchedCallback callback);
+
  private:
+  void GetFaviconFromCacheOrServer(
+      const GURL& page_url,
+      bool fallback_to_server,
+      partnerbookmarks::FaviconFetchedCallback callback);
+  void OnGetFaviconFromCacheFinished(
+      const GURL& page_url,
+      partnerbookmarks::FaviconFetchedCallback callback,
+      bool fallback_to_server,
+      const favicon_base::LargeIconResult& result);
+  void OnGetFaviconFromServerFinished(
+      const GURL& page_url,
+      partnerbookmarks::FaviconFetchedCallback callback,
+      favicon_base::GoogleFaviconServerRequestStatus status);
+
   PartnerBookmarksShim* partner_bookmarks_shim_;
   Profile* profile_;
+
+  favicon::LargeIconService* large_icon_service_;
+  base::CancelableTaskTracker favicon_task_tracker_;
 
   // JNI
   std::unique_ptr<bookmarks::BookmarkNode> wip_partner_bookmarks_root_;
