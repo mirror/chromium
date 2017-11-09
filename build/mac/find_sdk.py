435c3a746f7dd2b45bf1721b0583183284373b92
Mathias Bynens <mathias@chromium.org>
2017-11-09 13:03:32 +0100

    [build] Detect Xcode license agreement acceptance
    This patch prints a helpful error message if the Xcode license
    agreement hasn’t been accepted or if the CommandLineTools are
    installed separately from Xcode.
    
    BUG=729990
    
    Change-Id: I8d7c6550c6b144473892a8f8c491eba32cbf9e6c

--- build/mac/find_sdk.py
+++ build/mac/find_sdk.py
@@ -54,6 +54,13 @@ def main():
     raise Exception('Error %d running xcode-select' % job.returncode)
   sdk_dir = os.path.join(
       out.rstrip(), 'Platforms/MacOSX.platform/Developer/SDKs')
+  # Xcode must be installed, its license agreement must be accepted, and its
+  # command-line tools must be installed. Stand-alone installations (in
+  # /Library/Developer/CommandLineTools) are not supported.
+  # https://bugs.chromium.org/p/chromium/issues/detail?id=729990#c1
+  if not os.path.isdir(sdk_dir) or not '.app/Contents/Developer' in sdk_dir:
+    raise Exception('Install Xcode, launch it, and accept the license ' +
+      'agreement to continue.')
   sdks = [re.findall('^MacOSX(10\.\d+)\.sdk$', s) for s in os.listdir(sdk_dir)]
   sdks = [s[0] for s in sdks if s]  # [['10.5'], ['10.6']] => ['10.5', '10.6']
   sdks = [s for s in sdks  # ['10.5', '10.6'] => ['10.6']
