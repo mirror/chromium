56bfdbc125f00c64f0d458cf6e89ef7fd37ed0a2
Ioana Pandele <ioanap@google.com>
2017-11-09 16:27:41 +0100

    Separate password sorting from PasswordManagerPresenter
    Move password sorting and duplicates hiding to components/password_manager/core/browser/.
    
    Bug: 778146
    Change-Id: I425a57beb2d8032ab7907665852db6773a8658b7

---
+++ components/password_manager/core/browser/password_list_sorter.h
@@ -0,0 +1,44 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_LIST_SORTER_H_
+#define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_LIST_SORTER_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "components/autofill/core/common/password_form.h"
+#include "components/password_manager/core/browser/password_ui_utils.h"
+
+namespace password_manager {
+
+enum class PasswordEntryType { SAVED, BLACKLISTED };
+
+// Multimap from sort key to password forms.
+using DuplicatesMap =
+    std::multimap<std::string, std::unique_ptr<autofill::PasswordForm>>;
+
+// Creates key for sorting password or password exception entries. The key is
+// eTLD+1 followed by the reversed list of domains (e.g.
+// secure.accounts.example.com => example.com.com.example.accounts.secure) and
+// the scheme. If |entry_type == SAVED|, username, password and federation are
+// appended to the key. For Android credentials the canocial spec is included.
+std::string CreateSortKey(const autofill::PasswordForm& form,
+                          PasswordEntryType entry_type);
+
+// Sort entries of |list| based on sort key. The key is the concatenation of
+// origin, entry type (non-Android credential, Android w/ affiliated web realm
+// or Android w/o affiliated web realm). If |entry_type == SAVED|,
+// username, password and federation are also included in sort key. If there
+// are several forms with the same key, all such forms but the first one are
+// stored in |duplicates| instead of |list|.
+void SortEntriesAndHideDuplicates(
+    std::vector<std::unique_ptr<autofill::PasswordForm>>* list,
+    DuplicatesMap* duplicates,
+    PasswordEntryType entry_type);
+
+}  // namespace password_manager
+
+#endif  // COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_LIST_SORTER_H_
