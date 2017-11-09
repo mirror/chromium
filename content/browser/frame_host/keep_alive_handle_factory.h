cb08b3d973aa4cb7da218dbafdd89d22671818bb
Yutaka Hirano <yhirano@chromium.org>
2017-11-09 15:30:18 +0900

    Introduce RenderProcessHost::GetKeepAliveRequestTimeout
    This CL introduces the function and calls it from KeepAliveHandleFactory
    from two reasons:
    
     1. Using InitAndEnableFeatureWithParameters in
        IN_PROC_BROWSER_TEST_F is hard.
     2. We will remove the feature in the future and hence we should not
        rely on the feature to set the timeout value.
    
    This is a speculative fix for the flakiness in TSAN bots.
    
    Bug: 782037
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I29b41f63e4124aeb4cef26b9a9cab8b9d9f1e6f7

--- content/browser/frame_host/keep_alive_handle_factory.h
+++ content/browser/frame_host/keep_alive_handle_factory.h
@@ -15,16 +15,18 @@ class RenderProcessHost;
 // A KeepAliveHandleFactory creates mojom::KeepAliveHandle. Each created
 // handle prolongs the associated renderer's lifetime by using
 // RenderProcessHost's KeepAliveRefCounts while alive.
-// When a certain time (default: 30sec) passes after the factory is destroyed,
-// all created handles are invalidated, which will result in render process
-// shutdown.
+// When a certain time passes after the factory is destroyed, all created
+// handles are invalidated, which will result in render process shutdown.
 class KeepAliveHandleFactory final {
  public:
   // |process_host->DisableKeepAliveRefCount()| must be false.
-  explicit KeepAliveHandleFactory(RenderProcessHost* process_host);
+  KeepAliveHandleFactory(RenderProcessHost* process_host);
   ~KeepAliveHandleFactory();
 
   void Create(mojom::KeepAliveHandleRequest request);
+
+  // Sets the timeout after which all created handles will be invalidated.
+  void SetTimeout(base::TimeDelta timeout);
 
  private:
   class KeepAliveHandleImpl;
