0eb44123081fafa5d3c85b60a8ed9c52cb3da88e
Brian White <bcwhite@chromium.org>
2017-11-09 16:31:05 -0500

    Don't delete persistent metics file on exit.
    The persistent metrics file is no longer uploaded during startup but
    rather as a background task later on.  This means that there is no
    penalty to leaving the file around after shutdown but it will allow
    us to collect metrics up until the very exit of the process.
    
    Bug: 782879
    Change-Id: Ia353bdcc28affc029da69445f87e3c7592632cd6

--- chrome/browser/metrics/chrome_metrics_service_client.cc
+++ chrome/browser/metrics/chrome_metrics_service_client.cc
@@ -400,16 +400,6 @@ ChromeMetricsServiceClient::ChromeMetricsServiceClient(
 
 ChromeMetricsServiceClient::~ChromeMetricsServiceClient() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  base::GlobalHistogramAllocator* allocator =
-      base::GlobalHistogramAllocator::Get();
-  if (allocator) {
-    // A normal shutdown is almost complete so there is no benefit in keeping a
-    // file with no new data to be processed during the next startup sequence.
-    // Deleting the file during shutdown adds an extra disk-access or two to
-    // shutdown but eliminates the unnecessary processing of the contents during
-    // startup only to find nothing.
-    allocator->DeletePersistentLocation();
-  }
 }
 
 // static
