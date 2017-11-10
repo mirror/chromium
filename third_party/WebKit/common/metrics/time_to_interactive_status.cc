05cd95aefa780e4d1331dcf23efae044f301d2a0
Deepanjan Roy <dproy@chromium.org>
2017-11-10 11:52:06 -0500

    Add UMA for Time To Interactive
    Adds UMA for Time To Interactive by plumbing it through the Page Load
    Metrics system. The UMA is only logged if the page is never backgrouded,
    or if there is a no non-mouse-move user input hitting the renderer main
    thread before interactive time. A separate UMA histogram keeps track of
    why TTI was not recorded.
    
    
    Bug:631203
    Change-Id: Ie48d87645e1253ad7de505721d1dbec948e71dc0

---
+++ third_party/WebKit/common/metrics/time_to_interactive_status.cc
@@ -0,0 +1,15 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/WebKit/common/metrics/time_to_interactive_status.h"
+#include "base/metrics/histogram_macros.h"
+
+namespace blink {
+
+void RecordTimeToInteractiveStatus(blink::TimeToInteractiveStatus status) {
+  UMA_HISTOGRAM_ENUMERATION(blink::kHistogramTimeToInteractiveStatus, status,
+                            blink::TIME_TO_INTERACTIVE_LAST_ENTRY);
+}
+
+}  // namespace blink
