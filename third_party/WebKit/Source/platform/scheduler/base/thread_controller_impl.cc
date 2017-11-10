728ee0bccbde0bc2ff8b978b7029431bb441734b
Alexander Timin <altimin@chromium.org>
2017-11-10 17:23:41 +0000

    [scheduler] Replace TaskQueueManagerDelegate with ThreadController.
    Bug:
    Change-Id: I3c5577a68d58c6a7b9b68623d8c69e0c1e53b3db

---
+++ third_party/WebKit/Source/platform/scheduler/base/thread_controller_impl.cc
@@ -0,0 +1,97 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/scheduler/base/thread_controller_impl.h"
+
+#include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
+#include "base/time/tick_clock.h"
+#include "platform/scheduler/base/sequence.h"
+
+namespace blink {
+namespace scheduler {
+namespace internal {
+
+ThreadControllerImpl::ThreadControllerImpl(
+    base::MessageLoop* message_loop,
+    scoped_refptr<base::SingleThreadTaskRunner> task_runner,
+    std::unique_ptr<base::TickClock> time_source)
+    : message_loop_(message_loop),
+      task_runner_(task_runner),
+      time_source_(std::move(time_source)),
+      sequence_(nullptr) {
+  do_work_closure_ = base::BindRepeating(&ThreadControllerImpl::DoWork,
+                                         base::Unretained(this));
+}
+
+ThreadControllerImpl::~ThreadControllerImpl() {}
+
+std::unique_ptr<ThreadControllerImpl> ThreadControllerImpl::Create(
+    base::MessageLoop* message_loop,
+    std::unique_ptr<base::TickClock> time_source) {
+  return std::make_unique<ThreadControllerImpl>(
+      message_loop, message_loop->task_runner(), std::move(time_source));
+}
+
+void ThreadControllerImpl::SetSequence(Sequence* sequence) {
+  // DCHECK_MAIN_THREAD
+  DCHECK(!sequence_ || !sequence);
+  sequence_ = sequence;
+}
+
+void ThreadControllerImpl::ScheduleWork() {
+  DCHECK(sequence_);
+  cancelable_do_work_closure_.Reset(do_work_closure_);
+  task_runner_->PostTask(FROM_HERE, do_work_closure_);
+}
+
+void ThreadControllerImpl::ScheduleDelayedWork(base::TimeDelta delay) {
+  DCHECK(sequence_);
+  cancelable_do_work_closure_.Reset(do_work_closure_);
+  task_runner_->PostDelayedTask(FROM_HERE, do_work_closure_, delay);
+}
+
+void ThreadControllerImpl::CancelDelayedWork() {
+  DCHECK(sequence_);
+  cancelable_do_work_closure_.Cancel();
+}
+
+void ThreadControllerImpl::PostNonNestableTask(const base::Location& from_here,
+                                               base::Closure task) {
+  task_runner_->PostNonNestableTask(from_here, std::move(task));
+}
+
+bool ThreadControllerImpl::RunsTasksInCurrentSequence() {
+  return task_runner_->RunsTasksInCurrentSequence();
+}
+
+base::TickClock* ThreadControllerImpl::GetClock() {
+  return time_source_.get();
+}
+
+bool ThreadControllerImpl::IsNested() {
+  DCHECK(RunsTasksInCurrentSequence());
+  return base::RunLoop::IsNestedOnCurrentThread();
+}
+
+void ThreadControllerImpl::DoWork() {
+  DCHECK(sequence_);
+  base::PendingTask task = sequence_->TakeTask();
+  task_annotator_.RunTask("ThreadControllerImpl::DoWork", &task);
+  sequence_->DidRunTask();
+}
+
+void ThreadControllerImpl::AddNestingObserver(
+    base::RunLoop::NestingObserver* observer) {
+  base::RunLoop::AddNestingObserverOnCurrentThread(observer);
+}
+
+void ThreadControllerImpl::RemoveNestingObserver(
+    base::RunLoop::NestingObserver* observer) {
+  base::RunLoop::RemoveNestingObserverOnCurrentThread(observer);
+}
+
+}  // namespace internal
+}  // namespace scheduler
+}  // namespace blink
---
+++ third_party/WebKit/Source/platform/scheduler/base/thread_controller_impl.h
@@ -0,0 +1,68 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_WEBKIT_SOURCE_PLATFORM_SCHEDULER_BASE_WORKER_CONTROLLER_IMPL_H_
+#define THIRD_PARTY_WEBKIT_SOURCE_PLATFORM_SCHEDULER_BASE_WORKER_CONTROLLER_IMPL_H_
+
+#include "platform/scheduler/base/thread_controller.h"
+
+#include "base/cancelable_callback.h"
+#include "base/debug/task_annotator.h"
+#include "base/macros.h"
+#include "base/single_thread_task_runner.h"
+
+namespace base {
+class MessageLoop;
+class TickClock;
+}  // namespace base
+
+namespace blink {
+namespace scheduler {
+namespace internal {
+
+class ThreadControllerImpl : public ThreadController {
+ public:
+  ~ThreadControllerImpl() override;
+
+  std::unique_ptr<ThreadControllerImpl> Create(
+      base::MessageLoop* message_loop,
+      std::unique_ptr<base::TickClock> time_source);
+
+  // ThreadController implementation:
+  void ScheduleWork() override;
+  void ScheduleDelayedWork(base::TimeDelta delay) override;
+  void CancelDelayedWork() override;
+  void SetSequence(Sequence* sequence) override;
+  void PostNonNestableTask(const base::Location& from_here,
+                           base::Closure task) override;
+  bool RunsTasksInCurrentSequence() override;
+  base::TickClock* GetClock() override;
+  bool IsNested() override;
+  void AddNestingObserver(base::RunLoop::NestingObserver* observer) override;
+  void RemoveNestingObserver(base::RunLoop::NestingObserver* observer) override;
+
+ protected:
+  ThreadControllerImpl(base::MessageLoop* message_loop,
+                       scoped_refptr<base::SingleThreadTaskRunner> task_runner,
+                       std::unique_ptr<base::TickClock> time_source);
+
+ private:
+  void DoWork();
+
+  base::MessageLoop* message_loop_;
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
+  std::unique_ptr<base::TickClock> time_source_;
+  base::RepeatingClosure do_work_closure_;
+  base::CancelableClosure cancelable_do_work_closure_;
+  Sequence* sequence_;  // NOT OWNED
+  base::debug::TaskAnnotator task_annotator_;
+
+  DISALLOW_COPY_AND_ASSIGN(ThreadControllerImpl);
+};
+
+}  // namespace internal
+}  // namespace scheduler
+}  // namespace blink
+
+#endif  // THIRD_PARTY_WEBKIT_SOURCE_PLATFORM_SCHEDULER_BASE_WORKER_CONTROLLER_IMPL_H_
