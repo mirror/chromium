f2b9577c3e193b929ce5c9dbd067c45dadd4064d
Han Leon <leon.han@intel.com>
2017-11-10 08:35:50 +0800

    [ServiceWorker] Introduce ServiceWorkerHost interface
    BUG=772793
    
    Change-Id: I48b56e1da07429a4630c6ba7bc7625c13e85a252

--- content/browser/service_worker/service_worker_job_unittest.cc
+++ content/browser/service_worker/service_worker_job_unittest.cc
@@ -38,6 +38,7 @@
 #include "net/base/test_completion_callback.h"
 #include "net/http/http_response_headers.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/WebKit/public/platform/modules/serviceworker/service_worker.mojom.h"
 #include "third_party/WebKit/public/platform/modules/serviceworker/service_worker_event_status.mojom.h"
 #include "third_party/WebKit/public/platform/modules/serviceworker/service_worker_object.mojom.h"
 #include "third_party/WebKit/public/platform/modules/serviceworker/service_worker_registration.mojom.h"
@@ -1819,6 +1820,7 @@ class CheckPauseAfterDownloadEmbeddedWorkerInstanceClient
       mojom::ServiceWorkerEventDispatcherRequest dispatcher_request,
       mojom::ControllerServiceWorkerRequest controller_request,
       mojom::ServiceWorkerInstalledScriptsInfoPtr scripts_info,
+      blink::mojom::ServiceWorkerHostAssociatedPtrInfo service_worker_host,
       mojom::EmbeddedWorkerInstanceHostAssociatedPtrInfo instance_host,
       mojom::ServiceWorkerProviderInfoForStartWorkerPtr provider_info,
       blink::mojom::WorkerContentSettingsProxyPtr content_settings_proxy)
@@ -1828,8 +1830,9 @@ class CheckPauseAfterDownloadEmbeddedWorkerInstanceClient
     num_of_startworker_++;
     EmbeddedWorkerTestHelper::MockEmbeddedWorkerInstanceClient::StartWorker(
         params, std::move(dispatcher_request), std::move(controller_request),
-        std::move(scripts_info), std::move(instance_host),
-        std::move(provider_info), std::move(content_settings_proxy));
+        std::move(scripts_info), std::move(service_worker_host),
+        std::move(instance_host), std::move(provider_info),
+        std::move(content_settings_proxy));
   }
 
  private:
--- content/common/service_worker/service_worker_messages.h
+++ content/common/service_worker/service_worker_messages.h
@@ -170,10 +170,6 @@ IPC_MESSAGE_ROUTED2(ServiceWorkerHostMsg_SetCachedMetadata,
                     GURL /* url */,
                     std::vector<char> /* data */)
 
-// ServiceWorker -> Browser message to request that the ServiceWorkerStorage
-// clear the cache associated with |url|.
-IPC_MESSAGE_ROUTED1(ServiceWorkerHostMsg_ClearCachedMetadata, GURL /* url */)
-
 // Ask the browser to open a tab/window (renderer->browser).
 IPC_MESSAGE_ROUTED2(ServiceWorkerHostMsg_OpenNewTab,
                     int /* request_id */,
