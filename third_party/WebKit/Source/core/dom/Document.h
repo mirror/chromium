a86aa92b27009f95254c399095e51fec4f06b461
Avi Drissman <avi@chromium.org>
2017-11-10 11:14:07 -0500

    Clear the page title on reload.
    This change makes it so that Blink, as it loads a page,
    always reports a title, removing guesswork from the higher
    layers.
    
    BUG=96041, 783529
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ide09c91342cdb3d36f8a02fccf2f4755d551a64f

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -1470,6 +1470,7 @@ class CORE_EXPORT Document : public ContainerNode,
   ShadowCascadeOrder shadow_cascade_order_ = kShadowCascadeNone;
 
   void UpdateTitle(const String&);
+  void DispatchDidReceiveTitle();
   void UpdateFocusAppearanceTimerFired(TimerBase*);
   void UpdateBaseURL();
 
