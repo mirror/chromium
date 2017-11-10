1e056b5035e82e0a0f17c4feecc7a4541933a0c0
Tibor Goldschwendt <tiborg@chromium.org>
2017-11-10 16:08:55 -0500

    [vr] Re-enable Ganesh and link to correct GL interface
    Bind Ganesh to //ui/gl provided GL functions. This avoids the need for
    linking to system provided EGL and GLESv2 libraries.
    
    Bug: 782871, 782540
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I04f250ac68b926b25c7563a8fad4f7d939930da2

--- chrome/browser/vr/ganesh_surface_provider.cc
+++ chrome/browser/vr/ganesh_surface_provider.cc
@@ -4,19 +4,29 @@
 
 #include "chrome/browser/vr/ganesh_surface_provider.h"
 
+#include "gpu/command_buffer/service/create_gr_gl_interface.h"
 #include "skia/ext/texture_handle.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/gpu/GrContext.h"
-#include "third_party/skia/include/gpu/GrContextOptions.h"
-#include "third_party/skia/include/gpu/GrTypes.h"
-#include "third_party/skia/include/gpu/gl/GrGLInterface.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/gl/gl_implementation.h"
+#include "ui/gl/gl_version_info.h"
 
 namespace vr {
 
 GaneshSurfaceProvider::GaneshSurfaceProvider() {
-  gr_context_ = GrContext::MakeGL(nullptr);
+  const char* version_str =
+      reinterpret_cast<const char*>(glGetString(GL_VERSION));
+  const char* renderer_str =
+      reinterpret_cast<const char*>(glGetString(GL_RENDERER));
+  std::string extensions_string(gl::GetGLExtensionsFromCurrentContext());
+  gl::ExtensionSet extensions(gl::MakeExtensionSet(extensions_string));
+  gl::GLVersionInfo gl_version_info(version_str, renderer_str, extensions);
+  sk_sp<const GrGLInterface> gr_interface =
+      gpu::gles2::CreateGrGLInterface(gl_version_info);
+  DCHECK(gr_interface.get());
+  gr_context_ = GrContext::MakeGL(gr_interface.get());
   DCHECK(gr_context_.get());
   glGetIntegerv(GL_FRAMEBUFFER_BINDING, &main_fbo_);
 }
