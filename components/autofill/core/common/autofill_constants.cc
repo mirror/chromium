7d5742e993685c5f1739a55b5acc08961375100a
Roger McFarlane <rogerm@chromium.org>
2017-11-10 17:26:50 -0500

    [autofill] Add feature flag to disable requirement of min number of fields.
    This CL allows the autofill team to experiment with expanding the scope of
    field prediction and filling to single form fields.
    
    Bug: 731719, 626785, 604994
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: If91ec4c68e1e2d819a03818a6766f0fec9d04e32

--- components/autofill/core/common/autofill_constants.cc
+++ components/autofill/core/common/autofill_constants.cc
@@ -8,6 +8,20 @@
 
 namespace autofill {
 
+// TODO(rogerm): Move autofill_experiments from browser to common then relocate
+// this feature flags.
+const base::Feature kAutofillEnforceMinRequiredFieldsForHeuristics{
+    "AutofillEnforceMinRequiredFieldsForHeuristics",
+    base::FEATURE_ENABLED_BY_DEFAULT};
+
+const base::Feature kAutofillEnforceMinRequiredFieldsForQuery{
+    "AutofillEnforceMinRequiredFieldsForQuery",
+    base::FEATURE_ENABLED_BY_DEFAULT};
+
+const base::Feature kAutofillEnforceMinRequiredFieldsForUpload{
+    "AutofillEnforceMinRequiredFieldsForUpload",
+    base::FEATURE_DISABLED_BY_DEFAULT};
+
 const char kHelpURL[] =
 #if defined(OS_CHROMEOS)
     "https://support.google.com/chromebook/?p=settings_autofill";
@@ -16,5 +30,23 @@ const char kHelpURL[] =
 #endif
 
 const char kSettingsOrigin[] = "Chrome settings";
+
+size_t MinRequiredFieldsForHeuristics() {
+  return base::FeatureList::IsEnabled(
+             kAutofillEnforceMinRequiredFieldsForHeuristics)
+             ? 3
+             : 1;
+}
+size_t MinRequiredFieldsForQuery() {
+  return base::FeatureList::IsEnabled(kAutofillEnforceMinRequiredFieldsForQuery)
+             ? 3
+             : 1;
+}
+size_t MinRequiredFieldsForUpload() {
+  return base::FeatureList::IsEnabled(
+             kAutofillEnforceMinRequiredFieldsForUpload)
+             ? 3
+             : 1;
+}
 
 }  // namespace autofill
