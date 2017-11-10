33469d3a6b66e93f746b3b7badeb448c3d5a213f
Karl Anders Oygard <karlo@opera.com>
2017-11-10 16:08:13 +0100

    [LayoutNG] Implement support for text-align: -webkit-*
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I9c90e4ded306f2560dd82e4ed17b876a592e599e

--- third_party/WebKit/Source/core/layout/ng/ng_length_utils.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_length_utils.cc
@@ -520,21 +520,33 @@ NGBoxStrut ComputePadding(const NGConstraintSpace& constraint_space,
 }
 
 void ApplyAutoMargins(const ComputedStyle& style,
+                      const ComputedStyle& containing_block_style,
                       LayoutUnit available_inline_size,
                       LayoutUnit inline_size,
                       NGBoxStrut* margins) {
   DCHECK(margins) << "Margins cannot be NULL here";
   const LayoutUnit used_space = inline_size + margins->InlineSum();
   const LayoutUnit available_space = available_inline_size - used_space;
-  if (available_space < LayoutUnit())
+  if (available_space < LayoutUnit()) {
+    margins->inline_end =
+        available_inline_size - inline_size - margins->inline_start;
     return;
-  if (style.MarginStart().IsAuto() && style.MarginEnd().IsAuto()) {
-    margins->inline_start = available_space / 2;
+  }
+  if ((style.MarginStart().IsAuto() && style.MarginEnd().IsAuto()) ||
+      (!style.MarginStart().IsAuto() && !style.MarginEnd().IsAuto() &&
+       containing_block_style.GetTextAlign() == ETextAlign::kWebkitCenter)) {
+    margins->inline_start += available_space / 2;
     margins->inline_end = available_space - margins->inline_start;
-  } else if (style.MarginStart().IsAuto()) {
-    margins->inline_start = available_space;
-  } else if (style.MarginEnd().IsAuto()) {
-    margins->inline_end = available_space;
+  } else if (style.MarginStart().IsAuto() ||
+             containing_block_style.GetTextAlign() ==
+                 ETextAlign::kWebkitRight) {
+    margins->inline_start += available_space;
+  } else if (style.MarginEnd().IsAuto() ||
+             containing_block_style.GetTextAlign() == ETextAlign::kWebkitLeft) {
+    margins->inline_end += available_space;
+  } else {
+    margins->inline_end =
+        available_inline_size - inline_size - margins->inline_start;
   }
 }
 
