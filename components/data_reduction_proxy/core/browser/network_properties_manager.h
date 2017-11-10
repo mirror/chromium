7511030898aed491b2d3591073c707093d1b119d
Tarun Bansal <tbansal@chromium.org>
2017-11-09 18:48:18 -0800

    DRP DNR
    Bug:
    Change-Id: I1941fee1de13c0a5c2f7449e8300994c321ceb49

--- components/data_reduction_proxy/core/browser/network_properties_manager.h
+++ components/data_reduction_proxy/core/browser/network_properties_manager.h
@@ -5,17 +5,32 @@
 #ifndef COMPONENTS_DATA_REDUCTION_PROXY_CORE_BROWSER_NETWORK_PROPERTIES_MANAGER_H_
 #define COMPONENTS_DATA_REDUCTION_PROXY_CORE_BROWSER_NETWORK_PROPERTIES_MANAGER_H_
 
+#include <map>
+#include <memory>
+#include <string>
 #include "base/macros.h"
 #include "components/data_reduction_proxy/proto/network_properties.pb.h"
+
+#include "base/macros.h"
+#include "base/sequenced_task_runner.h"
+#include "base/single_thread_task_runner.h"
+#include "base/threading/thread_checker.h"
+#include "base/values.h"
+#include "components/prefs/pref_service.h"
 
 namespace data_reduction_proxy {
 
 // Stores the properties of a single network.
 class NetworkPropertiesManager {
  public:
-  NetworkPropertiesManager();
+  NetworkPropertiesManager(
+      PrefService* pref_service,
+      scoped_refptr<base::SequencedTaskRunner> ui_task_runner,
+      scoped_refptr<base::SingleThreadTaskRunner> io_task_runner);
 
   ~NetworkPropertiesManager();
+
+  void OnChangeInNetworkID(const std::string& network_id);
 
   // Returns true if usage of secure proxies are allowed on the current network.
   bool IsSecureProxyAllowed() const;
@@ -51,9 +66,51 @@ class NetworkPropertiesManager {
   void SetHasWarmupURLProbeFailed(bool secure_proxy,
                                   bool warmup_url_probe_failed);
 
+  // Map from network IDs to network properties.
+  typedef std::map<std::string, NetworkProperties> ParsedPrefs;
+
+  // Reads the prefs again, parses them, and returns the parsed map.
+  ParsedPrefs ForceReadPrefsForTesting() const;
+
  private:
-  // State of the proxies on the current network.
-  NetworkProperties network_properties_;
+  void OnChangeInNetworkPropertyOnIOThread();
+
+  void OnChangeInNetworkPropertyOnUIThread(
+      const std::string& network_id,
+      const NetworkProperties& network_properties);
+
+  static ParsedPrefs ConvertDictionaryValueToParsedPrefs(
+      const base::DictionaryValue* value);
+
+  scoped_refptr<base::SequencedTaskRunner> ui_task_runner_;
+  scoped_refptr<base::SingleThreadTaskRunner> io_task_runner_;
+
+  // Guaranteed to be non-null during the lifetime of |this|.
+  PrefService* pref_service_;
+
+  // |path_| is the location of the network quality estimator prefs.
+  const std::string path_;
+
+  // Current prefs on the disk. Should be accessed only on the UI thread.
+  std::unique_ptr<base::DictionaryValue> prefs_;
+
+  ParsedPrefs io_parsed_prefs_;
+
+  // Thread safe: Can be accessed on any thread.
+  const ParsedPrefs parsed_prefs_startup_;
+
+  // ID of the current network. Must be accessed on the IO thread.
+  std::string io_network_id_;
+
+  // State of the proxies on the current network. Must be accessed on the IO
+  // thread.
+  NetworkProperties io_network_properties_;
+
+  // Should be accessed only on the UI thread.
+  base::WeakPtr<NetworkPropertiesManager> ui_weak_ptr_;
+
+  // Used to get |weak_ptr_| to self on the UI thread.
+  base::WeakPtrFactory<NetworkPropertiesManager> ui_weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkPropertiesManager);
 };
