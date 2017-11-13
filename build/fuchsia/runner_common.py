1d5ddd75c82df1871624de3cbf1bbc17efd6ccc2
Scott Graham <scottmg@chromium.org>
2017-11-13 14:54:28 -0800

    fuchsia: Use zircon.autorun.system instead of relying on magic autorun
    Implicit launch of /system/autorun is being deprecated in
    https://fuchsia-review.googlesource.com/c/zircon/+/86815. Instead, use
    kernel command line option to accomplish the same. The autorun script
    file is also renamed from /system/autorun to /system/cr_autorun so that
    in the overlapping time before that CL lands, the autorun script won't
    be run twice at startup.
    
    Bug: 784586
    Change-Id: I7ac2eeb9aa79b20c1683e3b5fb504875b0d79191

--- build/fuchsia/runner_common.py
+++ build/fuchsia/runner_common.py
@@ -197,11 +197,13 @@ class BootfsData(object):
   bootfs: Local path to .bootfs image file.
   symbols_mapping: A dict mapping executables to their unstripped originals.
   target_cpu: GN's target_cpu setting for the image.
+  wrote_autorun: Whether an autorun file was written for /system/cr_autorun.
   """
-  def __init__(self, bootfs_name, symbols_mapping, target_cpu):
+  def __init__(self, bootfs_name, symbols_mapping, target_cpu, wrote_autorun):
     self.bootfs = bootfs_name
     self.symbols_mapping = symbols_mapping
     self.target_cpu = target_cpu
+    self.wrote_autorun = wrote_autorun
 
 
 def WriteAutorun(bin_name, child_args, summary_output, shutdown_machine,
@@ -259,10 +261,10 @@ def WriteAutorun(bin_name, child_args, summary_output, shutdown_machine,
 
   autorun_file.flush()
   os.chmod(autorun_file.name, 0750)
-  _DumpFile(dry_run, autorun_file.name, 'autorun')
+  _DumpFile(dry_run, autorun_file.name, 'cr_autorun')
 
   # Add the autorun file, logger file, and target binary to |file_mapping|.
-  file_mapping['autorun'] = autorun_file.name
+  file_mapping['cr_autorun'] = autorun_file.name
   file_mapping[os.path.basename(bin_name)] = bin_name
 
 def BuildBootfs(output_directory, runtime_deps, bin_name, child_args, dry_run,
@@ -273,8 +275,8 @@ def BuildBootfs(output_directory, runtime_deps, bin_name, child_args, dry_run,
   file_mapping = dict(runtime_deps)
 
   if use_autorun:
-      WriteAutorun(bin_name, child_args, summary_output, shutdown_machine,
-                   wait_for_network, dry_run, use_device, file_mapping)
+    WriteAutorun(bin_name, child_args, summary_output, shutdown_machine,
+                 wait_for_network, dry_run, use_device, file_mapping)
 
   # Find the full list of files to add to the bootfs.
   file_mapping = _ExpandDirectories(
@@ -301,7 +303,7 @@ def BuildBootfs(output_directory, runtime_deps, bin_name, child_args, dry_run,
   if _RunAndCheck(dry_run, args) != 0:
     return None
 
-  return BootfsData(bootfs_name, symbols_mapping, target_cpu)
+  return BootfsData(bootfs_name, symbols_mapping, target_cpu, use_autorun)
 
 
 def _SymbolizeEntries(entries):
@@ -500,6 +502,9 @@ def RunFuchsia(bootfs_data, use_device, kernel_path, dry_run,
                           bootfs_data.bootfs, '--',
                           '-o', 'zircon.nodename=%s' % INSTANCE_ID,
                           '-o', 'devmgr.epoch=%d' % time.time()]
+    if bootfs_data.wrote_autorun:
+      bootserver_command.extend(
+          ['-o', 'zircon.autorun.system=/boot/bin/sh+/system/cr_autorun'])
     _RunAndCheck(dry_run, bootserver_command)
 
     # Start listening for logging lines.
@@ -529,7 +534,9 @@ def RunFuchsia(bootfs_data, use_device, kernel_path, dry_run,
 
         # TERM=dumb tells the guest OS to not emit ANSI commands that trigger
         # noisy ANSI spew from the user's terminal emulator.
-        '-append', 'TERM=dumb kernel.halt_on_panic=true',
+        '-append', 'TERM=dumb kernel.halt_on_panic=true' +
+            (' zircon.autorun.system=/boot/bin/sh+/system/cr_autorun'
+             if bootfs_data.wrote_autorun else '')
       ]
 
     # Configure the machine & CPU to emulate, based on the target architecture.
