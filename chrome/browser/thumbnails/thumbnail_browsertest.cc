65ac8374c88e79ba8f2a744c90bfe4848e8dd541
Marc Treib <treib@chromium.org>
2017-11-13 13:30:21 +0100

    Thumbnail capturing: Try to deflake SlowPageLoad test
    This CL adds an explicit wait after the navigation to the page in
    question has finished, to give the renderer some time to actually paint
    it before we attempt to take a screenshot.
    
    Bug: 778248, 784300
    Change-Id: I2a0ecd73b6f182a064728d03a380d2ed40fa50f8

--- chrome/browser/thumbnails/thumbnail_browsertest.cc
+++ chrome/browser/thumbnails/thumbnail_browsertest.cc
@@ -85,6 +85,14 @@ std::string MakeHtmlDocument(const std::string& background_color) {
       background_color.c_str());
 }
 
+void GiveItSomeTime() {
+  base::RunLoop run_loop;
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE, run_loop.QuitClosure(),
+      base::TimeDelta::FromMilliseconds(100));
+  run_loop.Run();
+}
+
 class MockThumbnailService : public ThumbnailService {
  public:
   MOCK_METHOD2(SetPageThumbnail,
@@ -285,8 +293,13 @@ IN_PROC_BROWSER_TEST_F(ThumbnailTest,
       SetPageThumbnail(Field(&ThumbnailingContext::url, about_blank_url), _))
       .Times(0);
 
-  // Navigate to some page.
+  // Navigate to the red page.
   ui_test_utils::NavigateToURL(browser(), red_url);
+
+  // Give the renderer process some time to actually paint it. Without this,
+  // there's a chance we might attempt to take a screenshot before the first
+  // paing, which would fail.
+  GiveItSomeTime();
 
   // Before navigating away from the red page, we should take a thumbnail.
   // Note that the page load is deliberately slowed down, so that the
