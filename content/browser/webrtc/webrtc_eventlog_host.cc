8feb553b763943451de25f9712f268b19b17a846
Elad Alon <eladalon@chromium.org>
2017-11-13 13:25:55 +0100

    Pipe RtcEventLog fragments to BrowserThread::UI
    We're about to create an object called RtcEventLogManager, that would
    run on Browserthread::UI. It will be in charge of getting notifications
    of RTC event logs and handling them. Specifically, we expect it to
    write those to files and upload those files to remote servers. This CL
    pipes those messages from WebRTC back into close to where
    RtcEventLogManager will live.
    
    Bug: 775906
    Change-Id: I477832e3f1b18b3c5090bf7bb137ff2d60c4e37a

--- content/browser/webrtc/webrtc_eventlog_host.cc
+++ content/browser/webrtc/webrtc_eventlog_host.cc
@@ -27,6 +27,11 @@ namespace content {
 
 namespace {
 
+// TODO(eladalon): This blocks the new code until the code handling it is
+// introduce by the next CL in the stack.
+// Namely, https://chromium-review.googlesource.com/c/chromium/src/+/760816.
+constexpr bool kRtcEventLoggingFromHostApplication = false;
+
 // In addition to the limit to the number of files given below, the size of the
 // files is also capped, see content/renderer/media/peer_connection_tracker.cc.
 #if defined(OS_ANDROID)
@@ -146,14 +151,23 @@ WebRTCEventLogHost::ActivePeerConnectionsWithLogFiles() {
 
 void WebRTCEventLogHost::StartEventLogForPeerConnection(
     int peer_connection_local_id) {
-  if (ActivePeerConnectionsWithLogFiles().size() < kMaxNumberLogFiles) {
-    RtcEventLogAdded(peer_connection_local_id);
-    base::PostTaskWithTraitsAndReplyWithResult(
-        FROM_HERE, {base::MayBlock(), base::TaskPriority::BACKGROUND},
-        base::Bind(&CreateEventLogFileForChildProcess, base_file_path_,
-                   render_process_id_, peer_connection_local_id),
-        base::Bind(&WebRTCEventLogHost::SendEventLogFileToRenderer,
-                   weak_ptr_factory_.GetWeakPtr(), peer_connection_local_id));
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  if (kRtcEventLoggingFromHostApplication) {
+    RenderProcessHost* rph = RenderProcessHost::FromID(render_process_id_);
+    if (rph) {
+      rph->Send(new PeerConnectionTracker_StartEventLogOutput(
+          peer_connection_local_id));
+    }
+  } else {
+    if (ActivePeerConnectionsWithLogFiles().size() < kMaxNumberLogFiles) {
+      RtcEventLogAdded(peer_connection_local_id);
+      base::PostTaskWithTraitsAndReplyWithResult(
+          FROM_HERE, {base::MayBlock(), base::TaskPriority::BACKGROUND},
+          base::Bind(&CreateEventLogFileForChildProcess, base_file_path_,
+                     render_process_id_, peer_connection_local_id),
+          base::Bind(&WebRTCEventLogHost::SendEventLogFileToRenderer,
+                     weak_ptr_factory_.GetWeakPtr(), peer_connection_local_id));
+    }
   }
 }
 
@@ -168,8 +182,8 @@ void WebRTCEventLogHost::SendEventLogFileToRenderer(
   }
   RenderProcessHost* rph = RenderProcessHost::FromID(render_process_id_);
   if (rph) {
-    rph->Send(new PeerConnectionTracker_StartEventLog(peer_connection_local_id,
-                                                      file_for_transit));
+    rph->Send(new PeerConnectionTracker_StartEventLogFile(
+        peer_connection_local_id, file_for_transit));
   } else {
     bool removed = RtcEventLogRemoved(peer_connection_local_id);
     DCHECK(removed);
