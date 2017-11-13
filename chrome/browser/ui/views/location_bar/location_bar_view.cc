f46fcc394515284c7b9a885a5228e14d80912119
Allen Bauer <kylixrd@chromium.org>
2017-11-13 07:19:31 -0800

    Added check for existance of the FindBarController before causing it to be created on demand. This should prevent a startup performance regression.
    Bug: 783350
    Change-Id: Idbf4243e22db833fba8123c6b9e828c8c953ff12

--- chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -764,8 +764,10 @@ bool LocationBarView::RefreshSaveCreditCardIconView() {
 }
 
 bool LocationBarView::RefreshFindBarIcon() {
-  if (!find_bar_icon_ || !browser_ || !browser_->window())
+  if (!find_bar_icon_ || !browser_ || !browser_->window() ||
+      !browser_->HasFindBarController()) {
     return false;
+  }
   const bool was_visible = find_bar_icon_->visible();
   find_bar_icon_->SetVisible(
       browser_->GetFindBarController()->find_bar()->IsFindBarVisible());
