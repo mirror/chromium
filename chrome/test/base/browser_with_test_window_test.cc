9dcd0aa0cc8b2e1a36359ec98a5faf34a2fa9d00
Evan Stade <estade@chromium.org>
2017-11-13 14:47:27 -0700

    Migrate some more uses of g_browser_process->notification_ui_manager() to NotificationDisplayService.
    Also:
    - Combine the two generic click handling NotificationDelegates.
    - Change one place where an app ID is string16 to std::string (remove
      many string conversions).
    - Add a TestingProfileManager to BrowserWithTestWindowTest so that code
      which references the profile manager works properly in this kind of
      unit test.
    
    Bug: 783018, 755413
    Change-Id: I180350bfa0507851f658d619f2918c04a81c5d7a

--- chrome/test/base/browser_with_test_window_test.cc
+++ chrome/test/base/browser_with_test_window_test.cc
@@ -15,7 +15,10 @@
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
+#include "chrome/test/base/testing_profile_manager.h"
+#include "components/sync_preferences/pref_service_syncable.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
@@ -74,8 +77,13 @@ void BrowserWithTestWindowTest::SetUp() {
   if (content::IsBrowserSideNavigationEnabled())
     content::BrowserSideNavigationSetUp();
 
+  profile_manager_ = std::make_unique<TestingProfileManager>(
+      TestingBrowserProcess::GetGlobal());
+  ASSERT_TRUE(profile_manager_->SetUp());
+
   // Subclasses can provide their own Profile.
-  profile_ = CreateProfile();
+  profile_ = profile_manager_->CreateTestingProfile(
+      "testing_profile", nullptr, base::string16(), 0, std::string(), {});
   // Subclasses can provide their own test BrowserWindow. If they return NULL
   // then Browser will create the a production BrowserWindow and the subclass
   // is responsible for cleaning it up (usually by NativeWidget destruction).
@@ -103,20 +111,14 @@ void BrowserWithTestWindowTest::TearDown() {
   constrained_window::SetConstrainedWindowViewsClient(nullptr);
 #endif
 
+  profile_manager_.reset();
+  profile_ = nullptr;
+
 #if defined(OS_CHROMEOS)
-  // Destroy the shell before the profile to match production shutdown ordering.
   ash_test_helper_->TearDown();
 #elif defined(TOOLKIT_VIEWS)
   views_test_helper_.reset();
 #endif
-
-  // Destroy the profile here - otherwise, if the profile is freed in the
-  // destructor, and a test subclass owns a resource that the profile depends
-  // on (such as g_browser_process()->local_state()) there's no way for the
-  // subclass to free it after the profile.
-  if (profile_)
-    DestroyProfile(profile_);
-  profile_ = nullptr;
 
   testing::Test::TearDown();
 
