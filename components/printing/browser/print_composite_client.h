11cb3b8aebb5b37726fe46bee2e86a8f3fa1b8d4
Wei Li <weili@chromium.org>
2017-11-13 18:06:25 -0800

    Complete pdf compositor service
    Add interfaces and functions to allow passing in and composite
    contents from multiple frames in pdf compositor service.
    
    Each frame is identified by its frame id, while its drawing content
    is identified by the content's uid (unique id). Each frame may have
    subframes. The pdf compositor service will keep track of all the frame
    content and their relationship. When all subframes are painted or
    timeout is reached, the service can composite the entire frame and
    convert the result into PDF format.
    
    BUG=455764
    
    Change-Id: I3213d9ae0ca85d0ea695dd13acb00938499aa73b

--- components/printing/browser/print_composite_client.h
+++ components/printing/browser/print_composite_client.h
@@ -8,6 +8,7 @@
 #include "base/memory/ref_counted_memory.h"
 #include "base/memory/shared_memory.h"
 #include "base/memory/shared_memory_handle.h"
+#include "components/printing/common/print_messages.h"
 #include "components/printing/service/public/cpp/pdf_compositor_client.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
@@ -21,9 +22,12 @@ class PrintCompositeClient
   explicit PrintCompositeClient(content::WebContents* web_contents);
   ~PrintCompositeClient() override;
 
-  void DoComposite(base::SharedMemoryHandle handle,
-                   uint32_t data_size,
-                   mojom::PdfCompositor::CompositePdfCallback callback);
+  void DoCompositeToPdf(uint32_t frame_id,
+                        uint32_t page_num,
+                        base::SharedMemoryHandle handle,
+                        uint32_t data_size,
+                        std::vector<uint32_t> subframe_uids,
+                        mojom::PdfCompositor::CompositeToPdfCallback callback);
 
   void set_for_preview(bool for_preview) { for_preview_ = for_preview; }
   bool for_preview() const { return for_preview_; }
@@ -36,10 +40,23 @@ class PrintCompositeClient
 
  private:
   void CreateConnectorRequest();
+  void OnIsReadyToComposite(
+      int frame_id,
+      uint32_t page_num,
+      base::SharedMemoryHandle handle,
+      uint32_t data_size,
+      std::vector<uint32_t> subframe_uids,
+      mojom::PdfCompositor::CompositeToPdfCallback callback,
+      bool status);
 
   service_manager::mojom::ConnectorRequest connector_request_;
   std::unique_ptr<service_manager::Connector> connector_;
   bool for_preview_;
+
+  // All the subframes that have been requested for printing.
+  std::set<int> pending_subframe_ids_;
+
+  base::WeakPtrFactory<PrintCompositeClient> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(PrintCompositeClient);
 };
