07b24c601cfc010d693b174332a00a3d73ba16f3
Amirhossein Simjour <asimjour@chromium.org>
2017-11-14 15:47:09 -0500

    NOT FOR REVIEW
    Permission dialog in VR - Using View to texture
    
    Bug:
    Change-Id: I9d501a90594c71fb241e019d3473dbe8c24591e4

--- chrome/browser/vr/browser_ui_interface.h
+++ chrome/browser/vr/browser_ui_interface.h
@@ -5,6 +5,7 @@
 #ifndef CHROME_BROWSER_VR_BROWSER_UI_INTERFACE_H_
 #define CHROME_BROWSER_VR_BROWSER_UI_INTERFACE_H_
 
+#include "chrome/browser/vr/content_input_delegate.h"
 #include "chrome/browser/vr/ui_unsupported_mode.h"
 #include "components/security_state/core/security_state.h"
 
@@ -35,6 +36,8 @@ class BrowserUiInterface {
                                       UiUnsupportedMode reason) = 0;
   virtual void SetSpeechRecognitionEnabled(bool enabled) = 0;
   virtual void OnSpeechRecognitionStateChanged(int new_state) = 0;
+  virtual void SetAlertDialogEnabled(bool enabled,
+                                     ContentInputDelegate* delegate) = 0;
 
   // Tab handling.
   virtual void AppendToTabList(bool incognito,
