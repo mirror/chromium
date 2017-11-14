d6c9b51d64e79b909d30950d6c592a9c5332e8b4
Lucas Garron <lgarron@chromium.org>
2017-11-14 08:42:30 -0800

    Support committing error page content from BLOCK* throttle results on start/redirect.
    Bug: 780028
    Change-Id: Idc3b18ee255163f1c8b37aaec6936f3cf441a2f5

--- content/browser/frame_host/navigation_request.h
+++ content/browser/frame_host/navigation_request.h
@@ -222,12 +222,15 @@ class CONTENT_EXPORT NavigationRequest : public NavigationURLLoaderDelegate {
   void OnRequestStarted(base::TimeTicks timestamp) override;
 
   // A version of OnRequestFailed() that allows skipping throttles, to be used
-  // when a request failed due to a throttle result itself.
-  void OnRequestFailedInternal(bool has_stale_copy_in_cache,
-                               int net_error,
-                               const base::Optional<net::SSLInfo>& ssl_info,
-                               bool should_ssl_errors_be_fatal,
-                               bool skip_throttles);
+  // when a request failed due to a throttle result itself. |error_page_content|
+  // is ignored when |skip_throttles| is false.
+  void OnRequestFailedInternal(
+      bool has_stale_copy_in_cache,
+      int net_error,
+      const base::Optional<net::SSLInfo>& ssl_info,
+      bool should_ssl_errors_be_fatal,
+      bool skip_throttles,
+      const base::Optional<std::string>& error_page_content);
 
   // Called when the NavigationThrottles have been checked by the
   // NavigationHandle.
