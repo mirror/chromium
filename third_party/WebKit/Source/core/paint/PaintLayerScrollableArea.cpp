6cb222c65cba57fcf5689fc0766a3540bae36a84
Steve Kobes <skobes@chromium.org>
2017-11-13 17:54:30 -0800

    Composited scrolling for promoted overflow:hidden.
    Bug: 784053
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I251369bef65adb2f3e1c9811d2e5a8bd0f5770e5

--- third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
@@ -1967,8 +1967,8 @@ static bool LayerNodeMayNeedCompositedScrolling(const PaintLayer* layer) {
 }
 
 bool PaintLayerScrollableArea::ComputeNeedsCompositedScrolling(
-    const bool layer_has_been_composited,
-    const PaintLayer* layer) {
+    const bool layer_has_been_composited) {
+  const PaintLayer* layer = Layer();
   non_composited_main_thread_scrolling_reasons_ = 0;
 
   // The root scroller needs composited scrolling layers even if it doesn't
@@ -1976,10 +1976,15 @@ bool PaintLayerScrollableArea::ComputeNeedsCompositedScrolling(
   // viewport. If we're in non-RootLayerScrolling mode, the root layer will be
   // the global root scroller (by default) but it doesn't actually handle
   // scrolls itself so we don't need composited scrolling for it.
-  if (RootScrollerUtil::IsGlobal(*layer) && !Layer()->IsScrolledByFrameView())
+  if (RootScrollerUtil::IsGlobal(*layer) && !layer->IsScrolledByFrameView())
     return true;
 
-  if (!layer->ScrollsOverflow())
+  if (!HasHorizontalOverflow() && !HasVerticalOverflow())
+    return false;
+
+  // For overflow:hidden (scrollable by script but not input), don't force
+  // promotion, but use composited scrolling if already promoted.
+  if (!layer->ScrollsOverflow() && !layer_has_been_composited)
     return false;
 
   if (layer->Size().IsEmpty())
@@ -2048,7 +2053,7 @@ bool PaintLayerScrollableArea::ComputeNeedsCompositedScrolling(
 void PaintLayerScrollableArea::UpdateNeedsCompositedScrolling(
     bool layer_has_been_composited) {
   const bool needs_composited_scrolling =
-      ComputeNeedsCompositedScrolling(layer_has_been_composited, Layer());
+      ComputeNeedsCompositedScrolling(layer_has_been_composited);
 
   if (static_cast<bool>(needs_composited_scrolling_) !=
       needs_composited_scrolling) {
