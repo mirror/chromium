98cef1437cc1c82f81d8df7f52993b2f242f9bcd
dpapad <dpapad@chromium.org>
2017-11-14 09:44:56 -0800

    MD Extensions: Show load path for unpacked extensions.
    Bug:686473
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I28055c385165b29426e40ddbb8e6941ffa46659c

--- chrome/browser/resources/md_extensions/item.js
+++ chrome/browser/resources/md_extensions/item.js
@@ -52,6 +52,9 @@ cr.define('extensions', function() {
 
     /** @param {!chrome.developerPrivate.ExtensionInfo} extension */
     showItemOptionsPage(extension) {}
+
+    /** @param {string} id */
+    showPath(id) {}
   }
 
   const Item = Polymer({
