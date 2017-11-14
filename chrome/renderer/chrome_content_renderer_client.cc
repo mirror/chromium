73bd9240afd5ee26e4e0d342f10dcbb5816f4c3e
xzhan96 <xiaofeng.zhang@intel.com>
2017-11-14 11:35:22 +0800

    [ServiceWorker] Introduce blink.mojom.WebPageVisibilityState enum
    This CL creates blink.mojom.WebPageVisibilityState enum type and uses it
    to replace existing native enum type blink::WebPageVisibilityState.
    
    This is the preparation to impl the blink.mojom.ServiceWorkerClientInfo,
    and finally to remove content::ExtendableMessageEventSource, which is an
    incremental step toward Onion Soup: having less dependence on content types
    for service worker.
    
    BUG=782528
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ic066d85ff5e7cc5b25b7942fadfe9d5835d17d4c

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -114,6 +114,7 @@
 #include "printing/features/features.h"
 #include "services/service_manager/public/cpp/connector.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
+#include "third_party/WebKit/common/page_visibility_state.mojom.h"
 #include "third_party/WebKit/public/platform/URLConversion.h"
 #include "third_party/WebKit/public/platform/WebCache.h"
 #include "third_party/WebKit/public/platform/WebRuntimeFeatures.h"
@@ -1367,11 +1368,11 @@ ChromeContentRendererClient::GetPrescientNetworking() {
 
 bool ChromeContentRendererClient::ShouldOverridePageVisibilityState(
     const content::RenderFrame* render_frame,
-    blink::WebPageVisibilityState* override_state) {
+    blink::mojom::PageVisibilityState* override_state) {
   if (!prerender::PrerenderHelper::IsPrerendering(render_frame))
     return false;
 
-  *override_state = blink::kWebPageVisibilityStatePrerender;
+  *override_state = blink::mojom::PageVisibilityState::kPrerender;
   return true;
 }
 
