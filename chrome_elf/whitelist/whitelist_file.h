7e1802f2e00b66935dd9a9c8cc00f36c8394fba6
Penny MacNeil <pennymac@chromium.org>
2017-11-14 14:16:00 -0800

    [chrome_elf whitelist] Add whitelist file initial support.
    Not called yet.  Plumbs in initialization from a packed whitelist file
    supplied out of band, and the exported IsModuleWhitelisted functionality.
    Also includes tests.
    
    BUG=769590
    TEST=chrome_elf_unittests.exe, WhitelistFileTest.*
    
    Cq-Include-Trybots: master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: Ib93b5af525b8bcc228664188acc83106262da900

---
+++ chrome_elf/whitelist/whitelist_file.h
@@ -0,0 +1,39 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_ELF_WHITELIST_WHITELIST_FILE_H_
+#define CHROME_ELF_WHITELIST_WHITELIST_FILE_H_
+
+#include "windows.h"
+
+#include <string>
+
+namespace whitelist {
+
+// "static_cast<int>(FileStatus::value)" to access underlying value.
+enum class FileStatus {
+  kSuccess,
+  kUserDataDirFail,
+  kInitArrayFail,
+  kNoComponentFile,
+};
+
+// Look up a binary based on the required data points.
+// - Returns true if match found in whitelist.  I.E. Allow module load.
+bool IsModuleWhitelisted(const std::string& basename,
+                         DWORD image_size,
+                         DWORD time_date_stamp);
+
+// Get the full path of the whitelist file used.
+std::wstring GetFilePathUsed();
+
+// Initialize internal whitelist from file.
+FileStatus InitFromFile();
+
+// TESTING ONLY
+void OverrideFilePathForTesting(const std::wstring& new_path);
+
+}  // namespace whitelist
+
+#endif  // CHROME_ELF_WHITELIST_WHITELIST_FILE_H_
