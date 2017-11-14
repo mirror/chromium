1f6706c3aa4a58f3f2793187f49b20dbc807b3fb
Sky Malice <skym@chromium.org>
2017-11-14 11:10:21 -0800

    Record FieldTrial user events.
    Bug: 719038
    Change-Id: Ib62a18949dde41d194935347768b3fef0d2623d6

--- components/sync/user_events/user_event_service_impl.cc
+++ components/sync/user_events/user_event_service_impl.cc
@@ -8,15 +8,70 @@
 
 #include "base/bind.h"
 #include "base/feature_list.h"
+
+#include "base/bind_helpers.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/rand_util.h"
+#include "base/stl_util.h"
+#include "base/time/time.h"
 #include "components/sync/driver/sync_driver_switches.h"
 #include "components/sync/driver/sync_service.h"
 #include "components/sync/model/model_type_sync_bridge.h"
 #include "components/sync/user_events/user_event_sync_bridge.h"
+#include "components/variations/active_field_trials.h"
 
+using base::TimeDelta;
 using sync_pb::UserEventSpecifics;
 
 namespace syncer {
+
+namespace {
+
+enum NavigationPresence {
+  kMustHave,
+  kCannotHave,
+  kEitherOkay,
+};
+
+NavigationPresence GetNavigationPresence(
+    UserEventSpecifics::EventCase event_case) {
+  switch (event_case) {
+    case UserEventSpecifics::kTestEvent:
+      return kEitherOkay;
+    case UserEventSpecifics::kFieldTrialEvent:
+      return kCannotHave;
+    case UserEventSpecifics::kLanguageDetectionEvent:
+      return kMustHave;
+    case UserEventSpecifics::kTranslationEvent:
+      return kMustHave;
+    case UserEventSpecifics::kGaiaPasswordReuseEvent:
+      return kMustHave;
+    case UserEventSpecifics::EVENT_NOT_SET:
+      NOTREACHED();
+      return kEitherOkay;
+  }
+}
+
+bool NavigationPresenceValid(UserEventSpecifics::EventCase event_case,
+                             bool has_navigation_id) {
+  NavigationPresence presence = GetNavigationPresence(event_case);
+  return presence == kEitherOkay ||
+         (presence == kMustHave && has_navigation_id) ||
+         (presence == kCannotHave && !has_navigation_id);
+}
+
+TimeDelta GetFieldTrialDelay() {
+  return TimeDelta::FromSeconds(base::GetFieldTrialParamByFeatureAsInt(
+      switches::kSyncUserFieldTrialEvents, "field_trial_delay_seconds",
+      60 * 60 * 24));
+}
+
+size_t GetFieldTrialMaxCount() {
+  return base::GetFieldTrialParamByFeatureAsInt(
+      switches::kSyncUserFieldTrialEvents, "field_trial_max_count", 5);
+}
+
+}  // namespace
 
 UserEventServiceImpl::UserEventServiceImpl(
     SyncService* sync_service,
@@ -26,19 +81,36 @@ UserEventServiceImpl::UserEventServiceImpl(
       session_id_(base::RandUint64()) {
   DCHECK(bridge_);
   DCHECK(sync_service_);
-  // TODO(skym): Subscribe to events about field trial membership changing.
+  base::FieldTrialList::AddObserver(this);
 }
 
 UserEventServiceImpl::~UserEventServiceImpl() {}
 
-void UserEventServiceImpl::Shutdown() {}
+void UserEventServiceImpl::Shutdown() {
+  base::FieldTrialList::RemoveObserver(this);
+}
 
 void UserEventServiceImpl::RecordUserEvent(
     std::unique_ptr<UserEventSpecifics> specifics) {
   if (ShouldRecordEvent(*specifics)) {
     DCHECK(!specifics->has_session_id());
     specifics->set_session_id(session_id_);
+    bool updated = false;
+
+    if (recorded_events_.insert(specifics->event_case()).second) {
+      for (const std::pair<sync_pb::UserEventSpecifics::EventCase, std::string>&
+               pair : event_trial_deps_) {
+        if (pair.first == specifics->event_case()) {
+          updated |=
+              MaybeUpdateTrialsToRecord(specifics->event_case(), pair.second);
+        }
+      }
+    }
+
     bridge_->RecordUserEvent(std::move(specifics));
+    if (updated) {
+      RecordFieldTrials();
+    }
   }
 }
 
@@ -51,6 +123,31 @@ base::WeakPtr<ModelTypeSyncBridge> UserEventServiceImpl::GetSyncBridge() {
   return bridge_->AsWeakPtr();
 }
 
+void UserEventServiceImpl::RegisterDependentFieldTrial(
+    UserEventSpecifics::EventCase event_case,
+    const std::string& trial_name) {
+  if (event_trial_deps_.insert(std::make_pair(event_case, trial_name)).second &&
+      MaybeUpdateTrialsToRecord(event_case, trial_name)) {
+    RecordFieldTrials();
+  }
+}
+
+void UserEventServiceImpl::OnFieldTrialGroupFinalized(
+    const std::string& trial_name,
+    const std::string& group_name) {
+  for (const std::pair<sync_pb::UserEventSpecifics::EventCase, std::string>&
+           pair : event_trial_deps_) {
+    if (pair.second == trial_name &&
+        MaybeUpdateTrialsToRecord(pair.first, trial_name)) {
+      RecordFieldTrials();
+      // The |trial_name| will only switch from not recorded to recorded once,
+      // which means MaybeUpdateTrialsToRecord() is not going to return again
+      // for the same |trial_name| that is held constant here. Safe to return.
+      return;
+    }
+  }
+}
+
 // static
 bool UserEventServiceImpl::MightRecordEvents(bool off_the_record,
                                              SyncService* sync_service) {
@@ -58,20 +155,71 @@ bool UserEventServiceImpl::MightRecordEvents(bool off_the_record,
          base::FeatureList::IsEnabled(switches::kSyncUserEvents);
 }
 
-bool UserEventServiceImpl::ShouldRecordEvent(
-    const UserEventSpecifics& specifics) {
-  // We only record events if the user is syncing history (as indicated by
-  // GetPreferredDataTypes()) and has not enabled a custom passphrase (as
-  // indicated by IsUsingSecondaryPassphrase()).
+bool UserEventServiceImpl::CanRecordHistory() {
+  // Before the engine is initialized, we cannot trust the other fields.
   return sync_service_->IsEngineInitialized() &&
          !sync_service_->IsUsingSecondaryPassphrase() &&
          sync_service_->GetPreferredDataTypes().Has(HISTORY_DELETE_DIRECTIVES);
 }
 
-void UserEventServiceImpl::RegisterDependentFieldTrial(
-    const std::string& trial_name,
-    UserEventSpecifics::EventCase event_case) {
-  // TODO(skym): Implementation.
+bool UserEventServiceImpl::ShouldRecordEvent(
+    const UserEventSpecifics& specifics) {
+  if (specifics.event_case() == UserEventSpecifics::EVENT_NOT_SET) {
+    return false;
+  }
+
+  if (!NavigationPresenceValid(specifics.event_case(),
+                               specifics.has_navigation_id())) {
+    return false;
+  }
+
+  if (specifics.has_navigation_id() && !CanRecordHistory()) {
+    return false;
+  }
+
+  return true;
+}
+
+bool UserEventServiceImpl::MaybeUpdateTrialsToRecord(
+    UserEventSpecifics::EventCase event_case,
+    const std::string& trial_name) {
+  // Assume the dependency between |event_case| and |trial_name| was already
+  // registered instead of checking it, because all of our callers already do
+  // this implicitly.
+  DCHECK(base::ContainsKey(event_trial_deps_,
+                           std::make_pair(event_case, trial_name)));
+
+  if (!base::FeatureList::IsEnabled(switches::kSyncUserFieldTrialEvents) ||
+      base::ContainsKey(trials_to_record_, trial_name) ||
+      !base::ContainsKey(recorded_events_, event_case) ||
+      !base::FieldTrialList::IsTrialActive(trial_name) ||
+      trials_to_record_.size() >= GetFieldTrialMaxCount()) {
+    return false;
+  }
+
+  trials_to_record_.insert(trial_name);
+  return true;
+}
+
+void UserEventServiceImpl::RecordFieldTrials() {
+  DCHECK(!trials_to_record_.empty());
+  auto specifics = std::make_unique<UserEventSpecifics>();
+  specifics->set_event_time_usec(
+      (base::Time::Now() - base::Time()).InMicroseconds());
+
+  for (std::string trial_name : trials_to_record_) {
+    variations::ActiveGroupId field_ids = variations::MakeActiveGroupId(
+        trial_name, base::FieldTrialList::FindFullName(trial_name));
+    auto* pair =
+        specifics->mutable_field_trial_event()->add_field_trial_pairs();
+    pair->set_name_id(field_ids.name);
+    pair->set_group_id(field_ids.group);
+  }
+
+  RecordUserEvent(std::move(specifics));
+  field_trial_timer_.Start(FROM_HERE, GetFieldTrialDelay(),
+                           base::Bind(&UserEventServiceImpl::RecordFieldTrials,
+                                      base::Unretained(this)));
 }
 
 }  // namespace syncer
--- components/sync/user_events/user_event_service_impl.h
+++ components/sync/user_events/user_event_service_impl.h
@@ -6,10 +6,15 @@
 #define COMPONENTS_SYNC_USER_EVENTS_USER_EVENT_SERVICE_IMPL_H_
 
 #include <memory>
+#include <set>
 #include <string>
+#include <utility>
 
+#include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/field_trial.h"
+#include "base/timer/timer.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/sync/protocol/user_event_specifics.pb.h"
 #include "components/sync/user_events/user_event_service.h"
@@ -20,7 +25,8 @@ class ModelTypeSyncBridge;
 class SyncService;
 class UserEventSyncBridge;
 
-class UserEventServiceImpl : public UserEventService {
+class UserEventServiceImpl : public UserEventService,
+                             base::FieldTrialList::Observer {
  public:
   UserEventServiceImpl(SyncService* sync_service,
                        std::unique_ptr<UserEventSyncBridge> bridge);
@@ -34,16 +40,36 @@ class UserEventServiceImpl : public UserEventService {
       std::unique_ptr<sync_pb::UserEventSpecifics> specifics) override;
   void RecordUserEvent(const sync_pb::UserEventSpecifics& specifics) override;
   void RegisterDependentFieldTrial(
-      const std::string& trial_name,
-      sync_pb::UserEventSpecifics::EventCase event_case) override;
+      sync_pb::UserEventSpecifics::EventCase event_case,
+      const std::string& trial_name) override;
   base::WeakPtr<ModelTypeSyncBridge> GetSyncBridge() override;
+
+  // base::FieldTrialList::Observer implementation.
+  void OnFieldTrialGroupFinalized(const std::string& trial_name,
+                                  const std::string& group_name) override;
 
   // Checks known (and immutable) conditions that should not change at runtime.
   static bool MightRecordEvents(bool off_the_record, SyncService* sync_service);
 
  private:
+  // Whether allowed to record events that link to navigation data.
+  bool CanRecordHistory();
+
   // Checks dynamic or event specific conditions.
   bool ShouldRecordEvent(const sync_pb::UserEventSpecifics& specifics);
+
+  // Checks if conditions for the given |event_case| and |trial_name| pair have
+  // changed such that |trial_name| should now be added to |recorded_events_|.
+  // This method will modify |recorded_events_| and return whether
+  // RecordFieldTrials() should be called. If the caller is performing multiple
+  // invocations of MaybeUpdateTrialsToRecord() they should try to batch a
+  // single RecordFieldTrials() call.
+  bool MaybeUpdateTrialsToRecord(
+      sync_pb::UserEventSpecifics::EventCase event_case,
+      const std::string& trial_name) WARN_UNUSED_RESULT;
+
+  // Construct and record a field trial event.
+  void RecordFieldTrials();
 
   SyncService* sync_service_;
 
@@ -53,6 +79,23 @@ class UserEventServiceImpl : public UserEventService {
   // restart it will be regenerated. This can be attached to events to know
   // which events came from the same session.
   uint64_t session_id_;
+
+  // Holds the relationships established through RegisterDependentFieldTrial().
+  std::set<std::pair<sync_pb::UserEventSpecifics::EventCase, std::string>>
+      event_trial_deps_;
+
+  // The trial names that should be included when we record a FieldTrial event.
+  std::set<std::string> trials_to_record_;
+
+  // All of the event types that have been asked to be record that have been
+  // sent through to actually be recorded. It's possible that we commit events
+  // that were previously stored to disk, but we do not attempt to count them
+  // here, they're from a different session and will have a different session
+  // id.
+  std::set<sync_pb::UserEventSpecifics::EventCase> recorded_events_;
+
+  // Timer used to record a field trial event every given interval.
+  base::OneShotTimer field_trial_timer_;
 
   DISALLOW_COPY_AND_ASSIGN(UserEventServiceImpl);
 };
