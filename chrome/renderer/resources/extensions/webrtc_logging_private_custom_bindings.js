244ac54bfd9adee2ae10bea368d29892670e7b7b
Adrian Elder <aelder@chromium.org>
2017-11-14 18:10:40 -0500

    Support calling webrtcLoggingPrivate from an app for a webview.
    Looks up the render process id for a <webview> in webrtcLoggingPrivate's RequestInfo.
    There are 2 ways webrtcLoggingPrivate functions can be called.
    
      1. From a whitelisted component extension on behalf of a page with the
         appropriate origin via a message from that page. In this case, the
         guestProcessId is on the message received by the component extension, and
         the extension can pass that in on the RequestInfo param.
    
      2. From a whitelisted app that hosts a page in a webview. In this case, the app
         passes the appropriate webview in on the RequestInfo param and the
         custom_binding.js layer looks up its process id.
    
    Case 1 was already supported before. This change adds support for case 2.
    
    
    Bug: 777906
    Change-Id: I02f6d011fc7a6f1589c10aff19dc6e6e8f2e24a8

--- chrome/renderer/resources/extensions/webrtc_logging_private_custom_bindings.js
+++ chrome/renderer/resources/extensions/webrtc_logging_private_custom_bindings.js
@@ -12,6 +12,54 @@ var getBindDirectoryEntryCallback =
 
 binding.registerCustomHook(function(binding, id, contextType) {
   var apiFunctions = binding.apiFunctions;
+
+  // There are 2 ways these APIs get called.
+  //  1. From a whitelisted component extension on behalf page with the
+  //     appropriate origin via a message from that page . In this case,
+  //     the guestProcessId is on the message received by the component
+  //     extension, and the extension can pass that in on the RequestInfo param.
+  //  2. From a whitelisted app that hosts a page in a webview. In this case,
+  //     the app passes the appropriate webview in on the RequestInfo param.
+  //
+  // The following RequestInfo processing is done to accommodate case 2.
+  function populateGuestProcessIdFromWebview (functionName) {
+    apiFunctions.setUpdateArgumentsPostValidate(
+        functionName, function () {
+          var args = $Array.slice(arguments);
+          var requestInfo = args[0];
+          var webview = requestInfo.webview;
+          if (webview) {
+            if (requestInfo.guestProcessId != null) {
+              throw new Error('Cannot specify guestProcessId and webview');
+            }
+            if (!webview.tagName || webview.tagName != 'WEBVIEW') {
+              throw new Error(
+                  '<webview> element expected for RequestInfo.webview');
+            }
+            var webViewImpl = privates(webview).internal;
+            requestInfo.guestProcessId= webViewImpl.processId;
+            delete requestInfo.webview;
+          }
+          return args;
+        });
+  }
+
+  $Array.forEach([
+    'setMetaData',
+    'start',
+    'setUploadOnRenderClose',
+    'stop',
+    'store',
+    'uploadStored',
+    'upload',
+    'discard',
+    'startRtpDump',
+    'stopRtpDump',
+    'startAudioDebugRecordings',
+    'stopAudioDebugRecordings',
+    'startWebRtcEventLogging',
+    'stopWebRtcEventLogging',
+  ], populateGuestProcessIdFromWebview);
   apiFunctions.setCustomCallback('getLogsDirectory',
                                  getBindDirectoryEntryCallback());
 });
