e22b660df630ad0321fd578af0ec9c003599dd63
Qiang Xu <warx@chromium.org>
2017-11-14 09:22:48 -0800

    mash: promote Is{Set}MonoAudioEnabled to ash
    Bug: 767731
    Change-Id: Ie1803f9e6a783b7bbd76305d91c5f81dcf83f916

--- chrome/browser/chromeos/preferences.cc
+++ chrome/browser/chromeos/preferences.cc
@@ -146,8 +146,6 @@ void Preferences::RegisterPrefs(PrefRegistrySimple* registry) {
   // TODO(jamescook): Move ownership and registration into ash.
   registry->RegisterBooleanPref(
       ash::prefs::kAccessibilityVirtualKeyboardEnabled, false);
-  registry->RegisterBooleanPref(ash::prefs::kAccessibilityMonoAudioEnabled,
-                                false);
   registry->RegisterStringPref(prefs::kLogoutStartedLast, std::string());
   registry->RegisterStringPref(prefs::kSigninScreenTimezone, std::string());
   registry->RegisterBooleanPref(prefs::kResolveDeviceTimezoneByGeolocation,
@@ -247,7 +245,7 @@ void Preferences::RegisterProfilePrefs(
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   registry->RegisterBooleanPref(
       ash::prefs::kAccessibilityMonoAudioEnabled, false,
-      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
+      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF | PrefRegistry::PUBLIC);
   registry->RegisterBooleanPref(
       ash::prefs::kAccessibilityCaretHighlightEnabled, false,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
