f7cc2f4f0f4db9bd621a02b7924464bb77968314
Evan Stade <estade@chromium.org>
2017-11-13 21:02:51 -0700

    Revert "Revert "Message center cleanup: rename some classes to better reflect roles.""
    This reverts commit 2465a5493d38ca9b3376240bcefd6325e112bf3e.
    
    Bug:
    Change-Id: I9dc2790563bd92b4c46bb27fd0c0f84fe4650550

--- chrome/test/base/browser_with_test_window_test.h
+++ chrome/test/base/browser_with_test_window_test.h
@@ -51,6 +51,8 @@ namespace content {
 class NavigationController;
 }
 
+class TestingProfileManager;
+
 // Base class for browser based unit tests. BrowserWithTestWindowTest creates a
 // Browser with a TestingProfile and TestBrowserWindow. To add a tab use
 // AddTab. For example, the following adds a tab and navigates to
@@ -102,6 +104,8 @@ class BrowserWithTestWindowTest : public testing::Test {
   TestingProfile* profile() const { return profile_; }
 
   TestingProfile* GetProfile() { return profile_; }
+
+  TestingProfileManager* profile_manager() { return profile_manager_.get(); }
 
   BrowserWindow* release_browser_window() WARN_UNUSED_RESULT {
     return window_.release();
@@ -179,6 +183,8 @@ class BrowserWithTestWindowTest : public testing::Test {
   // |DestroyProfile()| function - which can be overwritten by derived testing
   // frameworks.
   TestingProfile* profile_;
+
+  std::unique_ptr<TestingProfileManager> profile_manager_;
   std::unique_ptr<BrowserWindow> window_;  // Usually a TestBrowserWindow.
   std::unique_ptr<Browser> browser_;
 
