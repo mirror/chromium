733d23dbf88ec10f511f61214fa625ffb1237710
Gary Kacmarcik <garykac@chromium.org>
2017-11-14 15:59:12 -0800

    Add clipboard permission type and context
    This cl adds the basic Clipboard PermissionType and the PermissionContext.
    
    UX for this permission (which will be behind a feature flag) will be added in a follow-up cl.
    
    Bug: 677564
    Change-Id: Ia5909c8f1feeedad5471db87630154071d7ba8ac

--- chrome/browser/permissions/permission_manager.cc
+++ chrome/browser/permissions/permission_manager.cc
@@ -12,6 +12,8 @@
 #include "build/build_config.h"
 #include "chrome/browser/accessibility/accessibility_permission_context.h"
 #include "chrome/browser/background_sync/background_sync_permission_context.h"
+#include "chrome/browser/clipboard/clipboard_read_permission_context.h"
+#include "chrome/browser/clipboard/clipboard_write_permission_context.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/generic_sensor/sensor_permission_context.h"
 #include "chrome/browser/media/midi_permission_context.h"
@@ -112,6 +114,10 @@ ContentSettingsType PermissionTypeToContentSetting(PermissionType permission) {
       return CONTENT_SETTINGS_TYPE_SENSORS;
     case PermissionType::ACCESSIBILITY_EVENTS:
       return CONTENT_SETTINGS_TYPE_ACCESSIBILITY_EVENTS;
+    case PermissionType::CLIPBOARD_READ:
+      return CONTENT_SETTINGS_TYPE_CLIPBOARD_READ;
+    case PermissionType::CLIPBOARD_WRITE:
+      return CONTENT_SETTINGS_TYPE_CLIPBOARD_WRITE;
     case PermissionType::NUM:
       // This will hit the NOTREACHED below.
       break;
@@ -296,6 +302,10 @@ PermissionManager::PermissionManager(Profile* profile)
       base::MakeUnique<SensorPermissionContext>(profile);
   permission_contexts_[CONTENT_SETTINGS_TYPE_ACCESSIBILITY_EVENTS] =
       base::MakeUnique<AccessibilityPermissionContext>(profile);
+  permission_contexts_[CONTENT_SETTINGS_TYPE_CLIPBOARD_READ] =
+      base::MakeUnique<ClipboardReadPermissionContext>(profile);
+  permission_contexts_[CONTENT_SETTINGS_TYPE_CLIPBOARD_WRITE] =
+      base::MakeUnique<ClipboardWritePermissionContext>(profile);
 }
 
 PermissionManager::~PermissionManager() {
