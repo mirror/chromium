45040d238b923f8851cf2a77085d5e35c0e10822
Gabriel Charette <gab@chromium.org>
2017-11-14 12:33:20 -0500

    Atomic EnqueueOrder with custom overflow support on 32-bit platforms.
    Here's how this could be done by using the same amount of state as
    before (i.e. one "next_" int in the generator and EnqueueOrders are mere
    integers as well).
    
    Machine word sized integers means overflow is only a concern on 32-bit.
    
    Overflow is managed with the assumption that each task enqueued is
    always at least processed before one billion (2^30) more tasks are
    enqueued. Comparison then supports overflow by considering
    EnqueueOrders in the 3B-4B range unconditonally smaller than those in
    the 0B-1B range (more details in CL comments).
    
    Bug: 783309
    Change-Id: I5bfaf7ddbf304a9730dd012ac0641f4635b9e8c5

--- third_party/WebKit/Source/platform/scheduler/base/enqueue_order.h
+++ third_party/WebKit/Source/platform/scheduler/base/enqueue_order.h
@@ -7,42 +7,124 @@
 
 #include <stdint.h>
 
+#include <type_traits>
+
+#include "base/atomicops.h"
+#include "base/macros.h"
 #include "base/synchronization/lock.h"
+#include "build/build_config.h"
 
 namespace blink {
 namespace scheduler {
 namespace internal {
 
-using EnqueueOrder = uint64_t;
+// An atomic machine word is used to generate indices.
+using EnqueueOrderGeneratorType = base::subtle::AtomicWord;
 
+// Each index is stored as an unsigned representation of the generated word. The
+// space of indices in a word is assumed to be infinite in practice, except on
+// 32-bit platforms where a few tricks are used in operator<() and
+// GenerateNext().
+using EnqueueOrderValueType =
+    typename std::make_unsigned<EnqueueOrderGeneratorType>::type;
+
+// Special EnqueueOrder values.
 // TODO(scheduler-dev): Remove explicit casts when c++17 comes.
-enum class EnqueueOrderValues : EnqueueOrder {
+enum class EnqueueOrderValues : EnqueueOrderValueType {
   // Invalid EnqueueOrder.
   NONE = 0,
 
-  // Earliest possible EnqueueOrder, to be used for fence blocking.
+  // Earliest possible EnqueueOrder, to be used for fence blocking  (ref.
+  // InsertFencePosition::BEGINNING_OF_TIME).
   BLOCKING_FENCE = 1,
-  FIRST = 2,
+  // More special values can be added above, this entry must remain last.
+  FIRST
 };
 
-// A 64bit integer used to provide ordering of tasks. NOTE The scheduler assumes
-// these values will not overflow.
+#if defined(ARCH_CPU_32_BITS)
+// operator<()'s overflow logic is based on the assumption that any enqueued
+// task is at least processed before one billion (2^30) more tasks are enqueued.
+// operator<() views the 2^32 space as 4 quadrants of 2^30 indices. The
+// comparison then enforces that for two EnqueueOrders in different quadrants,
+// the EnqueueOrder in the quadrant preceding the other is always considered
+// smaller. Since any enqueued task is at least processed before its quadrant
+// and the next have been fully used (assumption #1), the comparison operator is
+// guaranteed to never see numbers more than a single quadrant apart and
+// operator<() can hence be done without further state.
+static_assert(sizeof(EnqueueOrderValueType) == 4,
+              "A word on 32bit platform is assumed to be 4 bytes and therefore "
+              "require overflow support in this class.");
+constexpr EnqueueOrderValueType kBitsBeforeQuadrant =
+    8 * sizeof(EnqueueOrderValueType) - 2;
+#endif  // defined(ARCH_CPU_32_BITS)
+
+// An EnqueueOrder is an unsigned word-sized int with custom comparison
+// operators (see above).
+struct EnqueueOrder {
+  EnqueueOrder(EnqueueOrderValueType value) : value_(value) {}
+
+  bool operator<(const EnqueueOrder& other) const {
+#if defined(ARCH_CPU_32_BITS)
+    if (value_ < static_cast<EnqueueOrder>(EnqueueOrderValues::FIRST) ||
+        other_.value_ < static_cast<EnqueueOrder>(EnqueueOrderValues::FIRST)) {
+      // Special values always come first.
+      return value_ < other_.value_;
+    }
+
+    const int this_quadrant = value_ >> kBitsBeforeQuadrant;
+    const int other_quadrant = other_.value_ >> kBitsBeforeQuadrant;
+
+    // Simple case: same quadrant, compare values directly.
+    if (this_quadrant == other_quadrant)
+      return value_ < other_.value_;
+
+    // As explained above, this quadrant (and any value within it) is considered
+    // before the other when it immediately precedes it in the circular index
+    // generation.
+    const bool this_quadrant_precedes_other =
+        (other_quadrant - this_quadrant) % 4 == 1;
+
+    // As explained above, this algorithm relies on operator<() never being
+    // called with indices that are more than one quadrant apart.
+    DCHECK(this_quadrant_precedes_other ||
+           (this_quadrant - other_quadrant) % 4 == 1)
+
+    return this_quadrant_precedes_other;
+#else   // defined(ARCH_CPU_32_BITS)
+    return value_ < other_.value_;
+#endif  // defined(ARCH_CPU_32_BITS)
+  }
+
+  bool operator>(const EnqueueOrder& other) const { return other < *this; }
+
+  // This value is initialized from an atomic number but it's kept const
+  // thereafter and can hence be read without atomic loads.
+  EnqueueOrderValueType value_;
+};
+
+// An integer used to provide ordering of tasks. The scheduler assumes these
+// values will not overflow on 64-bit, special logic is used to support overflow
+// without using a lock on 32-bit. This overflow logic is based on the
+// assumption that every task is at least processed before one billion (2^30)
+// more tasks are enqueued.
 class EnqueueOrderGenerator {
  public:
-  EnqueueOrderGenerator();
-  ~EnqueueOrderGenerator();
+  EnqueueOrderGenerator() = default;
+  ~EnqueueOrderGenerator() = default;
 
   // Returns a monotonically increasing integer, starting from one. Can be
   // called from any thread.
   EnqueueOrder GenerateNext();
 
   static bool IsValidEnqueueOrder(EnqueueOrder enqueue_order) {
-    return enqueue_order != 0ull;
+    return enqueue_order != static_cast<EnqueueOrder>(EnqueueOrderValues::NONE);
   }
 
  private:
-  base::Lock lock_;
-  EnqueueOrder enqueue_order_;
+  EnqueueOrderGeneratorType next_ =
+      static_cast<EnqueueOrder>(EnqueueOrderValues::FIRST);
+
+  DISALLOW_COPY_AND_ASSIGN(EnqueueOrderGenerator);
 };
 
 }  // namespace internal
