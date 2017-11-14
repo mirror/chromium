98d6417f55ce0daee466bf6780559305f52b9ec6
Andreas Haas <ahaas@chromium.org>
2017-11-14 11:23:32 +0100

    [gin] Implement v8::TaskRunners
    With this CL the gin platform implements the new v8::Platform API
    functions {GetForegroundTaskRunner} and {GetBackgroundTaskRunner}.
    
    The existing task posting function of the V8Platform now also use these
    taskrunners to post tasks. Thereby we can move some of the
    implementation from the V8Platform and the PerIsolateData to the new
    taskrunners.
    
    R=rmcilroy@chromium.org
    
    Change-Id: I27ea6dd5f6ef8409e394f23ad914f468c09ded4a

--- gin/per_isolate_data.cc
+++ gin/per_isolate_data.cc
@@ -8,7 +8,6 @@
 
 #include "base/logging.h"
 #include "base/single_thread_task_runner.h"
-#include "base/threading/thread_task_runner_handle.h"
 #include "gin/public/gin_embedders.h"
 
 using v8::ArrayBuffer;
@@ -26,12 +25,15 @@ PerIsolateData::PerIsolateData(
     ArrayBuffer::Allocator* allocator,
     IsolateHolder::AccessMode access_mode,
     scoped_refptr<base::SingleThreadTaskRunner> task_runner)
-    : isolate_(isolate),
-      allocator_(allocator),
-      access_mode_(access_mode),
-      task_runner_(
-          task_runner ? task_runner : base::ThreadTaskRunnerHandle::Get()) {
+    : isolate_(isolate), allocator_(allocator) {
   isolate_->SetData(kEmbedderNativeGin, this);
+
+  if (access_mode == IsolateHolder::kUseLocker) {
+    task_runner_ = std::make_shared<V8ForegroundTaskRunnerWithLocker>(
+        isolate, task_runner);
+  } else {
+    task_runner_ = std::make_shared<V8ForegroundTaskRunner>(task_runner);
+  }
 }
 
 PerIsolateData::~PerIsolateData() {
@@ -120,7 +122,7 @@ NamedPropertyInterceptor* PerIsolateData::GetNamedPropertyInterceptor(
 
 void PerIsolateData::EnableIdleTasks(
     std::unique_ptr<V8IdleTaskRunner> idle_task_runner) {
-  idle_task_runner_ = std::move(idle_task_runner);
+  task_runner_->EnableIdleTasks(std::move(idle_task_runner));
 }
 
 }  // namespace gin
