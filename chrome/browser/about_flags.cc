a8398aee23b1742a11767fef19095367d377b465
Jan Krcal <jkrcal@chromium.org>
2017-11-14 19:40:24 +0100

    [Ntp Tiles] Remove an experiment NTPTilesLowerResolutionFavicons
    This CL removes an experimental feature and a corresponding flag from
    chrome://flags. The removed feature was not needed any more because
    the behaviour is now enabled by default if ChromeHome is enabled.
    
    Bug: 752130
    Change-Id: I6b51a14ce7560cb16b54f5e6b6ba71e0f87f97d3

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -2469,10 +2469,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnableNtpMostLikelyFaviconsFromServerDescription,
      kOsAndroid,
      FEATURE_VALUE_TYPE(ntp_tiles::kNtpMostLikelyFaviconsFromServerFeature)},
-    {"enable-ntp-tiles-lower-resolution-favicons",
-     flag_descriptions::kNtpTilesLowerResolutionFaviconsName,
-     flag_descriptions::kNtpTilesLowerResolutionFaviconsDescription, kOsAndroid,
-     FEATURE_VALUE_TYPE(ntp_tiles::kLowerResolutionFaviconsFeature)},
     {"enable-content-suggestions-thumbnail-dominant-color",
      flag_descriptions::kEnableContentSuggestionsThumbnailDominantColorName,
      flag_descriptions::
