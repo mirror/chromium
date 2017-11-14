d2d87c7db9ffb5fb7a6ef563fa951c7920839aeb
Wei Li <weili@chromium.org>
2017-11-14 12:00:51 -0800

    IPC message changes to support pdf composition
    Add new IPC messages and change a couple existing ones to allow
    subframe printing for OOPIFs and return relevant information.
    
    There are two major changes:
    -- Print subframe content and return it;
    -- For existing printing calls, return its subframe info as well.
    
    BUG=455764
    
    Change-Id: I41dd21f494b8c393ec40636498d053660c336c27

--- components/printing/renderer/print_render_frame_helper.h
+++ components/printing/renderer/print_render_frame_helper.h
@@ -26,6 +26,7 @@
 
 struct PrintMsg_Print_Params;
 struct PrintMsg_PrintPages_Params;
+struct PrintHostMsg_DidPrintContent_Params;
 struct PrintHostMsg_SetOptionsFromDocument_Params;
 
 // RenderViewTest-based tests crash on Android
@@ -201,6 +202,11 @@ class PrintRenderFrameHelper
   void OnPrintPreview(const base::DictionaryValue& settings);
   void OnClosePrintPreviewDialog();
 #endif  // BUILDFLAG(ENABLE_PRINT_PREVIEW)
+#if BUILDFLAG(ENABLE_PRINTING)
+  void OnPrintFrameContent(uint32_t rfh_id,
+                           const gfx::Rect& rect,
+                           uint32_t uid);
+#endif
   void OnPrintingDone(bool success);
 
   // Get |page_size| and |content_area| information from
@@ -337,7 +343,7 @@ class PrintRenderFrameHelper
   // Helper methods -----------------------------------------------------------
 
   bool CopyMetafileDataToSharedMem(const PdfMetafileSkia& metafile,
-                                   base::SharedMemoryHandle* shared_mem_handle);
+                                   PrintHostMsg_DidPrintContent_Params* params);
 
   // Helper method to get page layout in points and fit to page if needed.
   static void ComputePageLayoutInPointsForCss(
