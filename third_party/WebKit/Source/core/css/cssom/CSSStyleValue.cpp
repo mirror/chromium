04a9fdc686cc3b6340df8b1e40855a3a4c91744a
Darren Shen <shend@chromium.org>
2017-11-15 11:46:11 +1100

    [css-typed-om] Allow StylePropertyMaps to take string arguments.
    Currently StylePropertyMaps don't support strings e.g.
    
      styleMap.set('width', CSS.px(1)) works, but
      styleMap.set('width', '1px') doesn't.
    
    This patch adds string support by moving parsing code to
    StyleValueFactory so that both CSSStyleValue.parse and
    StylePropertyMaps can share the parsing logic.
    
    We also do some refactoring to make handling both CSSStyleValue
    and strings easier.
    
    There are still some code paths that we're not sure is correct,
    pending clarification on GitHub.
    
    Spec: https://drafts.css-houdini.org/css-typed-om-1/#the-stylepropertymap
    
    Bug: 545318
    Change-Id: Iaef3cfad69789fc115b7e98a296eec5cb4480cd8

--- third_party/WebKit/Source/core/css/cssom/CSSStyleValue.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSStyleValue.cpp
@@ -9,7 +9,7 @@
 #include "bindings/core/v8/ToV8ForCore.h"
 #include "core/StylePropertyShorthand.h"
 #include "core/css/cssom/StyleValueFactory.h"
-#include "core/css/parser/CSSParser.h"
+#include "core/css/properties/CSSProperty.h"
 
 namespace blink {
 
@@ -32,22 +32,7 @@ CSSStyleValueVector ParseCSSStyleValue(const String& property_name,
     return CSSStyleValueVector();
   }
 
-  // TODO(crbug.com/783031): This should probably use an existing parser context
-  // (e.g. from execution context) to parse relative URLs correctly.
-  const CSSValue* css_value =
-      CSSParser::ParseSingleValue(property_id, value, StrictCSSParserContext());
-  if (!css_value) {
-    exception_state.ThrowDOMException(
-        kSyntaxError, "The value provided ('" + value +
-                          "') could not be parsed as a '" + property_name +
-                          "'.");
-    return CSSStyleValueVector();
-  }
-
-  CSSStyleValueVector style_value_vector =
-      StyleValueFactory::CssValueToStyleValueVector(property_id, *css_value);
-  DCHECK(!style_value_vector.IsEmpty());
-  return style_value_vector;
+  return StyleValueFactory::FromString(property_id, value);
 }
 
 }  // namespace
