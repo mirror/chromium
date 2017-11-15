f3bbf3d5b14e7a6c4281820ec440e0702948a162
Daniel Park <danielpark@chromium.org>
2017-11-14 17:14:27 -0800

    [Chrome Home Survey] Improve Infobar triggering logic
    Uses a handler to start a timer for 5 seconds before
      logging an infobar as displayed
    
    Stores in shared preferences if infobar was displayed & closed.
    Checks with shared preferences before showing infobar.
    
    Adds animation listener to track frontmost infobar.
    
    Handles whether to log as displayed in cases where
    - Survey Infobar was dismissed
    - Survey Infobar was ignored (for threshold time)
    - Tab containing survey infobar was hidden before
      threshold was reached
    - Survey Infobar is hidden behind another infobar
    
    Bug:772081
    Change-Id: I16c8904b89820cf186919d8ba1d8d4eebcff0648

--- chrome/android/java/src/org/chromium/chrome/browser/survey/ChromeHomeSurveyController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/survey/ChromeHomeSurveyController.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.survey;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.annotation.VisibleForTesting;
 import android.text.TextUtils;
 
@@ -15,37 +16,45 @@ import org.chromium.base.StrictModeContext;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeSwitches;
+import org.chromium.chrome.browser.infobar.InfoBarContainer;
+import org.chromium.chrome.browser.infobar.InfoBarContainerLayout.Item;
+import org.chromium.chrome.browser.infobar.InfoBarIdentifier;
 import org.chromium.chrome.browser.infobar.SurveyInfoBar;
 import org.chromium.chrome.browser.infobar.SurveyInfoBarDelegate;
 import org.chromium.chrome.browser.preferences.ChromePreferenceManager;
 import org.chromium.chrome.browser.preferences.privacy.PrivacyPreferencesManager;
+import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
-import org.chromium.chrome.browser.tabmodel.EmptyTabModelObserver;
-import org.chromium.chrome.browser.tabmodel.TabModel;
-import org.chromium.chrome.browser.tabmodel.TabModel.TabSelectionType;
+import org.chromium.chrome.browser.tab.TabObserver;
+import org.chromium.chrome.browser.tabmodel.EmptyTabModelSelectorObserver;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
+import org.chromium.chrome.browser.tabmodel.TabModelSelectorObserver;
 import org.chromium.chrome.browser.util.AccessibilityUtil;
 import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.components.variations.VariationsAssociatedData;
 import org.chromium.content_public.browser.WebContents;
-import org.chromium.content_public.browser.WebContentsObserver;
 
 /**
  * Class that controls if and when to show surveys related to the Chrome Home experiment.
  */
-public class ChromeHomeSurveyController {
+public class ChromeHomeSurveyController implements InfoBarContainer.InfoBarAnimationListener {
     public static final String SURVEY_INFO_BAR_DISPLAYED_KEY =
             "chrome_home_survey_info_bar_displayed";
     public static final String PARAM_NAME = "survey_override_site_id";
 
     private static final String TRIAL_NAME = "ChromeHome";
+    private static final long REQUIRED_VISIBILITY_DURATION_MS = 5000;
 
     static final long ONE_WEEK_IN_MILLIS = 604800000L;
     static final String SURVEY_INFOBAR_DISMISSED_KEY = "chrome_home_survey_info_bar_dismissed";
 
     private TabModelSelector mTabModelSelector;
+    private Handler mLoggingHandler;
+    private Tab mSurveyInfoBarTab;
+    private TabModelSelectorObserver mTabModelObserver;
 
-    private ChromeHomeSurveyController() {
+    @VisibleForTesting
+    ChromeHomeSurveyController() {
         // Empty constructor.
     }
 
@@ -59,9 +68,16 @@ public class ChromeHomeSurveyController {
         new ChromeHomeSurveyController().startDownload(context, tabModelSelector);
     }
 
+    /**
+     * Downloads the survey if the user qualifies.
+     * @param context The current Android {@link Context}.
+     * @param tabModelSelector The tab model selector to access the tab on which the survey will be
+     *                         shown.
+     */
     private void startDownload(Context context, TabModelSelector tabModelSelector) {
         if (!doesUserQualifyForSurvey()) return;
 
+        mLoggingHandler = new Handler();
         mTabModelSelector = tabModelSelector;
 
         SurveyController surveyController = SurveyController.getInstance();
@@ -84,6 +100,7 @@ public class ChromeHomeSurveyController {
         surveyController.downloadSurvey(context, siteId, onSuccessRunnable);
     }
 
+    /** @return Whether the user qualifies for the survey. */
     private boolean doesUserQualifyForSurvey() {
         if (CommandLine.getInstance().hasSwitch(ChromeSwitches.CHROME_HOME_FORCE_ENABLE_SURVEY)) {
             return true;
@@ -95,41 +112,87 @@ public class ChromeHomeSurveyController {
         return wasChromeHomeEnabledForMinimumOneWeek();
     }
 
-    private void onSurveyAvailable(String siteId) {
-        Tab tab = mTabModelSelector.getCurrentTab();
-        if (isValidTabForSurvey(tab)) {
-            showSurveyInfoBar(tab, siteId);
-            return;
+    /**
+     * Called when the survey has finished downloading to show the survey info bar.
+     * @param siteId The site id of the survey to display.
+     */
+    @VisibleForTesting
+    void onSurveyAvailable(String siteId) {
+        mSurveyInfoBarTab = mTabModelSelector.getCurrentTab();
+        if (isValidTabForSurvey(mSurveyInfoBarTab) && mSurveyInfoBarTab.isUserInteractable()) {
+            WebContents webContents = mSurveyInfoBarTab.getWebContents();
+            if (!webContents.isLoading()) {
+                showSurveyInfoBar(mSurveyInfoBarTab, siteId);
+                return;
+            }
+            mSurveyInfoBarTab.addObserver(getTabObserver(mSurveyInfoBarTab, siteId));
         }
+        mTabModelObserver = getTabModelSelectorObserver(siteId);
+        mTabModelSelector.addObserver(mTabModelObserver);
+    }
 
-        TabModel normalModel = mTabModelSelector.getModel(false);
-        normalModel.addObserver(new EmptyTabModelObserver() {
+    /**
+     * Shows the survey info bar.
+     * @param tab The tab to attach the survey info bar.
+     * @param siteId The site id of the survey to display.
+     */
+    @VisibleForTesting
+    void showSurveyInfoBar(Tab tab, String siteId) {
+        mSurveyInfoBarTab = tab;
+        tab.getInfoBarContainer().addAnimationListener(this);
+        SurveyInfoBar.showSurveyInfoBar(tab.getWebContents(), siteId, true,
+                R.drawable.chrome_sync_logo, getSurveyInfoBarDelegate());
+    }
+
+    /** @return The observer that adds a tab observer to the current tab. */
+    private TabModelSelectorObserver getTabModelSelectorObserver(String siteId) {
+        return new EmptyTabModelSelectorObserver() {
             @Override
-            public void didSelectTab(Tab tab, TabSelectionType type, int lastId) {
-                if (isValidTabForSurvey(tab)) {
+            public void onChange() {
+                if (!isValidTabForSurvey(mTabModelSelector.getCurrentTab())) return;
+                Tab tab = mTabModelSelector.getCurrentTab();
+                tab.addObserver(getTabObserver(tab, siteId));
+            }
+        };
+    }
+
+    /** @return The observer that handles cases where the user switches tabs before an infobar is
+     * shown. */
+    @VisibleForTesting
+    TabObserver getTabObserver(Tab tab, String siteId) {
+        return new EmptyTabObserver() {
+            @Override
+            public void onInteractabilityChanged(boolean isInteractable) {
+                if (!isInteractable) {
+                    return;
+                }
+                WebContents webContents = tab.getWebContents();
+                if (!webContents.isLoading()) {
                     showSurveyInfoBar(tab, siteId);
-                    normalModel.removeObserver(this);
+                    mTabModelSelector.removeObserver(mTabModelObserver);
+                    tab.removeObserver(this);
+                    return;
                 }
             }
-        });
-    }
 
-    private void showSurveyInfoBar(Tab tab, String siteId) {
-        WebContents webContents = tab.getWebContents();
-        if (webContents.isLoading()) {
-            webContents.addObserver(new WebContentsObserver() {
-                @Override
-                public void didFinishLoad(long frameId, String validatedUrl, boolean isMainFrame) {
-                    if (!isMainFrame) return;
-                    showSurveyInfoBar(webContents, siteId);
-                    webContents.removeObserver(this);
+            @Override
+            public void onPageLoadFinished(Tab tab) {
+                if (!tab.isUserInteractable()) {
+                    return;
                 }
-            });
-        } else {
-            showSurveyInfoBar(webContents, siteId);
-        }
+                showSurveyInfoBar(tab, siteId);
+                mTabModelSelector.removeObserver(mTabModelObserver);
+                tab.removeObserver(this);
+            }
+
+            @Override
+            public void onHidden(Tab tab) {
+                tab.removeObserver(this);
+            }
+        };
     }
 
+    /** @return Whether the user has consented to reporting usage metrics and crash dumps. */
     private boolean isUMAEnabled() {
         try (StrictModeContext unused = StrictModeContext.allowDiskReads()) {
             return PrivacyPreferencesManager.getInstance()
@@ -137,13 +200,7 @@ public class ChromeHomeSurveyController {
         }
     }
 
-    private void showSurveyInfoBar(WebContents webContents, String siteId) {
-        SurveyInfoBar.showSurveyInfoBar(
-                webContents, siteId, true, R.drawable.chrome_sync_logo, getSurveyInfoBarDelegate());
-        SharedPreferences sharedPreferences = ContextUtils.getAppSharedPreferences();
-        sharedPreferences.edit().putBoolean(SURVEY_INFO_BAR_DISPLAYED_KEY, true).apply();
-    }
-
+    /** @return If the survey info bar was logged as seen before. */
     @VisibleForTesting
     boolean hasInfoBarBeenDisplayed() {
         try (StrictModeContext unused = StrictModeContext.allowDiskReads()) {
@@ -152,6 +209,7 @@ public class ChromeHomeSurveyController {
         }
     }
 
+    /** @return If it has been over a week since ChromeHome was enabled. */
     @VisibleForTesting
     boolean wasChromeHomeEnabledForMinimumOneWeek() {
         try (StrictModeContext unused = StrictModeContext.allowDiskReads()) {
@@ -163,14 +221,37 @@ public class ChromeHomeSurveyController {
         return false;
     }
 
+    /**
+     * Checks if the tab is valid for a survey (i.e. not null, no null webcontents & not incognito).
+     * @param tab The tab to be checked.
+     * @return Whether or not the tab is valid.
+     */
     @VisibleForTesting
     boolean isValidTabForSurvey(Tab tab) {
         return tab != null && tab.getWebContents() != null && !tab.isIncognito();
     }
 
+    /** @return A new {@link ChromeHomeSurveyController} for testing. */
     @VisibleForTesting
     public static ChromeHomeSurveyController createChromeHomeSurveyControllerForTests() {
         return new ChromeHomeSurveyController();
+    }
+
+    @Override
+    public void notifyAnimationFinished(int animationType) {}
+
+    @Override
+    public void notifyAllAnimationsFinished(Item frontInfoBar) {
+        if (frontInfoBar == null) return;
+
+        mLoggingHandler.removeCallbacksAndMessages(null);
+
+        // If the survey info bar is in front, start the countdown to log that it was displayed.
+        if (frontInfoBar.getInfoBarIdentifier() != InfoBarIdentifier.SURVEY_INFOBAR_ANDROID) {
+            return;
+        }
+        mLoggingHandler.postDelayed(
+                () -> recordInfoBarDisplayed(), REQUIRED_VISIBILITY_DURATION_MS);
     }
 
     /**
@@ -181,8 +262,7 @@ public class ChromeHomeSurveyController {
 
             @Override
             public void onSurveyInfoBarClosed() {
-                SharedPreferences sharedPreferences = ContextUtils.getAppSharedPreferences();
-                sharedPreferences.edit().putBoolean(SURVEY_INFOBAR_DISMISSED_KEY, true).apply();
+                recordInfoBarDisplayed();
             }
 
             @Override
@@ -196,5 +276,19 @@ public class ChromeHomeSurveyController {
                         R.string.chrome_home_survey_prompt);
             }
         };
+    }
+
+    /** Logs in {@link SharedPreferences} that the info bar was displayed. */
+    private void recordInfoBarDisplayed() {
+        if (!mSurveyInfoBarTab.isUserInteractable()) return;
+        SharedPreferences sharedPreferences = ContextUtils.getAppSharedPreferences();
+        sharedPreferences.edit().putBoolean(SURVEY_INFOBAR_DISMISSED_KEY, true).apply();
+        mLoggingHandler.removeCallbacksAndMessages(null);
+        mSurveyInfoBarTab.getInfoBarContainer().removeAnimationListener(this);
+    }
+
+    @VisibleForTesting
+    void setTabModelSelector(TabModelSelector tabModelSelector) {
+        mTabModelSelector = tabModelSelector;
     }
 }
