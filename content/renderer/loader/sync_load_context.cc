37e209915ff417834014d0973a124b718fd52f8d
arthursonzogni <arthursonzogni@chromium.org>
2017-11-15 17:23:37 +0100

    NavigationMojoResponse: Transfert URLLoaderClient
    This CL is behind the "NavigationMojoResponse" feature.
    
    During a navigation, after the headers are received, this CL unbinds the
    browser-side URLLoaderClient implementation. It passes the pair of
    URLLoaderPtr/URLLoaderClientRequest to the renderer. Then it binds it to
    the renderer-side URLLoaderClient implementation that will be used to
    continue the navigation (it will receive OnStartLoadingResponseBody()
    and OnComplete()).
    
    See the others CLs in this series:
    [1/3] https://chromium-review.googlesource.com/c/739502
    [2/3] https://chromium-review.googlesource.com/c/741237
    [3/3] this CL.
    
    This is part 5 of the implementation plan.
    Design doc: https://goo.gl/Rrrc7n
    Bug: 705744
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I9df447e28afe119dc678adac943a000de6787bf4

--- content/renderer/loader/sync_load_context.cc
+++ content/renderer/loader/sync_load_context.cc
@@ -34,6 +34,7 @@ void SyncLoadContext::StartAsyncWithWaitableEvent(
       true /* is_sync */, base::WrapUnique(context),
       blink::WebURLRequest::LoadingIPCType::kMojo,
       context->url_loader_factory_.get(), std::move(throttles),
+      mojom::URLLoaderPtr(), mojom::URLLoaderClientRequest(),
       mojo::ScopedDataPipeConsumerHandle());
 }
 
