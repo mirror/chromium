f79b3340d7ee4ae072a10c2ef8ca60351930d4cb
Xiaohui Chen <xiaohuic@google.com>
2017-11-15 10:39:32 -0800

    [assistant] Fix DCHECK error on getting screenshot
    gfx::Image object is not threadsafe, we process the image async to
    avoid blocking UI thread but causes DCHECK error.  Now we explicity
    transfer ownership of the object down to the callback.
    
    BUG=None
    TEST=running with DCHECK on and no error triggered after change.
    
    Change-Id: I1b3f678ddd9a411c59328e67de44b4e5db7ae865

--- chrome/browser/chromeos/arc/voice_interaction/arc_voice_interaction_framework_service.cc
+++ chrome/browser/chromeos/arc/voice_interaction/arc_voice_interaction_framework_service.cc
@@ -42,11 +42,13 @@
 #include "components/arc/instance_holder.h"
 #include "components/session_manager/core/session_manager.h"
 #include "content/public/browser/browser_thread.h"
+#include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_owner.h"
 #include "ui/compositor/layer_tree_owner.h"
+#include "ui/gfx/codec/jpeg_codec.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/image/image_util.h"
@@ -158,18 +160,18 @@ std::unique_ptr<ui::LayerTreeOwner> CreateLayerTreeForSnapshot(
 void EncodeAndReturnImage(
     ArcVoiceInteractionFrameworkService::CaptureFullscreenCallback callback,
     std::unique_ptr<ui::LayerTreeOwner> old_layer_owner,
-    const gfx::Image& image) {
+    gfx::Image image) {
   old_layer_owner.reset();
   base::PostTaskWithTraitsAndReplyWithResult(
       FROM_HERE,
       base::TaskTraits{base::MayBlock(), base::TaskPriority::USER_BLOCKING},
       base::BindOnce(
-          [](const gfx::Image& image) -> std::vector<uint8_t> {
+          [](SkBitmap image) -> std::vector<uint8_t> {
             std::vector<uint8_t> res;
-            gfx::JPEG1xEncodedDataFromImage(image, 100, &res);
+            gfx::JPEGCodec::Encode(image, 100, &res);
             return res;
           },
-          image),
+          image.AsBitmap()),
       std::move(callback));
 }
 
