ae6bd3bad978941918c3f42d14979589f143ee49
Ehsan Karamad <ekaramad@chromium.org>
2017-11-15 14:02:00 -0500

    Make ScrollFocusedEditableNodeIntoRect work with OOPIFs
    This CL introduces a temporary fix for ScrollFocusedEditableNodeIntoRect. This
    command which is issued by the browser for various input and IME related tasks
    (on touch platforms only) is expected to scale and scroll the current editable
    element into view. The legacy implementation lives in RenderViewImpl and
    WebViewImpl. This, however, is no longer relevant.
    
    The change involves two major parts:
      1) Bookkeeping of whether scrolling for a focused node was complete (OOPIFs).
      2) Actually scrolling the focused editable node into view.
    
    The first part was potentially broken for OOPIFs because
    RenderViewImpl::ScrollFocusedEditableNodeIntoRect would not complete all its
    tasks if WebView::ScrollFocusedEditableNodeIntoRect would return false; and it
    would return false for OOPIFs due to MainFrameImpl() == nullptr.
    
    The second part of the change is the major problem here since editable elements
    inside OOPIFs were not actually being scrolled at all. The core cause is that
    this API require finding a proper zoom and scroll point for each element and
    then set the scroll top accordingly. This is not directly applicable to OOPIFs
    (at least not trivially) given that OOPIFs do not know their root-layer
    coordinates in the renderer and furthermore, they cannot directly scroll
    themselves in the parent process.
    
    To temporarily resolve the second problem, this CL uses the best approximation
    at hand for the desired behavior, that is, scrolling the focused editable node
    into view through recursive scrolling of a rectangle to visible. The behavior,
    at least when PageScaleFactor() == 1, is reasonably close to that of in-process
    frames.
    
    The CL also adds a browser test to make sure the implemented feature works as
    expected.
    
    Bug: 676037, 784982
    Change-Id: Id27db87237c52084d19c69010caba03183e37d5f

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -193,6 +193,7 @@
 #include "third_party/WebKit/public/platform/modules/fetch/fetch_api_request.mojom-shared.h"
 #include "third_party/WebKit/public/platform/modules/permissions/permission.mojom.h"
 #include "third_party/WebKit/public/platform/modules/serviceworker/WebServiceWorkerNetworkProvider.h"
+#include "third_party/WebKit/public/web/WebAutofillClient.h"
 #include "third_party/WebKit/public/web/WebColorSuggestion.h"
 #include "third_party/WebKit/public/web/WebConsoleMessage.h"
 #include "third_party/WebKit/public/web/WebContextFeatures.h"
@@ -2024,9 +2025,7 @@ void RenderFrameImpl::OnMoveCaret(const gfx::Point& point) {
 
 void RenderFrameImpl::OnScrollFocusedEditableNodeIntoRect(
     const gfx::Rect& rect) {
-  // TODO(dtapuska): Move the implementation of scroll focused
-  // editable node into rect into this class.
-  render_view_->ScrollFocusedEditableNodeIntoRect(rect);
+  ScrollFocusedEditableElementIntoRect(rect);
 }
 
 void RenderFrameImpl::OnUndo() {
@@ -5463,6 +5462,7 @@ void RenderFrameImpl::HandleWebAccessibilityEvent(
 }
 
 void RenderFrameImpl::FocusedNodeChanged(const WebNode& node) {
+  has_scrolled_focused_editable_node_into_rect_ = false;
   bool is_editable = false;
   gfx::Rect node_bounds;
   if (!node.IsNull() && node.IsElementNode()) {
@@ -6641,6 +6641,30 @@ void RenderFrameImpl::SetCustomURLLoaderFactory(
   } else {
     custom_url_loader_factory_ = std::move(factory);
   }
+}
+
+void RenderFrameImpl::ScrollFocusedEditableElementIntoRect(
+    const gfx::Rect& rect) {
+  blink::WebAutofillClient* autofill_client = frame_->AutofillClient();
+  if (has_scrolled_focused_editable_node_into_rect_ &&
+      rect == rect_for_scrolled_focused_editable_node_ && autofill_client) {
+    autofill_client->DidCompleteFocusChangeInFrame();
+    return;
+  }
+
+  if (!render_view_->webview()->ScrollFocusedEditableElementIntoRect(rect))
+    return;
+
+  rect_for_scrolled_focused_editable_node_ = rect;
+  has_scrolled_focused_editable_node_into_rect_ = true;
+  if (!GetRenderWidget()->compositor()->HasPendingPageScaleAnimation() &&
+      autofill_client) {
+    autofill_client->DidCompleteFocusChangeInFrame();
+  }
+}
+
+void RenderFrameImpl::DidChangeVisibleViewport() {
+  has_scrolled_focused_editable_node_into_rect_ = false;
 }
 
 void RenderFrameImpl::InitializeUserMediaClient() {
