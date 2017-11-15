df2dc7f5fb9735688b7eb3b551575257419774dc
Anita Woodruff <awdf@chromium.org>
2017-11-15 17:17:05 +0000

    Create a metrics logger as a keyed service
    Rename some test cases
    
    
    WIP DO NOT REVIEW DO NOT PUSH DO NOT LOOK AT THIS
    
    Bug: 780534
    Change-Id: Icde7dbf2d141359098043c6b8745d709178e4498

---
+++ chrome/browser/notifications/metrics/notification_metrics_logger.cc
@@ -0,0 +1,15 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/notifications/metrics/notification_metrics_logger.h"
+
+#include "base/metrics/user_metrics.h"
+#include "base/metrics/user_metrics_action.h"
+#include "chrome/browser/notifications/metrics/notification_metrics_logger_factory.h"
+
+// static
+
+NotificationMetricsLogger::NotificationMetricsLogger() = default;
+
+NotificationMetricsLogger::~NotificationMetricsLogger() = default;
