e5fdd63afa91f69b07eb587ca2c183d45395b768
Andrew Grieve <agrieve@chromium.org>
2017-11-14 22:59:58 -0500

    Android: Remove reflection for a few isAtLeastO() instances
    No behavior change expected.
    
    Change-Id: I2f0bbb317db48ea8aecb2a9c8b03e02f622560fb

--- ui/android/java/src/org/chromium/ui/display/PhysicalDisplayAndroid.java
+++ ui/android/java/src/org/chromium/ui/display/PhysicalDisplayAndroid.java
@@ -11,12 +11,9 @@ import android.os.Build;
 import android.util.DisplayMetrics;
 import android.view.Display;
 
-import org.chromium.base.BuildInfo;
 import org.chromium.base.CommandLine;
 import org.chromium.base.Log;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 /**
  * A DisplayAndroid implementation tied to a physical Display.
@@ -106,13 +103,8 @@ import java.lang.reflect.Method;
         }
         if (hasForcedDIPScale()) displayMetrics.density = sForcedDIPScale.floatValue();
         boolean isWideColorGamut = false;
-        if (BuildInfo.isAtLeastO()) {
-            try {
-                Method method = display.getClass().getMethod("isWideColorGamut");
-                isWideColorGamut = (Boolean) method.invoke(display);
-            } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-                Log.e(TAG, "Error invoking isWideColorGamut:", e);
-            }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            isWideColorGamut = display.isWideColorGamut();
         }
 
         // JellyBean MR1 and later always uses RGBA_8888.
