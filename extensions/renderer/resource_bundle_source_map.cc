13170cee2c65ec227dbc7c294aa1b809e087743a
Yuzhu Shen <yzshen@chromium.org>
2017-11-14 16:57:40 -0800

    Enable new mojo JS bindings in extensions blessed context.
    This CL:
    - Adds support to load extensions JS sources from gzipped resources, since the
      JS file for new mojo JS bindings is gzipped. The alternative is to change the
      JS file to uncompressed, which would increase Chrome size quite significantly.
      For example, adding ~132KB to resources.pak for Linux official build.
    - Converts mimeHandlerPrivate to use new Mojo JS bindings.
    
    BUG=699569, 718047
    
    Change-Id: If485020558f607a1d2e81296476c60a20e7dd21b

--- extensions/renderer/resource_bundle_source_map.cc
+++ extensions/renderer/resource_bundle_source_map.cc
@@ -7,6 +7,7 @@
 #include "base/logging.h"
 #include "base/strings/string_piece.h"
 #include "extensions/renderer/static_v8_external_one_byte_string_resource.h"
+#include "third_party/zlib/google/compression_utils.h"
 #include "ui/base/resource/resource_bundle.h"
 
 namespace extensions {
@@ -23,6 +24,17 @@ v8::Local<v8::String> ConvertString(v8::Isolate* isolate,
 
 }  // namespace
 
+ResourceBundleSourceMap::ResourceInfo::ResourceInfo() = default;
+ResourceBundleSourceMap::ResourceInfo::ResourceInfo(int in_id, bool in_gzipped)
+    : id(in_id), gzipped(in_gzipped) {}
+ResourceBundleSourceMap::ResourceInfo::ResourceInfo(ResourceInfo&& other) =
+    default;
+
+ResourceBundleSourceMap::ResourceInfo::~ResourceInfo() = default;
+
+ResourceBundleSourceMap::ResourceInfo& ResourceBundleSourceMap::ResourceInfo::
+operator=(ResourceInfo&& other) = default;
+
 ResourceBundleSourceMap::ResourceBundleSourceMap(
     const ui::ResourceBundle* resource_bundle)
     : resource_bundle_(resource_bundle) {
@@ -32,30 +44,47 @@ ResourceBundleSourceMap::~ResourceBundleSourceMap() {
 }
 
 void ResourceBundleSourceMap::RegisterSource(const char* const name,
-                                             int resource_id) {
-  resource_id_map_[name] = resource_id;
+                                             int resource_id,
+                                             bool gzipped) {
+  resource_map_[name] = {resource_id, gzipped};
 }
 
 v8::Local<v8::String> ResourceBundleSourceMap::GetSource(
     v8::Isolate* isolate,
     const std::string& name) const {
-  const auto& resource_id_iter = resource_id_map_.find(name);
-  if (resource_id_iter == resource_id_map_.end()) {
+  const auto& resource_iter = resource_map_.find(name);
+  if (resource_iter == resource_map_.end()) {
     NOTREACHED() << "No module is registered with name \"" << name << "\"";
     return v8::Local<v8::String>();
   }
-  base::StringPiece resource =
-      resource_bundle_->GetRawDataResource(resource_id_iter->second);
+
+  ResourceInfo& info = resource_iter->second;
+  if (info.cached)
+    return ConvertString(isolate, *info.cached);
+
+  base::StringPiece resource = resource_bundle_->GetRawDataResource(info.id);
   if (resource.empty()) {
     NOTREACHED()
         << "Module resource registered as \"" << name << "\" not found";
     return v8::Local<v8::String>();
   }
+
+  if (info.gzipped) {
+    auto uncompressed_storage = std::make_unique<std::string>();
+    uint32_t size = compression::GetUncompressedSize(resource);
+    uncompressed_storage->resize(size);
+    base::StringPiece uncompressed(*uncompressed_storage);
+    if (!compression::GzipUncompress(resource, uncompressed))
+      return v8::Local<v8::String>();
+    info.cached = std::move(uncompressed_storage);
+    resource = uncompressed;
+  }
+
   return ConvertString(isolate, resource);
 }
 
 bool ResourceBundleSourceMap::Contains(const std::string& name) const {
-  return !!resource_id_map_.count(name);
+  return !!resource_map_.count(name);
 }
 
 }  // namespace extensions
