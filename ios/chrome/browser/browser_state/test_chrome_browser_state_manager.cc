cf2eb829b24e76aeeac64970a8fa74f69cdf450c
Mohamad Ahmadi <mahmadi@chromium.org>
2017-11-15 15:01:51 -0500

    Introduces a TabModelListObserver interface that notifies its implementations when a TabModel is registered to a browser state.
    Also makes loose utility functions in tab_model_list.h static functions
    of the TabModelList and expands the unit tests for the TabModelList class.
    
    
    Signed-off-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Bug:738938
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ie6dd56b8dd87467a3a5d280fc4f38ab35d9bad10

--- ios/chrome/browser/browser_state/test_chrome_browser_state_manager.cc
+++ ios/chrome/browser/browser_state/test_chrome_browser_state_manager.cc
@@ -2,11 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <vector>
+
+#include "base/files/file_path.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state_manager.h"
 
 TestChromeBrowserStateManager::TestChromeBrowserStateManager(
     const base::FilePath& user_data_dir)
     : browser_state_info_cache_(local_state_.Get(), user_data_dir) {}
+
+TestChromeBrowserStateManager::TestChromeBrowserStateManager(
+    ios::ChromeBrowserState* browser_state)
+    : TestChromeBrowserStateManager(base::FilePath()) {
+  browser_state_ = browser_state;
+}
 
 TestChromeBrowserStateManager::~TestChromeBrowserStateManager() {}
 
@@ -27,5 +36,8 @@ TestChromeBrowserStateManager::GetBrowserStateInfoCache() {
 
 std::vector<ios::ChromeBrowserState*>
 TestChromeBrowserStateManager::GetLoadedBrowserStates() {
-  return std::vector<ios::ChromeBrowserState*>();
+  std::vector<ios::ChromeBrowserState*> result;
+  if (browser_state_)
+    result.push_back(browser_state_);
+  return result;
 }
