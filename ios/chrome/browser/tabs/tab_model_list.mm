8bd5f6773874cc9408ddf680a7e3e3ff6f04a70b
Mohamad Ahmadi <mahmadi@chromium.org>
2017-11-15 15:01:51 -0500

    Introduces a TabModelListOberver interface that notifies its implementations when a TabModel is registered to a browser state.
    Also makes loose utility functions in tab_model_list.h static functions
    of the TabModelList and expands the unit tests for the TabModelList class.
    
    
    Signed-off-by: Mohamad Ahmadi <mahmadi@chromium.org>
    Bug:738938
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Ie6dd56b8dd87467a3a5d280fc4f38ab35d9bad10

--- ios/chrome/browser/tabs/tab_model_list.mm
+++ ios/chrome/browser/tabs/tab_model_list.mm
@@ -7,38 +7,21 @@
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
-#include "base/supports_user_data.h"
 #include "ios/chrome/browser/application_context.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state_manager.h"
 #import "ios/chrome/browser/tabs/tab_model.h"
+#import "ios/chrome/browser/tabs/tab_model_list_observer.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
 namespace {
-// Wrapper around a NSMutableArray<TabModel> allowing to bind it to a
-// base::SupportsUserData. Any base::SupportsUserData that owns this
-// wrapper will owns the reference to the TabModels.
-class TabModelList : public base::SupportsUserData::Data {
- public:
-  TabModelList();
-  ~TabModelList() override;
-
-  static TabModelList* GetForBrowserState(
-      ios::ChromeBrowserState* browser_state,
-      bool create);
-
-  NSMutableSet<TabModel*>* tab_models() const { return tab_models_; }
-
- private:
-  NSMutableSet<TabModel*>* tab_models_;
-
-  DISALLOW_COPY_AND_ASSIGN(TabModelList);
-};
 
 const char kTabModelListKey = 0;
+
+}  // namespace
 
 TabModelList::TabModelList() : tab_models_([[NSMutableSet alloc] init]) {}
 
@@ -46,6 +29,9 @@ TabModelList::~TabModelList() {
   // TabModelList is destroyed during base::SupportsUserData destruction. At
   // that point, all TabModels must have been unregistered already.
   DCHECK_EQ([tab_models_ count], 0u);
+
+  for (auto& observer : observers_)
+    observer.TabModelListDestroyed();
 }
 
 // static
@@ -61,34 +47,75 @@ TabModelList* TabModelList::GetForBrowserState(
   }
   return tab_model_list;
 }
-}  // namespace
 
-void RegisterTabModelWithChromeBrowserState(
+// static
+void TabModelList::AddObserver(TabModelListObserver* observer) {
+  std::vector<ios::ChromeBrowserState*> browser_states =
+      GetApplicationContext()
+          ->GetChromeBrowserStateManager()
+          ->GetLoadedBrowserStates();
+
+  for (ios::ChromeBrowserState* browser_state : browser_states) {
+    DCHECK(!browser_state->IsOffTheRecord());
+    TabModelList::GetForBrowserState(browser_state, true)
+        ->observers_.AddObserver(observer);
+  }
+}
+
+// static
+void TabModelList::RemoveObserver(TabModelListObserver* observer) {
+  std::vector<ios::ChromeBrowserState*> browser_states =
+      GetApplicationContext()
+          ->GetChromeBrowserStateManager()
+          ->GetLoadedBrowserStates();
+
+  for (ios::ChromeBrowserState* browser_state : browser_states) {
+    DCHECK(!browser_state->IsOffTheRecord());
+    TabModelList::GetForBrowserState(browser_state, true)
+        ->observers_.RemoveObserver(observer);
+  }
+}
+
+// static
+void TabModelList::RegisterTabModelWithChromeBrowserState(
     ios::ChromeBrowserState* browser_state,
     TabModel* tab_model) {
   NSMutableSet<TabModel*>* tab_models =
       TabModelList::GetForBrowserState(browser_state, true)->tab_models();
   DCHECK(![tab_models containsObject:tab_model]);
   [tab_models addObject:tab_model];
+
+  TabModelList* original_tab_model_list = TabModelList::GetForBrowserState(
+      browser_state->GetOriginalChromeBrowserState(), true);
+  for (auto& observer : original_tab_model_list->observers_)
+    observer.TabModelRegisteredWithBrowserState(tab_model, browser_state);
 }
 
-void UnregisterTabModelFromChromeBrowserState(
+// static
+void TabModelList::UnregisterTabModelFromChromeBrowserState(
     ios::ChromeBrowserState* browser_state,
     TabModel* tab_model) {
   NSMutableSet<TabModel*>* tab_models =
       TabModelList::GetForBrowserState(browser_state, false)->tab_models();
   DCHECK([tab_models containsObject:tab_model]);
   [tab_models removeObject:tab_model];
+
+  TabModelList* original_tab_model_list = TabModelList::GetForBrowserState(
+      browser_state->GetOriginalChromeBrowserState(), false);
+  for (auto& observer : original_tab_model_list->observers_)
+    observer.TabModelUnregisteredFromBrowserState(tab_model, browser_state);
 }
 
-NSArray<TabModel*>* GetTabModelsForChromeBrowserState(
+// static
+NSArray<TabModel*>* TabModelList::GetTabModelsForChromeBrowserState(
     ios::ChromeBrowserState* browser_state) {
   TabModelList* tab_model_list =
       TabModelList::GetForBrowserState(browser_state, false);
   return tab_model_list ? [tab_model_list->tab_models() allObjects] : nil;
 }
 
-TabModel* GetLastActiveTabModelForChromeBrowserState(
+// static
+TabModel* TabModelList::GetLastActiveTabModelForChromeBrowserState(
     ios::ChromeBrowserState* browser_state) {
   TabModelList* tab_model_list =
       TabModelList::GetForBrowserState(browser_state, false);
@@ -102,7 +129,8 @@ TabModel* GetLastActiveTabModelForChromeBrowserState(
   return [tab_model_list->tab_models() anyObject];
 }
 
-bool IsOffTheRecordSessionActive() {
+// static
+bool TabModelList::IsOffTheRecordSessionActive() {
   std::vector<ios::ChromeBrowserState*> browser_states =
       GetApplicationContext()
           ->GetChromeBrowserStateManager()
