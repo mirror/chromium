7a844d8d5ba68861192936bf200e07f26ab084ab
Vincent Palatin <vpalatin@chromium.org>
2017-11-15 14:40:13 +0100

    Mojo bridge interface for the USB Host
    Define the bridge interface to connect the machine USB devices to the
    Android USB host API.
    Implement the API using /device/usb.
    
    The permission request for the USB devices is currently stubbed and
    fails closed. So until the Permission UI and settings are submitted, the
    API basically rejects all the requests.
    
    Bug: 24572867
    Test: Manual, run with ARC++ app using android.hardware.usb to use USB
    devices.
    
    Change-Id: Ibce0a45e4359a97ee06d8d49981169bd5f18176f

---
+++ components/arc/usb/usb_host_bridge.h
@@ -0,0 +1,87 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ARC_USB_USB_HOST_BRIDGE_H_
+#define COMPONENTS_ARC_USB_USB_HOST_BRIDGE_H_
+
+#include <string>
+#include <vector>
+
+#include "base/callback_forward.h"
+#include "base/files/scoped_file.h"
+#include "base/macros.h"
+#include "base/scoped_observer.h"
+#include "components/arc/common/usb_host.mojom.h"
+#include "components/arc/instance_holder.h"
+#include "components/keyed_service/core/keyed_service.h"
+#include "device/usb/public/interfaces/device_manager.mojom.h"
+#include "device/usb/usb_device.h"
+#include "device/usb/usb_service.h"
+#include "mojo/public/cpp/bindings/binding.h"
+
+namespace content {
+class BrowserContext;
+}  // namespace content
+
+namespace arc {
+
+class ArcBridgeService;
+
+// Private implementation of UsbHost.
+class UsbHostBridge : public KeyedService,
+                      public InstanceHolder<mojom::UsbHostInstance>::Observer,
+                      public device::UsbService::Observer,
+                      public mojom::UsbHost {
+ public:
+  // Returns singleton instance for the given BrowserContext,
+  // or nullptr if the browser |context| is not allowed to use ARC.
+  static UsbHostBridge* GetForBrowserContext(content::BrowserContext* context);
+
+  // The constructor will register an Observer with ArcBridgeService.
+  explicit UsbHostBridge(content::BrowserContext* context,
+                         ArcBridgeService* bridge_service);
+  ~UsbHostBridge() override;
+
+  // mojom::UsbHost overrides:
+  void RequestPermission(const std::string& guid,
+                         const std::string& package,
+                         bool interactive,
+                         RequestPermissionCallback callback) override;
+  void OpenDevice(const std::string& guid,
+                  OpenDeviceCallback callback) override;
+  void GetDeviceInfo(const std::string& guid,
+                     GetDeviceInfoCallback callback) override;
+
+  // device::UsbService::Observer overrides:
+  void OnDeviceAdded(scoped_refptr<device::UsbDevice> device) override;
+  void OnDeviceRemoved(scoped_refptr<device::UsbDevice> device) override;
+  void WillDestroyUsbService() override;
+
+  // InstanceHolder<mojom::UsbHostInstance>::Observer overrides:
+  void OnInstanceReady() override;
+
+ private:
+  void OnInstanceInited();
+  void OnDeviceChecked(const std::string& guid, bool allowed);
+  void DoRequestUserAuthorization(const std::string& guid,
+                                  const std::string& package,
+                                  RequestPermissionCallback callback);
+  bool HasPermissionForDevice(const std::string& guid);
+
+  ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
+  mojo::Binding<arc::mojom::UsbHost> binding_;
+  mojom::UsbHostPtr usb_host_ptr_;
+  ScopedObserver<device::UsbService, device::UsbService::Observer>
+      usb_observer_;
+  device::UsbService* usb_service_;
+
+  // WeakPtrFactory to use for callbacks.
+  base::WeakPtrFactory<UsbHostBridge> weak_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(UsbHostBridge);
+};
+
+}  // namespace arc
+
+#endif  // COMPONENTS_ARC_USB_USB_HOST_BRIDGE_H_
