f89a2bb60666885f703ecc54518e4678121a0ff1
Alexandre Courbot <acourbot@chromium.org>
2017-11-15 18:04:38 +0900

    vaapi_wrapper: prepare for libva 2.0
    libva 2.0 is a major version upgrade, meaning that the library dlopen'ed
    by Chromium is renamed from libva.so.1 to libva.so.2.
    
    Due to the way ChromeOS is built, we may end up with a browser build
    against the old libva even after the sysroot is upgraded to the new
    version. For this reason we will keep both versions of the library on
    the system until all buildbots have caught up to the 2.0 upgrade.
    
    This patch makes Chromium capable of loading any of these two versions,
    by whitelisting the newly-introduced libraries on the rootfs, and
    loading the correct library depending on the version it has been
    compiled against.
    
    The old libva 1 paths will be removed once the buildbots have all caught
    up.
    
    BUG=chromium:785117
    TEST=Made sure that the chrome binary and video_decode_accelerator_unittest
    were both finding and loading the right library.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I93c582e4ff6bca730003706bd1e21fbb8959be1f

--- content/gpu/gpu_sandbox_hook_linux.cc
+++ content/gpu/gpu_sandbox_hook_linux.cc
@@ -249,20 +249,20 @@ void LoadStandardLibraries(
     // inside the sandbox, so preload them now.
     if (options.vaapi_accelerated_video_encode_enabled ||
         options.accelerated_video_decode_enabled) {
-      const char* I965DrvVideoPath = nullptr;
-      const char* I965HybridDrvVideoPath = nullptr;
       if (IsArchitectureX86_64()) {
-        I965DrvVideoPath = "/usr/lib64/va/drivers/i965_drv_video.so";
-        I965HybridDrvVideoPath = "/usr/lib64/va/drivers/hybrid_drv_video.so";
+        dlopen("/usr/lib64/va1/drivers/i965_drv_video.so", dlopen_flag);
+        dlopen("/usr/lib64/va1/drivers/hybrid_drv_video.so", dlopen_flag);
+        dlopen("/usr/lib64/va/drivers/i965_drv_video.so", dlopen_flag);
+        dlopen("/usr/lib64/va/drivers/hybrid_drv_video.so", dlopen_flag);
       } else if (IsArchitectureI386()) {
-        I965DrvVideoPath = "/usr/lib/va/drivers/i965_drv_video.so";
+        dlopen("/usr/lib/va1/drivers/i965_drv_video.so", dlopen_flag);
+        dlopen("/usr/lib/va/drivers/i965_drv_video.so", dlopen_flag);
       }
-      dlopen(I965DrvVideoPath, dlopen_flag);
-      if (I965HybridDrvVideoPath)
-        dlopen(I965HybridDrvVideoPath, dlopen_flag);
       dlopen("libva.so.1", dlopen_flag);
+      dlopen("libva.so.2", dlopen_flag);
 #if defined(USE_OZONE)
       dlopen("libva-drm.so.1", dlopen_flag);
+      dlopen("libva-drm.so.2", dlopen_flag);
 #elif defined(USE_X11)
       dlopen("libva-x11.so.1", dlopen_flag);
 #endif
