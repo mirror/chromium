7a6e8690ca885babf4b56ab839e0eba882e36759
Sergey Kuznetsov <kuznetsovs@yandex-team.ru>
2017-11-15 11:43:56 +0300

    Limit usage of beginBackgroundTaskWithExpirationHandler.
    beginBackgroundTaskWithExpirationHandler appears to be expensive function,
    especially on iOS 11. We observe huge impact on UI-thread performance in code,
    that heavy uses PostTask api for work offloading.
    According to Apple docs and ScopedCriticalAction usage pattern, it is possible
    to maintain single background task + some sort of 'reference counting' above it.
    
    Bug: 785460
    Change-Id: I41aeb124c05ac6f8903ca5942f96ff59559338c2

--- base/ios/scoped_critical_action.mm
+++ base/ios/scoped_critical_action.mm
@@ -6,71 +6,112 @@
 
 #import <UIKit/UIKit.h>
 
-#include "base/logging.h"
-#include "base/memory/ref_counted.h"
+#include "base/lazy_instance.h"
 #include "base/synchronization/lock.h"
 
 namespace base {
 namespace ios {
+namespace {
 
-ScopedCriticalAction::ScopedCriticalAction()
-    : core_(MakeRefCounted<ScopedCriticalAction::Core>()) {
-  ScopedCriticalAction::Core::StartBackgroundTask(core_);
-}
+class BackgroundTaskTracker {
+ public:
+  BackgroundTaskTracker()
+      : ui_application_([UIApplication sharedApplication]) {}
 
-ScopedCriticalAction::~ScopedCriticalAction() {
-  ScopedCriticalAction::Core::EndBackgroundTask(core_);
-}
+  int BeginBackgroundTask() {
+    if (!ui_application_) {
+      return -1;
+    }
 
-ScopedCriticalAction::Core::Core()
-    : background_task_id_(UIBackgroundTaskInvalid) {}
+    AutoLock lock_scope(lock_);
 
-ScopedCriticalAction::Core::~Core() {
-  DCHECK_EQ(background_task_id_, UIBackgroundTaskInvalid);
-}
+    int task_id = next_task_id_++;
 
-// This implementation calls |beginBackgroundTaskWithExpirationHandler:| when
-// instantiated and |endBackgroundTask:| when destroyed, creating a scope whose
-// execution will continue (temporarily) even after the app is backgrounded.
-// static
-void ScopedCriticalAction::Core::StartBackgroundTask(scoped_refptr<Core> core) {
-  UIApplication* application = [UIApplication sharedApplication];
-  if (!application) {
-    return;
+    ++running_task_count_;
+    if (running_task_count_ == 1) {
+      background_task_id_ =
+          [ui_application_ beginBackgroundTaskWithExpirationHandler:^{
+            OnBackgroundTaskExpired();
+          }];
+    }
+
+    return task_id;
   }
 
-  core->background_task_id_ =
-      [application beginBackgroundTaskWithExpirationHandler:^{
-        DLOG(WARNING) << "Background task with id " << core->background_task_id_
-                      << " expired.";
-        // Note if |endBackgroundTask:| is not called for each task before time
-        // expires, the system kills the application.
-        EndBackgroundTask(core);
-      }];
-
-  if (core->background_task_id_ == UIBackgroundTaskInvalid) {
-    DLOG(WARNING)
-        << "beginBackgroundTaskWithExpirationHandler: returned an invalid ID";
-  } else {
-    VLOG(3) << "Beginning background task with id "
-            << core->background_task_id_;
-  }
-}
-
-// static
-void ScopedCriticalAction::Core::EndBackgroundTask(scoped_refptr<Core> core) {
-  UIBackgroundTaskIdentifier task_id;
-  {
-    AutoLock lock_scope(core->background_task_id_lock_);
-    if (core->background_task_id_ == UIBackgroundTaskInvalid) {
+  void EndBackgroundTask(int task_id) {
+    if (!ui_application_) {
       return;
     }
-    task_id = core->background_task_id_;
-    core->background_task_id_ = UIBackgroundTaskInvalid;
+
+    UIBackgroundTaskIdentifier end_task = UIBackgroundTaskInvalid;
+
+    {
+      AutoLock lock_scope(lock_);
+
+      if (task_id < max_expired_task_id_) {
+        // Ignore expired tasks.
+        return;
+      }
+
+      --running_task_count_;
+      if (running_task_count_ == 0) {
+        std::swap(background_task_id_, end_task);
+      }
+    }
+
+    if (end_task != UIBackgroundTaskInvalid) {
+      [ui_application_ endBackgroundTask:end_task];
+    }
   }
 
-  VLOG(3) << "Ending background task with id " << task_id;
-  [[UIApplication sharedApplication] endBackgroundTask:task_id];
+  void OnBackgroundTaskExpired() {
+    DCHECK(ui_application_);
+    UIBackgroundTaskIdentifier end_task = UIBackgroundTaskInvalid;
+
+    {
+      AutoLock lock_scope(lock_);
+
+      // Mark all previous tasks as expired.
+      max_expired_task_id_ = next_task_id_;
+      // No running tasks at this moment.
+      running_task_count_ = 0;
+
+      std::swap(background_task_id_, end_task);
+    }
+
+    if (end_task != UIBackgroundTaskInvalid) {
+      [ui_application_ endBackgroundTask:end_task];
+    }
+  }
+
+ private:
+  const UIApplication* ui_application_;
+
+  Lock lock_;
+  // Number of running tasks. First running task begins system background task
+  // |background_task_id_| with |beginBackgroundTaskWithExpirationHandler|,
+  // last running task ends system background task |background_task_id_|
+  // with |endBackgroundTask|.
+  int running_task_count_ = 0;
+  // Generator for task id.
+  int next_task_id_ = 0;
+  // Tasks with task_id < |max_expired_task_id_| are expired and should be
+  // ignored in |EndBackgroundTask|.
+  int max_expired_task_id_ = 0;
+  // Task id for system background task.
+  UIBackgroundTaskIdentifier background_task_id_ = UIBackgroundTaskInvalid;
+};
+
+LazyInstance<BackgroundTaskTracker>::Leaky g_background_task_tracker =
+    LAZY_INSTANCE_INITIALIZER;
+
+}  // namespace
+
+ScopedCriticalAction::ScopedCriticalAction()
+    : task_id_(g_background_task_tracker.Get().BeginBackgroundTask()) {}
+
+ScopedCriticalAction::~ScopedCriticalAction() {
+  g_background_task_tracker.Get().EndBackgroundTask(task_id_);
 }
 
 }  // namespace ios
