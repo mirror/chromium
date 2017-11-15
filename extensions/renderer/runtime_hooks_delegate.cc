f41966eff3c76a51234b015aa26fbb4724ce2173
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-11-14 18:21:44 -0800

    messaging size Bug:
    Change-Id: I0ae97ba4f9a77e1aed6da1530865ba3963fc9b74

--- extensions/renderer/runtime_hooks_delegate.cc
+++ extensions/renderer/runtime_hooks_delegate.cc
@@ -268,11 +268,12 @@ RequestResult RuntimeHooksDelegate::HandleSendMessage(
   }
 
   v8::Local<v8::Value> v8_message = arguments[1];
+  std::string error;
   std::unique_ptr<Message> message =
-      messaging_util::MessageFromV8(v8_context, v8_message);
+      messaging_util::MessageFromV8(v8_context, v8_message, &error);
   if (!message) {
     RequestResult result(RequestResult::INVALID_INVOCATION);
-    result.error = "Illegal argument to runtime.sendMessage for 'message'.";
+    result.error = std::move(error);
     return result;
   }
 
@@ -297,12 +298,12 @@ RequestResult RuntimeHooksDelegate::HandleSendNativeMessage(
 
   v8::Local<v8::Value> v8_message = arguments[1];
   DCHECK(!v8_message.IsEmpty());
-  std::unique_ptr<Message> message =
-      messaging_util::MessageFromV8(script_context->v8_context(), v8_message);
+  std::string error;
+  std::unique_ptr<Message> message = messaging_util::MessageFromV8(
+      script_context->v8_context(), v8_message, &error);
   if (!message) {
     RequestResult result(RequestResult::INVALID_INVOCATION);
-    result.error =
-        "Illegal argument to runtime.sendNativeMessage for 'message'.";
+    result.error = std::move(error);
     return result;
   }
 
