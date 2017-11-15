20e319a888b282dd6efe2a0cee57219adbefe469
Gary Kacmarcik <garykac@chromium.org>
2017-11-15 14:43:06 -0800

    Add clipboard permission type and context
    This cl adds the basic Clipboard PermissionType and the PermissionContext.
    
    UX for this permission (which will be behind a feature flag) will be added in a follow-up cl.
    
    Bug: 677564
    Change-Id: Ia5909c8f1feeedad5471db87630154071d7ba8ac

---
+++ chrome/browser/clipboard/clipboard_write_permission_context.cc
@@ -0,0 +1,23 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/clipboard/clipboard_write_permission_context.h"
+
+ClipboardWritePermissionContext::ClipboardWritePermissionContext(
+    Profile* profile)
+    : PermissionContextBase(profile,
+                            CONTENT_SETTINGS_TYPE_CLIPBOARD_WRITE,
+                            blink::FeaturePolicyFeature::kNotFound) {}
+
+ClipboardWritePermissionContext::~ClipboardWritePermissionContext() = default;
+
+ContentSetting ClipboardWritePermissionContext::GetPermissionStatusInternal(
+    content::RenderFrameHost* render_frame_host,
+    const GURL& requesting_origin,
+    const GURL& embedding_origin) const {
+  return CONTENT_SETTING_ALLOW;
+}
+bool ClipboardWritePermissionContext::IsRestrictedToSecureOrigins() const {
+  return true;
+}
