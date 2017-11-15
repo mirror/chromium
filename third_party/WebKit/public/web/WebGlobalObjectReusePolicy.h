6f6a630023dcc46edaa3c6defcabf57727197c2a
Balazs Engedy <engedy@chromium.org>
2017-11-15 19:49:43 +0100

    Rebind RF's InterfaceProvider on navigation.
    Every RenderFrameHost exposes an InterfaceProvider to its corresponding
    RenderFrame, which is used by content/renderer/, Blink and WebUI JS code
    to access frame-scoped services implemented by the RFH.
    
    Previously, for each frame, there was a single InterfaceProvider
    interface connection used throughout the entire lifetime of the
    RenderFrame. The client end of this connection was passed in to the
    RenderFrame synchronously at construction time, and bound to
    |RenderFrameImpl::remote_interfaces_|.
    
    As of this CL, this primordial InterfaceProvider interface connection
    will only service interface requests originating from the initial empty
    document in the frame. For each subsequent non-same-document navigation,
    a new message pipe is created, with its client end being bound to
    |remote_interfaces_| when the navigation commits, and its request end
    sent to the browser process as part of DidCommitProvisionalLoad message.
    
    On the browser process side, when the RenderFrameHost receives an
    InterfaceProvider request along with DidCommitProvisionalLoad, it
    immediately breaks the old InterfaceProvider connection. Pending
    interface requests on the old connection are dropped, if any. Interface
    requests possibly pending on the new InterfaceProvider connection are
    only dispatched after WebContentsObserver::DidFinishNavigation fires.
    
    This ensures that interface requests racing with navigation commit will
    be either ignored, or correctly serviced in the security context of the
    document they originated from. In other words, when an interface request
    is dispatched, the invariant will hold that calling GetLastCommittedURL
    on the RenderFrameHost will return a URL that matches that of the
    document from which the interface request actually originated from.
    
    Note that |remote_interfaces_| remains a non-Channel-associated. Also
    note that it remains the responsibility of individual features using
    |remote_interfaces_| to close interface connections already established
    through the InterfaceProvider when a non-same-document navigation
    commits, if needed in particular use-case.
    
    Bug: 729021
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I1eb4aa78be9627f70fdf6e18af570f083b9e306d

---
+++ third_party/WebKit/public/web/WebGlobalObjectReusePolicy.h
@@ -0,0 +1,14 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WebGlobalObjectReusePolicy_h
+#define WebGlobalObjectReusePolicy_h
+
+namespace blink {
+
+enum class WebGlobalObjectReusePolicy { kCreateNew, kUseExisting };
+
+}  // namespace blink
+
+#endif
