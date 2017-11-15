e597dbca77e883aac8e430b52aaf10d090f4ea55
Jacob Dufault <jdufault@google.com>
2017-11-15 14:42:50 -0800

    cros: Basic views-based login screen.
    The views-based login screen shows the users on the device and the
    basic login flow works, but lots of additional features, such as
    registering a new user, do not work yet.
    
    Bug: 784495
    Change-Id: I67ee83d1ca84fedf465882a90e6be47e6b079967

--- chrome/browser/ui/ash/login_screen_client.cc
+++ chrome/browser/ui/ash/login_screen_client.cc
@@ -40,7 +40,13 @@ LoginScreenClient::~LoginScreenClient() {
 }
 
 // static
+bool LoginScreenClient::HasInstance() {
+  return !!g_instance;
+}
+
+// static
 LoginScreenClient* LoginScreenClient::Get() {
+  DCHECK(g_instance);
   return g_instance;
 }
 
@@ -56,6 +62,10 @@ void LoginScreenClient::AuthenticateUser(const AccountId& account_id,
 void LoginScreenClient::ShowLockScreen(
     ash::mojom::LoginScreen::ShowLockScreenCallback on_shown) {
   login_screen_->ShowLockScreen(std::move(on_shown));
+}
+
+void LoginScreenClient::ShowLoginScreen() {
+  login_screen_->ShowLoginScreen();
 }
 
 void LoginScreenClient::AttemptUnlock(const AccountId& account_id) {
