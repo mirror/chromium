fc44f13b11675ab9bc8d89bb0ba022b6cc3f8e8e
Ella Ge <eirage@chromium.org>
2017-11-15 10:49:49 -0500

    Run target-only TouchAdjustment on TouchStart
    This CL adds TouchAdjustment logic when targeting TouchStart, and
    following touch events will also be captured by the same target.
    We only change the touch event target but keep the original
    coordinates.
    
    Use GestureTap adjustment logic for TouchEvents, and use same padding
    for adjustment hittest, so result target should be same.
    Aura use min_gesture_bounds_length = 25.f, Android use min = 20.f,
    max = 32.f. We use min = 20.f max = 32.f as the padding bounds.
    
    Bug: 625198
    Change-Id: I1463f29415c46c1cd0f98be5eeac57654c5ff833

--- third_party/WebKit/Source/core/input/PointerEventManager.cpp
+++ third_party/WebKit/Source/core/input/PointerEventManager.cpp
@@ -19,6 +19,7 @@
 #include "core/layout/HitTestCanvasResult.h"
 #include "core/page/ChromeClient.h"
 #include "core/page/Page.h"
+#include "core/page/TouchAdjustment.h"
 #include "platform/wtf/AutoReset.h"
 #include "public/platform/WebTouchEvent.h"
 
@@ -64,6 +65,14 @@ Vector<std::pair<WebTouchPoint, TimeTicks>> GetCoalescedPoints(
     }
   }
   return related_points;
+}
+
+bool ShouldAdjustTouchEvent(const WebTouchEvent& event) {
+  return RuntimeEnabledFeatures::UnifiedTouchAdjustmentEnabled() &&
+         event.GetType() == WebInputEvent::kTouchStart &&
+         event.touches_length &&
+         event.TouchPointInRootFrame(0).pointer_type ==
+             WebPointerProperties::PointerType::kTouch;
 }
 
 }  // namespace
@@ -347,8 +356,14 @@ WebInputEventResult PointerEventManager::HandleTouchEvents(
   // Do the first point target calculation to create the user gesture.
   EventHandlingUtil::PointerEventTarget first_pointer_event_target;
   if (event.touches_length) {
-    first_pointer_event_target =
-        ComputePointerEventTarget(event.TouchPointInRootFrame(0));
+    if (ShouldAdjustTouchEvent(event)) {
+      WebTouchPoint first_touch_point = event.TouchPointInRootFrame(0);
+      AdjustPointForTouchStart(&first_touch_point);
+      first_pointer_event_target = ComputePointerEventTarget(first_touch_point);
+    } else {
+      first_pointer_event_target =
+          ComputePointerEventTarget(event.TouchPointInRootFrame(0));
+    }
   }
 
   // Any finger lifting is a user gesture only when it wasn't associated with a
@@ -395,6 +410,33 @@ WebInputEventResult PointerEventManager::HandleTouchEvents(
   // class. It will be called before rAF and also whenever we run in low latency
   // mode as mentioned in crbug.com/728250.
   return touch_event_manager_->FlushEvents();
+}
+
+void PointerEventManager::AdjustPointForTouchStart(WebTouchPoint* touch_point) {
+  DCHECK(touch_point);
+  LayoutSize padding = GetHitTestRectForAdjustment(
+      LayoutSize(touch_point->radius_x, touch_point->radius_y));
+  if (padding.IsEmpty())
+    return;
+
+  HitTestRequest::HitTestRequestType hit_type = HitTestRequest::kReadOnly |
+                                                HitTestRequest::kActive |
+                                                HitTestRequest::kListBased;
+  IntPoint hit_test_point = frame_->View()->RootFrameToContents(
+      FlooredIntPoint(touch_point->PositionInWidget()));
+  HitTestResult hit_test_result =
+      frame_->GetEventHandler().HitTestResultAtPoint(hit_test_point, hit_type,
+                                                     padding);
+
+  Node* adjusted_node = nullptr;
+  IntPoint adjusted_point;
+  bool adjusted = frame_->GetEventHandler().BestClickableNodeForHitTestResult(
+      hit_test_result, adjusted_point, adjusted_node);
+
+  if (!adjusted)
+    return;
+
+  touch_point->SetPositionInWidget(adjusted_point.X(), adjusted_point.Y());
 }
 
 EventHandlingUtil::PointerEventTarget
--- third_party/WebKit/Source/core/page/TouchAdjustment.cpp
+++ third_party/WebKit/Source/core/page/TouchAdjustment.cpp
@@ -39,11 +39,16 @@
 #include "platform/geometry/IntSize.h"
 #include "platform/text/TextBreakIterator.h"
 
+#include <cmath>
+
 namespace blink {
 
 namespace TouchAdjustment {
 
 const float kZeroTolerance = 1e-6f;
+
+const float kMaxAdjustmentRadiusDips = 16.f;
+const float kMinAdjustmentRadiusDips = 10.f;
 
 // Class for remembering absolute quads of a target node and what node they
 // represent.
@@ -572,6 +577,16 @@ bool FindBestZoomableArea(Node*& target_node,
   return TouchAdjustment::FindNodeWithLowestDistanceMetric(
       target_node, target_point, target_area, touch_hotspot, touch_area,
       subtargets, TouchAdjustment::ZoomableIntersectionQuotient);
+}
+
+LayoutSize GetHitTestRectForAdjustment(const LayoutSize& touch_area) {
+  float width = std::min(
+      std::max(touch_area.Width(), TouchAdjustment::kMinAdjustmentRadiusDips),
+      TouchAdjustment::kMaxAdjustmentRadiusDips);
+  float height = std::min(
+      std::max(touch_area.Height(), TouchAdjustment::kMinAdjustmentRadiusDips),
+      TouchAdjustment::kMaxAdjustmentRadiusDips);
+  return LayoutSize(width, height);
 }
 
 }  // namespace blink
