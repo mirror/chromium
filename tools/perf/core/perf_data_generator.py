b7f0b63daf71b77eb56e1c6662641edf55a7ed6f
Charlie Andrews <charliea@chromium.org>
2017-11-15 11:27:27 -0500

    Disable gpu_perftests on Android One
    It's been failing since at least September 20, and no one can figure
    out why.
    
    TBR=nednguyen@google.com, reveman@chromium.org
    Bug: 785291
    Change-Id: I57a1ca702ba52e56c5848216864aba0f794c10f2

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -274,7 +274,7 @@ def get_waterfall_config():
           ],
        'perf_tests': [
          ('tracing_perftests', 'build17-b1--device2'),
-         ('gpu_perftests', 'build18-b1--device2'),
+         # ('gpu_perftests', 'build18-b1--device2'), https://crbug.com/775219
          # ('cc_perftests', 'build47-b1--device2'), https://crbug.com/736150
         ]
       }
@@ -421,7 +421,8 @@ def get_waterfall_config():
            'build103-m1', 'build104-m1', 'build105-m1'
           ],
        'perf_tests': [
-         ('angle_perftests', 'build103-m1'),
+         # crbug.com/785291
+         # ('angle_perftests', 'build103-m1'),
          ('load_library_perf_tests', 'build103-m1'),
          ('performance_browser_tests', 'build103-m1'),
          ('media_perftests', 'build104-m1')]
