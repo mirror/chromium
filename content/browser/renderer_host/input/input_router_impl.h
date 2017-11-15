eb99c971d5a50f7ddd3ea90960b3c470e6602819
Xida Chen <xidachen@chromium.org>
2017-11-15 16:24:00 -0500

    [DONOT COMMIT] Debugging patch
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ie7e30be5bcd39124eb4414f5decb88d05ab6a022

--- content/browser/renderer_host/input/input_router_impl.h
+++ content/browser/renderer_host/input/input_router_impl.h
@@ -75,11 +75,11 @@ class CONTENT_EXPORT InputRouterImpl
   void SetDeviceScaleFactor(float device_scale_factor) override;
   void SetFrameTreeNodeId(int frame_tree_node_id) override;
   void SetForceEnableZoom(bool enabled) override;
-  cc::TouchAction AllowedTouchAction() override;
+  base::Optional<cc::TouchAction> AllowedTouchAction() override;
   void BindHost(mojom::WidgetInputHandlerHostRequest request) override;
 
   // InputHandlerHost impl
-  void CancelTouchTimeout() override;
+  void SetTouchActionFromMain(cc::TouchAction touch_action) override;
   void SetWhiteListedTouchAction(cc::TouchAction touch_action,
                                  uint32_t unique_touch_event_id,
                                  InputEventAckState state) override;
