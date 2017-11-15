ca0c13a8b74db73e44ca4af4291392e01af67cd0
Matt Menke <mmenke@chromium.org>
2017-11-15 17:46:15 -0500

    Add a DataElement::TYPE_RAW_FILE type that contains a file handle.
    This will eventually need to reaplce TYPE_FILE (Which just contains a
    path) in order to sandbox the network service process.
    
    Bug: 777879
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I8c67e828288443cbfc15aa774c1b97aa2f22ef29

--- content/public/common/resource_request_body.h
+++ content/public/common/resource_request_body.h
@@ -10,6 +10,8 @@
 #include <string>
 #include <vector>
 
+#include "base/files/file.h"
+#include "base/files/file_path.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "build/build_config.h"
@@ -50,10 +52,18 @@ class CONTENT_EXPORT ResourceRequestBody
 #endif
 
   void AppendBytes(const char* bytes, int bytes_len);
+  // TODO(mmenke): Remove this in favor of AppendFile.
   void AppendFileRange(const base::FilePath& file_path,
                        uint64_t offset,
                        uint64_t length,
                        const base::Time& expected_modification_time);
+  // Appends the specified part of |file|. Provided range must be valid, and may
+  // not extend beyond the end of the file.
+  void AppendRawFileRange(base::File file,
+                          const base::FilePath& file_path,
+                          uint64_t offset,
+                          uint64_t length,
+                          const base::Time& expected_modification_time);
 
   void AppendBlob(const std::string& uuid);
   void AppendFileSystemFileRange(const GURL& url,
