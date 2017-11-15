9dc9477c509ea63b84b267ff076c10b728d3f10b
dpapad <dpapad@chromium.org>
2017-11-15 12:27:14 -0800

    Settings WebUI: Preserve reset_profile_dialog state across invocations.
    This fixes a crash where the user could request a profile reset while one was
    already in progress.
    
    Bug:616089
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ia99f6ceea4d6f3c1ba797ba47affc42804c3fd5f

--- chrome/browser/resources/settings/reset_page/reset_profile_dialog.js
+++ chrome/browser/resources/settings/reset_page/reset_profile_dialog.js
@@ -41,7 +41,7 @@ Polymer({
     },
   },
 
-  /** @private {!settings.ResetBrowserProxy} */
+  /** @private {?settings.ResetBrowserProxy} */
   browserProxy_: null,
 
   /**
@@ -85,8 +85,7 @@ Polymer({
     });
   },
 
-  /** @private */
-  showDialog_: function() {
+  show: function() {
     this.$.dialog.showModal();
     this.browserProxy_.onShowResetProfileDialog();
   },
@@ -99,7 +98,6 @@ Polymer({
       this.browserProxy_.getTriggeredResetToolName().then(name => {
         this.resetRequestOrigin_ = 'triggeredreset';
         this.triggeredResetToolName_ = name;
-        this.showDialog_();
       });
     } else {
       // For the non-triggered reset dialog, a '#cct' hash indicates that the
@@ -109,7 +107,6 @@ Polymer({
           'cct' :
           settings.getQueryParameters().get('origin');
       this.resetRequestOrigin_ = origin || '';
-      this.showDialog_();
     }
   },
 
