cf25479e9f28b4786e5041c4c6f60b4e8ce537d2
Troy Hildebrandt <thildebr@chromium.org>
2017-11-16 11:47:47 -0800

    Use the partner bookmarks favicon cache in PartnerBookmarksReader.
    Hooks up the PartnerBookmarksCache in PartnerBookmarksReader to store
    the URL of pages that failed to retrieve a favicon from a server to
    throttle server requests in future partner bookmark loading attempts.
    
    Adds a bunch of new logic to the cache, to keep clutter out of
    PartnerBookmarksReader and improve testability.
    
    Bug:778805,780042
    Change-Id: I00ccc2d5a51d17f679178578be418226cec23c36

--- chrome/android/java/src/org/chromium/chrome/browser/partnerbookmarks/PartnerBookmarksReader.java
+++ chrome/android/java/src/org/chromium/chrome/browser/partnerbookmarks/PartnerBookmarksReader.java
@@ -13,8 +13,6 @@ import org.chromium.base.Log;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.browser.AppHooks;
-import org.chromium.chrome.browser.ChromeVersionInfo;
-import org.chromium.chrome.browser.ntp.snippets.FaviconFetchResult;
 
 import java.util.HashSet;
 import java.util.LinkedHashMap;
@@ -37,6 +35,9 @@ public class PartnerBookmarksReader {
 
     /** ID used to indicate an invalid bookmark node. */
     static final long INVALID_BOOKMARK_ID = -1;
+
+    /** Cache for storing failed favicon retrieval attempts, for throttling future requests. **/
+    private final PartnerBookmarksFaviconThrottle mFaviconThrottle;
 
     // JNI c++ pointer
     private long mNativePartnerBookmarksReader;
@@ -73,6 +74,7 @@ public class PartnerBookmarksReader {
     public PartnerBookmarksReader(Context context) {
         mContext = context;
         mNativePartnerBookmarksReader = nativeInit();
+        mFaviconThrottle = new PartnerBookmarksFaviconThrottle(context);
         initializeAndDisableEditingIfNecessary();
     }
 
@@ -102,7 +104,10 @@ public class PartnerBookmarksReader {
         FetchFaviconCallback callback = new FetchFaviconCallback() {
             @Override
             public void onFaviconFetched(@FaviconFetchResult int result) {
+                RecordHistogram.recordEnumeratedHistogram("PartnerBookmark.FaviconFetchResult",
+                        result, FaviconFetchResult.UMA_BOUNDARY);
                 synchronized (mProgressLock) {
+                    mFaviconThrottle.onFaviconFetched(url, result);
                     --mNumFaviconsInProgress;
                     if (mNumFaviconsInProgress == 0 && mFinishedReading) {
                         shutDown();
@@ -121,7 +126,7 @@ public class PartnerBookmarksReader {
         // to retrieve favicons so we don't retry too often.
         return nativeAddPartnerBookmark(mNativePartnerBookmarksReader, url, title, isFolder,
                 parentId, favicon, touchicon,
-                ChromeVersionInfo.isCanaryBuild() /* fetchUncachedFaviconsFromServer */, callback);
+                mFaviconThrottle.shouldFetchFromServerIfNecessary(url), callback);
     }
 
     /**
@@ -145,6 +150,7 @@ public class PartnerBookmarksReader {
             nativePartnerBookmarksCreationComplete(mNativePartnerBookmarksReader);
             nativeDestroy(mNativePartnerBookmarksReader);
             mNativePartnerBookmarksReader = 0;
+            mFaviconThrottle.commit();
             mShutDown = true;
         }
     }
