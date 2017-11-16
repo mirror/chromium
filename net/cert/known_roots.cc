da67e68eef830cdbfedafa2da48c6d9488119597
Ryan Sleevi <rsleevi@chromium.org>
2017-11-16 15:48:42 -0500

    Record per-verification and per-request histograms on trust anchor usage
    To better assess compatibility risks of Web Platform changes around
    trusted CAs, and to asses the interoperability pains due to differences
    in per-platform trust behaviours, collect metrics around various trust
    anchors usage.
    
    Because of how the Web PKI works, in which an existing trust anchor (CA)
    may sign a new trust anchor, and a given platform may support old,
    old+new, or new, the full union of all platforms is considered in
    histogramming. This allows for effective distinction between
    certificates that 'old' may have issued from those 'new' have issued,
    whether it is a single organization with multiple certificates or
    distinct organizations with distinct roots.
    
    As this list does not change frequently, and is used for histogram
    purposes, a static list of the root store is checked in, along with a
    static copy of a generated file based on that. Scripts are included for
    updating and maintaining the generated files and histograms.
    
    Measurements are done per-verification and per-request. If only
    per-verification metrics were measured, it would undercount the user
    impact due to a popular CA having their trust status changed. If only
    per-request metrics were measured, usage counts would be strongly biased
    towards popular CAs and not be useful in assessing performance impact on
    users for per-CA policies.
    
    BUG=none
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I5c34abc9b7aaef446d767fbe355d4dc29e07c6a7

---
+++ net/cert/known_roots.cc
@@ -0,0 +1,45 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/cert/known_roots.h"
+
+#include <algorithm>
+#include <cstring>
+
+#include "net/base/hash_value.h"
+#include "net/cert/root_cert_list_generated.h"
+
+namespace net {
+
+namespace {
+
+// Comparator-predicate that serves as a < function for comparing a
+// RootCertData to a HashValue
+struct HashValueToRootCertDataComp {
+  bool operator()(const HashValue& hash, const RootCertData& root_cert) {
+    DCHECK_EQ(HASH_VALUE_SHA256, hash.tag);
+    return memcmp(hash.data(), root_cert.sha256_spki_hash, 32) < 0;
+  }
+
+  bool operator()(const RootCertData& root_cert, const HashValue& hash) {
+    DCHECK_EQ(HASH_VALUE_SHA256, hash.tag);
+    return memcmp(root_cert.sha256_spki_hash, hash.data(), 32) < 0;
+  }
+};
+
+}  // namespace
+
+int32_t GetNetRootCertHistogramIdForHashValue(const HashValue& hash) {
+  if (hash.tag != HASH_VALUE_SHA256)
+    return 0;
+
+  auto* it = std::lower_bound(std::begin(kRootCerts), std::end(kRootCerts),
+                              hash, HashValueToRootCertDataComp());
+  return (it != std::end(kRootCerts) &&
+          !HashValueToRootCertDataComp()(hash, *it))
+             ? it->histogram_id
+             : 0;
+}
+
+}  // namespace net
