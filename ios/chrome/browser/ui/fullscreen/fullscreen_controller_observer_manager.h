eda8732822ba1a6ec82043258bf41c88df04edb9
Kurt Horimoto <kkhorimoto@chromium.org>
2017-11-15 21:39:20 -0800

    [iOS] Add preliminary fullscreen clases.
    This CL adds some base classes around which Fullscreen implementation
    will be built.  This includes:
    - FullscreenController and its observer class
    - FullscreenUIElement and its updater
    - FullscreenScrollEndAnimator
    
    Bug: 778016
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I11439513f4e52515bfee382ccd41cfe531c931a3

---
+++ ios/chrome/browser/ui/fullscreen/fullscreen_controller_observer_manager.h
@@ -0,0 +1,57 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_FULLSCREEN_FULLSCREEN_CONTROLLER_OBSERVER_MANAGER_H_
+#define IOS_CHROME_BROWSER_UI_FULLSCREEN_FULLSCREEN_CONTROLLER_OBSERVER_MANAGER_H_
+
+#include <memory>
+
+#include "base/macros.h"
+#include "base/observer_list.h"
+#import "ios/chrome/browser/ui/fullscreen/fullscreen_model_observer.h"
+#import "ios/chrome/browser/ui/fullscreen/fullscreen_scroll_end_animation_controller.h"
+
+class FullscreenController;
+class FullscreenControllerObserver;
+
+// A helper object that listens to FullscreenModel changes and forwards this
+// information to FullscreenControllerObservers.
+class FullscreenControllerObserverManager : public FullscreenModelObserver {
+ public:
+  FullscreenControllerObserverManager(FullscreenController* controller,
+                                      FullscreenModel* model);
+  ~FullscreenControllerObserverManager() override;
+
+  // Instructs the manager to forward changes from |model| to the
+  // FullscreenControllerObservers.
+  void SetModel(FullscreenModel* model);
+
+  // Adds and removes FullscreenControllerObservers.
+  void AddObserver(FullscreenControllerObserver* observer) {
+    observers_.AddObserver(observer);
+  }
+  void RemoveObserver(FullscreenControllerObserver* observer) {
+    observers_.RemoveObserver(observer);
+  }
+
+ private:
+  // FullscreenModelObserver:
+  void FullscreenModelProgressUpdated(FullscreenModel* model) override;
+  void FullscreenModelEnabledStateChanged(FullscreenModel* model) override;
+  void FullscreenModelScrollEventEnded(FullscreenModel* model) override;
+
+  // The controller.
+  FullscreenController* controller_ = nullptr;
+  // The model.
+  FullscreenModel* model_ = nullptr;
+  // The FullscreenControllerObservers that need to get notified of model
+  // changes.
+  base::ObserverList<FullscreenControllerObserver> observers_;
+  // The animation controller to use for scroll end animations.
+  FullscreenScrollEndAnimationController animation_controller_;
+
+  DISALLOW_COPY_AND_ASSIGN(FullscreenControllerObserverManager);
+};
+
+#endif  // IOS_CHROME_BROWSER_UI_FULLSCREEN_FULLSCREEN_CONTROLLER_OBSERVER_MANAGER_H_
