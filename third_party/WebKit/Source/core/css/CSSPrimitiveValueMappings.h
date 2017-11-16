75656d2b4a814021c18dcb146773a657e12bb342
zhuoyu.qian <zhuoyu.qian@samsung.com>
2017-11-16 09:47:26 +0800

    Make SnapAlignment and SnapAxis to be enum class.
    This patch makes SnapAlignment and SnapAxis to be enum class for
    better type safety.
    
    Bug: 684966
    
    Signed-off-by: zhuoyu.qian <zhuoyu.qian@samsung.com>
    Change-Id: Ied4b9620ff39c426cac362336a904be0865bf725

--- third_party/WebKit/Source/core/css/CSSPrimitiveValueMappings.h
+++ third_party/WebKit/Source/core/css/CSSPrimitiveValueMappings.h
@@ -1815,19 +1815,19 @@ template <>
 inline CSSIdentifierValue::CSSIdentifierValue(SnapAxis axis)
     : CSSValue(kIdentifierClass) {
   switch (axis) {
-    case kSnapAxisX:
+    case kX:
       value_id_ = CSSValueX;
       break;
-    case kSnapAxisY:
+    case kY:
       value_id_ = CSSValueY;
       break;
-    case kSnapAxisBlock:
+    case kBlock:
       value_id_ = CSSValueBlock;
       break;
-    case kSnapAxisInline:
+    case kInline:
       value_id_ = CSSValueInline;
       break;
-    case kSnapAxisBoth:
+    case kBoth:
       value_id_ = CSSValueBoth;
       break;
   }
@@ -1837,20 +1837,20 @@ template <>
 inline SnapAxis CSSIdentifierValue::ConvertTo() const {
   switch (GetValueID()) {
     case CSSValueX:
-      return kSnapAxisX;
+      return kX;
     case CSSValueY:
-      return kSnapAxisY;
+      return kY;
     case CSSValueBlock:
-      return kSnapAxisBlock;
+      return kBlock;
     case CSSValueInline:
-      return kSnapAxisInline;
+      return kInline;
     case CSSValueBoth:
-      return kSnapAxisBoth;
+      return kBoth;
     default:
       break;
   }
   NOTREACHED();
-  return kSnapAxisBoth;
+  return kBoth;
 }
 
 template <>
@@ -1884,16 +1884,16 @@ template <>
 inline CSSIdentifierValue::CSSIdentifierValue(SnapAlignment alignment)
     : CSSValue(kIdentifierClass) {
   switch (alignment) {
-    case kSnapAlignmentNone:
+    case SnapAlignment::kNone:
       value_id_ = CSSValueNone;
       break;
-    case kSnapAlignmentStart:
+    case SnapAlignment::kStart:
       value_id_ = CSSValueStart;
       break;
-    case kSnapAlignmentEnd:
+    case SnapAlignment::kEnd:
       value_id_ = CSSValueEnd;
       break;
-    case kSnapAlignmentCenter:
+    case SnapAlignment::kCenter:
       value_id_ = CSSValueCenter;
       break;
   }
@@ -1903,18 +1903,18 @@ template <>
 inline SnapAlignment CSSIdentifierValue::ConvertTo() const {
   switch (GetValueID()) {
     case CSSValueNone:
-      return kSnapAlignmentNone;
+      return SnapAlignment::kNone;
     case CSSValueStart:
-      return kSnapAlignmentStart;
+      return SnapAlignment::kStart;
     case CSSValueEnd:
-      return kSnapAlignmentEnd;
+      return SnapAlignment::kEnd;
     case CSSValueCenter:
-      return kSnapAlignmentCenter;
+      return SnapAlignment::kCenter;
     default:
       break;
   }
   NOTREACHED();
-  return kSnapAlignmentNone;
+  return SnapAlignment::kNone;
 }
 
 template <>
