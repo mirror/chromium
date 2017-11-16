c1c6ad3a5e88fbc58723bf26aaccc67dc60dfde5
Jay Civelli <jcivelli@google.com>
2017-11-16 00:19:32 -0800

    Adding a SafeXMLParser to the data-decoder service.
    Introducing a new XML parser service that parses XML into a
    base::DictionaryValue. It does so by JSONifying the XML content.
    
    Also factoring out TestServiceManagerListener to its own file, now that
    it is used in 2 places (safe_json_parser_browsertest.cc and
    safe_xml_parser_browsertest.cc).
    
    Bug: 784667
    Change-Id: I1f41f44d49910da43837aac8bca52e24533460ef

---
+++ services/data_decoder/xml_parser_unittest.cc
@@ -0,0 +1,188 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+
+#include "base/bind.h"
+#include "base/json/json_reader.h"
+#include "base/message_loop/message_loop.h"
+#include "services/data_decoder/xml_parser.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace data_decoder {
+
+namespace {
+
+void TestParseXmlCallback(std::unique_ptr<base::Value>* value_out,
+                          base::Optional<std::string>* error_out,
+                          std::unique_ptr<base::Value> value,
+                          const base::Optional<std::string>& error) {
+  *value_out = std::move(value);
+  *error_out = error;
+}
+
+// Parses the passed in |xml| and compares the result to |json|.
+// If |json| is empty, the parsing is expected to fail.
+void TestParseXml(const std::string& xml, const std::string& json) {
+  XmlParser parser_impl(/*service_ref=*/nullptr);
+  // Use a reference to mojom::XmlParser as XmlParser implements the interface
+  // privately.
+  mojom::XmlParser& parser = parser_impl;
+
+  std::unique_ptr<base::Value> actual_value;
+  base::Optional<std::string> error;
+  parser.Parse(xml, base::Bind(&TestParseXmlCallback, &actual_value, &error));
+  if (json.empty()) {
+    EXPECT_TRUE(error);
+    EXPECT_FALSE(actual_value);
+    return;
+  }
+
+  EXPECT_FALSE(error) << "Unexpected error: " << *error;
+  EXPECT_TRUE(actual_value);
+
+  std::unique_ptr<base::Value> expected_value = base::JSONReader::Read(json);
+  DCHECK(expected_value) << "Bad test, incorrect JSON: " << json;
+
+  EXPECT_EQ(*expected_value, *actual_value);
+}
+
+}  // namespace
+
+using XmlParserTest = testing::Test;
+
+TEST_F(XmlParserTest, ParseBadXml) {
+  std::string invalid_xml_strings[] = {"",
+                                       "  ",
+                                       "Awesome possum",
+                                       "[\"json\", \"or\", \"xml?\"]",
+                                       "<unbalanced>",
+                                       "<hello>bad tag</goodbye>"};
+  for (auto& xml : invalid_xml_strings)
+    TestParseXml(xml, "");
+}
+
+TEST_F(XmlParserTest, ParseSelfClosingTag) {
+  TestParseXml("<a/>", "{\"tag\":\"a\"}");
+  TestParseXml("<a><b/></a>", "{\"tag\":\"a\",\"children\":[{\"tag\":\"b\"}]}");
+  TestParseXml("<a><b/><b/><b/></a>",
+               "{\"tag\":\"a\",\"children\":["
+               "   {\"tag\":\"b\"},{\"tag\":\"b\"},{\"tag\":\"b\"}]}");
+}
+
+TEST_F(XmlParserTest, ParseEmptyTag) {
+  TestParseXml("<a></a>", "{\"tag\":\"a\"}");
+  TestParseXml("<a><b></b></a>",
+               "{\"tag\":\"a\",\"children\":[{\"tag\":\"b\"}]}");
+  TestParseXml("<a><b></b><b></b></a>",
+               "{\"tag\":\"a\",\"children\":["
+               "  {\"tag\":\"b\"},{\"tag\":\"b\"}]}");
+}
+
+TEST_F(XmlParserTest, ParseTextElement) {
+  TestParseXml("<hello>bonjour</hello>",
+               "{\"tag\":\"hello\",\"text\":\"bonjour\"}");
+}
+
+TEST_F(XmlParserTest, ParseMultipleSimilarTextElement) {
+  TestParseXml("<hello><fr>bonjour</fr><fr>salut</fr><fr>coucou</fr></hello>",
+               "{\"tag\":\"hello\",\"children\":["
+               "  {\"tag\":\"fr\",\"text\":\"bonjour\"},"
+               "  {\"tag\":\"fr\",\"text\":\"salut\"},"
+               "  {\"tag\":\"fr\",\"text\":\"coucou\"}]}");
+}
+
+TEST_F(XmlParserTest, ParseMixMatchTextNonTextElement) {
+  TestParseXml(
+      "<hello>"
+      "  <fr>coucou</fr>"
+      "  <fr><proper>bonjour</proper><slang>salut</slang></fr>"
+      "   <fr>ca va</fr>"
+      "</hello>",
+      "{\"tag\":\"hello\","
+      " \"children\":["
+      "    {\"tag\":\"fr\",\"text\":\"coucou\"},"
+      "    {\"tag\":\"fr\",\"children\":["
+      "        {\"tag\":\"proper\",\"text\":\"bonjour\"},"
+      "        {\"tag\":\"slang\",\"text\":\"salut\"}]},"
+      "    {\"tag\":\"fr\",\"text\":\"ca va\"}]}");
+}
+
+TEST_F(XmlParserTest, ParseNestedXml) {
+  TestParseXml(
+      "<M><a><t><r><y><o><s><h><k><a>Zdravstvuy"
+      "</a></k></h></s></o></y></r></t></a></M>",
+      "{\"tag\":\"M\",\"children\":[{\"tag\":\"a\",\"children\":["
+      "  {\"tag\":\"t\",\"children\":[{\"tag\":\"r\",\"children\":["
+      "    {\"tag\":\"y\",\"children\":[{\"tag\":\"o\",\"children\":["
+      "      {\"tag\":\"s\",\"children\":[{\"tag\":\"h\",\"children\":["
+      "        {\"tag\":\"k\",\"children\":[{\"tag\":\"a\","
+      "                                     \"text\":\"Zdravstvuy\"}]}"
+      "      ]}]}"
+      "    ]}]}"
+      "  ]}]}"
+      "]}]}");
+}
+
+TEST_F(XmlParserTest, ParseMultipleSimilarElements) {
+  TestParseXml("<a><b>b1</b><c>c1</c><b>b2</b><c>c2</c><b>b3</b><c>c3</c></a>",
+               "{\"tag\":\"a\",\"children\":["
+               "  {\"tag\":\"b\",\"text\":\"b1\"},"
+               "  {\"tag\":\"c\",\"text\":\"c1\"},"
+               "  {\"tag\":\"b\",\"text\":\"b2\"},"
+               "  {\"tag\":\"c\",\"text\":\"c2\"},"
+               "  {\"tag\":\"b\",\"text\":\"b3\"},"
+               "  {\"tag\":\"c\",\"text\":\"c3\"}"
+               "]}");
+}
+
+TEST_F(XmlParserTest, ParseTypicalXml) {
+  constexpr char kXml[] =
+      "<!-- This is an XML sample -->"
+      "<library>"
+      "  <book id=\"k123\">"
+      "    <author>Isaac Newton</author>"
+      "    <title>Philosophiae Naturalis Principia Mathematica</title>"
+      "    <genre>Science</genre>"
+      "    <price>40.95</price>"
+      "    <publish_date>1947-9-03</publish_date>"
+      "  </book>"
+      "  <book id=\"k456\">"
+      "    <author>Dr. Seuss</author>"
+      "    <title>Green Eggs and Ham</title>"
+      "    <genre>Kid</genre>"
+      "    <kids/>"
+      "    <price>4.95</price>"
+      "    <publish_date>1960-8-12</publish_date>"
+      "  </book>"
+      "</library>";
+
+  constexpr char kJson[] =
+      "{\"tag\": \"library\","
+      " \"children\":["
+      "  {\"tag\": \"book\","
+      "   \"children\": ["
+      "      {\"tag\": \"author\", \"text\": \"Isaac Newton\"},"
+      "      {\"tag\": \"title\","
+      "       \"text\": \"Philosophiae Naturalis Principia Mathematica\"},"
+      "      {\"tag\": \"genre\", \"text\": \"Science\"},"
+      "      {\"tag\": \"price\", \"text\": \"40.95\"},"
+      "      {\"tag\": \"publish_date\", \"text\": \"1947-9-03\"}"
+      "    ]"
+      "  },"
+      "  {\"tag\": \"book\","
+      "   \"children\": ["
+      "      {\"tag\": \"author\", \"text\": \"Dr. Seuss\"},"
+      "      {\"tag\": \"title\", \"text\": \"Green Eggs and Ham\"},"
+      "      {\"tag\": \"genre\", \"text\": \"Kid\"},"
+      "      {\"tag\": \"kids\"},"
+      "      {\"tag\": \"price\", \"text\": \"4.95\"},"
+      "      {\"tag\": \"publish_date\", \"text\": \"1960-8-12\"}"
+      "   ]"
+      "  }"
+      "]}";
+  TestParseXml(kXml, kJson);
+}
+
+}  // namespace data_decoder
