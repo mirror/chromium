e58e5de6f82cfb9fbef16f6b8a4772db884a90d3
Zhongyi Shi <zhongyi@chromium.org>
2017-11-16 02:00:56 -0800

    Add QuicConnectivityProbingManager which sends connectivity probing packet on new path. The manager will send out a new connectivity probing packet if there is no response from the server on the probed path. The manager will retry after a silent period from the previous sent event: 1 unit timeout, 1 unit timeout, 2 units timeout, 3 units timeout. 1 unit timeout is the smaller of 2 smoothed rtt from old path, or 300ms.
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I623b8a5aa8da6d553ca16d9f67655d4a32a4a1c9

---
+++ net/quic/chromium/quic_connectivity_probing_manager.h
@@ -0,0 +1,61 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_QUIC_CHROMIUM_QUIC_CONNECTIVITY_PROBING_MANAGER_H_
+#define NET_QUIC_CHROMIUM_QUIC_CONNECTIVITY_PROBING_MANAGER_H_
+
+#include "base/time/time.h"
+#include "net/base/net_export.h"
+#include "net/log/net_log_with_source.h"
+#include "net/quic/chromium/quic_chromium_client_session.h"
+#include "net/quic/chromium/quic_chromium_packet_reader.h"
+#include "net/quic/chromium/quic_chromium_packet_writer.h"
+#include "net/quic/core/quic_time.h"
+
+namespace net {
+
+// Responsible for sending and retransmitting connectivity probing packet on a
+// designated path to the specified peer, and for notifying associated session
+// when connectivity probe fails or succeeds.
+class QuicConnectivityProbingManager {
+ public:
+  QuicConnectivityProbingManager(QuicChromiumClientSession* session);
+  ~QuicConnectivityProbingManager();
+
+  void CancelProbe();
+
+  void StartProbeNetwork(NetworkChangeNotifier::NetworkHandle network,
+                         const QuicSocketAddress& peer_address,
+                         std::unique_ptr<DatagramClientSocket> socket,
+                         std::unique_ptr<QuicChromiumPacketWriter> writer,
+                         std::unique_ptr<QuicChromiumPacketReader> reader,
+                         int initial_timeout);
+
+  void OnConnectivityProbingReceived(const QuicSocketAddress& self_address,
+                                     const QuicSocketAddress& peer_address);
+
+ private:
+  void SendConnectivityProbingPacket(int timeout);
+
+  void MaybeResendConnectivityProbingPacket();
+  QuicChromiumClientSession* session_;
+
+  NetworkChangeNotifier::NetworkHandle network_;
+  QuicSocketAddress peer_address_;
+
+  std::unique_ptr<DatagramClientSocket> socket_;
+  std::unique_ptr<QuicChromiumPacketWriter> writer_;
+  std::unique_ptr<QuicChromiumPacketReader> reader_;
+
+  int retry_count_;
+  int initial_timeout_;
+  base::OneShotTimer retransmit_timer_;
+
+  base::WeakPtrFactory<QuicConnectivityProbingManager> weak_factory_;
+  DISALLOW_COPY_AND_ASSIGN(QuicConnectivityProbingManager);
+};
+
+}  // namespace net
+
+#endif  // NET_QUIC_CHROMIUM_QUIC_CONNECTIVITY_PROBING_MANAGER_H_
