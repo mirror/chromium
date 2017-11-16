3edb824ca666ce681360f4186f37bb2eceaecfee
Charles Vazac <cvazac@akamai.com>
2017-11-16 16:49:24 -0600

    Update Server Timing header parsing in accordance with spec change
    See https://www.w3.org/TR/server-timing/#the-server-timing-header-field
    Parameters for a metric in the Server-Timing header are now be named.
    Old:
    Server-Timing: db = 123; "database access"
    
    New:
    Server-Timing: db; dur=123; desc="database access"
    
    Bug: 702760
    Change-Id: I3f3060dc685ba699475e8c55ba46ec5a55849d8e

--- third_party/WebKit/Source/devtools/front_end/sdk/ServerTiming.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/ServerTiming.js
@@ -25,36 +25,137 @@ SDK.ServerTiming = class {
     if (!rawServerTimingHeaders.length)
       return null;
 
-    /**
-     * @param {?string} valueString
-     * @return {?Array<!SDK.ServerTiming>}
-     */
-    function createFromHeaderValue(valueString) {
-      // https://www.w3.org/TR/server-timing/
-      var serverTimingMetricRegExp =
-          /[ \t]*([\!\#\$\%\&\'\*\+\-\.\^\_\`\|\~0-9A-Za-z]+)[ \t]*(?:=[ \t]*(\d+(?:\.\d+)?))?[ \t]*(?:;[ \t]*(?:"([^"]+)"|([\!\#\$\%\&\'\*\+\-\.\^\_\`\|\~0-9A-Za-z]+)))?[ \t]*(?:,(.*))?/;
-      var metricMatch;
-      var result = [];
-      while (valueString && (metricMatch = serverTimingMetricRegExp.exec(valueString))) {
-        var metric = metricMatch[1];
-        var value = metricMatch[2];
-        var description = metricMatch[3] || metricMatch[4];
-        if (value !== undefined)
-          value = Math.abs(parseFloat(metricMatch[2]));
-        valueString = metricMatch[5];  // comma delimited headers
-        if (value === undefined || isNaN(value))
-          value = null;
-        result.push(new SDK.ServerTiming(metric, value, description));
-      }
-      return result;
-    }
-
     var serverTimings = rawServerTimingHeaders.reduce((memo, header) => {
-      var timing = createFromHeaderValue(header.value);
-      Array.prototype.push.apply(memo, timing);
+      var timing = this.createFromHeaderValue(header.value);
+      Array.prototype.push.apply(memo, timing.map(function(entry) {
+        return {
+          name: entry.name,
+          duration: entry.hasOwnProperty('duration') ? entry.duration : null,
+          description: entry.hasOwnProperty('description') ? entry.description : '',
+        };
+      }));
       return memo;
     }, []);
     serverTimings.sort((a, b) => a.metric.toLowerCase().compareTo(b.metric.toLowerCase()));
     return serverTimings;
+  }
+
+  /**
+   * @param {?string} valueString
+   * @return {?Array<!Object>}
+   */
+  static createFromHeaderValue(valueString) {
+    function consumeChar(char) {
+      console.assert(char.length === 1);
+      if (valueString[0] === char) {
+        valueString = valueString.substring(1);
+        return true;
+      }
+      return false;
+    }
+    function skipChars(chars) {
+      while (valueString.length && chars.indexOf(valueString[0]) > -1)
+        valueString = valueString.substring(1);
+    }
+    function skipWhitespace() {
+      skipChars(' \t');
+    }
+    function consumeToken(obj) {
+      while (valueString.length && isToken(valueString[0]))
+        shift(obj);
+      return obj.hasOwnProperty('value');
+    }
+    function isToken(char) {
+      // https://tools.ietf.org/html/rfc7230#appendix-B
+      console.assert(char.length === 1);
+      switch (char) {
+        case '!':
+        case '#':
+        case '$':
+        case '%':
+        case '&':
+        case '\'':
+        case '*':
+        case '+':
+        case '-':
+        case '.':
+        case '^':
+        case '_':
+        case '`':
+        case '|':
+        case '~':
+          return true;
+
+        default:
+          var charCode = char.charCodeAt(0);
+          return (
+              (charCode >= 48 && charCode <= 57) || (charCode >= 97 && charCode <= 122) ||
+              (charCode >= 65 && charCode <= 90));
+      }
+    }
+    function consumeTokenOrQuotedString(obj) {
+      return consumeChar('"') ? consumeQuotedString(obj) : consumeToken(obj);
+    }
+    function consumeQuotedString(obj) {
+      while (valueString.length) {
+        if (valueString[0] === '"') {
+          valueString = valueString.substring(1);
+          return obj.hasOwnProperty('value');
+        }
+        if (valueString[0] === '\\') {
+          valueString = valueString.substring(1);
+          if (!valueString.length)
+            return false;
+        }
+        shift(obj);
+      }
+      return false;
+    }
+    function shift(obj) {
+      obj.value = obj.value || '';
+      obj.value += valueString[0];
+      valueString = valueString.substring(1);
+    }
+
+    var name, result = [];
+
+    while (skipChars(', \t'), name = {}, consumeToken(name)) {
+      skipWhitespace();
+      var entry = {name: name.value};
+
+      while (skipWhitespace(), consumeChar(';')) {
+        skipWhitespace();
+
+        var paramName;
+        if (paramName = {}, consumeToken(paramName)) {
+          skipWhitespace();
+
+          if (consumeChar('=')) {
+            skipWhitespace();
+
+            var paramValue = {};
+            if (consumeTokenOrQuotedString(paramValue)) {
+              paramName = paramName.value.toLowerCase();
+              switch (paramName) {
+                case 'dur':
+                  if (!entry.hasOwnProperty('duration')) {
+                    var duration = parseFloat(paramValue.value);
+                    if (!isNaN(duration))
+                      entry.duration = duration;
+                  }
+                  break;
+                case 'desc':
+                  if (!entry.hasOwnProperty('description'))
+                    entry.description = paramValue.value;
+                  break;
+              }
+            }
+          }
+        }
+      }
+
+      result.push(entry);
+    }
+    return result;
   }
 };
