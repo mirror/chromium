cd75ea5f025fc5949bf664b6adffb62d2720df98
David Roger <droger@chromium.org>
2017-11-16 13:09:11 +0100

    [signin] Split Dice and Mirror specific logic out of AccountReconcilor
    This CL creates a AccountReconcilorDelegate class, with two subclasses
    specialized for Mirror and Dice respectively.
    
    Bug: 777774
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I4bdb4ecee7d4f12e910a2704a09802c383bca9bb

---
+++ components/signin/core/browser/account_reconcilor_delegate.h
@@ -0,0 +1,48 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SIGNIN_CORE_BROWSER_ACCOUNT_RECONCILOR_DELEGATE_H_
+#define COMPONENTS_SIGNIN_CORE_BROWSER_ACCOUNT_RECONCILOR_DELEGATE_H_
+
+#include <string>
+#include <vector>
+
+#include "google_apis/gaia/gaia_auth_util.h"
+
+namespace signin {
+
+// Base class for AccountReconcilorDelegate.
+class AccountReconcilorDelegate {
+ public:
+  virtual ~AccountReconcilorDelegate() {}
+
+  // Defaults to false.
+  virtual bool IsEnabled() const;
+
+  // Returns true if account consistency is enforced (Mirror or Dice).
+  // If this is false, reconcile is done, but its results are discarded and no
+  // changes to the accounts are made. Defaults to false.
+  virtual bool IsAccountConsistencyEnforced() const;
+
+  // Returns true if Reconcile should be aborted when the primary account is in
+  // error state. Defaults to false.
+  virtual bool AbortReconcileIfPrimaryHasError() const;
+
+  // Returns the first account to add in the Gaia cookie.
+  // If this returns an empty string, the user must be logged out of all
+  // accounts.
+  virtual std::string GetFirstGaiaAccountForReconcile(
+      const std::vector<std::string>& chrome_accounts,
+      const std::vector<gaia::ListedAccount>& gaia_accounts,
+      const std::string& primary_account,
+      bool first_execution) const;
+
+  // Called when reconcile is finished.
+  virtual void OnReconcileFinished(const std::string& first_account,
+                                   bool reconcile_is_noop) {}
+};
+
+}  // namespace signin
+
+#endif  // COMPONENTS_SIGNIN_CORE_BROWSER_ACCOUNT_RECONCILOR_DELEGATE_H_
