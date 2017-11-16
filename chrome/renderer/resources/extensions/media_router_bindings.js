94b087c81ac78f486b6ed2a114860835d577efbf
Takumi Fujimoto <takumif@chromium.org>
2017-11-16 10:45:45 -0800

    [Media Router] Use const refs in struct traits
    This CL makes some IssueInfo and MediaRoute attributes optional so that their struct traits can return const refs to those attributes instead of creating new optional objects.
    
    Bug:785099
    Change-Id: Idb47e048f6e99aad164cb403e0b25e045801f48b

--- chrome/renderer/resources/extensions/media_router_bindings.js
+++ chrome/renderer/resources/extensions/media_router_bindings.js
@@ -396,7 +396,7 @@ define('media_router_bindings', [
       'route_id': issue.routeId,
       'severity': issueSeverityToMojo_(issue.severity),
       'title': issue.title,
-      'message': issue.message,
+      'message': issue.message || '',
       'default_action': issueActionToMojo_(issue.defaultAction),
       'secondary_actions': secondaryActions,
       'help_page_id': issue.helpPageId,
