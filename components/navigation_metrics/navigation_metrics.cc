aad6a813f2b79656fc62da9dc9086c7294a141ba
Avi Drissman <avi@chromium.org>
2017-11-16 19:27:09 -0500

    Measure the schemes of alerting pages.
    BUG=696454
    
    Change-Id: I7ea839d6c9135c639b1a8a68843721142d4bf81c

--- components/navigation_metrics/navigation_metrics.cc
+++ components/navigation_metrics/navigation_metrics.cc
@@ -9,32 +9,9 @@
 #include "components/dom_distiller/core/url_constants.h"
 #include "url/gurl.h"
 
-namespace {
+namespace navigation_metrics {
 
-// These values are written to logs. New enum values can be added, but existing
-// enums must never be renumbered or deleted and reused. Any new scheme should
-// be added at the end, before SCHEME_MAX.
-enum Scheme {
-  SCHEME_UNKNOWN = 0,
-  SCHEME_HTTP = 1,
-  SCHEME_HTTPS = 2,
-  SCHEME_FILE = 3,
-  SCHEME_FTP = 4,
-  SCHEME_DATA = 5,
-  SCHEME_JAVASCRIPT = 6,
-  SCHEME_ABOUT = 7,
-  SCHEME_CHROME = 8,
-  SCHEME_BLOB = 9,
-  SCHEME_FILESYSTEM = 10,
-  SCHEME_CHROME_NATIVE = 11,
-  SCHEME_CHROME_SEARCH = 12,
-  SCHEME_CHROME_DISTILLER = 13,
-  SCHEME_CHROME_DEVTOOLS = 14,
-  SCHEME_CHROME_EXTENSION = 15,
-  SCHEME_VIEW_SOURCE = 16,
-  SCHEME_EXTERNALFILE = 17,
-  SCHEME_MAX,
-};
+namespace {
 
 const char* const kSchemeNames[] = {
     "unknown",
@@ -60,17 +37,15 @@ const char* const kSchemeNames[] = {
 static_assert(arraysize(kSchemeNames) == SCHEME_MAX,
               "kSchemeNames should have SCHEME_MAX elements");
 
+}  // namespace
+
 Scheme GetScheme(const GURL& url) {
-  for (int i = 1; i < SCHEME_MAX; ++i) {
+  for (int i = SCHEME_UNKNOWN + 1; i < SCHEME_MAX; ++i) {
     if (url.SchemeIs(kSchemeNames[i]))
       return static_cast<Scheme>(i);
   }
   return SCHEME_UNKNOWN;
 }
-
-}  // namespace
-
-namespace navigation_metrics {
 
 void RecordMainFrameNavigation(const GURL& url,
                                bool is_same_document,
--- components/navigation_metrics/navigation_metrics.h
+++ components/navigation_metrics/navigation_metrics.h
@@ -9,6 +9,36 @@ class GURL;
 
 namespace navigation_metrics {
 
+// A Scheme is an C++ enum type loggable in UMA for a histogram of UMA enum type
+// NavigationScheme.
+//
+// These values are written to logs. New enum values can be added, but existing
+// value must never be renumbered or deleted and reused. Any new scheme must
+// be added at the end, before SCHEME_MAX.
+enum Scheme : int {
+  SCHEME_UNKNOWN = 0,
+  SCHEME_HTTP = 1,
+  SCHEME_HTTPS = 2,
+  SCHEME_FILE = 3,
+  SCHEME_FTP = 4,
+  SCHEME_DATA = 5,
+  SCHEME_JAVASCRIPT = 6,
+  SCHEME_ABOUT = 7,
+  SCHEME_CHROME = 8,
+  SCHEME_BLOB = 9,
+  SCHEME_FILESYSTEM = 10,
+  SCHEME_CHROME_NATIVE = 11,
+  SCHEME_CHROME_SEARCH = 12,
+  SCHEME_CHROME_DISTILLER = 13,
+  SCHEME_CHROME_DEVTOOLS = 14,
+  SCHEME_CHROME_EXTENSION = 15,
+  SCHEME_VIEW_SOURCE = 16,
+  SCHEME_EXTERNALFILE = 17,
+  SCHEME_MAX,
+};
+
+Scheme GetScheme(const GURL& url);
+
 void RecordMainFrameNavigation(const GURL& url,
                                bool is_same_document,
                                bool is_off_the_record);
