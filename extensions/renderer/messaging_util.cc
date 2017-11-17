1a9c67f2a2add2a7492d0ad0aa2e3eda3de987ec
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-11-17 09:53:57 -0800

    [Extensions Bindings] Throw access errors on restricted sendRequest
    We throw errors when certain extensions (unpacked with an event page)
    try to use the deprecated sendRequest-related properties of the
    chrome.extension API. Implement this for native bindings. In order to
    do this, add an InitializeInstance() method on native binding hooks
    that allows hooks to modify the newly-created instance of an API for a
    specific context.
    
    Add unittests for both InitializeInstance and the restriction of
    sendRequest properties.
    
    Bug: 653596
    
    Change-Id: I22be1f0f9d4f299e127b21084649000bab63ddfa

--- extensions/renderer/messaging_util.cc
+++ extensions/renderer/messaging_util.cc
@@ -10,6 +10,8 @@
 #include "base/strings/stringprintf.h"
 #include "extensions/common/api/messaging/message.h"
 #include "extensions/common/extension.h"
+#include "extensions/common/manifest.h"
+#include "extensions/common/manifest_handlers/background_info.h"
 #include "extensions/renderer/script_context.h"
 #include "gin/converter.h"
 #include "gin/dictionary.h"
@@ -260,6 +262,12 @@ void MassageSendMessageArguments(
     *arguments_out = {target_id, message, options, response_callback};
   else
     *arguments_out = {target_id, message, response_callback};
+}
+
+bool IsSendRequestDisabled(ScriptContext* script_context) {
+  const Extension* extension = script_context->extension();
+  return extension && Manifest::IsUnpackedLocation(extension->location()) &&
+         BackgroundInfo::HasLazyBackgroundPage(extension);
 }
 
 }  // namespace messaging_util
