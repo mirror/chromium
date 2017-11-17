e0da64bd82f14f721495eebf82bff4c823d26e24
Aga Wronska <agawronska@chromium.org>
2017-11-17 14:47:18 -0800

    Return fixed storage from StorageMonitor::GetAllAvaialableStorages().
    Fetch information about all devices from OS in the DiskMountManger.
    Expose is_virtual property of disk to ignore virtual block devices.
    (Only information auto mountable devices and fixed storage is needed)
    
    Split disk changes callback into separate callbacks for auto mountable
    and fixed storage disks, so clients that are only interested
    in auto mountable devices do not need to do additional filtering.
    
    Consume fixed storage disk events in StorageMonitorChromeos.
    
    Bug: 420633
    Change-Id: I3380c1a0ff49d7167760633cc3ed30d151170a08

--- components/storage_monitor/storage_monitor_chromeos.cc
+++ components/storage_monitor/storage_monitor_chromeos.cc
@@ -78,11 +78,27 @@ bool GetDeviceInfo(const DiskMountManager::MountPointInfo& mount_info,
   return true;
 }
 
+// Returns true if the requested device is valid, else false. On success, fills
+// in |info| for fixed storage device.
+bool GetFixedStorageInfo(const DiskMountManager::Disk& disk,
+                         StorageInfo* info) {
+  DCHECK(info);
+
+  std::string unique_id = MakeDeviceUniqueId(disk);
+  if (unique_id.empty())
+    return false;
+
+  *info = StorageInfo(
+      StorageInfo::MakeDeviceId(StorageInfo::FIXED_MASS_STORAGE, unique_id),
+      disk.mount_path(), base::UTF8ToUTF16(disk.device_label()),
+      base::UTF8ToUTF16(disk.vendor_name()),
+      base::UTF8ToUTF16(disk.product_name()), disk.total_size_in_bytes());
+  return true;
+}
+
 }  // namespace
 
-StorageMonitorCros::StorageMonitorCros()
-    : weak_ptr_factory_(this) {
-}
+StorageMonitorCros::StorageMonitorCros() : weak_ptr_factory_(this) {}
 
 StorageMonitorCros::~StorageMonitorCros() {
   DiskMountManager* manager = DiskMountManager::GetInstance();
@@ -134,8 +150,59 @@ void StorageMonitorCros::CheckExistingMountPoints() {
                  weak_ptr_factory_.GetWeakPtr()));
 }
 
+void StorageMonitorCros::OnAutoMountableDiskEvent(
+    DiskMountManager::DiskEvent event,
+    const DiskMountManager::Disk* disk) {}
+
 void StorageMonitorCros::OnDiskEvent(DiskMountManager::DiskEvent event,
-                                     const DiskMountManager::Disk* disk) {}
+                                     const DiskMountManager::Disk* disk) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+
+  StorageInfo info;
+  if (!GetFixedStorageInfo(*disk, &info)) {
+    return;
+  }
+
+  switch (event) {
+    case DiskMountManager::DiskEvent::DISK_ADDED: {
+      const auto result =
+          mount_map_.insert(std::make_pair(disk->mount_path(), info));
+      DCHECK(result.second);
+      receiver()->ProcessAttach(info);
+      break;
+    }
+    case DiskMountManager::DiskEvent::DISK_CHANGED: {
+      for (auto it = mount_map_.cbegin(); it != mount_map_.end();) {
+        if (it->second.device_id() == info.device_id()) {
+          receiver()->ProcessDetach((info.device_id()));
+          mount_map_.erase(it++);
+          const auto result =
+              mount_map_.insert(std::make_pair(disk->mount_path(), info));
+          DCHECK(result.second);
+          receiver()->ProcessAttach(info);
+          break;
+        } else {
+          ++it;
+        }
+      }
+      break;
+    }
+    case DiskMountManager::DiskEvent::DISK_REMOVED: {
+      for (auto it = mount_map_.cbegin(); it != mount_map_.end();) {
+        if (it->second.device_id() == info.device_id()) {
+          receiver()->ProcessDetach((info.device_id()));
+          mount_map_.erase(it++);
+          break;
+        } else {
+          ++it;
+        }
+      }
+      break;
+    }
+    default:
+      NOTREACHED();
+  }
+}
 
 void StorageMonitorCros::OnDeviceEvent(DiskMountManager::DeviceEvent event,
                                        const std::string& device_path) {}
@@ -194,7 +261,6 @@ void StorageMonitorCros::SetMediaTransferProtocolManagerForTest(
   DCHECK(!media_transfer_protocol_manager_);
   media_transfer_protocol_manager_.reset(test_manager);
 }
-
 
 bool StorageMonitorCros::GetStorageInfoForPath(
     const base::FilePath& path,
