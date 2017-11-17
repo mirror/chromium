fdd3b757848c64dde7842779ef56a697461e1be5
Zhongyi Shi <zhongyi@chromium.org>
2017-11-17 00:47:20 -0800

    Add QuicConnectivityProbingManager which sends connectivity probing packet on new path. The manager will send out a new connectivity probing packet if there is no response from the server on the probed path. The manager will retry after a silent period from the previous sent event: 1 unit timeout, 1 unit timeout, 2 units timeout, 3 units timeout. 1 unit timeout is the smaller of 2 smoothed rtt from old path, or 300ms.
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I623b8a5aa8da6d553ca16d9f67655d4a32a4a1c9

---
+++ net/quic/chromium/quic_connectivity_probing_manager.cc
@@ -0,0 +1,112 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/quic/chromium/quic_connectivity_probing_manager.h"
+
+namespace net {
+
+namespace {
+
+// Default to 2 seconds timeout as the maximum timeout.
+const int64_t kMaxProbingTimeoutMs = 2000;
+
+}  // namespace
+
+QuicConnectivityProbingManager::QuicConnectivityProbingManager(
+    Delegate* delegate)
+    : delegate_(delegate),
+      retry_count_(0),
+      initial_timeout_ms_(0),
+      weak_factory_(this) {}
+
+QuicConnectivityProbingManager::~QuicConnectivityProbingManager() {
+  CancelProbing();
+}
+
+void QuicConnectivityProbingManager::CancelProbing() {
+  network_ = NetworkChangeNotifier::kInvalidNetworkHandle;
+  peer_address_ = QuicSocketAddress();
+  socket_.reset();
+  writer_.reset();
+  reader_.reset();
+  retry_count_ = 0;
+  initial_timeout_ms_ = 0;
+  retransmit_timer_.Stop();
+}
+
+void QuicConnectivityProbingManager::StartProbing(
+    NetworkChangeNotifier::NetworkHandle network,
+    const QuicSocketAddress& peer_address,
+    std::unique_ptr<DatagramClientSocket> socket,
+    std::unique_ptr<QuicChromiumPacketWriter> writer,
+    std::unique_ptr<QuicChromiumPacketReader> reader,
+    base::TimeDelta initial_timeout) {
+  // Start a new probe will always cancel the previous one.
+  CancelProbing();
+
+  network_ = network;
+  peer_address_ = peer_address;
+  socket_ = std::move(socket);
+  writer_ = std::move(writer);
+  reader_ = std::move(reader);
+  initial_timeout_ms_ = initial_timeout.InMilliseconds();
+
+  reader_->StartReading();
+  SendConnectivityProbingPacket(initial_timeout);
+}
+
+void QuicConnectivityProbingManager::OnConnectivityProbingReceived(
+    const QuicSocketAddress& self_address,
+    const QuicSocketAddress& peer_address) {
+  if (!socket_) {
+    DVLOG(1) << "Probing response is ignored as probing was cancelled "
+             << "or succeeded.";
+    return;
+  }
+
+  IPEndPoint local_address;
+  socket_->GetLocalAddress(&local_address);
+
+  DVLOG(1) << "Current probing is live at self ip:port "
+           << local_address.ToString() << ", to peer ip:port "
+           << peer_address_.ToString();
+
+  if (QuicSocketAddressImpl(local_address) != self_address.impl() ||
+      peer_address_ != peer_address) {
+    DVLOG(1) << "Received probing response from peer ip:port "
+             << peer_address.ToString() << ", to self ip:port "
+             << self_address.ToString() << ". Ignored.";
+    return;
+  }
+
+  // TODO(zhongyi): add metrics collection.
+  // Notify the delegate that the probe succeeds and reset everything.
+  delegate_->OnProbeNetworkSucceeded(network_, self_address, std::move(socket_),
+                                     std::move(writer_), std::move(reader_));
+  CancelProbing();
+}
+
+void QuicConnectivityProbingManager::SendConnectivityProbingPacket(
+    base::TimeDelta timeout) {
+  delegate_->OnSendConnectivityProbingPacket(writer_.get(), peer_address_);
+  retransmit_timer_.Start(
+      FROM_HERE, timeout,
+      base::Bind(
+          &QuicConnectivityProbingManager::MaybeResendConnectivityProbingPacket,
+          weak_factory_.GetWeakPtr()));
+}
+
+void QuicConnectivityProbingManager::MaybeResendConnectivityProbingPacket() {
+  // Use exponential backoff for the timeout
+  retry_count_++;
+  int64_t timeout_ms = (1 << retry_count_) * initial_timeout_ms_;
+  if (timeout_ms > kMaxProbingTimeoutMs) {
+    delegate_->OnProbeNetworkFailed(network_);
+    CancelProbing();
+    return;
+  }
+  SendConnectivityProbingPacket(base::TimeDelta::FromMilliseconds(timeout_ms));
+}
+
+}  // namespace net
