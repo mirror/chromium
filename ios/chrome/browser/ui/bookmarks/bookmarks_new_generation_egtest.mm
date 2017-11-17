8e7014169dfa265fc07536b4a0762a80c6b04536
Jérôme Lebel <jlebel@chromium.org>
2017-11-17 14:53:16 +0100

    [iOS][Signin] Moving all the sign-in promo view preferences to SigninPromoViewMediator
    The SigninPromoViewMediator should be in charge of registering and using:
      - prefs::kIosBookmarkSigninPromoDisplayedCount
      - prefs::kIosBookmarkPromoAlreadySeen
    
    Also, once the sign-in promo view has been seen too much by the user, it should not set
    prefs::kIosBookmarkPromoAlreadySeen to true. kIosBookmarkPromoAlreadySeen should be used only for
    the close button. Some views could have a max number of views, without a close button.
    
    This work is to prepare the clean up for the sign-in promo view in the settings.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Id7f7cfc1146afbec552aaa159b628caac691d105

--- ios/chrome/browser/ui/bookmarks/bookmarks_new_generation_egtest.mm
+++ ios/chrome/browser/ui/bookmarks/bookmarks_new_generation_egtest.mm
@@ -2004,8 +2004,6 @@ id<GREYMatcher> TappableBookmarkNodeWithLabel(NSString* label) {
   // Check the sign-in promo view is visible.
   [SigninEarlGreyUtils
       checkSigninPromoVisibleWithMode:SigninPromoViewModeColdState];
-  // Check the sign-in promo will not be shown anymore.
-  [BookmarksNewGenTestCase verifyPromoAlreadySeen:YES];
   GREYAssertEqual(
       20, prefs->GetInteger(prefs::kIosBookmarkSigninPromoDisplayedCount),
       @"Should have incremented the display count");
