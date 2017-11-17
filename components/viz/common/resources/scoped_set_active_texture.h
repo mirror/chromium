093c28a89a37a17ae75befd8bf5635f747b247b3
Xu Xing <xing.xu@intel.com>
2017-11-17 22:50:25 +0800

    [viz] Implement local resource in GLRenderer
    Local resource is resource used inside the same GL context and will not being sent
    into another process, no need to create fence and mailbox for these resources.
    For RenderPass in render_pass_textures_, it uses texture-backed resources, no need
    to support GpuMemoryBuffer.
    
    Currently there are three path may possible set is_overlay_ to true:
    ResourceProvider::CreateGpuMemoryBufferResource
    LayerTreeResourceProvider::CreateResourceFromTextureMailbox
    ResourceProvider::CreateGpuTextureResource (Used by GLRenderer::overlay_resource_pool_).
    Resource for RenderPass in render_pass_textures_ is not created by above three paths.
    Based on this, remove the support of overlay.
    
    Some opens:
    1), Better name ScopedLocalResource as RenderPassTexture?
    2), Where to place ScopedSetActiveTexture? gl_helper.h? Or put it into a standalone
    file under components/viz/common/resources? Currently this is duplicated.
    
    BUG=782044
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ia334842b12b8297ae4a768979adb8b5a468b906a

---
+++ components/viz/common/resources/scoped_set_active_texture.h
@@ -0,0 +1,47 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_VIZ_COMMON_RESOURCES_SCOPED_SET_ACTIVE_TEXTURE_H_
+#define COMPONENTS_VIZ_COMMON_RESOURCES_SCOPED_SET_ACTIVE_TEXTURE_H_
+
+#include <memory>
+
+#include "components/viz/common/viz_common_export.h"
+#include "gpu/command_buffer/client/gles2_interface.h"
+
+using gpu::gles2::GLES2Interface;
+
+namespace viz {
+
+static GLint GetActiveTextureUnit(GLES2Interface* gl) {
+  GLint active_unit = 0;
+  gl->GetIntegerv(GL_ACTIVE_TEXTURE, &active_unit);
+  return active_unit;
+}
+
+class VIZ_COMMON_EXPORT ScopedSetActiveTexture {
+ public:
+  ScopedSetActiveTexture(GLES2Interface* gl, GLenum unit)
+      : gl_(gl), unit_(unit) {
+    DCHECK_EQ(GL_TEXTURE0, GetActiveTextureUnit(gl_));
+
+    if (unit_ != GL_TEXTURE0)
+      gl_->ActiveTexture(unit_);
+  }
+
+  ~ScopedSetActiveTexture() {
+    // Active unit being GL_TEXTURE0 is effectively the ground state.
+    if (unit_ != GL_TEXTURE0)
+      gl_->ActiveTexture(GL_TEXTURE0);
+  }
+
+ private:
+  GLES2Interface* gl_;
+  GLenum unit_;
+  DISALLOW_COPY_AND_ASSIGN(ScopedSetActiveTexture);
+};
+
+}  // namespace viz
+
+#endif  // COMPONENTS_VIZ_COMMON_RESOURCES_SCOPED_SET_ACTIVE_TEXTURE_H_
