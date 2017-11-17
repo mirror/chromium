b670a6eb9261ee16bc940fe133a7171aef39672f
gogerald <gogerald@google.com>
2017-11-17 14:03:09 -0500

    [Payments] Implements payment_request_payment_app_browsertest,
    Makes service_worker_payment_app_factory singleton
    to set TestDownloader for testing,
    
    Bug:786116
    Change-Id: I263edd64e7452a81f07011f1a7bda83e8b57c90e

--- chrome/browser/ui/views/payments/payment_request_payment_app_browsertest.cc
+++ chrome/browser/ui/views/payments/payment_request_payment_app_browsertest.cc
@@ -3,27 +3,198 @@
 // found in the LICENSE file.
 
 #include "base/macros.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/payments/payment_request_browsertest_base.h"
+#include "chrome/browser/ui/views/payments/payment_request_dialog_view_ids.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "components/network_session_configurator/common/network_switches.h"
+#include "components/payments/content/service_worker_payment_app_factory.h"
+#include "components/payments/core/test_payment_manifest_downloader.h"
+#include "content/public/browser/browser_context.h"
+#include "content/public/browser/storage_partition.h"
+#include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
+#include "net/dns/mock_host_resolver.h"
+#include "net/url_request/url_request_context_getter.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace payments {
 
 class PaymentRequestPaymentAppTest : public PaymentRequestBrowserTestBase {
  protected:
-  PaymentRequestPaymentAppTest() {}
+  PaymentRequestPaymentAppTest()
+      : alicepay_(net::EmbeddedTestServer::TYPE_HTTPS),
+        bobpay_(net::EmbeddedTestServer::TYPE_HTTPS),
+        frankpay_(net::EmbeddedTestServer::TYPE_HTTPS) {
+    scoped_feature_list_.InitAndEnableFeature(
+        features::kServiceWorkerPaymentApps);
+    ServiceWorkerPaymentAppFactory::GetInstance()
+        ->IgnorePortInAppScopeForTesting();
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    // HTTPS server only serves a valid cert for localhost, so this is needed to
+    // load pages from the test servers with custom hostnames without an
+    // interstitial.
+    command_line->AppendSwitch(switches::kIgnoreCertificateErrors);
+  }
+
+  // Starts the test severs and opens a test page on alicepay.com.
+  void SetUpOnMainThread() override {
+    PaymentRequestBrowserTestBase::SetUpOnMainThread();
+
+    ASSERT_TRUE(StartTestServer("alicepay.com", &alicepay_));
+    ASSERT_TRUE(StartTestServer("bobpay.com", &bobpay_));
+    ASSERT_TRUE(StartTestServer("frankpay.com", &frankpay_));
+  }
+
+  // Invokes the JavaScript function install(|method_name|) in
+  // components/test/data/payments/alicepay.com/app1/index.js, which responds
+  // back via domAutomationController.
+  void InstallPaymentAppForMethod(const std::string& method_name) {
+    ui_test_utils::NavigateToURL(browser(),
+                                 alicepay_.GetURL("alicepay.com", "/app1/"));
+    std::string contents;
+    std::string script = "install('" + method_name + "');";
+    ASSERT_TRUE(content::ExecuteScriptAndExtractString(
+        browser()->tab_strip_model()->GetActiveWebContents(), script,
+        &contents))
+        << "Script execution failed: " << script;
+    ASSERT_NE(std::string::npos,
+              contents.find("Payment app for \"" + method_name +
+                            "\" method installed."))
+        << method_name << " method install message not found in:\n"
+        << contents;
+  }
+
+  // Sets a TestDownloader for alicepay.com, bobpay.com and frankpay.com to
+  // ServiceWorkerPaymentAppFactory.
+  void SetPaymentMethodManifestDownloaderForTesting() {
+    content::BrowserContext* context = browser()
+                                           ->tab_strip_model()
+                                           ->GetActiveWebContents()
+                                           ->GetBrowserContext();
+    auto downloader = std::make_unique<TestDownloader>(
+        content::BrowserContext::GetDefaultStoragePartition(context)
+            ->GetURLRequestContext());
+    downloader->AddTestServerURL("https://alicepay.com/",
+                                 alicepay_.GetURL("alicepay.com", "/"));
+    downloader->AddTestServerURL("https://bobpay.com/",
+                                 bobpay_.GetURL("bobpay.com", "/"));
+    downloader->AddTestServerURL("https://frankpay.com/",
+                                 frankpay_.GetURL("frankpay.com", "/"));
+    ServiceWorkerPaymentAppFactory::GetInstance()
+        ->SetPaymentMethodManifestDownloaderForTesting(std::move(downloader));
+  }
 
  private:
+  // Starts the |test_server| for |hostname|. Returns true on success.
+  bool StartTestServer(const std::string& hostname,
+                       net::EmbeddedTestServer* test_server) {
+    host_resolver()->AddRule(hostname, "127.0.0.1");
+    if (!test_server->InitializeAndListen())
+      return false;
+    test_server->ServeFilesFromSourceDirectory(
+        "components/test/data/payments/" + hostname);
+
+    test_server->StartAcceptingConnections();
+    return true;
+  }
+
+  // https://alicepay.com hosts the payment app.
+  net::EmbeddedTestServer alicepay_;
+
+  // https://bobpay.com/webpay does not permit any other origin to use this
+  // payment method.
+  net::EmbeddedTestServer bobpay_;
+
+  // https://frankpay.com/webpay supports payment apps from any origin.
+  net::EmbeddedTestServer frankpay_;
+
+  base::test::ScopedFeatureList scoped_feature_list_;
+
   DISALLOW_COPY_AND_ASSIGN(PaymentRequestPaymentAppTest);
 };
 
-IN_PROC_BROWSER_TEST_F(PaymentRequestPaymentAppTest, ShowNotSupportedError) {
+// Test payment request methods are not supported by the payment app.
+IN_PROC_BROWSER_TEST_F(PaymentRequestPaymentAppTest, NotSupportedError) {
+  InstallPaymentAppForMethod("https://frankpay.com");
+
+  SetPaymentMethodManifestDownloaderForTesting();
+
   NavigateTo("/payment_request_bobpay_test.html");
+
+  ResetEventObserverForSequence({DialogEvent::CAN_MAKE_PAYMENT_CALLED,
+                                 DialogEvent::CAN_MAKE_PAYMENT_RETURNED});
+  ASSERT_TRUE(
+      content::ExecuteScript(GetActiveWebContents(), "canMakePayment();"));
+  WaitForObservedEvent();
+  ExpectBodyContains({"false"});
+
   ResetEventObserver(DialogEvent::NOT_SUPPORTED_ERROR);
   ASSERT_TRUE(content::ExecuteScript(GetActiveWebContents(), "buy();"));
   WaitForObservedEvent();
   ExpectBodyContains({"NotSupportedError"});
+}
+
+// Test CanMakePayment and payment request can be fullfiled.
+IN_PROC_BROWSER_TEST_F(PaymentRequestPaymentAppTest, PayWithAlicePay) {
+  InstallPaymentAppForMethod("https://alicepay.com");
+
+  SetPaymentMethodManifestDownloaderForTesting();
+
+  NavigateTo("/payment_request_bobpay_test.html");
+
+  ResetEventObserverForSequence({DialogEvent::CAN_MAKE_PAYMENT_CALLED,
+                                 DialogEvent::CAN_MAKE_PAYMENT_RETURNED});
+  ASSERT_TRUE(
+      content::ExecuteScript(GetActiveWebContents(), "canMakePayment();"));
+  WaitForObservedEvent();
+  ExpectBodyContains({"true"});
+
+  InvokePaymentRequestUI();
+
+  ResetEventObserver(DialogEvent::DIALOG_CLOSED);
+  ClickOnDialogViewAndWait(DialogViewID::PAY_BUTTON, dialog_view());
+  ExpectBodyContains({"https://alicepay.com"});
+}
+
+// Test https://bobpay.com can not be used by https://alicepay.com
+IN_PROC_BROWSER_TEST_F(PaymentRequestPaymentAppTest, CanNotPayWithBobPay) {
+  InstallPaymentAppForMethod("https://bobpay.com");
+
+  SetPaymentMethodManifestDownloaderForTesting();
+
+  NavigateTo("/payment_request_bobpay_test.html");
+
+  ResetEventObserverForSequence({DialogEvent::CAN_MAKE_PAYMENT_CALLED,
+                                 DialogEvent::CAN_MAKE_PAYMENT_RETURNED});
+  ASSERT_TRUE(
+      content::ExecuteScript(GetActiveWebContents(), "canMakePayment();"));
+  WaitForObservedEvent();
+  ExpectBodyContains({"false"});
+
+  ResetEventObserver(DialogEvent::NOT_SUPPORTED_ERROR);
+  ASSERT_TRUE(content::ExecuteScript(GetActiveWebContents(), "buy();"));
+  WaitForObservedEvent();
+  ExpectBodyContains({"NotSupportedError"});
+}
+
+// Test can pay with 'basic-card' payment method from alicepay.
+IN_PROC_BROWSER_TEST_F(PaymentRequestPaymentAppTest, PayWithBasicCard) {
+  InstallPaymentAppForMethod("basic-card");
+
+  SetPaymentMethodManifestDownloaderForTesting();
+
+  NavigateTo("/payment_request_bobpay_and_basic_card_with_modifiers_test.html");
+  InvokePaymentRequestUI();
+
+  ResetEventObserver(DialogEvent::DIALOG_CLOSED);
+  ClickOnDialogViewAndWait(DialogViewID::PAY_BUTTON, dialog_view());
+  ExpectBodyContains({"basic-card"});
 }
 
 }  // namespace payments
