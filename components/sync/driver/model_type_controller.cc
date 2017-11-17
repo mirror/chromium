38ebfd1e2365bb2f589ad3bc30aaf6414010e52f
Gang Wu <gangwu@chromium.org>
2017-11-17 13:46:08 -0800

    [Sync] change BridgeTask from Callback to OnceCallback.
    change ModelTypeController::BridgeTask from Callback to OnceCallback.
    
    Bug:786478
    Change-Id: I5d559aafefa3917b5cde5e06f398078989b3bc10

--- components/sync/driver/model_type_controller.cc
+++ components/sync/driver/model_type_controller.cc
@@ -52,11 +52,9 @@ base::WeakPtr<ModelTypeSyncBridge> ReturnCapturedBridge(
   return arg;
 }
 
-void RunBridgeTask(const BridgeProvider& bridge_provider,
-                   const BridgeTask& task) {
-  if (base::WeakPtr<ModelTypeSyncBridge> bridge = bridge_provider.Run()) {
-    task.Run(bridge.get());
-  }
+void RunBridgeTask(BridgeProvider bridge_provider, BridgeTask task) {
+  base::WeakPtr<ModelTypeSyncBridge> bridge = std::move(bridge_provider).Run();
+  std::move(task).Run(bridge.get());
 }
 
 }  // namespace
@@ -263,10 +261,11 @@ BridgeProvider ModelTypeController::GetBridgeProvider() {
 }
 
 void ModelTypeController::PostBridgeTask(const base::Location& location,
-                                         const BridgeTask& task) {
+                                         BridgeTask task) {
   DCHECK(model_thread_);
   model_thread_->PostTask(
-      location, base::Bind(&RunBridgeTask, GetBridgeProvider(), task));
+      location, base::Bind(&RunBridgeTask, base::Passed(GetBridgeProvider()),
+                           base::Passed(std::move(task))));
 }
 
 }  // namespace syncer
