c63d6edada972d9113690befe4b69b23b6560443
Tibor Goldschwendt <tiborg@chromium.org>
2017-11-17 11:44:38 -0500

    [vr] Propagate content DPR changes to compositor
    VR changes the content DPR based on the content quad's projected size.
    Propagate those changes to the compositor properly. Also, defer commits
    to the compositor until the desired content resolution and DPR is known.
    
    Bug: 785934
    Change-Id: I81b74669567dd74a505d2672055672b78b31b725

--- content/public/browser/android/compositor.h
+++ content/public/browser/android/compositor.h
@@ -68,6 +68,13 @@ class CONTENT_EXPORT Compositor {
   // Set the output surface bounds.
   virtual void SetWindowBounds(const gfx::Size& size) = 0;
 
+  // Set the painted device's scale factor used by the layer tree host.
+  virtual void SetPaintedDeviceScaleFactor(
+      float painted_device_scale_factor) = 0;
+
+  // Defer commits on the layer tree host.
+  virtual void SetDeferCommits(bool defer_commits) = 0;
+
   // Set the output surface which the compositor renders into.
   virtual void SetSurface(jobject surface) = 0;
 
