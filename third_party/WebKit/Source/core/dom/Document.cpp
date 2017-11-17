f9c4764744719e5479d8382f46c02d5d69679600
Hajime Hoshi <hajimehoshi@chromium.org>
2017-11-17 21:00:37 +0900

    Use the terms Pause/Unpause instead of Suspend/Resume in Blink
    Bug: 780378
    Change-Id: Iac3e165e9721dc9fbe4b1771f8a85e89b98e0056

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -6406,7 +6406,7 @@ void Document::TasksWerePaused() {
   GetScriptRunner()->Suspend();
 
   if (parser_)
-    parser_->SuspendScheduledTasks();
+    parser_->PauseScheduledTasks();
   if (scripted_animation_controller_)
     scripted_animation_controller_->Pause();
 }
@@ -6415,7 +6415,7 @@ void Document::TasksWereUnpaused() {
   GetScriptRunner()->Resume();
 
   if (parser_)
-    parser_->ResumeScheduledTasks();
+    parser_->UnpauseScheduledTasks();
   if (scripted_animation_controller_)
     scripted_animation_controller_->Unpause();
 
