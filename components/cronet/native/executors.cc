23ff214e14ca47e837b6a89d61fac2ccf21e38d5
Misha Efimov <mef@chromium.org>
2017-11-17 17:43:43 -0500

    [Cronet] Implement Executors to run native UrlRequest callbacks.
    Bug:786559
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I22fca33a029b507811eb5f8a3afe8ec1cfc0c439

---
+++ components/cronet/native/executors.cc
@@ -0,0 +1,128 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/cronet/native/executors.h"
+
+#include "base/logging.h"
+#include "base/macros.h"
+#include "base/task_scheduler/post_task.h"
+#include "base/task_scheduler/task_traits.h"
+#include "components/cronet/native/generated/cronet.idl_impl_interface.h"
+
+namespace {
+
+// Implementation of DirectExecutor methods.
+void DirectExecutor_Execute(Cronet_ExecutorPtr self,
+                            Cronet_RunnablePtr command) {
+  CHECK(self);
+  DVLOG(1) << "Running directly";
+  Cronet_Runnable_Run(command);
+  DVLOG(1) << "Destroying command";
+  Cronet_Runnable_Destroy(command);
+}
+
+// Implementation of PostTaskExecutor methods.
+void PostTaskExecutor_Execute(Cronet_ExecutorPtr self,
+                              Cronet_RunnablePtr command) {
+  CHECK(self);
+  DVLOG(1) << "Post Task";
+  base::PostTask(FROM_HERE, base::BindOnce(
+                                [](Cronet_RunnablePtr command) {
+                                  DVLOG(1) << "Running posted task";
+                                  Cronet_Runnable_Run(command);
+                                  DVLOG(1) << "Destroying command";
+                                  Cronet_Runnable_Destroy(command);
+                                },
+                                command));
+}
+
+struct TaskRunnerExecutor : public Cronet_Executor {
+  TaskRunnerExecutor()
+      : task_runner_(CreateSequencedTaskRunnerWithTraits(base::TaskTraits())) {}
+  ~TaskRunnerExecutor() override = default;
+
+  void SetContext(Cronet_ExecutorContext context) override {
+    context_ = context;
+  }
+
+  Cronet_ExecutorContext GetContext() override { return context_; }
+
+  void Execute(Cronet_RunnablePtr command) override;
+
+ private:
+  scoped_refptr<base::SequencedTaskRunner> task_runner_;
+  Cronet_ExecutorContext context_ = nullptr;
+
+  DISALLOW_COPY_AND_ASSIGN(TaskRunnerExecutor);
+};
+
+// static
+void TaskRunnerExecutor::Execute(Cronet_RunnablePtr command) {
+  task_runner_->PostTask(FROM_HERE, base::BindOnce(
+                                        [](Cronet_RunnablePtr command) {
+                                          DVLOG(1) << "Running posted task";
+                                          Cronet_Runnable_Run(command);
+                                          DVLOG(1) << "Destroying command";
+                                          Cronet_Runnable_Destroy(command);
+                                        },
+                                        command));
+}
+
+class SequencedExecutor {
+ public:
+  static Cronet_ExecutorPtr Create();
+
+ private:
+  SequencedExecutor();
+  static void Execute(Cronet_ExecutorPtr self, Cronet_RunnablePtr command);
+
+  scoped_refptr<base::SequencedTaskRunner> task_runner_;
+  DISALLOW_COPY_AND_ASSIGN(SequencedExecutor);
+};
+
+SequencedExecutor::SequencedExecutor()
+    : task_runner_(CreateSequencedTaskRunnerWithTraits(base::TaskTraits())) {}
+
+// static
+Cronet_ExecutorPtr SequencedExecutor::Create() {
+  Cronet_ExecutorPtr executor = Cronet_Executor_CreateStub(Execute);
+  Cronet_Executor_SetContext(executor, new SequencedExecutor());
+  return executor;
+}
+
+// static
+void SequencedExecutor::Execute(Cronet_ExecutorPtr self,
+                                Cronet_RunnablePtr command) {
+  Cronet_ExecutorContext context = Cronet_Executor_GetContext(self);
+  SequencedExecutor* executor = static_cast<SequencedExecutor*>(context);
+  CHECK(self);
+  DVLOG(1) << "Post Task";
+  executor->task_runner_->PostTask(FROM_HERE,
+                                   base::BindOnce(
+                                       [](Cronet_RunnablePtr command) {
+                                         DVLOG(1) << "Running posted task";
+                                         Cronet_Runnable_Run(command);
+                                         DVLOG(1) << "Destroying command";
+                                         Cronet_Runnable_Destroy(command);
+                                       },
+                                       command));
+}
+
+}  // namespace
+
+Cronet_ExecutorPtr DirectExecutor_Create() {
+  return Cronet_Executor_CreateStub(DirectExecutor_Execute);
+}
+
+Cronet_ExecutorPtr PostTaskExecutor_Create() {
+  return Cronet_Executor_CreateStub(PostTaskExecutor_Execute);
+}
+
+Cronet_ExecutorPtr SequencedExecutor_Create() {
+  return SequencedExecutor::Create();
+}
+
+Cronet_ExecutorPtr TaskRunnerExecutor_Create() {
+  return new TaskRunnerExecutor();
+}
---
+++ components/cronet/native/executors_unittest.cc
@@ -0,0 +1,85 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/cronet/native/executors.h"
+
+#include "base/logging.h"
+#include "base/macros.h"
+#include "base/test/scoped_task_environment.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class ExecutorsTest : public ::testing::Test {
+ protected:
+  void SetUp() override {}
+
+  void TearDown() override {}
+
+  ExecutorsTest() = default;
+  ~ExecutorsTest() override {}
+
+ public:
+  bool Runnable_called_ = false;
+
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ExecutorsTest);
+};
+
+// Implementation of TestRunnable methods.
+void TestRunnable_Run(Cronet_RunnablePtr self) {
+  CHECK(self);
+  Cronet_RunnableContext context = Cronet_Runnable_GetContext(self);
+  ExecutorsTest* test = static_cast<ExecutorsTest*>(context);
+  CHECK(test);
+  test->Runnable_called_ = true;
+  DVLOG(1) << "Hello from TestRunnable_Run";
+}
+
+// Test that DirectExecutor runs the runnable.
+TEST_F(ExecutorsTest, TestDirect) {
+  ASSERT_FALSE(Runnable_called_);
+  Cronet_RunnablePtr runnable = Cronet_Runnable_CreateStub(TestRunnable_Run);
+  Cronet_Runnable_SetContext(runnable, this);
+  Cronet_ExecutorPtr executor = DirectExecutor_Create();
+  Cronet_Executor_Execute(executor, runnable);
+  Cronet_Executor_Destroy(executor);
+  ASSERT_TRUE(Runnable_called_);
+}
+
+// Test that PostTaskExecutor runs the runnable.
+TEST_F(ExecutorsTest, TestPostTaskExecutor) {
+  ASSERT_FALSE(Runnable_called_);
+  Cronet_RunnablePtr runnable = Cronet_Runnable_CreateStub(TestRunnable_Run);
+  Cronet_Runnable_SetContext(runnable, this);
+  Cronet_ExecutorPtr executor = PostTaskExecutor_Create();
+  Cronet_Executor_Execute(executor, runnable);
+  Cronet_Executor_Destroy(executor);
+  scoped_task_environment_.RunUntilIdle();
+  ASSERT_TRUE(Runnable_called_);
+}
+
+// Test that SequencedExecutor runs the runnable.
+TEST_F(ExecutorsTest, TestSequencedExecutor) {
+  ASSERT_FALSE(Runnable_called_);
+  Cronet_RunnablePtr runnable = Cronet_Runnable_CreateStub(TestRunnable_Run);
+  Cronet_Runnable_SetContext(runnable, this);
+  Cronet_ExecutorPtr executor = SequencedExecutor_Create();
+  Cronet_Executor_Execute(executor, runnable);
+  Cronet_Executor_Destroy(executor);
+  scoped_task_environment_.RunUntilIdle();
+  ASSERT_TRUE(Runnable_called_);
+}
+
+// Test that SequencedExecutor runs the runnable.
+TEST_F(ExecutorsTest, TestTaskRunnerExecutor) {
+  ASSERT_FALSE(Runnable_called_);
+  Cronet_RunnablePtr runnable = Cronet_Runnable_CreateStub(TestRunnable_Run);
+  Cronet_Runnable_SetContext(runnable, this);
+  Cronet_ExecutorPtr executor = TaskRunnerExecutor_Create();
+  Cronet_Executor_Execute(executor, runnable);
+  Cronet_Executor_Destroy(executor);
+  scoped_task_environment_.RunUntilIdle();
+  ASSERT_TRUE(Runnable_called_);
+}
