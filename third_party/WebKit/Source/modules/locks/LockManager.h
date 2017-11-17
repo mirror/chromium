8b1d1183c865880a832ce53744d9285127a36528
Joshua Bell <jsbell@chromium.org>
2017-11-17 12:37:11 -0800

    Web Locks API
    https://github.com/inexorabletash/web-locks
    
    Bug: 161072
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: If31fdff852f7e922a61d42687d813a61cb559dbe

---
+++ third_party/WebKit/Source/modules/locks/LockManager.h
@@ -0,0 +1,36 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LockManager_h
+#define LockManager_h
+
+#include "bindings/modules/v8/string_or_string_sequence.h"
+#include "modules/ModulesExport.h"
+#include "modules/locks/LockOptions.h"
+#include "platform/bindings/ScriptWrappable.h"
+
+namespace blink {
+
+class ScriptPromise;
+class ScriptState;
+class V8LockGrantedCallback;
+
+class LockManager final : public ScriptWrappable {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  ScriptPromise acquire(ScriptState*,
+                        const StringOrStringSequence& scope,
+                        V8LockGrantedCallback*,
+                        ExceptionState&);
+  ScriptPromise acquire(ScriptState*,
+                        const StringOrStringSequence& scope,
+                        const LockOptions&,
+                        V8LockGrantedCallback*,
+                        ExceptionState&);
+};
+
+}  // namespace blink
+
+#endif  // LockManager_h
