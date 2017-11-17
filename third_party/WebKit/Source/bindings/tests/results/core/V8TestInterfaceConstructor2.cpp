6ac01044a5b650cddd95ebd0c346bea8587ae545
Gyuyoung Kim <gyuyoung.kim@lge.com>
2017-11-17 15:22:37 +0900

    Change RefPtr.h include with scoped_refptr.h in bindings/ and public/ of blink
    All uses of RefPtr have been removed in blink. Now is time to remove RefPtr.h
    in blink. As a final step, this CL replaces RefPtr.h with scoped_refptr.h
    in bindings and public folders of blink. And this CL removes RefPtr.h and things
    regarding it finally.
    
    Bug: 763844
    Change-Id: I1caa17c6f20f51b192001efdd8096e38fcfad578

--- third_party/WebKit/Source/bindings/tests/results/core/V8TestInterfaceConstructor2.cpp
+++ third_party/WebKit/Source/bindings/tests/results/core/V8TestInterfaceConstructor2.cpp
@@ -20,7 +20,7 @@
 #include "core/frame/LocalDOMWindow.h"
 #include "platform/bindings/V8ObjectConstructor.h"
 #include "platform/wtf/GetPtr.h"
-#include "platform/wtf/RefPtr.h"
+#include "base/memory/scoped_refptr.h"
 
 namespace blink {
 
