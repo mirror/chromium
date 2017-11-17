0305a35c286a590a09d7987bab428bf0acb9095e
Ian Kilpatrick <ikilpatrick@chromium.org>
2017-11-17 15:08:57 -0800

    Add secure_context_mode_ to CSSParserContext.
    This is in order to allow the CSS Parser to treat the paint() function as
    invalid on insecure contexts.
    
    This doesn't add that functionality, just the flag to the context.
    I added a new enum for this to ensure type-safety, as this bool ends up next to
    a few other bools like "important".
    
    This shouldn't have any change in behaviour.
    
    Bug: 578252
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I42c05c9a204e84a021a331f74032a73499645a51

--- third_party/WebKit/Source/core/css/CSSPropertyValueSet.h
+++ third_party/WebKit/Source/core/css/CSSPropertyValueSet.h
@@ -39,6 +39,7 @@ class ImmutableCSSPropertyValueSet;
 class MutableCSSPropertyValueSet;
 class PropertyRegistry;
 class StyleSheetContents;
+enum class SecureContextMode;
 
 class CORE_EXPORT CSSPropertyValueSet
     : public GarbageCollectedFinalized<CSSPropertyValueSet> {
@@ -238,12 +239,14 @@ class CORE_EXPORT MutableCSSPropertyValueSet : public CSSPropertyValueSet {
   // These expand shorthand properties into multiple properties.
   SetResult SetProperty(CSSPropertyID unresolved_property,
                         const String& value,
-                        bool important = false,
+                        bool important,
+                        SecureContextMode,
                         StyleSheetContents* context_style_sheet = 0);
   SetResult SetProperty(const AtomicString& custom_property_name,
                         const PropertyRegistry*,
                         const String& value,
                         bool important,
+                        SecureContextMode,
                         StyleSheetContents* context_style_sheet,
                         bool is_animation_tainted);
   void SetProperty(CSSPropertyID, const CSSValue&, bool important = false);
@@ -264,6 +267,7 @@ class CORE_EXPORT MutableCSSPropertyValueSet : public CSSPropertyValueSet {
 
   void Clear();
   void ParseDeclarationList(const String& style_declaration,
+                            SecureContextMode,
                             StyleSheetContents* context_style_sheet);
 
   CSSStyleDeclaration* EnsureCSSStyleDeclaration();
