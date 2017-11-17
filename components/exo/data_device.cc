cb5042e4c7a3e6cb358e01a97cd8a42dfa470b99
Daichi Hirono <hirono@chromium.org>
2017-11-17 16:05:01 +0900

    Start sending selection event from DataDevice
    DataDevice sends selection event when:
    
     * the clipboard content is changed and the client has focus, or
     * the client newly obtained the keyboard focus
    
    Bug: 773978
    Change-Id: I7bda96adef5a9471667916307f80ae44a71ac04a

--- components/exo/data_device.cc
+++ components/exo/data_device.cc
@@ -7,21 +7,34 @@
 #include "base/logging.h"
 #include "components/exo/data_device_delegate.h"
 #include "components/exo/data_offer.h"
+#include "components/exo/seat.h"
 #include "components/exo/surface.h"
 #include "ui/base/clipboard/clipboard.h"
+#include "ui/base/clipboard/clipboard_monitor.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/base/dragdrop/drop_target_event.h"
 
 namespace exo {
 
-DataDevice::DataDevice(DataDeviceDelegate* delegate, FileHelper* file_helper)
-    : delegate_(delegate), file_helper_(file_helper) {
+DataDevice::DataDevice(DataDeviceDelegate* delegate,
+                       Seat* seat,
+                       FileHelper* file_helper)
+    : delegate_(delegate), seat_(seat), file_helper_(file_helper) {
   WMHelper::GetInstance()->AddDragDropObserver(this);
+  ui::ClipboardMonitor::GetInstance()->AddObserver(this);
+
+  seat_->AddObserver(this);
+
+  OnSurfacePreFocused(seat_->GetFocusedSurface());
 }
 
 DataDevice::~DataDevice() {
   delegate_->OnDataDeviceDestroying(this);
+
   WMHelper::GetInstance()->RemoveDragDropObserver(this);
+  ui::ClipboardMonitor::GetInstance()->RemoveObserver(this);
+
+  seat_->RemoveObserver(this);
 }
 
 void DataDevice::StartDrag(const DataSource* source_resource,
@@ -99,9 +112,40 @@ int DataDevice::OnPerformDrop(const ui::DropTargetEvent& event) {
   return ui::DragDropTypes::DRAG_NONE;
 }
 
+void DataDevice::OnClipboardDataChanged() {
+  if (!focused_surface_.get())
+    return;
+  SendSelection();
+}
+
+void DataDevice::OnSurfacePreFocused(Surface* surface) {
+  const bool current_focus =
+      surface && delegate_->CanAcceptDataEventsForSurface(surface);
+  const bool last_focus = focused_surface_.get();
+
+  // Check if the client newly obtained focus.
+  if (focused_surface_.get() == surface)
+    return;
+
+  if (current_focus) {
+    focused_surface_.reset(surface, this);
+    if (!last_focus)
+      SendSelection();
+  } else {
+    focused_surface_.reset();
+  }
+}
+
+void DataDevice::OnSurfaceFocused(Surface* surface) {}
+
 void DataDevice::OnDataOfferDestroying(DataOffer* data_offer) {
   if (data_offer_.get() == data_offer)
     data_offer_.reset();
+}
+
+void DataDevice::OnSurfaceDestroying(Surface* surface) {
+  if (focused_surface_.get() == surface)
+    focused_surface_.reset();
 }
 
 Surface* DataDevice::GetEffectiveTargetForEvent(
@@ -114,6 +158,12 @@ Surface* DataDevice::GetEffectiveTargetForEvent(
     return nullptr;
 
   return delegate_->CanAcceptDataEventsForSurface(target) ? target : nullptr;
+}
+
+void DataDevice::SendSelection() {
+  DataOffer* const data_offer = delegate_->OnDataOffer();
+  // TODO(hirono): Populate clipboard data to data offer.
+  delegate_->OnSelection(*data_offer);
 }
 
 }  // namespace exo
