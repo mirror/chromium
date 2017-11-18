c26aadccd730609526a8b1fffb0daf6c5bab644a
Miguel Casas <mcasas@chromium.org>
2017-11-17 19:30:29 -0500

    jda: move files from media/gpu to media/mojo
    This CL moves the Jpeg decode accelerator-related files from their
    locations in //media/gpu/ipc/{client,service} and //media/gpu/mojo
    into //media/mojo/{clients,interfaces,services}.
    
    This follows up on the TODOs in [1,2]. Most of the updates are fully
    automatic using tools/git/mass-rename.py, some others needed updating
    includes etc.
    
    
    Bug: 699255
    
    [1] https://cs.chromium.org/chromium/src/media/gpu/ipc/client/gpu_jpeg_decode_accelerator_host.h?dr&l=22
    [2] https://cs.chromium.org/chromium/src/media/gpu/ipc/client/gpu_jpeg_decode_accelerator_host.h?dr&l=22
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I0ed7f94f1bfc258756293cde6bdd228e6f0197c7

--- media/mojo/services/gpu_jpeg_decode_accelerator_factory_provider.h
+++ media/mojo/services/gpu_jpeg_decode_accelerator_factory_provider.h
@@ -2,10 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_GPU_IPC_SERVICE_GPU_JPEG_DECODE_ACCELERATOR_FACTORY_PROVIDER_H_
-#define MEDIA_GPU_IPC_SERVICE_GPU_JPEG_DECODE_ACCELERATOR_FACTORY_PROVIDER_H_
+#ifndef MEDIA_MOJO_SERVICES_GPU_JPEG_DECODE_ACCELERATOR_FACTORY_PROVIDER_H_
+#define MEDIA_MOJO_SERVICES_GPU_JPEG_DECODE_ACCELERATOR_FACTORY_PROVIDER_H_
 
 #include "base/memory/ref_counted.h"
+#include "media/mojo/services/media_mojo_export.h"
 #include "media/video/jpeg_decode_accelerator.h"
 
 namespace base {
@@ -14,7 +15,7 @@ class SingleThreadTaskRunner;
 
 namespace media {
 
-class GpuJpegDecodeAcceleratorFactoryProvider {
+class MEDIA_MOJO_EXPORT GpuJpegDecodeAcceleratorFactoryProvider {
  public:
   using CreateAcceleratorCB =
       base::Callback<std::unique_ptr<JpegDecodeAccelerator>(
@@ -29,4 +30,4 @@ class GpuJpegDecodeAcceleratorFactoryProvider {
 
 }  // namespace media
 
-#endif  // MEDIA_GPU_IPC_SERVICE_GPU_JPEG_DECODE_ACCELERATOR_FACTORY_PROVIDER_H_
+#endif  // MEDIA_MOJO_SERVICES_GPU_JPEG_DECODE_ACCELERATOR_FACTORY_PROVIDER_H_
