8e5fc917d2f565be0452516e75533b6cb4db5af4
Stuart Langley <slangley@chromium.org>
2017-11-20 09:57:35 +1100

    Convert clipboard_messages from IPC to Mojo.
    This CL converts all of the IPC messages in content/common/clipboard_messages.h
    to MOJO. This CL includes a number of changes as a result:
    
    - Remove the Dispatch of messages for OS_WIN on the IO thread. The requirement
      to do this seems to be inspired by deadlocks with NPAPI plugins, but as
      NPAPI is now persona non grata we can remove this and all the code that
      existed to support it.
    - interstitial page test was inserting a message_filter to intercept write calls
      to the clipboard - we replace this witl a ClipboardObserver and have the
      test_clipboard fire the event for observation. This also caused TSAN failures,
      so we can re-enable the cut test under TSAN now.
    - We remove the clipboard delegate entirely, and move some logic to write
      images to shared memory for transport to the impl file now.
    
    Bug: 775830, 631322
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I35c6238a41190b3bc82e0cfab7bbfe4325a3be21

--- content/renderer/renderer_blink_platform_impl.cc
+++ content/renderer/renderer_blink_platform_impl.cc
@@ -73,7 +73,6 @@
 #include "content/renderer/quota_dispatcher.h"
 #include "content/renderer/quota_message_filter.h"
 #include "content/renderer/render_thread_impl.h"
-#include "content/renderer/renderer_clipboard_delegate.h"
 #include "content/renderer/storage_util.h"
 #include "content/renderer/web_database_observer_impl.h"
 #include "content/renderer/webclipboard_impl.h"
@@ -277,8 +276,6 @@ RendererBlinkPlatformImpl::RendererBlinkPlatformImpl(
                             : nullptr),
       compositor_thread_(nullptr),
       main_thread_(renderer_scheduler->CreateMainThread()),
-      clipboard_delegate_(new RendererClipboardDelegate),
-      clipboard_(new WebClipboardImpl(clipboard_delegate_.get())),
       sudden_termination_disables_(0),
       plugin_refresh_allowed_(true),
       default_task_runner_(renderer_scheduler->DefaultTaskRunner()),
@@ -425,6 +422,9 @@ blink::WebClipboard* RendererBlinkPlatformImpl::Clipboard() {
       GetContentClient()->renderer()->OverrideWebClipboard();
   if (clipboard)
     return clipboard;
+  if (!clipboard_) {
+    clipboard_ = std::make_unique<WebClipboardImpl>(GetClipboardHost());
+  }
   return clipboard_.get();
 }
 
@@ -1426,6 +1426,13 @@ void RendererBlinkPlatformImpl::InitializeWebDatabaseHostIfNeeded() {
 blink::mojom::WebDatabaseHost& RendererBlinkPlatformImpl::GetWebDatabaseHost() {
   InitializeWebDatabaseHostIfNeeded();
   return **web_database_host_;
+}
+
+mojom::ClipboardHost& RendererBlinkPlatformImpl::GetClipboardHost() {
+  if (!clipboard_host_) {
+    GetConnector()->BindInterface(mojom::kBrowserServiceName, &clipboard_host_);
+  }
+  return *clipboard_host_;
 }
 
 }  // namespace content
