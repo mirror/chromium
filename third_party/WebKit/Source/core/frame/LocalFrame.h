eb1d506e2da48e3f761d3cba7920baa4e58d8d7e
Fadi Meawad <fmeawad@chromium.org>
2017-11-20 15:36:08 -0800

    [PageLifecycle] Call onpause when a frame is stopped
    As part of page lifecycle, we need to notify the page that it is getting
    stopped. This CL introduced window.onpause, a new callback that will
    fire when the page is stopped.
    
    Currently, the page is stopped on Android after being backgrounded for a
    few minutes. More stopping scenarios are being added. When a page is
    stopped, all the timers will stop firing, and any load activities will
    be halted. The onpause is the last callback made to the page before
    it is either resumed or killed.
    
    This feature is currently under a "test" RuntimeEnabled flag.
    
    Bug: 765401
    Change-Id: I09486e4298a80f4a08c6c8d78bf060745e81d687

--- third_party/WebKit/Source/core/frame/LocalFrame.h
+++ third_party/WebKit/Source/core/frame/LocalFrame.h
@@ -120,6 +120,7 @@ class CORE_EXPORT LocalFrame final : public Frame,
   void PrintNavigationWarning(const String&) override;
   bool PrepareForCommit() override;
   void DidChangeVisibilityState() override;
+  void DidPause() override;
   // This sets the is_inert_ flag and also recurses through this frame's
   // subtree, updating the inert bit on all descendant frames.
   void SetIsInert(bool) override;
