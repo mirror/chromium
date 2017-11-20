57b486bbf0d940088ef248380e02e4974955a743
yoshiki iguchi <yoshiki@chromium.org>
2017-11-20 16:04:49 +0900

    Initial implementation of sidebar-style message center
    This CL add a widget and a root view for Sidebar behind a flag. This is an initial implementation which has only basic feature of sidebar. The detailed behavior will be implemented in succeeding patches.
    
    Design doc: go/cros-sidebar
    Mock: go/cros-sidebar-mock (Early State)
    
    Bug: 726250
    Change-Id: I3ac1239f8509136eafff16b659e0040e36f702b9

---
+++ ash/sidebar/sidebar_mode.h
@@ -0,0 +1,14 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SIDEBAR_SIDEBAR_MODE_H_
+#define ASH_SIDEBAR_SIDEBAR_MODE_H_
+
+namespace ash {
+
+enum class SidebarMode { NORMAL, MESSAGE_CENTER_SETTINGS };
+
+}  // namespace ash
+
+#endif  // ASH_SIDEBAR_SIDEBAR_MODE_H_
