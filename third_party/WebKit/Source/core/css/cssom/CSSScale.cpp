3ce1439a59c3873f92b229afc662718a9a1835bb
Hwanseung Lee <hs1217.lee@samsung.com>
2017-11-20 21:53:42 +0900

    [css-typed-om] CSSScale should take Numberishes
    CSSScale currently only takes doubles,
    but it should take Numberishes.
    so replace double with Numberish at CSSScale.
    
    https://drafts.css-houdini.org/css-typed-om-1/#cssscale
    
    Bug: 784218
    Change-Id: I11c9e98a0c9468ecbdc82338d081b60918caa456

--- third_party/WebKit/Source/core/css/cssom/CSSScale.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSScale.cpp
@@ -5,6 +5,7 @@
 #include "core/css/cssom/CSSScale.h"
 
 #include "core/css/CSSPrimitiveValue.h"
+#include "core/css/cssom/CSSNumericValue.h"
 
 namespace blink {
 
@@ -12,24 +13,36 @@ namespace {
 
 CSSScale* FromScale(const CSSFunctionValue& value) {
   DCHECK(value.length() == 1U || value.length() == 2U);
-  double x = ToCSSPrimitiveValue(value.Item(0)).GetDoubleValue();
+  CSSNumericValue* x =
+      CSSNumericValue::FromCSSValue(ToCSSPrimitiveValue(value.Item(0)));
+  if (!x)
+    return nullptr;
   if (value.length() == 1U)
     return CSSScale::Create(x, x);
 
-  double y = ToCSSPrimitiveValue(value.Item(1)).GetDoubleValue();
+  CSSNumericValue* y =
+      CSSNumericValue::FromCSSValue(ToCSSPrimitiveValue(value.Item(1)));
+  if (!y)
+    return nullptr;
   return CSSScale::Create(x, y);
 }
 
 CSSScale* FromScaleXYZ(const CSSFunctionValue& value) {
   DCHECK_EQ(value.length(), 1U);
-  double double_value = ToCSSPrimitiveValue(value.Item(0)).GetDoubleValue();
+  CSSNumericValue* numeric_value =
+      CSSNumericValue::FromCSSValue(ToCSSPrimitiveValue(value.Item(0)));
+  if (!numeric_value)
+    return nullptr;
+
+  CSSUnitValue* default_value =
+      CSSUnitValue::Create(1, CSSPrimitiveValue::UnitType::kNumber);
   switch (value.FunctionType()) {
     case CSSValueScaleX:
-      return CSSScale::Create(double_value, 1);
+      return CSSScale::Create(numeric_value, default_value);
     case CSSValueScaleY:
-      return CSSScale::Create(1, double_value);
+      return CSSScale::Create(default_value, numeric_value);
     case CSSValueScaleZ:
-      return CSSScale::Create(1, 1, double_value);
+      return CSSScale::Create(default_value, default_value, numeric_value);
     default:
       NOTREACHED();
       return nullptr;
@@ -38,13 +51,52 @@ CSSScale* FromScaleXYZ(const CSSFunctionValue& value) {
 
 CSSScale* FromScale3d(const CSSFunctionValue& value) {
   DCHECK_EQ(value.length(), 3U);
-  double x = ToCSSPrimitiveValue(value.Item(0)).GetDoubleValue();
-  double y = ToCSSPrimitiveValue(value.Item(1)).GetDoubleValue();
-  double z = ToCSSPrimitiveValue(value.Item(2)).GetDoubleValue();
+  CSSNumericValue* x =
+      CSSNumericValue::FromCSSValue(ToCSSPrimitiveValue(value.Item(0)));
+  CSSNumericValue* y =
+      CSSNumericValue::FromCSSValue(ToCSSPrimitiveValue(value.Item(1)));
+  CSSNumericValue* z =
+      CSSNumericValue::FromCSSValue(ToCSSPrimitiveValue(value.Item(2)));
+  if (!x || !y || !z)
+    return nullptr;
+
   return CSSScale::Create(x, y, z);
 }
 
 }  // namespace
+
+CSSScale* CSSScale::Create(const CSSNumberish& x,
+                           const CSSNumberish& y,
+                           ExceptionState& exception_state) {
+  CSSNumericValue* x_value = CSSNumericValue::FromNumberish(x);
+  CSSNumericValue* y_value = CSSNumericValue::FromNumberish(y);
+
+  if (x_value->GetType() != CSSStyleValue::StyleValueType::kNumberType ||
+      y_value->GetType() != CSSStyleValue::StyleValueType::kNumberType) {
+    exception_state.ThrowTypeError("CSSScale does not support non-number type");
+    return nullptr;
+  }
+
+  return new CSSScale(x_value, y_value);
+}
+
+CSSScale* CSSScale::Create(const CSSNumberish& x,
+                           const CSSNumberish& y,
+                           const CSSNumberish& z,
+                           ExceptionState& exception_state) {
+  CSSNumericValue* x_value = CSSNumericValue::FromNumberish(x);
+  CSSNumericValue* y_value = CSSNumericValue::FromNumberish(y);
+  CSSNumericValue* z_value = CSSNumericValue::FromNumberish(z);
+
+  if (x_value->GetType() != CSSStyleValue::StyleValueType::kNumberType ||
+      y_value->GetType() != CSSStyleValue::StyleValueType::kNumberType ||
+      z_value->GetType() != CSSStyleValue::StyleValueType::kNumberType) {
+    exception_state.ThrowTypeError("CSSScale does not support non-number type");
+    return nullptr;
+  }
+
+  return new CSSScale(x_value, y_value, z_value);
+}
 
 CSSScale* CSSScale::FromCSSValue(const CSSFunctionValue& value) {
   switch (value.FunctionType()) {
@@ -62,18 +114,63 @@ CSSScale* CSSScale::FromCSSValue(const CSSFunctionValue& value) {
   }
 }
 
+void CSSScale::setX(const CSSNumberish& x, ExceptionState& exception_state) {
+  CSSNumericValue* value = CSSNumericValue::FromNumberish(x);
+
+  if (value->IsUnitValue() && ToCSSUnitValue(value)->GetType() !=
+                                  CSSStyleValue::StyleValueType::kNumberType) {
+    exception_state.ThrowTypeError("Must specify an number unit");
+    return;
+  }
+
+  x_ = value;
+}
+void CSSScale::setY(const CSSNumberish& y, ExceptionState& exception_state) {
+  CSSNumericValue* value = CSSNumericValue::FromNumberish(y);
+
+  if (value->IsUnitValue() && ToCSSUnitValue(value)->GetType() !=
+                                  CSSStyleValue::StyleValueType::kNumberType) {
+    exception_state.ThrowTypeError("Must specify an number unit");
+    return;
+  }
+
+  y_ = value;
+}
+void CSSScale::setZ(const CSSNumberish& z, ExceptionState& exception_state) {
+  CSSNumericValue* value = CSSNumericValue::FromNumberish(z);
+
+  if (value->IsUnitValue() && ToCSSUnitValue(value)->GetType() !=
+                                  CSSStyleValue::StyleValueType::kNumberType) {
+    exception_state.ThrowTypeError("Must specify an number unit");
+    return;
+  }
+
+  z_ = value;
+}
+
+const DOMMatrix* CSSScale::AsMatrix(ExceptionState& exception_state) const {
+  CSSUnitValue* x = x_->to(CSSPrimitiveValue::UnitType::kNumber);
+  CSSUnitValue* y = y_->to(CSSPrimitiveValue::UnitType::kNumber);
+  CSSUnitValue* z = z_->to(CSSPrimitiveValue::UnitType::kNumber);
+
+  if (!x || !y || !z) {
+    exception_state.ThrowTypeError(
+        "Cannot create matrix if valuse are not convert to CSSUnitValue");
+    return nullptr;
+  }
+
+  DOMMatrix* result = DOMMatrix::Create();
+  return result->scaleSelf(x->value(), y->value(), z->value());
+}
+
 const CSSFunctionValue* CSSScale::ToCSSValue() const {
   CSSFunctionValue* result =
       CSSFunctionValue::Create(is2D() ? CSSValueScale : CSSValueScale3d);
-
-  result->Append(
-      *CSSPrimitiveValue::Create(x_, CSSPrimitiveValue::UnitType::kNumber));
-  result->Append(
-      *CSSPrimitiveValue::Create(y_, CSSPrimitiveValue::UnitType::kNumber));
-  if (!is2D())
-    result->Append(
-        *CSSPrimitiveValue::Create(z_, CSSPrimitiveValue::UnitType::kNumber));
-
+  result->Append(*x_->ToCSSValue());
+  result->Append(*y_->ToCSSValue());
+  if (!is2D()) {
+    result->Append(*z_->ToCSSValue());
+  }
   return result;
 }
 
