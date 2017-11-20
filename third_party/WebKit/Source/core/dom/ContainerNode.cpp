2630f687a117488ced9a4656a155f919aeeab410
Yoshifumi Inoue <yosin@chromium.org>
2017-11-20 17:24:36 +0900

    Move LayoutText related codes in ContainerNode::GetUpperLeftCorner() to LayoutText
    This patch moves |LayoutText| related code in |GetUpperLeftCorner()| in
    |ContainerNode| class to |LayoutText| as |GetUpperLeftCorner()| function as
    a preparation of adopting LayoutNG[1]
    
    This patch is follow-up of the the patch[2].
    
    [1] http://crrev.com/c/763188: Utilize DCHECK(CanUseInlineBox())
    [2] http://crrev.com/c/775892: Refactor ContainerNode::GetUpperLeftCorner
    
    Change-Id: I6e92bbcb8ca5dfc94722b8f0470302c3646ab78b

--- third_party/WebKit/Source/core/dom/ContainerNode.cpp
+++ third_party/WebKit/Source/core/dom/ContainerNode.cpp
@@ -1021,21 +1021,22 @@ bool ContainerNode::GetUpperLeftCorner(FloatPoint& point) const {
     }
 
     if (o->IsText() && !o->IsBR()) {
-      if (previous->GetNode() == this && !ToLayoutText(o)->HasTextBoxes()) {
-        // Do nothing - skip unrendered whitespace that is a child or next
-        // sibling of the anchor.
-        // FIXME: This fails to skip a whitespace sibling when there was also a
-        // whitespace child (because |previous| has moved).
-        continue;
+      const Optional<FloatPoint> maybe_point =
+          ToLayoutText(o)->GetUpperLeftCorner();
+      if (!maybe_point.has_value()) {
+        if (previous->GetNode() == this) {
+          // Do nothing - skip unrendered whitespace that is a child or next
+          // sibling of the anchor.
+          // FIXME: This fails to skip a whitespace sibling when there was also
+          // a whitespace child (because |previous| has moved).
+          continue;
+        }
+        point = o->LocalToAbsolute(FloatPoint(), kUseTransforms);
+        return true;
       }
-      // TODO(xiaochengh): Move the code below to LayoutText.
-      DCHECK(CanUseInlineBox(*o));
-      point = FloatPoint();
-      if (ToLayoutText(o)->FirstTextBox()) {
-        point.Move(ToLayoutText(o)->LinesBoundingBox().X(),
-                   ToLayoutText(o)->FirstTextBox()->Root().LineTop().ToFloat());
-      }
-      point = o->LocalToAbsolute(point, kUseTransforms);
+      DCHECK(ToLayoutText(o)->HasTextBoxes());
+      point = o->LocalToAbsolute(maybe_point.value(), kUseTransforms);
+      DVLOG(0) << "GetUpperLeftCorner: " << point;
       return true;
     }
 
