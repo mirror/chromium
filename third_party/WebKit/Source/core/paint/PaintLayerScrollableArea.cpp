3b73e50a43f37f0811e7ffcc0303c89a7a5c43c7
MuVen <sataya.m@samsung.com>
2017-11-20 13:39:30 +0530

    Create scrollbar's if scroll size is greater than visible size.
    Create scrollbar's if scroll width/height is greater than the visible
    size width/height with scrollbars.
    
    Bug:633602
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Iccbda9a28c4406868a31be3691b729678ab6644f

--- third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
@@ -1371,24 +1371,35 @@ bool PaintLayerScrollableArea::TryRemovingAutoScrollbars(
   // remove auto scrollbars for the initial layout pass.
   DCHECK(!in_overflow_relayout_);
 
-  // TODO(pdr): Extend this logic to work with all overflow boxes.
-  if (Box().IsLayoutView()) {
     if (!needs_horizontal_scrollbar && !needs_vertical_scrollbar)
       return false;
 
-    ScrollbarMode h_mode;
-    ScrollbarMode v_mode;
-    ToLayoutView(Box()).CalculateScrollbarModes(h_mode, v_mode);
-    if (h_mode != kScrollbarAuto || v_mode != kScrollbarAuto)
-      return false;
+    if (Box().IsLayoutView()) {
+      ScrollbarMode h_mode;
+      ScrollbarMode v_mode;
+      ToLayoutView(Box()).CalculateScrollbarModes(h_mode, v_mode);
+      if (h_mode != kScrollbarAuto || v_mode != kScrollbarAuto)
+        return false;
 
-    IntSize visible_size_with_scrollbars =
-        VisibleContentRect(kIncludeScrollbars).Size();
-    if (ScrollWidth() <= visible_size_with_scrollbars.Width() &&
-        ScrollHeight() <= visible_size_with_scrollbars.Height()) {
-      return true;
-    }
+      IntSize visible_size_with_scrollbars =
+          VisibleContentRect(kIncludeScrollbars).Size();
+      if (ScrollWidth() <= visible_size_with_scrollbars.Width() &&
+          ScrollHeight() <= visible_size_with_scrollbars.Height()) {
+        return true;
+      }
+    } else {
+      if (!Box().HasAutoVerticalScrollbar() ||
+          !Box().HasAutoHorizontalScrollbar())
+        return false;
+
+      LayoutSize client_size_with_scrollbars =
+          LayoutContentRect(kIncludeScrollbars).Size();
+      if (ScrollWidth() <= client_size_with_scrollbars.Width() &&
+          ScrollHeight() <= client_size_with_scrollbars.Height()) {
+        return true;
+      }
   }
+
   return false;
 }
 
