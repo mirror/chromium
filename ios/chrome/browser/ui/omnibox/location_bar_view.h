566e89ab3849d538d145bb2f2dc4a9d34a3ede8e
stkhapugin@chromium.org <stkhapugin@chromium.org>
2017-11-20 14:25:17 +0100

    Move location bar view to a new file.
    Separates LocationBarView into its own file and adds necessary includes.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I1999c8b4ab7798d5d7c1c4b5352b1c22679a733d

---
+++ ios/chrome/browser/ui/omnibox/location_bar_view.h
@@ -0,0 +1,58 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_OMNIBOX_LOCATION_BAR_VIEW_H_
+#define IOS_CHROME_BROWSER_UI_OMNIBOX_LOCATION_BAR_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+@class OmniboxTextFieldIOS;
+
+@interface LocationBarView : UIView
+
+// Initialize the location bar with the given frame, font, text color, and tint
+// color for omnibox.
+- (instancetype)initWithFrame:(CGRect)frame
+                         font:(UIFont*)font
+                    textColor:(UIColor*)textColor
+                    tintColor:(UIColor*)tintColor NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+
+- (instancetype)initWithCoder:(NSCoder*)aDecoder NS_UNAVAILABLE;
+
+// The containted omnibox textfield.
+@property(nonatomic, strong) OmniboxTextFieldIOS* textField;
+
+// The leading button, such as the security status icon.
+@property(nonatomic, strong) UIButton* leadingButton;
+
+// Incognito status of the location bar changes the appearance, such as text
+// and icon colors.
+@property(nonatomic, assign) BOOL incognito;
+
+// Hides and shows the leading button, without animation.
+- (void)setLeadingButtonHidden:(BOOL)hidden;
+// Enables or disables the leading button for user interaction.
+- (void)setLeadingButtonEnabled:(BOOL)enabled;
+
+// Sets the leading button's image by resource id.
+- (void)setPlaceholderImage:(int)imageId;
+
+// Perform an animation of |leadingButton| fading in and sliding in from the
+// leading edge.
+- (void)fadeInLeadingButton;
+// Perform an animation of |leadingButton| sliding out and fading out towards
+// the leading edge.
+- (void)fadeOutLeadingButton;
+
+- (void)addExpandOmniboxAnimations:(UIViewPropertyAnimator*)animator
+    API_AVAILABLE(ios(10.0));
+
+- (void)addContractOmniboxAnimations:(UIViewPropertyAnimator*)animator
+    API_AVAILABLE(ios(10.0));
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_OMNIBOX_LOCATION_BAR_VIEW_H_
