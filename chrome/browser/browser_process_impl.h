8bdcc02631209dfc7a8ba873aeba5c4836991326
Michael Giuffrida <michaelpg@chromium.org>
2017-11-20 11:38:51 -0800

    Log UKMs for Tab Activity
    This is the beginning of several CLs to add UKMs for tab activity. The
    goal is to log certain features of tabs upon key events, like tab
    deactivation. For some of these features, we also log the tab when the
    features themselves change  This CL adds a small subset of such metrics.
    
    This introduces a new chrome/browser/tabs directory with a TabsTracker
    and TabLogger. The global TabsTracker listens for events and triggers
    its TabLogger, which logs the UKMs.
    
    The TabsTrackerTest unit test uses a mock TabLogger to test how the
    tracker responds to tab events.
    
    See bug for design doc.
    
    NOTE: This CL is temporarily rebased on top of a placeholder change.
    It's really based on the TabManager refactor at crrev.com/c/771774.
    
    Bug: 784639
    Change-Id: I211fd7202a1bf08c9a4c07904a60f65e4ef170f9

--- chrome/browser/browser_process_impl.h
+++ chrome/browser/browser_process_impl.h
@@ -161,6 +161,9 @@ class BrowserProcessImpl : public BrowserProcess,
       override;
   physical_web::PhysicalWebDataSource* GetPhysicalWebDataSource() override;
   prefs::InProcessPrefServiceFactory* pref_service_factory() const override;
+#if BUILDFLAG(ENABLE_RESOURCE_COORDINATOR)
+  tabs::TabsTracker* GetTabsTracker() override;
+#endif
 
   static void RegisterPrefs(PrefRegistrySimple* registry);
 
@@ -363,6 +366,11 @@ class BrowserProcessImpl : public BrowserProcess,
       physical_web_data_source_;
 
   std::unique_ptr<prefs::InProcessPrefServiceFactory> pref_service_factory_;
+
+// TODO(michaelpg): use feature flag instead
+#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
+  std::unique_ptr<tabs::TabsTracker> tabs_tracker_;
+#endif
 
   SEQUENCE_CHECKER(sequence_checker_);
 
