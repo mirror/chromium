421f562259872d12265e0478d838f54ed14eb264
Elad Alon <eladalon@chromium.org>
2017-11-20 13:01:19 +0100

    Introduce RtcEventLogManager
    RtcEventLogManager will manage both:
    1. Writing RTC event logs to local files, based on user input via
    WebRTCInternals. (Current CL.)
    2. Uploading RTC event logs to remote servers, based on JS-triggered
    configuration. (Upcoming CLs.)
    
    The latter of the two is the use case which motivated the creation of
    RtcEventLogManager, since we need this manager to outlive the call. We
    also want RtcEventLogManager to run in new Chrome instances, even if
    now WebRTC session exists, so that it may upload still-pending logs
    from previous sessions. (This functionality will be added in upcoming
    CLs.)
    
    Bug: 775415, 780863
    
    Pipe RtcEventLog fragments to BrowserThread::UI
    
    We're about to create an object called RtcEventLogManager, that would
    run on Browserthread::UI. It will be in charge of getting notifications
    of RTC event logs and handling them. Specifically, we expect it to
    write those to files and upload those files to remote servers. This CL
    pipes those messages from WebRTC back into close to where
    RtcEventLogManager will live.
    
    Bug: 775906
    Change-Id: I60764edd02da32640ca984160ad63247259f792d

--- base/files/file_util.h
+++ base/files/file_util.h
@@ -373,6 +373,8 @@ BASE_EXPORT bool SetCurrentDirectory(const FilePath& path);
 // unique. If |path| does not exist, 0 is returned.  If it fails to find such
 // a number, -1 is returned. If |suffix| is not empty, also checks the
 // existence of it with the given suffix.
+// TODO(eladalon): This function's implementation around |suffix| is confusing,
+// which is probably why nobody uses it. https://crbug.com/785333
 BASE_EXPORT int GetUniquePathNumber(const FilePath& path,
                                     const FilePath::StringType& suffix);
 
