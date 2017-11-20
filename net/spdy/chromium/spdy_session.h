c1afd91d213ef2b3e8ab867188bd6ab36a54c5aa
Bence Béky <bnc@chromium.org>
2017-11-20 13:29:42 -0500

    Do not update last_activity_time_ in SpdySession::DoWriteComplete().
    |last_activity_time_| is used to determine if a PING should be sent out
    on an HTTP/2 connection.  The relevant measurement for this is the time
    of the last read.  If |last_activity_time_| is also updated on writes,
    then it is possible that a connection times out but SpdySession is not
    sending any PING frames to check for this for a very long time.
    
    This CL removes the |last_activity_time_| update from
    SpdySession::DoWriteComplete(), and renames |last_activity_time_| to
    |last_read_time_| to better express its function.
    
    Bug: 784975
    Change-Id: I821cc5145f44557dd3f694833314336bb21f9608

--- net/spdy/chromium/spdy_session.h
+++ net/spdy/chromium/spdy_session.h
@@ -976,7 +976,7 @@ class NET_EXPORT SpdySession : public BufferedSpdyFramerVisitorInterface,
 
   SpdyPingId next_ping_id() const { return next_ping_id_; }
 
-  base::TimeTicks last_activity_time() const { return last_activity_time_; }
+  base::TimeTicks last_read_time() const { return last_read_time_; }
 
   bool check_ping_status_pending() const { return check_ping_status_pending_; }
 
@@ -1108,7 +1108,7 @@ class NET_EXPORT SpdySession : public BufferedSpdyFramerVisitorInterface,
   base::TimeTicks last_ping_sent_time_;
 
   // This is the last time we had activity in the session.
-  base::TimeTicks last_activity_time_;
+  base::TimeTicks last_read_time_;
 
   // This is the length of the last compressed frame.
   size_t last_compressed_frame_len_;
