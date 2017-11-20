c55cc2e10c464c813d20cca72fa69b40798a3d94
Xu Xing <xing.xu@intel.com>
2017-11-20 14:29:12 +0800

    [Test|viz] Implement local resource in GLRenderer-windbg
    Local resource is resource used inside the same GL context and will not being sent
    into another process, no need to create fence and mailbox for these resources.
    For RenderPass in render_pass_textures_, it uses texture-backed resources, no need
    to support GpuMemoryBuffer.
    
    Currently there are three path may possible set is_overlay_ to true:
    ResourceProvider::CreateGpuMemoryBufferResource
    LayerTreeResourceProvider::CreateResourceFromTextureMailbox
    ResourceProvider::CreateGpuTextureResource (Used by GLRenderer::overlay_resource_pool_).
    Resource for RenderPass in render_pass_textures_ is not created by above three paths.
    Based on this, remove the support of overlay.
    
    BUG=782044
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ia334842b12b8297ae4a768979adb8b5a468b906a
    
    Signed-off-by: Xu Xing <xing.xu@intel.com>
    Change-Id: I6dffe8cf9c2a9db1c675a51b56f6ea72a04a67c0

---
+++ cc/resources/scoped_local_resource.cc
@@ -0,0 +1,92 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "cc/resources/scoped_local_resource.h"
+
+#include "base/logging.h"
+#include "components/viz/common/resources/resource_format_utils.h"
+#include "components/viz/common/resources/scoped_set_active_texture.h"
+#include "gpu/GLES2/gl2extchromium.h"
+
+using gpu::gles2::GLES2Interface;
+
+namespace cc {
+
+ScopedLocalResource::ScopedLocalResource(GLES2Interface* gl,
+                                         const bool use_texture_usage_hint,
+                                         const bool use_texture_storage)
+    : gl_(gl),
+      use_texture_usage_hint_(use_texture_usage_hint),
+      use_texture_storage_(use_texture_storage) {
+  DCHECK(gl_);
+}
+
+ScopedLocalResource::~ScopedLocalResource() {
+  Free();
+}
+
+void ScopedLocalResource::CreateTexture() {
+  if (gl_id_)
+    return;
+
+  gl_->GenTextures(1, &gl_id_);
+  DCHECK(gl_id_);
+
+  // Create and set texture properties.
+  gl_->BindTexture(GL_TEXTURE_2D, gl_id_);
+  gl_->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  gl_->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  gl_->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  gl_->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  if (use_texture_usage_hint_) {
+    gl_->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_USAGE_ANGLE,
+                       GL_FRAMEBUFFER_ATTACHMENT_ANGLE);
+  }
+}
+
+void ScopedLocalResource::Allocate(const gfx::Size& size,
+                                   const gfx::ColorSpace& color_space) {
+  if (allocated_)
+    return;
+
+  if (!gl_id_)
+    CreateTexture();
+
+  size_ = size;
+  color_space_ = color_space;
+  allocated_ = true;
+
+  gl_->BindTexture(GL_TEXTURE_2D, gl_id_);
+
+  if (use_texture_storage_) {
+    GLint levels = 1;
+    gl_->TexStorage2DEXT(
+        GL_TEXTURE_2D, levels,
+        viz::TextureStorageFormat(viz::ResourceFormat::RGBA_8888),
+        size_.width(), size_.height());
+  } else {
+    gl_->TexImage2D(GL_TEXTURE_2D, 0,
+                    GLInternalFormat(viz::ResourceFormat::RGBA_8888),
+                    size_.width(), size_.height(), 0,
+                    GLDataFormat(viz::ResourceFormat::RGBA_8888),
+                    GLDataType(viz::ResourceFormat::RGBA_8888), nullptr);
+  }
+}
+
+void ScopedLocalResource::Free() {
+  if (!gl_id_)
+    return;
+
+  gl_->DeleteTextures(1, &gl_id_);
+  gl_id_ = 0;
+
+  allocated_ = false;
+}
+
+void ScopedLocalResource::BindForSampling(GLenum unit) {
+  viz::ScopedSetActiveTexture scoped_active_tex(gl_, unit);
+  gl_->BindTexture(GL_TEXTURE_2D, gl_id_);
+}
+
+}  // namespace cc
