6f56ded48bfa2f58a7f9ec198163cd27241bb805
Tom Anderson <thomasanderson@chromium.org>
2017-11-20 11:49:00 -0800

    Roll xdg-utils to fb9ee8c
    (Adding NOPRESUBMIT because some files in xdg-utils fail
    checkperms.py)
    
    BUG=786881
    R=thestig@chromium.org
    NOPRESUBMIT=true
    
    Change-Id: I711d7b4fa4fdba13878b5e526d0db8be8341fde9

---
+++ third_party/xdg-utils/autotests/t-xdg-open.sh
@@ -0,0 +1,157 @@
+#!/bin/sh
+COMMAND_TESTED=xdg-open
+. ./test-lib.sh
+
+test_open_url() {
+    local de=$1
+    shift
+    local cmd=$1
+
+    mock $cmd
+    run $de xdg-open http://www.freedesktop.org/
+    assert_run "$@" http://www.freedesktop.org/
+    unmock $cmd
+}
+
+mock_xdg_mime() {
+    local file="$1" mimetype="$2" handler="$3"
+    mock xdg-mime '
+if [ $# = 3 ] && [ "$1" = query ] && [ "$2" = filetype ] && \
+        [ "$3" = '\'"$file"\'' ]; then
+    echo '$mimetype'
+elif [ $# = 3 ] && [ "$1" = query ] && [ "$2" = default ] && \
+        [ "$3" = '$mimetype' ]; then
+    echo '$handler'.desktop
+else
+    echo "unexpected mock invokation: xdg-mime $*" >&2
+    exit 1
+fi
+'
+}
+
+test_generic_open_file() {
+    local filename="$1"
+    echo foo > "$LABDIR/$1"
+    mock_xdg_mime "$LABDIR/$1" text/plain textedit
+    mock_desktop_file textedit %f
+    mock textedit
+    run generic xdg-open "$LABDIR/$1"
+    assert_run textedit "$LABDIR/$1"
+}
+
+test_that_it opens a URL with "gio open" in recent GNOME 3, and Cinnamon
+mock "gio open"
+test_open_url gnome3 "gio open"
+test_open_url cinnamon "gio open"
+
+test_that_it opens a URL with gvfs-open if "gio open" is missing in GNOME 3, \
+             GNOME 2, and Cinnamon
+mock_missing "gio open"
+mock gvfs-open
+test_open_url gnome3 gvfs-open
+test_open_url gnome2 gvfs-open
+test_open_url cinnamon gvfs-open
+
+test_that_it opens a URL with gnome-open if "gio open" and gvfs-open are \
+             missing in GNOME 2
+mock_missing "gio open"
+mock_missing gvfs-open
+test_open_url gnome2 gnome-open
+
+test_that_it opens a URL with the generic method if "gio open" and gvfs-open \
+             are missing in GNOME 3, and Cinnamon
+mock_missing "gio open"
+mock_missing gvfs-open
+mock gnome-open
+mock_desktop_file mosaic %u
+mock_default_app x-scheme-handler/http mosaic
+test_open_url gnome3 mosaic
+test_open_url cinnamon mosaic
+
+test_that_it opens a URL with the generic method if "gio open", gvfs-open and \
+             gnome-open are missing in GNOME 2
+mock_missing "gio open"
+mock_missing gvfs-open
+mock_missing gnome-open
+mock_desktop_file mosaic %u
+mock_default_app x-scheme-handler/http mosaic
+test_open_url gnome2 mosaic
+
+test_that_it opens a URL with kfmclient in KDE 3
+test_open_url kde3 kfmclient exec
+
+test_that_it opens a URL with kde-open in KDE 4
+test_open_url kde4 kde-open
+
+test_that_it opens a URL with kde-open5 in KDE 5
+test_open_url kde5 kde-open5
+
+test_that_it opens a URL with gvfs-open in MATE
+test_open_url mate gvfs-open
+
+test_that_it opens a URL with mate-open if "gio open" and gvfs-open are \
+             missing in MATE
+mock_missing "gio open"
+mock_missing gvfs-open
+test_open_url mate mate-open
+
+test_that_it opens a URL with exo-open in XFCE
+test_open_url xfce exo-open
+
+test_that_it opens a URL with enlightenment_open in Enlightenment
+test_open_url enlightenment enlightenment_open
+
+test_that_it opens a file path with pcmanfm in LXDE
+mock pcmanfm
+touch $LABDIR/file.txt
+run lxde xdg-open $LABDIR/file.txt
+assert_run pcmanfm $(pwd)/$LABDIR/file.txt
+
+test_that_it percent-decodes a file:// URL and opens it with pcmanfm in LXDE
+mock pcmanfm
+touch $LABDIR/file.txt
+run lxde xdg-open file://$(pwd)/$LABDIR/file%2etxt
+assert_run pcmanfm $(pwd)/$LABDIR/file.txt
+
+test_that_it opens files with spaces in their name in LXDE
+echo foo > "$LABDIR/test file.txt"
+mock pcmanfm
+run lxde xdg-open "$LABDIR/test file.txt"
+assert_run pcmanfm "$(pwd)/$LABDIR/test file.txt"
+
+test_that_it looks up x-scheme-handler/\* in LXDE
+mock_desktop_file mosaic %u
+mock_default_app x-scheme-handler/http mosaic
+test_open_url lxde mosaic
+
+test_that_it looks up x-scheme-handler/\* in generic mode
+mock_desktop_file mosaic %u
+mock_default_app x-scheme-handler/http mosaic
+test_open_url generic mosaic
+
+test_that_it always uses \$BROWSER if set in generic mode
+BROWSER=cyberdog
+mock_desktop_file mosaic %u
+mock_default_app x-scheme-handler/http mosaic
+mock mosaic
+test_open_url generic cyberdog
+
+test_that_it works with multi-word \$BROWSER commands
+BROWSER="cyberdog --url %s"
+test_open_url generic cyberdog --url
+
+test_that_it is not vulnerable to command injection in URLs when using \
+             \$BROWSER in generic mode
+mock cyberdog
+BROWSER="cyberdog --url %s"
+run generic xdg-open 'http://www.freedesktop.org/; echo BUSTED'
+assert_run cyberdog --url 'http://www.freedesktop.org/; echo BUSTED'
+
+test_that_it opens files in generic mode
+test_generic_open_file test.txt
+
+test_that_it opens files with \# characters in their name in generic mode
+test_generic_open_file 'test#file.txt'
+
+test_that_it opens files with spaces in their name in generic mode
+test_generic_open_file 'test file.txt'
---
+++ third_party/xdg-utils/autotests/test-lib.sh
@@ -0,0 +1,219 @@
+set -e
+
+TEST_NAME=$0
+LABDIR=lab
+LABHOME=lab/home
+BINDIR=$LABDIR/bin
+COMMANDS_RUN=$LABDIR/commands-run
+FAILED_TESTS=failed-tests
+CURRENT_TEST_CASE=
+XDG_DATA_HOME=$LABHOME/.local/share
+XDG_DATA_DIR=$LABDIR/share
+XDG_DATA_DIR_LOCAL=$LABDIR/share-local
+XDG_DATA_DIRS=$XDG_DATA_DIR_LOCAL:$XDG_DATA_DIR
+XDG_CONFIG_HOME=$LABHOME/.config
+XDG_CONFIG_DIRS=$LABDIR/etc-xdg
+
+fatal() {
+    echo "FATAL: $*" >&2
+    exit 1
+}
+
+reset_lab_() {
+    [ -f test-lib.sh ] || fatal "You must run tests from the autotests directory!"
+    [ -e $LABDIR ] && rm -r $LABDIR
+
+    BROWSER=
+
+    mkdir -p \
+          $BINDIR \
+          $LABHOME \
+          $XDG_DATA_HOME/applications $XDG_DATA_DIR/applications \
+          $XDG_DATA_DIR_LOCAL/applications \
+          $XDG_DATA_HOME/icons/hicolor $XDG_DATA_DIR/icons/hicolor \
+          $XDG_DATA_DIR_LOCAL/icons/hicolor \
+          $XDG_CONFIG_HOME \
+          $XDG_CONFIG_DIRS \
+
+    mock x-www-browser
+    touch $COMMANDS_RUN
+}
+
+test_that_it() {
+    CURRENT_TEST_CASE="$*"
+    echo "  - $CURRENT_TEST_CASE"
+    reset_lab_
+}
+
+set_de_() {
+    unmock gnome-default-applications-properties
+    unmock kde-config
+    KDE_SESSION_VERSION=
+
+    case "$1" in
+        cinnamon)
+            XDG_CURRENT_DESKTOP=Cinnamon
+            ;;
+        enlightenment)
+            XDG_CURRENT_DESKTOP=ENLIGHTENMENT
+            ;;
+        gnome2)
+            XDG_CURRENT_DESKTOP=GNOME
+            mock gnome-default-applications-properties
+            ;;
+        gnome3)
+            XDG_CURRENT_DESKTOP=GNOME
+            ;;
+        kde3)
+            XDG_CURRENT_DESKTOP=KDE
+            mock_output kde-config "KDE: 3.5.5"
+            ;;
+        kde4)
+            XDG_CURRENT_DESKTOP=KDE
+            KDE_SESSION_VERSION=4
+            ;;
+        kde5)
+            XDG_CURRENT_DESKTOP=KDE
+            KDE_SESSION_VERSION=5
+            ;;
+        lxde)
+            XDG_CURRENT_DESKTOP=LXDE
+            ;;
+        mate)
+            XDG_CURRENT_DESKTOP=MATE
+            ;;
+        xfce)
+            XDG_CURRENT_DESKTOP=XFCE
+            ;;
+        generic)
+            XDG_CURRENT_DESKTOP=X-Generic
+            ;;
+        *)
+            fatal "unknown desktop environment: $1"
+            ;;
+    esac
+}
+
+mock() {
+    local command="$1" script="$2"
+    local executable="$BINDIR/$command"
+
+    cat >"$executable" <<EOF
+#!/bin/sh
+set -e
+echo "$command \$*" >> $(pwd)/$COMMANDS_RUN
+$script
+EOF
+    chmod +x "$executable"
+}
+
+mock_missing() {
+    local command="$1"
+    mock "$command" "exit 127"
+}
+
+mock_output() {
+    local command="$1" output="$2"
+    mock "$command" "echo '$output'"
+}
+
+unmock() {
+    if [ -e "$BINDIR/$1" ]; then
+        rm "$BINDIR/$1"
+    fi
+}
+
+is_mocked() {
+    if [ -e "$BINDIR/$1" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+mock_desktop_file() {
+    cat > "$XDG_DATA_DIR/applications/$1.desktop" <<EOF
+[Desktop Entry]
+Type=Application
+Name=$1
+Exec=$*
+EOF
+}
+
+mock_default_app() {
+    local mimetype="$1" app="$2"
+    local mimeapps=$XDG_CONFIG_HOME/mimeapps.list
+    if ! [ -e $mimeapps ]; then
+        echo "[Default Applications]" > $mimeapps
+    fi
+    echo "$mimetype=$app.desktop" >> $mimeapps
+}
+
+assertion_failed() {
+    echo "ASSERTION FAILED: $*" >&2
+    echo "$TEST_NAME: Assertion failed when testing that $COMMAND_TESTED" \
+         "$CURRENT_TEST_CASE: $*" >> $FAILED_TESTS
+}
+
+assert_run() {
+    if grep -Fxq "$*" $COMMANDS_RUN; then
+        return 0
+    else
+        assertion_failed "expected command to be run: $*"
+    fi
+}
+
+assert_equal() {
+    if [ "$1" = "$2" ]; then
+        return 0
+    else
+        assertion_failed "expected: '$1' got: '$2'"
+    fi
+}
+
+assert_file_exists() {
+    if [ -e "$1" ]; then
+        return 0
+    else
+        assertion_failed "expected file to exist: $1"
+    fi
+}
+
+run() {
+    local de="$1"
+    shift
+
+    local cmd="$1"
+    shift
+
+    : > $COMMANDS_RUN
+
+    set_de_ $de
+
+    local trace=
+    if [ "$TRACE" = 1 ]; then
+        trace="sh -x"
+    fi
+
+    if [ "$TRACE" = 1 ] || [ "$TRACE_RUN" = 1 ]; then
+        echo "RUN [$de] $cmd $*" >&2
+    fi
+
+    env -i \
+        PATH="$BINDIR:../scripts:$PATH" \
+        SHELL="$SHELL" \
+        USERNAME="$USERNAME" \
+        HOME="$LABHOME" \
+        LOGNAME="$LOGNAME" \
+        TERM="$TERM" \
+        XDG_CURRENT_DESKTOP="$XDG_CURRENT_DESKTOP" \
+        XDG_UTILS_DEBUG_LEVEL="$XDG_UTILS_DEBUG_LEVEL" \
+        KDE_SESSION_VERSION="$KDE_SESSION_VERSION" \
+        XDG_DATA_DIRS=$XDG_DATA_DIRS \
+        XDG_CONFIG_DIRS=$XDG_CONFIG_DIRS \
+        DISPLAY=x \
+        BROWSER="$BROWSER" \
+        $trace ../scripts/$cmd "$@"
+}
+
+echo "* Testing that $COMMAND_TESTED"
