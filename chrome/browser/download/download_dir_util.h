b238260efb90df2db980b928dbd9319f5780a0b2
Marc-Antoine Courteau <macourteau@chromium.org>
2017-11-20 14:38:35 -0500

    Adds the DefaultDownloadDirectory policy.
    Bug:616135
    Change-Id: Ic808a1dd6803e7702ceb8af445e42cbe0c03e2b4

---
+++ chrome/browser/download/download_dir_util.h
@@ -0,0 +1,25 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_DOWNLOAD_DOWNLOAD_DIR_UTIL_H_
+#define CHROME_BROWSER_DOWNLOAD_DOWNLOAD_DIR_UTIL_H_
+
+#include "base/files/file_path.h"
+
+namespace policy {
+struct PolicyHandlerParameters;
+}  // namespace policy
+
+#if defined(OS_CHROMEOS)
+// Returns whether |string_value| points to a directory in Drive or not.
+bool DownloadToDrive(const base::FilePath::StringType& string_value,
+                     const policy::PolicyHandlerParameters& parameters);
+#endif  // defined(OS_CHROMEOS)
+
+// Expands path variables in the download directory path |string_value|.
+base::FilePath::StringType ExpandDownloadDirectoryPath(
+    const base::FilePath::StringType& string_value,
+    const policy::PolicyHandlerParameters& parameters);
+
+#endif  // CHROME_BROWSER_DOWNLOAD_DOWNLOAD_DIR_UTIL_H_
