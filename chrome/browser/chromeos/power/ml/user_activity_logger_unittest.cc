9e4228b385288fb376193ba7fad422bad36165ce
Renjie Liu <renjieliu@chromium.org>
2017-11-21 11:54:59 +1100

    Init implementation of UserActivityLogger.
    * Currently only care abouts user activity.
    * Having an abstract logging_helper which will do the logging.
    * The logging_helper is not implemented yet.
    
    Bug: 784232
    Change-Id: Icde3f269b3120fae45e278ec9c4394786e6fe6eb

---
+++ chrome/browser/chromeos/power/ml/user_activity_logger_unittest.cc
@@ -0,0 +1,115 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/power/ml/user_activity_logger.h"
+
+#include <memory>
+#include <vector>
+
+#include "base/timer/timer.h"
+#include "chrome/browser/chromeos/power/ml/idle_event_notifier.h"
+#include "chrome/browser/chromeos/power/ml/user_activity_event.pb.h"
+#include "chrome/browser/chromeos/power/ml/user_activity_logger_delegate.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+namespace power {
+namespace ml {
+
+// Testing logger delegate.
+class TestingUserActivityLoggerDelegate : public UserActivityLoggerDelegate {
+ public:
+  TestingUserActivityLoggerDelegate() = default;
+  ~TestingUserActivityLoggerDelegate() override = default;
+
+  const std::vector<UserActivityEvent>& events() const { return events_; }
+
+  void LogActivity(const UserActivityEvent& event) override {
+    events_.push_back(event);
+  }
+
+ private:
+  std::vector<UserActivityEvent> events_;
+
+  DISALLOW_COPY_AND_ASSIGN(TestingUserActivityLoggerDelegate);
+};
+
+class UserActivityLoggerTest : public testing::Test {
+ public:
+  UserActivityLoggerTest() : activity_logger_(&delegate_) {}
+
+  ~UserActivityLoggerTest() override = default;
+
+ protected:
+  void ReportUserActivity(const ui::Event* event) {
+    activity_logger_.OnUserActivity(event);
+  }
+
+  void ReportIdleEvent(const IdleEventNotifier::ActivityData& data) {
+    activity_logger_.OnIdleEventObserved(data);
+  }
+
+  const std::vector<UserActivityEvent>& GetEvents() {
+    return delegate_.events();
+  }
+
+ private:
+  TestingUserActivityLoggerDelegate delegate_;
+  UserActivityLogger activity_logger_;
+};
+
+// After an idle event, we have a ui::Event, we should expect one
+// UserActivityEvent.
+TEST_F(UserActivityLoggerTest, LogAfterIdleEvent) {
+  // Trigger an idle event.
+  IdleEventNotifier::ActivityData data;
+  data.last_activity_time = base::TimeTicks::Now();
+  ReportIdleEvent(data);
+  ReportUserActivity(nullptr);
+
+  const auto& events = GetEvents();
+  ASSERT_EQ(events.size(), 1U);
+
+  UserActivityEvent::Event expected_event;
+  expected_event.set_type(UserActivityEvent::Event::REACTIVATE);
+  expected_event.set_reason(UserActivityEvent::Event::USER_ACTIVITY);
+  EXPECT_EQ(events[0].event().SerializeAsString(),
+            expected_event.SerializeAsString());
+}
+
+// Get a user event before an idle event, we should not log it.
+TEST_F(UserActivityLoggerTest, LogBeforeIdleEvent) {
+  ReportUserActivity(nullptr);
+  // Trigger an idle event.
+  IdleEventNotifier::ActivityData data;
+  data.last_activity_time = base::TimeTicks::Now();
+  ReportIdleEvent(data);
+
+  EXPECT_EQ(GetEvents().size(), 0U);
+}
+
+// Get a user event, then an idle event, then another user event,
+// we should log the last one/
+TEST_F(UserActivityLoggerTest, LogSecondEvent) {
+  ReportUserActivity(nullptr);
+  // Trigger an idle event.
+  IdleEventNotifier::ActivityData data;
+  data.last_activity_time = base::TimeTicks::Now();
+  ReportIdleEvent(data);
+  // Another user event.
+  ReportUserActivity(nullptr);
+
+  const auto& events = GetEvents();
+  ASSERT_EQ(events.size(), 1U);
+
+  UserActivityEvent::Event expected_event;
+  expected_event.set_type(UserActivityEvent::Event::REACTIVATE);
+  expected_event.set_reason(UserActivityEvent::Event::USER_ACTIVITY);
+  EXPECT_EQ(events[0].event().SerializeAsString(),
+            expected_event.SerializeAsString());
+}
+
+}  // namespace ml
+}  // namespace power
+}  // namespace chromeos
