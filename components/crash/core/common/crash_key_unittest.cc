e7e689bc6d6b2ccd5d11dabaa51a9e33e7c9a355
Robert Sesek <rsesek@chromium.org>
2017-11-21 10:57:57 -0500

    Add a utility to format a base::debug::StackTrace for CrashKeyString.
    Bug: 598854
    Change-Id: Ie92935f4919e57cae338c33750458c0509a93627

--- components/crash/core/common/crash_key_unittest.cc
+++ components/crash/core/common/crash_key_unittest.cc
@@ -4,6 +4,7 @@
 
 #include "components/crash/core/common/crash_key.h"
 
+#include "base/debug/stack_trace.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace crash_reporter {
@@ -25,6 +26,37 @@ TEST_F(CrashKeyStringTest, ScopedCrashKeyString) {
   }
 
   EXPECT_FALSE(key.is_set());
+}
+
+TEST_F(CrashKeyStringTest, FormatStackTrace) {
+  const uint64_t addresses[] = {
+      0x0badbeef, 0x7777888899990000, 0xabc, 0x000000ddeeff11, 0x123456789abc,
+  };
+  base::debug::StackTrace trace(reinterpret_cast<const void* const*>(addresses),
+                                arraysize(addresses));
+
+  std::string too_small = internal::FormatStackTrace(trace, 3);
+  EXPECT_EQ(0u, too_small.size());
+
+  std::string one_value = internal::FormatStackTrace(trace, 18);
+  EXPECT_EQ("0xbadbeef", one_value);
+
+  std::string three_values = internal::FormatStackTrace(trace, 34);
+  EXPECT_EQ("0xbadbeef 0x7777888899990000 0xabc", three_values);
+
+  std::string all_values = internal::FormatStackTrace(trace, 256);
+  EXPECT_EQ("0xbadbeef 0x7777888899990000 0xabc 0xddeeff11 0x123456789abc",
+            all_values);
+}
+
+TEST_F(CrashKeyStringTest, SetStackTrace) {
+  static CrashKeyString<1024> key("test-trace");
+
+  EXPECT_FALSE(key.is_set());
+
+  SetCrashKeyStringToStackTrace(&key, base::debug::StackTrace());
+
+  EXPECT_TRUE(key.is_set());
 }
 
 }  // namespace
