b014b5be48c0f2c9048e0b60e8cf430702e8274f
Mustafa Emre Acer <meacer@chromium.org>
2017-11-21 11:25:21 -0800

    Add SubjectAlternativeName info to Android certificate viewer
    Screenshot: https://drive.google.com/file/d/1W8r6aQlNAUD3AI8FxBN_6R0btyRn5VcQ/view?usp=sharing
    
    Bug: 167770
    Change-Id: Ief385060da9d8bbcde9bbed7786b216472f7e8ef

--- chrome/browser/ui/android/page_info/certificate_viewer_android.cc
+++ chrome/browser/ui/android/page_info/certificate_viewer_android.cc
@@ -104,3 +104,16 @@ static ScopedJavaLocalRef<jstring> GetCertSHA1FingerprintText(
   return ConvertUTF8ToJavaString(
       env, l10n_util::GetStringUTF8(IDS_CERT_INFO_SHA1_FINGERPRINT_LABEL));
 }
+
+static ScopedJavaLocalRef<jstring> GetCertExtensionText(
+    JNIEnv* env,
+    const JavaParamRef<jclass>&) {
+  return ConvertUTF8ToJavaString(
+      env, l10n_util::GetStringUTF8(IDS_CERT_DETAILS_EXTENSIONS));
+}
+
+static ScopedJavaLocalRef<jstring> GetCertSANText(JNIEnv* env,
+                                                  const JavaParamRef<jclass>&) {
+  return ConvertUTF8ToJavaString(
+      env, l10n_util::GetStringUTF8(IDS_CERT_X509_SUBJECT_ALT_NAME));
+}
