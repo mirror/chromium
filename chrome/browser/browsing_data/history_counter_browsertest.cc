5381a12e6ebaf7d2d3fc4d0ad34a1234c72d2ffd
Sarath Singapati <sarath.singapati@huawei.com>
2017-11-21 14:20:52 +0200

    History data counter update and deletion with OLDER_THAN_30_DAYS time-period.
    HistoryService::GetHistoryCount() takes period endtime instead of Time::Max().
    Added UT for history deletion.
    
    Bug: 243833
    Change-Id: If804a5b17b1f34d985d4a46a6d77a33fd39c0cdc

--- chrome/browser/browsing_data/history_counter_browsertest.cc
+++ chrome/browser/browsing_data/history_counter_browsertest.cc
@@ -240,6 +240,16 @@ IN_PROC_BROWSER_TEST_F(HistoryCounterTest, PeriodChanged) {
   AddVisit("https://www.example.com");
   AddVisit("https://www.example.com");
 
+  RevertTimeInDays(31);
+  AddVisit("https://www.google.com");
+  AddVisit("https://www.chrome.com");
+  AddVisit("https://www.chrome.com");
+
+  RevertTimeInDays(100);
+  AddVisit("https://www.google.com");
+  AddVisit("https://www.example.com");
+  AddVisit("https://www.example.com");
+
   Profile* profile = browser()->profile();
 
   HistoryCounter counter(
@@ -271,6 +281,10 @@ IN_PROC_BROWSER_TEST_F(HistoryCounterTest, PeriodChanged) {
   SetDeletionPeriodPref(browsing_data::TimePeriod::ALL_TIME);
   WaitForCounting();
   EXPECT_EQ(9u, GetLocalResult());
+
+  SetDeletionPeriodPref(browsing_data::TimePeriod::OLDER_THAN_30DAYS);
+  WaitForCounting();
+  EXPECT_EQ(4u, GetLocalResult());
 }
 
 // Test the behavior for a profile that syncs history.
