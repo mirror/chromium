290032be1ddcda662c71bd9a18c4ffab5d24e2f3
Jay Civelli <jcivelli@google.com>
2017-11-21 12:35:29 -0800

    Adding a SafeXMLParser to the data-decoder service.
    Introducing a new XML parser service that parses XML into a
    base::DictionaryValue. It does so by JSONifying the XML content.
    
    Also factoring out TestServiceManagerListener to its own file, now that
    it is used in 2 places (safe_json_parser_browsertest.cc and
    safe_xml_parser_browsertest.cc).
    
    Bug: 784667
    Change-Id: I1f41f44d49910da43837aac8bca52e24533460ef

---
+++ services/data_decoder/public/cpp/safe_xml_parser.cc
@@ -0,0 +1,184 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/data_decoder/public/cpp/safe_xml_parser.h"
+
+#include "base/callback.h"
+#include "base/macros.h"
+#include "base/threading/thread_checker.h"
+#include "base/unguessable_token.h"
+#include "base/values.h"
+#include "services/data_decoder/public/interfaces/constants.mojom.h"
+#include "services/data_decoder/public/interfaces/xml_parser.mojom.h"
+#include "services/service_manager/public/cpp/connector.h"
+#include "services/service_manager/public/interfaces/constants.mojom.h"
+
+namespace data_decoder {
+
+namespace {
+
+// Class that does the actual parsing. Deletes itself when parsing is done.
+class SafeXmlParser {
+ public:
+  SafeXmlParser(service_manager::Connector* connector,
+                const std::string& unsafe_xml,
+                XmlParserCallback callback);
+  ~SafeXmlParser();
+
+ private:
+  void ReportResults(std::unique_ptr<base::Value> parsed_json,
+                     const base::Optional<std::string>& error);
+
+  XmlParserCallback callback_;
+  mojom::XmlParserPtr xml_parser_ptr_;
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  DISALLOW_COPY_AND_ASSIGN(SafeXmlParser);
+};
+
+SafeXmlParser::SafeXmlParser(service_manager::Connector* connector,
+                             const std::string& unsafe_xml,
+                             XmlParserCallback callback)
+    : callback_(std::move(callback)) {
+  DCHECK(callback_);  // Parsing without ca callback is useless.
+
+  // Use a random instance ID to guarantee the connection is to a new service
+  // running in its own process.
+  base::UnguessableToken token = base::UnguessableToken::Create();
+  service_manager::Identity identity(mojom::kServiceName,
+                                     service_manager::mojom::kInheritUserID,
+                                     token.ToString());
+  connector->BindInterface(identity, &xml_parser_ptr_);
+
+  // Unretained(this) is safe as the xml_parser_ptr_ is owned by this class.
+  xml_parser_ptr_.set_connection_error_handler(base::BindOnce(
+      &SafeXmlParser::ReportResults, base::Unretained(this),
+      /*parsed_xml=*/nullptr,
+      base::make_optional(
+          std::string("Connection error with the XML parser process."))));
+  xml_parser_ptr_->Parse(
+      unsafe_xml,
+      base::BindOnce(&SafeXmlParser::ReportResults, base::Unretained(this)));
+}
+
+SafeXmlParser::~SafeXmlParser() = default;
+
+void SafeXmlParser::ReportResults(std::unique_ptr<base::Value> parsed_xml,
+                                  const base::Optional<std::string>& error) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  std::move(callback_).Run(std::move(parsed_xml), error);
+
+  // This should be the last interaction with this instance, safely delete.
+  delete this;
+}
+
+}  // namespace
+
+void ParseXml(service_manager::Connector* connector,
+              const std::string& unsafe_xml,
+              XmlParserCallback callback) {
+  new SafeXmlParser(connector, unsafe_xml, std::move(callback));
+}
+
+bool IsXmlElementNamed(const base::Value& element, const std::string& name) {
+  if (!element.is_dict())
+    return false;
+  const base::Value* tag_text = element.FindKeyOfType(
+      mojom::XmlParser::kTagKey, base::Value::Type::STRING);
+  return tag_text ? tag_text->GetString() == name : false;
+}
+
+bool GetXmlElementTagName(const base::Value& element, std::string* tag_name) {
+  DCHECK(tag_name);
+  if (!element.is_dict())
+    return false;
+  const base::Value* tag_text = element.FindKeyOfType(
+      mojom::XmlParser::kTagKey, base::Value::Type::STRING);
+  if (!tag_text)
+    return false;
+  *tag_name = tag_text->GetString();
+  return true;
+}
+
+bool GetXmlElementText(const base::Value& element, std::string* text) {
+  DCHECK(text);
+  if (!element.is_dict())
+    return false;
+  const base::Value* text_value = element.FindKeyOfType(
+      mojom::XmlParser::kTextKey, base::Value::Type::STRING);
+  if (!text_value)
+    return false;
+  *text = text_value->GetString();
+  return true;
+}
+
+int GetXmlElementChildrenCount(const base::Value& element,
+                               const std::string& name) {
+  if (!element.is_dict())
+    return 0;
+  const base::Value* children = element.FindKeyOfType(
+      mojom::XmlParser::kChildrenKey, base::Value::Type::LIST);
+  if (!children)
+    return 0;
+  int child_count = 0;
+  for (const base::Value& value : children->GetList()) {
+    DCHECK(value.is_dict());
+    std::string tag_name;
+    bool success = GetXmlElementTagName(value, &tag_name);
+    DCHECK(success);
+    if (tag_name == name)
+      child_count++;
+  }
+  return child_count;
+}
+
+const base::Value* GetXmlElementChildWithName(const base::Value& element,
+                                              const std::string& name) {
+  if (!element.is_dict())
+    return nullptr;
+  const base::Value* children = element.FindKeyOfType(
+      mojom::XmlParser::kChildrenKey, base::Value::Type::LIST);
+  if (!children)
+    return nullptr;
+  for (const base::Value& value : children->GetList()) {
+    DCHECK(value.is_dict());
+    if (IsXmlElementNamed(value, name))
+      return &value;
+  }
+  return nullptr;
+}
+
+const base::Value* FindXmlElementPath(
+    const base::Value& element,
+    std::initializer_list<base::StringPiece> path,
+    bool* unique_path) {
+  const base::Value* cur = nullptr;
+  if (unique_path)
+    *unique_path = true;
+
+  for (const base::StringPiece component_piece : path) {
+    std::string component(component_piece);
+    if (!cur) {
+      // First element has to match the current node.
+      if (!IsXmlElementNamed(element, component))
+        return nullptr;
+      cur = &element;
+      continue;
+    }
+
+    const base::Value* new_cur = GetXmlElementChildWithName(*cur, component);
+    if (!new_cur)
+      return nullptr;
+
+    if (unique_path && *unique_path &&
+        GetXmlElementChildrenCount(*cur, component) > 1)
+      *unique_path = false;
+
+    cur = new_cur;
+  }
+  return cur;
+}
+
+}  // namespace data_decoder
