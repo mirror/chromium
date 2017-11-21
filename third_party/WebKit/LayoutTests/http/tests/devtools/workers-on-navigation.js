a5e92cd5c97e288b6f5075e454828c89573186fa
Will Chen <chenwilliam@chromium.org>
2017-11-21 11:23:45 -0800

    Reland "DevTools: migrate a handful of assorted DevTools tests"
    Deflaked the flaky test: workers-on-navigation.js
    
    This is a reland of 614a066b19391e76a9637211012150c4bebe4e72
    Original change's description:
    > DevTools: migrate a handful of assorted DevTools tests
    >
    > Migrates a couple websocket tests and assorted tests in http/tests/devtools/*.html
    >
    > Bug: 667560
    > Change-Id: I9d23517e8be6ed2282bd111b37bd0fa64f28b0c1
    > Reviewed-on: https://chromium-review.googlesource.com/777738
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Commit-Queue: Will Chen <chenwilliam@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#518067}
    
    Bug: 667560
    Change-Id: If0148921e6133f2b5a7d351be9b59068738664fb

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/workers-on-navigation.js
@@ -0,0 +1,49 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function() {
+  TestRunner.addResult(`Tests that workers are correctly detached upon navigation.\n`);
+  TestRunner.printDevToolsConsole();
+
+  var workerTargetId;
+  var navigated = false;
+  var observer = {
+    targetAdded(target) {
+      if (!TestRunner.isDedicatedWorker(target))
+        return;
+      TestRunner.addResult('Worker added');
+      workerTargetId = target.id();
+      if (navigated) {
+        TestRunner.addResult('=== Error messages from console ====');
+        TestRunner.addResults(messages);
+        TestRunner.completeTest();
+      }
+    },
+    targetRemoved(target) {
+      if (!TestRunner.isDedicatedWorker(target))
+        return;
+      if (target.id() === workerTargetId) {
+        TestRunner.addResult('Worker removed');
+        workerTargetId = '';
+      } else {
+        TestRunner.addResult('Unknown worker removed');
+      }
+    }
+  };
+  await TestRunner.navigatePromise('resources/workers-on-navigation-resource.html');
+  SDK.targetManager.observeTargets(observer);
+  await TestRunner.evaluateInPagePromise('startWorker()');
+
+  // Print error messages after reload is complete to avoid a race between
+  // the "Page reloaded." and protocol error console messages.
+  var messages = [];
+  console.error = (message) => {
+    messages.push('error: ' + message);
+  };
+
+  await TestRunner.reloadPagePromise();
+
+  navigated = true;
+  await TestRunner.evaluateInPagePromise('startWorker()');
+})();
