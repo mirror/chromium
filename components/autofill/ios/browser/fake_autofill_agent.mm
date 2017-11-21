d52686df43844d12fbb232070b846cc48bf68fad
John Z Wu <jzw@chromium.org>
2017-11-20 17:12:19 -0800

    Fake autofillagent for use in testing.
    //ios/web_view needs a fake autofill agent in order to unit test
    CWVAutofillController, which depends on an autofill_agent for
    fetching suggestions.
    
    Bug:778764
    Change-Id: If6f552dc049739ea0ab322ffb50a32334ab5ce74

---
+++ components/autofill/ios/browser/fake_autofill_agent.mm
@@ -0,0 +1,91 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "components/autofill/ios/browser/fake_autofill_agent.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation FakeAutofillAgent {
+  NSMutableDictionary<NSString*, NSMutableArray<FormSuggestion*>*>*
+      _suggestionsByFormAndFieldName;
+  NSMutableDictionary<NSString*, FormSuggestion*>*
+      _selectedSuggestionByFormAndFieldName;
+}
+
+- (instancetype)initWithPrefService:(PrefService*)prefService
+                           webState:(web::WebState*)webState {
+  self = [super initWithPrefService:prefService webState:webState];
+  if (self) {
+    _suggestionsByFormAndFieldName = [NSMutableDictionary dictionary];
+    _selectedSuggestionByFormAndFieldName = [NSMutableDictionary dictionary];
+  }
+  return self;
+}
+
+#pragma mark - Public Methods
+
+- (void)addSuggestion:(FormSuggestion*)suggestion
+          forFormName:(NSString*)formName
+            fieldName:(NSString*)fieldName {
+  NSString* key = [self keyForFormName:formName fieldName:fieldName];
+  NSMutableArray* suggestions = _suggestionsByFormAndFieldName[key];
+  if (!suggestions) {
+    suggestions = [NSMutableArray array];
+    _suggestionsByFormAndFieldName[key] = suggestions;
+  }
+  [suggestions addObject:suggestion];
+}
+
+- (FormSuggestion*)selectedSuggestionForFormName:(NSString*)formName
+                                       fieldName:(NSString*)fieldName {
+  NSString* key = [self keyForFormName:formName fieldName:fieldName];
+  return _selectedSuggestionByFormAndFieldName[key];
+}
+
+#pragma mark - FormSuggestionProvider
+
+- (void)checkIfSuggestionsAvailableForForm:(NSString*)formName
+                                     field:(NSString*)fieldName
+                                 fieldType:(NSString*)fieldType
+                                      type:(NSString*)type
+                                typedValue:(NSString*)typedValue
+                                  webState:(web::WebState*)webState
+                         completionHandler:
+                             (SuggestionsAvailableCompletion)completion {
+  dispatch_async(dispatch_get_main_queue(), ^{
+    completion([_suggestionsByFormAndFieldName[formName] count] ? YES : NO);
+  });
+}
+
+- (void)retrieveSuggestionsForForm:(NSString*)formName
+                             field:(NSString*)fieldName
+                         fieldType:(NSString*)fieldType
+                              type:(NSString*)type
+                        typedValue:(NSString*)typedValue
+                          webState:(web::WebState*)webState
+                 completionHandler:(SuggestionsReadyCompletion)completion {
+  dispatch_async(dispatch_get_main_queue(), ^{
+    completion(_suggestionsByFormAndFieldName[formName], self);
+  });
+}
+
+- (void)didSelectSuggestion:(FormSuggestion*)suggestion
+                   forField:(NSString*)fieldName
+                       form:(NSString*)formName
+          completionHandler:(SuggestionHandledCompletion)completion {
+  _selectedSuggestionByFormAndFieldName[formName] = suggestion;
+  dispatch_async(dispatch_get_main_queue(), ^{
+    completion();
+  });
+}
+
+#pragma mark - Private Methods
+
+- (NSString*)keyForFormName:(NSString*)formName fieldName:(NSString*)fieldName {
+  return [NSString stringWithFormat:@"%@-%@", formName, fieldName];
+}
+
+@end
