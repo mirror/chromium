92651ccf07868c580eb94fb18984e81f30d2c623
Daniel Murphy <dmurph@chromium.org>
2017-11-21 13:15:28 -0800

    [BlobStorage] Adding size configurations to perftests
    Bug:782356
    Change-Id: I571b3780b314517097585af1dd488f360511ef80

--- tools/perf/benchmarks/blink_perf.py
+++ tools/perf/benchmarks/blink_perf.py
@@ -506,6 +506,12 @@ class BlinkPerfOWPStorage(_BlinkPerfBenchmark):
   tag = 'owp_storage'
   subdir = 'OWPStorage'
 
+  # This ensures that all blobs over 40MB will be transported by files.
+  def SetExtraBrowserOptions(self, options):
+    options.AppendExtraBrowserArgs([
+        '--blob-transport-by-file-min-size=41943040',
+    ])
+
   def GetExpectations(self):
     class StoryExpectations(story.expectations.StoryExpectations):
       def SetExpectations(self):
