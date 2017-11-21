bfe2c9f40888bd8c388c18a4e6ea6a7e0d6bc25c
Greg Thompson <grt@chromium.org>
2017-11-21 11:53:27 +0100

    Enable interactive_ui_tests on Windows that now run mostly reliably.
    r516772 was the last of many commits that resolved a number of problems
    that made this test suite flaky on Windows. This CL enables many tests
    that had been disabled over time due to flakes, yet now seem to run
    okay.
    
    BUG=50025,62311,62538,62539,62542,69034,69475,79493,81451,146006,147579,152938,158762,159000,171080,179063,253092,267119,270341,281527,293445,326986,331924,367199,401226,404081,419468,421116,428813,431575,499858,504054,515717,523255,544037,544998,581131,583124,617056,638692,639130,665296,704994,705203,751031,751543,761783,764415
    
    Change-Id: I4e7b3ac3f768ba8aab41d009b27d366f7eef453c

--- chrome/browser/autofill/autofill_interactive_uitest.cc
+++ chrome/browser/autofill/autofill_interactive_uitest.cc
@@ -1610,8 +1610,15 @@ IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest, DISABLED_FormFillableOnReset) {
 
 // Test Autofill distinguishes a middle initial in a name.
 // Flakily times out: http://crbug.com/270341
+#if defined(OS_WIN)
+#define MAYBE_DistinguishMiddleInitialWithinName \
+  DistinguishMiddleInitialWithinName
+#else
+#define MAYBE_DistinguishMiddleInitialWithinName \
+  DISABLED_DistinguishMiddleInitialWithinName
+#endif
 IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest,
-                       DISABLED_DistinguishMiddleInitialWithinName) {
+                       MAYBE_DistinguishMiddleInitialWithinName) {
   CreateTestProfile();
 
   GURL url =
@@ -1625,8 +1632,15 @@ IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest,
 // Test forms with multiple email addresses are filled properly.
 // Entire form should be filled with one user gesture.
 // Flakily times out: http://crbug.com/270341
+#if defined(OS_WIN)
+#define MAYBE_MultipleEmailFilledByOneUserGesture \
+  MultipleEmailFilledByOneUserGesture
+#else
+#define MAYBE_MultipleEmailFilledByOneUserGesture \
+  DISABLED_MultipleEmailFilledByOneUserGesture
+#endif
 IN_PROC_BROWSER_TEST_F(AutofillInteractiveTest,
-                       DISABLED_MultipleEmailFilledByOneUserGesture) {
+                       MAYBE_MultipleEmailFilledByOneUserGesture) {
   std::string email("bsmith@gmail.com");
 
   AutofillProfile profile;
