86c75fca345903960beccfee276f694bf03e7fa9
Victor Miura <vmiura@chromium.org>
2017-11-20 19:13:19 -0800

    gpu: Simplify CHROMIUM_texture_mailbox API.
    - Remove ProduceTextureCHROMIUM API from GLES2Interface.  Change all call sites
      to use ProduceTextureDirectCHROMIUM.
    - Remove <target> from ProduceTextureDirectCHROMIUM API.
    
    BUG=757607
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ic50059a0db3edfaeb51690645bce096f4f64228e

--- cc/test/layer_tree_pixel_test.cc
+++ cc/test/layer_tree_pixel_test.cc
@@ -248,8 +248,8 @@ SkBitmap LayerTreePixelTest::CopyTextureMailboxToBitmap(
   gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
   gl->TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  GLuint texture_id = gl->CreateAndConsumeTextureCHROMIUM(
-      texture_mailbox.target(), texture_mailbox.name());
+  GLuint texture_id =
+      gl->CreateAndConsumeTextureCHROMIUM(texture_mailbox.name());
 
   GLuint fbo = 0;
   gl->GenFramebuffers(1, &fbo);
