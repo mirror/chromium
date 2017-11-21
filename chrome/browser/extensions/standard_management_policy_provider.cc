d8418f4a2e4c8e8e21f31cf8048586cc44816ba7
Karan Bhatia <karandeepb@chromium.org>
2017-11-21 00:03:38 -0800

    Extensions: Whitelist bookmark apps in StandardManagementPolicyProvider.
    r496521 made the extension management policy checks more consistent. This had a
    side effect of disabling bookmark apps when the appropriate
    ExtensionInstallBlacklist/ExtensionAllowedTypes/ExtensionSettings policies were
    set. This CL whitelists bookmark apps in
    StandardManagementPolicyProvider::UserMayLoad. Hence the bookmark apps should no
    longer get disabled by the ExtensionInstallBlacklist, ExtensionAllowedTypes and
    ExtensionSettings policies.
    
    BUG=786061
    TEST=On Linux, navigate to google.com. Use chrome -> settings -> more tools ->
    "Add to Desktop..." to create a desktop shortcut for google.com. Change
    ExtensionInstallBlacklist to block all extensions ("*"). Ensure the same from
    chrome://policy. Click on the google.com shortcut created earlier (from
    chrome://apps or Desktop) and ensure that it works correctly.
    
    Change-Id: I586d35c5dae43646288caa8d50bc08430727f26c

--- chrome/browser/extensions/standard_management_policy_provider.cc
+++ chrome/browser/extensions/standard_management_policy_provider.cc
@@ -70,6 +70,12 @@ bool StandardManagementPolicyProvider::UserMayLoad(
   if (extension->is_shared_module())
     return true;
 
+  // Always allow bookmark apps. The fact that bookmark apps are an extension is
+  // an internal implementation detail and hence they should not be controlled
+  // by extension management policies. See crbug.com/786061.
+  if (extension->from_bookmark())
+    return true;
+
   ExtensionManagement::InstallationMode installation_mode =
       settings_->GetInstallationMode(extension);
 
