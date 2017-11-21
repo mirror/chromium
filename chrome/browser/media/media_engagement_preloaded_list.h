fb7c24c7a5cf9d2408c1d2867218be8782c18330
Becca Hughes <beccahughes@chromium.org>
2017-11-21 14:45:53 +0000

    Media Engagement: Add class to load preloaded list.
    Add a MediaEngagementPreloadedList class to load a stored trie from a
    chrome_browser_media::PreloadedData proto and check if origins are
    contained within.
    
    BUG=787464
    
    Change-Id: Ib5bdc464c0cf5fdaa4fbfd69252ff4078eb71ead

---
+++ chrome/browser/media/media_engagement_preloaded_list.h
@@ -0,0 +1,52 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_MEDIA_MEDIA_ENGAGEMENT_PRELOADED_LIST_H_
+#define CHROME_BROWSER_MEDIA_MEDIA_ENGAGEMENT_PRELOADED_LIST_H_
+
+#include <string>
+#include <vector>
+
+#include "base/macros.h"
+
+namespace base {
+class FilePath;
+}  // namespace base
+
+namespace url {
+class Origin;
+}  // namespace url
+
+class MediaEngagementPreloadedList {
+ public:
+  MediaEngagementPreloadedList();
+  ~MediaEngagementPreloadedList();
+
+  // Load the contents from |path|.
+  bool LoadFromFile(base::FilePath path);
+
+  // Checks whether |origin| has a high global engagement and is present in the
+  // preloaded list.
+  bool CheckOriginIsPresent(url::Origin origin);
+
+ private:
+  // Sets |result| to true if |input| is present in the list. If there was an
+  // internal error when checking the list the function itself will return
+  // false.
+  bool CheckStringIsPresent(std::string input, bool* result);
+
+  // The huffman tree.
+  std::vector<uint8_t> huffman_tree_;
+
+  // The trie data and the size of the trie.
+  std::vector<uint8_t> trie_data_;
+  size_t trie_bits_ = 0;
+
+  // The root position of the trie.
+  size_t root_position_;
+
+  DISALLOW_COPY_AND_ASSIGN(MediaEngagementPreloadedList);
+};
+
+#endif  // CHROME_BROWSER_MEDIA_MEDIA_ENGAGEMENT_PRELOADED_LIST_H_
