3e465470e453f8f09422f1b855e211edbd65dfaf
Hwanseung Lee <hs1217.lee@samsung.com>
2017-11-22 08:30:05 +0900

    relative_url
    Bug: 783031
    Change-Id: I4d375dd1a5452d08883d6726e95e1b7baebb3816

--- third_party/WebKit/Source/core/css/cssom/CSSStyleValue.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSStyleValue.cpp
@@ -34,11 +34,8 @@ CSSStyleValueVector ParseCSSStyleValue(
     return CSSStyleValueVector();
   }
 
-  // TODO(crbug.com/783031): This should probably use an existing parser context
-  // (e.g. from execution context) to parse relative URLs correctly.
   const CSSValue* css_value = CSSParser::ParseSingleValue(
-      property_id, value,
-      StrictCSSParserContext(execution_context->SecureContextMode()));
+      property_id, value, CSSParserContext::Create(*execution_context));
 
   if (!css_value) {
     exception_state.ThrowDOMException(
