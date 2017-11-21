2dada21bdff006a106ae79d4779eeac862a217df
Tetsui Ohkubo <tetsui@chromium.org>
2017-11-21 14:42:10 +0900

    Add fade in/out animation to message center button bar.
    According to the mock, during transition from MessageListView to
    NotifierSettingsView, the MessageCenterButtonBar should show fading
    animation from the settings button to the collapse button.
    
    Mock:
    http://gallery/projects/MCHbtQVoQ2HCZYhMpwexg3v6/files/MCEGdjSmZqO3NGtbvgcxElggwMSJbxAVNpw
    
    TEST=manually tested on both English and Hebrew (RTL language)
    BUG=779877
    
    Change-Id: I2b61990029d70eaa6b90b3124aa88d625471b92a

--- ash/message_center/message_center_button_bar.cc
+++ ash/message_center/message_center_button_bar.cc
@@ -13,6 +13,7 @@
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/simple_menu_model.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/text_constants.h"
@@ -100,6 +101,9 @@ MessageCenterButtonBar::MessageCenterButtonBar(
   layout->SetFlexForView(notification_label_, 1);
 
   button_container_ = new views::View;
+  button_container_->SetPaintToLayer();
+  button_container_->SetBackground(
+      views::CreateSolidBackground(message_center_style::kBackgroundColor));
   button_container_->SetLayoutManager(
       new views::BoxLayout(views::BoxLayout::kHorizontal));
   close_all_button_ = new views::ImageButton(this);
@@ -139,6 +143,9 @@ MessageCenterButtonBar::MessageCenterButtonBar(
   button_container_->AddChildView(CreateVerticalSeparator());
 
   collapse_button_ = new views::ImageButton(this);
+  collapse_button_->SetBackground(
+      views::CreateSolidBackground(message_center_style::kBackgroundColor));
+  collapse_button_->SetPaintToLayer();
   collapse_button_->SetImage(
       views::Button::STATE_NORMAL,
       gfx::CreateVectorIcon(kNotificationCenterCollapseIcon,
@@ -192,9 +199,49 @@ views::Button* MessageCenterButtonBar::GetSettingsButtonForTest() const {
 }
 
 void MessageCenterButtonBar::SetBackArrowVisible(bool visible) {
-  collapse_button_->SetVisible(visible);
-  button_container_->SetVisible(!visible);
-  Layout();
+  if (back_arrow_visible_ == visible)
+    return;
+
+  back_arrow_visible_ = visible;
+
+  collapse_button_->SetVisible(true);
+  button_container_->SetVisible(true);
+
+  collapse_button_->layer()->SetOpacity(visible ? 0.0 : 1.0);
+  button_container_->layer()->SetOpacity(visible ? 1.0 : 0.0);
+
+  ui::ScopedLayerAnimationSettings collapse_settings(
+      collapse_button_->layer()->GetAnimator());
+  collapse_settings.AddObserver(this);
+  collapse_settings.SetTweenType(gfx::Tween::EASE_IN_OUT);
+  collapse_settings.SetTransitionDuration(base::TimeDelta::FromMilliseconds(
+      message_center_style::kDefaultAnimationDurationMs));
+
+  ui::ScopedLayerAnimationSettings container_settings(
+      button_container_->layer()->GetAnimator());
+  container_settings.SetTweenType(gfx::Tween::EASE_IN_OUT);
+  container_settings.SetTransitionDuration(base::TimeDelta::FromMilliseconds(
+      message_center_style::kDefaultAnimationDurationMs));
+
+  collapse_button_->layer()->SetOpacity(visible ? 1.0 : 0.0);
+  button_container_->layer()->SetOpacity(visible ? 0.0 : 1.0);
+}
+
+void MessageCenterButtonBar::OnImplicitAnimationsCompleted() {
+  bool settings_focused =
+      GetFocusManager() &&
+      (GetFocusManager()->GetFocusedView() == collapse_button_ ||
+       GetFocusManager()->GetFocusedView() == settings_button_);
+
+  if (settings_focused) {
+    if (back_arrow_visible_)
+      collapse_button_->RequestFocus();
+    else
+      settings_button_->RequestFocus();
+  }
+
+  collapse_button_->SetVisible(back_arrow_visible_);
+  button_container_->SetVisible(!back_arrow_visible_);
 }
 
 void MessageCenterButtonBar::SetTitle(const base::string16& title) {
@@ -217,6 +264,22 @@ void MessageCenterButtonBar::SetQuietModeState(bool is_quiet_mode) {
 
 void MessageCenterButtonBar::ChildVisibilityChanged(views::View* child) {
   InvalidateLayout();
+}
+
+void MessageCenterButtonBar::Layout() {
+  views::View::Layout();
+
+  // The settings button and the collapse button always occupy the same space.
+  // They have to be explicitly laid out for animation.
+  gfx::Size button_container_size = button_container_->GetPreferredSize();
+  button_container_->SetBounds(
+      width() - button_container_size.width(), button_container_->y(),
+      button_container_size.width(), button_container_size.height());
+
+  gfx::Size collapse_button_size = collapse_button_->GetPreferredSize();
+  collapse_button_->SetBounds(
+      width() - collapse_button_size.width(), collapse_button_->y(),
+      collapse_button_size.width(), collapse_button_size.height());
 }
 
 void MessageCenterButtonBar::ButtonPressed(views::Button* sender,
