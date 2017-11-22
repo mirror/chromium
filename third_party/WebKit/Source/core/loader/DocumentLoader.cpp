5eaaa88afbfa0f271da4177bb4f85d3146cdbdc0
Ian Clelland <iclelland@google.com>
2017-11-22 16:13:12 -0500

    Track CSP-set sandbox flags separately from frame owner flags
    Bug: 740556
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: If696428160446e636eaa1a9fa3ef7e067b0c8922

--- third_party/WebKit/Source/core/loader/DocumentLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentLoader.cpp
@@ -1137,10 +1137,6 @@ void DocumentLoader::InstallNewDocument(
   // FeaturePolicy is reset in the browser process on commit, so this needs to
   // be initialized and replicated to the browser process after commit messages
   // are sent in didCommitNavigation().
-  // Feature-Policy header is currently disabled while the details of the policy
-  // syntax are being worked out. Unless the Feature Policy experimental
-  // features flag is enabled, then ignore any header received.
-  // TODO(iclelland): Re-enable once the syntax is finalized. (crbug.com/737643)
   document->SetFeaturePolicy(
       RuntimeEnabledFeatures::FeaturePolicyEnabled()
           ? response_.HttpHeaderField(HTTPNames::Feature_Policy)
