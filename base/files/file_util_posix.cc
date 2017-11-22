ce9b050bda0b86488a9d5f0a68f84a4372f05b00
bsheedy <bsheedy@chromium.org>
2017-11-22 13:55:36 -0800

    WIP Update Android NDK to r16
    All the hardcoding
    
    Bug: 771171
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I643f9f7bd75d111e540778d2fdf8c20851485a5a

--- base/files/file_util_posix.cc
+++ base/files/file_util_posix.cc
@@ -66,7 +66,7 @@ namespace base {
 
 namespace {
 
-#if defined(OS_BSD) || defined(OS_MACOSX) || defined(OS_NACL)
+#if defined(OS_BSD) || defined(OS_MACOSX) || defined(OS_NACL) || defined(OS_ANDROID) && __ANDROID_API__ < 21
 static int CallStat(const char *path, stat_wrapper_t *sb) {
   AssertBlockingAllowed();
   return stat(path, sb);
@@ -75,7 +75,7 @@ static int CallLstat(const char *path, stat_wrapper_t *sb) {
   AssertBlockingAllowed();
   return lstat(path, sb);
 }
-#else  //  defined(OS_BSD) || defined(OS_MACOSX) || defined(OS_NACL)
+#else  //  defined(OS_BSD) || defined(OS_MACOSX) || defined(OS_NACL) || defined(OS_ANDROID) && __ANDROID_API__ < 21
 static int CallStat(const char *path, stat_wrapper_t *sb) {
   AssertBlockingAllowed();
   return stat64(path, sb);
