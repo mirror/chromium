9211b3b0abe4512963f788654a0073a6c87fc709
Xi Han <hanxi@google.com>
2017-11-22 15:54:03 -0500

    Chrome sends notifications with Channel to WebAPKs which targets SDK 26 on Android O.
    In Chrome:
    - WebAPK's channel is set under "Sites" notification group and
      "readable origin" as the channel, the same channel as Webapps.
    - Chrome checks if the WebAPK has targeted to SDK 26, and don't
      set a channel to the notification if hasn't yet.
    
    In WebAPK, we update the WebApkServiceImpl in the runtime library
    to make sure the following cases all work:
    - New WebAPK, new Chrome;
    - New WebAPK, old Chrome;
    - Old WebAPK, new Chrome.
    
    Bug:700228
    Change-Id: I176a8d45f021a33ea441837476b1ca09e973a48b

--- chrome/android/webapk/libs/runtime_library/src/org/chromium/webapk/lib/runtime_library/WebApkServiceImpl.java
+++ chrome/android/webapk/libs/runtime_library/src/org/chromium/webapk/lib/runtime_library/WebApkServiceImpl.java
@@ -5,6 +5,7 @@
 package org.chromium.webapk.lib.runtime_library;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Binder;
@@ -13,6 +14,8 @@ import android.os.Parcel;
 import android.os.RemoteException;
 import android.support.v4.app.NotificationManagerCompat;
 
+import java.lang.reflect.Field;
+
 /**
  * Implements services offered by the WebAPK to Chrome.
  */
@@ -20,6 +23,11 @@ public class WebApkServiceImpl extends IWebApkApi.Stub {
 
     public static final String KEY_SMALL_ICON_ID = "small_icon_id";
     public static final String KEY_HOST_BROWSER_UID = "host_browser_uid";
+    public static final String KEY_DEFAULT_CHANNEL_NAME = "default_channel_name";
+    public static final String KEY_TARGET_SDK = "target_sdk";
+
+    /** The default channel id of the WebAPK. */
+    private static final String DEFAULT_NOTIFICATION_CHANNEL_ID = "default_channel_id";
 
     private static final String TAG = "WebApkServiceImpl";
 
@@ -36,6 +44,12 @@ public class WebApkServiceImpl extends IWebApkApi.Stub {
      */
     private final int mHostUid;
 
+    /** The default channel name of a WebAPK. */
+    private final String mChannelName;
+
+    /** The target SDK of the owner WebAPK. */
+    private int mTargetSdk = 23;
+
     /**
      * Creates an instance of WebApkServiceImpl.
      * @param context
@@ -44,7 +58,9 @@ public class WebApkServiceImpl extends IWebApkApi.Stub {
     public WebApkServiceImpl(Context context, Bundle bundle) {
         mContext = context;
         mSmallIconId = bundle.getInt(KEY_SMALL_ICON_ID);
+        mChannelName = bundle.getString(KEY_DEFAULT_CHANNEL_NAME);
         mHostUid = bundle.getInt(KEY_HOST_BROWSER_UID);
+        mTargetSdk = bundle.getInt(KEY_TARGET_SDK);
         assert mHostUid >= 0;
     }
 
@@ -63,9 +79,35 @@ public class WebApkServiceImpl extends IWebApkApi.Stub {
         return mSmallIconId;
     }
 
+    @SuppressWarnings("NewApi")
     @Override
     public void notifyNotification(String platformTag, int platformID, Notification notification) {
+        NotificationManager manager = getNotificationManager();
+        // We always rewrite the notification channel id to the WebAPK's default channel id on O+ if
+        // WebAPK targets to SDK 26+.
+        if (supportNotificationChannel()) {
+            ensureNotificationChannelExists(DEFAULT_NOTIFICATION_CHANNEL_ID, mChannelName);
+            if (!setNotificationChannelId(notification)) return;
+        }
         getNotificationManager().notify(platformTag, platformID, notification);
+    }
+
+    @SuppressWarnings("NewApi")
+    @Override
+    public void notifyNotificationWithChannel(
+            String platformTag, int platformID, Notification notification, String channelName) {
+        NotificationManager manager = getNotificationManager();
+        // We always rewrite the notification channel id to the WebAPK's default channel id on O+.
+        if (supportNotificationChannel()) {
+            String channelId = notification.getChannelId();
+            if (channelId == null) {
+                ensureNotificationChannelExists(DEFAULT_NOTIFICATION_CHANNEL_ID, mChannelName);
+                if (!setNotificationChannelId(notification)) return;
+            } else {
+                ensureNotificationChannelExists(channelId, channelName);
+            }
+        }
+        manager.notify(platformTag, platformID, notification);
     }
 
     @Override
@@ -80,5 +122,37 @@ public class WebApkServiceImpl extends IWebApkApi.Stub {
 
     private NotificationManager getNotificationManager() {
         return (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    private boolean supportNotificationChannel() {
+        return (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+                && mTargetSdk >= android.os.Build.VERSION_CODES.O;
+    }
+
+    @SuppressWarnings("NewApi")
+    /** Creates a WebAPK notification channel on Android O+ if one does not exist. */
+    private void ensureNotificationChannelExists(String channelId, String channelName) {
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.O) return;
+
+        NotificationChannel channel = new NotificationChannel(
+                channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+        getNotificationManager().createNotificationChannel(channel);
+    }
+
+    /** Sets the notification channel of the given notification object via reflection. */
+    private boolean setNotificationChannelId(Notification notification) {
+        try {
+            // Before WebAPKs target to SDK 26, there isn't a channel id is set when the
+            // notification is built by Chrome. We need to set a channel id to display the
+            // notification properly.
+            Field channelId = notification.getClass().getDeclaredField("mChannelId");
+            channelId.setAccessible(true);
+            channelId.set(notification, DEFAULT_NOTIFICATION_CHANNEL_ID);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
