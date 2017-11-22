6359f6e0f29e9ae8227c81c2cff7a540d68a5b48
Yoichi Osato <yoichio@chromium.org>
2017-11-22 16:41:56 +0900

    [LayoutNG] Offset mapping for selection paint on one node
    This patch introduces offset conversion for the case where selection is within one
    Text Node.
    In such case, LayoutSelection converts start/end DOM position to text content offsets
    and hold them itself and set SelectionState::kStartAndEnd to the LayoutBlockFlow.
    At the paint phase, NGBoxFragmentPainter detects text fragment is selected through
    LayoutBlockFlow::SelectionState and get converted offset from LocalFrame, which has
    LayoutSelection.
    
    Notes: We don't implement blue highlight yet so selected text is painted white out.
    Plus, painting selected text white algorithm(L168) is broken: which paints from
    |selection_start| to end of the text.
    As a result, for following html:
    foo<span id="span">bar</span>
    <script>
      getSelection().setBaseAndExtent(span.firstChild, 1, span.firstChild, 2);
    </script>
    
    We paint "foob[a]r" in legacy.
    This patch makes NG paiting "foob[ar]"(ar is white out) but we can convert offset
    from ("bar", 1) to ("foobar", 4) and from ("bar", 2) to ("foobar", 5).
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I1ece456da0c1bda840ab629cf26d08efa581d43e

--- third_party/WebKit/Source/core/paint/ng/ng_text_fragment_painter.cc
+++ third_party/WebKit/Source/core/paint/ng/ng_text_fragment_painter.cc
@@ -4,10 +4,14 @@
 
 #include "core/paint/ng/ng_text_fragment_painter.h"
 
+#include "core/editing/FrameSelection.h"
+#include "core/frame/LocalFrame.h"
+#include "core/layout/LayoutBlockFlow.h"
 #include "core/layout/ng/inline/ng_physical_text_fragment.h"
 #include "core/layout/ng/ng_physical_box_fragment.h"
 #include "core/layout/ng/ng_text_decoration_offset.h"
 #include "core/paint/PaintInfo.h"
+#include "core/paint/SelectionPaintingUtils.h"
 #include "core/paint/TextPainterBase.h"
 #include "core/paint/ng/ng_paint_fragment.h"
 #include "core/paint/ng/ng_text_painter.h"
@@ -21,6 +25,82 @@ NGTextFragmentPainter::NGTextFragmentPainter(
     const NGPaintFragment& text_fragment)
     : fragment_(text_fragment) {
   DCHECK(text_fragment.PhysicalFragment().IsText());
+}
+
+static unsigned clampOffset(unsigned offset,
+                            const NGPhysicalTextFragment& text_fragment) {
+  return std::min(std::max(offset, text_fragment.StartOffset()),
+                  text_fragment.EndOffset());
+}
+
+static std::pair<unsigned, unsigned> GetSelectionStartEndInFragment(
+    const Document& document,
+    const NGPhysicalTextFragment& text_fragment) {
+  // FrameSelection holds selection offsets in layout block flow at
+  // LayoutSelection::Commit().
+  // Unlike legacy layout tree, LayoutNGBlockFlow has only one atomic
+  // SelectionState.
+  const FrameSelection& selection = document.GetFrame()->Selection();
+  switch (text_fragment.GetLayoutObject()
+              ->EnclosingNGBlockFlow()
+              ->GetSelectionState()) {
+    case SelectionState::kStart: {
+      unsigned start_in_block =
+          (unsigned)selection.LayoutSelectionStart().value_or(0);
+      return {clampOffset(start_in_block, text_fragment),
+              text_fragment.EndOffset()};
+    }
+    case SelectionState::kEnd: {
+      unsigned end_in_block =
+          (unsigned)selection.LayoutSelectionEnd().value_or(0);
+      return {text_fragment.StartOffset(),
+              clampOffset(end_in_block, text_fragment)};
+    }
+    case SelectionState::kStartAndEnd: {
+      unsigned start_in_block =
+          (unsigned)selection.LayoutSelectionStart().value_or(0);
+      unsigned end_in_block =
+          (unsigned)selection.LayoutSelectionEnd().value_or(0);
+      return {clampOffset(start_in_block, text_fragment),
+              clampOffset(end_in_block, text_fragment)};
+    }
+    case SelectionState::kInside:
+      return {text_fragment.StartOffset(), text_fragment.EndOffset()};
+    default:
+      // This block is not included in selection.
+      return {0, 0};
+  }
+}
+
+static void PaintSelection(const Document& document,
+                           const NGPhysicalTextFragment& text_fragment,
+                           const ComputedStyle& style,
+                           GraphicsContext& context,
+                           const LayoutRect& box_rect,
+                           const Font& font,
+                           Color text_color,
+                           unsigned int selection_start,
+                           unsigned int selection_end) {
+  Color c = SelectionPaintingUtils::SelectionBackgroundColor(
+      document, style, text_fragment.GetNode());
+  if (!c.Alpha())
+    return;
+
+  // If the text color ends up being the same as the selection background,
+  // invert the selection background.
+  if (text_color == c)
+    c = Color(0xff - c.Red(), 0xff - c.Green(), 0xff - c.Blue());
+
+  TextRun text_run = TextRun(text_fragment.Text(), box_rect.X().ToFloat());
+  GraphicsContextStateSaver state_saver(context);
+  DCHECK_NE(text_fragment.StartOffset(), selection_start);
+  DCHECK_NE(text_fragment.StartOffset(), selection_end);
+  LayoutRect selection_rect = LayoutRect(font.SelectionRectForText(
+      text_run, FloatPoint(box_rect.Location()), box_rect.Height().ToFloat(),
+      selection_start - text_fragment.StartOffset(),
+      selection_end - text_fragment.StartOffset()));
+
+  context.FillRect(FloatRect(selection_rect), c);
 }
 
 void NGTextFragmentPainter::Paint(const Document& document,
@@ -47,7 +127,13 @@ void NGTextFragmentPainter::Paint(const Document& document,
   bool is_printing = paint_info.IsPrinting();
 
   // Determine whether or not we're selected.
-  bool have_selection = false;  // TODO(layout-dev): Implement.
+  const NGPhysicalTextFragment& text_fragment =
+      ToNGPhysicalTextFragment(fragment_.PhysicalFragment());
+  int selection_start;
+  int selection_end;
+  std::tie(selection_start, selection_end) =
+      GetSelectionStartEndInFragment(document, text_fragment);
+  const bool have_selection = selection_start < selection_end;
   if (!have_selection && paint_info.phase == PaintPhase::kSelection) {
     // When only painting the selection, don't bother to paint if there is none.
     return;
@@ -73,18 +159,14 @@ void NGTextFragmentPainter::Paint(const Document& document,
 
   // 1. Paint backgrounds behind text if needed. Examples of such backgrounds
   // include selection and composition highlights.
-  if (paint_info.phase != PaintPhase::kSelection &&
+
+  if (have_selection && paint_info.phase != PaintPhase::kSelection &&
       paint_info.phase != PaintPhase::kTextClip && !is_printing) {
-    // TODO(layout-dev): Implement.
+    PaintSelection(document, text_fragment, style, context, box_rect, font,
+                   selection_style.fill_color, selection_start, selection_end);
   }
 
   // 2. Now paint the foreground, including text and decorations.
-  int selection_start = 0;
-  int selection_end = 0;
-
-  const NGPhysicalTextFragment& text_fragment =
-      ToNGPhysicalTextFragment(fragment_.PhysicalFragment());
-
   NGTextPainter text_painter(context, font, text_fragment, text_origin,
                              box_rect, text_fragment.IsHorizontal());
 
@@ -123,12 +205,15 @@ void NGTextFragmentPainter::Paint(const Document& document,
     int start_offset = text_fragment.StartOffset();
     int end_offset = start_offset + length;
 
-    if (paint_selected_text_separately && selection_start < selection_end) {
-      start_offset = selection_end;
-      end_offset = selection_start;
+    if (have_selection && paint_selected_text_separately) {
+      // Paint only the text that is not selected.
+      if (start_offset < selection_start)
+        text_painter.Paint(start_offset, selection_start, length, text_style);
+      if (selection_end < end_offset)
+        text_painter.Paint(selection_end, end_offset, length, text_style);
+    } else {
+      text_painter.Paint(start_offset, end_offset, length, text_style);
     }
-
-    text_painter.Paint(start_offset, end_offset, length, text_style);
 
     // Paint line-through decoration if needed.
     if (has_line_through_decoration) {
@@ -138,9 +223,9 @@ void NGTextFragmentPainter::Paint(const Document& document,
     }
   }
 
-  if ((paint_selected_text_only || paint_selected_text_separately) &&
-      selection_start < selection_end) {
-    // Paint only the text that is selected
+  if (have_selection &&
+      (paint_selected_text_only || paint_selected_text_separately)) {
+    // Paint only the text that is selected.
     text_painter.Paint(selection_start, selection_end, length, selection_style);
   }
 }
