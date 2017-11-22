ee2ae8caf72bdb6ece23337fdb57f15952c1f98b
Anna Maria <offenwanger@chromium.org>
2017-11-22 14:58:22 -0800

    Giving vr_device_manager controll of when GetDevices callback returns
    Because it is impossible to syncronously determine if a device supports
    orietation events, if we want to supply a device that uses them, the
    vr_device_manager has to be able to decide when the GetDevices return call
    should fire.
    
    BUG: 695937
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I2bca74037793f7e2940a935237fb7cc6aa0bfdf3

--- chrome/browser/vr/service/vr_device_manager.h
+++ chrome/browser/vr/service/vr_device_manager.h
@@ -11,6 +11,7 @@
 #include <memory>
 #include <vector>
 
+#include "base/callback.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
 #include "base/threading/thread_checker.h"
@@ -38,7 +39,7 @@ class VRDeviceManager {
   // Automatically connects all currently available VR devices by querying
   // the device providers and, for each returned device, calling
   // VRServiceImpl::ConnectDevice.
-  void AddService(VRServiceImpl* service);
+  void AddService(VRServiceImpl* service, base::OnceClosure callback);
   void RemoveService(VRServiceImpl* service);
 
   device::VRDevice* GetDevice(unsigned int index);
