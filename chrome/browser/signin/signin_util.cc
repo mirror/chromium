d2315dd100178915b54411fd6502c648447f579f
Mihai Sardarescu <msarda@chromium.org>
2017-11-22 16:52:15 +0100

    [signin] Open Gaia add session URL in the current tab.
    This CL opens Gaia add session URL in the current tab when sign-in from
    is started from the welcome page. This CL also add google.com as the continue
    URL for the as otherwise Gaia navigates to
     myaccount which may be very
    confusing for the user if Chrome somehow fails to load the new tab at the end
    of the sign-in flow.
    
    Bug: 787843
    Change-Id: I5f3b942e71f92d856cecf5f413bab684aeb51c4b

--- chrome/browser/signin/signin_util.cc
+++ chrome/browser/signin/signin_util.cc
@@ -7,6 +7,8 @@
 #include "chrome/browser/browser_process.h"
 #include "chrome/common/pref_names.h"
 #include "components/prefs/pref_service.h"
+#include "google_apis/gaia/gaia_urls.h"
+#include "net/base/url_util.h"
 
 namespace signin_util {
 namespace {
@@ -40,6 +42,13 @@ void SetForceSigninForTesting(bool enable) {
 
 void ResetForceSigninForTesting() {
   g_is_force_signin_enabled_cache = NOT_CACHED;
+}
+
+GURL GetGaiaAddAccountUrlForDice() {
+  // Pass www.gooogle.com as the continue URL as otherwise Gaia navigates to
+  // myaccount which may be very confusing for the user.
+  return net::AppendQueryParameter(GaiaUrls::GetInstance()->add_account_url(),
+                                   "continue", "https://www.google.com");
 }
 
 }  // namespace signin_util
