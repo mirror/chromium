c0083bb594c4b8c431a0d74cd3bfdd18b99de39c
Koji Ishii <kojii@chromium.org>
2017-11-22 23:02:56 +0900

    inline-block
    Bug:
    Change-Id: Ifbeb0f93105f5c21d723d1c0c664a63d2df58511

--- third_party/WebKit/Source/core/paint/ng/ng_box_fragment_painter.cc
+++ third_party/WebKit/Source/core/paint/ng/ng_box_fragment_painter.cc
@@ -15,6 +15,7 @@
 #include "core/paint/AdjustPaintOffsetScope.h"
 #include "core/paint/BackgroundImageGeometry.h"
 #include "core/paint/BoxDecorationData.h"
+#include "core/paint/ObjectPainter.h"
 #include "core/paint/PaintInfo.h"
 #include "core/paint/PaintLayer.h"
 #include "core/paint/PaintPhase.h"
@@ -338,10 +339,16 @@ void NGBoxFragmentPainter::PaintChildren(
     if (fragment.Type() == NGPhysicalFragment::kFragmentBox) {
       if (child->HasSelfPaintingLayer())
         continue;
-      if (RequiresLegacyFallback(fragment))
-        fragment.GetLayoutObject()->Paint(paint_info, paint_offset);
-      else
+      if (RequiresLegacyFallback(fragment)) {
+        if (fragment.IsInlineBox()) {
+          ObjectPainter(*fragment.GetLayoutObject())
+            .PaintAllPhasesAtomically(paint_info, paint_offset);
+        } else {
+          fragment.GetLayoutObject()->Paint(paint_info, paint_offset);
+        }
+      } else {
         NGBoxFragmentPainter(*child).Paint(paint_info, paint_offset);
+      }
     } else if (fragment.Type() == NGPhysicalFragment::kFragmentLineBox) {
       PaintLineBox(*child, paint_info, paint_offset);
     } else if (fragment.Type() == NGPhysicalFragment::kFragmentText) {
