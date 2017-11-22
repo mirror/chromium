e0946755b6eea19f0b5db5b460e5a72fb0499787
danakj <danakj@chromium.org>
2017-11-21 19:26:09 -0500

    resultmailbox
    R=dcheng@chromium.org, enne@chromium.org, piman@chromium.org
    
    Bug:
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Ib405fc7bda4896ed463ca5ca524049c42bcf7b3a

--- services/viz/public/cpp/compositing/copy_output_result_struct_traits.h
+++ services/viz/public/cpp/compositing/copy_output_result_struct_traits.h
@@ -6,9 +6,8 @@
 #define SERVICES_VIZ_PUBLIC_CPP_COMPOSITING_COPY_OUTPUT_RESULT_STRUCT_TRAITS_H_
 
 #include "components/viz/common/frame_sinks/copy_output_result.h"
-#include "services/viz/public/cpp/compositing/texture_mailbox_struct_traits.h"
 #include "services/viz/public/interfaces/compositing/copy_output_result.mojom-shared.h"
-#include "services/viz/public/interfaces/compositing/texture_mailbox_releaser.mojom.h"
+#include "services/viz/public/interfaces/compositing/texture_releaser.mojom.h"
 #include "skia/public/interfaces/bitmap_skbitmap_struct_traits.h"
 #include "ui/gfx/geometry/mojo/geometry_struct_traits.h"
 
@@ -60,25 +59,39 @@ struct StructTraits<viz::mojom::CopyOutputResultDataView,
 
   static const SkBitmap& bitmap(
       const std::unique_ptr<viz::CopyOutputResult>& result) {
+    if (result->format() != viz::CopyOutputResult::Format::RGBA_BITMAP)
+      return SkBitmap();  // SkBitmap is a nullable mojo type.
     return result->AsSkBitmap();
   }
 
-  static base::Optional<viz::TextureMailbox> texture_mailbox(
+  static base::Optional<gpu::Mailbox> mailbox(
       const std::unique_ptr<viz::CopyOutputResult>& result) {
-    if (HasTextureResult(*result))
-      return *result->GetTextureMailbox();
-    else
+    if (result->format() != viz::CopyOutputResult::Format::RGBA_TEXTURE)
       return base::nullopt;
+    return result->GetTextureMailbox()->mailbox;
   }
 
-  static viz::mojom::TextureMailboxReleaserPtr releaser(
+  static base::Optional<gpu::SyncToken> sync_token(
+      const std::unique_ptr<viz::CopyOutputResult>& result) {
+    if (result->format() != viz::CopyOutputResult::Format::RGBA_TEXTURE)
+      return base::nullopt;
+    return result->GetTextureMailbox()->sync_token;
+  }
+
+  static base::Optional<gfx::ColorSpace> color_space(
+      const std::unique_ptr<viz::CopyOutputResult>& result) {
+    if (result->format() != viz::CopyOutputResult::Format::RGBA_TEXTURE)
+      return base::nullopt;
+    return result->GetTextureMailbox()->color_space;
+  }
+
+  static viz::mojom::TextureReleaserPtr releaser(
       const std::unique_ptr<viz::CopyOutputResult>& result);
 
   static bool Read(viz::mojom::CopyOutputResultDataView data,
                    std::unique_ptr<viz::CopyOutputResult>* out_p);
 
  private:
-  static bool HasTextureResult(const viz::CopyOutputResult& result);
 };
 
 }  // namespace mojo
