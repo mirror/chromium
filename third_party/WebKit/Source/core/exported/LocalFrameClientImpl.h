9583c072c13edf05f81d7c15b4642357f0c02337
Sam McNally <sammc@chromium.org>
2017-11-23 10:34:19 +1100

    Expose channel-associated interfaces to blink.
    Move AssociatedInterface{Provider,Registry} into blink/common so blink
    can use them. Add an accessor in LocalFrame to provide access to the
    RenderFrame's AssociatedInterfaceProvider.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I412f3b559a9777609b2998206970f5c16d43f0c4

--- third_party/WebKit/Source/core/exported/LocalFrameClientImpl.h
+++ third_party/WebKit/Source/core/exported/LocalFrameClientImpl.h
@@ -242,6 +242,8 @@ class LocalFrameClientImpl final : public LocalFrameClient {
   std::unique_ptr<WebURLLoaderFactory> CreateURLLoaderFactory() override;
 
   service_manager::InterfaceProvider* GetInterfaceProvider() override;
+  AssociatedInterfaceProvider* GetRemoteNavigationAssociatedInterfaces()
+      override;
 
   void AnnotatedRegionsChanged() override;
 
--- third_party/WebKit/public/web/WebFrameClient.h
+++ third_party/WebKit/public/web/WebFrameClient.h
@@ -86,6 +86,7 @@ enum class WebFeature : int32_t;
 }  // namespace mojom
 
 enum class WebTreeScopeType;
+class AssociatedInterfaceProvider;
 class WebApplicationCacheHost;
 class WebApplicationCacheHostClient;
 class WebColorChooser;
@@ -198,6 +199,14 @@ class BLINK_EXPORT WebFrameClient {
   // Returns an InterfaceProvider the frame can use to request interfaces from
   // the browser. This method may not return nullptr.
   virtual service_manager::InterfaceProvider* GetInterfaceProvider();
+
+  // Returns an AssociatedInterfaceProvider the frame can use to request
+  // navigation-associated interfaces from the browser. See also
+  // LocalFrame::GetRemoteNavigationAssociatedInterfaces().
+  virtual AssociatedInterfaceProvider*
+  GetRemoteNavigationAssociatedInterfaces() {
+    return nullptr;
+  }
 
   // General notifications -----------------------------------------------
 
