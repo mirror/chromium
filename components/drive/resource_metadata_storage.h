e3a0451afa81c395f536dbe54ee50e16c64eec1e
Lei Zhang <thestig@chromium.org>
2017-11-22 14:20:10 -0800

    Reorganize ResourceMetadataStorage::UpgradeOldDB().
    Split it into version specific upgrade functions. Also fix nits in the
    unit test and elsewhere in components/drive.
    
    Change-Id: Ifda20a32115bcd006addaf0801c09512142fa15c

--- components/drive/resource_metadata_storage.h
+++ components/drive/resource_metadata_storage.h
@@ -7,6 +7,7 @@
 
 #include <stdint.h>
 
+#include <map>
 #include <memory>
 #include <string>
 #include <vector>
@@ -79,10 +80,7 @@ class ResourceMetadataStorage {
     std::string md5;
     std::string title;
   };
-  typedef std::map<std::string, RecoveredCacheInfo> RecoveredCacheInfoMap;
-
-  // Returns true if the DB was successfully upgraded to the newest version.
-  static bool UpgradeOldDB(const base::FilePath& directory_path);
+  using RecoveredCacheInfoMap = std::map<std::string, RecoveredCacheInfo>;
 
   ResourceMetadataStorage(const base::FilePath& directory_path,
                           base::SequencedTaskRunner* blocking_task_runner);
@@ -93,11 +91,11 @@ class ResourceMetadataStorage {
   // initialization.
   bool cache_file_scan_is_needed() const { return cache_file_scan_is_needed_; }
 
-  // Destroys this object.
-  void Destroy();
-
   // Initializes this object.
   bool Initialize();
+
+  // Destroys this object.
+  void Destroy();
 
   // Collects cache info from trashed resource map DB.
   void RecoverCacheInfoFromTrashedResourceMap(RecoveredCacheInfoMap* out_info);
@@ -123,15 +121,15 @@ class ResourceMetadataStorage {
   // Returns the ID of the parent's child.
   FileError GetChild(const std::string& parent_id,
                      const std::string& child_name,
-                     std::string* child_id);
+                     std::string* child_id) const;
 
   // Returns the IDs of the parent's children.
   FileError GetChildren(const std::string& parent_id,
-                        std::vector<std::string>* children);
+                        std::vector<std::string>* children) const;
 
   // Returns the local ID associated with the given resource ID.
   FileError GetIdByResourceId(const std::string& resource_id,
-                              std::string* out_id);
+                              std::string* out_id) const;
 
  private:
   friend class ResourceMetadataStorageTest;
@@ -142,7 +140,12 @@ class ResourceMetadataStorage {
   // Used to implement Destroy().
   void DestroyOnBlockingPool();
 
+  // Returns true if the DB was successfully upgraded to the newest version.
+  // Exposed in the header for unit testing.
+  static bool UpgradeOldDB(const base::FilePath& directory_path);
+
   // Returns a string to be used as a key for child entry.
+  // Exposed in the header for unit testing.
   static std::string GetChildEntryKey(const std::string& parent_id,
                                       const std::string& child_name);
 
@@ -150,13 +153,13 @@ class ResourceMetadataStorage {
   FileError PutHeader(const ResourceMetadataHeader& header);
 
   // Gets header.
-  FileError GetHeader(ResourceMetadataHeader* out_header);
+  FileError GetHeader(ResourceMetadataHeader* out_header) const;
 
   // Checks validity of the data.
   bool CheckValidity();
 
   // Path to the directory where the data is stored.
-  base::FilePath directory_path_;
+  const base::FilePath directory_path_;
 
   bool cache_file_scan_is_needed_;
 
