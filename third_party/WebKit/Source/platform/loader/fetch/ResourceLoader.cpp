41444178ed679f8ac4208b6cb9fd89b536c38dcb
Liquan (Max) Gu <maxlg@chromium.org>
2017-11-23 13:36:48 -0500

    [NetworkIdleness] Plump resource info into network events
    Not for submission. This patch is going to add the following resource info into
    network events:
    * resource timing
    * resource size
    * resource type
    
    Bug: catapult: #3974
    Change-Id: I4bd2ce05060291fefe42415e1b44724daef12472

--- third_party/WebKit/Source/platform/loader/fetch/ResourceLoader.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/ResourceLoader.cpp
@@ -630,6 +630,8 @@ void ResourceLoader::DidReceiveTransferSizeUpdate(int transfer_size_diff) {
 void ResourceLoader::DidFinishLoadingFirstPartInMultipart() {
   network_instrumentation::EndResourceLoad(
       resource_->Identifier(),
+      (char*)Resource::ResourceTypeToString(
+          resource_->GetType(), resource_->Options().initiator_info.name),
       network_instrumentation::RequestOutcome::kSuccess);
 
   fetcher_->HandleLoaderFinish(resource_.Get(), 0,
@@ -649,6 +651,8 @@ void ResourceLoader::DidFinishLoading(double finish_time,
 
   network_instrumentation::EndResourceLoad(
       resource_->Identifier(),
+      (char*)Resource::ResourceTypeToString(
+          resource_->GetType(), resource_->Options().initiator_info.name),
       network_instrumentation::RequestOutcome::kSuccess);
 
   fetcher_->HandleLoaderFinish(resource_.Get(), finish_time,
@@ -678,7 +682,10 @@ void ResourceLoader::HandleError(const ResourceError& error) {
   loader_.reset();
 
   network_instrumentation::EndResourceLoad(
-      resource_->Identifier(), network_instrumentation::RequestOutcome::kFail);
+      resource_->Identifier(),
+      (char*)Resource::ResourceTypeToString(
+          resource_->GetType(), resource_->Options().initiator_info.name),
+      network_instrumentation::RequestOutcome::kFail);
 
   fetcher_->HandleLoaderError(resource_.Get(), error);
 }
