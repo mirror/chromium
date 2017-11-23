040cc4bc2d67a21f63028595b8c061d57d9b884a
Edward Jung <edwardjung@chromium.org>
2017-11-23 20:56:38 +0000

    chrome://flags Move enabled flags to the top of the list.
    Bug: 782911
    Change-Id: I2a1d11ac1a3fb80354137f7e410f543147a8285c

--- components/flags_ui/resources/flags.js
+++ components/flags_ui/resources/flags.js
@@ -176,6 +176,25 @@ function returnExperimentalFeatures(experimentalFeaturesData) {
 }
 
 /**
+ * Handles updating the UI after experiment selections have been made.
+ * Adds or removes experiment highlighting depending on whether the experiment
+ * is set to a default option. Shows the restart button.
+ * @param {HTMLElement} node The select node for the experiment being changed.
+ * @param {HTMLElement} experimentContainerEl
+ * @param {number} index The selected option index.
+ */
+function experimentChangesUiUpdates(node, experimentContainerEl, index) {
+  var selected = node.options[index];
+
+  experimentContainerEl.className =
+      ("default" in selected.dataset && selected.dataset.default == "1") ||
+      (!("default" in selected.dataset) && index === 0) ?
+      'experiment-default flex-container' :
+      'experiment-switched flex-container';
+  $('needs-restart').classList.add('show');
+}
+
+/**
  * Handles a 'enable' or 'disable' button getting clicked.
  * @param {HTMLElement} node The node for the experiment being changed.
  * @param {boolean} enable Whether to enable or disable the experiment.
@@ -184,7 +203,7 @@ function handleEnableExperimentalFeature(node, enable) {
   // Tell the C++ FlagsDOMHandler to enable/disable the experiment.
   chrome.send('enableExperimentalFeature', [String(node.internal_name),
                                             String(enable)]);
-  requestExperimentalFeaturesData();
+  experimentChangesUiUpdates(node, node.parentNode.parentNode, enable ? 1 : 0);
 }
 
 /**
@@ -197,7 +216,8 @@ function handleSelectExperimentalFeatureChoice(node, index) {
   // Tell the C++ FlagsDOMHandler to enable the selected choice.
   chrome.send('enableExperimentalFeature',
               [String(node.internal_name) + '@' + index, 'true']);
-  requestExperimentalFeaturesData();
+  experimentChangesUiUpdates(node, node.parentNode.parentNode.parentNode,
+      index);
 }
 
 /**
