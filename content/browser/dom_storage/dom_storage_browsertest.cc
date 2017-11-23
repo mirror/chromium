fd3059068b071f144704ae4fa4d99bbc1a16ca1e
Lei Zhang <thestig@chromium.org>
2017-11-22 17:36:32 -0800

    Add content::IsPreTest().
    Checks if a test has the PRE_ prefix. Use it for tests that wants to
    know, so they do not have to have their own version.
    
    Change-Id: I4c0dbf38ab5c3ab5b088b5564a91acc61f275656

--- content/browser/dom_storage/dom_storage_browsertest.cc
+++ content/browser/dom_storage/dom_storage_browsertest.cc
@@ -16,6 +16,7 @@
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
+#include "content/public/test/test_launcher.h"
 #include "content/shell/browser/shell.h"
 
 namespace content {
@@ -123,9 +124,8 @@ class DOMStorageMigrationBrowserTest : public DOMStorageBrowserTest {
   void SetUpCommandLine(base::CommandLine* command_line) override {
     ContentBrowserTest::SetUpCommandLine(command_line);
     // Only enable mojo local storage if this is not a PRE_ test.
-    const testing::TestInfo* test =
-        testing::UnitTest::GetInstance()->current_test_info();
-    if (base::StartsWith(test->name(), "PRE_", base::CompareCase::SENSITIVE))
+    auto* test = testing::UnitTest::GetInstance();
+    if (content::IsPreTest(test->current_test_info()->name()))
       command_line->AppendSwitch(switches::kDisableMojoLocalStorage);
   }
 };
