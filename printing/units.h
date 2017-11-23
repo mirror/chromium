5ed5410d6bfdeff33c313f1c8d91ed55c7190fba
Vladislav Kuzkokov <vkuzkokov@chromium.org>
2017-11-23 14:46:08 +0100

    ARC Print
    This allows container to use native printers.
    
    Bug: 625126
    Change-Id: Idb1b1de9a11877fdfa9e9eb4e9ec87d4bf089ef7

--- printing/units.h
+++ printing/units.h
@@ -12,6 +12,9 @@ namespace printing {
 // Length of a thousandth of inches in 0.01mm unit.
 const int kHundrethsMMPerInch = 2540;
 
+// Mil is a thousandth of an inch.
+constexpr float kMicronsPerMil = 25.4f;
+
 // Length of an inch in CSS's 1pt unit.
 // http://dev.w3.org/csswg/css3-values/#absolute-length-units-cm-mm.-in-pt-pc
 const int kPointsPerInch = 72;
