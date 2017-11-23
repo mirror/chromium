f6a3b8d66933e6ae2ee5a388669f5d32c3730146
Darren Shen <shend@chromium.org>
2017-11-23 11:44:00 +1100

    [css-typed-om] Refactor StylePropertyMap.getProperties/iteration.
    This patch is a follow up of crrev.com/770606. We make getProperties
    and getIterationEntries non-virtual. To override behaviour, subclasses
    just need to implement one function:
    
        void ForEachProperty(const IterationCallback&);
    
    which should iterate through every property in the style map and
    invoke the callback. Both getProperties and getIterationEntries
    can be implemented from this function.
    
    This reduces duplicate boilerplate code across different style maps.
    
    Bug: 785132
    Change-Id: I829a50191774a7730b534b8e4cdf419ca935fe8f

--- third_party/WebKit/Source/core/css/cssom/StylePropertyMapReadonly.cpp
+++ third_party/WebKit/Source/core/css/cssom/StylePropertyMapReadonly.cpp
@@ -5,9 +5,13 @@
 #include "core/css/cssom/StylePropertyMapReadonly.h"
 
 #include "bindings/core/v8/ExceptionState.h"
+#include "core/CSSPropertyNames.h"
+#include "core/css/CSSCustomPropertyDeclaration.h"
 #include "core/css/CSSValueList.h"
 #include "core/css/cssom/CSSStyleValue.h"
+#include "core/css/cssom/CSSUnsupportedStyleValue.h"
 #include "core/css/cssom/StyleValueFactory.h"
+#include "core/css/properties/CSSProperty.h"
 
 namespace blink {
 
@@ -78,9 +82,54 @@ bool StylePropertyMapReadonly::has(const String& property_name,
   return !getAll(property_name, exception_state).IsEmpty();
 }
 
+Vector<String> StylePropertyMapReadonly::getProperties() {
+  // TODO(779841): Needs to be sorted.
+  Vector<String> result;
+
+  ForEachProperty([&result](CSSPropertyID property_id, const CSSValue& value) {
+    DCHECK(isValidCSSPropertyID(property_id));
+    if (property_id == CSSPropertyVariable)
+      result.push_back(ToCSSCustomPropertyDeclaration(value).GetName());
+    else
+      result.push_back(getPropertyNameString(property_id));
+  });
+
+  return result;
+}
+
 StylePropertyMapReadonly::IterationSource*
 StylePropertyMapReadonly::StartIteration(ScriptState*, ExceptionState&) {
-  return new StylePropertyMapIterationSource(GetIterationEntries());
+  // TODO(779841): Needs to be sorted.
+  HeapVector<StylePropertyMapReadonly::StylePropertyMapEntry> result;
+
+  ForEachProperty([&result](CSSPropertyID property_id,
+                            const CSSValue& css_value) {
+    DCHECK(isValidCSSPropertyID(property_id));
+    String name;
+    CSSStyleValueOrCSSStyleValueSequence value;
+    if (property_id == CSSPropertyVariable) {
+      // TODO(meade): Eventually custom properties will support other types, so
+      // actually return them instead of always returning a
+      // CSSUnsupportedStyleValue.
+      // TODO(779477): Should these return CSSUnparsedValues?
+      const auto& custom_declaration =
+          ToCSSCustomPropertyDeclaration(css_value);
+      name = custom_declaration.GetName();
+      value.SetCSSStyleValue(
+          CSSUnsupportedStyleValue::Create(custom_declaration.CustomCSSText()));
+    } else {
+      name = getPropertyNameString(property_id);
+      auto style_value_vector =
+          StyleValueFactory::CssValueToStyleValueVector(property_id, css_value);
+      if (style_value_vector.size() == 1)
+        value.SetCSSStyleValue(style_value_vector[0]);
+      else
+        value.SetCSSStyleValueSequence(style_value_vector);
+    }
+    result.emplace_back(name, value);
+  });
+
+  return new StylePropertyMapIterationSource(result);
 }
 
 }  // namespace blink
