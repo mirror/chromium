2e053b09d3f80eff0b3e4651f74afdb6725476c7
Minjeong Lee <apenr1234@gmail.com>
2017-11-23 20:47:34 +0900

    Moved Checking Uppercase in header from SpdyStream::SaveResponseHeaders() to HeaderCoalescer::AddHeader()
    Bug: 695495
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Ie277eaec092ee1d7541c278a7501d48902473a0d

--- net/http/bidirectional_stream_unittest.cc
+++ net/http/bidirectional_stream_unittest.cc
@@ -1371,8 +1371,7 @@ TEST_F(BidirectionalStreamTest, PropagateProtocolError) {
   // BidirectionalStreamSpdyStreamJob does not count the bytes sent for |rst|
   // because it is sent after SpdyStream::Delegate::OnClose is called.
   EXPECT_EQ(CountWriteBytes(writes, 1), delegate->GetTotalSentBytes());
-  EXPECT_EQ(CountReadBytes(reads, arraysize(reads)),
-            delegate->GetTotalReceivedBytes());
+  EXPECT_EQ(0, delegate->GetTotalReceivedBytes());
 
   TestNetLogEntry::List entries;
   net_log_.GetEntries(&entries);
@@ -1600,8 +1599,7 @@ TEST_F(BidirectionalStreamTest, DeleteStreamDuringOnFailed) {
   // Bytes sent excludes the RST frame.
   EXPECT_EQ(CountWriteBytes(writes, arraysize(writes) - 1),
             delegate->GetTotalSentBytes());
-  EXPECT_EQ(CountReadBytes(reads, arraysize(reads)),
-            delegate->GetTotalReceivedBytes());
+  EXPECT_EQ(0, delegate->GetTotalReceivedBytes());
 }
 
 TEST_F(BidirectionalStreamTest, TestHonorAlternativeServiceHeader) {
