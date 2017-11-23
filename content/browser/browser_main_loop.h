883d76f3e2933404526697ffa5ee7e440904cbe7
danakj <danakj@chromium.org>
2017-11-23 18:08:19 -0500

    Add a forwarding CompositingModeReporter to the browser for viz
    When using --enable-viz, the display compositor and thus compositing
    mode decisions are in the viz process (gpu process). This adds a
    CompositingModeReporter to the viz process, that is connected to
    the browser process when it sets up its connection to the
    FrameSinkManager.
    
    Then the browser process owns a ForwardingCompositingModeReporterImpl
    which watches the gpu process' authoritative reporter, and acts as
    a CompositingModeReporter for other compositor clients (eg renderers)
    in the same way that the authoritative one would from the browser when
    not running viz.
    
    The BrowserMainLoop makes one or the other type of reporter, and
    connects clients to it. The VizProcessTransportFactory connects the
    forwarding one to the authoritative one, and reconnects it on gpu
    process restart.
    
    R=kylechar@chromium.org
    
    Bug: 730660
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I9d9995fabf192c5ca470551f73322b5215628bc2

--- content/browser/browser_main_loop.h
+++ content/browser/browser_main_loop.h
@@ -89,6 +89,7 @@ class ImageCursorsSet;
 
 namespace viz {
 class CompositingModeReporterImpl;
+class ForwardingCompositingModeReporterImpl;
 class FrameSinkManagerImpl;
 class HostFrameSinkManager;
 }
@@ -392,6 +393,10 @@ class CONTENT_EXPORT BrowserMainLoop {
   // ForwardingCompositingModeReporterImpl that redirects to the viz process.
   mojo::BindingSet<viz::mojom::CompositingModeReporter>
       compositing_mode_reporter_bindings_;
+  // Forwards requests to watch the compositing mode on to the viz process. This
+  // is null if the display compositor in this process.
+  std::unique_ptr<viz::ForwardingCompositingModeReporterImpl>
+      forwarding_compositing_mode_reporter_impl_;
   // Reports on the compositing mode in the system for clients to submit
   // resources of the right type. This is null if the display compositor
   // is not in this process.
