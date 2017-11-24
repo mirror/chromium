6fe773a9ab61ea6f4931ef0ac30bdd7f67e32dbf
Christos Froussios <cfroussios@chromium.org>
2017-11-24 17:13:59 +0100

    🔐 Preparing the passwords for export also serialised them
    Bug:785237
    Change-Id: Ie9466840b1e4bf11ddfaa4c69045326ab3b11fe3

--- components/password_manager/core/browser/export/password_manager_exporter.cc
+++ components/password_manager/core/browser/export/password_manager_exporter.cc
@@ -42,7 +42,23 @@ PasswordManagerExporter::PasswordManagerExporter(
 PasswordManagerExporter::~PasswordManagerExporter() {}
 
 void PasswordManagerExporter::PreparePasswordsForExport() {
-  password_list_ = credential_provider_interface_->GetAllPasswords();
+  std::vector<std::unique_ptr<autofill::PasswordForm>> password_list =
+      credential_provider_interface_->GetAllPasswords();
+  size_t password_list_size = password_list.size();
+
+  base::PostTaskAndReplyWithResult(
+      task_runner_.get(), FROM_HERE,
+      base::BindOnce(&password_manager::PasswordCSVWriter::SerializePasswords,
+                     base::Passed(CopyOf(password_list))),
+      base::BindOnce(&PasswordManagerExporter::SetSerialisedPasswordList,
+                     weak_factory_.GetWeakPtr(), password_list_size));
+}
+
+void PasswordManagerExporter::SetSerialisedPasswordList(
+    size_t count,
+    const std::string& serialised) {
+  serialised_password_list_ = serialised;
+  password_list_size_ = count;
 
   if (IsReadyForExport())
     Export();
@@ -61,33 +77,30 @@ void PasswordManagerExporter::Cancel() {
   weak_factory_.InvalidateWeakPtrs();
 
   destination_.reset();
-  password_list_.clear();
+  serialised_password_list_.clear();
 }
 
 bool PasswordManagerExporter::IsReadyForExport() {
-  return destination_ && !password_list_.empty();
+  return destination_ && !serialised_password_list_.empty();
 }
 
 void PasswordManagerExporter::Export() {
   UMA_HISTOGRAM_COUNTS("PasswordManager.ExportedPasswordsPerUserInCSV",
-                       password_list_.size());
+                       password_list_size_);
 
   base::PostTaskAndReplyWithResult(
       task_runner_.get(), FROM_HERE,
-      base::BindOnce(&password_manager::PasswordCSVWriter::SerializePasswords,
-                     base::Passed(CopyOf(password_list_))),
-      base::BindOnce(&PasswordManagerExporter::OnPasswordsSerialised,
-                     weak_factory_.GetWeakPtr(),
-                     base::Passed(std::move(destination_))));
+      base::BindOnce(&Destination::Write, std::move(destination_),
+                     std::move(serialised_password_list_)),
+      base::BindOnce(&PasswordManagerExporter::OnPasswordsExported,
+                     weak_factory_.GetWeakPtr()));
 
-  password_list_.clear();
+  serialised_password_list_.clear();
+  password_list_size_ = 0;
   destination_.reset();
 }
 
-void PasswordManagerExporter::OnPasswordsSerialised(
-    std::unique_ptr<Destination> destination,
-    const std::string& serialised) {
-  bool success = destination->Write(serialised);
+void PasswordManagerExporter::OnPasswordsExported(bool success) {
   password_ui_export_view_->OnCompletedWritingToDestination(
       success ? std::string() : std::string("Writing to destination failed."));
 }
