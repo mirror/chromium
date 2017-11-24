3249ab31574f57a5f99458f45f1781f2298e3600
Roman Sorokin <rsorokin@chromium.org>
2017-11-24 11:53:06 +0100

    authpolicy_client: Switch inputs of D-Bus calls to protos.
    Should be landed after Chrome OS part will be landed.
    We switch D-Bus calls to protobufs arguments to make it easier to change
    API.
    
    BUG=chromium:785956
    TEST=manual
    CQ-DEPEND=CL:771630,CL:774741
    
    Change-Id: I23db60080e684cc1d6cc51fcbafbb10c306bc924

--- chromeos/dbus/auth_policy_client.cc
+++ chromeos/dbus/auth_policy_client.cc
@@ -69,8 +69,10 @@ class AuthPolicyClientImpl : public AuthPolicyClient {
     dbus::MethodCall method_call(authpolicy::kAuthPolicyInterface,
                                  authpolicy::kJoinADDomainMethod);
     dbus::MessageWriter writer(&method_call);
-    writer.AppendString(machine_name);
-    writer.AppendString(user_principal_name);
+    authpolicy::JoinDomainRequest request;
+    request.set_machine_name(machine_name);
+    request.set_user_principal_name(user_principal_name);
+    writer.AppendProtoAsArrayOfBytes(request);
     writer.AppendFileDescriptor(password_fd);
     proxy_->CallMethod(
         &method_call, kSlowDbusTimeoutMilliseconds,
@@ -85,8 +87,10 @@ class AuthPolicyClientImpl : public AuthPolicyClient {
     dbus::MethodCall method_call(authpolicy::kAuthPolicyInterface,
                                  authpolicy::kAuthenticateUserMethod);
     dbus::MessageWriter writer(&method_call);
-    writer.AppendString(user_principal_name);
-    writer.AppendString(object_guid);
+    authpolicy::AuthenticateUserRequest request;
+    request.set_user_principal_name(user_principal_name);
+    request.set_account_id(object_guid);
+    writer.AppendProtoAsArrayOfBytes(request);
     writer.AppendFileDescriptor(password_fd);
     proxy_->CallMethod(
         &method_call, kSlowDbusTimeoutMilliseconds,
@@ -136,7 +140,7 @@ class AuthPolicyClientImpl : public AuthPolicyClient {
     dbus::MethodCall method_call(authpolicy::kAuthPolicyInterface,
                                  authpolicy::kRefreshUserPolicyMethod);
     dbus::MessageWriter writer(&method_call);
-    writer.AppendString(account_id.GetAccountIdKey());
+    writer.AppendString(account_id.GetObjGuid());
     proxy_->CallMethod(
         &method_call, kSlowDbusTimeoutMilliseconds,
         base::BindOnce(&AuthPolicyClientImpl::HandleRefreshPolicyCallback,
