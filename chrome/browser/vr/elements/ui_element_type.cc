45e18e0ce464466316ad55d20428e30a33b882a6
Ian Vollick <vollick@chromium.org>
2017-11-25 17:16:51 -0500

    [vr] Add a depth/scale adjustment element
    This change introduces a ScaledDepthAdjuster element whose role is to
    both position its descendants at the correct depth and ensure that they
    inherit a scale that permits their units to be expressed in standard
    DMM.
    
    Bug:782395
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iec5481edcf0a60f78fd0a16c35a32c981ee84b6f

---
+++ chrome/browser/vr/elements/ui_element_type.cc
@@ -0,0 +1,33 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/vr/elements/ui_element_type.h"
+
+#include "base/macros.h"
+
+namespace vr {
+
+namespace {
+
+static const char* g_ui_element_type_strings[] = {
+    "kTypeNone",
+    "kTypeButtonBackground",
+    "kTypeButtonForeground",
+    "kTypeButtonHitTarget",
+    "kTypeScaledDepthAdjuster",
+    "kNumUiElementTypes",
+};
+
+static_assert(
+    kNumUiElementTypes + 1 == arraysize(g_ui_element_type_strings),
+    "Mismatch between the kUiElementName enum and the corresponding array "
+    "of strings.");
+
+}  // namespace
+
+std::string UiElementTypeToString(UiElementType type) {
+  return g_ui_element_type_strings[type];
+}
+
+}  // namespace vr
