de29876ccdf95371f9960414f67bdb3862835215
Renee Wright <rjwright@chromium.org>
2017-11-25 20:25:26 +1100

    [WIP] Ribbonize ComputedStyleCSSValueMapping colors
    I'm waiting on Bugs to consolidate the parser utils before landing
    this CL. Hence I've put the parser method ParseBorderColorSide in
    a temporary class called ReneesParserStuff. Obviously I will delete
    that class before landing this.
    
    This CL also changes the behavior for ColumnRuleColor.
    It used to call VisitedDependentColor(CSSPropertyOutlineColor),
    but that seemed wrong so I changed it to use
    VisitedDependentColor(CSSPropertyColumnRuleColor).
    
    There will be more CLs immediately following to finish Ribbonizing
    this class. Please ping me for a link to the design doc.
    
    Bug: 775360
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I2b204146eafdd1fd7a74d8a9b8c8bb707d5f3eea

--- third_party/WebKit/Source/core/paint/compositing/CompositedLayerMapping.cpp
+++ third_party/WebKit/Source/core/paint/compositing/CompositedLayerMapping.cpp
@@ -2713,7 +2713,7 @@ float CompositedLayerMapping::CompositingOpacity(
 
 Color CompositedLayerMapping::LayoutObjectBackgroundColor() const {
   const auto& object = GetLayoutObject();
-  auto background_color = object.ResolveColor(CSSPropertyBackgroundColor);
+  auto background_color = object.ResolveColor(GetCSSPropertyBackgroundColor());
   if (object.IsLayoutView() && object.GetDocument().IsInMainFrame()) {
     return ToLayoutView(object).GetFrameView()->BaseBackgroundColor().Blend(
         background_color);
