1f019553cee2db3fb985ab313e49213a55c4f679
Renee Wright <rjwright@chromium.org>
2017-11-25 19:09:33 +1100

    Ribbonize ComputedStyleCSSValueMapping colors
    Bug: 775360
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I2b204146eafdd1fd7a74d8a9b8c8bb707d5f3eea

--- third_party/WebKit/Source/core/css/properties/ComputedStyleUtils.cpp
+++ third_party/WebKit/Source/core/css/properties/ComputedStyleUtils.cpp
@@ -2,21 +2,33 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "core/css/properties/longhands/BorderColorSide.h"
+#include "core/css/properties/ComputedStyleUtils.h"
 
-#include "core/CSSPropertyNames.h"
+#include "core/css/CSSColorValue.h"
+#include "core/css/CSSValue.h"
+#include "core/css/StyleColor.h"
+#include "core/style/ComputedStyle.h"
+
+// TODO(rjwright): move these
 #include "core/css/parser/CSSParserContext.h"
 #include "core/css/parser/CSSParserLocalContext.h"
-#include "core/css/parser/CSSParserMode.h"
 #include "core/css/parser/CSSPropertyParserHelpers.h"
 
 namespace blink {
-namespace CSSLonghand {
 
-const CSSValue* BorderColorSide::ParseSingleValue(
+const CSSValue* ComputedStyleUtils::CurrentColorOrValidColor(
+    const ComputedStyle& style,
+    const StyleColor& color) {
+  // This function does NOT look at visited information, so that computed style
+  // doesn't expose that.
+  return cssvalue::CSSColorValue::Create(color.Resolve(style.GetColor()).Rgb());
+}
+
+// TODO(rjwright): Move this
+const CSSValue* ComputedStyleUtils::ParseBorderColorSide(
     CSSParserTokenRange& range,
     const CSSParserContext& context,
-    const CSSParserLocalContext& local_context) const {
+    const CSSParserLocalContext& local_context) {
   CSSPropertyID shorthand = local_context.CurrentShorthand();
   bool allow_quirky_colors =
       IsQuirksModeBehavior(context.Mode()) &&
@@ -25,5 +37,4 @@ const CSSValue* BorderColorSide::ParseSingleValue(
                                                 allow_quirky_colors);
 }
 
-}  // namespace CSSLonghand
 }  // namespace blink
