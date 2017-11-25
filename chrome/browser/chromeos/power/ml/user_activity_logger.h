7dee3d745ef4e1382585e474158082095bfb24a8
Renjie Liu <renjieliu@chromium.org>
2017-11-25 16:40:03 +1100

    Add more features to user activity logger
    Add device_mode feature.
    Add Power feature (on battery, battery percentage)
    
    Bug:784232
    Change-Id: Ic7eddd66b8fdd3f1680358842d135e0a45b857b0

--- chrome/browser/chromeos/power/ml/user_activity_logger.h
+++ chrome/browser/chromeos/power/ml/user_activity_logger.h
@@ -6,8 +6,12 @@
 #define CHROME_BROWSER_CHROMEOS_POWER_ML_USER_ACTIVITY_LOGGER_H_
 
 #include "base/macros.h"
+#include "base/optional.h"
+#include "base/time/time.h"
 #include "chrome/browser/chromeos/power/ml/idle_event_notifier.h"
+#include "chrome/browser/chromeos/power/ml/user_activity_event.pb.h"
 #include "chrome/browser/chromeos/power/ml/user_activity_logger_delegate.h"
+#include "chromeos/dbus/power_manager_client.h"
 #include "ui/base/user_activity/user_activity_observer.h"
 
 namespace chromeos {
@@ -17,10 +21,21 @@ namespace ml {
 // Logs user activity after an idle event is observed.
 // TODO(renjieliu): Add power-related activity as well.
 class UserActivityLogger : public ui::UserActivityObserver,
-                           public IdleEventNotifier::Observer {
+                           public IdleEventNotifier::Observer,
+                           public PowerManagerClient::Observer {
  public:
-  explicit UserActivityLogger(UserActivityLoggerDelegate* delegate);
+  explicit UserActivityLogger(
+      UserActivityLoggerDelegate* delegate,
+      IdleEventNotifier* idle_event_notifier,
+      chromeos::PowerManagerClient* power_manager_client);
   ~UserActivityLogger() override;
+
+  // chromeos::PowerManagerClient::Observer overrides:
+  void LidEventReceived(chromeos::PowerManagerClient::LidState state,
+                        const base::TimeTicks& timestamp) override;
+  void PowerChanged(const power_manager::PowerSupplyProperties& proto) override;
+  void TabletModeEventReceived(chromeos::PowerManagerClient::TabletMode mode,
+                               const base::TimeTicks& timestamp) override;
 
  private:
   friend class UserActivityLoggerTest;
@@ -32,14 +47,42 @@ class UserActivityLogger : public ui::UserActivityObserver,
   void OnIdleEventObserved(
       const IdleEventNotifier::ActivityData& data) override;
 
+  // Update lid state and tablet mode from received switch states.
+  void OnReceiveSwitchStates(
+      base::Optional<chromeos::PowerManagerClient::SwitchStates> switch_states);
+
+  // Extract features from last known activity data and from device states.
+  const UserActivityEvent::Features ExtractFeatures() const;
+
   // Flag indicating whether an idle event has been observed.
   bool idle_event_observed_ = false;
 
   // UserActivity data derived from idle event.
   IdleEventNotifier::ActivityData activity_data_;
 
+  chromeos::PowerManagerClient::LidState lid_state_ =
+      chromeos::PowerManagerClient::LidState::NOT_PRESENT;
+
+  chromeos::PowerManagerClient::TabletMode tablet_mode_ =
+      chromeos::PowerManagerClient::TabletMode::UNSUPPORTED;
+
+  // Device type.
+  UserActivityEvent::Features::DeviceType device_type_;
+
+  // Flag indicating whether the device is on battery.
+  bool on_battery_ = false;
+
+  // Battery percent.
+  base::Optional<float> battery_percent_;
+
   // Logger delegate.
   UserActivityLoggerDelegate* const logger_delegate_;
+
+  // Idle event notifier.
+  IdleEventNotifier* const idle_event_notifier_;
+
+  // Power manager client.
+  chromeos::PowerManagerClient* const power_manager_client_;
 
   DISALLOW_COPY_AND_ASSIGN(UserActivityLogger);
 };
