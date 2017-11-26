e5114e5ee9c89843930db61083f491f786cd1e36
Renjie Liu <renjieliu@chromium.org>
2017-11-27 10:52:38 +1100

    Add more features to user activity logger
    Add device_mode feature.
    Add Power feature (on battery, battery percentage)
    
    Bug:784232
    Change-Id: Ic7eddd66b8fdd3f1680358842d135e0a45b857b0

--- chrome/browser/chromeos/power/ml/user_activity_logger.cc
+++ chrome/browser/chromeos/power/ml/user_activity_logger.cc
@@ -6,6 +6,9 @@
 
 #include "base/timer/timer.h"
 #include "chrome/browser/chromeos/power/ml/user_activity_event.pb.h"
+#include "chromeos/dbus/power_manager/power_supply_properties.pb.h"
+#include "chromeos/system/devicetype.h"
+#include "ui/base/user_activity/user_activity_detector.h"
 #include "ui/base/user_activity/user_activity_observer.h"
 
 namespace chromeos {
@@ -13,10 +16,43 @@ namespace power {
 namespace ml {
 
 UserActivityLogger::UserActivityLogger(
-    UserActivityLoggerDelegate* const delegate)
-    : logger_delegate_(delegate) {}
+    UserActivityLoggerDelegate* const delegate,
+    IdleEventNotifier* const idle_event_notifier,
+    chromeos::PowerManagerClient* const power_manager_client)
+    : logger_delegate_(delegate),
+      idle_event_notifier_(idle_event_notifier),
+      power_manager_client_(power_manager_client) {
+  DCHECK(idle_event_notifier_);
+  idle_event_notifier_->AddObserver(this);
 
-UserActivityLogger::~UserActivityLogger() = default;
+  ui::UserActivityDetector* detector = ui::UserActivityDetector::Get();
+  DCHECK(detector);
+  detector->AddObserver(this);
+
+  DCHECK(power_manager_client_);
+  power_manager_client_->AddObserver(this);
+  power_manager_client_->RequestStatusUpdate();
+  power_manager_client_->GetSwitchStates(base::BindOnce(
+      &UserActivityLogger::OnReceiveSwitchStates, base::Unretained(this)));
+
+  if (chromeos::GetDeviceType() == chromeos::DeviceType::kChromebook) {
+    device_type_ = UserActivityEvent::Features::CHROMEBOOK;
+  } else if (chromeos::GetDeviceType() == chromeos::DeviceType::kUnknown) {
+    device_type_ = UserActivityEvent::Features::UNKNOWN_DEVICE;
+  }
+}
+
+UserActivityLogger::~UserActivityLogger() {
+  DCHECK(idle_event_notifier_);
+  idle_event_notifier_->RemoveObserver(this);
+
+  DCHECK(power_manager_client_);
+  power_manager_client_->RemoveObserver(this);
+
+  ui::UserActivityDetector* detector = ui::UserActivityDetector::Get();
+  DCHECK(detector);
+  detector->RemoveObserver(this);
+}
 
 // Only log when we observe an idle event.
 void UserActivityLogger::OnUserActivity(const ui::Event* /* event */) {
@@ -28,21 +64,104 @@ void UserActivityLogger::OnUserActivity(const ui::Event* /* event */) {
   event->set_type(UserActivityEvent::Event::REACTIVATE);
   event->set_reason(UserActivityEvent::Event::USER_ACTIVITY);
 
-  UserActivityEvent::Features* features = activity_event.mutable_features();
-  features->set_last_activity_time_sec(
-      (activity_data_.last_activity_time -
-       activity_data_.last_activity_time.LocalMidnight())
-          .InSeconds());
+  *activity_event.mutable_features() = features_;
 
   // Log to metrics.
   logger_delegate_->LogActivity(activity_event);
   idle_event_observed_ = false;
 }
 
+void UserActivityLogger::LidEventReceived(
+    chromeos::PowerManagerClient::LidState state,
+    const base::TimeTicks& /* timestamp */) {
+  lid_state_ = state;
+}
+
+// TODO(renjieliu): Add power related activity.
+void UserActivityLogger::PowerChanged(
+    const power_manager::PowerSupplyProperties& proto) {
+  on_battery_ = (proto.external_power() ==
+                 power_manager::PowerSupplyProperties::DISCONNECTED);
+
+  if (proto.has_battery_percent()) {
+    battery_percent_ = proto.battery_percent();
+  }
+}
+
+void UserActivityLogger::TabletModeEventReceived(
+    chromeos::PowerManagerClient::TabletMode mode,
+    const base::TimeTicks& /* timestamp */) {
+  tablet_mode_ = mode;
+}
+
 void UserActivityLogger::OnIdleEventObserved(
     const IdleEventNotifier::ActivityData& activity_data) {
   idle_event_observed_ = true;
-  activity_data_ = activity_data;
+  ExtractFeatures(activity_data);
+}
+
+void UserActivityLogger::OnReceiveSwitchStates(
+    base::Optional<chromeos::PowerManagerClient::SwitchStates> switch_states) {
+  if (switch_states.has_value()) {
+    lid_state_ = switch_states->lid_state;
+    tablet_mode_ = switch_states->tablet_mode;
+  }
+}
+
+void UserActivityLogger::ExtractFeatures(
+    const IdleEventNotifier::ActivityData& activity_data) {
+  features_.Clear();
+
+  // Set time related features.
+  features_.set_last_activity_time_sec(
+      (activity_data.last_activity_time -
+       activity_data.last_activity_time.LocalMidnight())
+          .InSeconds());
+  if (activity_data.last_user_activity_time.has_value()) {
+    features_.set_last_user_activity_time_sec(
+        (*activity_data.last_user_activity_time -
+         activity_data.last_user_activity_time->LocalMidnight())
+            .InSeconds());
+  }
+
+  base::Time::Exploded exploded;
+  activity_data.last_activity_time.LocalExplode(&exploded);
+  features_.set_last_activity_day(
+      static_cast<chromeos::power::ml::UserActivityEvent_Features_DayOfWeek>(
+          exploded.day_of_week));
+
+  if (activity_data.last_mouse_time.has_value()) {
+    features_.set_time_since_last_mouse_sec(
+        (base::Time::Now() - *activity_data.last_mouse_time).InSeconds());
+  }
+  if (activity_data.last_key_time.has_value()) {
+    features_.set_time_since_last_key_sec(
+        (base::Time::Now() - *activity_data.last_key_time).InSeconds());
+  }
+
+  features_.set_recent_time_active_sec(
+      (activity_data.last_activity_time - activity_data.earliest_activity_time)
+          .InSeconds());
+
+  // Set device mode.
+  if (lid_state_ == chromeos::PowerManagerClient::LidState::CLOSED) {
+    features_.set_device_mode(UserActivityEvent::Features::CLOSED_LID);
+  } else if (lid_state_ == chromeos::PowerManagerClient::LidState::OPEN) {
+    if (tablet_mode_ == chromeos::PowerManagerClient::TabletMode::ON) {
+      features_.set_device_mode(UserActivityEvent::Features::TABLET);
+    } else {
+      features_.set_device_mode(UserActivityEvent::Features::CLAMSHELL);
+    }
+  } else {
+    features_.set_device_mode(UserActivityEvent::Features::UNKNOWN_MODE);
+  }
+
+  features_.set_device_type(device_type_);
+
+  if (battery_percent_.has_value()) {
+    features_.set_battery_percent(*battery_percent_);
+  }
+  features_.set_on_battery(on_battery_);
 }
 
 }  // namespace ml
