22430676d0dd55a993d385d76cd1576ebd234c98
Miguel Casas <mcasas@chromium.org>
2017-11-27 18:55:36 -0500

    vaapi: move surface blit to decoder thread
    VaapiVideoDecodeAccelerator decodes video frames using libva on
    a decoder thread, then jumps to its main thread to pass them on;
    before that, however, OutputPicture() needs to DownloadFromSurface()
    which boils down to VaapiWrapper::Blitsurface(). This last
    call takes a significant amount of time (e.g. several
    milliseconds), and happens on the GPU main thread, introducing
    jank.
    
    This CL moves the said OutputPicture() to the task runner,
    which in turn pulls a number of other methods to change
    sides.  Most notably, the release of the surfaces by the
    client, on which the decoder thread used to wait, needed
    to be rewritten: the condition variable on which the decoder
    thread used to wait is removed, and for the sake of flushing
    (where we need to produce video output without encoded input),
    the logic of DecodeTask() and GetInputBuffer_Locked() is
    modified accordingly.
    
    TEST=compile and load simplechrome in soraka, then navigate to
    crosvideo.appspot.com and select e.g. a VP9 30K 4K video. Make
    sure in chrome://media-internals that GpuVideoDecoder is used,
    and observe correct reproduction.
    Compiled and run video_decode_accelerator_unittests on same hw:
    video_decode_accelerator_unittest --test_video_data=test-25fps.vp9:320:240:250:250:35:150:12
    (all tests passing, same perf numbers).
    
    Bug: 717265
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I21e8ef8b7eed29b2242cd2660b5d4ba841e5e20b

--- media/gpu/vaapi_video_decode_accelerator.h
+++ media/gpu/vaapi_video_decode_accelerator.h
@@ -110,11 +110,6 @@ class MEDIA_GPU_EXPORT VaapiVideoDecodeAccelerator
   // returned. Will also release the mapping.
   void ReturnCurrInputBuffer_Locked();
 
-  // Wait for more surfaces to become available. Return true once they do or
-  // false if an early exit has been requested (due to an initiated
-  // reset/flush/destroy).
-  bool WaitForSurfaces_Locked();
-
   // Continue decoding given input buffers and sleep waiting for input/output
   // as needed. Will exit if a new set of surfaces or reset/flush/destroy
   // is requested.
@@ -154,13 +149,13 @@ class MEDIA_GPU_EXPORT VaapiVideoDecodeAccelerator
   // Puts contents of |va_surface| into given |picture|, releases the surface
   // and passes the resulting picture to client to output the given
   // |visible_rect| part of it.
-  void OutputPicture(const scoped_refptr<VASurface>& va_surface,
-                     int32_t input_id,
-                     gfx::Rect visible_rect,
-                     VaapiPicture* picture);
+  void OutputPictureTask(const scoped_refptr<VASurface>& va_surface,
+                         int32_t input_id,
+                         gfx::Rect visible_rect,
+                         VaapiPicture* picture);
 
-  // Try to OutputPicture() if we have both a ready surface and picture.
-  void TryOutputSurface();
+  // Try to OutputPictureTask() if we have both a ready surface and picture.
+  void TryOutputSurfaceTask();
 
   // Called when a VASurface is no longer in use by the decoder or is not being
   // synced/waiting to be synced to a picture. Returns it to available surfaces
@@ -184,12 +179,12 @@ class MEDIA_GPU_EXPORT VaapiVideoDecodeAccelerator
   // |dec_surface| is ready to be outputted once decode is finished.
   // This can be called before decode is actually done in hardware, and this
   // method is responsible for maintaining the ordering, i.e. the surfaces have
-  // to be outputted in the same order as SurfaceReady is called.
+  // to be outputted in the same order as SurfaceReadyTask is called.
   // On Intel, we don't have to explicitly maintain the ordering however, as the
   // driver will maintain ordering, as well as dependencies, and will process
   // each submitted command in order, and run each command only if its
   // dependencies are ready.
-  void SurfaceReady(const scoped_refptr<VaapiDecodeSurface>& dec_surface);
+  void SurfaceReadyTask(const scoped_refptr<VaapiDecodeSurface>& dec_surface);
 
   // Return a new VaapiDecodeSurface for decoding into, or nullptr if not
   // available.
@@ -243,9 +238,6 @@ class MEDIA_GPU_EXPORT VaapiVideoDecodeAccelerator
   // VA Surfaces no longer in use that can be passed back to the decoder for
   // reuse, once it requests them.
   std::list<VASurfaceID> available_va_surfaces_;
-  // Signalled when output surfaces are queued onto the available_va_surfaces_
-  // queue.
-  base::ConditionVariable surfaces_available_;
 
   // Pending output requests from the decoder. When it indicates that we should
   // output a surface and we have an available Picture (i.e. texture) ready
