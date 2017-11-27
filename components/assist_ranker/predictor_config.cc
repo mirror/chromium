716762e764e8c9c2eeb8dcbf6bd78d49961cbdea
Philippe Hamel <hamelphi@chromium.org>
2017-11-27 18:28:35 -0500

    Ranker owns predictor objects and uses a config for initialization.
    Still TODO:
    Logging
    Finch experiment control
    
    Bug: 786472, 778468
    Change-Id: Ie583616643ab8ad169df4739a4a18b81a950553f

---
+++ components/assist_ranker/predictor_config.cc
@@ -0,0 +1,7 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/assist_ranker/predictor_config.h"
+
+namespace assist_ranker {}  // namespace assist_ranker
