df05e08e29f6f9b9c8340919a3bff08064b61733
Kinuko Yasuda <kinuko@chromium.org>
2017-11-27 23:26:45 +0900

    [DO NOT COMMIT] WebPackage loader
    Cherry-picked https://chromium-review.googlesource.com/c/chromium/src/+/778703/07
    
    Bug:
    Change-Id: Ib0e55b0e826b01ed4ad76ff4bb7bd1cade56f971

--- content/browser/webauth/cbor/cbor_values.h
+++ content/browser/webauth/cbor/cbor_values.h
@@ -12,6 +12,7 @@
 
 #include "base/containers/flat_map.h"
 #include "base/macros.h"
+#include "base/optional.h"
 #include "base/strings/string_piece_forward.h"
 #include "content/common/content_export.h"
 
@@ -61,12 +62,13 @@ class CONTENT_EXPORT CBORValue {
   using MapValue = base::flat_map<std::string, CBORValue, CTAPLess>;
 
   enum class Type {
-    NONE,
-    UNSIGNED,
-    BYTESTRING,
-    STRING,
-    ARRAY,
-    MAP,
+    UNSIGNED = 0,
+    BYTE_STRING = 2,
+    STRING = 3,
+    ARRAY = 4,
+    MAP = 5,
+    TAG = 6,
+    NONE = -1,
   };
 
   CBORValue(CBORValue&& that) noexcept;
@@ -74,19 +76,29 @@ class CONTENT_EXPORT CBORValue {
 
   explicit CBORValue(Type type);
   explicit CBORValue(uint64_t in_unsigned);
+  explicit CBORValue(uint64_t in_unsigned, uint64_t tag);
 
   explicit CBORValue(const BinaryValue& in_bytes);
+  explicit CBORValue(const BinaryValue& in_bytes, uint64_t tag);
   explicit CBORValue(BinaryValue&& in_bytes) noexcept;
+  explicit CBORValue(BinaryValue&& in_bytes, uint64_t tag) noexcept;
 
   explicit CBORValue(const char* in_string);
+  explicit CBORValue(const char* in_string, uint64_t tag);
   explicit CBORValue(std::string&& in_string) noexcept;
+  explicit CBORValue(std::string&& in_string, uint64_t tag) noexcept;
   explicit CBORValue(base::StringPiece in_string);
+  explicit CBORValue(base::StringPiece in_string, uint64_t tag);
 
   explicit CBORValue(const ArrayValue& in_array);
+  explicit CBORValue(const ArrayValue& in_array, uint64_t tag);
   explicit CBORValue(ArrayValue&& in_array) noexcept;
+  explicit CBORValue(ArrayValue&& in_array, uint64_t tag) noexcept;
 
   explicit CBORValue(const MapValue& in_map);
+  explicit CBORValue(const MapValue& in_map, uint64_t tag);
   explicit CBORValue(MapValue&& in_map) noexcept;
+  explicit CBORValue(MapValue&& in_map, uint64_t tag) noexcept;
 
   CBORValue& operator=(CBORValue&& that) noexcept;
 
@@ -103,7 +115,7 @@ class CONTENT_EXPORT CBORValue {
   bool is_type(Type type) const { return type == type_; }
   bool is_none() const { return type() == Type::NONE; }
   bool is_unsigned() const { return type() == Type::UNSIGNED; }
-  bool is_bytestring() const { return type() == Type::BYTESTRING; }
+  bool is_bytestring() const { return type() == Type::BYTE_STRING; }
   bool is_string() const { return type() == Type::STRING; }
   bool is_array() const { return type() == Type::ARRAY; }
   bool is_map() const { return type() == Type::MAP; }
@@ -115,8 +127,11 @@ class CONTENT_EXPORT CBORValue {
   const ArrayValue& GetArray() const;
   const MapValue& GetMap() const;
 
+  const base::Optional<uint64_t> GetTag() const;
+
  private:
   Type type_;
+  uint64_t tag_;
 
   union {
     uint64_t unsigned_value_;
