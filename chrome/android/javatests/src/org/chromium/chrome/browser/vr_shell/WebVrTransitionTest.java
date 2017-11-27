a9985252a44c5645dffec4086ba15cc5df2407f1
bsheedy <bsheedy@chromium.org>
2017-11-27 14:23:29 -0800

    Fix NFC test not working in WebappActivity
    Fixes the issue of CCT/WAA NFC tests getting stuck in the DOFF flow.
    This was somehow caused by the webpage listening for vrdisplayactivate,
    but not actually presenting with it.
    
    Also adds a sleep at the end of the test to prevent a possible race
    condition with VrCore's 2 second NFC debounce timeout.
    
    Bug: 787878
    Change-Id: Ic5f16258cfa94ce3fac63781c1e225b8b2749f4e

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/WebVrTransitionTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/WebVrTransitionTest.java
@@ -13,6 +13,7 @@ import static org.chromium.chrome.test.util.ChromeRestriction.RESTRICTION_TYPE_V
 
 import android.app.Activity;
 import android.os.Build;
+import android.os.SystemClock;
 import android.support.test.filters.MediumTest;
 
 import org.junit.Assert;
@@ -114,13 +115,12 @@ public class WebVrTransitionTest {
 
     /**
      * Tests that scanning the Daydream View NFC tag on supported devices fires the
-     * vrdisplayactivate event.
+     * vrdisplayactivate event and the event allows presentation without a user gesture.
      */
     @Test
     @MediumTest
     @Restriction(RESTRICTION_TYPE_VIEWER_DAYDREAM)
-    @VrActivityRestriction({VrActivityRestriction.SupportedActivity.CTA,
-            VrActivityRestriction.SupportedActivity.CCT})
+    @VrActivityRestriction({VrActivityRestriction.SupportedActivity.ALL})
     public void testNfcFiresVrdisplayactivate() throws InterruptedException {
         mVrTestFramework.loadUrlAndAwaitInitialization(
                 VrTestFramework.getHtmlTestFile("test_nfc_fires_vrdisplayactivate"),
@@ -130,6 +130,11 @@ public class WebVrTransitionTest {
         NfcSimUtils.simNfcScan(mVrTestRule.getActivity());
         VrTestFramework.waitOnJavaScriptStep(mVrTestFramework.getFirstTabWebContents());
         VrTestFramework.endTest(mVrTestFramework.getFirstTabWebContents());
+        // VrCore has a 2000 ms debounce timeout on NFC scans. When run multiple times in different
+        // activities, it is possible for a latter test to be run in the 2 seconds after the
+        // previous test's NFC scan, causing it to fail flakily. So, wait 2 seconds to ensure that
+        // can't happen.
+        SystemClock.sleep(2000);
     }
 
     /**
