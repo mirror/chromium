32c1c7684fc5f444a39d4c07b06df262e7869ec3
Lily Houghton <lilyhoughton@chromium.org>
2017-11-27 12:57:38 -0500

    [Cronet] Populate cronet metrics object with data from net/.
    Currently this only creates the an appropriater delegate in net/, calling it from there.
    There still needs to be created code in cronet which registers and appropriate delegate,
    and then populates the metrics object that will finally be given to the client.
    
    Bug:702796
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I33a8ce1f489a7bfb74b4c9561a57b7b5a71013e0

--- components/cronet/ios/cronet_metrics.h
+++ components/cronet/ios/cronet_metrics.h
@@ -8,9 +8,19 @@
 #import <Foundation/Foundation.h>
 
 #include "components/grpc_support/include/bidirectional_stream_c.h"
+#include "ios/net/crn_http_protocol_handler.h"
+#include "net/http/http_network_session.h"
+
+// These are internal versions of NSURLSessionTaskTransactionMetrics and
+// NSURLSessionTaskMetrics, defined primarily so that Cronet can
+// initialize them and set their properties (the iOS classes are readonly)
+
+// The correspondences are
+//   CronetTransactionMetrics -> NSURLSessionTaskTransactionMetrics
+//   CronetMetrics -> NSURLSessionTaskMetrics
 
 FOUNDATION_EXPORT GRPC_SUPPORT_EXPORT NS_AVAILABLE_IOS(10.0)
-@interface CronetMetrics : NSURLSessionTaskTransactionMetrics
+@interface CronetTransactionMetrics : NSURLSessionTaskTransactionMetrics
 
 @property(copy, readwrite) NSURLRequest* request;
 @property(copy, readwrite) NSURLResponse* response;
@@ -34,5 +44,27 @@ FOUNDATION_EXPORT GRPC_SUPPORT_EXPORT NS_AVAILABLE_IOS(10.0)
     NSURLSessionTaskMetricsResourceFetchType resourceFetchType;
 
 @end
+
+NS_AVAILABLE_IOS(10.0) @interface CronetMetrics : NSURLSessionTaskMetrics
+@property(copy, readwrite)
+    NSArray<NSURLSessionTaskTransactionMetrics*>* transactionMetrics;
+@end
+
+// Helper methods.
+
+// Helper method that converts the ticks data found in LoadTimingInfo to an
+// NSDate value to be used in client-side data.
+NSDate*
+    ticksToDate(const net::LoadTimingInfo& reference, base::TimeTicks ticks);
+
+// Copy of GetProxy in components/cronet/android/cronet_url_request_adapter.cc
+// Returns the string representation of the HostPortPair of the proxy server
+// that was used to fetch the response.
+std::string getProxy(const net::HttpResponseInfo& response_info);
+
+// Converts net::Metrics metrics data into CronetTransactionMetrics (which
+// importantly implements the NSURLSessionTaskTransactionMetrics API)
+CronetTransactionMetrics* nativeToIOSMetrics(net::Metrics metrics)
+    NS_AVAILABLE_IOS(10.0);
 
 #endif // COMPONENTS_CRONET_IOS_CRONET_METRICS_H_
