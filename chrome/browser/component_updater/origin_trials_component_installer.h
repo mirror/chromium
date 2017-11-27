5107eaaba9ea7fe5b59a36420da0b9e21817208d
Xiaochu Liu <xiaochu@chromium.org>
2017-11-26 17:38:40 -0800

    Allow async CustomInstall in component updater
    By design OnCustomInstall is supposed to block the update until it's
    done before marks this update 'done'. However, certain operations in
    OnCustomInstall (dbus call) has to be async.
    
    We move OnCustomInstall from blocking to async with a callback. In order
    to avoid failure of calling the callback, I designed a
    CustomInstallRunner that automatically triggers the callback if callback
    is not called after a period.
    
    BUG=chormium:780201
    TEST=None
    
    Change-Id: I9c3d9f4e5987a976cdd3707786535285787e5caa

--- chrome/browser/component_updater/origin_trials_component_installer.h
+++ chrome/browser/component_updater/origin_trials_component_installer.h
@@ -31,9 +31,11 @@ class OriginTrialsComponentInstallerPolicy : public ComponentInstallerPolicy {
                           const base::FilePath& install_dir) const override;
   bool SupportsGroupPolicyEnabledComponentUpdates() const override;
   bool RequiresNetworkEncryption() const override;
-  update_client::CrxInstaller::Result OnCustomInstall(
+  void OnCustomInstall(
       const base::DictionaryValue& manifest,
-      const base::FilePath& install_dir) override;
+      const base::FilePath& install_dir,
+      scoped_refptr<CustomInstallRunner> custom_install_runner,
+      const scoped_refptr<base::SingleThreadTaskRunner>& task_runner) override;
   void OnCustomUninstall() override;
   void ComponentReady(const base::Version& version,
                       const base::FilePath& install_dir,
