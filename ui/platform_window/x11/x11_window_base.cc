8a9e8ccd4868a5a5c12406863278adc5860f7900
Antonio Gomes <tonikitoo@igalia.com>
2017-11-27 14:41:13 -0400

    [ozone/x11] Allow X11WindowBase::SetBounds proceed on hidden windows
    Today, X11WindowBase::SetBounds bails out earlier when it is
    called on a hidden window (i.e. when X11WindowBase::Show has
    not being called yet).
    
    Although this is Ok for ChromeOS/Ozone, where a native window
    corresponds to a physical display, and accurate bounds provided at
    its creation time, this breaks several cases in "external window mode"
    (Linux/Ozone).
    
    For instance, tooltips (which chrome/x11 has its on native window
    for) are created with arbitrary temporary bounds - see DesktopWindowTreeHostX11::Init,
    where 100x100 is used.
    When bounds are available, DesktopWindowTreeHostX11::SetBoundsInPixels is called,
    and only then DesktopWindowTreeHostX11::ShowImpl.
    
    In 'external window mode', this behavior is also mimic'ed: arbitrary
    (and temporary) bounds are also set so that an Ozone window is properly created,
    but prior to showing it, Aura/Mus is expected to set proper bounds to it.
    
    As is, Ozone's X11WindowBase, breaks this flow, since ::SetBounds conditions
    its execution to 'X11WindowBase::window_mapped_' being true, an attribution
    done in X11WindowBase::Show.
    
    CL changes ozone/x11 so that it mimics chrome/x11 behavior, allowing
    tooltips to be supported in external window mode, without changes
    in on client side.
    
    BUG=666958
    
    Change-Id: Icc0490fe011ce3e19f08f161c7c0ff6cef9ef8f5

--- ui/platform_window/x11/x11_window_base.cc
+++ ui/platform_window/x11/x11_window_base.cc
@@ -171,25 +171,23 @@ void X11WindowBase::Close() {
 }
 
 void X11WindowBase::SetBounds(const gfx::Rect& bounds) {
-  if (window_mapped_) {
-    XWindowChanges changes = {0};
-    unsigned value_mask = 0;
+  XWindowChanges changes = {0};
+  unsigned value_mask = 0;
 
-    if (bounds_.size() != bounds.size()) {
-      changes.width = bounds.width();
-      changes.height = bounds.height();
-      value_mask |= CWHeight | CWWidth;
-    }
-
-    if (bounds_.origin() != bounds.origin()) {
-      changes.x = bounds.x();
-      changes.y = bounds.y();
-      value_mask |= CWX | CWY;
-    }
-
-    if (value_mask)
-      XConfigureWindow(xdisplay_, xwindow_, value_mask, &changes);
+  if (bounds_.size() != bounds.size()) {
+    changes.width = bounds.width();
+    changes.height = bounds.height();
+    value_mask |= CWHeight | CWWidth;
   }
+
+  if (bounds_.origin() != bounds.origin()) {
+    changes.x = bounds.x();
+    changes.y = bounds.y();
+    value_mask |= CWX | CWY;
+  }
+
+  if (value_mask)
+    XConfigureWindow(xdisplay_, xwindow_, value_mask, &changes);
 
   // Assume that the resize will go through as requested, which should be the
   // case if we're running without a window manager.  If there's a window
