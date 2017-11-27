f2e6a5d05c3d9b240fae16656ab46c6f551dd77f
Jialiu Lin <jialiul@chromium.org>
2017-11-27 11:42:16 -0800

    Remove throttle from PasswordProtectionRequest
    When NavigationHandle is gone, remove its associated NavigationThrottle
    frome PasswordProtectionRequest to resolve crash.
    
    Bug:788733
    Change-Id: I9f2be37d705b445ddf3d3b6c1ff6d7df79a80e84

--- components/safe_browsing/password_protection/password_protection_navigation_throttle.cc
+++ components/safe_browsing/password_protection/password_protection_navigation_throttle.cc
@@ -4,16 +4,25 @@
 
 #include "components/safe_browsing/password_protection/password_protection_navigation_throttle.h"
 
+#include "components/safe_browsing/password_protection/password_protection_request.h"
 #include "content/public/browser/navigation_handle.h"
 
 namespace safe_browsing {
 PasswordProtectionNavigationThrottle::PasswordProtectionNavigationThrottle(
     content::NavigationHandle* navigation_handle,
+    scoped_refptr<PasswordProtectionRequest> request,
     bool is_warning_showing)
     : content::NavigationThrottle(navigation_handle),
-      is_warning_showing_(is_warning_showing) {}
+      request_(request),
+      is_warning_showing_(is_warning_showing) {
+  if (!is_warning_showing_)
+    request_->AddThrottle(this);
+}
 
-PasswordProtectionNavigationThrottle::~PasswordProtectionNavigationThrottle() {}
+PasswordProtectionNavigationThrottle::~PasswordProtectionNavigationThrottle() {
+  if (request_)
+    request_->RemoveThrottle(this);
+}
 
 content::NavigationThrottle::ThrottleCheckResult
 PasswordProtectionNavigationThrottle::WillStartRequest() {
