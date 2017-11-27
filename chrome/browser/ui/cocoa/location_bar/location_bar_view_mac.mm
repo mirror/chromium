c8cc3b2679af29cc440b249349ce33cba60185c4
Adrian Elder <aelder@chromium.org>
2017-11-27 12:05:16 -0500

    Show extension name in permission bubble.
    This applies to browser permissions checks, not app permissions. For
    example: getUserMedia. If the permission is not requested by an
    extension, or the extension name cannot be looked up, the origin from
    the URL is shown as before.
    
    Tested by launching an extension that attempts to access the mic with
    getUserMedia and verifying the existing behavior for a web site.
    
    Bug: 717625
    Change-Id: I4e17c97f932ee163afa2fe972687259d794e4cfc

--- chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.mm
+++ chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.mm
@@ -17,6 +17,7 @@
 #include "chrome/browser/command_updater.h"
 #include "chrome/browser/defaults.h"
 #include "chrome/browser/extensions/api/omnibox/omnibox_api.h"
+#include "chrome/browser/extensions/extension_ui_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/translate/chrome_translate_client.h"
@@ -592,7 +593,10 @@ void LocationBarViewMac::UpdatePageInfoText() {
   if (type == PageInfoVerboseType::kEVCert) {
     label = GetToolbarModel()->GetEVCertName();
   } else if (type == PageInfoVerboseType::kExtension) {
-    label = GetExtensionName(GetToolbarModel()->GetURL(), GetWebContents());
+    if (GetWebContents()) {
+      label = extensions::ui_util::GetEnabledExtensionNameForUrl(
+          GetToolbarModel()->GetURL(), GetWebContents()->GetBrowserContext());
+    }
   } else if (type == PageInfoVerboseType::kChrome) {
     label = l10n_util::GetStringUTF16(IDS_SHORT_PRODUCT_NAME);
   } else if (type == PageInfoVerboseType::kSecurity &&
