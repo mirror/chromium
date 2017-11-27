08c755385f06397347fe111e7433d6366a3731b0
Albert J. Wong <ajwong@chromium.org>
2017-11-27 13:39:42 -0800

    PA: PartitionPage: move file static funcs up.
    This moves the file local query functions that were implementing an
    ad-hoc "package protection" style API into the PartitionPage public API.
    
    After this, I think the functions are finally organized enough that
    these classes can start being detangled into separate files.
    
    See related bug for more details on methodology.
    
    Bug: 787153
    Change-Id: I4997fd73e1779c9ed4f9ded9aa8a4c94c0eb7d1e

--- base/allocator/partition_allocator/partition_alloc.h
+++ base/allocator/partition_allocator/partition_alloc.h
@@ -318,11 +318,21 @@ struct PartitionPage {
   }
 
   ALWAYS_INLINE size_t get_raw_size() const;
+  ALWAYS_INLINE void set_raw_size(size_t size);
 
   ALWAYS_INLINE void Reset();
 
   // TODO(ajwong): Can this be made private?  https://crbug.com/787153
   BASE_EXPORT static PartitionPage* get_sentinel_page();
+
+  // Page State accessors.
+  // Note that it's only valid to call these functions on pages found on one of
+  // the page lists. Specifically, you can't call these functions on full pages
+  // that were detached from the active list.
+  ALWAYS_INLINE bool is_active() const;
+  ALWAYS_INLINE bool is_full() const;
+  ALWAYS_INLINE bool is_empty() const;
+  ALWAYS_INLINE bool is_decommitted() const;
 };
 static_assert(sizeof(PartitionPage) <= kPageMetadataSize,
               "PartitionPage must be able to fit in a metadata slot");
@@ -431,6 +441,8 @@ struct PartitionDirectMapExtent {
   PartitionDirectMapExtent* prev_extent;
   PartitionBucket* bucket;
   size_t map_size;  // Mapped size, not including guard pages and meta-data.
+
+  ALWAYS_INLINE static PartitionDirectMapExtent* FromPage(PartitionPage* page);
 };
 
 struct BASE_EXPORT PartitionRootBase {
@@ -457,8 +469,10 @@ struct BASE_EXPORT PartitionRootBase {
 
   // Pubic API
 
-  // gOomHandlingFunction is invoked when ParitionAlloc hits OutOfMemory.
+  // gOomHandlingFunction is invoked when PartitionAlloc hits OutOfMemory.
   static void (*gOomHandlingFunction)();
+
+  ALWAYS_INLINE static PartitionRootBase* FromPage(PartitionPage* page);
 };
 
 enum PartitionPurgeFlags {
@@ -767,7 +781,8 @@ ALWAYS_INLINE size_t PartitionPage::get_raw_size() const {
   return 0;
 }
 
-ALWAYS_INLINE PartitionRootBase* PartitionPageToRoot(PartitionPage* page) {
+ALWAYS_INLINE PartitionRootBase* PartitionRootBase::FromPage(
+    PartitionPage* page) {
   PartitionSuperPageExtentEntry* extent_entry =
       reinterpret_cast<PartitionSuperPageExtentEntry*>(
           reinterpret_cast<uintptr_t>(page) & kSystemPageBaseMask);
@@ -775,7 +790,7 @@ ALWAYS_INLINE PartitionRootBase* PartitionPageToRoot(PartitionPage* page) {
 }
 
 ALWAYS_INLINE bool PartitionPage::IsPointerValid(PartitionPage* page) {
-  PartitionRootBase* root = PartitionPageToRoot(page);
+  PartitionRootBase* root = PartitionRootBase::FromPage(page);
   return root->inverted_self == ~reinterpret_cast<uintptr_t>(root);
 }
 
