8742d3b3c858925bd7208e19c55580099f112459
Benoit Lize <lizeb@chromium.org>
2017-11-27 18:16:12 +0100

    tools/cygprofile: Order dumps by timestamp.
    Add the timestamp to the filenames, and process them in increasing
    process creation time order.
    
    Bug: 776702
    Change-Id: I5e6b9eb61364ed194186900afa64fdd459e65fbe

--- tools/cygprofile/delayed_dumper.cc
+++ tools/cygprofile/delayed_dumper.cc
@@ -2,13 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <time.h>
 #include <unistd.h>
 
 #include <thread>
 
 #include "base/android/library_loader/anchor_functions.h"
 #include "base/files/file.h"
+#include "base/format_macros.h"
 #include "base/logging.h"
+#include "base/posix/eintr_wrapper.h"
 #include "base/strings/stringprintf.h"
 #include "build/build_config.h"
 #include "tools/cygprofile/lightweight_cygprofile.h"
@@ -25,13 +28,20 @@ namespace {
 class DelayedDumper {
  public:
   DelayedDumper() {
-    SanityChecks();
-    std::thread([]() {
+    // Not using base::TimeTicks() to not call too many base:: symbol that would
+    // pollute the reached symbols dumps.
+    struct timespec ts;
+    if (HANDLE_EINTR(clock_gettime(CLOCK_MONOTONIC, &ts)))
+      PLOG(FATAL) << "Cannot get the time.";
+    uint64_t start_ns_since_epoch =
+        static_cast<uint64_t>(ts.tv_sec) * 1000 * 1000 * 1000 + ts.tv_nsec;
+
+    std::thread([start_ns_since_epoch]() {
       sleep(kDelayInSeconds);
       auto path = base::StringPrintf(
           "/data/local/tmp/chrome/cyglog/"
-          "cygprofile-instrumented-code-hitmap-%d.txt",
-          getpid());
+          "cygprofile-instrumented-code-hitmap-%d-%" PRIu64 ".txt",
+          getpid(), start_ns_since_epoch);
       StopAndDumpToFile(base::FilePath(path));
     })
         .detach();
