648ea5e9e0f079a6c88416ec64667a22b98f15da
John Abd-El-Malek <jam@chromium.org>
2017-11-27 11:35:19 -0800

    Remove WebContentsObserver::DidGetResourceResponseStart since it's no longer used.
    BUG=783981
    
    Change-Id: I5fa5a48492d1bbded7fa5c43be2caf97626b659d

--- content/public/browser/web_contents_observer.h
+++ content/public/browser/web_contents_observer.h
@@ -44,7 +44,6 @@ struct FaviconURL;
 struct LoadCommittedDetails;
 struct PrunedDetails;
 struct Referrer;
-struct ResourceRequestDetails;
 
 // An observer API implemented by classes which are interested in various page
 // load events from WebContents.  They also get a chance to filter IPC messages.
@@ -251,11 +250,6 @@ class CONTENT_EXPORT WebContentsObserver : public IPC::Listener {
       const GURL& url,
       const std::string& mime_type,
       ResourceType resource_type) {}
-
-  // This method is invoked when a response has been received for a resource
-  // request.
-  virtual void DidGetResourceResponseStart(
-      const ResourceRequestDetails& details) {}
 
   // This method is invoked when a new non-pending navigation entry is created.
   // This corresponds to one NavigationController entry being created
