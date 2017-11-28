3dc03cf8bc2713c5892e5a35e321bcb9e59c8579
Hiroki Nakagawa <nhiroki@chromium.org>
2017-11-28 23:42:52 +0900

    Worklet: Set ReferrerPolicy in WorkletGlobalScope
    This is a follow-up CL for the CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/737311
    
    WorkletGlobalScope should inherit the owner Document's ReferrerPolicy and the
    previous CL meant to implement it. But, the CL actually forgot to set
    WorkletGlobalScope's ReferrerPolicy to the owner Document's ReferrerPolicy. This
    CL does it.
    
    One test case fails because of a bug in referrer handling on ES6 Modules. See
    issue 786862 for details.
    
    Bug: 773921, 786862
    Change-Id: I0eb95bd742c393ce6b39926ae41a9ef0ec3fc526

---
+++ third_party/WebKit/LayoutTests/external/wpt/worklets/resources/referrer-checker.py
@@ -0,0 +1,25 @@
+# Returns a valid response when request's |referrer| matches
+# |expected_referrer|.
+def main(request, response):
+    referrer = request.headers.get("referer", "")
+    referrer_policy = request.GET.first("referrer_policy")
+    expected_referrer = request.GET.first("expected_referrer", "")
+
+    response_headers = [("Content-Type", "text/javascript"),
+                        ("Access-Control-Allow-Origin", "*")]
+
+    if referrer_policy == "no-referrer" or referrer_policy == "origin":
+        if referrer == expected_referrer:
+            return (200, response_headers, "")
+        return (404, response_headers)
+
+    if referrer_policy == "same-origin":
+        if referrer == expected_referrer:
+            return (200, response_headers, "")
+        # The expected referrer doesn't contain query params for simplification,
+        # so we check the referrer by startswith() here.
+        if referrer.startswith(expected_referrer + "?"):
+            return (200, response_headers, "")
+        return (404, response_headers)
+
+    return (404, response_headers)
