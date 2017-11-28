0d4dfce2c2c9cac922b07c99e847e1d66c816071
Bence Béky <bnc@chromium.org>
2017-11-28 14:28:44 -0500

    Do not send HTTP/2 PING if there is a CheckPingStatus() posted.
    Do not send HTTP/2 PING if there is a CheckPingStatus() posted on the
    message loop, because it might get confused about which PING to expect a
    response to.
    
    Imagine the following scenario:
    * SpdySession sends out a preface PING then a request.
    * A CheckPingStatus() is posted on the message loop with delay.
    * SpdySession receives PING ACK right away.
    * Later, SpdySession sends out another preface PING then another
      request.
    * CheckPingStatus() gets executed shortly after.  |last_activity_time_|
      is when the first ACK was received.  CheckPingStatus() sees that there
      is an outstanding PING, and thinks that it timed out (even though it
      was just sent), so it tears down the connection.
    
    One could make the argument that this is impossible with the current
    values of kDefaultConnectionAtRiskOfLossSeconds and kHungIntervalSeconds
    (or at least very unlikely, depending on how much one trusts
    PostDelayedTask() timing), but that is not very transparent.
    
    This CL introduces two functional changes.  The first one is that it
    returns early from SendPrefacePingIfNoneInFlight() (freshly renamed to
    MaybeSendPrefacePing) if |check_ping_status_pending_|.  There is little
    harm in not sending another preface PING within kHungIntervalSeconds
    (10 seconds) since the last preface PING was sent.
    
    The other functional change is only increment PING opaque data by one
    with each PING frame.  "Receivers of a PING frame that does not include
    an ACK flag MUST send a PING frame with the ACK flag set in response,
    with an identical payload." according to RFC7540 Section 6.7, so this
    is acceptable.
    
    This CL also does some cleanup: rename SendPrefacePingIfNoneInFlight()
    to MaybeSendPrefacePing(), shuffle around some base::Time and
    base::TimeDelta in inequalities for easier readability, improve
    comments, inline SendPrefacePing(), and sharpen some expectations in
    unit tests.
    
    Bug: 784975
    Change-Id: Ic278ea87041120443fc73304d266b2f7a4eb54a2

--- net/spdy/chromium/spdy_session.h
+++ net/spdy/chromium/spdy_session.h
@@ -18,6 +18,7 @@
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "net/base/host_port_pair.h"
@@ -521,6 +522,7 @@ class NET_EXPORT SpdySession : public BufferedSpdyFramerVisitorInterface,
   // Allow tests to access our innards for testing purposes.
   FRIEND_TEST_ALL_PREFIXES(SpdySessionTest, ClientPing);
   FRIEND_TEST_ALL_PREFIXES(SpdySessionTest, FailedPing);
+  FRIEND_TEST_ALL_PREFIXES(SpdySessionTest, WaitingForWrongPing);
   FRIEND_TEST_ALL_PREFIXES(SpdySessionTest, DeleteExpiredPushStreams);
   FRIEND_TEST_ALL_PREFIXES(SpdySessionTest, MetricsCollectionOnPushStreams);
   FRIEND_TEST_ALL_PREFIXES(SpdySessionTest, CancelPushBeforeClaimed);
@@ -749,11 +751,10 @@ class NET_EXPORT SpdySession : public BufferedSpdyFramerVisitorInterface,
   // Adjust the send window size of all ActiveStreams and PendingStreamRequests.
   void UpdateStreamsSendWindowSize(int32_t delta_window_size);
 
-  // Send the PING (preface-PING) frame.
-  void SendPrefacePingIfNoneInFlight();
-
-  // Send PING if there are no PINGs in flight and we haven't heard from server.
-  void SendPrefacePing();
+  // Send PING frame if all previous PING frames have been ACKed,
+  // all posted CheckPingStatus() tasks have been executed,
+  // and too long time has passed since last read from server.
+  void MaybeSendPrefacePing();
 
   // Send a single WINDOW_UPDATE frame.
   void SendWindowUpdateFrame(SpdyStreamId stream_id,
@@ -1117,8 +1118,7 @@ class NET_EXPORT SpdySession : public BufferedSpdyFramerVisitorInterface,
   // expirations.
   base::TimeTicks next_unclaimed_push_stream_sweep_time_;
 
-  // Indicate if we have already scheduled a delayed task to check the ping
-  // status.
+  // True if there is a CheckPingStatus() task posted on the message loop.
   bool check_ping_status_pending_;
 
   // Current send window size.  Zero unless session flow control is turned on.
