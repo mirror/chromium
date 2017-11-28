3b435b989e099688ca27912095a3ba09c8a262c7
Balazs Engedy <engedy@chromium.org>
2017-11-28 12:51:43 +0100

    Let CredentialsContainer call mojom::CredentialManager directly.
    Previously, calls were plumbed through several layers, for example, a
    call to navigator.credentials.get() went through:
     1) CredentialsContainer::get (in Source/modules)
     2) CredentialManagerClient::DispatchGet (in Source/modules)
     3) WebCredentialManagerClient::DispatchGet (in public/platform)
     4) CredentialManagerClient::DispatchGet (in c/pw_m/content/renderer)
     5) mojom::CredentialManager::Get
    
    With this CL, layers 2..4 are completely removed, and the implementation
    of navigator.credentials methods in CredentialsContainer will now call
    into the mojom::CredentialManager interface directly.
    
    This allows removing:
     -- everything in components/password_manager/content/renderer,
     -- everything in WebKit/Source/platform/credentialmanager, that is, all
        Platform*Credential types.
     -- everything credential management related in WebKit/public/platform,
        and Source/platform/exported, that is, all Web*Credential* types.
     -- MockCredentialManagerClient in content/shell/test_runner.
    
    Public-key credential related functionality from WebAuthenticationClient
    was also merged into CredentialsContainer, and the former class removed.
    
    DO NOT SUBMIT BEFORE:
    https://chromium-review.googlesource.com/c/chromium/src/+/735686
    
    Bug: 740081
    Change-Id: I9d854ceea23a0eace69ad644339ad0cd6edf0938

--- third_party/WebKit/Source/modules/credentialmanager/PublicKeyCredential.cpp
+++ third_party/WebKit/Source/modules/credentialmanager/PublicKeyCredential.cpp
@@ -6,6 +6,10 @@
 
 namespace blink {
 
+namespace {
+constexpr char kPublicKeyCredentialType[] = "public-key";
+}
+
 PublicKeyCredential* PublicKeyCredential::Create(
     const String& id,
     DOMArrayBuffer* raw_id,
@@ -16,7 +20,9 @@ PublicKeyCredential* PublicKeyCredential::Create(
 PublicKeyCredential::PublicKeyCredential(const String& id,
                                          DOMArrayBuffer* raw_id,
                                          AuthenticatorResponse* response)
-    : Credential(id), raw_id_(raw_id), response_(response) {}
+    : Credential(id, kPublicKeyCredentialType),
+      raw_id_(raw_id),
+      response_(response) {}
 
 void PublicKeyCredential::Trace(blink::Visitor* visitor) {
   visitor->Trace(raw_id_);
