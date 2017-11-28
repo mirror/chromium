419547dcc13cd513acf5d913776afd3ca599397e
Derek Cheng <imcheng@chromium.org>
2017-11-28 08:35:00 -0800

    [Cast channel] Validate IP address from mDNS / cast channel requests.
    A valid Cast device address must be private. This is checked using
    the IPAddress::IsReserved() method, similar to DIAL's device description
    service.
    
    The check is performed in several entry points (some are redundant as
    extra safety net):
    - DnsSdRegistry, when it receives an device advertisement from mDNS
    - CastSocketService, before it opens socket
    - CastSocketServiceImpl::OpenChannel
    - CastChannelOpenFunction (entry point for chrome.cast.channel.open)
    
    Bug: 786109
    Change-Id: Iaad91834cd4149fd345b2ada4e2704a0e158ba49

---
+++ components/cast_channel/cast_channel_util.h
@@ -0,0 +1,17 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_CAST_CHANNEL_CAST_CHANNEL_UTIL_H_
+#define COMPONENTS_CAST_CHANNEL_CAST_CHANNEL_UTIL_H_
+
+#include "net/base/ip_address.h"
+
+namespace cast_channel {
+
+// Returns true if |ip_address| represents a valid IP address of a Cast device.
+bool IsValidCastIPAddress(const net::IPAddress& ip_address);
+
+}  // namespace cast_channel
+
+#endif  // COMPONENTS_CAST_CHANNEL_CAST_CHANNEL_UTIL_H_
