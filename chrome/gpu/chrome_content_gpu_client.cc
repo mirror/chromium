e4a5a4b409ddca8003e00f2512b539304a3c68e0
Hirokazu Honda <hiroh@chromium.org>
2017-11-28 11:43:05 +0900

    Refactor GPUArcVDA based on new VDA-like IPC design
    Implement GPUArcVDA working with new VDA-like IPC. GpuArcVDA currently
    translates mojo IPC to VDA API, and therefore ChromeArcVDA is deleted.
    ChromeArcVDA and GpuArcVDA based on old IPC still exist as deprecated classes
    for compatibility, so that the old IPC can be used.
    
    TEST=CTS MediaTest on veyron_minnie
    BUG=chromium:745457
    CQ-DEPEND=CL:609564
    
    Change-Id: I9d90a6945c81e20572616c04c1d4f496e2bb3f87

--- chrome/gpu/chrome_content_gpu_client.cc
+++ chrome/gpu/chrome_content_gpu_client.cc
@@ -20,6 +20,7 @@
 #include "services/service_manager/public/cpp/connector.h"
 
 #if defined(OS_CHROMEOS)
+#include "chrome/gpu/gpu_arc_video_decode_accelerator.h"
 #include "chrome/gpu/gpu_arc_video_decode_accelerator_deprecated.h"
 #include "chrome/gpu/gpu_arc_video_encode_accelerator.h"
 #include "chrome/gpu/protected_buffer_manager.h"
@@ -69,6 +70,10 @@ void ChromeContentGpuClient::InitializeRegistry(
           base::Unretained(this)),
       base::ThreadTaskRunnerHandle::Get());
   registry->AddInterface(
+      base::Bind(&ChromeContentGpuClient::CreateArcVideoDecodeAccelerator,
+                 base::Unretained(this)),
+      base::ThreadTaskRunnerHandle::Get());
+  registry->AddInterface(
       base::Bind(&ChromeContentGpuClient::CreateArcVideoEncodeAccelerator,
                  base::Unretained(this)),
       base::ThreadTaskRunnerHandle::Get());
@@ -104,6 +109,14 @@ void ChromeContentGpuClient::CreateArcVideoDecodeAcceleratorDeprecated(
     ::arc::mojom::VideoDecodeAcceleratorDeprecatedRequest request) {
   mojo::MakeStrongBinding(
       base::MakeUnique<chromeos::arc::GpuArcVideoDecodeAcceleratorDeprecated>(
+          gpu_preferences_, protected_buffer_manager_.get()),
+      std::move(request));
+}
+
+void ChromeContentGpuClient::CreateArcVideoDecodeAccelerator(
+    ::arc::mojom::VideoDecodeAcceleratorRequest request) {
+  mojo::MakeStrongBinding(
+      base::MakeUnique<chromeos::arc::GpuArcVideoDecodeAccelerator>(
           gpu_preferences_, protected_buffer_manager_.get()),
       std::move(request));
 }
