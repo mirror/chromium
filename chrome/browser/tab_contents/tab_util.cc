49ea7a06e24effc66fea4e276d42fc84468c4802
Nasko Oskov <nasko@chromium.org>
2017-11-28 15:40:08 -0800

    Respect the process limit on same-site new tabs.
    Bug: 774723
    Change-Id: I2e35b7bc1d62826a9a220f2e021c6119d9abcccf

--- chrome/browser/tab_contents/tab_util.cc
+++ chrome/browser/tab_contents/tab_util.cc
@@ -51,16 +51,17 @@ scoped_refptr<SiteInstance> GetSiteInstanceForNewTab(Profile* profile,
     return SiteInstance::CreateForURL(profile, url);
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
-   if (extensions::ExtensionRegistry::Get(
-       profile)->enabled_extensions().GetHostedAppByURL(url))
-     return SiteInstance::CreateForURL(profile, url);
+  if (extensions::ExtensionRegistry::Get(profile)
+          ->enabled_extensions()
+          .GetHostedAppByURL(url))
+    return SiteInstance::CreateForURL(profile, url);
 #endif
 
   // We used to share the SiteInstance for same-site links opened in new tabs,
   // to leverage the in-memory cache and reduce process creation.  It now
   // appears that it is more useful to have such links open in a new process,
   // so we create new tabs in a new BrowsingInstance.
-   return nullptr;
+  return SiteInstance::CreateForURL(profile, url);
 }
 
 }  // namespace tab_util
