77b19f88d60cd71a3a888277a85e0cc0b5d0cbcb
Bailey Berro <baileyberro@chromium.org>
2017-11-28 15:51:49 -0800

    Creating file system providers to handle capabilities and creation
    Bug:
    Change-Id: Ic0d8c9cf12d3386755cb147cec0fbd1e277fce5b

---
+++ chrome/browser/chromeos/file_system_provider/fake_extension_provider.cc
@@ -0,0 +1,74 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/file_system_provider/fake_extension_provider.h"
+
+#include <stddef.h>
+
+#include <memory>
+#include <utility>
+
+#include "base/memory/ptr_util.h"
+#include "chrome/browser/chromeos/file_system_provider/fake_provided_file_system.h"
+#include "chrome/browser/chromeos/file_system_provider/service.h"
+#include "extensions/browser/extension_registry.h"
+#include "extensions/common/permissions/permissions_data.h"
+
+namespace chromeos {
+namespace file_system_provider {
+namespace {
+
+bool GetProvidingExtensionInfo(const std::string& extension_id,
+                               ProvidingExtensionInfo* result,
+                               Profile* profile) {
+  DCHECK(result);
+  extensions::ExtensionRegistry* const registry =
+      extensions::ExtensionRegistry::Get(profile);
+  DCHECK(registry);
+
+  const extensions::Extension* const extension = registry->GetExtensionById(
+      extension_id, extensions::ExtensionRegistry::ENABLED);
+  if (!extension || !extension->permissions_data()->HasAPIPermission(
+                        extensions::APIPermission::kFileSystemProvider)) {
+    return false;
+  }
+
+  result->extension_id = extension->id();
+  result->name = extension->name();
+  const extensions::FileSystemProviderCapabilities* const capabilities =
+      extensions::FileSystemProviderCapabilities::Get(extension);
+  DCHECK(capabilities);
+  result->capabilities = *capabilities;
+
+  return true;
+}
+
+}  // namespace
+
+// Factory callback, to be used in Service::SetFileSystemFactory(). The
+// |event_router| argument can be NULL.
+std::unique_ptr<ProvidedFileSystemInterface>
+FakeExtensionProvider::CreateProvidedFileSystem(
+    Profile* profile,
+    const ProvidedFileSystemInfo& file_system_info) {
+  DCHECK(profile);
+  return base::MakeUnique<FakeProvidedFileSystem>(file_system_info);
+}
+
+Capabilities FakeExtensionProvider::GetCapabilities(
+    Profile* profile,
+    const ProviderId& provider_id) {
+  ProvidingExtensionInfo providing_extension_info;
+  GetProvidingExtensionInfo(provider_id.GetExtensionId(),
+                            &providing_extension_info, profile);
+
+  return Capabilities(providing_extension_info.capabilities.configurable(),
+                      providing_extension_info.capabilities.watchable(),
+                      providing_extension_info.capabilities.multiple_mounts());
+}
+
+FakeExtensionProvider::FakeExtensionProvider() {}
+
+}  // namespace file_system_provider
+}  // namespace chromeos
