113ff7f274500f1eeaacde070cb3dff9aeb49d99
Matt Falkenhagen <falken@chromium.org>
2017-11-28 14:36:29 +0900

    service worker: Remove foreign fetch and link registration.
    These were removed from the specification:
    https://github.com/w3c/ServiceWorker/commit/0d09f488c062f0db5d65a243da0bbddaec3de1dd
    
    For a smaller diff, some transitive cleanup is left for future patches, like
    cleaning up enums that only have one or two values now.
    
    Bug: 788604
    Change-Id: Ieb57111fc7f226adc15989d2250c7169ad73a4b9

--- content/browser/service_worker/service_worker_provider_host.h
+++ content/browser/service_worker/service_worker_provider_host.h
@@ -328,7 +328,6 @@ class CONTENT_EXPORT ServiceWorkerProviderHost
   void NotifyControllerLost();
 
  private:
-  friend class ForeignFetchRequestHandlerTest;
   friend class LinkHeaderServiceWorkerTest;
   friend class ServiceWorkerProviderHostTest;
   friend class ServiceWorkerWriteToCacheJobTest;
