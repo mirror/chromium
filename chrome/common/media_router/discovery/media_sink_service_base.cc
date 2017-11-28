088444e786d39756afde00d3c2bcf3a1073eb597
Derek Cheng <imcheng@chromium.org>
2017-11-28 10:09:06 -0800

    [Media Router] Fix threading issues with in-browser discovery + cleanup.
    - Rename DialMediaSinkServiceProxy to DialMediaSinkService to be
      consistent with Cast.
    - Changed the construction/destruction sequence of
      DialMediaSinkServiceImpl to be consistent with Cast.
    -- Now both always create Impl on UI thread, then post task to start /
       stop / delete the Impl.
    -- The Impl classes exposes a task_runner() method which can be used to
       post tasks to it.
    - Get rid of MediaSinkService. It serves no real purpose since MR
      always references the impl class directly, and we do not do any
      mocking with it.
    - Replaced the DialMediaSinkServiceObserver interface with a callback
      since it's only one method.
    -- The callback is invoked on the Impl's task_runner(). It acts as a
       trampoline to invoke the real callback on UI thread.
    - Get rid of RefCountedThreadSafe usage in Dial / Cast classes, and
      replaced with WeakPtrFactory where necessary.
    - Moved MediaSinkServiceBase to chrome/common/media_router/discovery.
    - CastSocketService requires that methods be invoked on its
      task_runner() only.
    -- Corresponding changes to CastChannelAPI / CastMessageHandler.
    - More stringent SequenceChecker / BrowserThread checks. The latter is
      necessitated by downstream dependencies.
    
    Bug: 784758
    Change-Id: I50b4658c73ad561a642c1ff2347a5e523d3c963a

---
+++ chrome/common/media_router/discovery/media_sink_service_base.cc
@@ -0,0 +1,73 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/common/media_router/discovery/media_sink_service_base.h"
+
+#include <vector>
+
+namespace {
+// Time interval when media sink service sends sinks to MRP.
+const int kFetchCompleteTimeoutSecs = 3;
+}  // namespace
+
+namespace media_router {
+
+MediaSinkServiceBase::MediaSinkServiceBase(
+    const OnSinksDiscoveredCallback& callback)
+    : discovery_timer_secs_(kFetchCompleteTimeoutSecs),
+      on_sinks_discovered_cb_(callback) {
+  DETACH_FROM_SEQUENCE(sequence_checker_);
+  DCHECK(!on_sinks_discovered_cb_.is_null());
+}
+
+MediaSinkServiceBase::~MediaSinkServiceBase() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+}
+
+void MediaSinkServiceBase::SetTimerForTest(std::unique_ptr<base::Timer> timer) {
+  DCHECK(!discovery_timer_);
+  discovery_timer_ = std::move(timer);
+}
+
+void MediaSinkServiceBase::OnDiscoveryComplete() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (current_sinks_ == mrp_sinks_) {
+    DVLOG(2) << "No update to sink list.";
+    return;
+  }
+
+  ForceSinkDiscoveryCallback();
+  RecordDeviceCounts();
+}
+
+void MediaSinkServiceBase::StartTimer() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  // Create a finish timer.
+  if (!discovery_timer_)
+    discovery_timer_.reset(new base::OneShotTimer());
+
+  base::TimeDelta finish_delay =
+      base::TimeDelta::FromSeconds(discovery_timer_secs_);
+  discovery_timer_->Start(FROM_HERE, finish_delay,
+                          base::Bind(&MediaSinkServiceBase::OnDiscoveryComplete,
+                                     base::Unretained(this)));
+}
+
+void MediaSinkServiceBase::RestartTimer() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (!discovery_timer_ || discovery_timer_->IsRunning())
+    return;
+
+  StartTimer();
+}
+
+void MediaSinkServiceBase::ForceSinkDiscoveryCallback() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DVLOG(2) << "Send sinks to media router, [size]: " << current_sinks_.size();
+  on_sinks_discovered_cb_.Run(std::vector<MediaSinkInternal>(
+      current_sinks_.begin(), current_sinks_.end()));
+  mrp_sinks_ = current_sinks_;
+}
+
+}  // namespace media_router
---
+++ chrome/common/media_router/discovery/media_sink_service_base.h
@@ -0,0 +1,78 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_COMMON_MEDIA_ROUTER_DISCOVERY_MEDIA_SINK_SERVICE_BASE_H_
+#define CHROME_COMMON_MEDIA_ROUTER_DISCOVERY_MEDIA_SINK_SERVICE_BASE_H_
+
+#include <memory>
+
+#include "base/containers/flat_set.h"
+#include "base/gtest_prod_util.h"
+#include "base/sequence_checker.h"
+#include "base/timer/timer.h"
+#include "chrome/common/media_router/discovery/media_sink_internal.h"
+#include "chrome/common/media_router/discovery/media_sink_service_util.h"
+
+namespace media_router {
+
+// Base class for discovering MediaSinks and notifying the caller with updated
+// list. The class uses a Timer to throttle invocations of the callback when
+// multiple updates happen in quick succession.
+// This class is not thread safe. This class may be created on any thread.
+// All methods must be invoked on the same thread.
+class MediaSinkServiceBase {
+ public:
+  // |callback|: The callback to invoke when the list of MediaSinks has been
+  // updated.
+  explicit MediaSinkServiceBase(const OnSinksDiscoveredCallback& callback);
+  virtual ~MediaSinkServiceBase();
+
+  // Forces |callback| to be invoked when the latest sink list.
+  // Marked virtual for tests.
+  virtual void ForceSinkDiscoveryCallback();
+
+ protected:
+  void SetTimerForTest(std::unique_ptr<base::Timer> timer);
+
+  // Called when |discovery_timer_| expires.
+  virtual void OnDiscoveryComplete();
+
+  // Overriden by subclass to report device counts.
+  virtual void RecordDeviceCounts() {}
+
+  // Helper function to start |discovery_timer_|. Create a new timer if none
+  // exists.
+  void StartTimer();
+
+  // Helper function to restart |discovery_timer|. No-op if timer does not exist
+  // or timer is currently running. Can be called any time after |StartTimer| is
+  // called.
+  void RestartTimer();
+
+  // Sorted sinks from current round of discovery.
+  base::flat_set<MediaSinkInternal> current_sinks_;
+
+ private:
+  friend class MediaSinkServiceBaseTest;
+  FRIEND_TEST_ALL_PREFIXES(MediaSinkServiceBaseTest,
+                           TestOnDiscoveryComplete_SameSink);
+
+  // Time out value for |discovery_timer_|.
+  int discovery_timer_secs_;
+
+  OnSinksDiscoveredCallback on_sinks_discovered_cb_;
+
+  // Sorted sinks sent to Media Router Provider in last |OnDiscoveryComplete()|
+  base::flat_set<MediaSinkInternal> mrp_sinks_;
+
+  // Timer for completing the current round of discovery.
+  std::unique_ptr<base::Timer> discovery_timer_;
+
+  SEQUENCE_CHECKER(sequence_checker_);
+  DISALLOW_COPY_AND_ASSIGN(MediaSinkServiceBase);
+};
+
+}  // namespace media_router
+
+#endif  // CHROME_COMMON_MEDIA_ROUTER_DISCOVERY_MEDIA_SINK_SERVICE_BASE_H_
