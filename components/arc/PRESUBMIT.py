c370a93be6b4d50d0bf496bafea67f0e7e46e6c9
Luis Hector Chavez <lhchavez@google.com>
2017-11-28 11:16:31 -0800

    arc: Removing the last base::Callback/base::Closure in components/arc
    This change completely deprecates base::Callback/base::Closure in
    components/arc.
    
    Bug: 714018
    Test: git cl try
    Change-Id: I7d36f83869513d1a1e0a71bfa898c25ee78b5fa6

--- components/arc/PRESUBMIT.py
+++ components/arc/PRESUBMIT.py
@@ -5,6 +5,43 @@
 _CPP_WHITE_LIST = (r'.+\.cc$', r'.+\.h$')
 
 
+def CheckCallback(input_api, output_api):
+  """Encourage to use base::{Once,Repeating}{Callback,Closure}."""
+  errors = []
+  def SourceFilter(affected_file):
+    return input_api.FilterSourceFile(affected_file, _CPP_WHITE_LIST,
+                                      input_api.DEFAULT_BLACK_LIST)
+  for f in input_api.AffectedSourceFiles(SourceFilter):
+    for line_number, line in f.ChangedContents():
+      if input_api.re.search(r'\bbase::Callback<', line):
+        errors.append(output_api.PresubmitError(
+            ('%s:%d uses base::Callback. '
+             'Use base::{Once,Repeating}Callback instead.') %
+            (f.LocalPath(), line_number)))
+      if input_api.re.search(r'\bbase::Closure<', line):
+        errors.append(output_api.PresubmitError(
+            ('%s:%d uses base::Closure. '
+             'Use base::{Once,Repeating}Closure instead.') %
+            (f.LocalPath(), line_number)))
+  return errors
+
+
+def CheckBind(input_api, output_api):
+  """Encourage to use base::Bind{Once,Repeating}."""
+  errors = []
+  def SourceFilter(affected_file):
+    return input_api.FilterSourceFile(affected_file, _CPP_WHITE_LIST,
+                                      input_api.DEFAULT_BLACK_LIST)
+  for f in input_api.AffectedSourceFiles(SourceFilter):
+    for line_number, line in f.ChangedContents():
+      if input_api.re.search(r'\bbase::Bind\(', line):
+        errors.append(output_api.PresubmitError(
+            ('%s:%d uses base::Bind. '
+             'Use base::Bind{Once,Repeating} instead.') %
+            (f.LocalPath(), line_number)))
+  return errors
+
+
 def CheckMakeUnique(input_api, output_api):
   """Encourage to use std::make_unique, instead of base::MakeUnique."""
   errors = []
