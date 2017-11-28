d5a2c9a6a535bf58f84cf7dc10397833740c279d
Jian Li <jianli@chromium.org>
2017-11-28 14:35:32 -0800

    Validate file digest when serving offline pages
    Bug: 758690
    Change-Id: Ibb0bbc0fa8415c93b38d18b869a89588f852b47f

--- chrome/browser/offline_pages/offline_page_utils.h
+++ chrome/browser/offline_pages/offline_page_utils.h
@@ -13,6 +13,7 @@
 #include "url/gurl.h"
 
 namespace base {
+class FilePath;
 class Time;
 }
 
@@ -149,6 +150,16 @@ class OfflinePageUtils {
       const SizeInBytesCallback& callback,
       const base::Time& begin_time,
       const base::Time& end_time);
+
+  // Computes a SHA256 digest of the specified file. Empty string will be
+  // returned if the digest cannot be computed.
+  static std::string ComputeDigest(const base::FilePath& file_path);
+
+  // Returns true if the specified file has |expected_file_size| and
+  // |expected_digest|.
+  static bool ValidateFile(const base::FilePath& file_path,
+                           int64_t expected_file_size,
+                           const std::string& expected_digest);
 };
 
 }  // namespace offline_pages
