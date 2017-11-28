0405a85c1329f541b7a19edadbe5fc83fbd0889d
Tommi <tommi@chromium.org>
2017-11-28 16:41:15 +0100

    Make WebRTC's rtc::Event implementation rely on WaitableEvent.
    Also add a gtest_prod_util.h override to avoid conflict of macro redefinition.
    
    Bug:788694
    Change-Id: Ib5a597da593a8fccc39b7c37041bd283f4160f67

---
+++ third_party/webrtc_overrides/rtc_base/gtest_prod_util.h
@@ -0,0 +1,16 @@
+/*
+ *  Copyright (c) 2012 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. An additional intellectual property rights grant can be found
+ *  in the file PATENTS.  All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
+
+#ifndef RTC_BASE_GTEST_PROD_UTIL_H_
+#define RTC_BASE_GTEST_PROD_UTIL_H_
+
+#include "base/gtest_prod_util.h"
+
+#endif  // RTC_BASE_GTEST_PROD_UTIL_H_
