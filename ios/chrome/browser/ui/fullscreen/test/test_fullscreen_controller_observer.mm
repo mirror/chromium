12c8902a51c02c1bb2a42259595a784d1af70763
Kurt Horimoto <kkhorimoto@chromium.org>
2017-11-28 15:36:53 -0800

    [iOS] Created FullscreenControllerObserverManager.
    This intermediary object listens to changs from the FullscreenModel
    and propagates them to FullscreenControllerObservers.
    
    Additionally, this CL creates FullscreenScrollEndAnimationController,
    which manages the animator passed to FullscreenControllerObservers to
    manage the scroll end toolbar adjustment animation.
    
    Bug: 785671
    Change-Id: I925ad42aec3acb11f6562d591121dad3476a9d2a

---
+++ ios/chrome/browser/ui/fullscreen/test/test_fullscreen_controller_observer.mm
@@ -0,0 +1,25 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/fullscreen/test/test_fullscreen_controller_observer.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+void TestFullscreenControllerObserver::FullscreenProgressUpdated(
+    FullscreenController* controller,
+    CGFloat progress) {
+  progress_ = progress;
+}
+void TestFullscreenControllerObserver::FullscreenEnabledStateChanged(
+    FullscreenController* controller,
+    bool enabled) {
+  enabled_ = enabled;
+}
+void TestFullscreenControllerObserver::FullscreenScrollEventEnded(
+    FullscreenController* controller,
+    FullscreenScrollEndAnimator* animator) {
+  animator_ = animator;
+}
