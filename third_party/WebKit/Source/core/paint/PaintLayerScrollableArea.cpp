e11f2d9baaaf51e04c0907c10d4cae257a1570a4
W. James MacLean <wjmaclean@chromium.org>
2017-11-28 15:43:02 -0500

    Refactor ScrollingCoordinator to use Frames for NonFastScrollableRegions
    The net effect of this CL is to allow scrollable iframes/divs within
    an OOPIF subframe, and which do not have their own composited layer for
    scrolling, to be able to scroll.
    
    This CL converts ScrollingCoordinator::UpdateAfterCompositingChangeIfNeeded
    to take a LocalFrameView* instead of just defaulting to the main frame's
    view, so that changes for OOPIF subframes can also be accomodated. All
    relevant call sites are updated, and a no-longer-needed hack regarding
    NonFastScrollableRegions is removed from RenderWidgetCompositor as a result.
    
    Bug: 787498
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I608cfcdc7f7a4edd6469f9c8fed41d9ac0cf9706

--- third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
@@ -1972,6 +1972,7 @@ bool PaintLayerScrollableArea::UsesCompositedScrolling() const {
   // See https://codereview.chromium.org/176633003/ for the tests that fail
   // without this disabler.
   DisableCompositingQueryAsserts disabler;
+
   return Layer()->HasCompositedLayerMapping() &&
          Layer()->GetCompositedLayerMapping()->ScrollingLayer();
 }
