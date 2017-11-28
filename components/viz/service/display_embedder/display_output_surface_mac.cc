69bcd03cad7568a5a2e0dc2611bfe52d5f0e8340
Christopher Cameron <ccameron@chromium.org>
2017-11-28 14:32:49 -0800

    mac/viz: Split out DisplayOutputSurfaceMac
    Break DisplayOutputSurfaceOzone into a base class,
    SurfacelessDisplayOutputSurface, and make DisplayOutputSurfaceMac
    inherit from that. This mirrors the behavior of GpuOutputSurfaceMac
    in content::.
    
    Make changes to RenderWidgetHostViewMac to allow for --enable-viz to
    run. In particular:
      * resize the ui::Compositor when the NSView resizes, as otherwise no
        frames will attempt to draw.
      * query the --enable-viz flag to set on the DelegatedFrameHost
    
    Bug: 772576
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I2489be4143c4cca26294f253e8527920c330a5e3

---
+++ components/viz/service/display_embedder/display_output_surface_mac.cc
@@ -0,0 +1,26 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/viz/service/display_embedder/display_output_surface_mac.h"
+
+#include "gpu/GLES2/gl2extchromium.h"
+
+namespace viz {
+
+DisplayOutputSurfaceMac::DisplayOutputSurfaceMac(
+    scoped_refptr<InProcessContextProvider> context_provider,
+    gfx::AcceleratedWidget widget,
+    SyntheticBeginFrameSource* synthetic_begin_frame_source,
+    gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager)
+    : SurfacelessDisplayOutputSurface(context_provider,
+                                      widget,
+                                      synthetic_begin_frame_source,
+                                      gpu_memory_buffer_manager,
+                                      GL_TEXTURE_RECTANGLE_ARB,
+                                      GL_RGBA,
+                                      gfx::BufferFormat::RGBA_8888) {}
+
+DisplayOutputSurfaceMac::~DisplayOutputSurfaceMac() {}
+
+}  // namespace viz
--- components/viz/service/display_embedder/display_output_surface_mac.h
+++ components/viz/service/display_embedder/display_output_surface_mac.h
@@ -2,24 +2,24 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_DISPLAY_OUTPUT_SURFACE_OZONE_H_
-#define COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_DISPLAY_OUTPUT_SURFACE_OZONE_H_
+#ifndef COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_DISPLAY_OUTPUT_SURFACE_MAC_H_
+#define COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_DISPLAY_OUTPUT_SURFACE_MAC_H_
 
 #include "components/viz/service/display_embedder/surfaceless_display_output_surface.h"
 
 namespace viz {
 
-class DisplayOutputSurfaceOzone : public SurfacelessDisplayOutputSurface {
+class DisplayOutputSurfaceMac : public SurfacelessDisplayOutputSurface {
  public:
-  DisplayOutputSurfaceOzone(
+  DisplayOutputSurfaceMac(
       scoped_refptr<InProcessContextProvider> context_provider,
       gfx::AcceleratedWidget widget,
       SyntheticBeginFrameSource* synthetic_begin_frame_source,
       gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager);
 
-  ~DisplayOutputSurfaceOzone() override;
+  ~DisplayOutputSurfaceMac() override;
 };
 
 }  // namespace viz
 
-#endif  // COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_DISPLAY_OUTPUT_SURFACE_OZONE_H_
+#endif  // COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_DISPLAY_OUTPUT_SURFACE_MAC_H_
