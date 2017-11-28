9693d0eaa92e1209ff5fb3746f0c01c1711b49d6
Zhenyao Mo <zmo@chromium.org>
2017-11-28 14:05:08 -0800

    PreEstablish a GPU channel in RenderProcessHostImpl before renderer asks for one.
    BUG=783512
    TEST=bots
    R=piman@chromium.org
    
    Change-Id: I331bc073ebb2fb77aaffd074fd97e979b057ee44

--- content/browser/gpu/gpu_client.cc
+++ content/browser/gpu/gpu_client.cc
@@ -30,6 +30,7 @@ void GpuClient::Add(ui::mojom::GpuRequest request) {
 }
 
 void GpuClient::OnError() {
+  callback_.Reset();
   if (!bindings_.empty())
     return;
   BrowserGpuMemoryBufferManager* gpu_memory_buffer_manager =
@@ -38,21 +39,43 @@ void GpuClient::OnError() {
     gpu_memory_buffer_manager->ProcessRemoved(render_process_id_);
 }
 
+void GpuClient::PreEstablishGpuChannel() {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  BrowserThread::PostTask(BrowserThread::IO, FROM_HERE,
+                          base::BindOnce(&GpuClient::EstablishGpuChannel,
+                                         weak_factory_.GetWeakPtr(),
+                                         EstablishGpuChannelCallback()));
+}
+
 void GpuClient::OnEstablishGpuChannel(
-    const EstablishGpuChannelCallback& callback,
     mojo::ScopedMessagePipeHandle channel_handle,
     const gpu::GPUInfo& gpu_info,
     const gpu::GpuFeatureInfo& gpu_feature_info,
     GpuProcessHost::EstablishChannelStatus status) {
   DCHECK_EQ(channel_handle.is_valid(),
             status == GpuProcessHost::EstablishChannelStatus::SUCCESS);
+  gpu_channel_requested_ = false;
+  EstablishGpuChannelCallback callback = std::move(callback_);
+  DCHECK(!callback_);
+
   if (status == GpuProcessHost::EstablishChannelStatus::GPU_HOST_INVALID) {
     // GPU process may have crashed or been killed. Try again.
     EstablishGpuChannel(callback);
     return;
   }
-  callback.Run(render_process_id_, std::move(channel_handle), gpu_info,
-               gpu_feature_info);
+  if (callback) {
+    // A request is waiting.
+    callback.Run(render_process_id_, std::move(channel_handle), gpu_info,
+                 gpu_feature_info);
+    return;
+  }
+  if (status == GpuProcessHost::EstablishChannelStatus::SUCCESS) {
+    // This is the case we pre-establish a channel before a request arrives.
+    // Cache the channel for a future request.
+    channel_handle_ = std::move(channel_handle);
+    gpu_info_ = gpu_info;
+    gpu_feature_info_ = gpu_feature_info;
+  }
 }
 
 void GpuClient::OnCreateGpuMemoryBuffer(
@@ -61,17 +84,36 @@ void GpuClient::OnCreateGpuMemoryBuffer(
   callback.Run(handle);
 }
 
+void GpuClient::ClearCallback() {
+  if (!callback_)
+    return;
+  EstablishGpuChannelCallback callback = std::move(callback_);
+  callback.Run(render_process_id_, mojo::ScopedMessagePipeHandle(),
+               gpu::GPUInfo(), gpu::GpuFeatureInfo());
+  DCHECK(!callback_);
+}
+
 void GpuClient::EstablishGpuChannel(
     const EstablishGpuChannelCallback& callback) {
+  // At most one channel should be requested. So clear previous request first.
+  ClearCallback();
   GpuProcessHost* host = GpuProcessHost::Get();
   if (!host) {
-    OnEstablishGpuChannel(
-        callback, mojo::ScopedMessagePipeHandle(), gpu::GPUInfo(),
-        gpu::GpuFeatureInfo(),
-        GpuProcessHost::EstablishChannelStatus::GPU_ACCESS_DENIED);
+    callback.Run(render_process_id_, mojo::ScopedMessagePipeHandle(),
+                 gpu::GPUInfo(), gpu::GpuFeatureInfo());
     return;
   }
-
+  if (channel_handle_.is_valid()) {
+    // If a channel has been pre-established and cached, return it right away.
+    callback.Run(render_process_id_, std::move(channel_handle_), gpu_info_,
+                 gpu_feature_info_);
+    DCHECK(!channel_handle_.is_valid());
+    return;
+  }
+  callback_ = callback;
+  if (gpu_channel_requested_)
+    return;
+  gpu_channel_requested_ = true;
   bool preempts = false;
   bool allow_view_command_buffers = false;
   bool allow_real_time_streams = false;
@@ -80,8 +122,8 @@ void GpuClient::EstablishGpuChannel(
       ChildProcessHostImpl::ChildProcessUniqueIdToTracingProcessId(
           render_process_id_),
       preempts, allow_view_command_buffers, allow_real_time_streams,
-      base::Bind(&GpuClient::OnEstablishGpuChannel, weak_factory_.GetWeakPtr(),
-                 callback));
+      base::Bind(&GpuClient::OnEstablishGpuChannel,
+                 weak_factory_.GetWeakPtr()));
 }
 
 void GpuClient::CreateJpegDecodeAccelerator(
