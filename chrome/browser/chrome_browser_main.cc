bc9662af26acd8987ee5b31c8738a6a34ca251dc
Michael Giuffrida <michaelpg@chromium.org>
2017-11-28 12:52:56 -0800

    Log UKMs for Tab Activity
    This is the beginning of several CLs to add UKMs for tab activity. The
    goal is to log certain features of tabs upon key events, like tab
    deactivation. For some of these features, we also log the tab when the
    features themselves change  This CL adds a small subset of such metrics.
    
    This CL adds a TabActivityWatcher to listen for events on tab strips and
    web contents indicating activity. It also adds TabSnapshotLogger, which
    logs relevant information about the tab as a UKM entry.
    
    The TabActivityWatcher unit test uses a mock TabSnapshotLogger to test
    how the tracker responds to tab events.
    
    See bug for design doc.
    
    Bug: 784639
    Change-Id: I211fd7202a1bf08c9a4c07904a60f65e4ef170f9

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -189,6 +189,7 @@
 #include "ui/base/resource/resource_bundle_android.h"
 #else
 #include "chrome/browser/feedback/feedback_profile_observer.h"
+#include "chrome/browser/ui/tabs/tab_activity_watcher.h"
 #endif  // defined(OS_ANDROID)
 
 #if BUILDFLAG(ENABLE_OFFLINE_PAGES)
@@ -1365,6 +1366,8 @@ void ChromeBrowserMainParts::PostBrowserStart() {
         base::BindOnce(&WebUsbDetector::Initialize,
                        base::Unretained(web_usb_detector_.get())));
   }
+  // Initialize the TabActivityWatcher to begin logging tab activity events.
+  TabActivityWatcher::GetInstance();
 #endif
 
   // At this point, StartupBrowserCreator::Start has run creating initial
---
+++ chrome/browser/ui/tabs/tab_snapshot_logger_impl.h
@@ -0,0 +1,29 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_TABS_TAB_SNAPSHOT_LOGGER_IMPL_H_
+#define CHROME_BROWSER_UI_TABS_TAB_SNAPSHOT_LOGGER_IMPL_H_
+
+#include "chrome/browser/ui/tabs/tab_snapshot_logger.h"
+
+#include "base/macros.h"
+
+// Logs a TabManager.TabSnapshot UKM for a tab when requested. Includes
+// information relevant to the tab and its WebContents.
+// Must be used on the UI thread.
+// TODO(michaelpg): Unit test for UKMs.
+class TabSnapshotLoggerImpl : public TabSnapshotLogger {
+ public:
+  TabSnapshotLoggerImpl();
+  ~TabSnapshotLoggerImpl() override;
+
+  // TabSnapshotLogger:
+  void LogBackgroundTab(ukm::SourceId ukm_source_id,
+                        content::WebContents* web_contents) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(TabSnapshotLoggerImpl);
+};
+
+#endif  // CHROME_BROWSER_UI_TABS_TAB_SNAPSHOT_LOGGER_IMPL_H_
