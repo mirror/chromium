f4407da5d111a9b43ff69c2d1d672d60ada6001a
Becca Hughes <beccahughes@chromium.org>
2017-11-29 10:24:13 +0000

    Feature Policy: Enable "autoplay" feature
    Add an "autoplay" feature that controls autoplay and delegate
    activation state through frames.
    
    BUG=788390
    
    Change-Id: Ifeee15cf526d8a27e1b73fa62784e860c73af1ee

---
+++ third_party/WebKit/LayoutTests/http/tests/media/autoplay/resources/video-test.js
@@ -0,0 +1,84 @@
+/**
+ * Tests autoplay on a page and it's subframes.
+ */
+
+// Tracks the results and how many active tests we have running.
+let activeTests;
+let testExpectations = {};
+let callback;
+
+function tearDown(result) {
+  // Reset the flag state.
+  window.internals.settings.setAutoplayPolicy('no-user-gesture-required');
+
+  const resultData = {
+    url: window.location.href,
+    message: result == undefined
+  };
+
+  if (callback) {
+    receivedResult(resultData);
+  } else {
+    top.postMessage(resultData, '*');
+  }
+}
+
+function receivedResult(data) {
+  activeTests--;
+  assert_equals(testExpectations[data.url], data.message);
+
+  if (!activeTests)
+    callback();
+}
+
+function runVideoTest() {
+  const video = document.createElement('video');
+  video.src = findMediaFile('video', '/media-resources/content/test');
+  video.play().then(tearDown, tearDown);
+}
+
+function simulateViewportClick() {
+  chrome.gpuBenchmarking.pointerActionSequence([
+      {"source": "mouse",
+       "actions": [
+       { "name": "pointerDown", "x": 0, "y": 0 },
+       { "name": "pointerUp" } ]}]);
+}
+
+function simulateFrameClick() {
+  const frame = document.getElementsByTagName('iframe')[0];
+  const rect = frame.getBoundingClientRect();
+
+  chrome.gpuBenchmarking.pointerActionSequence([
+      {"source": "mouse",
+       "actions": [
+       { "name": "pointerDown", "x": rect.left, "y": rect.top },
+       { "name": "pointerUp" } ]}]);
+}
+
+function runTest(expectations) {
+  async_test((t) => {
+    testExpectations = expectations;
+    activeTests = Object.keys(expectations).length;
+    callback = t.step_func_done();
+  });
+}
+
+window.addEventListener('load', () => {
+  if (!window.testRunner)
+    return;
+
+  // Setup the flags before the test is run.
+  window.internals.settings.setAutoplayPolicy('document-user-activation-required');
+
+  // Setup the event listener to forward messages.
+  window.addEventListener('message', (e) => {
+    if (callback) {
+      receivedResult(e.data);
+    } else {
+      top.postMessage(e.data, '*');
+    }
+  });
+
+  runVideoTest();
+}, { once: true });
