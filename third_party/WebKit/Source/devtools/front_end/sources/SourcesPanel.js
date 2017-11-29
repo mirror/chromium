19e305c6f1d26bc3cacb6bc363ea4a00eabb1f63
Joel Einbinder <einbinder@chromium.org>
2017-11-29 14:30:48 -0800

    DevTools: Remove RevisionHistoryView
    Removes RevisionHistoryView and uiSourceCode.history()
    
    Bug: none
    Change-Id: I743aeffd19715031c2ab1b59a6cdf6845963de58

--- third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
+++ third_party/WebKit/Source/devtools/front_end/sources/SourcesPanel.js
@@ -698,13 +698,6 @@ Sources.SourcesPanel = class extends UI.Panel {
   }
 
   /**
-   * @param {!Workspace.UISourceCode} uiSourceCode
-   */
-  _showLocalHistory(uiSourceCode) {
-    Sources.RevisionHistoryView.showHistory(uiSourceCode);
-  }
-
-  /**
    * @override
    * @param {!Event} event
    * @param {!UI.ContextMenu} contextMenu
