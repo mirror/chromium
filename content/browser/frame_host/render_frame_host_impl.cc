b1d60e3390ac04cd73998acf0a50376fe9d38aea
Becca Hughes <beccahughes@chromium.org>
2017-11-29 15:21:23 +0000

    Autoplay: Replicate user gesture before nav bit.
    The "HasUserReceivedUserGestureBeforeNavigation" bit is currently
    local to Blink and activation delegation will require that we can
    access this bit across cross origin iframes. This means we need
    to replicate this across processes.
    
    The tests include coverage for feature policy to ensure it works
    correctly with the delegation.
    
    BUG=767389
    
    Change-Id: Ia9c11c62bb9a2e195fbfefc09d53a32de73cddd5

--- content/browser/frame_host/render_frame_host_impl.cc
+++ content/browser/frame_host/render_frame_host_impl.cc
@@ -954,6 +954,8 @@ bool RenderFrameHostImpl::OnMessageReceived(const IPC::Message &msg) {
                         OnSetHasReceivedUserGesture)
     IPC_MESSAGE_HANDLER(FrameHostMsg_ScrollRectToVisibleInParentFrame,
                         OnScrollRectToVisibleInParentFrame)
+    IPC_MESSAGE_HANDLER(FrameHostMsg_SetHasReceivedUserGestureBeforeNavigation,
+                        OnSetHasReceivedUserGestureBeforeNavigation)
 #if BUILDFLAG(USE_EXTERNAL_POPUP_MENU)
     IPC_MESSAGE_HANDLER(FrameHostMsg_ShowPopup, OnShowPopup)
     IPC_MESSAGE_HANDLER(FrameHostMsg_HidePopup, OnHidePopup)
@@ -2785,6 +2787,10 @@ void RenderFrameHostImpl::OnScrollRectToVisibleInParentFrame(
   if (!proxy)
     return;
   proxy->ScrollRectToVisible(rect_to_scroll, properties);
+}
+
+void RenderFrameHostImpl::OnSetHasReceivedUserGestureBeforeNavigation() {
+  frame_tree_node_->OnSetHasReceivedUserGestureBeforeNavigation();
 }
 
 #if BUILDFLAG(USE_EXTERNAL_POPUP_MENU)
