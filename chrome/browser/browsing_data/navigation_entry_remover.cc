31581ddbd27d616869845b8097ce591f16aa25c0
Christian Dullweber <dullweber@chromium.org>
2017-11-29 15:58:42 +0100

    Cleanup navigation entries after CBD and history deletions
    When clearing history also clear navigation entry, rewrite
    stored sessions and tell the UI about these changes.
    
    Bug: 626007
    Change-Id: I5cdf0283ff688e2e7de540731d7adf6863534270

---
+++ chrome/browser/browsing_data/navigation_entry_remover.cc
@@ -0,0 +1,70 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/browsing_data/navigation_entry_remover.h"
+
+#include "build/build_config.h"
+#include "chrome/browser/profiles/profile.h"
+#include "content/public/browser/navigation_controller.h"
+#include "content/public/browser/web_contents.h"
+
+#if defined(OS_ANDROID)
+#include "chrome/browser/android/tab_android.h"
+#include "chrome/browser/ui/android/tab_model/tab_model.h"
+#include "chrome/browser/ui/android/tab_model/tab_model_list.h"
+#else
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#endif
+
+namespace {
+
+template <typename TabList>
+void PruneNavigationEntries(TabList* tab_list,
+                            int count,
+                            bool everything,
+                            const std::set<GURL>& origins) {
+  for (int i = 0; i < count; i++) {
+    content::WebContents* web_contents = tab_list->GetWebContentsAt(i);
+    if (web_contents->GetController().CanPruneAllButLastCommitted()) {
+      web_contents->GetController().PruneNavigationEntries(origins);
+    }
+  }
+}
+
+}  // namespace
+
+namespace browsing_data {
+
+// TODO(dullweber): Also remove navigation on IOS?
+void RemoveNavigationEntries(Profile* profile,
+                             const history::URLRows& deleted_rows) {
+  if (deleted_rows.empty())
+    return;
+  std::set<GURL> urls;
+  for (const history::URLRow& row : deleted_rows) {
+    urls.insert(row.url());
+    LOG(ERROR) << "his:" << row.url().GetOrigin();
+  }
+
+  DCHECK(profile->GetProfileType() == Profile::ProfileType::REGULAR_PROFILE);
+#if defined(OS_ANDROID)
+  for (auto it = TabModelList::begin(); it != TabModelList::end(); ++it) {
+    TabModel* tab_model = *it;
+    if (tab_model->GetProfile() == profile) {
+      PruneNavigationEntries(tab_model, tab_model->GetTabCount(), urls);
+    }
+  }
+#else
+  for (Browser* browser : *BrowserList::GetInstance()) {
+    TabStripModel* tab_strip = browser->tab_strip_model();
+    if (browser->profile() == profile) {
+      PruneNavigationEntries(tab_strip, tab_strip->count(), urls);
+    }
+  }
+#endif
+}
+
+}  // namespace browsing_data
--- content/browser/frame_host/navigation_controller_impl.cc
+++ content/browser/frame_host/navigation_controller_impl.cc
@@ -90,10 +90,10 @@ namespace {
 // current entries are [google, digg, yahoo], with the current entry google,
 // and the user types in cnet, then digg and yahoo are pruned.
 void NotifyPrunedEntries(NavigationControllerImpl* nav_controller,
-                         bool from_front,
+                         PrunedPosition position,
                          int count) {
   PrunedDetails details;
-  details.from_front = from_front;
+  details.position = position;
   details.count = count;
   nav_controller->delegate()->NotifyNavigationListPruned(details);
 }
@@ -1700,6 +1700,8 @@ bool NavigationControllerImpl::CanPruneAllButLastCommitted() {
 }
 
 void NavigationControllerImpl::PruneAllButLastCommitted() {
+  int entries_front = last_committed_entry_index_;
+  int entries_back = GetEntryCount() - last_committed_entry_index_;
   PruneAllButLastCommittedInternal();
 
   DCHECK_EQ(0, last_committed_entry_index_);
@@ -1707,6 +1709,11 @@ void NavigationControllerImpl::PruneAllButLastCommitted() {
 
   delegate_->SetHistoryOffsetAndLength(last_committed_entry_index_,
                                        GetEntryCount());
+  // TODO(dullweber): Use this as an optimization when "All time" is selected?
+  if (entries_front > 0)
+    NotifyPrunedEntries(this, PrunedPosition::FRONT, entries_front);
+  if (entries_back > 0)
+    NotifyPrunedEntries(this, PrunedPosition::BACK, entries_back);
 }
 
 void NavigationControllerImpl::PruneAllButLastCommittedInternal() {
@@ -1719,6 +1726,27 @@ void NavigationControllerImpl::PruneAllButLastCommittedInternal() {
                  entries_.begin() + last_committed_entry_index_);
   entries_.erase(entries_.begin() + 1, entries_.end());
   last_committed_entry_index_ = 0;
+}
+
+void NavigationControllerImpl::PruneNavigationEntries(
+    const std::set<GURL>& origins) {
+  int pruned = 0;
+  for (size_t i = 0; i < entries_.size();) {
+    LOG(ERROR) << "nav: " << entries_[i]->GetURL();
+    if (i == static_cast<size_t>(last_committed_entry_index_) ||
+        origins.find(entries_[i]->GetURL()) == origins.end()) {
+      i++;
+      continue;
+    }
+    pruned++;
+    RemoveEntryAtIndex(i);
+    NotifyPrunedEntries(this, PrunedPosition::INDEX, i);
+  }
+  LOG(ERROR) << "PRUNED: " << pruned;
+  if (pruned) {
+    delegate_->SetHistoryOffsetAndLength(last_committed_entry_index_,
+                                         GetEntryCount());
+  }
 }
 
 void NavigationControllerImpl::ClearAllScreenshots() {
@@ -1888,7 +1916,7 @@ void NavigationControllerImpl::InsertOrReplaceEntry(
       current_size--;
     }
     if (num_pruned > 0)  // Only notify if we did prune something.
-      NotifyPrunedEntries(this, false, num_pruned);
+      NotifyPrunedEntries(this, PrunedPosition::BACK, num_pruned);
   }
 
   PruneOldestEntryIfFull();
@@ -1902,7 +1930,7 @@ void NavigationControllerImpl::PruneOldestEntryIfFull() {
     DCHECK_EQ(max_entry_count(), entries_.size());
     DCHECK_GT(last_committed_entry_index_, 0);
     RemoveEntryAtIndex(0);
-    NotifyPrunedEntries(this, true, 1);
+    NotifyPrunedEntries(this, PrunedPosition::FRONT, 1);
   }
 }
 
