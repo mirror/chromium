db7ce7d93aa03f30d8494e12a6533bda8725e33e
Ian Vollick <vollick@chromium.org>
2017-11-28 22:02:13 -0500

    [vr] Fix autopresentation visibility issues
    I introduced a regression that caused the splash screen and the WebVR
    timeout spinner to coexist temporarily. This happened because I failed
    to set the "show the splash screen" bit to false when a timeout was
    imminent (i.e., when the splash screen was showing).
    
    Moreover, we also drew the UI over the first WebVR frame, clobbering it.
    
    With this change, the splash screen is correctly dismissed when the
    spinner appears, and the first WebVR frame is visible.
    
    Bug:None
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I90339dbf7d80619c13a6228233bead0d6d365473

--- chrome/browser/vr/ui_scene_creator.cc
+++ chrome/browser/vr/ui_scene_creator.cc
@@ -444,8 +444,7 @@ void UiSceneCreator::CreateSplashScreenForDirectWebVrLaunch() {
   transient_parent->SetTransitionedProperties({OPACITY});
   transient_parent->AddBinding(VR_BIND_FUNC(
       bool, Model, model_,
-      web_vr_show_splash_screen &&
-          model->web_vr_timeout_state == kWebVrNoTimeoutPending,
+      web_vr_show_splash_screen && model->received_web_vr_frame(),
       ShowUntilSignalTransientElement, transient_parent.get(), Signal));
   scene_->AddUiElement(kSplashScreenViewportAwareRoot,
                        std::move(transient_parent));
@@ -1071,10 +1070,11 @@ void UiSceneCreator::CreateWebVrUrlToast() {
   auto* parent =
       AddTransientParent(kWebVrUrlToastTransientParent, kWebVrViewportAwareRoot,
                          kWebVrUrlToastTimeoutSeconds, true, scene_);
-  parent->AddBinding(VR_BIND_FUNC(
-      bool, Model, model_,
-      web_vr_started_for_autopresentation && !model->web_vr_show_splash_screen,
-      UiElement, parent, SetVisible));
+  parent->AddBinding(VR_BIND_FUNC(bool, Model, model_,
+                                  web_vr_started_for_autopresentation &&
+                                      !model->web_vr_show_splash_screen &&
+                                      model->received_web_vr_frame(),
+                                  UiElement, parent, SetVisible));
 
   auto element = base::MakeUnique<WebVrUrlToast>(
       512, base::Bind(&UiBrowserInterface::OnUnsupportedMode,
@@ -1311,11 +1311,9 @@ void UiSceneCreator::CreateToasts() {
   // When we first get a web vr frame, we switch states to
   // kWebVrNoTimeoutPending, when that happens, we want to SetVisible(true) to
   // kick the visibility of this element.
-  parent->AddBinding(
-      VR_BIND_FUNC(bool, Model, model_,
-                   web_vr_timeout_state == kWebVrNoTimeoutPending &&
-                       model->web_vr_mode && model->web_vr_show_toast,
-                   UiElement, parent, SetVisible));
+  parent->AddBinding(VR_BIND_FUNC(
+      bool, Model, model_, received_web_vr_frame() && model->web_vr_show_toast,
+      UiElement, parent, SetVisible));
 
   element = base::MakeUnique<ExclusiveScreenToast>(512);
   element->set_name(kExclusiveScreenToastViewportAware);
