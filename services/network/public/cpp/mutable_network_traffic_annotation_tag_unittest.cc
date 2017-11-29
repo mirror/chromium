c015a736a8d643ef04daa95a2820bde003d16e46
Ramin Halavati <rhalavati@chromium.org>
2017-11-29 11:12:10 +0100

    Mutable Partial Network Traffic Annotation Tag Mojo interface added.
    Mojo interface added for MutablePartialNetworkTrafficAnnotationTag.
    This interface is now required for
    public_ip_address_geolocation_provider and will also be required for
    all Mojo interfaces that pass a partial network traffic annotation tag.
    
    Bug:656607
    Change-Id: I61dd345a0a60166413c2126c89098e9e1f031f43

---
+++ services/network/public/cpp/mutable_network_traffic_annotation_tag_unittest.cc
@@ -0,0 +1,40 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/macros.h"
+#include "base/message_loop/message_loop.h"
+#include "mojo/public/cpp/bindings/binding_set.h"
+#include "services/network/public/cpp/mutable_network_traffic_annotation_tag_struct_traits.h"
+#include "services/network/public/interfaces/mutable_network_traffic_annotation_tag.mojom.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace network {
+
+namespace {
+
+class MutableNetworkTrafficAnnottionTagsTest : public testing::Test {
+ protected:
+  MutableNetworkTrafficAnnottionTagsTest() = default;
+
+  DISALLOW_COPY_AND_ASSIGN(MutableNetworkTrafficAnnottionTagsTest);
+};
+
+}  // namespace
+
+TEST_F(MutableNetworkTrafficAnnottionTagsTest, BasicTest) {
+  net::MutableNetworkTrafficAnnotationTag original;
+  net::MutableNetworkTrafficAnnotationTag copy;
+
+  original.unique_id_hash_code = 1;
+  EXPECT_TRUE(mojom::MutableNetworkTrafficAnnotationTag::Deserialize(
+      mojom::MutableNetworkTrafficAnnotationTag::Serialize(&original), &copy));
+  EXPECT_EQ(copy.unique_id_hash_code, 1);
+
+  original.unique_id_hash_code = 2;
+  EXPECT_TRUE(mojom::MutableNetworkTrafficAnnotationTag::Deserialize(
+      mojom::MutableNetworkTrafficAnnotationTag::Serialize(&original), &copy));
+  EXPECT_EQ(copy.unique_id_hash_code, 1);
+}
+
+}  // namespace network
