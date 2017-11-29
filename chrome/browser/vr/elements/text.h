31379024b3c6642b65510913d05868c20ad06c71
Christopher Grant <cjgrant@chromium.org>
2017-11-29 14:15:24 -0500

    VR: Support non-multiline text
    Text elements can now default to multiline, but can be configured as
    single-line, to truncate rather than grow the texture vertically.
    
    Along the way, remove a field width parameter from the Text constructor.  This
    value needs to match the element size, and should be pulled from element size
    rather than explicitly set to match.
    
    BUG=
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I8a0e48b1efb3b562e9a8f0037e9718020bcfc01b

--- chrome/browser/vr/elements/text.h
+++ chrome/browser/vr/elements/text.h
@@ -17,15 +17,16 @@ class TextTexture;
 
 class Text : public TexturedElement {
  public:
-  Text(int maximum_width_pixels,
-       float font_height_meters,
-       float text_width_meters);
+  Text(int maximum_width_pixels, float font_height_meters);
   ~Text() override;
 
   void SetText(const base::string16& text);
   void SetColor(SkColor color);
 
   void SetTextAlignment(UiTexture::TextAlignment alignment);
+  void SetMultiLine(bool multiline);
+
+  void OnSetSize(gfx::SizeF size) override;
 
  private:
   UiTexture* GetTexture() const override;
