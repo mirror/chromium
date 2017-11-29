6ddbdc7234e17a35852e3db0e7b43e148db2766d
Adenilson Cavalcanti <adenilson.cavalcanti@arm.com>
2017-11-29 11:09:19 -0800

    Using ARMv8 CRC32 specific instruction
    CRC32 affects performance for both image decompression (PNG)
    as also in general browsing while accessing websites that serve
    content using compression (i.e. Content-Encoding: gzip).
    
    This patch implements an optimized CRC32 function using the
    dedicated instruction available in ARMv8. This instruction is available
    in new Android devices featuring an ARMv8 SoC, like Nexus 5x and
    Google Pixel.
    
    It should be between 6x (A53: 116ms X 22ms for a 4Kx4Kx4 buffer) to
    10x faster (A72: 91ms x 9ms) than the C implementation currently used
    by zlib.
    
    PNG decoding performance gains should be around 5-9%.
    
    Finally it also introduces code to perform the ARM CPU features detection
    using getauxval()@Linux/CrOS or android_getCpuFeatures(). We pre-built
    and link the CRC32 instruction dependent code but will decide if to
    use it at run time.
    
    If the feature is not supported, we fallback to the C implementation.
    
    This approach allows to use the instruction in both 32bits and 64bits
    builds and works fine either in ARMv7 or ARMv8 processor. I tested the
    generated Chromium apk in both a ARMv7 (Nexus 4 and 6) and ARMv8 (Nexus 5x and
    Google Pixel).
    
    Change-Id: I069408ebc06c49a3c2be4ba3253319e025ee09d7
    Bug: 709716

---
+++ third_party/zlib/contrib/optimizations/arm/arm_features.c
@@ -0,0 +1,64 @@
+/* arm_features.h -- SoC features detection.
+ * Copyright (C) 2017 ARM, Inc.
+ * For conditions of distribution and use, see copyright notice in zlib.h
+ */
+#include "arm_features.h"
+/* TODO(cavalcantii): Should we handle Windows? Anyway this is gated in
+ * the build.
+ */
+#include <pthread.h>
+#include <stdint.h>
+
+#if defined(ARMV8_OS_LINUX)
+#include <sys/auxv.h>
+#elif defined(ARMV8_OS_ANDROID)
+#include <cpu-features.h>
+#endif
+
+/* Keep the information concerning the features. */
+#define ZLIB_INTERNAL __attribute__((visibility ("hidden")))
+static unsigned char ZLIB_INTERNAL arm_cpu_enable_crc32 = 0;
+static unsigned char ZLIB_INTERNAL arm_cpu_enable_pmull = 0;
+
+/* A read on manpage for pthread_once() suggests this to safely
+ * dynamically perform initialization in C libraries.
+ */
+static pthread_once_t cpu_check_inited_once = PTHREAD_ONCE_INIT;
+
+static void init_arm_features(void);
+
+void arm_check_features(void)
+{
+    pthread_once(&cpu_check_inited_once, init_arm_features);
+}
+
+static void init_arm_features(void)
+{
+    uint32_t flag_crc32 = 0, flag_pmull = 0, capabilities = 0;
+#if defined(ARMV8_OS_LINUX)
+    flag_crc32 = (1 << 7);
+    flag_pmull = (1 << 4);
+    capabilities = getauxval(AT_HWCAP);
+#elif defined(ARMV8_OS_ANDROID)
+    flag_crc32 = ANDROID_CPU_ARM_FEATURE_CRC32;
+    flag_pmull = ANDROID_CPU_ARM_FEATURE_PMULL;
+    capabilities = android_getCpuFeatures();
+#endif
+
+    if (capabilities & flag_crc32)
+        arm_cpu_enable_crc32 = 1;
+
+    if (capabilities & flag_pmull)
+        arm_cpu_enable_pmull = 1;
+}
+
+unsigned char arm_supports_crc32()
+{
+    return arm_cpu_enable_crc32;
+}
+
+unsigned char arm_supports_pmull()
+{
+    return arm_cpu_enable_pmull;
+}
+
