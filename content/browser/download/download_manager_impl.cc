04a7e78236706decb93e16769b83afe4cae4df26
Joy Ming <jming@chromium.org>
2017-11-29 13:23:38 -0800

    In_progress_cache : Read/write implementation.
    The in-progress cache is used to store metadata related to in-progress
    downloads. The entry for the download is created upon the initiation of
    the download and removed after the download is completed. This CL
    continues the implementation of the in-progress cache by implementing
    the read/write functionality in the following manner:
    
    - making the in-progress cache an interface.
    - initializing the cache during the creation of the download item,
    halting the completion of the creation until the cache is initialized
    (similar to the history database, which persists longer-term info).
    - reading from the file during initialization and storing information
    in an |entries| object.
    - updating the |entries| object when adding/replacing/deleting and then
    writing to the file using ImportantFileWriter.
    - including unittests for this process.
    
    Bug:778425
    Change-Id: I8f69dd51dd619dbc688c0f2b6a3b079a3b1164ec

--- content/browser/download/download_manager_impl.cc
+++ content/browser/download/download_manager_impl.cc
@@ -343,6 +343,9 @@ InProgressDownloadObserver::~InProgressDownloadObserver() = default;
 void InProgressDownloadObserver::OnDownloadUpdated(DownloadItem* download) {
   // TODO(crbug.com/778425): Properly handle fail/resume/retry for downloads
   // that are in the INTERRUPTED state for a long time.
+  if (!in_progress_cache_)
+    return;
+
   switch (download->GetState()) {
     case DownloadItem::DownloadState::COMPLETE:
     case DownloadItem::DownloadState::CANCELLED:
@@ -359,6 +362,9 @@ void InProgressDownloadObserver::OnDownloadUpdated(DownloadItem* download) {
 }
 
 void InProgressDownloadObserver::OnDownloadRemoved(DownloadItem* download) {
+  if (!in_progress_cache_)
+    return;
+
   in_progress_cache_->RemoveEntry(download->GetGuid());
 }
 
@@ -367,6 +373,8 @@ DownloadManagerImpl::DownloadManagerImpl(BrowserContext* browser_context)
       file_factory_(new DownloadFileFactory()),
       shutdown_needed_(true),
       initialized_(false),
+      history_db_initialized_(false),
+      in_progress_cache_initialized_(false),
       browser_context_(browser_context),
       delegate_(nullptr),
       weak_factory_(this) {
@@ -886,11 +894,31 @@ DownloadItem* DownloadManagerImpl::CreateDownloadItem(
   return item;
 }
 
-void DownloadManagerImpl::PostInitialization() {
+void DownloadManagerImpl::PostInitialization(
+    DownloadInitializationDependency dependency) {
   DCHECK(!initialized_);
-  initialized_ = true;
-  for (auto& observer : observers_)
-    observer.OnManagerInitialized();
+
+  switch (dependency) {
+    case DOWNLOAD_INITIALIZATION_DEPENDENCY_HISTORY_DB:
+      history_db_initialized_ = true;
+      break;
+    case DOWNLOAD_INITIALIZATION_DEPENDENCY_IN_PROGRESS_CACHE:
+      in_progress_cache_initialized_ = true;
+      break;
+    case DOWNLOAD_INITIALIZATION_DEPENDENCY_NONE:
+    default:
+      NOTREACHED();
+      break;
+  }
+
+  // Download manager is only initialized if both history db and in progress
+  // cache are initialized.
+  initialized_ = history_db_initialized_ && in_progress_cache_initialized_;
+
+  if (initialized_) {
+    for (auto& observer : observers_)
+      observer.OnManagerInitialized();
+  }
 }
 
 bool DownloadManagerImpl::IsManagerInitialized() const {
