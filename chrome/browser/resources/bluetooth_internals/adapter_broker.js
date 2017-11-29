4cbedfde5a2630088c57630b0d1a2e5f3e6ff8a2
Vincent Scheib <scheib@chromium.org>
2017-11-29 10:30:25 -0800

    bluetooth: internals page: Refactor instantiation of Bluetooth service.
    Bug: 746132
    Change-Id: Idcaa51d1ed4fb818bd3d8ae617e41a28eabfcb44

--- chrome/browser/resources/bluetooth_internals/adapter_broker.js
+++ chrome/browser/resources/bluetooth_internals/adapter_broker.js
@@ -220,18 +220,21 @@ cr.define('adapter_broker', function() {
     if (adapterBroker)
       return Promise.resolve(adapterBroker);
 
-    var adapterFactory = new bluetooth.mojom.AdapterFactoryPtr;
+    var bluetoothInternalsHandler = new mojom.BluetoothInternalsHandlerPtr;
     Mojo.bindInterface(
-        bluetooth.mojom.AdapterFactory.name,
-        mojo.makeRequest(adapterFactory).handle);
+        mojom.BluetoothInternalsHandler.name,
+        mojo.makeRequest(bluetoothInternalsHandler).handle);
 
     // Get an Adapter service.
-    return adapterFactory.getAdapter().then(function(response) {
+    return bluetoothInternalsHandler.getAdapter().then(function(response) {
+console.error("getAdapterBroker");
       if (!response.adapter.ptr.isBound()) {
         throw new Error('Bluetooth Not Supported on this platform.');
       }
+console.error("getAdapterBroker");
 
       adapterBroker = new AdapterBroker(response.adapter);
+console.error("getAdapterBroker");
       return adapterBroker;
     });
   }
