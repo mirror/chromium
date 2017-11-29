09190520422384f3d8c4d85ee8cd8d905c41474f
Luciano Pacheco <lucmult@chromium.org>
2017-11-29 13:19:13 +1100

    Convert 3 FrameHost_* IPC calls to mojo
    Convert FrameHost_DidChangeName, FrameHost_DidSetFeaturePolicyHeader
    and FrameHost_EnforceInsecureRequestPolicy from IPC calls to mojo.
    
    Create mojo conversions for enum FeaturePolicyFeature and struct
    ParsedFeaturePolicyDeclaration.
    
    Update third_party/WebKit/common/feature_policy/OWNERS with IPC
    SECURITY_OWNERS settings for new mojo files.
    
    Bug: 786836
    Change-Id: I18d18956197d3c7c5a713c61dd9856c8a31c06f7

--- content/test/test_render_frame_host.cc
+++ content/test/test_render_frame_host.cc
@@ -318,7 +318,7 @@ void TestRenderFrameHost::SimulateFeaturePolicyHeader(
   header[0].feature = feature;
   header[0].matches_all_origins = false;
   header[0].origins = whitelist;
-  OnDidSetFramePolicyHeaders(blink::WebSandboxFlags::kNone, header);
+  DidSetFramePolicyHeaders(blink::WebSandboxFlags::kNone, header);
 }
 
 const std::vector<std::string>& TestRenderFrameHost::GetConsoleMessages() {
@@ -492,7 +492,7 @@ void TestRenderFrameHost::DidChangeOpener(int opener_routing_id) {
 
 void TestRenderFrameHost::DidEnforceInsecureRequestPolicy(
     blink::WebInsecureRequestPolicy policy) {
-  OnEnforceInsecureRequestPolicy(policy);
+  EnforceInsecureRequestPolicy(policy);
 }
 
 void TestRenderFrameHost::PrepareForCommit() {
@@ -591,7 +591,7 @@ void TestRenderFrameHost::SimulateWillStartRequest(
 void TestRenderFrameHost::SendFramePolicy(
     blink::WebSandboxFlags sandbox_flags,
     const blink::ParsedFeaturePolicy& declared_policy) {
-  OnDidSetFramePolicyHeaders(sandbox_flags, declared_policy);
+  DidSetFramePolicyHeaders(sandbox_flags, declared_policy);
 }
 
 mojom::FrameNavigationControl* TestRenderFrameHost::GetNavigationControl() {
