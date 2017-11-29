eb202e9541e75bee792781ddff1aeeead4ddf858
Jacob Dufault <jdufault@google.com>
2017-11-29 13:28:44 -0800

    Update cursor position when textfield text changes.
    Bug: 789190
    Change-Id: I1f1b2546d126ad35e27bd977e17b295477e10938

--- ui/views/controls/textfield/textfield.cc
+++ ui/views/controls/textfield/textfield.cc
@@ -342,6 +342,7 @@ void Textfield::SetTextInputFlags(int flags) {
 void Textfield::SetText(const base::string16& new_text) {
   model_->SetText(new_text);
   OnCaretBoundsChanged();
+  UpdateAfterChange(true /* text_changed */, true /* cursor_changed */);
   SchedulePaint();
   NotifyAccessibilityEvent(ui::AX_EVENT_VALUE_CHANGED, true);
 }
