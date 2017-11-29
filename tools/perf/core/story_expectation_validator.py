c247554b9ca80375b26e5888b894e10720217cef
rnephew <rnephew@chromium.org>
2017-11-29 14:00:25 -0800

    [Telemetry] Add skeleton code for converting code based expectations to flat files.
    This is to support the upcoming conversion to SoM 1-click disabling. It currently
    outputs in this format:
    
    ...
    \# Benchmark: start_with_url.warm.startup_pages
    crbug.com/667470 [ ['All Platforms'] ] start_with_url.warm.startup_pages/http://kapook.com [ Skip ]
    
    \# Benchmark: kraken
    crbug.com/624411 [ ['Android Svelte'] ] kraken/http://krakenbenchmark.mozilla.org/kraken-1.1/driver.html [ Skip ]
    
    \# Benchmark: system_health.memory_desktop
    crbug.com/728576 [ ['Mac Platforms'] ] system_health.memory_desktop/browse:news:cnn [ Skip ]
    crbug.com/64939 [ ['All Platforms'] ] system_health.memory_desktop/play:media:pandora [ Skip ]
    crbug.com/676336 [ ['Win Platforms', 'Mac Platforms'] ] system_health.memory_desktop/browse:news:hackernews [ Skip ]
    crbug.com/649392 [ ['Win Platforms'] ] system_health.memory_desktop/play:media:soundcloud [ Skip ]
    ....
    
    The 'platforms' section is currently wrong, and should be changed closer to the
    actual conversion.
    
    Bug:
    Change-Id: I471aafe0ecd452129bc49a59f2070c84c709fa0b

--- tools/perf/core/story_expectation_validator.py
+++ tools/perf/core/story_expectation_validator.py
@@ -55,6 +55,12 @@ def validate_story_names(benchmarks):
     assert not failed_stories, 'Incorrect story names: %s' % str(failed_stories)
 
 
+# TODO(rnephew): This needs to be changed when we know the final version of SoM
+# 1-click disabling expectations.
+def _ConditionsToString(conditions):
+  return [str(a).replace(' ', '_') for a in conditions]
+
+
 def GetDisabledStories(benchmarks):
   # Creates a dictionary of the format:
   # {
@@ -73,12 +79,32 @@ def GetDisabledStories(benchmarks):
     disables[name] = {}
     expectations = benchmark().GetExpectations().AsDict()['stories']
     for story in expectations:
-      for conditions, reason in  expectations[story]:
+      for conditions, reason in expectations[story]:
         if not disables[name].get(story):
           disables[name][story] = []
-          conditions_str = [str(a) for a in conditions]
+          conditions_str = _ConditionsToString(conditions)
         disables[name][story].append((conditions_str, reason))
   return disables
+
+
+# TODO(rnephew): Delete this when SoM conversion is complete.
+def ConvertStories(benchmarks, file_path):
+  """Writes StoryExpectations to a text file in a format compatible with SoM."""
+  with open(file_path, 'w') as fp:
+    for benchmark in benchmarks:
+      name = benchmark.Name()
+      expectations = benchmark().GetExpectations().AsDict()['stories']
+      if expectations:
+        fp.write('# Benchmark: %s\n' % name)
+        for story in expectations:
+          for conditions, reason in expectations[story]:
+            c = ' '.join(_ConditionsToString(conditions))
+            e = ''
+            if reason.startswith('crbug') or reason.startswith('Bug('):
+              e += '%s ' % reason
+            e += '[ %s ] %s/%s [ Skip ]\n' % (c, name, story)
+            fp.write(e)
+        fp.write('\n')
 
 
 def main(args):
@@ -87,10 +113,18 @@ def main(args):
   parser.add_argument(
       '--list', action='store_true', default=False,
       help=('Prints list of disabled stories.'))
+  # TODO(rnephew): This option is for our conversion from StoryExpectations
+  # to SoM's 1-click test disabling. It should be removed when that is done.
+  parser.add_argument('--convert',
+                      help=('Converts from code based expectations to a flat'
+                            ' file. Pass filename to save as.'))
   options = parser.parse_args(args)
   benchmarks = benchmark_finders.GetAllBenchmarks()
 
-  if options.list:
+  # TODO(rnephew): Delete this after conversion.
+  if options.convert:
+    ConvertStories(benchmarks, options.convert)
+  elif options.list:
     stories = GetDisabledStories(benchmarks)
     print json.dumps(stories, sort_keys=True, indent=4, separators=(',', ': '))
   else:
