e83fed2315653ae87425ee640d577c0a51fa4229
Jinsuk Kim <jinsukkim@chromium.org>
2017-11-29 16:09:06 +0900

    GestureListenerManager
    Bug: 789000
    Change-Id: Icfbd3480314d261d90454f585d181e56f64aa8da

--- chrome/android/javatests/src/org/chromium/chrome/browser/ModalDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ModalDialogTest.java
@@ -34,6 +34,7 @@ import org.chromium.content.browser.test.util.TestCallbackHelperContainer;
 import org.chromium.content.browser.test.util.TestCallbackHelperContainer
         .OnEvaluateJavaScriptResultHelper;
 import org.chromium.content.browser.test.util.TouchCommon;
+import org.chromium.content_public.browser.GestureListenerManager;
 import org.chromium.content_public.browser.GestureStateListener;
 
 import java.util.concurrent.ExecutionException;
@@ -225,7 +226,7 @@ public class ModalDialogTest {
                 scriptEvent.waitUntilHasValue());
     }
 
-    private static class TapGestureStateListener extends GestureStateListener {
+    private static class TapGestureStateListener implements GestureStateListener {
         private CallbackHelper mCallbackHelper = new CallbackHelper();
 
         public int getCallCount() {
@@ -242,14 +243,20 @@ public class ModalDialogTest {
         }
     }
 
+    private GestureListenerManager getGestureListenerManager() {
+        return mActivityTestRule.getActivity()
+                .getCurrentContentViewCore()
+                .getWebContents()
+                .getGestureListenerManager();
+    }
+
     /**
      * Taps on a view and waits for a callback.
      */
     private void tapViewAndWait() throws InterruptedException, TimeoutException {
         final TapGestureStateListener tapGestureStateListener = new TapGestureStateListener();
         int callCount = tapGestureStateListener.getCallCount();
-        mActivityTestRule.getActivity().getCurrentContentViewCore().addGestureStateListener(
-                tapGestureStateListener);
+        getGestureListenerManager().addListener(tapGestureStateListener);
 
         TouchCommon.singleClickView(mActivityTestRule.getActivity().getActivityTab().getView());
         tapGestureStateListener.waitForTap(callCount);
