d4bb4550fa2824323cca5ac8ebc0f9a7847a6e59
Benjamin Pastene <bpastene@chromium.org>
2017-11-29 12:09:21 -0800

    android: Retry test listing on devices when nothing is returned.
    Bug: 726880
    Change-Id: If4d630ff8dc921fe59a306c4b7d1d2844bbab742

--- build/android/pylib/local/device/local_device_gtest_run.py
+++ build/android/pylib/local/device/local_device_gtest_run.py
@@ -381,17 +381,26 @@ class LocalDeviceGtestRun(local_device_test_run.LocalDeviceTestRun):
         on_failure=self._env.BlacklistDevice)
     def list_tests(dev):
       timeout = 30
+      retries = 1
       if self._test_instance.wait_for_java_debugger:
         timeout = None
-      raw_test_list = crash_handler.RetryOnSystemCrash(
-          lambda d: self._delegate.Run(
-              None, d, flags='--gtest_list_tests', timeout=timeout),
-          device=dev)
-      tests = gtest_test_instance.ParseGTestListTests(raw_test_list)
-      if not tests:
-        logging.info('No tests found. Output:')
-        for l in raw_test_list:
-          logging.info('  %s', l)
+      for i in range(0, retries+1):
+        raw_test_list = crash_handler.RetryOnSystemCrash(
+            lambda d: self._delegate.Run(
+                None, d, flags='--gtest_list_tests', timeout=timeout),
+            device=dev)
+        tests = gtest_test_instance.ParseGTestListTests(raw_test_list)
+        if not tests:
+          logging.info('No tests found. Output:')
+          for l in raw_test_list:
+            logging.info('  %s', l)
+          logging.info('Logcat:')
+          logging.info(dev.RunShellCommand(
+              'logcat -d ; logcat -c', large_output=True, raw_output=True))
+          if i < retries:
+            logging.info('Retrying...')
+        else:
+          break
       return tests
 
     # Query all devices in case one fails.
