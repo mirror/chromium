76f3b0b49b9d8ea218865d3926f8cfefedea22ff
Lily Houghton <lilyhoughton@chromium.org>
2017-11-29 23:34:03 -0500

    [Cronet] Populate cronet metrics object with data from net/.
    Currently this only creates the an appropriater delegate in net/, calling it from there.
    There still needs to be created code in cronet which registers and appropriate delegate,
    and then populates the metrics object that will finally be given to the client.
    
    Bug:702796
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I33a8ce1f489a7bfb74b4c9561a57b7b5a71013e0

--- components/cronet/ios/cronet_metrics.h
+++ components/cronet/ios/cronet_metrics.h
@@ -8,9 +8,21 @@
 #import <Foundation/Foundation.h>
 
 #include "components/grpc_support/include/bidirectional_stream_c.h"
+#import "ios/net/crn_http_protocol_handler.h"
+#include "net/http/http_network_session.h"
+
+// These are internal versions of NSURLSessionTaskTransactionMetrics and
+// NSURLSessionTaskMetrics, defined primarily so that Cronet can
+// initialize them and set their properties (the iOS classes are readonly).
+
+// The correspondences are
+//   CronetTransactionMetrics -> NSURLSessionTaskTransactionMetrics
+//   CronetMetrics -> NSURLSessionTaskMetrics
 
 FOUNDATION_EXPORT GRPC_SUPPORT_EXPORT NS_AVAILABLE_IOS(10.0)
-@interface CronetMetrics : NSURLSessionTaskTransactionMetrics
+@interface CronetTransactionMetrics : NSURLSessionTaskTransactionMetrics
+
+// All of the below redefined as readwrite.
 
 @property(copy, readwrite) NSURLRequest* request;
 @property(copy, readwrite) NSURLResponse* response;
@@ -32,6 +44,36 @@ FOUNDATION_EXPORT GRPC_SUPPORT_EXPORT NS_AVAILABLE_IOS(10.0)
 @property(assign, readwrite, getter=isReusedConnection) BOOL reusedConnection;
 @property(assign, readwrite)
     NSURLSessionTaskMetricsResourceFetchType resourceFetchType;
+
+@end
+
+NS_AVAILABLE_IOS(10.0) @interface CronetMetrics : NSURLSessionTaskMetrics
+// Redefined as readwrite.
+@property(copy, readwrite)
+    NSArray<NSURLSessionTaskTransactionMetrics*>* transactionMetrics;
+@end
+
+class CronetMetricsDelegate : public net::MetricsDelegate {
+ public:
+  CronetMetricsDelegate() {}
+  void OnStopNetRequest(std::unique_ptr<net::Metrics> metrics) override;
+
+  // Returns the metrics collected for a specific task (removing that task's
+  // entry from the map in the process).
+  // It is called exactly once by the swizzled delegate proxy (see below),
+  // uses it to retrieve metrics data collected by net/ and pass them on to the
+  // client.
+  // If there is no metrics data for the pass task, this returns nullptr.
+  static std::unique_ptr<net::Metrics> MetricsForTask(NSURLSessionTask* task);
+
+ private:
+  static std::map<NSURLSessionTask*, std::unique_ptr<net::Metrics>>
+      task_metrics_map_ NS_AVAILABLE_IOS(10.0);
+};
+
+@interface NSURLSession (Cronet)
+
++ (void)swizzleSessionWithConfiguration;
 
 @end
 
--- ios/net/crn_http_protocol_handler.h
+++ ios/net/crn_http_protocol_handler.h
@@ -7,6 +7,9 @@
 
 #import <Foundation/Foundation.h>
 
+#include "net/base/load_timing_info.h"
+#include "net/http/http_response_info.h"
+
 namespace net {
 class URLRequestContextGetter;
 
@@ -31,12 +34,34 @@ class HTTPProtocolHandlerDelegate {
   virtual URLRequestContextGetter* GetDefaultURLRequestContext() = 0;
 };
 
+// Simple struct collecting all of the metrics data that is passed through to
+// the MetricsDelegate callback.
+struct Metrics {
+  NSURLSessionTask* task;
+  LoadTimingInfo load_timing_info;
+  HttpResponseInfo response_info;
+};
+
+// Delegate class which supplies a metrics callback that is invoked when a net
+// request is stopped.
+class MetricsDelegate {
+ public:
+  // Set the global instance of the MetricsDelegate.
+  static void SetInstance(MetricsDelegate* delegate);
+
+  virtual ~MetricsDelegate() = default;
+
+  // This is invoked once the request is finally stopped, with metrics data
+  // collect by net passed in as an argument.
+  virtual void OnStopNetRequest(std::unique_ptr<net::Metrics> metrics) = 0;
+};
+
 }  // namespace net
 
 // Custom NSURLProtocol handling HTTP and HTTPS requests.
-// The HttpProtocolHandler is registered as a NSURLProtocol in the iOS system.
-// This protocol is called for each NSURLRequest. This allows handling the
-// requests issued by UIWebView using our own network stack.
+// The HttpProtocolHandler is registered as a NSURLProtocol in the iOS
+// system. This protocol is called for each NSURLRequest. This allows
+// handling the requests issued by UIWebView using our own network stack.
 @interface CRNHTTPProtocolHandler : NSURLProtocol
 @end
 
