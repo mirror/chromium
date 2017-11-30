181fe2eebc5b0ddf89fe394ef5aafbf30e410956
EhsanK <ekaramad@chromium.org>
2017-11-30 11:22:44 -0500

    [refactor] Move Some Editing Related Methods from WebWidget to WebLocalFrame
    This CL moves some editing methods from WebWidget to WebLocalFrame. The
    immediate benefit of this move is deduping the code in between
    WebViewImpl and WebFrameWidgetImpl. Furthermore, most other editing
    methods are already defined per frame. The logic inside the methods moved
    in this CL also act on the focused frame inside a widget.
    
    Bug: 629721
    Change-Id: Iad9c291268aeab65965b016cf50b8863e3a0729f

--- third_party/WebKit/public/web/WebFrameWidget.h
+++ third_party/WebKit/public/web/WebFrameWidget.h
@@ -137,6 +137,11 @@ class WebFrameWidget : public WebWidget {
   // by the WebFrameWidget.
   virtual void TransferActiveWheelFlingAnimation(
       const WebActiveWheelFlingParameters&) = 0;
+
+  // Returns the currently focused WebLocalFrame (if any) inside this
+  // WebFrameWidget. That is a WebLocalFrame which is focused and belongs to the
+  // same frame tree as LocalRoot().
+  virtual WebLocalFrame* FocusedWebLocalFrameInWidget() const = 0;
 };
 
 }  // namespace blink
