0e8ba04950b5b26fc88d62df1232a1759aaea3ae
Ryan Landay <rlanday@chromium.org>
2017-11-29 17:09:40 -0800

    Improve how JavaScript event handlers interact with Android IMEs
    Currently we have a problem with the Android IME commands commitText() and
    setComposingText(). These commands allow an IME to both input text and move the
    cursor/selection in a single operation. Currently, if a JavaScript event handler
    changes the text or moves the cursor in response to an edit, in many cases, the
    selection will be clobbered by us attempting to do the IME's requested cursor
    update, even if the IME just requested the default behavior of "leave the cursor
    at the end of the text."
    
    This CL improves the behavior in these cases by not attempting to move the
    selection after running the JavaScript input handler in the case where the IME
    requested the default behavior of leaving the cursor after the text. This CL
    does not address the case where both JavaScript and the IME are trying to move
    the cursor, since it's not clear how we should handle that case in general, and
    we haven't seen examples of it in practice.
    
    Bug: 737395
    Change-Id: I6ec5c54b09a81b015324670698932fb6c59e0e4e

--- third_party/WebKit/Source/core/editing/ime/InputMethodController.cpp
+++ third_party/WebKit/Source/core/editing/ime/InputMethodController.cpp
@@ -572,6 +572,12 @@ bool InputMethodController::ReplaceCompositionAndMoveCaret(
 
   AddImeTextSpans(ime_text_spans, root_editable_element, text_start);
 
+  // If the IME wants to leave the selection at the end of the newly-inserted
+  // text, the selection is already in the correct place (and in fact, may have
+  // been moved by JavaScript, in which case we don't want to move it again).
+  if (relative_caret_position == 0)
+    return true;
+
   int absolute_caret_position = ComputeAbsoluteCaretPosition(
       text_start, text.length(), relative_caret_position);
   return MoveCaret(absolute_caret_position);
@@ -604,6 +610,12 @@ bool InputMethodController::InsertTextAndMoveCaret(
   if (root_editable_element) {
     AddImeTextSpans(ime_text_spans, root_editable_element, text_start);
   }
+
+  // If the IME wants to leave the selection at the end of the newly-inserted
+  // text, the selection is already in the correct place (and in fact, may have
+  // been moved by JavaScript, in which case we don't want to move it again).
+  if (relative_caret_position == 0)
+    return true;
 
   int absolute_caret_position = ComputeAbsoluteCaretPosition(
       text_start, text.length(), relative_caret_position);
@@ -759,8 +771,27 @@ void InputMethodController::SetComposition(
   // needs to be audited. see http://crbug.com/590369 for more details.
   GetDocument().UpdateStyleAndLayoutIgnorePendingStylesheets();
 
-  // We shouldn't close typing in the middle of setComposition.
-  SetEditableSelectionOffsets(selected_range, TypingContinuation::kContinue);
+  if (selection_start == static_cast<int>(text.length()) &&
+      selection_end == static_cast<int>(text.length())) {
+    // The IME requested to leave the selection at the end of the
+    // newly-inserted text. In this case, we don't want to move the selection
+    // since JavaScript may have moved it, or changed the text (which
+    // invalidates the IME selection offsets). However, we called
+    // InsertTextDuringCompositionWithEvents() with the kSelectInsertedText
+    // flag, so we still have to deselect the composition in this case.
+
+    // TODO(editing-dev): find a way to replace the composition without
+    // selecting the new text so we can handle the case where JavaScript sets a
+    // range selection.
+    const Position extent =
+        GetFrame().Selection().ComputeVisibleSelectionInDOMTree().Extent();
+    GetFrame().Selection().SetSelection(
+        SelectionInDOMTree::Builder().Collapse(extent).Build(),
+        SetSelectionOptions::Builder().SetShouldCloseTyping(false).Build());
+  } else {
+    // We shouldn't close typing in the middle of setComposition.
+    SetEditableSelectionOffsets(selected_range, TypingContinuation::kContinue);
+  }
 
   // Even though we would've returned already if SetComposition() were called
   // with an empty string, the composition range could still be empty right now
