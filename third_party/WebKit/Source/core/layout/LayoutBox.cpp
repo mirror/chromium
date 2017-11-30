b8c4d337aa4e1394eddbf5535fa52bdd1c61bf8a
zhuoyu.qian <zhuoyu.qian@samsung.com>
2017-11-30 16:41:57 +0800

    Change ComputedStyle enums to enum classes and rename enumerators
    This patch makes EFillAttachment, EFillBox, EFillRepeat and EFillLayerType
    to be enum class for better type safety.
    
    Bug: 684966
    
    Signed-off-by: zhuoyu.qian <zhuoyu.qian@samsung.com>
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I1738b905c71a566d7063a8467afbe920f4497509

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -913,7 +913,7 @@ FloatQuad LayoutBox::AbsoluteContentQuad(MapCoordinatesFlags flags) const {
 }
 
 LayoutRect LayoutBox::BackgroundRect(BackgroundRectType rect_type) const {
-  EFillBox background_box = kTextFillBox;
+  EFillBox background_box = EFillBox::kText;
   // Find the largest background rect of the given opaqueness.
   if (const FillLayer* current = &(Style()->BackgroundLayers())) {
     do {
@@ -927,8 +927,10 @@ LayoutRect LayoutBox::BackgroundRect(BackgroundRectType rect_type) const {
         bool layer_known_opaque = false;
         // Check if the image is opaque and fills the clip.
         if (const StyleImage* image = cur->GetImage()) {
-          if ((cur->RepeatX() == kRepeatFill || cur->RepeatX() == kRoundFill) &&
-              (cur->RepeatY() == kRepeatFill || cur->RepeatY() == kRoundFill) &&
+          if ((cur->RepeatX() == EFillRepeat::kRepeatFill ||
+               cur->RepeatX() == EFillRepeat::kRoundFill) &&
+              (cur->RepeatY() == EFillRepeat::kRepeatFill ||
+               cur->RepeatY() == EFillRepeat::kRoundFill) &&
               image->KnownToBeOpaque(GetDocument(), StyleRef())) {
             layer_known_opaque = true;
           }
@@ -947,28 +949,29 @@ LayoutRect LayoutBox::BackgroundRect(BackgroundRectType rect_type) const {
       }
       EFillBox current_clip = cur->Clip();
       // Restrict clip if attachment is local.
-      if (current_clip == kBorderFillBox &&
-          cur->Attachment() == kLocalBackgroundAttachment)
-        current_clip = kPaddingFillBox;
+      if (current_clip == EFillBox::kBorder &&
+          cur->Attachment() == EFillAttachment::kLocal)
+        current_clip = EFillBox::kPadding;
 
       // If we're asking for the clip rect, a content-box clipped fill layer can
       // be scrolled into the padding box of the overflow container.
-      if (rect_type == kBackgroundClipRect && current_clip == kContentFillBox &&
-          cur->Attachment() == kLocalBackgroundAttachment) {
-        current_clip = kPaddingFillBox;
+      if (rect_type == kBackgroundClipRect &&
+          current_clip == EFillBox::kContent &&
+          cur->Attachment() == EFillAttachment::kLocal) {
+        current_clip = EFillBox::kPadding;
       }
 
       background_box = EnclosingFillBox(background_box, current_clip);
     } while (current);
   }
   switch (background_box) {
-    case kBorderFillBox:
+    case EFillBox::kBorder:
       return BorderBoxRect();
       break;
-    case kPaddingFillBox:
+    case EFillBox::kPadding:
       return PaddingBoxRect();
       break;
-    case kContentFillBox:
+    case EFillBox::kContent:
       return ContentBoxRect();
       break;
     default:
@@ -5700,7 +5703,8 @@ bool LayoutBox::MustInvalidateFillLayersPaintOnHeightChange(
   if (!img || !img->CanRender())
     return false;
 
-  if (layer.RepeatY() != kRepeatFill && layer.RepeatY() != kNoRepeatFill)
+  if (layer.RepeatY() != EFillRepeat::kRepeatFill &&
+      layer.RepeatY() != EFillRepeat::kNoRepeatFill)
     return true;
 
   // TODO(alancutter): Make this work correctly for calc lengths.
@@ -5740,7 +5744,8 @@ bool LayoutBox::MustInvalidateFillLayersPaintOnWidthChange(
   if (!img || !img->CanRender())
     return false;
 
-  if (layer.RepeatX() != kRepeatFill && layer.RepeatX() != kNoRepeatFill)
+  if (layer.RepeatX() != EFillRepeat::kRepeatFill &&
+      layer.RepeatX() != EFillRepeat::kNoRepeatFill)
     return true;
 
   // TODO(alancutter): Make this work correctly for calc lengths.
