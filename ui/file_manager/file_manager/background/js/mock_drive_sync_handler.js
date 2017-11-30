fe73282132ddfa31a58a06ecb2f0b9c4670ca6b3
mariannet <mariannet@google.com>
2017-12-01 02:58:01 +0900

    Cloud import: enable retry upon failure with the affected items.
    When backing up things via cloud import, they first get snapshotted locally
    before being synced to drive. If there is not enough local space for the
    snapshot, backup for the affected file fails. However, sync to drive is the
    bottleneck to the whole operation, so backing up a sum of files whose size
    exceeds the storage available locally might have a couple of them failing. This
    shouldn't affect the user, though - so wait until drive sends a sync complete
    signal, then retry backing up the failed items, as long as at least one file is
    backed up successfully and at least one of them fails.
    
    Cloud import / Infini backup: Enable retry upon failure with the affected items.
    
    Bug: 354574
    Test: manual + ./out/Release/browser_tests --gtest_filter=*MediaImportHandler*
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Id29217980f7d42e7137fdb00fa7f508e1ab0c85d

---
+++ ui/file_manager/file_manager/background/js/mock_drive_sync_handler.js
@@ -0,0 +1,18 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+/**
+ * Mock of DriveSyncHandler.
+ * @constructor
+ * @struct
+ */
+function MockDriveSyncHandler() {}
+
+MockDriveSyncHandler.prototype = {
+  __proto__: cr.EventTarget.prototype,
+  get syncing() {
+    return this.syncing_;
+  },
+};
