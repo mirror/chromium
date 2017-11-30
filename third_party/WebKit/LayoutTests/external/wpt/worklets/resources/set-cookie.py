a9b54605299d4eaa4c76e7a3b5483d816b45e3b9
Hiroki Nakagawa <nhiroki@chromium.org>
2017-11-30 15:29:50 +0900

    Worklet: Credentials
    Bug:
    Change-Id: I9aeb11fca23dec3fd3057a765fa60eb6e59d8258

--- third_party/WebKit/LayoutTests/external/wpt/worklets/resources/set-cookie.py
+++ third_party/WebKit/LayoutTests/external/wpt/worklets/resources/set-cookie.py
@@ -1,6 +1,7 @@
 def main(request, response):
     name = request.GET.first("name")
-    source_origin = request.headers.get("origin", None);
-    response.headers.set("Set-Cookie", name + "=value")
-    response.headers.set("Access-Control-Allow-Origin", source_origin)
+    value = request.GET.first("value")
+
+    response.headers.set("Set-Cookie", name + "=" + value)
+    response.headers.set("Access-Control-Allow-Origin", "*")
     response.headers.set("Access-Control-Allow-Credentials", "true")
