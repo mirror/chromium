b31a7ffac72448ab98112be5e4d970544bebaeaf
Leonard Grey <lgrey@chromium.org>
2017-11-30 13:36:32 -0500

    [Mac] Forward "open URL" AppleEvents to running Chromium instance.
    In macOS 10.13, if:
    - Chromium is the default browser
    - Chromium has updated
    - The user has *not* relaunched
    
    and the user tries to open a link from an external program, instead of
    opening the link in the running instance of Chromium, LaunchServices
    tries to open a second instance of Chromium.
    
    Currently, this causes the running instance to open a blank new window.
    
    This change, when detecting that a running instance exists, checks
    for any queued open URL events and forwards them to the running instance.
    
    Bug: 777863
    Change-Id: I9532b1f8bf4c6624992cc4f18ec41857b89d94d5

---
+++ chrome/browser/process_singleton_mac.mm
@@ -0,0 +1,60 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/process_singleton.h"
+
+#include <Carbon/Carbon.h>
+#include <CoreServices/CoreServices.h>
+
+#include "base/mac/scoped_aedesc.h"
+
+namespace {
+
+// Forwards |theAppleEvent| to an already-running Chromium process.
+OSErr HandleGURLEvent(const AppleEvent* theAppleEvent,
+                      AppleEvent* reply,
+                      SRefCon handlerRefcon) {
+  // Forwarding the message without |kAENoReply| hangs us, so assume that a
+  // reply is unnecessary as an invariant.
+  DCHECK_EQ(reply->descriptorType, typeNull);
+
+  pid_t forwarding_pid = *(reinterpret_cast<pid_t*>(handlerRefcon));
+  base::mac::ScopedAEDesc<AEAddressDesc> address;
+  if (AEGetAttributeDesc(theAppleEvent, keyAddressAttr, typeWildCard,
+                         address.OutPointer()) == noErr) {
+    base::mac::ScopedAEDesc<> other_process_pid;
+    // Create an address descriptor for the running process.
+    AECreateDesc(typeKernelProcessID, &forwarding_pid, sizeof(forwarding_pid),
+                 other_process_pid.OutPointer());
+
+    // Change the event's address to the running process.
+    AppleEvent* event = const_cast<AppleEvent*>(theAppleEvent);
+    if (AEPutAttributeDesc(event, keyAddressAttr, other_process_pid) == noErr) {
+      // Send the message back out.
+      AESendMessage(event, reply, kAENoReply, kNoTimeOut);
+      return noErr;
+    }
+  }
+  return noErr;
+}
+}
+
+bool ProcessSingleton::ForwardOpenURLEvent(pid_t forwarding_pid) {
+  if (AEInstallEventHandler(kInternetEventClass, kAEGetURL,
+                            NewAEEventHandlerUPP(HandleGURLEvent),
+                            &forwarding_pid, false) != noErr) {
+    return false;
+  }
+  const EventTypeSpec spec = {kEventClassAppleEvent, kEventAppleEvent};
+  EventRef eventRef;
+  if (ReceiveNextEvent(1, &spec, kEventDurationMillisecond, true, &eventRef) ==
+      noErr) {
+    OSStatus processed = AEProcessEvent(eventRef);
+    ReleaseEvent(eventRef);
+    if (processed == noErr) {
+      return true;
+    }
+  }
+  return false;
+}
