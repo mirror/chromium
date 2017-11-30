7c7e2ea1c4df096334bf15c727f9ce30a8247629
Daniel Cheng <dcheng@chromium.org>
2017-11-30 15:14:34 -0800

    Forward resource timing info for cross-process iframe loads.
    content::ResourceTimingInfo and blink::WebResourceTimingInfo
    have been added to plumb this data through, since the Mojo
    glue for forwarding messages to frame proxies doesn't exist
    yet.
    
    PerformanceResourceTiming's constructor has been updated to
    set its internal fields based on blink::WebResourceTimingInfo.
    For now, the internal structure of PerformanceResourceTiming
    is retained, since PerformanceNavigationTiming inherits this
    class, but overrides certain methods in a way that makes it
    hard to share the logic.
    
    Followups will change PerformanceResourceTiming to read the
    fields directly out of blink::WebResourceTimingInfo, as well
    as remove the need to plumb around the policy bits for
    exposing timing and redirect information cross-origin.
    
    PerformanceServerTiming has been refactored to separate
    parsing of the server header from creation of the C++ DOM
    objects. In addition, it's been cleaned up to no longer
    require plumbing across access policy bit.
    
    Bug: 608780
    Change-Id: I6c9eba184fd0b1f8ebbc41ba915dea9414cce3b5

--- third_party/WebKit/Source/core/html/HTMLFrameOwnerElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLFrameOwnerElement.cpp
@@ -36,6 +36,8 @@
 #include "core/loader/FrameLoader.h"
 #include "core/page/Page.h"
 #include "core/plugins/PluginView.h"
+#include "core/timing/DOMWindowPerformance.h"
+#include "core/timing/Performance.h"
 #include "platform/heap/HeapAllocator.h"
 #include "platform/weborigin/SecurityOrigin.h"
 #include "public/platform/modules/fetch/fetch_api_request.mojom-shared.h"
@@ -78,8 +80,7 @@ HTMLFrameOwnerElement::HTMLFrameOwnerElement(const QualifiedName& tag_name,
     : HTMLElement(tag_name, document),
       content_frame_(nullptr),
       embedded_content_view_(nullptr),
-      sandbox_flags_(kSandboxNone),
-      did_load_non_empty_document_(false) {}
+      sandbox_flags_(kSandboxNone) {}
 
 LayoutEmbeddedContent* HTMLFrameOwnerElement::GetLayoutEmbeddedContent() const {
   // HTMLObjectElement and HTMLEmbedElement may return arbitrary layoutObjects
@@ -201,6 +202,13 @@ void HTMLFrameOwnerElement::FrameOwnerPropertiesChanged() {
 
 void HTMLFrameOwnerElement::DispatchLoad() {
   DispatchScopedEvent(Event::Create(EventTypeNames::load));
+}
+
+void HTMLFrameOwnerElement::AddResourceTiming(const ResourceTimingInfo& info) {
+  // Resource timing info should only be reported if the subframe is attached.
+  DCHECK(ContentFrame());
+  DOMWindowPerformance::performance(*GetDocument().domWindow())
+      ->GenerateAndAddResourceTiming(info);
 }
 
 const ParsedFeaturePolicy& HTMLFrameOwnerElement::ContainerPolicy() const {
