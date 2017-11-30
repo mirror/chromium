f151558971814ce4bc23e94836d3ed64e6e4e18f
Tom Sepez <tsepez@chromium.org>
2017-11-30 14:59:03 -0800

    Restrict command processed by syscall_broker process.
    As the syscall_broker process becomes more capable, it is important
    not to expose all the new capabilities to old clients which do no
    require them.
    
    Rename broker_common.{cc,h} to broker_command.{cc,h} since all the
    contents are related to commands issued to the broker. Consolidate
    client/host side checks in this new file.
    
    Tidy, brace initialize some vectors for readability.
    
    Change-Id: I1ca23543f54e3eb5445bd476c24cf4a1bed06c37

--- content/network/network_sandbox_hook_linux.cc
+++ content/network/network_sandbox_hook_linux.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "content/network/network_sandbox_hook_linux.h"
+#include "sandbox/linux/syscall_broker/broker_command.h"
 
 #include "base/rand_util.h"
 #include "base/sys_info.h"
@@ -14,12 +15,18 @@ namespace content {
 bool NetworkPreSandboxHook(service_manager::BPFBasePolicy* policy,
                            service_manager::SandboxLinux::Options options) {
   // TODO(tsepez): FIX THIS.
-  std::vector<BrokerFilePermission> file_permissions;
-  file_permissions.push_back(
-      BrokerFilePermission::ReadWriteCreateRecursive("/"));
+  std::vector<BrokerFilePermission> file_permissions = {
+      BrokerFilePermission::ReadWriteCreateRecursive("/")};
+
+  constexpr uint32_t allowed_command_mask =
+      sandbox::syscall_broker::kBrokerCommandAccessMask |
+      sandbox::syscall_broker::kBrokerCommandOpenMask |
+      sandbox::syscall_broker::kBrokerCommandReadlinkMask |
+      sandbox::syscall_broker::kBrokerCommandRenameMask |
+      sandbox::syscall_broker::kBrokerCommandStatMask;
 
   service_manager::SandboxLinux::GetInstance()->StartBrokerProcess(
-      policy, std::move(file_permissions),
+      policy, allowed_command_mask, std::move(file_permissions),
       service_manager::SandboxLinux::PreSandboxHook(), options);
 
   return true;
