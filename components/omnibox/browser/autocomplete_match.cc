949f1d7947793533636a71298273c3627d865282
Kevin Bailey <krb@chromium.org>
2017-11-30 14:10:55 -0800

    [omnibox] Handle URL ellision a little more consistently
    When the option hide-suggestion-url-scheme is enabled, Omnibox
    suggestions exclude the scheme in some cases where they would
    be inconsistent with other suggestions. This change preserves
    schemes when the user specified one.
    
    This change passes whether the input has a scheme to
    GetFormatTypes(). Certainly, we could have done the logic
    outside that function, but this lets us change the policy
    in one place.
    
    Added some minor test cases too.
    
    Bug: 761505
    Change-Id: I189ececbfad3baf8214e3f289e0c994d3cee9d55

--- components/omnibox/browser/autocomplete_match.cc
+++ components/omnibox/browser/autocomplete_match.cc
@@ -555,11 +555,12 @@ void AutocompleteMatch::GetMatchComponents(
 
 // static
 url_formatter::FormatUrlTypes AutocompleteMatch::GetFormatTypes(
+    bool has_scheme,
     bool preserve_scheme,
     bool preserve_subdomain,
     bool preserve_after_host) {
   auto format_types = url_formatter::kFormatUrlOmitDefaults;
-  if (preserve_scheme) {
+  if (has_scheme || preserve_scheme) {
     format_types &= ~url_formatter::kFormatUrlOmitHTTP;
   } else if (base::FeatureList::IsEnabled(
                  omnibox::kUIExperimentHideSuggestionUrlScheme)) {
