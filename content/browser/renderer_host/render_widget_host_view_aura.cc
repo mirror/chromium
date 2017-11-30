699cff873e25f929cd798f8743eb13f5fa8d76cb
Francois Doray <fdoray@chromium.org>
2017-11-30 10:50:45 -0500

    Add content::RenderWidgetHostViewBase::GetRenderWidgetHostImpl().
    This method avoids casts from RenderWidgetHost to
    RenderWidgetHostImpl.
    
    Bug: 788827
    Change-Id: I9618a61d9582e104b3041698d8368a8b14525d7f

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -540,10 +540,6 @@ void RenderWidgetHostViewAura::InitAsFullscreen(
   device_scale_factor_ = ui::GetScaleFactorForNativeView(window_);
 }
 
-RenderWidgetHost* RenderWidgetHostViewAura::GetRenderWidgetHost() const {
-  return host_;
-}
-
 void RenderWidgetHostViewAura::Show() {
   if (is_mus_browser_plugin_guest_)
     return;
@@ -2360,6 +2356,11 @@ void RenderWidgetHostViewAura::OnDidNavigateMainFrameToNewPage() {
   ui::GestureRecognizer::Get()->CancelActiveTouches(window_);
 }
 
+RenderWidgetHostImpl* RenderWidgetHostViewAura::GetRenderWidgetHostImpl()
+    const {
+  return host_;
+}
+
 viz::FrameSinkId RenderWidgetHostViewAura::GetFrameSinkId() {
   return frame_sink_id_;
 }
@@ -2391,8 +2392,7 @@ void RenderWidgetHostViewAura::OnUpdateTextInputStateCalled(
     GetInputMethod()->ShowImeIfNeeded();
   }
 
-  if (auto* render_widget_host =
-          RenderWidgetHostImpl::From(updated_view->GetRenderWidgetHost())) {
+  if (auto* render_widget_host = updated_view->GetRenderWidgetHostImpl()) {
     // Monitor the composition information if there is a focused editable node.
     render_widget_host->RequestCompositionUpdates(
         false /* immediate_request */,
