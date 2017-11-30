c3aaa57e3d96ddf47bc4f19525ee3883600cb536
Xi Han <hanxi@google.com>
2017-11-30 15:24:50 -0500

    Chrome sends notifications with Channel to WebAPKs which targets SDK 26 on Android O.
    In Chrome:
    - WebAPK's channel is set under "Sites" notification group and
      "readable origin" as the channel, the same channel as Webapps.
    - If the device is Android o+, Chrome will set a channel to the
      notification.
    
    In WebAPK:
    - WebApkServiceImpl#notifyNotification() is deprecated. Chrome calls
      notifyNotificationWithChannel() instead.
    - If on Android O+, WebAPK will ensure the notification channel is
      created if its notifications have a channel id.
    
    To test this change, we need to make sure the following cases all work:
    - New WebAPK, new Chrome;
    - New WebAPK, old Chrome;
    - Old WebAPK, new Chrome.
    
    Bug:700228
    Change-Id: I176a8d45f021a33ea441837476b1ca09e973a48b

--- chrome/android/webapk/libs/runtime_library/src/org/chromium/webapk/lib/runtime_library/WebApkServiceImpl.java
+++ chrome/android/webapk/libs/runtime_library/src/org/chromium/webapk/lib/runtime_library/WebApkServiceImpl.java
@@ -4,14 +4,18 @@
 
 package org.chromium.webapk.lib.runtime_library;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Binder;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.os.RemoteException;
 import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
 
 /**
  * Implements services offered by the WebAPK to Chrome.
@@ -21,7 +25,13 @@ public class WebApkServiceImpl extends IWebApkApi.Stub {
     public static final String KEY_SMALL_ICON_ID = "small_icon_id";
     public static final String KEY_HOST_BROWSER_UID = "host_browser_uid";
 
-    private static final String TAG = "WebApkServiceImpl";
+    private static final String TAG = "cr_WebApkServiceImpl";
+
+    /**
+     * Keeps this value consistent with {@link
+     * org.chromium.webapk.shell_apk.WebApkServiceImplWrapper#DEFAULT_NOTIFICATION_CHANNEL_ID}.
+     */
+    private static final String LEGACY_CHANNEL_ID = "default_channel_id";
 
     private final Context mContext;
 
@@ -65,7 +75,9 @@ public class WebApkServiceImpl extends IWebApkApi.Stub {
 
     @Override
     public void notifyNotification(String platformTag, int platformID, Notification notification) {
-        getNotificationManager().notify(platformTag, platformID, notification);
+        Log.w(TAG,
+                "Should NOT reach WebApkServiceImpl#notifyNotification(String, int,"
+                        + " Notification).");
     }
 
     @Override
@@ -78,7 +90,44 @@ public class WebApkServiceImpl extends IWebApkApi.Stub {
         return NotificationManagerCompat.from(mContext).areNotificationsEnabled();
     }
 
+    @Override
+    public void notifyNotificationWithChannel(
+            String platformTag, int platformID, Notification notification, String channelName) {
+        NotificationManager notificationManager = getNotificationManager();
+        if (usingChannels(notification)) {
+            // crbug.com/700228. We used to create a default notification channel for WebAPKs which
+            // target to SDK 26 when Chrome doesn't send notifications with channel ID to WebAPKs.
+            // See {@link WebApkServiceImplWrapper#notifyNotification()} for details.
+            cleanUpLegacyChannel(notificationManager);
+            createNotificationChannel(notificationManager, notification, channelName);
+        }
+        notificationManager.notify(platformTag, platformID, notification);
+    }
+
     private NotificationManager getNotificationManager() {
         return (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    /** Returns whether a channel ID has been set for the given notification. */
+    @TargetApi(Build.VERSION_CODES.O)
+    private boolean usingChannels(Notification notification) {
+        return notification.getChannelId() != null;
+    }
+
+    /** Creates a notification channel on Android O+. */
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createNotificationChannel(NotificationManager notificationManager,
+            Notification notification, String channelName) {
+        // The channel id shouldn't be empty since Chrome always sets the channel id for a
+        // notification when WebAPK targets to SDK 26+.
+        NotificationChannel channel = new NotificationChannel(
+                notification.getChannelId(), channelName, NotificationManager.IMPORTANCE_DEFAULT);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+    /** Deletes the legacy notification channel. */
+    @TargetApi(Build.VERSION_CODES.O)
+    private void cleanUpLegacyChannel(NotificationManager notificationManager) {
+        notificationManager.deleteNotificationChannel(LEGACY_CHANNEL_ID);
     }
 }
