e2d9a05fe90d8b6c2db90ca9967a51f95d663fa4
Mitsuru Oshima <oshima@chromium.org>
2017-11-30 06:20:59 -0800

    Move unit tests for ClientControlledShellSurface into client_controlled_shell_surface_unittest.c
    BUG=773461
    TEST=None
    
    Change-Id: I26ff2a68f449daa3aa0e239d31e3f7965a5fdd1d

---
+++ components/exo/test/mock_pointer_delegate.cc
@@ -0,0 +1,14 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/exo/test/mock_pointer_delegate.h"
+
+namespace exo {
+namespace test {
+
+MockPointerDelegate::MockPointerDelegate() = default;
+MockPointerDelegate::~MockPointerDelegate() = default;
+
+}  // namespace test
+}  // namespace exo
