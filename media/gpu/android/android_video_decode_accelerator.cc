1f1b8417f2f14e724a2c558071f7da852f0a8d31
liberato@chromium.org <liberato@chromium.org>
2017-11-30 12:42:27 -0800

    Remove ServiceContextRef from MCVD.
    Previously, MCVD ran on a thread that was owned by the mojo service.
    If the service was destroyed, then the thread would be also.  This
    made it difficult for things to outlive MCVD (e.g., overlays).
    
    Now, it is sufficient to hold the task runner instead.  This CL
    removes the ServiceContextRef, in favor of the task runner.
    
    Bug:  789447
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I1f07382a3977685d6a92f6f13a92d5455602d6ec

--- media/gpu/android/android_video_decode_accelerator.cc
+++ media/gpu/android/android_video_decode_accelerator.cc
@@ -427,8 +427,8 @@ void AndroidVideoDecodeAccelerator::StartSurfaceChooser() {
                          config_.overlay_info.surface_id);
   } else if (config_.overlay_info.HasValidRoutingToken() &&
              overlay_factory_cb_) {
-    factory = base::Bind(overlay_factory_cb_, nullptr,
-                         *config_.overlay_info.routing_token);
+    factory =
+        base::Bind(overlay_factory_cb_, *config_.overlay_info.routing_token);
   }
 
   // Notify |surface_chooser_| that we've started.  This guarantees that we'll
@@ -1306,7 +1306,7 @@ void AndroidVideoDecodeAccelerator::SetOverlayInfo(
   if (surface_id != previous_info.surface_id ||
       routing_token != previous_info.routing_token) {
     if (routing_token && overlay_factory_cb_)
-      new_factory = base::Bind(overlay_factory_cb_, nullptr, *routing_token);
+      new_factory = base::Bind(overlay_factory_cb_, *routing_token);
     else if (surface_id != SurfaceManager::kNoSurfaceID)
       new_factory = base::Bind(&ContentVideoViewOverlay::Create, surface_id);
   }
