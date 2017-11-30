e77a79a1fce81505513e36a7fdc862d316e8a747
Aseem Garg <aseemgarg@chromium.org>
2017-11-30 13:29:31 -0800

    Remove disable tanks code from wasm benchmark
    R=bradnelson@chromium.org,nednguyen@google.com
    BUG=v8:5558
    
    Change-Id: Id116ffab4da581f70c16595aa4acb3ab002ee7af

--- tools/perf/benchmarks/wasm.py
+++ tools/perf/benchmarks/wasm.py
@@ -60,9 +60,6 @@ class Wasm(perf_benchmark.PerfBenchmark):
     class StoryExpectations(story.expectations.StoryExpectations):
       def SetExpectations(self):
         self.DisableStory(
-            'WasmTanks', [story.expectations.ALL_ANDROID],
-            'Unity WebGL in not supported on mobile')
-        self.DisableStory(
             'WasmSpaceBuggy', [story.expectations.ANDROID_ONE,
                 story.expectations.ANDROID_WEBVIEW], 'crbug.com/788976')
     return StoryExpectations()
