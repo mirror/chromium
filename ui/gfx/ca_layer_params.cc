25a4f68da1283380856c221c0dac594ccf806915
Christopher Cameron <ccameron@chromium.org>
2017-11-29 22:09:12 -0800

    viz/mac: Prepare the ground for CoreAnimation plumbing
    The parameters for sending a CALayer tree from the GPU process to the
    browser process will have to be shuffled a bit for viz. Before doing
    this, clean up the existing path.
    
    Current relevant sequence of events:
      * gpu::ImageTransportSurfaceOverlayMac populates
        gpu::SwapBuffersCompleteParams in the GPU process.
          * Structure includes gfx::SwapResponse,
            gpu::TextureInUseResponses, and CALayer parameters.
      * gpu::CommandBuffer un-and-re-packs this structure as
        GpuCommandBufferMsg_SwapBuffersCompleted_Params to send via IPC.
      * gpu::CommandBufferProxyImpl::OnSwapBuffersCompleted then
        un-and-re-packs that structure as two separate arguments
          * gfx::SwapResponse
            * Is consumed on all platforms
          * gpu::GpuProcessHostedCALayerTreeParamsMac
            * Is consumed by GpuOutputSurfaceMac only, passed by pointer
            * Includes gpu::TextureInUseResponses as a member
      * These arguments ultimately resurface in a function named
        OnGpuSwapBuffersCompleted in viz::DisplayOutputSurface or
        content::GpuBrowserCompositorOutputSurface (or subclasses thereof).
    
    The reason for this patch is that, in viz, we will need to split up
    gpu::GpuProcessHostedCALayerTreeParamsMac, because different
    parts go to different places. In particular:
      * The actual CALayer-related parameters need to go up to the browser
      * The TextureInUseResponses parameter just goes to the GLRenderer
    Since we're doing surgery in the neighborhood, some of the
    un-and-re-packing can be done away with.
    
    The changes made are:
      * Add a new gfx::CALayerParams structure
          * In gfx because it will be passed into ui code on the browser
            side.
          * Not ifdef-ed out on non-macOS (though is largely depopulated)
            to avoid ifdef-spaghetti (an is_empty parameter will be used
            for early-out)
          * Keep the IOSurface as a mach port much longer, because viz
            will need to re-pass it to the browser side (via mojo).
      * Populate gpu::SwapBuffersCompleteParams with all three
        separately-destined components:
          * gfx::SwapResponse (as before)
          * gpu::TextureInUseResponses (as before)
              * This is not ifdef-ed out even though it is macOS-only
                avoid preprocessor-spaghetti and to help testing.
          * gpu::CALayerParams (new)
              * This was previously just inline
      * Move gpu::SwapBuffersCompleteParams from service into common
          * This is passed without any inspection or modification from
            gpu::ImageTransportSurfaceOverlayMac to *GpuOutputSurfaceMac,
            where its sub-parts finally diverge.
          * This lets us remove the un-and-re-pack of parameters.
      * Delete the redundant structures:
          * GpuCommandBufferMsg_SwapBuffersCompleted_Params
          * gpu::GpuProcessHostedCALayerTreeParamsMac
    
    Bug: 772576
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I8fd06898a4b1f8432b563c0bc63975787b05cb46

---
+++ ui/gfx/ca_layer_params.cc
@@ -0,0 +1,13 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/gfx/ca_layer_params.h"
+
+namespace gfx {
+
+CALayerParams::CALayerParams() = default;
+CALayerParams::CALayerParams(const CALayerParams& params) = default;
+CALayerParams::~CALayerParams() = default;
+
+}  // namespace gfx
