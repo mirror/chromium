f526f4b5447b33f44836d1c86fd7992f1ad5006d
Adrienne Walker <enne@chromium.org>
2017-11-30 14:34:22 -0800

    oop : Only adjust serialization alignment when needed
    Alignment code was showing up in paint op serialization perf, so this
    patch makes alignment explicit instead of implicit.  Callers that
    know that their writes are already aligned aren't forced into extra
    work.
    
    DrawRectOp serialization is 6.2x faster and deserialization is 2.2x faster.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I5678c0510c3200eff496c07a6fbb88e70054b27b

--- cc/paint/paint_op_reader.h
+++ cc/paint/paint_op_reader.h
@@ -73,6 +73,9 @@ class CC_PAINT_EXPORT PaintOpReader {
     *data = !!value;
   }
 
+  // Aligns the memory to the given alignment.
+  void AlignMemory(size_t alignment);
+
  private:
   template <typename T>
   void ReadSimple(T* val);
@@ -85,10 +88,6 @@ class CC_PAINT_EXPORT PaintOpReader {
             sk_sp<SkTextBlob>* blob);
 
   void SetInvalid();
-
-  // Attempts to align the memory to the given alignment. Returns false if there
-  // is unsufficient bytes remaining to do this padding.
-  bool AlignMemory(size_t alignment);
 
   const volatile char* memory_ = nullptr;
   size_t remaining_bytes_ = 0u;
