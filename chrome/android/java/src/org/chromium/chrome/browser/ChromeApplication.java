6438a870b828ced1e2a4f05d4632918338bb359d
agrieve <agrieve@chromium.org>
2017-11-30 15:20:39 +0000

    Revert "Move initCommandLine() to attachBaseContext"
    This reverts commit 125a47b2b9c30a0a233610c347b52499a3fc2edd.
    
    Reason for revert: Breaking non-rooted devices (see bugs)
    
    Original change's description:
    > Move initCommandLine() to attachBaseContext
    > 
    > This allows ContentProviders to not have to initialize the command line.
    > 
    > Change-Id: If8d5049a53f450b8111af5dc15f20a0ec1691e03
    > Reviewed-on: https://chromium-review.googlesource.com/761317
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Commit-Queue: agrieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#515626}
    
    TBR=yfriedman@chromium.org,agrieve@chromium.org
    Bug: 789275, 789245
    
    Change-Id: If45a34f03aa2577b61f28b663dfe40a43f37dc80

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeApplication.java
@@ -48,8 +48,6 @@ public class ChromeApplication extends ContentApplication {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         ContextUtils.initApplicationContext(this);
-        // Command-line is needed by content providers, so initialize nice and early.
-        initCommandLine();
         BuildHooksAndroid.initCustomResources(this);
         Boolean isIsolatedProcess = PureJavaExceptionReporter.detectIsIsolatedProcess();
         if (isIsolatedProcess != null && !isIsolatedProcess.booleanValue()) {
@@ -69,6 +67,7 @@ public class ChromeApplication extends ContentApplication {
     @Override
     public void onCreate() {
         UmaUtils.recordMainEntryPointTime();
+        initCommandLine();
         TraceEvent.maybeEnableEarlyTracing();
         TraceEvent.begin("ChromeApplication.onCreate");
 
