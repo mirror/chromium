9d3a1fec8a9126e38e3d73b9a20b415a8ba90b6b
Stephen McGruer <smcgruer@chromium.org>
2017-12-01 15:03:47 -0500

    Web Animations: use WTF::Optional for Keyframe::offset_
    Previously nullable values in the animations code were tracked by
    storing nulls as quiet_NaN() and using std::isnan as a null-detector.
    It is much more explicit to store such values using WTF::Optional, which
    forces code to consider whether or not the offset exists.
    
    Bug: 791086
    Change-Id: I4db0457b593d6be61dedb5c13a9daf8b671206b0

--- third_party/WebKit/Source/core/animation/CompositorAnimationsTest.cpp
+++ third_party/WebKit/Source/core/animation/CompositorAnimationsTest.cpp
@@ -171,7 +171,7 @@ class AnimationCompositorAnimationsTest : public RenderingTest {
 
   bool DuplicateSingleKeyframeAndTestIsCandidateOnResult(
       StringKeyframe* frame) {
-    EXPECT_EQ(frame->Offset(), 0);
+    EXPECT_EQ(frame->CheckedOffset(), 0);
     StringKeyframeVector frames;
     scoped_refptr<Keyframe> second = frame->CloneWithOffset(1);
 
@@ -264,7 +264,7 @@ class AnimationCompositorAnimationsTest : public RenderingTest {
       scoped_refptr<StringKeyframe> to,
       scoped_refptr<StringKeyframe> c = nullptr,
       scoped_refptr<StringKeyframe> d = nullptr) {
-    EXPECT_EQ(from->Offset(), 0);
+    EXPECT_EQ(from->CheckedOffset(), 0);
     StringKeyframeVector frames;
     frames.push_back(from);
     EXPECT_LE(from->Offset(), to->Offset());
@@ -276,9 +276,9 @@ class AnimationCompositorAnimationsTest : public RenderingTest {
     if (d) {
       frames.push_back(d);
       EXPECT_LE(c->Offset(), d->Offset());
-      EXPECT_EQ(d->Offset(), 1.0);
+      EXPECT_EQ(d->CheckedOffset(), 1.0);
     } else {
-      EXPECT_EQ(to->Offset(), 1.0);
+      EXPECT_EQ(to->CheckedOffset(), 1.0);
     }
     if (!HasFatalFailure()) {
       return StringKeyframeEffectModel::Create(frames);
