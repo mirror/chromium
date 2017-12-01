f056a57199e40f4979135e4a1f4a01dffd753c00
Wenzhao Zang <wzang@chromium.org>
2017-11-30 16:34:03 -0800

    wallpaper refactoring: Move RemoveUserWallpaper to WallpaperController
    All the temporary code have been explicitly marked as TODO. Those
    without the mark is the 'permanent' code.
    
    1) Replace RemoveUserWallpaper with a mojo call via
       |WallpaperControllerClient|.
    
    2) Add additional SetPathIds mojo call in |WallpaperControllerClient|
       interface.
    
    3) Add additional unit tests for |WallpaperController|.
    
    Bug: 779221
    Change-Id: Ife3bc79b4c353ed9d7e6e0332a9bf048cef3bbf6

--- chrome/browser/ui/ash/wallpaper_controller_client.h
+++ chrome/browser/ui/ash/wallpaper_controller_client.h
@@ -30,6 +30,17 @@ class WallpaperControllerClient : public ash::mojom::WallpaperControllerClient {
 
   static WallpaperControllerClient* Get();
 
+  // TODO(crbug.com/776464): Move this to anonymous namesapce.
+  // Returns true if wallpaper files id can be returned successfully.
+  bool CanGetWallpaperFilesId() const;
+
+  // Returns files identifier for the |account_id|.
+  wallpaper::WallpaperFilesId GetFilesId(const AccountId& account_id) const;
+
+  // TODO(crbug.com/776464): This should only be called internally.
+  // Retrieves paths of wallpaper directories and calls |SetFilePaths|.
+  void InitializeFilePaths() const;
+
   // Wrappers around the ash::mojom::WallpaperController interface.
   void SetCustomWallpaper(const AccountId& account_id,
                           const wallpaper::WallpaperFilesId& wallpaper_files_id,
