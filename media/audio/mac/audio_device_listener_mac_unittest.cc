2323c5c08d61a9162f8506dd9801b8f579ee1dc3
Guido Urdaneta <guidou@chromium.org>
2017-12-01 13:52:34 +0100

    Improve audio-device monitoring on Mac
    Before this CL, base::SystemMonitor on Mac only reported addition and
    removal of input devices.
    
    With this patch, the following events are reported:
    * Addition and removal of output-only devices.
    * Changes to the default input or output device.
    
    With this changes, it is safe to enable the SYSTEM_MONITOR caching
    policy for MediaDevicesManager.
    
    Bug: 788687, 648173
    Change-Id: I57549eb586716a3739d652c386f7740875521be8

--- media/audio/mac/audio_device_listener_mac_unittest.cc
+++ media/audio/mac/audio_device_listener_mac_unittest.cc
@@ -44,48 +44,81 @@ class AudioDeviceListenerMacTest : public testing::Test {
   void CreateDeviceListener() {
     // Force a post task using BindToCurrentLoop() to ensure device listener
     // internals are working correctly.
-    output_device_listener_.reset(new AudioDeviceListenerMac(BindToCurrentLoop(
+    device_listener_.reset(new AudioDeviceListenerMac(BindToCurrentLoop(
         base::Bind(&AudioDeviceListenerMacTest::OnDeviceChange,
                    base::Unretained(this)))));
   }
 
-  void DestroyDeviceListener() {
-    output_device_listener_.reset();
-  }
+  void DestroyDeviceListener() { device_listener_.reset(); }
 
-  bool ListenerIsValid() {
-    return !output_device_listener_->listener_cb_.is_null();
-  }
+  bool ListenerIsValid() { return !device_listener_->listener_cb_.is_null(); }
 
   // Simulate a device change where no output devices are available.
   bool SimulateDefaultOutputDeviceChange() {
     // Include multiple addresses to ensure only a single device change event
     // occurs.
     const AudioObjectPropertyAddress addresses[] = {
-      AudioDeviceListenerMac::kDeviceChangePropertyAddress,
-      { kAudioHardwarePropertyDevices,
-        kAudioObjectPropertyScopeGlobal,
-        kAudioObjectPropertyElementMaster }
-    };
+        AudioDeviceListenerMac::kDefaultOutputDeviceChangePropertyAddress,
+        {kAudioHardwarePropertyDevices, kAudioObjectPropertyScopeGlobal,
+         kAudioObjectPropertyElementMaster}};
 
-    return noErr == output_device_listener_->OnDefaultDeviceChanged(
-        kAudioObjectSystemObject, 1, addresses, output_device_listener_.get());
+    return noErr ==
+           device_listener_->OnDefaultOutputDeviceChanged(
+               kAudioObjectSystemObject, 1, addresses, device_listener_.get());
+  }
+
+  bool SimulateDefaultInputDeviceChange() {
+    // Include multiple addresses to ensure only a single device change event
+    // occurs.
+    const AudioObjectPropertyAddress addresses[] = {
+        AudioDeviceListenerMac::kDefaultInputDeviceChangePropertyAddress,
+        {kAudioHardwarePropertyDevices, kAudioObjectPropertyScopeGlobal,
+         kAudioObjectPropertyElementMaster}};
+
+    return noErr ==
+           device_listener_->OnDefaultInputDeviceChanged(
+               kAudioObjectSystemObject, 1, addresses, device_listener_.get());
+  }
+
+  bool SimulateDeviceAdditionRemoval() {
+    const AudioObjectPropertyAddress addresses[] = {
+        AudioDeviceListenerMac::kDevicesPropertyAddress,
+        {kAudioHardwarePropertyDevices, kAudioObjectPropertyScopeGlobal,
+         kAudioObjectPropertyElementMaster}};
+
+    return noErr ==
+           device_listener_->OnDevicesAddedOrRemoved(
+               kAudioObjectSystemObject, 1, addresses, device_listener_.get());
   }
 
   MOCK_METHOD0(OnDeviceChange, void());
 
  protected:
   base::MessageLoop message_loop_;
-  std::unique_ptr<AudioDeviceListenerMac> output_device_listener_;
+  std::unique_ptr<AudioDeviceListenerMac> device_listener_;
 
   DISALLOW_COPY_AND_ASSIGN(AudioDeviceListenerMacTest);
 };
 
-// Simulate a device change events and ensure we get the right callbacks.
-TEST_F(AudioDeviceListenerMacTest, OutputDeviceChange) {
+// Simulate a device change event and ensure we get the right callback.
+TEST_F(AudioDeviceListenerMacTest, DefaultOutputDeviceChange) {
   ASSERT_TRUE(ListenerIsValid());
   EXPECT_CALL(*this, OnDeviceChange()).Times(1);
   ASSERT_TRUE(SimulateDefaultOutputDeviceChange());
+  base::RunLoop().RunUntilIdle();
+}
+
+TEST_F(AudioDeviceListenerMacTest, DefaultInputDeviceChange) {
+  ASSERT_TRUE(ListenerIsValid());
+  EXPECT_CALL(*this, OnDeviceChange()).Times(1);
+  ASSERT_TRUE(SimulateDefaultInputDeviceChange());
+  base::RunLoop().RunUntilIdle();
+}
+
+TEST_F(AudioDeviceListenerMacTest, DevicesAddedOrRemoved) {
+  ASSERT_TRUE(ListenerIsValid());
+  EXPECT_CALL(*this, OnDeviceChange()).Times(1);
+  ASSERT_TRUE(SimulateDeviceAdditionRemoval());
   base::RunLoop().RunUntilIdle();
 }
 
