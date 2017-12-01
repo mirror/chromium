d878b782dbc117d5786fbc65d9d32453337648dc
Mohamad Ahmadi <mahmadi@chromium.org>
2017-12-01 16:43:04 -0500

    Make AutofillManagerTest and CreditCardSaveManagerTest use a central TestPersonalDataManager
    Bug: 778436
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I8ba2a695880a8594a145ecfa9724907253f5ad7f

--- ios/chrome/browser/payments/payment_request_unittest_base.mm
+++ ios/chrome/browser/payments/payment_request_unittest_base.mm
@@ -27,11 +27,11 @@ void PaymentRequestUnitTestBase::SetUp() {
                                      &ios::BuildFakeSigninManager);
   chrome_browser_state_ = test_cbs_builder.Build();
   web_state_.SetBrowserState(chrome_browser_state_.get());
-  personal_data_manager_.SetTestingPrefService(pref_service_.get());
+  personal_data_manager_.SetPrefService(pref_service_.get());
 }
 
 void PaymentRequestUnitTestBase::TearDown() {
-  personal_data_manager_.SetTestingPrefService(nullptr);
+  personal_data_manager_.SetPrefService(nullptr);
 }
 
 void PaymentRequestUnitTestBase::CreateTestPaymentRequest() {
@@ -43,14 +43,11 @@ void PaymentRequestUnitTestBase::CreateTestPaymentRequest() {
 
 void PaymentRequestUnitTestBase::AddAutofillProfile(
     autofill::AutofillProfile profile) {
-  profiles_.push_back(
-      base::MakeUnique<autofill::AutofillProfile>(std::move(profile)));
-  personal_data_manager_.AddTestingProfile(profiles_.back().get());
+  personal_data_manager_.AddProfile(profile);
 }
 
 void PaymentRequestUnitTestBase::AddCreditCard(autofill::CreditCard card) {
-  cards_.push_back(base::MakeUnique<autofill::CreditCard>(std::move(card)));
-  personal_data_manager_.AddTestingCreditCard(cards_.back().get());
+  personal_data_manager_.AddCreditCard(card);
 }
 
 SigninManager* PaymentRequestUnitTestBase::GetSigninManager() {
