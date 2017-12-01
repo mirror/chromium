0603b45754f0e5ff9e265a647abc55271e75d490
Luna Lu <loonybear@chromium.org>
2017-12-01 15:09:51 -0500

    Override ShouldObserveMimeType in UseCounterPageLoadMetricsObserver support SVG docs
    Blink side UseCounter counts SVG usage when containing document is an HTML doc.
    PageLoadMetrics restrict mime type to text/html or xhtml, which might filter out
    SVG document.
    Overriding mime type restriction supports usage tracking SVG docs in UseCounterPageLoadMetricsObserver.
    
    Bug: 786414
    Change-Id: I53e4afcae51166ce6f9cdde7c35d9a249dd94d2a

--- chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
@@ -1244,6 +1244,23 @@ IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest,
       static_cast<int32_t>(WebFeature::kPageVisits), 1);
 }
 
+// Test UseCounter Features observed for SVG pages.
+IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest,
+                       UseCounterObserveSVGImagePage) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+
+  auto waiter = CreatePageLoadMetricsWaiter();
+  waiter->AddPageExpectation(TimingField::FIRST_PAINT);
+  ui_test_utils::NavigateToURL(browser(), embedded_test_server()->GetURL(
+                                              "/page_load_metrics/circle.svg"));
+  waiter->Wait();
+  NavigateToUntrackedUrl();
+
+  histogram_tester_.ExpectBucketCount(
+      internal::kFeaturesHistogramName,
+      static_cast<int32_t>(WebFeature::kPageVisits), 1);
+}
+
 IN_PROC_BROWSER_TEST_F(PageLoadMetricsBrowserTest, LoadingMetrics) {
   ASSERT_TRUE(embedded_test_server()->Start());
   auto waiter = CreatePageLoadMetricsWaiter();
