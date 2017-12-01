5aad24108a7702c8d33cbe5e54941755fbdfee50
liberato@chromium.org <liberato@chromium.org>
2017-12-01 10:14:48 -0800

    Skip promotion hints for unused resources.
    If a resource is marked to request a promotion hint, but it isn't
    used by any quad, then it might not be synced by GLRenderer before
    promotion hints are sent.  In that case, locking the resource for
    read will DCHECK.
    
    This CL adds a check for unsynchronized resources, and skips sending
    hints for those that aren't synced.
    
    Bug: 788359
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I54a2d4ced392a313a56f48a1b066ca3f2f62d152

--- cc/resources/resource_provider_unittest.cc
+++ cc/resources/resource_provider_unittest.cc
@@ -797,7 +797,8 @@ TEST_P(ResourceProviderTest, TransferGLResources) {
   EXPECT_FALSE(child_resource_provider_->InUseByConsumer(id3));
 
   {
-    child_resource_provider_->WaitSyncToken(id1);
+    // ScopedWriteLockGL will wait for the sync token, which is convenient since
+    // |child_resource_provider_| doesn't expose that.
     ResourceProvider::ScopedWriteLockGL lock(child_resource_provider_.get(),
                                              id1);
     ASSERT_NE(0U, lock.GetTexture());
@@ -805,7 +806,7 @@ TEST_P(ResourceProviderTest, TransferGLResources) {
   // Ensure copying to resource doesn't fail.
   child_resource_provider_->CopyToResource(id2, data2, size);
   {
-    child_resource_provider_->WaitSyncToken(id2);
+    // ScopedWriteLockGL will wait for the sync token.
     ResourceProvider::ScopedWriteLockGL lock(child_resource_provider_.get(),
                                              id2);
     ASSERT_NE(0U, lock.GetTexture());
@@ -913,11 +914,15 @@ TEST_P(ResourceProviderTest, OverlayPromotionHint) {
       resource_provider_->GetChildToParentMap(child_id);
   viz::ResourceId mapped_id1 = resource_map[list[0].id];
   viz::ResourceId mapped_id2 = resource_map[list[1].id];
+
+  // The promotion hints should not be recorded until after we wait.
+  EXPECT_EQ(0u, resource_provider_->CountPromotionHintRequestsForTesting());
   {
     resource_provider_->WaitSyncToken(mapped_id1);
     DisplayResourceProvider::ScopedReadLockGL lock(resource_provider_.get(),
                                                    mapped_id1);
   }
+  EXPECT_EQ(1u, resource_provider_->CountPromotionHintRequestsForTesting());
 
   EXPECT_EQ(list[0].mailbox_holder.sync_token,
             context3d_->last_waited_sync_token());
@@ -1177,7 +1182,8 @@ TEST_P(ResourceProviderTest, ReadLockCountStopsReturnToChildOrDelete) {
   EXPECT_EQ(1u, returned_to_child.size());
   child_resource_provider_->ReceiveReturnsFromParent(returned_to_child);
 
-  child_resource_provider_->WaitSyncToken(id1);
+  // No need to wait for the sync token here -- it will be returned to the
+  // client on delete.
   EXPECT_EQ(1u, child_resource_provider_->num_resources());
   child_resource_provider_->DeleteResource(id1);
   EXPECT_EQ(0u, child_resource_provider_->num_resources());
