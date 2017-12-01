1aa8ab46796bed8e9c5d56fa1900b61c2c71e84b
Aleks Totic <atotic@chromium.org>
2017-12-01 15:46:44 -0800

    [WIP][LayoutNG] Propagate inline_container to NGOOFPosDescendant
    This is pretty close to the patch I'd like to land for abspos inside inline containing blocks. It performs very close to LegacyLayout right now (with the exception of handling border/padding on inline cb).
    
    The logic of the patch is:
    - collect pointers to inline-cb inside inline_layout_algorithm.
    - layout OOF descendants with inline-cb containers inside NGOutOfFlowLayoutPart for inline-cb parent block.
    - Inside NGOutOfFlowLayoutPart
      - compute inline-cb dimensions for each inline-cb. This computation is hard to follow, I've commented it as well as aI could.
      - use inline-cb dimensions to position inline-cb OOF descendants.
    
    Bug: 740993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Id3ad9f31d5e78cd5ed4324bc5dbc3ff54f296f5b

--- third_party/WebKit/Source/core/layout/ng/ng_container_fragment_builder.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_container_fragment_builder.cc
@@ -4,8 +4,10 @@
 
 #include "core/layout/ng/ng_container_fragment_builder.h"
 
+#include "core/layout/LayoutObject.h"
 #include "core/layout/ng/ng_exclusion_space.h"
 #include "core/layout/ng/ng_layout_result.h"
+#include "core/layout/ng/ng_physical_container_fragment.h"
 #include "core/layout/ng/ng_physical_fragment.h"
 #include "core/layout/ng/ng_unpositioned_float.h"
 #include "core/style/ComputedStyle.h"
@@ -125,12 +127,15 @@ NGContainerFragmentBuilder&
 NGContainerFragmentBuilder::AddInlineOutOfFlowChildCandidate(
     NGBlockNode child,
     const NGLogicalOffset& child_offset,
-    TextDirection line_direction) {
+    TextDirection line_direction,
+    LayoutObject* inline_container) {
   DCHECK(child);
   oof_positioned_candidates_.push_back(NGOutOfFlowPositionedCandidate(
-      NGOutOfFlowPositionedDescendant{
-          child, NGStaticPosition::Create(GetWritingMode(), line_direction,
-                                          NGPhysicalOffset())},
+      NGOutOfFlowPositionedDescendant(
+          child,
+          NGStaticPosition::Create(GetWritingMode(), line_direction,
+                                   NGPhysicalOffset()),
+          inline_container),
       child_offset, line_direction));
 
   child.SaveStaticOffsetForLegacy(child_offset);
@@ -165,8 +170,9 @@ void NGContainerFragmentBuilder::GetAndClearOutOfFlowDescendantCandidates(
     builder_relative_position.offset =
         child_offset + candidate.descendant.static_position.offset;
 
-    descendant_candidates->push_back(NGOutOfFlowPositionedDescendant{
-        candidate.descendant.node, builder_relative_position});
+    descendant_candidates->push_back(NGOutOfFlowPositionedDescendant(
+        candidate.descendant.node, builder_relative_position,
+        candidate.descendant.inline_container));
   }
 
   // Clear our current canidate list. This may get modified again if the
@@ -178,5 +184,87 @@ void NGContainerFragmentBuilder::GetAndClearOutOfFlowDescendantCandidates(
   // fixed descendant).
   oof_positioned_candidates_.clear();
 }
+
+void NGContainerFragmentBuilder::ComputeInlineContainerFragments(
+    HashMap<const LayoutObject*, FragmentPair>* inline_container_fragments,
+    NGLogicalSize* container_size) {
+  // This routine has detailed knowledge of inline fragment tree structure,
+  // and will break if this changes.
+
+  DCHECK_GE(inline_size_, LayoutUnit());
+  DCHECK_GE(block_size_, LayoutUnit());
+  container_size->inline_size = inline_size_;
+  container_size->block_size = block_size_;
+
+  for (size_t i = 0; i < children_.size(); i++) {
+    scoped_refptr<NGPhysicalFragment> child = children_[i];
+    if (child->IsLineBox()) {
+      for (auto& line_box_child :
+           ToNGPhysicalContainerFragment(*child).Children()) {
+        LayoutObject* key = nullptr;
+        if (line_box_child->IsText()) {
+          key = line_box_child->GetLayoutObject();
+          DCHECK(key);
+          key = key->Parent();
+          DCHECK(key);
+        } else if (line_box_child->IsBox()) {
+          // TODO(atotic)
+          // kojii: A box fragment may contain text or another box as children,
+          // so you may want to look into them recursively?
+          key = line_box_child->GetLayoutObject();
+        }
+        if (inline_container_fragments->Contains(key)) {
+          NGContainerFragmentBuilder::FragmentPair value =
+              inline_container_fragments->at(key);
+          if (!value.start_fragment) {
+            value.start_fragment = line_box_child;
+            value.start_linebox_fragment = child;
+            value.start_linebox_offset = offsets_.at(i);
+          }
+          value.end_fragment = line_box_child;
+          value.end_linebox_fragment = child;
+          value.end_linebox_offset = offsets_.at(i);
+          inline_container_fragments->Set(key, value);
+        }
+      }
+    }
+  }
+  // Sort fragments in the physical direction.
+  // Inline layout always places fragments in LTR order.
+  // Fragment order is significant when computing inline-cb geometry.
+  // When inline-cb is rtl direction, fragments are swapped.
+  // TODO(atotic) this might fail if start and end fragments are in different
+  // lineboxes, but there are multiple fragments from same inline-cb inside
+  // a single line.
+  for (auto fragment_pair : *inline_container_fragments) {
+    DCHECK(fragment_pair.value.start_fragment);
+    DCHECK(fragment_pair.value.end_fragment);
+    if (fragment_pair.value.start_fragment &&
+        fragment_pair.value.start_linebox_fragment.get() ==
+            fragment_pair.value.end_linebox_fragment.get() &&
+        fragment_pair.value.start_linebox_fragment->Style().Direction() ==
+            TextDirection::kRtl) {
+      std::swap(fragment_pair.value.start_fragment,
+                fragment_pair.value.end_fragment);
+      inline_container_fragments->Set(fragment_pair.key, fragment_pair.value);
+    }
+  }
+}
+
+#ifndef NDEBUG
+
+String NGContainerFragmentBuilder::ToString() const {
+  StringBuilder builder;
+  builder.Append(String::Format("ContainerFragment %.2fx%.2f, Children %lu\n",
+                                inline_size_.ToFloat(), block_size_.ToFloat(),
+                                children_.size()));
+  for (auto& child : children_) {
+    builder.Append(child->DumpFragmentTree(
+        NGPhysicalFragment::DumpAll & ~NGPhysicalFragment::DumpHeaderText));
+  }
+  return builder.ToString();
+}
+
+#endif
 
 }  // namespace blink
--- third_party/WebKit/Source/core/layout/ng/ng_container_fragment_builder.h
+++ third_party/WebKit/Source/core/layout/ng/ng_container_fragment_builder.h
@@ -98,13 +98,38 @@ class CORE_EXPORT NGContainerFragmentBuilder : public NGBaseFragmentBuilder {
   NGContainerFragmentBuilder& AddInlineOutOfFlowChildCandidate(
       NGBlockNode,
       const NGLogicalOffset& child_line_offset,
-      TextDirection line_direction);
+      TextDirection line_direction,
+      LayoutObject* inline_container);
 
   NGContainerFragmentBuilder& AddOutOfFlowDescendant(
       NGOutOfFlowPositionedDescendant);
 
   void GetAndClearOutOfFlowDescendantCandidates(
       Vector<NGOutOfFlowPositionedDescendant>* descendant_candidates);
+
+  // Inline containing block geometry is defined by two fragments:
+  // start and end. FragmentPair holds the information needed to compute
+  // inline containing block geometry wrt enclosing container block.
+  // start_fragment is the start fragment of inline containing block.
+  //   Its parent fragment is start_linebox_fragment.
+  // start_linebox_offset is offset of linebox from inline-cb
+  // end_fragment/linebox are complementary properties for end fragment.
+  struct FragmentPair {
+    scoped_refptr<NGPhysicalFragment> start_linebox_fragment;
+    scoped_refptr<NGPhysicalFragment> start_fragment;
+    NGLogicalOffset start_linebox_offset;
+    scoped_refptr<NGPhysicalFragment> end_linebox_fragment;
+    scoped_refptr<NGPhysicalFragment> end_fragment;
+    NGLogicalOffset end_linebox_offset;
+  };
+
+  void ComputeInlineContainerFragments(
+      HashMap<const LayoutObject*, FragmentPair>* inline_container_fragments,
+      NGLogicalSize* container_size);
+
+#ifndef NDEBUG
+  String ToString() const;
+#endif
 
  protected:
   // An out-of-flow positioned-candidate is a temporary data structure used
