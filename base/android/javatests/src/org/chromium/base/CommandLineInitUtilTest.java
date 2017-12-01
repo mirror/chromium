c49ca6dbb58776b7c5e1593041d416c1fe4fe8a0
Mohamed Heikal <mheikal@google.com>
2017-12-01 17:23:01 -0500

    Remove CmdLine handling from BaseChromiumApplication
    Removes static and member functions that initialize CmdLine from
    BaseChromiumApplication. Functionality is moved to ChromeApplication as
    well as other "Application"s
    
    Bug: 560466
    Change-Id: Ia5115dc5ce6009e645e01432c0013af5491e15e4

--- base/android/javatests/src/org/chromium/base/CommandLineInitUtilTest.java
+++ base/android/javatests/src/org/chromium/base/CommandLineInitUtilTest.java
@@ -24,7 +24,7 @@ public class CommandLineInitUtilTest {
     public void setUp() throws Exception {
         CommandLineInitUtil.initCommandLine(
                 InstrumentationRegistry.getInstrumentation().getTargetContext(),
-                "content-shell-command-line");
+                "test-cmdline-file");
     }
 
     /**
