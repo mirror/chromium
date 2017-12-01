d9c627862df5723732e7edc1bd96e3f539e6ea31
Christos Froussios <cfroussios@chromium.org>
2017-12-01 14:42:24 +0100

    [Password Manager] Export error message is localised
    We want to show a basic error message to the user, if exporting the
    passwords to the filesystem fails.
    
    Bug:789561
    Change-Id: Ieff5a590dd882374494c3d23c2aef0ff89734545

--- chrome/browser/ui/passwords/destination_file_system.cc
+++ chrome/browser/ui/passwords/destination_file_system.cc
@@ -7,12 +7,20 @@
 #include <utility>
 
 #include "base/files/file_util.h"
+#include "base/strings/utf_string_conversions.h"
+#include "components/strings/grit/components_strings.h"
+#include "ui/base/l10n/l10n_util.h"
 
 DestinationFileSystem::DestinationFileSystem(base::FilePath destination_path)
     : destination_path_(std::move(destination_path)) {}
 
-bool DestinationFileSystem::Write(const std::string& data) {
-  return base::WriteFile(destination_path_, data.c_str(), data.size());
+base::string16 DestinationFileSystem::Write(const std::string& data) {
+  if (base::WriteFile(destination_path_, data.c_str(), data.size()))
+    return base::string16();
+  else
+    return l10n_util::GetStringFUTF16(
+        IDS_PASSWORD_MANAGER_EXPORT_TO_FILE_FAILED,
+        base::UTF8ToUTF16(destination_path_.DirName().BaseName().value()));
 }
 
 const base::FilePath& DestinationFileSystem::GetDestinationPathForTesting() {
