64aecef1eab60a58badb1fad7c9008a89202d2f3
Jose Dapena Paz <jose.dapena@lge.com>
2017-12-01 15:49:36 +0100

    A11y: add ATK_STATE_FOCUSED support.
    Map AX_EVENT_FOCUS to state updates of ATK_STATE_FOCUSED. As Blur events
    are expected to be removed, this change adds a global static variable
    pointing to current focus. This is needed to be able to emit setting
    ATK_STATE_FOCUSED to false for the node that becomes unfocused.
    
    Bug: 791645
    Change-Id: If1c675279136e66bfc2b489552f505f4a0a38fe7

--- ui/accessibility/platform/ax_platform_node_auralinux.cc
+++ ui/accessibility/platform/ax_platform_node_auralinux.cc
@@ -775,6 +775,8 @@ void AXPlatformNodeAuraLinux::DestroyAtkObjects() {
     atk_hyperlink_ = nullptr;
   }
   if (atk_object_) {
+    if (atk_object_ == current_focused_)
+      current_focused_ = nullptr;
     if (GetParent()) {
       g_signal_emit_by_name(ATK_OBJECT(GetParent()), "children-changed::remove",
                             -1, atk_object_);
@@ -1080,7 +1082,34 @@ gfx::NativeViewAccessible AXPlatformNodeAuraLinux::GetNativeViewAccessible() {
   return atk_object_;
 }
 
+AtkObject* AXPlatformNodeAuraLinux::current_focused_ = nullptr;
+
+void AXPlatformNodeAuraLinux::OnFocused() {
+  DCHECK(atk_object_);
+
+  if (atk_object_ == current_focused_)
+    return;
+
+  if (current_focused_) {
+    g_signal_emit_by_name(current_focused_, "focus-event", false);
+    atk_object_notify_state_change(ATK_OBJECT(current_focused_),
+                                   ATK_STATE_FOCUSED, false);
+  }
+
+  current_focused_ = atk_object_;
+  g_signal_emit_by_name(atk_object_, "focus-event", true);
+  atk_object_notify_state_change(ATK_OBJECT(atk_object_), ATK_STATE_FOCUSED,
+                                 true);
+}
+
 void AXPlatformNodeAuraLinux::NotifyAccessibilityEvent(ui::AXEvent event_type) {
+  switch (event_type) {
+    case AX_EVENT_FOCUS:
+      OnFocused();
+      break;
+    default:
+      break;
+  }
 }
 
 int AXPlatformNodeAuraLinux::GetIndexInParent() {
