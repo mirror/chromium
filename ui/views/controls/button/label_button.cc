d3704d143e2cb0c07435fc71885b432933df3eb3
Trent Apted <tapted@chromium.org>
2017-12-01 13:51:46 +1100

    Do not call Layout() in LabelButton::ChildPreferredSizeChanged()
    LabelButton::ChildPreferredSizeChanged() is invoked for every property
    change on the button label. E.g. changing the text alignment during
    construction. A Layout() at this time is usually wasted effort.
    
    LabelButton::ChildPreferredSizeChanged() already calls PreferredSizeChanged()
    which invalidates layout. Require callers who change properties to
    trigger their own Layout().
    
    Bug: 788597
    Change-Id: Iad8598f5b939a15a038278ee07f924e36fe5dd68

--- ui/views/controls/button/label_button.cc
+++ ui/views/controls/button/label_button.cc
@@ -500,7 +500,6 @@ void LabelButton::SetTextInternal(const base::string16& text) {
 void LabelButton::ChildPreferredSizeChanged(View* child) {
   ResetCachedPreferredSize();
   PreferredSizeChanged();
-  Layout();
 }
 
 ui::NativeTheme::Part LabelButton::GetThemePart() const {
