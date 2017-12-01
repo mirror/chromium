6dd6be333e46e40470e403529983dff9c343e27b
Yoichi Osato <yoichio@chromium.org>
2017-12-01 14:41:45 +0900

    Remove unused virtual declaration
    Bug: 739062
    Change-Id: I49b1426f9fc89586d1e818b10385c18c5223399e

--- third_party/WebKit/Source/core/layout/LayoutObject.h
+++ third_party/WebKit/Source/core/layout/LayoutObject.h
@@ -1485,7 +1485,7 @@ class CORE_EXPORT LayoutObject : public ImageResourceObserver,
   SelectionState GetSelectionState() const {
     return bitfields_.GetSelectionState();
   }
-  virtual void SetSelectionState(SelectionState state) {
+  void SetSelectionState(SelectionState state) {
     bitfields_.SetSelectionState(state);
   }
   inline void SetSelectionStateIfNeeded(SelectionState);
