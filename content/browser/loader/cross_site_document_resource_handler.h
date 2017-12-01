49c6bad7ccc79a18ddf291c531e1f364da678f7f
Charles Reis <creis@chromium.org>
2017-12-01 12:23:13 -0800

    Block cross-site document responses in Site Isolation modes.
    When using --isolate-origins=... or  --site-per-process, some or all sites will
    be given dedicated processes.  These processes should not be allowed to
    receive documents (HTML, XML, JSON) from other sites unless they are
    made available via CORS or another exception.
    
    This CL adds a ResourceHandler to enforce this restriction in the browser
    process.  The handler inspects the response headers and decides whether
    to block it from reaching the renderer process.  It also sniffs the content to
    confirm the response is labeled correctly, to avoid blocking things like
    JavaScript mislabeled as JSON.
    
    Blocked responses are sent back as empty response bodies rather than as
    a network error, to ensure existing behavior does not change.  Other
    cross-site responses (e.g., images, scripts, etc) are still allowed.
    
    Note that the current approach assumes the renderer process is not
    compromised and will not lie about the initiating origin, etc.  This assumption
    will be removed in future work, for bug 268640.
    For more details, see:
    http://www.chromium.org/developers/design-documents/blocking-cross-site-documents
    
    BUG=786505
    
    Change-Id: Idc2be65fc869604738dc1607c5e7fee191443b9b
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation

---
+++ content/browser/loader/cross_site_document_resource_handler.h
@@ -0,0 +1,102 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_LOADER_CROSS_SITE_DOCUMENT_RESOURCE_HANDLER_H_
+#define CONTENT_BROWSER_LOADER_CROSS_SITE_DOCUMENT_RESOURCE_HANDLER_H_
+
+#include <memory>
+
+#include "base/macros.h"
+#include "content/browser/loader/layered_resource_handler.h"
+#include "content/common/cross_site_document_classifier.h"
+#include "content/public/common/resource_type.h"
+
+namespace net {
+class URLRequest;
+}  // namespace net
+
+namespace content {
+
+// A ResourceHandler that prevents the renderer process from receiving network
+// responses that contain cross-site documents (HTML, XML, some plain text) or
+// similar data that should be opaque (JSON), with appropriate exceptions to
+// preserve compatibility.  Other cross-site resources such as scripts, images,
+// stylesheets, etc are still allowed.
+//
+// This handler is not used for navigations, which create a new security context
+// based on the origin of the response.  It currently only protects documents
+// from sites that require dedicated renderer processes, though it could be
+// expanded to apply to all sites.
+//
+// When a response is blocked, the renderer is sent an empty response body
+// instead of seeing a failed request.  A failed request would change page-
+// visible behavior (e.g., for a blocked XHR).  An empty response can generally
+// be consumed by the renderer without noticing the difference.
+//
+// For more details, see:
+// http://chromium.org/developers/design-documents/blocking-cross-site-documents
+class CONTENT_EXPORT CrossSiteDocumentResourceHandler
+    : public LayeredResourceHandler {
+ public:
+  CrossSiteDocumentResourceHandler(
+      std::unique_ptr<ResourceHandler> next_handler,
+      net::URLRequest* request,
+      ResourceType resource_type);
+  ~CrossSiteDocumentResourceHandler() override;
+
+  // LayeredResourceHandler overrides:
+  void OnResponseStarted(
+      ResourceResponse* response,
+      std::unique_ptr<ResourceController> controller) override;
+  void OnWillRead(scoped_refptr<net::IOBuffer>* buf,
+                  int* buf_size,
+                  std::unique_ptr<ResourceController> controller) override;
+  void OnReadCompleted(int bytes_read,
+                       std::unique_ptr<ResourceController> controller) override;
+  void OnResponseCompleted(
+      const net::URLRequestStatus& status,
+      std::unique_ptr<ResourceController> controller) override;
+
+ private:
+  // Computes whether this response contains a cross-site document that needs to
+  // be blocked from the renderer process.
+  bool ShouldBlockResponse();
+
+  // The type of the request, as claimed by the renderer process.
+  ResourceType resource_type_;
+
+  // A local buffer for sniffing content and using for throwaway reads, which is
+  // not shared with the renderer process.
+  scoped_refptr<net::IOBuffer> local_buffer_;
+
+  // The buffer allocated by the next ResourceHandler for reads, which is used
+  // if sniffing determines that we should proceed with the response.
+  scoped_refptr<net::IOBuffer> next_handler_buffer_;
+
+  // The size of |next_handler_buffer_|.
+  int next_handler_buffer_size_ = 0;
+
+  // Whether this response is a cross-site document that should be blocked,
+  // pending the outcome of sniffing the content.  Set in OnResponseStarted and
+  // should only be read afterwards.
+  bool should_block_response_ = false;
+
+  // A canonicalization of the specified MIME type, to determine if blocking the
+  // response is needed, as well as which type of sniffing to perform.
+  CrossSiteDocumentMimeType canonical_mime_type_ =
+      CROSS_SITE_DOCUMENT_MIME_TYPE_OTHERS;
+
+  // Whether a "nosniff" header was present and thus sniffing should be skipped.
+  bool no_sniff_ = false;
+
+  // Whether the next ResourceHandler has already been told that the read has
+  // completed, and thus it is safe to cancel or detach on the next read.
+  bool blocked_read_completed_ = false;
+
+  DISALLOW_COPY_AND_ASSIGN(CrossSiteDocumentResourceHandler);
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_LOADER_CROSS_SITE_DOCUMENT_RESOURCE_HANDLER_H_
