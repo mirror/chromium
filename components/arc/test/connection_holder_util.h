9fd8b6a386d7e615f729889fb1abb9aa40524e7a
Luis Hector Chavez <lhchavez@google.com>
2017-11-30 18:29:25 -0800

    arc: Introduce WaitForInstanceReady()
    This change adds arc::WaitForInstanceReady(), so that tests that
    manually invoke SetInstance() can wait until the instance has been fully
    connected in a safe, race-free way.
    
    Bug: 782895
    Test: git cl try
    Change-Id: I4480ab78401bb145ba99fa60a1417b2c8c6af94d

---
+++ components/arc/test/connection_holder_util.h
@@ -0,0 +1,48 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ARC_TEST_CONNECTION_HOLDER_UTIL_H_
+#define COMPONENTS_ARC_TEST_CONNECTION_HOLDER_UTIL_H_
+
+#include <utility>
+
+#include "base/callback_helpers.h"
+#include "base/run_loop.h"
+#include "components/arc/connection_holder.h"
+
+namespace arc {
+
+// Waits for the instance to be ready.
+template <typename InstanceType, typename HostType>
+void WaitForInstanceReady(ConnectionHolder<InstanceType, HostType>* holder) {
+  if (holder->IsConnected())
+    return;
+
+  class ReadinessObserver
+      : public ConnectionHolder<InstanceType, HostType>::Observer {
+   public:
+    explicit ReadinessObserver(base::OnceClosure closure)
+        : closure_(std::move(closure)) {}
+    ~ReadinessObserver() override = default;
+
+   private:
+    void OnConnectionReady() override {
+      if (!closure_)
+        return;
+      base::ResetAndReturn(&closure_).Run();
+    }
+
+    base::OnceClosure closure_;
+  };
+
+  base::RunLoop run_loop;
+  ReadinessObserver readiness_observer(run_loop.QuitClosure());
+  holder->AddObserver(&readiness_observer);
+  run_loop.Run();
+  holder->RemoveObserver(&readiness_observer);
+}
+
+}  // namespace arc
+
+#endif  // COMPONENTS_ARC_TEST_CONNECTION_HOLDER_UTIL_H_
