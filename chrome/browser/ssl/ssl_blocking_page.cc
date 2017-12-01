1efeb44b46929a3b20c5bc84e50652c24da4b323
spqchan <spqchan@chromium.org>
2017-12-01 12:25:31 -0800

    Implement Urgent Interstitial
    Add an Urgent Interstitial type in ssl_error_assistant.proto
    and implement logic to load and check the data in
    SSLErrorAssistant.
    
    Testing: SSLErrorAssistantTest.UrgentInterstitialList
    
    Bug:790850
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Id0453be1f48a3c983023aa9a80494c972edc2287

--- chrome/browser/ssl/ssl_blocking_page.cc
+++ chrome/browser/ssl/ssl_blocking_page.cc
@@ -125,6 +125,7 @@ SSLBlockingPage* SSLBlockingPage::Create(
     const GURL& request_url,
     int options_mask,
     const base::Time& time_triggered,
+    const GURL& support_url,
     std::unique_ptr<SSLCertReporter> ssl_cert_reporter,
     bool is_superfish,
     const base::Callback<void(content::CertificateRequestResultType)>&
@@ -135,7 +136,7 @@ SSLBlockingPage* SSLBlockingPage::Create(
   metrics_helper.get()->StartRecordingCaptivePortalMetrics(overridable);
 
   return new SSLBlockingPage(web_contents, cert_error, ssl_info, request_url,
-                             options_mask, time_triggered,
+                             options_mask, time_triggered, support_url,
                              std::move(ssl_cert_reporter), overridable,
                              std::move(metrics_helper), is_superfish, callback);
 }
@@ -173,6 +174,7 @@ SSLBlockingPage::SSLBlockingPage(
     const GURL& request_url,
     int options_mask,
     const base::Time& time_triggered,
+    const GURL& support_url,
     std::unique_ptr<SSLCertReporter> ssl_cert_reporter,
     bool overridable,
     std::unique_ptr<ChromeMetricsHelper> metrics_helper,
@@ -215,6 +217,7 @@ SSLBlockingPage::SSLBlockingPage(
                                              ssl_info,
                                              options_mask,
                                              time_triggered,
+                                             support_url,
                                              controller())) {
   // Creating an interstitial without showing (e.g. from chrome://interstitials)
   // it leaks memory, so don't create it here.
