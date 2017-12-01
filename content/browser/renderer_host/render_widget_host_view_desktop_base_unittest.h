6db662bd565100020972bb17842d739b8b2bf56f
Francois Doray <fdoray@chromium.org>
2017-12-01 13:26:46 -0500

    Add content::RenderWidgetHostView::GetVisibility().
    This method replaces content::RenderWidgetHostView::IsShowing(). It
    returns an enum that indicates whether the view is visible, occluded
    or hidden. The enum allows callers to clearly communicate their
    intent when they ask about the visibility of a view.
    
    Note: content::Visibility will be reused by
    content::WebContents::GetVisibility, which will be added in a
    separate CL.
    
    Bug: 668690
    Change-Id: Id384f316a40cb1f7e596d7fba9f03ce82e8ce7b0

---
+++ content/browser/renderer_host/render_widget_host_view_desktop_base_unittest.h
@@ -0,0 +1,25 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_RENDERER_HOST_RENDER_WIDGET_HOST_VIEW_DESKTOP_BASE_UNITTEST_H_
+#define CONTENT_BROWSER_RENDERER_HOST_RENDER_WIDGET_HOST_VIEW_DESKTOP_BASE_UNITTEST_H_
+
+#include "base/callback_forward.h"
+
+namespace content {
+
+class RenderWidgetHostViewDesktopBase;
+
+using SetIsOccludedCallback = base::RepeatingCallback<void(bool is_occluded)>;
+
+// Tests for derived classes of RenderWidgetHostViewDesktopBase.
+void RenderWidgetHostViewDesktopBase_TestShowHide(
+    RenderWidgetHostViewDesktopBase* view);
+void RenderWidgetHostViewDesktopBase_TestOcclusion(
+    RenderWidgetHostViewDesktopBase* view,
+    const SetIsOccludedCallback& set_is_occluded);
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_RENDERER_HOST_RENDER_WIDGET_HOST_VIEW_DESKTOP_BASE_UNITTEST_H_
