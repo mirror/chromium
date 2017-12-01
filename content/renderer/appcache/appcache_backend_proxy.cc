8a235398f8388dc298f4985510a514fb29b0c7c0
Nicholas Verne <nverne@chromium.org>
2017-12-01 13:36:18 +1100

    Converting AppCacheHostMsg IPCs to mojo
    Bug: 611938
    Change-Id: I3a69783da5c3bcbe99b8a2c21f5f9c0a5c934e32

--- content/renderer/appcache/appcache_backend_proxy.cc
+++ content/renderer/appcache/appcache_backend_proxy.cc
@@ -3,23 +3,37 @@
 // found in the LICENSE file.
 
 #include "content/renderer/appcache/appcache_backend_proxy.h"
-
+#include "content/common/appcache.mojom.h"
 #include "content/common/appcache_messages.h"
+#include "content/public/common/service_names.mojom.h"
+#include "content/public/renderer/render_thread.h"
+#include "services/service_manager/public/cpp/connector.h"
 
 namespace content {
 
+AppCacheBackendProxy::AppCacheBackendProxy(IPC::Sender* sender)
+    : sender_(sender) {}
+AppCacheBackendProxy::~AppCacheBackendProxy() {}
+
+mojom::AppCacheHost* AppCacheBackendProxy::GetAppCacheHost() {
+  if (!app_cache_host_ptr_) {
+    RenderThread::Get()->GetConnector()->BindInterface(
+        mojom::kBrowserServiceName, mojo::MakeRequest(&app_cache_host_ptr_));
+  }
+  return app_cache_host_ptr_.get();
+}
+
 void AppCacheBackendProxy::RegisterHost(int host_id) {
-  sender_->Send(new AppCacheHostMsg_RegisterHost(host_id));
+  GetAppCacheHost()->RegisterHost(host_id);
 }
 
 void AppCacheBackendProxy::UnregisterHost(int host_id) {
-  sender_->Send(new AppCacheHostMsg_UnregisterHost(host_id));
+  GetAppCacheHost()->UnregisterHost(host_id);
 }
 
 void AppCacheBackendProxy::SetSpawningHostId(int host_id,
                                              int spawning_host_id) {
-  sender_->Send(new AppCacheHostMsg_SetSpawningHostId(
-                                    host_id, spawning_host_id));
+  GetAppCacheHost()->SetSpawningHostId(host_id, spawning_host_id);
 }
 
 void AppCacheBackendProxy::SelectCache(
@@ -27,48 +41,48 @@ void AppCacheBackendProxy::SelectCache(
     const GURL& document_url,
     const int64_t cache_document_was_loaded_from,
     const GURL& manifest_url) {
-  sender_->Send(new AppCacheHostMsg_SelectCache(
-                                    host_id, document_url,
-                                    cache_document_was_loaded_from,
-                                    manifest_url));
+  GetAppCacheHost()->SelectCache(host_id, document_url,
+                                 cache_document_was_loaded_from, manifest_url);
 }
 
 void AppCacheBackendProxy::SelectCacheForSharedWorker(int host_id,
                                                       int64_t appcache_id) {
-  sender_->Send(new AppCacheHostMsg_SelectCacheForSharedWorker(
-                                    host_id, appcache_id));
+  GetAppCacheHost()->SelectCacheForSharedWorker(host_id, appcache_id);
 }
 
 void AppCacheBackendProxy::MarkAsForeignEntry(
     int host_id,
     const GURL& document_url,
     int64_t cache_document_was_loaded_from) {
-  sender_->Send(new AppCacheHostMsg_MarkAsForeignEntry(
-                                    host_id, document_url,
-                                    cache_document_was_loaded_from));
+  GetAppCacheHost()->MarkAsForeignEntry(host_id, document_url,
+                                        cache_document_was_loaded_from);
 }
 
 AppCacheStatus AppCacheBackendProxy::GetStatus(int host_id) {
   AppCacheStatus status = AppCacheStatus::APPCACHE_STATUS_UNCACHED;
-  sender_->Send(new AppCacheHostMsg_GetStatus(host_id, &status));
+  GetAppCacheHost()->GetStatus(host_id, &status);
   return status;
 }
 
 bool AppCacheBackendProxy::StartUpdate(int host_id) {
   bool result = false;
-  sender_->Send(new AppCacheHostMsg_StartUpdate(host_id, &result));
+  GetAppCacheHost()->StartUpdate(host_id, &result);
   return result;
 }
 
 bool AppCacheBackendProxy::SwapCache(int host_id) {
   bool result = false;
-  sender_->Send(new AppCacheHostMsg_SwapCache(host_id, &result));
+  GetAppCacheHost()->SwapCache(host_id, &result);
   return result;
 }
 
 void AppCacheBackendProxy::GetResourceList(
     int host_id, std::vector<AppCacheResourceInfo>* resource_infos) {
-  sender_->Send(new AppCacheHostMsg_GetResourceList(host_id, resource_infos));
+  std::vector<mojom::AppCacheResourceInfoPtr> boxed_infos;
+  GetAppCacheHost()->GetResourceList(host_id, &boxed_infos);
+  for (auto& b : boxed_infos) {
+    resource_infos->emplace_back(std::move(*b));
+  }
 }
 
 }  // namespace content
