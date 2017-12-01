55302dbdb4e477aa7870174a03e815bd4e1a2597
Jun Choi <hongjunchoi@chromium.org>
2017-11-30 22:55:06 -0800

    Support CBOR encoding and decoding of negative integers
    Added support for major type 1 for CBORValue, CBORReader, and
    CBORWriter. While CBOR negative integer type has range [0, -2^64]
    as defined by the spec, current implementation only supports range
    between [-2^63, 2^63-1] as integer type values are represented
    using uint64_t.
    
    Bug:786217
    Change-Id: Ifee69efcddecea104532b99176d705b3d2801f41

--- content/browser/webauth/cbor/cbor_values.cc
+++ content/browser/webauth/cbor/cbor_values.cc
@@ -21,7 +21,8 @@ CBORValue::CBORValue(Type type) : type_(type) {
   // Initialize with the default value.
   switch (type_) {
     case Type::UNSIGNED:
-      unsigned_value_ = 0;
+    case Type::NEGATIVE:
+      integer_value_ = 0;
       return;
     case Type::BYTE_STRING:
       new (&bytestring_value_) BinaryValue();
@@ -41,8 +42,9 @@ CBORValue::CBORValue(Type type) : type_(type) {
   NOTREACHED();
 }
 
-CBORValue::CBORValue(uint64_t in_unsigned)
-    : type_(Type::UNSIGNED), unsigned_value_(in_unsigned) {}
+CBORValue::CBORValue(int64_t integer_value) : integer_value_(integer_value) {
+  type_ = integer_value >= 0 ? Type::UNSIGNED : Type::NEGATIVE;
+}
 
 CBORValue::CBORValue(const BinaryValue& in_bytes)
     : type_(Type::BYTE_STRING), bytestring_value_(in_bytes) {}
@@ -96,7 +98,9 @@ CBORValue CBORValue::Clone() const {
     case Type::NONE:
       return CBORValue();
     case Type::UNSIGNED:
-      return CBORValue(unsigned_value_);
+      return CBORValue(integer_value_);
+    case Type::NEGATIVE:
+      return CBORValue(integer_value_);
     case Type::BYTE_STRING:
       return CBORValue(bytestring_value_);
     case Type::STRING:
@@ -111,9 +115,14 @@ CBORValue CBORValue::Clone() const {
   return CBORValue();
 }
 
-uint64_t CBORValue::GetUnsigned() const {
+int64_t CBORValue::GetUnsigned() const {
   CHECK(is_unsigned());
-  return unsigned_value_;
+  return integer_value_;
+}
+
+int64_t CBORValue::GetNegative() const {
+  CHECK(is_negative());
+  return integer_value_;
 }
 
 const std::string& CBORValue::GetString() const {
@@ -141,7 +150,8 @@ void CBORValue::InternalMoveConstructFrom(CBORValue&& that) {
 
   switch (type_) {
     case Type::UNSIGNED:
-      unsigned_value_ = that.unsigned_value_;
+    case Type::NEGATIVE:
+      integer_value_ = that.integer_value_;
       return;
     case Type::BYTE_STRING:
       new (&bytestring_value_) BinaryValue(std::move(that.bytestring_value_));
@@ -177,6 +187,7 @@ void CBORValue::InternalCleanup() {
       break;
     case Type::NONE:
     case Type::UNSIGNED:
+    case Type::NEGATIVE:
       break;
   }
   type_ = Type::NONE;
