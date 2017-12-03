733bd36ec8d6cf90034cdbcd6a1e805f443c179c
Brett Wilson <brettw@chromium.org>
2017-12-02 20:17:17 -0800

    Convert some accessibility classes to use C++11 initializers.
    This simplifies some constructors.
    
    Change-Id: I5ca3d141c6ad7d4837cd0d97bdcc17524914629d

--- ui/accessibility/ax_tree_unittest.cc
+++ ui/accessibility/ax_tree_unittest.cc
@@ -268,7 +268,7 @@ TEST(AXTreeTest, SerializeSimpleAXTree) {
   ASSERT_TRUE(dst_tree.Unserialize(update));
 
   const AXNode* root_node = dst_tree.root();
-  ASSERT_TRUE(root_node != NULL);
+  ASSERT_TRUE(root_node != nullptr);
   EXPECT_EQ(root.id, root_node->id());
   EXPECT_EQ(root.role, root_node->data().role);
 
@@ -467,7 +467,7 @@ TEST(AXTreeTest, TreeDelegateIsCalled) {
 
   ASSERT_EQ(true, fake_delegate.root_changed());
 
-  tree.SetDelegate(NULL);
+  tree.SetDelegate(nullptr);
 }
 
 TEST(AXTreeTest, TreeDelegateIsCalledForTreeDataChanges) {
@@ -504,7 +504,7 @@ TEST(AXTreeTest, TreeDelegateIsCalledForTreeDataChanges) {
   EXPECT_TRUE(fake_delegate.tree_data_changed());
   EXPECT_EQ("New Title", tree.data().title);
 
-  tree.SetDelegate(NULL);
+  tree.SetDelegate(nullptr);
 }
 
 TEST(AXTreeTest, ReparentingDoesNotTriggerNodeCreated) {
@@ -581,7 +581,7 @@ TEST(AXTreeTest, TreeDelegateIsNotCalledForReparenting) {
 
   ASSERT_EQ(true, fake_delegate.root_changed());
 
-  tree.SetDelegate(NULL);
+  tree.SetDelegate(nullptr);
 }
 
 // UAF caught by ax_tree_fuzzer
@@ -661,7 +661,7 @@ TEST(AXTreeTest, RoleAndStateChangeCallbacks) {
   EXPECT_EQ("visited changed to true", change_log[1]);
   EXPECT_EQ("checkedState changed from 2 to 1", change_log[2]);
 
-  tree.SetDelegate(NULL);
+  tree.SetDelegate(nullptr);
 }
 
 TEST(AXTreeTest, AttributeChangeCallbacks) {
@@ -740,7 +740,7 @@ TEST(AXTreeTest, AttributeChangeCallbacks) {
   EXPECT_EQ("scrollX changed from 6 to 7", change_log2[8]);
   EXPECT_EQ("scrollXMax changed from 0 to 10", change_log2[9]);
 
-  tree.SetDelegate(NULL);
+  tree.SetDelegate(nullptr);
 }
 
 TEST(AXTreeTest, IntListChangeCallbacks) {
@@ -799,7 +799,7 @@ TEST(AXTreeTest, IntListChangeCallbacks) {
   EXPECT_EQ("radioGroupIds changed from 3 to 2,2", change_log2[1]);
   EXPECT_EQ("flowtoIds changed from  to 3", change_log2[2]);
 
-  tree.SetDelegate(NULL);
+  tree.SetDelegate(nullptr);
 }
 
 // Create a very simple tree and make sure that we can get the bounds of
