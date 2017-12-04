571d7861f57dbb79b570ec3808a7b172dcc462cf
John Z Wu <jzw@chromium.org>
2017-12-04 11:24:56 -0800

    Remove IOSSigninClient and have IOSWebViewSigninClient and IOSChromeSigninClient directly inherit from SigninClient.
    Partial implementations are not preferred when sharing code between
    //ios/chrome and //ios/web_view. Instead, convert to using helper
    classes (using composition) wherever possible.
    
    Introduced DelayCallbackHelper to handle saving callbacks and
    firing them later after regaining connection.
    
    Bug:774517
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I0fa0ad558ee518b0eefb110f34f9480d69b59a6a

--- ios/chrome/browser/signin/ios_chrome_signin_client.h
+++ ios/chrome/browser/signin/ios_chrome_signin_client.h
@@ -5,14 +5,23 @@
 #ifndef IOS_CHROME_BROWSER_SIGNIN_IOS_CHROME_SIGNIN_CLIENT_H_
 #define IOS_CHROME_BROWSER_SIGNIN_IOS_CHROME_SIGNIN_CLIENT_H_
 
-#include "components/signin/ios/browser/ios_signin_client.h"
+#include <memory>
+
+#include "base/macros.h"
+#include "components/content_settings/core/browser/cookie_settings.h"
+#include "components/content_settings/core/browser/host_content_settings_map.h"
+#include "components/signin/core/browser/signin_client.h"
+#include "components/signin/core/browser/signin_error_controller.h"
+#include "components/signin/ios/browser/delay_callback_helper.h"
+#include "net/url_request/url_request_context_getter.h"
 
 namespace ios {
 class ChromeBrowserState;
 }
 
-// Concrete implementation of IOSSigninClient for //ios/chrome.
-class IOSChromeSigninClient : public IOSSigninClient {
+// Concrete implementation of SigninClient for //ios/chrome.
+class IOSChromeSigninClient : public SigninClient,
+                              public SigninErrorController::Observer {
  public:
   IOSChromeSigninClient(
       ios::ChromeBrowserState* browser_state,
@@ -20,7 +29,10 @@ class IOSChromeSigninClient : public IOSSigninClient {
       scoped_refptr<content_settings::CookieSettings> cookie_settings,
       scoped_refptr<HostContentSettingsMap> host_content_settings_map,
       scoped_refptr<TokenWebData> token_web_data);
-  ~IOSChromeSigninClient() override = default;
+  ~IOSChromeSigninClient() override;
+
+  // KeyedService implementation.
+  void Shutdown() override;
 
   // SigninClient implementation.
   base::Time GetInstallDate() override;
@@ -34,6 +46,24 @@ class IOSChromeSigninClient : public IOSSigninClient {
       const std::string& source,
       net::URLRequestContextGetter* getter) override;
   void PreGaiaLogout(base::OnceClosure callback) override;
+  scoped_refptr<TokenWebData> GetDatabase() override;
+  PrefService* GetPrefs() override;
+  net::URLRequestContextGetter* GetURLRequestContext() override;
+  void DoFinalInit() override;
+  bool CanRevokeCredentials() override;
+  std::string GetSigninScopedDeviceId() override;
+  bool ShouldMergeSigninCredentialsIntoCookieJar() override;
+  bool IsFirstRun() const override;
+  bool AreSigninCookiesAllowed() override;
+  void AddContentSettingsObserver(
+      content_settings::Observer* observer) override;
+  void RemoveContentSettingsObserver(
+      content_settings::Observer* observer) override;
+  std::unique_ptr<CookieChangedSubscription> AddCookieChangedCallback(
+      const GURL& url,
+      const std::string& name,
+      const net::CookieStore::CookieChangedCallback& callback) override;
+  void DelayNetworkCall(const base::Closure& callback) override;
 
   // SigninErrorController::Observer implementation.
   void OnErrorChanged() override;
@@ -42,8 +72,13 @@ class IOSChromeSigninClient : public IOSSigninClient {
   // SigninClient private implementation.
   void OnSignedOut() override;
 
+  std::unique_ptr<DelayCallbackHelper> delay_callback_helper_;
   ios::ChromeBrowserState* browser_state_;
   SigninErrorController* signin_error_controller_;
+  scoped_refptr<content_settings::CookieSettings> cookie_settings_;
+  scoped_refptr<HostContentSettingsMap> host_content_settings_map_;
+  scoped_refptr<TokenWebData> token_web_data_;
+
   DISALLOW_COPY_AND_ASSIGN(IOSChromeSigninClient);
 };
 
