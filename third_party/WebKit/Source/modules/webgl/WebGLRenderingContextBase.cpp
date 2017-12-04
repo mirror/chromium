fa5f9cd244f9ff3d8810b62522717bee2641d556
Hiroshige Hayashizaki <hiroshige@chromium.org>
2017-12-03 16:57:43 -0800

    Make most of SecurityOrigin references const
    This CL only adds |const| to SecurityOrigin-related types and thus
    doesn't change the behavior.
    
    This CL replaces most of
    - WebPrivatePtr<SecurityOrigin> -> WebPrivatePtr<const SecurityOrigin>
    - scoped_refptr<SecurityOrigin> -> scoped_refptr<const SecurityOrigin>
    - SecurityOrigin* -> const SecurityOrigin*
    - SecurityOrigin& -> const SecurityOrigin&
    
    Most of the modifications are done mechanically by PCRE:
    s/(WebPrivatePtr<|scoped_refptr<|((?<!const)\s+)|\(|^)((::)?blink::)?SecurityOrigin([&\*>])/$1const $3SecurityOrigin$5/g
    
    Except for the following exceptions:
    
    [Memo for reviewers]
    PatchSet 15 has all mechanical changes applied, and
    I removed some |const| in PatchSet 16.
    See diffs between PatchSet 15 and PatchSet 16 for the list of exceptions
    for constifying.
    I'll update the commit message with the list of exceptions later.
    
    Bug: 779730
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie4a1a5cc2add20c1c1b9d0d6d5dff9ea9ec46240

--- third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.cpp
+++ third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.cpp
@@ -4850,7 +4850,7 @@ void WebGLRenderingContextBase::texImage2D(GLenum target,
 }
 
 void WebGLRenderingContextBase::TexImageHelperHTMLImageElement(
-    SecurityOrigin* security_origin,
+    const SecurityOrigin* security_origin,
     TexImageFunctionID function_id,
     GLenum target,
     GLint level,
@@ -5086,7 +5086,7 @@ void WebGLRenderingContextBase::TexImageByGPU(
 }
 
 void WebGLRenderingContextBase::TexImageHelperHTMLCanvasElement(
-    SecurityOrigin* security_origin,
+    const SecurityOrigin* security_origin,
     TexImageFunctionID function_id,
     GLenum target,
     GLint level,
@@ -5221,7 +5221,7 @@ scoped_refptr<Image> WebGLRenderingContextBase::VideoFrameToImage(
 }
 
 void WebGLRenderingContextBase::TexImageHelperHTMLVideoElement(
-    SecurityOrigin* security_origin,
+    const SecurityOrigin* security_origin,
     TexImageFunctionID function_id,
     GLenum target,
     GLint level,
@@ -7357,7 +7357,7 @@ void WebGLRenderingContextBase::RemoveBoundBuffer(WebGLBuffer* buffer) {
 }
 
 bool WebGLRenderingContextBase::ValidateHTMLImageElement(
-    SecurityOrigin* security_origin,
+    const SecurityOrigin* security_origin,
     const char* function_name,
     HTMLImageElement* image,
     ExceptionState& exception_state) {
@@ -7381,7 +7381,7 @@ bool WebGLRenderingContextBase::ValidateHTMLImageElement(
 }
 
 bool WebGLRenderingContextBase::ValidateHTMLCanvasElement(
-    SecurityOrigin* security_origin,
+    const SecurityOrigin* security_origin,
     const char* function_name,
     HTMLCanvasElement* canvas,
     ExceptionState& exception_state) {
@@ -7398,7 +7398,7 @@ bool WebGLRenderingContextBase::ValidateHTMLCanvasElement(
 }
 
 bool WebGLRenderingContextBase::ValidateHTMLVideoElement(
-    SecurityOrigin* security_origin,
+    const SecurityOrigin* security_origin,
     const char* function_name,
     HTMLVideoElement* video,
     ExceptionState& exception_state) {
