d853d540733ba14b9abc848e3983cc16898a53ef
Daniel Bratell <bratell@opera.com>
2017-12-04 17:42:01 +0100

    Adding jumbo support for content_unittests (~50-70 CPU minutes)
    content_unittests is a chunk of code that is slow to compile and slow
    to link so it benefits a lot from being jumbified.
    
    There are a lot of symbols reused between testcases so the simplest
    way to isloate them is to give colliding testcases their own custom
    namespaces. This has turned out to be simple and low maintenance and works
    because tests don't export any symbols.
    
    Bug: 746953
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Ic5c06687ce2c0397c5bff7b68fce7b5a34fa8e79

--- content/renderer/device_sensors/device_orientation_event_pump.h
+++ content/renderer/device_sensors/device_orientation_event_pump.h
@@ -11,6 +11,11 @@
 #include "third_party/WebKit/public/platform/modules/device_orientation/WebDeviceOrientationListener.h"
 
 namespace content {
+namespace device_orientation_event_pump_unittest {
+class DeviceOrientationEventPumpTest;
+FORWARD_DECLARE_TEST(DeviceOrientationEventPumpTest,
+                     SensorInitializedButItsSharedBufferIsNot);
+}  // namespace device_orientation_event_pump_unittest
 
 class RenderThread;
 
@@ -38,8 +43,9 @@ class CONTENT_EXPORT DeviceOrientationEventPump
   SensorEntry absolute_orientation_sensor_;
 
  private:
-  FRIEND_TEST_ALL_PREFIXES(DeviceOrientationEventPumpTest,
-                           SensorInitializedButItsSharedBufferIsNot);
+  FRIEND_TEST_ALL_PREFIXES(
+      device_orientation_event_pump_unittest::DeviceOrientationEventPumpTest,
+      SensorInitializedButItsSharedBufferIsNot);
 
   // DeviceSensorEventPump:
   bool SensorSharedBuffersReady() const override;
