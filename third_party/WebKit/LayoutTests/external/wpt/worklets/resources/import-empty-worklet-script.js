ad29115435130e3a0f507ca4e21b151baa7b09a1
Hiroki Nakagawa <nhiroki@chromium.org>
2017-12-04 15:13:20 +0900

    Worklet: Specify "Allow-Access-Control-Origin" header by *.js.headers files in WPT
    This is a clean-up CL. This specifies "Allow-Access-Control-Origin" header by
    *.js.headers files instead of "pipe=header()" query parameters for
    simplification.
    
    Bug: n/a
    Change-Id: Ia18ea8a1e00ea6854f69771d911bf3e4d88379d2

--- third_party/WebKit/LayoutTests/external/wpt/worklets/resources/import-empty-worklet-script.js
+++ third_party/WebKit/LayoutTests/external/wpt/worklets/resources/import-empty-worklet-script.js
@@ -1,3 +1,3 @@
-// This script can be imported as a remote-origin script, so the
-// Access-Control-Allow-Origin is specified here.
-import './empty-worklet-script.js?pipe=header(Access-Control-Allow-Origin, *)';
+// This script can be imported as a remote-origin script, so import the empty
+// script with the CORS header here.
+import './empty-worklet-script-with-cors-header.js';
