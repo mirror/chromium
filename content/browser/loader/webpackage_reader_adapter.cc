a7909975fcbc86219ff5e6562d940893bea0f44a
Kinuko Yasuda <kinuko@chromium.org>
2017-12-04 10:08:18 +0900

    [DO NOT COMMIT] WebPackage loader [streaming]
    --enable-webpackage-streaming enable streaming
    
    Bug:
    Change-Id: I58836230522c68faa277ab77a06d665648b90e88

--- content/browser/loader/webpackage_reader_adapter.cc
+++ content/browser/loader/webpackage_reader_adapter.cc
@@ -13,12 +13,7 @@
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "content/public/common/resource_request.h"
-#include "net/base/io_buffer.h"
-#include "net/filter/filter_source_stream.h"
-#include "net/filter/gzip_source_stream.h"
-#include "net/filter/source_stream.h"
 #include "net/http/http_util.h"
-#include "services/network/public/cpp/net_adapters.h"
 
 namespace content {
 
@@ -31,316 +26,43 @@ base::StringPiece MapGetAsStringPiece(Map& m, const Key& key) {
   return base::StringPiece(found->second.c_str(), found->second.size());
 }
 
-class WebPackageSourceStream : public net::SourceStream {
+class PushObserverCallbackWrapper {
  public:
-  explicit WebPackageSourceStream(WebPackageResponseHandler* handler)
-      : SourceStream(SourceStream::TYPE_NONE), handler_(handler) {}
-  ~WebPackageSourceStream() override {}
-
-  int Read(net::IOBuffer* dest_buffer,
-           int buffer_size,
-           const net::CompletionCallback& callback) override {
-    const char* src_buffer = nullptr;
-    size_t src_size = 0;
-    handler_->GetCurrentReadBuffer(&src_buffer, &src_size);
-    size_t size = std::min(src_size, base::checked_cast<size_t>(buffer_size));
-    memcpy(dest_buffer->data(), src_buffer, size);
-    handler_->UpdateConsumedReadSize(size);
-    return size;
+  PushObserverCallbackWrapper(
+      base::WeakPtr<WebPackageReaderAdapter::PushObserver> observer,
+      const GURL& request_url,
+      const std::string& method)
+      : observer_(std::move(observer)),
+        request_url_(request_url),
+        method_(method) {}
+  void OnResponse(int error_code,
+                  const ResourceResponseHead& response_head,
+                  mojo::ScopedDataPipeConsumerHandle body) {
+    if (!observer_)
+      return;
+    DCHECK(!completion_callback_);
+    observer_->OnResponse(request_url_, method_, response_head, std::move(body),
+                          &completion_callback_);
   }
-  std::string Description() const override { return "data pipe"; }
+  void OnComplete(const network::URLLoaderCompletionStatus& status) {
+    if (!observer_)
+      return;
+    DCHECK(completion_callback_);
+    std::move(completion_callback_).Run(status);
+  }
 
  private:
-  // Not owned, handler owns this.
-  WebPackageResponseHandler* handler_ = nullptr;
+  base::WeakPtr<WebPackageReaderAdapter::PushObserver> observer_;
+  const GURL& request_url_;
+  const std::string& method_;
+  WebPackageReaderAdapter::CompletionCallback completion_callback_;
+  DISALLOW_COPY_AND_ASSIGN(PushObserverCallbackWrapper);
 };
 
 }  // namespace
 
 WebPackageManifest::WebPackageManifest() = default;
 WebPackageManifest::~WebPackageManifest() = default;
-
-//----------------------------------------------------------------
-
-WebPackageResponseHandler::WebPackageResponseHandler(
-    const GURL& request_url,
-    ResourceResponseHead head,
-    base::OnceClosure done_callback)
-    : request_url_(request_url),
-      response_head_(std::move(head)),
-      writable_handle_watcher_(std::make_unique<mojo::SimpleWatcher>(
-          FROM_HERE,
-          mojo::SimpleWatcher::ArmingPolicy::MANUAL)),
-      done_callback_(std::move(done_callback)) {
-  LOG(ERROR) << "[" << request_url_ << "] Created ResponseHandler.";
-
-  source_stream_ = std::make_unique<WebPackageSourceStream>(this);
-  std::string type;
-  size_t iter = 0;
-  while (response_head_.headers->EnumerateHeader(&iter, "Content-Encoding",
-                                                 &type)) {
-    net::SourceStream::SourceType source_type =
-        net::FilterSourceStream::ParseEncodingType(type);
-    if (source_type == net::SourceStream::TYPE_DEFLATE ||
-        source_type == net::SourceStream::TYPE_GZIP) {
-      source_stream_ =
-          net::GzipSourceStream::Create(std::move(source_stream_), source_type);
-    } else {
-      // TODO: Maybe we could possibly support TYPE_BROTLI etc.
-      NOTREACHED();
-    }
-  }
-}
-
-WebPackageResponseHandler::~WebPackageResponseHandler() = default;
-
-void WebPackageResponseHandler::AddCallbacks(
-    ResourceCallback callback,
-    CompletionCallback completion_callback) {
-  DCHECK(!completion_callback_);
-  DCHECK(!producer_handle_.is_valid());
-
-  mojo::DataPipe data_pipe(512 * 1024);
-  producer_handle_ = std::move(data_pipe.producer_handle);
-  writable_handle_watcher_->Watch(
-      producer_handle_.get(), MOJO_HANDLE_SIGNAL_WRITABLE,
-      base::Bind(&WebPackageResponseHandler::OnPipeWritable,
-                 base::Unretained(this)));
-  if (!read_buffers_.empty())
-    writable_handle_watcher_->ArmOrNotify();
-
-  completion_callback_ = std::move(completion_callback);
-  std::move(callback).Run(net::OK, response_head_,
-                          std::move(data_pipe.consumer_handle));
-}
-
-void WebPackageResponseHandler::OnDataReceived(const void* data, size_t size) {
-  size_t consumed = 0;
-  LOG(ERROR) << "[" << request_url_ << "] OnDataReceived:" << size;
-
-  if (producer_handle_.is_valid()) {
-    uint32_t available = 0;
-    scoped_refptr<network::NetToMojoPendingBuffer> pending_write;
-    auto result = BeginWriteToPipe(&pending_write, &available);
-    if (available > 0u && !data_write_finished_) {
-      DCHECK_EQ(MOJO_RESULT_OK, result);
-      size_t dest_size = base::checked_cast<size_t>(available);
-      consumed = size;
-      WriteToPipe(pending_write.get(), 0, &dest_size, data, &consumed);
-      LOG(ERROR) << "- consumed / written to the pipe: " << size;
-      producer_handle_ = pending_write->Complete(dest_size);
-    }
-    DCHECK(producer_handle_.is_valid());
-  }
-
-  // Drop data if the pipe is closed.
-  if (data_write_finished_)
-    return;
-
-  CHECK_LE(consumed, size);
-  size_t remaining = size - consumed;
-  if (remaining == 0)
-    return;
-
-  auto buffer = base::MakeRefCounted<net::IOBuffer>(remaining);
-  read_buffers_.push_back(
-      base::MakeRefCounted<net::DrainableIOBuffer>(buffer.get(), remaining));
-  memcpy(buffer->data(), static_cast<const char*>(data) + consumed, remaining);
-  LOG(ERROR) << "- saving to the buffer: " << remaining;
-
-  if (producer_handle_.is_valid())
-    writable_handle_watcher_->ArmOrNotify();
-}
-
-void WebPackageResponseHandler::OnNotifyFinished(int error_code) {
-  LOG(ERROR) << "[" << request_url_ << "] OnNotifyFinished - data finished.";
-  if (error_code_ == net::OK)
-    error_code_ = error_code;
-  data_receive_finished_ = true;
-  if (read_buffers_.empty() && !source_stream_may_have_data_)
-    data_write_finished_ = true;
-  MaybeCompleteAndNotify();
-}
-
-void WebPackageResponseHandler::GetCurrentReadBuffer(const char** buf,
-                                                     size_t* size) {
-  DCHECK(current_read_pointer_ || current_read_size_ == 0);
-  *buf = current_read_pointer_;
-  *size = current_read_size_;
-}
-
-void WebPackageResponseHandler::UpdateConsumedReadSize(size_t size) {
-  consumed_read_size_ = size;
-}
-
-void WebPackageResponseHandler::OnPipeWritable(MojoResult mojo_result) {
-  if (mojo_result == MOJO_RESULT_FAILED_PRECONDITION) {
-    LOG(ERROR) << "Canceled? " << request_url_;
-    OnNotifyFinished(net::ERR_ABORTED);
-    return;
-  }
-
-  DCHECK_EQ(mojo_result, MOJO_RESULT_OK) << request_url_;
-  DCHECK(producer_handle_.is_valid());
-  DCHECK(!data_write_finished_);
-  if (read_buffers_.empty() && !source_stream_may_have_data_)
-    return;
-
-  uint32_t available = 0;
-  scoped_refptr<network::NetToMojoPendingBuffer> pending_write;
-  auto result = BeginWriteToPipe(&pending_write, &available);
-  if (available == 0u || data_write_finished_)
-    return;
-  DCHECK_EQ(MOJO_RESULT_OK, result);
-
-  size_t dest_offset = 0;
-  size_t dest_buffer_size = base::checked_cast<size_t>(available);
-  while (!read_buffers_.empty() && dest_buffer_size - dest_offset > 0) {
-    auto& src = read_buffers_.front();
-    size_t src_size = base::checked_cast<size_t>(src->BytesRemaining());
-    size_t dest_size = dest_buffer_size - dest_offset;
-    int ret = WriteToPipe(pending_write.get(), dest_offset, &dest_size,
-                          src->data(), &src_size);
-    if (ret != net::OK) {
-      LOG(ERROR) << "WriteToPipe error " << ret << "  " << request_url_;
-      return;
-    }
-    src->DidConsume(src_size);
-    dest_offset += dest_size;
-    if (src->BytesRemaining() > 0)
-      break;
-    read_buffers_.pop_front();
-  }
-
-  size_t dest_size = dest_buffer_size - dest_offset;
-  if (read_buffers_.empty() && dest_size > 0 && source_stream_may_have_data_) {
-    size_t src_size = 0;
-    int ret = WriteToPipe(pending_write.get(), dest_offset, &dest_size, nullptr,
-                          &src_size);
-    if (ret != net::OK) {
-      LOG(ERROR) << "WriteToPipe error " << ret << "  " << request_url_;
-      OnNotifyFinished(ret);
-      return;
-    }
-    if (dest_size == 0) {
-      source_stream_may_have_data_ = false;
-    }
-
-    dest_offset += dest_size;
-  }
-
-  producer_handle_ = pending_write->Complete(dest_offset);
-
-  if (read_buffers_.empty() && data_receive_finished_ &&
-      !source_stream_may_have_data_) {
-    data_write_finished_ = true;
-    MaybeCompleteAndNotify();
-    return;
-  }
-
-  writable_handle_watcher_->ArmOrNotify();
-}
-
-MojoResult WebPackageResponseHandler::BeginWriteToPipe(
-    scoped_refptr<network::NetToMojoPendingBuffer>* pending_buffer,
-    uint32_t* available) {
-  MojoResult result = network::NetToMojoPendingBuffer::BeginWrite(
-      &producer_handle_, pending_buffer, available);
-  if (result == MOJO_RESULT_SHOULD_WAIT) {
-    // The pipe is full.
-    writable_handle_watcher_->ArmOrNotify();
-  }
-  if (result != MOJO_RESULT_OK) {
-    data_write_finished_ = true;
-    writable_handle_watcher_->Cancel();
-    producer_handle_.reset();
-    error_code_ = net::ERR_FAILED;
-    LOG(ERROR) << "ERR_FAILED";
-    MaybeCompleteAndNotify();
-  }
-  return result;
-}
-
-int WebPackageResponseHandler::WriteToPipe(
-    network::NetToMojoPendingBuffer* dest_buffer,
-    size_t dest_offset,
-    size_t* dest_size_inout,
-    const void* src,
-    size_t* src_size_inout) {
-  DCHECK(!current_read_pointer_);
-
-  // TODO(kinuko):
-  // Read/write buffer handling is a bit ugly here, PendingBuffer should
-  // probably support changing the offset like DrainableIOBuffer.
-  size_t dest_size = *dest_size_inout;
-  size_t src_size = *src_size_inout;
-  size_t read = 0, written = 0;
-  while (dest_size > written &&
-         (src_size > read || (source_stream_may_have_data_ && src_size == 0))) {
-    current_read_pointer_ = static_cast<const char*>(src) + read;
-    current_read_size_ = src_size - read;
-    consumed_read_size_ = 0;
-    auto dest = base::MakeRefCounted<network::NetToMojoIOBuffer>(
-        dest_buffer, base::checked_cast<int>(dest_offset));
-    int rv = source_stream_->Read(dest.get(),
-                                  base::checked_cast<int>(dest_size - written),
-                                  net::CompletionCallback());
-    current_read_pointer_ = nullptr;
-    if (rv < 0) {
-      data_write_finished_ = true;
-      writable_handle_watcher_->Cancel();
-      producer_handle_.reset();
-      return rv;
-    }
-    if (rv == net::OK) {
-      source_stream_may_have_data_ = false;
-      break;
-    }
-    source_stream_may_have_data_ = true;
-    read += consumed_read_size_;
-    written += rv;
-    dest_offset += rv;
-  }
-  *dest_size_inout = written;
-  *src_size_inout = read;
-  return net::OK;
-}
-
-void WebPackageResponseHandler::MaybeCompleteAndNotify() {
-  if (!data_write_finished_ || !data_receive_finished_)
-    return;
-  LOG(ERROR) << "- Finished: " << request_url_;
-  writable_handle_watcher_.reset();
-  producer_handle_.reset();
-  if (completion_callback_) {
-    std::move(completion_callback_)
-        .Run(network::URLLoaderCompletionStatus(error_code_));
-  }
-  // May delete this.
-  std::move(done_callback_).Run();
-}
-
-//--------------------------------------------------------------------
-
-WebPackageReaderAdapter::PendingCallbacks::PendingCallbacks(
-    ResourceCallback callback,
-    CompletionCallback completion_callback)
-    : callback(std::move(callback)),
-      completion_callback(std::move(completion_callback)) {}
-
-WebPackageReaderAdapter::PendingCallbacks::~PendingCallbacks() = default;
-
-WebPackageReaderAdapter::PendingCallbacks::PendingCallbacks(
-    PendingCallbacks&& other) {
-  *this = std::move(other);
-}
-
-WebPackageReaderAdapter::PendingCallbacks&
-WebPackageReaderAdapter::PendingCallbacks::operator=(PendingCallbacks&& other) {
-  callback = std::move(other.callback);
-  completion_callback = std::move(other.completion_callback);
-  return *this;
-}
 
 //--------------------------------------------------------------------
 
@@ -363,12 +85,13 @@ WebPackageReaderAdapter::~WebPackageReaderAdapter() = default;
 void WebPackageReaderAdapter::GetFirstResource(
     ResourceCallback callback,
     CompletionCallback completion_callback) {
-  LOG(ERROR) << "** GetFirstResource";
+  LOG(INFO) << "** GetFirstResource";
+  DCHECK_EQ(kPull, fetch_mode_);
   if (state_ < kResponseStarted) {
     DCHECK(!pending_first_request_);
     pending_first_request_ =
         PendingCallbacks(std::move(callback), std::move(completion_callback));
-    LOG(ERROR) << "- Waiting for the first request.";
+    LOG(INFO) << "- Waiting for the first request.";
     return;
   }
   DCHECK_NE(kInvalidRequestId, first_request_id_);
@@ -376,14 +99,15 @@ void WebPackageReaderAdapter::GetFirstResource(
   DCHECK(found != response_map_.end());
   found->second->AddCallbacks(std::move(callback),
                               std::move(completion_callback));
-  LOG(ERROR) << "- Added callback on the response_handler_.";
+  LOG(INFO) << "- Added callback on the response_handler_.";
 }
 
 void WebPackageReaderAdapter::GetResource(
     const ResourceRequest& resource_request,
     ResourceCallback callback,
     CompletionCallback completion_callback) {
-  LOG(ERROR) << "** GetResource: " << resource_request.url;
+  LOG(INFO) << "** GetResource: " << resource_request.url;
+  DCHECK_EQ(kPull, fetch_mode_);
   RequestURLAndMethod request;
   request.first = resource_request.url;
   request.second = resource_request.method;
@@ -409,6 +133,37 @@ void WebPackageReaderAdapter::GetResource(
       request,
       PendingCallbacks(std::move(callback), std::move(completion_callback)));
   DCHECK(inserted.second);
+}
+
+void WebPackageReaderAdapter::StartPushResources(
+    base::WeakPtr<PushObserver> observer) {
+  LOG(ERROR) << "--- StartPushResources!";
+
+  DCHECK(!push_observer_);
+  DCHECK(observer);
+  DCHECK_EQ(kPull, fetch_mode_);
+  fetch_mode_ = kPush;
+  push_observer_ = observer;
+
+  for (auto& request : request_map_) {
+    if (pending_request_map_.find(request.first) != pending_request_map_.end())
+      continue;
+    push_observer_->OnRequest(request.first.first, request.first.second);
+  }
+
+  for (auto& response : response_map_) {
+    auto& handler = response.second;
+    if (handler->has_callback())
+      continue;
+    LOG(INFO) << "** Pushing: " << handler->request_url();
+    auto callback_wrapper = std::make_unique<PushObserverCallbackWrapper>(
+        push_observer_, handler->request_url(), handler->request_method());
+    handler->AddCallbacks(
+        base::BindOnce(&PushObserverCallbackWrapper::OnResponse,
+                       base::Unretained(callback_wrapper.get())),
+        base::BindOnce(&PushObserverCallbackWrapper::OnComplete,
+                       std::move(callback_wrapper)));
+  }
 }
 
 void WebPackageReaderAdapter::OnReadable(MojoResult unused) {
@@ -449,8 +204,11 @@ void WebPackageReaderAdapter::OnReadable(MojoResult unused) {
 
 void WebPackageReaderAdapter::NotifyCompletion(int error_code) {
   reader_->ConsumeEOS();
+  handle_watcher_.Cancel();
+  handle_.reset();
   state_ = kFinished;
   main_handle_error_code_ = error_code;
+  AbortAllPendingRequests(error_code);
   client_->OnFinishedPackage();
 }
 
@@ -479,7 +237,7 @@ void WebPackageReaderAdapter::OnResourceRequest(
   url_strings.push_back(MapGetAsStringPiece(request_headers, ":path"));
   request.first = GURL(base::JoinString(url_strings, ""));
 
-  LOG(ERROR) << "** Reading request: " << request.first;
+  LOG(INFO) << "** Reading request: " << request.first;
 
   auto inserted = request_map_.emplace(request, request_id);
   // DCHECK(inserted.second);
@@ -490,6 +248,10 @@ void WebPackageReaderAdapter::OnResourceRequest(
 
   auto reverse_inserted = reverse_request_map_.emplace(request_id, request);
   DCHECK(reverse_inserted.second);
+
+  if (fetch_mode_ == kPush && push_observer_) {
+    push_observer_->OnRequest(request.first, request.second);
+  }
 }
 
 void WebPackageReaderAdapter::OnResourceResponse(
@@ -510,7 +272,7 @@ void WebPackageReaderAdapter::OnResourceResponse(
     return;
   }
   RequestURLAndMethod request = request_found->second;
-  LOG(ERROR) << "** Reading response: " << request.first;
+  LOG(INFO) << "** Reading response: " << request.first;
 
   ResourceResponseHead response_head;
   response_head.request_start = base::TimeTicks::Now();
@@ -542,7 +304,7 @@ void WebPackageReaderAdapter::OnResourceResponse(
   auto inserted = response_map_.emplace(
       request_id,
       std::make_unique<WebPackageResponseHandler>(
-          request.first, std::move(response_head),
+          request.first, request.second, std::move(response_head),
           base::BindOnce(&WebPackageReaderAdapter::OnResponseHandlerFinished,
                          base::Unretained(this), request_id)));
   DCHECK(inserted.second);
@@ -550,7 +312,7 @@ void WebPackageReaderAdapter::OnResourceResponse(
 
   auto pending_found = pending_request_map_.find(request);
   if (pending_found != pending_request_map_.end()) {
-    LOG(ERROR) << "** pending request -- found: " << request.first;
+    LOG(INFO) << "** pending request -- found: " << request.first;
     handler->AddCallbacks(std::move(pending_found->second.callback),
                           std::move(pending_found->second.completion_callback));
     pending_request_map_.erase(pending_found);
@@ -558,12 +320,22 @@ void WebPackageReaderAdapter::OnResourceResponse(
   }
 
   if (pending_first_request_ && request_id == first_request_id_) {
-    LOG(ERROR) << "** first request -- found: " << request.first;
+    LOG(INFO) << "** first request -- found: " << request.first;
     handler->AddCallbacks(
         std::move(pending_first_request_->callback),
         std::move(pending_first_request_->completion_callback));
     pending_first_request_.reset();
     return;
+  }
+
+  if (fetch_mode_ == kPush && push_observer_) {
+    auto callback_wrapper = std::make_unique<PushObserverCallbackWrapper>(
+        push_observer_, request.first, request.second);
+    handler->AddCallbacks(
+        base::BindOnce(&PushObserverCallbackWrapper::OnResponse,
+                       base::Unretained(callback_wrapper.get())),
+        base::BindOnce(&PushObserverCallbackWrapper::OnComplete,
+                       std::move(callback_wrapper)));
   }
 }
 
