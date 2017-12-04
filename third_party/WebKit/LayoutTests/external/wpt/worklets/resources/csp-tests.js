a0b531dcbef3860a9aaace538c3d36b42329daa8
Hiroki Nakagawa <nhiroki@chromium.org>
2017-12-04 09:14:19 +0900

    WIP
    Bug:
    Change-Id: Ib6ddf6efaa7ce142000f36bdee25b2259bfc82be

--- third_party/WebKit/LayoutTests/external/wpt/worklets/resources/csp-tests.js
+++ third_party/WebKit/LayoutTests/external/wpt/worklets/resources/csp-tests.js
@@ -23,7 +23,7 @@ function openWindowAndExpectResult(windowURL, scriptURL, type, expectation) {
 // runContentSecurityPolicyTests("paint");
 function runContentSecurityPolicyTests(workletType) {
   const worklet = get_worklet(workletType);
-
+/*
   promise_test(t => {
     const kWindowURL =
         'resources/addmodule-window.html?pipe=header(' +
@@ -85,4 +85,44 @@ function runContentSecurityPolicyTests(workletType) {
   }, 'Importing a remote-origin worklet script should not be blocked by ' +
      'the worker-src directive because worklets obey the script-src ' +
      'directive.');
+*/
+
+  promise_test(t => {
+    const stashKey = token();
+    const kAggregatorURL = get_host_info().HTTPS_ORIGIN +
+                           '/worklets/resources/csp-report-aggregator.py';
+    const kWindowURL =
+        'resources/addmodule-window.html?pipe=header(' +
+        'Content-Security-Policy, script-src \'self\' \'unsafe-inline\'%3b ' +
+        'report-uri ' + kAggregatorURL + '?key=' + stashKey + ')';
+    const kScriptURL = 'eval-worklet-script.js';
+    return openWindowAndExpectResult(
+        kWindowURL, kScriptURL, workletType, 'RESOLVED')
+        .then(() => fetch(kAggregatorURL + '?key=' + stashKey))
+        .then(response => response.json())
+        .then(result => {
+            const directive = result['csp-report']['violated-directive'];
+            assert_true(directive.indexOf('script-src') != -1);
+        });
+  }, 'Importing a worklet script that calls eval() should be blocked because ' +
+     'the script-src unsafe-eval directive is not specified.');
+
+  promise_test(t => {
+    const stashKey = token();
+    const kAggregatorURL = get_host_info().HTTPS_ORIGIN +
+                           '/worklets/resources/csp-report-aggregator.py';
+    const kWindowURL =
+        'resources/addmodule-window.html?pipe=header(' +
+        'Content-Security-Policy, script-src \'self\' \'unsafe-inline\' ' +
+        '\'unsafe-eval\'%3b report-uri ' + kAggregatorURL + '?key=' + stashKey + ')';
+    const kScriptURL = 'eval-worklet-script.js';
+    return openWindowAndExpectResult(
+        kWindowURL, kScriptURL, workletType, 'RESOLVED')
+        .then(() => fetch(kAggregatorURL + '?key=' + stashKey))
+        .then(response => response.text())
+        .then(result => assert_true(result == ""));
+  }, 'Importing a worklet script that calls eval() should not be blocked ' +
+     'because the script-src unsafe-eval directive allows it.');
+
+  promise_test(() => assert_true(false));
 }
