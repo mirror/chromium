fb607ca4be30b7bf28fb72eb0f4ce37d714daa72
Elad Alon <eladalon@chromium.org>
2017-12-04 19:12:01 +0100

    Remove WebRTCEventLogHost
    Remove WebRTCEventLogHost, letting WebRtcEventLogManager take over its
    responsibilities.
    This paves the way to allowing WebRtcEventLogManager to turn off RTC
    event notifications from WebRTC when it doesn't need them, which will
    conserve CPU.
    
    Bug: 775415
    Change-Id: Id0f6b62fb2425fe7e36ba5126943580d94a017e9

--- content/browser/webrtc/webrtc_event_log_manager_unittest.cc
+++ content/browser/webrtc/webrtc_event_log_manager_unittest.cc
@@ -22,6 +22,7 @@
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "content/public/test/test_browser_thread_bundle.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 #if defined(OS_WIN)
@@ -32,13 +33,68 @@
 
 namespace content {
 
+using ::testing::_;
+using ::testing::NiceMock;
+using PeerConnectionKey = WebRtcEventLogManager::PeerConnectionKey;
+
 namespace {
-struct PeerConnectionKey {
-  int render_process_id;
-  int lid;  // Renderer-local PeerConnection ID.
+
+class MockLocalLogsObserver : public WebRtcEventLogManager::LocalLogsObserver {
+ public:
+  ~MockLocalLogsObserver() override = default;
+  MOCK_METHOD2(OnLocalLogsStarted, void(PeerConnectionKey, base::FilePath));
+  MOCK_METHOD1(OnLocalLogsStopped, void(PeerConnectionKey));
 };
 
-enum class ExpectedResult : bool { kFailure = false, kSuccess = true };
+//// Remembers the last call. Expects no multiple calls without reads in
+/// between, / to prevent the unit tests from missing anything.
+// class MemorizingLocalLogsObserver
+//    : public WebRtcEventLogManager::LocalLogsObserver {
+// public:
+//  MemorizingLocalLogsObserver() : last_call_(LastCall::NoCall) {}
+//  ~MemorizingLocalLogsObserver() override = default;
+//
+//  void OnLocalLogsStarted(
+//      const std::map<PeerConnectionKey, base::FilePath>& local_logs) override
+//      {
+//    EXPECT_EQ(last_call_, LastCall::NoCall);
+//    last_call_ = LastCall::Started;
+//    started_local_logs_ = local_logs;
+//  }
+//
+//  void OnLocalLogsStopped(
+//      const std::set<PeerConnectionKey>& local_logs) override {
+//    EXPECT_EQ(last_call_, LastCall::NoCall);
+//    last_call_ = LastCall::Stopped;
+//    stopped_local_logs_ = local_logs;
+//  }
+//
+//  void Clear() {
+//    last_call_ = LastCall::NoCall;
+//    started_local_logs_.clear();
+//    stopped_local_logs_.clear();
+//  }
+//
+//  std::map<PeerConnectionKey, base::FilePath> GetStartedLogs() {
+//    EXPECT_EQ(last_call_, LastCall::Started);
+//    auto result = started_local_logs_;
+//    Clear();
+//    return result;
+//  }
+//
+//  std::set<PeerConnectionKey> GetStoppedLogs() {
+//    EXPECT_EQ(last_call_, LastCall::Stopped);
+//    auto result = stopped_local_logs_;
+//    Clear();
+//    return result;
+//  }
+//
+// protected:
+//  enum class LastCall { NoCall, Started, Stopped } last_call_;
+//  std::map<PeerConnectionKey, base::FilePath> started_local_logs_;
+//  std::set<PeerConnectionKey> stopped_local_logs_;
+//};
+
 }  // namespace
 
 class WebRtcEventLogManagerTest : public ::testing::Test {
@@ -56,6 +112,7 @@ class WebRtcEventLogManagerTest : public ::testing::Test {
   }
 
   ~WebRtcEventLogManagerTest() override {
+    SetLocalLogsObserver(nullptr);
     DestroyUnitUnderTest();
     if (!base_dir_.empty()) {
       EXPECT_TRUE(base::DeleteFile(base_dir_, true));
@@ -69,65 +126,123 @@ class WebRtcEventLogManagerTest : public ::testing::Test {
     }
   }
 
+  //  // TODO: !!!
+  //  void ExpectBoolReply(bool expected_value, bool value) {
+  //    EXPECT_EQ(expected_value, value);
+  //    run_loop_->QuitWhenIdle();
+  //  }
+  //
+  //  // TODO: !!!
+  //  base::OnceCallback<void(bool)> ExpectBoolReplyClosure(bool expected_value)
+  //  {
+  //    return base::BindOnce(&WebRtcEventLogManagerTest::ExpectBoolReply,
+  //                          base::Unretained(this), expected_value);
+  //  }
+
   void WaitForReply() {
     run_loop_->Run();
     run_loop_.reset(new base::RunLoop);  // Allow re-blocking.
   }
 
-  void ExpectBoolReply(bool expected_value, bool value) {
-    EXPECT_EQ(expected_value, value);
+  void VoidReply() { run_loop_->QuitWhenIdle(); }
+
+  base::OnceCallback<void(void)> VoidReplyClosure() {
+    return base::BindOnce(&WebRtcEventLogManagerTest::VoidReply,
+                          base::Unretained(this));
+  }
+
+  void BoolReply(bool* output, bool value) {
+    *output = value;
     run_loop_->QuitWhenIdle();
   }
 
-  base::OnceCallback<void(bool)> ExpectBoolReplyClosure(bool expected_value) {
-    return base::BindOnce(&WebRtcEventLogManagerTest::ExpectBoolReply,
-                          base::Unretained(this), expected_value);
+  base::OnceCallback<void(bool)> BoolReplyClosure(bool* output) {
+    return base::BindOnce(&WebRtcEventLogManagerTest::BoolReply,
+                          base::Unretained(this), output);
   }
 
-  // With partial binding, we'll get a closure that will write the reply
-  // into a predefined destination. (Diverging from the style-guide by putting
-  // an output parameter first is necessary for partial binding.)
-  void OnFilePathReply(base::FilePath* out_path, base::FilePath value) {
-    *out_path = value;
-    run_loop_->QuitWhenIdle();
-  }
-
-  base::OnceCallback<void(base::FilePath)> FilePathReplyClosure(
-      base::FilePath* file_path) {
-    return base::BindOnce(&WebRtcEventLogManagerTest::OnFilePathReply,
-                          base::Unretained(this), file_path);
-  }
-
-  base::FilePath LocalWebRtcEventLogStart(int render_process_id,
-                                          int lid,
-                                          const base::FilePath& base_path,
-                                          size_t max_file_size) {
-    base::FilePath file_path;
-    manager_->LocalWebRtcEventLogStart(render_process_id, lid, base_path,
-                                       max_file_size,
-                                       FilePathReplyClosure(&file_path));
+  bool PeerConnectionAdded(int render_process_id, int lid) {
+    bool result;
+    manager_->PeerConnectionAdded(render_process_id, lid,
+                                  BoolReplyClosure(&result));
     WaitForReply();
-    return file_path;
+    return result;
   }
 
-  void LocalWebRtcEventLogStop(int render_process_id,
-                               int lid,
-                               ExpectedResult expected_result) {
-    const bool expected_result_bool = static_cast<bool>(expected_result);
-    manager_->LocalWebRtcEventLogStop(
-        render_process_id, lid, ExpectBoolReplyClosure(expected_result_bool));
+  bool PeerConnectionRemoved(int render_process_id, int lid) {
+    bool result;
+    manager_->PeerConnectionRemoved(render_process_id, lid,
+                                    BoolReplyClosure(&result));
+    WaitForReply();
+    return result;
+  }
+
+  bool EnableLocalLogging() {
+    bool result;
+    manager_->EnableLocalLogging(base_path_, BoolReplyClosure(&result));
+    WaitForReply();
+    return result;
+  }
+
+  bool DisableLocalLogging() {
+    bool result;
+    manager_->DisableLocalLogging(BoolReplyClosure(&result));
+    WaitForReply();
+    return result;
+  }
+
+  void SetLocalLogsObserver(
+      WebRtcEventLogManager::LocalLogsObserver* observer) {
+    manager_->SetLocalLogsObserver(observer, VoidReplyClosure());
     WaitForReply();
   }
 
-  void OnWebRtcEventLogWrite(int render_process_id,
+  //  // TODO: !!!
+  //  // With partial binding, we'll get a closure that will write the reply
+  //  // into a predefined destination. (Diverging from the style-guide by
+  //  putting
+  //  // an output parameter first is necessary for partial binding.)
+  //  void OnFilePathReply(base::FilePath* out_path, base::FilePath value) {
+  //    *out_path = value;
+  //    run_loop_->QuitWhenIdle();
+  //  }
+  //
+  //  base::OnceCallback<void(base::FilePath)> FilePathReplyClosure(
+  //      base::FilePath* file_path) {
+  //    return base::BindOnce(&WebRtcEventLogManagerTest::OnFilePathReply,
+  //                          base::Unretained(this), file_path);
+  //  }
+  //
+  //  base::FilePath LocalWebRtcEventLogStart(int render_process_id,
+  //                                          int lid,
+  //                                          const base::FilePath& base_path,
+  //                                          size_t max_file_size) {
+  //    base::FilePath file_path;
+  //    manager_->LocalWebRtcEventLogStart(render_process_id, lid, base_path,
+  //                                       max_file_size,
+  //                                       FilePathReplyClosure(&file_path));
+  //    WaitForReply();
+  //    return file_path;
+  //  }
+  //
+  //  void LocalWebRtcEventLogStop(int render_process_id,
+  //                               int lid,
+  //                               ExpectedResult expected_result) {
+  //    const bool expected_result_bool = static_cast<bool>(expected_result);
+  //    manager_->LocalWebRtcEventLogStop(
+  //        render_process_id, lid,
+  //        ExpectBoolReplyClosure(expected_result_bool));
+  //    WaitForReply();
+  //  }
+
+  bool OnWebRtcEventLogWrite(int render_process_id,
                              int lid,
-                             const std::string& output,
-                             ExpectedResult expected_result) {
-    bool expected_result_bool = static_cast<bool>(expected_result);
-    manager_->OnWebRtcEventLogWrite(
-        render_process_id, lid, output,
-        ExpectBoolReplyClosure(expected_result_bool));
+                             const std::string& output) {
+    bool result;
+    manager_->OnWebRtcEventLogWrite(render_process_id, lid, output,
+                                    BoolReplyClosure(&result));
     WaitForReply();
+    return result;
   }
 
   void FreezeClockAt(const base::Time::Exploded& frozen_time_exploded) {
@@ -155,6 +270,112 @@ class WebRtcEventLogManagerTest : public ::testing::Test {
   base::FilePath base_path_;  // base_dir_ +  log files' name prefix.
 };
 
+TEST_F(WebRtcEventLogManagerTest, LocalLogPeerConnectionAddedReturnsTrue) {
+  EXPECT_TRUE(PeerConnectionAdded(kRenderProcessId, kLocalPeerConnectionId));
+}
+
+TEST_F(WebRtcEventLogManagerTest,
+       LocalLogPeerConnectionAddedReturnsFalseIfAlreadyAdded) {
+  ASSERT_TRUE(PeerConnectionAdded(kRenderProcessId, kLocalPeerConnectionId));
+  EXPECT_FALSE(PeerConnectionAdded(kRenderProcessId, kLocalPeerConnectionId));
+}
+
+TEST_F(WebRtcEventLogManagerTest, LocalLogPeerConnectionRemovedReturnsTrue) {
+  ASSERT_TRUE(PeerConnectionAdded(kRenderProcessId, kLocalPeerConnectionId));
+  EXPECT_TRUE(PeerConnectionRemoved(kRenderProcessId, kLocalPeerConnectionId));
+}
+
+TEST_F(WebRtcEventLogManagerTest,
+       LocalLogPeerConnectionRemovedReturnsFalseIfNeverAdded) {
+  EXPECT_FALSE(PeerConnectionRemoved(kRenderProcessId, kLocalPeerConnectionId));
+}
+
+TEST_F(WebRtcEventLogManagerTest,
+       LocalLogPeerConnectionRemovedReturnsFalseIfAlreadyRemoved) {
+  ASSERT_TRUE(PeerConnectionAdded(kRenderProcessId, kLocalPeerConnectionId));
+  ASSERT_TRUE(PeerConnectionRemoved(kRenderProcessId, kLocalPeerConnectionId));
+  EXPECT_FALSE(PeerConnectionRemoved(kRenderProcessId, kLocalPeerConnectionId));
+}
+
+TEST_F(WebRtcEventLogManagerTest, LocalLogEnableLocalLoggingReturnsTrue) {
+  EXPECT_TRUE(EnableLocalLogging());
+}
+
+TEST_F(WebRtcEventLogManagerTest,
+       LocalLogEnableLocalLoggingReturnsFalseIfCalledWhenAlreadyEnabled) {
+  ASSERT_TRUE(EnableLocalLogging());
+  EXPECT_FALSE(EnableLocalLogging());
+}
+
+TEST_F(WebRtcEventLogManagerTest, LocalLogDisableLocalLoggingReturnsTrue) {
+  ASSERT_TRUE(EnableLocalLogging());
+  EXPECT_TRUE(DisableLocalLogging());
+}
+
+TEST_F(WebRtcEventLogManagerTest,
+       LocalLogDisableLocalLoggingReturnsIfNeverEnabled) {
+  EXPECT_FALSE(DisableLocalLogging());
+}
+
+TEST_F(WebRtcEventLogManagerTest,
+       LocalLogDisableLocalLoggingReturnsIfAlreadyDisabled) {
+  ASSERT_TRUE(EnableLocalLogging());
+  ASSERT_TRUE(DisableLocalLogging());
+  EXPECT_FALSE(DisableLocalLogging());
+}
+
+TEST_F(WebRtcEventLogManagerTest,
+       OnLocalLogsStartedNotCalledIfLocalLoggingEnabledWithoutPeerConnections) {
+  MockLocalLogsObserver observer;
+  SetLocalLogsObserver(&observer);
+  EXPECT_CALL(observer, OnLocalLogsStarted(_, _)).Times(0);
+  EXPECT_CALL(observer, OnLocalLogsStopped(_)).Times(0);
+  ASSERT_TRUE(EnableLocalLogging());
+}
+
+TEST_F(WebRtcEventLogManagerTest,
+       OnLocalLogsStartedCalledForPeerConnectionAddedAndLocalLoggingEnabled) {
+  MockLocalLogsObserver observer;
+  SetLocalLogsObserver(&observer);
+  PeerConnectionKey peer_connection(kRenderProcessId, kLocalPeerConnectionId);
+  EXPECT_CALL(observer, OnLocalLogsStarted(peer_connection, _)).Times(1);
+  ASSERT_TRUE(PeerConnectionAdded(kRenderProcessId, kLocalPeerConnectionId));
+  ASSERT_TRUE(EnableLocalLogging());
+}
+
+TEST_F(WebRtcEventLogManagerTest,
+       OnLocalLogsStartedCalledForLocalLoggingEnabledAndPeerConnectionAdded) {
+  MockLocalLogsObserver observer;
+  SetLocalLogsObserver(&observer);
+  PeerConnectionKey peer_connection(kRenderProcessId, kLocalPeerConnectionId);
+  EXPECT_CALL(observer, OnLocalLogsStarted(peer_connection, _)).Times(1);
+  ASSERT_TRUE(EnableLocalLogging());
+  ASSERT_TRUE(PeerConnectionAdded(kRenderProcessId, kLocalPeerConnectionId));
+}
+
+TEST_F(WebRtcEventLogManagerTest,
+       OnLocalLogsStoppedCalledAfterLocalLoggingDisabled) {
+  NiceMock<MockLocalLogsObserver> observer;
+  SetLocalLogsObserver(&observer);
+  PeerConnectionKey peer_connection(kRenderProcessId, kLocalPeerConnectionId);
+  EXPECT_CALL(observer, OnLocalLogsStopped(peer_connection)).Times(1);
+  ASSERT_TRUE(PeerConnectionAdded(kRenderProcessId, kLocalPeerConnectionId));
+  ASSERT_TRUE(EnableLocalLogging());
+  ASSERT_TRUE(DisableLocalLogging());
+}
+
+TEST_F(WebRtcEventLogManagerTest,
+       OnLocalLogsStoppedCalledAfterPeerConnectionRemoved) {
+  NiceMock<MockLocalLogsObserver> observer;
+  SetLocalLogsObserver(&observer);
+  PeerConnectionKey peer_connection(kRenderProcessId, kLocalPeerConnectionId);
+  EXPECT_CALL(observer, OnLocalLogsStopped(peer_connection)).Times(1);
+  ASSERT_TRUE(PeerConnectionAdded(kRenderProcessId, kLocalPeerConnectionId));
+  ASSERT_TRUE(EnableLocalLogging());
+  ASSERT_TRUE(PeerConnectionRemoved(kRenderProcessId, kLocalPeerConnectionId));
+}
+
+#if 0  // TODO: !!!
 TEST_F(WebRtcEventLogManagerTest, LocalLogCreateEmptyFile) {
   const base::FilePath file_path = LocalWebRtcEventLogStart(
       kRenderProcessId, kLocalPeerConnectionId, base_path_, kMaxFileSizeBytes);
@@ -565,5 +786,6 @@ TEST_F(WebRtcEventLogManagerTest, LocalLogMayNotBeStartedTwice) {
                                base_path_, kMaxFileSizeBytes),
       "");
 }
+#endif  // TODO: !!!
 
 }  // namespace content
