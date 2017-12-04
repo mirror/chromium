2f7e0afabbd9abe03edaa3da51111808b418ba3a
Marc Treib <treib@chromium.org>
2017-12-04 12:02:17 +0100

    Remove more search/instant code on Android
    This CL #ifdef's out a bunch of Instant-process-related functions in
    chrome/search/search.h/cc on Android. This code was never used on
    Android and doesn't make sense there; let's make that obvious.
    
    Bug: 787830
    Change-Id: I74d51009010df4633631102bacf051a5d1a52583

--- chrome/browser/search/search.h
+++ chrome/browser/search/search.h
@@ -5,6 +5,8 @@
 #ifndef CHROME_BROWSER_SEARCH_SEARCH_H_
 #define CHROME_BROWSER_SEARCH_SEARCH_H_
 
+#include "build/build_config.h"
+
 class GURL;
 class Profile;
 class TemplateURLService;
@@ -26,17 +28,6 @@ enum CacheableNTPLoad {
 
 // Returns whether the suggest is enabled for the given |profile|.
 bool IsSuggestPrefEnabled(Profile* profile);
-
-// Returns true if |url| should be rendered in the Instant renderer process.
-bool ShouldAssignURLToInstantRenderer(const GURL& url, Profile* profile);
-
-// Returns true if |contents| is rendered inside the Instant process for
-// |profile|.
-bool IsRenderedInInstantProcess(const content::WebContents* contents,
-                                Profile* profile);
-
-// Returns true if the Instant |url| should use process per site.
-bool ShouldUseProcessPerSiteForInstantURL(const GURL& url, Profile* profile);
 
 // Returns whether Google is selected as the default search engine.
 bool DefaultSearchProviderIsGoogle(Profile* profile);
@@ -61,6 +52,16 @@ bool NavEntryIsInstantNTP(const content::WebContents* contents,
 // in an Instant process.
 bool IsInstantNTPURL(const GURL& url, Profile* profile);
 
+// Returns the New Tab page URL for the given |profile|.
+GURL GetNewTabPageURL(Profile* profile);
+
+#if !defined(OS_ANDROID)
+
+// Returns true if |url| should be rendered in the Instant renderer process.
+bool ShouldAssignURLToInstantRenderer(const GURL& url, Profile* profile);
+
+// Returns true if the Instant |url| should use process per site.
+bool ShouldUseProcessPerSiteForInstantURL(const GURL& url, Profile* profile);
 
 // Transforms the input |url| into its "effective URL". |url| must be an
 // Instant URL, i.e. ShouldAssignURLToInstantRenderer must return true. The
@@ -92,8 +93,7 @@ bool HandleNewTabURLRewrite(GURL* url,
 bool HandleNewTabURLReverseRewrite(GURL* url,
                                    content::BrowserContext* browser_context);
 
-// Returns the New Tab page URL for the given |profile|.
-GURL GetNewTabPageURL(Profile* profile);
+#endif  // !defined(OS_ANDROID)
 
 }  // namespace search
 
