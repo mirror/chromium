43b56baf5a7bec522b571dfe171b8fb6af0738ad
Toni Barzic <tbarzic@google.com>
2017-12-04 12:40:30 -0800

    Control stylus related lock screen note state from ash
    Update power_button_display_controller to
     *  launch lock screen note when stylus is removed
     *  close lock screen note when screen is forced off
        (e.g. due to power button press)
    And remove code that currently does this from
    lock_screen_apps/state_controller.
    
    The main goal of this is to better handle lock screen app launch when
    stylus is ejected with display forced off. This action should perform
    two actions:
     1. turn the display on
     2. launch the lock screen note (if lock screen note action is enabled)
    Doing this at the same time can cause a flash of lock screen UI before
    the lock screen note app window is shown. With logic for launching
    the note moved to power_button_display_controller, it's possible to
    delay changing display state until the lock screen app window becomes
    visible, which should be long enough to prevent lock screen UI flash.
    
    BUG=767711
    
    Change-Id: Ie47f546edb51659bb82ba4618b069569a2d188c6

--- ash/shell_test_api.cc
+++ ash/shell_test_api.cc
@@ -8,6 +8,7 @@
 
 #include "ash/root_window_controller.h"
 #include "ash/shell.h"
+#include "ash/system/power/display_forced_off_setter.h"
 #include "ash/system/power/power_button_controller.h"
 #include "components/prefs/testing_pref_service.h"
 
@@ -47,7 +48,8 @@ void ShellTestApi::OnLocalStatePrefServiceInitialized(
 }
 
 void ShellTestApi::ResetPowerButtonControllerForTest() {
-  shell_->power_button_controller_ = std::make_unique<PowerButtonController>();
+  shell_->power_button_controller_ = std::make_unique<PowerButtonController>(
+      shell_->display_forced_off_setter_.get());
 }
 
 }  // namespace ash
