cd9525f92a485c72757142d180430d8aa85ec480
Abhishek Bhardwaj <abhishekbh@google.com>
2017-12-03 18:36:39 -0800

    arc: Add mojo service for wakeup alarms
    The instance can't set wake up alarms due to not having the
    CAP_WAKE_ALARM capability. This change adds a mojo service that will let
    the instance set wake up alarms and notify the instance when an alarm
    expires.
    
    BUG=b:64581307
    TEST=Tested manually by verifying the instance client gets callbacks.
    
    Change-Id: I45bda62997b8358d1638cb0fd1b6858cf18fc443

---
+++ components/arc/timer/arc_timer_request.h
@@ -0,0 +1,25 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ARC_TIMER_ARC_TIMER_REQUEST_H_
+#define COMPONENTS_ARC_TIMER_ARC_TIMER_REQUEST_H_
+
+#include <stdint.h>
+
+#include "base/files/scoped_file.h"
+
+// Typemapping for |ArcTimerRequest| in timer.mojom
+namespace arc {
+
+struct ArcTimerRequest {
+  ArcTimerRequest();
+  ArcTimerRequest(ArcTimerRequest&&);
+  ~ArcTimerRequest();
+  int32_t clock_id;
+  base::ScopedFD expiration_fd;
+};
+
+}  // namespace arc
+
+#endif  // COMPONENTS_ARC_TIMER_ARC_TIMER_REQUEST_H_
