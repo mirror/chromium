6fc064533ae8fbed1397007bf14da108ff6afb4c
tanvir.rizvi <tanvir.rizvi@samsung.com>
2017-12-05 16:40:21 +0530

    Move SelectionTemplate:is_directional to FrameSelection
    This patch is all in one patch to get rid of is_directional_ from
    SelectionTemplate and move it to FrameSelection.
    
    BUG=740804
    
    Change-Id: I29970ef81eb634b3b82bb7ac2daee06e17d95368

--- third_party/WebKit/Source/core/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/core/editing/FrameSelection.cpp
@@ -104,6 +104,7 @@ FrameSelection::FrameSelection(LocalFrame& frame)
       x_pos_for_vertical_arrow_navigation_(NoXPosForVerticalArrowNavigation()),
       focused_(frame.GetPage() &&
                frame.GetPage()->GetFocusController().FocusedFrame() == frame),
+      is_directional_(ShouldAlwaysUseDirectionalSelection(frame_)),
       frame_caret_(new FrameCaret(frame, *selection_editor_)) {}
 
 FrameSelection::~FrameSelection() {}
@@ -180,6 +181,7 @@ void FrameSelection::MoveCaretSelection(const IntPoint& point) {
                                     .SetShouldClearTypingStyle(true)
                                     .SetSetSelectionBy(SetSelectionBy::kUser)
                                     .SetShouldShowHandle(true)
+                                    .SetIsDirectional(IsDirectional())
                                     .Build());
 }
 
@@ -198,14 +200,19 @@ void FrameSelection::SetSelection(const SelectionInDOMTree& selection) {
 
 bool FrameSelection::SetSelectionDeprecated(
     const SelectionInDOMTree& passed_selection,
-    const SetSelectionOptions& options) {
+    const SetSelectionOptions& passed_options) {
   DCHECK(IsAvailable());
   passed_selection.AssertValidFor(GetDocument());
 
+  SetSelectionOptions::Builder options_builder(passed_options);
   SelectionInDOMTree::Builder builder(passed_selection);
-  if (ShouldAlwaysUseDirectionalSelection(frame_))
+  if (ShouldAlwaysUseDirectionalSelection(frame_)) {
     builder.SetIsDirectional(true);
+    options_builder.SetIsDirectional(true);
+  }
   SelectionInDOMTree new_selection = builder.Build();
+  SetSelectionOptions options = options_builder.Build();
+
   if (granularity_strategy_ && !options.DoNotClearStrategy())
     granularity_strategy_->Clear();
   granularity_ = options.Granularity();
@@ -222,10 +229,12 @@ bool FrameSelection::SetSelectionDeprecated(
       selection_editor_->GetSelectionInDOMTree();
   const bool is_changed = old_selection_in_dom_tree != new_selection;
   const bool should_show_handle = options.ShouldShowHandle();
-  if (!is_changed && is_handle_visible_ == should_show_handle)
+  if (!is_changed && is_handle_visible_ == should_show_handle &&
+      is_directional_ == options.IsDirectional())
     return false;
   if (is_changed)
     selection_editor_->SetSelection(new_selection);
+  is_directional_ = options.IsDirectional();
   should_shrink_next_tap_ = options.ShouldShrinkNextTap();
   is_handle_visible_ = should_show_handle;
   ScheduleVisualUpdateForPaintInvalidationIfNeeded();
@@ -344,7 +353,7 @@ bool FrameSelection::Modify(SelectionModifyAlteration alter,
                             SelectionModifyDirection direction,
                             TextGranularity granularity,
                             SetSelectionBy set_selection_by) {
-  SelectionModifier selection_modifier(*GetFrame(), GetSelectionInDOMTree(),
+  SelectionModifier selection_modifier(*this,
                                        x_pos_for_vertical_arrow_navigation_);
   const bool modified =
       selection_modifier.Modify(alter, direction, granularity);
@@ -367,11 +376,17 @@ bool FrameSelection::Modify(SelectionModifyAlteration alter,
     return true;
   }
 
+  // For MacOS only selection is directionless at the beginning.
+  // Selection gets direction on extent.
+  const bool is_directional = alter == SelectionModifyAlteration::kExtend ||
+                              ShouldAlwaysUseDirectionalSelection(frame_);
+
   SetSelection(selection_modifier.Selection().AsSelection(),
                SetSelectionOptions::Builder()
                    .SetShouldCloseTyping(true)
                    .SetShouldClearTypingStyle(true)
                    .SetSetSelectionBy(set_selection_by)
+                   .SetIsDirectional(is_directional)
                    .Build());
 
   if (granularity == TextGranularity::kLine ||
@@ -393,6 +408,7 @@ void FrameSelection::Clear() {
     granularity_strategy_->Clear();
   SetSelection(SelectionInDOMTree());
   is_handle_visible_ = false;
+  is_directional_ = ShouldAlwaysUseDirectionalSelection(frame_);
 }
 
 bool FrameSelection::SelectionHasFocus() const {
@@ -1202,7 +1218,7 @@ void FrameSelection::ClearLayoutSelection() {
 }
 
 bool FrameSelection::IsDirectional() const {
-  return GetSelectionInDOMTree().IsDirectional();
+  return is_directional_;
 }
 
 }  // namespace blink
