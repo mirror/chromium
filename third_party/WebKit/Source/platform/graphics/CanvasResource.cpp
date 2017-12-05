b4a4c34f8e957f8204137bf333b6e26a2d9e83bb
Victor Miura <vmiura@chromium.org>
2017-12-05 13:53:27 -0800

    gpu: Simplify CHROMIUM_texture_mailbox API.
    - Remove ProduceTextureCHROMIUM from GLES2Interface.  Change all call sites
      to use ProduceTextureDirectCHROMIUM.
    - Remove <target> from ProduceTextureDirectCHROMIUM.
    - Remove <target> from CreateAndConsumeTextureCHROMIUM.
    
    BUG=757607
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ic50059a0db3edfaeb51690645bce096f4f64228e

--- third_party/WebKit/Source/platform/graphics/CanvasResource.cpp
+++ third_party/WebKit/Source/platform/graphics/CanvasResource.cpp
@@ -92,7 +92,7 @@ void CanvasResource_Skia::TearDown() {
     DCHECK(image_->isTextureBacked());
     // To avoid leaking Mailbox records, we must disassociate the mailbox
     // before image_ goes out of scope because skia might recycle the texture.
-    gl->ProduceTextureDirectCHROMIUM(0, GL_TEXTURE_2D, GpuMailbox().name);
+    gl->ProduceTextureDirectCHROMIUM(0, GpuMailbox().name);
   }
   image_ = nullptr;
   context_provider_wrapper_ = nullptr;
