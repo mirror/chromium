7e9fc8a5e16c7c2125d6823a86a3092b5132705c
dpapad <dpapad@chromium.org>
2017-12-05 12:27:22 -0800

    MD Extensions: Split the app into a basic and a lazy module.
    This improves FMP performance by about 8.6% (~452ms -> ~414ms).
    
    Bug:764126
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If97526b783caa587bf13a17695eefd9bf3c62249

--- chrome/browser/resources/md_extensions/manager.js
+++ chrome/browser/resources/md_extensions/manager.js
@@ -176,9 +176,6 @@ cr.define('extensions', function() {
 
     /** @override */
     attached: function() {
-      document.documentElement.classList.remove('loading');
-      document.fonts.load('bold 12px Roboto');
-
       this.navigationListener_ = extensions.navigation.addListener(newPage => {
         this.changePage_(newPage);
       });
@@ -318,6 +315,23 @@ cr.define('extensions', function() {
 
       this.apps_ = apps;
       this.extensions_ = extensions;
+
+      requestIdleCallback(this.loadLazyModule_.bind(this));
+    },
+
+    /**
+     * Triggers loading of the "lazy loaded" module.
+     * @return {!Promise} A signal that the module was loaded.
+     * @private
+     */
+    loadLazyModule_: function() {
+      return new Promise((resolve, reject) => {
+        this.importHref('lazy_load.html', () => {
+          document.documentElement.classList.remove('loading');
+          document.fonts.load('bold 12px Roboto');
+          resolve();
+        }, reject);
+      });
     },
 
     /**
