7315abd16a5b6b283101c90acc74b33eaa6e2855
Koji Ishii <kojii@chromium.org>
2017-12-05 15:03:23 +0900

    paint-layer
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Iccd9fcfbce5b0fc1bbfb523730e7005f7d6e02d6

--- third_party/WebKit/Source/core/paint/ng/ng_paint_fragment.cc
+++ third_party/WebKit/Source/core/paint/ng/ng_paint_fragment.cc
@@ -133,4 +133,18 @@ void NGPaintFragment::AddOutlineRects(
   }
 }
 
+void NGPaintFragment::ForEachDescendants(
+    const LayoutObject* layout_object,
+    std::function<bool(const NGPaintFragment&, const NGPhysicalOffset&)> yield,
+    NGPhysicalOffset offset) const {
+  if (GetLayoutObject() == layout_object) {
+    if (!yield(*this, offset))
+      return;
+  }
+
+  for (const auto& child : Children()) {
+    child->ForEachDescendants(layout_object, yield, offset + Offset());
+  }
+}
+
 }  // namespace blink
