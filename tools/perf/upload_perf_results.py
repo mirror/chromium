3d7d76ea08bbe6b12c63c3322a3e4561d862afe3
Ashley Enstad <ashleymarie@chromium.org>
2017-12-05 11:39:35 -0500

    Testing out some stuff
    Please ignore
    
    BUG=chromium:758632
    
    Change-Id: I824762011f3584186f1a6d60c2d369bf756bcb5a

--- tools/perf/upload_perf_results.py
+++ tools/perf/upload_perf_results.py
@@ -12,6 +12,8 @@ def UploadJson(output_json, jsons_to_upload, debug=True):
   """Upload the contents of one or more results JSONs to the perf dashboard.
 
   Args:
+    output_json: A path to a JSON file to which the merged results should be
+      written.
     jsons_to_upload: A list of paths to JSON files that should be uploaded.
   """
   shard_output_list = []
@@ -37,6 +39,64 @@ def UploadJson(output_json, jsons_to_upload, debug=True):
   with open(output_json, 'w') as f:
       json.dump(merged_results, f)
 
+
+  """
+  So here's what this thing actually needs to do:
+  1) Separate out the output.json from perftest-output.json
+  2) Merge and dump the output.json files using
+    standard_isolated_script_merge.py
+  3) Upload all the perftest-output.json files to the perf dashboard
+
+  Step 1 actually consists of checking all the files in\
+    jsons_to_upload (needs renamed) to see if they have json 3 output,
+    json simplified output, chartjson, or histograms
+
+  Step 3 is the complicated step:
+  Determine if the benchmark is enabled or not (maybe? let's just
+    upload disabled benchmarks for now)
+  Figure out if they are chartjson or histogram
+  Generate an oauth token for histograms
+  Generate the args for upload_perf_dashboard_results
+  Call upload_perf_dashboard_results
+
+
+  Here are the args that upload_perf_dashboard_results.py takes:
+  --name
+  --results-file
+  --output-json-file
+  --got-revision-cp
+  --build-dir
+  --perf-id
+  --results-url
+  --buildername
+  --buildnumber
+  --got-webrtc-revision
+  --got-v8-revision
+  --version
+  --git-revision
+  --output-json-dashboard-url
+  --send-as-histograms
+  --oauth-token-file
+  --chromium-checkout-dir
+
+  hmm I wonder if we should just have people call
+    upload_perf_dashboard_results.py and just better
+    document what all those things are
+
+  I think it's best to have users of the script generate all those things
+  including the oauth token themselves cause otherwise we're giving everyone
+  the right to upload to the dashboard when really they should all be getting
+  those rights themselves
+
+  for the rest, they don't seem like generic params so really
+    folks should just call that script with all the params
+
+  then this script can focus on splitting out and handling the
+    different types of results
+  And eventually, I can rework upload_perf_dashboard_results to be
+    cleaner and not handle chartjson
+  """
+
   return 0
 
 
@@ -49,6 +109,8 @@ def main():
   parser.add_argument('jsons_to_merge', nargs='*')
 
   args = parser.parse_args()
+  print args.build_properties
+  print args.summary_json
   return UploadJson(args.output_json, args.jsons_to_merge)
 
 
