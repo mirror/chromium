768cbe3e4119794e7f3ab34942d71f94e088dfe9
Xu Xing <xing.xu@intel.com>
2017-12-05 14:20:57 +0800

    [viz] Implement local resource in GLRenderer
    Local resource is resource used inside the same GL context and will not being sent
    into another process, no need to create fence and mailbox for these resources.
    For RenderPass in render_pass_textures_, it uses texture-backed resources, no need
    to support GpuMemoryBuffer.
    
    Currently there are three path may possible set is_overlay_ to true:
    ResourceProvider::CreateGpuMemoryBufferResource
    LayerTreeResourceProvider::CreateResourceFromTextureMailbox
    ResourceProvider::CreateGpuTextureResource (Used by GLRenderer::overlay_resource_pool_).
    Resource for RenderPass in render_pass_textures_ is not created by above three paths.
    Based on this, remove the support of overlay.
    
    BUG=782044
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Ia334842b12b8297ae4a768979adb8b5a468b906a

---
+++ components/viz/service/display/scoped_render_pass_texture.h
@@ -0,0 +1,75 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_VIZ_SERVICE_DISPLAY_SCOPED_RENDER_PASS_TEXTURE_H_
+#define COMPONENTS_VIZ_SERVICE_DISPLAY_SCOPED_RENDER_PASS_TEXTURE_H_
+
+#include "base/macros.h"
+#include "components/viz/common/resources/resource_format.h"
+#include "components/viz/common/resources/resource_texture_hint.h"
+#include "components/viz/service/viz_service_export.h"
+#include "gpu/command_buffer/client/gles2_interface.h"
+#include "third_party/khronos/GLES2/gl2.h"
+#include "ui/gfx/geometry/size.h"
+
+namespace viz {
+
+// ScopedRenderPassTexture is resource used inside the same GL context and will
+// not being sent into another process. So no need to create fence and mailbox
+// for these resources.
+class VIZ_SERVICE_EXPORT ScopedRenderPassTexture {
+ public:
+  explicit ScopedRenderPassTexture(gpu::gles2::GLES2Interface* gl,
+                                   const gfx::Size& size,
+                                   ResourceTextureHint hint,
+                                   const bool use_texture_usage_hint,
+                                   const bool use_texture_storage,
+                                   const bool use_texture_npot);
+
+  explicit ScopedRenderPassTexture(gpu::gles2::GLES2Interface* gl,
+                                   GLuint gl_id,
+                                   const gfx::Size& size,
+                                   ResourceTextureHint hint,
+                                   const bool use_texture_usage_hint,
+                                   const bool use_texture_storage,
+                                   const bool use_texture_npot);
+
+  ScopedRenderPassTexture(ScopedRenderPassTexture&& other);
+  ScopedRenderPassTexture& operator=(ScopedRenderPassTexture&& other);
+  ~ScopedRenderPassTexture();
+
+  void BindForSampling();
+  GLuint id() const { return gl_id_; }
+  void set_size(const gfx::Size& size) { size_ = size; }
+  const gfx::Size& size() const { return size_; }
+  void set_hint(const ResourceTextureHint hint) { hint_ = hint; }
+  ResourceTextureHint hint() const { return hint_; }
+  void set_generate_mipmap() { mipmap_state_ = GENERATE; }
+
+ private:
+  enum MipmapState { INVALID, GENERATE, VALID };
+  void CreateTexture();
+  void Allocate();
+  void Allocate(const gfx::Size& size, ResourceTextureHint hint);
+  void Free();
+
+  gpu::gles2::GLES2Interface* gl_;
+  // Texture id for texture-backed resources.
+  GLuint gl_id_ = 0;
+  // Size of the resource in pixels.
+  gfx::Size size_;
+  // The current min filter for texture-backed resources.
+  GLenum min_filter_ = GL_LINEAR;
+  // A hint for texture-backed resources about how the resource will be used,
+  // that dictates how it should be allocated/used.
+  ResourceTextureHint hint_ = ResourceTextureHint::kFramebuffer;
+  MipmapState mipmap_state_ = INVALID;
+  bool use_texture_usage_hint_;
+  bool use_texture_storage_;
+  bool use_texture_npot_;
+};
+
+}  // namespace viz
+
+#endif  // COMPONENTS_VIZ_SERVICE_DISPLAY_SCOPED_RENDER_PASS_TEXTURE_H_
