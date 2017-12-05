4210af6d3db3e880451d93e4267b2cb1af9cabed
Fabio Tirelo <ftirelo@chromium.org>
2017-12-04 22:02:37 -0500

    Chrome Cleaner UI: Allow cleanups to start from Settings page
    This enables user-initiated cleanups from chrome://settings/cleanup
    when a feature is enabled. Cards for some special error conditions
    and information on registry keys to be removed/changed will be sent
    in a follow-up CL.
    
    Will handle scanner/cleanup failures and retries on a follow-up CL.
    
    Bug: 776538
    Change-Id: Ib398fd286fd135ba78ef29ce00a4edf0ea4e1f80
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation

--- chrome/test/data/webui/settings/chrome_cleanup_page_test.js
+++ chrome/test/data/webui/settings/chrome_cleanup_page_test.js
@@ -11,6 +11,7 @@ class TestChromeCleanupProxy extends TestBrowserProxy {
       'restartComputer',
       'setLogsUploadPermission',
       'startCleanup',
+      'startScanning',
       'notifyShowDetails',
       'notifyLearnMoreClicked',
     ]);
@@ -42,6 +43,11 @@ class TestChromeCleanupProxy extends TestBrowserProxy {
   }
 
   /** @override */
+  startScanning(logsUploadEnabled) {
+    this.methodCalled('startScanning', logsUploadEnabled);
+  }
+
+  /** @override */
   notifyShowDetails(enabled) {
     this.methodCalled('notifyShowDetails', enabled);
   }
@@ -63,16 +69,151 @@ suite('ChromeCleanupHandler', function() {
     settings.ChromeCleanupProxyImpl.instance_ = ChromeCleanupProxy;
 
     PolymerTest.clearBody();
-
-    chromeCleanupPage = document.createElement('settings-chrome-cleanup-page');
-    document.body.appendChild(chromeCleanupPage);
   });
 
   teardown(function() {
     chromeCleanupPage.remove();
+    loadTimeData.overrideValues({
+      userInitiatedCleanupsEnabled: false,
+    });
   });
 
-  test('startCleanupFromInfected', function() {
+  var initParametrizedTest = function(userInitiatedCleanupsEnabled) {
+    loadTimeData.overrideValues({
+      userInitiatedCleanupsEnabled: userInitiatedCleanupsEnabled,
+    });
+
+    chromeCleanupPage = document.createElement('settings-chrome-cleanup-page');
+    document.body.appendChild(chromeCleanupPage);
+  };
+
+  var scanOfferedOnInitiallyIdle = function(idleReason) {
+    initParametrizedTest(true);
+
+    cr.webUIListenerCallback('chrome-cleanup-on-idle', idleReason);
+    Polymer.dom.flush();
+
+    var actionButton = chromeCleanupPage.$$('#action-button');
+    assertTrue(!!actionButton);
+  };
+
+  test('scanOfferedOnInitiallyIdle_ReporterFoundNothing', function() {
+    scanOfferedOnInitiallyIdle(
+        settings.ChromeCleanupIdleReason.REPORTER_FOUND_NOTHING);
+  });
+
+  test('scanOfferedOnInitiallyIdle_ReporterFailed', function() {
+    scanOfferedOnInitiallyIdle(
+        settings.ChromeCleanupIdleReason.REPORTER_FAILED);
+  });
+
+  test('scanOfferedOnInitiallyIdle_ScanningFoundNothing', function() {
+    scanOfferedOnInitiallyIdle(
+        settings.ChromeCleanupIdleReason.SCANNING_FOUND_NOTHING);
+  });
+
+  test('scanOfferedOnInitiallyIdle_ScanningFailed', function() {
+    scanOfferedOnInitiallyIdle(
+        settings.ChromeCleanupIdleReason.SCANNING_FAILED);
+  });
+
+  test('scanOfferedOnInitiallyIdle_ConnectionLost', function() {
+    scanOfferedOnInitiallyIdle(
+        settings.ChromeCleanupIdleReason.CONNECTION_LOST);
+  });
+
+  test('scanOfferedOnInitiallyIdle_UserDeclinedCleanup', function() {
+    scanOfferedOnInitiallyIdle(
+        settings.ChromeCleanupIdleReason.USER_DECLINED_CLEANUP);
+  });
+
+  test('scanOfferedOnInitiallyIdle_CleaningFailed', function() {
+    scanOfferedOnInitiallyIdle(
+        settings.ChromeCleanupIdleReason.CLEANING_FAILED);
+  });
+
+  test('scanOfferedOnInitiallyIdle_CleaningSucceeded', function() {
+    scanOfferedOnInitiallyIdle(
+        settings.ChromeCleanupIdleReason.CLEANING_SUCCEEDED);
+  });
+
+  test('reporterFoundNothing', function() {
+    initParametrizedTest(true);
+
+    cr.webUIListenerCallback('chrome-cleanup-on-reporter-running');
+    cr.webUIListenerCallback(
+        'chrome-cleanup-on-idle',
+        settings.ChromeCleanupIdleReason.REPORTER_FOUND_NOTHING);
+    Polymer.dom.flush();
+
+    var actionButton = chromeCleanupPage.$$('#action-button');
+    assertFalse(!!actionButton);
+  });
+
+  test('reporterFoundNothing', function() {
+    initParametrizedTest(true);
+
+    cr.webUIListenerCallback('chrome-cleanup-on-reporter-running');
+    cr.webUIListenerCallback(
+        'chrome-cleanup-on-idle',
+        settings.ChromeCleanupIdleReason.REPORTER_FOUND_NOTHING);
+    Polymer.dom.flush();
+
+    var actionButton = chromeCleanupPage.$$('#action-button');
+    assertFalse(!!actionButton);
+  });
+
+  test('startScanFromIdle', function() {
+    initParametrizedTest(true);
+
+    cr.webUIListenerCallback('chrome-cleanup-upload-permission-change', false);
+    cr.webUIListenerCallback(
+        'chrome-cleanup-on-idle', settings.ChromeCleanupIdleReason.INITIAL);
+    Polymer.dom.flush();
+
+    var actionButton = chromeCleanupPage.$$('#action-button');
+    assertTrue(!!actionButton);
+    MockInteractions.tap(actionButton);
+    return ChromeCleanupProxy.whenCalled('startScanning')
+        .then(function(logsUploadEnabled) {
+          assertFalse(logsUploadEnabled);
+          cr.webUIListenerCallback('chrome-cleanup-on-scanning', false);
+          Polymer.dom.flush();
+
+          var spinner = chromeCleanupPage.$$('#waiting-spinner');
+          assertTrue(spinner.active);
+        });
+  });
+
+  test('scanFoundNothing', function() {
+    initParametrizedTest(true);
+
+    cr.webUIListenerCallback('chrome-cleanup-on-scanning', false);
+    cr.webUIListenerCallback(
+        'chrome-cleanup-on-idle',
+        settings.ChromeCleanupIdleReason.SCANNING_FOUND_NOTHING);
+    Polymer.dom.flush();
+
+    var actionButton = chromeCleanupPage.$$('#action-button');
+    assertFalse(!!actionButton);
+  });
+
+  test('scanFailure', function() {
+    initParametrizedTest(true);
+
+    cr.webUIListenerCallback('chrome-cleanup-on-scanning', false);
+    cr.webUIListenerCallback(
+        'chrome-cleanup-on-idle',
+        settings.ChromeCleanupIdleReason.SCANNING_FAILED);
+    Polymer.dom.flush();
+
+    var actionButton = chromeCleanupPage.$$('#action-button');
+    assertFalse(!!actionButton);
+  });
+
+  var startCleanupFromInfected = function(userInitiatedCleanupsEnabled) {
+    initParametrizedTest(userInitiatedCleanupsEnabled);
+
     cr.webUIListenerCallback('chrome-cleanup-upload-permission-change', false);
     cr.webUIListenerCallback(
         'chrome-cleanup-on-infected', ['file 1', 'file 2', 'file 3']);
@@ -88,18 +229,28 @@ suite('ChromeCleanupHandler', function() {
     var actionButton = chromeCleanupPage.$$('#action-button');
     assertTrue(!!actionButton);
     MockInteractions.tap(actionButton);
-    ChromeCleanupProxy.whenCalled('startCleanup').then(
-        function(logsUploadEnabled) {
+    return ChromeCleanupProxy.whenCalled('startCleanup')
+        .then(function(logsUploadEnabled) {
           assertFalse(logsUploadEnabled);
           cr.webUIListenerCallback('chrome-cleanup-on-cleaning', false);
           Polymer.dom.flush();
 
-          var spinner = chromeCleanupPage.$$('#cleaning-spinner');
+          var spinner = chromeCleanupPage.$$('#waiting-spinner');
           assertTrue(spinner.active);
         });
+  };
+
+  test('startCleanupFromInfected_UserInitiatedCleanupsDisabled', function() {
+    return startCleanupFromInfected(false);
   });
 
-  test('rebootFromRebootRequired', function() {
+  test('startCleanupFromInfected_UserInitiatedCleanupsEnabled', function() {
+    return startCleanupFromInfected(true);
+  });
+
+  var rebootFromRebootRequired = function(userInitiatedCleanupsEnabled) {
+    initParametrizedTest(userInitiatedCleanupsEnabled);
+
     cr.webUIListenerCallback('chrome-cleanup-on-reboot-required');
     Polymer.dom.flush();
 
@@ -107,9 +258,20 @@ suite('ChromeCleanupHandler', function() {
     assertTrue(!!actionButton);
     MockInteractions.tap(actionButton);
     return ChromeCleanupProxy.whenCalled('restartComputer');
+  };
+
+  test('rebootFromRebootRequired_UserInitiatedCleanupsDisabled', function() {
+    return rebootFromRebootRequired(false);
   });
 
-  test('dismissFromIdleCleaningFailure', function() {
+  test('rebootFromRebootRequired_UserInitiatedCleanupsEnabled', function() {
+    return rebootFromRebootRequired(true);
+  });
+
+  var cleanupFailure = function(userInitiatedCleanupsEnabled) {
+    initParametrizedTest(userInitiatedCleanupsEnabled);
+
+    cr.webUIListenerCallback('chrome-cleanup-upload-permission-change', false);
     cr.webUIListenerCallback('chrome-cleanup-on-cleaning', false);
     cr.webUIListenerCallback(
         'chrome-cleanup-on-idle',
@@ -117,12 +279,26 @@ suite('ChromeCleanupHandler', function() {
     Polymer.dom.flush();
 
     var actionButton = chromeCleanupPage.$$('#action-button');
-    assertTrue(!!actionButton);
-    MockInteractions.tap(actionButton);
-    return ChromeCleanupProxy.whenCalled('dismissCleanupPage');
+    if (userInitiatedCleanupsEnabled) {
+      assertFalse(!!actionButton);
+    } else {
+      assertTrue(!!actionButton);
+      MockInteractions.tap(actionButton);
+      return ChromeCleanupProxy.whenCalled('dismissCleanupPage');
+    }
+  };
+
+  test('cleanupFailure_UserInitiatedCleanupsDisabled', function() {
+    return cleanupFailure(false);
   });
 
-  test('dismissFromIdleCleaningSuccess', function() {
+  test('cleanupFailure_UserInitiatedCleanupsEnabled', function() {
+    return cleanupFailure(true);
+  });
+
+  var cleanupSuccess = function(userInitiatedCleanupsEnabled) {
+    initParametrizedTest(userInitiatedCleanupsEnabled);
+
     cr.webUIListenerCallback('chrome-cleanup-on-cleaning', false);
     cr.webUIListenerCallback(
         'chrome-cleanup-on-idle',
@@ -130,14 +306,34 @@ suite('ChromeCleanupHandler', function() {
     Polymer.dom.flush();
 
     var actionButton = chromeCleanupPage.$$('#action-button');
-    assertTrue(!!actionButton);
-    MockInteractions.tap(actionButton);
-    return ChromeCleanupProxy.whenCalled('dismissCleanupPage');
+    if (userInitiatedCleanupsEnabled) {
+      assertFalse(!!actionButton);
+    } else {
+      assertTrue(!!actionButton);
+      MockInteractions.tap(actionButton);
+      return ChromeCleanupProxy.whenCalled('dismissCleanupPage');
+    }
+  };
+
+  test('cleanupSuccess_UserInitiatedCleanupsDisabled', function() {
+    return cleanupSuccess(false);
   });
 
-  test('setLogsUploadPermission', function() {
-    cr.webUIListenerCallback(
-        'chrome-cleanup-on-infected', ['file 1', 'file 2', 'file 3']);
+  test('cleanupSuccess_UserInitiatedCleanupsEnabled', function() {
+    return cleanupSuccess(true);
+  });
+
+  var testLogsUploading = function(
+      testingScanOffered, userInitiatedCleanupsEnabled) {
+    initParametrizedTest(userInitiatedCleanupsEnabled);
+
+    if (testingScanOffered) {
+      cr.webUIListenerCallback(
+          'chrome-cleanup-on-infected', ['file 1', 'file 2', 'file 3']);
+    } else {
+      cr.webUIListenerCallback(
+          'chrome-cleanup-on-idle', settings.ChromeCleanupIdleReason.INITIAL);
+    }
     Polymer.dom.flush();
 
     var logsControl = chromeCleanupPage.$$('#chromeCleanupLogsUploadControl');
@@ -156,5 +352,21 @@ suite('ChromeCleanupHandler', function() {
         function(logsUploadEnabled) {
           assertTrue(logsUploadEnabled);
         });
+  };
+
+  test('logsUploadingOnScanOffered_UserInitiatedCleanupsDisabled', function() {
+    return testLogsUploading(false, false);
+  });
+
+  test('logsUploadingOnScanOffered_UserInitiatedCleanupsEnabled', function() {
+    return testLogsUploading(false, true);
+  });
+
+  test('logsUploadingOnInfected_UserInitiatedCleanupsDisabled', function() {
+    return testLogsUploading(true, false);
+  });
+
+  test('logsUploadingOnInfected_UserInitiatedCleanupsEnabled', function() {
+    return testLogsUploading(true, true);
   });
 });
