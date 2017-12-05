685f152cd46bd223b5c9185768545ff5c5ca6575
Daniele Castagna <dcastagna@chromium.org>
2017-12-04 23:25:47 -0500

    WIP: Convert 'content-image-texture-target' from a map to a list.
    Bug:
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Id6c289465a24e0d76c50db9d2befd4053ef440c3

--- gpu/ipc/host/gpu_memory_buffer_support.cc
+++ gpu/ipc/host/gpu_memory_buffer_support.cc
@@ -7,6 +7,7 @@
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "build/build_config.h"
+#include "gpu/command_buffer/common/gpu_memory_buffer_support.h"
 #include "gpu/ipc/common/gpu_memory_buffer_support.h"
 #include "gpu/ipc/host/gpu_switches.h"
 #include "ui/gl/gl_bindings.h"
@@ -96,24 +97,7 @@ uint32_t GetImageTextureTarget(gfx::BufferFormat format,
       native_configurations.end()) {
     return GL_TEXTURE_2D;
   }
-
-  switch (GetNativeGpuMemoryBufferType()) {
-    case gfx::NATIVE_PIXMAP:
-    case gfx::ANDROID_HARDWARE_BUFFER:
-      // GPU memory buffers that are shared with the GL using EGLImages
-      // require TEXTURE_EXTERNAL_OES.
-      return GL_TEXTURE_EXTERNAL_OES;
-    case gfx::IO_SURFACE_BUFFER:
-      // IOSurface backed images require GL_TEXTURE_RECTANGLE_ARB.
-      return GL_TEXTURE_RECTANGLE_ARB;
-    case gfx::SHARED_MEMORY_BUFFER:
-    case gfx::EMPTY_BUFFER:
-      break;
-    case gfx::DXGI_SHARED_HANDLE:
-      return GL_TEXTURE_2D;
-  }
-  NOTREACHED();
-  return GL_TEXTURE_2D;
+  return GetPlatformSpecificTextureTarget();
 #else  // defined(USE_OZONE) || defined(OS_MACOSX)
   return GL_TEXTURE_2D;
 #endif
