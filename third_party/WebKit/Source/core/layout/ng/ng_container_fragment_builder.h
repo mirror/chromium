4b76d09408dc979e813f60f5985990f51af6cbca
Aleks Totic <atotic@chromium.org>
2017-12-05 22:10:48 -0800

    [WIP][LayoutNG] Propagate inline_container to NGOOFPosDescendant
    This is pretty close to the patch I'd like to land for abspos inside inline containing blocks. It performs very close to LegacyLayout right now (with the exception of handling border/padding on inline cb).
    
    The logic of the patch is:
    - collect pointers to inline-cb inside inline_layout_algorithm.
    - layout OOF descendants with inline-cb containers inside NGOutOfFlowLayoutPart for inline-cb parent block.
    - Inside NGOutOfFlowLayoutPart
      - compute inline-cb dimensions for each inline-cb. This computation is hard to follow, I've commented it as well as aI could.
      - use inline-cb dimensions to position inline-cb OOF descendants.
    
    Bug: 740993
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Id3ad9f31d5e78cd5ed4324bc5dbc3ff54f296f5b

--- third_party/WebKit/Source/core/layout/ng/ng_container_fragment_builder.h
+++ third_party/WebKit/Source/core/layout/ng/ng_container_fragment_builder.h
@@ -107,6 +107,29 @@ class CORE_EXPORT NGContainerFragmentBuilder : public NGBaseFragmentBuilder {
   void GetAndClearOutOfFlowDescendantCandidates(
       Vector<NGOutOfFlowPositionedDescendant>* descendant_candidates);
 
+  // Inline containing block geometry is defined by two fragments:
+  // start and end. FragmentPair holds the information needed to compute
+  // inline containing block geometry wrt enclosing container block.
+  //
+  // start_fragment is the start fragment of inline containing block.
+  // start_fragment_offset is offset wrt start_linebox_fragment
+  // start_linebox_offset is offset of linebox from inline-cb
+  // end_fragment/linebox are complementary properties for end fragment.
+  struct FragmentPair {
+    scoped_refptr<const NGPhysicalFragment> start_linebox_fragment;
+    NGLogicalOffset start_linebox_offset;
+    scoped_refptr<const NGPhysicalFragment> start_fragment;
+    NGPhysicalOffset start_fragment_offset;
+    scoped_refptr<const NGPhysicalFragment> end_linebox_fragment;
+    NGLogicalOffset end_linebox_offset;
+    scoped_refptr<const NGPhysicalFragment> end_fragment;
+    NGPhysicalOffset end_fragment_offset;
+  };
+
+  void ComputeInlineContainerFragments(
+      HashMap<const LayoutObject*, FragmentPair>* inline_container_fragments,
+      NGLogicalSize* container_size);
+
 #ifndef NDEBUG
   String ToString() const;
 #endif
