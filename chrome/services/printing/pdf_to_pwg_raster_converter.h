69ef7ddc403bc9d8e9f0250ca8d060583ccdc03a
Jay Civelli <jcivelli@chromium.org>
2017-12-06 14:13:50 -0800

    Moving the PdfToPwgConverter service to a new chrome printing service.
    In preparation for more printing related servicification (PDF to EMF
    converter, printer capabilities...), creating a new Chrome printing
    service to host these interfaces and moving PdfToPwgRasterConverter
    service there.
    
    Also split the PdfRenderSettings from pdf_to_pwg_raster_converter.mojom
    to its own mojom/typemap/traits files as it will be used by other
    interfaces soon.
    
    There is no change in logic.
    
    Bug: 792225
    Change-Id: I2c71a9e035dafc1ade84e71da6d71a9ca38277cd

--- chrome/services/printing/pdf_to_pwg_raster_converter.h
+++ chrome/services/printing/pdf_to_pwg_raster_converter.h
@@ -2,14 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_UTILITY_PRINTING_PDF_TO_PWG_RASTER_CONVERTER_IMPL_H_
-#define CHROME_UTILITY_PRINTING_PDF_TO_PWG_RASTER_CONVERTER_IMPL_H_
+#ifndef CHROME_SERVICES_PRINTING_PDF_TO_PWG_RASTER_CONVERTER_H_
+#define CHROME_SERVICES_PRINTING_PDF_TO_PWG_RASTER_CONVERTER_H_
 
 #include <memory>
 
 #include "base/macros.h"
-#include "chrome/common/printing/pdf_to_pwg_raster_converter.mojom.h"
+#include "chrome/services/printing/public/interfaces/pdf_to_pwg_raster_converter.mojom.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
+#include "printing/pdf_render_settings.h"
 #include "services/service_manager/public/cpp/service_context_ref.h"
 
 namespace printing {
@@ -36,4 +37,4 @@ class PdfToPwgRasterConverterImpl
 
 }  // namespace printing
 
-#endif  // CHROME_UTILITY_PRINTING_PDF_TO_PWG_RASTER_CONVERTER_IMPL_H_
+#endif  // CHROME_SERVICES_PRINTING_PDF_TO_PWG_RASTER_CONVERTER_H_
