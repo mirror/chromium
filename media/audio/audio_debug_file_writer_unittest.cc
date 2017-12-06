daa93c26d0bf570d82dc4e278f8186c9ceaed7c8
Marina Ciocea <marinaciocea@chromium.org>
2017-12-06 10:17:00 +0100

    merge AudioDebugRecordingHelper and AudioDebugFileWriter into AudioDebugRecordingWriter
    Bug: 788657
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Idc5eb0c6f1bb8e108ef022e70ae0fbde40cfbd7a

--- media/audio/audio_debug_file_writer_unittest.cc
+++ media/audio/audio_debug_file_writer_unittest.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/*
 #include <stdint.h>
 
 #include "base/bind.h"
@@ -343,3 +344,4 @@ INSTANTIATE_TEST_CASE_P(
                              100)));
 
 }  // namespace media
+*/
