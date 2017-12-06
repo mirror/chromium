714548850c3f8ab948c4a515875563caf0574141
Stefan Zager <szager@chromium.org>
2017-12-05 21:46:17 -0800

    Enable root layer scrolling for try jobs 2017-12-05T21-45-06-0800
    ** DO NOT LAND **
    
    BUG=711468
    
    Change-Id: Ie3c3c6cf306cb8ed3c7212da9ba2d156590a44a6

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/run_webkit_tests.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/run_webkit_tests.py
@@ -44,6 +44,7 @@ _log = logging.getLogger(__name__)
 
 
 def main(argv, stdout, stderr):
+    argv.insert(0, '--additional-driver-flag=--root-layer-scrolls')
     options, args = parse_args(argv)
 
     if options.platform and 'test' in options.platform and not 'browser_test' in options.platform:
