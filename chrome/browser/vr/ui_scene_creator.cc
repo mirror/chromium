fc54bd808c13f9f9f646efe18c111bfda92a9416
Christopher Grant <cjgrant@chromium.org>
2017-12-06 16:16:44 -0500

    VR: Add omnibox hint text
    This change puts a prompt string into the omnibox whenever the field is
    empty.  Once a character is entered, or when the field later becomes
    empty, the hint is shown.
    
    BUG=
    R=bshe
    TBR=cpu@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ib05f091fb339a4c06d83a0502e1d0d1b51b0d277

--- chrome/browser/vr/ui_scene_creator.cc
+++ chrome/browser/vr/ui_scene_creator.cc
@@ -1125,6 +1125,8 @@ void UiSceneCreator::CreateOmnibox() {
       VR_BIND(TextInputInfo, Model, model_, omnibox_text_field_info,
               UiBrowserInterface, browser_, StartAutocomplete(value.text)));
   omnibox_text_field->SetSize(width, 0);
+  omnibox_text_field->SetHintText(
+      l10n_util::GetStringUTF16(IDS_SEARCH_OR_TYPE_URL));
   omnibox_text_field->set_name(kOmniboxTextField);
   omnibox_text_field->set_x_anchoring(LEFT);
   omnibox_text_field->set_x_centering(LEFT);
@@ -1143,6 +1145,8 @@ void UiSceneCreator::CreateOmnibox() {
             &TextInput::SetTextColor);
   BindColor(model_, omnibox_text_field.get(), &ColorScheme::cursor,
             &TextInput::SetCursorColor);
+  BindColor(model_, omnibox_text_field.get(), &ColorScheme::omnibox_hint,
+            &TextInput::SetHintColor);
 
   scene_->AddUiElement(kOmniboxContainer, std::move(omnibox_text_field));
 
