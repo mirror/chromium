a6f6e2f424679b92facea4de00d274791094fdba
Cary Clark <caryclark@google.com>
2017-12-06 15:25:15 -0500

    add flag for legacy SkPath arcTo behavior
    Skia upstream change keeps rounded rectangles
    constructed from SVG convex. This alters
    some existing SVG layout results. Disable
    the new behavior pending the rebaseline.
    
    TBR=djsollen@chromium.org
    Bug:b:69622768
    Change-Id: Ia32b9cffff4a011bc835eb014f3943e735d6775e

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -204,6 +204,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_USE_LEGACY_DISTANCE_FIELDS
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_BLUR_IMAGE
+#define SK_SUPPORT_LEGACY_BLUR_IMAGE
+#endif
+
 // Skia is enabling this feature soon. Chrome probably does
 // not want it for M64
 #ifndef SK_DISABLE_EXPLICIT_GPU_RESOURCE_ALLOCATION
@@ -220,6 +224,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_DELTA_AA
 #endif
 
+#ifndef SK_USE_LEGACY_INTERP_BLUR
+#define SK_USE_LEGACY_INTERP_BLUR
+#endif
+
 #ifndef SK_SUPPORT_LEGACY_SMALLRECT_AA
 #define SK_SUPPORT_LEGACY_SMALLRECT_AA
 #endif
@@ -232,6 +240,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_COLOR_SPACE_XFORM_LEGACY_PIPELINE
 #endif
 
+// remove after rebaselining svg layout tests
+#ifndef SK_SUPPORT_LEGACY_SVG_ARC_TO
+#define SK_SUPPORT_LEGACY_SVG_ARC_TO
+#endif
 
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
