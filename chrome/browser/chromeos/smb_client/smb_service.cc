305b3af667821fa69a5190c10de5b9fcca72e795
Bailey Berro <baileyberro@chromium.org>
2017-12-06 14:27:06 -0800

    Implementing Mount and Unmount for SmbFileSystem.
    Make SmbService::Mount api asynchronous.
    
    Bug: 778791
    Change-Id: I219214f381c1c78dd4d2a018ad909286a89e3719

--- chrome/browser/chromeos/smb_client/smb_service.cc
+++ chrome/browser/chromeos/smb_client/smb_service.cc
@@ -4,8 +4,11 @@
 
 #include "chrome/browser/chromeos/smb_client/smb_service.h"
 
+#include "base/strings/string_number_conversions.h"
 #include "chrome/browser/chromeos/file_system_provider/provided_file_system_info.h"
 #include "chrome/browser/chromeos/smb_client/smb_file_system.h"
+#include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/dbus/smb_provider_client.h"
 
 using chromeos::file_system_provider::Service;
 
@@ -15,15 +18,40 @@ namespace smb_client {
 SmbService::SmbService(Profile* profile)
     : profile_(profile),
       provider_id_(ProviderId::CreateFromNativeId("smb")),
-      capabilities_(false, false, false, extensions::SOURCE_NETWORK) {
+      capabilities_(false, false, false, extensions::SOURCE_NETWORK),
+      weak_ptr_factory_(this) {
   GetProviderService()->RegisterProvider(std::make_unique<SmbService>(profile));
 }
 
 SmbService::~SmbService() {}
 
-base::File::Error SmbService::Mount(
-    const file_system_provider::MountOptions& options) {
-  return GetProviderService()->MountFileSystem(provider_id_, options);
+void SmbService::Mount(const file_system_provider::MountOptions& options,
+                       const std::string& share_path,
+                       MountResponse callback) {
+  chromeos::DBusThreadManager::Get()->GetSmbProviderClient()->Mount(
+      share_path, base::BindOnce(&SmbService::OnMountResponse,
+                                 weak_ptr_factory_.GetWeakPtr(),
+                                 base::Passed(&callback), options));
+}
+
+void SmbService::OnMountResponse(
+    MountResponse callback,
+    const file_system_provider::MountOptions& options,
+    smbprovider::ErrorType error,
+    int32_t mount_id) {
+  if (error != smbprovider::ERROR_OK) {
+    std::move(callback).Run(std::move(SmbFileSystem::TranslateError(error)));
+    return;
+  }
+
+  file_system_provider::MountOptions mount_options(options);
+  mount_options.file_system_id = base::NumberToString(mount_id);
+
+  base::File::Error providerServiceMountResult =
+      GetProviderService()->MountFileSystem(
+          ProviderId::CreateFromNativeId("smb"), mount_options);
+
+  std::move(callback).Run(std::move(providerServiceMountResult));
 }
 
 Service* SmbService::GetProviderService() const {
