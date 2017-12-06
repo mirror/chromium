0825f6cb976ee242af49991e6c3b75c840f10b24
edchin <edchin@chromium.org>
2017-12-06 14:23:38 -0800

    [ios] ExternalAppLauncherTabHelper
    This CL pulls ExternalAppLauncher ownership out of Tab,
    which is an improvement in its own right.
    This change is also required to remove the use of rootVC
    in ExternalAppLauncher in a future CL.
    
    Bug: 791792
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I1d49629245483c7ca2e22e720ae20f90b0615b93

--- ios/chrome/browser/tabs/tab_unittest.mm
+++ ios/chrome/browser/tabs/tab_unittest.mm
@@ -32,7 +32,6 @@
 #import "ios/chrome/browser/tabs/tab_private.h"
 #import "ios/chrome/browser/ui/open_in_controller.h"
 #import "ios/chrome/browser/ui/open_in_controller_testing.h"
-#import "ios/chrome/browser/web/external_app_launcher.h"
 #import "ios/chrome/browser/web/tab_id_tab_helper.h"
 #include "ios/chrome/test/block_cleanup_test.h"
 #include "ios/chrome/test/ios_chrome_scoped_testing_chrome_browser_state_manager.h"
@@ -105,20 +104,6 @@ const char kValidFilenameUrl[] = "http://www.hostname.com/filename.pdf";
 
 - (void)closeTabAtIndex:(NSUInteger)index {
   [_tabsForTesting removeObjectAtIndex:index];
-}
-@end
-
-@interface ExternalAppLauncherMock : OCMockComplexTypeHelper
-@end
-
-@implementation ExternalAppLauncherMock
-typedef BOOL (^openURLBlockType)(const GURL&, const GURL&, BOOL);
-
-- (BOOL)requestToOpenURL:(const GURL&)url
-           sourcePageURL:(const GURL&)sourceURL
-             linkClicked:(BOOL)linkClicked {
-  return static_cast<openURLBlockType>([self blockForSelector:_cmd])(
-      url, sourceURL, linkClicked);
 }
 @end
 
@@ -201,10 +186,6 @@ class TabTest : public BlockCleanupTest {
     history::QueryResults results;
     QueryAllHistory(&results);
     EXPECT_EQ(0UL, results.size());
-    mock_external_app_launcher_ = [[ExternalAppLauncherMock alloc]
-        initWithRepresentedObject:
-            [OCMockObject mockForClass:[ExternalAppLauncher class]]];
-    [tab_ replaceExternalAppLauncher:mock_external_app_launcher_];
   }
 
   void TearDown() override {
@@ -314,7 +295,6 @@ class TabTest : public BlockCleanupTest {
   __weak CRWWebController* mock_web_controller_;
   UIView* web_controller_view_;
   ArrayTabModel* tabModel_;
-  id mock_external_app_launcher_;
   __weak Tab* tab_;
 };
 
