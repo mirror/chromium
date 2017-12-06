a4c575001f295017503842b21e879888cd26ed1b
Noel Gordon <noel@chromium.org>
2017-12-06 19:13:31 +1100

    Improve zlib inflate speed by using SSE chunk copy
    Using SSE2 chunk copies improves the decoding rate of the PNG 140
    corpus by an average 17%, giving a total 37% performance increase
    when combined with SIMD adler32 code (https://crbug.com/772870#c3
    for details).
    
    Move the arm-specific code back into the main chunk copy code and
    generalize the SIMD parts of chunkset_core() using macros for ARM
    and Intel devices. This removes the TODO from arm/chunkcopy_arm.h
    and that arm-specific file can also be deleted as a result.
    
    Change chunkcopy.h to use __builtin_memcpy if it's available, use
    zmemcpy otherwise such as on MSVC. Also call x86_check_features()
    in inflateInit2_() to keep the adler32 SIMD code path enabled.
    
    Update BUILD.gn to conditionally compile the SIMD chunk copy code
    on Intel SSE2 and ARM NEON devices. Update names.h to add the new
    symbol defined by the inflate chunk copy code path.
    
    Code had various comment styles; pick one and use it consistently
    everywhere.
    
    Bug: 772870
    Change-Id: I47004c68ee675acf418825fb0e1f8fa8018d4342

--- third_party/zlib/names.h
+++ third_party/zlib/names.h
@@ -171,4 +171,9 @@
 #define adler32_simd_ Cr_z_adler32_simd_
 #endif
 
+#if defined(INFLATE_FAST_CHUNK)
+/* Symbols added by contrib/optimizations/inffast_chunk */
+#define inflate_fast_chunk_ Cr_z_inflate_fast_chunk_
+#endif
+
 #endif  /* THIRD_PARTY_ZLIB_NAMES_H_ */
