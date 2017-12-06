768dbb9477093f4a27b704dab793120a4c36dc2b
k2.nagaraju <k2.nagaraju@samsung.com>
2017-12-06 19:46:16 +0530

    Update parsing of WebVTT regions to current/latest spec
    updated parsing logic
    updated test cases and expected results
    
    Bug:755487
    Change-Id: Ia5d55a117f5f5fa7217e7671337fa4bc7fafbf41

--- third_party/WebKit/Source/core/html/track/vtt/VTTParser.h
+++ third_party/WebKit/Source/core/html/track/vtt/VTTParser.h
@@ -66,6 +66,7 @@ class VTTParser final : public GarbageCollectedFinalized<VTTParser> {
     kId,
     kTimingsAndSettings,
     kCueText,
+    kRegion,
     kBadCue
   };
 
@@ -129,8 +130,7 @@ class VTTParser final : public GarbageCollectedFinalized<VTTParser> {
   void CreateNewCue();
   void ResetCueValues();
 
-  void CollectMetadataHeader(const String&);
-  void CreateNewRegion(const String& header_value);
+  VTTParser::ParseState CollectWebVTTBlock(const String&);
 
   static bool CollectTimeStamp(VTTScanner& input, double& time_stamp);
 
@@ -140,8 +140,9 @@ class VTTParser final : public GarbageCollectedFinalized<VTTParser> {
   double current_start_time_;
   double current_end_time_;
   StringBuilder current_content_;
+  String previous_line_;
   String current_settings_;
-
+  Member<VTTRegion> current_region_;
   Member<VTTParserClient> client_;
 
   HeapVector<Member<TextTrackCue>> cue_list_;
