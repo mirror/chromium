9deeebd98c4261deb7a5f509a7792c5fb14e7d84
Paul Jensen <pauljensen@chromium.org>
2017-12-07 14:04:31 -0500

    Plumb SocketTag through TCP and UDP socket classes.
    Exposes a Tag(SocketTag) API on StreamSocket and DatagramClientSocket.
    Deduplication of tagging is done at the TcpClientSocket and UdpClientSocket
    layers, which includes avoiding applying the default tag to newly created
    sockets.  This ensures that underlying TCPSocket::Tag() and UDPSocket::Tag()
    are never called on non-Android targets where tagging is not yet supported
    because it's not possible to create non-default SocketTags on these targets.
    Socket impls that wrap other sockets pass the tag down to the wrapped
    socket in cases where the wrapped socket only services the one wrapping
    socket.  Retagging is disallowed when the wrapped socket services multiple
    wrapping sockets, though the underlying socket can be tagged before being
    wrapped.
    
    Bug: 520198
    Change-Id: I53bf343409268a4c6a32f9a3f4f1a565bd44be0e

--- net/socket/udp_socket_posix.cc
+++ net/socket/udp_socket_posix.cc
@@ -35,6 +35,7 @@
 #include "net/log/net_log_source_type.h"
 #include "net/socket/socket_descriptor.h"
 #include "net/socket/socket_options.h"
+#include "net/socket/socket_tag.h"
 #include "net/socket/udp_net_log_parameters.h"
 
 #if defined(OS_ANDROID)
@@ -235,6 +236,8 @@ int UDPSocketPosix::Open(AddressFamily address_family) {
     Close();
     return err;
   }
+  if (tag_ != SocketTag())
+    tag_.Apply(socket_);
   return OK;
 }
 
@@ -1075,6 +1078,14 @@ int UDPSocketPosix::SetDiffServCodePoint(DiffServCodePoint dscp) {
 
 void UDPSocketPosix::DetachFromThread() {
   DETACH_FROM_THREAD(thread_checker_);
+}
+
+void UDPSocketPosix::ApplySocketTag(const SocketTag& tag) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  if (socket_ != kInvalidSocket && tag != tag_) {
+    tag.Apply(socket_);
+  }
+  tag_ = tag;
 }
 
 }  // namespace net
