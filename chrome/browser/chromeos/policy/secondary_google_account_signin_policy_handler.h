8a0a6aa978f9a0111f9cacb668452f0a40f7f5f6
Kush Sinha <sinhak@chromium.org>
2017-12-07 13:46:52 +0100

    Add policy for enforcing sign-in consistency between content area and OS
    Bug: 784760
    Change-Id: I037cbcde5cd776b1b5a57b4788ea6fe3fd27ad74

---
+++ chrome/browser/chromeos/policy/secondary_google_account_signin_policy_handler.h
@@ -0,0 +1,32 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_POLICY_SECONDARY_GOOGLE_ACCOUNT_SIGNIN_POLICY_HANDLER_H_
+#define CHROME_BROWSER_CHROMEOS_POLICY_SECONDARY_GOOGLE_ACCOUNT_SIGNIN_POLICY_HANDLER_H_
+
+#include "components/policy/core/browser/configuration_policy_handler.h"
+#include "components/policy/policy_export.h"
+
+namespace policy {
+
+// Handles secondary Google account sign-in (in the content area) policy.
+class POLICY_EXPORT SecondaryGoogleAccountSigninPolicyHandler
+    : public ConfigurationPolicyHandler {
+ public:
+  SecondaryGoogleAccountSigninPolicyHandler();
+  ~SecondaryGoogleAccountSigninPolicyHandler() override;
+
+  // ConfigurationPolicyHandler methods:
+  bool CheckPolicySettings(const PolicyMap& policies,
+                           PolicyErrorMap* errors) override;
+  void ApplyPolicySettings(const PolicyMap& policies,
+                           PrefValueMap* prefs) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(SecondaryGoogleAccountSigninPolicyHandler);
+};
+
+}  // namespace policy
+
+#endif  // CHROME_BROWSER_CHROMEOS_POLICY_SECONDARY_GOOGLE_ACCOUNT_SIGNIN_POLICY_HANDLER_H_
