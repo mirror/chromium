3a9f2a57749d12c71d6bcef01bb39c7820359b7f
Hiroki Nakagawa <nhiroki@chromium.org>
2017-12-07 10:45:04 +0900

    Worker: Implement WorkerOptions
    Bug: 680046
    Change-Id: I571e8f540d842bb06878e13d4b383eddc2df42f1

--- third_party/WebKit/Source/core/workers/DedicatedWorker.cpp
+++ third_party/WebKit/Source/core/workers/DedicatedWorker.cpp
@@ -30,6 +30,7 @@ namespace blink {
 
 DedicatedWorker* DedicatedWorker::Create(ExecutionContext* context,
                                          const String& url,
+                                         const WorkerOptions& options,
                                          ExceptionState& exception_state) {
   DCHECK(IsMainThread());
   Document* document = ToDocument(context);
@@ -45,15 +46,17 @@ DedicatedWorker* DedicatedWorker::Create(ExecutionContext* context,
   if (!script_url.IsValid())
     return nullptr;
 
-  DedicatedWorker* worker = new DedicatedWorker(context, script_url);
+  DedicatedWorker* worker = new DedicatedWorker(context, script_url, options);
   worker->Start();
   return worker;
 }
 
 DedicatedWorker::DedicatedWorker(ExecutionContext* context,
-                                 const KURL& script_url)
+                                 const KURL& script_url,
+                                 const WorkerOptions& options)
     : AbstractWorker(context),
       script_url_(script_url),
+      options_(options),
       context_proxy_(CreateMessagingProxy(context)) {
   DCHECK(IsMainThread());
   DCHECK(script_url_.IsValid());
