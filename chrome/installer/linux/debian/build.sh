3097d7b0fc8a2607a3a78d708638d9f17623840b
Tom Anderson <thomasanderson@chromium.org>
2017-12-06 16:31:47 -0800

    [Linux packager] Add check that RPATH is unset in binaries
    BUG=584597
    R=thestig@chromium.org
    
    Change-Id: Ie131b7eb5f994fef3a0808992ec02fa8aa6beed7

--- chrome/installer/linux/debian/build.sh
+++ chrome/installer/linux/debian/build.sh
@@ -135,7 +135,7 @@ cleanup() {
 
 usage() {
   echo "usage: $(basename $0) [-a target_arch] [-b 'dir'] -c channel"
-  echo "                      -d branding [-f] [-o 'dir'] -s 'dir'"
+  echo "                      -d branding [-f] [-o 'dir'] -s 'dir' -t target_os"
   echo "-a arch     package architecture (ia32 or x64)"
   echo "-b dir      build input directory    [${BUILDDIR}]"
   echo "-c channel  the package channel (unstable, beta, stable)"
@@ -144,6 +144,7 @@ usage() {
   echo "-h          this help message"
   echo "-o dir      package output directory [${OUTPUTDIR}]"
   echo "-s dir      /path/to/sysroot"
+  echo "-t platform target platform"
 }
 
 # Check that the channel name is one of the allowable ones.
@@ -171,7 +172,7 @@ verify_channel() {
 }
 
 process_opts() {
-  while getopts ":a:b:c:d:fho:s:" OPTNAME
+  while getopts ":a:b:c:d:fho:s:t:" OPTNAME
   do
     case $OPTNAME in
       a )
@@ -199,6 +200,9 @@ process_opts() {
         ;;
       s )
         SYSROOT="$OPTARG"
+        ;;
+      t )
+        TARGET_OS="$OPTARG"
         ;;
      \: )
         echo "'-$OPTARG' needs an argument."
