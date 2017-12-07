26c9b14fa871e6b9ac56a315ef7eb5cfaae4e74d
Karan Bhatia <karandeepb@chromium.org>
2017-12-06 20:24:48 -0800

    Extensions: Deprecate the redundant idltest permission.
    Idltest is a trunk-only API used in tests. This permission is unused currently.
    
    BUG=792323
    
    Change-Id: Ic40b6c666fc7f71f3bf2154cceb398535a68f155

--- chrome/common/extensions/permissions/permission_set_unittest.cc
+++ chrome/common/extensions/permissions/permission_set_unittest.cc
@@ -746,7 +746,6 @@ TEST(PermissionsTest, PermissionMessages) {
   skip.insert(APIPermission::kIdle);
   skip.insert(APIPermission::kImeWindowEnabled);
   skip.insert(APIPermission::kInlineInstallPrivate);
-  skip.insert(APIPermission::kIdltest);
   skip.insert(APIPermission::kOverrideEscFullscreen);
   skip.insert(APIPermission::kPointerLock);
   skip.insert(APIPermission::kPower);
