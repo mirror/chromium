df4baa25f3a6cf22b6bfc0e54ad4dec401287463
arthursonzogni <arthursonzogni@chromium.org>
2017-12-08 10:05:20 +0100

    TEST: disable NetworkHandler::SetExtraHTTPHeaders
    Bug: 793031
    Change-Id: Iade9b0b9549fe2fba488d87e2bf22a2d5ac1d35b

--- content/browser/devtools/protocol/network_handler.cc
+++ content/browser/devtools/protocol/network_handler.cc
@@ -925,20 +925,20 @@ Response NetworkHandler::SetUserAgentOverride(const std::string& user_agent) {
 
 Response NetworkHandler::SetExtraHTTPHeaders(
     std::unique_ptr<protocol::Network::Headers> headers) {
-  std::vector<std::pair<std::string, std::string>> new_headers;
-  std::unique_ptr<protocol::DictionaryValue> object = headers->toValue();
-  for (size_t i = 0; i < object->size(); ++i) {
-    auto entry = object->at(i);
-    std::string value;
-    if (!entry.second->asString(&value))
-      return Response::InvalidParams("Invalid header value, string expected");
-    if (!net::HttpUtil::IsValidHeaderName(entry.first))
-      return Response::InvalidParams("Invalid header name");
-    if (!net::HttpUtil::IsValidHeaderValue(value))
-      return Response::InvalidParams("Invalid header value");
-    new_headers.emplace_back(entry.first, value);
-  }
-  extra_headers_.swap(new_headers);
+  // std::vector<std::pair<std::string, std::string>> new_headers;
+  // std::unique_ptr<protocol::DictionaryValue> object = headers->toValue();
+  // for (size_t i = 0; i < object->size(); ++i) {
+  // auto entry = object->at(i);
+  // std::string value;
+  // if (!entry.second->asString(&value))
+  // return Response::InvalidParams("Invalid header value, string expected");
+  // if (!net::HttpUtil::IsValidHeaderName(entry.first))
+  // return Response::InvalidParams("Invalid header name");
+  // if (!net::HttpUtil::IsValidHeaderValue(value))
+  // return Response::InvalidParams("Invalid header value");
+  // new_headers.emplace_back(entry.first, value);
+  //}
+  // extra_headers_.swap(new_headers);
   return Response::FallThrough();
 }
 
