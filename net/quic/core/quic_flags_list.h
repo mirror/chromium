bd37efad27aae756723245cb6717b72d07d5f181
Dan Zhang <danzh@chromium.org>
2017-12-08 14:07:03 -0500

    Deprecate FLAGS_quic_reloadable_flag_quic_use_tls13_cipher_suites
    Merge internal change: 177236310
    
    R=rch@chromium.org
    
    Bug:
    Change-Id: Ie8f258d51e2732dbdbc290733bc40d3246dfcfaf

--- net/quic/core/quic_flags_list.h
+++ net/quic/core/quic_flags_list.h
@@ -121,10 +121,6 @@ QUIC_FLAG(bool, FLAGS_quic_reloadable_flag_quic_use_mem_slices, false)
 // Allow QUIC to accept initial packet numbers that are random, not 1.
 QUIC_FLAG(bool, FLAGS_quic_restart_flag_quic_enable_accept_random_ipn, false)
 
-// Report the more analogous TLS 1.3 cipher suites rather than TLS 1.2 ECDHE_RSA
-// ciphers in QuicDecrypters.
-QUIC_FLAG(bool, FLAGS_quic_reloadable_flag_quic_use_tls13_cipher_suites, true)
-
 // If true, read and write QUIC version labels in network byte order.
 QUIC_FLAG(bool,
           FLAGS_quic_reloadable_flag_quic_use_net_byte_order_version_label,
