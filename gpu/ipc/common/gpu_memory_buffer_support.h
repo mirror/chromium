03d9b86a6a7917af60284d046d4c7dcfcdacaa14
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-12-08 13:30:07 -0500

    Move two service functions to from //gpu/ipc/host/ to common/
    This CL moves two methods, namely:
    - AreNativeGpuMemoryBuffersEnabled()
    - GetNativeGpuMemoryBufferConfigurations()
    from //gpu/ipc/host/gpu_memory_buffer_support to the homonymous file
    in //gpu/ipc/common, so they can be used in e.g. crrev.com/c/812934,
    and because it makes more sense to have common methods in common
    than inside the host.
    
    It also moves gpu/ipc/host/gpu_switches.* to gpu/ipc/common
    to follow up the previous movements.
    
    Also changes a long find() into a base::ContainsKey().
    
    Bug: 791676
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ieaacde1ba3aa76d2b99fa61e7d47ffaadcd20837

--- gpu/ipc/common/gpu_memory_buffer_support.h
+++ gpu/ipc/common/gpu_memory_buffer_support.h
@@ -5,11 +5,40 @@
 #ifndef GPU_IPC_COMMON_GPU_MEMORY_BUFFER_SUPPORT_H_
 #define GPU_IPC_COMMON_GPU_MEMORY_BUFFER_SUPPORT_H_
 
+#include "base/containers/hash_tables.h"
+#include "base/hash.h"
 #include "gpu/gpu_export.h"
 #include "ui/gfx/buffer_types.h"
 #include "ui/gfx/gpu_memory_buffer.h"
 
 namespace gpu {
+
+using GpuMemoryBufferConfigurationKey =
+    std::pair<gfx::BufferFormat, gfx::BufferUsage>;
+using GpuMemoryBufferConfigurationSet =
+    base::hash_set<GpuMemoryBufferConfigurationKey>;
+
+}  // namespace gpu
+
+namespace BASE_HASH_NAMESPACE {
+
+template <>
+struct hash<gpu::GpuMemoryBufferConfigurationKey> {
+  size_t operator()(const gpu::GpuMemoryBufferConfigurationKey& key) const {
+    return base::HashInts(static_cast<int>(key.first),
+                          static_cast<int>(key.second));
+  }
+};
+
+}  // namespace BASE_HASH_NAMESPACE
+
+namespace gpu {
+
+GPU_EXPORT bool AreNativeGpuMemoryBuffersEnabled();
+
+// Returns the set of supported configurations.
+GPU_EXPORT GpuMemoryBufferConfigurationSet
+GetNativeGpuMemoryBufferConfigurations();
 
 // Returns the native GPU memory buffer factory type. Returns EMPTY_BUFFER
 // type if native buffers are not supported.
