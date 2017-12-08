ca466a9cb5660a0b8ddf7688c1b9a89acc0dea4e
Hirokazu Honda <hiroh@chromium.org>
2017-12-08 11:01:52 +0900

    Refactor GPUArcVDA based on new VDA-like IPC design
    Implement GPUArcVDA working with new VDA-like IPC. GpuArcVDA currently
    translates mojo IPC to VDA API, and therefore ChromeArcVDA is deleted.
    ChromeArcVDA and GpuArcVDA based on old IPC still exist as deprecated classes
    for compatibility, so that the old IPC can be used.
    
    TEST=CTS MediaTest on veyron_minnie and eve
    TEST=CTS MediaTest with --mus=viz on veyron_minnie and eve
    TEST=Secure Playback test using ExoPlayer
    BUG=chromium:745457
    CQ-DEPEND=CL:609564
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I9d90a6945c81e20572616c04c1d4f496e2bb3f87

--- components/arc/video_accelerator/video_accelerator_struct_traits.cc
+++ components/arc/video_accelerator/video_accelerator_struct_traits.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/arc/common/video_accelerator_struct_traits.h"
+#include "components/arc/video_accelerator/video_accelerator_struct_traits.h"
 
 namespace mojo {
 
@@ -51,9 +51,6 @@ CHECK_PROFILE_ENUM(DOLBYVISION_MAX);
 CHECK_PROFILE_ENUM(THEORAPROFILE_MIN);
 CHECK_PROFILE_ENUM(THEORAPROFILE_ANY);
 CHECK_PROFILE_ENUM(THEORAPROFILE_MAX);
-CHECK_PROFILE_ENUM(AV1PROFILE_MIN);
-CHECK_PROFILE_ENUM(AV1PROFILE_PROFILE0);
-CHECK_PROFILE_ENUM(AV1PROFILE_MAX);
 CHECK_PROFILE_ENUM(VIDEO_CODEC_PROFILE_MAX);
 
 #undef CHECK_PROFILE_ENUM
@@ -95,7 +92,6 @@ bool EnumTraits<arc::mojom::VideoCodecProfile, media::VideoCodecProfile>::
     case arc::mojom::VideoCodecProfile::DOLBYVISION_PROFILE5:
     case arc::mojom::VideoCodecProfile::DOLBYVISION_PROFILE7:
     case arc::mojom::VideoCodecProfile::THEORAPROFILE_ANY:
-    case arc::mojom::VideoCodecProfile::AV1PROFILE_MIN:
       *output = static_cast<media::VideoCodecProfile>(input);
       return true;
   }
