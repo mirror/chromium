604b410e13d89fbbaa5e5f3398b565b2b12868ae
Helen Li <xunjieli@chromium.org>
2017-12-08 09:46:50 -0500

    Revert recent CLs landed in HttpStreamFactoryImpl::JobController
    This CL reverts the following commits to investigate whether these
    CLs had caused crbug.com/767577.
    
    Revert "Resume HTTP job when retrying request with different proxy"
    
    This reverts commit 7c2e70a2d9251e49b4d72e2344de54d031a5b228.
    
    Revert "Check main job is not null when resuming"
    
    This reverts commit 9c858316907fbb206ef0e4e48506188d080dd1dc.
    
    Revert "Cancel any pending ResumeMainJob task"
    
    This reverts commit d5f1f0821489a8311dc437fc6b70ac0b0d72b28b.
    
    Bug: 789560,790776,787148,767577
    Change-Id: I81d96dab2592271c00ba213e956181033509cd80

--- net/http/http_stream_factory_impl_job_controller.cc
+++ net/http/http_stream_factory_impl_job_controller.cc
@@ -559,19 +559,17 @@ void HttpStreamFactoryImpl::JobController::ResumeMainJobLater(
     const base::TimeDelta& delay) {
   net_log_.AddEvent(NetLogEventType::HTTP_STREAM_JOB_DELAYED,
                     NetLog::Int64Callback("delay", delay.InMilliseconds()));
-  resume_main_job_callback_.Reset(
-      base::BindOnce(&HttpStreamFactoryImpl::JobController::ResumeMainJob,
-                     ptr_factory_.GetWeakPtr()));
   base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
-      FROM_HERE, resume_main_job_callback_.callback(), delay);
+      FROM_HERE,
+      base::Bind(&HttpStreamFactoryImpl::JobController::ResumeMainJob,
+                 ptr_factory_.GetWeakPtr()),
+      delay);
 }
 
 void HttpStreamFactoryImpl::JobController::ResumeMainJob() {
-  DCHECK(main_job_);
-
-  if (main_job_is_resumed_) {
+  if (main_job_is_resumed_)
     return;
-  }
+
   main_job_is_resumed_ = true;
   main_job_->net_log().AddEvent(
       NetLogEventType::HTTP_STREAM_JOB_RESUMED,
@@ -1320,13 +1318,6 @@ int HttpStreamFactoryImpl::JobController::ReconsiderProxyAfterError(Job* job,
     bound_job_ = nullptr;
     alternative_job_.reset();
     main_job_.reset();
-    // Also resets states that related to the old main job. In particular,
-    // cancels |resume_main_job_callback_| so there won't be any delayed
-    // ResumeMainJob() left in the task queue.
-    resume_main_job_callback_.Cancel();
-    main_job_is_resumed_ = false;
-    main_job_is_blocked_ = false;
-
     next_state_ = STATE_RESOLVE_PROXY_COMPLETE;
   } else {
     // If ReconsiderProxyAfterError() failed synchronously, it means
