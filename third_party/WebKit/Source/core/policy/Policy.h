b2e402e72dcb4c192b0891aff8b8a5ac1025c976
Luna Lu <loonybear@chromium.org>
2017-12-08 14:22:40 -0500

    JS exposure of feature policy (2)
    The feature policy can be inspected via the following methods:
    allowsFeature(feature, optional origin)
    allowedFeatures()
    getAllowlistForFeature(feature)
    
    Previously feature policy is exposed via document (https://chromium-review.googlesource.com/c/chromium/src/+/686036).
    This CL allow feature policy introspection in iframes.
    
    Bug: 703703
    Change-Id: I26ab245234c771330a72759f983807fbdd284781

--- third_party/WebKit/Source/core/policy/Policy.h
+++ third_party/WebKit/Source/core/policy/Policy.h
@@ -8,17 +8,29 @@
 #include "core/CoreExport.h"
 #include "platform/bindings/ScriptWrappable.h"
 #include "platform/heap/Member.h"
+#include "third_party/WebKit/common/feature_policy/feature_policy.h"
 
 namespace blink {
 
 class Document;
+class SecurityOrigin;
 
-class CORE_EXPORT Policy final : public ScriptWrappable {
+class CORE_EXPORT Policy : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
+  enum Type { DocumentPolicy = 1, FramePolicy = 2 };
+
   ~Policy() {}
-  static Policy* Create(Document*);
+  // Create a Policy, either or DocumentPolicy or a FramePolicy, specified by
+  // type.
+  static Policy* Create(
+      Type,
+      Document*,
+      const ParsedFeaturePolicy& container_policy = {},
+      scoped_refptr<const SecurityOrigin> src_origin = nullptr);
+
+  // Implementation of methods of the policy interface:
   // Returns whether or not the given feature is allowed on the origin of the
   // document that owns the policy.
   bool allowsFeature(const String& feature) const;
@@ -31,16 +43,24 @@ class CORE_EXPORT Policy final : public ScriptWrappable {
   // URL.
   Vector<String> getAllowlistForFeature(const String& url) const;
 
-  void Trace(blink::Visitor*);
+  // Inform the Policy object when the container policy on its frame element has
+  // changed.
+  virtual void UpdateContainerPolicy(
+      const ParsedFeaturePolicy& container_policy = {},
+      scoped_refptr<const SecurityOrigin> src_origin = nullptr) = 0;
+
+  virtual void Trace(blink::Visitor*);
+
+ protected:
+  virtual const FeaturePolicy* GetPolicy() const = 0;
+  // Get the containing document.
+  virtual Document* GetDocument() const = 0;
 
  private:
-  // Add console message to |document_|.
+  // Add console message to the containing document.
   void AddWarningForUnrecognizedFeature(const String& message) const;
-
-  explicit Policy(Document*);
-  Member<Document> document_;
 };
 
 }  // namespace blink
 
-#endif
+#endif  // Policy_h
