683e8a1ea5c256736c6f71f2b24a35b7d3e33d78
Nicholas Verne <nverne@chromium.org>
2017-12-08 13:47:32 +1100

    Implements AppCacheRenderer for appcache messages sent from host to the renderer.
    As a result of this, several intermediate classes that passed MojoPipeHandle
    over IPC now pass the URLLoaderFactoryPtr over mojo. Also,
    AppCacheDispatcherHost no longer needs to be an IPC::Sender.
    
    Further simplifications of the AppCache class hierarchy should now be possible,
    but are out of scope for this CL.
    
    Bug: 611938
    Change-Id: I5c05f66fbbab0dc4a2afa1aa6979b2ba0ff4f1f4

--- content/browser/appcache/appcache_dispatcher_host.h
+++ content/browser/appcache/appcache_dispatcher_host.h
@@ -26,22 +26,17 @@ class ChromeAppCacheService;
 // its child processes. There is a distinct host for each child process.
 // Messages are handled on the IO thread. The RenderProcessHostImpl creates
 // an instance and delegates calls to it.
-class AppCacheDispatcherHost : public IPC::Sender,
-                               public mojom::AppCacheBackend {
+class AppCacheDispatcherHost : public mojom::AppCacheBackend {
  public:
   AppCacheDispatcherHost(ChromeAppCacheService* appcache_service,
-                         int process_id,
-                         base::WeakPtr<IPC::Sender> sender);
+                         int process_id);
   ~AppCacheDispatcherHost() override;
 
   static void Create(ChromeAppCacheService* appcache_service,
                      int process_id,
-                     base::WeakPtr<IPC::Sender> sender,
                      mojom::AppCacheBackendRequest request);
 
-  void InitBackend();
-
-  bool Send(IPC::Message* msg) override;
+  void InitBackend(int process_id);
 
  private:
   // mojom::AppCacheHost
@@ -66,11 +61,6 @@ class AppCacheDispatcherHost : public IPC::Sender,
   scoped_refptr<ChromeAppCacheService> appcache_service_;
   AppCacheFrontendProxy frontend_proxy_;
   AppCacheBackendImpl backend_impl_;
-
-  // The corresponding ChildProcessHost object's id().
-  int process_id_;
-
-  base::WeakPtr<IPC::Sender> sender_;
 
   base::WeakPtrFactory<AppCacheDispatcherHost> weak_factory_;
 
