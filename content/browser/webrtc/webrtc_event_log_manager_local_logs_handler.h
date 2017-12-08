d6ac6d0afdfb7fdd47ef4cf8f08d565ece7c393d
Elad Alon <eladalon@chromium.org>
2017-12-08 15:01:16 +0100

    Refactor WebRtcEventLogManager
    Bug:
    Change-Id: I4dc116911540288203645d8a0494e4b381f079a1

---
+++ content/browser/webrtc/webrtc_event_log_manager_local_logs_handler.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_WEBRTC_WEBRTC_RTC_EVENT_LOG_MANAGER_LOCAL_LOGS_HANDLER_H_
+#define CONTENT_BROWSER_WEBRTC_WEBRTC_RTC_EVENT_LOG_MANAGER_LOCAL_LOGS_HANDLER_H_
+
+namespace content {
+
+class CONTENT_EXPORT WebRtcEventLogManager {
+ public:
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(WebRtcEventLogManagerLocalLogsHandler);
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_WEBRTC_WEBRTC_RTC_EVENT_LOG_MANAGER_LOCAL_LOGS_HANDLER_H_
