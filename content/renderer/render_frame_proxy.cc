214b11ce31abfb7902f3f0a870e2241332d534b4
Scott Violet <sky@chromium.org>
2017-12-08 08:44:44 -0800

    chromeos: fix oopifs for mus
    We need to ignore FrameSinkIds from mus when not running with viz.
    
    BUG=786453
    TEST=covered by tests
    
    Change-Id: I36c0ae7da151866ec7464846cb3498b1d6b3bc2c

--- content/renderer/render_frame_proxy.cc
+++ content/renderer/render_frame_proxy.cc
@@ -708,6 +708,9 @@ void RenderFrameProxy::OnMusEmbeddedFrameSurfaceChanged(
 
 void RenderFrameProxy::OnMusEmbeddedFrameSinkIdAllocated(
     const viz::FrameSinkId& frame_sink_id) {
+  if (!switches::IsMusHostingViz())
+    return;
+
   frame_sink_id_ = frame_sink_id;
   // Resend the FrameRects and allocate a new viz::LocalSurfaceId when the view
   // changes.
