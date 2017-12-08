d837c635085c2f6d20c7a4b0fb73a741d4fe934e
Shubhie Panicker <panicker@chromium.org>
2017-12-07 16:12:23 -0800

    Prototype to stop more task queues.
    This extends stopping to other task queues, beyond
    timers and loading. This will be used to test various
    breaking scenarios and edge cases.
    Clean up flag structuring as part of this.
    
    Bug:
    Change-Id: I98c2a863ffb1aa5af711a026b764ba7dff928441

--- third_party/WebKit/Source/platform/scheduler/renderer/web_frame_scheduler_impl.cc
+++ third_party/WebKit/Source/platform/scheduler/renderer/web_frame_scheduler_impl.cc
@@ -55,6 +55,10 @@ const char* CrossOriginStateToString(bool is_cross_origin) {
   }
 }
 
+bool StopNonTimersInBackgroundEnabled() {
+  return RuntimeEnabledFeatures::StopNonTimersInBackgroundEnabled();
+}
+
 }  // namespace
 
 WebFrameSchedulerImpl::ActiveConnectionHandleImpl::ActiveConnectionHandleImpl(
@@ -334,6 +338,7 @@ scoped_refptr<TaskQueue> WebFrameSchedulerImpl::DeferrableTaskQueue() {
             MainThreadTaskQueue::QueueType::kFrameThrottleable)
             .SetShouldReportWhenExecutionBlocked(true)
             .SetCanBeDeferred(true)
+            .SetCanBeStopped(StopNonTimersInBackgroundEnabled())
             .SetCanBePaused(true));
     deferrable_task_queue_->SetBlameContext(blame_context_);
     deferrable_task_queue_->SetFrameScheduler(this);
@@ -351,6 +356,7 @@ scoped_refptr<TaskQueue> WebFrameSchedulerImpl::PausableTaskQueue() {
         MainThreadTaskQueue::QueueCreationParams(
             MainThreadTaskQueue::QueueType::kFramePausable)
             .SetShouldReportWhenExecutionBlocked(true)
+            .SetCanBeStopped(StopNonTimersInBackgroundEnabled())
             .SetCanBePaused(true));
     pausable_task_queue_->SetBlameContext(blame_context_);
     pausable_task_queue_->SetFrameScheduler(this);
@@ -503,7 +509,7 @@ void WebFrameSchedulerImpl::UpdateThrottlingState() {
 
 WebFrameScheduler::ThrottlingState
 WebFrameSchedulerImpl::CalculateThrottlingState() const {
-  if (RuntimeEnabledFeatures::StopLoadingInBackgroundAndroidEnabled() &&
+  if (RuntimeEnabledFeatures::StopLoadingInBackgroundEnabled() &&
       page_stopped_) {
     DCHECK(!page_visible_);
     return WebFrameScheduler::ThrottlingState::kStopped;
