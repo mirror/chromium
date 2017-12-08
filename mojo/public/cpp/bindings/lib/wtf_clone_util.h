0b59d81409e336f56f26eb03e0801ab7bd7f90bd
Sam McNally <sammc@chromium.org>
2017-12-08 21:06:54 +1100

    Move ManifestManager into blink.
    Bug: 704441
    Change-Id: Ic0aa89aad78245f167b83df7342f1caa8b1908a8

---
+++ mojo/public/cpp/bindings/lib/wtf_clone_util.h
@@ -0,0 +1,44 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MOJO_PUBLIC_CPP_BINDINGS_LIB_WTF_CLONE_UTIL_H_
+#define MOJO_PUBLIC_CPP_BINDINGS_LIB_WTF_CLONE_UTIL_H_
+
+#include <type_traits>
+
+#include "mojo/public/cpp/bindings/clone_traits.h"
+#include "mojo/public/cpp/bindings/equals_traits.h"
+#include "third_party/WebKit/Source/platform/wtf/HashMap.h"
+#include "third_party/WebKit/Source/platform/wtf/Optional.h"
+#include "third_party/WebKit/Source/platform/wtf/Vector.h"
+#include "third_party/WebKit/Source/platform/wtf/text/WTFString.h"
+
+namespace mojo {
+
+template <typename T>
+struct CloneTraits<WTF::Vector<T>, false> {
+  static WTF::Vector<T> Clone(const WTF::Vector<T>& input) {
+    WTF::Vector<T> result;
+    result.ReserveCapacity(input.size());
+    for (const auto& element : input)
+      result.push_back(mojo::Clone(element));
+
+    return result;
+  }
+};
+
+template <typename K, typename V>
+struct CloneTraits<WTF::HashMap<K, V>, false> {
+  static WTF::HashMap<K, V> Clone(const WTF::HashMap<K, V>& input) {
+    WTF::HashMap<K, V> result;
+    for (const auto& element : input)
+      result.insert(mojo::Clone(element.key), mojo::Clone(element.value));
+
+    return result;
+  }
+};
+
+}  // namespace mojo
+
+#endif  // MOJO_PUBLIC_CPP_BINDINGS_LIB_WTF_CLONE_UTIL_H_
