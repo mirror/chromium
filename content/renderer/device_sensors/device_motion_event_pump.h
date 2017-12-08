bd7067f3d77031f5eb5a27ecdd7c096bd1d33f6c
Jun Cai <juncai@chromium.org>
2017-12-07 16:22:34 -0800

    Add flag to make sure SensorEntry::sensor only binds once
    This CL adds a |initializing_sensor| flag to make sure during the sensor
    initialization, any extra GetSensor() calls will have no effects.
    
    Bug: 788965
    Change-Id: Id20c745377b1320281dce72f097792c5b478cc38

--- content/renderer/device_sensors/device_motion_event_pump.h
+++ content/renderer/device_sensors/device_motion_event_pump.h
@@ -32,16 +32,14 @@ class CONTENT_EXPORT DeviceMotionEventPump
   // DeviceSensorEventPump:
   void FireEvent() override;
 
+  void SendStartMessageImpl();
+
   SensorEntry accelerometer_;
   SensorEntry linear_acceleration_sensor_;
   SensorEntry gyroscope_;
 
  private:
-  FRIEND_TEST_ALL_PREFIXES(DeviceMotionEventPumpTest,
-                           SensorInitializedButItsSharedBufferIsNot);
-
-  // DeviceSensorEventPump:
-  bool SensorSharedBuffersReady() const override;
+  friend class DeviceMotionEventPumpTest;
 
   void GetDataFromSharedMemory(device::MotionData* data);
 
