4adb5ddc365a44065550e7268d3dd4fb182d6528
Aaron Leventhal <aleventhal@chromium.org>
2017-12-09 15:14:35 -0500

    Do not treat omnibox search as answer
    When arrow to search with screen reader, do not say "answer" unless there
    really is an answer (such as the current weather).
    
    Bug: 793468
    Change-Id: Ic7de2826796f1f62b552940c3b8bfefd1f2878a4

--- components/omnibox/browser/autocomplete_match_type_unittest.cc
+++ components/omnibox/browser/autocomplete_match_type_unittest.cc
@@ -5,6 +5,7 @@
 #include "components/omnibox/browser/autocomplete_match_type.h"
 
 #include "base/strings/utf_string_conversions.h"
+#include "components/omnibox/browser/autocomplete_match.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 TEST(AutocompleteMatchTypeTest, AccessibilityLabels) {
@@ -12,9 +13,10 @@ TEST(AutocompleteMatchTypeTest, AccessibilityLabels) {
       base::UTF8ToUTF16("https://www.chromium.org");
   const base::string16& kTestTitle = base::UTF8ToUTF16("The Chromium Projects");
 
-  EXPECT_EQ(kTestUrl, AutocompleteMatchType::ToAccessibilityLabel(
-                          AutocompleteMatchType::URL_WHAT_YOU_TYPED, kTestUrl,
-                          kTestTitle));
+  AutocompleteMatch match;
+  match.type = AutocompleteMatchType::URL_WHAT_YOU_TYPED;
+  EXPECT_EQ(kTestUrl,
+            AutocompleteMatchType::ToAccessibilityLabel(match, kTestUrl));
   EXPECT_EQ(kTestTitle + base::UTF8ToUTF16(" ") + kTestUrl +
                 base::UTF8ToUTF16(" location from history"),
             AutocompleteMatchType::ToAccessibilityLabel(
