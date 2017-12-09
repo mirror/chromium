b92f9bc83f83412f60456381b0f4b95e702f3058
Joy Ming <jming@chromium.org>
2017-12-08 17:59:47 -0800

    [Downloads location] Create a preference for downloads location.
    This CL is part of the greater effort to build out functionality to
    allow users to change their default download storage location. This is
    the first step, surfacing the option to change download location in the
    preferences pane. This is not fully functional (the directory selector
    is not yet implemented) and is hidden behind a flag.
    
    Bug:792775
    Change-Id: Ic57aa2e4f61c83861e5711c2fe0b48737217904e

---
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/DownloadPreferences.java
@@ -0,0 +1,72 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.preferences;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+import org.chromium.chrome.R;
+
+/**
+ * Fragment to keep track of all downloads related preferences.
+ */
+public class DownloadPreferences extends PreferenceFragment {
+    private static final String PREF_LOCATION_CHANGE = "location_change";
+    private static final int REQUEST_CODE_LOCATION_CHANGE = 1;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActivity().setTitle(R.string.prefs_download);
+        PreferenceUtils.addPreferencesFromResource(this, R.xml.download_preferences);
+
+        Preference locationChangePref = (Preference) findPreference(PREF_LOCATION_CHANGE);
+        locationChangePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                // TODO(jming): Implement actual directory selector.
+                if (Build.VERSION.SDK_INT >= 21) {
+                    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+                    startActivityForResult(intent, REQUEST_CODE_LOCATION_CHANGE);
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    intent.setType("file/*");
+                    startActivityForResult(intent, REQUEST_CODE_LOCATION_CHANGE);
+                }
+                return true;
+            }
+        });
+
+        updateSummaries();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_LOCATION_CHANGE && resultCode == Activity.RESULT_OK) {
+            if (data == null || data.getData() == null) return;
+
+            // TODO(jming): Update download default directory in preferences.
+            // PrefServiceBridge prefServiceBridge = PrefServiceBridge.getInstance();
+            // prefServiceBridge.setDownloadDefaultDirectory(data.getData().getPath());
+
+            updateSummaries();
+        }
+    }
+
+    public void updateSummaries() {
+        PrefServiceBridge prefServiceBridge = PrefServiceBridge.getInstance();
+
+        Preference locationChangePref = (Preference) findPreference(PREF_LOCATION_CHANGE);
+        if (locationChangePref != null) {
+            locationChangePref.setSummary(prefServiceBridge.getDownloadDefaultDirectory());
+        }
+    }
+}
