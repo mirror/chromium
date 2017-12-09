165922e5be498c0c2c17716c3497995c3b80d554
Evan Stade <estade@chromium.org>
2017-12-08 19:44:20 -0700

    Fix Linux CFI bot by fixing ExtensionStorageMonitorTest.
    Reland 37bd8dbf991d69ecf3bc3804 with changes:
    
    1. Destroy the NotificationDisplayServiceTester before the profile goes
       down (to fix the error).
    2. Make the extension storage monitor depend on the notification service
       (for correctness --- doesn't help fix this error).
    3. Don't explicitly remove notifications when the profile goes down. The
       NotificationDisplayService should remove all transient notifications
       for its profile when it's destroyed, which happens when the profile
       is destroyed. This part is necessary because when you remove a testing
       factory it doesn't reinstate the normal, non-testing factory, so we
       get a null deref. See KeyedServiceFactory::testing_factories_
    
    Bug: 793548
    Change-Id: I834091ad6c53aaf923756e723a234013f92b1cd9

--- chrome/browser/extensions/extension_storage_monitor_factory.cc
+++ chrome/browser/extensions/extension_storage_monitor_factory.cc
@@ -6,6 +6,7 @@
 
 #include "chrome/browser/extensions/extension_storage_monitor.h"
 #include "chrome/browser/extensions/extension_system_factory.h"
+#include "chrome/browser/notifications/notification_display_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "extensions/browser/extension_prefs_factory.h"
@@ -32,6 +33,7 @@ ExtensionStorageMonitorFactory::ExtensionStorageMonitorFactory()
           BrowserContextDependencyManager::GetInstance()) {
   DependsOn(ExtensionsBrowserClient::Get()->GetExtensionSystemFactory());
   DependsOn(ExtensionPrefsFactory::GetInstance());
+  DependsOn(NotificationDisplayServiceFactory::GetInstance());
 }
 
 ExtensionStorageMonitorFactory::~ExtensionStorageMonitorFactory() {
