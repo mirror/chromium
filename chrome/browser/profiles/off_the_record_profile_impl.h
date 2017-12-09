64659b5ee83568bc99edbd081e25ca455c47b03b
John Abd-El-Malek <jam@chromium.org>
2017-12-08 17:58:34 -0800

    Persist cookies with network service.
    Bug: 789644, 769401, 776589
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I70320d5550e433049a910c64f055c1809aecbff1

--- chrome/browser/profiles/off_the_record_profile_impl.h
+++ chrome/browser/profiles/off_the_record_profile_impl.h
@@ -81,6 +81,7 @@ class OffTheRecordProfileImpl : public Profile {
   bool WasCreatedByVersionOrLater(const std::string& version) override;
   void SetExitType(ExitType exit_type) override;
   ExitType GetLastSessionExitType() override;
+  content::CookieStoreConfig::SessionCookieMode GetSessionCookieMode() override;
 
 #if defined(OS_CHROMEOS)
   void ChangeAppLocale(const std::string& locale, AppLocaleChangedVia) override;
