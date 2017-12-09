7806a6f7337711af51a3de019ceec92f26e4417e
Justin Cohen <justincohen@google.com>
2017-12-09 00:12:47 -0500

    POC for 627772
    Bug: 627772
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I4e6c38f89a47f725adbce327f38acf3b707ed35b

--- ios/chrome/browser/ui/stack_view/page_animation_util.h
+++ ios/chrome/browser/ui/stack_view/page_animation_util.h
@@ -66,6 +66,7 @@ void AnimateInPaperWithAnimationAndCompletion(UIView* view,
 // which the animation will take place.
 void AnimateNewBackgroundPageWithCompletion(CardView* currentPageCard,
                                             CGRect displayFrame,
+                                            CGRect visibleFrame,
                                             BOOL isPortrait,
                                             void (^completion)(void));
 
