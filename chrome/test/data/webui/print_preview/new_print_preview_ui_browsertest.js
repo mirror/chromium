fc98852b928c9e372b8e56c2bcc892d2197a66fb
rbpotter <rbpotter@chromium.org>
2017-12-11 15:18:13 -0800

    Print Preview componentization: use initial settings
    Use initial settings to populate some fields.
    
    Bug: 773928
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I817c34fabf416b7a935633bdc1830d3e69fc4086

--- chrome/test/data/webui/print_preview/new_print_preview_ui_browsertest.js
+++ chrome/test/data/webui/print_preview/new_print_preview_ui_browsertest.js
@@ -91,3 +91,32 @@ TEST_F('PrintPreviewSettingsSectionsTest', 'Scaling', function() {
 TEST_F('PrintPreviewSettingsSectionsTest', 'Other', function() {
   this.runMochaTest(settings_sections_tests.TestNames.Other);
 });
+
+PrintPreviewRestoreStateTest = class extends NewPrintPreviewTest {
+  /** @override */
+  get browsePreload() {
+    return 'chrome://print/new/app.html';
+  }
+
+  /** @override */
+  get extraLibraries() {
+    return super.extraLibraries.concat([
+      '../test_browser_proxy.js',
+      'native_layer_stub.js',
+      'restore_state_test.js',
+    ]);
+  }
+
+  /** @override */
+  get suiteName() {
+    return restore_state_test.suiteName;
+  }
+};
+
+TEST_F('PrintPreviewRestoreStateTest', 'Restore1', function() {
+  this.runMochaTest(restore_state_test.TestNames.Restore1);
+});
+
+TEST_F('PrintPreviewRestoreStateTest', 'Restore2', function() {
+  this.runMochaTest(restore_state_test.TestNames.Restore2);
+});
