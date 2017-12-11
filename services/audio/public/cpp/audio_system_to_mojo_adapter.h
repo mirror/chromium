5416fe21fd24ae6faec0813e5f2038495889b7e9
Olga Sharonova <olka@chromium.org>
2017-12-11 19:15:49 +0100

    Basic Audio Service hosting mojom::SystemInfo
    In this CL:
    * //services/audio infrastructure
    * AudioDeviceDescription type mapping - may need to be moved to //media? (Someday we want it all
    to be in //audio though.)
    * Build files/dependencies - please review vigorously, I'm not sure everything is fine there.
    * mojom::SystemInfo interface and implementation on top of AudioManager,
    * Service implementation.
    * AudioSystemToMojoAdapter: client-side media::AudioSystem implementation on top of
    mojom::SystemInfo
    * Unit tests.
    
    Upcoming CLs for AudioService infrastructure:
    - Run in-process Audio service behind a flag and
    instantiate BrowserMainLoop::audio_system_ with AudioSystemToMojoAdapter, so
    that audio system information is served through the service.
    - UMA stats for service usage.
    - Tear down the service when not in use (service side, client side)
    - Out-of-process AudioService implementation + unit tests
    
    Design doc (being updated now):
    https://docs.google.com/document/d/1s_Fd1WRDdpb5n6C2MSJjeC3fis6hULZwfKMeDd4K5tI/edit?usp=sharing
    
    Bug:740943,792441
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iae7f69386a6d2138c69d9566cf41655b57158b7c

---
+++ services/audio/public/cpp/audio_system_to_mojo_adapter.h
@@ -0,0 +1,62 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_AUDIO_PUBLIC_CPP_AUDIO_SYSTEM_TO_MOJO_ADAPTER_H_
+#define SERVICES_AUDIO_PUBLIC_CPP_AUDIO_SYSTEM_TO_MOJO_ADAPTER_H_
+
+#include <memory>
+
+#include "base/threading/thread_checker.h"
+#include "media/audio/audio_system.h"
+#include "services/audio/public/interfaces/system_info.mojom.h"
+
+namespace service_manager {
+class Connector;
+}
+
+namespace audio {
+
+// Provides AudioSystem implementation on top of Audio service.
+class AudioSystemToMojoAdapter : public media::AudioSystem {
+ public:
+  using BindSystemInfoCb =
+      base::RepeatingCallback<void(mojom::SystemInfoRequest request)>;
+
+  static std::unique_ptr<media::AudioSystem> Create(
+      service_manager::Connector* connector);
+
+  explicit AudioSystemToMojoAdapter(BindSystemInfoCb bind_system_info_cb);
+
+  ~AudioSystemToMojoAdapter() override;
+
+  // AudioSystem implementation.
+  void GetInputStreamParameters(const std::string& device_id,
+                                OnAudioParamsCallback on_params_cb) override;
+  void GetOutputStreamParameters(const std::string& device_id,
+                                 OnAudioParamsCallback on_params_cb) override;
+  void HasInputDevices(OnBoolCallback on_has_devices_cb) override;
+  void HasOutputDevices(OnBoolCallback on_has_devices_cb) override;
+  void GetDeviceDescriptions(
+      bool for_input,
+      OnDeviceDescriptionsCallback on_descriptions_cp) override;
+  void GetAssociatedOutputDeviceID(const std::string& input_device_id,
+                                   OnDeviceIdCallback on_device_id_cb) override;
+  void GetInputDeviceInfo(
+      const std::string& input_device_id,
+      OnInputDeviceInfoCallback on_input_device_info_cb) override;
+
+ private:
+  mojom::SystemInfo* GetSystemInfo();
+  void OnConnectionError();
+
+  BindSystemInfoCb bind_system_info_cb_;
+  mojom::SystemInfoPtr system_info_;
+
+  THREAD_CHECKER(thread_checker_);
+  DISALLOW_COPY_AND_ASSIGN(AudioSystemToMojoAdapter);
+};
+
+}  // namespace audio
+
+#endif  // SERVICES_AUDIO_PUBLIC_CPP_AUDIO_SYSTEM_TO_MOJO_ADAPTER_H_
