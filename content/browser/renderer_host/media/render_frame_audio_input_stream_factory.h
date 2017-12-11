9dfafc71093fea872bf1d30850d5a126e00fb355
Max Morin <maxmorin@chromium.org>
2017-12-11 15:51:33 +0100

    Add flag/glue for mojo audio input stream factories.
    This exposes the newly added RendererAudioInputStreamFactory code
    in frame hosts, and enables the renderer to create streams using this
    factory. This behavior is controlled by the new
    "UseMojoAudioInputStreamFactory" flag. Also rename
    AudioIPCFactory->AudioOutputIPCFactory to avoid confusion with the new
    AudioInputIPCFactory.
    
    Bug: 653871
    Change-Id: I3590ee2fab8b3a65ba00c659da9b33b81f39b2a2

--- content/browser/renderer_host/media/render_frame_audio_input_stream_factory.h
+++ content/browser/renderer_host/media/render_frame_audio_input_stream_factory.h
@@ -57,6 +57,8 @@ class CONTENT_EXPORT RenderFrameAudioInputStreamFactory
 
   ~RenderFrameAudioInputStreamFactory() override;
 
+  static bool UseMojoFactories();
+
  private:
   using InputStreamSet =
       base::flat_set<std::unique_ptr<AudioInputStreamHandle>>;
