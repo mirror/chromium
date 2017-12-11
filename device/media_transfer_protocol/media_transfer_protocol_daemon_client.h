a45ea130f88651d58a4ba9ec47b480e37ceb2189
Donna Wu <donna.wu@intel.com>
2017-12-11 03:54:11 -0330

    Add mtp.mojom file and modify the related code.
    Define structs in mtp.mojom to mirror the MTP protobuf messages. Use
    these structs in MtpManager and related code.
    
    This is a preparing work for MtpManager servicification.
    
    BUG=769630
    
    Change-Id: Ia01f5bc7c00fdcc258f50ae02e7a375caa83d6c9

--- device/media_transfer_protocol/media_transfer_protocol_daemon_client.h
+++ device/media_transfer_protocol/media_transfer_protocol_daemon_client.h
@@ -19,13 +19,11 @@
 #include "base/callback.h"
 #include "base/macros.h"
 #include "build/build_config.h"
+#include "device/media_transfer_protocol/public/interfaces/mtp.mojom.h"
 
 #if !defined(OS_CHROMEOS)
 #error "Only used on ChromeOS"
 #endif
-
-class MtpFileEntry;
-class MtpStorageInfo;
 
 namespace dbus {
 class Bus;
@@ -49,7 +47,7 @@ class MediaTransferProtocolDaemonClient {
   // A callback to handle the result of GetStorageInfo.
   // The argument is the information about the specified storage.
   using GetStorageInfoCallback =
-      base::Callback<void(const MtpStorageInfo& storage_info)>;
+      base::Callback<void(mojom::MtpStorageInfoPtr storage_info)>;
 
   // A callback to handle the result of OpenStorage.
   // The argument is the returned handle.
@@ -69,7 +67,7 @@ class MediaTransferProtocolDaemonClient {
   // A callback to handle the result of GetFileInfo.
   // The argument is a vector of file entries.
   using GetFileInfoCallback =
-      base::Callback<void(const std::vector<MtpFileEntry>& file_entries)>;
+      base::Callback<void(std::vector<mojom::MtpFileEntryPtr> file_entries)>;
 
   // A callback to handle the result of ReadFileChunkById.
   // The argument is a string containing the file data.
