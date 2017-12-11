f2497fe7aa85ab4df32aec3e451f1c75573ba192
Nate Chapin <japhet@chromium.org>
2017-12-11 14:21:26 -0800

    SetResource() in ResourceFetcher for FontResources
    Bug: 793028
    Change-Id: I3ebd0605be620c8fa0c798598656be289c719858

--- third_party/WebKit/Source/core/loader/DocumentLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentLoader.cpp
@@ -211,7 +211,7 @@ Resource* DocumentLoader::StartPreload(Resource::Type type,
       resource = CSSStyleSheetResource::Fetch(params, Fetcher());
       break;
     case Resource::kFont:
-      resource = FontResource::Fetch(params, Fetcher());
+      resource = FontResource::Fetch(params, Fetcher(), nullptr);
       break;
     case Resource::kMedia:
       resource = RawResource::FetchMedia(params, Fetcher(), nullptr);
