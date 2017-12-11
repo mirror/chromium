a1020a15d093271e845fd93816293b349ebd0690
Koji Ishii <kojii@chromium.org>
2017-12-11 17:05:47 +0900

    txovn
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Ib800a9b4264dfe724d59fdb220e7c0d43513685a

--- third_party/WebKit/Source/core/layout/ng/inline/ng_physical_text_fragment.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_physical_text_fragment.cc
@@ -14,36 +14,50 @@ NGPhysicalOffsetRect NGPhysicalTextFragment::SelfVisualRect() const {
   if (!shape_result_)
     return {};
 
-  // TODO(kojii): Copying InlineTextBox logic from
-  // InlineFlowBox::ComputeOverflow().
-  //
-  // InlineFlowBox::ComputeOverflow() computes GlpyhOverflow first
-  // (ComputeGlyphOverflow) then AddTextBoxVisualOverflow(). We probably don't
-  // have to keep these two steps separated.
-
-  // Glyph bounds is in logical coordinate, origin at the baseline.
-  FloatRect visual_float_rect = shape_result_->Bounds();
+  // Glyph bounds is in logical coordinate, origin at the alphabetic baseline.
+  LayoutRect visual_rect = EnclosingLayoutRect(shape_result_->Bounds());
 
   // Make the origin at the logical top of this fragment.
-  // ShapeResult::Bounds() is in logical coordinate with alphabetic baseline.
   const ComputedStyle& style = Style();
-  const SimpleFontData* font_data = style.GetFont().PrimaryFont();
-  visual_float_rect.SetY(
-      visual_float_rect.Y() +
-      font_data->GetFontMetrics().FixedAscent(kAlphabeticBaseline));
-
-  // TODO(kojii): Copying from AddTextBoxVisualOverflow()
-  if (float stroke_width = style.TextStrokeWidth()) {
-    visual_float_rect.Inflate(stroke_width / 2.0f);
+  const Font& font = style.GetFont();
+  const SimpleFontData* font_data = font.PrimaryFont();
+  if (font_data) {
+    visual_rect.SetY(visual_rect.Y() + font_data->GetFontMetrics().FixedAscent(
+                                           kAlphabeticBaseline));
   }
 
-  // TODO(kojii): Implement emphasis marks.
+  if (float stroke_width = style.TextStrokeWidth()) {
+    visual_rect.Inflate(LayoutUnit::FromFloatCeil(stroke_width / 2.0f));
+  }
+
+  LayoutRect frame_rect = {{}, Size().ToLayoutSize()};
+  if (style.GetTextEmphasisMark() != TextEmphasisMark::kNone) {
+    LayoutUnit height =
+        LayoutUnit(font.EmphasisMarkHeight(style.TextEmphasisMarkString()));
+    DCHECK_GT(height, LayoutUnit());
+    LineLogicalSide side = style.GetTextEmphasisLineLogicalSide();
+    if (side == LineLogicalSide::kOver) {
+      visual_rect.ShiftYEdgeTo(
+          std::min(visual_rect.Y(), frame_rect.Y() - height));
+    } else {
+      visual_rect.ShiftMaxYEdgeTo(
+          std::max(visual_rect.MaxY(), frame_rect.MaxY() + height));
+    }
+  }
 
   if (ShadowList* text_shadow = style.TextShadow()) {
-    // TODO(kojii): Implement text shadow.
+    LayoutRectOutsets text_shadow_logical_outsets =
+        LayoutRectOutsets(text_shadow->RectOutsetsIncludingOriginal())
+            .LineOrientationOutsets(style.GetWritingMode());
+    text_shadow_logical_outsets.ClampNegativeToZero();
+    visual_rect.Expand(text_shadow_logical_outsets);
   }
 
-  LayoutRect visual_rect = EnclosingLayoutRect(visual_float_rect);
+#if 0
+  visual_rect.Unite(frame_rect);
+#endif
+  visual_rect = LayoutRect(EnclosingIntRect(visual_rect));
+
   switch (LineOrientation()) {
     case NGLineOrientation::kHorizontal:
       return NGPhysicalOffsetRect(visual_rect);
