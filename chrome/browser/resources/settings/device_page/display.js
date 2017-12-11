b9b2742cdfa137eaa1a2088107ac84f879c3499b
Weidong Guo <weidongg@chromium.org>
2017-12-11 10:48:28 -0800

    Disable Overscan option for internal display in display settings
    Bug: 781747
    Change-Id: I128f64943031087f08bdf3f64c743bcbb7784694

--- chrome/browser/resources/settings/device_page/display.js
+++ chrome/browser/resources/settings/device_page/display.js
@@ -286,6 +286,16 @@ Polymer({
   },
 
   /**
+   * Returns true if the current display is not an internal display.
+   * @param {!chrome.system.display.DisplayUnitInfo} display
+   * @return {boolean}
+   * @private
+   */
+  showOverscanSetting_: function(display) {
+    return !display.isInternal;
+  },
+
+  /**
    * @param {!Array<!chrome.system.display.DisplayUnitInfo>} displays
    * @return {boolean}
    * @private
