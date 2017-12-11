ea6c4de2b9c901ac6c1a8c1b9bd0343d619e8c1a
Robert Sesek <rsesek@chromium.org>
2017-12-11 18:24:08 -0500

    crash_keys: Convert keys in //chromecast to the new API.
    Bug: 598854
    Change-Id: Id79de5ef8f24b0dced2ac17cfd5bc1fbe196121e

--- chromecast/crash/cast_crash_keys.h
+++ chromecast/crash/cast_crash_keys.h
@@ -7,16 +7,10 @@
 
 #include <stddef.h>
 
-#include "base/debug/crash_logging.h"
-
 namespace chromecast {
 namespace crash_keys {
 
 size_t RegisterCastCrashKeys();
-
-extern const char kCurrentApp[];
-extern const char kLastApp[];
-extern const char kPreviousApp[];
 
 }  // namespace chromecast
 }  // namespace crash_keys
