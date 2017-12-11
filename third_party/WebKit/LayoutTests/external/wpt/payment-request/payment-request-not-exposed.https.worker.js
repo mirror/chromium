4cd947afab49fadebd69d6093aa5810a2fd2992f
Jinho Bang <jinho.bang@samsung.com>
2017-12-12 04:20:08 +0900

    PaymentRequest: Add [Exposed=Window] to the PaymentRequest API
    There is no behavior change but add a test to check it.
    
    Bug: none
    Change-Id: I883fd6eb5f848c7416b810460c182c7509d6c35c

---
+++ third_party/WebKit/LayoutTests/external/wpt/payment-request/payment-request-not-exposed.https.worker.js
@@ -0,0 +1,10 @@
+importScripts("/resources/testharness.js");
+
+test(function() {
+  assert_true(isSecureContext);
+  assert_false('PaymentRequest' in self);
+  assert_false('PaymentResponse' in self);
+  assert_false('PaymentRequestUpdateEvent' in self);
+}, "PaymentRequest constructor must not be exposed in worker global scope");
+
+done();
