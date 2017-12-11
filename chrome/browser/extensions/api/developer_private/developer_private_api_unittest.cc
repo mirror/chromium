627d5d84555f4f9008fe696c340526a9063232c9
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-12-11 14:18:51 -0800

    load error for drag-and-dropped Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I888b2094dcda1e964bca0f2680573387e714ea77

--- chrome/browser/extensions/api/developer_private/developer_private_api_unittest.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_api_unittest.cc
@@ -709,16 +709,16 @@ TEST_F(DeveloperPrivateApiUnitTest, LoadUnpackedRetryId) {
 
   {
     // Try reloading the extension by supplying the retry id. It should succeed.
-    scoped_refptr<UIThreadExtensionFunction> function(
-        new api::DeveloperPrivateLoadUnpackedFunction());
+    auto function =
+        base::MakeRefCounted<api::DeveloperPrivateLoadUnpackedFunction>();
     function->SetRenderFrameHost(web_contents->GetMainFrame());
     TestExtensionRegistryObserver observer(registry());
-    api_test_utils::RunFunction(function.get(),
-                                base::StringPrintf("[{\"failQuietly\": true,"
-                                                   "\"populateError\": true,"
-                                                   "\"retryGuid\": \"%s\"}]",
-                                                   retry_guid.c_str()),
-                                profile());
+    std::string args = base::StringPrintf(
+        "[{\"failQuietly\": true,"
+        "\"populateError\": true,"
+        "\"retryGuid\": \"%s\"}]",
+        retry_guid.c_str());
+    api_test_utils::RunFunction(function.get(), args, profile());
     const Extension* extension = observer.WaitForExtensionLoaded();
     ASSERT_TRUE(extension);
     EXPECT_EQ(extension->path(), path);
@@ -859,6 +859,64 @@ TEST_F(DeveloperPrivateApiUnitTest, ReloadBadExtensionToLoadUnpackedRetry) {
     EXPECT_EQ(extension->path(), path);
     EXPECT_TRUE(registry()->enabled_extensions().Contains(id));
   }
+}
+
+TEST_F(DeveloperPrivateApiUnitTest,
+       DeveloperPrivateNotifyDragInstallInProgress) {
+  std::unique_ptr<content::WebContents> web_contents(
+      content::WebContentsTester::CreateTestWebContents(profile(), nullptr));
+
+  TestExtensionDir dir;
+  dir.WriteManifest(
+      R"({
+           "name": "foo",
+           "description": "bar",
+           "version": "1",
+           "manifest_version": 2
+         })");
+  base::FilePath path = dir.UnpackedPath();
+  api::DeveloperPrivateNotifyDragInstallInProgressFunction::
+      SetDropPathForTesting(&path);
+
+  DeveloperPrivateAPI::UnpackedRetryId retry_guid;
+  {
+    auto function = base::MakeRefCounted<
+        api::DeveloperPrivateNotifyDragInstallInProgressFunction>();
+    function->SetRenderFrameHost(web_contents->GetMainFrame());
+    std::unique_ptr<base::Value> result =
+        api_test_utils::RunFunctionAndReturnSingleResult(function.get(), "[]",
+                                                         profile());
+    ASSERT_TRUE(result);
+    ASSERT_TRUE(result->is_string());
+    retry_guid = result->GetString();
+  }
+
+  // Set the picker to choose an invalid path (the picker should be skipped if
+  // we supply a retry id).
+  base::FilePath empty_path;
+  api::EntryPicker::SkipPickerAndAlwaysSelectPathForTest(&empty_path);
+
+  {
+    // Try reloading the extension by supplying the retry id. It should succeed.
+    scoped_refptr<UIThreadExtensionFunction> function(
+        new api::DeveloperPrivateLoadUnpackedFunction());
+    function->SetRenderFrameHost(web_contents->GetMainFrame());
+    TestExtensionRegistryObserver observer(registry());
+    api_test_utils::RunFunction(function.get(),
+                                base::StringPrintf("[{\"failQuietly\": true,"
+                                                   "\"populateError\": true,"
+                                                   "\"retryGuid\": \"%s\"}]",
+                                                   retry_guid.c_str()),
+                                profile());
+    const Extension* extension = observer.WaitForExtensionLoaded();
+    ASSERT_TRUE(extension);
+    EXPECT_EQ(extension->path(), path);
+  }
+
+  // Cleanup.
+  api::DeveloperPrivateNotifyDragInstallInProgressFunction::
+      SetDropPathForTesting(nullptr);
+  api::EntryPicker::SkipPickerAndAlwaysSelectPathForTest(nullptr);
 }
 
 // Test developerPrivate.requestFileSource.
