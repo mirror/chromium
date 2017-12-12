34039bb0d3a8fe337022d3b7860b16218d8a28c1
Mitsuru Oshima <oshima@chromium.org>
2017-12-12 13:48:49 -0800

    Animation
    BUG=
    
    Change-Id: I87a0d27d0edb7a7ce1d8fbefc35a584692cf159c

--- components/exo/client_controlled_shell_surface.h
+++ components/exo/client_controlled_shell_surface.h
@@ -131,7 +131,6 @@ class ClientControlledShellSurface
   // Overridden from ShellSurface:
   void SetWidgetBounds(const gfx::Rect& bounds) override;
   void InitializeWindowState(ash::wm::WindowState* window_state) override;
-  void UpdateBackdrop() override;
   float GetScale() const override;
   bool CanAnimateWindowStateTransitions() const override;
   aura::Window* GetDragWindow() override;
@@ -141,6 +140,8 @@ class ClientControlledShellSurface
   bool OnMouseDragged(const ui::MouseEvent& event) override;
   gfx::Point GetWidgetOrigin() const override;
   gfx::Point GetSurfaceOrigin() const override;
+
+  void UpdateBackdrop();
 
   // Lock the compositor if it's not already locked, or extends the
   // lock timeout if it's already locked.
@@ -166,6 +167,9 @@ class ClientControlledShellSurface
   Orientation expected_orientation_ = Orientation::LANDSCAPE;
 
   ash::wm::ClientControlledState* client_controlled_state_ = nullptr;
+
+  ui::WindowShowState pending_show_state_ = ui::SHOW_STATE_NORMAL;
+  bool enable_crossfade_ = false;
 
   std::unique_ptr<ui::CompositorLock> orientation_compositor_lock_;
 
