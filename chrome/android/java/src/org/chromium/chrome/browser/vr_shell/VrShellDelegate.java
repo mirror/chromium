ae5167e180bf331f26335e443901e4634a2327e7
Michael Thiessen <mthiesse@chromium.org>
2017-12-12 17:24:54 -0500

    WIP: Proof of concept 2D UI fallback for VR.
    This will draw any 2D UI VR isn't aware of on top of the content quad in
    VR.
    
    Bug: 793430
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Idc9c52e344daad77bac92b5f3aa0ff78c3e9e00f

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
@@ -128,7 +128,6 @@ public class VrShellDelegate
     private VrDaydreamApi mVrDaydreamApi;
     private Boolean mIsDaydreamCurrentViewer;
     private VrCoreVersionChecker mVrCoreVersionChecker;
-    private TabModelSelector mTabModelSelector;
 
     private boolean mProbablyInDon;
     private boolean mInVr;
@@ -1651,11 +1650,12 @@ public class VrShellDelegate
         assert mVrShell == null;
         if (mVrClassesWrapper == null) return false;
         if (mActivity.getCompositorViewHolder() == null) return false;
-        mTabModelSelector = mActivity.getCompositorViewHolder().detachForVr();
-        if (mTabModelSelector == null) return false;
+        TabModelSelector tabModelSelector = mActivity.getTabModelSelector();
+        if (tabModelSelector == null) return false;
+        mActivity.getCompositorViewHolder().detachForVr();
         StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskWrites();
         try {
-            mVrShell = mVrClassesWrapper.createVrShell(mActivity, this, mTabModelSelector);
+            mVrShell = mVrClassesWrapper.createVrShell(mActivity, this, tabModelSelector);
         } finally {
             StrictMode.setThreadPolicy(oldPolicy);
         }
@@ -1669,13 +1669,14 @@ public class VrShellDelegate
                 ViewGroup.LayoutParams.MATCH_PARENT);
         decor.addView(mVrShell.getContainer(), params);
         mActivity.onEnterVr();
+        mActivity.getToolbarManager().setProgressBarEnabled(false);
     }
 
     private void removeVrViews() {
-        mVrShell.onBeforeWindowDetached();
         mActivity.onExitVr();
         FrameLayout decor = (FrameLayout) mActivity.getWindow().getDecorView();
         decor.removeView(mVrShell.getContainer());
+        mActivity.getToolbarManager().setProgressBarEnabled(true);
     }
 
     /**
@@ -1687,9 +1688,8 @@ public class VrShellDelegate
             mVrShell.teardown();
             mVrShell = null;
             if (mActivity.getCompositorViewHolder() != null) {
-                mActivity.getCompositorViewHolder().onExitVr(mTabModelSelector);
+                mActivity.getCompositorViewHolder().onExitVr();
             }
-            mTabModelSelector = null;
         }
     }
 
