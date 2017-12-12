2950288182ae5f19efe99f868ec26434734750d5
Alex Clarke <alexclarke@chromium.org>
2017-12-12 11:16:32 +0000

    TaskQueueManager to deal with non-nestible tasks internally
    We no longer rely on the message loop for dealing with them!
    
    Bug: 522843
    Change-Id: I53aa0f14b783228939d6a058c483caacad16c56f

--- third_party/WebKit/Source/platform/scheduler/base/task_queue_impl.cc
+++ third_party/WebKit/Source/platform/scheduler/base/task_queue_impl.cc
@@ -876,6 +876,21 @@ void TaskQueueImpl::PushImmediateIncomingTaskForTest(
   immediate_incoming_queue().push_back(std::move(task));
 }
 
+void TaskQueueImpl::RequeueDeferredNonNestableTask(TaskQueueImpl::Task&& task,
+                                                   bool delayed) {
+  // Note this adds extra delay to |task| but since non nestable tasks can be
+  // arbitrarily delayed that's fine.
+  // The re-queued tasks have to be pushed onto the front because we'd otherwise
+  // violate the strict monotonically increasing enqueue order within the
+  // WorkQueue.  We can't assign them a new enqueue order here because that will
+  // not behave correctly with fences and things will break (e.g Idle TQ).
+  if (delayed) {
+    main_thread_only().delayed_work_queue->PushFront(std::move(task));
+  } else {
+    main_thread_only().immediate_work_queue->PushFront(std::move(task));
+  }
+}
+
 void TaskQueueImpl::SetOnNextWakeUpChangedCallback(
     TaskQueueImpl::OnNextWakeUpChangedCallback callback) {
 #if DCHECK_IS_ON()
