11d7138b9d1284e71e5cd4290b60b70f7ee13a94
Yves Arrouye <drcrash@google.com>
2017-12-12 12:22:11 -0800

    Set the type of certificates when uploading them.
    This will allow certificates to be typed even if we send an empty
    PEM certificate which the server can act on.
    
    Bug: 778535
    Test: unit_tests
    Change-Id: Iadc2d94c4cdd558dc46086b7cea629f7f92e8ae7

--- components/policy/core/common/cloud/mock_cloud_policy_client.h
+++ components/policy/core/common/cloud/mock_cloud_policy_client.h
@@ -32,8 +32,15 @@ class MockCloudPolicyClient : public CloudPolicyClient {
            const std::string&));
   MOCK_METHOD0(FetchPolicy, void(void));
   MOCK_METHOD0(Unregister, void(void));
-  MOCK_METHOD2(UploadCertificate,
+  MOCK_METHOD2(UploadEnterpriseMachineCertificate,
                void(const std::string&, const StatusCallback&));
+  MOCK_METHOD2(UploadEnterpriseEnrollmentCertificate,
+               void(const std::string&, const StatusCallback&));
+  MOCK_METHOD3(
+      UploadCertificate,
+      void(const std::string&,
+           enterprise_management::DeviceCertUploadRequest::CertificateType,
+           const StatusCallback&));
   MOCK_METHOD3(UploadDeviceStatus,
                void(const enterprise_management::DeviceStatusReportRequest*,
                     const enterprise_management::SessionStatusReportRequest*,
