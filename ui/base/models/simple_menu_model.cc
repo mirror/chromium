1afbe76a2aba9598a429e9f557cd5fac38510563
Alex Newcomer <newcomer@chromium.org>
2017-12-12 12:11:25 -0800

    Add context menu metrics for the shelf
    These metrics are wanted in order to measure current user behavior in
    regards to context menus, and to measure the effect of upcoming
    features. This is part 1 of a 2 part CL, part 2 will include all
    representation of apps for AppList.
    
    This contains metrics for:
    - Context Menu User Journey Time(timestamp at close - timestamp at
      open) for the ShelfButton.
    
    - App Context Menu Show Source(touch, keyboard, mouse) for
      ShelfButton.
    
    - Context Menu command execution(Pin to shelf, Unpin, launch, etc)
      split by:
      - Context Menus that were shown for apps.
      - Context Menus that were shown the shelf or wallpaper.
    
    TEST=manual
    Bug:791069
    Change-Id: Ia3a0b836fc0a195f66991d81b275176f04b27afa

--- ui/base/models/simple_menu_model.cc
+++ ui/base/models/simple_menu_model.cc
@@ -8,6 +8,7 @@
 
 #include "base/bind.h"
 #include "base/location.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/single_thread_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -379,13 +380,17 @@ void SimpleMenuModel::HighlightChangedTo(int index) {
 }
 
 void SimpleMenuModel::ActivatedAt(int index) {
+  int command_id = GetCommandIdAt(index);
+  RecordHistogram(command_id);
   if (delegate_)
-    delegate_->ExecuteCommand(GetCommandIdAt(index), 0);
+    delegate_->ExecuteCommand(command_id, 0);
 }
 
 void SimpleMenuModel::ActivatedAt(int index, int event_flags) {
+  int command_id = GetCommandIdAt(index);
+  RecordHistogram(command_id);
   if (delegate_)
-    delegate_->ExecuteCommand(GetCommandIdAt(index), event_flags);
+    delegate_->ExecuteCommand(command_id, event_flags);
 }
 
 MenuModel* SimpleMenuModel::GetSubmenuModelAt(int index) const {
@@ -455,6 +460,20 @@ void SimpleMenuModel::ValidateItem(const Item& item) {
     DCHECK_GE(item.command_id, 0);
   }
 #endif  // NDEBUG
+}
+
+void SimpleMenuModel::SetHistogramName(std::string histogram_name) {
+  if (!histogram_name_.empty())
+    return;
+
+  histogram_name_ = histogram_name;
+}
+
+void SimpleMenuModel::RecordHistogram(int command) {
+  if (histogram_name_.empty())
+    return;
+
+  UMA_HISTOGRAM_SPARSE_SLOWLY(histogram_name_, command);
 }
 
 }  // namespace ui
