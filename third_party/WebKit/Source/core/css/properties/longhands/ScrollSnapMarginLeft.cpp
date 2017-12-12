64e3feecdf922bbff701818960ee6e5cc227941b
Renee Wright <rjwright@chromium.org>
2017-12-12 19:52:21 +1100

    [Ribbon] Ribbonize ComputedStyleCSSValueMapping
    Bug: 775360
    Change-Id: I92c1f2d8f302ebab37fb17ef54a152f154dceb9b

--- third_party/WebKit/Source/core/css/properties/longhands/ScrollSnapMarginLeft.cpp
+++ third_party/WebKit/Source/core/css/properties/longhands/ScrollSnapMarginLeft.cpp
@@ -6,6 +6,7 @@
 
 #include "core/css/parser/CSSParserContext.h"
 #include "core/css/parser/CSSPropertyParserHelpers.h"
+#include "core/css/properties/ComputedStyleUtils.h"
 
 namespace blink {
 namespace CSSLonghand {
@@ -17,6 +18,16 @@ const CSSValue* ScrollSnapMarginLeft::ParseSingleValue(
   return ConsumeLengthOrPercent(
       range, context.Mode(), kValueRangeNonNegative,
       CSSPropertyParserHelpers::UnitlessQuirk::kAllow);
+}
+
+const CSSValue* ScrollSnapMarginLeft::CSSValueFromComputedStyleInternal(
+    const ComputedStyle& style,
+    const SVGComputedStyle&,
+    const LayoutObject*,
+    Node*,
+    bool allow_visited_style) const {
+  return ComputedStyleUtils::ZoomAdjustedPixelValueForLength(
+      style.ScrollSnapMarginLeft(), style);
 }
 
 }  // namespace CSSLonghand
--- third_party/WebKit/Source/core/css/properties/longhands/Variable.h
+++ third_party/WebKit/Source/core/css/properties/longhands/Variable.h
@@ -19,6 +19,16 @@ class Variable : public CSSProperty {
 
   bool IsInherited() const override { return true; }
   bool IsAffectedByAll() const override { return false; }
+
+  const CSSValue* CSSValueFromComputedStyleInternal(
+      const ComputedStyle&,
+      const SVGComputedStyle&,
+      const LayoutObject*,
+      Node*,
+      bool allow_visited_style) const override {
+    NOTREACHED();
+    return nullptr;
+  }
 };
 
 }  // namespace blink
