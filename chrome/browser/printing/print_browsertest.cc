4c21775d56c36fee22e88f540ba457e3d02b8a45
Wei Li <weili@chromium.org>
2017-12-12 10:55:44 -0800

    Support OOPIF printing in browser
    Allow browser side to handle subframe printing message and notify the
    render of the subframe to print its content. Add communications with pdf
    compositor service to compose the final content.
    
    The blink side of change is at crrev.com/c/818525. The high level design doc
    is at goo.gl/CF1RUy
    
    BUG=455764
    
    Change-Id: I1a039a5f76e186b34b54dba6d974f5f41d1a6550

--- chrome/browser/printing/print_browsertest.cc
+++ chrome/browser/printing/print_browsertest.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+
 #include "base/auto_reset.h"
 #include "base/run_loop.h"
 #include "chrome/browser/printing/print_view_manager_common.h"
@@ -13,6 +15,12 @@
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/prefs/pref_service.h"
+#include "components/printing/common/print_messages.h"
+#include "content/public/browser/browser_message_filter.h"
+#include "content/public/browser/render_frame_host.h"
+#include "content/public/browser/render_process_host.h"
+#include "content/public/test/browser_test_utils.h"
+#include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 
 namespace printing {
@@ -60,6 +68,14 @@ class PrintBrowserTest : public InProcessBrowserTest {
   PrintBrowserTest() {}
   ~PrintBrowserTest() override {}
 
+  void SetUpOnMainThread() override {
+    host_resolver()->AddRule("*", "127.0.0.1");
+    ASSERT_TRUE(embedded_test_server()->InitializeAndListen());
+    content::SetupCrossSiteRedirector(embedded_test_server());
+    embedded_test_server()->ServeFilesFromSourceDirectory("test/data");
+    embedded_test_server()->StartAcceptingConnections();
+  }
+
   void PrintAndWaitUntilPreviewIsReady(bool print_only_selection) {
     PrintPreviewObserver print_preview_observer;
 
@@ -71,16 +87,85 @@ class PrintBrowserTest : public InProcessBrowserTest {
   }
 };
 
+class TestPrintFrameContentMsgFilter : public content::BrowserMessageFilter {
+ public:
+  explicit TestPrintFrameContentMsgFilter(base::OnceClosure quit_closure)
+      : content::BrowserMessageFilter(PrintMsgStart),
+        task_runner_(base::SequencedTaskRunnerHandle::Get()),
+        quit_closure_(std::move(quit_closure)) {}
+
+  bool OnMessageReceived(const IPC::Message& message) override {
+    // Only expect this particular message.
+    IPC_BEGIN_MESSAGE_MAP(TestPrintFrameContentMsgFilter, message)
+      IPC_MESSAGE_HANDLER(PrintHostMsg_DidPrintFrameContent, CheckMessage)
+    IPC_END_MESSAGE_MAP()
+    task_runner_->PostTask(FROM_HERE, std::move(quit_closure_));
+    return true;
+  }
+
+ private:
+  ~TestPrintFrameContentMsgFilter() override {}
+
+  void CheckMessage(const PrintHostMsg_DidPrintContent_Params& param) {
+    EXPECT_TRUE(param.metafile_data_handle.IsValid());
+    EXPECT_GT(param.data_size, 0u);
+  }
+
+  scoped_refptr<base::SequencedTaskRunner> task_runner_;
+  base::OnceClosure quit_closure_;
+};
+
 // Printing only a selection containing iframes is partially supported.
 // Iframes aren't currently displayed. This test passes whenever the print
 // preview is rendered (i.e. no timeout in the test).
 // This test shouldn't crash. See https://crbug.com/732780.
 IN_PROC_BROWSER_TEST_F(PrintBrowserTest, SelectionContainsIframe) {
-  ASSERT_TRUE(embedded_test_server()->Start());
+  ASSERT_TRUE(embedded_test_server()->Started());
   GURL url(embedded_test_server()->GetURL("/printing/selection_iframe.html"));
   ui_test_utils::NavigateToURL(browser(), url);
 
   PrintAndWaitUntilPreviewIsReady(/*print_only_selection=*/true);
+}
+
+IN_PROC_BROWSER_TEST_F(PrintBrowserTest, PrintFrameContent) {
+  ASSERT_TRUE(embedded_test_server()->Started());
+  GURL url(embedded_test_server()->GetURL("/printing/test1.html"));
+  ui_test_utils::NavigateToURL(browser(), url);
+
+  base::RunLoop run_loop;
+  content::RenderFrameHost* rfh =
+      browser()->tab_strip_model()->GetActiveWebContents()->GetMainFrame();
+  scoped_refptr<TestPrintFrameContentMsgFilter> filter =
+      new TestPrintFrameContentMsgFilter(run_loop.QuitWhenIdleClosure());
+  rfh->GetProcess()->AddFilter(filter.get());
+  rfh->Send(new PrintMsg_PrintFrameContent(
+      rfh->GetRoutingID(), gfx::Rect(800, 600), 0 /* content id */));
+
+  run_loop.Run();
+}
+
+IN_PROC_BROWSER_TEST_F(PrintBrowserTest, PrintSubFrameContent) {
+  ASSERT_TRUE(embedded_test_server()->Started());
+  GURL url(
+      embedded_test_server()->GetURL("/printing/content_with_iframe.html"));
+  ui_test_utils::NavigateToURL(browser(), url);
+
+  base::RunLoop run_loop;
+  content::WebContents* original_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  WaitForLoadStop(original_contents);
+
+  ASSERT_EQ(2u, original_contents->GetAllFrames().size());
+  content::RenderFrameHost* child_frame = original_contents->GetAllFrames()[1];
+  ASSERT_TRUE(child_frame);
+
+  scoped_refptr<TestPrintFrameContentMsgFilter> filter =
+      new TestPrintFrameContentMsgFilter(run_loop.QuitWhenIdleClosure());
+  child_frame->GetProcess()->AddFilter(filter.get());
+  child_frame->Send(new PrintMsg_PrintFrameContent(
+      child_frame->GetRoutingID(), gfx::Rect(800, 600), 0 /* content id */));
+
+  run_loop.Run();
 }
 
 }  // namespace printing
