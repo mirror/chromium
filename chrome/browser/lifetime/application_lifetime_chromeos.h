ff7ff88e2364f50de9426e53f2059579d01ebcdd
Alexander Alekseev <alemate@chromium.org>
2017-12-12 03:44:44 -0800

    ChromeOS: flush user profile data before starting exit sequence.
    On Chrome OS session_manager imposes timeout on Chrome exit.
    If browser fails to shut down within specified time, it will be killed.
    To minimize chance of getting inconsistent user prefs vs Local State
    this CL flushes user pref storage data before starting exit sequence.
    
    Bug: 777935
    Change-Id: I50b9c4acd27356d8453b44fa35b7bc2903e86e3b

---
+++ chrome/browser/lifetime/application_lifetime_chromeos.h
@@ -0,0 +1,32 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_LIFETIME_APPLICATION_LIFETIME_CHROMEOS_H_
+#define CHROME_BROWSER_LIFETIME_APPLICATION_LIFETIME_CHROMEOS_H_
+
+#include "base/callback_forward.h"
+
+namespace chromeos {
+
+// Starts restart flow.
+void AttemptRestart();
+
+// Starts exit flow.
+void AttemptUserExitChromeOS();
+
+// Shutdown chrome cleanly without blocking. This is called
+// when SIGTERM is received on Chrome OS, and always sets
+// exit-cleanly bit and exits the browser, even if there is
+// ongoing downloads or a page with onbeforeunload handler.
+//
+// If you need to exit or restart in your code on ChromeOS,
+// use AttemptExit or AttemptRestart respectively.
+void ExitCleanly();
+
+// Returns true if any of the above Attempt calls have been called.
+bool IsAttemptingShutdown();
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_LIFETIME_APPLICATION_LIFETIME_CHROMEOS_H_
