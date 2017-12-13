81dd2dcaf173792a0bb1aef35da1b9e9f7208425
Ryan Sturm <ryansturm@chromium.org>
2017-12-13 14:46:39 -0800

    Adding a feature for DogFood and sending dogfood to d_r_p
    DataReductionProxy would like to know about dogfood.
    
    Bug: 771727
    Change-Id: I7e69a9447abf49bec78dff8b36e927e942bd7448

---
+++ base/metrics/metrics_features.cc
@@ -0,0 +1,22 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/metrics/metrics_features.h"
+
+#include "base/feature_list.h"
+
+namespace base {
+
+namespace {
+
+// Enabled for Chrome dogfooders.
+const Feature kDogfood{"Dogfood", FEATURE_DISABLED_BY_DEFAULT};
+
+}  // namespace
+
+bool IsDogfoodUser() {
+  return FeatureList::IsEnabled(kDogfood);
+}
+
+}  // namespace base
---
+++ base/metrics/metrics_features.h
@@ -0,0 +1,17 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_METRICS_METRICS_FEATURES_H_
+#define BASE_METRICS_METRICS_FEATURES_H_
+
+#include "base/base_export.h"
+
+namespace base {
+
+// Whether the user is a Chrome dogfooder.
+BASE_EXPORT bool IsDogfoodUser();
+
+}  // namespace base
+
+#endif  // BASE_METRICS_METRICS_FEATURES_H_
