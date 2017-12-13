03752b5b9ff3a3bc284acd2082067ad1e599e357
Robert Sesek <rsesek@chromium.org>
2017-12-13 05:05:02 -0500

    crash_keys: Convert "seccomp-sigsys" to the new API.
    Bug: 598854
    Cq-Include-Trybots: master.tryserver.chromium.android:android_arm64_dbg_recipe;master.tryserver.chromium.android:android_compile_mips_dbg;master.tryserver.chromium.android:android_compile_x64_dbg;master.tryserver.chromium.android:android_compile_x86_dbg
    Change-Id: I9aae58daee5ba3eedbfc4601e1fabdab18013912

--- android_webview/common/crash_reporter/crash_keys.cc
+++ android_webview/common/crash_reporter/crash_keys.cc
@@ -48,9 +48,6 @@ size_t RegisterWebViewCrashKeys() {
       // media/:
       {kZeroEncodeDetails, kSmallSize},
 
-      // sandbox/:
-      {"seccomp-sigsys", kMediumSize},
-
       // Temporary for https://crbug.com/685996.
       {"user-cloud-policy-manager-connect-trace", kMediumSize},
 
