1fa3ec389f12a91be878e861d693c93decb72e1d
James Cook <jamescook@chromium.org>
2017-12-13 15:36:15 -0800

    Clean up mash mojo service registration
    Ensure that code associated with the UI Service, which is used on both
    chromeos and linux-ozone, uses files and functions named *mus*.
    
    Reserve *mash* for chromeos-only code that runs under --mash and uses
    the ash window manager.
    
    Bug: 722527
    Test: chrome, chrome --mus, and chrome --mash run with target_os="chromeos" and linux-ozone still compiles (I can't run linux-ozone due to xkb issues)
    Change-Id: I452e80dffb16d6445f40757d1198ff98233bb528

--- chrome/utility/chrome_content_utility_client.cc
+++ chrome/utility/chrome_content_utility_client.cc
@@ -291,8 +291,8 @@ void ChromeContentUtilityClient::RegisterServices(
 #endif
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
-#if BUILDFLAG(ENABLE_MUS)
-  RegisterMashServices(services);
+#if defined(OS_CHROMEOS)
+  RegisterOutOfProcessMashServices(services);
 #endif
 }
 
