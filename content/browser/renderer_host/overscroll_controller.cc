30cd8705fc866afd162820870722cf23e244a2e0
Sandra Sun <sunyunjia@chromium.org>
2017-12-13 12:23:24 -0500

    Use overscroll-behavior to control swipe-navigation on CrOS
    Use overscroll-behavior css property on viewport defining element to
    control overscroll navigation. This patch enables this for CrOS swipe
    navigation. Details can be found at
    https://wicg.github.io/overscroll-behavior/
    
    This is implemented by plumbing OverscrollBehavior to
    OverscrollWindowAnimation by OnOverscrollBehaviorUpdate(). By looking
    at the OverscrollBehavior value, the OverscrollWindowAnimation decides
    whether to navigate or not.
    
    Bug: 762023
    Change-Id: I47e50fec12a7f508ffa04c2e53376101fc1b98be

--- content/browser/renderer_host/overscroll_controller.cc
+++ content/browser/renderer_host/overscroll_controller.cc
@@ -142,6 +142,12 @@ bool OverscrollController::WillHandleEvent(const blink::WebInputEvent& event) {
           event.GetType() == blink::WebInputEvent::kGestureFlingStart);
 }
 
+void OverscrollController::OnDidOverscroll(
+    const ui::DidOverscrollParams& params) {
+  if (delegate_)
+    delegate_->OnOverscrollBehaviorUpdate(params.overscroll_behavior);
+}
+
 void OverscrollController::ReceivedEventACK(const blink::WebInputEvent& event,
                                             bool processed) {
   if (!ShouldProcessEvent(event))
