66688f59fe581c542301053fb2936c8eb7f0a4b9
John Z Wu <jzw@chromium.org>
2017-12-13 15:54:56 -0800

    Clean shut down for //ios/web_view.
    If you quit the app in iOS multitasking, it DCHECKs in a few places. This makes it impossible to
    write integration tests.
    
    This happens because, in our current setup, ObjC classes stay around longer than C++ classes.
    More specifically, CWVWebViewConfiguration stays around longer than WebViewWebMainParts.
    Since any client may retain CWVWebViewConfiguration for any reason, we cannot rely on dealloc to
    clean things up. Instead, a |shutDown| method is used to tear things down in the proper order.
    
    Bug:733452
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I5c7b3e2f5fc81ee9d67e3a8a499301e4c814726e

---
+++ ios/web_view/internal/cwv_web_view_internal.h
@@ -0,0 +1,19 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_VIEW_INTERNAL_CWV_WEB_VIEW_INTERNAL_H_
+#define IOS_WEB_VIEW_INTERNAL_CWV_WEB_VIEW_INTERNAL_H_
+
+#import "ios/web_view/public/cwv_web_view.h"
+
+@interface CWVWebView ()
+
+// This is called by the CWVWebViewConfiguration that created this class in
+// order to shut down cleanly. See CWVWebViewConfiguration's |shutDown| method
+// for more information.
+- (void)shutDown;
+
+@end
+
+#endif  // IOS_WEB_VIEW_INTERNAL_CWV_WEB_VIEW_INTERNAL_H_
