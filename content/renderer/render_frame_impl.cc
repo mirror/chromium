126ab3dd89413ec6169b666d4d5a7bdce8e64d33
Matt Falkenhagen <falken@chromium.org>
2017-12-13 23:11:56 +0900

    Teach Blink to read request bodies made of data pipes.
    This is needed for service worker since the flow is:
    1) Blink sends //content the WebHTTPBody containing blobs.
    2) //content makes a ResourceRequestBody where blobs are converted to
    data pipes.
    3) //content sends the ResourceRequestBody back to Blink for service
    worker FetchEvent dispatch. It is converted to a WebHTTPBody with
    data pipes.
    
    Bug: 778878
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Id260d34dcfd5457e0b4abf72d8e185728eed15a6

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -6386,7 +6386,8 @@ void RenderFrameImpl::NavigateInternal(
                            : network::mojom::RequestContextFrameType::kNested);
 
   if (IsBrowserSideNavigationEnabled() && common_params.post_data) {
-    request.SetHTTPBody(GetWebHTTPBodyForRequestBody(common_params.post_data));
+    request.SetHTTPBody(
+        GetWebHTTPBodyForRequestBody(*(common_params.post_data)));
     if (!request_params.post_content_type.empty()) {
       request.AddHTTPHeaderField(
           WebString::FromASCII(net::HttpRequestHeaders::kContentType),
@@ -6522,7 +6523,7 @@ void RenderFrameImpl::NavigateInternal(
     if (common_params.method == "POST" && !browser_side_navigation &&
         common_params.post_data) {
       request.SetHTTPBody(
-          GetWebHTTPBodyForRequestBody(common_params.post_data));
+          GetWebHTTPBodyForRequestBody(*(common_params.post_data)));
     }
 
     should_load_request = true;
