417cf81dae613cfc91dae6455c98da583ae2fb66
Matt Falkenhagen <falken@chromium.org>
2017-12-13 12:22:36 +0900

    WIP for trybot
    Bug: 778878
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Id260d34dcfd5457e0b4abf72d8e185728eed15a6

--- content/renderer/service_worker/service_worker_context_client.cc
+++ content/renderer/service_worker/service_worker_context_client.cc
@@ -203,7 +203,7 @@ void ToWebServiceWorkerRequest(const ResourceRequest& request,
   }
   if (request.request_body) {
     blink::WebHTTPBody body =
-        GetWebHTTPBodyForRequestBody(request.request_body);
+        GetWebHTTPBodyForRequestBody(*(request.request_body));
     body.SetUniqueBoundary();
     web_request->SetBody(body);
   }
