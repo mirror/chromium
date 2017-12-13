bb37bdf283be0977411d004a507e0355309c1551
Dale Curtis <dalecurtis@chromium.org>
2017-12-13 10:12:21 -0800

    Add TimeToMetadata, TimeToFirstFrame, TimeToPlayReady UMA+UKM
    We should watch these values in the wild to determine how various
    sites are performing with their networking as well as for regressions
    in our metadata and decode speed processing.
    
    Ideally we'd like to record time to play as a proxy for user experience,
    but this is hard to record at the moment since Blink lies to WMPI about
    when play is actually called. In the mean time these methods should be
    fairly close to the actual time to play (which would include time from
    when blink start processing the play gesture).
    
    As an artifact of this change, I've switched VideoFrameCompositor
    to use base::OnceCallback since I get presubmit errors about banned
    base::Bind() calls otherwise.
    
    This adds the following UMA stats:
    Media.TimeToFirstFrame.(SRC|MSE|EME)
    Media.TimeToMetadata.(SRC|MSE|EME)
    Media.TimeToPlayReady.(SRC|MSE|EME)
    
    BUG=716643,794263
    TEST=new unittest.
    
    Change-Id: I92996d26d33095b3dbb9b4e11ab14e755d94b6f2

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -526,6 +526,7 @@ void WebMediaPlayerImpl::DoLoad(LoadType load_type,
   SetNetworkState(WebMediaPlayer::kNetworkStateLoading);
   SetReadyState(WebMediaPlayer::kReadyStateHaveNothing);
   media_log_->AddEvent(media_log_->CreateLoadEvent(url.GetString().Utf8()));
+  load_start_time_ = base::TimeTicks::Now();
 
   // URL is used for UKM reporting. Privacy requires we only report origin of
   // the top frame. |is_top_frame| signals how to interpret the origin.
@@ -1161,8 +1162,11 @@ void WebMediaPlayerImpl::SetContentDecryptionModule(
   // Recreate the watch time reporter if necessary.
   const bool was_encrypted = is_encrypted_;
   is_encrypted_ = true;
-  if (!was_encrypted && watch_time_reporter_)
-    CreateWatchTimeReporter();
+  if (!was_encrypted) {
+    media_metrics_provider_->SetIsEME();
+    if (watch_time_reporter_)
+      CreateWatchTimeReporter();
+  }
 
   // For now MediaCapabilities only handles clear content.
   video_decode_stats_reporter_.reset();
@@ -1180,8 +1184,11 @@ void WebMediaPlayerImpl::OnEncryptedMediaInitData(
   // Recreate the watch time reporter if necessary.
   const bool was_encrypted = is_encrypted_;
   is_encrypted_ = true;
-  if (!was_encrypted && watch_time_reporter_)
-    CreateWatchTimeReporter();
+  if (!was_encrypted) {
+    media_metrics_provider_->SetIsEME();
+    if (watch_time_reporter_)
+      CreateWatchTimeReporter();
+  }
 
   // For now MediaCapabilities only handles clear content.
   video_decode_stats_reporter_.reset();
@@ -1451,6 +1458,14 @@ void WebMediaPlayerImpl::OnEnded() {
 void WebMediaPlayerImpl::OnMetadata(PipelineMetadata metadata) {
   DVLOG(1) << __func__;
   DCHECK(main_task_runner_->BelongsToCurrentThread());
+  const base::TimeDelta elapsed = base::TimeTicks::Now() - load_start_time_;
+  media_metrics_provider_->SetTimeToMetadata(elapsed);
+  if (chunk_demuxer_)
+    UMA_HISTOGRAM_MEDIUM_TIMES("Media.TimeToMetadata.MSE", elapsed);
+  else
+    UMA_HISTOGRAM_MEDIUM_TIMES("Media.TimeToMetadata.SRC", elapsed);
+  if (is_encrypted_)
+    UMA_HISTOGRAM_MEDIUM_TIMES("Media.TimeToMetadata.EME", elapsed);
 
   pipeline_metadata_ = metadata;
 
@@ -1585,6 +1600,17 @@ void WebMediaPlayerImpl::OnBufferingStateChange(BufferingState state) {
                  media_log_->id());
     SetReadyState(CanPlayThrough() ? WebMediaPlayer::kReadyStateHaveEnoughData
                                    : WebMediaPlayer::kReadyStateHaveFutureData);
+    if (!have_reported_time_to_play_ready_) {
+      have_reported_time_to_play_ready_ = true;
+      const base::TimeDelta elapsed = base::TimeTicks::Now() - load_start_time_;
+      media_metrics_provider_->SetTimeToPlayReady(elapsed);
+      if (chunk_demuxer_)
+        UMA_HISTOGRAM_MEDIUM_TIMES("Media.TimeToPlayReady.MSE", elapsed);
+      else
+        UMA_HISTOGRAM_MEDIUM_TIMES("Media.TimeToPlayReady.SRC", elapsed);
+      if (is_encrypted_)
+        UMA_HISTOGRAM_MEDIUM_TIMES("Media.TimeToPlayReady.EME", elapsed);
+    }
 
     // Let the DataSource know we have enough data. It may use this information
     // to release unused network connections.
@@ -1839,14 +1865,14 @@ void WebMediaPlayerImpl::OnFrameShown() {
   // for.
   if ((!paused_ && IsBackgroundOptimizationCandidate()) ||
       paused_when_hidden_) {
-    frame_time_report_cb_.Reset(
-        base::Bind(&WebMediaPlayerImpl::ReportTimeFromForegroundToFirstFrame,
-                   AsWeakPtr(), base::TimeTicks::Now()));
+    frame_time_report_cb_.Reset(base::BindOnce(
+        &WebMediaPlayerImpl::ReportTimeFromForegroundToFirstFrame, AsWeakPtr(),
+        base::TimeTicks::Now()));
     vfc_task_runner_->PostTask(
         FROM_HERE,
-        base::Bind(&VideoFrameCompositor::SetOnNewProcessedFrameCallback,
-                   base::Unretained(compositor_.get()),
-                   BindToCurrentLoop(frame_time_report_cb_.callback())));
+        base::BindOnce(&VideoFrameCompositor::SetOnNewProcessedFrameCallback,
+                       base::Unretained(compositor_.get()),
+                       BindToCurrentLoop(frame_time_report_cb_.callback())));
   }
 
   UpdateBackgroundVideoOptimizationState();
@@ -2150,6 +2176,13 @@ void WebMediaPlayerImpl::StartPipeline() {
   Demuxer::EncryptedMediaInitDataCB encrypted_media_init_data_cb =
       BindToCurrentLoop(base::Bind(
           &WebMediaPlayerImpl::OnEncryptedMediaInitData, AsWeakPtr()));
+
+  vfc_task_runner_->PostTask(
+      FROM_HERE,
+      base::BindOnce(&VideoFrameCompositor::SetOnNewProcessedFrameCallback,
+                     base::Unretained(compositor_.get()),
+                     BindToCurrentLoop(base::BindOnce(
+                         &WebMediaPlayerImpl::OnFirstFrame, AsWeakPtr()))));
 
   if (renderer_factory_selector_->GetCurrentFactory()
           ->GetRequiredMediaResourceType() == MediaResource::Type::URL) {
@@ -2898,6 +2931,18 @@ void WebMediaPlayerImpl::RecordVideoNaturalSize(const gfx::Size& natural_size) {
 void WebMediaPlayerImpl::SetTickClockForTest(base::TickClock* tick_clock) {
   tick_clock_ = tick_clock;
   buffered_data_source_host_.SetTickClockForTest(tick_clock);
+}
+
+void WebMediaPlayerImpl::OnFirstFrame(base::TimeTicks frame_time) {
+  DCHECK(!load_start_time_.is_null());
+  const base::TimeDelta elapsed = frame_time - load_start_time_;
+  media_metrics_provider_->SetTimeToFirstFrame(elapsed);
+  if (chunk_demuxer_)
+    UMA_HISTOGRAM_MEDIUM_TIMES("Media.TimeToFirstFrame.MSE", elapsed);
+  else
+    UMA_HISTOGRAM_MEDIUM_TIMES("Media.TimeToFirstFrame.SRC", elapsed);
+  if (is_encrypted_)
+    UMA_HISTOGRAM_MEDIUM_TIMES("Media.TimeToFirstFrame.EME", elapsed);
 }
 
 }  // namespace media
