39d3857af6658e1cb67d6f37b861aef1c78a64e8
Simeon Anfinrud <sanfin@chromium.org>
2017-12-13 14:49:42 -0800

    [media] Extract builder class from MediaCodecBridge.
    This detangles a lot of the setup logic in MediaCodecBridge,
    clearly delineating logic that should happen before start().
    
    The main goal of this CL is to reduce the number of JNI back-
    and-forth in the create*coder static MediaCodecBridge methods.
    Now all the native information is passed to Java code in one go,
    while dedicated methods for each MediaCodec type help reduce
    redundant arguments.
    
    Bug: 794722
    Bug: Internal b/31597555
    Test: media_unittests
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I17bfe37191e4c2bd03cd895dd0ba605c42b1b06b

---
+++ media/base/android/jni_hdr_metadata_test.cc
@@ -0,0 +1,19 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/base/android/jni_hdr_metadata.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace media {
+
+class JniHdrMetadataTest : public ::testing::Test {
+ protected:
+  JniHdrMetadataTest();
+  ~JniHdrMetadataTest();
+};
+
+TEST_F(JniHdrMetadataTest, TestSomething) {}
+
+}  // namespace media
