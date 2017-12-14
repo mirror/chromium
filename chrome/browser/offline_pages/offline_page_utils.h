3417ce4d2b39d6fb030d8781696251a4e1a5a832
Jian Li <jianli@chromium.org>
2017-12-14 15:57:45 -0800

    Validate file digest when serving offline pages
    Bug: 758690
    Change-Id: Ibb0bbc0fa8415c93b38d18b869a89588f852b47f

--- chrome/browser/offline_pages/offline_page_utils.h
+++ chrome/browser/offline_pages/offline_page_utils.h
@@ -13,6 +13,7 @@
 #include "url/gurl.h"
 
 namespace base {
+class FilePath;
 class Time;
 }
 
@@ -54,17 +55,18 @@ class OfflinePageUtils {
   // Callback to inform the duplicate checking result.
   using DuplicateCheckCallback = base::Callback<void(DuplicateCheckResult)>;
 
-  // Returns via callback an offline page related to |url|, if any. The
-  // page is chosen based on creation date; a more recently created offline
-  // page will be preferred over an older one. The offline page captured from
-  // last visit in the tab will not be considered if its tab id does not match
-  // the provided |tab_id|.
-  static void SelectPageForURL(
+  // Returns via callback all offline pages related to |url|. The offline page
+  // captured from last visit in the tab will be excluded if its tab id does not
+  // match the provided |tab_id|. The returned list is sorted based creation
+  // date in descending order. That is, the most recently created offline will
+  // appear as the first element of the list.
+  static void SelectPagesForURL(
       content::BrowserContext* browser_context,
       const GURL& url,
       URLSearchMode url_search_mode,
       int tab_id,
-      const base::Callback<void(const OfflinePageItem*)>& callback);
+      const base::Callback<void(const std::vector<OfflinePageItem>&)>&
+          callback);
 
   // Gets the offline page corresponding to the given web contents.  The
   // returned pointer is owned by the web_contents and may be deleted by user
@@ -149,6 +151,16 @@ class OfflinePageUtils {
       const SizeInBytesCallback& callback,
       const base::Time& begin_time,
       const base::Time& end_time);
+
+  // Computes a SHA256 digest of the specified file. Empty string will be
+  // returned if the digest cannot be computed.
+  static std::string ComputeDigest(const base::FilePath& file_path);
+
+  // Returns true if the specified file has |expected_file_size| and
+  // |expected_digest|.
+  static bool ValidateFile(const base::FilePath& file_path,
+                           int64_t expected_file_size,
+                           const std::string& expected_digest);
 };
 
 }  // namespace offline_pages
