138fa3f251c39de9d43f087d04b4f7d92ff000ba
Dave Schuyler <dschuyler@chromium.org>
2017-12-13 17:34:09 -0800

    [MD extensions] View-in-store feature for Details page
    This CL adds a View in store link row that links to the extension home
    page or Chrome Web Store for the extension.
    
    Bug: 794286
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I06a8c078f0659adc3c69e6139133ec9cf6a81d9a

--- chrome/test/data/webui/extensions/extension_detail_view_test.js
+++ chrome/test/data/webui/extensions/extension_detail_view_test.js
@@ -106,8 +106,12 @@ cr.define('extension_detail_view_tests', function() {
       item.set('data.optionsPage', {openInTab: true, url: optionsUrl});
       expectTrue(testIsVisible('#extensions-options'));
 
-      // TODO(devlin): Add checks for homepage once it's added back to the
-      // mocks.
+      item.set('data.homePage.url', 'http://example.com');
+      Polymer.dom.flush();
+      expectTrue(testIsVisible('#viewInStore'));
+      item.set('data.homePage.url', '');
+      Polymer.dom.flush();
+      expectFalse(testIsVisible('#viewInStore'));
 
       expectFalse(testIsVisible('#id-section'));
       expectFalse(testIsVisible('#inspectable-views'));
