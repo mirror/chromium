nwjs/chromium.src

commit 8f1e606d49b0b2cea3a0c0796a912f50171e7173

cros: Replace some use of mojo::BindingSet in ash with Binding
Several mojo interface implementation in //ash only support a single
connection. For example, many interfaces exist primarily to provide
a single client/delegate interface back to the browser. Replace
their usage of BindingSet with Binding.

Also clean up some interface overrides where a FooImpl in ash
overrides both mojom::Foo and mojom::FooClient. These impl objects
are not "is-a" client interface impls, so don't derive from that
interface.

Bug: none
Test: ash_unittests, browser_tests
Change-Id: Id65aadda0ad9dd74702fbf21fbb8aa307b39d9fc
Reviewed-on: https://chromium-review.googlesource.com/766649
Commit-Queue: James Cook <jamescook@chromium.org>
Reviewed-by: Michael Wasserman <msw@chromium.org>
Cr-Commit-Position: refs/heads/master@{#516100}

ash/new_window_controller.cc
@@ -8,13 +8,13 @@
 
 namespace ash {
 
-NewWindowController::NewWindowController() {}
+NewWindowController::NewWindowController() : binding_(this) {}
 
 NewWindowController::~NewWindowController() {}
 
 void NewWindowController::BindRequest(
     mojom::NewWindowControllerRequest request) {
-  bindings_.AddBinding(this, std::move(request));
+  binding_.Bind(std::move(request));
 }
 
 void NewWindowController::SetClient(

ash/new_window_controller.h
@@ -9,7 +9,7 @@
 #include "ash/public/interfaces/new_window.mojom.h"
 #include "base/macros.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
-#include "mojo/public/cpp/bindings/binding_set.h"
+#include "mojo/public/cpp/bindings/binding.h"
 
 namespace ash {
 
@@ -38,7 +38,7 @@ class ASH_EXPORT NewWindowController : public mojom::NewWindowController {
   void OpenFeedbackPage();
 
  private:
-  mojo::BindingSet<mojom::NewWindowController> bindings_;
+  mojo::Binding<mojom::NewWindowController> binding_;
 
   mojom::NewWindowClientAssociatedPtr client_;
 

ash/cast_config_controller.cc
@@ -9,7 +9,7 @@
 
 namespace ash {
 
-CastConfigController::CastConfigController() {}
+CastConfigController::CastConfigController() : binding_(this) {}
 
 CastConfigController::~CastConfigController() {}
 
@@ -27,7 +27,7 @@ void CastConfigController::RemoveObserver(
 }
 
 void CastConfigController::BindRequest(mojom::CastConfigRequest request) {
-  bindings_.AddBinding(this, std::move(request));
+  binding_.Bind(std::move(request));
 }
 
 void CastConfigController::SetClient(

ash/cast_config_controller.h
@@ -11,7 +11,7 @@
 #include "base/macros.h"
 #include "base/observer_list.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
-#include "mojo/public/cpp/bindings/binding_set.h"
+#include "mojo/public/cpp/bindings/binding.h"
 
 namespace ash {
 
@@ -29,8 +29,7 @@ class CastConfigControllerObserver {
 
 // We want to establish our connection lazily and preferably only once, as
 // TrayCast instances will come and go.
-class CastConfigController : public ash::mojom::CastConfig,
-                             public ash::mojom::CastConfigClient {
+class CastConfigController : public ash::mojom::CastConfig {
  public:
   CastConfigController();
   ~CastConfigController() override;
@@ -48,14 +47,14 @@ class CastConfigController : public ash::mojom::CastConfig,
   void SetClient(mojom::CastConfigClientAssociatedPtrInfo client) override;
   void OnDevicesUpdated(std::vector<mojom::SinkAndRoutePtr> devices) override;
 
-  // ash::mojom::CastConfigClient:
-  void RequestDeviceRefresh() override;
-  void CastToSink(mojom::CastSinkPtr sink) override;
-  void StopCasting(mojom::CastRoutePtr route) override;
+  // Methods to forward to |client_|.
+  void RequestDeviceRefresh();
+  void CastToSink(mojom::CastSinkPtr sink);
+  void StopCasting(mojom::CastRoutePtr route);
 
  private:
-  // Bindings for the CastConfig interface.
-  mojo::BindingSet<mojom::CastConfig> bindings_;
+  // Binding for the CastConfig interface.
+  mojo::Binding<mojom::CastConfig> binding_;
 
   mojom::CastConfigClientAssociatedPtr client_;
 

ash/ime/ime_controller.cc
@@ -11,7 +11,7 @@
 
 namespace ash {
 
-ImeController::ImeController() = default;
+ImeController::ImeController() : binding_(this) {}
 
 ImeController::~ImeController() = default;
 
@@ -24,7 +24,7 @@ void ImeController::RemoveObserver(Observer* observer) {
 }
 
 void ImeController::BindRequest(mojom::ImeControllerRequest request) {
-  bindings_.AddBinding(this, std::move(request));
+  binding_.Bind(std::move(request));
 }
 
 void ImeController::SetClient(mojom::ImeControllerClientPtr client) {

ash/ime/ime_controller.h
@@ -12,7 +12,7 @@
 #include "ash/public/interfaces/ime_info.mojom.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
-#include "mojo/public/cpp/bindings/binding_set.h"
+#include "mojo/public/cpp/bindings/binding.h"
 
 namespace ui {
 class Accelerator;
@@ -100,8 +100,8 @@ class ASH_EXPORT ImeController : public mojom::ImeController {
   std::vector<std::string> GetCandidateImesForAccelerator(
       const ui::Accelerator& accelerator) const;
 
-  // Bindings for users of the mojo interface.
-  mojo::BindingSet<mojom::ImeController> bindings_;
+  // Binding for the mojo interface.
+  mojo::Binding<mojom::ImeController> binding_;
 
   // Client interface back to IME code in chrome.
   mojom::ImeControllerClientPtr client_;

ash/login/lock_screen_controller.cc
@@ -31,7 +31,8 @@ std::string CalculateHash(const std::string& password,
 
 }  // namespace
 
-LockScreenController::LockScreenController() = default;
+LockScreenController::LockScreenController() : binding_(this) {}
+
 LockScreenController::~LockScreenController() = default;
 
 // static
@@ -48,7 +49,7 @@ void LockScreenController::RegisterProfilePrefs(PrefRegistrySimple* registry,
 }
 
 void LockScreenController::BindRequest(mojom::LockScreenRequest request) {
-  bindings_.AddBinding(this, std::move(request));
+  binding_.Bind(std::move(request));
 }
 
 void LockScreenController::SetClient(mojom::LockScreenClientPtr client) {

ash/login/lock_screen_controller.h
@@ -9,7 +9,7 @@
 #include "ash/public/interfaces/lock_screen.mojom.h"
 #include "base/macros.h"
 #include "base/observer_list.h"
-#include "mojo/public/cpp/bindings/binding_set.h"
+#include "mojo/public/cpp/bindings/binding.h"
 
 class PrefRegistrySimple;
 
@@ -109,8 +109,8 @@ class ASH_EXPORT LockScreenController : public mojom::LockScreen {
   // Client interface in chrome browser. May be null in tests.
   mojom::LockScreenClientPtr lock_screen_client_;
 
-  // Bindings for the LockScreen interface.
-  mojo::BindingSet<mojom::LockScreen> bindings_;
+  // Binding for the LockScreen interface.
+  mojo::Binding<mojom::LockScreen> binding_;
 
   // User authentication call that will run when we have system salt.
   PendingAuthenticateUserCall pending_user_auth_;

mirror/chromium

commit 07671a13e57fc1adfbac0a44393704b08eee7d41

cros: Fix crash when opening keyboard shortcut overlay from settings
Crash was introduced in commit 8f1e606d49b0b2cea3a0c0796a912f50171e7173

More than one part of chrome calls mojo methods in NewWindowController,
so there can be more than one binding. I've audited the other changes
in the above CL and they should be OK, so keeping this change small
for easier backport.

Bug: 794581
Test: ash_unittests, chrome unit_tests and browser_tests, manually open keyboard shortcut overlay from webui settings > keyboard section
Change-Id: I6b274ecd7b8e6c0c366c36a398e9094b4c361dd9

ash/new_window_controller.cc
@@ -8,13 +8,13 @@
 
 namespace ash {
 
-NewWindowController::NewWindowController() : binding_(this) {}
+NewWindowController::NewWindowController() = default;
 
 NewWindowController::~NewWindowController() = default;
 
 void NewWindowController::BindRequest(
     mojom::NewWindowControllerRequest request) {
-  binding_.Bind(std::move(request));
+  bindings_.AddBinding(this, std::move(request));
 }
 
 void NewWindowController::SetClient(

ash/new_window_controller.h
@@ -9,7 +9,7 @@
 #include "ash/public/interfaces/new_window.mojom.h"
 #include "base/macros.h"
 #include "mojo/public/cpp/bindings/associated_binding.h"
-#include "mojo/public/cpp/bindings/binding.h"
+#include "mojo/public/cpp/bindings/binding_set.h"
 
 namespace ash {
 
@@ -38,7 +38,9 @@ class ASH_EXPORT NewWindowController : public mojom::NewWindowController {
   void OpenFeedbackPage();
 
  private:
-  mojo::Binding<mojom::NewWindowController> binding_;
+  // More than one part of chrome may connect to call the mojo methods, so use
+  // BindingSet instead of Binding. http://crbug.com/794581
+  mojo::BindingSet<mojom::NewWindowController> bindings_;
 
   mojom::NewWindowClientAssociatedPtr client_;
 