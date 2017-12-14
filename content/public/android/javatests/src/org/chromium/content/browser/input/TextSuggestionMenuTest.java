1ecb80c3b65c01cbb6ff8b735e2801f5bb615e48
Xiaocheng Hu <xiaochengh@chromium.org>
2017-12-14 12:48:44 -0800

    Re-Enable TextSuggestionMenuTest.testDeleteWordMarkedWithSpellingMarker
    A previous CL [1] disabled the test to ease a change in spellchecker [2].
    
    Now that the change is done, the test can be re-enabled.
    
    [1] crrev.com/c/823604
    
    [2] crrev.com/c/821496
    
    Bug: 710097
    Change-Id: Iaf666bd0b4f45c6dabdb3fa34537e0f6156cdbed

--- content/public/android/javatests/src/org/chromium/content/browser/input/TextSuggestionMenuTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/input/TextSuggestionMenuTest.java
@@ -20,7 +20,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content.R;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content.browser.test.ContentJUnit4ClassRunner;
@@ -86,7 +85,6 @@ public class TextSuggestionMenuTest {
     }
 
     @Test
-    @DisabledTest(message = "crbug.com/710097")
     @LargeTest
     public void testDeleteWordMarkedWithSpellingMarker()
             throws InterruptedException, Throwable, TimeoutException {
