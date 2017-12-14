89a42f425507ba91573376c34d3e12daebaaa43c
Chandan Padhi <c.padhi@samsung.com>
2017-12-14 16:22:24 +0530

    Update MediaStreamDevice's |input| field to use base::Optional
    MediaStreamDevice's |input| field is initialized with UnavailableDeviceParams()
    to make it serializable i.e. to allow it to be passed over IPC/mojo. However,
    this masks the absence of a device and therefore has to be reset to empty parameters
    to indicate the absence.
    Making |input| field base::Optional takes care of both the above requirements.
    
    Bug: 793255
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I237965ae3133363f6bb8a667b6d33ed3e81150ca

--- content/browser/renderer_host/media/audio_input_device_manager.cc
+++ content/browser/renderer_host/media/audio_input_device_manager.cc
@@ -88,10 +88,11 @@ int AudioInputDeviceManager::Open(const MediaStreamDevice& device) {
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kUseFakeDeviceForMediaStream)) {
     audio_system_->GetAssociatedOutputDeviceID(
-        device.id, base::BindOnce(&AudioInputDeviceManager::OpenedOnIOThread,
-                                  base::Unretained(this), session_id, device,
-                                  base::TimeTicks::Now(),
-                                  base::Optional<media::AudioParameters>()));
+        device.id,
+        base::BindOnce(&AudioInputDeviceManager::OpenedOnIOThread,
+                       base::Unretained(this), session_id, device,
+                       base::TimeTicks::Now(),
+                       media::AudioParameters::UnavailableDeviceParams()));
   } else {
     // TODO(tommi): As is, we hit this code path when device.type is
     // MEDIA_TAB_AUDIO_CAPTURE and the device id is not a device that
@@ -199,11 +200,8 @@ void AudioInputDeviceManager::OpenedOnIOThread(
 
   MediaStreamDevice media_stream_device(device.type, device.id, device.name);
   media_stream_device.session_id = session_id;
-  media_stream_device.input =
-      input_params.value_or(media::AudioParameters::UnavailableDeviceParams());
+  media_stream_device.input = input_params;
   media_stream_device.matched_output_device_id = matched_output_device_id;
-
-  DCHECK(media_stream_device.input.IsValid());
 
   devices_.push_back(media_stream_device);
 
