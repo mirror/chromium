2cad3eb60736e973f44838710b51f7ab669ba0f8
Scott Violet <sky@chromium.org>
2017-12-14 10:23:31 -0800

    chromeos: switches presentation-callback on by default for chromeos
    My understanding is presentation-callback is completely wired for
    chromeos, so we should be able to turn it on now.
    
    BUG=792214,776877
    TEST=covered by test
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I1be1e89c65caa971ed108d361f4d9352651d2c35

---
+++ ui/gl/gl_switches_util.cc
@@ -0,0 +1,22 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/gl/gl_switches_util.h"
+
+#include "base/command_line.h"
+#include "base/macros.h"
+#include "ui/gl/gl_switches.h"
+
+namespace gl {
+
+bool IsPresentationCallbackEnabled() {
+#if defined(OS_CHROMEOS)
+  return true;
+#else
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kEnablePresentationCallback);
+#endif
+}
+
+}  // namespace gl
