8ca38a76f671c42f269200162079735e8235cb8a
Qiang Xu <warx@chromium.org>
2017-12-14 13:16:54 +0800

    reland: mash: make ash learn/control SpokenFeedbackEnabled
    changes:
    - Move Is{Toggle}SpokenFeedbackEnabled code to ash, which used to be
      AccessibilityDelegate and called into chrome. This won't work in
      mustash world. This change breaks the delegate way, by having prefs
      in ash, the Is{Set} could be done in ash.
    - On pref changed on ash, defining a mojo call to update spoken
      feedback state, which includes Loading/Unloading ChromeVox, Braille
      IME state, ChromeVox focus highlight.
    - Change ToggleSpokenFeedbackEnabled to SetSpokenFeedbackEnabled to
      align with other method names.
    
    Bug: 594887
    Test: tested on emulator with --mash, feature works fine on signing out
    and re-logged-in.
    Change-Id: I09d83e93784924ff84f2d1a9ad0b16e2a41418d8
    enabled update the tray icon visibility. This is one step forward.

--- chrome/browser/chromeos/accessibility/accessibility_manager.h
+++ chrome/browser/chromeos/accessibility/accessibility_manager.h
@@ -153,9 +153,6 @@ class AccessibilityManager
   // Returns true if spoken feedback is enabled, or false if not.
   bool IsSpokenFeedbackEnabled() const;
 
-  // Toggles whether Chrome OS spoken feedback is on or off.
-  void ToggleSpokenFeedback(ash::AccessibilityNotificationVisibility notify);
-
   // Enables or disables the high contrast mode for Chrome.
   void EnableHighContrast(bool enabled);
 
@@ -321,7 +318,7 @@ class AccessibilityManager
   void UpdateAlwaysShowMenuFromPref();
   void OnLargeCursorChanged();
   void UpdateStickyKeysFromPref();
-  void UpdateSpokenFeedbackFromPref();
+  void OnSpokenFeedbackChanged();
   void OnHighContrastChanged();
   void UpdateAutoclickFromPref();
   void UpdateAutoclickDelayFromPref();
@@ -397,7 +394,6 @@ class AccessibilityManager
   PrefHandler switch_access_pref_handler_;
 
   bool sticky_keys_enabled_;
-  bool spoken_feedback_enabled_;
   bool autoclick_enabled_;
   base::TimeDelta autoclick_delay_ms_;
   bool virtual_keyboard_enabled_;
