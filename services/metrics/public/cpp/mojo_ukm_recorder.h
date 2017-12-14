a650f92a07d56457ee0d8724f0b4eb202e98d77a
Steven Holte <holte@google.com>
2017-12-14 13:30:19 -0800

    Whitelist UpdateSourceURL callers.
    New UKM recorders should avoid using this API, since it requires
    additional privacy auditting.
    
    Bug: 780930
    Change-Id: If4aef754cc56997a82dce27ffd4df308e894dd7d

--- services/metrics/public/cpp/mojo_ukm_recorder.h
+++ services/metrics/public/cpp/mojo_ukm_recorder.h
@@ -37,9 +37,6 @@ class METRICS_EXPORT MojoUkmRecorder : public UkmRecorder {
   explicit MojoUkmRecorder(mojom::UkmRecorderInterfacePtr recorder_interface);
   ~MojoUkmRecorder() override;
 
-  // UkmRecorder:
-  void UpdateSourceURL(SourceId source_id, const GURL& url) override;
-
   // Helper for getting the wrapper from a connector.
   static std::unique_ptr<MojoUkmRecorder> Create(
       service_manager::Connector* connector);
@@ -48,6 +45,7 @@ class METRICS_EXPORT MojoUkmRecorder : public UkmRecorder {
 
  private:
   // UkmRecorder:
+  void UpdateSourceURL(SourceId source_id, const GURL& url) override;
   void AddEntry(mojom::UkmEntryPtr entry) override;
 
   mojom::UkmRecorderInterfacePtr interface_;
