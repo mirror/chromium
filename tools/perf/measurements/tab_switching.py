69a7f67b8c81d3aaa809ea7967d6717e675dda8f
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-12-14 18:20:01 -0500

    [exp] tab_switching: Use tracing to measure tab-switching latency.
    [still an experiment]
    
    BUG=tbd
    
    Change-Id: I2e5fbb50ee36b2d93f66637dd1601865cbd1553d

--- tools/perf/measurements/tab_switching.py
+++ tools/perf/measurements/tab_switching.py
@@ -13,6 +13,10 @@ Power usage is also measured.
 from telemetry.page import legacy_page_test
 from telemetry.value import histogram
 from telemetry.value import histogram_util
+from telemetry.value import improvement_direction
+from telemetry.value import list_of_scalar_values
+from telemetry.timeline import model as model_module
+from telemetry.timeline import tracing_config
 
 from metrics import keychain_metric
 
@@ -24,8 +28,14 @@ class TabSwitching(legacy_page_test.LegacyPageTest):
 
   def CustomizeBrowserOptions(self, options):
     keychain_metric.KeychainMetric.CustomizeBrowserOptions(options)
-
     options.AppendExtraBrowserArgs(['--enable-stats-collection-bindings'])
+
+  def WillNavigateToPage(self, page, tab):
+    config = tracing_config.TracingConfig()
+    config.enable_chrome_trace = True
+    config.enable_platform_display_trace = True
+    config.chrome_trace_config.category_filter.AddFilterString('latency')
+    tab.browser.platform.tracing_controller.StartTracing(config)
 
   @classmethod
   def _GetTabSwitchHistogram(cls, tab_to_switch):
@@ -43,11 +53,29 @@ class TabSwitching(legacy_page_test.LegacyPageTest):
     last_histogram = self._GetTabSwitchHistogram(tab)
     total_diff_histogram = histogram_util.SubtractHistogram(last_histogram,
                             self._first_histogram)
-
     display_name = 'MPArch_RWH_TabSwitchPaintDuration'
     results.AddSummaryValue(
         histogram.HistogramValue(None, display_name, 'ms',
             raw_value_json=total_diff_histogram,
             important=False))
+
+    trace_result = tab.browser.platform.tracing_controller.StopTracing()
+    if isinstance(trace_result, tuple):
+      trace_result = trace_result[0]
+    latencies = None
+    none_value_reason = None
+    model = model_module.TimelineModel(trace_result)
+    if model.browser_process:
+      process = model.browser_process
+      latencies = [event.args['ms'] for event in
+          process.IterAllEventsOfName('TabSwitching::Latency')]
+    else:
+      none_value_reason = ('Not enough data.')
+    results.AddValue(list_of_scalar_values.ListOfScalarValues(
+        page, 'tab_switch_to_paint', 'ms', latencies,
+        description='Delay between requesting a tab to become to visible to '
+                    'when the tab is made visible on screen.',
+        none_value_reason=none_value_reason,
+        improvement_direction=improvement_direction.DOWN))
 
     keychain_metric.KeychainMetric().AddResults(tab, results)
