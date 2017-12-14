7ff6790517fd4d439010e897cd4d867d10a2dda1
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2017-12-13 22:52:27 -0800

    [ui] open url from clipboard on GESTURE_TWO_FINGER_TAP by new tab button
    If user use middle button to click by new tab page, chrome will open
    url from clipboard in new tab if any.
    This feature does not work well on devices with touchpad, so let's use
    two finger tap  gesture to support the same behavior
    
    Bug:none
    Change-Id: I74d2b58886a753cdf5b16d8df0595c6970ac395a
    R:sky@chromium.org

--- chrome/browser/ui/views/tabs/tab_strip_impl.cc
+++ chrome/browser/ui/views/tabs/tab_strip_impl.cc
@@ -2359,20 +2359,19 @@ void TabStripImpl::ButtonPressed(views::Button* sender,
     base::RecordAction(UserMetricsAction("NewTab_Button"));
     UMA_HISTOGRAM_ENUMERATION("Tab.NewTab", TabStripModel::NEW_TAB_BUTTON,
                               TabStripModel::NEW_TAB_ENUM_COUNT);
-    if (event.IsMouseEvent()) {
-      const ui::MouseEvent& mouse = static_cast<const ui::MouseEvent&>(event);
-      if (mouse.IsOnlyMiddleMouseButton()) {
-        if (ui::Clipboard::IsSupportedClipboardType(
-                ui::CLIPBOARD_TYPE_SELECTION)) {
-          ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
-          CHECK(clipboard);
-          base::string16 clipboard_text;
-          clipboard->ReadText(ui::CLIPBOARD_TYPE_SELECTION, &clipboard_text);
-          if (!clipboard_text.empty())
-            controller_->CreateNewTabWithLocation(clipboard_text);
-        }
-        return;
+    if ((event.IsMouseEvent() &&
+         static_cast<const ui::MouseEvent&>(event).IsOnlyMiddleMouseButton()) ||
+        event.type() == ui::ET_GESTURE_TWO_FINGER_TAP) {
+      if (ui::Clipboard::IsSupportedClipboardType(
+              ui::CLIPBOARD_TYPE_SELECTION)) {
+        ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+        CHECK(clipboard);
+        base::string16 clipboard_text;
+        clipboard->ReadText(ui::CLIPBOARD_TYPE_SELECTION, &clipboard_text);
+        if (!clipboard_text.empty())
+          controller_->CreateNewTabWithLocation(clipboard_text);
       }
+      return;
     }
 
     controller_->CreateNewTab();
