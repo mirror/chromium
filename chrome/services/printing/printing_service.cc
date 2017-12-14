d1e92ce56b39e8adef156abd410de68e12513987
Jay Civelli <jcivelli@chromium.org>
2017-12-14 14:48:11 -0800

    Servicifying PdfToEmfConverter.
    Bug:
    Change-Id: Ic748547f56bb193a558210f97558640ef8d6e44b

--- chrome/services/printing/printing_service.cc
+++ chrome/services/printing/printing_service.cc
@@ -5,6 +5,8 @@
 #include "chrome/services/printing/printing_service.h"
 
 #include "build/build_config.h"
+#include "chrome/services/printing/pdf_to_emf_converter.h"
+#include "chrome/services/printing/pdf_to_emf_converter_factory.h"
 #include "chrome/services/printing/pdf_to_pwg_raster_converter.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 
@@ -12,13 +14,20 @@ namespace printing {
 
 namespace {
 
+void OnPdfToEmfConverterFactoryRequest(
+    service_manager::ServiceContextRefFactory* ref_factory,
+    printing::mojom::PdfToEmfConverterFactoryRequest request) {
+  mojo::MakeStrongBinding(std::make_unique<printing::PdfToEmfConverterFactory>(
+                              ref_factory->CreateRef()),
+                          std::move(request));
+}
+
 void OnPdfToPwgRasterConverterRequest(
     service_manager::ServiceContextRefFactory* ref_factory,
     printing::mojom::PdfToPwgRasterConverterRequest request) {
-  mojo::MakeStrongBinding(
-      std::make_unique<printing::PdfToPwgRasterConverterImpl>(
-          ref_factory->CreateRef()),
-      std::move(request));
+  mojo::MakeStrongBinding(std::make_unique<printing::PdfToPwgRasterConverter>(
+                              ref_factory->CreateRef()),
+                          std::move(request));
 }
 
 }  // namespace
@@ -35,6 +44,8 @@ void PrintingService::OnStart() {
   ref_factory_ = std::make_unique<service_manager::ServiceContextRefFactory>(
       base::Bind(&service_manager::ServiceContext::RequestQuit,
                  base::Unretained(context())));
+  registry_.AddInterface(
+      base::Bind(&OnPdfToEmfConverterFactoryRequest, ref_factory_.get()));
   registry_.AddInterface(
       base::Bind(&OnPdfToPwgRasterConverterRequest, ref_factory_.get()));
 }
