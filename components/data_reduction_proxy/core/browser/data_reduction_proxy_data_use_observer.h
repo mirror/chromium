584d8d6953bbde317b6af2db1e8637a5e34a485f
Doug Arnett <dougarnett@chromium.org>
2017-12-14 11:13:06 -0800

    Adds new observer method to DRPDataUseObserver for DidFinishLoad
    This is plumbing preparation for estimating data savings for the
    NoScript preview (determined when page load finished event occurs).
    Bug: 781885
    Change-Id: I145b9d270327c757ea1fbfbc44aa5932822c592e

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_data_use_observer.h
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_data_use_observer.h
@@ -37,6 +37,7 @@ class DataReductionProxyDataUseObserver
   void OnPageLoadCommit(data_use_measurement::DataUse* data_use) override;
   void OnPageResourceLoad(const net::URLRequest& request,
                           data_use_measurement::DataUse* data_use) override;
+  void OnPageDidFinishLoad(data_use_measurement::DataUse* data_use) override;
   void OnPageLoadComplete(data_use_measurement::DataUse* data_use) override {}
 
   // |data_reduction_proxy_io_data_| owns |this| and is destroyed only after
