16b912604e41a65609dc5f733cd704584e183261
Scott Violet <sky@chromium.org>
2017-12-13 16:47:48 -0800

    chromeos: makes EasyResizeWindowTargeter apply a hit mask if appropriate
    EasyResizeWindowTargeter may be used to shrink the region of a window
    you can interact with (the shelf uses this in certain modes). For mus
    we only support this with a hit-test mask. This patch wires up
    EasyResizeWindowTargeter to use a hit-test mask when necessary.
    
    BUG=775223
    TEST=covered by test
    
    Change-Id: I3e1b494e890f0f8366ce4a8a1fc64f839a61542f

--- ui/aura/mus/window_port_mus.h
+++ ui/aura/mus/window_port_mus.h
@@ -12,6 +12,7 @@
 
 #include "base/logging.h"
 #include "base/macros.h"
+#include "base/optional.h"
 #include "components/viz/client/client_layer_tree_frame_sink.h"
 #include "components/viz/common/surfaces/surface_info.h"
 #include "services/ui/public/interfaces/cursor/cursor.mojom.h"
@@ -82,6 +83,9 @@ class AURA_EXPORT WindowPortMus : public WindowPort, public WindowMus {
   // in the window manager.
   void SetExtendedHitRegionForChildren(const gfx::Insets& mouse_insets,
                                        const gfx::Insets& touch_insets);
+
+  // See description in mojom for details on this.
+  void SetHitTestMask(const base::Optional<gfx::Rect>& rect);
 
   // Embeds a new client in this Window. See WindowTreeClient::Embed() for
   // details on arguments.
