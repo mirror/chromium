d2bd8451c9d529914ddf0cfaea42fcde609ccb3c
Charles Vazac <cvazac@akamai.com>
2017-12-15 11:02:51 -0600

    Update Server Timing header parsing in accordance with spec change
    See https://www.w3.org/TR/server-timing/#the-server-timing-header-field
    Parameters for a metric in the Server-Timing header are now be named.
    Old:
    Server-Timing: db = 123; "database access"
    
    New:
    Server-Timing: db; dur=123; desc="database access"
    
    Bug: 702760
    Change-Id: I3f3060dc685ba699475e8c55ba46ec5a55849d8e

--- third_party/WebKit/Source/devtools/front_end/sdk/ServerTiming.js
+++ third_party/WebKit/Source/devtools/front_end/sdk/ServerTiming.js
@@ -25,36 +25,148 @@ SDK.ServerTiming = class {
     if (!rawServerTimingHeaders.length)
       return null;
 
-    /**
-     * @param {?string} valueString
-     * @return {?Array<!SDK.ServerTiming>}
-     */
-    function createFromHeaderValue(valueString) {
-      // https://www.w3.org/TR/server-timing/
-      var serverTimingMetricRegExp =
-          /[ \t]*([\!\#\$\%\&\'\*\+\-\.\^\_\`\|\~0-9A-Za-z]+)[ \t]*(?:=[ \t]*(\d+(?:\.\d+)?))?[ \t]*(?:;[ \t]*(?:"([^"]+)"|([\!\#\$\%\&\'\*\+\-\.\^\_\`\|\~0-9A-Za-z]+)))?[ \t]*(?:,(.*))?/;
-      var metricMatch;
-      var result = [];
-      while (valueString && (metricMatch = serverTimingMetricRegExp.exec(valueString))) {
-        var metric = metricMatch[1];
-        var value = metricMatch[2];
-        var description = metricMatch[3] || metricMatch[4];
-        if (value !== undefined)
-          value = Math.abs(parseFloat(metricMatch[2]));
-        valueString = metricMatch[5];  // comma delimited headers
-        if (value === undefined || isNaN(value))
-          value = null;
-        result.push(new SDK.ServerTiming(metric, value, description));
-      }
-      return result;
-    }
-
     var serverTimings = rawServerTimingHeaders.reduce((memo, header) => {
-      var timing = createFromHeaderValue(header.value);
-      Array.prototype.push.apply(memo, timing);
+      var timing = this.createFromHeaderValue(header.value);
+      memo.pushAll(timing.map(function(entry) {
+        return new SDK.ServerTiming(
+            entry.name, entry.hasOwnProperty('duration') ? entry.duration : null,
+            entry.hasOwnProperty('description') ? entry.description : '');
+      }));
       return memo;
     }, []);
     serverTimings.sort((a, b) => a.metric.toLowerCase().compareTo(b.metric.toLowerCase()));
     return serverTimings;
+  }
+
+  /**
+   * @param {?string} valueString
+   * @return {?Array<!Object>}
+   */
+  static createFromHeaderValue(valueString) {
+    function trimLeadingWhiteSpace() {
+      valueString = valueString.replace(/^\s*/, '');
+    }
+    function consumeDelimiter(char) {
+      console.assert(char.length === 1);
+      trimLeadingWhiteSpace();
+      if (valueString.charAt(0) !== char)
+        return false;
+
+      valueString = valueString.substring(1);
+      trimLeadingWhiteSpace();
+      return true;
+    }
+    function consumeToken() {
+      // https://tools.ietf.org/html/rfc7230#appendix-B
+      var result = /^(?:\s*)([\w!#$%&'*+\-.^`|~]+)(?:\s*)(.*)/.exec(valueString);
+      if (!result)
+        return false;
+
+      valueString = result[2];
+      return result;
+    }
+    function consumeTokenOrQuotedString() {
+      if (valueString.charAt(0) === '"')
+        return consumeQuotedString();
+
+      var valueMatch = consumeToken();
+      if (valueMatch)
+        return valueMatch[1];
+    }
+    function consumeQuotedString() {
+      console.assert(valueString.charAt(0) === '"');
+      valueString = valueString.substring(1);  // remove leading DQUOTE
+
+      var value = '';
+      while (valueString.length) {
+        // split into two parts:
+        //  -everything before the first " or \
+        //  -everything else
+        var result = /^([^"\\]*)(.*)/.exec(valueString);
+        value += result[1];
+        if (result[2].charAt(0) === '"') {
+          // we have found our closing "
+          valueString = result[2].substring(1);  // strip off everything after the closing "
+          return value;                          // we are done here
+        }
+
+        console.assert(result[2].charAt(0) === '\\');
+        // special rules for \ found in quoted-string (https://tools.ietf.org/html/rfc7230#section-3.2.6)
+        value += result[2].charAt(1);          // grab the character AFTER the \ (if there was one)
+        valueString = result[2].substring(2);  // strip off \ and next character
+      }
+
+      return null;  // not a valid quoted-string
+    }
+
+    var result = [];
+    var nameMatch;
+    while ((nameMatch = consumeToken()) !== false) {
+      var entry = {name: nameMatch[1]};
+
+      while (consumeDelimiter(';')) {
+        var paramNameMatch;
+        if (!(paramNameMatch = consumeToken()))
+          continue;
+
+        var paramName = paramNameMatch[1].toLowerCase();
+        var parseParameter = this.getParserForParameter(paramName);
+        if (!consumeDelimiter('=')) {
+          if (parseParameter)
+            this.showNoValueWarning(paramName);  // only show this warning if it is a parameter name we recognize
+          continue;
+        }
+
+        // always parse the value, even if we don't recognize the parameter name
+        var paramValue = consumeTokenOrQuotedString();
+        if (!parseParameter) {
+          this.showWarning(`Unrecognized parameter \"${paramName}\".`);
+          continue;
+        }
+        if (paramValue === null) {
+          this.showNoValueWarning(paramName);
+          continue;
+        }
+
+        parseParameter.call(this, entry, paramValue);
+      }
+
+      result.push(entry);
+      if (!consumeDelimiter(','))
+        break;
+    }
+
+    if (valueString.length)
+      this.showWarning(`Extraneous trailing characters.`);
+    return result;
+  }
+
+  static getParserForParameter(paramName) {
+    switch (paramName) {
+      case 'dur':
+        return function(entry, paramValue) {
+          if (!entry.hasOwnProperty('duration')) {
+            var duration = parseFloat(paramValue);
+            if (isNaN(duration))
+              this.showWarning(`Unable to parse \"${paramName}\" value \"${paramValue}\".`);
+            else
+              entry.duration = duration;
+          }
+        };
+
+      case 'desc':
+        return function(entry, paramValue) {
+          if (!entry.hasOwnProperty('description'))
+            entry.description = paramValue;
+        };
+    }
+  }
+
+  static showNoValueWarning(paramName) {
+    this.showWarning(`No value found for parameter \"${paramName}\".`);
+  }
+
+  static showWarning(msg) {
+    Common.console.warn(`ServerTiming: ${msg}`);
   }
 };
