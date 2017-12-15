08a20a95ca8b92276fba47f2d2f8fd2eedaa884c
Lalit Maganti <lalitm@chromium.org>
2017-12-15 21:34:33 +0000

    memory-infra: add generic mechanism to return allocator dump entries
    We want a way to generically return the entries for particular nodes
    in the memory graph. This CL allows the consumer of memory-infra
    to pass in the nodes they are looking for which will then be returned
    to them.
    
    Bug: 728199
    Change-Id: Ib6364280dde0a2de55680eb630b05f2abf7ad0ed

--- services/resource_coordinator/public/cpp/memory_instrumentation/tracing_integration_unittest.cc
+++ services/resource_coordinator/public/cpp/memory_instrumentation/tracing_integration_unittest.cc
@@ -115,9 +115,11 @@ class MockCoordinator : public Coordinator, public mojom::Coordinator {
   void RegisterClientProcess(mojom::ClientProcessPtr,
                              mojom::ProcessType) override {}
 
-  void RequestGlobalMemoryDump(MemoryDumpType dump_type,
-                               MemoryDumpLevelOfDetail level_of_detail,
-                               const RequestGlobalMemoryDumpCallback&) override;
+  void RequestGlobalMemoryDump(
+      MemoryDumpType dump_type,
+      MemoryDumpLevelOfDetail level_of_detail,
+      const std::vector<std::string>& allocator_dump_names,
+      const RequestGlobalMemoryDumpCallback&) override;
 
   void RequestGlobalMemoryDumpAndAppendToTrace(
       MemoryDumpType dump_type,
@@ -243,6 +245,7 @@ class MemoryTracingIntegrationTest : public testing::Test {
 void MockCoordinator::RequestGlobalMemoryDump(
     MemoryDumpType dump_type,
     MemoryDumpLevelOfDetail level_of_detail,
+    const std::vector<std::string>& allocator_dump_names,
     const RequestGlobalMemoryDumpCallback& callback) {
   client_->RequestChromeDump(dump_type, level_of_detail);
   callback.Run(true, mojom::GlobalMemoryDumpPtr());
