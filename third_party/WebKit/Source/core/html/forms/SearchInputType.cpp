04850b5509f766504f422e65993f7e317d64b1d0
Ryan Landay <rlanday@chromium.org>
2017-12-15 11:25:02 -0800

    Update implementation of autocapitalize attribute to match spec proposal
    Bug: 776618
    Change-Id: I5c3d5c2ad2dca8d31f2ab42b204d08d08c3d74d0

--- third_party/WebKit/Source/core/html/forms/SearchInputType.cpp
+++ third_party/WebKit/Source/core/html/forms/SearchInputType.cpp
@@ -149,11 +149,6 @@ void SearchInputType::UpdateView() {
   UpdateCancelButtonVisibility();
 }
 
-const AtomicString& SearchInputType::DefaultAutocapitalize() const {
-  DEFINE_STATIC_LOCAL(const AtomicString, sentences, ("sentences"));
-  return sentences;
-}
-
 void SearchInputType::UpdateCancelButtonVisibility() {
   Element* button = GetElement().UserAgentShadowRoot()->getElementById(
       ShadowElementNames::SearchClearButton());
