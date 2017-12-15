e90127c642ac4789d7fd68887ed6aaa02143dffa
Kinuko Yasuda <kinuko@chromium.org>
2017-12-15 17:02:14 +0900

    S13nServiceWorker: do not destroy SW's main resource loader too early
    After the main resource loader is bound to a loader request
    the loader shouldn't be deleted when the request handler is
    deleted. (The loader should be able to outlive as far as the
    loader's Mojo pipe is held by its client)
    
    Bug: 790933
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I81dab1599d1b0af3aa03f2acfed5f8edc4dd53dd

--- content/browser/service_worker/service_worker_url_loader_job.cc
+++ content/browser/service_worker/service_worker_url_loader_job.cc
@@ -87,7 +87,9 @@ ServiceWorkerURLLoaderJob::ServiceWorkerURLLoaderJob(
   response_head_.load_timing.request_start_time = base::Time::Now();
 }
 
-ServiceWorkerURLLoaderJob::~ServiceWorkerURLLoaderJob() {}
+ServiceWorkerURLLoaderJob::~ServiceWorkerURLLoaderJob() {
+  LOG(ERROR) << "SWURLLoaderJob: DTOR";
+}
 
 void ServiceWorkerURLLoaderJob::FallbackToNetwork() {
   response_type_ = ResponseType::FALLBACK_TO_NETWORK;
@@ -137,10 +139,16 @@ void ServiceWorkerURLLoaderJob::Cancel() {
   url_loader_client_->OnComplete(
       network::URLLoaderCompletionStatus(net::ERR_ABORTED));
   url_loader_client_.reset();
+  DeleteIfNeeded();
 }
 
 bool ServiceWorkerURLLoaderJob::WasCanceled() const {
   return status_ == Status::kCancelled;
+}
+
+void ServiceWorkerURLLoaderJob::DetachedFromRequest() {
+  detached_from_request_ = true;
+  DeleteIfNeeded();
 }
 
 void ServiceWorkerURLLoaderJob::StartRequest() {
@@ -416,6 +424,11 @@ void ServiceWorkerURLLoaderJob::OnComplete(
   DCHECK(url_loader_client_.is_bound());
   status_ = Status::kCompleted;
   url_loader_client_->OnComplete(status);
+}
+
+void ServiceWorkerURLLoaderJob::DeleteIfNeeded() {
+  if (!binding_.is_bound() && detached_from_request_)
+    delete this;
 }
 
 }  // namespace content
