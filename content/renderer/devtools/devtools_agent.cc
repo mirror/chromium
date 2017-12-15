37bcb6c7ee1f95e2632069d5348f66a7f7e27a7a
Dmitry Gozman <dgozman@chromium.org>
2017-12-15 14:22:28 -0800

    [DevTools] Stop listening to WidgetWillClose
    It was introduced to resume other pages in the same render process
    when the page paused on breakpoint is closed.
    
    This now works due to Mojo connection error handler triggering
    detach automatically and unpausing as a result.
    
    Removing content-specific code from DevToolsAgent unblocks
    migrating it to blink.
    
    Bug: 776009
    Change-Id: I20113df99a947eeaeabfb59886a07fc3f866ea27

--- content/renderer/devtools/devtools_agent.cc
+++ content/renderer/devtools/devtools_agent.cc
@@ -163,10 +163,6 @@ DevToolsAgent::DevToolsAgent(RenderFrameImpl* frame)
 DevToolsAgent::~DevToolsAgent() {
 }
 
-void DevToolsAgent::WidgetWillClose() {
-  ContinueProgram();
-}
-
 void DevToolsAgent::OnDestruct() {
   delete this;
 }
