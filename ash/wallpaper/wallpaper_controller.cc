af9eaaa49e3c71b8bcef55a145533464d4c66eae
xdai <xdai@chromium.org>
2017-12-15 09:52:17 -0800

    wallpaper refactoring: update |current_user_wallpaper_info_| when setting wallpaper.
    Bug:794780
    Change-Id: I1696b84bb4987ab5ccf5c91525a195b110befa70

--- ash/wallpaper/wallpaper_controller.cc
+++ ash/wallpaper/wallpaper_controller.cc
@@ -506,6 +506,7 @@ void WallpaperController::SetCustomizedDefaultWallpaperImpl(
 
 void WallpaperController::SetWallpaperImage(const gfx::ImageSkia& image,
                                             const WallpaperInfo& info) {
+  current_user_wallpaper_info_ = info;
   wallpaper::WallpaperLayout layout = info.layout;
   VLOG(1) << "SetWallpaper: image_id="
           << wallpaper::WallpaperResizer::GetImageId(image)
@@ -516,7 +517,6 @@ void WallpaperController::SetWallpaperImage(const gfx::ImageSkia& image,
     return;
   }
 
-  current_location_ = info.location;
   // Cancel any in-flight color calculation because we have a new wallpaper.
   if (color_calculator_) {
     color_calculator_->RemoveObserver(this);
@@ -906,8 +906,8 @@ void WallpaperController::OnColorCalculationComplete() {
   color_calculator_.reset();
   // TODO(crbug.com/787134): The prominent colors of wallpapers with empty
   // location should be cached as well.
-  if (!current_location_.empty())
-    CacheProminentColors(colors, current_location_);
+  if (!current_user_wallpaper_info_.location.empty())
+    CacheProminentColors(colors, current_user_wallpaper_info_.location);
   SetProminentColors(colors);
 }
 
@@ -1117,9 +1117,9 @@ void WallpaperController::CalculateWallpaperColors() {
     color_calculator_.reset();
   }
 
-  if (!current_location_.empty()) {
+  if (!current_user_wallpaper_info_.location.empty()) {
     base::Optional<std::vector<SkColor>> cached_colors =
-        GetCachedColors(current_location_);
+        GetCachedColors(current_user_wallpaper_info_.location);
     if (cached_colors.has_value()) {
       SetProminentColors(cached_colors.value());
       return;
