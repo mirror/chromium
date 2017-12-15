3d2da447cf78df3e35a8c442a2419f2e37a01dea
Sahel Sharify <sahel@chromium.org>
2017-12-15 16:00:01 -0500

    touchpad fling on browser side
    This cl implements touchpad fling processing on browser side.
    Instead of sending GFS events to the rendered, the fling controller generates
    the fling_curve_ on GFS, and ProgressFling generates and sends
    wheel events with momentum phase every 16ms. GSU events with inertial state
    will be generated from wheel events and handled by the renderer as usual.
    If the GFS has zero velocity the controller sends a wheel end event immediately
    rather than generating a fling curve.
    
    Once the fling curve is over or a GSU event with inertial state is not
    consumed, the fling controller will send a wheel event with momentum phase
    ended. Processing the wheel end event will generate a GSE event which shows
    the end of fling. Fling boosting also happens on the browser side whenever
    possible.
    
    Bug: 249063
    Test: FlingControllerTest.*
    Change-Id: I69a89bd38da7e5253bf921a1aa33e5efe601f361

--- content/renderer/render_widget.h
+++ content/renderer/render_widget.h
@@ -74,7 +74,6 @@ namespace blink {
 namespace scheduler {
 class RenderWidgetSchedulingState;
 }
-struct WebActiveWheelFlingParameters;
 struct WebDeviceEmulationParams;
 class WebDragData;
 class WebFrameWidget;
@@ -399,9 +398,6 @@ class CONTENT_EXPORT RenderWidget
   // Helper to convert |point| using ConvertWindowToViewport().
   gfx::PointF ConvertWindowPointToViewport(const gfx::PointF& point);
   gfx::Point ConvertWindowPointToViewport(const gfx::Point& point);
-
-  void TransferActiveWheelFlingAnimation(
-      const blink::WebActiveWheelFlingParameters& params);
 
   uint32_t GetContentSourceId();
   void IncrementContentSourceId();
