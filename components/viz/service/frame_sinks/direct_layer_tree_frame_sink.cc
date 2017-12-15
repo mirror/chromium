280c619e82d810ff61cfad9ced981d65efca64d2
Victor Miura <vmiura@chromium.org>
2017-12-14 19:30:16 -0800

    oop: Split viz::ContextProvider interface into GL and Raster interfaces.
    With https://chromium-review.googlesource.com/c/chromium/src/+/778242,
    viz::ContextProviders now support both GL and Raster interfaces.  This
    change is the first step to splitting availability of those APIs into
    separate viz::GLContextProvider and viz::RasterContextProvider interfaces.
    
    All compositor worker contexts in FrameSinks and code below them have been
    switched to viz::RasterContextProviders.
    
    BUG=757607
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I7cce7a545f3c77938e0a97570cb468a75d7cb945

--- components/viz/service/frame_sinks/direct_layer_tree_frame_sink.cc
+++ components/viz/service/frame_sinks/direct_layer_tree_frame_sink.cc
@@ -22,7 +22,7 @@ DirectLayerTreeFrameSink::DirectLayerTreeFrameSink(
     FrameSinkManagerImpl* frame_sink_manager,
     Display* display,
     scoped_refptr<ContextProvider> context_provider,
-    scoped_refptr<ContextProvider> worker_context_provider,
+    scoped_refptr<RasterContextProvider> worker_context_provider,
     scoped_refptr<base::SingleThreadTaskRunner> compositor_task_runner,
     gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager,
     SharedBitmapManager* shared_bitmap_manager)
