3526cd9a458df01917a9c5e5b7a601b495a514f4
Takayoshi Kochi <kochi@chromium.org>
2017-12-15 19:30:09 +0900

    Remove dead code after multiple shadow removal
    Bug: 795221
    Change-Id: Ic59efd874e3ce68249ecdcd9e0ca02e6ad549e8a

--- third_party/WebKit/Source/core/dom/ShadowRoot.h
+++ third_party/WebKit/Source/core/dom/ShadowRoot.h
@@ -105,20 +105,19 @@ class CORE_EXPORT ShadowRoot final : public DocumentFragment, public TreeScope {
   void SetNeedsAssignmentRecalc();
 
   // For V0
-  ShadowRoot* YoungerShadowRoot() const;
-  ShadowRoot* OlderShadowRoot() const;
-  void SetYoungerShadowRoot(ShadowRoot&);
-  void SetOlderShadowRoot(ShadowRoot&);
-  bool IsYoungest() const { return !YoungerShadowRoot(); }
-  bool IsOldest() const { return !OlderShadowRoot(); }
+  ShadowRoot* YoungerShadowRoot() const { return nullptr; }
+  ShadowRoot* OlderShadowRoot() const { return nullptr; }
+  bool IsYoungest() const { return true; }
+  bool IsOldest() const { return true; }
   bool ContainsShadowElements() const;
   bool ContainsContentElements() const;
   bool ContainsInsertionPoints() const {
     return ContainsShadowElements() || ContainsContentElements();
   }
   unsigned DescendantShadowElementCount() const;
-  HTMLShadowElement* ShadowInsertionPointOfYoungerShadowRoot() const;
-  void SetShadowInsertionPointOfYoungerShadowRoot(HTMLShadowElement*);
+  HTMLShadowElement* ShadowInsertionPointOfYoungerShadowRoot() const {
+    return nullptr;
+  }
   void DidAddInsertionPoint(V0InsertionPoint*);
   void DidRemoveInsertionPoint(V0InsertionPoint*);
   const HeapVector<Member<V0InsertionPoint>>& DescendantInsertionPoints();
