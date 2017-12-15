f4314aab616fd8b84e78aec16b8dd53cc12668b3
Dave Tapuska <dtapuska@chromium.org>
2017-12-15 17:38:50 -0500

    Add mojo based synchronous compositor for Android.
    This change restructures some chrome IPC code into a subclasses. On the
    renderer side the Chrome IPC is moved into the
    SynchronousCompositorProxyChromeIPC subclass and the base class is
    generalized to use callbacks that Mojo can use directly.
    
    On the browser side an impl of the mojo SynchronousCompositor interface
    is made to send Chrome IPC to the renderer.
    
    There are two Hosts for the Synchronous Compositor. The control host
    which is hosted by the SynchronousCompositorBrowserFilter on the IO thread
    and the host which is hosted on the browser ui thread.
    
    All mojo channels are associated channels with the widget_input_handler
    so input is handled in order.
    
    BUG=722928
    
    Change-Id: I517ab8b391433fa6b5e4b4fe2f9440dcfed29c05

--- content/renderer/android/synchronous_compositor_proxy.h
+++ content/renderer/android/synchronous_compositor_proxy.h
@@ -8,22 +8,22 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "base/callback.h"
 #include "base/macros.h"
 #include "base/optional.h"
+#include "content/common/input/synchronous_compositor.mojom.h"
 #include "content/public/common/input_event_ack_state.h"
 #include "content/renderer/android/synchronous_layer_tree_frame_sink.h"
+#include "mojo/public/cpp/bindings/associated_binding.h"
 #include "ui/events/blink/synchronous_input_handler_proxy.h"
 #include "ui/gfx/geometry/scroll_offset.h"
 #include "ui/gfx/geometry/size_f.h"
 
-namespace IPC {
-class Message;
-class Sender;
-}  // namespace IPC
-
 namespace viz {
+namespace cc {
 class CompositorFrame;
 }  // namespace cc
+}  // namespace viz
 
 namespace content {
 
@@ -34,13 +34,14 @@ struct SyncCompositorDemandDrawSwParams;
 struct SyncCompositorSetSharedMemoryParams;
 
 class SynchronousCompositorProxy : public ui::SynchronousInputHandler,
-                                   public SynchronousLayerTreeFrameSinkClient {
+                                   public SynchronousLayerTreeFrameSinkClient,
+                                   public mojom::SynchronousCompositor {
  public:
   SynchronousCompositorProxy(
-      int routing_id,
-      IPC::Sender* sender,
       ui::SynchronousInputHandlerProxy* input_handler_proxy);
   ~SynchronousCompositorProxy() override;
+
+  void Init();
 
   // ui::SynchronousInputHandler overrides.
   void SetNeedsSynchronousAnimateInput() override;
@@ -56,67 +57,60 @@ class SynchronousCompositorProxy : public ui::SynchronousInputHandler,
   void Invalidate() override;
   void SubmitCompositorFrame(uint32_t layer_tree_frame_sink_id,
                              viz::CompositorFrame frame) override;
+  void SinkDestroyed() override;
 
   void SetLayerTreeFrameSink(
       SynchronousLayerTreeFrameSink* layer_tree_frame_sink);
-  void OnMessageReceived(const IPC::Message& message);
-  bool Send(IPC::Message* message);
-  void PopulateCommonParams(SyncCompositorCommonRendererParams* params) const;
+  void PopulateCommonParams(SyncCompositorCommonRendererParams* params);
 
- private:
+  // mojom::SynchronousCompositor overrides.
+  void SynchronizeState() override;
+  void ComputeScroll(base::TimeTicks animation_time) override;
+  void DemandDrawHwAsync(
+      const SyncCompositorDemandDrawHwParams& draw_params) override;
+  void DemandDrawHw(const SyncCompositorDemandDrawHwParams& params,
+                    DemandDrawHwCallback callback) override;
+  void SetSharedMemory(const SyncCompositorSetSharedMemoryParams& params,
+                       SetSharedMemoryCallback callback) override;
+  void DemandDrawSw(const SyncCompositorDemandDrawSwParams& params,
+                    DemandDrawSwCallback callback) override;
+  void ZeroSharedMemory() override;
+  void ZoomBy(float zoom_delta,
+              const gfx::Point& anchor,
+              ZoomByCallback) override;
+  void SetMemoryPolicy(uint32_t bytes_limit) override;
+  void ReclaimResources(
+      uint32_t layer_tree_frame_sink_id,
+      const std::vector<viz::ReturnedResource>& resources) override;
+  void SetScroll(const gfx::ScrollOffset& total_scroll_offset) override;
+
+  void BindChannel(
+      mojom::SynchronousCompositorControlHostPtr control_host,
+      mojom::SynchronousCompositorHostAssociatedPtrInfo host,
+      mojom::SynchronousCompositorAssociatedRequest compositor_request);
+
+ protected:
+  virtual void SendAsyncRendererStateIfNeeded();
+  virtual void LayerTreeFrameSinkCreated();
+  void DemandDrawHwAsyncReply(
+      const content::SyncCompositorCommonRendererParams&,
+      uint32_t,
+      base::Optional<viz::CompositorFrame>);
+
   struct SharedMemoryWithSize;
 
-  // IPC handlers.
-  void OnComputeScroll(base::TimeTicks animation_time);
-  void DemandDrawHwAsync(const SyncCompositorDemandDrawHwParams& params);
-  void DemandDrawHw(const SyncCompositorDemandDrawHwParams& params,
-                    IPC::Message* reply_message);
-  void SetSharedMemory(
-      const SyncCompositorSetSharedMemoryParams& params,
-      bool* success,
-      SyncCompositorCommonRendererParams* common_renderer_params);
-  void ZeroSharedMemory();
-  void DemandDrawSw(const SyncCompositorDemandDrawSwParams& params,
-                    IPC::Message* reply_message);
-  void SynchronouslyZoomBy(
-      float zoom_delta,
-      const gfx::Point& anchor,
-      SyncCompositorCommonRendererParams* common_renderer_params);
-  void SetScroll(const gfx::ScrollOffset& total_scroll_offset);
-
-  void SubmitCompositorFrameHwAsync(uint32_t layer_tree_frame_sink_id,
-                                    viz::CompositorFrame frame);
-  void SubmitCompositorFrameHw(uint32_t layer_tree_frame_sink_id,
-                               viz::CompositorFrame frame);
-  void SendDemandDrawHwReply(base::Optional<viz::CompositorFrame> frame,
-                             uint32_t layer_tree_frame_sink_id,
-                             IPC::Message* reply_message);
-  void SendDemandDrawHwReplyAsync(base::Optional<viz::CompositorFrame> frame,
-                                  uint32_t layer_tree_frame_sink_id);
-  void DoDemandDrawSw(const SyncCompositorDemandDrawSwParams& params);
-  void SubmitCompositorFrameSw(viz::CompositorFrame frame);
-  void SendDemandDrawSwReply(
-      base::Optional<viz::CompositorFrameMetadata> metadata,
-      IPC::Message* reply_message);
-  void SendAsyncRendererStateIfNeeded();
-  void DoDemandDrawHw(const SyncCompositorDemandDrawHwParams& params,
-                      IPC::Message* reply_message);
-
-  const int routing_id_;
-  IPC::Sender* const sender_;
   ui::SynchronousInputHandlerProxy* const input_handler_proxy_;
   const bool use_in_process_zero_copy_software_draw_;
   SynchronousLayerTreeFrameSink* layer_tree_frame_sink_;
-  bool inside_receive_;
-  IPC::Message* hardware_draw_reply_;
-  IPC::Message* software_draw_reply_;
-  bool hardware_draw_reply_async_;
+  DemandDrawHwCallback hardware_draw_reply_;
+  DemandDrawSwCallback software_draw_reply_;
+  ZoomByCallback zoom_by_reply_;
 
   // From browser.
   std::unique_ptr<SharedMemoryWithSize> software_draw_shm_;
 
   // To browser.
-  mutable uint32_t version_;  // Mustable so PopulateCommonParams can be const.
+  uint32_t version_;
   gfx::ScrollOffset total_scroll_offset_;  // Modified by both.
   gfx::ScrollOffset max_scroll_offset_;
   gfx::SizeF scrollable_size_;
@@ -124,8 +118,14 @@ class SynchronousCompositorProxy : public ui::SynchronousInputHandler,
   float min_page_scale_factor_;
   float max_page_scale_factor_;
   bool need_animate_scroll_;
+  bool needs_layer_frame_sink_create_ = false;
   uint32_t need_invalidate_count_;
   uint32_t did_activate_pending_tree_count_;
+
+ private:
+  mojom::SynchronousCompositorControlHostPtr control_host_;
+  mojom::SynchronousCompositorHostAssociatedPtr host_;
+  mojo::AssociatedBinding<mojom::SynchronousCompositor> binding_;
 
   DISALLOW_COPY_AND_ASSIGN(SynchronousCompositorProxy);
 };
--- content/renderer/android/synchronous_layer_tree_frame_sink.cc
+++ content/renderer/android/synchronous_layer_tree_frame_sink.cc
@@ -27,7 +27,6 @@
 #include "components/viz/service/display/texture_deleter.h"
 #include "components/viz/service/frame_sinks/compositor_frame_sink_support.h"
 #include "components/viz/service/frame_sinks/frame_sink_manager_impl.h"
-#include "content/common/input/sync_compositor_messages.h"
 #include "content/common/view_messages.h"
 #include "content/renderer/android/synchronous_compositor_filter.h"
 #include "content/renderer/android/synchronous_compositor_registry.h"
@@ -114,6 +113,7 @@ SynchronousLayerTreeFrameSink::SynchronousLayerTreeFrameSink(
     scoped_refptr<viz::ContextProvider> worker_context_provider,
     scoped_refptr<base::SingleThreadTaskRunner> compositor_task_runner,
     gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager,
+    IPC::Sender* sender,
     int routing_id,
     uint32_t layer_tree_frame_sink_id,
     std::unique_ptr<viz::BeginFrameSource> begin_frame_source,
@@ -127,13 +127,12 @@ SynchronousLayerTreeFrameSink::SynchronousLayerTreeFrameSink(
       routing_id_(routing_id),
       layer_tree_frame_sink_id_(layer_tree_frame_sink_id),
       registry_(registry),
-      sender_(RenderThreadImpl::current()->sync_compositor_message_filter()),
+      sender_(sender),
       memory_policy_(0u),
       frame_swap_message_queue_(frame_swap_message_queue),
       parent_local_surface_id_allocator_(
           new viz::ParentLocalSurfaceIdAllocator),
       begin_frame_source_(std::move(begin_frame_source)) {
-  DCHECK(registry_);
   DCHECK(sender_);
   DCHECK(begin_frame_source_);
   thread_checker_.DetachFromThread();
@@ -145,21 +144,7 @@ SynchronousLayerTreeFrameSink::~SynchronousLayerTreeFrameSink() = default;
 
 void SynchronousLayerTreeFrameSink::SetSyncClient(
     SynchronousLayerTreeFrameSinkClient* compositor) {
-  DCHECK(CalledOnValidThread());
   sync_client_ = compositor;
-  if (sync_client_)
-    Send(new SyncCompositorHostMsg_LayerTreeFrameSinkCreated(routing_id_));
-}
-
-bool SynchronousLayerTreeFrameSink::OnMessageReceived(
-    const IPC::Message& message) {
-  bool handled = true;
-  IPC_BEGIN_MESSAGE_MAP(SynchronousLayerTreeFrameSink, message)
-    IPC_MESSAGE_HANDLER(SyncCompositorMsg_SetMemoryPolicy, SetMemoryPolicy)
-    IPC_MESSAGE_HANDLER(SyncCompositorMsg_ReclaimResources, OnReclaimResources)
-    IPC_MESSAGE_UNHANDLED(handled = false)
-  IPC_END_MESSAGE_MAP()
-  return handled;
 }
 
 bool SynchronousLayerTreeFrameSink::BindToClient(
@@ -177,7 +162,8 @@ bool SynchronousLayerTreeFrameSink::BindToClient(
   client_->SetTreeActivationCallback(
       base::Bind(&SynchronousLayerTreeFrameSink::DidActivatePendingTree,
                  base::Unretained(this)));
-  registry_->RegisterLayerTreeFrameSink(routing_id_, this);
+  if (registry_)
+    registry_->RegisterLayerTreeFrameSink(routing_id_, this);
 
   constexpr bool root_support_is_root = true;
   constexpr bool child_support_is_root = false;
@@ -217,7 +203,10 @@ void SynchronousLayerTreeFrameSink::DetachFromClient() {
   client_->SetBeginFrameSource(nullptr);
   // Destroy the begin frame source on the same thread it was bound on.
   begin_frame_source_ = nullptr;
-  registry_->UnregisterLayerTreeFrameSink(routing_id_, this);
+  if (sync_client_)
+    sync_client_->SinkDestroyed();
+  if (registry_)
+    registry_->UnregisterLayerTreeFrameSink(routing_id_, this);
   client_->SetTreeActivationCallback(base::Closure());
   root_support_.reset();
   child_support_.reset();
@@ -340,7 +329,8 @@ void SynchronousLayerTreeFrameSink::DidNotProduceFrame(
     const viz::BeginFrameAck& ack) {
   DCHECK(!ack.has_damage);
   DCHECK_LE(viz::BeginFrameArgs::kStartingFrameNumber, ack.sequence_number);
-  Send(new ViewHostMsg_DidNotProduceFrame(routing_id_, ack));
+  DCHECK(sync_client_);
+  //  Send(new ViewHostMsg_DidNotProduceFrame(routing_id_, ack));
 }
 
 void SynchronousLayerTreeFrameSink::CancelFallbackTick() {
@@ -436,7 +426,7 @@ void SynchronousLayerTreeFrameSink::InvokeComposite(
   }
 }
 
-void SynchronousLayerTreeFrameSink::OnReclaimResources(
+void SynchronousLayerTreeFrameSink::ReclaimResources(
     uint32_t layer_tree_frame_sink_id,
     const std::vector<viz::ReturnedResource>& resources) {
   // Ignore message if it's a stale one coming from a different output surface
