8067b881967a4e33ea2109e45ddb4b97a20bcea2
Eric Karl <ericrk@chromium.org>
2017-12-14 18:11:21 -0800

    OOP: Image Support
    This patch adds initial support for images in OOP raster, using the transfer cache.
    This has a few sub-components:
    - Update the ServiceImageTransferCacheEntry to handle > max texture size images.
    - Update the GPUImageDecodeCache to have a TRANSFER_CACHE upload mode.
    - Update GPUImageDecodeCache's unit tests to hit the TRANSFER_CACHE path.
    - Update RasterCHROMIUM to take an ImageProvider, and re-factor RasterCHROMIUM to reduce number
      of arguments.
    
    Bug: 777628
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I037391fa150529883f1fc534064e29802cf2d08f

--- cc/tiles/tile_manager.cc
+++ cc/tiles/tile_manager.cc
@@ -1217,7 +1217,7 @@ scoped_refptr<TileTask> TileManager::CreateRasterTask(
           resource, resource_content_id, tile->invalidated_id());
 
   base::Optional<PlaybackImageProvider> image_provider;
-  const bool has_sync_decoded_images = !sync_decoded_images.empty();
+  const bool has_sync_decoded_images = true;  // sync_decoded_images.empty();
   const bool has_at_raster_images = !at_raster_images.empty();
   if (skip_images || has_checker_images || has_sync_decoded_images ||
       has_at_raster_images) {
