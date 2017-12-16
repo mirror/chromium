3f9d77396ea6b21bcb101aceff9b9818a99106b5
Pedro Amaral <amaralp@chromium.org>
2017-12-15 16:33:10 -0800

    Dismiss handles after cut/paste in the browser to fix HTC bug
    In a previous CL
    (https://chromium.googlesource.com/chromium/src/+/e55d1bb1d54e0cb898e2884213dc86aa2dc42270),
    I moved the clearing of handles on paste/cut from the browser to the renderer
    (in order to fix a similar bug except on Samsung and LG). Turns out that this
    doesn't work for HTC's email. This CL brings back clearing on the browser meaning
    we clear on both the browser and renderer.
    
    Bug: 794877
    Change-Id: Ib83faf306dc848b0a8b62340a3329f21a14e0871

--- content/public/android/java/src/org/chromium/content/browser/SelectionPopupController.java
+++ content/public/android/java/src/org/chromium/content/browser/SelectionPopupController.java
@@ -368,11 +368,13 @@ public class SelectionPopupController extends ActionModeCallbackHelper {
             @Override
             public void paste() {
                 SelectionPopupController.this.paste();
+                mWebContents.dismissTextHandles();
             }
 
             @Override
             public void pasteAsPlainText() {
                 SelectionPopupController.this.pasteAsPlainText();
+                mWebContents.dismissTextHandles();
             }
 
             @Override
@@ -734,14 +736,17 @@ public class SelectionPopupController extends ActionModeCallbackHelper {
             selectAll();
         } else if (id == R.id.select_action_menu_cut) {
             cut();
+            mode.finish();
         } else if (id == R.id.select_action_menu_copy) {
             copy();
             mode.finish();
         } else if (id == R.id.select_action_menu_paste) {
             paste();
+            mode.finish();
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O
                 && id == R.id.select_action_menu_paste_as_plain_text) {
             pasteAsPlainText();
+            mode.finish();
         } else if (id == R.id.select_action_menu_share) {
             share();
             mode.finish();
