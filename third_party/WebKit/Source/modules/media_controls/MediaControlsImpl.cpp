8a9be890ac18d4aac82fb5b8c02b73ec71786474
Jennifer Apacible <apacible@chromium.org>
2017-12-18 10:33:35 -0800

    [Picture in Picture] Add Picture In Picture Media Controls Button.
    This adds a button in the media controls for the picture in
    picture feature. This is currently behind a flag and currently doesn't
    do anything.
    
    Bug:726619
    Change-Id: I2cb3151769016ff4e5c26fb6eb2319bdeab2fcae

--- third_party/WebKit/Source/modules/media_controls/MediaControlsImpl.cpp
+++ third_party/WebKit/Source/modules/media_controls/MediaControlsImpl.cpp
@@ -70,6 +70,7 @@
 #include "modules/media_controls/elements/MediaControlOverlayPlayButtonElement.h"
 #include "modules/media_controls/elements/MediaControlPanelElement.h"
 #include "modules/media_controls/elements/MediaControlPanelEnclosureElement.h"
+#include "modules/media_controls/elements/MediaControlPictureInPictureButtonElement.h"
 #include "modules/media_controls/elements/MediaControlPlayButtonElement.h"
 #include "modules/media_controls/elements/MediaControlRemainingTimeDisplayElement.h"
 #include "modules/media_controls/elements/MediaControlTextTrackListElement.h"
@@ -301,6 +302,7 @@ MediaControlsImpl::MediaControlsImpl(HTMLMediaElement& media_element)
       overflow_list_(nullptr),
       media_button_panel_(nullptr),
       loading_panel_(nullptr),
+      picture_in_picture_button_(nullptr),
       cast_button_(nullptr),
       fullscreen_button_(nullptr),
       download_button_(nullptr),
@@ -406,6 +408,8 @@ MediaControlsImpl* MediaControlsImpl::Create(HTMLMediaElement& media_element,
 //     |  |    (-webkit-media-controls-mute-button)
 //     |  +-MediaControlVolumeSliderElement
 //     |  |    (-webkit-media-controls-volume-slider)
+//     |  +-MediaControlPictureInPictureButtonElement
+//     |  |   (-webkit-media-controls-picture-in-picture-button)
 //     |  +-MediaControlFullscreenButtonElement
 //     |  |    (-webkit-media-controls-fullscreen-button)
 //     |  +-MediaControlDownloadButtonElement
@@ -491,6 +495,11 @@ void MediaControlsImpl::InitializeControls() {
   if (PreferHiddenVolumeControls(GetDocument()))
     volume_slider_->SetIsWanted(false);
 
+  if (RuntimeEnabledFeatures::PictureInPictureEnabled()) {
+    picture_in_picture_button_ =
+        new MediaControlPictureInPictureButtonElement(*this);
+    button_panel->AppendChild(picture_in_picture_button_);
+  }
   fullscreen_button_ = new MediaControlFullscreenButtonElement(*this);
   button_panel->AppendChild(fullscreen_button_);
 
@@ -523,6 +532,11 @@ void MediaControlsImpl::InitializeControls() {
   // overflow menu.
   overflow_list_->AppendChild(play_button_->CreateOverflowElement(
       new MediaControlPlayButtonElement(*this)));
+  if (RuntimeEnabledFeatures::PictureInPictureEnabled()) {
+    overflow_list_->AppendChild(
+        picture_in_picture_button_->CreateOverflowElement(
+            new MediaControlPictureInPictureButtonElement(*this)));
+  }
   overflow_list_->AppendChild(fullscreen_button_->CreateOverflowElement(
       new MediaControlFullscreenButtonElement(*this)));
   overflow_list_->AppendChild(download_button_->CreateOverflowElement(
@@ -901,6 +915,7 @@ void MediaControlsImpl::UpdateOverflowMenuWanted() const {
   std::pair<MediaControlElementBase*, bool> row_elements[] = {
       std::make_pair(play_button_.Get(), true),
       std::make_pair(mute_button_.Get(), true),
+      std::make_pair(picture_in_picture_button_.Get(), true),
       std::make_pair(fullscreen_button_.Get(), true),
       std::make_pair(current_time_display_.Get(), true),
       std::make_pair(duration_display_.Get(), true),
@@ -1310,15 +1325,11 @@ void MediaControlsImpl::ComputeWhichControlsFit() {
   // Controls that we'll hide / show, in order of decreasing priority.
   MediaControlElementBase* elements[] = {
       // Exclude m_overflowMenu; we handle it specially.
-      play_button_.Get(),
-      fullscreen_button_.Get(),
-      download_button_.Get(),
-      timeline_.Get(),
-      mute_button_.Get(),
-      volume_slider_.Get(),
-      toggle_closed_captions_button_.Get(),
-      cast_button_.Get(),
-      current_time_display_.Get(),
+      play_button_.Get(),       picture_in_picture_button_.Get(),
+      fullscreen_button_.Get(), download_button_.Get(),
+      timeline_.Get(),          mute_button_.Get(),
+      volume_slider_.Get(),     toggle_closed_captions_button_.Get(),
+      cast_button_.Get(),       current_time_display_.Get(),
       duration_display_.Get(),
   };
 
@@ -1433,6 +1444,7 @@ void MediaControlsImpl::MaybeRecordElementsDisplayed() const {
 
   MediaControlElementBase* elements[] = {
       play_button_.Get(),
+      picture_in_picture_button_.Get(),
       fullscreen_button_.Get(),
       download_button_.Get(),
       timeline_.Get(),
@@ -1584,6 +1596,7 @@ void MediaControlsImpl::Trace(blink::Visitor* visitor) {
   visitor->Trace(timeline_);
   visitor->Trace(mute_button_);
   visitor->Trace(volume_slider_);
+  visitor->Trace(picture_in_picture_button_);
   visitor->Trace(toggle_closed_captions_button_);
   visitor->Trace(fullscreen_button_);
   visitor->Trace(download_button_);
