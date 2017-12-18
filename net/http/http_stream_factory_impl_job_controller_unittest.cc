2d99136eeaef8321f69aabc303d7916b3485026b
Helen Li <xunjieli@chromium.org>
2017-12-18 18:26:11 -0500

    Moves Jingle ProxyResolvingClientSocket to services/network/
    ProxyResolvingClientSocket accesses socket pools, http auth cache,
    client certs cache directly.  As a part of the network servicification
    effort, all interactions with //net need to be done using mojo APIs.
    Socket usage will also be through mojo APIs. This CL moves this class to
    services/network so the API surface that we need to export is smaller.
    
    See design doc:  https://docs.google.com/document/d/1iQl_Y2o7vykiPXpZiNbKov-WZbUb4RprmXIk311tTso/edit?usp=sharing.
    
    Bug: 721401
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: Idbc0cb2e3a9713ef111ddd8b5845f27b1445574e

--- net/http/http_stream_factory_impl_job_controller_unittest.cc
+++ net/http/http_stream_factory_impl_job_controller_unittest.cc
@@ -1,7 +1,6 @@
 // Copyright (c) 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 #include "net/http/http_stream_factory_impl_job_controller.h"
 
 #include <string>
