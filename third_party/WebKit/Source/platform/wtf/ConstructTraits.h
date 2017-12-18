e766ecb5cff9776f76da3360299c62d6a22f9177
Michael Lippautz <mlippautz@chromium.org>
2017-12-18 18:41:23 +0100

    [oilpan] Fix write barrier for HeapVector<Member<T>>
    The optimization in
      faf0c6646d65d165018b9af7331033b4c358e7ba
    was unsound for containers as they can construct elements in place, even
    after the containing object was already marked black.
    
    This patch keeps the ability of doing initializing stores without write
    barrier while adding the barrier for HeapVector operations.
    
    Bug:
    Change-Id: I8273edf45863daa7516580a2926883a69b8d8024

---
+++ third_party/WebKit/Source/platform/wtf/ConstructTraits.h
@@ -0,0 +1,102 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WTF_ConstructTraits_h
+#define WTF_ConstructTraits_h
+
+#include "platform/wtf/Allocator.h"
+#include "platform/wtf/TypeTraits.h"
+#include "platform/wtf/VectorTraits.h"
+
+namespace WTF {
+
+template <typename T>
+class ConstructTraitsBase {
+ protected:
+  template <typename... Args>
+  static T* ConstructElementInternal(void* location, Args&&... args) {
+    return new (NotNull, location) T(std::forward<Args>(args)...);
+  }
+};
+
+template <typename T,
+          typename Allocator,
+          bool = Allocator::kIsGarbageCollected,
+          bool = VectorTraits<T>::template IsTraceableInCollection<>::value>
+class ConstructTraits;
+
+template <typename T, typename Allocator>
+class ConstructTraits<T,
+                      Allocator,
+                      false /*kIsGarbageCollected*/,
+                      false /*IsTraceableInCollection*/>
+    : public ConstructTraitsBase<T> {
+ public:
+  template <typename... Args>
+  static T* ConstructElement(void* location, Args&&... args) {
+    return ConstructTraitsBase<T>::ConstructElementInternal(
+        location, std::forward<Args>(args)...);
+  }
+
+  static void NewInitializedElements(T* begin, T* end) {}
+};
+
+template <typename T, typename Allocator>
+class ConstructTraits<T,
+                      Allocator,
+                      true /*kIsGarbageCollected*/,
+                      false /*IsTraceableInCollection*/>
+    : public ConstructTraitsBase<T> {
+ public:
+  template <typename... Args>
+  static T* ConstructElement(void* location, Args&&... args) {
+    return ConstructTraitsBase<T>::ConstructElementInternal(
+        location, std::forward<Args>(args)...);
+  }
+
+  static void NewInitializedElements(T* begin, T* end) {}
+};
+
+// Special case for WTF::Vector<WTF::Vector<T>> where the inner vector is
+// actually tracable but the whole data structure is allocated on an
+// unmanaged heap.
+template <typename T, typename Allocator>
+class ConstructTraits<T,
+                      Allocator,
+                      false /*kIsGarbageCollected*/,
+                      true /*IsTraceableInCollection*/>
+    : public ConstructTraitsBase<T> {
+ public:
+  template <typename... Args>
+  static T* ConstructElement(void* location, Args&&... args) {
+    return ConstructTraitsBase<T>::ConstructElementInternal(
+        location, std::forward<Args>(args)...);
+  }
+
+  static void NewInitializedElements(T* begin, T* end) {}
+};
+
+template <typename T, typename Allocator>
+class ConstructTraits<T,
+                      Allocator,
+                      true /*kIsGarbageCollected*/,
+                      true /*IsTraceableInCollection*/>
+    : public ConstructTraitsBase<T> {
+ public:
+  template <typename... Args>
+  static T* ConstructElement(void* location, Args&&... args) {
+    T* tmp = ConstructTraitsBase<T>::ConstructElementInternal(
+        location, std::forward<Args>(args)...);
+    Allocator::template NotifyNewObject<T, VectorTraits<T>>(tmp);
+    return tmp;
+  }
+
+  static void NewInitializedElements(T* begin, T* end) {
+    Allocator::template NotifyNewObjects<T, VectorTraits<T>>(begin, end);
+  }
+};
+
+}  // namespace WTF
+
+#endif  // WTF_ConstructTraits_h
