1a9e7de8ccd9823c12d3c59c12846962a8e7e14d
arthursonzogni <arthursonzogni@chromium.org>
2017-12-18 17:53:26 +0100

    Pause/Resume navigation in WillProcessResponse.
    Ensure that the request is properly paused in the net stack while the
    navigation code is performing the WillProcessResponse checks, and only
    restart it when Resume is called.
    
    This is part 3 of the implementation plan.
    Design doc: https://goo.gl/Rrrc7n
    Bug: 705744, 791049
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo;master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: I4166aa3eb50b934238bf352378df257c2026da6a

--- content/browser/loader/resource_dispatcher_host_impl.cc
+++ content/browser/loader/resource_dispatcher_host_impl.cc
@@ -1476,7 +1476,8 @@ ResourceDispatcherHostImpl::CreateBaseResourceHandler(
   if (mojo_request.is_pending()) {
     handler.reset(new MojoAsyncResourceHandler(
         request, this, std::move(mojo_request), std::move(url_loader_client),
-        resource_type));
+        resource_type,
+        false));  // defer_on_response_started.
   } else {
     handler.reset(new AsyncResourceHandler(request, this));
   }
@@ -2174,7 +2175,8 @@ void ResourceDispatcherHostImpl::BeginNavigationRequest(
   if (IsNavigationMojoResponseEnabled()) {
     handler = std::make_unique<MojoAsyncResourceHandler>(
         new_request.get(), this, std::move(url_loader_request),
-        std::move(url_loader_client), resource_type);
+        std::move(url_loader_client), resource_type,
+        true);  // defer_on_response_started.
   } else {
     StreamContext* stream_context =
         GetStreamContextForResourceContext(resource_context);
