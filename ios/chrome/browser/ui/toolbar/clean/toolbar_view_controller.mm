4b10c97dd55624765110d05ffee7ec35fcddab97
Gauthier Ambard <gambard@chromium.org>
2017-12-18 17:50:55 +0100

    Use autolayout instead of frame for omnibox popup
    The omnibox popup is currently positioned using frames. It is not very
    practical as it means that the frames need to be recalculated when the
    UI changes, for example during rotations.
    This CL adds a UILayoutGuide to the toolbar, allowing the popup to be
    positioned and sized using the guide, including when its frame needs
    to be changed after a rotation.
    
    Bug:795660, 788640, 786940
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I761a433f84a9c52fd73728b2c83a9ecc15f7fc02

--- ios/chrome/browser/ui/toolbar/clean/toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/clean/toolbar_view_controller.mm
@@ -26,6 +26,7 @@
 #import "ios/chrome/browser/ui/toolbar/public/web_toolbar_controller_constants.h"
 #import "ios/chrome/browser/ui/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/util/constraints_ui_util.h"
+#import "ios/chrome/browser/ui/util/named_guide.h"
 #import "ios/chrome/common/material_timing.h"
 #include "ios/chrome/grit/ios_theme_resources.h"
 #import "ios/third_party/material_components_ios/src/components/ProgressView/src/MaterialProgressView.h"
@@ -820,6 +821,20 @@
   viewController.view.frame = subview.bounds;
   [subview addSubview:viewController.view];
   [viewController didMoveToParentViewController:self];
+}
+
+- (void)didMoveToParentViewController:(UIViewController*)parent {
+  UILayoutGuide* omniboxPopupGuide = FindNamedGuide(kOmniboxGuide, self.view);
+  [NSLayoutConstraint activateConstraints:@[
+    [self.locationBarContainer.leadingAnchor
+        constraintEqualToAnchor:omniboxPopupGuide.leadingAnchor],
+    [self.locationBarContainer.trailingAnchor
+        constraintEqualToAnchor:omniboxPopupGuide.trailingAnchor],
+    [self.locationBarContainer.topAnchor
+        constraintEqualToAnchor:omniboxPopupGuide.topAnchor],
+    [self.locationBarContainer.bottomAnchor
+        constraintEqualToAnchor:omniboxPopupGuide.bottomAnchor],
+  ]];
 }
 
 #pragma mark - Trait Collection Changes
