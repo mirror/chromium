fde1c861d1771f4668e2a313297c7aae6a5cf915
Marti Wong <martiw@chromium.org>
2017-12-18 13:34:09 +1100

    Refine the shadow style of the MDCAppBar (iOS)
    Refine the shadow style (length and opacity) of the top MDCAppBar so
    that it is consistent with the shadow of the bottom bar in Bookmarks,
    History, Reading List.
    
    After the change, every iOS ui which is using MDCAppBar will be
    affected, this includes:
    Settings, ContentSuggestions, Signin, PaymentRequest...
    
    Screenshots after change:
    https://drive.google.com/file/d/1WrNwjqEucwZ3IannIViE04n4jH3C9KYm
    https://drive.google.com/file/d/1QxSXVw3B_nEwamleFT8scTcS_ObGyuNa
    
    Bug:795623
    Change-Id: I5210a65310acb87b457b977ff016a7fdf7dc1211

--- ios/chrome/browser/ui/material_components/utils.mm
+++ ios/chrome/browser/ui/material_components/utils.mm
@@ -10,10 +10,29 @@
 #import "ios/third_party/material_components_ios/src/components/FlexibleHeader/src/MaterialFlexibleHeader.h"
 #import "ios/third_party/material_components_ios/src/components/NavigationBar/src/MaterialNavigationBar.h"
 #import "ios/third_party/material_components_ios/src/components/Palettes/src/MaterialPalettes.h"
+#import "ios/third_party/material_components_ios/src/components/ShadowLayer/src/MaterialShadowLayer.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+// Customize the length and opacity of AppBar's shadow.
+void CustomizeAppBarShadow(MDCAppBar* appBar) {
+  // Adjust the length of the shadow using a customized intensityBlock.
+  MDCFlexibleHeaderShadowIntensityChangeBlock intensityBlock =
+      ^(CALayer* _Nonnull shadowLayer, CGFloat intensity) {
+        // elevation = 10.26 makes the shadow 12pt tall like the bottom
+        // toolbar's.
+        CGFloat elevation = 10.26f * intensity;
+        [(MDCShadowLayer*)shadowLayer setElevation:elevation];
+      };
+  // Adjust the opacity of the shadow on the customized shadow layer.
+  MDCShadowLayer* shadowLayer = [MDCShadowLayer layer];
+  shadowLayer.opacity = 0.4f;
+  // Apply the customized shadow layer on the headerView of appBar.
+  [appBar.headerViewController.headerView setShadowLayer:shadowLayer
+                                 intensityDidChangeBlock:intensityBlock];
+}
 
 void ConfigureAppBarWithCardStyle(MDCAppBar* appBar) {
   appBar.headerViewController.headerView.canOverExtend = NO;
@@ -23,4 +42,6 @@ void ConfigureAppBarWithCardStyle(MDCAppBar* appBar) {
       [[MDCPalette greyPalette] tint200];
   appBar.navigationBar.tintColor = [[MDCPalette greyPalette] tint900];
   appBar.navigationBar.titleAlignment = MDCNavigationBarTitleAlignmentLeading;
+
+  CustomizeAppBarShadow(appBar);
 }
