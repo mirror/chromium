ae0f5b437de826e709b8b03167c26f1b31500680
Alexandr Ilin <alexilin@chromium.org>
2017-12-18 17:19:19 +0100

    android: Record FirstContentfulPaint startup histogram
    This CL adds a new startup histogram
    Startup.Android.Experimental.Cold.TimeToFirstContentfulPaint.
    
    The histogram is recorded only for the first navigation in the application
    lifetime under the same conditions as
    Startup.Android.Experimental.Cold.TimeToFirstNavigationCommit.
    
    Also this CL adds a C++ UMA_HISTOGRAM_COUNTS_100 equivalent to the Java side,
    because now it's used by multiple clients.
    
    Bug: 778302
    Change-Id: I8f81fe70c90e8c9ce9be597f22052786c8d28f63

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/keyboard/GvrKeyboardLoaderClient.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/keyboard/GvrKeyboardLoaderClient.java
@@ -4,7 +4,6 @@
 
 package org.chromium.chrome.browser.vr_shell.keyboard;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -34,7 +33,6 @@ public class GvrKeyboardLoaderClient {
     // GVR doesn't support setting the context twice in the application's lifetime and crashes if we
     // do so. Setting the same context wrapper is a no-op, so we keep a reference to the one we
     // create and use it across re-initialization of the keyboard api.
-    @SuppressLint("StaticFieldLeak")
     private static KeyboardContextWrapper sContextWrapper = null;
 
     @CalledByNative
