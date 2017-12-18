e0bba332e8f14caf2bd49668b7b601c069085c16
Jialiu Lin <jialiul@chromium.org>
2017-12-18 14:55:57 -0800

    force_update: prototyping CL don't submit Bug:
    Change-Id: Id5dd17b2e0c5cd4e2289433704993a1e1560ee6f

--- chromeos/login/auth/user_context.h
+++ chromeos/login/auth/user_context.h
@@ -9,6 +9,7 @@
 
 #include "chromeos/chromeos_export.h"
 #include "chromeos/login/auth/key.h"
+#include "components/password_manager/core/browser/hash_password_manager.h"
 #include "components/signin/core/account_id/account_id.h"
 #include "components/user_manager/user_type.h"
 
@@ -63,6 +64,7 @@ class CHROMEOS_EXPORT UserContext {
   const std::string& GetPublicSessionInputMethod() const;
   const std::string& GetDeviceId() const;
   const std::string& GetGAPSCookie() const;
+  const password_manager::SyncPasswordData& GetSyncPasswordData() const;
 
   bool HasCredentials() const;
 
@@ -81,6 +83,8 @@ class CHROMEOS_EXPORT UserContext {
   void SetPublicSessionInputMethod(const std::string& input_method);
   void SetDeviceId(const std::string& device_id);
   void SetGAPSCookie(const std::string& gaps_cookie);
+  void SetSyncPasswordData(
+      const password_manager::SyncPasswordData& sync_password_data);
 
   void ClearSecrets();
 
@@ -100,6 +104,7 @@ class CHROMEOS_EXPORT UserContext {
   std::string public_session_input_method_;
   std::string device_id_;
   std::string gaps_cookie_;
+  password_manager::SyncPasswordData sync_password_data_;
 };
 
 }  // namespace chromeos
