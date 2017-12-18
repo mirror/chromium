fbdf381bf86c36fb8f50b1c9eb12a38c0222d392
Jun Cai <juncai@chromium.org>
2017-12-18 11:15:00 -0800

    (DO NOT SUBMIT) Add tls client auth
    Bug: 783996
    Change-Id: I9bd44d29e8f05718a531678d9b3eb7f222f11e78

--- services/network/public/cpp/network_param_ipc_traits.h
+++ services/network/public/cpp/network_param_ipc_traits.h
@@ -14,7 +14,9 @@
 #include "net/cert/ct_policy_status.h"
 #include "net/cert/signed_certificate_timestamp.h"
 #include "net/cert/signed_certificate_timestamp_and_status.h"
+#include "net/cert/x509_certificate.h"
 #include "net/http/http_request_headers.h"
+#include "net/ssl/ssl_cert_request_info.h"
 #include "net/ssl/ssl_info.h"
 
 #ifndef INTERNAL_SERVICES_NETWORK_PUBLIC_CPP_NETWORK_PARAM_IPC_TRAITS_H_
@@ -60,8 +62,28 @@ struct ParamTraits<net::HttpRequestHeaders> {
 };
 
 template <>
+struct ParamTraits<scoped_refptr<net::SSLCertRequestInfo>> {
+  typedef scoped_refptr<net::SSLCertRequestInfo> param_type;
+  static void Write(base::Pickle* m, const param_type& p);
+  static bool Read(const base::Pickle* m,
+                   base::PickleIterator* iter,
+                   param_type* r);
+  static void Log(const param_type& p, std::string* l);
+};
+
+template <>
 struct ParamTraits<net::SSLInfo> {
   typedef net::SSLInfo param_type;
+  static void Write(base::Pickle* m, const param_type& p);
+  static bool Read(const base::Pickle* m,
+                   base::PickleIterator* iter,
+                   param_type* r);
+  static void Log(const param_type& p, std::string* l);
+};
+
+template <>
+struct ParamTraits<scoped_refptr<net::X509Certificate>> {
+  typedef scoped_refptr<net::X509Certificate> param_type;
   static void Write(base::Pickle* m, const param_type& p);
   static bool Read(const base::Pickle* m,
                    base::PickleIterator* iter,
@@ -92,6 +114,9 @@ IPC_ENUM_TRAITS_MAX_VALUE(net::OCSPRevocationStatus,
                           net::OCSPRevocationStatus::UNKNOWN)
 
 IPC_ENUM_TRAITS_MAX_VALUE(net::ct::SCTVerifyStatus, net::ct::SCT_STATUS_MAX)
+
+IPC_ENUM_TRAITS_MAX_VALUE(net::SSLClientCertType,
+                          net::SSLClientCertType::CLIENT_CERT_INVALID_TYPE)
 
 IPC_ENUM_TRAITS_MAX_VALUE(net::SSLInfo::HandshakeType,
                           net::SSLInfo::HANDSHAKE_FULL)
