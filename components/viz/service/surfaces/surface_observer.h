47ccbae6cb83fc1d59b60cecd5143e5b5c891e16
Rijubrata Bhaumik <rijubrata.bhaumik@intel.com>
2017-12-18 11:04:49 +0200

    [viz] Remove SurfaceObserver::OnSurfaceSubtreeDamaged.
    SurfaceObserver::OnSurfaceSubtreeDamaged is only used in tests.
    AggregatedDamageCallback can be used for the same effect.
    
    BUG=776098
    
    Change-Id: I3c9546ee90aed63065fa7100f4d33f3dd616b787

--- components/viz/service/surfaces/surface_observer.h
+++ components/viz/service/surfaces/surface_observer.h
@@ -49,7 +49,6 @@ class SurfaceObserver {
   // damaged at aggregation time.
   // TODO(crbug.com/776098): This is only used in tests. We can probably remove
   // it.
-  virtual void OnSurfaceSubtreeDamaged(const SurfaceId& surface_id) = 0;
 
   // Called whenever |surface| will be drawn in the next display frame.
   virtual void OnSurfaceWillBeDrawn(Surface* surface) {}
