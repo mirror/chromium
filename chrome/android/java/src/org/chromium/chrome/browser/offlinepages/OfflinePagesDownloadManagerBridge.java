66fc45e16fdca9c617f9ee456184b0222ffc142f
Pete Williamson <petewil@chromium.org>
2017-12-19 15:33:13 -0800

    Implement JNI Bridge for using AndroidDownloadManager.
    C++ code in the platform will need to make calls to ADM, which is only
    avialable from Java. So, we'll need a bridge to make the calls to ADM.
    This is the new bridge.  Methods are not yet implemented.
    
    Bug: 758690
    Change-Id: I132d126e0cfb9c313dbb06907627068998f4cf70

---
+++ chrome/android/java/src/org/chromium/chrome/browser/offlinepages/OfflinePagesDownloadManagerBridge.java
@@ -0,0 +1,45 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.offlinepages;
+
+import android.net.Uri;
+
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.base.annotations.JNINamespace;
+
+/**
+ * Since the ADM can only be accessed from Java, this bridge will transfer all C++ calls over to
+ * Java land for making the call to ADM.  This is a one-way bridge, from C++ to Java only. The
+ * Java side of this bridge is not called by other Java code.
+ */
+@JNINamespace("offline_pages::android")
+public class OfflinePagesDownloadManagerBridge {
+    /** This will check to see if ADM is installed on this phone.
+     *  Returns true if the android download app is installed.
+     */
+    @CalledByNative
+    private static boolean isAndroidDownloadManagerInstalled() {
+        // TODO(petewil): Implement
+        return true;
+    }
+
+    /** This is a pass through to the ADM function of the same name.
+     */
+    @CalledByNative
+    private static long addCompletedDownload(String title, String description,
+            boolean isMediaScannerScannable, String mimeType, String path, long length,
+            boolean showNotification, Uri uri, Uri referer) {
+        // TODO(petewil): implement
+        return 0;
+    }
+
+    /** This is a pass through to the ADM function of the same name.
+     */
+    @CalledByNative
+    private static int remove(long[] ids) {
+        // TODO(petewil): implement
+        return 0;
+    }
+}
