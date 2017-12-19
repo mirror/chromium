6e8f9632f13dd72afc3950f81c6e0dc0c0a1748d
Xiaohan Wang <xhwang@chromium.org>
2017-12-19 13:54:27 -0800

    media: Move cdm_file_adapter.*
    Move to media/cdm/ppapi/external_clear_key/ because they are only used
    by ClearKeyCdm.
    
    Change-Id: I40f210f0d20588642c7235473ab6fb4179e2332f

--- media/cdm/ppapi/external_clear_key/cdm_file_adapter.h
+++ media/cdm/ppapi/external_clear_key/cdm_file_adapter.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_CDM_CDM_FILE_ADAPTER_H_
-#define MEDIA_CDM_CDM_FILE_ADAPTER_H_
+#ifndef MEDIA_CDM_PPAPI_EXTERNAL_CLEAR_KEY_CDM_FILE_ADAPTER_H_
+#define MEDIA_CDM_PPAPI_EXTERNAL_CLEAR_KEY_CDM_FILE_ADAPTER_H_
 
 #include <stdint.h>
 
@@ -64,4 +64,4 @@ class MEDIA_EXPORT CdmFileAdapter : public cdm::FileIOClient {
 
 }  // namespace media
 
-#endif  // MEDIA_CDM_CDM_FILE_ADAPTER_H_
+#endif  // MEDIA_CDM_PPAPI_EXTERNAL_CLEAR_KEY_CDM_FILE_ADAPTER_H_
