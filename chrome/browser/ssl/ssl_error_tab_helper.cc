7d2e84adeba8dbfa7024bce49a7695a2912a96ec
Carlos IL <carlosil@chromium.org>
2017-12-19 13:44:55 -0800

    Added mojo interface for interstitial commands.
    Added mojo interface to handle interstitial commands and implemented it
    in SSLErrorTabHelper, which passes the command to SSLBlockingPage.
    
    Bug: 779782
    Change-Id: I261dddd835a9e5360c214acd6cedd6d67d2d3710

--- chrome/browser/ssl/ssl_error_tab_helper.cc
+++ chrome/browser/ssl/ssl_error_tab_helper.cc
@@ -3,8 +3,9 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/ssl/ssl_error_tab_helper.h"
-
+#include "base/strings/string_number_conversions.h"
 #include "components/security_interstitials/content/security_interstitial_page.h"
+#include "components/security_interstitials/core/controller_client.h"
 #include "content/public/browser/navigation_handle.h"
 
 DEFINE_WEB_CONTENTS_USER_DATA_KEY(SSLErrorTabHelper);
@@ -64,11 +65,123 @@ SSLErrorTabHelper::GetBlockingPageForCurrentlyCommittedNavigationForTesting() {
 }
 
 SSLErrorTabHelper::SSLErrorTabHelper(content::WebContents* web_contents)
-    : WebContentsObserver(web_contents) {}
+    : WebContentsObserver(web_contents), binding_(web_contents, this) {}
 
 void SSLErrorTabHelper::SetBlockingPage(
     int64_t navigation_id,
     std::unique_ptr<security_interstitials::SecurityInterstitialPage>
         blocking_page) {
   blocking_pages_for_navigations_[navigation_id] = std::move(blocking_page);
+}
+
+void SSLErrorTabHelper::DontProceed() {
+  if (blocking_page_for_currently_committed_navigation_) {
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(
+            security_interstitials::SecurityInterstitialCommand::
+                CMD_DONT_PROCEED));
+  }
+}
+
+void SSLErrorTabHelper::Proceed() {
+  if (blocking_page_for_currently_committed_navigation_) {
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(
+            security_interstitials::SecurityInterstitialCommand::CMD_PROCEED));
+  }
+}
+
+void SSLErrorTabHelper::ShowMoreSection() {
+  if (blocking_page_for_currently_committed_navigation_) {
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(
+            security_interstitials::SecurityInterstitialCommand::
+                CMD_SHOW_MORE_SECTION));
+  }
+}
+
+void SSLErrorTabHelper::OpenHelpCenter() {
+  if (blocking_page_for_currently_committed_navigation_) {
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(
+            security_interstitials::SecurityInterstitialCommand::
+                CMD_OPEN_HELP_CENTER));
+  }
+}
+
+void SSLErrorTabHelper::OpenDiagnostic() {
+  if (blocking_page_for_currently_committed_navigation_) {
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(
+            security_interstitials::SecurityInterstitialCommand::
+                CMD_OPEN_DIAGNOSTIC));
+  }
+}
+
+void SSLErrorTabHelper::Reload() {
+  if (blocking_page_for_currently_committed_navigation_) {
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(
+            security_interstitials::SecurityInterstitialCommand::CMD_RELOAD));
+  }
+}
+
+void SSLErrorTabHelper::OpenDateSettings() {
+  if (blocking_page_for_currently_committed_navigation_) {
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(
+            security_interstitials::SecurityInterstitialCommand::
+                CMD_OPEN_DATE_SETTINGS));
+  }
+}
+
+void SSLErrorTabHelper::OpenLogin() {
+  if (blocking_page_for_currently_committed_navigation_) {
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(security_interstitials::
+                                 SecurityInterstitialCommand::CMD_OPEN_LOGIN));
+  }
+}
+
+void SSLErrorTabHelper::DoReport() {
+  if (blocking_page_for_currently_committed_navigation_) {
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(security_interstitials::
+                                 SecurityInterstitialCommand::CMD_DO_REPORT));
+  }
+}
+
+void SSLErrorTabHelper::DontReport() {
+  if (blocking_page_for_currently_committed_navigation_) {
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(security_interstitials::
+                                 SecurityInterstitialCommand::CMD_DONT_REPORT));
+  }
+}
+
+void SSLErrorTabHelper::OpenReportingPrivacy() {
+  if (blocking_page_for_currently_committed_navigation_) {
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(
+            security_interstitials::SecurityInterstitialCommand::
+                CMD_OPEN_REPORTING_PRIVACY));
+  }
+}
+
+void SSLErrorTabHelper::OpenWhitepaper() {
+  if (blocking_page_for_currently_committed_navigation_) {
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(
+            security_interstitials::SecurityInterstitialCommand::
+                CMD_OPEN_WHITEPAPER));
+  }
+}
+
+void SSLErrorTabHelper::ReportPhishingError() {
+  if (blocking_page_for_currently_committed_navigation_) {
+    blocking_page_for_currently_committed_navigation_->CommandReceived(
+        base::NumberToString(
+            security_interstitials::SecurityInterstitialCommand::
+                CMD_REPORT_PHISHING_ERROR));
+  }
 }
