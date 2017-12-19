d77434476697e448daabe0787f6cd938a4cf3446
Mustaq Ahmed <mustaq@google.com>
2017-12-19 11:02:17 -0500

    Fix pointer capture release caused by fake mouse moves.
    Fake mousemoves are not sent to DOM.  So they shouldn't update internal
    states that control real events sent to DOM.
    
    This CL also updates a few names & comments to reflect the current
    state of the code.
    
    Bug: 787365
    Change-Id: Ie380c959dd0c44a14cc2517574372e4118654736

--- third_party/WebKit/Source/core/input/PointerEventManager.cpp
+++ third_party/WebKit/Source/core/input/PointerEventManager.cpp
@@ -490,14 +490,6 @@ WebInputEventResult PointerEventManager::SendMousePointerEvent(
   EventTarget* effective_target = GetEffectiveTargetForPointerEvent(
       pointer_event_target, pointer_event->pointerId());
 
-  // Do not send the fake mouse move event to the DOM, because the mouse does
-  // not move.
-  if ((mouse_event_type == EventTypeNames::mousemove) &&
-      mouse_event.GetModifiers() &
-          WebInputEvent::Modifiers::kRelativeMotionEvent) {
-    return WebInputEventResult::kHandledSuppressed;
-  }
-
   WebInputEventResult result =
       DispatchPointerEvent(effective_target, pointer_event);
 
