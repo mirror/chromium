24d8e33dd65eb3efe4009c0935a7f59ab16c9c8c
Thomas Tangl <tangltom@chromium.org>
2017-12-19 14:48:59 +0100

    Add advanced turn-on-sync UI for DICE user menu
    When DICE is enabled and Sync is not turned on, the user menu should
    show a list of available GAIA accounts to sign in, if there are any.
    
    This CL adds a simplified version of this feature. It displays one
    button for the first user in the GAIA accounts list and another
    button for syncing to another account.
    
    Mocks can be found in the bug report.
    
    Bug: 786369
    Change-Id: I1dcb42a72471a3de8d0c64c7a9790bc5d4e79da5

--- chrome/browser/ui/views/profiles/profile_chooser_view.h
+++ chrome/browser/ui/views/profiles/profile_chooser_view.h
@@ -192,6 +192,7 @@ class ProfileChooserView : public content::WebContentsDelegate,
   views::Link* manage_accounts_link_;
   views::LabelButton* manage_accounts_button_;
   views::LabelButton* signin_current_profile_button_;
+  views::LabelButton* signin_with_gaia_account_button_;
 
   // For material design user menu, the active profile card owns the profile
   // name and photo.
@@ -224,6 +225,10 @@ class ProfileChooserView : public content::WebContentsDelegate,
 
   // The current access point of sign in.
   const signin_metrics::AccessPoint access_point_;
+
+  // ID of the GAIA account that should be signed in when
+  // |signin_with_gaia_account_button_| is pressed.
+  std::string signin_with_gaia_account_id_;
 
   const int menu_width_;
 
