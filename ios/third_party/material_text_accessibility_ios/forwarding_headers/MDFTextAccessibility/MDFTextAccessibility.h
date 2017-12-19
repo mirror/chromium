1982d898094bf385308ccad55dea90d6618119ac
Louis Romero <lpromero@chromium.org>
2017-12-19 14:37:15 +0100

    Roll Material dependencies
    This CL rolls different material repositories to their latest releases.
    It also adds forwarding headers simulating a framework structure to be able to
    compile Material and internal sources together.
    
    Bug: 785248
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I72f7801aa698bda3acd4c8518a8644528a445245

---
+++ ios/third_party/material_text_accessibility_ios/forwarding_headers/MDFTextAccessibility/MDFTextAccessibility.h
@@ -0,0 +1,5 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/third_party/material_text_accessibility_ios/src/src/MDFTextAccessibility.h"
