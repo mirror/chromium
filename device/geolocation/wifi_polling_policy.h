4d7bcc1176bae4f7cf079808bbcdb344452b72af
Matt Reynolds <mattreynolds@google.com>
2017-12-19 13:38:58 -0800

    Move WifiPollingPolicy to a global instance
    The WifiPollingPolicy controls how frequently the network location
    provider will initiate Wi-Fi scans. Previously, the policy was attached
    to the WifiDataProvider, which is destroyed when there are no active
    geolocation API calls. Destruction of the provider causes us to lose track
    of our polling policy.
    
    The policy allows the first scan to be performed immediately and schedules
    subsequent scans at regular intervals. When the provider is destroyed and
    recreated rapidly it considers each scan to be "first", which allows many
    Wi-Fi scans to be performed in a short window.
    
    This change moves the policy state to a global instance so it can be saved
    when the provider is destroyed and recreated. The new policy allows the
    first-ever scan to be performed immediately, and subsequent scans may also
    be performed immediately if enough time has lapsed since the previous scan.
    In all other situations, the configured polling interval is enforced.
    
    BUG=764954
    
    Change-Id: I00649dde2a707127e53e0330511172fef4a43471

--- device/geolocation/wifi_polling_policy.h
+++ device/geolocation/wifi_polling_policy.h
@@ -6,19 +6,29 @@
 #define DEVICE_GEOLOCATION_WIFI_POLLING_POLICY_H_
 
 #include "base/macros.h"
+#include "base/time/time.h"
+#include "device/geolocation/geolocation_export.h"
 
 namespace device {
 
 // Allows sharing and mocking of the update polling policy function.
-class WifiPollingPolicy {
+class DEVICE_GEOLOCATION_EXPORT WifiPollingPolicy {
  public:
-  WifiPollingPolicy() {}
-  virtual ~WifiPollingPolicy() {}
+  virtual ~WifiPollingPolicy() = default;
+
+  static void Initialize(std::unique_ptr<WifiPollingPolicy>);
+  static void Shutdown();
+  static WifiPollingPolicy* Get();
+  static bool IsInitialized();
+
   // Calculates the new polling interval for wiFi scans, given the previous
   // interval and whether the last scan produced new results.
   virtual void UpdatePollingInterval(bool scan_results_differ) = 0;
   virtual int PollingInterval() = 0;
   virtual int NoWifiInterval() = 0;
+
+ protected:
+  WifiPollingPolicy() = default;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(WifiPollingPolicy);
@@ -33,6 +43,7 @@ template <int DEFAULT_INTERVAL,
 class GenericWifiPollingPolicy : public WifiPollingPolicy {
  public:
   GenericWifiPollingPolicy() : polling_interval_(DEFAULT_INTERVAL) {}
+
   // WifiPollingPolicy
   void UpdatePollingInterval(bool scan_results_differ) override {
     if (scan_results_differ) {
@@ -45,11 +56,39 @@ class GenericWifiPollingPolicy : public WifiPollingPolicy {
       polling_interval_ = TWO_NO_CHANGE_INTERVAL;
     }
   }
-  int PollingInterval() override { return polling_interval_; }
-  int NoWifiInterval() override { return NO_WIFI_INTERVAL; }
+
+  int PollingInterval() override { return ComputeInterval(polling_interval_); }
+
+  int NoWifiInterval() override { return ComputeInterval(NO_WIFI_INTERVAL); }
 
  private:
+  int ComputeInterval(int polling_interval) {
+    base::Time now = base::Time::Now();
+
+    // Scan immediately if this is our first scan.
+    if (interval_complete_time_.is_null()) {
+      interval_complete_time_ = now;
+      return 0;
+    }
+
+    // Scan immediately if the last scan was more than one interval ago.
+    base::TimeDelta interval_delta =
+        base::TimeDelta::FromMilliseconds(polling_interval);
+    base::Time complete_time = interval_complete_time_ + interval_delta;
+    if (complete_time < now) {
+      interval_complete_time_ = now;
+      return 0;
+    }
+
+    // Otherwise, schedule the next scan after the polling interval.
+    interval_complete_time_ = now + interval_delta;
+    return polling_interval;
+  }
+
   int polling_interval_;
+
+  // Record the expected time that the current interval will be complete.
+  base::Time interval_complete_time_;
 };
 
 }  // namespace device
