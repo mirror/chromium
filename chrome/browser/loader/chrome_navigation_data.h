1c5d79ae3d9c73f62a13e8ccd1c7425f08b840b4
arthursonzogni <arthursonzogni@chromium.org>
2017-12-19 13:18:53 +0100

    Remove content::NavigationData.
    content::NavigationData is a copyable interface for embedders to pass
    opaque data to content/.
    
    To make the NetworkService or the NavigationMojoResponse features work,
    we need to pass this object with mojo. This isn't possible
    since content/ doesn't know how to serialize it.
    
    This CL removes content::NavigationData. To mitigate its removal, a
    base::Value can be used instead.
    
    This CL implements the ChromeNavigationData <=> base::Value conversion.
    
    Bug: 705744
    Change-Id: I49e2bf617322200b0dfe1462da94880fc6651b8f

--- chrome/browser/loader/chrome_navigation_data.h
+++ chrome/browser/loader/chrome_navigation_data.h
@@ -11,24 +11,24 @@
 #include "base/supports_user_data.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_data.h"
 #include "components/previews/core/previews_user_data.h"
-#include "content/public/browser/navigation_data.h"
 #include "content/public/common/previews_state.h"
+
+namespace base {
+class Value;
+}
 
 namespace net {
 class URLRequest;
 }
 
-class ChromeNavigationData : public content::NavigationData,
-                             public base::SupportsUserData::Data {
+class ChromeNavigationData : public base::SupportsUserData::Data {
  public:
   ChromeNavigationData();
   ~ChromeNavigationData() override;
 
-  // Creates a new ChromeNavigationData that is a deep copy of the original. Any
-  // changes to the original after the clone is created will not be reflected in
-  // the clone.
-  // |data_reduction_proxy_data_| is deep copied.
-  std::unique_ptr<content::NavigationData> Clone() const override;
+  // Convert from/to a base::Value.
+  base::Value ToValue();
+  explicit ChromeNavigationData(const base::Value& value);
 
   // Takes ownership of |data_reduction_proxy_data|.
   void SetDataReductionProxyData(
