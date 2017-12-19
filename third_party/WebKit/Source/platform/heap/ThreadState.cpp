419f6c2e842e0c4c394d6fb0de1ee35632ec4465
Hannes Payer <hpayer@chromium.org>
2017-12-19 16:46:51 +0100

    [oilpan] Report total garbage collection time to UMA.
    Bug:
    Change-Id: Ia4a87a4fa6e98902bca8b3328399dcd4de79af3a

--- third_party/WebKit/Source/platform/heap/ThreadState.cpp
+++ third_party/WebKit/Source/platform/heap/ThreadState.cpp
@@ -1246,6 +1246,8 @@ void ThreadState::CollectGarbage(BlinkGC::StackState stack_state,
   if (SweepForbidden())
     return;
 
+  double total_collect_garbage_time = WTF::CurrentTimeTicksInMilliseconds();
+
   CompleteSweep();
 
   RUNTIME_CALL_TIMER_SCOPE_IF_ISOLATE_EXISTS(
@@ -1282,6 +1284,13 @@ void ThreadState::CollectGarbage(BlinkGC::StackState stack_state,
 #endif
 
   PreSweep(gc_type);
+
+  double total_collect_garbage_time =
+      WTF::CurrentTimeTicksInMilliseconds() - total_collect_garbage_start_time;
+  DEFINE_THREAD_SAFE_STATIC_LOCAL(
+      CustomCountHistogram, time_for_total_collect_garbage_histogram,
+      ("BlinkGC.TimeForCoalesce", 1, 10 * 1000, 50));
+  time_for_heap_coalesce_histogram.Count(total_collect_garbage_time);
 }
 
 void ThreadState::MarkPhasePrologue(BlinkGC::StackState stack_state,
