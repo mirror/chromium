bd895b8a4ad0be6f0b995ce6ff1625919baf696f
Zhenyao Mo <zmo@chromium.org>
2017-12-18 16:52:51 -0800

    Shift Telemetry and about:gpu to use GpuFeatureInfo.
    So we can retire the old data structure soon.
    
    Also, this CL makes Telemetry crash if Chrome doesn't send GPU data back in 5s.
    
    BUG=744658
    TEST=bots
    R=kbr@chromium.org,piman@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iae1ddb56c5681934b17f3539f070d0c1a15e9f80

--- content/test/gpu/gpu_tests/gpu_process_expectations.py
+++ content/test/gpu/gpu_tests/gpu_process_expectations.py
@@ -14,11 +14,7 @@ class GpuProcessExpectations(GpuTestExpectations):
     self.Fail('GpuProcess_video', ['linux'], bug=257109)
 
     # Chrome on Android doesn't support software fallback.
-    self.Skip('GpuProcess_no_gpu_process', ['android'], bug=643282)
     self.Skip('GpuProcess_skip_gpu_process', ['android'], bug=(610951, 610023))
-
-    # TODO(zmo): Consider deleting this test on Linux/Win due to SwiftShader?
-    self.Fail('GpuProcess_no_gpu_process', ['win', 'linux'])
 
     # Chrome on Windows and Linux create a GPU process that uses SwiftShader
     # when using either --disable-gpu or a blacklisted GPU.
