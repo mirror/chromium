aaad693a0e66ca1cf7038971b0fbc3e18f049ade
Aga Wronska <agawronska@chromium.org>
2017-12-19 11:06:32 -0800

    Refactor IsUsingWebUiLock to IsUsingViewsLock to match IsUsingViewsLogin.
    Bug:
    Change-Id: I38c2ce0bed6c7ef5545e0ccd7d18266d0c2fa97b

--- chrome/browser/ui/webui/chromeos/login/oobe_ui.cc
+++ chrome/browser/ui/webui/chromeos/login/oobe_ui.cc
@@ -542,7 +542,7 @@ void OobeUI::GetLocalizedStrings(base::DictionaryValue* localized_strings) {
                                ? "off"
                                : "on");
   localized_strings->SetString(
-      "showMdLogin", ash::switches::IsUsingWebUiLock() ? "off" : "on");
+      "showViewsLogin", ash::switches::IsUsingViewsLock() ? "on" : "off");
 }
 
 void OobeUI::AddWebUIHandler(std::unique_ptr<BaseWebUIHandler> handler) {
