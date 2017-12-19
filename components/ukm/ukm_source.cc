04917203544c237f0297238670d897a21bdcf8c3
Steven Holte <holte@google.com>
2017-12-19 15:44:44 -0800

    Don't keep multiple URLs for non-navigation IDs.
    Bug: 786190
    Change-Id: Ib158b7f08497008ceb50fb83b19e35f8b2f9be3b

--- components/ukm/ukm_source.cc
+++ components/ukm/ukm_source.cc
@@ -51,7 +51,8 @@ void UkmSource::UpdateUrl(const GURL& url) {
   DCHECK(!url.is_empty() && !url_.is_empty());
   if (url_ == url)
     return;
-  if (initial_url_.is_empty())
+  if (initial_url_.is_empty() &&
+      GetSourceIdType(id_) == SourceIdType::NAVIGATION_ID)
     initial_url_ = url_;
   url_ = url;
 }
@@ -63,8 +64,10 @@ void UkmSource::PopulateProto(Source* proto_source) const {
 
   proto_source->set_id(id_);
   proto_source->set_url(GetShortenedURL(url_));
-  if (!initial_url_.is_empty())
+  if (!initial_url_.is_empty()) {
+    DCHECK_EQ(SourceIdType::NAVIGATION_ID, GetSourceIdType(id_));
     proto_source->set_initial_url(GetShortenedURL(initial_url_));
+  }
 
   if (custom_tab_state_ != kCustomTabUnset)
     proto_source->set_is_custom_tab(custom_tab_state_ == kCustomTabTrue);
