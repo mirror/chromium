44f297ea890f4083fd75800f8f88202424be7888
Xiaohan Wang <xhwang@chromium.org>
2017-12-18 16:44:02 -0800

    media: Split CdmService from MediaService
    Current CdmService is just an alias of MediaService because the latter
    can also support hosting a CDM. This help prevent duplication of code.
    However, there are a few issues with this approach:
    1. It's makes the code harder to understand.
    2. CdmService doesn't need all the functionality provided by the
    MediaService.
    3. CdmService needs some customization (e.g. LoadCdm) which introduces
    some complexity to MediaService.
    
    This CL split CdmService from MediaService so that we can do some clean
    up. More simplification will follow in future CLs.
    
    BUG=771791
    TEST=No functionality change.
    
    Change-Id: I0ee627f2d01a77b33610956482197fbdc5425dcd

--- media/mojo/services/media_service.cc
+++ media/mojo/services/media_service.cc
@@ -14,13 +14,6 @@
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "services/service_manager/public/cpp/connector.h"
 
-#if BUILDFLAG(ENABLE_LIBRARY_CDMS)
-#include "media/cdm/cdm_module.h"
-#if defined(OS_MACOSX)
-#include "sandbox/mac/seatbelt_extension.h"
-#endif  // defined(OS_MACOSX)
-#endif  // BUILDFLAG(ENABLE_LIBRARY_CDMS)
-
 namespace media {
 
 MediaService::MediaService(std::unique_ptr<MojoMediaClient> mojo_media_client)
@@ -58,66 +51,6 @@ bool MediaService::OnServiceManagerConnectionLost() {
 
 void MediaService::Create(mojom::MediaServiceRequest request) {
   bindings_.AddBinding(this, std::move(request));
-}
-
-#if defined(OS_MACOSX)
-void MediaService::LoadCdm(
-    const base::FilePath& cdm_path,
-    mojom::SeatbeltExtensionTokenProviderPtr token_provider) {
-#else
-void MediaService::LoadCdm(const base::FilePath& cdm_path) {
-#endif  // defined(OS_MACOSX)
-  DVLOG(1) << __func__ << ": cdm_path = " << cdm_path.value();
-
-  // Ignore request if service has already stopped.
-  if (!mojo_media_client_)
-    return;
-
-#if BUILDFLAG(ENABLE_LIBRARY_CDMS)
-  CdmModule* instance = CdmModule::GetInstance();
-  if (instance->was_initialize_called()) {
-    DCHECK_EQ(cdm_path, instance->GetCdmPath());
-    return;
-  }
-
-#if defined(OS_MACOSX)
-  std::vector<sandbox::SeatbeltExtensionToken> tokens;
-  CHECK(token_provider->GetTokens(&tokens));
-
-  std::vector<std::unique_ptr<sandbox::SeatbeltExtension>> extensions;
-
-  for (auto&& token : tokens) {
-    DVLOG(3) << "token: " << token.token();
-    auto extension = sandbox::SeatbeltExtension::FromToken(std::move(token));
-    if (!extension->Consume()) {
-      DVLOG(1) << "Failed to comsume sandbox seatbelt extension. This could "
-                  "happen if --no-sandbox is specified.";
-    }
-    extensions.push_back(std::move(extension));
-  }
-#endif  // defined(OS_MACOSX)
-
-#if BUILDFLAG(ENABLE_CDM_HOST_VERIFICATION)
-  std::vector<CdmHostFilePath> cdm_host_file_paths;
-  mojo_media_client_->AddCdmHostFilePaths(&cdm_host_file_paths);
-  if (!instance->Initialize(cdm_path, cdm_host_file_paths))
-    return;
-#else
-  if (!instance->Initialize(cdm_path))
-    return;
-#endif  // BUILDFLAG(ENABLE_CDM_HOST_VERIFICATION)
-
-  // This may trigger the sandbox to be sealed.
-  mojo_media_client_->EnsureSandboxed();
-
-#if defined(OS_MACOSX)
-  for (auto&& extension : extensions)
-    extension->Revoke();
-#endif  // defined(OS_MACOSX)
-
-  // Always called within the sandbox.
-  instance->InitializeCdmModule();
-#endif  // BUILDFLAG(ENABLE_LIBRARY_CDMS)
 }
 
 void MediaService::CreateInterfaceFactory(
