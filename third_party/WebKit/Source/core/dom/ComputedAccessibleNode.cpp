fe250eecac28974d767e7960ca62c9d6b039756d
Meredith Lane <meredithl@google.com>
2017-12-19 12:55:43 +1100

    Implement ComputedAccessibleNode.
    Add basic implementation of ComputedAccessibleNode, including IDL and
    layout tests. This CL simply exposes a the getComputedAccessibleNode
    method to the window, and a bare sketch of ComputedAccessibleNode
    containing a name and role. A promise is returned, which exposes the
    ComputedAccessibleNode once resolved.
    
    BUG=792783
    
    Change-Id: Ibc4076d86daa490383033bddd33a6f34684b209e

---
+++ third_party/WebKit/Source/core/dom/ComputedAccessibleNode.cpp
@@ -0,0 +1,48 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/dom/ComputedAccessibleNode.h"
+#include "bindings/core/v8/ScriptPromise.h"
+#include "core/dom/Element.h"
+#include "platform/bindings/ScriptState.h"
+
+namespace blink {
+
+ComputedAccessibleNode* ComputedAccessibleNode::Create(Element* elt) {
+  return new ComputedAccessibleNode(elt);
+}
+
+ComputedAccessibleNode::ComputedAccessibleNode(Element* elt) : element_(elt) {
+  DCHECK(RuntimeEnabledFeatures::AccessibilityObjectModelEnabled());
+}
+
+ComputedAccessibleNode::~ComputedAccessibleNode() {}
+
+ScriptPromise ComputedAccessibleNode::ComputePromiseProperty(
+    ScriptState* script_state) {
+  if (!computed_property_) {
+    computed_property_ =
+        new ComputedPromiseProperty(ExecutionContext::From(script_state), this,
+                                    ComputedPromiseProperty::kReady);
+    computed_property_->Resolve(this);
+  }
+
+  return computed_property_->Promise(script_state->World());
+}
+
+const AtomicString& ComputedAccessibleNode::role() const {
+  return element_->computedRole();
+}
+
+const String ComputedAccessibleNode::name() const {
+  return element_->computedName();
+}
+
+void ComputedAccessibleNode::Trace(blink::Visitor* visitor) {
+  ScriptWrappable::Trace(visitor);
+  visitor->Trace(computed_property_);
+  visitor->Trace(element_);
+}
+
+}  // namespace blink
