3ed3ed765583b7f93ff1bf8f859b79e0ca5daa71
Takashi Toyoshima <toyoshim@chromium.org>
2017-12-19 15:33:48 +0900

    MidiManager: remove dynamic instantiation study
    Now that the mode is enabled on all platforms, let's remove
    the study flag, and enable it always on production code.
    
    Unit tests still use the legacy mode in some cases, but it
    should be modified in follow-up changes soon.
    
    Bug: 672793
    Change-Id: I99cf9d15e9cf8c3305d5bf73f2e7350159f45e56
    Tbr: mpearson@chromium.org

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1848,10 +1848,10 @@ const char kEnableExpandedAutofillCreditCardPopupLayoutName[] =
 const char kEnableExpandedAutofillCreditCardPopupLayoutDescription[] =
     "If enabled, displays autofill credit card popup using expanded layout.";
 
-const char kEnableMidiManagerDynamicInstantiationName[] =
-    "MIDIManager dynamic instantiation for Web MIDI.";
-const char kEnableMidiManagerDynamicInstantiationDescription[] =
-    "Enable MIDIManager dynamic instantiation for Web MIDI.";
+const char kEnableFaviconsFromWebManifestName[] =
+    "Load favicons from Web Manifests";
+const char kEnableFaviconsFromWebManifestDescription[] =
+    "Fetch Web Manifests on page load to read favicons from them.";
 
 const char kEnableNtpAssetDownloadSuggestionsName[] =
     "Show asset downloads on the New Tab page";
