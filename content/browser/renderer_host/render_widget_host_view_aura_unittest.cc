dc6c92091aa8d5b1abbe6502d54baeca7ec97e93
Saman Sami <samans@chromium.org>
2017-12-20 18:33:22 -0500

    Surface synchronization: Allocate new id when ScreenInfo changes
    RenderWidgetHostViewAura::UpdateScreenInfo only calls
    window_->AllocateLocalSurfaceId() in auto-resize mode. We want
    ScreenInfo to be synchronized across clients independent of whether
    auto-resize is on or not. This CL fixes crbug.com/791154, in
    which the renderer crashes because it doesn't receive a new
    LocalSurfaceId when device scale factor changes.
    
    Bug:672962,791154
    Change-Id: If9c3836aee980a31e8d50159f6491b52bf812a6d

--- content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
+++ content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
@@ -2319,9 +2319,9 @@ TEST_F(RenderWidgetHostViewAuraTest, AutoResizeWithScale) {
   sink_->ClearMessages();
   aura_test_helper_->test_screen()->SetDeviceScaleFactor(2.0f);
 
-  EXPECT_EQ(2u, sink_->message_count());
+  EXPECT_EQ(1u, sink_->message_count());
   {
-    const IPC::Message* msg = sink_->GetMessageAt(1);
+    const IPC::Message* msg = sink_->GetMessageAt(0);
     EXPECT_EQ(static_cast<uint32_t>(ViewMsg_SetLocalSurfaceIdForAutoResize::ID),
               msg->type());
     ViewMsg_SetLocalSurfaceIdForAutoResize::Param params;
@@ -3078,6 +3078,7 @@ TEST_F(RenderWidgetHostViewAuraSurfaceSynchronizationTest,
       view_->GetNativeView(), parent_view_->GetNativeView()->GetRootWindow(),
       gfx::Rect());
 
+  view_->window_->layer()->SetShowSolidColorContent();
   EXPECT_FALSE(view_->HasPrimarySurface());
   ASSERT_TRUE(view_->delegated_frame_host_);
 
@@ -3104,7 +3105,6 @@ TEST_F(RenderWidgetHostViewAuraSurfaceSynchronizationTest, SurfaceChanges) {
 
   // Prevent the DelegatedFrameHost from skipping frames.
   view_->DisableResizeLock();
-  EXPECT_FALSE(view_->HasPrimarySurface());
   ASSERT_TRUE(view_->delegated_frame_host_);
 
   view_->SetSize(gfx::Size(300, 300));
@@ -3140,7 +3140,6 @@ TEST_F(RenderWidgetHostViewAuraSurfaceSynchronizationTest,
 
   // Prevent the DelegatedFrameHost from skipping frames.
   view_->DisableResizeLock();
-  EXPECT_FALSE(view_->HasPrimarySurface());
 
   view_->SetSize(gfx::Size(300, 300));
   ASSERT_TRUE(view_->HasPrimarySurface());
