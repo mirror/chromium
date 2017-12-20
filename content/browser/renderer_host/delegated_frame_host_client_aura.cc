eb823237d0a10006d3b0f3b42c59c8bbcb79d1ae
Christopher Cameron <ccameron@chromium.org>
2017-12-20 11:48:50 -0800

    viz/mac: Fix some resize race conditions
    When a renderer produces a frame for a local surface id, the pixel size
    and scale factor must always be the same for that id.
    
    The RenderWidgetHostImpl in the browser tells the renderer what these
    three parameters should be in RenderWidgetHostImpl::GetResizeParams.
    The three parameters come from three different function calls. In
    particular, we query:
    - the local surface id from RWHView::GetLocalSurfaceId
    - the DIP size from RWHView::GetRequestedRendererSize
    - the scale factor from GetScreenInfo (over in WebContentsView)
    - the pixel size (too) just to add more opportunities for inconsistency
    These parameters will only be updated atomically by the wildest of
    coincidences.
    
    Start in the direction of fixing this problem by making the function
    RWHView::GetRequestedRendererSize return both the DIP size and the
    device scale factor together, and add a comment to
    DelegatedFrameHostClientAura where there are likely to be problems
    here (there were on macOS).
    
    Delete some device scale factor update code in RWHVMac, since it
    wasn't having the intended effects.
    
    The next patch in this sequence will
    - Add local surface id to RWHView::GetRequestedRendererSize, and
      rename it to GetRequestedRendererParams.
    - Do something about the fact that the function GetPhysicalBackingSize
      is another source of potential inconsistency.
    
    Bug: 772576
    Change-Id: I3d07adc3e5e66bc74b688d3fe480b55cc2f23c87

--- content/browser/renderer_host/delegated_frame_host_client_aura.cc
+++ content/browser/renderer_host/delegated_frame_host_client_aura.cc
@@ -10,6 +10,7 @@
 #include "ui/aura/window.h"
 #include "ui/aura/window_event_dispatcher.h"
 #include "ui/aura/window_tree_host.h"
+#include "ui/base/layout.h"
 #include "ui/compositor/layer.h"
 
 namespace content {
@@ -41,9 +42,15 @@ SkColor DelegatedFrameHostClientAura::DelegatedFrameHostGetGutterColor(
   return color;
 }
 
-gfx::Size DelegatedFrameHostClientAura::DelegatedFrameHostDesiredSizeInDIP()
-    const {
-  return render_widget_host_view_->window_->bounds().size();
+void DelegatedFrameHostClientAura::DelegatedFrameHostDesiredSize(
+    gfx::Size* size_in_dip,
+    float* scale_factor) const {
+  *size_in_dip = render_widget_host_view_->window_->bounds().size();
+  // This value is likely to race with allocation of local surface ids.
+  // TODO(ccameron): Ensure that this be updated atomically with the value
+  // returned by GetLocalSurfaceId.
+  *scale_factor =
+      ui::GetScaleFactorForNativeView(render_widget_host_view_->window_);
 }
 
 bool DelegatedFrameHostClientAura::DelegatedFrameCanCreateResizeLock() const {
--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -1087,14 +1087,14 @@ void RenderWidgetHostViewMac::SetTooltipText(
 }
 
 void RenderWidgetHostViewMac::UpdateScreenInfo(gfx::NativeView view) {
-  RenderWidgetHostViewBase::UpdateScreenInfo(view);
-
-  if (!render_widget_host_ || !render_widget_host_->auto_resize_enabled())
-    return;
-
-  render_widget_host_->DidAllocateLocalSurfaceIdForAutoResize(
-      render_widget_host_->last_auto_resize_request_number());
   browser_compositor_->WasResized();
+  RenderWidgetHostViewBase::UpdateScreenInfo(view);
+}
+
+void RenderWidgetHostViewMac::GetRequestedRendererSize(
+    gfx::Size* dip_size,
+    float* scale_factor) const {
+  browser_compositor_->DelegatedFrameHostDesiredSize(dip_size, scale_factor);
 }
 
 bool RenderWidgetHostViewMac::SupportsSpeech() const {
@@ -1782,19 +1782,6 @@ void RenderWidgetHostViewMac::OnDisplayMetricsChanged(
   display::Screen* screen = display::Screen::GetScreen();
   if (display.id() != screen->GetDisplayNearestView(cocoa_view_).id())
     return;
-
-  if (changed_metrics & DisplayObserver::DISPLAY_METRIC_DEVICE_SCALE_FACTOR) {
-    RenderWidgetHostImpl* host =
-        RenderWidgetHostImpl::From(GetRenderWidgetHost());
-    if (host) {
-      if (host->auto_resize_enabled()) {
-        host->DidAllocateLocalSurfaceIdForAutoResize(
-            host->last_auto_resize_request_number());
-      }
-      host->WasResized();
-    }
-  }
-
   UpdateBackingStoreProperties();
 }
 
