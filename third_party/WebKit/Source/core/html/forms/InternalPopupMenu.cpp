0d0d3f0b6f569bc7098ee82c4e33cd302274af0d
Bruce Dawson <brucedawson@chromium.org>
2017-12-20 14:07:07 -0800

    Avoid using PostMessage to avoid Windows SDK conflicts
    The Windows SDK insists on defining PostMessage to PostMessageW. Working
    with this requires a delicate and fragile dance that was recently broken
    on jumbo builds with some settings.
    
    While the long-term fix is to reduce how frequently we include Windows.h
    the pragmatic short-term fix is to rename the virtual function.
    
    Bug: 746953, 796644
    Change-Id: I9c8a46d34fa19b35ca065cc27cae0b40b936510a

--- third_party/WebKit/Source/core/html/forms/InternalPopupMenu.cpp
+++ third_party/WebKit/Source/core/html/forms/InternalPopupMenu.cpp
@@ -550,7 +550,7 @@ void InternalPopupMenu::Update() {
       OwnerElement().GetDocument().View());
   AddProperty("anchorRectInScreen", anchor_rect_in_screen, data.get());
   PagePopupClient::AddString("}\n", data.get());
-  popup_->PostMessage(String::FromUTF8(data->Data(), data->size()));
+  popup_->PostJavaScriptMessage(String::FromUTF8(data->Data(), data->size()));
 }
 
 void InternalPopupMenu::DisconnectClient() {
