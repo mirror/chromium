2361380a9e0eb07c465f25b71028066c84890092
Charlie Andrews <charliea@chromium.org>
2017-12-20 12:02:28 -0500

    Add expectations file for javaperftests
    This file is the new (hopefully easier) way to disable Telemetry
    benchmarks. To disable a benchmark, add a line that looks like:
    
    crbug.com/123456 [ MAC] run.CronetPerfTestBenchmark [ Skip ]
    
    For more detailed instructions, see http://bit.ly/2BNw4Ca
    
    This CL is part of a broader effort to eventually remove the
    GetExpectations method altogether to simplify the Telemetry API.
    
    Bug: 796340
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: I32c6d55ff9ce890cd9ab0e9ec1101e882166a695

--- components/cronet/android/test/javaperftests/run.py
+++ components/cronet/android/test/javaperftests/run.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 # Copyright 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
@@ -221,12 +222,6 @@ class CronetPerfTestBenchmark(benchmark.Benchmark):
   def CreateStorySet(self, options):
     return CronetPerfTestStorySet(self._device)
 
-  def GetExpectations(self):
-    class StoryExpectations(story.expectations.StoryExpectations):
-      def SetExpectations(self):
-        pass # Nothing disabled.
-    return StoryExpectations()
-
 
 class QuicServer(object):
 
@@ -343,13 +338,15 @@ def main():
   # Chromium checkout in Telemetry.
   perf_config_file = os.path.join(REPOSITORY_ROOT, 'tools', 'perf', 'core',
       'binary_dependencies.json')
+  expectations_file = os.path.join(top_level_dir, 'expectations.config')
   with open(perf_config_file, "w") as config_file:
     config_file.write('{"config_type": "BaseConfig"}')
   runner_config = project_config.ProjectConfig(
       top_level_dir=top_level_dir,
       benchmark_dirs=[top_level_dir],
       client_configs=[perf_config_file],
-      default_chrome_root=REPOSITORY_ROOT)
+      default_chrome_root=REPOSITORY_ROOT,
+      expectations_file=expectations_file)
   sys.argv.insert(1, 'run')
   sys.argv.insert(2, 'run.CronetPerfTestBenchmark')
   benchmark_runner.main(runner_config)
