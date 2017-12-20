ef66a660819c66e66db9b811053a2708e47fe608
jdoerrie <jdoerrie@chromium.org>
2017-12-20 18:48:37 +0100

    Update value_cleanup tool for base::ListValue rewrites
    This change updates the value_cleanup tool to be able to rewrite the
    base::ListValue APIs
    
    - ListValue::Clear()
    - ListValue::GetSize()
    - ListValue::empty()
    - ListValue::Reserve()
    - ListValue::AppendBoolean()
    - ListValue::AppendInteger()
    - ListValue::AppendDouble()
    - ListValue::AppendString()
    
    to their std::vector<base::Value> equivalent.
    
    Bug: 646113
    Change-Id: I9773fced17a5529d8d383ee88f81d4699c5a4d16

--- tools/clang/value_cleanup/ValueRewriter.cpp
+++ tools/clang/value_cleanup/ValueRewriter.cpp
@@ -6,29 +6,36 @@
 
 using namespace clang::ast_matchers;
 
-ValueRewriter::GetTypeCallback::GetTypeCallback(
+ValueRewriter::ListValueCallback::ListValueCallback(
     std::set<clang::tooling::Replacement>* replacements)
     : replacements_(replacements) {}
 
-// Replaces calls to |base::Value::GetType| with calls to |base::Value::type|.
-void ValueRewriter::GetTypeCallback::run(
+void ValueRewriter::ListValueCallback::run(
     const MatchFinder::MatchResult& result) {
-  // Replace 'GetType' with 'type'.
-  auto* callExpr = result.Nodes.getNodeAs<clang::CXXMemberCallExpr>("callExpr");
+  for (const auto& method_replacement : kMethodReplacements) {
+    const auto& method = method_replacement.first;
+    const auto& replacement = method_replacement.second;
+    auto* callExpr = result.Nodes.getNodeAs<clang::CXXMemberCallExpr>(method);
+    if (!callExpr)
+      continue;
 
-  clang::CharSourceRange call_range =
-      clang::CharSourceRange::getTokenRange(callExpr->getExprLoc());
-  replacements_->emplace(*result.SourceManager, call_range, "type");
+    clang::CharSourceRange call_range =
+        clang::CharSourceRange::getTokenRange(callExpr->getExprLoc());
+    replacements_->emplace(*result.SourceManager, call_range, replacement);
+  }
 }
 
 ValueRewriter::ValueRewriter(
     std::set<clang::tooling::Replacement>* replacements)
-    : get_type_callback_(replacements) {}
+    : list_value_callback_(replacements) {}
 
 void ValueRewriter::RegisterMatchers(MatchFinder* match_finder) {
-  match_finder->addMatcher(
-      id("callExpr", cxxMemberCallExpr(callee(cxxMethodDecl(
-                                           hasName("::base::Value::GetType"))),
-                                       argumentCountIs(0))),
-      &get_type_callback_);
+  for (const auto& replacement : list_value_callback_.kMethodReplacements) {
+    const auto& method = replacement.first;
+    match_finder->addMatcher(
+        callExpr(callee(functionDecl(
+                     hasName(list_value_callback_.kPrefix + method))))
+            .bind(method),
+        &list_value_callback_);
+  }
 }
--- tools/clang/value_cleanup/ValueRewriter.h
+++ tools/clang/value_cleanup/ValueRewriter.h
@@ -8,6 +8,9 @@
 #define TOOLS_CLANG_VALUE_CLEANUP_VALUE_REWRITER_H_
 
 #include <set>
+#include <string>
+#include <utility>
+#include <vector>
 
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/Tooling/Refactoring.h"
@@ -19,20 +22,33 @@ class ValueRewriter {
   void RegisterMatchers(clang::ast_matchers::MatchFinder* match_finder);
 
  private:
-  class GetTypeCallback
+  class ListValueCallback
       : public clang::ast_matchers::MatchFinder::MatchCallback {
    public:
-    explicit GetTypeCallback(
+    explicit ListValueCallback(
         std::set<clang::tooling::Replacement>* replacements);
 
     void run(
         const clang::ast_matchers::MatchFinder::MatchResult& result) override;
 
+    const std::string kPrefix = "::base::ListValue::";
+    const std::vector<std::pair<std::string, std::string>> kMethodReplacements =
+        {
+            {"Clear", "GetList().clear"},
+            {"GetSize", "GetList().size"},
+            {"empty", "GetList().empty"},
+            {"Reserve", "GetList().reserve"},
+            {"AppendBoolean", "GetList().emplace_back"},
+            {"AppendInteger", "GetList().emplace_back"},
+            {"AppendDouble", "GetList().emplace_back"},
+            {"AppendString", "GetList().emplace_back"},
+        };
+
    private:
     std::set<clang::tooling::Replacement>* const replacements_;
   };
 
-  GetTypeCallback get_type_callback_;
+  ListValueCallback list_value_callback_;
 };
 
 #endif  // TOOLS_CLANG_VALUE_CLEANUP_VALUE_REWRITER_H_
