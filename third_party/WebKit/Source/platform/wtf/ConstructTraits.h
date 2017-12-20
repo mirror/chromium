8bd1d520bbe34fefb2221d2cf463e0d3ed5c868a
Michael Lippautz <mlippautz@chromium.org>
2017-12-20 12:51:54 +0100

    [oilpan] HeapVector support for incremental marking
    The optimization in
      faf0c6646d65d165018b9af7331033b4c358e7ba
    was unsound for containers as they can construct elements in place, even
    after the containing object was already marked black.
    
    This patch keeps the ability of doing initializing stores without write
    barrier while adding the barrier for HeapVector operations.
    
    Bug: chromium:757440
    Change-Id: I8273edf45863daa7516580a2926883a69b8d8024

---
+++ third_party/WebKit/Source/platform/wtf/ConstructTraits.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WTF_ConstructTraits_h
+#define WTF_ConstructTraits_h
+
+#include "platform/wtf/Allocator.h"
+#include "platform/wtf/TypeTraits.h"
+#include "platform/wtf/VectorTraits.h"
+
+namespace WTF {
+
+// ConstructTraits is used to construct elements in WTF collections. Instead of
+// constructing elements using placement new, they should be constructed using
+// ConstructTraits.
+template <typename T, typename Allocator>
+class ConstructTraits {
+  STATIC_ONLY(ConstructTraits);
+
+ public:
+  template <typename... Args>
+  static T* ConstructElement(void* location, Args&&... args) {
+    T* object = new (NotNull, location) T(std::forward<Args>(args)...);
+    Allocator::template NotifyNewObject<T, VectorTraits<T>>(object);
+    return object;
+  }
+
+  static void ConstructElements(T* array, size_t len) {
+    Allocator::template NotifyNewObjects<T, VectorTraits<T>>(array, len);
+  }
+};
+
+}  // namespace WTF
+
+#endif  // WTF_ConstructTraits_h
