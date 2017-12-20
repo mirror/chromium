d8dfcef1c9ffdaa93a5cc9bde191b98c84474a95
Joy Ming <jming@chromium.org>
2017-12-20 07:42:03 -0800

    [Downloads location] Implement dummy storage directory selector.
    This is part of a series of changes to implement the functionality to
    change default download location on Android. This CL creates a simple
    toggle between internal and external storage locations as a temporary
    step while the layout of the final directory selector is being decided.
    
    Bug:792775
    Change-Id: I3a5b11c84c3e4dcd79ab941351fdd20c310d569d

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/DownloadPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/DownloadPreferences.java
@@ -4,56 +4,107 @@
 
 package org.chromium.chrome.browser.preferences;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 
 import org.chromium.chrome.R;
 
+import java.io.File;
+
 /**
  * Fragment to keep track of all downloads related preferences.
  */
-public class DownloadPreferences extends PreferenceFragment {
+public class DownloadPreferences
+        extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
     private static final String PREF_LOCATION_CHANGE = "location_change";
-    private static final int REQUEST_CODE_LOCATION_CHANGE = 1;
+
+    private PrefServiceBridge mPrefServiceBridge;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mPrefServiceBridge = PrefServiceBridge.getInstance();
 
         getActivity().setTitle(R.string.menu_downloads);
         PreferenceUtils.addPreferencesFromResource(this, R.xml.download_preferences);
 
+        updateSummaries();
+        getLocationOptions();
+    }
+
+    private void updateSummaries() {
         Preference locationChangePref = (Preference) findPreference(PREF_LOCATION_CHANGE);
-        locationChangePref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(Preference preference) {
-                // TODO(jming): Implement actual directory selector.
-                return false;
-            }
-        });
+        if (locationChangePref != null) {
+            locationChangePref.setSummary(mPrefServiceBridge.getDownloadDefaultDirectory());
+        }
+    }
+
+    private void getLocationOptions() {
+        // Currently assuming that there are only two possible options: internal and external.
+        ListPreference locationsPreference = (ListPreference) findPreference(PREF_LOCATION_CHANGE);
+
+        int numLocations = hasExternalStorage() ? 2 : 1;
+        CharSequence[] keys = new String[numLocations];
+        CharSequence[] descriptions = new String[numLocations];
+
+        keys[0] = "internal";
+        descriptions[0] = "Internal";
+        if (hasExternalStorage()) {
+            keys[1] = "external";
+            descriptions[1] = "External";
+        }
+
+        locationsPreference.setEntryValues(keys);
+        locationsPreference.setEntries(descriptions);
+        int index = isExternalStorageDefault() ? 1 : 0;
+        locationsPreference.setValueIndex(index);
+        locationsPreference.setOnPreferenceChangeListener(this);
+    }
+
+    private boolean hasExternalStorage() {
+        // TODO(jming): Confirm this works on the other end for phones with SD cards.
+        return Environment.isExternalStorageRemovable() && !Environment.isExternalStorageEmulated();
+    }
+
+    private boolean isExternalStorageDefault() {
+        return getExternalDownloadDirectory().equals(
+                new File(mPrefServiceBridge.getDownloadDefaultDirectory()));
+    }
+
+    // Preference.OnPreferenceChangeListener:
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (PREF_LOCATION_CHANGE.equals(preference.getKey())) {
+            updateStorageLocation((String) newValue);
+        }
+        return true;
+    }
+
+    private void updateStorageLocation(String newValue) {
+        File directory = newValue.equals("external") ? getExternalDownloadDirectory()
+                                                     : getInternalDownloadDirectory();
+        mPrefServiceBridge.setDownloadDefaultDirectory(directory.getAbsolutePath());
 
         updateSummaries();
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE_LOCATION_CHANGE && resultCode == Activity.RESULT_OK) {
-            if (data == null || data.getData() == null) return;
-            // TODO(jming): Update download default directory in preferences.
-            updateSummaries();
-        }
+    /**
+     * @return The default Android download directory.
+     */
+    private File getInternalDownloadDirectory() {
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
     }
 
-    public void updateSummaries() {
-        PrefServiceBridge prefServiceBridge = PrefServiceBridge.getInstance();
-
-        Preference locationChangePref = (Preference) findPreference(PREF_LOCATION_CHANGE);
-        if (locationChangePref != null) {
-            locationChangePref.setSummary(prefServiceBridge.getDownloadDefaultDirectory());
-        }
+    /**
+     * @return The SD card.
+     */
+    private File getExternalDownloadDirectory() {
+        // TODO(jming): Should we create a new downloads directory or just save directly on SD card?
+        return Environment.getExternalStorageDirectory();
     }
 }
