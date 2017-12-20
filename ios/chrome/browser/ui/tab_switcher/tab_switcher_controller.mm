2ec3e6c9b83034f311f7a7247f4af131e91e5e16
Jérôme Lebel <jlebel@chromium.org>
2017-12-20 13:53:46 +0100

    [iOS][Signin] Cleanup in TabSwitcherPanelOverlayView
    This patch is related to crrev.com/c/832391.
    The goal is to remove the knowledge of SigninPromoViewMediator from
    TabSwitcherPanelOverlayView.
    
    Bug: 795895, 725925
    Change-Id: I4ef0ee97d0f4adf91c40f5676ad53b2e53700fe5

--- ios/chrome/browser/ui/tab_switcher/tab_switcher_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_switcher_controller.mm
@@ -24,6 +24,7 @@
 #include "ios/chrome/browser/sync/ios_chrome_profile_sync_service_factory.h"
 #import "ios/chrome/browser/tabs/tab.h"
 #import "ios/chrome/browser/tabs/tab_model.h"
+#import "ios/chrome/browser/ui/authentication/signin_promo_view_configurator.h"
 #import "ios/chrome/browser/ui/authentication/signin_promo_view_consumer.h"
 #import "ios/chrome/browser/ui/authentication/signin_promo_view_mediator.h"
 #include "ios/chrome/browser/ui/commands/application_commands.h"
@@ -1048,24 +1049,33 @@ enum class SnapshotViewOption {
   _signInPanelType = panelType;
   DCHECK_EQ(nil, _signInPanelOverlayView);
   if (panelType != TabSwitcherSignInPanelsType::NO_PANEL) {
-    TabSwitcherPanelOverlayView* panelView =
-        [[TabSwitcherPanelOverlayView alloc]
-            initWithFrame:CGRectZero
-             browserState:_browserState
-                presenter:self /* id<SigninPresenter, SyncPresenter> */
-               dispatcher:self.dispatcher];
-    _signInPanelOverlayView = panelView;
+    _signInPanelOverlayView = [[TabSwitcherPanelOverlayView alloc]
+        initWithFrame:CGRectZero
+         browserState:_browserState
+            presenter:self /* id<SigninPresenter, SyncPresenter> */
+           dispatcher:self.dispatcher];
+    [_signInPanelOverlayView
+        setOverlayType:PanelOverlayTypeFromSignInPanelsType(panelType)];
     if (panelType == TabSwitcherSignInPanelsType::PANEL_USER_SIGNED_OUT) {
+      DCHECK_NE(nil, _signInPanelOverlayView.signinPromoView);
       _signinPromoViewMediator = [[SigninPromoViewMediator alloc]
           initWithBrowserState:_browserState
                    accessPoint:signin_metrics::AccessPoint::
                                    ACCESS_POINT_TAB_SWITCHER
                      presenter:self];
       _signinPromoViewMediator.consumer = self;
-      panelView.signinPromoViewMediator = _signinPromoViewMediator;
+      [[_signinPromoViewMediator createConfigurator]
+          configureSigninPromoView:_signInPanelOverlayView.signinPromoView];
+      __weak SigninPromoViewMediator* weakMediator = _signinPromoViewMediator;
+      _signInPanelOverlayView.wasShownCallback = ^() {
+        [weakMediator signinPromoViewVisible];
+      };
+      _signInPanelOverlayView.wasHiddenCallback = ^() {
+        [weakMediator signinPromoViewHidden];
+      };
     }
-    [panelView setOverlayType:PanelOverlayTypeFromSignInPanelsType(panelType)];
-    [_tabSwitcherView addPanelView:panelView atIndex:kSignInPromoPanelIndex];
+    [_tabSwitcherView addPanelView:_signInPanelOverlayView
+                           atIndex:kSignInPromoPanelIndex];
   }
 }
 
@@ -1307,10 +1317,10 @@ enum class SnapshotViewOption {
 - (void)configureSigninPromoWithConfigurator:
             (SigninPromoViewConfigurator*)configurator
                              identityChanged:(BOOL)identityChanged {
-  DCHECK(nil != _signInPanelOverlayView);
-  [_signInPanelOverlayView
-      configureSigninPromoWithConfigurator:configurator
-                           identityChanged:identityChanged];
+  DCHECK_NE(nil, _signInPanelOverlayView);
+  DCHECK_NE(nil, _signInPanelOverlayView.signinPromoView);
+  [configurator
+      configureSigninPromoView:_signInPanelOverlayView.signinPromoView];
 }
 
 - (void)signinDidFinish {
