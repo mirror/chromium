5e509c3bf0a5f8da929b984b3bfb5627e56b5648
Chong Zhang <chongz@chromium.org>
2017-12-21 15:47:40 -0800

    Add reconnect logic to URLLoaderFactoryGetter
    This CL:
    1. Stores |WeakPtr<StoragePartitionImpl>| in |URLLoaderFactoryGetter|,
       and re-create |network_factory_| on connection error.
    2. Adds basic 'content_browsertests' against |URLLoaderFactoryGetter|
       and |NavigationURLLoader|.
    
    Note:
    Other |URLLoaderFactoryGetter| consumers such as Service Worker and APP
    Cache should be able to survive the crash as well. Will add more tests
    in follow up CLs.
    
    Bug: 780956
    Change-Id: I65678ece5c852b9454d9c800ff80e4d1cc2de178

--- content/browser/storage_partition_impl.cc
+++ content/browser/storage_partition_impl.cc
@@ -456,6 +456,8 @@ StoragePartitionImpl::StoragePartitionImpl(
 StoragePartitionImpl::~StoragePartitionImpl() {
   browser_context_ = nullptr;
 
+  url_loader_factory_getter_->ClearCachedStoragePartitionPointer();
+
   if (GetDatabaseTracker()) {
     GetDatabaseTracker()->task_runner()->PostTask(
         FROM_HERE, base::BindOnce(&storage::DatabaseTracker::Shutdown,
@@ -1064,6 +1066,8 @@ void StoragePartitionImpl::FlushNetworkInterfaceForTesting() {
   network_context_.FlushForTesting();
   if (url_loader_factory_for_browser_process_)
     url_loader_factory_for_browser_process_.FlushForTesting();
+  if (url_loader_factory_getter_)
+    url_loader_factory_getter_->FlushNetworkInterfaceOnIOThreadForTesting();
 }
 
 BrowserContext* StoragePartitionImpl::browser_context() const {
