c6181faa3bf891a20e9734b00c9a02ff0a24a47b
Yafei Duan <romax@chromium.org>
2017-12-20 18:50:09 -0800

    [Offline Pages] Adding a util method for generating unique file names.
    Adding a utility method in model_utils to generate a unique file name
    given the target directory, url and title of the page.
    
    The file name will be composed as:
    {hostname of url}-{title truncated to 40 bytes} [(counter)].mhtml
    
    It will scan the directory looking for files with same name, if there is
    one, it will start a counter from 1 to 99, and search for the next
    available file name in the form of "{suggested_file_name} (x).mhtml" to
    keep in sync with normal download files.
    
    Bug: 757073
    Change-Id: I1dd467b29e4ac89e87cddf524aae88f3d174bbf3

--- components/offline_pages/core/model/offline_page_model_utils.cc
+++ components/offline_pages/core/model/offline_page_model_utils.cc
@@ -6,11 +6,68 @@
 
 #include <string>
 
+#include "base/bind.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/logging.h"
+#include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
+#include "base/task_scheduler/post_task.h"
+#include "build/build_config.h"
 #include "components/offline_pages/core/client_namespace_constants.h"
 #include "components/offline_pages/core/offline_page_item.h"
 
+#if defined(OS_WIN)
+#include "base/strings/utf_string_conversions.h"
+#endif
+
 namespace offline_pages {
+
+namespace {
+
+const int kTitleMaxByteSize = 40;
+const int kMaxUniqueFiles = 100;
+const char kFileNameComponentsSeparator[] = "-";
+const char kReplaceChars[] = " ";
+const char kReplaceWith[] = "_";
+const base::FilePath::CharType kMHTMLExtenstion[] = FILE_PATH_LITERAL("mhtml");
+
+base::FilePath GenerateUniqueFileNameSync(const base::FilePath& target_dir,
+                                          const GURL& url,
+                                          const std::string& title) {
+  std::string title_part;
+  base::TruncateUTF8ToByteSize(title, kTitleMaxByteSize, &title_part);
+  std::string suggested_name(url.host() + kFileNameComponentsSeparator +
+                             title_part);
+  base::ReplaceChars(suggested_name, kReplaceChars, kReplaceWith,
+                     &suggested_name);
+
+#if defined(OS_POSIX)
+  base::FilePath::StringType suggested_path = suggested_name;
+#elif defined(OS_WIN)
+  base::FilePath::StringType suggested_path = base::UTF8ToUTF16(suggested_name);
+#endif
+
+  base::FilePath path_to_check(
+      target_dir.Append(suggested_path).AddExtension(kMHTMLExtenstion));
+
+  // If there's no duplicate names, the |path_to_check| is available.
+  if (!base::PathExists(path_to_check))
+    return path_to_check;
+  // Try to add suffixes to |path_to_check| until an available name is found or
+  // the limit is hit.
+  for (int file_number = 1; file_number < kMaxUniqueFiles; ++file_number) {
+    std::string suffix(base::StringPrintf(" (%d)", file_number));
+    base::FilePath path_with_counter =
+        path_to_check.InsertBeforeExtensionASCII(suffix);
+    if (!base::PathExists(path_with_counter))
+      return path_with_counter;
+  }
+  // If there's no available file name, just return an empty path.
+  return base::FilePath();
+}
+
+}  // namespace
 
 namespace model_utils {
 
@@ -49,6 +106,16 @@ std::string AddHistogramSuffix(const ClientId& client_id,
   adjusted_histogram_name += ".";
   adjusted_histogram_name += client_id.name_space;
   return adjusted_histogram_name;
+}
+
+void GenerateUniqueFileName(const base::FilePath& target_dir,
+                            const GURL& url,
+                            const std::string& title,
+                            GenerateUniqueFileNameCallback callback) {
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::BACKGROUND},
+      base::BindOnce(&GenerateUniqueFileNameSync, target_dir, url, title),
+      std::move(callback));
 }
 
 }  // namespace model_utils
