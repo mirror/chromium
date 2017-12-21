24fff65a97faad1b5287b9a96f81cd30b0623662
Filip Gorski <fgorski@chromium.org>
2017-12-21 15:05:30 -0800

    [Offline pages] Enables sharing of pages saved in public Downloads/
    This patch removes sharing restriction from cached pages only, and
    allows the user to share user-requested pages saved in the public
    Downloads/ folder.
    
    Bug: 758690
    Change-Id: Iecd76c87b2d0e35dde8222ba9facf5a82c33bb13

--- chrome/browser/offline_pages/android/offline_page_model_factory.cc
+++ chrome/browser/offline_pages/android/offline_page_model_factory.cc
@@ -12,6 +12,7 @@
 #include "base/sequenced_task_runner.h"
 #include "base/task_scheduler/post_task.h"
 #include "base/time/default_clock.h"
+#include "chrome/browser/download/download_prefs.h"
 #include "chrome/browser/offline_pages/android/cct_origin_observer.h"
 #include "chrome/browser/offline_pages/fresh_offline_content_observer.h"
 #include "chrome/browser/profiles/profile.h"
@@ -52,7 +53,8 @@ KeyedService* OfflinePageModelFactory::BuildServiceInstanceFor(
       new OfflinePageMetadataStoreSQL(background_task_runner, store_path));
 
   base::FilePath persistent_archives_dir =
-      profile->GetPath().Append(chrome::kOfflinePageArchivesDirname);
+      DownloadPrefs::GetDefaultDownloadDirectory();
+
   // If PathService::Get returns false, the temporary_archives_dir will be
   // empty, and no temporary pages will be saved during this chrome lifecycle.
   base::FilePath temporary_archives_dir;
