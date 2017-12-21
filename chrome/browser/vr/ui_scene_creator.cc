6f98b999e48bf984bf313b5f66ba91b38e27b41e
Patti <patricialor@chromium.org>
2017-12-21 15:03:10 +1100

    Harmony/Desktop Page Info: Update icons from .png to .icon.
    Use vectors for Page Info icons, which has the added benefit of being able to
    programmatically generate the red 'x' badges on blocked permission icons.
    
    This patch also renames a few icons to match the official naming for Material
    Design icons.
    
    Screenshots - https://drive.google.com/open?id=1ZhPw2apMGNr0e6ey86y_eTHzkywnL10i
    
    Bug: 535074, 647551
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ic30d0d7269395312d77dcec3ac8a88617cf221ea

--- chrome/browser/vr/ui_scene_creator.cc
+++ chrome/browser/vr/ui_scene_creator.cc
@@ -394,7 +394,7 @@ void UiSceneCreator::CreateSystemIndicators() {
     bool PermissionsModel::*signal;
   };
   const std::vector<Indicator> indicators = {
-      {kAudioCaptureIndicator, vector_icons::kMicrophoneIcon,
+      {kAudioCaptureIndicator, vector_icons::kMicIcon,
        IDS_AUDIO_CALL_NOTIFICATION_TEXT_2,
        &PermissionsModel::audio_capture_enabled},
       {kVideoCaptureIndicator, vector_icons::kVideocamIcon,
@@ -765,7 +765,7 @@ void UiSceneCreator::CreateVoiceSearchUiGroup() {
       kVoiceSearchButton, kPhaseForeground,
       base::BindRepeating(&UiBrowserInterface::SetVoiceSearchActive,
                           base::Unretained(browser_), true),
-      vector_icons::kMicrophoneIcon);
+      vector_icons::kMicIcon);
   voice_search_button->SetSize(kVoiceSearchButtonDiameterDMM,
                                kVoiceSearchButtonDiameterDMM);
   voice_search_button->set_hover_offset(kButtonZOffsetHoverDMM);
@@ -843,7 +843,7 @@ void UiSceneCreator::CreateVoiceSearchUiGroup() {
 
   auto microphone = base::MakeUnique<VectorIcon>(512);
   microphone->SetName(kSpeechRecognitionResultMicrophoneIcon);
-  microphone->SetIcon(vector_icons::kMicrophoneIcon);
+  microphone->SetIcon(vector_icons::kMicIcon);
   microphone->SetDrawPhase(kPhaseForeground);
   microphone->set_hit_testable(false);
   microphone->SetSize(kCloseButtonWidth, kCloseButtonHeight);
@@ -911,7 +911,7 @@ void UiSceneCreator::CreateVoiceSearchUiGroup() {
   scene_->AddUiElement(kSpeechRecognitionListening, std::move(inner_circle));
 
   auto microphone_icon = base::MakeUnique<VectorIcon>(512);
-  microphone_icon->SetIcon(vector_icons::kMicrophoneIcon);
+  microphone_icon->SetIcon(vector_icons::kMicIcon);
   microphone_icon->SetName(kSpeechRecognitionListeningMicrophoneIcon);
   microphone_icon->SetDrawPhase(kPhaseForeground);
   microphone_icon->set_hit_testable(false);
