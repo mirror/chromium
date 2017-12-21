a3f6214fe4f20957b21365d5a7de09957b2d7e8b
Ryan Sleevi <rsleevi@chromium.org>
2017-12-21 14:56:37 -0500

    Implement the first phase of Symantec Distrust
    The outline of these details is captured in the blog post at
    https://security.googleblog.com/2017/09/chromes-plan-to-distrust-symantec.html
    
    The following behaviour changes are included:
     * DigiCert Global Root G2 (https://crt.sh/?caid=5885) and
       DigiCert Global Root G3 (https://crt.sh/?caid=5699) are treated as
       indepedently operated sub-CAs, as they were CAs already trusted on Chrome
       and Android, despite having been recently cross-signed by the legacy
       Symantec infrastructure. Their audits remain unchanged, and they are
       exempt from the CT requirement specific to Symantec.
     * DigiCert Transition RSA Root (https://crt.sh/?id=250864698) and
       DigiCert Transition ECC Root (https://crt.sh/?id=250864681) are treated
       as Managed Sub-CAs, subject to the Managed Partner Infrastructure policies.
       In particular, all certificates from these CAs are required to be
       "CT Qualified" in order to be trusted
     * All new certificates issued from the legacy Symantec infrastructure -
       excluding those CAs above and the pre-existing independent sub-CAs - issued
       after 2017-12-01 are not trusted.
     * Control over CT requirements uses the existing TransportSecurityState
       delegate methods.
     * Control over the phased trust status is expressed as a
       CertVerifier::Verify() flag.
     * Control over the CertVerifier::Verify() flag is exposed to the SSLConfig,
       and by proxy, the SSLConfigService.
    
    These changes are made for Chromium and Chrome on all supported platforms.
    These changes are made for Cronet, where applicable to Cronet (trust, but not
    CT enforcement.
    These changes are made for all content embedders by default.
    
    Consistent with Android WebView behaving functionally identical to the Android
    system, these changes are not included in Android WebView. Any changes to trust
    will propogate via System Updates or through opting-in to the Google Play
    Services security provider, as described at
    https://developer.android.com/training/articles/security-gms-provider.html
    
    BUG=796230
    
    Change-Id: Ib9afa2cfec88dfadb7ae2300487513097f78253d

---
+++ android_webview/browser/aw_browser_context_unittest.cc
@@ -0,0 +1,67 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "android_webview/browser/aw_browser_context.h"
+
+#include <memory>
+
+#include "android_webview/browser/net/aw_url_request_context_getter.h"
+#include "base/android/jni_android.h"
+#include "base/files/scoped_temp_dir.h"
+#include "base/memory/ref_counted.h"
+#include "components/prefs/testing_pref_service.h"
+#include "content/public/browser/browser_thread.h"
+#include "content/public/test/test_browser_thread_bundle.h"
+#include "net/log/net_log.h"
+#include "net/proxy/proxy_config_service.h"
+#include "net/proxy/proxy_service.h"
+#include "net/ssl/ssl_config.h"
+#include "net/ssl/ssl_config_service.h"
+#include "net/url_request/url_request_context.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+class AwBrowserContextTest : public ::testing::Test {
+ public:
+  AwBrowserContextTest() = default;
+
+ protected:
+  void SetUp() override {
+    ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
+    env_ = base::android::AttachCurrentThread();
+    ASSERT_TRUE(env_);
+
+    browser_context_ = std::make_unique<android_webview::AwBrowserContext>(
+        temp_dir_.GetPath());
+    browser_context_->PreMainMessageLoopRun(&net_log_);
+  }
+
+  content::TestBrowserThreadBundle thread_bundle_;
+  base::ScopedTempDir temp_dir_;
+  JNIEnv* env_;
+  net::NetLog net_log_;
+  std::unique_ptr<android_webview::AwBrowserContext> browser_context_;
+};
+
+// Tests that constraints on trust for Symantec-issued certificates are not
+// enforced for the AwURLRequestContext(Getter), as it should behave like
+// the Android system.
+TEST_F(AwBrowserContextTest, SymantecPoliciesExempted) {
+  android_webview::AwURLRequestContextGetter* getter =
+      browser_context_->GetAwURLRequestContext();
+  ASSERT_TRUE(getter);
+  net::URLRequestContext* context =
+      getter->GetURLRequestContext();
+  ASSERT_TRUE(context);
+  net::SSLConfigService* config_service = context->ssl_config_service();
+  ASSERT_TRUE(config_service);
+
+  net::SSLConfig config;
+  EXPECT_FALSE(config.symantec_enforcement_disabled);
+  config_service->GetSSLConfig(&config);
+  EXPECT_TRUE(config.symantec_enforcement_disabled);
+}
+
+}  // namespace
