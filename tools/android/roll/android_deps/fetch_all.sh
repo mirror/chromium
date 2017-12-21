c6b877ba237e5af0b0a7228a06da7455e41ecc70
Nicolas Dossou-gbete <dgn@chromium.org>
2017-12-21 18:44:16 +0000

    🔧 Add the //third_party/android_deps repository
    Adds a tool to download android and java libraries by
    specifying them as Gradle dependencies. The new tool downloads
    them to //third_party/android_deps and generate the BUILD.gn
    entries and README.chromium files as appropriate.
    
    As part of adding the tool, this patch adds a new third_party
    dependency: //third_party/gradle_wrapper.
    
    Design doc: go/chromium-m2-repo
    Bug: 787005
    Change-Id: I071f9318ea87eefcbdc156a4507db4d1d52f105a

---
+++ tools/android/roll/android_deps/fetch_all.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+CHROMIUM_DIR=${SCRIPT_DIR}/../../../..
+
+if [[ $# > 0 ]]; then
+  cat ${SCRIPT_DIR}/README.md
+  exit 1
+fi
+
+${CHROMIUM_DIR}/third_party/gradle_wrapper/gradlew \
+    -b ${SCRIPT_DIR}/build.gradle setUpRepository
+ret=$?
+
+if [[ $ret = 0 ]]; then
+  gn format ${CHROMIUM_DIR}/third_party/android_deps/BUILD.gn
+  gn gen /tmp/dummy_gn_run \
+    --args="update_android_aar_prebuilts=true target_os=\"android\""
+  echo -e "------------------------------------------------------------\n" \
+          "Instructions to upload CIPD packages are available in each"     \
+          "package's 'cipd.yaml' file. To see which ones changed and need" \
+          "to be updated, run\n"                                           \
+          "  git diff third_party/android_deps/android_deps.ensure\n"      \
+          "\n"                                                             \
+          "To test the new build once the packages are uploaded, before a" \
+          "new build run the following commands:\n"                        \
+          "  rm -rf third_party/android_deps/repository/*/\n"              \
+          "  rm -rf third_party/android_deps/.cipd\n"                      \
+          "  gclient runhooks\n"                                           \
+          "\n"                                                             \
+          "\n------------------------------------------------------------"
+fi
