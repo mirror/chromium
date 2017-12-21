5bf5258324a3d7b3830a3fa2cbb450b3d4049ee6
Kouhei Ueno <kouhei@chromium.org>
2017-12-21 12:36:05 +0900

    [ES6 modules] Store base URL in ReferrerScriptInfo
    This CL is a part of work to align base URL handling of "script".
    
    This CL makes ReferrerScriptInfo contain referencingScriptOrModule's "base URL"
    as ReferrerScriptInfo::base_url_.
    In future CLs:
    - The added field will be referenced in [ResolveModuleSpecifier] Step 3, which is called from [HIMD] step 2.1.
    - The "base URL" of script is plumbed to set correct value (which we currently use script's source URL in this CL).
    
    [ResolveModuleSpecifier] https://html.spec.whatwg.org/multipage/webappapis.html#resolve-a-module-specifier
    [HIMD] https://html.spec.whatwg.org/multipage/webappapis.html#hostimportmoduledynamically(referencingscriptormodule,-specifier,-promisecapability)
    
    Bug: 796034
    Change-Id: Ifbd280e84f3eea4e6a4d308793be0f8f2bcd692f

--- third_party/WebKit/Source/bindings/core/v8/ScriptController.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScriptController.cpp
@@ -125,9 +125,10 @@ v8::Local<v8::Value> ScriptController::ExecuteScriptAndReturnValue(
     v8::TryCatch try_catch(GetIsolate());
     try_catch.SetVerbose(true);
 
-    const ReferrerScriptInfo referrer_info(fetch_options.CredentialsMode(),
-                                           fetch_options.Nonce(),
-                                           fetch_options.ParserState());
+    const KURL& wrong_base_url = source.Url();
+    const ReferrerScriptInfo referrer_info(
+        wrong_base_url, fetch_options.CredentialsMode(), fetch_options.Nonce(),
+        fetch_options.ParserState());
 
     v8::Local<v8::Script> script;
 
