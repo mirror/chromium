216cf4727da8722b393272f3ee3aec8b932ce3bb
Chris Pickel <sfiera@chromium.org>
2017-12-21 14:20:09 +0100

    Local NTP: size Doodle iframe from ddljson
    Parse iframe_{width,height}_px out of ddljson. Pass it along to the JS.
    Size the iframe appropriately, and change the margin to preserve the
    position of the Fakebox.
    
    This breaks the previously-functional ability of resizeIframe to specify
    height: null to reset to the default. Since the Doodle should know its
    own height, remove that feature instead of fixing it.
    
    Bug:
    Change-Id: I2c6dd47e046c2c8ab6de087551045429a69d40a6

--- components/search_provider_logos/google_logo_api.cc
+++ components/search_provider_logos/google_logo_api.cc
@@ -181,7 +181,7 @@ std::unique_ptr<EncodedLogo> ParseDoodleLogoResponse(
   }
 
   const bool is_animated = (logo->metadata.type == LogoType::ANIMATED);
-  const bool is_interactive = (logo->metadata.type == LogoType::INTERACTIVE);
+  bool is_interactive = (logo->metadata.type == LogoType::INTERACTIVE);
 
   // Check if the main image is animated.
   if (is_animated) {
@@ -242,6 +242,18 @@ std::unique_ptr<EncodedLogo> ParseDoodleLogoResponse(
         (behavior == "NEW_WINDOW")) {
       logo->metadata.type = LogoType::SIMPLE;
       logo->metadata.on_click_url = logo->metadata.full_page_url;
+      is_interactive = false;
+    }
+  }
+
+  logo->metadata.iframe_width_px = 0;
+  logo->metadata.iframe_height_px = 0;
+  if (is_interactive) {
+    if (!(ddljson->GetInteger("iframe_width_px",
+                              &logo->metadata.iframe_width_px) &&
+          ddljson->GetInteger("iframe_height_px",
+                              &logo->metadata.iframe_height_px))) {
+      return nullptr;
     }
   }
 
