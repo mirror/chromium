c009a4568f9e997ca1ef0f0bfd6de702a4f7cc37
Yafei Duan <romax@chromium.org>
2017-12-20 17:56:26 -0800

    [Offline Pages] Switching from OPMImpl to OPMTaskified.
    Switching OfflinePageModelFactory from creating OfflinePageModelImpl to
    OfflinePageModelTaskified. Also have related changes required in tests.
    
    
    Bug: 753595
    Change-Id: I8de03e1e943cf3e8c3040e5b3572643d72b9fca2

--- components/offline_pages/core/model/offline_page_model_taskified.cc
+++ components/offline_pages/core/model/offline_page_model_taskified.cc
@@ -121,6 +121,7 @@ OfflinePageModelTaskified::OfflinePageModelTaskified(
       policy_controller_(new ClientPolicyController()),
       task_queue_(this),
       clock_(std::move(clock)),
+      skip_clearing_original_url_for_testing_(false),
       weak_ptr_factory_(this) {
   CreateArchivesDirectoryIfNeeded();
   PostClearLegacyTemporaryPagesTask();
@@ -146,9 +147,11 @@ void OfflinePageModelTaskified::SavePage(
     const SavePageCallback& callback) {
   auto task = base::MakeUnique<CreateArchiveTask>(
       GetArchiveDirectory(save_page_params.client_id.name_space),
-      save_page_params, archiver.get(),
+      save_page_params, archiver.get(), clock_.get(),
       base::Bind(&OfflinePageModelTaskified::OnCreateArchiveDone,
                  weak_ptr_factory_.GetWeakPtr(), callback));
+  if (skip_clearing_original_url_for_testing_)
+    task->set_skip_clearing_original_url_for_testing();
   pending_archivers_.push_back(std::move(archiver));
   task_queue_.AddTask(std::move(task));
 }
