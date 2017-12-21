9fd74112f029f2dd953c1218680acbf4876268fa
Anita Woodruff <awdf@chromium.org>
2017-12-21 13:53:11 +0000

    [Mojoification] Add notification.mojom with struct traits
    - Added NotificationData mojo struct and struct traits to
    allow conversion to/from blink::WebNotificationData and
    content::PlatformNotificationData.
    
    - Currently the struct only contains the title, more properties will
    be added in due course.
    
    - The struct traits live in //content/common/ for now because there
    is still event dispatching code in //content/renderer/. Once
    mojification is complete, they can be moved to //content/browser/.
    
    - Note the mojo pathway is still behind the NotificationsWithMojo
    flag while development continues.
    
    Bug: 595685
    Change-Id: I60e195f8c0bf9a2c0e8f9dc78ebaf95349b738c8

---
+++ content/common/notifications/notification_struct_traits.cc
@@ -0,0 +1,20 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/common/notifications/notification_struct_traits.h"
+
+namespace mojo {
+
+// static
+bool StructTraits<blink::mojom::NotificationDataDataView,
+                  content::PlatformNotificationData>::
+    Read(blink::mojom::NotificationDataDataView notification_data,
+         content::PlatformNotificationData* platform_notification_data) {
+  return notification_data.ReadTitle(&platform_notification_data->title) &&
+         notification_data.ReadLang(&platform_notification_data->lang) &&
+         notification_data.ReadBody(&platform_notification_data->body) &&
+         notification_data.ReadTag(&platform_notification_data->tag);
+}
+
+}  // namespace mojo
---
+++ third_party/WebKit/Source/platform/mojo/NotificationStructTraits.cpp
@@ -0,0 +1,69 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/mojo/NotificationStructTraits.h"
+
+namespace mojo {
+
+// static
+WTF::String StructTraits<
+    blink::mojom::NotificationDataDataView,
+    blink::WebNotificationData>::title(const blink::WebNotificationData& data) {
+  return data.title;
+}
+
+// static
+WTF::String StructTraits<
+    blink::mojom::NotificationDataDataView,
+    blink::WebNotificationData>::lang(const blink::WebNotificationData& data) {
+  return data.lang;
+}
+
+// static
+WTF::String StructTraits<
+    blink::mojom::NotificationDataDataView,
+    blink::WebNotificationData>::body(const blink::WebNotificationData& data) {
+  return data.body;
+}
+
+// static
+WTF::String StructTraits<
+    blink::mojom::NotificationDataDataView,
+    blink::WebNotificationData>::tag(const blink::WebNotificationData& data) {
+  return data.tag;
+}
+
+// static
+blink::KURL StructTraits<
+    blink::mojom::NotificationDataDataView,
+    blink::WebNotificationData>::badge(const blink::WebNotificationData& data) {
+  return data.badge;
+}
+
+// static
+bool StructTraits<blink::mojom::NotificationDataDataView,
+                  blink::WebNotificationData>::
+    Read(blink::mojom::NotificationDataDataView data,
+         blink::WebNotificationData* out) {
+  WTF::String title;
+  WTF::String lang;
+  WTF::String body;
+  WTF::String tag;
+
+  blink::KURL badge;
+
+  if (!data.ReadTitle(&title) || !data.ReadLang(&lang) ||
+      !data.ReadBody(&body) || !data.ReadTag(&tag) || !data.ReadBadge(&badge)) {
+    return false;
+  }
+
+  out->title = title;
+  out->lang = lang;
+  out->body = body;
+  out->tag = tag;
+  out->badge = badge;
+  return true;
+}
+
+}  // namespace mojo
