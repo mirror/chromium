dca785b12a05d905625b9ddb4d348d872a1f74ba
Rakina Zata Amni <rakina@chromium.org>
2017-12-22 15:49:21 +0900

    Implement assignedElements for HTMLSlotElement
    The spec for HTMLSlotElement was recently updated, adding the assignedElements
    method that returns a slot's assigned nodes, limited to elements. Spec:
    https://html.spec.whatwg.org/#dom-slot-assignedelements
    
    Bug:795544
    Change-Id: I7a2642b594d939e70b7ebdcf2817d71e66ca114a

--- third_party/WebKit/Source/core/html/HTMLSlotElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLSlotElement.cpp
@@ -89,6 +89,16 @@ const HeapVector<Member<Node>> HTMLSlotElement::assignedNodesForBinding(
   return assigned_nodes_;
 }
 
+const HeapVector<Member<Node>> HTMLSlotElement::assignedElementsForBinding(
+    const AssignedNodesOptions& options) {
+  HeapVector<Member<Node>> elements;
+  for (auto node : assignedNodesForBinding(options)) {
+    if (node->IsElementNode())
+      elements.push_back(node);
+  }
+  return elements;
+}
+
 const HeapVector<Member<Node>>& HTMLSlotElement::GetDistributedNodes() {
   DCHECK(!NeedsDistributionRecalc());
   DCHECK(SupportsAssignment() || distributed_nodes_.IsEmpty());
