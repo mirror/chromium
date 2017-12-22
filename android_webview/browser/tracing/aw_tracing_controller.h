3644dbd0b090fba51b900d4a157bc2eb55326bd3
Tim Volodine <timvolodine@google.com>
2017-12-22 16:08:30 +0000

    [Android WebView] Chromium side implementation of WebView Tracing API.
    This is the initial patch that implements the basic mechanism for
    the tracing api in chromium. Contains AwTracingController.java and its
    native counterpart aw_tracing_controller.{h,cc} with JNI.
    There is not more than one instance of AwTracingController which is
    created on first call to getTracingController.
    
    BUG=781249
    
    Change-Id: Ie407117efb97da0894f8441e9db5d1c5b60e5a7e

---
+++ android_webview/browser/tracing/aw_tracing_controller.h
@@ -0,0 +1,51 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ANDROID_WEBVIEW_BROWSER_TRACING_AW_TRACING_CONTROLLER_H_
+#define ANDROID_WEBVIEW_BROWSER_TRACING_AW_TRACING_CONTROLLER_H_
+
+#include "base/android/jni_weak_ref.h"
+#include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+
+#include "content/public/browser/tracing_controller.h"
+
+namespace android_webview {
+
+class AwTracingController {
+ public:
+  // GENERATED_JAVA_ENUM_PACKAGE: org.chromium.android_webview
+  enum AwTracingMode {
+    RECORD_UNTIL_FULL = 0,
+    RECORD_CONTINUOSLY = 1,
+    RECORD_AS_MUCH_AS_POSSIBLE = 2,
+    RECORD_TO_CONSOLE = 3,
+  };
+
+  AwTracingController(JNIEnv* env, jobject obj);
+
+  bool Start(JNIEnv* env,
+             const base::android::JavaParamRef<jobject>& obj,
+             const base::android::JavaParamRef<jstring>& categories,
+             jint mode);
+  bool StopAndFlush(JNIEnv* env,
+                    const base::android::JavaParamRef<jobject>& obj);
+  bool IsTracing(JNIEnv* env, const base::android::JavaParamRef<jobject>& obj);
+
+ private:
+  ~AwTracingController();
+
+  void OnTraceDataReceived(
+      std::unique_ptr<const base::DictionaryValue> metadata,
+      base::RefCountedString* trace_data);
+
+  JavaObjectWeakGlobalRef weak_java_object_;
+  base::WeakPtrFactory<AwTracingController> weak_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(AwTracingController);
+};
+
+}  // namespace android_webview
+
+#endif  // ANDROID_WEBVIEW_BROWSER_TRACING_AW_TRACING_CONTROLLER_H_
