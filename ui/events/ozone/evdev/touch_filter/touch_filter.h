56f06116366b277172a1dfa25191d86a494d3975
Eric Willigers <ericwilligers@chromium.org>
2017-12-22 19:45:47 +1100

    [jumbo] touch_filter Distance2 is now defined once
    The duplicate definitions in far_apart_taps_touch_noise_filter.cc
    and single_position_touch_noise_filter.cc caused problems
    with jumbo builds.
    
    BUG=773275
    
    Change-Id: I01437f04d0f7abaeb4869e6fe2a3d25bbea2a4f5

--- ui/events/ozone/evdev/touch_filter/touch_filter.h
+++ ui/events/ozone/evdev/touch_filter/touch_filter.h
@@ -19,6 +19,14 @@ class TouchFilter {
   virtual void Filter(const std::vector<InProgressTouchEvdev>& touches,
                       base::TimeTicks time,
                       std::bitset<kNumTouchEvdevSlots>* slots_to_suppress) = 0;
+
+ protected:
+  // Returns the squared distance between (|x1|, |y1|) and (|x2|, |y2|)
+  static int Distance2(int x1, int y1, int x2, int y2) {
+    int offset_x = x2 - x1;
+    int offset_y = y2 - y1;
+    return offset_x * offset_x + offset_y * offset_y;
+  }
 };
 
 }  // namespace ui
