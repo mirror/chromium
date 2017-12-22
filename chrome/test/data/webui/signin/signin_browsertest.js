4e8d3ff8eaa6303ebd55fc0d6a99147b02e857c0
Scott Chen <scottchen@chromium.org>
2017-12-21 17:00:19 -0800

    [signin] fix iron-icon path in sync confirmation dialog.
    Bug: 796536
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Icdd1b45f3c87ef18a7ece9f361788c2011746f35

---
+++ chrome/test/data/webui/signin/signin_browsertest.js
@@ -0,0 +1,46 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/** @fileoverview Runs the Polymer Settings tests. */
+
+/** @const {string} Path to source root. */
+const ROOT_PATH = '../../../../../';
+
+// Polymer BrowserTest fixture.
+GEN_INCLUDE(
+    [ROOT_PATH + 'chrome/test/data/webui/polymer_browser_test_base.js']);
+GEN('#include "base/command_line.h"');
+GEN('#include "chrome/test/data/webui/signin_browsertest.h"');
+
+/**
+ * Test fixture for
+ * chrome/browser/resources/signin/dice_sync_confirmation/sync_confirmation.html.
+ * @constructor
+ * @extends {PolymerTest}
+ */
+const SigninSyncConfirmationTest = class extends PolymerTest {
+  get typedefCppFixture() {
+    return 'SigninBrowserTest';
+  }
+
+  testGenPreamble() {
+    GEN('  EnableDice();');
+  }
+
+  /** @override */
+  get browsePreload() {
+    return 'chrome://sync-confirmation/sync_confirmation_app.html';
+  }
+
+  /** @override */
+  get extraLibraries() {
+    return PolymerTest.getLibraries(ROOT_PATH).concat([
+      'sync_confirmation_test.js',
+    ]);
+  }
+};
+
+TEST_F('SigninSyncConfirmationTest', 'DialogWithDice', function() {
+  mocha.run();
+});
