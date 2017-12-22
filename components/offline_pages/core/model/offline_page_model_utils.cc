5d8d088a5850001de5725d460a3811a11f93bab6
Yafei Duan <romax@chromium.org>
2017-12-21 17:28:10 -0800

    [Offline Pages] Adding a util method for generating unique file names.
    Adding a utility method in model_utils to generate a unique file name
    given the target directory, url and title of the page.
    
    The file name will be composed as:
    {hostname of url}-{title truncated to 40 bytes} [(counter)].mhtml
    
    It will scan the directory looking for files with same name, if there is
    one, it will start a counter from 1 to 99, and search for the next
    available file name in the form of "{suggested_file_name} (x).mhtml" to
    keep in sync with normal download files.
    
    Bug: 757073
    Change-Id: I1dd467b29e4ac89e87cddf524aae88f3d174bbf3

--- components/offline_pages/core/model/offline_page_model_utils.cc
+++ components/offline_pages/core/model/offline_page_model_utils.cc
@@ -6,11 +6,62 @@
 
 #include <string>
 
+#include "base/bind.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/i18n/file_util_icu.h"
 #include "base/logging.h"
+#include "base/strings/string16.h"
+#include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
+#include "base/task_scheduler/post_task.h"
+#include "build/build_config.h"
 #include "components/offline_pages/core/client_namespace_constants.h"
 #include "components/offline_pages/core/offline_page_item.h"
 
+#if defined(OS_WIN)
+#include "base/strings/utf_string_conversions.h"
+#endif
+
 namespace offline_pages {
+
+namespace {
+
+const base::FilePath::CharType kMHTMLExtension[] = FILE_PATH_LITERAL("mhtml");
+const base::FilePath::CharType kFileNameForEmptyTitle[] =
+    FILE_PATH_LITERAL("NO TITLE.mhtml");
+
+base::FilePath GenerateUniqueFileNameSync(const base::FilePath& target_dir,
+                                          const GURL& url,
+                                          const base::string16& title) {
+  // TODO(romax): fallback to url once the function for generating file names
+  // from urls are moved into base::.
+  if (title.size() == 0)
+    return base::FilePath(target_dir.Append(kFileNameForEmptyTitle));
+
+  base::FilePath path_from_title = base::FilePath::FromUTF16Unsafe(title);
+  base::FilePath::StringType file_name = path_from_title.value();
+  base::i18n::ReplaceIllegalCharactersInPath(&file_name, '_');
+
+  base::FilePath path_to_check(
+      target_dir.Append(file_name).AddExtension(kMHTMLExtension));
+
+  // Returns 0 if path_to_check is available; -1 if there's an error; other
+  // positive number for uniquifier.
+  int uniquifier =
+      base::GetUniquePathNumber(path_to_check, base::FilePath::StringType());
+
+  if (uniquifier < 0)
+    return base::FilePath();
+
+  if (uniquifier > 0) {
+    path_to_check = path_to_check.InsertBeforeExtensionASCII(
+        base::StringPrintf(" (%d)", uniquifier));
+  }
+  return path_to_check;
+}
+
+}  // namespace
 
 namespace model_utils {
 
@@ -49,6 +100,16 @@ std::string AddHistogramSuffix(const ClientId& client_id,
   adjusted_histogram_name += ".";
   adjusted_histogram_name += client_id.name_space;
   return adjusted_histogram_name;
+}
+
+void GenerateUniqueFileName(const base::FilePath& target_dir,
+                            const GURL& url,
+                            const base::string16& title,
+                            GenerateUniqueFileNameCallback callback) {
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::BACKGROUND},
+      base::BindOnce(&GenerateUniqueFileNameSync, target_dir, url, title),
+      std::move(callback));
 }
 
 }  // namespace model_utils
--- components/offline_pages/core/model/offline_page_model_utils_unittest.cc
+++ components/offline_pages/core/model/offline_page_model_utils_unittest.cc
@@ -4,12 +4,56 @@
 
 #include "components/offline_pages/core/model/offline_page_model_utils.h"
 
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/files/scoped_temp_dir.h"
+#include "base/memory/ptr_util.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/test/mock_callback.h"
+#include "base/test/scoped_task_environment.h"
+#include "base/test/test_simple_task_runner.h"
+#include "base/threading/thread_task_runner_handle.h"
+#include "components/offline_pages/core/client_id.h"
 #include "components/offline_pages/core/client_namespace_constants.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+using testing::A;
+using testing::Eq;
+using testing::Invoke;
+using testing::SaveArg;
 
 namespace offline_pages {
 
-TEST(OfflinePageModelUtilsTest, ToNamespaceEnum) {
+namespace {
+
+// Used to create a placeholder file for |file_path| so that it's possible to
+// generate file names with suffixes.
+void WriteFileAsPlaceHolder(const base::FilePath& file_path) {
+  EXPECT_EQ(0, base::WriteFile(file_path, NULL, 0));
+}
+
+}  // namespace
+
+class OfflinePageModelUtilsTest : public testing::Test {
+ public:
+  void SetUp() override;
+
+  void PumpLoop() { scoped_task_environment_.RunUntilIdle(); }
+
+  const base::FilePath& target_dir() { return temp_dir_.GetPath(); }
+
+ private:
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
+  base::ScopedTempDir temp_dir_;
+};
+
+void OfflinePageModelUtilsTest::SetUp() {
+  ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
+}
+
+TEST_F(OfflinePageModelUtilsTest, ToNamespaceEnum) {
   EXPECT_EQ(model_utils::ToNamespaceEnum(kDefaultNamespace),
             OfflinePagesNamespaceEnumeration::DEFAULT);
   EXPECT_EQ(model_utils::ToNamespaceEnum(kBookmarkNamespace),
@@ -28,6 +72,180 @@ TEST(OfflinePageModelUtilsTest, ToNamespaceEnum) {
             OfflinePagesNamespaceEnumeration::SUGGESTED_ARTICLES);
   EXPECT_EQ(model_utils::ToNamespaceEnum(kBrowserActionsNamespace),
             OfflinePagesNamespaceEnumeration::BROWSER_ACTIONS);
+}
+
+TEST_F(OfflinePageModelUtilsTest, GenerateUniqueFileName) {
+  // The MockCallback is used as the callback passed into file name generation.
+  // The Run() method is expected to be called with the expected result and in
+  // order to enable the counter of suffix to increase, every time the callback
+  // is called, we create a placeholder file in the directory using
+  // WillOnce(Invoke(WriteFileAsPlaceHolder)), WriteFileAsPlaceHolder will take
+  // the returned value in the callback as the input argument;
+  base::MockCallback<model_utils::GenerateUniqueFileNameCallback> callback;
+
+  GURL url_1("http://news.google.com/page1");
+  base::string16 title_1 = base::UTF8ToUTF16("Google News Page");
+
+  base::FilePath expected_1(FILE_PATH_LITERAL("Google News Page.mhtml"));
+  EXPECT_CALL(callback, Run(Eq(target_dir().Append(expected_1))))
+      .WillOnce(Invoke(WriteFileAsPlaceHolder));
+
+  model_utils::GenerateUniqueFileName(target_dir(), url_1, title_1,
+                                      callback.Get());
+  PumpLoop();
+
+  // Adding same file again should introduce suffix "(1)".
+  base::FilePath expected_1_dup1(
+      FILE_PATH_LITERAL("Google News Page (1).mhtml"));
+  EXPECT_CALL(callback, Run(Eq(target_dir().Append(expected_1_dup1))))
+      .WillOnce(Invoke(WriteFileAsPlaceHolder));
+
+  model_utils::GenerateUniqueFileName(target_dir(), url_1, title_1,
+                                      callback.Get());
+  PumpLoop();
+
+  // Adding same file again should introduce suffix "(2)".
+  base::FilePath expected_1_dup2(
+      FILE_PATH_LITERAL("Google News Page (2).mhtml"));
+  EXPECT_CALL(callback, Run(Eq(target_dir().Append(expected_1_dup2))))
+      .WillOnce(Invoke(WriteFileAsPlaceHolder));
+
+  model_utils::GenerateUniqueFileName(target_dir(), url_1, title_1,
+                                      callback.Get());
+  PumpLoop();
+
+  // Try another url and title.
+  GURL url_2("https://en.m.wikipedia.org/Sample_page_about_stuff");
+  base::string16 title_2 = base::UTF8ToUTF16("Some Wiki Page - Wikipedia");
+
+  base::FilePath expected_2(
+      FILE_PATH_LITERAL("Some Wiki Page - Wikipedia.mhtml"));
+  EXPECT_CALL(callback, Run(Eq(target_dir().Append(expected_2))))
+      .WillOnce(Invoke(WriteFileAsPlaceHolder));
+
+  model_utils::GenerateUniqueFileName(target_dir(), url_2, title_2,
+                                      callback.Get());
+  PumpLoop();
+}
+
+TEST_F(OfflinePageModelUtilsTest, GenerateUniqueFileName_LongTitle) {
+  base::MockCallback<model_utils::GenerateUniqueFileNameCallback> callback;
+
+  // Try a long title.
+  GURL url("https://www.google.com/search");
+  base::string16 title = base::UTF8ToUTF16(
+      "A really really really really really long long^ - LONG LONG TITLE");
+  base::FilePath expected(
+      FILE_PATH_LITERAL("A really really really really really long long^ - "
+                        "LONG LONG TITLE.mhtml"));
+  EXPECT_CALL(callback, Run(Eq(target_dir().Append(expected))))
+      .WillOnce(Invoke(WriteFileAsPlaceHolder));
+
+  model_utils::GenerateUniqueFileName(target_dir(), url, title, callback.Get());
+  PumpLoop();
+
+  // Try the long title again, to see suffix.
+  base::FilePath expected_dup(
+      FILE_PATH_LITERAL("A really really really really really long long^ - "
+                        "LONG LONG TITLE (1).mhtml"));
+  EXPECT_CALL(callback, Run(Eq(target_dir().Append(expected_dup))))
+      .WillOnce(Invoke(WriteFileAsPlaceHolder));
+
+  model_utils::GenerateUniqueFileName(target_dir(), url, title, callback.Get());
+  PumpLoop();
+}
+
+TEST_F(OfflinePageModelUtilsTest, GenerateUniqueFileName_FillingHoles) {
+  base::MockCallback<model_utils::GenerateUniqueFileNameCallback> callback;
+
+  GURL url("http://news.google.com/page1");
+  base::string16 title = base::UTF8ToUTF16("Google News Page");
+  base::FilePath expected(FILE_PATH_LITERAL("Google News Page.mhtml"));
+  EXPECT_CALL(callback, Run(Eq(target_dir().Append(expected))))
+      .WillOnce(Invoke(WriteFileAsPlaceHolder));
+
+  model_utils::GenerateUniqueFileName(target_dir(), url, title, callback.Get());
+  PumpLoop();
+
+  // Adding same file again should introduce suffix "(1)".
+  base::FilePath expected_dup1(FILE_PATH_LITERAL("Google News Page (1).mhtml"));
+  EXPECT_CALL(callback, Run(Eq(target_dir().Append(expected_dup1))))
+      .WillOnce(Invoke(WriteFileAsPlaceHolder));
+
+  model_utils::GenerateUniqueFileName(target_dir(), url, title, callback.Get());
+  PumpLoop();
+
+  // The last suffix of the file names is (99).
+  // For the file names returned via the callback, we will save them and:
+  //  1. Create a placeholder file on the disk, to simulate archive creation.
+  //  2. Confirm the path returned is not empty, since it's still within the
+  //     limit of unique file name.
+  //  3. Confirm the suffix counter was increased successfully.
+  base::FilePath file_path;
+  EXPECT_CALL(callback, Run(A<base::FilePath>()))
+      .WillRepeatedly(SaveArg<0>(&file_path));
+  for (int i = 2; i < 100; i++) {
+    model_utils::GenerateUniqueFileName(target_dir(), url, title,
+                                        callback.Get());
+    PumpLoop();
+    EXPECT_EQ(0, base::WriteFile(file_path, NULL, 0));
+    EXPECT_FALSE(file_path.empty());
+    EXPECT_TRUE(file_path.value().find(std::to_string(i)) != std::string::npos);
+  }
+
+  // Delete the name with (42).
+  base::FilePath file_42(
+      target_dir().Append(FILE_PATH_LITERAL("Google News Page (42).mhtml")));
+  ASSERT_TRUE(base::DeleteFile(file_42, false));
+
+  // Try generating file name one more time and it will return a file path
+  // ending with (42).
+  model_utils::GenerateUniqueFileName(target_dir(), url, title, callback.Get());
+  PumpLoop();
+  EXPECT_TRUE(file_path.value().find("42") != std::string::npos);
+}
+
+TEST_F(OfflinePageModelUtilsTest,
+       GenerateUniqueFileName_NameWithIllegalCharacters) {
+  base::MockCallback<model_utils::GenerateUniqueFileNameCallback> callback;
+
+  GURL url("http://news.google.com/page1");
+  base::string16 title = base::UTF8ToUTF16("Google/\\\?\%*:|\"<>. News Page");
+  base::FilePath expected(
+      FILE_PATH_LITERAL("Google___%______. News Page.mhtml"));
+  EXPECT_CALL(callback, Run(Eq(target_dir().Append(expected))))
+      .WillOnce(Invoke(WriteFileAsPlaceHolder));
+
+  model_utils::GenerateUniqueFileName(target_dir(), url, title, callback.Get());
+  PumpLoop();
+}
+
+TEST_F(OfflinePageModelUtilsTest, GenerateUniqueFileName_NameWithUnicode) {
+  base::MockCallback<model_utils::GenerateUniqueFileNameCallback> callback;
+
+  GURL url("http://news.google.com/page1");
+  base::string16 title =
+      base::UTF8ToUTF16("Google News Page ßiÂÃÄÅÆÇÈÉ ïðñòóôõö÷øùúûü");
+  base::FilePath expected(
+      FILE_PATH_LITERAL("Google News Page ßiÂÃÄÅÆÇÈÉ ïðñòóôõö÷øùúûü.mhtml"));
+  EXPECT_CALL(callback, Run(Eq(target_dir().Append(expected))))
+      .WillOnce(Invoke(WriteFileAsPlaceHolder));
+
+  model_utils::GenerateUniqueFileName(target_dir(), url, title, callback.Get());
+  PumpLoop();
+}
+
+TEST_F(OfflinePageModelUtilsTest, GenerateUniqueFileName_EmptyTitle) {
+  base::MockCallback<model_utils::GenerateUniqueFileNameCallback> callback;
+
+  GURL url("http://news.google.com/page1");
+  base::string16 title = base::UTF8ToUTF16("");
+  base::FilePath expected(FILE_PATH_LITERAL("NO TITLE.mhtml"));
+  EXPECT_CALL(callback, Run(Eq(target_dir().Append(expected))))
+      .WillOnce(Invoke(WriteFileAsPlaceHolder));
+
+  model_utils::GenerateUniqueFileName(target_dir(), url, title, callback.Get());
+  PumpLoop();
 }
 
 }  // namespace offline_pages
