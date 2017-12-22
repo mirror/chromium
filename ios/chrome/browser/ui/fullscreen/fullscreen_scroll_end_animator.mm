2fe2deb6d0f465caa41e02a00c26dab1de245b49
Kurt Horimoto <kkhorimoto@chromium.org>
2017-12-21 16:31:29 -0800

    [iOS] Fullscreen animation polish for fast scoll events.
    This CL updates fullscreen to:
    - not perform an animation if a scoll ends when the toolbar is fully
      visible or hidden.
    - scale the scroll end adjustment animation duration to the fraction
      of progress change required.
    
    Bug: 796597
    Change-Id: I8939fbdd93735155e41a9ed1e2d19d1b72902b4a

--- ios/chrome/browser/ui/fullscreen/fullscreen_scroll_end_animator.mm
+++ ios/chrome/browser/ui/fullscreen/fullscreen_scroll_end_animator.mm
@@ -33,8 +33,12 @@
   UICubicTimingParameters* timingParams = [[UICubicTimingParameters alloc]
       initWithControlPoint1:CGPointMake(0.0, 0.0)
               controlPoint2:CGPointMake(0.2, 0.1)];
-  self = [super initWithDuration:ios::material::kDuration1
-                timingParameters:timingParams];
+  DCHECK_GE(startProgress, 0.0);
+  DCHECK_LE(startProgress, 1.0);
+  CGFloat finalProgress = roundf(startProgress);
+  NSTimeInterval duration =
+      2.0 * fabs(finalProgress - startProgress) * ios::material::kDuration1;
+  self = [super initWithDuration:duration timingParameters:timingParams];
   if (self) {
     DCHECK_GE(startProgress, 0.0);
     DCHECK_LE(startProgress, 1.0);
