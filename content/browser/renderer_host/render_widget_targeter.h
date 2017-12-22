01f683336f23bffdd1ea6eea6cdad5d9a107e163
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-12-22 14:16:15 -0500

    event router: Use mojom.InputTargetClient for targeting.
    Introduce a RenderWidgetTargeter. Briefly:
    
    . RenderWidgetTargeter is responsible for finding a target for a
      particular event. Right now, it first asks the router itself to find a
      target. The router returns a target, and a bool flag, indicating
      whether the returned target needs to be asked for the final target
      (i.e. whether InputTargetClient should be used).
    . Once HitTestQuery is ready, the same RenderWidgetTargeter can be used,
      but instead of asking the router itself for the target, and bool flag,
      the targeter will ask the HitTestQuery.
    . If an event-targeting request is sent to a client using the
      mojom.InputTargetClient API, then subsequent events are put in a queue,
      until the client responds to the request.
    . Once a client responds to a targeting request, RenderWidgetTargeter
      attempts to flush the queue, until the queue becomes empty, or another
      targeting request to a client is made using InputTargetClient api.
    
    Bug: 796656
    Change-Id: I55a75f9d69136731af96b2bf292c0f361fa729c4

---
+++ content/browser/renderer_host/render_widget_targeter.h
@@ -0,0 +1,107 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_RENDERER_HOST_RENDER_WIDGET_TARGETER_H_
+#define CONTENT_BROWSER_RENDERER_HOST_RENDER_WIDGET_TARGETER_H_
+
+#include <queue>
+
+#include "base/memory/weak_ptr.h"
+#include "ui/events/blink/web_input_event_traits.h"
+#include "ui/latency/latency_info.h"
+
+namespace blink {
+class WebInputEvent;
+}  // namespace blink
+
+namespace gfx {
+class PointF;
+}
+
+namespace viz {
+class FrameSinkId;
+}
+
+namespace content {
+
+class RenderWidgetHostViewBase;
+
+struct RenderWidgetTargetResult {
+  RenderWidgetHostViewBase* view = nullptr;
+  bool should_query_view = false;
+};
+
+class RenderWidgetTargeter {
+ public:
+  class Delegate {
+   public:
+    virtual RenderWidgetTargetResult FindTargetSynchronously(
+        RenderWidgetHostViewBase* root_view,
+        const blink::WebInputEvent& event,
+        gfx::PointF* out_location) = 0;
+
+    virtual void DispatchEventToTarget(RenderWidgetHostViewBase* root_view,
+                                       RenderWidgetHostViewBase* target,
+                                       const blink::WebInputEvent& event,
+                                       const ui::LatencyInfo& latency) = 0;
+  };
+
+  // The delegate must outlive this targeter.
+  explicit RenderWidgetTargeter(Delegate* delegate);
+  ~RenderWidgetTargeter();
+
+  // Finds the appropriate target inside |root_view| for |event|, and dispatches
+  // it through the delegate. |event| is in the coord-space of |root_view|.
+  void FindTargetAndDispatch(RenderWidgetHostViewBase* root_view,
+                             const blink::WebInputEvent& event,
+                             const ui::LatencyInfo& latency);
+
+ private:
+  // Attempts to target and dispatch all events in the queue. It stops if it has
+  // to query a client, or if the queue becomes empty.
+  void FlushEventQueue();
+
+  // Queries |target| to find the correct target for |event|.
+  // |event| is in the coordinate space of |root_view|.
+  void QueryClient(RenderWidgetHostViewBase* root_view,
+                   RenderWidgetHostViewBase* target,
+                   const blink::WebInputEvent& event,
+                   const ui::LatencyInfo& latency);
+
+  // |event| is in the coordinate space of |root_view|.
+  void FoundFrameSinkId(base::WeakPtr<RenderWidgetHostViewBase> root_view,
+                        base::WeakPtr<RenderWidgetHostViewBase> target,
+                        const blink::WebInputEvent& event,
+                        const ui::LatencyInfo& latency,
+                        const viz::FrameSinkId& frame_sink_id);
+
+  // |event| is in the coordinate space of |root_view|.
+  void FoundTarget(RenderWidgetHostViewBase* root_view,
+                   RenderWidgetHostViewBase* target,
+                   const blink::WebInputEvent& event,
+                   const ui::LatencyInfo& latency);
+
+  struct TargetingRequest {
+    TargetingRequest();
+    TargetingRequest(TargetingRequest&& request);
+    TargetingRequest& operator=(TargetingRequest&& other);
+    ~TargetingRequest();
+
+    base::WeakPtr<RenderWidgetHostViewBase> root_view;
+    ui::WebScopedInputEvent event;
+    ui::LatencyInfo latency;
+  };
+
+  bool request_in_flight_ = false;
+  std::queue<TargetingRequest> requests_;
+
+  Delegate* const delegate_;
+  base::WeakPtrFactory<RenderWidgetTargeter> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(RenderWidgetTargeter);
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_RENDERER_HOST_RENDER_WIDGET_TARGETER_H_
