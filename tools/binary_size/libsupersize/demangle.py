1f047235e3c56138493f01daa895cf42ec41d9e7
Samuel Huang <huangs@chromium.org>
2017-12-22 15:56:34 -0500

    [Supersize] Refactor: Use c++filt to demangle nm symbols.
    Previously nm symbols were demangles using the '--demangle' option. This
    CL removes this, and uses external calls c++filt to demangle instead.
    This is a prerequisite to storing mangled symbols in .size files, which
    in turn, facilitates the upcoming transition from Gold to LLD.
    
    demangle.py is added to handle all demangle-related functionalities,
    with the exception of objdump demangling in supersize console.
    
    Bug:
    Change-Id: I1ab8deecf814282c9d4b459d8c591df4ea51d6ae

---
+++ tools/binary_size/libsupersize/demangle.py
@@ -0,0 +1,70 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Utilities for demangling C++ symbols."""
+
+import collections
+import logging
+import subprocess
+
+import path_util
+
+def _DemangleNames(names, tool_prefix):
+  """Uses c++filt to demangle a list of names."""
+  proc = subprocess.Popen([path_util.GetCppFiltPath(tool_prefix)],
+                          stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+  stdout = proc.communicate('\n'.join(names))[0]
+  assert proc.returncode == 0
+  return stdout.splitlines()
+
+
+def DemangleRemainingSymbols(raw_symbols, tool_prefix):
+  """Demangles any symbols that need it."""
+  to_process = [s for s in raw_symbols if s.full_name.startswith('_Z')]
+  if not to_process:
+    return
+
+  logging.info('Demangling %d symbols', len(to_process))
+  names = _DemangleNames((s.full_name for s in to_process), tool_prefix)
+  for i, name in enumerate(names):
+    to_process[i].full_name = name
+
+
+def DemangleSetsInDicts(key_to_names, tool_prefix):
+  """|key_to_names| is a dict from key to sets (or lists) of mangled names.
+     Demangles values as sets, and returns the result.
+  """
+  all_names = []
+  for _, names in key_to_names.iteritems():
+    all_names += [n for n in names if n.startswith('_Z')]
+  if not all_names:
+    return key_to_names
+
+  logging.info('Demangling %d values', len(all_names))
+  it = iter(_DemangleNames(all_names, tool_prefix))
+  ret = collections.defaultdict(set)
+  for key, names in key_to_names.iteritems():
+    ret[key] = set(next(it) if n.startswith('_Z') else n for n in names)
+  assert(next(it, None) is None)
+  return ret
+
+
+def DemangleKeysAndMergeLists(name_to_list, tool_prefix):
+  """|name_to_list| is a dict from mangled name to a list. Demangles keys and
+     returns a new dict with the new keys, and list values possibly merged
+     (unsorted with possible duplicates).
+  """
+  keys = [key for key, _ in name_to_list.iteritems()
+          if key.startswith('_Z')]
+  if not keys:
+    return name_to_list
+
+  logging.info('Demangling %d keys', len(keys))
+  key_iter = iter(_DemangleNames(keys, tool_prefix))
+  ret = collections.defaultdict(list)
+  for key, val in name_to_list.iteritems():
+    ret[next(key_iter) if key.startswith('_Z') else key] += val
+  assert(next(key_iter, None) is None)
+  logging.info('* %d keys become %d keys' % (len(name_to_list), len(ret)))
+  return ret
