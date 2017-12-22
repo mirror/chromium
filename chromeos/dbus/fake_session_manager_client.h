60f321130bb06e3b8cbd1571fcd3d2d161e293a9
Qijiang Fan <fqj@chromium.org>
2017-12-22 12:37:39 +0100

    merge SessionManagerClientStubImpl into FakeSessionManagerClient.
    Bug: 429094
    Change-Id: I6ec7425d0bd771c6a23a596c05b00fbb8f73f6e2

--- chromeos/dbus/fake_session_manager_client.h
+++ chromeos/dbus/fake_session_manager_client.h
@@ -23,6 +23,8 @@ namespace chromeos {
 class FakeSessionManagerClient : public SessionManagerClient {
  public:
   FakeSessionManagerClient();
+  explicit FakeSessionManagerClient(bool emit_observers,
+                                    bool use_host_stub_policy);
   ~FakeSessionManagerClient() override;
 
   // SessionManagerClient overrides
@@ -176,6 +178,7 @@ class FakeSessionManagerClient : public SessionManagerClient {
   int request_lock_screen_call_count_;
   int notify_lock_screen_shown_call_count_;
   int notify_lock_screen_dismissed_call_count_;
+  bool screen_is_locked_;
 
   bool arc_available_;
   base::TimeTicks arc_start_time_;
@@ -183,6 +186,14 @@ class FakeSessionManagerClient : public SessionManagerClient {
   bool low_disk_ = false;
   // Pseudo running container id. If not running, empty.
   std::string container_instance_id_;
+
+  StubDelegate* delegate_;
+
+  // Whether to use the device policy on the harddisk of the
+  // running device.
+  bool use_host_stub_policy_;
+  // Whether to emit signal to observers.
+  bool emit_observers_;
 
   base::WeakPtrFactory<FakeSessionManagerClient> weak_ptr_factory_;
   DISALLOW_COPY_AND_ASSIGN(FakeSessionManagerClient);
