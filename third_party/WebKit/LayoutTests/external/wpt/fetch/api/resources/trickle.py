b442c68e714d620d719c9a06e59eb289f437443b
Yutaka Hirano <yhirano@chromium.org>
2017-12-25 10:00:33 +0900

    trickle.py
    Bug: 695939
    Change-Id: I27a92003138a8791e1d9bd7622d79ce795d7a1bd

--- third_party/WebKit/LayoutTests/external/wpt/fetch/api/resources/trickle.py
+++ third_party/WebKit/LayoutTests/external/wpt/fetch/api/resources/trickle.py
@@ -1,12 +1,14 @@
 import time
 
 def main(request, response):
-    delay = float(request.GET.first("ms", 500)) / 1E3
-    count = int(request.GET.first("count", 50))
+    content = 'TEST_TRICKLE\n'
+    delay = float(request.GET.first('ms', 500)) / 1E3
+    count = int(request.GET.first('count', 50))
     time.sleep(delay)
-    response.headers.set("Content-type", "text/plain")
+    response.headers.set('content-type', "text/plain")
+    response.headers.set('content-length', count * len(content))
     response.write_status_headers()
     time.sleep(delay);
     for i in xrange(count):
-        response.writer.write_content("TEST_TRICKLE\n")
+        response.writer.write_content(content)
         time.sleep(delay)
