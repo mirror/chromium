51f4edf4b4c448f1e58e0928e18e28b5fad39f2d
Wei Li <weili@chromium.org>
2017-12-27 15:34:20 -0800

    Enable printing remote frame in Blink
    This CL made plumbing changes in Blink to print remote frame. Adds
    RemoteFramePainter to paint remote frame, and adds the plumbing to
    bubble up the remote frame print command to browser. The browser
    side of change is at https://crrev.com/c/822155.
    
    The high level design doc is at goo.gl/CF1RUy
    
    BUG=455764
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Iea35e06649b20521c96f4f3a620d25d68040ec5d

--- third_party/WebKit/Source/core/loader/EmptyClients.h
+++ third_party/WebKit/Source/core/loader/EmptyClients.h
@@ -444,6 +444,7 @@ class CORE_EXPORT EmptyRemoteFrameClient : public RemoteFrameClient {
   void SetIsInert(bool) override {}
   void UpdateRenderThrottlingStatus(bool is_throttled,
                                     bool subtree_throttled) override {}
+  void Print(const IntRect& rect, uint64_t content_id) const override {}
 
   // FrameClient implementation.
   bool InShadowTree() const override { return false; }
