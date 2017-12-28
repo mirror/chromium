7cf7bd6f0a44ffd53ee56f2309fde13903efb0d2
Sergey Ulanov <sergeyu@chromium.org>
2017-12-28 14:49:19 -0800

    [Fuchsia] Use Mesa as GL implementation for headless Ozone
    Previously all layout tests were failing because
    HeadlessSurfaceFactory::GetGLOzone() was always returning nullptr.
    
    Bug: 778467
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Id0ec9576d9972dedf7d5f6cb5953aef5f0dbfc3e

--- ui/gl/gl_implementation_osmesa.cc
+++ ui/gl/gl_implementation_osmesa.cc
@@ -8,6 +8,7 @@
 #include "base/logging.h"
 #include "base/native_library.h"
 #include "base/path_service.h"
+#include "build/build_config.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_gl_api_implementation.h"
 #include "ui/gl/gl_implementation.h"
@@ -17,10 +18,14 @@ namespace gl {
 
 bool InitializeStaticGLBindingsOSMesaGL() {
   base::FilePath module_path;
+
+#if !defined(OS_FUCHSIA)
+  // On Fuchsia use relative path and let the loader service find libosmesa.so.
   if (!PathService::Get(base::DIR_MODULE, &module_path)) {
     LOG(ERROR) << "PathService::Get failed.";
     return false;
   }
+#endif  // !defined(OS_FUCHSIA)
 
   base::FilePath library_path = module_path.Append("libosmesa.so");
   base::NativeLibrary library = LoadLibraryAndPrintError(library_path);
