fae5817e8cd70a0ed430b2baceeadd8e28aff2e7
Egor Pasko <pasko@chromium.org>
2017-12-29 15:24:59 +0100

    Add Startup.BrowserMessageLoopStartTimeFromMainEntry3
    Keep the old Startup.BrowserMessageLoopStartTimeFromMainEntry2 for
    comparison on benchmarks and in the field. The new histogram records the
    same value, but is based solely on TimeTicks::Now() instead of going
    through wallclock-to-ticks conversion.
    
    Bug: 797762
    Change-Id: I364f5c8011f3883fc95bebff667dcffae4e56294

--- components/startup_metric_utils/browser/startup_metric_utils.h
+++ components/startup_metric_utils/browser/startup_metric_utils.h
@@ -52,11 +52,14 @@ void RecordStartupProcessCreationTime(base::Time time);
 // Call this with a time recorded as early as possible in the startup process.
 // On Android, the entry point time is the time at which the Java code starts.
 // In Mojo, the entry point time is the time at which the shell starts.
-void RecordMainEntryPointTime(base::Time time);
+// TODO(pasko): Remove the wallclock-based main entry point time calculation,
+// see: http://crbug.com/797762.
+void RecordMainEntryPointTime(base::Time wall_time, base::TimeTicks ticks);
 
 // Call this with the time when the executable is loaded and main() is entered.
-// Can be different from |RecordMainEntryPointTime| when the startup process is
-// contained in a separate dll, such as with chrome.exe / chrome.dll on Windows.
+// Can be different from |RecordMainEntryPointTime| when the startup
+// process is contained in a separate dll, such as with chrome.exe / chrome.dll
+// on Windows.
 void RecordExeMainEntryPointTicks(base::TimeTicks time);
 
 // Call this with the time recorded just before the message loop is started.
@@ -106,8 +109,9 @@ void RecordBrowserWindowFirstPaint(base::TimeTicks ticks);
 void RecordBrowserWindowFirstPaintCompositingEnded(base::TimeTicks ticks);
 
 // Returns the TimeTicks corresponding to main entry as recorded by
-// RecordMainEntryPointTime. Returns a null TimeTicks if a value has not been
-// recorded yet. This method is expected to be called from the UI thread.
+// |RecordMainEntryPointTimeWallClock|. Returns a null TimeTicks if a value has
+// not been recorded yet. This method is expected to be called from the UI
+// thread.
 base::TimeTicks MainEntryPointTicks();
 
 }  // namespace startup_metric_utils
