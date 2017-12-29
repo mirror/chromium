12de1b7ffe4b9c8a95c51d8bda5d08c389ca6948
Nate Fischer <ntfschr@chromium.org>
2017-12-28 18:37:40 -0800

    AW: cleanup malformed setCookie calls
    No intended change to observable behavior.
    
    Some applications invoke CookieManager#setCookie() with malformed URLs
    (e.g. ".some.domain" and "http://.some.domain"). This hasn't been
    explicitly supported, but has worked by accident (although it hits a
    DCHECK).
    
    This CL explicitly handles these invalid URLs by creating a valid URL
    and adding a Domain attribute for the intended domain, matching current
    behavior while no only passing well-formed URLs to the rest of chromium.
    
    In the case of a malformed URL and an explicit Domain attribute, we
    prefer the Domain attribute (also following current behavior).
    
    This adds tests to make sure we don't regress and hit the DCHECK again.
    
    Bug: 779887
    Test: run_webview_instrumentation_test_apk -f CookieManagerTest#testSetCookieWithDomainForUrl
    Test: manual - quora app (see bug for steps)
    Change-Id: Ia1bdd6c40be84d8f5816e5879a31e9e47ec21e93

--- android_webview/javatests/src/org/chromium/android_webview/test/CookieManagerTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/CookieManagerTest.java
@@ -174,6 +174,37 @@ public class CookieManagerTest {
     @Test
     @MediumTest
     @Feature({"AndroidWebView", "Privacy"})
+    public void testSetCookieWithDomainForUrl() throws Throwable {
+        // If the app passes ".www.example.com" or "http://.www.example.com", the glue layer "fixes"
+        // this to "http:///.www.example.com"
+        String url = "http:///.www.example.com";
+        String cookie = "name=test";
+        mCookieManager.setCookie(url, cookie);
+        Assert.assertEquals(cookie, mCookieManager.getCookie(url));
+
+        TestCallback<Boolean> callback = new TestCallback<Boolean>();
+        int callCount = callback.getOnResultHelper().getCallCount();
+        removeAllCookiesOnUiThread(callback);
+        callback.getOnResultHelper().waitForCallback(callCount);
+
+        mCookieManager.setCookie(url, cookie + "; Domain=.example.com");
+        Assert.assertEquals(cookie, mCookieManager.getCookie(url));
+        // The Domain attribute should take precedence over a domain-ish URL
+        Assert.assertEquals(cookie, mCookieManager.getCookie("http://example.com"));
+
+        callback = new TestCallback<Boolean>();
+        callCount = callback.getOnResultHelper().getCallCount();
+        removeAllCookiesOnUiThread(callback);
+        callback.getOnResultHelper().waitForCallback(callCount);
+
+        mCookieManager.setCookie(url, cookie + ";");
+        Assert.assertEquals(cookie, mCookieManager.getCookie(url));
+        Assert.assertNull(mCookieManager.getCookie("http://example.com"));
+    }
+
+    @Test
+    @MediumTest
+    @Feature({"AndroidWebView", "Privacy"})
     public void testSetSecureCookieForHttpUrl() throws Throwable {
         String url = "http://www.example.com";
         String secureUrl = "https://www.example.com";
