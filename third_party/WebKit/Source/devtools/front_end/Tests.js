d9cf8527b9bb25acc817deef47587414250e1cad
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-01-02 11:45:22 -0800

    [DevTools] debug WorkerDevTools...PauseInSharedWorkerInitialization
    Bug:chromium:432444
    Change-Id: I4e703013cc1c0ddb243bf18e95eda199da618859

--- third_party/WebKit/Source/devtools/front_end/Tests.js
+++ third_party/WebKit/Source/devtools/front_end/Tests.js
@@ -1202,7 +1202,7 @@
       if (SDK.targetManager.targets().length >= n)
         callback.call(null);
       else
-        this.addSniffer(SDK.TargetManager.prototype, 'addTarget', checkTargets.bind(this));
+        this.addSniffer(SDK.TargetManager.prototype, 'createTarget', checkTargets.bind(this));
     }
   };
 
