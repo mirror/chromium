201e2330fff8e2ded0b7ab346864b9e50bcc2086
Misha Efimov <mef@chromium.org>
2018-01-02 13:11:18 -0500

    [Cronet] Extract common CronetURLRequestContext from Android CronetURLRequestContextAdapter.
    Note:
    - Patchset 1 copies android/cronet_url_request_context_adapter.* into cronet_url_request_context.*
    - android/cronet_url_request_context_adapter.* use cronet_url_request_context.*
    - cronet_global_state.h defines cross-platform API used to initialize and maintain Cronet state shared by multiple engines.
    
    Bug:786559
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I1cbe2162584745580e54bd0838a4a2303d7e6970

---
+++ components/cronet/cronet_global_state.h
@@ -0,0 +1,28 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_CRONET_CRONET_GLOBAL_STATE_H_
+#define COMPONENTS_CRONET_CRONET_GLOBAL_STATE_H_
+
+#include <string>
+#include "base/sequenced_task_runner.h"
+
+namespace net {
+class ProxyConfigService;
+}  // namespace net
+
+namespace cronet {
+namespace global_state {
+
+bool IsOnInitThread();
+
+// Creates a proxy config service appropriate for this platform that fetches the
+// system proxy settings.
+std::unique_ptr<net::ProxyConfigService> CreateProxyConfigService(
+    const scoped_refptr<base::SequencedTaskRunner>& io_task_runner);
+
+}  // namespace global_state
+}  // namespace cronet
+
+#endif  // COMPONENTS_CRONET_CRONET_GLOBAL_STATE_H_
