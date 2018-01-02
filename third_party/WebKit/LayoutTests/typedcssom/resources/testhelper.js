1a5d939879867b996587f5b0dd17b2943e38f12b
Darren Shen <shend@chromium.org>
2018-01-02 18:07:06 +1100

    assert
    Bug:
    Change-Id: I0d493ba839dd09939279294952a6b4b3ffcf6508

--- third_party/WebKit/LayoutTests/typedcssom/resources/testhelper.js
+++ third_party/WebKit/LayoutTests/typedcssom/resources/testhelper.js
@@ -1,66 +1,88 @@
-// Compares two CSSStyleValues to check if they're the same type
-// and have the same attributes.
-function assert_style_value_equals(a, b) {
-  if (a == null || b == null) {
-    assert_equals(a, b);
-    return;
-  }
+const gStyleValueAttributes = {
+  CSSKeywordValue: ['value']
+};
 
-  assert_equals(a.constructor.name, b.constructor.name);
-  const className = a.constructor.name;
-  switch (className) {
-    case 'CSSKeywordValue':
-      assert_equals(a.value, b.value);
-      break;
-    case 'CSSUnitValue':
-      assert_approx_equals(a.value, b.value, 1e-6);
-      assert_equals(a.unit, b.unit);
-      break;
-    case 'CSSMathSum':
-    case 'CSSMathProduct':
-    case 'CSSMathMin':
-    case 'CSSMathMax':
-      assert_style_value_array_equals(a.values, b.values);
-      break;
-    case 'CSSMathInvert':
-    case 'CSSMathNegate':
-      assert_style_value_equals(a.value, b.value);
-      break;
-    case 'CSSUnparsedValue':
-      assert_style_value_array_equals(a, b);
-      break;
-    case 'CSSVariableReferenceValue':
-      assert_equals(a.variable, b.variable);
-      assert_style_value_equals(a.fallback, b.fallback);
-      break;
-    case 'CSSTransformValue':
-      assert_style_value_array_equals(a, b);
-      break;
-    case 'CSSRotation':
-      assert_style_value_equals(a.angle, b.angle);
-      // fallthrough
-    case 'CSSTranslation':
-    case 'CSSScale':
-      assert_style_value_equals(a.x, b.x);
-      assert_style_value_equals(a.y, b.y);
-      assert_style_value_equals(a.z, b.z);
-      assert_style_value_equals(a.is2D, b.is2D);
-      break;
-    case 'CSSSkew':
-      assert_style_value_equals(a.ax, b.ax);
-      assert_style_value_equals(a.ay, b.ay);
-      break;
-    case 'CSSPerspective':
-      assert_style_value_equals(a.length, b.length);
-      break;
-    case 'CSSMatrixComponent':
-      assert_matrix_approx_equals(a.matrix, b.matrix, 1e-6);
-      break;
-    default:
-      assert_equals(a, b);
-      break;
+let Matchers = {};
+for (const [name, attributes] of Object.entries(gStyleValueAttributes)) {
+  const matcher_name = name.startsWith('CSS') ? name.slice(3) : name;
+  Matchers[matcher_name] = (...args) => {
+    const attributes = gStyleValueAttributes[name];
+    let matcher = { name: name };
+    for (let i = 0; i < attributes.length; i++) {
+      matcher[attributes[i]] = args[i];
+    }
+    return matcher;
+  };
+}
+
+function value_repr(value) {
+  if (value == null)
+    return 'null';
+
+  if (typeof(value) == 'string')
+    return '"' + value + '"';
+
+  if (value instanceof Array)
+    return '[' + value.map(value_repr).join(', ') + ']';
+
+  if (typeof(value) == 'object') {
+    const class_name = value.constructor == Object ? value.name : value.constructor.name;
+    const attributes = gStyleValueAttributes[class_name];
+
+    if (!attributes)
+      return class_name + '()';
+
+    return class_name + '(' + attributes.map(attr => attr + '=' + value_repr(value[attr])).join(', ') + ')';
   }
 }
+
+function assert_matches(value, matcher) {
+  assert_true(value_matches(value, matcher),
+    'Expected ' + value_repr(value) + ', but got ' + value_repr(matcher));
+}
+
+function value_matches(value, matcher) {
+  if (value == null && matcher == null)
+    return true;
+  if (value == null || matcher == null)
+    return false;
+
+  if (typeof(value) == 'string' && typeof(matcher) == 'string')
+    return value === matcher;
+
+  if (value instanceof Array && matcher instanceof Array) {
+    if (value.length != matcher.length)
+      return false;
+
+    for (let i = 0; i < value.length; i++) {
+      if (!value_matches(value[i], matcher[i]))
+        return false;
+    }
+
+    return true;
+  }
+
+  if (typeof(value) == 'object' && typeof(matcher) == 'object') {
+    const class_name = value.constructor.name;
+    if (class_name != matcher.name)
+      return false;
+
+    const attributes = gStyleValueAttributes[class_name];
+    if (!attributes)
+      return false;
+
+    for (const attribute of attributes) {
+      if (!value_matches(value[attribute], matcher[attribute]))
+        return false;
+    }
+
+    return true;
+  }
+
+  return false;
+}
+
+/*
 
 // Compares two arrays of CSSStyleValues to check if every element is equal
 function assert_style_value_array_equals(a, b) {
@@ -69,7 +91,7 @@ function assert_style_value_array_equals(a, b) {
     assert_style_value_equals(a[i], b[i]);
   }
 }
-
+*/
 const gValidUnits = [
   'number', 'percent', 'em', 'ex', 'ch',
   'ic', 'rem', 'lh', 'rlh', 'vw',
