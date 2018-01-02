1c4abf08ae8f660d423b93742e4d4546dc8d2517
Peter E Conn <peconn@chromium.org>
2018-01-02 10:42:40 +0000

    📺 Dismiss InfoBars before entering PiP.
    Bug: 793267
    Change-Id: Ia3bfa5852b2d00031a8467e75b4ee9f686397660

--- chrome/android/java/src/org/chromium/chrome/browser/media/PictureInPictureController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/PictureInPictureController.java
@@ -185,10 +185,14 @@ public class PictureInPictureController {
 
         webContents.setHasPersistentVideo(true);
 
+        // We don't want InfoBars displaying while in PiP, they cover too much content.
+        activity.getActivityTab().getInfoBarContainer().setIsObscuredByOtherView(true);
+
         mOnLeavePipCallbacks.add(new Callback<ChromeActivity>() {
             @Override
             public void onResult(ChromeActivity activity2) {
                 webContents.setHasPersistentVideo(false);
+                activity.getActivityTab().getInfoBarContainer().setIsObscuredByOtherView(false);
             }
         });
 
