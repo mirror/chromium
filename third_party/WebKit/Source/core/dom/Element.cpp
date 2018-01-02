adacf91d5d3fe1e56b2fb54043cfd9ad781f0e55
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2018-01-02 10:52:38 +0800

    Rename LayoutObjectIsFocusable to IsFocusableStyle
    As the comment in Element.h by tkent@,
    rename LayoutObjectIsFocusable to IsFocusableStyle.
    
    BUG=
    R=tkent@chromium.org
    
    Signed-off-by: zhuoyu.qian <zhuoyu.qian@samsung.com>
    Change-Id: If1bb004daefe29cb7bac05e8072a468412b0e224

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -239,7 +239,7 @@ int Element::tabIndex() const {
              : 0;
 }
 
-bool Element::LayoutObjectIsFocusable() const {
+bool Element::IsFocusableStyle() const {
   // Elements in canvas fallback content are not rendered, but they are allowed
   // to be focusable as long as their canvas is displayed and visible.
   if (IsInCanvasSubtree()) {
@@ -3019,8 +3019,7 @@ bool Element::IsFocusable() const {
   // needsLayoutTreeUpdateForNode check is invalid.
   DCHECK(!GetDocument().IsActive() ||
          !GetDocument().NeedsLayoutTreeUpdateForNode(*this));
-  return isConnected() && SupportsFocus() && !IsInert() &&
-         LayoutObjectIsFocusable();
+  return isConnected() && SupportsFocus() && !IsInert() && IsFocusableStyle();
 }
 
 bool Element::IsKeyboardFocusable() const {
