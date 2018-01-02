9eaa564ebb15c303b450d2be13be6a9d506b3dbb
Gauthier Ambard <gambard@chromium.org>
2018-01-02 17:10:24 +0100

    Create a PrimaryToolbar protocol
    This CL creates an AbstractPrimaryToolbar protocol and moves some of
    the methods from the LegacyToolbarCoordinator to it.
    
    It also split the uses of the toolbar coordinator in the BVC in three:
    - The PrimaryToolbarCoordinator, for the calls only related to the
       primary toolbar.
    - The LegacyToolbarCoordinator, for the calls which will be deleted
       once the old toolbar is removed.
    - The _toolbarCoordinator (current) for the calls which might be going
       to both toolbars or undecided yet.
    
    This separation makes things clearer in BVC. It will allow an easier
    transition to the adaptive toolbar as the responsibilities will be
    cleaner.
    
    It also moves the OmniboxFocuser and SideSwipeToolbarInteracting
    protocols to the toolbar/public folder as they are used by the
    AbstractPrimaryToolbar.
    
    Bug:798418
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I18822be75c483af7f1b810b66c450f521f720b37

---
+++ ios/chrome/browser/ui/toolbar/public/abstract_primary_toolbar.h
@@ -0,0 +1,43 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_TOOLBAR_PUBLIC_ABSTRACT_PRIMARY_TOOLBAR_H_
+#define IOS_CHROME_BROWSER_UI_TOOLBAR_PUBLIC_ABSTRACT_PRIMARY_TOOLBAR_H_
+
+#import "ios/chrome/browser/ui/toolbar/public/omnibox_focuser.h"
+#import "ios/chrome/browser/ui/toolbar/public/side_swipe_toolbar_interacting.h"
+
+@protocol QRScannerResultLoading;
+@protocol TabHistoryPositioner;
+@protocol TabHistoryUIUpdater;
+@protocol VoiceSearchControllerDelegate;
+@protocol WebToolbarDelegate;
+
+// Protocol defining a primary toolbar, in a paradigm where the toolbar can be
+// split between primary and secondary.
+@protocol AbstractPrimaryToolbar<OmniboxFocuser, SideSwipeToolbarInteracting>
+
+// Returns the different protocols and superclass now implemented by the
+// internal ViewController.
+- (id<VoiceSearchControllerDelegate>)voiceSearchDelegate;
+- (id<QRScannerResultLoading>)QRScannerResultLoader;
+- (id<TabHistoryPositioner>)tabHistoryPositioner;
+- (id<TabHistoryUIUpdater>)tabHistoryUIUpdater;
+
+// The toolbar ViewController.
+@property(nonatomic, strong) UIViewController* toolbarViewController;
+
+// Sets the delegate for the toolbar.
+- (void)setToolbarDelegate:(id<WebToolbarDelegate>)delegate;
+
+// Show the animation when transitioning to a prerendered page.
+- (void)showPrerenderingAnimation;
+// Whether the omnibox is currently the first responder.
+- (BOOL)isOmniboxFirstResponder;
+// Whether the omnibox popup is currently presented.
+- (BOOL)showingOmniboxPopup;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_TOOLBAR_PUBLIC_ABSTRACT_PRIMARY_TOOLBAR_H_
