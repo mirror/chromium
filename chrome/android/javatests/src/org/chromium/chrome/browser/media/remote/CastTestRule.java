a70e146e9d1c02539c90bbfc6fb53d3011e3aad0
Eric Karl <ericrk@chromium.org>
2018-01-02 15:27:20 -0800

    Improve timing of placeholder image on Chrome resume
    Currently, when Chrome is foregrounded, Android OS shows a placeholder
    image of the last screen rendered. It does this until Chrome fires the
    surfaceRedrawAsync callback. Chrome currently does this as soon as it
    produces its first frame after foregrounding.
    
    Unfortunately, Chrome doesn't wait for renderer content before
    producing a frame, so the first frame may include a blank renderer.
    This is especially common on low-end devices which produce frames
    more slowly. In these cases, we see the placeholder, then a blank
    renderer, then a populated renderer, which produces a flickering
    effect.
    
    This CL ports the ui/compositor's compositor lock to the android
    CompositorImpl. We take this lock when a renderer first connects to the
    browser, and release it when the renderer produces a frame. This
    prevents the browser from producing a frame until it has renderer
    content (or a failsafe timeout has been reached).
    
    Note that the VrCompositor used to provide similar functionality by
    calling DeferComimits direclty. This code wasn't working as intended
    and after discussion with the VR team has been removed.
    
    Bug:792120
    Change-Id: I011428bfb75e7455aae00426258ea05daf89fb38

--- chrome/android/javatests/src/org/chromium/chrome/browser/media/remote/CastTestRule.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/media/remote/CastTestRule.java
@@ -95,7 +95,7 @@ public class CastTestRule extends ChromeActivityTestRule<ChromeActivity> {
     public static final String VIDEO_ELEMENT = "video";
 
     // Max time to open a view.
-    public static final int MAX_VIEW_TIME_MS = 10000;
+    public static final int MAX_VIEW_TIME_MS = 5000;
 
     // Time to let a video run to ensure that it has started.
     public static final int RUN_TIME_MS = 1000;
@@ -279,7 +279,12 @@ public class CastTestRule extends ChromeActivityTestRule<ChromeActivity> {
                 mMediaRouteController.addUiListener(new TestListener());
             }
         });
+
+        Log.e(TAG, "EK: about to click button");
+
         tapCastButton(tab, videoRect);
+
+        Log.e(TAG, "EK: clicked buttn");
 
         // Wait for the test device to appear in the device list.
         try {
