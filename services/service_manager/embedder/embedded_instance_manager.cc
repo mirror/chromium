e99fb4f000afdd36bd7aefe2a8fc6ba94302d429
Christian Fremerey <chfremer@chromium.org>
2018-01-02 15:44:56 -0800

    Simplify/Clarify EmbeddedInstanceManager
    * Functional Change:
      EmbeddedInstanceManager no longer blocks the "owner's thread" during
      calls to ShutDown (or when connection to the last ServiceContext is lost)
      when using a dedicated thread. Instead, the thread join is scheduled to a
      background task.
    * Clarification of Behavior:
      EmbeddedInstanceManager always invokes the given QuitClosure when either
      ShutDown() is called or the connection to the last ServiceContext is lost.
    * Removed unit test that verified the blocking behavior
    * Added unit tests to cover the actual behavior of the class.
    * Added class-level content describing the actual behavior of the class.
    * Change names of methods/variables to better reveal the behavior/intent.
    
    Change-Id: I6fd3daf6d13c212268ed18faeaef1ad6695de86f

--- services/service_manager/embedder/embedded_instance_manager.cc
+++ services/service_manager/embedder/embedded_instance_manager.cc
@@ -7,6 +7,7 @@
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
 #include "base/single_thread_task_runner.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_task_runner_handle.h"
 
@@ -15,59 +16,64 @@ namespace service_manager {
 EmbeddedInstanceManager::EmbeddedInstanceManager(
     const base::StringPiece& name,
     const EmbeddedServiceInfo& info,
-    const base::Closure& quit_closure)
+    base::OnceClosure quit_closure)
     : name_(name.as_string()),
       factory_callback_(info.factory),
-      use_own_thread_(!info.task_runner && info.use_own_thread),
+      use_dedicated_thread_(!info.task_runner && info.use_own_thread),
       message_loop_type_(info.message_loop_type),
       thread_priority_(info.thread_priority),
-      quit_closure_(quit_closure),
-      quit_task_runner_(base::ThreadTaskRunnerHandle::Get()),
+      quit_closure_(std::move(quit_closure)),
+      owner_task_runner_(base::ThreadTaskRunnerHandle::Get()),
       service_task_runner_(info.task_runner) {
-  if (!use_own_thread_ && !service_task_runner_)
+  if (!use_dedicated_thread_ && !service_task_runner_)
     service_task_runner_ = base::ThreadTaskRunnerHandle::Get();
 }
 
 void EmbeddedInstanceManager::BindServiceRequest(
     service_manager::mojom::ServiceRequest request) {
-  DCHECK_CALLED_ON_VALID_THREAD(runner_thread_checker_);
+  DCHECK(owner_task_runner_->RunsTasksInCurrentSequence());
 
-  if (use_own_thread_ && !thread_) {
+  if (use_dedicated_thread_ && !optional_dedicated_service_thread_) {
     // Start a new thread if necessary.
-    thread_.reset(new base::Thread(name_));
+    optional_dedicated_service_thread_.reset(new base::Thread(name_));
     base::Thread::Options options;
     options.message_loop_type = message_loop_type_;
     options.priority = thread_priority_;
-    thread_->StartWithOptions(options);
-    service_task_runner_ = thread_->task_runner();
+    optional_dedicated_service_thread_->StartWithOptions(options);
+    service_task_runner_ = optional_dedicated_service_thread_->task_runner();
   }
 
   DCHECK(service_task_runner_);
-  service_task_runner_->PostTask(
-      FROM_HERE,
-      base::Bind(&EmbeddedInstanceManager::BindServiceRequestOnServiceSequence,
-                 this, base::Passed(&request)));
-}
-
-void EmbeddedInstanceManager::ShutDown() {
-  DCHECK_CALLED_ON_VALID_THREAD(runner_thread_checker_);
-  if (!service_task_runner_)
-    return;
-  // Any extant ServiceContexts must be destroyed on the application thread.
   if (service_task_runner_->RunsTasksInCurrentSequence()) {
-    QuitOnServiceSequence();
+    BindServiceRequestOnServiceSequence(std::move(request));
   } else {
     service_task_runner_->PostTask(
         FROM_HERE,
-        base::Bind(&EmbeddedInstanceManager::QuitOnServiceSequence, this));
+        base::BindOnce(
+            &EmbeddedInstanceManager::BindServiceRequestOnServiceSequence, this,
+            base::Passed(&request)));
   }
-  thread_.reset();
+}
+
+void EmbeddedInstanceManager::ShutDown() {
+  DCHECK(owner_task_runner_->RunsTasksInCurrentSequence());
+  if (!service_task_runner_)
+    return;
+  // Any extant ServiceContexts must be destroyed on |service_task_runner_|.
+  if (service_task_runner_->RunsTasksInCurrentSequence()) {
+    QuitOnServiceSequence(std::move(optional_dedicated_service_thread_));
+  } else {
+    service_task_runner_->PostTask(
+        FROM_HERE,
+        base::BindOnce(&EmbeddedInstanceManager::QuitOnServiceSequence, this,
+                       std::move(optional_dedicated_service_thread_)));
+  }
 }
 
 EmbeddedInstanceManager::~EmbeddedInstanceManager() {
   // If this instance had its own thread, it MUST be explicitly destroyed by
-  // QuitOnRunnerThread() by the time this destructor is run.
-  DCHECK(!thread_);
+  // QuitOnOwnerThread() by the time this destructor is run.
+  DCHECK(!optional_dedicated_service_thread_);
 }
 
 void EmbeddedInstanceManager::BindServiceRequestOnServiceSequence(
@@ -75,54 +81,70 @@ void EmbeddedInstanceManager::BindServiceRequestOnServiceSequence(
   DCHECK(service_task_runner_->RunsTasksInCurrentSequence());
 
   int instance_id = next_instance_id_++;
-
-  std::unique_ptr<service_manager::ServiceContext> context =
-      std::make_unique<service_manager::ServiceContext>(factory_callback_.Run(),
-                                                        std::move(request));
-
-  service_manager::ServiceContext* raw_context = context.get();
-  context->SetQuitClosure(
-      base::Bind(&EmbeddedInstanceManager::OnInstanceLost, this, instance_id));
-  contexts_.insert(std::make_pair(raw_context, std::move(context)));
-  id_to_context_map_.insert(std::make_pair(instance_id, raw_context));
+  auto context = std::make_unique<service_manager::ServiceContext>(
+      factory_callback_.Run(), std::move(request));
+  context->SetQuitClosure(base::BindRepeating(
+      &EmbeddedInstanceManager::OnInstanceLost, this, instance_id));
+  id_to_context_map_.insert(std::make_pair(instance_id, std::move(context)));
 }
 
 void EmbeddedInstanceManager::OnInstanceLost(int instance_id) {
   DCHECK(service_task_runner_->RunsTasksInCurrentSequence());
 
-  auto id_iter = id_to_context_map_.find(instance_id);
-  CHECK(id_iter != id_to_context_map_.end());
-
-  auto context_iter = contexts_.find(id_iter->second);
-  CHECK(context_iter != contexts_.end());
-  contexts_.erase(context_iter);
-  id_to_context_map_.erase(id_iter);
+  auto context_iter = id_to_context_map_.find(instance_id);
+  CHECK(context_iter != id_to_context_map_.end());
+  id_to_context_map_.erase(context_iter);
 
   // If we've lost the last instance, run the quit closure.
-  if (contexts_.empty())
-    QuitOnServiceSequence();
+  if (id_to_context_map_.empty())
+    QuitOnServiceSequence(std::move(optional_dedicated_service_thread_));
 }
 
-void EmbeddedInstanceManager::QuitOnServiceSequence() {
+void EmbeddedInstanceManager::QuitOnServiceSequence(
+    std::unique_ptr<base::Thread> optional_dedicated_service_thread) {
   DCHECK(service_task_runner_->RunsTasksInCurrentSequence());
 
-  contexts_.clear();
-  if (quit_task_runner_->RunsTasksInCurrentSequence()) {
-    QuitOnRunnerThread();
-  } else {
-    quit_task_runner_->PostTask(
+  id_to_context_map_.clear();
+
+  if (optional_dedicated_service_thread) {
+    scoped_refptr<base::TaskRunner> sequenced_task_runner =
+        base::CreateSequencedTaskRunnerWithTraits(
+            {base::TaskPriority::BACKGROUND, base::MayBlock()});
+    sequenced_task_runner->PostTask(
         FROM_HERE,
-        base::Bind(&EmbeddedInstanceManager::QuitOnRunnerThread, this));
+        base::BindOnce(
+            &EmbeddedInstanceManager::JoinAndReleaseDedicatedServiceThread,
+            this, std::move(optional_dedicated_service_thread)));
+    sequenced_task_runner->PostTask(
+        FROM_HERE,
+        base::BindOnce(
+            &EmbeddedInstanceManager::RunOrPostQuitClosureOnOwnerTaskRunner,
+            this));
+  } else {
+    RunOrPostQuitClosureOnOwnerTaskRunner();
   }
 }
 
-void EmbeddedInstanceManager::QuitOnRunnerThread() {
-  DCHECK_CALLED_ON_VALID_THREAD(runner_thread_checker_);
-  if (thread_) {
-    thread_.reset();
-    service_task_runner_ = nullptr;
+void EmbeddedInstanceManager::JoinAndReleaseDedicatedServiceThread(
+    std::unique_ptr<base::Thread> thread) {
+  if (!thread)
+    return;
+
+  thread.reset();
+  service_task_runner_ = nullptr;
+}
+
+void EmbeddedInstanceManager::RunOrPostQuitClosureOnOwnerTaskRunner() {
+  if (!owner_task_runner_->RunsTasksInCurrentSequence()) {
+    owner_task_runner_->PostTask(
+        FROM_HERE,
+        base::BindOnce(
+            &EmbeddedInstanceManager::RunOrPostQuitClosureOnOwnerTaskRunner,
+            this));
+    return;
   }
-  quit_closure_.Run();
+
+  base::ResetAndReturn(&quit_closure_).Run();
 }
 
 }  // namespace service_manager
