b6e624135b290e239cd4f81e66a14fc608a35814
Xiaocheng Hu <xiaochengh@chromium.org>
2018-01-04 13:21:25 -0800

    [All-in-one] Mojofy spellcheck messages
    This patch mojofies all spellcheck messages on all platforms.
    
    This patch is not for landing. It will be split into smaller patches.
    
    Bug:
    Change-Id: I1a25307284b8973f0b39cae58dfa46b27232349a

--- chrome/browser/spellchecker/spellcheck_message_filter_platform_mac_unittest.cc
+++ chrome/browser/spellchecker/spellcheck_message_filter_platform_mac_unittest.cc
@@ -2,34 +2,32 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/spellcheck/browser/spellcheck_message_filter_platform.h"
+#include "chrome/browser/spellchecker/spell_check_host_chrome_impl.h"
+
+// TODO(xiaochengh): Rename to spell_check_host_chrome_impl_mac_unittest.cc
 
 #include <stddef.h>
 #include <stdint.h>
 
 #include "base/macros.h"
 #include "base/strings/utf_string_conversions.h"
-#include "components/spellcheck/common/spellcheck_messages.h"
 #include "components/spellcheck/common/spellcheck_result.h"
 #include "content/public/browser/browser_thread.h"
-#include "ipc/ipc_message.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
 
-TEST(SpellcheckMessageFilterPlatformMacTest, CombineResults) {
+TEST(SpellCheckHostChromeImplMacTest, CombineResults) {
   std::vector<SpellCheckResult> local_results;
   std::vector<SpellCheckResult> remote_results;
   base::string16 remote_suggestion = base::ASCIIToUTF16("remote");
   base::string16 local_suggestion = base::ASCIIToUTF16("local");
 
   // Remote-only result - must be flagged as GRAMMAR after combine
-  remote_results.push_back(
-      SpellCheckResult(SpellCheckResult::SPELLING, 0, 5));
+  remote_results.push_back(SpellCheckResult(SpellCheckResult::SPELLING, 0, 5));
 
   // Local-only result - must be discarded after combine
-  local_results.push_back(
-      SpellCheckResult(SpellCheckResult::SPELLING, 10, 5));
+  local_results.push_back(SpellCheckResult(SpellCheckResult::SPELLING, 10, 5));
 
   // local & remote result - must be flagged SPELLING, uses remote suggestion.
   SpellCheckResult result(SpellCheckResult::SPELLING, 20, 5, local_suggestion);
@@ -37,8 +35,7 @@ TEST(SpellcheckMessageFilterPlatformMacTest, CombineResults) {
   result.replacements[0] = remote_suggestion;
   remote_results.push_back(result);
 
-  SpellCheckMessageFilterPlatform::CombineResults(&remote_results,
-      local_results);
+  SpellCheckHostChromeImpl::CombineResults(&remote_results, local_results);
 
   ASSERT_EQ(2U, remote_results.size());
   EXPECT_EQ(SpellCheckResult::GRAMMAR, remote_results[0].decoration);
@@ -46,23 +43,6 @@ TEST(SpellcheckMessageFilterPlatformMacTest, CombineResults) {
   EXPECT_EQ(SpellCheckResult::SPELLING, remote_results[1].decoration);
   EXPECT_EQ(20, remote_results[1].location);
   EXPECT_EQ(remote_suggestion, remote_results[1].replacements[0]);
-}
-
-TEST(SpellCheckMessageFilterPlatformMacTest, TestOverrideThread) {
-  static const uint32_t kSpellcheckMessages[] = {
-      SpellCheckHostMsg_RequestTextCheck::ID,
-  };
-  scoped_refptr<SpellCheckMessageFilterPlatform> filter(
-      new SpellCheckMessageFilterPlatform(0));
-  content::BrowserThread::ID thread;
-  IPC::Message message;
-  for (size_t i = 0; i < arraysize(kSpellcheckMessages); ++i) {
-    message.SetHeaderValues(
-        0, kSpellcheckMessages[i], IPC::Message::PRIORITY_NORMAL);
-    thread = content::BrowserThread::IO;
-    filter->OverrideThreadForMessage(message, &thread);
-    EXPECT_EQ(content::BrowserThread::UI, thread);
-  }
 }
 
 }  // namespace
