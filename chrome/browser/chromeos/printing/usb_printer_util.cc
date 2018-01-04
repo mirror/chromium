0efd8ce9deba2931a9fcb9f72597cc8b53b4c013
David Valleau <valleau@chromium.org>
2018-01-03 16:11:40 -0800

    Adding detections for ipp-over-usb printers
    This change checks the interfaces on a detected USB printer to see if any of
    them support ipp-over-usb. A new flag has been added to the Printer class to
    indicate whether or not it does.
    
    Once the printer has been detected, in the event that we don't have a PPD for it
    and it supports ipp-over-usb then we set it up as an ipp everywhere printer.
    
    R=justincarlson@chromium.org, skau@chromium.org
    
    Bug:768606
    Change-Id: I38214e0ce4f8ecee262ae94ac548cd79626f8a82

--- chrome/browser/chromeos/printing/usb_printer_util.cc
+++ chrome/browser/chromeos/printing/usb_printer_util.cc
@@ -30,6 +30,14 @@ namespace {
 // (https://www.usb.org/developers/defined_class).
 constexpr uint8_t kPrinterInterfaceClass = 7;
 
+// Subclass used for printers
+// (http://www.usb.org/developers/docs/devclass_docs/usbprint11a021811.pdf).
+constexpr uint8_t kPrinterInterfaceSubclass = 1;
+
+// Protocol for ippusb printing.
+// (http://www.usb.org/developers/docs/devclass_docs/IPP.zip).
+constexpr uint8_t kPrinterIppusbProtocol = 4;
+
 // Escape URI strings the same way cups does it, so we end up with a URI cups
 // recognizes.  Cups hex-encodes '%', ' ', and anything not in the standard
 // ASCII range.  CUPS lets everything else through unchanged.
@@ -118,6 +126,20 @@ bool UsbDeviceIsPrinter(const device::UsbDevice& usb_device) {
   auto printer_filter = device::mojom::UsbDeviceFilter::New();
   printer_filter->has_class_code = true;
   printer_filter->class_code = kPrinterInterfaceClass;
+  printer_filter->has_subclass_code = true;
+  printer_filter->subclass_code = kPrinterInterfaceSubclass;
+  return UsbDeviceFilterMatches(*printer_filter, usb_device);
+}
+
+bool UsbDeviceSupportsIppusb(const device::UsbDevice& usb_device) {
+  auto printer_filter = device::mojom::UsbDeviceFilter::New();
+  printer_filter->has_class_code = true;
+  printer_filter->class_code = kPrinterInterfaceClass;
+  printer_filter->has_subclass_code = true;
+  printer_filter->subclass_code = kPrinterInterfaceSubclass;
+  printer_filter->has_protocol_code = true;
+  printer_filter->protocol_code = kPrinterIppusbProtocol;
+
   return UsbDeviceFilterMatches(*printer_filter, usb_device);
 }
 
@@ -184,6 +206,7 @@ std::unique_ptr<Printer> UsbDeviceToPrinter(const device::UsbDevice& device) {
   printer->set_description(printer->display_name());
   printer->set_uri(UsbPrinterUri(device));
   printer->set_id(UsbPrinterId(device));
+  printer->set_supports_ippusb(UsbDeviceSupportsIppusb(device));
   return printer;
 }
 
