64187cd98caec7c7c53d55ac7aaa0ca1a339c062
David Tseng <dtseng@chromium.org>
2018-01-03 16:01:26 -0800

    Support dictation on Chrome OS (initial version/prototype)
    - bound to ctrl+alt+s
    - each invokation triggers a new dictation session
    - sound icon prompts for user to start dictating
    - silence triggers dictation completion and recognition result
    
    Test: manual.
    Bug: 546373
    Change-Id: Ie9950dc6a21270998963222cf457fd218c40be89

--- chrome/browser/speech/speech_recognizer_delegate.h
+++ chrome/browser/speech/speech_recognizer_delegate.h
@@ -2,19 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_APP_LIST_SPEECH_RECOGNIZER_DELEGATE_H_
-#define CHROME_BROWSER_UI_APP_LIST_SPEECH_RECOGNIZER_DELEGATE_H_
+#ifndef CHROME_BROWSER_SPEECH_SPEECH_RECOGNIZER_DELEGATE_H_
+#define CHROME_BROWSER_SPEECH_SPEECH_RECOGNIZER_DELEGATE_H_
 
 #include <stdint.h>
 #include <string>
 
 #include "base/strings/string16.h"
-#include "ui/app_list/speech_ui_model_observer.h"
+#include "ui/app_list/speech_ui_model.h"
 
-namespace app_list {
-
-// Delegate for the app list speech recognizer. All methods are called from the
-// UI thread.
+// Delegate for speech recognizer. All methods are called from the UI
+// thread.
 class SpeechRecognizerDelegate {
  public:
   // Receive a speech recognition result. |is_final| indicated whether the
@@ -27,7 +25,7 @@ class SpeechRecognizerDelegate {
 
   // Invoked when the state of speech recognition is changed.
   virtual void OnSpeechRecognitionStateChanged(
-      SpeechRecognitionState new_state) = 0;
+      app_list::SpeechRecognitionState new_state) = 0;
 
   // Get the OAuth2 scope and token to pass to the speech recognizer. Does not
   // modify the arguments if no auth token is available or allowed.
@@ -38,6 +36,4 @@ class SpeechRecognizerDelegate {
   virtual ~SpeechRecognizerDelegate() {}
 };
 
-}  // namespace app_list
-
-#endif  // CHROME_BROWSER_UI_APP_LIST_SPEECH_RECOGNIZER_DELEGATE_H_
+#endif  // CHROME_BROWSER_SPEECH_SPEECH_RECOGNIZER_DELEGATE_H_
