bdfd6585f1de473fbf234d812e4d63879f32f945
Dave Schuyler <dschuyler@chromium.org>
2018-01-03 18:15:10 -0800

    [Rich Entity Suggest] add flag for omnibox-rich-entity-suggest
    This CL adds a flag for enable/disable the GUI enhancements for rich
    entity suggestions in the omnibox. The default value is Disabled.
    
    As of this CL, the flag has no effect.
    
    Bug: 798630
    Change-Id: I9e3ca0660965019dfbab16d2b98255d8cf7128c0

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1342,6 +1342,9 @@ extern const char kEnableNewAppMenuIconDescription[];
 extern const char kOmniboxEntitySuggestionsName[];
 extern const char kOmniboxEntitySuggestionsDescription[];
 
+extern const char kOmniboxRichEntitySuggestionsName[];
+extern const char kOmniboxRichEntitySuggestionsDescription[];
+
 extern const char kOmniboxTabSwitchSuggestionsName[];
 extern const char kOmniboxTabSwitchSuggestionsDescription[];
 
