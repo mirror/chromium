df9b62759754d5de1ea6a6fff7028fcad406250f
Mihai Sardarescu <msarda@chromium.org>
2018-01-04 11:37:59 +0100

    Try sign-in button
    Bug:
    Change-Id: I1011318cdbb7c396e84299f93d4d2766c0862e8b

--- chrome/browser/ui/views/sync/bubble_sync_promo_view.cc
+++ chrome/browser/ui/views/sync/bubble_sync_promo_view.cc
@@ -15,6 +15,33 @@
 #include "ui/views/controls/styled_label.h"
 #include "ui/views/layout/fill_layout.h"
 
+#include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/ui/views/harmony/chrome_layout_provider.h"
+#include "ui/views/controls/button/md_text_button.h"
+#include "ui/views/layout/box_layout.h"
+
+#include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/profiles/profile_avatar_icon_util.h"
+#include "chrome/browser/ui/views/hover_button.h"
+#include "chrome/browser/ui/views/profiles/badged_profile_photo.h"
+#include "components/vector_icons/vector_icons.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/paint_vector_icon.h"
+#include "ui/views/background.h"
+#include "ui/views/border.h"
+#include "ui/views/controls/button/label_button_label.h"
+
+const int kDiceSigninButtonId = 12345;
+const int kDiceSigninButtonDropDownArrowId = 54321;
+
+std::unique_ptr<BadgedProfilePhoto> GetAccountIcon() {
+  gfx::Image account_icon =
+      ui::ResourceBundle::GetSharedInstance().GetImageNamed(
+          profiles::GetPlaceholderAvatarIconResourceID());
+  return std::make_unique<BadgedProfilePhoto>(
+      BadgedProfilePhoto::BADGE_TYPE_NONE, account_icon);
+}
+
 BubbleSyncPromoView::BubbleSyncPromoView(BubbleSyncPromoDelegate* delegate,
                                          int link_text_resource_id,
                                          int message_text_resource_id)
@@ -48,4 +75,116 @@ void BubbleSyncPromoView::StyledLabelLinkClicked(views::StyledLabel* label,
                                                  const gfx::Range& range,
                                                  int event_flags) {
   delegate_->OnSignInLinkClicked();
+}
+
+class DiceSigninButton : public views::MdTextButton {
+ public:
+  DiceSigninButton(views::ButtonListener* listener,
+                   views::ButtonListener* drop_down_arrow_listener)
+      :  // views::MdTextButton(listener, views::style::CONTEXT_BUTTON_MD) {
+        views::MdTextButton(listener, views::style::CONTEXT_BUTTON) {
+    set_id(kDiceSigninButtonId);
+
+    // First create the child views.
+    subtitle_ = new views::LabelButtonLabel(
+        base::ASCIIToUTF16("msarda@google.com"), views::style::CONTEXT_BUTTON);
+    subtitle_->SetAutoColorReadabilityEnabled(false);
+    subtitle_->SetEnabledColor(SK_ColorWHITE);
+    subtitle_->SetDisabledColor(SK_ColorWHITE);
+    AddChildView(subtitle_);
+
+    divider_ = new views::View();
+    divider_->SetBackground(views::CreateSolidBackground(SK_ColorWHITE));
+    AddChildViewAt(divider_, 0);
+
+    arrow_ = views::MdTextButton::CreateSecondaryUiBlueButton(
+        drop_down_arrow_listener, base::string16());
+    arrow_->SetImage(views::Button::STATE_NORMAL,
+                     gfx::CreateVectorIcon(kSigninButtonDropDownArrowIcon, 15,
+                                           SK_ColorWHITE));
+    arrow_->set_id(kDiceSigninButtonDropDownArrowId);
+    AddChildViewAt(arrow_, 0);
+
+    // Set the text.
+    SetText(base::ASCIIToUTF16("Sync to Mihai Sardarescu"));
+    SetFocusForPlatform();
+    SetProminent(true);
+    SetHorizontalAlignment(gfx::ALIGN_LEFT);
+
+    // Set the image
+    gfx::Image account_icon =
+        ui::ResourceBundle::GetSharedInstance().GetImageNamed(
+            profiles::GetPlaceholderAvatarIconResourceID());
+    gfx::Image profile_photo_circular = profiles::GetSizedAvatarIcon(
+        account_icon, true, 40, 40, profiles::SHAPE_CIRCLE);
+    SetImage(views::Button::STATE_NORMAL,
+             *profile_photo_circular.ToImageSkia());
+  }
+  ~DiceSigninButton() override = default;
+
+  gfx::Rect GetChildAreaBounds() override {
+    gfx::Size s = size();
+    s.set_width(s.width() - 65);
+
+    return gfx::Rect(s);
+  }
+
+  void Layout() override {
+    DCHECK(arrow_);
+    views::MdTextButton::Layout();
+
+    gfx::Size label_size = label()->size();
+    label()->SetSize(gfx::Size(label_size.width(), label_size.height() / 2));
+
+    gfx::Rect label_bounds = label()->bounds();
+    subtitle_->SetBoundsRect(
+        gfx::Rect(label_bounds.bottom_left(), subtitle_->GetPreferredSize()));
+
+    subtitle_->SetEnabledColor(label()->enabled_color());
+
+    gfx::Size s = size();
+    divider_->SetBounds(s.width() - 60 - 4, 10, 1, s.height() - 2 * 10);
+    arrow_->SetBounds(s.width() - 60, 0, 60, s.height());
+  }
+
+ private:
+  views::LabelButtonLabel* subtitle_;
+  views::View* divider_;
+  views::LabelButton* arrow_;
+  DISALLOW_COPY_AND_ASSIGN(DiceSigninButton);
+};
+
+DiceBubbleSyncPromoView::DiceBubbleSyncPromoView(
+    BubbleSyncPromoDelegate* delegate,
+    int link_text_resource_id,
+    int message_text_resource_id)
+    : View() {
+  std::unique_ptr<views::BoxLayout> layout = std::make_unique<views::BoxLayout>(
+      views::BoxLayout::kVertical, gfx::Insets(),
+      ChromeLayoutProvider::Get()->GetDistanceMetric(
+          views::DISTANCE_RELATED_CONTROL_VERTICAL));
+  SetLayoutManager(std::move(layout));
+
+  BubbleSyncPromoView* promo_text = new BubbleSyncPromoView(
+      delegate, link_text_resource_id, message_text_resource_id);
+  AddChildView(promo_text);
+
+  //  auto* signin_button = views::MdTextButton::CreateSecondaryUiBlueButton(
+  //      nullptr, base::ASCIIToUTF16("Sign in"));
+  //  auto* title = views::MdTextButton::CreateSecondaryUiBlueButton(
+  //      nullptr, base::ASCIIToUTF16(">>>"));
+  //  signin_button->AddChildView(title);
+  AddChildView(new DiceSigninButton(this, this));
+}
+
+DiceBubbleSyncPromoView::~DiceBubbleSyncPromoView() {}
+
+void DiceBubbleSyncPromoView::Layout() {
+  views::View::Layout();
+  // LOG(ERROR) << PrintViewGraph(true);
+}
+
+void DiceBubbleSyncPromoView::ButtonPressed(views::Button* sender,
+                                            const ui::Event& event) {
+  LOG(ERROR) << "Button pressed: " << sender->id();
 }
