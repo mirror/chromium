c0bcafb7bf6d9108b46a7689741197fb1066fe88
Boris Sazonov <bsazonov@chromium.org>
2018-01-04 18:50:04 +0100

    [Android] Don't extend Fragment in FirstRunPage
    This CL reimplements FirstRunPage to make it an interface instead of
    a base class. It also introduces FirstRunFragment - an interface that
    may optionally be implemented by FRE fragments to intercept Back button
    or get notifications about native initialization.
    
    Bug: 783344
    Change-Id: I05e13ef41ff8a4ac5cac4a9d82cb6d33e97b3eba

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/DefaultSearchEngineFirstRunFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/DefaultSearchEngineFirstRunFragment.java
@@ -21,7 +21,15 @@ import org.chromium.chrome.browser.search_engines.TemplateUrlService;
 import org.chromium.chrome.browser.widget.RadioButtonLayout;
 
 /** A {@link Fragment} that presents a set of search engines for the user to choose from. */
-public class DefaultSearchEngineFirstRunFragment extends FirstRunPage {
+public class DefaultSearchEngineFirstRunFragment extends Fragment {
+    /** FRE page that instantiates this fragment. */
+    public static class Page implements FirstRunPage {
+        @Override
+        public Fragment instantiateFragment() {
+            return new DefaultSearchEngineFirstRunFragment();
+        }
+    }
+
     @SearchEnginePromoType
     private int mSearchEnginePromoDialoType;
     private boolean mShownRecorded;
@@ -48,7 +56,7 @@ public class DefaultSearchEngineFirstRunFragment extends FirstRunPage {
             Runnable dismissRunnable = new Runnable() {
                 @Override
                 public void run() {
-                    advanceToNextPage();
+                    getPageDelegate().advanceToNextPage();
                 }
             };
             new DefaultSearchEngineDialogHelper(
@@ -67,7 +75,7 @@ public class DefaultSearchEngineFirstRunFragment extends FirstRunPage {
                 ThreadUtils.postOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        advanceToNextPage();
+                        getPageDelegate().advanceToNextPage();
                     }
                 });
             }
@@ -86,5 +94,9 @@ public class DefaultSearchEngineFirstRunFragment extends FirstRunPage {
         }
 
         mShownRecorded = true;
+    }
+
+    private FirstRunPageDelegate getPageDelegate() {
+        return (FirstRunPageDelegate) getActivity();
     }
 }
