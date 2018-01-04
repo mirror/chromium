cae92f60284ca5fece468424489de6c43c7209fe
Bailey Berro <baileyberro@chromium.org>
2018-01-04 10:21:52 -0800

    Updating SmbProviderClient to use modified CloseFile proto.
    Bug: 757625
    Change-Id: I8e6f2dcfa3460c153403d265bf2fc38f26ba7a8e

--- chromeos/dbus/smb_provider_client.h
+++ chromeos/dbus/smb_provider_client.h
@@ -75,7 +75,9 @@ class CHROMEOS_EXPORT SmbProviderClient : public DBusClient {
 
   // Calls CloseFile. This closes the file with handle |file_id|. Subsequent
   // operations using file with this handle will fail.
-  virtual void CloseFile(int32_t file_id, CloseFileCallback callback) = 0;
+  virtual void CloseFile(int32_t mount_id,
+                         int32_t file_id,
+                         CloseFileCallback callback) = 0;
 
  protected:
   // Create() should be used instead.
