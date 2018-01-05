7ad712d5651eb4bb26035e99aaa05f838f4bd9fc
Peter Qiu <zqiu@chromium.org>
2018-01-05 13:55:11 -0800

    [Android Camera Capture]: Camera2: fixed reporting of supported formats
    There are couple issues with reported supported formats when
    using Camera2:
    - Frame rate calculation is incorrect
      (1.0 / kNanoSecondsToFps * minFrameDuration), where the division
      will performed before the multiplication, which is incorrect.
    - Image format is not included when reporting supported formats
      to the higher layer, which results in the camera device being
      ignored by the video capture service since it "doesn't" support
      any formats.
    
    This also adds the support to report I420 format as one of the
    supported formats.
    
    Bug: None
    Test: Manual
    Change-Id: I85e7f18b24417f449c11b5300298ff34d6b9cd7a

--- media/capture/video/android/java/src/org/chromium/media/VideoCaptureCamera2.java
+++ media/capture/video/android/java/src/org/chromium/media/VideoCaptureCamera2.java
@@ -185,7 +185,7 @@ public class VideoCaptureCamera2 extends VideoCapture {
         }
     };
 
-    private static final double kNanoSecondsToFps = 1.0E-9;
+    private static final double kNanoSecondsPerSecond = 1000000000;
     private static final String TAG = "VideoCapture";
     private static final long PRECAPTURE_TIMEOUT_MS = 1000;
 
@@ -604,14 +604,14 @@ public class VideoCaptureCamera2 extends VideoCapture {
                     final long minFrameDuration = streamMap.getOutputMinFrameDuration(format, size);
                     minFrameRate = (minFrameDuration == 0)
                             ? 0.0f
-                            : (1.0 / kNanoSecondsToFps * minFrameDuration);
+                            : (kNanoSecondsPerSecond / minFrameDuration);
                 } else {
                     // TODO(mcasas): find out where to get the info from in this case.
                     // Hint: perhaps using SCALER_AVAILABLE_PROCESSED_MIN_DURATIONS.
                     minFrameRate = 0.0;
                 }
                 formatList.add(new VideoCaptureFormat(
-                        size.getWidth(), size.getHeight(), (int) minFrameRate, 0));
+                        size.getWidth(), size.getHeight(), (int) minFrameRate, format));
             }
         }
         return formatList.toArray(new VideoCaptureFormat[formatList.size()]);
