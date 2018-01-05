21ebe3445c9b18be184b99ef32a64d4823044aa9
Kevin Marshall <kmarshall@chromium.org>
2018-01-04 16:16:30 -0800

    Build Fuchsia packages for test targets.
    All targets that use the test() rule will produce Fuchsia packages
    containing the test executable and its runtime dependencies as
    build artifacts. The packages are modules which can be deployed in
    an already-running system, as opposed to the current scheme which
    bakes the files into the system image.
    
    Deployment and support for dynamic libraries will be addressed
    in followup CLs.
    
    This CL also:
    * Fixes PathService logic for packaged invocations.
    * Adds a new "build_manifest.py" GN helper script.
    * Fixes the dependency graph for Fuchsia runner script generation
      so that it accurately captures the real dependency ordering.
    
    
    Bug:796779
    Change-Id: I4e3a38e0075573b328ad98425cc493e28cff1ca4

--- base/base_paths_fuchsia.cc
+++ base/base_paths_fuchsia.cc
@@ -10,38 +10,91 @@
 #include "base/files/file_path.h"
 
 namespace base {
+namespace {
 
-bool PathProviderFuchsia(int key, FilePath* result) {
-  switch (key) {
-    case FILE_MODULE:
-    // Not supported in debug or component builds. Fall back on using the EXE
-    // path for now.
-    // TODO(fuchsia): Get this value from an API. See crbug.com/726124
-    case FILE_EXE: {
-      // Use the binary name as specified on the command line.
-      // TODO(fuchsia): It would be nice to get the canonical executable path
-      // from a kernel API. See https://crbug.com/726124
-      char bin_dir[PATH_MAX + 1];
-      if (realpath(base::CommandLine::ForCurrentProcess()
-                       ->GetProgram()
-                       .AsUTF8Unsafe()
-                       .c_str(),
-                   bin_dir) == NULL) {
-        return false;
-      }
-      *result = FilePath(bin_dir);
-      return true;
-    }
-    case DIR_SOURCE_ROOT:
-      // This is only used for tests, so we return the binary location for now.
-      *result = FilePath("/system");
-      return true;
-    case DIR_CACHE:
-      *result = FilePath("/data");
-      return true;
+const char kPackageBinaryLocation[] = "/pkg/bin/app";
+const char kPackageUrlPrefix[] = "file://";
+
+// TODO(fuchsia): We might need to have a more robust solution for detecting the
+// package context in case we launch separate executables, or eliminate the
+// option of running outside a package entirely.
+bool IsPackageProcess() {
+  std::string exe_path =
+      base::CommandLine::ForCurrentProcess()->GetProgram().AsUTF8Unsafe();
+  return exe_path.find(kPackageUrlPrefix) == 0 ||
+         exe_path == kPackageBinaryLocation;
+}
+
+bool GetExecutablePath(FilePath* result) {
+  // The path to the binary executable inside a packaged app is always
+  // "/pkg/bin/app", but the path indicated by argv is a URL which looks more
+  // like this: file://system/foo.far. The URL-based executable path is
+  // meaningless within the context of the process, so we must detect that case
+  // and convert it to the actual executable path in the process' namespace.
+  if (IsPackageProcess()) {
+    *result = FilePath(kPackageBinaryLocation);
+    return true;
   }
 
-  return false;
+  char bin_dir[PATH_MAX + 1];
+  if (realpath(base::CommandLine::ForCurrentProcess()
+                   ->GetProgram()
+                   .AsUTF8Unsafe()
+                   .c_str(),
+               bin_dir) == NULL) {
+    return false;
+  }
+
+  *result = FilePath(bin_dir);
+  return true;
+}
+
+}  // namespace
+
+bool PathProviderFuchsia(int key, FilePath* result) {
+  if (IsPackageProcess()) {
+    // We use an alternate set of path mappings depending on whether or not
+    // the process is executed from within a package.
+    // Packaged processes have a substantially different namespace layout than
+    // regular processes (ones invoked outside the application controller).
+    switch (key) {
+      case FILE_MODULE:
+      // Not supported in debug or component builds. Fall back on using the EXE
+      // path for now.
+      // TODO(fuchsia): Get this value from an API. See https://crbug.com/726124
+      case FILE_EXE:
+        return GetExecutablePath(result);
+      case DIR_SOURCE_ROOT:
+        *result = FilePath("/pkg");
+        return true;
+      case DIR_CACHE:
+        *result = FilePath("/data");
+        return true;
+      case DIR_EXE:
+        *result = FilePath("/pkg/lib");
+        return true;
+    }
+
+    return false;
+  } else {
+    switch (key) {
+      case FILE_MODULE:
+      // Not supported in debug or component builds. Fall back on using the EXE
+      // path for now.
+      // TODO(fuchsia): Get this value from an API. See https://crbug.com/726124
+      case FILE_EXE:
+        return GetExecutablePath(result);
+      case DIR_SOURCE_ROOT:
+        // This is only used for tests, so we return the binary location for
+        // now.
+        *result = FilePath("/system");
+        return true;
+      case DIR_CACHE:
+        *result = FilePath("/data");
+        return true;
+    }
+    return false;
+  }
 }
 
 }  // namespace base
