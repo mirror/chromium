7534493e75c8e7c72c3cecba7c1db64becb08f44
Sadrul Habib Chowdhury <sadrul@chromium.org>
2018-01-05 14:25:13 -0500

    oopif events: Return the correct FrameSinkId.
    The RenderFrameProxy has the correct FrameSinkId for the embedded oopif.
    So return that directly as the hit-testing result, instead of
    constructing it from the frame's routing id.
    
    BUG=797661
    
    Change-Id: I9e6e263f6929b5ce53baeef5c8093bd9de052626

--- content/renderer/input/render_widget_input_handler.h
+++ content/renderer/input/render_widget_input_handler.h
@@ -26,6 +26,10 @@ namespace ui {
 class LatencyInfo;
 }
 
+namespace viz {
+class FrameSinkId;
+}
+
 namespace content {
 
 class RenderWidget;
@@ -41,7 +45,7 @@ class CONTENT_EXPORT RenderWidgetInputHandler {
 
   // Hit test the given point to find out the frame underneath and
   // return the routing id for that frame.
-  int GetWidgetRoutingIdAtPoint(const gfx::Point& point);
+  viz::FrameSinkId GetFrameSinkIdAtPoint(const gfx::Point& point);
 
   // Handle input events from the input event provider.
   virtual void HandleInputEvent(
