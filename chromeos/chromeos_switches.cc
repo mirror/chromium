54932ba9d99b69c3ba84d23c3fddb371072c5d27
David Tseng <dtseng@chromium.org>
2018-01-05 14:02:07 -0800

    Support dictation on Chrome OS (initial version/prototype)
    - bound to ctrl+alt+Volume up
    - each invokation triggers a new dictation session
    - sound icon prompts for user to start dictating
    - silence triggers dictation completion and recognition result
    
    Test: manual.
    Bug: 546373
    Change-Id: Ie9950dc6a21270998963222cf457fd218c40be89

--- chromeos/chromeos_switches.cc
+++ chromeos/chromeos_switches.cc
@@ -665,5 +665,10 @@ bool IsNetworkSettingsConfigEnabled() {
       chromeos::switches::kDisableNetworkSettingsConfig);
 }
 
+bool AreExperimentalAccessibilityFeaturesEnabled() {
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+      chromeos::switches::kEnableExperimentalAccessibilityFeatures);
+}
+
 }  // namespace switches
 }  // namespace chromeos
