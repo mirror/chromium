46b7cde9d5d2eb18d50d799f7d24751f54ae6573
Charles Harrison <csharrison@chromium.org>
2018-01-05 00:01:22 -0500

    Safe Browsing e2e instrumentation tests
    This CL adds a MockSafeBrowsingApiHandler which vends fake response
    JSON. It is used by a simple test harness which checks that an
    interstitial is properly shown.
    
    Bug: 775530
    Change-Id: I5a8c21edd57a6a57c1de85feed1205502799673b

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/MockSafeBrowsingApiHandler.java
@@ -0,0 +1,70 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser;
+
+import android.content.Context;
+
+import org.chromium.base.ThreadUtils;
+import org.chromium.components.safe_browsing.SafeBrowsingApiHandler;
+import org.chromium.components.safe_browsing.SafeBrowsingApiHandler.Observer;
+
+import java.util.HashMap;
+
+/**
+ * SafeBrowsingApiHandler that vends fake responses.
+ */
+class MockSafeBrowsingApiHandler implements SafeBrowsingApiHandler {
+    private Observer mObserver;
+    // Mock time it takes for a lookup request to complete.
+    private static final long DEFAULT_CHECK_DELTA_US = 10;
+    private static final String SAFE_METADATA = "{}";
+
+    // Global url -> metadataResponse map. In practice there is only one SafeBrowsingApiHandler, but
+    // it is cumbersome for tests to reach into the singleton instance directly. So just make this
+    // static and modifiable from java tests using a static method. This map is copied into the
+    // instance during |init|.
+    private static HashMap<String, String> sResponseMap = new HashMap<String, String>();
+
+    private HashMap<String, String> mResponseMap;
+
+    @Override
+    public boolean init(Context context, Observer observer) {
+        mObserver = observer;
+        mResponseMap = new HashMap<String, String>(sResponseMap);
+        return true;
+    }
+
+    @Override
+    public void startUriLookup(final long callbackId, String uri, int[] threatsOfInterest) {
+        final String metadata;
+        if (mResponseMap.containsKey(uri)) {
+            metadata = mResponseMap.get(uri);
+        } else {
+            metadata = SAFE_METADATA;
+        }
+
+        // clang-format off
+      ThreadUtils.runOnUiThread(
+          (Runnable) () -> mObserver.onUrlCheckDone(
+              callbackId, STATUS_SUCCESS, metadata, DEFAULT_CHECK_DELTA_US));
+        // clang-format on
+    }
+
+    /*
+     * Adds a mock response to the static map.
+     * Should be called before the main activity starts up, to avoid thread upsafe behavior.
+     */
+    public static void addMockResponse(String uri, String metadata) {
+        sResponseMap.put(uri, metadata);
+    }
+
+    /*
+     * Clears the mock responses from the static map.
+     * Should be called in the test tearDown method.
+     */
+    public static void clearMockResponses() {
+        sResponseMap.clear();
+    }
+}
