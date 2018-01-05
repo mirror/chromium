755f9d923cf6d3cf634898438aa1f5c8ee4409e0
Hector Carmona <hcarmona@chromium.org>
2018-01-05 14:12:52 -0800

    MD Extensions: Error Console: Fix a11y issues caught by a11y test.
    Bug: 799597
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I9e0277906ae9d54597bba5ac7fb62c67ad625873

--- chrome/browser/ui/webui/extensions/extensions_ui.cc
+++ chrome/browser/ui/webui/extensions/extensions_ui.cc
@@ -155,6 +155,12 @@ content::WebUIDataSource* CreateMdExtensionsSource(bool in_dev_mode) {
   source->AddLocalizedString("errorsPageHeading",
                              IDS_MD_EXTENSIONS_ERROR_PAGE_HEADING);
   source->AddLocalizedString("clearAll", IDS_MD_EXTENSIONS_ERROR_CLEAR_ALL);
+  source->AddLocalizedString("clearLog", IDS_MD_EXTENSIONS_LOG_CLEAR);
+  source->AddLocalizedString("clearWarn", IDS_MD_EXTENSIONS_WARN_CLEAR);
+  source->AddLocalizedString("clearError", IDS_MD_EXTENSIONS_ERROR_CLEAR);
+  source->AddLocalizedString("logLevel", IDS_EXTENSIONS_LOG_LEVEL_INFO);
+  source->AddLocalizedString("warnLevel", IDS_EXTENSIONS_LOG_LEVEL_WARN);
+  source->AddLocalizedString("errorLevel", IDS_EXTENSIONS_LOG_LEVEL_ERROR);
   source->AddLocalizedString("anonymousFunction",
                              IDS_MD_EXTENSIONS_ERROR_ANONYMOUS_FUNCTION);
   source->AddLocalizedString("errorContext", IDS_MD_EXTENSIONS_ERROR_CONTEXT);
