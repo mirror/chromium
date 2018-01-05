883ceecd7e53ce7da87705da736e75c9ea37a2c3
arthursonzogni <arthursonzogni@chromium.org>
2018-01-05 16:30:02 +0100

    NavigationMojoResponse: Fix GetCertificateChainsSizeInKB
    Fix a null scoped_refptr dereferencement error in
    GetCertificateChainsSizeInKB.
    
    |ssl_info.unverrified_cert| is null in the following test:
    CaptivePortalBrowserTest.InterstitialTimerCertErrorAfterSlowLoad
    
    When applied on top of: https://chromium-review.googlesource.com/852094,
    this test is now working.
    
    Bug: 705744
    Change-Id: I74c8b42d08da39f6841d8ab2ede211d71f852b39

--- content/browser/loader/navigation_url_loader_network_service.cc
+++ content/browser/loader/navigation_url_loader_network_service.cc
@@ -63,11 +63,19 @@ namespace {
 int g_next_request_id = -2;
 
 size_t GetCertificateChainsSizeInKB(const net::SSLInfo& ssl_info) {
-  base::Pickle cert_pickle;
-  ssl_info.cert->Persist(&cert_pickle);
-  base::Pickle unverified_cert_pickle;
-  ssl_info.unverified_cert->Persist(&unverified_cert_pickle);
-  return (cert_pickle.size() + unverified_cert_pickle.size()) / 1000;
+  size_t size = 0;
+  if (ssl_info.cert) {
+    base::Pickle pickle;
+    ssl_info.cert->Persist(&pickle);
+    size += pickle.size();
+  }
+  if (ssl_info.unverified_cert) {
+    base::Pickle pickle;
+    ssl_info.unverified_cert->Persist(&pickle);
+    size += pickle.size();
+  }
+
+  return size / 1000;
 }
 
 WebContents* GetWebContentsFromFrameTreeNodeID(int frame_tree_node_id) {
