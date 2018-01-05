67b5dbdc80eec4179649c831d2b6b5b9a399ff56
Renjie Liu <renjieliu@chromium.org>
2018-01-05 11:41:01 +1100

    Placeholder for learning example
    Bug:
    Change-Id: Id00d8dfd1a120b43d57cb4e6e12e2c323053b1a3

---
+++ components/learning/learning_example.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_LEARNING_LEARNING_EXAMPLE_H
+#define COMPONENTS_LEARNING_LEARNING_EXAMPLE_H
+
+namespace learning {
+
+struct LearningExample {
+  LearningExample();
+  ~LearningExample();
+  // Feature list.
+};
+}  // namespace learning
+
+#endif  // COMPONENTS_LEARNING_LEARNING_EXAMPLE_H
