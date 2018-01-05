2afcb2f5c669f1a0480d4d3472c9d13499527806
Kevin Marshall <kmarshall@chromium.org>
2018-01-04 16:32:42 -0800

    Initial implementation of new SSH-based Fuchsia runner libraries.
    The new runner is based heavily around the use of SSH and SCP.
    Rather than build large boot filesystems with Chrome or test binaries,
    as we do now, we will instantiate a basic Fuchsia instance and use
    SSH for our binary deployment, execution, and log extraction needs.
    It can also be used as a layer to facilitate connections between the
    client and server portions of the Blink layout test system.
    
    This CL defines the abstraction layer for connecting and interacting
    with a Fuchsia target, and implementation code for using it on QEMU.
    
    
    Bug: 799309
    Change-Id: Ia2613c36d1a6f10457aa9c90435d4c95f9721e6c

---
+++ build/fuchsia/runner_v2/remote_cmd.py
@@ -0,0 +1,90 @@
+# Copyright 2018 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Helper functions for remotely executing and copying files over a SSH
+connection."""
+
+import subprocess
+
+__SSH_PATH = "/usr/bin/ssh"
+__SCP_PATH = "/usr/bin/scp"
+__SSH_TEMPLATE = "/usr/bin/ssh -F {ssh_config} {host} -p {port} " + \
+                 "\"{escaped_command}\""
+__SCP_TEMPLATE = "/usr/bin/scp -F {ssh_config} -p {port} {source} {dest}"
+
+COPY_TO_TARGET = 0
+COPY_FROM_TARGET = 1
+
+
+def RunSSH(config_path, host, port, command, silent):
+  """Executes an SSH command on the remote host and blocks until completion.
+
+  config_path: Full path to SSH configuration.
+  host: The hostname or IP address of the remote host.
+  port: The port to connect to.
+  command: A list of strings containing the command and its arguments.
+  silent: If true, suppresses all output from 'ssh'.
+
+  Returns the exit code from the remote command."""
+
+  ssh_command = ['/usr/bin/ssh',
+                 '-F', config_path,
+                 host,
+                 '-p', str(port)] + command
+  if silent:
+    devnull = open('/dev/null', 'w')
+    return subprocess.call(ssh_command, stderr=devnull, stdout=devnull)
+  else:
+    return subprocess.call(ssh_command)
+
+
+def RunPipedSSH(config_path, host, port, command):
+  """Executes an SSH command on the remote host and returns a process object
+  with access to the command's stdio streams. Does not block.
+
+  config_path: Full path to SSH configuration.
+  host: The hostname or IP address of the remote host.
+  port: The port to connect to.
+  command: A list of strings containing the command and its arguments.
+  silent: If true, suppresses all output from 'ssh'.
+
+  Returns a Popen object for the command."""
+
+  ssh_command = ['/usr/bin/ssh',
+                 '-F', config_path,
+                 host,
+                 '-p', str(port)] + command
+  return subprocess.Popen(ssh_command,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          stdin=subprocess.PIPE)
+
+
+def RunSCP(config_path, host, port, source, dest, direction):
+  """Copies a file to or from a remote host using SCP and blocks until
+  completion.
+
+  config_path: Full path to SSH configuration.
+  host: The hostname or IP address of the remote host.
+  port: The port to connect to.
+  source: The path of the file to be copied.
+  dest: The path that |source| will be copied to.
+  direction: Indicates whether the file should be copied to
+             or from the remote side.
+             Valid values are COPY_TO_TARGET or COPY_FROM_TARGET.
+
+  Function will raise an assertion if a failure occurred."""
+
+  if direction == COPY_TO_TARGET:
+    dest = "%s:%s" % (host, dest)
+  else:
+    source = "%s:%s" % (host, source)
+
+  scp_command = ['/usr/bin/scp',
+                 '-F', config_path,
+                 '-P', str(port),
+                 source,
+                 dest]
+  devnull = open('/dev/null', 'w')
+  subprocess.check_call(scp_command, stdout=devnull)
