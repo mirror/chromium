9d51c81aab7aa32e06573a44f2ad315859dae69f
edchin <edchin@chromium.org>
2018-01-05 15:45:49 -0500

    [ios] Refactor ExternalAppLauncherTabHelper
    This CL achieves several objectives:
    
    1) Removes the use of |rootViewController| to present UI.
    
    2) Separates launching from the policy decider logic. The
    tab helper holds the policy decider logic. The app launcher
    coordinator does the actual app launching.
    
    3) Better naming: "ExternalAppLauncher" was renamed to
    "AppLauncher". It is more concise, and "external" did not
    provide any value as there is no "internal app".
    
    4) Better folder structure: the tab helper was pulled out
    to it's own folder /chrome/browser/app_launcher. The app
    launcher coordinator lives in /chrome/browser/ui/app_launcher.
    
    Bug: 791792
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I53ab441db05b85211102df169a264f0fcf53195c

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -56,6 +56,7 @@
 #include "components/strings/grit/components_strings.h"
 #include "components/toolbar/toolbar_model_impl.h"
 #include "ios/chrome/app/tests_hook.h"
+#import "ios/chrome/browser/app_launcher/app_launcher_tab_helper.h"
 #include "ios/chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
@@ -116,6 +117,7 @@
 #import "ios/chrome/browser/ui/activity_services/requirements/activity_service_presentation.h"
 #import "ios/chrome/browser/ui/alert_coordinator/alert_coordinator.h"
 #import "ios/chrome/browser/ui/alert_coordinator/repost_form_coordinator.h"
+#import "ios/chrome/browser/ui/app_launcher/app_launcher_coordinator.h"
 #import "ios/chrome/browser/ui/authentication/re_signin_infobar_delegate.h"
 #import "ios/chrome/browser/ui/background_generator.h"
 #import "ios/chrome/browser/ui/bookmarks/bookmark_interaction_controller.h"
@@ -605,6 +607,9 @@ NSString* const kBrowserViewControllerSnackbarCategory =
   // Coordinator for the PassKit UI presentation.
   PassKitCoordinator* _passKitCoordinator;
 
+  // Coordinator for UI related to launching external apps.
+  AppLauncherCoordinator* _appLauncherCoordinator;
+
   // Fake status bar view used to blend the toolbar into the status bar.
   UIView* _fakeStatusBarView;
 
@@ -981,6 +986,9 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
 
     _passKitCoordinator =
         [[PassKitCoordinator alloc] initWithBaseViewController:self];
+
+    _appLauncherCoordinator =
+        [[AppLauncherCoordinator alloc] initWithBaseViewController:self];
 
     _javaScriptDialogPresenter.reset(
         new JavaScriptDialogPresenterImpl(_dialogPresenter));
@@ -2845,6 +2853,11 @@ bubblePresenterForFeature:(const base::Feature&)feature
               self.browserState)) {
     accountConsistencyService->SetWebStateHandler(tab.webState, self);
   }
+
+  if (AppLauncherTabHelper* appLauncherTabHelper =
+          AppLauncherTabHelper::FromWebState(tab.webState)) {
+    appLauncherTabHelper->SetDelegate(_appLauncherCoordinator);
+  }
 }
 
 - (void)uninstallDelegatesForTab:(Tab*)tab {
@@ -2880,6 +2893,11 @@ bubblePresenterForFeature:(const base::Feature&)feature
   }
 
   SnapshotTabHelper::FromWebState(tab.webState)->SetDelegate(nil);
+
+  if (AppLauncherTabHelper* appLauncherTabHelper =
+          AppLauncherTabHelper::FromWebState(tab.webState)) {
+    appLauncherTabHelper->SetDelegate(nil);
+  }
 }
 
 - (void)tabSelected:(Tab*)tab notifyToolbar:(BOOL)notifyToolbar {
