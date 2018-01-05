2acf0515c12fe32512f814904c8dbcaa662f7fa7
Xianzhu Wang <wangxianzhu@chromium.org>
2018-01-05 10:30:51 -0800

    [SPv175] Short-term solution for printing
    - Don't apply viewport clip property when printing because we create
      artificial clip for each page.
    - Use the old path for repeating table headers/footers.
    - Partly use the old path for repeating fixed-position object in paged
      media.
    
    The long-term solution might be to create FragmentData for page
    fragments.
    
    Bug: 771643
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Iee2b106fa81a53d024a4f08af17463ad1b757cc9

--- third_party/WebKit/Source/core/paint/AdjustPaintOffsetScope.cpp
+++ third_party/WebKit/Source/core/paint/AdjustPaintOffsetScope.cpp
@@ -40,7 +40,7 @@ bool AdjustPaintOffsetScope::AdjustPaintOffset(const LayoutBox& box) {
     return true;
   }
 
-  if (box.IsTableSection()) {
+  if (!old_paint_info_.IsPrinting() && box.IsTableSection()) {
     const auto& section = ToLayoutTableSection(box);
     if (section.IsRepeatingHeaderGroup() || section.IsRepeatingFooterGroup()) {
       adjusted_paint_offset_ = fragment->PaintOffset();
--- third_party/WebKit/Source/core/paint/TableSectionPainter.cpp
+++ third_party/WebKit/Source/core/paint/TableSectionPainter.cpp
@@ -26,7 +26,9 @@ void TableSectionPainter::PaintRepeatingHeaderGroup(
     const PaintInfo& paint_info,
     const LayoutPoint& paint_offset,
     ItemToPaint item_to_paint) {
-  if (RuntimeEnabledFeatures::SlimmingPaintV175Enabled())
+  if (RuntimeEnabledFeatures::SlimmingPaintV175Enabled() &&
+      // TODO(wangxianzhu): Use the PaintPropertyTreeBuilder path for printing.
+      !paint_info.IsPrinting())
     return;
 
   if (!layout_table_section_.IsRepeatingHeaderGroup())
@@ -94,7 +96,9 @@ void TableSectionPainter::PaintRepeatingFooterGroup(
     const PaintInfo& paint_info,
     const LayoutPoint& paint_offset,
     ItemToPaint item_to_paint) {
-  if (RuntimeEnabledFeatures::SlimmingPaintV175Enabled())
+  if (RuntimeEnabledFeatures::SlimmingPaintV175Enabled() &&
+      // TODO(wangxianzhu): Use the PaintPropertyTreeBuilder path for printing.
+      !paint_info.IsPrinting())
     return;
 
   if (!layout_table_section_.IsRepeatingFooterGroup())
