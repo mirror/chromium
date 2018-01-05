1128fbecae05b627b99ebc34606635d1e7791bd8
Katie Dektar <katydek@google.com>
2018-01-05 11:28:24 -0800

    Integration tests for select-to-speak.
    Adds tests to read text at keystroke using the select-to-speak
    extension.
    
    More tests coming up now that this framework will be in place.
    
    Bug: 713303
    Change-Id: I6d07693e5e63975e1821d7fd325df8764357e6ae

---
+++ chrome/browser/resources/chromeos/select_to_speak/mock_tts.js
@@ -0,0 +1,33 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/*
+ * A mock text-to-speech engine for tests.
+ * This class has functions and callbacks necessary for Select-to-Speak
+ * to function. It keeps track of the utterances currently being spoken,
+ * and whether TTS should be speaking or is stopped.
+ */
+var MockTts = function() {
+  this.pendingUtterances_ = [];
+  this.currentlySpeaking_ = false;
+};
+
+// Functions based on methods in
+// https://developer.chrome.com/extensions/tts
+MockTts.prototype = {
+  speak: function(utterance, options) {
+    this.pendingUtterances_.push(utterance);
+    this.currentlySpeaking_ = true;
+  },
+  stop: function() {
+    this.pendingUtterances_ = [];
+    this.currentlySpeaking_ = false;
+  },
+  getVoices: function(callback) {
+    callback([{voiceName: 'English US', lang: 'English'}]);
+  },
+  isSpeaking: function(callback) {
+    callback(this.currentlySpeaking_);
+  }
+};
