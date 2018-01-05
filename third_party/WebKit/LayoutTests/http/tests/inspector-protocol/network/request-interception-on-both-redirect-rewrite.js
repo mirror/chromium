786bc6e3b8c00c26b5ca897cf69c68a645c569a0
Eugene Ostroukhov <eostroukhov@chromium.org>
2018-01-05 08:56:16 -0800

    DevTools: make tests run on newer Linux distros
    1. Remove usage of the PHP split function (had been removed from newer
       PHP versions)
    2. Do not expect "X-Powered-By" header that is no longer added by newer
       PHP version.
    3. Fix expectations for the redirect requests.
    
    Bug:
    Change-Id: I64d83aeb4e02ef5e13925244b0918e0b1f37cf23

--- third_party/WebKit/LayoutTests/http/tests/inspector-protocol/network/request-interception-on-both-redirect-rewrite.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/network/request-interception-on-both-redirect-rewrite.js
@@ -2,7 +2,8 @@
   var {page, session, dp} = await testRunner.startBlank(
       `Tests interception for redirects in a chain but rewrite last response.`);
 
-  var headersMaskList = new Set(['date', 'server', 'last-modified', 'etag', 'keep-alive', 'x-powered-by', 'expires']);
+  var headersMaskList = new Set(['date', 'server', 'last-modified', 'etag', 'keep-alive', 'expires']);
+  var headersHideList = new Set(['x-powered-by']);
   var fileNameForRequestId = new Map();
   var responseReceivedEventProimseForFile = new Map();
   var responseReceivedResolverForFile = new Map();
@@ -119,6 +120,8 @@
       testRunner.log('  responseHeaders:');
       for (var headerName of Object.keys(event.params.responseHeaders).sort()) {
         var headerValue = event.params.responseHeaders[headerName].split(';')[0]; // Sometimes "; charset=UTF-8" gets in here.
+        if (headersHideList.has(headerName.toLowerCase()))
+          continue;
         if (headersMaskList.has(headerName.toLowerCase()))
           headerValue = '<Masked>';
         testRunner.log(`    ${headerName}: ${headerValue}`);
