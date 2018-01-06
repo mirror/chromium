531263c598941d5c6fd88a8da4d2834a3d691125
Sarath Singapati <sarath.singapati@huawei.com>
2018-01-06 20:33:14 +0200

    AutofillCounter to use GetPeriodEnd() instead of Time::Max().
    Added unittest for AutofillTable::RemoveFormElementsAddedBetween()
    to test for removal of form elements added before 30 days.
    Autofill_GetCountOfValuesContainedBetween test already covers
    (t1, t2), ((Time(), Time::Max()), (Time(), t2) cases.
    
    Bug: 243833
    Change-Id: I2cb8c3d8adcb31da2b51383c76a44dba056e776c

--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_unittest.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_unittest.cc
@@ -48,6 +48,7 @@
 #include "components/autofill/core/browser/credit_card.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/browser/personal_data_manager_observer.h"
+#include "components/autofill/core/browser/test_autofill_clock.h"
 #include "components/autofill/core/common/autofill_constants.h"
 #include "components/bookmarks/browser/bookmark_model.h"
 #include "components/bookmarks/test/bookmark_test_helpers.h"
@@ -1422,6 +1423,44 @@ TEST_F(ChromeBrowsingDataRemoverDelegateTest, AutofillRemovalLastHour) {
       AnHourAgo(), base::Time::Max(),
       ChromeBrowsingDataRemoverDelegate::DATA_TYPE_FORM_DATA, false);
 
+  EXPECT_EQ(ChromeBrowsingDataRemoverDelegate::DATA_TYPE_FORM_DATA,
+            GetRemovalMask());
+  EXPECT_EQ(content::BrowsingDataRemover::ORIGIN_TYPE_UNPROTECTED_WEB,
+            GetOriginTypeMask());
+  ASSERT_FALSE(tester.HasProfile());
+}
+
+// Verify the clearing of autofill data that was modified more than 30 days ago.
+TEST_F(ChromeBrowsingDataRemoverDelegateTest, AutofillRemovalOlderThan30Days) {
+  GetProfile()->CreateWebDataService();
+  RemoveAutofillTester tester(GetProfile());
+
+  base::Time now = base::Time::Now();
+  const base::Time time_30days_ago = now - base::TimeDelta::FromDays(30);
+  const base::Time time_31days_ago = now - base::TimeDelta::FromDays(31);
+  const base::Time time_32days_ago = now - base::TimeDelta::FromDays(32);
+
+  // Add profiles and cards with modification date as 31 days old from now.
+  autofill::TestAutofillClock test_clock;
+  test_clock.SetNow(time_31days_ago);
+
+  ASSERT_FALSE(tester.HasProfile());
+  tester.AddProfilesAndCards();
+  ASSERT_TRUE(tester.HasProfile());
+
+  BlockUntilBrowsingDataRemoved(
+      base::Time(), time_32days_ago,
+      ChromeBrowsingDataRemoverDelegate::DATA_TYPE_FORM_DATA, false);
+  ASSERT_TRUE(tester.HasProfile());
+
+  BlockUntilBrowsingDataRemoved(
+      time_30days_ago, base::Time::Max(),
+      ChromeBrowsingDataRemoverDelegate::DATA_TYPE_FORM_DATA, false);
+  ASSERT_TRUE(tester.HasProfile());
+
+  BlockUntilBrowsingDataRemoved(
+      base::Time(), time_30days_ago,
+      ChromeBrowsingDataRemoverDelegate::DATA_TYPE_FORM_DATA, false);
   EXPECT_EQ(ChromeBrowsingDataRemoverDelegate::DATA_TYPE_FORM_DATA,
             GetRemovalMask());
   EXPECT_EQ(content::BrowsingDataRemover::ORIGIN_TYPE_UNPROTECTED_WEB,
