cb74128df49c8d3c01ab781763c456e36584e9b1
Jiaquan He <hejq@google.com>
2018-01-05 19:13:49 -0800

    app_list: handle ash-to-chrome calls.
    This is a pre-flight commit for breaking ChromeAppListItem
    from AppListItem. When we break the inheritance, we'll also
    break method calls between the two classes.
    
    AppListModelUpdater handles calls from Chrome to Ash. At this
    moment we put calls from Ash to Chrome (e.g. AppListItem::Activate
    and AppListItem::GetContextMenu) in AppListViewDelegate to avoid
    breakges. In the future when we move models from Chrome to Ash,
    they will be converted into mojo calls.
    
    Bug: 733662
    Change-Id: Ie09dc5d2f01172aa17d519ef7c8e0cb0e460eede

--- ui/app_list/app_list_view_delegate.h
+++ ui/app_list/app_list_view_delegate.h
@@ -5,12 +5,14 @@
 #ifndef UI_APP_LIST_APP_LIST_VIEW_DELEGATE_H_
 #define UI_APP_LIST_APP_LIST_VIEW_DELEGATE_H_
 
+#include <string>
 #include <vector>
 
 #include "base/strings/string16.h"
 #include "base/time/time.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/app_list/app_list_export.h"
+#include "ui/base/models/menu_model.h"
 
 namespace gfx {
 class Size;
@@ -82,6 +84,14 @@ class APP_LIST_EXPORT AppListViewDelegate {
 
   // Gets the wallpaper prominent colors.
   virtual void GetWallpaperProminentColors(std::vector<SkColor>* colors) = 0;
+
+  // Activates (opens) the item.
+  virtual void ActivateItem(const std::string& id, int event_flags) = 0;
+
+  // Returns the context menu model for this item, or NULL if there is currently
+  // no menu for the item (e.g. during install).
+  // Note the returned menu model is owned by this item.
+  virtual ui::MenuModel* GetContextMenuModel(const std::string& id) = 0;
 
   // Add/remove observer for AppListViewDelegate.
   virtual void AddObserver(AppListViewDelegateObserver* observer) = 0;
