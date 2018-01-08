bf00315eebf66359da57e2cbbf97d605e014a0b0
Daniel Bratell <bratell@opera.com>
2018-01-08 14:34:02 +0100

    Correcting include guard for safe_browsing/features.h
    Change-Id: Ie3bbe6779eeb321c72b76a255549afd10d9eb460

--- components/safe_browsing/features.h
+++ components/safe_browsing/features.h
@@ -50,5 +50,5 @@ extern const base::Feature kAppendRecentNavigationEvents;
 
 base::ListValue GetFeatureStatusList();
 
-#endif  // COMPONENTS_SAFE_BROWSING_FEATURES_H_
 }  // namespace safe_browsing
+#endif  // COMPONENTS_SAFE_BROWSING_FEATURES_H_
