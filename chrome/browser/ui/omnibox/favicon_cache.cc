19ba072328ede105f39d0fa5ffe545c89eb66bf9
Tommy C. Li <tommycli@chromium.org>
2018-01-08 10:44:17 -0800

    Omnibox UI Experiments: For FaviconCache, coalesce multiple requests.
    This CL prevents FaviconCache from hitting the underlying (uncached)
    FaviconService multiple times for the same page URL. Instead it keep
    track of its pending requests and add a callback to be called when
    the original request (for the same page URL) returns.
    
    Bug: 798490
    Change-Id: Ic8104c7ef5c48facec30270b3f006e70a0030901

--- chrome/browser/ui/omnibox/favicon_cache.cc
+++ chrome/browser/ui/omnibox/favicon_cache.cc
@@ -4,6 +4,8 @@
 
 #include "chrome/browser/ui/omnibox/favicon_cache.h"
 
+#include <utility>
+
 #include "base/containers/mru_cache.h"
 #include "components/favicon/core/favicon_service.h"
 #include "components/omnibox/browser/autocomplete_result.h"
@@ -37,26 +39,40 @@ gfx::Image FaviconCache::GetFaviconForPageUrl(
   if (!favicon_service_)
     return gfx::Image();
 
+  // We have an outstanding request for this page. Add one more waiting callback
+  // and return an empty gfx::Image.
+  auto it = pending_requests_.find(page_url);
+  if (it != pending_requests_.end()) {
+    pending_requests_.insert(
+        it, std::make_pair(page_url, std::move(on_favicon_fetched)));
+    return gfx::Image();
+  }
+
   // TODO(tommycli): Investigate using the version of this method that specifies
   // the desired size.
   favicon_service_->GetFaviconImageForPageURL(
       page_url,
       base::BindRepeating(&FaviconCache::OnFaviconFetched,
-                          weak_factory_.GetWeakPtr(), page_url,
-                          base::Passed(std::move(on_favicon_fetched))),
+                          weak_factory_.GetWeakPtr(), page_url),
       &task_tracker_);
+  pending_requests_.insert(
+      std::make_pair(page_url, std::move(on_favicon_fetched)));
 
   return gfx::Image();
 }
 
 void FaviconCache::OnFaviconFetched(
     const GURL& page_url,
-    FaviconFetchedCallback on_favicon_fetched,
     const favicon_base::FaviconImageResult& result) {
+  // TODO(tommycli): Also cache null image results with a reasonable expiry.
   if (result.image.IsEmpty())
     return;
 
   mru_cache_.Put(page_url, result.image);
 
-  std::move(on_favicon_fetched).Run(result.image);
+  auto requests = pending_requests_.equal_range(page_url);
+  for (auto it = requests.first; it != requests.second; ++it) {
+    std::move(it->second).Run(result.image);
+  }
+  pending_requests_.erase(requests.first, requests.second);
 }
--- chrome/browser/ui/omnibox/favicon_cache.h
+++ chrome/browser/ui/omnibox/favicon_cache.h
@@ -5,6 +5,8 @@
 #ifndef CHROME_BROWSER_UI_OMNIBOX_FAVICON_CACHE_H_
 #define CHROME_BROWSER_UI_OMNIBOX_FAVICON_CACHE_H_
 
+#include <map>
+
 #include "base/callback_forward.h"
 #include "base/containers/mru_cache.h"
 #include "base/macros.h"
@@ -37,13 +39,14 @@ class FaviconCache {
 
  private:
   void OnFaviconFetched(const GURL& page_url,
-                        FaviconFetchedCallback on_favicon_fetched,
                         const favicon_base::FaviconImageResult& result);
 
   // Non-owning pointer to a KeyedService.
   favicon::FaviconService* favicon_service_;
 
   base::CancelableTaskTracker task_tracker_;
+  std::multimap<GURL, FaviconFetchedCallback> pending_requests_;
+
   base::MRUCache<GURL, gfx::Image> mru_cache_;
   base::WeakPtrFactory<FaviconCache> weak_factory_;
 
