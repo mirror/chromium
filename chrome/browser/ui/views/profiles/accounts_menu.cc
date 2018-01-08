1297a259789fa8a2119549cce0c14c1afbfe4ddd
Thomas Tangl <tangltom@chromium.org>
2018-01-08 15:23:05 +0100

    Add accounts submenu to DICE user menu (turn-on-sync)
    When the "Sync to another account" button is clicked in the DICE
    user menu, a submenu showing a list of web accounts (other than the
    first one) should be displayed. This CL adds this feature.
    
    This CL only adds the buttons, i.e. the UI, but not the actions for
    them. The action handling will come with another CL.
    
    Mocks can be found in the bug report.
    
    Bug: 786369
    Change-Id: I582b1095b5e8004c2a0ff2a4fa3ab4fdbc19b5a7

---
+++ chrome/browser/ui/views/profiles/accounts_menu.cc
@@ -0,0 +1,93 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/profiles/accounts_menu.h"
+
+#include <vector>
+
+#include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/platform_util.h"
+#include "chrome/browser/profiles/profile_avatar_icon_util.h"
+#include "chrome/browser/ui/views/hover_button.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/image/image.h"
+#include "ui/views/controls/button/button.h"
+#include "ui/views/layout/box_layout.h"
+#include "ui/views/view.h"
+#include "ui/views/widget/widget.h"
+
+namespace {
+
+// Helpers --------------------------------------------------------------------
+
+constexpr int kFixedMenuWidth = 240;
+constexpr int kIconSize = 16;
+constexpr int kVerticalSpace = 8;
+
+}  // namespace
+
+// static
+views::Widget* menu_widget = nullptr;
+
+void AccountsMenu::ShowBubble(const std::vector<AccountInfo>& accounts,
+                              views::ButtonListener* listener,
+                              views::View* anchor_view) {
+  if (menu_widget)
+    return;
+  AccountsMenu* bubble = new AccountsMenu(accounts, listener, anchor_view);
+  menu_widget = views::BubbleDialogDelegateView::CreateBubble(bubble);
+  bubble->SetAlignment(views::BubbleBorder::ALIGN_ARROW_TO_MID_ANCHOR);
+  bubble->SetArrowPaintType(views::BubbleBorder::PAINT_NONE);
+  menu_widget->Show();
+}
+
+AccountsMenu::AccountsMenu(const std::vector<AccountInfo>& accounts,
+                           views::ButtonListener* listener,
+                           views::View* anchor_view)
+    : BubbleDialogDelegateView(anchor_view, views::BubbleBorder::TOP_RIGHT),
+      listener_(listener),
+      accounts_(accounts) {}
+
+void AccountsMenu::Init() {
+  std::unique_ptr<views::BoxLayout> layout = std::make_unique<views::BoxLayout>(
+      views::BoxLayout::kVertical, gfx::Insets(kVerticalSpace, 0), 0);
+  layout->set_minimum_cross_axis_size(kFixedMenuWidth);
+  this->SetLayoutManager(std::move(layout));
+
+  // TODO(http://crbug.com/794522): Use the account pictures instead of the
+  // default avatar.
+  gfx::Image default_icon =
+      ui::ResourceBundle::GetSharedInstance().GetImageNamed(
+          profiles::GetPlaceholderAvatarIconResourceID());
+  gfx::Image default_icon_circle = profiles::GetSizedAvatarIcon(
+      default_icon, true, kIconSize, kIconSize, profiles::SHAPE_CIRCLE);
+  for (const AccountInfo& account : accounts_) {
+    views::View* hover_button =
+        new HoverButton(listener_, *default_icon_circle.ToImageSkia(),
+                        base::UTF8ToUTF16(account.email));
+    this->AddChildView(hover_button);
+  }
+  views::View* use_another_account =
+      new HoverButton(listener_, *default_icon_circle.ToImageSkia(),
+                      base::ASCIIToUTF16("Use another account"));
+  this->AddChildView(use_another_account);
+}
+
+int AccountsMenu::GetDialogButtons() const {
+  return ui::DIALOG_BUTTON_NONE;
+}
+
+void AccountsMenu::WindowClosing() {
+  menu_widget = nullptr;
+}
+
+void AccountsMenu::OnWidgetDestroyed(views::Widget* widget) {
+  views::Widget* parent_widget = GetAnchorView()->GetWidget();
+  gfx::NativeWindow window =
+      platform_util::GetTopLevel(parent_widget->GetNativeView());
+  if (!platform_util::IsWindowActive(window))
+    parent_widget->Close();
+}
+
+AccountsMenu::~AccountsMenu() {}
