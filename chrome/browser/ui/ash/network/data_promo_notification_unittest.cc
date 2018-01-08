3210f22bdf3bc7be993bbf158de1e55ae737e016
Steven Bennetts <stevenjb@chromium.org>
2018-01-08 09:04:15 -0800

    Move network ui from c/b/chromeos to c/b/ui/ash
    Bug: 798569
    Change-Id: Ia6275766b1a31afdb26404aeee544acad0dde1b9

--- chrome/browser/ui/ash/network/data_promo_notification_unittest.cc
+++ chrome/browser/ui/ash/network/data_promo_notification_unittest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/status/data_promo_notification.h"
+#include "chrome/browser/ui/ash/network/data_promo_notification.h"
 
 #include "base/command_line.h"
 #include "base/macros.h"
