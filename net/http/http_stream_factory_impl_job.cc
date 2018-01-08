c6a81547a90b793f00ab4efa31652d8d97b3b371
Paul Jensen <pauljensen@chromium.org>
2018-01-08 13:06:26 -0500

    Plumb SocketTag from HttpRequestInfo down to sockets through pools
    Sockets are tagged in three places:
    1. TransportClientSockets are tagged immediately prior to Connect() so that TCP connect packets are
       correctly tagged and hence the traffic can be accurately attributed.
    2. ClientSocketPoolBase tags sockets returned synchronously, e.g. reused idle sockets.
    3. ClientSocketPoolBase tags sockets returned asynchronously, e.g. newly created and connected
       sockets, and re-adopted orphaned connect jobs.
    
    Proxying and SSL sockets pass the tag down to underlying TransportClientSockets during creation
    via their TransportSocketParams, and apply the tag on the underlying TransportClientSockets when
    retagging.
    
    Tests verify the various ClientSocketPool impls tag sockets appropriately.
    
    Socket tagging is only supported via Cronet for the time being so the following situations are
    not supported (either return ERR_NOT_IMPLEMENTED or CHECK(false) or don't accept SocketTag):
    1. WebSockets
    2. tunneling proxy connections
    3. preconnecting
    
    The following are unsupported but will hopefully be supported in future CLs:
    1. Tagging SpdySessions
    2. Tagging QuicSessions
    
    Bug: 520198
    Change-Id: I758c07aeb202b68c79bfe450127c26dbb403deb2

--- net/http/http_stream_factory_impl_job.cc
+++ net/http/http_stream_factory_impl_job.cc
@@ -949,6 +949,8 @@ int HttpStreamFactoryImpl::Job::DoInitConnectionImpl() {
 
   if (job_type_ == PRECONNECT) {
     DCHECK(!delegate_->for_websockets());
+    if (request_info_.socket_tag != SocketTag())
+      return ERR_NOT_IMPLEMENTED;
     return PreconnectSocketsForHttpRequest(
         GetSocketGroup(), destination_, request_info_.extra_headers,
         request_info_.load_flags, priority_, session_, proxy_info_,
@@ -965,6 +967,8 @@ int HttpStreamFactoryImpl::Job::DoInitConnectionImpl() {
                        spdy_session_key_, enable_ip_based_pooling_)
           : OnHostResolutionCallback();
   if (delegate_->for_websockets()) {
+    if (request_info_.socket_tag != SocketTag())
+      return ERR_NOT_IMPLEMENTED;
     SSLConfig websocket_server_ssl_config = server_ssl_config_;
     websocket_server_ssl_config.alpn_protos.clear();
     return InitSocketHandleForWebSocketRequest(
@@ -979,8 +983,8 @@ int HttpStreamFactoryImpl::Job::DoInitConnectionImpl() {
       GetSocketGroup(), destination_, request_info_.extra_headers,
       request_info_.load_flags, priority_, session_, proxy_info_, expect_spdy_,
       quic_version_, server_ssl_config_, proxy_ssl_config_,
-      request_info_.privacy_mode, net_log_, connection_.get(),
-      resolution_callback, io_callback_);
+      request_info_.privacy_mode, request_info_.socket_tag, net_log_,
+      connection_.get(), resolution_callback, io_callback_);
 }
 
 void HttpStreamFactoryImpl::Job::OnQuicHostResolution(int result) {
--- net/socket/socket_test_util.cc
+++ net/socket/socket_test_util.cc
@@ -1260,6 +1260,10 @@ bool MockSSLClientSocket::GetSSLInfo(SSLInfo* requested_ssl_info) {
   return true;
 }
 
+void MockSSLClientSocket::ApplySocketTag(const SocketTag& tag) {
+  return transport_->socket()->ApplySocketTag(tag);
+}
+
 void MockSSLClientSocket::GetSSLCertRequestInfo(
     SSLCertRequestInfo* cert_request_info) {
   DCHECK(cert_request_info);
@@ -1679,6 +1683,7 @@ int MockTransportClientSocketPool::RequestSocket(
     const std::string& group_name,
     const void* socket_params,
     RequestPriority priority,
+    const SocketTag& socket_tag,
     RespectLimits respect_limits,
     ClientSocketHandle* handle,
     const CompletionCallback& callback,
@@ -1734,13 +1739,14 @@ MockSOCKSClientSocketPool::~MockSOCKSClientSocketPool() = default;
 int MockSOCKSClientSocketPool::RequestSocket(const std::string& group_name,
                                              const void* socket_params,
                                              RequestPriority priority,
+                                             const SocketTag& socket_tag,
                                              RespectLimits respect_limits,
                                              ClientSocketHandle* handle,
                                              const CompletionCallback& callback,
                                              const NetLogWithSource& net_log) {
   return transport_pool_->RequestSocket(group_name, socket_params, priority,
-                                        respect_limits, handle, callback,
-                                        net_log);
+                                        socket_tag, respect_limits, handle,
+                                        callback, net_log);
 }
 
 void MockSOCKSClientSocketPool::SetPriority(const std::string& group_name,
@@ -1952,5 +1958,18 @@ uint64_t GetTaggedBytes(int32_t expected_tag) {
   return bytes;
 }
 #endif
+
+std::unique_ptr<StreamSocket>
+MockTaggingClientSocketFactory::CreateTransportClientSocket(
+    const AddressList& addresses,
+    std::unique_ptr<SocketPerformanceWatcher> socket_performance_watcher,
+    NetLog* net_log,
+    const NetLogSource& source) {
+  std::unique_ptr<MockTaggingStreamSocket> socket(new MockTaggingStreamSocket(
+      MockClientSocketFactory::CreateTransportClientSocket(
+          addresses, std::move(socket_performance_watcher), net_log, source)));
+  socket_ = socket.get();
+  return std::move(socket);
+}
 
 }  // namespace net
