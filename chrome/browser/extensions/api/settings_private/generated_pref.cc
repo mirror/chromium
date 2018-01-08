07744aa2c275a9b53ee2d0b52a3267d0a354a364
Alexander Alekseev <alemate@chromium.org>
2018-01-08 13:16:34 -0800

    Chrome settings_private API: implement GeneratedPrefs
    This Cl implements GeneratedPrefs "pref storage" to simplify
    creating "virtual user prefs" that exist only for Settings API.
    It can be used to implement Settings UI control elements that do not
    directly map to real user preferences.
    
    Bug: 721578
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I3195ec463e0d9eaa5e23ed0061e3d9b93eff7fca

---
+++ chrome/browser/extensions/api/settings_private/generated_pref.cc
@@ -0,0 +1,27 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/api/settings_private/generated_pref.h"
+
+#include "base/callback.h"
+
+namespace extensions {
+namespace settings_private {
+
+GeneratedPref::Observer::Observer() = default;
+GeneratedPref::Observer::~Observer() = default;
+
+GeneratedPref::GeneratedPref() = default;
+GeneratedPref::~GeneratedPref() = default;
+
+void GeneratedPref::AddObserver(Observer* observer) {
+  observers_.AddObserver(observer);
+}
+
+void GeneratedPref::RemoveObserver(Observer* observer) {
+  observers_.RemoveObserver(observer);
+}
+
+}  // namespace settings_private
+}  // namespace extensions
