830ddd60f2cc08ace61ee826255d8630fe87f754
Geoff Lang <geofflang@chromium.org>
2018-01-08 14:32:24 -0500

    Rebaseline MP4 video tests for intel and temporarily supress VP9 failure.
    TBR=zmo@chromium.org
    
    BUG=602688
    
    Change-Id: I444e8786012d0dcb06b4b74fa4acac9e83d94970

--- content/test/gpu/gpu_tests/pixel_expectations.py
+++ content/test/gpu/gpu_tests/pixel_expectations.py
@@ -99,5 +99,4 @@ class PixelExpectations(GpuTestExpectations):
 
     # Temporary supression to rebaseline Video tests on Windows with the
     # passthrough command decoder
-    self.Fail('Pixel_DirectComposition_Video_MP4', ['win'], bug=602688)
-    self.Fail('Pixel_DirectComposition_Video_VP9', ['win'], bug=602688)
+    self.Fail('Pixel_Video_VP9', ['win', 'intel'], bug=602688)
