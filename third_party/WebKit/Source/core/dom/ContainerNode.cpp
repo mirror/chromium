01b1271866ed145a7d1e11e5a4dffb4aa8d6211e
Adithya Srinivasan <adithyas@chromium.org>
2018-01-08 15:38:43 -0500

    Skip NodeList cache invalidation for text change
    For any updates to character data, we can skip invalidation of node
    list caches as no nodes are added or removed.
    
    Bug:
    Change-Id: Ibb22c9e519d37b785c3c71b809dfe10b88981b24

--- third_party/WebKit/Source/core/dom/ContainerNode.cpp
+++ third_party/WebKit/Source/core/dom/ContainerNode.cpp
@@ -1629,6 +1629,9 @@ void ContainerNode::InvalidateNodeListCachesInAncestors(
     const QualifiedName* attr_name,
     Element* attribute_owner_element,
     const ChildrenChange* change) {
+  if (change && change->type == kTextChanged)
+    return;
+
   if (HasRareData() && (!attr_name || IsAttributeNode())) {
     if (NodeListsNodeData* lists = RareData()->NodeLists()) {
       if (ChildNodeList* child_node_list = lists->GetChildNodeList(*this)) {
