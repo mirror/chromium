f9ca274d79caaeb8b720d0c7711fc446fb86b884
Elliot Glaysher <erg@chromium.org>
2018-01-08 13:21:44 -0800

    Add a metric for mouse events in LatencyTracker.
    Bug: 798775
    Change-Id: I21dceea4719da385103eed485e0d99d945469c72

--- ui/latency/latency_tracker.cc
+++ ui/latency/latency_tracker.cc
@@ -26,6 +26,8 @@ std::string LatencySourceEventTypeToInputModalityString(
   switch (type) {
     case ui::SourceEventType::WHEEL:
       return "Wheel";
+    case ui::SourceEventType::MOUSE:
+      return "Mouse";
     case ui::SourceEventType::TOUCH:
       return "Touch";
     case ui::SourceEventType::KEY_PRESS:
@@ -91,12 +93,12 @@ void LatencyTracker::OnGpuSwapBuffersCompleted(const LatencyInfo& latency) {
 
   ui::SourceEventType source_event_type = latency.source_event_type();
   if (source_event_type == ui::SourceEventType::WHEEL ||
+      source_event_type == ui::SourceEventType::MOUSE ||
       source_event_type == ui::SourceEventType::TOUCH ||
       source_event_type == ui::SourceEventType::KEY_PRESS) {
     ComputeEndToEndLatencyHistograms(gpu_swap_begin_component,
                                      gpu_swap_end_component, latency);
   }
-
 }
 
 void LatencyTracker::ReportRapporScrollLatency(
@@ -219,6 +221,10 @@ void LatencyTracker::ComputeEndToEndLatencyHistograms(
     if (input_modality == "KeyPress") {
       UMA_HISTOGRAM_INPUT_LATENCY_HIGH_RESOLUTION_MICROSECONDS(
           "Event.Latency.EndToEnd.KeyPress", original_component,
+          gpu_swap_begin_component);
+    } else if (input_modality == "Mouse") {
+      UMA_HISTOGRAM_INPUT_LATENCY_HIGH_RESOLUTION_MICROSECONDS(
+          "Event.Latency.EndToEnd.Mouse", original_component,
           gpu_swap_begin_component);
     }
     return;
