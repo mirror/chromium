968cd7b2f88e658534e5c233625256518b7051b7
Lalit Maganti <lalitm@chromium.org>
2018-01-08 12:28:00 +0000

    memory-infra: split up memory-infra coordinator service into two
    This allows for heap profiler to use its own service with correct
    capabilities and all other instances to use the existing coordinator
    service.
    
    Bug: 792028
    Change-Id: I84e4ec71f5f1d00991c0516b1424ce7334bcd3cd

--- chrome/profiling/profiling_service.cc
+++ chrome/profiling/profiling_service.cc
@@ -5,8 +5,11 @@
 #include "chrome/profiling/profiling_service.h"
 
 #include "base/logging.h"
+#include "content/public/common/service_manager_connection.h"
 #include "mojo/public/cpp/system/platform_handle.h"
-#include "services/resource_coordinator/public/cpp/memory_instrumentation/memory_instrumentation.h"
+#include "services/resource_coordinator/public/cpp/resource_coordinator_features.h"
+#include "services/resource_coordinator/public/interfaces/memory_instrumentation/memory_instrumentation.mojom.h"
+#include "services/resource_coordinator/public/interfaces/service_constants.mojom.h"
 
 namespace profiling {
 
@@ -75,12 +78,15 @@ void ProfilingService::DumpProcessesForTracing(
     DumpProcessesForTracingCallback callback) {
   // Need a memory map to make sense of the dump. The dump will be triggered
   // in the memory map global dump callback.
-  // TODO(brettw) this should be a OnceCallback to avoid base::Passed.
-  memory_instrumentation::MemoryInstrumentation::GetInstance()
-      ->GetVmRegionsForHeapProfiler(base::Bind(
-          &ProfilingService::OnGetVmRegionsCompleteForDumpProcessesForTracing,
-          weak_factory_.GetWeakPtr(), keep_small_allocations,
-          strip_path_from_mapped_files, base::Passed(&callback)));
+  service_manager::Connector* connector =
+      content::ServiceManagerConnection::GetForProcess()->GetConnector();
+  connector->BindInterface(resource_coordinator::mojom::kServiceName,
+                           mojo::MakeRequest(&helper_));
+
+  helper_->GetVmRegionsForHeapProfiler(base::Bind(
+      &ProfilingService::OnGetVmRegionsCompleteForDumpProcessesForTracing,
+      weak_factory_.GetWeakPtr(), keep_small_allocations,
+      strip_path_from_mapped_files, base::Passed(&callback)));
 }
 
 void ProfilingService::GetProfiledPids(GetProfiledPidsCallback callback) {
