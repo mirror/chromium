94ccd0c6658a4142a41c1a1d3bfd7f4ad3f2953b
Christopher Lam <calamity@chromium.org>
2018-01-08 14:12:07 +1100

    Reland "Expire redirect parents when expiring any history visit."
    This is a reland of e4d9e6a382b2f94e01abb2d7d09473b636717ba4
    Original change's description:
    > Expire redirect parents when expiring any history visit.
    > 
    > This CL fixes an issue with history deletion where Visits that were
    > the end of redirect chains were not deleting their redirect parents.
    > In effect, removing a single history entry from chrome://history was
    > not removing all Visits associated with it, causing similar URLs to
    > continue to show up in the omnibox autocomplete.
    > 
    > Bug: 786878
    > Change-Id: Id207dd7810629458d19a847d3ff52c0e562f2ce6
    > Reviewed-on: https://chromium-review.googlesource.com/795527
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Commit-Queue: calamity <calamity@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#525589}
    
    Bug: 786878
    Change-Id: I18ab0ce4ba2dd4e14fff4ab79d58a9714b6003c7

--- components/history/core/browser/expire_history_backend.cc
+++ components/history/core/browser/expire_history_backend.cc
@@ -12,11 +12,13 @@
 
 #include "base/bind.h"
 #include "base/compiler_specific.h"
+#include "base/containers/flat_set.h"
 #include "base/feature_list.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
 #include "base/location.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/sequenced_task_runner.h"
 #include "components/history/core/browser/history_backend_client.h"
 #include "components/history/core/browser/history_backend_notifier.h"
@@ -262,18 +264,30 @@ void ExpireHistoryBackend::ExpireVisits(const VisitVector& visits) {
   if (visits.empty())
     return;
 
+  base::Time start = base::Time::Now();
+
+  const VisitVector visits_and_redirects = GetVisitsAndRedirectParents(visits);
+  base::TimeDelta get_redirects_time = base::Time::Now() - start;
+
   DeleteEffects effects;
-  DeleteVisitRelatedInfo(visits, &effects);
+  DeleteVisitRelatedInfo(visits_and_redirects, &effects);
 
   // Delete or update the URLs affected. We want to update the visit counts
   // since this is called by the user who wants to delete their recent history,
   // and we don't want to leave any evidence.
-  ExpireURLsForVisits(visits, &effects);
+  ExpireURLsForVisits(visits_and_redirects, &effects);
   DeleteFaviconsIfPossible(&effects);
   BroadcastNotifications(&effects, DELETION_USER_INITIATED);
 
   // Pick up any bits possibly left over.
   ParanoidExpireHistory();
+
+  base::TimeDelta expire_visits_time = base::Time::Now() - start;
+  UMA_HISTOGRAM_TIMES("History.ExpireVisitsTime", expire_visits_time);
+  if (!expire_visits_time.is_zero()) {
+    UMA_HISTOGRAM_COUNTS_100("History.GetRedirectsTimePercentage",
+                             get_redirects_time * 100 / expire_visits_time);
+  }
 }
 
 void ExpireHistoryBackend::ExpireHistoryBefore(base::Time end_time) {
@@ -355,6 +369,24 @@ void ExpireHistoryBackend::BroadcastNotifications(DeleteEffects* effects,
                                  effects->deleted_urls,
                                  effects->deleted_favicons);
   }
+}
+
+VisitVector ExpireHistoryBackend::GetVisitsAndRedirectParents(
+    const VisitVector& visits) {
+  base::flat_set<VisitID> seen_visits;
+  VisitVector visits_and_redirects;
+  for (const auto v : visits) {
+    VisitRow current_visit = v;
+    do {
+      if (!seen_visits.insert(current_visit.visit_id).second)
+        break;
+
+      visits_and_redirects.push_back(current_visit);
+    } while (current_visit.referring_visit &&
+             main_db_->GetRowForVisit(current_visit.referring_visit,
+                                      &current_visit));
+  }
+  return visits_and_redirects;
 }
 
 void ExpireHistoryBackend::DeleteVisitRelatedInfo(const VisitVector& visits,
