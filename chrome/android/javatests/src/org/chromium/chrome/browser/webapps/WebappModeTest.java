349e88bc471f6e9ecdc2554ab8b86b4d3c9bca2d
Ran Ji <ranj@chromium.org>
2018-01-08 13:08:45 -0500

    Write WebAPK integration test which uses WebappLauncherActivity
    Bug:735772
    Change-Id: Ie4d2d4ac27de9ff3cbfd2058dcb1eeb09d975d44

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappModeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappModeTest.java
@@ -133,6 +133,26 @@ public class WebappModeTest {
     }
 
     /**
+     * Tests that WebappActivities are started properly by WebappLauncherActivity.
+     */
+    @Test
+    @MediumTest
+    @Feature({"Webapps"})
+    public void testWebappLaunchesByLauncherActivity() {
+        WebappInfo webappInfo = WebappInfo.create(
+                createIntent(WEBAPP_1_ID, WEBAPP_1_URL, WEBAPP_1_TITLE, WEBAPP_ICON, true));
+        Intent launchIntent = WebappLauncherActivity.createWebappLaunchIntent(webappInfo, false);
+        WebappActivityTestRule mActivityTestRule = new WebappActivityTestRule();
+
+        WebappActivity webappActivity =
+                (WebappActivity) mActivityTestRule.startAndWaitForActivityType(
+                        launchIntent, WebappActivity.class);
+
+        Assert.assertEquals(webappInfo.id(), webappActivity.getWebappInfo().id());
+        Assert.assertEquals(webappInfo.uri(), webappActivity.getWebappInfo().uri());
+    }
+
+    /**
      * Tests that WebappActivities are started properly.
      */
     @Test
