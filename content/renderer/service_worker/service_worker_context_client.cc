e6b8b36a71e8f5e8c05314e049df8a0e0bc923e4
xzhan96 <xiaofeng.zhang@intel.com>
2018-01-08 14:26:22 +0800

    [ServiceWorker] Implement mojom interface ServiceWorkerHost.GetClients
    This CL replaces native struct content::ServiceWorkerClientQueryOptions
    with mojom struct blink.mojom.ServiceWorkerClientQueryOptions.
    
    And based on that, the CL mojofies the following legacy IPCs:
    ServiceWorkerHostMsg_GetClients
    ServiceWorkerMsg_DidGetClients
    
    BUG=782528, 772793
    
    Change-Id: If72455743b2ddbef8da012831b16a9a9ce38a8ff

--- content/renderer/service_worker/service_worker_context_client.cc
+++ content/renderer/service_worker/service_worker_context_client.cc
@@ -365,8 +365,6 @@ void OnResponseBlobDispatchDone(
 // Holding data that needs to be bound to the worker context on the
 // worker thread.
 struct ServiceWorkerContextClient::WorkerContextData {
-  using ClientsCallbacksMap =
-      base::IDMap<std::unique_ptr<blink::WebServiceWorkerClientsCallbacks>>;
   using ClientCallbacksMap =
       base::IDMap<std::unique_ptr<blink::WebServiceWorkerClientCallbacks>>;
   using SkipWaitingCallbacksMap =
@@ -392,9 +390,6 @@ struct ServiceWorkerContextClient::WorkerContextData {
   // only owner is |this|.
   scoped_refptr<blink::mojom::ThreadSafeServiceWorkerHostAssociatedPtr>
       service_worker_host;
-
-  // Pending callbacks for GetClientDocuments().
-  ClientsCallbacksMap clients_callbacks;
 
   // Pending callbacks for OpenWindow() and FocusClient().
   ClientCallbacksMap client_callbacks;
@@ -672,7 +667,6 @@ void ServiceWorkerContextClient::OnMessageReceived(
   bool handled = true;
   IPC_BEGIN_MESSAGE_MAP(ServiceWorkerContextClient, message)
     IPC_MESSAGE_HANDLER(ServiceWorkerMsg_DidGetClient, OnDidGetClient)
-    IPC_MESSAGE_HANDLER(ServiceWorkerMsg_DidGetClients, OnDidGetClients)
     IPC_MESSAGE_HANDLER(ServiceWorkerMsg_OpenWindowResponse,
                         OnOpenWindowResponse)
     IPC_MESSAGE_HANDLER(ServiceWorkerMsg_OpenWindowError,
@@ -702,12 +696,16 @@ void ServiceWorkerContextClient::GetClients(
     const blink::WebServiceWorkerClientQueryOptions& weboptions,
     std::unique_ptr<blink::WebServiceWorkerClientsCallbacks> callbacks) {
   DCHECK(callbacks);
-  int request_id = context_->clients_callbacks.Add(std::move(callbacks));
-  ServiceWorkerClientQueryOptions options;
-  options.client_type = weboptions.client_type;
-  options.include_uncontrolled = weboptions.include_uncontrolled;
-  Send(new ServiceWorkerHostMsg_GetClients(
-      GetRoutingID(), request_id, options));
+  auto options = blink::mojom::ServiceWorkerClientQueryOptions::New(
+      weboptions.include_uncontrolled, weboptions.client_type);
+  // base::Unretained(this) is safe because the callback passed to
+  // GetClients() is owned by |context_->service_worker_host|, whose only
+  // owner is |this| and won't outlive |this|.
+  (*context_->service_worker_host)
+      ->GetClients(
+          std::move(options),
+          base::BindOnce(&ServiceWorkerContextClient::OnDidGetClients,
+                         base::Unretained(this), std::move(callbacks)));
 }
 
 void ServiceWorkerContextClient::OpenNewTab(
@@ -1641,24 +1639,17 @@ void ServiceWorkerContextClient::OnDidGetClient(
 }
 
 void ServiceWorkerContextClient::OnDidGetClients(
-    int request_id,
-    const std::vector<blink::mojom::ServiceWorkerClientInfo>& clients) {
+    std::unique_ptr<blink::WebServiceWorkerClientsCallbacks> callbacks,
+    std::vector<blink::mojom::ServiceWorkerClientInfoPtr> clients) {
   TRACE_EVENT0("ServiceWorker",
                "ServiceWorkerContextClient::OnDidGetClients");
-  blink::WebServiceWorkerClientsCallbacks* callbacks =
-      context_->clients_callbacks.Lookup(request_id);
-  if (!callbacks) {
-    NOTREACHED() << "Got stray response: " << request_id;
-    return;
-  }
   blink::WebServiceWorkerClientsInfo info;
   blink::WebVector<blink::WebServiceWorkerClientInfo> convertedClients(
       clients.size());
   for (size_t i = 0; i < clients.size(); ++i)
-    convertedClients[i] = ToWebServiceWorkerClientInfo(clients[i]);
+    convertedClients[i] = ToWebServiceWorkerClientInfo(*(clients[i]));
   info.clients.Swap(convertedClients);
   callbacks->OnSuccess(info);
-  context_->clients_callbacks.Remove(request_id);
 }
 
 void ServiceWorkerContextClient::OnOpenWindowResponse(
