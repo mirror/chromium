84ad02806833ae720b2327f681639f3e5289028c
Sandra Sun <sunyunjia@chromium.org>
2018-01-09 18:17:38 -0500

    Support viewport to snap on main.
    Bug: 778257
    Change-Id: Ife2f2068eccf4c0607b3f33765a49c1ee53c36a2

--- third_party/WebKit/Source/core/input/ScrollSnapTest.cpp
+++ third_party/WebKit/Source/core/input/ScrollSnapTest.cpp
@@ -174,4 +174,53 @@ TEST_F(ScrollSnapTest, ScrollSnapOnBoth) {
   ASSERT_EQ(scroller->scrollTop(), 200);
 }
 
+TEST_F(ScrollSnapTest, SnapOnViewport) {
+  WebView().Resize(WebSize(300, 300));
+  SimRequest request("https://example.com/test.html", "text/html");
+  LoadURL("https://example.com/test.html");
+  request.Complete(R"HTML(
+    <style>
+    body {
+      scroll-snap-type: both mandatory;
+      margin: 0px;
+    }
+    #container {
+      margin: 0px;
+      padding: 0px;
+      width: 500px;
+      height: 500px;
+    }
+    #area {
+      position: relative;
+      left: 200px;
+      top: 200px;
+      width: 100px;
+      height: 100px;
+      scroll-snap-align: start;
+    }
+    </style>
+    <div id='container'>
+      <div id='area'></div>
+    </div>
+  )HTML");
+
+  Compositor().BeginFrame();
+
+  ScrollBegin(100, 100, 0, -50);
+  // The finger moves left.
+  ScrollUpdate(100, 100, 0, -50);
+  // The finger moves up.
+  ScrollUpdate(100, 50, -50, 0);
+  ScrollEnd(50, 50);
+
+  // Wait for animation to finish.
+  Compositor().BeginFrame();  // update run_state_.
+  Compositor().BeginFrame();  // Set start_time = now.
+  Compositor().BeginFrame(0.3);
+
+  // A scroll gesture that has move in both x and y would snap on both axes.
+  ASSERT_EQ(Window().scrollX(), 200);
+  ASSERT_EQ(Window().scrollY(), 200);
+}
+
 }  // namespace blink
