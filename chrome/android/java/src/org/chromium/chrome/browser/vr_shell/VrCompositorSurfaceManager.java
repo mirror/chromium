f7bcf101bc2fcc4a9ecac61df87a8d057384dc9c
Michael Thiessen <mthiesse@chromium.org>
2018-01-09 14:12:28 -0500

    Allow VR to reuse the CompositorView with its own Surface
    This CL allows the CompositorSurfaceManager to be replaced while in VR,
    and allows VR to provide the Surface to render into.
    
    The WindowAndroid the CompositorImplAndroid uses also has to be swapped
    out as this isn't yet covered by the Tab reparenting flow (presumably
    because it doesn't expect the reparented tab to stay in the same
    Activity).
    
    This CL was split off of
    https://chromium-review.googlesource.com/c/chromium/src/+/652354, where
    this new path is actually used.
    
    Bug: 793430
    Change-Id: Id210df51b6f11206d492bb21ec0a64b09e45f953

---
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrCompositorSurfaceManager.java
@@ -0,0 +1,85 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.vr_shell;
+
+import android.graphics.drawable.Drawable;
+import android.view.Surface;
+
+import org.chromium.chrome.browser.compositor.CompositorSurfaceManager;
+
+/**
+ * Provides the texture-backed surface used for drawing Android UI in VR to the CompositorView.
+ */
+public class VrCompositorSurfaceManager implements CompositorSurfaceManager {
+    private boolean mSurfaceRequestPending;
+    private boolean mSurfaceProvided;
+    private Surface mSurface;
+    private int mFormat;
+    private int mWidth;
+    private int mHeight;
+
+    // Client that we notify about surface change events.
+    private SurfaceManagerCallbackTarget mClient;
+
+    public VrCompositorSurfaceManager(SurfaceManagerCallbackTarget client) {
+        mClient = client;
+    }
+
+    /* package */ void setSurface(Surface surface, int format, int width, int height) {
+        mSurface = surface;
+        mFormat = format;
+        mWidth = width;
+        mHeight = height;
+        if (mSurfaceRequestPending) {
+            mClient.surfaceCreated(mSurface);
+            mClient.surfaceChanged(mSurface, mFormat, mWidth, mHeight);
+            mSurfaceProvided = true;
+        }
+        mSurfaceRequestPending = false;
+    }
+
+    /* package */ void surfaceResized(int width, int height) {
+        mWidth = width;
+        mHeight = height;
+        if (mSurfaceProvided) mClient.surfaceChanged(mSurface, mFormat, mWidth, mHeight);
+    }
+
+    /* package */ void surfaceDestroyed() {
+        shutDown();
+    }
+
+    @Override
+    public void shutDown() {
+        if (mSurface != null && mSurfaceProvided) mClient.surfaceDestroyed();
+        mSurface = null;
+        mSurfaceProvided = false;
+    }
+
+    @Override
+    public void requestSurface(int format) {
+        if (mSurface == null) {
+            mSurfaceRequestPending = true;
+            return;
+        }
+        mClient.surfaceCreated(mSurface);
+        mClient.surfaceChanged(mSurface, mFormat, mWidth, mHeight);
+        mSurfaceProvided = true;
+    }
+
+    @Override
+    public void doneWithUnownedSurface() {}
+
+    @Override
+    public void recreateSurfaceForJellyBean() {}
+
+    @Override
+    public void setBackgroundDrawable(Drawable background) {}
+
+    @Override
+    public void setWillNotDraw(boolean willNotDraw) {}
+
+    @Override
+    public void setVisibility(int visibility) {}
+}
