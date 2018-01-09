d7c5321ace84629222ad327ffc5a77a71f4ec858
Steven Bennetts <stevenjb@chromium.org>
2018-01-09 09:30:59 -0800

    Move network ui from c/b/chromeos to c/b/ui/ash
    This primarily moves some UI focused networking files from
    c/b/chromeos to c/b/ui/ash.
    
    It also moves the UI classes that were owned by
    ChromeBrowserMainChromeOS to ChromeBrowserMainExtraPartsAsh.
    
    Bug: 798569
    Change-Id: Ia6275766b1a31afdb26404aeee544acad0dde1b9

--- chrome/browser/ui/ash/network/data_promo_notification.h
+++ chrome/browser/ui/ash/network/data_promo_notification.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_CHROMEOS_STATUS_DATA_PROMO_NOTIFICATION_H_
-#define CHROME_BROWSER_CHROMEOS_STATUS_DATA_PROMO_NOTIFICATION_H_
+#ifndef CHROME_BROWSER_UI_ASH_NETWORK_DATA_PROMO_NOTIFICATION_H_
+#define CHROME_BROWSER_UI_ASH_NETWORK_DATA_PROMO_NOTIFICATION_H_
 
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
@@ -47,4 +47,4 @@ class DataPromoNotification : public NetworkStateHandlerObserver {
 
 }  // namespace chromeos
 
-#endif  // CHROME_BROWSER_CHROMEOS_STATUS_DATA_PROMO_NOTIFICATION_H_
+#endif  // CHROME_BROWSER_UI_ASH_NETWORK_DATA_PROMO_NOTIFICATION_H_
