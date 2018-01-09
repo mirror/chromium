d3408848812a43d6c7de62ca92f9ef119e8d87bd
Jun Choi <hongjunchoi@chromium.org>
2018-01-09 00:08:30 -0800

    Create objects to encapsulate CTAP device reponse
    Added classes to encapsulate response data received from CTAP
    devices as specified by the CTAP protocol.
    
    https://fidoalliance.org/specs/fido-v2.0-rd-20170927/fido-client-to-authenticator-protocol-v2.0-rd-20170927.html#authenticatorMakeCredential
    
    Bug:796782
    Change-Id: If5a80846d565ab6a7919aa3236ff78e22b5209ae

---
+++ device/ctap/ctap_authenticator_response.h
@@ -0,0 +1,31 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DEVICE_CTAP_CTAP_AUTHENTICATOR_RESPONSE_H_
+#define DEVICE_CTAP_CTAP_AUTHENTICATOR_RESPONSE_H_
+
+#include "device/ctap/ctap_constants.h"
+
+namespace device {
+
+// Response object for AuthenticatorCancel and AuthenticatorReset commands.
+class CTAPAuthenticatorResponse {
+ public:
+  explicit CTAPAuthenticatorResponse(
+      constants::CTAPDeviceResponseCode response_code);
+  CTAPAuthenticatorResponse(CTAPAuthenticatorResponse&& that);
+  CTAPAuthenticatorResponse& operator=(CTAPAuthenticatorResponse&& other);
+  ~CTAPAuthenticatorResponse();
+
+  constants::CTAPDeviceResponseCode response_code() const {
+    return response_code_;
+  }
+
+ private:
+  constants::CTAPDeviceResponseCode response_code_;
+};
+
+}  // namespace device
+
+#endif  // DEVICE_CTAP_CTAP_AUTHENTICATOR_RESPONSE_H_
