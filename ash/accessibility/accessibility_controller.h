7cb9dd19ab220551135d518f96b2852d68e1307d
Qiang Xu <warx@chromium.org>
2018-01-09 17:02:05 +0800

    reland: mash: make ash learn/control SpokenFeedbackEnabled
    changes:
    - Move Is{Toggle}SpokenFeedbackEnabled code to ash, which used to be
      AccessibilityDelegate and called into chrome. This won't work in
      mustash world. This change breaks the delegate way, by having prefs
      in ash, the Is{Set} could be done in ash.
    - Explicitly call OnSpokenFeedbackChanged() on chrome side when prefs
      is registered.
    - Change ToggleSpokenFeedbackEnabled to SetSpokenFeedbackEnabled to
      align with other method names.
    
    Bug: 594887
    Test: tested on emulator with --mash, feature works fine on signing out
    and re-logged-in.
    Change-Id: I09d83e93784924ff84f2d1a9ad0b16e2a41418d8

--- ash/accessibility/accessibility_controller.h
+++ ash/accessibility/accessibility_controller.h
@@ -9,6 +9,7 @@
 
 #include "ash/ash_constants.h"
 #include "ash/ash_export.h"
+#include "ash/public/cpp/accessibility_types.h"
 #include "ash/public/interfaces/accessibility_controller.mojom.h"
 #include "ash/session/session_observer.h"
 #include "base/macros.h"
@@ -56,6 +57,10 @@ class ASH_EXPORT AccessibilityController
   void SetMonoAudioEnabled(bool enabled);
   bool IsMonoAudioEnabled() const;
 
+  void SetSpokenFeedbackEnabled(bool enabled,
+                                AccessibilityNotificationVisibility notify);
+  bool IsSpokenFeedbackEnabled() const;
+
   // Triggers an accessibility alert to give the user feedback.
   void TriggerAccessibilityAlert(mojom::AccessibilityAlert alert);
 
@@ -80,6 +85,9 @@ class ASH_EXPORT AccessibilityController
   void OnSigninScreenPrefServiceInitialized(PrefService* prefs) override;
   void OnActiveUserPrefServiceChanged(PrefService* prefs) override;
 
+  // TODO(warx): remove this method for browser tests (crbug.com/789285).
+  void SetPrefServiceForTest(PrefService* prefs);
+
   // Test helpers:
   void FlushMojoForTest();
 
@@ -88,10 +96,15 @@ class ASH_EXPORT AccessibilityController
   // initial settings.
   void ObservePrefs(PrefService* prefs);
 
+  // Returns |pref_service_for_test_| if not null, otherwise return
+  // SessionController::GetActivePrefService().
+  PrefService* GetActivePrefService() const;
+
   void UpdateAutoclickFromPref();
   void UpdateHighContrastFromPref();
   void UpdateLargeCursorFromPref();
   void UpdateMonoAudioFromPref();
+  void UpdateSpokenFeedbackFromPref();
 
   service_manager::Connector* connector_ = nullptr;
   std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
@@ -107,6 +120,12 @@ class ASH_EXPORT AccessibilityController
   bool large_cursor_enabled_ = false;
   int large_cursor_size_in_dip_ = kDefaultLargeCursorSize;
   bool mono_audio_enabled_ = false;
+  bool spoken_feedback_enabled_ = false;
+
+  AccessibilityNotificationVisibility spoken_feedback_notification_ =
+      A11Y_NOTIFICATION_NONE;
+
+  PrefService* pref_service_for_test_ = nullptr;
 
   // Used to force the backlights off to darken the screen.
   std::unique_ptr<ScopedBacklightsForcedOff> scoped_backlights_forced_off_;
