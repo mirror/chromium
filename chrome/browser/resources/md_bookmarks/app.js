fc25e7ee8b86556bff8e9d6406de0423961c904f
Atcharatip Pinmook <atcharatip9035@gmail.com>
2018-01-09 18:14:33 +0000

    Revert "MD Bookmarks: Announce to screen readers when search results are available"
    This reverts commit 2533c2514b6981851d388ae9d8d02c049ce082ca.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > MD Bookmarks: Announce to screen readers when search results are available
    > 
    > Bug: 735808
    > Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    > Change-Id: I9449bca20368229dbe9c6e4860ad2ba0467fd0d8
    > Reviewed-on: https://chromium-review.googlesource.com/567898
    > Reviewed-by: calamity <calamity@chromium.org>
    > Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#487413}
    
    TBR=calamity@chromium.org,tsergeant@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 735808
    Change-Id: Ibe1a66c8de1bd66c057c4515105f911f8b93aae2
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation

--- chrome/browser/resources/md_bookmarks/app.js
+++ chrome/browser/resources/md_bookmarks/app.js
@@ -122,11 +122,6 @@ Polymer({
         return node.id;
       });
       this.dispatch(bookmarks.actions.setSearchResults(ids));
-      this.fire('iron-announce', {
-        text: ids.length > 0 ?
-            loadTimeData.getStringF('searchResults', this.searchTerm_) :
-            loadTimeData.getString('noSearchResults')
-      });
     }.bind(this));
   },
 
