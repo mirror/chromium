2e93b531cbbf92830a45e34a6806d3729242b40e
Jochen Eisinger <jochen@chromium.org>
2018-01-09 17:33:41 +0100

    Remove suborigin implementation
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I394691c475e33c73edda817d5124938f25ed398c

--- url/url_canon_unittest.cc
+++ url/url_canon_unittest.cc
@@ -2305,8 +2305,6 @@ TEST(URLCanonTest, DefaultPortForScheme) {
       {"ws", 80},
       {"wss", 443},
       {"gopher", 70},
-      {"http-so", 80},
-      {"https-so", 443},
       {"fake-scheme", PORT_UNSPECIFIED},
       {"HTTP", PORT_UNSPECIFIED},
       {"HTTPS", PORT_UNSPECIFIED},
@@ -2314,8 +2312,6 @@ TEST(URLCanonTest, DefaultPortForScheme) {
       {"WS", PORT_UNSPECIFIED},
       {"WSS", PORT_UNSPECIFIED},
       {"GOPHER", PORT_UNSPECIFIED},
-      {"HTTP-SO", PORT_UNSPECIFIED},
-      {"HTTPS-SO", PORT_UNSPECIFIED},
   };
 
   for (auto& test_case : cases) {
