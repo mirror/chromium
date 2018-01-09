63cda6e87d4d9b9a19c23e06a259e37d00d8535d
Kunihiko Sakamoto <ksakamoto@chromium.org>
2018-01-09 13:35:29 +0900

    [NOT FOR COMMIT] response body inlining
    Bug: 773194
    Change-Id: I56c328efe07226d3e53fb9c83c9b95ac25e1eb76

--- content/browser/appcache/appcache_update_url_loader_request.h
+++ content/browser/appcache/appcache_update_url_loader_request.h
@@ -64,6 +64,7 @@ class AppCacheUpdateJob::UpdateURLLoaderRequest
                         int64_t total_size,
                         OnUploadProgressCallback ack_callback) override;
   void OnReceiveCachedMetadata(const std::vector<uint8_t>& data) override;
+  void OnReceivedInlinedDataChunk(const std::vector<uint8_t>& data) override;
   void OnTransferSizeUpdated(int32_t transfer_size_diff) override;
   void OnStartLoadingResponseBody(
       mojo::ScopedDataPipeConsumerHandle body) override;
--- content/renderer/service_worker/service_worker_subresource_loader.cc
+++ content/renderer/service_worker/service_worker_subresource_loader.cc
@@ -110,6 +110,11 @@ class HeaderRewritingURLLoaderClient : public mojom::URLLoaderClient {
     url_loader_client_->OnReceiveCachedMetadata(data);
   }
 
+  void OnReceivedInlinedDataChunk(const std::vector<uint8_t>& data) override {
+    DCHECK(url_loader_client_.is_bound());
+    url_loader_client_->OnReceivedInlinedDataChunk(data);
+  }
+
   void OnTransferSizeUpdated(int32_t transfer_size_diff) override {
     DCHECK(url_loader_client_.is_bound());
     url_loader_client_->OnTransferSizeUpdated(transfer_size_diff);
