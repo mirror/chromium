18ab84ed6e643a6c307b4b34444d7f919e30e573
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2018-01-09 14:57:29 +0800

    Move some global functions to EditingUtilities.cpp
    As the TODOs leave by yosin@, move IsEmptyNonEditableNodeInEditable(),
    EditingIgnoresContent(), RootEditableElementOrTreeScopeRootNodeOf()
    to EditingUtilities.cpp.
    
    BUG=
    
    Signed-off-by: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Change-Id: I2676b42293b3c47d4f2070b9e70c94eefaeb0e8b

--- third_party/WebKit/Source/core/editing/EditingUtilities.cpp
+++ third_party/WebKit/Source/core/editing/EditingUtilities.cpp
@@ -2199,4 +2199,39 @@ InputEvent::InputType DeletionInputTypeFromTextGranularity(
   }
 }
 
+// |IsEmptyNonEditableNodeInEditable()| is introduced for fixing
+// http://crbug.com/428986.
+static bool IsEmptyNonEditableNodeInEditable(const Node& node) {
+  // Editability is defined the DOM tree rather than the flat tree. For example:
+  // DOM:
+  //   <host>
+  //     <span>unedittable</span>
+  //     <shadowroot><div ce><content /></div></shadowroot>
+  //   </host>
+  //
+  // Flat Tree:
+  //   <host><div ce><span1>unedittable</span></div></host>
+  // e.g. editing/shadow/breaking-editing-boundaries.html
+  return !NodeTraversal::HasChildren(node) && !HasEditableStyle(node) &&
+         node.parentNode() && HasEditableStyle(*node.parentNode());
+}
+
+// TODO(yosin): We should not use |IsEmptyNonEditableNodeInEditable()| in
+// |EditingIgnoresContent()| since |IsEmptyNonEditableNodeInEditable()|
+// requires clean layout tree.
+bool EditingIgnoresContent(const Node& node) {
+  return !node.CanContainRangeEndPoint() ||
+         IsEmptyNonEditableNodeInEditable(node);
+}
+
+ContainerNode* RootEditableElementOrTreeScopeRootNodeOf(
+    const Position& position) {
+  Element* selection_root = RootEditableElementOf(position);
+  if (selection_root)
+    return selection_root;
+
+  Node* const node = position.ComputeContainerNode();
+  return node ? &node->GetTreeScope().RootNode() : nullptr;
+}
+
 }  // namespace blink
