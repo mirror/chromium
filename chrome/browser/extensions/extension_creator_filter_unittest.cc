92f5599c6d87224d8372a9e6c5580a537ed7967c
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-01-09 14:33:08 -0800

    [Extensions Cleanup] Move ExtensionCreator to //extensions
    Move ExtensionCreator to //extensions. It has no //chrome dependencies
    and seems useful for a generic extensions embedder.
    
    Bug: None
    Change-Id: I16f3a2603affcf4d014e2e6d7907b9028f20fb40

--- chrome/browser/extensions/extension_creator_filter_unittest.cc
+++ chrome/browser/extensions/extension_creator_filter_unittest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/extension_creator_filter.h"
+#include "extensions/browser/extension_creator_filter.h"
 
 #include <stddef.h>
 
