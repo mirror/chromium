40f0bb70525f78106d016388b786419e01990575
Steven Bennetts <stevenjb@chromium.org>
2018-01-09 12:39:39 -0700

    Move remaining UI code to ChromeBrowserMainExtraPartsAsh
    This removes the remaining UI code from ChromeBrowserMainPartsChromeOS
    and moves it to ChromeBrowserMainExtraPartsAsh.
    
    This also:
    * Replaces instances of the deprecated ash_util::IsRunningInMash()
      with chromeos::GetAshConfig() == ash::Config::MASH.
    * Alphabetizes construction of instances in
      ChromeBrowserMainExtraPartsAsh (they should not have any implicit
      co-dependencies).
    * Moves PushProcessCreationTimeToAsh() to
      ChromeBrowserMainExtraPartsAsh.
    
    Bug: 796007
    Change-Id: I8a1d45d3ef12ee5f31781e774e42dc4098faa5d6

--- chrome/browser/ui/ash/chrome_browser_main_extra_parts_ash.h
+++ chrome/browser/ui/ash/chrome_browser_main_extra_parts_ash.h
@@ -24,22 +24,28 @@ class AshShellInit;
 class CastConfigClientMediaRouter;
 class ChromeNewWindowClient;
 class ChromeShellContentState;
-class LoginScreenClient;
 class ImeControllerClient;
 class ImmersiveContextMus;
 class ImmersiveHandlerFactoryMus;
+class LoginScreenClient;
 class MediaClient;
 class SessionControllerClient;
 class SystemTrayClient;
+class TabletModeClient;
 class VolumeController;
 class VpnListForwarder;
+class WallpaperControllerClient;
 
 #if BUILDFLAG(ENABLE_WAYLAND_SERVER)
 class ExoParts;
 #endif
 
-// Browser initialization for Ash UI. Only use this for initialization of
-// chrome/browser/ui/ash classes.
+namespace internal {
+class ChromeLauncherControllerInitializer;
+}
+
+// Browser initialization for Ash UI. Only use this for Ash specific
+// intitialization (e.g. initialization of chrome/browser/ui/ash classes).
 class ChromeBrowserMainExtraPartsAsh : public ChromeBrowserMainExtraParts {
  public:
   ChromeBrowserMainExtraPartsAsh();
@@ -55,19 +61,24 @@ class ChromeBrowserMainExtraPartsAsh : public ChromeBrowserMainExtraParts {
  private:
   std::unique_ptr<AccessibilityControllerClient>
       accessibility_controller_client_;
-  std::unique_ptr<ChromeShellContentState> chrome_shell_content_state_;
+  std::unique_ptr<AshShellInit> ash_shell_init_;
   std::unique_ptr<CastConfigClientMediaRouter> cast_config_client_media_router_;
-  std::unique_ptr<MediaClient> media_client_;
-  std::unique_ptr<ImmersiveHandlerFactoryMus> immersive_handler_factory_;
+  std::unique_ptr<ChromeNewWindowClient> new_window_client_;
+  std::unique_ptr<ChromeShellContentState> chrome_shell_content_state_;
+  std::unique_ptr<ImeControllerClient> ime_controller_client_;
   std::unique_ptr<ImmersiveContextMus> immersive_context_;
+  std::unique_ptr<ImmersiveHandlerFactoryMus> immersive_handler_factory_;
+  std::unique_ptr<LoginScreenClient> login_screen_client_;
+  std::unique_ptr<MediaClient> media_client_;
   std::unique_ptr<SessionControllerClient> session_controller_client_;
   std::unique_ptr<SystemTrayClient> system_tray_client_;
-  std::unique_ptr<ImeControllerClient> ime_controller_client_;
-  std::unique_ptr<ChromeNewWindowClient> new_window_client_;
+  std::unique_ptr<TabletModeClient> tablet_mode_client_;
   std::unique_ptr<VolumeController> volume_controller_;
   std::unique_ptr<VpnListForwarder> vpn_list_forwarder_;
-  std::unique_ptr<AshShellInit> ash_shell_init_;
-  std::unique_ptr<LoginScreenClient> login_screen_client_;
+  std::unique_ptr<WallpaperControllerClient> wallpaper_controller_client_;
+
+  std::unique_ptr<internal::ChromeLauncherControllerInitializer>
+      chrome_launcher_controller_initializer_;
 
   // Used only for mash.
   std::unique_ptr<ui::UserActivityDetector> user_activity_detector_;
