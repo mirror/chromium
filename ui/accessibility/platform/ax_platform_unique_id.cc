cffd514d647f61bedbb0b5456b9806f30f01dc64
Aaron Leventhal <aleventhal@chromium.org>
2018-01-09 15:26:35 -0500

    Consistent, unified Unique IDs for accessibility objects in browser process
    Summary:
    - Needed to form relationships between objects
    - Unique IDs for all objects in Chrome via AXUniqueID
    - Windows will now use the same IDs for firing events, instead of creating its own in ax_platform_node_win.cc
    - Separate Chrome unique IDs from Blink IDs by consistently using the name "UniqueId"
    and by making them different types.
    - AXPlatformNodeDelegate is now responsible for providing IDs
    
    Every accessibility node Chrome needs a unique ids so that events can be fired for it in Windows,
    and so that it can be the target of relationships. Unlike the ids provided by Blink, these ids
    must be unique for the entire Chrome browser process.
    
    Because there are these two types of ids (Blink IDs which are only unique within a frame, and must
    be created by Blink, and Chrome unique IDs that are unique within the entire Chrome process), it
    is best to make this fool proof and not allow the two to be mixed. This is accomplished via a
    UniqueId class for the Chrome IDs, which cannot be initialized with a Blink ID (int32_t).
    
    Bug: 797992
    Change-Id: I60d2dfeca8fe107b15838cfcc17dc07a90c29cc9

--- ui/accessibility/platform/ax_platform_unique_id.cc
+++ ui/accessibility/platform/ax_platform_unique_id.cc
@@ -2,19 +2,47 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+
 #include "ui/accessibility/platform/ax_platform_unique_id.h"
 
 namespace ui {
 
-int32_t GetNextAXPlatformNodeUniqueId() {
-  static int32_t next_unique_id = 1;
-  int32_t unique_id = next_unique_id;
-  if (next_unique_id == INT32_MAX)
-    next_unique_id = 1;
-  else
-    next_unique_id++;
+AXUniqueId::AXUniqueId() : id_(GetNextAXUniqueId()) {}
 
-  return unique_id;
+AXUniqueId::~AXUniqueId() {
+  AssignedIds()->erase(id_);
+}
+
+base::hash_set<int32_t>* AXUniqueId::AssignedIds() {
+  static base::hash_set<int32_t>* assigned_ids;
+  if (!assigned_ids)
+    assigned_ids = new base::hash_set<int32_t>();
+  return assigned_ids;
+}
+
+bool AXUniqueId::IsAssigned(int32_t id) const {
+  return AssignedIds()->find(id) != AssignedIds()->end();
+}
+
+int32_t AXUniqueId::GetNextAXUniqueId() {
+  static int32_t current_unique_id = 1;
+  static bool has_wrapped = false;
+
+  while (true) {
+    if (current_unique_id == INT32_MAX) {
+      current_unique_id = 1;
+      has_wrapped = true;
+    } else {
+      current_unique_id++;
+    }
+    if (!has_wrapped || !IsAssigned(current_unique_id))
+      break;
+  }
+
+  AssignedIds()->insert(current_unique_id);
+
+  return current_unique_id;
 }
 
 }  // namespace ui
