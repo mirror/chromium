5e12bf8c72f7ad77c59ae20c0a7ccf6de49dcc0d
Jennifer Apacible <apacible@chromium.org>
2018-01-08 18:26:49 -0800

    add size
    Change-Id: Ib5e24c685664dbf51d43c781957b853c42e9dae5

--- chrome/browser/ui/overlay/overlay_surface_embedder.cc
+++ chrome/browser/ui/overlay/overlay_surface_embedder.cc
@@ -9,13 +9,18 @@
 
 OverlaySurfaceEmbedder::OverlaySurfaceEmbedder(OverlayWindow* window)
     : window_(window) {
+  DCHECK(window_);
+
   surface_layer_ = base::MakeUnique<ui::Layer>(ui::LAYER_TEXTURED);
   surface_layer_->SetMasksToBounds(true);
 
   // The frame provided by the parent window's layer needs to show through
   // the surface layer.
   surface_layer_->SetFillsBoundsOpaquely(false);
+  surface_layer_->SetBounds(
+      gfx::Rect(gfx::Point(0, 0), window_->GetBounds().size()));
   window_->GetLayer()->Add(surface_layer_.get());
+  window_->GetLayer()->StackAtTop(surface_layer_.get());
   ref_factory_ = new viz::StubSurfaceReferenceFactory();
 }
 
