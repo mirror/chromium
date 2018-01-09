26ee9919811dfafca279d6b34f2f312beb199e0b
Jun Choi <hongjunchoi@chromium.org>
2018-01-08 22:12:14 -0800

    Create objects to encapsulate CTAP device reponse
    Added classes to encapsulate response data received from CTAP
    devices as specified by the CTAP protocol.
    
    https://fidoalliance.org/specs/fido-v2.0-rd-20170927/fido-client-to-authenticator-protocol-v2.0-rd-20170927.html#authenticatorMakeCredential
    
    Bug:796782
    Change-Id: If5a80846d565ab6a7919aa3236ff78e22b5209ae

---
+++ device/ctap/authenticator_options.h
@@ -0,0 +1,47 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DEVICE_CTAP_AUTHENTICATOR_OPTIONS_H_
+#define DEVICE_CTAP_AUTHENTICATOR_OPTIONS_H_
+
+#include <string>
+
+#include "base/optional.h"
+#include "components/cbor/cbor_values.h"
+
+namespace device {
+
+class AuthenticatorOptions {
+ public:
+  AuthenticatorOptions();
+  AuthenticatorOptions(AuthenticatorOptions&& other);
+  AuthenticatorOptions& operator=(AuthenticatorOptions&& other);
+  ~AuthenticatorOptions();
+
+  cbor::CBORValue ConvertToCBOR() const;
+  AuthenticatorOptions& SetPlatformDevice(bool platform_device);
+  AuthenticatorOptions& SetResidentKey(bool rk);
+  AuthenticatorOptions& SetUserVerification(bool uv);
+  AuthenticatorOptions& SetUserPresence(bool is_platform_device);
+  AuthenticatorOptions& SetClientPin(bool client_pin);
+
+  bool platform_device() const { return platform_device_; }
+  bool rk() const { return resident_key_; }
+  bool uv() const { return user_verification_; }
+  bool up() const { return user_presence_; }
+  const base::Optional<bool>& client_pin() const { return client_pin_; }
+
+ private:
+  bool platform_device_ = false;
+  bool resident_key_ = false;
+  bool user_verification_ = false;
+  bool user_presence_ = true;
+  base::Optional<bool> client_pin_;
+
+  DISALLOW_COPY_AND_ASSIGN(AuthenticatorOptions);
+};
+
+}  // namespace device
+
+#endif  // DEVICE_CTAP_AUTHENTICATOR_OPTIONS_H_
--- device/ctap/ctap_make_credential_request_param.cc
+++ device/ctap/ctap_make_credential_request_param.cc
@@ -8,7 +8,7 @@
 
 #include "base/numerics/safe_conversions.h"
 #include "components/cbor/cbor_writer.h"
-#include "device/ctap/ctap_request_command.h"
+#include "device/ctap/ctap_constants.h"
 
 namespace device {
 
@@ -53,13 +53,25 @@ CTAPMakeCredentialRequestParam::SerializeToCBOR() const {
     cbor_map[cbor::CBORValue(9)] = cbor::CBORValue(*pin_protocol_);
   }
 
+  auto resident_key = resident_key_ ? cbor::CBORValue::SimpleValue::TRUE_VALUE
+                                    : cbor::CBORValue::SimpleValue::FALSE_VALUE;
+  auto user_verification = user_verification_
+                               ? cbor::CBORValue::SimpleValue::TRUE_VALUE
+                               : cbor::CBORValue::SimpleValue::FALSE_VALUE;
+
+  cbor::CBORValue::MapValue option_map;
+  option_map[cbor::CBORValue("rk")] = cbor::CBORValue(resident_key);
+  option_map[cbor::CBORValue("uv")] = cbor::CBORValue(user_verification);
+  cbor_map[cbor::CBORValue(7)] = cbor::CBORValue(std::move(option_map));
+
   auto serialized_param =
       cbor::CBORWriter::Write(cbor::CBORValue(std::move(cbor_map)));
+
   if (!serialized_param)
     return base::nullopt;
 
   std::vector<uint8_t> cbor_request({base::strict_cast<uint8_t>(
-      CTAPRequestCommand::kAuthenticatorMakeCredential)});
+      constants::CTAPRequestCommand::kAuthenticatorMakeCredential)});
   cbor_request.insert(cbor_request.end(), serialized_param->begin(),
                       serialized_param->end());
   return cbor_request;
@@ -80,6 +92,18 @@ CTAPMakeCredentialRequestParam& CTAPMakeCredentialRequestParam::SetPinAuth(
 CTAPMakeCredentialRequestParam& CTAPMakeCredentialRequestParam::SetPinProtocol(
     uint8_t pin_protocol) {
   pin_protocol_ = pin_protocol;
+  return *this;
+}
+
+CTAPMakeCredentialRequestParam&
+CTAPMakeCredentialRequestParam::SetUserVerification(bool user_verification) {
+  user_verification_ = user_verification;
+  return *this;
+}
+
+CTAPMakeCredentialRequestParam& CTAPMakeCredentialRequestParam::SetResidentKey(
+    bool resident_key) {
+  resident_key_ = resident_key;
   return *this;
 }
 
