c4503242f697671fddc73710d056b3d1c17c11d4
David Tseng <dtseng@chromium.org>
2018-01-09 10:38:47 -0800

    Use web-based hit testing for focus recovery
    Bug: 800171
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: If93297dcb2051d43e29982abd23a5dfbe2d5bd6e

--- third_party/closure_compiler/externs/automation.js
+++ third_party/closure_compiler/externs/automation.js
@@ -1,4 +1,4 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -34,6 +34,7 @@ chrome.automation.EventType = {
   FOCUS: 'focus',
   IMAGE_FRAME_UPDATED: 'imageFrameUpdated',
   HIDE: 'hide',
+  HIT_TEST_RESULT: 'hitTestResult',
   HOVER: 'hover',
   INVALID_STATUS_CHANGED: 'invalidStatusChanged',
   LAYOUT_COMPLETE: 'layoutComplete',
