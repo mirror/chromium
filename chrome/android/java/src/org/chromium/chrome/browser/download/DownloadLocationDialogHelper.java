01be011ae6a23580cd10e8db904d6905e9e6f3d3
Joy Ming <jming@chromium.org>
2018-01-09 15:18:37 -0800

    [Downloads location] Initial implementation of download location dialog.
    The new flow for changing downloads location involves a dialog that
    appears before the user is downloading that asks to confirm the location
    of the download. In this change, I create a dummy dialog that pops up
    when the user is about to download, only if the preference for showing
    this dialog is checked off in settings. Clicking on any button on this
    dialog should allow the download to proceed as usual.
    
    Screenshot of the dialog:
    https://drive.google.com/file/d/1tSbEWm7PCmHS75uQBs2-21NeF1HFbx90/
    (Note: This is not the final UI, just a dummy dialog)
    
    Bug:792775
    Change-Id: Id61e57af6153a3c066af68d44a5a47e761d1a9d6

---
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadLocationDialogHelper.java
@@ -0,0 +1,56 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.download;
+
+import android.app.Activity;
+
+import org.chromium.base.annotations.CalledByNative;
+import org.chromium.content_public.browser.WebContents;
+
+import java.io.File;
+
+/**
+ * Helper class to handle communication between download location dialog and native.
+ */
+public class DownloadLocationDialogHelper
+        implements DownloadLocationDialog.DownloadLocationDialogListener {
+    private long mNativeDownloadLocationDialogHelper;
+    private DownloadLocationDialog mLocationDialog;
+
+    private DownloadLocationDialogHelper(long nativeDownloadLocationDialogHelper) {
+        mNativeDownloadLocationDialogHelper = nativeDownloadLocationDialogHelper;
+    }
+
+    @CalledByNative
+    public static DownloadLocationDialogHelper create(long nativeDownloadLocationDialogHelper) {
+        return new DownloadLocationDialogHelper(nativeDownloadLocationDialogHelper);
+    }
+
+    @CalledByNative
+    private void destroy() {
+        mNativeDownloadLocationDialogHelper = 0;
+        mLocationDialog.dismiss();
+    }
+
+    @CalledByNative
+    public void showDialog(WebContents webContents, String suggestedPath) {
+        Activity windowAndroidActivity = webContents.getTopLevelNativeWindow().getActivity().get();
+        if (windowAndroidActivity == null) return;
+
+        mLocationDialog =
+                new DownloadLocationDialog(windowAndroidActivity, this, new File(suggestedPath));
+        mLocationDialog.show();
+    }
+
+    @Override
+    public void onComplete(File returnedPath) {
+        if (mNativeDownloadLocationDialogHelper == 0) return;
+
+        nativeOnComplete(mNativeDownloadLocationDialogHelper, returnedPath.getAbsolutePath());
+    }
+
+    public native void nativeOnComplete(
+            long nativeDownloadLocationDialogHelper, String returnedPath);
+}
