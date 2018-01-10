aa395ca5a9ff339e999460aa8a107ca384ea0ee0
Misha Efimov <mef@google.com>
2018-01-10 15:25:58 -0500

    [Cronet] Implement native Cronet_Engine.
    Bug:786559
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I5ddece478b1b77db85f48860102d6e669761494e

--- components/cronet/cronet_url_request_context.h
+++ components/cronet/cronet_url_request_context.h
@@ -35,6 +35,7 @@ class CertVerifier;
 class NetLog;
 class ProxyConfigService;
 class URLRequestContext;
+class URLRequestContextGetter;
 class FileNetLogObserver;
 }  // namespace net
 
@@ -121,6 +122,8 @@ class CronetURLRequestContext {
 
   net::URLRequestContext* GetURLRequestContext();
 
+  net::URLRequestContextGetter* GetURLRequestContextGetter();
+
   // TODO(xunjieli): Keep only one version of StartNetLog().
 
   // Starts NetLog logging to file. This can be called on any thread.
@@ -160,8 +163,9 @@ class CronetURLRequestContext {
 
  private:
   friend class TestUtil;
+  class ContextGetter;
 
-  // Network Thread performs tasks on Network Thread and owns objects that
+  // Network Tasls performs tasks on Network Thread and owns objects that
   // live on network thread.
   class NetworkTasks : public net::EffectiveConnectionTypeObserver,
                        public net::RTTAndThroughputEstimatesObserver,
