8f8d142ea2bc54c930ca50b0395f9a140b892c31
Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
2018-01-10 17:07:19 +0200

    [Sensors][DeviceOrientation]Browser-side Feature Policy check
    The sensor policy-controlled features should be
    checked in the browser process, in order to improve
    security robustness.
    
    This patch introduces browser-side feature policy check
    for both the Generic Sensor and the Device Orientation APIs.
    
    Bug: 796894
    Bug: 795723
    Change-Id: Idc9869d75bca70f3f9ee915101195c7cbee22587

--- content/browser/generic_sensor/sensor_provider_proxy_impl.cc
+++ content/browser/generic_sensor/sensor_provider_proxy_impl.cc
@@ -15,6 +15,9 @@
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "services/device/public/interfaces/constants.mojom.h"
 #include "services/service_manager/public/cpp/connector.h"
+#include "third_party/WebKit/common/feature_policy/feature_policy_feature.h"
+
+using device::mojom::SensorType;
 
 namespace content {
 
@@ -34,13 +37,12 @@ void SensorProviderProxyImpl::Bind(
   binding_set_.AddBinding(this, std::move(request));
 }
 
-void SensorProviderProxyImpl::GetSensor(
-    device::mojom::SensorType type,
-    GetSensorCallback callback) {
+void SensorProviderProxyImpl::GetSensor(SensorType type,
+                                        GetSensorCallback callback) {
   ServiceManagerConnection* connection =
       ServiceManagerConnection::GetForProcess();
 
-  if (!connection || !CheckPermission(type)) {
+  if (!connection || !CheckFeaturePolicies(type) || !CheckPermission()) {
     std::move(callback).Run(nullptr);
     return;
   }
@@ -54,10 +56,9 @@ void SensorProviderProxyImpl::GetSensor(
   sensor_provider_->GetSensor(type, std::move(callback));
 }
 
-bool SensorProviderProxyImpl::CheckPermission(
-    device::mojom::SensorType type) const {
-  content::WebContents* web_contents =
-      content::WebContents::FromRenderFrameHost(render_frame_host_);
+bool SensorProviderProxyImpl::CheckPermission() const {
+  WebContents* web_contents =
+      WebContents::FromRenderFrameHost(render_frame_host_);
   if (!web_contents)
     return false;
 
@@ -68,6 +69,46 @@ bool SensorProviderProxyImpl::CheckPermission(
       permission_manager_->GetPermissionStatus(
           PermissionType::SENSORS, requesting_origin, embedding_origin);
   return permission_status == blink::mojom::PermissionStatus::GRANTED;
+}
+
+namespace {
+
+std::vector<blink::FeaturePolicyFeature> SensorTypeToFeaturePolicyFeatures(
+    SensorType type) {
+  switch (type) {
+    case SensorType::AMBIENT_LIGHT:
+      return {blink::FeaturePolicyFeature::kAmbientLightSensor};
+    case SensorType::ACCELEROMETER:
+    case SensorType::LINEAR_ACCELERATION:
+      return {blink::FeaturePolicyFeature::kAccelerometer};
+    case SensorType::GYROSCOPE:
+      return {blink::FeaturePolicyFeature::kGyroscope};
+    case SensorType::MAGNETOMETER:
+      return {blink::FeaturePolicyFeature::kMagnetometer};
+    case SensorType::ABSOLUTE_ORIENTATION_EULER_ANGLES:
+    case SensorType::ABSOLUTE_ORIENTATION_QUATERNION:
+      return {blink::FeaturePolicyFeature::kAccelerometer,
+              blink::FeaturePolicyFeature::kGyroscope,
+              blink::FeaturePolicyFeature::kMagnetometer};
+    case SensorType::RELATIVE_ORIENTATION_EULER_ANGLES:
+    case SensorType::RELATIVE_ORIENTATION_QUATERNION:
+      return {blink::FeaturePolicyFeature::kAccelerometer,
+              blink::FeaturePolicyFeature::kGyroscope};
+    default:
+      NOTREACHED() << "Unknown sensor type " << type;
+      return {};
+  }
+}
+
+}  // namespace
+
+bool SensorProviderProxyImpl::CheckFeaturePolicies(SensorType type) const {
+  const std::vector<blink::FeaturePolicyFeature>& features =
+      SensorTypeToFeaturePolicyFeatures(type);
+  return std::all_of(features.begin(), features.end(),
+                     [this](blink::FeaturePolicyFeature feature) {
+                       return render_frame_host_->IsFeatureEnabled(feature);
+                     });
 }
 
 void SensorProviderProxyImpl::OnConnectionError() {
