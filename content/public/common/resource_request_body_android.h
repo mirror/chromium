51d6c21d1c50b6b32f3e0aa1c91e7054b7ef36f9
John Abd-El-Malek <jam@chromium.org>
2018-01-10 07:52:19 -0800

    Remove dependencies from content::ResourceRequest in preparation for its move to services/network.
    Bug: 753658
    Change-Id: I656d2b49a7995d57db4514104165056300861c77

--- content/public/common/resource_request_body_android.h
+++ content/public/common/resource_request_body_android.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_ANDROID_RESOURCE_REQUEST_BODY_JNI_H_
-#define CONTENT_BROWSER_ANDROID_RESOURCE_REQUEST_BODY_JNI_H_
+#ifndef CONTENT_PUBLIC_COMMON_RESOURCE_REQUEST_BODY_ANDROID_H_
+#define CONTENT_PUBLIC_COMMON_RESOURCE_REQUEST_BODY_ANDROID_H_
 
 #include <jni.h>
 
@@ -30,4 +30,4 @@ scoped_refptr<ResourceRequestBody> ExtractResourceRequestBodyFromJavaObject(
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_ANDROID_RESOURCE_REQUEST_BODY_JNI_H_
+#endif  // CONTENT_PUBLIC_COMMON_RESOURCE_REQUEST_BODY_ANDROID_H_
