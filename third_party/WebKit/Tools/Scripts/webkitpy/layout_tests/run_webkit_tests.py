055c3daf464d1305c5387ed6eccacf9687939fe8
Xianzhu Wang <wangxianzhu@chromium.org>
2018-01-09 17:46:05 -0800

    Debug flakiness of fast/forms/select/listbox-in-multi-column.html
    Bug: 798116
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I194b33373a7f0592fefca431f2badb1978956dd5

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/run_webkit_tests.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/run_webkit_tests.py
@@ -44,6 +44,7 @@ _log = logging.getLogger(__name__)
 
 
 def main(argv, stdout, stderr):
+    argv += ['--no-retry-failures', 'fast']
     options, args = parse_args(argv)
 
     if options.platform and 'test' in options.platform and not 'browser_test' in options.platform:
