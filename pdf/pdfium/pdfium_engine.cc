5c0bfb5cabb0fbb08306bf53fcbecff374eb9dea
Ryan Harrison <rharrison@chromium.org>
2018-01-10 15:31:43 -0500

    Add handling specific to XFA fields
    The current mouse down logic assumes there is going to be an
    annotation for a form field. This is only true if the field is an
    AcroForm field. If it is an XFA field then there is no annotation.
    
    This CL depends on
    https://pdfium-review.googlesource.com/c/pdfium/+/22677 being rolled
    into chromium.
    
    BUG=pdfium:952,chromium:763129
    
    Change-Id: I1aafb646e7fb1bfb5f4f2a499f983809ef3279cf

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -3965,9 +3965,16 @@ bool PDFiumEngine::IsPointInEditableFormTextArea(FPDF_PAGE page,
                                                  double page_x,
                                                  double page_y,
                                                  int form_type) {
+#if defined(PDF_ENABLE_XFA)
+  if (IS_XFA_FORMFIELD(form_type))
+    return (form_type == FPDF_FORMFIELD_XFA_TEXTFIELD);
+#endif  // defined(PDF_ENABLE_XFA)
+
   FPDF_ANNOTATION annot =
       FPDFAnnot_GetFormFieldAtPoint(form_, page, page_x, page_y);
   DCHECK(annot);
+  if (!annot)
+    return false;
 
   int flags = FPDFAnnot_GetFormFieldFlags(page, annot);
   bool is_editable_form_text_area =
