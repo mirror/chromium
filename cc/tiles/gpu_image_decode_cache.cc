9c9aae22b9a8705d73cad275f787d18ce3571ba0
Khushal <khushalsagar@chromium.org>
2018-01-10 15:02:09 -0800

    blink/canvas: Switch Canvas to use cc's ImageDecodeCache.
    Currently rasterization for canvas uses skia's image decode cache which
    does not cache color converted images, causing an expensive conversion
    performed for every frame. This replaces this with cc's ImageDecodeCache
    which does cache the result of color conversion. This
    change only does this for accelerated canvas, follow up changes should
    also set up a cc::SoftwareImageDecodeCache in blink for the
    non-accelerated case.
    
    In addition, this also provides an easy hook that canvas can use to
    serialize PaintRecords for OOP raster, since the
    PaintOpBufferSerializer requires the same ImageProvider/Cache for
    image serialization.
    
    R=chrishtr@chromium.org, ccameron@chromium.org
    Bug:791828
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ief9aa958fd174e1d0ea1165600c1405968f271c2

--- cc/tiles/gpu_image_decode_cache.cc
+++ cc/tiles/gpu_image_decode_cache.cc
@@ -556,7 +556,10 @@ GpuImageDecodeCache::GpuImageDecodeCache(viz::RasterContextProvider* context,
   // Acquire the context_lock so that we can safely retrieve
   // |max_texture_size_|.
   {
-    viz::RasterContextProvider::ScopedRasterContextLock context_lock(context_);
+    base::Optional<viz::RasterContextProvider::ScopedRasterContextLock>
+        context_lock;
+    if (context_->GetLock())
+      context_lock.emplace(context_);
     max_texture_size_ = context_->GrContext()->caps()->maxTextureSize();
   }
 
@@ -699,7 +702,7 @@ DecodedDrawImage GpuImageDecodeCache::GetDecodedImageForDraw(
 
   // We are being called during raster. The context lock must already be
   // acquired by the caller.
-  context_->GetLock()->AssertAcquired();
+  CheckContextLockAcquiredIfNecessary();
 
   // If we're skipping the image, then the filter quality doesn't matter.
   if (SkipImage(draw_image))
@@ -771,7 +774,7 @@ void GpuImageDecodeCache::DrawWithImageFinished(
 
   // We are being called during raster. The context lock must already be
   // acquired by the caller.
-  context_->GetLock()->AssertAcquired();
+  CheckContextLockAcquiredIfNecessary();
 
   if (SkipImage(draw_image))
     return;
@@ -794,10 +797,12 @@ void GpuImageDecodeCache::ReduceCacheUsage() {
   // This is typically called when no tasks are running (between scheduling
   // tasks). Try to lock and run pending operations if possible, but don't
   // block on it.
-  if (context_->GetLock()->Try()) {
-    RunPendingContextThreadOperations();
+  if (context_->GetLock() && !context_->GetLock()->Try())
+    return;
+
+  RunPendingContextThreadOperations();
+  if (context_->GetLock())
     context_->GetLock()->Release();
-  }
 }
 
 void GpuImageDecodeCache::SetShouldAggressivelyFreeResources(
@@ -806,7 +811,11 @@ void GpuImageDecodeCache::SetShouldAggressivelyFreeResources(
                "GpuImageDecodeCache::SetShouldAggressivelyFreeResources",
                "agressive_free_resources", aggressively_free_resources);
   if (aggressively_free_resources) {
-    viz::RasterContextProvider::ScopedRasterContextLock context_lock(context_);
+    base::Optional<viz::RasterContextProvider::ScopedRasterContextLock>
+        context_lock;
+    if (context_->GetLock())
+      context_lock.emplace(context_);
+
     base::AutoLock lock(lock_);
     aggressively_freeing_resources_ = aggressively_free_resources;
     EnsureCapacity(0);
@@ -952,7 +961,11 @@ void GpuImageDecodeCache::DecodeImage(const DrawImage& draw_image,
 void GpuImageDecodeCache::UploadImage(const DrawImage& draw_image) {
   TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("cc.debug"),
                "GpuImageDecodeCache::UploadImage");
-  viz::RasterContextProvider::ScopedRasterContextLock context_lock(context_);
+  base::Optional<viz::RasterContextProvider::ScopedRasterContextLock>
+      context_lock;
+  if (context_->GetLock())
+    context_lock.emplace(context_);
+
   base::AutoLock lock(lock_);
   ImageData* image_data = GetImageDataForDrawImage(draw_image);
   DCHECK(image_data);
@@ -1335,7 +1348,7 @@ void GpuImageDecodeCache::DecodeImageIfNecessary(const DrawImage& draw_image,
 
 void GpuImageDecodeCache::UploadImageIfNecessary(const DrawImage& draw_image,
                                                  ImageData* image_data) {
-  context_->GetLock()->AssertAcquired();
+  CheckContextLockAcquiredIfNecessary();
   lock_.AssertAcquired();
 
   // We are about to upload a new image and are holding the context lock.
@@ -1479,7 +1492,7 @@ void GpuImageDecodeCache::UnlockImage(ImageData* image_data) {
 // we need to call GlIdFromSkImage, which flushes pending IO on the image,
 // rather than just using a cached GL ID.
 void GpuImageDecodeCache::RunPendingContextThreadOperations() {
-  context_->GetLock()->AssertAcquired();
+  CheckContextLockAcquiredIfNecessary();
   lock_.AssertAcquired();
 
   for (auto* image : images_pending_complete_lock_) {
@@ -1680,6 +1693,12 @@ bool GpuImageDecodeCache::SupportsColorSpaces() const {
     default:
       return false;
   }
+}
+
+void GpuImageDecodeCache::CheckContextLockAcquiredIfNecessary() {
+  if (!context_->GetLock())
+    return;
+  context_->GetLock()->AssertAcquired();
 }
 
 }  // namespace cc
