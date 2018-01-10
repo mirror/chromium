7b957c1feb1ce5592320c8507079d1ef0214006e
Bailey Berro <baileyberro@chromium.org>
2018-01-10 12:26:11 -0800

    Adding call to file_system_provider::Service::UnmountFileSystem().
    This change adds a call to file_system_provider::Service::UnmountFileSystem()
    to SmbFileSystem::HandleRequestUnmountCallback() via SmbService, fixing a bug
    where smb mounts were not actually removed from VolumeManager when
    unmounting via Files App.
    
    Bug: 757625
    Change-Id: Ib38fd944a04e589c33670b084fd052a74bfeb9b1

--- chrome/browser/chromeos/smb_client/smb_provider.h
+++ chrome/browser/chromeos/smb_client/smb_provider.h
@@ -11,6 +11,7 @@
 #include "chrome/browser/chromeos/file_system_provider/provided_file_system_info.h"
 #include "chrome/browser/chromeos/file_system_provider/provided_file_system_interface.h"
 #include "chrome/browser/chromeos/file_system_provider/provider_interface.h"
+#include "chrome/browser/chromeos/smb_client/smb_file_system.h"
 
 class Profile;
 
@@ -25,7 +26,8 @@ using file_system_provider::ProvidedFileSystemInterface;
 
 class SmbProvider : public ProviderInterface {
  public:
-  SmbProvider();
+  explicit SmbProvider(SmbFileSystem::UnmountCallback unmount_callback);
+  ~SmbProvider() override;
   // ProviderInterface overrides.
   std::unique_ptr<ProvidedFileSystemInterface> CreateProvidedFileSystem(
       Profile* profile,
@@ -38,6 +40,8 @@ class SmbProvider : public ProviderInterface {
   ProviderId provider_id_;
   Capabilities capabilities_;
   std::string name_;
+
+  SmbFileSystem::UnmountCallback unmount_callback_;
 
   DISALLOW_COPY_AND_ASSIGN(SmbProvider);
 };
