e8975579d1e234c710fb97f9af18db56c2b470c7
Yutaka Hirano <yhirano@chromium.org>
2018-01-10 14:17:33 +0900

    Move ResourceScheduler to content/network
    ...to share the throttling logic between the existing path and the
    network service.
    
    This CL also moves ResourceThrottle to content/public/network. Test files
    are not moved because they use content/browser specific components.
    
    Bug: 754918
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I065f6789fc52e06b34c554590e3290f57ccff04b

--- content/public/network/resource_throttle.h
+++ content/public/network/resource_throttle.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_PUBLIC_BROWSER_RESOURCE_THROTTLE_H_
-#define CONTENT_PUBLIC_BROWSER_RESOURCE_THROTTLE_H_
+#ifndef CONTENT_PUBLIC_NETWORK_RESOURCE_THROTTLE_H_
+#define CONTENT_PUBLIC_NETWORK_RESOURCE_THROTTLE_H_
 
 #include "content/common/content_export.h"
 
@@ -89,4 +89,4 @@ class CONTENT_EXPORT ResourceThrottle {
 
 }  // namespace content
 
-#endif  // CONTENT_PUBLIC_BROWSER_RESOURCE_THROTTLE_H_
+#endif  // CONTENT_NETWORK_RESOURCE_THROTTLE_H_
