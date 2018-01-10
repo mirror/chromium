40ebdc2297d3f415074d6baf4e6cb008d5929c7e
Liquan (Max) Gu <maxlg@chromium.org>
2018-01-10 14:11:31 -0500

    [CustomUserTiming] Implement start&option arg for performance.mark()
    Custom User Timing allows user to use arbitrary timestamps and add
    custom information for performance timeline (performance.mark and
    performance.measure).
    
    This CL implements the performance.mark part of Custom User Timing,
    Basically, it adds an "startTime or PerformanceMarkOptions" argument
    for performance.mark, and hidden behind a runtime-enabled-flag called
    "CustomUserTiming". This feature is set to "Test" status.
    
    Bug: 758385
    Change-Id: I157bc88866e232d07319959e148d9694bf13f11f

---
+++ third_party/WebKit/LayoutTests/http/tests/performance-timing/custom-user-timing/performanceobservers.js
@@ -0,0 +1,23 @@
+// Compares a performance entry to a predefined one
+// perfEntriesToCheck is an array of performance entries from the user agent
+// expectedEntries is an array of performance entries minted by the test
+function checkEntries(perfEntriesToCheck, expectedEntries) {
+  function findMatch(pe) {
+    // we match based on entryType, name, detail(if listed), startTime(if listed)
+    for (var i = expectedEntries.length - 1; i >= 0; i--) {
+      var ex = expectedEntries[i];
+      if (ex.entryType === pe.entryType && ex.name === pe.name &&
+          (ex.detail === undefined || JSON.stringify(ex.detail) === JSON.stringify(pe.detail)) &&
+          (ex.startTime === undefined || ex.startTime === pe.startTime)) {
+        return ex;
+      }
+    }
+    return null;
+  }
+
+  assert_equals(perfEntriesToCheck.length, expectedEntries.length, "performance entries must match");
+
+  perfEntriesToCheck.forEach(function (pe1) {
+    assert_not_equals(findMatch(pe1), null, "Entry matches");
+  });
+}
