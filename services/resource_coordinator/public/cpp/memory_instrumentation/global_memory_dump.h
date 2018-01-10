09d70179fb68eb6888493b4244fea78b2bd1b6a2
Lalit Maganti <lalitm@chromium.org>
2018-01-10 19:25:04 +0000

    memory-infra: add wrapper for global memory dumps
    This wrapper will allow for consumers to obtain metrics without writing
    boilerplate code and also allows us to expose more advanced and esoteric
    data in the future if we wish
    
    Bug: 728199
    Change-Id: I2dfa44840b5712c357144f6329484773db065b44

---
+++ services/resource_coordinator/public/cpp/memory_instrumentation/global_memory_dump.h
@@ -0,0 +1,56 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_RESOURCE_COORDINATOR_PUBLIC_CPP_MEMORY_INSTRUMENTATION_GLOBAL_MEMORY_DUMP_H_
+#define SERVICES_RESOURCE_COORDINATOR_PUBLIC_CPP_MEMORY_INSTRUMENTATION_GLOBAL_MEMORY_DUMP_H_
+
+#include "base/optional.h"
+#include "services/resource_coordinator/public/interfaces/memory_instrumentation/memory_instrumentation.mojom.h"
+
+namespace memory_instrumentation {
+
+// The returned data structure to consumers of the memory_instrumentation
+// service containing dumps for each process.
+class SERVICES_RESOURCE_COORDINATOR_PUBLIC_CPP_EXPORT GlobalMemoryDump {
+ public:
+  class ProcessDump {
+   public:
+    ProcessDump(mojom::ProcessMemoryDumpPtr process_memory_dump);
+    ~ProcessDump();
+
+    base::Optional<uint64_t> GetMetricForDump(const std::string& dump_name,
+                                              const std::string& metric_name);
+
+    base::ProcessId pid() const { return mojo_dump_->pid; }
+    mojom::ProcessType process_type() const { return mojo_dump_->process_type; }
+
+    // TODO(lalitm): remove this when all consumers have migrated to using
+    // GetMetricForDump.
+    const mojom::ChromeMemDump& chrome_dump() const {
+      return *mojo_dump_->chrome_dump;
+    }
+    const mojom::OSMemDump& os_dump() const { return *mojo_dump_->os_dump; }
+
+   private:
+    mojom::ProcessMemoryDumpPtr mojo_dump_;
+
+    DISALLOW_COPY_AND_ASSIGN(ProcessDump);
+  };
+
+  GlobalMemoryDump(std::vector<mojom::ProcessMemoryDumpPtr> process_dumps);
+  ~GlobalMemoryDump();
+
+  const std::forward_list<ProcessDump>& process_dumps() const {
+    return process_dumps_;
+  }
+
+ private:
+  std::forward_list<ProcessDump> process_dumps_;
+
+  DISALLOW_COPY_AND_ASSIGN(GlobalMemoryDump);
+};
+
+}  // namespace memory_instrumentation
+
+#endif  // SERVICES_RESOURCE_COORDINATOR_PUBLIC_CPP_MEMORY_INSTRUMENTATION_GLOBAL_MEMORY_DUMP_H_
