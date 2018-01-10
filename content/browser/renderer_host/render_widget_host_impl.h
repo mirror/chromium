2526e358ee3e9cf89288f3aca69f018954d7b26c
Sahel Sharify <sahel@chromium.org>
2018-01-10 11:57:42 -0500

    Early touchpad fling termination when overscroll mode is not none.
    This cl terminates a fling when overscroll mode is not None and the ack
    for a GSU generated from the fling progress arrives. The overscroll
    action gets completed on GSE generated from the early fling termination.
    Without this change the overscroll action would get completed on GSE
    generated at the end of fling. The early fling termination saves
    overscroll completion from waiting for the fling progress completion.
    This change is particularly useful when the fling velocity is large.
    
    Bug:797855
    Test:FlingControllerTest.EarlyFlingCancelationOnFlingStop
    Change-Id: Ic73bfd4225bcc4484700e339649fb2f448e37bbd

--- content/browser/renderer_host/render_widget_host_impl.h
+++ content/browser/renderer_host/render_widget_host_impl.h
@@ -630,6 +630,7 @@ class CONTENT_EXPORT RenderWidgetHostImpl
   void OnImeCancelComposition() override;
 
   void ProgressFling(base::TimeTicks current_time);
+  void StopFling();
 
  protected:
   // ---------------------------------------------------------------------------
