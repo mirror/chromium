87b397e9fc146fb3a57ceb165eda72e825afebaa
Alexander Semashko <ahest@yandex-team.ru>
2018-01-11 16:16:01 +0300

    Make sure RFH is in 'loading' state if a commit is pending.
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ifa046c93189657af64afeffa982ccfeb2a06cc20

--- content/browser/frame_host/render_frame_host_impl.h
+++ content/browser/frame_host/render_frame_host_impl.h
@@ -669,6 +669,8 @@ class CONTENT_EXPORT RenderFrameHostImpl
     return active_sandbox_flags_;
   }
 
+  bool pending_commit_for_testing() { return pending_commit_; }
+
  protected:
   friend class RenderFrameHostFactory;
 
@@ -798,7 +800,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
       bool present,
       blink::WebSuddenTerminationDisablerType disabler_type);
   void OnDidStartLoading(bool to_different_document);
-  void OnDidStopLoading();
+  void OnDidStopLoading(bool reset_pending_commit);
   void OnDidChangeLoadProgress(double load_progress);
   void OnSerializeAsMHTMLResponse(
       int job_id,
@@ -1133,8 +1135,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
   // document or not.
   bool is_loading_;
 
-  // Used to track whether a commit is expected in this frame. Only used in
-  // tests.
+  // Used to track whether a commit is expected in this frame.
   bool pending_commit_;
 
   // The unique ID of the latest NavigationEntry that this RenderFrameHost is
