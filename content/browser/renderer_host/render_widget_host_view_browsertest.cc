a3e4e58b83a1652f22e065b6a20cda0077bc2679
Stephen McGruer <smcgruer@chromium.org>
2018-01-11 11:50:36 -0500

    [url] Add GURL::GetContentWithoutRef
    To allow proper parsing of data URIs, it is desirable to have a version
    of GetContent() that does not return the ref part of the URI (or the '#'
    character). See bug for more details.
    
    Bug: 123004
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I49c6f4d739a2dd42eecc9947f8e75071b84e9be7

--- content/browser/renderer_host/render_widget_host_view_browsertest.cc
+++ content/browser/renderer_host/render_widget_host_view_browsertest.cc
@@ -586,7 +586,9 @@ class CompositingRenderWidgetHostViewBrowserTestTabCapture
 
   GURL TestUrl() override { return GURL(test_url_); }
 
-  void SetTestUrl(const std::string& url) { test_url_ = url; }
+  void SetTestDataUrl(const std::string& url) {
+    base::ReplaceChars(url, "#", "%23", &test_url_);
+  }
 
   // Loads a page two boxes side-by-side, each half the width of
   // |html_rect_size|, and with different background colors. The test then
@@ -600,7 +602,7 @@ class CompositingRenderWidgetHostViewBrowserTestTabCapture
                                      bool video_frame) {
     const gfx::Size box_size(html_rect_size.width() / 2,
                              html_rect_size.height());
-    SetTestUrl(base::StringPrintf(
+    SetTestDataUrl(base::StringPrintf(
         "data:text/html,<!doctype html>"
         "<div class='left'>"
         "  <div class='right'></div>"
@@ -1003,7 +1005,9 @@ class CompositingRenderWidgetHostViewBrowserTestHiDPI
 
   GURL TestUrl() override { return GURL(test_url_); }
 
-  void SetTestUrl(const std::string& url) { test_url_ = url; }
+  void SetTestDataUrl(const std::string& url) {
+    base::ReplaceChars(url, "#", "%23", &test_url_);
+  }
 
   bool ShouldContinueAfterTestURLLoad() {
     // Short-circuit a pass for platforms where setting up high-DPI fails.
@@ -1034,7 +1038,7 @@ IN_PROC_BROWSER_TEST_P(CompositingRenderWidgetHostViewBrowserTestHiDPI,
   const int kContentHeight = 2000;
   const int kScrollAmount = 100;
 
-  SetTestUrl(
+  SetTestDataUrl(
       base::StringPrintf("data:text/html,<!doctype html>"
                          "<div class='box'></div>"
                          "<style>"
