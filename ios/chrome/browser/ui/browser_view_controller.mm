e0de4010d7262a0a9e4efc69c16d99e4d3a9c743
Gauthier Ambard <gambard@chromium.org>
2018-01-11 14:39:19 +0100

    Add util to check AdaptiveToolbar flag
    This CL adds a until to check if the AdaptiveToolbar is enabled or not.
    It will allow the test to change the hooks and be able to force the
    adaptive toolbar.
    
    Bug:801145
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I5dd4a8615902ee1da06d2639d0daef864e168a9f

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -1929,7 +1929,7 @@ applicationCommandEndpoint:(id<ApplicationCommands>)applicationCommandEndpoint {
   _toolbarModelIOS.reset([_dependencyFactory
       newToolbarModelIOSWithDelegate:_toolbarModelDelegate.get()]);
 
-  if (base::FeatureList::IsEnabled(kAdaptiveToolbar)) {
+  if (IsAdaptiveToolbarEnabled()) {
     PrimaryToolbarCoordinator* topToolbarCoordinator =
         [[PrimaryToolbarCoordinator alloc] initWithBrowserState:_browserState];
     self.primaryToolbarCoordinator = topToolbarCoordinator;
