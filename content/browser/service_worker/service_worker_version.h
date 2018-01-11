9d1094f7ff577851f26c35e6a93e2157da8f4e0e
Kinuko Yasuda <kinuko@chromium.org>
2018-01-10 18:34:03 -0800

    S13nServiceWorker: Send ControllerServiceWorker on navigation commit
    To fix the IPC race problem. More details in:
    https://docs.google.com/document/d/1dvvksKQYWEnUdOcjkyreaTOxJD9L4Gybszb5mxrSzHk/
    
    - also sends controller_obj_info with pre-created handle ID
    - also changes SetController to also send ControllerServiceWorker
    
    The new code doesn't rely on SetController after navigation, therefore
    there's no race condition there. Note that currently we still call
    SetController in ServiceWorkerProviderHost::CompleteNavigationInitialized
    but it's just to update UseCounter. You can see mojo_linux results for
    https://chromium-review.googlesource.com/c/chromium/src/+/724565
    to verify that all layout tests (except for usecounter one) pass without
    calling SetController.
    
    Bug: 778898
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ie1d15bd3695b636427df6329cc5e07878bd24ee6

--- content/browser/service_worker/service_worker_version.h
+++ content/browser/service_worker/service_worker_version.h
@@ -328,10 +328,18 @@ class CONTENT_EXPORT ServiceWorkerVersion
     return event_dispatcher_.get();
   }
 
-  // This must be called when the worker is running.
-  // Returns the 'controller' interface of this worker.
+  // S13nServiceWorker:
+  // Returns the 'controller' interface ptr of this worker. It is expected
+  // that the worker is already starting or running, or is going to be started
+  // soon.
+  // TODO(kinuko): Relying on the callsites to start the worker when it's
+  // not running is a bit sketchy, maybe this should queue a task to check
+  // if the pending request is pending too long? https://crbug.com/797222
   mojom::ControllerServiceWorker* controller() {
-    DCHECK(controller_ptr_.is_bound());
+    if (!controller_ptr_.is_bound()) {
+      DCHECK(!pending_controller_request_.is_pending());
+      pending_controller_request_ = mojo::MakeRequest(&controller_ptr_);
+    }
     return controller_ptr_.get();
   }
 
@@ -779,7 +787,12 @@ class CONTENT_EXPORT ServiceWorkerVersion
 
   // Connected to ServiceWorkerContextClient while the worker is running.
   mojom::ServiceWorkerEventDispatcherPtr event_dispatcher_;
+
+  // S13nServiceWorker: connected to the controller service worker.
+  // |pending_controller_request_| is non-null if the worker is not started
+  // but the |controller_ptr_| is requested.
   mojom::ControllerServiceWorkerPtr controller_ptr_;
+  mojom::ControllerServiceWorkerRequest pending_controller_request_;
 
   std::unique_ptr<ServiceWorkerInstalledScriptsSender>
       installed_scripts_sender_;
