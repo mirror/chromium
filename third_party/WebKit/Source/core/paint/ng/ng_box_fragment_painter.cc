8da4ab28df00ca3fd72e8073325899b8a4c7e6ea
Koji Ishii <kojii@chromium.org>
2018-01-11 16:36:54 +0900

    BoxClipper
    Bug:
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_layout_ng;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I8ed2c541fcfc3fecd04e39e0b0af168af2262008

--- third_party/WebKit/Source/core/paint/ng/ng_box_fragment_painter.cc
+++ third_party/WebKit/Source/core/paint/ng/ng_box_fragment_painter.cc
@@ -20,6 +20,7 @@
 #include "core/paint/PaintInfo.h"
 #include "core/paint/PaintLayer.h"
 #include "core/paint/PaintPhase.h"
+#include "core/paint/ng/ng_box_clipper.h"
 #include "core/paint/ng/ng_fragment_painter.h"
 #include "core/paint/ng/ng_paint_fragment.h"
 #include "core/paint/ng/ng_text_fragment_painter.h"
@@ -110,9 +111,7 @@ void NGBoxFragmentPainter::PaintWithAdjustedOffset(
 
   if (original_phase != PaintPhase::kSelfBlockBackgroundOnly &&
       original_phase != PaintPhase::kSelfOutlineOnly) {
-    // TODO(layout-dev): Clip using BoxClipper.
-    // BoxClipper box_clipper(layout_block_, info, paint_offset,
-    //                       contents_clip_behavior);
+    NGBoxClipper box_clipper(box_fragment_, info);
     PaintObject(info, paint_offset);
   }
 
