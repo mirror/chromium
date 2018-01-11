4e89ddbcd7def58ede832274c21b49b609967bff
edchin <edchin@chromium.org>
2018-01-11 12:48:59 -0800

    [ios] Refactor ExternalAppLauncherTabHelper
    This CL achieves several objectives:
    
    1) Removes the use of |rootViewController| to present UI.
    
    2) Separates launching from the policy decider logic. The
    tab helper holds the policy decider logic. The app launcher
    coordinator does the actual app launching.
    
    3) Better naming: "ExternalAppLauncher" was renamed to
    "AppLauncher". It is more concise, and "external" did not
    provide any value as there is no "internal app".
    
    4) Better folder structure: the tab helper was pulled out
    to it's own folder /chrome/browser/app_launcher. The app
    launcher coordinator lives in /chrome/browser/ui/app_launcher.
    
    Bug: 791792
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I53ab441db05b85211102df169a264f0fcf53195c

---
+++ ios/chrome/browser/app_launcher/app_launcher_tab_helper_unittest.mm
@@ -0,0 +1,73 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/app_launcher/app_launcher_tab_helper.h"
+
+#import "ios/chrome/browser/app_launcher/app_launcher_tab_helper_delegate.h"
+#import "ios/web/public/test/fakes/test_web_state.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#import "testing/gtest_mac.h"
+#include "testing/platform_test.h"
+#include "url/gurl.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+// An object that conforms to AppLauncherTabHelperDelegate for testing.
+@interface TestAppLauncherTabHelperDelegate
+    : NSObject<AppLauncherTabHelperDelegate>
+@property(nonatomic, assign) NSUInteger countOfInvocationsToLaunchApp;
+@end
+
+@implementation TestAppLauncherTabHelperDelegate
+@synthesize countOfInvocationsToLaunchApp = _countOfInvocationsToLaunchApp;
+- (BOOL)appLauncherTabHelper:(AppLauncherTabHelper*)tabHelper
+            launchAppWithURL:(const GURL&)URL
+                  linkTapped:(BOOL)linkTapped {
+  self.countOfInvocationsToLaunchApp++;
+  return YES;
+}
+- (void)appLauncherTabHelper:(AppLauncherTabHelper*)tabHelper
+    showAlertOfRepeatedLaunchesWithCompletionHandler:
+        (ProceduralBlockWithBool)completionHandler {
+}
+@end
+
+// Test fixture for AppLauncherTabHelper class.
+class AppLauncherTabHelperTest : public PlatformTest {
+ protected:
+  AppLauncherTabHelperTest()
+      : delegate_([[TestAppLauncherTabHelperDelegate alloc] init]) {
+    AppLauncherTabHelper::CreateForWebState(&web_state_);
+    tab_helper_ = AppLauncherTabHelper::FromWebState(&web_state_);
+    tab_helper_->SetDelegate(delegate_);
+  }
+
+  web::TestWebState web_state_;
+  TestAppLauncherTabHelperDelegate* delegate_ = nil;
+  AppLauncherTabHelper* tab_helper_;
+};
+
+// Tests that an empty URL does not invoke app launch.
+TEST_F(AppLauncherTabHelperTest, EmptyUrl) {
+  delegate_.countOfInvocationsToLaunchApp = 0U;
+  tab_helper_->RequestToLaunchApp(GURL::EmptyGURL(), GURL::EmptyGURL(), false);
+  EXPECT_EQ(0U, delegate_.countOfInvocationsToLaunchApp);
+}
+
+// Tests that an invalid URL does not invoke app launch.
+TEST_F(AppLauncherTabHelperTest, InvalidUrl) {
+  delegate_.countOfInvocationsToLaunchApp = 0U;
+  tab_helper_->RequestToLaunchApp(GURL("invalid"), GURL::EmptyGURL(), false);
+  EXPECT_EQ(0U, delegate_.countOfInvocationsToLaunchApp);
+}
+
+// Tests that a valid URL does invoke app launch.
+TEST_F(AppLauncherTabHelperTest, ValidUrl) {
+  delegate_.countOfInvocationsToLaunchApp = 0U;
+  tab_helper_->RequestToLaunchApp(GURL("valid://1234"), GURL::EmptyGURL(),
+                                  false);
+  EXPECT_EQ(1U, delegate_.countOfInvocationsToLaunchApp);
+}
