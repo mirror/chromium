28d59387d17176cc4cbfa696fc033dd52f1ff4e0
Albert Chaulk <achaulk@google.com>
2018-01-11 15:10:57 -0500

    Preliminary patch to enable extensions for cast_shell
    This ports most of the app_shell (extensions/shell) environment into
    cast_shell. All changes are gated on enable_extensions=true
    
    Guest views are part of the extension framework. Chrome has what they call
    app_shell, which is content_shell + extensions.
    
    This change starts to pull in what we need from app_shell into cast_shell.
    
    From explorations, it looked like the most practical way to approach this
    is to bring in extension framework instead of trying to cherry-pick
    specific parts as that code is fairly deeply intertwined in Chrome
    and would be hard to garden.
    
    Once compiled it should only leave the parts we need anyway.
    
    Bug: b/70571389
    Test: manual with Browser Sample hosted on localhost
    Change-Id: Id585fe76cbca5d8635082d5210765705e78ab23c

---
+++ chromecast/browser/extensions/stub_storage_monitor.cc
@@ -0,0 +1,11 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/storage_monitor/storage_monitor.h"
+
+namespace storage_monitor {
+StorageMonitor* StorageMonitor::CreateInternal() {
+  return nullptr;
+}
+}  // namespace storage_monitor
