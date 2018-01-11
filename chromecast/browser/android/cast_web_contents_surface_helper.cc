46e7cf9ca06c736e67a6fe444bb23920802e1d42
Zhiheng Li <vincentli@google.com>
2018-01-10 19:55:54 -0800

    Convert cast activity to fragment:  - Add CastWebContentsFragment to allow external activity to run cast  receiver app in its own app.  - Refactor CastWebContentsActivity to share codes with fragment class  - Add a build flag to config CastWebContentsFragment or  CastWebContentsActivity
     BUG: 65100261,65565116
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I0146360c22d3487ffb3971fe04e344aabfcf2372
    Bug:

--- chromecast/browser/android/cast_web_contents_surface_helper.cc
+++ chromecast/browser/android/cast_web_contents_surface_helper.cc
@@ -1,12 +1,12 @@
-// Copyright 2016 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chromecast/browser/android/cast_web_contents_activity.h"
+#include "chromecast/browser/android/cast_web_contents_surface_helper.h"
 
 #include "base/memory/ptr_util.h"
 #include "content/public/browser/web_contents.h"
-#include "jni/CastWebContentsActivity_jni.h"
+#include "jni/CastWebContentsSurfaceHelper_jni.h"
 
 using base::android::JavaParamRef;
 using base::android::ScopedJavaGlobalRef;
@@ -16,13 +16,13 @@ namespace chromecast {
 namespace shell {
 
 namespace {
-const void* kCastWebContentsActivityData;
-const void* kCastWebContentsActivityKey =
-    static_cast<const void*>(&kCastWebContentsActivityData);
+const void* kCastWebContentsSurfaceHelperData;
+const void* kCastWebContentsSurfaceHelperKey =
+    static_cast<const void*>(&kCastWebContentsSurfaceHelperData);
 }  // namespace
 
 // static
-void JNI_CastWebContentsActivity_SetContentVideoViewEmbedder(
+void JNI_CastWebContentsSurfaceHelper_SetContentVideoViewEmbedder(
     JNIEnv* env,
     const JavaParamRef<jobject>& jcaller,
     const JavaParamRef<jobject>& webContents,
@@ -30,37 +30,37 @@ void JNI_CastWebContentsActivity_SetContentVideoViewEmbedder(
   content::WebContents* web_contents =
       content::WebContents::FromJavaWebContents(webContents);
   DCHECK(web_contents);
-  CastWebContentsActivity* activity =
-      CastWebContentsActivity::Get(web_contents);
-  activity->SetContentVideoViewEmbedder(embedder);
+  CastWebContentsSurfaceHelper* fragment =
+      CastWebContentsSurfaceHelper::Get(web_contents);
+  fragment->SetContentVideoViewEmbedder(embedder);
 }
 
 // static
-CastWebContentsActivity* CastWebContentsActivity::Get(
+CastWebContentsSurfaceHelper* CastWebContentsSurfaceHelper::Get(
     content::WebContents* web_contents) {
   DCHECK(web_contents);
-  CastWebContentsActivity* instance = static_cast<CastWebContentsActivity*>(
-      web_contents->GetUserData(kCastWebContentsActivityKey));
+  CastWebContentsSurfaceHelper* instance =
+      static_cast<CastWebContentsSurfaceHelper*>(
+      web_contents->GetUserData(kCastWebContentsSurfaceHelperKey));
   if (!instance) {
-    instance = new CastWebContentsActivity(web_contents);
-    web_contents->SetUserData(kCastWebContentsActivityKey,
+    instance = new CastWebContentsSurfaceHelper();
+    web_contents->SetUserData(kCastWebContentsSurfaceHelperKey,
                               base::WrapUnique(instance));
   }
   return instance;
 }
 
-CastWebContentsActivity::CastWebContentsActivity(
-    content::WebContents* web_contents)
+CastWebContentsSurfaceHelper::CastWebContentsSurfaceHelper()
     : content_video_view_embedder_(ScopedJavaLocalRef<jobject>()) {}
 
-CastWebContentsActivity::~CastWebContentsActivity() {}
+CastWebContentsSurfaceHelper::~CastWebContentsSurfaceHelper() {}
 
-ScopedJavaLocalRef<jobject>
-CastWebContentsActivity::GetContentVideoViewEmbedder() {
+ScopedJavaLocalRef<jobject> CastWebContentsSurfaceHelper
+    ::GetContentVideoViewEmbedder() {
   return ScopedJavaLocalRef<jobject>(content_video_view_embedder_);
 }
 
-void CastWebContentsActivity::SetContentVideoViewEmbedder(
+void CastWebContentsSurfaceHelper::SetContentVideoViewEmbedder(
     const JavaParamRef<jobject>& content_video_view_embedder) {
   content_video_view_embedder_ =
       ScopedJavaGlobalRef<jobject>(content_video_view_embedder);
