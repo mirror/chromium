2bb45d3240d1757da92e3a49410e9b618a5aa61c
Saman Sami <samans@chromium.org>
2018-01-10 19:46:41 -0500

    Surface synchronization: Allocate new LocalSurfaceId on navigation
    In order to be able to tell the difference between the graphical output
    of the renderer before and after navigation, we should allocate a new
    LocalSurfaceId. Currently we rely on content_source_id to achieve this,
    but that is not compatible with Viz. When navigation happens, browser
    allocates a new id for the child and immediately embeds it. After 4
    seconds it drops the fallback SurfaceId if it was generated before
    navigation.
    
    Bug: 695579,775030,777881
    Change-Id: I8ae11dd3de2e8d6cc889ad31f94bdb0bb5f6a5d4

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -768,6 +768,8 @@ bool RenderWidgetHostImpl::GetResizeParams(ResizeParams* resize_params) {
       resize_params->local_surface_id = local_surface_id;
   }
 
+  resize_params->content_source_id = current_content_source_id_;
+
   const bool size_changed =
       !old_resize_params_ ||
       old_resize_params_->new_size != resize_params->new_size ||
@@ -789,6 +791,8 @@ bool RenderWidgetHostImpl::GetResizeParams(ResizeParams* resize_params) {
           resize_params->bottom_controls_height ||
       old_resize_params_->visible_viewport_size !=
           resize_params->visible_viewport_size ||
+      old_resize_params_->content_source_id !=
+          resize_params->content_source_id ||
       (enable_surface_synchronization_ &&
        old_resize_params_->local_surface_id != resize_params->local_surface_id);
 
@@ -800,6 +804,7 @@ bool RenderWidgetHostImpl::GetResizeParams(ResizeParams* resize_params) {
       (!enable_surface_synchronization_ ||
        (resize_params->local_surface_id.has_value() &&
         resize_params->local_surface_id->is_valid()));
+
   return dirty;
 }
 
@@ -1071,13 +1076,19 @@ void RenderWidgetHostImpl::StartNewContentRenderingTimeout(
     uint32_t next_source_id) {
   current_content_source_id_ = next_source_id;
 
-  if (!new_content_rendering_timeout_)
-    return;
+  if (enable_surface_synchronization_) {
+    if (view_)
+      view_->DidNavigate();
+    WasResized();
+  } else {
+    // It is possible for a compositor frame to arrive before the browser is
+    // notified about the page being committed, in which case no timer is
+    // necessary.
+    if (last_received_content_source_id_ >= current_content_source_id_)
+      return;
+  }
 
-  // It is possible for a compositor frame to arrive before the browser is
-  // notified about the page being committed, in which case no timer is
-  // necessary.
-  if (last_received_content_source_id_ >= current_content_source_id_)
+  if (!new_content_rendering_timeout_)
     return;
 
   new_content_rendering_timeout_->Start(new_content_rendering_delay_);
@@ -2499,7 +2510,8 @@ void RenderWidgetHostImpl::DidAllocateLocalSurfaceIdForAutoResize(
     GetScreenInfo(&screen_info);
     Send(new ViewMsg_SetLocalSurfaceIdForAutoResize(
         routing_id_, sequence_number, min_size_for_auto_resize_,
-        max_size_for_auto_resize_, screen_info, local_surface_id));
+        max_size_for_auto_resize_, screen_info, current_content_source_id_,
+        local_surface_id));
   }
 }
 
@@ -2750,32 +2762,47 @@ void RenderWidgetHostImpl::SubmitCompositorFrame(
   if (touch_emulator_)
     touch_emulator_->SetDoubleTapSupportForPageEnabled(!is_mobile_optimized);
 
-  // Ignore this frame if its content has already been unloaded. Source ID
-  // is always zero for an OOPIF because we are only concerned with displaying
-  // stale graphics on top-level frames. We accept frames that have a source ID
-  // greater than |current_content_source_id_| because in some cases the first
-  // compositor frame can arrive before the navigation commit message that
-  // updates that value.
-  if (view_ && frame.metadata.content_source_id >= current_content_source_id_) {
-    view_->SubmitCompositorFrame(local_surface_id, std::move(frame),
-                                 std::move(hit_test_region_list));
-    view_->DidReceiveRendererFrame();
-  } else {
+  if (enable_surface_synchronization_) {
     if (view_) {
-      frame.metadata.begin_frame_ack.has_damage = false;
-      view_->OnDidNotProduceFrame(frame.metadata.begin_frame_ack);
+      // If Surface Synchronization is on, then |new_content_rendering_timeout_|
+      // is stopped in DidReceiveFirstFrameAfterNavigation.
+      view_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                                   std::move(hit_test_region_list));
+      view_->DidReceiveRendererFrame();
+    } else {
+      std::vector<viz::ReturnedResource> resources =
+          viz::TransferableResource::ReturnResources(frame.resource_list);
+      renderer_compositor_frame_sink_->DidReceiveCompositorFrameAck(resources);
     }
-    std::vector<viz::ReturnedResource> resources =
-        viz::TransferableResource::ReturnResources(frame.resource_list);
-    renderer_compositor_frame_sink_->DidReceiveCompositorFrameAck(resources);
-  }
+  } else {
+    // Ignore this frame if its content has already been unloaded. Source ID
+    // is always zero for an OOPIF because we are only concerned with displaying
+    // stale graphics on top-level frames. We accept frames that have a source
+    // ID greater than |current_content_source_id_| because in some cases the
+    // first compositor frame can arrive before the navigation commit message
+    // that updates that value.
+    if (view_ &&
+        frame.metadata.content_source_id >= current_content_source_id_) {
+      view_->SubmitCompositorFrame(local_surface_id, std::move(frame),
+                                   std::move(hit_test_region_list));
+      view_->DidReceiveRendererFrame();
+    } else {
+      if (view_) {
+        frame.metadata.begin_frame_ack.has_damage = false;
+        view_->OnDidNotProduceFrame(frame.metadata.begin_frame_ack);
+      }
+      std::vector<viz::ReturnedResource> resources =
+          viz::TransferableResource::ReturnResources(frame.resource_list);
+      renderer_compositor_frame_sink_->DidReceiveCompositorFrameAck(resources);
+    }
 
-  // After navigation, if a frame belonging to the new page is received, stop
-  // the timer that triggers clearing the graphics of the last page.
-  if (new_content_rendering_timeout_ &&
-      last_received_content_source_id_ >= current_content_source_id_ &&
-      new_content_rendering_timeout_->IsRunning()) {
-    new_content_rendering_timeout_->Stop();
+    // After navigation, if a frame belonging to the new page is received, stop
+    // the timer that triggers clearing the graphics of the last page.
+    if (new_content_rendering_timeout_ &&
+        last_received_content_source_id_ >= current_content_source_id_ &&
+        new_content_rendering_timeout_->IsRunning()) {
+      new_content_rendering_timeout_->Stop();
+    }
   }
 
   if (delegate_)
@@ -2917,6 +2944,15 @@ void RenderWidgetHostImpl::SetWidget(mojom::WidgetPtr widget) {
 void RenderWidgetHostImpl::ProgressFling(base::TimeTicks current_time) {
   if (input_router_)
     input_router_->ProgressFling(current_time);
+}
+
+void RenderWidgetHostImpl::DidReceiveFirstFrameAfterNavigation() {
+  DCHECK(enable_surface_synchronization_);
+  if (!new_content_rendering_timeout_ ||
+      !new_content_rendering_timeout_->IsRunning()) {
+    return;
+  }
+  new_content_rendering_timeout_->Stop();
 }
 
 }  // namespace content
--- content/browser/renderer_host/render_widget_host_unittest.cc
+++ content/browser/renderer_host/render_widget_host_unittest.cc
@@ -1723,8 +1723,12 @@ TEST_F(RenderWidgetHostTest, MultipleInputEvents) {
 // Test that the rendering timeout for newly loaded content fires
 // when enough time passes without receiving a new compositor frame.
 TEST_F(RenderWidgetHostTest, NewContentRenderingTimeout) {
-  const viz::LocalSurfaceId local_surface_id(1,
-                                             base::UnguessableToken::Create());
+  const viz::LocalSurfaceId local_surface_id1(1,
+                                              base::UnguessableToken::Create());
+  const viz::LocalSurfaceId local_surface_id2(1,
+                                              base::UnguessableToken::Create());
+  const viz::LocalSurfaceId local_surface_id3(1,
+                                              base::UnguessableToken::Create());
 
   host_->set_new_content_rendering_delay_for_testing(
       base::TimeDelta::FromMicroseconds(10));
@@ -1736,7 +1740,7 @@ TEST_F(RenderWidgetHostTest, NewContentRenderingTimeout) {
                    .AddDefaultRenderPass()
                    .SetContentSourceId(5)
                    .Build();
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame), nullptr, 0);
+  host_->SubmitCompositorFrame(local_surface_id1, std::move(frame), nullptr, 0);
   base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
       FROM_HERE, base::MessageLoop::QuitWhenIdleClosure(),
       TimeDelta::FromMicroseconds(20));
@@ -1752,7 +1756,7 @@ TEST_F(RenderWidgetHostTest, NewContentRenderingTimeout) {
               .AddDefaultRenderPass()
               .SetContentSourceId(9)
               .Build();
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame), nullptr, 0);
+  host_->SubmitCompositorFrame(local_surface_id2, std::move(frame), nullptr, 0);
   base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
       FROM_HERE, base::MessageLoop::QuitWhenIdleClosure(),
       TimeDelta::FromMicroseconds(20));
@@ -1767,7 +1771,7 @@ TEST_F(RenderWidgetHostTest, NewContentRenderingTimeout) {
               .AddDefaultRenderPass()
               .SetContentSourceId(7)
               .Build();
-  host_->SubmitCompositorFrame(local_surface_id, std::move(frame), nullptr, 0);
+  host_->SubmitCompositorFrame(local_surface_id3, std::move(frame), nullptr, 0);
   host_->StartNewContentRenderingTimeout(7);
   base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
       FROM_HERE, base::MessageLoop::QuitWhenIdleClosure(),
@@ -1790,8 +1794,12 @@ TEST_F(RenderWidgetHostTest, NewContentRenderingTimeout) {
 // This tests that a compositor frame received with a stale content source ID
 // in its metadata is properly discarded.
 TEST_F(RenderWidgetHostTest, SwapCompositorFrameWithBadSourceId) {
-  const viz::LocalSurfaceId local_surface_id(1,
-                                             base::UnguessableToken::Create());
+  const viz::LocalSurfaceId local_surface_id1(1,
+                                              base::UnguessableToken::Create());
+  const viz::LocalSurfaceId local_surface_id2(2,
+                                              base::UnguessableToken::Create());
+  const viz::LocalSurfaceId local_surface_id3(3,
+                                              base::UnguessableToken::Create());
 
   host_->StartNewContentRenderingTimeout(100);
   host_->set_new_content_rendering_delay_for_testing(
@@ -1804,7 +1812,7 @@ TEST_F(RenderWidgetHostTest, SwapCompositorFrameWithBadSourceId) {
                      .SetBeginFrameAck(viz::BeginFrameAck(0, 1, true))
                      .SetContentSourceId(99)
                      .Build();
-    host_->SubmitCompositorFrame(local_surface_id, std::move(frame), nullptr,
+    host_->SubmitCompositorFrame(local_surface_id1, std::move(frame), nullptr,
                                  0);
     EXPECT_FALSE(
         static_cast<TestView*>(host_->GetView())->did_swap_compositor_frame());
@@ -1820,7 +1828,7 @@ TEST_F(RenderWidgetHostTest, SwapCompositorFrameWithBadSourceId) {
                      .AddDefaultRenderPass()
                      .SetContentSourceId(100)
                      .Build();
-    host_->SubmitCompositorFrame(local_surface_id, std::move(frame), nullptr,
+    host_->SubmitCompositorFrame(local_surface_id2, std::move(frame), nullptr,
                                  0);
     EXPECT_TRUE(
         static_cast<TestView*>(host_->GetView())->did_swap_compositor_frame());
@@ -1835,7 +1843,7 @@ TEST_F(RenderWidgetHostTest, SwapCompositorFrameWithBadSourceId) {
                      .AddDefaultRenderPass()
                      .SetContentSourceId(101)
                      .Build();
-    host_->SubmitCompositorFrame(local_surface_id, std::move(frame), nullptr,
+    host_->SubmitCompositorFrame(local_surface_id3, std::move(frame), nullptr,
                                  0);
     EXPECT_TRUE(
         static_cast<TestView*>(host_->GetView())->did_swap_compositor_frame());
@@ -2947,17 +2955,21 @@ TEST_F(RenderWidgetHostTest, RenderWidgetSurfaceProperties) {
   RenderWidgetSurfaceProperties prop1;
   prop1.size = gfx::Size(200, 200);
   prop1.device_scale_factor = 1.f;
+  prop1.content_source_id = 8;
   RenderWidgetSurfaceProperties prop2;
   prop2.size = gfx::Size(300, 300);
   prop2.device_scale_factor = 2.f;
+  prop2.content_source_id = 9;
 
   EXPECT_EQ(
       "RenderWidgetSurfaceProperties(size(this: 200x200, other: 300x300), "
-      "device_scale_factor(this: 1, other: 2))",
+      "device_scale_factor(this: 1, other: 2), content_source_id(this: 8, "
+      "other: 9))",
       prop1.ToDiffString(prop2));
   EXPECT_EQ(
       "RenderWidgetSurfaceProperties(size(this: 300x300, other: 200x200), "
-      "device_scale_factor(this: 2, other: 1))",
+      "device_scale_factor(this: 2, other: 1), content_source_id(this: 9, "
+      "other: 8))",
       prop2.ToDiffString(prop1));
   EXPECT_EQ("", prop1.ToDiffString(prop1));
   EXPECT_EQ("", prop2.ToDiffString(prop2));
