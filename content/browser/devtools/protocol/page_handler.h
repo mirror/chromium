3e3ba482a635d1b79211f08cf6fabfbbab311eb5
David Vallet <dvallet@chromium.org>
2018-01-11 17:07:38 +1100

    Initial version of DevTools download to stream functionality Design Doc: https://docs.google.com/document/d/1kXMLyq9fSSrOANtcJSlDtJKFjlI3B-vkKgTEvsNjugA Bug:696481
    Change-Id: I40584d41d21b1e37fed9f5c952bb206d44988c89

--- content/browser/devtools/protocol/page_handler.h
+++ content/browser/devtools/protocol/page_handler.h
@@ -136,6 +136,8 @@ class PageHandler : public DevToolsDomainHandler,
 
   Response SetDownloadBehavior(const std::string& behavior,
                                Maybe<std::string> download_path) override;
+  void OnDownloadStarted(content::DownloadItem* item,
+                         content::DownloadInterruptReason interrupt_reason);
 
   void GetAppManifest(
       std::unique_ptr<GetAppManifestCallback> callback) override;
