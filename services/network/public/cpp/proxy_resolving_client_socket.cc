b66c3a9bf6104dece0f3bd0f387fee60a87f694e
Helen Li <xunjieli@chromium.org>
2018-01-11 16:33:19 -0500

    Make ProxyResolvingClientSocket not create a new network session for each socket
    ProxyResolvingClientSocket creates a fresh HttpNetworkSession for each socket.
    This is very expensive. This CL creates a factory class which takes care of
    creating network session, so it can be shared across different socket instances.
    This will also make mojoifying this class easier because NetworkContext can be
    re-used.
    
    A follow-up CL will disable H2 and QUIC proxies on the HttpNetworkSession
    created for this purpose (more please see linked bug).
    
    Bug: 793066
    Change-Id: I7b942db3246915abd0ce755ca1f9f29349b50776

--- services/network/public/cpp/proxy_resolving_client_socket.cc
+++ services/network/public/cpp/proxy_resolving_client_socket.cc
@@ -23,82 +23,23 @@
 #include "net/socket/client_socket_handle.h"
 #include "net/socket/client_socket_pool_manager.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
-#include "net/url_request/url_request_context.h"
-#include "net/url_request/url_request_context_getter.h"
 
 namespace network {
 
 ProxyResolvingClientSocket::ProxyResolvingClientSocket(
-    net::ClientSocketFactory* socket_factory,
-    const scoped_refptr<net::URLRequestContextGetter>& request_context_getter,
+    net::HttpNetworkSession* network_session,
     const net::SSLConfig& ssl_config,
     const GURL& url)
-    : ssl_config_(ssl_config),
-      pac_request_(NULL),
+    : network_session_(network_session),
+      ssl_config_(ssl_config),
+      pac_request_(nullptr),
       url_(url),
-      net_log_(net::NetLogWithSource::Make(
-          request_context_getter->GetURLRequestContext()->net_log(),
-          net::NetLogSourceType::SOCKET)),
+      net_log_(net::NetLogWithSource::Make(network_session_->net_log(),
+                                           net::NetLogSourceType::SOCKET)),
       weak_factory_(this) {
-  DCHECK(request_context_getter.get());
-  net::URLRequestContext* request_context =
-      request_context_getter->GetURLRequestContext();
-  DCHECK(request_context);
   // TODO(xunjieli): Handle invalid URLs more gracefully (at mojo API layer
   // or when the request is created).
   DCHECK(url_.is_valid());
-
-  // TODO(xunjieli): https://crbug.com/793066. Avoid creating one context per
-  // socket.
-  net::HttpNetworkSession::Context session_context;
-  session_context.client_socket_factory = socket_factory;
-  session_context.host_resolver = request_context->host_resolver();
-  session_context.cert_verifier = request_context->cert_verifier();
-  session_context.transport_security_state =
-      request_context->transport_security_state();
-  session_context.cert_transparency_verifier =
-      request_context->cert_transparency_verifier();
-  session_context.ct_policy_enforcer = request_context->ct_policy_enforcer();
-  // TODO(rkn): This is NULL because ChannelIDService is not thread safe.
-  // TODO(mmenke):  The above comment makes no sense, as not a single one of
-  // these classes is thread safe. Figure out if the comment's wrong, or if this
-  // entire class is badly broken.
-  session_context.channel_id_service = NULL;
-  session_context.proxy_service = request_context->proxy_service();
-  session_context.ssl_config_service = request_context->ssl_config_service();
-  session_context.http_auth_handler_factory =
-      request_context->http_auth_handler_factory();
-  session_context.http_server_properties =
-      request_context->http_server_properties();
-  session_context.net_log = request_context->net_log();
-
-  const net::HttpNetworkSession::Params* reference_params =
-      request_context->GetNetworkSessionParams();
-  net::HttpNetworkSession::Params session_params;
-  if (reference_params) {
-    // TODO(mmenke):  Just copying specific parameters seems highly regression
-    // prone.  Should have a better way to do this.
-    session_params.host_mapping_rules = reference_params->host_mapping_rules;
-    session_params.ignore_certificate_errors =
-        reference_params->ignore_certificate_errors;
-    session_params.testing_fixed_http_port =
-        reference_params->testing_fixed_http_port;
-    session_params.testing_fixed_https_port =
-        reference_params->testing_fixed_https_port;
-    session_params.enable_http2 = reference_params->enable_http2;
-    session_params.enable_http2_alternative_service =
-        reference_params->enable_http2_alternative_service;
-  }
-
-  network_session_.reset(
-      new net::HttpNetworkSession(session_params, session_context));
-
-  net::HttpAuthCache* other_auth_cache =
-      request_context->http_transaction_factory()
-          ->GetSession()
-          ->http_auth_cache();
-  DCHECK(other_auth_cache);
-  network_session_->http_auth_cache()->UpdateAllFrom(*other_auth_cache);
 }
 
 ProxyResolvingClientSocket::~ProxyResolvingClientSocket() {
@@ -287,8 +228,8 @@ void ProxyResolvingClientSocket::ConnectToProxy(int net_error) {
   // Now that the proxy is resolved, issue a socket connect.
   net::HostPortPair host_port_pair = net::HostPortPair::FromURL(url_);
   net_error = net::InitSocketHandleForRawConnect(
-      host_port_pair, network_session_.get(), proxy_info_, ssl_config_,
-      ssl_config_, net::PRIVACY_MODE_DISABLED, net_log_, transport_.get(),
+      host_port_pair, network_session_, proxy_info_, ssl_config_, ssl_config_,
+      net::PRIVACY_MODE_DISABLED, net_log_, transport_.get(),
       base::BindRepeating(&ProxyResolvingClientSocket::ConnectToProxyDone,
                           base::Unretained(this)));
   if (net_error != net::ERR_IO_PENDING) {
