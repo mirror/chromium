7d4ce9c6e9fdbd751b8232dc192fbea1d159059c
Gauthier Ambard <gambard@chromium.org>
2018-01-11 14:43:25 +0100

    Add test suite for the adaptive toolbar
    This CL adds a simple test for the adaptive toolbar, passing only with
    the adaptive toolbar.
    It makes sure that the app can still start with the flag enabled, and
    that the flag is enabling the toolbar.
    
    It uses the test hooks to make sure the flag is enabled for this test.
    
    Bug:801145
    Change-Id: I1ec3c85e5fdbeade51979386ac2c5dd1f997a34a

--- testing/buildbot/manage.py
+++ testing/buildbot/manage.py
@@ -94,6 +94,7 @@ SKIP_GN_ISOLATE_MAP_TARGETS = {
   'ios_chrome_bookmarks_egtests',
   'ios_chrome_integration_egtests',
   'ios_chrome_payments_egtests',
+  'ios_chrome_adaptive_toolbar_egtests',
   'ios_chrome_reading_list_egtests',
   'ios_chrome_settings_egtests',
   'ios_chrome_smoke_egtests',
