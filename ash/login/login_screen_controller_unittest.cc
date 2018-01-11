d543cd8f343386167fb013b8d41e9ee1f8f8ad0c
Jialiu Lin <jialiul@chromium.org>
2018-01-11 14:03:28 -0800

    Capture GAIA password for view-based offline login flow
    Follow-up CL of https://chromium-review.googlesource.com/827709
    to cover view-based offline login flow.
    
    This requires passing password_manager::SyncPasswordData to
    AuthenticateUser(..) mojo call defined in ash/public/interfaces/login_screen.mojom.
    Therefore, also creating components/password_manager/public/interfaces/* to mirror
    SyncPasswordData in mojo.
    
    Bug:777994
    
    Change-Id: Ied03f6cf38fb9dbaebc4eeb8bee8a6908476a59c

--- ash/login/login_screen_controller_unittest.cc
+++ ash/login/login_screen_controller_unittest.cc
@@ -36,7 +36,7 @@ TEST_F(LoginScreenControllerTest, RequestAuthentication) {
 
   // Verify AuthenticateUser mojo call is run with the same account id, a
   // (hashed) password, and the correct PIN state.
-  EXPECT_CALL(*client, AuthenticateUser_(id, hashed_password, false, _));
+  EXPECT_CALL(*client, AuthenticateUser_(id, hashed_password, _, false, _));
   base::Optional<bool> callback_result;
   controller->AuthenticateUser(
       id, password, false,
@@ -61,7 +61,7 @@ TEST_F(LoginScreenControllerTest, RequestAuthentication) {
   std::string hashed_pin = "cqgMB9rwrcE35iFxm+4vP2toO6qkzW+giCnCcEou92Y=";
   EXPECT_NE(pin, hashed_pin);
 
-  EXPECT_CALL(*client, AuthenticateUser_(id, hashed_pin, true, _));
+  EXPECT_CALL(*client, AuthenticateUser_(id, hashed_pin, _, true, _));
   controller->AuthenticateUser(
       id, pin, true,
       base::BindOnce([](base::Optional<bool>* result,
