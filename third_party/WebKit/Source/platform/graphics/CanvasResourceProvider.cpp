44a5f5340971c0ddfb3ce63fcb818a5db52ec8b5
xlai <xlai@chromium.org>
2018-01-11 17:42:18 -0500

    Fix perf regression on replacing AcceleratedImageBufferSurface with CanvasResourceProvider
    AcceleratedImageBufferSurface::NewImageSnapshot() did call
    RetainOriginalSkImageForCopyOnWrite(), which is essential in triggering texture
    caching. CanvasResourceProvider::Snapshot() should do the same thing.
    
    
    Bug: 801140
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I1a3880d9db7b50c3b7cdf17a638608fd9628651a

--- third_party/WebKit/Source/platform/graphics/CanvasResourceProvider.cpp
+++ third_party/WebKit/Source/platform/graphics/CanvasResourceProvider.cpp
@@ -8,6 +8,7 @@
 #include "gpu/GLES2/gl2extchromium.h"
 #include "gpu/command_buffer/common/capabilities.h"
 #include "gpu/command_buffer/common/gpu_memory_buffer_support.h"
+#include "platform/graphics/AcceleratedStaticBitmapImage.h"
 #include "platform/graphics/CanvasResource.h"
 #include "platform/graphics/StaticBitmapImage.h"
 #include "platform/graphics/gpu/SharedGpuContext.h"
@@ -325,12 +326,8 @@ scoped_refptr<StaticBitmapImage> CanvasResourceProvider::Snapshot() {
     // the compositor's behavior. Therefore, we must trigger copy-on-write
     // even though we are not technically writing to the texture, only to its
     // parameters.
-    // If this issue with readback affecting state is ever fixed, then we'll
-    // have to do this instead of triggering a copy-on-write:
-    // static_cast<AcceleratedStaticBitmapImage*>(image.get())
-    //   ->RetainOriginalSkImageForCopyOnWrite();
-    GetSkSurface()->notifyContentWillChange(
-        SkSurface::kRetain_ContentChangeMode);
+    static_cast<AcceleratedStaticBitmapImage*>(image.get())
+        ->RetainOriginalSkImageForCopyOnWrite();
   }
   return image;
 }
