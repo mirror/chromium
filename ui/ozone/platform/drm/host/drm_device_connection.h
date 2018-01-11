ccd2c123b53c86f660e7cc6a455557dc755beee5
Robert Kroeger <rjkroege@chromium.org>
2018-01-11 11:17:05 -0500

    presub

--- ui/ozone/platform/drm/host/drm_device_connection.h
+++ ui/ozone/platform/drm/host/drm_device_connection.h
@@ -31,7 +31,8 @@ class DrmDeviceConnector : public GpuPlatformSupportHost {
       int host_id,
       scoped_refptr<base::SingleThreadTaskRunner> ui_runner,
       scoped_refptr<base::SingleThreadTaskRunner> send_runner,
-      const base::RepeatingCallback<void(IPC::Message*)>& send_callback) override;
+      const base::RepeatingCallback<void(IPC::Message*)>& send_callback)
+      override;
   void OnChannelDestroyed(int host_id) override;
   void OnGpuServiceLaunched(
       scoped_refptr<base::SingleThreadTaskRunner> ui_runner,
