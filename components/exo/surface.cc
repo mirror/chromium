4c5045b0a66436bc28b09224037d486e0adcb3e4
Mike Reed <reed@google.com>
2018-01-11 08:36:31 -0500

    make input_region optional
    Bug:
    Change-Id: Iaf7676d126823a22c9827791e180e135cfd55788

--- components/exo/surface.cc
+++ components/exo/surface.cc
@@ -492,7 +492,7 @@ void Surface::CommitSurfaceHierarchy(bool synchronized) {
     pending_state_.only_visible_on_secure_output = false;
 
     window_->SetEventTargetingPolicy(
-        state_.input_region.IsEmpty()
+        (state_.input_region.has_value() && state_.input_region->IsEmpty())
             ? ui::mojom::EventTargetingPolicy::DESCENDANTS_ONLY
             : ui::mojom::EventTargetingPolicy::TARGET_AND_DESCENDANTS);
 
@@ -552,8 +552,11 @@ void Surface::CommitSurfaceHierarchy(bool synchronized) {
   }
 
   surface_hierarchy_content_bounds_ = gfx::Rect(content_size_);
-  hit_test_region_ = state_.input_region;
-  hit_test_region_.Intersect(surface_hierarchy_content_bounds_);
+  if (state_.input_region) {
+    hit_test_region_ = state_.input_region;
+    hit_test_region_.Intersect(surface_hierarchy_content_bounds_);
+  } else
+    hit_test_region_surface_hierarchy_content_bounds_;
 
   int outset = state_.input_outset;
   if (outset > 0) {
@@ -718,7 +721,7 @@ bool Surface::FillsBoundsOpaquely() const {
 ////////////////////////////////////////////////////////////////////////////////
 // Buffer, private:
 
-Surface::State::State() : input_region(SkRegion(SkIRect::MakeLargest())) {}
+Surface::State::State() {}
 
 Surface::State::~State() = default;
 
