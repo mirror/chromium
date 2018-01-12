5d6a32b8b34554be3a916c16482d7bcbb5ed35d6
Charles Harrison <csharrison@chromium.org>
2018-01-12 13:46:19 -0500

    Remove PageLoad.Experimental.Cache.RequestPercent.ParseStop
    Bug: 800203
    Change-Id: I4fc5d6e9af061c7b4e06c149d39197e7c9b80b39

--- chrome/browser/page_load_metrics/observers/core_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/core_page_load_metrics_observer.cc
@@ -194,8 +194,6 @@ const char kHistogramForegroundToFirstContentfulPaint[] =
 const char kHistogramForegroundToFirstMeaningfulPaint[] =
     "PageLoad.Experimental.PaintTiming.ForegroundToFirstMeaningfulPaint";
 
-const char kHistogramCacheRequestPercentParseStop[] =
-    "PageLoad.Experimental.Cache.RequestPercent.ParseStop";
 const char kHistogramCacheTotalRequestsParseStop[] =
     "PageLoad.Experimental.Cache.TotalRequests.ParseStop";
 const char kHistogramTotalRequestsParseStop[] =
@@ -600,9 +598,6 @@ void CorePageLoadMetricsObserver::OnParseStop(
 
     int total_resources = num_cache_resources_ + num_network_resources_;
     if (total_resources) {
-      int percent_cached = (100 * num_cache_resources_) / total_resources;
-      UMA_HISTOGRAM_PERCENTAGE(internal::kHistogramCacheRequestPercentParseStop,
-                               percent_cached);
       UMA_HISTOGRAM_COUNTS(internal::kHistogramCacheTotalRequestsParseStop,
                            num_cache_resources_);
       UMA_HISTOGRAM_COUNTS(internal::kHistogramTotalRequestsParseStop,
