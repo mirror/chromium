08d04881de986885cf6b4a9c38c47c1008f778e4
Jeffrey Kardatzke <jkardatzke@google.com>
2018-01-12 15:00:34 -0800

    Add Mojo interface for Android screen capture
    This is used to perform screen capture of the entire ChromeOS desktop
    from Android applications via the MediaProjection API. This is just a
    stub for the implementation in Chrome to keep the review size smaller.
    
    Design doc is here: goto/arcppscreencapture
    
    Bug: b:38452042
    Change-Id: I91085b26c055ab2dce076ee143146e9978640ae3

---
+++ chrome/browser/chromeos/arc/screen_capture/arc_screen_capture_bridge.h
@@ -0,0 +1,54 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_ARC_SCREEN_CAPTURE_ARC_SCREEN_CAPTURE_BRIDGE_H_
+#define CHROME_BROWSER_CHROMEOS_ARC_SCREEN_CAPTURE_ARC_SCREEN_CAPTURE_BRIDGE_H_
+
+#include "base/macros.h"
+#include "components/arc/common/screen_capture.mojom.h"
+#include "components/keyed_service/core/keyed_service.h"
+
+namespace content {
+class BrowserContext;
+}  // namespace content
+
+namespace arc {
+
+class ArcBridgeService;
+
+class ArcScreenCaptureBridge : public KeyedService,
+                               public mojom::ScreenCaptureHost {
+ public:
+  // Returns singleton instance for the given BrowserContext,
+  // or nullptr if the browser |context| is not allowed to use ARC.
+  static ArcScreenCaptureBridge* GetForBrowserContext(
+      content::BrowserContext* context);
+
+  ArcScreenCaptureBridge(content::BrowserContext* context,
+                         ArcBridgeService* bridge_service);
+  ~ArcScreenCaptureBridge() override;
+
+  // ScreenCapture Mojo host interface
+  void OpenSession(mojom::ScreenCaptureSessionRequest request,
+                   mojom::ScreenCaptureSessionNotifierPtr notifier,
+                   const std::string& package_name,
+                   uint32_t width,
+                   uint32_t height,
+                   OpenSessionCallback callback) override;
+  void RequestPermission(const std::string& display_name,
+                         const std::string& package_name,
+                         RequestPermissionCallback callback) override;
+
+ private:
+  ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
+
+  // WeakPtrFactory to use for callbacks.
+  base::WeakPtrFactory<ArcScreenCaptureBridge> weak_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(ArcScreenCaptureBridge);
+};
+
+}  // namespace arc
+
+#endif  // CHROME_BROWSER_CHROMEOS_ARC_SCREEN_CAPTURE_ARC_SCREEN_CAPTURE_BRIDGE_H_
