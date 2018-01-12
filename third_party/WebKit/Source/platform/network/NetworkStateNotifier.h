e51cded30bbf774d8e48a1b8435476399f861f87
Tarun Bansal <tbansal@chromium.org>
2018-01-11 23:06:48 -0800

    dns netinfo
    Bug:
    Change-Id: I19ece2a1bbbe71e919b9c15aa247269958f4b252

--- third_party/WebKit/Source/platform/network/NetworkStateNotifier.h
+++ third_party/WebKit/Source/platform/network/NetworkStateNotifier.h
@@ -211,15 +211,19 @@ class PLATFORM_EXPORT NetworkStateNotifier {
   // and supplied overridden values are used instead until clearOverride() is
   // called.  This is used for layout tests (see crbug.com/377736) and inspector
   // emulation.
+  // If WebConnectionType is set to WebConnectionType::kUnknown, its value would
+  // be computed using the provided |http_rtt_msec|.
+  // |max_bandwidth_mbps| is used to override both the |max_bandwidth_mbps| and
+  // |downlink_throughput_mbps|.
   //
   // Since this class is a singleton, tests must clear override when completed
   // to avoid indeterminate state across the test harness.
-  void SetNetworkConnectionInfoOverride(bool on_line,
-                                        WebConnectionType,
-                                        double max_bandwidth_mbps);
-  void SetNetworkQualityInfoOverride(WebEffectiveConnectionType effective_type,
-                                     unsigned long transport_rtt_msec,
-                                     double downlink_throughput_mbps);
+  void SetNetworkConnectionInfoOverride(
+      bool on_line,
+      WebConnectionType,
+      WebEffectiveConnectionType effective_type,
+      unsigned long http_rtt_msec,
+      double max_bandwidth_mbps);
   void SetSaveDataEnabledOverride(bool enabled);
   void ClearOverride();
 
