47f4fd28e12a6d63ffa3f602e714023568f4bc1d
Thomas Tangl <tangltom@chromium.org>
2018-01-12 17:28:20 +0100

    Add accounts submenu to DICE user menu (turn-on-sync)
    When the "Sync to another account" button is clicked in the DICE
    user menu, a submenu showing a list of web accounts (other than the
    first one) should be displayed. This CL adds this feature.
    
    This CL only adds the buttons, i.e. the UI, but not the actions for
    them. The action handling will come with another CL.
    
    Mocks can be found in the bug report.
    
    Bug: 786369
    Change-Id: I582b1095b5e8004c2a0ff2a4fa3ab4fdbc19b5a7

--- chrome/browser/ui/views/hover_button_unittest.cc
+++ chrome/browser/ui/views/hover_button_unittest.cc
@@ -11,11 +11,15 @@
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/gfx/text_utils.h"
+#include "ui/views/bubble/bubble_dialog_delegate.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/label.h"
+#include "ui/views/focus/focus_manager.h"
 #include "ui/views/test/test_views_delegate.h"
 #include "ui/views/test/views_test_base.h"
+#include "ui/views/test/widget_test.h"
 #include "ui/views/view.h"
+#include "ui/views/widget/widget.h"
 
 namespace {
 
@@ -41,6 +45,14 @@ constexpr TitleSubtitlePair kTitleSubtitlePairs[] = {
      "If you're happy and you know it, clap your hands!", true},
 };
 
+class BubbleWithButton : public views::BubbleDialogDelegateView {
+ public:
+  BubbleWithButton(HoverButton* button, views::View* anchor)
+      : BubbleDialogDelegateView(anchor, views::BubbleBorder::NONE) {
+    this->AddChildView(button);
+  }
+};
+
 class HoverButtonTest : public views::ViewsTestBase {
  public:
   HoverButtonTest() {}
@@ -62,6 +74,28 @@ class HoverButtonTest : public views::ViewsTestBase {
 
  private:
   DISALLOW_COPY_AND_ASSIGN(HoverButtonTest);
+};
+
+class HoverButtonWidgetTest : public views::test::WidgetTest {
+ public:
+  HoverButtonWidgetTest() {}
+
+  void SetUp() override {
+    WidgetTest::SetUp();
+    desktop_widget_ = CreateTopLevelPlatformWidget();
+    test_views_delegate()->set_layout_provider(
+        ChromeLayoutProvider::CreateLayoutProvider());
+  }
+
+  void TearDown() override {
+    desktop_widget_->CloseNow();
+    WidgetTest::TearDown();
+  }
+
+ private:
+  views::Widget* desktop_widget_;
+
+  DISALLOW_COPY_AND_ASSIGN(HoverButtonWidgetTest);
 };
 
 }  // namespace
@@ -150,4 +184,50 @@ TEST_F(HoverButtonTest, CustomTooltip) {
         base::ASCIIToUTF16("\n"));
     EXPECT_EQ(expected, base::UTF8ToUTF16(accessible_name));
   }
+}
+
+// Test that a hover button only gets focus when it is in the active window.
+TEST_F(HoverButtonWidgetTest, CorrectFocusState) {
+  HoverButton* button1 =
+      new HoverButton(nullptr, base::ASCIIToUTF16("Button 1"));
+  HoverButton* button2 =
+      new HoverButton(nullptr, base::ASCIIToUTF16("Button 2"));
+
+  views::Widget* anchor = CreateTopLevelPlatformWidget();
+  // Place |button1| and |button2| in different windows.
+  views::Widget* bubble1 = BubbleWithButton::CreateBubble(
+      new BubbleWithButton(button1, anchor->GetContentsView()));
+  views::Widget* bubble2 = BubbleWithButton::CreateBubble(
+      new BubbleWithButton(button2, anchor->GetContentsView()));
+
+  // Show |bubble1| first and then |bubble2|, such that |bubble2| is the active
+  // window.
+  bubble1->Show();
+  bubble2->Show();
+
+  // Check whether the buttons are assigned to the correct widgets.
+  EXPECT_EQ(bubble1, button1->GetWidget());
+  EXPECT_EQ(bubble2, button2->GetWidget());
+
+  // Since the buttons are in different windows, they should have different
+  // focus managers.
+  views::FocusManager* manager1 = button1->GetFocusManager();
+  views::FocusManager* manager2 = button2->GetFocusManager();
+  EXPECT_NE(manager1, manager2);
+
+  // Hover over |button1|. It shouldn't affect the focus.
+  button1->SetState(views::Button::ButtonState::STATE_HOVERED);
+  EXPECT_NE(manager1->GetFocusedView(), button1);
+
+  // |button2| also shouldn't be focused at this point.
+  EXPECT_NE(manager2->GetFocusedView(), button2);
+
+  // Hover over |button2| such that it gets the focus.
+  button2->SetState(views::Button::ButtonState::STATE_HOVERED);
+  EXPECT_EQ(manager2->GetFocusedView(), button2);
+
+  // Close all showing widgets.
+  bubble1->Close();
+  bubble2->Close();
+  anchor->Close();
 }
