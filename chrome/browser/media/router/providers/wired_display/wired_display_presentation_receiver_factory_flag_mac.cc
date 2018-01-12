526b22f1485f37915ffb407a6504b157828b43f8
Brandon Tolsch <btolsch@chromium.org>
2018-01-12 12:28:06 -0800

    [Presentation API] Add a flag to handle local screens for non-Views Mac
    The existing WiredDisplayPresentationReceiver implementation works on
    Mac, but only with mac_views_browser=1.  This is expected to be enabled
    by default soon enough that we aren't pursuing a separate Cocoa
    implementation.
    
    This change adds a flag to WiredDisplayPresentationReceiverFactory so
    that it can still be linked on Mac, in case mac_views_browser is
    enabled, but tells WiredDisplayMediaRouteProvider not to call it when
    running under Cocoa.  This also allows more
    WiredDisplayMediaRouteProvider test paths to be enabled for Mac.
    
    Bug: 777654
    Change-Id: Ia9a3406c6865165023c10da4588988efe072d9d1

---
+++ chrome/browser/media/router/providers/wired_display/wired_display_presentation_receiver_factory_flag_mac.cc
@@ -0,0 +1,13 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/media/router/providers/wired_display/wired_display_presentation_receiver_factory.h"
+
+namespace media_router {
+
+// static
+const bool WiredDisplayPresentationReceiverFactory::
+    kWiredDisplayPresentationReceiverAvailable = false;
+
+}  // namespace media_router
