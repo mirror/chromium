d32cfe36623e2e4991b6bb884ac118d61ff82187
Takashi Toyoshima <toyoshim@chromium.org>
2018-01-12 18:22:52 +0900

    OOR-CORS: Implements network service version of CheckAccess()
    This patch implements an equivalent of blink::WebCORS::CheckAccess() in
    network service.
    
    Bug: 736308
    Change-Id: If8acc31a74f9680518ea319a4e25c0843bc9521d

---
+++ services/network/public/cpp/cors/cors.h
@@ -0,0 +1,40 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_NETWORK_PUBLIC_CPP_CORS_CORS_H_
+#define SERVICES_NETWORK_PUBLIC_CPP_CORS_CORS_H_
+
+#include <string>
+
+#include "base/optional.h"
+#include "services/network/public/interfaces/cors.mojom-shared.h"
+#include "services/network/public/interfaces/fetch_api.mojom-shared.h"
+
+class GURL;
+namespace url {
+class Origin;
+}  // namespace url
+
+namespace network {
+
+namespace cors {
+
+// Perform a CORS access check on the response parameters.
+//
+// Use |GetErrorString()| to construct a user-friendly error message.
+// TODO(toyoshim): Implement network::cors::GetErrorString().
+base::Optional<mojom::CORSError> CheckAccess(
+    const GURL& response_url,
+    const int response_status_code,
+    const base::Optional<std::string>& allow_origin_header,
+    const base::Optional<std::string>& allow_suborigin_header,
+    const base::Optional<std::string>& allow_credentials_header,
+    network::mojom::FetchCredentialsMode credentials_mode,
+    const url::Origin& origin);
+
+}  // namespace cors
+
+}  // namespace network
+
+#endif  // SERVICES_NETWORK_PUBLIC_CPP_CORS_CORS_H_
