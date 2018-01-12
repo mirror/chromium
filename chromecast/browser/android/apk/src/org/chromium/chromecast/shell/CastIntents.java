848967293f8d9e0115ff9262f8461c18bbdbbf34
Zhiheng Li <vincentli@google.com>
2018-01-11 18:55:08 -0800

    Convert cast activity to fragment:  - Add CastWebContentsFragment to allow external activity to run cast  receiver app in its own app.  - Refactor CastWebContentsActivity to share codes with fragment class  - Add a build flag to config CastWebContentsFragment or  CastWebContentsActivity
     BUG: 65100261,65565116
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I0146360c22d3487ffb3971fe04e344aabfcf2372
    Bug:

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastIntents.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastIntents.java
@@ -13,4 +13,23 @@ public class CastIntents {
             "com.google.android.apps.castshell.intent.action.STOP_ACTIVITY";
     public static final String ACTION_SCREEN_OFF =
             "com.google.android.apps.castshell.intent.action.ACTION_SCREEN_OFF";
+
+    /**
+     * Action type of intent from CastWebContentsComponent to activity to show web contents in a
+     * CastWebContentsFragment. (To start CastWebContentsActivity use Context.startActivity())
+     */
+    public static final String ACTION_SHOW_WEB_CONTENT = "com.google.assistant.SHOW_WEB_CONTENT";
+
+    /**
+     * Action type of intent from CastWebContentsComponent to mInternalStopReceiver to detach
+     * WebContents and then file an intent to CastWebContentsFragment's host activity to stop
+     * fragment.
+     */
+    public static final String ACTION_STOP_WEB_CONTENT_INTERNAL =
+            "com.google.assistant.STOP_WEB_CONTENT_INTERNAL";
+
+    /**
+     * Action type of intent to host activity of CastWebContentsFragment to stop fragment.
+     */
+    public static final String ACTION_STOP_WEB_CONTENT = "com.google.assistant.STOP_WEB_CONTENT";
 }
