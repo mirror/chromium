d2edb68657cb3a78f5bb7f89752abdda4d87abea
Jennifer Apacible <apacible@chromium.org>
2018-01-11 16:33:09 -0800

    Rebase
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Id0c979b8cadade067eb3559e904463056ff02ba1

--- chrome/browser/ui/overlay/overlay_surface_embedder.cc
+++ chrome/browser/ui/overlay/overlay_surface_embedder.cc
@@ -9,13 +9,17 @@
 
 OverlaySurfaceEmbedder::OverlaySurfaceEmbedder(OverlayWindow* window)
     : window_(window) {
+  DCHECK(window_);
+
   surface_layer_ = base::MakeUnique<ui::Layer>(ui::LAYER_TEXTURED);
   surface_layer_->SetMasksToBounds(true);
 
   // The frame provided by the parent window's layer needs to show through
   // the surface layer.
   surface_layer_->SetFillsBoundsOpaquely(false);
+  surface_layer_->SetBounds(gfx::Rect(gfx::Point(0, 0), gfx::Size(500, 300)));
   window_->GetLayer()->Add(surface_layer_.get());
+  window_->GetLayer()->StackAtTop(surface_layer_.get());
   ref_factory_ = new viz::StubSurfaceReferenceFactory();
 }
 
