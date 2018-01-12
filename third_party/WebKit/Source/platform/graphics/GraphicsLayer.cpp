73c50cb6676e43b4f2c88dd565bf9d5aa6af2dc9
Xianzhu Wang <wangxianzhu@chromium.org>
2018-01-12 15:02:36 -0800

    [SPv175+] Fix devtools layout tests
    - Use real visual rect when converting chunks into cc layers;
    - Send paint invalidation timeline event for both SPv1 and SPv175
      by moving the event from
      ObjectPaintInvalidator::InvalidatePaintUsingContainer() (which is
      spv1 only) into InvalidateDisplayItemClient() (which is common for
      spv1 and spv175). This changes "paint invalidation" event to really
      mean it.
    
    Bug: 771643
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I1585b3c197639cf9a1f625ab32e9d34747f80105

--- third_party/WebKit/Source/platform/graphics/GraphicsLayer.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsLayer.cpp
@@ -135,7 +135,12 @@ GraphicsLayer::~GraphicsLayer() {
 
 LayoutRect GraphicsLayer::VisualRect() const {
   LayoutRect bounds = LayoutRect(FloatPoint(), Size());
-  bounds.Move(OffsetFromLayoutObjectWithSubpixelAccumulation());
+  if (RuntimeEnabledFeatures::SlimmingPaintV175Enabled()) {
+    DCHECK(layer_state_);
+    bounds.MoveBy(layer_state_->offset);
+  } else {
+    bounds.Move(OffsetFromLayoutObjectWithSubpixelAccumulation());
+  }
   return bounds;
 }
 
@@ -565,6 +570,9 @@ RasterInvalidationTracking* GraphicsLayer::GetRasterInvalidationTracking()
 void GraphicsLayer::TrackRasterInvalidation(const DisplayItemClient& client,
                                             const IntRect& rect,
                                             PaintInvalidationReason reason) {
+  if (FirstPaintInvalidationTracking::IsEnabled())
+    debug_info_.AppendAnnotatedInvalidateRect(rect, reason);
+
   if (RuntimeEnabledFeatures::PaintUnderInvalidationCheckingEnabled())
     EnsureRasterInvalidator().EnsureTracking();
 
@@ -1143,12 +1151,8 @@ void GraphicsLayer::SetNeedsDisplayInRect(
   if (!DrawsContent())
     return;
 
-  if (!ScopedSetNeedsDisplayInRectForTrackingOnly::s_enabled_) {
+  if (!ScopedSetNeedsDisplayInRectForTrackingOnly::s_enabled_)
     SetNeedsDisplayInRectInternal(rect);
-    // TODO(wangxianzhu): Need equivalence for SPv175/SPv2.
-    if (FirstPaintInvalidationTracking::IsEnabled())
-      debug_info_.AppendAnnotatedInvalidateRect(rect, invalidation_reason);
-  }
 
   TrackRasterInvalidation(client, rect, invalidation_reason);
 }
