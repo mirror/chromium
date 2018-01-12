445495c5afd204f8697fb8471901037c3c27aebd
arthursonzogni <arthursonzogni@chromium.org>
2018-01-12 12:00:30 +0100

    CSP: Fix blocked form submission with target="_blank"
    This fixes bug 798698. It happens when the "form-action 'self'" CSP is
    used and a form submission with the attribute "target='_blank'" happens.
    Since the form-action CSP is checked against the CSP of the new windows
    instead of the one of the old window, it was blocked.
    
    This regression probably started after this CL (which is good):
    https://chromium-review.googlesource.com/c/chromium/src/+/765969.
    
    Solution was to disable browser side checks and only rely on renderer
    side checks for the initial load.
    
    Bug: 798698
    Change-Id: Iade17b80f493af265ddb86fe95305d96c7ce0975

--- content/browser/frame_host/form_submission_throttle.h
+++ content/browser/frame_host/form_submission_throttle.h
@@ -18,6 +18,10 @@ class NavigationHandle;
 // directive, blocking requests which violate them.
 // It is enforcing it only if PlzNavigate is enabled. Blink is still enforcing
 // the 'form-action' directive on the renderer process.
+// TODO(arthursonzogni): https://crbug.com/663512: Depending on specification
+// clarification, we might be able to delete FormSubmissionThrottle altogether.
+// It will be deleted if the final specification clarifies that form-action
+// should NOT be enforced on redirects.
 class CONTENT_EXPORT FormSubmissionThrottle : public NavigationThrottle {
  public:
   static std::unique_ptr<NavigationThrottle> MaybeCreateThrottleFor(
