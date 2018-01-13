5027e02f8903b5179c6ec3cf77060b6cf741ff2a
Allen Vicencio <allenvic@chromium.org>
2018-01-12 16:29:36 -0800

    Implement ReadFile for SmbFileSystem
    Implements ReadFile for SmbFileSystem. This will allow files to be read
    from the files app.
    
    Bug: chromium:757625
    Change-Id: I3bae2b3c631ef78941a3f403f31c45c98afdf33a

--- chrome/browser/chromeos/smb_client/smb_file_system.h
+++ chrome/browser/chromeos/smb_client/smb_file_system.h
@@ -190,6 +190,12 @@ class SmbFileSystem : public file_system_provider::ProvidedFileSystemInterface {
       const storage::AsyncFileUtil::StatusCallback& callback,
       smbprovider::ErrorType error) const;
 
+  void HandleRequestReadFileCallback(int32_t length,
+                                     scoped_refptr<net::IOBuffer> buffer,
+                                     const ReadChunkReceivedCallback& callback,
+                                     smbprovider::ErrorType error,
+                                     const base::ScopedFD& fd) const;
+
   int32_t GetMountId() const;
 
   SmbProviderClient* GetSmbProviderClient() const;
