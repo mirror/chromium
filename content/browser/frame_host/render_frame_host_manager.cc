1b83178b3145478a8865e3824f201258560af3ae
Lukasz Anforowicz <lukasza@chromium.org>
2018-01-15 10:49:33 -0800

    [reland] Improve granularity of window namespaces in Blink.
    This is a reland of r521917 that got reverted in r523609 because of
    https://crbug.com/794079.
    
    This CL ensures that blink::FrameTree::Find(const AtomicString& name)
    only looks for a match in the current set of related browsing contexts
    (represented in the browser by content::BrowsingInstance).  This CL
    means that window.open's behavior won't change just because a renderer
    process happens to host multiple unrelated browsing contexts (possible
    for example after reusing a renderer process because of hitting the
    process limit).
    
    This CL consists of 3 parts:
    
    - New browser tests.
      - RenderFrameHostManagerTest.ProcessReuseVsBrowsingInstance
        for verifying browsing instance boundaries when the
        renderer processes get reused.
      - ExtensionFunctionalTest.FindingUnrelatedExtensionFramesFromAboutBlank
        for verifying that extensions can still lookup unrelated frames
        from the same extension.
    
    - Having blink::Page maintain a set of related pages.
      - Page::next_related_page_ and Page::prev_related_page_ form a
        circular, double-linked list of related pages.
      - Page::CreateOrdinary takes a new |opener| parameter and treats the
        new page and the |opener| as related and puts them on the same list.
      - |opener| is propagated from content::RenderViewImpl, through
        blink::WebViewImpl into blink::Page.
    
    - Falling back to blink embedder when blink::FrameTree::Find finds no frame
      with the given name.
      - The fallback is needed to preserve the old behavior for extensions.
      - The fallback goes through blink::LocalFrameClient,
        blink::WebFrameClient / content::RenderFrameImpl,
        content::ContentRendererClient / ::ChromeContentRendererClient,
        ::ChromeExtensionsRendererClient and finally is implemented
        by extensions::ExtensionFrameHelper.
      - Currently the fallback iterates through all same-origin frames in
        the given process, but requires that the |relative_to_frame| is an
        extension frame.  In the future we might want to restrict piercing
        of browsing instances to specific scenarios where it is needed
        (e.g. restrict it to background pages / contents only?).
    
    I've tested this CL via:
    (all tests below pass before and after the CL, except for
    ProcessReuseVsBrowsingInstance which is fixed by this CL)
    
    - New tests:
        - RenderFrameHostManagerTest.ProcessReuseVsBrowsingInstance
          (web -> web shouldn't violate browsing instance)
        - ExtensionFunctionalTest.FindingUnrelatedExtensionFramesFromAboutBlank
          (extension/about:blank -> extension can violate browsing instance)
    
    - Existing tests:
        - ExtensionApiTest.WindowsCreate_WithOpener and _NoOpener
          (chrome.windows.create stays in the same browsing instance depending
           on the setSelfAsOpener parameter)
        - AppBackgroundPageApiTest.Basic
          (hosted app -> background page can violate browsing instance;
           tests handling of mapping of web urls [full url, not just origin]
           to extensions)
    
    - Manual testing:
      - Hangouts Chrome *extension* continues to work (sufficient to
        validate that sign-in works).  Tested with version 2017.1019.418.1.
    
    Bug: 718489
    Test: See above.
    Change-Id: I6a10b2c222a77d454195af1eb9f204f7e22e00df
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Tbr: tommycli@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/764487
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#521917}

--- content/browser/frame_host/render_frame_host_manager.cc
+++ content/browser/frame_host/render_frame_host_manager.cc
@@ -1928,21 +1928,19 @@ int RenderFrameHostManager::CreateRenderFrameProxy(SiteInstance* instance) {
   CHECK(instance);
   CHECK_NE(instance, render_frame_host_->GetSiteInstance());
 
-  RenderViewHostImpl* render_view_host = nullptr;
+  RenderFrameProxyHost* proxy = GetRenderFrameProxyHost(instance);
+  if (proxy && proxy->is_render_frame_proxy_live())
+    return proxy->GetRoutingID();
 
   // Ensure a RenderViewHost exists for |instance|, as it creates the page
   // level structure in Blink.
-  render_view_host =
+  RenderViewHostImpl* render_view_host =
       frame_tree_node_->frame_tree()->GetRenderViewHost(instance);
   if (!render_view_host) {
     CHECK(frame_tree_node_->IsMainFrame());
     render_view_host = frame_tree_node_->frame_tree()->CreateRenderViewHost(
         instance, MSG_ROUTING_NONE, MSG_ROUTING_NONE, true, true);
   }
-
-  RenderFrameProxyHost* proxy = GetRenderFrameProxyHost(instance);
-  if (proxy && proxy->is_render_frame_proxy_live())
-    return proxy->GetRoutingID();
 
   if (!proxy)
     proxy = CreateRenderFrameProxyHost(instance, render_view_host);
