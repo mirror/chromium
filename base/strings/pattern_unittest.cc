c172830c58785d883f52285901f7ebe5cc0db0d1
Bernhard Bauer <bauerb@chromium.org>
2018-01-15 14:54:56 +0000

    Avoid recursion in base::MatchPattern().
    The recursion could lead to exponential run time, which in the previous
    implementation was avoided only by restricting the maximum recursion depth.
    
    The idea behind the new implementation is based on
    https://research.swtch.com/glob, specifically the fact that once a subpattern
    (the sequence of characters between two runs of wildcards) has been matched, the
    algorithm never needs to backtrack to before it anymore.
    
    One notable difference in the syntax of base::MatchPattern() to "standard" globs
    is that the '?' character matches 0 or 1 characters here, whereas the standard
    glob '?' matches exactly 1 character. This means that consecutive runs of
    wildcards define a maximum distance between exact subpatterns to match (which is
    infinite if the run contains at least one '*').
    
    Bug: 52839
    Change-Id: I2401ca6e2719ea31b4afbe6e65b6b0d76f7f3da9

--- base/strings/pattern_unittest.cc
+++ base/strings/pattern_unittest.cc
@@ -22,8 +22,7 @@ TEST(StringUtilTest, MatchPatternTest) {
   EXPECT_TRUE(MatchPattern("", ""));
   EXPECT_FALSE(MatchPattern("Hello", ""));
   EXPECT_TRUE(MatchPattern("Hello*", "Hello*"));
-  // Stop after a certain recursion depth.
-  EXPECT_FALSE(MatchPattern("123456789012345678", "?????????????????*"));
+  EXPECT_TRUE(MatchPattern("123456789012345678", "?????????????????*"));
 
   // Test UTF8 matching.
   EXPECT_TRUE(MatchPattern("heart: \xe2\x99\xa0", "*\xe2\x99\xa0"));
@@ -41,10 +40,15 @@ TEST(StringUtilTest, MatchPatternTest) {
                            UTF8ToUTF16("He??o\\*1*")));
 
   // This test verifies that consecutive wild cards are collapsed into 1
-  // wildcard (when this doesn't occur, MatchPattern reaches it's maximum
-  // recursion depth).
-  EXPECT_TRUE(MatchPattern(UTF8ToUTF16("Hello"),
-                           UTF8ToUTF16("He********************************o")));
+  // wildcard.
+  EXPECT_TRUE(MatchPattern("Hello", "He********************************o"));
+
+  EXPECT_TRUE(MatchPattern("aaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*a*b"));
+
+  EXPECT_TRUE(MatchPattern("abcd", "*?????"));
+  EXPECT_FALSE(MatchPattern("abcd", "???"));
+
+  EXPECT_TRUE(MatchPattern("abcb", "a*b"));
 }
 
 }  // namespace base
