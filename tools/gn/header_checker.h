873cfec8bd29ba4cee8b4637e76a16718ec32a07
Marcin Simonides <msimonides@opera.com>
2018-01-15 09:37:47 +0100

    Add support for include_dirs to gn check.
    With this change "gn check" no longer assumes all #include paths are
    relative to the source root. Instead it searches for the includes by
    checking each of the directories listed in include_dirs for the target.
    
    A number of new issues has been discovered. The trivial ones are fixed,
    the larger and more complex ones have been reported and the related
    parts of the project have been excluded from the default gn check run.
    
    BUG=794926
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I39c17a7ac47dc024dd374c8891ce4911809195cb

--- tools/gn/header_checker.h
+++ tools/gn/header_checker.h
@@ -16,6 +16,7 @@
 #include "base/strings/string_piece.h"
 #include "base/synchronization/lock.h"
 #include "tools/gn/err.h"
+#include "tools/gn/source_dir.h"
 
 class BuildSettings;
 class InputFile;
@@ -24,6 +25,7 @@ class SourceFile;
 class Target;
 
 namespace base {
+class FilePath;
 class MessageLoop;
 }
 
@@ -67,6 +69,9 @@ class HeaderChecker : public base::RefCountedThreadSafe<HeaderChecker> {
   FRIEND_TEST_ALL_PREFIXES(HeaderCheckerTest, CheckInclude);
   FRIEND_TEST_ALL_PREFIXES(HeaderCheckerTest, PublicFirst);
   FRIEND_TEST_ALL_PREFIXES(HeaderCheckerTest, CheckIncludeAllowCircular);
+  FRIEND_TEST_ALL_PREFIXES(HeaderCheckerTest, SourceFileForInclude);
+  FRIEND_TEST_ALL_PREFIXES(HeaderCheckerTest,
+                           SourceFileForInclude_FileNotFound);
   ~HeaderChecker();
 
   struct TargetInfo {
@@ -88,6 +93,8 @@ class HeaderChecker : public base::RefCountedThreadSafe<HeaderChecker> {
 
   typedef std::vector<TargetInfo> TargetVector;
   typedef std::map<SourceFile, TargetVector> FileMap;
+  typedef base::RepeatingCallback<bool(const base::FilePath& path)>
+      PathExistsCallback;
 
   // Backend for Run() that takes the list of files to check. The errors_ list
   // will be populate on failure.
@@ -102,7 +109,11 @@ class HeaderChecker : public base::RefCountedThreadSafe<HeaderChecker> {
   bool IsFileInOuputDir(const SourceFile& file) const;
 
   // Resolves the contents of an include to a SourceFile.
-  SourceFile SourceFileForInclude(const base::StringPiece& input) const;
+  SourceFile SourceFileForInclude(const base::StringPiece& relative_file_path,
+                                  const std::vector<SourceDir>& include_dirs,
+                                  const InputFile& source_file,
+                                  const LocationRange& range,
+                                  Err* err) const;
 
   // from_target is the target the file was defined from. It will be used in
   // error messages.
