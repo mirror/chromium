d7e5f3bb4e20640c48b9d14646d653399e957819
Ehsan Karamad <ekaramad@chromium.org>
2018-01-15 14:06:21 -0500

    Introduce ScrollCustomization Property
    This CL introduces the CSS property, 'scroll-customization'. Through
    modifying scroll-customization, unnecessary calls to apply/distributeScroll
    handlers can be avoided.
    
    This CL implements the logic inside blink's main thread. If an scrollable
    node has customized scrolling handlers, it can further specify which class
    of gestures it is interested in for custom scrolling.
    
    The list of valid values for customization includes 'auto', 'pan-x',
    'pan-left', 'pan-right', 'pan-y', 'pan-up', and 'pan-down', and 'none'.
    The default value is 'none' (any node interested in customized scroll
    handlers should specify that).
    
    For RootScroller we ignore customized scroll as it will regress certain
    features which depend on global RootScroller.
    
    This CL will eventually be followed up by changes to the compositor layer
    so that for nodes with custom scroll, the scroll events corresponding
    not allowed through scroll customization do not get sent to the main thread.
    
    Bug: 765326
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Ia54971b5535cb0b841682d242ef90761975f8c81

--- third_party/WebKit/Source/core/css/ComputedStyleCSSValueMapping.cpp
+++ third_party/WebKit/Source/core/css/ComputedStyleCSSValueMapping.cpp
@@ -963,6 +963,38 @@ static CSSValue* TouchActionFlagsToCSSValue(TouchAction touch_action) {
   return list;
 }
 
+static CSSValue* ScrollCustomizationFlagsToCSSValue(
+    ScrollCustomizationEnabledDirection scroll_customization) {
+  CSSValueList* list = CSSValueList::CreateSpaceSeparated();
+  if (scroll_customization == ScrollCustomizationEnabledDirection::kAuto) {
+    list->Append(*CSSIdentifierValue::Create(CSSValueAuto));
+  } else if (scroll_customization ==
+             ScrollCustomizationEnabledDirection::kNone) {
+    list->Append(*CSSIdentifierValue::Create(CSSValueNone));
+  } else {
+    if ((scroll_customization & ScrollCustomizationEnabledDirection::kPanX) ==
+        ScrollCustomizationEnabledDirection::kPanX)
+      list->Append(*CSSIdentifierValue::Create(CSSValuePanX));
+    else if (scroll_customization &
+             ScrollCustomizationEnabledDirection::kPanLeft)
+      list->Append(*CSSIdentifierValue::Create(CSSValuePanLeft));
+    else if (scroll_customization &
+             ScrollCustomizationEnabledDirection::kPanRight)
+      list->Append(*CSSIdentifierValue::Create(CSSValuePanRight));
+    if ((scroll_customization & ScrollCustomizationEnabledDirection::kPanY) ==
+        ScrollCustomizationEnabledDirection::kPanY)
+      list->Append(*CSSIdentifierValue::Create(CSSValuePanY));
+    else if (scroll_customization & ScrollCustomizationEnabledDirection::kPanUp)
+      list->Append(*CSSIdentifierValue::Create(CSSValuePanUp));
+    else if (scroll_customization &
+             ScrollCustomizationEnabledDirection::kPanDown)
+      list->Append(*CSSIdentifierValue::Create(CSSValuePanDown));
+  }
+
+  DCHECK(list->length());
+  return list;
+}
+
 static CSSValue* ValueForWillChange(
     const Vector<CSSPropertyID>& will_change_properties,
     bool will_change_contents,
@@ -2321,6 +2353,8 @@ const CSSValue* ComputedStyleCSSValueMapping::Get(
       return CSSIdentifierValue::Create(CSSValueNone);
     case CSSPropertyRight:
       return ValueForPositionOffset(style, resolved_property, layout_object);
+    case CSSPropertyScrollCustomization:
+      return ScrollCustomizationFlagsToCSSValue(style.ScrollCustomization());
     case CSSPropertyTextDecoration:
       return ValuesForShorthandProperty(textDecorationShorthand(), style,
                                         layout_object, styled_node,
