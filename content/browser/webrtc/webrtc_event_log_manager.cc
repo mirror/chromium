6ca7ad6e31b1fc078cd65dfebf0bfbe84dff1463
Elad Alon <eladalon@chromium.org>
2018-01-15 12:51:41 +0100

    Add unit tests in WebRtcEventLogManager
    Bug:
    Change-Id: Ib9ef3001a9979ff07a428b50872c4c5774f03f27

--- content/browser/webrtc/webrtc_event_log_manager.cc
+++ content/browser/webrtc/webrtc_event_log_manager.cc
@@ -11,6 +11,31 @@
 
 namespace content {
 
+namespace {
+class PeerConnectionTrackerProxyImpl : public PeerConnectionTrackerProxy {
+ public:
+  ~PeerConnectionTrackerProxyImpl() override = default;
+
+  void StartEventLogOutput(WebRtcEventLogPeerConnectionKey pc_key) override {
+    RenderProcessHost* host =
+        RenderProcessHost::FromID(pc_key.render_process_id);
+    if (!host) {
+      return;  // The host has been asynchronously removed; not a problem.
+    }
+    host->Send(new PeerConnectionTracker_StartEventLogOutput(pc_key.lid));
+  }
+
+  void StopEventLogOutput(WebRtcEventLogPeerConnectionKey pc_key) override {
+    RenderProcessHost* host =
+        RenderProcessHost::FromID(pc_key.render_process_id);
+    if (!host) {
+      return;  // The host has been asynchronously removed; not a problem.
+    }
+    host->Send(new PeerConnectionTracker_StopEventLog(pc_key.lid));
+  }
+};
+}  // namespace
+
 const size_t kWebRtcEventLogManagerUnlimitedFileSize = 0;
 
 WebRtcEventLogManager* WebRtcEventLogManager::g_webrtc_event_log_manager =
@@ -29,6 +54,7 @@ WebRtcEventLogManager* WebRtcEventLogManager::GetInstance() {
 WebRtcEventLogManager::WebRtcEventLogManager()
     : local_logs_observer_(nullptr),
       local_logs_manager_(this),
+      pc_tracker_proxy_(new PeerConnectionTrackerProxyImpl),
       task_runner_(base::CreateSequencedTaskRunnerWithTraits(
           {base::MayBlock(), base::TaskPriority::BACKGROUND,
            base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN})) {
@@ -245,19 +271,11 @@ void WebRtcEventLogManager::SetLocalLogsObserverInternal(
 void WebRtcEventLogManager::UpdateWebRtcEventLoggingState(
     PeerConnectionKey peer_connection,
     bool enabled) {
-  // TODO(eladalon): Add unit tests that would make sure that we really
-  // instruct WebRTC to start/stop event logs. https://crbug.com/775415
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  RenderProcessHost* host =
-      RenderProcessHost::FromID(peer_connection.render_process_id);
-  if (!host) {
-    return;  // The host has been asynchronously removed; not a problem.
-  }
   if (enabled) {
-    host->Send(
-        new PeerConnectionTracker_StartEventLogOutput(peer_connection.lid));
+    pc_tracker_proxy_->StartEventLogOutput(peer_connection);
   } else {
-    host->Send(new PeerConnectionTracker_StopEventLog(peer_connection.lid));
+    pc_tracker_proxy_->StopEventLogOutput(peer_connection);
   }
 }
 
@@ -265,6 +283,14 @@ void WebRtcEventLogManager::InjectClockForTesting(base::Clock* clock) {
   // Testing only; no need for threading guarantees (called before anything
   // could be put on the TQ).
   local_logs_manager_.InjectClockForTesting(clock);
+  // TODO: !!! Remote?
+}
+
+void WebRtcEventLogManager::InjectPeerConnectionTrackerProxy(
+    std::unique_ptr<PeerConnectionTrackerProxy> pc_tracker_proxy) {
+  // Testing only; no need for threading guarantees (called before anything
+  // could be put on the TQ).
+  pc_tracker_proxy_ = std::move(pc_tracker_proxy);
 }
 
 }  // namespace content
