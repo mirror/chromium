f52883d149d4e0273ab25eabeb2464fb8fd525a0
Gauthier Ambard <gambard@chromium.org>
2018-01-15 14:44:28 +0100

    Constrain named layout guide to toolbar button
    This CL adds a "guideName" property to the toolbar buttons.
    This property allows the buttons to the associated named layout guide
    to themselves when visible.
    The constraints to the layout guide are removed when the button is
    hidden. It allows the layout guide to be constrained to another button
    which would become visible when this one becomes hidden.
    
    Bug:802034
    Change-Id: Ie4d000aed8e61c50c4981d4290d134c0fd5976e4

--- ios/chrome/browser/ui/toolbar/clean/toolbar_button.mm
+++ ios/chrome/browser/ui/toolbar/clean/toolbar_button.mm
@@ -10,10 +10,18 @@
 #error "This file requires ARC support."
 #endif
 
+@interface ToolbarButton ()
+// Constraints for the named layout guide.
+@property(nonatomic, strong)
+    NSArray<NSLayoutConstraint*>* namedGuideConstraints;
+@end
+
 @implementation ToolbarButton
 @synthesize visibilityMask = _visibilityMask;
+@synthesize guideName = _guideName;
 @synthesize hiddenInCurrentSizeClass = _hiddenInCurrentSizeClass;
 @synthesize hiddenInCurrentState = _hiddenInCurrentState;
+@synthesize namedGuideConstraints = _namedGuideConstraints;
 
 + (instancetype)toolbarButtonWithImageForNormalState:(UIImage*)normalImage
                             imageForHighlightedState:(UIImage*)highlightedImage
@@ -40,6 +48,13 @@
     self.imageView.frame = AlignRectToPixel(self.imageView.frame);
     self.titleLabel.frame = self.bounds;
   }
+}
+
+#pragma mark - Property accessors
+
+- (void)setGuideName:(GuideName*)guideName {
+  _guideName = guideName;
+  self.namedGuideConstraints = nil;
 }
 
 #pragma mark - Public Methods
@@ -97,6 +112,20 @@
 // and hiddenInCurrentState properties, then updates its visibility accordingly.
 - (void)setHiddenForCurrentStateAndSizeClass {
   self.hidden = self.hiddenInCurrentState || self.hiddenInCurrentSizeClass;
+  if (self.guideName && !self.namedGuideConstraints) {
+    UILayoutGuide* guide = FindNamedGuide(self.guideName, self);
+    if (guide) {
+      self.namedGuideConstraints = @[
+        [guide.leadingAnchor constraintEqualToAnchor:self.leadingAnchor],
+        [guide.trailingAnchor constraintEqualToAnchor:self.trailingAnchor],
+        [guide.topAnchor constraintEqualToAnchor:self.topAnchor],
+        [guide.bottomAnchor constraintEqualToAnchor:self.bottomAnchor]
+      ];
+    }
+  }
+  for (NSLayoutConstraint* constraint in self.namedGuideConstraints) {
+    constraint.active = !self.hidden;
+  }
 }
 
 @end
