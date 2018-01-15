f0dcad909386cdce1d079e5b27c0ff422cd9f655
Xu Xing <xing.xu@intel.com>
2018-01-15 09:41:33 +0800

    [viz] Move DeleteResource and SynchronousFence into LayerTree and Display
    DeleteResource into LayerTree. SynchronousFence and ReadLockFenceHasPassed into Display.
    
    BUG=738190
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Icb174122c37122a56024a16ab3a89fc3d52d10e2

--- cc/resources/display_resource_provider.h
+++ cc/resources/display_resource_provider.h
@@ -8,6 +8,7 @@
 #include "build/build_config.h"
 #include "cc/output/overlay_candidate.h"
 #include "cc/resources/resource_provider.h"
+#include "components/viz/common/resources/resource_fence.h"
 
 namespace viz {
 class SharedBitmapManager;
@@ -165,6 +166,29 @@ class CC_EXPORT DisplayResourceProvider : public ResourceProvider {
     DISALLOW_COPY_AND_ASSIGN(ScopedBatchReturnResources);
   };
 
+  class CC_EXPORT SynchronousFence : public viz::ResourceFence {
+   public:
+    explicit SynchronousFence(gpu::gles2::GLES2Interface* gl);
+
+    // viz::ResourceFence implementation.
+    void Set() override;
+    bool HasPassed() override;
+    void Wait() override;
+
+    // Returns true if fence has been set but not yet synchornized.
+    bool has_synchronized() const { return has_synchronized_; }
+
+   private:
+    ~SynchronousFence() override;
+
+    void Synchronize();
+
+    gpu::gles2::GLES2Interface* gl_;
+    bool has_synchronized_;
+
+    DISALLOW_COPY_AND_ASSIGN(SynchronousFence);
+  };
+
   // Sets the current read fence. If a resource is locked for read
   // and has read fences enabled, the resource will not allow writes
   // until this fence has passed.
@@ -210,6 +234,10 @@ class CC_EXPORT DisplayResourceProvider : public ResourceProvider {
 
   const viz::internal::Resource* LockForRead(viz::ResourceId id);
   void UnlockForRead(viz::ResourceId id);
+  bool ReadLockFenceHasPassed(const viz::internal::Resource* resource) {
+    return !resource->read_lock_fence.get() ||
+           resource->read_lock_fence->HasPassed();
+  }
 
   struct Child {
     Child();
