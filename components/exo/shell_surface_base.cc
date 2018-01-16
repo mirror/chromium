42135dd1a8406816f0c77f4b771df9d2757051b4
Mitsuru Oshima <oshima@chromium.org>
2018-01-16 12:29:43 -0800

    Compositor/Server Side Drag to move/resize
    This CL implements a mode for a compositor to drive move/resize operation.
    
    * compositor notifies start and end of drag operation with
      direction parameter.
      - "none" direction means move, otherwise resize.
    * bounds updates are sent with reasons so that client can use
      proper bounds update scheme for each operation.
    * Extends the hit test slightly outside (equivalent to outset but
      can extend further for otuch event).
    
    This is backward compatible and will use the old mode for old clients.
    
    BUG=795119
    TEST=Tested with and without v10 protocol.
    
    Change-Id: Icacfe4aeb91fa20e456531ed908cfc46dd3c2ce6

--- components/exo/shell_surface_base.cc
+++ components/exo/shell_surface_base.cc
@@ -69,8 +69,11 @@ class CustomFrameView : public ash::CustomFrameViewAsh {
  public:
   using ShapeRects = std::vector<gfx::Rect>;
 
-  CustomFrameView(views::Widget* widget, bool enabled)
-      : CustomFrameViewAsh(widget) {
+  CustomFrameView(views::Widget* widget,
+                  bool enabled,
+                  bool client_controlled_move_resize)
+      : CustomFrameViewAsh(widget),
+        client_controlled_move_resize_(client_controlled_move_resize) {
     SetEnabled(enabled);
     if (!enabled)
       CustomFrameViewAsh::SetShouldPaintHeader(false);
@@ -121,7 +124,7 @@ class CustomFrameView : public ash::CustomFrameViewAsh {
     return client_bounds;
   }
   int NonClientHitTest(const gfx::Point& point) override {
-    if (enabled())
+    if (enabled() || !client_controlled_move_resize_)
       return ash::CustomFrameViewAsh::NonClientHitTest(point);
     return GetWidget()->client_view()->NonClientHitTest(point);
   }
@@ -147,6 +150,10 @@ class CustomFrameView : public ash::CustomFrameViewAsh {
   }
 
  private:
+  // TODO(oshima): Remove this once the transition to new drag/resize
+  // is complete. https://crbug.com/801666.
+  const bool client_controlled_move_resize_;
+
   DISALLOW_COPY_AND_ASSIGN(CustomFrameView);
 };
 
@@ -705,7 +712,9 @@ void ShellSurfaceBase::OnSurfaceDestroying(Surface* surface) {
 // views::WidgetDelegate overrides:
 
 bool ShellSurfaceBase::CanResize() const {
-  return !movement_disabled_;
+  // The surface base is resizable by default when min/max size is empty.
+  return !movement_disabled_ &&
+          (minimum_size_.IsEmpty() || minimum_size_ != maximum_size_);
 }
 
 bool ShellSurfaceBase::CanMaximize() const {
@@ -757,7 +766,8 @@ views::NonClientFrameView* ShellSurfaceBase::CreateNonClientFrameView(
   if (!frame_enabled_ && !window_state->HasDelegate()) {
     window_state->SetDelegate(std::make_unique<CustomWindowStateDelegate>());
   }
-  CustomFrameView* frame_view = new CustomFrameView(widget, frame_enabled_);
+  CustomFrameView* frame_view = new CustomFrameView(
+      widget, frame_enabled_, client_controlled_move_resize_);
   if (has_frame_colors_)
     frame_view->SetFrameColors(active_frame_color_, inactive_frame_color_);
   return frame_view;
@@ -794,6 +804,8 @@ gfx::Size ShellSurfaceBase::GetMinimumSize() const {
 }
 
 gfx::Size ShellSurfaceBase::GetMaximumSize() const {
+  // On ChromeOS, non empty maximum size will make the window
+  // non maximizable.
   return maximum_size_;
 }
 
@@ -1120,7 +1132,7 @@ void ShellSurfaceBase::UpdateWidgetBounds() {
   }
 
   // 2) When a window is being dragged.
-  if (IsResizing())
+  if (resizer_ && IsResizing())
     return;
 
   // Return early if there is pending configure requests.
