f9d1ed437d034a84794803aab89b78ce884e1e90
Becky Zhou <huayinz@chromium.org>
2018-01-16 14:29:48 -0800

    [Home] Fix NullPointerException on bottom sheet destroyed before native
    Bug: 801650
    Change-Id: I6d33ed9c35af0c4d6d0c8a336dee26dc184d53dc

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetContent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetContent.java
@@ -354,7 +354,6 @@ public class SuggestionsBottomSheetContent implements BottomSheet.BottomSheetCon
 
     @Override
     public void destroy() {
-        mBottomSheetObserver.onDestroy();
         mSheet.getNewTabController().removeObserver(this);
         mLocationBar.removeUrlFocusChangeListener(this);
         if (mAnimator != null) {
@@ -363,6 +362,7 @@ public class SuggestionsBottomSheetContent implements BottomSheet.BottomSheetCon
         }
 
         if (mSuggestionsInitialized) {
+            mBottomSheetObserver.onDestroy();
             mSuggestionsUiDelegate.onDestroy();
             mTileGroupDelegate.destroy();
             TemplateUrlService.getInstance().removeObserver(this);
