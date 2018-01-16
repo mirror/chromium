ac97be810a0b1641b9640f4f16bbf90a3d9f997d
François Beaufort <beaufort.francois@gmail.com>
2018-01-16 12:05:58 +0000

    [Picture In Picture] Add Web API
    Change-Id: I64d8e17c6975017565c850afeeca19d94e9f9ceb

---
+++ third_party/WebKit/Source/modules/picture_in_picture/HTMLVideoElementPictureInPicture.cpp
@@ -0,0 +1,24 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "modules/picture_in_picture/HTMLVideoElementPictureInPicture.h"
+
+#include "bindings/core/v8/ScriptPromise.h"
+#include "bindings/core/v8/ScriptPromiseResolver.h"
+#include "core/html/media/HTMLVideoElement.h"
+#include "public/platform/Platform.h"
+#include "public/platform/TaskType.h"
+
+namespace blink {
+
+// static
+ScriptPromise HTMLVideoElementPictureInPicture::requestPictureInPicture(
+    ScriptState* script_state,
+    HTMLVideoElement& element) {
+  ScriptPromiseResolver* resolver_ =
+      ScriptPromiseResolver::Create(script_state);
+  return resolver_->Promise();
+}
+
+}  // namespace blink
