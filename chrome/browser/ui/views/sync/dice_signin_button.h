df3a983b56104354d92e27b8d648d71bf1d18bc7
Mihai Sardarescu <msarda@chromium.org>
2018-01-16 17:55:31 +0100

    [Dice] Show a personalized sign-in promo in bookmark bubble.
    This CL adds a DICE sign-in button that is a either a plain text "Sign in"
    button if the user is signed out or a button containing the user avatar
    image and full name for the first account present in the Gaia cookies.
    
    It then uses this button to personalize the sync promo in the bookmark
    bubble. As it lives side-by-side with the pre-DICE sign-in promo, the
    delegate of the bubble sync promo was slightly changed to cater for the
    new case to enable sync with an account already available in Chrome.
    
    Design doc: see link from bug
    
    Screenshots:
    https://drive.google.com/file/d/1c-SDxk55XJhj-ov98HVcTgjswq6yCBEZ/view?usp=sharing
    https://drive.google.com/file/d/1Ss61VJv8GW2rGm4zVbMSejnYPpAhQ2xH/view?usp=sharing
    
    
    Bug: 802189
    Change-Id: I1011318cdbb7c396e84299f93d4d2766c0862e8b

---
+++ chrome/browser/ui/views/sync/dice_signin_button.h
@@ -0,0 +1,49 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_SYNC_DICE_SIGNIN_BUTTON_H_
+#define CHROME_BROWSER_UI_VIEWS_SYNC_DICE_SIGNIN_BUTTON_H_
+
+#include "base/macros.h"
+#include "base/optional.h"
+#include "components/signin/core/browser/account_info.h"
+#include "ui/views/controls/button/image_button.h"
+#include "ui/views/controls/button/md_text_button.h"
+#include "ui/views/controls/label.h"
+
+// Sign-in button used for Dice that presents a button containing the account
+// information (avatar image and email) that invites the user to sign in to
+// chrome.
+//
+// The button also presents on the right hand side a drown-down arrow button
+// that the user can interact with.
+class DiceSigninButton : public views::MdTextButton {
+ public:
+  // Create a sign-in button for the has a title.
+  // |button_listener| is called evey time the user interacts with this button.
+  explicit DiceSigninButton(views::ButtonListener* button_listener);
+
+  // Creates a sign-in button personalized with the data from |account|.
+  // |button_listener| will be called for events originating from |this| or from
+  // |drop_down_arrow|.
+  DiceSigninButton(const AccountInfo& account_info,
+                   views::ButtonListener* button_listener);
+  ~DiceSigninButton() override;
+
+  const views::Button* drop_down_arrow() const { return arrow_; }
+  const base::Optional<AccountInfo> account() const { return account_; }
+
+  // views::MdTextButton:
+  gfx::Rect GetChildAreaBounds() override;
+  void Layout() override;
+
+ private:
+  const base::Optional<AccountInfo> account_;
+  views::Label* subtitle_;
+  views::View* divider_;
+  views::ImageButton* arrow_;
+  DISALLOW_COPY_AND_ASSIGN(DiceSigninButton);
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_SYNC_DICE_SIGNIN_BUTTON_H_
