cb7d09e564e0a5fc978439c0b3b34bd7aa5ef48b
Ehsan Chiniforooshan <chiniforooshan@chromium.org>
2018-01-16 13:21:57 -0500

    Tracing: Add an Android tracing agent.
    The agent just writes clock sync markers to trace_marker when tracing
    started or stopped.
    
    Also, since most tracing agents only care about receiving start/stop
    tracing signals, a default implementation is provided for other
    methods to reduce boilerplate code.
    
    Bug: 797809
    Change-Id: I4645add6e7e81a25d0e923c8afcdf8f5dc3a7a13

---
+++ content/browser/tracing/android_tracing_agent.cc
@@ -0,0 +1,34 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/tracing/android_tracing_agent.h"
+
+#include "base/trace_event/trace_log.h"
+#include "services/service_manager/public/cpp/connector.h"
+
+namespace content {
+
+AndroidTracingAgent::AndroidTracingAgent(
+    service_manager::Connector* connector) {
+  // This agent does not send any trace event to the coordinator. It writes to
+  // trace_marker. So, there is no need to specify a label.
+  Register(connector, "" /* label */, tracing::mojom::TraceDataType::STRING,
+           true /* supports_explicit_clock_sync */);
+}
+
+AndroidTracingAgent::~AndroidTracingAgent() = default;
+
+void AndroidTracingAgent::StartTracing(
+    const std::string& config,
+    base::TimeTicks coordinator_time,
+    const Agent::StartTracingCallback& callback) {
+  base::trace_event::TraceLog::GetInstance()->AddClockSyncMetadataEvent();
+  callback.Run(true /* success */);
+}
+
+void AndroidTracingAgent::StopAndFlush(tracing::mojom::RecorderPtr recorder) {
+  base::trace_event::TraceLog::GetInstance()->AddClockSyncMetadataEvent();
+}
+
+}  // namespace content
