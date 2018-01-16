079d560267596bb47e34eb2d45e6a15a6223f1c8
Ryan Landay <rlanday@chromium.org>
2018-01-16 13:09:24 -0800

    Add layout test for ExtendSelectionAndDelete() JavaScript events
    I'm likely going to refactor InputMethodController::ExtendSelectionAndDelete()
    in https://chromium-review.googlesource.com/c/chromium/src/+/862521
    to use a new static helper method for deleting a selection and properly firing
    the beforeinput event. This CL adds a layout test to make sure we don't
    accidentally change the existing behavior of this method.
    
    It appears that we currently fire both a beforeinput and an input method, so
    this method seems to already fire the correct events.
    
    Change-Id: I9f1acb1e485cc6735799fca41e66599bbde6174a

--- content/shell/test_runner/text_input_controller.cc
+++ content/shell/test_runner/text_input_controller.cc
@@ -48,6 +48,7 @@ class TextInputControllerBindings
   void UnmarkText();
   void UnmarkAndUnselectText();
   void DoCommand(const std::string& text);
+  void ExtendSelectionAndDelete(int before, int after);
   void SetMarkedText(const std::string& text, int start, int length);
   void SetMarkedTextFromExistingText(int start, int length);
   bool HasMarkedText();
@@ -101,6 +102,8 @@ TextInputControllerBindings::GetObjectTemplateBuilder(v8::Isolate* isolate) {
       .SetMethod("unmarkAndUnselectText",
                  &TextInputControllerBindings::UnmarkAndUnselectText)
       .SetMethod("doCommand", &TextInputControllerBindings::DoCommand)
+      .SetMethod("extendSelectionAndDelete",
+                 &TextInputControllerBindings::ExtendSelectionAndDelete)
       .SetMethod("setMarkedText", &TextInputControllerBindings::SetMarkedText)
       .SetMethod("setMarkedTextFromExistingText",
                  &TextInputControllerBindings::SetMarkedTextFromExistingText)
@@ -132,6 +135,12 @@ void TextInputControllerBindings::UnmarkAndUnselectText() {
 void TextInputControllerBindings::DoCommand(const std::string& text) {
   if (controller_)
     controller_->DoCommand(text);
+}
+
+void TextInputControllerBindings::ExtendSelectionAndDelete(int before,
+                                                           int after) {
+  if (controller_)
+    controller_->ExtendSelectionAndDelete(before, after);
 }
 
 void TextInputControllerBindings::SetMarkedText(const std::string& text,
@@ -216,6 +225,16 @@ void TextInputController::DoCommand(const std::string& text) {
                                                      "is not a local frame.";
     view()->MainFrame()->ToWebLocalFrame()->ExecuteCommand(
         blink::WebString::FromUTF8(text));
+  }
+}
+
+void TextInputController::ExtendSelectionAndDelete(int before, int after) {
+  if (view()->MainFrame()) {
+    CHECK(view()->MainFrame()->ToWebLocalFrame()) << "This function cannot be "
+                                                     "called if the main frame "
+                                                     "is not a local frame.";
+    view()->MainFrame()->ToWebLocalFrame()->ExtendSelectionAndDelete(before,
+                                                                     after);
   }
 }
 
