0f6871330a734b313ef85e0a3ca9cdb712ccfff2
Mohamed Heikal <mheikal@google.com>
2018-01-16 14:32:58 -0500

    remove wait for librarary dependancies call
    Initial delete
    
    Change-Id: If6b3aa2aff389e0592a4ec36d11b75befa82dd91

--- content/public/android/java/src/org/chromium/content/app/ContentApplication.java
+++ content/public/android/java/src/org/chromium/content/app/ContentApplication.java
@@ -5,7 +5,6 @@
 package org.chromium.content.app;
 
 import org.chromium.base.BaseChromiumApplication;
-import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.MainDex;
 
 /**
@@ -14,21 +13,5 @@ import org.chromium.base.annotations.MainDex;
  */
 @MainDex
 public abstract class ContentApplication extends BaseChromiumApplication {
-    private boolean mLibraryDependenciesInitialized;
 
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        mLibraryDependenciesInitialized = true;
-    }
-
-    /**
-     * @return Whether the library dependencies have been initialized and it is safe to issue
-     *         requests to load the native library.
-     */
-    @VisibleForTesting
-    public boolean areLibraryDependenciesInitialized() {
-        return mLibraryDependenciesInitialized;
-    }
 }
--- content/public/test/android/javatests/src/org/chromium/content/browser/test/util/ApplicationUtils.java
+++ content/public/test/android/javatests/src/org/chromium/content/browser/test/util/ApplicationUtils.java
@@ -4,34 +4,9 @@
 
 package org.chromium.content.browser.test.util;
 
-import android.app.Instrumentation;
-import android.content.Context;
-
-import org.chromium.content.app.ContentApplication;
-
 /**
  * Test utilities for dealing with applications.
  */
 public class ApplicationUtils {
-
-    /**
-     * Wait for the library dependencies to be initialized for the application being
-     * instrumented.
-     *
-     * @param instrumentation The test instrumentation.
-     */
-    public static void waitForLibraryDependencies(final Instrumentation instrumentation) {
-        CriteriaHelper.pollUiThread(new Criteria() {
-            @Override
-            public boolean isSatisfied() {
-                Context context = instrumentation.getTargetContext();
-                if (context == null) return false;
-                if (context.getApplicationContext() == null) return false;
-                ContentApplication application =
-                        (ContentApplication) context.getApplicationContext();
-                return application.areLibraryDependenciesInitialized();
-            }
-        });
-    }
 
 }
