fdba2aeaeb473a887c3989768a7847f3372a3dfb
Friedrich Horschig <fhorschig@chromium.org>
2018-01-16 15:41:51 +0100

    Add feature-guarded search bar to Android password settings
    By enabling the feature PasswordSearch, a search icon will appear in the
    action bar in Chrome > Settings > Save Passwords.
    Clicking the icon will trigger a search box that has no function yet.
    
    Update: instead of adding a new white icon, the existing is recolored.
    
    Bug: 794108
    Change-Id: I9b4e3c7754bb5b0cc56e3156a746bcbf44aa5bd3

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeFeatureList.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeFeatureList.java
@@ -231,6 +231,7 @@ public abstract class ChromeFeatureList {
     public static final String NTP_SNIPPETS_INCREASED_VISIBILITY = "NTPSnippetsIncreasedVisibility";
     public static final String OMNIBOX_SPARE_RENDERER = "OmniboxSpareRenderer";
     public static final String PAY_WITH_GOOGLE_V1 = "PayWithGoogleV1";
+    public static final String PASSWORD_SEARCH = "PasswordSearch";
     public static final String PROGRESS_BAR_THROTTLE = "ProgressBarThrottle";
     public static final String PWA_PERSISTENT_NOTIFICATION = "PwaPersistentNotification";
     public static final String READER_MODE_IN_CCT = "ReaderModeInCCT";
