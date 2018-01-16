2e882f0eeb4e78cbe79666b3f860092e52b75c76
Mike Wasserman <msw@chromium.org>
2018-01-16 10:49:41 -0800

    mus: Fix gpu context lost errors and crashes on display removal
    Bug: 798538
    Change-Id: I064416cfc377bf19ec9afa214baa28a75846acf8

--- ash/display/window_tree_host_manager.cc
+++ ash/display/window_tree_host_manager.cc
@@ -583,7 +583,21 @@ void WindowTreeHostManager::DeleteHost(AshWindowTreeHost* host_to_delete) {
   controller->Shutdown();
   if (primary_tree_host_for_replace_ == host_to_delete)
     primary_tree_host_for_replace_ = nullptr;
-  base::ThreadTaskRunnerHandle::Get()->DeleteSoon(FROM_HERE, controller);
+
+  // TODO(msw): ReleaseAcceleratedWidget causes a crash entering unified mode: 
+  // [94772:94849:0116/103628.460743:FATAL:window_tree.cc(1511)] Check failed: display_root. 
+  // #0 0x7fda92627d2c base::debug::StackTrace::StackTrace()
+  // #1 0x7fda9264ebdc logging::LogMessage::~LogMessage()
+  // #2 0x7fda908726dd ui::ws::WindowTree::DispatchInputEventImpl()
+  // #3 0x7fda908779e8 ui::ws::WindowTree::OnWindowInputEventAck()
+  // #4 0x7fda8f677a00 ui::mojom::WindowTreeStubDispatch::Accept()
+
+  // Mus will destroy the platform display/window and its accelerated widget; 
+  // prevent the compositor from using the asynchronously destroyed surface. 
+  // host_to_delete->AsWindowTreeHost()->compositor()->SetVisible(false);
+  // host_to_delete->AsWindowTreeHost()->compositor()->ReleaseAcceleratedWidget();
+
+  base::ThreadTaskRunnerHandle::Get()->DeleteSoon(FROM_HERE, controller); 
 }
 
 void WindowTreeHostManager::OnDisplayRemoved(const display::Display& display) {
