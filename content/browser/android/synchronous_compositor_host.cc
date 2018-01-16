7db1476b2ff16e90fe02a7ac0cd04e6dc301a932
James Wallace-Lee <jamwalla@chromium.org>
2018-01-16 14:20:04 -0800

    WebView: Make draws asynchronous during fling
    This moves the work of OnComputeScroll into DidSendBeginFrame.
    Previously, draws after calls to OnComputeScroll were synchronous so that
    child views would scroll with the webview. This change makes draws after
    OnComputeScroll asynchronous and ticks fling animations during BeginFrame.
    
    Bug: 801759
    Change-Id: Ie818644117f6345db43d11b009b92add79c2dfb2

--- content/browser/android/synchronous_compositor_host.cc
+++ content/browser/android/synchronous_compositor_host.cc
@@ -58,7 +58,8 @@ SynchronousCompositorHost::SynchronousCompositorHost(
       renderer_param_version_(0u),
       need_animate_scroll_(false),
       need_invalidate_count_(0u),
-      did_activate_pending_tree_count_(0u) {
+      did_activate_pending_tree_count_(0u),
+      did_request_compute_scroll_(false) {
   client_->DidInitializeCompositor(this, process_id_, routing_id_);
 }
 
@@ -87,6 +88,7 @@ SynchronousCompositorHost::DemandDrawHwAsync(
     const gfx::Rect& viewport_rect_for_tile_priority,
     const gfx::Transform& transform_for_tile_priority) {
   scoped_refptr<FrameFuture> frame_future = new FrameFuture();
+
   if (compute_scroll_needs_synchronous_draw_) {
     compute_scroll_needs_synchronous_draw_ = false;
     auto frame_ptr = std::make_unique<Frame>();
@@ -354,6 +356,12 @@ void SynchronousCompositorHost::SynchronouslyZoomBy(float zoom_delta,
 
 void SynchronousCompositorHost::OnComputeScroll(
     base::TimeTicks animation_time) {
+  // Scroll is computed in DidSendBeginFrame after the first time this
+  // function is called.
+  if (did_request_compute_scroll_)
+    return;
+  did_request_compute_scroll_ = true;
+
   if (!need_animate_scroll_)
     return;
   need_animate_scroll_ = false;
@@ -372,8 +380,16 @@ void SynchronousCompositorHost::DidOverscroll(
 }
 
 void SynchronousCompositorHost::DidSendBeginFrame(
-    ui::WindowAndroid* window_android) {
-  compute_scroll_needs_synchronous_draw_ = false;
+    ui::WindowAndroid* window_android,
+    const viz::BeginFrameArgs& args) {
+  if (compute_scroll_needs_synchronous_draw_) {
+    compute_scroll_needs_synchronous_draw_ = false;
+  } else if (need_animate_scroll_ && did_request_compute_scroll_) {
+    need_animate_scroll_ = false;
+    sender_->Send(
+        new SyncCompositorMsg_ComputeScroll(routing_id_, args.frame_time));
+  }
+
   if (SynchronousCompositorBrowserFilter* filter = GetFilter())
     filter->SyncStateAfterVSync(window_android, this);
 }
