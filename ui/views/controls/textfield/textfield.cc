aaad332bb1d1c2214fc3fd2e9cee73c873619225
Alex Newcomer <newcomer@chromium.org>
2018-01-17 14:30:14 -0800

    cros: Use of placeholder text with harmony
    In CrOS, we don't always want to use harmony settings by default.
    
    This is a quick fix that makes textfield use the set placeholder
    text if it is set, otherwise we use the default or harmony
    aligned placeholder texts.
    
    TEST=manual
    
    Bug: 779717
    Change-Id: I85e5ab2aed2786e54086706e76ec50e7e0ac3c14

--- ui/views/controls/textfield/textfield.cc
+++ ui/views/controls/textfield/textfield.cc
@@ -2032,14 +2032,19 @@ void Textfield::PaintTextAndCursor(gfx::Canvas* canvas) {
     if (SkColorGetA(GetBackgroundColor()) != SK_AlphaOPAQUE)
       placeholder_text_draw_flags |= gfx::Canvas::NO_SUBPIXEL_RENDERING;
 
+    SkColor placeholder_text_color = placeholder_text_color_;
+
+#if !defined(OS_CHROMEOS)
+    if (ui::MaterialDesignController::IsSecondaryUiMaterial())
+      placeholder_text_color = SkColorSetA(GetTextColor(), 0x83);
+#endif
+
     canvas->DrawStringRectWithFlags(
         GetPlaceholderText(),
         placeholder_font_list_.has_value() ? placeholder_font_list_.value()
                                            : GetFontList(),
-        ui::MaterialDesignController::IsSecondaryUiMaterial()
-            ? SkColorSetA(GetTextColor(), 0x83)
-            : placeholder_text_color_,
-        render_text->display_rect(), placeholder_text_draw_flags);
+        placeholder_text_color, render_text->display_rect(),
+        placeholder_text_draw_flags);
   }
 
   render_text->Draw(canvas);
