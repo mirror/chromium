22ad4d60bdfcf7fd499fbf6179ba91471a624750
Ken Rockot <rockot@chromium.org>
2018-01-17 11:15:34 -0800

    Correct mojo::WrapSharedMemoryHandle usage
    Fixes some incorrect uses of mojo::WrapSharedMemoryHandle which
    were assuming that the call actually has any control over the memory
    protection applied to a handle when mapped.
    
    Where fixing usage is infeasible for this CL, TODOs are added to
    annotate follow-up work.
    
    Also updates the API and documentation to (hopefully) improve clarity
    and avoid similar mistakes from being made in the future.
    
    BUG=792900
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I0578aaa9ca3bfcb01aaf2451315d1ede95458477

--- content/renderer/media/video_capture_impl_unittest.cc
+++ content/renderer/media/video_capture_impl_unittest.cc
@@ -136,10 +136,10 @@ class VideoCaptureImplTest : public ::testing::Test {
   }
 
   void SimulateOnBufferCreated(int buffer_id, const base::SharedMemory& shm) {
-    auto handle = base::SharedMemory::DuplicateHandle(shm.handle());
     video_capture_impl_->OnBufferCreated(
-        buffer_id, mojo::WrapSharedMemoryHandle(handle, shm.mapped_size(),
-                                                true /* read_only */));
+        buffer_id, mojo::WrapSharedMemoryHandle(
+                       shm.GetReadOnlyHandle(), shm.mapped_size(),
+                       mojo::UnwrappedSharedMemoryHandleProtection::kReadOnly));
   }
 
   void SimulateBufferReceived(int buffer_id, const gfx::Size& size) {
