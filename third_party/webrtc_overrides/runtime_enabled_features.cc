776d320af70789a43cb67de52482def49e54e65d
Ilya Nikolaevskiy <ilnik@chromium.org>
2018-01-17 11:57:35 +0100

    Fix code style in webrtc_overrides
    Bug: webrtc:8287
    Change-Id: I557d227f63dcb06bcf30fba7f3e616926c21b846

--- third_party/webrtc_overrides/runtime_enabled_features.cc
+++ third_party/webrtc_overrides/runtime_enabled_features.cc
@@ -2,22 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/feature_list.h"
-#include "third_party/webrtc/system_wrappers/include/runtime_enabled_features.h"
-
 // Define webrtc::runtime_features::IsFeatureEnabled to provide webrtc with a
 // chromium runtime flags access.
+
+#include "base/feature_list.h"
+#include "third_party/webrtc/system_wrappers/include/runtime_enabled_features.h"
 
 namespace webrtc {
 
 namespace runtime_enabled_features {
 
-
 const base::Feature kWebRtcDualStreamMode{"WebRTC-DualStreamMode",
                                           base::FEATURE_DISABLED_BY_DEFAULT};
 
 bool IsFeatureEnabled(std::string feature_name) {
-  if (feature_name == "WebRtcDualStreamMode")
+  if (feature_name == kDualStreamModeFeatureName)
     return base::FeatureList::IsEnabled(kWebRtcDualStreamMode);
   return false;
 }
