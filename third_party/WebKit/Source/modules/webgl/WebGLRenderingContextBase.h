d74ec18d741cd299a1e202db276613d6079f1572
Michael Lippautz <mlippautz@chromium.org>
2018-01-17 10:35:16 +0100

    [bindings] Make TraceWrapperBase::TraceWrappers abstract
    Implement an empty TraceWrappers on SW that will serve as the base
    implementation.
    
    A future CL will make sure that all children properly dispatch to
    ScriptWrappable::TraceWrappers when needed, i.e., when they have their
    own implementation.
    
    This is prework for generlizing visiting of wrapper traced objects.
    
    Bug: chromium:802273
    Change-Id: I0957934360ba852e572c9432743bdf086fc38719

--- third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.h
+++ third_party/WebKit/Source/modules/webgl/WebGLRenderingContextBase.h
@@ -834,6 +834,7 @@ class MODULES_EXPORT WebGLRenderingContextBase : public CanvasRenderingContext,
     virtual WebGLExtension* GetExtensionObjectIfAlreadyEnabled() = 0;
 
     virtual void Trace(blink::Visitor* visitor) {}
+    void TraceWrappers(const ScriptWrappableVisitor*) const override {}
 
    private:
     bool draft_;
