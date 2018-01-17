ee39632c21e218165e6b6f153e441316f72ada41
David Tseng <dtseng@chromium.org>
2018-01-17 10:54:39 -0800

    Support dictation on Chrome OS (initial version/prototype)
    - bound to Search+Shift+S (placholder)
    - each invokation toggles between dictation start and stop
    - sound icon prompts for user to start dictating
    - silence triggers dictation completion and recognition result
    
    Test: manual.
    Bug: 546373
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ie9950dc6a21270998963222cf457fd218c40be89

--- chromeos/chromeos_switches.cc
+++ chromeos/chromeos_switches.cc
@@ -676,5 +676,10 @@ bool IsNetworkSettingsConfigEnabled() {
       chromeos::switches::kDisableNetworkSettingsConfig);
 }
 
+bool AreExperimentalAccessibilityFeaturesEnabled() {
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+      chromeos::switches::kEnableExperimentalAccessibilityFeatures);
+}
+
 }  // namespace switches
 }  // namespace chromeos
