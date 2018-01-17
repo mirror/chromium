4d4c94849a8ee89cb160f37de924b22bf6ecd1fe
Yutaka Hirano <yhirano@chromium.org>
2018-01-17 20:20:22 +0900

    Have URLLoader use ResourceScheduler
    With this CL, content::URLLoader consults ResourceScheduler when it
    starts loading.
    
    ResourceScheduler with URLLoader works as similar to that with
    ResourceDispatcherHost, but there are some differences.
    
     - A ResourceScheduler::Client is created for a
       NetworkURLLoaderFactory in contrast to a RenderView in the existing
       path.
     - We don't provide "deperecated" signals. Part of them are moved to
       the renderer (RendererSideResourceScheduler), and others are not
       used.
    
    Bug: 754918
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I9b8b4448682b05cf57874dfcf68b1c3fbee9c386

--- content/network/network_service_url_loader_factory.cc
+++ content/network/network_service_url_loader_factory.cc
@@ -7,6 +7,7 @@
 #include "base/logging.h"
 #include "content/network/network_context.h"
 #include "content/network/network_service_impl.h"
+#include "content/network/resource_scheduler_client.h"
 #include "content/network/url_loader.h"
 #include "services/network/public/cpp/resource_request.h"
 
@@ -14,12 +15,13 @@ namespace content {
 
 NetworkServiceURLLoaderFactory::NetworkServiceURLLoaderFactory(
     NetworkContext* context,
-    uint32_t process_id)
-    : context_(context), process_id_(process_id) {
-  ignore_result(process_id_);
-}
+    uint32_t process_id,
+    scoped_refptr<ResourceSchedulerClient> resource_scheduler_client)
+    : context_(context),
+      process_id_(process_id),
+      resource_scheduler_client_(std::move(resource_scheduler_client)) {}
 
-NetworkServiceURLLoaderFactory::~NetworkServiceURLLoaderFactory() = default;
+NetworkServiceURLLoaderFactory::~NetworkServiceURLLoaderFactory() {}
 
 void NetworkServiceURLLoaderFactory::CreateLoaderAndStart(
     mojom::URLLoaderRequest request,
@@ -40,12 +42,13 @@ void NetworkServiceURLLoaderFactory::CreateLoaderAndStart(
       context_, std::move(request), options, url_request, report_raw_headers,
       std::move(client),
       static_cast<net::NetworkTrafficAnnotationTag>(traffic_annotation),
-      process_id_);
+      process_id_, resource_scheduler_client_);
 }
 
 void NetworkServiceURLLoaderFactory::Clone(
     mojom::URLLoaderFactoryRequest request) {
-  context_->CreateURLLoaderFactory(std::move(request), process_id_);
+  context_->CreateURLLoaderFactory(std::move(request), process_id_,
+                                   resource_scheduler_client_);
 }
 
 }  // namespace content
