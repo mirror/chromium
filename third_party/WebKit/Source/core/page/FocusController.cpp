db9cbd959a9b8d1ddbc28e1dbc6cab432120b240
Hugo Holgersson <hugoh@vewd.com>
2018-01-17 09:21:16 +0100

    Snav: Search containers that reached their scroll limit
    Problem:
    A fully scrolled container might still have
    nearby focusables. Before this CL those were ignored.
    
    Solution:
    This adds a new helper IsContainer() that we can use to
    find ScrollableEnclosingBoxOrParentFrameOf().
    
    We define:
    A node's _container_ as its enclosing scrollable area
    or its document (possibly unscrollable).
    
    In other words, we don't need a direction to determine
    a node's container. This is simplification but it's also
    needed to fix bug 770103.
    
    Bug: 770147, 770103
    Change-Id: I439d532631e9d424e40e801873ef330cd15ffe7f

--- third_party/WebKit/Source/core/page/FocusController.cpp
+++ third_party/WebKit/Source/core/page/FocusController.cpp
@@ -1491,8 +1491,7 @@ bool FocusController::AdvanceFocusDirectionally(WebFocusType type) {
         starting_rect = VirtualRectForAreaElementAndDirection(*area, type);
       }
     }
-    container = ScrollableEnclosingBoxOrParentFrameForNodeInDirection(
-        type, focused_element);
+    container = ScrollableEnclosingBoxOrParentFrameOf(focused_element);
   }
 
   bool consumed = false;
@@ -1501,8 +1500,7 @@ bool FocusController::AdvanceFocusDirectionally(WebFocusType type) {
         AdvanceFocusDirectionallyInContainer(container, starting_rect, type);
     starting_rect =
         NodeRectInAbsoluteCoordinates(container, true /* ignore border */);
-    container =
-        ScrollableEnclosingBoxOrParentFrameForNodeInDirection(type, container);
+    container = ScrollableEnclosingBoxOrParentFrameOf(container);
     if (container && container->IsDocumentNode())
       ToDocument(container)->UpdateStyleAndLayoutIgnorePendingStylesheets();
   }
