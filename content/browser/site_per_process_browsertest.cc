19460c69906eaa84d73509ff5a63b003a1d08aa8
Gang Wu <gangwu@chromium.org>
2018-01-16 16:12:17 -0800

    disable the test
    Bug:
    Change-Id: I86b46466801a550d8607c38a9e42c76b6be844e8

--- content/browser/site_per_process_browsertest.cc
+++ content/browser/site_per_process_browsertest.cc
@@ -6715,10 +6715,11 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessMouseWheelBrowserTest,
   EXPECT_EQ(nullptr, router->wheel_target_.target);
 }
 
+// TODO: Flaking test crbug.com/802828
 // Ensure that a cross-process subframe with a touch-handler can receive touch
 // events.
 IN_PROC_BROWSER_TEST_F(SitePerProcessBrowserTest,
-                       SubframeTouchEventRouting) {
+                       DISABLED_SubframeTouchEventRouting) {
   GURL main_url(embedded_test_server()->GetURL(
       "/frame_tree/page_with_positioned_nested_frames.html"));
   EXPECT_TRUE(NavigateToURL(shell(), main_url));
