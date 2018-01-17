865a6852b23ba1353499644ffae21fb3e8836023
Joe Downing <joedow@chromium.org>
2018-01-17 13:35:55 -0800

    Simple Active Key Filter for KeyboardLock Impl
    This CL adds a simple filter (i.e. filters all keys) for use with the
    KeyboardLock Prototype.  Future CLs will add the capability to
    filter specific keys out.
    
    BUG=680809
    
    Change-Id: I52adab204aedb48e5d7fa4fa88aaa26a16e16d64

---
+++ content/browser/keyboard_lock/active_key_filter.h
@@ -0,0 +1,34 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_KEYBOARD_LOCK_ACTIVE_KEY_FILTER_H_
+#define CONTENT_BROWSER_KEYBOARD_LOCK_ACTIVE_KEY_FILTER_H_
+
+#include "base/macros.h"
+#include "ui/events/event.h"
+#include "ui/events/platform/key_event_filter.h"
+
+namespace content {
+
+class RenderWidgetHostView;
+
+// Filters out key events which have been requested by the KeyboardLock API.
+// TODO(joedow): Implement selective kb lock, i.e. only trap specific keys.
+class ActiveKeyFilter final : public ui::KeyEventFilter {
+ public:
+  explicit ActiveKeyFilter(RenderWidgetHostView* host_view);
+  ~ActiveKeyFilter() override;
+
+  // ui::KeyEventFilter interface.
+  bool OnKeyEvent(const ui::KeyEvent& event) override;
+
+ private:
+  RenderWidgetHostView* host_view_ = nullptr;
+
+  DISALLOW_COPY_AND_ASSIGN(ActiveKeyFilter);
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_KEYBOARD_LOCK_ACTIVE_KEY_FILTER_H_
