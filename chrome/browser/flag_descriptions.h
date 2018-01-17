cb02548d6bc7d58370276fdd259266f7266fb162
Michael Thiessen <mthiesse@chromium.org>
2018-01-17 13:19:21 -0500

    VR: Add dynamically-toggled icon in Daydream Home
    This CL adds the ability to show the Chrome icon in Daydream Home, which
    launches Chrome in VR mode, behind a feature flag.
    
    We're able to do this by enabling/disabling the component that adds the
    icon at runtime.
    
    Bug: 802080
    Change-Id: I878202c110d2a7bd092173457de73ce1578d2ff6

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1736,6 +1736,9 @@ extern const char kVrBrowsingExperimentalRenderingDescription[];
 extern const char kVrBrowsingInCustomTabName[];
 extern const char kVrBrowsingInCustomTabDescription[];
 
+extern const char kVrIconInDaydreamHomeName[];
+extern const char kVrIconInDaydreamHomeDescription[];
+
 extern const char kVrLaunchIntentsName[];
 extern const char kVrLaunchIntentsDescription[];
 
