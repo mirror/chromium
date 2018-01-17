0188b5f717dbeeaf874e1f33ce0ead8fe36bfafe
Daniel Cheng <dcheng@chromium.org>
2018-01-17 15:34:53 -0800

    Add base::NoDestructor<T>.
    This is simlar to base::LazyInstance<T>::Leaky but intended for use in
    function-local scope.
    
    This is intended to be a drop-in replacement for:
    - CR_DEFINE_STATIC_LOCAL(...); in function scope
    - static T* x = new X; in function scope
    - static T& x = *new X; in function scope
    and should be preferred in new code.
    
    Change-Id: I75eda47e7dca8205f4086bd298839c261454a3e7

---
+++ base/no_destructor.h
@@ -0,0 +1,64 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_NO_DESTRUCTOR_H_
+#define BASE_NO_DESTRUCTOR_H_
+
+#include <utility>
+
+namespace base {
+
+// A wrapper that makes it easy to create an object of type T with static
+// storage duration that:
+// - is only constructed on first access
+// - never invokes the destructor
+// in order to satisfy the styleguide ban on global constructors and
+// destructors. Example usage:
+//
+// T& GetT() {
+//   static base::NoDestructor<T> value(arg1, arg2, arg3...);
+//   return *value;
+// }
+//
+// NoDestructor<T> stores the object inline, so it also avoids a pointer
+// indirection and a malloc. Code should prefer to use NoDestructor<T> over:
+// - The CR_DEFINE_STATIC_LOCAL() helper macro.
+// - A function scoped static T* or T& that is dynamically initialized.
+// - A global base::LazyInstance<T>.
+//
+// Note that since the destructor is never run, this *will* leak memory if used
+// as a stack or member variable. Furthermore, a NoDestructor<T> should never
+// have global scope as that may require a static initializer.
+template <typename T>
+class NoDestructor {
+ public:
+  // Not constexpr; just write static constexpr T x = ...; if the value should
+  // be a constexpr.
+  template <typename... Args>
+  explicit NoDestructor(Args&&... args)
+      : storage_(std::forward<Args>(args)...) {}
+
+  ~NoDestructor() {
+    // |storage_| is part of a union, so its destructor is skipped unless
+    // manually invoked.
+  }
+
+  const T& operator*() const { return storage_; }
+  T& operator*() { return storage_; }
+
+  const T* operator->() const { return &storage_; }
+  T* operator->() { return &storage_; }
+
+  const T* get() const { return &storage_; }
+  T* get() { return &storage_; }
+
+ private:
+  union {
+    T storage_;
+  };
+};
+
+}  // namespace base
+
+#endif  // BASE_NO_DESTRUCTOR_H_
