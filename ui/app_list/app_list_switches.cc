8ce279eba69472e7b8fd750aa6455185be0313b0
Vadim Tryshev <vadimt@google.com>
2018-01-18 10:39:44 -0800

    More of removal voice search from Launcher.
    There are some possible cleanups around StartPageService and
    SearchResourceManager that I plan for a separate CL.
    
    Bug: 781407
    Change-Id: I9c9042e46d792bf5cf06fb0dc1c29cd8f6edb560

--- ui/app_list/app_list_switches.cc
+++ ui/app_list/app_list_switches.cc
@@ -32,11 +32,6 @@ const char kDisableDriveSearchInChromeLauncher[] =
 // list hasn't been enabled (as in kEnableAppList) yet.
 const char kResetAppListInstallState[] = "reset-app-list-install-state";
 
-bool IsVoiceSearchEnabled() {
-  // Speech recognition in AppList is only for ChromeOS right now.
-  return true;
-}
-
 bool ShouldNotDismissOnBlur() {
   return base::CommandLine::ForCurrentProcess()->HasSwitch(
       kDisableAppListDismissOnBlur);
