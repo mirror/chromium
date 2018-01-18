66255e2d3c9d599fac87f93b1c9e6c9c7b2f958e
Jacob Dufault <jdufault@google.com>
2018-01-18 13:36:53 -0800

    cros: Port StartArcKiosk to LoginDisplayHost.
    Bug: 784495
    Change-Id: If58401246ba905279624805b31b866ad93b0ec62

--- chrome/browser/chromeos/login/ui/login_display_host_views.cc
+++ chrome/browser/chromeos/login/ui/login_display_host_views.cc
@@ -102,7 +102,7 @@ void LoginDisplayHostViews::OnStartAppLaunch() {
   NOTIMPLEMENTED();
 }
 
-void LoginDisplayHostViews::StartArcKiosk(const AccountId& account_id) {
+void LoginDisplayHostViews::OnStartArcKiosk() {
   NOTIMPLEMENTED();
 }
 
