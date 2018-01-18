81193d282f3dadeb26f65ad73ab60f54ece812ef
Olga Sharonova <olka@chromium.org>
2018-01-18 11:49:18 +0100

    Audio service: quit with timeout is there are no incoming connections.
    Bug:792441
    Change-Id: I0d715e485ce1d7f414d65e4c08291537d3d48614

--- services/audio/service.cc
+++ services/audio/service.cc
@@ -10,11 +10,15 @@
 #include "media/audio/audio_manager.h"
 #include "services/audio/system_info.h"
 #include "services/service_manager/public/cpp/service_context.h"
+#include "services/service_manager/public/cpp/service_context_ref.h"
 
 namespace audio {
 
-Service::Service(std::unique_ptr<AudioManagerAccessor> audio_manager_accessor)
-    : audio_manager_accessor_(std::move(audio_manager_accessor)) {
+Service::Service(std::unique_ptr<AudioManagerAccessor> audio_manager_accessor,
+                 base::TimeDelta quit_timeout)
+    : quit_timeout_(quit_timeout),
+      audio_manager_accessor_(std::move(audio_manager_accessor)),
+      weak_factory_(this) {
   DCHECK(audio_manager_accessor_);
 }
 
@@ -24,6 +28,9 @@ Service::~Service() {
 
 void Service::OnStart() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  ref_factory_ = std::make_unique<service_manager::ServiceContextRefFactory>(
+      base::BindRepeating(&Service::MaybeRequestQuitDelayed,
+                          base::Unretained(this)));
   registry_.AddInterface<mojom::SystemInfo>(base::BindRepeating(
       &Service::BindSystemInfoRequest, base::Unretained(this)));
 }
@@ -42,13 +49,38 @@ bool Service::OnServiceManagerConnectionLost() {
   return true;
 }
 
+void Service::SetQuitClosureForTesting(base::RepeatingClosure quit_closure) {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  quit_closure_ = std::move(quit_closure);
+}
+
 void Service::BindSystemInfoRequest(mojom::SystemInfoRequest request) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   if (!system_info_) {
     system_info_ = std::make_unique<SystemInfo>(
         audio_manager_accessor_->GetAudioManager());
   }
-  system_info_->Bind(std::move(request));
+  system_info_->Bind(std::move(request), ref_factory_->CreateRef());
+}
+
+void Service::MaybeRequestQuitDelayed() {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  if (quit_timeout_ <= base::TimeDelta())
+    return;
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&Service::MaybeRequestQuit, weak_factory_.GetWeakPtr()),
+      quit_timeout_);
+}
+
+void Service::MaybeRequestQuit() {
+  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+  DCHECK(ref_factory_);
+  if (ref_factory_->HasNoRefs()) {
+    context()->RequestQuit();
+    if (!quit_closure_.is_null())
+      std::move(quit_closure_).Run();
+  }
 }
 
 }  // namespace audio
