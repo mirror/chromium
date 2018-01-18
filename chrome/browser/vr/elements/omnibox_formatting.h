284c10607bb88fe16c31f5d5ba82854d27c2fbe7
Christopher Grant <cjgrant@chromium.org>
2018-01-18 17:31:13 -0500

    VR: Move to fade-based elision on URL presentation
    As per UX spec, avoid ellipsis-based elision and favor fading the edges
    of the URL instead.  In this implementation, we use a gradient shader to
    draw a fade over the edges of the text, if necessary, after applying
    offset to position the TLD appropriately.
    
    Alternatively, RenderText can be augmented to supply the fading portion.
    To do this, we'd need to sort out how to make its fade shader work with
    colored text.  The VR-specific case may work around that complexity by
    drawing the URL onto a dedicated texture.
    
    BUG=799194
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Id108184951f9dba2793e9372c15f1ea75b18a1dc

--- chrome/browser/vr/elements/omnibox_formatting.h
+++ chrome/browser/vr/elements/omnibox_formatting.h
@@ -11,12 +11,35 @@
 #include "chrome/browser/vr/model/color_scheme.h"
 #include "components/omnibox/browser/autocomplete_match.h"
 
+namespace gfx {
+class RenderText;
+}
+
 namespace vr {
 
+// The set of URL format attributes used by VR.
+extern const url_formatter::FormatUrlTypes kUrlFormatTypes;
+
+// Convert Autocomplete's suggestion formatting to generic VR text formatting.
 TextFormatting ConvertClassification(
     const ACMatchClassifications& classifications,
     size_t text_length,
     const ColorScheme& color_scheme);
+
+struct ElisionParameters {
+  int offset = 0;
+  bool fade_left = false;
+  bool fade_right = false;
+};
+
+// Based on a URL and the RenderText that will draw it, determine the required
+// elision parameters.  This means computing an offset such that the rightmost
+// portion of the TLD is visible (along with a small part of the path), and
+// fading either edge if they overflow available space.
+ElisionParameters GetElisionParameters(const GURL& gurl,
+                                       const url::Parsed& parsed,
+                                       gfx::RenderText* render_text,
+                                       int min_path_pixels);
 
 }  // namespace vr
 
