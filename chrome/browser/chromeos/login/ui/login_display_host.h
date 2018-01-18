0fc1962ef08c82e45883524ef99530d474eafced
Jacob Dufault <jdufault@google.com>
2018-01-18 13:59:51 -0800

    cros: Port default_host_ to LoginDisplayHost
    Bug: 784495
    Change-Id: Ia7a4450ea63eda77555b87357dda8834ea6ae3a4

--- chrome/browser/chromeos/login/ui/login_display_host.h
+++ chrome/browser/chromeos/login/ui/login_display_host.h
@@ -113,9 +113,6 @@ class LoginDisplayHost {
   virtual bool IsVoiceInteractionOobe() = 0;
 
  protected:
-  // Default LoginDisplayHost. Child class sets the reference.
-  static LoginDisplayHost* default_host_;
-
   virtual void OnStartSignInScreen(const LoginScreenContext& context) = 0;
   virtual void OnStartAppLaunch() = 0;
   virtual void OnStartArcKiosk() = 0;
@@ -136,6 +133,9 @@ class LoginDisplayHost {
   std::unique_ptr<ArcKioskController> arc_kiosk_controller_;
 
  private:
+  // Global LoginDisplayHost instance.
+  static LoginDisplayHost* default_host_;
+
   base::WeakPtrFactory<LoginDisplayHost> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(LoginDisplayHost);
