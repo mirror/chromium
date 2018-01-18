cd2c021575eedf23e03e86df2fb9839fc0948102
Jinho Bang <jinho.bang@samsung.com>
2018-01-19 00:25:55 +0900

    ios: Replace base::MakeUnique with std::make_unique
    Should use std::make_unique directly instead of using base::MakeUnique
    since August[1]. So, this change removes usages of base::MakeUnique in
    //ios.
    
    This change removes `#include "base/memory/ptr_util.h"` if it's no more
    used in files. (Might not remove if base::WrapUnique is used)
    
    This change also adds `#include <memory>` header except the following
    two cases:
      - Not add the header if the file contains the <memory> header
      - Not add the header if the related header contains <memory> header
        (The style guide[2] says that foo.cc can rely on foo.h's includes)
    
    Confirmed no additional lint errors(by `git cl lint`) after this change.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/616016
    [2] https://google.github.io/styleguide/cppguide.html#Names_and_Order_of_Includes
    
    Bug: 755727
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I7d62699d862da199d76ed44e22a988e0d96bab4e

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_header_synchronizer_unittest.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_header_synchronizer_unittest.mm
@@ -4,10 +4,11 @@
 
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_header_synchronizer.h"
 
-#include "base/memory/ptr_util.h"
+#include <memory>
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_controlling.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_header_controlling.h"
 #import "ios/chrome/test/base/scoped_block_swizzler.h"
+
 #include "testing/platform_test.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
 #import "third_party/ocmock/gtest_support.h"
@@ -36,7 +37,7 @@ class ContentSuggestionsHeaderSynchronizerTest : public PlatformTest {
 
   id CollectionController() { return collection_controller_; }
   void SetAsIPhone() {
-    device_type_swizzler_ = base::MakeUnique<ScopedBlockSwizzler>(
+    device_type_swizzler_ = std::make_unique<ScopedBlockSwizzler>(
         [UIDevice class], @selector(userInterfaceIdiom),
         ^UIUserInterfaceIdiom(id self) {
           return UIUserInterfaceIdiomPhone;
