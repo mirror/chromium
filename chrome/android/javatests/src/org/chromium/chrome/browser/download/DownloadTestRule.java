cce976c6af7e184cb8479ea7d5f72c613a90acd2
Mohamed Heikal <mheikal@google.com>
2018-01-18 13:48:51 -0500

    Android: Delete ContentApplication.java
    ContentApplication.java has been almost empty for while now. The only
    code remaining was used by polling during tests to check if Native has
    been loaded. This turned out to not be required (removing the polling
    code, the trybot tests succeeded 3/3 times).
    
    This cl removes ContentApplication as well as all the polling code.
    
    Bug: 560466
    TBR=slan  # Trivial cast change
    
    Change-Id: If6b3aa2aff389e0592a4ec36d11b75befa82dd91

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTestRule.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadTestRule.java
@@ -21,7 +21,6 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.test.ChromeActivityTestRule;
-import org.chromium.content.browser.test.util.ApplicationUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -210,7 +209,6 @@ public class DownloadTestRule extends ChromeActivityTestRule<ChromeActivity> {
 
         cleanUpAllDownloads();
 
-        ApplicationUtils.waitForLibraryDependencies(InstrumentationRegistry.getInstrumentation());
         final Context context = InstrumentationRegistry.getTargetContext().getApplicationContext();
 
         ThreadUtils.runOnUiThreadBlocking(() -> {
