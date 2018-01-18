a2121740447c7d3776f2e9381173fa38a178f7b4
Tien-Ren Chen <trchen@chromium.org>
2018-01-17 16:41:16 -0800

    [Blink/Paint] Descendant bounds should be snapped before passing to CompositingRecorder
    It is almost never correct to cast LayoutUnit to floats directly. They
    should be snapped first to determine the used layout position.
    
    BUG=798627
    
    Change-Id: Ib0e3a5139e393cee3fdc643c66555afe05777eb5

--- third_party/WebKit/Source/core/paint/PaintLayerPainter.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerPainter.cpp
@@ -432,9 +432,10 @@ PaintResult PaintLayerPainter::PaintLayerContents(
   if (should_composite_for_blend_mode ||
       paint_layer_.PaintsWithTransparency(
           painting_info.GetGlobalPaintFlags())) {
-    FloatRect compositing_bounds = FloatRect(paint_layer_.PaintingExtent(
-        painting_info.root_layer, painting_info.sub_pixel_accumulation,
-        painting_info.GetGlobalPaintFlags()));
+    FloatRect compositing_bounds =
+        PixelSnappedIntRect(paint_layer_.PaintingExtent(
+            painting_info.root_layer, painting_info.sub_pixel_accumulation,
+            painting_info.GetGlobalPaintFlags()));
     compositing_recorder.emplace(
         context, paint_layer_.GetLayoutObject(),
         WebCoreCompositeToSkiaComposite(
