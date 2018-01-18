d05aa0add44c499cdd59db101b8ba0ba941845e4
Julia Tuttle <juliatuttle@chromium.org>
2018-01-18 21:17:54 +0000

    Network Error Logging: Check for and fix AppRequestContext isolation.
    Apps should be able to use NEL, but they shouldn't share state with the
    main request context.
    
    Bug: 803604
    Change-Id: I6533ce46e21c2b47e888524aeeeb293367919ab3

--- chrome/browser/profiles/profile_browsertest.cc
+++ chrome/browser/profiles/profile_browsertest.cc
@@ -50,6 +50,7 @@
 #include "extensions/common/value_builder.h"
 #include "net/base/net_errors.h"
 #include "net/dns/mock_host_resolver.h"
+#include "net/network_error_logging/network_error_logging_feature.h"
 #include "net/reporting/reporting_feature.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/embedded_test_server/http_request.h"
@@ -560,6 +561,10 @@ void CompareURLRequestContexts(
     EXPECT_NE(extension_context->reporting_service(),
               main_context->reporting_service());
   }
+  if (extension_context->network_error_logging_delegate()) {
+    EXPECT_NE(extension_context->network_error_logging_delegate(),
+              main_context->network_error_logging_delegate());
+  }
 
   // Check that the ChannelIDService in the HttpNetworkSession is the same as
   // the one directly on the URLRequestContext.
@@ -583,7 +588,8 @@ IN_PROC_BROWSER_TEST_F(ProfileBrowserTest, URLRequestContextIsolation) {
   ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
 
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kReporting);
+  feature_list.InitWithFeatures(
+      {features::kReporting, features::kNetworkErrorLogging}, {});
 
   MockProfileDelegate delegate;
   EXPECT_CALL(delegate, OnProfileCreated(testing::NotNull(), true, true));
@@ -623,7 +629,8 @@ IN_PROC_BROWSER_TEST_F(ProfileBrowserTest,
   ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
 
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(features::kReporting);
+  feature_list.InitWithFeatures(
+      {features::kReporting, features::kNetworkErrorLogging}, {});
 
   MockProfileDelegate delegate;
   EXPECT_CALL(delegate, OnProfileCreated(testing::NotNull(), true, true));
