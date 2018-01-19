c9fdb3185a62473abba13aa4a678e06c64b68be8
MinChen <minch@chromium.org>
2018-01-19 10:45:40 -0800

    cros: Create power button menu.
    changes in this cl,
    1. Create power button menu if ash::switches::kShowPowerButtonMenu is set.
       Create the corresponding view hierarchy of the menu.
    2. Add PowerButtonScreenshotShowMenuTest* and
       TabletPowerButtonControllerShowMenuTest*.
    
    Following up cls,
    1. Add corresponding switch in chromeos.
    2. Adjust the menu position according to the position of the power button.
       The menu is put at the center of the screen currently.
    
    Bug: 799524
    Change-Id: Id435328537fe08c7faf3e073564ba81498a86f56

--- ash/system/power/tablet_power_button_controller.h
+++ ash/system/power/tablet_power_button_controller.h
@@ -9,6 +9,7 @@
 #include <utility>
 
 #include "ash/ash_export.h"
+#include "ash/system/power/backlights_forced_off_setter.h"
 #include "ash/wm/tablet_mode/tablet_mode_observer.h"
 #include "base/macros.h"
 #include "base/scoped_observer.h"
@@ -23,15 +24,19 @@ namespace base {
 class TickClock;
 }  // namespace base
 
-namespace ash {
+namespace views {
+class Widget;
+}  // namespace views
 
+namespace ash {
 class LockStateController;
 class PowerButtonDisplayController;
 
 // Handles power button events on convertible/tablet device. This class is
 // instantiated and used in PowerButtonController.
 class ASH_EXPORT TabletPowerButtonController
-    : public chromeos::PowerManagerClient::Observer,
+    : public BacklightsForcedOffSetter::Observer,
+      public chromeos::PowerManagerClient::Observer,
       public TabletModeObserver {
  public:
   // Public for tests.
@@ -48,20 +53,34 @@ class ASH_EXPORT TabletPowerButtonController
   static constexpr base::TimeDelta kIgnoreRepeatedButtonUpDelay =
       base::TimeDelta::FromMilliseconds(500);
 
-  TabletPowerButtonController(PowerButtonDisplayController* display_controller,
-                              bool show_power_button_menu,
-                              base::TickClock* tick_clock);
+  TabletPowerButtonController(
+      PowerButtonDisplayController* display_controller,
+      BacklightsForcedOffSetter* backlights_forced_off_setter,
+      bool show_power_button_menu,
+      base::TickClock* tick_clock);
   ~TabletPowerButtonController() override;
 
   // Handles a power button event.
   void OnPowerButtonEvent(bool down, const base::TimeTicks& timestamp);
 
-  // Overridden from chromeos::PowerManagerClient::Observer:
+  // BacklightsForcedOffSetter::Observer:
+  void OnBacklightsForcedOffChanged(bool forced_off) override;
+  void OnScreenStateChanged(
+      BacklightsForcedOffSetter::ScreenState screen_state) override;
+
+  // chromeos::PowerManagerClient::Observer:
+  void SuspendImminent(power_manager::SuspendImminent::Reason reason) override;
   void SuspendDone(const base::TimeDelta& sleep_duration) override;
 
   // TabletModeObserver:
   void OnTabletModeStarted() override;
   void OnTabletModeEnded() override;
+
+  // True if the menu is opened.
+  bool IsMenuOpened() const;
+
+  // Dismisses the menu.
+  void DismissMenu();
 
   // Cancel the ongoing tablet power button behavior.
   void CancelTabletPowerButton();
@@ -83,6 +102,10 @@ class ASH_EXPORT TabletPowerButtonController
   // Called by |power_button_menu_timer_| to start showing the power button
   // menu.
   void OnPowerButtonMenuTimeout();
+
+  // Creates the fullscreen widget responsible for showing the power button
+  // menu.
+  std::unique_ptr<views::Widget> CreateFullscreenWidget();
 
   // True if the screen was off when the power button was pressed.
   bool screen_off_when_power_button_down_ = false;
@@ -106,10 +129,16 @@ class ASH_EXPORT TabletPowerButtonController
   // to show the power button menu.
   base::OneShotTimer power_button_menu_timer_;
 
+  // The fullscreen widget of power button menu.
+  std::unique_ptr<views::Widget> fullscreen_widget_;
+
   LockStateController* lock_state_controller_;  // Not owned.
 
   // Used to interact with the display.
   PowerButtonDisplayController* display_controller_;  // Not owned.
+
+  ScopedObserver<BacklightsForcedOffSetter, BacklightsForcedOffSetter::Observer>
+      backlights_forced_off_observer_;
 
   // If true, start the |power_button_menu_timer_| to show the menu when the
   // power button is long pressed.
