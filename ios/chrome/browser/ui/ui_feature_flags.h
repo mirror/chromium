9db5e81bfc6d56f861c99b4356462ce58c96ce05
Justin Cohen <justincohen@google.com>
2018-01-18 22:53:09 -0500

    [ios] Add shared feature flag for all ui reboot work.
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I146f9834b3ce0f6b26a428104dcfa96485aae1be

---
+++ ios/chrome/browser/ui/ui_feature_flags.h
@@ -0,0 +1,12 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_UI_FEATURE_FLAGS_H_
+#define IOS_CHROME_BROWSER_UI_UI_FEATURE_FLAGS_H_
+
+#include "base/feature_list.h"
+
+extern const base::Feature kUIRefreshPhase1;
+
+#endif  // IOS_CHROME_BROWSER_UI_UI_FEATURE_FLAGS_H_
