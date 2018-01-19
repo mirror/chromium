cdd97e56fde0636ea76dc441e4fcd124b2f633a0
Koji Ishii <kojii@chromium.org>
2018-01-20 00:55:30 +0900

    uashadow
    Change-Id: Ife501c7b2a40fc77eb741ddef89601336ab4769f

--- third_party/WebKit/Source/core/css/resolver/StyleAdjuster.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleAdjuster.cpp
@@ -628,13 +628,33 @@ void StyleAdjuster::AdjustComputedStyle(StyleResolverState& state,
     }
   }
 
-  // TODO(layout-dev): Once LayoutnG handles inline content editable, we should
-  // get rid of following code fragment.
-  if (RuntimeEnabledFeatures::LayoutNGEnabled() &&
-      style.UserModify() != EUserModify::kReadOnly &&
-      style.Display() == EDisplay::kInline &&
-      parent_style.UserModify() == EUserModify::kReadOnly) {
-    style.SetDisplay(EDisplay::kInlineBlock);
+  if (!style.IsLayoutNGDisabled()) {
+    bool should_disable_layout_ng = !RuntimeEnabledFeatures::LayoutNGEnabled();
+
+    // Content editable is not supported yet.
+    if (!should_disable_layout_ng &&
+        style.UserModify() != EUserModify::kReadOnly) {
+      should_disable_layout_ng = true;
+
+      // Change inline to inline-block because all boxes within an inline
+      // formatting context must use one layout engine.
+      if (style.Display() == EDisplay::kInline &&
+          parent_style.UserModify() == EUserModify::kReadOnly)
+        style.SetDisplay(EDisplay::kInlineBlock);
+    }
+
+    // Disable for form controls that create UA shadow. Some controls such as
+    // <fieldset> or <output> are normal flow, and they need to use LayoutNG
+    // when they appear in NG inline formatting context.
+    if (!should_disable_layout_ng && element && element->IsInShadowTree()) {
+      if (ShadowRoot* root = element->ContainingShadowRoot()) {
+        if (root->IsUserAgent() && root->host().IsFormControlElement())
+          should_disable_layout_ng = true;
+      }
+    }
+
+    if (should_disable_layout_ng)
+      style.SetIsLayoutNGDisabled(true);
   }
 }
 }  // namespace blink
