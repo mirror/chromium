261b93c8a27a7294c711b836b67dd7bd9434fa41
Hugo Holgersson <hugoh@vewd.com>
2018-01-19 13:33:11 +0100

    Snav: Start at the visual viewport's edge when focus is off screen
    By initializing starting_rect to one of the visual viewport's
    edges we don't need to recalculate this value every time
    AdvanceFocusDirectionallyInContainer iterates.
    
    Q: So now we always use the frame's visual view port when the
       focused element is off screen?
    
    A: Yes. The visual viewport is the default starting point for
       snav's search. This was already the implicit behavior users
       relied on (see bug).
       When the focused element F is off screen (say JS scrolled
       <body> downwards) we want spatnav to first look for already
       visible focusables, i.e not go to focusables nearby F because
       that will cause the window to scroll (bad UX).
    
    This lets users reach things that they can _see_ first.
    
    Bug: 770147, 796493
    Test: Already tested in snav-use-visual-viewport.html.
    Change-Id: Iaa861d3f75effe14eac558e2214dbc3af2df365b

--- third_party/WebKit/Source/core/page/FocusController.cpp
+++ third_party/WebKit/Source/core/page/FocusController.cpp
@@ -1462,10 +1462,10 @@ bool FocusController::AdvanceFocusDirectionally(WebFocusType type) {
   // FIXME: Directional focus changes don't yet work with RemoteFrames.
   if (!FocusedOrMainFrame()->IsLocalFrame())
     return false;
-  LocalFrame* cur_frame = ToLocalFrame(FocusedOrMainFrame());
-  DCHECK(cur_frame);
+  LocalFrame* current_frame = ToLocalFrame(FocusedOrMainFrame());
+  DCHECK(current_frame);
 
-  Document* focused_document = cur_frame->GetDocument();
+  Document* focused_document = current_frame->GetDocument();
   if (!focused_document)
     return false;
 
@@ -1475,17 +1475,18 @@ bool FocusController::AdvanceFocusDirectionally(WebFocusType type) {
   if (container->IsDocumentNode())
     ToDocument(container)->UpdateStyleAndLayoutIgnorePendingStylesheets();
 
-  // Figure out the starting rect.
-  LayoutRect starting_rect;
+  // Figure out the initial starting rect.
+  LayoutRect starting_rect = VirtualRectForDirection(
+      type, FrameRectInAbsoluteCoordinates(current_frame));
   if (focused_element) {
+    auto* area_element = ToHTMLAreaElementOrNull(*focused_element);
+    if (area_element)
+      focused_element = area_element->ImageElement();
     if (!HasOffscreenRect(focused_element)) {
-      starting_rect = NodeRectInAbsoluteCoordinates(focused_element,
-                                                    true /* ignore border */);
-    } else if (auto* area = ToHTMLAreaElementOrNull(*focused_element)) {
-      if (area->ImageElement()) {
-        focused_element = area->ImageElement();
-        starting_rect = VirtualRectForAreaElementAndDirection(*area, type);
-      }
+      starting_rect =
+          area_element
+              ? VirtualRectForAreaElementAndDirection(*area_element, type)
+              : NodeRectInAbsoluteCoordinates(focused_element, true);
     }
     container = ScrollableAreaOrDocumentOf(focused_element);
   }
@@ -1498,6 +1499,7 @@ bool FocusController::AdvanceFocusDirectionally(WebFocusType type) {
     if (consumed)
       break;
 
+    // Nothing found in |container| so search the parent container.
     pruned_sub_tree_root = container;
     container = ScrollableAreaOrDocumentOf(container);
     if (container && container->IsDocumentNode())
