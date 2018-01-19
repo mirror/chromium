4d3ba08caba152e9c831613b38e0f8d6398c9d4a
Scott Chen <scottchen@chromium.org>
2018-01-18 17:21:49 -0800

    MD Extensions: add more kiosk mode tests
    Follow-up of 872200.
    
    Bug: 801755
    Change-Id: Ice20453d3b90243885b3aca99ef68ab9868fede2

--- chrome/test/data/webui/extensions/extension_kiosk_mode_test.js
+++ chrome/test/data/webui/extensions/extension_kiosk_mode_test.js
@@ -6,25 +6,174 @@
 cr.define('extension_kiosk_mode_tests', function() {
   /** @enum {string} */
   let TestNames = {
+    Layout: 'Layout',
     AddButton: 'AddButton',
+    AutoLaunch: 'AutoLaunch',
+    Bailout: 'Bailout',
+    Updated: 'Updated',
+    AddError: 'AddError',
   };
+
 
   let suiteName = 'kioskModeTests';
 
   suite(suiteName, function() {
-    let browserProxy = null;
-    let dialog = null;
 
-    setup(function() {
+    /** @type {extensions.KioskBrowserProxy} */
+    let browserProxy;
+
+    /** @type {extensions.KioskDialog} */
+    let dialog;
+
+    /** @type {!Array<!KioskApp>} */
+    const basicApps = [
+      {
+        id: 'app_1',
+        name: 'App1 Name',
+        iconURL: '',
+        autoLaunch: false,
+        isLoading: false,
+      },
+      {
+        id: 'app_2',
+        name: '',  // no name
+        iconURL: '',
+        autoLaunch: false,
+        isLoading: false,
+      },
+    ];
+
+    /** @return {!Promise} */
+    function initPage() {
       PolymerTest.clearBody();
-
-      browserProxy = new TestKioskBrowserProxy();
-      extensions.KioskBrowserProxyImpl.instance_ = browserProxy;
-
+      browserProxy.reset();
       dialog = document.createElement('extensions-kiosk-dialog');
       document.body.appendChild(dialog);
 
-      Polymer.dom.flush();
+      return browserProxy.whenCalled('getKioskAppSettings')
+          .then(() => PolymerTest.flushTasks());
+    }
+
+    setup(function() {
+      browserProxy = new TestKioskBrowserProxy();
+      // Setup with basic apps fixtures.
+      browserProxy.appSettings.apps = basicApps.slice(0);
+      extensions.KioskBrowserProxyImpl.instance_ = browserProxy;
+
+      return initPage();
+    });
+
+    test(assert(TestNames.Layout), function() {
+      browserProxy.appSettings.apps[1].autoLaunch = true;
+      browserProxy.appSettings.apps[1].isLoading = true;
+      browserProxy.appSettings.hasAutoLaunchApp = true;
+
+      return initPage()
+          .then(() => {
+            const items = dialog.shadowRoot.querySelectorAll('.list-item');
+            expectEquals(items.length, 2);
+            expectTrue(items[0].textContent.indexOf(basicApps[0].name) !== -1);
+            expectTrue(items[1].textContent.indexOf(basicApps[1].name) !== -1);
+            // Second item should show the auto-lauch label.
+            expectTrue(items[0].querySelector('span').hidden);
+            expectFalse(items[1].querySelector('span').hidden);
+            // No permission to edit auto-launch so buttons should be hidden.
+            expectTrue(items[0].querySelector('paper-button').hidden);
+            expectTrue(items[1].querySelector('paper-button').hidden);
+            // Bailout checkbox should be hidden when auto-launch editing
+            // disabled.
+            expectTrue(dialog.$$('paper-checkbox').hidden);
+
+            MockInteractions.tap(items[0].querySelector('.icon-delete-gray'));
+            Polymer.dom.flush();
+            return browserProxy.whenCalled('removeKioskApp');
+          })
+          .then(appId => {
+            expectEquals(appId, basicApps[0].id);
+          });
+    });
+
+    test(assert(TestNames.AutoLaunch), function() {
+      browserProxy.initialSettings.autoLaunchEnabled = true;
+      browserProxy.appSettings.apps[1].autoLaunch = true;
+      browserProxy.appSettings.hasAutoLaunchApp = true;
+
+      let items;
+      return initPage()
+          .then(() => {
+            items = dialog.shadowRoot.querySelectorAll('.list-item');
+            // Has permission to edit auto-launch so buttons should be seen.
+            expectFalse(items[0].querySelector('paper-button').hidden);
+            expectFalse(items[1].querySelector('paper-button').hidden);
+
+            MockInteractions.tap(items[0].querySelector('paper-button'));
+            return browserProxy.whenCalled('enableKioskAutoLaunch');
+          })
+          .then(appId => {
+            expectEquals(appId, basicApps[0].id);
+
+            MockInteractions.tap(items[1].querySelector('paper-button'));
+            return browserProxy.whenCalled('disableKioskAutoLaunch');
+          })
+          .then(appId => {
+            expectEquals(appId, basicApps[1].id);
+          });
+    });
+
+    test(assert(TestNames.Bailout), function() {
+      browserProxy.initialSettings.autoLaunchEnabled = true;
+      browserProxy.appSettings.apps[1].autoLaunch = true;
+      browserProxy.appSettings.hasAutoLaunchApp = true;
+
+      expectFalse(dialog.$['confirm-dialog'].open);
+
+      let bailoutCheckbox;
+      return initPage()
+          .then(() => {
+            bailoutCheckbox = dialog.$$('paper-checkbox');
+            // Bailout checkbox should be usable when auto-launching.
+            expectFalse(bailoutCheckbox.hidden);
+            expectFalse(bailoutCheckbox.disabled);
+            expectFalse(bailoutCheckbox.checked);
+
+            // Making sure canceling doesn't change anything.
+            bailoutCheckbox.dispatchEvent(new PointerEvent('pointerdown'));
+            Polymer.dom.flush();
+            expectTrue(dialog.$['confirm-dialog'].open);
+
+            MockInteractions.tap(
+                dialog.$['confirm-dialog'].querySelector('.cancel-button'));
+            Polymer.dom.flush();
+            expectFalse(bailoutCheckbox.checked);
+            expectFalse(dialog.$['confirm-dialog'].open);
+            expectTrue(dialog.$.dialog.open);
+
+            // Accepting confirmation dialog should trigger browserProxy call.
+            bailoutCheckbox.dispatchEvent(new PointerEvent('pointerdown'));
+            Polymer.dom.flush();
+            expectTrue(dialog.$['confirm-dialog'].open);
+
+            MockInteractions.tap(
+                dialog.$['confirm-dialog'].querySelector('.action-button'));
+            Polymer.dom.flush();
+            expectTrue(bailoutCheckbox.checked);
+            expectFalse(dialog.$['confirm-dialog'].open);
+            expectTrue(dialog.$.dialog.open);
+            return browserProxy.whenCalled('setDisableBailoutShortcut');
+          })
+          .then(disabled => {
+            expectTrue(disabled);
+
+            // Test clicking on checkbox again should simply re-enable bailout.
+            browserProxy.reset();
+            bailoutCheckbox.dispatchEvent(new PointerEvent('pointerdown'));
+            expectFalse(bailoutCheckbox.checked);
+            expectFalse(dialog.$['confirm-dialog'].open);
+            return browserProxy.whenCalled('setDisableBailoutShortcut');
+          })
+          .then(disabled => {
+            expectFalse(disabled);
+          });
     });
 
     test(assert(TestNames.AddButton), function() {
@@ -40,6 +189,35 @@ cr.define('extension_kiosk_mode_tests', function() {
       return browserProxy.whenCalled('addKioskApp').then(appId => {
         expectEquals(appId, 'blah');
       });
+    });
+
+    test(assert(TestNames.Updated), function() {
+      const items = dialog.shadowRoot.querySelectorAll('.list-item');
+      expectTrue(items[0].textContent.indexOf(basicApps[0].name) !== -1);
+
+      const newName = 'completely different name';
+
+      cr.webUIListenerCallback('kiosk-app-updated', {
+        id: basicApps[0].id,
+        name: newName,
+        iconURL: '',
+        autoLaunch: false,
+        isLoading: false,
+      });
+
+      expectFalse(items[0].textContent.indexOf(basicApps[0].name) !== -1);
+      expectTrue(items[0].textContent.indexOf(newName) !== -1);
+    });
+
+    test(assert(TestNames.AddError), function() {
+      const addInput = dialog.$['add-input'];
+
+      expectFalse(!!addInput.invalid);
+      cr.webUIListenerCallback('kiosk-app-error', basicApps[0].id);
+
+      expectTrue(!!addInput.invalid);
+      console.log(addInput.errorMessage);
+      expectTrue(addInput.errorMessage.indexOf(basicApps[0].id) !== -1);
     });
   });
 
