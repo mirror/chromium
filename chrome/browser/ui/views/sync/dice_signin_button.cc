e71037de248b01b58d5c66716a59783215bd265b
Mihai Sardarescu <msarda@chromium.org>
2018-01-19 09:51:22 +0100

    [Dice] Show a personalized sign-in promo in bookmark bubble.
    This CL adds a DICE sign-in button that is a either a plain text "Sign in"
    button if the user is signed out or a button containing the user avatar
    image and full name for the first account present in the Gaia cookies.
    
    It then uses this button to personalize the sync promo in the bookmark
    bubble. As it lives side-by-side with the pre-DICE sign-in promo, the
    delegate of the bubble sync promo was slightly changed to cater for the
    new case to enable sync with an account already available in Chrome.
    
    I chose this design based on the specs and mocks (copied in-line from the document):
    
    The “Sync to” button should ideally be a single button with 2 targets:
    * If the user taps on Sync to <account name>, then sync should start
    * If the user taps on the arrow, then it should present a menu on top of
      the parent bubble. This is still TODO and out of scope for this CL.
    * The ink ripple effect when clicking on the "Sync to" portion of
      the button should go all the way through to the rightward-facing arrow
      (it should feel like one button)
    * The ink ripple effect for the drop-down arrow is local only to the the
      drop-down arrow with a circle (like for Chrome back button)
    * Tapping "tab" should focus first on the "Sync to" portion of the
      button, followed by the arrow part of the button (i.e. we should have
      two focus targets)
    
    Screenshots:
    macOS:
    https://drive.google.com/file/d/1fiPljuSer1iTO6BfsUvdlyy7aCTAfkCz/view?usp=sharing
    https://drive.google.com/file/d/1tJwXJLapBddH55TusQPUs0NzD1-MaDdR/view?usp=sharing
    
    Windows 10:
    https://drive.google.com/file/d/1Fh6d9UPfIIYco2rBb9QDrZBHySPzzHb2/view?usp=sharing
    https://drive.google.com/file/d/1CeXD5f5KfuJlZ3Epmwb6dZshUE7qL4vZ/view?usp=sharing
    
    Linux:
    https://drive.google.com/file/d/1TCpnADD3V8Z1GpKRHuo2c5YSPA-Tlgyr/view?usp=sharing
    https://drive.google.com/file/d/1_eFp-x-hGdfRTNrfSiwaYYzaZAiEHKpg/view?usp=sharing
    
    
    Design doc: see link from bug
    
    Bug: 802189
    Change-Id: I1011318cdbb7c396e84299f93d4d2766c0862e8b

---
+++ chrome/browser/ui/views/sync/dice_signin_button.cc
@@ -0,0 +1,158 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/sync/dice_signin_button.h"
+
+#include "base/strings/string16.h"
+#include "base/strings/utf_string_conversions.h"
+#include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/profiles/profile_avatar_icon_util.h"
+#include "chrome/grit/generated_resources.h"
+#include "components/vector_icons/vector_icons.h"
+#include "ui/base/l10n/l10n_util.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/paint_vector_icon.h"
+#include "ui/views/background.h"
+#include "ui/views/border.h"
+
+namespace {
+
+// Drop down arrow sizes.
+constexpr int kDropDownArrowIconSize = 15;
+constexpr int kDropDownArrowButtonWidth = 50;
+
+// Divider sizes
+constexpr int kDividerHorizontalPadding = 4;
+constexpr int kDividerWidth = 1;
+constexpr int kDividerVerticalPadding = 10;
+
+int GetDividerAndArrowReservedWidth() {
+  return 2 * kDividerHorizontalPadding + kDividerWidth +
+         kDropDownArrowButtonWidth;
+}
+
+}  // namespace
+
+DiceSigninButton::DiceSigninButton(views::ButtonListener* button_listener)
+    : views::MdTextButton(button_listener, views::style::CONTEXT_BUTTON),
+      account_(base::nullopt) {
+  // Regular MD text button when there is not account.
+  SetText(l10n_util::GetStringUTF16(IDS_PROFILES_DICE_SIGNIN_BUTTON));
+  SetFocusForPlatform();
+  SetProminent(true);
+  SetHorizontalAlignment(gfx::ALIGN_CENTER);
+}
+
+DiceSigninButton::DiceSigninButton(const AccountInfo& account,
+                                   views::ButtonListener* button_listener)
+    : views::MdTextButton(button_listener, views::style::CONTEXT_BUTTON),
+      account_(account) {
+  // First create the child views.
+  subtitle_ = new views::Label(base::ASCIIToUTF16(account_->email));
+  subtitle_->SetAutoColorReadabilityEnabled(false);
+  subtitle_->SetEnabledColor(SK_ColorWHITE);
+  subtitle_->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+  AddChildView(subtitle_);
+
+  divider_ = new views::View();
+  divider_->SetBackground(views::CreateSolidBackground(SK_ColorWHITE));
+  AddChildView(divider_);
+
+  arrow_ = new views::ImageButton(button_listener);
+  arrow_->SetImageAlignment(views::ImageButton::ALIGN_CENTER,
+                            views::ImageButton ::ALIGN_MIDDLE);
+  arrow_->SetInkDropMode(views::InkDropHostView::InkDropMode::ON);
+  arrow_->set_has_ink_drop_action_on_click(true);
+  arrow_->SetFocusForPlatform();
+  arrow_->SetFocusPainter(nullptr);
+  arrow_->SetImage(
+      views::Button::STATE_NORMAL,
+      gfx::CreateVectorIcon(kSigninButtonDropDownArrowIcon,
+                            kDropDownArrowIconSize, SK_ColorWHITE));
+  AddChildView(arrow_);
+
+  // Set the title text for main Sign-in button.
+  base::string16 button_title =
+      account_->full_name.empty()
+          ? l10n_util::GetStringUTF16(
+                IDS_PROFILES_DICE_SIGNIN_FIRST_ACCOUNT_BUTTON_NO_NAME)
+          : l10n_util::GetStringFUTF16(
+                IDS_PROFILES_DICE_SIGNIN_FIRST_ACCOUNT_BUTTON,
+                base::UTF8ToUTF16(account_->full_name));
+  SetText(button_title);
+  SetFocusForPlatform();
+  SetProminent(true);
+  SetHorizontalAlignment(gfx::ALIGN_LEFT);
+
+  // Set the image
+  gfx::Image account_icon =
+      ui::ResourceBundle::GetSharedInstance().GetImageNamed(
+          profiles::GetPlaceholderAvatarIconResourceID());
+  gfx::Image profile_photo_circular = profiles::GetSizedAvatarIcon(
+      account_icon, true, 40, 40, profiles::SHAPE_CIRCLE);
+  SetImage(views::Button::STATE_NORMAL, *profile_photo_circular.ToImageSkia());
+}
+
+DiceSigninButton::~DiceSigninButton() = default;
+
+gfx::Rect DiceSigninButton::GetChildAreaBounds() {
+  gfx::Rect child_area = MdTextButton::GetChildAreaBounds();
+  if (!account_)
+    return child_area;
+
+  // Make room on the right for the divider and the drop-down arrow.
+  child_area.set_width(child_area.width() - GetDividerAndArrowReservedWidth());
+  return child_area;
+}
+
+int DiceSigninButton::GetHeightForWidth(int width) const {
+  if (!account_)
+    return MdTextButton::GetHeightForWidth(width);
+
+  // Title and subtitle are labels with a single line. So their preferred
+  // height is not affected by |width|.
+  int height_without_subtitle = MdTextButton::GetHeightForWidth(width);
+  int title_subtitle_height = label()->GetHeightForWidth(width) +
+                              subtitle_->GetHeightForWidth(width) +
+                              GetInsets().height();
+  return std::max(height_without_subtitle, title_subtitle_height);
+}
+
+gfx::Size DiceSigninButton::CalculatePreferredSize() const {
+  NOTREACHED() << "DiceSigninButton is only used in bubbles that place "
+                  " child views using |GetHeightForWidth|. This method is "
+                  " intentionally not implemented to avoid confusion about "
+                  " the way this button is being layed out.";
+  return MdTextButton::CalculatePreferredSize();
+}
+
+void DiceSigninButton::Layout() {
+  views::MdTextButton::Layout();
+  if (!account_)
+    return;
+
+  // By default, |title| takes the entire height of the button. Shink |title|
+  // to make space for |subtitle_|.
+  views::Label* title = label();
+  gfx::Size title_size = title->size();
+  int min_title_height = title->GetHeightForWidth(title_size.width());
+  int title_height = std::max(min_title_height, title_size.height() / 2);
+  title->SetSize(gfx::Size(title_size.width(), title_height));
+
+  // Lay out |subtitle_| right below |title|.
+  gfx::Point title_bottom_left = title->bounds().bottom_left();
+  int subtitle_width = GetChildAreaBounds().width() - title_bottom_left.x();
+  int subtitle_height = subtitle_->GetHeightForWidth(subtitle_width);
+  subtitle_->SetBounds(title_bottom_left.x(), title_bottom_left.y(),
+                       subtitle_width, subtitle_height);
+
+  // Lay the divider and the arrow on the right.
+  gfx::Rect bounds = GetLocalBounds();
+  int divider_x = bounds.width() - GetDividerAndArrowReservedWidth() +
+                  kDividerHorizontalPadding;
+  divider_->SetBounds(divider_x, kDividerVerticalPadding, kDividerWidth,
+                      bounds.height() - 2 * kDividerVerticalPadding);
+  arrow_->SetBounds(divider_x + kDividerWidth + kDividerHorizontalPadding, 0,
+                    kDropDownArrowButtonWidth, bounds.height());
+}
