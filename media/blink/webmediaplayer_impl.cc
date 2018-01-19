f5ad8fd9386330e45003ff3561b84680d0632c37
CJ DiMeglio <lethalantidote@chromium.org>
2018-01-19 13:52:16 -0800

    Updates rotation information for video surfaces.
    This CL allows video surfaces to utilize rotation information provided in the metadata.
    Providing this information is important because it ensures that videos are played in the
    correct orientation.
    
    In order to accomplish this, we wait to enable submission of frames until after metadata
    has loaded. This CL also gets rid of the use of the |submitter_| variable in
    VideoFrameCompositor, only using it for ownership purposes, simplifying the code.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I06eb182be69d326f97b9ae9613cd196db55621a8
    Bug:750313

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -273,13 +273,6 @@ WebMediaPlayerImpl::WebMediaPlayerImpl(
   if (surface_layer_for_video_enabled_)
     bridge_ = params->create_bridge_callback().Run(this);
 
-  if (surface_layer_for_video_enabled_) {
-    vfc_task_runner_->PostTask(
-        FROM_HERE, base::Bind(&VideoFrameCompositor::EnableSubmission,
-                              base::Unretained(compositor_.get()),
-                              bridge_->GetFrameSinkId()));
-  }
-
   // If we're supposed to force video overlays, then make sure that they're
   // enabled all the time.
   always_enable_overlays_ = base::CommandLine::ForCurrentProcess()->HasSwitch(
@@ -419,8 +412,6 @@ void WebMediaPlayerImpl::RegisterContentsLayer(blink::WebLayer* web_layer) {
   DCHECK(bridge_);
   bridge_->GetWebLayer()->CcLayer()->SetContentsOpaque(opaque_);
   bridge_->GetWebLayer()->SetContentsOpaqueIsFixed(true);
-  // TODO(lethalantidote): Figure out how to pass along rotation information.
-  // https://crbug/750313.
   client_->SetWebLayer(web_layer);
 }
 
@@ -1096,6 +1087,7 @@ void WebMediaPlayerImpl::Paint(blink::WebCanvas* canvas,
       return;
     }
   }
+
   video_renderer_.Paint(
       video_frame, canvas, gfx::RectF(gfx_rect), flags,
       pipeline_metadata_.video_decoder_config.video_rotation(), context_3d);
@@ -1553,6 +1545,13 @@ void WebMediaPlayerImpl::OnMetadata(PipelineMetadata metadata) {
       video_weblayer_->layer()->SetContentsOpaque(opaque_);
       video_weblayer_->SetContentsOpaqueIsFixed(true);
       client_->SetWebLayer(video_weblayer_.get());
+    } else {
+      vfc_task_runner_->PostTask(
+          FROM_HERE,
+          base::BindOnce(
+              &VideoFrameCompositor::EnableSubmission,
+              base::Unretained(compositor_.get()), bridge_->GetFrameSinkId(),
+              pipeline_metadata_.video_decoder_config.video_rotation()));
     }
   }
 
@@ -1765,6 +1764,15 @@ void WebMediaPlayerImpl::OnVideoNaturalSizeChange(const gfx::Size& size) {
   gfx::Size old_size = pipeline_metadata_.natural_size;
   if (rotated_size == old_size)
     return;
+
+  if (surface_layer_for_video_enabled_) {
+    vfc_task_runner_->PostTask(
+        FROM_HERE,
+        base::BindOnce(
+            &VideoFrameCompositor::UpdateRotation,
+            base::Unretained(compositor_.get()),
+            pipeline_metadata_.video_decoder_config.video_rotation()));
+  }
 
   pipeline_metadata_.natural_size = rotated_size;
   CreateWatchTimeReporter();
