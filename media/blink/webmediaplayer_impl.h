1c7d2e1dbcfe4f69103863e1e54327229702d888
Jennifer Apacible <apacible@chromium.org>
2018-01-19 12:58:38 -0800

    [Picture in Picture] Show video in OverlayWindow.
    This change allows videos to be shown in picture in picture mode.
    When the picture in picture media controls button on a video player
    is clicked, a window opens and the corresponding video plays in the
    window.
    
    This works behind a flag. This is the first of more patches and lays
    the ground work for additional behavioral implementations and tests.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I01cc0af01986030ccafb623bc06592ce143a8970

--- media/blink/webmediaplayer_impl.h
+++ media/blink/webmediaplayer_impl.h
@@ -121,6 +121,10 @@ class MEDIA_BLINK_EXPORT WebMediaPlayerImpl
   void OnWebLayerUpdated() override;
   void RegisterContentsLayer(blink::WebLayer* web_layer) override;
   void UnregisterContentsLayer(blink::WebLayer* web_layer) override;
+  void OnSurfaceIdUpdated(uint32_t client_id,
+                          uint32_t frame_sink_id,
+                          uint32_t parent_id,
+                          base::UnguessableToken nonce) override;
 
   void Load(LoadType load_type,
             const blink::WebMediaPlayerSource& source,
@@ -867,6 +871,13 @@ class MEDIA_BLINK_EXPORT WebMediaPlayerImpl
 
   base::Callback<mojom::VideoDecodeStatsRecorderPtr()>
       create_decode_stats_recorder_cb_;
+
+  // Keeps track of the components of FrameSinkId and SurfaceId for picture in
+  // picture. This is used to route the video to be shown in the picture in
+  // picture window.
+  viz::FrameSinkId frame_sink_id_;
+  uint32_t parent_id_;
+  base::UnguessableToken nonce_;
 
   DISALLOW_COPY_AND_ASSIGN(WebMediaPlayerImpl);
 };
