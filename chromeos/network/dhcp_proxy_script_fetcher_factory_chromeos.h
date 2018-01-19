8dfa3138dc7d80e8c41c3763cf3c651de69947df
Lily Houghton <lilyhoughton@chromium.org>
2018-01-19 15:31:24 -0500

    Move net/proxy to net/proxy_resolution
    This renames net/proxy to net/proxy_resolution, and hoists net/proxy/proxy_server* to net/base.
    
    Bug:613632
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.linux:linux_mojo;master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Id8ceccb9b75ab1fc87e184de8661527444eedf1f

--- chromeos/network/dhcp_proxy_script_fetcher_factory_chromeos.h
+++ chromeos/network/dhcp_proxy_script_fetcher_factory_chromeos.h
@@ -9,7 +9,7 @@
 
 #include "base/macros.h"
 #include "chromeos/chromeos_export.h"
-#include "net/proxy/dhcp_proxy_script_fetcher_factory.h"
+#include "net/proxy_resolution/dhcp_proxy_script_fetcher_factory.h"
 
 namespace net {
 class DhcpProxyScriptFetcher;
