86c7fa1db9291dc0d15a30fbd4a28c7d8c60878d
Dave Tapuska <dtapuska@chromium.org>
2018-01-19 16:30:48 -0500

    Enable mojo based synchronous compositor
    Turn off disabling mojo input messages for webview. That will enable
    the mojo input channel and the mojo synchronous compositor input channel.
    
    BUG=722928
    
    Change-Id: I49e1d5ee2da83f862d208425f67719bd49c33c42

--- android_webview/lib/aw_main_delegate.cc
+++ android_webview/lib/aw_main_delegate.cc
@@ -152,8 +152,6 @@ bool AwMainDelegate::BasicStartupComplete(int* exit_code) {
   CommandLineHelper::AddDisabledFeature(*cl,
                                         media::kMediaDrmPersistentLicense.name);
 
-  CommandLineHelper::AddDisabledFeature(*cl, features::kMojoInputMessages.name);
-
   android_webview::RegisterPathProvider();
 
   safe_browsing_api_handler_.reset(
