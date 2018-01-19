86f719c95a3070e94a1d5559228263510e301465
Blake O'Hare <blakeo@chromium.org>
2018-01-19 14:32:09 +0900

    Persist keyboard position relative to the closest edge
    This changes the persistence from a single coordinate into a struct
    that takes into consideration which side of the screen the keyboard is
    closest to.
    
    If the keyboard, for example, is effectively docked to the bottom right
    corner of the screen, and the user flips the orientation, the new
    position will also be in the bottom right corner of the screen.
    
    Change-Id: Ieed97751adb72272683f4fd943866e5aa4263d05

--- ui/keyboard/keyboard_controller.cc
+++ ui/keyboard/keyboard_controller.cc
@@ -736,7 +736,8 @@ bool KeyboardController::IsOverscrollAllowed() const {
 }
 
 void KeyboardController::HandlePointerEvent(const ui::LocatedEvent& event) {
-  container_behavior_->HandlePointerEvent(event);
+  container_behavior_->HandlePointerEvent(
+      event, container_->GetRootWindow()->bounds());
 }
 
 void KeyboardController::SetContainerType(const ContainerType type) {
