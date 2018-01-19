1b0e35b59782b3f30d93c2fde2a7954200392872
Joe Downing <joedow@chromium.org>
2018-01-19 10:18:44 -0800

    KeyboardLockHost integrated into RenderWidgetViewHost* with tab observer
    Change-Id: Id6b7acbd50fc7118b0bd796ce112b3340aea5f09

--- content/browser/keyboard_lock/keyboard_lock_host.cc
+++ content/browser/keyboard_lock/keyboard_lock_host.cc
@@ -14,14 +14,42 @@
 #include "base/memory/singleton.h"
 #include "base/single_thread_task_runner.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/render_view_host.h"
+#include "content/public/browser/render_widget_host.h"
+#include "content/public/browser/web_contents.h"
 #include "ui/base/keyboard_lock/platform_hook.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
 
 namespace content {
 
-KeyboardLockHost::KeyboardLockHost(RenderWidgetHostView* host_view)
-    : host_view_(host_view), platform_hook_(ui::PlatformHook::Create()) {
-  DCHECK(host_view_);
+KeyboardLockHost::KeyboardLockHost(RenderWidgetHost* host)
+    : render_widget_host_(host), platform_hook_(ui::PlatformHook::Create()) {
+  DCHECK(render_widget_host_);
+  DCHECK(platform_hook_);
+
+  RenderViewHost* render_view_host = RenderViewHost::From(render_widget_host_);
+  if (render_view_host) {
+    web_contents_ = WebContents::FromRenderViewHost(render_view_host);
+  }
+
+  if (web_contents_) {
+    // NOTE:
+    // WebContentsObserver appears to be the only way to distinguish between tab
+    // initiated and browser initiated fullscreen (javascript vs. user action).
+    // As specified we only want to enable KBLock for tab/javascript initiated
+    // fullscreen.
+    tab_observer_ = std::make_unique<KeyboardLockTabObserver>(
+        render_widget_host_, web_contents_,
+        base::BindRepeating(&KeyboardLockHost::UpdateLockState,
+                            base::Unretained(this)));
+  } else {
+    LOG(ERROR) << "NO WEBCONTENTS!!";
+  }
+
+  // NOTE:
+  // |web_contents_| will be used by the longpress listener to exit fullscreen
+  // mode (none of the classes under Webcontents has the ability to do so via
+  // their interface).
 }
 
 KeyboardLockHost::~KeyboardLockHost() = default;
@@ -41,45 +69,36 @@ void KeyboardLockHost::ReserveKeys(const std::vector<std::string>& codes,
 void KeyboardLockHost::ClearReservedKeys() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
 
-  platform_hook_.reset();
   active_key_filter_.reset();
+  UpdateLockState();
 }
 
 void KeyboardLockHost::ReserveKeyCodes(const std::vector<int>& key_codes,
                                        base::OnceCallback<void(bool)> done) {
-  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-
-  if (!active_key_filter_) {
-    active_key_filter_.reset(new ActiveKeyFilter(host_view_));
-  }
-
   // TODO(joedow): |active_key_filter_| always returns true for all keys now,
-  // however we will need to pass |key_codes| in later on.
-
-  if (activated_) {
-    platform_hook_->Register(active_key_filter_.get());
+  // however we will need to pass |key_codes| in later on.  We will need to
+  // update the list of keys in the filter once this functionality exists.
+  if (!active_key_filter_) {
+    active_key_filter_.reset(new ActiveKeyFilter(render_widget_host_));
   }
+  UpdateLockState();
 
   if (done) {
     std::move(done).Run(true);
   }
 }
 
-void KeyboardLockHost::Activate() {
-  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-
-  activated_ = true;
-
-  if (active_key_filter_) {
-    platform_hook_->Register(active_key_filter_.get());
+void KeyboardLockHost::UpdateLockState() {
+  if (!tab_observer_) {
+    return;
   }
-}
 
-void KeyboardLockHost::Deactivate() {
-  DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-
-  activated_ = false;
-  platform_hook_->Unregister();
+  if (tab_observer_->is_fullscreen() && tab_observer_->is_focused() &&
+      active_key_filter_) {
+    platform_hook_->Register(active_key_filter_.get());
+  } else {
+    platform_hook_->Unregister();
+  }
 }
 
 }  // namespace content
