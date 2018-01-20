4db99384a45bfa225b2ef178113a6f48b945efdf
Xiaohan Wang <xhwang@chromium.org>
2018-01-19 17:08:15 -0800

    media: Add CDM ID support for MojoCdmProxyService
    This CL connects the CdmProxy's CdmContext with the media components,
    e.g. audio/video decoder. If the CdmProxy is destroyed (e.g. on
    connection error), all the CdmContext calls will fail.
    
    Details:
    - Update CdmContext to support GetDecryptContext().
    - Update CdmProxy to support CdmContext.
    - Add CdmProxyContextRef to support weak CdmContext pointers.
    - Update MojoCdmServiceContext to support CdmProxy.
    
    BUG=803710
    
    Change-Id: I055e740de0e71790e364d48aca05a067b123a14f

--- media/mojo/services/mojo_cdm_service_context.h
+++ media/mojo/services/mojo_cdm_service_context.h
@@ -15,27 +15,43 @@
 
 namespace media {
 
+class CdmProxy;
 class CdmContextRef;
 class MojoCdmService;
+class MojoCdmProxyService;
 
 // A class that creates, owns and manages all MojoCdmService instances.
+// TODO(xhwang): Update this class to manage generic CdmContext objects instead
+// of concrete MojoCdmService or MojoCdmProxyService objects. This would require
+// having CdmContext holding reference to the CDM or CdmProxy it is associated
+// with.
 class MEDIA_MOJO_EXPORT MojoCdmServiceContext {
  public:
   MojoCdmServiceContext();
   ~MojoCdmServiceContext();
 
-  // Registers The |cdm_service| with |cdm_id|.
-  void RegisterCdm(int cdm_id, MojoCdmService* cdm_service);
+  // Registers the |cdm_service| and returns a unique (per-process) CDM ID.
+  int RegisterCdm(MojoCdmService* cdm_service);
+  int RegisterCdmProxy(MojoCdmProxyService* cdm_proxy_service);
 
   // Unregisters the CDM. Must be called before the CDM is destroyed.
   void UnregisterCdm(int cdm_id);
+  void UnregisterCdmProxy(int cdm_id);
 
-  // Returns the CDM associated with |cdm_id|.
+  // Returns the CdmContextRef associated with |cdm_id|.
   std::unique_ptr<CdmContextRef> GetCdmContextRef(int cdm_id);
 
  private:
+  // CDM ID to be assigned to the next successfully initialized CDM. This ID is
+  // unique per process. It will be used to locate the CDM by the media players
+  // living in the same process.
+  static int next_cdm_id_;
+
   // A map between CDM ID and MojoCdmService.
   std::map<int, MojoCdmService*> cdm_services_;
+
+  // A map between CDM ID and MojoCdmProxyService.
+  std::map<int, MojoCdmProxyService*> cdm_proxy_services_;
 
   DISALLOW_COPY_AND_ASSIGN(MojoCdmServiceContext);
 };
