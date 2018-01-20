56b8e8ff1bed1154d1aab77cde043ecf0a77951b
Minh X. Nguyen <mxnguyen@chromium.org>
2018-01-19 17:02:44 -0800

    [Extensions] Add UMA counters to measure extension updater results.
    Bug: 722942
    Change-Id: I3beee6eb439cc4ed0819771996ee4f9f4f09197f

---
+++ extensions/common/extension_updater_uma.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef EXTENSIONS_COMMON_EXTENSION_UPDATER_UMA_H_
+#define EXTENSIONS_COMMON_EXTENSION_UPDATER_UMA_H_
+
+namespace extensions {
+
+enum ExtensionUpdaterUpdateResult {
+  NO_UPDATE = 0,
+  UPDATE_SUCCESS = 1,
+  UPDATE_ERROR = 2,
+
+  UPDATE_RESULT_COUNT
+};
+
+}  // namespace extensions
+
+#endif  // EXTENSIONS_COMMON_EXTENSION_UPDATER_UMA_H_
