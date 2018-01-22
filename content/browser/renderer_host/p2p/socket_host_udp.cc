7eeb34c15216c5aa9ab8fafe40cd9153d0e646f6
Gayane Petrosyan <gayane@chromium.org>
2018-01-22 15:32:31 -0500

    Remove WebRTC-SystemUDPSendSocketSize study
    Remove WebRTC-SystemUDPSendSocketSize which is long expired and not useful anymore.
    
    Also removing an unused include from ipc_socket_factory.cc which I missed in
    https://chromium-review.googlesource.com/c/chromium/src/+/854947
    
    Bug: 799180
    Change-Id: Iaa733fff48e7334ceafb3b212866c83822f369d1

--- content/browser/renderer_host/p2p/socket_host_udp.cc
+++ content/browser/renderer_host/p2p/socket_host_udp.cc
@@ -6,10 +6,8 @@
 
 #include "base/bind.h"
 #include "base/memory/ptr_util.h"
-#include "base/metrics/field_trial.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/stl_util.h"
-#include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
@@ -29,6 +27,8 @@ namespace {
 const int kUdpReadBufferSize = 65536;
 // Socket receive buffer size.
 const int kUdpRecvSocketBufferSize = 65536;  // 64K
+// Defaulf send buffer size.
+const int kDefaultSendBufferSize = 65536;
 
 // Defines set of transient errors. These errors are ignored when we get them
 // from sendto() or recvfrom() calls.
@@ -104,7 +104,6 @@ P2PSocketHostUdp::P2PSocketHostUdp(
       send_pending_(false),
       last_dscp_(net::DSCP_CS0),
       throttler_(throttler),
-      send_buffer_size_(0),
       net_log_(net_log),
       socket_factory_(socket_factory) {}
 
@@ -125,20 +124,10 @@ P2PSocketHostUdp::~P2PSocketHostUdp() {
   }
 }
 
-void P2PSocketHostUdp::SetSendBufferSize() {
-  unsigned int send_buffer_size = 0;
-
-  base::StringToUint(
-      base::FieldTrialList::FindFullName("WebRTC-SystemUDPSendSocketSize"),
-      &send_buffer_size);
-
-  if (send_buffer_size > 0) {
-    if (!SetOption(P2P_SOCKET_OPT_SNDBUF, send_buffer_size)) {
-      LOG(WARNING) << "Failed to set socket send buffer size to "
-                   << send_buffer_size;
-    } else {
-      send_buffer_size_ = send_buffer_size;
-    }
+void P2PSocketHostUdp::SetDefaultSendBufferSize() {
+  if (!SetOption(P2P_SOCKET_OPT_SNDBUF, kDefaultSendBufferSize)) {
+    LOG(WARNING) << "Failed to set socket send buffer size to "
+                 << kDefaultSendBufferSize;
   }
 }
 
@@ -191,8 +180,6 @@ bool P2PSocketHostUdp::Init(const net::IPEndPoint& local_address,
   VLOG(1) << "Local address: " << address.ToString();
 
   state_ = STATE_OPEN;
-
-  SetSendBufferSize();
 
   // NOTE: Remote address will be same as what renderer provided.
   message_sender_->Send(new P2PMsg_OnSocketCreated(
@@ -433,11 +420,6 @@ bool P2PSocketHostUdp::SetOption(P2PSocketOption option, int value) {
     case P2P_SOCKET_OPT_RCVBUF:
       return socket_->SetReceiveBufferSize(value) == net::OK;
     case P2P_SOCKET_OPT_SNDBUF:
-      // Ignore any following call to set the send buffer size if we're under
-      // experiment.
-      if (send_buffer_size_ > 0) {
-        return true;
-      }
       return socket_->SetSendBufferSize(value) == net::OK;
     case P2P_SOCKET_OPT_DSCP:
       return (net::OK == socket_->SetDiffServCodePoint(
