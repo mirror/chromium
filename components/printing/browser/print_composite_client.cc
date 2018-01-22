747c803d1eafb65d1f6e043595de578eedc678ba
Wei Li <weili@chromium.org>
2018-01-21 22:08:44 -0800

    Complete pdf compositor service
    Add interfaces and functions to allow passing in and compositing
    contents from multiple frames in pdf compositor service.
    
    Each frame is identified by its frame id, while its drawing content
    is identified by the content's id, a unique id generated by
    concatenating parent frame id and picture id. Each frame may have
    subframes. The pdf compositor service will keep track of all the frame
    content and their relationship. When all subframes are painted, the
    service can composite the entire frame and convert it into PDF format.
    
    Please refer document goo.gl/ACV6VM for more explanations.
    
    BUG=455764
    
    Change-Id: I3213d9ae0ca85d0ea695dd13acb00938499aa73b

--- components/printing/browser/print_composite_client.cc
+++ components/printing/browser/print_composite_client.cc
@@ -4,15 +4,26 @@
 
 #include "components/printing/browser/print_composite_client.h"
 
-#include <memory>
 #include <utility>
 
-#include "base/threading/thread_task_runner_handle.h"
+#include "base/bind.h"
+#include "base/memory/shared_memory_handle.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/service_manager_connection.h"
+#include "mojo/public/cpp/system/platform_handle.h"
 #include "services/service_manager/public/cpp/connector.h"
 
 DEFINE_WEB_CONTENTS_USER_DATA_KEY(printing::PrintCompositeClient);
+
+namespace {
+void OnDidCompositeDocumentToPdf(
+    printing::mojom::PdfCompositorPtr compositor,
+    printing::mojom::PdfCompositor::CompositeDocumentToPdfCallback callback,
+    printing::mojom::PdfCompositor::Status status,
+    mojo::ScopedSharedBufferHandle handle) {
+  std::move(callback).Run(status, std::move(handle));
+}
+}  // namespace
 
 namespace printing {
 
@@ -21,24 +32,83 @@ PrintCompositeClient::PrintCompositeClient(content::WebContents* web_contents)
 
 PrintCompositeClient::~PrintCompositeClient() {}
 
-void PrintCompositeClient::CreateConnectorRequest() {
-  connector_ = service_manager::Connector::Create(&connector_request_);
-  content::ServiceManagerConnection::GetForProcess()
-      ->GetConnector()
-      ->BindConnectorRequest(std::move(connector_request_));
-}
-
-void PrintCompositeClient::DoComposite(
+void PrintCompositeClient::DoCompositePageToPdf(
+    int document_cookie,
+    uint64_t frame_guid,
+    uint32_t page_num,
     base::SharedMemoryHandle handle,
     uint32_t data_size,
-    mojom::PdfCompositor::CompositePdfCallback callback) {
+    ContentToFrameMap subframe_content_map,
+    mojom::PdfCompositor::CompositePageToPdfCallback callback) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  DCHECK(data_size);
+  auto& compositor = GetCompositeRequest(document_cookie);
 
-  if (!connector_)
-    CreateConnectorRequest();
-  Composite(connector_.get(), handle, data_size, std::move(callback),
-            base::ThreadTaskRunnerHandle::Get());
+  DCHECK(data_size);
+  mojo::ScopedSharedBufferHandle buffer_handle = mojo::WrapSharedMemoryHandle(
+      handle, data_size,
+      mojo::UnwrappedSharedMemoryHandleProtection::kReadOnly);
+  compositor->CompositePageToPdf(frame_guid, page_num, std::move(buffer_handle),
+                                 std::move(subframe_content_map),
+                                 std::move(callback));
+}
+
+void PrintCompositeClient::DoCompositeDocumentToPdf(
+    int document_cookie,
+    uint64_t frame_guid,
+    base::SharedMemoryHandle handle,
+    uint32_t data_size,
+    ContentToFrameMap subframe_content_map,
+    mojom::PdfCompositor::CompositeDocumentToPdfCallback callback) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  auto compositor = OwnCompositeRequest(document_cookie);
+
+  DCHECK(data_size);
+  mojo::ScopedSharedBufferHandle buffer_handle = mojo::WrapSharedMemoryHandle(
+      handle, data_size,
+      mojo::UnwrappedSharedMemoryHandleProtection::kReadOnly);
+  auto call_once = base::AdaptCallbackForRepeating(std::move(callback));
+  compositor->CompositeDocumentToPdf(
+      frame_guid, std::move(buffer_handle), std::move(subframe_content_map),
+      base::BindOnce(&OnDidCompositeDocumentToPdf, std::move(compositor),
+                     std::move(call_once)));
+}
+
+mojom::PdfCompositorPtr& PrintCompositeClient::GetCompositeRequest(int cookie) {
+  auto iter = compositor_map_.find(cookie);
+  if (iter != compositor_map_.end()) {
+    if (!iter->second.is_bound()) {
+      NOTREACHED();
+      iter->second = CreateCompositeRequest();
+    }
+    return iter->second;
+  }
+
+  compositor_map_[cookie] = CreateCompositeRequest();
+  return compositor_map_[cookie];
+}
+
+mojom::PdfCompositorPtr PrintCompositeClient::OwnCompositeRequest(int cookie) {
+  auto iter = compositor_map_.find(cookie);
+  if (iter == compositor_map_.end()) {
+    return CreateCompositeRequest();
+  }
+
+  auto compositor = std::move(iter->second);
+  compositor_map_.erase(iter);
+  return compositor;
+}
+
+mojom::PdfCompositorPtr PrintCompositeClient::CreateCompositeRequest() {
+  if (!connector_) {
+    service_manager::mojom::ConnectorRequest connector_request;
+    connector_ = service_manager::Connector::Create(&connector_request);
+    content::ServiceManagerConnection::GetForProcess()
+        ->GetConnector()
+        ->BindConnectorRequest(std::move(connector_request));
+  }
+  mojom::PdfCompositorPtr compositor;
+  connector_->BindInterface(mojom::kServiceName, &compositor);
+  return compositor;
 }
 
 }  // namespace printing
--- components/printing/browser/print_composite_client.h
+++ components/printing/browser/print_composite_client.h
@@ -5,34 +5,72 @@
 #ifndef COMPONENTS_PRINTING_BROWSER_PRINT_COMPOSITE_CLIENT_H_
 #define COMPONENTS_PRINTING_BROWSER_PRINT_COMPOSITE_CLIENT_H_
 
-#include "components/printing/service/public/cpp/pdf_compositor_client.h"
+#include <map>
+#include <memory>
+
+#include "base/containers/flat_map.h"
+#include "components/printing/service/public/interfaces/pdf_compositor.mojom.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
+#include "services/service_manager/public/cpp/connector.h"
 
 namespace printing {
 
+// Class to manage print requests and their communication with pdf
+// compositor service.
+// Print preview always print indivial pages and then the complete
+// document. In this case, we store the interface pointer in a map to be
+// reused among these serial requests.
+// Basic print always print the entire document, so we will simply
+// create a new interface pointer every time.
 class PrintCompositeClient
-    : public PdfCompositorClient,
-      public content::WebContentsUserData<PrintCompositeClient> {
+    : public content::WebContentsUserData<PrintCompositeClient> {
  public:
+  using ContentToFrameMap = std::unordered_map<uint32_t, uint64_t>;
+
   explicit PrintCompositeClient(content::WebContents* web_contents);
   ~PrintCompositeClient() override;
 
   // NOTE: |handle| must be a READ-ONLY base::SharedMemoryHandle, i.e. one
   // acquired by base::SharedMemory::GetReadOnlyHandle().
-  void DoComposite(base::SharedMemoryHandle handle,
-                   uint32_t data_size,
-                   mojom::PdfCompositor::CompositePdfCallback callback);
+  void DoCompositePageToPdf(
+      int cookie,
+      uint64_t frame_guid,
+      uint32_t page_num,
+      base::SharedMemoryHandle handle,
+      uint32_t data_size,
+      ContentToFrameMap subframe_content_map,
+      mojom::PdfCompositor::CompositePageToPdfCallback callback);
+
+  void DoCompositeDocumentToPdf(
+      int cookie,
+      uint64_t frame_guid,
+      base::SharedMemoryHandle handle,
+      uint32_t data_size,
+      ContentToFrameMap subframe_content_map,
+      mojom::PdfCompositor::CompositeDocumentToPdfCallback callback);
 
   void set_for_preview(bool for_preview) { for_preview_ = for_preview; }
   bool for_preview() const { return for_preview_; }
 
  private:
-  void CreateConnectorRequest();
+  // Get the request, but doesn't own it.
+  mojom::PdfCompositorPtr& GetCompositeRequest(int cookie);
 
-  service_manager::mojom::ConnectorRequest connector_request_;
-  std::unique_ptr<service_manager::Connector> connector_;
+  // Find an existing request or create a new one, and own it.
+  mojom::PdfCompositorPtr OwnCompositeRequest(int cookie);
+
+  mojom::PdfCompositorPtr CreateCompositeRequest();
+
+  // Whether this client is created for print preview dialog.
   bool for_preview_;
+
+  std::unique_ptr<service_manager::Connector> connector_;
+
+  // Stores the mapping between document cookies and their corresponding
+  // requests. A document cookie is unique for each print query, thus can be a
+  // key for print requests.
+  base::flat_map<int, mojom::PdfCompositorPtr> compositor_map_;
 
   DISALLOW_COPY_AND_ASSIGN(PrintCompositeClient);
 };
