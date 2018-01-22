379deeb790ddbb9287f0a19ef3089f50855bc95e
Tommi <tommi@chromium.org>
2018-01-22 19:36:15 +0100

    Add rtc::ScopedAllowBaseSyncPrimitives for WebRTC.
    This class links together Chromium's ScopedAllowBaseSyncPrimitives and WebRTCs.
    Now that WebRTC is starting to use Chromium's sync primitives, this allows
    us to continue reduce locking inside of WebRTC that can cause slowdown
    on Chromium threads, while guarding against regressions.
    
    Bug: 796889, 795340
    Change-Id: Icd71c13e04433aa67e3f22f5d510aa724b413fa9

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -119,6 +119,10 @@ namespace viz {
 class ServerGpuMemoryBufferManager;
 }
 
+namespace webrtc {
+class DesktopConfigurationMonitor;
+}
+
 namespace base {
 
 namespace android {
@@ -270,6 +274,7 @@ class BASE_EXPORT ScopedAllowBaseSyncPrimitives {
   friend class net::MultiThreadedCertVerifierScopedAllowBaseSyncPrimitives;
   friend class rlz_lib::FinancialPing;
   friend class shell_integration::LaunchXdgUtilityScopedAllowBaseSyncPrimitives;
+  friend class webrtc::DesktopConfigurationMonitor;
 
   ScopedAllowBaseSyncPrimitives() EMPTY_BODY_IF_DCHECK_IS_OFF;
   ~ScopedAllowBaseSyncPrimitives() EMPTY_BODY_IF_DCHECK_IS_OFF;
