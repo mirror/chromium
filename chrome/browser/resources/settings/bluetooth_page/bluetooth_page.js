df30ccbfdef7cb93eca6f32b30b03327c97ec9cf
Sonny Sasaka <sonnysasaka@chromium.org>
2018-01-22 13:33:04 -0800

    Fix toggleable disabled bluetooth button.
    Currently the bluetooth toggle is toggleable even though it's disabled
    (i.e. property disabled=true and grayed). This is due to onTap_ function
    that overrides the disabled property. This CL fixes this by asking
    onTap_ function to do nothing if the button is disabled.
    
    BUG=795190
    TEST=Simulate delay of bluetooth power change, open the settings page
    and click the bluetooth toggle button twice rapidly, the second click
    should be ignored.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ieb172495f76e543957bdebde179dccf89dbac1a2

--- chrome/browser/resources/settings/bluetooth_page/bluetooth_page.js
+++ chrome/browser/resources/settings/bluetooth_page/bluetooth_page.js
@@ -172,7 +172,8 @@ Polymer({
 
   /** @private */
   onTap_: function() {
-    if (this.adapterState_.available === false)
+    if (this.adapterState_.available === false ||
+        this.bluetoothToggleDisabled_ === true)
       return;
     if (!this.bluetoothToggleState_)
       this.bluetoothToggleState_ = true;
@@ -206,6 +207,10 @@ Polymer({
     this.bluetoothToggleDisabled_ = true;
     this.bluetoothPrivate.setAdapterState(
         {powered: this.bluetoothToggleState_}, () => {
+          // Always resets the button's disabled property when callback is
+          // called regardless of error or success.
+          this.bluetoothToggleDisabled_ = !this.adapterState_.available;
+
           const error = chrome.runtime.lastError;
           if (error && error != 'Error setting adapter properties: powered') {
             console.error('Error enabling bluetooth: ' + error.message);
