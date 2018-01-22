b4d3a2adff75f5742ca10cef73e53f6a5466e516
Xi Cheng <chengx@chromium.org>
2018-01-21 21:25:55 -0800

    Introduce chrome_server.exe to handle Win10 toast activation
    We are bringing the Windows 10 native notification feature to Chrome. Apart from
    displaying the notifications in Windows action center, we want Chrome capable of
    responding to user interaction on those notifications. In addition, now that toast
    notifications can stay in action center for days until the user sees them, we want
    Chrome to be activated at any time from a toast. So instead of requiring Chrome to
    use an event handler for activation (IToastNotification::add_Activated) that only
    works while Chrome is running, we need to introduce a COM server to invoke Chrome
    when users interact with the notifications when Chrome is not running.
    
    This CL implements this requirement. It consists of the following main changes:
    1) Introduce a new EXE: chrome_server.exe. It servers as a COM server which is
    invoked whenever there is activation from toast notifications.
    2) Implement the INotificationActivationCallBack interface in a new class named
    NotificationActivator.
    3) Each time chrome_server.exe is invoked, it registers the NotificationActivator
    class object. This enables Windows to know the NotificationActivator class, so that
    Windows will create an object of this class and call its Activate() member function
    to handle toast activations when necessary.
    4) The Activate() method launches Chrome if it is not running.
    5) Register chrome_server.exe to Windows via registry when installing Chrome, and
    unregister it when uninstalling Chrome.
    
    Bug: 734095
    Change-Id: Iacd9fb8bdb69f88667c7d81eade448e7a4d1e4ac

---
+++ chrome_server/notification_activator.h
@@ -0,0 +1,113 @@
+// Copyright (c) 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_SERVER_NOTIFICATION_ACTIVATOR_H_
+#define CHROME_SERVER_NOTIFICATION_ACTIVATOR_H_
+
+#include "windows.h"
+
+#include <NotificationActivationCallback.h>
+#include <wrl.h>
+
+#include "base/logging.h"
+#include "chrome/install_static/install_util.h"
+
+namespace mswr = Microsoft::WRL;
+
+using Microsoft::WRL::ComPtr;
+
+// A Win32 component that participates with Action Center will need to create a
+// COM component that exposes the INotificationActivationCallback interface.
+class NotificationActivator
+    : public mswr::RuntimeClass<mswr::RuntimeClassFlags<mswr::ClassicCom>,
+                                INotificationActivationCallback> {
+ public:
+  NotificationActivator();
+  ~NotificationActivator() override;
+
+  IFACEMETHODIMP Activate(LPCWSTR /* appUserModelId */,
+                          LPCWSTR /* invokedArgs */,
+                          const NOTIFICATION_USER_INPUT_DATA* /* data */,
+                          ULONG /* count */) override;
+};
+
+// This class is used to keep track of registering and unregistering the
+// NotificationActivator COM object.
+class ActivatorRegisterer {
+ public:
+  ActivatorRegisterer() = default;
+
+  ~ActivatorRegisterer() {
+    // Unregister the NotificationActivator class object if it is now
+    // registered.
+    if (!has_registered_)
+      return;
+    mswr::Module<mswr::OutOfProc>& module =
+        mswr::Module<mswr::OutOfProc>::GetModule();
+    HRESULT hr = module.UnregisterCOMObject(nullptr, &cookies_, 1);
+    if (FAILED(hr))
+      LOG(ERROR) << "NotificationActivator unregistration failed " << hr;
+
+    has_registered_ = false;
+  }
+
+  // Registers the NotificationActivator COM object so other applications can
+  // connect to it.
+  HRESULT Run() {
+    // Usually COM module classes statically define their CLSID at compile time
+    // through the use of various macros, and Windows internals takes care of
+    // creating the class objects and registering them.  However, we need to
+    // register the same object with different CLSIDs depending on a runtime
+    // setting, so we handle that logic here.
+    ComPtr<IUnknown> factory;
+    unsigned int flags = mswr::ModuleType::OutOfProc;
+    mswr::Details::FactoryCache factory_cache;
+    mswr::Details::CreatorMap entry = {
+        ::mswr::Details::CreateClassFactory<
+            ::mswr::SimpleClassFactory<NotificationActivator>>,
+        {&install_static::GetToastActivatorClsid()},
+        nullptr,
+        &factory_cache,
+        nullptr};
+    HRESULT hr =
+        entry.factoryCreator(&flags, &entry, __uuidof(IClassFactory), &factory);
+    if (FAILED(hr)) {
+      LOG(ERROR) << "Factory creation failed " << hr;
+      return hr;
+    }
+
+    IID class_ids[] = {*entry.activationId.clsid};
+    ComPtr<IClassFactory> class_factory;
+    hr = factory.As(&class_factory);
+    if (FAILED(hr))
+      return hr;
+
+    // Create a Out-of-Process COM module and register the NotificationActivator
+    // class object to it.
+    mswr::Module<mswr::OutOfProc>::Create([] {});
+    mswr::Module<mswr::OutOfProc>& module =
+        mswr::Module<mswr::OutOfProc>::GetModule();
+
+    hr = module.RegisterCOMObject(nullptr, class_ids,
+                                  class_factory.GetAddressOf(), &cookies_, 1);
+    if (FAILED(hr)) {
+      LOG(ERROR) << "NotificationActivator registration failed " << hr;
+      return hr;
+    }
+
+    has_registered_ = true;
+    return hr;
+  }
+
+ private:
+  // A boolean flag indicating if the NotificationActivator class object has
+  // registered or not.
+  bool has_registered_ = false;
+
+  // An array of pointers to values that identify the class objects that were
+  // registered or to be unregistered.
+  DWORD cookies_;
+};
+
+#endif  // CHROME_SERVER_NOTIFICATION_ACTIVATOR_H_
