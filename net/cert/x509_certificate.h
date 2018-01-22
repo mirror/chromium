d5fabe9444bb1ecd3d58cefce5829fa6d1424b4e
Ryan Sleevi <rsleevi@chromium.org>
2018-01-22 17:11:45 -0500

    Remove Common Name Fallback code
    Use of the common name for matching the hostname is fundamentally
    insecure, as it allows for the bypass of security controls within
    certificates. Remove the remaining aspects of supporting logic that
    allow common name fallback, which were enabled by Enterprise Policy,
    now that the Enterprise Policy has been removed.
    
    BUG: 804349
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester;master.tryserver.chromium.mac:ios-simulator-cronet
    Change-Id: I6e595f106b0d989935c9edc0d4c02e9a0f0f4584

--- net/cert/x509_certificate.h
+++ net/cert/x509_certificate.h
@@ -199,14 +199,10 @@ class NET_EXPORT X509Certificate
   // Verifies that |hostname| matches this certificate.
   // Does not verify that the certificate is valid, only that the certificate
   // matches this host.
-  // If |allow_common_name_fallback| is set to true, and iff no SANs are
-  // present of type dNSName or iPAddress, then fallback to using the
-  // certificate's commonName field in the Subject.
-  bool VerifyNameMatch(const std::string& hostname,
-                       bool allow_common_name_fallback) const;
+  bool VerifyNameMatch(const std::string& hostname) const;
 
-  // Returns the PEM encoded data from a DER encoded certificate. If the return
-  // value is true, then the PEM encoded certificate is written to
+  // Returns the PEM encoded data from a DER encoded certificate. If the
+  // return value is true, then the PEM encoded certificate is written to
   // |pem_encoded|.
   static bool GetPEMEncodedFromDER(base::StringPiece der_encoded,
                                    std::string* pem_encoded);
@@ -294,20 +290,14 @@ class NET_EXPORT X509Certificate
   // Verifies that |hostname| matches one of the certificate names or IP
   // addresses supplied, based on TLS name matching rules - specifically,
   // following http://tools.ietf.org/html/rfc6125.
-  // |cert_common_name| is the Subject CN, e.g. from X509Certificate::subject().
   // The members of |cert_san_dns_names| and |cert_san_ipaddrs| must be filled
   // from the dNSName and iPAddress components of the subject alternative name
   // extension, if present. Note these IP addresses are NOT ascii-encoded:
   // they must be 4 or 16 bytes of network-ordered data, for IPv4 and IPv6
   // addresses, respectively.
-  // If |allow_common_name_fallback| is true, then the |cert_common_name| will
-  // be used if the |cert_san_dns_names| and |cert_san_ip_addrs| parameters are
-  // empty.
   static bool VerifyHostname(const std::string& hostname,
-                             const std::string& cert_common_name,
                              const std::vector<std::string>& cert_san_dns_names,
-                             const std::vector<std::string>& cert_san_ip_addrs,
-                             bool allow_common_name_fallback);
+                             const std::vector<std::string>& cert_san_ip_addrs);
 
   // The subject of the certificate.
   CertPrincipal subject_;
