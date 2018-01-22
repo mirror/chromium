2e8a5cc2d2678bc04e8527df524073a13ebab3b2
Scott Chen <scottchen@chromium.org>
2018-01-22 15:29:26 -0800

    Settings [DICE]: Add a link row to go to /manageProfile.
    This CL makes the "diceEnabled" boolean available in loadTimeData, and
    uses it to determine whether or not to show a new link row that opens
    the /manageProfile subpage. This change only applies to non-chromeOS builds.
    
    Bug: 800968
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I021e5c62ba214b609313682644a35d64c80d6170

--- chrome/browser/ui/webui/settings/md_settings_ui.cc
+++ chrome/browser/ui/webui/settings/md_settings_ui.cc
@@ -43,6 +43,7 @@
 #include "chrome/grit/settings_resources_map.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "components/pref_registry/pref_registry_syncable.h"
+#include "components/signin/core/browser/profile_management_switches.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -222,7 +223,6 @@ MdSettingsUI::MdSettingsUI(content::WebUI* web_ui)
   // should never change while Chrome is open.
   html_source->AddBoolean("userInitiatedCleanupsEnabled",
                           userInitiatedCleanupsEnabled);
-
 #endif  // defined(OS_WIN)
 
   bool password_protection_available = false;
@@ -281,6 +281,9 @@ MdSettingsUI::MdSettingsUI(content::WebUI* web_ui)
     AddSettingsPageUIHandler(std::make_unique<chromeos::settings::PowerHandler>(
         profile->GetPrefs()));
   }
+#else
+  html_source->AddBoolean("diceEnabled",
+                          signin::IsDiceEnabledForProfile(profile->GetPrefs()));
 #endif
 
   html_source->AddBoolean("showExportPasswords",
