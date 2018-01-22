d74c3e7c525c0560e31609d092e39a7fdb7f6065
Gauthier Ambard <gambard@chromium.org>
2018-01-22 15:06:23 +0100

    Add navigation history popup to adaptive toolbar
    This CL adds triggers for the navigation history popup displayed when
    the user long presses on the back/forward buttons.
    
    Bug: 803373,802034
    Change-Id: Ie9b6f8ca7b1193fc27d52b2c91bcfabb6fb51af0

--- ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view_controller.mm
+++ ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view_controller.mm
@@ -5,7 +5,10 @@
 #import "ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view_controller.h"
 
 #import "base/logging.h"
+#import "ios/chrome/browser/ui/commands/browser_commands.h"
+#import "ios/chrome/browser/ui/history_popup/requirements/tab_history_constants.h"
 #import "ios/chrome/browser/ui/toolbar/adaptive/primary_toolbar_view.h"
+#import "ios/chrome/browser/ui/toolbar/clean/toolbar_button.h"
 #import "ios/chrome/browser/ui/toolbar/clean/toolbar_constants.h"
 #import "ios/chrome/browser/ui/toolbar/clean/toolbar_tools_menu_button.h"
 #import "ios/chrome/browser/ui/util/named_guide.h"
@@ -39,13 +42,67 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.view.toolsMenuButton.guideName = kTabSwitcherGuide;
-  self.view.toolsMenuButton.constraintPriority = kPrimaryToolbarButtonPriority;
+  self.view.toolsMenuButton.constraintPriority =
+      kPrimaryToolbarTrailingButtonPriority;
+  self.view.forwardLeadingButton.guideName = kForwardButtonGuide;
+  self.view.forwardLeadingButton.constraintPriority =
+      kPrimaryToolbarLeadingButtonPriority;
+  self.view.forwardTrailingButton.guideName = kForwardButtonGuide;
+  self.view.forwardTrailingButton.constraintPriority =
+      kPrimaryToolbarTrailingButtonPriority;
+  self.view.backButton.guideName = kBackButtonGuide;
+  self.view.backButton.constraintPriority =
+      kPrimaryToolbarLeadingButtonPriority;
+
+  // Add navigation popup menu triggers.
+  [self addLongPressGestureToView:self.view.backButton];
+  [self addLongPressGestureToView:self.view.forwardLeadingButton];
+  [self addLongPressGestureToView:self.view.forwardTrailingButton];
 }
 
 #pragma mark - Property accessors
 
 - (void)setLocationBarView:(UIView*)locationBarView {
   self.view.locationBarView = locationBarView;
+}
+
+#pragma mark - TabHistoryUIUpdater
+
+- (void)updateUIForTabHistoryPresentationFrom:(ToolbarButtonType)buttonType {
+  if (buttonType == ToolbarButtonTypeBack) {
+    self.view.backButton.selected = YES;
+  } else {
+    self.view.forwardLeadingButton.selected = YES;
+    self.view.forwardTrailingButton.selected = YES;
+  }
+}
+
+- (void)updateUIForTabHistoryWasDismissed {
+  self.view.backButton.selected = NO;
+  self.view.forwardLeadingButton.selected = NO;
+  self.view.forwardTrailingButton.selected = NO;
+}
+
+#pragma mark - Private
+
+- (void)addLongPressGestureToView:(UIView*)view {
+  UILongPressGestureRecognizer* navigationHistoryLongPress =
+      [[UILongPressGestureRecognizer alloc]
+          initWithTarget:self
+                  action:@selector(handleLongPress:)];
+  [view addGestureRecognizer:navigationHistoryLongPress];
+}
+
+- (void)handleLongPress:(UILongPressGestureRecognizer*)gesture {
+  if (gesture.state != UIGestureRecognizerStateBegan)
+    return;
+
+  if (gesture.view == self.view.backButton) {
+    [self.dispatcher showTabHistoryPopupForBackwardHistory];
+  } else if (gesture.view == self.view.forwardLeadingButton ||
+             gesture.view == self.view.forwardTrailingButton) {
+    [self.dispatcher showTabHistoryPopupForForwardHistory];
+  }
 }
 
 @end
