6ce7b0610498292a7b9a4e15c31f2c231a60c5ad
Stefan Zager <szager@chromium.org>
2018-01-23 13:09:34 -0800

    [RootLayerScrolls] Defer visibility check for viewport-fixed objects
    In non-RLS mode, InvalidateViewportConstrainedObjects is only called
    from ScrollContentsIfNeededRecursive, which is called during lifecycle
    update *after* compositing inputs are updated, during which PaintLayer::
    UpdateDescendantDependentFlags runs, so it's safe to query
    SubtreeIsInvisible.  But in RLS mode, this method is called whenever
    the root layer is programmatically scrolled, which can happen when we're
    not compositing clean.
    
    So, just use SetMayNeedPaintInvalidationSubtree to defer the work until
    paint invalidation.
    
    This fixes DCHECK failures in browser_tests with RLS enabled.
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I5f702afc31ee929d34b2893c632f29d813598ff7

--- third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/paint/PaintLayerScrollableArea.cpp
@@ -544,8 +544,8 @@ void PaintLayerScrollableArea::InvalidatePaintForScrollOffsetChange(
   }
 
   if (requires_paint_invalidation) {
-    GetLayoutBox()
-        ->SetShouldDoFullPaintInvalidationIncludingNonCompositingDescendants();
+    GetLayoutBox()->SetShouldDoFullPaintInvalidation();
+    GetLayoutBox()->SetMayNeedPaintInvalidationSubtree();
   }
 }
 
