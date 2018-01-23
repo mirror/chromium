4d7341e24766e117a03c93ae567b9e58aad7f8e1
Chris Harrelson <chrishtr@chromium.org>
2018-01-22 20:27:05 -0800

    [OOPIF] Implement intrinsic size update for remote frames.
    When IntrinsicSizingInfo of a remote frame changes in a way
    that can affect remote parent frames' layout, send an IPC
    from the child renderer to the browser process, and from
    there to the parent renderer with the new IntrinsicSizingInfo.
    
    Bug:798974
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I150156708faa566c656c3f7a624ef57f1dba1235

--- third_party/WebKit/Source/core/frame/FrameOwner.h
+++ third_party/WebKit/Source/core/frame/FrameOwner.h
@@ -46,7 +46,7 @@ class CORE_EXPORT FrameOwner : public GarbageCollectedMixin {
 
   // The intrinsic dimensions of the embedded object changed. This is relevant
   // for SVG documents that are embedded via <object> or <embed>.
-  virtual void IntrinsicDimensionsChanged() = 0;
+  virtual void IntrinsicSizingInfoChanged() = 0;
 
   // Returns the 'name' content attribute value of the browsing context
   // container.
@@ -84,7 +84,7 @@ class CORE_EXPORT DummyFrameOwner final
   void DispatchLoad() override {}
   bool CanRenderFallbackContent() const override { return false; }
   void RenderFallbackContent() override {}
-  void IntrinsicDimensionsChanged() override {}
+  void IntrinsicSizingInfoChanged() override {}
   AtomicString BrowsingContextContainerName() const override {
     return AtomicString();
   }
