0562e7684dde59d69f92524bc886c021fb470d3d
danakj <danakj@chromium.org>
2018-01-23 13:25:09 -0500

    Remove the log message for media stream counter being 0.
    When glXGetSyncValuesOML is true, we expected this counter to be non-0
    and added this error to find bugs, but it has become standard behaviour
    so this log is only generating noise.
    
    R=kbr@chromium.org
    
    Bug: 772844
    Change-Id: I38377c74d45197cf33b2caf3cf81239075f8cae8

--- ui/gl/sync_control_vsync_provider.cc
+++ ui/gl/sync_control_vsync_provider.cc
@@ -67,11 +67,8 @@ bool SyncControlVSyncProvider::GetVSyncParametersIfAvailable(
   // associated with the surface. crbug.com/231945
   bool prev_invalid_msc = invalid_msc_;
   invalid_msc_ = (media_stream_counter == 0);
-  if (invalid_msc_) {
-    LOG_IF(ERROR, !prev_invalid_msc) << "glXGetSyncValuesOML "
-        "should not return TRUE with a media stream counter of 0.";
+  if (invalid_msc_)
     return false;
-  }
 
   struct timespec real_time;
   clock_gettime(CLOCK_REALTIME, &real_time);
