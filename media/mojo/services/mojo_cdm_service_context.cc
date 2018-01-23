e39c6393ded473401a2a2e059dc9fec977e2f2b7
Xiaohan Wang <xhwang@chromium.org>
2018-01-22 16:52:43 -0800

    media: Add CdmContext support for MojoCdmProxyService
    This CL connects the CdmProxy's CdmContext with the media components,
    e.g. audio/video decoder. If the CdmProxy is destroyed (e.g. on
    connection error), all the CdmContext calls will fail.
    
    Details:
    - Update CdmContext to support GetDecryptContext().
    - Update CdmProxy to support CdmContext.
    - Add CdmProxyContextRef to support weak CdmContext pointers.
    - Let MojoCdmServiceContext generates CDM ID, to make sure there's no
      duplicate CDM IDs among CDMs and CdmProxies.
    - Update MojoCdmServiceContext to support CdmProxy.
    - Also update MojoCdmService constructor parameter order to match other
      classes that use MojoCdmServiceContext.
    
    BUG=803710
    
    Change-Id: I055e740de0e71790e364d48aca05a067b123a14f

--- media/mojo/services/mojo_cdm_service_context.cc
+++ media/mojo/services/mojo_cdm_service_context.cc
@@ -5,21 +5,62 @@
 #include "media/mojo/services/mojo_cdm_service_context.h"
 
 #include "base/logging.h"
+#include "media/base/cdm_context.h"
 #include "media/base/content_decryption_module.h"
 #include "media/cdm/cdm_context_ref_impl.h"
 #include "media/mojo/services/mojo_cdm_service.h"
 
+#if BUILDFLAG(ENABLE_LIBRARY_CDMS)
+#include "media/mojo/services/mojo_cdm_proxy_service.h"
+#endif  // BUILDFLAG(ENABLE_LIBRARY_CDMS)
+
 namespace media {
+
+#if BUILDFLAG(ENABLE_LIBRARY_CDMS)
+
+namespace {
+
+class CdmProxyContextRef : public CdmContextRef, public CdmContext {
+ public:
+  explicit CdmProxyContextRef(base::WeakPtr<CdmContext> cdm_context)
+      : cdm_context_(cdm_context) {}
+  ~CdmProxyContextRef() final {}
+
+  // CdmContextRef implementation.
+  CdmContext* GetCdmContext() final { return this; }
+
+ private:
+  // CdmContext implementation.
+  bool GetDecryptContext() final {
+    DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
+
+    if (!cdm_context_)
+      return false;
+
+    return cdm_context_->GetDecryptContext();
+  }
+
+  base::WeakPtr<CdmContext> cdm_context_;
+  THREAD_CHECKER(thread_checker_);
+
+  DISALLOW_COPY_AND_ASSIGN(CdmProxyContextRef);
+};
+
+}  // namespace
+
+#endif  // BUILDFLAG(ENABLE_LIBRARY_CDMS)
+
+int MojoCdmServiceContext::next_cdm_id_ = CdmContext::kInvalidCdmId + 1;
 
 MojoCdmServiceContext::MojoCdmServiceContext() = default;
 
 MojoCdmServiceContext::~MojoCdmServiceContext() = default;
 
-void MojoCdmServiceContext::RegisterCdm(int cdm_id,
-                                        MojoCdmService* cdm_service) {
-  DCHECK(!cdm_services_.count(cdm_id));
+int MojoCdmServiceContext::RegisterCdm(MojoCdmService* cdm_service) {
   DCHECK(cdm_service);
+  int cdm_id = next_cdm_id_++;
   cdm_services_[cdm_id] = cdm_service;
+  return cdm_id;
 }
 
 void MojoCdmServiceContext::UnregisterCdm(int cdm_id) {
@@ -27,21 +68,44 @@ void MojoCdmServiceContext::UnregisterCdm(int cdm_id) {
   cdm_services_.erase(cdm_id);
 }
 
+#if BUILDFLAG(ENABLE_LIBRARY_CDMS)
+int MojoCdmServiceContext::RegisterCdmProxy(
+    MojoCdmProxyService* cdm_proxy_service) {
+  DCHECK(cdm_proxy_service);
+  int cdm_id = next_cdm_id_++;
+  cdm_proxy_services_[cdm_id] = cdm_proxy_service;
+  return cdm_id;
+}
+
+void MojoCdmServiceContext::UnregisterCdmProxy(int cdm_id) {
+  DCHECK(cdm_proxy_services_.count(cdm_id));
+  cdm_proxy_services_.erase(cdm_id);
+}
+#endif  // BUILDFLAG(ENABLE_LIBRARY_CDMS)
+
 std::unique_ptr<CdmContextRef> MojoCdmServiceContext::GetCdmContextRef(
     int cdm_id) {
+  // Check all CDMs first.
   auto cdm_service = cdm_services_.find(cdm_id);
-  if (cdm_service == cdm_services_.end()) {
-    // This could happen when called after the actual CDM has been destroyed.
-    DVLOG(1) << "CDM service not found: " << cdm_id;
-    return nullptr;
+  if (cdm_service != cdm_services_.end()) {
+    if (!cdm_service->second->GetCdm()->GetCdmContext()) {
+      NOTREACHED() << "All CDMs should support CdmContext.";
+      return nullptr;
+    }
+    return std::make_unique<CdmContextRefImpl>(cdm_service->second->GetCdm());
   }
 
-  if (!cdm_service->second->GetCdm()->GetCdmContext()) {
-    NOTREACHED() << "All CDMs should support CdmContext.";
-    return nullptr;
+#if BUILDFLAG(ENABLE_LIBRARY_CDMS)
+  // Next check all CdmProxies.
+  auto cdm_proxy_service = cdm_proxy_services_.find(cdm_id);
+  if (cdm_proxy_service != cdm_proxy_services_.end()) {
+    return std::make_unique<CdmProxyContextRef>(
+        cdm_proxy_service->second->GetCdmContext());
   }
+#endif  // BUILDFLAG(ENABLE_LIBRARY_CDMS)
 
-  return std::make_unique<CdmContextRefImpl>(cdm_service->second->GetCdm());
+  LOG(ERROR) << "CdmContextRef cannot be obtained for CDM ID: " << cdm_id;
+  return nullptr;
 }
 
 }  // namespace media
