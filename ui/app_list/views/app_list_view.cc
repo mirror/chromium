66fea7d4ca8bd741bc300e91d11e75c1e5bfeb64
Alex Newcomer <newcomer@chromium.org>
2018-01-23 15:06:29 -0800

    cros: Stop transform animations when dragging AppList from shelf
    This CL stops all TRANSFORM animations when
    AppListView::UpdateYPositionAndOpacity is called.
    
    This fixes the bug where when dragging up from the shelf to show
    the launcher with a fullscreen window open, the launcher would
    animate over 200ms from the drag start point to the height of
    the shelf (manifesting as a white flash).
    
    Bug: 804019
    Change-Id: I830701a4609d37a0ea49ba77ace51d300c73ace5

--- ui/app_list/views/app_list_view.cc
+++ ui/app_list/views/app_list_view.cc
@@ -1182,6 +1182,12 @@ void AppListView::UpdateYPositionAndOpacity(int y_position_in_screen,
   if (app_list_state_ == AppListViewState::CLOSED)
     return;
 
+  if (fullscreen_widget_->GetLayer()->GetAnimator()->IsAnimatingProperty(
+          ui::LayerAnimationElement::TRANSFORM)) {
+    fullscreen_widget_->GetLayer()->GetAnimator()->StopAnimatingProperty(
+        ui::LayerAnimationElement::TRANSFORM);
+  }
+
   SetIsInDrag(true);
   background_opacity_ = background_opacity;
   gfx::Rect new_widget_bounds = fullscreen_widget_->GetWindowBoundsInScreen();
