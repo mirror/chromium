f40e9a25216d233547771716945b65631301d205
tmartino <tmartino@chromium.org>
2018-01-23 13:12:18 -0500

    [Autofill Views] Adding events and styling
    Bug:
    Change-Id: Ie6a733b392ae33ef557e20fcc43ddd05c9d06f6b

--- chrome/browser/ui/views/autofill/autofill_popup_view_native_views.h
+++ chrome/browser/ui/views/autofill/autofill_popup_view_native_views.h
@@ -10,9 +10,51 @@
 #include "chrome/browser/ui/autofill/autofill_popup_view.h"
 #include "chrome/browser/ui/views/autofill/autofill_popup_base_view.h"
 
+#include <vector>
+
+namespace views {
+class Label;
+}
+
 namespace autofill {
 
 class AutofillPopupController;
+
+// Child view representing one row (i.e., one suggestion) in the Autofill
+// Popup.
+class AutofillPopupChildNativeView : public views::View {
+ public:
+  AutofillPopupChildNativeView(AutofillPopupController* controller,
+                               int line_number);
+
+  ~AutofillPopupChildNativeView() override {}
+
+  void AcceptSelection();
+  void SetStyle(bool is_selected);
+
+  // views::View:
+  // TODO(tmartino): Consolidate and deprecate code in AutofillPopupBaseView
+  // where overlap exists with these events.
+  void OnMouseEntered(const ui::MouseEvent& event) override;
+  void OnMouseReleased(const ui::MouseEvent& event) override;
+
+  bool OnMouseDragged(const ui::MouseEvent& event) override;
+  bool OnMousePressed(const ui::MouseEvent& event) override;
+
+ private:
+  // views::View:
+  void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
+  void CreateContent();
+
+  AutofillPopupController* controller_;
+  views::Label* first_line_;
+  views::Label* second_line_;
+  int line_number_;
+  bool is_separator_;
+  bool is_warning_;
+
+  DISALLOW_COPY_AND_ASSIGN(AutofillPopupChildNativeView);
+};
 
 // Views implementation for the autofill and password suggestion.
 // TODO(https://crbug.com/768881): Once this implementation is complete, this
@@ -39,8 +81,14 @@ class AutofillPopupViewNativeViews : public AutofillPopupBaseView,
   // Creates child views based on the suggestions given by |controller_|.
   void CreateChildViews();
 
+  // AutofillPopupBaseView:
+  gfx::Rect GetUpdatedBounds() override;
+  void DoUpdateBoundsAndRedrawPopup() override;
+
   // Controller for this view.
   AutofillPopupController* controller_;
+
+  std::vector<AutofillPopupChildNativeView*> rows_;
 
   DISALLOW_COPY_AND_ASSIGN(AutofillPopupViewNativeViews);
 };
