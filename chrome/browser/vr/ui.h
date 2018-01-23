650d6a657dd4f03263fa77d6cb64b57ddd860694
Yash Malik <ymalik@google.com>
2018-01-23 11:28:45 -0500

    VR: Show splash screen for a minimum duration
    Before this CL, we would hide the splash screen as soon as we got the first
    frame WebVR frame. Although TransientElement had a notion of "minimum duration",
    it didn't really work because we stop drawing non-overalay 2D elements in
    WebVR mode.
    
    This CL fixes this by pausing WebVR VSyncs until the minimum duration for the
    splash screen is passed.
    
    Bug: 801639
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: If2580a97e714a17de5445fba85ab0aa6b487964b

--- chrome/browser/vr/ui.h
+++ chrome/browser/vr/ui.h
@@ -98,6 +98,12 @@ class Ui : public BrowserUiInterface, public KeyboardUiInterface {
 
   void OnAssetsLoading();
   bool ShouldRenderWebVr();
+  // TODO(ymalik): We expose this to stop sending VSync to the WebVR page until
+  // the splash screen has been visible for its minimum duration. The visibility
+  // logic currently lives in the UI, and it'd be much cleaner if the UI didn't
+  // have to worry about this, and if it were told to hide the splash screen
+  // like other WebVR phases (e.g. OnWebVrFrameAvailable below).
+  bool CanSendWebVrVSync();
   void OnGlInitialized(unsigned int content_texture_id,
                        UiElementRenderer::TextureLocation content_location,
                        bool use_ganesh);
