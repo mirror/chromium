be22970dfc8d6219340c417e19003b00db5eed7c
Tsuyoshi Horo <horo@chromium.org>
2018-01-23 15:53:00 +0900

    [Not for review] OriginSignedResponseHandler
    Change-Id: I53d397d7cdfe79d48d6891db65ab1a1e12100b92

---
+++ third_party/WebKit/LayoutTests/http/tests/loading/wpk/resources/origin-signed-response-iframe.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: application/http-exchange+cbor');
+
+header('OriginSignedResponseUrlForDemo: https://example.com/test.html');
+?>
+<!DOCTYPE html>
+
+<body>
+<script>
+
+window.addEventListener('message', (event) => {
+  event.data.port.postMessage({location: document.location.href});
+}, false);
+
+</script>
+hello<br>
+
+<?php
+
+  ob_flush();
+  flush();
+  usleep(500000);
+
+?>
+world
+</body>
