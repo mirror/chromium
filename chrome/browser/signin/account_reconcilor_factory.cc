12660e666cf2f2edf47c1b531bb30ffba9f04b4f
Kush Sinha <sinhak@chromium.org>
2018-01-23 18:15:02 +0100

    [TEST ONLY] Run tests for
    Add Mirror timeout behaviour on Chrome OS
    
    Unicorn accounts on Chrome OS will be forcibly logged out if account
    reconciliation timeout is reached.
    Non-Unicorn accounts will maintain their existing behaviour.
    
    Timer should be started before any abort conditions
    
    
    Fix timer stoppage bug
    
    
    Add support for handling timeouts during account reconciliation
    
    Change-Id: If4ee2544511772df12aa0d2f0c30a43015155e12

--- chrome/browser/signin/account_reconcilor_factory.cc
+++ chrome/browser/signin/account_reconcilor_factory.cc
@@ -4,9 +4,11 @@
 
 #include "chrome/browser/signin/account_reconcilor_factory.h"
 
+#include <memory>
 #include <utility>
 
 #include "base/logging.h"
+#include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/account_consistency_mode_manager.h"
 #include "chrome/browser/signin/chrome_signin_client_factory.h"
@@ -18,11 +20,33 @@
 #include "components/signin/core/browser/account_reconcilor_delegate.h"
 #include "components/signin/core/browser/mirror_account_reconcilor_delegate.h"
 #include "components/signin/core/browser/profile_management_switches.h"
+#include "components/signin/core/browser/reconcilor_delegate_helper.h"
 #include "components/signin/core/browser/signin_features.h"
+
+#if defined(OS_CHROMEOS)
+#include "components/user_manager/user_manager.h"
+#endif
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
 #include "components/signin/core/browser/dice_account_reconcilor_delegate.h"
 #endif
+
+namespace {
+
+#if defined(OS_CHROMEOS)
+class Helper : public ReconcilorDelegateHelper {
+  void ForceUserOnlineSignIn() override {
+    const user_manager::User* primary_user =
+        user_manager::UserManager::Get()->GetPrimaryUser();
+    user_manager::UserManager::Get()->SaveForceOnlineSignin(
+        primary_user->GetAccountId(), true /* force_online_signin */);
+  }
+
+  void AttemptUserExit() override { chrome::AttemptUserExit(); }
+};
+#endif  // defined(OS_CHROMEOS)
+
+}  // namespace
 
 AccountReconcilorFactory::AccountReconcilorFactory()
     : BrowserContextKeyedServiceFactory(
@@ -64,16 +88,24 @@ KeyedService* AccountReconcilorFactory::BuildServiceInstanceFor(
 // static
 std::unique_ptr<signin::AccountReconcilorDelegate>
 AccountReconcilorFactory::CreateAccountReconcilorDelegate(Profile* profile) {
+#if defined(OS_CHROMEOS)
+  std::unique_ptr<ReconcilorDelegateHelper> helper = std::make_unique<Helper>();
+#else
+  std::unique_ptr<ReconcilorDelegateHelper> helper =
+      std::make_unique<ReconcilorDelegateHelper>();
+#endif
   if (AccountConsistencyModeManager::IsMirrorEnabledForProfile(profile)) {
     return std::make_unique<signin::MirrorAccountReconcilorDelegate>(
-        SigninManagerFactory::GetForProfile(profile));
+        SigninManagerFactory::GetForProfile(profile), std::move(helper),
+        profile->IsChild());
   }
   // TODO(droger): Remove this switch case. |AccountConsistencyModeManager| is
   // the source of truth.
   switch (signin::GetAccountConsistencyMethod()) {
     case signin::AccountConsistencyMethod::kMirror:
       return std::make_unique<signin::MirrorAccountReconcilorDelegate>(
-          SigninManagerFactory::GetForProfile(profile));
+          SigninManagerFactory::GetForProfile(profile), std::move(helper),
+          profile->IsChild());
     case signin::AccountConsistencyMethod::kDisabled:
     case signin::AccountConsistencyMethod::kDiceFixAuthErrors:
       return std::make_unique<signin::AccountReconcilorDelegate>();
