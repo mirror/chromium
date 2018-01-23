51a37319204c5e28f5e8a969a5efefa32e9c2167
Kush Sinha <sinhak@chromium.org>
2018-01-23 21:05:15 +0100

    Set Mirror delegate timeout behavior for Unicorn on Chrome OS
    Unicorn accounts on Chrome OS will be forcibly logged out if account
    reconciliation timeout is reached.
    Non-Unicorn accounts will maintain their existing behavior.
    
    Bug: 795808
    Change-Id: I9226d9d4090e8a331db7968a2047a6be0561818b

--- ios/chrome/browser/signin/account_reconcilor_factory.cc
+++ ios/chrome/browser/signin/account_reconcilor_factory.cc
@@ -5,11 +5,13 @@
 #include "ios/chrome/browser/signin/account_reconcilor_factory.h"
 
 #include <memory>
+#include <utility>
 
 #include "base/memory/singleton.h"
 #include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "components/signin/core/browser/account_reconcilor.h"
 #include "components/signin/core/browser/mirror_account_reconcilor_delegate.h"
+#include "components/signin/core/browser/reconcilor_delegate_helper.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/signin/gaia_cookie_manager_service_factory.h"
 #include "ios/chrome/browser/signin/oauth2_token_service_factory.h"
@@ -48,13 +50,16 @@ std::unique_ptr<KeyedService> AccountReconcilorFactory::BuildServiceInstanceFor(
       ios::ChromeBrowserState::FromBrowserState(context);
   SigninManager* signin_manager =
       SigninManagerFactory::GetForBrowserState(chrome_browser_state);
-  std::unique_ptr<AccountReconcilor> reconcilor(new AccountReconcilor(
-      OAuth2TokenServiceFactory::GetForBrowserState(chrome_browser_state),
-      signin_manager,
-      SigninClientFactory::GetForBrowserState(chrome_browser_state),
-      GaiaCookieManagerServiceFactory::GetForBrowserState(chrome_browser_state),
-      std::make_unique<signin::MirrorAccountReconcilorDelegate>(
-          signin_manager)));
+  std::unique_ptr<AccountReconcilor> reconcilor =
+      std::make_unique<AccountReconcilor>(
+          OAuth2TokenServiceFactory::GetForBrowserState(chrome_browser_state),
+          signin_manager,
+          SigninClientFactory::GetForBrowserState(chrome_browser_state),
+          GaiaCookieManagerServiceFactory::GetForBrowserState(
+              chrome_browser_state),
+          std::make_unique<signin::MirrorAccountReconcilorDelegate>(
+              signin_manager, std::make_unique<ReconcilorDelegateHelper>(),
+              false));
   reconcilor->Initialize(true /* start_reconcile_if_tokens_available */);
   return reconcilor;
 }
