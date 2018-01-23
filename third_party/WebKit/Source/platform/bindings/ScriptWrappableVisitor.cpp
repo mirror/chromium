595037b067fd69e89793b2755042179d38553cc8
Hitoshi Yoshida <peria@chromium.org>
2018-01-23 15:23:17 +0900

    bindings: Remove Trace() and TraceWrappers() from WrapperTypeInfo
    Now ScriptWrappable inherits from GCFinalized, so we no longer need
    the hack to go around with wrapper type info in tracing.
    This CL reduces 16 Bytes/WrapperTypeInfo on 64bit platforms.
    
    Bug: 778996
    Change-Id: Ia1a226700693d799f481a64a65b024743d28839b

--- third_party/WebKit/Source/platform/bindings/ScriptWrappableVisitor.cpp
+++ third_party/WebKit/Source/platform/bindings/ScriptWrappableVisitor.cpp
@@ -170,8 +170,7 @@ void ScriptWrappableMarkingVisitor::RegisterV8Reference(
 
   ScriptWrappable* script_wrappable =
       reinterpret_cast<ScriptWrappable*>(internal_fields.second);
-
-  wrapper_type_info->TraceWrappers(this, script_wrappable);
+  TraceWrappersFromGeneratedCode(script_wrappable);
 }
 
 void ScriptWrappableMarkingVisitor::RegisterV8References(
