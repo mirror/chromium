cb4f11d81f8ad04ddb99e399d4a61ef7b363b5f8
Troy Hildebrandt <thildebr@chromium.org>
2018-01-23 13:25:32 -0800

    Remove VoiceSearchTabHelper.
    This change removes the VoiceSearchTabHelper, and instead allows voice
    searches to trigger a user activation on the frame. This allows the
    search results audio to autoplay under the new autoplay policy
    guidelines.
    
    This is achieved by adding an IPC from the RenderFrameHost to the
    RenderFrame to allow us to call Frame::NotifyUserActivation when a
    voice search is performed.
    
    Bug:715588
    Change-Id: I3440d5482712c21377914a4ccda028b42161bd8f

--- third_party/WebKit/Source/core/frame/WebLocalFrameImpl.cpp
+++ third_party/WebKit/Source/core/frame/WebLocalFrameImpl.cpp
@@ -2179,7 +2179,7 @@ void WebLocalFrameImpl::SetCommittedFirstRealLoad() {
 
 void WebLocalFrameImpl::SetHasReceivedUserGesture() {
   if (GetFrame())
-    GetFrame()->UpdateUserActivationInFrameTree();
+    Frame::NotifyUserActivation(GetFrame(), UserGestureToken::kNewGesture);
 }
 
 void WebLocalFrameImpl::BlinkFeatureUsageReport(const std::set<int>& features) {
