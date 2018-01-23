bf6e8d344d075d0d6c0f99cfb0cb3487c605dd6a
Jonathan Backer <backer@chromium.org>
2018-01-23 10:06:22 -0500

    Autogenerate some raster cmd buffer code
    I started enabling cmd_ids and cmd_format and then added the bare
    minimum to make things compile. I then added autogenerated
    gpu_unittests to check that things work.
    
    I opted to use module globals in build_cmd_buffer_lib.py for prefixes
    because the plumbing to avoid this was a lot of work and hurt
    readability.
    
    I did not do that for year because it's possible that we'll want
    different years in the copyright notices in the future.
    
    I opted to create a new RasterUtils instead of reusing existing
    GLES2Utils (including RASTER_UTILS_EXPORT) because it was easier. We
    could probably share these. Alternatively, there is probably unused
    autogen code in the RasterUtils that could be pruned.
    
    Bug: 789238
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ibdaac3eb09785b285e238fbc4ffe235e3ab9e077

---
+++ gpu/command_buffer/common/raster_cmd_utils_autogen.h
@@ -0,0 +1,30 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file is auto-generated from
+// gpu/command_buffer/build_raster_cmd_buffer.py
+// It's formatted by clang-format using chromium coding style:
+//    clang-format -i -style=chromium filename
+// DO NOT EDIT!
+
+#ifndef GPU_COMMAND_BUFFER_COMMON_RASTER_CMD_UTILS_AUTOGEN_H_
+#define GPU_COMMAND_BUFFER_COMMON_RASTER_CMD_UTILS_AUTOGEN_H_
+
+static std::string GetStringBufferUsage(uint32_t value);
+static std::string GetStringClientBufferUsage(uint32_t value);
+static std::string GetStringCompressedTextureFormat(uint32_t value);
+static std::string GetStringGLState(uint32_t value);
+static std::string GetStringPixelStore(uint32_t value);
+static std::string GetStringPixelType(uint32_t value);
+static std::string GetStringQueryObjectParameter(uint32_t value);
+static std::string GetStringQueryTarget(uint32_t value);
+static std::string GetStringResetStatus(uint32_t value);
+static std::string GetStringTextureBindTarget(uint32_t value);
+static std::string GetStringTextureFormat(uint32_t value);
+static std::string GetStringTextureInternalFormat(uint32_t value);
+static std::string GetStringTextureInternalFormatStorage(uint32_t value);
+static std::string GetStringTextureParameter(uint32_t value);
+static std::string GetStringTextureTarget(uint32_t value);
+
+#endif  // GPU_COMMAND_BUFFER_COMMON_RASTER_CMD_UTILS_AUTOGEN_H_
