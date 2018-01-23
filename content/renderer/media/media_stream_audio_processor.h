545966a3bb234262be3af468ae4cdc2cb7f5f15f
Sam Zackrisson <saza@chromium.org>
2018-01-23 14:03:26 +0100

    Remove redundant APM render buffer
    This CL removes the render_fifo_ buffer from MediaStreamAudioProcessor
    and DCHECKs that the input audio is 10 ms long.
    
    MediaStreamAudioProcessor currently buffers audio provided to
    OnPlayoutData, to feed frames in 10 ms chunks to the APM. However, the
    caller of OnPlayoutData already DCHECKs that this is the case.
    
    Change-Id: Ia20cf0e0810c59570305f1ca295016e0ca11057e

--- content/renderer/media/media_stream_audio_processor.h
+++ content/renderer/media/media_stream_audio_processor.h
@@ -191,16 +191,11 @@ class CONTENT_EXPORT MediaStreamAudioProcessor
   // Receives processing output.
   std::unique_ptr<MediaStreamAudioBus> output_bus_;
 
-  // FIFO to provide 10 ms render chunks when the AEC is enabled.
-  std::unique_ptr<MediaStreamAudioFifo> render_fifo_;
-
   // These are mutated on the main render thread in OnCaptureFormatChanged().
   // The caller guarantees this does not run concurrently with accesses on the
   // capture audio thread.
   media::AudioParameters input_format_;
   media::AudioParameters output_format_;
-  // Only used on the render audio thread.
-  media::AudioParameters render_format_;
 
   // Raw pointer to the WebRtcPlayoutDataSource, which is valid for the
   // lifetime of RenderThread.
