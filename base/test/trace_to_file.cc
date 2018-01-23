0c2a3bb4967a4ef2f3dae9b1c1f4570d92ae7726
Ehsan Chiniforooshan <chiniforooshan@chromium.org>
2018-01-23 15:31:21 -0500

    Tracing: Fix --trace-to-file crash
    content_unittests --trace-to-file crashes because at flush time there is no
    task runner.
    
    Bug: 805023
    Change-Id: I1ee0d2025b4993a2612158a97265813ba14cbae6

--- base/test/trace_to_file.cc
+++ base/test/trace_to_file.cc
@@ -9,6 +9,7 @@
 #include "base/command_line.h"
 #include "base/files/file_util.h"
 #include "base/memory/ref_counted_memory.h"
+#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/trace_event/trace_buffer.h"
 #include "base/trace_event/trace_log.h"
@@ -94,6 +95,7 @@ void TraceToFile::EndTracingIfNeeded() {
   buffer.SetOutputCallback(
       Bind(&TraceToFile::TraceOutputCallback, Unretained(this)));
 
+  MessageLoop message_loop;
   RunLoop run_loop;
   trace_event::TraceLog::GetInstance()->Flush(
       Bind(&OnTraceDataCollected, run_loop.QuitClosure(), Unretained(&buffer)));
