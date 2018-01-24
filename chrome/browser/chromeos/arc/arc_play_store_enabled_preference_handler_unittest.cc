5f432fa5a8f05092d129254d4626bc80f7648059
khmel <khmel@google.com>
2018-01-24 13:47:46 -0800

    arc: Enable ToS in OOBE for all devices.
    This forces ARC OptIn OOBE for all devices.
    
    Test: Manually + test_that minnie suite:bvt-arc
    Bug: b/69807512
    
    Change-Id: Iab37c463cc2c65fd25879ca9df382304f581ccb3

--- chrome/browser/chromeos/arc/arc_play_store_enabled_preference_handler_unittest.cc
+++ chrome/browser/chromeos/arc/arc_play_store_enabled_preference_handler_unittest.cc
@@ -10,7 +10,6 @@
 #include "base/files/scoped_temp_dir.h"
 #include "base/run_loop.h"
 #include "base/threading/sequenced_worker_pool.h"
-#include "chrome/browser/chromeos/arc/arc_auth_notification.h"
 #include "chrome/browser/chromeos/arc/arc_session_manager.h"
 #include "chrome/browser/chromeos/arc/arc_util.h"
 #include "chrome/browser/chromeos/arc/test/arc_data_removed_waiter.h"
@@ -44,7 +43,6 @@ class ArcPlayStoreEnabledPreferenceHandlerTest : public testing::Test {
     SetArcAvailableCommandLineForTesting(
         base::CommandLine::ForCurrentProcess());
     ArcSessionManager::DisableUIForTesting();
-    ArcAuthNotification::DisableForTesting();
 
     ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
     TestingProfile::Builder profile_builder;
