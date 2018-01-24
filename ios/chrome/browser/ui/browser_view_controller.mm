74a75766cdfd56bef346bae58fd16f4b44bdb3ec
stkhapugin@chromium.org <stkhapugin@chromium.org>
2018-01-24 18:56:11 +0100

    Change relationship between BVC, Toolbar and Location Bar delegation.
    Before this CL, BVC is Toolbar's delegate, which is a
    LocationBarControllerImpl's delegate. We would like for the
    LocationBarCoordinator to own LBControllerImpl fully. For this, the
    LocationBarCoordinator needs to become its delegate. However, the
    delegate is mostly forwarding commands to BVC. So after this CL:
    
    1. BVC is the delegate for LBCoordinator.
    2. BVC calls Toolbar methods for Toolbar to react to events in LB.
    3. LocationBarCoordinator is delegate for LBControllerImpl.
    
    Bug: 785253
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I7a735ed60a54c040437a91ff225f0c6c31e0e799

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -4232,8 +4232,11 @@ bubblePresenterForFeature:(const base::Feature&)feature
 #pragma mark - WebToolbarDelegate (Public)
 
 - (void)locationBarDidBecomeFirstResponder {
-  if (_locationBarHasFocus)
+  if (_locationBarHasFocus) {
+    [self.primaryToolbarCoordinator transitionToLocationBarFocusedState:YES];
     return;  // TODO(crbug.com/244366): This should not be necessary.
+  }
+
   _locationBarHasFocus = YES;
   [[NSNotificationCenter defaultCenter]
       postNotificationName:kLocationBarBecomesFirstResponderNotification
@@ -4250,11 +4253,15 @@ bubblePresenterForFeature:(const base::Feature&)feature
   }
   [[OmniboxGeolocationController sharedInstance]
       locationBarDidBecomeFirstResponder:_browserState];
+
+  [self.primaryToolbarCoordinator transitionToLocationBarFocusedState:YES];
 }
 
 - (void)locationBarDidResignFirstResponder {
-  if (!_locationBarHasFocus)
+  if (!_locationBarHasFocus) {
+    [self.primaryToolbarCoordinator transitionToLocationBarFocusedState:NO];
     return;  // TODO(crbug.com/244366): This should not be necessary.
+  }
   _locationBarHasFocus = NO;
   [self.sideSwipeController setEnabled:YES];
   [[NSNotificationCenter defaultCenter]
@@ -4287,6 +4294,7 @@ bubblePresenterForFeature:(const base::Feature&)feature
       webState->GetNavigationManager()->Reload(web::ReloadType::NORMAL,
                                                false /* check_for_repost */);
   }
+  [self.primaryToolbarCoordinator transitionToLocationBarFocusedState:NO];
 }
 
 - (void)locationBarBeganEdit {
