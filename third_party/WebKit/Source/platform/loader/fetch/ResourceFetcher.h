48bfb9adef9a56d6c3c83fd4aabe62e490303935
Takashi Toyoshima <toyoshim@chromium.org>
2018-01-25 00:44:53 +0900

    Blink Loader: refine FetchContext::NullInstance
    Bug: 803849
    Change-Id: I7a8b927e07101679250962ac48ed502400bf05ae

--- third_party/WebKit/Source/platform/loader/fetch/ResourceFetcher.h
+++ third_party/WebKit/Source/platform/loader/fetch/ResourceFetcher.h
@@ -99,9 +99,7 @@ class PLATFORM_EXPORT ResourceFetcher
   void SetAutoLoadImages(bool);
   void SetImagesEnabled(bool);
 
-  FetchContext& Context() const {
-    return context_ ? *context_.Get() : FetchContext::NullInstance();
-  }
+  FetchContext& Context() const { return *context_.Get(); }
   void ClearContext();
 
   int BlockingRequestCount() const;
