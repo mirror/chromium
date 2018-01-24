d838c330b721c1c970af4133b3c91a396e184356
Evan Stade <estade@chromium.org>
2018-01-23 18:47:01 -0700

    Update more tests to use NotificationDisplayServiceTester. (try 2)
    For the changes to FileManagerBrowserTestBase, you can run affected
    tests with --gtest_filter=DriveSpecific*
    
    Original, which was reverted: 2a3c7c8469960
    
    Bug: 783018
    Change-Id: Ie77f44e2e7a5af15e24e630c412bebe5788cb5a0

--- chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest_base.cc
@@ -25,7 +25,7 @@
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/extensions/component_loader.h"
 #include "chrome/browser/extensions/extension_service.h"
-#include "chrome/browser/notifications/notification_ui_manager.h"
+#include "chrome/browser/notifications/notification_display_service_tester.h"
 #include "chrome/common/chrome_switches.h"
 #include "chromeos/chromeos_switches.h"
 #include "components/drive/chromeos/file_system_interface.h"
@@ -549,6 +549,9 @@ void FileManagerBrowserTestBase::SetUpOnMainThread() {
     drive_volume_->ConfigureShareUrlBase(share_url_base);
     test_util::WaitUntilDriveMountPointIsAdded(profile());
   }
+
+  display_service_ =
+      std::make_unique<NotificationDisplayServiceTester>(profile());
 }
 
 void FileManagerBrowserTestBase::SetUpCommandLine(
@@ -562,6 +565,11 @@ void FileManagerBrowserTestBase::SetUpCommandLine(
     command_line->AppendSwitch(switches::kIncognito);
   }
   ExtensionApiTest::SetUpCommandLine(command_line);
+}
+
+void FileManagerBrowserTestBase::TearDownOnMainThread() {
+  display_service_.reset();
+  ExtensionApiTest::TearDownOnMainThread();
 }
 
 void FileManagerBrowserTestBase::InstallExtension(const base::FilePath& path,
@@ -729,12 +737,13 @@ void FileManagerBrowserTestBase::OnMessage(const std::string& name,
     ASSERT_TRUE(value.GetInteger("index", &index));
 
     const std::string delegate_id = extension_id + "-" + notification_id;
-    const message_center::Notification* notification =
-        g_browser_process->notification_ui_manager()->FindById(delegate_id,
-                                                               profile());
-    ASSERT_TRUE(notification);
 
-    notification->delegate()->ButtonClick(index);
+    base::Optional<message_center::Notification> notification =
+        display_service_->GetNotification(delegate_id);
+    EXPECT_TRUE(notification);
+
+    display_service_->SimulateClick(NotificationHandler::Type::EXTENSION,
+                                    delegate_id, index, base::nullopt);
     return;
   }
 
