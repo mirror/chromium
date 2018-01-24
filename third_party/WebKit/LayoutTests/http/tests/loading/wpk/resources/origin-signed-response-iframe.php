35268410a65517144d7d0e851bdcbe27f07bf493
Tsuyoshi Horo <horo@chromium.org>
2018-01-24 12:58:17 +0900

    [Not for review] OriginSignedResponseHandler
    Change-Id: I53d397d7cdfe79d48d6891db65ab1a1e12100b92

---
+++ third_party/WebKit/LayoutTests/http/tests/loading/wpk/resources/origin-signed-response-iframe.php
@@ -0,0 +1,35 @@
+<?php
+header('Content-Type: application/http-exchange+cbor');
+
+$payload  = <<< EOM
+<!DOCTYPE html>
+<body>
+<script>
+window.addEventListener('message', (event) => {
+  event.data.port.postMessage({location: document.location.href});
+}, false);
+</script>
+hello<br>
+world
+</body>
+EOM;
+
+$response = array(
+  'htxg',
+  'request',
+  array(
+    ':method' => 'GET',
+    ':url' => 'https://example.com/test.html',
+    'accept'=> '*/*'
+  ),
+  'response',
+  array(
+    ':status' => '200',
+    'content-type'=> 'text/html'
+  ),
+  'payload',
+  $payload
+);
+echo json_encode($response);
+?>
+
