8bcc3873316bf39c1860aaae47b5d526742a4e1f
Malcolm White <malcolmwhite@google.com>
2018-01-24 16:11:55 -0600

    Transfer ArrayBuffers and ImageBitmaps over mojo for MessageChannel
    Change-Id: Iab2b0d4708b3289e953fc84d2ddd01341bfc2d2e

--- third_party/WebKit/common/message_port/transferable_message_struct_traits.cc
+++ third_party/WebKit/common/message_port/transferable_message_struct_traits.cc
@@ -6,6 +6,7 @@
 
 #include "base/containers/span.h"
 #include "third_party/WebKit/common/message_port/cloneable_message_struct_traits.h"
+#include "third_party/skia/include/core/SkBitmap.h"
 
 namespace mojo {
 
@@ -14,12 +15,19 @@ bool StructTraits<blink::mojom::TransferableMessage::DataView,
     Read(blink::mojom::TransferableMessage::DataView data,
          blink::TransferableMessage* out) {
   std::vector<mojo::ScopedMessagePipeHandle> ports;
+  std::vector<blink::mojom::SerializedArrayBufferContentsPtr>
+      array_buffer_contents_array;
+  std::vector<SkBitmap> image_bitmap_contents_array;
   if (!data.ReadMessage(static_cast<blink::CloneableMessage*>(out)) ||
+      !data.ReadArrayBufferContentsArray(&array_buffer_contents_array) ||
+      !data.ReadImageBitmapContentsArray(&image_bitmap_contents_array) ||
       !data.ReadPorts(&ports)) {
     return false;
   }
 
   out->ports = blink::MessagePortChannel::CreateFromHandles(std::move(ports));
+  out->array_buffer_contents_array = std::move(array_buffer_contents_array);
+  out->image_bitmap_contents_array = std::move(image_bitmap_contents_array);
   return true;
 }
 
