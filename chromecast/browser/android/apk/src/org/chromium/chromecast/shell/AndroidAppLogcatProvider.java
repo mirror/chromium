33b795778cc14423fea5f3f22b1f5f04c582be18
Sandeep Vijayasekar <sandv@chromium.org>
2018-01-24 12:52:01 -0800

    [Chromecast] Added AIDL to get assistant device logs
    Bug: 71798364
    Bug: 71646628
    Test: Send feedback on home app
    Change-Id: Iba23a4e07369a9557a8ef43c23a7f93f3184a9c6

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/AndroidAppLogcatProvider.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/AndroidAppLogcatProvider.java
@@ -1,11 +1,10 @@
-// Copyright 2017 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 package org.chromium.chromecast.shell;
 
 import org.chromium.base.Log;
-import org.chromium.base.VisibleForTesting;
 import org.chromium.chromecast.base.CircularBuffer;
 
 import java.io.BufferedReader;
@@ -20,25 +19,13 @@ import java.nio.charset.UnsupportedCharsetException;
  * <p>Elided information includes: Emails, IP address, MAC address, URL/domains as well as
  * Javascript console messages.
  */
-class LogcatExtractor {
-    private static final String TAG = "LogcatExtractor";
+class AndroidAppLogcatProvider extends ElidedLogcatProvider {
+    private static final String TAG = "LogcatProvider";
     private static final long HALF_SECOND = 500;
 
-    public static String getElidedLogcat() throws IOException, InterruptedException {
-        return getElidedLogcat(getLogcat());
-    }
-
-    @VisibleForTesting
-    static String getElidedLogcat(Iterable<String> rawLogcat) {
-        StringBuilder builder = new StringBuilder();
-        for (String line : rawLogcat) {
-            builder.append(LogcatElision.elide(line));
-            builder.append("\n");
-        }
-        return builder.toString();
-    }
-
-    private static Iterable<String> getLogcat() throws IOException, InterruptedException {
+    @Override
+    protected void getRawLogcat(RawLogcatCallback callback)
+            throws IOException, InterruptedException {
         CircularBuffer<String> rawLogcat = new CircularBuffer<>(BuildConfig.LOGCAT_SIZE);
         String logLn = null;
         Integer exitValue = null;
@@ -64,11 +51,14 @@ class LogcatExtractor {
                 Log.w(TAG, msg);
                 throw new IOException(msg);
             }
-            return rawLogcat;
+
+            callback.onLogsDone(rawLogcat);
+            // String elidedLogcat = LogcatProvider.elideLogcat(rawLogcat);
+            // callback.onLogsDone(elidedLogcat);
         } catch (UnsupportedCharsetException e) {
             // Should never happen; all Java implementations are required to support UTF-8.
             Log.wtf(TAG, "UTF-8 not supported", e);
-            return rawLogcat;
+            callback.onLogsDone(null);
         } finally {
             if (bReader != null) {
                 bReader.close();
