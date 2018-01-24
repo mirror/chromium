3321a5ce363405b88e6c1c502f4bd3cb2e4e61b8
Elad Alon <eladalon@chromium.org>
2018-01-24 11:08:22 +0100

    WebRtcRemoteEventLogManager
    Bug: TBD
    Change-Id: I01e8ea4f7a76ee7250999d7992b1bec31d6593c2

--- content/browser/webrtc/webrtc_event_log_manager_common.h
+++ content/browser/webrtc/webrtc_event_log_manager_common.h
@@ -5,8 +5,11 @@
 #ifndef CONTENT_BROWSER_WEBRTC_WEBRTC_RTC_EVENT_LOG_MANAGER_COMMON_H_
 #define CONTENT_BROWSER_WEBRTC_WEBRTC_RTC_EVENT_LOG_MANAGER_COMMON_H_
 
+#include <map>
+#include <string>
 #include <tuple>
 
+#include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "build/build_config.h"
 #include "content/common/content_export.h"
@@ -17,6 +20,8 @@ CONTENT_EXPORT extern const size_t kWebRtcEventLogManagerUnlimitedFileSize;
 
 CONTENT_EXPORT extern const size_t kDefaultMaxLocalLogFileSizeBytes;
 CONTENT_EXPORT extern const size_t kMaxNumberLocalWebRtcEventLogFiles;
+
+CONTENT_EXPORT extern const size_t kMaxNumberActiveRemoteWebRtcEventLogFiles;
 
 // For a given Chrome session, this is a unique key for PeerConnections.
 // It's not, however, unique between sessions (after Chrome is restarted).
@@ -44,8 +49,55 @@ class WebRtcLocalEventLogsObserver {
  public:
   virtual ~WebRtcLocalEventLogsObserver() = default;
   virtual void OnLocalLogStarted(WebRtcEventLogPeerConnectionKey key,
-                                 base::FilePath file_path) = 0;
+                                 const base::FilePath& file_path) = 0;
   virtual void OnLocalLogStopped(WebRtcEventLogPeerConnectionKey key) = 0;
+};
+
+// An observer for notifications of remote-bound log files being
+// started/stopped. The start event would likely only interest unit tests
+// (because it exposes the randomized filename to them). The stop event is of
+// general interest, because it would often mean that WebRTC can stop sending
+// us event logs for this peer connection.
+// Some cases where OnRemoteLogStopped would be called include::
+// 1. The PeerConnection has become inactive.
+// 2. The file's maximum size has been reached.
+// 3. Any type of error while writing to the file.
+class WebRtcRemoteEventLogsObserver {
+ public:
+  virtual ~WebRtcRemoteEventLogsObserver() = default;
+  virtual void OnRemoteLogStarted(WebRtcEventLogPeerConnectionKey key,
+                                  const base::FilePath& file_path) = 0;
+  virtual void OnRemoteLogStopped(WebRtcEventLogPeerConnectionKey key) = 0;
+};
+
+struct LogFile {
+  LogFile(const base::FilePath& path,
+          base::File file,
+          size_t max_file_size_bytes)
+      : path(path),
+        file(std::move(file)),
+        max_file_size_bytes(max_file_size_bytes),
+        file_size_bytes(0) {}
+  const base::FilePath path;
+  base::File file;
+  const size_t max_file_size_bytes;
+  size_t file_size_bytes;
+};
+
+// WebRtcLocalEventLogManager and WebRtcRemoteEventLogManager share some logic
+// when it comes to handling of files on disk.
+class LogFileWriter {
+ public:
+  virtual ~LogFileWriter() = default;
+
+ protected:
+  using PeerConnectionKey = WebRtcEventLogPeerConnectionKey;
+
+  typedef std::map<PeerConnectionKey, LogFile> LogFilesMap;
+
+  bool WriteToLogFile(LogFilesMap::iterator it, const std::string& output);
+
+  virtual LogFilesMap::iterator CloseLogFile(LogFilesMap::iterator it) = 0;
 };
 
 }  // namespace content
