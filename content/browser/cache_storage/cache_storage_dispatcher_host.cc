b52bfe5d9cd242e1821384e9ac361ec66b84918d
Luciano Pacheco <lucmult@chromium.org>
2018-01-24 13:52:52 +1100

    Testing IPC failure to convert to mojo.
    Change-Id: I9744f27c72be232e153595977c07f3a2df7c5e17

--- content/browser/cache_storage/cache_storage_dispatcher_host.cc
+++ content/browser/cache_storage/cache_storage_dispatcher_host.cc
@@ -74,7 +74,6 @@ bool CacheStorageDispatcherHost::OnMessageReceived(
   IPC_MESSAGE_HANDLER(CacheStorageHostMsg_CacheStorageOpen, OnCacheStorageOpen)
   IPC_MESSAGE_HANDLER(CacheStorageHostMsg_CacheStorageDelete,
                       OnCacheStorageDelete)
-  IPC_MESSAGE_HANDLER(CacheStorageHostMsg_CacheStorageKeys, OnCacheStorageKeys)
   IPC_MESSAGE_HANDLER(CacheStorageHostMsg_CacheStorageMatch,
                       OnCacheStorageMatch)
   IPC_MESSAGE_HANDLER(CacheStorageHostMsg_CacheMatch, OnCacheMatch)
@@ -147,19 +146,21 @@ void CacheStorageDispatcherHost::OnCacheStorageDelete(
                      this, thread_id, request_id));
 }
 
-void CacheStorageDispatcherHost::OnCacheStorageKeys(int thread_id,
-                                                    int request_id,
-                                                    const url::Origin& origin) {
+void CacheStorageDispatcherHost::Keys(
+    const url::Origin& origin,
+    blink::mojom::CacheStorage::KeysCallback callback) {
   TRACE_EVENT0("CacheStorage",
                "CacheStorageDispatcherHost::OnCacheStorageKeys");
+
   if (!OriginCanAccessCacheStorage(origin)) {
+    // TODO(lucmult): Check how to handle bad msg with Mojo, .reset() ?
     bad_message::ReceivedBadMessage(this, bad_message::CSDH_INVALID_ORIGIN);
     return;
   }
   context_->cache_manager()->EnumerateCaches(
       origin.GetURL(),
       base::BindOnce(&CacheStorageDispatcherHost::OnCacheStorageKeysCallback,
-                     this, thread_id, request_id));
+                     this, std::move(callback)));
 }
 
 void CacheStorageDispatcherHost::OnCacheStorageMatch(
@@ -366,14 +367,13 @@ void CacheStorageDispatcherHost::OnCacheStorageDeleteCallback(
 }
 
 void CacheStorageDispatcherHost::OnCacheStorageKeysCallback(
-    int thread_id,
-    int request_id,
+    blink::mojom::CacheStorage::KeysCallback callback,
     const CacheStorageIndex& cache_index) {
   std::vector<base::string16> string16s;
   for (const auto& metadata : cache_index.ordered_cache_metadata())
     string16s.push_back(base::UTF8ToUTF16(metadata.name));
-  Send(new CacheStorageMsg_CacheStorageKeysSuccess(thread_id, request_id,
-                                                   string16s));
+
+  std::move(callback).Run(string16s);
 }
 
 void CacheStorageDispatcherHost::OnCacheStorageMatchCallback(
@@ -515,6 +515,11 @@ void CacheStorageDispatcherHost::DropBlobDataHandle(const std::string& uuid) {
   it->second.pop_front();
   if (it->second.empty())
     blob_handle_store_.erase(it);
+}
+
+void CacheStorageDispatcherHost::AddBinding(
+    blink::mojom::CacheStorageRequest request) {
+  bindings_.AddBinding(this, std::move(request));
 }
 
 }  // namespace content
