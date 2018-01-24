425c94c9b97235c86a1da63d52a3c9b9d47ff243
Olga Sharonova <olka@chromium.org>
2018-01-24 23:04:11 +0100

    Audio service embedded in content and AudioSystem on top of it
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie69df7bf9f593fda6a2e85f5f57a49a90aeccdb9

---
+++ services/audio/service_factory.h
@@ -0,0 +1,31 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_AUDIO_SERVICE_FACTORY_H_
+#define SERVICES_AUDIO_SERVICE_FACTORY_H_
+
+#include <memory>
+
+#include "base/macros.h"
+#include "services/service_manager/public/cpp/service.h"
+
+namespace media {
+class AudioManager;
+}  // namespace media
+
+namespace audio {
+
+namespace service_factory {
+
+// Create an instance of Audio service which will live in the current process
+// on top of AudioManager instance belonging to that process. Must be called on
+// the device thread of AudioManager.
+std::unique_ptr<service_manager::Service> CreateInProcess(
+    media::AudioManager* audio_manager);
+
+}  // namespace service_factory
+
+}  // namespace audio
+
+#endif  // SERVICES_AUDIO_SERVICE_FACTORY_H_
