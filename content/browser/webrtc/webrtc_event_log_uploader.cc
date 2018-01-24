1f1af2edd7051d78187892005233ca467030759c
Elad Alon <eladalon@chromium.org>
2018-01-24 17:23:40 +0100

    WebRtcRemoteEventLogManager
    Bug: TBD
    Change-Id: I01e8ea4f7a76ee7250999d7992b1bec31d6593c2

---
+++ content/browser/webrtc/webrtc_event_log_uploader.cc
@@ -0,0 +1,50 @@
+// Copyright (c) 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/webrtc/webrtc_event_log_uploader.h"
+
+#include "base/files/file_util.h"
+#include "base/logging.h"
+
+namespace content {
+
+WebRtcEventLogUploaderImpl::WebRtcEventLogUploaderImpl(
+    WebRtcEventLogUploaderObserver* observer,
+    const base::FilePath& path) {
+  DCHECK(observer);
+
+  // TODO(eladalon): Provide an actual implementation; really upload the file.
+  // https://crbug.com/775415
+
+  // If the upload was successful, the file is no longer needed.
+  // If the upload failed, we don't want to retry, because we run the risk of
+  // uploading significant amounts of data once again, only for the upload to
+  // fail again after (as an example) wasting 50MBs of upload bandwidth.
+  const bool deletion_successful = base::DeleteFile(path, /*recursive=*/false);
+  if (!deletion_successful) {
+    // This is a somewhat serious (though unlikely) error, because now we'll try
+    // to upload this file again next time Chrome launches.
+    LOG(ERROR) << "Could not delete pending log file.";
+  }
+
+  // TODO(eladalon): Provide actual success/failure of upload.
+  // https://crbug.com/775415
+  observer->OnWebRtcEventLogUploadSuccess();
+}
+
+WebRtcEventLogUploaderImpl::~WebRtcEventLogUploaderImpl() {
+  // TODO(eladalon): Provide an actual implementation. https://crbug.com/775415
+}
+
+WebRtcEventLogUploaderImplFactory::WebRtcEventLogUploaderImplFactory(
+    WebRtcEventLogUploaderObserver* observer)
+    : observer_(observer) {}
+
+std::unique_ptr<WebRtcEventLogUploader>
+WebRtcEventLogUploaderImplFactory::Create(
+    const base::FilePath& log_file) const {
+  return std::make_unique<WebRtcEventLogUploaderImpl>(observer_, log_file);
+}
+
+}  // namespace content
---
+++ content/browser/webrtc/webrtc_event_log_uploader.h
@@ -0,0 +1,65 @@
+// Copyright (c) 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_WEBRTC_WEBRTC_EVENT_LOG_UPLOADER_H_
+#define CONTENT_BROWSER_WEBRTC_WEBRTC_EVENT_LOG_UPLOADER_H_
+
+#include <memory>
+
+#include "base/files/file_path.h"
+
+namespace content {
+
+// A class implementing this interace can register for notification of an
+// upload's eventual result (success/failure).
+class WebRtcEventLogUploaderObserver {
+ public:
+  virtual ~WebRtcEventLogUploaderObserver() = default;
+  virtual void OnWebRtcEventLogUploadSuccess() = 0;
+  virtual void OnWebRtcEventLogUploadFailure() = 0;
+};
+
+// A sublcass of this interface would take ownership of a file, and either
+// upload it to a remote server (actual implementation), or pretend to do
+// so (in unit tests). It will typically take on an observer of type
+// WebRtcEventLogUploaderObserver, and inform it of the success or failure
+// of the upload.
+class WebRtcEventLogUploader {
+ public:
+  virtual ~WebRtcEventLogUploader() = default;
+};
+
+// Since we'll need more than one instance of the abstract
+// WebRtcEventLogUploader, we'll need an abstract factory for it.
+class WebRtcEventLogUploaderFactory {
+ public:
+  virtual ~WebRtcEventLogUploaderFactory() = default;
+
+  virtual std::unique_ptr<WebRtcEventLogUploader> Create(
+      const base::FilePath& log_file) const = 0;
+};
+
+class WebRtcEventLogUploaderImpl : public WebRtcEventLogUploader {
+ public:
+  WebRtcEventLogUploaderImpl(WebRtcEventLogUploaderObserver* observer,
+                             const base::FilePath& path);
+  ~WebRtcEventLogUploaderImpl() override;
+};
+
+class WebRtcEventLogUploaderImplFactory : public WebRtcEventLogUploaderFactory {
+ public:
+  explicit WebRtcEventLogUploaderImplFactory(
+      WebRtcEventLogUploaderObserver* observer);
+  ~WebRtcEventLogUploaderImplFactory() override = default;
+
+  std::unique_ptr<WebRtcEventLogUploader> Create(
+      const base::FilePath& log_file) const override;
+
+ private:
+  WebRtcEventLogUploaderObserver* const observer_;
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_WEBRTC_WEBRTC_EVENT_LOG_UPLOADER_H_
