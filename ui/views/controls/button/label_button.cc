37c647d3fb7baf66b1ad6a36f091d7633a777a3f
Allen Bauer <kylixrd@chromium.org>
2018-01-24 09:54:29 -0800

    Add vertical image centering/top alignment to LabelButton. This allows radio buttons (and checkboxes) to align the image with the first line if the label is wrapped.
    Bug: 652031
    Change-Id: I6c6ac29540c83367936547788eda76d2c659af4c

--- ui/views/controls/button/label_button.cc
+++ ui/views/controls/button/label_button.cc
@@ -273,7 +273,13 @@ void LabelButton::Layout() {
       label_area.height());
 
   gfx::Point image_origin(child_area.origin());
-  image_origin.Offset(0, (child_area.height() - image_size.height()) / 2);
+  if (label_->multi_line()) {
+    image_origin.Offset(
+        0, std::max(
+               0, (label_->font_list().GetHeight() - image_size.height()) / 2));
+  } else {
+    image_origin.Offset(0, (child_area.height() - image_size.height()) / 2);
+  }
   if (horizontal_alignment_ == gfx::ALIGN_CENTER) {
     const int spacing = (image_size.width() > 0 && label_size.width() > 0) ?
         image_label_spacing_ : 0;
