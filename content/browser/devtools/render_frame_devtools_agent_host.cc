b7fa5cbea1e1a7b39ae9c49c7a453c13bf9d0eae
Pavel Feldman <pfeldman@chromium.org>
2018-01-24 18:20:21 -0800

    DevTools: introduce Page.navigate(frameId).
    Change-Id: I8e7854752054f0e4ac1c88fe9b9ec5e9e9547876

--- content/browser/devtools/render_frame_devtools_agent_host.cc
+++ content/browser/devtools/render_frame_devtools_agent_host.cc
@@ -164,16 +164,18 @@ void RenderFrameDevToolsAgentHost::AddAllAgentHosts(
 // static
 void RenderFrameDevToolsAgentHost::OnResetNavigationRequest(
     NavigationRequest* navigation_request) {
-  RenderFrameDevToolsAgentHost* agent_host =
-      FindAgentHost(navigation_request->frame_tree_node());
-  if (!agent_host)
-    return;
-  if (navigation_request->net_error() != net::OK) {
-    for (auto* network : protocol::NetworkHandler::ForAgentHost(agent_host))
-      network->NavigationFailed(navigation_request);
+  for (FrameTreeNode* node = navigation_request->frame_tree_node(); node;
+       node = node->parent()) {
+    RenderFrameDevToolsAgentHost* agent_host = FindAgentHost(node);
+    if (!agent_host)
+      continue;
+    if (navigation_request->net_error() != net::OK) {
+      for (auto* network : protocol::NetworkHandler::ForAgentHost(agent_host))
+        network->NavigationFailed(navigation_request);
+    }
+    for (auto* page : protocol::PageHandler::ForAgentHost(agent_host))
+      page->NavigationReset(navigation_request);
   }
-  for (auto* page : protocol::PageHandler::ForAgentHost(agent_host))
-    page->NavigationReset(navigation_request);
 }
 
 // static
