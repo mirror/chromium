af0cb0782fe08302995e6e717526339c1f611cab
Marina Ciocea <marinaciocea@chromium.org>
2018-01-25 15:15:24 +0100

    Introduce audio debug recording session.
    * Add AudioDebugRecordingSession factory
    * Enable debug recording in AudioDebugRecordingSessionImpl constructor
    * Disable debug recording in AudioDebugRecordingSessionImpl destructor
    
    Bug: 88657
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I44383d0994b8eebfd36c368827973e614937d8cd

---
+++ media/audio/audio_debug_recording_session_impl_unittest.cc
@@ -0,0 +1,79 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/audio/audio_debug_recording_session_impl.h"
+#include "base/test/scoped_task_environment.h"
+#include "media/audio/audio_debug_recording_helper.h"
+#include "media/audio/audio_thread_impl.h"
+#include "media/audio/mock_audio_manager.h"
+#include "media/audio/test_audio_thread.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace media {
+
+namespace {
+const base::FilePath::CharType kFilePath[] = FILE_PATH_LITERAL("file_path");
+}
+
+class AudioDebugRecordingSessionImplTest : public testing::Test {
+ public:
+  void CreateAudioManager() {
+    mock_audio_manager_ =
+        std::make_unique<MockAudioManager>(std::make_unique<AudioThreadImpl>());
+    ASSERT_TRUE(AudioManager::Get() != nullptr);
+  }
+
+  void ShutdownAudioManager() { ASSERT_TRUE(mock_audio_manager_->Shutdown()); }
+
+  void CreateDebugRecordingSession() {
+    audio_debug_recording_session_impl_ =
+        std::make_unique<media::AudioDebugRecordingSessionImpl>(file_path_);
+  }
+
+  void DestroyDebugRecordingSession() {
+    audio_debug_recording_session_impl_.reset();
+  }
+
+ protected:
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
+  std::unique_ptr<MockAudioManager> mock_audio_manager_;
+  std::unique_ptr<AudioDebugRecordingSessionImpl>
+      audio_debug_recording_session_impl_;
+  base::FilePath file_path_ = base::FilePath(kFilePath);
+};
+
+TEST_F(AudioDebugRecordingSessionImplTest,
+       ConstructorEnablesAndDestructorDisablesDebugRecordingOnAudioManager) {
+  ::testing::InSequence seq;
+
+  CreateAudioManager();
+  EXPECT_CALL(*mock_audio_manager_, EnableDebugRecording(file_path_));
+  CreateDebugRecordingSession();
+
+  EXPECT_CALL(*mock_audio_manager_, DisableDebugRecording());
+  DestroyDebugRecordingSession();
+
+  ShutdownAudioManager();
+}
+
+TEST_F(AudioDebugRecordingSessionImplTest,
+       CreateDestroySessionDontCrashWithNoAudioManager) {
+  ASSERT_TRUE(AudioManager::Get() == nullptr);
+  CreateDebugRecordingSession();
+  DestroyDebugRecordingSession();
+}
+
+TEST_F(AudioDebugRecordingSessionImplTest,
+       EnableDisableNotCalledAfterAudioManagerShutdown) {
+  CreateAudioManager();
+  ShutdownAudioManager();
+
+  EXPECT_CALL(*mock_audio_manager_, EnableDebugRecording(testing::_)).Times(0);
+  CreateDebugRecordingSession();
+
+  EXPECT_CALL(*mock_audio_manager_, DisableDebugRecording()).Times(0);
+  DestroyDebugRecordingSession();
+}
+}  // namespace media
