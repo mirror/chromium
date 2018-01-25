d6fd79588df136af115eee01da6244a0fa6cbd3b
Olga Sharonova <olka@chromium.org>
2018-01-25 07:08:38 +0100

    Audio service embedded in content and AudioSystem on top of it
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie69df7bf9f593fda6a2e85f5f57a49a90aeccdb9

--- services/audio/common/system_info.cc
+++ services/audio/common/system_info.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "services/audio/system_info.h"
+#include "services/audio/common/system_info.h"
 
 namespace audio {
 
