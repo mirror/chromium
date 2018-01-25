bb9de0691577df94bdb45d0c6bd63c3a7df4b539
Pavel Feldman <pfeldman@chromium.org>
2018-01-24 16:42:46 -0800

    DevTools: don't update all frames aggresively upon render frame swap.
    We used to detach from targets aggressively because of the timing of the
    UpdateFrames call. Current frame host was still non-cross-process and hence
    was detached from. This change migrates from bulk-frame update to treating
    render frame host swap in AutoAttachToFrame.
    
    Bug: 805248
    Change-Id: Id6c8a665676167fea5048c23db20cfc8fafd86f1

--- content/browser/devtools/protocol/target_auto_attacher.cc
+++ content/browser/devtools/protocol/target_auto_attacher.cc
@@ -141,26 +141,44 @@ void TargetAutoAttacher::AgentHostClosed(DevToolsAgentHost* host) {
 }
 
 bool TargetAutoAttacher::ShouldThrottleFramesNavigation() {
-  return auto_attach_ && attach_to_frames_ && wait_for_debugger_on_start_;
+  return auto_attach_ && attach_to_frames_;
 }
 
 DevToolsAgentHost* TargetAutoAttacher::AutoAttachToFrame(
-    NavigationHandle* navigation_handle) {
+    NavigationHandleImpl* navigation_handle) {
   if (!ShouldThrottleFramesNavigation())
     return nullptr;
-  if (!navigation_handle->GetRenderFrameHost() ||
-      !navigation_handle->GetRenderFrameHost()->IsCrossProcessSubframe()) {
+
+  RenderFrameHost* old_host =
+      navigation_handle->frame_tree_node()->current_frame_host();
+  RenderFrameHost* new_host = navigation_handle->GetRenderFrameHost();
+
+  bool old_cross_process = old_host && old_host->IsCrossProcessSubframe();
+  bool new_cross_process = new_host && new_host->IsCrossProcessSubframe();
+
+  if (old_cross_process == new_cross_process)
     return nullptr;
+
+  if (new_cross_process) {
+    scoped_refptr<DevToolsAgentHost> agent_host =
+        RenderFrameDevToolsAgentHost::GetOrCreateForDangling(
+            navigation_handle->frame_tree_node());
+    DCHECK(auto_attached_hosts_.find(agent_host) == auto_attached_hosts_.end());
+    attach_callback_.Run(agent_host.get(), true /* waiting_for_debugger */);
+    auto_attached_hosts_.insert(agent_host);
+    // TODO(pfeldman): rename method to make throttling logic clear.
+    return wait_for_debugger_on_start_ ? agent_host.get() : nullptr;
   }
+
+  DCHECK(old_cross_process);
   scoped_refptr<DevToolsAgentHost> agent_host =
-      RenderFrameDevToolsAgentHost::GetOrCreateForDangling(
-          static_cast<NavigationHandleImpl*>(navigation_handle)
-              ->frame_tree_node());
-  if (auto_attached_hosts_.find(agent_host) != auto_attached_hosts_.end())
-    return nullptr;
-  attach_callback_.Run(agent_host.get(), true /* waiting_for_debugger */);
-  auto_attached_hosts_.insert(agent_host);
-  return agent_host.get();
+      RenderFrameDevToolsAgentHost::GetOrCreateFor(
+          navigation_handle->frame_tree_node());
+  auto it = auto_attached_hosts_.find(agent_host);
+  DCHECK(it != auto_attached_hosts_.end());
+  auto_attached_hosts_.erase(it);
+  detach_callback_.Run(agent_host.get());
+  return nullptr;
 }
 
 void TargetAutoAttacher::ReattachServiceWorkers(bool waiting_for_debugger) {
--- content/browser/devtools/render_frame_devtools_agent_host.cc
+++ content/browser/devtools/render_frame_devtools_agent_host.cc
@@ -505,9 +505,6 @@ void RenderFrameDevToolsAgentHost::DidStartNavigation(
 void RenderFrameDevToolsAgentHost::RenderFrameHostChanged(
     RenderFrameHost* old_host,
     RenderFrameHost* new_host) {
-  for (auto* target : protocol::TargetHandler::ForAgentHost(this))
-    target->RenderFrameHostChanged();
-
   if (old_host != frame_host_)
     return;
 
