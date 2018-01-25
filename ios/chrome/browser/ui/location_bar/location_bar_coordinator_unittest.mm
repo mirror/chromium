b255ac45682feacb934d011298208e4e38bf01c3
Gauthier Ambard <gambard@chromium.org>
2018-01-25 11:31:03 +0100

    Have the LocationBar observing the WebState
    This CL changes the location bar such as its mediator is observing the
    currently active WebState and update the omnibox state accordingly.
    This is done so the BVC doesn't have to send update commands to the
    location bar through the toolbar.
    
    Bug:803383
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Id1ba13fc1c37e7c1a5a351dbd52c6859b44ce80c

--- ios/chrome/browser/ui/location_bar/location_bar_coordinator_unittest.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_coordinator_unittest.mm
@@ -8,6 +8,10 @@
 
 #include "base/test/scoped_task_environment.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
+#include "ios/chrome/browser/web_state_list/fake_web_state_list_delegate.h"
+#include "ios/chrome/browser/web_state_list/web_state_list.h"
+#import "ios/chrome/browser/web_state_list/web_state_opener.h"
+#import "ios/web/public/test/fakes/test_web_state.h"
 #include "testing/platform_test.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
@@ -25,19 +29,23 @@ class LocationBarCoordinatorTest : public PlatformTest {
     TestChromeBrowserState::Builder test_cbs_builder;
     browser_state_ = test_cbs_builder.Build();
 
+    web_state_list_ = std::make_unique<WebStateList>(&web_state_list_delegate_);
+    auto web_state = std::make_unique<web::TestWebState>();
+    web_state->SetCurrentURL(GURL("http://test/"));
+    web_state_list_->InsertWebState(0, std::move(web_state),
+                                    WebStateList::INSERT_FORCE_INDEX,
+                                    WebStateOpener());
+
     coordinator_ = [[LocationBarCoordinator alloc] init];
     coordinator_.browserState = browser_state_.get();
+    coordinator_.webStateList = web_state_list_.get();
   }
 
   LocationBarCoordinator* coordinator_;
   std::unique_ptr<TestChromeBrowserState> browser_state_;
+  FakeWebStateListDelegate web_state_list_delegate_;
+  std::unique_ptr<WebStateList> web_state_list_;
 };
-
-TEST_F(LocationBarCoordinatorTest, Starts) {
-  EXPECT_TRUE(coordinator_.locationBarView == nil);
-  [coordinator_ start];
-  EXPECT_TRUE(coordinator_.locationBarView != nil);
-}
 
 TEST_F(LocationBarCoordinatorTest, Stops) {
   EXPECT_TRUE(coordinator_.locationBarView == nil);
