17bb8a59b7f3f0ff873857e4d97b5457850ad7ae
John Z Wu <jzw@chromium.org>
2018-01-24 18:22:36 -0800

    More integration tests for autofill in //ios/web_view.
    One test that verifies fetching, filling, and clearing.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: Icc484e63fc0787bdfcedca95a15528111e4d7dcb

--- ios/web_view/test/web_view_autofill_inttest.mm
+++ ios/web_view/test/web_view_autofill_inttest.mm
@@ -5,7 +5,6 @@
 #import <ChromeWebView/ChromeWebView.h>
 #import <Foundation/Foundation.h>
 
-#include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
 #import "ios/testing/wait_util.h"
 #import "ios/web_view/test/web_view_int_test.h"
@@ -19,6 +18,9 @@
 #error "This file requires ARC support."
 #endif
 
+using testing::kWaitForActionTimeout;
+using testing::WaitUntilConditionOrTimeout;
+
 namespace ios_web_view {
 
 namespace {
@@ -27,7 +29,7 @@ NSString* const kTestFieldName = @"FieldName";
 NSString* const kTestFieldValue = @"FieldValue";
 NSString* const kTestFormHtml =
     [NSString stringWithFormat:
-                  @"<form name='%@'>"
+                  @"<form name='%@' onsubmit='return false;'>"
                    "<input type='text' name='%@' value='%@'/>"
                    "<input type='submit'/>"
                    "</form>",
@@ -98,6 +100,62 @@ TEST_F(WebViewAutofillTest, TestDelegateCallbacks) {
        "document.forms[0].dispatchEvent(event);",
       nil);
   [delegate verify];
+}
+
+// Tests that CWVAutofillController can fetch, fill, and clear suggestions.
+TEST_F(WebViewAutofillTest, TestSuggestionFetchFillClear) {
+  test::EvaluateJavaScript(web_view_,
+                           @"document.forms[0][1].click();"
+                            "document.forms[0][0].value = '';",
+                           nil);
+
+  __block bool suggestions_fetched = false;
+  __block CWVAutofillSuggestion* fetched_suggestion = nil;
+  id fetch_completion_handler =
+      ^(NSArray<CWVAutofillSuggestion*>* suggestions) {
+        EXPECT_EQ(1U, suggestions.count);
+        fetched_suggestion = suggestions.firstObject;
+        EXPECT_NSEQ(kTestFieldValue, fetched_suggestion.value);
+        EXPECT_NSEQ(kTestFormName, fetched_suggestion.formName);
+        EXPECT_NSEQ(kTestFieldName, fetched_suggestion.fieldName);
+        suggestions_fetched = true;
+      };
+  [[web_view_ autofillController]
+      fetchSuggestionsForFormWithName:kTestFormName
+                            fieldName:kTestFieldName
+                    completionHandler:fetch_completion_handler];
+  EXPECT_TRUE(WaitUntilConditionOrTimeout(kWaitForActionTimeout, ^bool {
+    return suggestions_fetched;
+  }));
+
+  // The input element needs to be focused before it can be filled or cleared.
+  test::EvaluateJavaScript(web_view_, @"document.forms[0][0].focus()", nil);
+
+  __block bool suggestion_filled = false;
+  id fill_completion_handler = ^{
+    suggestion_filled = true;
+  };
+  [[web_view_ autofillController] fillSuggestion:fetched_suggestion
+                               completionHandler:fill_completion_handler];
+  EXPECT_TRUE(WaitUntilConditionOrTimeout(kWaitForActionTimeout, ^bool {
+    NSString* filled_value =
+        test::EvaluateJavaScript(web_view_, @"document.forms[0][0].value", nil);
+    EXPECT_NSEQ(fetched_suggestion.value, filled_value);
+    return suggestion_filled;
+  }));
+
+  __block bool form_cleared = false;
+  id clear_completion_handler = ^{
+    form_cleared = true;
+  };
+  [[web_view_ autofillController] clearFormWithName:kTestFormName
+                                  completionHandler:clear_completion_handler];
+  EXPECT_TRUE(WaitUntilConditionOrTimeout(kWaitForActionTimeout, ^bool {
+    NSString* current_value =
+        test::EvaluateJavaScript(web_view_, @"document.forms[0][0].value", nil);
+    EXPECT_NSEQ(@"", current_value);
+    return form_cleared;
+  }));
 }
 
 }  // namespace ios_web_view
