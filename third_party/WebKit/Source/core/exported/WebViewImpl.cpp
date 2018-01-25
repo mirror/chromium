538fed581bbea099a3fbb06635135124a086f382
Peter Mayo <petermayo@chromium.org>
2018-01-25 14:04:45 -0500

    [animation-worklet] Move work to a separate backing thread
    This version introduces the backing thread.
    It uses a completion event to do the animation synchronously; this
    lines up with the division of tasks outlined in the task issues.
    The mutation update is sent asynchronously.
    In moving some classes (CompositorMutator based) to a compositor managed
    lifetime they change to refcounting to match the thread behavior.
    
    Bug: 731727
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: Ia774e53b5bf34e0cef5d7f0dd9d02fc3b27f4b20

--- third_party/WebKit/Source/core/exported/WebViewImpl.cpp
+++ third_party/WebKit/Source/core/exported/WebViewImpl.cpp
@@ -3859,7 +3859,7 @@ CompositorMutatorImpl& WebViewImpl::Mutator() {
 CompositorMutatorImpl* WebViewImpl::CompositorMutator() {
   if (!mutator_) {
     std::unique_ptr<CompositorMutatorClient> mutator_client =
-        CompositorMutatorImpl::CreateClient();
+        CompositorMutatorImpl::CreateClientOnMainThread();
     mutator_ = static_cast<CompositorMutatorImpl*>(mutator_client->Mutator());
     layer_tree_view_->SetMutatorClient(std::move(mutator_client));
   }
