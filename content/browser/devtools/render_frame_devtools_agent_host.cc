dfdf206f7217812700f7278e7eb84b739fffc971
Pavel Feldman <pfeldman@chromium.org>
2018-01-24 16:42:46 -0800

    DevTools: don't update all frames aggresively upon render frame swap.
    We used to detach from targets aggressively because of the timing of the
    UpdateFrames call. Current frame host was still non-cross-process and hence
    was detached from. This change migrates from bulk-frame update to treating
    render frame host swap in AutoAttachToFrame.
    
    Bug: 805248
    Change-Id: Id6c8a665676167fea5048c23db20cfc8fafd86f1

--- content/browser/devtools/render_frame_devtools_agent_host.cc
+++ content/browser/devtools/render_frame_devtools_agent_host.cc
@@ -513,9 +513,6 @@ void RenderFrameDevToolsAgentHost::DidStartNavigation(
 void RenderFrameDevToolsAgentHost::RenderFrameHostChanged(
     RenderFrameHost* old_host,
     RenderFrameHost* new_host) {
-  for (auto* target : protocol::TargetHandler::ForAgentHost(this))
-    target->RenderFrameHostChanged();
-
   if (old_host != frame_host_)
     return;
 
