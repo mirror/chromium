7cb3e513db1c4f74e90bfd2da019183051fdab62
rbpotter <rbpotter@chromium.org>
2018-01-26 15:37:08 -0800

    Print Preview Componentization: Destinations Dialog additions
    Add the recent destinations list and search box, and enable selection
    of printers using the dialog.
    
    Bug: 773928
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I157461a6a2ec5223e88b97067a4ddc8933f08ea8

---
+++ chrome/browser/resources/print_preview/new/print_preview_search_box.js
@@ -0,0 +1,36 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+Polymer({
+  is: 'print-preview-search-box',
+
+  behaviors: [CrSearchFieldBehavior],
+
+  properties: {
+    /** @type {?RegExp} */
+    searchQuery: {
+      type: Object,
+      notify: true,
+    },
+  },
+
+  listeners: {
+    'search-changed': 'onSearchChanged_',
+  },
+
+  /** @return {!HTMLInputElement} */
+  getSearchInput: function() {
+    return this.$.searchInput;
+  },
+
+  /**
+   * @param {!CustomEvent} e Event containing the new search.
+   */
+  onSearchChanged_: function(e) {
+    const safeQuery =
+        e.detail.trim().replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+    this.searchQuery =
+        safeQuery.length > 0 ? new RegExp(`(${safeQuery})`, 'ig') : null;
+  },
+});
