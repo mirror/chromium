c98f9beb670e3d18b181caf874ed6d3380fed120
Brian Salomon <bsalomon@google.com>
2018-01-26 16:01:34 -0500

    Use SkColorType/GL format to create raster SkSurfaces
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I3e7c72de549394761cbb46c60e9553b33ff6656a

--- components/viz/common/resources/resource_format_utils.h
+++ components/viz/common/resources/resource_format_utils.h
@@ -35,7 +35,8 @@ VIZ_RESOURCE_FORMAT_EXPORT bool DoesResourceFormatSupportAlpha(
     ResourceFormat format);
 VIZ_RESOURCE_FORMAT_EXPORT unsigned int TextureStorageFormat(
     ResourceFormat format);
-VIZ_RESOURCE_FORMAT_EXPORT GrPixelConfig ToGrPixelConfig(ResourceFormat format);
+VIZ_RESOURCE_FORMAT_EXPORT unsigned int SizedInternalFormat(
+    ResourceFormat format);
 
 }  // namespace viz
 
