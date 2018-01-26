b52ac75fb0db04f2c775d12da5dad67a05944eab
François Beaufort <beaufort.francois@gmail.com>
2018-01-26 20:01:38 +0000

    [Picture-In-Picture] Expose some APIs behind a runtime feature.
    https://wicg.github.io/picture-in-picture/
    
    The API is currently not working. It is mostly about implementing the shell: it
    exposes document.pictureInPictureEnabled, document.exitPictureInPicture(), and
    video.requestPictureInPicture(). It also adds the "picture-in-picture" feature
    policy. Everything is gated by the "PictureInPictureAPI" runtime feature.
    
    Intent to implement: TODO
    
    Change-Id: I64d8e17c6975017565c850afeeca19d94e9f9ceb
    Bug: 806249

---
+++ third_party/WebKit/Source/modules/picture_in_picture/PictureInPictureController.cpp
@@ -0,0 +1,70 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "modules/picture_in_picture/PictureInPictureController.h"
+
+#include "core/dom/Document.h"
+#include "platform/feature_policy/FeaturePolicy.h"
+
+namespace blink {
+
+PictureInPictureController::PictureInPictureController(Document& document)
+    : Supplement<Document>(document), picture_in_picture_enabled_(true) {}
+
+PictureInPictureController::~PictureInPictureController() = default;
+
+// static
+PictureInPictureController& PictureInPictureController::Ensure(
+    Document& document) {
+  PictureInPictureController* controller =
+      static_cast<PictureInPictureController*>(
+          Supplement<Document>::From(document, SupplementName()));
+  if (!controller) {
+    controller = new PictureInPictureController(document);
+    ProvideTo(document, SupplementName(), controller);
+  }
+  return *controller;
+}
+
+// static
+const char* PictureInPictureController::SupplementName() {
+  return "PictureInPictureController";
+}
+
+bool PictureInPictureController::PictureInPictureEnabled() {
+  PictureInPictureController::Status status = GetStatus();
+  return (status == Status::kEnabled);
+}
+
+PictureInPictureController::Status PictureInPictureController::GetStatus() {
+  DCHECK(GetSupplementable());
+
+  // If document is not allowed to use the policy-controlled feature named
+  // "picture-in-picture", return kDisabledByFeaturePolicy status.
+  LocalFrame* frame = GetSupplementable()->GetFrame();
+  if (IsSupportedInFeaturePolicy(
+          blink::FeaturePolicyFeature::kPictureInPicture) &&
+      !frame->IsFeatureEnabled(
+          blink::FeaturePolicyFeature::kPictureInPicture)) {
+    return Status::kDisabledByFeaturePolicy;
+  }
+
+  // TODO(crbug.com/806249): Handle status when disabled by attribute
+
+  // If Picture-in-Picture is disabled, return kDisabledBySystem status.
+  if (!picture_in_picture_enabled_) {
+    // This happens if not Android O and onwards or if Android user has
+    // deactivated PiP for Chrome.
+    return Status::kDisabledBySystem;
+  }
+
+  return Status::kEnabled;
+}
+
+void PictureInPictureController::SetPictureInPictureEnabledForTesting(
+    bool value) {
+  picture_in_picture_enabled_ = value;
+}
+
+}  // namespace blink
