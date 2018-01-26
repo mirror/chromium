55b32a33b704941300a9d279ce2ca42f802e3036
François Beaufort <beaufort.francois@gmail.com>
2018-01-26 09:03:41 +0000

    [Media Engagement] Filter out origins whose playback is 0 in about:media-engagement
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I042e565d27078e50ae6ac3ce01726452df3a9e2d

--- chrome/browser/resources/media/media_engagement.js
+++ chrome/browser/resources/media/media_engagement.js
@@ -19,6 +19,7 @@ var engagementTableBody = null;
 var sortReverse = true;
 var sortKey = 'totalScore';
 var configTableBody = null;
+var showNoPlaybacks = false;
 
 /**
  * Creates a single row in the engagement table.
@@ -121,7 +122,8 @@ function renderConfigTable(config) {
 function renderTable() {
   clearTable();
   sortInfo();
-  info.forEach(rowInfo => engagementTableBody.appendChild(createRow(rowInfo)));
+  info.filter(rowInfo => (showNoPlaybacks || rowInfo.mediaPlaybacks > 0))
+      .forEach(rowInfo => engagementTableBody.appendChild(createRow(rowInfo)));
 }
 
 /**
@@ -173,5 +175,12 @@ document.addEventListener('DOMContentLoaded', function() {
       renderTable();
     });
   }
+
+  // Add handler to 'show no playbacks' checkbox
+  var showNoPlaybacksCheckbox = $('show-no-playbacks');
+  showNoPlaybacksCheckbox.addEventListener('change', (e) => {
+    showNoPlaybacks = e.target.checked;
+    renderTable();
+  });
 });
 })();
