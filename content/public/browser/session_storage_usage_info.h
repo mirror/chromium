cd54f2a67b998bac2e7d48c5fe907724cda19b95
Daniel Murphy <dmurph@chromium.org>
2018-01-26 11:30:43 -0800

    [SessionStorageMojo] Updating piping for better sequences and lifetimes.
    Bug: 716490
    Change-Id: Ief73b6f0e9b64290a333bd87a1ac213740eac168

--- content/public/browser/session_storage_usage_info.h
+++ content/public/browser/session_storage_usage_info.h
@@ -5,6 +5,8 @@
 #ifndef CONTENT_PUBLIC_BROWSER_SESSION_STORAGE_USAGE_INFO_H_
 #define CONTENT_PUBLIC_BROWSER_SESSION_STORAGE_USAGE_INFO_H_
 
+#include "url/gurl.h"
+
 namespace content {
 
 // Used to report Session Storage usage info by DOMStorageContext.
