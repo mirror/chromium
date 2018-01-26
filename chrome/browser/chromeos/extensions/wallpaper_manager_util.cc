eab53b3436bbb540ab387ff81fa8ab5d75bd6cd4
Wenzhao Zang <wzang@chromium.org>
2018-01-26 10:55:46 -0800

    cros: Move OpenWallpaperPicker to WallpaperControllerClient
    1) Opening wallpaper picker is NOT allowed if:
       a) The active user is policy controlled (must be checked by
          //ash/WallpaperController), or
       b) The active user type is GUEST or KIOSK etc. (can be checked
          from //ash or //chrome).
    
    2) The wallpaper picker can be opened either from the setting page
       (//chrome) or the shelf context menu (//ash). In addition, the
       setting page need to distinguish between cases a) and b), in order
       to show different UI. So we need to create two mojo callbacks.
    
    3) In addition, WallpaperControllerClient should not rely on the
       setting page to check a) and b): it must call into //ash to ask,
       in case a refactoring of the setting page will cause
       regression (there was once a bug because of this.)
    
    4) The check of b) used to rely on LoginState but it was recently
       deprecated, so it's using SessionController in //ash instead.
    
    Bug: 798604
    Test: 1) ash_unittests 2) Manual test by checking the 'Wallpaper' item
          in Settings is a) invisible for guest session, b) disabled for
          users controlled by wallpaper policy, c) enabled for other cases.
    Change-Id: Icb1479d2112f02b823237c418622cc0cdbae65fc

--- chrome/browser/chromeos/extensions/wallpaper_manager_util.cc
+++ chrome/browser/chromeos/extensions/wallpaper_manager_util.cc
@@ -49,10 +49,6 @@ const char kAndroidWallpapersAppPackage[] = "com.google.android.apps.wallpaper";
 const char kAndroidWallpapersAppActivity[] =
     "com.google.android.apps.wallpaper.picker.CategoryPickerActivity";
 
-// Only if the current profile is the primary profile && ARC service is enabled
-// && the Android Wallpapers App has been installed && the finch experiment or
-// chrome flag is enabled, launch the Android Wallpapers App. Otherwise launch
-// the old Chrome OS Wallpaper Picker App.
 bool ShouldUseAndroidWallpapersApp(Profile* profile) {
   if (!chromeos::ProfileHelper::IsPrimaryProfile(profile))
     return false;
