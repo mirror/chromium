8c5e131d44ce91e112e410954a6d0518dc7a1dc9
Bryan McQuade <bmcquade@chromium.org>
2018-01-26 14:39:23 -0500

    Add Android incognito tests.
    Change-Id: If998e13c0f794af05f139f1072298d55db6f3ce8

--- chrome/android/javatests/src/org/chromium/chrome/browser/metrics/UkmIncognitoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/metrics/UkmIncognitoTest.java
@@ -4,6 +4,7 @@
 
 package org.chromium.chrome.browser.metrics;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SmallTest;
 
 import org.junit.Assert;
@@ -16,8 +17,11 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tabmodel.TabModel;
+import org.chromium.chrome.browser.tabmodel.TabModelUtils;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
+import org.chromium.chrome.test.util.ChromeTabUtils;
 import org.chromium.content.browser.test.util.JavaScriptUtils;
 import org.chromium.ui.base.PageTransition;
 
@@ -31,11 +35,18 @@ public class UkmIncognitoTest {
     @Rule
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
 
+    TabModel mIncognito, mNonIncognito;
+
     private static final String DEBUG_PAGE = "chrome://ukm";
 
     @Before
     public void setUp() throws InterruptedException {
         mActivityTestRule.startMainActivityOnBlankPage();
+
+        mIncognito = mActivityTestRule.getActivity().getTabModelSelector().getModel(
+                true /* incognito */);
+        mNonIncognito = mActivityTestRule.getActivity().getTabModelSelector().getModel(
+                false /* !incognito */);
     }
 
     public String getUkmState(Tab normalTab) throws Exception {
@@ -49,13 +60,55 @@ public class UkmIncognitoTest {
     @Test
     @SmallTest
     @RetryOnFailure
-    public void testUkmIncognito() throws Exception {
+    public void testRegularPlusIncognitoCheck() throws Exception {
         Tab normalTab = mActivityTestRule.getActivity().getActivityTab();
 
         Assert.assertEquals("UKM State:", "\"True\"", getUkmState(normalTab));
 
+        // TODO(bmcquade): extract original_client_id from DEBUG_PAGE.
+
         mActivityTestRule.newIncognitoTabFromMenu();
 
         Assert.assertEquals("UKM State:", "\"False\"", getUkmState(normalTab));
+
+        // Opening another regular tab mustn't enable UKM.
+        ChromeTabUtils.newTabFromMenu(
+                InstrumentationRegistry.getInstrumentation(), mActivityTestRule.getActivity());
+        Assert.assertEquals("UKM State:", "\"False\"", getUkmState(normalTab));
+
+        // Opening and closing another Incognito tab mustn't enable UKM.
+        mActivityTestRule.newIncognitoTabFromMenu();
+        TabModelUtils.closeCurrentTab(mIncognito);
+        Assert.assertEquals("UKM State:", "\"False\"", getUkmState(normalTab));
+
+        TabModelUtils.closeCurrentTab(mNonIncognito);
+        normalTab = TabModelUtils.getCurrentTab(mNonIncognito);
+        Assert.assertEquals("UKM State:", "\"False\"", getUkmState(normalTab));
+
+        mIncognito.closeAllTabs();
+        Assert.assertEquals("UKM State:", "\"True\"", getUkmState(normalTab));
+
+        // Client ID should not have been reset.
+        // TODO(bmcquade): extract client id from DEBUG_PAGE.
+    }
+
+    @Test
+    @SmallTest
+    @RetryOnFailure
+    public void testIncognitoPlusRegularCheck() throws Exception {
+        // Close all non-incognito tabs.
+        mNonIncognito.closeAllTabs();
+
+        mActivityTestRule.newIncognitoTabFromMenu();
+
+        ChromeTabUtils.newTabFromMenu(
+                InstrumentationRegistry.getInstrumentation(), mActivityTestRule.getActivity());
+        Tab normalTab = TabModelUtils.getCurrentTab(mNonIncognito);
+
+        Assert.assertEquals("UKM State:", "\"False\"", getUkmState(normalTab));
+
+        TabModelUtils.closeCurrentTab(mNonIncognito);
+
+        Assert.assertEquals("UKM State:", "\"True\"", getUkmState(normalTab));
     }
 }
