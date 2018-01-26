cdb637748acc36de5f32a62fef8c368abf54def9
Tom Sepez <tsepez@chromium.org>
2018-01-26 15:36:38 -0800

    Split ZygoteMain into content/ specific and general parts.
    Change-Id: Ib10951263cfff3cc42826cbc3a2cdd9b735f2741

--- content/zygote/zygote_main_linux.cc
+++ content/zygote/zygote_main_linux.cc
@@ -2,9 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/zygote/zygote_main.h"
+#include "content/zygote/zygote_main_linux.h"
 
-#include <dlfcn.h>
 #include <fcntl.h>
 #include <pthread.h>
 #include <signal.h>
@@ -31,11 +30,7 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/sys_info.h"
 #include "build/build_config.h"
-#include "content/common/font_config_ipc_linux.h"
 #include "content/common/zygote_commands_linux.h"
-#include "content/public/common/common_sandbox_support_linux.h"
-#include "content/public/common/content_descriptors.h"
-#include "content/public/common/content_switches.h"
 #include "content/public/common/zygote_fork_delegate_linux.h"
 #include "content/zygote/zygote_linux.h"
 #include "media/media_features.h"
@@ -49,26 +44,8 @@
 #include "services/service_manager/sandbox/linux/sandbox_debug_handling_linux.h"
 #include "services/service_manager/sandbox/linux/sandbox_linux.h"
 #include "services/service_manager/sandbox/sandbox.h"
-#include "third_party/WebKit/public/web/linux/WebFontRendering.h"
-#include "third_party/boringssl/src/include/openssl/crypto.h"
-#include "third_party/boringssl/src/include/openssl/rand.h"
+#include "services/service_manager/sandbox/switches.h"
 #include "third_party/icu/source/i18n/unicode/timezone.h"
-#include "third_party/skia/include/ports/SkFontConfigInterface.h"
-#include "third_party/skia/include/ports/SkFontMgr_android.h"
-
-#if BUILDFLAG(ENABLE_PLUGINS)
-#include "content/common/pepper_plugin_list.h"
-#include "content/public/common/pepper_plugin_info.h"
-#endif
-
-#if BUILDFLAG(ENABLE_LIBRARY_CDMS)
-#include "content/public/common/cdm_info.h"
-#include "content/public/common/content_client.h"
-#endif
-
-#if BUILDFLAG(ENABLE_WEBRTC)
-#include "third_party/webrtc_overrides/init_webrtc.h"  // nogncheck
-#endif
 
 namespace content {
 
@@ -94,47 +71,9 @@ base::OnceClosure ClosureFromTwoClosures(base::OnceClosure one,
 
 }  // namespace
 
-#if BUILDFLAG(ENABLE_PLUGINS)
-// Loads the (native) libraries but does not initialize them (i.e., does not
-// call PPP_InitializeModule). This is needed by the zygote on Linux to get
-// access to the plugins before entering the sandbox.
-void PreloadPepperPlugins() {
-  std::vector<PepperPluginInfo> plugins;
-  ComputePepperPluginList(&plugins);
-  for (const auto& plugin : plugins) {
-    if (!plugin.is_internal) {
-      base::NativeLibraryLoadError error;
-      base::NativeLibrary library = base::LoadNativeLibrary(plugin.path,
-                                                            &error);
-      VLOG_IF(1, !library) << "Unable to load plugin "
-                           << plugin.path.value() << " "
-                           << error.ToString();
-
-      ignore_result(library);  // Prevent release-mode warning.
-    }
-  }
-}
-#endif
-
-#if BUILDFLAG(ENABLE_LIBRARY_CDMS)
-// Loads registered library CDMs but does not initialize them. This is needed by
-// the zygote on Linux to get access to the CDMs before entering the sandbox.
-void PreloadLibraryCdms() {
-  std::vector<CdmInfo> cdms;
-  GetContentClient()->AddContentDecryptionModules(&cdms, nullptr);
-  for (const auto& cdm : cdms) {
-    base::NativeLibraryLoadError error;
-    base::NativeLibrary library = base::LoadNativeLibrary(cdm.path, &error);
-    VLOG_IF(1, !library) << "Unable to load CDM " << cdm.path.value()
-                         << " (error: " << error.ToString() << ")";
-    ignore_result(library);  // Prevent release-mode warning.
-  }
-}
-#endif  // BUILDFLAG(ENABLE_LIBRARY_CDMS)
-
 // This function triggers the static and lazy construction of objects that need
 // to be created before imposing the sandbox.
-static void ZygotePreSandboxInit() {
+static void ZygotePreSandboxInit(base::OnceClosure pre_sandbox_init) {
   base::RandUint64();
 
   base::SysInfo::AmountOfPhysicalMemory();
@@ -146,62 +85,8 @@ static void ZygotePreSandboxInit() {
   // cached and there's no more need to access the file system.
   std::unique_ptr<icu::TimeZone> zone(icu::TimeZone::createDefault());
 
-#if defined(ARCH_CPU_ARM_FAMILY)
-  // On ARM, BoringSSL requires access to /proc/cpuinfo to determine processor
-  // features. Query this before entering the sandbox.
-  CRYPTO_library_init();
-#endif
-
-  // Pass BoringSSL a copy of the /dev/urandom file descriptor so RAND_bytes
-  // will work inside the sandbox.
-  RAND_set_urandom_fd(base::GetUrandomFD());
-
-#if BUILDFLAG(ENABLE_PLUGINS)
-  // Ensure access to the Pepper plugins before the sandbox is turned on.
-  PreloadPepperPlugins();
-#endif
-#if BUILDFLAG(ENABLE_LIBRARY_CDMS)
-  // Ensure access to the library CDMs before the sandbox is turned on.
-  PreloadLibraryCdms();
-#endif
-#if BUILDFLAG(ENABLE_WEBRTC)
-  InitializeWebRtcModule();
-#endif
-
-  SkFontConfigInterface::SetGlobal(new FontConfigIPC(GetSandboxFD()))->unref();
-
-  // Set the android SkFontMgr for blink. We need to ensure this is done
-  // before the sandbox is initialized to allow the font manager to access
-  // font configuration files on disk.
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kAndroidFontsPath)) {
-    std::string android_fonts_dir =
-        base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
-            switches::kAndroidFontsPath);
-
-    if (android_fonts_dir.size() > 0 && android_fonts_dir.back() != '/')
-      android_fonts_dir += '/';
-
-    SkFontMgr_Android_CustomFonts custom;
-    custom.fSystemFontUse =
-        SkFontMgr_Android_CustomFonts::SystemFontUse::kOnlyCustom;
-    custom.fBasePath = android_fonts_dir.c_str();
-
-    std::string font_config;
-    std::string fallback_font_config;
-    if (android_fonts_dir.find("kitkat") != std::string::npos) {
-      font_config = android_fonts_dir + "system_fonts.xml";
-      fallback_font_config = android_fonts_dir + "fallback_fonts.xml";
-      custom.fFallbackFontsXml = fallback_font_config.c_str();
-    } else {
-      font_config = android_fonts_dir + "fonts.xml";
-      custom.fFallbackFontsXml = nullptr;
-    }
-    custom.fFontsXml = font_config.c_str();
-    custom.fIsolated = true;
-
-    blink::WebFontRendering::SetSkiaFontManager(SkFontMgr_New_Android(&custom));
-  }
+  if (!pre_sandbox_init.is_null())
+    std::move(pre_sandbox_init).Run();
 }
 
 static bool CreateInitProcessReaper(
@@ -269,10 +154,9 @@ static void EnterNamespaceSandbox(service_manager::SandboxLinux* linux_sandbox,
 
 static void EnterLayerOneSandbox(service_manager::SandboxLinux* linux_sandbox,
                                  const bool using_layer1_sandbox,
+                                 base::OnceClosure pre_sandbox_init,
                                  base::OnceClosure post_fork_parent_callback) {
-  DCHECK(linux_sandbox);
-
-  ZygotePreSandboxInit();
+  ZygotePreSandboxInit(std::move(pre_sandbox_init));
 
 // Check that the pre-sandbox initialization didn't spawn threads.
 // It's not just our code which may do so - some system-installed libraries
@@ -295,14 +179,16 @@ static void EnterLayerOneSandbox(service_manager::SandboxLinux* linux_sandbox,
 }
 
 bool ZygoteMain(
+    base::OnceClosure pre_sandbox_init,
+    base::GlobalDescriptors::Descriptor backchannel_descriptor,
     std::vector<std::unique_ptr<ZygoteForkDelegate>> fork_delegates) {
-  sandbox::SetAmZygoteOrRenderer(true, GetSandboxFD());
+  sandbox::SetAmZygoteOrDescendant(true, backchannel_descriptor.fd);
 
   auto* linux_sandbox = service_manager::SandboxLinux::GetInstance();
 
   // Skip pre-initializing sandbox under --no-sandbox for crbug.com/444900.
   if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kNoSandbox)) {
+          service_manager::switches::kNoSandbox)) {
     // This will pre-initialize the various sandboxes that need it.
     linux_sandbox->PreinitializeSandbox();
   }
@@ -334,12 +220,12 @@ bool ZygoteMain(
   VLOG(1) << "ZygoteMain: initializing " << fork_delegates.size()
           << " fork delegates";
   for (const auto& fork_delegate : fork_delegates) {
-    fork_delegate->Init(GetSandboxFD(), using_layer1_sandbox);
+    fork_delegate->Init(backchannel_descriptor.fd, using_layer1_sandbox);
   }
 
   // Turn on the first layer of the sandbox if the configuration warrants it.
   EnterLayerOneSandbox(
-      linux_sandbox, using_layer1_sandbox,
+      linux_sandbox, using_layer1_sandbox, std::move(pre_sandbox_init),
       base::BindOnce(CloseFds, linux_sandbox->GetFileDescriptorsToClose()));
 
   const int sandbox_flags = linux_sandbox->GetStatus();
@@ -351,12 +237,10 @@ bool ZygoteMain(
       !!(sandbox_flags & service_manager::SandboxLinux::kUserNS);
   CHECK_EQ(using_namespace_sandbox, namespace_sandbox_engaged);
 
-  Zygote zygote(sandbox_flags, std::move(fork_delegates),
-                base::GlobalDescriptors::Descriptor(
-                    static_cast<uint32_t>(kSandboxIPCChannel), GetSandboxFD()));
-
   // This function call can return multiple times, once per fork().
-  return zygote.ProcessRequests();
+  return Zygote(sandbox_flags, std::move(fork_delegates),
+                std::move(backchannel_descriptor))
+      .ProcessRequests();
 }
 
 }  // namespace content
