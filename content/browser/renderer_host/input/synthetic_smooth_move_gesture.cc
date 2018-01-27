7927150b835741923d3e843f4273bfd6008d3f39
Nico Weber <thakis@chromium.org>
2018-01-26 23:13:18 -0500

    Work more on getting things built with -Wimplicit-fallthrough.
    This CL was uploaded by git cl split.
    
    R=dtapuska@chromium.org
    
    Bug: 177475
    Change-Id: I2c3ed9b3cbab13e6f120cf542119fc9005490bc7

--- content/browser/renderer_host/input/synthetic_smooth_move_gesture.cc
+++ content/browser/renderer_host/input/synthetic_smooth_move_gesture.cc
@@ -199,6 +199,7 @@ void SyntheticSmoothMoveGesture::ForwardMouseWheelInputEvents(
     case SETUP:
       NOTREACHED() << "State SETUP invalid for synthetic scroll using mouse "
                       "wheel input.";
+      break;
     case STOPPING:
       NOTREACHED() << "State STOPPING invalid for synthetic scroll using mouse "
                       "wheel input.";
