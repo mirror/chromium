7fa805e1580b2c95346d90404d0d04a630ccbd62
Sandra Sun <sunyunjia@chromium.org>
2018-01-28 17:58:54 -0500

    Support viewport's snapping on cc.
    To support viewport snapping on cc, before RootLayerScrolling is
    enabled, we should plumb the SnapContainerData to the composited layers in
    PaintLayerCompositor.
    
    Bug: 795404
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I836f291e1c43d54f3d2470702934d6b63e8fc964

--- third_party/WebKit/LayoutTests/resources/testharnessreport.js
+++ third_party/WebKit/LayoutTests/resources/testharnessreport.js
@@ -110,7 +110,8 @@
             pathAndBase.startsWith('/pointerlock/') ||
             pathAndBase.startsWith('/html/') ||
             pathAndBase.startsWith('/input-events/') ||
-            pathAndBase.startsWith('/css/selectors/')) {
+            pathAndBase.startsWith('/css/selectors/') ||
+            pathAndBase.startsWith('/css/css-scroll-snap/')) {
             // Per-test automation scripts.
             src = automationPath + pathAndBase + '-automation.js';
         } else {
