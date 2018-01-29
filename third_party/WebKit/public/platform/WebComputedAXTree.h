9b650f7309c1f42c4a9752d8f4fee5c0d8c1c8ee
Meredith Lane <meredithl@google.com>
2018-01-29 11:33:31 +1100

    Implement int accessors for ComputedAccessibleNode
    Adds the attributes into the ComputedAccessibleNode interface, and
    accessors on the content tree for an AXNode with a given ID. This is the
    first of the accessors for the wider set of ComputedAccessibleNode's
    properties. The other types will be added in seperate CLs.
    
    This change also introduces an AOMIntAttribute enum, which is translated
    to an AXIntAttribute inside content to retrieve the corresponding
    accessible property. This will also be expanded to the other types in
    later CLs.
    
    BUG=792783
    
    Change-Id: I39875ae8eef03620b85c8480fd1ffc590c3591d5

--- third_party/WebKit/public/platform/WebComputedAXTree.h
+++ third_party/WebKit/public/platform/WebComputedAXTree.h
@@ -9,6 +9,19 @@
 
 namespace blink {
 
+enum WebAOMIntAttribute {
+  WEB_AOM_INT_ATTRIBUTE_NONE,
+  WEB_AOM_ATTR_COLUMN_COUNT,
+  WEB_AOM_ATTR_COLUMN_INDEX,
+  WEB_AOM_ATTR_COLUMN_SPAN,
+  WEB_AOM_ATTR_HIERARCHICAL_LEVEL,
+  WEB_AOM_ATTR_POS_IN_SET,
+  WEB_AOM_ATTR_ROW_COUNT,
+  WEB_AOM_ATTR_ROW_INDEX,
+  WEB_AOM_ATTR_ROW_SPAN,
+  WEB_AOM_ATTR_SET_SIZE,
+};
+
 class WebComputedAXTree {
  public:
   virtual ~WebComputedAXTree() {}
@@ -16,6 +29,13 @@ class WebComputedAXTree {
   virtual bool ComputeAccessibilityTree() = 0;
   virtual WebString GetNameForAXNode(int32_t axID) = 0;
   virtual WebString GetRoleForAXNode(int32_t axID) = 0;
+
+  // Get the specified attribute for a given AXID, returning true if the
+  // node exists in the tree and contains the attribute. Stores the result in
+  // |out_param|.
+  virtual bool GetIntAttributeForAXNode(int32_t axID,
+                                        WebAOMIntAttribute,
+                                        int32_t* out_param) = 0;
 };
 
 }  // namespace blink
