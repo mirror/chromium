cbe96ac005b0b09cfe3a7ea930c689e519721d68
Michael Giuffrida <michaelpg@chromium.org>
2018-01-28 16:50:36 -0800

    Observe window changes with BrowserWindowObserver
    Add an observer interface to BrowserWindow to listen for possible
    changes to the window show state (minimizing, maximizing, etc.). This is
    useful for logging updated WindowMetrics UKMs to describe the new state
    of the window.
    
    It should also be useful as a general pattern. Currently,
    SaveWindowPlacement() exists on WidgetDelegate and ViewsDelegate in
    order to route window changes specifically to Chrome browser windows to
    update session restore settings. That can be updated to use a general
    Observer pattern by adding a BoundsChanged method to the
    BrowserWindowObserver.
    
    Note that the observer being called doesn't guarantee the show state
    actually did change, e.g. if a window tries to enter fullscreen but
    doesn't successfully do so the observer might still be called.
    
    The observer interface could also exist on BaseWindow instead of
    BrowserWindow; it should be moved there if we start logging window UKMs
    for app windows.
    
    Bug: 791824
    Change-Id: I3f10fade877fc3d63370efa7c5bfa12c0b270b1e

---
+++ chrome/browser/ui/browser_window_observer.h
@@ -0,0 +1,20 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_BROWSER_WINDOW_OBSERVER_H_
+#define CHROME_BROWSER_UI_BROWSER_WINDOW_OBSERVER_H_
+
+#include "ui/base/ui_base_types.h"
+
+// Observes changes to window show state.
+class BrowserWindowObserver {
+ public:
+  // Called when the window show state changes.
+  virtual void OnShowStateChanged(ui::WindowShowState show_state) = 0;
+
+ protected:
+  virtual ~BrowserWindowObserver() = default;
+};
+
+#endif  // CHROME_BROWSER_UI_BROWSER_WINDOW_OBSERVER_H_
