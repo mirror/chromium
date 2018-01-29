984639d656cd085e79c4e701b384aac20a99d032
Xiaohui Chen <xiaohuic@google.com>
2018-01-29 14:13:06 -0800

    Add empty assistant service and stub platform impl
    Bug: 807015
    Test: compiles with ninja -C out chrome
    Change-Id: I66fc1bb6b3f44027b283f224841397b1d3245fc2

---
+++ chrome/browser/chromeos/assistant/assistant_service.h
@@ -0,0 +1,52 @@
+// Copyright (c) 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_ASSISTANT_ASSISTANT_SERVICE_H_
+#define CHROME_BROWSER_CHROMEOS_ASSISTANT_ASSISTANT_SERVICE_H_
+
+#include <memory>
+#include <string>
+
+// TODO(xiaohuic): replace with "base/macros.h" once we remove
+// libassistant/contrib dependency.
+#include "chrome/browser/chromeos/assistant/platform_api_impl.h"
+#include "libassistant/contrib/core/macros.h"
+
+namespace assistant_client {
+class AssistantManager;
+class AssistantManagerInternal;
+class PlatformApi;
+}  // namespace assistant_client
+
+namespace chromeos {
+namespace assistant {
+
+class AssistantService {
+ public:
+  class AuthTokenProvider {
+   public:
+    virtual std::string& GetAccessToken();
+
+   protected:
+    virtual ~AuthTokenProvider();
+  };
+
+  explicit AssistantService(AuthTokenProvider* auth_token_provider);
+  virtual ~AssistantService();
+
+  void Start();
+
+ private:
+  std::unique_ptr<PlatformApiImpl> platform_api_;
+  std::unique_ptr<assistant_client::AssistantManager> assistant_manager_;
+  assistant_client::AssistantManagerInternal* assistant_manager_internal_;
+  AuthTokenProvider* auth_token_provider_;
+
+  DISALLOW_COPY_AND_ASSIGN(AssistantService);
+};
+
+}  // namespace assistant
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_ASSISTANT_ASSISTANT_SERVICE_H_
