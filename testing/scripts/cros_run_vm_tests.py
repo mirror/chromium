9f523648a746d3482ce9314dfb9eeff8f03b0142
Achuith Bhandarkar <achuith@chromium.org>
2018-01-29 15:47:49 -0800

    cros_run_vm_tests.py for amd64_generic bots.
    To run this locally:
    change running_on_bot = False
    update checkout to the root of your checkout
    testing/scripts/cros_run_vm_tests.py run --output /var/tmp/foo
    
    BUG=chromium:724327
    
    Change-Id: I7f2922d02cd58aeb9dafa8354b8bee32f51127ac

---
+++ testing/scripts/cros_run_vm_tests.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import json
+import os
+import sys
+
+
+import common
+
+
+def main_run(args):
+  running_on_bot = True
+  if running_on_bot:
+    checkout = args.paths['checkout']
+    build_config_fs = args.build_config_fs
+    extra_sdk_args = ['--clear-sdk-cache']
+    extra_vm_test_args = ['--disable-kvm', '--no-display']
+  else:
+    checkout = os.path.abspath(os.path.join(os.path.realpath(__file__),
+                                           '../../../'))
+    build_config_fs = 'Release'
+    extra_sdk_args = []
+    extra_vm_test_args = []
+
+  board = 'amd64-generic'
+  sdk_args = ['cros', 'chrome-sdk', '--board', board, '--download-vm',
+              '--internal']
+  vm_test_args = ['--', 'cros_run_vm_test', '--deploy', '--build-dir',
+                  os.path.join(checkout, 'out_' + board, build_config_fs)]
+
+  run_args = sdk_args + extra_sdk_args + vm_test_args + extra_vm_test_args
+
+  rc = common.run_command(run_args, cwd=checkout)
+
+  failures = ['vm_sanity'] if rc else []
+
+  json.dump({
+      'valid': True,
+      'failures': failures
+  }, args.output)
+
+  return rc
+
+
+def main_compile_targets(args):
+  json.dump([], args.output)
+
+
+if __name__ == '__main__':
+  funcs = {
+    'run': main_run,
+    'compile_targets': main_compile_targets,
+  }
+  sys.exit(common.run_script(sys.argv[1:], funcs))
