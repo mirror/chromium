92f2bc9627e0316ebf276037f624630af9fcf5ff
Gauthier Ambard <gambard@chromium.org>
2018-01-29 10:12:57 +0100

    Strip the query part when fetching the favicon.ico
    This CL changes the creation of the URL used to get the favicon at the
    root of the WebSite (www.website.com/favicon.ico), which is used to get
    a favicon if non is provided in the html of the page.
    It strips the query part of the URL, to avoid re-downloading a new
    favicon everytime the query part of the URL changes.
    
    Bug:804077
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I1d6224af357065ab749031ca3533d5f5a5d092ff

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -2459,6 +2459,7 @@ registerLoadRequestForURL:(const GURL&)requestURL
       if (originGURL.is_valid() && originGURL.SchemeIsHTTPOrHTTPS()) {
         GURL::Replacements replacements;
         replacements.SetPathStr("/favicon.ico");
+        replacements.ClearQuery();
         urls.push_back(web::FaviconURL(
             originGURL.ReplaceComponents(replacements),
             web::FaviconURL::IconType::kFavicon, std::vector<gfx::Size>()));
