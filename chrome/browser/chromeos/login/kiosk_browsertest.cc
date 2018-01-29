2a973d5ba9b24c36619889b47c28daaa8f99c838
Olga Sharonova <olka@chromium.org>
2018-01-29 15:47:02 +0100

    Audio service embedded in content and AudioSystem on top of it
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie69df7bf9f593fda6a2e85f5f57a49a90aeccdb9

--- chrome/browser/chromeos/login/kiosk_browsertest.cc
+++ chrome/browser/chromeos/login/kiosk_browsertest.cc
@@ -80,6 +80,7 @@
 #include "media/audio/sounds/sounds_manager.h"
 #include "media/audio/test_audio_thread.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "services/audio/public/cpp/mock_system_info.h"
 #include "ui/aura/window.h"
 #include "ui/base/accelerators/accelerator.h"
 #include "ui/keyboard/keyboard_switches.h"
@@ -2371,6 +2372,7 @@ class KioskVirtualKeyboardTest : public KioskTest {
 
   // Use class variable for sane lifetime.
   std::unique_ptr<media::MockAudioManager> mock_audio_manager_;
+  std::unique_ptr<audio::MockSystemInfo> mock_system_info_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(KioskVirtualKeyboardTest);
@@ -2384,6 +2386,9 @@ IN_PROC_BROWSER_TEST_F(KioskVirtualKeyboardTest, RestrictFeatures) {
   mock_audio_manager_ = std::make_unique<media::MockAudioManager>(
       std::make_unique<media::TestAudioThread>());
   mock_audio_manager_->SetHasInputDevices(true);
+  mock_system_info_ =
+      std::make_unique<audio::MockSystemInfo>(mock_audio_manager_.get());
+  audio::MockSystemInfo::OverrideGlobalBinder(mock_system_info_.get());
 
   set_test_app_id(kTestVirtualKeyboardKioskApp);
   set_test_app_version("0.1");
@@ -2391,7 +2396,7 @@ IN_PROC_BROWSER_TEST_F(KioskVirtualKeyboardTest, RestrictFeatures) {
 
   extensions::ResultCatcher catcher;
   StartAppLaunchFromLoginScreen(SimulateNetworkOnlineClosure());
-  ASSERT_TRUE(catcher.GetNextResult()) << catcher.message();
+  EXPECT_TRUE(catcher.GetNextResult()) << catcher.message();
 
   // Shutdown should be done in the same thread, thus not in the destructor.
   mock_audio_manager_->Shutdown();
