d970de1c79e642f3a7ce247d651c760a9c6270ce
Gauthier Ambard <gambard@chromium.org>
2018-01-29 15:04:43 +0100

    Display WKWebView below the toolbar
    This CL changes the scroll view of the WKWebView used in
    CRWWebViewContentView so it doesn't clips to bound its content.
    It allows the content of the WebView to be fully displayed on the page,
    including below the toolbar.
    
    Bug:805845
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I3591b354a21ebb7b9f29500bc93f697e37547129

--- ios/web/public/web_state/ui/crw_web_view_scroll_view_proxy.h
+++ ios/web/public/web_state/ui/crw_web_view_scroll_view_proxy.h
@@ -48,6 +48,9 @@
 - (void)removeGestureRecognizer:(UIGestureRecognizer*)gestureRecognizer;
 - (void)setContentOffset:(CGPoint)contentOffset animated:(BOOL)animated;
 
+// Sets the -clipsToBounds property of the scroll view to |clipsToBounds|.
+- (void)setClipsToBounds:(BOOL)clipsToBounds;
+
 // Used by the CRWWebViewProxy to set the UIScrollView to be managed.
 - (void)setScrollView:(UIScrollView*)scrollView;
 
