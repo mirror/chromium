3afefd3edd8053fc8ed4bd0c68aa6d55d904d52e
Wenzhao Zang <wzang@chromium.org>
2018-01-29 14:42:27 -0800

    wallpaper refactoring: Switch to ShowUserWallpaper in //ash
    The implementation of ShowUserWallpaper has been fully moved to //ash.
    
    Bug: 779221
    Test: ash_unittests, manual
    Change-Id: I3c8de532228669baf56e336ad91c467407cfd9e5

--- chrome/browser/chromeos/login/users/chrome_user_manager_impl.cc
+++ chrome/browser/chromeos/login/users/chrome_user_manager_impl.cc
@@ -48,7 +48,6 @@
 #include "chrome/browser/chromeos/login/users/default_user_image/default_user_images.h"
 #include "chrome/browser/chromeos/login/users/multi_profile_user_controller.h"
 #include "chrome/browser/chromeos/login/users/supervised_user_manager_impl.h"
-#include "chrome/browser/chromeos/login/users/wallpaper/wallpaper_manager.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/printing/external_printers.h"
 #include "chrome/browser/chromeos/printing/external_printers_factory.h"
@@ -810,7 +809,8 @@ void ChromeUserManagerImpl::GuestUserLoggedIn() {
       user_manager::User::USER_IMAGE_INVALID, false);
 
   // Initializes wallpaper after active_user_ is set.
-  WallpaperManager::Get()->ShowUserWallpaper(user_manager::GuestAccountId());
+  WallpaperControllerClient::Get()->ShowUserWallpaper(
+      user_manager::GuestAccountId());
 }
 
 void ChromeUserManagerImpl::RegularUserLoggedIn(
@@ -838,7 +838,7 @@ void ChromeUserManagerImpl::RegularUserLoggedInAsEphemeral(
   ChromeUserManager::RegularUserLoggedInAsEphemeral(account_id, user_type);
 
   GetUserImageManager(account_id)->UserLoggedIn(IsCurrentUserNew(), false);
-  WallpaperManager::Get()->ShowUserWallpaper(account_id);
+  WallpaperControllerClient::Get()->ShowUserWallpaper(account_id);
 }
 
 void ChromeUserManagerImpl::SupervisedUserLoggedIn(
@@ -909,7 +909,7 @@ void ChromeUserManagerImpl::KioskAppLoggedIn(user_manager::User* user) {
       user_manager::User::USER_IMAGE_INVALID, false);
 
   const AccountId& kiosk_app_account_id = user->GetAccountId();
-  WallpaperManager::Get()->ShowUserWallpaper(kiosk_app_account_id);
+  WallpaperControllerClient::Get()->ShowUserWallpaper(kiosk_app_account_id);
 
   // TODO(bartfab): Add KioskAppUsers to the users_ list and keep metadata like
   // the kiosk_app_id in these objects, removing the need to re-parse the
@@ -979,7 +979,8 @@ void ChromeUserManagerImpl::DemoAccountLoggedIn() {
           *ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
               IDR_LOGIN_DEFAULT_USER)),
       user_manager::User::USER_IMAGE_INVALID, false);
-  WallpaperManager::Get()->ShowUserWallpaper(user_manager::DemoAccountId());
+  WallpaperControllerClient::Get()->ShowUserWallpaper(
+      user_manager::DemoAccountId());
 
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
   command_line->AppendSwitch(::switches::kForceAppMode);
