d5e093253cdf867e5af3cd40c228b4f7a8ba074d
David Roger <droger@chromium.org>
2018-01-30 17:18:42 +0100

    [Unity] Add feature flag for enabling Unity
    Change-Id: I44027badf2aaa5d4aa556ab6bfb686b0e8913b98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3687,6 +3687,10 @@ const FeatureEntry kFeatureEntries[] = {
      SINGLE_VALUE_TYPE(chromeos::switches::kEnableDisplayZoomSetting)},
 #endif  // defined(OS_CHROMEOS)
 
+    {"unified-consent", flag_descriptions::kUnifiedConsentName,
+     flag_descriptions::kUnifiedConsentDescription, kOsAll,
+     FEATURE_VALUE_TYPE(features::kUnifiedConsent)},
+
     // NOTE: Adding a new flag requires adding a corresponding entry to enum
     // "LoginCustomFlags" in tools/metrics/histograms/enums.xml. See "Flag
     // Histograms" in tools/metrics/histograms/README.md (run the
