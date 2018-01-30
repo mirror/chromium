ce0f1ec1a267f7196e443910abc83d8469e858e0
Sandra Sun <sunyunjia@chromium.org>
2018-01-29 20:56:02 -0500

    Programmatic scrolls should land on snap points.
    Bug: 803606
    Change-Id: I1e70d88f31a2b2f93c92cc063adf653ed923adeb

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -135,6 +135,7 @@
 #include "core/page/scrolling/ScrollCustomizationCallbacks.h"
 #include "core/page/scrolling/ScrollState.h"
 #include "core/page/scrolling/ScrollStateCallback.h"
+#include "core/page/scrolling/SnapCoordinator.h"
 #include "core/page/scrolling/TopDocumentRootScrollerController.h"
 #include "core/paint/PaintLayer.h"
 #include "core/probe/CoreProbes.h"
@@ -149,6 +150,7 @@
 #include "platform/bindings/V8DOMWrapper.h"
 #include "platform/bindings/V8PerContextData.h"
 #include "platform/runtime_enabled_features.h"
+#include "platform/scroll/ScrollTypes.h"
 #include "platform/scroll/ScrollableArea.h"
 #include "platform/scroll/SmoothScrollSequencer.h"
 #include "platform/wtf/BitVector.h"
@@ -911,8 +913,18 @@ void Element::setScrollLeft(double new_left) {
   new_left = ScrollableArea::NormalizeNonFiniteScroll(new_left);
 
   if (GetDocument().ScrollingElementNoLayout() == this) {
-    if (LocalDOMWindow* window = GetDocument().domWindow())
-      window->scrollTo(new_left, window->scrollY());
+    LocalFrame* frame = GetDocument().GetFrame();
+    if (!frame || !frame->View() || !GetDocument().GetPage())
+      return;
+
+    ScrollableArea* viewport = frame->View()->LayoutViewportScrollableArea();
+    if (!viewport)
+      return;
+
+    double scaled_y = viewport->GetScrollOffset().Height();
+    double scaled_x = new_left * frame->PageZoomFactor();
+    viewport->SetScrollOffset(ScrollOffset(scaled_x, scaled_y),
+                              kProgrammaticScroll);
   } else {
     LayoutBox* box = GetLayoutBox();
     if (box)
@@ -930,8 +942,18 @@ void Element::setScrollTop(double new_top) {
   new_top = ScrollableArea::NormalizeNonFiniteScroll(new_top);
 
   if (GetDocument().ScrollingElementNoLayout() == this) {
-    if (LocalDOMWindow* window = GetDocument().domWindow())
-      window->scrollTo(window->scrollX(), new_top);
+    LocalFrame* frame = GetDocument().GetFrame();
+    if (!frame || !frame->View() || !GetDocument().GetPage())
+      return;
+
+    ScrollableArea* viewport = frame->View()->LayoutViewportScrollableArea();
+    if (!viewport)
+      return;
+
+    double scaled_x = viewport->GetScrollOffset().Width();
+    double scaled_y = new_top * frame->PageZoomFactor();
+    viewport->SetScrollOffset(ScrollOffset(scaled_x, scaled_y),
+                              kProgrammaticScroll);
   } else {
     LayoutBox* box = GetLayoutBox();
     if (box)
@@ -1057,8 +1079,14 @@ void Element::ScrollLayoutBoxBy(const ScrollToOptions& scroll_to_options) {
         left * box->Style()->EffectiveZoom() + current_scaled_left;
     float new_scaled_top =
         top * box->Style()->EffectiveZoom() + current_scaled_top;
-    box->ScrollToPosition(FloatPoint(new_scaled_left, new_scaled_top),
-                          scroll_behavior);
+
+    FloatPoint new_scaled_position(new_scaled_left, new_scaled_top);
+    if (SnapCoordinator* coordinator = GetDocument().GetSnapCoordinator()) {
+      new_scaled_position = coordinator->GetSnapPositionForNaturalPosition(
+          *box, new_scaled_position, scroll_to_options.hasLeft(),
+          scroll_to_options.hasTop());
+    }
+    box->ScrollToPosition(new_scaled_position, scroll_behavior);
   }
 }
 
@@ -1079,7 +1107,14 @@ void Element::ScrollLayoutBoxTo(const ScrollToOptions& scroll_to_options) {
       scaled_top =
           ScrollableArea::NormalizeNonFiniteScroll(scroll_to_options.top()) *
           box->Style()->EffectiveZoom();
-    box->ScrollToPosition(FloatPoint(scaled_left, scaled_top), scroll_behavior);
+
+    FloatPoint new_scaled_position(scaled_left, scaled_top);
+    if (SnapCoordinator* coordinator = GetDocument().GetSnapCoordinator()) {
+      new_scaled_position = coordinator->GetSnapPositionForNaturalPosition(
+          *box, new_scaled_position, scroll_to_options.hasLeft(),
+          scroll_to_options.hasTop());
+    }
+    box->ScrollToPosition(new_scaled_position, scroll_behavior);
   }
 }
 
@@ -1108,8 +1143,17 @@ void Element::ScrollFrameBy(const ScrollToOptions& scroll_to_options) {
       left * frame->PageZoomFactor() + viewport->GetScrollOffset().Width();
   float new_scaled_top =
       top * frame->PageZoomFactor() + viewport->GetScrollOffset().Height();
-  viewport->SetScrollOffset(ScrollOffset(new_scaled_left, new_scaled_top),
-                            kProgrammaticScroll, scroll_behavior);
+
+  FloatPoint new_scaled_position = ScrollOffsetToPosition(
+      ScrollOffset(new_scaled_left, new_scaled_top), viewport->ScrollOrigin());
+  if (SnapCoordinator* coordinator = GetDocument().GetSnapCoordinator()) {
+    new_scaled_position = coordinator->GetSnapPositionForNaturalPosition(
+        *GetDocument().GetLayoutView(), new_scaled_position,
+        scroll_to_options.hasLeft(), scroll_to_options.hasTop());
+  }
+  viewport->SetScrollOffset(
+      ScrollPositionToOffset(new_scaled_position, viewport->ScrollOrigin()),
+      kProgrammaticScroll, scroll_behavior);
 }
 
 void Element::ScrollFrameTo(const ScrollToOptions& scroll_to_options) {
@@ -1134,8 +1178,17 @@ void Element::ScrollFrameTo(const ScrollToOptions& scroll_to_options) {
     scaled_top =
         ScrollableArea::NormalizeNonFiniteScroll(scroll_to_options.top()) *
         frame->PageZoomFactor();
-  viewport->SetScrollOffset(ScrollOffset(scaled_left, scaled_top),
-                            kProgrammaticScroll, scroll_behavior);
+
+  FloatPoint new_scaled_position = ScrollOffsetToPosition(
+      ScrollOffset(scaled_left, scaled_top), viewport->ScrollOrigin());
+  if (SnapCoordinator* coordinator = GetDocument().GetSnapCoordinator()) {
+    new_scaled_position = coordinator->GetSnapPositionForNaturalPosition(
+        *GetDocument().GetLayoutView(), new_scaled_position,
+        scroll_to_options.hasLeft(), scroll_to_options.hasTop());
+  }
+  viewport->SetScrollOffset(
+      ScrollPositionToOffset(new_scaled_position, viewport->ScrollOrigin()),
+      kProgrammaticScroll, scroll_behavior);
 }
 
 bool Element::HasNonEmptyLayoutSize() const {
