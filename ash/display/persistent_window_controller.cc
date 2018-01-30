94403ad6667b32b17aa123eae64bf6549c4de78e
Qiang Xu <warx@google.com>
2018-01-30 09:13:01 -0800

    cros: restore bounds on Dis{Re}Connect & Off{On} mirror mode
    changes:
    - WindowState caches a member for saving (display_id, bounds_in_screen)
      info, which is saved on OnWillProcessDisplayChanges() and restored on
      OnDisplayConfigurationChanged().
    - Saving when it hasn't been saved. This is based on the fact that info
      is reset when window bounds is changed by user.
    - Restoring when (1) we have saved persistent info, (2) persistent
      display id is a valid display currently, (3) persistent display id
      is not current display id.
    
    This is the first CL of a series of changes.
    
    Bug: 805046
    Test: ash_unittests --gtest_filter=PersistentWindowControllerTest*
    Change-Id: I67426ca17f0d25f92284eb690f849bf77bb748be

---
+++ ash/display/persistent_window_controller.cc
@@ -0,0 +1,95 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/display/persistent_window_controller.h"
+
+#include "ash/public/cpp/ash_switches.h"
+#include "ash/session/session_controller.h"
+#include "ash/shell.h"
+#include "ash/wm/mru_window_tracker.h"
+#include "ash/wm/window_state.h"
+#include "base/command_line.h"
+#include "ui/display/manager/display_manager.h"
+#include "ui/display/screen.h"
+
+namespace ash {
+
+namespace {
+
+display::DisplayManager* GetDisplayManager() {
+  return Shell::Get()->display_manager();
+}
+
+MruWindowTracker::WindowList GetWindowList() {
+  return Shell::Get()->mru_window_tracker()->BuildWindowForCycleList();
+}
+
+bool ShouldProcessWindowList() {
+  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kAshEnablePersistentWindowBounds)) {
+    return false;
+  }
+
+  if (Shell::Get()->session_controller()->IsUserSessionBlocked())
+    return false;
+
+  if (GetDisplayManager()->IsInMirrorMode())
+    return false;
+
+  return true;
+}
+
+}  // namespace
+
+PersistentWindowController::PersistentWindowController(
+    WindowTreeHostManager* window_tree_host_manager)
+    : screen_(display::Screen::GetScreen()),
+      window_tree_host_manager_(window_tree_host_manager) {
+  screen_->AddObserver(this);
+  window_tree_host_manager_->AddObserver(this);
+}
+
+PersistentWindowController::~PersistentWindowController() {
+  window_tree_host_manager_->RemoveObserver(this);
+  screen_->RemoveObserver(this);
+}
+
+void PersistentWindowController::OnWillProcessDisplayChanges() {
+  if (!ShouldProcessWindowList())
+    return;
+
+  for (auto* window : GetWindowList()) {
+    wm::WindowState* window_state = wm::GetWindowState(window);
+    if (!window_state->persistent_window_info()) {
+      const int64_t display_id = screen_->GetDisplayNearestWindow(window).id();
+      window_state->SetPersistentWindowInfo(
+          std::make_pair(display_id, window->GetBoundsInScreen()));
+    }
+  }
+}
+
+void PersistentWindowController::OnDisplayConfigurationChanged() {
+  if (!ShouldProcessWindowList())
+    return;
+
+  for (auto* window : GetWindowList()) {
+    wm::WindowState* window_state = wm::GetWindowState(window);
+    if (!window_state->persistent_window_info())
+      continue;
+    const auto persistent_window_info = *window_state->persistent_window_info();
+    const int64_t persistent_display_id = persistent_window_info.first;
+    const gfx::Rect persistent_window_bounds = persistent_window_info.second;
+    if (persistent_display_id ==
+            screen_->GetDisplayNearestWindow(window).id() ||
+        !GetDisplayManager()->IsDisplayIdValid(persistent_display_id)) {
+      continue;
+    }
+
+    const auto display = display::Screen::GetScreen()->GetDisplayMatching(
+        persistent_window_bounds);
+    window->SetBoundsInScreen(persistent_window_bounds, display);
+  }
+}
+
+}  // namespace ash
