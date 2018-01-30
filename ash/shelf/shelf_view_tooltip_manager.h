13b6cceadcc8cc016484d713ca26bfb6a896a489
MinChen <minch@chromium.org>
2018-01-29 18:05:19 -0800

    cros: Create StatusAreaTooltipManager to manage the tooltips of status area.
    changes in this cl,
    1. Extract ShelfTooltipManager to a public class.
    2. Create StatusAreaTooltipManager to help manage the tooltips of the status
       area items.
    
    Bug: 662857
    Change-Id: I4c220a3737a8412361e34423cfcf5fc4fadca3df

---
+++ ash/shelf/shelf_view_tooltip_manager.h
@@ -0,0 +1,54 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SHELF_SHELF_VIEW_TOOLTIP_MANAGER_H_
+#define ASH_SHELF_SHELF_VIEW_TOOLTIP_MANAGER_H_
+
+#include "ash/ash_export.h"
+#include "ash/shelf/shelf_tooltip_manager.h"
+#include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "ui/events/event_handler.h"
+
+namespace views {
+class View;
+}
+
+namespace ash {
+class ShelfView;
+
+// ShelfViewTooltipManager manages the tooltip bubble that appears for shelf
+// view items.
+class ASH_EXPORT ShelfViewTooltipManager : public ShelfTooltipManager,
+                                           public ui::EventHandler {
+ public:
+  explicit ShelfViewTooltipManager(ShelfView* shelf_view);
+  ~ShelfViewTooltipManager() override;
+
+  // Initializes the tooltip manager once the shelf is shown.
+  void Init();
+
+  // Returns the view to which the tooltip bubble is anchored. May be null.
+  views::View* GetCurrentAnchorView() const;
+
+ private:
+  friend class ShelfViewTest;
+
+  // ShelfTooltipManager:
+  void ShowTooltip(views::View* view) override;
+  bool ShouldShowTooltipForView(views::View* view) override;
+
+  // ui::EventHandler:
+  void OnMouseEvent(ui::MouseEvent* event) override;
+
+  ShelfView* shelf_view_;
+
+  base::WeakPtrFactory<ShelfViewTooltipManager> weak_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(ShelfViewTooltipManager);
+};
+
+}  // namespace ash
+
+#endif  // ASH_SHELF_SHELF_VIEW_TOOLTIP_MANAGER_H_
---
+++ ash/system/status_area_tooltip_manager.h
@@ -0,0 +1,50 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_STATUS_AREA_TOOLTIP_MANAGER_H_
+#define ASH_SYSTEM_STATUS_AREA_TOOLTIP_MANAGER_H_
+
+#include "ash/ash_export.h"
+#include "ash/shelf/shelf_tooltip_manager.h"
+#include "base/memory/weak_ptr.h"
+#include "ui/events/event_handler.h"
+
+namespace views {
+class View;
+}  // namespace views
+
+namespace ash {
+class StatusAreaWidgetDelegate;
+
+// StatusAreaTooltipManager manages the tooltip bubble that appears for status
+// area tray items.
+class ASH_EXPORT StatusAreaTooltipManager : public ShelfTooltipManager,
+                                            public ui::EventHandler {
+ public:
+  StatusAreaTooltipManager(StatusAreaWidgetDelegate* delegate, Shelf* shelf_);
+  ~StatusAreaTooltipManager() override;
+
+  // Initializes the tooltip manager once the status area is shown.
+  void Init();
+
+ private:
+  // ShelfTooltipManager:
+  void ShowTooltip(views::View* view) override;
+  bool ShouldShowTooltipForView(views::View* view) override;
+
+  // ui::EventHandler:
+  void OnMouseEvent(ui::MouseEvent* event) override;
+
+  StatusAreaWidgetDelegate* delegate_;
+  Shelf* shelf_;
+  base::string16 tooltip_text_;
+
+  base::WeakPtrFactory<StatusAreaTooltipManager> weak_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(StatusAreaTooltipManager);
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_STATUS_AREA_TOOLTIP_MANAGER_H_
