184dee54560f7dbd6423d21a50f628f635e28b5f
Mythri Alle <mythria@chromium.org>
2018-01-30 10:59:57 +0000

    Add a feature to enable code caching after execute
    This feature is to run a finch experiment to measure the performance
    of requesting code cache after execute.
    
    Bug:chromium:783124
    Change-Id: I7cc90c3f341c19e3727ad84903a774f8197663f2

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -423,6 +423,9 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
 
   WebRuntimeFeatures::EnablePictureInPicture(
       base::FeatureList::IsEnabled(media::kPictureInPicture));
+
+  WebRuntimeFeatures::EnableCodeCacheAfterExecute(
+      base::FeatureList::IsEnabled(features::kCodeCacheAfterExecute));
 };
 
 }  // namespace content
