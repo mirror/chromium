dd3046f347fcc625de1879a66976b66146afd7aa
Qiyu Hu <qiyuh@chromium.org>
2018-01-29 17:52:00 -0800

    Add a listener in chrome bluez stack.
    Bug:b:70811583
    Change-Id: I4f994505440e638377ef0355a843bd7184098b99

--- device/bluetooth/bluez/bluetooth_adapter_bluez.h
+++ device/bluetooth/bluez/bluetooth_adapter_bluez.h
@@ -31,6 +31,10 @@
 #include "device/bluetooth/dbus/bluetooth_profile_manager_client.h"
 #include "device/bluetooth/dbus/bluetooth_profile_service_provider.h"
 
+#if defined(OS_CHROMEOS)
+#include "device/bluetooth/dbus/bluetooth_adapter_client.h"
+#endif  // defined(OS_CHROMEOS)
+
 namespace base {
 class SequencedTaskRunner;
 class TimeDelta;
@@ -70,6 +74,9 @@ class BluetoothPairingBlueZ;
 // BluetoothBlueZTest, Shutdown.
 class DEVICE_BLUETOOTH_EXPORT BluetoothAdapterBlueZ
     : public device::BluetoothAdapter,
+#if defined(OS_CHROMEOS)
+      public chromeos::PowerManagerClient::Observer,
+#endif  // defined(OS_CHROMEOS)
       public bluez::BluetoothAdapterClient::Observer,
       public bluez::BluetoothDeviceClient::Observer,
       public bluez::BluetoothInputClient::Observer,
