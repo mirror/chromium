89a54f49ffed4bbfff73460e3ca0b709fa600a7f
Matthew Jones <mdjones@chromium.org>
2018-01-30 15:05:07 -0800

    Remove obsolete theme color method from LayoutTab
    Theme colors will be enabled for Chrome modern design, so there is no
    need to force the default theme color in LayoutTab.
    
    BUG=
    
    Change-Id: I40511489e2d03ebbdf1daa688468633a5bd688e8

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/TabListSceneLayer.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/scene_layer/TabListSceneLayer.java
@@ -77,15 +77,6 @@ public class TabListSceneLayer extends SceneLayer {
             int toolbarBackgroundColor = getTabThemeColor(context, t);
             int textBoxBackgroundColor = t.getTextBoxBackgroundColor();
             float textBoxAlpha = t.getTextBoxAlpha();
-            if (t.getForceDefaultThemeColor()) {
-                toolbarBackgroundColor = defaultThemeColor;
-                textBoxBackgroundColor = ColorUtils.getTextBoxColorForToolbarBackground(
-                        res, false, toolbarBackgroundColor, useModernDesign);
-                // In the modern design, the text box is always drawn in the Java layer rather
-                // than the compositor layer.
-                textBoxAlpha = useModernDesign ? 0.f : t.isIncognito() ? textBoxAlpha : 1f;
-            }
-
             int closeButtonColor =
                     ColorUtils.getThemedAssetColor(toolbarBackgroundColor, t.isIncognito());
 
