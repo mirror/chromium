3f073a194540e79cdb7dfa4a4a6e594b06bab841
Shivani Sharma <shivanisha@chromium.org>
2018-01-30 17:52:35 -0500

    Propagation of ad tagging from parent frame to child frame.
    This CL tags a subframe as an ad if its parent frame is tagged as well.
    This extends the coverage of frames tagged as ads from just those that
    match the ruleset to those that do not match but their parent frame is
    tagged as an ad frame.
    
    Change-Id: Ifee656298cdbe8bc16bcd24a16bc9a9905e2c2a3

--- components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.cc
+++ components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.cc
@@ -56,6 +56,7 @@ void ContentSubresourceFilterThrottleManager::OnSubresourceFilterGoingAway() {
 void ContentSubresourceFilterThrottleManager::RenderFrameDeleted(
     content::RenderFrameHost* frame_host) {
   activated_frame_hosts_.erase(frame_host);
+  ad_frames_.erase(frame_host);
   DestroyRulesetHandleIfNoLongerUsed();
 }
 
@@ -103,6 +104,9 @@ void ContentSubresourceFilterThrottleManager::ReadyToCommitNavigation(
 
   content::RenderFrameHost* frame_host =
       navigation_handle->GetRenderFrameHost();
+  if (is_ad_subframe)
+    ad_frames_.insert(frame_host);
+
   frame_host->Send(new SubresourceFilterMsg_ActivateForNextCommittedLoad(
       frame_host->GetRoutingID(), filter->activation_state(), is_ad_subframe));
 }
@@ -216,11 +220,21 @@ void ContentSubresourceFilterThrottleManager::OnSubframeNavigationEvaluated(
     LoadPolicy load_policy) {
   DCHECK(!navigation_handle->IsInMainFrame());
   auto it = ongoing_activation_throttles_.find(navigation_handle);
-  if (it != ongoing_activation_throttles_.end()) {
-    // Note that is_ad_subframe is only relevant for
-    // LoadPolicy:WOULD_DISALLOW(dryrun mode), although also setting it for
-    // DISALLOW for completeness.
-    it->second.is_ad_subframe = load_policy != LoadPolicy::ALLOW;
+  if (it == ongoing_activation_throttles_.end())
+    return;
+
+  // Note that is_ad_subframe is only relevant for
+  // LoadPolicy:WOULD_DISALLOW(dryrun mode), although also setting it for
+  // DISALLOW for completeness.
+  it->second.is_ad_subframe = load_policy != LoadPolicy::ALLOW;
+
+  // If this frame was not identified as an ad via ruleset matching, tag it
+  // based on whether its parent frame is an ad or not.
+  if (!it->second.is_ad_subframe) {
+    content::RenderFrameHost* parent_frame =
+        navigation_handle->GetParentFrame();
+    if (parent_frame && base::ContainsKey(ad_frames_, parent_frame))
+      it->second.is_ad_subframe = true;
   }
 }
 
