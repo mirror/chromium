3822545fee6d4fc2c3c5e5cd64017b28c2c20a7e
Alex Clarke <alexclarke@chromium.org>
2018-01-30 15:36:18 +0000

    Refactor away TaskQueueManager::DoWork infavor of the selector interface
    Change-Id: I5935e41ad5bc725b6109a187b329c82ce5a70689
    Bug: 783309

--- third_party/WebKit/Source/platform/scheduler/base/thread_controller_impl.cc
+++ third_party/WebKit/Source/platform/scheduler/base/thread_controller_impl.cc
@@ -9,6 +9,7 @@
 #include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/time/tick_clock.h"
+#include "base/trace_event/trace_event.h"
 #include "platform/scheduler/base/sequence.h"
 
 namespace blink {
@@ -51,13 +52,36 @@ void ThreadControllerImpl::SetSequence(Sequence* sequence) {
 
 void ThreadControllerImpl::ScheduleWork() {
   DCHECK(sequence_);
+  base::AutoLock lock(any_sequence_lock_);
+  // Don't post a DoWork if there's an immediate DoWork in flight or if we're
+  // inside a top level DoWork. We can rely on a continuation being posted as
+  // needed.
+  if (any_sequence().immediate_do_work_posted ||
+      (any_sequence().do_work_running_count > any_sequence().nesting_depth)) {
+    return;
+  }
+  any_sequence().immediate_do_work_posted = true;
+
+  TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("renderer.scheduler"),
+               "ThreadControllerImpl::ScheduleWork::PostTask");
   task_runner_->PostTask(FROM_HERE, immediate_do_work_closure_);
+}
+
+bool ThreadControllerImpl::DoWorkRunningOrPostedWithoutDelay() {
+  if (main_sequence_only().do_work_running_count >
+      main_sequence_only().nesting_depth) {
+    return true;
+  }
+
+  base::AutoLock lock(any_sequence_lock_);
+  return any_sequence().immediate_do_work_posted;
 }
 
 void ThreadControllerImpl::ScheduleDelayedWork(base::TimeDelta delay) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(sequence_);
   cancelable_delayed_do_work_closure_.Reset(delayed_do_work_closure_);
+
   task_runner_->PostDelayedTask(
       FROM_HERE, cancelable_delayed_do_work_closure_.callback(), delay);
 }
@@ -89,28 +113,107 @@ void ThreadControllerImpl::RestoreDefaultTaskRunner() {
   message_loop_->SetTaskRunner(message_loop_task_runner_);
 }
 
+void ThreadControllerImpl::DidQueueTask(const base::PendingTask& pending_task) {
+  task_annotator_.DidQueueTask("TaskQueueManager::PostTask", pending_task);
+}
+
 void ThreadControllerImpl::DoWork(Sequence::WorkType work_type) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(sequence_);
-  base::Optional<base::PendingTask> task = sequence_->TakeTask(work_type);
-  if (!task)
-    return;
+
+  {
+    base::AutoLock lock(any_sequence_lock_);
+    if (work_type == Sequence::WorkType::kImmediate)
+      any_sequence().immediate_do_work_posted = false;
+    any_sequence().do_work_running_count++;
+  }
+
+  main_sequence_only().do_work_running_count++;
+
   base::WeakPtr<ThreadControllerImpl> weak_ptr = weak_factory_.GetWeakPtr();
-  task_annotator_.RunTask("ThreadControllerImpl::DoWork", &task.value());
-  if (weak_ptr)
-    sequence_->DidRunTask();
+  base::TimeDelta delay_till_next_task = base::TimeDelta::Max();
+  for (int i = 0; i < main_sequence_only().work_batch_size_; i++) {
+    base::Optional<base::PendingTask> task = sequence_->TakeTask();
+    if (!task) {
+      // TakeTask will have posted a delayed continuation if needed.
+      delay_till_next_task = base::TimeDelta::Max();
+      break;
+    }
+
+    TRACE_TASK_EXECUTION("ThreadControllerImpl::DoWork", *task);
+    task_annotator_.RunTask("ThreadControllerImpl::DoWork", &*task);
+
+    if (!weak_ptr)
+      return;
+
+    delay_till_next_task = sequence_->DidRunTask();
+    if (delay_till_next_task > base::TimeDelta())
+      break;
+  }
+
+  main_sequence_only().do_work_running_count--;
+  {
+    base::AutoLock lock(any_sequence_lock_);
+    any_sequence().do_work_running_count--;
+    DCHECK_GE(any_sequence().do_work_running_count, 0);
+    if (delay_till_next_task <= base::TimeDelta()) {
+      if (!any_sequence().immediate_do_work_posted) {
+        any_sequence().immediate_do_work_posted = true;
+        task_runner_->PostTask(FROM_HERE, immediate_do_work_closure_);
+      }
+    } else if (delay_till_next_task < base::TimeDelta::Max()) {
+      cancelable_delayed_do_work_closure_.Reset(delayed_do_work_closure_);
+      task_runner_->PostDelayedTask(
+          FROM_HERE, cancelable_delayed_do_work_closure_.callback(),
+          delay_till_next_task);
+    }
+  }
 }
 
 void ThreadControllerImpl::AddNestingObserver(
     base::RunLoop::NestingObserver* observer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  base::RunLoop::AddNestingObserverOnCurrentThread(observer);
+  nesting_observer_ = observer;
+  base::RunLoop::AddNestingObserverOnCurrentThread(this);
 }
 
 void ThreadControllerImpl::RemoveNestingObserver(
     base::RunLoop::NestingObserver* observer) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  base::RunLoop::RemoveNestingObserverOnCurrentThread(observer);
+  DCHECK_EQ(observer, nesting_observer_);
+  nesting_observer_ = nullptr;
+  base::RunLoop::RemoveNestingObserverOnCurrentThread(this);
+}
+
+void ThreadControllerImpl::OnBeginNestedRunLoop() {
+  main_sequence_only().nesting_depth++;
+  {
+    base::AutoLock lock(any_sequence_lock_);
+    any_sequence().nesting_depth++;
+    if (!any_sequence().immediate_do_work_posted) {
+      any_sequence().immediate_do_work_posted = true;
+      TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("renderer.scheduler"),
+                   "ThreadControllerImpl::OnBeginNestedRunLoop::PostTask");
+      task_runner_->PostTask(FROM_HERE, immediate_do_work_closure_);
+    }
+  }
+  if (nesting_observer_)
+    nesting_observer_->OnBeginNestedRunLoop();
+}
+
+void ThreadControllerImpl::OnExitNestedRunLoop() {
+  main_sequence_only().nesting_depth--;
+  {
+    base::AutoLock lock(any_sequence_lock_);
+    any_sequence().nesting_depth--;
+    DCHECK_GE(any_sequence().nesting_depth, 0);
+  }
+  if (nesting_observer_)
+    nesting_observer_->OnExitNestedRunLoop();
+}
+
+void ThreadControllerImpl::SetWorkBatchSize(int work_batch_size) {
+  main_sequence_only().work_batch_size_ = work_batch_size;
 }
 
 }  // namespace internal
