0a6b22600b8c303687a7e139b0ca67763c0b835b
Lei Zhang <thestig@chromium.org>
2018-01-30 10:45:08 -0800

    Make services/network/public/cpp:cpp target a component.
    If this target is always a static_library, then in component builds it
    gets linked into multiple .so files and produces ODR violations. As a
    result, if services/network/public/cpp/features.h were to get added,
    base::FeatureList would have trouble keeping track of its features when
    DCHECKs are turned on.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I61f83087bc87b0ccfc4d4884e41adf39524d46e6

--- services/network/public/cpp/features.h
+++ services/network/public/cpp/features.h
@@ -2,19 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SERVICES_NETWORK_PUBLIC_CPP_FEATURES_
-#define SERVICES_NETWORK_PUBLIC_CPP_FEATURES_
+#ifndef SERVICES_NETWORK_PUBLIC_CPP_FEATURES_H_
+#define SERVICES_NETWORK_PUBLIC_CPP_FEATURES_H_
 
+#include "base/component_export.h"
 #include "base/feature_list.h"
-#include "services/network/public/cpp/features_export.h"
 
 namespace network {
 namespace features {
 
-NETWORK_FEATURES_EXPORT extern const base::Feature kReporting;
-NETWORK_FEATURES_EXPORT extern const base::Feature kNetworkErrorLogging;
+COMPONENT_EXPORT(SERVICES_NETWORK_PUBLIC_CPP)
+extern const base::Feature kReporting;
+COMPONENT_EXPORT(SERVICES_NETWORK_PUBLIC_CPP)
+extern const base::Feature kNetworkErrorLogging;
 
 }  // namespace features
 }  // namespace network
 
-#endif  // SERVICES_NETWORK_PUBLIC_CPP_FEATURES_
+#endif  // SERVICES_NETWORK_PUBLIC_CPP_FEATURES_H_
