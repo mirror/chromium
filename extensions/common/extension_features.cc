a997868387dd8b12c4b7cff2e8154fb2bdcddce5
Kunihiko Sakamoto <ksakamoto@chromium.org>
2018-01-30 10:15:44 +0900

    Enable YieldBetweenContentScriptRuns by default
    Bug: 705400
    Change-Id: Iffc701df6997a0341839cf5b294694ffd5dc20d8

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -12,8 +12,9 @@ const base::Feature kNativeCrxBindings{"NativeCrxBindings",
                                        base::FEATURE_DISABLED_BY_DEFAULT};
 
 // Enables splitting content script injections into multiple tasks.
+// TODO(ksakamoto): Remove this feature flag in M67.
 const base::Feature kYieldBetweenContentScriptRuns{
-    "YieldBetweenContentScriptRuns", base::FEATURE_DISABLED_BY_DEFAULT};
+    "YieldBetweenContentScriptRuns", base::FEATURE_ENABLED_BY_DEFAULT};
 
 }  // namespace features
 }  // namespace extensions
