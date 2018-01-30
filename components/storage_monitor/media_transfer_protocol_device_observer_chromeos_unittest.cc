cad3d8029c7e11175bf11a7098bce5cde25b7c48
Donna Wu <donna.wu@intel.com>
2018-01-30 11:00:32 +0800

    Separate StorageInfo utils functions out and add back the unit tests.
    This CL separate the general functions for getting needed information
    from MtpStorageInfo to storage_monitor::StorageInfo and add the
    |id| and |location| test back.
    
    BUG=769630
    
    Change-Id: I1d77c1e4ea9471e04706ac9e94778e8bea771a94

--- components/storage_monitor/media_transfer_protocol_device_observer_chromeos_unittest.cc
+++ components/storage_monitor/media_transfer_protocol_device_observer_chromeos_unittest.cc
@@ -15,6 +15,7 @@
 #include "base/strings/utf_string_conversions.h"
 #include "components/storage_monitor/mock_removable_storage_observer.h"
 #include "components/storage_monitor/storage_info.h"
+#include "components/storage_monitor/storage_info_utils.h"
 #include "components/storage_monitor/storage_monitor.h"
 #include "components/storage_monitor/test_storage_monitor.h"
 #include "content/public/test/test_browser_thread_bundle.h"
@@ -152,12 +153,17 @@ class MediaTransferProtocolDeviceObserverChromeOSTest : public testing::Test {
 
 // Test to verify basic mtp storage attach and detach notifications.
 TEST_F(MediaTransferProtocolDeviceObserverChromeOSTest, BasicAttachDetach) {
+  auto* mtpStorageInfo = GetFakeMtpStorageInfo(kStorageWithValidInfo);
+  std::string device_id = GetDeviceIdFromStorageInfo(*mtpStorageInfo);
 
   // Attach a mtp storage.
   mtp_device_observer()->MtpStorageAttached(kStorageWithValidInfo);
 
   EXPECT_EQ(1, observer().attach_calls());
   EXPECT_EQ(0, observer().detach_calls());
+  EXPECT_EQ(device_id, observer().last_attached().device_id());
+  EXPECT_EQ(GetDeviceLocationFromStorageName(kStorageWithValidInfo),
+            observer().last_attached().location());
   EXPECT_EQ(base::ASCIIToUTF16(kStorageVendor),
             observer().last_attached().vendor_name());
   EXPECT_EQ(base::ASCIIToUTF16(kStorageProduct),
@@ -168,6 +174,7 @@ TEST_F(MediaTransferProtocolDeviceObserverChromeOSTest, BasicAttachDetach) {
 
   EXPECT_EQ(1, observer().attach_calls());
   EXPECT_EQ(1, observer().detach_calls());
+  EXPECT_EQ(device_id, observer().last_detached().device_id());
 }
 
 // When a mtp storage device with invalid storage label and id is
