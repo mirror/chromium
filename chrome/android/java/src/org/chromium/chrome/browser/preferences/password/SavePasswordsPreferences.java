1139d708a9e0fbe23490fa27ef03d7df993409ec
Friedrich Horschig <fhorschig@chromium.org>
2018-01-30 17:40:55 +0100

    Add metrics for password search on Android
    This CL adds two metrics lettings us know how users use the password
    search feature (go/android-password-search):
    1. "ViewedAfterSearched" as new bucket on
     PasswordManager.Android.PasswordCredentialEntry capturing the number
     of passwords that were clicked as a search result. This happens in
     addition to the existing "Viewed" bucket.
    2. PasswordManager.Android.PasswordSearchTriggered recording how many
     users who opened a the preferences triggered the search.
    
    Bug: 794108
    Change-Id: Ib1bcc2c1080ee196ea6634007726e572a6305b65

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/password/SavePasswordsPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/password/SavePasswordsPreferences.java
@@ -34,6 +34,7 @@ import org.chromium.base.Callback;
 import org.chromium.base.ContentUriUtils;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.VisibleForTesting;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.preferences.ChromeBaseCheckBoxPreference;
@@ -79,6 +80,9 @@ public class SavePasswordsPreferences
     public static final String PREF_SAVE_PASSWORDS_SWITCH = "save_passwords_switch";
     public static final String PREF_AUTOSIGNIN_SWITCH = "autosignin_switch";
 
+    // The id of the boolean intent telling a clicked entry whether it was found in a search.
+    public static final String EXTRA_FOUND_VIA_SEARCH = "found_via_search_args";
+
     private static final String PREF_KEY_CATEGORY_SAVED_PASSWORDS = "saved_passwords";
     private static final String PREF_KEY_CATEGORY_EXCEPTIONS = "exceptions";
     private static final String PREF_KEY_MANAGE_ACCOUNT_LINK = "manage_account_link";
@@ -89,6 +93,11 @@ public class SavePasswordsPreferences
 
     // Name of the subdirectory in cache which stores the exported passwords file.
     private static final String PASSWORDS_CACHE_DIR = "/passwords";
+
+    // Name of the histogram that records whether the user visiting the settings triggered the
+    // search.
+    private static final String HISTOGRAM_SEARCH_TRIGGERED =
+            "PasswordManager.Android.PasswordSearchTriggered";
 
     private static final int ORDER_SWITCH = 0;
     private static final int ORDER_AUTO_SIGNIN_CHECKBOX = 1;
@@ -120,6 +129,7 @@ public class SavePasswordsPreferences
     private ChromeSwitchPreference mSavePasswordsSwitch;
     private ChromeBaseCheckBoxPreference mAutoSignInSwitch;
     private TextMessagePreference mEmptyView;
+    private boolean mSearchTriggered;
     private Menu mMenuForTesting;
 
     @Override
@@ -130,6 +140,8 @@ public class SavePasswordsPreferences
         PasswordManagerHandlerProvider.getInstance().addObserver(this);
 
         setHasOptionsMenu(providesPasswordExport() || providesPasswordSearch());
+
+        mSearchTriggered = false; // Checked and recorded in onDestroy().
 
         if (savedInstanceState == null) return;
 
@@ -173,6 +185,7 @@ public class SavePasswordsPreferences
         searchItem.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
             @Override
             public boolean onMenuItemActionExpand(MenuItem menuItem) {
+                mSearchTriggered = true;
                 filterPasswords(""); // Hide other menu elements.
                 return true; // Continue expanding.
             }
@@ -573,6 +586,9 @@ public class SavePasswordsPreferences
     @Override
     public void onDestroy() {
         super.onDestroy();
+        if (providesPasswordSearch()) {
+            RecordHistogram.recordBooleanHistogram(HISTOGRAM_SEARCH_TRIGGERED, mSearchTriggered);
+        }
         PasswordManagerHandlerProvider.getInstance().removeObserver(this);
     }
 
@@ -593,6 +609,7 @@ public class SavePasswordsPreferences
             Intent intent = PreferencesLauncher.createIntentForSettingsPage(
                     getActivity(), PasswordEntryEditor.class.getName());
             intent.putExtra(Preferences.EXTRA_SHOW_FRAGMENT_ARGUMENTS, preference.getExtras());
+            intent.putExtra(SavePasswordsPreferences.EXTRA_FOUND_VIA_SEARCH, mSearchQuery != null);
             startActivity(intent);
         }
         return true;
