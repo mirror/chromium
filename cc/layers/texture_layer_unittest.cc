0d23fb72f19b91e95302b2ea38d87e82f0651d5a
danakj <danakj@chromium.org>
2018-01-30 15:03:59 -0500

    Move ownership of ResourcePool zero-copy resources to buffer provider
    This adds a GpuBacking object to PoolResource, which holds ownership
    of all the GpuMemoryBuffer-based backing objects. The
    ZeroCopyRasterBufferImpl has a subclass with its own specific types
    in it, and populates them:
    a) When AcquireBufferForRaster is called it creates the GpuBacking
    and ensures the sync token is waited on if present.
    b) When the RasterBuffer is destroyed, back on the compositor thread,
    it binds the GpuMemoryBuffer to a mailbox, and stores those onto the
    GpuBacking.
    
    Then ResourcePool can export the GpuBacking by reading the mailbox
    of it. And when the resource is destroyed, the GpuBacking cleans
    up the ownership of the mailboxed texture and GpuMemoryBuffer.
    
    R=vmpstr@chromium.org
    
    Bug: 730660, 738190
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Iac964108e2ab767239e9968e03f82bc80d6a6e3b

--- cc/layers/texture_layer_unittest.cc
+++ cc/layers/texture_layer_unittest.cc
@@ -771,8 +771,9 @@ class TextureLayerImplWithResourceTest : public TextureLayerTest {
 // Test conditions for results of TextureLayerImpl::WillDraw under
 // different configurations of different mailbox, texture_id, and draw_mode.
 TEST_F(TextureLayerImplWithResourceTest, TestWillDraw) {
-  EXPECT_CALL(test_data_.mock_callback_,
-              Release(test_data_.mailbox_name1_, gpu::SyncToken(), false))
+  EXPECT_CALL(
+      test_data_.mock_callback_,
+      Release(test_data_.mailbox_name1_, test_data_.sync_token1_, false))
       .Times(AnyNumber());
   EXPECT_CALL(
       test_data_.mock_callback_,
