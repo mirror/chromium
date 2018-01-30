7cda378be777b9373f126ae53f194601adeae095
Yixin Wang <wangyix@chromium.org>
2018-01-29 16:37:37 -0800

    deprecate FLAGS_quic_reloadable_flag_quic_enable_hq_deframer.
    Merge internal change: 182818920
    
    R=rch@chromium.org
    
    Change-Id: I40f92c00c6ecfc7da872e3d5339f3c78f470f780

--- net/quic/core/quic_flags_list.h
+++ net/quic/core/quic_flags_list.h
@@ -130,9 +130,6 @@ QUIC_FLAG(bool,
           FLAGS_quic_reloadable_flag_quic_fix_sequencer_buffer_block_count2,
           true)
 
-// If true, use deframer from net/quic/http instead of net/http2.
-QUIC_FLAG(bool, FLAGS_quic_reloadable_flag_quic_enable_hq_deframer, true)
-
 // If true, then 1) at sender, avoid sending empty acks, 2) at receiver, close
 // connection when a ack frame\'s first block length is 0, unless the ack is
 // completely empty.
