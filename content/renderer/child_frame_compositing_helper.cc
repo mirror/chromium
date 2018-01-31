5ff35fa7a7bfd33c2c931cdc01f94d28f491ec08
Fady Samuel <fsamuel@chromium.org>
2018-01-31 16:00:54 -0500

    Surface synchronization: Don't block on OOPIFs
    Change-Id: Ide782fc9b4bbac2cd75d31d05f62b39431ae8c14

--- content/renderer/child_frame_compositing_helper.cc
+++ content/renderer/child_frame_compositing_helper.cc
@@ -133,8 +133,8 @@ void ChildFrameCompositingHelper::SetPrimarySurfaceId(
   surface_layer_->SetMasksToBounds(true);
   surface_layer_->SetBackgroundColor(SK_ColorTRANSPARENT);
 
-  surface_layer_->SetPrimarySurfaceId(surface_id,
-                                      cc::DeadlinePolicy::UseDefaultDeadline());
+  surface_layer_->SetPrimarySurfaceId(
+      surface_id, cc::DeadlinePolicy::UseSpecifiedDeadline(0u));
   surface_layer_->SetFallbackSurfaceId(fallback_surface_id_);
 
   std::unique_ptr<cc_blink::WebLayerImpl> layer(
