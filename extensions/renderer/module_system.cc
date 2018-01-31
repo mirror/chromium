45664fcf405a5bf64e70289eb78ab50936df2a7a
Devlin Cronin <rdevlin.cronin@chromium.org>
2018-01-30 19:01:33 -0800

    [Extensions Bindings] Lazily initialize NativeHandlers
    With native bindings, delay initialization of native handlers until
    first access.
    
    Bug: 805244
    Change-Id: I4c66ede2ba3f6e05e3b7cfa7ebac0471195646cb

--- extensions/renderer/module_system.cc
+++ extensions/renderer/module_system.cc
@@ -6,6 +6,7 @@
 
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/feature_list.h"
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/metrics/histogram_macros.h"
@@ -16,6 +17,7 @@
 #include "content/public/renderer/render_frame.h"
 #include "content/public/renderer/render_view.h"
 #include "extensions/common/extension.h"
+#include "extensions/common/extension_features.h"
 #include "extensions/common/extensions_client.h"
 #include "extensions/renderer/console.h"
 #include "extensions/renderer/safe_builtins.h"
@@ -347,7 +349,11 @@ void ModuleSystem::RegisterNativeHandler(
     const std::string& name,
     std::unique_ptr<NativeHandler> native_handler) {
   ClobberExistingNativeHandler(name);
-  native_handler->Initialize();
+  // Without native bindings, we initialize handlers immediately. Otherwise, we
+  // initialize them lazily on first access.
+  if (!base::FeatureList::IsEnabled(features::kNativeCrxBindings))
+    native_handler->Initialize();
+
   native_handler_map_[name] = std::move(native_handler);
 }
 
@@ -609,6 +615,14 @@ v8::MaybeLocal<v8::Object> ModuleSystem::RequireNativeFromString(
           "Couldn't find native for requireNative(" + native_name + ")");
     return v8::MaybeLocal<v8::Object>();
   }
+
+  // Initialize native handlers lazily on first access if native bindings are
+  // enabled.
+  if (base::FeatureList::IsEnabled(features::kNativeCrxBindings) &&
+      !i->second->IsInitialized()) {
+    i->second->Initialize();
+  }
+
   return i->second->NewInstance();
 }
 
