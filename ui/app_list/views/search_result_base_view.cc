d8b13231844b32c11a6227bb2a5ed9d2c23b3895
Weidong Guo <weidongg@chromium.org>
2018-01-31 10:45:17 -0800

    Fix first search result highlight
    Background:
    The first search result should be highlighted after the search results
    are updated.
    
    Changes:
    1. Create SearchResultBaseView as the parent class for SearchResultView,
    SearchResultTileItemView and SearchAnswerContainerView so that we could
    easily highlight different search results.
    2. Rename SetFirstResultSelected to SetFirstResultHighlighted since we
    will remove selection completely in crbug.com/766807.
    
    BUG=804023
    
    Change-Id: I731657350ed76b3a8b92adce2c7e70e86b9a013c

---
+++ ui/app_list/views/search_result_base_view.cc
@@ -0,0 +1,13 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/app_list/views/search_result_base_view.h"
+
+namespace app_list {
+
+SearchResultBaseView::SearchResultBaseView() : Button(this) {}
+
+SearchResultBaseView::~SearchResultBaseView() = default;
+
+}  // namespace app_list
