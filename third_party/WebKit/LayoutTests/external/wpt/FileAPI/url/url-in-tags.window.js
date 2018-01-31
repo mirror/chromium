1ef0a51af4230025e4b5f4f228ba809e069c5e70
Marijn Kruisselbrink <mek@chromium.org>
2018-01-30 16:29:22 -0800

    Add a few more blob URL tests, checking for races with revoking and resolving.
    Revoking right after navigating and/or loading script should not break
    the loading.
    
    Bug: 800901
    Change-Id: I5b2fc378ae4681fb4d42e1e3742111f67a2d1a41

--- third_party/WebKit/LayoutTests/external/wpt/FileAPI/url/url-in-tags.window.js
+++ third_party/WebKit/LayoutTests/external/wpt/FileAPI/url/url-in-tags.window.js
@@ -48,3 +48,37 @@ async_test(t => {
     assert_equals(frame.contentWindow.scrollY, 5000);
   });
 }, 'Blob URL fragment is implemented.');
+
+async_test(t => {
+  const run_result = 'test_frame_OK';
+  const blob_contents = '<!doctype html>\n<meta charset="utf-8">\n' +
+    '<script>window.test_result = "' + run_result + '";</script>';
+  const blob = new Blob([blob_contents], {type: 'text/html'});
+  const url = URL.createObjectURL(blob);
+
+  const frame = document.createElement('iframe');
+  frame.setAttribute('src', url);
+  frame.setAttribute('style', 'display:none;');
+  document.body.appendChild(frame);
+  URL.revokeObjectURL(url);
+
+  frame.onload = t.step_func_done(() => {
+    assert_equals(frame.contentWindow.test_result, run_result);
+  });
+}, 'Blob URL can be used in iframe, even when URL is immediately revoked.');
+
+async_test(t => {
+  const run_result = 'test_script_OK';
+  const blob_contents = 'window.test2_result = "' + run_result + '";';
+  const blob = new Blob([blob_contents]);
+  const url = URL.createObjectURL(blob);
+
+  const e = document.createElement('script');
+  e.setAttribute('src', url);
+  e.onload = t.step_func_done(() => {
+    assert_equals(window.test2_result, run_result);
+  });
+
+  document.body.appendChild(e);
+  URL.revokeObjectURL(url);
+}, 'Blob URLs can be used in <script> tags, even when URL is immediately revoked.');
