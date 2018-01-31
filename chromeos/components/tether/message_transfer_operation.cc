40d283cfede98257705e474ec1a30ec9426703f1
Kyle Horimoto <khorimoto@google.com>
2018-01-30 17:33:40 -0800

    [CrOS Tether] Add metrics for GATT connection success/failures.
    This CL adds two metrics:
      (1) Success rate for GATT connection attempts.
      (2) Effective success rate for one or multiple connection attempts,
          using retries if a previous connection failed (up to the maximum
          number of retries).
    
    These metrics aid in our ability to analyze what our effective
    connection stability is.
    
    Bug: 805219, 672263
    Change-Id: I407c3af03eb86ccc80807395e1f18a5bff2b71a4

--- chromeos/components/tether/message_transfer_operation.cc
+++ chromeos/components/tether/message_transfer_operation.cc
@@ -7,6 +7,7 @@
 #include <memory>
 #include <set>
 
+#include "base/metrics/histogram_macros.h"
 #include "chromeos/components/tether/message_wrapper.h"
 #include "chromeos/components/tether/timer_factory.h"
 #include "components/proximity_auth/logging/logging.h"
@@ -105,6 +106,12 @@ void MessageTransferOperation::OnSecureChannelStatusChanged(
 
   switch (new_status) {
     case cryptauth::SecureChannel::Status::AUTHENTICATED:
+      UMA_HISTOGRAM_BOOLEAN(
+          "InstantTethering.GattConnectionAttempt.SuccessRate", true);
+      UMA_HISTOGRAM_BOOLEAN(
+          "InstantTethering.GattConnectionAttempt."
+          "EffectiveSuccessRateWithRetries",
+          true);
       StartTimerForDevice(*remote_device);
       OnDeviceAuthenticated(*remote_device);
       break;
@@ -214,6 +221,8 @@ void MessageTransferOperation::HandleDeviceDisconnection(
                    << remote_device.GetTruncatedDeviceIdForLogs() << ". "
                    << "Number of GATT error: "
                    << attempts_for_device.gatt_connection_attempts;
+      UMA_HISTOGRAM_BOOLEAN(
+          "InstantTethering.GattConnectionAttempt.SuccessRate", false);
 
       if (attempts_for_device.gatt_connection_attempts >=
           kMaxGattConnectionAttemptsPerDevice) {
@@ -221,6 +230,10 @@ void MessageTransferOperation::HandleDeviceDisconnection(
                      << "device with ID "
                      << remote_device.GetTruncatedDeviceIdForLogs() << ". "
                      << "Unregistering device.";
+        UMA_HISTOGRAM_BOOLEAN(
+            "InstantTethering.GattConnectionAttempt."
+            "EffectiveSuccessRateWithRetries",
+            false);
         UnregisterDevice(remote_device);
       }
       break;
