f4378fe654c9e9a407c159b277dd9f62f141eee7
John Abd-El-Malek <jam@chromium.org>
2018-01-31 23:32:12 -0800

    Reduce network::URLLoader dependencies to net::URLRequestContextGetter and network::mojom::NetworkServiceClient.
    This makes it easier to reuse on iOS for servicifing network::mojom::URLLoader requests.
    
    This also allows the removal of the URLLoader lifetime management in NetworkContext, and instead we can use the lifetime management in net::URLRequestContextGetter.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I5dfeefd8b29f37ea24ac26167ccb064b6af42afc

--- services/network/public/cpp/url_request_context_owner.h
+++ services/network/public/cpp/url_request_context_owner.h
@@ -8,18 +8,41 @@
 #include <memory>
 
 #include "base/component_export.h"
+#include "base/memory/scoped_refptr.h"
+#include "net/url_request/url_request_context_getter.h"
 
 class PrefService;
 
-namespace net {
-class URLRequestContext;
-}
-
 namespace network {
+
+class COMPONENT_EXPORT(NETWORK_CPP) NetworkURLRequestContextGetter
+    : public net::URLRequestContextGetter {
+ public:
+  explicit NetworkURLRequestContextGetter(
+      std::unique_ptr<net::URLRequestContext> url_request_context);
+
+  // net::URLRequestContextGetter implementation:
+  net::URLRequestContext* GetURLRequestContext() override;
+  scoped_refptr<base::SingleThreadTaskRunner> GetNetworkTaskRunner()
+      const override;
+
+  void NotifyContextShuttingDown();
+
+ protected:
+  ~NetworkURLRequestContextGetter() override;
+
+ private:
+  bool shutdown_ = false;
+  std::unique_ptr<net::URLRequestContext> url_request_context_;
+  scoped_refptr<base::SingleThreadTaskRunner> network_task_runner_;
+};
 
 // This owns a net::URLRequestContext and other state that's used with it.
 struct COMPONENT_EXPORT(NETWORK_CPP) URLRequestContextOwner {
   URLRequestContextOwner();
+  URLRequestContextOwner(
+      std::unique_ptr<PrefService> pref_service,
+      std::unique_ptr<net::URLRequestContext> url_request_context);
   ~URLRequestContextOwner();
   URLRequestContextOwner(URLRequestContextOwner&& other);
   URLRequestContextOwner& operator=(URLRequestContextOwner&& other);
@@ -27,7 +50,7 @@ struct COMPONENT_EXPORT(NETWORK_CPP) URLRequestContextOwner {
   // This needs to be destroyed after the URLRequestContext.
   std::unique_ptr<PrefService> pref_service;
 
-  std::unique_ptr<net::URLRequestContext> url_request_context;
+  scoped_refptr<NetworkURLRequestContextGetter> url_request_context_getter;
 };
 
 }  // namespace network
