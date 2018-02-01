f7d746051fb0c802b6a60401b055420781dc77b4
Kevin Marshall <kmarshall@chromium.org>
2018-02-01 13:34:35 -0800

    Specify lower limits on Fuchsia for load tests on DiskCacheBackendTest.
    Fuchsia tests run under two layers of virtualization which carries
    a significant performance penalty. The DiskCacheBackendTest load tests
    specified values which were too computationally intensive, causing the
    tests to time out.
    
    
    Bug: 807882
    Change-Id: I1f44436a9f0967b072aa9da160b5fc15bc7f730f

--- net/disk_cache/backend_unittest.cc
+++ net/disk_cache/backend_unittest.cc
@@ -23,6 +23,7 @@
 #include "base/trace_event/memory_allocator_dump.h"
 #include "base/trace_event/process_memory_dump.h"
 #include "base/trace_event/trace_event_argument.h"
+#include "build/build_config.h"
 #include "net/base/cache_type.h"
 #include "net/base/io_buffer.h"
 #include "net/base/net_errors.h"
@@ -1043,7 +1044,13 @@ void DiskCacheBackendTest::BackendLoad() {
   int seed = static_cast<int>(Time::Now().ToInternalValue());
   srand(seed);
 
+#if defined(OS_FUCHSIA)
+  // Load tests with large numbers of file descriptors perform poorly on
+  // virtualized test execution environments.
+  const int kNumEntries = 100;
+#else
   const int kNumEntries = 512;
+#endif  // defined(OS_FUCHSIA)
 
   disk_cache::Entry* entries[kNumEntries];
   for (int i = 0; i < kNumEntries; i++) {
