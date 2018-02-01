0aaf3122df898c71f73f901ac33c37488953bca9
Sigurdur Asgeirsson <siggi@chromium.org>
2018-02-01 13:13:05 -0500

    Analyze and record Windows system crashes on browser crash.
    Bug: 799526
    Change-Id: Ie61ff4ada2c1ed57949414c0c400ea93481a68c5

--- components/metrics/stability_metrics_provider.h
+++ components/metrics/stability_metrics_provider.h
@@ -5,6 +5,8 @@
 #ifndef COMPONENTS_METRICS_STABILITY_METRICS_PROVIDER_H_
 #define COMPONENTS_METRICS_STABILITY_METRICS_PROVIDER_H_
 
+#include "base/time/time.h"
+#include "build/build_config.h"
 #include "components/metrics/metrics_provider.h"
 
 class PrefService;
@@ -28,13 +30,18 @@ class StabilityMetricsProvider : public MetricsProvider {
   void CheckLastSessionEndCompleted();
   void MarkSessionEndCompleted(bool end_completed);
 
-  void LogCrash();
+  void LogCrash(base::Time last_live_timestamp);
   void LogStabilityLogDeferred();
   void LogStabilityDataDiscarded();
   void LogLaunch();
   void LogStabilityVersionMismatch();
 
  private:
+#if defined(OS_WIN)
+  // This funciton is virtual for testing.
+  virtual bool IsUncleanSystemSession(base::Time last_live_timestamp);
+  void MaybeLogSystemCrash(base::Time last_live_timestamp);
+#endif
   // Increments an Integer pref value specified by |path|.
   void IncrementPrefValue(const char* path);
 
