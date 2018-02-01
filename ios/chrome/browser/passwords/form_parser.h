7da887a58143a70fd993f8df56a1067b6ad79c05
Vadym Doroshenko <dvadym@chromium.org>
2018-02-01 15:57:50 +0100

    [IOS Password Manager] Form parser.
    This is implementation of new form parsing algorithms.
    More details in go/ios-password-form-parsing sections
      - changes in code
      - form parsing algorithms
    
    Bug: 807623
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I073b375ba4f6ba1b0fabe02e80ed171363438ceb

---
+++ ios/chrome/browser/passwords/form_parser.h
@@ -0,0 +1,33 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PASSWORDS_FORM_PARSER_H_
+#define IOS_CHROME_BROWSER_PASSWORDS_FORM_PARSER_H_
+
+#include <memory>
+
+namespace autofill {
+struct FormData;
+struct PasswordForm;
+}  // namespace autofill
+
+namespace password_manager {
+
+enum class FormParsingMode { FILLING, SAVING };
+
+class FormParser {
+ public:
+  FormParser();
+
+  // Parse DOM information |form_data| to Password Manager form representation
+  // PasswordForm.
+  // Return nullptr when parsing is unsuccessful.
+  std::unique_ptr<autofill::PasswordForm> Parse(
+      const autofill::FormData& form_data,
+      FormParsingMode mode);
+};
+
+}  // namespace password_manager
+
+#endif  // IOS_CHROME_BROWSER_PASSWORDS_FORM_PARSER_H_
