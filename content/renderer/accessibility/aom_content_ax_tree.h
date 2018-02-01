5c4afc1bfe0ce8f77b2a28ce71f2bc98743780b5
Meredith Lane <meredithl@google.com>
2018-02-02 09:18:38 +1100

    Implement basic relation accessors
    This change adds the IDL and accessors for getting related computed
    accessible nodes. It mirrors the relation accessors on Node, which is
    parent, firstChild, lastChild, previousSibling, and nextSibling.
    Currently, the ComputedAccessibleNode is owned on the Element, so if a
    related node is requested and it does not have an element (for example
    the root of the document) it will return a null ComputedAccessibleNode.
    This is desirable behaviour, so this change has uncovered a use case for
    owning or at least associating the ComputedAccessibleNode with a Node,
    which will be added in a later change.
    
    BUG=792783
    
    Change-Id: Ifbe4785e2f0cf453527507f973f830634840694a

--- content/renderer/accessibility/aom_content_ax_tree.h
+++ content/renderer/accessibility/aom_content_ax_tree.h
@@ -22,15 +22,20 @@ class AomContentAxTree : public blink::WebComputedAXTree {
 
   // blink::WebComputedAXTree implementation.
   bool ComputeAccessibilityTree() override;
-
-  // String accessible property getters.
-  // TODO(meredithl): Change these to return null rather than empty strings.
-  blink::WebString GetNameForAXNode(int32_t) override;
-  blink::WebString GetRoleForAXNode(int32_t) override;
-
-  bool GetIntAttributeForAXNode(int32_t axID,
+  bool GetRoleForAXNode(int32_t ax_id, blink::WebString* out_param) override;
+  bool GetStringAttributeForAXNode(int32_t,
+                                   blink::WebAOMStringAttribute,
+                                   blink::WebString* out_param) override;
+  bool GetIntAttributeForAXNode(int32_t ax_id,
                                 blink::WebAOMIntAttribute,
                                 int32_t* out_param) override;
+
+  bool GetParentIdForAXNode(int32_t ax_id, int32_t* out_param) override;
+  bool GetFirstChildIdForAXNode(int32_t ax_id, int32_t* out_param) override;
+  bool GetLastChildIdForAXNode(int32_t ax_id, int32_t* out_param) override;
+  bool GetPreviousSiblingIdForAXNode(int32_t ax_id,
+                                     int32_t* out_param) override;
+  bool GetNextSiblingIdForAXNode(int32_t ax_id, int32_t* out_param) override;
 
  private:
   ui::AXTree tree_;
