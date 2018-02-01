f54d60ad78b6b8a099b4aaf8ee385a6a4269d506
Chris Blume <cblume@chromium.org>
2018-02-01 12:59:43 -0800

    Use new child surface allocation for resize case
    This patch introduces a child allocator for surface IDs.
    This allows a renderer to create a surface as it needs (such as when
    creating the Cast dialog), as opposed to request a surface from the
    parent.
    
    BUG=789259
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I36acec47f083dffd029aba56431eda720579e63e

--- ui/aura/window.h
+++ ui/aura/window.h
@@ -365,6 +365,14 @@ class AURA_EXPORT Window : public ui::LayerDelegate,
   // Gets the current viz::LocalSurfaceId.
   const viz::LocalSurfaceId& GetLocalSurfaceId() const;
 
+  // Sets the current viz::LocalSurfaceId, in cases where the child has
+  // allocated one.
+  void SetLocalSurfaceId(const viz::LocalSurfaceId& local_surface_id);
+
+  // Sets the child sequence number of the viz::LocalSurfaceId, in cases where
+  // the a parent is given the child-allocated one.
+  void SetLocalSurfaceIdChildSequenceNumber(uint32_t child_sequence_number);
+
   // Returns the FrameSinkId. In LOCAL mode, this returns a valid FrameSinkId
   // only if a LayerTreeFrameSink has been created. In MUS mode, this always
   // return a valid FrameSinkId.
