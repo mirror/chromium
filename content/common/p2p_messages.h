d708c0023b619ec2746b14ec92d9f6638ae20485
Ramin Halavati <rhalavati@chromium.org>
2018-02-01 13:56:47 +0100

    Add traffic annotation argument to P2PHostMsg_Send.
    To have proper network traffic annotation in
    P2PSocketDispatcherHost::OnSend, the argument is added to
    P2PHostMsg_Send IPC message.
    
    Bug:656607
    Change-Id: Ie563b8f6da7d3855013648968fa5eb36b81abf01

--- content/common/p2p_messages.h
+++ content/common/p2p_messages.h
@@ -66,6 +66,12 @@ IPC_STRUCT_TRAITS_BEGIN(content::P2PPortRange)
   IPC_STRUCT_TRAITS_MEMBER(max_port)
 IPC_STRUCT_TRAITS_END()
 
+IPC_STRUCT_TRAITS_BEGIN(content::P2PPacketInfo)
+  IPC_STRUCT_TRAITS_MEMBER(destination)
+  IPC_STRUCT_TRAITS_MEMBER(packet_options)
+  IPC_STRUCT_TRAITS_MEMBER(packet_id)
+IPC_STRUCT_TRAITS_END()
+
 // P2P Socket messages sent from the browser to the renderer.
 
 IPC_MESSAGE_CONTROL3(P2PMsg_NetworkListChanged,
@@ -124,12 +130,12 @@ IPC_MESSAGE_CONTROL3(P2PHostMsg_AcceptIncomingTcpConnection,
                     int /* connected_socket_id */)
 
 // TODO(sergeyu): Use shared memory to pass the data.
-IPC_MESSAGE_CONTROL5(P2PHostMsg_Send,
-                     int /* socket_id */,
-                     net::IPEndPoint /* socket_address */,
-                     std::vector<char> /* data */,
-                     rtc::PacketOptions /* packet options */,
-                     uint64_t /* packet_id */)
+IPC_MESSAGE_CONTROL4(
+    P2PHostMsg_Send,
+    int /* socket_id */,
+    std::vector<char> /* data */,
+    content::P2PPacketInfo /* packet_info */,
+    net::MutableNetworkTrafficAnnotationTag /* traffic_annotation */)
 
 IPC_MESSAGE_CONTROL1(P2PHostMsg_DestroySocket,
                      int /* socket_id */)
