84ae95b50d21316c54cefdd0bf9a147a71f60109
Yash Malik <ymalik@google.com>
2018-01-31 21:02:22 -0500

    VR: Web input editing in VR (WIP)
    Not ready for review.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: If668f95bcdab8388ff2be5255adebf1f1837fcd1

--- chrome/browser/android/vr_shell/vr_shell.h
+++ chrome/browser/android/vr_shell/vr_shell.h
@@ -144,6 +144,23 @@ class VrShell : device::GvrGamepadDataProvider,
       JNIEnv* env,
       const base::android::JavaParamRef<jobject>& obj,
       int mode);
+  void ShowSoftInput(JNIEnv* env,
+                     const base::android::JavaParamRef<jobject>& obj);
+  void HideSoftInput(JNIEnv* env,
+                     const base::android::JavaParamRef<jobject>& obj);
+  void UpdateWebInputSelectionIndices(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& obj,
+      int selection_start,
+      int selection_end);
+  void UpdateWebInputCompositionIndices(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& obj,
+      int composition_start,
+      int composition_end);
+  void UpdateWebInputText(JNIEnv* env,
+                          const base::android::JavaParamRef<jobject>& obj,
+                          jstring text);
   void OnWebInputEdited(vr::TextInputInfo info);
   void OnWebInputCommitted(vr::TextInputInfo info);
 
