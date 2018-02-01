00af1cb481c6fa1fb2032f6e83a9b4137753dab5
Elly Fong-Jones <ellyjones@chromium.org>
2018-02-01 10:29:45 -0500

    polychrome: collapse XControllerForWindow implementations
    This change merges the views and cocoa browser implementations of the
    BrowserWindowControllerForWindow and TabWindowControllerForWindow helpers. The
    behavior is the same as before in all configs, but there is no longer a symbol
    conflict.
    
    Bug: 804950
    Change-Id: I4799d27c3119a6a1a98676265ccad92412665a56

--- chrome/browser/ui/cocoa/browser_window_cocoa_views_mac.mm
+++ chrome/browser/ui/cocoa/browser_window_cocoa_views_mac.mm
@@ -4,13 +4,32 @@
 
 #import "chrome/browser/ui/cocoa/browser_window_views_mac.h"
 
+#include "build/buildflag.h"
 #import "chrome/browser/ui/cocoa/browser_window_controller.h"
 #import "chrome/browser/ui/cocoa/tabs/tab_window_controller.h"
+#include "chrome/browser/ui/views_mode_controller.h"
+#include "ui/base/ui_features.h"
+
+namespace {
+
+bool HasCocoaWindow() {
+#if BUILDFLAG(MAC_VIEWS_BROWSER)
+  return views_mode_controller::IsViewsBrowserCocoa();
+#else
+  return true;
+#endif
+}
+
+}  // namespace
 
 BrowserWindowController* BrowserWindowControllerForWindow(NSWindow* window) {
-  return [BrowserWindowController browserWindowControllerForWindow:window];
+  return HasCocoaWindow()
+             ? [BrowserWindowController browserWindowControllerForWindow:window]
+             : nullptr;
 }
 
 TabWindowController* TabWindowControllerForWindow(NSWindow* window) {
-  return [TabWindowController tabWindowControllerForWindow:window];
+  return HasCocoaWindow()
+             ? [TabWindowController tabWindowControllerForWindow:window]
+             : nullptr;
 }
