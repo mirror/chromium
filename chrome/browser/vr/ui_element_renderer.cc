3f1f6f78f256edf18e55fd4d2465ab2cab711ca3
Tibor Goldschwendt <tiborg@chromium.org>
2018-02-01 10:15:28 -0500

    [vr] Procedural controller model
    Procedurally generate a controller model as a replacement for the Gltf
    mesh. Procedural controller is disabled for now until we have the proper
    button icons.
    
    Bug: 799606
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I09ce54d3374154db9baf352e5a2e7b5b923d1618

--- chrome/browser/vr/ui_element_renderer.cc
+++ chrome/browser/vr/ui_element_renderer.cc
@@ -42,7 +42,8 @@ void UiElementRenderer::Init() {
   webvr_renderer_ = std::make_unique<WebVrRenderer>();
   reticle_renderer_ = std::make_unique<Reticle::Renderer>();
   laser_renderer_ = std::make_unique<Laser::Renderer>();
-  controller_renderer_ = std::make_unique<Controller::Renderer>();
+  gltf_controller_renderer_ = std::make_unique<GltfController::Renderer>();
+  procedural_controller_renderer_ = std::make_unique<Controller::Renderer>();
   gradient_grid_renderer_ = std::make_unique<Grid::Renderer>();
   shadow_renderer_ = std::make_unique<Shadow::Renderer>();
   stars_renderer_ = std::make_unique<Stars::Renderer>();
@@ -95,15 +96,22 @@ void UiElementRenderer::DrawGradientGridQuad(
                                 opacity);
 }
 
-void UiElementRenderer::DrawController(
+void UiElementRenderer::DrawGltfController(
     ControllerMesh::State state,
     float opacity,
     const gfx::Transform& model_view_proj_matrix) {
-  if (!controller_renderer_->IsSetUp()) {
+  if (!gltf_controller_renderer_->IsSetUp()) {
     return;
   }
-  FlushIfNecessary(controller_renderer_.get());
-  controller_renderer_->Draw(state, opacity, model_view_proj_matrix);
+  FlushIfNecessary(gltf_controller_renderer_.get());
+  gltf_controller_renderer_->Draw(state, opacity, model_view_proj_matrix);
+}
+
+void UiElementRenderer::DrawController(
+    float opacity,
+    const gfx::Transform& model_view_proj_matrix) {
+  FlushIfNecessary(procedural_controller_renderer_.get());
+  procedural_controller_renderer_->Draw(opacity, model_view_proj_matrix);
 }
 
 void UiElementRenderer::DrawLaser(
@@ -170,7 +178,7 @@ void UiElementRenderer::Flush() {
 }
 
 void UiElementRenderer::SetUpController(std::unique_ptr<ControllerMesh> mesh) {
-  controller_renderer_->SetUp(std::move(mesh));
+  gltf_controller_renderer_->SetUp(std::move(mesh));
 }
 
 void UiElementRenderer::FlushIfNecessary(BaseRenderer* renderer) {
