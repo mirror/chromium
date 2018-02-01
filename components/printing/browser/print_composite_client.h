8edc1efca3270a7fa307863aa340260b6da18098
Wei Li <weili@chromium.org>
2018-02-01 12:33:51 -0800

    Support OOPIF printing in browser
    Allow browser side to handle subframe printing message and notify the
    renderer of the subframe to print its content. Add communications with
    pdf compositor service to compose the final content.
    
    The blink side of change is at crrev.com/c/818525. The high level
    design doc is at goo.gl/CF1RUy
    
    BUG=455764
    
    Change-Id: I1a039a5f76e186b34b54dba6d974f5f41d1a6550

--- components/printing/browser/print_composite_client.h
+++ components/printing/browser/print_composite_client.h
@@ -9,24 +9,45 @@
 #include <memory>
 
 #include "base/optional.h"
+#include "components/printing/service/public/cpp/pdf_service_mojo_types.h"
 #include "components/printing/service/public/interfaces/pdf_compositor.mojom.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
+#include "printing/common/pdf_metafile_utils.h"
 #include "services/service_manager/public/cpp/connector.h"
+
+struct PrintHostMsg_DidPrintContent_Params;
 
 namespace printing {
 
 // Class to manage print requests and their communication with pdf
 // compositor service.
 // Each composite request have a separate interface pointer to connect
-// with remote service.
+// with remote service. The request and its subframe printing results are
+// tracked by its document cookie and print page number.
 class PrintCompositeClient
-    : public content::WebContentsUserData<PrintCompositeClient> {
+    : public content::WebContentsUserData<PrintCompositeClient>,
+      public content::WebContentsObserver {
  public:
-  using ContentToFrameMap = std::unordered_map<uint32_t, uint64_t>;
-
   explicit PrintCompositeClient(content::WebContents* web_contents);
   ~PrintCompositeClient() override;
+
+  // content::WebContentsObserver
+  bool OnMessageReceived(const IPC::Message& message,
+                         content::RenderFrameHost* render_frame_host) override;
+
+  // IPC message handler.
+  void OnDidPrintFrameContent(
+      content::RenderFrameHost* render_frame_host,
+      int document_cookie,
+      int page_number,
+      const PrintHostMsg_DidPrintContent_Params& params);
+
+  // Instructs the specified subframe to print.
+  void PrintSubframe(const gfx::Rect& rect,
+                     int document_cookie,
+                     int page_number,
+                     content::RenderFrameHost* render_frame_host);
 
   // NOTE: |handle| must be a READ-ONLY base::SharedMemoryHandle, i.e. one
   // acquired by base::SharedMemory::GetReadOnlyHandle().
@@ -49,6 +70,13 @@ class PrintCompositeClient
 
   void set_for_preview(bool for_preview) { for_preview_ = for_preview; }
   bool for_preview() const { return for_preview_; }
+
+  // Convert a content id to proxy id map to a content id to frame's global
+  // unique id map.
+  static ContentToFrameMap ConvertContentInfoMap(
+      content::WebContents* web_contents,
+      content::RenderFrameHost* render_frame_host,
+      const ContentToProxyIdMap& content_proxy_map);
 
  private:
   // Since page number is always non-negative, use this value to indicate it is
