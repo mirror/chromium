15ff41f9bbbccde9c189cba0da95b6fdafca7767
Shivani Sharma <shivanisha@chromium.org>
2018-02-01 12:28:45 -0500

    Add a flag to ResourceRequest identifying whether it as an ad resource.
    This CL adds a flag to ResourceRequest which is currently set based on
    the subresource filter component matching. Subsequently this will also
    be set when the initiator of this resource is an ad resource even if
    this does not match the subresource filter ruleset.
    
    This plumbs the flag from blink::ResourceRequest up to
    content::ResourceRequest so that it can be used in the browser process
    as well in a future CL.
    
    Bug:
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: I09fe5b3168cae53b47b4eefff4a1bddfac6a98b4

--- third_party/WebKit/Source/platform/loader/fetch/ResourceRequest.h
+++ third_party/WebKit/Source/platform/loader/fetch/ResourceRequest.h
@@ -65,6 +65,8 @@ enum InputToLoadPerfMetricReportPolicy : uint8_t {
   kReportIntent,  // Report metrics for this request as initiated by an intent.
 };
 
+enum class AdResource { NOT_SET, SET_TRUE, SET_FALSE };
+
 struct CrossThreadResourceRequestData;
 
 // A ResourceRequest is a "request" object for ResourceLoader. Conceptually
@@ -361,6 +363,9 @@ class PLATFORM_EXPORT ResourceRequest final {
   }
   bool IsSameDocumentNavigation() const { return is_same_document_navigation_; }
 
+  void SetIsAdResource(bool is_ad_resource);
+  bool IsAdResource() const { return is_ad_resource_ == AdResource::SET_TRUE; }
+
  private:
   const CacheControlHeader& GetCacheControlHeader() const;
 
@@ -413,6 +418,8 @@ class PLATFORM_EXPORT ResourceRequest final {
   static double default_timeout_interval_;
 
   double navigation_start_ = 0;
+
+  AdResource is_ad_resource_ = AdResource::NOT_SET;
 };
 
 // This class is needed to copy a ResourceRequest across threads, because it
@@ -468,6 +475,7 @@ struct CrossThreadResourceRequestData {
   InputToLoadPerfMetricReportPolicy input_perf_metric_report_policy_;
   ResourceRequest::RedirectStatus redirect_status_;
   base::Optional<String> suggested_filename_;
+  AdResource is_ad_resource_;
 };
 
 }  // namespace blink
