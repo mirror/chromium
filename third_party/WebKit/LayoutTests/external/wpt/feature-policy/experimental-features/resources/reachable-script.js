6b114d55b033b415305a0e15103fcce82a2c6ce4
Nate Chapin <japhet@chromium.org>
2018-02-01 09:12:10 -0800

    [WIP] FeaturePolicy sync-script
    Change-Id: Icd8a5d747373a819c7073ac459b5e8823d7986a8

---
+++ third_party/WebKit/LayoutTests/external/wpt/feature-policy/experimental-features/resources/reachable-script.js
@@ -0,0 +1,1 @@
+document.body.appendChild(document.createTextNode("PASS: async or defer script should run"));
---
+++ third_party/WebKit/LayoutTests/external/wpt/feature-policy/experimental-features/resources/unreachable-script.js
@@ -0,0 +1,1 @@
+document.body.appendChild(document.createTextNode("FAIL: external sync script should not be run"));
