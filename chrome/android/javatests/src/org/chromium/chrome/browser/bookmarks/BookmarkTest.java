8f50e0c64839ac23d5c0a4d212b532b72f92a2fc
Maria Khomenko <mariakhomenko@chromium.org>
2018-01-31 16:21:27 -0800

    Disable BookmarkTest#addBookmark because it's flaky.
    Bug: 807807
    Change-Id: Ibaff48b7c5d998da98526c10d5996b5920b199c6

--- chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkTest.java
@@ -26,6 +26,7 @@ import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
@@ -151,6 +152,7 @@ public class BookmarkTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "crbug.com/807807")
     public void testAddBookmark() throws InterruptedException {
         mActivityTestRule.loadUrl(mTestPage);
         // Check partner bookmarks are lazily loaded.
