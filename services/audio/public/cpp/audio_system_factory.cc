1e7c0338eb2749ab3f6433c9b97c399a64ce073a
Olga Sharonova <olka@chromium.org>
2018-02-01 18:15:54 +0100

    Audio service embedded in content and AudioSystem on top of it
    1) Audio service is embedded into /content. For that AudioManager creation is moved before mojo
    initialization in BrowserMainLoop::BrowserThreadsStarted(), since the service needs to hold a
    pointer to an already-created AudioManager.
    
    2) audio::CreateAudioSystemInstance() is introduced as a replacement to
    media::AudioSystem::CreateInstance(). At this point it still creates media::AudioSystemImpl
    instance. The intention is to switch it to audio::AudioSystemToServiceAdapter creation in the
    next CL, to simplify a revert if one would be required. (PS22 has a dry run with
    audio::AudioSystemToServiceAdapter instantiation of AudioSystem.)
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ie69df7bf9f593fda6a2e85f5f57a49a90aeccdb9

---
+++ services/audio/public/cpp/audio_system_factory.cc
@@ -0,0 +1,24 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/audio/audio_system.h"
+
+#include "media/audio/audio_system_impl.h"
+#include "services/audio/public/cpp/audio_system_to_service_adapter.h"
+#include "services/service_manager/public/cpp/connector.h"
+
+namespace audio {
+
+std::unique_ptr<media::AudioSystem> CreateAudioSystemInstance(
+    std::unique_ptr<service_manager::Connector> connector) {
+  // TODO(olka): Switch to the service-based AudioSystem in the next CL.
+  const bool kSystemInfoViaService = false;
+  if (kSystemInfoViaService) {
+    return std::make_unique<audio::AudioSystemToServiceAdapter>(
+        std::move(connector));
+  }
+  return media::AudioSystemImpl::CreateInstance();
+}
+
+}  // namespace audio
