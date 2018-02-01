5504711512911af5f2bf1fdd7a9f72769b6cc7b4
James Wallace-Lee <jamwalla@chromium.org>
2018-02-01 11:04:10 -0800

    Create empty PlatformServiceBridgeImpl
    PlatformServiceBridge, instead of using reflection, should load
    an instance of PlatformServiceBridgeImpl that is determined by GN.
    A downstream cl, which should land first, provides an alternate
    implementation of PlatformServiceBridgeImpl.
    
    Bug: 682070
    Change-Id: I642603ccb4e4dd4a602803772acd343bfe6cf0dd

---
+++ android_webview/java/src/org/chromium/android_webview/PlatformServiceBridgeImpl.java
@@ -0,0 +1,12 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview;
+
+/**
+ * Instantiable version of {@link PlatformServiceBridge}, don't add anything to this class!
+ * Downstream targets may provide a different implementation. See http://crbug/682070.
+ */
+
+public class PlatformServiceBridgeImpl extends PlatformServiceBridge {}
