2c02b3b1bb984033d37263188af2e490117a03d3
James Cook <jamescook@chromium.org>
2018-02-01 11:12:45 -0800

    ozone: Fix mash_browser_tests DCHECKs in gfx::ClientNativePixmapFactory
    In chrome --mash and browser_tests --mash the ash service and window
    service run in the same process but on different threads. There's a race
    between the ash service creation of gfx::ClientNativePixmapFactory and
    the window service's use of it.
    
    Change the gfx::ClientNativePixmapFactory to be owned by a thread-safe
    base::Singleton so that it gets created once by whichever service needs
    it first.
    
    Also eliminate an unused reference to the factory in BrowserMainLoop.
    
    Bug: 807781
    Test: mash_browser_tests
    
    Change-Id: I7b77c3d6ad957c87ed947caa703b7f65161ebb04

--- ui/ozone/public/client_native_pixmap_factory_ozone.cc
+++ ui/ozone/public/client_native_pixmap_factory_ozone.cc
@@ -4,17 +4,48 @@
 
 #include "ui/ozone/public/client_native_pixmap_factory_ozone.h"
 
+#include <memory>
+
+#include "base/memory/singleton.h"
 #include "base/trace_event/trace_event.h"
+#include "ui/gfx/client_native_pixmap_factory.h"
 #include "ui/ozone/platform_object.h"
 #include "ui/ozone/platform_selection.h"
 
 namespace ui {
+namespace {
 
-std::unique_ptr<gfx::ClientNativePixmapFactory>
-CreateClientNativePixmapFactoryOzone() {
-  TRACE_EVENT1("ozone", "CreateClientNativePixmapFactoryOzone", "platform",
-               GetOzonePlatformName());
-  return PlatformObject<gfx::ClientNativePixmapFactory>::Create();
+// Thread-safe owner of the gfx::ClientNativePixmapFactory. Not a LazyInstance
+// because it uses PlatformObject<>::Create() for factory construction.
+class PixmapFactorySingleton {
+ public:
+  static PixmapFactorySingleton* GetInstance() {
+    return base::Singleton<PixmapFactorySingleton>::get();
+  }
+
+ private:
+  friend struct base::DefaultSingletonTraits<PixmapFactorySingleton>;
+
+  PixmapFactorySingleton() {
+    LOG(ERROR) << "JAMES ui::CreateClientNativePixmapFactoryOzone";
+    TRACE_EVENT1("ozone", "CreateClientNativePixmapFactoryOzone", "platform",
+                 GetOzonePlatformName());
+    pixmap_factory_ = PlatformObject<gfx::ClientNativePixmapFactory>::Create();
+    gfx::ClientNativePixmapFactory::SetInstance(pixmap_factory_.get());
+  }
+
+  std::unique_ptr<gfx::ClientNativePixmapFactory> pixmap_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(PixmapFactorySingleton);
+};
+
+}  // namespace
+
+void CreateClientNativePixmapFactoryOzone() {
+  // Multiple threads may race to create the factory (e.g. when the UI service
+  // and ash service are running in the same process). Create the object as a
+  // side effect of a thread-safe singleton.
+  PixmapFactorySingleton::GetInstance();
 }
 
 }  // namespace ui
