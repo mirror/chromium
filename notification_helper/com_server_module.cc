81b9667f8a8afd7e30a0a71b86a9873865366b5b
Xi Cheng <chengx@chromium.org>
2018-01-31 18:06:09 -0800

    Introduce notification_helper.exe to handle Win10 toast activation
    We are bringing the Windows 10 native notification feature to Chrome. Apart from
    displaying the notifications in Windows Action Center, Chrome should be capable
    of responding to user interaction on those notifications. To achieve this, we
    need to create a class that implements the INotificationActivationCallback
    interface. Besides, we also need to register this class object at runtime so
    that COM knows how to instantiate it when there is a request from Windows Action
    Center (where the toast notification lives). It is dangerous to do the
    registration in Chrome.exe because this makes Chrome.exe a COM server. This
    means Chrome is now bound to the lifetime of objects owned outside of Chrome.
    In this case, Chrome.exe may not be able to shut down when the user wants to.
    Therefore, we need to create a new EXE to do so. Moreover, now that toast
    notifications can stay in Action Center for days until the user sees them, we
    want Chrome to be activated at any time from a toast if necessary. So in
    addition to requiring Chrome to use an event handler for activation
    (IToastNotification::add_Activated) that only works while Chrome is running, we
    need to introduce a COM server to invoke Chrome when users interact with the
    notifications when Chrome is not running.
    
    This CL implements this requirement. It consists of the following main changes:
    
    1) Introduce a new EXE: notification_helper.exe. It servers as a COM server
       which is invoked whenever there is activation from toast notifications.
    2) Introduce a new class called NotificationActivator which implements the
       INotificationActivationCallBack interface.
    3) Each time notification_helper.exe is invoked, it registers the
       NotificationActivator class object to a COM module. This allows COM to
       create the object in the COM server and call the object's Activate() method
       to handle toast activation when required. Then the NotificationActivator
       class is unregistered from the module. This CL does not change the existing
       toast activation behavior which requires Chrome to be running. Handling toast
       activation when Chrome is not running requires more steps such as launching
       Chrome in Activate(), which will be implemented in a following CL.
    4) After the unregistration of the NotificationActivator class,
       notification_helper.exe exits. A waitable event is used to prevent the EXE
       from exiting until the registration, activatation, and unregistration steps
       are finished.
    5) Register notification_helper.exe to Windows via registry when installing
       Chrome, and unregister it when uninstalling Chrome. We use different CLSIDs
       for the new NotificationActivator class in each channel. In this way, Chrome
       with the right channel will be activated when there is toast activation.
    
    Bug: 734095
    Change-Id: Iacd9fb8bdb69f88667c7d81eade448e7a4d1e4ac

---
+++ notification_helper/com_server_module.cc
@@ -0,0 +1,124 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This macro is used in <wrl/module.h>. Since only the COM functionality is
+// used here (while WinRT isn't being used), define this macro to optimize
+// compilation of <wrl/module.h> for COM-only.
+#ifndef __WRL_CLASSIC_COM_STRICT__
+#define __WRL_CLASSIC_COM_STRICT__
+
+#include "notification_helper/com_server_module.h"
+
+#include <atltrace.h>
+#include <wrl/module.h>
+
+#include "chrome/install_static/install_util.h"
+#include "notification_helper/notification_activator.h"
+
+namespace notification_helper {
+
+namespace {
+
+// This value is used to initialize the WaitableEvent object. This MUST BE set
+// to MANUAL for correct operation of the IsSignaled() call in
+// IsEventSignaled(). See the comment there for why.
+constexpr base::WaitableEvent::ResetPolicy kResetPolicy =
+    base::WaitableEvent::ResetPolicy::MANUAL;
+
+}  // namespace
+
+ComServerModule::ComServerModule()
+    : com_object_released_(kResetPolicy,
+                           base::WaitableEvent::InitialState::NOT_SIGNALED) {}
+
+ComServerModule::~ComServerModule() = default;
+
+HRESULT ComServerModule::Run() {
+  HRESULT hr = RegisterClassObjects();
+  if (FAILED(hr))
+    return hr;
+
+  WaitForAllObjectsReleased();
+  UnregisterClassObjects();
+
+  return hr;
+}
+
+HRESULT ComServerModule::RegisterClassObjects() {
+  // Create the class factory for NotificationActivator.
+  Microsoft::WRL::ComPtr<IUnknown> factory;
+  unsigned int flags = Microsoft::WRL::ModuleType::OutOfProcDisableCaching;
+
+  HRESULT hr = ::Microsoft::WRL::Details::CreateClassFactory<
+      ::Microsoft::WRL::SimpleClassFactory<NotificationActivator>>(
+      &flags, nullptr, __uuidof(IClassFactory), &factory);
+  if (FAILED(hr)) {
+    ATL::AtlTrace("Factory creation failed\n");
+    return hr;
+  }
+
+  Microsoft::WRL::ComPtr<IClassFactory> class_factory;
+  hr = factory.As(&class_factory);
+  if (FAILED(hr))
+    return hr;
+
+  // Create an out-of-proc COM module with caching disabled. The supplied
+  // callback (SignalObjectReleaseEvent) is called when the last instance object
+  // of the module is released.
+  auto& module =
+      Microsoft::WRL::Module<Microsoft::WRL::OutOfProcDisableCaching>::Create(
+          this, &ComServerModule::SignalObjectReleaseEvent);
+
+  // Usually COM module classes statically define their CLSID at compile time
+  // through the use of various macros, and WRL::Module internals takes care of
+  // creating the class objects and registering them. However, we need to
+  // register the same object with different CLSIDs depending on a runtime
+  // setting, so we handle that logic here.
+  IID class_ids[] = {install_static::GetToastActivatorClsid()};
+  static_assert(arraysize(cookies_) == arraysize(class_ids),
+                "Arrays cookies_ and class_ids must be the same size.");
+
+  // The use of class_factory.GetAddressOf() below assumes that there's only one
+  // factory being registered, which is true for now. We need to put the
+  // interface pointer in an array like class_ids and cookies_ if more than one
+  // class is ever registered.
+  hr =
+      module.RegisterCOMObject(nullptr, class_ids, class_factory.GetAddressOf(),
+                               cookies_, arraysize(cookies_));
+  if (FAILED(hr)) {
+    ATL::AtlTrace("NotificationActivator registration failed\n");
+    return hr;
+  }
+
+  return hr;
+}
+
+void ComServerModule::UnregisterClassObjects() {
+  auto& module = Microsoft::WRL::Module<
+      Microsoft::WRL::OutOfProcDisableCaching>::GetModule();
+  HRESULT hr =
+      module.UnregisterCOMObject(nullptr, cookies_, arraysize(cookies_));
+  if (FAILED(hr))
+    ATL::AtlTrace("NotificationActivator unregistration failed\n");
+}
+
+bool ComServerModule::IsEventSignaled() {
+  // The IsSignaled() check below requires that the WaitableEvent be manually
+  // reset, to avoid signaling the event in IsSignaled() itself.
+  static_assert(kResetPolicy == base::WaitableEvent::ResetPolicy::MANUAL,
+                "The reset policy must be set to MANUAL");
+  return com_object_released_.IsSignaled();
+}
+
+void ComServerModule::WaitForAllObjectsReleased() {
+  com_object_released_.Wait();
+}
+
+void ComServerModule::SignalObjectReleaseEvent() {
+  com_object_released_.Signal();
+}
+
+}  // namespace notification_helper
+
+#endif  // __WRL_CLASSIC_COM_STRICT__
