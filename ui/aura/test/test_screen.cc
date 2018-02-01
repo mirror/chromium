54803e3f552c26b69ddcbc276f7fe20e2802afff
Mitsuru Oshima <oshima@chromium.org>
2018-01-31 23:25:08 -0800

    Update the ooip frame's WebView's scale when dsf changes
    BUG=803442
    TEST=Manual
    
    Change-Id: I1b77c6d589596f66ed1bc9135d6be26318df872a

--- ui/aura/test/test_screen.cc
+++ ui/aura/test/test_screen.cc
@@ -40,7 +40,12 @@ TestScreen* TestScreen::Create(const gfx::Size& size,
                         window_tree_client);
 }
 
-TestScreen::~TestScreen() {}
+TestScreen::~TestScreen() {
+  if (host_) {
+    host_->window()->RemoveObserver(this);
+    delete host_;
+  }
+}
 
 WindowTreeHost* TestScreen::CreateHostForPrimaryDisplay() {
   DCHECK(!host_);
@@ -148,8 +153,10 @@ void TestScreen::OnWindowBoundsChanged(Window* window,
 }
 
 void TestScreen::OnWindowDestroying(Window* window) {
-  if (host_->window() == window)
+  if (host_->window() == window) {
+    host_->window()->RemoveObserver(this);
     host_ = NULL;
+  }
 }
 
 gfx::Point TestScreen::GetCursorScreenPoint() {
