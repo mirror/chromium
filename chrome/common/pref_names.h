bcd6dce51250fac49b53e7210c30c7bba464e4b6
Patrick Monette <pmonette@chromium.org>
2018-02-02 11:18:52 -0500

    Add a class to track problematic programs in Local State
    Bug: 717696
    Change-Id: I6ff0a2ca192b2fe11426f59336a5e5efe2a8b76e

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -914,6 +914,8 @@ extern const char kSitePerProcess[];
 // Preference for controlling whether or not third party blocking is enabled on
 // Windows.
 extern const char kThirdPartyBlockingEnabled[];
+
+extern const char kProblematicPrograms[];
 #endif
 
 }  // namespace prefs
