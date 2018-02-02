c32d37236184491c26fba4ff197d01ba30834cd3
Adrienne Walker <enne@chromium.org>
2018-02-02 14:13:46 -0800

    oop: Clear textures to prevent visual garbage
    Textures in oop raster are currently cleared only when they are new.
    However, when textures are reused (both for partial raster and for
    recycling) they need to clear what's underneath them.
    
    This also fixes several bugs in RasterSource clearing.  In particular,
    clipDeviceRegion (possibly only for gpu?) does not reset the clip,
    only adds to it.  It also unions together all the rects in the
    region.  This leads to increased clipping, and no clearing of texels
    outside of the raster full rect.
    
    This passes more parameters to RasterSource and PaintOpBuffer so that
    the following clearing can be done:
    (1) if not opaque and not partial raster, clear everything to transparent.
    (2) if not opaque and partial raster, clear inside clip to transparent.
    (3) if opaque and playback rect intersects with one texel inside the
    content size for the layer, then draw 2xheight and widthx2 pixel rectangles
    to clear the last pixels.
    
    Also add a number of unit tests to verify that minimal clearing is done
    that would have caught the RasterSource changes.
    
    This also removes debug clearing code in RasterSource which is old and
    is not as useful as it once was.
    
    Bug: 805746,806014
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I5f68ef63ee8c2831906890e3e2239ac1e3db430e

--- gpu/command_buffer/client/raster_interface.h
+++ gpu/command_buffer/client/raster_interface.h
@@ -16,7 +16,7 @@ class ImageProvider;
 
 namespace gfx {
 class Rect;
-class Vector2d;
+class Size;
 class Vector2dF;
 }  // namespace gfx
 
@@ -100,10 +100,12 @@ class RasterInterface {
   // OOP-Raster
   virtual void RasterCHROMIUM(const cc::DisplayItemList* list,
                               cc::ImageProvider* provider,
-                              const gfx::Vector2d& translate,
+                              const gfx::Size& content_size,
+                              const gfx::Rect& full_raster_rect,
                               const gfx::Rect& playback_rect,
                               const gfx::Vector2dF& post_translate,
-                              GLfloat post_scale) = 0;
+                              GLfloat post_scale,
+                              bool requires_clear) = 0;
 
   // Raster via GrContext.
   virtual void BeginGpuRaster() = 0;
