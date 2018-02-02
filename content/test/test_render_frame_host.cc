fc3adebfea29fa58ac17f978df8525c44ee58dc3
Arthur Hemery <ahemery@chromium.org>
2018-02-02 10:46:16 +0100

    Navigation: Introducing the NavigationClient mojo interface.
    This CL introduces a new interface called NavigationClient,
    implemented by the RenderFrame. This interface should allow to
    solve a number of race conditions as well as enabling
    other things. See the related BUG for more information on this.
    
    As a first step we replace two IPCs by reworking BeginNavigation.
    The idea is to send an InterfacePtr over BeginNavigation to allow
    further control of the navigation.
    
    An important part of this CL is getting the proper lifetime for the
    Interface. We think that provisional_document_loader_ in the
    FrameLoader is a good candidate to own the binding.
    On the browser side, the NavigationRequest is in charge of holding
    the InterfacePtr.
    
    BeginNavigation gets a new parameter: the InterfacePtr being
    transmitted. FrameMsg_DroppedNavigation is replaced by the browser
    closing the pipe. FrameHostMsg_AbortNavigation is replaced by the
    renderer closing the pipe.
    The new interface does not get any method for now.
    
    Bug: 784904
    Change-Id: Ifca1b3f6bdab1b3988f867c7cce44b7292e6668e

--- content/test/test_render_frame_host.cc
+++ content/test/test_render_frame_host.cc
@@ -38,6 +38,12 @@
 
 namespace content {
 
+MockNavigationClientImpl::MockNavigationClientImpl(
+    mojom::NavigationClientRequest request)
+    : navigation_client_binding_(this, std::move(request)) {}
+
+MockNavigationClientImpl::~MockNavigationClientImpl() {}
+
 TestRenderFrameHostCreationObserver::TestRenderFrameHostCreationObserver(
     WebContents* web_contents)
     : WebContentsObserver(web_contents), last_created_frame_(nullptr) {}
@@ -117,7 +123,8 @@ TestRenderFrameHost::TestRenderFrameHost(SiteInstance* site_instance,
                                         : nullptr),
       contents_mime_type_("text/html"),
       simulate_history_list_was_cleared_(false),
-      last_commit_was_error_page_(false) {}
+      last_commit_was_error_page_(false),
+      navigation_client_impl_(nullptr) {}
 
 TestRenderFrameHost::~TestRenderFrameHost() {
 }
@@ -496,24 +503,28 @@ void TestRenderFrameHost::SendRendererInitiatedNavigationRequest(
   // initialized. Do it if it hasn't happened yet.
   InitializeRenderFrameIfNeeded();
 
-  if (IsBrowserSideNavigationEnabled()) {
-    // TODO(mkwst): The initiator origin here is incorrect.
-    mojom::BeginNavigationParamsPtr begin_params =
-        mojom::BeginNavigationParams::New(
-            std::string() /* headers */, net::LOAD_NORMAL,
-            false /* skip_service_worker */, REQUEST_CONTEXT_TYPE_HYPERLINK,
-            blink::WebMixedContentContextType::kBlockable,
-            false /* is_form_submission */, GURL() /* searchable_form_url */,
-            std::string() /* searchable_form_encoding */, url::Origin(),
-            GURL() /* client_side_redirect_url */);
-    CommonNavigationParams common_params;
-    common_params.url = url;
-    common_params.referrer = Referrer(GURL(), blink::kWebReferrerPolicyDefault);
-    common_params.transition = ui::PAGE_TRANSITION_LINK;
-    common_params.navigation_type = FrameMsg_Navigate_Type::DIFFERENT_DOCUMENT;
-    common_params.has_user_gesture = has_user_gesture;
-    BeginNavigation(common_params, std::move(begin_params));
-  }
+  // TODO(mkwst): The initiator origin here is incorrect.
+  mojom::BeginNavigationParamsPtr begin_params =
+      mojom::BeginNavigationParams::New(
+          std::string() /* headers */, net::LOAD_NORMAL,
+          false /* skip_service_worker */, REQUEST_CONTEXT_TYPE_HYPERLINK,
+          blink::WebMixedContentContextType::kBlockable,
+          false /* is_form_submission */, GURL() /* searchable_form_url */,
+          std::string() /* searchable_form_encoding */, url::Origin(),
+          GURL() /* client_side_redirect_url */);
+  CommonNavigationParams common_params;
+  common_params.url = url;
+  common_params.referrer = Referrer(GURL(), blink::kWebReferrerPolicyDefault);
+  common_params.transition = ui::PAGE_TRANSITION_LINK;
+  common_params.navigation_type = FrameMsg_Navigate_Type::DIFFERENT_DOCUMENT;
+  common_params.has_user_gesture = has_user_gesture;
+
+  mojom::NavigationClientPtr navigation_client_ptr;
+  navigation_client_impl_ = std::make_unique<MockNavigationClientImpl>(
+      mojo::MakeRequest(&navigation_client_ptr));
+
+  BeginNavigation(common_params, std::move(begin_params),
+                  std::move(navigation_client_ptr));
 }
 
 void TestRenderFrameHost::DidChangeOpener(int opener_routing_id) {
--- content/test/test_render_frame_host.h
+++ content/test/test_render_frame_host.h
@@ -12,6 +12,7 @@
 
 #include "base/macros.h"
 #include "content/browser/frame_host/render_frame_host_impl.h"
+#include "content/common/navigation.mojom.h"
 #include "content/common/navigation_params.mojom.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/test/mock_render_process_host.h"
@@ -27,6 +28,15 @@ class HostPortPair;
 }
 
 namespace content {
+
+class MockNavigationClientImpl : public mojom::NavigationClient {
+ public:
+  MockNavigationClientImpl(mojom::NavigationClientRequest request);
+  ~MockNavigationClientImpl() override;
+
+ private:
+  mojo::Binding<NavigationClient> navigation_client_binding_;
+};
 
 class TestRenderFrameHostCreationObserver : public WebContentsObserver {
  public:
@@ -229,6 +239,9 @@ class TestRenderFrameHost : public RenderFrameHostImpl,
 
   // Used to track and forward outgoing navigation requests from the host.
   std::unique_ptr<NavigationInterceptor> navigation_interceptor_;
+
+  // Unbound navigation request
+  std::unique_ptr<MockNavigationClientImpl> navigation_client_impl_;
 
   DISALLOW_COPY_AND_ASSIGN(TestRenderFrameHost);
 };
