392c38d86402dd7808fd20f43d9ed63f97b64e28
Ehsan Karamad <ekaramad@chromium.org>
2018-02-01 19:08:08 -0500

    Fix an issue with speculative frame cleanup in OOPIFs
    When the beforeunload handler is ACKed by the OOPIF process with "do no
    proceed", the corresponding speculative frame is cleaned up. When the
    speculative frame is in parent frame's process it will set the
    EmbeddedContentView of the owner element to nullptr. This is incorrect
    as the navigation has been canceled.
    
    This CL will avoid setting the EmbeddedContentView to nullptr when the
    ContentFrame() is not equal to Document::GetFrame() (i.e., speculative)
    and also the EmbeddedContentView is a RemoteFrameView.
    
    Bug: 578349, 673170, 807772
    Change-Id: Ic6f5a16ae6f02bfbe11238851936ee323472a407

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -2740,7 +2740,16 @@ void Document::Shutdown() {
   // in LocalFrame::CreateView(). See also https://crbug.com/673170 and the
   // comment in LocalFrameView::Dispose().
   HTMLFrameOwnerElement* owner_element = frame_->DeprecatedLocalOwner();
-  if (owner_element)
+
+  // If the current EmbeddedContentView is of RemoteFrameView type we should not
+  // remove it (https://crbug.com/807772).
+  bool clear_embedded_content_view =
+      owner_element &&
+      !(owner_element->ContentFrame() &&
+        owner_element->ContentFrame() != frame_ &&
+        owner_element->OwnedEmbeddedContentView() &&
+        owner_element->OwnedEmbeddedContentView()->IsFrameView());
+  if (clear_embedded_content_view)
     owner_element->SetEmbeddedContentView(nullptr);
 
   markers_->PrepareForDestruction();
