9f08e85bf4b64f0a4a0f397171ca46886a2755e4
Tsuyoshi Horo <horo@chromium.org>
2018-02-02 14:15:25 +0900

    Implement the parser for TLS 1.3 Certificate message for signed HTTP exchange.
    Bug: 803774
    Change-Id: I55182556a39c1ed852f2412df18707eae7a7c2c2

---
+++ content/browser/loader/signed_exchange_cert_fetcher.h
@@ -0,0 +1,30 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_LOADER_SIGNED_EXCHANGE_CERT_CETCHER_H_
+#define CONTENT_BROWSER_LOADER_SIGNED_EXCHANGE_CERT_CETCHER_H_
+
+#include <string>
+#include <vector>
+
+#include "base/gtest_prod_util.h"
+#include "base/optional.h"
+#include "base/strings/string_piece_forward.h"
+#include "content/common/content_export.h"
+
+namespace content {
+
+class CONTENT_EXPORT SignedExchangeCertFetcher {
+ private:
+  friend class SignedExchangeCertFetcherTest;
+
+  // Parses a TLS 1.3 Certificate message containing X.509v3 certificates and
+  // returns a vector of cert_data. Returns nullopt when failed to parse.
+  static base::Optional<std::vector<base::StringPiece>> GetCertChainFromMessage(
+      const base::StringPiece& message);
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_LOADER_SIGNED_EXCHANGE_CERT_CETCHER_H_
