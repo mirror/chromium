0eeaa2bc80296828da2718fecede7230a8b74d15
Malay Keshav <malaykeshav@chromium.org>
2018-02-01 23:06:38 -0800

    Implements Material design 2 UI for tab and tabstrip
    This patch adds multiple layout changes to the tab and tabstrip under
    the new google material design 2 standard.
    
    These changes include:
     - Increasing the tab and tabstrip height.
     - Adjusting tab content layout including the title, clost tab button
       and the alert indicator.
     - Decreasing the tab touch width and increasing the tab standard width.
     - Adds the material design 2 tab close button icon.
     - Adds the basic color constants required for google material design 2.
       These colors are also added to the tab, tab strip and their contents.
    
    What this patch does not modify in tab and tabstrp?
     - Colors for alert indicator.
     - Separator colors.
     - Typography.
     - New tab button.
    
    The most recent screenshots for the change are posted on the bug below.
    
    Bug:805762
    Change-Id: Id7c1fc92f0a78a3429ff695b96f122374cbb1202
    Component: Tabs, tabstrip, material design controller, layout constants

--- chrome/browser/ui/views/tabs/tab_close_button.cc
+++ chrome/browser/ui/views/tabs/tab_close_button.cc
@@ -4,17 +4,63 @@
 
 #include "chrome/browser/ui/views/tabs/tab_close_button.h"
 
+#include <map>
 #include <memory>
+#include <vector>
 
+#include "base/hash.h"
+#include "base/stl_util.h"
 #include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/ui/layout_constants.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/material_design/material_design_controller.h"
+#include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/rect_based_targeting_utils.h"
 
 #if defined(USE_AURA)
 #include "ui/aura/env.h"
 #endif
+
+namespace {
+
+size_t GetCacheKey(const TabCloseButton::StateColorInfo& info) {
+  size_t base = base::HashInts32(info.icon, info.icon_hover);
+  base = base::HashInts32(base, info.background);
+  base = base::HashInts32(base, info.background_hover);
+  base = base::HashInts32(base, info.background_pressed);
+  return base;
+}
+
+// Returns a vector list of skia images. Each index in the vector represents a
+// button state in the following order - {NORMAL, HOVERED, PRESSED}
+std::vector<gfx::ImageSkia> GetSkiaImagesForInfo(
+    const TabCloseButton::StateColorInfo& info) {
+  static std::map<size_t, std::vector<gfx::ImageSkia>> cache;
+  const size_t key = GetCacheKey(info);
+  if (base::ContainsKey(cache, key))
+    return cache.at(key);
+
+  cache[key] = std::vector<gfx::ImageSkia>();
+  const std::vector<SkColor> icon_colors = {info.icon, info.icon_hover,
+                                            info.icon_hover};
+  const std::vector<SkColor> base_colors = {
+      info.background, info.background_hover, info.background_pressed};
+
+  for (std::size_t i = 0; i < icon_colors.size(); i++) {
+    const gfx::ImageSkia& icon =
+        gfx::CreateVectorIcon(kTabCloseButtonTouchIcon, icon_colors[i]);
+    const gfx::ImageSkia& base =
+        gfx::CreateVectorIcon(kTabCloseButtonBaseIcon, base_colors[i]);
+    const gfx::ImageSkia final_image =
+        gfx::ImageSkiaOperations::CreateSuperimposedImage(base, icon);
+    cache[key].push_back(final_image);
+  }
+  return cache.at(key);
+}
+
+}  // namespace
 
 TabCloseButton::TabCloseButton(views::ButtonListener* listener,
                                MouseEventCallback mouse_event_callback)
@@ -23,15 +69,17 @@ TabCloseButton::TabCloseButton(views::ButtonListener* listener,
   SetEventTargeter(std::make_unique<views::ViewTargeter>(this));
   SetAccessibleName(l10n_util::GetStringUTF16(IDS_ACCNAME_CLOSE));
 
-  // The normal image is set by OnButtonColorMaybeChanged() because it depends
-  // on the current theme and active state.  The hovered and pressed images
-  // don't depend on the these, so we can set them here.
-  const gfx::ImageSkia& hovered = gfx::CreateVectorIcon(
-      kTabCloseHoveredPressedIcon, SkColorSetRGB(0xDB, 0x44, 0x37));
-  const gfx::ImageSkia& pressed = gfx::CreateVectorIcon(
-      kTabCloseHoveredPressedIcon, SkColorSetRGB(0xA8, 0x35, 0x2A));
-  SetImage(views::Button::STATE_HOVERED, &hovered);
-  SetImage(views::Button::STATE_PRESSED, &pressed);
+  if (!ui::MaterialDesignController::IsTouchOptimizedMaterial()) {
+    // The normal image is set by OnButtonColorMaybeChanged() because it depends
+    // on the current theme and active state.  The hovered and pressed images
+    // don't depend on the these, so we can set them here.
+    const gfx::ImageSkia& hovered = gfx::CreateVectorIcon(
+        kTabCloseHoveredPressedIcon, SkColorSetRGB(0xDB, 0x44, 0x37));
+    const gfx::ImageSkia& pressed = gfx::CreateVectorIcon(
+        kTabCloseHoveredPressedIcon, SkColorSetRGB(0xA8, 0x35, 0x2A));
+    SetImage(views::Button::STATE_HOVERED, &hovered);
+    SetImage(views::Button::STATE_PRESSED, &pressed);
+  }
 
   // Disable animation so that the red danger sign shows up immediately
   // to help avoid mis-clicks.
@@ -44,6 +92,22 @@ void TabCloseButton::SetTabColor(SkColor color) {
   const gfx::ImageSkia& close_button_normal_image =
       gfx::CreateVectorIcon(kTabCloseNormalIcon, color);
   SetImage(views::Button::STATE_NORMAL, &close_button_normal_image);
+}
+
+void TabCloseButton::SetStateColorInfo(bool for_active_state,
+                                       const StateColorInfo& info) {
+  if (for_active_state)
+    active_state_info_ = info;
+  else
+    inactive_state_info_ = info;
+}
+
+void TabCloseButton::ActiveStateChanged(bool is_active) {
+  std::vector<gfx::ImageSkia> images = GetSkiaImagesForInfo(
+      is_active ? active_state_info_ : inactive_state_info_);
+  SetImage(views::Button::STATE_NORMAL, images[0]);
+  SetImage(views::Button::STATE_HOVERED, images[1]);
+  SetImage(views::Button::STATE_PRESSED, images[2]);
 }
 
 views::View* TabCloseButton::GetTooltipHandlerForPoint(
