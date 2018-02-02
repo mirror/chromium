1838334dba83469d99f1d69e9a1d334f26d18833
Tim Brown <timbrown@chromium.org>
2018-02-02 10:51:48 -0800

    Add libappindicator3-1 as a dependency
    This is to simplify our status icon code.
    
    Ubuntu Zesty has been EOL'd, so that has been removed too.
    
    Bug: 799144
    Change-Id: I325144461a330b6321a087e4357e9033d7fdd880

--- chrome/installer/linux/debian/update_dist_package_versions.py
+++ chrome/installer/linux/debian/update_dist_package_versions.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2017 The Chromium Authors. All rights reserved.
+# Copyright 2018 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -30,11 +30,11 @@ SUPPORTED_DEBIAN_RELEASES = {
 SUPPORTED_UBUNTU_RELEASES = {
     'Ubuntu 14.04 (Trusty)': 'trusty',
     'Ubuntu 16.04 (Xenial)': 'xenial',
-    'Ubuntu 17.04 (Zesty)': 'zesty',
     'Ubuntu 17.10 (Artful)': 'artful',
 }
 
 PACKAGE_FILTER = set([
+    "libappindicator3-1",
     "libasound2",
     "libatk1.0-0",
     "libatk-bridge2.0-0",
