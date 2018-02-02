6e84e2bb76e202a87a23c6adc34f8d847211f0b5
Eric Karl <ericrk@chromium.org>
2018-02-02 10:49:23 -0800

    Add UnpremultiplyAndDitherCopyCHROMIUM extension
    This change adds a new GL command (intended for use via the
    RasterInterface) which copies data from a normal-bit-depth (8888)
    texture to a low-bit-depth (4444) texture, performing an unpremultiply
    and a dither in the process.
    
    This is a pretty special-case extension designed to address the
    speicific issues that:
    - GPU raster output is currently not dithered, leading to banding in
       some cases
    - Dithering premultiplied output does not alleviate all banding.
    - Storing premultiplied ouptut in 4444 textures can lead to artifacts
       even if dithered.
    
    This extension will be used when Chrome rasterizes to 4444 textures on
    low-end devices.
    
    To achieve this behavior, we add a "dither" parameter to the
    CopyTextureCHROMIUMResourceManager, allowing it to perform a dither
    while copying. The new extension leverages this existing class.
    
    The actual dither code is the same logic used internally in Skia, and
    has been tested there for performance. The only difference is that in
    our case we dither alpha as well.
    
    Bug: 789153
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ibce954f90eb7c461e127c5eb8bcf3ad9799bfe20

--- gpu/command_buffer/service/gles2_cmd_decoder_passthrough_doers.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_passthrough_doers.cc
@@ -4724,5 +4724,16 @@ error::Error GLES2DecoderPassthroughImpl::DoDestroyGpuFenceCHROMIUM(
   return error::kNoError;
 }
 
+error::Error GLES2DecoderPassthroughImpl::DoUnpremultiplyAndDitherCopyCHROMIUM(
+    GLuint src_texture,
+    GLuint dst_texture,
+    GLint x,
+    GLint y,
+    GLsizei width,
+    GLsizei height) {
+  NOTIMPLEMENTED();
+  return error::kNoError;
+}
+
 }  // namespace gles2
 }  // namespace gpu
