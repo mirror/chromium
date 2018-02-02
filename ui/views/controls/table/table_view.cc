caf62bde564e834681f5b3636d1ffc1f509cd81c
Aaron Leventhal <aleventhal@chromium.org>
2018-02-02 17:23:46 -0500

    Test all UI Views for accessibility
    Bug: None
    Change-Id: I627deac6d4d8dcdfd9f838b73a7206404ed82903

--- ui/views/controls/table/table_view.cc
+++ ui/views/controls/table/table_view.cc
@@ -461,17 +461,19 @@ void TableView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
       node_data->AddState(ax::mojom::State::kSelected);
     }
 
+    // Create accessible name from column header.
     std::vector<base::string16> name_parts;
     for (const VisibleColumn& visible_column : visible_columns_) {
-      base::string16 value = model_->GetText(
-          selection_model_.active(), visible_column.column.id);
+      base::string16 value =
+          model_->GetText(selection_model_.active(), visible_column.column.id);
       if (!value.empty()) {
         name_parts.push_back(visible_column.column.title);
         name_parts.push_back(value);
       }
     }
     node_data->SetName(base::JoinString(name_parts, base::ASCIIToUTF16(", ")));
-  }
+  } else  // Name requires a selection.
+    node_data->SetNameFrom(ax::mojom::NameFrom::kAttributeExplicitlyEmpty);
 }
 
 void TableView::OnModelChanged() {
