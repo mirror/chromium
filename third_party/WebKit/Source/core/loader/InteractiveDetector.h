3a0d7cc6e63b6c07a64316cfa5894798ea756aa6
Deepanjan Roy <dproy@chromium.org>
2018-02-02 18:33:57 -0500

    Do not pass TTI to browser if FMP was invalidated
    If First Meaningful Paint is invalidated on the renderer side, we should
    not pass a TTI value to the browser. If we do, we end up with a
    PageLoadTiming struct that contains a TTI value but no FMP value, and we
    throw away that struct as invalid.
    
    We still log a trace event (following the convention of FMP), because
    the reaching of TTI quiescence can be useful data for post processing.
    
    Bug:808536
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Change-Id: I66869d0691bfc934fb94f427f6566f6a70463a73

--- third_party/WebKit/Source/core/loader/InteractiveDetector.h
+++ third_party/WebKit/Source/core/loader/InteractiveDetector.h
@@ -55,7 +55,8 @@ class CORE_EXPORT InteractiveDetector
   void OnResourceLoadEnd(WTF::Optional<TimeTicks> load_finish_time);
 
   void SetNavigationStartTime(TimeTicks navigation_start_time);
-  void OnFirstMeaningfulPaintDetected(TimeTicks fmp_time);
+  void OnFirstMeaningfulPaintDetected(TimeTicks fmp_time,
+                                      bool input_invalidated);
   void OnDomContentLoadedEnd(TimeTicks dcl_time);
   void OnInvalidatingInputEvent(TimeTicks invalidation_time);
 
@@ -82,12 +83,13 @@ class CORE_EXPORT InteractiveDetector
   TimeTicks interactive_detection_time_;
 
   // Page event times that Interactive Detector depends on.
-  // Value of 0.0 indicate the event has not been detected yet.
+  // Null TimeTicks values indicate the event has not been detected yet.
   struct {
     TimeTicks first_meaningful_paint;
     TimeTicks dom_content_loaded_end;
     TimeTicks nav_start;
     TimeTicks first_invalidating_input;
+    bool first_meaningful_paint_invalidated = false;
   } page_event_times_;
 
   // Stores sufficiently long quiet windows on main thread and network.
