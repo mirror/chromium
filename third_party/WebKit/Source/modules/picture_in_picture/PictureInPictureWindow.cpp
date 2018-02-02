e63539e74ba1363ac19ca5157f3965fe4985bdee
François Beaufort <beaufort.francois@gmail.com>
2018-02-02 21:21:06 +0000

    [Picture-in-Picture] Add PictureInPictureWindow width and height
    Bug: 806249
    Change-Id: I5e0f0875a53b7f659bc3469e562d3db5e075f31d

---
+++ third_party/WebKit/Source/modules/picture_in_picture/PictureInPictureWindow.cpp
@@ -0,0 +1,22 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "modules/picture_in_picture/PictureInPictureWindow.h"
+
+#include "core/dom/Document.h"
+
+namespace blink {
+
+PictureInPictureWindow* PictureInPictureWindow::Create(const Document& document,
+                                                       unsigned width,
+                                                       unsigned height) {
+  return new PictureInPictureWindow(document, width, height);
+}
+
+PictureInPictureWindow::PictureInPictureWindow(const Document& document,
+                                               unsigned width,
+                                               unsigned height)
+    : width_(width), height_(height) {}
+
+}  // namespace blink
---
+++ third_party/WebKit/Source/modules/picture_in_picture/PictureInPictureWindow.h
@@ -0,0 +1,35 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PictureInPictureWindow_h
+#define PictureInPictureWindow_h
+
+#include "platform/bindings/ScriptWrappable.h"
+#include "platform/heap/Handle.h"
+
+namespace blink {
+
+class Document;
+
+class PictureInPictureWindow : public ScriptWrappable {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  static PictureInPictureWindow* Create(const Document&,
+                                        unsigned width,
+                                        unsigned height);
+
+  unsigned width() const { return width_; }
+  unsigned height() const { return height_; }
+
+ private:
+  PictureInPictureWindow(const Document&, unsigned width, unsigned height);
+
+  unsigned width_;
+  unsigned height_;
+};
+
+}  // namespace blink
+
+#endif  // PictureInPictureWindow_h
