e409c3bcae4bed11a29c07b0c86799ca0da1a1be
Morten Stenshorne <mstensho@chromium.org>
2018-02-02 08:07:01 -0800

    [LayoutNG] Simplify and correct LayoutNewFormattingContext().
    Let the layout algorithm for a child figure out the size of the child,
    instead of estimating it in LayoutNewFormattingContext(). Gets rid of
    the "fixed inline size" case. This also means that we're trusting
    legacy layout more than before to correctly size legacy objects (such
    as tables, flexbox and replaced content), so we need to be more
    careful to provide legacy layout with the correct input data, i.e.
    the containing block size override. In particular, we cannot always
    use the percentage resolution size from the constraint space to
    resolve widths and heights on the child (only do that if the specified
    size is indeed a percentage; otherwise use available size, which has
    been adjusted for adjacent floats). Some work was also needed to make
    sure that legacy layout also respects this override to a greater
    extent than before.
    
    Absolutely positioned objects are still not allowed to size themselves
    (legacy or not). Not sure if that's good in the long run (then again,
    sizing of absolutely positioned objects is rather special). There was
    one missing check for content logical height override for abspos
    objects, which suddenly became an issue, and had to be fixed.
    
    Remove margin hacks for LayoutTable. Similar hacks would have been
    needed for flexbox, grid, etc. anyway. Instead, have
    LayoutNewFormattingContext() add the child's inline margins to the
    layout opportunity, so that all algorithms resolve auto inline sizes
    correctly (typically by subtracting margins from available
    space). Even pure NG layout algorithms need this.
    
    If it turns out (after layout) that the child won't fit within the
    layout opportunity found, find a new opportunity and lay out
    again. Repeat this step until we find something that fits. This
    potentially results in more layout passes than before, but we're going
    to have to do this for block fragmentation anyway, so better be
    consistent about it.
    
    Quite a few tests started to pass. Then, also, two tests regressed,
    both pertaining to the non-standard width:-webkit-fill-available declaration.
    One of the changes almost looks like an improvement, but it's not compatible
    with other engines. The spec is unclear.
    
    Bug: 807708, 807830
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Idf9f20c89640ea1c8d7659865a265e889c8211fd

--- third_party/WebKit/Source/core/layout/LayoutBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBox.cpp
@@ -3144,7 +3144,12 @@ void LayoutBox::ComputeLogicalHeight(
 
   Length h;
   if (IsOutOfFlowPositioned()) {
-    ComputePositionedLogicalHeight(computed_values);
+    if (HasOverrideLogicalContentHeight()) {
+      computed_values.extent_ =
+          OverrideLogicalContentHeight() + BorderAndPaddingLogicalHeight();
+    } else {
+      ComputePositionedLogicalHeight(computed_values);
+    }
   } else {
     LayoutBlock* cb = ContainingBlock();
 
@@ -4932,7 +4937,17 @@ bool LayoutBox::ShrinkToAvoidFloats() const {
     return false;
 
   // Only auto width objects can possibly shrink to avoid floats.
-  return Style()->Width().IsAuto();
+  if (!Style()->Width().IsAuto())
+    return false;
+
+  // If the containing block is LayoutNG, we will not let legacy layout deal
+  // with positioning of floats or sizing of auto-width new formatting context
+  // block level objects adjacent to them.
+  if (const auto* containing_block = ContainingBlock()) {
+    if (containing_block->IsLayoutNGMixin())
+      return false;
+  }
+  return true;
 }
 
 DISABLE_CFI_PERF
