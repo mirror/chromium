4bb0614ae9131c7fb151a14fa9727258722f9022
Nico Weber <thakis@chromium.org>
2018-02-03 16:32:14 -0500

    Fix -Wimplicit-fallthrough warnings for Chrome OS.
    This CL was uploaded by git cl split.
    
    R=hashimoto@chromium.org
    
    Bug: 177475,808623
    Change-Id: I570798f6420a1e0e84e651bec67ae92ad97ac808

--- chromeos/dbus/shill_ipconfig_client.cc
+++ chromeos/dbus/shill_ipconfig_client.cc
@@ -131,6 +131,7 @@ void ShillIPConfigClientImpl::SetProperty(const dbus::ObjectPath& ipconfig_path,
       }
       variant_writer.CloseContainer(&array_writer);
       writer.CloseContainer(&variant_writer);
+      FALLTHROUGH; // ?? https://crbug.com/808623
     }
     case base::Value::Type::BOOLEAN:
     case base::Value::Type::INTEGER:
