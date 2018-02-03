93692e4b1e87dca0020dc78d286168b8113f635f
Jun Choi <hongjunchoi@chromium.org>
2018-02-02 17:28:22 -0800

    Migrate device/u2f/apdu to src/components
    U2fApduCommand and U2fApduResponse objects in device/u2f are transport
    and protocol agnostic. Migrate all APDU related objects and tests to
    src/components.
    
    Bug:807836
    Change-Id: I96e71128eb0fcf13b8c38fc7e9a4d0b31203c99f

--- components/apdu/apdu_response.cc
+++ components/apdu/apdu_response.cc
@@ -2,12 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "components/apdu/apdu_response.h"
+
+#include <utility>
+
 #include "base/memory/ptr_util.h"
 
-#include "u2f_apdu_response.h"
+namespace apdu {
 
-namespace device {
-std::unique_ptr<U2fApduResponse> U2fApduResponse::CreateFromMessage(
+std::unique_ptr<APDUResponse> APDUResponse::CreateFromMessage(
     const std::vector<uint8_t>& message) {
   uint16_t status_bytes;
   Status response_status;
@@ -20,13 +23,12 @@ std::unique_ptr<U2fApduResponse> U2fApduResponse::CreateFromMessage(
   response_status = static_cast<Status>(status_bytes);
   std::vector<uint8_t> data(message.begin(), message.end() - 2);
 
-  return std::make_unique<U2fApduResponse>(std::move(data), response_status);
+  return std::make_unique<APDUResponse>(std::move(data), response_status);
 }
 
-U2fApduResponse::U2fApduResponse(std::vector<uint8_t> message,
-                                 Status response_status)
+APDUResponse::APDUResponse(std::vector<uint8_t> message, Status response_status)
     : response_status_(response_status), data_(std::move(message)) {}
 
-U2fApduResponse::~U2fApduResponse() = default;
+APDUResponse::~APDUResponse() = default;
 
-}  // namespace device
+}  // namespace apdu
