b9ff2c273ac610ac5509f7ea70d9cd4dee01de81
Alexei Filippov <alph@chromium.org>
2018-02-03 15:27:37 -0800

    Sampling Heap Profiler: Move profiler to src/base/heap_profiler
    BUG=803276
    
    Change-Id: I3e8391fa2fe8a78b29e8ee14323ae0d01671e911

---
+++ third_party/WebKit/Source/platform/SamplingHeapProfiler.cpp
@@ -0,0 +1,45 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/SamplingHeapProfiler.h"
+
+#include "base/heap_profiler/sampling_heap_profiler.h"
+
+namespace blink {
+
+// static
+uint32_t SamplingHeapProfiler::Start() {
+  return base::heap_profiler::SamplingHeapProfiler::GetInstance()->Start();
+}
+
+// static
+void SamplingHeapProfiler::Stop() {
+  base::heap_profiler::SamplingHeapProfiler::GetInstance()->Stop();
+}
+
+// static
+void SamplingHeapProfiler::SetSamplingInterval(size_t interval) {
+  base::heap_profiler::SamplingHeapProfiler::GetInstance()->SetSamplingInterval(
+      interval);
+}
+
+// static
+void SamplingHeapProfiler::SuppressRandomnessForTest() {
+  base::heap_profiler::SamplingHeapProfiler::GetInstance()
+      ->SuppressRandomnessForTest();
+}
+
+// static
+std::vector<SamplingHeapProfiler::Sample> SamplingHeapProfiler::GetSamples(
+    uint32_t profile_id) {
+  std::vector<SamplingHeapProfiler::Sample> result;
+  std::vector<base::heap_profiler::SamplingHeapProfiler::Sample> samples =
+      base::heap_profiler::SamplingHeapProfiler::GetInstance()->GetSamples(
+          profile_id);
+  for (auto& sample : samples)
+    result.push_back({sample.size, sample.count, std::move(sample.stack)});
+  return result;
+}
+
+}  // namespace blink
