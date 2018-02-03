4f920418e3aa98cf9d680b5adefbea05255f4305
Eve Martin-Jones <evem@chromium.org>
2018-02-03 14:39:16 +1100

    WIP: add LINUX_OR_ANDROID enabled feature to memory_instrumentation.mojom
    Change-Id: Ia5203f3ad37940270cca75945236f4bdfc0e2fdd

--- chrome/browser/metrics/process_memory_metrics_emitter_unittest.cc
+++ chrome/browser/metrics/process_memory_metrics_emitter_unittest.cc
@@ -81,8 +81,12 @@ class ProcessMemoryMetricsEmitterFake : public ProcessMemoryMetricsEmitter {
 
 OSMemDumpPtr GetFakeOSMemDump(uint32_t resident_set_kb,
                               uint32_t private_footprint_kb,
-                              uint32_t shared_footprint_kb,
-                              uint32_t private_swap_footprint_kb) {
+                              uint32_t shared_footprint_kb
+#if defined(OS_LINUX) || defined(OS_ANDROID)
+                              ,
+                              uint32_t private_swap_footprint_kb
+#endif
+                              ) {
   using memory_instrumentation::mojom::VmRegion;
 
   std::vector<memory_instrumentation::mojom::VmRegionPtr> vm_regions;
@@ -100,7 +104,12 @@ OSMemDumpPtr GetFakeOSMemDump(uint32_t resident_set_kb,
                     200));  // byte_stats_proportional_resident
   return memory_instrumentation::mojom::OSMemDump::New(
       resident_set_kb, private_footprint_kb, shared_footprint_kb,
-      std::move(vm_regions), private_swap_footprint_kb);
+      std::move(vm_regions)
+#if defined(OS_LINUX) || defined(OS_ANDROID)
+          ,
+      private_swap_footprint_kb
+#endif
+      );
 }
 
 void PopulateBrowserMetrics(GlobalMemoryDumpPtr& global_dump,
@@ -115,14 +124,13 @@ void PopulateBrowserMetrics(GlobalMemoryDumpPtr& global_dump,
   OSMemDumpPtr os_dump =
       GetFakeOSMemDump(metrics_mb["Resident"] * 1024,
                        metrics_mb["PrivateMemoryFootprint"] * 1024,
-                       metrics_mb["SharedMemoryFootprint"] * 1024,
+                       metrics_mb["SharedMemoryFootprint"] * 1024
 #if defined(OS_LINUX) || defined(OS_ANDROID)
                        // accessing PrivateSwapFootprint on other OSes will
                        // modify metrics_mb to create the value, which leads to
                        // expectation failures.
+                       ,
                        metrics_mb["PrivateSwapFootprint"] * 1024
-#else
-                       0
 #endif
                        );
   pmd->os_dump = std::move(os_dump);
@@ -163,14 +171,13 @@ void PopulateRendererMetrics(GlobalMemoryDumpPtr& global_dump,
   OSMemDumpPtr os_dump =
       GetFakeOSMemDump(metrics_mb["Resident"] * 1024,
                        metrics_mb["PrivateMemoryFootprint"] * 1024,
-                       metrics_mb["SharedMemoryFootprint"] * 1024,
+                       metrics_mb["SharedMemoryFootprint"] * 1024
 #if defined(OS_LINUX) || defined(OS_ANDROID)
                        // accessing PrivateSwapFootprint on other OSes will
                        // modify metrics_mb to create the value, which leads to
                        // expectation failures.
+                       ,
                        metrics_mb["PrivateSwapFootprint"] * 1024
-#else
-                       0
 #endif
                        );
   pmd->os_dump = std::move(os_dump);
@@ -216,14 +223,13 @@ void PopulateGpuMetrics(GlobalMemoryDumpPtr& global_dump,
   OSMemDumpPtr os_dump =
       GetFakeOSMemDump(metrics_mb["Resident"] * 1024,
                        metrics_mb["PrivateMemoryFootprint"] * 1024,
-                       metrics_mb["SharedMemoryFootprint"] * 1024,
+                       metrics_mb["SharedMemoryFootprint"] * 1024
 #if defined(OS_LINUX) || defined(OS_ANDROID)
                        // accessing PrivateSwapFootprint on other OSes will
                        // modify metrics_mb to create the value, which leads to
                        // expectation failures.
+                       ,
                        metrics_mb["PrivateSwapFootprint"] * 1024
-#else
-                       0
 #endif
                        );
   pmd->os_dump = std::move(os_dump);
