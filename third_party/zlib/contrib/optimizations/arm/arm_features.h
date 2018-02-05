138f5e706efd8ce82b2796927e7fc8befc985b1d
Adenilson Cavalcanti <adenilson.cavalcanti@arm.com>
2018-02-05 14:45:47 -0800

    Using ARMv8 CRC32 specific instruction
    CRC32 affects performance for both image decompression (PNG)
    as also in general browsing while accessing websites that serve
    content using compression (i.e. Content-Encoding: gzip).
    
    This patch implements an optimized CRC32 function using the
    dedicated instruction available in ARMv8. This instruction is available
    in new Android devices featuring an ARMv8 SoC, like Nexus 5x and
    Google Pixel.
    
    It should be between 3x (A72) to 7x faster (A53) than the
    C implementation currently used by zlib for 8KB vectors.
    
    This is performance critical code and can be called with both large (8KB)
    or small vectors, therefore we must avoid extraneous function calls or
    branching (otherwise the performance benefits are negated). So the use
    of 'public' variables to read the CPU features status flags
    (i.e. arm_cpu_enable_crc32 | pmull).
    
    Finally it also introduces code to perform the ARM CPU features detection
    using getauxval()@Linux/CrOS or android_getCpuFeatures(). We pre-built
    and link the CRC32 instruction dependent code but will decide if to
    use it at run time.
    
    If the feature is not supported, we fallback to the C implementation.
    
    This approach allows to use the instruction in both 32bits and 64bits and
    works fine either in ARMv7 or ARMv8 processor. I tested the generated
    Chrome apk in both a Nexus 6 (ARMv7) and a Google Pixel (ARMv8).
    
    Bug: 709716
    Change-Id: I315c1216f8b3a8d88607630a28737c41f52a2f5d

---
+++ third_party/zlib/contrib/optimizations/arm/arm_features.h
@@ -0,0 +1,22 @@
+/* arm_features.h -- ARM processor features detection.
+ * Copyright 2018 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the Chromium source repository LICENSE file.
+ */
+#ifndef __ARM_FEATURES__
+#define __ARM_FEATURES__
+#include <stdbool.h>
+
+/* Chromium zlib symbols mangling. */
+#include "names.h"
+
+/* A bit nasty as this are 'public' but we save a few cycles
+ * by accessing it directly in crc32() instead of using functions
+ * to return the values.
+ */
+extern bool arm_cpu_enable_crc32;
+extern bool arm_cpu_enable_pmull;
+
+void arm_check_features(void);
+
+#endif
