1951a5f260313d72c16ccaca2f941503997c5f99
Xida Chen <xidachen@chromium.org>
2018-02-05 16:25:59 -0500

    [PaintWorklet] Fix crash when hitting breakpoint during lifecycle
    If we put a breakpoint in the paint() function and start debugging, the
    renderer will crash when we resize the window or switch to the element
    tab. This is because when we are in the middle of lifecycle when we get
    to the paint function, and anything triggers layout update will cause a
    renderer crash.
    
    The fix in CL is that when we hit a breakpoint, check if we are in the
    middle of lifecycle or not, if we are, flip a bit to be true in the
    |LocalFrameView|, so that later on whenever there is a layout update we
    can check that bit and do early exit.
    
    Bug: 788219
    Change-Id: I4fbafab868508ed4ba453339d91364253d17f523

--- third_party/WebKit/Source/core/inspector/MainThreadDebugger.h
+++ third_party/WebKit/Source/core/inspector/MainThreadDebugger.h
@@ -47,6 +47,7 @@ class ErrorEvent;
 class LocalFrame;
 class SecurityOrigin;
 class SourceLocation;
+class DocumentLifecycle;
 
 class CORE_EXPORT MainThreadDebugger final : public ThreadDebugger {
  public:
@@ -121,6 +122,8 @@ class CORE_EXPORT MainThreadDebugger final : public ThreadDebugger {
   std::unique_ptr<InspectorTaskRunner> task_runner_;
   bool paused_;
   static MainThreadDebugger* instance_;
+  std::unique_ptr<DocumentLifecycle::PostponeTransitionScope>
+      postponed_transition_scope_;
   DISALLOW_COPY_AND_ASSIGN(MainThreadDebugger);
 };
 
