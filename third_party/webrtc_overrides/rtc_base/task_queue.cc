7d9f84186cee77384e9914c54a3355097b89783f
Tommi <tommi@chromium.org>
2018-02-05 11:47:28 +0100

    Trying out swapping WebRTC task queues for chromium's SequencedTaskRunner
    Change-Id: Ifd218c5e14e2ce634fd428606e9713a815bd8205
    Bug: 808801

--- third_party/webrtc_overrides/rtc_base/task_queue.cc
+++ third_party/webrtc_overrides/rtc_base/task_queue.cc
@@ -13,8 +13,10 @@
 #include "base/bind.h"
 #include "base/lazy_instance.h"
 #include "base/memory/ref_counted.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_local.h"
+#include "build/build_config.h"
 #include "third_party/webrtc/rtc_base/refcount.h"
 #include "third_party/webrtc/rtc_base/refcountedobject.h"
 
@@ -44,8 +46,8 @@ class PostAndReplyTask : public rtc::QueuedTask {
     if (!task_->Run())
       task_.release();
 
-    reply_task_runner_->PostTask(FROM_HERE,
-                                 base::Bind(&RunTask, base::Passed(&reply_)));
+    reply_task_runner_->PostTask(
+        FROM_HERE, base::BindOnce(&RunTask, base::Passed(&reply_)));
     return true;
   }
 
@@ -54,9 +56,45 @@ class PostAndReplyTask : public rtc::QueuedTask {
   scoped_refptr<base::SingleThreadTaskRunner> reply_task_runner_;
 };
 
+class PostAndReplyTask2 : public rtc::QueuedTask {
+ public:
+  PostAndReplyTask2(
+      std::unique_ptr<rtc::QueuedTask> task,
+      std::unique_ptr<rtc::QueuedTask> reply,
+      const scoped_refptr<base::SequencedTaskRunner>& reply_task_runner)
+      : task_(std::move(task)),
+        reply_(std::move(reply)),
+        reply_task_runner_(reply_task_runner) {}
+
+  ~PostAndReplyTask2() override {}
+
+ private:
+  bool Run() override {
+    // TODO: Handle Current();
+    if (!task_->Run())
+      task_.release();
+
+    reply_task_runner_->PostTask(
+        FROM_HERE, base::BindOnce(&RunTask, base::Passed(&reply_)));
+    return true;
+  }
+
+  std::unique_ptr<rtc::QueuedTask> task_;
+  std::unique_ptr<rtc::QueuedTask> reply_;
+  scoped_refptr<base::SequencedTaskRunner> reply_task_runner_;
+};
+
 // A lazily created thread local storage for quick access to a TaskQueue.
 base::LazyInstance<base::ThreadLocalPointer<rtc::TaskQueue>>::Leaky
     lazy_tls_ptr = LAZY_INSTANCE_INITIALIZER;
+
+void RunTask2(rtc::TaskQueue* tq, std::unique_ptr<rtc::QueuedTask> task) {
+  auto* prev = lazy_tls_ptr.Pointer()->Get();
+  lazy_tls_ptr.Pointer()->Set(tq);
+  if (!task->Run())
+    task.release();
+  lazy_tls_ptr.Pointer()->Set(prev);
+}
 
 }  // namespace
 
@@ -71,10 +109,71 @@ class TaskQueue::Impl : public RefCountInterface, public base::Thread {
   Impl(const char* queue_name, TaskQueue* queue);
   ~Impl() override;
 
+  void InitializeRunner(const base::TaskTraits& traits) {
+    DCHECK(!task_runner_);
+    task_runner_ = base::CreateSequencedTaskRunnerWithTraits(traits);
+  }
+
+  void PostTask(std::unique_ptr<QueuedTask> task) {
+    if (task_runner_) {
+      // TODO: figure out a safe way for setting the current queue.
+      task_runner_->PostTask(
+          FROM_HERE, base::BindOnce(&RunTask2, queue_, base::Passed(&task)));
+      return;
+    }
+    task_runner()->PostTask(FROM_HERE,
+                            base::BindOnce(&RunTask, base::Passed(&task)));
+  }
+
+  void PostDelayedTask(std::unique_ptr<QueuedTask> task,
+                       uint32_t milliseconds) {
+    if (task_runner_) {
+      // TODO: Handle Current();
+      task_runner_->PostDelayedTask(
+          FROM_HERE, base::BindOnce(&RunTask2, queue_, base::Passed(&task)),
+          base::TimeDelta::FromMilliseconds(milliseconds));
+      return;
+    }
+    task_runner()->PostDelayedTask(
+        FROM_HERE, base::BindOnce(&RunTask, base::Passed(&task)),
+        base::TimeDelta::FromMilliseconds(milliseconds));
+  }
+
+  void PostTaskAndReply(std::unique_ptr<QueuedTask> task,
+                        std::unique_ptr<QueuedTask> reply,
+                        TaskQueue* reply_queue) {
+    std::unique_ptr<QueuedTask> t;
+    if (reply_queue->impl_->task_runner_) {
+      t = std::unique_ptr<QueuedTask>(new PostAndReplyTask2(
+          std::move(task), std::move(reply), reply_queue->impl_->task_runner_));
+    } else {
+      t = std::unique_ptr<QueuedTask>(
+          new PostAndReplyTask(std::move(task), std::move(reply),
+                               reply_queue->impl_->task_runner()));
+    }
+
+    PostTask(std::move(t));
+  }
+
+  void PostTaskAndReply(std::unique_ptr<QueuedTask> task,
+                        std::unique_ptr<QueuedTask> reply) {
+    if (task_runner_) {
+      // TODO: Handle Current();
+      task_runner_->PostTaskAndReply(
+          FROM_HERE, base::BindOnce(&RunTask, base::Passed(&task)),
+          base::BindOnce(&RunTask, base::Passed(&reply)));
+      return;
+    }
+    task_runner()->PostTaskAndReply(
+        FROM_HERE, base::BindOnce(&RunTask, base::Passed(&task)),
+        base::BindOnce(&RunTask, base::Passed(&reply)));
+  }
+
  private:
   virtual void Init() override;
 
   TaskQueue* const queue_;
+  ::scoped_refptr<base::SequencedTaskRunner> task_runner_;
 };
 
 TaskQueue::Impl::Impl(const char* queue_name, TaskQueue* queue)
@@ -92,25 +191,39 @@ TaskQueue::TaskQueue(const char* queue_name,
                      Priority priority /*= Priority::NORMAL*/)
     : impl_(new RefCountedObject<Impl>(queue_name, this)) {
   DCHECK(queue_name);
-  base::Thread::Options options;
+  // base::Thread::Options options;
   switch (priority) {
     case Priority::HIGH:
-      options.priority = base::ThreadPriority::REALTIME_AUDIO;
+#if defined(OS_ANDROID)
+      impl_->InitializeRunner(
+          {base::WithBaseSyncPrimitives(), base::TaskPriority::HIGHEST});
+#else
+      impl_->InitializeRunner({base::TaskPriority::HIGHEST});
+#endif
+      // options.priority = base::ThreadPriority::REALTIME_AUDIO;
       break;
     case Priority::LOW:
-      options.priority = base::ThreadPriority::BACKGROUND;
+      impl_->InitializeRunner(
+          {base::MayBlock(), base::TaskPriority::BACKGROUND});
+      // options.priority = base::ThreadPriority::BACKGROUND;
       break;
     case Priority::NORMAL:
     default:
-      options.priority = base::ThreadPriority::NORMAL;
+#if defined(OS_ANDROID)
+      impl_->InitializeRunner({base::WithBaseSyncPrimitives()});
+#else
+      impl_->InitializeRunner({});
+#endif
+      // options.priority = base::ThreadPriority::NORMAL;
       break;
   }
-  CHECK(impl_->StartWithOptions(options));
+  // CHECK(impl_->StartWithOptions(options));
 }
 
 TaskQueue::~TaskQueue() {
   DCHECK(!IsCurrent());
-  impl_->Stop();
+  if (impl_->IsRunning())
+    impl_->Stop();
 }
 
 // static
@@ -119,29 +232,23 @@ TaskQueue* TaskQueue::Current() {
 }
 
 void TaskQueue::PostTask(std::unique_ptr<QueuedTask> task) {
-  impl_->task_runner()->PostTask(FROM_HERE,
-                                 base::Bind(&RunTask, base::Passed(&task)));
+  impl_->PostTask(std::move(task));
 }
 
 void TaskQueue::PostDelayedTask(std::unique_ptr<QueuedTask> task,
                                 uint32_t milliseconds) {
-  impl_->task_runner()->PostDelayedTask(
-      FROM_HERE, base::Bind(&RunTask, base::Passed(&task)),
-      base::TimeDelta::FromMilliseconds(milliseconds));
+  impl_->PostDelayedTask(std::move(task), milliseconds);
 }
 
 void TaskQueue::PostTaskAndReply(std::unique_ptr<QueuedTask> task,
                                  std::unique_ptr<QueuedTask> reply,
                                  TaskQueue* reply_queue) {
-  PostTask(std::unique_ptr<QueuedTask>(new PostAndReplyTask(
-      std::move(task), std::move(reply), reply_queue->impl_->task_runner())));
+  impl_->PostTaskAndReply(std::move(task), std::move(reply), reply_queue);
 }
 
 void TaskQueue::PostTaskAndReply(std::unique_ptr<QueuedTask> task,
                                  std::unique_ptr<QueuedTask> reply) {
-  impl_->task_runner()->PostTaskAndReply(
-      FROM_HERE, base::Bind(&RunTask, base::Passed(&task)),
-      base::Bind(&RunTask, base::Passed(&reply)));
+  impl_->PostTaskAndReply(std::move(task), std::move(reply));
 }
 
 }  // namespace rtc
