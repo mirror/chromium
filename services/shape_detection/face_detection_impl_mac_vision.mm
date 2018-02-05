c5612a5d9c12da8f085940d30943c3577acf94b8
junweifu <junwei.fu@intel.com>
2018-02-05 10:43:35 +0800

    ShapeDetection: Forward declaring the necessary symbols of Vision APIs
    Forward declaring all the necessary symbols in sdk_forward_declarations.h [1],
    then use the @available(macOS 10.13, *) in the face detection provider to choose
    different implementation.
    Start the minimum implementation of mojom::FaceDetection based on Vision
    Framework.
    
    Link Mac 10.13 build bots [2] and Face Detection demo[3] here.
    Split original large CL[4] up in smaller subpatches including this CL.
    
    [1] https://cs.chromium.org/chromium/src/base/mac/sdk_forward_declarations.h?sq=package:chromium&dr=CSs&l=5
    [2] https://ci.chromium.org/buildbot/chromium.fyi/Chromium%20Mac%2010.13/
    [3] https://codepen.io/miguelao/pen/PmJWro
    [4] https://chromium-review.googlesource.com/c/chromium/src/+/867651/10
    
    BUG=799319
    
    Change-Id: I18d06c7cadd9088d2c637258c8bd36299cb3c250

---
+++ services/shape_detection/face_detection_impl_mac_vision.mm
@@ -0,0 +1,23 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/shape_detection/face_detection_impl_mac_vision.h"
+
+namespace shape_detection {
+
+FaceDetectionImplMacVision::FaceDetectionImplMacVision() {
+  Class landmarks_request_class =
+      NSClassFromString(@"VNDetectFaceLandmarksRequest");
+  if (landmarks_request_class)
+    landmarks_request_.reset([[landmarks_request_class alloc] init]);
+}
+
+FaceDetectionImplMacVision::~FaceDetectionImplMacVision() = default;
+
+void FaceDetectionImplMacVision::Detect(const SkBitmap& bitmap,
+                                        DetectCallback callback) {
+  std::move(callback).Run({});
+}
+
+}  // namespace shape_detection
