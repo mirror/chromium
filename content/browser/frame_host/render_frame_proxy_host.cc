1b438ce96ffd2f940767356bbdb0b81bdac12e37
Wei Li <weili@chromium.org>
2018-02-05 15:13:42 -0800

    Support OOPIF printing in browser
    Allow browser side to handle subframe printing message and notify the
    renderer of the subframe to print its content. Add communications with
    pdf compositor service to compose the final content.
    
    The blink side of change is at crrev.com/c/818525. The high level
    design doc is at goo.gl/CF1RUy
    
    BUG=455764
    
    Change-Id: I1a039a5f76e186b34b54dba6d974f5f41d1a6550

--- content/browser/frame_host/render_frame_proxy_host.cc
+++ content/browser/frame_host/render_frame_proxy_host.cc
@@ -149,6 +149,7 @@ bool RenderFrameProxyHost::OnMessageReceived(const IPC::Message& msg) {
     IPC_MESSAGE_HANDLER(FrameHostMsg_DidChangeOpener, OnDidChangeOpener)
     IPC_MESSAGE_HANDLER(FrameHostMsg_AdvanceFocus, OnAdvanceFocus)
     IPC_MESSAGE_HANDLER(FrameHostMsg_FrameFocused, OnFrameFocused)
+    IPC_MESSAGE_HANDLER(FrameHostMsg_PrintSubframe, OnPrintSubframe)
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
   return handled;
@@ -385,6 +386,13 @@ void RenderFrameProxyHost::OnAdvanceFocus(blink::WebFocusType type,
 void RenderFrameProxyHost::OnFrameFocused() {
   frame_tree_node_->current_frame_host()->delegate()->SetFocusedFrame(
       frame_tree_node_, GetSiteInstance());
+}
+
+void RenderFrameProxyHost::OnPrintSubframe(const gfx::Rect& rect,
+                                           int document_cookie,
+                                           int page_number) {
+  RenderFrameHostImpl* rfh = frame_tree_node_->current_frame_host();
+  rfh->delegate()->PrintSubframe(rect, document_cookie, page_number, rfh);
 }
 
 }  // namespace content
