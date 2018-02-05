c9d691665c1b375f0d258de7c133e7d6e7c10942
Joe Downing <joedow@chromium.org>
2018-02-05 09:44:58 -0800

    Adding UMA entries for KeyboardLock APIs
    This change adds UMA entries so we can determine how often the new API
    is called and in the case of the Lock method, whether the caller
    supplied a set of keys or if they requested all keys.
    
    BUG=680809
    
    Change-Id: I5c6f1a29e5aa2cb51cb44d80f71f4201e8381219

--- content/browser/keyboard_lock/keyboard_lock_service_impl.cc
+++ content/browser/keyboard_lock/keyboard_lock_service_impl.cc
@@ -8,8 +8,28 @@
 #include <utility>
 
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_macros.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
+
+namespace {
+
+// These values must stay in sync with tools/metrics/histograms.xml.
+// Enum values should never be renumbered or reused as they are stored and can
+// be used for multi-release queries.  Insert any new values before |kCount| and
+// increment the count.
+enum class KeyboardLockMethods {
+  kRequestAllKeys = 0,
+  kRequestSomeKeys = 1,
+  kCancelLock = 2,
+  kCount = 3
+};
+
+void LogKeyboardLockMethodCalled(KeyboardLockMethods method) {
+  UMA_HISTOGRAM_ENUMERATION("Keyboard.LockApi.MethodCalled", method, kCount);
+}
+
+}  // namespace
 
 namespace content {
 
@@ -33,12 +53,20 @@ void KeyboardLockServiceImpl::CreateMojoService(
 void KeyboardLockServiceImpl::RequestKeyboardLock(
     const std::vector<std::string>& key_codes,
     RequestKeyboardLockCallback callback) {
-  // TODO(zijiehe): Implementation required.
+  if (key_codes.empty()) {
+    LogKeyboardLockMethodCalled(kRequestAllKeys);
+  } else {
+    LogKeyboardLockMethodCalled(kRequestSomeKeys);
+  }
+
+  // TODO(joedow): Implementation required.
   std::move(callback).Run(blink::mojom::KeyboardLockRequestResult::SUCCESS);
 }
 
 void KeyboardLockServiceImpl::CancelKeyboardLock() {
-  // TODO(zijiehe): Implementation required.
+  LogKeyboardLockMethodCalled(kCancelLock);
+
+  // TODO(joedow): Implementation required.
 }
 
 }  // namespace content
