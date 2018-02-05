fcc96d04c27e4f279ebb5027536ec01e774e81df
Noel Gordon <noel@chromium.org>
2018-02-05 20:15:16 +1100

    Remove x86_check_features from inflate
    Bug: 762564
    Change-Id: Id84da0eb52bc10edb541a284eab9aef652ba2c72

--- third_party/zlib/adler32.c
+++ third_party/zlib/adler32.c
@@ -98,9 +98,21 @@ uLong ZEXPORT adler32_z(adler, buf, len)
         return adler | (sum2 << 16);
     }
 
+#if defined(ADLER32_SIMD_SSSE3)
+    /*
+     * Use SSSE3 to compute the adler32. Since this routine can be
+     * freely used, check CPU features here.
+     */
+    if (buf == Z_NULL) {
+        if (!len) /* Assume user is calling adler32(0, NULL, 0); */
+            x86_check_features();
+        return 1L;
+    }
+#else
     /* initial Adler-32 value (deferred check for len == 1 speed) */
     if (buf == Z_NULL)
         return 1L;
+#endif
 
     /* in case short lengths are provided, keep it somewhat fast */
     if (len < 16) {
