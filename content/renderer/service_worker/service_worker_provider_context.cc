d961c60bba1c507f38a6b0f32e1a8fef3c709663
arthursonzogni <arthursonzogni@chromium.org>
2018-02-05 16:39:08 +0100

    NavigationMojoResponse: SendControllerServiceWorker on navigation commit.
    This fix an IPC race problem. More details in the design doc:
    https://docs.google.com/document/d/1dvvksKQYWEnUdOcjkyreaTOxJD9L4Gybszb5mxrSzHk/
    
    This was already fixed with S13nServiceWorker by:
    https://chromium-review.googlesource.com/742961
    This CL does the same with NavigationMojoResponse. In reality it was
    simpler here, because it reuses what was done in the previous CL.
    
    Design doc: https://goo.gl/Rrrc7n (NavigationMojoResponse)
    
    Bug: 778898, 705744
    Change-Id: I05676fcc0b4aa581f0f575cc6e7c91cc22b8549b

--- content/renderer/service_worker/service_worker_provider_context.cc
+++ content/renderer/service_worker/service_worker_provider_context.cc
@@ -15,6 +15,7 @@
 #include "content/child/child_thread_impl.h"
 #include "content/child/thread_safe_sender.h"
 #include "content/common/service_worker/service_worker_utils.h"
+#include "content/public/common/browser_side_navigation_policy.h"
 #include "content/public/common/service_names.mojom.h"
 #include "content/public/common/shared_url_loader_factory.h"
 #include "content/renderer/service_worker/controller_service_worker_connector.h"
@@ -109,13 +110,15 @@ ServiceWorkerProviderContext::ServiceWorkerProviderContext(
   state_for_client_ = std::make_unique<ProviderStateForClient>(
       std::move(default_loader_factory));
 
-  if (!CanCreateSubresourceLoaderFactory())
+  if (!CanCreateSubresourceLoaderFactory() &&
+      !IsNavigationMojoResponseEnabled())
     return;
 
-  // S13nServiceWorker:
+  // S13nServiceWorker/NavigationMojoResponse:
   // Set up the URL loader factory for sending subresource requests to
   // the controller.
-  DCHECK(ServiceWorkerUtils::IsServicificationEnabled());
+  DCHECK(ServiceWorkerUtils::IsServicificationEnabled() ||
+         IsNavigationMojoResponseEnabled());
   if (controller_info) {
     SetController(std::move(controller_info),
                   std::vector<blink::mojom::WebFeature>(),
@@ -317,7 +320,7 @@ void ServiceWorkerProviderContext::SetController(
   for (blink::mojom::WebFeature feature : used_features)
     state->used_features.insert(feature);
 
-  // S13nServiceWorker:
+  // S13nServiceWorker/NavigationMojoResponse:
   // Reset subresource loader factory if necessary.
   if (CanCreateSubresourceLoaderFactory()) {
     DCHECK(ServiceWorkerUtils::IsServicificationEnabled());
@@ -419,8 +422,8 @@ bool ServiceWorkerProviderContext::CanCreateSubresourceLoaderFactory() const {
   // for SharedWorker case (which is not supported by S13nServiceWorker
   // yet, https://crbug.com/796819) and in unit tests, return early in such
   // cases too.
-  return (ServiceWorkerUtils::IsServicificationEnabled() &&
-          state_for_client_->default_loader_factory);
+  return ServiceWorkerUtils::IsServicificationEnabled() &&
+         state_for_client_->default_loader_factory;
 }
 
 void ServiceWorkerProviderContext::DestructOnMainThread() const {
