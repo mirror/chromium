e018d6bac7e0a34f8231c690a6e6cd9a6067d307
Tibor Goldschwendt <tiborg@chromium.org>
2018-02-05 18:37:03 -0500

    [vr] Deprecate VR.Component.Assets.DurationUntilReady.OnChromeStart
    Bug: 799074
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I06c20525b10187804949ed9bed7485e483802d15

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
@@ -333,7 +333,6 @@ public class VrShellDelegate
         // Daydream is not supported on pre-N devices.
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) return;
         if (isVrEnabled() && !sChromeStarted) {
-            nativeOnChromeStarted();
             sChromeStarted = true;
         }
         if (sInstance != null) return; // Will be handled in onResume.
@@ -1808,5 +1807,4 @@ public class VrShellDelegate
     private native boolean nativeIsClearActivatePending(long nativeVrShellDelegate);
     private native void nativeDestroy(long nativeVrShellDelegate);
     private static native void nativeRegisterVrAssetsComponent();
-    private static native void nativeOnChromeStarted();
 }
