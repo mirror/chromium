000633d576d82b9e6217ee1464dacb23c6399f33
Weiliang Chen <weiliangc@chromium.org>
2018-02-06 16:00:24 -0500

    WIP: temporary CL for having skia renderer make copy request
    This CL doesn't have test, and should not be checked in, but can be
    used in ct.
    
    Change-Id: I0b68f385fce10c520b5c16f48034ea8a5ee7738f

--- content/browser/compositor/gpu_process_transport_factory.cc
+++ content/browser/compositor/gpu_process_transport_factory.cc
@@ -388,7 +388,7 @@ void GpuProcessTransportFactory::EstablishedGpuChannel(
       shared_worker_context_provider_ = nullptr;
 
     if (!shared_worker_context_provider_) {
-      bool need_alpha_channel = false;
+      bool need_alpha_channel = true;
       bool support_locking = true;
       bool support_gles2_interface = false;
       bool support_raster_interface = true;
@@ -414,7 +414,7 @@ void GpuProcessTransportFactory::EstablishedGpuChannel(
       // front buffer into a mailbox, insert a sync token, and send the
       // mailbox+sync to the ui service process.
       gpu::SurfaceHandle surface_handle = data->surface_handle;
-      bool need_alpha_channel = false;
+      bool need_alpha_channel = true;
       bool support_locking = false;
       bool support_gles2_interface = true;
       bool support_raster_interface = false;
@@ -973,7 +973,7 @@ GpuProcessTransportFactory::SharedMainThreadContextProvider() {
 
   // We need a separate context from the compositor's so that skia and gl_helper
   // don't step on each other.
-  bool need_alpha_channel = false;
+  bool need_alpha_channel = true;
   bool support_locking = false;
   bool support_gles2_interface = true;
   bool support_raster_interface = false;
