e8cedba070c4cebf4f916d22736f355f93cf9870
Zhenyao Mo <zmo@chromium.org>
2018-02-06 15:55:32 -0800

    Remove browser side GPU info collection on Android plus redo Android WebView GL bindings initialization.
    Patchset 1:
    
    The last bit is pre-lollipop Android blacklisting for
    accelerated_video_decode. To handle this, we query it on renderer
    startup from GPU process (through GpuChannelHost). This will cause
    slight renderer startup perf regression, but only on older Android
    systems.
    
    This is the LAST piece of this refactoring. This is reviewed at:
    https://chromium-review.googlesource.com/c/chromium/src/+/894734
    
    **************************
    
    Patchset 3:
    
    Right now initialization always happens on in-process GPU thread
    and others are blocked by that.
    
    With this CL, we could initialize them in either render thread or
    GPU thread.
    
    This is reviewed at:
    https://chromium-review.googlesource.com/c/chromium/src/+/898202
    
    BUG=744658,808078
    TEST=bots
    R=piman@chromium.org,kbr@chromium.org
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I07280fc5f6f513df87047a7b29b9d547fde08c8e

--- android_webview/browser/aw_contents.cc
+++ android_webview/browser/aw_contents.cc
@@ -64,7 +64,6 @@
 #include "content/public/browser/browsing_data_remover.h"
 #include "content/public/browser/child_process_security_policy.h"
 #include "content/public/browser/favicon_status.h"
-#include "content/public/browser/gpu_data_manager.h"
 #include "content/public/browser/interstitial_page.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/navigation_handle.h"
@@ -78,7 +77,6 @@
 #include "content/public/common/mhtml_generation_params.h"
 #include "content/public/common/renderer_preferences.h"
 #include "content/public/common/use_zoom_for_dsf_policy.h"
-#include "gpu/config/gpu_info.h"
 #include "jni/AwContents_jni.h"
 #include "net/base/auth.h"
 #include "net/cert/x509_certificate.h"
@@ -419,9 +417,11 @@ static jlong JNI_AwContents_Init(JNIEnv* env,
 static jboolean JNI_AwContents_HasRequiredHardwareExtensions(
     JNIEnv* env,
     const JavaParamRef<jclass>&) {
-  return content::GpuDataManager::GetInstance()
-      ->GetGPUInfo()
-      .can_support_threaded_texture_mailbox;
+  // Make sure GPUInfo is collected. This will initialize GL bindings,
+  // collect GPUInfo, and compute GpuFeatureInfo if they have not been
+  // already done.
+  DeferredGpuCommandService::GetInstance();
+  return DeferredGpuCommandService::CanSupportThreadedTextureMailbox();
 }
 
 static void JNI_AwContents_SetAwDrawSWFunctionTable(JNIEnv* env,
