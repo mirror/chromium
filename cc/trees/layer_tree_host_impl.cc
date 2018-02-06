6bb6838909d9552a1246cc849ad470715192f934
jonross <jonross@chromium.org>
2018-02-06 11:11:59 -0500

    WIP Renderer observer of frame submission
    A proposal to allow the renderer to notify the browser of frame submission,
    along with RenderFrameMetadata.
    
    Bug: 785013
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: Ic01e81331b34119a08488778f5dbe0c86a1775a3

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -72,6 +72,7 @@
 #include "cc/trees/layer_tree_impl.h"
 #include "cc/trees/mutator_host.h"
 #include "cc/trees/render_frame_metadata.h"
+#include "cc/trees/render_frame_metadata_observer.h"
 #include "cc/trees/scroll_node.h"
 #include "cc/trees/single_thread_proxy.h"
 #include "cc/trees/transform_node.h"
@@ -1915,6 +1916,12 @@ bool LayerTreeHostImpl::DrawLayers(FrameData* frame) {
 
   active_tree()->FinishSwapPromises(&metadata, &render_frame_metadata);
 
+  if (render_frame_metadata_observer_ &&
+      render_frame_metadata_observer_->enabled()) {
+    render_frame_metadata_observer_->OnRenderFrameSubmission(
+        &render_frame_metadata);
+  }
+
   metadata.latency_info.emplace_back(ui::SourceEventType::FRAME);
   ui::LatencyInfo& new_latency_info = metadata.latency_info.back();
   if (CommitToActiveTree()) {
@@ -3747,6 +3754,13 @@ void LayerTreeHostImpl::UpdateImageDecodingHints(
         decoding_mode_map) {
   tile_manager_.checker_image_tracker().UpdateImageDecodingHints(
       std::move(decoding_mode_map));
+}
+
+void LayerTreeHostImpl::SetRenderFrameObserver(
+    std::unique_ptr<RenderFrameMetadataObserver> observer) {
+  render_frame_metadata_observer_ = std::move(observer);
+  render_frame_metadata_observer_->SetCompositorTaskRunner(
+      task_runner_provider_->ImplThreadTaskRunner());
 }
 
 InputHandlerScrollResult LayerTreeHostImpl::ScrollBy(
