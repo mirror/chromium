c401b632d5c1239e7816345c991a6c015844315e
Tibor Goldschwendt <tiborg@chromium.org>
2018-02-06 10:40:54 -0500

    [vr] Deprecate VR.Component.Assets.DurationUntilReady.OnChromeStart
    Bug: 799074
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_vr;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I06c20525b10187804949ed9bed7485e483802d15

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
@@ -118,7 +118,6 @@ public class VrShellDelegate
     private static boolean sRegisteredDaydreamHook;
     private static boolean sAddedBlackOverlayView;
     private static boolean sRegisteredVrAssetsComponent;
-    private static boolean sChromeStarted;
     private static Boolean sIconComponentEnabled;
 
     private ChromeActivity mActivity;
@@ -332,10 +331,6 @@ public class VrShellDelegate
     public static void maybeRegisterVrEntryHook(final ChromeActivity activity) {
         // Daydream is not supported on pre-N devices.
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) return;
-        if (isVrEnabled() && !sChromeStarted) {
-            nativeOnChromeStarted();
-            sChromeStarted = true;
-        }
         if (sInstance != null) return; // Will be handled in onResume.
         if (!activitySupportsVrBrowsing(activity) && sRegisteredVrAssetsComponent) return;
 
@@ -1808,5 +1803,4 @@ public class VrShellDelegate
     private native boolean nativeIsClearActivatePending(long nativeVrShellDelegate);
     private native void nativeDestroy(long nativeVrShellDelegate);
     private static native void nativeRegisterVrAssetsComponent();
-    private static native void nativeOnChromeStarted();
 }
