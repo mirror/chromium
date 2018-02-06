d27c0efe7e27acdda3a762211004ea70f080ced0
Raymond Toy <rtoy@chromium.org>
2018-02-06 10:26:22 -0800

    Remove value setter deprecation messages
    Rmove the deprecation messages about the AudioParam value setter
    changing to be exactly equivalent to AudioParam.setValueAtTime()
    
    Bug: 764396
    Test: AudioParam/value-setter-warnings.html
    Change-Id: Ia6f15f2994f33ba742a5b63b57b9e8716539721f

--- third_party/WebKit/Source/modules/webaudio/AudioParam.cpp
+++ third_party/WebKit/Source/modules/webaudio/AudioParam.cpp
@@ -247,9 +247,6 @@ int AudioParamHandler::ComputeQHistogramValue(float new_value) const {
 
 // ----------------------------------------------------------------
 
-// TODO(crbug.com/764396): Remove this when fixed.
-bool AudioParam::s_value_setter_warning_done_ = false;
-
 AudioParam::AudioParam(BaseAudioContext& context,
                        AudioParamType param_type,
                        String param_name,
@@ -296,21 +293,19 @@ void AudioParam::WarnIfOutsideRange(const String& param_method, float value) {
 }
 
 void AudioParam::setValue(float value) {
-  // TODO(crbug.com/764396): Remove this when fixed.
-  WarnIfSetterOverlapsEvent();
-
   WarnIfOutsideRange("value", value);
   Handler().SetValue(value);
 }
 
-// TODO(crbug.com/764396): Remove this when fixed.
-void AudioParam::WarnIfSetterOverlapsEvent() {
-  DCHECK(IsMainThread());
+void AudioParam::setValue(float value, ExceptionState& exception_state) {
+  WarnIfOutsideRange("value", value);
 
-  // Check for overlap and print a warning only if we haven't already
-  // printed a warning.
-  Handler().Timeline().WarnIfSetterOverlapsEvent(
-      Context(), Handler().GetParamName(), !s_value_setter_warning_done_);
+  // This is to signal any errors, if necessary, about conflicting
+  // automations.
+  setValueAtTime(value, Context()->currentTime(), exception_state);
+  // This is to change the value so that an immediate query for the
+  // value returns the expected values.
+  Handler().SetValue(value);
 }
 
 float AudioParam::defaultValue() const {
