3bb1df276120468ba854fca9050e3cb51eb1f7e0
Egor Manzhula <dolbybeast@gmail.com>
2018-02-06 22:11:55 +0200

    Fix disable-web-security flag
    Fixed wrong conditions order, that leads to skip disable-web-security flag
    
    Bug: 575690
    Change-Id: I891cd1662a71a10b48a9020c783f9cd708851a38

--- content/browser/frame_host/navigation_controller_impl.cc
+++ content/browser/frame_host/navigation_controller_impl.cc
@@ -1647,8 +1647,8 @@ bool NavigationControllerImpl::IsURLSameDocumentNavigation(
                         // for now.
                         last_committed_url == url::kAboutBlankURL ||
                         last_committed_url.GetOrigin() == url.GetOrigin() ||
-                        committed_origin == origin ||
                         !prefs.web_security_enabled ||
+                        committed_origin == origin ||
                         (prefs.allow_universal_access_from_file_urls &&
                          committed_origin.scheme() == url::kFileScheme);
   if (!is_same_origin && renderer_says_same_document) {
