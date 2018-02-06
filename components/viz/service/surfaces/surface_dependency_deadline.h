e5dbbf436897ff0a07d00ce3381256a7a45d3d3e
Fady Samuel <fsamuel@chromium.org>
2018-02-06 16:20:58 -0500

    Surface synchronization: Measure deadlines in wall time instead of BeginFrames
    Queuing delay can cause BeginFrames to arrive late in SurfaceDependencyDeadline.
    
    Prior to this CL, SurfaceDependencyDeadline counted BeginFrames to determine
    whether a deadline was hit. The problem is each BeginFrame could be delayed and
    so 4+ BeginFrames could translate into hundreds of milliseconds even if the
    system is intended to run at 60fps (67ms for 4 BeginFrames).
    
    This CL converts SurfaceDependencyDeadline to measure wall time instead and uses
    BeginFrames as a signal to check how much time is left until the deadline.
    
    This CL mocks out base::TimeTicks::Now() through a base::TickClock object. This
    allows this code to be unit-testable.
    
    Bug: 808315, 672962
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I6f76d1f55ea9e7920d7872414618ec225c25d454

--- components/viz/service/surfaces/surface_dependency_deadline.h
+++ components/viz/service/surfaces/surface_dependency_deadline.h
@@ -9,28 +9,34 @@
 
 #include "components/viz/service/surfaces/surface_deadline_client.h"
 
+namespace base {
+class TickClock;
+}  // namespace base
+
 namespace viz {
 
 class SurfaceDependencyDeadline : public BeginFrameObserver {
  public:
   SurfaceDependencyDeadline(SurfaceDeadlineClient* client,
-                            BeginFrameSource* begin_frame_source);
+                            BeginFrameSource* begin_frame_source,
+                            base::TickClock* tick_clock);
   ~SurfaceDependencyDeadline() override;
 
-  void Set(uint32_t number_of_frames_to_deadline);
+  void Set(uint32_t number_of_frames_to_deadline,
+           base::TimeTicks frame_time,
+           base::TimeDelta frame_interval);
 
   // If a deadline had been set, then cancel the deadline and return the
   // the duration of the event tracked by this object. If there was no
   // deadline set, then return base::nullopt.
   base::Optional<base::TimeDelta> Cancel();
 
-  bool has_deadline() const {
-    return number_of_frames_to_deadline_.has_value();
-  }
+  bool has_deadline() const { return deadline_.has_value(); }
 
   // Takes on the same BeginFrameSource and deadline as |other|. Returns
   // false if they're already the same, and true otherwise.
-  bool InheritFrom(const SurfaceDependencyDeadline& other);
+  bool InheritFrom(const SurfaceDependencyDeadline& other,
+                   base::TimeTicks frame_time);
 
   bool operator==(const SurfaceDependencyDeadline& other);
   bool operator!=(const SurfaceDependencyDeadline& other) {
@@ -48,8 +54,9 @@ class SurfaceDependencyDeadline : public BeginFrameObserver {
 
   SurfaceDeadlineClient* const client_;
   BeginFrameSource* begin_frame_source_ = nullptr;
-  base::Optional<uint32_t> number_of_frames_to_deadline_;
+  base::TickClock* tick_clock_;
   base::TimeTicks start_time_;
+  base::Optional<base::TimeTicks> deadline_;
 
   BeginFrameArgs last_begin_frame_args_;
 
