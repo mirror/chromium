ae1f197ec1eb35b8e7ce54910bf9c9b7d5629087
Kent Tamura <tkent@chromium.org>
2018-02-06 09:27:30 +0900

    Rename CustomElement::CreateCustomElementSync to CreateCustomElement, and add async support to it.
    This CL simplifies the code, and has no behavior changes.
    
    Bug: 806641
    Change-Id: I46c7f8469f0ba231ea1179ac19de2afb0fbaa991

--- third_party/WebKit/Source/core/html/custom/CustomElement.cpp
+++ third_party/WebKit/Source/core/html/custom/CustomElement.cpp
@@ -92,25 +92,28 @@ static CustomElementDefinition* DefinitionFor(
 }
 
 // https://dom.spec.whatwg.org/#concept-create-element
-HTMLElement* CustomElement::CreateCustomElementSync(
+HTMLElement* CustomElement::CreateCustomElement(
     Document& document,
     const QualifiedName& tag_name,
+    const CreateElementFlags flags,
     CustomElementDefinition* definition) {
+  DCHECK(definition);
   DCHECK(ShouldCreateCustomElement(tag_name) ||
          ShouldCreateCustomizedBuiltinElement(tag_name))
       << tag_name;
+
+  if (flags.IsAsyncCustomElements())
+    return definition->CreateElementAsync(document, tag_name, flags);
+
   HTMLElement* element;
 
-  if (definition && definition->Descriptor().IsAutonomous()) {
+  if (definition->Descriptor().IsAutonomous()) {
     // 6. If definition is non-null and we have an autonomous custom element
     element = definition->CreateElementSync(document, tag_name);
-  } else if (definition) {
+  } else {
     // 5. If definition is non-null and we have a customized built-in element
     element = CreateUndefinedElement(document, tag_name);
     definition->Upgrade(element);
-  } else {
-    // 7. Otherwise
-    element = CreateUndefinedElement(document, tag_name);
   }
   return element;
 }
