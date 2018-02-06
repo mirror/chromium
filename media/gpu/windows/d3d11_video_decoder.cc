1c7df273110e1fd48c5065e8b99acf7aaa94b00f
liberato@chromium.org <liberato@chromium.org>
2018-02-06 15:07:43 -0800

    PostOnlyOwner for classes with thread affinity.
    PostOnlyOwner allows for easy creation, posting, and destruction of
    objects on a designated thread.  They do not expose the underlying
    pointer, so it's hard to accidentally pass them around in callbacks
    that are then run on the wrong thread.
    
    The PostOnlyOwner may be created anywhere, passed (moved) between
    threads, etc., safely.  Destruction of the owner object schedules
    destruction of the underlying object on the appropriate thread.
    
    Not implemented here (but prototyped elsewhere) are weak pointers
    to these objects that may be passed and used between threads safely;
    utility functions to generate thread-hopping callbacks; support for
    strong shared ownership semantics.
    
    Bug:
    Tests: post_only_owner_unittests.cc
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I3bbf7e975884fce8e18f3bb15a0d211ead58a619

--- media/gpu/windows/d3d11_video_decoder.cc
+++ media/gpu/windows/d3d11_video_decoder.cc
@@ -13,56 +13,16 @@
 #include "media/base/video_frame.h"
 #include "media/gpu/windows/d3d11_video_decoder_impl.h"
 
-namespace {
-
-// Check |weak_ptr| and run |cb| with |args| if it's non-null.
-template <typename T, typename... Args>
-void CallbackOnProperThread(base::WeakPtr<T> weak_ptr,
-                            base::Callback<void(Args...)> cb,
-                            Args... args) {
-  if (weak_ptr.get())
-    cb.Run(args...);
-}
-
-// Given a callback, |cb|, return another callback that will call |cb| after
-// switching to the thread that BindToCurrent.... is called on.  We will check
-// |weak_ptr| on the current thread.  This is different than just calling
-// BindToCurrentLoop because we'll check the weak ptr.  If |cb| is some method
-// of |T|, then one can use BindToCurrentLoop directly.  However, in our case,
-// we have some unrelated callback that we'd like to call only if we haven't
-// been destroyed yet.  I suppose this could also just be a method:
-// template<CB, ...> D3D11VideoDecoder::CallSomeCallback(CB, ...) that's bound
-// via BindToCurrentLoop directly.
-template <typename T, typename... Args>
-base::Callback<void(Args...)> BindToCurrentThreadIfWeakPtr(
-    base::WeakPtr<T> weak_ptr,
-    base::Callback<void(Args...)> cb) {
-  return media::BindToCurrentLoop(
-      base::Bind(&CallbackOnProperThread<T, Args...>, weak_ptr, cb));
-}
-
-}  // namespace
-
 namespace media {
 
 D3D11VideoDecoder::D3D11VideoDecoder(
     scoped_refptr<base::SingleThreadTaskRunner> gpu_task_runner,
     base::RepeatingCallback<gpu::CommandBufferStub*()> get_stub_cb)
-    : impl_task_runner_(std::move(gpu_task_runner)), weak_factory_(this) {
-  // We create |impl_| on the wrong thread, but we never use it here.
-  // Note that the output callback will hop to our thread, post the video
-  // frame, and along with a callback that will hop back to the impl thread
-  // when it's released.
-  impl_ = std::make_unique<D3D11VideoDecoderImpl>(get_stub_cb);
-  impl_weak_ = impl_->GetWeakPtr();
-}
+    : impl_(TaskRunnerBound<D3D11VideoDecoderImpl>::Create(
+          std::move(gpu_task_runner),
+          get_stub_cb)) {}
 
-D3D11VideoDecoder::~D3D11VideoDecoder() {
-  // Post destruction to the main thread.  When this executes, it will also
-  // cancel pending callbacks into |impl_| via |impl_weak_|.  Callbacks out
-  // from |impl_| will be cancelled by |weak_factory_| when we return.
-  impl_task_runner_->DeleteSoon(FROM_HERE, std::move(impl_));
-}
+D3D11VideoDecoder::~D3D11VideoDecoder() {}
 
 std::string D3D11VideoDecoder::GetDisplayName() const {
   return "D3D11VideoDecoder";
@@ -83,42 +43,31 @@ void D3D11VideoDecoder::Initialize(const VideoDecoderConfig& config,
   // Bind our own init / output cb that hop to this thread, so we don't call the
   // originals on some other thread.
   // TODO(liberato): what's the lifetime of |cdm_context|?
-  impl_task_runner_->PostTask(
-      FROM_HERE,
-      base::BindOnce(
-          &VideoDecoder::Initialize, impl_weak_, config, low_delay, cdm_context,
-          BindToCurrentThreadIfWeakPtr(weak_factory_.GetWeakPtr(), init_cb),
-          BindToCurrentThreadIfWeakPtr(weak_factory_.GetWeakPtr(), output_cb)));
+  impl_.Post(FROM_HERE, &VideoDecoder::Initialize, config, low_delay,
+             cdm_context, BindToCurrentLoop(init_cb),
+             BindToCurrentLoop(output_cb));
 }
 
 void D3D11VideoDecoder::Decode(const scoped_refptr<DecoderBuffer>& buffer,
                                const DecodeCB& decode_cb) {
-  impl_task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(&VideoDecoder::Decode, impl_weak_, buffer,
-                                BindToCurrentThreadIfWeakPtr(
-                                    weak_factory_.GetWeakPtr(), decode_cb)));
+  impl_.Post(FROM_HERE, &VideoDecoder::Decode, buffer,
+             BindToCurrentLoop(decode_cb));
 }
 
 void D3D11VideoDecoder::Reset(const base::Closure& closure) {
-  impl_task_runner_->PostTask(
-      FROM_HERE, base::BindOnce(&VideoDecoder::Reset, impl_weak_,
-                                BindToCurrentThreadIfWeakPtr(
-                                    weak_factory_.GetWeakPtr(), closure)));
+  impl_.Post(FROM_HERE, &VideoDecoder::Reset, BindToCurrentLoop(closure));
 }
 
 bool D3D11VideoDecoder::NeedsBitstreamConversion() const {
-  // Wrong thread, but it's okay.
-  return impl_->NeedsBitstreamConversion();
+  return true;
 }
 
 bool D3D11VideoDecoder::CanReadWithoutStalling() const {
-  // Wrong thread, but it's okay.
-  return impl_->CanReadWithoutStalling();
+  return false;
 }
 
 int D3D11VideoDecoder::GetMaxDecodeRequests() const {
-  // Wrong thread, but it's okay.
-  return impl_->GetMaxDecodeRequests();
+  return 4;
 }
 
 }  // namespace media
