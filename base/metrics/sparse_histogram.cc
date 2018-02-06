efabd749ab2f883723752faba135db576cda8f8a
François Degros <fdegros@chromium.org>
2018-02-06 11:44:03 +1100

    Nicer chrome://histograms page.
    Used HTML headers to structure the histograms page. Now, the histogram names
    clearly stand out.
    
    Used fixed column sizes when printing histograms. This simplifies the code and
    works beautifully. Everything is aligned and it is easier to read.
    
    Grouped columns with numbers on the left, and pushed histogram bars to the
    right.
    
    Added a column with cumulated percentage.
    
    Added column labels.
    
    Removed code duplication. Removed unused code in HistogramBase and subclasses.
    
    Change-Id: Iff3562f64b59a1d53ada3b2471960ff6ee2b4235
    Bug: 788270

--- base/metrics/sparse_histogram.cc
+++ base/metrics/sparse_histogram.cc
@@ -4,6 +4,7 @@
 
 #include "base/metrics/sparse_histogram.h"
 
+#include <algorithm>
 #include <utility>
 
 #include "base/memory/ptr_util.h"
@@ -155,16 +156,6 @@ bool SparseHistogram::AddSamplesFromPickle(PickleIterator* iter) {
   return unlogged_samples_->AddFromPickle(iter);
 }
 
-void SparseHistogram::WriteHTMLGraph(std::string* output) const {
-  output->append("<PRE>");
-  WriteAsciiImpl(true, "<br>", output);
-  output->append("</PRE>");
-}
-
-void SparseHistogram::WriteAscii(std::string* output) const {
-  WriteAsciiImpl(true, "\n", output);
-}
-
 void SparseHistogram::SerializeInfoImpl(Pickle* pickle) const {
   pickle->WriteString(histogram_name());
   pickle->WriteInt(flags());
@@ -217,67 +208,6 @@ void SparseHistogram::GetCountAndBucketData(Count* count,
                                             int64_t* sum,
                                             ListValue* buckets) const {
   // TODO(kaiwang): Implement. (See HistogramBase::WriteJSON.)
-}
-
-void SparseHistogram::WriteAsciiImpl(bool graph_it,
-                                     const std::string& newline,
-                                     std::string* output) const {
-  // Get a local copy of the data so we are consistent.
-  std::unique_ptr<HistogramSamples> snapshot = SnapshotSamples();
-  Count total_count = snapshot->TotalCount();
-  double scaled_total_count = total_count / 100.0;
-
-  WriteAsciiHeader(total_count, output);
-  output->append(newline);
-
-  // Determine how wide the largest bucket range is (how many digits to print),
-  // so that we'll be able to right-align starts for the graphical bars.
-  // Determine which bucket has the largest sample count so that we can
-  // normalize the graphical bar-width relative to that sample count.
-  Count largest_count = 0;
-  Sample largest_sample = 0;
-  std::unique_ptr<SampleCountIterator> it = snapshot->Iterator();
-  while (!it->Done()) {
-    Sample min;
-    int64_t max;
-    Count count;
-    it->Get(&min, &max, &count);
-    if (min > largest_sample)
-      largest_sample = min;
-    if (count > largest_count)
-      largest_count = count;
-    it->Next();
-  }
-  size_t print_width = GetSimpleAsciiBucketRange(largest_sample).size() + 1;
-
-  // iterate over each item and display them
-  it = snapshot->Iterator();
-  while (!it->Done()) {
-    Sample min;
-    int64_t max;
-    Count count;
-    it->Get(&min, &max, &count);
-
-    // value is min, so display it
-    std::string range = GetSimpleAsciiBucketRange(min);
-    output->append(range);
-    for (size_t j = 0; range.size() + j < print_width + 1; ++j)
-      output->push_back(' ');
-
-    if (graph_it)
-      WriteAsciiBucketGraph(count, largest_count, output);
-    WriteAsciiBucketValue(count, scaled_total_count, output);
-    output->append(newline);
-    it->Next();
-  }
-}
-
-void SparseHistogram::WriteAsciiHeader(const Count total_count,
-                                       std::string* output) const {
-  StringAppendF(output, "Histogram: %s recorded %d samples", histogram_name(),
-                total_count);
-  if (flags())
-    StringAppendF(output, " (flags = 0x%x)", flags());
 }
 
 }  // namespace base
