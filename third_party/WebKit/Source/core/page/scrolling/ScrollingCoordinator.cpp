eaed84a7bd6d87e643ec09c4f1bcbd20039f014b
W. James MacLean <wjmaclean@chromium.org>
2018-02-07 11:00:50 -0500

    Use LocalFrames in ScrollingCoordinator compositing update.
    This CL converts the remaining methods called from
    UpdateAfterCompositingChangeIfNeeded() to use a provided
    LocalFrame instead of relying on the page's main frame,
    which will often not be local when OOPIFs are in use.
    
    This removes the TODO associated with bug 680606.
    
    Bug: 803227, 680606
    Change-Id: I42e054e666c0701207a4eafc60cf00a1e6be12e2

--- third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
+++ third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
@@ -191,18 +191,13 @@ void ScrollingCoordinator::UpdateAfterCompositingChangeIfNeeded(
     frame_view->SetScrollGestureRegionIsDirty(false);
   }
 
-  // TODO(wjmaclean): Make the stuff below this point work for OOPIFs too.
-  // https://crbug.com/680606
-  if (frame != frame_view->GetPage()->MainFrame())
-    return;
-
   if (!(touch_event_target_rects_are_dirty_ ||
         should_scroll_on_main_thread_dirty_ || FrameScrollerIsDirty())) {
     return;
   }
 
   if (touch_event_target_rects_are_dirty_) {
-    UpdateTouchEventTargetRectsIfNeeded();
+    UpdateTouchEventTargetRectsIfNeeded(frame);
     touch_event_target_rects_are_dirty_ = false;
   }
 
@@ -709,7 +704,8 @@ static void ProjectRectsToGraphicsLayerSpace(
       layer_child_frame_map);
 }
 
-void ScrollingCoordinator::UpdateTouchEventTargetRectsIfNeeded() {
+void ScrollingCoordinator::UpdateTouchEventTargetRectsIfNeeded(
+    LocalFrame* frame) {
   TRACE_EVENT0("input",
                "ScrollingCoordinator::updateTouchEventTargetRectsIfNeeded");
 
@@ -718,8 +714,8 @@ void ScrollingCoordinator::UpdateTouchEventTargetRectsIfNeeded() {
     return;
 
   LayerHitTestRects touch_event_target_rects;
-  ComputeTouchEventTargetRects(touch_event_target_rects);
-  SetTouchEventTargetRects(touch_event_target_rects);
+  ComputeTouchEventTargetRects(frame, touch_event_target_rects);
+  SetTouchEventTargetRects(frame, touch_event_target_rects);
 }
 
 void ScrollingCoordinator::UpdateUserInputScrollable(
@@ -754,6 +750,7 @@ void ScrollingCoordinator::Reset() {
 // computeTouchEventTargetRects, for example during a non-composited scroll
 // (although that's not yet implemented - crbug.com/261307).
 void ScrollingCoordinator::SetTouchEventTargetRects(
+    LocalFrame* frame,
     LayerHitTestRects& layer_rects) {
   TRACE_EVENT0("input", "ScrollingCoordinator::setTouchEventTargetRects");
 
@@ -794,8 +791,7 @@ void ScrollingCoordinator::SetTouchEventTargetRects(
     }
   }
 
-  ProjectRectsToGraphicsLayerSpace(page_->DeprecatedLocalMainFrame(),
-                                   layer_rects, graphics_layer_rects);
+  ProjectRectsToGraphicsLayerSpace(frame, layer_rects, graphics_layer_rects);
 
   for (const auto& layer_rect : graphics_layer_rects) {
     const GraphicsLayer* graphics_layer = layer_rect.key;
@@ -856,6 +852,7 @@ void ScrollingCoordinator::WillDestroyLayer(PaintLayer* layer) {
 }
 
 void ScrollingCoordinator::SetShouldUpdateScrollLayerPositionOnMainThread(
+    LocalFrame* frame,
     MainThreadScrollingReasons main_thread_scrolling_reasons) {
   if (!page_->MainFrame()->IsLocalFrame() ||
       !page_->DeprecatedLocalMainFrame()->View())
@@ -864,10 +861,8 @@ void ScrollingCoordinator::SetShouldUpdateScrollLayerPositionOnMainThread(
   GraphicsLayer* visual_viewport_layer =
       page_->GetVisualViewport().ScrollLayer();
   WebLayer* visual_viewport_scroll_layer = toWebLayer(visual_viewport_layer);
-  GraphicsLayer* layer = page_->DeprecatedLocalMainFrame()
-                             ->View()
-                             ->LayoutViewportScrollableArea()
-                             ->LayerForScrolling();
+  GraphicsLayer* layer =
+      frame->View()->LayoutViewportScrollableArea()->LayerForScrolling();
   if (WebLayer* scroll_layer = toWebLayer(layer)) {
     last_main_thread_scrolling_reasons_ = main_thread_scrolling_reasons;
     if (main_thread_scrolling_reasons) {
@@ -1151,10 +1146,11 @@ static void AccumulateDocumentTouchEventTargetRects(
 }
 
 void ScrollingCoordinator::ComputeTouchEventTargetRects(
+    LocalFrame* frame,
     LayerHitTestRects& rects) {
   TRACE_EVENT0("input", "ScrollingCoordinator::computeTouchEventTargetRects");
 
-  Document* document = page_->DeprecatedLocalMainFrame()->GetDocument();
+  Document* document = frame->GetDocument();
   if (!document || !document->View() || !document->GetFrame())
     return;
 
--- third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.h
+++ third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.h
@@ -132,7 +132,7 @@ class CORE_EXPORT ScrollingCoordinator final
   Region ComputeShouldHandleScrollGestureOnMainThreadRegion(
       const LocalFrame*) const;
 
-  void UpdateTouchEventTargetRectsIfNeeded();
+  void UpdateTouchEventTargetRectsIfNeeded(LocalFrame*);
 
   void UpdateUserInputScrollable(ScrollableArea*);
 
@@ -165,12 +165,13 @@ class CORE_EXPORT ScrollingCoordinator final
 
  private:
   void SetShouldUpdateScrollLayerPositionOnMainThread(
+      LocalFrame*,
       MainThreadScrollingReasons);
 
   void SetShouldHandleScrollGestureOnMainThreadRegion(const Region&,
                                                       LocalFrameView*);
-  void SetTouchEventTargetRects(LayerHitTestRects&);
-  void ComputeTouchEventTargetRects(LayerHitTestRects&);
+  void SetTouchEventTargetRects(LocalFrame*, LayerHitTestRects&);
+  void ComputeTouchEventTargetRects(LocalFrame*, LayerHitTestRects&);
 
   WebScrollbarLayer* AddWebScrollbarLayer(ScrollableArea*,
                                           ScrollbarOrientation,
