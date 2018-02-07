3fc2b426e1caaf9528d2ad882e902b5168e063e6
Yoichi Osato <yoichio@chromium.org>
2018-02-07 15:31:04 +0900

    Renderedposition textarea
    Bug: 807930
    Change-Id: I46630d86401c165e0bb908d748adc069e03a5374

--- third_party/WebKit/Source/core/editing/RenderedPositionTest.cpp
+++ third_party/WebKit/Source/core/editing/RenderedPositionTest.cpp
@@ -6,7 +6,9 @@
 
 #include "build/build_config.h"
 #include "core/editing/FrameSelection.h"
+#include "core/editing/InlineBoxPosition.h"
 #include "core/editing/SelectionTemplate.h"
+#include "core/editing/VisiblePosition.h"
 #include "core/editing/testing/EditingTestBase.h"
 #include "core/frame/Settings.h"
 #include "core/html/forms/HTMLInputElement.h"
@@ -14,7 +16,31 @@
 
 namespace blink {
 
-class RenderedPositionTest : public EditingTestBase {};
+class RenderedPositionTest : public EditingTestBase {
+  void SetUp() override {
+    EditingTestBase::SetUp();
+    // Enable compositing.
+    GetPage().GetSettings().SetAcceleratedCompositingEnabled(true);
+    GetDocument().View()->SetParentVisible(true);
+    GetDocument().View()->SetSelfVisible(true);
+    GetDocument().View()->UpdateAllLifecyclePhases();
+
+    LoadAhem();
+  }
+
+ protected:
+  CompositedSelection ComputeCompositedSelection(Element* focus,
+                                                 const Node& select) {
+    DCHECK(focus);
+    focus->focus();
+    Selection().SetSelection(
+        SelectionInDOMTree::Builder().SelectAllChildren(select).Build(),
+        SetSelectionOptions::Builder().SetShouldShowHandle(true).Build());
+    UpdateAllLifecyclePhases();
+
+    return RenderedPosition::ComputeCompositedSelection(Selection());
+  }
+};
 
 #if defined(OS_ANDROID)
 #define MAYBE_ComputeCompositedSelection DISABLED_ComputeCompositedSelection
@@ -22,30 +48,48 @@ class RenderedPositionTest : public EditingTestBase {};
 #define MAYBE_ComputeCompositedSelection ComputeCompositedSelection
 #endif
 TEST_F(RenderedPositionTest, MAYBE_ComputeCompositedSelection) {
-  // Enable compositing.
-  GetPage().GetSettings().SetAcceleratedCompositingEnabled(true);
-  GetDocument().View()->SetParentVisible(true);
-  GetDocument().View()->SetSelfVisible(true);
-  GetDocument().View()->UpdateAllLifecyclePhases();
-
   SetBodyContent(
       "<input id=target width=20 value='test test test test test tes tes test'"
       "style='width: 100px; height: 20px;'>");
   HTMLInputElement* target =
       ToHTMLInputElement(GetDocument().getElementById("target"));
-  DCHECK(target);
-  target->focus();
-  Selection().SetSelection(
-      SelectionInDOMTree::Builder()
-          .SelectAllChildren(*target->InnerEditorElement())
-          .Build(),
-      SetSelectionOptions::Builder().SetShouldShowHandle(true).Build());
-  UpdateAllLifecyclePhases();
-
   const CompositedSelection& composited_selection =
-      RenderedPosition::ComputeCompositedSelection(Selection());
+      ComputeCompositedSelection(target, *target->InnerEditorElement());
   EXPECT_FALSE(composited_selection.start.hidden);
   EXPECT_TRUE(composited_selection.end.hidden);
+}
+
+// crbug.com/807930
+TEST_F(RenderedPositionTest, ContentEditableLinebreak) {
+  SetBodyContent("<div contenteditable>test<br><br></div>");
+  Element* target = GetDocument().QuerySelector("div");
+  const CompositedSelection& composited_selection =
+      ComputeCompositedSelection(target, *target);
+  EXPECT_EQ(composited_selection.start.edge_top_in_layer,
+            FloatPoint(8.0f, 8.0f));
+  EXPECT_EQ(composited_selection.start.edge_bottom_in_layer,
+            FloatPoint(8.0f, 9.0f));
+  EXPECT_EQ(composited_selection.end.edge_top_in_layer, FloatPoint(8.0f, 9.0f));
+  EXPECT_EQ(composited_selection.end.edge_bottom_in_layer,
+            FloatPoint(8.0f, 10.0f));
+}
+
+// crbug.com/807930
+TEST_F(RenderedPositionTest, TextAreaLinebreak) {
+  SetBodyContent("<textarea id=target >test\n</textarea>");
+  TextControlElement* target =
+      ToTextControlElement(GetDocument().getElementById("target"));
+  const CompositedSelection& composited_selection =
+      ComputeCompositedSelection(target, *target->InnerEditorElement());
+
+  EXPECT_EQ(composited_selection.start.edge_top_in_layer,
+            FloatPoint(11.0f, 11.0f));
+  EXPECT_EQ(composited_selection.start.edge_bottom_in_layer,
+            FloatPoint(11.0f, 27.0f));
+  EXPECT_EQ(composited_selection.end.edge_top_in_layer,
+            FloatPoint(11.0f, 27.0f));
+  EXPECT_EQ(composited_selection.end.edge_bottom_in_layer,
+            FloatPoint(11.0f, 43.0f));
 }
 
 }  // namespace blink
