fc91e10d60c105c8a627485f53fa1c781f2dd9e1
Marijn Kruisselbrink <mek@chromium.org>
2018-02-07 11:32:16 -0800

    [Mojo Blob URLs] Refactor Blob URL loading for subresources.
    This changes the XHR and fetch codepaths to resolve blob URLs earlier
    (i.e when the request is created rather than when the fetch is made), in
    order to fix bugs where the blob URL is revoked after creating a request
    but before fetching.
    
    This also addresses race conditions between revoking blob URLs and
    fetching them by resolving a blob URL as soon as possible for subresource
    requests.
    
    Not addressed yet are navigation and download requests, that will be
    done in follow ups.
    
    Bug: 800901, 800898, 695031, 807435
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_mojo
    Change-Id: Ifd5eb11abaf04c091c51e607fdaeafa0c89bc1a9

--- third_party/WebKit/Source/platform/loader/fetch/FetchContext.h
+++ third_party/WebKit/Source/platform/loader/fetch/FetchContext.h
@@ -226,9 +226,12 @@ class PLATFORM_EXPORT FetchContext
     return platform_probe_sink_;
   }
 
+  // If the passed in URLLoaderFactoryPtr is not null, it should be used to
+  // create the URLLoader for blob URLs.
   virtual std::unique_ptr<WebURLLoader> CreateURLLoader(
       const ResourceRequest&,
-      scoped_refptr<base::SingleThreadTaskRunner>) {
+      scoped_refptr<base::SingleThreadTaskRunner>,
+      network::mojom::blink::URLLoaderFactoryPtr blob_url_loader_factory) {
     NOTREACHED();
     return nullptr;
   }
