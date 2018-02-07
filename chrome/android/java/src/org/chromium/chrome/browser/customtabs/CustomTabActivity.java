bc60f70944e5f5a2bb165cec05b1a11c443aebd6
Bernhard Bauer <bauerb@chromium.org>
2018-02-07 15:55:15 +0000

    Send page title along with URL for custom tab menu items and buttons.
    Bug: 807290
    Change-Id: I9b1ba96d51ce85ee8a467e0ce25760c99ab32fe2

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -958,8 +958,9 @@ public class CustomTabActivity extends ChromeActivity {
             getToolbarManager().addCustomActionButton(
                     params.getIcon(getResources()), params.getDescription(), v -> {
                         if (getActivityTab() == null) return;
-                        mIntentDataProvider.sendButtonPendingIntentWithUrl(
-                                getApplicationContext(), params, getActivityTab().getUrl());
+                        mIntentDataProvider.sendButtonPendingIntentWithUrlAndTitle(
+                                getApplicationContext(), params, getActivityTab().getUrl(),
+                                getActivityTab().getTitle());
                         RecordUserAction.record("CustomTabsCustomActionButtonClick");
                         if (mIntentDataProvider.shouldEnableEmbeddedMediaExperience()
                                 && TextUtils.equals(
@@ -986,8 +987,8 @@ public class CustomTabActivity extends ChromeActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int menuIndex = getAppMenuPropertiesDelegate().getIndexOfMenuItem(item);
         if (menuIndex >= 0) {
-            mIntentDataProvider.clickMenuItemWithUrl(this, menuIndex,
-                    getTabModelSelector().getCurrentTab().getUrl());
+            mIntentDataProvider.clickMenuItemWithUrlAndTitle(
+                    this, menuIndex, getActivityTab().getUrl(), getActivityTab().getTitle());
             RecordUserAction.record("CustomTabsMenuCustomMenuItem");
             return true;
         }
