779c368451f925ac321d76bca8abbe1dda29d8c3
Peng Huang <penghuang@chromium.org>
2018-02-07 11:16:38 -0500

    WIP
    Very hack
    
    
    WP
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: If2842a184754884a429be168a83790d51651ccbe

--- cc/trees/layer_tree_frame_sink.cc
+++ cc/trees/layer_tree_frame_sink.cc
@@ -17,6 +17,8 @@
 #include "gpu/command_buffer/client/context_support.h"
 #include "gpu/command_buffer/client/raster_interface.h"
 
+#include "base/debug/stack_trace.h"
+
 namespace cc {
 
 class LayerTreeFrameSink::ContextLostForwarder
@@ -52,15 +54,22 @@ LayerTreeFrameSink::LayerTreeFrameSink(
       shared_bitmap_manager_(shared_bitmap_manager),
       weak_ptr_factory_(this) {
   DETACH_FROM_THREAD(thread_checker_);
+  if (!shared_bitmap_manager_)
+    base::debug::StackTrace().Print();
 }
 
 LayerTreeFrameSink::LayerTreeFrameSink(
-    scoped_refptr<viz::VulkanContextProvider> vulkan_context_provider)
+    scoped_refptr<viz::VulkanContextProvider> vulkan_context_provider,
+    gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager,
+    viz::SharedBitmapManager* shared_bitmap_manager)
     : vulkan_context_provider_(vulkan_context_provider),
-      gpu_memory_buffer_manager_(nullptr),
-      shared_bitmap_manager_(nullptr),
+      gpu_memory_buffer_manager_(gpu_memory_buffer_manager),
+      shared_bitmap_manager_(shared_bitmap_manager),
       weak_ptr_factory_(this) {
   DETACH_FROM_THREAD(thread_checker_);
+  LOG(ERROR) << "======";
+  if (!shared_bitmap_manager_)
+    base::debug::StackTrace().Print();
 }
 
 LayerTreeFrameSink::~LayerTreeFrameSink() {
