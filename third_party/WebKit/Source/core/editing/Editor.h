7ab5785bb3a1b72c50db3ad5f016c94b60725929
Zhuoyu Qian <zhuoyu.qian@samsung.com>
2018-02-07 10:29:41 +0800

    Move Paste() to EditorCommand.cpp
    Editor::Paste() is called by ExecutePasteGlobalSelection() and
    ExecutePaste() in EditorCommand.cpp. Move it with related functions to
    make |Editor| class simpler for improving code health.
    
    BUG=672405
    
    Signed-off-by: Zhuoyu Qian <zhuoyu.qian@samsung.com>
    Change-Id: I0aa8dcfd46abc01fee409909b5ebc3a25899768b

--- third_party/WebKit/Source/core/editing/Editor.h
+++ third_party/WebKit/Source/core/editing/Editor.h
@@ -51,7 +51,6 @@ class FrameSelection;
 class LocalFrame;
 class HitTestResult;
 class KillRing;
-class Pasteboard;
 class SetSelectionOptions;
 class SpellChecker;
 class CSSPropertyValueSet;
@@ -94,8 +93,6 @@ class CORE_EXPORT Editor final : public GarbageCollectedFinalized<Editor> {
 
   void Cut(EditorCommandSource);
   void Copy(EditorCommandSource);
-  void Paste(EditorCommandSource);
-  void PasteAsPlainText(EditorCommandSource);
 
   static void CountEvent(ExecutionContext*, const Event*);
   void CopyImage(const HitTestResult&);
@@ -316,6 +313,11 @@ class CORE_EXPORT Editor final : public GarbageCollectedFinalized<Editor> {
 
   void Trace(blink::Visitor*);
 
+  bool DispatchClipboardEvent(const AtomicString&,
+                              DataTransferAccessPolicy,
+                              EditorCommandSource,
+                              PasteMode = kAllMimeTypes);
+
  private:
   Member<LocalFrame> frame_;
   Member<CompositeEditCommand> last_edit_command_;
@@ -343,15 +345,7 @@ class CORE_EXPORT Editor final : public GarbageCollectedFinalized<Editor> {
   // Returns true if Editor should continue with default processing.
   bool DispatchCopyEvent(EditorCommandSource);
   bool DispatchCutEvent(EditorCommandSource);
-  bool DispatchPasteEvent(PasteMode, EditorCommandSource);
-  bool DispatchClipboardEvent(const AtomicString&,
-                              DataTransferAccessPolicy,
-                              EditorCommandSource,
-                              PasteMode = kAllMimeTypes);
 
-  bool CanSmartReplaceWithPasteboard(Pasteboard*);
-  void PasteAsPlainTextWithPasteboard(Pasteboard*, EditorCommandSource);
-  void PasteWithPasteboard(Pasteboard*, EditorCommandSource);
   void WriteSelectionToPasteboard();
 
   void RevealSelectionAfterEditingOperation(
