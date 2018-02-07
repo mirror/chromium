69416f345bf7928052798bcb4ca14c31ea457f28
Trevor Dixon <trevordixon@google.com>
2018-02-07 15:55:44 +0100

    Beginnings of dynamically-loaded components for Android. Proof of concept component and component host API.
    Bug: 757561
    Change-Id: Ie4d34c5bb481ea565b3d6236a7f05821b05ff338

---
+++ components/android_plugins/example_plugin_a/plugin/java/src/org/chromium/example_plugin_a/ExamplePluginA.java
@@ -0,0 +1,24 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.example_plugin_a;
+
+import org.chromium.android_plugins.PluginAPI;
+
+/** An example Android plugin. */
+public class ExamplePluginA implements ExamplePluginAInterface {
+    @Override
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public String hello() {
+        return notPartOfApi();
+    }
+
+    public String notPartOfApi() {
+        return PluginAPI.packageLabel();
+    }
+}
---
+++ components/android_plugins/java/src/org/chromium/android_plugins/PluginAPI.java
@@ -0,0 +1,17 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_plugins;
+
+import org.chromium.base.BuildInfo;
+
+/** @since 1.0 */
+public class PluginAPI {
+    public static String packageLabel() {
+        return BuildInfo.getPackageLabel();
+    }
+    public static String wow() {
+        return "wow";
+    }
+}
