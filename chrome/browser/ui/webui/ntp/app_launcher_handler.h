60bf81660ca73d53d3ed1d4268138330d0ecd915
Alan Cutter <alancutter@chromium.org>
2018-02-07 18:23:02 +1100

    Replace "App info" with "Site settings" for bookmark apps in chrome:apps
    Bookmark apps don't get special Chrome app permissions and it doesn't make
    sense to display the permissions allocated to them when it will always be
    none.
    This replaces the "App info" context menu entry with "Site settings" for
    the bookmark app's URL.
    
    Bug: 800205
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I4d4d203aa0cbb8611824ee4090eb7542fdbacc52

--- chrome/browser/ui/webui/ntp/app_launcher_handler.h
+++ chrome/browser/ui/webui/ntp/app_launcher_handler.h
@@ -106,6 +106,9 @@ class AppLauncherHandler
   // false.
   void HandleUninstallApp(const base::ListValue* args);
 
+  // Handles the "siteSettings" message with |args| containing [extension_id].
+  void HandleSiteSettings(const base::ListValue* args);
+
   // Handles the "createAppShortcut" message with |args| containing
   // [extension_id].
   void HandleCreateAppShortcut(const base::ListValue* args);
