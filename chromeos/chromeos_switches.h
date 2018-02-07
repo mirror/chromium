fb931bf663c43e60afdf70c14418e4dd806e5946
Sammie Quon <sammiequon@google.com>
2018-02-07 14:44:35 -0800

    Remove kShowNonMdLogin switch.
    A couple resource files in c/b/r/chromeos/login/ are no longer referenced.
    Delete those files. The files all have md_ versions which is now the ui
    of choice.
    
    Test: trybots
    Change-Id: I8f956cb00ba2dcbe7f581f96115bef9a601e9fac

--- chromeos/chromeos_switches.h
+++ chromeos/chromeos_switches.h
@@ -148,7 +148,6 @@ CHROMEOS_EXPORT extern const char kOobeTimerInterval[];
 CHROMEOS_EXPORT extern const char kProfileRequiresPolicy[];
 CHROMEOS_EXPORT extern const char kShillStub[];
 CHROMEOS_EXPORT extern const char kShowLoginDevOverlay[];
-CHROMEOS_EXPORT extern const char kShowNonMdLogin[];
 CHROMEOS_EXPORT extern const char kSmsTestMessages[];
 CHROMEOS_EXPORT extern const char kStubCrosSettings[];
 CHROMEOS_EXPORT extern const char kSystemDevMode[];
