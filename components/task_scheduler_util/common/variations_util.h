b452826f79d2ae5cfe0a8d2c2227d6ed406ff330
Francois Doray <fdoray@chromium.org>
2018-02-07 13:03:12 -0500

    Do not pass TaskScheduler field trial params on the command line.
    Field trials params are now automatically passed to child processes
    (https://cs.chromium.org/chromium/src/content/app/content_main_runner.cc?l=160&rcl=9030625190ec0a2b60be2af279bcb234f47d3f51)
    
    Bug: 810049
    Change-Id: Ib3bc65e2eaa990930271b4a91c8c4672fe0f54c5

--- components/task_scheduler_util/common/variations_util.h
+++ components/task_scheduler_util/common/variations_util.h
@@ -5,39 +5,18 @@
 #ifndef COMPONENTS_TASK_SCHEDULER_UTIL_COMMON_VARIATIONS_UTIL_H_
 #define COMPONENTS_TASK_SCHEDULER_UTIL_COMMON_VARIATIONS_UTIL_H_
 
-#include <map>
 #include <memory>
-#include <string>
 
 #include "base/strings/string_piece.h"
 #include "base/task_scheduler/task_scheduler.h"
-#include "build/build_config.h"
-
-namespace base {
-class CommandLine;
-}
 
 namespace task_scheduler_util {
 
-// Builds a TaskScheduler::InitParams from pool descriptors in
-// |variations_params| that are prefixed with |variation_param_prefix|. Returns
+// Builds a TaskScheduler::InitParams from variations params that are prefixed
+// with |variation_param_prefix| in the BrowserScheduler field trial. Returns
 // nullptr on failure.
 std::unique_ptr<base::TaskScheduler::InitParams> GetTaskSchedulerInitParams(
-    base::StringPiece variation_param_prefix,
-    const std::map<std::string, std::string>& variation_params);
-
-#if !defined(OS_IOS)
-// Serializes variation params from the BrowserScheduler field trial whose key
-// start with |prefix| to the --task-scheduler-variation-params switch of
-// |command_line|.
-void AddVariationParamsToCommandLine(base::StringPiece key_prefix,
-                                     base::CommandLine* command_line);
-
-// Returns a map of key-value pairs deserialized from the
-// --task-scheduler-variation-params switch of |command_line|.
-std::map<std::string, std::string> GetVariationParamsFromCommandLine(
-    const base::CommandLine& command_line);
-#endif  // !defined(OS_IOS)
+    base::StringPiece variation_param_prefix);
 
 }  // namespace task_scheduler_util
 
