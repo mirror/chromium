23e1eef6a52f433cb6b4f49c7ab1ab17a4569e3a
Xianzhu Wang <wangxianzhu@chromium.org>
2018-02-07 13:19:30 -0800

    Improve checking of exception during handling java exception
    We may encounter OOM java exception during handling a java exception,
    especially when the java exception itself is an OOM exception.
    
    Now,
    
    - On OutOfMemoryError in JNI code except for exception handling code,
      the process will die directly, so that the crash report will have
      magic signature indicating the JNI function getting OOM.
    
    - CheckException will handle any exception raised during exception
      handling.
    
    Bug: 808342
    Change-Id: Idd7ef29aada297ed88f844d7cd9b450d2c161ba7

--- base/android/jni_android.cc
+++ base/android/jni_android.cc
@@ -12,6 +12,7 @@
 #include "base/android/build_info.h"
 #include "base/android/jni_string.h"
 #include "base/android/jni_utils.h"
+#include "base/auto_reset.h"
 #include "base/debug/debugging_flags.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
@@ -31,6 +32,8 @@ jmethodID g_class_loader_load_class_method_id = 0;
 base::LazyInstance<base::ThreadLocalPointer<void>>::Leaky
     g_stack_frame_pointer = LAZY_INSTANCE_INITIALIZER;
 #endif
+
+bool g_in_get_java_exception_info = false;
 
 }  // namespace
 
@@ -235,7 +238,10 @@ void CheckException(JNIEnv* env) {
 
   // Exception has been found, might as well tell breakpad about it.
   jthrowable java_throwable = env->ExceptionOccurred();
-  if (java_throwable) {
+  if (java_throwable &&
+      // Should die directly for OOM.
+      !env->IsInstanceOf(java_throwable,
+                         env->FindClass("java/lang/OutOfMemoryError"))) {
     // Clear the pending exception, since a local reference is now held.
     env->ExceptionDescribe();
     env->ExceptionClear();
@@ -251,6 +257,12 @@ void CheckException(JNIEnv* env) {
 }
 
 std::string GetJavaExceptionInfo(JNIEnv* env, jthrowable java_throwable) {
+  if (g_in_get_java_exception_info) {
+    return "Java OOM'ed in exception handling, check logcat";
+  }
+
+  AutoReset<bool> reset(&g_in_get_java_exception_info, true);
+
   ScopedJavaLocalRef<jclass> throwable_clazz =
       GetClass(env, "java/lang/Throwable");
   jmethodID throwable_printstacktrace =
@@ -271,6 +283,7 @@ std::string GetJavaExceptionInfo(JNIEnv* env, jthrowable java_throwable) {
   ScopedJavaLocalRef<jobject> bytearray_output_stream(env,
       env->NewObject(bytearray_output_stream_clazz.obj(),
                      bytearray_output_stream_constructor));
+  CheckException(env);
 
   // Create an instance of PrintStream.
   ScopedJavaLocalRef<jclass> printstream_clazz =
@@ -282,19 +295,19 @@ std::string GetJavaExceptionInfo(JNIEnv* env, jthrowable java_throwable) {
   ScopedJavaLocalRef<jobject> printstream(env,
       env->NewObject(printstream_clazz.obj(), printstream_constructor,
                      bytearray_output_stream.obj()));
+  CheckException(env);
 
   // Call Throwable.printStackTrace(PrintStream)
   env->CallVoidMethod(java_throwable, throwable_printstacktrace,
       printstream.obj());
+  CheckException(env);
 
   // Call ByteArrayOutputStream.toString()
   ScopedJavaLocalRef<jstring> exception_string(
       env, static_cast<jstring>(
           env->CallObjectMethod(bytearray_output_stream.obj(),
                                 bytearray_output_stream_tostring)));
-  if (ClearException(env)) {
-    return "Java OOM'd in exception handling, check logcat";
-  }
+  CheckException(env);
 
   return ConvertJavaStringToUTF8(exception_string);
 }
