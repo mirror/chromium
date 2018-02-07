5f86887615319bc263d94c6fcea9fa758b4946c2
Chris Blume <cblume@chromium.org>
2018-02-07 00:33:47 -0800

    Use new child surface allocation for resize case
    This patch introduces a child allocator for surface IDs.
    This allows a renderer to create a surface as it needs (such as when
    creating the Cast dialog), as opposed to request a surface from the
    parent.
    
    BUG=789259
    
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I36acec47f083dffd029aba56431eda720579e63e

--- content/browser/renderer_host/render_widget_host_view_base.h
+++ content/browser/renderer_host/render_widget_host_view_base.h
@@ -21,6 +21,7 @@
 #include "components/viz/common/quads/compositor_frame.h"
 #include "components/viz/common/surfaces/surface_id.h"
 #include "content/browser/renderer_host/event_with_latency_info.h"
+#include "content/browser/renderer_host/scoped_allocation_pending_lock.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/common/input_event_ack_state.h"
@@ -169,8 +170,17 @@ class CONTENT_EXPORT RenderWidgetHostViewBase : public RenderWidgetHostView,
 
   // Informs the view that the renderer has resized to |new_size| because auto-
   // resize is enabled.
-  virtual void ResizeDueToAutoResize(const gfx::Size& new_size,
-                                     uint64_t sequence_number);
+  virtual void ResizeDueToAutoResize(
+      const gfx::Size& new_size,
+      uint64_t sequence_number,
+      const viz::LocalSurfaceId& local_surface_id);
+
+  virtual bool IsAllocationPending() const;
+  virtual void SetAllocationPendingFlag() const;
+  virtual void ClearAllocationPendingFlag() const;
+  ScopedAllocationPendingLock GetAllocationPendingLock() {
+    return ScopedAllocationPendingLock(this);
+  }
 
   base::WeakPtr<RenderWidgetHostViewBase> GetWeakPtr();
 
--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -2865,7 +2865,8 @@ void WebContentsImpl::UpdatePreferredSize(const gfx::Size& pref_size) {
 void WebContentsImpl::ResizeDueToAutoResize(
     RenderWidgetHostImpl* render_widget_host,
     const gfx::Size& new_size,
-    uint64_t sequence_number) {
+    uint64_t sequence_number,
+    const viz::LocalSurfaceId& local_surface_id) {
   if (render_widget_host != GetRenderViewHost()->GetWidget())
     return;
 
@@ -2883,13 +2884,20 @@ void WebContentsImpl::ResizeDueToAutoResize(
     }
   }
 
-  if (delegate_)
-    delegate_->ResizeDueToAutoResize(this, new_size);
+  {
+    base::Optional<content::ScopedAllocationPendingLock>
+        allocation_pending_lock;
+    RenderWidgetHostViewBase* view =
+        static_cast<RenderWidgetHostViewBase*>(GetRenderWidgetHostView());
 
-  RenderWidgetHostViewBase* view =
-      static_cast<RenderWidgetHostViewBase*>(GetRenderWidgetHostView());
-  if (view)
-    view->ResizeDueToAutoResize(new_size, sequence_number);
+    if (delegate_)
+      delegate_->ResizeDueToAutoResize(this, new_size);
+
+    if (view) {
+      view->ResizeDueToAutoResize(new_size, sequence_number, local_surface_id);
+      allocation_pending_lock = view->GetAllocationPendingLock();
+    }
+  }
 }
 
 gfx::Size WebContentsImpl::GetAutoResizeSize() {
