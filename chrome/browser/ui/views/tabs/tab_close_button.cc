e4247109cd9f9be34e6a2d4d245c40445f355918
Malay Keshav <malaykeshav@chromium.org>
2018-02-07 12:42:11 -0800

    Implements Touch Optimized design for tab and tabstrip
    This patch adds multiple layout changes to the tab and tabstrip under
    the new touch optimized UI standard.
    
    These changes include:
     - Increasing the tab and tabstrip height.
     - Adjusting tab content layout including the title, close tab button
       and the alert indicator.
     - Decreasing the tab touch width and increasing the tab standard width.
     - Adds the material design 2 tab close button icon.
     - Adds the basic color constants required for google material design 2.
       These colors are also added to the tab, tab strip and their contents.
     - Adds the tab alert indicator icons and their colors.
    
    What this patch does not modify in tab and tabstrp?
     - Separator colors.
     - Typography.
     - New tab button.
    
    The most recent screenshots for the change are posted on the bug below.
    
    Bug:805762
    Change-Id: Id7c1fc92f0a78a3429ff695b96f122374cbb1202
    Component: Tabs, tabstrip, material design controller, layout constants

--- chrome/browser/ui/views/tabs/tab_close_button.cc
+++ chrome/browser/ui/views/tabs/tab_close_button.cc
@@ -4,17 +4,42 @@
 
 #include "chrome/browser/ui/views/tabs/tab_close_button.h"
 
+#include <map>
 #include <memory>
+#include <vector>
 
+#include "base/hash.h"
+#include "base/no_destructor.h"
+#include "base/stl_util.h"
 #include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/ui/layout_constants.h"
+#include "chrome/browser/ui/views/tabs/tab.h"
+#include "chrome/browser/ui/views/tabs/tab_controller.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/material_design/material_design_controller.h"
+#include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/views/rect_based_targeting_utils.h"
 
 #if defined(USE_AURA)
 #include "ui/aura/env.h"
 #endif
+
+namespace {
+
+const gfx::VectorIcon& GetVectorIcon(views::Button::ButtonState state,
+                                     bool is_incognito) {
+  if (!is_incognito)
+    return kTabCloseButtonTouchIcon;
+
+  if (state == views::Button::STATE_NORMAL)
+    return kTabCloseButtonTouchIncognitoIcon;
+
+  return kTabCloseButtonTouchIncognitoHoveredPressedIcon;
+}
+
+}  // namespace
 
 TabCloseButton::TabCloseButton(views::ButtonListener* listener,
                                MouseEventCallback mouse_event_callback)
@@ -23,15 +48,17 @@ TabCloseButton::TabCloseButton(views::ButtonListener* listener,
   SetEventTargeter(std::make_unique<views::ViewTargeter>(this));
   SetAccessibleName(l10n_util::GetStringUTF16(IDS_ACCNAME_CLOSE));
 
-  // The normal image is set by OnButtonColorMaybeChanged() because it depends
-  // on the current theme and active state.  The hovered and pressed images
-  // don't depend on the these, so we can set them here.
-  const gfx::ImageSkia& hovered = gfx::CreateVectorIcon(
-      kTabCloseHoveredPressedIcon, SkColorSetRGB(0xDB, 0x44, 0x37));
-  const gfx::ImageSkia& pressed = gfx::CreateVectorIcon(
-      kTabCloseHoveredPressedIcon, SkColorSetRGB(0xA8, 0x35, 0x2A));
-  SetImage(views::Button::STATE_HOVERED, &hovered);
-  SetImage(views::Button::STATE_PRESSED, &pressed);
+  if (!ui::MaterialDesignController::IsTouchOptimizedUiEnabled()) {
+    // The normal image is set by OnButtonColorMaybeChanged() because it depends
+    // on the current theme and active state.  The hovered and pressed images
+    // don't depend on the these, so we can set them here.
+    const gfx::ImageSkia& hovered = gfx::CreateVectorIcon(
+        kTabCloseHoveredPressedIcon, SkColorSetRGB(0xDB, 0x44, 0x37));
+    const gfx::ImageSkia& pressed = gfx::CreateVectorIcon(
+        kTabCloseHoveredPressedIcon, SkColorSetRGB(0xA8, 0x35, 0x2A));
+    SetImage(views::Button::STATE_HOVERED, &hovered);
+    SetImage(views::Button::STATE_PRESSED, &pressed);
+  }
 
   // Disable animation so that the red danger sign shows up immediately
   // to help avoid mis-clicks.
@@ -44,6 +71,19 @@ void TabCloseButton::SetTabColor(SkColor color) {
   const gfx::ImageSkia& close_button_normal_image =
       gfx::CreateVectorIcon(kTabCloseNormalIcon, color);
   SetImage(views::Button::STATE_NORMAL, &close_button_normal_image);
+}
+
+void TabCloseButton::ActiveStateChanged(const Tab* parent_tab) {
+  const std::vector<views::Button::ButtonState> states = {
+      views::Button::STATE_NORMAL, views::Button::STATE_HOVERED,
+      views::Button::STATE_PRESSED};
+  const bool is_incognito = parent_tab->controller()->IsIncognito();
+
+  for (const auto& state : states) {
+    SetImage(state,
+             gfx::CreateVectorIcon(GetVectorIcon(state, is_incognito),
+                                   parent_tab->GetCloseTabButtonColor(state)));
+  }
 }
 
 views::View* TabCloseButton::GetTooltipHandlerForPoint(
