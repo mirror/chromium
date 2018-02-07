e866fe2b1398de8cf773066bf74a32f2894418ec
Lei Tian <ltian@chromium.org>
2018-02-07 11:57:55 -0800

    Retrieve images of Browser Actions through BrowserServiceFileProvider
    This CL syncs the implemntation of sending images from Browser Actions
    through FileProvider. It also implement to use the uris from the provider
    to access the images and populate the ui.
    
    Bug: 775798
    Change-Id: I17bee57063c6db1d9387b156f7e6341eae5ba15b

--- chrome/android/java/src/org/chromium/chrome/browser/browseractions/BrowserActionsCustomContextMenuItem.java
+++ chrome/android/java/src/org/chromium/chrome/browser/browseractions/BrowserActionsCustomContextMenuItem.java
@@ -5,10 +5,13 @@
 package org.chromium.chrome.browser.browseractions;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.support.annotation.IdRes;
 import android.support.customtabs.browseractions.BrowserActionItem;
 
+import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.browser.contextmenu.ContextMenuItem;
 
 /**
@@ -18,18 +21,31 @@ public class BrowserActionsCustomContextMenuItem implements ContextMenuItem {
     @IdRes
     private final int mMenuId;
     private final String mTitle;
-    private final Drawable mIcon;
+    private final Uri mIconUri;
+    private Drawable mIcon;
+    private IconListener mListener;
+
+    /**
+     * Interface used to test when {@link Bitmap} from the uri is available.
+     */
+    @VisibleForTesting
+    interface IconListener {
+        void onIconPreLoad();
+        void onIconShown(Bitmap bm);
+    }
 
     /**
      * Constructor to build a custom context menu item from {@link BrowserActionItem}.
      * @param id The {@link IdRes} of the custom context menu item.
      * @param title The title of the custom context menu item.
      * @param icon The icon of the custom context menu item.
+     * @param iconUri The {@link Uri} used to access the icon of the custom context menu item.
      */
-    BrowserActionsCustomContextMenuItem(@IdRes int id, String title, Drawable icon) {
+    BrowserActionsCustomContextMenuItem(@IdRes int id, String title, Drawable icon, Uri iconUri) {
         mMenuId = id;
         mTitle = title;
         mIcon = icon;
+        mIconUri = iconUri;
     }
 
     @Override
@@ -45,5 +61,36 @@ public class BrowserActionsCustomContextMenuItem implements ContextMenuItem {
     @Override
     public Drawable getDrawable(Context context) {
         return mIcon;
+    }
+
+    /**
+     * Set the drawable icon of custom context menu item.
+     */
+    @VisibleForTesting
+    void setDrawable(Drawable drawable) {
+        mIcon = drawable;
+    }
+
+    /**
+     * @return the {@link Uri} used to access the icon of custom context menu item.
+     */
+    public Uri getIconUri() {
+        return mIconUri;
+    }
+
+    /**
+     * Set the listener used to notify when {@link Bitmap} from the uri is available.
+     */
+    @VisibleForTesting
+    void setIconListener(IconListener listener) {
+        mListener = listener;
+    }
+
+    /**
+     * @return the listener used to notify when {@link Bitmap} from the uri is available.
+     */
+    @VisibleForTesting
+    IconListener getIconListener() {
+        return mListener;
     }
 }
