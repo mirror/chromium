1e31daaf69a6b6ccd94ff2500172d9f4142d96b9
Finnur Thorarinsson <finnur@chromium.org>
2018-02-07 14:23:45 +0000

    Notifications: Use base::Feature to switch MD notifications on/off.
    - Removes command line flag.
    - Adds a base::Feature switch to control it.
    
    Bug: 768300
    Change-Id: I79e97b4eed9e8493e0b49571d7785f98b99a1c2c

--- ui/message_center/public/cpp/message_center_switches.cc
+++ ui/message_center/public/cpp/message_center_switches.cc
@@ -4,33 +4,19 @@
 
 #include "ui/message_center/public/cpp/message_center_switches.h"
 
-#include "base/command_line.h"
-
 namespace message_center {
 
-bool IsNewStyleNotificationEnabled() {
-// For Chrome OS, the default is Enabled.
-// For other platforms, the default is Disabled.
+const base::Feature kNewStyleNotifications {
+  "NewStyleNotifications",
 #if defined(OS_CHROMEOS)
-  // Returns true if not explicitly disabled.
-  return !base::CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kDisableMessageCenterNewStyleNotification);
+      base::FEATURE_ENABLED_BY_DEFAULT
 #else
-  // Returns true if explicitly enabled.
-  return base::CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableMessageCenterNewStyleNotification);
+      base::FEATURE_DISABLED_BY_DEFAULT
 #endif
+};
+
+bool IsNewStyleNotificationEnabled() {
+  return base::FeatureList::IsEnabled(kNewStyleNotifications);
 }
-
-namespace switches {
-
-// Flag to enable or disable new-style notification. This flag will be removed
-// once the feature gets stable.
-const char kEnableMessageCenterNewStyleNotification[] =
-    "enabled-new-style-notification";
-const char kDisableMessageCenterNewStyleNotification[] =
-    "disabled-new-style-notification";
-
-}  // namespace switches
 
 }  // namespace message_center
