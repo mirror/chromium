cfe8259234930079d03220d148a49054c1e374ab
Vadym Doroshenko <dvadym@chromium.org>
2018-02-07 17:40:24 +0100

    Use FormParser
    Bug:807623
    Cq-Include-Trybots: master.tryserver.chromium.mac:ios-simulator-cronet;master.tryserver.chromium.mac:ios-simulator-full-configs
    Change-Id: I256301ae09f9bdad06522847022d155fa0d78ba7

--- components/autofill/ios/browser/autofill_util.h
+++ components/autofill/ios/browser/autofill_util.h
@@ -5,12 +5,34 @@
 #ifndef COMPONENTS_AUTOFILL_IOS_BROWSER_AUTOFILL_UTIL_H_
 #define COMPONENTS_AUTOFILL_IOS_BROWSER_AUTOFILL_UTIL_H_
 
+#include <vector>
+
 #import "ios/web/public/web_state/web_state.h"
+
+class GURL;
 
 namespace autofill {
 
+struct FormData;
+struct FormFieldData;
+
 // Checks if current context is secure from an autofill standpoint.
 bool IsContextSecureForWebState(web::WebState* web_state);
+
+std::unique_ptr<base::Value> ParseJSON(NSString* JSONString);
+
+bool GetExtractedFormsData(NSString* formJSON,
+                           BOOL filtered,
+                           const base::string16& formName,
+                           const GURL& pageURL,
+                           std::vector<autofill::FormData>* formsData);
+bool ExtractFormData(const base::Value& form,
+                     BOOL filtered,
+                     const base::string16& formName,
+                     const GURL& pageURL,
+                     autofill::FormData* formData);
+bool ExtractFormField(const base::DictionaryValue& field,
+                      FormFieldData* fieldData);
 
 }  // namespace autofill
 
