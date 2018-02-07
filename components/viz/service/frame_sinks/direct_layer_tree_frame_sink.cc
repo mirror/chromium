4f4cb7a0a60d56516c7e976ecedfb36a8f298222
Peng Huang <penghuang@chromium.org>
2018-02-07 15:37:14 -0500

    Make vulkan works on Linux desktop
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: If2842a184754884a429be168a83790d51651ccbe

--- components/viz/service/frame_sinks/direct_layer_tree_frame_sink.cc
+++ components/viz/service/frame_sinks/direct_layer_tree_frame_sink.cc
@@ -57,8 +57,12 @@ DirectLayerTreeFrameSink::DirectLayerTreeFrameSink(
     Display* display,
     mojom::DisplayClient* display_client,
     scoped_refptr<VulkanContextProvider> vulkan_context_provider,
+    gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager,
+    SharedBitmapManager* shared_bitmap_manager,
     bool use_viz_hit_test)
-    : LayerTreeFrameSink(std::move(vulkan_context_provider)),
+    : LayerTreeFrameSink(std::move(vulkan_context_provider),
+                         gpu_memory_buffer_manager,
+                         shared_bitmap_manager),
       frame_sink_id_(frame_sink_id),
       support_manager_(support_manager),
       frame_sink_manager_(frame_sink_manager),
