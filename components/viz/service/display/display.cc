594502072acef6a8c7761985255213a32a0be66e
yiyix <yiyix@chromium.org>
2018-02-07 02:15:31 -0500

    Add Metrics to measure effectiveness of Draw Occlusion
    In this patch, I added some metrics to measure effectiveness of draw occlusion
    so that we can get more meaningful data from finch trials. Metrics added
    includes measuring the number of quads that are removed or resized, total
    area of drawing that are skipped, and total number draw quads that need to
    go through complex transforms.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel
    Change-Id: I354c95a3ac5525ab81694ad67f4f1b183c86f0cb

--- components/viz/service/display/display.cc
+++ components/viz/service/display/display.cc
@@ -367,7 +367,6 @@ bool Display::DrawAndSwap() {
   bool should_swap = should_draw && size_matches;
   if (should_swap) {
     swapped_since_resize_ = true;
-
     if (scheduler_) {
       frame.metadata.latency_info.emplace_back(ui::SourceEventType::FRAME);
       frame.metadata.latency_info.back().AddLatencyNumberWithTimestamp(
@@ -393,7 +392,6 @@ bool Display::DrawAndSwap() {
       scheduler_->DidSwapBuffers();
   } else {
     TRACE_EVENT_INSTANT0("viz", "Swap skipped.", TRACE_EVENT_SCOPE_THREAD);
-
     if (have_damage && !size_matches)
       aggregator_->SetFullDamageForSurface(current_surface_id_);
 
@@ -568,20 +566,70 @@ void Display::RemoveOverdrawQuads(CompositorFrame* frame) {
       settings_.kMinimumDrawOcclusionSize.height() * device_scale_factor_;
   int minimum_draw_occlusion_width =
       settings_.kMinimumDrawOcclusionSize.width() * device_scale_factor_;
+
+  // The minimum size required to apply the layer occlusion.
+  gfx::Size layer_occlusion_skip_rect_size(160 * device_scale_factor_,
+                                           160 * device_scale_factor_);
+
+  // Record the total number of DrawQuads that are skipped from applying draw
+  // occlusion.
+  size_t total_quad_skipped = 0;
+
+  // Record the total number of DrawQuads that are not shown on screen so that
+  // it's removed by draw occlusion.
+  size_t total_quad_removed = 0;
+
+  // Record the total number of DrawQuads that are partially shown on screen so
+  // that its visible rect is updated by draw occlusion.
+  size_t total_quad_resized = 0;
+
+  // Record the total number of DrawQuads has non scale or translation
+  // transform.
+  size_t total_quad_with_complex_transform = 0;
+
+  // Record the total number of DrawQudas has a visible rect that are smaller
+  // than 160x160
+  size_t total_small_quads = 0;
+
+  // Total quad area to be drawn on screen.
+  size_t total_quad_area_to_be_drawn_px = 0;
+
+  // Total area not skipped by draw occlusion.
+  size_t total_area_saved_in_px = 0;
+
   for (const auto& pass : frame->render_pass_list) {
     // TODO(yiyix): Add filter effects to draw occlusion calculation and perform
     // draw occlusion on render pass.
-    if (!pass->filters.IsEmpty() || !pass->background_filters.IsEmpty())
+    if (!pass->filters.IsEmpty() || !pass->background_filters.IsEmpty()) {
+      for (auto* const quad : pass->quad_list) {
+        total_quad_area_to_be_drawn_px +=
+            quad->visible_rect.height() * quad->visible_rect.width();
+      }
       continue;
+    }
 
     // TODO(yiyix): Perform draw occlusion inside the render pass with
     // transparent background.
-    if (pass != frame->render_pass_list.back())
+    if (pass != frame->render_pass_list.back()) {
+      for (auto* const quad : pass->quad_list) {
+        total_quad_area_to_be_drawn_px +=
+            quad->visible_rect.height() * quad->visible_rect.width();
+      }
       continue;
+    }
 
     auto quad_list_end = pass->quad_list.end();
     gfx::Rect occlusion_in_quad_content_space;
     for (auto quad = pass->quad_list.begin(); quad != quad_list_end;) {
+      total_quad_area_to_be_drawn_px +=
+          quad->visible_rect.height() * quad->visible_rect.width();
+      if (quad->visible_rect.width() <=
+              layer_occlusion_skip_rect_size.width() &&
+          quad->visible_rect.height() <=
+              layer_occlusion_skip_rect_size.height()) {
+        total_small_quads += 1;
+      }
+
       // Skip quad if it is a RenderPassDrawQuad because RenderPassDrawQuad is a
       // special type of DrawQuad where the visible_rect of shared quad state is
       // not entirely covered by draw quads in it; or the DrawQuad size is
@@ -590,6 +638,7 @@ void Display::RemoveOverdrawQuads(CompositorFrame* frame) {
           (quad->visible_rect.width() <= minimum_draw_occlusion_width &&
            quad->visible_rect.height() <= minimum_draw_occlusion_height)) {
         ++quad;
+        total_quad_skipped += 1;
         continue;
       }
 
@@ -646,6 +695,7 @@ void Display::RemoveOverdrawQuads(CompositorFrame* frame) {
               cc::MathUtil::MapEnclosedRectWith2dAxisAlignedTransform(
                   reverse_transform, occlusion_in_target_space.bounds());
         } else {
+          total_quad_with_complex_transform += 1;
           occlusion_in_quad_content_space = gfx::Rect();
         }
       }
@@ -659,14 +709,23 @@ void Display::RemoveOverdrawQuads(CompositorFrame* frame) {
         // Case 1: for simple transforms (scale or translation), define the
         // occlusion region in the quad content space. If the |quad| is not
         // shown on the screen, then remove |quad| from the compositor frame.
+        total_area_saved_in_px +=
+            quad->visible_rect.height() * quad->visible_rect.width();
         quad = pass->quad_list.EraseAndInvalidateAllPointers(quad);
+        total_quad_removed += 1;
 
       } else if (occlusion_in_quad_content_space.Intersects(
                      quad->visible_rect)) {
         // Case 2: for simple transforms, if the quad is partially shown on
         // screen and the region formed by (occlusion region - visible_rect) is
         // a rect, then update visible_rect to the resulting rect.
+        gfx::Rect origin_rect = quad->visible_rect;
         quad->visible_rect.Subtract(occlusion_in_quad_content_space);
+        if (!quad->visible_rect.Contains(origin_rect)) {
+          total_quad_resized += 1;
+          origin_rect.Subtract(quad->visible_rect);
+          total_area_saved_in_px += origin_rect.height() * origin_rect.width();
+        }
         ++quad;
       } else if (occlusion_in_quad_content_space.IsEmpty() &&
                  occlusion_in_target_space.Contains(
@@ -675,12 +734,33 @@ void Display::RemoveOverdrawQuads(CompositorFrame* frame) {
         // Case 3: for non simple transforms, define the occlusion region in
         // target space. If the |quad| is not shown on the screen, then remove
         // |quad| from the compositor frame.
+        total_area_saved_in_px +=
+            quad->visible_rect.height() * quad->visible_rect.width();
         quad = pass->quad_list.EraseAndInvalidateAllPointers(quad);
+        total_quad_removed += 1;
       } else {
         ++quad;
       }
     }
   }
+  UMA_HISTOGRAM_COUNTS_1000("Compositing.Display.Draw.Quads.Skipped",
+                            total_quad_skipped);
+  UMA_HISTOGRAM_COUNTS_1000("Compositing.Display.Draw.Quads.Removed",
+                            total_quad_removed);
+  UMA_HISTOGRAM_COUNTS_1000("Compositing.Display.Draw.Quads.Resized",
+                            total_quad_resized);
+  UMA_HISTOGRAM_COUNTS_1000(
+      "Compositing.Display.Draw.Quads.With.Complex.Transform",
+      total_quad_with_complex_transform);
+  UMA_HISTOGRAM_COUNTS_1000("Compositing.Display.Draw.Quads.Small.Quads",
+                            total_small_quads);
+  UMA_HISTOGRAM_COUNTS_1000(
+      "Compositing.Display.Draw.Occlusion.Percentage.Saved",
+      (float)total_area_saved_in_px * 100 /
+          (float)total_quad_area_to_be_drawn_px);
+  UMA_HISTOGRAM_COUNTS_1000(
+      "Compositing.Display.Draw.Occlusion.Drawing.Area.Saved",
+      total_area_saved_in_px);
 }
 
 }  // namespace viz
