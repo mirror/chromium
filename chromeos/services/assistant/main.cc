3737be3c163ce4be9362568209c92d356a3510f4
Muyuan Li <muyuanli@chromium.org>
2018-02-07 17:10:08 -0800

    WIP: inintialize assistant service.
    Change-Id: I3578517e78dffa6139616913c3ef2c02f956a699

--- chromeos/services/assistant/main.cc
+++ chromeos/services/assistant/main.cc
@@ -2,11 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+#include <utility>
+
 #include "services/service_manager/public/c/main.h"
 #include "chromeos/services/assistant/service.h"
 #include "services/service_manager/public/cpp/service_runner.h"
 
 MojoResult ServiceMain(MojoHandle service_request_handle) {
-  service_manager::ServiceRunner runner(new chromeos::assistant::Service());
+  auto assistant_delegate = std::make_unique<
+      chromeos::assistant::Service::AssistantServiceDelegate>();
+  service_manager::ServiceRunner runner(
+      new chromeos::assistant::Service(assistant_delegate.get()));
   return runner.Run(service_request_handle);
 }
