8377293fde5bdeb18114cb9cee1f66e0c89445c3
Yuzhu Shen <yzshen@chromium.org>
2018-02-07 16:58:45 -0800

    Android WebView: support feature flag S13nSafeBrowsingCheckByURLLoaderThrottle.
    Bug: 807399
    Change-Id: I622690df526d76ac0a9b5896c1b6bccbdc36b1aa

--- chrome/browser/safe_browsing/url_checker_delegate_impl.cc
+++ chrome/browser/safe_browsing/url_checker_delegate_impl.cc
@@ -90,7 +90,11 @@ bool UrlCheckerDelegateImpl::IsUrlWhitelisted(const GURL& url) {
 
 bool UrlCheckerDelegateImpl::IsRequestWhitelisted(
     content::ResourceContext* resource_context,
-    const GURL& original_url) {
+    const GURL& original_url,
+    int frame_tree_node_id,
+    int render_process_id,
+    int render_frame_id,
+    bool originated_from_service_worker) {
   return !base::FeatureList::IsEnabled(network::features::kNetworkService) &&
          IsDataReductionProxyEnabledForUrl(resource_context, original_url);
 }
