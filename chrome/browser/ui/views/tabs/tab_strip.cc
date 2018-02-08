39c9de1606cc75153839c617de166f88efbbf799
Malay Keshav <malaykeshav@chromium.org>
2018-02-07 18:47:54 -0800

    Implements Touch Optimized design for tab and tabstrip
    This patch adds multiple layout changes to the tab and tabstrip under
    the new touch optimized UI standard.
    
    These changes include:
     - Increasing the tab and tabstrip height.
     - Adjusting tab content layout including the title, close tab button
       and the alert indicator.
     - Decreasing the tab touch width and increasing the tab standard width.
     - Adds the touch optimized tab close button icon.
     - Adds the basic color constants required for touch optimized UI.
       These colors are also added to the tab, tab strip and their contents.
     - Adds the tab alert indicator icons and their colors.
    
    What this patch does not modify in tab and tabstrp?
     - Separator colors.
     - Typography.
     - New tab button.
    
    The most recent screenshots for the change are posted on the bug below.
    
    Bug:805762
    Change-Id: Id7c1fc92f0a78a3429ff695b96f122374cbb1202
    Component: Tabs, tabstrip, material design controller, layout constants

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -45,6 +45,7 @@
 #include "ui/base/default_theme_provider.h"
 #include "ui/base/dragdrop/drag_drop_types.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/material_design/material_design_controller.h"
 #include "ui/base/models/list_selection_model.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/compositor/compositing_recorder.h"
@@ -845,6 +846,10 @@ bool TabStrip::IsTabPinned(const Tab* tab) const {
          controller_->IsTabPinned(model_index);
 }
 
+bool TabStrip::IsIncognito() const {
+  return controller_->IsIncognito();
+}
+
 void TabStrip::MaybeStartDrag(
     Tab* tab,
     const ui::LocatedEvent& event,
@@ -1175,8 +1180,8 @@ gfx::Size TabStrip::CalculatePreferredSize() const {
   if (touch_layout_ || adjust_layout_) {
     // For stacked tabs the minimum size is calculated as the size needed to
     // handle showing any number of tabs.
-    needed_tab_width =
-        Tab::GetTouchWidth() + (2 * kStackedPadding * kMaxStackedCount);
+    needed_tab_width = GetLayoutConstant(TAB_TOUCH_WIDTH) +
+                       (2 * kStackedPadding * kMaxStackedCount);
   } else {
     // Otherwise the minimum width is based on the actual number of tabs.
     const int pinned_tab_count = GetPinnedTabCount();
@@ -2288,7 +2293,8 @@ void TabStrip::SwapLayoutIfNecessary() {
     return;
 
   if (needs_touch) {
-    gfx::Size tab_size(Tab::GetTouchWidth(), GetLayoutConstant(TAB_HEIGHT));
+    gfx::Size tab_size(GetLayoutConstant(TAB_TOUCH_WIDTH),
+                       GetLayoutConstant(TAB_HEIGHT));
 
     const int overlap = Tab::GetOverlap();
     touch_layout_.reset(new StackedTabStripLayout(
@@ -2320,7 +2326,7 @@ bool TabStrip::NeedsTouchLayout() const {
   int normal_count = tab_count() - pinned_tab_count;
   if (normal_count <= 1 || normal_count == pinned_tab_count)
     return false;
-  return (Tab::GetTouchWidth() * normal_count -
+  return (GetLayoutConstant(TAB_TOUCH_WIDTH) * normal_count -
           Tab::GetOverlap() * (normal_count - 1)) >
          GetTabAreaWidth() - GetStartXForNormalTabs();
 }
@@ -2328,6 +2334,11 @@ bool TabStrip::NeedsTouchLayout() const {
 void TabStrip::SetResetToShrinkOnExit(bool value) {
   if (!adjust_layout_)
     return;
+
+  // We always prefer stacked layout in Touch optimzed UI. Unless stacking is
+  // not required and there is enough space for tabs on the tabstrip.
+  if (value && ui::MaterialDesignController::IsTouchOptimizedUiEnabled())
+    value = false;
 
   if (value && !stacked_layout_)
     value = false;  // We're already using shrink (not stacked) layout.
