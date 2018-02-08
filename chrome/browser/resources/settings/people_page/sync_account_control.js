27829ad1edf574faad2d24f7a1662d9647467b62
Scott Chen <scottchen@chromium.org>
2018-02-07 16:36:28 -0800

    WebUI: cr-action-menu split required config and optional config.
    Change-Id: Ic5ffc3338995cd57b19f0e1373e8753239e68519

--- chrome/browser/resources/settings/people_page/sync_account_control.js
+++ chrome/browser/resources/settings/people_page/sync_account_control.js
@@ -128,7 +128,9 @@ Polymer({
   onMenuButtonTap_: function() {
     const actionMenu =
         /** @type {!CrActionMenuElement} */ (this.$$('#menu'));
-    actionMenu.showAt(assert(this.$$('#dots')));
+    actionMenu.showAt(assert(this.$$('#dots')), {
+      anchorAlignmentY: AnchorAlignment.AFTER_END,
+    });
   },
 
   /**
